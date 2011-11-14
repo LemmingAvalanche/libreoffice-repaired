LemmingAvalanche/libreoffice-repaired

commit b45eddb55e24990df6ca9a9eb780c3c200506342

fdo#35091 do not honour ForceFocusAndToFront preference for preview

framework/source/loadenv/loadenv.cxx
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; eval:(c-set-style "bsd"); tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -1688,14 +1688,21 @@ void LoadEnv::impl_makeFrameWindowVisible(const css::uno::Reference< css::awt::X
     Window* pWindow = VCLUnoHelper::GetWindow(xWindow);
     if ( pWindow )
     {
+        bool preview(false);
+        css::uno::Any a = m_lMediaDescriptor[::comphelper::MediaDescriptor::PROP_PREVIEW()];
+        a >>= preview;
+
         bool bForceFrontAndFocus(false);
-        css::uno::Any a = ::comphelper::ConfigurationHelper::readDirectKey(
-            xSMGR,
-            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("org.openoffice.Office.Common/View")),
-            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("NewDocumentHandling")),
-            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ForceFocusAndToFront")),
-            ::comphelper::ConfigurationHelper::E_READONLY);
-        a >>= bForceFrontAndFocus;
+        if ( !preview )
+        {
+            a = ::comphelper::ConfigurationHelper::readDirectKey(
+                  xSMGR,
+                  ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("org.openoffice.Office.Common/View")),
+                  ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("NewDocumentHandling")),
+                  ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ForceFocusAndToFront")),
+                  ::comphelper::ConfigurationHelper::E_READONLY);
+            a >>= bForceFrontAndFocus;
+        }
 
         if( pWindow->IsVisible() && (bForceFrontAndFocus || bForceToFront) )
             pWindow->ToTop();

commit a1bd3812ca067e840694c1f09b1f22bfcc59cfe5

LoadEnv::impl_makeFrameWindowVisible: fix crash:
do not insert "Preview" property if it does not exist already
(dbaccess crashes after throwing uncaught exception because of this
when creating new database;
regression in b45eddb55e24990df6ca9a9eb780c3c200506342)

framework/source/loadenv/loadenv.cxx
@@ -1688,14 +1688,14 @@ void LoadEnv::impl_makeFrameWindowVisible(const css::uno::Reference< css::awt::X
     Window* pWindow = VCLUnoHelper::GetWindow(xWindow);
     if ( pWindow )
     {
-        bool preview(false);
-        css::uno::Any a = m_lMediaDescriptor[::comphelper::MediaDescriptor::PROP_PREVIEW()];
-        a >>= preview;
+        bool const preview( m_lMediaDescriptor.getUnpackedValueOrDefault(
+                ::comphelper::MediaDescriptor::PROP_PREVIEW(), sal_False) );
 
         bool bForceFrontAndFocus(false);
         if ( !preview )
         {
-            a = ::comphelper::ConfigurationHelper::readDirectKey(
+            css::uno::Any const a =
+                ::comphelper::ConfigurationHelper::readDirectKey(
                   xSMGR,
                   ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("org.openoffice.Office.Common/View")),
                   ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("NewDocumentHandling")),