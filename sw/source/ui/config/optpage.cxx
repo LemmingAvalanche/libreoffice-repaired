LemmingAvalanche/libreoffice-repaired

commit 25e9b6e9e227213294f600ebf37126f00109444d

SvStringsDtor->std::vector

sw/source/ui/config/optpage.cxx
@@ -504,11 +504,11 @@ IMPL_LINK_INLINE_START( SwAddPrinterTabPage, AutoClickHdl, CheckBox *, EMPTYARG 
 }
 IMPL_LINK_INLINE_END( SwAddPrinterTabPage, AutoClickHdl, CheckBox *, EMPTYARG )
 
-void  SwAddPrinterTabPage::SetFax( const SvStringsDtor& rFaxLst )
+void  SwAddPrinterTabPage::SetFax( const std::vector<String>& rFaxLst )
 {
     aFaxLB.InsertEntry(sNone);
-    for ( sal_uInt16 i = 0; i < rFaxLst.Count(); ++i )
-        aFaxLB.InsertEntry( *rFaxLst.GetObject(i) );
+    for(size_t i = 0; i < rFaxLst.size(); ++i)
+        aFaxLB.InsertEntry(rFaxLst[0]);
     aFaxLB.SelectEntryPos(0);
 }
 
@@ -530,14 +530,10 @@ void SwAddPrinterTabPage::PageCreated (SfxAllItemSet aSet)
     }
     if (pListItem && pListItem->GetValue())
     {
-        SvStringsDtor aFaxList;
+        std::vector<String> aFaxList;
         const std::vector<rtl::OUString>& rPrinters = Printer::GetPrinterQueues();
         for (unsigned int i = 0; i < rPrinters.size(); ++i)
-        {
-            String* pString = new String( rPrinters[i] );
-            String* &rpString = pString;
-            aFaxList.Insert(rpString, 0);
-        }
+            aFaxList.insert(aFaxList.begin(), rPrinters[i]);
         SetFax( aFaxList );
     }
 }

sw/source/ui/inc/optpage.hxx
@@ -41,7 +41,6 @@
 #include <svtools/ctrlbox.hxx>
 #include <svx/fntctrl.hxx>
 #include <fontcfg.hxx>
-class SvStringsDtor;
 class SfxPrinter;
 class SwWrtShell;
 class FontList;
@@ -148,7 +147,7 @@ public:
 
     virtual sal_Bool        FillItemSet( SfxItemSet& rSet );
     virtual void        Reset( const SfxItemSet& rSet );
-    void                SetFax( const SvStringsDtor& );
+    void                SetFax( const std::vector<String>& );
     void                SelectFax( const String& );
     void                SetPreview(sal_Bool bPrev);
     virtual void        PageCreated (SfxAllItemSet aSet);

commit c37fca38b2355d032cf30688831607565667d4b0

sw: SwAddPrinterTabPage::SetFax: fix STL conversion
regression from 25e9b6e9e227213294f600ebf37126f00109444d

sw/source/ui/config/optpage.cxx
@@ -508,7 +508,9 @@ void  SwAddPrinterTabPage::SetFax( const std::vector<String>& rFaxLst )
 {
     aFaxLB.InsertEntry(sNone);
     for(size_t i = 0; i < rFaxLst.size(); ++i)
-        aFaxLB.InsertEntry(rFaxLst[0]);
+    {
+        aFaxLB.InsertEntry(rFaxLst[i]);
+    }
     aFaxLB.SelectEntryPos(0);
 }
 