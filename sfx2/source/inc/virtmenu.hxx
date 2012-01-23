LemmingAvalanche/libreoffice-repaired

commit c592c0c701d31577c5cd7a0f7257bfb34eef8f2b

SV_DECL_PTRARR->std::vector

sfx2/source/inc/virtmenu.hxx
@@ -28,8 +28,6 @@
 #ifndef _SFXVIRTMENU_HXX
 #define _SFXVIRTMENU_HXX
 
-
-#include <svl/svarray.hxx>
 #include <sfx2/mnuitem.hxx>
 #include "mnucfga.hxx"
 
@@ -37,7 +35,7 @@ class SfxBindings;
 class Timer;
 class SfxMenuImageControl_Impl;
 
-SV_DECL_PTRARR_DEL( SfxMenuCtrlArr_Impl, SfxMenuControl*, 2, 2 )
+typedef std::vector<SfxMenuControl*> SfxMenuCtrlArr_Impl;
 
 class SAL_DLLPUBLIC_EXPORT SfxVirtualMenu
 {

sfx2/source/menu/virtmenu.cxx
@@ -66,14 +66,9 @@ using namespace ::com::sun::star::container;
 using namespace ::com::sun::star::frame;
 using namespace ::com::sun::star::uno;
 
-//=========================================================================
-
 DBG_NAME(SfxVirtualMenu)
 
-//=========================================================================
-
 typedef SfxMenuControl* SfxMenuControlPtr;
-SV_IMPL_PTRARR(SfxMenuCtrlArr_Impl, SfxMenuControlPtr);
 
 class SfxMenuImageControl_Impl : public SfxControllerItem
 {
@@ -401,7 +396,7 @@ void SfxVirtualMenu::CreateFromSVMenu()
                 pPopup = 0;
 
                 SfxMenuCtrlArr_Impl &rCtrlArr = GetAppCtrl_Impl();
-                rCtrlArr.C40_INSERT( SfxMenuControl, pMnuCtrl, rCtrlArr.Count() );
+                rCtrlArr.push_back(pMnuCtrl);
                 (pItems+nPos)->Bind( 0, nSlotId, sItemText, *pBindings);
                 pMnuCtrl->Bind( this, nSlotId, sItemText, *pBindings);
 
@@ -444,7 +439,7 @@ void SfxVirtualMenu::CreateFromSVMenu()
                         if ( pMnuCtrl )
                         {
                             SfxMenuCtrlArr_Impl &rCtrlArr = GetAppCtrl_Impl();
-                            rCtrlArr.C40_INSERT( SfxMenuControl, pMnuCtrl, rCtrlArr.Count());
+                            rCtrlArr.push_back(pMnuCtrl);
                             (pItems+nPos)->Bind( 0, nSlotId, sItemText, *pBindings);
                         }
                     }
@@ -456,7 +451,7 @@ void SfxVirtualMenu::CreateFromSVMenu()
                         if ( pMnuCtrl )
                         {
                             SfxMenuCtrlArr_Impl &rCtrlArr = GetAppCtrl_Impl();
-                            rCtrlArr.C40_INSERT( SfxMenuControl, pMnuCtrl, rCtrlArr.Count());
+                            rCtrlArr.push_back(pMnuCtrl);
                             (pItems+nPos)->Bind( 0, nSlotId, sItemText, *pBindings);
                         }
                         else
@@ -736,12 +731,11 @@ void SfxVirtualMenu::BindControllers()
     }
 
     SfxMenuCtrlArr_Impl& rCtrlArr = GetAppCtrl_Impl();
-    for ( nPos=0; nPos<rCtrlArr.Count(); nPos++ )
+    for(SfxMenuCtrlArr_Impl::const_iterator i = rCtrlArr.begin(); i != rCtrlArr.end(); ++i)
     {
-        SfxMenuControl* pCtrl = rCtrlArr[nPos];
-        sal_uInt16 nSlotId = pCtrl->GetId();
+        sal_uInt16 nSlotId = (*i)->GetId();
         if ( !pSVMenu->GetItemCommand(nSlotId).Len() )
-            pCtrl->ReBind();
+            (*i)->ReBind();
     }
 
     pBindings->LEAVEREGISTRATIONS();
@@ -761,12 +755,11 @@ void SfxVirtualMenu::UnbindControllers()
     }
 
     SfxMenuCtrlArr_Impl& rCtrlArr = GetAppCtrl_Impl();
-    for ( nPos=0; nPos<rCtrlArr.Count(); nPos++ )
+    for(SfxMenuCtrlArr_Impl::const_iterator i = rCtrlArr.begin(); i != rCtrlArr.end(); ++i)
     {
-        SfxMenuControl* pCtrl = rCtrlArr[nPos];
-        if ( pCtrl->IsBound() )
+        if((*i)->IsBound())
             // UnoController is not binded!
-            pCtrl->UnBind();
+            (*i)->UnBind();
     }
 
     pBindings->LEAVEREGISTRATIONS();

commit d84ee2ea47ed64e8a1640dd1088c6a7656914e6a

SfxVirtualMenu: fix context menu crash:
SfxMenuCtrlArr_Impl used to be a PTRARR_DEL, so use a boost::ptr_vector
to ensure elements are deleted.
(regression from c592c0c701d31577c5cd7a0f7257bfb34eef8f2b)

sfx2/source/inc/virtmenu.hxx
@@ -25,8 +25,10 @@
  * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
-#ifndef _SFXVIRTMENU_HXX
-#define _SFXVIRTMENU_HXX
+#ifndef SFXVIRTMENU_HXX
+#define SFXVIRTMENU_HXX
+
+#include <boost/ptr_container/ptr_vector.hpp>
 
 #include <sfx2/mnuitem.hxx>
 #include "mnucfga.hxx"
@@ -35,7 +37,7 @@ class SfxBindings;
 class Timer;
 class SfxMenuImageControl_Impl;
 
-typedef std::vector<SfxMenuControl*> SfxMenuCtrlArr_Impl;
+typedef ::boost::ptr_vector<SfxMenuControl> SfxMenuCtrlArr_Impl;
 
 class SAL_DLLPUBLIC_EXPORT SfxVirtualMenu
 {

sfx2/source/menu/virtmenu.cxx
@@ -731,11 +731,14 @@ void SfxVirtualMenu::BindControllers()
     }
 
     SfxMenuCtrlArr_Impl& rCtrlArr = GetAppCtrl_Impl();
-    for(SfxMenuCtrlArr_Impl::const_iterator i = rCtrlArr.begin(); i != rCtrlArr.end(); ++i)
+    for (SfxMenuCtrlArr_Impl::iterator i = rCtrlArr.begin();
+            i != rCtrlArr.end(); ++i)
     {
-        sal_uInt16 nSlotId = (*i)->GetId();
+        sal_uInt16 nSlotId = i->GetId();
         if ( !pSVMenu->GetItemCommand(nSlotId).Len() )
-            (*i)->ReBind();
+        {
+            i->ReBind();
+        }
     }
 
     pBindings->LEAVEREGISTRATIONS();
@@ -755,11 +758,14 @@ void SfxVirtualMenu::UnbindControllers()
     }
 
     SfxMenuCtrlArr_Impl& rCtrlArr = GetAppCtrl_Impl();
-    for(SfxMenuCtrlArr_Impl::const_iterator i = rCtrlArr.begin(); i != rCtrlArr.end(); ++i)
+    for (SfxMenuCtrlArr_Impl::iterator i = rCtrlArr.begin();
+            i != rCtrlArr.end(); ++i)
     {
-        if((*i)->IsBound())
-            // UnoController is not binded!
-            (*i)->UnBind();
+        if (i->IsBound())
+        {
+            // UnoController is not bound!
+            i->UnBind();
+        }
     }
 
     pBindings->LEAVEREGISTRATIONS();