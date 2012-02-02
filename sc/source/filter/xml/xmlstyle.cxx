LibreOffice/core

commit 7da57d17b6179e71c8b6d7549ad89eaf3a4a28c6

Ported calc-distributed-cell-text-*.diff from ooo-build.
This feature enables horizontal 'distributed' alignment and vertical
'justified' and 'distributed' alignments for cell contents in Calc.
Note that this feature relies on ODF 1.2 extended in order for the
relevant cell attributes to be saved in ODS.

sc/source/filter/xml/xmlstyle.cxx
@@ -35,6 +35,7 @@
 
 #include "XMLConverter.hxx"
 #include "rangeutl.hxx"
+#include "unonames.hxx"
 
 #include <xmloff/xmlnmspe.hxx>
 #include <xmloff/xmluconv.hxx>
@@ -49,8 +50,9 @@
 #include <tools/debug.hxx>
 #include <com/sun/star/util/CellProtection.hpp>
 #include <com/sun/star/table/CellOrientation.hpp>
-#include <com/sun/star/table/CellVertJustify.hpp>
+#include <com/sun/star/table/CellVertJustify2.hpp>
 #include <com/sun/star/table/CellHoriJustify.hpp>
+#include <com/sun/star/table/CellJustifyMethod.hpp>
 #include <com/sun/star/table/TableBorder.hpp>
 #include <com/sun/star/sheet/XSheetConditionalEntries.hpp>
 #include <com/sun/star/sheet/XSheetConditionalEntry.hpp>
@@ -89,6 +91,7 @@ const XMLPropertyMapEntry aXMLScCellStylesProperties[] =
     MAP( "HoriJustify", XML_NAMESPACE_FO, XML_TEXT_ALIGN, XML_TYPE_PROP_PARAGRAPH|XML_SC_TYPE_HORIJUSTIFY|MID_FLAG_MERGE_PROPERTY, 0 ),
     MAP( "HoriJustify", XML_NAMESPACE_STYLE, XML_TEXT_ALIGN_SOURCE, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_HORIJUSTIFYSOURCE|MID_FLAG_MERGE_PROPERTY, 0 ),
     MAP( "HoriJustify", XML_NAMESPACE_STYLE, XML_REPEAT_CONTENT, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_HORIJUSTIFYREPEAT|MID_FLAG_MERGE_PROPERTY, 0 ),
+    MAP( SC_UNONAME_CELLHJUS_METHOD, XML_NAMESPACE_CSS3TEXT, XML_TEXT_JUSTIFY, XML_TYPE_PROP_PARAGRAPH|XML_SC_TYPE_HORIJUSTIFY_METHOD, 0 ),
     MAP( "IsCellBackgroundTransparent", XML_NAMESPACE_FO, XML_BACKGROUND_COLOR, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_ISTRANSPARENT|MID_FLAG_MULTI_PROPERTY|MID_FLAG_MERGE_ATTRIBUTE, 0 ),
     MAP( "IsTextWrapped", XML_NAMESPACE_FO, XML_WRAP_OPTION, XML_TYPE_PROP_TABLE_CELL|XML_SC_ISTEXTWRAPPED, 0 ),
     MAP( "LeftBorder", XML_NAMESPACE_FO, XML_BORDER, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_BORDER, CTF_SC_ALLBORDER ),
@@ -116,7 +119,9 @@ const XMLPropertyMapEntry aXMLScCellStylesProperties[] =
     MAP( "UserDefinedAttributes", XML_NAMESPACE_TEXT, XML_XMLNS, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_ATTRIBUTE_CONTAINER | MID_FLAG_SPECIAL_ITEM, 0 ),
     MAP( "ValidationXML", XML_NAMESPACE_TABLE, XML_CONTENT_VALIDATION, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_BUILDIN_CMP_ONLY, CTF_SC_VALIDATION ),
     MAP( "VertJustify", XML_NAMESPACE_STYLE, XML_VERTICAL_ALIGN, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_VERTJUSTIFY, 0),
+    MAP( SC_UNONAME_CELLVJUS_METHOD, XML_NAMESPACE_STYLE, XML_VERTICAL_JUSTIFY, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_VERTJUSTIFY_METHOD, 0 ),
 //    MAP( "WritingMode", XML_NAMESPACE_STYLE, XML_WRITING_MODE, XML_TYPE_PROP_PARAGRAPH|XML_TYPE_TEXT_WRITING_MODE_WITH_DEFAULT, 0 ),
+
     MAP_END()
 };
 
@@ -877,6 +882,12 @@ const XMLPropertyHandler* XMLScPropHdlFactory::GetPropertyHandler( sal_Int32 nTy
                 pHdl = new XmlScPropHdl_PrintContent;
             }
             break;
+            case XML_SC_TYPE_HORIJUSTIFY_METHOD:
+            case XML_SC_TYPE_VERTJUSTIFY_METHOD:
+            {
+                pHdl = new XmlScPropHdl_JustifyMethod;
+            }
+            break;
             case XML_SC_TYPE_HORIJUSTIFY :
             {
                 pHdl = new XmlScPropHdl_HoriJustify;
@@ -1149,6 +1160,81 @@ sal_Bool XmlScPropHdl_PrintContent::exportXML(
         bRetval = sal_True;
     }
 
+    return bRetval;
+}
+
+
+XmlScPropHdl_JustifyMethod::~XmlScPropHdl_JustifyMethod()
+{
+}
+
+bool XmlScPropHdl_JustifyMethod::equals(
+    const ::com::sun::star::uno::Any& r1,
+    const ::com::sun::star::uno::Any& r2 ) const
+{
+    sal_Int32 nVal1, nVal2;
+
+    if((r1 >>= nVal1) && (r2 >>= nVal2))
+        return (nVal1 == nVal2);
+    return sal_False;
+}
+
+sal_Bool XmlScPropHdl_JustifyMethod::importXML(
+    const ::rtl::OUString& rStrImpValue,
+    ::com::sun::star::uno::Any& rValue,
+    const SvXMLUnitConverter& /* rUnitConverter */ ) const
+{
+    bool bRetval = false;
+
+    sal_Int32 nValue = table::CellJustifyMethod::AUTO;
+    if (IsXMLToken(rStrImpValue, XML_AUTO))
+    {
+        nValue = table::CellJustifyMethod::AUTO;
+        rValue <<= nValue;
+        bRetval = true;
+    }
+    else if (IsXMLToken(rStrImpValue, XML_DISTRIBUTE))
+    {
+        nValue = table::CellJustifyMethod::DISTRIBUTE;
+        rValue <<= nValue;
+        bRetval = true;
+    }
+    else
+        bRetval = true;
+
+    return bRetval;
+}
+
+sal_Bool XmlScPropHdl_JustifyMethod::exportXML(
+    ::rtl::OUString& rStrExpValue,
+    const ::com::sun::star::uno::Any& rValue,
+    const SvXMLUnitConverter& /* rUnitConverter */ ) const
+{
+    sal_Int32 nVal;
+    bool bRetval = false;
+
+    if (rValue >>= nVal)
+    {
+        switch (nVal)
+        {
+            case table::CellJustifyMethod::AUTO:
+            {
+                rStrExpValue = GetXMLToken(XML_AUTO);
+                bRetval = true;
+            }
+            break;
+            case table::CellJustifyMethod::DISTRIBUTE:
+            {
+                rStrExpValue = GetXMLToken(XML_DISTRIBUTE);
+                bRetval = true;
+            }
+            break;
+            default:
+            {
+                // added to avoid warnings
+            }
+        }
+    }
     return bRetval;
 }
 
@@ -1507,7 +1593,7 @@ bool XmlScPropHdl_RotateReference::equals(
     const ::com::sun::star::uno::Any& r1,
     const ::com::sun::star::uno::Any& r2 ) const
 {
-    table::CellVertJustify aReference1, aReference2;
+    sal_Int32 aReference1, aReference2;
 
     if((r1 >>= aReference1) && (r2 >>= aReference2))
         return (aReference1 == aReference2);
@@ -1521,28 +1607,28 @@ sal_Bool XmlScPropHdl_RotateReference::importXML(
 {
     sal_Bool bRetval(sal_False);
 
-    table::CellVertJustify nValue;
+    sal_Int32 nValue;
     if (IsXMLToken(rStrImpValue, XML_NONE))
     {
-        nValue = table::CellVertJustify_STANDARD;
+        nValue = table::CellVertJustify2::STANDARD;
         rValue <<= nValue;
         bRetval = sal_True;
     }
     else if (IsXMLToken(rStrImpValue, XML_BOTTOM))
     {
-        nValue = table::CellVertJustify_BOTTOM;
+        nValue = table::CellVertJustify2::BOTTOM;
         rValue <<= nValue;
         bRetval = sal_True;
     }
     else if (IsXMLToken(rStrImpValue, XML_TOP))
     {
-        nValue = table::CellVertJustify_TOP;
+        nValue = table::CellVertJustify2::TOP;
         rValue <<= nValue;
         bRetval = sal_True;
     }
     else if (IsXMLToken(rStrImpValue, XML_CENTER))
     {
-        nValue = table::CellVertJustify_CENTER;
+        nValue = table::CellVertJustify2::CENTER;
         rValue <<= nValue;
         bRetval = sal_True;
     }
@@ -1555,32 +1641,32 @@ sal_Bool XmlScPropHdl_RotateReference::exportXML(
     const ::com::sun::star::uno::Any& rValue,
     const SvXMLUnitConverter& /* rUnitConverter */ ) const
 {
-    table::CellVertJustify nVal;
+    sal_Int32 nVal;
     sal_Bool bRetval(sal_False);
 
     if(rValue >>= nVal)
     {
         switch (nVal)
         {
-            case table::CellVertJustify_BOTTOM :
+            case table::CellVertJustify2::BOTTOM :
             {
                 rStrExpValue = GetXMLToken(XML_BOTTOM);
                 bRetval = sal_True;
             }
             break;
-            case table::CellVertJustify_CENTER :
+            case table::CellVertJustify2::CENTER :
             {
                 rStrExpValue = GetXMLToken(XML_CENTER);
                 bRetval = sal_True;
             }
             break;
-            case table::CellVertJustify_STANDARD :
+            case table::CellVertJustify2::STANDARD :
             {
                 rStrExpValue = GetXMLToken(XML_NONE);
                 bRetval = sal_True;
             }
             break;
-            case table::CellVertJustify_TOP :
+            case table::CellVertJustify2::TOP :
             {
                 rStrExpValue = GetXMLToken(XML_TOP);
                 bRetval = sal_True;
@@ -1604,7 +1690,7 @@ bool XmlScPropHdl_VertJustify::equals(
     const ::com::sun::star::uno::Any& r1,
     const ::com::sun::star::uno::Any& r2 ) const
 {
-    table::CellVertJustify aReference1, aReference2;
+    sal_Int32 aReference1, aReference2;
 
     if((r1 >>= aReference1) && (r2 >>= aReference2))
         return (aReference1 == aReference2);
@@ -1618,28 +1704,34 @@ sal_Bool XmlScPropHdl_VertJustify::importXML(
 {
     sal_Bool bRetval(sal_False);
 
-    table::CellVertJustify nValue;
+    sal_Int32 nValue;
     if (IsXMLToken(rStrImpValue, XML_AUTOMATIC))
     {
-        nValue = table::CellVertJustify_STANDARD;
+        nValue = table::CellVertJustify2::STANDARD;
         rValue <<= nValue;
         bRetval = sal_True;
     }
     else if (IsXMLToken(rStrImpValue, XML_BOTTOM))
     {
-        nValue = table::CellVertJustify_BOTTOM;
+        nValue = table::CellVertJustify2::BOTTOM;
         rValue <<= nValue;
         bRetval = sal_True;
     }
     else if (IsXMLToken(rStrImpValue, XML_TOP))
     {
-        nValue = table::CellVertJustify_TOP;
+        nValue = table::CellVertJustify2::TOP;
         rValue <<= nValue;
         bRetval = sal_True;
     }
     else if (IsXMLToken(rStrImpValue, XML_MIDDLE))
     {
-        nValue = table::CellVertJustify_CENTER;
+        nValue = table::CellVertJustify2::CENTER;
+        rValue <<= nValue;
+        bRetval = sal_True;
+    }
+    else if (IsXMLToken(rStrImpValue, XML_JUSTIFY))
+    {
+        nValue = table::CellVertJustify2::BLOCK;
         rValue <<= nValue;
         bRetval = sal_True;
     }
@@ -1652,34 +1744,40 @@ sal_Bool XmlScPropHdl_VertJustify::exportXML(
     const ::com::sun::star::uno::Any& rValue,
     const SvXMLUnitConverter& /* rUnitConverter */ ) const
 {
-    table::CellVertJustify nVal;
+    sal_Int32 nVal;
     sal_Bool bRetval(sal_False);
 
     if(rValue >>= nVal)
     {
         switch (nVal)
         {
-            case table::CellVertJustify_BOTTOM :
+            case table::CellVertJustify2::BOTTOM :
             {
                 rStrExpValue = GetXMLToken(XML_BOTTOM);
                 bRetval = sal_True;
             }
             break;
-            case table::CellVertJustify_CENTER :
+            case table::CellVertJustify2::CENTER :
             {
                 rStrExpValue = GetXMLToken(XML_MIDDLE);
                 bRetval = sal_True;
             }
             break;
-            case table::CellVertJustify_STANDARD :
+            case table::CellVertJustify2::STANDARD :
             {
                 rStrExpValue = GetXMLToken(XML_AUTOMATIC);
                 bRetval = sal_True;
             }
             break;
-            case table::CellVertJustify_TOP :
+            case table::CellVertJustify2::TOP :
             {
                 rStrExpValue = GetXMLToken(XML_TOP);
+                bRetval = sal_True;
+            }
+            break;
+            case table::CellVertJustify2::BLOCK :
+            {
+                rStrExpValue = GetXMLToken(XML_JUSTIFY);
                 bRetval = sal_True;
             }
             break;

sc/inc/autoform.hxx
@@ -60,6 +60,7 @@ JP 20.07.95:
 #include <editeng/shdditem.hxx>
 #include <editeng/udlnitem.hxx>
 #include <editeng/wghtitem.hxx>
+#include <editeng/justifyitem.hxx>
 #include <svx/rotmodit.hxx>
 #include <svl/intitem.hxx>
 #include <editeng/bolnitem.hxx>

sc/inc/scitems.hxx
@@ -93,75 +93,77 @@
 #define ATTR_HANGPUNCTUATION    127
 #define ATTR_FORBIDDEN_RULES    128
 #define ATTR_HOR_JUSTIFY        129
-#define ATTR_INDENT             130
-#define ATTR_VER_JUSTIFY        131
-#define ATTR_STACKED            132
-#define ATTR_ROTATE_VALUE       133
-#define ATTR_ROTATE_MODE        134
-#define ATTR_VERTICAL_ASIAN     135
-#define ATTR_WRITINGDIR         136
-#define ATTR_LINEBREAK          137
-#define ATTR_SHRINKTOFIT        138
-#define ATTR_BORDER_TLBR        139
-#define ATTR_BORDER_BLTR        140
-#define ATTR_MARGIN             141
-#define ATTR_MERGE              142
-#define ATTR_MERGE_FLAG         143
-#define ATTR_VALUE_FORMAT       144
-#define ATTR_LANGUAGE_FORMAT    145
-#define ATTR_BACKGROUND         146
-#define ATTR_PROTECTION         147
-#define ATTR_BORDER             148
-#define ATTR_BORDER_INNER       149     // innen, wg. Vorlagen-EditDialog
-#define ATTR_SHADOW             150
-#define ATTR_VALIDDATA          151
-#define ATTR_CONDITIONAL        152
+#define ATTR_HOR_JUSTIFY_METHOD 130
+#define ATTR_INDENT             131
+#define ATTR_VER_JUSTIFY        132
+#define ATTR_VER_JUSTIFY_METHOD 133
+#define ATTR_STACKED            134
+#define ATTR_ROTATE_VALUE       135
+#define ATTR_ROTATE_MODE        136
+#define ATTR_VERTICAL_ASIAN     137
+#define ATTR_WRITINGDIR         138
+#define ATTR_LINEBREAK          139
+#define ATTR_SHRINKTOFIT        140
+#define ATTR_BORDER_TLBR        141
+#define ATTR_BORDER_BLTR        142
+#define ATTR_MARGIN             143
+#define ATTR_MERGE              144
+#define ATTR_MERGE_FLAG         145
+#define ATTR_VALUE_FORMAT       146
+#define ATTR_LANGUAGE_FORMAT    147
+#define ATTR_BACKGROUND         148
+#define ATTR_PROTECTION         149
+#define ATTR_BORDER             150
+#define ATTR_BORDER_INNER       151     // innen, wg. Vorlagen-EditDialog
+#define ATTR_SHADOW             152
+#define ATTR_VALIDDATA          153
+#define ATTR_CONDITIONAL        154
 
-#define ATTR_PATTERN_END        152     // Ende Zellattribut-Pattern
+#define ATTR_PATTERN_END        154     // Ende Zellattribut-Pattern
 
-#define ATTR_PATTERN            153
+#define ATTR_PATTERN            155
                                         // Seitenattribute
-#define ATTR_LRSPACE            154     // Editor: PageDesc-TabPage
-#define ATTR_ULSPACE            155
-#define ATTR_PAGE               156
-#define ATTR_PAGE_PAPERTRAY     157
-#define ATTR_PAGE_PAPERBIN      158
-#define ATTR_PAGE_SIZE          159
-#define ATTR_PAGE_MAXSIZE       160
-#define ATTR_PAGE_HORCENTER     161
-#define ATTR_PAGE_VERCENTER     162
+#define ATTR_LRSPACE            156     // Editor: PageDesc-TabPage
+#define ATTR_ULSPACE            157
+#define ATTR_PAGE               158
+#define ATTR_PAGE_PAPERTRAY     159
+#define ATTR_PAGE_PAPERBIN      160
+#define ATTR_PAGE_SIZE          161
+#define ATTR_PAGE_MAXSIZE       162
+#define ATTR_PAGE_HORCENTER     163
+#define ATTR_PAGE_VERCENTER     164
 
-#define ATTR_PAGE_ON            163     // Editor: Header/Footer-Page
-#define ATTR_PAGE_DYNAMIC       164
-#define ATTR_PAGE_SHARED        165
+#define ATTR_PAGE_ON            165     // Editor: Header/Footer-Page
+#define ATTR_PAGE_DYNAMIC       166
+#define ATTR_PAGE_SHARED        167
 
-#define ATTR_PAGE_NOTES         166     // Editor: Tabelle
-#define ATTR_PAGE_GRID          167
-#define ATTR_PAGE_HEADERS       168
-#define ATTR_PAGE_CHARTS        169
-#define ATTR_PAGE_OBJECTS       170
-#define ATTR_PAGE_DRAWINGS      171
-#define ATTR_PAGE_TOPDOWN       172
-#define ATTR_PAGE_SCALE         173
-#define ATTR_PAGE_SCALETOPAGES  174
-#define ATTR_PAGE_FIRSTPAGENO   175
+#define ATTR_PAGE_NOTES         168     // Editor: Tabelle
+#define ATTR_PAGE_GRID          169
+#define ATTR_PAGE_HEADERS       170
+#define ATTR_PAGE_CHARTS        171
+#define ATTR_PAGE_OBJECTS       172
+#define ATTR_PAGE_DRAWINGS      173
+#define ATTR_PAGE_TOPDOWN       174
+#define ATTR_PAGE_SCALE         175
+#define ATTR_PAGE_SCALETOPAGES  176
+#define ATTR_PAGE_FIRSTPAGENO   177
 
-#define ATTR_PAGE_PRINTAREA     176     // Editor: Druckbereiche
-#define ATTR_PAGE_REPEATROW     177
-#define ATTR_PAGE_REPEATCOL     178
-#define ATTR_PAGE_PRINTTABLES   179
+#define ATTR_PAGE_PRINTAREA     178     // Editor: Druckbereiche
+#define ATTR_PAGE_REPEATROW     179
+#define ATTR_PAGE_REPEATCOL     180
+#define ATTR_PAGE_PRINTTABLES   181
 
-#define ATTR_PAGE_HEADERLEFT    180     // Inhalte der Kopf-/
-#define ATTR_PAGE_FOOTERLEFT    181     // Fusszeilen (links)
-#define ATTR_PAGE_HEADERRIGHT   182     // Inhalte der Kopf-/
-#define ATTR_PAGE_FOOTERRIGHT   183     // Fusszeilen (rechts)
-#define ATTR_PAGE_HEADERSET     184     // die Sets dazu...
-#define ATTR_PAGE_FOOTERSET     185
+#define ATTR_PAGE_HEADERLEFT    182     // Inhalte der Kopf-/
+#define ATTR_PAGE_FOOTERLEFT    183     // Fusszeilen (links)
+#define ATTR_PAGE_HEADERRIGHT   184     // Inhalte der Kopf-/
+#define ATTR_PAGE_FOOTERRIGHT   185     // Fusszeilen (rechts)
+#define ATTR_PAGE_HEADERSET     186     // die Sets dazu...
+#define ATTR_PAGE_FOOTERSET     187
 
-#define ATTR_PAGE_FORMULAS      186
-#define ATTR_PAGE_NULLVALS      187
+#define ATTR_PAGE_FORMULAS      188
+#define ATTR_PAGE_NULLVALS      189
 
-#define ATTR_PAGE_SCALETO       188     // #i8868# scale printout to with/height
+#define ATTR_PAGE_SCALETO       190     // #i8868# scale printout to with/height
 
 #define ATTR_ENDINDEX           ATTR_PAGE_SCALETO        // Ende Pool-Range
 

sc/inc/unonames.hxx
@@ -106,6 +106,8 @@
 #define SC_UNONAME_CELLPRO          "CellProtection"
 #define SC_UNONAME_CELLHJUS         "HoriJustify"
 #define SC_UNONAME_CELLVJUS         "VertJustify"
+#define SC_UNONAME_CELLHJUS_METHOD  "HoriJustifyMethod"
+#define SC_UNONAME_CELLVJUS_METHOD  "VertJustifyMethod"
 #define SC_UNONAME_CELLORI          "Orientation"
 #define SC_UNONAME_NUMFMT           "NumberFormat"
 #define SC_UNONAME_SHADOW           "ShadowFormat"

sc/source/core/data/attarray.cxx
@@ -39,6 +39,7 @@
 #include <editeng/frmdiritem.hxx>
 #include <editeng/shaditem.hxx>
 #include <editeng/editobj.hxx>
+#include <editeng/justifyitem.hxx>
 #include <svl/poolcach.hxx>
 #include <editeng/fontitem.hxx>
 #include <unotools/fontcvt.hxx>

sc/source/core/data/column2.cxx
@@ -44,6 +44,7 @@
 #include <svx/rotmodit.hxx>
 #include <editeng/scripttypeitem.hxx>
 #include <editeng/unolingu.hxx>
+#include <editeng/justifyitem.hxx>
 #include <svl/zforlist.hxx>
 #include <svl/broadcast.hxx>
 #include <svl/listeneriter.hxx>

sc/source/core/data/docpool.cxx
@@ -67,6 +67,7 @@
 #include <editeng/wghtitem.hxx>
 #include <editeng/wrlmitem.hxx>
 #include <editeng/xmlcnitm.hxx>
+#include <editeng/justifyitem.hxx>
 
 #include "docpool.hxx"
 #include "global.hxx"
@@ -127,8 +128,10 @@ static SfxItemInfo __READONLY_DATA  aItemInfos[] =
     { 0,                            SFX_ITEM_POOLABLE },    // ATTR_HANGPUNCTUATION     from 614d
     { SID_ATTR_PARA_FORBIDDEN_RULES,SFX_ITEM_POOLABLE },    // ATTR_FORBIDDEN_RULES     from 614d
     { SID_ATTR_ALIGN_HOR_JUSTIFY,   SFX_ITEM_POOLABLE },    // ATTR_HOR_JUSTIFY
+    { SID_ATTR_ALIGN_HOR_JUSTIFY_METHOD, SFX_ITEM_POOLABLE }, // ATTR_HOR_JUSTIFY_METHOD
     { SID_ATTR_ALIGN_INDENT,        SFX_ITEM_POOLABLE },    // ATTR_INDENT          ab 350
     { SID_ATTR_ALIGN_VER_JUSTIFY,   SFX_ITEM_POOLABLE },    // ATTR_VER_JUSTIFY
+    { SID_ATTR_ALIGN_VER_JUSTIFY_METHOD, SFX_ITEM_POOLABLE }, // ATTR_VER_JUSTIFY_METHOD
     { SID_ATTR_ALIGN_STACKED,       SFX_ITEM_POOLABLE },    // ATTR_STACKED         from 680/dr14 (replaces ATTR_ORIENTATION)
     { SID_ATTR_ALIGN_DEGREES,       SFX_ITEM_POOLABLE },    // ATTR_ROTATE_VALUE    ab 367
     { SID_ATTR_ALIGN_LOCKPOS,       SFX_ITEM_POOLABLE },    // ATTR_ROTATE_MODE     ab 367
@@ -260,8 +263,10 @@ ScDocumentPool::ScDocumentPool( SfxItemPool* pSecPool, BOOL bLoadRefCounts )
     ppPoolDefaults[ ATTR_HANGPUNCTUATION - ATTR_STARTINDEX ] = new SvxHangingPunctuationItem( sal_False, ATTR_HANGPUNCTUATION);
     ppPoolDefaults[ ATTR_FORBIDDEN_RULES - ATTR_STARTINDEX ] = new SvxForbiddenRuleItem( sal_False, ATTR_FORBIDDEN_RULES);
     ppPoolDefaults[ ATTR_HOR_JUSTIFY     - ATTR_STARTINDEX ] = new SvxHorJustifyItem( SVX_HOR_JUSTIFY_STANDARD, ATTR_HOR_JUSTIFY);
+    ppPoolDefaults[ ATTR_HOR_JUSTIFY_METHOD - ATTR_STARTINDEX ] = new SvxJustifyMethodItem( SVX_JUSTIFY_METHOD_AUTO, ATTR_HOR_JUSTIFY_METHOD);
     ppPoolDefaults[ ATTR_INDENT          - ATTR_STARTINDEX ] = new SfxUInt16Item( ATTR_INDENT, 0 );
     ppPoolDefaults[ ATTR_VER_JUSTIFY     - ATTR_STARTINDEX ] = new SvxVerJustifyItem( SVX_VER_JUSTIFY_STANDARD, ATTR_VER_JUSTIFY);
+    ppPoolDefaults[ ATTR_VER_JUSTIFY_METHOD - ATTR_STARTINDEX ] = new SvxJustifyMethodItem( SVX_JUSTIFY_METHOD_AUTO, ATTR_VER_JUSTIFY_METHOD);
     ppPoolDefaults[ ATTR_STACKED         - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_STACKED, FALSE );
     ppPoolDefaults[ ATTR_ROTATE_VALUE    - ATTR_STARTINDEX ] = new SfxInt32Item( ATTR_ROTATE_VALUE, 0 );
     ppPoolDefaults[ ATTR_ROTATE_MODE     - ATTR_STARTINDEX ] = new SvxRotateModeItem( SVX_ROTATE_MODE_BOTTOM, ATTR_ROTATE_MODE );

sc/source/core/data/dpoutput.cxx
@@ -37,6 +37,7 @@
 #include <editeng/boxitem.hxx>
 #include <editeng/brshitem.hxx>
 #include <editeng/wghtitem.hxx>
+#include <editeng/justifyitem.hxx>
 #include <unotools/transliterationwrapper.hxx>
 
 #include "dpoutput.hxx"

LemmingAvalanche/libreoffice-repaired

commit 1911255dcbca8eb1c55cdd58282182588bfcb7b1

fdo#45450: sc: ODF export: cell styles:
Only write "style:vertical-justify" and "css3t:text-justify" in ODF
extended mode.
(regression from 7da57d17b6179e71c8b6d7549ad89eaf3a4a28c6)

sc/source/filter/xml/xmlstyle.cxx
@@ -90,7 +90,7 @@ const XMLPropertyMapEntry aXMLScCellStylesProperties[] =
     MAP( "HoriJustify", XML_NAMESPACE_FO, XML_TEXT_ALIGN, XML_TYPE_PROP_PARAGRAPH|XML_SC_TYPE_HORIJUSTIFY|MID_FLAG_MERGE_PROPERTY, 0 ),
     MAP( "HoriJustify", XML_NAMESPACE_STYLE, XML_TEXT_ALIGN_SOURCE, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_HORIJUSTIFYSOURCE|MID_FLAG_MERGE_PROPERTY, 0 ),
     MAP( "HoriJustify", XML_NAMESPACE_STYLE, XML_REPEAT_CONTENT, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_HORIJUSTIFYREPEAT|MID_FLAG_MERGE_PROPERTY, 0 ),
-    MAP( SC_UNONAME_CELLHJUS_METHOD, XML_NAMESPACE_CSS3TEXT, XML_TEXT_JUSTIFY, XML_TYPE_PROP_PARAGRAPH|XML_SC_TYPE_HORIJUSTIFY_METHOD, 0 ),
+    MAP_EXT( SC_UNONAME_CELLHJUS_METHOD, XML_NAMESPACE_CSS3TEXT, XML_TEXT_JUSTIFY, XML_TYPE_PROP_PARAGRAPH|XML_SC_TYPE_HORIJUSTIFY_METHOD, 0 ),
     MAP( "IsCellBackgroundTransparent", XML_NAMESPACE_FO, XML_BACKGROUND_COLOR, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_ISTRANSPARENT|MID_FLAG_MULTI_PROPERTY|MID_FLAG_MERGE_ATTRIBUTE, 0 ),
     MAP( "IsTextWrapped", XML_NAMESPACE_FO, XML_WRAP_OPTION, XML_TYPE_PROP_TABLE_CELL|XML_SC_ISTEXTWRAPPED, 0 ),
     MAP( "LeftBorder", XML_NAMESPACE_FO, XML_BORDER, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_BORDER, CTF_SC_ALLBORDER ),
@@ -117,7 +117,7 @@ const XMLPropertyMapEntry aXMLScCellStylesProperties[] =
     MAP( "UserDefinedAttributes", XML_NAMESPACE_TEXT, XML_XMLNS, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_ATTRIBUTE_CONTAINER | MID_FLAG_SPECIAL_ITEM, 0 ),
     MAP( "ValidationXML", XML_NAMESPACE_TABLE, XML_CONTENT_VALIDATION, XML_TYPE_PROP_TABLE_CELL|XML_TYPE_BUILDIN_CMP_ONLY, CTF_SC_VALIDATION ),
     MAP( "VertJustify", XML_NAMESPACE_STYLE, XML_VERTICAL_ALIGN, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_VERTJUSTIFY, 0),
-    MAP( SC_UNONAME_CELLVJUS_METHOD, XML_NAMESPACE_STYLE, XML_VERTICAL_JUSTIFY, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_VERTJUSTIFY_METHOD, 0 ),
+    MAP_EXT( SC_UNONAME_CELLVJUS_METHOD, XML_NAMESPACE_STYLE, XML_VERTICAL_JUSTIFY, XML_TYPE_PROP_TABLE_CELL|XML_SC_TYPE_VERTJUSTIFY_METHOD, 0 ),
 
     MAP_END()
 };