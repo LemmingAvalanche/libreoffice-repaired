LemmingAvalanche/libreoffice-repaired

commit 3be8cfc12f3ff619defe4279034d3f88eebf4fc4

eliminate SvUShorts type

sw/source/core/docnode/ndtbl1.cxx
@@ -1301,7 +1301,7 @@ sal_uInt16 lcl_CalcCellFit( const SwLayoutFrm *pCell )
  *dieser erhalten, kleinere Wuensche werden ueberschrieben.
  */
 
-void lcl_CalcSubColValues( SvUShorts &rToFill, const SwTabCols &rCols,
+void lcl_CalcSubColValues( std::vector<sal_uInt16> &rToFill, const SwTabCols &rCols,
                               const SwLayoutFrm *pCell, const SwLayoutFrm *pTab,
                               sal_Bool bWishValues )
 {
@@ -1361,7 +1361,7 @@ void lcl_CalcSubColValues( SvUShorts &rToFill, const SwTabCols &rCols,
  *                      schneidet wird der Minimalwert ermittelt.
  */
 
-void lcl_CalcColValues( SvUShorts &rToFill, const SwTabCols &rCols,
+void lcl_CalcColValues( std::vector<sal_uInt16> &rToFill, const SwTabCols &rCols,
                            const SwLayoutFrm *pStart, const SwLayoutFrm *pEnd,
                            sal_Bool bWishValues )
 {
@@ -1471,14 +1471,14 @@ void SwDoc::AdjustCellWidth( const SwCursor& rCursor, sal_Bool bBalance )
         return;
 
     const sal_uInt8 nTmp = (sal_uInt8)Max( sal_uInt16(255), sal_uInt16(aTabCols.Count() + 1) );
-    SvUShorts aWish( nTmp, nTmp ),
+    std::vector<sal_uInt16> aWish( nTmp, nTmp ),
               aMins( nTmp, nTmp );
     sal_uInt16 i;
 
     for ( i = 0; i <= aTabCols.Count(); ++i )
     {
-        aWish.Insert( sal_uInt16(0), aWish.Count() );
-        aMins.Insert( sal_uInt16(0), aMins.Count() );
+        aWish.push_back( 0 );
+        aMins.push_back( 0 );
     }
     ::lcl_CalcColValues( aWish, aTabCols, pStart, pEnd, sal_True  );
 
@@ -1511,7 +1511,7 @@ void SwDoc::AdjustCellWidth( const SwCursor& rCursor, sal_Bool bBalance )
             }
         }
         nWish = nWish / nCnt;
-        for ( i = 0; i < aWish.Count(); ++i )
+        for ( i = 0; i < aWish.size(); ++i )
             if ( aWish[i] )
                 aWish[i] = nWish;
     }

cui/source/dialogs/iconcdlg.cxx
@@ -30,8 +30,6 @@
 #include <tools/rc.h>
 #include <tools/shl.hxx>
 
-#define _SVSTDARR_USHORTS
-#include <svl/svstdarr.hxx>
 #include <dialmgr.hxx>
 
 #include "iconcdlg.hxx"
@@ -44,11 +42,6 @@
 #include <vcl/i18nhelp.hxx>
 
 using ::std::vector;
-
-int SAL_CALL IconcDlgCmpUS_Impl( const void* p1, const void* p2 )
-{
-    return *(sal_uInt16*)p1 - *(sal_uInt16*)p2;
-}
 
 // some stuff for easier changes for SvtViewOptions
 static const sal_Char*      pViewOptDataName = "dialog data";
@@ -962,7 +955,7 @@ const sal_uInt16* IconChoiceDialog::GetInputRanges( const SfxItemPool& rPool )
 
     if ( pRanges )
         return pRanges;
-    SvUShorts aUS( 16, 16 );
+    std::vector<sal_uInt16> aUS;
 
     size_t nCount = maPageList.size();
     for ( size_t i = 0; i < nCount; ++i )
@@ -976,32 +969,26 @@ const sal_uInt16* IconChoiceDialog::GetInputRanges( const SfxItemPool& rPool )
             sal_uInt16 nLen;
             for( nLen = 0; *pIter; ++nLen, ++pIter )
                 ;
-            aUS.Insert( pTmpRanges, nLen, aUS.Count() );
+            aUS.insert( aUS.end(), pTmpRanges, pTmpRanges + nLen );
         }
     }
 
     // remove double Id's
     {
-        nCount = aUS.Count();
+        nCount = aUS.size();
         for ( size_t i = 0; i < nCount; ++i )
             aUS[i] = rPool.GetWhich( aUS[i] );
     }
 
     // sortieren
-    if ( aUS.Count() > 1 )
+    if ( aUS.size() > 1 )
     {
-#if defined __SUNPRO_CC
-#pragma disable_warn
-#endif
-        qsort( (void*)aUS.GetData(), aUS.Count(), sizeof(sal_uInt16), IconcDlgCmpUS_Impl );
-#if defined __SUNPRO_CC
-#pragma enable_warn
-#endif
+        std::sort( aUS.begin(), aUS.end() );
     }
 
-    pRanges = new sal_uInt16[aUS.Count() + 1];
-    memcpy(pRanges, aUS.GetData(), sizeof(sal_uInt16) * aUS.Count());
-    pRanges[aUS.Count()] = 0;
+    pRanges = new sal_uInt16[aUS.size() + 1];
+    std::copy( aUS.begin(), aUS.end(), pRanges );
+    pRanges[aUS.size()] = 0;
 
     return pRanges;
 }

cui/source/inc/cfg.hxx
@@ -49,9 +49,6 @@
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/lang/XSingleComponentFactory.hpp>
 
-#define _SVSTDARR_USHORTS
-#define _SVSTDARR_STRINGSDTOR
-#include <svl/svstdarr.hxx>     // SvUShorts
 #include <sfx2/minarray.hxx>
 #include <sfx2/tabdlg.hxx>
 #include <vector>

cui/source/inc/selector.hxx
@@ -39,9 +39,6 @@
 #include <com/sun/star/container/XNameAccess.hpp>
 #include <com/sun/star/script/browse/XBrowseNode.hpp>
 
-#define _SVSTDARR_USHORTS
-#define _SVSTDARR_STRINGSDTOR
-#include <svl/svstdarr.hxx>     // SvUShorts
 #include <sfx2/minarray.hxx>
 
 #define SVX_CFGGROUP_FUNCTION 1

editeng/inc/editeng/svxrtf.hxx
@@ -34,8 +34,6 @@
 #include <svl/itemset.hxx>
 #include <svtools/parrtf.hxx>
 
-#define _SVSTDARR_sal_uInt16S
-#include <svl/svstdarr.hxx>
 #include <editeng/editengdllapi.h>
 
 #include <deque>
@@ -231,9 +229,9 @@ class EDITENG_DLLPUBLIC SvxRTFParser : public SvRTFParser
     SvxRTFItemStack aAttrStack;
     SvxRTFItemStackList aAttrSetList;
 
-    SvUShorts aPlainMap;
-    SvUShorts aPardMap;
-    SvUShorts aWhichMap;
+    std::vector<sal_uInt16> aPlainMap;
+    std::vector<sal_uInt16> aPardMap;
+    std::vector<sal_uInt16> aWhichMap;
     String  sBaseURL;
 
     SvxPosition* pInsPos;
@@ -360,8 +358,8 @@ protected:
 
     // Query/Set the mapping IDs for the Pard/Plain attributes
     //(Set: It is noted in the pointers, which thus does not create a copy)
-    void AddPardAttr( sal_uInt16 nWhich ) { aPardMap.Insert( nWhich, aPardMap.Count() ); }
-    void AddPlainAttr( sal_uInt16 nWhich ) { aPlainMap.Insert( nWhich, aPlainMap.Count() ); }
+    void AddPardAttr( sal_uInt16 nWhich ) { aPardMap.push_back( nWhich ); }
+    void AddPlainAttr( sal_uInt16 nWhich ) { aPlainMap.push_back( nWhich ); }
 
     SvxRTFStyleTbl& GetStyleTbl()               { return aStyleTbl; }
     SvxRTFItemStack& GetAttrStack()             { return aAttrStack; }
@@ -393,9 +391,9 @@ public:
     void SetAttrPool( SfxItemPool* pNewPool )   { pAttrPool = pNewPool; }
     // to set different WhichIds for a different pool.
     RTFPardAttrMapIds& GetPardMap()
-                        { return (RTFPardAttrMapIds&)*aPardMap.GetData(); }
+                        { return (RTFPardAttrMapIds&)*aPardMap.begin(); }
     RTFPlainAttrMapIds& GetPlainMap()
-                        { return (RTFPlainAttrMapIds&)*aPlainMap.GetData(); }
+                        { return (RTFPlainAttrMapIds&)*aPlainMap.begin(); }
     // to be able to assign them from the outside as for example table cells
     void ReadBorderAttr( int nToken, SfxItemSet& rSet, int bTableDef=sal_False );
     void ReadBackgroundAttr( int nToken, SfxItemSet& rSet, int bTableDef=sal_False  );

editeng/source/rtf/rtfitem.cxx
@@ -109,14 +109,14 @@ inline const SvxLRSpaceItem& GetLRSpace(const SfxItemSet& rSet,sal_uInt16 nId,sa
 inline const SvxULSpaceItem& GetULSpace(const SfxItemSet& rSet,sal_uInt16 nId,sal_Bool bInP=sal_True)
     { return (const SvxULSpaceItem&)rSet.Get( nId,bInP); }
 
-#define PARDID      ((RTFPardAttrMapIds*)aPardMap.GetData())
-#define PLAINID     ((RTFPlainAttrMapIds*)aPlainMap.GetData())
+#define PARDID      ((RTFPardAttrMapIds*)&aPardMap[0])
+#define PLAINID     ((RTFPlainAttrMapIds*)&aPlainMap[0])
 
 void SvxRTFParser::SetScriptAttr( RTF_CharTypeDef eType, SfxItemSet& rSet,
                                     SfxPoolItem& rItem )
 {
     const sal_uInt16 *pNormal = 0, *pCJK = 0, *pCTL = 0;
-    const RTFPlainAttrMapIds* pIds = (RTFPlainAttrMapIds*)aPlainMap.GetData();
+    const RTFPlainAttrMapIds* pIds = (RTFPlainAttrMapIds*)&aPlainMap[0];
     switch( rItem.Which() )
     {
     case SID_ATTR_CHAR_FONT:
@@ -1761,13 +1761,13 @@ void SvxRTFParser::RTFPardPlain( int bPard, SfxItemSet** ppSet )
             if( bPard )
             {
                 pAkt->nStyleNo = 0;
-                pPtr = aPardMap.GetData();
-                nCnt = aPardMap.Count();
+                pPtr = &aPardMap[0];
+                nCnt = aPardMap.size();
             }
             else
             {
-                pPtr = aPlainMap.GetData();
-                nCnt = aPlainMap.Count();
+                pPtr = &aPlainMap[0];
+                nCnt = aPlainMap.size();
             }
 
             for( sal_uInt16 n = 0; n < nCnt; ++n, ++pPtr )
@@ -1827,7 +1827,7 @@ void SvxRTFParser::SetDefault( int nToken, int nValue )
     if( !bNewDoc )
         return;
 
-    SfxItemSet aTmp( *pAttrPool, aWhichMap.GetData() );
+    SfxItemSet aTmp( *pAttrPool, &aWhichMap[0] );
     sal_Bool bOldFlag = bIsLeftToRightDef;
     bIsLeftToRightDef = sal_True;
     switch( nToken )

editeng/source/rtf/svxrtf.cxx
@@ -94,13 +94,13 @@ SvxRTFParser::SvxRTFParser( SfxItemPool& rPool, SvStream& rIn,
 
     {
         RTFPlainAttrMapIds aTmp( rPool );
-        aPlainMap.Insert( (sal_uInt16*)&aTmp,
-                    sizeof( RTFPlainAttrMapIds ) / sizeof(sal_uInt16), 0 );
+        aPlainMap.insert( aPlainMap.begin(), (sal_uInt16*)&aTmp,
+                (sal_uInt16*)&aTmp + (sizeof( RTFPlainAttrMapIds ) / sizeof(sal_uInt16)) );
     }
     {
         RTFPardAttrMapIds aTmp( rPool );
-        aPardMap.Insert( (sal_uInt16*)&aTmp,
-                    sizeof( RTFPardAttrMapIds ) / sizeof(sal_uInt16), 0 );
+        aPardMap.insert( aPardMap.begin(), (sal_uInt16*)&aTmp,
+                (sal_uInt16*)&aTmp + (sizeof( RTFPardAttrMapIds ) / sizeof(sal_uInt16)) );
     }
     pDfltFont = new Font;
     pDfltColor = new Color;
@@ -340,7 +340,7 @@ void SvxRTFParser::ReadStyleTable()
     int nToken, bSaveChkStyleAttr = bChkStyleAttr;
     short nStyleNo = 0;
     int _nOpenBrakets = 1;      // the first was already detected earlier!!
-    SvxRTFStyleType* pStyle = new SvxRTFStyleType( *pAttrPool, aWhichMap.GetData() );
+    SvxRTFStyleType* pStyle = new SvxRTFStyleType( *pAttrPool, &aWhichMap[0] );
     pStyle->aAttrSet.Put( GetRTFDefaults() );
 
     bIsInReadStyleTab = sal_True;
@@ -396,7 +396,7 @@ void SvxRTFParser::ReadStyleTable()
                 }
                 // All data from the font is available, so off to the table
                 aStyleTbl.Insert( nStyleNo, pStyle );
-                pStyle = new SvxRTFStyleType( *pAttrPool, aWhichMap.GetData() );
+                pStyle = new SvxRTFStyleType( *pAttrPool, &aWhichMap[0] );
                 pStyle->aAttrSet.Put( GetRTFDefaults() );
                 nStyleNo = 0;
             }
@@ -841,7 +841,7 @@ const Font& SvxRTFParser::GetFont( sal_uInt16 nId )
     {
         const SvxFontItem& rDfltFont = (const SvxFontItem&)
                         pAttrPool->GetDefaultItem(
-                    ((RTFPlainAttrMapIds*)aPlainMap.GetData())->nFont );
+                    ((RTFPlainAttrMapIds*)&aPlainMap[0])->nFont );
         pDfltFont->SetName( rDfltFont.GetStyleName() );
         pDfltFont->SetFamily( rDfltFont.GetFamily() );
         pFont = pDfltFont;
@@ -856,7 +856,7 @@ SvxRTFItemStackType* SvxRTFParser::_GetAttrSet( int bCopyAttr )
     if( pAkt )
         pNew = new SvxRTFItemStackType( *pAkt, *pInsPos, bCopyAttr );
     else
-        pNew = new SvxRTFItemStackType( *pAttrPool, aWhichMap.GetData(),
+        pNew = new SvxRTFItemStackType( *pAttrPool, &aWhichMap[0],
                                         *pInsPos );
     pNew->SetRTFDefaults( GetRTFDefaults() );
 
@@ -982,7 +982,7 @@ void SvxRTFParser::AttrGroupEnd()   // process the current, delete from Stack
                         pNew->aAttrSet.SetParent( pOld->aAttrSet.GetParent() );
 
                         // Delete all paragraph attributes from pNew
-                        for( sal_uInt16 n = 0; n < aPardMap.Count() &&
+                        for( sal_uInt16 n = 0; n < aPardMap.size() &&
                                             pNew->aAttrSet.Count(); ++n )
                             if( aPardMap[n] )
                                 pNew->aAttrSet.ClearItem( aPardMap[n] );
@@ -1132,24 +1132,23 @@ void SvxRTFParser::SetAttrInDoc( SvxRTFItemStackType & )
 
 void SvxRTFParser::BuildWhichTbl()
 {
-    if( aWhichMap.Count() )
-        aWhichMap.Remove( 0, aWhichMap.Count() );
-    aWhichMap.Insert( (sal_uInt16)0, (sal_uInt16)0 );
+    aWhichMap.clear();
+    aWhichMap.push_back( 0 );
 
     // Building a Which-Map 'rWhichMap' from an Array of
     // 'pWhichIds' frm Which-Ids. It has the long 'nWhichIds'.
     // The Which-Map is not going to be deleted.
-    SvParser::BuildWhichTbl( aWhichMap, (sal_uInt16*)aPardMap.GetData(), aPardMap.Count() );
-    SvParser::BuildWhichTbl( aWhichMap, (sal_uInt16*)aPlainMap.GetData(), aPlainMap.Count() );
+    SvParser::BuildWhichTbl( aWhichMap, (sal_uInt16*)&aPardMap[0], aPardMap.size() );
+    SvParser::BuildWhichTbl( aWhichMap, (sal_uInt16*)&aPlainMap[0], aPlainMap.size() );
 }
 
 const SfxItemSet& SvxRTFParser::GetRTFDefaults()
 {
     if( !pRTFDefaults )
     {
-        pRTFDefaults = new SfxItemSet( *pAttrPool, aWhichMap.GetData() );
+        pRTFDefaults = new SfxItemSet( *pAttrPool, &aWhichMap[0] );
         sal_uInt16 nId;
-        if( 0 != ( nId = ((RTFPardAttrMapIds*)aPardMap.GetData())->nScriptSpace ))
+        if( 0 != ( nId = ((RTFPardAttrMapIds*)&aPardMap[0])->nScriptSpace ))
         {
             SvxScriptSpaceItem aItem( sal_False, nId );
             if( bNewDoc )

fpicker/source/office/iodlgimp.hxx
@@ -45,7 +45,6 @@ class Accelerator;
 class CheckBox;
 class SvtFileDialog;
 class SvStringsDtor;
-class SvUShorts;
 
 //*****************************************************************************
 

commit 67427f3f7f84f6c42f218f798b9072dc0597bc63

fdo#46144: "Distributy Columns Evenly" does not work with the selected columns
regression from 3be8cfc12f3ff619defe4279034d3f88eebf4fc4:

-    SvUShorts aWish( nTmp, nTmp ),
+    std::vector<sal_uInt16> aWish( nTmp, nTmp ),
               aMins( nTmp, nTmp );

sw/source/core/docnode/ndtbl1.cxx
@@ -1469,16 +1469,11 @@ void SwDoc::AdjustCellWidth( const SwCursor& rCursor, sal_Bool bBalance )
     if ( ! aTabCols.Count() )
         return;
 
-    const sal_uInt8 nTmp = (sal_uInt8)Max( sal_uInt16(255), sal_uInt16(aTabCols.Count() + 1) );
-    std::vector<sal_uInt16> aWish( nTmp, nTmp ),
-              aMins( nTmp, nTmp );
+    std::vector<sal_uInt16> aWish(aTabCols.Count() + 1);
+    std::vector<sal_uInt16> aMins(aTabCols.Count() + 1);
+
     sal_uInt16 i;
 
-    for ( i = 0; i <= aTabCols.Count(); ++i )
-    {
-        aWish.push_back( 0 );
-        aMins.push_back( 0 );
-    }
     ::lcl_CalcColValues( aWish, aTabCols, pStart, pEnd, sal_True  );
 
     //Es ist Robuster wenn wir die Min-Werte fuer die ganze Tabelle berechnen.