LemmingAvalanche/libreoffice-repaired

commit 48f0cf7818442ca90a1dd39106ecc6d102be9c78

refactor SdrModel::GetDocumentStream
Remove 3 ~identical implementations of GetDocumentStream and the associated
struct SdrDocumentStreamInfo.

svx/source/svdraw/svdograf.cxx
@@ -35,6 +35,7 @@
 #include <vcl/salbtype.hxx>
 #include <sot/formats.hxx>
 #include <sot/storage.hxx>
+#include <comphelper/storagehelper.hxx>
 #include <unotools/ucbstreamhelper.hxx>
 #include <unotools/localfilehelper.hxx>
 #include <svl/style.hxx>
@@ -71,6 +72,7 @@
 #include <basegfx/polygon/b2dpolygontools.hxx>
 #include <osl/thread.hxx>
 
+using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::io;
 
@@ -1220,12 +1222,13 @@ IMPL_LINK( SdrGrafObj, ImpSwapHdl, GraphicObject*, pO )
         {
             if( pGraphic->HasUserData() )
             {
-                SdrDocumentStreamInfo aStreamInfo;
+                ::comphelper::LifecycleProxy proxy;
+                uno::Reference<io::XInputStream> const xStream(
+                    pModel->GetDocumentStream(pGraphic->GetUserData(), proxy));
 
-                aStreamInfo.mbDeleteAfterUse = sal_False;
-                aStreamInfo.maUserData = pGraphic->GetUserData();
-
-                SvStream* pStream = pModel->GetDocumentStream( aStreamInfo );
+                ::boost::scoped_ptr<SvStream> const pStream( (xStream.is())
+                        ? ::utl::UcbStreamHelper::CreateStream(xStream)
+                        : 0 );
 
                 if( pStream != NULL )
                 {
@@ -1251,7 +1254,7 @@ IMPL_LINK( SdrGrafObj, ImpSwapHdl, GraphicObject*, pO )
                     }
 
                     if( !GraphicFilter::GetGraphicFilter().ImportGraphic( aGraphic,
-                                                                          aStreamInfo.maUserData,
+                                                                          pGraphic->GetUserData(),
                                                                           *pStream,
                                                                           GRFILTER_FORMAT_DONTKNOW,
                                                                           NULL, 0, pFilterData ) )
@@ -1267,17 +1270,6 @@ IMPL_LINK( SdrGrafObj, ImpSwapHdl, GraphicObject*, pO )
                     delete pFilterData;
 
                     pStream->ResetError();
-
-                    if( aStreamInfo.mbDeleteAfterUse || aStreamInfo.mxStorageRef.is() )
-                    {
-                        if ( aStreamInfo.mxStorageRef.is() )
-                        {
-                            aStreamInfo.mxStorageRef->dispose();
-                            aStreamInfo.mxStorageRef = 0;
-                        }
-
-                        delete pStream;
-                    }
                 }
             }
             else if( !ImpUpdateGraphicLink( sal_False ) )
@@ -1316,15 +1308,9 @@ Reference< XInputStream > SdrGrafObj::getInputStream()
         // kann aus dem original Doc-Stream nachgeladen werden...
         if( pGraphic->HasUserData() )
         {
-            SdrDocumentStreamInfo aStreamInfo;
-
-            aStreamInfo.mbDeleteAfterUse = sal_False;
-            aStreamInfo.maUserData = pGraphic->GetUserData();
-
-            SvStream* pStream = pModel->GetDocumentStream( aStreamInfo );
-
-            if( pStream )
-                xStream.set( new utl::OInputStreamWrapper( pStream, sal_True ) );
+            ::comphelper::LifecycleProxy proxy;
+            xStream.set(
+                pModel->GetDocumentStream(pGraphic->GetUserData(), proxy));
         }
         else if( pGraphic && GetGraphic().IsLink() )
         {

sc/inc/drwlayer.hxx
@@ -115,7 +115,6 @@ public:
     virtual void    SetChanged( sal_Bool bFlg = sal_True );
 
     virtual Window* GetCurDocViewWin();
-    virtual SvStream* GetDocumentStream(SdrDocumentStreamInfo& rStreamInfo) const;
 
     virtual SdrLayerID GetControlExportLayerId( const SdrObject & ) const;
 

sc/source/core/data/drwlayer.cxx
@@ -1904,59 +1904,6 @@ void ScDrawLayer::SetChanged( sal_Bool bFlg /* = sal_True */ )
     FmFormModel::SetChanged( bFlg );
 }
 
-SvStream* ScDrawLayer::GetDocumentStream(SdrDocumentStreamInfo& rStreamInfo) const
-{
-    OSL_ENSURE( pDoc, "ScDrawLayer::GetDocumentStream without document" );
-    if ( !pDoc )
-        return NULL;
-
-    uno::Reference< embed::XStorage > xStorage = pDoc->GetDocumentShell() ?
-                                                        pDoc->GetDocumentShell()->GetStorage() :
-                                                        NULL;
-    SvStream*   pRet = NULL;
-
-    if( xStorage.is() )
-    {
-        if( rStreamInfo.maUserData.Len() &&
-            ( rStreamInfo.maUserData.GetToken( 0, ':' ) ==
-              String( RTL_CONSTASCII_USTRINGPARAM( "vnd.sun.star.Package" ) ) ) )
-        {
-            const String aPicturePath( rStreamInfo.maUserData.GetToken( 1, ':' ) );
-
-            // graphic from picture stream in picture storage in XML package
-            if( aPicturePath.GetTokenCount( '/' ) == 2 )
-            {
-                const String aPictureStreamName( aPicturePath.GetToken( 1, '/' ) );
-                const String aPictureStorageName( aPicturePath.GetToken( 0, '/' ) );
-
-                try {
-                    if ( xStorage->isStorageElement( aPictureStorageName ) )
-                    {
-                        uno::Reference< embed::XStorage > xPictureStorage =
-                                    xStorage->openStorageElement( aPictureStorageName, embed::ElementModes::READ );
-
-                        if( xPictureStorage.is() &&
-                            xPictureStorage->isStreamElement( aPictureStreamName ) )
-                        {
-                            uno::Reference< io::XStream > xStream =
-                                xPictureStorage->openStreamElement( aPictureStreamName, embed::ElementModes::READ );
-                            if ( xStream.is() )
-                                pRet = ::utl::UcbStreamHelper::CreateStream( xStream );
-                        }
-                    }
-                }
-                catch( uno::Exception& )
-                {
-                    // TODO: error handling
-                }
-            }
-        }
-        rStreamInfo.mbDeleteAfterUse = ( pRet != NULL );
-    }
-
-    return pRet;
-}
-
 SdrLayerID ScDrawLayer::GetControlExportLayerId( const SdrObject & ) const
 {
     //  Layer fuer Export von Form-Controls in Versionen vor 5.0 - immer SC_LAYER_FRONT

sd/inc/drawdoc.hxx
@@ -229,7 +229,6 @@ public:
     virtual void        DisposeLoadedModels();
     virtual bool    IsReadOnly() const;
     virtual void        SetChanged(sal_Bool bFlag = sal_True);
-    virtual SvStream*   GetDocumentStream(SdrDocumentStreamInfo& rStreamInfo) const;
 
     SfxItemPool&        GetPool() { return( *pItemPool ); }
 

sd/source/core/drawdoc3.cxx
@@ -1242,88 +1242,6 @@ List* SdDrawDocument::GetCustomShowList(sal_Bool bCreate)
 
 /*************************************************************************
 |*
-|* Document-Stream herausgeben (fuer load-on-demand Graphiken)
-|*
-\************************************************************************/
-
-SvStream* SdDrawDocument::GetDocumentStream(SdrDocumentStreamInfo& rStreamInfo) const
-{
-    uno::Reference < embed::XStorage > xStor;
-    if (mpDocSh)
-        xStor = mpDocSh->GetStorage();
-    SvStream*   pRet = NULL;
-
-    if( xStor.is() )
-    {
-        //TODO/MBA: binary format removed, needs testing
-        if( rStreamInfo.maUserData.Len() &&
-            ( rStreamInfo.maUserData.GetToken( 0, ':' ) ==
-              String( RTL_CONSTASCII_USTRINGPARAM( "vnd.sun.star.Package" ) ) ) )
-        {
-            const String aPicturePath( rStreamInfo.maUserData.GetToken( 1, ':' ) );
-
-            // graphic from picture stream in picture storage in XML package
-            if( aPicturePath.GetTokenCount( '/' ) == 2 ) try
-            {
-                const String aPictureStreamName( aPicturePath.GetToken( 1, '/' ) );
-                const String aPictureStorageName( aPicturePath.GetToken( 0, '/' ) );
-                if( xStor->isStorageElement( aPictureStorageName )  )
-                {
-                    uno::Reference < embed::XStorage > xPictureStorage =
-                            xStor->openStorageElement( aPictureStorageName, embed::ElementModes::READ );
-                    try
-                    {
-                        if( xPictureStorage.is() && xPictureStorage->isStreamElement( aPictureStreamName ) )
-                        {
-                            uno::Reference < io::XStream > xStream = xPictureStorage->openStreamElement( aPictureStreamName, embed::ElementModes::READ );
-                            if( xStream.is() )
-                                pRet = ::utl::UcbStreamHelper::CreateStream( xStream );
-                        }
-                    }
-                    catch( container::NoSuchElementException& )
-                    {
-                    }
-                }
-            }
-            catch( uno::Exception& e )
-            {
-                (void)e;
-                OSL_FAIL(
-                    (rtl::OString("sd::SdDrawDocument::GetDocumentStream(), "
-                            "exception caught: ") +
-                    rtl::OUStringToOString(
-                        comphelper::anyToString( cppu::getCaughtException() ),
-                        RTL_TEXTENCODING_UTF8 ) +
-                        rtl::OString("\r\nATTENTION: Graphics may get lost now, please inform CL or KA!") ).getStr() );
-            }
-
-            rStreamInfo.mbDeleteAfterUse = ( pRet != NULL );
-        }
-    }
-
-#if OSL_DEBUG_LEVEL > 1
-    if( pRet )
-    {
-        // try to get some information from stream
-        const sal_uLong nStartPos = pRet->Tell();
-        const sal_uLong nEndPos = pRet->Seek( STREAM_SEEK_TO_END );
-        const sal_uLong nStmLen = nEndPos - nStartPos;
-        sal_uChar   aTestByte;
-
-        // try to read one byte
-        if( nStmLen )
-            *pRet >> aTestByte;
-
-        pRet->Seek( nStartPos );
-    }
-#endif
-
-    return pRet;
-}
-
-
-/*************************************************************************
-|*
 |* Nicht benutzte MasterPages und Layouts entfernen
 |*
 \************************************************************************/

sd/source/ui/inc/DrawDocShell.hxx
@@ -47,7 +47,6 @@ class SdDrawDocument;
 class SvxItemFactory;
 class SdPage;
 class SfxPrinter;
-struct SdrDocumentStreamInfo;
 struct SpellCallbackInfo;
 class AbstractSvxNameDialog;
 class SfxUndoManager;

svx/inc/svx/svdmodel.hxx
@@ -91,10 +91,13 @@ class SotStorage;
 class SdrOutlinerCache;
 class SotStorageRef;
 class SdrUndoFactory;
-namespace comphelper{
+namespace comphelper
+{
     class IEmbeddedHelper;
+    class LifecycleProxy;
 }
-namespace sfx2{
+namespace sfx2
+{
     class LinkManager;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -155,13 +158,6 @@ public:
 // Flag um nach dem Laden des Pools Aufzuraeumen (d.h. die RefCounts
 // neu zu bestimmen und unbenutztes wegzuwerfen). sal_False == aktiv
 #define LOADREFCOUNTS (false)
-
-struct SdrDocumentStreamInfo
-{
-    bool            mbDeleteAfterUse;
-    String          maUserData;
-    com::sun::star::uno::Reference < com::sun::star::embed::XStorage > mxStorageRef;
-};
 
 struct SdrModelImpl;
 
@@ -330,7 +326,12 @@ public:
     // Datei angelegt.
     // Geliefert werden muss der Stream, aus dem das Model geladen wurde
     // bzw. in den es zuletzt gespeichert wurde.
-    virtual SvStream* GetDocumentStream( SdrDocumentStreamInfo& rStreamInfo ) const;
+    virtual ::com::sun::star::uno::Reference<
+                ::com::sun::star::embed::XStorage> GetDocumentStorage() const;
+    ::com::sun::star::uno::Reference<
+            ::com::sun::star::io::XInputStream >
+        GetDocumentStream(::rtl::OUString const& rURL,
+                ::comphelper::LifecycleProxy & rProxy) const;
     // Die Vorlagenattribute der Zeichenobjekte in harte Attribute verwandeln.
     void BurnInStyleSheetAttributes();
     // Wer sich von SdrPage ableitet muss sich auch von SdrModel ableiten

svx/source/svdraw/svdmodel.cxx
@@ -29,11 +29,16 @@
 
 #include <svx/svdmodel.hxx>
 
-#include <com/sun/star/lang/XComponent.hpp>
+#include <math.h>
+
 #include <osl/endian.h>
 #include <rtl/logfile.hxx>
 #include <rtl/strbuf.hxx>
-#include <math.h>
+
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/document/XStorageBasedDocument.hpp>
+#include <com/sun/star/embed/ElementModes.hpp>
+
 #include <tools/urlobj.hxx>
 #include <unotools/ucbstreamhelper.hxx>
 
@@ -82,6 +87,7 @@
 #include <svl/zforlist.hxx>
 #include <comphelper/processfactory.hxx>
 #include <comphelper/servicehelper.hxx>
+#include <comphelper/storagehelper.hxx>
 
 #include <tools/tenccvt.hxx>
 #include <unotools/syslocale.hxx>
@@ -886,9 +892,43 @@ void SdrModel::ImpReformatAllEdgeObjects()
     }
 }
 
-SvStream* SdrModel::GetDocumentStream(SdrDocumentStreamInfo& /*rStreamInfo*/) const
+uno::Reference<embed::XStorage> SdrModel::GetDocumentStorage() const
 {
-    return NULL;
+    uno::Reference<document::XStorageBasedDocument> const xSBD(
+            const_cast<SdrModel*>(this)->getUnoModel(), uno::UNO_QUERY);
+    if (!xSBD.is())
+    {
+        SAL_WARN("svx", "no UNO model");
+        return 0;
+    }
+    return xSBD->getDocumentStorage();
+}
+
+uno::Reference<io::XInputStream>
+SdrModel::GetDocumentStream( ::rtl::OUString const& rURL,
+                ::comphelper::LifecycleProxy & rProxy) const
+{
+    uno::Reference<embed::XStorage> const xStorage(GetDocumentStorage());
+    if (!xStorage.is())
+    {
+        SAL_WARN("svx", "no storage?");
+        return 0;
+    }
+    try {
+        uno::Reference<io::XStream> const xStream(
+            ::comphelper::OStorageHelper::GetStreamAtPackageURL(
+                xStorage, rURL, embed::ElementModes::READ, rProxy));
+        return (xStream.is()) ? xStream->getInputStream() : 0;
+    }
+    catch (container::NoSuchElementException const&)
+    {
+        SAL_INFO("svx", "not found");
+    }
+    catch (uno::Exception const&)
+    {
+        SAL_WARN("svx", "exception");
+    }
+    return 0;
 }
 
 // convert template attributes from the string into "hard" attributes

commit 4f8304650727e473c2493af1358c6ead56f2fff9

fdo#46340: fix crash in SdrGrafObj::getInputStream:
Add the ridiculous hack that used to be here (and distributed across
various GetDocumentStream methods to make it non-obvious)
that makes this not crash by accident.

(regression from 48f0cf7818442ca90a1dd39106ecc6d102be9c78)

svx/source/svdraw/svdograf.cxx
@@ -1335,6 +1335,14 @@ Reference< XInputStream > SdrGrafObj::getInputStream()
             ::comphelper::LifecycleProxy proxy;
             xStream.set(
                 pModel->GetDocumentStream(pGraphic->GetUserData(), proxy));
+            // fdo#46340: this may look completely insane, and it is,
+            // but it also prevents a crash: the LifecycleProxy will go out
+            // of scope, but the xStream must be returned; the UcbStreamHelper
+            // will actually copy the xStream to a temp file (because it is
+            // not seekable), which makes it not crash...
+            SvStream *const pStream =
+                utl::UcbStreamHelper::CreateStream(xStream);
+            xStream.set(new utl::OInputStreamWrapper(pStream, true));
         }
         else if( pGraphic && GetGraphic().IsLink() )
         {