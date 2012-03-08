LemmingAvalanche/libreoffice-repaired

commit 0885e1b2ed3ca50f31d10aac9db769a3416b8adb

Resolves: rhbz#800272 complain about unknown command-line options

desktop/source/app/cmdlineargs.cxx
@@ -304,6 +304,15 @@ void CommandLineArgs::ParseCommandLine_Impl( Supplier& supplier )
                     {
                         bConversionOutEvent = true;
                     }
+#if defined UNX
+                    else
+                    {
+                        printf("Unknown option %s\n",
+                            rtl::OUStringToOString(aArg, osl_getThreadTextEncoding()).getStr());
+                        printf("Run 'soffice --help' to see a full list of available command line options.\n");
+                        SetBoolParam_Impl( CMD_BOOLPARAM_UNKNOWN, sal_True );
+                    }
+#endif
                 }
                 else
                 {
@@ -640,7 +649,7 @@ sal_Bool CommandLineArgs::InterpretCommandLineParameter( const ::rtl::OUString& 
 
     if (bDeprecated)
     {
-        rtl::OString sArg(rtl::OUStringToOString(aArg, RTL_TEXTENCODING_UTF8));
+        rtl::OString sArg(rtl::OUStringToOString(aArg, osl_getThreadTextEncoding()));
         fprintf(stderr, "Warning: %s is deprecated.  Use -%s instead.\n", sArg.getStr(), sArg.getStr());
     }
     return sal_True;
@@ -849,6 +858,12 @@ sal_Bool CommandLineArgs::IsVersion() const
 {
     osl::MutexGuard  aMutexGuard( m_aMutex );
     return m_aBoolParams[ CMD_BOOLPARAM_VERSION ];
+}
+
+sal_Bool CommandLineArgs::HasUnknown() const
+{
+    osl::MutexGuard  aMutexGuard( m_aMutex );
+    return m_aBoolParams[ CMD_BOOLPARAM_UNKNOWN ];
 }
 
 sal_Bool CommandLineArgs::HasModuleParam() const

desktop/source/app/cmdlineargs.hxx
@@ -71,6 +71,7 @@ class CommandLineArgs
             CMD_BOOLPARAM_HELPBASE,
             CMD_BOOLPARAM_PSN,
             CMD_BOOLPARAM_VERSION,
+            CMD_BOOLPARAM_UNKNOWN,
             CMD_BOOLPARAM_COUNT             // must be last element!
         };
 
@@ -162,6 +163,7 @@ class CommandLineArgs
         sal_Bool                IsWeb() const;
         sal_Bool                IsVersion() const;
         sal_Bool                HasModuleParam() const;
+        sal_Bool                HasUnknown() const;
         sal_Bool                WantsToLoadDocument() const;
 
         // Access to string parameters

desktop/source/app/officeipcthread.cxx
@@ -709,6 +709,12 @@ void OfficeIPCThread::execute()
 #endif
                 continue;
             }
+
+#ifdef UNX
+            if (aCmdLineArgs->HasUnknown() || aCmdLineArgs->IsVersion() || aCmdLineArgs->IsHelp())
+                continue;
+#endif
+
             const CommandLineArgs &rCurrentCmdLineArgs = Desktop::GetCommandLineArgs();
 
             if ( aCmdLineArgs->IsQuickstart() )

desktop/source/app/sofficemain.cxx
@@ -71,6 +71,10 @@ extern "C" int DESKTOP_DLLPUBLIC soffice_main()
         desktop::displayVersion();
         return EXIT_SUCCESS;
     }
+    else if ( rCmdLineArgs.HasUnknown() )
+    {
+        return EXIT_FAILURE;
+    }
 #endif
     return SVMain();
 #ifdef ANDROID

commit 218dfe1b64f37bb2dbc28758c29a0598efae15b3

desktop: do not complain about soffice command line options:
These are handled in the soffice wrapper script, but that is too stupid
to be able to filter these out, so just ignore them in soffice.bin.

(regression from 0885e1b2ed3ca50f31d10aac9db769a3416b8adb)

Also, print errors on stderr.

desktop/source/app/cmdlineargs.cxx
@@ -306,10 +306,16 @@ void CommandLineArgs::ParseCommandLine_Impl( Supplier& supplier )
                     }
 #if defined UNX
                     else
+                    // because it's impossible to filter these options that
+                    // are handled in the soffice shell script with the
+                    // primitive tools that /bin/sh offers, ignore them here
+                    if (!oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("backtrace")) &&
+                        !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("strace")) &&
+                        !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("valgrind")))
                     {
-                        printf("Unknown option %s\n",
+                        fprintf(stderr, "Unknown option %s\n",
                             rtl::OUStringToOString(aArg, osl_getThreadTextEncoding()).getStr());
-                        printf("Run 'soffice --help' to see a full list of available command line options.\n");
+                        fprintf(stderr, "Run 'soffice --help' to see a full list of available command line options.\n");
                         SetBoolParam_Impl( CMD_BOOLPARAM_UNKNOWN, sal_True );
                     }
 #endif