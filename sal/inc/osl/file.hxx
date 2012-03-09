LemmingAvalanche/libreoffice-repaired

commit 6f51827415e10f0ccd33fa23903cc5c2d814162c

fix SvStream to not require a custom open or lstat method.

tools/source/stream/strmunx.cxx
@@ -223,7 +223,7 @@ class StreamData
 public:
     oslFileHandle rHandle;
 
-            StreamData() { }
+    StreamData() : rHandle( 0 ) { }
 };
 
 // -----------------------------------------------------------------------
@@ -683,8 +683,6 @@ void SvFileStream::Open( const String& rFilename, StreamMode nOpenMode )
 {
     sal_uInt32 uFlags;
     oslFileHandle nHandleTmp;
-    struct stat buf;
-    sal_Bool bStatValid = sal_False;
 
     Close();
     errno = 0;
@@ -707,16 +705,22 @@ void SvFileStream::Open( const String& rFilename, StreamMode nOpenMode )
     OSL_TRACE( "%s", aTraceStr.getStr() );
 #endif
 
-    if ( osl_lstatFilePath( aLocalFilename.getStr(), &buf ) == osl_File_E_None )
-      {
-        bStatValid = sal_True;
-        // SvFileStream soll kein Directory oeffnen
-        if( S_ISDIR( buf.st_mode ) )
-          {
-            SetError( ::GetSvError( EISDIR ) );
-            return;
-          }
-      }
+    rtl::OUString aFileURL;
+    osl::DirectoryItem aItem;
+    osl::FileStatus aStatus( osl_FileStatus_Mask_Type | osl_FileStatus_Mask_LinkTargetURL );
+
+    // FIXME: we really need to switch to a pure URL model ...
+    if ( osl::File::getFileURLFromSystemPath( aFilename, aFileURL ) != osl::FileBase::RC::E_None )
+        aFileURL = aFilename;
+    bool bStatValid = ( osl::DirectoryItem::get( aFileURL, aItem) != osl::FileBase::RC::E_None &&
+                        aItem.getFileStatus( aStatus ) != osl::FileBase::RC::E_None );
+
+    // SvFileStream can't open a directory
+    if( bStatValid && aStatus.getFileType() == osl::FileStatus::Type::Directory )
+    {
+        SetError( ::GetSvError( EISDIR ) );
+        return;
+    }
 
     if ( !( nOpenMode & STREAM_WRITE ) )
         uFlags = osl_File_OpenFlag_Read;
@@ -736,44 +740,37 @@ void SvFileStream::Open( const String& rFilename, StreamMode nOpenMode )
 
     if ( nOpenMode & STREAM_WRITE)
     {
-      if ( nOpenMode & STREAM_COPY_ON_SYMLINK )
-          {
-          if ( bStatValid  &&  S_ISLNK( buf.st_mode ) < 0 )
+        if ( nOpenMode & STREAM_COPY_ON_SYMLINK )
+        {
+            if ( bStatValid && aStatus.getFileType() == osl::FileStatus::Type::Link &&
+                 aStatus.getLinkTargetURL().getLength() > 0 )
             {
-              char *pBuf = new char[ 1024+1 ];
-              if ( readlink( aLocalFilename.getStr(), pBuf, 1024 ) > 0 )
+                // delete the symbolic link, and replace it with the contents of the link
+                if (osl::File::remove( aFileURL ) == osl::FileBase::RC::E_None )
                 {
-                  if (  unlink(aLocalFilename.getStr())  == 0 )
-                      {
-#ifdef DBG_UTIL
-                      fprintf( stderr,
-                               "Copying file on symbolic link (%s).\n",
-                               aLocalFilename.getStr() );
+                    File::copy( aStatus.getLinkTargetURL(), aFileURL );
+#if OSL_DEBUG_LEVEL > 0
+                    fprintf( stderr,
+                             "Removing link and replacing with file contents (%s) -> (%s).\n",
+                             rtl::OUStringToOString( aStatus.getLinkTargetURL(),
+                                                     RTL_TEXTENCODING_UTF8).getStr(),
+                             rtl::OUStringToOString( aFileURL,
+                                                     RTL_TEXTENCODING_UTF8).getStr() );
 #endif
-                      String aTmpString( pBuf, osl_getThreadTextEncoding() );
-                      const DirEntry aSourceEntry( aTmpString );
-                      const DirEntry aTargetEntry( aFilename );
-                      FileCopier aFileCopier( aSourceEntry, aTargetEntry );
-                      aFileCopier.Execute();
-                    }
                 }
-              delete [] pBuf;
             }
         }
     }
 
-    oslFileError rc = osl_openFilePath( aLocalFilename.getStr(),&nHandleTmp, uFlags );
-
+    oslFileError rc = osl_openFile( aFileURL.pData, &nHandleTmp, uFlags );
     if ( rc != osl_File_E_None )
     {
         if ( uFlags & osl_File_OpenFlag_Write )
         {
             // auf Lesen runterschalten
             uFlags &= ~osl_File_OpenFlag_Write;
-            rc = osl_openFilePath( aLocalFilename.getStr(),
-                                   &nHandleTmp,
-                                   uFlags );
-            }
+            rc = osl_openFile( aFileURL.pData, &nHandleTmp, uFlags );
+        }
     }
     if ( rc == osl_File_E_None )
     {

sal/inc/osl/detail/file.h
@@ -49,25 +49,13 @@ extern "C" {
 #define osl_File_OpenFlag_Trunc     0x00000010L
 #define osl_File_OpenFlag_NoExcl    0x00000020L
 
-/* Variant of osl_openFile that takes the file pathname directly as a
-   char*
-*/
-
-SAL_DLLPUBLIC oslFileError SAL_CALL osl_openFilePath(
-    const char *cpFilePath,
-    oslFileHandle *pHandle,
-    sal_uInt32 uFlags );
-
 /* Compare directory items for being the same underlying file
  * this unwinds unix hard-links and symlinks etc.
  */
 
 SAL_DLLPUBLIC sal_Bool SAL_CALL osl_identicalDirectoryItem(oslDirectory a, oslDirectory b);
 
-SAL_DLLPUBLIC oslFileError SAL_CALL osl_lstatFilePath( const char *cpFilePath, struct stat *statb );
-
 /*  Get the OS specific "handle" of an open file. */
-
 SAL_DLLPUBLIC oslFileError SAL_CALL osl_getFileOSHandle(
     oslFileHandle Handle,
     sal_IntPtr *piFileHandle );

sal/inc/osl/file.h
@@ -664,6 +664,7 @@ typedef void *oslFileHandle;
 #define osl_File_OpenFlag_Write     0x00000002L
 #define osl_File_OpenFlag_Create    0x00000004L
 #define osl_File_OpenFlag_NoLock    0x00000008L
+/* larger bit-fields reserved for internal use cf. detail/file.h */
 
 /** Open a regular file.
 

sal/osl/unx/file.cxx
@@ -892,7 +892,7 @@ SAL_CALL osl_openMemoryAsFile( void *address, size_t size, oslFileHandle *pHandl
 #define OPEN_CREATE_FLAGS ( O_CREAT | O_RDWR )
 #endif
 
-oslFileError
+static oslFileError
 SAL_CALL osl_openFilePath( const char *cpFilePath, oslFileHandle* pHandle, sal_uInt32 uFlags )
 {
     oslFileError eRet;

sal/osl/unx/file_stat.cxx
@@ -462,7 +462,6 @@ SAL_CALL osl_identicalDirectoryItem( oslDirectoryItem a, oslDirectoryItem b)
         return sal_True;
 
     fprintf (stderr, "We have to do an inode compare !\n");
-
     /*
     int rc = stat_c( cpFilePath, statb );
 
@@ -473,17 +472,6 @@ SAL_CALL osl_identicalDirectoryItem( oslDirectoryItem a, oslDirectoryItem b)
         return osl_File_E_None;
     */
     return sal_False;
-}
-
-oslFileError
-SAL_CALL osl_lstatFilePath( const char *cpFilePath, struct stat *statb )
-{
-    int rc = lstat_c( cpFilePath, statb );
-
-    if (rc == -1)
-        return oslTranslateFileError(OSL_FET_ERROR, errno);
-    else
-        return osl_File_E_None;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sal/util/sal.map
@@ -647,9 +647,7 @@ PRIVATE_textenc.1 { # LibreOffice 3.6
 
 PRIVATE_file.1 { # LibreOffice 3.6
     global:
-        osl_openFilePath;
         osl_identicalDirectoryItem;
-        osl_lstatFilePath;
         osl_getFileOSHandle;
 };
 

tools/inc/tools/stream.hxx
@@ -576,10 +576,10 @@ class TOOLS_DLLPUBLIC SvFileStream : public SvStream
     friend class FileCopier;
 
 private:
-    StreamData*         pInstanceData;
+    StreamData*     pInstanceData;
     String          aFilename;
     sal_uInt16      nLockCounter;
-    sal_Bool            bIsOpen;
+    sal_Bool        bIsOpen;
     sal_uInt32      GetFileHandle() const;
 
     // Forbidden and not implemented.

commit 4a40f665cd2ce6ee24dd8127edd77993e0028c1a

Let osl::FileStatus getters assert programming errors
...instead of arbitrarily returning certain values when the requested
information is not available.

This reveals a problem in strmunx.cxx that is apparently a regression introduced
with 6f51827415e10f0ccd33fa23903cc5c2d814162c "fix SvStream to not require a
custom open or lstat method."

sal/inc/osl/file.hxx
@@ -29,14 +29,16 @@
 #ifndef _OSL_FILE_HXX_
 #define _OSL_FILE_HXX_
 
-#ifdef __cplusplus
+#include "sal/config.h"
+
+#include <cassert>
 
 #include <osl/time.h>
-#   include <rtl/memory.h>
-#   include <rtl/ustring.hxx>
+#include <rtl/memory.h>
+#include <rtl/ustring.hxx>
 
 #include <osl/file.h>
-#   include <rtl/byteseq.hxx>
+#include <rtl/byteseq.hxx>
 
 #include <stdio.h>
 
@@ -723,11 +725,12 @@ public:
     /** Get the file type.
 
         @return
-        The file type if this information is valid, Unknown otherwise.
+        The file type.
     */
     inline Type getFileType() const
     {
-        return (_aStatus.uValidFields & osl_FileStatus_Mask_Type) ?  (Type) _aStatus.eType : Unknown;
+        assert(isValid(osl_FileStatus_Mask_Type));
+        return static_cast< Type >(_aStatus.eType);
     }
 
     /** Is it a file?
@@ -796,88 +799,93 @@ public:
 
     inline sal_uInt64 getAttributes() const
     {
+        assert(isValid(osl_FileStatus_Mask_Attributes));
         return _aStatus.uAttributes;
     }
 
     /** Get the creation time of this file.
 
         @return
-        The creation time if this information is valid,
-        an uninitialized TimeValue otherwise.
+        The creation time.
     */
 
     inline TimeValue getCreationTime() const
     {
+        assert(isValid(osl_FileStatus_Mask_CreationTime));
         return _aStatus.aCreationTime;
     }
 
     /** Get the file access time.
 
         @return
-        The last access time if this information is valid,
-        an uninitialized TimeValue otherwise.
+        The last access time.
     */
 
     inline TimeValue getAccessTime() const
     {
+        assert(isValid(osl_FileStatus_Mask_AccessTime));
         return _aStatus.aAccessTime;
     }
 
     /** Get the file modification time.
 
         @return
-        The last modified time if this information is valid,
-        an uninitialized TimeValue otherwise.
+        The last modified time.
     */
 
     inline TimeValue getModifyTime() const
     {
+        assert(isValid(osl_FileStatus_Mask_ModifyTime));
         return _aStatus.aModifyTime;
     }
 
     /** Get the size of the file.
 
         @return
-        The actual file size if this information is valid, 0 otherwise.
+        The actual file size.
     */
 
     inline sal_uInt64 getFileSize() const
     {
+        assert(isValid(osl_FileStatus_Mask_FileSize));
         return _aStatus.uFileSize;
     }
 
     /** Get the file name.
 
         @return
-        The file name if this information is valid, an empty string otherwise.
+        The file name.
     */
 
     inline ::rtl::OUString getFileName() const
     {
-        return _aStatus.ustrFileName ? ::rtl::OUString(_aStatus.ustrFileName) : ::rtl::OUString();
+        assert(isValid(osl_FileStatus_Mask_FileName));
+        return rtl::OUString(_aStatus.ustrFileName);
     }
 
 
     /** Get the URL of the file.
 
         @return
-        The full qualified URL of the file if this information is valid, an empty string otherwise.
+        The full qualified URL of the file.
     */
 
     inline ::rtl::OUString getFileURL() const
     {
-        return _aStatus.ustrFileURL ? ::rtl::OUString(_aStatus.ustrFileURL) : ::rtl::OUString();
+        assert(isValid(osl_FileStatus_Mask_FileURL));
+        return rtl::OUString(_aStatus.ustrFileURL);
     }
 
     /** Get the link target URL.
 
         @return
-        The link target URL if this information is valid, an empty string otherwise.
+        The link target URL.
     */
 
     inline ::rtl::OUString getLinkTargetURL() const
     {
-        return _aStatus.ustrLinkTargetURL ? ::rtl::OUString(_aStatus.ustrLinkTargetURL) : ::rtl::OUString();
+        assert(isValid(osl_FileStatus_Mask_LinkTargetURL));
+        return rtl::OUString(_aStatus.ustrLinkTargetURL);
     }
 
     friend class DirectoryItem;
@@ -1947,7 +1955,6 @@ public:
 
 } /* namespace osl */
 
-#endif  /* __cplusplus */
 #endif  /* _OSL_FILE_HXX_ */
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

tools/source/stream/strmunx.cxx
@@ -628,8 +628,8 @@ void SvFileStream::Open( const String& rFilename, StreamMode nOpenMode )
     // FIXME: we really need to switch to a pure URL model ...
     if ( osl::File::getFileURLFromSystemPath( aFilename, aFileURL ) != osl::FileBase::E_None )
         aFileURL = aFilename;
-    bool bStatValid = ( osl::DirectoryItem::get( aFileURL, aItem) != osl::FileBase::E_None &&
-                        aItem.getFileStatus( aStatus ) != osl::FileBase::E_None );
+    bool bStatValid = ( osl::DirectoryItem::get( aFileURL, aItem) == osl::FileBase::E_None &&
+                        aItem.getFileStatus( aStatus ) == osl::FileBase::E_None );
 
     // SvFileStream can't open a directory
     if( bStatValid && aStatus.getFileType() == osl::FileStatus::Directory )