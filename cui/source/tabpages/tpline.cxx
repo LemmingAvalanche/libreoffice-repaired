LemmingAvalanche/libreoffice-repaired

commit b2e6a3322cad0b80773051f84f7dc233d82d2ebb

Remove deprecated List in SvxLineTabPage for std::vector<String>.

cui/source/tabpages/tpline.cxx
@@ -266,12 +266,6 @@ SvxLineTabPage::~SvxLineTabPage()
     if(pSymbolList)
         delete aSymbolMB.GetPopupMenu()->GetPopupMenu( MN_SYMBOLS );
 
-    String* pStr = (String*)aGrfNames.First();
-    while( pStr )
-    {
-        delete pStr;
-        pStr = (String*)aGrfNames.Next();
-    }
     SvxBmpItemInfo* pInfo = (SvxBmpItemInfo*)aGrfBrushItems.First();
     while( pInfo )
     {
@@ -1474,19 +1468,18 @@ IMPL_LINK( SvxLineTabPage, MenuCreateHdl_Impl, MenuButton *, pButton )
 
         PopupMenu* pPopup = new PopupMenu;
         String aEmptyStr;
-
-        nNumMenuGalleryItems=aGrfNames.Count();
-        for(long i = 0; i < nNumMenuGalleryItems; i++)
+        const String *pUIName = NULL;
+        sal_uInt32 i = 0;
+        for(std::vector<String>::iterator it = aGrfNames.begin(); it != aGrfNames.end(); ++it, ++i)
         {
-            const String* pGrfName = (const String*)aGrfNames.GetObject(i);
-            const String* pUIName = pGrfName;
+            pUIName = &(*it);
 
             // convert URL encodings to UI characters (eg %20 for spaces)
             String aPhysicalName;
-            if( ::utl::LocalFileHelper::ConvertURLToPhysicalName( *pGrfName, aPhysicalName ))
+            if( ::utl::LocalFileHelper::ConvertURLToPhysicalName( *it, aPhysicalName ))
                 pUIName = &aPhysicalName;
 
-            SvxBrushItem* pBrushItem = new SvxBrushItem(*pGrfName, aEmptyStr, GPOS_AREA, SID_ATTR_BRUSH);
+            SvxBrushItem* pBrushItem = new SvxBrushItem(*it, aEmptyStr, GPOS_AREA, SID_ATTR_BRUSH);
             pBrushItem->SetDoneLink(STATIC_LINK(this, SvxLineTabPage, GraphicArrivedHdl_Impl));
 
             SvxBmpItemInfo* pInfo = new SvxBmpItemInfo();
@@ -1519,7 +1512,8 @@ IMPL_LINK( SvxLineTabPage, MenuCreateHdl_Impl, MenuButton *, pButton )
             }
         }
         aSymbolMB.GetPopupMenu()->SetPopupMenu( MN_GALLERY, pPopup );
-        if(!aGrfNames.Count())
+
+        if(aGrfNames.empty())
             aSymbolMB.GetPopupMenu()->EnableItem(MN_GALLERY, sal_False);
     }
 
@@ -1546,8 +1540,7 @@ IMPL_LINK( SvxLineTabPage, MenuCreateHdl_Impl, MenuButton *, pButton )
             if(pObj==NULL)
                 break;
             pObj=pObj->Clone();
-            String *pStr=new String();
-            aGrfNames.Insert(pStr,LIST_APPEND);
+            aGrfNames.push_back(aEmptyStr);
             pPage->NbcInsertObject(pObj);
             pView->MarkObj(pObj,pPageView);
             if(pSymbolAttr)
@@ -1583,10 +1576,11 @@ IMPL_LINK( SvxLineTabPage, MenuCreateHdl_Impl, MenuButton *, pButton )
                 aBitmap.Scale(nScale, nScale);
             }
             Image aImage(aBitmap);
-            pPopup->InsertItem(pInfo->nItemId,*pStr,aImage);
+            pPopup->InsertItem(pInfo->nItemId,aEmptyStr,aImage);
         }
         aSymbolMB.GetPopupMenu()->SetPopupMenu( MN_SYMBOLS, pPopup );
-        if(!aGrfNames.Count())
+
+        if(aGrfNames.empty())
             aSymbolMB.GetPopupMenu()->EnableItem(MN_SYMBOLS, sal_False);
 
         delete pView;

cui/source/inc/cuitabline.hxx
@@ -29,6 +29,8 @@
 #define _CUI_TAB_LINE_HXX
 
 // include ---------------------------------------------------------------
+#include <vector>
+
 #include <svx/tabline.hxx>
 /*************************************************************************
 |*
@@ -146,7 +148,7 @@ private:
     FixedText           aSymbolHeightFT;
     MetricField         aSymbolHeightMF;
     CheckBox            aSymbolRatioCB;
-    List                aGrfNames;
+    std::vector<String> aGrfNames;
     List                aGrfBrushItems;
     String              sNumCharFmtName;
     sal_Bool                bLastWidthModified;

commit 4924c4adc21d18063ab5c724f3381fc154cb92e6

fdo#46942: Fix a regression caused by List removal.
b2e6a3322cad0b80773051f84f7dc233d82d2ebb accidentally removed the gallery
item count which was used to differentiate the gallery symbols from
normal ones.

cui/source/tabpages/tpline.cxx
@@ -277,6 +277,7 @@ void SvxLineTabPage::InitSymbols(MenuButton* pButton)
         rtl::OUString aEmptyStr;
         const rtl::OUString *pUIName = NULL;
         sal_uInt32 i = 0;
+        nNumMenuGalleryItems = aGrfNames.size();
         for(std::vector<rtl::OUString>::iterator it = aGrfNames.begin(); it != aGrfNames.end(); ++it, ++i)
         {
             pUIName = &(*it);