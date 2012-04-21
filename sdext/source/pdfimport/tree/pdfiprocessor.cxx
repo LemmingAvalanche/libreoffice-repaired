LibreOffice/core

commit 5dd4f9ecb2f50cf76b44472e4438297b6c65184c

inefficient emptiness check

sdext/source/pdfimport/tree/pdfiprocessor.cxx
@@ -221,7 +221,7 @@ sal_Int32 PDFIProcessor::getFontId( const FontAttributes& rAttr ) const
 // line diagnose block - start
 void PDFIProcessor::processGlyphLine()
 {
-    if( m_GlyphsList.size()<1 )
+    if( m_GlyphsList.empty() )
         return;
 
     double fPreAvarageSpaceValue= 0.0;
@@ -317,7 +317,7 @@ void PDFIProcessor::processGlyphLine()
     ParagraphElement* pPara= NULL ;
     FrameElement* pFrame= NULL ;
 
-    if(m_GlyphsList.size()>0)
+    if(!m_GlyphsList.empty())
     {
         pFrame = m_pElFactory->createFrameElement( m_GlyphsList[0].getCurElement(), getGCId( getTransformGlyphContext( m_GlyphsList[0])) );
         pFrame->ZOrder = m_nNextZOrder++;
@@ -401,7 +401,7 @@ void PDFIProcessor::processGlyphLine()
 
     }
 
-    if(m_GlyphsList.size()>1)
+    if(!m_GlyphsList.empty())
      processGlyph( 0,
                   m_GlyphsList[m_GlyphsList.size()-1],
                   pPara,

sdext/source/minimizer/pagecollector.cxx
@@ -96,7 +96,7 @@ void PageCollector::CollectNonCustomShowPages( const com::sun::star::uno::Refere
     {
         std::vector< Reference< XDrawPage > > vUsedPageList;
         PageCollector::CollectCustomShowPages( rxModel, rCustomShowName, vUsedPageList );
-        if ( vUsedPageList.size() )
+        if ( !vUsedPageList.empty() )
         {
             Reference< XDrawPagesSupplier > xDrawPagesSupplier( rxModel, UNO_QUERY_THROW );
             Reference< XDrawPages > xDrawPages( xDrawPagesSupplier->getDrawPages(), UNO_QUERY_THROW );

LemmingAvalanche/libreoffice-repaired

commit 84b9554af8ac66c1bd7147e6d2b236dbfee96cfc

Fix fdo#45848
regression from 5dd4f9ecb2f50cf76b44472e4438297b6c65184c
The change:
-    if(m_GlyphsList.size()>1)
+    if(!m_GlyphsList.empty())
is incorrect, so change it back.

sdext/source/pdfimport/tree/pdfiprocessor.cxx
@@ -399,7 +399,7 @@ void PDFIProcessor::processGlyphLine()
 
     }
 
-    if(!m_GlyphsList.empty())
+    if(m_GlyphsList.size()>1)
      processGlyph( 0,
                   m_GlyphsList[m_GlyphsList.size()-1],
                   pPara,