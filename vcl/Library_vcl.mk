LemmingAvalanche/libreoffice-repaired

commit a69c8acc5b4c7933ca69fb9affd1b80ee6c9f664

Enable configure switch for cups
This allows us not to have cups during the build time of libreoffice.

Commit is inspired from patch from Dave Flogeras <dflogeras@gmail.com>.

Change-Id: I I32a14eb6e7bdd13de1a737d4798c852a830ae326

vcl/Library_vcl.mk
@@ -445,10 +445,15 @@ $(eval $(call gb_Library_add_defs,vcl,\
 $(eval $(call gb_Library_add_exception_objects,vcl,\
 	$(vcl_generic_code) \
     vcl/unx/generic/plugadapt/salplug \
-    vcl/unx/generic/printer/cupsmgr \
     vcl/unx/generic/printer/jobdata \
     vcl/unx/generic/printer/ppdparser \
-    vcl/unx/generic/printer/printerinfomanager \
+    $(if $(filter YES,$(ENABLE_CUPS)),\
+        vcl/unx/generic/printer/cupsmgr \
+        vcl/unx/generic/printer/printerinfomanager \
+    ) \
+    $(if $(filter NO,$(ENABLE_CUPS)),\
+        vcl/null/printerinfomanager \
+    ) \
 ))
 $(eval $(call gb_Library_use_externals,vcl,\
 	fontconfig \

vcl/unx/generic/printer/ppdparser.cxx
@@ -655,7 +655,7 @@ const PPDParser* PPDParser::getParser( const String& rFile )
         PrinterInfoManager& rMgr = PrinterInfoManager::get();
         if( rMgr.getType() == PrinterInfoManager::CUPS )
         {
-#if !defined(ANDROID) && !defined(LIBO_HEADLESS)
+#if defined HAVE_CUPS_H
             pNewParser = const_cast<PPDParser*>(static_cast<CUPSManager&>(rMgr).createCUPSParser( aFile ));
 #endif
         }

config_host.mk.in
@@ -98,6 +98,7 @@ export DPKG=@DPKG@
 export DYNAMIC_CRT=@DYNAMIC_CRT@
 export ENABLE_CAIRO_CANVAS=@ENABLE_CAIRO_CANVAS@
 export ENABLE_CRASHDUMP=@ENABLE_CRASHDUMP@
+export ENABLE_CUPS=@ENABLE_CUPS@
 export ENABLE_DBUS=@ENABLE_DBUS@
 export ENABLE_DEBUG_FOR=@ENABLE_DEBUG_FOR@
 export ENABLE_DIRECTX=@ENABLE_DIRECTX@

configure.in
@@ -848,6 +848,11 @@ AC_ARG_ENABLE(nsplugin,
         [Do not build nsplugin extension for browser embedding.])
 )
 
+AC_ARG_ENABLE(cups,
+    AS_HELP_STRING([--disable-cups],
+        [Do not build cups support.])
+)
+
 AC_ARG_ENABLE(ccache,
     AS_HELP_STRING([--disable-ccache],
         [Do not try to use ccache automatically.
@@ -3605,6 +3610,13 @@ fi
 
 dnl check for cups support
 dnl ===================================================================
+if test "$enable_cups" = "no"; then
+    test_cups=no
+    ENABLE_CUPS="NO"
+else
+    ENABLE_CUPS="YES"
+fi
+AC_SUBST(ENABLE_CUPS)
 if test "$test_cups" = "yes"; then
     AC_MSG_CHECKING([whether cups support is present])
     AC_CHECK_LIB([cups], [cupsPrintFiles], [:])

vcl/null/printerinfomanager.cxx
@@ -84,6 +84,16 @@ void PrinterInfoManager::initialize()
     // ???
 }
 
+bool PrinterInfoManager::isCUPSDisabled() const
+{
+    return m_bDisableCUPS;
+}
+
+void PrinterInfoManager::setCUPSDisabled( bool bDisable )
+{
+    // cups is already disabled in config so do nothing
+}
+
 void PrinterInfoManager::listPrinters( ::std::list< OUString >& rList ) const
 {
     rList.clear();

commit 8f308eb923e0853ee746e8f1ace5340b7fa03818

fix use of cups for default case
a69c8acc5b4c7933ca69fb9affd1b80ee6c9f664 broke printing

Change-Id: Ib1cd9b31c7e3e5644b5207cb28c4fd178837c19d

vcl/Library_vcl.mk
@@ -436,6 +436,12 @@ $(eval $(call gb_Library_add_defs,vcl,\
     ) \
 ))
 
+ifeq ($(ENABLE_CUPS),YES)
+$(eval $(call gb_Library_add_defs,vcl,\
+    -DENABLE_CUPS\
+))
+endif
+
 ifeq ($(GUIBASE),unx)
 $(eval $(call gb_Library_add_defs,vcl,\
     -DSAL_DLLPREFIX=\"$(gb_Library_SYSPRE)\" \

vcl/unx/generic/printer/ppdparser.cxx
@@ -655,7 +655,7 @@ const PPDParser* PPDParser::getParser( const String& rFile )
         PrinterInfoManager& rMgr = PrinterInfoManager::get();
         if( rMgr.getType() == PrinterInfoManager::CUPS )
         {
-#if defined HAVE_CUPS_H
+#ifdef ENABLE_CUPS
             pNewParser = const_cast<PPDParser*>(static_cast<CUPSManager&>(rMgr).createCUPSParser( aFile ));
 #endif
         }