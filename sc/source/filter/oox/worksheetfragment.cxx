LemmingAvalanche/libreoffice-repaired

commit 0453abc58db530572219abe017934c81c6a7a6f7

fix missing ole and form control(s) on xlsx import
chang the visibility of aMceState in order to access that member to filter out reading and parsing of ./xl/ctrlProps/ctrlProp[N].xml fragments for 'controls' & 'oleObjects' elements nested withing 'mc:AlternateContent' elements

Change-Id: I6892db7e3302b9977f0fdaabbe3ac1444315e011

sc/source/filter/oox/worksheetfragment.cxx
@@ -332,10 +332,10 @@ ContextHandlerRef WorksheetFragment::onCreateContext( sal_Int32 nElement, const 
         break;
 
         case XLS_TOKEN( oleObjects ):
-            if( nElement == XLS_TOKEN( oleObject ) ) importOleObject( rAttribs );
+            if( nElement == XLS_TOKEN( oleObject ) && aMceState.empty() ) importOleObject( rAttribs );
         break;
         case XLS_TOKEN( controls ):
-            if( nElement == XLS_TOKEN( control ) ) importControl( rAttribs );
+            if( nElement == XLS_TOKEN( control ) && aMceState.empty() ) importControl( rAttribs );
         break;
     }
     return 0;

oox/inc/oox/core/fragmenthandler2.hxx
@@ -41,7 +41,7 @@ namespace core {
 
 class OOX_DLLPUBLIC FragmentHandler2 : public FragmentHandler, public ContextHandler2Helper
 {
-private:
+protected:
     enum MCE_STATE
     {
         MCE_UNUSED,
@@ -50,7 +50,6 @@ private:
     };
     ::std::vector<MCE_STATE>           aMceState;
 
-private:
     bool                prepareMceContext( sal_Int32 nElement, const AttributeList& rAttribs );
 
 

commit d2cb51b30e67d9939d349eff590fa14a6b2a6edf

attempt to use mce::FallBack for control/oleObject import
commit commit 0453abc58db530572219abe017934c81c6a7a6f7 broke the existing mce::FallBack processing.

Change-Id: I05296f6aad3841a8dd40aa345207c0114e0c7701

sc/source/filter/oox/worksheetfragment.cxx
@@ -330,12 +330,26 @@ ContextHandlerRef WorksheetFragment::onCreateContext( sal_Int32 nElement, const 
                 case XLS_TOKEN( evenFooter ):       return this;    // collect h/f contents in onCharacters()
             }
         break;
-
+        // Only process an oleObject or control if outside a mc:AlternateContent
+        // element OR if within a mc:Fallback. I suppose ideally we
+        // should process the stuff within 'mc:Choice'
+	case XLS_TOKEN( controls ):
         case XLS_TOKEN( oleObjects ):
-            if( nElement == XLS_TOKEN( oleObject ) && aMceState.empty() ) importOleObject( rAttribs );
-        break;
-        case XLS_TOKEN( controls ):
-            if( nElement == XLS_TOKEN( control ) && aMceState.empty() ) importControl( rAttribs );
+            if ( getCurrentElement() == XLS_TOKEN( controls ) )
+            {
+                if( aMceState.empty() || ( !aMceState.empty() && aMceState.back() == MCE_STARTED ) )
+                {
+                    if ( getCurrentElement() == XLS_TOKEN( oleObjects ) ) importOleObject( rAttribs );
+                    else
+                        importControl( rAttribs );
+                }
+                else if ( !aMceState.empty() && aMceState.back() == MCE_FOUND_CHOICE )
+                {
+                    // reset the handling within 'Choice'
+                    // this will force attempted handling in Fallback
+                    aMceState.back() = MCE_STARTED;
+                }
+            }
         break;
     }
     return 0;