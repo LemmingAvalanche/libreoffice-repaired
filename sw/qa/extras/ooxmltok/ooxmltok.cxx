LemmingAvalanche/libreoffice-repaired

commit 1bfcc6d14966c2fef58286821876f4b5a77e8daa

n#764005 dmapper: don't insert "at page" anchored graphics as "as character"
Change-Id: Ibdde32014d2fc2fe43d59a5cc6ee7f722adcbfb6

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1524,6 +1524,10 @@ void DomainMapper_Impl::PushShapeContext( const uno::Reference< drawing::XShape 
         xProps->getPropertyValue(rPropNameSupplier.GetName(PROP_VERT_ORIENT_POSITION)) >>= nVertPosition;
         if (nHoriPosition != 0 || nVertPosition != 0)
             bIsGraphic = false;
+        text::TextContentAnchorType nAnchorType(text::TextContentAnchorType_AT_PARAGRAPH);
+        xProps->getPropertyValue(rPropNameSupplier.GetName( PROP_ANCHOR_TYPE )) >>= nAnchorType;
+        if (nAnchorType == text::TextContentAnchorType_AT_PAGE)
+            bIsGraphic = false;
 
         xProps->setPropertyValue(
                 rPropNameSupplier.GetName( PROP_OPAQUE ),

commit e5dcc32287690e944b612e43bb2e75ce6ea87ccc

test also for inline vml shape being anchored inline
This got broken by 1bfcc6d14966c2fef58286821876f4b5a77e8daa.

Change-Id: I8d500ea54bee8b09e1a01cf4cd14d08669f49a73

sw/qa/extras/ooxmltok/ooxmltok.cxx
@@ -263,9 +263,11 @@ void Test::testN705956_1()
     load( "n705956-1.docx" );
 /*
 Get the first image in the document and check it's the one image in the document.
+It should be also anchored inline (as character).
 image = ThisComponent.DrawPage.getByIndex(0)
 graphic = image.Graphic
 xray graphic.Size
+xray image.AnchorType
 */
     uno::Reference<text::XTextDocument> textDocument(mxComponent, uno::UNO_QUERY);
     uno::Reference<drawing::XDrawPageSupplier> drawPageSupplier(textDocument, uno::UNO_QUERY);
@@ -279,6 +281,9 @@ xray graphic.Size
     uno::Reference<awt::XBitmap> bitmap(graphic, uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL( static_cast<sal_Int32>(120), bitmap->getSize().Width );
     CPPUNIT_ASSERT_EQUAL( static_cast<sal_Int32>(106), bitmap->getSize().Height );
+    text::TextContentAnchorType anchorType;
+    imageProperties->getPropertyValue( "AnchorType" ) >>= anchorType;
+    CPPUNIT_ASSERT_EQUAL( text::TextContentAnchorType_AS_CHARACTER, anchorType );
 }
 
 void Test::testN705956_2()