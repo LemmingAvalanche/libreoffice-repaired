LemmingAvalanche/libreoffice-repaired

commit 46a509489efadc4db490f9b45be0700b8224f8cd

mib18: #163225# support long strings in basic

basic/source/sbx/sbxstr.cxx
@@ -44,12 +44,12 @@ using namespace rtl;
 // Die Konversion eines Items auf String wird ueber die Put-Methoden
 // der einzelnen Datentypen abgewickelt, um doppelten Code zu vermeiden.
 
-XubString ImpGetString( const SbxValues* p )
+::rtl::OUString ImpGetString( const SbxValues* p )
 {
     SbxValues aTmp;
-    XubString aRes;
+    ::rtl::OUString aRes;
     aTmp.eType = SbxSTRING;
-    aTmp.pString = &aRes;
+    aTmp.pOUString = &aRes;
     switch( +p->eType )
     {
         case SbxNULL:
@@ -86,8 +86,8 @@ XubString ImpGetString( const SbxValues* p )
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( p->pString )
-                aRes = *p->pString;
+            if ( p->pOUString )
+                *aTmp.pOUString = *p->pOUString;
             break;
         case SbxOBJECT:
         {
@@ -109,7 +109,7 @@ XubString ImpGetString( const SbxValues* p )
         case SbxERROR:
             // Hier wird der String "Error n" erzeugt
             aRes = SbxRes( STRING_ERRORMSG );
-            aRes += p->nUShort; break;
+            aRes += ::rtl::OUString( p->nUShort ); break;
         case SbxDATE:
             ImpPutDate( &aTmp, p->nDouble ); break;
 
@@ -145,7 +145,7 @@ XubString ImpGetString( const SbxValues* p )
 }
 
 // AB 10.4.97, neue Funktion fuer SbxValue::GetCoreString()
-XubString ImpGetCoreString( const SbxValues* p )
+::rtl::OUString ImpGetCoreString( const SbxValues* p )
 {
     // Vorerst nur fuer double
     if( ( p->eType & (~SbxBYREF) ) == SbxDOUBLE )
@@ -153,7 +153,6 @@ XubString ImpGetCoreString( const SbxValues* p )
         SbxValues aTmp;
         XubString aRes;
         aTmp.eType = SbxSTRING;
-        aTmp.pString = &aRes;
         if( p->eType == SbxDOUBLE )
             ImpPutDouble( &aTmp, p->nDouble, /*bCoreString=*/TRUE );
         else
@@ -164,15 +163,15 @@ XubString ImpGetCoreString( const SbxValues* p )
         return ImpGetString( p );
 }
 
-void ImpPutString( SbxValues* p, const XubString* n )
+void ImpPutString( SbxValues* p, const ::rtl::OUString* n )
 {
     SbxValues aTmp;
     aTmp.eType = SbxSTRING;
-    XubString* pTmp = NULL;
+    ::rtl::OUString* pTmp = NULL;
     // Sicherheitshalber, falls ein NULL-Ptr kommt
     if( !n )
-        n = pTmp = new XubString;
-    aTmp.pString = (XubString*) n;
+        n = pTmp = new ::rtl::OUString;
+    aTmp.pOUString = (::rtl::OUString*)n;
     switch( +p->eType )
     {
         case SbxCHAR:
@@ -209,14 +208,15 @@ void ImpPutString( SbxValues* p, const XubString* n )
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( n->Len() )
+            if( n->getLength() )
             {
-                if( !p->pString )
-                    p->pString = new XubString;
-                *p->pString = *n;
+                if( !p->pOUString )
+                    p->pOUString = new ::rtl::OUString( *n );
+                else
+                    *p->pOUString = *n;
             }
             else
-                delete p->pString, p->pString = NULL;
+                delete p->pOUString, p->pOUString = NULL;
             break;
         case SbxOBJECT:
         {
@@ -258,18 +258,18 @@ void ImpPutString( SbxValues* p, const XubString* n )
 }
 
 // Convert string to an array of bytes, preserving unicode (2bytes per character)
-SbxArray* StringToByteArray(const String& rStr)
+SbxArray* StringToByteArray(const ::rtl::OUString& rStr)
 {
-    USHORT nArraySize = rStr.Len() * 2;
-    const sal_Unicode* pSrc = rStr.GetBuffer();
+    sal_Int32 nArraySize = rStr.getLength() * 2;
+    const sal_Unicode* pSrc = rStr.getStr();
     SbxDimArray* pArray = new SbxDimArray(SbxBYTE);
     bool bIncIndex = ( IsBaseIndexOne() && SbiRuntime::isVBAEnabled() );
     if( nArraySize )
     {
         if( bIncIndex )
-            pArray->AddDim( 1, nArraySize );
+            pArray->AddDim32( 1, nArraySize );
         else
-            pArray->AddDim( 0, nArraySize-1 );
+            pArray->AddDim32( 0, nArraySize-1 );
     }
     else
     {
@@ -290,7 +290,7 @@ SbxArray* StringToByteArray(const String& rStr)
 }
 
 // Convert an array of bytes to string (2bytes per character)
-String ByteArrayToString(SbxArray* pArr)
+::rtl::OUString ByteArrayToString(SbxArray* pArr)
 {
     USHORT nCount = pArr->Count();
     OUStringBuffer aStrBuf;
@@ -315,7 +315,5 @@ String ByteArrayToString(SbxArray* pArr)
         aStrBuf.append(aChar);
     }
 
-    String aStr(aStrBuf.makeStringAndClear());
-
-    return aStr;
+    return aStrBuf.makeStringAndClear();
 }

basic/inc/basic/sbxvar.hxx
@@ -178,7 +178,7 @@ struct SbxValues
         sal_uInt64      uInt64;
         int             nInt;
         unsigned int    nUInt;
-        String*         pString;
+        ::rtl::OUString* pOUString;
         SbxDecimal*     pDecimal;
 
         SbxBase*        pObj;
@@ -212,7 +212,7 @@ struct SbxValues
     SbxValues( double _nDouble ): nDouble( _nDouble ), eType(SbxDOUBLE) {}
     SbxValues( int _nInt ): nInt( _nInt ), eType(SbxINT) {}
     SbxValues( unsigned int _nUInt ): nUInt( _nUInt ), eType(SbxUINT) {}
-    SbxValues( const String* _pString ): pString( (String*) _pString ), eType(SbxSTRING) {}
+    SbxValues( const ::rtl::OUString* _pString ): pOUString( (::rtl::OUString*)_pString ), eType(SbxSTRING) {}
     SbxValues( SbxBase* _pObj ): pObj( _pObj ), eType(SbxOBJECT) {}
     SbxValues( sal_Unicode* _pChar ): pChar( _pChar ), eType(SbxLPSTR) {}
     SbxValues( void* _pData ): pData( _pData ), eType(SbxPOINTER) {}
@@ -237,7 +237,8 @@ class SbxValue : public SbxBase
     SbxValue* TheRealValue() const;
 protected:
     SbxValues aData; // Data
-    String    aPic;  // Picture-String
+    ::rtl::OUString aPic;  // Picture-String
+    String          aToolString;  // tool string copy
 
     virtual void Broadcast( ULONG );    // Broadcast-Call
     virtual ~SbxValue();
@@ -303,6 +304,7 @@ public:
     UINT16 GetErr() const;
     const  String& GetString() const;
     const  String& GetCoreString() const;
+    ::rtl::OUString GetOUString() const;
     SbxDecimal* GetDecimal() const;
     SbxBase* GetObject() const;
     BOOL     HasObject() const;
@@ -325,8 +327,8 @@ public:
     BOOL PutDate( double );
     BOOL PutBool( BOOL );
     BOOL PutErr( USHORT );
-    BOOL PutStringExt( const String& );     // with extended analysis (International, "TRUE"/"FALSE")
-    BOOL PutString( const String& );
+    BOOL PutStringExt( const ::rtl::OUString& );     // with extended analysis (International, "TRUE"/"FALSE")
+    BOOL PutString( const ::rtl::OUString& );
     BOOL PutString( const sal_Unicode* );   // Type = SbxSTRING
     BOOL PutpChar( const sal_Unicode* );    // Type = SbxLPSTR
     BOOL PutDecimal( SbxDecimal* pDecimal );

basic/source/classes/sbunoobj.cxx
@@ -1461,7 +1461,7 @@ Any sbxToUnoValue( SbxVariable* pVar, const Type& rType, Property* pUnoProperty 
             aRetVal.setValue( &c , getCharCppuType() );
             break;
         }
-        case TypeClass_STRING:          aRetVal <<= ::rtl::OUString( pVar->GetString() ); break;
+        case TypeClass_STRING:          aRetVal <<= pVar->GetOUString(); break;
         case TypeClass_FLOAT:           aRetVal <<= pVar->GetSingle(); break;
         case TypeClass_DOUBLE:          aRetVal <<= pVar->GetDouble(); break;
         //case TypeClass_OCTET:         break;

basic/source/sbx/sbxbool.cxx
@@ -87,21 +87,20 @@ enum SbxBOOL ImpGetBool( const SbxValues* p )
         case SbxSTRING:
         case SbxLPSTR:
             nRes = SbxFALSE;
-            if( p->pString )
+            if ( p->pOUString )
             {
-                if( p->pString->EqualsIgnoreCaseAscii( SbxRes( STRING_TRUE ) ) )
+                if( p->pOUString->equalsIgnoreAsciiCase( SbxRes( STRING_TRUE ) ) )
                     nRes = SbxTRUE;
-                else if( !p->pString->EqualsIgnoreCaseAscii( SbxRes( STRING_FALSE ) ) )
+                else if( p->pOUString->equalsIgnoreAsciiCase( SbxRes( STRING_FALSE ) ) )
                 {
                     // Jetzt kann es noch in eine Zahl konvertierbar sein
                     BOOL bError = TRUE;
                     double n;
                     SbxDataType t;
                     USHORT nLen = 0;
-                    String s( *p->pString );
-                    if( ImpScan( s, n, t, &nLen ) == SbxERR_OK )
+                    if( ImpScan( *p->pOUString, n, t, &nLen ) == SbxERR_OK )
                     {
-                        if( nLen == s.Len() )
+                        if( nLen == p->pOUString->getLength() )
                         {
                             bError = FALSE;
                             if( n != 0.0 )
@@ -202,9 +201,10 @@ void ImpPutBool( SbxValues* p, INT16 n )
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
-                p->pString = new XubString;
-            *p->pString = SbxRes( n ? STRING_TRUE : STRING_FALSE );
+            if ( !p->pOUString )
+                p->pOUString = new ::rtl::OUString( SbxRes( n ? STRING_TRUE : STRING_FALSE ) );
+            else
+                *p->pOUString = SbxRes( n ? STRING_TRUE : STRING_FALSE );
             break;
 
         case SbxOBJECT:

basic/source/sbx/sbxbyte.cxx
@@ -165,13 +165,13 @@ start:
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
+            if( !p->pOUString )
                 nRes = 0;
             else
             {
                 double d;
                 SbxDataType t;
-                if( ImpScan( *p->pString, d, t, NULL ) != SbxERR_OK )
+                if( ImpScan( *p->pOUString, d, t, NULL ) != SbxERR_OK )
                     nRes = 0;
                 else if( d > SbxMAXBYTE )
                 {
@@ -279,9 +279,9 @@ void ImpPutByte( SbxValues* p, BYTE n )
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
-                p->pString = new XubString;
-            ImpCvtNum( (double) n, 0, *p->pString );
+            if( !p->pOUString )
+                p->pOUString = new ::rtl::OUString;
+            ImpCvtNum( (double) n, 0, *p->pOUString );
             break;
         case SbxOBJECT:
         {

basic/source/sbx/sbxchar.cxx
@@ -39,7 +39,7 @@ using namespace rtl;
 xub_Unicode ImpGetChar( const SbxValues* p )
 {
     SbxValues aTmp;
-    xub_Unicode nRes;
+    xub_Unicode nRes = 0;
 start:
     switch( +p->eType )
     {
@@ -156,13 +156,11 @@ start:
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
-                nRes = 0;
-            else
+            if ( p->pOUString )
             {
                 double d;
                 SbxDataType t;
-                if( ImpScan( *p->pString, d, t, NULL ) != SbxERR_OK )
+                if( ImpScan( *p->pOUString, d, t, NULL ) != SbxERR_OK )
                     nRes = 0;
                 else if( d > SbxMAXCHAR )
                 {
@@ -274,9 +272,10 @@ start:
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
-                p->pString = new XubString;
-            *p->pString = n;
+            if ( !p->pOUString )
+                p->pOUString = new ::rtl::OUString( n );
+            else
+                *p->pOUString = ::rtl::OUString( n );
             break;
         case SbxOBJECT:
         {

basic/source/sbx/sbxconv.hxx
@@ -33,13 +33,13 @@
 class SbxArray;
 
 // SBXSCAN.CXX
-extern void ImpCvtNum( double nNum, short nPrec, String& rRes, BOOL bCoreString=FALSE );
+extern void ImpCvtNum( double nNum, short nPrec, ::rtl::OUString& rRes, BOOL bCoreString=FALSE );
 extern SbxError ImpScan
-    ( const String& rSrc, double& nVal, SbxDataType& rType, USHORT* pLen,
+    ( const ::rtl::OUString& rSrc, double& nVal, SbxDataType& rType, USHORT* pLen,
       BOOL bAllowIntntl=FALSE, BOOL bOnlyIntntl=FALSE );
 
 // mit erweiterter Auswertung (International, "TRUE"/"FALSE")
-extern BOOL ImpConvStringExt( String& rSrc, SbxDataType eTargetType );
+extern BOOL ImpConvStringExt( ::rtl::OUString& rSrc, SbxDataType eTargetType );
 
 // SBXINT.CXX
 
@@ -116,9 +116,9 @@ void    ImpPutDate( SbxValues*, double );
 
 // SBXSTR.CXX
 
-String  ImpGetString( const SbxValues* );
-String  ImpGetCoreString( const SbxValues* );
-void    ImpPutString( SbxValues*, const String* );
+::rtl::OUString     ImpGetString( const SbxValues* );
+::rtl::OUString     ImpGetCoreString( const SbxValues* );
+void    ImpPutString( SbxValues*, const ::rtl::OUString* );
 
 // SBXCHAR.CXX
 
@@ -145,7 +145,7 @@ enum SbxBOOL ImpGetBool( const SbxValues* );
 void    ImpPutBool( SbxValues*, INT16 );
 
 // ByteArry <--> String
-SbxArray* StringToByteArray(const String& rStr);
-String ByteArrayToString(SbxArray* pArr);
+SbxArray* StringToByteArray(const ::rtl::OUString& rStr);
+::rtl::OUString ByteArrayToString(SbxArray* pArr);
 
 #endif

basic/source/sbx/sbxcurr.cxx
@@ -37,8 +37,8 @@
 #include <basic/sbxvar.hxx>
 #include "sbxconv.hxx"
 
-static String   ImpCurrencyToString( const SbxINT64& );
-static SbxINT64 ImpStringToCurrency( const String& );
+static ::rtl::OUString   ImpCurrencyToString( const SbxINT64& );
+static SbxINT64 ImpStringToCurrency( const ::rtl::OUString& );
 
 SbxINT64 ImpGetCurrency( const SbxValues* p )
 {
@@ -121,10 +121,10 @@ start:
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
+            if( !p->pOUString )
                 nRes.SetNull();
             else
-                nRes = ImpStringToCurrency( *p->pString );
+                nRes = ImpStringToCurrency( *p->pOUString );
             break;
         case SbxOBJECT:
         {
@@ -224,10 +224,10 @@ start:
         case SbxBYREF | SbxSTRING:
         case SbxSTRING:
         case SbxLPSTR:
-            if( !p->pString )
-                p->pString = new XubString;
+            if( !p->pOUString )
+                p->pOUString = new ::rtl::OUString;
 
-            *p->pString = ImpCurrencyToString( r );
+            *p->pOUString = ImpCurrencyToString( r );
             break;
         case SbxOBJECT:
         {
@@ -319,7 +319,7 @@ start:
 
 // Hilfs-Funktionen zur Wandlung
 
-static String ImpCurrencyToString( const SbxINT64 &r )
+static ::rtl::OUString ImpCurrencyToString( const SbxINT64 &r )
 {
     BigInt a10000 = 10000;
 
@@ -331,20 +331,20 @@ static String ImpCurrencyToString( const SbxINT64 &r )
     aFrac %= a10000;
     aFrac += a10000;
 
-    String aString;
+    ::rtl::OUString aString;
     if( r.nHigh < 0 )
-        aString = '-';
+        aString = ::rtl::OUString( (sal_Unicode)'-' );
     aString += aInt.GetString();
-    aString += '.';
+    aString += ::rtl::OUString( (sal_Unicode)'.' );
     aString += aFrac.GetString().GetBuffer()+1;
     return aString;
 }
 
-static SbxINT64 ImpStringToCurrency( const String &r )
+static SbxINT64 ImpStringToCurrency( const ::rtl::OUString &r )
 {
     int nDec = 4;
     String aStr;
-    const sal_Unicode* p = r.GetBuffer();
+    const sal_Unicode* p = r.getStr();
 
     if( *p == '-' )
         aStr += *p++;

commit 0d2f3c7f30233c9e63eb99b71092f868d6bc2fbb

very suspicious combination of used SbxValues and unused aRes
Looks like a regression from 46a509489efadc4db490f9b45be0700b8224f8cd where for
every other case we did...

-    XubString aRes;
+    ::rtl::OUString aRes;
     aTmp.eType = SbxSTRING;
-    aTmp.pString = &aRes;
+    aTmp.pOUString = &aRes;

while for this case we did only...

     SbxValues aTmp;
     XubString aRes;
     aTmp.eType = SbxSTRING;
-    aTmp.pString = &aRes;

Change-Id: I106cfbcc0fc0b27a9adcbb243d0d69c65b167005

basic/source/sbx/sbxstr.cxx
@@ -134,8 +134,9 @@
     if( ( p->eType & (~SbxBYREF) ) == SbxDOUBLE )
     {
         SbxValues aTmp;
-        XubString aRes;
+        rtl::OUString aRes;
         aTmp.eType = SbxSTRING;
+        aTmp.pOUString = &aRes;
         if( p->eType == SbxDOUBLE )
             ImpPutDouble( &aTmp, p->nDouble, sal_True );    // true = bCoreString
         else