LemmingAvalanche/libreoffice-repaired

commit 7e8f4c1cc9a9a03a09810c0a3ddce25c3d649669

Double-capital autocor takes spellcheck in account

editeng/source/misc/svxacorr.cxx
@@ -443,14 +443,22 @@ BOOL SvxAutoCorrect::FnCptlSttWrd( SvxAutoCorrDoc& rDoc, const String& rTxt,
         String sWord( rTxt.Copy( nSttPos - 1, nEndPos - nSttPos + 1 ));
         if( !FindInWrdSttExceptList(eLang, sWord) )
         {
-            sal_Unicode cSave = rTxt.GetChar( nSttPos );
-            String sChar( cSave );
-            rCC.toLower( sChar );
-            if( sChar.GetChar(0) != cSave && rDoc.Replace( nSttPos, sChar ))
+            ::com::sun::star::uno::Reference<
+                ::com::sun::star::linguistic2::XSpellChecker1 > xSpeller =
+                SvxGetSpellChecker();
+            Sequence< ::com::sun::star::beans::PropertyValue > aEmptySeq;
+            // Check that word isn't correctly spelled before correcting
+            if(xSpeller->spell( sWord, eLang, aEmptySeq ) != NULL)
             {
-                if( SaveWordWrdSttLst & nFlags )
-                    rDoc.SaveCpltSttWord( CptlSttWrd, nSttPos, sWord, cSave );
-                bRet = TRUE;
+                sal_Unicode cSave = rTxt.GetChar( nSttPos );
+                String sChar( cSave );
+                rCC.toLower( sChar );
+                if( sChar.GetChar(0) != cSave && rDoc.Replace( nSttPos, sChar ))
+                {
+                    if( SaveWordWrdSttLst & nFlags )
+                        rDoc.SaveCpltSttWord( CptlSttWrd, nSttPos, sWord, cSave );
+                    bRet = TRUE;
+                }
             }
         }
     }

commit 503ae1ae5042206d9548db9774fec3997a01e807

Related fdo#53006 Make CApitals correction work without dictionary
In a language for which there is no dictionary available (no "ABC" check in
front of the current item in Writer's "Format - Character... - Font - Language"
list), "Correct TWo INitial CApitals" (from Writer's "Tools - AutoCorrect
Options... - Options") did not work (i.e., typing "FOo" followed by a space
would not change it to "Foo").  That was apparently a regression introduced with
7e8f4c1cc9a9a03a09810c0a3ddce25c3d649669 "Double-capital autocor takes
spellcheck in account."  (I verified that with this fix words like "MPs" in
"English (UK)" are still left as "MPs.")  Thanks to Caolán for help.

Change-Id: Ia76286e4ca73138ce3571145b9c40b031a4553ba

editeng/source/misc/svxacorr.cxx
@@ -371,24 +371,26 @@ sal_Bool SvxAutoCorrect::FnCptlSttWrd( SvxAutoCorrDoc& rDoc, const String& rTxt,
         String sWord( rTxt.Copy( nSttPos - 1, nEndPos - nSttPos + 1 ));
         if( !FindInWrdSttExceptList(eLang, sWord) )
         {
+            // Check that word isn't correctly spelled before correcting:
             ::com::sun::star::uno::Reference<
                 ::com::sun::star::linguistic2::XSpellChecker1 > xSpeller =
                 SvxGetSpellChecker();
-            Sequence< ::com::sun::star::beans::PropertyValue > aEmptySeq;
-            // Check that word isn't correctly spelled before correcting
-            ::com::sun::star::uno::Reference< ::com::sun::star::linguistic2::XSpellAlternatives > xSpellAlt
-                = xSpeller->spell(sWord, eLang, aEmptySeq);
-            if(xSpellAlt.is())
+            if( xSpeller->hasLanguage(eLang) )
             {
-                sal_Unicode cSave = rTxt.GetChar( nSttPos );
-                rtl::OUString sChar( cSave );
-                sChar = rCC.lowercase( sChar );
-                if( sChar[0] != cSave && rDoc.ReplaceRange( nSttPos, 1, sChar ))
+                Sequence< ::com::sun::star::beans::PropertyValue > aEmptySeq;
+                if (!xSpeller->spell(sWord, eLang, aEmptySeq).is())
                 {
-                    if( SaveWordWrdSttLst & nFlags )
-                        rDoc.SaveCpltSttWord( CptlSttWrd, nSttPos, sWord, cSave );
-                    bRet = sal_True;
+                    return false;
                 }
+            }
+            sal_Unicode cSave = rTxt.GetChar( nSttPos );
+            rtl::OUString sChar( cSave );
+            sChar = rCC.lowercase( sChar );
+            if( sChar[0] != cSave && rDoc.ReplaceRange( nSttPos, 1, sChar ))
+            {
+                if( SaveWordWrdSttLst & nFlags )
+                    rDoc.SaveCpltSttWord( CptlSttWrd, nSttPos, sWord, cSave );
+                bRet = sal_True;
             }
         }
     }