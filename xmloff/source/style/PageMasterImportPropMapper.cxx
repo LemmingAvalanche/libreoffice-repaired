LemmingAvalanche/libreoffice-repaired

commit 14a8652814ece944c9f95547587ddf078da9edeb

fdo#38056: ODF import: fix page style attributes:
PageMasterImportPropMapper: in the case of a single "fo:border"
and "style:border-line-width" attribute, it is possible that the border
is imported wrongly, like this:

1. pAllBorderProperty is set from the imported value, with name "TopBorder"
2. individual pNewBorder[i] are created as copies from pAllBorderProperty,
   one of which also with name "TopBorder"
3. pNewBorder[i] is updated with widths from pBorderWidths[i]
4. the individual pNewBorder[i] are added to the property vector
5. the property vector is sorted by property name
6. the properites are applied in order; if the pNewBorder[TOP]
   happens to precede the pAllBorderProperty (which is indeterminate
   as they both have name "TopBorder"), then the pAllBorderProperty
   will overwrite the border widths computed in step 3.

Thus, nerf the various pAllFoo properties so they do not override
the individual Foo properties later on.

Change-Id: I87755f1184d59da2aa72ac053e6f77d7295d6958

xmloff/source/style/PageMasterImportPropMapper.cxx
@@ -408,6 +408,57 @@ void PageMasterImportPropertyMapper::finished(::std::vector< XMLPropertyState >&
         rProperties.push_back(*pFooterDynamic);
         delete pFooterDynamic;
     }
+
+    // fdo#38056: nerf the various AllFoo properties so they do not override
+    // the individual Foo properties later on
+    if (pAllPaddingProperty)
+    {
+        pAllPaddingProperty->mnIndex = -1;
+    }
+    if (pAllBorderProperty)
+    {
+        pAllBorderProperty->mnIndex = -1;
+    }
+    if (pAllBorderWidthProperty)
+    {
+        pAllBorderWidthProperty->mnIndex = -1;
+    }
+    if (pAllHeaderPaddingProperty)
+    {
+        pAllHeaderPaddingProperty->mnIndex = -1;
+    }
+    if (pAllHeaderBorderProperty)
+    {
+        pAllHeaderBorderProperty->mnIndex = -1;
+    }
+    if (pAllHeaderBorderWidthProperty)
+    {
+        pAllHeaderBorderWidthProperty->mnIndex = -1;
+    }
+    if (pAllFooterPaddingProperty)
+    {
+        pAllFooterPaddingProperty->mnIndex = -1;
+    }
+    if (pAllFooterBorderProperty)
+    {
+        pAllFooterBorderProperty->mnIndex = -1;
+    }
+    if (pAllFooterBorderWidthProperty)
+    {
+        pAllFooterBorderWidthProperty->mnIndex = -1;
+    }
+    if (pAllMarginProperty)
+    {
+        pAllMarginProperty->mnIndex = -1;
+    }
+    if (pAllHeaderMarginProperty)
+    {
+        pAllHeaderMarginProperty->mnIndex = -1;
+    }
+    if (pAllFooterMarginProperty)
+    {
+        pAllFooterMarginProperty->mnIndex = -1;
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit ad17b40ae6f242a58a04c0effbdbf77096cd7481

fdo#38056: fix XMLTextImportPropertyMapper regression:
The invalidation introduced in 14a8652814ece944c9f95547587ddf078da9edeb
is unfortunately in the wrong place; the pAllFoo actually point at
vector elements, and so they must not be dereferenced after new elements
are added to the vector, so move the invalidation up a bit.

Change-Id: I244d1d5cfd40469ff6d2c081e057355460d34cf4

xmloff/source/style/PageMasterImportPropMapper.cxx
@@ -353,6 +353,58 @@ void PageMasterImportPropertyMapper::finished(::std::vector< XMLPropertyState >&
         aAny.setValue( &bValue, ::getBooleanCppuType() );
         pFooterDynamic = new XMLPropertyState(pFooterMinHeight->mnIndex + 1, aAny);
     }
+
+    // fdo#38056: nerf the various AllFoo properties so they do not override
+    // the individual Foo properties later on
+    if (pAllPaddingProperty)
+    {
+        pAllPaddingProperty->mnIndex = -1;
+    }
+    if (pAllBorderProperty)
+    {
+        pAllBorderProperty->mnIndex = -1;
+    }
+    if (pAllBorderWidthProperty)
+    {
+        pAllBorderWidthProperty->mnIndex = -1;
+    }
+    if (pAllHeaderPaddingProperty)
+    {
+        pAllHeaderPaddingProperty->mnIndex = -1;
+    }
+    if (pAllHeaderBorderProperty)
+    {
+        pAllHeaderBorderProperty->mnIndex = -1;
+    }
+    if (pAllHeaderBorderWidthProperty)
+    {
+        pAllHeaderBorderWidthProperty->mnIndex = -1;
+    }
+    if (pAllFooterPaddingProperty)
+    {
+        pAllFooterPaddingProperty->mnIndex = -1;
+    }
+    if (pAllFooterBorderProperty)
+    {
+        pAllFooterBorderProperty->mnIndex = -1;
+    }
+    if (pAllFooterBorderWidthProperty)
+    {
+        pAllFooterBorderWidthProperty->mnIndex = -1;
+    }
+    if (pAllMarginProperty)
+    {
+        pAllMarginProperty->mnIndex = -1;
+    }
+    if (pAllHeaderMarginProperty)
+    {
+        pAllHeaderMarginProperty->mnIndex = -1;
+    }
+    if (pAllFooterMarginProperty)
+    {
+        pAllFooterMarginProperty->mnIndex = -1;
+    }
+
     for (sal_uInt16 i = 0; i < 4; i++)
     {
         if (pNewMargins[i].get())
@@ -407,57 +459,6 @@ void PageMasterImportPropertyMapper::finished(::std::vector< XMLPropertyState >&
     {
         rProperties.push_back(*pFooterDynamic);
         delete pFooterDynamic;
-    }
-
-    // fdo#38056: nerf the various AllFoo properties so they do not override
-    // the individual Foo properties later on
-    if (pAllPaddingProperty)
-    {
-        pAllPaddingProperty->mnIndex = -1;
-    }
-    if (pAllBorderProperty)
-    {
-        pAllBorderProperty->mnIndex = -1;
-    }
-    if (pAllBorderWidthProperty)
-    {
-        pAllBorderWidthProperty->mnIndex = -1;
-    }
-    if (pAllHeaderPaddingProperty)
-    {
-        pAllHeaderPaddingProperty->mnIndex = -1;
-    }
-    if (pAllHeaderBorderProperty)
-    {
-        pAllHeaderBorderProperty->mnIndex = -1;
-    }
-    if (pAllHeaderBorderWidthProperty)
-    {
-        pAllHeaderBorderWidthProperty->mnIndex = -1;
-    }
-    if (pAllFooterPaddingProperty)
-    {
-        pAllFooterPaddingProperty->mnIndex = -1;
-    }
-    if (pAllFooterBorderProperty)
-    {
-        pAllFooterBorderProperty->mnIndex = -1;
-    }
-    if (pAllFooterBorderWidthProperty)
-    {
-        pAllFooterBorderWidthProperty->mnIndex = -1;
-    }
-    if (pAllMarginProperty)
-    {
-        pAllMarginProperty->mnIndex = -1;
-    }
-    if (pAllHeaderMarginProperty)
-    {
-        pAllHeaderMarginProperty->mnIndex = -1;
-    }
-    if (pAllFooterMarginProperty)
-    {
-        pAllFooterMarginProperty->mnIndex = -1;
     }
 }
 