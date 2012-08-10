LibreOffice/core

fdo#43135 - fix for MULTINOMIAL only supporting parameters up to 170
Change-Id: I9366de24414fa10e292e42df08a9658d90e5c1c1

scaddins/source/analysis/analysis.cxx
@@ -689,26 +689,21 @@ AnalysisAddIn::getMultinomial( constREFXPS& xOpt, const SEQSEQ( sal_Int32 )& aVL
     if( aValList.Count() == 0 )
         return 0.0;
 
-    sal_Int32 nZ = 0;
-    double    fN = 1.0;
+    sal_Int64 nZ = 0;
+    double    fRet = 1.0;
 
     for( const double *p = aValList.First(); p; p = aValList.Next() )
     {
         double fInt = (*p >= 0.0) ? rtl::math::approxFloor( *p ) : rtl::math::approxCeil( *p );
-        if ( fInt < 0.0 || fInt > 170.0 )
+        if ( fInt < 0.0 )
             THROW_IAE;
-        sal_Int32 n = static_cast< sal_Int32 >( fInt );
+        sal_Int64 n = static_cast< sal_Int64 >( fInt );
         if( n > 0 )
         {
             nZ += n;
-            fN *= Fak( n );
+            fRet *= BinomialCoefficient(nZ, n);
         }
     }
-
-    if( nZ > 170 )
-        THROW_IAE;
-
-    double fRet = Fak( nZ ) / fN;
     RETURN_FINITE( fRet );
 }
 

scaddins/source/analysis/analysishelper.hxx
@@ -94,7 +94,7 @@ inline double       GetYearFrac( constREFXPS& xOpt, sal_Int32 nStartDate, sal_In
                         THROWDEF_RTE_IAE;
 inline void         AlignDate( sal_uInt16& rDay, sal_uInt16 nMonth, sal_uInt16 nYear );
 
-double              Fak( sal_Int32 n );
+double              BinomialCoefficient( sal_Int64 n, sal_Int64 k );
 double              GetGcd( double f1, double f2 );
 double              ConvertToDec( const STRING& rFromNum, sal_uInt16 nBaseFrom, sal_uInt16 nCharLim ) THROWDEF_RTE_IAE;
 STRING              ConvertFromDec(

LemmingAvalanche/libreoffice-repaired

scaddins/source/analysis/analysis.cxx
@@ -689,26 +689,21 @@ AnalysisAddIn::getMultinomial( constREFXPS& xOpt, const SEQSEQ( sal_Int32 )& aVL
     if( aValList.Count() == 0 )
         return 0.0;
 
-    sal_Int32 nZ = 0;
-    double    fN = 1.0;
+    double nZ = 0;
+    double fRet = 1.0;
 
     for( const double *p = aValList.First(); p; p = aValList.Next() )
     {
-        double fInt = (*p >= 0.0) ? rtl::math::approxFloor( *p ) : rtl::math::approxCeil( *p );
-        if ( fInt < 0.0 || fInt > 170.0 )
+        double n = (*p >= 0.0) ? rtl::math::approxFloor( *p ) : rtl::math::approxCeil( *p );
+        if ( n < 0.0 )
             THROW_IAE;
-        sal_Int32 n = static_cast< sal_Int32 >( fInt );
-        if( n > 0 )
+
+        if( n > 0.0 )
         {
             nZ += n;
-            fN *= Fak( n );
+            fRet *= BinomialCoefficient(nZ, n);
         }
     }
-
-    if( nZ > 170 )
-        THROW_IAE;
-
-    double fRet = Fak( nZ ) / fN;
     RETURN_FINITE( fRet );
 }
 

scaddins/source/analysis/analysishelper.hxx
@@ -94,7 +94,7 @@ inline double       GetYearFrac( constREFXPS& xOpt, sal_Int32 nStartDate, sal_In
                         THROWDEF_RTE_IAE;
 inline void         AlignDate( sal_uInt16& rDay, sal_uInt16 nMonth, sal_uInt16 nYear );
 
-double              Fak( sal_Int32 n );
+double              BinomialCoefficient( double n, double k );
 double              GetGcd( double f1, double f2 );
 double              ConvertToDec( const STRING& rFromNum, sal_uInt16 nBaseFrom, sal_uInt16 nCharLim ) THROWDEF_RTE_IAE;
 STRING              ConvertFromDec(