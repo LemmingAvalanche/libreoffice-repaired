LemmingAvalanche/libreoffice-repaired

commit e78e40c76343ba8d5e407d2cd5cde84593e1d30c

fdo#46193 MessBox was made copyable
Part of MultiLineEdit was moved down from stvools to vcl
with name VCLMultiLineEdit. MessBox uses it to display the
message in read-only mode. Some of svtools' classes - which
are necessary to implement VCLMultiLineEdit - were moved to
vcl as a whole, and their includes are rewrite.
Note: ExtTextView and ExtTextEngine classes would be leaved in svtools
if VCLMultiLineEdit is a template class, but two macros: IMPL_LINK
end IMPL_LINK_NOARG make it impossible to use template syntax.

Change-Id: I26543868d8081c225c7125404d23369de3c3afcd

vcl/source/window/msgbox.cxx
@@ -38,6 +38,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/wrkwin.hxx>
 #include <vcl/fixed.hxx>
+#include <vcl/vclmedit.hxx>
 #include <vcl/msgbox.hxx>
 #include <vcl/button.hxx>
 #include <vcl/mnemonic.hxx>
@@ -66,7 +67,7 @@ static void ImplInitMsgBoxImageList()
 
 void MessBox::ImplInitMessBoxData()
 {
-    mpFixedText         = NULL;
+    mpVCLMultiLineEdit  = NULL;
     mpFixedImage        = NULL;
     mbHelpBtn           = sal_False;
     mpCheckBox          = NULL;
@@ -203,7 +204,7 @@ void MessBox::ImplLoadRes( const ResId& )
 
 MessBox::~MessBox()
 {
-    delete mpFixedText;
+    delete mpVCLMultiLineEdit;
     delete mpFixedImage;
     delete mpCheckBox;
 }
@@ -236,16 +237,16 @@ void MessBox::ImplPosControls()
     Point           aTextPos( IMPL_DIALOG_OFFSET, IMPL_DIALOG_OFFSET+IMPL_MSGBOX_OFFSET_EXTRA_Y );
     Size            aImageSize;
     Size            aPageSize;
-    Size            aFixedSize;
+    Size            aMEditSize;
     long            nTitleWidth;
     long            nButtonSize = ImplGetButtonSize();
     long            nMaxWidth = GetDesktopRectPixel().GetWidth()-8;
     long            nMaxLineWidth;
     long            nWidth;
-    WinBits         nWinStyle = WB_LEFT | WB_WORDBREAK | WB_NOLABEL | WB_INFO;
+    WinBits         nWinStyle = WB_LEFT | WB_WORDBREAK | WB_NOLABEL;
     sal_uInt16          nTextStyle = TEXT_DRAW_MULTILINE | TEXT_DRAW_TOP | TEXT_DRAW_LEFT;
 
-    delete mpFixedText;
+    delete mpVCLMultiLineEdit;
     if ( mpFixedImage )
     {
         delete mpFixedImage;
@@ -325,25 +326,27 @@ void MessBox::ImplPosControls()
         aFormatRect = GetTextRect( aRect, aMessText, nTextStyle, &aTextInfo );
     }
 
-    // Style fuer FixedText ermitteln
+    // Style fuer VCLMultiLineEdit ermitteln
+    mpVCLMultiLineEdit = new VCLMultiLineEdit( this, nWinStyle );
+    mpVCLMultiLineEdit->SetText( aMessText );
+    aMEditSize = mpVCLMultiLineEdit->CalcMinimumSize();
+
     aPageSize.Width()   = aImageSize.Width();
-    aFixedSize.Width()  = aTextInfo.GetMaxLineWidth()+1;
-    aFixedSize.Height() = aFormatRect.GetHeight();
-    if ( aFixedSize.Height() < aImageSize.Height() )
+    if ( aMEditSize.Height() < aImageSize.Height() )
     {
         nWinStyle |= WB_VCENTER;
         aPageSize.Height()  = aImageSize.Height();
-        aFixedSize.Height() = aImageSize.Height();
+        aMEditSize.Height() = aImageSize.Height();
     }
     else
     {
         nWinStyle |= WB_TOP;
-        aPageSize.Height()  = aFixedSize.Height();
+        aPageSize.Height()  = aMEditSize.Height();
     }
     if ( aImageSize.Width() )
         aPageSize.Width() += IMPL_SEP_MSGBOX_IMAGE;
     aPageSize.Width()  += (IMPL_DIALOG_OFFSET*2)+(IMPL_MSGBOX_OFFSET_EXTRA_X*2);
-    aPageSize.Width()  += aFixedSize.Width()+1;
+    aPageSize.Width()  += aMEditSize.Width()+1;
     aPageSize.Height() += (IMPL_DIALOG_OFFSET*2)+(IMPL_MSGBOX_OFFSET_EXTRA_Y*2);
 
     if ( aPageSize.Width() < IMPL_MINSIZE_MSGBOX_WIDTH )
@@ -353,7 +356,7 @@ void MessBox::ImplPosControls()
 
     if ( maCheckBoxText.Len() )
     {
-        Size aMinCheckboxSize ( aFixedSize );
+        Size aMinCheckboxSize ( aMEditSize );
         if ( aPageSize.Width() < IMPL_MINSIZE_MSGBOX_WIDTH+80 )
         {
             aPageSize.Width() = IMPL_MINSIZE_MSGBOX_WIDTH+80;
@@ -388,7 +391,7 @@ void MessBox::ImplPosControls()
         mpCheckBox->SetText( maCheckBoxText );
 
         Point aPos( aTextPos );
-        aPos.Y() += aFixedSize.Height() + (IMPL_DIALOG_OFFSET)+(IMPL_MSGBOX_OFFSET_EXTRA_Y*2);
+        aPos.Y() += aMEditSize.Height() + (IMPL_DIALOG_OFFSET)+(IMPL_MSGBOX_OFFSET_EXTRA_Y*2);
 
         // increase messagebox
         aPageSize.Height() += aSize.Height() + (IMPL_DIALOG_OFFSET*2)+(IMPL_MSGBOX_OFFSET_EXTRA_Y*2);
@@ -397,12 +400,10 @@ void MessBox::ImplPosControls()
         mpCheckBox->Show();
     }
 
-    mpFixedText = new FixedText( this, nWinStyle );
-    if( mpFixedText->GetStyle() & WB_EXTRAOFFSET ) // TODO: use CalcMinimumSize() instead
-        aFixedSize.Width() += 2;
-    mpFixedText->SetPosSizePixel( aTextPos, aFixedSize );
-    mpFixedText->SetText( aMessText );
-    mpFixedText->Show();
+
+    mpVCLMultiLineEdit->SetPosSizePixel( aTextPos, aMEditSize );
+    mpVCLMultiLineEdit->Show();
+    mpVCLMultiLineEdit->SetPaintTransparent(sal_True);
     SetPageSizePixel( aPageSize );
 }
 

accessibility/inc/accessibility/extended/textwindowaccessibility.hxx
@@ -31,10 +31,10 @@
 
 #include <toolkit/awt/vclxaccessiblecomponent.hxx>
 #include <svl/lstner.hxx>
-#include <svtools/textdata.hxx>
-#include <svtools/texteng.hxx>
-#include <svtools/textview.hxx>
-#include <svtools/txtattr.hxx>
+#include <vcl/textdata.hxx>
+#include <vcl/texteng.hxx>
+#include <vcl/textview.hxx>
+#include <vcl/txtattr.hxx>
 #include <com/sun/star/awt/FontWeight.hpp>
 #include <com/sun/star/lang/EventObject.hpp>
 #include <com/sun/star/uno/Reference.hxx>

basctl/source/basicide/baside2.cxx
@@ -42,7 +42,7 @@
 #include <svl/aeitem.hxx>
 #include <svl/srchitem.hxx>
 #include <svl/whiter.hxx>
-#include <svtools/xtextedt.hxx>
+#include <vcl/xtextedt.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
 #include <vcl/msgbox.hxx>
 

basctl/source/basicide/baside2b.cxx
@@ -38,8 +38,8 @@
 #include <sfx2/viewfrm.hxx>
 #include <vcl/msgbox.hxx>
 #include <svl/urihelper.hxx>
-#include <svtools/xtextedt.hxx>
-#include <svtools/txtattr.hxx>
+#include <vcl/xtextedt.hxx>
+#include <vcl/txtattr.hxx>
 #include <svtools/textwindowpeer.hxx>
 #include <vcl/taskpanelist.hxx>
 #include <vcl/help.hxx>

basctl/source/basicide/basides1.cxx
@@ -50,7 +50,7 @@
 #include <svl/srchitem.hxx>
 #include <svl/visitem.hxx>
 #include <svl/whiter.hxx>
-#include <svtools/xtextedt.hxx>
+#include <vcl/xtextedt.hxx>
 #include <vcl/msgbox.hxx>
 
 using namespace ::com::sun::star;

basctl/source/basicide/basides2.cxx
@@ -29,9 +29,9 @@
 #include <baside2.hxx>
 #include <basdoc.hxx>
 #include <basobj.hxx>
-#include <svtools/texteng.hxx>
-#include <svtools/textview.hxx>
-#include <svtools/xtextedt.hxx>
+#include <vcl/texteng.hxx>
+#include <vcl/textview.hxx>
+#include <vcl/xtextedt.hxx>
 #include <tools/diagnose_ex.h>
 #include <sfx2/childwin.hxx>
 #include <sfx2/dispatch.hxx>

basctl/source/basicide/linenumberwindow.cxx
@@ -29,8 +29,8 @@
 #include "baside2.hxx"
 #include "linenumberwindow.hxx"
 
-#include <svtools/xtextedt.hxx>
-#include <svtools/textview.hxx>
+#include <vcl/xtextedt.hxx>
+#include <vcl/textview.hxx>
 
 LineNumberWindow::LineNumberWindow( Window* pParent, ModulWindow* pModulWin ) :
     Window( pParent, WB_BORDER ),

cui/source/dialogs/SpellAttrib.hxx
@@ -28,7 +28,7 @@
 #ifndef _SVX_SPELL_ATTRIB
 #define _SVX_SPELL_ATTRIB
 
-#include <svtools/txtattr.hxx>
+#include <vcl/txtattr.hxx>
 #include <i18npool/lang.h>
 #include <com/sun/star/uno/Reference.h>
 #include <com/sun/star/uno/Sequence.h>

commit 2d60a8fb6087ff63142cf1785a03aab63b0d121a

message boxes always showed one line of text
restore the old code, use aTextInfo to determine the width, as before.
(regression from e78e40c76343ba8d5e407d2cd5cde84593e1d30c)

Change-Id: I91def4db307cb4d1af74d79831afa3b9fd68bcbc

vcl/source/window/msgbox.cxx
@@ -327,10 +327,8 @@ void MessBox::ImplPosControls()
     }
 
     // Style fuer VCLMultiLineEdit ermitteln
-    mpVCLMultiLineEdit = new VCLMultiLineEdit( this, nWinStyle );
-    mpVCLMultiLineEdit->SetText( aMessText );
-    aMEditSize = mpVCLMultiLineEdit->CalcMinimumSize();
-
+    aMEditSize.Width()  = aTextInfo.GetMaxLineWidth()+1;
+    aMEditSize.Height() = aFormatRect.GetHeight();
     aPageSize.Width()   = aImageSize.Width();
     if ( aMEditSize.Height() < aImageSize.Height() )
     {
@@ -400,7 +398,8 @@ void MessBox::ImplPosControls()
         mpCheckBox->Show();
     }
 
-
+    mpVCLMultiLineEdit = new VCLMultiLineEdit( this, nWinStyle );
+    mpVCLMultiLineEdit->SetText( aMessText );
     mpVCLMultiLineEdit->SetPosSizePixel( aTextPos, aMEditSize );
     mpVCLMultiLineEdit->Show();
     mpVCLMultiLineEdit->SetPaintTransparent(sal_True);