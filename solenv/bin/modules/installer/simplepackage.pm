LemmingAvalanche/libreoffice-repaired

commit 769b4155e47eca3b504b24a8138a28884bc0cac0

Related fdo#51252: No more prereg, no more unopkg sync
Now that 604a8fa01aeb6ffd880b2a32757646af23065145 "fdo#51252 Disable copying
share/prereg/bundled to avoid startup crashes" removed the use of share/prereg,
there is no longer need to generate it in the first place (by calling "unopkg
sync" at build or installation time), and so no need for the "unopkg sync" sub-
command, either.  This also allows to simplify some of the jvmfwk code that was
only there so that "unopkg sync" (which can require a JVM) can work in "hostile"
environments (during build and installation).

Change-Id: I52657384f4561bf27948ba4f0f88f4498e90987f

solenv/bin/modules/installer/simplepackage.pm
@@ -60,119 +60,6 @@ sub check_simple_packager_project
     }
 }
 
-####################################################
-# Detecting the directory with extensions
-####################################################
-
-sub get_extensions_dir
-{
-    my ( $subfolderdir ) = @_;
-
-    my $extensiondir = $subfolderdir . $installer::globals::separator;
-    if ( $installer::globals::officedirhostname ne "" ) { $extensiondir = $extensiondir . $installer::globals::officedirhostname . $installer::globals::separator; }
-    my $extensionsdir = $extensiondir . "share" . $installer::globals::separator . "extensions";
-    my $preregdir = $extensiondir . "share" . $installer::globals::separator . "prereg" . $installer::globals::separator . "bundled";
-
-    return ( $extensionsdir, $preregdir );
-}
-
-####################################################
-# Registering extensions
-####################################################
-
-sub register_extensions
-{
-    my ($officedir, $languagestringref, $preregdir) = @_;
-
-    my $infoline = "";
-
-    if ( $preregdir eq "" )
-    {
-        $infoline = "ERROR: Failed to determine directory \"prereg\" for extension registration! Please check your installation set.\n";
-        push( @installer::globals::logfileinfo, $infoline);
-        installer::exiter::exit_program($infoline, "register_extensions");
-    }
-
-    my $programdir = $officedir . $installer::globals::separator;
-    if ( $installer::globals::officedirhostname ne "" ) { $programdir = $programdir . $installer::globals::officedirhostname . $installer::globals::separator; }
-    $programdir = $programdir . "program";
-
-    my $from = cwd();
-    chdir($programdir);
-
-    my $unopkgfile = $installer::globals::unopkgfile;
-
-    my $unopkgexists = 1;
-    if (( $installer::globals::languagepack ) && ( ! -f $unopkgfile ))
-    {
-        $unopkgexists = 0;
-        $infoline = "Language packs do not contain unopkg!\n";
-        push( @installer::globals::logfileinfo, $infoline);
-    }
-
-    if (( $installer::globals::helppack ) && ( ! -f $unopkgfile ))
-    {
-        $unopkgexists = 0;
-        $infoline = "Help packs do not contain unopkg!\n";
-        push( @installer::globals::logfileinfo, $infoline);
-    }
-
-    if ( ! -f $unopkgfile )
-    {
-        $unopkgexists = 0;
-        $infoline = "Info: File $unopkgfile does not exist! Extensions cannot be registered.\n";
-        push( @installer::globals::logfileinfo, $infoline);
-    }
-
-    if ( $unopkgexists )
-    {
-        my $currentdir = cwd();
-        print "... current dir: $currentdir ...\n";
-        $infoline = "Current dir: $currentdir\n";
-        push( @installer::globals::logfileinfo, $infoline);
-
-        if ( ! -f $unopkgfile ) { installer::exiter::exit_program("ERROR: $unopkgfile not found!", "register_extensions"); }
-
-        my $systemcall = "JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1 " . $unopkgfile . " sync --verbose" . " -env:UNO_JAVA_JFW_ENV_JREHOME=true 2\>\&1 |";
-
-        print "... $systemcall ...\n";
-
-        $infoline = "Systemcall: $systemcall\n";
-        push( @installer::globals::logfileinfo, $infoline);
-
-        my @unopkgoutput = ();
-
-        open (UNOPKG, $systemcall);
-        while (<UNOPKG>)
-        {
-            my $lastline = $_;
-            push(@unopkgoutput, $lastline);
-        }
-        close (UNOPKG);
-
-        my $returnvalue = $?;   # $? contains the return value of the systemcall
-
-        if ($returnvalue)
-        {
-            # Writing content of @unopkgoutput only in the error case into the log file. Sometimes it
-            # contains strings like "Error" even in the case of success. This causes a packaging error
-            # when the log file is analyzed at the end, even if there is no real error.
-            for ( my $j = 0; $j <= $#unopkgoutput; $j++ ) { push( @installer::globals::logfileinfo, "$unopkgoutput[$j]"); }
-
-            $infoline = "ERROR: Could not execute \"$systemcall\"!\nExitcode: '$returnvalue'\n";
-            push( @installer::globals::logfileinfo, $infoline);
-            installer::exiter::exit_program("ERROR: $systemcall failed!", "register_extensions");
-        }
-        else
-        {
-            $infoline = "Success: Executed \"$systemcall\" successfully!\n";
-            push( @installer::globals::logfileinfo, $infoline);
-        }
-    }
-
-    chdir($from);
-}
-
 ########################################################################
 # Getting the translation file for the Mac Language Pack installer
 ########################################################################
@@ -783,15 +670,6 @@ sub create_simple_package
 
     installer::logger::print_message( "... removing superfluous directories ...\n" );
     installer::logger::include_header_into_logfile("Removing superfluous directories:");
-
-    my ( $extensionfolder, $preregdir ) = get_extensions_dir($subfolderdir);
-    installer::systemactions::remove_empty_dirs_in_folder($extensionfolder);
-
-    # Registering the extensions
-
-    installer::logger::print_message( "... registering extensions ...\n" );
-    installer::logger::include_header_into_logfile("Registering extensions:");
-    register_extensions($subfolderdir, $languagestringref, $preregdir);
 
     if ( $installer::globals::compiler =~ /^unxmacx/ )
     {

solenv/bin/modules/installer/systemactions.pm
@@ -1578,51 +1578,4 @@ sub read_full_directory {
     return
 }
 
-##############################################################
-# Removing all empty directories below a specified directory
-##############################################################
-
-sub remove_empty_dirs_in_folder
-{
-    my ( $dir ) = @_;
-
-    my @content = ();
-    my $infoline = "";
-
-    $dir =~ s/\Q$installer::globals::separator\E\s*$//;
-
-    if ( -d $dir )
-    {
-        opendir(DIR, $dir);
-        @content = readdir(DIR);
-        closedir(DIR);
-
-        my $oneitem;
-
-        foreach $oneitem (@content)
-        {
-            if ((!($oneitem eq ".")) && (!($oneitem eq "..")))
-            {
-                my $item = $dir . $installer::globals::separator . $oneitem;
-
-                if ( -d $item ) # recursive
-                {
-                    remove_empty_dirs_in_folder($item);
-                }
-            }
-        }
-
-        # try to remove empty directory
-        my $returnvalue = rmdir $dir;
-
-        if ( $returnvalue )
-        {
-            $infoline = "Successfully removed empty dir $dir\n";
-            push(@installer::globals::logfileinfo, $infoline);
-        }
-
-    }
-
-}
-
 1;

android/qa/desktop/Makefile
@@ -37,7 +37,6 @@ buildrcs:
 	echo 'URE_MORE_TYPES=file:///assets/ure/share/misc/types.rdb file:///assets/program/types/types.rdb' >> assets/program/fundamentalrc
 	echo 'URE_MORE_SERVICES= <$$BRAND_BASE_DIR/program/services>*' >> assets/program/fundamentalrc
 	echo 'BUNDLED_EXTENSIONS=$$BRAND_BASE_DIR/share/extensions' >> assets/program/fundamentalrc
-	echo 'BUNDLED_EXTENSIONS_PREREG=$$BRAND_BASE_DIR/share/prereg/bundled' >> assets/program/fundamentalrc
 	echo 'BUNDLED_EXTENSIONS_USER=$${$$BRAND_BASE_DIR/program/bootstraprc:UserInstallation}/user/extensions/bundled' >> assets/program/fundamentalrc
 	echo 'TMP_EXTENSIONS=$${$$BRAND_BASE_DIR/program/bootstraprc:UserInstallation}/user/extensions/tmp' >> assets/program/fundamentalrc
 	echo 'SHARED_EXTENSIONS_USER=$${$$BRAND_BASE_DIR/program/bootstraprc:UserInstallation}/user/extensions/shared' >> assets/program/fundamentalrc

bin/distro-install-file-lists
@@ -554,7 +554,6 @@ if test "z$RPM_CONFIG_FILE_TAGS" != "z" ; then
 	    -e "s|^($INSTALLDIR/program/[a-zA-Z0-9_\.]*rc)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \
 	    -e "s|^($INSTALLDIR/program/.*\.xsl)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \
 	    -e "s|^($INSTALLDIR/share/config/[a-zA-Z0-9]*rc)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \
-	    -e "s|^($INSTALLDIR/share/config/javasettingsunopkginstall.xml)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \
 	    -e "s|^($INSTALLDIR/share/dict/ooo/.*\.lst)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \
 	    -e "s|^($INSTALLDIR/share/psprint/.*\.conf)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \
 	    -e "s|^($INSTALLDIR/share/registry/.*\.xcu)\$|$RPM_CONFIG_FILE_TAGS \\1|;" \

desktop/scripts/unopkg.sh
@@ -64,27 +64,18 @@ esac
 
 #collect all bootstrap variables specified on the command line
 #so that they can be passed as arguments to javaldx later on
-#Recognize the "sync" option. sync must be applied without any other
-#options except bootstrap variables or the verbose option
 for arg in $@
 do
   case "$arg" in
        -env:*) BOOTSTRAPVARS=$BOOTSTRAPVARS" ""$arg";;
-       sync) OPTSYNC=true;;
        -v) VERBOSE=true;;
        --verbose) VERBOSE=true;;
-       *) OPTOTHER=$arg;;
   esac
 done
 
-if [ "$OPTSYNC" = "true" ] && [ -z "$OPTOTHER" ]
-then
-    JVMFWKPARAMS='-env:UNO_JAVA_JFW_INSTALL_DATA=$BRAND_BASE_DIR/share/config/javasettingsunopkginstall.xml -env:JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1'
-fi
-
 # extend the ld_library_path for java: javaldx checks the sofficerc for us
 if [ -x "$sd_prog/../ure-link/bin/javaldx" ] ; then
-    my_path=`"$sd_prog/../ure-link/bin/javaldx" $BOOTSTRAPVARS $JVMFWKPARAMS \
+    my_path=`"$sd_prog/../ure-link/bin/javaldx" $BOOTSTRAPVARS \
         "-env:INIFILENAME=vnd.sun.star.pathname:$sd_prog/redirectrc"`
     if [ -n "$my_path" ] ; then
         sd_platform=`uname -s`
@@ -110,6 +101,6 @@ unset XENVIRONMENT
 # SAL_NO_XINITTHREADS=true; export SAL_NO_XINITTHREADS
 
 # execute binary
-exec "$sd_prog/unopkg.bin" "$@" $JVMFWKPARAMS \
+exec "$sd_prog/unopkg.bin" "$@" \
     "-env:INIFILENAME=vnd.sun.star.pathname:$sd_prog/redirectrc"
 

desktop/source/app/app.cxx
@@ -148,10 +148,6 @@ namespace desktop
 static oslSignalHandler pSignalHandler = 0;
 static sal_Bool _bCrashReporterEnabled = sal_True;
 
-#ifndef ANDROID
-static ::rtl::OUString getBrandSharePreregBundledPathURL();
-#endif
-
 namespace {
 
 void removeTree(OUString const & url) {
@@ -513,246 +509,6 @@ rtl::OUString ReplaceStringHookProc( const rtl::OUString& rStr )
     return sRet;
 }
 
-static const char      pLastSyncFileName[]     = "lastsynchronized";
-static const sal_Int32 nStrLenLastSync         = 16;
-
-#ifndef ANDROID
-
-static bool needsSynchronization(
-    ::rtl::OUString const & baseSynchronizedURL, ::rtl::OUString const & userSynchronizedURL )
-{
-    bool bNeedsSync( false );
-
-    ::osl::DirectoryItem itemUserFile;
-    ::osl::File::RC err1 =
-          ::osl::DirectoryItem::get(userSynchronizedURL, itemUserFile);
-
-    //If it does not exist, then there is nothing to be done
-    if (err1 == ::osl::File::E_NOENT)
-    {
-        return true;
-    }
-    else if (err1 != ::osl::File::E_None)
-    {
-        OSL_FAIL("Cannot access lastsynchronized in user layer");
-        return true; //sync just in case
-    }
-
-    //If last synchronized does not exist in base layer, then do nothing
-    ::osl::DirectoryItem itemBaseFile;
-    ::osl::File::RC err2 = ::osl::DirectoryItem::get(baseSynchronizedURL, itemBaseFile);
-    if (err2 == ::osl::File::E_NOENT)
-    {
-        return true;
-
-    }
-    else if (err2 != ::osl::File::E_None)
-    {
-        OSL_FAIL("Cannot access file lastsynchronized in base layer");
-        return true; //sync just in case
-    }
-
-    //compare the modification time of the extension folder and the last
-    //modified file
-    ::osl::FileStatus statUser(osl_FileStatus_Mask_ModifyTime);
-    ::osl::FileStatus statBase(osl_FileStatus_Mask_ModifyTime);
-    if (itemUserFile.getFileStatus(statUser) == ::osl::File::E_None)
-    {
-        if (itemBaseFile.getFileStatus(statBase) == ::osl::File::E_None)
-        {
-            TimeValue timeUser = statUser.getModifyTime();
-            TimeValue timeBase = statBase.getModifyTime();
-
-            if (timeUser.Seconds < timeBase.Seconds)
-                bNeedsSync = true;
-        }
-        else
-        {
-            OSL_ASSERT(0);
-            bNeedsSync = true;
-        }
-    }
-    else
-    {
-        OSL_ASSERT(0);
-        bNeedsSync = true;
-    }
-
-    return bNeedsSync;
-}
-
-static ::rtl::OUString getBrandSharePreregBundledPathURL()
-{
-    ::rtl::OUString url(
-        "$BRAND_BASE_DIR/share/prereg/bundled");
-
-    ::rtl::Bootstrap::expandMacros(url);
-    return url;
-}
-
-static ::rtl::OUString getUserBundledExtPathURL()
-{
-    ::rtl::OUString folder( "$BUNDLED_EXTENSIONS_USER" );
-    ::rtl::Bootstrap::expandMacros(folder);
-
-    return folder;
-}
-
-static ::rtl::OUString getLastSyncFileURLFromBrandInstallation()
-{
-    ::rtl::OUString aURL = getBrandSharePreregBundledPathURL();
-    ::sal_Int32    nLastIndex         = aURL.lastIndexOf('/');
-
-    ::rtl::OUStringBuffer aTmp( aURL );
-
-    if ( nLastIndex != aURL.getLength()-1 )
-        aTmp.appendAscii( "/" );
-    aTmp.appendAscii( pLastSyncFileName );
-
-    return aTmp.makeStringAndClear();
-}
-
-static ::rtl::OUString getLastSyncFileURLFromUserInstallation()
-{
-    ::rtl::OUString aUserBundledPathURL = getUserBundledExtPathURL();
-    ::sal_Int32    nLastIndex          = aUserBundledPathURL.lastIndexOf('/');
-
-    ::rtl::OUStringBuffer aTmp( aUserBundledPathURL );
-
-    if ( nLastIndex != aUserBundledPathURL.getLength()-1 )
-        aTmp.appendAscii( "/" );
-    aTmp.appendAscii( pLastSyncFileName );
-
-    return aTmp.makeStringAndClear();
-}
-
-#endif
-
-//Checks if the argument src is the folder of the help or configuration
-//backend in the prereg folder
-static bool excludeTmpFilesAndFolders(const rtl::OUString & src)
-{
-    const char helpBackend[] = "com.sun.star.comp.deployment.help.PackageRegistryBackend";
-    const char configBackend[] = "com.sun.star.comp.deployment.configuration.PackageRegistryBackend";
-    if (src.endsWithAsciiL(helpBackend, sizeof(helpBackend) - 1 )
-        || src.endsWithAsciiL(configBackend, sizeof(configBackend) - 1))
-    {
-        return true;
-    }
-    return false;
-}
-
-//If we are about to copy the contents of some special folder as determined
-//by excludeTmpFilesAndFolders, then we omit those files or folders with a name
-//derived from temporary folders.
-static bool isExcludedFileOrFolder( const rtl::OUString & name)
-{
-    char const * allowed[] = {
-        "backenddb.xml",
-        "configmgr.ini",
-        "registered_packages.db"
-    };
-
-    const unsigned int size = sizeof(allowed) / sizeof (char const *);
-    bool bExclude = true;
-    for (unsigned int i= 0; i < size; i ++)
-    {
-        ::rtl::OUString allowedName = ::rtl::OUString::createFromAscii(allowed[i]);
-        if (allowedName.equals(name))
-        {
-            bExclude = false;
-            break;
-        }
-    }
-    return bExclude;
-}
-
-static osl::FileBase::RC copy_bundled_recursive(
-    const rtl::OUString& srcUnqPath,
-    const rtl::OUString& dstUnqPath,
-    sal_Int32            TypeToCopy )
-throw()
-{
-    osl::FileBase::RC err = osl::FileBase::E_None;
-
-    if( TypeToCopy == -1 ) // Document
-    {
-        err = osl::File::copy( srcUnqPath,dstUnqPath );
-    }
-    else if( TypeToCopy == +1 ) // Folder
-    {
-        err = osl::Directory::create( dstUnqPath );
-        osl::FileBase::RC next = err;
-        if( err == osl::FileBase::E_None || err == osl::FileBase::E_EXIST )
-        {
-            err = osl::FileBase::E_None;
-
-            osl::Directory aDir( srcUnqPath );
-            bool bExcludeFiles = excludeTmpFilesAndFolders(srcUnqPath);
-            if (aDir.open() == osl::FileBase::E_None)
-            {
-                sal_Int32 n_Mask = osl_FileStatus_Mask_FileURL |
-                                   osl_FileStatus_Mask_FileName |
-                                   osl_FileStatus_Mask_Type;
-
-                osl::DirectoryItem aDirItem;
-                while( err == osl::FileBase::E_None && ( next = aDir.getNextItem( aDirItem ) ) == osl::FileBase::E_None )
-                {
-                    sal_Bool IsDoc = false;
-                    sal_Bool bFilter = false;
-                    osl::FileStatus aFileStatus( n_Mask );
-                    aDirItem.getFileStatus( aFileStatus );
-                    if( aFileStatus.isValid( osl_FileStatus_Mask_Type ) )
-                        IsDoc = aFileStatus.getFileType() == osl::FileStatus::Regular;
-
-                    // Getting the information for the next recursive copy
-                    sal_Int32 newTypeToCopy = IsDoc ? -1 : +1;
-
-                    rtl::OUString newSrcUnqPath;
-                    if( aFileStatus.isValid( osl_FileStatus_Mask_FileURL ) )
-                        newSrcUnqPath = aFileStatus.getFileURL();
-
-                    rtl::OUString newDstUnqPath = dstUnqPath;
-                    rtl::OUString tit;
-                    if( aFileStatus.isValid( osl_FileStatus_Mask_FileName ) )
-                    {
-                        ::rtl::OUString aFileName = aFileStatus.getFileName();
-                        tit = rtl::Uri::encode( aFileName,
-                                                rtl_UriCharClassPchar,
-                                                rtl_UriEncodeIgnoreEscapes,
-                                                RTL_TEXTENCODING_UTF8 );
-
-                        // Special treatment for "lastsychronized" file. Must not be
-                        // copied from the bundled folder!
-                        //Also do not copy *.tmp files and *.tmp_ folders. This affects the files/folders
-                        //from the help and configuration backend
-                        if ( IsDoc && (aFileName.equalsAscii( pLastSyncFileName )
-                                       || (bExcludeFiles && isExcludedFileOrFolder(aFileName))))
-                            bFilter = true;
-                        else if (!IsDoc && bExcludeFiles && isExcludedFileOrFolder(aFileName))
-                            bFilter = true;
-                    }
-
-                    if( newDstUnqPath.lastIndexOf( sal_Unicode('/') ) != newDstUnqPath.getLength()-1 )
-                        newDstUnqPath += rtl::OUString("/");
-
-                    newDstUnqPath += tit;
-
-                    if (( newSrcUnqPath != dstUnqPath ) && !bFilter )
-                        err = copy_bundled_recursive( newSrcUnqPath,newDstUnqPath, newTypeToCopy );
-                }
-
-                if( err == osl::FileBase::E_None && next != osl::FileBase::E_NOENT )
-                    err = next;
-
-                aDir.close();
-            }
-        }
-    }
-
-    return err;
-}
-
 Desktop::Desktop()
 : m_bServicesRegistered( false )
 , m_aBootstrapError( BE_OK )
@@ -773,26 +529,6 @@ void Desktop::Init()
     SetBootstrapStatus(BS_OK);
 
     refreshBundledExtensionsDir();
-
-    // Check for lastsynchronized file for bundled extensions in the user directory
-    // and test if synchronzation is necessary!
-#ifndef ANDROID
-    {
-        ::rtl::OUString aUserLastSyncFilePathURL = getLastSyncFileURLFromUserInstallation();
-        ::rtl::OUString aPreregSyncFilePathURL = getLastSyncFileURLFromBrandInstallation();
-
-        if ( needsSynchronization( aPreregSyncFilePathURL, aUserLastSyncFilePathURL ))
-        {
-            rtl::OUString aUserPath = getUserBundledExtPathURL();
-            rtl::OUString aPreregBundledPath = getBrandSharePreregBundledPathURL();
-
-            // copy bundled folder to the user directory
-            osl::FileBase::RC rc = osl::Directory::createPath(aUserPath);
-            (void) rc;
-            if (false) copy_bundled_recursive( aPreregBundledPath, aUserPath, +1 );
-        }
-    }
-#endif
 
     // We need to have service factory before going further, but see fdo#37195.
     // Doing this will mmap common.rdb, making it not overwritable on windows,

desktop/source/deployment/manager/dp_extensionmanager.cxx
@@ -1258,64 +1258,6 @@ void ExtensionManager::reinstallDeployedExtensions(
     }
 }
 
-/** Works on the bundled repository. That is using the variables
-    BUNDLED_EXTENSIONS and BUNDLED_EXTENSIONS_USER.
- */
-void ExtensionManager::synchronizeBundledPrereg(
-    Reference<task::XAbortChannel> const & xAbortChannel,
-    Reference<ucb::XCommandEnvironment> const & xCmdEnv )
-    throw (deploy::DeploymentException,
-           uno::RuntimeException)
-{
-    try
-    {
-        String sSynchronizingBundled(StrSyncRepository::get());
-        sSynchronizingBundled.SearchAndReplaceAllAscii( "%NAME", OUSTR("bundled"));
-        dp_misc::ProgressLevel progressBundled(xCmdEnv, sSynchronizingBundled);
-
-        Reference<deploy::XPackageManagerFactory> xPackageManagerFactory(
-            deploy::thePackageManagerFactory::get(m_xContext));
-
-        Reference<deploy::XPackageManager> xMgr =
-            xPackageManagerFactory->getPackageManager(OUSTR("bundled_prereg"));
-        xMgr->synchronize(xAbortChannel, xCmdEnv);
-        progressBundled.update(OUSTR("\n\n"));
-
-        uno::Sequence<Reference<deploy::XPackage> > extensions = xMgr->getDeployedPackages(
-            xAbortChannel, xCmdEnv);
-        try
-        {
-            for (sal_Int32 i = 0; i < extensions.getLength(); i++)
-            {
-                extensions[i]->registerPackage(true, xAbortChannel, xCmdEnv);
-            }
-        }
-        catch (...)
-        {
-            OSL_ASSERT(0);
-        }
-        OUString lastSyncBundled(RTL_CONSTASCII_USTRINGPARAM(
-                                     "$BUNDLED_EXTENSIONS_PREREG/lastsynchronized"));
-        writeLastModified(lastSyncBundled, xCmdEnv);
-
-    } catch ( const deploy::DeploymentException& ) {
-        throw;
-    } catch ( const ucb::CommandFailedException & ) {
-        throw;
-    } catch ( const ucb::CommandAbortedException & ) {
-        throw;
-    } catch (const lang::IllegalArgumentException &) {
-        throw;
-    } catch (const uno::RuntimeException &) {
-        throw;
-    } catch (...) {
-        uno::Any exc = ::cppu::getCaughtException();
-        throw deploy::DeploymentException(
-            OUSTR("Extension Manager: exception in synchronize"),
-            static_cast<OWeakObject*>(this), exc);
-    }
-}
-
 sal_Bool ExtensionManager::synchronize(
     Reference<task::XAbortChannel> const & xAbortChannel,
     Reference<ucb::XCommandEnvironment> const & xCmdEnv )
@@ -1342,14 +1284,15 @@ sal_Bool ExtensionManager::synchronize(
         bModified |= getBundledRepository()->synchronize(xAbortChannel, xCmdEnv);
         progressBundled.update(OUSTR("\n\n"));
 
-        //Always determine the active extension. This is necessary for the
-        //first-start optimization. The setup creates the registration data for the
-        //bundled extensions (brand_layer/share/prereg/bundled), which is copied to the user
-        //installation (user_installation/extension/bundled) when a user starts OOo
-        //for the first time after running setup. All bundled extensions are registered
-        //at that moment. However, extensions with the same identifier can be in the
-        //shared or user repository, in which case the respective bundled extensions must
-        //be revoked.
+        //Always determine the active extension.
+        //TODO: Is this still necessary?  (It used to be necessary for the
+        // first-start optimization:  The setup created the registration data
+        // for the bundled extensions (share/prereg/bundled) which was copied to
+        // the user installation when a user started OOo for the first time
+        // after running setup.  All bundled extensions were registered at that
+        // moment.  However, extensions with the same identifier could be in the
+        // shared or user repository, in which case the respective bundled
+        // extensions had to be revoked.)
         try
         {
             const uno::Sequence<uno::Sequence<Reference<deploy::XPackage> > >

desktop/source/deployment/manager/dp_extensionmanager.hxx
@@ -201,12 +201,6 @@ public:
                css::lang::IllegalArgumentException,
                css::uno::RuntimeException);
 
-    virtual void SAL_CALL synchronizeBundledPrereg(
-        css::uno::Reference<css::task::XAbortChannel> const & xAbortChannel,
-        css::uno::Reference<css::ucb::XCommandEnvironment> const & xCmdEnv )
-        throw (css::deployment::DeploymentException,
-               css::uno::RuntimeException);
-
     virtual css::uno::Sequence<css::uno::Reference<css::deployment::XPackage> > SAL_CALL
     getExtensionsWithUnacceptedLicenses(
         ::rtl::OUString const & repository,

commit 698ffb56b5ca0ad4c3830e0419013965fcc17c1c

installer: remove empty directories again:
An "archive" installer contains one empty share/extensions/dict-XY/
directory for every dictionary (except the 3 default ones), causing
numerous assertions about missing META-INF/manifest.xml and an extension
manager cluttered; this is due to overzealous removal of extension
"prereg" code.  (regression from
769b4155e47eca3b504b24a8138a28884bc0cac0, partially revert that)

Change-Id: I3a063ebfed1012aeb27ec3076cdd5ca545f918a2

solenv/bin/modules/installer/simplepackage.pm
@@ -60,6 +60,22 @@ sub check_simple_packager_project
     }
 }
 
+####################################################
+# Detecting the directory with extensions
+####################################################
+
+sub get_extensions_dir
+{
+    my ( $subfolderdir ) = @_;
+
+    my $extensiondir = $subfolderdir . $installer::globals::separator;
+    if ( $installer::globals::officedirhostname ne "" ) { $extensiondir = $extensiondir . $installer::globals::officedirhostname . $installer::globals::separator; }
+    my $extensionsdir = $extensiondir . "share" . $installer::globals::separator . "extensions";
+    my $preregdir = $extensiondir . "share" . $installer::globals::separator . "prereg" . $installer::globals::separator . "bundled";
+
+    return ( $extensionsdir, $preregdir );
+}
+
 ##################################################################
 # Collecting all identifier from ulf file
 ##################################################################
@@ -654,6 +670,9 @@ sub create_simple_package
 
     installer::logger::print_message( "... removing superfluous directories ...\n" );
     installer::logger::include_header_into_logfile("Removing superfluous directories:");
+
+    my ( $extensionfolder, $preregdir ) = get_extensions_dir($subfolderdir);
+    installer::systemactions::remove_empty_dirs_in_folder($extensionfolder);
 
     if ( $installer::globals::compiler =~ /^unxmacx/ )
     {

solenv/bin/modules/installer/systemactions.pm
@@ -1355,4 +1355,51 @@ sub read_full_directory {
     return
 }
 
+##############################################################
+# Removing all empty directories below a specified directory
+##############################################################
+
+sub remove_empty_dirs_in_folder
+{
+    my ( $dir ) = @_;
+
+    my @content = ();
+    my $infoline = "";
+
+    $dir =~ s/\Q$installer::globals::separator\E\s*$//;
+
+    if ( -d $dir )
+    {
+        opendir(DIR, $dir);
+        @content = readdir(DIR);
+        closedir(DIR);
+
+        my $oneitem;
+
+        foreach $oneitem (@content)
+        {
+            if ((!($oneitem eq ".")) && (!($oneitem eq "..")))
+            {
+                my $item = $dir . $installer::globals::separator . $oneitem;
+
+                if ( -d $item ) # recursive
+                {
+                    remove_empty_dirs_in_folder($item);
+                }
+            }
+        }
+
+        # try to remove empty directory
+        my $returnvalue = rmdir $dir;
+
+        if ( $returnvalue )
+        {
+            $infoline = "Successfully removed empty dir $dir\n";
+            push(@installer::globals::logfileinfo, $infoline);
+        }
+
+    }
+
+}
+
 1;