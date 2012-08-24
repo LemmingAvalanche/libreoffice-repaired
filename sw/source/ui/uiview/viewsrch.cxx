LibreOffice/core

commit cda75d6f358d438f7ed36ed7fa296ac23e3c4a04

Fix for i#8288: "Replace all" should leave the cursor at original position

sw/source/ui/uiview/viewsrch.cxx
@@ -286,6 +286,10 @@ void SwView::ExecSearch(SfxRequest& rReq, BOOL bNoMessage)
                 {
                     SwSearchOptions aOpts( pWrtShell, pSrchItem->GetBackward() );
 
+                    // Fix for i#8288: "Replace all" should leave the cursor at the place it was
+                    // before executing the command, rather than at the site of the final replacement.
+                    // To do this take note of the current cursor position before replace all begins:
+                    SwPosition TmpPointPos = *pWrtShell->GetSwCrsr()->GetPoint();
 
                     if( !pSrchItem->GetSelection() )
                     {
@@ -305,8 +309,13 @@ void SwView::ExecSearch(SfxRequest& rReq, BOOL bNoMessage)
                         SwWait aWait( *GetDocShell(), TRUE );
                         pWrtShell->StartAllAction();
                         nFound = FUNC_Search( aOpts );
+
+                        // i#8288: Now that everything has been replaced, restore the original cursor position.
+                        *(pWrtShell->GetSwCrsr()->GetPoint()) = TmpPointPos;
+
                         pWrtShell->EndAllAction();
                     }
+
                     rReq.SetReturnValue(SfxBoolItem(nSlot, nFound != 0 && ULONG_MAX != nFound));
                     if( !nFound )
                     {

LemmingAvalanche/libreoffice-repaired

commit 705c4f821c7600f3ba4ec7d2532784228dec8e3c

fdo#49901: SwView::ExecSearch: fix Replace All cursor:
Because the "fix" for i#8288 only set the point of the cursor and not
the mark, it can happen that after a Replace All weird things are
selected; fix that (and i#8288 properly) by using the SwCrsrShell stack,
and don't do it when searching in the selection.
This cannot be done in SwCrsrShell itself because ExecSearch sets the
cursor to the beginning/end of the document already.
(regression from cda75d6f358d438f7ed36ed7fa296ac23e3c4a04)

Change-Id: Ie9f8d7da2fa26000a0237c24f8742e47ffa29213

sw/source/ui/uiview/viewsrch.cxx
@@ -282,36 +282,36 @@ void SwView::ExecSearch(SfxRequest& rReq, sal_Bool bNoMessage)
             case SVX_SEARCHCMD_REPLACE_ALL:
                 {
                     SwSearchOptions aOpts( pWrtShell, pSrchItem->GetBackward() );
-
-                    // Fix for i#8288: "Replace all" should leave the cursor at the place it was
-                    // before executing the command, rather than at the site of the final replacement.
-                    // To do this take note of the current cursor position before replace all begins:
-                    // note: must be stored so that it is corrected by PamCorr*
-                    ::boost::scoped_ptr<SwUnoCrsr> const pTmpCursor(
-                            pWrtShell->GetDoc()->CreateUnoCrsr(
-                                *pWrtShell->GetSwCrsr()->GetPoint()));
-
-                    if( !pSrchItem->GetSelection() )
-                    {
-                        // bestehende Selektionen aufheben,
-                        // wenn nicht in selektierten Bereichen gesucht werden soll
-                        pWrtShell->KillSelection(0, false);
-                        if( DOCPOS_START == aOpts.eEnd )
-                            pWrtShell->EndDoc();
-                        else
-                            pWrtShell->SttDoc();
-                    }
-
                     bExtra = sal_False;
                     sal_uLong nFound;
 
                     {   //Scope for SwWait-Object
                         SwWait aWait( *GetDocShell(), sal_True );
                         pWrtShell->StartAllAction();
+                        if (!pSrchItem->GetSelection())
+                        {
+                            // if we don't want to search in the selection...
+                            pWrtShell->KillSelection(0, false);
+                            // i#8288 "replace all" should not change cursor
+                            // position, so save current cursor
+                            pWrtShell->Push();
+                            if (DOCPOS_START == aOpts.eEnd)
+                            {
+                                pWrtShell->EndDoc();
+                            }
+                            else
+                            {
+                                pWrtShell->SttDoc();
+                            }
+                        }
                         nFound = FUNC_Search( aOpts );
-                        // #i8288# Now that everything has been replaced, restore the original cursor position.
-                        *(pWrtShell->GetSwCrsr()->GetPoint()) =
-                            *pTmpCursor->GetPoint();
+                        if (!pSrchItem->GetSelection())
+                        {
+                            // create it just to overwrite it with stack cursor
+                            pWrtShell->CreateCrsr();
+                            // i#8288 restore the original cursor position
+                            pWrtShell->Pop(false);
+                        }
                         pWrtShell->EndAllAction();
                     }
 