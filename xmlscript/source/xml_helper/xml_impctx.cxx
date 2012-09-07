LemmingAvalanche/libreoffice-repaired

commit a72d1262231bf5d754c62c08daf23c4838753148

kill RTL_CONSTASCII_USTRINGPARAM in xmlscript
Plus rtl::OUString cleanup
Only on rebased files

Change-Id: Icddaa20742cc45f08e5a48790447fcf8865f4bd6
Reviewed-on: https://gerrit.libreoffice.org/527
Reviewed-by: Olivier Hallot <olivier.hallot@alta.org.br>
Tested-by: Olivier Hallot <olivier.hallot@alta.org.br>

xmlscript/source/xml_helper/xml_impctx.cxx
@@ -45,15 +45,13 @@ const sal_Int32 UID_UNKNOWN = -1;
 
 Sequence< OUString > getSupportedServiceNames_DocumentHandlerImpl()
 {
-    OUString name( RTL_CONSTASCII_USTRINGPARAM(
-                       "com.sun.star.xml.input.SaxDocumentHandler") );
+    OUString name( "com.sun.star.xml.input.SaxDocumentHandler" );
     return Sequence< OUString >( &name, 1 );
 }
 
 OUString getImplementationName_DocumentHandlerImpl()
 {
-    return OUString( RTL_CONSTASCII_USTRINGPARAM(
-                         "com.sun.star.comp.xml.input.SaxDocumentHandler") );
+    return OUString( "com.sun.star.comp.xml.input.SaxDocumentHandler" );
 }
 
 typedef ::boost::unordered_map< OUString, sal_Int32, OUStringHash > t_OUString2LongMap;
@@ -191,14 +189,12 @@ DocumentHandlerImpl::DocumentHandlerImpl(
     bool bSingleThreadedUse )
     : m_xRoot( xRoot ),
       m_uid_count( 0 ),
-      m_sXMLNS_PREFIX_UNKNOWN(
-          RTL_CONSTASCII_USTRINGPARAM("<<< unknown prefix >>>") ),
-      m_sXMLNS( RTL_CONSTASCII_USTRINGPARAM("xmlns") ),
+      m_sXMLNS_PREFIX_UNKNOWN( "<<< unknown prefix >>>" ),
+      m_sXMLNS( "xmlns" ),
       m_nLastURI_lookup( UID_UNKNOWN ),
-      m_aLastURI_lookup( RTL_CONSTASCII_USTRINGPARAM("<<< unknown URI >>>") ),
+      m_aLastURI_lookup( "<<< unknown URI >>>" ),
       m_nLastPrefix_lookup( UID_UNKNOWN ),
-      m_aLastPrefix_lookup(
-          RTL_CONSTASCII_USTRINGPARAM("<<< unknown URI >>>") ),
+      m_aLastPrefix_lookup( "<<< unknown URI >>>" ),
       m_nSkipElements( 0 ),
       m_pMutex( 0 )
 {
@@ -473,10 +469,7 @@ void DocumentHandlerImpl::initialize(
     }
     else
     {
-        throw RuntimeException(
-            OUString( RTL_CONSTASCII_USTRINGPARAM(
-                          "missing root instance!") ),
-            Reference< XInterface >() );
+        throw RuntimeException( "missing root instance!", Reference< XInterface >() );
     }
 }
 
@@ -504,9 +497,7 @@ OUString DocumentHandlerImpl::getUriByUid( sal_Int32 Uid )
         if (iPos->second == Uid)
             return iPos->first;
     }
-    throw container::NoSuchElementException(
-        OUString( RTL_CONSTASCII_USTRINGPARAM("no such xmlns uid!") ),
-        static_cast< OWeakObject * >(this) );
+    throw container::NoSuchElementException( "no such xmlns uid!" , static_cast< OWeakObject * >(this) );
 }
 
 
@@ -516,8 +507,7 @@ OUString DocumentHandlerImpl::getUriByUid( sal_Int32 Uid )
 void DocumentHandlerImpl::startDocument()
     throw (xml::sax::SAXException, RuntimeException)
 {
-    m_xRoot->startDocument(
-        static_cast< xml::input::XNamespaceMapping * >( this ) );
+    m_xRoot->startDocument( static_cast< xml::input::XNamespaceMapping * >( this ) );
 }
 
 //______________________________________________________________________________
@@ -606,8 +596,7 @@ void DocumentHandlerImpl::startElement(
         {
             OUString const & rQAttributeName = pQNames[ nPos ];
             OSL_ENSURE(
-                rQAttributeName.compareToAscii(
-                    RTL_CONSTASCII_STRINGPARAM("xmlns:") ) != 0,
+                rQAttributeName.compareToAscii( "xmlns:" ) != 0,
                 "### unexpected xmlns!" );
 
             // collect attribute's uid and current prefix

xmlscript/inc/xmlscript/xml_helper.hxx
@@ -42,7 +42,7 @@ class XMLSCRIPT_DLLPUBLIC XMLElement
     : public ::cppu::WeakImplHelper1< ::com::sun::star::xml::sax::XAttributeList >
 {
 public:
-    inline XMLElement( ::rtl::OUString const & name )
+    inline XMLElement( OUString const & name )
         SAL_THROW(())
         : _name( name )
         {}
@@ -67,7 +67,7 @@ public:
         @param rAttrName qname of attribute
         @param rValue value string of element
     */
-    void SAL_CALL addAttribute( ::rtl::OUString const & rAttrName, ::rtl::OUString const & rValue )
+    void SAL_CALL addAttribute( OUString const & rAttrName, OUString const & rValue )
         SAL_THROW(());
 
     /** Gets the tag name (qname) of element.
@@ -75,7 +75,7 @@ public:
         @return
                 qname of element
     */
-    inline ::rtl::OUString SAL_CALL getName() SAL_THROW(())
+    inline OUString SAL_CALL getName() SAL_THROW(())
         { return _name; }
 
     /** Dumps out element (and all sub elements).
@@ -94,22 +94,22 @@ public:
     // XAttributeList
     virtual sal_Int16 SAL_CALL getLength()
         throw (::com::sun::star::uno::RuntimeException);
-    virtual ::rtl::OUString SAL_CALL getNameByIndex( sal_Int16 nPos )
+    virtual OUString SAL_CALL getNameByIndex( sal_Int16 nPos )
         throw (::com::sun::star::uno::RuntimeException);
-    virtual ::rtl::OUString SAL_CALL getTypeByIndex( sal_Int16 nPos )
+    virtual OUString SAL_CALL getTypeByIndex( sal_Int16 nPos )
         throw (::com::sun::star::uno::RuntimeException);
-    virtual ::rtl::OUString SAL_CALL getTypeByName( ::rtl::OUString const & rName )
+    virtual OUString SAL_CALL getTypeByName( OUString const & rName )
         throw (::com::sun::star::uno::RuntimeException);
-    virtual ::rtl::OUString SAL_CALL getValueByIndex( sal_Int16 nPos )
+    virtual OUString SAL_CALL getValueByIndex( sal_Int16 nPos )
         throw (::com::sun::star::uno::RuntimeException);
-    virtual ::rtl::OUString SAL_CALL getValueByName( ::rtl::OUString const & rName )
+    virtual OUString SAL_CALL getValueByName( OUString const & rName )
         throw (::com::sun::star::uno::RuntimeException);
 
 protected:
-    ::rtl::OUString _name;
+    OUString _name;
 
-    ::std::vector< ::rtl::OUString > _attrNames;
-    ::std::vector< ::rtl::OUString > _attrValues;
+    ::std::vector< OUString > _attrNames;
+    ::std::vector< OUString > _attrValues;
 
     ::std::vector< ::com::sun::star::uno::Reference<
                    ::com::sun::star::xml::sax::XAttributeList > > _subElems;

xmlscript/inc/xmlscript/xmllib_imexp.hxx
@@ -35,12 +35,12 @@ namespace xmlscript
 // functionality from xmlscript
 struct XMLSCRIPT_DLLPUBLIC LibDescriptor
 {
-    ::rtl::OUString aName;
-    ::rtl::OUString aStorageURL;
+    OUString aName;
+    OUString aStorageURL;
     sal_Bool bLink;
     sal_Bool bReadOnly;
     sal_Bool bPasswordProtected;
-    ::com::sun::star::uno::Sequence< ::rtl::OUString > aElementNames;
+    ::com::sun::star::uno::Sequence< OUString > aElementNames;
     sal_Bool bPreload;
 };
 

xmlscript/inc/xmlscript/xmlmod_imexp.hxx
@@ -34,10 +34,10 @@ namespace xmlscript
 // functionality from xmlscript
 struct XMLSCRIPT_DLLPUBLIC ModuleDescriptor
 {
-    ::rtl::OUString aName;
-    ::rtl::OUString aLanguage;
-    ::rtl::OUString aCode;
-    ::rtl::OUString aModuleType; // VBA
+    OUString aName;
+    OUString aLanguage;
+    OUString aCode;
+    OUString aModuleType; // VBA
 };
 
 XMLSCRIPT_DLLPUBLIC void

xmlscript/source/inc/misc.hxx
@@ -22,9 +22,6 @@
 
 #include "com/sun/star/uno/Any.hxx"
 
-#define OUSTR(x) ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM(x) )
-
-
 namespace xmlscript
 {
 
@@ -35,7 +32,7 @@ inline void extract_throw( T * p, ::com::sun::star::uno::Any const & a )
     if (! (a >>= *p))
     {
         throw ::com::sun::star::uno::RuntimeException(
-            OUSTR("expected ") + ::getCppuType( p ).getTypeName(),
+            "expected " + ::getCppuType( p ).getTypeName(),
             ::com::sun::star::uno::Reference<
             ::com::sun::star::uno::XInterface>() );
     }

xmlscript/source/xmldlg_imexp/exp_share.hxx
@@ -53,7 +53,7 @@ struct Style
     short _all;
     short _set;
 
-    ::rtl::OUString _id;
+    OUString _id;
 
     inline Style( short all_ ) SAL_THROW(())
         : _fontRelief( css::awt::FontRelief::NONE )
@@ -71,7 +71,7 @@ class StyleBag
 public:
     ~StyleBag() SAL_THROW(());
 
-    ::rtl::OUString getStyleId( Style const & rStyle ) SAL_THROW(());
+    OUString getStyleId( Style const & rStyle ) SAL_THROW(());
 
     void dump( css::uno::Reference< css::xml::sax::XExtendedDocumentHandler >
                const & xOut );
@@ -88,7 +88,7 @@ public:
     inline ElementDescriptor(
         css::uno::Reference< css::beans::XPropertySet > const & xProps,
         css::uno::Reference< css::beans::XPropertyState > const & xPropState,
-        ::rtl::OUString const & name, css::uno::Reference< css::frame::XModel > const & xDocument )
+        OUString const & name, css::uno::Reference< css::frame::XModel > const & xDocument )
         SAL_THROW(())
         : XMLElement( name )
         , _xProps( xProps )
@@ -96,65 +96,65 @@ public:
         , _xDocument( xDocument )
         {}
     inline ElementDescriptor(
-        ::rtl::OUString const & name )
+        OUString const & name )
         SAL_THROW(())
         : XMLElement( name )
         {}
 
     template<typename T>
     inline void read(
-        ::rtl::OUString const & propName, ::rtl::OUString const & attrName,
+        OUString const & propName, OUString const & attrName,
         bool forceAttribute = false );
 
     template<typename T>
-    inline bool readProp( T * ret, ::rtl::OUString const & rPropName );
-    css::uno::Any readProp( ::rtl::OUString const & rPropName );
+    inline bool readProp( T * ret, OUString const & rPropName );
+    css::uno::Any readProp( OUString const & rPropName );
     void readDefaults( bool supportPrintable = true, bool supportVisible = true );
     void readStringAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     inline void readDoubleAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName )
+        OUString const & rPropName, OUString const & rAttrName )
         { read<double>( rPropName, rAttrName ); }
     inline void readLongAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName,
+        OUString const & rPropName, OUString const & rAttrName,
         bool forceAttribute = false )
         { read<sal_Int32>( rPropName, rAttrName, forceAttribute ); }
     void readHexLongAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     inline void readShortAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName )
+        OUString const & rPropName, OUString const & rAttrName )
         { read<sal_Int32>( rPropName, rAttrName ); }
     inline void readBoolAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName )
+        OUString const & rPropName, OUString const & rAttrName )
         { read<sal_Bool>( rPropName, rAttrName ); }
 
     void readAlignAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readVerticalAlignAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readImageURLAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readImageAlignAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readImagePositionAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readDateFormatAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readTimeFormatAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readOrientationAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readButtonTypeAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readLineEndFormatAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readSelectionTypeAttr(
-        ::rtl::OUString const & rPropName, ::rtl::OUString const & rAttrName );
+        OUString const & rPropName, OUString const & rAttrName );
     void readDataAwareAttr(
-        ::rtl::OUString const & rAttrName );
+        OUString const & rAttrName );
     inline void addBoolAttr(
-        ::rtl::OUString const & rAttrName, sal_Bool bValue )
-        { addAttribute( rAttrName, ::rtl::OUString::valueOf(bValue) ); }
+        OUString const & rAttrName, sal_Bool bValue )
+        { addAttribute( rAttrName, OUString::valueOf(bValue) ); }
     void addNumberFormatAttr(
         css::uno::Reference< css::beans::XPropertySet >
         const & xFormatProperties );
@@ -218,7 +218,7 @@ public:
 
 template<typename T>
 inline void ElementDescriptor::read(
-    ::rtl::OUString const & propName, ::rtl::OUString const & attrName,
+    OUString const & propName, OUString const & attrName,
     bool forceAttribute )
 {
     if (forceAttribute ||
@@ -228,7 +228,7 @@ inline void ElementDescriptor::read(
         css::uno::Any a( _xProps->getPropertyValue( propName ) );
         T v = T();
         if (a >>= v)
-            addAttribute( attrName, ::rtl::OUString::valueOf(v) );
+            addAttribute( attrName, OUString::valueOf(v) );
         else
             OSL_FAIL( "### unexpected property type!" );
     }
@@ -236,7 +236,7 @@ inline void ElementDescriptor::read(
 
 template<typename T>
 inline bool ElementDescriptor::readProp(
-    T * ret, ::rtl::OUString const & rPropName )
+    T * ret, OUString const & rPropName )
 {
     _xProps->getPropertyValue( rPropName ) >>= *ret;
     return css::beans::PropertyState_DEFAULT_VALUE !=

xmlscript/source/xmldlg_imexp/xmldlg_addfunc.cxx
@@ -68,18 +68,16 @@ Reference< io::XInputStreamProvider > SAL_CALL exportDialogModel(
     Reference< lang::XMultiComponentFactory > xSMgr( xContext->getServiceManager() );
     if (! xSMgr.is())
     {
-        throw RuntimeException(
-            OUString( RTL_CONSTASCII_USTRINGPARAM("no service manager available!") ),
+        throw RuntimeException("no service manager available!",
             Reference< XInterface >() );
     }
 
     Reference< xml::sax::XExtendedDocumentHandler > xHandler( xSMgr->createInstanceWithContext(
-        OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.xml.sax.Writer") ), xContext ), UNO_QUERY );
+        "com.sun.star.xml.sax.Writer", xContext ), UNO_QUERY );
     OSL_ASSERT( xHandler.is() );
     if (! xHandler.is())
     {
-        throw RuntimeException(
-            OUString( RTL_CONSTASCII_USTRINGPARAM("could not create sax-writer component!") ),
+        throw RuntimeException("could not create sax-writer component!",
             Reference< XInterface >() );
     }
 
@@ -103,18 +101,16 @@ void SAL_CALL importDialogModel(
     Reference< lang::XMultiComponentFactory > xSMgr( xContext->getServiceManager() );
     if (! xSMgr.is())
     {
-        throw RuntimeException(
-            OUString( RTL_CONSTASCII_USTRINGPARAM("no service manager available!") ),
+        throw RuntimeException("no service manager available!",
             Reference< XInterface >() );
     }
 
     Reference< xml::sax::XParser > xParser( xSMgr->createInstanceWithContext(
-        OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.xml.sax.Parser") ), xContext ), UNO_QUERY );
+        "com.sun.star.xml.sax.Parser", xContext ), UNO_QUERY );
     OSL_ASSERT( xParser.is() );
     if (! xParser.is())
     {
-        throw RuntimeException(
-            OUString( RTL_CONSTASCII_USTRINGPARAM("could not create sax-parser component!") ),
+        throw RuntimeException("could not create sax-parser component!",
             Reference< XInterface >() );
     }
 
@@ -123,7 +119,7 @@ void SAL_CALL importDialogModel(
 
     xml::sax::InputSource source;
     source.aInputStream = xInput;
-    source.sSystemId = OUString( RTL_CONSTASCII_USTRINGPARAM("virtual file") );
+    source.sSystemId = "virtual file";
 
     xParser->parseStream( source );
 }

commit 389df1ff0bdf4b6b770fb54790c4b7a2ca4b9b2d

Correct OUString check with proper startsWith()
This patch fixes an error introduced in a72d1262231bf5d754c62c08daf23c4838753148,
with OUString::startsWith() introduced by Lubos Lunak in 86e10251d6e627f81c74b886165b88771dec9675
Thanks to Lubos and Kendy.

Change-Id: Idcfb9ef59cfdf4c7b08193f5c137b985fa8b9965
Reviewed-on: https://gerrit.libreoffice.org/573
Reviewed-by: Jan Holesovsky <kendy@suse.cz>
Tested-by: Jan Holesovsky <kendy@suse.cz>

xmlscript/source/xml_helper/xml_impctx.cxx
@@ -596,7 +596,7 @@ void DocumentHandlerImpl::startElement(
         {
             OUString const & rQAttributeName = pQNames[ nPos ];
             OSL_ENSURE(
-                rQAttributeName.compareToAscii( "xmlns:" ) != 0,
+                !rQAttributeName.startsWith( "xmlns:" ),
                 "### unexpected xmlns!" );
 
             // collect attribute's uid and current prefix