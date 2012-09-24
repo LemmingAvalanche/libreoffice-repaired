LemmingAvalanche/libreoffice-repaired

commit ee4dfbc543d6826c48566dff54af5a66d98585fa

Related fdo#53006: Do not instantiate service manager too early
... in soffice_main calling GetCommandLineArgs, before Desktop::Init takes care
of synchronization of the per-user bundled/shared extension information (as the
latter potentially modifies data that the service manager consumes upon
instantiation; this e.g. lead to bundled extensions not working during first
start after an upgrade).

The only reason GetCommandLineArgs needed ensureProcessServiceFactory appears to
be the ExternalUriReferenceTranslator.  So defer its usage to when the relevant
cmd line args are actually processed (which, luckily, does not yet happen in
soffice_main's usage of GetCommandLineArgs).

Change-Id: I6ebbf0a4ad1c6f64c8fbbe2b0d7628fa42a1afb6

desktop/source/app/app.cxx
@@ -283,7 +283,6 @@ namespace
 
 CommandLineArgs& Desktop::GetCommandLineArgs()
 {
-    ensureProcessServiceFactory();
     return theCommandLineArgs::get();
 }
 

desktop/source/app/cmdlineargs.cxx
@@ -32,9 +32,7 @@
 #include <rtl/ustring.hxx>
 #include "rtl/process.h"
 #include <comphelper/processfactory.hxx>
-#include <com/sun/star/uri/XExternalUriReferenceTranslator.hpp>
-#include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/uri/ExternalUriReferenceTranslator.hpp>
 #include "tools/getprocessworkingdir.hxx"
 
 #include <svl/documentlockfile.hxx>
@@ -51,6 +49,26 @@ namespace desktop
 {
 
 namespace {
+
+OUString translateExternalUris(OUString const & input) {
+    OUString t(
+        com::sun::star::uri::ExternalUriReferenceTranslator::create(
+            comphelper::getProcessComponentContext())->
+        translateToInternal(input));
+    return t.isEmpty() ? input : t;
+}
+
+std::vector< OUString > translateExternalUris(
+    std::vector< OUString > const & input)
+{
+    std::vector< OUString > t;
+    for (std::vector< OUString >::const_iterator i(input.begin());
+         i != input.end(); ++i)
+    {
+        t.push_back(translateExternalUris(*i));
+    }
+    return t;
+}
 
 class ExtCommandLineSupplier: public CommandLineArgs::Supplier {
 public:
@@ -117,14 +135,6 @@ CommandLineArgs::CommandLineArgs( Supplier& supplier )
 void CommandLineArgs::ParseCommandLine_Impl( Supplier& supplier )
 {
     m_cwdUrl = supplier.getCwdUrl();
-    Reference<XMultiServiceFactory> xMS(comphelper::getProcessServiceFactory(), UNO_QUERY);
-    OSL_ENSURE(xMS.is(), "CommandLineArgs: no ProcessServiceFactory.");
-
-    Reference< XExternalUriReferenceTranslator > xTranslator(
-        xMS->createInstance(
-        OUString(
-        "com.sun.star.uri.ExternalUriReferenceTranslator")),
-        UNO_QUERY);
 
     // parse command line arguments
     bool bOpenEvent(true);
@@ -149,14 +159,6 @@ void CommandLineArgs::ParseCommandLine_Impl( Supplier& supplier )
         if ( !supplier.next( &aArg ) )
         {
             break;
-        }
-        // convert file URLs to internal form
-        if (aArg.indexOfAsciiL(RTL_CONSTASCII_STRINGPARAM("file:"))==0 &&
-            xTranslator.is())
-        {
-            OUString tmp(xTranslator->translateToInternal(aArg));
-            if (!tmp.isEmpty())
-                aArg = tmp;
         }
 
         if ( !aArg.isEmpty() )
@@ -785,39 +787,39 @@ std::vector< rtl::OUString > const & CommandLineArgs::GetUnaccept() const
     return m_unaccept;
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetOpenList() const
+std::vector< rtl::OUString > CommandLineArgs::GetOpenList() const
 {
-    return m_openlist;
+    return translateExternalUris(m_openlist);
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetViewList() const
+std::vector< rtl::OUString > CommandLineArgs::GetViewList() const
 {
-    return m_viewlist;
+    return translateExternalUris(m_viewlist);
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetStartList() const
+std::vector< rtl::OUString > CommandLineArgs::GetStartList() const
 {
-    return m_startlist;
+    return translateExternalUris(m_startlist);
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetForceOpenList() const
+std::vector< rtl::OUString > CommandLineArgs::GetForceOpenList() const
 {
-    return m_forceopenlist;
+    return translateExternalUris(m_forceopenlist);
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetForceNewList() const
+std::vector< rtl::OUString > CommandLineArgs::GetForceNewList() const
 {
-    return m_forcenewlist;
+    return translateExternalUris(m_forcenewlist);
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetPrintList() const
+std::vector< rtl::OUString > CommandLineArgs::GetPrintList() const
 {
-    return m_printlist;
+    return translateExternalUris(m_printlist);
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetPrintToList() const
+std::vector< rtl::OUString > CommandLineArgs::GetPrintToList() const
 {
-    return m_printtolist;
+    return translateExternalUris(m_printtolist);
 }
 
 rtl::OUString CommandLineArgs::GetPrinterName() const
@@ -835,9 +837,9 @@ std::vector< rtl::OUString > const & CommandLineArgs::GetInFilter() const
     return m_infilter;
 }
 
-std::vector< rtl::OUString > const & CommandLineArgs::GetConversionList() const
+std::vector< rtl::OUString > CommandLineArgs::GetConversionList() const
 {
-    return m_conversionlist;
+    return translateExternalUris(m_conversionlist);
 }
 
 rtl::OUString CommandLineArgs::GetConversionParams() const
@@ -846,7 +848,7 @@ rtl::OUString CommandLineArgs::GetConversionParams() const
 }
 rtl::OUString CommandLineArgs::GetConversionOut() const
 {
-    return m_conversionout;
+    return translateExternalUris(m_conversionout);
 }
 
 bool CommandLineArgs::IsEmpty() const

desktop/source/app/cmdlineargs.hxx
@@ -100,17 +100,17 @@ class CommandLineArgs: private boost::noncopyable
         bool                    HasSplashPipe() const;
         std::vector< rtl::OUString > const & GetAccept() const;
         std::vector< rtl::OUString > const & GetUnaccept() const;
-        std::vector< rtl::OUString > const & GetOpenList() const;
-        std::vector< rtl::OUString > const & GetViewList() const;
-        std::vector< rtl::OUString > const & GetStartList() const;
-        std::vector< rtl::OUString > const & GetForceOpenList() const;
-        std::vector< rtl::OUString > const & GetForceNewList() const;
-        std::vector< rtl::OUString > const & GetPrintList() const;
-        std::vector< rtl::OUString > const & GetPrintToList() const;
+        std::vector< rtl::OUString > GetOpenList() const;
+        std::vector< rtl::OUString > GetViewList() const;
+        std::vector< rtl::OUString > GetStartList() const;
+        std::vector< rtl::OUString > GetForceOpenList() const;
+        std::vector< rtl::OUString > GetForceNewList() const;
+        std::vector< rtl::OUString > GetPrintList() const;
+        std::vector< rtl::OUString > GetPrintToList() const;
         rtl::OUString       GetPrinterName() const;
         rtl::OUString       GetLanguage() const;
         std::vector< rtl::OUString > const & GetInFilter() const;
-        std::vector< rtl::OUString > const & GetConversionList() const;
+        std::vector< rtl::OUString > GetConversionList() const;
         rtl::OUString       GetConversionParams() const;
         rtl::OUString       GetConversionOut() const;
 
@@ -160,17 +160,17 @@ class CommandLineArgs: private boost::noncopyable
         bool m_bDocumentArgs; // A document creation/open/load arg is used
         std::vector< rtl::OUString > m_accept;
         std::vector< rtl::OUString > m_unaccept;
-        std::vector< rtl::OUString > m_openlist;
-        std::vector< rtl::OUString > m_viewlist;
-        std::vector< rtl::OUString > m_startlist;
-        std::vector< rtl::OUString > m_forceopenlist;
-        std::vector< rtl::OUString > m_forcenewlist;
-        std::vector< rtl::OUString > m_printlist;
-        std::vector< rtl::OUString > m_printtolist;
+        std::vector< rtl::OUString > m_openlist; // contains external URIs
+        std::vector< rtl::OUString > m_viewlist; // contains external URIs
+        std::vector< rtl::OUString > m_startlist; // contains external URIs
+        std::vector< rtl::OUString > m_forceopenlist; // contains external URIs
+        std::vector< rtl::OUString > m_forcenewlist; // contains external URIs
+        std::vector< rtl::OUString > m_printlist; // contains external URIs
+        std::vector< rtl::OUString > m_printtolist; // contains external URIs
         rtl::OUString m_printername;
-        std::vector< rtl::OUString > m_conversionlist;
+        std::vector< rtl::OUString > m_conversionlist; // contains external URIs
         rtl::OUString m_conversionparams;
-        rtl::OUString m_conversionout;
+        rtl::OUString m_conversionout; // contains external URIs
         std::vector< rtl::OUString > m_infilter;
         rtl::OUString m_language;
 };

commit 6903fd27e9ad0af4d57cda63536fa99d986e0088

fdo#54385: Displaying help/version early on UNX requires service manager
This regression was introduced with ee4dfbc543d6826c48566dff54af5a66d98585fa
"Related fdo#53006: Do not instantiate service manager too early."

Change-Id: If22ea3ac6474188bf0792246620e5c705a813445

desktop/inc/app.hxx
@@ -136,11 +136,11 @@ class Desktop : public Application
         void                    SetSplashScreenText( const ::rtl::OUString& rText );
         void                    SetSplashScreenProgress( sal_Int32 );
 
-    private:
         // Bootstrap methods
-        static ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > CreateApplicationServiceManager();
-            // returns a non-null reference or throws an exception
+        static void             InitApplicationServiceManager();
+            // throws an exception upon failure
 
+    private:
         void                    RegisterServices();
         void                    DeregisterServices();
 

desktop/source/app/app.cxx
@@ -600,7 +600,7 @@ void Desktop::Init()
     // something to enforce this gotcha
     try
     {
-        comphelper::setProcessServiceFactory(CreateApplicationServiceManager());
+        InitApplicationServiceManager();
     }
     catch (css::uno::Exception & e)
     {

desktop/source/app/appinit.cxx
@@ -111,20 +111,21 @@ static void configureUcb()
 #endif // GNOME_VFS_ENABLED
 }
 
-Reference< XMultiServiceFactory > Desktop::CreateApplicationServiceManager()
+void Desktop::InitApplicationServiceManager()
 {
     RTL_LOGFILE_CONTEXT( aLog, "desktop (cd100003) ::createApplicationServiceManager" );
-
+    Reference<XMultiServiceFactory> sm;
 #ifdef ANDROID
     rtl::OUString aUnoRc( OUString( "file:///assets/program/unorc"  ) );
-    return Reference<XMultiServiceFactory>(
+    sm.set(
         cppu::defaultBootstrap_InitialComponentContext( aUnoRc )->getServiceManager(),
         UNO_QUERY_THROW);
 #else
-    return Reference<XMultiServiceFactory>(
+    sm.set(
         cppu::defaultBootstrap_InitialComponentContext()->getServiceManager(),
         UNO_QUERY_THROW);
 #endif
+    comphelper::setProcessServiceFactory(sm);
 }
 
 void Desktop::DestroyApplicationServiceManager( Reference< XMultiServiceFactory >& xSMgr )

desktop/source/app/sofficemain.cxx
@@ -62,11 +62,13 @@ extern "C" int DESKTOP_DLLPUBLIC soffice_main()
     const desktop::CommandLineArgs& rCmdLineArgs = aDesktop.GetCommandLineArgs();
     if ( rCmdLineArgs.IsHelp() )
     {
+        desktop::Desktop::InitApplicationServiceManager();
         desktop::displayCmdlineHelp();
         return EXIT_SUCCESS;
     }
     else if ( rCmdLineArgs.IsVersion() )
     {
+        desktop::Desktop::InitApplicationServiceManager();
         desktop::displayVersion();
         return EXIT_SUCCESS;
     }