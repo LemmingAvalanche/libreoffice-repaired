LemmingAvalanche/libreoffice-repaired

commit 6b4dd8569e67e24ed70fa9b7073a384e0aabfb4e

n#693238 dmapper: fix margin values vs SBkc = 0 at the end of the doc
The problem was that a continous section break at the end of the
document caused the section margins to be ignored. Just ignoring the
continous section break (only in case it's at the end of the document)
makes the problem go away.

The original fix was commit cd1c434c4bd4756fa1355e906cb2e8de4aae0618,
but it got reverted in commit 418f49a32d5c8e773a0fd0fad3015c1b6f753a16,
as it caused fdo#44292. This second attempt fixes the original bug
again, without introducing the "4 columns" bug.

Change-Id: I75d612430a5cd631dec0d224127cba580070900d

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2369,7 +2369,12 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext, SprmType
         */
         OSL_ENSURE(pSectionContext, "SectionContext unavailable!");
         if(pSectionContext)
-            pSectionContext->SetBreakType( nIntValue );
+        {
+            // Ignore continous section break at the end of the document
+            // It makes the importer loose margin settings with no benefit
+            if (m_pImpl->GetParaSectpr() || nIntValue != 0)
+                pSectionContext->SetBreakType( nIntValue );
+        }
         break;
     case 143:
     case NS_sprm::LN_SFTitlePage: // sprmSFTitlePage

commit 6d77cefff0485feb89e786dd0798329c327e1ae7

fdo#52208 DOCX import: don't always ignore cont sect break at the doc end
Regression from commit 6b4dd8569e67e24ed70fa9b7073a384e0aabfb4e,
continuous section break does make sense at the end of the doc, if the
previous type was a non-continuous.

Change-Id: I6d82c67e068d8dc3ce1edb1a5fe6ad293afd805d

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2392,9 +2392,17 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext, SprmType
         OSL_ENSURE(pSectionContext, "SectionContext unavailable!");
         if(pSectionContext)
         {
-            // Ignore continous section break at the end of the document
+            // Ignore continous section break at the end of the document, if the previous section had the same type as well
             // It makes the importer loose margin settings with no benefit
-            if (m_pImpl->GetParaSectpr() || nIntValue != 0)
+            SectionPropertyMap* pLastContext = m_pImpl->GetLastSectionContext();
+            int nPrevBreakType = 0;
+            bool bHasPrevSection = false;
+            if (pLastContext)
+            {
+                bHasPrevSection = true;
+                nPrevBreakType = pLastContext->GetBreakType();
+            }
+            if (m_pImpl->GetParaSectpr() || nIntValue != 0 || (bHasPrevSection && nPrevBreakType != nIntValue))
                 pSectionContext->SetBreakType( nIntValue );
         }
         break;

writerfilter/source/dmapper/PropertyMap.hxx
@@ -235,6 +235,7 @@ public:
     void SetPageNoRestart( bool bSet ) { m_bPageNoRestart = bSet; }
     void SetPageNumber( sal_Int32 nSet ) { m_nPageNumber = nSet; }
     void SetBreakType( sal_Int32 nSet ) { m_nBreakType = nSet; }
+    sal_Int32 GetBreakType( ) { return m_nBreakType; }
     void SetPaperBin( sal_Int32 nSet );
     void SetFirstPaperBin( sal_Int32 nSet );
 

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -105,6 +105,7 @@ public:
     void testN782061();
     void testN782345();
     void testN783638();
+    void testFdo52208();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -152,6 +153,7 @@ public:
     CPPUNIT_TEST(testN782061);
     CPPUNIT_TEST(testN782345);
     CPPUNIT_TEST(testN783638);
+    CPPUNIT_TEST(testFdo52208);
 #endif
     CPPUNIT_TEST_SUITE_END();
 
@@ -1088,6 +1090,17 @@ void Test::testN783638()
     uno::Reference<container::XIndexAccess> xDraws(xDrawPageSupplier->getDrawPage(), uno::UNO_QUERY);
     uno::Reference<beans::XPropertySet> xPropertySet(xDraws->getByIndex(0), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xPropertySet, "LeftMargin"));
+}
+
+void Test::testFdo52208()
+{
+    // The problem was that the document had 2 pages instead of 1.
+    load("fdo52208.docx");
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextViewCursorSupplier> xTextViewCursorSupplier(xModel->getCurrentController(), uno::UNO_QUERY);
+    uno::Reference<text::XPageCursor> xCursor(xTextViewCursorSupplier->getViewCursor(), uno::UNO_QUERY);
+    xCursor->jumpToLastPage();
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(1), xCursor->getPage());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);