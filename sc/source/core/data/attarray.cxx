LemmingAvalanche/libreoffice-repaired

commit 1f55577c98547550597dbbd0ef3c7924bda0e163

support overlapping conditional formats
Still missing:

*Removing conditional formats
*Handling of Manage Names dialog
*UNO
*Undo/Redo
*Import/export
*Adjusting the dialogs to support this

Change-Id: I72481cfb807fee2c3a1546222fe7e786d515ab44

sc/source/core/data/attarray.cxx
@@ -273,6 +273,55 @@ const ScPatternAttr* ScAttrArray::GetPatternRange( SCROW& rStartRow,
     return NULL;
 }
 
+void ScAttrArray::AddCondFormat( SCROW nStartRow, SCROW nEndRow, sal_uInt32 nIndex )
+{
+    if(!VALIDROW(nStartRow) || !VALIDROW(nEndRow))
+        return;
+
+    if(nEndRow < nStartRow)
+        return;
+
+    SCROW nTempStartRow = nStartRow;
+    SCROW nTempEndRow = nEndRow;
+
+    do
+    {
+        const ScPatternAttr* pPattern = GetPattern(nTempStartRow);
+
+        ScPatternAttr aPattern( pDocument->GetPool() );
+        if(pPattern)
+        {
+            SCROW nPatternStartRow;
+            SCROW nPatternEndRow;
+            GetPatternRange( nPatternStartRow, nPatternEndRow, nTempStartRow );
+
+            nTempEndRow = std::min<SCROW>( nPatternEndRow, nEndRow );
+            const SfxPoolItem* pItem = NULL;
+            SfxItemState eState = pPattern->GetItemSet().GetItemState( ATTR_CONDITIONAL, true, &pItem );
+            std::vector< sal_uInt32 > aCondFormatData;
+            if(pItem)
+                aCondFormatData = static_cast<const ScCondFormatItem*>(pItem)->GetCondFormatData();
+            aCondFormatData.push_back(nIndex);
+
+            ScCondFormatItem aItem;
+            aItem.SetCondFormatData( aCondFormatData );
+            aPattern.GetItemSet().Put( aItem );
+        }
+        else
+        {
+            ScCondFormatItem aItem;
+            aItem.AddCondFormatData(nIndex);
+            aPattern.GetItemSet().Put( aItem );
+            nTempEndRow = nEndRow;
+        }
+
+        SetPatternArea( nTempStartRow, nTempEndRow, &aPattern, true );
+        nTempStartRow = nTempEndRow + 1;
+    }
+    while(nTempEndRow < nEndRow);
+
+}
+
 //------------------------------------------------------------------------
 
 void ScAttrArray::SetPattern( SCROW nRow, const ScPatternAttr* pPattern, bool bPutToPool )

sc/inc/attarray.hxx
@@ -138,6 +138,8 @@ public:
     void    ApplyLineStyleArea( SCROW nStartRow, SCROW nEndRow,
                                 const ::editeng::SvxBorderLine* pLine, bool bColorOnly );
 
+    void    AddCondFormat( SCROW nStartRow, SCROW nEndRow, sal_uInt32 nIndex );
+
     void    ClearItems( SCROW nStartRow, SCROW nEndRow, const sal_uInt16* pWhich );
     void    ChangeIndent( SCROW nStartRow, SCROW nEndRow, bool bIncrement );
 

sc/inc/attrib.hxx
@@ -393,6 +393,30 @@ private:
     sal_uInt16                  mnHeight;
 };
 
+class ScCondFormatItem : public SfxPoolItem
+{
+public:
+    TYPEINFO();
+
+    explicit ScCondFormatItem();
+    explicit ScCondFormatItem(sal_uInt32 nIndex);
+    explicit ScCondFormatItem(const std::vector<sal_uInt32>& nIndex);
+
+    virtual ~ScCondFormatItem();
+
+    virtual int operator==(const SfxPoolItem& rCmp ) const;
+    virtual ScCondFormatItem*  Clone( SfxItemPool* = 0 ) const;
+
+    const std::vector<sal_uInt32>& GetCondFormatData() const;
+    void AddCondFormatData( sal_uInt32 nIndex );
+    void SetCondFormatData( const std::vector<sal_uInt32>& aIndex );
+    void RemoveCondFormatData( sal_uInt32 nIndex );
+
+private:
+
+    std::vector<sal_uInt32> maIndex;
+};
+
 // ============================================================================
 
 #endif

sc/inc/column.hxx
@@ -317,6 +317,7 @@ public:
     void        ApplySelectionStyle(const ScStyleSheet& rStyle, const ScMarkData& rMark);
     void        ApplySelectionLineStyle( const ScMarkData& rMark,
                                     const ::editeng::SvxBorderLine* pLine, bool bColorOnly );
+    void        AddCondFormat(SCROW nStartRow, SCROW nEndRow, sal_uInt32 nIndex );
 
     const ScStyleSheet* GetStyle( SCROW nRow ) const;
     const ScStyleSheet* GetSelectionStyle( const ScMarkData& rMark, bool& rFound ) const;

sc/inc/document.hxx
@@ -1155,6 +1155,7 @@ public:
     SC_DLLPUBLIC const ScPatternAttr*    GetMostUsedPattern( SCCOL nCol, SCROW nStartRow, SCROW nEndRow, SCTAB nTab ) const;
     const ScPatternAttr*    GetSelectionPattern( const ScMarkData& rMark, bool bDeep = true );
     ScPatternAttr*          CreateSelectionPattern( const ScMarkData& rMark, bool bDeep = true );
+    SC_DLLPUBLIC void AddCondFormatData( const ScRangeList& rRange, SCTAB nTab, sal_uInt32 nIndex );
 
     SC_DLLPUBLIC ScConditionalFormat* GetCondFormat( SCCOL nCol, SCROW nRow, SCTAB nTab ) const;
     SC_DLLPUBLIC const SfxItemSet*  GetCondResult( SCCOL nCol, SCROW nRow, SCTAB nTab ) const;

sc/inc/scitems.hxx
@@ -118,51 +118,51 @@
 #define ATTR_VALIDDATA          153
 #define ATTR_CONDITIONAL        154
 
-#define ATTR_PATTERN_END        155     // end cell-attribute-pattern
+#define ATTR_PATTERN_END        154     // end cell-attribute-pattern
 
-#define ATTR_PATTERN            156
+#define ATTR_PATTERN            155
                                         // page attributes
-#define ATTR_LRSPACE            157     // editor: PageDesc-TabPage
-#define ATTR_ULSPACE            158
-#define ATTR_PAGE               159
-#define ATTR_PAGE_PAPERTRAY     160
-#define ATTR_PAGE_PAPERBIN      161
-#define ATTR_PAGE_SIZE          162
-#define ATTR_PAGE_MAXSIZE       163
-#define ATTR_PAGE_HORCENTER     164
-#define ATTR_PAGE_VERCENTER     165
+#define ATTR_LRSPACE            156     // editor: PageDesc-TabPage
+#define ATTR_ULSPACE            157
+#define ATTR_PAGE               158
+#define ATTR_PAGE_PAPERTRAY     159
+#define ATTR_PAGE_PAPERBIN      160
+#define ATTR_PAGE_SIZE          161
+#define ATTR_PAGE_MAXSIZE       162
+#define ATTR_PAGE_HORCENTER     163
+#define ATTR_PAGE_VERCENTER     164
 
-#define ATTR_PAGE_ON            166     // editor: header/footer-page
-#define ATTR_PAGE_DYNAMIC       167
-#define ATTR_PAGE_SHARED        168
+#define ATTR_PAGE_ON            165     // editor: header/footer-page
+#define ATTR_PAGE_DYNAMIC       166
+#define ATTR_PAGE_SHARED        167
 
-#define ATTR_PAGE_NOTES         169     // editor: table
-#define ATTR_PAGE_GRID          170
-#define ATTR_PAGE_HEADERS       171
-#define ATTR_PAGE_CHARTS        172
-#define ATTR_PAGE_OBJECTS       173
-#define ATTR_PAGE_DRAWINGS      174
-#define ATTR_PAGE_TOPDOWN       175
-#define ATTR_PAGE_SCALE         176
-#define ATTR_PAGE_SCALETOPAGES  177
-#define ATTR_PAGE_FIRSTPAGENO   178
+#define ATTR_PAGE_NOTES         168     // editor: table
+#define ATTR_PAGE_GRID          169
+#define ATTR_PAGE_HEADERS       170
+#define ATTR_PAGE_CHARTS        171
+#define ATTR_PAGE_OBJECTS       172
+#define ATTR_PAGE_DRAWINGS      173
+#define ATTR_PAGE_TOPDOWN       174
+#define ATTR_PAGE_SCALE         175
+#define ATTR_PAGE_SCALETOPAGES  176
+#define ATTR_PAGE_FIRSTPAGENO   177
 
-#define ATTR_PAGE_PRINTAREA     179     // editor: print areas
-#define ATTR_PAGE_REPEATROW     180
-#define ATTR_PAGE_REPEATCOL     181
-#define ATTR_PAGE_PRINTTABLES   182
+#define ATTR_PAGE_PRINTAREA     178     // editor: print areas
+#define ATTR_PAGE_REPEATROW     179
+#define ATTR_PAGE_REPEATCOL     180
+#define ATTR_PAGE_PRINTTABLES   181
 
-#define ATTR_PAGE_HEADERLEFT    183     // contents of header/
-#define ATTR_PAGE_FOOTERLEFT    184     // footer (left)
-#define ATTR_PAGE_HEADERRIGHT   185     // contents of header/
-#define ATTR_PAGE_FOOTERRIGHT   186     // footer (right)
-#define ATTR_PAGE_HEADERSET     187     // the corresponding sets
-#define ATTR_PAGE_FOOTERSET     188
+#define ATTR_PAGE_HEADERLEFT    182     // contents of header/
+#define ATTR_PAGE_FOOTERLEFT    183     // footer (left)
+#define ATTR_PAGE_HEADERRIGHT   184     // contents of header/
+#define ATTR_PAGE_FOOTERRIGHT   185     // footer (right)
+#define ATTR_PAGE_HEADERSET     186     // the corresponding sets
+#define ATTR_PAGE_FOOTERSET     187
 
-#define ATTR_PAGE_FORMULAS      189
-#define ATTR_PAGE_NULLVALS      190
+#define ATTR_PAGE_FORMULAS      188
+#define ATTR_PAGE_NULLVALS      189
 
-#define ATTR_PAGE_SCALETO       191     // #i8868# scale printout to width/height
+#define ATTR_PAGE_SCALETO       190     // #i8868# scale printout to width/height
 
 #define ATTR_ENDINDEX           ATTR_PAGE_SCALETO        // end of pool-range
 

sc/inc/table.hxx
@@ -543,6 +543,7 @@ public:
     void        SetPattern( SCCOL nCol, SCROW nRow, const ScPatternAttr& rAttr, bool bPutToPool = false );
     void        ApplyPatternIfNumberformatIncompatible( const ScRange& rRange,
                             const ScPatternAttr& rPattern, short nNewType );
+    void        AddCondFormatData( const ScRangeList& rRange, sal_uInt32 nIndex );
 
     void        ApplyStyle( SCCOL nCol, SCROW nRow, const ScStyleSheet& rStyle );
     void        ApplyStyleArea( SCCOL nStartCol, SCROW nStartRow, SCCOL nEndCol, SCROW nEndRow, const ScStyleSheet& rStyle );

sc/source/core/data/attrib.cxx
@@ -60,6 +60,7 @@ TYPEINIT1(ScPageHFItem,         SfxPoolItem);
 TYPEINIT1(ScViewObjectModeItem, SfxEnumItem);
 TYPEINIT1(ScDoubleItem,         SfxPoolItem);
 TYPEINIT1(ScPageScaleToItem,    SfxPoolItem);
+TYPEINIT1(ScCondFormatItem,    SfxPoolItem);
 
 //------------------------------------------------------------------------
 
@@ -1221,6 +1222,52 @@ bool ScPageScaleToItem::PutValue( const uno::Any& rAny, sal_uInt8 nMemberId )
 }
 
 // ============================================================================
+
+ScCondFormatItem::ScCondFormatItem():
+    SfxPoolItem( ATTR_CONDITIONAL )
+{
+}
+
+ScCondFormatItem::ScCondFormatItem( sal_uInt32 nIndex ):
+    SfxPoolItem( ATTR_CONDITIONAL )
+{
+    maIndex.push_back(nIndex);
+}
+
+ScCondFormatItem::ScCondFormatItem( const std::vector<sal_uInt32>& rIndex ):
+    SfxPoolItem( ATTR_CONDITIONAL ),
+    maIndex( rIndex )
+{
+}
+
+ScCondFormatItem::~ScCondFormatItem()
+{
+}
+
+int ScCondFormatItem::operator==( const SfxPoolItem& rCmp ) const
+{
+    return maIndex == static_cast<const ScCondFormatItem&>(rCmp).maIndex;
+}
+
+ScCondFormatItem* ScCondFormatItem::Clone(SfxItemPool*) const
+{
+    return new ScCondFormatItem(maIndex);
+}
+
+const std::vector<sal_uInt32>& ScCondFormatItem::GetCondFormatData() const
+{
+    return maIndex;
+}
+
+void ScCondFormatItem::AddCondFormatData( sal_uInt32 nIndex )
+{
+    maIndex.push_back(nIndex);
+}
+
+void ScCondFormatItem::SetCondFormatData( const std::vector<sal_uInt32>& rIndex )
+{
+    maIndex = rIndex;
+}
 
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit e8b60ba8aebd153e89d15f050edc028ef6e4ffe1

sc: fix erroneous casts of ScCondFormatItem
These cause valgrind warnings in sc_unoapi.
(regression from 1f55577c98547550597dbbd0ef3c7924bda0e163)

Change-Id: If3128410bfabc23ff85c24156a8602379b8b7edd

sc/source/core/data/attarray.cxx
@@ -1320,7 +1320,8 @@ bool ScAttrArray::HasAttrib( SCROW nRow1, SCROW nRow2, sal_uInt16 nMask ) const
             else if ((SvxCellHorJustify)((const SvxHorJustifyItem&)pPattern->
                         GetItem( ATTR_HOR_JUSTIFY )).GetValue() == SVX_HOR_JUSTIFY_BLOCK)
                 bFound = true;
-            else if (((const SfxUInt32Item&)pPattern->GetItem( ATTR_CONDITIONAL )).GetValue())
+
+            else if (!static_cast<const ScCondFormatItem&>(pPattern->GetItem(ATTR_CONDITIONAL)).GetCondFormatData().empty())
                 bFound = true;
             else if (((const SfxInt32Item&)pPattern->GetItem( ATTR_ROTATE_VALUE )).GetValue())
                 bFound = true;

sc/source/core/data/column2.cxx
@@ -711,9 +711,12 @@ void ScColumn::GetOptimalHeight( SCROW nStartRow, SCROW nEndRow, sal_uInt16* pHe
                 bStdOnly = !bBreak;
 
                 // bedingte Formatierung: Zellen durchgehen
-                if ( bStdOnly && ((const SfxUInt32Item&)pPattern->
-                                    GetItem(ATTR_CONDITIONAL)).GetValue() )
+                if (bStdOnly &&
+                    !static_cast<const ScCondFormatItem&>(pPattern->GetItem(
+                            ATTR_CONDITIONAL)).GetCondFormatData().empty())
+                {
                     bStdOnly = false;
+                }
 
                 // gedrehter Text: Zellen durchgehen
                 if ( bStdOnly && ((const SfxInt32Item&)pPattern->