LemmingAvalanche/libreoffice-repaired

commit 5433d501b67fc35b629d18c95d116436f61dacec

SvStringsDtor->std::vector

sw/source/ui/utlui/initui.cxx
@@ -233,37 +233,38 @@ SwGlossaryList* GetGlossaryList()
 
 struct ImpAutoFmtNameListLoader : public Resource
 {
-    ImpAutoFmtNameListLoader( SvStringsDtor& rLst );
+    ImpAutoFmtNameListLoader( std::vector<String>& rLst );
 };
 
 void ShellResource::_GetAutoFmtNameLst() const
 {
-    SvStringsDtor** ppLst = (SvStringsDtor**)&pAutoFmtNameLst;
-    *ppLst = new SvStringsDtor( STR_AUTOFMTREDL_END );
-    ImpAutoFmtNameListLoader aTmp( **ppLst );
+    std::vector<String>* pLst(pAutoFmtNameLst);
+    pLst = new std::vector<String>;
+    pLst->reserve(STR_AUTOFMTREDL_END);
+    ImpAutoFmtNameListLoader aTmp( *pLst );
 }
 
-ImpAutoFmtNameListLoader::ImpAutoFmtNameListLoader( SvStringsDtor& rLst )
+ImpAutoFmtNameListLoader::ImpAutoFmtNameListLoader( std::vector<String>& rLst )
     : Resource( ResId(RID_SHELLRES_AUTOFMTSTRS, *pSwResMgr) )
 {
     for( sal_uInt16 n = 0; n < STR_AUTOFMTREDL_END; ++n )
     {
-        String* p = new String( ResId( n + 1, *pSwResMgr) );
+        String p(ResId(n + 1, *pSwResMgr));
         if(STR_AUTOFMTREDL_TYPO == n)
         {
 #ifdef WNT
             //fuer Windows Sonderbehandlung, da MS hier ein paar Zeichen im Dialogfont vergessen hat
-            p->SearchAndReplace(C2S("%1"), C2S(",,"));
-            p->SearchAndReplace(C2S("%2"), C2S("''"));
+            p.SearchAndReplace(C2S("%1"), C2S(",,"));
+            p.SearchAndReplace(C2S("%2"), C2S("''"));
 #else
             const SvtSysLocale aSysLocale;
             const LocaleDataWrapper& rLclD = aSysLocale.GetLocaleData();
             //unter richtigen Betriebssystemen funktioniert es auch so
-            p->SearchAndReplace(C2S("%1"), rLclD.getDoubleQuotationMarkStart());
-            p->SearchAndReplace(C2S("%2"), rLclD.getDoubleQuotationMarkEnd());
+            p.SearchAndReplace(C2S("%1"), rLclD.getDoubleQuotationMarkStart());
+            p.SearchAndReplace(C2S("%2"), rLclD.getDoubleQuotationMarkEnd());
 #endif
         }
-        rLst.Insert( p, n );
+        rLst.insert(rLst.begin() + n, p);
     }
     FreeResource();
 }

commit 0135a51c3b587030c87d78fc49789b2e851a877b

Resolves: fdo#56549 crash in format->autocorrect->apply and edit changes
regression from 5433d501b67fc35b629d18c95d116436f61dacec

Change-Id: I1aa36bf86969a5a90456817d360d59125948e519

sw/inc/shellres.hxx
@@ -87,7 +87,7 @@ struct SW_DLLPUBLIC ShellResource : public Resource
 
 private:
     void _GetAutoFmtNameLst() const;
-    std::vector<String> *pAutoFmtNameLst;
+    mutable std::vector<String> *pAutoFmtNameLst;
     String          sPageDescFirstName;
     String          sPageDescFollowName;
     String          sPageDescName;

sw/source/ui/utlui/initui.cxx
@@ -229,10 +229,10 @@ struct ImpAutoFmtNameListLoader : public Resource
 
 void ShellResource::_GetAutoFmtNameLst() const
 {
-    std::vector<String>* pLst(pAutoFmtNameLst);
-    pLst = new std::vector<String>;
-    pLst->reserve(STR_AUTOFMTREDL_END);
-    ImpAutoFmtNameListLoader aTmp( *pLst );
+    assert(!pAutoFmtNameLst);
+    pAutoFmtNameLst = new std::vector<String>;
+    pAutoFmtNameLst->reserve(STR_AUTOFMTREDL_END);
+    ImpAutoFmtNameListLoader aTmp(*pAutoFmtNameLst);
 }
 
 ImpAutoFmtNameListLoader::ImpAutoFmtNameListLoader( std::vector<String>& rLst )