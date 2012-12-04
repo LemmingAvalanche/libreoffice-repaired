LemmingAvalanche/libreoffice-repaired

commit 5f6e4522648f267b05db9b2fe9de63e63e1d6c9c

enforce only one possible use of gb_CustomTarget
This brings two changes:
- no more recursive calling of make
- gbuild_simple is now not used => removed

solenv/gbuild/gbuild.mk
@@ -49,7 +49,6 @@ GBUILDDIR:=$(SRCDIR)/solenv/gbuild
 .DELETE_ON_ERROR:
 
 SHELL := /bin/sh
-MAKEFLAGS+=-r
 true := T
 false :=
 define NEWLINE

accessibility/CustomTarget_bridge.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,accessibility/bridge,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,accessibility/bridge))
 
 ACBR := $(call gb_CustomTarget_get_workdir,accessibility/bridge)
 

accessibility/CustomTarget_bridge_inc.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,accessibility/bridge/inc,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,accessibility/bridge/inc))
 
 ACBI := $(call gb_CustomTarget_get_workdir,accessibility/bridge/inc)
 

clucene/CustomTarget_source.mk
@@ -26,7 +26,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,clucene/source,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,clucene/source))
 
 CLSO := $(call gb_CustomTarget_get_workdir,clucene/source)
 

desktop/CustomTarget_soffice.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,desktop/soffice,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,desktop/soffice))
 
 DESO := $(call gb_CustomTarget_get_workdir,desktop/soffice)
 

extensions/CustomTarget_so_activex_idl.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,extensions/source/activex/idl,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,extensions/source/activex/idl))
 
 EXAI := $(call gb_CustomTarget_get_workdir,extensions/source/activex/idl)
 

extensions/CustomTarget_so_activex_x64.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,extensions/source/activex,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,extensions/source/activex))
 
 EXSA := $(call gb_CustomTarget_get_workdir,extensions/source/activex)
 

filter/CustomTarget_svg.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_CustomTarget_CustomTarget,filter/source/svg,new_style))
+$(eval $(call gb_CustomTarget_CustomTarget,filter/source/svg))
 
 FISS := $(call gb_CustomTarget_get_workdir,filter/source/svg)
 

commit 4161e6d4b31452964db8ee8122b09be57c82d14c

revert the huge performance regression in gbuild intoduced with 5f6e4522648f267b05db9b2fe9de63e63e1d6c9c
- we should default to make -r
- everything else grows exponentially with the number of targets
- gbuild should now have sane noop builds again even on make 3.82

solenv/gbuild/gbuild.mk
@@ -48,10 +48,19 @@ GBUILDDIR:=$(SRCDIR)/solenv/gbuild
 
 .DELETE_ON_ERROR:
 
+# do not use built-in rules
+# DO NOT TOUCH THIS LINE UNLESS YOU REALLY KNOW WHAT YOU ARE DOING
+# REMOVING THIS MAKES e.g. MODULE SW ALONE SLOWER BY SOME 300%
+# FOR TAIL_BUILD THE IMPACT IS HUGE!
+# (unless you are doing make -r, which we should explicitly NOT require from
+# users)
+MAKEFLAGS+=-r
+
 # by default gbuild use /bin/sh
 # if you want to use a particular shell
 # you can export gb_SHELL=<path_to_shell>
 #
+
 ifdef gb_SHELL
 SHELL := $(gb_SHELL)
 else