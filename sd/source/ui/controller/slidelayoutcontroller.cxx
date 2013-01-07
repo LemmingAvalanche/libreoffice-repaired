LemmingAvalanche/libreoffice-repaired

commit 2d7a638613ec731ed72003e37a363530a8d138b5

fix some memory leaks in sd
Change-Id: I7c7d0772a3d10601c6d1a9ea22c697abe762036d

sd/source/ui/controller/slidelayoutcontroller.cxx
@@ -331,7 +331,7 @@ Sequence< OUString >  SlideLayoutController_getSupportedServiceNames() throw( Ru
 
 Reference< XInterface > SAL_CALL SlideLayoutController_createInstance( const Reference< XMultiServiceFactory >& rSMgr ) throw( RuntimeException )
 {
-    return *new SlideLayoutController( rSMgr, ".uno:AssignLayout", false );
+    return SlideLayoutController( rSMgr, ".uno:AssignLayout", false );
 }
 
 // --------------------------------------------------------------------
@@ -354,7 +354,7 @@ Sequence< OUString >  InsertSlideController_getSupportedServiceNames() throw( Ru
 
 Reference< XInterface > SAL_CALL InsertSlideController_createInstance( const Reference< XMultiServiceFactory >& rSMgr ) throw( RuntimeException )
 {
-    return *new SlideLayoutController( rSMgr, ".uno:InsertPage" , true );
+    return SlideLayoutController( rSMgr, ".uno:InsertPage" , true );
 }
 
 //========================================================================

sd/source/ui/toolpanel/ToolPanelFactory.cxx
@@ -101,7 +101,7 @@ namespace sd { namespace toolpanel
     //------------------------------------------------------------------------------------------------------------------
     Reference< XInterface > SAL_CALL ToolPanelFactory_createInstance( const Reference< XComponentContext >& i_rContext )
     {
-        return Reference< XInterface >( *new ToolPanelFactory( i_rContext ) );
+        return Reference< XInterface >( ToolPanelFactory( i_rContext ) );
     }
 
     //------------------------------------------------------------------------------------------------------------------

sd/source/ui/unoidl/sddetect.cxx
@@ -515,7 +515,7 @@ rtl::OUString SdFilterDetect::impl_getStaticImplementationName()
 /* Helper for registry */
 UNOREFERENCE< UNOXINTERFACE > SAL_CALL SdFilterDetect::impl_createInstance( const UNOREFERENCE< UNOXMULTISERVICEFACTORY >& xServiceManager ) throw( UNOEXCEPTION )
 {
-    return UNOREFERENCE< UNOXINTERFACE >( *new SdFilterDetect( xServiceManager ) );
+    return UNOREFERENCE< UNOXINTERFACE >( SdFilterDetect( xServiceManager ) );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sd/source/filter/xml/sdxmlwrp.cxx
@@ -282,7 +282,7 @@ sal_Int32 ReadThroughComponent(
 
         if( rStreamName.Len() )
         {
-            return *new TwoStringErrorInfo(
+            return TwoStringErrorInfo(
                             (bMustBeSuccessfull ? ERR_FORMAT_FILE_ROWCOL
                                                     : WARN_FORMAT_FILE_ROWCOL),
                             rStreamName, sErr,
@@ -291,7 +291,7 @@ sal_Int32 ReadThroughComponent(
         else
         {
             DBG_ASSERT( bMustBeSuccessfull, "Warnings are not supported" );
-            return *new StringErrorInfo( ERR_FORMAT_ROWCOL, sErr,
+            return StringErrorInfo( ERR_FORMAT_ROWCOL, sErr,
                              ERRCODE_BUTTON_OK | ERRCODE_MSG_ERROR );
         }
     }

commit d26771a909ecf6eb1219717cae5a12eb080eab3a

UNO objects must be allocated on the heap
This was a regression introduced with 2d7a638613ec731ed72003e37a363530a8d138b5
"fix some memory leaks in sd."

Change-Id: Ibcb2527b396886a5a904d8843c8560765ed859f5

sd/source/ui/controller/slidelayoutcontroller.cxx
@@ -331,7 +331,8 @@ Sequence< OUString >  SlideLayoutController_getSupportedServiceNames() throw( Ru
 
 Reference< XInterface > SAL_CALL SlideLayoutController_createInstance( const Reference< XMultiServiceFactory >& rSMgr ) throw( RuntimeException )
 {
-    return SlideLayoutController( rSMgr, ".uno:AssignLayout", false );
+    return static_cast< cppu::OWeakObject * >(
+        new SlideLayoutController( rSMgr, ".uno:AssignLayout", false ));
 }
 
 // --------------------------------------------------------------------
@@ -354,7 +355,8 @@ Sequence< OUString >  InsertSlideController_getSupportedServiceNames() throw( Ru
 
 Reference< XInterface > SAL_CALL InsertSlideController_createInstance( const Reference< XMultiServiceFactory >& rSMgr ) throw( RuntimeException )
 {
-    return SlideLayoutController( rSMgr, ".uno:InsertPage" , true );
+    return static_cast< cppu::OWeakObject * >(
+        new SlideLayoutController( rSMgr, ".uno:InsertPage" , true ) );
 }
 
 //========================================================================

sd/source/ui/toolpanel/ToolPanelFactory.cxx
@@ -100,7 +100,8 @@ namespace sd { namespace toolpanel
     //------------------------------------------------------------------------------------------------------------------
     Reference< XInterface > SAL_CALL ToolPanelFactory_createInstance( const Reference< XComponentContext >& i_rContext )
     {
-        return Reference< XInterface >( ToolPanelFactory( i_rContext ) );
+        return static_cast< cppu::OWeakObject * >(
+            new ToolPanelFactory( i_rContext ) );
     }
 
     //------------------------------------------------------------------------------------------------------------------

sd/source/ui/unoidl/sddetect.cxx
@@ -515,7 +515,8 @@ rtl::OUString SdFilterDetect::impl_getStaticImplementationName()
 /* Helper for registry */
 UNOREFERENCE< UNOXINTERFACE > SAL_CALL SdFilterDetect::impl_createInstance( const UNOREFERENCE< UNOXMULTISERVICEFACTORY >& xServiceManager ) throw( UNOEXCEPTION )
 {
-    return UNOREFERENCE< UNOXINTERFACE >( SdFilterDetect( xServiceManager ) );
+    return static_cast< cppu::OWeakObject * >(
+        new SdFilterDetect( xServiceManager ) );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */