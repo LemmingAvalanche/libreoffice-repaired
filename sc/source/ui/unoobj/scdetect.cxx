LemmingAvalanche/libreoffice-repaired

commit ccf126663d98ae7eca1ef186b144804ccee14318

fix some memory leaks in sc
Change-Id: I1e3b9a55561f941346cb9f553f960dc2bafbb1b6

sc/source/ui/unoobj/scdetect.cxx
@@ -930,7 +930,7 @@ OUString ScFilterDetect::impl_getStaticImplementationName()
 /* Helper for registry */
 UNOREFERENCE< UNOXINTERFACE > SAL_CALL ScFilterDetect::impl_createInstance( const UNOREFERENCE< UNOXMULTISERVICEFACTORY >& xServiceManager ) throw( UNOEXCEPTION )
 {
-    return UNOREFERENCE< UNOXINTERFACE >( *new ScFilterDetect( xServiceManager ) );
+    return UNOREFERENCE< UNOXINTERFACE >( ScFilterDetect( xServiceManager ) );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/source/filter/xml/xmlwrap.cxx
@@ -219,7 +219,7 @@ sal_uInt32 ScXMLImportWrapper::ImportFromComponent(uno::Reference<lang::XMultiSe
 
             if( !sDocName.isEmpty() )
             {
-                nReturn = *new TwoStringErrorInfo(
+                nReturn = TwoStringErrorInfo(
                                 (bMustBeSuccessfull ? SCERR_IMPORT_FILE_ROWCOL
                                                         : SCWARN_IMPORT_FILE_ROWCOL),
                                 sDocName, sErr,
@@ -228,7 +228,7 @@ sal_uInt32 ScXMLImportWrapper::ImportFromComponent(uno::Reference<lang::XMultiSe
             else
             {
                 OSL_ENSURE( bMustBeSuccessfull, "Warnings are not supported" );
-                nReturn = *new StringErrorInfo( SCERR_IMPORT_FORMAT_ROWCOL, sErr,
+                nReturn = StringErrorInfo( SCERR_IMPORT_FORMAT_ROWCOL, sErr,
                                  ERRCODE_BUTTON_OK | ERRCODE_MSG_ERROR );
             }
         }

sc/source/ui/docshell/docsh.cxx
@@ -2293,7 +2293,7 @@ sal_Bool ScDocShell::ConvertTo( SfxMedium &rMed )
             aImExport.SetStreamPath( rMed.GetName() );
             bRet = aImExport.ExportStream( *pStream, rMed.GetBaseURL( true ), SOT_FORMATSTR_ID_HTML );
             if ( bRet && aImExport.GetNonConvertibleChars().Len() )
-                SetError( *new StringErrorInfo(
+                SetError( StringErrorInfo(
                     SCWARN_EXPORT_NONCONVERTIBLE_CHARS,
                     aImExport.GetNonConvertibleChars(),
                     ERRCODE_BUTTON_OK | ERRCODE_MSG_INFO ), ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ) );

sc/source/ui/docshell/docsh8.cxx
@@ -1156,12 +1156,12 @@ sal_uLong ScDocShell::DBaseExport( const rtl::OUString& rFullFileName, CharSet e
             }
             String sPosition( ScAddress( nDocCol, nDocRow, nTab).GetColRowString());
             String sEncoding( SvxTextEncodingTable().GetTextString( eCharSet));
-            nErr = *new TwoStringErrorInfo( (bEncErr ? SCERR_EXPORT_ENCODING :
+            nErr = TwoStringErrorInfo( (bEncErr ? SCERR_EXPORT_ENCODING :
                         SCERR_EXPORT_FIELDWIDTH), sPosition, sEncoding,
                     ERRCODE_BUTTON_OK | ERRCODE_MSG_ERROR);
         }
         else if ( !aException.Message.isEmpty() )
-            nErr = *new StringErrorInfo( (SCERR_EXPORT_SQLEXCEPTION), aException.Message, ERRCODE_BUTTON_OK | ERRCODE_MSG_ERROR);
+            nErr = StringErrorInfo( (SCERR_EXPORT_SQLEXCEPTION), aException.Message, ERRCODE_BUTTON_OK | ERRCODE_MSG_ERROR);
         else
             nErr = SCERR_EXPORT_DATA;
     }

commit 6b222b2467d97bdbe1443cd85e2920d9fff9f2bf

UNO objects must be allocated on the heap
This was a regression introduced with ccf126663d98ae7eca1ef186b144804ccee14318
"fix some memory leaks in sc."

Change-Id: I482516d31b857801c321ed252d19f06b4b11542e

sc/source/ui/unoobj/scdetect.cxx
@@ -930,7 +930,8 @@ OUString ScFilterDetect::impl_getStaticImplementationName()
 /* Helper for registry */
 UNOREFERENCE< UNOXINTERFACE > SAL_CALL ScFilterDetect::impl_createInstance( const UNOREFERENCE< UNOXMULTISERVICEFACTORY >& xServiceManager ) throw( UNOEXCEPTION )
 {
-    return UNOREFERENCE< UNOXINTERFACE >( ScFilterDetect( xServiceManager ) );
+    return static_cast< cppu::OWeakObject * >(
+        new ScFilterDetect( xServiceManager ) );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */