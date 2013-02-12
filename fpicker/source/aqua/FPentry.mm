LemmingAvalanche/libreoffice-repaired

commit e1b92b5dc1f1378e35b740d52f45e0adc080a986

fdo#46808, Adapt ui::dialogs::FilePicker UNO service to new style
Change-Id: I1cafbfc53994e5d74241042dbd1d292ddbda67d5

fpicker/source/aqua/SalAquaFilePicker.hxx
@@ -20,13 +20,9 @@
 #ifndef _SALAQUAFILEPICKER_HXX_
 #define _SALAQUAFILEPICKER_HXX_
 
-#include <cppuhelper/compbase8.hxx>
-#include <com/sun/star/util/XCancellable.hpp>
-#include <com/sun/star/lang/XInitialization.hpp>
+#include <cppuhelper/compbase3.hxx>
 #include <com/sun/star/lang/XServiceInfo.hpp>
-#include <com/sun/star/ui/dialogs/XFilePickerNotifier.hpp>
-#include <com/sun/star/ui/dialogs/XFilterManager.hpp>
-#include <com/sun/star/ui/dialogs/XFilterGroupManager.hpp>
+#include <com/sun/star/ui/dialogs/XFilePicker3.hpp>
 #include <com/sun/star/ui/dialogs/XFilePickerControlAccess.hpp>
 #include <com/sun/star/beans/StringPair.hpp>
 
@@ -53,14 +49,9 @@
 
 class SalAquaFilePicker :
     public SalAquaPicker,
-    public cppu::WeakComponentImplHelper8<
-    ::com::sun::star::ui::dialogs::XFilterManager,
-    ::com::sun::star::ui::dialogs::XFilterGroupManager,
+    public cppu::WeakComponentImplHelper3<
+    ::com::sun::star::ui::dialogs::XFilePicker3,
     ::com::sun::star::ui::dialogs::XFilePickerControlAccess,
-    ::com::sun::star::ui::dialogs::XFilePickerNotifier,
-    ::com::sun::star::lang::XInitialization,
-    ::com::sun::star::util::XCancellable,
-    ::com::sun::star::lang::XEventListener,
     ::com::sun::star::lang::XServiceInfo >
 {
 public:

fpicker/source/aqua/SalAquaFilePicker.mm
@@ -92,11 +92,12 @@ namespace
 // constructor
 //-----------------------------------------------------------------------------------------
 
-SalAquaFilePicker::SalAquaFilePicker( const uno::Reference<lang::XMultiServiceFactory>& xServiceMgr ) :
-cppu::WeakComponentImplHelper8<XFilterManager, XFilterGroupManager, XFilePickerControlAccess, XFilePickerNotifier,
- lang::XInitialization, util::XCancellable, lang::XEventListener, lang::XServiceInfo>( m_rbHelperMtx )
-, m_xServiceMgr( xServiceMgr )
-, m_pFilterHelper( NULL )
+SalAquaFilePicker::SalAquaFilePicker( const uno::Reference<lang::XMultiServiceFactory>& xServiceMgr )
+  : cppu::WeakComponentImplHelper2<
+             XFilePicker3,
+             lang::XServiceInfo>( m_rbHelperMtx )
+   , m_xServiceMgr( xServiceMgr )
+   , m_pFilterHelper( NULL )
 {
     DBG_PRINT_ENTRY(CLASS_NAME, __func__);
 

basctl/source/basicide/baside2.cxx
@@ -34,6 +34,7 @@
 #include <com/sun/star/script/ModuleType.hpp>
 #include <com/sun/star/ui/dialogs/ExtendedFilePickerElementIds.hpp>
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
+#include <com/sun/star/ui/dialogs/FilePicker.hpp>
 #include <com/sun/star/ui/dialogs/XFilePickerControlAccess.hpp>
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
 #include <comphelper/processfactory.hxx>
@@ -433,23 +434,15 @@ bool ModulWindow::LoadBasic()
     DBG_CHKTHIS( ModulWindow, 0 );
     bool bDone = false;
 
-    Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
-    Reference < XFilePicker > xFP;
-    if( xMSF.is() )
-    {
-        Sequence <Any> aServiceType(1);
-        aServiceType[0] <<= TemplateDescription::FILEOPEN_SIMPLE;
-        xFP = Reference< XFilePicker >( xMSF->createInstanceWithArguments(
-            "com.sun.star.ui.dialogs.FilePicker", aServiceType ), UNO_QUERY );
-    }
+    Reference< uno::XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
+    Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILEOPEN_SIMPLE);
 
     if ( !aCurPath.isEmpty() )
         xFP->setDisplayDirectory ( aCurPath );
 
-    Reference< XFilterManager > xFltMgr(xFP, UNO_QUERY);
-    xFltMgr->appendFilter( "BASIC" , "*.bas" );
-    xFltMgr->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), OUString( FilterMask_All ) );
-    xFltMgr->setCurrentFilter( "BASIC" );
+    xFP->appendFilter( "BASIC" , "*.bas" );
+    xFP->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), OUString( FilterMask_All ) );
+    xFP->setCurrentFilter( "BASIC" );
 
     if( xFP->execute() == RET_OK )
     {
@@ -487,15 +480,8 @@ bool ModulWindow::SaveBasicSource()
     DBG_CHKTHIS( ModulWindow, 0 );
     bool bDone = false;
 
-    Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
-    Reference < XFilePicker > xFP;
-    if( xMSF.is() )
-    {
-        Sequence <Any> aServiceType(1);
-        aServiceType[0] <<= TemplateDescription::FILESAVE_AUTOEXTENSION_PASSWORD;
-        xFP = Reference< XFilePicker >( xMSF->createInstanceWithArguments(
-                    "com.sun.star.ui.dialogs.FilePicker", aServiceType ), UNO_QUERY );
-    }
+    Reference< uno::XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
+    Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILESAVE_AUTOEXTENSION_PASSWORD);
 
     Reference< XFilePickerControlAccess > xFPControl(xFP, UNO_QUERY);
     xFPControl->enableControl(ExtendedFilePickerElementIds::CHECKBOX_PASSWORD, false);
@@ -506,10 +492,9 @@ bool ModulWindow::SaveBasicSource()
     if ( !aCurPath.isEmpty() )
         xFP->setDisplayDirectory ( aCurPath );
 
-    Reference< XFilterManager > xFltMgr(xFP, UNO_QUERY);
-    xFltMgr->appendFilter( "BASIC", "*.bas" );
-    xFltMgr->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), OUString( FilterMask_All ) );
-    xFltMgr->setCurrentFilter( "BASIC" );
+    xFP->appendFilter( "BASIC", "*.bas" );
+    xFP->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), OUString( FilterMask_All ) );
+    xFP->setCurrentFilter( "BASIC" );
 
     if( xFP->execute() == RET_OK )
     {

basctl/source/basicide/baside3.cxx
@@ -39,6 +39,7 @@
 #include <com/sun/star/ucb/SimpleFileAccess.hpp>
 #include <com/sun/star/ui/dialogs/ExtendedFilePickerElementIds.hpp>
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
+#include <com/sun/star/ui/dialogs/FilePicker.hpp>
 #include <com/sun/star/ui/dialogs/XFilePickerControlAccess.hpp>
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
 #include <comphelper/processfactory.hxx>
@@ -689,15 +690,8 @@ bool DialogWindow::SaveDialog()
     DBG_CHKTHIS( DialogWindow, 0 );
     bool bDone = false;
 
-    Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
-    Reference < XFilePicker > xFP;
-    if( xMSF.is() )
-    {
-        Sequence <Any> aServiceType(1);
-        aServiceType[0] <<= TemplateDescription::FILESAVE_AUTOEXTENSION_PASSWORD;
-        xFP = Reference< XFilePicker >( xMSF->createInstanceWithArguments(
-            "com.sun.star.ui.dialogs.FilePicker", aServiceType ), UNO_QUERY );
-    }
+    Reference< XComponentContext > xContext( comphelper::getProcessComponentContext() );
+    Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILESAVE_AUTOEXTENSION_PASSWORD);
 
     Reference< XFilePickerControlAccess > xFPControl(xFP, UNO_QUERY);
     xFPControl->enableControl(ExtendedFilePickerElementIds::CHECKBOX_PASSWORD, false);
@@ -711,10 +705,9 @@ bool DialogWindow::SaveDialog()
     xFP->setDefaultName( OUString( GetName() ) );
 
     OUString aDialogStr(IDE_RESSTR(RID_STR_STDDIALOGNAME));
-    Reference< XFilterManager > xFltMgr(xFP, UNO_QUERY);
-    xFltMgr->appendFilter( aDialogStr, String( "*.xdl" ) );
-    xFltMgr->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), String( FilterMask_All ) );
-    xFltMgr->setCurrentFilter( aDialogStr );
+    xFP->appendFilter( aDialogStr, String( "*.xdl" ) );
+    xFP->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), String( FilterMask_All ) );
+    xFP->setCurrentFilter( aDialogStr );
 
     if( xFP->execute() == RET_OK )
     {
@@ -723,11 +716,10 @@ bool DialogWindow::SaveDialog()
 
         // export dialog model to xml
         Reference< container::XNameContainer > xDialogModel = GetDialog();
-        Reference< XComponentContext > xContext( comphelper::getProcessComponentContext() );
         Reference< XInputStreamProvider > xISP = ::xmlscript::exportDialogModel( xDialogModel, xContext, GetDocument().isDocument() ? GetDocument().getDocument() : Reference< frame::XModel >() );
         Reference< XInputStream > xInput( xISP->createInputStream() );
 
-        Reference< XSimpleFileAccess3 > xSFI( SimpleFileAccess::create(comphelper::getProcessComponentContext()) );
+        Reference< XSimpleFileAccess3 > xSFI( SimpleFileAccess::create(xContext) );
 
         Reference< XOutputStream > xOutput;
         try
@@ -950,14 +942,8 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
     bool bDone = false;
 
     Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
-    Reference < XFilePicker > xFP;
-    if( xMSF.is() )
-    {
-        Sequence <Any> aServiceType(1);
-        aServiceType[0] <<= TemplateDescription::FILEOPEN_SIMPLE;
-        xFP = Reference< XFilePicker >( xMSF->createInstanceWithArguments(
-            "com.sun.star.ui.dialogs.FilePicker", aServiceType ), UNO_QUERY );
-    }
+    Reference< XComponentContext > xContext( comphelper::getComponentContext( xMSF ) );
+    Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILEOPEN_SIMPLE);
 
     Reference< XFilePickerControlAccess > xFPControl(xFP, UNO_QUERY);
     xFPControl->enableControl(ExtendedFilePickerElementIds::CHECKBOX_PASSWORD, false);
@@ -970,10 +956,9 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
         xFP->setDisplayDirectory ( aCurPath );
 
     OUString aDialogStr(IDE_RESSTR(RID_STR_STDDIALOGNAME));
-    Reference< XFilterManager > xFltMgr(xFP, UNO_QUERY);
-    xFltMgr->appendFilter( aDialogStr, String( "*.xdl" ) );
-    xFltMgr->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), String( FilterMask_All ) );
-    xFltMgr->setCurrentFilter( aDialogStr );
+    xFP->appendFilter( aDialogStr, String( "*.xdl" ) );
+    xFP->appendFilter( IDE_RESSTR(RID_STR_FILTER_ALLFILES), String( FilterMask_All ) );
+    xFP->setCurrentFilter( aDialogStr );
 
     if( xFP->execute() == RET_OK )
     {
@@ -998,8 +983,6 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
             if( xSFI->exists( aCurPath ) )
                 xInput = xSFI->openFileRead( aCurPath );
 
-            Reference< XComponentContext > xContext(
-                comphelper::getComponentContext( xMSF ) );
             ::xmlscript::importDialogModel( xInput, xDialogModel, xContext, rDocument.isDocument() ? rDocument.getDocument() : Reference< frame::XModel >() );
 
             OUString aXmlDlgName;

basctl/source/basicide/moduldl2.cxx
@@ -45,7 +45,7 @@
 
 #include <sot/storage.hxx>
 #include <com/sun/star/io/Pipe.hpp>
-#include <com/sun/star/ui/dialogs/XFilePicker.hpp>
+#include <com/sun/star/ui/dialogs/FilePicker.hpp>
 #include <com/sun/star/ui/dialogs/FolderPicker.hpp>
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
@@ -781,16 +781,10 @@ void LibPage::NewLib()
 
 void LibPage::InsertLib()
 {
-    // file open dialog
     Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
-    Reference < XFilePicker > xFP;
-    if( xMSF.is() )
-    {
-        Sequence <Any> aServiceType(1);
-        aServiceType[0] <<= TemplateDescription::FILEOPEN_SIMPLE;
-        xFP = Reference< XFilePicker >( xMSF->createInstanceWithArguments(
-                    "com.sun.star.ui.dialogs.FilePicker", aServiceType ), UNO_QUERY );
-    }
+    Reference< uno::XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
+    // file open dialog
+    Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILEOPEN_SIMPLE);
     xFP->setTitle( String( IDEResId( RID_STR_APPENDLIBS ) ) );
 
     // filter
@@ -852,27 +846,24 @@ void LibPage::InsertLib()
             aDlgURLObj.setBase( aDlgBase );
         }
 
-        if ( xMSF.is() )
+        Reference< XSimpleFileAccess3 > xSFA( SimpleFileAccess::create(comphelper::getProcessComponentContext()) );
+
+        OUString aModURL( aModURLObj.GetMainURL( INetURLObject::NO_DECODE ) );
+        if ( xSFA->exists( aModURL ) )
         {
-            Reference< XSimpleFileAccess3 > xSFA( SimpleFileAccess::create(comphelper::getProcessComponentContext()) );
+            Sequence <Any> aSeqModURL(1);
+            aSeqModURL[0] <<= aModURL;
+            xModLibContImport = Reference< script::XLibraryContainer2 >( xMSF->createInstanceWithArguments(
+                        "com.sun.star.script.DocumentScriptLibraryContainer", aSeqModURL ), UNO_QUERY );
+        }
 
-            OUString aModURL( aModURLObj.GetMainURL( INetURLObject::NO_DECODE ) );
-            if ( xSFA->exists( aModURL ) )
-            {
-                Sequence <Any> aSeqModURL(1);
-                aSeqModURL[0] <<= aModURL;
-                xModLibContImport = Reference< script::XLibraryContainer2 >( xMSF->createInstanceWithArguments(
-                            "com.sun.star.script.DocumentScriptLibraryContainer", aSeqModURL ), UNO_QUERY );
-            }
-
-            OUString aDlgURL( aDlgURLObj.GetMainURL( INetURLObject::NO_DECODE ) );
-            if ( xSFA->exists( aDlgURL ) )
-            {
-                Sequence <Any> aSeqDlgURL(1);
-                aSeqDlgURL[0] <<= aDlgURL;
-                xDlgLibContImport = Reference< script::XLibraryContainer2 >( xMSF->createInstanceWithArguments(
-                            "com.sun.star.script.DocumentDialogLibraryContainer", aSeqDlgURL ), UNO_QUERY );
-            }
+        OUString aDlgURL( aDlgURLObj.GetMainURL( INetURLObject::NO_DECODE ) );
+        if ( xSFA->exists( aDlgURL ) )
+        {
+            Sequence <Any> aSeqDlgURL(1);
+            aSeqDlgURL[0] <<= aDlgURL;
+            xDlgLibContImport = Reference< script::XLibraryContainer2 >( xMSF->createInstanceWithArguments(
+                        "com.sun.star.script.DocumentDialogLibraryContainer", aSeqDlgURL ), UNO_QUERY );
         }
 
         if ( xModLibContImport.is() || xDlgLibContImport.is() )
@@ -1253,16 +1244,11 @@ Reference< XProgressHandler > OLibCommandEnvironment::getProgressHandler()
 void LibPage::ExportAsPackage( const String& aLibName )
 {
     // file open dialog
-    Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
     Reference< uno::XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
     Reference< task::XInteractionHandler2 > xHandler( task::InteractionHandler::createWithParent(xContext, 0) );
     Reference< XSimpleFileAccess3 > xSFA = SimpleFileAccess::create(xContext);
 
-    Reference < XFilePicker > xFP;
-    Sequence <Any> aServiceType(1);
-    aServiceType[0] <<= TemplateDescription::FILESAVE_SIMPLE;
-    xFP = Reference< XFilePicker >( xMSF->createInstanceWithArguments(
-                "com.sun.star.ui.dialogs.FilePicker", aServiceType ), UNO_QUERY );
+    Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILESAVE_SIMPLE);
 
     xFP->setTitle( String( IDEResId( RID_STR_EXPORTPACKAGE ) ) );
 

cui/source/dialogs/insdlg.cxx
@@ -19,7 +19,7 @@
 
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
 #include <com/sun/star/ui/dialogs/ExecutableDialogResults.hpp>
-#include <com/sun/star/ui/dialogs/XFilePicker.hpp>
+#include <com/sun/star/ui/dialogs/FilePicker.hpp>
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/lang/XInitialization.hpp>
@@ -112,40 +112,28 @@ IMPL_LINK_INLINE_END( SvInsertOleDlg, DoubleClickHdl, ListBox *, pListBox )
 
 IMPL_LINK_NOARG(SvInsertOleDlg, BrowseHdl)
 {
-    Reference< XMultiServiceFactory > xFactory( ::comphelper::getProcessServiceFactory() );
-    if( xFactory.is() )
+    Reference< XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
+
+    Reference< XFilePicker3 > xFilePicker = FilePicker::createWithMode(xContext, TemplateDescription::FILEOPEN_SIMPLE);
+
+    // add filter
+    try
     {
-        Reference< XFilePicker > xFilePicker( xFactory->createInstance( "com.sun.star.ui.dialogs.FilePicker" ), UNO_QUERY );
-        DBG_ASSERT( xFilePicker.is(), "could not get FilePicker service" );
+        xFilePicker->appendFilter(
+             OUString(),
+             OUString( "*.*" )
+             );
+    }
+    catch( const IllegalArgumentException& )
+    {
+        DBG_ASSERT( 0, "caught IllegalArgumentException when registering filter\n" );
+    }
 
-        Reference< XInitialization > xInit( xFilePicker, UNO_QUERY );
-        Reference< XFilterManager > xFilterMgr( xFilePicker, UNO_QUERY );
-        if( xInit.is() && xFilePicker.is() && xFilterMgr.is() )
-        {
-            Sequence< Any > aServiceType( 1 );
-            aServiceType[0] <<= TemplateDescription::FILEOPEN_SIMPLE;
-            xInit->initialize( aServiceType );
-
-            // add filter
-            try
-            {
-                xFilterMgr->appendFilter(
-                     OUString(),
-                     OUString( "*.*" )
-                     );
-            }
-            catch( IllegalArgumentException& )
-            {
-                DBG_ASSERT( 0, "caught IllegalArgumentException when registering filter\n" );
-            }
-
-            if( xFilePicker->execute() == ExecutableDialogResults::OK )
-            {
-                Sequence< OUString > aPathSeq( xFilePicker->getFiles() );
-                INetURLObject aObj( aPathSeq[0] );
-                m_pEdFilepath->SetText( aObj.PathToFileName() );
-            }
-        }
+    if( xFilePicker->execute() == ExecutableDialogResults::OK )
+    {
+        Sequence< OUString > aPathSeq( xFilePicker->getFiles() );
+        INetURLObject aObj( aPathSeq[0] );
+        m_pEdFilepath->SetText( aObj.PathToFileName() );
     }
 
     return 0;

cui/source/options/personalization.cxx
@@ -24,7 +24,7 @@
 #include <com/sun/star/ucb/SimpleFileAccess.hpp>
 #include <com/sun/star/ui/dialogs/ExecutableDialogResults.hpp>
 #include <com/sun/star/ui/dialogs/ExtendedFilePickerElementIds.hpp>
-#include <com/sun/star/ui/dialogs/XFilePicker.hpp>
+#include <com/sun/star/ui/dialogs/FilePicker.hpp>
 #include <com/sun/star/ui/dialogs/XFilePickerControlAccess.hpp>
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
 
@@ -209,13 +209,9 @@ void SvxPersonalizationTabPage::Reset( const SfxItemSet & )
 
 IMPL_LINK( SvxPersonalizationTabPage, SelectBackground, PushButton*, /*pButton*/ )
 {
-    uno::Reference< lang::XMultiServiceFactory > xFactory( ::comphelper::getProcessServiceFactory() );
-    if ( !xFactory.is() )
-        return 0;
+    uno::Reference< uno::XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
 
-    uno::Reference< ui::dialogs::XFilePicker > xFilePicker( xFactory->createInstance( "com.sun.star.ui.dialogs.FilePicker" ), uno::UNO_QUERY );
-    if ( !xFilePicker.is() )
-        return 0;
+    uno::Reference< ui::dialogs::XFilePicker3 > xFilePicker = ui::dialogs::FilePicker::createDefault(xContext);
 
     xFilePicker->setMultiSelectionMode( false );
 
@@ -223,9 +219,7 @@ IMPL_LINK( SvxPersonalizationTabPage, SelectBackground, PushButton*, /*pButton*/
     if ( xController.is() )
         xController->setValue( ui::dialogs::ExtendedFilePickerElementIds::CHECKBOX_PREVIEW, 0, uno::makeAny( sal_True ) );
 
-    uno::Reference< ui::dialogs::XFilterManager > xFilterMgr( xFilePicker, uno::UNO_QUERY );
-    if ( xFilterMgr.is() )
-        xFilterMgr->appendFilter( "Background images (*.jpg;*.png)", "*.jpg;*.png" ); // TODO localize
+    xFilePicker->appendFilter( "Background images (*.jpg;*.png)", "*.jpg;*.png" ); // TODO localize
 
     while ( xFilePicker->execute() == ui::dialogs::ExecutableDialogResults::OK )
     {

desktop/source/deployment/gui/dp_gui_dialog2.cxx
@@ -63,7 +63,7 @@
 
 #include "com/sun/star/ui/dialogs/ExecutableDialogResults.hpp"
 #include "com/sun/star/ui/dialogs/TemplateDescription.hpp"
-#include "com/sun/star/ui/dialogs/XFilePicker.hpp"
+#include "com/sun/star/ui/dialogs/FilePicker.hpp"
 #include "com/sun/star/ui/dialogs/XFilterManager.hpp"
 
 #include "com/sun/star/uno/Any.hxx"
@@ -886,12 +886,9 @@ bool ExtMgrDialog::acceptLicense( const uno::Reference< deployment::XPackage > &
 //------------------------------------------------------------------------------
 uno::Sequence< OUString > ExtMgrDialog::raiseAddPicker()
 {
-    const uno::Any mode( static_cast< sal_Int16 >( ui::dialogs::TemplateDescription::FILEOPEN_SIMPLE ) );
     const uno::Reference< uno::XComponentContext > xContext( m_pManager->getContext() );
-    const uno::Reference< ui::dialogs::XFilePicker > xFilePicker(
-        xContext->getServiceManager()->createInstanceWithArgumentsAndContext(
-            "com.sun.star.ui.dialogs.FilePicker",
-            uno::Sequence< uno::Any >( &mode, 1 ), xContext ), uno::UNO_QUERY_THROW );
+    const uno::Reference< ui::dialogs::XFilePicker3 > xFilePicker =
+        ui::dialogs::FilePicker::createWithMode(xContext, ui::dialogs::TemplateDescription::FILEOPEN_SIMPLE);
     xFilePicker->setTitle( m_sAddPackages );
 
     if ( m_sLastFolderURL.Len() )
@@ -927,15 +924,14 @@ uno::Sequence< OUString > ExtMgrDialog::raiseAddPicker()
         }
     }
 
-    const uno::Reference< ui::dialogs::XFilterManager > xFilterManager( xFilePicker, uno::UNO_QUERY_THROW );
     // All files at top:
-    xFilterManager->appendFilter( StrAllFiles::get(), "*.*" );
+    xFilePicker->appendFilter( StrAllFiles::get(), "*.*" );
     // then supported ones:
     t_string2string::const_iterator iPos( title2filter.begin() );
     const t_string2string::const_iterator iEnd( title2filter.end() );
     for ( ; iPos != iEnd; ++iPos ) {
         try {
-            xFilterManager->appendFilter( iPos->first, iPos->second );
+            xFilePicker->appendFilter( iPos->first, iPos->second );
         }
         catch (const lang::IllegalArgumentException & exc) {
             OSL_FAIL( ::rtl::OUStringToOString(
@@ -943,7 +939,7 @@ uno::Sequence< OUString > ExtMgrDialog::raiseAddPicker()
             (void) exc;
         }
     }
-    xFilterManager->setCurrentFilter( sDefaultFilter );
+    xFilePicker->setCurrentFilter( sDefaultFilter );
 
     if ( xFilePicker->execute() != ui::dialogs::ExecutableDialogResults::OK )
         return uno::Sequence<OUString>(); // cancelled

commit 3bc5768718e9db9aa16b5277d96f01425dc7808e

Fix compilation of MacOS FilePicker
...broken by e1b92b5dc1f1378e35b740d52f45e0adc080a986 "fdo#46808,
Adapt ui::dialogs::FilePicker UNO service to new style."

Also remove unused field while we're there.

Change-Id: If7e2057b9383edf4ee2fa581df984dea0e04e430

fpicker/source/aqua/FPentry.mm
@@ -38,8 +38,7 @@ using ::com::sun::star::ui::dialogs::XFilePicker;
 static Reference< XInterface > SAL_CALL createFileInstance(
     const Reference< XMultiServiceFactory >& rServiceManager )
 {
-    return Reference< XInterface >(
-        *new SalAquaFilePicker( rServiceManager ) );
+    return Reference< XInterface >( *new SalAquaFilePicker );
 }
 
 static Reference< XInterface > SAL_CALL createFolderInstance(

fpicker/source/aqua/SalAquaFilePicker.hxx
@@ -57,7 +57,7 @@ class SalAquaFilePicker :
 public:
 
     // constructor
-    SalAquaFilePicker( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceMgr );
+    SalAquaFilePicker();
 
     //------------------------------------------------------------------------------------
     // XFilePickerNotifier
@@ -196,9 +196,6 @@ private:
     SalAquaFilePicker& operator=( const SalAquaFilePicker& );
 
     virtual void ensureFilterHelper();
-
-    // to instantiate own services
-    ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > m_xServiceMgr;
 
     ::com::sun::star::uno::Reference< ::com::sun::star::ui::dialogs::XFilePickerListener >
     m_xListener;

fpicker/source/aqua/SalAquaFilePicker.mm
@@ -92,11 +92,11 @@ namespace
 // constructor
 //-----------------------------------------------------------------------------------------
 
-SalAquaFilePicker::SalAquaFilePicker( const uno::Reference<lang::XMultiServiceFactory>& xServiceMgr )
-  : cppu::WeakComponentImplHelper2<
+SalAquaFilePicker::SalAquaFilePicker()
+  : cppu::WeakComponentImplHelper3<
              XFilePicker3,
+             XFilePickerControlAccess,
              lang::XServiceInfo>( m_rbHelperMtx )
-   , m_xServiceMgr( xServiceMgr )
    , m_pFilterHelper( NULL )
 {
     DBG_PRINT_ENTRY(CLASS_NAME, __func__);