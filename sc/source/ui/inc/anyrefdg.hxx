LemmingAvalanche/libreoffice-repaired

commit bb9c5a9795509640ff0c925cafe2ba6822ef75e8

Remove excessive inclusion of cell.hxx from other headers.
In favor of forward delaration and/or inclusion of more specialized
headers.

Change-Id: I75f727cc50228b8958b60173ce14e5ce1fb7e6f7

sc/source/ui/inc/anyrefdg.hxx
@@ -26,7 +26,6 @@
 #include <sfx2/basedlgs.hxx>
 #include <sfx2/tabdlg.hxx>
 #include "address.hxx"
-#include "cell.hxx"
 #include "compiler.hxx"
 #include "formula/funcutl.hxx"
 #include "IAnyRefDialog.hxx"
@@ -41,6 +40,7 @@ class ScDocument;
 class ScTabViewShell;
 class ScRefHandler;
 class ScRangeList;
+class ScFormulaCell;
 
 class ScFormulaReferenceHelper
 {

sc/source/core/data/documen4.cxx
@@ -41,6 +41,7 @@
 #include "externalrefmgr.hxx"
 #include "colorscale.hxx"
 #include "attrib.hxx"
+#include "cell.hxx"
 
 using namespace formula;
 

sc/source/core/inc/interpre.hxx
@@ -24,18 +24,20 @@
 #include <rtl/math.hxx>
 #include <rtl/ustring.hxx>
 #include "formula/errorcodes.hxx"
-#include "cell.hxx"
+#include "formula/tokenarray.hxx"
 #include "scdll.hxx"
 #include "document.hxx"
 #include "scmatrix.hxx"
 #include "externalrefmgr.hxx"
 #include "calcconfig.hxx"
+#include "token.hxx"
 
 #include <map>
 
 class ScDocument;
 class SbxVariable;
 class ScBaseCell;
+class ScValueCell;
 class ScFormulaCell;
 class SvNumberFormatter;
 class ScDBRangeBase;
@@ -45,8 +47,11 @@ struct ScQueryEntry;
 
 struct ScCompare;
 struct ScCompareOptions;
+class ScSingleRefData;
+class ScComplexRefData;
 
 class ScToken;
+class ScJumpMatrix;
 
 #define MAXSTACK      (4096 / sizeof(formula::FormulaToken*))
 
@@ -187,19 +192,11 @@ double GetValueCellValue( const ScAddress&, const ScValueCell* );
 ScBaseCell* GetCell( const ScAddress& rPos )
     { return pDok->GetCell( rPos ); }
 void GetCellString( String& rStr, const ScBaseCell* pCell );
-inline sal_uInt16 GetCellErrCode( const ScBaseCell* pCell )
-    { return pCell ? pCell->GetErrorCode() : 0; }
-inline CellType GetCellType( const ScBaseCell* pCell )
-    { return pCell ? pCell->GetCellType() : CELLTYPE_NONE; }
-/// Really empty or inherited emptiness.
-inline bool HasCellEmptyData( const ScBaseCell* pCell )
-    { return pCell ? pCell->HasEmptyData() : true; }
-/// This includes inherited emptiness, which usually is regarded as value!
-inline bool HasCellValueData( const ScBaseCell* pCell )
-    { return pCell ? pCell->HasValueData() : false; }
-/// Not empty and not value.
-inline bool HasCellStringData( const ScBaseCell* pCell )
-    { return pCell ? pCell->HasStringData() : false; }
+sal_uInt16 GetCellErrCode( const ScBaseCell* pCell );
+CellType GetCellType( const ScBaseCell* pCell );
+bool HasCellEmptyData( const ScBaseCell* pCell );
+bool HasCellValueData( const ScBaseCell* pCell );
+bool HasCellStringData( const ScBaseCell* pCell );
 
 bool CreateDoubleArr(SCCOL nCol1, SCROW nRow1, SCTAB nTab1,
                      SCCOL nCol2, SCROW nRow2, SCTAB nTab2, sal_uInt8* pCellArr);

sc/source/core/tool/interpr4.cxx
@@ -191,6 +191,30 @@ double ScInterpreter::GetValueCellValue( const ScAddress& rPos, const ScValueCel
     return fVal;
 }
 
+sal_uInt16 ScInterpreter::GetCellErrCode( const ScBaseCell* pCell )
+{
+    return pCell ? pCell->GetErrorCode() : 0;
+}
+
+CellType ScInterpreter::GetCellType( const ScBaseCell* pCell )
+{
+    return pCell ? pCell->GetCellType() : CELLTYPE_NONE;
+}
+
+bool ScInterpreter::HasCellEmptyData( const ScBaseCell* pCell )
+{
+    return pCell ? pCell->HasEmptyData() : true;
+}
+
+bool ScInterpreter::HasCellValueData( const ScBaseCell* pCell )
+{
+    return pCell ? pCell->HasValueData() : false;
+}
+
+bool ScInterpreter::HasCellStringData( const ScBaseCell* pCell )
+{
+    return pCell ? pCell->HasStringData() : false;
+}
 
 /** Convert string content to numeric value.
 

sc/source/filter/excel/xestream.cxx
@@ -37,6 +37,7 @@
 #include "xcl97rec.hxx"
 #include "rangelst.hxx"
 #include "compiler.hxx"
+#include "cell.hxx"
 
 #include <../../ui/inc/docsh.hxx>
 #include <../../ui/inc/viewdata.hxx>

sc/source/filter/inc/biff.hxx
@@ -24,7 +24,6 @@
 #include <sal/config.h>
 #include "filter.hxx"
 #include "document.hxx"
-#include "cell.hxx"
 
 #include "flttypes.hxx"
 #include "ftools.hxx"

sc/source/filter/inc/excrecds.hxx
@@ -36,7 +36,6 @@
 #include "xestring.hxx"
 #include "root.hxx"
 #include "excdefs.hxx"
-#include "cell.hxx"
 #include <boost/shared_ptr.hpp>
 
 //------------------------------------------------------------------ Forwards -

sc/source/filter/inc/qpro.hxx
@@ -23,7 +23,6 @@
 #include <sal/config.h>
 #include "filter.hxx"
 #include "document.hxx"
-#include "cell.hxx"
 #include <tools/string.hxx>
 
 #include "flttypes.hxx"

commit 2c5f057855ec11affa6c530b860321efeba40081

avoid MSVC C4150 , caused by bb9c5a9795

sc/source/ui/inc/anyrefdg.hxx
@@ -26,6 +26,7 @@
 #include <sfx2/basedlgs.hxx>
 #include <sfx2/tabdlg.hxx>
 #include "address.hxx"
+#include "cell.hxx"
 #include "compiler.hxx"
 #include "formula/funcutl.hxx"
 #include "IAnyRefDialog.hxx"
@@ -40,7 +41,6 @@ class ScDocument;
 class ScTabViewShell;
 class ScRefHandler;
 class ScRangeList;
-class ScFormulaCell;
 
 class ScFormulaReferenceHelper
 {