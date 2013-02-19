LemmingAvalanche/libreoffice-repaired

commit 39d5a6e6f55f84e14570163a129474ca626b605c

fdo#59928: sw: fix mouse selection of fields:
SwEditWin::MouseButtonDown: the selection of fields was messed up by a
call to rSh.SetCursor near the bottom of the method. This used to be
fixed up by SwEditWin::MouseButtonUp previously, but that was broken
with commit ec9d841c7f2e66c524563c75584f3628d39b60ee.

To fix that move the field selection code to a place where it prevents
the other code from running, in the hope that it is not necessary.

Also it does not make any sense to select whole words when clicking on a
field, just selecting the field seems much better.

Change-Id: I8d604450789844b6a446cf3a35f62ed530d0328e

sw/source/ui/docvw/edtwin.cxx
@@ -131,6 +131,7 @@
 
 #include <IMark.hxx>
 #include <doc.hxx>
+#include <txatbase.hxx> // FIXME this sucks
 #include <xmloff/odffields.hxx>
 
 #include <PostItMgr.hxx>
@@ -3210,22 +3211,6 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                             if (aVEvt.eEvent == SDREVENT_EXECUTEURL)
                                 bExecDrawTextLink = true;
                         }
-
-                        SwContentAtPos aFieldAtPos ( SwContentAtPos::SW_FIELD );
-
-                        // Are we selecting a field?
-                        if ( rSh.GetContentAtPos( aDocPos, aFieldAtPos ) )
-                        {
-                            // select work, AdditionalMode if applicable
-                            if ( KEY_MOD1 == rMEvt.GetModifier() && !rSh.IsAddMode() )
-                            {
-                                rSh.EnterAddMode();
-                                rSh.SelWrd( &aDocPos );
-                                rSh.LeaveAddMode();
-                            }
-                            else
-                                rSh.SelWrd( &aDocPos );
-                        }
                         break;
                     }
                     case 2:
@@ -3561,6 +3546,47 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                 {
                     rSh.GCAttr();
                     rSh.ClearGCAttr();
+                }
+
+                SwContentAtPos aFieldAtPos(SwContentAtPos::SW_FIELD);
+
+                // Are we clicking on a field?
+                if (rSh.GetContentAtPos(aDocPos, aFieldAtPos))
+                {
+                    bool bAddMode(false); // AdditionalMode if applicable
+                    if (KEY_MOD1 == rMEvt.GetModifier() && !rSh.IsAddMode())
+                    {
+                        bAddMode = true;
+                        rSh.EnterAddMode();
+                    }
+                    rSh.SetCursor(&aDocPos, bOnlyText);
+                    // Select the field. Unfortunately cursor may be on field
+                    // position or on position after field depending on which
+                    // half of the field was clicked on.
+                    SwTxtAttr const*const pTxtFld(aFieldAtPos.pFndTxtAttr);
+                    if (rSh.GetCurrentShellCursor().GetPoint()->nContent
+                            .GetIndex() == *pTxtFld->GetStart())
+                    {
+                        rSh.Right( CRSR_SKIP_CHARS, true, 1, false );
+                        rSh.NormalizePam();
+                    }
+                    else
+                    {
+                        assert(rSh.GetCurrentShellCursor().GetPoint()->nContent
+                                .GetIndex() == (*pTxtFld->GetStart() + 1));
+                        rSh.Left( CRSR_SKIP_CHARS, true, 1, false );
+                    }
+                    // it's a bit of a mystery what this is good for?
+                    // in this case we assume it's valid since we just
+                    // selected a field
+                    bValidCrsrPos = true;
+                    if (bAddMode)
+                    {
+                        rSh.LeaveAddMode();
+                    }
+                    // don't go into the !bOverSelect block below - it moves
+                    // the cursor
+                    break;
                 }
 
                 sal_Bool bOverSelect = rSh.ChgCurrPam( aDocPos ), bOverURLGrf = sal_False;

commit 0cb5e502ffb933cca8df840fc580ca64480fde02

fdo#59928: sw: fix mouse selection of fields differently
Unfortunately selecting the field in SwEditWin::MouseButtonDown()
prevents the click field handling code in SwEditWin::MouseButtonUp()
from running.  Instead select the field in MouseButtonUp and
SwWrtShell::ClickToField(), which seems to work better.
(regression from 39d5a6e6f55f84e14570163a129474ca626b605c)

Change-Id: I3d769487b9d5225ffd07b384fbb0ce01e0733be5

sw/source/ui/docvw/edtwin.cxx
@@ -3559,36 +3559,17 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                 // Are we clicking on a field?
                 if (rSh.GetContentAtPos(aDocPos, aFieldAtPos))
                 {
-                    bool bAddMode(false); // AdditionalMode if applicable
-                    if (KEY_MOD1 == rMEvt.GetModifier() && !rSh.IsAddMode())
-                    {
-                        bAddMode = true;
-                        rSh.EnterAddMode();
-                    }
                     rSh.SetCursor(&aDocPos, bOnlyText);
-                    // Select the field. Unfortunately cursor may be on field
+                    // Unfortunately the cursor may be on field
                     // position or on position after field depending on which
                     // half of the field was clicked on.
                     SwTxtAttr const*const pTxtFld(aFieldAtPos.pFndTxtAttr);
                     if (rSh.GetCurrentShellCursor().GetPoint()->nContent
-                            .GetIndex() == *pTxtFld->GetStart())
-                    {
-                        rSh.Right( CRSR_SKIP_CHARS, true, 1, false );
-                        rSh.NormalizePam();
-                    }
-                    else
+                            .GetIndex() != *pTxtFld->GetStart())
                     {
                         assert(rSh.GetCurrentShellCursor().GetPoint()->nContent
                                 .GetIndex() == (*pTxtFld->GetStart() + 1));
-                        rSh.Left( CRSR_SKIP_CHARS, true, 1, false );
-                    }
-                    // it's a bit of a mystery what this is good for?
-                    // in this case we assume it's valid since we just
-                    // selected a field
-                    bValidCrsrPos = true;
-                    if (bAddMode)
-                    {
-                        rSh.LeaveAddMode();
+                        rSh.Left( CRSR_SKIP_CHARS, false, 1, false );
                     }
                     // don't go into the !bOverSelect block below - it moves
                     // the cursor
@@ -4422,7 +4403,7 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
                         if(pApplyTempl)
                             bExecHyperlinks = sal_False;
 
-                        SwContentAtPos aCntntAtPos( SwContentAtPos::SW_CLICKFIELD |
+                        SwContentAtPos aCntntAtPos( SwContentAtPos::SW_FIELD |
                                                     SwContentAtPos::SW_INETATTR |
                                                     SwContentAtPos::SW_SMARTTAG  | SwContentAtPos::SW_FORMCTRL);
 
@@ -4441,7 +4422,23 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
 
                             if( SwContentAtPos::SW_FIELD == aCntntAtPos.eCntntAtPos )
                             {
+                                bool bAddMode(false);
+                                // AdditionalMode if applicable
+                                if (KEY_MOD1 == rMEvt.GetModifier()
+                                    && !rSh.IsAddMode())
+                                {
+                                    bAddMode = true;
+                                    rSh.EnterAddMode();
+                                }
                                 rSh.ClickToField( *aCntntAtPos.aFnd.pFld );
+                                // a bit of a mystery what this is good for?
+                                // in this case we assume it's valid since we
+                                // just selected a field
+                                bValidCrsrPos = true;
+                                if (bAddMode)
+                                {
+                                    rSh.LeaveAddMode();
+                                }
                             }
                             else if ( SwContentAtPos::SW_SMARTTAG == aCntntAtPos.eCntntAtPos )
                             {

sw/source/ui/wrtsh/wrtsh2.cxx
@@ -236,6 +236,15 @@ sal_Bool SwWrtShell::UpdateTableOf(const SwTOXBase& rTOX, const SfxItemSet* pSet
 
 void SwWrtShell::ClickToField( const SwField& rFld )
 {
+    // cross reference field must not be selected because it moves the cursor
+    if (RES_GETREFFLD != rFld.GetTyp()->Which())
+    {
+        StartAllAction();
+        Right( CRSR_SKIP_CHARS, true, 1, false ); // Select the field.
+        NormalizePam();
+        EndAllAction();
+    }
+
     bIsInClickToEdit = true;
     switch( rFld.GetTyp()->Which() )
     {
@@ -256,11 +265,6 @@ void SwWrtShell::ClickToField( const SwField& rFld )
             case JE_FMT_OLE:        nSlotId = SID_INSERT_OBJECT;        break;
 
             }
-
-            StartAllAction();
-            Right( CRSR_SKIP_CHARS, true, 0, false ); // Select the field.
-            NormalizePam();   // Cursor at the beginning of it.
-            EndAllAction();
 
             if( nSlotId )
             {