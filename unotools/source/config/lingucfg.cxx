LemmingAvalanche/libreoffice-repaired

commit 9fe2d09db5dd2c05d3111adfa7e2410f88acc1af

Remove RTL_CONSTASCII_STRINGPARAM (unotools)
Change-Id: I2911e50ddcd007d80498b9d65efd14368ac3baca

unotools/source/config/lingucfg.cxx
@@ -895,7 +895,7 @@ sal_Bool SvtLinguConfig::GetElementNamesFor(
     try
     {
         uno::Reference< container::XNameAccess > xNA( GetMainUpdateAccess(), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ServiceManager"))), uno::UNO_QUERY_THROW );
+        xNA.set( xNA->getByName(OUString("ServiceManager")), uno::UNO_QUERY_THROW );
         xNA.set( xNA->getByName( rNodeName ), uno::UNO_QUERY_THROW );
         rElementNames = xNA->getElementNames();
         bSuccess = true;
@@ -917,10 +917,10 @@ sal_Bool SvtLinguConfig::GetSupportedDictionaryFormatsFor(
     try
     {
         uno::Reference< container::XNameAccess > xNA( GetMainUpdateAccess(), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ServiceManager"))), uno::UNO_QUERY_THROW );
+        xNA.set( xNA->getByName(OUString("ServiceManager")), uno::UNO_QUERY_THROW );
         xNA.set( xNA->getByName( rSetName ), uno::UNO_QUERY_THROW );
         xNA.set( xNA->getByName( rSetEntry ), uno::UNO_QUERY_THROW );
-        if (xNA->getByName( rtl::OUString(aG_SupportedDictionaryFormats) ) >>= rFormatList)
+        if (xNA->getByName( OUString(aG_SupportedDictionaryFormats) ) >>= rFormatList)
             bSuccess = true;
         DBG_ASSERT( rFormatList.getLength(), "supported dictionary format list is empty" );
     }
@@ -953,8 +953,8 @@ static bool lcl_GetFileUrlFromOrigin(
     bool bSuccess = false;
     if (!rOrigin.isEmpty())
     {
-        rtl::OUString aURL( rOrigin );
-        if (( aURL.compareToAscii( RTL_CONSTASCII_STRINGPARAM( EXPAND_PROTOCOL )) == 0 ) &&
+        OUString aURL( rOrigin );
+        if (( aURL.compareTo( EXPAND_PROTOCOL ) == 0 ) &&
             rxMacroExpander.is() )
         {
             // cut protocol
@@ -964,7 +964,7 @@ static bool lcl_GetFileUrlFromOrigin(
             // expand macro string
             aURL = rxMacroExpander->expandMacros( aMacro );
 
-            bool bIsFileUrl = aURL.compareToAscii( RTL_CONSTASCII_STRINGPARAM( FILE_PROTOCOL )) == 0;
+            bool bIsFileUrl = aURL.compareTo( FILE_PROTOCOL ) == 0;
             if (bIsFileUrl)
             {
                 rFileUrl = aURL;
@@ -994,17 +994,17 @@ sal_Bool SvtLinguConfig::GetDictionaryEntry(
     try
     {
         uno::Reference< container::XNameAccess > xNA( GetMainUpdateAccess(), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ServiceManager"))), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName(rtl::OUString(aG_Dictionaries)), uno::UNO_QUERY_THROW );
+        xNA.set( xNA->getByName(OUString("ServiceManager")), uno::UNO_QUERY_THROW );
+        xNA.set( xNA->getByName(OUString(aG_Dictionaries)), uno::UNO_QUERY_THROW );
         xNA.set( xNA->getByName( rNodeName ), uno::UNO_QUERY_THROW );
 
         // read group data...
-        uno::Sequence< rtl::OUString >  aLocations;
-        rtl::OUString                   aFormatName;
-        uno::Sequence< rtl::OUString >  aLocaleNames;
-        bSuccess =  (xNA->getByName( rtl::OUString(aG_Locations) ) >>= aLocations)  &&
-                    (xNA->getByName( rtl::OUString(aG_Format) )    >>= aFormatName) &&
-                    (xNA->getByName( rtl::OUString(aG_Locales) )   >>= aLocaleNames);
+        uno::Sequence< OUString >  aLocations;
+        OUString                   aFormatName;
+        uno::Sequence< OUString >  aLocaleNames;
+        bSuccess =  (xNA->getByName( OUString(aG_Locations) ) >>= aLocations)  &&
+                    (xNA->getByName( OUString(aG_Format) )    >>= aFormatName) &&
+                    (xNA->getByName( OUString(aG_Locales) )   >>= aLocaleNames);
         DBG_ASSERT( aLocations.getLength(), "Dictionary locations not set" );
         DBG_ASSERT( !aFormatName.isEmpty(), "Dictionary format name not set" );
         DBG_ASSERT( aLocaleNames.getLength(), "No locales set for the dictionary" );
@@ -1042,8 +1042,8 @@ uno::Sequence< rtl::OUString > SvtLinguConfig::GetDisabledDictionaries() const
     try
     {
         uno::Reference< container::XNameAccess > xNA( GetMainUpdateAccess(), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ServiceManager"))), uno::UNO_QUERY_THROW );
-        xNA->getByName( rtl::OUString(aG_DisabledDictionaries) ) >>= aResult;
+        xNA.set( xNA->getByName(OUString("ServiceManager")), uno::UNO_QUERY_THROW );
+        xNA->getByName( OUString(aG_DisabledDictionaries) ) >>= aResult;
     }
     catch (uno::Exception &)
     {
@@ -1052,7 +1052,7 @@ uno::Sequence< rtl::OUString > SvtLinguConfig::GetDisabledDictionaries() const
 }
 
 std::vector< SvtLinguConfigDictionaryEntry > SvtLinguConfig::GetActiveDictionariesByFormat(
-    const rtl::OUString &rFormatName )
+    const OUString &rFormatName )
 {
     std::vector< SvtLinguConfigDictionaryEntry > aRes;
     if (rFormatName.isEmpty())
@@ -1115,13 +1115,13 @@ uno::Reference< util::XChangesBatch > SvtLinguConfig::GetMainUpdateAccess() cons
 
             // get configuration update access
             beans::PropertyValue aValue;
-            aValue.Name  = rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("nodepath"));
-            aValue.Value = uno::makeAny(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("org.openoffice.Office.Linguistic")));
+            aValue.Name  = OUString("nodepath");
+            aValue.Value = uno::makeAny(OUString("org.openoffice.Office.Linguistic"));
             uno::Sequence< uno::Any > aProps(1);
             aProps[0] <<= aValue;
             m_xMainUpdateAccess = uno::Reference< util::XChangesBatch >(
                     xConfigurationProvider->createInstanceWithArguments(
-                        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.configuration.ConfigurationUpdateAccess")), aProps),
+                        OUString("com.sun.star.configuration.ConfigurationUpdateAccess"), aProps),
                         uno::UNO_QUERY_THROW );
         }
         catch (uno::Exception &)
@@ -1141,16 +1141,16 @@ rtl::OUString SvtLinguConfig::GetVendorImageUrl_Impl(
     try
     {
         uno::Reference< container::XNameAccess > xImagesNA( GetMainUpdateAccess(), uno::UNO_QUERY_THROW );
-        xImagesNA.set( xImagesNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Images"))), uno::UNO_QUERY_THROW );
+        xImagesNA.set( xImagesNA->getByName(OUString("Images")), uno::UNO_QUERY_THROW );
 
-        uno::Reference< container::XNameAccess > xNA( xImagesNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ServiceNameEntries"))), uno::UNO_QUERY_THROW );
+        uno::Reference< container::XNameAccess > xNA( xImagesNA->getByName(OUString("ServiceNameEntries")), uno::UNO_QUERY_THROW );
         xNA.set( xNA->getByName( rServiceImplName ), uno::UNO_QUERY_THROW );
-        uno::Any aAny(xNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("VendorImagesNode"))));
+        uno::Any aAny(xNA->getByName(OUString("VendorImagesNode")));
         rtl::OUString aVendorImagesNode;
         if (aAny >>= aVendorImagesNode)
         {
             xNA = xImagesNA;
-            xNA.set( xNA->getByName(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("VendorImages"))), uno::UNO_QUERY_THROW );
+            xNA.set( xNA->getByName(OUString("VendorImages")), uno::UNO_QUERY_THROW );
             xNA.set( xNA->getByName( aVendorImagesNode ), uno::UNO_QUERY_THROW );
             aAny = xNA->getByName( rImageName );
             rtl::OUString aTmp;
@@ -1220,10 +1220,10 @@ bool SvtLinguConfig::HasGrammarChecker() const
     try
     {
         uno::Reference< container::XNameAccess > xNA( GetMainUpdateAccess(), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName( rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("ServiceManager")) ), uno::UNO_QUERY_THROW );
-        xNA.set( xNA->getByName( rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("GrammarCheckerList")) ), uno::UNO_QUERY_THROW );
+        xNA.set( xNA->getByName( OUString("ServiceManager") ), uno::UNO_QUERY_THROW );
+        xNA.set( xNA->getByName( OUString("GrammarCheckerList") ), uno::UNO_QUERY_THROW );
 
-        uno::Sequence< rtl::OUString > aElementNames( xNA->getElementNames() );
+        uno::Sequence< OUString > aElementNames( xNA->getElementNames() );
         bRes = aElementNames.getLength() > 0;
     }
     catch (const uno::Exception&)

unotools/source/config/bootstrap.cxx
@@ -72,13 +72,11 @@ namespace utl
 
     namespace
     {
-        rtl::OUString makeImplName()
+        OUString makeImplName()
         {
-            rtl::OUString uri;
-            rtl::Bootstrap::get(
-                rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("BRAND_BASE_DIR")),
-                uri);
-            return uri + rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/program/" BOOTSTRAP_DATA_NAME));
+            OUString uri;
+            rtl::Bootstrap::get( OUString("BRAND_BASE_DIR"), uri);
+            return uri + "/program/" + BOOTSTRAP_DATA_NAME;
         }
     }
 
@@ -426,7 +424,7 @@ Bootstrap::PathStatus implGetBootstrapFile(rtl::Bootstrap& _rData, Bootstrap::Im
 static
 Bootstrap::PathStatus implGetVersionFile(rtl::Bootstrap& _rData, Bootstrap::Impl::PathData & _rVersionFile)
 {
-    OUString const csVersionFileItem(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_VERSIONFILE));
+    OUString const csVersionFileItem(BOOTSTRAP_ITEM_VERSIONFILE);
 
     _rData.getFrom(csVersionFileItem,_rVersionFile.path);
 
@@ -569,7 +567,7 @@ static Bootstrap::FailureCode describeError(OUStringBuffer& _rBuf, Bootstrap::Im
 
 OUString Bootstrap::getProductKey()
 {
-    OUString const csProductKeyItem(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_PRODUCT_KEY));
+    OUString const csProductKeyItem(BOOTSTRAP_ITEM_PRODUCT_KEY);
 
     OUString const sDefaultProductKey = getExecutableBaseName();
 
@@ -579,7 +577,7 @@ OUString Bootstrap::getProductKey()
 
 OUString Bootstrap::getProductKey(OUString const& _sDefault)
 {
-    OUString const csProductKeyItem(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_PRODUCT_KEY));
+    OUString const csProductKeyItem(BOOTSTRAP_ITEM_PRODUCT_KEY);
 
     return data().getBootstrapValue( csProductKeyItem, _sDefault );
 }
@@ -598,7 +596,7 @@ OUString Bootstrap::getBuildVersion(OUString const& _sDefault)
 
 OUString Bootstrap::getBuildIdData(OUString const& _sDefault)
 {
-    OUString const csBuildIdItem(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_BUILDID));
+    OUString const csBuildIdItem(BOOTSTRAP_ITEM_BUILDID);
 
     OUString sBuildId;
     // read buildid from version.ini (versionrc), if it doesn't exist or buildid is empty
@@ -632,7 +630,7 @@ PathStatus Bootstrap::locateUserInstallation(OUString& _rURL)
 
 PathStatus Bootstrap::locateUserData(OUString& _rURL)
 {
-    OUString const csUserDirItem(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_USERDIR));
+    OUString const csUserDirItem(BOOTSTRAP_ITEM_USERDIR);
 
     rtl::Bootstrap aData( data().getImplName() );
 
@@ -642,7 +640,7 @@ PathStatus Bootstrap::locateUserData(OUString& _rURL)
     }
     else
     {
-        OUString const csUserDir(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_DIRNAME_USERDIR));
+        OUString const csUserDir(BOOTSTRAP_DIRNAME_USERDIR);
         return getDerivedPath(_rURL, data().aUserInstall_ ,csUserDir, aData, csUserDirItem);
     }
 }
@@ -692,8 +690,8 @@ Bootstrap::Status Bootstrap::checkBootstrapStatus(rtl::OUString& _rDiagnosticMes
 
 bool Bootstrap::Impl::initBaseInstallationData(rtl::Bootstrap& _rData)
 {
-    OUString const csBaseInstallItem( RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_BASEINSTALLATION) );
-    OUString const csBaseInstallDefault( RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_DEFAULT_BASEINSTALL) );
+    OUString const csBaseInstallItem( BOOTSTRAP_ITEM_BASEINSTALLATION );
+    OUString const csBaseInstallDefault( BOOTSTRAP_DEFAULT_BASEINSTALL );
 
     _rData.getFrom(csBaseInstallItem, aBaseInstall_.path, csBaseInstallDefault);
 
@@ -707,7 +705,7 @@ bool Bootstrap::Impl::initBaseInstallationData(rtl::Bootstrap& _rData)
 
 bool Bootstrap::Impl::initUserInstallationData(rtl::Bootstrap& _rData)
 {
-    OUString const csUserInstallItem( RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_USERINSTALLATION) );
+    OUString const csUserInstallItem( BOOTSTRAP_ITEM_USERINSTALLATION );
 
     if (_rData.getFrom(csUserInstallItem, aUserInstall_.path))
     {
@@ -719,12 +717,12 @@ bool Bootstrap::Impl::initUserInstallationData(rtl::Bootstrap& _rData)
         aUserInstall_.status = DATA_MISSING;
 
         // .. or this - look for a single-user user directory ?
-        OUString const csUserDirItem(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_ITEM_USERDIR));
+        OUString const csUserDirItem(BOOTSTRAP_ITEM_USERDIR);
         OUString sDummy;
         // look for $BASEINSTALLATION/user only if default UserDir setting is used
         if (! _rData.getFrom(csUserDirItem, sDummy))
         {
-            OUString const csUserDir(RTL_CONSTASCII_USTRINGPARAM(BOOTSTRAP_DIRNAME_USERDIR));
+            OUString const csUserDir(BOOTSTRAP_DIRNAME_USERDIR);
 
             if ( PATH_EXISTS == getDerivedPath(sDummy, aBaseInstall_, csUserDir, _rData, csUserDirItem) )
                 aUserInstall_ = aBaseInstall_;
@@ -789,11 +787,9 @@ OUString Bootstrap::Impl::getBootstrapValue(OUString const& _sName, OUString con
 sal_Bool Bootstrap::Impl::getVersionValue(OUString const& _sName, OUString& _rValue, OUString const& _sDefault) const
 {
     // try to open version.ini (versionrc)
-    rtl::OUString uri;
-    rtl::Bootstrap::get(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("BRAND_BASE_DIR")), uri);
-    rtl::Bootstrap aData( uri +
-                          OUString(RTL_CONSTASCII_USTRINGPARAM("/program/" SAL_CONFIGFILE("version"))) );
+    OUString uri;
+    rtl::Bootstrap::get( OUString("BRAND_BASE_DIR"), uri);
+    rtl::Bootstrap aData( uri + "/program/" + SAL_CONFIGFILE("version") );
     if ( aData.getHandle() == NULL )
         // version.ini (versionrc) doesn't exist
         return sal_False;

unotools/source/config/cmdoptions.cxx
@@ -39,17 +39,17 @@ using namespace ::osl                   ;
 using namespace ::com::sun::star::uno   ;
 using namespace ::com::sun::star::beans ;
 
-#define ROOTNODE_CMDOPTIONS                             OUString(RTL_CONSTASCII_USTRINGPARAM("Office.Commands/Execute"  ))
-#define PATHDELIMITER                                   OUString(RTL_CONSTASCII_USTRINGPARAM("/"                        ))
+#define ROOTNODE_CMDOPTIONS                             OUString("Office.Commands/Execute")
+#define PATHDELIMITER                                   OUString("/")
 
-#define SETNODE_DISABLED                                OUString(RTL_CONSTASCII_USTRINGPARAM("Disabled"                 ))
+#define SETNODE_DISABLED                                OUString("Disabled")
 
-#define PROPERTYNAME_CMD                                OUString(RTL_CONSTASCII_USTRINGPARAM("Command"                  ))
+#define PROPERTYNAME_CMD                                OUString("Command")
 
 // Method to retrieve a hash code from a string. May be we have to change it to decrease collisions in the hash map
 struct OUStringHashCode
 {
-    size_t operator()( const ::rtl::OUString& sString ) const
+    size_t operator()( const OUString& sString ) const
     {
         return sString.hashCode();
     }
@@ -235,7 +235,7 @@ SvtCommandOptions_Impl::SvtCommandOptions_Impl()
     // Enable notification mechanism of ouer baseclass.
     // We need it to get information about changes outside these class on ouer used configuration keys! */
     Sequence< OUString > aNotifySeq( 1 );
-    aNotifySeq[0] = OUString( RTL_CONSTASCII_USTRINGPARAM( "Disabled" ));
+    aNotifySeq[0] = "Disabled";
     EnableNotification( aNotifySeq, sal_True );
 }
 

unotools/source/config/compatibility.cxx
@@ -42,9 +42,9 @@ using namespace ::osl;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
 
-#define ROOTNODE_OPTIONS        OUString( RTL_CONSTASCII_USTRINGPARAM( "Office.Compatibility/" ) )
-#define PATHDELIMITER           OUString( RTL_CONSTASCII_USTRINGPARAM( "/" ) )
-#define SETNODE_ALLFILEFORMATS  OUString( RTL_CONSTASCII_USTRINGPARAM( "AllFileFormats" ) )
+#define ROOTNODE_OPTIONS        OUString( "Office.Compatibility/" )
+#define PATHDELIMITER           OUString( "/" )
+#define SETNODE_ALLFILEFORMATS  OUString( "AllFileFormats" )
 
 #define PROPERTYNAME_NAME               COMPATIBILITY_PROPERTYNAME_NAME
 #define PROPERTYNAME_MODULE             COMPATIBILITY_PROPERTYNAME_MODULE

unotools/source/config/configmgr.cxx
@@ -66,19 +66,16 @@ getConfigurationProvider() {
     return css::configuration::theDefaultProvider::get( comphelper::getProcessComponentContext() );
 }
 
-rtl::OUString getConfigurationString(
-    rtl::OUString const & module, rtl::OUString const & path)
+OUString getConfigurationString(OUString const & module, OUString const & path)
 {
     css::uno::Sequence< css::uno::Any > args(1);
     args[0] <<= css::beans::NamedValue(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("nodepath")),
+        OUString("nodepath"),
         css::uno::makeAny(module));
     return
         css::uno::Reference< css::container::XHierarchicalNameAccess >(
             getConfigurationProvider()->createInstanceWithArguments(
-                rtl::OUString(
-                    RTL_CONSTASCII_USTRINGPARAM(
-                        "com.sun.star.configuration.ConfigurationAccess")),
+                OUString("com.sun.star.configuration.ConfigurationAccess"),
                 args),
             css::uno::UNO_QUERY_THROW)->
         getByHierarchicalName(path).get< rtl::OUString >();
@@ -90,77 +87,70 @@ struct theConfigManager:
 
 }
 
-rtl::OUString utl::ConfigManager::getAboutBoxProductVersion() {
+OUString utl::ConfigManager::getAboutBoxProductVersion() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(
-            RTL_CONSTASCII_USTRINGPARAM("Product/ooSetupVersionAboutBox")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooSetupVersionAboutBox"));
 }
 
-rtl::OUString utl::ConfigManager::getAboutBoxProductVersionSuffix() {
+OUString utl::ConfigManager::getAboutBoxProductVersionSuffix() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(
-            RTL_CONSTASCII_USTRINGPARAM("Product/ooSetupVersionAboutBoxSuffix")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooSetupVersionAboutBoxSuffix"));
 }
 
-rtl::OUString utl::ConfigManager::getDefaultCurrency() {
+OUString utl::ConfigManager::getDefaultCurrency() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("L10N/ooSetupCurrency")));
+        OUString("/org.openoffice.Setup"),
+        OUString("L10N/ooSetupCurrency"));
 }
 
-rtl::OUString utl::ConfigManager::getLocale() {
+OUString utl::ConfigManager::getLocale() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("L10N/ooLocale")));
+        OUString("/org.openoffice.Setup"),
+        OUString("L10N/ooLocale"));
 }
 
-rtl::OUString utl::ConfigManager::getProductExtension() {
+OUString utl::ConfigManager::getProductExtension() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Product/ooSetupExtension")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooSetupExtension"));
 }
 
-rtl::OUString utl::ConfigManager::getProductName() {
+OUString utl::ConfigManager::getProductName() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Product/ooName")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooName"));
 }
 
-rtl::OUString utl::ConfigManager::getProductVersion() {
+OUString utl::ConfigManager::getProductVersion() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Product/ooSetupVersion")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooSetupVersion"));
 }
 
-rtl::OUString utl::ConfigManager::getProductXmlFileFormat() {
+OUString utl::ConfigManager::getProductXmlFileFormat() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(
-            RTL_CONSTASCII_USTRINGPARAM("Product/ooXMLFileFormatName")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooXMLFileFormatName"));
 }
 
-rtl::OUString utl::ConfigManager::getProductXmlFileFormatVersion() {
+OUString utl::ConfigManager::getProductXmlFileFormatVersion() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(
-            RTL_CONSTASCII_USTRINGPARAM("Product/ooXMLFileFormatVersion")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooXMLFileFormatVersion"));
 }
 
-rtl::OUString utl::ConfigManager::getVendor() {
+OUString utl::ConfigManager::getVendor() {
     return getConfigurationString(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.Setup")),
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Product/ooVendor")));
+        OUString("/org.openoffice.Setup"),
+        OUString("Product/ooVendor"));
 }
 
-rtl::OUString utl::ConfigManager::getWriterCompatibilityVersionOOo_1_1() {
+OUString utl::ConfigManager::getWriterCompatibilityVersionOOo_1_1() {
     return getConfigurationString(
-        rtl::OUString(
-            RTL_CONSTASCII_USTRINGPARAM(
-                "/org.openoffice.Office.Compatibility")),
-        rtl::OUString(
-            RTL_CONSTASCII_USTRINGPARAM("WriterCompatibilityVersion/OOo11")));
+        OUString("/org.openoffice.Office.Compatibility"),
+        OUString("WriterCompatibilityVersion/OOo11"));
 }
 
 void utl::ConfigManager::storeConfigItems() {
@@ -175,21 +165,16 @@ css::uno::Reference< css::container::XHierarchicalNameAccess >
 utl::ConfigManager::acquireTree(utl::ConfigItem & item) {
     css::uno::Sequence< css::uno::Any > args(1);
     args[0] <<= css::beans::NamedValue(
-        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("nodepath")),
+        OUString("nodepath"),
         css::uno::makeAny(
-            rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/org.openoffice.")) +
-            item.GetSubTreeName()));
+            OUString("/org.openoffice.") + item.GetSubTreeName()));
     if ((item.GetMode() & CONFIG_MODE_ALL_LOCALES) != 0) {
         args.realloc(2);
-        args[1] <<= css::beans::NamedValue(
-            rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("locale")),
-            css::uno::makeAny(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("*"))));
+        args[1] <<= css::beans::NamedValue(OUString("locale"), css::uno::makeAny(OUString("*")));
     }
     return css::uno::Reference< css::container::XHierarchicalNameAccess >(
         getConfigurationProvider()->createInstanceWithArguments(
-            rtl::OUString(
-                RTL_CONSTASCII_USTRINGPARAM(
-                    "com.sun.star.configuration.ConfigurationUpdateAccess")),
+            OUString("com.sun.star.configuration.ConfigurationUpdateAccess"),
             args),
         css::uno::UNO_QUERY_THROW);
 }

unotools/source/config/configpaths.cxx
@@ -45,13 +45,13 @@ void lcl_resolveCharEntities(OUString & aLocalString)
     do
     {
         sal_Unicode ch = 0;
-        if (aLocalString.matchAsciiL(RTL_CONSTASCII_STRINGPARAM("&amp;"),nEscapePos))
+        if (aLocalString.match("&amp;",nEscapePos))
             ch = '&';
 
-        else if (aLocalString.matchAsciiL(RTL_CONSTASCII_STRINGPARAM("&apos;"),nEscapePos))
+        else if (aLocalString.match("&apos;",nEscapePos))
             ch = '\'';
 
-        else if (aLocalString.matchAsciiL(RTL_CONSTASCII_STRINGPARAM("&quot;"),nEscapePos))
+        else if (aLocalString.match("&quot;",nEscapePos))
             ch = '"';
 
         OSL_ENSURE(ch,"Configuration path contains '&' that is not part of a valid character escape");
@@ -256,7 +256,7 @@ OUString lcl_wrapName(const OUString& _sContent, const OUString& _sType)
     rtl::OUStringBuffer aNormalized(_sType.getLength() + _sContent.getLength() + 4); // reserve approximate size initially
 
     // prefix: type, opening bracket and quote
-    aNormalized.append( _sType ).appendAscii( RTL_CONSTASCII_STRINGPARAM("['") );
+    aNormalized.append( _sType ).append( "['" );
 
     // content: copy over each char and handle escaping
     for(const sal_Unicode* pCur = pBeginContent; pCur != pEndContent; ++pCur)
@@ -264,16 +264,16 @@ OUString lcl_wrapName(const OUString& _sContent, const OUString& _sType)
         // append (escape if needed)
         switch(*pCur)
         {
-        case sal_Unicode('&') : aNormalized.appendAscii( RTL_CONSTASCII_STRINGPARAM("&amp;") ); break;
-        case sal_Unicode('\''): aNormalized.appendAscii( RTL_CONSTASCII_STRINGPARAM("&apos;") ); break;
-        case sal_Unicode('\"'): aNormalized.appendAscii( RTL_CONSTASCII_STRINGPARAM("&quot;") ); break;
+        case sal_Unicode('&') : aNormalized.append( "&amp;" ); break;
+        case sal_Unicode('\''): aNormalized.append( "&apos;" ); break;
+        case sal_Unicode('\"'): aNormalized.append( "&quot;" ); break;
 
         default: aNormalized.append( *pCur );
         }
     }
 
     // suffix: closing quote and bracket
-    aNormalized.appendAscii( RTL_CONSTASCII_STRINGPARAM("']") );
+    aNormalized.append( "']" );
 
     return aNormalized.makeStringAndClear();
 }
@@ -282,7 +282,7 @@ OUString lcl_wrapName(const OUString& _sContent, const OUString& _sType)
 
 OUString wrapConfigurationElementName(OUString const& _sElementName)
 {
-    return lcl_wrapName(_sElementName, OUString(RTL_CONSTASCII_USTRINGPARAM("*")) );
+    return lcl_wrapName(_sElementName, "*" );
 }
 
 //----------------------------------------------------------------------------

unotools/source/config/defaultoptions.cxx
@@ -260,7 +260,7 @@ SvtDefaultOptions_Impl::SvtDefaultOptions_Impl() : ConfigItem( "Office.Common/Pa
                                 aTempStr = aPathOpt.SubstituteVariable( aList[ nPosition ] );
                                 aFullPath += aTempStr;
                                 if ( nPosition < nCount-1 )
-                                    aFullPath += OUString( RTL_CONSTASCII_USTRINGPARAM(";") );
+                                    aFullPath += ";";
                             }
                         }
                         else

commit 9772aa12fc2984889b5a472c54571966df56acf3

compareToAscii(RTL_CONSTASCII_STRINGPARAM(s)) != compareTo(s)
...broken with 9fe2d09db5dd2c05d3111adfa7e2410f88acc1af "Remove
RTL_CONSTASCII_STRINGPARAM (unotools)."

Change-Id: Id810c157a6e10380d28e4d6e85f908dc79cf0057

unotools/source/config/lingucfg.cxx
@@ -938,7 +938,7 @@ static bool lcl_GetFileUrlFromOrigin(
     if (!rOrigin.isEmpty())
     {
         OUString aURL( rOrigin );
-        if ( aURL.compareTo( EXPAND_PROTOCOL ) == 0 )
+        if ( aURL.startsWith( EXPAND_PROTOCOL ) )
         {
             // cut protocol
             OUString aMacro( aURL.copy( sizeof ( EXPAND_PROTOCOL ) -1 ) );
@@ -949,7 +949,7 @@ static bool lcl_GetFileUrlFromOrigin(
                 comphelper::getProcessComponentContext() )->expandMacros(
                     aMacro );
 
-            bool bIsFileUrl = aURL.compareTo( FILE_PROTOCOL ) == 0;
+            bool bIsFileUrl = aURL.startsWith( FILE_PROTOCOL );
             if (bIsFileUrl)
             {
                 rFileUrl = aURL;