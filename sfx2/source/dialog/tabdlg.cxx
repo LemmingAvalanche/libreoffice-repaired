LemmingAvalanche/libreoffice-repaired

commit 0b4974ebf510ac16cfa00d65b20916258a0679a7

coverity#440839 Resource leak in object
Change-Id: Icf65fdaac8c5ed852a85dce18fc49d401c12074e

sfx2/source/dialog/tabdlg.cxx
@@ -175,6 +175,10 @@ struct TabDlg_Impl
         pData           ( new SfxTabDlgData_Impl( nCnt ) ),
         pController     ( NULL )
     {}
+    ~TabDlg_Impl()
+    {
+        delete(pData);
+    }
 };
 
 Data_Impl* Find( SfxTabDlgData_Impl& rArr, sal_uInt16 nId, sal_uInt16* pPos = 0 );

commit 8cb579a6a12fa9a5f3e56fe2bb71b69a6bf593b5

sfx2: TabDlg_Impl::pData is deleted twice
(regression from 0b4974ebf510ac16cfa00d65b20916258a0679a7)

Change-Id: I1752d60496a572152b571e871db818cf8c9264ed

sfx2/source/dialog/tabdlg.cxx
@@ -177,7 +177,8 @@ struct TabDlg_Impl
     {}
     ~TabDlg_Impl()
     {
-        delete(pData);
+        delete pController;
+        delete pData;
     }
 };
 
@@ -534,8 +535,6 @@ SfxTabDialog::~SfxTabDialog()
         delete pDataObject;
     }
 
-    delete pImpl->pController;
-    delete pImpl->pData;
     delete pImpl;
     delete pOutSet;
     delete pExampleSet;