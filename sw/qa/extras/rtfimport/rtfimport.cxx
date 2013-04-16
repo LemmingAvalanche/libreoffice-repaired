LemmingAvalanche/libreoffice-repaired

commit 86afb613817e4ef1895f2a2e90d570403c8336cb

API CHANGE: add a "position" parameter to XParagraph/TextPortionAppend methods
So we can use the new RTF import for clipboard pastes in Writer without
inserting text content to the end of the document only.

Notes:

- SwXText::insertTextPortion: the MovePara() call is removed, as all it did was
trying to move the cursor beyond the end of the document.
- SwRTFReader::Read: the double fake paragraph insertion / deletion is
motivated by the ODT filter.
- RtfFilter::filter: if TextInsertModeRange is not passed, then the behaviour
is not changed.

v2:

- added missing @since tags
- added insertTextContentWithProperties() method
- removed unused appendParagraph() method

Change-Id: I24cddb00a78e3b798e7d88764e59e6a77a6e98a4
Helped-by: Michael Stahl <mstahl@redhat.com>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -156,6 +156,7 @@ DomainMapper_Impl::DomainMapper_Impl(
             uno::Reference < uno::XComponentContext >  xContext,
             uno::Reference< lang::XComponent >  xModel,
             SourceDocumentType eDocumentType,
+            uno::Reference< text::XTextRange > xInsertTextRange,
             bool bIsNewDoc) :
         m_eDocumentType( eDocumentType ),
         m_rDMapper( rDMapper ),
@@ -184,12 +185,14 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bParaSectpr( false ),
         m_bUsingEnhancedFields( false ),
         m_bSdt(false),
+        m_xInsertTextRange(xInsertTextRange),
         m_bIsNewDoc(bIsNewDoc)
 {
     appendTableManager( );
     GetBodyText();
     uno::Reference< text::XTextAppend > xBodyTextAppend = uno::Reference< text::XTextAppend >( m_xBodyText, uno::UNO_QUERY );
-    m_aTextAppendStack.push(xBodyTextAppend);
+    m_aTextAppendStack.push(TextAppendContext(xBodyTextAppend,
+                m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(m_xInsertTextRange)));
 
     //todo: does it make sense to set the body text as static text interface?
     uno::Reference< text::XTextAppendAndConvert > xBodyTextAppendAndConvert( m_xBodyText, uno::UNO_QUERY );
@@ -268,8 +271,14 @@ void DomainMapper_Impl::RemoveLastParagraph( )
         return;
     try
     {
-        uno::Reference< text::XTextCursor > xCursor = xTextAppend->createTextCursor();
-        xCursor->gotoEnd(false);
+        uno::Reference< text::XTextCursor > xCursor;
+        if (m_bIsNewDoc)
+        {
+            xCursor = xTextAppend->createTextCursor();
+            xCursor->gotoEnd(false);
+        }
+        else
+            xCursor.set(m_aTextAppendStack.top().xCursor, uno::UNO_QUERY);
         xCursor->goLeft( 1, true );
         xCursor->setString(OUString());
     }
@@ -1043,13 +1052,22 @@ void DomainMapper_Impl::finishParagraph( PropertyMapPtr pPropertyMap )
                     aProperties[nLength].Value <<= aDrop;
                     aProperties[nLength].Name = rPropNameSupplier.GetName(PROP_DROP_CAP_FORMAT);
                 }
-                uno::Reference< text::XTextRange > xTextRange =
-                    xTextAppend->finishParagraph( aProperties );
+                uno::Reference< text::XTextRange > xTextRange;
+                if (rAppendContext.xInsertPosition.is())
+                {
+                    xTextRange = xTextAppend->finishParagraphInsert( aProperties, rAppendContext.xInsertPosition );
+                    rAppendContext.xCursor->gotoNextParagraph(false);
+                }
+                else
+                    xTextRange = xTextAppend->finishParagraph( aProperties );
                 getTableManager( ).handle(xTextRange);
 
                 // Get the end of paragraph character inserted
                 uno::Reference< text::XTextCursor > xCur = xTextRange->getText( )->createTextCursor( );
-                xCur->gotoEnd( false );
+                if (rAppendContext.xInsertPosition.is())
+                    xCur->gotoRange( rAppendContext.xInsertPosition, false );
+                else
+                    xCur->gotoEnd( false );
                 xCur->goLeft( 1 , true );
                 uno::Reference< text::XTextRange > xParaEnd( xCur, uno::UNO_QUERY );
                 CheckParaRedline( xParaEnd );
@@ -1112,9 +1130,14 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, PropertyMapP
     {
         try
         {
-            uno::Reference< text::XTextRange > xTextRange =
-                xTextAppend->appendTextPortion
-                (rString, pPropertyMap->GetPropertyValues());
+            uno::Reference< text::XTextRange > xTextRange;
+            if (m_aTextAppendStack.top().xInsertPosition.is())
+            {
+                xTextRange = xTextAppend->insertTextPortion(rString, pPropertyMap->GetPropertyValues(), m_aTextAppendStack.top().xInsertPosition);
+                m_aTextAppendStack.top().xCursor->gotoRange(xTextRange->getEnd(), false);
+            }
+            else
+                xTextRange = xTextAppend->appendTextPortion(rString, pPropertyMap->GetPropertyValues());
             CheckRedline( xTextRange );
             m_bParaChanged = true;
 
@@ -1143,7 +1166,10 @@ void DomainMapper_Impl::appendTextContent(
     {
         try
         {
-            xTextAppendAndConvert->appendTextContent( xContent, xPropertyValues );
+            if (m_aTextAppendStack.top().xInsertPosition.is())
+                xTextAppendAndConvert->insertTextContentWithProperties( xContent, xPropertyValues, m_aTextAppendStack.top().xInsertPosition );
+            else
+                xTextAppendAndConvert->appendTextContent( xContent, xPropertyValues );
         }
         catch(const lang::IllegalArgumentException&)
         {
@@ -1249,7 +1275,10 @@ uno::Reference< beans::XPropertySet > DomainMapper_Impl::appendTextSectionAfter(
                 xTextAppend->createTextCursorByRange( xBefore ), uno::UNO_QUERY_THROW);
             //the cursor has been moved to the end of the paragraph because of the appendTextPortion() calls
             xCursor->gotoStartOfParagraph( false );
-            xCursor->gotoEnd( true );
+            if (m_aTextAppendStack.top().xInsertPosition.is())
+                xCursor->gotoRange( m_aTextAppendStack.top().xInsertPosition, true );
+            else
+                xCursor->gotoEnd( true );
             //the paragraph after this new section is already inserted
             xCursor->goLeft(1, true);
             static const OUString sSectionService("com.sun.star.text.TextSection");
@@ -1299,7 +1328,8 @@ void DomainMapper_Impl::PushPageHeader(SectionPropertyMap::PageType eType)
             //set the interface
             uno::Reference< text::XText > xHeaderText;
             xPageStyle->getPropertyValue(rPropNameSupplier.GetName( bLeft ? PROP_HEADER_TEXT_LEFT : PROP_HEADER_TEXT) ) >>= xHeaderText;
-            m_aTextAppendStack.push( uno::Reference< text::XTextAppend >( xHeaderText, uno::UNO_QUERY_THROW));
+            m_aTextAppendStack.push( TextAppendContext(uno::Reference< text::XTextAppend >( xHeaderText, uno::UNO_QUERY_THROW),
+                        m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(xHeaderText->getStart())));
         }
         catch( const uno::Exception& )
         {
@@ -1339,7 +1369,8 @@ void DomainMapper_Impl::PushPageFooter(SectionPropertyMap::PageType eType)
             //set the interface
             uno::Reference< text::XText > xFooterText;
             xPageStyle->getPropertyValue(rPropNameSupplier.GetName( bLeft ? PROP_FOOTER_TEXT_LEFT : PROP_FOOTER_TEXT) ) >>= xFooterText;
-            m_aTextAppendStack.push(uno::Reference< text::XTextAppend >( xFooterText, uno::UNO_QUERY_THROW ));
+            m_aTextAppendStack.push(TextAppendContext(uno::Reference< text::XTextAppend >( xFooterText, uno::UNO_QUERY_THROW ),
+                        m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(xFooterText->getStart())));
         }
         catch( const uno::Exception& )
         {
@@ -1394,7 +1425,8 @@ void DomainMapper_Impl::PushFootOrEndnote( bool bIsFootnote )
             aFontProperties = aFontProps->GetPropertyValues();
         }
         appendTextContent( uno::Reference< text::XTextContent >( xFootnoteText, uno::UNO_QUERY_THROW ), aFontProperties );
-        m_aTextAppendStack.push(uno::Reference< text::XTextAppend >( xFootnoteText, uno::UNO_QUERY_THROW ));
+        m_aTextAppendStack.push(TextAppendContext(uno::Reference< text::XTextAppend >( xFootnoteText, uno::UNO_QUERY_THROW ),
+                    m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(xFootnoteText->getStart())));
 
         // Redlines for the footnote anchor
         CheckRedline( xFootnote->getAnchor( ) );
@@ -1493,7 +1525,8 @@ void DomainMapper_Impl::PushAnnotation()
             uno::UNO_QUERY_THROW );
         uno::Reference< text::XText > xAnnotationText;
         m_xAnnotationField->getPropertyValue("TextRange") >>= xAnnotationText;
-        m_aTextAppendStack.push(uno::Reference< text::XTextAppend >( xAnnotationText, uno::UNO_QUERY_THROW ));
+        m_aTextAppendStack.push(TextAppendContext(uno::Reference< text::XTextAppend >( xAnnotationText, uno::UNO_QUERY_THROW ),
+                    m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(xAnnotationText->getStart())));
     }
     catch( const uno::Exception& )
     {
@@ -1548,8 +1581,10 @@ void DomainMapper_Impl::PushShapeContext( const uno::Reference< drawing::XShape 
     uno::Reference<text::XTextAppend> xTextAppend = m_aTextAppendStack.top().xTextAppend;
     try
     {
+        uno::Reference< text::XTextRange > xShapeText( xShape, uno::UNO_QUERY_THROW);
         // Add the shape to the text append stack
-        m_aTextAppendStack.push( uno::Reference< text::XTextAppend >( xShape, uno::UNO_QUERY_THROW ) );
+        m_aTextAppendStack.push( TextAppendContext(uno::Reference< text::XTextAppend >( xShape, uno::UNO_QUERY_THROW ),
+                    m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(xShapeText->getStart() )));
 
         // Add the shape to the anchored objects stack
         uno::Reference< text::XTextContent > xTxtContent( xShape, uno::UNO_QUERY_THROW );

editeng/inc/editeng/unotext.hxx
@@ -440,11 +440,13 @@ public:
     virtual void SAL_CALL moveTextRange( const ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange >& xRange, sal_Int16 nParagraphs ) throw(::com::sun::star::uno::RuntimeException);
 
     // com::sun::star::text::XParagraphAppend (new import API)
-    virtual ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange > SAL_CALL appendParagraph( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& CharacterAndParagraphProperties ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange > SAL_CALL finishParagraph( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& CharacterAndParagraphProperties ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange > SAL_CALL finishParagraphInsert( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& CharacterAndParagraphProperties, const ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange >& xInsertPosition ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
 
     // com::sun::star::text::XTextPortionAppend (new import API)
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange > SAL_CALL appendTextPortion( const ::rtl::OUString& Text, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& CharacterAndParagraphProperties ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange > SAL_CALL insertTextPortion( const ::rtl::OUString& Text, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& CharacterAndParagraphProperties, const com::sun::star::uno::Reference< com::sun::star::text::XTextRange>& rTextRange ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
 
     // com::sun::star::text::XTextCopy
     virtual void SAL_CALL copyText( const ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextCopy >& xSource ) throw (::com::sun::star::uno::RuntimeException);

editeng/source/uno/unotext.cxx
@@ -2142,34 +2142,12 @@ void SvxPropertyValuesToItemSet(
     }
 }
 
-// com::sun::star::text::XParagraphAppend (new import API)
-uno::Reference< text::XTextRange > SAL_CALL SvxUnoTextBase::appendParagraph(
-        const uno::Sequence< beans::PropertyValue >& rCharAndParaProps )
+uno::Reference< text::XTextRange > SAL_CALL SvxUnoTextBase::finishParagraphInsert(
+        const uno::Sequence< beans::PropertyValue >& /*rCharAndParaProps*/,
+        const uno::Reference< text::XTextRange >& /*rTextRange*/ )
     throw (lang::IllegalArgumentException, beans::UnknownPropertyException, uno::RuntimeException)
 {
-    SolarMutexGuard aGuard;
     uno::Reference< text::XTextRange > xRet;
-    SvxEditSource *pEditSource = GetEditSource();
-    SvxTextForwarder *pTextForwarder = pEditSource ? pEditSource->GetTextForwarder() : 0;
-    if (pTextForwarder)
-    {
-        sal_uInt16 nParaCount = pTextForwarder->GetParagraphCount();
-        DBG_ASSERT( nParaCount > 0, "paragraph count is 0 or negative" );
-        pTextForwarder->AppendParagraph();
-
-        // set properties for new appended (now last) paragraph
-        ESelection aSel( nParaCount, 0, nParaCount, 0 );
-        SfxItemSet aItemSet( *pTextForwarder->GetEmptyItemSetPtr() );
-        SvxPropertyValuesToItemSet( aItemSet, rCharAndParaProps,
-                            ImplGetSvxUnoOutlinerTextCursorSfxPropertySet(),
-                            pTextForwarder,
-                            nParaCount );
-        pTextForwarder->QuickSetAttribs( aItemSet, aSel );
-        pEditSource->UpdateData();
-        SvxUnoTextRange* pRange = new SvxUnoTextRange( *this );
-        xRet = pRange;
-        pRange->SetSelection( aSel );
-    }
     return xRet;
 }
 
@@ -2200,6 +2178,16 @@ uno::Reference< text::XTextRange > SAL_CALL SvxUnoTextBase::finishParagraph(
         xRet = pRange;
         pRange->SetSelection( aSel );
     }
+    return xRet;
+}
+
+uno::Reference< text::XTextRange > SAL_CALL SvxUnoTextBase::insertTextPortion(
+        const ::rtl::OUString& /*rText*/,
+        const uno::Sequence< beans::PropertyValue >& /*rCharAndParaProps*/,
+        const uno::Reference< text::XTextRange>& /*rTextRange*/ )
+    throw (lang::IllegalArgumentException, beans::UnknownPropertyException, uno::RuntimeException)
+{
+    uno::Reference< text::XTextRange > xRet;
     return xRet;
 }
 

offapi/com/sun/star/text/XParagraphAppend.idl
@@ -35,19 +35,6 @@
  */
 interface XParagraphAppend : com::sun::star::uno::XInterface
 {
-        /** appends a new and empty paragraph at the end of the text.
-
-                <p>The properties are applied to the new paragraph.
-        </p>
-
-        @param
-            CharacterAndParagraphProperties can contain all the properties defined by the service
-                      <type scope="com::sun::star::text">Paragraph.
-
-     */
-         com::sun::star::text::XTextRange appendParagraph(  [in] com::sun::star::beans::PropertyValues CharacterAndParagraphProperties )
-                        raises( com::sun::star::lang::IllegalArgumentException,
-                                       com::sun::star::beans::UnknownPropertyException );
          /** appends a new and empty paragraph at the end of the text.
 
                 <p>The properties are applied to the last paragraph before the new paragraph is inserted.
@@ -59,6 +46,26 @@ interface XParagraphAppend : com::sun::star::uno::XInterface
 
          */
          com::sun::star::text::XTextRange finishParagraph(  [in] com::sun::star::beans::PropertyValues CharacterAndParagraphProperties )
+                        raises( com::sun::star::lang::IllegalArgumentException,
+                                       com::sun::star::beans::UnknownPropertyException );
+
+         /** inserts a new and empty paragraph to the text at a given position.
+
+                <p>The properties are applied to the last paragraph before the new paragraph is inserted.
+                </p>
+
+        @param
+            CharacterAndParagraphProperties can contain all the properties defined by the service
+                      <type scope="com::sun::star::text">Paragraph.
+
+        @param
+            TextRange specifies the position of the insertion.
+
+        @since LibreOffice 4.0
+
+         */
+         com::sun::star::text::XTextRange finishParagraphInsert(  [in] com::sun::star::beans::PropertyValues CharacterAndParagraphProperties,
+                                                                  [in] com::sun::star::text::XTextRange TextRange )
                         raises( com::sun::star::lang::IllegalArgumentException,
                                        com::sun::star::beans::UnknownPropertyException );
 

offapi/com/sun/star/text/XTextContentAppend.idl
@@ -55,6 +55,27 @@ interface XTextContentAppend : com::sun::star::uno::XInterface
                                 raises( com::sun::star::lang::IllegalArgumentException,
                                        com::sun::star::beans::UnknownPropertyException );
 
+        /** inserts a text content at the given position.
+
+        @param TextContent
+            contains the object to be inserted.
+        @param CharacterAndParagraphProperties
+            can contain all the properties defined by the service
+                      <type scope="com::sun::star::text">Paragraph.
+        @param TextRange
+            insert position
+        @return
+            the anchor text range of the inserted text content.
+
+        @since LibreOffice 4.0
+     */
+         com::sun::star::text::XTextRange insertTextContentWithProperties(
+             [in] com::sun::star::text::XTextContent TextContent,
+             [in] com::sun::star::beans::PropertyValues CharacterAndParagraphProperties,
+             [in] com::sun::star::text::XTextRange TextRange )
+                                raises( com::sun::star::lang::IllegalArgumentException,
+                                       com::sun::star::beans::UnknownPropertyException );
+
 };
 
 

offapi/com/sun/star/text/XTextPortionAppend.idl
@@ -53,6 +53,31 @@ interface XTextPortionAppend : com::sun::star::uno::XInterface
                         raises( com::sun::star::lang::IllegalArgumentException,
                                         com::sun::star::beans::UnknownPropertyException );
 
+        /** inserts a new text portion to the paragraph at a given position.
+
+                <p> The sequence can contain all the properties defined by the service <type scope="com::sun::star::text">TextPortion.
+        </p>
+
+        @param
+            Text contains the text to be inserted.
+
+        @param
+            CharacterAndParagraphProperties can contain all the properties defined by the service
+                      <type scope="com::sun::star::text">Paragraph.
+
+        @param
+            TextRange specifies the position of the insert.
+
+        @since LibreOffice 4.0
+
+     */
+         com::sun::star::text::XTextRange  insertTextPortion(
+                                [in] string Text,
+                                [in] com::sun::star::beans::PropertyValues CharacterAndParagraphProperties,
+                                [in] com::sun::star::text::XTextRange TextRange)
+                        raises( com::sun::star::lang::IllegalArgumentException,
+                                        com::sun::star::beans::UnknownPropertyException );
+
 };
 
 

sw/inc/unotext.hxx
@@ -206,7 +206,7 @@ public:
     // XParagraphAppend
     virtual ::com::sun::star::uno::Reference<
                 ::com::sun::star::text::XTextRange > SAL_CALL
-        appendParagraph(
+        finishParagraph(
             const ::com::sun::star::uno::Sequence<
                     ::com::sun::star::beans::PropertyValue >&
                 rCharacterAndParagraphProperties)
@@ -214,10 +214,13 @@ public:
                 ::com::sun::star::uno::RuntimeException);
     virtual ::com::sun::star::uno::Reference<
                 ::com::sun::star::text::XTextRange > SAL_CALL
-        finishParagraph(
+        finishParagraphInsert(
             const ::com::sun::star::uno::Sequence<
                     ::com::sun::star::beans::PropertyValue >&
-                rCharacterAndParagraphProperties)
+                rCharacterAndParagraphProperties,
+            const ::com::sun::star::uno::Reference<
+                    ::com::sun::star::text::XTextRange >&
+                xInsertPosition)
         throw (::com::sun::star::lang::IllegalArgumentException,
                 ::com::sun::star::uno::RuntimeException);
 
@@ -232,6 +235,19 @@ public:
         throw (::com::sun::star::lang::IllegalArgumentException,
                 ::com::sun::star::uno::RuntimeException);
 
+    virtual ::com::sun::star::uno::Reference<
+                ::com::sun::star::text::XTextRange > SAL_CALL
+        insertTextPortion(
+            const ::rtl::OUString& rText,
+            const ::com::sun::star::uno::Sequence<
+                    ::com::sun::star::beans::PropertyValue >&
+                rCharacterAndParagraphProperties,
+            const ::com::sun::star::uno::Reference<
+                    ::com::sun::star::text::XTextRange >&
+                rTextRange)
+        throw (::com::sun::star::lang::IllegalArgumentException,
+                ::com::sun::star::uno::RuntimeException);
+
     // XTextContentAppend
     virtual ::com::sun::star::uno::Reference<
                 ::com::sun::star::text::XTextRange > SAL_CALL
@@ -241,6 +257,17 @@ public:
             const ::com::sun::star::uno::Sequence<
                     ::com::sun::star::beans::PropertyValue >&
                 rCharacterAndParagraphProperties)
+        throw (::com::sun::star::lang::IllegalArgumentException,
+                ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference<
+                ::com::sun::star::text::XTextRange > SAL_CALL
+        insertTextContentWithProperties(
+            const ::com::sun::star::uno::Reference<
+                ::com::sun::star::text::XTextContent >& xTextContent,
+            const ::com::sun::star::uno::Sequence<
+                    ::com::sun::star::beans::PropertyValue >&
+                rCharacterAndParagraphProperties,
+            const ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange >& xInsertPosition)
         throw (::com::sun::star::lang::IllegalArgumentException,
                 ::com::sun::star::uno::RuntimeException);
 

sw/inc/unotextrange.hxx
@@ -43,7 +43,7 @@ class SwPaM;
 class SwUnoCrsr;
 class SwFrmFmt;
 
-class SwUnoInternalPaM
+class SW_DLLPUBLIC SwUnoInternalPaM
     : public SwPaM
 {
 
@@ -62,7 +62,7 @@ namespace sw {
 
     void DeepCopyPaM(SwPaM const & rSource, SwPaM & rTarget);
 
-    bool XTextRangeToSwPaM(SwUnoInternalPaM& rToFill,
+    SW_DLLPUBLIC bool XTextRangeToSwPaM(SwUnoInternalPaM& rToFill,
             const ::com::sun::star::uno::Reference<
                 ::com::sun::star::text::XTextRange > & xTextRange);
 

commit cce2bad852cca6806239bf859e9592cd400cc925

fdo#61193 fix crash on RTF paste of footnote
Regression from 86afb613817e4ef1895f2a2e90d570403c8336cb.

Change-Id: Ia0873851979df5bba0a4693f044aab2aeff2b8e4

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -722,6 +722,8 @@ void lcl_AddRangeAndStyle(
 void DomainMapper_Impl::CheckUnregisteredFrameConversion( )
 {
     PropertyNameSupplier& rPropNameSupplier = PropertyNameSupplier::GetPropertyNameSupplier();
+    if (m_aTextAppendStack.empty())
+        return;
     TextAppendContext& rAppendContext = m_aTextAppendStack.top();
     // n#779642: ignore fly frame inside table as it could lead to messy situations
     if( rAppendContext.pLastParagraphProperties.get() && rAppendContext.pLastParagraphProperties->IsFrameMode()
@@ -965,6 +967,8 @@ void DomainMapper_Impl::finishParagraph( PropertyMapPtr pPropertyMap )
 #endif
 
     ParagraphPropertyMap* pParaContext = dynamic_cast< ParagraphPropertyMap* >( pPropertyMap.get() );
+    if (!m_aTextAppendStack.size())
+        return;
     TextAppendContext& rAppendContext = m_aTextAppendStack.top();
     uno::Reference< text::XTextAppend >  xTextAppend;
     if (!m_aTextAppendStack.empty())
@@ -1449,14 +1453,14 @@ void DomainMapper_Impl::PushFootOrEndnote( bool bIsFootnote )
         }
         appendTextContent( uno::Reference< text::XTextContent >( xFootnoteText, uno::UNO_QUERY_THROW ), aFontProperties );
         m_aTextAppendStack.push(TextAppendContext(uno::Reference< text::XTextAppend >( xFootnoteText, uno::UNO_QUERY_THROW ),
-                    m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : m_xBodyText->createTextCursorByRange(xFootnoteText->getStart())));
+                    m_bIsNewDoc ? uno::Reference<text::XTextCursor>() : xFootnoteText->createTextCursorByRange(xFootnoteText->getStart())));
 
         // Redlines for the footnote anchor
         CheckRedline( xFootnote->getAnchor( ) );
     }
-    catch( const uno::Exception& )
+    catch( const uno::Exception& e )
     {
-        OSL_FAIL( "exception in PushFootOrEndnote" );
+        SAL_WARN("writerfilter", "exception in PushFootOrEndnote: " << e.Message);
     }
 }
 

sw/qa/extras/rtfimport/data/fdo61193.rtf
@@ -1,0 +1,1 @@
+{\rtf1\ansi text.{\footnote footnote}}

sw/qa/extras/rtfimport/data/hello.rtf
@@ -1,0 +1,1 @@
+{\rtf1 Hello world!\par}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -150,6 +150,7 @@ public:
     void testFdo62288();
     void testFdo37716();
     void testFdo51916();
+    void testFdo61193();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -272,6 +273,7 @@ void Test::run()
         {"fdo62288.rtf", &Test::testFdo62288},
         {"fdo37716.rtf", &Test::testFdo37716},
         {"fdo51916.rtf", &Test::testFdo51916},
+        {"hello.rtf", &Test::testFdo61193},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -924,6 +926,15 @@ void Test::testCopyPasteFootnote()
     paste("copypaste-footnote-paste.rtf", xTextRange);
 
     CPPUNIT_ASSERT_EQUAL(OUString("bbb"), xTextRange->getString());
+}
+
+void Test::testFdo61193()
+{
+    // Pasting content that contained a footnote caused a crash.
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xText(xTextDocument->getText(), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xEnd = xText->getEnd();
+    paste("fdo61193.rtf", xEnd);
 }
 
 void Test::testShptxtPard()