LemmingAvalanche/libreoffice-repaired

commit dffcee027ce70d560ac902704dd8b531dc9e717b

n#783638 DOCX import of wp:inline's distT/B/L/R attributes
Change-Id: I181fcae021eed07608b06744b469628e4d78ccba

writerfilter/source/dmapper/GraphicImport.cxx
@@ -339,6 +339,15 @@ public:
     {
         return bYSizeValid;
     }
+
+    void applyMargins(uno::Reference< beans::XPropertySet > xGraphicObjectProperties) const
+    {
+        PropertyNameSupplier& rPropNameSupplier = PropertyNameSupplier::GetPropertyNameSupplier();
+        xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_LEFT_MARGIN ), uno::makeAny(nLeftMargin));
+        xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_RIGHT_MARGIN ), uno::makeAny(nRightMargin));
+        xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_TOP_MARGIN ), uno::makeAny(nTopMargin));
+        xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_BOTTOM_MARGIN ), uno::makeAny(nBottomMargin));
+    }
 };
 
 
@@ -969,10 +978,17 @@ void GraphicImport::lcl_attribute(Id nName, Value & val)
             }
         break;
         case NS_ooxml::LN_CT_Inline_distT:
+            m_pImpl->nTopMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+        break;
         case NS_ooxml::LN_CT_Inline_distB:
+            m_pImpl->nBottomMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+        break;
         case NS_ooxml::LN_CT_Inline_distL:
+            m_pImpl->nLeftMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+        break;
         case NS_ooxml::LN_CT_Inline_distR:
-            //TODO: need to be handled
+            m_pImpl->nRightMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+        break;
         break;
         case NS_ooxml::LN_CT_GraphicalObjectData_uri:
             val.getString();
@@ -1453,14 +1469,7 @@ uno::Reference< text::XTextContent > GraphicImport::createGraphicObject( const b
                     uno::makeAny(m_pImpl->bContour));
                 xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_CONTOUR_OUTSIDE ),
                     uno::makeAny(m_pImpl->bContourOutside));
-                xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_LEFT_MARGIN ),
-                    uno::makeAny(m_pImpl->nLeftMargin));
-                xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_RIGHT_MARGIN ),
-                    uno::makeAny(m_pImpl->nRightMargin));
-                xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_TOP_MARGIN ),
-                    uno::makeAny(m_pImpl->nTopMargin));
-                xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_BOTTOM_MARGIN ),
-                    uno::makeAny(m_pImpl->nBottomMargin));
+                m_pImpl->applyMargins(xGraphicObjectProperties);
 
                 if( m_pImpl->eColorMode == drawing::ColorMode_STANDARD &&
                     m_pImpl->nContrast == -70 &&
@@ -1550,6 +1559,7 @@ uno::Reference< text::XTextContent > GraphicImport::createGraphicObject( const b
                 if( m_pImpl->getXSize() && m_pImpl->getYSize() )
                     xGraphicObjectProperties->setPropertyValue(rPropNameSupplier.GetName(PROP_SIZE),
                         uno::makeAny( awt::Size( m_pImpl->getXSize(), m_pImpl->getYSize() )));
+                m_pImpl->applyMargins(xGraphicObjectProperties);
                 try
                 {
                     if( !m_pImpl->sName.isEmpty() )

commit 93cd107337773771870c42df26073f34e3960df5

fdo#63685 wp:inline's distT/B/L/R is in EMU's, not twips
Regression from dffcee027ce70d560ac902704dd8b531dc9e717b.

Change-Id: I9ab3f9f41316a01d5a74f3e500bad7a83c8d2dca

writerfilter/source/dmapper/GraphicImport.cxx
@@ -991,16 +991,16 @@ void GraphicImport::lcl_attribute(Id nName, Value & val)
             }
         break;
         case NS_ooxml::LN_CT_Inline_distT:
-            m_pImpl->nTopMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+            m_pImpl->nTopMargin = ConversionHelper::convertEMUToMM100(nIntValue);
         break;
         case NS_ooxml::LN_CT_Inline_distB:
-            m_pImpl->nBottomMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+            m_pImpl->nBottomMargin = ConversionHelper::convertEMUToMM100(nIntValue);
         break;
         case NS_ooxml::LN_CT_Inline_distL:
-            m_pImpl->nLeftMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+            m_pImpl->nLeftMargin = ConversionHelper::convertEMUToMM100(nIntValue);
         break;
         case NS_ooxml::LN_CT_Inline_distR:
-            m_pImpl->nRightMargin = ConversionHelper::convertTwipToMM100(nIntValue);
+            m_pImpl->nRightMargin = ConversionHelper::convertEMUToMM100(nIntValue);
         break;
         case NS_ooxml::LN_CT_GraphicalObjectData_uri:
             val.getString();

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -125,6 +125,7 @@ public:
     void testFdo59273();
     void testTableWidth();
     void testConditionalstylesTbllook();
+    void testFdo63685();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -201,6 +202,7 @@ void Test::run()
         {"fdo59273.docx", &Test::testFdo59273},
         {"table_width.docx", &Test::testTableWidth},
         {"conditionalstyles-tbllook.docx", &Test::testConditionalstylesTbllook},
+        {"fdo63685.docx", &Test::testFdo63685},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1322,6 +1324,14 @@ void Test::testConditionalstylesTbllook()
     uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
     // Background was -1.
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0x7F7F7F), getProperty<sal_Int32>(xTable->getCellByName("A1"), "BackColor"));
+}
+
+void Test::testFdo63685()
+{
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xDraws(xDrawPageSupplier->getDrawPage(), uno::UNO_QUERY);
+    // Was 85697, i.e. original 114120 was converted to mm100 from twips, not from EMUs.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(318), getProperty<sal_Int32>(xDraws->getByIndex(0), "TopMargin"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);