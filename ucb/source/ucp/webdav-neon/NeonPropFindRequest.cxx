LemmingAvalanche/libreoffice-repaired

commit 2f695c8b17779adb78155bdb2cbc047fe2c52919

Remove RTL_CONSTASCII_(U)STRINGPARAM in ucb(ucp)
Change-Id: Iae4ea99e6eff6de8db3d40a6b86e5fd28ff857d0

ucb/source/ucp/webdav-neon/NeonPropFindRequest.cxx
@@ -140,8 +140,7 @@ extern "C" int NPFR_propfind_iter( void* userdata,
             if ( !aValue.isEmpty() )
             {
                 aValue = stripDavNamespace( aValue ).toAsciiLowerCase();
-                if ( aValue.compareTo(
-                         RTL_CONSTASCII_STRINGPARAM( "<collection" ) ) == 0 )
+                if ( aValue.compareTo( "<collection" ) == 0 )
                 {
                     thePropertyValue.Value
                         <<= OUString("collection");

ucb/source/ucp/file/bc.cxx
@@ -338,11 +338,11 @@ BaseContent::execute( const Command& aCommand,
 
     Any aAny;
 
-    if (aCommand.Name.equalsAsciiL(RTL_CONSTASCII_STRINGPARAM("getPropertySetInfo")))  // No exceptions
+    if (aCommand.Name == "getPropertySetInfo")  // No exceptions
     {
         aAny <<= getPropertySetInfo( CommandId );
     }
-    else if (aCommand.Name.equalsAsciiL(RTL_CONSTASCII_STRINGPARAM("getCommandInfo")))  // no exceptions
+    else if (aCommand.Name == "getCommandInfo")  // no exceptions
     {
         aAny <<= getCommandInfo();
     }

ucb/source/ucp/file/filcmd.cxx
@@ -87,7 +87,7 @@ XCommandInfo_impl::getCommandInfoByName(
         if( m_pMyShell->m_sCommandInfo[i].Name == aName )
             return m_pMyShell->m_sCommandInfo[i];
 
-    throw UnsupportedCommandException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+    throw UnsupportedCommandException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
 }
 
 
@@ -101,7 +101,7 @@ XCommandInfo_impl::getCommandInfoByHandle(
         if( m_pMyShell->m_sCommandInfo[i].Handle == Handle )
             return m_pMyShell->m_sCommandInfo[i];
 
-    throw UnsupportedCommandException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+    throw UnsupportedCommandException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
 }
 
 

ucb/source/ucp/file/filinpstr.cxx
@@ -136,7 +136,7 @@ XInputStream_impl::readBytes(
            io::IOException,
            uno::RuntimeException)
 {
-    if( ! m_nIsOpen ) throw io::IOException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+    if( ! m_nIsOpen ) throw io::IOException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
 
     aData.realloc(nBytesToRead);
         //TODO! translate memory exhaustion (if it were detectable...) into
@@ -145,7 +145,7 @@ XInputStream_impl::readBytes(
     sal_uInt64 nrc(0);
     if(m_aFile.read( aData.getArray(),sal_uInt64(nBytesToRead),nrc )
        != osl::FileBase::E_None)
-        throw io::IOException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+        throw io::IOException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
 
     // Shrink aData in case we read less than nBytesToRead (XInputStream
     // documentation does not tell whether this is required, and I do not know
@@ -202,7 +202,7 @@ XInputStream_impl::closeInput(
     {
         osl::FileBase::RC err = m_aFile.close();
         if( err != osl::FileBase::E_None )
-            throw io::IOException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+            throw io::IOException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
         m_nIsOpen = false;
     }
 }
@@ -216,9 +216,9 @@ XInputStream_impl::seek(
            uno::RuntimeException )
 {
     if( location < 0 )
-        throw lang::IllegalArgumentException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >(), 0 );
+        throw lang::IllegalArgumentException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >(), 0 );
     if( osl::FileBase::E_None != m_aFile.setPos( osl_Pos_Absolut, sal_uInt64( location ) ) )
-        throw io::IOException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+        throw io::IOException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
 }
 
 
@@ -230,7 +230,7 @@ XInputStream_impl::getPosition(
 {
     sal_uInt64 uPos;
     if( osl::FileBase::E_None != m_aFile.getPos( uPos ) )
-        throw io::IOException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+        throw io::IOException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
     return sal_Int64( uPos );
 }
 
@@ -242,7 +242,7 @@ XInputStream_impl::getLength(
 {
     sal_uInt64 uEndPos;
     if ( m_aFile.getSize(uEndPos) != osl::FileBase::E_None )
-        throw io::IOException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+        throw io::IOException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
     else
         return sal_Int64( uEndPos );
 }

ucb/source/ucp/file/filprp.cxx
@@ -116,7 +116,7 @@ XPropertySetInfo_impl::getPropertyByName(
   for( sal_Int32 i = 0; i < m_seq.getLength(); ++i )
     if( m_seq[i].Name == aName ) return m_seq[i];
 
-  throw beans::UnknownPropertyException( ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ), uno::Reference< uno::XInterface >() );
+  throw beans::UnknownPropertyException( OUString( OSL_LOG_PREFIX ), uno::Reference< uno::XInterface >() );
 }
 
 

ucb/source/ucp/file/filtask.cxx
@@ -58,7 +58,7 @@ TaskManager::startTask(
     if( it != m_aTaskMap.end() )
     {
         throw DuplicateCommandIdentifierException(
-            ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( OSL_LOG_PREFIX ) ),
+            OUString( OSL_LOG_PREFIX ),
             uno::Reference< uno::XInterface >() );
     }
     m_aTaskMap[ CommandId ] = TaskHandling( xCommandEnv );

ucb/source/ucp/gio/gio_content.cxx
@@ -645,12 +645,12 @@ uno::Sequence< uno::Any > Content::setPropertyValues(
 #if OSL_DEBUG_LEVEL > 1
         g_warning("Set prop '%s'", rtl::OUStringToOString(rValue.Name, RTL_TEXTENCODING_UTF8).getStr());
 #endif
-        if ( rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "ContentType" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "MediaType" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "IsDocument" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "IsFolder" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "Size" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "CreatableContentsInfo" ) ) )
+        if ( rValue.Name == "ContentType" ) ||
+             rValue.Name == "MediaType" ) ||
+             rValue.Name == "IsDocument" ) ||
+             rValue.Name == "IsFolder" ) ||
+             rValue.Name == "Size" ) ||
+             rValue.Name == "CreatableContentsInfo" ) )
         {
             aRet[ n ] <<= getReadOnlyException( static_cast< cppu::OWeakObject * >(this) );
         }
@@ -931,7 +931,7 @@ uno::Any SAL_CALL Content::execute(
             ucbhelper::cancelCommandExecution ( getBadArgExcept (), xEnv );
         aRet <<= setPropertyValues( aProperties, xEnv );
     }
-    else if (aCommand.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "createNewContent" ) )
+    else if (aCommand.Name.equals == "createNewContent" )
              && isFolder( xEnv ) )
     {
         ucb::ContentInfo arg;

ucb/source/ucp/gvfs/gvfs_content.cxx
@@ -649,8 +649,7 @@ uno::Reference< sdbc::XRow > Content::getPropertyValues(
             else
                 xRow->appendVoid( rProp );
 
-        else if (rProp.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "IsVolume" ) ) ||
-             rProp.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "IsCompactDisk" ) ) )
+        else if ( rProp.Name == "IsVolume" || rProp.Name == "IsCompactDisk" )
             xRow->appendBoolean( rProp, sal_False );
 
         else if ( rProp.Name == "DateCreated" ) {
@@ -784,12 +783,12 @@ uno::Sequence< uno::Any > Content::setPropertyValues(
 #if OSL_DEBUG_LEVEL > 1
         g_warning( "Set prop '%s'", OUStringToGnome( rValue.Name ) );
 #endif
-        if ( rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "ContentType" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "MediaType" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "IsDocument" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "IsFolder" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "Size" ) ) ||
-             rValue.Name.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "CreatableContentsInfo" ) ) )
+        if ( rValue.Name == "ContentType" ||
+             rValue.Name == "MediaType" ||
+             rValue.Name == "IsDocument" ||
+             rValue.Name == "IsFolder" ||
+             rValue.Name == "Size" ||
+             rValue.Name == "CreatableContentsInfo" )
             aRet[ n ] <<= getReadOnlyException( this );
 
         else if ( rValue.Name == "Title" ) {

commit 425e9a36f0a54712133d1648d62dfe7128062067

Related rhbz#961460: Fix regression around OUString::compareTo usage
...originally introduced with 2f695c8b17779adb78155bdb2cbc047fe2c52919 "Remove
RTL_CONSTASCII_(U)STRINGPARAM in ucb(ucp)," leading to "IsFolder" not being
properly set in the WebDAV UCP, leading to failure when saving documents.

Change-Id: Id2cc98582c9feffaa501a68069cd606fb420fd29

ucb/source/ucp/webdav-neon/NeonPropFindRequest.cxx
@@ -135,7 +135,7 @@ extern "C" int NPFR_propfind_iter( void* userdata,
             if ( !aValue.isEmpty() )
             {
                 aValue = stripDavNamespace( aValue ).toAsciiLowerCase();
-                if ( aValue == "<collection" )
+                if ( aValue.startsWith("<collection") )
                 {
                     thePropertyValue.Value
                         <<= OUString("collection");