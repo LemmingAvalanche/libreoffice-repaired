LemmingAvalanche/libreoffice-repaired

commit 50b6d1812891493cf5fd0920d2a45b239f89b51b

fdo#46808, Convert awt::UnoControlDialogModel to new style
Change-Id: I4b912034ef3f4855b87d6d6f18ff13bd1ecc8f72

scripting/source/dlgprov/dlgprov.cxx
@@ -21,6 +21,7 @@
 #include "DialogModelProvider.hxx"
 #include "dlgprov.hxx"
 #include "dlgevtatt.hxx"
+#include <com/sun/star/awt/UnoControlDialogModel.hpp>
 #include <com/sun/star/awt/Toolkit.hpp>
 #include <com/sun/star/awt/XControlContainer.hpp>
 #include <com/sun/star/awt/XWindowPeer.hpp>
@@ -86,8 +87,6 @@ namespace dlgprov
 {
 //.........................................................................
 
-static OUString aResourceResolverPropName("ResourceResolver");
-
     Reference< resource::XStringResourceManager > lcl_getStringResourceManager(const Reference< XComponentContext >& i_xContext,const OUString& i_sURL)
     {
         INetURLObject aInetObj( i_sURL );
@@ -120,23 +119,20 @@ static OUString aResourceResolverPropName("ResourceResolver");
         }
         return xStringResourceManager;
     }
-    Reference< container::XNameContainer > lcl_createControlModel(const Reference< XComponentContext >& i_xContext)
+    static Reference< XUnoControlDialogModel > lcl_createControlModel(const Reference< XComponentContext >& i_xContext)
     {
-        Reference< XMultiComponentFactory > xSMgr_( i_xContext->getServiceManager(), UNO_QUERY_THROW );
-        Reference< container::XNameContainer > xControlModel( xSMgr_->createInstanceWithContext( OUString( "com.sun.star.awt.UnoControlDialogModel"  ), i_xContext ), UNO_QUERY_THROW );
+        Reference< XUnoControlDialogModel > xControlModel = UnoControlDialogModel::create( i_xContext );
         return xControlModel;
     }
-    Reference< container::XNameContainer > lcl_createDialogModel( const Reference< XComponentContext >& i_xContext,
+    Reference< XUnoControlDialogModel > lcl_createDialogModel( const Reference< XComponentContext >& i_xContext,
         const Reference< io::XInputStream >& xInput,
         const Reference< frame::XModel >& xModel,
         const Reference< resource::XStringResourceManager >& xStringResourceManager,
-        const Any &aDialogSourceURL) throw ( Exception )
+        const OUString& aDialogSourceURL) throw ( Exception )
     {
-        Reference< container::XNameContainer > xDialogModel(  lcl_createControlModel(i_xContext) );
+        Reference< XUnoControlDialogModel > xDialogModel(  lcl_createControlModel(i_xContext) );
 
-        OUString aDlgSrcUrlPropName( "DialogSourceURL"  );
-        Reference< beans::XPropertySet > xDlgPropSet( xDialogModel, UNO_QUERY );
-        xDlgPropSet->setPropertyValue( aDlgSrcUrlPropName, aDialogSourceURL );
+        xDialogModel->setDialogSourceURL( aDialogSourceURL );
 
         // #TODO we really need to detect the source of the Dialog, is it
         // the dialog. E.g. if the dialog was created from basic ( then we just
@@ -150,10 +146,7 @@ static OUString aResourceResolverPropName("ResourceResolver");
         // Set resource property
         if( xStringResourceManager.is() )
         {
-            Reference< beans::XPropertySet > xDlgPSet( xDialogModel, UNO_QUERY );
-            Any aStringResourceManagerAny;
-            aStringResourceManagerAny <<= xStringResourceManager;
-            xDlgPSet->setPropertyValue( aResourceResolverPropName, aStringResourceManagerAny );
+            xDialogModel->setResourceResolver( xStringResourceManager );
         }
 
         return xDialogModel;
@@ -255,15 +248,15 @@ static OUString aResourceResolverPropName("ResourceResolver");
         return xStringResourceManager;
     }
 
-    Reference< container::XNameContainer > DialogProviderImpl::createDialogModel(
+    Reference< XUnoControlDialogModel > DialogProviderImpl::createDialogModel(
         const Reference< io::XInputStream >& xInput,
         const Reference< resource::XStringResourceManager >& xStringResourceManager,
-        const Any &aDialogSourceURL) throw ( Exception )
+        const OUString &aDialogSourceURL) throw ( Exception )
     {
         return lcl_createDialogModel(m_xContext,xInput,m_xModel,xStringResourceManager,aDialogSourceURL);
     }
 
-    Reference< XControlModel > DialogProviderImpl::createDialogModelForBasic() throw ( Exception )
+    Reference< XUnoControlDialogModel > DialogProviderImpl::createDialogModelForBasic() throw ( Exception )
     {
         if ( !m_BasicInfo.get() )
             // shouln't get here
@@ -271,13 +264,10 @@ static OUString aResourceResolverPropName("ResourceResolver");
         Reference< resource::XStringResourceManager > xStringResourceManager = getStringResourceFromDialogLibrary( m_BasicInfo->mxDlgLib );
 
         OUString aURL("" );
-        Any aDialogSourceURL;
-        aDialogSourceURL <<= aURL;
-        Reference< XControlModel > xCtrlModel( createDialogModel( m_BasicInfo->mxInput, xStringResourceManager, aDialogSourceURL ), UNO_QUERY_THROW );
-        return xCtrlModel;
+        return createDialogModel( m_BasicInfo->mxInput, xStringResourceManager, aURL );
     }
 
-    Reference< XControlModel > DialogProviderImpl::createDialogModel( const OUString& sURL )
+    Reference< XUnoControlDialogModel > DialogProviderImpl::createDialogModel( const OUString& sURL )
     {
 
         OUString aURL( sURL );
@@ -447,7 +437,7 @@ static OUString aResourceResolverPropName("ResourceResolver");
         }
 
         // import dialog model
-        Reference< XControlModel > xCtrlModel;
+        Reference< XUnoControlDialogModel > xCtrlModel;
         if ( xInput.is() && m_xContext.is() )
         {
             Reference< resource::XStringResourceManager > xStringResourceManager;
@@ -460,12 +450,7 @@ static OUString aResourceResolverPropName("ResourceResolver");
                 xStringResourceManager = getStringResourceFromDialogLibrary( xDialogLib );
             }
 
-            Any aDialogSourceURLAny;
-            aDialogSourceURLAny <<= aURL;
-
-            Reference< container::XNameContainer > xDialogModel( createDialogModel( xInput , xStringResourceManager, aDialogSourceURLAny  ), UNO_QUERY_THROW);
-
-            xCtrlModel = Reference< XControlModel >( xDialogModel, UNO_QUERY );
+            xCtrlModel = createDialogModel( xInput , xStringResourceManager, aURL  );
         }
         return xCtrlModel;
     }

basctl/source/basicide/baside3.cxx
@@ -35,6 +35,7 @@
 #include "objdlg.hxx"
 
 #include <basic/basmgr.hxx>
+#include <com/sun/star/awt/UnoControlDialogModel.hpp>
 #include <com/sun/star/resource/StringResourceWithLocation.hpp>
 #include <com/sun/star/ucb/SimpleFileAccess.hpp>
 #include <com/sun/star/ui/dialogs/ExtendedFilePickerElementIds.hpp>
@@ -944,8 +945,7 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
 {
     bool bDone = false;
 
-    Reference< lang::XMultiServiceFactory > xMSF( ::comphelper::getProcessServiceFactory() );
-    Reference< XComponentContext > xContext( comphelper::getComponentContext( xMSF ) );
+    Reference< XComponentContext > xContext( ::comphelper::getProcessComponentContext() );
     Reference < XFilePicker3 > xFP = FilePicker::createWithMode(xContext, TemplateDescription::FILEOPEN_SIMPLE);
 
     Reference< XFilePickerControlAccess > xFPControl(xFP, UNO_QUERY);
@@ -977,10 +977,9 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
         try
         {
             // create dialog model
-            Reference< container::XNameContainer > xDialogModel( xMSF->createInstance
-                ( "com.sun.star.awt.UnoControlDialogModel" ), UNO_QUERY_THROW );
+            Reference< awt::XUnoControlDialogModel > xDialogModel = awt::UnoControlDialogModel::create( xContext );
 
-            Reference< XSimpleFileAccess3 > xSFI( SimpleFileAccess::create(comphelper::getProcessComponentContext()) );
+            Reference< XSimpleFileAccess3 > xSFI( SimpleFileAccess::create(xContext) );
 
             Reference< XInputStream > xInput;
             if( xSFI->exists( aCurPath ) )
@@ -988,20 +987,7 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
 
             ::xmlscript::importDialogModel( xInput, xDialogModel, xContext, rDocument.isDocument() ? rDocument.getDocument() : Reference< frame::XModel >() );
 
-            OUString aXmlDlgName;
-            Reference< beans::XPropertySet > xDialogModelPropSet( xDialogModel, UNO_QUERY );
-            if( xDialogModelPropSet.is() )
-            {
-                try
-                {
-                    Any aXmlDialogNameAny = xDialogModelPropSet->getPropertyValue( DLGED_PROP_NAME );
-                    OUString aOUXmlDialogName;
-                    aXmlDialogNameAny >>= aOUXmlDialogName;
-                    aXmlDlgName = aOUXmlDialogName;
-                }
-                catch(const beans::UnknownPropertyException& )
-                {}
-            }
+            OUString aXmlDlgName = xDialogModel->getName();
             bool bValidName = !aXmlDlgName.isEmpty();
             OSL_ASSERT( bValidName );
             if( !bValidName )
@@ -1186,30 +1172,8 @@ bool implImportDialog( Window* pWin, const OUString& rCurPath, const ScriptDocum
 
             if( eNameClashMode == CLASH_RENAME_DIALOG )
             {
-                bool bRenamed = false;
-                if( xDialogModelPropSet.is() )
-                {
-                    try
-                    {
-                        Any aXmlDialogNameAny;
-                        aXmlDialogNameAny <<= OUString( aNewDlgName );
-                        xDialogModelPropSet->setPropertyValue( DLGED_PROP_NAME, aXmlDialogNameAny );
-                        bRenamed = true;
-                    }
-                    catch(const beans::UnknownPropertyException& )
-                    {}
-                }
-
-
-                if( bRenamed )
-                {
-                    LocalizationMgr::renameStringResourceIDs( rDocument, aLibName, aNewDlgName, xDialogModel );
-                }
-                else
-                {
-                    // TODO: Assertion?
-                    return bDone;
-                }
+                xDialogModel->setName( aNewDlgName );
+                LocalizationMgr::renameStringResourceIDs( rDocument, aLibName, aNewDlgName, xDialogModel );
             }
 
             Reference< XInputStreamProvider > xISP = ::xmlscript::exportDialogModel( xDialogModel, xContext, rDocument.isDocument() ? rDocument.getDocument() : Reference< frame::XModel >() );

basctl/source/basicide/basides3.cxx
@@ -26,8 +26,9 @@
 #include <localizationmgr.hxx>
 #include <dlgedview.hxx>
 #include <comphelper/processfactory.hxx>
-#include <com/sun/star/script/XLibraryContainer.hpp>
+#include <com/sun/star/awt/UnoControlDialogModel.hpp>
 #include <com/sun/star/container/XNameContainer.hpp>
+#include <com/sun/star/script/XLibraryContainer.hpp>
 #include <xmlscript/xmldlg_imexp.hxx>
 #include <sfx2/dispatch.hxx>
 #include <sfx2/docfac.hxx>
@@ -75,12 +76,9 @@ DialogWindow* Shell::CreateDlgWin( const ScriptDocument& rDocument, const OUStri
             if ( xISP.is() )
             {
                 // create dialog model
-                Reference< lang::XMultiServiceFactory > xMSF = getProcessServiceFactory();
-                Reference< container::XNameContainer > xDialogModel( xMSF->createInstance
-                    ( "com.sun.star.awt.UnoControlDialogModel" ), UNO_QUERY );
+                Reference< uno::XComponentContext > xContext = getProcessComponentContext();
+                Reference< awt::XUnoControlDialogModel > xDialogModel = awt::UnoControlDialogModel::create( xContext );
                 Reference< XInputStream > xInput( xISP->createInputStream() );
-                Reference< XComponentContext > xContext(
-                    comphelper::getComponentContext( xMSF ) );
                 ::xmlscript::importDialogModel( xInput, xDialogModel, xContext, rDocument.isDocument() ? rDocument.getDocument() : Reference< frame::XModel >() );
                 LocalizationMgr::setStringResourceAtDialog( rDocument, rLibName, aDlgName, xDialogModel );
 

basctl/source/basicide/moduldlg.cxx
@@ -28,6 +28,7 @@
 #include "iderdll.hxx"
 
 #include <basic/basmgr.hxx>
+#include <com/sun/star/awt/UnoControlDialogModel.hpp>
 #include <com/sun/star/script/XLibraryContainerPassword.hpp>
 #include <comphelper/processfactory.hxx>
 #include <sfx2/app.hxx>
@@ -284,31 +285,25 @@ void Shell::CopyDialogResources(
         return;
 
     // create dialog model
-    Reference< lang::XMultiServiceFactory > xMSF = ::comphelper::getProcessServiceFactory();
-    Reference< container::XNameContainer > xDialogModel = Reference< container::XNameContainer >( xMSF->createInstance
-        ( "com.sun.star.awt.UnoControlDialogModel" ), UNO_QUERY );
+    Reference< uno::XComponentContext > xContext = ::comphelper::getProcessComponentContext();
+    Reference< awt::XUnoControlDialogModel > xDialogModel = awt::UnoControlDialogModel::create( xContext );
     Reference< io::XInputStream > xInput( io_xISP->createInputStream() );
-    Reference< XComponentContext > xContext(
-        comphelper::getComponentContext( xMSF ) );
     ::xmlscript::importDialogModel( xInput, xDialogModel, xContext, rSourceDoc.isDocument() ? rSourceDoc.getDocument() : Reference< frame::XModel >() );
 
-    if( xDialogModel.is() )
+    if( bSourceLocalized && bDestLocalized )
     {
-        if( bSourceLocalized && bDestLocalized )
-        {
-            Reference< resource::XStringResourceResolver > xSourceStringResolver( xSourceMgr, UNO_QUERY );
-            LocalizationMgr::copyResourceForDroppedDialog( xDialogModel, rDlgName, xDestMgr, xSourceStringResolver );
-        }
-        else if( bSourceLocalized )
-        {
-            LocalizationMgr::resetResourceForDialog( xDialogModel, xSourceMgr );
-        }
-        else if( bDestLocalized )
-        {
-            LocalizationMgr::setResourceIDsForDialog( xDialogModel, xDestMgr );
-        }
-        io_xISP = ::xmlscript::exportDialogModel( xDialogModel, xContext, rDestDoc.isDocument() ? rDestDoc.getDocument() : Reference< frame::XModel >() );
+        Reference< resource::XStringResourceResolver > xSourceStringResolver( xSourceMgr, UNO_QUERY );
+        LocalizationMgr::copyResourceForDroppedDialog( xDialogModel, rDlgName, xDestMgr, xSourceStringResolver );
     }
+    else if( bSourceLocalized )
+    {
+        LocalizationMgr::resetResourceForDialog( xDialogModel, xSourceMgr );
+    }
+    else if( bDestLocalized )
+    {
+        LocalizationMgr::setResourceIDsForDialog( xDialogModel, xDestMgr );
+    }
+    io_xISP = ::xmlscript::exportDialogModel( xDialogModel, xContext, rDestDoc.isDocument() ? rDestDoc.getDocument() : Reference< frame::XModel >() );
 }
 
 

basctl/source/basicide/scriptdocument.cxx
@@ -25,17 +25,18 @@
 #include "doceventnotifier.hxx"
 #include "documentenumeration.hxx"
 
-#include <com/sun/star/uri/UriReferenceFactory.hpp>
-#include <com/sun/star/util/theMacroExpander.hpp>
+#include <com/sun/star/awt/XWindow2.hpp>
+#include <com/sun/star/awt/UnoControlDialogModel.hpp>
 #include <com/sun/star/document/MacroExecMode.hpp>
-#include <com/sun/star/frame/XStorable.hpp>
+#include <com/sun/star/document/XEmbeddedScripts.hpp>
 #include <com/sun/star/frame/FrameSearchFlag.hpp>
 #include <com/sun/star/frame/XDesktop.hpp>
 #include <com/sun/star/frame/XModel2.hpp>
-#include <com/sun/star/awt/XWindow2.hpp>
-#include <com/sun/star/document/XEmbeddedScripts.hpp>
+#include <com/sun/star/frame/XStorable.hpp>
 #include <com/sun/star/script/vba/XVBACompatibility.hpp>
 #include <com/sun/star/script/vba/XVBAModuleInfo.hpp>
+#include <com/sun/star/uri/UriReferenceFactory.hpp>
+#include <com/sun/star/util/theMacroExpander.hpp>
 
 #include <sfx2/objsh.hxx>
 #include <sfx2/app.hxx>
@@ -629,12 +630,7 @@ namespace basctl
                 if ( _rxExistingDialogModel.is() )
                     xDialogModel = _rxExistingDialogModel;
                 else
-                    xDialogModel.set(
-                        ( aContext->getServiceManager()->
-                          createInstanceWithContext(
-                              "com.sun.star.awt.UnoControlDialogModel",
-                              aContext ) ),
-                        UNO_QUERY_THROW );
+                    xDialogModel = css::awt::UnoControlDialogModel::create( aContext );
 
                 // import dialog model
                 Reference< XInputStreamProvider > xISP( aElement, UNO_QUERY_THROW );
@@ -753,14 +749,10 @@ namespace basctl
             // create new dialog model
             Reference< XComponentContext > aContext(
                 comphelper::getProcessComponentContext() );
-            Reference< XNameContainer > xDialogModel(
-                aContext->getServiceManager()->createInstanceWithContext(
-                    "com.sun.star.awt.UnoControlDialogModel", aContext ),
-                UNO_QUERY_THROW );
+            Reference< css::awt::XUnoControlDialogModel > xDialogModel = css::awt::UnoControlDialogModel::create( aContext );
 
             // set name property
-            Reference< XPropertySet > xDlgPSet( xDialogModel, UNO_QUERY_THROW );
-            xDlgPSet->setPropertyValue( DLGED_PROP_NAME, makeAny( _rDialogName ) );
+            xDialogModel->setName( _rDialogName );
 
             // export dialog model
             _out_rDialogProvider = ::xmlscript::exportDialogModel( xDialogModel, aContext, isDocument() ? getDocument() : Reference< XModel >() );

commit e96e8e3bf622d317cb5dcc6884d848a695ebfd41

scripting: get CreateUnoDialog() work again
Trivial reproducer:

Dim Dlg As Object
DialogLibraries.LoadLibrary("Standard")
Dlg = CreateUnoDialog(DialogLibraries.Standard.Dialog1)
Dlg.Execute()
Dlg.dispose()

Regression from 50b6d1812891493cf5fd0920d2a45b239f89b51b.

Change-Id: Ia62778c6d94f54e6097a307701e5c81be847665d
Reviewed-on: https://gerrit.libreoffice.org/4023
Reviewed-by: Miklos Vajna <vmiklos@suse.cz>
Tested-by: Miklos Vajna <vmiklos@suse.cz>

scripting/source/dlgprov/dlgprov.cxx
@@ -149,9 +149,10 @@ static OUString aResourceResolverPropName("ResourceResolver");
         // Set resource property
         if( xStringResourceManager.is() )
         {
+            Reference< beans::XPropertySet > xDlgPSet( xDialogModel, UNO_QUERY );
             Any aStringResourceManagerAny;
             aStringResourceManagerAny <<= xStringResourceManager;
-            xDialogModel->setPropertyValue( aResourceResolverPropName, aStringResourceManagerAny );
+            xDlgPSet->setPropertyValue( aResourceResolverPropName, aStringResourceManagerAny );
         }
 
         return xDialogModel;