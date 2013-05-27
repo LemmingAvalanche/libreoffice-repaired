LemmingAvalanche/libreoffice-repaired

commit d01bf37d115daba15e431e7d72da80e4c99d7c60

reduce use of UniString ctor from null-terminated unicodearray
Change-Id: I98361309ada084d8f78517ce88f210d785f59cc0

sc/source/filter/dif/difimp.cxx
@@ -70,7 +70,7 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
     sal_Bool        bSyntErrWarn = false;
     sal_Bool        bOverflowWarn = false;
 
-    String&     rData = aDifParser.aData;
+    rtl::OUString   aData = aDifParser.aData;
     sal_Bool        bData = false;
 
     rIn.Seek( 0 );
@@ -83,7 +83,7 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
 
         aPrgrsBar.Progress();
 
-        bData = rData.Len() > 0;
+        bData = !aData.isEmpty();
 
         switch( eTopic )
         {
@@ -92,7 +92,7 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
                 if( aDifParser.nVector != 0 || aDifParser.nVal != 1 )
                     bSyntErrWarn = sal_True;
                 if( bData )
-                    pDoc->RenameTab( nBaseTab, rData );
+                    pDoc->RenameTab( nBaseTab, aData );
             }
                 break;
             case T_VECTORS:
@@ -164,14 +164,14 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
                     if( ValidCol(nColCnt) && ValidRow(nRowCnt) )
                     {
                         ScBaseCell*     pCell;
-                        if( DifParser::IsV( rData.GetBuffer() ) )
+                        if( DifParser::IsV( aData.getStr() ) )
                         {
                             pCell = new ScValueCell( aDifParser.fVal );
                             if( !bPlain )
                                 aAttrCache.SetNumFormat( nColCnt, nRowCnt,
                                     aDifParser.nNumFormat );
                         }
-                        else if( rData == pKeyTRUE || rData == pKeyFALSE )
+                        else if( aData == pKeyTRUE || aData == pKeyFALSE )
                         {
                             pCell = new ScValueCell( aDifParser.fVal );
                             if( bPlain )
@@ -180,12 +180,12 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
                                 aAttrCache.SetNumFormat( nColCnt, nRowCnt,
                                     aDifParser.nNumFormat );
                         }
-                        else if( rData == pKeyNA || rData == pKeyERROR  )
-                            pCell = new ScStringCell( rData );
+                        else if( aData == pKeyNA || aData == pKeyERROR  )
+                            pCell = new ScStringCell( aData );
                         else
                         {
                             String aTmp( RTL_CONSTASCII_USTRINGPARAM( "#IND: " ));
-                            aTmp += rData;
+                            aTmp += aData;
                             aTmp += sal_Unicode('?');
                             pCell = new ScStringCell( aTmp );
                         }
@@ -203,10 +203,10 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
 
                     if( ValidCol(nColCnt) && ValidRow(nRowCnt) )
                     {
-                        if( rData.Len() > 0 )
+                        if (!aData.isEmpty())
                         {
                             pDoc->PutCell( nColCnt, nRowCnt, nBaseTab,
-                                ScBaseCell::CreateTextCell( rData, pDoc ), true );
+                                ScBaseCell::CreateTextCell( aData, pDoc ), true );
                         }
                     }
                     else
@@ -434,7 +434,7 @@ DATASET DifParser::GetNumberDataset( const sal_Unicode* pPossibleNumericData )
     else
     {   // ...und zur Strafe mit'm Numberformatter...
         OSL_ENSURE( pNumFormatter, "-DifParser::GetNextDataset(): No Formatter, more fun!" );
-        String aTestVal( pPossibleNumericData );
+        rtl::OUString aTestVal( pPossibleNumericData );
         sal_uInt32 nFormat = 0;
         double fTmpVal;
         if( pNumFormatter->IsNumberFormat( aTestVal, nFormat, fTmpVal ) )

basic/source/classes/image.cxx
@@ -480,12 +480,9 @@ String SbiImage::GetString( short nId ) const
             }
         }
         else
-        {
-            String aStr( pStr );
-            return aStr;
-        }
+            return rtl::OUString(pStr);
     }
-    return String();
+    return rtl::OUString();
 }
 
 const SbxObject* SbiImage::FindType (String aTypeName) const

basic/source/sbx/sbxexec.cxx
@@ -153,7 +153,7 @@ static SbxVariable* Operand
     {
         // A number could be scanned in directly!
         sal_uInt16 nLen;
-        if( !refVar->Scan( XubString( p ), &nLen ) )
+        if( !refVar->Scan( rtl::OUString( p ), &nLen ) )
             refVar.Clear();
         else
             p += nLen;

connectivity/source/drivers/flat/ETable.cxx
@@ -504,11 +504,11 @@ String OFlatTable::getEntry()
         }
         xDir->beforeFirst(); // move back to before first record
     }
-    catch(Exception&)
+    catch(const Exception&)
     {
         OSL_ASSERT(0);
     }
-    return sURL.getStr();
+    return sURL;
 }
 // -------------------------------------------------------------------------
 void OFlatTable::refreshColumns()

cui/source/dialogs/scriptdlg.cxx
@@ -535,7 +535,7 @@ SvxScriptOrgDialog::SvxScriptOrgDialog( Window* pParent, ::rtl::OUString languag
     // must be a neater way to deal with the strings than as above
     // append the language to the dialog title
     String winTitle( GetText() );
-    winTitle.SearchAndReplace( String::CreateFromAscii( "%MACROLANG" ), language.pData->buffer );
+    winTitle.SearchAndReplace( String::CreateFromAscii( "%MACROLANG" ), m_sLanguage );
     SetText( winTitle );
 
     aScriptsBox.SetSelectHdl( LINK( this, SvxScriptOrgDialog, ScriptSelectHdl ) );

dbaccess/source/ui/browser/unodatbr.cxx
@@ -1857,7 +1857,7 @@ FeatureState SbaTableQueryBrowser::GetState(sal_uInt16 nId) const
                     }
                     ::rtl::OUString aName;
                     xProp->getPropertyValue(PROPERTY_COMMAND) >>= aName;
-                    String sObject(aName.getStr());
+                    String sObject(aName);
 
                     sTitle.SearchAndReplace('#',sObject);
                     aReturn.sTitle = sTitle;
@@ -2873,7 +2873,7 @@ void SAL_CALL SbaTableQueryBrowser::elementRemoved( const ContainerEvent& _rEven
     SvLBoxEntry* pContainer = getEntryFromContainer(xNames);
     if ( pContainer )
     { // a query or table has been removed
-        String aName = ::comphelper::getString(_rEvent.Accessor).getStr();
+        String aName = ::comphelper::getString(_rEvent.Accessor);
 
         if ( isCurrentlyDisplayedChanged( aName, pContainer) )
         {   // the element displayed currently has been replaced
@@ -2924,7 +2924,7 @@ void SAL_CALL SbaTableQueryBrowser::elementReplaced( const ContainerEvent& _rEve
     SvLBoxEntry* pContainer = getEntryFromContainer(xNames);
     if ( pContainer )
     {    // a table or query as been replaced
-        String aName = ::comphelper::getString(_rEvent.Accessor).getStr();
+        String aName = ::comphelper::getString(_rEvent.Accessor);
 
         if ( isCurrentlyDisplayedChanged( aName, pContainer) )
         {   // the element displayed currently has been replaced

dbaccess/source/ui/dlg/DbAdminImpl.cxx
@@ -971,7 +971,7 @@ void ODbDataSourceAdministrationHelper::implTranslateProperty( SfxItemSet& _rSet
             {
                 ::rtl::OUString sValue;
                 _rValue >>= sValue;
-                _rSet.Put(SfxStringItem(_nId, sValue.getStr()));
+                _rSet.Put(SfxStringItem(_nId, sValue));
             }
             else {
                 OSL_FAIL(

dbaccess/source/ui/dlg/dbfindex.cxx
@@ -350,7 +350,6 @@ void ODbaseIndexDialog::Init()
 
     ::std::vector< String > aUsedIndexes;
 
-    String aExt;
     const ::rtl::OUString *pBegin = aFolderContent.getConstArray();
     const ::rtl::OUString *pEnd   = pBegin + aFolderContent.getLength();
     aURL.SetSmartProtocol(INET_PROT_FILE);
@@ -359,12 +358,12 @@ void ODbaseIndexDialog::Init()
         rtl::OUString aName;
         ::utl::LocalFileHelper::ConvertURLToPhysicalName(pBegin->getStr(),aName);
         aURL.SetSmartURL(aName);
-        aExt = aURL.getExtension();
-        if(aExt == aIndexExt.getStr())
+        rtl::OUString aExt = aURL.getExtension();
+        if (aExt == aIndexExt)
         {
             m_aFreeIndexList.push_back( OTableIndex(aURL.getName()) );
         }
-        else if(aExt == aTableExt.getStr())
+        else if (aExt == aTableExt)
         {
             m_aTableInfoList.push_back( OTableInfo(aURL.getName()) );
             OTableInfo& rTabInfo = m_aTableInfoList.back();

commit ead6424ec135b6f9ce960f1c0e20c16413d061f7

we need a reference to the DifParser.aData, fdo#64920
regression from d01bf37d115daba15e431e7d72da80e4c99d7c60

Change-Id: If7fc18e7d0bcbf1075a0ecdb1c0dcf3d207f1bda

sc/source/filter/dif/difimp.cxx
@@ -61,7 +61,7 @@ FltError ScFormatFilterPluginImpl::ScImportDif( SvStream& rIn, ScDocument* pDoc,
     bool        bSyntErrWarn = false;
     bool        bOverflowWarn = false;
 
-    OUString   aData = aDifParser.aData;
+    OUString&   aData = aDifParser.aData;
     bool        bData = false;
 
     rIn.Seek( 0 );
@@ -377,7 +377,7 @@ TOPIC DifParser::GetNextTopic( void )
                 if( aLine.getLength() > 2 )
                     aData = aLine.copy( 1, aLine.getLength() - 2 );
                 else
-                    aData.Erase();
+                    aData = OUString();
                 eS = S_END;
                 break;
             case S_END:
@@ -400,20 +400,14 @@ TOPIC DifParser::GetNextTopic( void )
 }
 
 
-static void lcl_DeEscapeQuotesDif( String& rString )
+static void lcl_DeEscapeQuotesDif( OUString& rString )
 {
     //  Special handling for DIF import: Escaped (duplicated) quotes are resolved.
     //  Single quote characters are left in place because older versions didn't
     //  escape quotes in strings (and Excel doesn't when using the clipboard).
     //  The quotes around the string are removed before this function is called.
 
-    static const sal_Unicode aDQ[] = { '"', '"', 0 };
-    xub_StrLen nPos = 0;
-    while ( (nPos = rString.Search( aDQ, nPos )) != STRING_NOTFOUND )
-    {
-        rString.Erase( nPos, 1 );
-        ++nPos;
-    }
+    rString = rString.replaceAll("\"\"", "\"");
 }
 
 // Determine if passed in string is numeric data and set fVal/nNumFormat if so
@@ -501,9 +495,9 @@ bool DifParser::LookAhead()
 
 DATASET DifParser::GetNextDataset( void )
 {
-    DATASET             eRet = D_UNKNOWN;
-    OUString       aLine;
-    const sal_Unicode*      pAktBuffer;
+    DATASET eRet = D_UNKNOWN;
+    OUString aLine;
+    const sal_Unicode* pAktBuffer;
 
     ReadNextLine( aLine );
 
@@ -533,15 +527,14 @@ DATASET DifParser::GetNextDataset( void )
                 ReadNextLine( aTmpLine );
                 if ( eRet == D_SYNT_ERROR )
                 {   // for broken records write "#ERR: data" to cell
-                    OUString aTmp = "#ERR: " + OUString( pAktBuffer ) + " (";
-                    OSL_ENSURE( aTmpLine.getLength() <= STRING_MAXLEN - aTmp.getLength() - 1, "GetNextDataset(): line doesn't fit into data");
-                    aTmp += aTmpLine + ")";
-                    aData = aTmp;
+                    OUStringBuffer aTmp("#ERR: ");
+                    aTmp.append(pAktBuffer).append(" (");
+                    aTmp.append(aTmpLine).append(')');
+                    aData = aTmp.makeStringAndClear();
                     eRet = D_STRING;
                 }
                 else
                 {
-                    OSL_ENSURE( aTmpLine.getLength() <= STRING_MAXLEN, "GetNextDataset(): line doesn't fit into data");
                     aData = aTmpLine;
                 }
             }
@@ -563,7 +556,6 @@ DATASET DifParser::GetNextDataset( void )
                         // Single line string
                         if( nLineLength >= 2 && pLine[nLineLength - 1] == '"' )
                         {
-                            OSL_ENSURE( aLine.getLength() - 2 <= STRING_MAXLEN, "GetNextDataset(): line doesn't fit into data");
                             aData = aLine.copy( 1, nLineLength - 2 );
                             lcl_DeEscapeQuotesDif( aData );
                             eRet = D_STRING;
@@ -572,12 +564,11 @@ DATASET DifParser::GetNextDataset( void )
                     else
                     {
                         // Multiline string
-                        OSL_ENSURE( aLine.getLength() - 1 <= STRING_MAXLEN, "GetNextDataset(): line doesn't fit into data");
                         aData = aLine.copy( 1 );
                         bool bContinue = true;
                         while ( bContinue )
                         {
-                            aData.Append( '\n' );
+                            aData = aData + "\n";
                             bContinue = !rIn.IsEof() && ReadNextLine( aLine );
                             if( bContinue )
                             {
@@ -588,13 +579,11 @@ DATASET DifParser::GetNextDataset( void )
                                     bContinue = !LookAhead();
                                     if( bContinue )
                                     {
-                                        OSL_ENSURE( aLine.getLength() <= STRING_MAXLEN - aData.Len(), "GetNextDataset(): line doesn't fit into data");
-                                        aData.Append( aLine );
+                                        aData = aData + aLine;
                                     }
                                     else if( pLine[nLineLength - 1] == '"' )
                                     {
-                                        OSL_ENSURE( nLineLength - 1 <= STRING_MAXLEN - aData.Len(), "GetNextDataset(): line doesn't fit into data");
-                                        aData.Append( pLine, nLineLength - 1 );
+                                        aData = aData + aLine.copy(0, nLineLength -1 );
                                         lcl_DeEscapeQuotesDif( aData );
                                         eRet = D_STRING;
                                     }

sc/source/filter/inc/dif.hxx
@@ -62,7 +62,7 @@ class ScPatternAttr;
 class DifParser
 {
 public:
-    String              aData;
+    OUString              aData;
     double              fVal;
     sal_uInt32              nVector;
     sal_uInt32              nVal;