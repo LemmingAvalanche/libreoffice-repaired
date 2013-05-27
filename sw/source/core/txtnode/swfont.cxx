LemmingAvalanche/libreoffice-repaired

commit e03819e5b4f8370d716daa1b674d50d09eee2689

fix stupid over-optimization
There's so little to be gained by storing pointers to simple
or refcounted types instead of using an instance. At least
SwTxtNode::GetMinMaxSize() and SwTxtNode::GetScalingOfSelectedText()
pass string temporaries to SwDrawTextInfo, so keeping a pointer
is clearly broken, and MSVC manages to optimize the code enough
to crash because of this. As all the pointers point to const references
and are accesses using const references as well, there shouldn't
be any harm in making a copy.
This fixes smoketest crash on Windows.

Change-Id: I7d7cb42e175a2e64fe9c84c98c0d0204a0c25c13

sw/source/core/txtnode/swfont.cxx
@@ -1163,17 +1163,17 @@ void SwDrawTextInfo::Shift( sal_uInt16 nDir )
     switch ( nDir )
     {
     case 0 :
-        ((Point*)pPos)->X() += GetSize().Width();
+        pPos.X() += GetSize().Width();
         break;
     case 900 :
-        OSL_ENSURE( ((Point*)pPos)->Y() >= GetSize().Width(), "Going underground" );
-        ((Point*)pPos)->Y() -= GetSize().Width();
+        OSL_ENSURE( pPos.Y() >= GetSize().Width(), "Going underground" );
+        pPos.Y() -= GetSize().Width();
         break;
     case 1800 :
-        ((Point*)pPos)->X() -= GetSize().Width();
+        pPos.X() -= GetSize().Width();
         break;
     case 2700 :
-        ((Point*)pPos)->Y() += GetSize().Width();
+        pPos.Y() += GetSize().Width();
         break;
     }
 }

sw/source/core/inc/drawfont.hxx
@@ -42,12 +42,12 @@ class SwDrawTextInfo
     OutputDevice* pOut;
     ViewShell* pSh;
     const SwScriptInfo* pScriptInfo;
-    const Point* pPos;
-    const OUString* pText;
+    Point pPos;
+    OUString pText;
     const SwWrongList* pWrong;
     const SwWrongList* pGrammarCheck;
     const SwWrongList* pSmartTags;
-    const Size* pSize;
+    Size pSize;
     SwFont *pFnt;
     SwUnderlineFont* pUnderFnt;
     xub_StrLen* pHyphPos;
@@ -111,7 +111,7 @@ public:
         pSh = pS;
         pOut = &rO;
         pScriptInfo = pSI;
-        pText = &rSt;
+        pText = rSt;
         nIdx = nI;
         nLen = nL;
         nKern = 0;
@@ -129,11 +129,9 @@ public:
 
         // These values are initialized but have to be set explicitly via their
         // Set-function before they may be accessed by their Get-function:
-        pPos = 0;
         pWrong = 0;
         pGrammarCheck = 0;
         pSmartTags = 0;
-        pSize = 0;
         pFnt = 0;
         pHyphPos = 0;
         nLeft = 0;
@@ -192,7 +190,7 @@ public:
 #ifdef DBG_UTIL
         OSL_ENSURE( m_bPos, "DrawTextInfo: Undefined Position" );
 #endif
-        return *pPos;
+        return pPos;
     }
 
     xub_StrLen *GetHyphPos() const
@@ -205,7 +203,7 @@ public:
 
     const OUString &GetText() const
     {
-        return *pText;
+        return pText;
     }
 
     const SwWrongList* GetWrong() const
@@ -234,7 +232,7 @@ public:
 #ifdef DBG_UTIL
         OSL_ENSURE( m_bSize, "DrawTextInfo: Undefined Size" );
 #endif
-        return *pSize;
+        return pSize;
     }
 
     SwFont* GetFont() const
@@ -402,7 +400,7 @@ public:
 
     void SetPos( const Point &rNew )
     {
-        pPos = &rNew;
+        pPos = rNew;
 #ifdef DBG_UTIL
         m_bPos = true;
 #endif
@@ -418,7 +416,7 @@ public:
 
     void SetText( const OUString &rNew )
     {
-        pText = &rNew;
+        pText = rNew;
     }
 
     void SetWrong( const SwWrongList* pNew )
@@ -444,7 +442,7 @@ public:
 
     void SetSize( const Size &rNew )
     {
-        pSize = &rNew;
+        pSize = rNew;
 #ifdef DBG_UTIL
         m_bSize = true;
 #endif

commit 0d688502351c3e5f29013acbb89ab4e5c99f1833

Fix fdo#64685 - Superscript broken in master
After e03819e5b4f8370d716daa1b674d50d09eee2689 the position member of
SwDrawTextInfo is no longer a pointer, so changing the passed Point
after calling SetPos() has no effect, but some code seems to be relying
on it being pointer. Moving SetPos() to after changing the point seems
to do the trick, but there may be other similarly broken code like
SwSubFont::_DrawStretchText() below.

Change-Id: I2f563c1543ff7bff68e573f69fce39694181b370
Reviewed-on: https://gerrit.libreoffice.org/4032
Reviewed-by: David Tardon <dtardon@redhat.com>
Tested-by: David Tardon <dtardon@redhat.com>

sw/source/core/txtnode/swfont.cxx
@@ -850,12 +850,12 @@ void SwSubFont::_DrawText( SwDrawTextInfo &rInf, const sal_Bool bGrey )
     SwDigitModeModifier aDigitModeModifier( rInf.GetOut(), rInf.GetFont()->GetLanguage() );
 
     Point aPos( rInf.GetPos() );
-    const Point &rOld = rInf.GetPos();
-    rInf.SetPos( aPos );
 
     if( GetEscapement() )
         CalcEsc( rInf, aPos );
 
+    const Point &rOld = rInf.GetPos();
+    rInf.SetPos( aPos );
     rInf.SetKern( CheckKerning() + rInf.GetSperren() / SPACING_PRECISION_FACTOR );
 
     if( IsCapital() )