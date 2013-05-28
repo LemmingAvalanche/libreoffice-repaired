LemmingAvalanche/libreoffice-repaired

commit 583505d108c0be92d34f3ad8a1e49f6a8294d2bb

coverity#705732: Resource leak
Change-Id: I926c3cbe25b7b4310d67a8f9e96b4a98e61b26e0

svx/source/svdraw/svdundo.cxx
@@ -1136,16 +1136,13 @@ void SdrUndoObjSetText::Undo()
 
 void SdrUndoObjSetText::Redo()
 {
-    // copy text for Undo, because the original now belongs to SetOutlinerParaObject()
-    OutlinerParaObject* pText1 = pNewText;
-
-    if(pText1)
-        pText1 = new OutlinerParaObject(*pText1);
-
     SdrText* pText = static_cast< SdrTextObj*>( pObj )->getText(mnText);
-    if( pText )
+    if( pText && pNewText)
+    {
+        // copy text for Undo, because the original now belongs to SetOutlinerParaObject()
+        OutlinerParaObject* pText1 = new OutlinerParaObject(*pNewText);
         static_cast< SdrTextObj* >( pObj )->NbcSetOutlinerParaObjectForText( pText1, pText );
-
+    }
     pObj->ActionChanged();
 
     // Trigger PageChangeCall

commit b8729e1dd6ed7a37664580a55acf8183052e3ebc

Fix editing text in draw object undo regression
Regression since 583505d108c0be92d34f3ad8a1e49f6a8294d2bb and
b869ce9eda4091d9087c762c8abf7c735472b723 pText1 can legally be NULL

Change-Id: I0d54f4aeaa18834c32daeabad43776bc4f68088b

svx/source/svdraw/svdundo.cxx
@@ -1144,10 +1144,10 @@ void SdrUndoObjSetText::Undo()
         AfterSetText();
 
     SdrText* pText = static_cast< SdrTextObj*>( pObj )->getText(mnText);
-    if (pText && pOldText)
+    if (pText)
     {
         // copy text for Undo, because the original now belongs to SetOutlinerParaObject()
-        OutlinerParaObject* pText1 = new OutlinerParaObject(*pOldText);
+        OutlinerParaObject* pText1 = pOldText ? new OutlinerParaObject(*pOldText) : NULL;
         pText->SetOutlinerParaObject(pText1);
     }
 
@@ -1158,10 +1158,10 @@ void SdrUndoObjSetText::Undo()
 void SdrUndoObjSetText::Redo()
 {
     SdrText* pText = static_cast< SdrTextObj*>( pObj )->getText(mnText);
-    if (pText && pNewText)
+    if (pText)
     {
         // copy text for Undo, because the original now belongs to SetOutlinerParaObject()
-        OutlinerParaObject* pText1 = new OutlinerParaObject(*pNewText);
+        OutlinerParaObject* pText1 = pNewText ? new OutlinerParaObject(*pNewText) : NULL;
         static_cast< SdrTextObj* >( pObj )->NbcSetOutlinerParaObjectForText( pText1, pText );
     }
     pObj->ActionChanged();