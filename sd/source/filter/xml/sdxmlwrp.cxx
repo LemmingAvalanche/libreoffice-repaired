LemmingAvalanche/libreoffice-repaired

commit 39224e5b10c3517c2fd6517f42e0247f1b7f2a44

Remove RTL_CONSTASCII_(U)STRINGPARAM (sd)
Change-Id: I179208d5ffe94e95719c7c6d0ea4eb354141531a
Reviewed-on: https://gerrit.libreoffice.org/4107
Reviewed-by: David Tardon <dtardon@redhat.com>
Tested-by: David Tardon <dtardon@redhat.com>

sd/source/filter/xml/sdxmlwrp.cxx
@@ -268,8 +268,7 @@ sal_Int32 ReadThroughComponent(
             return ERRCODE_SFX_WRONGPASSWORD;
 
 #if OSL_DEBUG_LEVEL > 1
-        OStringBuffer aError(RTL_CONSTASCII_STRINGPARAM(
-            "SAX parse exception caught while importing:\n"));
+        OStringBuffer aError("SAX parse exception caught while importing:\n");
         aError.append(OUStringToOString(r.Message,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aError.getStr());
@@ -304,8 +303,7 @@ sal_Int32 ReadThroughComponent(
             return ERRCODE_SFX_WRONGPASSWORD;
 
 #if OSL_DEBUG_LEVEL > 1
-        OStringBuffer aError(RTL_CONSTASCII_STRINGPARAM(
-            "SAX exception caught while importing:\n"));
+        OStringBuffer aError("SAX exception caught while importing:\n");
         aError.append(OUStringToOString(r.Message,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aError.getStr());
@@ -315,8 +313,7 @@ sal_Int32 ReadThroughComponent(
     catch (const packages::zip::ZipIOException& r)
     {
 #if OSL_DEBUG_LEVEL > 1
-        OStringBuffer aError(RTL_CONSTASCII_STRINGPARAM(
-            "Zip exception caught while importing:\n"));
+        OStringBuffer aError("Zip exception caught while importing:\n");
         aError.append(OUStringToOString(r.Message,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aError.getStr());
@@ -328,8 +325,7 @@ sal_Int32 ReadThroughComponent(
     catch (const io::IOException& r)
     {
 #if OSL_DEBUG_LEVEL > 1
-        OStringBuffer aError(RTL_CONSTASCII_STRINGPARAM(
-            "IO exception caught while importing:\n"));
+        OStringBuffer aError("IO exception caught while importing:\n");
         aError.append(OUStringToOString(r.Message,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aError.getStr());
@@ -341,8 +337,7 @@ sal_Int32 ReadThroughComponent(
     catch (const uno::Exception& r)
     {
 #if OSL_DEBUG_LEVEL > 1
-        OStringBuffer aError(RTL_CONSTASCII_STRINGPARAM(
-            "uno exception caught while importing:\n"));
+        OStringBuffer aError("uno exception caught while importing:\n");
         aError.append(OUStringToOString(r.Message,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aError.getStr());
@@ -810,8 +805,8 @@ sal_Bool SdXMLFilter::Import( ErrCode& nError )
                     {
                         const String& rTypeName = pFilter->GetRealTypeName();
 
-                        if( (rTypeName.CompareToAscii( RTL_CONSTASCII_STRINGPARAM("impress_StarImpress" ) ) == 0) ||
-                            (rTypeName.CompareToAscii( RTL_CONSTASCII_STRINGPARAM("draw_StarDraw" ) ) == 0) )
+                        if( (rTypeName.CompareToAscii( "impress_StarImpress" ) == 0) ||
+                            (rTypeName.CompareToAscii( "draw_StarDraw" ) == 0) )
                         {
                             bTransform = true;
                         }
@@ -1071,8 +1066,7 @@ sal_Bool SdXMLFilter::Export()
     catch (const uno::Exception &e)
     {
 #if OSL_DEBUG_LEVEL > 1
-        OStringBuffer aError(RTL_CONSTASCII_STRINGPARAM(
-            "uno Exception caught while exporting:\n"));
+        OStringBuffer aError("uno Exception caught while exporting:\n");
         aError.append(OUStringToOString(e.Message,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aError.getStr());

sd/source/core/CustomAnimationPreset.cxx
@@ -471,7 +471,7 @@ void CustomAnimationPresets::importPresets( const Reference< XMultiServiceFactor
                         else
                         {
                             aMissedPresetIds += String(*pEffectNames);
-                            aMissedPresetIds += String( RTL_CONSTASCII_USTRINGPARAM("\n") );
+                            aMissedPresetIds += String( "\n" );
                         }
 #endif
                         pEffectNames++;
@@ -491,7 +491,7 @@ void CustomAnimationPresets::importPresets( const Reference< XMultiServiceFactor
 #ifdef DEBUG
     if( aMissedPresetIds.Len() )
     {
-        OStringBuffer aTmp(RTL_CONSTASCII_STRINGPARAM("sd::CustomAnimationPresets::importPresets(), invalid preset id!\n"));
+        OStringBuffer aTmp("sd::CustomAnimationPresets::importPresets(), invalid preset id!\n");
         aTmp.append(OUStringToOString(aMissedPresetIds,
             RTL_TEXTENCODING_ASCII_US));
         OSL_FAIL(aTmp.getStr());

sd/source/core/drawdoc3.cxx
@@ -1667,7 +1667,7 @@ void SdDrawDocument::SetMasterPage(sal_uInt16 nSdPageNum,
         }
         aName = aTest;
         String aPageLayoutName(aName);
-        aPageLayoutName.AppendAscii( RTL_CONSTASCII_STRINGPARAM( SD_LT_SEPARATOR ));
+        aPageLayoutName.AppendAscii( SD_LT_SEPARATOR );
         aPageLayoutName += String(SdResId(STR_LAYOUT_OUTLINE));
 
         // Generate new stylesheets

sd/source/core/drawdoc4.cxx
@@ -1110,7 +1110,7 @@ void SdDrawDocument::RenameLayoutTemplate(const String& rOldLayoutName, const St
     // Now set the layout name of the drawing and the notes page, as well as
     // their master pages.
     String aPageLayoutName(rNewName);
-    aPageLayoutName.AppendAscii( RTL_CONSTASCII_STRINGPARAM( SD_LT_SEPARATOR ));
+    aPageLayoutName.AppendAscii( SD_LT_SEPARATOR );
     aPageLayoutName += String(SdResId(STR_LAYOUT_OUTLINE));
 
     // Inform all text objects on pages that use the renamed layout and set the

sd/source/core/sdpage.cxx
@@ -561,7 +561,7 @@ SdrObject* SdPage::CreatePresObj(PresObjKind eObjKind, sal_Bool bVertical, const
 SfxStyleSheet* SdPage::GetStyleSheetForMasterPageBackground() const
 {
     String aName(GetLayoutName());
-    String aSep( RTL_CONSTASCII_USTRINGPARAM( SD_LT_SEPARATOR ));
+    String aSep( SD_LT_SEPARATOR );
     sal_uInt16 nPos = aName.Search(aSep);
 
     if (nPos != STRING_NOTFOUND)
@@ -580,7 +580,7 @@ SfxStyleSheet* SdPage::GetStyleSheetForMasterPageBackground() const
 SfxStyleSheet* SdPage::GetStyleSheetForPresObj(PresObjKind eObjKind) const
 {
     String aName(GetLayoutName());
-    String aSep( RTL_CONSTASCII_USTRINGPARAM( SD_LT_SEPARATOR ));
+    String aSep( SD_LT_SEPARATOR );
     sal_uInt16 nPos = aName.Search(aSep);
     if (nPos != STRING_NOTFOUND)
     {
@@ -631,7 +631,7 @@ SfxStyleSheet* SdPage::GetStyleSheetForPresObj(PresObjKind eObjKind) const
 SdStyleSheet* SdPage::getPresentationStyle( sal_uInt32 nHelpId ) const
 {
     String aStyleName( pPage->GetLayoutName() );
-    const String aSep( RTL_CONSTASCII_USTRINGPARAM( SD_LT_SEPARATOR ));
+    const String aSep( SD_LT_SEPARATOR );
     aStyleName.Erase(aStyleName.Search(aSep) + aSep.Len());
 
     sal_uInt16 nNameId;

sd/source/core/stlfamily.cxx
@@ -68,7 +68,7 @@ PresStyleMap& SdStyleFamilyImpl::getStyleSheets()
         maLayoutName = mxMasterPage->GetLayoutName();
 
         String aLayoutName( maLayoutName );
-        const sal_uInt16 nLen = aLayoutName.Search(String( RTL_CONSTASCII_USTRINGPARAM(SD_LT_SEPARATOR)))+4;
+        const sal_uInt16 nLen = aLayoutName.Search(String( SD_LT_SEPARATOR ))+4;
         aLayoutName.Erase( nLen );
 
         if( (maStyleSheets.empty()) || !((*maStyleSheets.begin()).second->GetName().Equals( aLayoutName, 0, nLen )) )
@@ -209,7 +209,7 @@ OUString SAL_CALL SdStyleFamily::getName() throw (RuntimeException)
             throw DisposedException();
 
         String aLayoutName( pPage->GetLayoutName() );
-        const String aSep( RTL_CONSTASCII_USTRINGPARAM( SD_LT_SEPARATOR ));
+        const String aSep( SD_LT_SEPARATOR );
         aLayoutName.Erase(aLayoutName.Search(aSep));
 
         return OUString( aLayoutName );

sd/source/core/stlpool.cxx
@@ -123,7 +123,7 @@ SfxStyleSheetBase* SdStyleSheetPool::Create(const SdStyleSheet& rStyle)
 SfxStyleSheetBase* SdStyleSheetPool::GetTitleSheet(const String& rLayoutName)
 {
     String aName(rLayoutName);
-    aName.AppendAscii( RTL_CONSTASCII_STRINGPARAM( SD_LT_SEPARATOR ));
+    aName.AppendAscii( SD_LT_SEPARATOR );
     aName += String(SdResId(STR_LAYOUT_TITLE));
     SfxStyleSheetBase* pResult = Find(aName, SD_STYLE_FAMILY_MASTERPAGE);
     return pResult;
@@ -139,7 +139,7 @@ SfxStyleSheetBase* SdStyleSheetPool::GetTitleSheet(const String& rLayoutName)
 void SdStyleSheetPool::CreateOutlineSheetList (const String& rLayoutName, std::vector<SfxStyleSheetBase*> &rOutlineStyles)
 {
     String aName(rLayoutName);
-    aName.AppendAscii( RTL_CONSTASCII_STRINGPARAM( SD_LT_SEPARATOR ));
+    aName.AppendAscii( SD_LT_SEPARATOR );
     aName += String(SdResId(STR_LAYOUT_OUTLINE));
 
     for (sal_uInt16 nSheet = 1; nSheet < 10; nSheet++)
@@ -170,7 +170,7 @@ void SdStyleSheetPool::CreateLayoutStyleSheets(const String& rLayoutName, sal_Bo
     SfxStyleSheetBase* pSheet = NULL;
 
     String aPrefix(rLayoutName);
-    String aSep( RTL_CONSTASCII_USTRINGPARAM( SD_LT_SEPARATOR ));
+    String aSep( SD_LT_SEPARATOR );
     aPrefix.Insert(aSep);
 
     Font aLatinFont, aCJKFont, aCTLFont;
@@ -715,7 +715,7 @@ void SdStyleSheetPool::CopyLayoutSheets(const String& rLayoutName, SdStyleSheetP
 void SdStyleSheetPool::CreateLayoutSheetNames(const String& rLayoutName, std::vector<String> &aNameList) const
 {
     String aPrefix(rLayoutName);
-    String aSep( RTL_CONSTASCII_USTRINGPARAM( SD_LT_SEPARATOR ));
+    String aSep( SD_LT_SEPARATOR );
     aPrefix.Insert(aSep);
 
     String aName(SdResId(STR_LAYOUT_OUTLINE));
@@ -761,7 +761,7 @@ void SdStyleSheetPool::CreateLayoutSheetNames(const String& rLayoutName, std::ve
 void SdStyleSheetPool::CreateLayoutSheetList(const String& rLayoutName, SdStyleSheetVector& rLayoutSheets )
 {
     String aLayoutNameWithSep(rLayoutName);
-    aLayoutNameWithSep.AppendAscii( RTL_CONSTASCII_STRINGPARAM( SD_LT_SEPARATOR ));
+    aLayoutNameWithSep.AppendAscii( SD_LT_SEPARATOR );
     sal_uInt16 nLen = aLayoutNameWithSep.Len();
 
     SfxStyleSheetIterator aIter(this, SD_STYLE_FAMILY_MASTERPAGE);

sd/source/filter/eppt/pptx-text.cxx
@@ -926,9 +926,9 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider& rBuProv, sal_Int1
                                 case SVX_NUM_CHARS_UPPER_LETTER :
                                 case SVX_NUM_CHARS_UPPER_LETTER_N :
                                 {
-                                    if ( sSuffix == String( RTL_CONSTASCII_USTRINGPARAM( ")" ) ) )
+                                    if ( sSuffix == String( ")" ) )
                                     {
-                                        if ( sPrefix == String( RTL_CONSTASCII_USTRINGPARAM( "(" ) ) )
+                                        if ( sPrefix == String( "(" ) )
                                             nMappedNumType = 0xa0001;   // (A)
                                         else
                                             nMappedNumType = 0xb0001;   // A)
@@ -940,9 +940,9 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider& rBuProv, sal_Int1
                                 case SVX_NUM_CHARS_LOWER_LETTER :
                                 case SVX_NUM_CHARS_LOWER_LETTER_N :
                                 {
-                                    if ( sSuffix == String( RTL_CONSTASCII_USTRINGPARAM( ")" ) ) )
+                                    if ( sSuffix == String( ")" ) )
                                     {
-                                        if ( sPrefix == String( RTL_CONSTASCII_USTRINGPARAM( "(" ) ) )
+                                        if ( sPrefix == String( "(" ) )
                                             nMappedNumType = 0x80001;   // (a)
                                         else
                                             nMappedNumType = 0x90001;   // a)
@@ -953,9 +953,9 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider& rBuProv, sal_Int1
                                 break;
                                 case SVX_NUM_ROMAN_UPPER :
                                 {
-                                    if ( sSuffix == String( RTL_CONSTASCII_USTRINGPARAM( ")" ) ) )
+                                    if ( sSuffix == String( ")" ) )
                                     {
-                                        if ( sPrefix == String( RTL_CONSTASCII_USTRINGPARAM( "(" ) ) )
+                                        if ( sPrefix == String( "(" ) )
                                             nMappedNumType = 0xe0001;   // (I)
                                         else
                                             nMappedNumType = 0xf0001;   // I)
@@ -966,9 +966,9 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider& rBuProv, sal_Int1
                                 break;
                                 case SVX_NUM_ROMAN_LOWER :
                                 {
-                                    if ( sSuffix == String( RTL_CONSTASCII_USTRINGPARAM( ")" ) ) )
+                                    if ( sSuffix == String( ")" ) )
                                     {
-                                        if ( sPrefix == String( RTL_CONSTASCII_USTRINGPARAM( "(" ) ) )
+                                        if ( sPrefix == String( "(" ) )
                                             nMappedNumType = 0x40001;   // (i)
                                         else
                                             nMappedNumType = 0x50001;   // i)
@@ -979,9 +979,9 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider& rBuProv, sal_Int1
                                 break;
                                 case SVX_NUM_ARABIC :
                                 {
-                                    if ( sSuffix == String( RTL_CONSTASCII_USTRINGPARAM( ")" ) ) )
+                                    if ( sSuffix == String( ")" ) )
                                     {
-                                        if ( sPrefix == String( RTL_CONSTASCII_USTRINGPARAM( "(" ) ) )
+                                        if ( sPrefix == String( "(" ) )
                                             nMappedNumType = 0xc0001;   // (1)
                                         else
                                             nMappedNumType = 0x20001;   // 1)
@@ -1054,7 +1054,7 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider& rBuProv, sal_Int1
 
 void ParagraphObj::ImplGetParagraphValues( PPTExBulletProvider& rBuProv, sal_Bool bGetPropStateValue )
 {
-    static String sNumberingLevel   ( RTL_CONSTASCII_USTRINGPARAM( "NumberingLevel" ) );
+    static String sNumberingLevel( "NumberingLevel" );
 
     ::com::sun::star::uno::Any aAny;
     if ( GetPropertyValue( aAny, mXPropSet, sNumberingLevel, sal_True ) )

commit c9df6d860e2547bdba66b6fd16e26d8aeb60b19c

UniString::CompareToAscii(RTL_CONSTASCII_STRINGPARAM("x"))==COMPARE_EQUAL ...
...corresponds to OUString::startsWith("x"), not
UniString::CompareToAscii("x")==COMPARE_EQUAL.  Regression introduced with
39224e5b10c3517c2fd6517f42e0247f1b7f2a44 "Remove RTL_CONSTASCII_(U)STRINGPARAM
(sd)."

Change-Id: I116defec056ff1e2260a20ba5279180683eefd87

sd/source/filter/xml/sdxmlwrp.cxx
@@ -803,10 +803,9 @@ sal_Bool SdXMLFilter::Import( ErrCode& nError )
                      const SfxFilter * pFilter = mrMedium.GetFilter();
                     if( pFilter )
                     {
-                        const String& rTypeName = pFilter->GetRealTypeName();
-
-                        if( (rTypeName.CompareToAscii( "impress_StarImpress" ) == 0) ||
-                            (rTypeName.CompareToAscii( "draw_StarDraw" ) == 0) )
+                        OUString typeName(pFilter->GetRealTypeName());
+                        if( typeName.startsWith( "impress_StarImpress" ) ||
+                            typeName.startsWith( "draw_StarDraw" ) )
                         {
                             bTransform = true;
                         }