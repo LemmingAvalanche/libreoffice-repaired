LemmingAvalanche/libreoffice-repaired

commit 5084224dd475c7d5164a67625b249859dbafc019

fdo#46808, Convert frame::Frame to new style
Change-Id: I74427d1e0059808f04960c648b93245b06c20f7f

sw/source/ui/envelp/mailmrge.cxx
@@ -42,19 +42,20 @@
 #include <sfx2/docfile.hxx>
 #include <sfx2/docfilt.hxx>
 #include <comphelper/sequenceashashmap.hxx>
-#include <com/sun/star/ui/dialogs/FolderPicker.hpp>
 #include <com/sun/star/frame/XDispatchProvider.hpp>
+#include <com/sun/star/container/XChild.hpp>
+#include <com/sun/star/container/XContainerQuery.hpp>
+#include <com/sun/star/container/XEnumeration.hpp>
+#include <com/sun/star/form/runtime/XFormController.hpp>
+#include <com/sun/star/frame/Frame.hpp>
 #include <com/sun/star/sdbcx/XColumnsSupplier.hpp>
 #include <com/sun/star/sdbcx/XRowLocate.hpp>
 #include <com/sun/star/sdb/XResultSetAccess.hpp>
 #include <com/sun/star/sdbc/XDataSource.hpp>
+#include <com/sun/star/ui/dialogs/FolderPicker.hpp>
 #include <toolkit/unohlp.hxx>
 #include <comphelper/processfactory.hxx>
-#include <com/sun/star/form/runtime/XFormController.hpp>
 #include <cppuhelper/implbase1.hxx>
-#include <com/sun/star/container/XChild.hpp>
-#include <com/sun/star/container/XContainerQuery.hpp>
-#include <com/sun/star/container/XEnumeration.hpp>
 
 #include <unomid.h>
 
@@ -267,24 +268,17 @@ SwMailMergeDlg::SwMailMergeDlg(Window* pParent, SwWrtShell& rShell,
         try
         {
             // create a frame wrapper for myself
-            uno::Reference< XMultiServiceFactory >
-                                        xMgr = comphelper::getProcessServiceFactory();
-            xFrame = uno::Reference< XFrame >(xMgr->createInstance("com.sun.star.frame.Frame"), UNO_QUERY);
-            if(xFrame.is())
-            {
-                xFrame->initialize( VCLUnoHelper::GetInterface ( pBeamerWin ) );
-            }
+            m_xFrame = frame::Frame::create( comphelper::getProcessComponentContext() );
         }
         catch (const Exception&)
         {
-            xFrame.clear();
+            m_xFrame.clear();
         }
-        if(xFrame.is())
+        if(m_xFrame.is())
         {
-            uno::Reference<XDispatchProvider> xDP(xFrame, UNO_QUERY);
             URL aURL;
             aURL.Complete = ".component:DB/DataSourceBrowser";
-            uno::Reference<XDispatch> xD = xDP->queryDispatch(aURL,
+            uno::Reference<XDispatch> xD = m_xFrame->queryDispatch(aURL,
                         "",
                         0x0C);
             if(xD.is())
@@ -300,7 +294,7 @@ SwMailMergeDlg::SwMailMergeDlg(Window* pParent, SwWrtShell& rShell,
                 xD->dispatch(aURL, aProperties);
                 pBeamerWin->Show();
             }
-            uno::Reference<XController> xController = xFrame->getController();
+            uno::Reference<XController> xController = m_xFrame->getController();
             pImpl->xFController = uno::Reference<runtime::XFormController>(xController, UNO_QUERY);
             if(pImpl->xFController.is())
             {
@@ -452,10 +446,10 @@ SwMailMergeDlg::SwMailMergeDlg(Window* pParent, SwWrtShell& rShell,
 
 SwMailMergeDlg::~SwMailMergeDlg()
 {
-    if(xFrame.is())
+    if(m_xFrame.is())
     {
-        xFrame->setComponent(NULL, NULL);
-        xFrame->dispose();
+        m_xFrame->setComponent(NULL, NULL);
+        m_xFrame->dispose();
     }
     else
         delete pBeamerWin;

UnoControls/source/controls/framecontrol.cxx
@@ -19,12 +19,13 @@
 
 #include "framecontrol.hxx"
 
+#include <com/sun/star/beans/PropertyAttribute.hpp>
+#include <com/sun/star/frame/Frame.hpp>
+#include <com/sun/star/frame/FrameSearchFlag.hpp>
 #include <com/sun/star/frame/XDispatchProvider.hpp>
+#include <com/sun/star/frame/XDispatch.hpp>
 #include <com/sun/star/util/URLTransformer.hpp>
 #include <com/sun/star/util/XURLTransformer.hpp>
-#include <com/sun/star/frame/XDispatch.hpp>
-#include <com/sun/star/frame/FrameSearchFlag.hpp>
-#include <com/sun/star/beans/PropertyAttribute.hpp>
 #include <comphelper/processfactory.hxx>
 #include <cppuhelper/typeprovider.hxx>
 #include <osl/diagnose.h>
@@ -459,8 +460,8 @@ void FrameControl::impl_createFrame(    const   Reference< XWindowPeer >&   xPee
                                         const   OUString&                   rURL        ,
                                         const   Sequence< PropertyValue >&  rArguments  )
 {
-    Reference< XFrame >     xOldFrame   ;
-    Reference< XFrame >     xNewFrame   ;
+    Reference< XFrame2 >     xOldFrame   ;
+    Reference< XFrame2 >     xNewFrame   ;
 
     {
         MutexGuard  aGuard ( m_aMutex ) ;
@@ -469,29 +470,24 @@ void FrameControl::impl_createFrame(    const   Reference< XWindowPeer >&   xPee
 
 
 
-    xNewFrame = Reference< XFrame >  ( impl_getComponentContext()->getServiceManager()->createInstanceWithContext("com.sun.star.frame.Frame", impl_getComponentContext()), UNO_QUERY ) ;
-    Reference< XDispatchProvider >  xDSP ( xNewFrame, UNO_QUERY ) ;
+    xNewFrame = Frame::create( impl_getComponentContext() );
 
-    if (xDSP.is())
+    Reference< XWindow >  xWP ( xPeer, UNO_QUERY ) ;
+    xNewFrame->initialize ( xWP ) ;
+
+    //  option
+    //xFrame->setName( "WhatYouWant" );
+
+    Reference< XURLTransformer > xTrans = URLTransformer::create( impl_getComponentContext() );
+    // load file
+    URL aURL ;
+    aURL.Complete = rURL ;
+    xTrans->parseStrict( aURL ) ;
+
+    Reference< XDispatch >  xDisp = xNewFrame->queryDispatch ( aURL, OUString (), FrameSearchFlag::SELF ) ;
+    if (xDisp.is())
     {
-        Reference< XWindow >  xWP ( xPeer, UNO_QUERY ) ;
-        xNewFrame->initialize ( xWP ) ;
-
-        //  option
-        //xFrame->setName( "WhatYouWant" );
-
-        Reference< XURLTransformer > xTrans = URLTransformer::create( impl_getComponentContext() );
-        // load file
-        URL aURL ;
-
-        aURL.Complete = rURL ;
-        xTrans->parseStrict( aURL ) ;
-
-        Reference< XDispatch >  xDisp = xDSP->queryDispatch ( aURL, OUString (), FrameSearchFlag::SELF ) ;
-        if (xDisp.is())
-        {
-            xDisp->dispatch ( aURL, rArguments ) ;
-        }
+        xDisp->dispatch ( aURL, rArguments ) ;
     }
 
     // set the frame
@@ -519,20 +515,20 @@ void FrameControl::impl_createFrame(    const   Reference< XWindowPeer >&   xPee
 
 void FrameControl::impl_deleteFrame()
 {
-    Reference< XFrame >  xOldFrame;
-    Reference< XFrame >  xNullFrame;
+    Reference< XFrame2 >  xOldFrame;
+    Reference< XFrame2 >  xNullFrame;
 
     {
         // do not dispose the frame in this guarded section (deadlock?)
         MutexGuard aGuard( m_aMutex );
         xOldFrame = m_xFrame;
-        m_xFrame = Reference< XFrame > ();
+        m_xFrame = Reference< XFrame2 > ();
     }
 
     // notify the listeners
     sal_Int32 nFrameId = PROPERTYHANDLE_FRAME;
-    Any aNewFrame( &xNullFrame, ::getCppuType((const Reference< XFrame >*)0) );
-    Any aOldFrame( &xOldFrame, ::getCppuType((const Reference< XFrame >*)0) );
+    Any aNewFrame( &xNullFrame, ::getCppuType((const Reference< XFrame2 >*)0) );
+    Any aOldFrame( &xOldFrame, ::getCppuType((const Reference< XFrame2 >*)0) );
     fire( &nFrameId, &aNewFrame, &aOldFrame, 1, sal_False );
 
     // dispose the frame

UnoControls/source/inc/framecontrol.hxx
@@ -22,7 +22,7 @@
 
 #include <com/sun/star/frame/XFrameActionListener.hpp>
 #include <com/sun/star/frame/XComponentLoader.hpp>
-#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/frame/XFrame2.hpp>
 #include <com/sun/star/frame/FrameActionEvent.hpp>
 #include <com/sun/star/frame/FrameAction.hpp>
 #include <com/sun/star/lang/XServiceName.hpp>
@@ -593,7 +593,7 @@ private:
 
 private:
 
-    ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >         m_xFrame                    ;
+    ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame2 >        m_xFrame                    ;
     OUString                                        m_sComponentURL             ;
     ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >   m_seqLoaderArguments        ;
     ::cppu::OMultiTypeInterfaceContainerHelper      m_aInterfaceContainer       ;

basctl/source/dlged/propbrw.cxx
@@ -28,6 +28,7 @@
 #include <svx/svxids.hrc>
 
 #include <com/sun/star/awt/PosSize.hpp>
+#include <com/sun/star/frame/Frame.hpp>
 #include <com/sun/star/inspection/XObjectInspector.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <comphelper/types.hxx>
@@ -85,7 +86,6 @@ DBG_NAME(PropBrw)
 PropBrw::PropBrw (DialogWindowLayout& rLayout_):
     DockingWindow(&rLayout_),
     m_bInitialStateChange(true),
-    m_xORB(comphelper::getProcessServiceFactory()),
     m_xContextDocument(SfxViewShell::Current() ? SfxViewShell::Current()->GetCurrentDocument() : Reference<XModel>()),
     rLayout(rLayout_),
     pView(0)
@@ -99,12 +99,9 @@ PropBrw::PropBrw (DialogWindowLayout& rLayout_):
     try
     {
         // create a frame wrapper for myself
-        m_xMeAsFrame = Reference< XFrame >(m_xORB->createInstance( "com.sun.star.frame.Frame" ), UNO_QUERY);
-        if (m_xMeAsFrame.is())
-        {
-            m_xMeAsFrame->initialize( VCLUnoHelper::GetInterface ( this ) );
-            m_xMeAsFrame->setName( "form property browser" );  // change name!
-        }
+        m_xMeAsFrame = frame::Frame::create( comphelper::getProcessComponentContext() );
+        m_xMeAsFrame->initialize( VCLUnoHelper::GetInterface ( this ) );
+        m_xMeAsFrame->setName( "form property browser" );  // change name!
     }
     catch (const Exception&)
     {
@@ -127,8 +124,7 @@ void PropBrw::ImplReCreateController()
 
     try
     {
-        Reference< XComponentContext > xOwnContext(
-            comphelper::getComponentContext( m_xORB ) );
+        Reference< XComponentContext > xOwnContext = comphelper::getProcessComponentContext();
 
         // a ComponentContext for the
         ::cppu::ContextEntry_Init aHandlerContextInfo[] =
@@ -160,7 +156,7 @@ void PropBrw::ImplReCreateController()
             }
             else
             {
-                xAsXController->attachFrame(m_xMeAsFrame);
+                xAsXController->attachFrame( Reference<XFrame>(m_xMeAsFrame,UNO_QUERY_THROW) );
                 m_xBrowserComponentWindow = m_xMeAsFrame->getComponentWindow();
                 DBG_ASSERT(m_xBrowserComponentWindow.is(), "PropBrw::PropBrw: attached the controller, but have no component window!");
             }

basctl/source/inc/propbrw.hxx
@@ -21,6 +21,7 @@
 #define BASCTL_PROPBRW_HXX
 
 #include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/frame/XFrame2.hpp>
 #include <comphelper/stl_types.hxx>
 #include <svl/lstner.hxx>
 #include <svl/brdcst.hxx>
@@ -30,9 +31,6 @@
 class SfxBindings;
 class SdrView;
 class SfxViewShell;
-namespace com { namespace sun { namespace star { namespace lang {
-    class XMultiServiceFactory;
-}}}}
 
 namespace basctl
 {
@@ -44,9 +42,7 @@ class PropBrw : public DockingWindow, public SfxListener, public SfxBroadcaster
 private:
     bool        m_bInitialStateChange;
 
-    ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >
-                    m_xORB;
-    ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >
+    ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame2 >
                     m_xMeAsFrame;
     ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >
                     m_xBrowserController;

dbaccess/source/ui/app/AppDetailPageHelper.cxx
@@ -25,30 +25,30 @@
 #include "tabletree.hxx"
 #include "UITools.hxx"
 #include "dbtreelistbox.hxx"
-#include <com/sun/star/frame/XLayoutManager.hpp>
-#include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/awt/XTabController.hpp>
-#include <com/sun/star/sdbc/XConnection.hpp>
-#include <com/sun/star/sdbcx/XTablesSupplier.hpp>
-#include <com/sun/star/sdbcx/XViewsSupplier.hpp>
-#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/awt/XWindow.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/container/XChild.hpp>
+#include <com/sun/star/container/XContainer.hpp>
+#include <com/sun/star/form/XLoadable.hpp>
+#include <com/sun/star/frame/XLayoutManager.hpp>
+#include <com/sun/star/frame/Frame.hpp>
 #include <com/sun/star/frame/FrameSearchFlag.hpp>
 #include <com/sun/star/frame/XComponentLoader.hpp>
 #include <com/sun/star/frame/XFrames.hpp>
 #include <com/sun/star/frame/XFramesSupplier.hpp>
-#include <com/sun/star/awt/XWindow.hpp>
-#include <toolkit/helper/vclunohelper.hxx>
-#include <com/sun/star/ucb/XCommandEnvironment.hpp>
-#include <com/sun/star/ucb/XCommandProcessor.hpp>
-#include <com/sun/star/ucb/Command.hpp>
-#include <com/sun/star/form/XLoadable.hpp>
-#include <com/sun/star/container/XChild.hpp>
-#include <com/sun/star/container/XContainer.hpp>
 #include <com/sun/star/sdb/CommandType.hpp>
-#include <com/sun/star/util/XCloseable.hpp>
 #include <com/sun/star/sdb/application/XDatabaseDocumentUI.hpp>
 #include <com/sun/star/sdb/application/DatabaseObject.hpp>
 #include <com/sun/star/sdb/application/DatabaseObjectContainer.hpp>
+#include <com/sun/star/sdbc/XConnection.hpp>
+#include <com/sun/star/sdbcx/XTablesSupplier.hpp>
+#include <com/sun/star/sdbcx/XViewsSupplier.hpp>
+#include <com/sun/star/ucb/XCommandEnvironment.hpp>
+#include <com/sun/star/ucb/XCommandProcessor.hpp>
+#include <com/sun/star/ucb/Command.hpp>
+#include <com/sun/star/util/XCloseable.hpp>
+#include <toolkit/helper/vclunohelper.hxx>
 #include "AppView.hxx"
 #include "dbaccess_helpid.hrc"
 #include "dbu_app.hrc"
@@ -1141,20 +1141,19 @@ void OAppDetailPageHelper::showPreview( const OUString& _sDataSourceName,
         {
             try
             {
-                m_xFrame = Reference < XFrame > ( getBorderWin().getView()->getORB()->getServiceManager()->createInstanceWithContext( OUString("com.sun.star.frame.Frame"), getBorderWin().getView()->getORB() ), UNO_QUERY );
+                m_xFrame = Frame::create( getBorderWin().getView()->getORB() );
                 m_xFrame->initialize( m_xWindow );
 
                 // no layout manager (and thus no toolbars) in the preview
                 // Must be called after initialize ... but before any other call to this frame.
                 // Otherwise frame throws "life time exceptions" as e.g. NON_INITIALIZED
-                Reference< XPropertySet > xFrameProps( m_xFrame, UNO_QUERY_THROW );
-                xFrameProps->setPropertyValue( OUString( "LayoutManager" ), makeAny(Reference< XLayoutManager >()) );
+                m_xFrame->setLayoutManager( Reference< XLayoutManager >() );
 
                 Reference<XFramesSupplier> xSup(getBorderWin().getView()->getAppController().getXController()->getFrame(),UNO_QUERY);
                 if ( xSup.is() )
                 {
                     Reference<XFrames> xFrames = xSup->getFrames();
-                    xFrames->append(m_xFrame);
+                    xFrames->append( Reference<XFrame>(m_xFrame,UNO_QUERY_THROW));
                 }
             }
             catch(const Exception&)
@@ -1169,7 +1168,7 @@ void OAppDetailPageHelper::showPreview( const OUString& _sDataSourceName,
             xApplication, NULL, _bTable
         ) );
         SAL_WNODEPRECATED_DECLARATIONS_POP
-        pDispatcher->setTargetFrame( m_xFrame );
+        pDispatcher->setTargetFrame( Reference<XFrame>(m_xFrame,UNO_QUERY_THROW) );
 
         ::comphelper::NamedValueCollection aArgs;
         aArgs.put( "Preview", sal_True );

dbaccess/source/ui/app/AppDetailPageHelper.hxx
@@ -37,7 +37,7 @@
 #include <memory>
 
 namespace com{ namespace sun { namespace star { namespace awt   { class XWindow; } } } }
-namespace com{ namespace sun { namespace star { namespace frame { class XFrame; } } } }
+namespace com{ namespace sun { namespace star { namespace frame { class XFrame2; } } } }
 namespace com{ namespace sun { namespace star { namespace io    { class XPersist; } } } }
 
 #define ELEMENT_COUNT   size_t(E_ELEMENT_TYPE_COUNT)
@@ -90,7 +90,7 @@ namespace dbaui
         Window*             m_pTablePreview;
         ::std::auto_ptr<PopupMenu> m_aMenu;
         PreviewMode         m_ePreviewMode;
-        ::com::sun::star::uno::Reference < ::com::sun::star::frame::XFrame >
+        ::com::sun::star::uno::Reference < ::com::sun::star::frame::XFrame2 >
                             m_xFrame;
         ::com::sun::star::uno::Reference < ::com::sun::star::io::XPersist >
                             m_xDocInfo;

dbaccess/source/ui/inc/querycontainerwindow.hxx
@@ -23,7 +23,7 @@
 #include <vcl/window.hxx>
 #include <vcl/split.hxx>
 #include "dataview.hxx"
-#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/frame/XFrame2.hpp>
 #include "QueryViewSwitch.hxx"
 #include <vcl/dockwin.hxx>
 
@@ -50,7 +50,7 @@ namespace dbaui
         OQueryViewSwitch*   m_pViewSwitch;
         OBeamer*            m_pBeamer;
         Splitter*           m_pSplitter;
-        ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame > m_xBeamer;
+        ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame2 > m_xBeamer;
 
         DECL_LINK( SplitHdl, void* );
     public:
@@ -66,7 +66,7 @@ namespace dbaui
             // called when the beamer has been disposed
         void    disposingPreview();
 
-        ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >
+        ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame2 >
                 getPreviewFrame() const { return m_xBeamer; }
 
         OQueryDesignView*   getDesignView() { return m_pViewSwitch->getDesignView(); }

commit eb867e9ade46ec8d96fb69f4d64191c7f934c239

fdo#64283: SwMailMergeDlg: add back missing initialize
(regression from 5084224dd475c7d5164a67625b249859dbafc019)

Change-Id: I0ed8c4ec91a095babccd089c7fe4508d123ba86d

sw/source/ui/envelp/mailmrge.cxx
@@ -269,6 +269,7 @@ SwMailMergeDlg::SwMailMergeDlg(Window* pParent, SwWrtShell& rShell,
         {
             // create a frame wrapper for myself
             m_xFrame = frame::Frame::create( comphelper::getProcessComponentContext() );
+            m_xFrame->initialize( VCLUnoHelper::GetInterface ( pBeamerWin ) );
         }
         catch (const Exception&)
         {