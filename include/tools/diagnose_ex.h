LemmingAvalanche/libreoffice-repaired

commit 1ff17c10dc7460be3002cf6f6e560a01c5146447

move DBG_UNHANDLED_EXCEPTION out of line
makes it easier to set a breakpoint on it.
Plus it's getting a little big to be a macro.

Change-Id: I2827aa3618ba966fbc85a4a56e0e794a55630730
Reviewed-on: https://gerrit.libreoffice.org/3988
Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

include/tools/diagnose_ex.h
@@ -30,6 +30,8 @@
 #define OSL_UNUSED( expression ) \
     (void)(expression)
 
+
+
 #if OSL_DEBUG_LEVEL > 0
     #include <com/sun/star/configuration/CorruptedConfigurationException.hpp>
     #include <com/sun/star/task/ErrorCodeIOException.hpp>
@@ -39,51 +41,15 @@
     #include <boost/current_function.hpp>
     #include <typeinfo>
 
+    void DbgUnhandledException(const ::com::sun::star::uno::Any& caughtException, const char* currentFunction);
+
     /** reports a caught UNO exception via OSL diagnostics
 
         Note that whenever you use this, it might be an indicator that your error
         handling is not correct ....
     */
     #define DBG_UNHANDLED_EXCEPTION()   \
-        ::com::sun::star::uno::Any caught( ::cppu::getCaughtException() ); \
-        OString sMessage( "caught an exception!" ); \
-        sMessage += "\nin function:"; \
-        sMessage += BOOST_CURRENT_FUNCTION; \
-        sMessage += "\ntype: "; \
-        sMessage += OUStringToOString( caught.getValueTypeName(), osl_getThreadTextEncoding() ); \
-        ::com::sun::star::uno::Exception exception; \
-        caught >>= exception; \
-        if ( !exception.Message.isEmpty() ) \
-        { \
-            sMessage += "\nmessage: "; \
-            sMessage += OUStringToOString( exception.Message, osl_getThreadTextEncoding() ); \
-        } \
-        if ( exception.Context.is() ) \
-        { \
-            const char* pContext = typeid( *exception.Context.get() ).name(); \
-            sMessage += "\ncontext: "; \
-            sMessage += pContext; \
-        } \
-        { \
-            ::com::sun::star::configuration::CorruptedConfigurationException \
-                specialized; \
-            if ( caught >>= specialized ) \
-            { \
-                sMessage += "\ndetails: "; \
-                sMessage += OUStringToOString( \
-                    specialized.Details, osl_getThreadTextEncoding() ); \
-            } \
-        } \
-        { \
-            ::com::sun::star::task::ErrorCodeIOException specialized; \
-            if ( caught >>= specialized ) \
-            { \
-                sMessage += "\ndetails: "; \
-                sMessage += OString::valueOf( specialized.ErrCode ); \
-            } \
-        } \
-        sMessage += "\n"; \
-        OSL_ENSURE( false, sMessage.getStr() )
+        DbgUnhandledException( ::cppu::getCaughtException(), BOOST_CURRENT_FUNCTION);
 
 #else   // OSL_DEBUG_LEVEL
     #define DBG_UNHANDLED_EXCEPTION()

tools/source/debug/debug.cxx
@@ -42,6 +42,7 @@
 #include <vector>
 
 #include <osl/diagnose.h>
+#include <tools/diagnose_ex.h>
 
 #ifdef DBG_UTIL
 
@@ -1584,5 +1585,53 @@ void DbgOutTypef( sal_uInt16, const sal_Char*, ... ) {}
 void DbgOutf( const sal_Char*, ... ) {}
 
 #endif
+
+
+#if OSL_DEBUG_LEVEL > 0
+
+void DbgUnhandledException(const css::uno::Any & caught, const char* currentFunction)
+{
+        OString sMessage( "caught an exception!" );
+        sMessage += "\nin function:";
+        sMessage += currentFunction;
+        sMessage += "\ntype: ";
+        sMessage += OUStringToOString( caught.getValueTypeName(), osl_getThreadTextEncoding() );
+        ::com::sun::star::uno::Exception exception;
+        caught >>= exception;
+        if ( !exception.Message.isEmpty() )
+        {
+            sMessage += "\nmessage: ";
+            sMessage += OUStringToOString( exception.Message, osl_getThreadTextEncoding() );
+        }
+        if ( exception.Context.is() )
+        {
+            const char* pContext = typeid( *exception.Context.get() ).name();
+            sMessage += "\ncontext: ";
+            sMessage += pContext;
+        }
+        {
+            ::com::sun::star::configuration::CorruptedConfigurationException
+                specialized;
+            if ( caught >>= specialized )
+            {
+                sMessage += "\ndetails: ";
+                sMessage += OUStringToOString(
+                    specialized.Details, osl_getThreadTextEncoding() );
+            }
+        }
+        {
+            ::com::sun::star::task::ErrorCodeIOException specialized;
+            if ( caught >>= specialized )
+            {
+                sMessage += "\ndetails: ";
+                sMessage += OString::valueOf( specialized.ErrCode );
+            }
+        }
+        sMessage += "\n";
+        OSL_ENSURE( false, sMessage.getStr() );
+}
+
+#endif  // OSL_DEBUG_LEVEL
+
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 211dab2f1d6cb1b60e08323bd13e0b009357c835

fix file and lineno info for DBG_UNHANDLED_EXCEPTION
which I broke in commit 1ff17c10dc7460be3002cf6f6e560a01c5146447
"move DBG_UNHANDLED_EXCEPTION out of line"

Change-Id: Ic30e985cc356d81013ced1ce74ec6c78469d9882

include/tools/diagnose_ex.h
@@ -42,7 +42,7 @@
     #include <typeinfo>
     #include <tools/toolsdllapi.h>
 
-    TOOLS_DLLPUBLIC void DbgUnhandledException(const ::com::sun::star::uno::Any& caughtException, const char* currentFunction);
+    TOOLS_DLLPUBLIC void DbgUnhandledException(const ::com::sun::star::uno::Any& caughtException, const char* currentFunction, const char* fileAndLineNo);
 
     /** reports a caught UNO exception via OSL diagnostics
 
@@ -50,7 +50,7 @@
         handling is not correct ....
     */
     #define DBG_UNHANDLED_EXCEPTION()   \
-        DbgUnhandledException( ::cppu::getCaughtException(), BOOST_CURRENT_FUNCTION);
+        DbgUnhandledException( ::cppu::getCaughtException(), BOOST_CURRENT_FUNCTION, SAL_DETAIL_WHERE);
 
 #else   // OSL_DEBUG_LEVEL
     #define DBG_UNHANDLED_EXCEPTION()

tools/source/debug/debug.cxx
@@ -1589,7 +1589,7 @@ void DbgOutf( const sal_Char*, ... ) {}
 
 #if OSL_DEBUG_LEVEL > 0
 
-TOOLS_DLLPUBLIC void DbgUnhandledException(const css::uno::Any & caught, const char* currentFunction)
+TOOLS_DLLPUBLIC void DbgUnhandledException(const css::uno::Any & caught, const char* currentFunction, const char* fileAndLineNo)
 {
         OString sMessage( "caught an exception!" );
         sMessage += "\nin function:";
@@ -1628,7 +1628,10 @@ TOOLS_DLLPUBLIC void DbgUnhandledException(const css::uno::Any & caught, const c
             }
         }
         sMessage += "\n";
-        OSL_ENSURE( false, sMessage.getStr() );
+
+        SAL_DETAIL_LOG_FORMAT(
+            SAL_DETAIL_ENABLE_LOG_WARN, SAL_DETAIL_LOG_LEVEL_WARN,
+            "legacy.osl", fileAndLineNo, "%s", sMessage.getStr());
 }
 
 #endif  // OSL_DEBUG_LEVEL