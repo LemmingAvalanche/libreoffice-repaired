LemmingAvalanche/libreoffice-repaired

commit 5cc2702e9f56d66d0882e9c174ae653581a808fe

Avoid crash on iOS: We need to get a valid context here
Change-Id: Ib3fc01213fc6bc370a6c8b774a063f6f275ba6f2

vcl/coretext/ctlayout.cxx
@@ -417,6 +417,12 @@ void CTLayout::GetCaretPositions( int nMaxIndex, sal_Int32* pCaretXArray ) const
 bool CTLayout::GetBoundRect( SalGraphics& rGraphics, Rectangle& rVCLRect ) const
 {
     AquaSalGraphics& rAquaGraphics = static_cast<AquaSalGraphics&>(rGraphics);
+
+#ifdef IOS
+    if( !rAquaGraphics.CheckContext() )
+        return false;
+#endif
+
     CGRect aMacRect = CTLineGetImageBounds( mpCTLine, rAquaGraphics.mrContext );
     CGPoint aMacPos = CGContextGetTextPosition( rAquaGraphics.mrContext );
     aMacRect.origin.x -= aMacPos.x;

commit 5b05861cf1bed4f491faa97dc8298b084e1447b8

Fix CTLayout::GetBoundRect()
was broken in 5cc2702e9f56d66d0882e9c174ae653581a808fe

Change-Id: I803b510fc875ff9e48ed762114f6d63dad51ad7a

vcl/coretext/ctlayout.cxx
@@ -442,9 +442,9 @@ bool CTLayout::GetBoundRect( SalGraphics& rGraphics, Rectangle& rVCLRect ) const
 
     // CoreText top-bottom are vertically flipped from a VCL aspect
     rVCLRect.Left()   = aPos.X() + aMacRect.origin.x;
-    rVCLRect.Right()  = aPos.X() + aMacRect.origin.x + aMacRect.size.width;
+    rVCLRect.Right()  = aPos.X() + (aMacRect.origin.x + aMacRect.size.width);
     rVCLRect.Bottom() = aPos.Y() - aMacRect.origin.y;
-    rVCLRect.Top()    = aPos.Y() - aMacRect.origin.y + aMacRect.size.height;
+    rVCLRect.Top()    = aPos.Y() - (aMacRect.origin.y + aMacRect.size.height);
     return true;
 }
 