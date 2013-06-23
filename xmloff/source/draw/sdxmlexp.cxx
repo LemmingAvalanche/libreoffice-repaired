LemmingAvalanche/libreoffice-repaired

commit e92dae3d629d3ee45d9ef0a2044b7a26030beadf

use document settings / embed fonts when exporting odp
Change-Id: I9e8db1ae08b570f093b13d50bfde823c85cdba95

xmloff/source/draw/sdxmlexp.cxx
@@ -2901,4 +2901,17 @@ OUString SAL_CALL SdXMLExport::getImplementationName() throw( uno::RuntimeExcept
     }
 }
 
+XMLFontAutoStylePool* SdXMLExport::CreateFontAutoStylePool()
+{
+    bool bEmbedFonts = false;
+    Reference< lang::XMultiServiceFactory > xFac( GetModel(), UNO_QUERY );
+    if( xFac.is() )
+    {
+        Reference< beans::XPropertySet > xProps( xFac->createInstance( OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.document.Settings" ) ) ), UNO_QUERY );
+        if( xProps.is() )
+            xProps->getPropertyValue("EmbedFonts") >>= bEmbedFonts;
+    }
+    return new XMLFontAutoStylePool( *this, bEmbedFonts );
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

xmloff/source/draw/sdxmlexp_impl.hxx
@@ -174,6 +174,7 @@ class SdXMLExport : public SvXMLExport
 protected:
     virtual void GetViewSettings(com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aProps);
     virtual void GetConfigurationSettings(com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aProps);
+    virtual XMLFontAutoStylePool* CreateFontAutoStylePool();
 
 public:
     SdXMLExport(

commit 16821af253a68ed8af8edb9460010b970bbff634

Resolves: rhbz#976304 gallery elements may not support document.Settings
so the createInstance throws, so the element doesn't get inserted into
the documents.

regression since e92dae3d629d3ee45d9ef0a2044b7a26030beadf

Change-Id: Ie6cef7a4f0d5ac8a34d41139c3439fc04e9c7f20

xmloff/source/draw/sdxmlexp.cxx
@@ -2915,15 +2915,16 @@ XMLFontAutoStylePool* SdXMLExport::CreateFontAutoStylePool()
         Reference< lang::XMultiServiceFactory > xFac( GetModel(), UNO_QUERY );
         if( xFac.is() )
         {
-            Reference<beans::XPropertySet> const xProps( xFac->createInstance(
-                         "com.sun.star.document.Settings"), UNO_QUERY );
-            if (xProps.is())
+            try
             {
-                try // clipboard document doesn't have shell so throws here
-                {
-                    xProps->getPropertyValue("EmbedFonts") >>= bEmbedFonts;
-                }
-                catch (uno::Exception const&) { }
+                Reference<beans::XPropertySet> const xProps( xFac->createInstance(
+                             "com.sun.star.document.Settings"), UNO_QUERY_THROW );
+                xProps->getPropertyValue("EmbedFonts") >>= bEmbedFonts;
+            }
+            catch (...)
+            {
+                // clipboard document doesn't have shell so throws from getPropertyValue
+                // gallery elements may not support com.sun.star.document.Settings so throws from createInstance
             }
         }
     }