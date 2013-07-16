LemmingAvalanche/libreoffice-repaired

commit e0eced9fbffae86a866b4d41ae7670043c6530b5

fdo#34772 Add word count to the status bar in writer
This change adds a new status bar control,
"SwWordCountStatusBarControl". The control shows the number of words
selected as well as the number of words in the whole
document. Double-clicking the control launches the modeless word count
dialog.

Change-Id: I756183ab1aaf4d059e782dfb05508370178cfbe3

sw/source/ui/uiview/view2.cxx
@@ -140,6 +140,7 @@
 #include <fmthdft.hxx>
 #include <svx/ofaitem.hxx>
 #include <unomid.h>
+#include <docstat.hxx>
 
 const char sStatusDelim[] = " : ";
 const char sStatusComma[] = " , ";//#outlinelevel, define a Variable for "," add by zhaojianwei
@@ -1197,6 +1198,25 @@ void SwView::StateStatusLine(SfxItemSet &rSet)
                 }
             }
             break;
+
+            case FN_STAT_WORDCOUNT:
+            {
+                SwDocStat selectionStats;
+                SwDocStat documentStats;
+                {
+                    SwWait aWait( *GetDocShell(), sal_True );
+                    rShell.StartAction();
+                    rShell.CountWords(selectionStats);
+                    documentStats = rShell.GetUpdatedDocStat();
+                    rShell.EndAction();
+                }
+                rSet.Put(SfxStringItem(FN_STAT_WORDCOUNT, rtl::OUStringBuffer("Words: ")
+                                                            .append(rtl::OUString::valueOf(static_cast<sal_Int64>(selectionStats.nWord)))
+                                                            .append('/')
+                                                            .append(rtl::OUString::valueOf(static_cast<sal_Int64>(documentStats.nWord))).makeStringAndClear()));
+            }
+            break;
+
             case FN_STAT_TEMPLATE:
             {
                 rSet.Put(SfxStringItem( FN_STAT_TEMPLATE,
@@ -1467,6 +1487,13 @@ void SwView::ExecuteStatusLine(SfxRequest &rReq)
         case FN_STAT_PAGE:
         {
             GetViewFrame()->GetDispatcher()->Execute( SID_NAVIGATOR,
+                                      SFX_CALLMODE_SYNCHRON|SFX_CALLMODE_RECORD );
+        }
+        break;
+
+        case FN_STAT_WORDCOUNT:
+        {
+            GetViewFrame()->GetDispatcher()->Execute(FN_WORDCOUNT_DIALOG,
                                       SFX_CALLMODE_SYNCHRON|SFX_CALLMODE_RECORD );
         }
         break;

sw/Library_sw.mk
@@ -708,6 +708,7 @@ $(eval $(call gb_Library_add_exception_objects,sw,\
     sw/source/ui/utlui/uitool \
     sw/source/ui/utlui/unotools \
     sw/source/ui/utlui/viewlayoutctrl \
+    sw/source/ui/utlui/wordcountctrl \
     sw/source/ui/utlui/zoomctrl \
     sw/source/ui/web/wdocsh \
     sw/source/ui/web/wformsh \

sw/inc/cmdid.h
@@ -1165,6 +1165,7 @@ included in c-context files, so c++ style stuff will cause problems.
 #define FN_STAT_SELMODE             (FN_STAT + 5)
 #define FN_STAT_CONTEXT             (FN_STAT + 7)
 #define FN_STAT_BOOKMARK            (FN_STAT + 8)  /* For Popup Bookmarks*/
+#define FN_STAT_WORDCOUNT           (FN_STAT + 9)
 
 /*--------------------------------------------------------------------
     Region: Page preview

sw/sdi/_viewsh.sdi
@@ -317,6 +317,11 @@ interface BaseTextEditView
         ExecMethod = ExecuteStatusLine ;
         StateMethod = StateStatusLine ;
     ]
+    FN_STAT_WORDCOUNT // status()
+    [
+        ExecMethod = ExecuteStatusLine ;
+        StateMethod = StateStatusLine ;
+    ]
     FN_STAT_BOOKMARK // status()
     [
         ExecMethod = ExecuteStatusLine ;

sw/sdi/swriter.sdi
@@ -8027,6 +8027,31 @@ SfxStringItem StateZoom FN_STAT_ZOOM
 ]
 
 //--------------------------------------------------------------------------
+SfxStringItem StateWordCount FN_STAT_WORDCOUNT
+()
+[
+    /* flags: */
+    AutoUpdate = FALSE,
+    Cachable = Cachable,
+    FastCall = FALSE,
+    HasCoreId = FALSE,
+    HasDialog = FALSE,
+    ReadOnlyDoc = TRUE,
+    Toggle = FALSE,
+    Container = FALSE,
+    RecordAbsolute = FALSE,
+    RecordPerSet;
+    Synchron;
+
+    /* config: */
+    AccelConfig = FALSE,
+    MenuConfig = FALSE,
+    StatusBarConfig = TRUE,
+    ToolBoxConfig = FALSE,
+    GroupId = GID_VIEW;
+]
+
+//--------------------------------------------------------------------------
 SfxBoolItem SubScript FN_SET_SUB_SCRIPT
 
 [

sw/sdi/viewsh.sdi
@@ -275,8 +275,8 @@ interface TextPrintPreview
     [
         StateMethod = StateUndo ;
     ]
-     //---------------------------------------------------------------------
-     //Ab hier Statusleiste
+    //---------------------------------------------------------------------
+    // Begin StatusBar
     FN_STAT_ZOOM // status()
     [
         ExecMethod = Execute ;
@@ -307,13 +307,18 @@ interface TextPrintPreview
         ExecMethod = Execute ;
         StateMethod = GetState ;
     ]
-        SID_ATTR_ZOOMSLIDER // status()
-        [
-                ExecMethod = Execute ;
-                StateMethod = GetState ;
-        ]
-     //----------------------------------------------
-     //End mit Statusleiste
+    SID_ATTR_ZOOMSLIDER // status()
+    [
+            ExecMethod = Execute ;
+            StateMethod = GetState ;
+    ]
+    FN_STAT_WORDCOUNT // status()
+    [
+        ExecMethod = Execute ;
+        StateMethod = GetState ;
+    ]
+    //----------------------------------------------
+    // End StatusBar
 }
 
  //=========================================================================

sw/source/ui/app/swmodule.cxx
@@ -107,6 +107,7 @@
 #include <svx/zoomsliderctrl.hxx>
 #include <tblctrl.hxx>
 #include <zoomctrl.hxx>
+#include <wordcountctrl.hxx>
 #include <workctrl.hxx>
 #include <tbxanchr.hxx>
 #include <fldwrap.hxx>
@@ -372,6 +373,7 @@ void SwDLL::RegisterControls()
     SvxInsertStatusBarControl::RegisterControl(SID_ATTR_INSERT, pMod );
     SvxSelectionModeControl::RegisterControl(FN_STAT_SELMODE, pMod );
     XmlSecStatusBarControl::RegisterControl( SID_SIGNATURE, pMod );
+    SwWordCountStatusBarControl::RegisterControl(FN_STAT_WORDCOUNT, pMod);
 
     SwBookmarkControl::RegisterControl(FN_STAT_PAGE, pMod );
     SwTemplateControl::RegisterControl(FN_STAT_TEMPLATE, pMod );

sw/source/ui/inc/wordcountctrl.hxx
@@ -1,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * Copyright 2012 LibreOffice contributors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef _WORDCOUNTCTRL_HXX
+#define _WORDCOUNTCTRL_HXX
+
+#include <sfx2/stbitem.hxx>
+
+/**
+Word count status bar control for Writer.
+
+@remarks This is a simple status bar control of type SfxStringItem, and it has no custom
+logic whatsoever. The actual updating of the word count string happens in
+SwView::StateStatusLine (see sw/source/ui/uiview/view2.cxx).
+*/
+class SwWordCountStatusBarControl : public SfxStatusBarControl
+{
+public:
+    SFX_DECL_STATUSBAR_CONTROL();
+
+    SwWordCountStatusBarControl(sal_uInt16 nSlotId, sal_uInt16 nId, StatusBar& rStb);
+    ~SwWordCountStatusBarControl();
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 048297762e9c91de6252d3a9e6878d6275cbbac0

fdo#63273 sw: let word count not update stat. fields
Updating fields would generate loads of selection change events and it's
not what we asked for.

Regression from e0eced9fbffae86a866b4d41ae7670043c6530b5.

Change-Id: If237df1f31436357022ca8d77b924681e403abd8

sw/inc/IDocumentStatistics.hxx
@@ -43,8 +43,9 @@
       * modified and returns a reference to the result.
       * \param bCompleteAsync if true will return a partial result,
       * and potentially trigger a timeout to complete the work.
+      * \param bFields if stat. fields should be updated
       */
-    virtual const SwDocStat &GetUpdatedDocStat(bool bCompleteAsync) = 0;
+    virtual const SwDocStat &GetUpdatedDocStat(bool bCompleteAsync, bool bFields) = 0;
 
     /// Set the document statistics
     virtual void SetDocStat(const SwDocStat& rStat) = 0;
@@ -53,8 +54,9 @@
       * Updates the internal document's statistics
       * \param bCompleteAsync if true it may do part of the
       * work and trigger a timeout to complete it.
+      * \param bFields if stat. fields should be updated
       */
-    virtual void UpdateDocStat(bool bCompleteAsync) = 0;
+    virtual void UpdateDocStat(bool bCompleteAsync, bool bFields) = 0;
 
 protected:
     virtual ~IDocumentStatistics() {};

sw/inc/doc.hxx
@@ -910,9 +910,9 @@ public:
     */
     virtual void DocInfoChgd();
     virtual const SwDocStat &GetDocStat() const;
-    virtual const SwDocStat &GetUpdatedDocStat(bool bCompleteAsync = false);
+    virtual const SwDocStat &GetUpdatedDocStat(bool bCompleteAsync = false, bool bFields = true);
     virtual void SetDocStat(const SwDocStat& rStat);
-    virtual void UpdateDocStat(bool bCompleteAsync = false);
+    virtual void UpdateDocStat(bool bCompleteAsync = false, bool bFields = true);
 
     /** IDocumentState
     */
@@ -2082,10 +2082,11 @@ private:
     /** continue computing a chunk of document statistics
       * \param nTextNodes number of paragraphs to calculate before
       * exiting
+      * \param bFields if stat. fields should be updated
       *
       * returns false when there is no more to calculate
       */
-    bool IncrementalDocStatCalculate(long nTextNodes = 250);
+    bool IncrementalDocStatCalculate(long nTextNodes = 250, bool bFields = true);
 
     /// Our own 'StatsUpdateTimer' calls the following method
     DECL_LINK( DoIdleStatsUpdate, Timer * );

sw/source/core/doc/doc.cxx
@@ -1120,11 +1120,11 @@ const SwDocStat& SwDoc::GetDocStat() const
     return *mpDocStat;
 }
 
-const SwDocStat& SwDoc::GetUpdatedDocStat( bool bCompleteAsync )
+const SwDocStat& SwDoc::GetUpdatedDocStat( bool bCompleteAsync, bool bFields )
 {
     if( mpDocStat->bModified )
     {
-        UpdateDocStat( bCompleteAsync );
+        UpdateDocStat( bCompleteAsync, bFields );
     }
     return *mpDocStat;
 }
@@ -1644,7 +1644,7 @@ void SwDoc::CalculatePagePairsForProspectPrinting(
 }
 
 // returns true while there is more to do
-bool SwDoc::IncrementalDocStatCalculate( long nTextNodes )
+bool SwDoc::IncrementalDocStatCalculate( long nTextNodes, bool bFields )
 {
     mpDocStat->Reset();
     mpDocStat->nPara = 0; // default is 1!
@@ -1728,8 +1728,11 @@ bool SwDoc::IncrementalDocStatCalculate( long nTextNodes )
     }
 
     // optionally update stat. fields
-    SwFieldType *pType = GetSysFldType(RES_DOCSTATFLD);
-    pType->UpdateFlds();
+    if (bFields)
+    {
+        SwFieldType *pType = GetSysFldType(RES_DOCSTATFLD);
+        pType->UpdateFlds();
+    }
 
     return nTextNodes <= 0;
 }
@@ -1746,16 +1749,16 @@ IMPL_LINK( SwDoc, DoIdleStatsUpdate, Timer *, pTimer )
     return 0;
 }
 
-void SwDoc::UpdateDocStat( bool bCompleteAsync )
+void SwDoc::UpdateDocStat( bool bCompleteAsync, bool bFields )
 {
     if( mpDocStat->bModified )
     {
         if (!bCompleteAsync)
         {
-            while (IncrementalDocStatCalculate()) {}
+            while (IncrementalDocStatCalculate(250, bFields)) {}
             maStatsUpdateTimer.Stop();
         }
-        else if (IncrementalDocStatCalculate())
+        else if (IncrementalDocStatCalculate(250, bFields))
             maStatsUpdateTimer.Start();
     }
 }

sw/source/ui/uiview/view2.cxx
@@ -1268,7 +1268,7 @@ void SwView::StateStatusLine(SfxItemSet &rSet)
                 SwDocStat documentStats;
                 {
                     rShell.CountWords(selectionStats);
-                    documentStats = rShell.GetDoc()->GetUpdatedDocStat( true /* complete-async */ );
+                    documentStats = rShell.GetDoc()->GetUpdatedDocStat( true /* complete-async */, false /* don't update fields */ );
                 }
 
                 const sal_uInt32 stringId = selectionStats.nWord? STR_STATUSBAR_WORDCOUNT : STR_STATUSBAR_WORDCOUNT_NO_SELECTION;