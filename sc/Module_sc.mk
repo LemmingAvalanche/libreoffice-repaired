LemmingAvalanche/libreoffice-repaired

commit a067f36f46f1cc7e68c201a93c43437e7f19ff9d

Create a new library to store shared qa code for sc.
Change-Id: I38aa1461d05e3dee096e69b7cd95e2ddfccc9594

sc/Module_sc.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_Module_add_targets,sc,\
 	Library_scd \
 	Library_scfilt \
 	Library_scui \
+	Library_scqahelper \
 	UIConfig_scalc \
 ))
 

Repository.mk
@@ -432,6 +432,7 @@ $(eval $(call gb_Helper_register_libraries,PLAINLIBS_NONE, \
 	unobootstrapprotector \
 	unoexceptionprotector \
 	unotest \
+	scqahelper \
 ))
 
 $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_URE,ure, \

sc/CppunitTest_sc_filters_test.mk
@@ -44,6 +44,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sc_filters_test, \
     salhelper \
     sax \
     sc \
+    scqahelper \
     sfx \
     sot \
     svl \

sc/CppunitTest_sc_subsequent_export_test.mk
@@ -38,6 +38,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sc_subsequent_export_test, \
     salhelper \
     sax \
     sc \
+    scqahelper \
     sfx \
     sot \
     svl \

sc/CppunitTest_sc_subsequent_filters_test.mk
@@ -38,6 +38,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sc_subsequent_filters_test, \
     salhelper \
     sax \
     sc \
+    scqahelper \
     sfx \
     sot \
     svl \

sc/CppunitTest_sc_ucalc.mk
@@ -51,6 +51,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sc_ucalc, \
     salhelper \
     sax \
     sb \
+    scqahelper \
     sfx \
     sot \
     svl \

sc/Library_scqahelper.mk
@@ -1,0 +1,48 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Library_Library,scqahelper))
+
+$(eval $(call gb_Library_set_include,scqahelper,\
+	-I$(SRCDIR)/sc/inc \
+	-I$(SRCDIR)/sc/source/ui/inc \
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_Library_use_externals,scqahelper, \
+	boost_headers \
+	mdds_headers \
+	orcus \
+	orcus-parser \
+	cppunit \
+))
+
+$(eval $(call gb_Library_use_sdk_api,scqahelper))
+
+$(eval $(call gb_Library_use_libraries,scqahelper,\
+	comphelper \
+	cppu \
+	cppuhelper \
+	sal \
+	sfx \
+	sot \
+	svl \
+	svt \
+	tl \
+	utl \
+	sc \
+	ucbhelper \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_Library_add_exception_objects,scqahelper,\
+	sc/qa/unit/helper/qahelper \
+))
+
+# vim: set noet sw=4 ts=4:

sc/qa/unit/filters-test.cxx
@@ -20,7 +20,6 @@
 #include <sfx2/sfxmodelfactory.hxx>
 #include <svl/stritem.hxx>
 
-#define CALC_DEBUG_OUTPUT 0
 #define TEST_BUG_FILES 0
 
 #include "helper/qahelper.hxx"

commit 89febb61ea61a9df13327128cee5a789da0795e3

try to avoid build breakage with cppunit on android
Broken by a067f36f46f1cc7e68c201a93c43437e7f19ff9d , and I've had enough
of tinderbox mails during the last week.

Change-Id: Iab0fe0363e8ca3212eb673ae1da9492140140bc3

sc/Module_sc.mk
@@ -16,9 +16,14 @@ $(eval $(call gb_Module_add_targets,sc,\
 	Library_scd \
 	Library_scfilt \
 	Library_scui \
-	Library_scqahelper \
 	UIConfig_scalc \
 ))
+
+ifneq (,$(filter DESKTOP,$(BUILD_TYPE)))
+$(eval $(call gb_Module_add_targets,sc,\
+	Library_scqahelper \
+))
+endif
 
 ifeq ($(ENABLE_TELEPATHY),TRUE)
 