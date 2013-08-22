LemmingAvalanche/libreoffice-repaired

commit 106eadbb7aa09116fdb89d98e6c17fe1a48b3772

String to OUString, some formatting
Conflicts:
	sw/source/filter/basflt/fltini.cxx
	sw/source/ui/dialog/ascfldlg.cxx

Change-Id: Ifa12105aa6f747e2797891b6c71128bd96f78bf3

sw/source/filter/ascii/wrtasc.cxx
@@ -33,17 +33,17 @@
 
 //-----------------------------------------------------------------
 
-SwASCWriter::SwASCWriter( const String& rFltNm )
+SwASCWriter::SwASCWriter( const OUString& rFltNm )
 {
     SwAsciiOptions aNewOpts;
 
-    switch( 5 <= rFltNm.Len() ? rFltNm.GetChar( 4 ) : 0 )
+    switch( 5 <= rFltNm.getLength() ? rFltNm[4] : 0 )
     {
     case 'D':
                 aNewOpts.SetCharSet( RTL_TEXTENCODING_IBM_850 );
                 aNewOpts.SetParaFlags( LINEEND_CRLF );
-                if( 5 < rFltNm.Len() )
-                    switch( rFltNm.Copy( 5 ).ToInt32() )
+                if( 5 < rFltNm.getLength() )
+                    switch( rFltNm.copy( 5 ).toInt32() )
                     {
                     case 437: aNewOpts.SetCharSet( RTL_TEXTENCODING_IBM_437 );  break;
                     case 850: aNewOpts.SetCharSet( RTL_TEXTENCODING_IBM_850 );  break;
@@ -74,7 +74,7 @@ SwASCWriter::SwASCWriter( const String& rFltNm )
                 break;
 
     default:
-        if( rFltNm.Copy( 4 ).EqualsAscii( "_DLG" ))
+        if( rFltNm.copy( 4 )=="_DLG" )
         {
             // use the options
             aNewOpts = GetAsciiOptions();
@@ -87,22 +87,17 @@ SwASCWriter::~SwASCWriter() {}
 
 sal_uLong SwASCWriter::WriteStream()
 {
-    sal_Char cLineEnd[ 3 ];
-    sal_Char* pCEnd = cLineEnd;
     if( bASCII_ParaAsCR )           // If predefined
-        *pCEnd++ = '\015';
+        m_sLineEnd = "\015";
     else if( bASCII_ParaAsBlanc )
-        *pCEnd++ = ' ';
+        m_sLineEnd = " ";
     else
         switch( GetAsciiOptions().GetParaFlags() )
         {
-        case LINEEND_CR:    *pCEnd++ = '\015'; break;
-        case LINEEND_LF:    *pCEnd++ = '\012'; break;
-        case LINEEND_CRLF:  *pCEnd++ = '\015', *pCEnd++ = '\012'; break;
+        case LINEEND_CR:    m_sLineEnd = "\015"; break;
+        case LINEEND_LF:    m_sLineEnd = "\012"; break;
+        case LINEEND_CRLF:  m_sLineEnd = "\015\012"; break;
         }
-    *pCEnd = 0;
-
-    sLineEnd.AssignAscii( cLineEnd );
 
     long nMaxNode = pDoc->GetNodes().Count();
 
@@ -200,7 +195,7 @@ sal_uLong SwASCWriter::WriteStream()
 }
 
 
-void GetASCWriter( const String& rFltNm, const String& /*rBaseURL*/, WriterRef& xRet )
+void GetASCWriter( const OUString& rFltNm, const OUString& /*rBaseURL*/, WriterRef& xRet )
 {
   xRet = new SwASCWriter( rFltNm );
 }

sw/inc/SwXMLSectionList.hxx
@@ -32,11 +32,11 @@ protected:
                   const ::com::sun::star::uno::Reference<
                     ::com::sun::star::xml::sax::XAttributeList > & xAttrList );
 public:
-    std::vector<String*> &rSectionList;
+    std::vector<OUString*> &rSectionList;
 
     SwXMLSectionList(
         const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > xContext,
-        std::vector<String*> &rNewSectionList );
+        std::vector<OUString*> &rNewSectionList );
 
     virtual ~SwXMLSectionList ( )
         throw();

sw/inc/shellio.hxx
@@ -29,7 +29,6 @@
 #include <sot/formats.hxx>
 #include <sot/storage.hxx>
 #include <svtools/parhtml.hxx>
-#include <tools/string.hxx>
 #include <tools/date.hxx>
 #include <tools/time.hxx>
 #include <tools/datetime.hxx>
@@ -63,15 +62,15 @@ struct Writer_Impl;
 
 class SW_DLLPUBLIC SwAsciiOptions
 {
-    String sFont;
+    OUString sFont;
     rtl_TextEncoding eCharSet;
     sal_uInt16 nLanguage;
     LineEnd eCRLF_Flag;
 
 public:
 
-    const String& GetFontName() const { return sFont; }
-    void SetFontName( const String& rFont ) { sFont = rFont; }
+    OUString GetFontName() const { return sFont; }
+    void SetFontName( const OUString& rFont ) { sFont = rFont; }
 
     rtl_TextEncoding GetCharSet() const { return eCharSet; }
     void SetCharSet( rtl_TextEncoding nVal ) { eCharSet = nVal; }
@@ -84,7 +83,7 @@ public:
 
     void Reset()
     {
-        sFont.Erase();
+        sFont = OUString();
         eCRLF_Flag = GetSystemLineEnd();
         eCharSet = ::osl_getThreadTextEncoding();
         nLanguage = 0;
@@ -155,35 +154,35 @@ class SwReader: public SwDocFac
     SfxMedium* pMedium;     // Who wants to obtain a Medium (W4W).
 
     SwPaM* pCrsr;
-    String aFileName;
-    String sBaseURL;
+    OUString aFileName;
+    OUString sBaseURL;
 
 public:
 
     // Initial reading. Document is created only at Read(...)
     // or in case it is given, into that.
     // Special case for Load with Sw3Reader.
-    SwReader( SfxMedium&, const String& rFilename, SwDoc *pDoc = 0 );
+    SwReader( SfxMedium&, const OUString& rFilename, SwDoc *pDoc = 0 );
 
     // Read into existing document.
     // Document and position in document are taken from SwPaM.
-    SwReader( SvStream&, const String& rFilename, const String& rBaseURL, SwPaM& );
-    SwReader( SfxMedium&, const String& rFilename, SwPaM& );
-    SwReader( const com::sun::star::uno::Reference < com::sun::star::embed::XStorage >&, const String& rFilename, SwPaM& );
+    SwReader( SvStream&, const OUString& rFilename, const OUString& rBaseURL, SwPaM& );
+    SwReader( SfxMedium&, const OUString& rFilename, SwPaM& );
+    SwReader( const com::sun::star::uno::Reference < com::sun::star::embed::XStorage >&, const OUString& rFilename, SwPaM& );
 
     // The only export interface is SwReader::Read(...)!!!
     sal_Bool NeedsPasswd( const Reader& );
-    sal_Bool CheckPasswd( const String&, const Reader& );
+    sal_Bool CheckPasswd( const OUString&, const Reader& );
     sal_uLong Read( const Reader& );
 
     // Ask for glossaries.
     sal_Bool HasGlossaries( const Reader& );
     sal_Bool ReadGlossaries( const Reader&, SwTextBlocks&, sal_Bool bSaveRelFiles );
 
-    const String&       GetBaseURL() const { return sBaseURL;}
+    OUString            GetBaseURL() const { return sBaseURL;}
 
 protected:
-    void                SetBaseURL( const String& rURL ) { sBaseURL = rURL; }
+    void                SetBaseURL( const OUString& rURL ) { sBaseURL = rURL; }
 };
 
 
@@ -198,7 +197,7 @@ class SW_DLLPUBLIC Reader
 {
     friend class SwReader;
     SwDoc* pTemplate;
-    String aTemplateNm;
+    OUString aTemplateNm;
 
     Date aDStamp;
     Time aTStamp;
@@ -219,7 +218,7 @@ protected:
     sal_Bool bHasAskTemplateName : 1;
     sal_Bool bIgnoreHTMLComments : 1;
 
-    virtual String GetTemplateName() const;
+    virtual OUString GetTemplateName() const;
 
 public:
     Reader();
@@ -228,7 +227,7 @@ public:
     virtual int GetReaderType();
     SwgReaderOption& GetReaderOpt() { return aOpt; }
 
-    virtual void SetFltName( const String& rFltNm );
+    virtual void SetFltName( const OUString& rFltNm );
 
     // Adapt item-set of a Frm-Format to the old format.
     static void ResetFrmFmtAttrs( SfxItemSet &rFrmSet );
@@ -241,7 +240,7 @@ public:
     SwDoc* GetTemplateDoc();
     sal_Bool SetTemplate( SwDoc& rDoc );
     void ClearTemplate();
-    void SetTemplateName( const String& rDir );
+    void SetTemplateName( const OUString& rDir );
     void MakeHTMLDummyTemplateDoc();
 
     sal_Bool IsReadUTF8() const { return bReadUTF8; }
@@ -261,13 +260,13 @@ public:
     // Read the sections of the document, which is equal to the medium.
     // Returns the count of it
     virtual size_t GetSectionList( SfxMedium& rMedium,
-                                   std::vector<String*>& rStrings ) const;
+                                   std::vector<OUString*>& rStrings ) const;
 
     SotStorageRef getSotStorageRef() { return pStg; };
     void setSotStorageRef(SotStorageRef pStgRef) { pStg = pStgRef; };
 
 private:
-    virtual sal_uLong Read(SwDoc &, const String& rBaseURL, SwPaM &,const String &)=0;
+    virtual sal_uLong Read(SwDoc &, const OUString& rBaseURL, SwPaM &, const OUString &)=0;
 
     // Everyone who does not need the streams / storages open
     // has to overload the method (W4W!!).
@@ -277,21 +276,21 @@ private:
 class AsciiReader: public Reader
 {
     friend class SwReader;
-    virtual sal_uLong Read( SwDoc &, const String& rBaseURL, SwPaM &,const String &);
+    virtual sal_uLong Read( SwDoc &, const OUString& rBaseURL, SwPaM &, const OUString &);
 public:
     AsciiReader(): Reader() {}
 };
 
 class SW_DLLPUBLIC StgReader : public Reader
 {
-    String aFltName;
+    OUString aFltName;
 
 protected:
     sal_uLong OpenMainStream( SotStorageStreamRef& rRef, sal_uInt16& rBuffSize );
 public:
     virtual int GetReaderType();
-    const String& GetFltName() { return aFltName; }
-    virtual void SetFltName( const String& r );
+    OUString GetFltName() { return aFltName; }
+    virtual void SetFltName( const OUString& r );
 };
 
 
@@ -380,7 +379,7 @@ class SW_DLLPUBLIC Writer
     , private ::boost::noncopyable
 {
     SwAsciiOptions aAscOpts;
-    String          sBaseURL;
+    OUString       sBaseURL;
 
     void _AddFontItem( SfxItemPool& rPool, const SvxFontItem& rFont );
     void _AddFontItems( SfxItemPool& rPool, sal_uInt16 nWhichId );
@@ -390,7 +389,7 @@ class SW_DLLPUBLIC Writer
 protected:
 
     SwPaM* pOrigPam;            // Last Pam that has to be processed.
-    const String* pOrigFileName;
+    const OUString* pOrigFileName;
 
     void ResetWriter();
     sal_Bool CopyNextPam( SwPaM ** );
@@ -399,7 +398,7 @@ protected:
     void PutEditEngFontsInAttrPool( bool bIncl_CJK_CTL = true );
 
     virtual sal_uLong WriteStream() = 0;
-    void                SetBaseURL( const String& rURL ) { sBaseURL = rURL; }
+    void                SetBaseURL( const OUString& rURL ) { sBaseURL = rURL; }
 
     IDocumentSettingAccess* getIDocumentSettingAccess();
     const IDocumentSettingAccess* getIDocumentSettingAccess() const;
@@ -426,22 +425,22 @@ public:
     Writer();
     virtual ~Writer();
 
-    virtual sal_uLong Write( SwPaM&, SfxMedium&, const String* = 0 );
-            sal_uLong Write( SwPaM&, SvStream&,  const String* = 0 );
-    virtual sal_uLong Write( SwPaM&, const com::sun::star::uno::Reference < com::sun::star::embed::XStorage >&, const String* = 0, SfxMedium* = 0 );
-    virtual sal_uLong Write( SwPaM&, SotStorage&, const String* = 0 );
+    virtual sal_uLong Write( SwPaM&, SfxMedium&, const OUString* = 0 );
+            sal_uLong Write( SwPaM&, SvStream&,  const OUString* = 0 );
+    virtual sal_uLong Write( SwPaM&, const com::sun::star::uno::Reference < com::sun::star::embed::XStorage >&, const OUString* = 0, SfxMedium* = 0 );
+    virtual sal_uLong Write( SwPaM&, SotStorage&, const OUString* = 0 );
 
-    virtual void SetVersion( const String&, long );
+    virtual void SetVersion( const OUString&, long );
     virtual sal_Bool IsStgWriter() const;
 
     void SetShowProgress( sal_Bool bFlag = sal_False )  { bShowProgress = bFlag; }
 
-    const String* GetOrigFileName() const       { return pOrigFileName; }
+    const OUString* GetOrigFileName() const       { return pOrigFileName; }
 
     const SwAsciiOptions& GetAsciiOptions() const { return aAscOpts; }
     void SetAsciiOptions( const SwAsciiOptions& rOpt ) { aAscOpts = rOpt; }
 
-    const String&       GetBaseURL() const { return sBaseURL;}
+    OUString GetBaseURL() const { return sBaseURL;}
 
     // Look up next bookmark position from bookmark-table.
     sal_Int32 FindPos_Bkmk( const SwPosition& rPos ) const;
@@ -482,7 +481,7 @@ SV_IMPL_REF(Writer)
 class SW_DLLPUBLIC StgWriter : public Writer
 {
 protected:
-    String aFltName;
+    OUString aFltName;
     SotStorageRef pStg;
     com::sun::star::uno::Reference < com::sun::star::embed::XStorage > xStg;
 
@@ -498,8 +497,8 @@ public:
 
     virtual sal_Bool IsStgWriter() const;
 
-    virtual sal_uLong Write( SwPaM&, const com::sun::star::uno::Reference < com::sun::star::embed::XStorage >&, const String* = 0, SfxMedium* = 0 );
-    virtual sal_uLong Write( SwPaM&, SotStorage&, const String* = 0 );
+    virtual sal_uLong Write( SwPaM&, const com::sun::star::uno::Reference < com::sun::star::embed::XStorage >&, const OUString* = 0, SfxMedium* = 0 );
+    virtual sal_uLong Write( SwPaM&, SotStorage&, const OUString* = 0 );
 
     SotStorage& GetStorage() const       { return *pStg; }
 };
@@ -521,7 +520,7 @@ class SwWriter
     sal_Bool bWriteAll;
 
 public:
-    sal_uLong Write( WriterRef& rxWriter, const String* = 0);
+    sal_uLong Write( WriterRef& rxWriter, const OUString* = 0);
 
     SwWriter( SvStream&, SwCrsrShell &,sal_Bool bWriteAll = sal_False );
     SwWriter( SvStream&, SwDoc & );
@@ -536,8 +535,8 @@ public:
 
 
 typedef Reader* (*FnGetReader)();
-typedef void (*FnGetWriter)(const String&, const String& rBaseURL, WriterRef&);
-sal_uLong SaveOrDelMSVBAStorage( SfxObjectShell&, SotStorage&, sal_Bool, const String& );
+typedef void (*FnGetWriter)(const OUString&, const OUString& rBaseURL, WriterRef&);
+sal_uLong SaveOrDelMSVBAStorage( SfxObjectShell&, SotStorage&, sal_Bool, const OUString& );
 sal_uLong GetSaveWarningOfMSVBAStorage( SfxObjectShell &rDocS );
 
 struct SwReaderWriterEntry
@@ -555,7 +554,7 @@ struct SwReaderWriterEntry
     Reader* GetReader();
 
     /// Get access to the writer.
-    void GetWriter( const String& rNm, const String& rBaseURL, WriterRef& xWrt ) const;
+    void GetWriter( const OUString& rNm, const OUString& rBaseURL, WriterRef& xWrt ) const;
 };
 
 namespace SwReaderWriter
@@ -564,17 +563,17 @@ namespace SwReaderWriter
     Reader* GetReader( ReaderWriterEnum eReader );
 
     /// Return reader based on the name.
-    Reader* GetReader( const String& rFltName );
+    Reader* GetReader( const OUString& rFltName );
 
     /// Return writer based on the name.
-    void GetWriter( const String& rFltName, const String& rBaseURL, WriterRef& xWrt );
+    void GetWriter( const OUString& rFltName, const OUString& rBaseURL, WriterRef& xWrt );
 }
 
-void GetRTFWriter( const String&, const String&, WriterRef& );
-void GetASCWriter( const String&, const String&, WriterRef& );
-void GetHTMLWriter( const String&, const String&, WriterRef& );
-void GetXMLWriter( const String&, const String&, WriterRef& );
-void GetWW8Writer( const String&, const String&, WriterRef& );
+void GetRTFWriter( const OUString&, const OUString&, WriterRef& );
+void GetASCWriter( const OUString&, const OUString&, WriterRef& );
+void GetHTMLWriter( const OUString&, const OUString&, WriterRef& );
+void GetXMLWriter( const OUString&, const OUString&, WriterRef& );
+void GetWW8Writer( const OUString&, const OUString&, WriterRef& );
 
 #endif
 

sw/source/core/swg/SwXMLSectionList.cxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <tools/string.hxx>
 #include <SwXMLSectionList.hxx>
 #include <xmloff/nmspmap.hxx>
 #include <xmloff/xmlnmspe.hxx>
@@ -26,13 +25,15 @@
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
+// TODO: verify if these should match the same-name constants
+//       in xmloff/source/core/xmlimp.cxx ("_office" and "_office")
 sal_Char const sXML_np__office[] = "_ooffice";
 sal_Char const sXML_np__text[] = "_otext";
 
 // #110680#
 SwXMLSectionList::SwXMLSectionList(
     const uno::Reference< uno::XComponentContext > xContext,
-    std::vector<String*> &rNewSectionList)
+    std::vector<OUString*> &rNewSectionList)
 :   SvXMLImport( xContext ),
     rSectionList ( rNewSectionList )
 {
@@ -93,11 +94,11 @@ SvXMLImportContext *SvXMLSectionListContext::CreateChildContext(
     const uno::Reference< xml::sax::XAttributeList > & xAttrList )
 {
     SvXMLImportContext *pContext = 0;
-    String sName;
 
     if (nPrefix == XML_NAMESPACE_TEXT && ( IsXMLToken ( rLocalName, XML_SECTION ) ||
                                            IsXMLToken ( rLocalName, XML_BOOKMARK) ) )
     {
+        OUString sName;
         sal_Int16 nAttrCount = xAttrList.is() ? xAttrList->getLength() : 0;
 
         for (sal_Int16 i=0; i < nAttrCount; i++)
@@ -105,12 +106,11 @@ SvXMLImportContext *SvXMLSectionListContext::CreateChildContext(
             const OUString& rAttrName = xAttrList->getNameByIndex( i );
             OUString aLocalName;
             sal_uInt16 nPrefx = rLocalRef.GetNamespaceMap().GetKeyByAttrName( rAttrName, &aLocalName);
-            const OUString& rAttrValue = xAttrList->getValueByIndex( i );
             if (XML_NAMESPACE_TEXT == nPrefx && IsXMLToken ( aLocalName, XML_NAME ) )
-                sName = rAttrValue;
+                sName = xAttrList->getValueByIndex( i );
         }
-        if ( sName.Len() )
-            rLocalRef.rSectionList.push_back( new String(sName) );
+        if ( !sName.isEmpty() )
+            rLocalRef.rSectionList.push_back( new OUString(sName) );
     }
 
     pContext = new SvXMLSectionListContext (rLocalRef, nPrefix, rLocalName, xAttrList);

sw/source/filter/ascii/parasc.cxx
@@ -69,7 +69,7 @@ public:
 
 
 // Call for the general reader interface
-sal_uLong AsciiReader::Read( SwDoc &rDoc, const String&, SwPaM &rPam, const String & )
+sal_uLong AsciiReader::Read( SwDoc &rDoc, const OUString&, SwPaM &rPam, const OUString & )
 {
     if( !pStrm )
     {
@@ -110,7 +110,7 @@ SwASCIIParser::SwASCIIParser(SwDoc* pD, const SwPaM& rCrsr, SvStream& rIn,
         pItemSet->Put( aLang, RES_CHRATR_CJK_LANGUAGE );
         pItemSet->Put( aLang, RES_CHRATR_CTL_LANGUAGE );
     }
-    if( rOpt.GetFontName().Len() )
+    if( !rOpt.GetFontName().isEmpty() )
     {
         Font aTextFont( rOpt.GetFontName(), Size( 0, 10 ) );
         if( pDoc->getPrinter( false ) )

sw/source/filter/ascii/wrtasc.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef _WRTASC_HXX
-#define _WRTASC_HXX
+#ifndef WRTASC_HXX
+#define WRTASC_HXX
 
 #include <shellio.hxx>
 #include <wrt_fn.hxx>
@@ -29,15 +29,15 @@ extern SwNodeFnTab aASCNodeFnTab;
 
 class SwASCWriter : public Writer
 {
-    String sLineEnd;
+    OUString m_sLineEnd;
 
     virtual sal_uLong WriteStream();
 
 public:
-    SwASCWriter( const String& rFilterName );
+    SwASCWriter( const OUString& rFilterName );
     virtual ~SwASCWriter();
 
-    const String& GetLineEnd() const      { return sLineEnd; }
+    OUString GetLineEnd() const { return m_sLineEnd; }
 };
 
 

sw/source/filter/basflt/fltini.cxx
@@ -95,7 +95,7 @@ Reader* SwReaderWriterEntry::GetReader()
     return NULL;
 }
 
-void SwReaderWriterEntry::GetWriter( const String& rNm, const String& rBaseURL, WriterRef& xWrt ) const
+void SwReaderWriterEntry::GetWriter( const OUString& rNm, const OUString& rBaseURL, WriterRef& xWrt ) const
 {
     if ( fnGetWriter )
         (*fnGetWriter)( rNm, rBaseURL, xWrt );
@@ -171,34 +171,34 @@ Reader* GetReader( ReaderWriterEnum eReader )
     return aReaderWriter[eReader].GetReader();
 }
 
-void GetWriter( const String& rFltName, const String& rBaseURL, WriterRef& xRet )
+void GetWriter( const OUString& rFltName, const OUString& rBaseURL, WriterRef& xRet )
 {
-        for( sal_uInt16 n = 0; n < MAXFILTER; ++n )
-                if( aFilterDetect[n].IsFilter( rFltName ) )
-                {
+    for( sal_uInt16 n = 0; n < MAXFILTER; ++n )
+        if ( aFilterDetect[n].IsFilter( rFltName ) )
+        {
             aReaderWriter[n].GetWriter( rFltName, rBaseURL, xRet );
-                        break;
-                }
+            break;
+        }
 }
 
-SwRead GetReader( const String& rFltName )
+SwRead GetReader( const OUString& rFltName )
 {
-        SwRead pRead = 0;
-        for( sal_uInt16 n = 0; n < MAXFILTER; ++n )
-                if( aFilterDetect[n].IsFilter( rFltName ) )
-                {
-                        pRead = aReaderWriter[n].GetReader();
-                        // fuer einige Reader noch eine Sonderbehandlung:
-                        if ( pRead )
-                                pRead->SetFltName( rFltName );
-                        break;
-                }
-        return pRead;
+    SwRead pRead = 0;
+    for( sal_uInt16 n = 0; n < MAXFILTER; ++n )
+        if ( aFilterDetect[n].IsFilter( rFltName ) )
+        {
+            pRead = aReaderWriter[n].GetReader();
+            // fuer einige Reader noch eine Sonderbehandlung:
+            if ( pRead )
+                    pRead->SetFltName( rFltName );
+            break;
+        }
+    return pRead;
 }
 
 } // namespace SwReaderWriter
 
-void Writer::SetVersion( const String&, long ) {}
+void Writer::SetVersion( const OUString&, long ) {}
 
 
 sal_Bool Writer::IsStgWriter() const { return sal_False; }
@@ -214,7 +214,7 @@ sal_Bool SwReader::NeedsPasswd( const Reader& /*rOptions*/ )
 }
 
 
-sal_Bool SwReader::CheckPasswd( const String& /*rPasswd*/, const Reader& /*rOptions*/ )
+sal_Bool SwReader::CheckPasswd( const OUString& /*rPasswd*/, const Reader& /*rOptions*/ )
 {
     return sal_True;
 }
@@ -298,7 +298,7 @@ void SwFilterOptions::Notify( const ::com::sun::star::uno::Sequence< OUString >&
 
 
 
-void StgReader::SetFltName( const String& rFltNm )
+void StgReader::SetFltName( const OUString& rFltNm )
 {
         if( SW_STORAGE_READER & GetReaderType() )
                 aFltName = rFltNm;
@@ -436,6 +436,9 @@ void CalculateFlySize(SfxItemSet& rFlySet, const SwNodeIndex& rAnchor,
         }
 }
 
+namespace
+{
+
 struct CharSetNameMap
 {
     rtl_TextEncoding eCode;
@@ -543,17 +546,18 @@ const CharSetNameMap *GetCharSetNameMap()
     };
     return &aMapArr[0];
 }
+
 /*
  Get a rtl_TextEncoding from its name
  */
-rtl_TextEncoding CharSetFromName(const String& rChrSetStr)
+rtl_TextEncoding CharSetFromName(const OUString& rChrSetStr)
 {
     const CharSetNameMap *pStart = GetCharSetNameMap();
     rtl_TextEncoding nRet = pStart->eCode;
 
     for(const CharSetNameMap *pMap = pStart; pMap->pName; ++pMap)
     {
-        if(rChrSetStr.EqualsIgnoreCaseAscii(pMap->pName))
+        if(rChrSetStr.equalsIgnoreAsciiCaseAscii(pMap->pName))
         {
             nRet = pMap->eCode;
             break;
@@ -565,11 +569,10 @@ rtl_TextEncoding CharSetFromName(const String& rChrSetStr)
         return nRet;
 }
 
-
 /*
  Get the String name of an rtl_TextEncoding
  */
-String NameFromCharSet(rtl_TextEncoding nChrSet)
+OUString NameFromCharSet(rtl_TextEncoding nChrSet)
 {
     const CharSetNameMap *pStart = GetCharSetNameMap();
     const char *pRet = pStart->pName;
@@ -588,6 +591,8 @@ String NameFromCharSet(rtl_TextEncoding nChrSet)
     return OUString::createFromAscii(pRet);
 }
 
+}
+
 // for the automatic conversion (mail/news/...)
 // The user data contains the options for the ascii import/export filter.
 // The format is:
@@ -602,9 +607,9 @@ void SwAsciiOptions::ReadUserData( const OUString& rStr )
 {
     sal_Int32 nToken = 0;
     sal_uInt16 nCnt = 0;
-    OUString sToken;
     do {
-        if( !(sToken = rStr.getToken( 0, ',', nToken )).isEmpty() )
+        const OUString sToken = rStr.getToken( 0, ',', nToken );
+        if (!sToken.isEmpty())
         {
             switch( nCnt )
             {
@@ -612,9 +617,9 @@ void SwAsciiOptions::ReadUserData( const OUString& rStr )
                 eCharSet = CharSetFromName(sToken);
                 break;
             case 1:         // LineEnd
-                if( sToken.equalsIgnoreAsciiCase( "CRLF" ))
+                if (sToken.equalsIgnoreAsciiCase("CRLF"))
                     eCRLF_Flag = LINEEND_CRLF;
-                else if( sToken.equalsIgnoreAsciiCase( "LF" ))
+                else if (sToken.equalsIgnoreAsciiCase("LF"))
                     eCRLF_Flag = LINEEND_LF;
                 else
                     eCRLF_Flag = LINEEND_CR;
@@ -634,8 +639,7 @@ void SwAsciiOptions::ReadUserData( const OUString& rStr )
 void SwAsciiOptions::WriteUserData(OUString& rStr)
 {
     // 1. charset
-    rStr = NameFromCharSet(eCharSet);
-    rStr += ",";
+    rStr = NameFromCharSet(eCharSet) + ",";
 
     // 2. LineEnd
     switch(eCRLF_Flag)
@@ -653,8 +657,7 @@ void SwAsciiOptions::WriteUserData(OUString& rStr)
     rStr += ",";
 
     // 3. Fontname
-    rStr += sFont;
-    rStr += ",";
+    rStr += sFont + ",";
 
     // 4. Language
     if (nLanguage)
@@ -693,7 +696,7 @@ Reader* GetRTFReader()
 
 }
 
-void GetRTFWriter( const String& rFltName, const String& rBaseURL, WriterRef& xRet )
+void GetRTFWriter( const OUString& rFltName, const OUString& rBaseURL, WriterRef& xRet )
 {
 #ifndef DISABLE_DYNLOADING
     FnGetWriter pFunction = reinterpret_cast<FnGetWriter>( SwGlobals::getFilters().GetMswordLibSymbol( "ExportRTF" ) );
@@ -721,7 +724,7 @@ Reader* GetWW8Reader()
 #endif
 }
 
-void GetWW8Writer( const String& rFltName, const String& rBaseURL, WriterRef& xRet )
+void GetWW8Writer( const OUString& rFltName, const OUString& rBaseURL, WriterRef& xRet )
 {
 #ifndef DISABLE_DYNLOADING
     FnGetWriter pFunction = reinterpret_cast<FnGetWriter>( SwGlobals::getFilters().GetMswordLibSymbol( "ExportDOC" ) );
@@ -738,7 +741,7 @@ void GetWW8Writer( const String& rFltName, const String& rBaseURL, WriterRef& xR
 typedef sal_uLong ( SAL_CALL *SaveOrDel )( SfxObjectShell&, SotStorage&, sal_Bool, const String& );
 typedef sal_uLong ( SAL_CALL *GetSaveWarning )( SfxObjectShell& );
 
-sal_uLong SaveOrDelMSVBAStorage( SfxObjectShell& rDoc, SotStorage& rStor, sal_Bool bSaveInto, const String& rStorageName )
+sal_uLong SaveOrDelMSVBAStorage( SfxObjectShell& rDoc, SotStorage& rStor, sal_Bool bSaveInto, const OUString& rStorageName )
 {
 #ifndef DISABLE_DYNLOADING
     SaveOrDel pFunction = reinterpret_cast<SaveOrDel>( SwGlobals::getFilters().GetMswordLibSymbol( "SaveOrDelMSVBAStorage_ww8" ) );

commit e363f09674d8d58cbf5e9f99254c38118bc3105b

SwASCWriter: out of bounds substring access
Regression from 106eadbb7aa09116fdb89d98e6c17fe1a48b3772, when simply
copy-pasting some string to a terminal window, rFltNm is empty.

Change-Id: I874e262ef1a3ebb38d90d9ef4f1b8d3457c5daff

sw/source/filter/ascii/wrtasc.cxx
@@ -74,7 +74,7 @@ SwASCWriter::SwASCWriter( const OUString& rFltNm )
                 break;
 
     default:
-        if( rFltNm.copy( 4 )=="_DLG" )
+        if( rFltNm.getLength() >= 4 && rFltNm.copy( 4 )=="_DLG" )
         {
             // use the options
             aNewOpts = GetAsciiOptions();