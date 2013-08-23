LemmingAvalanche/libreoffice-repaired

commit 3cb8707ad60b6288b36288ee13acb4a3285c4f54

sw: let SwTxtNode::GetTxt() return OUString
Change-Id: I36e777b2f623429773277e092284f7d2ba61e6ef

sw/source/core/crsr/findtxt.cxx
@@ -59,7 +59,7 @@ static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen
     const SwpHints *pHts = rNd.GetpSwpHints();
 
     sal_uInt16 n = 0;
-    xub_StrLen nSoftHyphen = nStart;
+    sal_Int32 nSoftHyphen = nStart;
     xub_StrLen nHintStart = STRING_LEN;
     bool bNewHint       = true;
     bool bNewSoftHyphen = true;
@@ -74,9 +74,11 @@ static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen
                          STRING_LEN;
 
         if ( bNewSoftHyphen )
-            nSoftHyphen = bRemoveSoftHyphen ?
-                          rNd.GetTxt().Search( CHAR_SOFTHYPHEN, nSoftHyphen ) :
-                          STRING_LEN;
+        {
+            nSoftHyphen = (bRemoveSoftHyphen)
+                    ?  rNd.GetTxt().indexOf(CHAR_SOFTHYPHEN, nSoftHyphen)
+                    : -1;
+        }
 
         bNewHint       = false;
         bNewSoftHyphen = false;
@@ -84,19 +86,21 @@ static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen
         xub_StrLen nStt = 0;
 
         // Check if next stop is a hint.
-        if ( STRING_LEN != nHintStart && nHintStart < nSoftHyphen && nHintStart < nEnd )
+        if ( STRING_LEN != nHintStart
+            && (-1 == nSoftHyphen || nHintStart < nSoftHyphen)
+            && nHintStart < nEnd )
         {
             nStt = nHintStart;
             bNewHint = true;
         }
         // Check if next stop is a soft hyphen.
-        else if ( STRING_LEN != nSoftHyphen && nSoftHyphen < nHintStart && nSoftHyphen < nEnd )
+        else if (-1 != nSoftHyphen && nSoftHyphen < nHintStart && nSoftHyphen < nEnd)
         {
             nStt = nSoftHyphen;
             bNewSoftHyphen = true;
         }
         // If nSoftHyphen == nHintStart, the current hint *must* be a hint with an end.
-        else if ( STRING_LEN != nSoftHyphen && nSoftHyphen == nHintStart )
+        else if (-1 != nSoftHyphen && nSoftHyphen == nHintStart)
         {
             nStt = nSoftHyphen;
             bNewHint = true;
@@ -258,7 +262,7 @@ sal_uInt8 SwPaM::Find( const SearchOptions& rSearchOpt, sal_Bool bSearchInNotes 
     {
         if( pNode->IsTxtNode() )
         {
-            nTxtLen = ((SwTxtNode*)pNode)->GetTxt().Len();
+            nTxtLen = static_cast<SwTxtNode*>(pNode)->GetTxt().getLength();
             if( rNdIdx == pPam->GetMark()->nNode )
                 nEnd = pPam->GetMark()->nContent.GetIndex();
             else
@@ -668,7 +672,8 @@ String *ReplaceBackReferences( const SearchOptions& rSearchOpt, SwPaM* pPam )
             }
             xub_StrLen nEnd = aStr.Len();
             bool bDeleteLastX = false;
-            if( pPam->End()->nContent < (static_cast<const SwTxtNode*>(pTxtNode))->GetTxt().Len() )
+            if (pPam->End()->nContent <
+                static_cast<const SwTxtNode*>(pTxtNode)->GetTxt().getLength())
             {
                 aStr.Insert( sX );
                 bDeleteLastX = true;

commit c4c44a34328e57b833ef472539fc287fe75f1eca

fdo#68319: sw: fix Chinese Conversion
... and perhaps other things too: the SwScriptIterator constructor gets
passed a temporary String created by implicit conversion from OUString
and retains a reference to that; convert the thing to use OUString
(and adapt some sCleanStr too not to get the inverse problem...).

(regression from 3cb8707ad60b6288b36288ee13acb4a3285c4f54)

Change-Id: I038ad0734385cdcae99ec404153f51ee427c7c64

sw/inc/txatritr.hxx
@@ -24,8 +24,8 @@
 #include <editeng/langitem.hxx>
 #include <hintids.hxx>
 #include <deque>
+#include <tools/string.hxx>
 
-class String;
 class SwTxtNode;
 class SwTxtAttr;
 class SfxPoolItem;
@@ -33,20 +33,20 @@ class SfxPoolItem;
 
 class SwScriptIterator
 {
-    const String& rText;
-    xub_StrLen nChgPos;
+    const OUString& m_rText;
+    sal_Int32 m_nChgPos;
     sal_uInt16 nCurScript;
     bool bForward;
 
 public:
-    SwScriptIterator( const String& rStr, xub_StrLen nStart = 0,
+    SwScriptIterator( const OUString& rStr, xub_StrLen nStart = 0,
                       bool bFrwrd = true );
 
     bool Next();
 
     sal_uInt16 GetCurrScript() const        { return nCurScript; }
-    xub_StrLen GetScriptChgPos() const      { return nChgPos; }
-    const String& GetText() const           { return rText; }
+    xub_StrLen GetScriptChgPos() const      { return (m_nChgPos == -1) ? STRING_LEN : m_nChgPos; }
+    const OUString& GetText() const         { return m_rText; }
 };
 
 

sw/source/core/crsr/findtxt.cxx
@@ -49,11 +49,11 @@ using namespace util;
 
 String *ReplaceBackReferences( const SearchOptions& rSearchOpt, SwPaM* pPam );
 
-static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen& rEnd,
-                      std::vector<sal_uLong> &rArr, String& rRet,
-                      bool bRemoveSoftHyphen )
+static OUString
+lcl_CleanStr(const SwTxtNode& rNd, xub_StrLen const nStart, xub_StrLen& rEnd,
+             std::vector<sal_uLong> &rArr, bool const bRemoveSoftHyphen)
 {
-    rRet = rNd.GetTxt();
+    OUStringBuffer buf(rNd.GetTxt());
     rArr.clear();
 
     const SwpHints *pHts = rNd.GetpSwpHints();
@@ -140,13 +140,13 @@ static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen
                            {
                             rArr.push_back( nAkt );
                             --rEnd;
-                            rRet.Erase( nAkt, 1 );
+                            buf.remove(nAkt, 1);
                            }
                         else
                            {
                             if ( bEmpty )
                                 aReplaced.push_back( nAkt );
-                            rRet.SetChar( nAkt, '\x7f' );
+                            buf[nAkt] = '\x7f';
                            }
                        }
                        break;
@@ -162,7 +162,7 @@ static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen
         {
             rArr.push_back( nAkt );
             --rEnd;
-            rRet.Erase( nAkt, 1 );
+            buf.remove(nAkt, 1);
             ++nSoftHyphen;
         }
     }
@@ -171,15 +171,15 @@ static String& lcl_CleanStr( const SwTxtNode& rNd, xub_StrLen nStart, xub_StrLen
     for( sal_uInt16 i = aReplaced.size(); i; )
     {
         const xub_StrLen nTmp = aReplaced[ --i ];
-        if( nTmp == rRet.Len() - 1 )
+        if (nTmp == buf.getLength() - 1)
         {
-            rRet.Erase( nTmp );
+            buf.truncate(nTmp);
             rArr.push_back( nTmp );
             --rEnd;
         }
     }
 
-    return rRet;
+    return buf.makeStringAndClear();
 }
 
 // skip all non SwPostIts inside the array
@@ -412,7 +412,7 @@ bool SwPaM::DoSearch( const SearchOptions& rSearchOpt, utl::TextSearch& rSTxt,
     bool bFound = false;
     SwNodeIndex& rNdIdx = pPam->GetPoint()->nNode;
     const SwNode* pSttNd = &rNdIdx.GetNode();
-    String sCleanStr;
+    OUString sCleanStr;
     std::vector<sal_uLong> aFltArr;
     LanguageType eLastLang = 0;
     // if the search string contains a soft hypen,
@@ -432,11 +432,11 @@ bool SwPaM::DoSearch( const SearchOptions& rSearchOpt, utl::TextSearch& rSTxt,
     }
 
     if( bSrchForward )
-        lcl_CleanStr( *(SwTxtNode*)pNode, nStart, nEnd,
-                        aFltArr, sCleanStr, bRemoveSoftHyphens );
+        sCleanStr = lcl_CleanStr(*static_cast<SwTxtNode*>(pNode), nStart, nEnd,
+                        aFltArr, bRemoveSoftHyphens);
     else
-        lcl_CleanStr( *(SwTxtNode*)pNode, nEnd, nStart,
-                        aFltArr, sCleanStr, bRemoveSoftHyphens );
+        sCleanStr = lcl_CleanStr(*static_cast<SwTxtNode*>(pNode), nEnd, nStart,
+                        aFltArr, bRemoveSoftHyphens);
 
     SwScriptIterator* pScriptIter = 0;
     sal_uInt16 nSearchScript = 0;

sw/source/core/txtnode/txatritr.cxx
@@ -30,11 +30,12 @@
 using namespace ::com::sun::star;
 
 
-SwScriptIterator::SwScriptIterator( const String& rStr, xub_StrLen nStt, bool bFrwrd )
-    : rText( rStr ),
-      nChgPos( rStr.Len() ),
-      nCurScript( i18n::ScriptType::WEAK ),
-      bForward( bFrwrd )
+SwScriptIterator::SwScriptIterator(
+            const OUString& rStr, xub_StrLen nStt, bool const bFrwrd)
+    : m_rText(rStr)
+    , m_nChgPos(rStr.getLength())
+    , nCurScript(i18n::ScriptType::WEAK)
+    , bForward(bFrwrd)
 {
     if( g_pBreakIt->GetBreakIter().is() )
     {
@@ -42,24 +43,27 @@ SwScriptIterator::SwScriptIterator( const String& rStr, xub_StrLen nStt, bool bF
             --nStt;
 
         xub_StrLen nPos = nStt;
-        nCurScript = g_pBreakIt->GetBreakIter()->getScriptType( rText, nPos );
+        nCurScript = g_pBreakIt->GetBreakIter()->getScriptType(m_rText, nPos);
         if( i18n::ScriptType::WEAK == nCurScript )
         {
             if( nPos )
             {
                 nPos = (xub_StrLen)g_pBreakIt->GetBreakIter()->beginOfScript(
-                                                rText, nPos, nCurScript );
-                if( nPos && nPos < rText.Len() )
+                                                m_rText, nPos, nCurScript);
+                if (nPos && nPos < m_rText.getLength())
                 {
                     nStt = --nPos;
-                    nCurScript = g_pBreakIt->GetBreakIter()->getScriptType( rText,nPos);
+                    nCurScript =
+                        g_pBreakIt->GetBreakIter()->getScriptType(m_rText,nPos);
                 }
             }
         }
 
-        nChgPos = bForward ?
-                  (xub_StrLen)g_pBreakIt->GetBreakIter()->endOfScript( rText, nStt, nCurScript ) :
-                  (xub_StrLen)g_pBreakIt->GetBreakIter()->beginOfScript( rText, nStt, nCurScript );
+        m_nChgPos = (bForward)
+            ?  g_pBreakIt->GetBreakIter()->endOfScript(
+                    m_rText, nStt, nCurScript)
+            :  g_pBreakIt->GetBreakIter()->beginOfScript(
+                    m_rText, nStt, nCurScript);
     }
 }
 
@@ -68,24 +72,26 @@ bool SwScriptIterator::Next()
     bool bRet = false;
     if( g_pBreakIt->GetBreakIter().is() )
     {
-        if ( bForward && nChgPos < rText.Len() )
+        if (bForward && (m_nChgPos != -1) && (m_nChgPos < m_rText.getLength()))
         {
-            nCurScript = g_pBreakIt->GetBreakIter()->getScriptType( rText, nChgPos );
-            nChgPos = (xub_StrLen)g_pBreakIt->GetBreakIter()->endOfScript(
-                                                rText, nChgPos, nCurScript );
+            nCurScript =
+                g_pBreakIt->GetBreakIter()->getScriptType(m_rText, m_nChgPos);
+            m_nChgPos = g_pBreakIt->GetBreakIter()->endOfScript(
+                                            m_rText, m_nChgPos, nCurScript);
             bRet = true;
         }
-        else if ( ! bForward && nChgPos )
+        else if (!bForward && m_nChgPos > 0)
         {
-            --nChgPos;
-            nCurScript = g_pBreakIt->GetBreakIter()->getScriptType( rText, nChgPos );
-            nChgPos = (xub_StrLen)g_pBreakIt->GetBreakIter()->beginOfScript(
-                                                rText, nChgPos, nCurScript );
+            --m_nChgPos;
+            nCurScript =
+                g_pBreakIt->GetBreakIter()->getScriptType(m_rText, m_nChgPos);
+            m_nChgPos = g_pBreakIt->GetBreakIter()->beginOfScript(
+                                                m_rText, m_nChgPos, nCurScript);
             bRet = true;
         }
     }
     else
-        nChgPos = rText.Len();
+        m_nChgPos = m_rText.getLength();
     return bRet;
 }
 
@@ -104,7 +110,7 @@ SwTxtAttrIterator::SwTxtAttrIterator( const SwTxtNode& rTNd, sal_uInt16 nWhchId,
 bool SwTxtAttrIterator::Next()
 {
     bool bRet = false;
-    if( nChgPos < aSIter.GetText().Len() )
+    if (nChgPos < aSIter.GetText().getLength())
     {
         bRet = true;
         if( !aStack.empty() )