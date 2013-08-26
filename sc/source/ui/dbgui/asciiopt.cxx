LemmingAvalanche/libreoffice-repaired

commit 95e0ec8d5bed7bef9f6ff67efa299f410f6ebadf

fdo#38838 Replaced some use of (Xub)String with OUString.
Change-Id: I5a8f2d1a5762c20d21d31b04778a9e2cac8df9cb
Reviewed-on: https://gerrit.libreoffice.org/2364
Reviewed-by: Thomas Arnhold <thomas@arnhold.org>
Tested-by: Thomas Arnhold <thomas@arnhold.org>

sc/source/ui/dbgui/asciiopt.cxx
@@ -309,40 +309,35 @@ void ScAsciiOptions::ReadFromString( const String& rString )
 
 String ScAsciiOptions::WriteToString() const
 {
-    String aOutStr;
+    OUString aOutStr;
 
         //
         //  Feld-Trenner
         //
 
     if ( bFixedLen )
-        aOutStr.AppendAscii(pStrFix);
+        aOutStr += pStrFix;
     else if ( !aFieldSeps.Len() )
-        aOutStr += '0';
+        aOutStr += "0";
     else
     {
         xub_StrLen nLen = aFieldSeps.Len();
         for (xub_StrLen i=0; i<nLen; i++)
         {
             if (i)
-                aOutStr += '/';
+                aOutStr += "/";
             aOutStr += OUString::number(aFieldSeps.GetChar(i));
         }
         if ( bMergeFieldSeps )
         {
-            aOutStr += '/';
-            aOutStr.AppendAscii(pStrMrg);
+            aOutStr += "/";
+            aOutStr += pStrMrg;
         }
     }
 
-    aOutStr += ',';                 // Token-Ende
-
-        //
-        //  Text-Trenner
-        //
-
-    aOutStr += OUString::number(cTextSep);
-    aOutStr += ',';                 // Token-Ende
+    aOutStr += "," +
+               // Text-Trenner
+               OUString::number(cTextSep) + ",";
 
         //
         //  Zeichensatz
@@ -352,14 +347,9 @@ String ScAsciiOptions::WriteToString() const
         aOutStr += ScGlobal::GetCharsetString( RTL_TEXTENCODING_DONTKNOW );
     else
         aOutStr += ScGlobal::GetCharsetString( eCharSet );
-    aOutStr += ',';                 // Token-Ende
-
-        //
-        //  Startzeile
-        //
-
-    aOutStr += OUString::number(nStartRow);
-    aOutStr += ',';                 // Token-Ende
+    aOutStr += "," +
+               // Startzeile
+               OUString::number(nStartRow) + ",";
 
         //
         //  Spalten-Infos
@@ -369,27 +359,22 @@ String ScAsciiOptions::WriteToString() const
     for (sal_uInt16 nInfo=0; nInfo<nInfoCount; nInfo++)
     {
         if (nInfo)
-            aOutStr += '/';
-        aOutStr += OUString::number(pColStart[nInfo]);
-        aOutStr += '/';
-        aOutStr += OUString::number(pColFormat[nInfo]);
+            aOutStr += "/";
+        aOutStr += OUString::number(pColStart[nInfo]) +
+                   "/" +
+                   OUString::number(pColFormat[nInfo]);
     }
 
     // #i112025# the options string is used in macros and linked sheets,
     // so new options must be added at the end, to remain compatible
 
-    aOutStr += ',';
-
-    // Language
-    aOutStr += OUString::number(eLang);
-    aOutStr += ',';
-
-    // Import quoted field as text.
-    aOutStr += bQuotedFieldAsText ? rtl::OUString("true") : rtl::OUString("false");
-    aOutStr += ',';
-
-    // Detect special nubmers.
-    aOutStr += bDetectSpecialNumber ? rtl::OUString("true") : rtl::OUString("false");
+    aOutStr += "," +
+               // Language
+               OUString::number(eLang) + "," +
+               // Import quoted field as text.
+               OUString::boolean( bQuotedFieldAsText ) + ",";
+               // Detect special numbers.
+               OUString::boolean( bDetectSpecialNumber );
 
     // 9th token is used for "Save as shown" in export options
     // 10th token is used for "Save cell formulas" in export options

cui/source/tabpages/autocdlg.cxx
@@ -683,9 +683,7 @@ void OfaSwAutoFmtOptionsPage::Reset( const SfxItemSet& )
     aCheckLB.SetUserData(  REPLACE_BULLETS, pUserData );
 
     nPercent = pOpt->nRightMargin;
-    sMargin = ' ';
-    sMargin += OUString::number( nPercent );
-    sMargin += '%';
+    sMargin = " " + OUString::number( nPercent ) + "%";
     pUserData = new ImpUserData(&sMargin, 0);
     aCheckLB.SetUserData( MERGE_SINGLE_LINE_PARA, pUserData );
 
@@ -739,9 +737,7 @@ IMPL_LINK_NOARG(OfaSwAutoFmtOptionsPage, EditHdl)
         if(RET_OK == aDlg.Execute())
         {
             nPercent = (sal_uInt16)aDlg.GetPrcntFld().GetValue();
-            sMargin = ' ';
-            sMargin += OUString::number( nPercent );
-            sMargin += '%';
+            sMargin = " " + OUString::number( nPercent ) + "%";
         }
     }
     aCheckLB.Invalidate();

dbaccess/source/ui/dlg/DbAdminImpl.cxx
@@ -135,8 +135,7 @@ namespace
 
         if ( _pPortNumber )
         {
-            sNewUrl += OUString::createFromAscii(":");
-            sNewUrl += OUString::number(_pPortNumber->GetValue());
+            sNewUrl = sNewUrl + ":" + OUString::number(_pPortNumber->GetValue());
         }
 
         return sNewUrl;
@@ -472,7 +471,7 @@ sal_Bool ODbDataSourceAdministrationHelper::hasAuthentication(const SfxItemSet& 
 // -----------------------------------------------------------------------------
 String ODbDataSourceAdministrationHelper::getConnectionURL() const
 {
-    String sNewUrl;
+    OUString sNewUrl;
 
     OUString eType = getDatasourceType(*m_pItemSetHelper->getOutputSet());
 
@@ -497,7 +496,7 @@ String ODbDataSourceAdministrationHelper::getConnectionURL() const
                 OUString sNewFileName;
                 if ( ::osl::FileBase::getSystemPathFromFileURL( sFileName, sNewFileName ) == ::osl::FileBase::E_None )
                 {
-                    sNewUrl += String(sNewFileName);
+                    sNewUrl += sNewFileName;
                 }
             }
             break;
@@ -528,7 +527,7 @@ String ODbDataSourceAdministrationHelper::getConnectionURL() const
                 SFX_ITEMSET_GET(*m_pItemSetHelper->getOutputSet(), pDatabaseName, SfxStringItem, DSID_DATABASENAME, sal_True);
                 if ( pHostName && pHostName->GetValue().getLength() )
                 {
-                    sNewUrl = OUString("@") + lcl_createHostWithPort(pHostName,pPortNumber);
+                    sNewUrl = "@" + lcl_createHostWithPort(pHostName,pPortNumber);
                     OUString sDatabaseName = pDatabaseName ? pDatabaseName->GetValue() : OUString();
                     if ( !sDatabaseName.getLength() && pUrlItem )
                         sDatabaseName = pCollection->cutPrefix( pUrlItem->GetValue() );
@@ -546,8 +545,7 @@ String ODbDataSourceAdministrationHelper::getConnectionURL() const
         case  ::dbaccess::DST_LDAP:
             {
                 SFX_ITEMSET_GET(*m_pItemSetHelper->getOutputSet(), pPortNumber, SfxInt32Item, DSID_CONN_LDAP_PORTNUMBER, sal_True);
-                sNewUrl = pCollection->cutPrefix(pUrlItem->GetValue());
-                sNewUrl += lcl_createHostWithPort(NULL,pPortNumber);
+                sNewUrl = pCollection->cutPrefix(pUrlItem->GetValue()) + lcl_createHostWithPort(NULL,pPortNumber);
             }
             break;
         case  ::dbaccess::DST_JDBC:
@@ -555,12 +553,8 @@ String ODbDataSourceAdministrationHelper::getConnectionURL() const
         default:
             break;
     }
-    if ( sNewUrl.Len() )
-    {
-        String sUrl = pCollection->getPrefix(eType);
-        sUrl += sNewUrl;
-        sNewUrl = sUrl;
-    }
+    if ( !sNewUrl.isEmpty() )
+        sNewUrl = pCollection->getPrefix(eType) + sNewUrl;
     else
         sNewUrl = pUrlItem->GetValue();
 

dbaccess/source/ui/dlg/directsql.cxx
@@ -286,16 +286,12 @@ DBG_NAME(DirectSQLDialog)
     //--------------------------------------------------------------------
     void DirectSQLDialog::addStatusText(const String& _rMessage)
     {
-        String sAppendMessage = OUString::number(m_nStatusCount++);
-        sAppendMessage += rtl::OUString(": ");
-        sAppendMessage += _rMessage;
-        sAppendMessage += rtl::OUString("\n\n");
+        OUString sAppendMessage = OUString::number(m_nStatusCount++) + ": " + _rMessage + "\n\n";
 
-        String sCompleteMessage = m_aStatus.GetText();
-        sCompleteMessage += sAppendMessage;
+        OUString sCompleteMessage = m_aStatus.GetText() + sAppendMessage;
         m_aStatus.SetText(sCompleteMessage);
 
-        m_aStatus.SetSelection(Selection(sCompleteMessage.Len(), sCompleteMessage.Len()));
+        m_aStatus.SetSelection(Selection(sCompleteMessage.getLength(), sCompleteMessage.getLength()));
     }
 
     //--------------------------------------------------------------------

dbaccess/source/ui/querydesign/QueryTableView.cxx
@@ -255,11 +255,7 @@ sal_Int32 OQueryTableView::CountTableAlias(const String& rName, sal_Int32& rMax)
     OTableWindowMapIterator aIter = GetTabWinMap()->find(rName);
     while(aIter != GetTabWinMap()->end())
     {
-        String aNewName;
-        aNewName = rName;
-        aNewName += '_';
-        aNewName += OUString::number(++nRet);
-
+        OUString aNewName = OUString(rName) + "_" + OUString::number(++nRet);
         aIter = GetTabWinMap()->find(aNewName);
     }
 

dbaccess/source/ui/tabledesign/TEditControl.cxx
@@ -895,8 +895,7 @@ String OTableEditorCtrl::GenerateName( const String& rName )
     sal_Int32 i=1;
     while( HasFieldName(aFieldName) )
     {
-        aFieldName = aBaseName;
-        aFieldName += OUString::number(i);
+        aFieldName = aBaseName + OUString::number(i);
         i++;
     }
 

editeng/source/items/frmitems.cxx
@@ -97,7 +97,7 @@ inline void SetValueProp( XubString& rStr, const sal_uInt16 nValue,
     if( 100 == nProp )
         rStr += OUString::number( nValue );
     else
-        ( rStr += OUString::number( nProp )) += sal_Unicode('%');
+        rStr += OUString::number(nProp) + "%";
 }
 
 inline void SetValueProp( XubString& rStr, const short nValue,
@@ -106,7 +106,7 @@ inline void SetValueProp( XubString& rStr, const short nValue,
     if( 100 == nProp )
         rStr += OUString::number( nValue );
     else
-        ( rStr += OUString::number( nProp )) += sal_Unicode('%');
+        rStr += OUString::number(nProp) + "%";
 }
 
 // -----------------------------------------------------------------------

editeng/source/items/itemtype.cxx
@@ -137,7 +137,7 @@ XubString GetSvxString( sal_uInt16 nId )
 
 XubString GetColorString( const Color& rCol )
 {
-    XubString sStr;
+    OUString sStr;
 
     ColorData nColData =
         RGB_COLORDATA( rCol.GetRed(), rCol.GetGreen(), rCol.GetBlue() );
@@ -158,16 +158,12 @@ XubString GetColorString( const Color& rCol )
     if ( nColor < sizeof(aColAry)/sizeof(ColorData) )
         sStr = EE_RESSTR( RID_SVXITEMS_COLOR_BEGIN + nColor + 1 );
 
-    if ( !sStr.Len() )
+    if ( sStr.isEmpty() )
     {
-        sStr.AppendAscii(RTL_CONSTASCII_STRINGPARAM( "RGB" ));
-        sStr += sal_Unicode('(');
-        sStr += OUString::number( rCol.GetRed() );
-        sStr += cpDelim;
-        sStr += OUString::number( rCol.GetGreen() );
-        sStr += cpDelim;
-        sStr += OUString::number( rCol.GetBlue() );
-        sStr += sal_Unicode(')');
+        sStr += "RGB(" +
+                OUString::number( rCol.GetRed() )   + OUString(cpDelim) +
+                OUString::number( rCol.GetGreen() ) + OUString(cpDelim) +
+                OUString::number( rCol.GetBlue() )  + ")";
     }
     return sStr;
 }

commit cb7e75c83c9bb76a4fca3b26bb7bf6d23bbb6a8a

resolved fdo#68437 append the bDetectSpecialNumber "true" string in options
Regression introduced with 95e0ec8d5bed7bef9f6ff67efa299f410f6ebadf

Change-Id: I7c05c7f246dddd0271c421aa358068365d44fb98

sc/source/ui/dbgui/asciiopt.cxx
@@ -372,7 +372,7 @@ String ScAsciiOptions::WriteToString() const
                // Language
                OUString::number(eLang) + "," +
                // Import quoted field as text.
-               OUString::boolean( bQuotedFieldAsText ) + ",";
+               OUString::boolean( bQuotedFieldAsText ) + "," +
                // Detect special numbers.
                OUString::boolean( bDetectSpecialNumber );
 