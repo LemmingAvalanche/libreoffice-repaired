LemmingAvalanche/libreoffice-repaired

commit 0ff25c82f7def37c5989c867279830f1a89fa047

re-base on ALv2 code. Includes (at least) relevant parts of:
    linecap: Reintegrating finished LineCap feature
    Patch contributed by Regina Henschel
    http://svn.apache.org/viewvc?view=revision&revision=1232507

    Patches contributed by Sven Jacobi
    impress212: #i81610# fixed animation export
    http://svn.apache.org/viewvc?view=revision&revision=1167620
    impress212: drawinglayer gbuild environment changes
    http://svn.apache.org/viewvc?view=revision&revision=1167627
    http://svn.apache.org/viewvc?view=revision&revision=1167628
    impress212: DffPropSet -> minor code improvements, removing table
    http://svn.apache.org/viewvc?view=revision&revision=1167634
    impress212: #158494# fixed excel import (text rotation)
    http://svn.apache.org/viewvc?view=revision&revision=1167638

    Patches contributed by Armin Le Grand
    Svg: Reintegrated Svg replacement from /branches/alg/svgreplavement
    http://svn.apache.org/viewvc?view=revision&revision=1220836
    #118728# changed indentifying definitions for Svg file detection
    http://svn.apache.org/viewvc?view=revision&revision=1229961
    #118838# LineGeometry creation for complicated cases optimized to
	create single Polygons
    http://svn.apache.org/viewvc?view=revision&revision=1236232
    #119176# corrected file type detection for SVG for svg files
	without xml header
    http://svn.apache.org/viewvc?view=revision&revision=1309445
    #118728# Extended Svg file detection
    http://svn.apache.org/viewvc?view=revision&revision=1230531
    #118529# solve break converters and convert commands for OLEs and images
    http://svn.apache.org/viewvc?view=revision&revision=1186168
    svg: added WaE changes from branch svgreplacement to trunc
    http://svn.apache.org/viewvc?view=revision&revision=1222974
    svg: corrected missing member initialization
    http://svn.apache.org/viewvc?view=revision&revision=1226134
    fix for #118525#: Using primitives for chart sub-geometry visualisation

xmloff/source/core/xmlmultiimagehelper.cxx
@@ -1,0 +1,140 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <xmloff/xmlmultiimagehelper.hxx>
+#include <rtl/ustring.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace ::com::sun::star;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace
+{
+    sal_uInt32 getQualityIndex(const rtl::OUString& rString)
+    {
+        sal_uInt32 nRetval(0);
+
+        // pixel formats first
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".bmp")))
+        {
+            return 10;
+        }
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".gif")))
+        {
+            return 20;
+        }
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".jpg")))
+        {
+            return 30;
+        }
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".png")))
+        {
+            return 40;
+        }
+
+        // vector formats, prefer always
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".svm")))
+        {
+            return 1000;
+        }
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".wmf")))
+        {
+            return 1010;
+        }
+        if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".emf")))
+        {
+            return 1020;
+        }
+        else if(rString.endsWithAsciiL(RTL_CONSTASCII_STRINGPARAM(".svg")))
+        {
+            return 1030;
+        }
+
+        return nRetval;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+multiImageImportHelper::multiImageImportHelper()
+:   maImplContextVector(),
+    mbSupportsMultipleContents(false)
+{
+}
+
+multiImageImportHelper::~multiImageImportHelper()
+{
+    while(!maImplContextVector.empty())
+    {
+        delete *(maImplContextVector.end() - 1);
+        maImplContextVector.pop_back();
+    }
+}
+
+void multiImageImportHelper::solveMultipleImages()
+{
+    if(maImplContextVector.size() > 1)
+    {
+        // multiple child contexts were imported, decide which is the most valuable one
+        // and remove the rest
+        sal_uInt32 nIndexOfPreferred(maImplContextVector.size());
+        sal_uInt32 nBestQuality(0), a(0);
+
+        for(a = 0; a < maImplContextVector.size(); a++)
+        {
+            const rtl::OUString aStreamURL(getGraphicURLFromImportContext(**maImplContextVector[a]));
+            const sal_uInt32 nNewQuality(getQualityIndex(aStreamURL));
+
+            if(nNewQuality > nBestQuality)
+            {
+                nBestQuality = nNewQuality;
+                nIndexOfPreferred = a;
+            }
+        }
+
+        // correct if needed, default is to use the last entry
+        if(nIndexOfPreferred >= maImplContextVector.size())
+        {
+            nIndexOfPreferred = maImplContextVector.size() - 1;
+        }
+
+        // Take out the most valuable one
+        const std::vector< SvXMLImportContextRef* >::iterator aRemove(maImplContextVector.begin() + nIndexOfPreferred);
+        delete *aRemove;
+        maImplContextVector.erase(aRemove);
+
+        // remove the rest from parent
+        for(a = 0; a < maImplContextVector.size(); a++)
+        {
+            removeGraphicFromImportContext(**maImplContextVector[a]);
+        }
+    }
+}
+
+void multiImageImportHelper::addContent(const SvXMLImportContext& rSvXMLImportContext)
+{
+    if(dynamic_cast< const SvXMLImportContext* >(&rSvXMLImportContext))
+    {
+        maImplContextVector.push_back(new SvXMLImportContextRef(const_cast< SvXMLImportContext* >(&rSvXMLImportContext)));
+    }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

xmloff/source/draw/ximpshap.cxx
@@ -1,30 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2000, 2010 Oracle and/or its affiliates.
+ * This file incorporates work covered by the following license notice:
  *
- * OpenOffice.org - a multi-platform office productivity suite
- *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * only, as published by the Free Software Foundation.
- *
- * OpenOffice.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
- *
- ************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 #include <tools/debug.hxx>
 #include <com/sun/star/document/XEventsSupplier.hpp>
@@ -83,6 +74,7 @@
 #include "xmloff/xmlerror.hxx"
 #include <basegfx/matrix/b2dhommatrix.hxx>
 #include <com/sun/star/drawing/XEnhancedCustomShapeDefaulter.hpp>
+#include <com/sun/star/container/XChild.hpp>
 #include <com/sun/star/text/XTextDocument.hpp>
 
 using ::rtl::OUString;
@@ -2704,6 +2696,30 @@ void SdXMLObjectShapeContext::StartElement( const ::com::sun::star::uno::Referen
 
 void SdXMLObjectShapeContext::EndElement()
 {
+    // #i67705#
+    const sal_uInt16 nGeneratorVersion(GetImport().getGeneratorVersion());
+
+#ifdef FIXME_REMOVE_WHEN_RE_BASE_COMPLETE
+    if(nGeneratorVersion < SvXMLImport::OOo_34x)
+#else
+    if(nGeneratorVersion < SvXMLImport::OOo_Current)
+#endif
+    {
+        // #i118485#
+        // If it's an old file from us written before OOo3.4, we need to correct
+        // FillStyle and LineStyle for OLE2 objects. The error was that the old paint
+        // implementations just ignored added fill/linestyles completely, thus
+        // those objects need to be corrected to not show blue and hairline which
+        // always was the default, but would be shown now
+        uno::Reference< beans::XPropertySet > xProps(mxShape, uno::UNO_QUERY);
+
+        if( xProps.is() )
+        {
+            xProps->setPropertyValue(::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("FillStyle")), uno::makeAny(drawing::FillStyle_NONE));
+            xProps->setPropertyValue(::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("LineStyle")), uno::makeAny(drawing::LineStyle_NONE));
+        }
+    }
+
     // #100592#
     if( mxBase64Stream.is() )
     {
@@ -3337,7 +3353,10 @@ SdXMLFrameShapeContext::SdXMLFrameShapeContext( SvXMLImport& rImport, sal_uInt16
         com::sun::star::uno::Reference< com::sun::star::drawing::XShapes >& rShapes,
         sal_Bool bTemporaryShape)
 : SdXMLShapeContext( rImport, nPrfx, rLocalName, xAttrList, rShapes, bTemporaryShape ),
-    mbSupportsReplacement( sal_False )
+    multiImageImportHelper(),
+    mbSupportsReplacement( sal_False ),
+    mxImplContext(),
+    mxReplImplContext()
 {
     uno::Reference < util::XCloneable > xClone( xAttrList, uno::UNO_QUERY );
     if( xClone.is() )
@@ -3349,6 +3368,73 @@ SdXMLFrameShapeContext::SdXMLFrameShapeContext( SvXMLImport& rImport, sal_uInt16
 
 SdXMLFrameShapeContext::~SdXMLFrameShapeContext()
 {
+}
+
+void SdXMLFrameShapeContext::removeGraphicFromImportContext(const SvXMLImportContext& rContext) const
+{
+    const SdXMLGraphicObjectShapeContext* pSdXMLGraphicObjectShapeContext = dynamic_cast< const SdXMLGraphicObjectShapeContext* >(&rContext);
+
+    if(pSdXMLGraphicObjectShapeContext)
+    {
+        try
+        {
+            uno::Reference< container::XChild > xChild(pSdXMLGraphicObjectShapeContext->getShape(), uno::UNO_QUERY_THROW);
+
+            if(xChild.is())
+            {
+                uno::Reference< drawing::XShapes > xParent(xChild->getParent(), uno::UNO_QUERY_THROW);
+
+                if(xParent.is())
+                {
+                    // remove from parent
+                    xParent->remove(pSdXMLGraphicObjectShapeContext->getShape());
+
+                    // dispose
+                    uno::Reference< lang::XComponent > xComp(pSdXMLGraphicObjectShapeContext->getShape(), UNO_QUERY);
+
+                    if(xComp.is())
+                    {
+                        xComp->dispose();
+                    }
+                }
+            }
+        }
+        catch( uno::Exception& )
+        {
+            OSL_FAIL( "Error in cleanup of multiple graphic object import (!)" );
+        }
+    }
+}
+
+rtl::OUString SdXMLFrameShapeContext::getGraphicURLFromImportContext(const SvXMLImportContext& rContext) const
+{
+    rtl::OUString aRetval;
+    const SdXMLGraphicObjectShapeContext* pSdXMLGraphicObjectShapeContext = dynamic_cast< const SdXMLGraphicObjectShapeContext* >(&rContext);
+
+    if(pSdXMLGraphicObjectShapeContext)
+    {
+        try
+        {
+            const uno::Reference< beans::XPropertySet > xPropSet(pSdXMLGraphicObjectShapeContext->getShape(), uno::UNO_QUERY_THROW);
+
+            if(xPropSet.is())
+            {
+                xPropSet->getPropertyValue(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("GraphicStreamURL"))) >>= aRetval;
+
+                if(!aRetval.getLength())
+                {
+                    // it maybe a link, try GraphicURL
+                    xPropSet->getPropertyValue(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("GraphicURL"))) >>= aRetval;
+                }
+            }
+        }
+        catch( uno::Exception& )
+        {
+            OSL_FAIL( "Error in cleanup of multiple graphic object import (!)" );
+        }
+    }
+
+    return aRetval;
 }
 
 SvXMLImportContext *SdXMLFrameShapeContext::CreateChildContext( sal_uInt16 nPrefix,
@@ -3370,8 +3456,25 @@ SvXMLImportContext *SdXMLFrameShapeContext::CreateChildContext( sal_uInt16 nPref
             pShapeContext->setHyperlink( msHyperlink );
 
         mxImplContext = pContext;
-        mbSupportsReplacement = IsXMLToken( rLocalName, XML_OBJECT ) ||
-                                IsXMLToken( rLocalName, XML_OBJECT_OLE );
+        mbSupportsReplacement = IsXMLToken(rLocalName, XML_OBJECT ) || IsXMLToken(rLocalName, XML_OBJECT_OLE);
+        setSupportsMultipleContents(IsXMLToken(rLocalName, XML_IMAGE));
+
+        if(getSupportsMultipleContents() && dynamic_cast< SdXMLGraphicObjectShapeContext* >(pContext))
+        {
+            addContent(*mxImplContext);
+        }
+    }
+    else if(getSupportsMultipleContents() && XML_NAMESPACE_DRAW == nPrefix && IsXMLToken(rLocalName, XML_IMAGE))
+    {
+        // read another image
+        pContext = GetImport().GetShapeImport()->CreateFrameChildContext(
+            GetImport(), nPrefix, rLocalName, xAttrList, mxShapes, mxAttrList);
+        mxImplContext = pContext;
+
+        if(dynamic_cast< SdXMLGraphicObjectShapeContext* >(pContext))
+        {
+            addContent(*mxImplContext);
+        }
     }
     else if( mbSupportsReplacement && !mxReplImplContext &&
              XML_NAMESPACE_DRAW == nPrefix &&
@@ -3431,6 +3534,9 @@ void SdXMLFrameShapeContext::StartElement(const uno::Reference< xml::sax::XAttri
 
 void SdXMLFrameShapeContext::EndElement()
 {
+    /// solve if multiple image child contexts were imported
+    solveMultipleImages();
+
     if( !mxImplContext.Is() )
     {
         // now check if this is an empty presentation object

xmloff/source/text/XMLTextFrameContext.cxx
@@ -1,30 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2000, 2010 Oracle and/or its affiliates.
+ * This file incorporates work covered by the following license notice:
  *
- * OpenOffice.org - a multi-platform office productivity suite
- *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * only, as published by the Free Software Foundation.
- *
- * OpenOffice.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
- *
- ************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 #include <tools/debug.hxx>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
@@ -429,6 +420,7 @@ public:
     TYPEINFO();
 
     sal_Bool CreateIfNotThere();
+    const OUString& GetHRef() const { return sHRef; }
 
     XMLTextFrameContext_Impl( SvXMLImport& rImport,
             sal_uInt16 nPrfx,
@@ -463,7 +455,7 @@ public:
     ::com::sun::star::text::TextContentAnchorType GetAnchorType() const { return eAnchorType; }
 
     const ::com::sun::star::uno::Reference <
-        ::com::sun::star::beans::XPropertySet >& GetPropSet() { return xPropSet; }
+        ::com::sun::star::beans::XPropertySet >& GetPropSet() const { return xPropSet; }
 };
 
 TYPEINIT1( XMLTextFrameContext_Impl, SvXMLImportContext );
@@ -756,8 +748,6 @@ void XMLTextFrameContext_Impl::Create( sal_Bool /*bHRefOrBase64*/ )
         xTextImportHelper->InsertTextContent( xTxtCntnt );
     }
 
-    Reference < XShape > xShape( xPropSet, UNO_QUERY );
-
     // #107848#
     // Make adding the shepe to Z-Ordering dependent from if we are
     // inside a inside_deleted_section (redlining). That is necessary
@@ -767,6 +757,8 @@ void XMLTextFrameContext_Impl::Create( sal_Bool /*bHRefOrBase64*/ )
     if(!GetImport().HasTextImport()
         || !GetImport().GetTextImport()->IsInsideDeleteContext())
     {
+        Reference < XShape > xShape( xPropSet, UNO_QUERY );
+
         GetImport().GetShapeImport()->shapeWithZIndexAdded( xShape, nZIndex );
     }
 
@@ -783,6 +775,43 @@ void XMLTextFrameContext_Impl::Create( sal_Bool /*bHRefOrBase64*/ )
         xTextImportHelper->PushListContext();
         mbListContextPushed = true;
     }
+}
+
+
+void XMLTextFrameContext::removeGraphicFromImportContext(const SvXMLImportContext& rContext) const
+{
+    const XMLTextFrameContext_Impl* pXMLTextFrameContext_Impl = dynamic_cast< const XMLTextFrameContext_Impl* >(&rContext);
+
+    if(pXMLTextFrameContext_Impl)
+    {
+        try
+        {
+            // just dispose to delete
+            uno::Reference< lang::XComponent > xComp(pXMLTextFrameContext_Impl->GetPropSet(), UNO_QUERY);
+
+            if(xComp.is())
+            {
+                xComp->dispose();
+            }
+        }
+        catch( uno::Exception& )
+        {
+            OSL_FAIL( "Error in cleanup of multiple graphic object import (!)" );
+        }
+    }
+}
+
+rtl::OUString XMLTextFrameContext::getGraphicURLFromImportContext(const SvXMLImportContext& rContext) const
+{
+    rtl::OUString aRetval;
+    const XMLTextFrameContext_Impl* pXMLTextFrameContext_Impl = dynamic_cast< const XMLTextFrameContext_Impl* >(&rContext);
+
+    if(pXMLTextFrameContext_Impl)
+    {
+        return pXMLTextFrameContext_Impl->GetHRef();
+    }
+
+    return aRetval;
 }
 
 sal_Bool XMLTextFrameContext_Impl::CreateIfNotThere()
@@ -1313,6 +1342,7 @@ XMLTextFrameContext::XMLTextFrameContext(
         const Reference< XAttributeList > & xAttrList,
         TextContentAnchorType eATyp )
 :   SvXMLImportContext( rImport, nPrfx, rLName )
+,   multiImageImportHelper()
 ,   m_xAttrList( new SvXMLAttributeList( xAttrList ) )
 ,   m_pHyperlink( 0 )
     // Implement Title/Description Elements UI (#i73249#)
@@ -1371,6 +1401,9 @@ XMLTextFrameContext::~XMLTextFrameContext()
 
 void XMLTextFrameContext::EndElement()
 {
+    /// solve if multiple image child contexts were imported
+    solveMultipleImages();
+
     SvXMLImportContext *pContext = &m_xImplContext;
     XMLTextFrameContext_Impl *pImpl = PTR_CAST( XMLTextFrameContext_Impl, pContext );
     if( pImpl )
@@ -1468,6 +1501,10 @@ SvXMLImportContext *XMLTextFrameContext::CreateChildContext(
                 {
                     m_bSupportsReplacement = sal_True;
                 }
+                else if(XML_TEXT_FRAME_GRAPHIC == nFrameType)
+                {
+                    setSupportsMultipleContents(IsXMLToken(rLocalName, XML_IMAGE));
+                }
 
                 if( !pContext )
                 {
@@ -1480,8 +1517,23 @@ SvXMLImportContext *XMLTextFrameContext::CreateChildContext(
                 }
 
                 m_xImplContext = pContext;
+
+                if(getSupportsMultipleContents() && XML_TEXT_FRAME_GRAPHIC == nFrameType)
+                {
+                    addContent(*m_xImplContext);
+                }
             }
         }
+    }
+    else if(getSupportsMultipleContents() && XML_NAMESPACE_DRAW == p_nPrefix && IsXMLToken(rLocalName, XML_IMAGE))
+    {
+        // read another image
+        pContext = new XMLTextFrameContext_Impl(
+            GetImport(), p_nPrefix, rLocalName, xAttrList,
+            m_eDefaultAnchorType, XML_TEXT_FRAME_GRAPHIC, m_xAttrList);
+
+        m_xImplContext = pContext;
+        addContent(*m_xImplContext);
     }
     else if( m_bSupportsReplacement && !m_xReplImplContext &&
              XML_NAMESPACE_DRAW == p_nPrefix &&

xmloff/inc/xmloff/xmlmultiimagehelper.hxx
@@ -1,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef _XMLOFF_XMLMULTIIMAGEHELPER_HXX
+#define _XMLOFF_XMLMULTIIMAGEHELPER_HXX
+
+#include <sal/types.h>
+#include <xmloff/xmlimp.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+class multiImageImportHelper
+{
+private:
+    std::vector< SvXMLImportContextRef* >       maImplContextVector;
+    bool                                        mbSupportsMultipleContents;
+
+protected:
+    /// helper to get the created xShape instance, needs to be overloaded
+    virtual rtl::OUString getGraphicURLFromImportContext(const SvXMLImportContext& rContext) const = 0;
+    virtual void removeGraphicFromImportContext(const SvXMLImportContext& rContext) const = 0;
+
+public:
+    multiImageImportHelper();
+    virtual ~multiImageImportHelper();
+
+    /// solve multiple imported images. The most valuable one is choosen,
+    /// see imlementation for evtl. changing weights and/or adding filetypes
+    void solveMultipleImages();
+
+    /// add a content to the remembered image import contexts
+    void addContent(const SvXMLImportContext& rSvXMLImportContext);
+
+    /// read/write access to boolean switch
+    bool getSupportsMultipleContents() const { return mbSupportsMultipleContents; }
+    void setSupportsMultipleContents(bool bNew) { mbSupportsMultipleContents = bNew; }
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif  //  _XMLOFF_XMLMULTIIMAGEHELPER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 188b70c50d203f050e5304ebc5f1ed1242f735bf

fdo#68839: ODF import: fix name of multi-image frame
When multiple image child elements are inside a frame, each one is
imported and gets unique name via SwDoc::SetFlyName().  But the
retained one is not necessarily the first one, which is the only one
that may have the original name.

Also the solveMultipleImages needs to return a smart pointer, as nothing
else keeps the image contexts alive.

(regression from 0ff25c82f7def37c5989c867279830f1a89fa047)

Change-Id: I28a8a752f3eed176cc2ebb4c9af11a0dd4d18ea6

include/xmloff/xmlmultiimagehelper.hxx
@@ -44,7 +44,7 @@ public:
     /// see imlementation for evtl. changing weights and/or adding filetypes.
     ///
     /// @returns import context of the selected image
-    const SvXMLImportContext* solveMultipleImages();
+    SvXMLImportContextRef solveMultipleImages();
 
     /// add a content to the remembered image import contexts
     void addContent(const SvXMLImportContext& rSvXMLImportContext);

xmloff/source/core/xmlmultiimagehelper.cxx
@@ -83,9 +83,9 @@ MultiImageImportHelper::~MultiImageImportHelper()
     }
 }
 
-const SvXMLImportContext* MultiImageImportHelper::solveMultipleImages()
+SvXMLImportContextRef MultiImageImportHelper::solveMultipleImages()
 {
-    const SvXMLImportContext* pContext(0);
+    SvXMLImportContextRef pContext;
     if(maImplContextVector.size() > 1)
     {
         // multiple child contexts were imported, decide which is the most valuable one

xmloff/source/draw/ximpshap.cxx
@@ -3426,8 +3426,9 @@ void SdXMLFrameShapeContext::StartElement(const uno::Reference< xml::sax::XAttri
 void SdXMLFrameShapeContext::EndElement()
 {
     // solve if multiple image child contexts were imported
-    const SvXMLImportContext* const pSelectedContext(solveMultipleImages());
-    const SdXMLGraphicObjectShapeContext* pShapeContext( dynamic_cast<const SdXMLGraphicObjectShapeContext*>( pSelectedContext ) );
+    SvXMLImportContextRef const pSelectedContext(solveMultipleImages());
+    const SdXMLGraphicObjectShapeContext* pShapeContext(
+        dynamic_cast<const SdXMLGraphicObjectShapeContext*>(&pSelectedContext));
     if ( pShapeContext && !maShapeId.isEmpty() )
     {
         // fdo#64512 and fdo#60075 - make sure *this* shape is

xmloff/source/text/XMLTextFrameContext.cxx
@@ -364,6 +364,7 @@ class XMLTextFrameContext_Impl : public SvXMLImportContext
     const OUString sTextBoxServiceName;
     const OUString sGraphicServiceName;
 
+    OUString m_sOrigName;
     OUString sName;
     OUString sStyleName;
     OUString sNextName;
@@ -440,6 +441,8 @@ public:
     void SetTitle( const OUString& rTitle );
 
     void SetDesc( const OUString& rDesc );
+
+    void SetName();
 
     ::com::sun::star::text::TextContentAnchorType GetAnchorType() const { return eAnchorType; }
 
@@ -893,6 +896,7 @@ XMLTextFrameContext_Impl::XMLTextFrameContext_Impl(
             sStyleName = rValue;
             break;
         case XML_TOK_TEXT_FRAME_NAME:
+            m_sOrigName = rValue;
             sName = rValue;
             break;
         case XML_TOK_TEXT_FRAME_FRAME_NAME:
@@ -1276,6 +1280,19 @@ void XMLTextFrameContext_Impl::SetHyperlink( const OUString& rHRef,
     }
 }
 
+void XMLTextFrameContext_Impl::SetName()
+{
+    Reference<XNamed> xNamed(xPropSet, UNO_QUERY);
+    if (xNamed.is())
+    {
+        OUString const name(xNamed->getName());
+        if (name != m_sOrigName)
+        {
+            xNamed->setName(m_sOrigName);
+        }
+    }
+}
+
 // Implement Title/Description Elements UI (#i73249#)
 void XMLTextFrameContext_Impl::SetTitle( const OUString& rTitle )
 {
@@ -1383,13 +1400,22 @@ XMLTextFrameContext::~XMLTextFrameContext()
 void XMLTextFrameContext::EndElement()
 {
     /// solve if multiple image child contexts were imported
-    solveMultipleImages();
+    SvXMLImportContextRef const pMultiContext(solveMultipleImages());
 
-    SvXMLImportContext *pContext = &m_xImplContext;
+    SvXMLImportContext const*const pContext =
+        (pMultiContext) ? &pMultiContext : &m_xImplContext;
     XMLTextFrameContext_Impl *pImpl = PTR_CAST( XMLTextFrameContext_Impl, pContext );
+    assert(!pMultiContext || pImpl);
     if( pImpl )
     {
         pImpl->CreateIfNotThere();
+
+        // fdo#68839: in case the surviving image was not the first one,
+        // it will have a counter added to its name - set the original name
+        if (pMultiContext) // do this only when necessary; esp. not for text
+        {                  // frames that may have entries in GetRenameMap()!
+            pImpl->SetName();
+        }
 
         if( !m_sTitle.isEmpty() )
         {