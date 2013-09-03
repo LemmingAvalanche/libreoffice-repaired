LemmingAvalanche/libreoffice-repaired

commit c5eff79891ceb6f5d65b4a5b8c1b4a82a079ee54

finish deprecation of O(U)String::valueOf()
Compiler plugin to replace with matching number(), boolean() or OUString ctor,
ran it, few manual tweaks, mark as really deprecated.

Change-Id: I4a79bdbcf4c460d21e73b635d2bd3725c22876b2

codemaker/source/codemaker/global.cxx
@@ -355,7 +355,7 @@ bool FileStream::write(void const * buffer, sal_uInt64 size) {
 
 FileStream &operator<<(FileStream& o, sal_uInt32 i) {
     sal_uInt64 writtenBytes;
-    OString s = OString::valueOf((sal_Int32)i);
+    OString s = OString::number(i);
     osl_writeFile(o.m_file, s.getStr(), s.getLength() * sizeof(sal_Char), &writtenBytes);
     return o;
 }

commit 3774a02c7114083704f3c822c0a1606f7a79929c

Write integers as signed sal_Int32
...this was a regression introduced with
c5eff79891ceb6f5d65b4a5b8c1b4a82a079ee54 "finish deprecation of
O(U)String::valueOf()" and it caused e.g. the value -0x100 of the enum member
css.i18n.TranliterationModules.IGNORE_MASK to be written as 4294967040 rather
than as -256.  (Though the relevant code is dead ugly, for sure.)

Change-Id: Icb3d3365135bc2a07e438317b70abdf9d74d6d7a

codemaker/source/codemaker/global.cxx
@@ -355,7 +355,7 @@ bool FileStream::write(void const * buffer, sal_uInt64 size) {
 
 FileStream &operator<<(FileStream& o, sal_uInt32 i) {
     sal_uInt64 writtenBytes;
-    OString s = OString::number(i);
+    OString s = OString::number((sal_Int32)i);
     osl_writeFile(o.m_file, s.getStr(), s.getLength() * sizeof(sal_Char), &writtenBytes);
     return o;
 }