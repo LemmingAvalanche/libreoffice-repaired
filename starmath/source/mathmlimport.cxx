LemmingAvalanche/libreoffice-repaired

commit 186a01c680644dedbf3c5303d16b5217aaa7efcf

String.AppendAscii() cleanup in math
Change-Id: I567e1a80ab75d6ebdaba52cb9afd47dbd52f39ce
Reviewed-on: https://gerrit.libreoffice.org/4933
Reviewed-by: Andras Timar <atimar@suse.com>
Tested-by: Andras Timar <atimar@suse.com>

starmath/source/mathmlimport.cxx
@@ -555,7 +555,7 @@ void SmXMLImport::endDocument(void)
             SmDocShell *pDocShell =
                 static_cast<SmDocShell*>(pModel->GetObjectShell());
             pDocShell->SetFormulaTree(pTree);
-            if (0 == aText.Len())  //If we picked up no annotation text
+            if (aText.isEmpty())  //If we picked up no annotation text
             {
                 // Get text from imported formula
                 pTree->CreateTextFromNode(aText);
@@ -1194,7 +1194,7 @@ void SmXMLAnnotationContext_Impl::StartElement(const uno::Reference<
 void SmXMLAnnotationContext_Impl::Characters(const OUString &rChars)
 {
     if (bIsStarMath)
-        GetSmImport().GetText().Append(String(rChars));
+        GetSmImport().GetText() + rChars;
 }
 
 ////////////////////////////////////////////////////////////

starmath/source/mathmlimport.hxx
@@ -83,7 +83,7 @@ class SmXMLImport : public SvXMLImport
 
         SmNodeStack aNodeStack;
         sal_Bool bSuccess;
-        String aText;
+        OUString aText;
 
 public:
     SmXMLImport(
@@ -256,7 +256,7 @@ public:
     }
 
     sal_Bool GetSuccess()           { return bSuccess; }
-    String &GetText()               { return aText; }
+    OUString &GetText()             { return aText; }
 
     virtual void SetViewSettings(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aViewProps);
     virtual void SetConfigurationSettings(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aViewProps);

starmath/inc/document.hxx
@@ -92,7 +92,7 @@ class SmDocShell : public SfxObjectShell, public SfxListener
     friend class SmModel;
     friend class SmCursor;
 
-    String              aText;
+    OUString            aText;
     SmFormat            aFormat;
     SmParser            aInterpreter;
     OUString            aAccText;
@@ -186,7 +186,7 @@ public:
 
     void        UpdateText();
     void        SetText(const OUString& rBuffer);
-    OUString    GetText() { return (OUString(aText)); }
+    OUString    GetText() { return (aText); }
     void        SetFormat(SmFormat& rFormat);
     const SmFormat&  GetFormat() { return (aFormat); }
 

starmath/inc/node.hxx
@@ -155,7 +155,7 @@ public:
     virtual void Move(const Point &rPosition);
     void MoveTo(const Point &rPosition) { Move(rPosition - GetTopLeft()); }
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    virtual void CreateTextFromNode(String &rText);
+    virtual void CreateTextFromNode(OUString &rText);
 
     virtual void    GetAccessibleText( OUStringBuffer &rText ) const;
     sal_Int32       GetAccessibleIndex() const { return nAccIndex; }
@@ -403,7 +403,7 @@ public:
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
 
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 
@@ -489,7 +489,7 @@ public:
 
     virtual void Prepare(const SmFormat &rFormat, const SmDocShell &rDocShell);
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    virtual void CreateTextFromNode(String &rText);
+    virtual void CreateTextFromNode(OUString &rText);
 
     virtual void  GetAccessibleText( OUStringBuffer &rText ) const;
     void Accept(SmVisitor* pVisitor);
@@ -575,7 +575,7 @@ public:
 
     virtual void Prepare(const SmFormat &rFormat, const SmDocShell &rDocShell);
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 
@@ -741,7 +741,7 @@ public:
     {}
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 
@@ -794,7 +794,7 @@ public:
     }
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 
     SmNode* Argument();
@@ -867,7 +867,7 @@ public:
     virtual SmNode * GetLeftMost();
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 
@@ -979,7 +979,7 @@ public:
     void SetSubSup(SmSubSup eSubSup, SmNode* pScript) { SetSubNode( 1 + eSubSup, pScript); }
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 
 };
@@ -1017,7 +1017,7 @@ public:
     const SmMathSymbolNode* ClosingBrace() const;
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 
@@ -1162,7 +1162,7 @@ public:
     {}
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 
     SmNode* Attribute();
@@ -1197,7 +1197,7 @@ public:
 
     virtual void Prepare(const SmFormat &rFormat, const SmDocShell &rDocShell);
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 
@@ -1229,7 +1229,7 @@ public:
     virtual SmNode * GetLeftMost();
 
     virtual void Arrange(const OutputDevice &rDev, const SmFormat &rFormat);
-    void CreateTextFromNode(String &rText);
+    void CreateTextFromNode(OUString &rText);
     void Accept(SmVisitor* pVisitor);
 };
 

starmath/qa/cppunit/test_nodetotextvisitors.cxx
@@ -519,11 +519,11 @@ void Test::ParseAndCompare(const char *formula1, const char *formula2, const cha
 
 void Test::testBinomInBinHor()
 {
-    String sInput, sExpected;
+    OUString sInput, sExpected;
     SmNode* pTree;
 
     // set up a binom (table) node
-    sInput.AppendAscii("binom a b + c");
+    sInput += "binom a b + c";
     pTree = SmParser().Parse(sInput);
     pTree->Prepare(xDocShRef->GetFormat(), *xDocShRef);
 
@@ -539,19 +539,19 @@ void Test::testBinomInBinHor()
     aCursor.InsertElement(PlusElement);
     aCursor.InsertText("d");
 
-    sExpected.AppendAscii(" { { binom a b + c } + d } ");
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Binom Node in BinHor Node", sExpected, String(xDocShRef->GetText()));
+    sExpected += " { { binom a b + c } + d } ";
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Binom Node in BinHor Node", sExpected, xDocShRef->GetText());
 
     delete pTree;
 }
 
 void Test::testBinVerInUnary()
 {
-    String sInput, sExpected;
+    OUString sInput, sExpected;
     SmNode* pTree;
 
     // set up a unary operator with operand
-    sInput.AppendAscii("- 1");
+    sInput += "- 1";
     pTree = SmParser().Parse(sInput);
     pTree->Prepare(xDocShRef->GetFormat(), *xDocShRef);
 
@@ -570,19 +570,19 @@ void Test::testBinVerInUnary()
     aCursor.Move(&aOutputDevice, MoveDown);
     aCursor.InsertText("2");
 
-    sExpected.AppendAscii(" - { 1 over 2 } ");
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Binary Vertical in Unary Operator", sExpected, String(xDocShRef->GetText()));
+    sExpected += " - { 1 over 2 } ";
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Binary Vertical in Unary Operator", sExpected, xDocShRef->GetText());
 
     delete pTree;
 }
 
 void Test::testBinHorInSubSup()
 {
-    String sInput, sExpected;
+    OUString sInput, sExpected;
     SmNode* pTree;
 
     // set up a blank formula
-    sInput.AppendAscii("");
+    sInput += "";
     pTree = SmParser().Parse(sInput);
     pTree->Prepare(xDocShRef->GetFormat(), *xDocShRef);
 
@@ -601,7 +601,7 @@ void Test::testBinHorInSubSup()
     aCursor.InsertElement(PlusElement);
     aCursor.InsertText("d");
 
-    sExpected.AppendAscii(" { a rsup { b + c } + d } ");
+    sExpected += " { a rsup { b + c } + d } ";
 //FIXME    CPPUNIT_ASSERT_EQUAL_MESSAGE("BinHor in SubSup", sExpected, xDocShRef->GetText());
 
     delete pTree;
@@ -609,11 +609,11 @@ void Test::testBinHorInSubSup()
 
 void Test::testUnaryInMixedNumberAsNumerator()
 {
-    String sInput, sExpected;
+    OUString sInput, sExpected;
     SmNode* pTree;
 
     // set up a unary operator
-    sInput.AppendAscii("- 1");
+    sInput += "- 1";
     pTree = SmParser().Parse(sInput);
     pTree->Prepare(xDocShRef->GetFormat(), *xDocShRef);
 
@@ -648,8 +648,8 @@ void Test::testUnaryInMixedNumberAsNumerator()
     aCursor.InsertElement(PlusElement);
     aCursor.InsertText("4");
 
-    sExpected.AppendAscii(" { 2 { - 1 over 2 } + 4 } ");
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Unary in mixed number as Numerator", sExpected, String(xDocShRef->GetText()));
+    sExpected += " { 2 { - 1 over 2 } + 4 } ";
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Unary in mixed number as Numerator", sExpected, xDocShRef->GetText());
 
     delete pTree;
 }

starmath/source/document.cxx
@@ -154,7 +154,7 @@ void SmDocShell::SetText(const OUString& rBuffer)
 {
     SAL_INFO( "starmath", "starmath: SmDocShell::SetText" );
 
-    if (rBuffer != OUString(aText))
+    if (rBuffer != aText)
     {
         bool bIsEnabled = IsEnableSetModified();
         if( bIsEnabled )
@@ -656,7 +656,7 @@ void SmDocShell::OnDocumentPrinterChanged( Printer *pPrt )
     SetFormulaArranged(false);
     Size aOldSize = GetVisArea().GetSize();
     Repaint();
-    if( aOldSize != GetVisArea().GetSize() && aText.Len() )
+    if( aOldSize != GetVisArea().GetSize() && !aText.isEmpty() )
         SetModified( true );
     pTmpPrinter = 0;
 }

starmath/source/mathtype.hxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
- #ifndef MATHTYPE_HXX
+#ifndef MATHTYPE_HXX
 #define MATHTYPE_HXX
 
 #include "node.hxx"
@@ -36,7 +36,7 @@ public:
     sal_uInt8 nStyle;
     MathTypeFont() : nTface(0),nStyle(0) {}
     MathTypeFont(sal_uInt8 nFace) : nTface(nFace),nStyle(0) {}
-    void AppendStyleToText(String &rS);
+    void AppendStyleToText(OUString &rS);
 };
 
 struct LessMathTypeFont
@@ -53,7 +53,7 @@ typedef ::std::set< MathTypeFont, LessMathTypeFont > MathTypeFontSet;
 class MathType
 {
 public:
-    MathType(String &rIn) :
+    MathType(OUString &rIn) :
         rRet(rIn), nHAlign(0), nVAlign(0), nDefaultSize(12),
         nLSize(0), nDSize(0), nCurSize(0), nLastSize(0), bIsSilent(sal_False),
         nTypeFace(0)
@@ -61,7 +61,7 @@ public:
         Init();
     }
 
-    MathType(String &rIn,SmNode *pIn) :
+    MathType(OUString &rIn,SmNode *pIn) :
         rRet(rIn), pTree(pIn), nHAlign(2), nVAlign(0), nInsertion(0), nDefaultSize(12),
         nLSize(0), nDSize(0), nCurSize(0), nLastSize(0), nSpec(0), bIsSilent(sal_False),
         nTypeFace(0)
@@ -94,10 +94,10 @@ private:
     void HandleMatrixSeparator(int nMatrixRows,int nMatrixCols,int &rCurCol,
         int &rCurRow);
     int HandleTemplate(int nLevel,sal_uInt8 &rSelector,sal_uInt8 &rVariation,
-        xub_StrLen &rLastTemplateBracket);
+        sal_Int32 &rLastTemplateBracket);
     void HandleEmblishments();
     void HandleSetSize();
-    int HandleChar(xub_StrLen &rTextStart,int &rSetSize,int nLevel,
+    int HandleChar(sal_Int32 &rTextStart,int &rSetSize,int nLevel,
         sal_uInt8 nTag,sal_uInt8 nSelector,sal_uInt8 nVariation,
         sal_Bool bSilent);
     void HandleNudge();
@@ -126,9 +126,9 @@ private:
     void HandleMath(SmNode *pNode,int nLevel);
     void HandleText(SmNode *pNode,int nLevel);
     void HandleAttributes(SmNode *pNode,int nLevel);
-    void TypeFaceToString(String &rRet,sal_uInt8 nFace);
+    void TypeFaceToString(OUString &rRet,sal_uInt8 nFace);
 
-    String &rRet;
+    OUString &rRet;
     SmNode *pTree;
 
     sal_uInt8 nHAlign;
@@ -144,10 +144,10 @@ private:
     sal_Int16 nCurSize;
     sal_Int16 nLastSize;
     sal_uInt8 nSpec;
-    sal_Bool bIsSilent, bIsReInterpBrace;
-    String sPost;
-    xub_StrLen nPostSup;
-    xub_StrLen nPostlSup;
+    sal_Bool  bIsSilent, bIsReInterpBrace;
+    OUString  sPost;
+    sal_Int32 nPostSup;
+    sal_Int32 nPostlSup;
     sal_uInt8 nTypeFace;
     MathTypeFontSet aUserStyles;
 
@@ -160,7 +160,7 @@ private:
         tmDSINT,tmTSINT,tmUHBRACE,tmLHBRACE,tmSUM
     };
 public:
-    static sal_Bool LookupChar(sal_Unicode nChar,String &rRet,
+    static sal_Bool LookupChar(sal_Unicode nChar,OUString &rRet,
         sal_uInt8 nVersion=3,sal_uInt8 nTypeFace=0);
 };
 

commit 1f625fca16cd85b4208db7a8dba6656357eaecbe

fdo#68956: fix string conversion
Added the SetText method to make it absolutely clear.

Regression from 186a01c680644dedbf3c5303d16b5217aaa7efcf

Change-Id: I4e9935fd13fca8f9cfdfda65c980d08b1e85b459
Reviewed-on: https://gerrit.libreoffice.org/5819
Reviewed-by: Marcos Souza <marcos.souza.org@gmail.com>
Reviewed-by: Thomas Arnhold <thomas@arnhold.org>
Tested-by: Thomas Arnhold <thomas@arnhold.org>

starmath/source/mathmlimport.cxx
@@ -1192,7 +1192,7 @@ void SmXMLAnnotationContext_Impl::StartElement(const uno::Reference<
 void SmXMLAnnotationContext_Impl::Characters(const OUString &rChars)
 {
     if (bIsStarMath)
-        GetSmImport().GetText() + rChars;
+        GetSmImport().SetText( GetSmImport().GetText() + rChars );
 }
 
 ////////////////////////////////////////////////////////////

starmath/source/mathmlimport.hxx
@@ -255,8 +255,9 @@ public:
         return result;
     }
 
-    sal_Bool GetSuccess()           { return bSuccess; }
-    OUString &GetText()             { return aText; }
+    sal_Bool GetSuccess()              { return bSuccess; }
+    OUString GetText()                 { return aText; }
+    void SetText(const OUString &rStr) { aText = rStr; }
 
     virtual void SetViewSettings(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aViewProps);
     virtual void SetConfigurationSettings(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aViewProps);