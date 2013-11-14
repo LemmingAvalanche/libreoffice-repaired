LemmingAvalanche/libreoffice-repaired

commit 710f361a8e899b9fc59cb9435065f2f2ffe5c7b2

make l10n buildable separately
- this renames the 'almost' module target to non-l10n
- and adds a l10n target which is intended to only build l10n parts of
  the product
- packagers should then be able to build l10n and non-l10n parts of the
  product independently, thus:
   - enable quicker rebuilds
   - distribution of load
   - updates to l10n without a full rebuild
   - security fixes to binaries without rebuilding all l10n
- the new targets are called build-l10n-only and build-non-l10n-only
- note this is not intended to move a concept of split packages
  upstream -- while this exsists in distros, the number of test
  scenarios for this would explode upstream

Change-Id: Ib8ccc9bc52718d9b0ebbfee76ad93dc29c260863

Conflicts:
	filter/Module_filter.mk

solenv/gbuild/Module.mk
@@ -40,6 +40,7 @@
 gb_Module_ALLMODULES :=
 gb_Module_MODULELOCATIONS :=
 gb_Module_TARGETSTACK :=
+gb_Module_L10NTARGETSTACK :=
 gb_Module_CHECKTARGETSTACK :=
 gb_Module_SLOWCHECKTARGETSTACK :=
 gb_Module_SUBSEQUENTCHECKTARGETSTACK :=
@@ -53,8 +54,8 @@ gb_Module_CLEANTARGETSTACK :=
 # on $(MAKEFILE_LIST).
 gb_Module_CURRENTMAKEFILE :=
 
-$(call gb_Module_get_almost_target,%) :
-	$(call gb_Output_announce,$*,$(true),ALM,5)
+$(call gb_Module_get_nonl10n_target,%) :
+	$(call gb_Output_announce,$*,$(true),BIN,5)
 	-$(call gb_Helper_abbreviate_dirs,\
 		mkdir -p $(dir $@) && \
 		touch $@)
@@ -64,7 +65,14 @@ $(call gb_Module_get_clean_target,%) :
 	$(call gb_Output_announce,$*,$(false),MOD,5)
 	$(call gb_Output_announce_title,module $* cleared.)
 	-$(call gb_Helper_abbreviate_dirs,\
-		rm -f $(call gb_Module_get_target,$*) $(call gb_Module_get_almost_target,$*) $(call gb_Module_get_check_target,$*) $(call gb_Module_get_slowcheck_target,$*) $(call gb_Module_get_subsequentcheck_target,$*))
+		rm -f $(call gb_Module_get_target,$*) $(call gb_Module_get_nonl10n_target,$*) $(call gb_Module_get_l10n_target,$*) $(call gb_Module_get_check_target,$*) $(call gb_Module_get_slowcheck_target,$*) $(call gb_Module_get_subsequentcheck_target,$*)
+
+$(call gb_Module_get_l10n_target,%) :
+	$(call gb_Output_announce,$*,$(true),LOC,5)
+	$(call gb_Output_announce_title,module $* done.)
+	-$(call gb_Helper_abbreviate_dirs,\
+		mkdir -p $(dir $@) && \
+		touch $@)
 
 $(call gb_Module_get_check_target,%) :
 	$(call gb_Output_announce,$*,$(true),CHK,5)
@@ -94,7 +102,7 @@ $(call gb_Module_get_target,%) :
 		mkdir -p $(dir $@) && \
 		touch $@)
 
-.PHONY : all build unitcheck slowcheck subsequentcheck clean check debugrun help showmodules translations
+.PHONY : all build build-l10n-only build-non-l10n-only unitcheck slowcheck subsequentcheck clean check debugrun help showmodules translations
 .DEFAULT_GOAL := all
 
 all : build $(if $(CROSS_COMPILING),,unitcheck $(if $(gb_PARTIAL_BUILD),,slowcheck))
@@ -108,6 +116,18 @@ build :
 	$(call gb_Output_announce,top level modules: $(foreach module,$(filter-out deliverlog $(WORKDIR)/bootstrap,$^),$(notdir $(module))),$(true),ALL,6)
 	$(call gb_Output_announce,loaded modules: $(sort $(gb_Module_ALLMODULES)),$(true),ALL,6)
 	$(call gb_Output_announce_title,build done.)
+	$(call gb_Output_announce_bell)
+
+build-l10n-only :
+	$(call gb_Output_announce,top level modules: $(foreach module,$(filter-out deliverlog $(WORKDIR)/bootstrap,$^),$(notdir $(module))),$(true),LOC,6)
+	$(call gb_Output_announce,loaded modules: $(sort $(gb_Module_ALLMODULES)),$(true),LOC,6)
+	$(call gb_Output_announce_title,l10n done.)
+	$(call gb_Output_announce_bell)
+
+build-non-l10n-only :
+	$(call gb_Output_announce,top level modules: $(foreach module,$(filter-out deliverlog $(WORKDIR)/bootstrap,$^),$(notdir $(module))),$(true),NLC,6)
+	$(call gb_Output_announce,loaded modules: $(sort $(gb_Module_ALLMODULES)),$(true),BIN,6)
+	$(call gb_Output_announce_title,non-l10n done.)
 	$(call gb_Output_announce_bell)
 
 unitcheck :
@@ -167,6 +187,7 @@ define gb_Module_Module
 gb_Module_ALLMODULES += $(1)
 gb_Module_MODULELOCATIONS += $(1):$(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 gb_Module_TARGETSTACK := $(call gb_Module_get_target,$(1)) $(gb_Module_TARGETSTACK)
+gb_Module_L10NTARGETSTACK := $(call gb_Module_get_l10n_target,$(1)) $(gb_Module_L10NTARGETSTACK)
 gb_Module_CHECKTARGETSTACK := $(call gb_Module_get_check_target,$(1)) $(gb_Module_CHECKTARGETSTACK)
 gb_Module_SLOWCHECKTARGETSTACK := $(call gb_Module_get_slowcheck_target,$(1)) $(gb_Module_SLOWCHECKTARGETSTACK)
 gb_Module_SUBSEQUENTCHECKTARGETSTACK := $(call gb_Module_get_subsequentcheck_target,$(1)) $(gb_Module_SUBSEQUENTCHECKTARGETSTACK)
@@ -206,9 +227,19 @@ endif
 endef
 
 define gb_Module_add_target
+$(if $(filter AllLang% Dictionary%,$(2)),$(warning target $(2) should be a l10n target))
 $(call gb_Module__read_targetfile,$(1),$(2),target)
 
-$(call gb_Module_get_almost_target,$(1)) : $$(gb_Module_CURRENTTARGET)
+$(call gb_Module_get_nonl10n_target,$(1)) : $$(gb_Module_CURRENTTARGET)
+$(call gb_Module_get_clean_target,$(1)) : $$(gb_Module_CURRENTCLEANTARGET)
+
+endef
+
+define gb_Module_add_l10n_target
+$(if $(filter AllLang% Dictionary%,$(2)),,$(warning target $(2) should not be a l10n target))
+$(call gb_Module__read_targetfile,$(1),$(2),target)
+
+$(call gb_Module_get_l10n_target,$(1)) : $$(gb_Module_CURRENTTARGET)
 $(call gb_Module_get_clean_target,$(1)) : $$(gb_Module_CURRENTCLEANTARGET)
 
 endef
@@ -244,11 +275,13 @@ endef
 define gb_Module_add_moduledir
 include $(patsubst $(1):%,%,$(filter $(1):%,$(gb_Module_MODULELOCATIONS)))/$(2)/Module_$(2).mk
 $(call gb_Module_get_target,$(1)) : $$(firstword $$(gb_Module_TARGETSTACK))
+$(call gb_Module_get_l10n_target,$(1)) : $$(firstword $$(gb_Module_L10NTARGETSTACK))
 $(call gb_Module_get_check_target,$(1)) : $$(firstword $$(gb_Module_CHECKTARGETSTACK))
 $(call gb_Module_get_slowcheck_target,$(1)) : $$(firstword $$(gb_Module_SLOWCHECKTARGETSTACK))
 $(call gb_Module_get_subsequentcheck_target,$(1)) : $$(firstword $$(gb_Module_SUBSEQUENTCHECKTARGETSTACK))
 $(call gb_Module_get_clean_target,$(1)) : $$(firstword $$(gb_Module_CLEANTARGETSTACK))
 gb_Module_TARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_TARGETSTACK)),$$(gb_Module_TARGETSTACK))
+gb_Module_L10NTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_L10NTARGETSTACK)),$$(gb_Module_L10NTARGETSTACK))
 gb_Module_CHECKTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_CHECKTARGETSTACK)),$$(gb_Module_CHECKTARGETSTACK))
 gb_Module_SLOWCHECKTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_SLOWCHECKTARGETSTACK)),$$(gb_Module_SLOWCHECKTARGETSTACK))
 gb_Module_SUBSEQUENTCHECKTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_SUBSEQUENTCHECKTARGETSTACK)),$$(gb_Module_SUBSEQUENTCHECKTARGETSTACK))
@@ -257,8 +290,14 @@ gb_Module_CLEANTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_CLEANTARGETSTA
 endef
 
 define gb_Module_add_targets
-$(call gb_Module_get_target,$(1)) : $(call gb_Module_get_almost_target,$(1))
+$(call gb_Module_get_target,$(1)) : $(call gb_Module_get_nonl10n_target,$(1))
 $(foreach target,$(2),$(call gb_Module_add_target,$(1),$(target)))
+
+endef
+
+define gb_Module_add_l10n_targets
+$(call gb_Module_get_target,$(1)) : $(call gb_Module_get_l10n_target,$(1))
+$(foreach target,$(2),$(call gb_Module_add_l10n_target,$(1),$(target)))
 
 endef
 
@@ -291,7 +330,9 @@ endif
 
 include $(1)
 
-build : $$(firstword $$(gb_Module_TARGETSTACK))
+build : build-non-l10n-only build-l10n-only
+build-non-l10n-only : $$(firstword $$(gb_Module_TARGETSTACK))
+build-l10n-only : $$(firstword $$(gb_Module_L10NTARGETSTACK))
 unitcheck : $$(firstword $$(gb_Module_CHECKTARGETSTACK))
 slowcheck : $$(firstword $$(gb_Module_SLOWCHECKTARGETSTACK))
 subsequentcheck : $$(firstword $$(gb_Module_SUBSEQUENTCHECKTARGETSTACK))
@@ -302,12 +343,13 @@ $$(eval $$(call gb_Output_error,Corrupted module target stack! $(gb_Module_TARGE
 endif
 
 gb_Module_TARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_TARGETSTACK)),$$(gb_Module_TARGETSTACK))
+gb_Module_L10NTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_L10NTARGETSTACK)),$$(gb_Module_L10NTARGETSTACK))
 gb_Module_CHECKTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_CHECKTARGETSTACK)),$$(gb_Module_CHECKTARGETSTACK))
 gb_Module_SLOWCHECKTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_SLOWCHECKTARGETSTACK)),$$(gb_Module_SLOWCHECKTARGETSTACK))
 gb_Module_SUBSEQUENTCHECKTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_SUBSEQUENTCHECKTARGETSTACK)),$$(gb_Module_SUBSEQUENTCHECKTARGETSTACK))
 gb_Module_CLEANTARGETSTACK := $$(wordlist 2,$$(words $$(gb_Module_CLEANTARGETSTACK)),$$(gb_Module_CLEANTARGETSTACK))
 
-ifneq ($$(and $$(gb_Module_TARGETSTACK),$$(gb_Module_CHECKTARGETSTACK),$$(gb_Module_SLOWCHECKTARGETSTACK),$$(gb_Module_SUBSEQUENTCHECKTARGETSTACK)),)
+ifneq ($$(and $$(gb_Module_TARGETSTACK),$$(gb_Module_CHECKTARGETSTACK),$$(gb_Module_SLOWCHECKTARGETSTACK),$$(gb_Module_SUBSEQUENTCHECKTARGETSTACK),$$(gb_Module_L10NTARGETSTACK)),)
 $$(eval $$(call gb_Output_error,Corrupted module target stack!3))
 endif
 

Makefile.in
@@ -7,7 +7,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-.PHONY : all bootstrap build check clean clean-build clean-host test-install distclean distro-pack-install docs download fetch findunusedcode get-submodules id install install-strip subsequentcheck tags
+.PHONY : all bootstrap build build-non-l10n-only build-l10n-only check clean clean-build clean-host test-install distclean distro-pack-install docs download fetch findunusedcode get-submodules id install install-strip subsequentcheck tags
 
 ifeq ($(MAKECMDGOALS),)
 MAKECMDGOALS:=all
@@ -245,6 +245,9 @@ endif
 
 build-nocheck: bootstrap fetch $(if $(CROSS_COMPILING),cross-toolset)
 	$(GNUMAKE) -j $(PARALLELISM) $(GMAKE_OPTIONS) -f $(SRCDIR)/Makefile.gbuild build
+
+build-l10n-only build-non-l10n-only: bootstrap fetch $(if $(CROSS_COMPILING),cross-toolset)
+	$(GNUMAKE) -j $(PARALLELISM) $(GMAKE_OPTIONS) -f $(SRCDIR)/Makefile.gbuild $@
 
 cross-toolset: bootstrap fetch
 	$(GNUMAKE) gb_Side=build -j $(PARALLELISM) $(GMAKE_OPTIONS) -f $(SRCDIR)/Makefile.gbuild build-tools

accessibility/Module_accessibility.mk
@@ -10,8 +10,11 @@
 $(eval $(call gb_Module_Module,accessibility))
 
 $(eval $(call gb_Module_add_targets,accessibility,\
-    AllLangResTarget_acc \
     Library_acc \
+))
+
+$(eval $(call gb_Module_add_l10n_targets,accessibility,\
+    AllLangResTarget_acc \
 ))
 
 ifneq ($(ENABLE_JAVA),)

avmedia/Module_avmedia.mk
@@ -10,8 +10,11 @@
 $(eval $(call gb_Module_Module,avmedia))
 
 $(eval $(call gb_Module_add_targets,avmedia,\
-	AllLangResTarget_avmedia \
 	Library_avmedia \
+))
+
+$(eval $(call gb_Module_add_l10n_targets,avmedia,\
+	AllLangResTarget_avmedia \
 ))
 
 ifeq ($(ENABLE_GSTREAMER),TRUE)

basctl/Module_basctl.mk
@@ -22,8 +22,11 @@ $(eval $(call gb_Module_Module,basctl))
 ifneq ($(DISABLE_SCRIPTING),TRUE)
 
 $(eval $(call gb_Module_add_targets,basctl,\
-	AllLangResTarget_basctl \
 	Library_basctl \
+))
+
+$(eval $(call gb_Module_add_l10n_targets,basctl,\
+	AllLangResTarget_basctl \
 ))
 
 endif

basic/Module_basic.mk
@@ -11,7 +11,7 @@ $(eval $(call gb_Module_Module,basic))
 
 ifneq ($(DISABLE_SCRIPTING),TRUE)
 
-$(eval $(call gb_Module_add_targets,basic,\
+$(eval $(call gb_Module_add_l10n_targets,basic,\
 	AllLangResTarget_sb \
 ))
 

chart2/Module_chart2.mk
@@ -10,10 +10,13 @@
 $(eval $(call gb_Module_Module,chart2))
 
 $(eval $(call gb_Module_add_targets,chart2,\
-    AllLangResTarget_chartcontroller \
     Library_chartcontroller \
     Library_chartcore \
 	UIConfig_chart2 \
+))
+
+$(eval $(call gb_Module_add_l10n_targets,chart2,\
+    AllLangResTarget_chartcontroller \
 ))
 
 $(eval $(call gb_Module_add_slowcheck_targets,chart2,\

connectivity/Module_connectivity.mk
@@ -11,9 +11,6 @@
 $(eval $(call gb_Module_Module,connectivity))
 
 $(eval $(call gb_Module_add_targets,connectivity,\
-	AllLangResTarget_cnr \
-	AllLangResTarget_sdbcl \
-	AllLangResTarget_sdberr \
 	Configuration_calc \
 	Configuration_dbase \
 	Configuration_flat \
@@ -30,6 +27,12 @@ $(eval $(call gb_Module_add_targets,connectivity,\
 		Library_odbc \
 		Library_odbcbase) \
 	Library_sdbc2 \
+))
+
+$(eval $(call gb_Module_add_l10n_targets,connectivity,\
+	AllLangResTarget_cnr \
+	AllLangResTarget_sdbcl \
+	AllLangResTarget_sdberr \
 ))
 
 ifneq ($(ENABLE_JAVA),)

commit 1ea9380790e520aba7f0c9abd4a4d5b02297726b

gbuild: fix typo in Module clean rule
regression from 710f361a8e899b9fc59cb9435065f2f2ffe5c7b2

Change-Id: I623ec25a0666cd3cac1d4f6371e6460a25faa6c4

solenv/gbuild/Module.mk
@@ -65,7 +65,7 @@ $(call gb_Module_get_clean_target,%) :
 	$(call gb_Output_announce,$*,$(false),MOD,5)
 	$(call gb_Output_announce_title,module $* cleared.)
 	-$(call gb_Helper_abbreviate_dirs,\
-		rm -f $(call gb_Module_get_target,$*) $(call gb_Module_get_nonl10n_target,$*) $(call gb_Module_get_l10n_target,$*) $(call gb_Module_get_check_target,$*) $(call gb_Module_get_slowcheck_target,$*) $(call gb_Module_get_subsequentcheck_target,$*)
+		rm -f $(call gb_Module_get_target,$*) $(call gb_Module_get_nonl10n_target,$*) $(call gb_Module_get_l10n_target,$*) $(call gb_Module_get_check_target,$*) $(call gb_Module_get_slowcheck_target,$*) $(call gb_Module_get_subsequentcheck_target,$*))
 
 $(call gb_Module_get_l10n_target,%) :
 	$(call gb_Output_announce,$*,$(true),LOC,5)