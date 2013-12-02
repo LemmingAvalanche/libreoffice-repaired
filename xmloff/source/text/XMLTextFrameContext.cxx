LemmingAvalanche/libreoffice-repaired

commit 188b70c50d203f050e5304ebc5f1ed1242f735bf

fdo#68839: ODF import: fix name of multi-image frame
When multiple image child elements are inside a frame, each one is
imported and gets unique name via SwDoc::SetFlyName().  But the
retained one is not necessarily the first one, which is the only one
that may have the original name.

Also the solveMultipleImages needs to return a smart pointer, as nothing
else keeps the image contexts alive.

(regression from 0ff25c82f7def37c5989c867279830f1a89fa047)

Change-Id: I28a8a752f3eed176cc2ebb4c9af11a0dd4d18ea6

xmloff/source/text/XMLTextFrameContext.cxx
@@ -364,6 +364,7 @@ class XMLTextFrameContext_Impl : public SvXMLImportContext
     const OUString sTextBoxServiceName;
     const OUString sGraphicServiceName;
 
+    OUString m_sOrigName;
     OUString sName;
     OUString sStyleName;
     OUString sNextName;
@@ -440,6 +441,8 @@ public:
     void SetTitle( const OUString& rTitle );
 
     void SetDesc( const OUString& rDesc );
+
+    void SetName();
 
     ::com::sun::star::text::TextContentAnchorType GetAnchorType() const { return eAnchorType; }
 
@@ -893,6 +896,7 @@ XMLTextFrameContext_Impl::XMLTextFrameContext_Impl(
             sStyleName = rValue;
             break;
         case XML_TOK_TEXT_FRAME_NAME:
+            m_sOrigName = rValue;
             sName = rValue;
             break;
         case XML_TOK_TEXT_FRAME_FRAME_NAME:
@@ -1276,6 +1280,19 @@ void XMLTextFrameContext_Impl::SetHyperlink( const OUString& rHRef,
     }
 }
 
+void XMLTextFrameContext_Impl::SetName()
+{
+    Reference<XNamed> xNamed(xPropSet, UNO_QUERY);
+    if (xNamed.is())
+    {
+        OUString const name(xNamed->getName());
+        if (name != m_sOrigName)
+        {
+            xNamed->setName(m_sOrigName);
+        }
+    }
+}
+
 // Implement Title/Description Elements UI (#i73249#)
 void XMLTextFrameContext_Impl::SetTitle( const OUString& rTitle )
 {
@@ -1383,13 +1400,22 @@ XMLTextFrameContext::~XMLTextFrameContext()
 void XMLTextFrameContext::EndElement()
 {
     /// solve if multiple image child contexts were imported
-    solveMultipleImages();
+    SvXMLImportContextRef const pMultiContext(solveMultipleImages());
 
-    SvXMLImportContext *pContext = &m_xImplContext;
+    SvXMLImportContext const*const pContext =
+        (pMultiContext) ? &pMultiContext : &m_xImplContext;
     XMLTextFrameContext_Impl *pImpl = PTR_CAST( XMLTextFrameContext_Impl, pContext );
+    assert(!pMultiContext || pImpl);
     if( pImpl )
     {
         pImpl->CreateIfNotThere();
+
+        // fdo#68839: in case the surviving image was not the first one,
+        // it will have a counter added to its name - set the original name
+        if (pMultiContext) // do this only when necessary; esp. not for text
+        {                  // frames that may have entries in GetRenameMap()!
+            pImpl->SetName();
+        }
 
         if( !m_sTitle.isEmpty() )
         {

include/xmloff/xmlmultiimagehelper.hxx
@@ -44,7 +44,7 @@ public:
     /// see imlementation for evtl. changing weights and/or adding filetypes.
     ///
     /// @returns import context of the selected image
-    const SvXMLImportContext* solveMultipleImages();
+    SvXMLImportContextRef solveMultipleImages();
 
     /// add a content to the remembered image import contexts
     void addContent(const SvXMLImportContext& rSvXMLImportContext);

xmloff/source/core/xmlmultiimagehelper.cxx
@@ -83,9 +83,9 @@ MultiImageImportHelper::~MultiImageImportHelper()
     }
 }
 
-const SvXMLImportContext* MultiImageImportHelper::solveMultipleImages()
+SvXMLImportContextRef MultiImageImportHelper::solveMultipleImages()
 {
-    const SvXMLImportContext* pContext(0);
+    SvXMLImportContextRef pContext;
     if(maImplContextVector.size() > 1)
     {
         // multiple child contexts were imported, decide which is the most valuable one

xmloff/source/draw/ximpshap.cxx
@@ -3426,8 +3426,9 @@ void SdXMLFrameShapeContext::StartElement(const uno::Reference< xml::sax::XAttri
 void SdXMLFrameShapeContext::EndElement()
 {
     // solve if multiple image child contexts were imported
-    const SvXMLImportContext* const pSelectedContext(solveMultipleImages());
-    const SdXMLGraphicObjectShapeContext* pShapeContext( dynamic_cast<const SdXMLGraphicObjectShapeContext*>( pSelectedContext ) );
+    SvXMLImportContextRef const pSelectedContext(solveMultipleImages());
+    const SdXMLGraphicObjectShapeContext* pShapeContext(
+        dynamic_cast<const SdXMLGraphicObjectShapeContext*>(&pSelectedContext));
     if ( pShapeContext && !maShapeId.isEmpty() )
     {
         // fdo#64512 and fdo#60075 - make sure *this* shape is

commit e2d1a0c8695671f9eceffbfa2d5adf887e868fd8

fdo#71450 fdo#71698: ODF import: fix frame name corner cases
Trying to set a name that is already in use will throw an exception (and
set a different, generated name); if there is actually no name in the
file then there's no point trying to set anything.

(regression from 188b70c50d203f050e5304ebc5f1ed1242f735bf)

Change-Id: Ie54d4a830cc23e2853a6efeb81f77dcc788192ea

xmloff/source/text/XMLTextFrameContext.cxx
@@ -1307,12 +1307,20 @@ void XMLTextFrameContext_Impl::SetHyperlink( const OUString& rHRef,
 void XMLTextFrameContext_Impl::SetName()
 {
     Reference<XNamed> xNamed(xPropSet, UNO_QUERY);
-    if (xNamed.is())
+    if (!m_sOrigName.isEmpty() && xNamed.is())
     {
         OUString const name(xNamed->getName());
         if (name != m_sOrigName)
         {
-            xNamed->setName(m_sOrigName);
+            try
+            {
+                xNamed->setName(m_sOrigName);
+            }
+            catch (uno::Exception const& e)
+            {   // fdo#71698 document contains 2 frames with same draw:name
+                SAL_INFO("xmloff.text", "SetName(): exception setting \""
+                        << m_sOrigName << "\": " << e.Message);
+            }
         }
     }
 }