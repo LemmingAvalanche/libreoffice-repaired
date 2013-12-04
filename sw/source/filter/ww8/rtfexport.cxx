LemmingAvalanche/libreoffice-repaired

commit 07990deb3f9f2b44c7de55a9f1ed098e3046a44c

convert sw/source/filter/ww8/*.cxx from String to OUString
Change-Id: Ibdd454008244b9a6db5037f511ac2da723648d89

sw/source/filter/ww8/rtfexport.cxx
@@ -1165,17 +1165,17 @@ class SwRTFWriter : public Writer
         bool m_bOutOutlineOnly;
 
     public:
-        SwRTFWriter( const String& rFilterName, const String& rBaseURL );
+        SwRTFWriter( const OUString& rFilterName, const OUString& rBaseURL );
         virtual ~SwRTFWriter();
                virtual sal_uLong WriteStream();
 };
 
-SwRTFWriter::SwRTFWriter( const String& rFltName, const String & rBaseURL )
+SwRTFWriter::SwRTFWriter( const OUString& rFltName, const OUString & rBaseURL )
 {
     SAL_INFO("sw.rtf", OSL_THIS_FUNC);
     SetBaseURL( rBaseURL );
     // export outline nodes, only (send outline to clipboard/presentation)
-    m_bOutOutlineOnly = 'O' == rFltName.GetChar( 0 );
+    m_bOutOutlineOnly = 'O' == rFltName[0];
 }
 
 SwRTFWriter::~SwRTFWriter()
@@ -1190,7 +1190,7 @@ sal_uLong SwRTFWriter::WriteStream()
     return 0;
 }
 
-extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL ExportRTF( const String& rFltName, const String& rBaseURL, WriterRef& xRet )
+extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL ExportRTF( const OUString& rFltName, const OUString& rBaseURL, WriterRef& xRet )
 {
     SAL_INFO("sw.rtf", OSL_THIS_FUNC);
     xRet = new SwRTFWriter( rFltName, rBaseURL );

sw/source/filter/basflt/fltini.cxx
@@ -673,8 +673,8 @@ extern "C" {
     Reader *ImportRTF();
     void ExportRTF( const String&, const String& rBaseURL, WriterRef& );
     Reader *ImportDOC();
-    void ExportDOC( const String&, const String& rBaseURL, WriterRef& );
-    sal_uLong SaveOrDelMSVBAStorage_ww8( SfxObjectShell&, SotStorage&, sal_Bool, const String& );
+    void ExportDOC( const OUString&, const OUString& rBaseURL, WriterRef& );
+    sal_uLong SaveOrDelMSVBAStorage_ww8( SfxObjectShell&, SotStorage&, sal_Bool, const OUString& );
     sal_uLong GetSaveWarningOfMSVBAStorage_ww8( SfxObjectShell& );
 }
 

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -1014,7 +1014,7 @@ void DocxAttributeOutput::EndField_Impl( FieldInfos& rInfos )
             rInfos.sCmd += "\" ";
 
             // Clean the field bookmark data to avoid infinite loop
-            m_sFieldBkm = String( );
+            m_sFieldBkm = OUString( );
 
             // Write the end of the field
             EndField_Impl( rInfos );
@@ -1449,7 +1449,7 @@ void DocxAttributeOutput::Redline( const SwRedlineData* pRedline)
         return;
 
     OString aId( OString::number( pRedline->GetSeqNo() ) );
-    const String &rAuthor( SW_MOD()->GetRedlineAuthor( pRedline->GetAuthor() ) );
+    const OUString &rAuthor( SW_MOD()->GetRedlineAuthor( pRedline->GetAuthor() ) );
     OString aAuthor( OUStringToOString( rAuthor, RTL_TEXTENCODING_UTF8 ) );
     OString aDate( msfilter::util::DateTimeToOString( pRedline->GetTimeStamp() ) );
 
@@ -1509,7 +1509,7 @@ void DocxAttributeOutput::StartRedline()
 
     OString aId( OString::number( m_nRedlineId++ ) );
 
-    const String &rAuthor( SW_MOD()->GetRedlineAuthor( pRedlineData->GetAuthor() ) );
+    const OUString &rAuthor( SW_MOD()->GetRedlineAuthor( pRedlineData->GetAuthor() ) );
     OString aAuthor( OUStringToOString( rAuthor, RTL_TEXTENCODING_UTF8 ) );
 
     OString aDate( msfilter::util::DateTimeToOString( pRedlineData->GetTimeStamp() ) );
@@ -3549,9 +3549,9 @@ void DocxAttributeOutput::WriteOutliner(const OutlinerParaObject& rParaObj)
         if( n )
             aAttrIter.NextPara( n );
 
-        String aStr( rEditObj.GetText( n ));
+        OUString aStr( rEditObj.GetText( n ));
         xub_StrLen nAktPos = 0;
-        xub_StrLen nEnd = aStr.Len();
+        xub_StrLen nEnd = aStr.getLength();
 
         m_pSerializer->startElementNS( XML_w, XML_p, FSEND );
 
@@ -3575,7 +3575,7 @@ void DocxAttributeOutput::WriteOutliner(const OutlinerParaObject& rParaObj)
             bool bTxtAtr = aAttrIter.IsTxtAttr( nAktPos );
             if( !bTxtAtr )
             {
-                String aOut( aStr.Copy( nAktPos, nNextAttr - nAktPos ) );
+                OUString aOut( aStr.copy( nAktPos, nNextAttr - nAktPos ) );
                 RunText(aOut);
             }
 
@@ -4815,7 +4815,7 @@ bool DocxAttributeOutput::DropdownField( const SwField* pFld )
     bool bExpand = false;
 
     ww::eField eType = ww::eFORMDROPDOWN;
-    String sCmd = FieldString( eType  );
+    OUString sCmd = FieldString( eType  );
     GetExport( ).OutputField( pFld, eType, sCmd );
 
     return bExpand;
@@ -4860,7 +4860,7 @@ void DocxAttributeOutput::SetField( const SwField& rFld, ww::eField eType, const
 void DocxAttributeOutput::WriteExpand( const SwField* pFld )
 {
     // Will be written in the next End Run
-    String sCmd;
+    OUString sCmd;
     m_rExport.OutputField( pFld, ww::eUNKNOWN, sCmd );
 }
 

sw/source/filter/ww8/docxexport.cxx
@@ -868,9 +868,9 @@ void DocxExport::WriteOutliner(const OutlinerParaObject& rParaObj, sal_uInt8 nTy
 
         AttrOutput().StartParagraph( ww8::WW8TableNodeInfo::Pointer_t());
         rtl_TextEncoding eChrSet = aAttrIter.GetNodeCharSet();
-        String aStr( rEditObj.GetText( n ));
+        OUString aStr( rEditObj.GetText( n ));
         xub_StrLen nAktPos = 0;
-        xub_StrLen nEnd = aStr.Len();
+        xub_StrLen nEnd = aStr.getLength();
         do {
             AttrOutput().StartRun( NULL );
             xub_StrLen nNextAttr = aAttrIter.WhereNext();
@@ -882,11 +882,11 @@ void DocxExport::WriteOutliner(const OutlinerParaObject& rParaObj, sal_uInt8 nTy
             bool bTxtAtr = aAttrIter.IsTxtAttr( nAktPos );
             if( !bTxtAtr )
             {
-                if( nAktPos == 0 && nNextAttr - nAktPos == aStr.Len())
+                if( nAktPos == 0 && nNextAttr - nAktPos == aStr.getLength())
                     AttrOutput().RunText( aStr, eChrSet );
                 else
                 {
-                    String tmp( aStr.Copy( nAktPos, nNextAttr - nAktPos ));
+                    OUString tmp( aStr.copy( nAktPos, nNextAttr - nAktPos ));
                     AttrOutput().RunText( tmp, eChrSet );
                 }
             }

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -466,8 +466,8 @@ bool RtfAttributeOutput::StartURL( const OUString& rUrl, const OUString& rTarget
     m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FLDINST);
     m_aStyles.append(" HYPERLINK ");
 
-    String sURL( rUrl );
-    if( sURL.Len() )
+    OUString sURL( rUrl );
+    if( !sURL.isEmpty() )
     {
         m_aStyles.append("\"");
         m_aStyles.append(msfilter::rtfutil::OutString( sURL, m_rExport.eCurrentEncoding));
@@ -2606,13 +2606,13 @@ void RtfAttributeOutput::ParaNumRule_Impl( const SwTxtNode* pTxtNd, sal_Int32 nL
             m_aStyles.append(*pString);
 
         {
-            String sTxt;
+            OUString sTxt;
             if( SVX_NUM_CHAR_SPECIAL == pFmt->GetNumberingType() || SVX_NUM_BITMAP == pFmt->GetNumberingType() )
-                sTxt = pFmt->GetBulletChar();
+                sTxt = OUString(pFmt->GetBulletChar());
             else
                 sTxt = pTxtNd->GetNumString();
 
-            if (sTxt.Len())
+            if (!sTxt.isEmpty())
             {
                 m_aStyles.append(' ');
                 m_aStyles.append(msfilter::rtfutil::OutString(sTxt, m_rExport.eDefaultEncoding));
@@ -2620,7 +2620,7 @@ void RtfAttributeOutput::ParaNumRule_Impl( const SwTxtNode* pTxtNd, sal_Int32 nL
 
             if( OUTLINE_RULE != pRule->GetRuleType() )
             {
-                if (sTxt.Len())
+                if (!sTxt.isEmpty())
                     m_aStyles.append(OOO_STRING_SVTOOLS_RTF_TAB);
                 m_aStyles.append('}');
                 m_aStyles.append(OOO_STRING_SVTOOLS_RTF_ILVL);
@@ -3129,7 +3129,7 @@ void RtfAttributeOutput::ParaOutlineLevel(const SfxUInt16Item& /*rItem*/)
 void RtfAttributeOutput::WriteExpand( const SwField* pFld )
 {
     SAL_INFO("sw.rtf", OSL_THIS_FUNC);
-    String sCmd;        // for optional Parameters
+    OUString sCmd;        // for optional Parameters
     switch (pFld->GetTyp()->Which())
     {
         //#i119803# Export user field and DB field for RTF filter
@@ -3430,12 +3430,12 @@ static OString ExportPICT( const SwFlyFrmFmt* pFlyFrmFmt, const Size &rOrig, con
 
         if( pFlyFrmFmt )
         {
-            String sDescription = pFlyFrmFmt->GetObjDescription();
+            OUString sDescription = pFlyFrmFmt->GetObjDescription();
             //write picture properties - wzDescription at first
             //looks like: "{\*\picprop{\sp{\sn PropertyName}{\sv PropertyValue}}}"
             aRet.append( "{" OOO_STRING_SVTOOLS_RTF_IGNORE OOO_STRING_SVTOOLS_RTF_PICPROP );//"{\*\picprop
             lcl_AppendSP( aRet, "wzDescription", sDescription, rExport );
-            String sName = pFlyFrmFmt->GetObjTitle();
+            OUString sName = pFlyFrmFmt->GetObjTitle();
             lcl_AppendSP( aRet, "wzName", sName, rExport );
             aRet.append( "}" ); //"}"
         }

sw/source/filter/ww8/rtfsdrexport.cxx
@@ -539,9 +539,9 @@ void RtfSdrExport::WriteOutliner(const OutlinerParaObject& rParaObj)
 
         rtl_TextEncoding eChrSet = aAttrIter.GetNodeCharSet();
 
-        String aStr( rEditObj.GetText( n ));
+        OUString aStr( rEditObj.GetText( n ));
         xub_StrLen nAktPos = 0;
-        xub_StrLen nEnd = aStr.Len();
+        xub_StrLen nEnd = aStr.getLength();
 
         aAttrIter.OutParaAttr(false);
         m_rAttrOutput.RunText().append(m_rAttrOutput.Styles().makeStringAndClear());
@@ -558,7 +558,7 @@ void RtfSdrExport::WriteOutliner(const OutlinerParaObject& rParaObj)
             bool bTxtAtr = aAttrIter.IsTxtAttr( nAktPos );
             if( !bTxtAtr )
             {
-                String aOut( aStr.Copy( nAktPos, nNextAttr - nAktPos ) );
+                OUString aOut( aStr.copy( nAktPos, nNextAttr - nAktPos ) );
                 m_rAttrOutput.RunText().append( msfilter::rtfutil::OutString( aOut, eChrSet ) );
             }
 

sw/source/filter/ww8/wrtw8esh.cxx
@@ -373,7 +373,7 @@ void WW8Export::DoFormText(const SwInputField * pFld)
 
     OutputField(0, ww::eFORMTEXT, aEmptyStr, WRITEFIELD_CMD_END);
 
-    String const fieldStr( pFld->ExpandField(true) );
+    OUString const fieldStr( pFld->ExpandField(true) );
     SwWW8Writer::WriteString16(Strm(), fieldStr, false);
 
     static sal_uInt8 aArr2[] = {
@@ -943,7 +943,7 @@ void MSWord_SdrAttrIter::OutEEField(const SfxPoolItem& rHt)
         const SvxURLField *pURL = (const SvxURLField *)pFld;
         m_rExport.AttrOutput().StartURL( pURL->GetURL(), pURL->GetTargetFrame() );
 
-        const String &rStr = pURL->GetRepresentation();
+        const OUString &rStr = pURL->GetRepresentation();
         m_rExport.AttrOutput().RawText( rStr, true, GetNodeCharSet() ); // FIXME kendy: is the 'true' actually correct here?  It was here before, but... ;-)
 
         m_rExport.AttrOutput().EndURL();
@@ -1144,9 +1144,9 @@ void WW8Export::WriteOutliner(const OutlinerParaObject& rParaObj, sal_uInt8 nTyp
 
         OSL_ENSURE( pO->empty(), " pO ist am Zeilenanfang nicht leer" );
 
-        String aStr( rEditObj.GetText( n ));
+        OUString aStr( rEditObj.GetText( n ));
         xub_StrLen nAktPos = 0;
-        xub_StrLen nEnd = aStr.Len();
+        xub_StrLen nEnd = aStr.getLength();
         do {
             xub_StrLen nNextAttr = aAttrIter.WhereNext();
             rtl_TextEncoding eNextChrSet = aAttrIter.GetNextCharSet();
@@ -2994,8 +2994,7 @@ bool SwMSConvertControls::ExportControl(WW8Export &rWW8Wrt, const SdrObject *pOb
     sal_uInt8 *pData = aSpecOLE+2;
     Set_UInt32(pData,nObjId );
 
-    OUString sFld(FieldString(ww::eCONTROL));
-    sFld += "Forms." + sUName + ".1 \\s ";
+    OUString sFld = FieldString(ww::eCONTROL) + "Forms." + sUName + ".1 \\s ";
 
     rWW8Wrt.OutputField(0, ww::eCONTROL, sFld,
         WRITEFIELD_START|WRITEFIELD_CMD_START|WRITEFIELD_CMD_END);

sw/source/filter/ww8/wrtw8nds.cxx
@@ -222,14 +222,14 @@ SwWW8AttrIter::SwWW8AttrIter(MSWordExportBase& rWr, const SwTxtNode& rTxtNd) :
     nAktSwPos = SearchNext(1);
 }
 
-xub_StrLen lcl_getMinPos( xub_StrLen pos1, xub_StrLen pos2 )
+sal_Int32 lcl_getMinPos( sal_Int32 pos1, sal_Int32 pos2 )
 {
-    xub_StrLen min = STRING_NOTFOUND;
-    if ( pos1 == STRING_NOTFOUND && pos2 != STRING_NOTFOUND )
+    xub_StrLen min = -1;
+    if ( pos1 == -1 && pos2 != -1 )
         min = pos2;
-    else if ( pos2 == STRING_NOTFOUND && pos1 != STRING_NOTFOUND )
+    else if ( pos2 == -1 && pos1 != -1 )
         min = pos1;
-    else if ( pos1 != STRING_NOTFOUND && pos2 != STRING_NOTFOUND )
+    else if ( pos1 != -1 && pos2 != -1 )
     {
         if ( pos1 < pos2 )
             min = pos1;
@@ -246,16 +246,16 @@ xub_StrLen SwWW8AttrIter::SearchNext( xub_StrLen nStartPos )
     xub_StrLen nMinPos = STRING_MAXLEN;
     xub_StrLen i=0;
 
-    const String aTxt = rNd.GetTxt();
-    xub_StrLen fieldEndPos = aTxt.Search(CH_TXT_ATR_FIELDEND, nStartPos);
-    xub_StrLen fieldStartPos = aTxt.Search(CH_TXT_ATR_FIELDSTART, nStartPos);
-    xub_StrLen formElementPos = aTxt.Search(CH_TXT_ATR_FORMELEMENT, nStartPos);
+    const OUString aTxt = rNd.GetTxt();
+    sal_Int32 fieldEndPos = aTxt.indexOf(CH_TXT_ATR_FIELDEND, nStartPos);
+    sal_Int32 fieldStartPos = aTxt.indexOf(CH_TXT_ATR_FIELDSTART, nStartPos);
+    sal_Int32 formElementPos = aTxt.indexOf(CH_TXT_ATR_FORMELEMENT, nStartPos);
 
-    xub_StrLen pos = lcl_getMinPos( fieldEndPos, fieldStartPos );
+    sal_Int32 pos = lcl_getMinPos( fieldEndPos, fieldStartPos );
     pos = lcl_getMinPos( pos, formElementPos );
 
-    if (pos!=STRING_NOTFOUND)
-        nMinPos=pos;
+    if (pos != -1)
+        nMinPos = pos;
 
     // first the redline, then the attributes
     if( pCurRedline )
@@ -686,7 +686,7 @@ void WW8AttributeOutput::StartRuby( const SwTxtNode& rNode, xub_StrLen /*nPos*/,
 
     const SwTxtRuby* pRubyTxt = rRuby.GetTxtRuby();
     const SwCharFmt* pFmt = pRubyTxt ? pRubyTxt->GetCharFmt() : 0;
-    String sFamilyName;
+    OUString sFamilyName;
     long nHeight;
     if ( pFmt )
     {
@@ -760,11 +760,11 @@ void WW8AttributeOutput::EndRuby()
 }
 
 /*#i15387# Better ideas welcome*/
-String &TruncateBookmark( String &rRet )
+OUString &TruncateBookmark( OUString &rRet )
 {
-    if ( rRet.Len() > 40 )
-        rRet.Erase( 40 );
-    OSL_ENSURE( rRet.Len() <= 40, "Word cannot have bookmarks longer than 40 chars" );
+    if ( rRet.getLength() > 40 )
+        rRet = rRet.copy( 0, 40 );
+    OSL_ENSURE( rRet.getLength() <= 40, "Word cannot have bookmarks longer than 40 chars" );
     return rRet;
 }
 
@@ -880,10 +880,10 @@ bool WW8AttributeOutput::StartURL( const OUString &rUrl, const OUString &rTarget
 
     // Compare the URL written by AnalyzeURL with the original one to see if
     // the output URL is absolute or relative.
-    String sRelativeURL;
+    OUString sRelativeURL;
     if ( !rUrl.isEmpty() )
         sRelativeURL = URIHelper::simpleNormalizedMakeRelative( m_rWW8Export.GetWriter().GetBaseURL(), rUrl );
-    bool bAbsolute = sRelativeURL.Equals( rUrl );
+    bool bAbsolute = sRelativeURL == rUrl;
 
     static sal_uInt8 aURLData1[] = {
         0,0,0,0,        // len of struct
@@ -1009,7 +1009,7 @@ bool WW8AttributeOutput::EndURL()
 
 OUString BookmarkToWord(const OUString &rBookmark)
 {
-    String sRet(INetURLObject::encode(rBookmark,
+    OUString sRet(INetURLObject::encode(rBookmark,
         INetURLObject::PART_REL_SEGMENT_EXTRA, '%',
         INetURLObject::ENCODE_ALL, RTL_TEXTENCODING_ASCII_US));
     return TruncateBookmark(sRet);
@@ -1789,8 +1789,7 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
 
     if ( aAttrIter.RequiresImplicitBookmark() )
     {
-        String sBkmkName = String(  "_toc"  );
-        sBkmkName += OUString::number( rNode.GetIndex() );
+        OUString sBkmkName =  "_toc" + OUString::number( rNode.GetIndex() );
         AppendWordBookmark( sBkmkName );
     }
 
@@ -1870,7 +1869,7 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
                 else if ( pFieldmark && pFieldmark->GetFieldname( ) == ODF_HYPERLINK )
                     WriteHyperlinkData( *pFieldmark );
                 if (!bCommentRange)
-                    OutputField( NULL, lcl_getFieldId( pFieldmark ), String(), WRITEFIELD_CMD_END );
+                    OutputField( NULL, lcl_getFieldId( pFieldmark ), OUString(), WRITEFIELD_CMD_END );
 
                 if ( pFieldmark && pFieldmark->GetFieldname() == ODF_UNHANDLED )
                 {
@@ -1908,7 +1907,7 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
                 if (pFieldmark && pFieldmark->GetFieldname() == ODF_COMMENTRANGE)
                     AttrOutput().WritePostitFieldEnd();
                 else
-                    OutputField( NULL, eFieldId, String(), WRITEFIELD_CLOSE );
+                    OutputField( NULL, eFieldId, OUString(), WRITEFIELD_CLOSE );
 
                 if ( pFieldmark && pFieldmark->GetFieldname() == ODF_FORMTEXT )
                     AppendBookmark( pFieldmark->GetName(), false );
@@ -1929,18 +1928,18 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
                         WRITEFIELD_START | WRITEFIELD_CMD_START );
                 if ( isDropdownOrCheckbox )
                     WriteFormData( *pFieldmark );
-                OutputField( NULL, lcl_getFieldId( pFieldmark ), String(), WRITEFIELD_CLOSE );
+                OutputField( NULL, lcl_getFieldId( pFieldmark ), OUString(), WRITEFIELD_CLOSE );
                 if ( isDropdownOrCheckbox )
                     AppendBookmark( pFieldmark->GetName(), false );
             }
             nLen -= static_cast< sal_Int32 >( ofs );
 
-            String aSnippet( aAttrIter.GetSnippet( aStr, nAktPos + static_cast< sal_Int32 >( ofs ), nLen ) );
+            OUString aSnippet( aAttrIter.GetSnippet( aStr, nAktPos + static_cast< sal_Int32 >( ofs ), nLen ) );
             if ( ( nTxtTyp == TXT_EDN || nTxtTyp == TXT_FTN ) && nAktPos == 0 && nLen > 0 )
             {
                 // Insert tab for aesthetic puposes #i24762#
-                if ( aSnippet.GetChar( 0 ) != 0x09 )
-                    aSnippet.Insert( 0x09, 0 );
+                if ( aSnippet[0] != 0x09 )
+                    aSnippet = OUString( 0x09 ) + aSnippet;
             }
             AttrOutput().RunText( aSnippet, eChrSet );
         }

commit 879bc3e0b843fb479e45295c987bb199a0ea588d

SwRTFWriter: fix string conversion
Regression from 07990deb3f9f2b44c7de55a9f1ed098e3046a44c (convert
sw/source/filter/ww8/*.cxx from String to OUString, 2013-10-14), check
for string length before invoking OUString::operator[].

This can be reproduced if copy&pasting some text inside a Writer doc
from normal text to rectangle shapes inside group shapes.

Change-Id: Ia8f774b27ca63773792e6af913dfe7e493bea4b8

sw/source/filter/ww8/rtfexport.cxx
@@ -1178,7 +1178,7 @@ SwRTFWriter::SwRTFWriter( const OUString& rFltName, const OUString & rBaseURL )
     SAL_INFO("sw.rtf", OSL_THIS_FUNC);
     SetBaseURL( rBaseURL );
     // export outline nodes, only (send outline to clipboard/presentation)
-    m_bOutOutlineOnly = 'O' == rFltName[0];
+    m_bOutOutlineOnly = (!rFltName.isEmpty() && 'O' == rFltName[0]);
 }
 
 SwRTFWriter::~SwRTFWriter()