LemmingAvalanche/libreoffice-repaired

commit 581450a64e2096b7a128a59722f2551ce784f0c2

sw: Improved document comparison based on RSIDs.

sw/source/core/frmedt/fecopy.cxx
@@ -1043,6 +1043,7 @@ sal_Bool SwFEShell::Paste( SwDoc* pClpDoc, sal_Bool bIncludingPageFrames )
                 // muessen die BoxAttribute aber entfernt werden.
                 GetDoc()->ClearBoxNumAttrs( rInsPos.nNode );
             }
+
             //find out if the clipboard document starts with a table
             bool bStartWithTable = 0 != aCpyPam.Start()->nNode.GetNode().FindTableNode();
             SwPosition aInsertPosition( rInsPos );
@@ -1060,6 +1061,22 @@ sal_Bool SwFEShell::Paste( SwDoc* pClpDoc, sal_Bool bIncludingPageFrames )
                                SwPosition(rInsPos.nNode));
 
                     aPaM.GetDoc()->MakeUniqueNumRules(aPaM);
+                }
+            }
+
+            // Update the rsid of each pasted text node.
+            {
+                xub_StrLen nNodesCnt = aCpyPam.End()->nNode.GetIndex() - aCpyPam.Start()->nNode.GetIndex();
+                SwNodes &rDestNodes = GetDoc()->GetNodes();
+                xub_StrLen nDestStart = PCURCRSR->GetPoint()->nNode.GetIndex() - nNodesCnt;
+
+                for ( sal_uInt64 nIdx = 0; nIdx <= nNodesCnt; nIdx++ )
+                {
+                    SwTxtNode *pTxtNode = rDestNodes[ nDestStart + nIdx ]->GetTxtNode();
+                    if ( pTxtNode )
+                    {
+                        GetDoc()->UpdateParRsid( pTxtNode );
+                    }
                 }
             }
 

cui/source/options/treeopt.cxx
@@ -385,6 +385,7 @@ static OptionsMapping_Impl const OptionsMap_Impl[] =
     { "Writer",             "Print",                RID_SW_TP_OPTPRINT_PAGE },
     { "Writer",             "Table",                RID_SW_TP_OPTTABLE_PAGE },
     { "Writer",             "Changes",              RID_SW_TP_REDLINE_OPT },
+    { "Writer",             "Comparison",           RID_SW_TP_COMPARISON_OPT },
     { "Writer",             "Compatibility",        RID_SW_TP_OPTCOMPATIBILITY_PAGE },
     { "Writer",             "AutoCaption",          RID_SW_TP_OPTCAPTION_PAGE },
     { "Writer",             "MailMerge",            RID_SW_TP_MAILCONFIG },

cui/source/options/treeopt.src
@@ -148,6 +148,7 @@ Resource RID_OFADLG_OPTIONS_TREE_PAGES
             < "Print" ;       RID_SW_TP_OPTPRINT_PAGE  ; > ;
             < "Table" ;         RID_SW_TP_OPTTABLE_PAGE  ; > ;
             < "Changes" ;    RID_SW_TP_REDLINE_OPT    ; > ;
+            < "Comparison" ; RID_SW_TP_COMPARISON_OPT ; > ;
             < "Compatibility" ; RID_SW_TP_OPTCOMPATIBILITY_PAGE    ; > ;
             < "AutoCaption" ; RID_SW_TP_OPTCAPTION_PAGE ; > ;
             < "Mail Merge E-mail" ; RID_SW_TP_MAILCONFIG ; >;

editeng/Package_inc.mk
@@ -113,6 +113,7 @@ $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/postitem.hxx,editeng/p
 $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/prntitem.hxx,editeng/prntitem.hxx))
 $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/protitem.hxx,editeng/protitem.hxx))
 $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/prszitem.hxx,editeng/prszitem.hxx))
+$(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/rsiditem.hxx,editeng/rsiditem.hxx))
 $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/scriptspaceitem.hxx,editeng/scriptspaceitem.hxx))
 $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/scripttypeitem.hxx,editeng/scripttypeitem.hxx))
 $(eval $(call gb_Package_add_file,editeng_inc,inc/editeng/shaditem.hxx,editeng/shaditem.hxx))

editeng/inc/editeng/rsiditem.hxx
@@ -1,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * Version: MPL 1.1 / GPLv3+ / LGPLv3+
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License or as specified alternatively below. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Major Contributor(s):
+ * [ Copyright (C) 2009 Tzvetelina Tzeneva <tzvetelina@gmail.com> (initial developer) ]
+ */
+#ifndef _SVX_RSIDITEM_HXX
+#define _SVX_RSIDITEM_HXX
+
+#include <svl/intitem.hxx>
+#include "editeng/editengdllapi.h"
+
+//----------------------
+// SvxRsidItem
+//----------------------
+
+class EDITENG_DLLPUBLIC SvxRsidItem : public SfxUInt32Item
+{
+public:
+    TYPEINFO();
+
+    SvxRsidItem( sal_uInt32 nRsid, sal_uInt16 nId ) : SfxUInt32Item( nId, nRsid ) {}
+    SvxRsidItem( SvStream& rIn, sal_uInt16 nId ) : SfxUInt32Item( nId, rIn ) {}
+
+    virtual SfxPoolItem* Clone( SfxItemPool* pPool = NULL ) const;
+    virtual SfxPoolItem* Create( SvStream& rIn, sal_uInt16 nVer ) const;
+
+    virtual bool QueryValue( com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 ) const;
+    virtual bool PutValue( const com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 );
+};
+
+#endif // _SVX_RSIDITEM_HXX

editeng/inc/editeng/svxenum.hxx
@@ -222,6 +222,12 @@ enum SvxExtNumType
     SVX_NUM_CHARS_LOWER_LETTER_N
 };
 
+enum SvxCompareMode
+{
+    SVX_CMP_AUTO = 0,
+    SVX_CMP_BY_WORD,
+    SVX_CMP_BY_CHAR
+};
 
 #endif
 

editeng/source/items/textitem.cxx
@@ -73,6 +73,7 @@
 #include <com/sun/star/lang/Locale.hpp>
 #include <com/sun/star/text/FontEmphasis.hpp>
 #include <com/sun/star/i18n/ScriptType.hpp>
+#include <editeng/rsiditem.hxx>
 #include <editeng/memberids.hrc>
 #include <editeng/flstitem.hxx>
 #include <editeng/fontitem.hxx>
@@ -157,7 +158,7 @@ TYPEINIT1_FACTORY(SvxScriptTypeItem, SfxUInt16Item, new SvxScriptTypeItem);
 TYPEINIT1_FACTORY(SvxCharRotateItem, SfxUInt16Item, new SvxCharRotateItem(0, sal_False, 0));
 TYPEINIT1_FACTORY(SvxCharScaleWidthItem, SfxUInt16Item, new SvxCharScaleWidthItem(100, 0));
 TYPEINIT1_FACTORY(SvxCharReliefItem, SfxEnumItem, new SvxCharReliefItem(RELIEF_NONE, 0));
-
+TYPEINIT1_FACTORY(SvxRsidItem, SfxUInt32Item, new SvxRsidItem(0, 0));
 
 TYPEINIT1(SvxScriptSetItem, SfxSetItem );
 
@@ -3740,4 +3741,29 @@ short GetI18NScriptType( sal_uInt16 nItemType )
     return 0;
 }
 
+bool SvxRsidItem::QueryValue( uno::Any& rVal, sal_uInt8 ) const
+{
+    rVal <<= ( (sal_uInt32)GetValue() );
+    return true;
+}
+
+bool SvxRsidItem::PutValue( const uno::Any& rVal, sal_uInt8 )
+{
+    sal_uInt32 nRsid = 0;
+    if( !( rVal >>= nRsid ) )
+        return false;
+
+    SetValue( nRsid );
+    return true;
+}
+
+SfxPoolItem* SvxRsidItem::Clone( SfxItemPool * ) const
+{
+    return new SvxRsidItem( *this );
+}
+
+SfxPoolItem* SvxRsidItem::Create(SvStream& rIn, sal_uInt16 ) const
+{
+    return new SvxRsidItem( rIn, Which() );
+}
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

officecfg/registry/schema/org/openoffice/Office/Writer.xcs
@@ -2810,6 +2810,36 @@
 				</prop>
 			</group>
 		</group>
+        <group oor:name="Comparison">
+            <info>
+                <desc>Contains settings for comparing files.</desc>
+            </info>
+            <prop oor:name="Mode" oor:type="xs:short">
+                <info>
+                    <author>OS</author>
+                    <desc>Defines the Comparison Mode.</desc>
+                </info>
+            </prop>
+
+            <prop oor:name="UseRSID" oor:type="xs:boolean">
+                <info>
+                    <author>OS</author>
+                    <desc>Specifies if RSIDs are used in comparison.</desc>
+                </info>
+            </prop>
+            <prop oor:name="IgnorePieces" oor:type="xs:boolean">
+                <info>
+                    <author>OS</author>
+                    <desc>Specifies if short pieces of matched text are ignored.</desc>
+                </info>
+            </prop>
+            <prop oor:name="IgnoreLength" oor:type="xs:short">
+                <info>
+                    <author>OS</author>
+                    <desc>Defines the length of ignored pieces.</desc>
+                </info>
+            </prop>
+        </group>
 		<group oor:name="Insert">
 			<info>
 				<desc>Specifies the settings for inserting various object types.</desc>

commit d1d25d7be64568db940c95f16ea7f7d3939b1342

fdo#71892: sw: fix crash when pasting table in footnote
The code that updates RSIDs assumes that the number of nodes copied is
the same as the number of nodes inserted, which is not true when pasting
a table into a footnote because Writer can't do that, hence all table
nodes are missing.  Count inserted nodes instead.

(regression from 581450a64e2096b7a128a59722f2551ce784f0c2)

Change-Id: I77b5b7751d1036a6401f708532537d874969502e

sw/source/core/frmedt/fecopy.cxx
@@ -1039,25 +1039,22 @@ sal_Bool SwFEShell::Paste( SwDoc* pClpDoc, sal_Bool bIncludingPageFrames )
                 aIndexBefore--;
 
                 pClpDoc->CopyRange( aCpyPam, rInsPos, false );
+                // Note: aCpyPam is invalid now
 
-                {
-                    ++aIndexBefore;
-                    SwPaM aPaM(SwPosition(aIndexBefore),
-                               SwPosition(rInsPos.nNode));
+                ++aIndexBefore;
+                SwPaM aPaM(SwPosition(aIndexBefore),
+                           SwPosition(rInsPos.nNode));
 
-                    aPaM.GetDoc()->MakeUniqueNumRules(aPaM);
-                }
-            }
+                aPaM.GetDoc()->MakeUniqueNumRules(aPaM);
 
-            // Update the rsid of each pasted text node.
-            {
-                sal_Int32 nNodesCnt = aCpyPam.End()->nNode.GetIndex() - aCpyPam.Start()->nNode.GetIndex();
+                // Update the rsid of each pasted text node.
                 SwNodes &rDestNodes = GetDoc()->GetNodes();
-                sal_Int32 nDestStart = PCURCRSR->GetPoint()->nNode.GetIndex() - nNodesCnt;
+                sal_uLong const nEndIdx = aPaM.End()->nNode.GetIndex();
 
-                for (sal_Int32 nIdx = 0; nIdx <= nNodesCnt; ++nIdx)
+                for (sal_uLong nIdx = aPaM.Start()->nNode.GetIndex();
+                        nIdx <= nEndIdx; ++nIdx)
                 {
-                    SwTxtNode *pTxtNode = rDestNodes[ nDestStart + nIdx ]->GetTxtNode();
+                    SwTxtNode *const pTxtNode = rDestNodes[nIdx]->GetTxtNode();
                     if ( pTxtNode )
                     {
                         GetDoc()->UpdateParRsid( pTxtNode );