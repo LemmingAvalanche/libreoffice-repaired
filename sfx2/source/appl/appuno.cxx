LemmingAvalanche/libreoffice-repaired

commit 30d850a9d6a91bd351338dc465e70fe74f891d7c

convert sfx2/source/appl/* from String to OUString
Change-Id: I59901bdc3c953bf883c0b543bd1e0b5d6a96e80a

sfx2/source/appl/appuno.cxx
@@ -243,8 +243,7 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
         if ( !pItem )
         {
 #ifdef DBG_UTIL
-            OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM(
-                "No creator method for item: "));
+            OStringBuffer aStr("No creator method for item: ");
             aStr.append(static_cast<sal_Int32>(nSlotId));
             OSL_FAIL(aStr.getStr());
 #endif
@@ -257,8 +256,8 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
         sal_uInt16 nSubCount = pType->nAttribs;
 
         const beans::PropertyValue& rProp = pPropsVal[0];
-        String aName = rProp.Name;
-        if ( nCount == 1 && aName.CompareToAscii( pSlot->pUnoName ) == COMPARE_EQUAL )
+        OUString aName = rProp.Name;
+        if ( nCount == 1 && aName == OUString( pSlot->pUnoName, strlen( pSlot->pUnoName ), RTL_TEXTENCODING_UTF8 ) )
         {
             // there is only one parameter and its name matches the name of the property,
             // so it's either a simple property or a complex property in one single UNO struct
@@ -268,7 +267,7 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
 #ifdef DBG_UTIL
             else
             {
-                OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM("Property not convertible: "));
+                OStringBuffer aStr("Property not convertible: ");
                 aStr.append(pSlot->pUnoName);
                 OSL_FAIL( aStr.getStr() );
             }
@@ -278,8 +277,8 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
         else if ( nSubCount == 0 )
         {
             // for a simple property there can be only one parameter and its name *must* match
-            OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM("Property name does not match: "));
-            aStr.append(OUStringToOString(aName, RTL_TEXTENCODING_UTF8));
+            OUStringBuffer aStr("Property name does not match: ");
+            aStr.append(aName);
             OSL_FAIL( aStr.getStr() );
         }
 #endif
@@ -293,8 +292,7 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
             // so this should be notified as a warning only
             if ( nCount != nSubCount )
             {
-                OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM(
-                    "MacroPlayer: wrong number of parameters for slot: "));
+                OStringBuffer aStr("MacroPlayer: wrong number of parameters for slot: ");
                 aStr.append(static_cast<sal_Int32>(nSlotId));
                 DBG_WARNING(aStr.getStr());
             }
@@ -382,8 +380,8 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
             for ( sal_uInt16 n=0; n<nCount; n++ )
             {
                 const beans::PropertyValue& rProp = pPropsVal[n];
-                String aName = rProp.Name;
-                if ( aName.CompareToAscii(rArg.pName) == COMPARE_EQUAL )
+                OUString aName = rProp.Name;
+                if ( aName == OUString( rArg.pName, strlen(rArg.pName), RTL_TEXTENCODING_UTF8 )  )
                 {
 #ifdef DBG_UTIL
                     ++nFoundArgs;
@@ -394,7 +392,7 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
 #ifdef DBG_UTIL
                     else
                     {
-                        OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM("Property not convertible: "));
+                        OStringBuffer aStr("Property not convertible: ");
                         aStr.append(rArg.pName);
                         OSL_FAIL( aStr.getStr() );
                     }
@@ -410,8 +408,8 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
             for ( sal_uInt16 n=0; n<nCount; n++ )
             {
                 const beans::PropertyValue& rProp = pPropsVal[n];
-                String aName = rProp.Name;
-                if ( aName.CompareToAscii(rArg.pName) == COMPARE_EQUAL )
+                OUString aName = rProp.Name;
+                if ( aName == OUString(rArg.pName, strlen(rArg.pName), RTL_TEXTENCODING_UTF8) )
                 {
                     bAsWholeItem = sal_True;
 #ifdef DBG_UTIL
@@ -423,7 +421,7 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
 #ifdef DBG_UTIL
                     else
                     {
-                        OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM("Property not convertible: "));
+                        OStringBuffer aStr("Property not convertible: ");
                         aStr.append(rArg.pName);
                         OSL_FAIL( aStr.getStr() );
                     }
@@ -933,14 +931,14 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
     else
     {
         // transform parameter "OptionsPageURL" of slot "OptionsTreeDialog"
-        String sSlotName( "OptionsTreeDialog" );
-        String sPropName( "OptionsPageURL" );
-        if ( sSlotName.EqualsAscii( pSlot->pUnoName ) )
+        OUString sSlotName( "OptionsTreeDialog" );
+        OUString sPropName( "OptionsPageURL" );
+        if ( sSlotName == OUString( pSlot->pUnoName, strlen(pSlot->pUnoName), RTL_TEXTENCODING_UTF8 ) )
         {
             for ( sal_uInt16 n = 0; n < nCount; ++n )
             {
                 const PropertyValue& rProp = pPropsVal[n];
-                String sName( rProp.Name );
+                OUString sName( rProp.Name );
                 if ( sName == sPropName )
                 {
                     OUString sURL;
@@ -955,7 +953,7 @@ void TransformParameters( sal_uInt16 nSlotId, const uno::Sequence<beans::Propert
     if ( nFoundArgs == nCount )
     {
         // except for the "special" slots: assure that every argument was convertible
-        OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM("MacroPlayer: Some properties didn't match to any formal argument for slot: "));
+        OStringBuffer aStr("MacroPlayer: Some properties didn't match to any formal argument for slot: ");
         aStr.append(pSlot->pUnoName);
         DBG_WARNING( aStr.getStr() );
     }
@@ -1353,18 +1351,16 @@ void TransformItems( sal_uInt16 nSlotId, const SfxItemSet& rSet, uno::Sequence<b
                         nSubId |= CONVERT_TWIPS;
 
                     DBG_ASSERT(( pType->aAttrib[n-1].nAID ) <= 127, "Member ID out of range" );
-                    String aName( OUString::createFromAscii( pSlot->pUnoName ) ) ;
-                    aName += '.';
+                    OUString aName( OUString::createFromAscii( pSlot->pUnoName ) ) ;
+                    aName += ".";
                     aName += OUString::createFromAscii( pType->aAttrib[n-1].pName ) ;
                     pValue[nActProp].Name = aName;
                     if ( !pItem->QueryValue( pValue[nActProp++].Value, nSubId ) )
                     {
-                        OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM(
-                            "Sub item "));
+                        OStringBuffer aStr("Sub item ");
                         aStr.append(static_cast<sal_Int32>(
                             pType->aAttrib[n-1].nAID));
-                        aStr.append(RTL_CONSTASCII_STRINGPARAM(
-                            " not convertible in slot: "));
+                        aStr.append(" not convertible in slot: ");
                         aStr.append(static_cast<sal_Int32>(nSlotId));
                         OSL_FAIL( aStr.getStr() );
                     }
@@ -1408,18 +1404,16 @@ void TransformItems( sal_uInt16 nSlotId, const SfxItemSet& rSet, uno::Sequence<b
                         nSubId |= CONVERT_TWIPS;
 
                     DBG_ASSERT((rArg.pType->aAttrib[n-1].nAID) <= 127, "Member ID out of range" );
-                    String aName( OUString::createFromAscii( rArg.pName ) ) ;
-                    aName += '.';
+                    OUString aName( OUString::createFromAscii( rArg.pName ) ) ;
+                    aName += ".";
                     aName += OUString::createFromAscii( rArg.pType->aAttrib[n-1].pName ) ;
                     pValue[nActProp].Name = aName;
                     if ( !pItem->QueryValue( pValue[nActProp++].Value, nSubId ) )
                     {
-                        OStringBuffer aStr(RTL_CONSTASCII_STRINGPARAM(
-                            "Sub item "));
+                        OStringBuffer aStr("Sub item ");
                         aStr.append(static_cast<sal_Int32>(
                             rArg.pType->aAttrib[n-1].nAID));
-                        aStr.append(RTL_CONSTASCII_STRINGPARAM(
-                            " not convertible in slot: "));
+                        aStr.append(" not convertible in slot: ");
                         aStr.append(static_cast<sal_Int32>(rArg.nSlotId));
                         OSL_FAIL(aStr.getStr());
                     }
@@ -1855,22 +1849,22 @@ ErrCode SfxMacroLoader::loadMacro( const OUString& rURL, com::sun::star::uno::An
     // 'macro:///lib.mod.proc(args)' => macro of App-BASIC
     // 'macro://[docname|.]/lib.mod.proc(args)' => macro of current or qualified document
     // 'macro://obj.method(args)' => direct API call, execute it via App-BASIC
-    String aMacro( rURL );
-    sal_uInt16 nHashPos = aMacro.Search( '/', 8 );
-    sal_uInt16 nArgsPos = aMacro.Search( '(' );
+    OUString aMacro( rURL );
+    sal_Int32 nHashPos = aMacro.indexOf( '/', 8 );
+    sal_Int32 nArgsPos = aMacro.indexOf( '(' );
     BasicManager *pAppMgr = SFX_APP()->GetBasicManager();
     BasicManager *pBasMgr = 0;
     ErrCode nErr = ERRCODE_NONE;
 
     // should a macro function be executed ( no direct API call)?
-    if ( STRING_NOTFOUND != nHashPos && nHashPos < nArgsPos )
+    if ( -1 != nHashPos && nHashPos < nArgsPos )
     {
         // find BasicManager
         SfxObjectShell* pDoc = NULL;
-        String aBasMgrName( INetURLObject::decode(aMacro.Copy( 8, nHashPos-8 ), INET_HEX_ESCAPE, INetURLObject::DECODE_WITH_CHARSET) );
-        if ( !aBasMgrName.Len() )
+        OUString aBasMgrName( INetURLObject::decode(aMacro.copy( 8, nHashPos-8 ), INET_HEX_ESCAPE, INetURLObject::DECODE_WITH_CHARSET) );
+        if ( aBasMgrName.isEmpty() )
             pBasMgr = pAppMgr;
-        else if ( aBasMgrName.EqualsAscii(".") )
+        else if ( aBasMgrName == "." )
         {
             // current/actual document
             pDoc = pCurrent;
@@ -1898,13 +1892,13 @@ ErrCode SfxMacroLoader::loadMacro( const OUString& rURL, com::sun::star::uno::An
             if ( pDoc )
             {
                 // security check for macros from document basic if an SFX doc is given
-                if ( !pDoc->AdjustMacroMode( String() ) )
+                if ( !pDoc->AdjustMacroMode( OUString() ) )
                     // check forbids execution
                     return ERRCODE_IO_ACCESSDENIED;
             }
             else if ( pDoc && pDoc->GetMedium() )
             {
-                pDoc->AdjustMacroMode( String() );
+                pDoc->AdjustMacroMode( OUString() );
                 SFX_ITEMSET_ARG( pDoc->GetMedium()->GetItemSet(), pUpdateDocItem, SfxUInt16Item, SID_UPDATEDOCMODE, sal_False);
                 SFX_ITEMSET_ARG( pDoc->GetMedium()->GetItemSet(), pMacroExecModeItem, SfxUInt16Item, SID_MACROEXECMODE, sal_False);
                 if ( pUpdateDocItem && pMacroExecModeItem
@@ -1914,16 +1908,16 @@ ErrCode SfxMacroLoader::loadMacro( const OUString& rURL, com::sun::star::uno::An
             }
 
             // find BASIC method
-            String aQualifiedMethod( INetURLObject::decode(aMacro.Copy( nHashPos+1 ), INET_HEX_ESCAPE, INetURLObject::DECODE_WITH_CHARSET) );
-            String aArgs;
-            if ( STRING_NOTFOUND != nArgsPos )
+            OUString aQualifiedMethod( INetURLObject::decode(aMacro.copy( nHashPos+1 ), INET_HEX_ESCAPE, INetURLObject::DECODE_WITH_CHARSET) );
+            OUString aArgs;
+            if ( -1 != nArgsPos )
             {
                 // remove arguments from macro name
-                aArgs = aQualifiedMethod.Copy( nArgsPos - nHashPos - 1 );
-                aQualifiedMethod.Erase( nArgsPos - nHashPos - 1 );
+                aArgs = aQualifiedMethod.copy( nArgsPos - nHashPos - 1 );
+                aQualifiedMethod = aQualifiedMethod.copy( 0, nArgsPos - nHashPos - 1 );
             }
 
-            if ( pBasMgr->HasMacro( OUString(aQualifiedMethod) ) )
+            if ( pBasMgr->HasMacro( aQualifiedMethod ) )
             {
                 Any aOldThisComponent;
                 const bool bSetDocMacroMode = ( pDoc != NULL ) && bIsDocBasic;
@@ -1977,7 +1971,7 @@ ErrCode SfxMacroLoader::loadMacro( const OUString& rURL, com::sun::star::uno::An
     {
         // direct API call on a specified object
         OUStringBuffer aCall;
-        aCall.append('[').append(INetURLObject::decode(aMacro.Copy(6), INET_HEX_ESCAPE,
+        aCall.append('[').append(INetURLObject::decode(aMacro.copy(6), INET_HEX_ESCAPE,
             INetURLObject::DECODE_WITH_CHARSET));
         aCall.append(']');
         pAppMgr->GetLib(0)->Execute(aCall.makeStringAndClear());

sfx2/source/appl/app.cxx
@@ -302,8 +302,7 @@ void SfxApplication::SetLastDir_Impl
 
 void SfxApplication::ResetLastDir()
 {
-    String aEmpty;
-    pAppData_Impl->aLastDir = aEmpty;
+    pAppData_Impl->aLastDir = "";
 }
 
 //--------------------------------------------------------------------

sfx2/source/appl/appdde.cxx
@@ -77,7 +77,7 @@ public:
 //--------------------------------------------------------------------
 namespace
 {
-    sal_Bool lcl_IsDocument( const String& rContent )
+    sal_Bool lcl_IsDocument( const OUString& rContent )
     {
         using namespace com::sun::star;
 
@@ -122,14 +122,14 @@ bool ImplDdeService::MakeTopic( const OUString& rNm )
     // First only loop over the ObjectShells to find those
     // with the specific name:
     sal_Bool bRet = sal_False;
-    String sNm( rNm );
-    sNm.ToLowerAscii();
+    OUString sNm( rNm );
+    sNm = sNm.toAsciiLowerCase();
     TypeId aType( TYPE(SfxObjectShell) );
     SfxObjectShell* pShell = SfxObjectShell::GetFirst( &aType );
     while( pShell )
     {
-        String sTmp( pShell->GetTitle(SFX_TITLE_FULLNAME) );
-        sTmp.ToLowerAscii();
+        OUString sTmp( pShell->GetTitle(SFX_TITLE_FULLNAME) );
+        sTmp = sTmp.toAsciiLowerCase();
         if( sTmp == sNm )
         {
             SFX_APP()->AddDdeTopic( pShell );
@@ -531,7 +531,7 @@ void SfxApplication::AddDdeTopic( SfxObjectShell* pSh )
         return;
 
     // prevent double submit
-    String sShellNm;
+    OUString sShellNm;
     sal_Bool bFnd = sal_False;
     for (size_t n = pAppData_Impl->pDocTopics->size(); n;)
     {
@@ -541,10 +541,10 @@ void SfxApplication::AddDdeTopic( SfxObjectShell* pSh )
             if( !bFnd )
             {
                 bFnd = sal_True;
-                (sShellNm = pSh->GetTitle(SFX_TITLE_FULLNAME)).ToLowerAscii();
+                sShellNm = pSh->GetTitle(SFX_TITLE_FULLNAME).toAsciiLowerCase();
             }
-            String sNm( (*pAppData_Impl->pDocTopics)[ n ]->GetName() );
-            if( sShellNm == sNm.ToLowerAscii() )
+            OUString sNm( (*pAppData_Impl->pDocTopics)[ n ]->GetName() );
+            if( sShellNm == sNm.toAsciiLowerCase() )
                 return ;
         }
     }
@@ -594,7 +594,7 @@ bool SfxDdeTriggerTopic_Impl::Execute( const OUString* )
 //--------------------------------------------------------------------
 DdeData* SfxDdeDocTopic_Impl::Get( sal_uIntPtr nFormat )
 {
-    String sMimeType( SotExchange::GetFormatMimeType( nFormat ));
+    OUString sMimeType( SotExchange::GetFormatMimeType( nFormat ));
     ::com::sun::star::uno::Any aValue;
     long nRet = pSh->DdeGetData( GetCurItem(), sMimeType, aValue );
     if( nRet && aValue.hasValue() && ( aValue >>= aSeq ) )
@@ -615,7 +615,7 @@ bool SfxDdeDocTopic_Impl::Put( const DdeData* pData )
     {
         ::com::sun::star::uno::Any aValue;
         aValue <<= aSeq;
-        String sMimeType( SotExchange::GetFormatMimeType( pData->GetFormat() ));
+        OUString sMimeType( SotExchange::GetFormatMimeType( pData->GetFormat() ));
         bRet = 0 != pSh->DdeSetData( GetCurItem(), sMimeType, aValue );
     }
     else

sfx2/source/appl/appopen.cxx
@@ -398,7 +398,7 @@ sal_uIntPtr SfxApplication::LoadTemplate( SfxObjectShellLock& xDoc, const OUStri
 
                xDoc->GetStorage()->copyToStorage( xTempStorage );
 
-            if ( !xDoc->DoSaveCompleted( new SfxMedium( xTempStorage, String() ) ) )
+            if ( !xDoc->DoSaveCompleted( new SfxMedium( xTempStorage, OUString() ) ) )
                 throw uno::RuntimeException();
         }
         catch( uno::Exception& )
@@ -410,10 +410,10 @@ sal_uIntPtr SfxApplication::LoadTemplate( SfxObjectShellLock& xDoc, const OUStri
             return ERRCODE_SFX_GENERAL;
         }
 
-        SetTemplate_Impl( rFileName, String(), xDoc );
+        SetTemplate_Impl( rFileName, OUString(), xDoc );
     }
     else
-        SetTemplate_Impl( rFileName, String(), xDoc );
+        SetTemplate_Impl( rFileName, OUString(), xDoc );
 
     xDoc->SetNoName();
     xDoc->InvalidateName();
@@ -444,7 +444,7 @@ sal_uIntPtr SfxApplication::LoadTemplate( SfxObjectShellLock& xDoc, const OUStri
 void SfxApplication::NewDocDirectExec_Impl( SfxRequest& rReq )
 {
     SFX_REQUEST_ARG( rReq, pFactoryItem, SfxStringItem, SID_NEWDOCDIRECT, sal_False);
-    String aFactName;
+    OUString aFactName;
     if ( pFactoryItem )
         aFactName = pFactoryItem->GetValue();
    else
@@ -452,7 +452,7 @@ void SfxApplication::NewDocDirectExec_Impl( SfxRequest& rReq )
 
 
     SfxRequest aReq( SID_OPENDOC, SFX_CALLMODE_SYNCHRON, GetPool() );
-    String aFact = OUString("private:factory/");
+    OUString aFact("private:factory/");
     aFact += aFactName;
     aReq.AppendItem( SfxStringItem( SID_FILE_NAME, aFact ) );
     aReq.AppendItem( SfxFrameItem( SID_DOCFRAME, GetFrame() ) );
@@ -614,7 +614,7 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
     SFX_REQUEST_ARG( rReq, pFileNameItem, SfxStringItem, SID_FILE_NAME, sal_False );
     if ( pFileNameItem )
     {
-        String aCommand( pFileNameItem->GetValue() );
+        OUString aCommand( pFileNameItem->GetValue() );
         const SfxSlot* pSlot = GetInterface()->GetSlot( aCommand );
         if ( pSlot )
         {
@@ -622,10 +622,10 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
         }
         else
         {
-            sal_Int32 nIndex = aCommand.SearchAscii("slot:");
-            if ( !nIndex )
+            sal_Int32 nIndex = aCommand.indexOf("slot:");
+            if ( nIndex == 0 )
             {
-                sal_uInt16 nSlotId = (sal_uInt16) String( aCommand, 5, aCommand.Len()-5 ).ToInt32();
+                sal_uInt16 nSlotId = (sal_uInt16) aCommand.copy(5).toInt32();
                 if ( nSlotId == SID_OPENDOC )
                     pFileNameItem = NULL;
             }
@@ -654,7 +654,7 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
         if ( pSystemDialogItem )
             nDialog = pSystemDialogItem->GetValue() ? SFX2_IMPL_DIALOG_SYSTEM : SFX2_IMPL_DIALOG_OOO;
 
-        String sStandardDir;
+        OUString sStandardDir;
 
         SFX_REQUEST_ARG( rReq, pStandardDirItem, SfxStringItem, SID_STANDARD_DIR, sal_False );
         if ( pStandardDirItem )
@@ -797,7 +797,7 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
     }
 
     SFX_REQUEST_ARG( rReq, pFileName, SfxStringItem, SID_FILE_NAME, sal_False );
-    String aFileName = pFileName->GetValue();
+    OUString aFileName = pFileName->GetValue();
 
     OUString aReferer;
     SFX_REQUEST_ARG( rReq, pRefererItem, SfxStringItem, SID_REFERER, sal_False );
@@ -837,7 +837,7 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
     }
 
     // Mark without URL cannot be handled by hyperlink code
-    if ( bHyperlinkUsed && aFileName.Len() && aFileName.GetChar(0) != '#' )
+    if ( bHyperlinkUsed && !aFileName.isEmpty() && aFileName[0] != '#' )
     {
         Reference< ::com::sun::star::document::XTypeDetection > xTypeDetection(
                                                                     ::comphelper::getProcessServiceFactory()->createInstance(
@@ -1064,12 +1064,12 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
         // make URL ready
         SFX_REQUEST_ARG( rReq, pURLItem, SfxStringItem, SID_FILE_NAME, sal_False );
         aFileName = pURLItem->GetValue();
-        if( aFileName.Len() && aFileName.GetChar(0) == '#' ) // Mark without URL
+        if( !aFileName.isEmpty() && aFileName[0] == '#' ) // Mark without URL
         {
             SfxViewFrame *pView = pTargetFrame ? pTargetFrame->GetCurrentViewFrame() : 0;
             if ( !pView )
                 pView = SfxViewFrame::Current();
-            pView->GetViewShell()->JumpToMark( aFileName.Copy(1) );
+            pView->GetViewShell()->JumpToMark( aFileName.copy(1) );
             rReq.SetReturnValue( SfxViewFrameItem( 0, pView ) );
             return;
         }

sfx2/source/appl/appserv.cxx
@@ -1004,7 +1004,7 @@ void SfxApplication::OfaExec_Impl( SfxRequest& rReq )
     {
         case SID_OPTIONS_TREEDIALOG:
         {
-            String sPageURL;
+            OUString sPageURL;
             SFX_REQUEST_ARG( rReq, pURLItem, SfxStringItem, SID_OPTIONS_PAGEURL, sal_False );
             if ( pURLItem )
                 sPageURL = pURLItem->GetValue();
@@ -1113,7 +1113,7 @@ void SfxApplication::OfaExec_Impl( SfxRequest& rReq )
 
                 pView = lcl_getBasicIDEViewFrame( pBasicIDE );
                 if ( pView )
-                    pView->SetName( String( RTL_CONSTASCII_USTRINGPARAM( "BASIC:1" ) ) );
+                    pView->SetName( OUString( "BASIC:1" ) );
             }
 
             if ( pView )
@@ -1240,7 +1240,7 @@ void SfxApplication::OfaExec_Impl( SfxRequest& rReq )
             OSL_TRACE("SfxApplication::OfaExec_Impl: case ScriptOrg");
             const SfxItemSet* pArgs = rReq.GetArgs();
             const SfxPoolItem* pItem;
-            String aLanguage;
+            OUString aLanguage;
             if(pArgs && SFX_ITEM_SET == pArgs->GetItemState(SID_SCRIPTORGANIZER, sal_False, &pItem) )
             {
                 aLanguage = ((SfxScriptOrganizerItem*)pItem)->getLanguage();

sfx2/source/appl/childwin.cxx
@@ -361,45 +361,45 @@ void SfxChildWindow::InitializeChildWinFactory_Impl( sal_uInt16 nId, SfxChildWin
     if ( aSeq.getLength() )
         aSeq[0].Value >>= aTmp;
 
-    String aWinData( aTmp );
+    OUString aWinData( aTmp );
     rInfo.aWinState = OUStringToOString(aWinOpt.GetWindowState(), RTL_TEXTENCODING_UTF8);
 
 
-    if ( aWinData.Len() )
+    if ( !aWinData.isEmpty() )
     {
         // Search for version ID
-        if ( aWinData.GetChar((sal_uInt16)0) != 0x0056 ) // 'V' = 56h
+        if ( aWinData[0] != 0x0056 ) // 'V' = 56h
             // A version ID, so do not use
             return;
 
         // Delete 'V'
-        aWinData.Erase(0,1);
+        aWinData = aWinData.copy(1);
 
         // Read version
         char cToken = ',';
-        sal_uInt16 nPos = aWinData.Search( cToken );
-        sal_uInt16 nActVersion = (sal_uInt16)aWinData.Copy( 0, nPos + 1 ).ToInt32();
+        sal_Int32 nPos = aWinData.indexOf( cToken );
+        sal_uInt16 nActVersion = (sal_uInt16)aWinData.copy( 0, nPos + 1 ).toInt32();
         if ( nActVersion != nVersion )
             return;
 
-        aWinData.Erase(0,nPos+1);
+        aWinData = aWinData.copy(nPos+1);
 
         // Load Visibility: is coded as a char
-        rInfo.bVisible = (aWinData.GetChar(0) == 0x0056); // 'V' = 56h
-        aWinData.Erase(0,1);
-        nPos = aWinData.Search( cToken );
-        if (nPos != STRING_NOTFOUND)
+        rInfo.bVisible = (aWinData[0] == 0x0056); // 'V' = 56h
+        aWinData = aWinData.copy(1);
+        nPos = aWinData.indexOf( cToken );
+        if (nPos != -1)
         {
-            sal_uInt16 nNextPos = aWinData.Search( cToken, 2 );
-            if ( nNextPos != STRING_NOTFOUND )
+            sal_Int32 nNextPos = aWinData.indexOf( cToken, 2 );
+            if ( nNextPos != -1 )
             {
                 // there is extra information
-                rInfo.nFlags = (sal_uInt16)aWinData.Copy( nPos+1, nNextPos - nPos - 1 ).ToInt32();
-                aWinData.Erase( nPos, nNextPos-nPos+1 );
+                rInfo.nFlags = (sal_uInt16)aWinData.copy( nPos+1, nNextPos - nPos - 1 ).toInt32();
+                aWinData = aWinData.replaceAt( nPos, nNextPos-nPos+1, "" );
                 rInfo.aExtraString = aWinData;
             }
             else
-                rInfo.nFlags = (sal_uInt16)aWinData.Copy( nPos+1 ).ToInt32();
+                rInfo.nFlags = (sal_uInt16)aWinData.copy( nPos+1 ).toInt32();
         }
     }
 }

sfx2/source/appl/fileobj.cxx
@@ -309,7 +309,7 @@ sal_Bool SvFileObject::GetGraphic_Impl( Graphic& rGrf, SvStream* pStream )
                             ? rGF.GetImportFormatNumber( sFilter )
                             : GRFILTER_FORMAT_DONTKNOW;
 
-    String aEmptyStr;
+    OUString aEmptyStr;
     int nRes;
 
     // To avoid that a native link is created
@@ -378,10 +378,10 @@ sal_Bool SvFileObject::GetGraphic_Impl( Graphic& rGrf, SvStream* pStream )
         If the URL doesn't denote a valid (existent and accessible) file, the
         request is silently dropped.
 */
-String impl_getFilter( const String& _rURL )
+OUString impl_getFilter( const OUString& _rURL )
 {
-    String sFilter;
-    if ( _rURL.Len() == 0 )
+    OUString sFilter;
+    if ( _rURL.isEmpty() )
         return sFilter;
 
     try
@@ -458,7 +458,7 @@ void SvFileObject::Edit( Window* pParent, sfx2::SvBaseLink* pLink, const Link& r
                 Application::SetDefDialogParent( pParent );
 
                 ::sfx2::FileDialogHelper & rFileDlg =
-                    pLink->GetInsertFileDialog( String() );
+                    pLink->GetInsertFileDialog( OUString() );
                 rFileDlg.StartExecuteModal(
                         LINK( this, SvFileObject, DialogClosedHdl ) );
             }
@@ -470,7 +470,7 @@ void SvFileObject::Edit( Window* pParent, sfx2::SvBaseLink* pLink, const Link& r
                 pOldParent = Application::GetDefDialogParent();
                 Application::SetDefDialogParent( pParent );
 
-                String sFactory;
+                OUString sFactory;
                 SfxObjectShell* pShell = pLink->GetLinkManager()->GetPersist();
                 if ( pShell )
                     sFactory = pShell->GetFactory().GetFactoryName();
@@ -533,17 +533,17 @@ IMPL_STATIC_LINK( SvFileObject, DelMedium_Impl, SfxMediumRef*, pDelMed )
 
 IMPL_LINK( SvFileObject, DialogClosedHdl, sfx2::FileDialogHelper*, _pFileDlg )
 {
-    String sFile;
+    OUString sFile;
     Application::SetDefDialogParent( pOldParent );
 
     if ( FILETYPE_TEXT == nType || FILETYPE_OBJECT == nType )
     {
         if ( _pFileDlg && _pFileDlg->GetError() == ERRCODE_NONE )
         {
-            String sURL( _pFileDlg->GetPath() );
+            OUString sURL( _pFileDlg->GetPath() );
             sFile = sURL;
-            sFile += ::sfx2::cTokenSeparator;
-            sFile += ::sfx2::cTokenSeparator;
+            sFile += OUString(::sfx2::cTokenSeparator);
+            sFile += OUString(::sfx2::cTokenSeparator);
             sFile += impl_getFilter( sURL );
         }
     }

sfx2/source/appl/helpdispatch.cxx
@@ -59,7 +59,7 @@ void SAL_CALL HelpDispatch_Impl::dispatch(
 
     // search for a keyword (dispatch from the basic ide)
     sal_Bool bHasKeyword = sal_False;
-    String sKeyword;
+    OUString sKeyword;
     const PropertyValue* pBegin = aArgs.getConstArray();
     const PropertyValue* pEnd   = pBegin + aArgs.getLength();
     for ( ; pBegin != pEnd; ++pBegin )
@@ -69,8 +69,8 @@ void SAL_CALL HelpDispatch_Impl::dispatch(
             OUString sHelpKeyword;
             if ( ( ( *pBegin ).Value >>= sHelpKeyword ) && !sHelpKeyword.isEmpty() )
             {
-                sKeyword = String( sHelpKeyword );
-                bHasKeyword = ( sKeyword.Len() > 0 );
+                sKeyword = sHelpKeyword;
+                bHasKeyword = !sKeyword.isEmpty();
                 break;
             }
         }

commit fd29f726cd2233ad7579a3ecedca0e21d1d4ae5c

fdo#72037: Fix comparison of indexOf values after string conversion.
regression from 30d850a9d6a91bd351338dc465e70fe74f891d7c

Change-Id: I369a9a342984084f5269bcd9d829c42de9ac4a23

sfx2/source/appl/appuno.cxx
@@ -1855,7 +1855,7 @@ ErrCode SfxMacroLoader::loadMacro( const OUString& rURL, com::sun::star::uno::An
     ErrCode nErr = ERRCODE_NONE;
 
     // should a macro function be executed ( no direct API call)?
-    if ( -1 != nHashPos && nHashPos < nArgsPos )
+    if ( -1 != nHashPos && ( -1 == nArgsPos || nHashPos < nArgsPos ) )
     {
         // find BasicManager
         SfxObjectShell* pDoc = NULL;