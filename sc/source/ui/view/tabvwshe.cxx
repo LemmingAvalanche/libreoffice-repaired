LemmingAvalanche/libreoffice-repaired

commit bc74bbe92d2a6badd6678ef629db72e37f88f21f

Integrate branch of IAccessible2
Conflicts:
    everything

Change-Id: I3d8cf956f511a0d953a1cefe83b6ef987806da25

sc/source/ui/view/tabvwshe.cxx
@@ -38,6 +38,7 @@
 #include "editsh.hxx"
 #include "dociter.hxx"
 #include "inputhdl.hxx"
+#include <svx/srchdlg.hxx>
 #include "document.hxx"
 
 //==================================================================
@@ -235,7 +236,23 @@ void ScTabViewShell::ExecSearch( SfxRequest& rReq )
                     const SvxSearchItem* pSearchItem = (const SvxSearchItem*) pItem;
 
                     ScGlobal::SetSearchItem( *pSearchItem );
-                    SearchAndReplace( pSearchItem, sal_True, rReq.IsAPI() );
+                    sal_Bool bSuccess = SearchAndReplace( pSearchItem, sal_True, rReq.IsAPI() );
+                    SvxSearchDialog* pSearchDlg =
+                        ((SvxSearchDialog*)(SfxViewFrame::Current()->GetChildWindow(
+                        SvxSearchDialogWrapper::GetChildWindowId())->GetWindow()));
+                    if( pSearchDlg )
+                    {
+                        ScTabView* pTabView = GetViewData()->GetView();
+                        if( pTabView )
+                        {
+                            Window* pWin = pTabView->GetActiveWin();
+                            if( pWin )
+                            {
+                                pSearchDlg->SetDocWin( pWin );
+                                pSearchDlg->SetSrchFlag( bSuccess );
+                            }
+                        }
+                    }
                     rReq.Done();
                 }
             }
@@ -287,6 +304,22 @@ void ScTabViewShell::ExecSearch( SfxRequest& rReq )
                             rReq.IsAPI() ? SFX_CALLMODE_API|SFX_CALLMODE_SYNCHRON :
                                             SFX_CALLMODE_STANDARD,
                             &aSearchItem, 0L );
+                    SvxSearchDialog* pSearchDlg =
+                        ((SvxSearchDialog*)(SfxViewFrame::Current()->GetChildWindow(
+                        SvxSearchDialogWrapper::GetChildWindowId())->GetWindow()));
+                    if( pSearchDlg )
+                    {
+                        ScTabView* pTabView = GetViewData()->GetView();
+                        if( pTabView )
+                        {
+                            Window* pWin = pTabView->GetActiveWin();
+                            if( pWin )
+                            {
+                                pSearchDlg->SetDocWin( pWin );
+                                pSearchDlg->SetSrchFlag();
+                            }
+                        }
+                    }
                 }
                 else
                 {

sc/inc/chgtrack.hxx
@@ -25,9 +25,10 @@
 #include <set>
 #include <stack>
 
+#include <tools/color.hxx>
 #include <tools/datetime.hxx>
+#include <tools/link.hxx>
 #include <tools/mempool.hxx>
-#include "tools/link.hxx"
 #include <unotools/options.hxx>
 #include "global.hxx"
 #include "bigrange.hxx"
@@ -37,6 +38,26 @@
 
 class ScDocument;
 class ScFormulaCell;
+class ScChangeAction;
+class ScChangeTrack;
+class ScAppOptions;
+
+class ScActionColorChanger
+{
+private:
+    const ScAppOptions&     rOpt;
+    const std::set<OUString>& rUsers;
+    OUString                aLastUserName;
+    sal_uInt16              nLastUserIndex;
+    ColorData               nColor;
+
+
+public:
+    ScActionColorChanger( const ScChangeTrack& rTrack );
+    ~ScActionColorChanger() {}
+    void        Update( const ScChangeAction& rAction );
+    ColorData   GetColor() const    { return nColor; }
+};
 
 enum ScChangeActionType
 {

sc/inc/detfunc.hxx
@@ -153,6 +153,7 @@ public:
     static ColorData GetCommentColor();
     static void InitializeColors();
     static sal_Bool IsColorsInitialized();
+    static void AppendChangTrackNoteSeparator(OUString &str);
 };
 
 

sc/inc/document.hxx
@@ -237,6 +237,7 @@ friend class ScQueryCellIterator;
 friend class ScHorizontalCellIterator;
 friend class ScHorizontalAttrIterator;
 friend class ScDocAttrIterator;
+friend class ScAccessibleTableBase;
 friend class ScAttrRectIterator;
 friend class ScDocShell;
 friend class ScDocRowHeightUpdater;
@@ -438,7 +439,20 @@ private:
 
     bool                mbUseEmbedFonts;
 
+    OUString msDocAccTitle;
+
 public:
+    void setDocAccTitle( const OUString& rTitle ) { msDocAccTitle = rTitle; }
+    const OUString getDocAccTitle() const { return msDocAccTitle; }
+
+private:
+    bool mbReadOnly;
+
+public:
+    void setDocReadOnly(bool b){ mbReadOnly = b; }
+    bool getDocReadOnly() const { return mbReadOnly; }
+    sal_Bool IsCellInChangeTrack(const ScAddress &cell,Color *pColCellBoder);
+    void GetCellChangeTrackNote(const ScAddress &cell, OUString &strTrackText, sal_Bool &pbLeftEdge);
     bool              IsUsingEmbededFonts() { return mbUseEmbedFonts; }
     void              SetIsUsingEmbededFonts( bool bUse ) { mbUseEmbedFonts = bUse; }
     SC_DLLPUBLIC sal_uLong          GetCellCount() const;       // all cells

sc/inc/drwlayer.hxx
@@ -197,6 +197,9 @@ public:
 
     static ScMacroInfo* GetMacroInfo( SdrObject* pObj, sal_Bool bCreate = false );
 
+    virtual ImageMap* GetImageMapForObject(SdrObject* pObj);
+    virtual sal_Int32 GetHyperlinkCount(SdrObject* pObj);
+
 private:
     static SfxObjectShell* pGlobalDrawPersist;          // for AllocModel
 public:

sc/inc/table.hxx
@@ -1067,6 +1067,9 @@ private:
         SCROW mnCurRow;
         SCROW mnUBound;
     };
+
+public :
+    ScColumn* GetColumnByIndex(sal_Int32 index);
 };
 
 

sc/source/core/data/documen2.cxx
@@ -82,6 +82,9 @@
 #include "recursionhelper.hxx"
 #include "lookupcache.hxx"
 #include "externalrefmgr.hxx"
+#include "appoptio.hxx"
+#include "scmod.hxx"
+#include "../../ui/inc/viewutil.hxx"
 #include "tabprotection.hxx"
 #include "formulaparserpool.hxx"
 #include "clipparam.hxx"
@@ -216,7 +219,8 @@ ScDocument::ScDocument( ScDocumentMode eMode, SfxObjectShell* pDocShell ) :
         mbStreamValidLocked( false ),
         mbUserInteractionEnabled(true),
         mnNamedRangesLockCount(0),
-        mbUseEmbedFonts(false)
+        mbUseEmbedFonts(false),
+        mbReadOnly(false)
 {
     SetStorageGrammar( formula::FormulaGrammar::GRAM_STORAGE_DEFAULT);
 
@@ -1213,6 +1217,161 @@ void ScDocument::ClearLookupCaches()
 {
     if( pLookupCacheMapImpl )
         pLookupCacheMapImpl->clear();
+}
+
+sal_Bool ScDocument::IsCellInChangeTrack(const ScAddress &cell,Color *pColCellBoder)
+{
+    ScChangeTrack* pTrack = GetChangeTrack();
+    ScChangeViewSettings* pSettings = GetChangeViewSettings();
+    if ( !pTrack || !pTrack->GetFirst() || !pSettings || !pSettings->ShowChanges() )
+        return sal_False;           // nix da oder abgeschaltet
+    ScActionColorChanger aColorChanger(*pTrack);
+    //  Clipping passiert von aussen
+    //! ohne Clipping, nur betroffene Zeilen painten ??!??!?
+    const ScChangeAction* pAction = pTrack->GetFirst();
+    while (pAction)
+    {
+        ScChangeActionType eType;
+        if ( pAction->IsVisible() )
+        {
+            eType = pAction->GetType();
+            const ScBigRange& rBig = pAction->GetBigRange();
+            if ( rBig.aStart.Tab() == cell.Tab())
+            {
+                ScRange aRange = rBig.MakeRange();
+                if ( eType == SC_CAT_DELETE_ROWS )
+                    aRange.aEnd.SetRow( aRange.aStart.Row() );
+                else if ( eType == SC_CAT_DELETE_COLS )
+                    aRange.aEnd.SetCol( aRange.aStart.Col() );
+                if (ScViewUtil::IsActionShown( *pAction, *pSettings, *this ) )
+                {
+                    if (aRange.In(cell))
+                    {
+                        if (pColCellBoder != NULL)
+                        {
+                            aColorChanger.Update( *pAction );
+                            Color aColor( aColorChanger.GetColor() );
+                            *pColCellBoder = aColor;
+                        }
+                        return sal_True;
+                    }
+                }
+            }
+            if ( eType == SC_CAT_MOVE &&
+                ((const ScChangeActionMove*)pAction)->
+                GetFromRange().aStart.Tab() == cell.Col() )
+            {
+                ScRange aRange = ((const ScChangeActionMove*)pAction)->
+                    GetFromRange().MakeRange();
+                if (ScViewUtil::IsActionShown( *pAction, *pSettings, *this ) )
+                {
+                    if (aRange.In(cell))
+                    {
+                        if (pColCellBoder != NULL)
+                        {
+                            aColorChanger.Update( *pAction );
+                            Color aColor( aColorChanger.GetColor() );
+                            *pColCellBoder = aColor;
+                        }
+                        return sal_True;
+                    }
+                }
+            }
+        }
+        pAction = pAction->GetNext();
+    }
+    return sal_False;
+}
+
+void ScDocument::GetCellChangeTrackNote( const ScAddress &aCellPos, OUString &aTrackText,sal_Bool &bLeftEdge)
+{
+    aTrackText = OUString();
+    //  Change-Tracking
+    ScChangeTrack* pTrack = GetChangeTrack();
+    ScChangeViewSettings* pSettings = GetChangeViewSettings();
+    if ( pTrack && pTrack->GetFirst() && pSettings && pSettings->ShowChanges())
+    {
+        const ScChangeAction* pFound = NULL;
+        const ScChangeAction* pFoundContent = NULL;
+        const ScChangeAction* pFoundMove = NULL;
+        long nModified = 0;
+        const ScChangeAction* pAction = pTrack->GetFirst();
+        while (pAction)
+        {
+            if ( pAction->IsVisible() &&
+                 ScViewUtil::IsActionShown( *pAction, *pSettings, *this ) )
+            {
+                ScChangeActionType eType = pAction->GetType();
+                const ScBigRange& rBig = pAction->GetBigRange();
+                if ( rBig.aStart.Tab() == aCellPos.Tab())
+                {
+                    ScRange aRange = rBig.MakeRange();
+                    if ( eType == SC_CAT_DELETE_ROWS )
+                        aRange.aEnd.SetRow( aRange.aStart.Row() );
+                    else if ( eType == SC_CAT_DELETE_COLS )
+                        aRange.aEnd.SetCol( aRange.aStart.Col() );
+                    if ( aRange.In( aCellPos ) )
+                    {
+                        pFound = pAction;       // der letzte gewinnt
+                        switch ( eType )
+                        {
+                            case SC_CAT_CONTENT :
+                                pFoundContent = pAction;
+                            break;
+                            case SC_CAT_MOVE :
+                                pFoundMove = pAction;
+                            break;
+                            default:
+                                break;
+                        }
+                        ++nModified;
+                    }
+                }
+                if ( eType == SC_CAT_MOVE )
+                {
+                    ScRange aRange =
+                        ((const ScChangeActionMove*)pAction)->
+                        GetFromRange().MakeRange();
+                    if ( aRange.In( aCellPos ) )
+                    {
+                        pFound = pAction;
+                        ++nModified;
+                    }
+                }
+            }
+            pAction = pAction->GetNext();
+        }
+        if ( pFound )
+        {
+            if ( pFoundContent && pFound->GetType() != SC_CAT_CONTENT )
+                pFound = pFoundContent;     // Content gewinnt
+            if ( pFoundMove && pFound->GetType() != SC_CAT_MOVE &&
+                    pFoundMove->GetActionNumber() >
+                    pFound->GetActionNumber() )
+                pFound = pFoundMove;        // Move gewinnt
+            //  bei geloeschten Spalten: Pfeil auf die linke Seite der Zelle
+            if ( pFound->GetType() == SC_CAT_DELETE_COLS )
+                bLeftEdge = sal_True;
+            DateTime aDT = pFound->GetDateTime();
+            aTrackText  = pFound->GetUser();
+            aTrackText += ", ";
+            aTrackText += ScGlobal::pLocaleData->getDate(aDT);
+            aTrackText += " ";
+            aTrackText += ScGlobal::pLocaleData->getTime(aDT);
+            aTrackText += ":\n";
+            OUString aComStr = pFound->GetComment();
+            if(!aComStr.isEmpty())
+            {
+                aTrackText += aComStr;
+                aTrackText += "\n( ";
+            }
+            pFound->GetDescription( aTrackText, this );
+            if (!aComStr.isEmpty())
+            {
+                aTrackText += ")";
+            }
+        }
+    }
 }
 
 void ScDocument::SetPreviewFont( SfxItemSet* pFont )

sc/source/core/data/document.cxx
@@ -178,7 +178,6 @@ void ScDocument::MakeTable( SCTAB nTab,bool _bNeedsNameCheck )
     }
 }
 
-
 bool ScDocument::HasTable( SCTAB nTab ) const
 {
     if (ValidTab(nTab) && nTab < static_cast<SCTAB>(maTabs.size()))

commit 73bb68d83816f55c43a2936d3aedba34159788a1

fixed crash when searching via Find Text bar
Introduced with bc74bbe92d2a6badd6678ef629db72e37f88f21f
Integrate branch of IAccessible2

Find Text bar is not the search dialog, so there is no child window
unless the Find & Replace dialog is also open.

Change-Id: Ice230cb2c05534c754fefc55bb8d8b189724d328

sc/source/ui/view/tabvwshe.cxx
@@ -237,19 +237,22 @@ void ScTabViewShell::ExecSearch( SfxRequest& rReq )
 
                     ScGlobal::SetSearchItem( *pSearchItem );
                     sal_Bool bSuccess = SearchAndReplace( pSearchItem, sal_True, rReq.IsAPI() );
-                    SvxSearchDialog* pSearchDlg =
-                        ((SvxSearchDialog*)(SfxViewFrame::Current()->GetChildWindow(
-                        SvxSearchDialogWrapper::GetChildWindowId())->GetWindow()));
-                    if( pSearchDlg )
+                    SfxChildWindow* pChildWindow = SfxViewFrame::Current()->GetChildWindow(
+                            SvxSearchDialogWrapper::GetChildWindowId());
+                    if (pChildWindow)
                     {
-                        ScTabView* pTabView = GetViewData()->GetView();
-                        if( pTabView )
+                        SvxSearchDialog* pSearchDlg = (SvxSearchDialog*)(pChildWindow->GetWindow());
+                        if( pSearchDlg )
                         {
-                            Window* pWin = pTabView->GetActiveWin();
-                            if( pWin )
+                            ScTabView* pTabView = GetViewData()->GetView();
+                            if( pTabView )
                             {
-                                pSearchDlg->SetDocWin( pWin );
-                                pSearchDlg->SetSrchFlag( bSuccess );
+                                Window* pWin = pTabView->GetActiveWin();
+                                if( pWin )
+                                {
+                                    pSearchDlg->SetDocWin( pWin );
+                                    pSearchDlg->SetSrchFlag( bSuccess );
+                                }
                             }
                         }
                     }