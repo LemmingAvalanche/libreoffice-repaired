LemmingAvalanche/libreoffice-repaired

commit 6ac34c57e3a6ee11e328ebe3f75ac082508c2a41

don't blow up on .odt export with !hasValue footers/headers
Change-Id: I2507c23b7b17b9172f720548d32cdfe299d4fa20

xmloff/source/text/XMLTextMasterPageExport.cxx
@@ -127,20 +127,21 @@ void XMLTextMasterPageExport::exportMasterPageContent(
     else
     {
         aAny = rPropSet->getPropertyValue( sHeaderOn );
-        sal_Bool bHeader = *(sal_Bool *)aAny.getValue();
+        sal_Bool bHeader = sal_False;
+        aAny >>= bHeader;
 
         sal_Bool bHeaderFirst = sal_False;
         if( bHeader )
         {
             aAny = rPropSet->getPropertyValue( sFirstShareContent );
-            bHeaderFirst = !*(sal_Bool *)aAny.getValue();
+            aAny >>= bHeaderFirst;
         }
 
         sal_Bool bHeaderLeft = sal_False;
         if( bHeader )
         {
             aAny = rPropSet->getPropertyValue( sHeaderShareContent );
-            bHeaderLeft = !*(sal_Bool *)aAny.getValue();
+            aAny >>= bHeaderLeft;
         }
 
         if( xHeaderText.is() )
@@ -174,20 +175,21 @@ void XMLTextMasterPageExport::exportMasterPageContent(
         }
 
         aAny = rPropSet->getPropertyValue( sFooterOn );
-        sal_Bool bFooter = *(sal_Bool *)aAny.getValue();
+        sal_Bool bFooter = sal_False;
+        aAny >>= bFooter;
 
         sal_Bool bFooterFirst = sal_False;
         if( bFooter )
         {
             aAny = rPropSet->getPropertyValue( sFirstShareContent );
-            bFooterFirst = !*(sal_Bool *)aAny.getValue();
+            aAny >>= bFooterFirst;
         }
 
         sal_Bool bFooterLeft = sal_False;
         if( bFooter )
         {
             aAny = rPropSet->getPropertyValue( sFooterShareContent );
-            bFooterLeft = !*(sal_Bool *)aAny.getValue();
+            aAny >>= bFooterLeft;
         }
 
         if( xFooterText.is() )

commit 865ac132e94aefa26ef4be5f134eb710bd205d3f

fdo#72850: ODF export: don't export spurious style:display="false"
... on style:header-left, style:header-first, style:footer-left,
style:footer-first.

(regression from 6ac34c57e3a6ee11e328ebe3f75ac082508c2a41)

Change-Id: I48c51fcd2b07ae8b0e3ec2c1087a388c6900b366

xmloff/source/text/XMLTextHeaderFooterContext.cxx
@@ -54,6 +54,7 @@ XMLTextHeaderFooterContext::XMLTextHeaderFooterContext( SvXMLImport& rImport, sa
     bLeft( bLft ),
     bFirst( bFrst )
 {
+    // NOTE: if this ever handles XML_DISPLAY attr then beware of fdo#72850 !
     if( bLeft || bFirst )
     {
         Any aAny;

xmloff/source/text/XMLTextMasterPageExport.cxx
@@ -128,18 +128,18 @@ void XMLTextMasterPageExport::exportMasterPageContent(
         sal_Bool bHeader = sal_False;
         aAny >>= bHeader;
 
-        sal_Bool bHeaderFirst = sal_False;
+        sal_Bool bHeaderFirstShared = sal_False;
         if( bHeader )
         {
             aAny = rPropSet->getPropertyValue( sFirstShareContent );
-            aAny >>= bHeaderFirst;
+            aAny >>= bHeaderFirstShared;
         }
 
-        sal_Bool bHeaderLeft = sal_False;
+        sal_Bool bHeaderLeftShared = sal_False;
         if( bHeader )
         {
             aAny = rPropSet->getPropertyValue( sHeaderShareContent );
-            aAny >>= bHeaderLeft;
+            aAny >>= bHeaderLeftShared;
         }
 
         if( xHeaderText.is() )
@@ -154,7 +154,7 @@ void XMLTextMasterPageExport::exportMasterPageContent(
 
         if( xHeaderTextFirst.is() && xHeaderTextFirst != xHeaderText )
         {
-            if( !bHeaderFirst )
+            if (bHeaderFirstShared)
                 GetExport().AddAttribute( XML_NAMESPACE_STYLE,
                                           XML_DISPLAY, XML_FALSE );
             SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_STYLE,
@@ -164,7 +164,7 @@ void XMLTextMasterPageExport::exportMasterPageContent(
 
         if( xHeaderTextLeft.is() && xHeaderTextLeft != xHeaderText )
         {
-            if( !bHeaderLeft )
+            if (bHeaderLeftShared)
                 GetExport().AddAttribute( XML_NAMESPACE_STYLE,
                                           XML_DISPLAY, XML_FALSE );
             SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_STYLE,
@@ -176,18 +176,18 @@ void XMLTextMasterPageExport::exportMasterPageContent(
         sal_Bool bFooter = sal_False;
         aAny >>= bFooter;
 
-        sal_Bool bFooterFirst = sal_False;
+        sal_Bool bFooterFirstShared = sal_False;
         if( bFooter )
         {
             aAny = rPropSet->getPropertyValue( sFirstShareContent );
-            aAny >>= bFooterFirst;
+            aAny >>= bFooterFirstShared;
         }
 
-        sal_Bool bFooterLeft = sal_False;
+        sal_Bool bFooterLeftShared = sal_False;
         if( bFooter )
         {
             aAny = rPropSet->getPropertyValue( sFooterShareContent );
-            aAny >>= bFooterLeft;
+            aAny >>= bFooterLeftShared;
         }
 
         if( xFooterText.is() )
@@ -202,7 +202,7 @@ void XMLTextMasterPageExport::exportMasterPageContent(
 
         if( xFooterTextFirst.is() && xFooterTextFirst != xFooterText )
         {
-            if( !bFooterFirst )
+            if (bFooterFirstShared)
                 GetExport().AddAttribute( XML_NAMESPACE_STYLE,
                                           XML_DISPLAY, XML_FALSE );
             SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_STYLE,
@@ -212,7 +212,7 @@ void XMLTextMasterPageExport::exportMasterPageContent(
 
         if( xFooterTextLeft.is() && xFooterTextLeft != xFooterText )
         {
-            if( !bFooterLeft )
+            if (bFooterLeftShared)
                 GetExport().AddAttribute( XML_NAMESPACE_STYLE,
                                           XML_DISPLAY, XML_FALSE );
             SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_STYLE,