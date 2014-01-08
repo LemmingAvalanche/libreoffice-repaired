LibreOffice/core

123792: complete annotations on text ranges feature
- rely annotations on text ranges on new annotation marks
- support arbitrary text ranges for annotations
- fix undo/redo regarding annotations an text ranges
- support annotations on overlapping text ranges
- fix *.docx import for annotations on overlapping text ranges
- fix ODF import of annotations on text ranges

cherry-picked from branch ooxml-osba

sw/source/filter/html/htmlatr.cxx
@@ -2482,8 +2482,9 @@ Writer& OutHTML_SwTxtNode( Writer& rWrt, const SwCntntNode& rNode )
             aEndPosLst.OutEndAttrs( rHTMLWrt, nStrPos + nOffset );
 
             nAttrPos++;
-            if( RES_TXTATR_FIELD == pHt->Which() )      // Felder nicht
-                continue;                               // ausgeben
+            if( pHt->Which() == RES_TXTATR_FIELD
+                || pHt->Which() == RES_TXTATR_ANNOTATION )
+                continue;
 
             if ( pHt->End() && !pHt->HasDummyChar() )
             {
@@ -2577,8 +2578,7 @@ Writer& OutHTML_SwTxtNode( Writer& rWrt, const SwCntntNode& rNode )
                     else
                     {
                         // Hints ohne-Ende werden als letztes ausgebeben
-                        ASSERT( !pTxtHt,
-                                "Wieso gibt es da schon ein Attribut ohne Ende?" );
+                        ASSERT( !pTxtHt, "Wieso gibt es da schon ein Attribut ohne Ende?" );
                         if( rHTMLWrt.nTxtAttrsToIgnore>0 )
                         {
                             rHTMLWrt.nTxtAttrsToIgnore--;
@@ -2587,10 +2587,12 @@ Writer& OutHTML_SwTxtNode( Writer& rWrt, const SwCntntNode& rNode )
                         {
                             pTxtHt = pHt;
                             sal_uInt16 nFldWhich;
-                            if( RES_TXTATR_FIELD != pHt->Which() ||
-                                ( RES_POSTITFLD != (nFldWhich = ((const SwFmtFld&)pHt->GetAttr()).GetField()->Which()) &&
-                                RES_SCRIPTFLD != nFldWhich ) )
+                            if( RES_TXTATR_FIELD != pHt->Which()
+                                || ( RES_POSTITFLD != (nFldWhich = ((const SwFmtFld&)pHt->GetAttr()).GetField()->Which())
+                                     && RES_SCRIPTFLD != nFldWhich ) )
+                            {
                                 bWriteBreak = sal_False;
+                            }
                         }
                         bOutChar = sal_False;       // keine 255 ausgeben
                     }
@@ -3418,7 +3420,7 @@ SwAttrFnTab aHTMLAttrFnTab = {
 /* RES_TXTATR_FIELD */              OutHTML_SwFmtFld,
 /* RES_TXTATR_FLYCNT */             OutHTML_SwFlyCnt,
 /* RES_TXTATR_FTN */                OutHTML_SwFmtFtn,
-/* RES_TXTATR_DUMMY4 */             0,
+/* RES_TXTATR_ANNOTATION */         OutHTML_SwFmtFld,
 /* RES_TXTATR_DUMMY3 */             0,
 /* RES_TXTATR_DUMMY1 */             0, // Dummy:
 /* RES_TXTATR_DUMMY2 */             0, // Dummy:

LemmingAvalanche/libreoffice-repaired

(cherry picked from commit ebd2922ebd3cf2d3899fb80780d38f367197aadf)
Conflicts:
	sw/Library_sw.mk
	sw/inc/IDocumentMarkAccess.hxx
	sw/inc/SidebarWin.hxx
	sw/inc/doc.hxx
	sw/inc/docufld.hxx
	sw/inc/hintids.hxx
	sw/inc/ndhints.hxx
	sw/inc/txatbase.hxx
	sw/inc/unobookmark.hxx
	sw/inc/unofield.hxx
	sw/inc/unoport.hxx
	sw/source/core/access/accpara.cxx
	sw/source/core/bastyp/init.cxx
	sw/source/core/crsr/bookmrk.cxx
	sw/source/core/crsr/findtxt.cxx
	sw/source/core/doc/dbgoutsw.cxx
	sw/source/core/doc/docbm.cxx
	sw/source/core/doc/doccorr.cxx
	sw/source/core/doc/docdde.cxx
	sw/source/core/doc/docedt.cxx
	sw/source/core/doc/docfld.cxx
	sw/source/core/doc/doclay.cxx
	sw/source/core/doc/tblrwcl.cxx
	sw/source/core/docnode/ndcopy.cxx
	sw/source/core/docnode/nodes.cxx
	sw/source/core/docnode/section.cxx
	sw/source/core/edit/edfld.cxx
	sw/source/core/fields/docufld.cxx
	sw/source/core/fields/postithelper.cxx
	sw/source/core/fields/reffld.cxx
	sw/source/core/inc/MarkManager.hxx
	sw/source/core/inc/crossrefbookmark.hxx
	sw/source/core/text/inftxt.cxx
	sw/source/core/text/itratr.cxx
	sw/source/core/text/txtfrm.cxx
	sw/source/core/txtnode/atrfld.cxx
	sw/source/core/txtnode/ndtxt.cxx
	sw/source/core/txtnode/thints.cxx
	sw/source/core/undo/undel.cxx
	sw/source/core/undo/undobj.cxx
	sw/source/core/unocore/unofield.cxx
	sw/source/core/unocore/unoport.cxx
	sw/source/core/unocore/unoportenum.cxx
	sw/source/filter/html/htmlatr.cxx
	sw/source/filter/html/htmlgrin.cxx
	sw/source/filter/html/wrthtml.cxx
	sw/source/filter/writer/writer.cxx
	sw/source/filter/ww1/fltshell.cxx
	sw/source/filter/ww8/wrtw8nds.cxx
	sw/source/filter/ww8/ww8par.cxx
	sw/source/ui/dialog/uiregionsw.cxx
	sw/source/ui/dochdl/swdtflvr.cxx
	sw/source/ui/docvw/PostItMgr.cxx
	sw/source/ui/docvw/SidebarWin.cxx
	sw/source/ui/fldui/fldmgr.cxx
	sw/source/ui/fldui/fldref.cxx
	sw/source/ui/inc/fldmgr.hxx
	sw/source/ui/inc/wrtsh.hxx
	sw/source/ui/shells/textfld.cxx
	sw/source/ui/uiview/view2.cxx
	sw/source/ui/utlui/navipi.cxx
	sw/s

sw/source/filter/html/htmlatr.cxx
@@ -2385,8 +2385,9 @@ Writer& OutHTML_SwTxtNode( Writer& rWrt, const SwCntntNode& rNode )
             aEndPosLst.OutEndAttrs( rHTMLWrt, nStrPos + nOffset );
 
             nAttrPos++;
-            if( RES_TXTATR_FIELD == pHt->Which() )      // Felder nicht
-                continue;                               // ausgeben
+            if( pHt->Which() == RES_TXTATR_FIELD
+                || pHt->Which() == RES_TXTATR_ANNOTATION )
+                continue;
 
             if ( pHt->End() && !pHt->HasDummyChar() )
             {
@@ -2464,8 +2465,7 @@ Writer& OutHTML_SwTxtNode( Writer& rWrt, const SwCntntNode& rNode )
                     else
                     {
                         // Hints ohne-Ende werden als letztes ausgebeben
-                        OSL_ENSURE( !pTxtHt,
-                                "Wieso gibt es da schon ein Attribut ohne Ende?" );
+                        OSL_ENSURE( !pTxtHt, "Wieso gibt es da schon ein Attribut ohne Ende?" );
                         if( rHTMLWrt.nTxtAttrsToIgnore>0 )
                         {
                             rHTMLWrt.nTxtAttrsToIgnore--;
@@ -2474,10 +2474,12 @@ Writer& OutHTML_SwTxtNode( Writer& rWrt, const SwCntntNode& rNode )
                         {
                             pTxtHt = pHt;
                             sal_uInt16 nFldWhich;
-                            if( RES_TXTATR_FIELD != pHt->Which() ||
-                                ( RES_POSTITFLD != (nFldWhich = ((const SwFmtFld&)pHt->GetAttr()).GetField()->Which()) &&
-                                RES_SCRIPTFLD != nFldWhich ) )
+                            if( RES_TXTATR_FIELD != pHt->Which()
+                                || ( RES_POSTITFLD != (nFldWhich = ((const SwFmtFld&)pHt->GetAttr()).GetField()->Which())
+                                     && RES_SCRIPTFLD != nFldWhich ) )
+                            {
                                 bWriteBreak = sal_False;
+                            }
                         }
                         bOutChar = sal_False;       // keine 255 ausgeben
                     }
@@ -3293,7 +3295,7 @@ SwAttrFnTab aHTMLAttrFnTab = {
 /* RES_TXTATR_FIELD */              OutHTML_SwFmtFld,
 /* RES_TXTATR_FLYCNT */             OutHTML_SwFlyCnt,
 /* RES_TXTATR_FTN */                OutHTML_SwFmtFtn,
-/* RES_TXTATR_DUMMY4 */             0,
+/* RES_TXTATR_ANNOTATION */         OutHTML_SwFmtFld,
 /* RES_TXTATR_DUMMY3 */             0,
 /* RES_TXTATR_DUMMY1 */             0, // Dummy:
 /* RES_TXTATR_DUMMY2 */             0, // Dummy: