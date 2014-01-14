LemmingAvalanche/libreoffice-repaired

commit 3827ea8e5970bcf954033ffd340ac3a1d1b5efaa

fdo#72488: Broken text when showing visible space
Turning on showing nonprinting characters replaces the space with bullet
character, but still draws the text with the original kern array, this
works fine until there are ligatures involving the space character as
the number of glyphs after replacing the space with the bullet will be
different and the kern array will be completely off.

This is a hack that gives up on replacing the space with a bullet when
its width is zero, not sure if it would interfere with other legitimate
uses.

Change-Id: I5ed17132ead7cd141a4e8b0372e37541c163be30
Reviewed-on: https://gerrit.libreoffice.org/6995
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sw/source/core/txtnode/fntcache.cxx
@@ -1569,7 +1569,13 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
 
             for( sal_Int32 i = 0; i < aStr.getLength(); ++i )
                 if( CH_BLANK == aStr[ i ] )
-                    aStr = aStr.replaceAt(i, 1, OUString(CH_BULLET));
+                {
+                    /* fdo#72488 Hack: try to see if the space is zero width
+                     * and don't bother with inserting a bullet in this case.
+                     */
+                    if (pKernArray[i + nCopyStart] != pKernArray[ i + nCopyStart + 1])
+                        aStr = aStr.replaceAt(i, 1, OUString(CH_BULLET));
+                }
         }
 
         sal_Int32 nCnt = rInf.GetText().getLength();

commit be027bb57f6d6c13f473d22a55c6a3d59e6f3d53

fdo#73095: fix invalid access in SwFntObj::DrawText()
aStr may be 1 larger than pKernArray.
Trivial fix by checking the largest index; not sure if it would be a
good idea to allocate pKernArray with the larger size in the first
place, but that would be a bigger change...
(regression from 3827ea8e5970bcf954033ffd340ac3a1d1b5efaa)

Change-Id: Ia33feab001c34e85066b7596d8873f41588984e9

sw/source/core/txtnode/fntcache.cxx
@@ -1573,8 +1573,11 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
                     /* fdo#72488 Hack: try to see if the space is zero width
                      * and don't bother with inserting a bullet in this case.
                      */
-                    if (pKernArray[i + nCopyStart] != pKernArray[ i + nCopyStart + 1])
+                    if ((i + nCopyStart + 1 >= rInf.GetLen()) ||
+                        pKernArray[i + nCopyStart] != pKernArray[ i + nCopyStart + 1])
+                    {
                         aStr = aStr.replaceAt(i, 1, OUString(CH_BULLET));
+                    }
                 }
         }
 