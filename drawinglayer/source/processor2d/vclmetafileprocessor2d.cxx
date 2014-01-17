LemmingAvalanche/libreoffice-repaired

commit 6c9bbe0a8f24dd5dec8fad57a6c72870ede1afc0

Resolves: #i123295# corrected object and clip rect...
in PDFExtOutDevData::EndGroup call in VclMetafileProcessor2D

(cherry picked from commit b756639c139f043a613df6ca16c18f426fca1f29)

Change-Id: I75faf2c8d925ec210cd74d8c29b90f5f9bf61ec8

drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
@@ -862,10 +862,13 @@ namespace drawinglayer
                                 sal_Int32(ceil(aCropRange.getMaxX())), sal_Int32(ceil(aCropRange.getMaxY())));
                         }
 
+                        // #i123295# 3rd param is uncropped rect, 4th is cropped. The primitive has the cropped
+                        // object transformation, thus aCurrentRect *is* the clip region while aCropRect is the expanded,
+                        // uncropped region. Thus, correct order is aCropRect, aCurrentRect
                         mpPDFExtOutDevData->EndGroup(rGraphicPrimitive.getGraphicObject().GetGraphic(),
                             rAttr.GetTransparency(),
-                            aCurrentRect,
-                            aCropRect);
+                            aCropRect,
+                            aCurrentRect);
                     }
 
                     break;

commit 6cafd0fd2a6faf5579605c2129a794b478441a2c

fdo#72530: drawinglayer: fix PDF export of lossless picture
Don't pass empty Rectangle to PDFExtOutDevData::EndGroup(), it will
result in early return from PDFWriterImpl::drawJPGBitmap() and no
picture in the PDF.  Passing in the same Rectangle for the 3rd and 4th
parameter will avoid cropping in PDFExtOutDevData.

(regression from 6c9bbe0a8f24dd5dec8fad57a6c72870ede1afc0)

Change-Id: Icf110fe3e635319419fcacc7bc684c792a34d6ad

drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
@@ -824,7 +824,8 @@ namespace drawinglayer
                             sal_Int32(floor(aCurrentRange.getMinX())), sal_Int32(floor(aCurrentRange.getMinY())),
                             sal_Int32(ceil(aCurrentRange.getMaxX())), sal_Int32(ceil(aCurrentRange.getMaxY())));
                         const GraphicAttr& rAttr = rGraphicPrimitive.getGraphicAttr();
-                        Rectangle aCropRect;
+                        // fdo#72530 don't pass empty Rectangle to EndGroup
+                        Rectangle aCropRect(aCurrentRect);
 
                         if(rAttr.IsCropped())
                         {