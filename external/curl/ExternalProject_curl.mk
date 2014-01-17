LemmingAvalanche/libreoffice-repaired

commit 040b8effee9a4f33430efe26098bafef0dcc6bcf

Get bundled curl to be used on OS X too Don't force "yes" on Darwin.
Change-Id: I3a6a2ecdf15325d07fb22b9a6bb0da7b313036cb
Reviewed-on: https://gerrit.libreoffice.org/7197
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

external/curl/ExternalProject_curl.mk
@@ -40,10 +40,9 @@ $(call gb_ExternalProject_get_state_target,curl,build):
 		CPPFLAGS="$(curl_CPPFLAGS)" \
 		LDFLAGS=$(curl_LDFLAGS) \
 		./configure \
-			$(if $(filter MACOSX IOS,$(OS)),\
-				--with-darwinssl, \
+			$(if $(filter IOS,$(OS)),--with-darwinssl,--without-ssl) \
+			$(if $(filter MACOSX,$(OS)),,\
 				--with-nss$(if $(filter NO,$(SYSTEM_NSS)),="$(call gb_UnpackedTarball_get_dir,nss)/dist/out")) \
-			--without-ssl \
 			--without-libidn --enable-ftp --enable-ipv6 --enable-http --disable-gopher \
 			--disable-file --disable-ldap --disable-telnet --disable-dict --without-libssh2 \
 			$(if $(filter YES,$(CROSS_COMPILING)),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \

configure.ac
@@ -8654,14 +8654,7 @@ dnl Check for system curl
 dnl ===================================================================
 AC_MSG_CHECKING([which libcurl to use])
 if test "$with_system_curl" = "auto"; then
-    case "$_os" in
-    Darwin)
-        with_system_curl=yes
-        ;;
-    *)
-        with_system_curl="$with_system_libs"
-        ;;
-    esac
+    with_system_curl="$with_system_libs"
 fi
 
 if test "$with_system_curl" = "yes"; then

external/curl/ExternalPackage_curl.mk
@@ -17,6 +17,8 @@ ifeq ($(OS)$(COM),WNTGCC)
 $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.dll,lib/.libs/libcurl.dll))
 else ifeq ($(COM),MSC)
 $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl$(if $(MSVC_USE_DEBUG_RUNTIME),d).dll,lib/libcurl$(if $(MSVC_USE_DEBUG_RUNTIME),d).dll))
+else ifeq ($(OS),MACOSX)
+$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.4.dylib,lib/.libs/libcurl.4.dylib))
 else ifeq ($(OS),AIX)
 $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so,lib/.libs/libcurl.so.4))
 else

commit 7712f0d0f5fe5965d27730e2771de02b4a5ec14b

curl: we really want --with-darwinssl on Mac for libcmis
(regression from 040b8effee9a4f33430efe26098bafef0dcc6bcf)

Change-Id: I9e1d4d17582011a067aecf47144bb70643fac806

external/curl/ExternalProject_curl.mk
@@ -35,14 +35,16 @@ ifeq ($(SYSTEM_NSS),NO)
 curl_CPPFLAGS += -I$(call gb_UnpackedTarball_get_dir,nss)/dist/public/nss
 endif
 
+# use --with-darwinssl on Mac to get a native UI for SSL certs for CMIS usage
 $(call gb_ExternalProject_get_state_target,curl,build):
 	$(call gb_ExternalProject_run,build,\
 		CPPFLAGS="$(curl_CPPFLAGS)" \
 		LDFLAGS=$(curl_LDFLAGS) \
 		./configure \
-			$(if $(filter IOS,$(OS)),--with-darwinssl,--without-ssl) \
-			$(if $(filter MACOSX,$(OS)),,\
+			$(if $(filter MACOSX IOS,$(OS)),\
+				--with-darwinssl, \
 				--with-nss$(if $(filter NO,$(SYSTEM_NSS)),="$(call gb_UnpackedTarball_get_dir,nss)/dist/out")) \
+			--without-ssl \
 			--without-libidn --enable-ftp --enable-ipv6 --enable-http --disable-gopher \
 			--disable-file --disable-ldap --disable-telnet --disable-dict --without-libssh2 \
 			$(if $(filter YES,$(CROSS_COMPILING)),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \