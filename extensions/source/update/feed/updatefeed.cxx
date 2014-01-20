LemmingAvalanche/libreoffice-repaired

commit c5eff79891ceb6f5d65b4a5b8c1b4a82a079ee54

finish deprecation of O(U)String::valueOf()
Compiler plugin to replace with matching number(), boolean() or OUString ctor,
ran it, few manual tweaks, mark as really deprecated.

Change-Id: I4a79bdbcf4c460d21e73b635d2bd3725c22876b2

extensions/source/update/feed/updatefeed.cxx
@@ -244,7 +244,7 @@ public:
         OSL_ASSERT( m_xUpdateInformationProvider.is() );
 
         if( !(m_nCount < m_nNodes ) )
-            throw container::NoSuchElementException(OUString::valueOf(m_nCount), *this);
+            throw container::NoSuchElementException(OUString::number(m_nCount), *this);
 
         try
         {
@@ -301,7 +301,7 @@ public:
     uno::Any SAL_CALL nextElement() throw (container::NoSuchElementException, lang::WrappedTargetException, uno::RuntimeException)
     {
         if( m_nCount > 0 )
-            throw container::NoSuchElementException(OUString::valueOf(m_nCount), *this);
+            throw container::NoSuchElementException(OUString::boolean(m_nCount), *this);
 
         ++m_nCount;
         return uno::makeAny(m_aEntry);

include/comphelper/logging.hxx
@@ -51,13 +51,13 @@ namespace comphelper
             return OUString::createFromAscii( _pAsciiValue );
         }
 
-        inline OUString convertLogArgToString( double      _nValue ) { return OUString::valueOf( _nValue ); }
-        inline OUString convertLogArgToString( float       _nValue ) { return OUString::valueOf( _nValue ); }
-        inline OUString convertLogArgToString( sal_Int64   _nValue ) { return OUString::valueOf( _nValue ); }
-        inline OUString convertLogArgToString( sal_Int32   _nValue ) { return OUString::valueOf( _nValue ); }
-        inline OUString convertLogArgToString( sal_Int16   _nValue ) { return OUString::valueOf( (sal_Int32)_nValue ); }
-        inline OUString convertLogArgToString( sal_Unicode _nValue ) { return OUString::valueOf( _nValue ); }
-        inline OUString convertLogArgToString( sal_Bool    _nValue ) { return OUString::valueOf( _nValue ); }
+        inline OUString convertLogArgToString( double      _nValue ) { return OUString::number( _nValue ); }
+        inline OUString convertLogArgToString( float       _nValue ) { return OUString::number( _nValue ); }
+        inline OUString convertLogArgToString( sal_Int64   _nValue ) { return OUString::number( _nValue ); }
+        inline OUString convertLogArgToString( sal_Int32   _nValue ) { return OUString::number( _nValue ); }
+        inline OUString convertLogArgToString( sal_Int16   _nValue ) { return OUString::number( _nValue ); }
+        inline OUString convertLogArgToString( sal_Unicode _nValue ) { return OUString( _nValue ); }
+        inline OUString convertLogArgToString( sal_Bool    _nValue ) { return OUString::boolean( _nValue ); }
 
     } } // namespace log::convert
 

sw/source/ui/utlui/attrdesc.cxx
@@ -213,12 +213,12 @@ SfxItemPresentation SwFmtDrop::GetPresentation
             {
                 if ( GetChars() > 1 )
                 {
-                    rText = OUString::valueOf( GetChars() ) + " ";
+                    rText = OUString::boolean( GetChars() ) + " ";
                 }
                 rText = rText +
                         OUString( SW_RESSTR( STR_DROP_OVER ) ) +
                         " " +
-                        OUString::valueOf( GetLines() ) +
+                        OUString::boolean( GetLines() ) +
                         " " +
                         OUString( SW_RESSTR( STR_DROP_LINES ) );
             }
@@ -336,7 +336,7 @@ SfxItemPresentation SwFmtFrmSize::GetPresentation
             rText = SW_RESSTR( STR_FRM_WIDTH ) + " ";
             if ( GetWidthPercent() )
             {
-                rText = rText + OUString::valueOf(GetWidthPercent()) + "%";
+                rText = rText + OUString::boolean(GetWidthPercent()) + "%";
             }
             else
             {
@@ -349,7 +349,7 @@ SfxItemPresentation SwFmtFrmSize::GetPresentation
                 rText = rText + ", " + SW_RESSTR( nId ) + " ";
                 if ( GetHeightPercent() )
                 {
-                    rText = rText + OUString::valueOf(GetHeightPercent()) + "%";
+                    rText = rText + OUString::boolean(GetHeightPercent()) + "%";
                 }
                 else
                 {
@@ -685,7 +685,7 @@ SfxItemPresentation SwFmtCol::GetPresentation
             sal_uInt16 nCnt = GetNumCols();
             if ( nCnt > 1 )
             {
-                rText = OUString::valueOf(nCnt) + " " + SW_RESSTR( STR_COLUMNS );
+                rText = OUString(nCnt) + " " + SW_RESSTR( STR_COLUMNS );
                 if ( COLADJ_NONE != GetLineAdj() )
                 {
                     sal_uInt16 nWdth = sal_uInt16(GetLineWidth());

commit fe9a12403f9b816b082eea67ae4303234e66c507

misuse of OUString::boolean on sal_uInt8 values
regression since c5eff79891ceb6f5d65b4a5b8c1b4a82a079ee54

Change-Id: Ifc907a34430c36a64318fc42342e630e2b84ad13

extensions/source/update/feed/updatefeed.cxx
@@ -300,14 +300,14 @@ public:
     uno::Any SAL_CALL nextElement() throw (container::NoSuchElementException, lang::WrappedTargetException, uno::RuntimeException)
     {
         if( m_nCount > 0 )
-            throw container::NoSuchElementException(OUString::boolean(m_nCount), *this);
+            throw container::NoSuchElementException(OUString::number(m_nCount), *this);
 
         ++m_nCount;
         return uno::makeAny(m_aEntry);
     };
 
 private:
-    sal_uInt8 m_nCount;
+    sal_Int32 m_nCount;
     deployment::UpdateInformationEntry m_aEntry;
 };
 

include/comphelper/logging.hxx
@@ -57,7 +57,7 @@ namespace comphelper
         inline OUString convertLogArgToString( sal_Int32   _nValue ) { return OUString::number( _nValue ); }
         inline OUString convertLogArgToString( sal_Int16   _nValue ) { return OUString::number( _nValue ); }
         inline OUString convertLogArgToString( sal_Unicode _nValue ) { return OUString( _nValue ); }
-        inline OUString convertLogArgToString( sal_Bool    _nValue ) { return OUString::boolean( _nValue ); }
+        inline OUString convertLogArgToString( sal_Bool    _bValue ) { return OUString::boolean( _bValue ); }
 
     } } // namespace log::convert
 

sw/source/ui/utlui/attrdesc.cxx
@@ -213,12 +213,12 @@ SfxItemPresentation SwFmtDrop::GetPresentation
             {
                 if ( GetChars() > 1 )
                 {
-                    rText = OUString::boolean( GetChars() ) + " ";
+                    rText = OUString::number( GetChars() ) + " ";
                 }
                 rText = rText +
                         OUString( SW_RESSTR( STR_DROP_OVER ) ) +
                         " " +
-                        OUString::boolean( GetLines() ) +
+                        OUString::number( GetLines() ) +
                         " " +
                         OUString( SW_RESSTR( STR_DROP_LINES ) );
             }
@@ -336,7 +336,7 @@ SfxItemPresentation SwFmtFrmSize::GetPresentation
             rText = SW_RESSTR( STR_FRM_WIDTH ) + " ";
             if ( GetWidthPercent() )
             {
-                rText = rText + OUString::boolean(GetWidthPercent()) + "%";
+                rText = rText + OUString::number(GetWidthPercent()) + "%";
             }
             else
             {
@@ -350,7 +350,7 @@ SfxItemPresentation SwFmtFrmSize::GetPresentation
                 rText = rText + ", " + SW_RESSTR( nId ) + " ";
                 if ( GetHeightPercent() )
                 {
-                    rText = rText + OUString::boolean(GetHeightPercent()) + "%";
+                    rText = rText + OUString::number(GetHeightPercent()) + "%";
                 }
                 else
                 {