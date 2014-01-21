LemmingAvalanche/libreoffice-repaired

commit e03819e5b4f8370d716daa1b674d50d09eee2689

fix stupid over-optimization
There's so little to be gained by storing pointers to simple
or refcounted types instead of using an instance. At least
SwTxtNode::GetMinMaxSize() and SwTxtNode::GetScalingOfSelectedText()
pass string temporaries to SwDrawTextInfo, so keeping a pointer
is clearly broken, and MSVC manages to optimize the code enough
to crash because of this. As all the pointers point to const references
and are accesses using const references as well, there shouldn't
be any harm in making a copy.
This fixes smoketest crash on Windows.

Change-Id: I7d7cb42e175a2e64fe9c84c98c0d0204a0c25c13

sw/source/core/txtnode/swfont.cxx
@@ -1163,17 +1163,17 @@ void SwDrawTextInfo::Shift( sal_uInt16 nDir )
     switch ( nDir )
     {
     case 0 :
-        ((Point*)pPos)->X() += GetSize().Width();
+        pPos.X() += GetSize().Width();
         break;
     case 900 :
-        OSL_ENSURE( ((Point*)pPos)->Y() >= GetSize().Width(), "Going underground" );
-        ((Point*)pPos)->Y() -= GetSize().Width();
+        OSL_ENSURE( pPos.Y() >= GetSize().Width(), "Going underground" );
+        pPos.Y() -= GetSize().Width();
         break;
     case 1800 :
-        ((Point*)pPos)->X() -= GetSize().Width();
+        pPos.X() -= GetSize().Width();
         break;
     case 2700 :
-        ((Point*)pPos)->Y() += GetSize().Width();
+        pPos.Y() += GetSize().Width();
         break;
     }
 }

sw/source/core/inc/drawfont.hxx
@@ -42,12 +42,12 @@ class SwDrawTextInfo
     OutputDevice* pOut;
     ViewShell* pSh;
     const SwScriptInfo* pScriptInfo;
-    const Point* pPos;
-    const OUString* pText;
+    Point pPos;
+    OUString pText;
     const SwWrongList* pWrong;
     const SwWrongList* pGrammarCheck;
     const SwWrongList* pSmartTags;
-    const Size* pSize;
+    Size pSize;
     SwFont *pFnt;
     SwUnderlineFont* pUnderFnt;
     xub_StrLen* pHyphPos;
@@ -111,7 +111,7 @@ public:
         pSh = pS;
         pOut = &rO;
         pScriptInfo = pSI;
-        pText = &rSt;
+        pText = rSt;
         nIdx = nI;
         nLen = nL;
         nKern = 0;
@@ -129,11 +129,9 @@ public:
 
         // These values are initialized but have to be set explicitly via their
         // Set-function before they may be accessed by their Get-function:
-        pPos = 0;
         pWrong = 0;
         pGrammarCheck = 0;
         pSmartTags = 0;
-        pSize = 0;
         pFnt = 0;
         pHyphPos = 0;
         nLeft = 0;
@@ -192,7 +190,7 @@ public:
 #ifdef DBG_UTIL
         OSL_ENSURE( m_bPos, "DrawTextInfo: Undefined Position" );
 #endif
-        return *pPos;
+        return pPos;
     }
 
     xub_StrLen *GetHyphPos() const
@@ -205,7 +203,7 @@ public:
 
     const OUString &GetText() const
     {
-        return *pText;
+        return pText;
     }
 
     const SwWrongList* GetWrong() const
@@ -234,7 +232,7 @@ public:
 #ifdef DBG_UTIL
         OSL_ENSURE( m_bSize, "DrawTextInfo: Undefined Size" );
 #endif
-        return *pSize;
+        return pSize;
     }
 
     SwFont* GetFont() const
@@ -402,7 +400,7 @@ public:
 
     void SetPos( const Point &rNew )
     {
-        pPos = &rNew;
+        pPos = rNew;
 #ifdef DBG_UTIL
         m_bPos = true;
 #endif
@@ -418,7 +416,7 @@ public:
 
     void SetText( const OUString &rNew )
     {
-        pText = &rNew;
+        pText = rNew;
     }
 
     void SetWrong( const SwWrongList* pNew )
@@ -444,7 +442,7 @@ public:
 
     void SetSize( const Size &rNew )
     {
-        pSize = &rNew;
+        pSize = rNew;
 #ifdef DBG_UTIL
         m_bSize = true;
 #endif

commit 8aacc48a5b7dbd0baa4559f32821b2375ab1b6c1

fdo#68959: sw: fix painting of tab fill characters in small caps text
The member SwDrawTextInfo::pPos was changed from a pointer to some
externally provided Point to a real object; unfortunately there were
some functions which hold backups of the current pPos as a
reference/pointer and then restore it, and the restore is a no-op now.
So use real Points as backups.

(regression from e03819e5b4f8370d716daa1b674d50d09eee2689)

Change-Id: I8470143ec494cb194a88ddcbd12d96867974278f

sw/source/core/text/txtfly.cxx
@@ -568,7 +568,7 @@ sal_Bool SwTxtFly::DrawTextOpaque( SwDrawTextInfo &rInf )
     }
 
     Point aPos( rInf.GetPos().X(), rInf.GetPos().Y() + rInf.GetAscent() );
-    const Point &rOld = rInf.GetPos();
+    const Point aOldPos(rInf.GetPos());
     rInf.SetPos( aPos );
 
     if( !bOpaque )
@@ -577,7 +577,7 @@ sal_Bool SwTxtFly::DrawTextOpaque( SwDrawTextInfo &rInf )
             rInf.GetFont()->_DrawStretchText( rInf );
         else
             rInf.GetFont()->_DrawText( rInf );
-        rInf.SetPos( rOld );
+        rInf.SetPos(aOldPos);
         return sal_False;
     }
     else if( !aRegion.empty() )
@@ -595,7 +595,7 @@ sal_Bool SwTxtFly::DrawTextOpaque( SwDrawTextInfo &rInf )
                 rInf.GetFont()->_DrawText( rInf );
         }
     }
-    rInf.SetPos( rOld );
+    rInf.SetPos(aOldPos);
     return sal_True;
 }
 

sw/source/core/txtnode/fntcap.cxx
@@ -516,9 +516,9 @@ void SwSubFont::DrawStretchCapital( SwDrawTextInfo &rInf )
     if( rInf.GetLen() == COMPLETE_STRING )
         rInf.SetLen( rInf.GetText().getLength() );
 
-    const Point& rOldPos = rInf.GetPos();
+    const Point aOldPos = rInf.GetPos();
     const sal_uInt16 nCapWidth = (sal_uInt16)( GetCapitalSize( rInf ).Width() );
-    rInf.SetPos( rOldPos );
+    rInf.SetPos(aOldPos);
 
     rInf.SetDrawSpace( GetUnderline() != UNDERLINE_NONE ||
                        GetOverline()  != UNDERLINE_NONE ||

sw/source/core/txtnode/swfont.cxx
@@ -1195,12 +1195,12 @@ void SwSubFont::_DrawText( SwDrawTextInfo &rInf, const sal_Bool bGrey )
 
     SwDigitModeModifier aDigitModeModifier( rInf.GetOut(), rInf.GetFont()->GetLanguage() );
 
+    const Point aOldPos(rInf.GetPos());
     Point aPos( rInf.GetPos() );
 
     if( GetEscapement() )
         CalcEsc( rInf, aPos );
 
-    const Point &rOld = rInf.GetPos();
     rInf.SetPos( aPos );
     rInf.SetKern( CheckKerning() + rInf.GetSperren() / SPACING_PRECISION_FACTOR );
 
@@ -1300,7 +1300,7 @@ void SwSubFont::_DrawText( SwDrawTextInfo &rInf, const sal_Bool bGrey )
         rInf.SetLen( nOldLen );
     }
 
-    rInf.SetPos( rOld );
+    rInf.SetPos(aOldPos);
 }
 
 void SwSubFont::_DrawStretchText( SwDrawTextInfo &rInf )
@@ -1325,13 +1325,13 @@ void SwSubFont::_DrawStretchText( SwDrawTextInfo &rInf )
 
     rInf.ApplyAutoColor();
 
+    const Point aOldPos(rInf.GetPos());
     Point aPos( rInf.GetPos() );
 
     if( GetEscapement() )
         CalcEsc( rInf, aPos );
 
     rInf.SetKern( CheckKerning() + rInf.GetSperren() / SPACING_PRECISION_FACTOR );
-    const Point &rOld = rInf.GetPos();
     rInf.SetPos( aPos );
 
     if( IsCapital() )
@@ -1382,7 +1382,7 @@ void SwSubFont::_DrawStretchText( SwDrawTextInfo &rInf )
         rInf.SetLen( nOldLen );
     }
 
-    rInf.SetPos( rOld );
+    rInf.SetPos(aOldPos);
 }
 
 sal_Int32 SwSubFont::_GetCrsrOfst( SwDrawTextInfo& rInf )