LemmingAvalanche/libreoffice-repaired

commit a592765cc0424230e1da52c82c322f84670b5ed2

fdo#69649 Fix for pre-rendered Table Of Contents
Added Import and Export Uts for the same

Conflicts:
	sw/qa/extras/ooxmlexport/ooxmlexport.cxx
Reviewed on:
	https://gerrit.libreoffice.org/7207

Change-Id: I966b3d268dd407aa05823e29fe1da4d097091265

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -56,6 +56,7 @@
 #include <com/sun/star/container/XIndexContainer.hpp>
 #include <com/sun/star/awt/XControlModel.hpp>
 #include <com/sun/star/drawing/XControlShape.hpp>
+#include <com/sun/star/text/ControlCharacter.hpp>
 #include <oox/mathml/import.hxx>
 
 #ifdef DEBUG_DOMAINMAPPER
@@ -151,6 +152,8 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bIsFirstSection( true ),
         m_bIsColumnBreakDeferred( false ),
         m_bIsPageBreakDeferred( false ),
+        m_bStartTOC(false),
+        m_bTOCPageRef(false),
         m_pLastSectionContext( ),
         m_pLastCharacterContext(),
         m_nCurrentTabStopIndex( 0 ),
@@ -1183,14 +1186,46 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, PropertyMapP
     {
         try
         {
+            uno::Sequence< beans::PropertyValue > pValues = pPropertyMap->GetPropertyValues();
+            sal_Int32 len = pValues.getLength();
+
+            if (m_bStartTOC)
+                for( int i =0; i < len; ++i )
+                {
+                    if (pValues[i].Name == "CharHidden")
+                        pValues[i].Value = uno::makeAny(sal_False);
+                }
+
             uno::Reference< text::XTextRange > xTextRange;
             if (m_aTextAppendStack.top().xInsertPosition.is())
             {
-                xTextRange = xTextAppend->insertTextPortion(rString, pPropertyMap->GetPropertyValues(), m_aTextAppendStack.top().xInsertPosition);
+                xTextRange = xTextAppend->insertTextPortion(rString, pValues, m_aTextAppendStack.top().xInsertPosition);
                 m_aTextAppendStack.top().xCursor->gotoRange(xTextRange->getEnd(), false);
             }
             else
-                xTextRange = xTextAppend->appendTextPortion(rString, pPropertyMap->GetPropertyValues());
+            {
+                if (m_bStartTOC)
+                {
+                    uno::Reference< text::XTextCursor > xTOCTextCursor;
+                    xTOCTextCursor = xTextAppend->getEnd()->getText( )->createTextCursor( );
+                    xTOCTextCursor->gotoEnd(false);
+                    if (xTOCTextCursor.is())
+                    {
+                        xTextRange = xTextAppend->insertTextPortion(rString, pValues, xTOCTextCursor);
+                        xTOCTextCursor->gotoRange(xTextRange->getEnd(), true);
+                    }
+                    else
+                    {
+                        xTextRange = xTextAppend->appendTextPortion(rString, pValues);
+                        xTOCTextCursor = xTextAppend->createTextCursor();
+                        xTOCTextCursor->gotoRange(xTextRange->getEnd(), false);
+                    }
+                    m_aTextAppendStack.push(TextAppendContext(xTextAppend, xTOCTextCursor));
+                }
+                else
+                    xTextRange = xTextAppend->appendTextPortion(rString, pValues);
+            }
+
             CheckRedline( xTextRange );
             m_bParaChanged = true;
 
@@ -2657,6 +2692,7 @@ void DomainMapper_Impl::handleToc
     const OUString & sTOCServiceName)
 {
     OUString sValue;
+    m_bStartTOC = true;
     bool bTableOfFigures = false;
     bool bHyperlinks = false;
     bool bFromOutline = false;
@@ -2853,6 +2889,25 @@ void DomainMapper_Impl::handleToc
         }
     }
     pContext->SetTOC( xTOC );
+
+
+    OUString sMarker("Y");
+    //insert index
+    uno::Reference< text::XTextContent > xToInsert( xTOC, uno::UNO_QUERY );
+    uno::Reference< text::XTextAppend >  xTextAppend = m_aTextAppendStack.top().xTextAppend;
+    if (xTextAppend.is())
+    {
+        uno::Reference< text::XTextCursor > xCrsr = xTextAppend->getText()->createTextCursor();
+
+        uno::Reference< text::XText > xText = xTextAppend->getText();
+        if(xCrsr.is() && xText.is())
+        {
+            xCrsr->gotoEnd(false);
+            xText->insertString(xCrsr, sMarker, sal_False);
+            xText->insertTextContent(uno::Reference< text::XTextRange >( xCrsr, uno::UNO_QUERY_THROW ), xToInsert, sal_False);
+            xTOCMarkerCursor = xCrsr;
+        }
+    }
 }
 
 
@@ -2915,6 +2970,11 @@ void DomainMapper_Impl::CloseFieldCommand()
                 }
                 default:
                     break;
+                }
+                if (m_bStartTOC && (aIt->second.eFieldId == FIELD_PAGEREF) )
+                {
+                    m_bTOCPageRef = true;
+                    bCreateField = false;
                 }
 
                 if( bCreateField || bCreateEnhancedField )
@@ -3227,7 +3287,7 @@ void DomainMapper_Impl::CloseFieldCommand()
                     break;
                     case FIELD_PAGEREF:
                     case FIELD_REF:
-                    if (xFieldProperties.is())
+                    if (xFieldProperties.is() && !m_bStartTOC)
                     {
                         bool bPageRef = aIt->second.eFieldId == FIELD_PAGEREF;
                         OUString sBookmark = lcl_ExtractParameter(pContext->GetCommand(),
@@ -3554,15 +3614,22 @@ void DomainMapper_Impl::PopFieldContext()
                 uno::Reference< text::XTextContent > xToInsert( pContext->GetTOC(), uno::UNO_QUERY );
                 if( xToInsert.is() )
                 {
-                    xCrsr->gotoEnd( true );
-                    xToInsert->attach( uno::Reference< text::XTextRange >( xCrsr, uno::UNO_QUERY_THROW ));
+                    m_bStartTOC = false;
+                    m_aTextAppendStack.pop();
+                    if(xTOCMarkerCursor.is())
+                    {
+                        xTOCMarkerCursor->goLeft(1,sal_True);
+                        xTOCMarkerCursor->setString(OUString());
+                        xTOCMarkerCursor->goLeft(1,sal_True);
+                        xTOCMarkerCursor->setString(OUString());
+                    }
                 }
                 else
                 {
                     xToInsert = uno::Reference< text::XTextContent >(pContext->GetTC(), uno::UNO_QUERY);
-                    if( !xToInsert.is() )
+                    if( !xToInsert.is() && !m_bStartTOC )
                         xToInsert = uno::Reference< text::XTextContent >(pContext->GetTextField(), uno::UNO_QUERY);
-                    if( xToInsert.is() )
+                    if( xToInsert.is() && !m_bStartTOC)
                     {
                         uno::Sequence<beans::PropertyValue> aValues;
                         // Character properties of the field show up here the
@@ -3599,6 +3666,12 @@ void DomainMapper_Impl::PopFieldContext()
                             uno::Reference< beans::XPropertySet > xCrsrProperties( xCrsr, uno::UNO_QUERY_THROW );
                             xCrsrProperties->setPropertyValue(rPropNameSupplier.GetName(PROP_HYPER_LINK_U_R_L), uno::
                                                               makeAny(pContext->GetHyperlinkURL()));
+
+                            if (m_bStartTOC) {
+                                OUString sDisplayName("Index Link");
+                                xCrsrProperties->setPropertyValue("VisitedCharStyleName",uno::makeAny(sDisplayName));
+                                xCrsrProperties->setPropertyValue("UnvisitedCharStyleName",uno::makeAny(sDisplayName));
+                            }
                         }
                     }
                 }

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -329,6 +329,8 @@ private:
     bool                                                                            m_bIsFirstSection;
     bool                                                                            m_bIsColumnBreakDeferred;
     bool                                                                            m_bIsPageBreakDeferred;
+    bool                                                                            m_bStartTOC;
+    bool                                                                            m_bTOCPageRef;
 
     LineNumberSettings                                                              m_aLineNumberSettings;
 
@@ -391,6 +393,8 @@ private:
     /// If the current paragraph is inside a structured document element.
     bool                            m_bSdt;
     bool                            m_bIsFirstRun;
+
+    uno::Reference< text::XTextCursor > xTOCMarkerCursor;
 
     //annotation import
     uno::Reference< beans::XPropertySet >                                      m_xAnnotationField;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -43,6 +43,7 @@
 #include <unotools/tempfile.hxx>
 #include <unotools/ucbstreamhelper.hxx>
 #include <rtl/strbuf.hxx>
+#include <com/sun/star/text/XDocumentIndex.hpp>
 
 #include <libxml/xpathInternals.h>
 #include <libxml/parserInternals.h>
@@ -2211,6 +2212,18 @@ DECLARE_OOXMLEXPORT_TEST(testTrackChangesInsertedParagraphMark, "testTrackChange
     if (!pXmlDoc)
         return;
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:rPr/w:ins");
+}
+
+DECLARE_OOXMLEXPORT_TEST(testFdo69649, "fdo69649.docx")
+{
+    // The DOCX containing the Table of Contents was not exported with correct page nos
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    xmlNodeSetPtr pXmlNodes = getXPathNode(pXmlDoc,"/w:document/w:body/w:p[21]/w:hyperlink/w:r[2]/w:t");
+    xmlNodePtr pXmlNode = pXmlNodes->nodeTab[0];
+    OUString contents = OUString::createFromAscii((const char*)((pXmlNode->children[0]).content));
+    CPPUNIT_ASSERT(contents.match("15"));
 }
 
 #endif

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -43,7 +43,7 @@
 #include <com/sun/star/table/TableBorder2.hpp>
 #include <com/sun/star/text/SizeType.hpp>
 #include <com/sun/star/xml/dom/XDocument.hpp>
-
+#include <com/sun/star/text/XDocumentIndex.hpp>
 #include <vcl/svapp.hxx>
 #include <unotools/fltrcfg.hxx>
 
@@ -1670,6 +1670,21 @@ DECLARE_OOXMLIMPORT_TEST(testFdo65090, "fdo65090.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty< uno::Sequence<text::TableColumnSeparator> >(xTableRows->getByIndex(0), "TableColumnSeparators").getLength());
 }
 
+DECLARE_OOXMLIMPORT_TEST(testFdo69649, "fdo69649.docx")
+{
+    // The DOCX containing the Table of Contents was not imported with correct page nos
+    uno::Reference<text::XDocumentIndexesSupplier> xIndexSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xIndexes(xIndexSupplier->getDocumentIndexes( ), uno::UNO_QUERY);
+    uno::Reference<text::XDocumentIndex> xTOCIndex(xIndexes->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xTextRange(xTOCIndex->getAnchor(), uno::UNO_QUERY);
+    uno::Reference<text::XText> xText(xTextRange->getText( ), uno::UNO_QUERY);
+    uno::Reference<text::XTextCursor> xTextCursor(xText->createTextCursor( ), uno::UNO_QUERY);
+    xTextCursor->gotoRange(xTextRange->getStart(),false);
+    xTextCursor->gotoRange(xTextRange->getEnd(),true);
+    OUString xTocString(xTextCursor->getString());
+    xTocString = xTocString.copy(256);
+    CPPUNIT_ASSERT(xTocString.startsWithIgnoreAsciiCase( "Heading 15.1:\t15" ) );
+}
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

writerfilter/source/dmapper/ModelEventListener.cxx
@@ -58,19 +58,9 @@ void ModelEventListener::notifyEvent( const document::EventObject& rEvent ) thro
         {
             PropertyNameSupplier& rPropNameSupplier = PropertyNameSupplier::GetPropertyNameSupplier();
 
-            uno::Reference< text::XDocumentIndexesSupplier> xIndexesSupplier( rEvent.Source, uno::UNO_QUERY );
             //remove listener
             uno::Reference<document::XEventBroadcaster>(rEvent.Source, uno::UNO_QUERY )->removeEventListener(
             uno::Reference<document::XEventListener>(this));
-
-            uno::Reference< container::XIndexAccess > xIndexes = xIndexesSupplier->getDocumentIndexes();
-
-            sal_Int32 nIndexes = xIndexes->getCount();
-            for( sal_Int32 nIndex = 0; nIndex < nIndexes; ++nIndex)
-            {
-                uno::Reference< text::XDocumentIndex> xIndex( xIndexes->getByIndex( nIndex ), uno::UNO_QUERY );
-                xIndex->update();
-            }
 
             // If we have PAGEREF fields, update fields as well.
             uno::Reference<text::XTextFieldsSupplier> xTextFieldsSupplier(rEvent.Source, uno::UNO_QUERY);

commit 63f622a290d89691471c26d93934554bdd754e18

fdo#69649 RTF import: don't crash on empty TOC result
Regression from commit a592765cc0424230e1da52c82c322f84670b5ed2
(fdo#69649 Fix for pre-rendered Table Of Contents, 2013-12-30), the
situation that the field doesn't have a result wasn't handled, resulting
in a m_aTextAppendStack.pop() call in dmapper, without a matching
.push() one.

Change-Id: I456d9096d01fb27832a40f6b72802ec555d62647

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -145,6 +145,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bIsColumnBreakDeferred( false ),
         m_bIsPageBreakDeferred( false ),
         m_bStartTOC(false),
+        m_bStartedTOC(false),
         m_bTOCPageRef(false),
         m_pLastSectionContext( ),
         m_pLastCharacterContext(),
@@ -1164,6 +1165,7 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, PropertyMapP
             {
                 if (m_bStartTOC)
                 {
+                    m_bStartedTOC = true;
                     uno::Reference< text::XTextCursor > xTOCTextCursor;
                     xTOCTextCursor = xTextAppend->getEnd()->getText( )->createTextCursor( );
                     xTOCTextCursor->gotoEnd(false);
@@ -3584,7 +3586,11 @@ void DomainMapper_Impl::PopFieldContext()
                 if( xToInsert.is() )
                 {
                     m_bStartTOC = false;
-                    m_aTextAppendStack.pop();
+                    if (m_bStartedTOC)
+                    {
+                        m_aTextAppendStack.pop();
+                        m_bStartedTOC = false;
+                    }
                     if(xTOCMarkerCursor.is())
                     {
                         xTOCMarkerCursor->goLeft(1,sal_True);

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -313,6 +313,8 @@ private:
     bool                                                                            m_bIsColumnBreakDeferred;
     bool                                                                            m_bIsPageBreakDeferred;
     bool                                                                            m_bStartTOC;
+    /// If we got any text that is the pre-rendered result of the TOC field.
+    bool                                                                            m_bStartedTOC;
     bool                                                                            m_bTOCPageRef;
 
     LineNumberSettings                                                              m_aLineNumberSettings;

sw/qa/core/data/rtf/pass/fdo54641.rtf
@@ -1,0 +1,39 @@
+{\rtf1
+{\stylesheet
+{\s0\qj\widctlpar\f0\fs20 \snext0 Normal;}
+{\cs10 \additive\ssemihidden Default Paragraph Font;}
+{\s1\qc\sb240\sa120\keepn\f0\b\fs40 \sbasedon0\snext0 Part;}
+{\s2\ql\sb240\sa120\keepn\f0\b\fs40 \sbasedon0\snext0 heading 1;}
+{\s3\ql\sb240\sa120\keepn\f0\b\fs32 \sbasedon0\snext0 heading 2;}
+}
+\paperw11960\paperh16900\margl2500\margr2560\margt2520\margb1820\pgnstart0\widowctrl\qj\ftnbj\f0\aftnnar
+{\pard\plain\s20\qc\sb240\sa240\b\f0\fs36\sl240\slmult1 \fi0 De titel van het testfile\par
+\pard\plain\s21\qc\sa120\f0\fs20\sl240\slmult1 \sb120 \fi0 \chdate \par
+\pard\plain\s80\ql\sb240\sa120\keepn\f0\b\fs20\sl240\slmult1 \sb120 \fi0  Inhoudsopgave\par
+\pard\plain\s0\qj\widctlpar\f0\fs20\sl240\slmult1 \sb120 \fi0 \par
+{\field
+{\*\fldinst TOC \\o "1-3" }
+{\fldrslt }
+}
+\page
+\pard\plain\s0\qj\widctlpar\f0\fs20\sl240\slmult1 \sb180 \fi0 
+{}
+\par
+\pard\plain\ql\sb240\sa120\keepn\f0\b\fs40\sl240\slmult1 \sb120 \fi0 Hoofdstuk 1\par
+\pard\plain\s2\ql\sb240\sa120\keepn\f0\b\fs40\sl240\slmult1 \sb360 \fi0 De eerste test\par
+\pard\plain\s0\qj\widctlpar\f0\fs20\sl240\slmult1 \sb360 \fi0 In dit hoofdstuk zullen de eerste tests uitgevoerd worden.\par
+\pard\plain\s3\ql\sb240\sa120\keepn\f0\b\fs32\sl240\slmult1 \sb360 \fi0 1.1  Figuren\par
+{\pard\plain\s31\qc\sb120\sa0\keep\widctlpar\f0\fs20\sl240\slmult1 \sb420 \fi0  
+{\pict\picscalex92\picscaley92\piccropl0\piccropr0\piccropt0\piccropb0
+\picw20032\pich2725\picwgoal5357\pichgoal1545\pngblip\bliptag-1891142031
+{\*\blipuid 8f477671d3377e167ce61bb2e9de72f7}
+47494638396110001000d5ff00000000ffffffc0c0c0555f00ffffaafcfcfcf6f6f6eaeaeae6e6e6e4e4e4e3e3e3c2c2c2c1c1c1bcbcbcb5b5b5b3b3b3b0b0b0adadada5a5a5a2a2a2a1a1a19f9f9f9494948a8a8a8888888686867b7b7b6c6c6c5c5c5c4e4e4e4b4b4b4747474646463d3d3d3c3c3c2e2e2e2525251b1b1b18181810101009090906060603030300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000021f90401000002002c0000000010001000000684408170482c0a06c8a4728924389f506833b281302a8e6b164b18103024c52111504cca67332102e0042e9a40d9319f8300a343c1200f54e47f7e2a00001e0b0a7d0d728a010d838400261a7c0d94947784252700127e9d159f6c8411140019080ea7a9a85f842122281612b1b3b25d6b1f29291d0fbbbdbc5d5e51c34e4cc64a46c94341003b
+}
+\par
+}
+\pard\plain\s0\qj\widctlpar\f0\fs20\sl240\slmult1 \sb240 \fi0 De test wordt gevormd door een enkele, simpele figuur
+[
+].\par
+\pard\plain\s3\ql\sb240\sa120\keepn\f0\b\fs32\sl240\slmult1 \sb360 \fi0 1.2  Tabellen\par
+}
+}