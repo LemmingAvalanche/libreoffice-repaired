LemmingAvalanche/libreoffice-repaired

commit 932a03ad428402bc97417479afbae3e3eec53fde

fdo#69450 - IDF_ATTRIB & IDF_CONTENTS must be disjoint.
Change-Id: Ib971824a7f274334012b872acec65310535ad767

sc/inc/global.hxx
@@ -187,7 +187,7 @@ const sal_uInt16 IDF_OUTLINE    = 0x0800;   /// Sheet / outlining (grouping) inf
 const sal_uInt16 IDF_NOCAPTIONS = 0x0200;   /// Internal use only (undo etc.): do not copy/delete caption objects of cell notes.
 const sal_uInt16 IDF_ADDNOTES   = 0x0400;   /// Internal use only (copy from clip): do not delete existing cell contents when pasting notes.
 const sal_uInt16 IDF_SPECIAL_BOOLEAN = 0x1000;
-const sal_uInt16 IDF_ATTRIB     = IDF_HARDATTR | IDF_STYLES | IDF_OUTLINE;
+const sal_uInt16 IDF_ATTRIB     = IDF_HARDATTR | IDF_STYLES;
 const sal_uInt16 IDF_CONTENTS   = IDF_VALUE | IDF_DATETIME | IDF_STRING | IDF_NOTE | IDF_FORMULA | IDF_OUTLINE;
 const sal_uInt16 IDF_ALL        = IDF_CONTENTS | IDF_ATTRIB | IDF_OBJECTS;
 

sc/qa/unit/ucalc.cxx
@@ -503,6 +503,11 @@ void Test::testInput()
     m_pDoc->DeleteTab(0);
 }
 
+void Test::testCopyAttributes()
+{
+    CPPUNIT_ASSERT_MESSAGE ("mashed up attributes", !(IDF_ATTRIB & IDF_CONTENTS));
+}
+
 void Test::testCopyToDocument()
 {
     CPPUNIT_ASSERT_MESSAGE ("failed to insert sheet", m_pDoc->InsertTab (0, "src"));

sc/qa/unit/ucalc.hxx
@@ -105,6 +105,7 @@ public:
     void testFuncINDIRECT();
     void testFuncIFERROR();
     void testFuncGETPIVOTDATA();
+    void testCopyAttributes();
 
     void testCopyToDocument();
     /**

commit 041763b8f459df2b8428c9e4dfe12b9bbf6584e8

Better check this with a static assert
...check had been introduced with 932a03ad428402bc97417479afbae3e3eec53fde
"fdo#69450 - IDF_ATTRIB & IDF_CONTENTS must be disjoint" but confuses
compilerplugins/clang/literaltoboolconversion.cxx.

Change-Id: I6577e127a02b9007d098dc6201e48e8dbd5b6a55

sc/inc/global.hxx
@@ -28,6 +28,7 @@
 #include "scdllapi.h"
 #include <rtl/ustring.hxx>
 
+#include <boost/static_assert.hpp>
 #include <boost/unordered_map.hpp>
 #include <vector>
 
@@ -190,6 +191,8 @@ const sal_uInt16 IDF_SPECIAL_BOOLEAN = 0x1000;
 const sal_uInt16 IDF_ATTRIB     = IDF_HARDATTR | IDF_STYLES;
 const sal_uInt16 IDF_CONTENTS   = IDF_VALUE | IDF_DATETIME | IDF_STRING | IDF_NOTE | IDF_FORMULA | IDF_OUTLINE;
 const sal_uInt16 IDF_ALL        = IDF_CONTENTS | IDF_ATTRIB | IDF_OBJECTS;
+
+BOOST_STATIC_ASSERT(!(IDF_ATTRIB & IDF_CONTENTS));
 
 /// Copy flags for auto/series fill functions: do not touch notes and drawing objects.
 const sal_uInt16 IDF_AUTOFILL   = IDF_ALL & ~(IDF_NOTE | IDF_OBJECTS);

sc/qa/unit/ucalc.cxx
@@ -834,11 +834,6 @@ void Test::testSelectionFunction()
     m_pDoc->DeleteTab(0);
 }
 
-void Test::testCopyAttributes()
-{
-    CPPUNIT_ASSERT_MESSAGE ("mashed up attributes", !(IDF_ATTRIB & IDF_CONTENTS));
-}
-
 void Test::testCopyToDocument()
 {
     CPPUNIT_ASSERT_MESSAGE ("failed to insert sheet", m_pDoc->InsertTab (0, "src"));

sc/qa/unit/ucalc.hxx
@@ -125,7 +125,6 @@ public:
     void testFuncIFERROR();
     void testFuncGETPIVOTDATA();
     void testFuncGETPIVOTDATALeafAccess();
-    void testCopyAttributes();
 
     void testCopyToDocument();
     /**