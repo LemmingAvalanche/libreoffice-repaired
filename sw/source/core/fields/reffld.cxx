LemmingAvalanche/libreoffice-repaired

commit 9d810946e06fae3acd02dbdbce0353f917a9cb7a

fdo#35669: Update the references even if nothing uses them here
Merge the references with the other document even if there is no field
using them as this may break the sequence and references in other parts
of a master document.

sw/source/core/fields/reffld.cxx
@@ -1074,8 +1074,7 @@ void _RefIdsMap::Check( SwDoc& rDoc, SwDoc& rDestDoc, SwGetRefField& rFld,
 
 void SwGetRefFieldType::MergeWithOtherDoc( SwDoc& rDestDoc )
 {
-    if( &rDestDoc != pDoc &&
-        rDestDoc.GetSysFldType( RES_GETREFFLD )->GetDepends() )
+    if( &rDestDoc != pDoc )
     {
         // dann gibt es im DestDoc RefFelder, also muessen im SourceDoc
         // alle RefFelder auf einduetige Ids in beiden Docs umgestellt

commit 75f5da6de6e56bc0962339ed09616ff7d6c10638

fdo#63553: sw: fix copy/paste of cross references
If one of aIds or aDestIds contains nSeqNo then adding an identity
mapping for it will actually re-define a previous mapping.  Likely this
can only happen when clipboard document is the source.

(regression from 9d810946e06fae3acd02dbdbce0353f917a9cb7a)

(also it looks like 42689ed5b5a3cd139a169df1c3f92b8dea603c98 has subtly
 changed what the "else" branch does when it is erroneously taken -
 results are differently wrong)

Change-Id: I34e2979e45cffeb62ebf3d7b3e5ac6d2afff0c2b

sw/source/core/fields/reffld.cxx
@@ -997,7 +997,7 @@ void _RefIdsMap::Init( SwDoc& rDoc, SwDoc& rDestDoc, bool bField )
         for ( std::set<sal_uInt16>::iterator pIt = aDstIds.begin(); pIt != aDstIds.end(); ++pIt )
             AddId( GetFirstUnusedId(aIds), *pIt );
 
-        // Change the Sequence number of all the SetExp fields in the destination document
+        // Change the Sequence number of all SetExp fields in the source document
         SwFieldType* pType = rDoc.GetFldType( RES_SETEXPFLD, aName, false );
         if( pType )
         {
@@ -1053,16 +1053,24 @@ void _RefIdsMap::Check( SwDoc& rDoc, SwDoc& rDestDoc, SwGetRefField& rFld,
 {
     Init( rDoc, rDestDoc, bField);
 
-    // dann teste mal, ob die Nummer schon vergeben ist
-    // oder ob eine neue bestimmt werden muss.
     sal_uInt16 nSeqNo = rFld.GetSeqNo();
-    if( aIds.count( nSeqNo ) && aDstIds.count( nSeqNo ))
+
+    // Check if the number is used in both documents
+    // Note: For fields, aIds contains both the ids of SetExp from rDestDoc
+    // and the targets of the already remapped ones from rDoc.
+    // It is possible that aDstIds contains numbers that aIds does not contain!
+    // For example, copying a selection to clipboard that does not contain
+    // the first SwSetExpField will result in id 0 missing, then pasting that
+    // into empty document gives a mapping 1->0 ... N->N-1 (fdo#63553).
+    if (aIds.count(nSeqNo) || aDstIds.count(nSeqNo))
     {
         // Number already taken, so need a new one.
         if( sequencedIds.count(nSeqNo) )
             rFld.SetSeqNo( sequencedIds[nSeqNo] );
         else
         {
+            assert(!bField || !aDstIds.count(nSeqNo)); // postcond of Init
+
             sal_uInt16 n = GetFirstUnusedId( aIds );
 
             // die neue SeqNo eintragen, damit die "belegt" ist
@@ -1084,7 +1092,7 @@ void _RefIdsMap::Check( SwDoc& rDoc, SwDoc& rDestDoc, SwGetRefField& rFld,
     }
     else
     {
-        AddId( nSeqNo, nSeqNo );
+        AddId( nSeqNo, nSeqNo ); // this requires that nSeqNo is unused in both!
     }
 }
 