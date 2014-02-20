LemmingAvalanche/libreoffice-repaired

commit 0db9fab7cd757a5b0392f1fc8b0c827922982f9b

sw: Add support for different grab bags at PropertyMap.
Now there are two types of grab bags, for character and paragraph
attributes.

Change-Id: I41362358cd2765e5642499520d94f23f2380631d

writerfilter/source/dmapper/PropertyMap.cxx
@@ -64,19 +64,28 @@ uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues()
 {
     if(!m_aValues.getLength() && size())
     {
-        size_t nGrabBag = 0;
+        size_t nCharGrabBag = 0;
+        size_t nParaGrabBag = 0;
         for (PropertyMap::iterator i = begin(); i != end(); ++i)
-            if (i->second.hasGrabBag())
-                nGrabBag++;
+        {
+            if ( i->second.getGrabBagType() == CHAR_GRAB_BAG )
+                nCharGrabBag++;
+            else if ( i->second.getGrabBagType() == PARA_GRAB_BAG )
+                nParaGrabBag++;
+        }
         // If there are any grab bag properties, we need one slot for them.
-        m_aValues.realloc( size() - nGrabBag + (nGrabBag ? 1 : 0));
+        m_aValues.realloc( size() - nCharGrabBag + (nCharGrabBag ? 1 : 0)
+                                  - nParaGrabBag + (nParaGrabBag ? 1 : 0));
         ::com::sun::star::beans::PropertyValue* pValues = m_aValues.getArray();
-        uno::Sequence<beans::PropertyValue> aGrabBagValues(nGrabBag);
-        beans::PropertyValue* pGrabBagValues = aGrabBagValues.getArray();
+        uno::Sequence<beans::PropertyValue> aCharGrabBagValues(nCharGrabBag);
+        uno::Sequence<beans::PropertyValue> aParaGrabBagValues(nParaGrabBag);
+        beans::PropertyValue* pCharGrabBagValues = aCharGrabBagValues.getArray();
+        beans::PropertyValue* pParaGrabBagValues = aParaGrabBagValues.getArray();
         //style names have to be the first elements within the property sequence
         //otherwise they will overwrite 'hard' attributes
         sal_Int32 nValue = 0;
-        sal_Int32 nGrabBagValue = 0;
+        sal_Int32 nParaGrabBagValue = 0;
+        sal_Int32 nCharGrabBagValue = 0;
         PropertyNameSupplier& rPropNameSupplier = PropertyNameSupplier::GetPropertyNameSupplier();
         PropertyMap::iterator aParaStyleIter = find(PROP_PARA_STYLE_NAME);
         if( aParaStyleIter != end())
@@ -105,24 +114,36 @@ uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues()
         {
             if( aMapIter != aParaStyleIter && aMapIter != aCharStyleIter && aMapIter != aNumRuleIter )
             {
-                if (!aMapIter->second.hasGrabBag())
+                if ( aMapIter->second.getGrabBagType() == CHAR_GRAB_BAG )
+                {
+                    pCharGrabBagValues[nCharGrabBagValue].Name = rPropNameSupplier.GetName( aMapIter->first );
+                    pCharGrabBagValues[nCharGrabBagValue].Value = aMapIter->second.getValue();
+                    ++nCharGrabBagValue;
+                }
+                else if ( aMapIter->second.getGrabBagType() == PARA_GRAB_BAG )
+                {
+                    pParaGrabBagValues[nParaGrabBagValue].Name = rPropNameSupplier.GetName( aMapIter->first );
+                    pParaGrabBagValues[nParaGrabBagValue].Value = aMapIter->second.getValue();
+                    ++nParaGrabBagValue;
+                }
+                else
                 {
                     pValues[nValue].Name = rPropNameSupplier.GetName( aMapIter->first );
                     pValues[nValue].Value = aMapIter->second.getValue();
                     ++nValue;
                 }
-                else
-                {
-                    pGrabBagValues[nGrabBagValue].Name = rPropNameSupplier.GetName( aMapIter->first );
-                    pGrabBagValues[nGrabBagValue].Value = aMapIter->second.getValue();
-                    ++nGrabBagValue;
-                }
             }
         }
-        if (nGrabBag)
+        if (nCharGrabBag)
+        {
+            pValues[nValue].Name = "CharInteropGrabBag";
+            pValues[nValue].Value = uno::makeAny(aCharGrabBagValues);
+            ++nValue;
+        }
+        if (nParaGrabBag)
         {
             pValues[nValue].Name = "ParaInteropGrabBag";
-            pValues[nValue].Value = uno::makeAny(aGrabBagValues);
+            pValues[nValue].Value = uno::makeAny(aParaGrabBagValues);
             ++nValue;
         }
     }
@@ -154,7 +175,7 @@ static void lcl_AnyToTag(const uno::Any & rAny)
 }
 #endif
 
-void PropertyMap::Insert( PropertyIds eId, const uno::Any& rAny, bool bOverwrite, bool bGrabBag )
+void PropertyMap::Insert( PropertyIds eId, const uno::Any& rAny, bool bOverwrite, GrabBagType rGrabBagType )
 {
 #ifdef DEBUG_DMAPPER_PROPERTY_MAP
     const OUString& rInsert = PropertyNameSupplier::
@@ -170,11 +191,11 @@ void PropertyMap::Insert( PropertyIds eId, const uno::Any& rAny, bool bOverwrite
     if (aElement != end())
     {
         if (bOverwrite)
-            aElement->second = PropValue(rAny, bGrabBag);
+            aElement->second = PropValue(rAny, rGrabBagType);
 
         return;
     }
-    _PropertyMap::insert(_PropertyMap::value_type(eId, PropValue(rAny, bGrabBag)));
+    _PropertyMap::insert(_PropertyMap::value_type(eId, PropValue(rAny, rGrabBagType)));
 
     Invalidate();
 }

writerfilter/source/dmapper/PropertyMap.hxx
@@ -64,21 +64,29 @@ enum BorderPosition
     BORDER_BOTTOM
 };
 
+enum GrabBagType
+{
+    NO_GRAB_BAG,
+    PARA_GRAB_BAG,
+    CHAR_GRAB_BAG
+};
+
 class PropValue
 {
     uno::Any m_aValue;
-    bool m_bGrabBag;
+    GrabBagType m_rGrabBagType;
 
 public:
-    PropValue(const uno::Any& rValue, bool bGrabBag = false) :
-        m_aValue(rValue), m_bGrabBag(bGrabBag) {}
+    PropValue(const uno::Any& rValue, GrabBagType rGrabBagType = NO_GRAB_BAG) :
+        m_aValue(rValue), m_rGrabBagType(rGrabBagType) {}
 
-    PropValue() : m_aValue(), m_bGrabBag() {}
+    PropValue() : m_aValue(), m_rGrabBagType(NO_GRAB_BAG) {}
 
-    PropValue& operator=(const PropValue& rProp) { m_aValue = rProp.m_aValue; m_bGrabBag = rProp.m_bGrabBag; return *this; }
+    PropValue& operator=(const PropValue& rProp) { m_aValue = rProp.m_aValue; m_rGrabBagType = rProp.m_rGrabBagType; return *this; }
 
     const uno::Any& getValue() const { return m_aValue; }
-    bool hasGrabBag() const { return m_bGrabBag; }
+    bool hasGrabBag() const { return m_rGrabBagType != NO_GRAB_BAG; }
+    GrabBagType getGrabBagType() const { return m_rGrabBagType; }
 };
 typedef std::map< PropertyIds, PropValue > _PropertyMap;
 
@@ -108,7 +116,7 @@ public:
     bool hasEmptyPropertyValues() const {return !m_aValues.getLength();}
     /** Add property, usually overwrites already available attributes. It shouldn't overwrite in case of default attributes
      */
-    void Insert( PropertyIds eId, const ::com::sun::star::uno::Any& rAny, bool bOverwrite = true, bool bGrabBag = false );
+    void Insert( PropertyIds eId, const ::com::sun::star::uno::Any& rAny, bool bOverwrite = true, GrabBagType rGrabBagType = NO_GRAB_BAG );
     void Insert( PropertyIds eId, const PropValue& rValue, bool bOverwrite = true );
     void InsertProps(const boost::shared_ptr<PropertyMap> pMap);
     const ::com::sun::star::uno::Reference< ::com::sun::star::text::XFootnote>&  GetFootnote() const;

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1197,7 +1197,7 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
             m_pImpl->GetTopContext()->Insert( PROP_PARA_TOP_MARGIN, uno::makeAny( ConversionHelper::convertTwipToMM100(default_spacing) ) );
             if  (nIntValue) // If auto spacing is set, then only store set value in InteropGrabBag
             {
-                m_pImpl->GetTopContext()->Insert( PROP_PARA_TOP_MARGIN_BEFORE_AUTO_SPACING, uno::makeAny( ConversionHelper::convertTwipToMM100(default_spacing) ),true,true );
+                m_pImpl->GetTopContext()->Insert( PROP_PARA_TOP_MARGIN_BEFORE_AUTO_SPACING, uno::makeAny( ConversionHelper::convertTwipToMM100(default_spacing) ),true, PARA_GRAB_BAG );
             }
         }
         break;
@@ -1212,7 +1212,7 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
             m_pImpl->GetTopContext()->Insert( PROP_PARA_BOTTOM_MARGIN, uno::makeAny( ConversionHelper::convertTwipToMM100(default_spacing) ) );
             if  (nIntValue) // If auto spacing is set, then only store set value in InteropGrabBag
             {
-                m_pImpl->GetTopContext()->Insert( PROP_PARA_BOTTOM_MARGIN_AFTER_AUTO_SPACING, uno::makeAny( ConversionHelper::convertTwipToMM100(default_spacing) ),true,true );
+                m_pImpl->GetTopContext()->Insert( PROP_PARA_BOTTOM_MARGIN_AFTER_AUTO_SPACING, uno::makeAny( ConversionHelper::convertTwipToMM100(default_spacing) ),true, PARA_GRAB_BAG );
             }
         }
         break;
@@ -3475,7 +3475,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext, SprmType
         rContext->Insert(PROP_PARA_CONTEXT_MARGIN, uno::makeAny( sal_Bool( nIntValue ) ));
     break;
     case 0x2470: // mirrorIndents
-        rContext->Insert(PROP_MIRROR_INDENTS, uno::makeAny(sal_Bool(nIntValue)), true, true);
+        rContext->Insert(PROP_MIRROR_INDENTS, uno::makeAny(sal_Bool(nIntValue)), true, PARA_GRAB_BAG);
     break;
     case NS_ooxml::LN_EG_SectPrContents_formProt: //section protection, only form editing is enabled - unsupported
     case NS_ooxml::LN_EG_SectPrContents_vAlign:

commit ebafa2ab7b87dd9035021a5ef72a1fd2844f7706

DOCX import: fix missing underline in comment text
Regression from 0db9fab7cd757a5b0392f1fc8b0c827922982f9b (sw: Add
support for different grab bags at PropertyMap., 2013-11-29),
PropertyMap::GetPropertyValues() assumed all XTextRange implementations
support the new property, which is not true for at least editeng.

Change-Id: Ib5657be522d30f203cecbbbae74d6594cef984fb

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1120,7 +1120,8 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, PropertyMapP
     {
         try
         {
-            uno::Sequence< beans::PropertyValue > pValues = pPropertyMap->GetPropertyValues();
+            // If we are in comments, then disable CharGrabBag, comment text doesn't support that.
+            uno::Sequence< beans::PropertyValue > pValues = pPropertyMap->GetPropertyValues(/*bCharGrabBag=*/!m_bIsInComments);
             sal_Int32 len = pValues.getLength();
 
             if (m_bStartTOC || m_bStartIndex)

writerfilter/source/dmapper/PropertyMap.cxx
@@ -60,7 +60,7 @@ PropertyMap::~PropertyMap()
 }
 
 
-uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues()
+uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues(bool bCharGrabBag)
 {
     if(!m_aValues.getLength() && size())
     {
@@ -73,8 +73,14 @@ uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues()
             else if ( i->second.getGrabBagType() == PARA_GRAB_BAG )
                 nParaGrabBag++;
         }
+
+        // In case there are properties to be grab-bagged and we can have a char grab-bag, allocate one slot for it.
+        size_t nCharGrabBagSize = 0;
+        if (bCharGrabBag)
+            nCharGrabBagSize = nCharGrabBag ? 1 : 0;
+
         // If there are any grab bag properties, we need one slot for them.
-        m_aValues.realloc( size() - nCharGrabBag + (nCharGrabBag ? 1 : 0)
+        m_aValues.realloc( size() - nCharGrabBag + nCharGrabBagSize
                                   - nParaGrabBag + (nParaGrabBag ? 1 : 0));
         ::com::sun::star::beans::PropertyValue* pValues = m_aValues.getArray();
         uno::Sequence<beans::PropertyValue> aCharGrabBagValues(nCharGrabBag);
@@ -116,9 +122,12 @@ uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues()
             {
                 if ( aMapIter->second.getGrabBagType() == CHAR_GRAB_BAG )
                 {
-                    pCharGrabBagValues[nCharGrabBagValue].Name = rPropNameSupplier.GetName( aMapIter->first );
-                    pCharGrabBagValues[nCharGrabBagValue].Value = aMapIter->second.getValue();
-                    ++nCharGrabBagValue;
+                    if (bCharGrabBag)
+                    {
+                        pCharGrabBagValues[nCharGrabBagValue].Name = rPropNameSupplier.GetName( aMapIter->first );
+                        pCharGrabBagValues[nCharGrabBagValue].Value = aMapIter->second.getValue();
+                        ++nCharGrabBagValue;
+                    }
                 }
                 else if ( aMapIter->second.getGrabBagType() == PARA_GRAB_BAG )
                 {
@@ -134,7 +143,7 @@ uno::Sequence< beans::PropertyValue > PropertyMap::GetPropertyValues()
                 }
             }
         }
-        if (nCharGrabBag)
+        if (nCharGrabBag && bCharGrabBag)
         {
             pValues[nValue].Name = "CharInteropGrabBag";
             pValues[nValue].Value = uno::makeAny(aCharGrabBagValues);

writerfilter/source/dmapper/PropertyMap.hxx
@@ -112,7 +112,7 @@ public:
     PropertyMap();
     virtual ~PropertyMap();
 
-    ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > GetPropertyValues();
+    ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > GetPropertyValues(bool bCharGrabBag = true);
     bool hasEmptyPropertyValues() const {return !m_aValues.getLength();}
     /** Add property, usually overwrites already available attributes. It shouldn't overwrite in case of default attributes
      */

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -13,6 +13,7 @@
 #if !defined(WNT)
 
 #include <com/sun/star/awt/XBitmap.hpp>
+#include <com/sun/star/awt/FontUnderline.hpp>
 #include <com/sun/star/awt/FontWeight.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/document/XEmbeddedObjectSupplier2.hpp>
@@ -1869,6 +1870,17 @@ DECLARE_OOXMLIMPORT_TEST(testFloatingTablesAnchor, "floating-tables-anchor.docx"
     xText.set(xRange->getText(), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("Anchor point"), xText->getString());
 }
+
+DECLARE_OOXMLIMPORT_TEST(testAnnotationFormatting, "annotation-formatting.docx")
+{
+    uno::Reference<beans::XPropertySet> xTextField = getProperty< uno::Reference<beans::XPropertySet> >(getRun(getParagraph(2), 2), "TextField");
+    uno::Reference<text::XText> xText = getProperty< uno::Reference<text::XText> >(xTextField, "TextRange");
+    // Make sure we test the right annotation.
+    uno::Reference<text::XTextRange> xParagraph = getParagraphOfText(1, xText, "days");
+    // Formatting was lost: the second text portion was NONE, not SINGLE.
+    CPPUNIT_ASSERT_EQUAL(awt::FontUnderline::SINGLE, getProperty<sal_Int16>(getRun(xParagraph, 1), "CharUnderline"));
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();