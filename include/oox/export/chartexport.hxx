LibreOffice/core

fdo#74826: File corruption-Issue related to Series idx & order val.
Problem Description:
- If the document contains more than one charts together.
  ex.Bar & Line chart
- In that case, LO writes idx & order val equal to 0,
  instead of 1 for second chart series.
- After roundtrip, the document get corrupted.

Implementation:
- Added varible to take the series count
  in case of multiple chart.

Note:
- Some of the UT's are failing when --enable-dbgutil is enabled.

Change-Id: I40606b4d69026939fa19ae534dd7b2bb36ec97fc

include/oox/export/chartexport.hxx
@@ -83,6 +83,7 @@ public:
 
 private:
     sal_Int32           mnXmlNamespace;
+    sal_Int32           mSeriesCount;
     Fraction            maFraction;
     ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel > mxChartModel;
     com::sun::star::uno::Reference< com::sun::star::chart::XDiagram > mxDiagram;

LemmingAvalanche/libreoffice-repaired

include/oox/export/chartexport.hxx
@@ -83,6 +83,7 @@ public:
 
 private:
     sal_Int32           mnXmlNamespace;
+    sal_Int32           mnSeriesCount;
     Fraction            maFraction;
     ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel > mxChartModel;
     com::sun::star::uno::Reference< com::sun::star::chart::XDiagram > mxDiagram;