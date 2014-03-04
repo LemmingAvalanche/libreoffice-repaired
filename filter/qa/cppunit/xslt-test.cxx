LemmingAvalanche/libreoffice-repaired

commit 58827ddbf2cd370e0f78dc4ed2f6a32f31321989

rework selection of transformer for an XSLT filter
Change-Id: I765762f11813c6b612416a8db2707bf94114c876

filter/source/xsltfilter/LibXSLTTransformer.cxx
@@ -494,9 +494,11 @@ namespace XSLT
     }
 
     void
-    LibXSLTTransformer::initialize(const Sequence<Any>& params)
+    LibXSLTTransformer::initialize(const Sequence<Any>& args)
             throw (RuntimeException)
     {
+        Sequence<Any> params;
+        args[0] >>= params;
         xmlSubstituteEntitiesDefault(0);
         m_parameters.clear();
         for (int i = 0; i < params.getLength(); i++)

filter/source/xsltdialog/typedetectionexport.cxx
@@ -183,7 +183,7 @@ void TypeDetectionExporter::doExport( Reference< XOutputStream > xOS,  const XML
                 sValue += sComma;
                 sValue += sXSLTFilterService;
                 sValue += sDelim;
-                sValue += pFilter->maXSLTTransformerImpl;
+                sValue += OUString::valueOf( pFilter->mbNeedsXSLT2 );
                 sValue += sDelim;
 
                 const application_info_impl* pAppInfo = getApplicationInfo( pFilter->maExportService );

filter/source/xsltdialog/typedetectionimport.cxx
@@ -170,7 +170,7 @@ filter_info_impl* TypeDetectionImporter::createFilterForNode( Node * pNode )
 
     OUString aAdapterService( getSubdata( 0, aDelim, aFilterUserData ) );
     //Import/ExportService
-    pFilter->maXSLTTransformerImpl = getSubdata( 1, aDelim, aFilterUserData );
+    pFilter->mbNeedsXSLT2 = getSubdata( 1, aDelim, aFilterUserData ).toBoolean();
     pFilter->maImportService = getSubdata( 2, aDelim, aFilterUserData );
     pFilter->maExportService = getSubdata( 3, aDelim, aFilterUserData );
     pFilter->maImportXSLT = getSubdata( 4, aDelim, aFilterUserData );

filter/source/xsltdialog/xmlfiltercommon.hxx
@@ -68,7 +68,7 @@ public:
 
     sal_Bool        mbReadonly;
 
-    rtl::OUString   maXSLTTransformerImpl;
+    sal_Bool        mbNeedsXSLT2;
 
     filter_info_impl();
     filter_info_impl( const filter_info_impl& rInfo );

filter/source/xsltdialog/xmlfilterhelpids.hrc
@@ -41,8 +41,7 @@
 #define HID_XML_FILTER_DOCTYPE                              "FILTER_HID_XML_FILTER_DOCTYPE"
 #define HID_XML_FILTER_DTD                                  "FILTER_HID_XML_FILTER_DTD"
 #define HID_XML_FILTER_DTD_BROWSE                           "FILTER_HID_XML_FILTER_DTD_BROWSE"
-#define HID_XML_FILTER_TRANSFORM_SERVICE_SAXON_J			"FILTER_HID_XML_FILTER_TRANSFORM_SERVICE_SAXON_J"
-#define HID_XML_FILTER_TRANSFORM_SERVICE_LIBXSLT			"FILTER_HID_XML_FILTER_TRANSFORM_SERVICE_LIBXSLT"
+#define HID_XML_FILTER_NEEDS_XSLT2			    "FILTER_HID_XML_FILTER_NEEDS_XSLT2"
 #define HID_XML_FILTER_EXPORT_XSLT                          "FILTER_HID_XML_FILTER_EXPORT_XSLT"
 #define HID_XML_FILTER_EXPORT_XSLT_BROWSE                   "FILTER_HID_XML_FILTER_EXPORT_XSLT_BROWSE"
 #define HID_XML_FILTER_IMPORT_XSLT                          "FILTER_HID_XML_FILTER_IMPORT_XSLT"

filter/source/xsltdialog/xmlfiltersettingsdialog.cxx
@@ -1152,7 +1152,7 @@ void XMLFilterSettingsDialog::initFilterList()
                     continue;
 
                 // get filter information from userdata
-                pTempFilter->maXSLTTransformerImpl = aUserData[1];
+                pTempFilter->mbNeedsXSLT2 = aUserData[1].toBoolean();
                 pTempFilter->maImportService = aUserData[2];
                 pTempFilter->maExportService = aUserData[3];
                 pTempFilter->maImportXSLT = aUserData[4];
@@ -1572,7 +1572,8 @@ filter_info_impl::filter_info_impl()
 :   maFlags(0x00080040),
     maFileFormatVersion(0),
     mnDocumentIconID(0),
-    mbReadonly(sal_False)
+    mbReadonly(sal_False),
+    mbNeedsXSLT2(sal_False)
 {
 }
 
@@ -1597,7 +1598,7 @@ filter_info_impl::filter_info_impl( const filter_info_impl& rInfo ) :
     maFileFormatVersion( rInfo.maFileFormatVersion ),
     mnDocumentIconID( rInfo.mnDocumentIconID ),
     mbReadonly( rInfo.mbReadonly ),
-    maXSLTTransformerImpl( rInfo.maXSLTTransformerImpl )
+    mbNeedsXSLT2( rInfo.mbNeedsXSLT2 )
 {
 }
 
@@ -1621,7 +1622,7 @@ int filter_info_impl::operator==( const filter_info_impl& r ) const
         maImportTemplate != r.maImportTemplate ||
         maFlags != r.maFlags ||
         maFileFormatVersion != r.maFileFormatVersion ||
-        maXSLTTransformerImpl != r.maXSLTTransformerImpl
+        mbNeedsXSLT2 != r.mbNeedsXSLT2
         )
         return false;
 
@@ -1635,7 +1636,7 @@ Sequence< OUString > filter_info_impl::getFilterUserData() const
     Sequence< OUString > aUserData(8);
 
     aUserData[0] = OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.documentconversion.XSLTFilter" ) );
-    aUserData[1] = maXSLTTransformerImpl;
+    aUserData[1] = OUString::valueOf( mbNeedsXSLT2 );
     aUserData[2] = maImportService;
     aUserData[3] = maExportService;
     aUserData[4] = maImportXSLT;

filter/source/xsltdialog/xmlfiltertabpagexslt.cxx
@@ -59,9 +59,8 @@ XMLFilterTabPageXSLT::XMLFilterTabPageXSLT( Window* pParent, ResMgr& rResMgr, co
     maEDImportTemplate( this, ResId( ED_XML_IMPORT_TEMPLATE, rResMgr ), INET_PROT_FILE ),
     maPBImportTemplate( this, ResId( PB_XML_IMPORT_TEMPLATE_BROWSE, rResMgr ) ),
 
-    maFTTransformationService( this, ResId (FT_XML_TRANSFORM_SERVICE, rResMgr ) ),
-    maRBTransformationServiceLibXSLT( this, ResId (RB_XML_TRANSFORM_SERVICE_LIBXSLT, rResMgr ) ),
-    maRBTransformationServiceSaxonJ( this, ResId (RB_XML_TRANSFORM_SERVICE_SAXON_J, rResMgr ) ),
+    maFTNeedsXSLT2( this, ResId (FT_XML_NEEDS_XSLT2, rResMgr ) ),
+    maCBNeedsXSLT2( this, ResId (CB_XML_NEEDS_XSLT2, rResMgr ) ),
 
     sHTTPSchema( RTL_CONSTASCII_USTRINGPARAM( "http://" ) ),
     sSHTTPSchema( RTL_CONSTASCII_USTRINGPARAM( "shttp://" ) ),
@@ -91,8 +90,7 @@ XMLFilterTabPageXSLT::XMLFilterTabPageXSLT( Window* pParent, ResMgr& rResMgr, co
     maEDExportXSLT.SetHelpId( HID_XML_FILTER_EXPORT_XSLT );
     maEDImportXSLT.SetHelpId( HID_XML_FILTER_IMPORT_XSLT );
     maEDImportTemplate.SetHelpId( HID_XML_FILTER_IMPORT_TEMPLATE );
-    maRBTransformationServiceLibXSLT.SetHelpId( HID_XML_FILTER_TRANSFORM_SERVICE_LIBXSLT );
-    maRBTransformationServiceSaxonJ.SetHelpId( HID_XML_FILTER_TRANSFORM_SERVICE_SAXON_J );
+    maCBNeedsXSLT2.SetHelpId( HID_XML_FILTER_NEEDS_XSLT2 );
 }
 
 XMLFilterTabPageXSLT::~XMLFilterTabPageXSLT()
@@ -108,10 +106,7 @@ bool XMLFilterTabPageXSLT::FillInfo( filter_info_impl* pInfo )
         pInfo->maExportXSLT = GetURL( maEDExportXSLT );
         pInfo->maImportXSLT = GetURL( maEDImportXSLT );
         pInfo->maImportTemplate = GetURL( maEDImportTemplate );
-        pInfo->maXSLTTransformerImpl
-                = maRBTransformationServiceSaxonJ.IsChecked() ? OUString(
-                        RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.JAXTHelper" ))
-                        : OUString(RTL_CONSTASCII_USTRINGPARAM( "" ));
+        pInfo->mbNeedsXSLT2 = maCBNeedsXSLT2.IsChecked();
     }
 
     return true;
@@ -127,12 +122,7 @@ void XMLFilterTabPageXSLT::SetInfo(const filter_info_impl* pInfo)
         SetURL( maEDExportXSLT, pInfo->maExportXSLT );
         SetURL( maEDImportXSLT, pInfo->maImportXSLT );
         SetURL( maEDImportTemplate, pInfo->maImportTemplate );
-        if (pInfo->maXSLTTransformerImpl.equals(OUString(
-                RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.JAXTHelper" )))) {
-            maRBTransformationServiceSaxonJ.Check();
-        } else {
-            maRBTransformationServiceLibXSLT.Check();
-        }
+        maCBNeedsXSLT2.Check( pInfo->mbNeedsXSLT2 );
     }
 }
 

filter/source/xsltdialog/xmlfiltertabpagexslt.hrc
@@ -35,8 +35,7 @@
 #define FT_XML_IMPORT_TEMPLATE          12
 #define ED_XML_IMPORT_TEMPLATE          13
 #define PB_XML_IMPORT_TEMPLATE_BROWSE   14
-#define FT_XML_TRANSFORM_SERVICE		15
-#define RB_XML_TRANSFORM_SERVICE_LIBXSLT 16
-#define RB_XML_TRANSFORM_SERVICE_SAXON_J 17
+#define FT_XML_NEEDS_XSLT2		15
+#define CB_XML_NEEDS_XSLT2		16
 
 #endif

commit 7e458aaf6fac2b4fb183ed14f3cca07339d05a7e

filter: make LibXSLTTransformer init backward compatible
As reported by Fernand Vanrie, initializing the XSLT service can fail
because the LibXSLTTransformer::initialize was changed to require
the parameters as a nested Sequence; accept previous parameter
convention as a fall-back.

(regression from 58827ddbf2cd370e0f78dc4ed2f6a32f31321989)

Change-Id: I136e6e6338f11ffecf9f856f0736d1d0e6b17c3f

filter/CppunitTest_filter_xslt.mk
@@ -1,0 +1,43 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,filter_xslt))
+
+$(eval $(call gb_CppunitTest_use_api,filter_xslt,\
+	offapi \
+	udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,filter_xslt))
+
+$(eval $(call gb_CppunitTest_use_configuration,filter_xslt))
+
+$(eval $(call gb_CppunitTest_use_libraries,filter_xslt, \
+	comphelper \
+	test \
+	unotest \
+	cppuhelper \
+	cppu \
+	sal \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_use_components,filter_xslt,\
+	configmgr/source/configmgr \
+	filter/source/xsltfilter/xsltfilter \
+	i18npool/util/i18npool \
+	ucb/source/core/ucb1 \
+	ucb/source/ucp/file/ucpfile1 \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,filter_xslt, \
+	filter/qa/cppunit/xslt-test \
+))
+
+# vim: set noet sw=4 ts=4:

filter/Module_filter.mk
@@ -77,6 +77,10 @@ $(eval $(call gb_Module_add_targets,filter,\
 ))
 endif
 
+$(eval $(call gb_Module_add_check_targets,filter,\
+    CppunitTest_filter_xslt \
+))
+
 ifneq ($(DISABLE_CVE_TESTS),TRUE)
 $(eval $(call gb_Module_add_check_targets,filter,\
     CppunitTest_filter_pict_test \

filter/source/xsltfilter/LibXSLTTransformer.cxx
@@ -478,7 +478,10 @@ namespace XSLT
             throw (RuntimeException, std::exception)
     {
         Sequence<Any> params;
-        args[0] >>= params;
+        if (!(args[0] >>= params))
+        {   // backward compatibility for old clients using createInstance
+            params = args;
+        }
         xmlSubstituteEntitiesDefault(0);
         m_parameters.clear();
         for (int i = 0; i < params.getLength(); i++)

filter/qa/cppunit/data/xslt/copy.xslt
@@ -1,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">
+   <xsl:copy-of select="/"/>
+</xsl:template>
+
+</xsl:stylesheet>

filter/qa/cppunit/xslt-test.cxx
@@ -1,0 +1,190 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <limits>
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include <sal/types.h>
+
+#include <rtl/ref.hxx>
+
+#include <osl/file.hxx>
+#include <osl/thread.h>
+
+#include <com/sun/star/beans/NamedValue.hpp>
+#include <com/sun/star/io/XStreamListener.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/ucb/SimpleFileAccess.hpp>
+#include <com/sun/star/xml/xslt/XSLTTransformer.hpp>
+
+#include <cppuhelper/implbase1.hxx>
+
+#include <test/bootstrapfixture.hxx>
+
+
+using namespace std;
+using namespace ::com::sun::star;
+
+
+namespace {
+
+class XsltFilterTest
+    : public test::BootstrapFixture
+{
+public:
+    void testXsltCopyOld();
+    void testXsltCopyNew();
+
+    CPPUNIT_TEST_SUITE(XsltFilterTest);
+    CPPUNIT_TEST(testXsltCopyOld);
+    CPPUNIT_TEST(testXsltCopyNew);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+struct Listener : public ::cppu::WeakImplHelper1<io::XStreamListener>
+{
+    bool m_bDone;
+
+    Listener() : m_bDone(false) {}
+
+    virtual void disposing(const lang::EventObject&) throw() SAL_OVERRIDE {}
+    virtual void SAL_CALL started() throw() SAL_OVERRIDE { m_bDone = false; }
+    virtual void SAL_CALL closed() throw() SAL_OVERRIDE { m_bDone = true; }
+    virtual void SAL_CALL terminated() throw() SAL_OVERRIDE { m_bDone = true; }
+    virtual void SAL_CALL error(const uno::Any& e) throw() SAL_OVERRIDE
+    {
+        SAL_WARN("filter.xslt", "exception " << e);
+        CPPUNIT_FAIL("exception while in XSLT");
+    }
+};
+
+void XsltFilterTest::testXsltCopyNew()
+{
+    OUString tempDirURL;
+    osl_getTempDirURL(&tempDirURL.pData);
+    oslFileHandle tempFile;
+    OUString tempURL;
+    osl::File::RC rc = osl::File::createTempFile(0, &tempFile, &tempURL);
+    CPPUNIT_ASSERT(osl::FileBase::E_None == rc);
+
+    OUString source(
+            getURLFromSrc("/filter/source/xsltfilter/xsltfilter.component"));
+    uno::Sequence<uno::Any> args(7);
+    args[0] <<= beans::NamedValue("StylesheetURL",
+        uno::makeAny(getURLFromSrc("/filter/qa/cppunit/data/xslt/copy.xslt")));
+    args[1] <<= beans::NamedValue("SourceURL", uno::makeAny(source));
+    args[2] <<= beans::NamedValue("TargetURL", uno::makeAny(tempURL));
+    args[3] <<= beans::NamedValue("SourceBaseURL",
+        uno::makeAny(getURLFromSrc("/filter/source/xsltfilter/")));
+    args[4] <<= beans::NamedValue("TargetBaseURL", uno::makeAny(tempDirURL));
+    args[5] <<= beans::NamedValue("SystemType", uno::makeAny(OUString()));
+    args[6] <<= beans::NamedValue("PublicType", uno::makeAny(OUString()));
+
+    uno::Reference<ucb::XSimpleFileAccess3> xSFA =
+        ucb::SimpleFileAccess::create(getComponentContext());
+
+    uno::Reference<io::XInputStream> xIn = xSFA->openFileRead(source);
+    uno::Reference<io::XOutputStream> xOut = xSFA->openFileWrite(tempURL);
+
+    rtl::Reference<Listener> xListener = new Listener();
+
+    uno::Reference<xml::xslt::XXSLTTransformer> xXslt(
+            xml::xslt::XSLTTransformer::create(getComponentContext(), args));
+
+    xXslt->addListener(uno::Reference<io::XStreamListener>(xListener.get()));
+    xXslt->setInputStream(xIn);
+    xXslt->setOutputStream(xOut);
+
+    xXslt->start();
+
+    TimeValue delay;
+    delay.Seconds = 0;
+    delay.Nanosec = 1000000;
+    while (!xListener->m_bDone) { osl_waitThread(&delay); }
+
+    xIn->closeInput();
+    xOut->closeOutput();
+
+    osl::File foo(tempURL); // apparently it's necessary to open it again?
+    foo.open(osl_File_OpenFlag_Read);
+    sal_uInt64 size(0);
+    foo.getSize(size);
+    CPPUNIT_ASSERT(size > 1000); // check that something happened
+}
+
+void XsltFilterTest::testXsltCopyOld()
+{
+    OUString tempDirURL;
+    osl_getTempDirURL(&tempDirURL.pData);
+    oslFileHandle tempFile;
+    OUString tempURL;
+    osl::File::RC rc = osl::File::createTempFile(0, &tempFile, &tempURL);
+    CPPUNIT_ASSERT(osl::FileBase::E_None == rc);
+
+    OUString source(
+            getURLFromSrc("/filter/source/xsltfilter/xsltfilter.component"));
+    uno::Sequence<uno::Any> args(7);
+    args[0] <<= beans::NamedValue("StylesheetURL",
+        uno::makeAny(getURLFromSrc("/filter/qa/cppunit/data/xslt/copy.xslt")));
+    args[1] <<= beans::NamedValue("SourceURL", uno::makeAny(source));
+    args[2] <<= beans::NamedValue("TargetURL", uno::makeAny(tempURL));
+    args[3] <<= beans::NamedValue("SourceBaseURL",
+        uno::makeAny(getURLFromSrc("/filter/source/xsltfilter/")));
+    args[4] <<= beans::NamedValue("TargetBaseURL", uno::makeAny(tempDirURL));
+    args[5] <<= beans::NamedValue("SystemType", uno::makeAny(OUString()));
+    args[6] <<= beans::NamedValue("PublicType", uno::makeAny(OUString()));
+
+
+    uno::Reference<ucb::XSimpleFileAccess3> xSFA =
+        ucb::SimpleFileAccess::create(getComponentContext());
+
+    uno::Reference<io::XInputStream> xIn = xSFA->openFileRead(source);
+    uno::Reference<io::XOutputStream> xOut = xSFA->openFileWrite(tempURL);
+
+    rtl::Reference<Listener> xListener = new Listener();
+
+    uno::Reference<xml::xslt::XXSLTTransformer> xXslt(
+        getMultiServiceFactory()->createInstance(
+            "com.sun.star.comp.documentconversion.LibXSLTTransformer"),
+        uno::UNO_QUERY_THROW);
+
+    uno::Reference<lang::XInitialization> xInit(xXslt, uno::UNO_QUERY_THROW);
+    xInit->initialize(args);
+    xXslt->addListener(uno::Reference<io::XStreamListener>(xListener.get()));
+    xXslt->setInputStream(xIn);
+    xXslt->setOutputStream(xOut);
+
+    xXslt->start();
+
+    TimeValue delay;
+    delay.Seconds = 0;
+    delay.Nanosec = 1000000;
+    while (!xListener->m_bDone) { osl_waitThread(&delay); }
+
+    xIn->closeInput();
+    xOut->closeOutput();
+
+    osl::File foo(tempURL); // apparently it's necessary to open it again?
+    foo.open(osl_File_OpenFlag_Read);
+    sal_uInt64 size(0);
+    foo.getSize(size);
+    CPPUNIT_ASSERT(size > 1000); // check that something happened
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(XsltFilterTest);
+
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */