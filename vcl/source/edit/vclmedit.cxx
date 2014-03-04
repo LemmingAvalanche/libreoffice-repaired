LemmingAvalanche/libreoffice-repaired

commit dcfa23cfb6770e2c95a749d44b4f76440cb32311

Integrate branch of IAccessible2
Change-Id: I3b5936bb92bb258f9e9e76402dd2b55f29aa686a

vcl/source/edit/vclmedit.cxx
@@ -901,7 +901,6 @@ void TextWindow::Command( const CommandEvent& rCEvt )
 
 void TextWindow::GetFocus()
 {
-    Window::GetFocus();
     if ( !mbActivePopup )
     {
         sal_Bool bGotoCursor = !mpExtTextView->IsReadOnly();
@@ -1195,7 +1194,6 @@ void VclMultiLineEdit::GetFocus()
     if ( !pImpVclMEdit )  // might be called from within the dtor, when pImpVclMEdit == NULL is a valid state
         return;
 
-    Edit::GetFocus();
     pImpVclMEdit->GetFocus();
 }
 

include/svtools/accessibleruler.hxx
@@ -1,0 +1,247 @@
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef _SVTRULERACCESSIBLE_HXX
+#define _SVTRULERACCESSIBLE_HXX
+
+#include <com/sun/star/accessibility/XAccessible.hpp>
+#include <com/sun/star/accessibility/XAccessibleComponent.hpp>
+#include <com/sun/star/accessibility/XAccessibleContext.hpp>
+#include <com/sun/star/accessibility/XAccessibleEventBroadcaster.hpp>
+#include <com/sun/star/accessibility/IllegalAccessibleComponentStateException.hpp>
+
+#include <com/sun/star/beans/XPropertyChangeListener.hpp>
+#include <com/sun/star/uno/Reference.hxx>
+#include <cppuhelper/weak.hxx>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/lang/XTypeProvider.hpp>
+#include <com/sun/star/lang/XServiceName.hpp>
+#include <com/sun/star/lang/IndexOutOfBoundsException.hpp>
+#include <com/sun/star/lang/DisposedException.hpp>
+#include <cppuhelper/interfacecontainer.h>
+#include <cppuhelper/compbase5.hxx>
+#include <comphelper/broadcasthelper.hxx>
+#include <cppuhelper/implbase5.hxx>
+#include <comphelper/servicehelper.hxx>
+
+namespace com { namespace sun { namespace star { namespace awt {
+    struct Point;
+    struct Rectangle;
+    struct Size;
+    class XFocusListener;
+} } } }
+
+class Rectangle;
+class Ruler;
+class Window;
+
+
+typedef ::cppu::WeakAggComponentImplHelper5<
+            ::com::sun::star::accessibility::XAccessible,
+            ::com::sun::star::accessibility::XAccessibleComponent,
+            ::com::sun::star::accessibility::XAccessibleContext,
+            ::com::sun::star::accessibility::XAccessibleEventBroadcaster,
+            ::com::sun::star::lang::XServiceInfo >
+            SvtRulerAccessible_Base;
+
+class SvtRulerAccessible : public ::comphelper::OBaseMutex, public SvtRulerAccessible_Base
+{
+public:
+    //=====  internal  ========================================================
+    SvtRulerAccessible(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible>& rxParent, Ruler& rRepresentation, const ::rtl::OUString& rName );
+protected:
+    virtual ~SvtRulerAccessible();
+public:
+    //=====  XAccessible  =====================================================
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleContext> SAL_CALL
+        getAccessibleContext( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    //=====  XAccessibleComponent  ============================================
+
+    virtual sal_Bool SAL_CALL
+        containsPoint( const ::com::sun::star::awt::Point& rPoint ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > SAL_CALL
+        getAccessibleAtPoint( const ::com::sun::star::awt::Point& rPoint ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::awt::Rectangle SAL_CALL
+        getBounds() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::awt::Point SAL_CALL
+        getLocation() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::awt::Point SAL_CALL
+        getLocationOnScreen() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::awt::Size SAL_CALL
+        getSize() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Bool SAL_CALL
+        isShowing() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Bool SAL_CALL
+        isVisible() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Bool SAL_CALL
+        isFocusTraversable() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL
+        addFocusListener(
+            const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XFocusListener >& xListener )
+            throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL
+        removeFocusListener(
+            const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XFocusListener >& xListener )
+            throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL
+        grabFocus() throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::uno::Any SAL_CALL
+        getAccessibleKeyBinding() throw( ::com::sun::star::uno::RuntimeException );
+    virtual sal_Int32 SAL_CALL
+        getForeground(  ) throw (::com::sun::star::uno::RuntimeException);
+    virtual sal_Int32 SAL_CALL
+        getBackground(  ) throw (::com::sun::star::uno::RuntimeException);
+
+    //=====  XAccessibleContext  ==============================================
+
+    virtual sal_Int32 SAL_CALL
+        getAccessibleChildCount( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible> SAL_CALL
+        getAccessibleChild( sal_Int32 nIndex )
+            throw( ::com::sun::star::uno::RuntimeException, ::com::sun::star::lang::IndexOutOfBoundsException );
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible> SAL_CALL
+        getAccessibleParent( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Int32 SAL_CALL
+        getAccessibleIndexInParent( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Int16 SAL_CALL
+        getAccessibleRole( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::rtl::OUString SAL_CALL
+        getAccessibleDescription( void ) throw (::com::sun::star::uno::RuntimeException);
+
+    virtual ::rtl::OUString SAL_CALL
+        getAccessibleName( void ) throw (::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleRelationSet > SAL_CALL
+        getAccessibleRelationSet( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleStateSet > SAL_CALL
+        getAccessibleStateSet( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::lang::Locale SAL_CALL
+        getLocale( void )
+            throw(  ::com::sun::star::uno::RuntimeException,
+                    ::com::sun::star::accessibility::IllegalAccessibleComponentStateException );
+    //=====  XAccessibleEventBroadcaster  =====================================
+
+    virtual void SAL_CALL
+        addAccessibleEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleEventListener >& xListener )throw( com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL
+        removeAccessibleEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleEventListener >& xListener ) throw( com::sun::star::uno::RuntimeException );
+
+    //=====  XServiceInfo  ====================================================
+
+    virtual ::rtl::OUString SAL_CALL
+        getImplementationName( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Bool SAL_CALL
+        supportsService( const ::rtl::OUString& sServiceName ) throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString> SAL_CALL
+        getSupportedServiceNames( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    //=====  XTypeProvider  ===================================================
+
+    virtual ::com::sun::star::uno::Sequence<sal_Int8> SAL_CALL
+        getImplementationId( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+
+
+
+public:
+
+    /// Sets the name
+    void setName( const ::rtl::OUString& rName );
+
+    /// Sets the description
+    void setDescription( const ::rtl::OUString& rDescr );
+private:
+    static ::com::sun::star::uno::Sequence< sal_Int8 > getUniqueId( void );
+protected:
+
+    /// @Return the object's current bounding box relative to the desktop.
+    virtual Rectangle GetBoundingBoxOnScreen( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+    /// @Return the object's current bounding box relative to the parent object.
+    virtual Rectangle GetBoundingBox( void ) throw( ::com::sun::star::uno::RuntimeException );
+
+
+    virtual void SAL_CALL disposing();
+
+    /// @returns true if it's disposed or in disposing
+    inline sal_Bool IsAlive( void ) const;
+
+    /// @returns true if it's not disposed and no in disposing
+    inline sal_Bool IsNotAlive( void ) const;
+
+    /// throws the exception DisposedException if it's not alive
+    void ThrowExceptionIfNotAlive( void ) throw( ::com::sun::star::lang::DisposedException );
+
+private:
+    /** Description of this object.  This is not a constant because it can
+        be set from the outside.
+    */
+    ::rtl::OUString                     msDescription;
+
+    /** Name of this object.
+    */
+    ::rtl::OUString                     msName;
+
+    /// Reference to the parent object.
+    ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible >
+                                        mxParent;
+
+    /// pointer to internal representation
+    Ruler*                          mpRepr;
+
+        /// client id in the AccessibleEventNotifier queue
+    sal_uInt32 mnClientId;
+
+
+};
+
+inline sal_Bool SvtRulerAccessible::IsAlive( void ) const
+{
+    return !rBHelper.bDisposed && !rBHelper.bInDispose;
+}
+
+inline sal_Bool SvtRulerAccessible::IsNotAlive( void ) const
+{
+    return rBHelper.bDisposed || rBHelper.bInDispose;
+}
+
+#endif

include/svtools/embedhlp.hxx
@@ -108,6 +108,8 @@ public:
     bool IsLocked() const;
     bool IsChart() const;
 
+    OUString GetChartType();
+
     // #i104867#
     // Provides a graphic version number for the fetchable Graphic during this object's lifetime. Internally,
     // that number is incremented at each change of the Graphic. This mechanism is needed to identify if a

include/svtools/headbar.hxx
@@ -231,6 +231,8 @@ typedef sal_uInt16 HeaderBarItemBits;
 // - HeaderBar -
 // -------------
 
+class VCLXHeaderBar;
+
 class SVT_DLLPUBLIC HeaderBar : public Window
 {
 private:
@@ -258,6 +260,9 @@ private:
     Link                maSelectHdl;
     Link                maDoubleClickHdl;
     Link                maCreateAccessibleHdl;
+
+    VCLXHeaderBar*      m_pVCLXHeaderBar;
+
 
     ::com::sun::star::uno::Reference<
         ::com::sun::star::accessibility::XAccessible >
@@ -368,6 +373,8 @@ public:
     virtual ::com::sun::star::uno::Reference<
         ::com::sun::star::accessibility::XAccessible >  CreateAccessible();
     void SetAccessible( ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > );
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindowPeer > GetComponentInterface( sal_Bool bCreate );
+
 };
 
 #endif // INCLUDED_SVTOOLS_HEADBAR_HXX

include/svtools/ruler.hxx
@@ -28,6 +28,8 @@
 
 #include <boost/scoped_ptr.hpp>
 
+#include <svtools/accessibleruler.hxx>
+
 class MouseEvent;
 class TrackingEvent;
 class DataChangedEvent;
@@ -658,6 +660,8 @@ private:
     boost::scoped_ptr<RulerSelection> mpCurrentHitTest;
     boost::scoped_ptr<RulerSelection> mpPreviousHitTest;
 
+    SvtRulerAccessible* pAccContext;
+
     SVT_DLLPRIVATE void ImplVDrawLine( long nX1, long nY1, long nX2, long nY2 );
     SVT_DLLPRIVATE void ImplVDrawRect( long nX1, long nY1, long nX2, long nY2 );
     SVT_DLLPRIVATE void ImplVDrawText( long nX, long nY, const OUString& rText, long nMin = LONG_MIN, long nMax = LONG_MAX );
@@ -813,6 +817,8 @@ public:
     void            SetLineHeight( long nHeight ) { mnLineHeight = nHeight ; }
 
     void            DrawTicks();
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > CreateAccessible();
 };
 
 #endif  // _RULER_HXX

include/svtools/svlbitm.hxx
@@ -101,6 +101,7 @@ public:
     virtual sal_uInt16 GetType() const;
     virtual void    InitViewData(SvTreeListBox*, SvTreeListEntry*, SvViewDataItem*);
     OUString   GetText() const { return maText; }
+    virtual OUString GetExtendText() const { return OUString(); }
     void            SetText( const OUString& rText ) { maText = rText; }
 
     virtual void Paint(
@@ -176,6 +177,7 @@ public:
 
     // Check whether this button can be modified via UI
     bool            CheckModification() const;
+    SvLBoxButtonData* GetButtonData() const{ return pData;}
 };
 
 inline void SvLBoxButton::SetStateChecked()

include/svtools/svtools.hrc
@@ -180,8 +180,9 @@
 #define STR_SVT_ACC_DESC_FOLDER             (STR_SVT_ACC_BEGIN+4)
 #define STR_SVT_ACC_DESC_FILE               (STR_SVT_ACC_BEGIN+5)
 #define STR_SVT_ACC_EMPTY_FIELD             (STR_SVT_ACC_BEGIN+6)
-//IAccessible2 implementation
-#define STR_SVT_ACC_RULER_HORZ_NAME (STR_SVT_ACC_BEGIN+9)
+#define STR_SVT_ACC_LISTENTRY_SELCTED_STATE (STR_SVT_ACC_BEGIN+7)
+#define STR_SVT_ACC_RULER_VERT_NAME         (STR_SVT_ACC_BEGIN+8)
+#define STR_SVT_ACC_RULER_HORZ_NAME         (STR_SVT_ACC_BEGIN+9)
 #define STR_SVT_ACC_END                     (STR_SVT_ACC_RULER_HORZ_NAME)
 
 #define STR_SVT_INDEXENTRY_START            (STR_SVT_ACC_END + 1)
@@ -313,7 +314,6 @@
 
 #define RID_IMG_PRNDLG_NOCOLLATE            (STR_SVT_PRNDLG_START + 30)
 
-// IAccessible2 implementation in 2009
 #define STR_PARAGRAPH_START                 (STR_SVT_PRNDLG_START + 50)
 #define STR_PARAGRAPH                       (STR_PARAGRAPH_START + 0)
 

include/svtools/treelistbox.hxx
@@ -97,6 +97,10 @@ enum SvButtonState { SV_BUTTON_UNCHECKED, SV_BUTTON_CHECKED, SV_BUTTON_TRISTATE 
 #define TREEFLAG_MANINS         0x0004
 #define TREEFLAG_RECALCTABS     0x0008
 
+#define TREEBOX_ALLITEM_ACCROLE_TYPE_NOTSET 0x00
+#define TREEBOX_ALLITEM_ACCROLE_TYPE_LIST   0x01
+#define TREEBOX_ALLITEM_ACCROLE_TYPE_TREE   0x02
+
 typedef sal_Int64   ExtendedWinBits;
 
 // disable the behavior of automatically selecting a "CurEntry" upon painting the control
@@ -106,6 +110,7 @@ typedef sal_Int64   ExtendedWinBits;
 #define SV_ITEM_ID_LBOXBMP          2
 #define SV_ITEM_ID_LBOXBUTTON       3
 #define SV_ITEM_ID_LBOXCONTEXTBMP   4
+#define SV_ITEM_ID_EXTENDRLBOXSTRING    5
 
 class SvLBoxTab
 {
@@ -226,6 +231,7 @@ class SVT_DLLPUBLIC SvTreeListBox
     short           nEntryHeightOffs;
     short           nIndent;
     short           nFocusWidth;
+    short           nAllItemAccRoleType;
     sal_uInt16      nFirstSelTab;
     sal_uInt16      nLastSelTab;
     long mnCheckboxItemWidth;
@@ -501,6 +507,17 @@ public:
 
     /** Enables, that one cell of a tablistbox entry can be focused */
     void EnableCellFocus();
+
+                        // For overwriting accessible role for all entries - normally 0, so each entry can be different
+    void                SetAllEntriesAccessibleRoleType( short n ) { nAllItemAccRoleType = n; }
+    short               GetAllEntriesAccessibleRoleType() const { return nAllItemAccRoleType; }
+
+    sal_uInt16          GetTreeFlags() const {return nTreeFlags;}
+
+    OUString            headString;
+    OUString            SearchEntryTextWithHeadTitle(SvTreeListEntry* pEntry);
+    virtual OUString    GetEntryAltText(SvTreeListEntry* pEntry) const;
+    virtual OUString    GetEntryLongDescription(SvTreeListEntry* pEntry) const;
 
     void set_min_width_in_chars(sal_Int32 nChars);
 

commit fbb123cbaa6c10d310dd9fb50dda5b55ad7a94c0

Resolves: fdo#75229 missing object:state-changed:focused
in multiline edits, focus remains stuck on internal
TextWindow instead of being placed on container MultiLineEdit

regression from dcfa23cfb6770e2c95a749d44b4f76440cb32311 which
removed it with no explanation

Change-Id: I9d6bd546e341651442538bf40de6c43c521868d7

vcl/source/edit/vclmedit.cxx
@@ -908,6 +908,7 @@ void TextWindow::Command( const CommandEvent& rCEvt )
 
 void TextWindow::GetFocus()
 {
+    Window::GetFocus();
     if ( !mbActivePopup )
     {
         bool bGotoCursor = !mpExtTextView->IsReadOnly();