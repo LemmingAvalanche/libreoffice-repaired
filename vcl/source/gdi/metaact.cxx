LemmingAvalanche/libreoffice-repaired

commit 28e4dc5254960ea38ffeb427aaeca8ae4bbba8c3

longparas: drop 16bit string limits in meta text actions
Change-Id: I35144034f002cdaaa759a84731aae5fa48c6aa5a

vcl/source/gdi/metaact.cxx
@@ -1188,7 +1188,7 @@ IMPL_META_ACTION( Text, META_TEXT_ACTION )
 // ------------------------------------------------------------------------
 
 MetaTextAction::MetaTextAction( const Point& rPt, const OUString& rStr,
-                                sal_uInt16 nIndex, sal_uInt16 nLen ) :
+                                sal_Int32 nIndex, sal_Int32 nLen ) :
     MetaAction  ( META_TEXT_ACTION ),
     maPt        ( rPt ),
     maStr       ( rStr ),
@@ -1242,10 +1242,10 @@ sal_Bool MetaTextAction::Compare( const MetaAction& rMetaAction ) const
 void MetaTextAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
 {
     WRITE_BASE_COMPAT( rOStm, 2, pData );
-    rOStm   << maPt;
+    rOStm  << maPt;
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
-    rOStm  .WriteUInt16( mnIndex );
-    rOStm  .WriteUInt16( mnLen );
+    rOStm.WriteInt32(mnIndex);
+    rOStm.WriteInt32(mnLen);
 
     write_lenPrefixed_uInt16s_FromOUString<sal_uInt16>(rOStm, maStr); // version 2
 }
@@ -1299,13 +1299,13 @@ MetaTextArrayAction::MetaTextArrayAction( const MetaTextArrayAction& rAction ) :
 MetaTextArrayAction::MetaTextArrayAction( const Point& rStartPt,
                                           const OUString& rStr,
                                           const sal_Int32* pDXAry,
-                                          sal_uInt16 nIndex,
-                                          sal_uInt16 nLen ) :
+                                          sal_Int32 nIndex,
+                                          sal_Int32 nLen ) :
     MetaAction  ( META_TEXTARRAY_ACTION ),
     maStartPt   ( rStartPt ),
     maStr       ( rStr ),
     mnIndex     ( nIndex ),
-    mnLen       ( ( nLen == STRING_LEN ) ? rStr.getLength() : nLen )
+    mnLen       ( nLen )
 {
     const sal_uLong nAryLen = pDXAry ? mnLen : 0;
 
@@ -1376,16 +1376,16 @@ sal_Bool MetaTextArrayAction::Compare( const MetaAction& rMetaAction ) const
 
 void MetaTextArrayAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
 {
-    const sal_uInt32 nAryLen = mpDXAry ? mnLen : 0;
+    const sal_Int32 nAryLen = mpDXAry ? mnLen : 0;
 
     WRITE_BASE_COMPAT( rOStm, 2, pData );
-    rOStm   << maStartPt;
+    rOStm  << maStartPt;
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
-    rOStm  .WriteUInt16( mnIndex );
-    rOStm  .WriteUInt16( mnLen );
-    rOStm  .WriteUInt32( nAryLen );
+    rOStm.WriteInt32(mnIndex);
+    rOStm.WriteInt32(mnLen);
+    rOStm.WriteInt32(nAryLen);
 
-    for( sal_uLong i = 0UL; i < nAryLen; i++ )
+    for (sal_Int32 i = 0; i < nAryLen; ++i)
         rOStm.WriteInt32( mpDXAry[ i ] );
 
     write_lenPrefixed_uInt16s_FromOUString<sal_uInt16>(rOStm, maStr); // version 2
@@ -1395,7 +1395,7 @@ void MetaTextArrayAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
 
 void MetaTextArrayAction::Read( SvStream& rIStm, ImplMetaReadData* pData )
 {
-    sal_uInt32 nAryLen;
+    sal_Int32 nAryLen;
 
     delete[] mpDXAry;
 
@@ -1421,8 +1421,8 @@ void MetaTextArrayAction::Read( SvStream& rIStm, ImplMetaReadData* pData )
             mpDXAry = new (std::nothrow)sal_Int32[ mnLen ];
             if ( mpDXAry )
             {
-                   sal_uLong i;
-                for( i = 0UL; i < nAryLen; i++ )
+                sal_Int32 i;
+                for( i = 0; i < nAryLen; i++ )
                     rIStm >> mpDXAry[ i ];
 
                 // #106172# setup remainder
@@ -1459,7 +1459,7 @@ IMPL_META_ACTION( StretchText, META_STRETCHTEXT_ACTION )
 
 MetaStretchTextAction::MetaStretchTextAction( const Point& rPt, sal_uInt32 nWidth,
                                               const OUString& rStr,
-                                              sal_uInt16 nIndex, sal_uInt16 nLen ) :
+                                              sal_Int32 nIndex, sal_Int32 nLen ) :
     MetaAction  ( META_STRETCHTEXT_ACTION ),
     maPt        ( rPt ),
     maStr       ( rStr ),
@@ -1516,11 +1516,11 @@ sal_Bool MetaStretchTextAction::Compare( const MetaAction& rMetaAction ) const
 void MetaStretchTextAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
 {
     WRITE_BASE_COMPAT( rOStm, 2, pData );
-    rOStm   << maPt;
+    rOStm  << maPt;
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
-    rOStm  .WriteUInt32( mnWidth );
-    rOStm  .WriteUInt16( mnIndex );
-    rOStm  .WriteUInt16( mnLen );
+    rOStm.WriteUInt32( mnWidth );
+    rOStm.WriteInt32( mnIndex );
+    rOStm.WriteInt32( mnLen );
 
     write_lenPrefixed_uInt16s_FromOUString<sal_uInt16>(rOStm, maStr); // version 2
 }
@@ -1599,9 +1599,9 @@ sal_Bool MetaTextRectAction::Compare( const MetaAction& rMetaAction ) const
 void MetaTextRectAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
 {
     WRITE_BASE_COMPAT( rOStm, 2, pData );
-    rOStm   << maRect;
+    rOStm  << maRect;
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
-    rOStm  .WriteUInt16( mnStyle );
+    rOStm.WriteUInt16( mnStyle );
 
     write_lenPrefixed_uInt16s_FromOUString<sal_uInt16>(rOStm, maStr); // version 2
 }

include/vcl/metaact.hxx
@@ -423,10 +423,10 @@ class VCL_DLLPUBLIC MetaTextAction : public MetaAction
 {
 private:
 
-    Point               maPt;
-    OUString       maStr;
-    sal_uInt16          mnIndex;
-    sal_uInt16          mnLen;
+    Point           maPt;
+    OUString        maStr;
+    sal_Int32       mnIndex;
+    sal_Int32       mnLen;
 
     virtual sal_Bool    Compare( const MetaAction& ) const;
 
@@ -434,26 +434,26 @@ public:
                         DECL_META_ACTION( Text, META_TEXT_ACTION )
 
     MetaTextAction( const Point& rPt, const OUString& rStr,
-                    sal_uInt16 nIndex, sal_uInt16 nLen );
+                    sal_Int32 nIndex, sal_Int32 nLen );
 
-    virtual void        Move( long nHorzMove, long nVertMove );
-    virtual void        Scale( double fScaleX, double fScaleY );
+    virtual void    Move( long nHorzMove, long nVertMove );
+    virtual void    Scale( double fScaleX, double fScaleY );
 
-    const Point&        GetPoint() const { return maPt; }
+    const Point&    GetPoint() const { return maPt; }
     const OUString& GetText() const { return maStr; }
-    sal_uInt16              GetIndex() const { return mnIndex; }
-    sal_uInt16              GetLen() const { return mnLen; }
+    sal_Int32       GetIndex() const { return mnIndex; }
+    sal_Int32       GetLen() const { return mnLen; }
 };
 
 class VCL_DLLPUBLIC MetaTextArrayAction : public MetaAction
 {
 private:
 
-    Point               maStartPt;
-    OUString       maStr;
-    sal_Int32*          mpDXAry;
-    sal_uInt16          mnIndex;
-    sal_uInt16          mnLen;
+    Point       maStartPt;
+    OUString    maStr;
+    sal_Int32*  mpDXAry;
+    sal_Int32   mnIndex;
+    sal_Int32   mnLen;
 
     virtual sal_Bool    Compare( const MetaAction& ) const;
 
@@ -464,35 +464,35 @@ public:
                         MetaTextArrayAction();
                         MetaTextArrayAction( const MetaTextArrayAction& rAction );
     MetaTextArrayAction( const Point& rStartPt, const OUString& rStr,
-                         const sal_Int32* pDXAry, sal_uInt16 nIndex,
-                         sal_uInt16 nLen );
+                         const sal_Int32* pDXAry, sal_Int32 nIndex,
+                         sal_Int32 nLen );
 
     virtual void        Execute( OutputDevice* pOut );
 
     virtual MetaAction* Clone();
 
-    virtual void        Move( long nHorzMove, long nVertMove );
-    virtual void        Scale( double fScaleX, double fScaleY );
+    virtual void    Move( long nHorzMove, long nVertMove );
+    virtual void    Scale( double fScaleX, double fScaleY );
 
-    virtual void        Write( SvStream& rOStm, ImplMetaWriteData* pData );
-    virtual void        Read( SvStream& rIStm, ImplMetaReadData* pData );
+    virtual void    Write( SvStream& rOStm, ImplMetaWriteData* pData );
+    virtual void    Read( SvStream& rIStm, ImplMetaReadData* pData );
 
-    const Point&        GetPoint() const { return maStartPt; }
+    const Point&    GetPoint() const { return maStartPt; }
     const OUString& GetText() const { return maStr; }
-    sal_uInt16              GetIndex() const { return mnIndex; }
-    sal_uInt16              GetLen() const { return mnLen; }
-    sal_Int32*          GetDXArray() const { return mpDXAry; }
+    sal_Int32       GetIndex() const { return mnIndex; }
+    sal_Int32       GetLen() const { return mnLen; }
+    sal_Int32*      GetDXArray() const { return mpDXAry; }
 };
 
 class VCL_DLLPUBLIC MetaStretchTextAction : public MetaAction
 {
 private:
 
-    Point               maPt;
-    OUString       maStr;
-    sal_uInt32          mnWidth;
-    sal_uInt16          mnIndex;
-    sal_uInt16          mnLen;
+    Point       maPt;
+    OUString    maStr;
+    sal_uInt32  mnWidth;
+    sal_Int32   mnIndex;
+    sal_Int32   mnLen;
 
     virtual sal_Bool    Compare( const MetaAction& ) const;
 
@@ -501,16 +501,16 @@ public:
 
     MetaStretchTextAction( const Point& rPt, sal_uInt32 nWidth,
                            const OUString& rStr,
-                           sal_uInt16 nIndex, sal_uInt16 nLen );
+                           sal_Int32 nIndex, sal_Int32 nLen );
 
-    virtual void        Move( long nHorzMove, long nVertMove );
-    virtual void        Scale( double fScaleX, double fScaleY );
+    virtual void    Move( long nHorzMove, long nVertMove );
+    virtual void    Scale( double fScaleX, double fScaleY );
 
-    const Point&        GetPoint() const { return maPt; }
+    const Point&    GetPoint() const { return maPt; }
     const OUString& GetText() const { return maStr; }
-    sal_uInt32          GetWidth() const { return mnWidth; }
-    sal_uInt16              GetIndex() const { return mnIndex; }
-    sal_uInt16              GetLen() const { return mnLen; }
+    sal_uInt32      GetWidth() const { return mnWidth; }
+    sal_Int32       GetIndex() const { return mnIndex; }
+    sal_Int32       GetLen() const { return mnLen; }
 };
 
 class VCL_DLLPUBLIC MetaTextRectAction : public MetaAction

vcl/source/filter/wmf/emfwr.cxx
@@ -1354,7 +1354,7 @@ void EMFWriter::ImplWrite( const GDIMetaFile& rMtf )
             case META_TEXT_ACTION:
             {
                 const MetaTextAction*   pA = (const MetaTextAction*) pAction;
-                const OUString          aText = pA->GetText().copy( pA->GetIndex(), std::min<sal_Int32>(pA->GetText().getLength() - pA->GetIndex(), pA->GetLen()) );
+                const OUString          aText = pA->GetText().copy( pA->GetIndex(), std::min(pA->GetText().getLength() - pA->GetIndex(), pA->GetLen()) );
 
                 ImplCheckTextAttr();
                 ImplWriteTextRecord( pA->GetPoint(), aText, NULL, 0 );
@@ -1374,7 +1374,7 @@ void EMFWriter::ImplWrite( const GDIMetaFile& rMtf )
             case META_TEXTARRAY_ACTION:
             {
                 const MetaTextArrayAction*  pA = (const MetaTextArrayAction*) pAction;
-                const OUString              aText = pA->GetText().copy( pA->GetIndex(), std::min<sal_Int32>(pA->GetText().getLength() - pA->GetIndex(), pA->GetLen()) );
+                const OUString              aText = pA->GetText().copy( pA->GetIndex(), std::min(pA->GetText().getLength() - pA->GetIndex(), pA->GetLen()) );
 
                 ImplCheckTextAttr();
                 ImplWriteTextRecord( pA->GetPoint(), aText, pA->GetDXArray(), 0 );
@@ -1384,7 +1384,7 @@ void EMFWriter::ImplWrite( const GDIMetaFile& rMtf )
             case META_STRETCHTEXT_ACTION:
             {
                 const MetaStretchTextAction*    pA = (const MetaStretchTextAction*) pAction;
-                const OUString                  aText = pA->GetText().copy( pA->GetIndex(), std::min<sal_Int32>(pA->GetText().getLength() - pA->GetIndex(), pA->GetLen()) );
+                const OUString                  aText = pA->GetText().copy( pA->GetIndex(), std::min(pA->GetText().getLength() - pA->GetIndex(), pA->GetLen()) );
 
                 ImplCheckTextAttr();
                 ImplWriteTextRecord( pA->GetPoint(), aText, NULL, pA->GetWidth() );

vcl/source/gdi/cvtsvm.cxx
@@ -161,7 +161,7 @@ void ImplWritePopAction( SvStream& rOStm )
     rOStm.WriteInt32( (sal_Int32) 4 );
 }
 
-void ImplWriteLineColor( SvStream& rOStm, const Color& rColor, sal_Int16 nStyle, sal_Int32 nWidth = 0L )
+void ImplWriteLineColor( SvStream& rOStm, const Color& rColor, sal_Int16 nStyle, sal_Int32 nWidth = 0 )
 {
     if( rColor.GetTransparency() > 127 )
         nStyle = 0;
@@ -497,7 +497,7 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
         rMtf.SetPrefMapMode( aMapMode );
         size_t nLastPolygonAction(0);
 
-        for( sal_Int32 i = 0L; i < nActions; i++ )
+        for (sal_Int32 i = 0; i < nActions; ++i)
         {
             rIStm >> nType;
             sal_Int32 nActBegin = rIStm.Tell();
@@ -828,7 +828,7 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
                     sal_Int32       nIndex, nLen;
 
                     rIStm >> aPt >> nIndex >> nLen >> nTmp;
-                    if ( nTmp && ( static_cast< sal_uInt32 >( nTmp ) < ( SAL_MAX_UINT16 - 1 ) ) )
+                    if (nTmp > 0)
                     {
                         OString aByteStr = read_uInt8s_ToOString(rIStm, nTmp);
                         sal_uInt8 nTerminator = 0;
@@ -838,7 +838,7 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
                         OUString aStr(OStringToOUString(aByteStr, eActualCharSet));
                         if ( nUnicodeCommentActionNumber == i )
                             ImplReadUnicodeComment( nUnicodeCommentStreamPos, rIStm, aStr );
-                        rMtf.AddAction( new MetaTextAction( aPt, aStr, (sal_uInt16) nIndex, (sal_uInt16) nLen ) );
+                        rMtf.AddAction( new MetaTextAction( aPt, aStr, nIndex, nLen ) );
                     }
                     rIStm.Seek( nActBegin + nActionSize );
                 }
@@ -850,7 +850,7 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
                     sal_Int32   nIndex, nLen, nAryLen;
 
                     rIStm >> aPt >> nIndex >> nLen >> nTmp >> nAryLen;
-                    if ( nTmp && ( static_cast< sal_uInt32 >( nTmp ) < ( SAL_MAX_UINT16 - 1 ) ) )
+                    if (nTmp > 0)
                     {
                         OString aByteStr = read_uInt8s_ToOString(rIStm, nTmp);
                         sal_uInt8 nTerminator = 0;
@@ -859,13 +859,13 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
 
                         OUString aStr(OStringToOUString(aByteStr, eActualCharSet));
 
-                        if( nAryLen > 0L )
+                        if (nAryLen > 0)
                         {
                             sal_Int32 nStrLen( aStr.getLength() );
 
                             pDXAry = new sal_Int32[ std::max( nAryLen, nStrLen ) ];
 
-                            for( long j = 0L; j < nAryLen; j++ )
+                            for (sal_Int32 j = 0; j < nAryLen; ++j)
                                 rIStm >> nTmp, pDXAry[ j ] = nTmp;
 
                             // #106172# Add last DX array elem, if missing
@@ -899,10 +899,9 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
                         }
                         if ( nUnicodeCommentActionNumber == i )
                             ImplReadUnicodeComment( nUnicodeCommentStreamPos, rIStm, aStr );
-                        rMtf.AddAction( new MetaTextArrayAction( aPt, aStr, pDXAry, (sal_uInt16) nIndex, (sal_uInt16) nLen ) );
+                        rMtf.AddAction( new MetaTextArrayAction( aPt, aStr, pDXAry, nIndex, nLen ) );
 
-                        if( pDXAry )
-                            delete[] pDXAry;
+                        delete[] pDXAry;
                     }
                     rIStm.Seek( nActBegin + nActionSize );
                 }
@@ -913,7 +912,7 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
                     sal_Int32       nIndex, nLen, nWidth;
 
                     rIStm >> aPt >> nIndex >> nLen >> nTmp >> nWidth;
-                    if ( nTmp && ( static_cast< sal_uInt32 >( nTmp ) < ( SAL_MAX_INT16 - 1 ) ) )
+                    if (nTmp > 0)
                     {
                         OString aByteStr = read_uInt8s_ToOString(rIStm, nTmp);
                         sal_uInt8 nTerminator = 0;
@@ -923,7 +922,7 @@ void SVMConverter::ImplConvertFromSVM1( SvStream& rIStm, GDIMetaFile& rMtf )
                         OUString aStr(OStringToOUString(aByteStr, eActualCharSet));
                         if ( nUnicodeCommentActionNumber == i )
                             ImplReadUnicodeComment( nUnicodeCommentStreamPos, rIStm, aStr );
-                        rMtf.AddAction( new MetaStretchTextAction( aPt, nWidth, aStr, (sal_uInt16) nIndex, (sal_uInt16) nLen ) );
+                        rMtf.AddAction( new MetaStretchTextAction( aPt, nWidth, aStr, nIndex, nLen ) );
                     }
                     rIStm.Seek( nActBegin + nActionSize );
                 }
@@ -1686,17 +1685,17 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                 OUString        aUniText( pAct->GetText() );
                 OString aText(OUStringToOString(aUniText,
                     rActualCharSet));
-                const sal_uLong nStrLen = aText.getLength();
+                const sal_Int32 nStrLen = aText.getLength();
 
                 if ( ImplWriteUnicodeComment( rOStm, aUniText ) )
                     nCount++;
 
                 rOStm.WriteInt16( (sal_Int16) GDI_TEXT_ACTION );
-                rOStm.WriteInt32( (sal_Int32) ( 24 + ( nStrLen + 1 ) ) );
+                rOStm.WriteInt32( ( 24 + ( nStrLen + 1 ) ) );
                 rOStm << pAct->GetPoint();
-                rOStm.WriteInt32( (sal_Int32) pAct->GetIndex() );
-                rOStm.WriteInt32( (sal_Int32) pAct->GetLen() );
-                rOStm.WriteInt32( (sal_Int32) nStrLen );
+                rOStm.WriteInt32( pAct->GetIndex() );
+                rOStm.WriteInt32( pAct->GetLen() );
+                rOStm.WriteInt32( nStrLen );
                 rOStm.Write( aText.getStr(), nStrLen + 1 );
                 nCount++;
             }
@@ -1708,9 +1707,9 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                 OString aText(OUStringToOString(pAct->GetText(),
                     rActualCharSet));
                 OUString                aUniText = pAct->GetText().copy( pAct->GetIndex(), std::min<sal_Int32>(pAct->GetText().getLength() - pAct->GetIndex(), pAct->GetLen()) );
-                sal_uLong               nAryLen;
-                sal_uLong               nLen = pAct->GetLen();
-                const sal_uLong         nTextLen = aText.getLength();
+                sal_Int32               nAryLen;
+                sal_Int32               nLen = pAct->GetLen();
+                const sal_Int32         nTextLen = aText.getLength();
                 sal_Int32*              pDXArray = pAct->GetDXArray();
 
                 if ( ImplWriteUnicodeComment( rOStm, aUniText ) )
@@ -1721,7 +1720,7 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                     if( pAct->GetIndex() <= nTextLen )
                         nLen = nTextLen - pAct->GetIndex();
                     else
-                        nLen = 0UL;
+                        nLen = 0;
                 }
 
                 if( !pDXArray || !nLen )
@@ -1730,16 +1729,16 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                     nAryLen = nLen; // #105987# Write out all of DX array
 
                 rOStm.WriteInt16( (sal_Int16) GDI_TEXTARRAY_ACTION );
-                rOStm.WriteInt32( (sal_Int32) ( 28 + ( nLen + 1 ) + ( nAryLen * 4 ) ) );
+                rOStm.WriteInt32( ( 28 + ( nLen + 1 ) + ( nAryLen * 4 ) ) );
                 rOStm << pAct->GetPoint();
-                rOStm.WriteInt32( (sal_Int32) 0 );
-                rOStm.WriteInt32( (sal_Int32) nLen );
-                rOStm.WriteInt32( (sal_Int32) nLen );
-                rOStm.WriteInt32( (sal_Int32) nAryLen );
+                rOStm.WriteInt32( 0 );
+                rOStm.WriteInt32( nLen );
+                rOStm.WriteInt32( nLen );
+                rOStm.WriteInt32( nAryLen );
                 rOStm.Write( aText.getStr()+pAct->GetIndex(), nLen + 1 );
 
-                for( sal_uLong n = 0UL ; n < nAryLen; n++ )
-                    rOStm.WriteInt32( (sal_Int32) pDXArray[ n ] );
+                for (sal_Int32 n = 0; n < nAryLen; ++n)
+                    rOStm.WriteInt32( pDXArray[ n ] );
 
                 nCount++;
             }
@@ -1751,17 +1750,17 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                 OUString                aUniText( pAct->GetText() );
                 OString aText(OUStringToOString(aUniText,
                     rActualCharSet));
-                const sal_uLong nStrLen = aText.getLength();
+                const sal_Int32         nStrLen = aText.getLength();
 
                 if ( ImplWriteUnicodeComment( rOStm, aUniText ) )
                     nCount++;
 
                 rOStm.WriteInt16( (sal_Int16) GDI_STRETCHTEXT_ACTION );
-                rOStm.WriteInt32( (sal_Int32) ( 28 + ( nStrLen + 1 ) ) );
+                rOStm.WriteInt32( ( 28 + ( nStrLen + 1 ) ) );
                 rOStm << pAct->GetPoint();
-                rOStm.WriteInt32( (sal_Int32) pAct->GetIndex() );
-                rOStm.WriteInt32( (sal_Int32) pAct->GetLen() );
-                rOStm.WriteInt32( (sal_Int32) nStrLen );
+                rOStm.WriteInt32( pAct->GetIndex() );
+                rOStm.WriteInt32( pAct->GetLen() );
+                rOStm.WriteInt32( nStrLen );
                 rOStm.WriteInt32( (sal_Int32) pAct->GetWidth() );
                 rOStm.Write( aText.getStr(), nStrLen + 1 );
                 nCount++;

commit 9837788fb65dde6f34c10156bee11ba8e3bec59d

vcl: MetaText*Action: revert unwanted file format changes
Horrible things happen while loading bugdoc from rhbz#1069030.

(regression from 28e4dc5254960ea38ffeb427aaeca8ae4bbba8c3)

Change-Id: I7f71a41289f57598861c3c005c1e23cbfb4f8111

vcl/source/gdi/metaact.cxx
@@ -1258,8 +1258,8 @@ void MetaTextAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
     WRITE_BASE_COMPAT( rOStm, 2, pData );
     WritePair( rOStm, maPt );
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
-    rOStm.WriteInt32(mnIndex);
-    rOStm.WriteInt32(mnLen);
+    rOStm.WriteUInt16(mnIndex);
+    rOStm.WriteUInt16(mnLen);
 
     write_uInt16_lenPrefixed_uInt16s_FromOUString(rOStm, maStr); // version 2
 }
@@ -1271,8 +1271,11 @@ void MetaTextAction::Read( SvStream& rIStm, ImplMetaReadData* pData )
     COMPAT( rIStm );
     ReadPair( rIStm, maPt );
     maStr = rIStm.ReadUniOrByteString(pData->meActualCharSet);
-    rIStm  .ReadInt32( mnIndex );
-    rIStm  .ReadInt32( mnLen );
+    sal_uInt16 nTmp(0);
+    rIStm.ReadUInt16(nTmp);
+    mnIndex = nTmp;
+    rIStm.ReadUInt16(nTmp);
+    mnLen = nTmp;
 
     if ( aCompat.GetVersion() >= 2 )                            // Version 2
         maStr = read_uInt16_lenPrefixed_uInt16s_ToOUString(rIStm);
@@ -1395,9 +1398,9 @@ void MetaTextArrayAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
     WRITE_BASE_COMPAT( rOStm, 2, pData );
     WritePair( rOStm, maStartPt );
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
-    rOStm.WriteInt32(mnIndex);
-    rOStm.WriteInt32(mnLen);
-    rOStm.WriteInt32(nAryLen);
+    rOStm.WriteUInt16(mnIndex);
+    rOStm.WriteUInt16(mnLen);
+    rOStm.WriteUInt16(nAryLen);
 
     for (sal_Int32 i = 0; i < nAryLen; ++i)
         rOStm.WriteInt32( mpDXAry[ i ] );
@@ -1416,9 +1419,13 @@ void MetaTextArrayAction::Read( SvStream& rIStm, ImplMetaReadData* pData )
     COMPAT( rIStm );
     ReadPair( rIStm, maStartPt );
     maStr = rIStm.ReadUniOrByteString(pData->meActualCharSet);
-    rIStm  .ReadInt32( mnIndex );
-    rIStm  .ReadInt32( mnLen );
-    rIStm  .ReadInt32( nAryLen );
+    sal_uInt16 nTmp(0);
+    rIStm.ReadUInt16(nTmp);
+    mnIndex = nTmp;
+    rIStm.ReadUInt16(nTmp);
+    mnLen = nTmp;
+    rIStm.ReadUInt16(nTmp);
+    nAryLen = nTmp;
 
     if ( mnIndex + mnLen > maStr.getLength() )
     {
@@ -1533,8 +1540,8 @@ void MetaStretchTextAction::Write( SvStream& rOStm, ImplMetaWriteData* pData )
     WritePair( rOStm, maPt );
     rOStm.WriteUniOrByteString( maStr, pData->meActualCharSet );
     rOStm.WriteUInt32( mnWidth );
-    rOStm.WriteInt32( mnIndex );
-    rOStm.WriteInt32( mnLen );
+    rOStm.WriteUInt16( mnIndex );
+    rOStm.WriteUInt16( mnLen );
 
     write_uInt16_lenPrefixed_uInt16s_FromOUString(rOStm, maStr); // version 2
 }
@@ -1546,9 +1553,12 @@ void MetaStretchTextAction::Read( SvStream& rIStm, ImplMetaReadData* pData )
     COMPAT( rIStm );
     ReadPair( rIStm, maPt );
     maStr = rIStm.ReadUniOrByteString(pData->meActualCharSet);
-    rIStm  .ReadUInt32( mnWidth );
-    rIStm  .ReadInt32( mnIndex );
-    rIStm  .ReadInt32( mnLen );
+    rIStm.ReadUInt32( mnWidth );
+    sal_uInt16 nTmp(0);
+    rIStm.ReadUInt16(nTmp);
+    mnIndex = nTmp;
+    rIStm.ReadUInt16(nTmp);
+    mnLen = nTmp;
 
     if ( aCompat.GetVersion() >= 2 )                            // Version 2
         maStr = read_uInt16_lenPrefixed_uInt16s_ToOUString(rIStm);

vcl/source/gdi/outdev3.cxx
@@ -5680,6 +5680,9 @@ ImplLayoutArgs OutputDevice::ImplPrepareLayoutArgs( OUString& rStr,
                                        const sal_Int32 nMinIndex, const sal_Int32 nLen,
                                        long nPixelWidth, const sal_Int32* pDXArray ) const
 {
+    assert(nMinIndex >= 0);
+    assert(nLen >= 0);
+
     // get string length for calculating extents
     sal_Int32 nEndIndex = rStr.getLength();
     if( nMinIndex + nLen < nEndIndex )