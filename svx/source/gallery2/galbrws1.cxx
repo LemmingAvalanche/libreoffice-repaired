LemmingAvalanche/libreoffice-repaired

commit 528aee64f4417bc75f5e94f30d774f6ab0206bac

Window::PreNotify should return bool
Change-Id: Ic9903fd887f2c3fab2630ebeb20df39392177c8d

svx/source/gallery2/galbrws1.cxx
@@ -87,9 +87,9 @@ void GalleryThemeListBox::DataChanged( const DataChangedEvent& rDCEvt )
         ListBox::DataChanged( rDCEvt );
 }
 
-long GalleryThemeListBox::PreNotify( NotifyEvent& rNEvt )
+bool GalleryThemeListBox::PreNotify( NotifyEvent& rNEvt )
 {
-    long nDone = 0;
+    bool nDone = true;
 
     if( rNEvt.GetType() == EVENT_COMMAND )
     {
@@ -106,7 +106,7 @@ long GalleryThemeListBox::PreNotify( NotifyEvent& rNEvt )
             nDone = static_cast< GalleryBrowser1* >( GetParent() )->KeyInput( *pKEvt, this );
     }
 
-    return( nDone ? nDone : ListBox::PreNotify( rNEvt ) );
+    return( nDone || ListBox::PreNotify( rNEvt ) );
 }
 
 // - GalleryBrowser1 -

commit 43400ed7eb93c9e580712e502228d531a16ee78d

fix inability to interact with gallery theme list box
i.e. can't scroll it or select any entries

regression from 528aee64f4417bc75f5e94f30d774f6ab0206bac

Change-Id: I9c108cde6185332bc32284b0cafb63e36b048220

svx/source/gallery2/galbrws1.cxx
@@ -90,7 +90,7 @@ void GalleryThemeListBox::DataChanged( const DataChangedEvent& rDCEvt )
 
 bool GalleryThemeListBox::PreNotify( NotifyEvent& rNEvt )
 {
-    bool nDone = true;
+    bool bDone = false;
 
     if( rNEvt.GetType() == EVENT_COMMAND )
     {
@@ -104,10 +104,10 @@ bool GalleryThemeListBox::PreNotify( NotifyEvent& rNEvt )
         const KeyEvent* pKEvt = rNEvt.GetKeyEvent();
 
         if( pKEvt )
-            nDone = static_cast< GalleryBrowser1* >( GetParent() )->KeyInput( *pKEvt, this );
+            bDone = static_cast< GalleryBrowser1* >( GetParent() )->KeyInput( *pKEvt, this );
     }
 
-    return( nDone || ListBox::PreNotify( rNEvt ) );
+    return( bDone || ListBox::PreNotify( rNEvt ) );
 }
 
 // - GalleryBrowser1 -