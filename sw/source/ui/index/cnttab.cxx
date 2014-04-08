LemmingAvalanche/libreoffice-repaired

commit 137c659f7de1991a3aefa187279b51b3dca361ea

idxexample.odt and html.stw cleanup (fdo#49101, fdo#49098)
idxexample.odt will be localized through the standard l10n
framework, i.e. with use of .po files, because now strings
are read from resources.

html.stw is language independent now, defaults for different
scripts are handled in the code.

Change-Id: Ie20cafc6191721a959b33e2559b9866e3762a146

sw/source/ui/index/cnttab.cxx
@@ -510,6 +510,7 @@ IMPL_LINK( SwMultiTOXTabDialog, ShowPreviewHdl, CheckBox *, pBox )
             sTemplate.AppendAscii( RTL_CONSTASCII_STRINGPARAM(".odt") );
 
             SvtPathOptions aOpt;
+            aOpt.SetTemplatePath(String("share/template/common"));
             // 6.0 (extension .sxw)
             sal_Bool bExist = aOpt.SearchFile( sTemplate, SvtPathOptions::PATH_TEMPLATE );
 

commit 0fc67608f6256b0e3dcda943b906b73dcfe05fec

SwMultiTOXTabDialog: fortunately the bogus SetTemplatePath call
... fails with exception and does not modify the configuration.

(regression from 137c659f7de1991a3aefa187279b51b3dca361ea)

Change-Id: I0e12cf0b531bc68147cf2be22c28c75c0f05f3ff

sw/source/ui/index/cnttab.cxx
@@ -476,7 +476,6 @@ IMPL_LINK_NOARG( SwMultiTOXTabDialog, ShowPreviewHdl )
             OUString sTemplate("internal/idxexample.odt");
 
             SvtPathOptions aOpt;
-            aOpt.SetTemplatePath(OUString(LIBO_SHARE_FOLDER "/template/common"));
             bool bExist = aOpt.SearchFile( sTemplate, SvtPathOptions::PATH_TEMPLATE );
 
             if(!bExist)

unotools/source/config/pathoptions.cxx
@@ -287,8 +287,9 @@ void SvtPathOptions_Impl::SetPath( SvtPathOptions::Paths ePath, const OUString& 
         {
             m_xPathSettings->setFastPropertyValue( m_aMapEnumToPropHandle[ (sal_Int32)ePath], a );
         }
-        catch (const Exception&)
+        catch (const Exception& e)
         {
+            SAL_WARN("unotools.config", "SetPath: exception: " << e.Message);
         }
     }
 }