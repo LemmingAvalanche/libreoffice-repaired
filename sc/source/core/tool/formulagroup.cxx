LemmingAvalanche/libreoffice-repaired

commit 8c64224b4fba5a2d056f220205476b7d2a14250d

loplugin:unreffun -- Wrap opencl-only functions in appropriate ifdef.
This section is only needed if we have opencl enabled, hence we should
wrap it in HAVE_FEATURE_OPENCL.

Change-Id: I5ec479d7c1739fcb8baf0936104425b515b6a843

sc/source/core/tool/formulagroup.cxx
@@ -511,6 +511,7 @@ public:
 
 #endif
 
+#if HAVE_FEATURE_OPENCL
 #ifndef DISABLE_DYNLOADING
 
 class FormulaGroupInterpreterOpenCLMissing : public FormulaGroupInterpreter
@@ -552,6 +553,7 @@ osl::Module* getOpenCLModule()
     return bLoaded ? &aModule : NULL;
 }
 
+#endif
 #endif
 
 /// load and/or configure the correct formula group interpreter

commit 9b4e44ca989d1cf6de6ae0179c366ddafbf61954

fix "Wrap opencl-only functions in appropriate ifdef."
8c64224b4fba5a2d056f220205476b7d2a14250d
(loplugin:unreffun fix) caused:
  'undefined reference to `sc::FormulaGroupInterpreter::msInstance'
when building with --disable-opencl.

(It appears I didn't check whether my build had fully completed before pushing.)

Change-Id: I948205867985c7722662d673e95d5639bc9d9bcc

sc/source/core/tool/formulagroup.cxx
@@ -533,9 +533,11 @@ typedef bool (*__switchOpenClDevice)(const OUString*, bool, bool);
 typedef void (*__getOpenCLDeviceInfo)(size_t*, size_t*);
 
 #endif
+#endif
 
 FormulaGroupInterpreter *FormulaGroupInterpreter::msInstance = NULL;
 
+#if HAVE_FEATURE_OPENCL
 #ifndef DISABLE_DYNLOADING
 
 osl::Module* getOpenCLModule()