LemmingAvalanche/libreoffice-repaired

commit 68347b8a661fd4b47941454df2d5778ebfb8332a

Resolves: #i122041# Unified and centralized control for Color ValueSets
(cherry picked from commit 68e707bbc6fe23881b822e6efab8a2933343dc1a)

Conflicts:
	cui/source/inc/backgrnd.hxx
	cui/source/inc/cuitabarea.hxx
	cui/source/tabpages/backgrnd.cxx
	cui/source/tabpages/tpcolor.cxx
	sd/source/ui/view/drviews6.cxx
	svx/Package_inc.mk
	svx/inc/svx/colrctrl.hxx
	svx/source/sidebar/tools/ColorControl.cxx
	svx/source/tbxctrls/colorwindow.hxx
	svx/source/tbxctrls/colrctrl.cxx
	svx/source/tbxctrls/tbcontrl.cxx

Change-Id: Ie06fe355846b737ec8aae9aade4d408232c83193

Related: #i122041# Add a11y values for defining behaviour of ColorValueSets

(cherry picked from commit 401e8f2db607081e62eaaa1f08a8cd8971a3f637)

Conflicts:
	cui/source/tabpages/backgrnd.cxx
	officecfg/registry/schema/org/openoffice/Office/Common.xcs
	sc/source/ui/miscdlgs/tabbgcolordlg.cxx
	svtools/inc/svtools/accessibilityoptions.hxx
	svtools/source/inc/configitems/accessibilityoptions_const.hxx
	vcl/inc/vcl/bitmap.hxx
	vcl/source/gdi/bitmap4.cxx

Change-Id: If9d0db5f22974cad1ac886eca3719ca92ca7f240

svx/source/tbxctrls/tbcontrl.cxx
@@ -1065,10 +1065,6 @@ void SvxFontNameBox_Impl::Select()
 #define WB_NO_DIRECTSELECT      ((WinBits)0x04000000)
 #endif
 
-#define PALETTE_X 8
-#define PALETTE_Y 13
-#define PALETTE_SIZE (PALETTE_X * PALETTE_Y)
-
 SvxColorWindow_Impl::SvxColorWindow_Impl( const OUString&            rCommand,
                                           sal_uInt16                     nSlotId,
                                           const Reference< XFrame >& rFrame,
@@ -1087,7 +1083,6 @@ SvxColorWindow_Impl::SvxColorWindow_Impl( const OUString&            rCommand,
     SfxObjectShell* pDocSh = SfxObjectShell::Current();
     const SfxPoolItem* pItem = NULL;
     XColorListRef pColorList;
-    const Size aSize12( 13, 13 );
 
     if ( pDocSh )
         if ( 0 != ( pItem = pDocSh->GetItem( SID_COLOR_TABLE ) ) )
@@ -1125,41 +1120,19 @@ SvxColorWindow_Impl::SvxColorWindow_Impl( const OUString&            rCommand,
 
     if ( pColorList.is() )
     {
-        short i = 0;
-        long nCount = pColorList->Count();
-        XColorEntry* pEntry = NULL;
-        ::Color aColWhite( COL_WHITE );
-        String aStrWhite( EditResId(RID_SVXITEMS_COLOR_WHITE) );
+        const long nColorCount(pColorList->Count());
+        const Size aNewSize(aColorSet.layoutAllVisible(nColorCount));
+        aColorSet.SetOutputSizePixel(aNewSize);
+        static sal_Int32 nAdd = 4;
 
-        if ( nCount > PALETTE_SIZE )
-            // Show scrollbar if more than PALLETTE_SIZE colors are available
-            aColorSet.SetStyle( aColorSet.GetStyle() | WB_VSCROLL );
-
-        for ( i = 0; i < nCount; i++ )
-        {
-            pEntry = pColorList->GetColor(i);
-            aColorSet.InsertItem( i+1, pEntry->GetColor(), pEntry->GetName() );
-            if( pEntry->GetColor() == mLastColor )
-                aColorSet.SelectItem( i+1 );
-        }
-
-        while ( i < PALETTE_SIZE )
-        {
-            // fill empty elements if less then PALLETTE_SIZE colors are available
-            aColorSet.InsertItem( i+1, aColWhite, aStrWhite );
-            i++;
-        }
+        SetOutputSizePixel(Size(aNewSize.Width() + nAdd, aNewSize.Height() + nAdd));
+        aColorSet.Clear();
+        aColorSet.addEntriesForXColorList(*pColorList);
     }
 
     aColorSet.SetSelectHdl( LINK( this, SvxColorWindow_Impl, SelectHdl ) );
-    aColorSet.SetColCount( PALETTE_X );
-    aColorSet.SetLineCount( PALETTE_Y );
-
-    lcl_CalcSizeValueSet( *this, aColorSet, aSize12 );
-
     SetHelpId( HID_POPUP_COLOR );
     aColorSet.SetHelpId( HID_POPUP_COLOR_CTRL );
-
     SetText( rWndTitle );
     aColorSet.Show();
 
@@ -1264,34 +1237,14 @@ void SvxColorWindow_Impl::StateChanged( sal_uInt16 nSID, SfxItemState eState, co
             if ( pState )
             {
                 XColorListRef pColorList = ((SvxColorListItem *)pState)->GetColorList();
+                const long nColorCount(pColorList->Count());
+                const Size aNewSize(aColorSet.layoutAllVisible(nColorCount));
+                aColorSet.SetOutputSizePixel(aNewSize);
+                static sal_Int32 nAdd = 4;
 
-                short i = 0;
-                long nCount = pColorList->Count();
-                XColorEntry* pEntry = NULL;
-                ::Color aColWhite( COL_WHITE );
-                String aStrWhite( EditResId(RID_SVXITEMS_COLOR_WHITE) );
-
-                // ScrollBar on or off
-                WinBits nBits = aColorSet.GetStyle();
-                if ( nCount > PALETTE_SIZE )
-                    nBits &= ~WB_VSCROLL;
-                else
-                    nBits |= WB_VSCROLL;
-                aColorSet.SetStyle( nBits );
-
-                for ( i = 0; i < nCount; ++i )
-                {
-                    pEntry = pColorList->GetColor(i);
-                    aColorSet.SetItemColor( i + 1, pEntry->GetColor() );
-                    aColorSet.SetItemText ( i + 1, pEntry->GetName() );
-                }
-
-                while ( i < PALETTE_SIZE )
-                {
-                    aColorSet.SetItemColor( i + 1, aColWhite );
-                    aColorSet.SetItemText ( i + 1, aStrWhite );
-                    i++;
-                }
+                SetOutputSizePixel(Size(aNewSize.Width() + nAdd, aNewSize.Height() + nAdd));
+                aColorSet.Clear();
+                aColorSet.addEntriesForXColorList(*pColorList);
             }
         }
     }

cui/source/inc/backgrnd.hxx
@@ -22,7 +22,7 @@
 #include <vcl/group.hxx>
 #include <svtools/stdctrl.hxx>
 #include <vcl/graph.hxx>
-#include <svtools/valueset.hxx>
+#include <svx/SvxColorValueSet.hxx>
 #include <svx/dlgctrl.hxx>
 #include <editeng/brushitem.hxx>
 
@@ -82,7 +82,7 @@ private:
     ListBox*                m_pParaLBox;
 
     VclFrame*               m_pBackGroundColorFrame;
-    ValueSet*               m_pBackgroundColorSet;
+    SvxColorValueSet*       m_pBackgroundColorSet;
     BackgroundPreviewImpl*  m_pPreviewWin1;
 
     FixedText*              m_pColTransFT;///<color transparency

cui/source/inc/cuitabarea.hxx
@@ -27,6 +27,7 @@
 #include <svx/xfillit0.hxx>
 #include <svx/xfillit.hxx>
 #include <svx/tabarea.hxx>
+#include <svx/SvxColorValueSet.hxx>
 
 class SdrModel;
 class SdrView;
@@ -650,7 +651,7 @@ private:
     Edit*               m_pEdtName;
     ColorLB*            m_pLbColor;
 
-    ValueSet*           m_pValSetColorList;
+    SvxColorValueSet*   m_pValSetColorList;
 
     SvxXRectPreview*    m_pCtlPreviewOld;
     SvxXRectPreview*    m_pCtlPreviewNew;
@@ -698,7 +699,8 @@ private:
     sal_uInt16  ColorToPercent_Impl( sal_uInt16 nColor );
     sal_uInt16  PercentToColor_Impl( sal_uInt16 nPercent );
 
-    void    FillValueSet_Impl( ValueSet& rVs );
+    void ImpColorCountChanged();
+
     //-----------------------------------------------------------------------------------------------------
     DECL_LINK( ClickAddHdl_Impl, void * );
     DECL_LINK( ClickModifyHdl_Impl, void * );

cui/source/tabpages/backgrnd.cxx
@@ -413,6 +413,8 @@ SvxBackgroundTabPage::SvxBackgroundTabPage(Window* pParent, const SfxItemSet& rC
     FillColorValueSets_Impl();
 
     m_pBackgroundColorSet->SetSelectHdl( HDL(BackgroundColorHdl_Impl) );
+    m_pBackgroundColorSet->SetStyle(m_pBackgroundColorSet->GetStyle() | WB_ITEMBORDER | WB_NAMEFIELD | WB_NONEFIELD);
+    m_pBackgroundColorSet->SetText(SVX_RESSTR(RID_SVXSTR_TRANSPARENT));
 }
 
 //------------------------------------------------------------------------
@@ -1169,47 +1171,23 @@ void SvxBackgroundTabPage::FillColorValueSets_Impl()
     SfxObjectShell* pDocSh = SfxObjectShell::Current();
     const SfxPoolItem* pItem = NULL;
     XColorListRef pColorTable = NULL;
-    const Size aSize15x15 = Size( 15, 15 );
-
     if ( pDocSh && ( 0 != ( pItem = pDocSh->GetItem( SID_COLOR_TABLE ) ) ) )
+    {
         pColorTable = ( (SvxColorListItem*)pItem )->GetColorList();
+    }
 
     if ( !pColorTable.is() )
         pColorTable = XColorList::CreateStdColorList();
 
     if ( pColorTable.is() )
     {
-        short i = 0;
-        long nCount = pColorTable->Count();
-        XColorEntry* pEntry = NULL;
-        Color aColWhite( COL_WHITE );
-        String aStrWhite( EditResId( RID_SVXITEMS_COLOR_WHITE ) );
-        WinBits nBits = ( m_pBackgroundColorSet->GetStyle() | WB_ITEMBORDER | WB_NAMEFIELD | WB_NONEFIELD );
-        m_pBackgroundColorSet->SetText( SVX_RESSTR( RID_SVXSTR_TRANSPARENT ) );
-        m_pBackgroundColorSet->SetStyle( nBits );
-        m_pBackgroundColorSet->SetAccessibleName(m_pBackGroundColorFrame->get_label_widget()->GetText());
-        for ( i = 0; i < nCount; i++ )
-        {
-            pEntry = pColorTable->GetColor(i);
-            m_pBackgroundColorSet->InsertItem( i + 1, pEntry->GetColor(), pEntry->GetName() );
-        }
-
-        while ( i < 104 )
-        {
-            m_pBackgroundColorSet->InsertItem( i + 1, aColWhite, aStrWhite );
-            i++;
-        }
-
-        if ( nCount > 104 )
-        {
-            m_pBackgroundColorSet->SetStyle( nBits | WB_VSCROLL );
-        }
+        m_pBackgroundColorSet->Clear();
+        m_pBackgroundColorSet->addEntriesForXColorList(*pColorTable);
     }
 
-    m_pBackgroundColorSet->SetColCount( 8 );
-    m_pBackgroundColorSet->SetLineCount( 13 );
-    m_pBackgroundColorSet->CalcWindowSizePixel( aSize15x15 );
-
+    const WinBits nBits(m_pBackgroundColorSet->GetStyle() | WB_ITEMBORDER | WB_NAMEFIELD | WB_NONEFIELD);
+    m_pBackgroundColorSet->SetStyle(nBits);
+    m_pBackgroundColorSet->SetColCount(m_pBackgroundColorSet->getColumnCount());
 }
 
 //------------------------------------------------------------------------

cui/source/tabpages/tpcolor.cxx
@@ -391,10 +391,7 @@ SvxColorTabPage::SvxColorTabPage(Window* pParent, const SfxItemSet& rInAttrs)
         LINK( this, SvxColorTabPage, ClickDeleteHdl_Impl ) );
 
     // ValueSet
-    m_pValSetColorList->SetStyle( m_pValSetColorList->GetStyle() | WB_VSCROLL | WB_ITEMBORDER );
-    m_pValSetColorList->SetColCount( 8 );
-    m_pValSetColorList->SetLineCount( 13 );
-    m_pValSetColorList->SetExtraSpacing( 0 );
+    m_pValSetColorList->SetStyle( m_pValSetColorList->GetStyle() | WB_ITEMBORDER );
     m_pValSetColorList->Show();
 }
 
@@ -405,11 +402,24 @@ SvxColorTabPage::~SvxColorTabPage()
 
 // -----------------------------------------------------------------------
 
+void SvxColorTabPage::ImpColorCountChanged()
+{
+    if (!pColorList.is())
+        return;
+    m_pValSetColorList->SetColCount(m_pValSetColorList->getColumnCount());
+}
+
+// -----------------------------------------------------------------------
+
 void SvxColorTabPage::Construct()
 {
-    m_pLbColor->Fill(pColorList);
-    FillValueSet_Impl(*m_pValSetColorList);
-    UpdateTableName();
+    if (pColorList.is())
+    {
+        m_pLbColor->Fill(pColorList);
+        m_pValSetColorList->addEntriesForXColorList(*pColorList);
+        ImpColorCountChanged();
+        UpdateTableName();
+    }
 }
 
 // -----------------------------------------------------------------------
@@ -722,6 +732,7 @@ IMPL_LINK_NOARG(SvxColorTabPage, ClickAddHdl_Impl)
         m_pLbColor->Append( pEntry );
         m_pValSetColorList->InsertItem( m_pValSetColorList->GetItemCount() + 1,
                 pEntry->GetColor(), pEntry->GetName() );
+        ImpColorCountChanged();
 
         m_pLbColor->SelectEntryPos( m_pLbColor->GetEntryCount() - 1 );
 
@@ -874,7 +885,9 @@ IMPL_LINK_NOARG(SvxColorTabPage, ClickDeleteHdl_Impl)
             // update Listbox and ValueSet
             m_pLbColor->RemoveEntry( nPos );
             m_pValSetColorList->Clear();
-            FillValueSet_Impl(*m_pValSetColorList);
+            m_pValSetColorList->addEntriesForXColorList(*pColorList);
+            ImpColorCountChanged();
+            //FillValueSet_Impl(*m_pValSetColorList);
 
             // positioning
             m_pLbColor->SelectEntryPos( nPos );
@@ -1043,20 +1056,20 @@ long SvxColorTabPage::ChangeColorHdl_Impl( void* )
 
 //------------------------------------------------------------------------
 
-void SvxColorTabPage::FillValueSet_Impl( ValueSet& rVs )
-{
-    long nCount = pColorList->Count();
-    XColorEntry* pColorEntry;
-
-    if( nCount > 104 )
-        rVs.SetStyle( rVs.GetStyle() | WB_VSCROLL );
-
-    for( long i = 0; i < nCount; i++ )
-    {
-        pColorEntry = pColorList->GetColor( i );
-        rVs.InsertItem( (sal_uInt16) i + 1, pColorEntry->GetColor(), pColorEntry->GetName() );
-    }
-}
+//void SvxColorTabPage::FillValueSet_Impl( ValueSet& rVs )
+//{
+//    long nCount = pColorList->Count();
+//    XColorEntry* pColorEntry;
+//
+//    if( nCount > 104 )
+//        rVs.SetStyle( rVs.GetStyle() | WB_VSCROLL );
+//
+//    for( long i = 0; i < nCount; i++ )
+//    {
+//        pColorEntry = pColorList->GetColor( i );
+//        rVs.InsertItem( (sal_uInt16) i + 1, pColorEntry->GetColor(), pColorEntry->GetName() );
+//    }
+//}
 
 //------------------------------------------------------------------------
 

cui/uiconfig/ui/backgroundpage.ui
@@ -217,7 +217,7 @@
                   </packing>
                 </child>
                 <child>
-                  <object class="svtlo-ValueSet" id="backgroundcolorset">
+                  <object class="svxlo-SvxColorValueSet" id="backgroundcolorset">
                     <property name="width_request">150</property>
                     <property name="height_request">150</property>
                     <property name="visible">True</property>

cui/uiconfig/ui/colorpage.ui
@@ -129,7 +129,7 @@
                           </packing>
                         </child>
                         <child>
-                          <object class="svtlo-ValueSet" id="colorset">
+                          <object class="svxlo-SvxColorValueSet" id="colorset">
                             <property name="visible">True</property>
                             <property name="can_focus">False</property>
                             <property name="hexpand">True</property>

include/svtools/accessibilityoptions.hxx
@@ -49,6 +49,15 @@ public:
     sal_Bool    IsSelectionInReadonly() const;
     sal_Bool    GetAutoDetectSystemHC() const;
 
+    // options for configuring the look and feel for the ColorValueSet (color selector)
+    sal_Int16   GetColorValueSetMaximumRowCount() const;
+    sal_Int16   GetColorValueSetEntryEdgeLength() const;
+    sal_Int16   GetColorValueSetColumnCount() const;
+
+    // option to activate EdgeBlending for previews in the UI (ColorValueSet, ColorDropDown,
+    // FillStyle/LineStyle previews, etc...). 0 == off, 100% == full paint, in-between == alpha
+    sal_Int16   GetEdgeBlending() const;
+
     void        SetIsForPagePreviews(sal_Bool bSet);
     void        SetIsHelpTipsDisappear(sal_Bool bSet);
     void        SetIsAllowAnimatedGraphics(sal_Bool bSet);
@@ -58,6 +67,11 @@ public:
     void        SetHelpTipSeconds(sal_Int16 nSet);
     void        SetSelectionInReadonly(sal_Bool bSet);
     void        SetAutoDetectSystemHC(sal_Bool bSet);
+
+    void        SetColorValueSetMaximumRowCount(sal_Int16 nSet);
+    void        SetColorValueSetEntryEdgeLength(sal_Int16 nSet);
+    void        SetColorValueSetColumnCount(sal_Int16 nSet);
+    void        SetEdgeBlending(sal_Int16 nSet);
 
     sal_Bool                IsModified() const;
     void                    Commit();

commit dca27d8cd8a29738b91b5e99d7f3ce3614d62807

fdo#73891 Fix "UI color panel does not show the actual color in Writer"
1.The Font/Background/Highlight Color panels identify the actual color
 by a small frame.
 (Regression from the commit 68347b8a661fd4b47941454df2d5778ebfb8332a
  See also fdo#32376 - Set default color to the current one in toolbar
  popups)

2.Fix this in the Floating Font/Background/Highlight Color Toolbar,too.

Change-Id: I5c2db8ab8923ab146cbf921dcb90894765f8420c
Reviewed-on: https://gerrit.libreoffice.org/9103
Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>
Tested-by: Kohei Yoshida <libreoffice@kohei.us>

svx/source/tbxctrls/tbcontrl.cxx
@@ -1138,6 +1138,17 @@ SvxColorWindow_Impl::SvxColorWindow_Impl( const OUString&            rCommand,
         SetOutputSizePixel(Size(aNewSize.Width() + nAdd, aNewSize.Height() + nAdd));
         aColorSet.Clear();
         aColorSet.addEntriesForXColorList(*pColorList);
+
+        short i = 0;
+        long nCount = pColorList->Count();
+        XColorEntry* pEntry = NULL;
+
+        for ( i = 0; i < nCount; i++ )
+        {
+            pEntry = pColorList->GetColor(i);
+            if( pEntry->GetColor() == mLastColor )
+                aColorSet.SelectItem( i+1 );
+        }
     }
 
     aColorSet.SetSelectHdl( LINK( this, SvxColorWindow_Impl, SelectHdl ) );
@@ -1210,6 +1221,7 @@ IMPL_LINK_NOARG(SvxColorWindow_Impl, SelectHdl)
         SfxToolBoxControl::Dispatch( Reference< XDispatchProvider >( GetFrame()->getController(), UNO_QUERY ),
                                      maCommand,
                                      aArgs );
+        aColorSet.SelectItem( nItemId );
     }
 
     return 0;