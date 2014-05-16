LemmingAvalanche/libreoffice-repaired

commit 26194ad2849e04a30149c78e8f19a09455b6e57d

Fixed a crash: GDIMetafile was deleted twice

svtools/source/graphic/grfcache.cxx
@@ -371,7 +371,7 @@ void GraphicCacheEntry::GraphicObjectWasSwappedOut( const GraphicObject& /*rObj*
     if( mbSwappedAll )
     {
         delete mpBmpEx, mpBmpEx = NULL;
-        delete mpMtf, mpMtf = NULL;
+        mpMtf = NULL; // No need to delete it as it has already been dereferenced
         delete mpAnimation, mpAnimation = NULL;
     }
 }

commit 3170d0f6b2cf22b5783ea9b3d0a856e50ad8aa12

fix memory leak on pasting metafiles into writer
copying from mspaint to writer and then deleting the image in a loop will
eventually exhaust all memory.

regression since 26194ad2849e04a30149c78e8f19a09455b6e57d

Change-Id: I381285cda3823de7df0c1725a339943caf9536fe

svtools/source/graphic/grfcache.cxx
@@ -367,7 +367,7 @@ void GraphicCacheEntry::GraphicObjectWasSwappedOut( const GraphicObject& /*rObj*
     if( mbSwappedAll )
     {
         delete mpBmpEx, mpBmpEx = NULL;
-        mpMtf = NULL; // No need to delete it as it has already been dereferenced
+        delete mpMtf, mpMtf = NULL;
         delete mpAnimation, mpAnimation = NULL;
 
         // #119176# also reset SvgData