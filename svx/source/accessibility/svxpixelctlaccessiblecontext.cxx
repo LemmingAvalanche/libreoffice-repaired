LemmingAvalanche/libreoffice-repaired

commit ea39486c425fb99daf075ab1bce3193e3249b3cb

Integrate branch of IAccessible2
Change-Id: I8327fb4ba2a86d4caa52b875221175b80464842a

svx/source/dialog/dlgctrl.cxx
@@ -31,6 +31,10 @@
 #include <vcl/region.hxx>
 #include <vcl/gradient.hxx>
 #include <vcl/hatch.hxx>
+#include <com/sun/star/accessibility/AccessibleEventObject.hpp>
+#include <com/sun/star/accessibility/AccessibleEventId.hpp>
+#include <com/sun/star/accessibility/AccessibleStateType.hpp>
+#include "svxpixelctlaccessiblecontext.hxx"
 #include <svtools/colorcfg.hxx>
 #include <svxrectctaccessiblecontext.hxx>
 #include <com/sun/star/lang/XUnoTunnel.hpp>
@@ -582,6 +586,13 @@ Point SvxRectCtl::SetActualRPWithoutInvalidate( RECT_POINT eNewRP )
 void SvxRectCtl::GetFocus()
 {
     SetFocusRect();
+    // Send the accessible focused event
+    Control::GetFocus();
+    // Send accessibility event.
+    if(pAccContext)
+    {
+        pAccContext->FireChildFocus(GetActualRP());
+    }
 }
 
 
@@ -667,7 +678,7 @@ void SvxRectCtl::SetActualRP( RECT_POINT eNewRP )
 
     // notify accessibility object about change
     if( pAccContext )
-        pAccContext->selectChild( eNewRP );
+        pAccContext->selectChild( eNewRP /* MT, bFireFocus */ );
 }
 
 void SvxRectCtl::SetState( CTL_STATE nState )
@@ -761,10 +772,74 @@ void SvxRectCtl::SetCS(CTL_STYLE eNew)
 
 // Control for editing bitmaps
 
+::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > SvxPixelCtl::CreateAccessible()
+{
+    if(!m_xAccess.is())
+    {
+        m_xAccess = m_pAccess =  new SvxPixelCtlAccessible(*this);
+    }
+    return m_xAccess;
+}
+
+//Logic Pixel
+long SvxPixelCtl::PointToIndex(const Point &aPt) const
+{
+    sal_uInt16  nX, nY;
+
+    nX = (sal_uInt16) ( aPt.X() * nLines / aRectSize.Width() );
+    nY = (sal_uInt16) ( aPt.Y() * nLines / aRectSize.Height() );
+
+    return nX + nY * nLines ;
+}
+
+Point SvxPixelCtl::IndexToPoint(long nIndex) const
+{
+    DBG_ASSERT(nIndex >= 0 && nIndex < nSquares ," Check Index");
+
+    sal_uInt16 nXIndex = nIndex % nLines ;
+    sal_uInt16 nYIndex = sal_uInt16(nIndex / nLines) ;
+
+    Point aPtTl;
+    aPtTl.Y() = aRectSize.Height() * nYIndex / nLines + 1;
+    aPtTl.X() = aRectSize.Width() * nXIndex / nLines + 1;
+
+    return aPtTl;
+}
+
+long SvxPixelCtl::GetFoucsPosIndex() const
+{
+    return aFocusPosition.getX() + aFocusPosition.getY() * nLines ;
+}
+
+long SvxPixelCtl::ShowPosition( const Point &pt)
+{
+    Point aPt = PixelToLogic( pt );
+
+    sal_uInt16  nX, nY;
+    nX = (sal_uInt16) ( aPt.X() * nLines / aRectSize.Width() );
+    nY = (sal_uInt16) ( aPt.Y() * nLines / aRectSize.Height() );
+
+    ChangePixel( nX + nY * nLines );
+
+    //Solution:Set new focus position and repaint
+    //Invalidate( Rectangle( aPtTl, aPtBr ) );
+    aFocusPosition.setX(nX);
+    aFocusPosition.setY(nY);
+    Invalidate(Rectangle(Point(0,0),aRectSize));
+
+    Window *pTabPage = getNonLayoutParent(this);
+    if (pTabPage && WINDOW_TABPAGE == pTabPage->GetType())
+        ( (SvxTabPage*) GetParent() )->PointChanged( this, RP_MM ); // RectPoint ist dummy
+
+    return GetFoucsPosIndex();
+
+}
+
 SvxPixelCtl::SvxPixelCtl(Window* pParent, sal_uInt16 nNumber)
     : Control(pParent, WB_BORDER)
     , nLines(nNumber)
     , bPaintable(true)
+    , aFocusPosition(0,0)
 {
     SetPixelColor( Color( COL_BLACK ) );
     SetBackgroundColor( Color( COL_WHITE ) );
@@ -773,6 +848,7 @@ SvxPixelCtl::SvxPixelCtl(Window* pParent, sal_uInt16 nNumber)
     nSquares = nLines * nLines;
     pPixel = new sal_uInt16[ nSquares ];
     memset(pPixel, 0, nSquares * sizeof(sal_uInt16));
+    m_pAccess=NULL;
 }
 
 void SvxPixelCtl::Resize()
@@ -814,25 +890,18 @@ void SvxPixelCtl::MouseButtonDown( const MouseEvent& rMEvt )
     if (!aRectSize.Width() || !aRectSize.Height())
         return;
 
-    Point aPt = PixelToLogic( rMEvt.GetPosPixel() );
-    Point aPtTl, aPtBr;
-    sal_uInt16  nX, nY;
+    //Grab focus when click in window
+    if (!HasFocus())
+    {
+        GrabFocus();
+    }
 
-    nX = (sal_uInt16) ( aPt.X() * nLines / aRectSize.Width() );
-    nY = (sal_uInt16) ( aPt.Y() * nLines / aRectSize.Height() );
+    long nIndex = ShowPosition(rMEvt.GetPosPixel());
 
-    ChangePixel( nX + nY * nLines );
-
-    aPtTl.X() = aRectSize.Width() * nX / nLines + 1;
-    aPtBr.X() = aRectSize.Width() * (nX + 1) / nLines - 1;
-    aPtTl.Y() = aRectSize.Height() * nY / nLines + 1;
-    aPtBr.Y() = aRectSize.Height() * (nY + 1) / nLines - 1;
-
-    Invalidate( Rectangle( aPtTl, aPtBr ) );
-
-    Window *pTabPage = getNonLayoutParent(this);
-    if (pTabPage && WINDOW_TABPAGE == pTabPage->GetType())
-        ((SvxTabPage*)pTabPage)->PointChanged(this, RP_MM); // RectPoint is a dummy
+    if(m_pAccess)
+    {
+        m_pAccess->NotifyChild(nIndex,sal_True, sal_True);
+    }
 }
 
 // Draws the Control (Rectangle with nine circles)
@@ -882,6 +951,11 @@ void SvxPixelCtl::Paint( const Rectangle& )
                 DrawRect( Rectangle( aPtTl, aPtBr ) );
             }
         }
+        //Draw visual focus when has focus
+        if( HasFocus() )
+        {
+            ShowFocus(implCalFocusRect(aFocusPosition));
+        }
     } // bPaintable
     else
     {
@@ -890,6 +964,129 @@ void SvxPixelCtl::Paint( const Rectangle& )
         DrawLine( Point( 0, 0 ), Point( aRectSize.Width(), aRectSize.Height() ) );
         DrawLine( Point( 0, aRectSize.Height() ), Point( aRectSize.Width(), 0 ) );
     }
+}
+
+//Calculate visual focus rectangle via focus position
+Rectangle SvxPixelCtl::implCalFocusRect( const Point& aPosition )
+{
+    long nLeft,nTop,nRight,nBottom;
+    long i,j;
+    i = aPosition.Y();
+    j = aPosition.X();
+    nLeft = aRectSize.Width() * j / nLines + 1;
+    nRight = aRectSize.Width() * (j + 1) / nLines - 1;
+    nTop = aRectSize.Height() * i / nLines + 1;
+    nBottom = aRectSize.Height() * (i + 1) / nLines - 1;
+    return Rectangle(nLeft,nTop,nRight,nBottom);
+}
+
+//Solution:Keyboard fucntion
+void SvxPixelCtl::KeyInput( const KeyEvent& rKEvt )
+{
+    KeyCode aKeyCode = rKEvt.GetKeyCode();
+    sal_uInt16 nCode = aKeyCode.GetCode();
+    sal_Bool bIsMod = aKeyCode.IsShift() || aKeyCode.IsMod1() || aKeyCode.IsMod2();
+
+    if( !bIsMod )
+    {
+        Point pRepaintPoint( aRectSize.Width() *( aFocusPosition.getX() - 1)/ nLines - 1,
+                             aRectSize.Height() *( aFocusPosition.getY() - 1)/ nLines -1
+                            );
+        Size  mRepaintSize( aRectSize.Width() *3/ nLines + 2,aRectSize.Height() *3/ nLines + 2);
+        Rectangle mRepaintRect( pRepaintPoint, mRepaintSize );
+        sal_Bool bFocusPosChanged=sal_False;
+        switch(nCode)
+        {
+            case KEY_LEFT:
+                if((aFocusPosition.getX() >= 1))
+                {
+                    aFocusPosition.setX( aFocusPosition.getX() - 1 );
+                    Invalidate(mRepaintRect);
+                    bFocusPosChanged=sal_True;
+                }
+                break;
+            case KEY_RIGHT:
+                if( aFocusPosition.getX() < (nLines - 1) )
+                {
+                    aFocusPosition.setX( aFocusPosition.getX() + 1 );
+                    Invalidate(mRepaintRect);
+                    bFocusPosChanged=sal_True;
+                }
+                break;
+            case KEY_UP:
+                if((aFocusPosition.getY() >= 1))
+                {
+                    aFocusPosition.setY( aFocusPosition.getY() - 1 );
+                    Invalidate(mRepaintRect);
+                    bFocusPosChanged=sal_True;
+                }
+                break;
+            case KEY_DOWN:
+                if( aFocusPosition.getY() < ( nLines - 1 ) )
+                {
+                    aFocusPosition.setY( aFocusPosition.getY() + 1 );
+                    Invalidate(mRepaintRect);
+                    bFocusPosChanged=sal_True;
+                }
+                break;
+            case KEY_SPACE:
+                ChangePixel( sal_uInt16(aFocusPosition.getX() + aFocusPosition.getY() * nLines) );
+                Invalidate( implCalFocusRect(aFocusPosition) );
+                break;
+            default:
+                Control::KeyInput( rKEvt );
+                return;
+        }
+        if(m_xAccess.is())
+        {
+            long nIndex = GetFoucsPosIndex();
+            switch(nCode)
+            {
+            case KEY_LEFT:
+            case KEY_RIGHT:
+            case KEY_UP:
+            case KEY_DOWN:
+                if (bFocusPosChanged)
+                {
+                    m_pAccess->NotifyChild(nIndex,sal_False,sal_False);
+                }
+                break;
+            case KEY_SPACE:
+                m_pAccess->NotifyChild(nIndex,sal_False,sal_True);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    else
+    {
+        Control::KeyInput( rKEvt );
+    }
+}
+
+//Draw focus when get focus
+void SvxPixelCtl::GetFocus()
+{
+    Invalidate(implCalFocusRect(aFocusPosition));
+
+    if(m_pAccess)
+    {
+        m_pAccess->NotifyChild(GetFoucsPosIndex(),sal_True,sal_False);
+    }
+
+    Control::GetFocus();
+}
+
+//Hide focus when lose focus
+void SvxPixelCtl::LoseFocus()
+{
+    HideFocus();
+    if (m_pAccess)
+    {
+        m_pAccess->LoseFocus();
+    }
+    Control::LoseFocus();
 }
 
 void SvxPixelCtl::SetXBitmap( const BitmapEx& rBitmapEx )

include/svx/AccessibleControlShape.hxx
@@ -59,6 +59,8 @@ public:
         const AccessibleShapeTreeInfo& rShapeTreeInfo);
     virtual ~AccessibleControlShape( );
 
+    ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > SAL_CALL  GetControlModel( ) { return m_xControlModel;} ;
+    AccessibleControlShape* SAL_CALL GetLabeledByControlShape();
 protected:
     //---  XAccessible  ----------------------------------------
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleContext> SAL_CALL getAccessibleContext( ) throw(::com::sun::star::uno::RuntimeException);

include/svx/AccessibleGraphicShape.hxx
@@ -85,7 +85,8 @@ public:
     virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type> SAL_CALL
         getTypes (void)
         throw (::com::sun::star::uno::RuntimeException);
-
+/// Return this object's role.
+    virtual sal_Int16 SAL_CALL getAccessibleRole (void) throw (::com::sun::star::uno::RuntimeException);
 protected:
     /// Create a name string that contains the accessible name.
     virtual OUString

include/svx/AccessibleOLEShape.hxx
@@ -91,7 +91,8 @@ public:
     virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type> SAL_CALL
         getTypes (void)
         throw (::com::sun::star::uno::RuntimeException);
-
+// ====== XAccessibleExtendedAttributes =====================================
+    virtual ::com::sun::star::uno::Any SAL_CALL getExtendedAttributes() throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException) ;
 protected:
     /// Create a name string that contains the accessible name.
     virtual OUString

include/svx/AccessibleShape.hxx
@@ -28,6 +28,12 @@
 #include <com/sun/star/accessibility/XAccessible.hpp>
 #include <com/sun/star/accessibility/XAccessibleExtendedComponent.hpp>
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
+#include <com/sun/star/accessibility/XAccessibleSelection.hpp>
+#include <com/sun/star/accessibility/XAccessibleText.hpp>
+#include <com/sun/star/accessibility/XAccessibleExtendedAttributes.hpp>
+#include <com/sun/star/accessibility/XAccessibleGroupPosition.hpp>
+#include <com/sun/star/accessibility/XAccessibleHyperlink.hpp>
+#include <com/sun/star/accessibility/XAccessibleHypertext.hpp>
 #include <com/sun/star/drawing/XShape.hpp>
 #include <com/sun/star/lang/XEventListener.hpp>
 #include <com/sun/star/lang/XUnoTunnel.hpp>
@@ -70,6 +76,10 @@ class IAccessibleParent;
 class SVX_DLLPUBLIC AccessibleShape
     :   public AccessibleContextBase,
         public AccessibleComponentBase,
+        public ::com::sun::star::accessibility::XAccessibleSelection,
+        public ::com::sun::star::accessibility::XAccessibleExtendedAttributes,
+        public ::com::sun::star::accessibility::XAccessibleGroupPosition,
+        public com::sun::star::accessibility::XAccessibleHypertext,
         public IAccessibleViewForwarderListener,
         public ::com::sun::star::document::XEventListener,
         public ::com::sun::star::lang::XUnoTunnel
@@ -95,7 +105,54 @@ public:
     AccessibleShape (
         const AccessibleShapeInfo& rShapeInfo,
         const AccessibleShapeTreeInfo& rShapeTreeInfo);
+    AccessibleShape (
+        const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShape>& rxShape, const ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible>& rxParent, const AccessibleShapeTreeInfo& rShapeTreeInfo, sal_Int32 nIndex = -1);
+     //Solution: Overwrite the object's current name.
+    virtual OUString SAL_CALL    getAccessibleName (void) throw (::com::sun::star::uno::RuntimeException);
+    virtual OUString SAL_CALL    getAccessibleDescription() throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleRelationSet> SAL_CALL getAccessibleRelationSet (void) throw (::com::sun::star::uno::RuntimeException);
+//=====  XAccessibleSelection  ============================================
 
+    virtual void SAL_CALL selectAccessibleChild(
+        sal_Int32 nChildIndex )
+        throw ( ::com::sun::star::lang::IndexOutOfBoundsException,
+        ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Bool SAL_CALL isAccessibleChildSelected(
+        sal_Int32 nChildIndex )
+        throw ( ::com::sun::star::lang::IndexOutOfBoundsException,
+        ::com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL clearAccessibleSelection(  )
+        throw ( ::com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL selectAllAccessibleChildren(  )
+        throw ( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Int32 SAL_CALL getSelectedAccessibleChildCount(  )
+        throw ( ::com::sun::star::uno::RuntimeException );
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > SAL_CALL getSelectedAccessibleChild(
+        sal_Int32 nSelectedChildIndex )
+        throw ( ::com::sun::star::lang::IndexOutOfBoundsException,
+        ::com::sun::star::uno::RuntimeException);
+
+    virtual void SAL_CALL deselectAccessibleChild(
+        sal_Int32 nSelectedChildIndex )
+        throw ( ::com::sun::star::lang::IndexOutOfBoundsException,
+        ::com::sun::star::uno::RuntimeException );
+
+    // ====== XAccessibleExtendedAttributes =====================================
+    virtual ::com::sun::star::uno::Any SAL_CALL getExtendedAttributes()
+    throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException) ;
+    /// Return this object's role.
+    virtual sal_Int16 SAL_CALL getAccessibleRole (void) throw (::com::sun::star::uno::RuntimeException);
+    //=====  XAccessibleGroupPosition  =========================================
+    virtual ::com::sun::star::uno::Sequence< sal_Int32 > SAL_CALL
+        getGroupPosition( const ::com::sun::star::uno::Any& rAny )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual OUString SAL_CALL getObjectLink( const ::com::sun::star::uno::Any& accoject )
+        throw (::com::sun::star::uno::RuntimeException);
     /** The destructor releases its children manager and text engine if
         still existent.  These are responsible to send appropriate events.
     */
@@ -301,6 +358,32 @@ public:
     static AccessibleShape*                                     getImplementation( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& _rxIFace ) throw();
     sal_Int64                                                   SAL_CALL getSomething( const ::com::sun::star::uno::Sequence< sal_Int8 >& _rIdentifier ) throw(::com::sun::star::uno::RuntimeException);
 
+    //===== XAccessibleHypertext ========================================================
+    virtual sal_Int32 SAL_CALL getHyperLinkCount()  throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessibleHyperlink >
+        SAL_CALL getHyperLink( sal_Int32 nLinkIndex )
+        throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+    virtual sal_Int32 SAL_CALL getHyperLinkIndex( sal_Int32 nCharIndex )
+        throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+    //=====  XAccesibleText  ==================================================
+        virtual sal_Int32 SAL_CALL getCaretPosition(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Bool SAL_CALL setCaretPosition( sal_Int32 nIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Unicode SAL_CALL getCharacter( sal_Int32 nIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);//Shen Zhen Jie changed sal_Unicode to sal_uInt32; change back to sal_Unicode
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > SAL_CALL getCharacterAttributes( sal_Int32 nIndex, const ::com::sun::star::uno::Sequence< OUString >& aRequestedAttributes ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Rectangle SAL_CALL getCharacterBounds( sal_Int32 nIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getCharacterCount(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getIndexAtPoint( const ::com::sun::star::awt::Point& aPoint ) throw (::com::sun::star::uno::RuntimeException);
+        virtual OUString SAL_CALL getSelectedText(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getSelectionStart(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getSelectionEnd(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Bool SAL_CALL setSelection( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual OUString SAL_CALL getText(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual OUString SAL_CALL getTextRange( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::accessibility::TextSegment SAL_CALL getTextAtIndex( sal_Int32 nIndex, sal_Int16 aTextType ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::accessibility::TextSegment SAL_CALL getTextBeforeIndex( sal_Int32 nIndex, sal_Int16 aTextType ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::accessibility::TextSegment SAL_CALL getTextBehindIndex( sal_Int32 nIndex, sal_Int16 aTextType ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Bool SAL_CALL copyText( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+
     //===== Misc ========================================================
 
     ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShape >
@@ -367,7 +450,12 @@ protected:
     virtual OUString
         CreateAccessibleDescription (void)
         throw (::com::sun::star::uno::RuntimeException);
-
+    virtual OUString
+       GetFullAccessibleName(AccessibleShape *shape)
+       throw (::com::sun::star::uno::RuntimeException);
+    virtual OUString GetStyle();
+    void UpdateDocumentAllSelState(::com::sun::star::uno::Reference<
+        ::com::sun::star::accessibility::XAccessibleStateSet > &xStateSet);
     /** Update the <const>OPAQUE</const> and <const>SELECTED</const> state.
     */
     virtual void UpdateStates (void);
@@ -381,6 +469,8 @@ private:
     SVX_DLLPRIVATE explicit AccessibleShape (const AccessibleShape&);
     /// Don't use the assignment operator.  Do we need this?
     SVX_DLLPRIVATE AccessibleShape& operator= (const AccessibleShape&);
+    //Old accessible name
+    OUString aAccName;
 
     /** Call this method when the title, name, or description of the mxShape
         member (may) have been changed.

include/svx/AccessibleSvxFindReplaceDialog.hxx
@@ -1,0 +1,55 @@
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef _ACCESSIBLESVXFINDREPLACEDIALOG_HXX_
+#define _ACCESSIBLESVXFINDREPLACEDIALOG_HXX_
+
+#include <toolkit/awt/vclxwindow.hxx>
+#include <toolkit/awt/vclxaccessiblecomponent.hxx>
+#include <toolkit/awt/vclxwindows.hxx>
+
+
+class VCLXAccessibleSvxFindReplaceDialog : public VCLXAccessibleComponent
+{
+public:
+    VCLXAccessibleSvxFindReplaceDialog(VCLXWindow* pVCLXindow);
+    virtual ~VCLXAccessibleSvxFindReplaceDialog();
+    virtual void FillAccessibleRelationSet( utl::AccessibleRelationSetHelper& rRelationSet );
+    // XServiceInfo
+    virtual ::rtl::OUString SAL_CALL getImplementationName() throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames() throw (::com::sun::star::uno::RuntimeException);
+
+};
+
+
+class VCLXSvxFindReplaceDialog : public VCLXDialog
+{
+public:
+    VCLXSvxFindReplaceDialog(Window* pSplDlg)
+    {
+        SetWindow(pSplDlg);
+    }
+    virtual ~VCLXSvxFindReplaceDialog()
+    {};
+private:
+    virtual ::com::sun::star::uno::Reference<
+        ::com::sun::star::accessibility::XAccessibleContext >  CreateAccessibleContext()
+    {
+        return new VCLXAccessibleSvxFindReplaceDialog(this);
+    }
+};
+#endif // _ACCESSIBLESVXFINDREPLACEDIALOG_HXX_

commit 4d6d23d86da9059247b9432ed1c856dee6120e8e

Resolves: fdo#78841 crash because parent isn't SvxTabPage
regression from ea39486c425fb99daf075ab1bce3193e3249b3cb

Change-Id: I244dd9fba3dc8230fcac8710a1289151b25dcf6c

svx/source/accessibility/svxpixelctlaccessiblecontext.cxx
@@ -101,7 +101,9 @@ uno::Reference< XAccessible > SvxPixelCtlAccessible::getAccessibleParent(  )
     throw (uno::RuntimeException, std::exception)
 {
     ::osl::MutexGuard   aGuard( m_aMutex );
-    return mrPixelCtl.GetParent()->GetAccessible( true );
+    Window *pTabPage = getNonLayoutParent(&mrPixelCtl);
+    assert(pTabPage && WINDOW_TABPAGE == pTabPage->GetType());
+    return pTabPage->GetAccessible( true );
 }
 
 sal_Int32 SvxPixelCtlAccessible::getAccessibleIndexInParent(  )
@@ -109,7 +111,8 @@ sal_Int32 SvxPixelCtlAccessible::getAccessibleIndexInParent(  )
 {
     ::osl::MutexGuard   aGuard( m_aMutex );
     sal_uInt16 nIdx = 0;
-    Window* pTabPage = mrPixelCtl.GetParent();
+    Window *pTabPage = getNonLayoutParent(&mrPixelCtl);
+    assert(pTabPage && WINDOW_TABPAGE == pTabPage->GetType());
     sal_uInt16 nChildren = pTabPage->GetChildCount();
     for(nIdx = 0; nIdx < nChildren; nIdx++)
         if(pTabPage->GetChild( nIdx ) == &mrPixelCtl)

svx/source/dialog/dlgctrl.cxx
@@ -264,21 +264,12 @@ void SvxRectCtl::MouseButtonDown( const MouseEvent& rMEvt )
 
             SetActualRP( eRP );
 
-            Window* pParent = GetParent();
-            while (pParent)
-            {
-                if( WINDOW_TABPAGE == pParent->GetType() )
-                {
-                    ( (SvxTabPage*) pParent )->PointChanged( this, eRP );
-                    break;
-                }
-                pParent = pParent->GetParent();
-            }
+            Window *pTabPage = getNonLayoutParent(this);
+            if (pTabPage && WINDOW_TABPAGE == pTabPage->GetType())
+                ( (SvxTabPage*)pTabPage)->PointChanged( this, eRP );
         }
     }
 }
-
-
 
 void SvxRectCtl::KeyInput( const KeyEvent& rKeyEvt )
 {
@@ -812,7 +803,7 @@ long SvxPixelCtl::ShowPosition( const Point &pt)
 
     Window *pTabPage = getNonLayoutParent(this);
     if (pTabPage && WINDOW_TABPAGE == pTabPage->GetType())
-        ( (SvxTabPage*) GetParent() )->PointChanged( this, RP_MM ); // RectPoint ist dummy
+        ( (SvxTabPage*)pTabPage)->PointChanged( this, RP_MM ); // RectPoint ist dummy
 
     return GetFoucsPosIndex();
 