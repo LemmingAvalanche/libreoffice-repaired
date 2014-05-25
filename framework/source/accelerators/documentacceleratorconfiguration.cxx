LemmingAvalanche/libreoffice-repaired

commit 7b86a0907d1f715f3dc6df1c29bd62a97054d781

Introduce static inline cppu::acquire(), and make use of that.
This is much better approach compared to the callback function, as it allows
passing arguments to the c++ constructor directly, while still allowing some
additional initialization after having acquired the instance.

Change-Id: I5a0f981915dd58f1522ee6054e53a3550b29d624

framework/source/accelerators/documentacceleratorconfiguration.cxx
@@ -68,10 +68,8 @@ public:
                 reference to an uno service manager, which is used internaly.
      */
     DocumentAcceleratorConfiguration(
-            const css::uno::Reference< css::uno::XComponentContext >& xContext);
-
-    void SAL_CALL constructorInit(const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& aArguments)
-        throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+            const css::uno::Reference< css::uno::XComponentContext >& xContext,
+            const css::uno::Sequence< css::uno::Any >& lArguments);
 
     virtual ~DocumentAcceleratorConfiguration();
 
@@ -114,12 +112,9 @@ private:
 
 //-----------------------------------------------
 DocumentAcceleratorConfiguration::DocumentAcceleratorConfiguration(
-        const css::uno::Reference< css::uno::XComponentContext >& xContext)
+        const css::uno::Reference< css::uno::XComponentContext >& xContext,
+        const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& lArguments)
     : DocumentAcceleratorConfiguration_BASE(xContext)
-{
-}
-
-void DocumentAcceleratorConfiguration::constructorInit(const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& lArguments) throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException)
 {
     WriteGuard aWriteLock(m_aLock);
 
@@ -229,12 +224,9 @@ void DocumentAcceleratorConfiguration::impl_ts_clearCache()
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
 com_sun_star_comp_framework_DocumentAcceleratorConfiguration_get_implementation(
     css::uno::XComponentContext *context,
-    cppu::constructor_InitializationFunc &init_func)
+    css::uno::Sequence<css::uno::Any> const &arguments)
 {
-    // 2nd phase initialization needed
-    init_func = static_cast<cppu::constructor_InitializationFunc>(&DocumentAcceleratorConfiguration::constructorInit);
-
-    return static_cast<cppu::OWeakObject *>(new DocumentAcceleratorConfiguration(context));
+    return cppu::acquire(new DocumentAcceleratorConfiguration(context, arguments));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit d0345fa5656cb40bb10062f9bb1b060f22b7ffa3

Resolves: fdo#79178 Crash when opening a form created with a previous version
A regression from 7b86a0907d1f715f3dc6df1c29bd62a97054d781 I think. Rework
this to follow the same ctor + init pattern as the others

move the fillCache out of the ctor to acquire the obj first, then
call the extra init before returning it

Change-Id: Ia0dc878654780294a4935f07ac70c4358ca51dfc

framework/source/accelerators/documentacceleratorconfiguration.cxx
@@ -98,14 +98,14 @@ public:
     virtual sal_Bool SAL_CALL hasStorage()
         throw(css::uno::RuntimeException, std::exception) SAL_OVERRIDE;
 
-private:
-
     /** read all data into the cache. */
-    void impl_ts_fillCache();
+    void fillCache();
+
+private:
 
     /** forget all currently cached data AND(!)
         forget all currently used storages. */
-    void impl_ts_clearCache();
+    void clearCache();
 };
 
 DocumentAcceleratorConfiguration::DocumentAcceleratorConfiguration(
@@ -128,8 +128,6 @@ DocumentAcceleratorConfiguration::DocumentAcceleratorConfiguration(
                 css::uno::Reference< css::embed::XStorage >());
         }
     }
-
-    impl_ts_fillCache();
 }
 
 DocumentAcceleratorConfiguration::~DocumentAcceleratorConfiguration()
@@ -150,10 +148,10 @@ void SAL_CALL DocumentAcceleratorConfiguration::setStorage(const css::uno::Refer
     }
 
     if (bForgetOldStorages)
-        impl_ts_clearCache();
+        clearCache();
 
     if (xStorage.is())
-        impl_ts_fillCache();
+        fillCache();
 }
 
 sal_Bool SAL_CALL DocumentAcceleratorConfiguration::hasStorage()
@@ -163,7 +161,7 @@ sal_Bool SAL_CALL DocumentAcceleratorConfiguration::hasStorage()
     return m_xDocumentRoot.is();
 }
 
-void DocumentAcceleratorConfiguration::impl_ts_fillCache()
+void DocumentAcceleratorConfiguration::fillCache()
 {
     css::uno::Reference< css::embed::XStorage > xDocumentRoot;
     {
@@ -203,7 +201,7 @@ void DocumentAcceleratorConfiguration::impl_ts_fillCache()
     {}
 }
 
-void DocumentAcceleratorConfiguration::impl_ts_clearCache()
+void DocumentAcceleratorConfiguration::clearCache()
 {
     m_aPresetHandler.forgetCachedStorages();
 }
@@ -215,7 +213,12 @@ com_sun_star_comp_framework_DocumentAcceleratorConfiguration_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &arguments)
 {
-    return cppu::acquire(new DocumentAcceleratorConfiguration(context, arguments));
+    DocumentAcceleratorConfiguration *inst = new DocumentAcceleratorConfiguration(context, arguments);
+    css::uno::XInterface *acquired_inst = cppu::acquire(inst);
+
+    inst->fillCache();
+
+    return acquired_inst;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */