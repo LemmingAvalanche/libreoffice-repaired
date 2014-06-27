LemmingAvalanche/libreoffice-repaired

commit 39031a5d54a25b6730ca15addb2664d72ba3f062

Resolves: #i119963#, #i120877# import and export actual TOC content
Patch by: zhengfan, Oliver
Review by: Oliver

reintegrate from branch writer001

(cherry picked from commit 8f2a21eac4a904db3cc4c448179e2d2cf5878ef4)

Conflicts:
	sw/inc/tox.hxx
	sw/source/core/tox/tox.cxx
	sw/source/filter/ww8/dump/ww8scan.cxx
	sw/source/filter/ww8/wrtw8nds.cxx
	sw/source/filter/ww8/wrtww8.cxx
	sw/source/filter/ww8/ww8atr.cxx
	sw/source/filter/ww8/ww8par.cxx
	sw/source/filter/ww8/ww8par.hxx
	sw/source/filter/ww8/ww8par5.cxx
	sw/source/filter/ww8/ww8par6.cxx

Change-Id: I75b2971f23754afa0bcb0b549bfb820dd5924b3b

sw/source/filter/ww8/attributeoutputbase.hxx
@@ -214,7 +214,9 @@ public:
 
     void StartTOX( const SwSection& rSect );
 
-    void EndTOX( const SwSection& rSect );
+    void EndTOX( const SwSection& rSect,bool bCareEnd=true );
+
+    virtual void OnTOXEnding() {}
 
     virtual void TOXMark( const SwTxtNode& rNode, const SwTOXMark& rAttr );
 

sw/source/filter/ww8/wrtw8nds.cxx
@@ -1974,7 +1974,7 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
                     if ( pTOXSect )
                     {
                         m_aCurrentCharPropStarts.pop();
-                        AttrOutput().EndTOX( *pTOXSect );
+                        AttrOutput().EndTOX( *pTOXSect ,false);
                     }
                     //For i120928,the position of the bullet's graphic is at end of doc
                     if (bLastCR && (!bExported))
@@ -2019,6 +2019,7 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
                 aAttrIter.OutFlys( nEnd );
                 // insert final bookmarks if any before CR and after flys
                 AppendBookmarks( rNode, nEnd, 1 );
+                WriteCR( pTextNodeInfoInner );
                 // #i120928 - position of the bullet's graphic is at end of doc
                 if (bLastCR && (!bExported))
                 {
@@ -2031,8 +2032,6 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
                     m_aCurrentCharPropStarts.pop();
                     AttrOutput().EndTOX( *pTOXSect );
                 }
-
-                WriteCR( pTextNodeInfoInner );
 
                 if ( bRedlineAtEnd )
                 {
@@ -2447,7 +2446,8 @@ void MSWordExportBase::OutputSectionNode( const SwSectionNode& rSectionNode )
 
     SwNodeIndex aIdx( rSectionNode, 1 );
     const SwNode& rNd = aIdx.GetNode();
-    if ( !rNd.IsSectionNode() && !IsInTable() ) //No sections in table
+    if ( !rNd.IsSectionNode() && !IsInTable()
+        && rSection.GetType() != TOX_CONTENT_SECTION && rSection.GetType() != TOX_HEADER_SECTION) //No sections in table
     {
         // if the first Node inside the section has an own
         // PageDesc or PageBreak attribut, then dont write

sw/source/filter/ww8/ww8attributeoutput.hxx
@@ -51,6 +51,8 @@ public:
     ///
     virtual void StartRun( const SwRedlineData* pRedlineData, bool bSingleEmptyRun = false );
 
+    virtual void OnTOXEnding();
+
     /// End of the text run.
     ///
     /// No-op for binary filters.
@@ -441,8 +443,10 @@ protected:
     /// of the field results if we were forced to split text.
     sal_uInt16 m_nFieldResults;
 
+    bool mbOnTOXEnding;
+
 public:
-    WW8AttributeOutput( WW8Export &rWW8Export ) : AttributeOutputBase(), m_rWW8Export( rWW8Export ) {}
+    WW8AttributeOutput( WW8Export &rWW8Export ) : AttributeOutputBase(), m_rWW8Export( rWW8Export ),mbOnTOXEnding(false) {}
     virtual ~WW8AttributeOutput() {}
 
     /// Return the right export class.

sw/inc/tox.hxx
@@ -463,6 +463,13 @@ class SW_DLLPUBLIC SwTOXBase : public SwClient
     sal_Bool        bFromObjectNames : 1;   // create a table or object index
                                     // from the names rather than the caption
     sal_Bool        bLevelFromChapter : 1; // User index: get the level from the source chapter
+
+protected:
+    // Add a data member, for record the TOC field expression of MS Word binary format
+    // For keeping fedality and may giving a better exporting performance
+    OUString maMSTOCExpression;
+    sal_Bool mbKeepExpression;
+
 public:
     SwTOXBase( const SwTOXType* pTyp, const SwForm& rForm,
                sal_uInt16 nCreaType, const OUString& rTitle );
@@ -481,6 +488,12 @@ public:
 
     OUString            GetTOXName() const {return aName;}
     void                SetTOXName(const OUString& rSet) {aName = rSet;}
+
+    // for record the TOC field expression of MS Word binary format
+    const OUString&     GetMSTOCExpression() const{return maMSTOCExpression;}
+    void                SetMSTOCExpression(const OUString& rExp) {maMSTOCExpression = rExp;}
+    void                EnableKeepExpression() {mbKeepExpression = sal_True;}
+    void                DisableKeepExpression() {mbKeepExpression = sal_False;}
 
     OUString            GetTitle() const;           // Title
     OUString            GetTypeName() const;        // Name

sw/source/core/doc/doctxm.cxx
@@ -752,7 +752,14 @@ void SwTOXBaseSection::Update(const SfxItemSet* pAttr,
         !GetFmt() || 0 == (pSectNd = GetFmt()->GetSectionNode() ) ||
         !pSectNd->GetNodes().IsDocNodes() ||
         IsHiddenFlag() )
+    {
         return;
+    }
+
+    if ( !mbKeepExpression )
+    {
+        maMSTOCExpression = OUString();
+    }
 
     SwDoc* pDoc = (SwDoc*)pSectNd->GetDoc();
 

sw/source/core/tox/tox.cxx
@@ -506,17 +506,19 @@ OUString SwForm::GetFormAuth()        {return OUString("<A>");}
 
 SwTOXBase::SwTOXBase(const SwTOXType* pTyp, const SwForm& rForm,
                      sal_uInt16 nCreaType, const OUString& rTitle )
-    : SwClient((SwModify*)pTyp),
-    aForm(rForm),
-    aTitle(rTitle),
-    eLanguage(::GetAppLanguage()),
-    nCreateType(nCreaType),
-    nOLEOptions(0),
-    eCaptionDisplay(CAPTION_COMPLETE),
-    bProtected( sal_True ),
-    bFromChapter(sal_False),
-    bFromObjectNames(sal_False),
-    bLevelFromChapter(sal_False)
+    : SwClient((SwModify*)pTyp)
+    , aForm(rForm)
+    , aTitle(rTitle)
+    , eLanguage((LanguageType)::GetAppLanguage())
+    , nCreateType(nCreaType)
+    , nOLEOptions(0)
+    , eCaptionDisplay(CAPTION_COMPLETE)
+    , bProtected( sal_True )
+    , bFromChapter(sal_False)
+    , bFromObjectNames(sal_False)
+    , bLevelFromChapter(sal_False)
+    , maMSTOCExpression()
+    , mbKeepExpression(sal_True)
 {
     aData.nOptions = 0;
 }
@@ -524,6 +526,7 @@ SwTOXBase::SwTOXBase(const SwTOXType* pTyp, const SwForm& rForm,
 
 SwTOXBase::SwTOXBase( const SwTOXBase& rSource, SwDoc* pDoc )
     : SwClient( rSource.GetRegisteredInNonConst() )
+    , mbKeepExpression(sal_True)
 {
     CopyTOXBase( pDoc, rSource );
 }
@@ -535,6 +538,7 @@ void SwTOXBase::RegisterToTOXType( SwTOXType& rType )
 
 SwTOXBase& SwTOXBase::CopyTOXBase( SwDoc* pDoc, const SwTOXBase& rSource )
 {
+    maMSTOCExpression = rSource.maMSTOCExpression;
     SwTOXType* pType = (SwTOXType*)rSource.GetTOXType();
     if( pDoc && USHRT_MAX == pDoc->GetTOXTypes().GetPos( pType ))
     {

sw/source/filter/ww8/writerwordglue.cxx
@@ -494,6 +494,10 @@ namespace sw
                 return true;
             if (nB == RES_TXTATR_CHARFMT)
                 return false;
+            if (nA == RES_TXTATR_INETFMT)
+                return true;
+            if (nB == RES_TXTATR_INETFMT)
+               return false;
             return nA < nB;
         }
 

sw/source/filter/ww8/wrtww8.cxx
@@ -26,6 +26,9 @@
 
 #include <algorithm>
 
+#include <map>
+#include <set>
+
 #include <hintids.hxx>
 #include <string.h>
 #include <osl/endian.h>
@@ -177,40 +180,145 @@ public:
     WW8_FC GetStartFc() const       { return nStartFc; }
 };
 
+typedef std::map<OUString,long> BKMKNames;
+typedef BKMKNames::iterator BKMKNmItr;
+typedef std::pair<bool,OUString> BKMK;
+typedef std::pair<long,BKMK> BKMKCP;
+typedef std::multimap<long,BKMKCP*> BKMKCPs;
+typedef BKMKCPs::iterator CPItr;
+
 class WW8_WrtBookmarks
 {
 private:
-    //! Holds information about a single bookmark.
-    struct BookmarkInfo {
-        sal_uLong  startPos; //!< Starting character position.
-        sal_uLong  endPos;   //!< Ending character position.
-        bool       isField;  //!< True if the bookmark is in a field result.
-        OUString    name;    //!< Name of this bookmark.
-        inline BookmarkInfo(sal_uLong start, sal_uLong end, bool isFld, const OUString& bkName) : startPos(start), endPos(end), isField(isFld), name(bkName) {};
-        //! Operator < is defined purely for sorting.
-        inline bool operator<(const BookmarkInfo &other) const { return startPos < other.startPos; }
-    };
-    std::vector<BookmarkInfo> aBookmarks;
-    typedef std::vector<BookmarkInfo>::iterator BkmIter;
-
-    //! Return the position in aBookmarks where the string rNm can be found.
-    BkmIter GetPos( const OUString& rNm );
-
-    //No copying
+    BKMKCPs aSttCps,aEndCps;
+    BKMKNames maSwBkmkNms;
     WW8_WrtBookmarks(const WW8_WrtBookmarks&);
     WW8_WrtBookmarks& operator=(const WW8_WrtBookmarks&);
+
 public:
     WW8_WrtBookmarks();
     ~WW8_WrtBookmarks();
-
     //! Add a new bookmark to the list OR add an end position to an existing bookmark.
     void Append( WW8_CP nStartCp, const OUString& rNm, const ::sw::mark::IMark* pBkmk=NULL );
     //! Write out bookmarks to file.
     void Write( WW8Export& rWrt );
     //! Move existing field marks from one position to another.
-    void MoveFieldMarks(sal_uLong nFrom,sal_uLong nTo);
-
+    void MoveFieldMarks(WW8_CP nFrom, WW8_CP nTo);
 };
+
+WW8_WrtBookmarks::WW8_WrtBookmarks()
+{}
+
+WW8_WrtBookmarks::~WW8_WrtBookmarks()
+{
+    CPItr aEnd = aSttCps.end();
+    for (CPItr aItr = aSttCps.begin();aItr!=aEnd;aItr++)
+    {
+        if (aItr->second)
+        {
+            delete aItr->second;
+            aItr->second = NULL;
+        }
+    }
+}
+
+void WW8_WrtBookmarks::Append( WW8_CP nStartCp, const OUString& rNm, const ::sw::mark::IMark*)
+{
+    std::pair<BKMKNmItr,bool> aResult = maSwBkmkNms.insert(std::pair<OUString,long>(rNm,0L));
+    if (aResult.second)
+    {
+        BKMK aBK(false,rNm);
+        BKMKCP* pBKCP = new BKMKCP((long)nStartCp,aBK);
+        aSttCps.insert(std::pair<long,BKMKCP*>(nStartCp,pBKCP));
+        aResult.first->second = (long)nStartCp;
+    }
+    else
+    {
+        std::pair<CPItr,CPItr> aRange = aSttCps.equal_range(aResult.first->second);
+        for (CPItr aItr = aRange.first;aItr != aRange.second;aItr++)
+        {
+            if (aItr->second && aItr->second->second.second == rNm)
+            {
+                if (aItr->second->second.first)
+                    nStartCp--;
+                aItr->second->first = (long)nStartCp;
+                break;
+            }
+        }
+    }
+}
+
+void WW8_WrtBookmarks::Write( WW8Export& rWrt)
+{
+    if (!aSttCps.size())
+        return;
+    CPItr aItr;
+    long n;
+    std::vector<OUString> aNames;
+    SvMemoryStream aTempStrm1(65535,65535);
+    SvMemoryStream aTempStrm2(65535,65535);
+    for (aItr = aSttCps.begin();aItr!=aSttCps.end();aItr++)
+    {
+        if (aItr->second)
+        {
+            aEndCps.insert(std::pair<long,BKMKCP*>(aItr->second->first,aItr->second));
+            aNames.push_back(aItr->second->second.second);
+            SwWW8Writer::WriteLong( aTempStrm1, aItr->first);
+        }
+    }
+
+    aTempStrm1.Seek(0L);
+    for (aItr = aEndCps.begin(), n = 0;aItr != aEndCps.end();aItr++,n++)
+    {
+        if (aItr->second)
+        {
+            aItr->second->first = n;
+            SwWW8Writer::WriteLong( aTempStrm2, aItr->first);
+        }
+    }
+
+    aTempStrm2.Seek(0L);
+    rWrt.WriteAsStringTable(aNames, rWrt.pFib->fcSttbfbkmk,rWrt.pFib->lcbSttbfbkmk);
+    SvStream& rStrm = rWrt.bWrtWW8 ? *rWrt.pTableStrm : rWrt.Strm();
+    rWrt.pFib->fcPlcfbkf = rStrm.Tell();
+    rStrm<<aTempStrm1;
+    SwWW8Writer::WriteLong(rStrm, rWrt.pFib->ccpText + rWrt.pFib->ccpTxbx);
+    for (aItr = aSttCps.begin();aItr!=aSttCps.end();aItr++)
+    {
+        if (aItr->second)
+        {
+            SwWW8Writer::WriteLong(rStrm, aItr->second->first);
+        }
+    }
+    rWrt.pFib->lcbPlcfbkf = rStrm.Tell() - rWrt.pFib->fcPlcfbkf;
+    rWrt.pFib->fcPlcfbkl = rStrm.Tell();
+    rStrm<<aTempStrm2;
+    SwWW8Writer::WriteLong(rStrm, rWrt.pFib->ccpText + rWrt.pFib->ccpTxbx);
+    rWrt.pFib->lcbPlcfbkl = rStrm.Tell() - rWrt.pFib->fcPlcfbkl;
+}
+
+void WW8_WrtBookmarks::MoveFieldMarks(WW8_CP nFrom, WW8_CP nTo)
+{
+    std::pair<CPItr,CPItr> aRange = aSttCps.equal_range(nFrom);
+    CPItr aItr = aRange.first;
+    while (aItr != aRange.second)
+    {
+        if (aItr->second)
+        {
+            if (aItr->second->first == nFrom)
+            {
+                aItr->second->second.first = true;
+                aItr->second->first = nTo;
+            }
+            aSttCps.insert(std::pair<long,BKMKCP*>(nTo,aItr->second));
+            aItr->second = NULL;
+            aRange = aSttCps.equal_range(nFrom);
+            aItr = aRange.first;
+            continue;
+        }
+        aItr++;
+    }
+}
 
 #define ANZ_DEFAULT_STYLES 16
 
@@ -1237,122 +1345,6 @@ WW8_CP WW8_WrPct::Fc2Cp( sal_uLong nFc ) const
     return nFc + aPcts.back().GetStartCp();
 }
 
-//--------------------------------------------------------------------------
-
-WW8_WrtBookmarks::WW8_WrtBookmarks()
-{
-}
-
-WW8_WrtBookmarks::~WW8_WrtBookmarks()
-{
-}
-
-void WW8_WrtBookmarks::Append( WW8_CP nStartCp, const OUString& rNm,  const ::sw::mark::IMark* )
-{
-    BkmIter bkIter = GetPos( rNm );
-    if( bkIter == aBookmarks.end() )
-    {
-        // new bookmark -> insert with start==end
-        aBookmarks.push_back( BookmarkInfo(nStartCp, nStartCp, false, rNm) );
-    }
-    else
-    {
-        // old bookmark -> this should be the end position
-        OSL_ENSURE( bkIter->endPos == bkIter->startPos, "end position is valid" );
-
-        //If this bookmark was around a field in writer, then we want to move
-        //it to the field result in word. The end is therefore one cp
-        //backwards from the 0x15 end mark that was inserted.
-        if (bkIter->isField)
-            --nStartCp;
-        bkIter->endPos = nStartCp;
-    }
-}
-
-
-void WW8_WrtBookmarks::Write( WW8Export& rWrt )
-{
-    if (!aBookmarks.empty())
-    {
-        //Make sure the bookmarks are sorted in order of start position.
-        std::sort(aBookmarks.begin(), aBookmarks.end());
-
-        // First write the Bookmark Name Stringtable
-        std::vector<OUString> aNames;
-        aNames.reserve(aBookmarks.size());
-        for (BkmIter bIt = aBookmarks.begin(); bIt < aBookmarks.end(); ++bIt)
-            aNames.push_back(bIt->name);
-        rWrt.WriteAsStringTable(aNames, rWrt.pFib->fcSttbfbkmk, rWrt.pFib->lcbSttbfbkmk);
-
-        // Second write the Bookmark start positions as pcf of longs
-        SvStream& rStrm = rWrt.bWrtWW8 ? *rWrt.pTableStrm : rWrt.Strm();
-        rWrt.pFib->fcPlcfbkf = rStrm.Tell();
-        for (BkmIter bIt = aBookmarks.begin(); bIt < aBookmarks.end(); ++bIt)
-            SwWW8Writer::WriteLong( rStrm, bIt->startPos );
-        SwWW8Writer::WriteLong(rStrm, rWrt.pFib->ccpText + rWrt.pFib->ccpTxbx);
-
-        //Lastly, need to write out the end positions (sorted by end position). But
-        //before that we need a lookup table (sorted by start position) to link
-        //start and end positions.
-        //   Start by sorting the end positions.
-        std::vector<sal_uLong> aEndSortTab;
-        aEndSortTab.reserve(aBookmarks.size());
-        for (BkmIter bIt = aBookmarks.begin(); bIt < aBookmarks.end(); ++bIt)
-            aEndSortTab.push_back(bIt->endPos);
-        std::sort(aEndSortTab.begin(), aEndSortTab.end());
-
-        //Now write out the lookups.
-        //Note that in most cases, the positions in both vectors will be very close.
-        for( sal_uLong i = 0; i < aBookmarks.size(); ++i )
-        {
-            sal_uLong nEndCP = aBookmarks[ i ].endPos;
-            sal_uInt16 nPos = i;
-            if( aEndSortTab[ nPos ] > nEndCP )
-            {
-                while( aEndSortTab[ --nPos ] != nEndCP )
-                    ;
-            }
-            else if( aEndSortTab[ nPos ] < nEndCP )
-                while( aEndSortTab[ ++nPos ] != nEndCP )
-                    ;
-            SwWW8Writer::WriteLong( rStrm, nPos );
-        }
-        rWrt.pFib->lcbPlcfbkf = rStrm.Tell() - rWrt.pFib->fcPlcfbkf;
-
-        // Finally, the actual Bookmark end positions.
-        rWrt.pFib->fcPlcfbkl = rStrm.Tell();
-        for(sal_uLong i = 0; i < aEndSortTab.size(); ++i )
-            SwWW8Writer::WriteLong( rStrm, aEndSortTab[ i ] );
-        SwWW8Writer::WriteLong(rStrm, rWrt.pFib->ccpText + rWrt.pFib->ccpTxbx);
-        rWrt.pFib->lcbPlcfbkl = rStrm.Tell() - rWrt.pFib->fcPlcfbkl;
-    }
-}
-
-WW8_WrtBookmarks::BkmIter WW8_WrtBookmarks::GetPos( const OUString& rNm )
-{
-    for (BkmIter bIt = aBookmarks.begin(); bIt < aBookmarks.end(); ++bIt) {
-        if (rNm == bIt->name)
-            return bIt;
-    }
-    return aBookmarks.end();
-}
-
-void WW8_WrtBookmarks::MoveFieldMarks(sal_uLong nFrom, sal_uLong nTo)
-{
-    for (BkmIter i = aBookmarks.begin(); i < aBookmarks.end(); ++i)
-    {
-        if (i->startPos == nFrom)
-        {
-            i->startPos = nTo;
-            if (i->endPos == nFrom)
-            {
-                i->isField = true;
-                i->endPos = nTo;
-            }
-        }
-    }
-}
-
 void WW8Export::AppendBookmarks( const SwTxtNode& rNd,
     xub_StrLen nAktPos, xub_StrLen nLen )
 {
@@ -1397,7 +1389,7 @@ void WW8Export::AppendBookmarks( const SwTxtNode& rNd,
     }
 }
 
-void WW8Export::MoveFieldMarks(sal_uLong nFrom, sal_uLong nTo)
+void WW8Export::MoveFieldMarks(WW8_CP nFrom, WW8_CP nTo)
 {
     pBkmks->MoveFieldMarks(nFrom, nTo);
 }
@@ -2540,7 +2532,8 @@ void MSWordExportBase::WriteText()
                 ;
             else if ( aIdx.GetNode().IsSectionNode() )
                 ;
-            else if ( !IsInTable() ) //No sections in table
+            else if ( !IsInTable()
+                && (rSect.GetType() != TOX_CONTENT_SECTION && rSect.GetType() != TOX_HEADER_SECTION )) //No sections in table
             {
                 //#120140# Do not need to insert a page/section break after a section end. Check this case first
                 sal_Bool bNeedExportBreakHere = sal_True;

commit f4fe44655f81f528536bc3b44053d659b717997a

fdo#78758: sw: RTF export: don't export multiple \fldrst for one hyperlink
Ensure that we export only one \fldresult per hyperlink by doing that in
StartURL() and EndURL(); the TextINetFormat() is called once per text
portion.  This shouldn't cause problems as there can't be anything
between the end of the \field group and the start of \fldresult anyway.

Replace the annoying call to EndURL() from EndRun() with a special case
in EndURL() to store things in the right buffer (hopefully).

(somehow this is regression from 39031a5d54a25b6730ca15addb2664d72ba3f062)

Change-Id: I209ea7a384fb1cb5d1505a70ecc4a4536bbf26a2

sw/source/filter/ww8/attributeoutputbase.hxx
@@ -203,7 +203,7 @@ public:
     virtual bool StartURL( const OUString& rUrl, const OUString& rTarget ) = 0;
 
     /// Output URL end.
-    virtual bool EndURL() = 0;
+    virtual bool EndURL(bool isAtEndOfParagraph) = 0;
 
     virtual void FieldVanish( const OUString& rTxt, ww::eField eType ) = 0;
 

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -2070,7 +2070,7 @@ bool DocxAttributeOutput::StartURL( const OUString& rUrl, const OUString& rTarge
     return true;
 }
 
-bool DocxAttributeOutput::EndURL()
+bool DocxAttributeOutput::EndURL(bool const)
 {
     m_closeHyperlinkInThisRun = true;
     if(m_startedHyperlink && m_hyperLinkAnchor != "" && m_hyperLinkAnchor.startsWith("_Toc"))

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -190,7 +190,7 @@ public:
     virtual bool StartURL( const OUString& rUrl, const OUString& rTarget ) SAL_OVERRIDE;
 
     /// Output URL end.
-    virtual bool EndURL() SAL_OVERRIDE;
+    virtual bool EndURL(bool) SAL_OVERRIDE;
 
     virtual void FieldVanish( const OUString& rTxt, ww::eField eType ) SAL_OVERRIDE;
 

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -408,8 +408,6 @@ void RtfAttributeOutput::StartRun(const SwRedlineData* pRedlineData, bool bSingl
 
 void RtfAttributeOutput::EndRun()
 {
-    if (m_bInURL)
-        EndURL();
     m_aRun->append(SAL_NEWLINE_STRING);
     m_aRun.appendAndClear(m_aRunText);
     if (!m_bSingleEmptyRun && m_bInRun)
@@ -479,23 +477,30 @@ bool RtfAttributeOutput::StartURL(const OUString& rUrl, const OUString& rTarget)
     }
 
     m_aStyles.append("}");
-    m_bHadFieldResult = false;
-    m_bInURL = true;
+    m_aStyles.append("{" OOO_STRING_SVTOOLS_RTF_FLDRSLT " {");
     return true;
 }
 
-bool RtfAttributeOutput::EndURL()
+bool RtfAttributeOutput::EndURL(bool const isAtEndOfParagraph)
 {
-    if (m_bInURL)
-        m_bInURL = false;
-    else
-        return true;
-
-    // close the fldrslt group
-    if (m_bHadFieldResult)
+    // UGLY: usually EndRun is called earlier, but there is an extra
+    // call to OutAttrWithRange() when at the end of the paragraph,
+    // so in that special case the output needs to be appended to the
+    // new run's text instead of the previous run
+    if (isAtEndOfParagraph)
+    {
+        // close the fldrslt group
+        m_aRunText->append("}}");
+        // close the field group
         m_aRunText->append('}');
-    // close the field group
-    m_aRunText->append('}');
+    }
+    else
+    {
+        // close the fldrslt group
+        m_aRun->append("}}");
+        // close the field group
+        m_aRun->append('}');
+    }
     return true;
 }
 
@@ -2404,8 +2409,6 @@ void RtfAttributeOutput::TextINetFormat(const SwFmtINetFmt& rURL)
         const SwCharFmt* pFmt;
         const SwTxtINetFmt* pTxtAtr = rURL.GetTxtINetFmt();
 
-        m_aStyles.append("{" OOO_STRING_SVTOOLS_RTF_FLDRSLT " ");
-        m_bHadFieldResult = true;
         if (pTxtAtr && 0 != (pFmt = pTxtAtr->GetCharFmt()))
         {
             sal_uInt16 nStyle = m_rExport.GetId(*pFmt);
@@ -3245,12 +3248,10 @@ RtfAttributeOutput::RtfAttributeOutput(RtfExport& rExport)
       m_bBufferSectionHeaders(false),
       m_bLastTable(true),
       m_bWroteCellInfo(false),
-      m_bHadFieldResult(false),
       m_bTableRowEnded(false),
       m_aCells(),
       m_bSingleEmptyRun(false),
       m_bInRun(false),
-      m_bInURL(false),
       m_pFlyFrameSize(0),
       m_pPrevPageDesc(0)
 {

sw/source/filter/ww8/rtfattributeoutput.hxx
@@ -94,7 +94,7 @@ public:
     virtual bool StartURL(const OUString& rUrl, const OUString& rTarget) SAL_OVERRIDE;
 
     /// Output URL end.
-    virtual bool EndURL() SAL_OVERRIDE;
+    virtual bool EndURL(bool isAtEndOfParagraph) SAL_OVERRIDE;
 
     virtual void FieldVanish(const OUString& rTxt, ww::eField eType) SAL_OVERRIDE;
 
@@ -573,11 +573,6 @@ private:
      */
     bool m_bWroteCellInfo;
 
-    /*
-     * If we had a field result in the URL.
-     */
-    bool m_bHadFieldResult;
-
     /// If we ended a table row without starting a new one.
     bool m_bTableRowEnded;
 
@@ -588,8 +583,6 @@ private:
     bool m_bSingleEmptyRun;
 
     bool m_bInRun;
-
-    bool m_bInURL;
 
     /// Maps ID's to postit fields, used in atrfstart/end and atnref.
     std::map<sal_uInt16, const SwPostItField*> m_aPostitFields;

sw/source/filter/ww8/wrtw8esh.cxx
@@ -1155,7 +1155,7 @@ void MSWord_SdrAttrIter::OutEEField(const SfxPoolItem& rHt)
         const OUString &rStr = pURL->GetRepresentation();
         m_rExport.AttrOutput().RawText( rStr, true, GetNodeCharSet() ); // FIXME kendy: is the 'true' actually correct here?  It was here before, but... ;-)
 
-        m_rExport.AttrOutput().EndURL();
+        m_rExport.AttrOutput().EndURL(false);
         m_rExport.nTxtTyp = nOldTxtTyp;
     }
 }

sw/source/filter/ww8/wrtw8nds.cxx
@@ -1071,7 +1071,7 @@ bool WW8AttributeOutput::StartURL( const OUString &rUrl, const OUString &rTarget
     return true;
 }
 
-bool WW8AttributeOutput::EndURL()
+bool WW8AttributeOutput::EndURL(bool const)
 {
     // hyperlinks only in WW8
     if ( !m_rWW8Export.bWrtWW8 )
@@ -1213,7 +1213,7 @@ int SwWW8AttrIter::OutAttrWithRange(sal_Int32 nPos)
                     }
                     if ( 0 != ( pEnd = pHt->End() ) && nPos == *pEnd )
                     {
-                        if ( m_rExport.AttrOutput().EndURL() )
+                        if (m_rExport.AttrOutput().EndURL(nPos == rNd.Len()))
                             --nRet;
                     }
                     break;

sw/source/filter/ww8/wrtww8gr.cxx
@@ -511,7 +511,7 @@ void WW8Export::OutGrf(const sw::Frame &rFrame)
     //Added for i120568,the hyperlink info within a graphic whose anchor type is
     //"As character" will be exported to ensure the fidelity
     if( bURLStarted )
-        m_pAttrOutput->EndURL();
+        m_pAttrOutput->EndURL(false);
 }
 
 GraphicDetails& GraphicDetails::operator=(const GraphicDetails &rOther)

sw/source/filter/ww8/ww8attributeoutput.hxx
@@ -80,7 +80,7 @@ public:
     virtual bool StartURL( const OUString &rUrl, const OUString &rTarget ) SAL_OVERRIDE;
 
     /// Output URL end.
-    virtual bool EndURL() SAL_OVERRIDE;
+    virtual bool EndURL(bool) SAL_OVERRIDE;
 
     virtual void FieldVanish( const OUString& rTxt, ww::eField eType ) SAL_OVERRIDE;
 

sw/qa/extras/rtfexport/data/fdo78758.rtf
@@ -1,0 +1,9 @@
+{\rtf1\ansi
+\par \pard\plain
+{{\field{\*\fldinst HYPERLINK "#__RefHeading___Toc264438068" }
+{\fldrslt {\langfe1024\rtlch \ltrch\loch\lang1024
+EE5E EeEEE5EE}{\scaps\langfe1024\rtlch \ltrch\loch\lang1024
+e}{\langfe1024\rtlch \ltrch\loch\lang1024
+\tab 46}}}}
+\par
+}

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -468,6 +468,16 @@ DECLARE_RTFEXPORT_TEST(testHyperlink, "hyperlink.rtf")
     CPPUNIT_ASSERT_EQUAL(OUString(""), getProperty<OUString>(getRun(getParagraph(1), 3, "!"), "HyperLinkURL"));
 }
 
+DECLARE_RTFEXPORT_TEST(test78758, "fdo78758.rtf")
+{
+    CPPUNIT_ASSERT_EQUAL(OUString("#__RefHeading___Toc264438068"),
+        getProperty<OUString>(getRun(getParagraph(2), 1, "EE5E EeEEE5EE"), "HyperLinkURL"));
+    CPPUNIT_ASSERT_EQUAL(OUString("#__RefHeading___Toc264438068"),
+        getProperty<OUString>(getRun(getParagraph(2), 2, "e"), "HyperLinkURL"));
+    CPPUNIT_ASSERT_EQUAL(OUString("#__RefHeading___Toc264438068"),
+        getProperty<OUString>(getRun(getParagraph(2), 3, "\t46"), "HyperLinkURL"));
+}
+
 DECLARE_RTFEXPORT_TEST(testTextFrameBorders, "textframe-borders.rtf")
 {
     uno::Reference<text::XTextFramesSupplier> xTextFramesSupplier(mxComponent, uno::UNO_QUERY);