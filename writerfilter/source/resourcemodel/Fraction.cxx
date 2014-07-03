LemmingAvalanche/libreoffice-repaired

commit 22f6e8dd78aa525e2c1f611266e10b065f4b915b

fdo#41068: writerfilter: fix image wrap polygon import
Mainly the problem seems to be that Stein's GCD algorithm requires
non-negative input parameters, and the document has this:
 <wp:lineTo x="-480" y="6104"/>

(regression from 86898639d4144a078ed295d0a8bef406868802cb)

Change-Id: I8da1272c3caae84f43472aa4acb65ed66dfbd8ae

writerfilter/source/resourcemodel/Fraction.cxx
@@ -22,6 +22,8 @@
 namespace writerfilter {
 namespace resourcemodel {
 
+// Stein's binary GCD for non-negative integers
+// https://en.wikipedia.org/wiki/Binary_GCD_algorithm
 sal_uInt32 gcd(sal_uInt32 a, sal_uInt32 b)
 {
     if (a == 0 || b == 0)
@@ -77,7 +79,7 @@ Fraction::~Fraction()
 
 void Fraction::init(sal_Int32 nNumerator, sal_Int32 nDenominator)
 {
-    sal_uInt32 nGCD = gcd(nNumerator, nDenominator);
+    sal_uInt32 nGCD = gcd(abs(nNumerator), abs(nDenominator));
 
     mnNumerator = nNumerator/ nGCD;
     mnDenominator = nDenominator / nGCD;

writerfilter/source/dmapper/WrapPolygonHandler.hxx
@@ -70,8 +70,8 @@ public:
 private:
     WrapPolygon::Pointer_t mpPolygon;
 
-    sal_uInt32 mnX;
-    sal_uInt32 mnY;
+    sal_Int32 mnX;
+    sal_Int32 mnY;
 
     // Properties
     virtual void lcl_attribute(Id Name, Value & val);

commit 235c2a021665a94e49ea4fbf91683c51e698816d

fdo#76803: writerfilter: fix image wrap polygon import again
The division in Fraction::init() should be a signed one, to prevent a
change in sign, e.g., -220869/9 = +477194047.

(regression from 22f6e8dd78aa525e2c1f611266e10b065f4b915b)

Change-Id: Icbbd1721144ff42c53ae71312641bd601ba60762

writerfilter/source/resourcemodel/Fraction.cxx
@@ -79,9 +79,11 @@ Fraction::~Fraction()
 
 void Fraction::init(sal_Int32 nNumerator, sal_Int32 nDenominator)
 {
-    sal_uInt32 nGCD = gcd(abs(nNumerator), abs(nDenominator));
+    // fdo#41068 pass non-negative numbers to gcd
+    sal_Int32 const nGCD = gcd(abs(nNumerator), abs(nDenominator));
 
-    mnNumerator = nNumerator/ nGCD;
+    // fdo#76803 do signed division
+    mnNumerator = nNumerator / nGCD;
     mnDenominator = nDenominator / nGCD;
 }
 