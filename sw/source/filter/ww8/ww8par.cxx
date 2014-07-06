LemmingAvalanche/libreoffice-repaired

commit ce9318fd2e09028ad9ebe5d8351e149486ce4987

cp#2013101510000026: doc import of comments affecting more text nodes
Change-Id: I3932d82cb4cd640b19957b93cc7e59711af1b564

sw/source/filter/ww8/ww8par.cxx
@@ -2162,11 +2162,50 @@ long SwWW8ImplReader::Read_And(WW8PLCFManResult* pRes)
                 WW8_CP nStart = GetAnnotationStart(nAtnIndex);
                 WW8_CP nEnd = GetAnnotationEnd(nAtnIndex);
                 sal_Int32 nLen = nEnd - nStart;
-                // Don't support ranges affecting multiple SwTxtNode for now.
-                if (nLen && pPaM->GetPoint()->nContent.GetIndex() >= nLen)
-                {
-                    pPaM->SetMark();
-                    pPaM->GetPoint()->nContent -= nLen;
+                if( nLen )
+                 {
+                    if (pPaM->GetPoint()->nContent.GetIndex() >= nLen)
+                    {
+                        pPaM->SetMark();
+                        pPaM->GetPoint()->nContent -= nLen;
+                    }
+                    else if (pPaM->GetPoint()->nNode.GetNode().IsTxtNode() )
+                    {
+                        pPaM->SetMark();
+                        nLen -= pPaM->GetPoint()->nContent.GetIndex();
+
+                        SwTxtNode* pTxtNode = 0;
+                        // Find first text node which affected by the comment
+                        while( pPaM->GetPoint()->nNode >= 0 )
+                        {
+                            SwNode* pNode = 0;
+                            // Find previous text node
+                            do
+                            {
+                                pPaM->GetPoint()->nNode--;
+                                nLen--; // End line character
+                                pNode = &pPaM->GetPoint()->nNode.GetNode();
+                            }
+                            while( !pNode->IsTxtNode() && pPaM->GetPoint()->nNode >= 0 );
+
+                            // Subtrackt previous text node's length
+                            if( pNode && pNode->IsTxtNode() )
+                            {
+                                pTxtNode = pNode->GetTxtNode();
+                                if( nLen < pTxtNode->Len() )
+                                    break;
+                                else
+                                    nLen -= pTxtNode->Len();
+                            }
+                        }
+
+                        // Set postion of the text range's first character
+                        if( pTxtNode )
+                        {
+                            pTxtNode->MakeStartIndex(&pPaM->GetPoint()->nContent);
+                            pPaM->GetPoint()->nContent += pTxtNode->Len() - nLen;
+                        }
+                    }
                 }
             }
         }

sw/qa/extras/ww8export/ww8export.cxx
@@ -163,7 +163,6 @@ DECLARE_WW8EXPORT_TEST(testFdo59530, "fdo59530.doc")
     uno::Reference<beans::XPropertySet> xPropertySet(xRunEnum->nextElement(), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("TextFieldStart"), getProperty<OUString>(xPropertySet, "TextPortionType"));
     xRunEnum->nextElement();
-    xRunEnum->nextElement();
     xPropertySet.set(xRunEnum->nextElement(), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("TextFieldEnd"), getProperty<OUString>(xPropertySet, "TextPortionType"));
 
@@ -173,6 +172,20 @@ DECLARE_WW8EXPORT_TEST(testFdo59530, "fdo59530.doc")
     uno::Reference<container::XEnumeration> xFields(xFieldsAccess->createEnumeration());
     xPropertySet.set(xFields->nextElement(), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("M"), getProperty<OUString>(xPropertySet, "Initials"));
+
+    // Test commented text range which spans over more text nodes
+    // Comment starts in the second paragraph
+    xRunEnumAccess.set(xParaEnum->nextElement(), uno::UNO_QUERY);
+    xRunEnum = xRunEnumAccess->createEnumeration();
+    xRunEnum->nextElement();
+    xPropertySet.set(xRunEnum->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("TextFieldStart"), getProperty<OUString>(xPropertySet, "TextPortionType"));
+    // Comment ends in the third paragraph
+    xRunEnumAccess.set(xParaEnum->nextElement(), uno::UNO_QUERY);
+    xRunEnum = xRunEnumAccess->createEnumeration();
+    xRunEnum->nextElement();
+    xPropertySet.set(xRunEnum->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("TextFieldEnd"), getProperty<OUString>(xPropertySet, "TextPortionType"));
 }
 
 #endif

commit 38c6b6637b94ec0d2a4611d15b4d6f2f6a9f3b0f

fix crash loading ooo#93570-3.doc
regression from ce9318fd2e09028ad9ebe5d8351e149486ce4987
cp#2013101510000026: doc import of comments affecting more text nodes

use Move(fnMoveBackward, fnGoNode) to at least ensure we stop going
backwards when we run out of valid places to go backwards to.

This still isn't great because the distance between two msword character
indexes only equates to the same distance between our characters in the very
simple cases

Change-Id: I248fd12c067577d2f1fd64f48583321eb6d453e4

sw/source/filter/ww8/ww8par.cxx
@@ -2170,9 +2170,18 @@ long SwWW8ImplReader::Read_And(WW8PLCFManResult* pRes)
             {
                 WW8_CP nStart = GetAnnotationStart(nAtnIndex);
                 WW8_CP nEnd = GetAnnotationEnd(GetAnnotationEndIndex(nAtnIndex));
+                //It is unfortunately fragile and wrong to assume that two
+                //character positions in the original word document, which is
+                //what nStart and nEnd are, will equate to the same length in
+                //the destination writer document.
+                //
+                //Better would be, while writing the content into the writer
+                //document to store the equivalent writer document positions
+                //that relate to each annotation index as the parser passes
+                //those points.
                 sal_Int32 nLen = nEnd - nStart;
                 if( nLen )
-                 {
+                {
                     if (pPaM->GetPoint()->nContent.GetIndex() >= nLen)
                     {
                         pPaM->SetMark();
@@ -2184,24 +2193,28 @@ long SwWW8ImplReader::Read_And(WW8PLCFManResult* pRes)
                         nLen -= pPaM->GetPoint()->nContent.GetIndex();
 
                         SwTxtNode* pTxtNode = 0;
-                        // Find first text node which affected by the comment
-                        while( pPaM->GetPoint()->nNode >= 0 )
+
+                        // Find first text node which is affected by the comment
+                        while (nLen > 0)
                         {
-                            SwNode* pNode = 0;
-                            // Find previous text node
-                            do
+                            // Move to previous content node
+                            bool bSuccess = pPaM->Move(fnMoveBackward, fnGoNode);
+
+                            if (!bSuccess)
                             {
-                                pPaM->GetPoint()->nNode--;
-                                nLen--; // End line character
-                                pNode = &pPaM->GetPoint()->nNode.GetNode();
+                                nLen = 0;
+                                break;
                             }
-                            while( !pNode->IsTxtNode() && pPaM->GetPoint()->nNode >= 0 );
+
+                            --nLen; // End line character
+
+                            SwNode& rNode = pPaM->GetPoint()->nNode.GetNode();
 
                             // Subtract previous text node's length
-                            if( pNode->IsTxtNode() )
+                            if (rNode.IsTxtNode())
                             {
-                                pTxtNode = pNode->GetTxtNode();
-                                if( nLen < pTxtNode->Len() )
+                                pTxtNode = rNode.GetTxtNode();
+                                if (nLen < pTxtNode->Len())
                                     break;
                                 else
                                     nLen -= pTxtNode->Len();