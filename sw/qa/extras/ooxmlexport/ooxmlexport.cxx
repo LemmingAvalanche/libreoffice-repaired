LemmingAvalanche/libreoffice-repaired

commit 7182ed9faabb85f7d1d55e96bcfd81d9322736b7

fdo#73596 [DOCX] Multiple Columns in Index
The number of columns in an Index is given by the "\c" switch.
During export, added section breaks before and after the Index;
this is to preserve the layout of the round tripped file in MS Office.

Modified MSWordExportBase::NoPageBreakSection so that
an incorrect "nextPage" section break is not exported.

Conflicts:
	sw/qa/extras/ooxmlexport/ooxmlexport.cxx
Reviewed on:
	https://gerrit.libreoffice.org/8047

Change-Id: I364e19d6739c4c6a60e3c605044fc1fafd9147d3

sw/source/filter/ww8/wrtw8nds.cxx
@@ -1910,7 +1910,6 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
             bLastCR = true;
     }
 
-    AttrOutput().StartParagraph( pTextNodeInfo );
 
     bool bFlyInTable = mpParentFrame && IsInTable();
 
@@ -1934,6 +1933,8 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
             m_aCurrentCharPropStarts.push( 0 );
         }
     }
+
+    AttrOutput().StartParagraph( pTextNodeInfo );
 
     const SwSection* pTOXSect = 0;
     if( bInWriteTOX )
@@ -2606,14 +2607,9 @@ bool MSWordExportBase::NoPageBreakSection( const SfxItemSet* pSet )
     const SfxPoolItem* pI;
     if( pSet)
     {
-        bool bNoPageBreak = false;
+        bool bNoPageBreak = true;
         if ( SFX_ITEM_ON != pSet->GetItemState(RES_PAGEDESC, true, &pI)
             || 0 == ((SwFmtPageDesc*)pI)->GetPageDesc() )
-        {
-            bNoPageBreak = true;
-        }
-
-        if (bNoPageBreak)
         {
             if (SFX_ITEM_ON != pSet->GetItemState(RES_BREAK, true, &pI))
                 bNoPageBreak = true;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -3661,6 +3661,28 @@ DECLARE_OOXMLEXPORT_TEST(testPageBreak,"fdo74566.docx")
     getRun(xParagraph4, 1, "Second Page First line after Page Break");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testAlphabeticalIndex_MultipleColumns,"alphabeticalIndex_MultipleColumns.docx")
+{
+    // Bug :: fdo#73596
+    /*
+     * Index with multiple columns was not imported correctly and
+     * hence not exported correctly...
+     * The column count is given by the \c switch.
+     * If the column count is explicitly specified,
+     * MS Office adds section breaks before and after the Index.
+     */
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+        if (!pXmlDoc)
+            return;
+    xmlNodeSetPtr pXmlNodes = getXPathNode(pXmlDoc,"/w:document/w:body/w:p[3]/w:r[2]/w:instrText");
+    xmlNodePtr pXmlNode = pXmlNodes->nodeTab[0];
+    OUString contents = OUString::createFromAscii((const char*)((pXmlNode->children[0]).content));
+    CPPUNIT_ASSERT( contents.match(" INDEX \\c \"4\"\\e \"") );
+    // check for section breaks after and before the Index Section
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[2]/w:pPr/w:sectPr/w:type","val","continuous");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[9]/w:pPr/w:sectPr/w:type","val","continuous");
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -271,6 +271,7 @@ void DocxAttributeOutput::StartParagraph( ww8::WW8TableNodeInfo::Pointer_t pText
     m_pSectionInfo.reset();
 
     m_bParagraphOpened = true;
+    m_bIsFirstParagraph = false;
 }
 
 void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pTextNodeInfoInner )
@@ -3969,7 +3970,9 @@ void DocxAttributeOutput::SectionBreak( sal_uInt8 nC, const WW8_SepInfo* pSectio
         case msword::PageBreak:
             if ( pSectionInfo )
             {
-                if ( !m_bParagraphOpened )
+                // don't add section properties if this will be the first
+                // paragraph in the document
+                if ( !m_bParagraphOpened && !m_bIsFirstParagraph)
                 {
                     // Create a dummy paragraph if needed
                     m_pSerializer->startElementNS( XML_w, XML_p, FSEND );
@@ -6624,6 +6627,7 @@ DocxAttributeOutput::DocxAttributeOutput( DocxExport &rExport, FSHelperPtr pSeri
       m_nNextAnnotationMarkId( 0 ),
       m_pTableWrt( NULL ),
       m_bParagraphOpened( false ),
+      m_bIsFirstParagraph( true ),
       m_nColBreakStatus( COLBRK_NONE ),
       m_nTextFrameLevel( 0 ),
       m_closeHyperlinkInThisRun( false ),

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -745,6 +745,7 @@ private:
     SwWriteTable *m_pTableWrt;
 
     bool m_bParagraphOpened;
+    bool m_bIsFirstParagraph;
 
     // Remember that a column break has to be opened at the
     // beginning of the next paragraph

sw/source/filter/ww8/docxexport.hxx
@@ -117,6 +117,8 @@ public:
 
     virtual bool FieldsQuoted() const { return true; }
 
+    virtual bool AddSectionBreaksForTOX() const { return true; }
+
     virtual bool ignoreAttributeForStyles( sal_uInt16 nWhich ) const;
 
     /// Guess the script (asian/western).

sw/source/filter/ww8/rtfexport.hxx
@@ -65,6 +65,9 @@ public:
     virtual bool SupportsOneColumnBreak() const { return false; }
 
     virtual bool FieldsQuoted() const { return true; }
+
+    virtual bool AddSectionBreaksForTOX() const { return false; }
+
     /// Guess the script (asian/western).
     virtual bool CollapseScriptsforWordOk( sal_uInt16 nScript, sal_uInt16 nWhich );
 

sw/source/filter/ww8/wrtww8.hxx
@@ -639,6 +639,9 @@ public:
     /// Determines if the import filter already quoted fields or not.
     virtual bool FieldsQuoted() const = 0;
 
+    /// Determines the Section Breaks are to be added for TOX Section
+    virtual bool AddSectionBreaksForTOX() const = 0;
+
     /// Used to filter out attributes that can be e.g. written to .doc but not to .docx
     virtual bool ignoreAttributeForStyles( sal_uInt16 /*nWhich*/ ) const { return false; }
 
@@ -965,6 +968,8 @@ public:
     virtual bool SupportsOneColumnBreak() const { return false; }
 
     virtual bool FieldsQuoted() const { return false; }
+
+    virtual bool AddSectionBreaksForTOX() const { return false; }
 private:
     /// Format-dependent part of the actual export.
     virtual void ExportDocument_Impl();

commit 3199dec71add10752d612cd27305380a2f61601d

MSWordExportBase: fix export of header/footer in case of multiple columns
Regression from 7182ed9faabb85f7d1d55e96bcfd81d9322736b7 (fdo#73596
[DOCX] Multiple Columns in Index, 2014-02-13), header / footer was lost
in multi-column section. This fixes both DOC and DOCX export.

Change-Id: Icb30ec882b38853e25289299d8f829a5cf56e6d8

sw/source/filter/ww8/wrtw8nds.cxx
@@ -2733,9 +2733,14 @@ bool MSWordExportBase::NoPageBreakSection( const SfxItemSet* pSet )
     const SfxPoolItem* pI;
     if( pSet)
     {
-        bool bNoPageBreak = true;
+        bool bNoPageBreak = false;
         if ( SFX_ITEM_ON != pSet->GetItemState(RES_PAGEDESC, true, &pI)
             || 0 == ((SwFmtPageDesc*)pI)->GetPageDesc() )
+        {
+            bNoPageBreak = true;
+        }
+
+        if (bNoPageBreak)
         {
             if (SFX_ITEM_ON != pSet->GetItemState(RES_BREAK, true, &pI))
                 bNoPageBreak = true;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -3745,6 +3745,13 @@ DECLARE_OOXMLEXPORT_TEST(testfdo80898, "fdo80898.docx")
                 "/word/embeddings/oleObject1.doc");
 }
 
+DECLARE_OOXMLEXPORT_TEST(test2colHeader, "2col-header.docx")
+{
+    // Header was lost on export when the document had multiple columns.
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(true, getProperty<bool>(xPageStyle, "HeaderIsOn"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */