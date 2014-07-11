LemmingAvalanche/libreoffice-repaired

commit e4422866795281f3540da8e935c0008cc6b50e44

Related: #i124638# Corrected relationship between DrawModel and...
DocShell in Writer, made SwDrawModel and handling more known

(includes suspicious removal of setting SID_ATTR_LINEEND_WIDTH_DEFAULT)

(cherry picked from commit a7ccadbf3b1ac378ca15d3630d48f48734700e5c)

Conflicts:
	sw/inc/IDocumentDrawModelAccess.hxx
	sw/inc/doc.hxx
	sw/inc/docsh.hxx
	sw/inc/dpage.hxx
	sw/inc/drawdoc.hxx
	sw/source/core/access/accmap.cxx
	sw/source/core/doc/doc.cxx
	sw/source/core/doc/docdesc.cxx
	sw/source/core/doc/docdraw.cxx
	sw/source/core/doc/docfly.cxx
	sw/source/core/doc/doclay.cxx
	sw/source/core/doc/docnew.cxx
	sw/source/core/draw/dcontact.cxx
	sw/source/core/draw/dpage.cxx
	sw/source/core/draw/drawdoc.cxx
	sw/source/core/frmedt/feshview.cxx
	sw/source/core/layout/fly.cxx
	sw/source/core/layout/frmtool.cxx
	sw/source/core/layout/newfrm.cxx
	sw/source/core/layout/paintfrm.cxx
	sw/source/core/undo/docundo.cxx
	sw/source/core/undo/unattr.cxx
	sw/source/core/unocore/unodraw.cxx
	sw/source/core/unocore/unoframe.cxx
	sw/source/core/view/vdraw.cxx
	sw/source/core/view/viewimp.cxx
	sw/source/core/view/viewsh.cxx
	sw/source/filter/html/htmldrawreader.cxx
	sw/source/filter/rtf/rtffly.cxx
	sw/source/filter/rtf/swparrtf.cxx
	sw/source/filter/ww8/docxattributeoutput.cxx
	sw/source/filter/ww8/rtfattributeoutput.cxx
	sw/source/filter/ww8/wrtw8esh.cxx
	sw/source/filter/ww8/wrtww8.cxx
	sw/source/filter/ww8/ww8graf.cxx
	sw/source/filter/xml/swxml.cxx
	sw/source/filter/xml/xmlexp.cxx
	sw/source/filter/xml/xmlimp.cxx
	sw/source/ui/app/docshdrw.cxx
	sw/source/ui/uiview/view.cxx
	sw/source/ui/uno/unodefaults.cxx
	sw/source/uibase/app/docst.cxx
	sw/source/uibase/app/docstyle.cxx
	sw/source/uibase/dialog/SwSpellDialogChildWindow.cxx
	sw/source/uibase/lingu/sdrhhcwrap.cxx
	sw/source/uibase/shells/drwbassh.cxx
	sw/source/uibase/shells/frmsh.cxx
	sw/source/uibase/shells/grfsh.cxx
	sw/source/uibase/shells/textsh1.cxx
	sw/source/uibase/uno/unotxdoc.cxx
	sw/source/uibase/utlui/content.cxx

Conflicts:

sw/source/core/doc/docnew.cxx
@@ -677,13 +677,20 @@ void SwDoc::SetDocShell( SwDocShell* pDSh )
         }
 
         mpLinkMgr->SetPersist( mpDocShell );
+
         if( GetDocumentDrawModelManager().GetDrawModel() )
         {
-            ((SwDrawDocument*)GetDocumentDrawModelManager().GetDrawModel())->SetObjectShell( mpDocShell );
+            GetDocumentDrawModelManager().GetDrawModel()->SetObjectShell( mpDocShell );
             GetDocumentDrawModelManager().GetDrawModel()->SetPersist( mpDocShell );
             OSL_ENSURE( GetDocumentDrawModelManager().GetDrawModel()->GetPersist() == GetPersist(),
                     "draw model's persist is out of sync" );
         }
+
+        // set DocShell pointer also on DrawModel
+        InitDrawModelAndDocShell(mpDocShell, GetDocumentDrawModelManager().GetDrawModel());
+        OSL_ENSURE(!GetDocumentDrawModelManager().GetDrawModel() ||
+            GetDocumentDrawModelManager().GetDrawModel()->GetPersist() == GetPersist(),
+            "draw model's persist is out of sync");
     }
 }
 

sw/source/uibase/app/docshdrw.cxx
@@ -33,25 +33,59 @@
 using namespace ::com::sun::star;
 
 // Load Document
-void  SwDocShell::InitDraw()
+void InitDrawModelAndDocShell(SwDocShell* pSwDocShell, SwDrawModel* pSwDrawDocument)
 {
-    SdrModel *pDrDoc = mpDoc->getIDocumentDrawModelAccess().GetDrawModel();
-    if( pDrDoc )
+    if(pSwDrawDocument)
     {
-        // create Lists and Tables DocShell's ItemSet
-        PutItem( SvxGradientListItem( pDrDoc->GetGradientList(), SID_GRADIENT_LIST ) );
-        PutItem( SvxHatchListItem( pDrDoc->GetHatchList(), SID_HATCH_LIST ) );
-        PutItem( SvxBitmapListItem( pDrDoc->GetBitmapList(), SID_BITMAP_LIST ) );
-        PutItem( SvxDashListItem( pDrDoc->GetDashList(), SID_DASH_LIST ) );
-        PutItem( SvxLineEndListItem( pDrDoc->GetLineEndList(), SID_LINEEND_LIST ) );
-        PutItem( SvxColorListItem( pDrDoc->GetColorList(), SID_COLOR_TABLE ));
+        if(pSwDocShell == pSwDrawDocument->GetObjectShell())
+        {
+            // association already done, nothing to do
+        }
+        else
+        {
+            // set object shell (mainly for FormControl stuff), maybe zero
+            pSwDrawDocument->SetObjectShell(pSwDocShell);
 
-        Outliner& rOutliner = pDrDoc->GetDrawOutliner();
-        uno::Reference<linguistic2::XHyphenator> xHyphenator( ::GetHyphenator() );
-        rOutliner.SetHyphenator( xHyphenator );
+            // set persist, maybe zero
+            pSwDrawDocument->SetPersist(pSwDocShell);
+
+            // get and decide on the color table to use
+            if(pSwDocShell)
+            {
+                const SvxColorListItem* pColItemFromDocShell = static_cast< const SvxColorListItem* >(pSwDocShell->GetItem(SID_COLOR_TABLE));
+
+                if(pColItemFromDocShell)
+                {
+                    // the DocShell has a ColorTable, use it also in DrawingLayer
+                    XColorListRef xCol(pColItemFromDocShell->GetColorList());
+                    pSwDrawDocument->SetPropertyList(static_cast<XPropertyList*>(xCol.get()));
+                }
+                else
+                {
+                    // Use the ColorTable which is used at the DrawingLayer's SdrModel
+                    pSwDocShell->PutItem(SvxColorListItem(pSwDrawDocument->GetColorList(), SID_COLOR_TABLE));
+                }
+
+                // add other tables in SfxItemSet of the DocShell
+                pSwDocShell->PutItem(SvxGradientListItem(pSwDrawDocument->GetGradientList(), SID_GRADIENT_LIST));
+                pSwDocShell->PutItem(SvxHatchListItem(pSwDrawDocument->GetHatchList(), SID_HATCH_LIST));
+                pSwDocShell->PutItem(SvxBitmapListItem(pSwDrawDocument->GetBitmapList(), SID_BITMAP_LIST));
+                pSwDocShell->PutItem(SvxDashListItem(pSwDrawDocument->GetDashList(), SID_DASH_LIST));
+                pSwDocShell->PutItem(SvxLineEndListItem(pSwDrawDocument->GetLineEndList(), SID_LINEEND_LIST));
+            }
+
+            // init hyphenator for DrawingLayer outliner
+            uno::Reference<linguistic2::XHyphenator> xHyphenator(::GetHyphenator());
+            Outliner& rOutliner = pSwDrawDocument->GetDrawOutliner();
+
+            rOutliner.SetHyphenator(xHyphenator);
+        }
     }
-    else
-        PutItem( SvxColorListItem( XColorList::GetStdColorList(), SID_COLOR_TABLE ));
+    else if(pSwDocShell)
+    {
+        // fallback: add the default color list to have one when someone requests it from the DocShell
+        pSwDocShell->PutItem(SvxColorListItem(XColorList::GetStdColorList(), SID_COLOR_TABLE));
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/inc/IDocumentDrawModelAccess.hxx
@@ -22,7 +22,7 @@
 
 #include <svx/svdtypes.hxx>
 
-class SwDrawDocument;
+class SwDrawModel;
 class SdrPageView;
 
 class IDocumentDrawModelAccess
@@ -31,10 +31,10 @@ public:
 
     /** Draw Model and id accessors
      */
-    virtual const SwDrawDocument* GetDrawModel() const = 0;
-    virtual SwDrawDocument* GetDrawModel() = 0;
-    virtual SwDrawDocument* _MakeDrawModel() = 0;
-    virtual SwDrawDocument* GetOrCreateDrawModel() = 0;
+    virtual const SwDrawModel* GetDrawModel() const = 0;
+    virtual SwDrawModel* GetDrawModel() = 0;
+    virtual SwDrawModel* _MakeDrawModel() = 0;
+    virtual SwDrawModel* GetOrCreateDrawModel() = 0;
     virtual SdrLayerID GetHeavenId() const = 0;
     virtual SdrLayerID GetHellId() const = 0;
     virtual SdrLayerID GetControlsId() const = 0;

sw/inc/doc.hxx
@@ -87,7 +87,7 @@ class Outliner;
 class OutputDevice;
 class Point;
 class SbxArray;
-class SdrModel;
+class SwDrawModel;
 class SdrObject;
 class SdrUndoAction;
 class VirtualDevice;

sw/inc/docsh.hxx
@@ -51,6 +51,12 @@ class IDocumentDeviceAccess;
 class IDocumentSettingAccess;
 class IDocumentTimerAccess;
 class IDocumentChartDataProviderAccess;
+class SwDocShell;
+class SwDrawModel;
+
+// initialize DrawModel (in form of a SwDrawModel) and DocShell (in form of a SwDocShell)
+// as needed, one or both parameters may be zero
+void SAL_DLLPRIVATE InitDrawModelAndDocShell(SwDocShell* pSwDocShell, SwDrawModel* pSwDrawModel);
 
 class SW_DLLPUBLIC SwDocShell: public SfxObjectShell, public SfxListener
 {

sw/inc/dpage.hxx
@@ -23,7 +23,7 @@
 #include <svx/svdobj.hxx>
 
 class SdrPageGridFrameList;
-class SwDrawDocument;
+class SwDrawModel;
 class SwDoc;
 
 class SwDPage : public FmFormPage, public SdrObjUserCall
@@ -32,7 +32,7 @@ class SwDPage : public FmFormPage, public SdrObjUserCall
     SwDoc&                  rDoc;
 
 public:
-    SwDPage(SwDrawDocument& rNewModel, bool bMasterPage=false);
+    SwDPage(SwDrawModel& rNewModel, bool bMasterPage=false);
     virtual ~SwDPage();
 
     // #i3694#

sw/inc/drawdoc.hxx
@@ -24,12 +24,12 @@
 class SwDoc;
 class SwDocShell;
 
-class SwDrawDocument : public FmFormModel
+class SwDrawModel : public FmFormModel
 {
     SwDoc* pDoc;
 public:
-    SwDrawDocument( SwDoc* pDoc );
-    virtual ~SwDrawDocument();
+    SwDrawModel( SwDoc* pDoc );
+    virtual ~SwDrawModel();
 
     const SwDoc& GetDoc() const { return *pDoc; }
           SwDoc& GetDoc()       { return *pDoc; }

sw/source/core/doc/DocumentDrawModelManager.cxx
@@ -98,8 +98,8 @@ void DocumentDrawModelManager::InitDrawModel()
     m_rSwdoc.GetAttrPool().SetPoolDefaultItem(SvxFontHeightItem( 240, 100, EE_CHAR_FONTHEIGHT ));
 
     SAL_INFO( "sw.doc", "before create DrawDocument" );
-    // The document owns the SwDrawDocument. We always have two layers and one page.
-    mpDrawModel = new SwDrawDocument( &m_rSwdoc );
+    // The document owns the SwDrawModel. We always have two layers and one page.
+    mpDrawModel = new SwDrawModel( &m_rSwdoc );
 
     mpDrawModel->EnableUndo( m_rSwdoc.GetIDocumentUndoRedo().DoesUndo() );
 
@@ -196,17 +196,17 @@ void DocumentDrawModelManager::ReleaseDrawModel()
 
 
 
-const SwDrawDocument* DocumentDrawModelManager::GetDrawModel() const
+const SwDrawModel* DocumentDrawModelManager::GetDrawModel() const
 {
     return mpDrawModel;
 }
 
-SwDrawDocument* DocumentDrawModelManager::GetDrawModel()
+SwDrawModel* DocumentDrawModelManager::GetDrawModel()
 {
     return mpDrawModel;
 }
 
-SwDrawDocument* DocumentDrawModelManager::_MakeDrawModel()
+SwDrawModel* DocumentDrawModelManager::_MakeDrawModel()
 {
     OSL_ENSURE( !mpDrawModel, "_MakeDrawModel: Why?" );
     InitDrawModel();
@@ -229,7 +229,7 @@ SwDrawDocument* DocumentDrawModelManager::_MakeDrawModel()
     return mpDrawModel;
 }
 
-SwDrawDocument* DocumentDrawModelManager::GetOrCreateDrawModel()
+SwDrawModel* DocumentDrawModelManager::GetOrCreateDrawModel()
 {
     return GetDrawModel() ? GetDrawModel() : _MakeDrawModel();
 }

commit 19af079b607c29ed299e81b860d34a65e455e12c

Resolves: fdo#81147 fix mismatching drawlayer and docshell colorlists
a bit of mis-merge + a bit of underlying crashiness

regression from e4422866795281f3540da8e935c0008cc6b50e44

Change-Id: Id0f7b6b6f13e1ec70dc900c21d2d020a44cf6081

sw/source/core/doc/docnew.cxx
@@ -678,14 +678,6 @@ void SwDoc::SetDocShell( SwDocShell* pDSh )
 
         mpLinkMgr->SetPersist( mpDocShell );
 
-        if( GetDocumentDrawModelManager().GetDrawModel() )
-        {
-            GetDocumentDrawModelManager().GetDrawModel()->SetObjectShell( mpDocShell );
-            GetDocumentDrawModelManager().GetDrawModel()->SetPersist( mpDocShell );
-            OSL_ENSURE( GetDocumentDrawModelManager().GetDrawModel()->GetPersist() == GetPersist(),
-                    "draw model's persist is out of sync" );
-        }
-
         // set DocShell pointer also on DrawModel
         InitDrawModelAndDocShell(mpDocShell, GetDocumentDrawModelManager().GetDrawModel());
         OSL_ENSURE(!GetDocumentDrawModelManager().GetDrawModel() ||

sw/source/uibase/app/docshdrw.cxx
@@ -63,7 +63,19 @@ void InitDrawModelAndDocShell(SwDocShell* pSwDocShell, SwDrawModel* pSwDrawDocum
                 else
                 {
                     // Use the ColorTable which is used at the DrawingLayer's SdrModel
-                    pSwDocShell->PutItem(SvxColorListItem(pSwDrawDocument->GetColorList(), SID_COLOR_TABLE));
+                    XColorListRef xColorList = pSwDrawDocument->GetColorList();
+                    if (xColorList.is())
+                    {
+                        pSwDocShell->PutItem(SvxColorListItem(xColorList, SID_COLOR_TABLE));
+                    }
+                    else
+                    {
+                        // there wasn't one, get the standard and set to the
+                        // docshell and then to the drawdocument
+                        xColorList = XColorList::GetStdColorList();
+                        pSwDocShell->PutItem(SvxColorListItem(xColorList, SID_COLOR_TABLE));
+                        pSwDrawDocument->SetPropertyList(xColorList.get());
+                    }
                 }
 
                 // add other tables in SfxItemSet of the DocShell