LemmingAvalanche/libreoffice-repaired

commit db94d6c833433a47a8346849ea7366f4f95f2d26

vcl consitent use of long for corrdinate
most of length in vcl are calculated in 'long'
but array of X position tend to be in sal_Int32.
As a prep work to be able to support 'double'
as the base type of Device Coordinate, harmonize
the use of 'long' for non-float coordinate.

Change-Id: I7cb33301ff6a5e2c62247b36a4e07e168a58a323

vcl/source/gdi/metaact.cxx
@@ -1176,8 +1176,8 @@ MetaTextArrayAction::MetaTextArrayAction( const MetaTextArrayAction& rAction ) :
     {
         const sal_Int32 nAryLen = mnLen;
 
-        mpDXAry = new sal_Int32[ nAryLen ];
-        memcpy( mpDXAry, rAction.mpDXAry, nAryLen * sizeof( sal_Int32 ) );
+        mpDXAry = new long[ nAryLen ];
+        memcpy( mpDXAry, rAction.mpDXAry, nAryLen * sizeof( long ) );
     }
     else
         mpDXAry = NULL;
@@ -1185,7 +1185,7 @@ MetaTextArrayAction::MetaTextArrayAction( const MetaTextArrayAction& rAction ) :
 
 MetaTextArrayAction::MetaTextArrayAction( const Point& rStartPt,
                                           const OUString& rStr,
-                                          const sal_Int32* pDXAry,
+                                          const long* pDXAry,
                                           sal_Int32 nIndex,
                                           sal_Int32 nLen ) :
     MetaAction  ( META_TEXTARRAY_ACTION ),
@@ -1198,7 +1198,7 @@ MetaTextArrayAction::MetaTextArrayAction( const Point& rStartPt,
 
     if( nAryLen )
     {
-        mpDXAry = new sal_Int32[ nAryLen ];
+        mpDXAry = new long[ nAryLen ];
         memcpy( mpDXAry, pDXAry, nAryLen * sizeof( sal_Int32 ) );
     }
     else
@@ -1293,13 +1293,16 @@ void MetaTextArrayAction::Read( SvStream& rIStm, ImplMetaReadData* pData )
         // #i9762#, #106172# Ensure that DX array is at least mnLen entries long
         if ( mnLen >= nAryLen )
         {
-            mpDXAry = new (std::nothrow)sal_Int32[ mnLen ];
+            mpDXAry = new (std::nothrow)long[ mnLen ];
             if ( mpDXAry )
             {
                 sal_Int32 i;
+                sal_Int32 val;
                 for( i = 0; i < nAryLen; i++ )
-                    rIStm.ReadInt32( mpDXAry[ i ] );
-
+                {
+                    rIStm.ReadInt32( val);
+                    mpDXAry[ i ] = val;
+                }
                 // #106172# setup remainder
                 for( ; i < mnLen; i++ )
                     mpDXAry[ i ] = 0;

commit efc2c9667e34534ab5cc5fa10211f9c0a4c22c00

vcl: fix MetaTextArrayAction copying only half of mpDXArray
This causes an infinite loop in forms_unoapi test forms.OCheckBoxModel

(regression from db94d6c833433a47a8346849ea7366f4f95f2d26)

Change-Id: I46715e1f1381fa92419558c34c6d47c3767de3e1

vcl/source/gdi/metaact.cxx
@@ -1199,7 +1199,7 @@ MetaTextArrayAction::MetaTextArrayAction( const Point& rStartPt,
     if( nAryLen )
     {
         mpDXAry = new long[ nAryLen ];
-        memcpy( mpDXAry, pDXAry, nAryLen * sizeof( sal_Int32 ) );
+        memcpy( mpDXAry, pDXAry, nAryLen * sizeof(long) );
     }
     else
         mpDXAry = NULL;