LemmingAvalanche/libreoffice-repaired

commit f900ef8b80507804d285fc0a1cdef83490553956

RTF import: handle RTF_FTNSEP and RTF_CHFTNSEP
Commit 011524d3d7f754d1e09969cabceba3f183527bb7 (fdo#68787 DOCX import:
handle when w:separator is missing for footnotes, 2013-09-04) disabled
footnote separator by default in dmapper, as the OOXML tokenizer always
provided a uFtnEdnSep in case a separator was wanted.

Let the RTF tokenizer do the same, this way we're in sync with Word
again: if RTF_CHFTNSEP is in RTF_FTNSEP, then we show the separator,
otherwise we don't.

Change-Id: I74b46c5d71227682e093695336dc9eb6fde22121

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1739,6 +1739,10 @@ int RTFDocumentImpl::dispatchDestination(RTFKeyword nKeyword)
                 m_aStates.top().bInShapeGroup = true;
             }
             break;
+        case RTF_FTNSEP:
+            m_aStates.top().nDestinationState = DESTINATION_FOOTNOTESEPARATOR;
+            m_aStates.top().aCharacterAttributes.set(NS_ooxml::LN_CT_FtnEdn_type, RTFValue::Pointer_t(new RTFValue(NS_ooxml::LN_Value_wordprocessingml_ST_FtnEdn_separator)));
+            break;
         default:
             SAL_INFO("writerfilter", "TODO handle destination '" << lcl_RtfToString(nKeyword) << "'");
             // Make sure we skip destinations (even without \*) till we don't handle them
@@ -1806,7 +1810,13 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
                     runProps();
                 if (!m_aStates.top().pCurrentBuffer)
                 {
-                    parBreak();
+                    if (m_aStates.top().nDestinationState == DESTINATION_FOOTNOTESEPARATOR)
+                    {
+                        static const sal_Unicode uCR = 0xd;
+                        Mapper().utext((const sal_uInt8*)&uCR, 1);
+                    }
+                    else
+                        parBreak();
                     // Not in table? Reset max width.
                     m_nCellxMax = 0;
                 }
@@ -2018,6 +2028,12 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
                 text(aStr);
                 singleChar(0x14, true);
                 singleChar(0x15);
+            }
+            break;
+        case RTF_CHFTNSEP:
+            {
+                static const sal_Unicode uFtnEdnSep = 0x3;
+                Mapper().utext((const sal_uInt8*)&uFtnEdnSep, 1);
             }
             break;
         default:

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1405,6 +1405,13 @@ DECLARE_RTFIMPORT_TEST(testBackground, "background.rtf")
     CPPUNIT_ASSERT_EQUAL(false, bool(getProperty<sal_Bool>(getShape(2), "Opaque")));
 }
 
+DECLARE_RTFIMPORT_TEST(testFdo68787, "fdo68787.rtf")
+{
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
+    // This was 0, the 'lack of \chftnsep' <-> '0 line width' mapping was missing in the RTF tokenizer.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(25), getProperty<sal_Int32>(xPageStyle, "FootnoteLineRelativeWidth"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/extras/rtfimport/data/fdo68787.rtf
@@ -1,0 +1,9 @@
+{\rtf1
+\ftnbj
+{\*\ftnsep \chftnsep \par}
+\pard\plain
+This will have a separator
+\chftn 
+{\footnote \chftn Footnote.}
+\par
+}

writerfilter/source/rtftok/rtflistener.hxx
@@ -150,6 +150,7 @@ namespace writerfilter {
             DESTINATION_LFOLEVEL,
             DESTINATION_BACKGROUND,
             DESTINATION_SHAPEGROUP,
+            DESTINATION_FOOTNOTESEPARATOR,
         };
 
         enum RTFErrors

commit 1fcca46ccdb831a2b5e9e7ab84578fb96bc084c3

fdo#80486 writerfilter: RTF import: ignore \par inside \ftnsep
Word can apparently have complex footnote separators, but the
domain-mapper only sets a single flag that there is a separator
(m_bHasFtnSep), so the only thing that makes sense for \par here
is to completely ignore it.

(regression from f900ef8b80507804d285fc0a1cdef83490553956)

Change-Id: I32d9b01a0a9248ba646654295a74953a6cb74fb3

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2040,6 +2040,8 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
     break;
     case RTF_PAR:
     {
+        if (m_aStates.top().nDestinationState == DESTINATION_FOOTNOTESEPARATOR)
+            break; // just ignore it - only thing we read in here is CHFTNSEP
         checkFirstRun();
         bool bNeedPap = m_bNeedPap;
         checkNeedPap();
@@ -2047,13 +2049,7 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
             runProps();
         if (!m_aStates.top().pCurrentBuffer)
         {
-            if (m_aStates.top().nDestinationState == DESTINATION_FOOTNOTESEPARATOR)
-            {
-                static const sal_Unicode uCR = 0xd;
-                Mapper().utext((const sal_uInt8*)&uCR, 1);
-            }
-            else
-                parBreak();
+            parBreak();
             // Not in table? Reset max width.
             m_nCellxMax = 0;
         }

sw/qa/extras/rtfimport/data/fdo77996.rtf
@@ -26,6 +26,7 @@
 {\operator {\uc1 \u106 ?}{\uc1 \u97 ?}{\uc1 \u121 ?}}
 }
 
+\fet2 {\*\ftnsep \pard \plain {\insrsid \chftnsep \par }}{\*\ftnsepc \pard \plain {\insrsid \chftnsepc \par }}{\*\aftnsep \pard \plain {\insrsid \chftnsep \par }}{\*\aftnsepc \pard \plain {\insrsid \chftnsepc \par }}
 
 {\loch \af11 \hich \af11 \dbch \f11
 {\uc1 \u21414 ?}{\uc1 \u38376 ?}{\uc1 \u38056 ?}{\uc1 \u19994 ?}{\uc1 \u32929 ?}{\uc1 \u20221 ?}{\uc1 \u26377 ?}{\uc1 \u38480 ?}{\uc1 \u20844 ?}{\uc1 \u21496 ?}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1337,12 +1337,14 @@ DECLARE_RTFIMPORT_TEST(testFdo77996, "fdo77996.rtf")
     uno::Reference<document::XDocumentPropertiesSupplier> xDocumentPropertiesSupplier(mxComponent, uno::UNO_QUERY);
     uno::Reference<document::XDocumentProperties> xProps(xDocumentPropertiesSupplier->getDocumentProperties());
     CPPUNIT_ASSERT_EQUAL(OUString("Aln Lin (Bei Jing)"), xProps->getAuthor());
-    CPPUNIT_ASSERT_EQUAL(
-        OUString("\xe5\x8e\xa6\xe9\x97\xa8\xe9\x92\xa8\xe4\xb8\x9a\xe8\x82\xa1\xe4\xbb\xbd\xe6\x9c\x89\xe9\x99\x90\xe5\x85\xac\xe5\x8f\xb8", 30,
-            RTL_TEXTENCODING_UTF8),
-        xProps->getTitle());
+    OUString aTitle("\xe5\x8e\xa6\xe9\x97\xa8\xe9\x92\xa8\xe4\xb8\x9a\xe8\x82\xa1\xe4\xbb\xbd\xe6\x9c\x89\xe9\x99\x90\xe5\x85\xac\xe5\x8f\xb8", 30,
+            RTL_TEXTENCODING_UTF8);
+    CPPUNIT_ASSERT_EQUAL(aTitle, xProps->getTitle());
     uno::Reference<beans::XPropertySet> xUDProps(xProps->getUserDefinedProperties(), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("jay"), getProperty<OUString>(xUDProps, "Operator"));
+
+    // fdo#80486 also check that the ftnsep doesn't insert paragraph breaks
+    getParagraph(1, aTitle);
 }
 
 DECLARE_RTFIMPORT_TEST(testFdo47802, "fdo47802.rtf")