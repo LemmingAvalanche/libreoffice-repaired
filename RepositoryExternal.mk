LemmingAvalanche/libreoffice-repaired

commit 8affcf6e6eefec61b27003eced27d9490acf3196

fdo#63756 build libxml2 with ICU support
Change-Id: I0523e49e640812be435ba4c97b1881ca253eb2ab

RepositoryExternal.mk
@@ -915,22 +915,24 @@ define gb_LinkTarget__use_libxml2
 $(call gb_LinkTarget_use_package,$(1),xml2)
 $(call gb_LinkTarget_set_include,$(1),\
 	$$(INCLUDE) \
-	-I$(call gb_UnpackedTarball_get_dir,xml2)/include \
+	$(LIBXML_CFLAGS) \
+)
+
+$(call gb_LinkTarget_add_libs,$(1),\
+	$(LIBXML_LIBS) \
 )
 
 ifeq ($(COM),MSC)
-$(call gb_LinkTarget_add_libs,$(1),\
-	$(call gb_UnpackedTarball_get_dir,xml2)/win32/bin.msvc/libxml2.lib \
-)
-else
-$(call gb_LinkTarget_add_libs,$(1),\
-	-L$(call gb_UnpackedTarball_get_dir,xml2)/.libs -lxml2 \
-)
+$(call gb_LinkTarget_use_external,$(1),icu_headers)
 endif
 
 endef
 define gb_ExternalProject__use_libxml2
 $(call gb_ExternalProject_use_package,$(1),xml2)
+
+ifeq ($(COM),MSC)
+$(call gb_ExternalProject_use_external_project,$(1),icu)
+endif
 
 endef
 

configure.ac
@@ -7999,6 +7999,15 @@ if test "$with_system_libxml" = "yes"; then
 else
     AC_MSG_RESULT([internal])
     SYSTEM_LIBXML=
+    LIBXML_CFLAGS="-I${WORKDIR}/UnpackedTarball/xml2/include"
+    if test "$COM" = "MSC"; then
+        LIBXML_CFLAGS="${LIBXML_CFLAGS} -I${WORKDIR}/UnpackedTarball/icu/source/i18n -I${WORKDIR}/UnpackedTarball/icu/source/common"
+    fi
+    if test "$COM" = "MSC"; then
+        LIBXML_LIBS="${WORKDIR}/UnpackedTarball/xml2/win32/bin.msvc/libxml2.lib"
+    else
+        LIBXML_LIBS="-L${WORKDIR}/UnpackedTarball/xml2/.libs -lxml2"
+    fi
     BUILD_TYPE="$BUILD_TYPE LIBXML2"
 fi
 AC_SUBST(SYSTEM_LIBXML)

external/libabw/ExternalProject_libabw.mk
@@ -35,8 +35,7 @@ $(call gb_ExternalProject_get_state_target,libabw,build) :
 			--disable-werror \
 			$(if $(VERBOSE)$(verbose),--disable-silent-rules,--enable-silent-rules) \
 			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),\
-				-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side)) \
-			$(if $(SYSTEM_LIBXML),,-I$(call gb_UnpackedTarball_get_dir,xml2)/include)" \
+				-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side))" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)

external/libebook/ExternalProject_libebook.mk
@@ -39,7 +39,7 @@ $(call gb_ExternalProject_get_state_target,libebook,build) :
 			--disable-werror \
 			--disable-weffc \
 			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side))" \
-			XML_CFLAGS="$(if $(SYSTEM_LIBXML),$(LIBXML_CFLAGS),-I$(call gb_UnpackedTarball_get_dir,xml2)/include)" \
+			XML_CFLAGS="$(LIBXML_CFLAGS)" \
 			XML_LIBS="$(LIBXML_LIBS)" \
 			REVENGE_GENERATORS_CFLAGS=' ' REVENGE_GENERATORS_LIBS=' ' REVENGE_STREAM_CFLAGS=' ' REVENGE_STREAM_LIBS=' ' \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \

external/libetonyek/ExternalProject_libetonyek.mk
@@ -36,7 +36,7 @@ $(call gb_ExternalProject_get_state_target,libetonyek,build) :
 			--without-tools \
 			$(if $(VERBOSE)$(verbose),--disable-silent-rules,--enable-silent-rules) \
 			BOOST_CFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side))" \
-			XML_CFLAGS="$(if $(SYSTEM_LIBXML),$(LIBXML_CFLAGS),-I$(call gb_UnpackedTarball_get_dir,xml2)/include)" \
+			XML_CFLAGS="$(LIBXML_CFLAGS)" \
 			XML_LIBS="$(LIBXML_LIBS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \

external/liblangtag/ExternalProject_langtag.mk
@@ -25,10 +25,8 @@ $(call gb_ExternalProject_get_state_target,langtag,build):
 		./configure --disable-modules --disable-test --disable-introspection --disable-shared --enable-static --with-pic \
 		$(if $(filter TRUE,$(HAVE_GCC_BUILTIN_ATOMIC)),"lt_cv_has_atomic=yes","lt_cv_has_atomic=no") \
 		$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) "ac_cv_va_copy=no") \
-		$(if $(SYSTEM_LIBXML),\
-			$(if $(filter MACOSX,$(OS)),LIBXML2_CFLAGS="$(LIBXML_CFLAGS)" LIBXML2_LIBS="$(LIBXML_LIBS)"), \
-			LIBXML2_CFLAGS="-I$(call gb_UnpackedTarball_get_dir,xml2)/include" \
-			LIBXML2_LIBS=$(if $(filter WNTMSC,$(OS)$(COM)),"-L$(call gb_UnpackedTarball_get_dir,xml2)/win32/bin.msvc -llibxml2","-L$(call gb_UnpackedTarball_get_dir,xml2)/.libs -lxml2")) \
+		LIBXML2_CFLAGS="$(LIBXML_CFLAGS)" \
+		LIBXML2_LIBS="$(if $(filter WNTMSC,$(OS)$(COM)),-L$(call gb_UnpackedTarball_get_dir,xml2)/win32/bin.msvc -llibxml2,$(LIBXML_LIBS))" \
 		$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 		$(if $(filter-out LINUX FREEBSD,$(OS)),,LDFLAGS="-Wl$(COMMA)-z$(COMMA)origin -Wl$(COMMA)-rpath,\\"\$$\$$ORIGIN:'\'\$$\$$ORIGIN/../ure-link/lib) \
 		$(if $(filter-out SOLARIS,$(OS)),,LDFLAGS="-Wl$(COMMA)-z$(COMMA)origin -Wl$(COMMA)-R$(COMMA)\\"\$$\$$ORIGIN:'\'\$$\$$ORIGIN/../ure-link/lib) \

external/libvisio/ExternalProject_libvisio.mk
@@ -35,8 +35,7 @@ $(call gb_ExternalProject_get_state_target,libvisio,build) :
 			--disable-debug \
 			--disable-werror \
 			$(if $(VERBOSE)$(verbose),--disable-silent-rules,--enable-silent-rules) \
-			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side)) \
-			$(if $(SYSTEM_LIBXML),,-I$(call gb_UnpackedTarball_get_dir,xml2)/include)" \
+			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side))" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)

external/libxml2/ExternalProject_xml2.mk
@@ -28,10 +28,12 @@ $(call gb_ExternalProject_get_state_target,xml2,build):
 		&& $(MAKE) \
 	)
 else # COM=MSC
+$(call gb_ExternalProject_use_external_project,xml2,icu)
+
 $(call gb_ExternalProject_get_state_target,xml2,build):
 	$(call gb_ExternalProject_run,build,\
 		cscript configure.js \
-			iconv=no sax1=yes $(if $(MSVC_USE_DEBUG_RUNTIME),cruntime=/MDd) \
+			iconv=no icu=yes sax1=yes $(if $(MSVC_USE_DEBUG_RUNTIME),cruntime=/MDd) \
 		&& unset MAKEFLAGS \
 		&& LIB="$(ILIB)" nmake \
 	,win32)

commit 581fc1622770ce42c8fc1747ac68e104f6463418

icu: un-break installation of Java extensions on Windows (rel. fdo#63756)
Linking libxml2 against ICU libraries has a nasty side effect:
The URE library javavm.dll links against URE libxml2.dll, which
is now linked against OOO icuuc53.dll; when a URE program, like
uno.exe, tries to load javavm.dll it fails because the OOO layer
"program" dir is not on PATH; this breaks the installation of Java
extensions.

Fix that by splitting up ICU libraries and putting the required ones
into URE layer.

(regression from 8affcf6e6eefec61b27003eced27d9490acf3196)

Change-Id: If98dd0357162cb632d9762cd2d20162de5eb1a52

RepositoryExternal.mk
@@ -1393,6 +1393,10 @@ endef
 
 else # !SYSTEM_ICU
 
+$(eval $(call gb_Helper_register_packages_for_install,ure, \
+	icu_ure \
+))
+
 $(eval $(call gb_Helper_register_packages_for_install,ooo, \
 	icu \
 ))

external/icu/ExternalPackage_icu.mk
@@ -18,14 +18,10 @@ ifeq ($(OS),WNT)
 
 ifeq ($(COM),GCC)
 $(eval $(call gb_ExternalPackage_add_files,icu,$(LIBO_LIB_FOLDER),\
-	source/lib/icudt$(ICU_MAJOR).dll \
-	source/lib/icuuc$(ICU_MAJOR).dll \
 	source/lib/icuin$(ICU_MAJOR).dll \
 ))
 else
 $(eval $(call gb_ExternalPackage_add_files,icu,$(LIBO_LIB_FOLDER),\
-	source/lib/icudt$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
-	source/lib/icuuc$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
 	source/lib/icuin$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
 ))
 endif # $(COM)

external/icu/ExternalPackage_icu_ure.mk
@@ -1,0 +1,33 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+# libxml2 is in URE and depends on icuuc*.dll on Windows - extra package needed
+$(eval $(call gb_ExternalPackage_ExternalPackage,icu_ure,icu))
+
+$(eval $(call gb_ExternalPackage_use_external_project,icu_ure,icu))
+
+ifneq ($(DISABLE_DYNLOADING),TRUE)
+ifeq ($(OS),WNT)
+
+ifeq ($(COM),GCC)
+$(eval $(call gb_ExternalPackage_add_files,icu_ure,$(LIBO_URE_LIB_FOLDER),\
+	source/lib/icudt$(ICU_MAJOR).dll \
+	source/lib/icuuc$(ICU_MAJOR).dll \
+))
+else
+$(eval $(call gb_ExternalPackage_add_files,icu_ure,$(LIBO_URE_LIB_FOLDER),\
+	source/lib/icudt$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
+	source/lib/icuuc$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
+))
+endif # $(COM)
+
+endif # $(OS)
+endif # DISABLE_DYNLOADING
+
+# vim: set noet sw=4 ts=4:

external/icu/Module_icu.mk
@@ -12,6 +12,7 @@ $(eval $(call gb_Module_Module,icu))
 $(eval $(call gb_Module_add_targets,icu,\
 	UnpackedTarball_icu \
 	ExternalPackage_icu \
+	ExternalPackage_icu_ure \
 	ExternalProject_icu \
 ))
 