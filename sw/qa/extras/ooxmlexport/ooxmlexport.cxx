LemmingAvalanche/libreoffice-repaired

commit 1046affe9d4085fe8b69d314df5020f318a97562

docx export: invalid sectPr added at the beginning of the doc
This reverts commit 5880f0db98f9a5596135884302a0021d75ddd462,
which was a revert of 7b7eef4d (fix for ooo#106749) with an
alternative fix to avoid a regression (fdo#56513).

This commit contain a fix for the sectPr issue, and does not
regress on the 2 previously fixed issue.

Change-Id: Ibc551b38d25554c59b7c4ac5a447a0d60323f53f
Reviewed-on: https://gerrit.libreoffice.org/1647
Reviewed-by: Noel Power <noel.power@suse.com>
Tested-by: Noel Power <noel.power@suse.com>

sw/source/filter/ww8/ww8atr.cxx
@@ -403,6 +403,15 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
     //section.
     bool bBreakSet = false;
 
+    const SwPageDesc * pPageDesc = rNd.FindPageDesc(sal_False);
+
+    if (pAktPageDesc != pPageDesc)
+    {
+        bBreakSet = true;
+        bNewPageDesc = true;
+        pAktPageDesc = pPageDesc;
+    }
+
     if ( pSet && pSet->Count() )
     {
         if ( SFX_ITEM_SET == pSet->GetItemState( RES_PAGEDESC, false, &pItem ) &&
@@ -456,7 +465,7 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
                     if ( pBreak &&
                          pBreak->GetBreak() == SVX_BREAK_PAGE_BEFORE )
                     {
-                        bNewPageDesc = SetAktPageDescFromNode( rNd );
+                        bNewPageDesc |= SetAktPageDescFromNode( rNd );
                     }
                 }
                 if ( !bNewPageDesc )

sw/source/filter/ww8/wrtww8.cxx
@@ -2368,9 +2368,7 @@ void MSWordExportBase::WriteText()
         }
         else if ( pNd->IsTableNode() )
         {
-            SwTable * pTable = &pNd->GetTableNode()->GetTable();
-            OutputSectionBreaks( &pTable->GetTableFmt()->GetAttrSet(), *pNd );
-            mpTableInfo->processSwTable( pTable );
+            mpTableInfo->processSwTable( &pNd->GetTableNode()->GetTable() );
         }
         else if ( pNd->IsSectionNode() && TXT_MAINTEXT == nTxtTyp )
             OutputSectionNode( *pNd->GetSectionNode() );

commit 25be4baa8ef9ef9a6f150f1268ef3786d22b0b74

MSWordExportBase::OutputSectionBreaks: avoid fake section breaks
Regression from 1046affe9d4085fe8b69d314df5020f318a97562 (docx export:
invalid sectPr added at the beginning of the doc, 2013-01-10), the
problem was that we even tried to generate section breaks at places
where the two page styles are in practice the same.

Change-Id: Iccf91ce542d9b075b93da25bcd42236db667e3d4

sw/source/filter/ww8/ww8atr.cxx
@@ -443,7 +443,7 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
             if ( isCellOpen && ( pAktPageDesc->GetName() != pPageDesc->GetName() ) )
                 pSet = NULL;
         }
-        else
+        else if (!sw::util::IsPlausableSingleWordSection(pAktPageDesc->GetFirstMaster(), pPageDesc->GetMaster()))
         {
             bBreakSet = true;
             bNewPageDesc = true;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -2532,7 +2532,7 @@ DECLARE_OOXMLEXPORT_TEST(testFdo77718, "fdo77718.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testTableCurruption, "tableCurrupt.docx")
 {
-    xmlDocPtr pXmlDoc = parseExport("word/header4.xml");
+    xmlDocPtr pXmlDoc = parseExport("word/header2.xml");
     if (!pXmlDoc)
         return;
     CPPUNIT_ASSERT(pXmlDoc) ;
@@ -2635,7 +2635,7 @@ DECLARE_OOXMLEXPORT_TEST(testOLEObjectinHeader, "2129393649.docx")
     // Problem was relationship entry for oleobject from header was
     // exported into document.xml.rels file because of this rels file
     // for headers were missing from document/word/rels.
-    xmlDocPtr pXmlDoc = parseExport("word/_rels/header3.xml.rels");
+    xmlDocPtr pXmlDoc = parseExport("word/_rels/header1.xml.rels");
     if(!pXmlDoc)
         return;
 
@@ -2971,12 +2971,12 @@ DECLARE_OOXMLEXPORT_TEST(testContentTypeOLE, "fdo77759.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testfdo78420, "fdo78420.docx")
 {
-    xmlDocPtr pXmlHeader = parseExport("word/header3.xml");
+    xmlDocPtr pXmlHeader = parseExport("word/header2.xml");
 
     if (!pXmlHeader)
        return;
 
-    xmlDocPtr pXmlHeaderRels = parseExport("word/_rels/header3.xml.rels");
+    xmlDocPtr pXmlHeaderRels = parseExport("word/_rels/header2.xml.rels");
     if(!pXmlHeaderRels)
        return;
 
@@ -3407,6 +3407,13 @@ DECLARE_OOXMLEXPORT_TEST(testPlausableBorder, "plausable-border.docx")
     if (xmlDocPtr pXmlDoc = parseExport())
         // Page break was exported as section break, this was 0
         assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:r/w:br", 1);
+}
+
+DECLARE_OOXMLEXPORT_TEST(testUnwantedSectionBreak, "unwanted-section-break.docx")
+{
+    if (xmlDocPtr pXmlDoc = parseExport())
+        // This was 2: an additional sectPr was added to the document.
+        assertXPath(pXmlDoc, "//w:sectPr", 1);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testfdo80897 , "fdo80897.docx")