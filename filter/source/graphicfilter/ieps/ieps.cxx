LemmingAvalanche/libreoffice-repaired

commit 927cd2c989a42f494a0790099fd64b3e28ae3755

Improves fdo#41407: Make gs a higher priority than convert for EPS rendering.
This speeds up EPS loading on systems where convert is configured to execute
gs for conversion instead of using libgs. This seems to be how convert is compiled
on most linux distos.

Change-Id: Ibfc95bff11076d286b6ba210d2db2129ac0bb45b
Reviewed-on: https://gerrit.libreoffice.org/6009
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

filter/source/graphicfilter/ieps/ieps.cxx
@@ -342,10 +342,10 @@ static bool RenderAsPNGThroughGS(const sal_uInt8* pBuf, sal_uInt32 nBytesRead,
 
 static bool RenderAsPNG(const sal_uInt8* pBuf, sal_uInt32 nBytesRead, Graphic &rGraphic)
 {
-    if (RenderAsPNGThroughConvert(pBuf, nBytesRead, rGraphic))
+    if (RenderAsPNGThroughGS(pBuf, nBytesRead, rGraphic))
         return true;
     else
-        return RenderAsPNGThroughGS(pBuf, nBytesRead, rGraphic);
+        return RenderAsPNGThroughConvert(pBuf, nBytesRead, rGraphic);
 }
 
 // this method adds a replacement action containing the original wmf or tiff replacement,

commit 7607b8c4a685ee180b9a43c21ac73624693d9016

fdo#81592 Use 24-bit color depth, not 256 colors when converting an EPS-file.
This regression was introduced in 927cd2c989a42f494a0790099fd64b3e28ae3755.

Change-Id: Ica48732398cd5d225b433b177ff6e6fff649e732
Reviewed-on: https://gerrit.libreoffice.org/11268
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

filter/source/graphicfilter/ieps/ieps.cxx
@@ -379,7 +379,7 @@ static bool RenderAsBMPThroughGS(const sal_uInt8* pBuf, sal_uInt32 nBytesRead,
     OUString arg6("-dTextAlphaBits=4");
     OUString arg7("-dGraphicsAlphaBits=4");
     OUString arg8("-r300x300");
-    OUString arg9("-sDEVICE=bmp256");
+    OUString arg9("-sDEVICE=bmp16m");
     OUString arg10("-sOutputFile=-");
     OUString arg11("-");
     rtl_uString *args[] =