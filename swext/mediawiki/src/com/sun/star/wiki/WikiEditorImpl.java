LemmingAvalanche/libreoffice-repaired

commit 0ba82ca67c56e3644945f8b08a881e54a1ab9f6f

java: remove dead methods
found by UCDetector

Change-Id: I219caa8e680dba5a395541a778df6144841c4dde

swext/mediawiki/src/com/sun/star/wiki/WikiEditorImpl.java
@@ -72,18 +72,7 @@ public final class WikiEditorImpl extends WeakBase
         m_aSettings = Settings.getSettings( m_xContext );
     }
 
-    public static XSingleComponentFactory __getComponentFactory( String sImplementationName )
-    {
-        XSingleComponentFactory xFactory = null;
 
-        if ( sImplementationName.equals( m_implementationName ) )
-            xFactory = Factory.createComponentFactory( WikiEditorImpl.class, m_serviceNames );
-        else if ( sImplementationName.equals( WikiOptionsEventHandlerImpl.m_sImplementationName ) )
-            xFactory = Factory.createComponentFactory( WikiOptionsEventHandlerImpl.class,
-                                                       WikiOptionsEventHandlerImpl.m_pServiceNames );
-
-        return xFactory;
-    }
 
     // com.sun.star.lang.XServiceInfo:
     public String getImplementationName()
@@ -193,15 +182,7 @@ public final class WikiEditorImpl extends WeakBase
         callStatusListener( urlstring );
     }
 
-    public void callStatusListeners()
-    {
-        for (Iterator<String> iter = m_statusListeners.keySet().iterator(); iter.hasNext(); )
-        {
-            String uristring = iter.next();
-            XStatusListener listener = m_statusListeners.get(uristring);
-            callStatusListener( uristring );
-        }
-    }
+
 
     private void callStatusListener( String uristring )
     {

commit 79885ed6422d52fc2c77f968d75b42828ee7d0ea

mediawiki options went missing
regression from 0ba82ca67c56e3644945f8b08a881e54a1ab9f6f

Change-Id: Ibd0645bd218c81da110b3704a530e3849b761fb4
Date:   Wed Aug 13 10:08:31 2014 +0200
    java: remove dead methods
    found by UCDetector

swext/mediawiki/src/com/sun/star/wiki/WikiEditorImpl.java
@@ -68,7 +68,18 @@ public final class WikiEditorImpl extends WeakBase
         m_aSettings = Settings.getSettings( m_xContext );
     }
 
+    public static XSingleComponentFactory __getComponentFactory( String sImplementationName )
+    {
+        XSingleComponentFactory xFactory = null;
 
+        if ( sImplementationName.equals( m_implementationName ) )
+            xFactory = Factory.createComponentFactory( WikiEditorImpl.class, m_serviceNames );
+        else if ( sImplementationName.equals( WikiOptionsEventHandlerImpl.m_sImplementationName ) )
+            xFactory = Factory.createComponentFactory( WikiOptionsEventHandlerImpl.class,
+                                                       WikiOptionsEventHandlerImpl.m_pServiceNames );
+
+        return xFactory;
+    }
 
     // com.sun.star.lang.XServiceInfo:
     public String getImplementationName()