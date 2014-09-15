LemmingAvalanche/libreoffice-repaired

commit 8a683cc3315f3cebc8406621594fd7170db20898

Minimize the constructor functions to a bare minimum.
Most of the constructors are supposed to be only a call of

  new TheInstance(arguments)

or an equivalent; so let's just change the constructor caller accordingly, to
accept unacquired new instance.

If there are exceptions that need to do more heavy lifting, they do not have
to use the constructor feature, or there can be a wrapper for the real
implementation, doing the additional work in their (C++) constructor.

Change-Id: I035c378778aeda60d15af4e56ca3761c586d5ded

sfx2/source/appl/appbaslib.cxx
@@ -121,7 +121,7 @@ void SfxBasicManagerHolder::storeLibrariesToStorage( const Reference< XStorage >
 #endif
 }
 
-Reference< XLibraryContainer > SfxBasicManagerHolder::getLibraryContainer( ContainerType _eType )
+XLibraryContainer * SfxBasicManagerHolder::getLibraryContainer( ContainerType _eType )
 {
     OSL_PRECOND( isValid(), "SfxBasicManagerHolder::getLibraryContainer: not initialized!" );
 
@@ -161,9 +161,7 @@ com_sun_star_comp_sfx2_ApplicationDialogLibraryContainer_get_implementation(
     css::uno::Sequence<css::uno::Any> const &)
 {
     SFX_APP()->GetBasicManager();
-    Reference< XInterface > xRet( SFX_APP()->GetDialogContainer(), UNO_QUERY );
-    xRet->acquire();
-    return xRet.get();
+    return SFX_APP()->GetDialogContainer();
 }
 
 //============================================================================
@@ -175,9 +173,7 @@ com_sun_star_comp_sfx2_ApplicationScriptLibraryContainer_get_implementation(
     css::uno::Sequence<css::uno::Any> const &)
 {
     SFX_APP()->GetBasicManager();
-    Reference< XInterface > xRet( SFX_APP()->GetBasicContainer(), UNO_QUERY );
-    xRet->acquire();
-    return xRet.get();
+    return SFX_APP()->GetBasicContainer();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

codemaker/source/cppumaker/cpputype.cxx
@@ -3463,7 +3463,7 @@ void ServiceType::dumpHxxFile(
                       "LO_URE_CTOR_FUN_")
                   << name_.replaceAll(".", "_dot_")
                   << (")(the_context.get(), ::css::uno::Sequence<"
-                      " ::css::uno::Any >())), ::SAL_NO_ACQUIRE),"
+                      " ::css::uno::Any >()))),"
                       " ::css::uno::UNO_QUERY);\n#else\n")
                   << indent() << "the_instance = ::css::uno::Reference< "
                   << scopedBaseName
@@ -3606,7 +3606,7 @@ void ServiceType::dumpHxxFile(
                 } else {
                     o << "the_arguments";
                 }
-                o << ")), ::SAL_NO_ACQUIRE), ::css::uno::UNO_QUERY);\n" << indent()
+                o << "))), ::css::uno::UNO_QUERY);\n" << indent()
                   << ("::css::uno::Reference< ::css::lang::XInitialization > "
                       "init(the_instance, ::css::uno::UNO_QUERY);\n")
                   << indent() << "if (init.is()) {\n"

cppuhelper/source/servicemanager.cxx
@@ -706,8 +706,7 @@ cppuhelper::ServiceManager::Data::Implementation::createInstance(
         if (constructor != 0) {
             return css::uno::Reference<css::uno::XInterface>(
                 (*constructor)(
-                    context.get(), css::uno::Sequence<css::uno::Any>()),
-                SAL_NO_ACQUIRE);
+                    context.get(), css::uno::Sequence<css::uno::Any>()));
         }
         if (factory1.is()) {
             return factory1->createInstanceWithContext(context);
@@ -730,8 +729,7 @@ cppuhelper::ServiceManager::Data::Implementation::createInstance(
         if (constructor != 0) {
             singleton.set(
                 (*constructor)(
-                    context.get(), css::uno::Sequence<css::uno::Any>()),
-                SAL_NO_ACQUIRE);
+                    context.get(), css::uno::Sequence<css::uno::Any>()));
         } else if (factory1.is()) {
             singleton = factory1->createInstanceWithContext(context);
         } else if (factory2.is()) {
@@ -761,7 +759,7 @@ cppuhelper::ServiceManager::Data::Implementation::createInstanceWithArguments(
             // should be removed again once XInitialization-based
             // implementations have become rare:
             css::uno::Reference<css::uno::XInterface> inst(
-                (*constructor)(context.get(), arguments), SAL_NO_ACQUIRE);
+                (*constructor)(context.get(), arguments));
             css::uno::Reference<css::lang::XInitialization> init(
                 inst, css::uno::UNO_QUERY);
             if (init.is()) {
@@ -793,8 +791,7 @@ cppuhelper::ServiceManager::Data::Implementation::createInstanceWithArguments(
         }
         if (constructor != 0) {
             //HACK: see above
-            singleton.set(
-                (*constructor)(context.get(), arguments), SAL_NO_ACQUIRE);
+            singleton.set((*constructor)(context.get(), arguments));
             css::uno::Reference<css::lang::XInitialization> init(
                 singleton, css::uno::UNO_QUERY);
             if (init.is()) {

include/sfx2/app.hxx
@@ -189,10 +189,8 @@ public:
     static ErrCode              CallAppBasic( const OUString& i_macroName, SbxArray* i_args = NULL, SbxValue* i_ret = NULL )
                                 { return CallBasic( i_macroName, SfxApplication::GetOrCreate()->GetBasicManager(), i_args, i_ret ); }
     BasicManager*               GetBasicManager();
-    com::sun::star::uno::Reference< com::sun::star::script::XLibraryContainer >
-                                GetDialogContainer();
-    com::sun::star::uno::Reference< com::sun::star::script::XLibraryContainer >
-                                GetBasicContainer();
+    com::sun::star::script::XLibraryContainer * GetDialogContainer();
+    com::sun::star::script::XLibraryContainer * GetBasicContainer();
     StarBASIC*                  GetBasic();
     sal_uInt16                  SaveBasicAndDialogContainer() const;
 

sax/source/expatwrap/sax_expat.cxx
@@ -1025,9 +1025,7 @@ com_sun_star_comp_extensions_xml_sax_ParserExpat_get_implementation(
     css::uno::XComponentContext *,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    rtl::Reference<SaxExpatParser> x(new SaxExpatParser);
-    x->acquire();
-    return static_cast<cppu::OWeakObject *>(x.get());
+    return static_cast<cppu::OWeakObject *>(new SaxExpatParser);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sax/source/expatwrap/saxwriter.cxx
@@ -1375,9 +1375,7 @@ com_sun_star_extensions_xml_sax_Writer_get_implementation(
     css::uno::XComponentContext *,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    rtl::Reference<SAXWriter> x(new SAXWriter);
-    x->acquire();
-    return static_cast<cppu::OWeakObject *>(x.get());
+    return static_cast<cppu::OWeakObject *>(new SAXWriter);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sax/source/fastparser/fastparser.cxx
@@ -1431,9 +1431,7 @@ com_sun_star_comp_extensions_xml_sax_FastParser_get_implementation(
     css::uno::XComponentContext *,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    rtl::Reference<FastSaxParser> x(new FastSaxParser);
-    x->acquire();
-    return static_cast<cppu::OWeakObject *>(x.get());
+    return static_cast<cppu::OWeakObject *>(new FastSaxParser);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/appl/appbas.cxx
@@ -127,11 +127,10 @@ BasicManager* SfxApplication::GetBasicManager()
 
 //--------------------------------------------------------------------
 
-Reference< XLibraryContainer > SfxApplication::GetDialogContainer()
+XLibraryContainer * SfxApplication::GetDialogContainer()
 {
 #ifdef DISABLE_SCRIPTING
-    Reference< XLibraryContainer >  dummy;
-    return dummy;
+    return NULL;
 #else
     if ( !pAppData_Impl->pBasicManager->isValid() )
         GetBasicManager();
@@ -141,11 +140,10 @@ Reference< XLibraryContainer > SfxApplication::GetDialogContainer()
 
 //--------------------------------------------------------------------
 
-Reference< XLibraryContainer > SfxApplication::GetBasicContainer()
+XLibraryContainer * SfxApplication::GetBasicContainer()
 {
 #ifdef DISABLE_SCRIPTING
-    Reference< XLibraryContainer >  dummy;
-    return dummy;
+    return NULL;
 #else
     if ( !pAppData_Impl->pBasicManager->isValid() )
         GetBasicManager();

commit 94be9471e26aeb4e999a255019fa2d6dabbc267c

Resolves: fdo#81635 crash after 3rd call to ApplicationScriptLibraryContainer
in PackageImpl::processPackage_

xComponentContext->getServiceManager()->createInstanceWithContext(
                    "com.sun.star.script.ApplicationScriptLibraryContainer"

does not increment the refcount, so on leaving the method the refcount
is decremented by 1, because on the first enter the refcount happens to
be three, then if the extension has more than 3 items in it the refcount
hits 0 here and falls over

some of fdo#79301, fdo#70607, fdo#66706 might be dups of this

regression from 8a683cc3315f3cebc8406621594fd7170db20898 I believe

Change-Id: I3da50693fe656b68c5555b641d9b32246c8fc208

sfx2/source/appl/appbaslib.cxx
@@ -152,28 +152,28 @@ bool SfxBasicManagerHolder::LegacyPsswdBinaryLimitExceeded( Sequence< OUString >
     return true;
 }
 
-
 // Service for application library container
-
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
 com_sun_star_comp_sfx2_ApplicationDialogLibraryContainer_get_implementation(
     css::uno::XComponentContext *,
     css::uno::Sequence<css::uno::Any> const &)
 {
     SfxGetpApp()->GetBasicManager();
-    return SfxGetpApp()->GetDialogContainer();
+    css::uno::XInterface* pRet = SfxGetpApp()->GetDialogContainer();
+    pRet->acquire();
+    return pRet;
 }
 
-
 // Service for application library container
-
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
 com_sun_star_comp_sfx2_ApplicationScriptLibraryContainer_get_implementation(
     css::uno::XComponentContext *,
     css::uno::Sequence<css::uno::Any> const &)
 {
     SfxGetpApp()->GetBasicManager();
-    return SfxGetpApp()->GetBasicContainer();
+    css::uno::XInterface* pRet = SfxGetpApp()->GetBasicContainer();
+    pRet->acquire();
+    return pRet;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */