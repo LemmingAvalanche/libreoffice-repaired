LemmingAvalanche/libreoffice-repaired

commit 350ff5d40cf68858b441d29b34cc633653681cee

java: reduce scope, make member classes private
found by UCDetector

Change-Id: Ief32d078090102b14b60b35fc36542f8d4fb252b

scripting/java/com/sun/star/script/framework/provider/beanshell/ScriptProviderForBeanShell.java
@@ -49,7 +49,7 @@ import com.sun.star.script.framework.container.ScriptMetaData;
 
 public class ScriptProviderForBeanShell
 {
-    public static class ScriptProviderForBeanShell_2 extends ScriptProvider
+    private static class ScriptProviderForBeanShell_2 extends ScriptProvider
     {
         public ScriptProviderForBeanShell_2(XComponentContext ctx)
         {

scripting/java/com/sun/star/script/framework/provider/java/ScriptProviderForJava.java
@@ -43,7 +43,7 @@ import com.sun.star.script.framework.log.LogUtils;
 
 public class ScriptProviderForJava
 {
-    public static class _ScriptProviderForJava extends ScriptProvider
+    private static class _ScriptProviderForJava extends ScriptProvider
     {
         private Resolver m_resolutionPolicy = new StrictResolver();
 

scripting/java/com/sun/star/script/framework/provider/javascript/ScriptProviderForJavaScript.java
@@ -50,7 +50,7 @@ import org.mozilla.javascript.JavaScriptException;
 
 public class ScriptProviderForJavaScript
 {
-    public static class ScriptProviderForJavaScript_2 extends ScriptProvider
+    private static class ScriptProviderForJavaScript_2 extends ScriptProvider
     {
         public ScriptProviderForJavaScript_2(XComponentContext ctx)
         {

commit 0922ae3cd3fed427b707c058be68b935c7d021e6

ScriptProvider implementations need to be accessible
...from com.sun.star.comp.loader.FactoryHelper.  Regression introduced with
350ff5d40cf68858b441d29b34cc633653681cee "java: reduce scope, make member
classes private."

Change-Id: Iabf41a5eca2df25408e90428c60736b4a73db4c3

scripting/java/com/sun/star/script/framework/provider/beanshell/ScriptProviderForBeanShell.java
@@ -49,7 +49,7 @@ import com.sun.star.script.framework.container.ScriptMetaData;
 
 public class ScriptProviderForBeanShell
 {
-    private static class ScriptProviderForBeanShell_2 extends ScriptProvider
+    public static class ScriptProviderForBeanShell_2 extends ScriptProvider
     {
         public ScriptProviderForBeanShell_2(XComponentContext ctx)
         {

scripting/java/com/sun/star/script/framework/provider/java/ScriptProviderForJava.java
@@ -43,7 +43,7 @@ import com.sun.star.script.framework.log.LogUtils;
 
 public class ScriptProviderForJava
 {
-    private static class _ScriptProviderForJava extends ScriptProvider
+    public static class _ScriptProviderForJava extends ScriptProvider
     {
         private Resolver m_resolutionPolicy = new StrictResolver();
 

scripting/java/com/sun/star/script/framework/provider/javascript/ScriptProviderForJavaScript.java
@@ -50,7 +50,7 @@ import org.mozilla.javascript.JavaScriptException;
 
 public class ScriptProviderForJavaScript
 {
-    private static class ScriptProviderForJavaScript_2 extends ScriptProvider
+    public static class ScriptProviderForJavaScript_2 extends ScriptProvider
     {
         public ScriptProviderForJavaScript_2(XComponentContext ctx)
         {