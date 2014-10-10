LemmingAvalanche/libreoffice-repaired

commit 47e267a1bb68cf17d0addc20562439fe6583a486

Use boost::optional for these members.
Change-Id: I60c52bf99d0e63e27f7c576e2582c26e19809c8c

chart2/source/view/axes/VAxisProperties.cxx
@@ -156,8 +156,6 @@ AxisProperties::AxisProperties( const uno::Reference< XAxis >& xAxisModel
     , m_eCrossoverType( ::com::sun::star::chart::ChartAxisPosition_ZERO )
     , m_eLabelPos( ::com::sun::star::chart::ChartAxisLabelPosition_NEAR_AXIS )
     , m_eTickmarkPos( ::com::sun::star::chart::ChartAxisMarkPosition_AT_LABELS_AND_AXIS )
-    , m_pfMainLinePositionAtOtherAxis(NULL)
-    , m_pfExrtaLinePositionAtOtherAxis(NULL)
     , m_bCrossingAxisHasReverseDirection(false)
     , m_bCrossingAxisIsCategoryAxes(false)
     , m_fLabelDirectionSign(1.0)
@@ -184,9 +182,6 @@ AxisProperties::AxisProperties( const AxisProperties& rAxisProperties )
     , m_bSwapXAndY( rAxisProperties.m_bSwapXAndY )
     , m_eCrossoverType( rAxisProperties.m_eCrossoverType )
     , m_eLabelPos( rAxisProperties.m_eLabelPos )
-    , m_eTickmarkPos( rAxisProperties.m_eTickmarkPos )
-    , m_pfMainLinePositionAtOtherAxis( NULL )
-    , m_pfExrtaLinePositionAtOtherAxis( NULL )
     , m_bCrossingAxisHasReverseDirection( rAxisProperties.m_bCrossingAxisHasReverseDirection )
     , m_bCrossingAxisIsCategoryAxes( rAxisProperties.m_bCrossingAxisIsCategoryAxes )
     , m_fLabelDirectionSign( rAxisProperties.m_fLabelDirectionSign )
@@ -205,15 +200,13 @@ AxisProperties::AxisProperties( const AxisProperties& rAxisProperties )
     , m_xAxisTextProvider( rAxisProperties.m_xAxisTextProvider )
 {
     if( rAxisProperties.m_pfMainLinePositionAtOtherAxis )
-        m_pfMainLinePositionAtOtherAxis = new double(*rAxisProperties.m_pfMainLinePositionAtOtherAxis);
+        m_pfMainLinePositionAtOtherAxis.reset(*rAxisProperties.m_pfMainLinePositionAtOtherAxis);
     if( rAxisProperties.m_pfExrtaLinePositionAtOtherAxis )
-        m_pfExrtaLinePositionAtOtherAxis = new double (*rAxisProperties.m_pfExrtaLinePositionAtOtherAxis);
+        m_pfExrtaLinePositionAtOtherAxis.reset(*rAxisProperties.m_pfExrtaLinePositionAtOtherAxis);
 }
 
 AxisProperties::~AxisProperties()
 {
-    delete m_pfMainLinePositionAtOtherAxis;
-    delete m_pfExrtaLinePositionAtOtherAxis;
 }
 
 LabelAlignment lcl_getLabelAlignmentForZAxis( const AxisProperties& rAxisProperties )
@@ -256,10 +249,10 @@ void AxisProperties::initAxisPositioning( const uno::Reference< beans::XProperty
 
                 if( m_bCrossingAxisIsCategoryAxes )
                     fValue = ::rtl::math::round(fValue);
-                m_pfMainLinePositionAtOtherAxis = new double(fValue);
+                m_pfMainLinePositionAtOtherAxis.reset(fValue);
             }
             else if( ::com::sun::star::chart::ChartAxisPosition_ZERO == m_eCrossoverType )
-                m_pfMainLinePositionAtOtherAxis = new double(0.0);
+                m_pfMainLinePositionAtOtherAxis.reset(0.0);
 
             xAxisProp->getPropertyValue("LabelPosition") >>= m_eLabelPos;
             xAxisProp->getPropertyValue("MarkPosition") >>= m_eTickmarkPos;

chart2/source/view/axes/VAxisBase.cxx
@@ -111,11 +111,9 @@ sal_Int32 VAxisBase::estimateMaximumAutoMainIncrementCount()
     return 10;
 }
 
-void VAxisBase::setExrtaLinePositionAtOtherAxis( const double& fCrossingAt )
+void VAxisBase::setExrtaLinePositionAtOtherAxis( double fCrossingAt )
 {
-    if( m_aAxisProperties.m_pfExrtaLinePositionAtOtherAxis )
-        delete m_aAxisProperties.m_pfExrtaLinePositionAtOtherAxis;
-    m_aAxisProperties.m_pfExrtaLinePositionAtOtherAxis = new double(fCrossingAt);
+    m_aAxisProperties.m_pfExrtaLinePositionAtOtherAxis.reset(fCrossingAt);
 }
 
 sal_Int32 VAxisBase::getDimensionCount() const

chart2/source/view/axes/VAxisBase.hxx
@@ -62,7 +62,7 @@ public:
     virtual sal_Int32 estimateMaximumAutoMainIncrementCount();
     virtual void createAllTickInfos( ::std::vector< ::std::vector< TickInfo > >& rAllTickInfos );
 
-    void setExrtaLinePositionAtOtherAxis( const double& fCrossingAt );
+    void setExrtaLinePositionAtOtherAxis( double fCrossingAt );
 
 protected: //methods
     sal_Int32 getIndexOfLongestLabel( const ::com::sun::star::uno::Sequence< OUString >& rLabels );

chart2/source/view/axes/VAxisProperties.hxx
@@ -37,6 +37,7 @@
 #include <com/sun/star/lang/Locale.hpp>
 
 #include <vector>
+#include <boost/optional.hpp>
 
 namespace chart
 {
@@ -95,8 +96,8 @@ struct AxisProperties
     ::com::sun::star::chart::ChartAxisLabelPosition m_eLabelPos;
     ::com::sun::star::chart::ChartAxisMarkPosition  m_eTickmarkPos;
 
-    double*     m_pfMainLinePositionAtOtherAxis;
-    double*     m_pfExrtaLinePositionAtOtherAxis;
+    boost::optional<double> m_pfMainLinePositionAtOtherAxis;
+    boost::optional<double> m_pfExrtaLinePositionAtOtherAxis;
 
     bool        m_bCrossingAxisHasReverseDirection;
     bool        m_bCrossingAxisIsCategoryAxes;

chart2/source/view/axes/VPolarRadiusAxis.cxx
@@ -152,7 +152,7 @@ void VPolarRadiusAxis::createShapes()
         }
 
         //xxxxx pTickInfo->updateUnscaledValue( xInverseScaling );
-        aAxisProperties.m_pfMainLinePositionAtOtherAxis = new double( pTickInfo->getUnscaledTickValue() );
+        aAxisProperties.m_pfMainLinePositionAtOtherAxis.reset(pTickInfo->getUnscaledTickValue());
         aAxisProperties.m_bDisplayLabels=false;
 
         VCartesianAxis aAxis(aAxisProperties,m_xNumberFormatsSupplier

commit cef290760f8f3f12a05b2671371aa1f73f521ca6

Fix CppunitTest_chart2_xshape
...regression from 47e267a1bb68cf17d0addc20562439fe6583a486 "Use boost::optional
for these members."

Change-Id: I8e02033d75fd52489be03b34fbce494ae49ab7e3

chart2/source/view/axes/VAxisProperties.cxx
@@ -182,6 +182,7 @@ AxisProperties::AxisProperties( const AxisProperties& rAxisProperties )
     , m_bSwapXAndY( rAxisProperties.m_bSwapXAndY )
     , m_eCrossoverType( rAxisProperties.m_eCrossoverType )
     , m_eLabelPos( rAxisProperties.m_eLabelPos )
+    , m_eTickmarkPos( rAxisProperties.m_eTickmarkPos )
     , m_bCrossingAxisHasReverseDirection( rAxisProperties.m_bCrossingAxisHasReverseDirection )
     , m_bCrossingAxisIsCategoryAxes( rAxisProperties.m_bCrossingAxisIsCategoryAxes )
     , m_fLabelDirectionSign( rAxisProperties.m_fLabelDirectionSign )