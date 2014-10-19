LemmingAvalanche/libreoffice-repaired

commit 4c073ca614897572a034134f196fafaf08d66e12

RTF import: fix handling of \loch \hich \dbch \ltrch \rtlch
The logic is not immediately obvious from the RTF spec; let's do what
the editengine RTF import does, but without the unnecessary complexity.

Change-Id: I60e69130e6e5aed1f5d237f64b1656c3141e402a

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2825,8 +2825,10 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         break;
     case RTF_LTRCH:
         // dmapper does not support this.
+        m_aStates.top().isRightToLeft = false;
         break;
     case RTF_RTLCH:
+        m_aStates.top().isRightToLeft = true;
         if (m_aDefaultState.nCurrentEncoding == RTL_TEXTENCODING_MS_1255)
             m_aStates.top().nCurrentEncoding = m_aDefaultState.nCurrentEncoding;
         break;
@@ -3493,11 +3495,20 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
     {
     case RTF_F:
     case RTF_AF:
-        if (nKeyword == RTF_F)
-            nSprm = NS_ooxml::LN_CT_Fonts_ascii;
+        if (m_aStates.top().isRightToLeft
+            || m_aStates.top().eRunType == RTFParserState::HICH)
+        {
+            nSprm = NS_ooxml::LN_CT_Fonts_cs;
+        }
+        else if (m_aStates.top().eRunType == RTFParserState::DBCH)
+        {
+            nSprm = NS_ooxml::LN_CT_Fonts_eastAsia;
+        }
         else
-            nSprm = (m_aStates.top().eRunType == RTFParserState::HICH
-                ? NS_ooxml::LN_CT_Fonts_eastAsia : NS_ooxml::LN_CT_Fonts_cs);
+        {
+            assert(m_aStates.top().eRunType == RTFParserState::LOCH);
+            nSprm = NS_ooxml::LN_CT_Fonts_ascii;
+        }
         if (m_aStates.top().nDestinationState == DESTINATION_FONTTABLE || m_aStates.top().nDestinationState == DESTINATION_FONTENTRY)
         {
             m_aFontIndexes.push_back(nParam);
@@ -3521,7 +3532,8 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
             m_nCurrentFontIndex = getFontIndex(nParam);
             RTFValue::Pointer_t pValue(new RTFValue(getFontName(m_nCurrentFontIndex)));
             lcl_putNestedAttribute(m_aStates.top().aCharacterSprms, NS_ooxml::LN_EG_RPrBase_rFonts, nSprm, pValue);
-            m_aStates.top().nCurrentEncoding = getEncoding(m_nCurrentFontIndex);
+            if (nKeyword == RTF_F)
+                m_aStates.top().nCurrentEncoding = getEncoding(m_nCurrentFontIndex);
         }
         break;
     case RTF_RED:
@@ -5741,6 +5753,7 @@ RTFParserState::RTFParserState(RTFDocumentImpl* pDocumentImpl)
       aDrawingObject(),
       aFrame(this),
       eRunType(LOCH),
+      isRightToLeft(false),
       nYear(0),
       nMonth(0),
       nDay(0),

writerfilter/source/rtftok/rtfdocumentimpl.hxx
@@ -257,6 +257,8 @@ public:
 
     /// CJK or CTL?
     enum { LOCH, HICH, DBCH } eRunType;
+    /// ltrch or rtlch
+    bool isRightToLeft;
 
     // Info group.
     int nYear;

commit b6eb3c651d23185977dcf89d4e9f0d860637dac1

fdo#83464 RTF import: \plain should reset \rtlch and \hich as well
Regression from 4c073ca614897572a034134f196fafaf08d66e12 (RTF import:
fix handling of \loch \hich \dbch \ltrch \rtlch, 2014-06-03), now that
\f no longer means the ascii font name, \plain should reset a few more
parser state members.

Change-Id: Ia6b0bdf25563dd5300e108b46731751f0886f67d

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2850,6 +2850,8 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         m_aStates.top().nCurrentEncoding = getEncoding(getFontIndex(m_nDefaultFontIndex));
         m_aStates.top().aCharacterAttributes = getDefaultState().aCharacterAttributes;
         m_aStates.top().nCurrentCharacterStyleIndex = -1;
+        m_aStates.top().isRightToLeft = false;
+        m_aStates.top().eRunType = RTFParserState::LOCH;
     }
     break;
     case RTF_PARD:

sw/qa/extras/rtfimport/data/fdo83464.rtf
@@ -1,0 +1,24 @@
+{\rtf1\adeflang1025\ansi\ansicpg1252\uc1\adeff31507\deff0\stshfdbch0\stshfloch0\stshfhich0\stshfbi0\deflang1033\deflangfe1033\themelang1033\themelangfe0\themelangcs1025
+{\fonttbl
+{\f0\fbidi \froman\fcharset0\fprq2 Times New Roman;}
+{\f39\fbidi \fswiss\fcharset0\fprq2 Verdana;}
+}
+{\stylesheet
+{\s43\ql \f39\fs16 Table Text;}
+}
+\pard\plain \ltrpar\ql \li0\ri0\sb60\sa60\widctlpar\tx360\jclisttab\tx720\tx1080\tx1440\tx1800\tx2160\tx2520\tx2880\tx3240\tx3600\tx3960\tx4320\tx4680\tx5040\tx5400\tx5760\wrapdefault\aspalpha\aspnum\faauto\adjustright\rin0\lin0\itap0\pararsid10449364 \rtlch\fcs1 \af0\afs18\alang1037 \ltrch\fcs0 \fs16\lang1024\langfe1024\loch\af2\hich\af2\dbch\af35\cgrid\noproof\langnp1033\langfenp1033 
+\par
+{\rtlch\fcs1 \ltrch\fcs0 \insrsid11419963 
+{\shp
+{\*\shpinst\shpleft-90\shptop720\shpright9405\shpbottom2940\shpfhdr0\shpbxcolumn\shpbxignore\shpbymargin\shpbyignore\shpwr3\shpwrk0\shpfblwtxt0\shpz2\shplid1027
+{\sp
+{\sn shapeType}
+{\sv 202}
+}
+{\shptxt \pard\plain\f39\fs16 Hello\par
+}
+}
+}
+}
+\par
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1974,6 +1974,15 @@ DECLARE_RTFIMPORT_TEST(testFdo82071, "fdo82071.rtf")
     CPPUNIT_ASSERT_EQUAL(awt::FontUnderline::NONE, getProperty<sal_Int16>(xRun, "CharUnderline"));
 }
 
+DECLARE_RTFIMPORT_TEST(testFdo83464, "fdo83464.rtf")
+{
+    // Problem was that the text in the textfrme had wrong font.
+    uno::Reference<text::XTextRange> xFrameText(getShape(1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Hello"), xFrameText->getString());
+    // This was Times New Roman.
+    CPPUNIT_ASSERT_EQUAL(OUString("Verdana"), getProperty<OUString>(getRun(getParagraphOfText(1, xFrameText->getText()), 1), "CharFontName"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */