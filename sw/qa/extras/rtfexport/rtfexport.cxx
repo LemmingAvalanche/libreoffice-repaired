LemmingAvalanche/libreoffice-repaired

commit 59610f1e53e47dd66fb301a174d70b757314a9b8

RtfAttributeOutput::FormatBox: export line{Color,Width} shape properties
Change-Id: Id1fd8b4c7ebcc8ffd3b6914596385eb3eda922c6

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -2948,6 +2948,22 @@ void RtfAttributeOutput::FormatBox( const SvxBoxItem& rBox )
         m_aFlyProperties.push_back(std::make_pair<OString, OString>("dyTextTop", OString::number(rBox.GetDistance(BOX_LINE_TOP) * 635)));
         m_aFlyProperties.push_back(std::make_pair<OString, OString>("dxTextRight", OString::number(rBox.GetDistance(BOX_LINE_RIGHT) * 635)));
         m_aFlyProperties.push_back(std::make_pair<OString, OString>("dyTextBottom", OString::number(rBox.GetDistance(BOX_LINE_BOTTOM) * 635)));
+
+        const SvxBorderLine* pLeft = rBox.GetLine(BOX_LINE_LEFT);
+        const SvxBorderLine* pRight = rBox.GetLine(BOX_LINE_RIGHT);
+        const SvxBorderLine* pTop = rBox.GetLine(BOX_LINE_TOP);
+        const SvxBorderLine* pBottom = rBox.GetLine(BOX_LINE_BOTTOM);
+        if (pLeft && pRight && pTop && pBottom && *pLeft == *pRight && *pLeft == *pTop && *pLeft == *pBottom)
+        {
+            const Color& rColor = pTop->GetColor();
+            // We in fact need RGB to BGR, but the transformation is symmetric.
+            m_aFlyProperties.push_back(std::make_pair<OString, OString>("lineColor", OString::number(msfilter::util::BGRToRGB(rColor.GetColor()))));
+
+            double const fConverted(editeng::ConvertBorderWidthToWord(pTop->GetBorderLineStyle(), pTop->GetWidth()));
+            sal_Int32 nWidth = sal_Int32(fConverted * 635); // Twips -> EMUs
+            m_aFlyProperties.push_back(std::make_pair<OString, OString>("lineWidth", OString::number(nWidth)));
+        }
+
         return;
     }
 

commit 2bba5b51b9c18207698b1873884bafb8dfffdd6b

fdo#82858 RTF export: fix "none" line style of TextFrames
Regression from commit 59610f1e53e47dd66fb301a174d70b757314a9b8
(RtfAttributeOutput::FormatBox: export line{Color,Width} shape
properties, 2013-04-15), in case lineColor is written, but there is no
border style, then an explicit fLine=0 property is needed, otherwise a
black border shows up.

Change-Id: I6d6eedbc8d3ee4bee0f2aadb51a376c7734c6640

sw/CppunitTest_sw_rtfexport.mk
@@ -76,6 +76,7 @@ $(eval $(call gb_CppunitTest_use_components,sw_rtfexport,\
 	uui/util/uui \
     writerfilter/util/writerfilter \
     xmloff/util/xo \
+	oox/util/oox \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sw_rtfexport))

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -3104,6 +3104,9 @@ void RtfAttributeOutput::FormatBox(const SvxBoxItem& rBox)
                 sal_Int32 nWidth = sal_Int32(fConverted * 635); // Twips -> EMUs
                 m_aFlyProperties.push_back(std::make_pair<OString, OString>("lineWidth", OString::number(nWidth)));
             }
+            else
+                // No border: no line.
+                m_aFlyProperties.push_back(std::make_pair<OString, OString>("fLine", "0"));
         }
 
         return;

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -719,6 +719,12 @@ DECLARE_RTFEXPORT_TEST(testFdo82860, "fdo82860.odt")
     CPPUNIT_ASSERT_EQUAL(OUString("hello"), getParagraphOfText(1, xText)->getString());
 }
 
+DECLARE_RTFEXPORT_TEST(testFdo82858, "fdo82858.docx")
+{
+    // This was table::BorderLineStyle::SOLID, exporter failed to write explicit no line when line color was written.
+    CPPUNIT_ASSERT_EQUAL(table::BorderLineStyle::NONE, getProperty<table::BorderLine2>(getShape(1), "TopBorder").LineStyle);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */