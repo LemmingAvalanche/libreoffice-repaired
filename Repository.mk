LemmingAvalanche/libreoffice-repaired

commit 7008341f17e4618e20bfa62b60504a5da1786220

scp2: move unx wrapper scripts to AutoInstall
Change-Id: I661656e486b088c28b78e5c3ed4fbe9945991097

Repository.mk
@@ -804,9 +804,35 @@ $(eval $(call gb_Helper_register_packages_for_install,ure,\
 	) \
 ))
 
+ifeq ($(GUIBASE),unx)
 $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	xmlsec \
 ))
+
+$(eval $(call gb_Helper_register_packages_for_install,base_brand,\
+	desktop_sbase_sh \
+))
+
+$(eval $(call gb_Helper_register_packages_for_install,calc_brand,\
+	desktop_scalc_sh \
+))
+
+$(eval $(call gb_Helper_register_packages_for_install,draw_brand,\
+	desktop_sdraw_sh \
+))
+
+$(eval $(call gb_Helper_register_packages_for_install,impress_brand,\
+	desktop_simpress_sh \
+))
+
+$(eval $(call gb_Helper_register_packages_for_install,math_brand,\
+	desktop_smath_sh \
+))
+
+$(eval $(call gb_Helper_register_packages_for_install,writer_brand,\
+	desktop_swriter_sh \
+))
+endif # GUIBASE=unx
 
 # External executables
 $(eval $(call gb_ExternalExecutable_register_executables,\

scp2/source/base/file_base.scp
@@ -18,15 +18,6 @@
 
 #include "macros.inc"
 
-#if !defined MACOSX && !defined WNT
-File gid_File_Exe_Sbase
-    BIN_FILE_BODY;
-    Dir = gid_Brand_Dir_Program;
-    Name = "sbase";
-    Styles = (PACKED);
-End
-#endif
-
 STD_RES_FILE( gid_File_Res_Abp, abp)
 
 STD_RES_FILE( gid_File_Res_Dbp, dbp)

scp2/source/base/module_base.scp
@@ -51,7 +51,7 @@ Module gid_Module_Brand_Prg_Base
     Minimal = YES;
     Default = YES;
     Styles = (HIDDEN_ROOT);
-    Files = (auto_base_brand_ALL,gid_File_Exe_Sbase);
+    Files = (auto_base_brand_ALL);
 End
 
 Module gid_Module_Langpack_Base_Template

scp2/source/calc/file_calc.scp
@@ -41,15 +41,6 @@ File gid_File_Help_Scalc_Zip
 End
 #endif
 
-#if !defined MACOSX && !defined WNT
-File gid_File_Exe_Scalc
-    BIN_FILE_BODY;
-    Dir = gid_Brand_Dir_Program;
-    Name = "scalc";
-    Styles = (PACKED);
-End
-#endif
-
 #if defined(WITH_LPSOLVER)
 
 STD_RES_FILE( gid_File_Res_Solver, solver)

scp2/source/calc/module_calc.scp
@@ -60,7 +60,7 @@ Module gid_Module_Brand_Prg_Calc
     Minimal = YES;
     Default = YES;
     Styles = (HIDDEN_ROOT);
-    Files = (auto_calc_brand_ALL,gid_File_Exe_Scalc);
+    Files = (auto_calc_brand_ALL);
 End
 
 Module gid_Module_Langpack_Calc_Template

scp2/source/draw/file_draw.scp
@@ -33,11 +33,3 @@ File gid_File_Help_Sdraw_Zip
 End
 #endif
 
-#if !defined MACOSX && !defined WNT
-File gid_File_Exe_Sdraw
-    BIN_FILE_BODY;
-    Dir = gid_Brand_Dir_Program;
-    Name = "sdraw";
-    Styles = (PACKED);
-End
-#endif

scp2/source/draw/module_draw.scp
@@ -50,7 +50,7 @@ Module gid_Module_Brand_Prg_Draw
     Minimal = YES;
     Default = YES;
     Styles = (HIDDEN_ROOT);
-    Files = (auto_draw_brand_ALL,gid_File_Exe_Sdraw);
+    Files = (auto_draw_brand_ALL);
 End
 
 Module gid_Module_Langpack_Draw_Template

scp2/source/impress/file_impress.scp
@@ -33,15 +33,6 @@ File gid_File_Help_Simpress_Zip
 End
 #endif
 
-#if !defined MACOSX && !defined WNT
-File gid_File_Exe_Simpress
-    BIN_FILE_BODY;
-    Dir = gid_Brand_Dir_Program;
-    Name = "simpress";
-    Styles = (PACKED);
-End
-#endif
-
 // configuration files for custom animation effects
 
 File gid_File_sd_xml

commit 005bcf5a7418fdc15c3dfcd8d4a4e89b80e7d976

Repository.mk: move conditional to the right place
regression from 7008341f17e4618e20bfa62b60504a5da1786220, thanks _david_

Change-Id: Ifce88cc5dd3dfc252a16407b4141ba2751564171

Repository.mk
@@ -804,11 +804,11 @@ $(eval $(call gb_Helper_register_packages_for_install,ure,\
 	) \
 ))
 
-ifeq ($(GUIBASE),unx)
 $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	xmlsec \
 ))
 
+ifeq ($(GUIBASE),unx)
 $(eval $(call gb_Helper_register_packages_for_install,base_brand,\
 	desktop_sbase_sh \
 ))