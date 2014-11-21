LibreOffice/core

make glxtest available in salmain
Change-Id: Ic8bc3f2d5d96506590d35138089ead2eac984314

vcl/Executable_ui-previewer.mk
@@ -18,6 +18,7 @@ $(eval $(call gb_Executable_use_api,ui-previewer,\
 
 $(eval $(call gb_Executable_use_static_libraries,ui-previewer,\
     vclmain \
+	glxtest \
 ))
 
 $(eval $(call gb_Executable_use_libraries,ui-previewer,\
@@ -33,5 +34,16 @@ $(eval $(call gb_Executable_use_libraries,ui-previewer,\
 $(eval $(call gb_Executable_add_exception_objects,ui-previewer,\
     vcl/source/uipreviewer/previewer \
 ))
+
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Library_add_libs,ui-previewer,\
+	-lm \
+	-ldl \
+	-lpthread \
+    -lGL \
+    -lGLU \
+    -lX11 \
+))
+endif
 
 # vim: set noet sw=4 ts=4:

vcl/Executable_vcldemo.mk
@@ -39,6 +39,18 @@ $(eval $(call gb_Executable_add_exception_objects,vcldemo,\
 
 $(eval $(call gb_Executable_use_static_libraries,vcldemo,\
     vclmain \
+	glxtest \
 ))
+
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Library_add_libs,vcldemo,\
+	-lm \
+	-ldl \
+	-lpthread \
+    -lGL \
+    -lGLU \
+    -lX11 \
+))
+endif
 
 # vim: set noet sw=4 ts=4:

LemmingAvalanche/libreoffice-repaired

vcl/Executable_ui-previewer.mk
@@ -34,4 +34,19 @@ $(eval $(call gb_Executable_add_exception_objects,ui-previewer,\
     vcl/source/uipreviewer/previewer \
 ))
 
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Executable_add_libs,ui-previewer,\
+	-lm \
+	-ldl \
+	-lpthread \
+    -lGL \
+    -lGLU \
+    -lX11 \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,ui-previewer,\
+	glxtest \
+))
+endif
+
 # vim: set noet sw=4 ts=4:

vcl/Executable_vcldemo.mk
@@ -41,4 +41,19 @@ $(eval $(call gb_Executable_use_static_libraries,vcldemo,\
     vclmain \
 ))
 
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Executable_add_libs,vcldemo,\
+	-lm \
+	-ldl \
+	-lpthread \
+    -lGL \
+    -lGLU \
+    -lX11 \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,vcldemo,\
+	glxtest \
+))
+endif
+
 # vim: set noet sw=4 ts=4: