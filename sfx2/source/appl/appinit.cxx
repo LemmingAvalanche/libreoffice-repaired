LemmingAvalanche/libreoffice-repaired

commit 2b608f976bf47ce34fea8bbe324200b040a625e8

osl_Module -> osl::Module
related coverity#1242504 Resource leak but it still leaks a dlopen
handle here of course

Change-Id: Iaf692e7b2753c1c0af580e236bc147dc30a39e9f

sfx2/source/appl/appinit.cxx
@@ -178,14 +178,14 @@ OUString GetSpecialCharsForEdit(vcl::Window* pParent, const vcl::Font& rFont)
         bDetermineFunction = true;
 
 #ifndef DISABLE_DYNLOADING
-        static OUString aLibName( SVLIBRARY( "cui" ) );
-        oslModule handleMod = osl_loadModuleRelative(
-            &thisModule, aLibName.pData, 0 );
+        osl::Module aMod;
+        aMod.loadRelative(&thisModule, "cui", 0);
 
         // get symbol
         OUString aSymbol( "GetSpecialCharsForEdit"  );
-        pfunc_getSpecialCharsForEdit = (PFunc_getSpecialCharsForEdit)osl_getFunctionSymbol( handleMod, aSymbol.pData );
+        pfunc_getSpecialCharsForEdit = (PFunc_getSpecialCharsForEdit)aMod.getFunctionSymbol(aSymbol);
         DBG_ASSERT( pfunc_getSpecialCharsForEdit, "GetSpecialCharsForEdit() not found!" );
+        aMod.release();
 #else
         pfunc_getSpecialCharsForEdit = GetSpecialCharsForEdit;
 #endif

commit 411c27ef7360051ed90c1460ac16759a4e73f663

Resolves: fdo#86799 fix insert special char in Find/Find and Replace
regression from

2b608f976bf47ce34fea8bbe324200b040a625e8
author	Caolán McNamara <caolanm@redhat.com>	2014-10-13 09:25:26 (GMT)
osl_Module -> osl::Module

Change-Id: Idb8fec23bdd0ee3ea0488047aadaf21e3cd2900f

sfx2/source/appl/appinit.cxx
@@ -181,7 +181,7 @@ OUString GetSpecialCharsForEdit(vcl::Window* pParent, const vcl::Font& rFont)
 
 #ifndef DISABLE_DYNLOADING
         osl::Module aMod;
-        aMod.loadRelative(&thisModule, "cui", 0);
+        aMod.loadRelative(&thisModule, SVLIBRARY("cui"), 0);
 
         // get symbol
         OUString aSymbol( "GetSpecialCharsForEdit"  );