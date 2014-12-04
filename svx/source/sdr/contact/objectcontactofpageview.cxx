LemmingAvalanche/libreoffice-repaired

commit f95ff384fd64afa7c3c3a2f57c80d79bc1b2fafd

Don't use this quick hack on Android.
It *seems* to cause missing areas (at least in Impress).

(i.e. the bottom half or right side of headers on slides can sometimes
 go missing, not using this hack fixes the problem for me).

Change-Id: I205531666e0ad12470020e5ff2568ebf16bc2f01

svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -243,7 +243,7 @@ namespace sdr
             updateViewInformation2D(aNewViewInformation2D);
 
             drawinglayer::primitive2d::Primitive2DSequence xPrimitiveSequence;
-#if HAVE_FEATURE_DESKTOP
+#if defined( HAVE_FEATURE_DESKTOP ) || defined( ANDROID )
             // get whole Primitive2DSequence; this will already make use of updated ViewInformation2D
             // and may use the MapMode from the Target OutDev in the DisplayInfo
             xPrimitiveSequence = rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo);

commit fd94e67759d4d7451a45a0ce71b8c2fdfb5ded59

HAVE_FEATURE_DESKTOP is always defined, it is 0 or 1
One should not use #ifdef (or #if defined) on the macros from
<config_features.h>.

Fix mistake introduced in f95ff384fd64afa7c3c3a2f57c80d79bc1b2fafd.

svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -243,7 +243,7 @@ namespace sdr
             updateViewInformation2D(aNewViewInformation2D);
 
             drawinglayer::primitive2d::Primitive2DSequence xPrimitiveSequence;
-#if defined( HAVE_FEATURE_DESKTOP ) || defined( ANDROID )
+#if HAVE_FEATURE_DESKTOP || defined( ANDROID )
             // get whole Primitive2DSequence; this will already make use of updated ViewInformation2D
             // and may use the MapMode from the Target OutDev in the DisplayInfo
             xPrimitiveSequence = rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo);