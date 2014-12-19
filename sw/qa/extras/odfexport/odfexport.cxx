LemmingAvalanche/libreoffice-repaired

commit ccf185008ea7525e2714212e1379c96013478acd

fdo#85554 SwXShape: fix getting ZOrder property when doc contains TextBoxes
Change-Id: I9b6b83f0f6d627bb14a880a19769ee70564cf52b

sw/source/core/unocore/unodraw.cxx
@@ -1756,6 +1756,19 @@ uno::Any SwXShape::getPropertyValue(const OUString& rPropertyName)
                 aRet >>= aPath;
                 aRet <<= _ConvertPolyPolygonBezierToLayoutDir( aPath );
             }
+            else if (rPropertyName == "ZOrder")
+            {
+                // Convert the real draw page position to the logical one that ignores textboxes.
+                if (pFmt)
+                {
+                    const SdrObject* pObj = pFmt->FindRealSdrObject();
+                    if (pObj)
+                    {
+                        std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pFmt->GetDoc());
+                        aRet <<= SwTextBoxHelper::getOrdNum(pObj, aTextBoxes);
+                    }
+                }
+            }
         }
     }
     return aRet;

sw/inc/textboxhelper.hxx
@@ -22,6 +22,7 @@
 #include <swdllapi.h>
 
 class SdrPage;
+class SdrObject;
 class SfxItemSet;
 class SwFrmFmt;
 class SwFrmFmts;
@@ -82,6 +83,8 @@ public:
     static sal_Int32 getCount(SdrPage* pPage, std::set<const SwFrmFmt*>& rTextBoxes);
     /// Get a shape by index, excluding TextBoxes.
     static css::uno::Any getByIndex(SdrPage* pPage, sal_Int32 nIndex, std::set<const SwFrmFmt*>& rTextBoxes) throw(css::lang::IndexOutOfBoundsException);
+    /// Get the order of the shape, excluding TextBoxes.
+    static sal_Int32 getOrdNum(const SdrObject* pObject, std::set<const SwFrmFmt*>& rTextBoxes);
 
     /// Saves the current shape -> textbox links in a map, so they can be restored later.
     static void saveLinks(const SwFrmFmts& rFormats, std::map<const SwFrmFmt*, const SwFrmFmt*>& rLinks);

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -56,6 +56,7 @@ public:
     void testChineseConversionNonChineseText();
     void testChineseConversionTraditionalToSimplified();
     void testChineseConversionSimplifiedToTraditional();
+    void testFdo85554();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -78,6 +79,7 @@ public:
     CPPUNIT_TEST(testChineseConversionNonChineseText);
     CPPUNIT_TEST(testChineseConversionTraditionalToSimplified);
     CPPUNIT_TEST(testChineseConversionSimplifiedToTraditional);
+    CPPUNIT_TEST(testFdo85554);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -559,6 +561,29 @@ void SwUiWriterTest::testChineseConversionSimplifiedToTraditional()
     SwTxtNode* pTxtNode = aPaM.GetNode().GetTxtNode();
     CPPUNIT_ASSERT_EQUAL(CHINESE_TRADITIONAL_CONTENT, pTxtNode->GetTxt());
 
+}
+
+void SwUiWriterTest::testFdo85554()
+{
+    // Load the document, it contains one shape with a textbox.
+    load("/sw/qa/extras/uiwriter/data/", "fdo85554.odt");
+
+    // Add a second shape to the document.
+    uno::Reference<css::lang::XMultiServiceFactory> xFactory(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XShape> xShape(xFactory->createInstance("com.sun.star.drawing.RectangleShape"), uno::UNO_QUERY);
+    xShape->setSize(awt::Size(10000, 10000));
+    xShape->setPosition(awt::Point(1000, 1000));
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    xDrawPage->add(xShape);
+
+    // Save it and load it back.
+    reload("writer8", "fdo85554.odt");
+
+    xDrawPageSupplier.set(mxComponent, uno::UNO_QUERY);
+    xDrawPage = xDrawPageSupplier->getDrawPage();
+    // This was 1, we lost a shape on export.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2), xDrawPage->getCount());
 }
 
 

sw/source/core/doc/textboxhelper.cxx
@@ -215,6 +215,25 @@ uno::Any SwTextBoxHelper::getByIndex(SdrPage* pPage, sal_Int32 nIndex, std::set<
     return pRet ? uno::makeAny(uno::Reference<drawing::XShape>(pRet->getUnoShape(), uno::UNO_QUERY)) : uno::Any();
 }
 
+sal_Int32 SwTextBoxHelper::getOrdNum(const SdrObject* pObject, std::set<const SwFrmFmt*>& rTextBoxes)
+{
+    if (const SdrPage* pPage = pObject->GetPage())
+    {
+        sal_Int32 nOrder = 0; // Current logical order.
+        for (size_t i = 0; i < pPage->GetObjCount(); ++i)
+        {
+            if (lcl_isTextBox(pPage->GetObj(i), rTextBoxes))
+                continue;
+            if (pPage->GetObj(i) == pObject)
+                return nOrder;
+            ++nOrder;
+        }
+    }
+
+    SAL_WARN("sw.core", "SwTextBoxHelper::getOrdNum: no page or page doesn't contain the object");
+    return pObject->GetOrdNum();
+}
+
 SwFrmFmt* SwTextBoxHelper::findTextBox(uno::Reference<drawing::XShape> xShape)
 {
     SwXShape* pShape = dynamic_cast<SwXShape*>(xShape.get());

commit b6f9775ff58a92f40489d8e376ef379777568cfc

fdo#86963 SwXShape: don't ask SwTextBoxHelper for ZOrder of child shapes
Regression from commit ccf185008ea7525e2714212e1379c96013478acd
(fdo#85554 SwXShape: fix getting ZOrder property when doc contains
TextBoxes, 2014-11-20), the problem was that we returned wrong ZOrder of
shapes inside group shapes.

In SwXShape::getPropertyValue(), pObj points to the Writer-interfacing
outermost group shape in case of shapes contained by group shapes, while
GetSvxShape() gives access to the real shape. Given that TextBoxes are
only possible at the highest level (and not inside group shapes), just
check if the two pointers are the same: when not, then no need to
convert anything.

With this, child shapes get back their original ZOrder -- before in case
the group shape had ZOrder=0, all its child shapes had ZOrder=0 as well.

Change-Id: I9c4097154130cd04f6ab2f2082abafc1d4333872

sw/source/core/unocore/unodraw.cxx
@@ -1763,8 +1763,17 @@ uno::Any SwXShape::getPropertyValue(const OUString& rPropertyName)
                     const SdrObject* pObj = pFmt->FindRealSdrObject();
                     if (pObj)
                     {
-                        std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pFmt->GetDoc());
-                        aRet <<= SwTextBoxHelper::getOrdNum(pObj, aTextBoxes);
+                        bool bConvert = true;
+                        if (SvxShape* pSvxShape = GetSvxShape())
+                            // In case of group shapes, pSvxShape points to the child shape, while pObj points to the outermost group shape.
+                            if (pSvxShape->GetSdrObject() != pObj)
+                                // Textboxes are not expected inside group shapes, so no conversion is necessary there.
+                                bConvert = false;
+                        if (bConvert)
+                        {
+                            std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pFmt->GetDoc());
+                            aRet <<= SwTextBoxHelper::getOrdNum(pObj, aTextBoxes);
+                        }
                     }
                 }
             }

sw/qa/extras/odfexport/odfexport.cxx
@@ -402,6 +402,14 @@ DECLARE_ODFEXPORT_TEST(testTextboxRoundedCorners, "textbox-rounded-corners.odt")
     CPPUNIT_ASSERT_EQUAL(OUString("a"), xCell->getString());
 }
 
+DECLARE_ODFEXPORT_TEST(testFdo86963, "fdo86963.odt")
+{
+    // Export of this document failed with beans::UnknownPropertyException.
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xDrawPage->getCount());
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();