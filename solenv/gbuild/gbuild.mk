LemmingAvalanche/libreoffice-repaired

commit 7b322d8d69dcff943c3a4313980958ecee86424c

drop crashrep unused since start of LibreOffice
Change-Id: I3df1216054c133314b2317849744a0a37e9fbc8f
Reviewed-on: https://gerrit.libreoffice.org/13733
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

configure.ac
@@ -4527,22 +4527,6 @@ dnl Check for syslog header
 dnl ===================================================================
 AC_CHECK_HEADER(syslog.h, AC_DEFINE(HAVE_SYSLOG_H))
 
-dnl ===================================================================
-dnl Set the ENABLE_CRASHDUMP variable.
-dnl ===================================================================
-AC_MSG_CHECKING([whether to enable crashdump feature])
-if test "$enable_crashdump" = "yes"; then
-    ENABLE_CRASHDUMP="TRUE"
-    BUILD_TYPE="$BUILD_TYPE CRASHREP"
-    AC_MSG_RESULT([yes])
-    AC_DEFINE(HAVE_FEATURE_CRASHDUMP)
-else
-    ENABLE_CRASHDUMP=""
-    AC_MSG_RESULT([no])
-fi
-AC_SUBST(ENABLE_CRASHDUMP)
-
-
 dnl Set the ENABLE_WERROR variable. (Activate --enable-werror)
 dnl ===================================================================
 AC_MSG_CHECKING([whether to turn warnings to errors])

Repository.mk
@@ -159,13 +159,6 @@ $(eval $(call gb_Helper_register_executables_for_install,OOO,quickstart, \
 ))
 endif
 
-ifneq ($(ENABLE_CRASHDUMP),)
-$(eval $(call gb_Helper_register_executables_for_install,OOO,crashrep, \
-	crashrep \
-	$(if $(filter WNT,$(OS)),crashrep_com) \
-))
-endif
-
 $(eval $(call gb_Helper_register_executables_for_install,OOO,python, \
 	$(if $(filter WNT,$(OS)), \
 		python \

RepositoryFixes.mk
@@ -27,7 +27,6 @@ gb_Executable_FILENAMES := $(patsubst uno:uno,uno:uno.bin,$(gb_Executable_FILENA
 endif
 gb_Executable_FILENAMES := $(patsubst unopkg_bin:unopkg_bin%,unopkg_bin:unopkg.bin,$(gb_Executable_FILENAMES))
 gb_Executable_FILENAMES := $(patsubst unopkg_com:unopkg_com%,unopkg_com:unopkg.com,$(gb_Executable_FILENAMES))
-gb_Executable_FILENAMES := $(patsubst crashrep_com:crashrep_com%,crashrep_com:crashrep.com,$(gb_Executable_FILENAMES))
 gb_Executable_FILENAMES := $(patsubst gengal:gengal,gengal:gengal.bin,$(gb_Executable_FILENAMES))
 
 ifeq ($(OS),MACOSX)

RepositoryModule_host.mk
@@ -46,7 +46,6 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	cppu \
 	cppuhelper \
 	cpputools \
-	$(call gb_Helper_optional,CRASHREP,crashrep) \
 	cui \
 	$(call gb_Helper_optional,DBCONNECTIVITY,dbaccess) \
 	desktop \

bin/find-german-comments
@@ -253,7 +253,6 @@ class Parser:
             "cppu" : 1,
             "cppuhelper" : 1,
             "cpputools" : 1,
-            "crashrep" : 1,
             "cui" : 1,
             "dbaccess" : 1,
             "desktop" : 1,

config_host/config_features.h.in
@@ -146,7 +146,4 @@
  */
 #define HAVE_FEATURE_COLLADA 0
 
-/* see --enable-crashdump: */
-#define HAVE_FEATURE_CRASHDUMP 0
-
 #endif

cppuhelper/source/bootstrap.cxx
@@ -152,7 +152,6 @@ Reference< XComponentContext > SAL_CALL bootstrap()
             OUString("--nologo"),
             OUString("--nodefault"),
             OUString("--norestore"),
-            OUString("--nocrashreport"),
             OUString("--nolockcheck"),
             OUString("--accept=pipe,name=" + sPipeName + ";urp;")
         };

crashrep/CustomTarget_crashrep_res.mk
@@ -1,41 +1,0 @@
-# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
-#
-# This file is part of the LibreOffice project.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-
-$(eval $(call gb_CustomTarget_CustomTarget,crashrep/source/win32))
-
-$(eval $(call gb_CustomTarget_register_targets,crashrep/source/win32, \
-	crashrep.ulf \
-	crashrep_impl.rc \
-))
-
-$(call gb_CustomTarget_get_workdir,crashrep/source/win32)/crashrep_impl.rc : \
-	$(call gb_CustomTarget_get_workdir,crashrep/source/win32)/crashrep.ulf \
-	$(SRCDIR)/crashrep/source/win32/rcfooter.txt \
-	$(SRCDIR)/crashrep/source/win32/rcheader.txt \
-	$(SRCDIR)/crashrep/source/win32/rctemplate.txt \
-	$(call gb_Executable_get_runtime_dependencies,lngconvex)
-
-$(call gb_CustomTarget_get_workdir,crashrep/source/win32)/crashrep_impl.rc :
-	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),LCX,1)
-	$(call gb_Helper_abbreviate_dirs,\
-		cd $(SRCDIR)/crashrep/source/win32 && \
-		$(call gb_Executable_get_command,lngconvex) \
-			-ulf $< \
-			-rc $@ \
-			-rct rctemplate.txt \
-			-rch rcheader.txt \
-			-rcf rcfooter.txt)
-
-$(eval $(call gb_CustomTarget_ulfex_rule,\
-	$(call gb_CustomTarget_get_workdir,crashrep/source/win32)/crashrep.ulf,\
-	$(SRCDIR)/crashrep/source/win32/crashrep.ulf,\
-	$(foreach lang,$(gb_TRANS_LANGS),\
-		$(gb_POLOCATION)/$(lang)/crashrep/source/win32.po)))
-
-# vim: set shiftwidth=4 tabstop=4 noexpandtab:

commit e386e27034287af30843b39f20467a3ee81367a3

gbuild: try to fix windows non-debug builds
The ENABLE_CRASHREP is no longer AC_SUBSTed by configure, so it is
always non-empty in config_host.mk, resulting in -Zi without -FS,
which breaks MSVC 2013 builds.

Leave a placeholder in configure for the new crash reporter vaporware,
and force the CFLAGS to be the same as in the --eanble-symbols case.

(regression from 7b322d8d69dcff943c3a4313980958ecee86424c)

Change-Id: I476fb2e4f50de5304632628b25e3997ea6c31c27

configure.ac
@@ -4527,6 +4527,10 @@ dnl Check for syslog header
 dnl ===================================================================
 AC_CHECK_HEADER(syslog.h, AC_DEFINE(HAVE_SYSLOG_H))
 
+# placeholder for future crash reporter feature
+ENABLE_CRASHDUMP=""
+AC_SUBST(ENABLE_CRASHDUMP)
+
 dnl Set the ENABLE_WERROR variable. (Activate --enable-werror)
 dnl ===================================================================
 AC_MSG_CHECKING([whether to turn warnings to errors])

solenv/gbuild/gbuild.mk
@@ -133,10 +133,11 @@ ifeq ($(HARDLINKDELIVER),TRUE)
 gb_Deliver_HARDLINK := $(true)
 endif
 
+# note: ENABLE_CRASHDUMP turns on gb_SYMBOL
 ifeq ($(or $(ENABLE_SYMBOLS),$(enable_symbols)),FALSE)
 gb_SYMBOL := $(false)
 else
-ifneq ($(strip $(ENABLE_SYMBOLS)$(enable_symbols)),)
+ifneq ($(strip $(ENABLE_SYMBOLS)$(enable_symbols)$(ENABLE_CRASHDUMP)),)
 gb_SYMBOL := $(true)
 endif
 endif

solenv/gbuild/platform/com_MSC_defs.mk
@@ -265,11 +265,6 @@ gb_DEBUG_CFLAGS+=-FS
 endif
 
 # this does not use CFLAGS so it is not overridable
-ifneq ($(ENABLE_CRASHDUMP),)
-gb_CFLAGS+=-Zi
-gb_CXXFLAGS+=-Zi
-endif
-
 ifeq ($(gb_SYMBOL),$(true))
 gb_CFLAGS+=$(gb_DEBUG_CFLAGS)
 gb_CXXFLAGS+=$(gb_DEBUG_CFLAGS)