LemmingAvalanche/libreoffice-repaired

commit d20553513fde9ea0a9d26308b0d2e27721e39dff

java: when rethrowing, store the original exception
Change-Id: I34ce000c48d2d79bfec854c8dd55d12f2bee29c7

reportbuilder/java/org/libreoffice/report/pentaho/SOReportJobFactory.java
@@ -216,12 +216,12 @@ public class SOReportJobFactory
             catch (java.lang.Exception e)
             {
                 LOGGER.error("ReportProcessing failed", e);
-                throw new com.sun.star.lang.WrappedTargetException(e.getMessage(), this, null);
+                throw new com.sun.star.lang.WrappedTargetException(e.getMessage(), this, e);
             }
             catch (java.lang.IncompatibleClassChangeError e2)
             {
                 LOGGER.error("Detected an IncompatibleClassChangeError");
-                throw new com.sun.star.lang.WrappedTargetException("caught a " + e2.getClass().getName(), this, new com.sun.star.uno.Exception(e2.getLocalizedMessage()));
+                throw new com.sun.star.lang.WrappedTargetException("caught a " + e2.getClass().getName(), this, e2);
             }
             Thread.currentThread().setContextClassLoader(cl);
 

bean/com/sun/star/comp/beans/LocalOfficeConnection.java
@@ -186,10 +186,12 @@ public class LocalOfficeConnection
                 mProtocol = aURL.getProtocol();
                 mInitialObject = aURL.getRootOid();
             }
-            catch ( com.sun.star.lang.IllegalArgumentException eIll )
+            catch ( com.sun.star.lang.IllegalArgumentException ex1 )
             {
-                throw new java.net.MalformedURLException(
+                java.net.MalformedURLException ex2 = new java.net.MalformedURLException(
                     "Invalid UNO connection URL.");
+                ex2.initCause(ex1);
+                throw ex2;
             }
         }
         mURL    = url;
@@ -425,7 +427,7 @@ public class LocalOfficeConnection
                     xLocalServiceManager.createInstanceWithContext(
                         "com.sun.star.bridge.BridgeFactory", xLocalContext));
         } catch (com.sun.star.uno.Exception e) {
-            throw new com.sun.star.uno.RuntimeException(e.getMessage());
+            throw new com.sun.star.uno.RuntimeException(e);
         }
         synchronized(this) {
             if(mBridge == null) {
@@ -434,7 +436,7 @@ public class LocalOfficeConnection
                     connector = xLocalServiceManager.createInstanceWithContext(
                             "com.sun.star.connection.Connector", xLocalContext);
                 } catch (com.sun.star.uno.Exception e) {
-                    throw new com.sun.star.uno.RuntimeException(e.getMessage());
+                    throw new com.sun.star.uno.RuntimeException(e);
                 }
                 XConnector connector_xConnector = UnoRuntime.queryInterface(XConnector.class, connector);
                 // connect to the server
@@ -447,7 +449,7 @@ public class LocalOfficeConnection
                 try {
                     mBridge = xBridgeFactory.createBridge(sBridgeName, protDcp, xConnection, null);
                 } catch (com.sun.star.bridge.BridgeExistsException e) {
-                    throw new com.sun.star.uno.RuntimeException(e.getMessage());
+                    throw new com.sun.star.uno.RuntimeException(e);
                 }
             }
             rootObject = mBridge.getInstance(rootOid);

bean/com/sun/star/comp/beans/NoConnectionException.java
@@ -26,6 +26,11 @@ package com.sun.star.comp.beans;
  */
 public class NoConnectionException extends Exception
 {
+
+    public NoConnectionException() {}
+
+    public NoConnectionException(Throwable cause) { super(cause); }
+
 }
 
 

bean/com/sun/star/comp/beans/OOoBean.java
@@ -157,26 +157,27 @@ public class OOoBean
 
         @internal
      */
-    private synchronized void setOOoConnection( OfficeConnection iNewConnection )
-        throws  HasConnectionException, NoConnectionException
-    {
+    private synchronized void setOOoConnection(OfficeConnection iNewConnection)
+            throws HasConnectionException, NoConnectionException {
         // the connection cannot be exchanged
-        if ( iConnection != null )
+        if (iConnection != null)
             throw new HasConnectionException();
 
         // is there a real connection, not just the proxy?
         com.sun.star.uno.XComponentContext xComponentContext = null;
-        try { xComponentContext = iNewConnection.getComponentContext(); }
-        catch ( java.lang.Throwable aExc )
-        { throw new NoConnectionException(); }
-        if ( xComponentContext == null )
+        try {
+            xComponentContext = iNewConnection.getComponentContext();
+        } catch (java.lang.Throwable aExc) {
+            throw new NoConnectionException(aExc);
+        }
+        if (xComponentContext == null)
             throw new NoConnectionException();
 
         // set the connection
         iConnection = iNewConnection;
 
         // get notified when connection dies
-        if ( xConnectionListener != null )
+        if (xConnectionListener != null)
             xConnectionListener.end();
         xConnectionListener = this.new EventListener("setOOoConnection");
     }
@@ -300,7 +301,7 @@ public class OOoBean
             aConnectorThread.start();
             try { aConnectorThread.join(nOOoStartTimeOut); }
             catch ( InterruptedException aExc )
-            { throw new NoConnectionException(); }
+            { throw new NoConnectionException(aExc); }
             if ( xServiceFactory == null )
                 throw new NoConnectionException();
         }
@@ -493,13 +494,15 @@ public class OOoBean
 
             // @requirement FUNC.CON.LOST/0.2
             NoConnectionException
-    {
-        if ( iConnection == null )
+ {
+        if (iConnection == null)
             throw new NoConnectionException();
 
-        try { xFrameWindow.getAWTComponent().setVisible(false); }
-        catch ( com.sun.star.lang.DisposedException aExc )
-        { throw new NoConnectionException(); }
+        try {
+            xFrameWindow.getAWTComponent().setVisible(false);
+        } catch (com.sun.star.lang.DisposedException aExc) {
+            throw new NoConnectionException(aExc);
+        }
     }
 
        // @requirement FUNC.BEAN.LOAD/0.4
@@ -675,8 +678,9 @@ public class OOoBean
                 catch ( com.sun.star.uno.Exception aExc )
                 {
                     // TDB: handling failure in createInstance
-                    aExc.printStackTrace();
-                    throw new java.io.IOException();
+                    java.io.IOException ex2 = new java.io.IOException();
+                    ex2.initCause(aExc);
+                    throw ex2;
                 }
 
                 aCallWatchThread.cancel();
@@ -692,7 +696,7 @@ public class OOoBean
         }
         catch ( InterruptedException aExc )
         {
-            throw new NoConnectionException();
+            throw new NoConnectionException(aExc);
         }
     }
 
@@ -808,27 +812,30 @@ public class OOoBean
             java.io.IOException,
             com.sun.star.lang.IllegalArgumentException,
             NoDocumentException
-    {
+ {
         // no document available?
-        if ( aDocument == null )
+        if (aDocument == null)
             throw new NoDocumentException();
 
-        try
-        {
+        try {
             // start runtime timeout
-            CallWatchThread aCallWatchThread =
-                new CallWatchThread( nOOoCallTimeOut, "storeToURL" );
+            CallWatchThread aCallWatchThread = new CallWatchThread(
+                    nOOoCallTimeOut, "storeToURL");
 
             // store the document
-            try { aDocument.storeToURL( aURL, aArguments ); }
-            catch ( com.sun.star.io.IOException aExc )
-            { throw new java.io.IOException(); }
+            try {
+                aDocument.storeToURL(aURL, aArguments);
+            } catch (com.sun.star.io.IOException aExc) {
+                java.io.IOException ex2 = new java.io.IOException();
+                ex2.initCause(aExc);
+                throw ex2;
+            }
 
             // end runtime timeout
             aCallWatchThread.cancel();
+        } catch (java.lang.InterruptedException aExc) {
+            throw new NoConnectionException(aExc);
         }
-        catch ( java.lang.InterruptedException aExc )
-        { throw new NoConnectionException(); }
     }
 
     /** Stores a document to a stream.
@@ -848,26 +855,28 @@ public class OOoBean
 
     {
         // wrap Java stream into UNO stream
-        com.sun.star.lib.uno.adapter.OutputStreamToXOutputStreamAdapter aStream =
-                new com.sun.star.lib.uno.adapter.OutputStreamToXOutputStreamAdapter(
-                    aOutStream );
+        com.sun.star.lib.uno.adapter.OutputStreamToXOutputStreamAdapter aStream = new com.sun.star.lib.uno.adapter.OutputStreamToXOutputStreamAdapter(
+                aOutStream);
 
         // add stream to arguments
-        com.sun.star.beans.PropertyValue[] aExtendedArguments =
-            addArgument( aArguments, new com.sun.star.beans.PropertyValue(
-                "OutputStream", -1, aStream, com.sun.star.beans.PropertyState.DIRECT_VALUE ) );
+        com.sun.star.beans.PropertyValue[] aExtendedArguments = addArgument(
+                aArguments, new com.sun.star.beans.PropertyValue(
+                        "OutputStream", -1, aStream,
+                        com.sun.star.beans.PropertyState.DIRECT_VALUE));
 
         // call normal store method
-        storeToURL( "private:stream", aExtendedArguments );
+        storeToURL("private:stream", aExtendedArguments);
 
         // get byte array from document stream
-        try { aStream.closeOutput(); }
-        catch ( com.sun.star.io.NotConnectedException aExc )
-        { /* TDB */ }
-        catch ( com.sun.star.io.BufferSizeExceededException aExc )
-        { /* TDB */ }
-        catch ( com.sun.star.io.IOException aExc )
-        { throw new java.io.IOException(); }
+        try {
+            aStream.closeOutput();
+        } catch (com.sun.star.io.NotConnectedException aExc) { /* TDB */
+        } catch (com.sun.star.io.BufferSizeExceededException aExc) { /* TDB */
+        } catch (com.sun.star.io.IOException aExc) {
+            java.io.IOException ex2 = new java.io.IOException();
+            ex2.initCause(aExc);
+            throw ex2;
+        }
         return aOutStream;
     }
 
@@ -1081,11 +1090,11 @@ xLayoutManager.showElement("private:resource/menubar/menubar");
                 }
                 catch (  com.sun.star.beans.UnknownPropertyException aExc )
                 {
-                    throw new RuntimeException( "not layout manager found" );
+                    throw new RuntimeException( "not layout manager found", aExc );
                 }
                 catch (  com.sun.star.lang.WrappedTargetException aExc )
                 {
-                    throw new RuntimeException( "not layout manager found" );
+                    throw new RuntimeException( "not layout manager found", aExc );
                 }
 
                 // notify change

connectivity/com/sun/star/sdbcx/comp/hsqldb/NativeStorageAccess.java
@@ -41,8 +41,10 @@ public class NativeStorageAccess {
                 mode = NativeStorageAccess.READWRITE | NativeStorageAccess.SEEKABLE;
 
             openStream(name, (String)key, mode);
-        } catch(Exception e){
-            throw new java.io.IOException();
+        } catch(Exception ex1){
+            java.io.IOException ex2 = new java.io.IOException();
+            ex2.initCause(ex1);
+            throw ex2;
         }
     }
     private native void openStream(String name,String key, int mode);

connectivity/com/sun/star/sdbcx/comp/hsqldb/StorageAccess.java
@@ -32,8 +32,10 @@ public class StorageAccess implements org.hsqldb.lib.Storage {
             access = new NativeStorageAccess(name,
                     this.readonly ? "r" : "rw"
                     ,key);
-        } catch(Exception e){
-            throw new java.io.IOException();
+        } catch(Exception ex1){
+            java.io.IOException ex2 = new java.io.IOException();
+            ex2.initCause(ex1);
+            throw ex2;
         }
     }
     public void close() throws java.io.IOException{

connectivity/qa/complex/connectivity/HsqlDriverTest.java
@@ -59,7 +59,7 @@ public class HsqlDriverTest extends ComplexTestCase {
             HsqlDatabase database = new HsqlDatabase( param.getMSF() );
             ds = database.getDataSource().getXDataSource();
         } catch(Exception ex) {
-            throw new RuntimeException("factory: unable to construct data source" );
+            throw new RuntimeException("factory: unable to construct data source", ex );
         }
 
         try{

connectivity/qa/connectivity/tools/RowSet.java
@@ -58,8 +58,7 @@ public class RowSet implements XRowSet, XRow
         }
         catch ( Exception e )
         {
-            e.printStackTrace(System.err);
-            throw new java.lang.InstantiationError();
+            throw new java.lang.RuntimeException(e);
         }
     }
 

commit 9e4811266840cab0d100c6b897ae8342a4444af2

Related fdo#85190: Don't dress arbitrary Java Throwables as UNO Any values
Regression introduced with d20553513fde9ea0a9d26308b0d2e27721e39dff "java: when
rethrowing, store the original exception," but these two appear to be the only
two cases related to com.sun.star.lang.WrappedTarget[Runtime]Exception in that
patch that would now create bad UNO Any values.  Happily reverted to the
original but oddly inconsistent setting of the WrappedTargetException's
TargetException member...

Change-Id: I9421da721d5a111d0d89ee0601238a2c01cfc12c

reportbuilder/java/org/libreoffice/report/pentaho/SOReportJobFactory.java
@@ -216,12 +216,12 @@ public class SOReportJobFactory
             catch (java.lang.Exception e)
             {
                 LOGGER.error("ReportProcessing failed", e);
-                throw new com.sun.star.lang.WrappedTargetException(e.getMessage(), this, e);
+                throw new com.sun.star.lang.WrappedTargetException(e, e.getMessage(), this, null);
             }
             catch (java.lang.IncompatibleClassChangeError e2)
             {
                 LOGGER.error("Detected an IncompatibleClassChangeError");
-                throw new com.sun.star.lang.WrappedTargetException("caught a " + e2.getClass().getName(), this, e2);
+                throw new com.sun.star.lang.WrappedTargetException(e2, "caught a " + e2.getClass().getName(), this, new com.sun.star.uno.Exception(e2.getLocalizedMessage()));
             }
             Thread.currentThread().setContextClassLoader(cl);
 