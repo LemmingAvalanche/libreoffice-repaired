LemmingAvalanche/libreoffice-repaired

commit 9cd0c2ec4bf22a90106851cfd0e7abc7637d7db1

Add missing copy&paste of SfxAppToolBoxControl_Impl::Select
(cherry picked from commit 47204c74d9e52f54f6983af19b66af2a96b42e61)

Conflicts:
	framework/inc/uielement/popuptoolbarcontroller.hxx
	svtools/inc/svtools/toolboxcontroller.hxx

Change-Id: I304d17c662450f29eeffb17f576b418986f3fb0a

framework/source/uielement/popuptoolbarcontroller.cxx
@@ -17,6 +17,8 @@
  */
 
 #include <uielement/popuptoolbarcontroller.hxx>
+#include <framework/menuconfiguration.hxx>
+#include <toolkit/awt/vclxmenu.hxx>
 #include <comphelper/processfactory.hxx>
 #include <svtools/imagemgr.hxx>
 #include <svtools/miscopt.hxx>
@@ -28,9 +30,10 @@
 
 #include <com/sun/star/awt/PopupMenuDirection.hpp>
 #include <com/sun/star/frame/PopupMenuControllerFactory.hpp>
-
+#include <com/sun/star/frame/XDispatchProvider.hpp>
 
 #define UNO_COMMAND_RECENT_FILE_LIST    ".uno:RecentFileList"
+#define SFX_REFERER_USER                "private:user"
 
 namespace css = ::com::sun::star;
 
@@ -270,6 +273,39 @@ NewToolbarController::statusChanged(
     enable( rEvent.IsEnabled );
 }
 
+void SAL_CALL
+NewToolbarController::execute( sal_Int16 /*KeyModifier*/ )
+    throw ( css::uno::RuntimeException )
+{
+    osl::MutexGuard aGuard( m_aMutex );
+    if ( !m_aLastURL.getLength() )
+        return;
+
+    OUString aTarget( RTL_CONSTASCII_USTRINGPARAM( "_default" ) );
+    if ( m_xPopupMenu.is() )
+    {
+        // TODO investigate how to wrap Get/SetUserValue in css::awt::XMenu
+        MenuConfiguration::Attributes* pMenuAttributes( 0 );
+        VCLXPopupMenu*  pTkPopupMenu = dynamic_cast< VCLXPopupMenu * >(
+            VCLXMenu::GetImplementation( m_xPopupMenu ) );
+
+        SolarMutexGuard aSolarMutexGuard;
+        PopupMenu* pVCLPopupMenu = dynamic_cast< PopupMenu * >( pTkPopupMenu->GetMenu() );
+        if ( pVCLPopupMenu )
+            pMenuAttributes = reinterpret_cast< MenuConfiguration::Attributes* >(
+                pVCLPopupMenu->GetUserValue( pVCLPopupMenu->GetCurItemId() ) );
+
+        if ( pMenuAttributes )
+            aTarget = pMenuAttributes->aTargetFrame;
+    }
+
+    css::uno::Sequence< css::beans::PropertyValue > aArgs( 1 );
+    aArgs[0].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "Referer"  ));
+    aArgs[0].Value <<= OUString( RTL_CONSTASCII_USTRINGPARAM( SFX_REFERER_USER ) );
+
+    dispatchCommand( m_aLastURL, aArgs, aTarget );
+}
+
 void NewToolbarController::functionExecuted( const OUString &rCommand )
 {
     setItemImage( rCommand );
@@ -400,6 +436,8 @@ void NewToolbarController::setItemImage( const OUString &rCommand )
     }
     else
         pToolBox->SetItemImage( m_nToolBoxId, aImage );
+
+    m_aLastURL = aURL;
 }
 
 

framework/inc/uielement/popuptoolbarcontroller.hxx
@@ -78,7 +78,10 @@ namespace framework
     private:
         void functionExecuted( const OUString &rCommand );
         void SAL_CALL statusChanged( const ::com::sun::star::frame::FeatureStateEvent& rEvent ) throw ( ::com::sun::star::uno::RuntimeException );
+        void SAL_CALL execute( sal_Int16 KeyModifier ) throw (::com::sun::star::uno::RuntimeException);
         void setItemImage( const OUString &rCommand );
+
+        OUString m_aLastURL;
     };
 
     class WizardsToolbarController : public PopupMenuToolbarController

include/svtools/toolboxcontroller.hxx
@@ -46,8 +46,6 @@ class ToolBox;
 namespace svt
 {
 
-struct DispatchInfo;
-
 class SVT_DLLPUBLIC ToolboxController : public ::com::sun::star::frame::XStatusListener,
                           public ::com::sun::star::frame::XToolbarController,
                           public ::com::sun::star::lang::XInitialization,
@@ -116,12 +114,9 @@ class SVT_DLLPUBLIC ToolboxController : public ::com::sun::star::frame::XStatusL
         const OUString& getCommandURL() const { return  m_aCommandURL; }
         const OUString& getModuleName() const { return m_sModuleName; }
 
-
-        void dispatchCommand( const OUString& sCommandURL, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& rArgs );
+        void dispatchCommand( const OUString& sCommandURL, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& rArgs, const OUString &rTarget = OUString() );
 
         void enable( bool bEnable );
-
-        DECL_STATIC_LINK( ToolboxController, ExecuteHdl_Impl, DispatchInfo* );
 
     protected:
         bool getToolboxId( sal_uInt16& rItemId, ToolBox** ppToolBox );
@@ -134,6 +129,23 @@ class SVT_DLLPUBLIC ToolboxController : public ::com::sun::star::frame::XStatusL
             ::com::sun::star::util::URL aURL;
             ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDispatch > xDispatch;
         };
+
+        struct DispatchInfo
+        {
+            ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDispatch > mxDispatch;
+            const ::com::sun::star::util::URL maURL;
+            const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > maArgs;
+
+            DispatchInfo( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDispatch >& xDispatch,
+                          const ::com::sun::star::util::URL& rURL,
+                          const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& rArgs )
+                : mxDispatch( xDispatch )
+                , maURL( rURL )
+                , maArgs( rArgs )
+                {}
+        };
+
+        DECL_STATIC_LINK( ToolboxController, ExecuteHdl_Impl, DispatchInfo* );
 
         typedef ::boost::unordered_map< OUString,
                                  com::sun::star::uno::Reference< com::sun::star::frame::XDispatch >,

svtools/source/uno/toolboxcontroller.cxx
@@ -49,16 +49,6 @@ using namespace ::com::sun::star::frame;
 namespace svt
 {
 
-struct DispatchInfo
-{
-    Reference< XDispatch > mxDispatch;
-    const URL maURL;
-    const Sequence< PropertyValue > maArgs;
-
-    DispatchInfo( const Reference< XDispatch >& xDispatch, const URL& rURL, const Sequence< PropertyValue >& rArgs )
-        : mxDispatch( xDispatch ), maURL( rURL ), maArgs( rArgs ) {}
-};
-
 ToolboxController::ToolboxController(
     const Reference< XComponentContext >& rxContext,
     const Reference< XFrame >& xFrame,
@@ -705,7 +695,7 @@ Reference< ::com::sun::star::awt::XWindow > ToolboxController::getParent() const
     return m_xParentWindow;
 }
 
-void ToolboxController::dispatchCommand( const OUString& sCommandURL, const Sequence< PropertyValue >& rArgs )
+void ToolboxController::dispatchCommand( const OUString& sCommandURL, const Sequence< PropertyValue >& rArgs, const OUString &sTarget )
 {
     try
     {
@@ -714,9 +704,12 @@ void ToolboxController::dispatchCommand( const OUString& sCommandURL, const Sequ
         aURL.Complete = sCommandURL;
         getURLTransformer()->parseStrict( aURL );
 
-        Reference< XDispatch > xDispatch( xDispatchProvider->queryDispatch( aURL, OUString(), 0 ), UNO_QUERY_THROW );
+        Reference< XDispatch > xDispatch( xDispatchProvider->queryDispatch( aURL, sTarget, 0 ), UNO_QUERY_THROW );
 
-        Application::PostUserEvent( STATIC_LINK(0, ToolboxController, ExecuteHdl_Impl), new DispatchInfo( xDispatch, aURL, rArgs ) );
+        DispatchInfo *pDispatchInfo = new DispatchInfo( xDispatch, aURL, rArgs );
+        if ( !Application::PostUserEvent( STATIC_LINK(0, ToolboxController, ExecuteHdl_Impl),
+                                          pDispatchInfo ) )
+            delete pDispatchInfo;
 
     }
     catch( Exception& )

commit a308ad55927fcf6f48737f9e7820b23b60023282

Resolves: fdo#87636 New button not clickable when Templates preselected
regression from

commit 9cd0c2ec4bf22a90106851cfd0e7abc7637d7db1
Author: Ariel Constenla-Haile <arielch@apache.org>
Date:   Tue May 28 10:01:32 2013 +0000

    Add missing copy&paste of SfxAppToolBoxControl_Impl::Select

    (cherry picked from commit 47204c74d9e52f54f6983af19b66af2a96b42e61)

    Conflicts:
        framework/inc/uielement/popuptoolbarcontroller.hxx
        svtools/inc/svtools/toolboxcontroller.hxx

The menu shown in the ui is a copy of a menu. The original menu has UserData
set on it which contains the target for the command, but the copy doesn't, so
looking up the target always fails and ses the "_default" frame and not the
correct "_self" required for the template menu

Change-Id: Ic1887467bf15f081d27286bd284929c17a2b19ad

framework/inc/uielement/newmenucontroller.hxx
@@ -37,6 +37,7 @@
 
 #include <svtools/popupmenucontrollerbase.hxx>
 #include <toolkit/awt/vclxmenu.hxx>
+#include <framework/menuconfiguration.hxx>
 #include <cppuhelper/weak.hxx>
 #include <rtl/ustring.hxx>
 #include <vcl/accel.hxx>
@@ -80,13 +81,9 @@ namespace framework
 
         private:
             virtual void impl_setPopupMenu() SAL_OVERRIDE;
-            struct AddInfo
-            {
-                OUString aTargetFrame;
-                OUString aImageId;
-            };
+            typedef MenuConfiguration::Attributes AddInfo;
 
-            typedef std::unordered_map< int, AddInfo > AddInfoForId;
+            typedef std::unordered_map< int, std::unique_ptr<AddInfo> > AddInfoForId;
 
             void fillPopupMenu( com::sun::star::uno::Reference< com::sun::star::awt::XPopupMenu >& rPopupMenu );
             void retrieveShortcutsFromConfiguration( const ::com::sun::star::uno::Reference< ::com::sun::star::ui::XAcceleratorConfiguration >& rAccelCfg,

framework/source/uielement/newmenucontroller.cxx
@@ -85,7 +85,7 @@ void NewMenuController::setMenuImages( PopupMenu* pPopupMenu, bool bSetImages )
 
                 AddInfoForId::const_iterator pInfo = m_aAddInfoForItem.find( nItemId );
                 if ( pInfo != m_aAddInfoForItem.end() )
-                    aImageId = pInfo->second.aImageId; // Retrieve image id for menu item
+                    aImageId = pInfo->second->aImageId; // Retrieve image id for menu item
 
                 if ( !aImageId.isEmpty() )
                 {
@@ -336,7 +336,6 @@ void NewMenuController::fillPopupMenu( Reference< css::awt::XPopupMenu >& rPopup
         *pVCLPopupMenu = *pSubMenu;
 
         Image           aImage;
-        AddInfo         aAddInfo;
 
         // retrieve additional parameters from bookmark menu and
         // store it in a unordered_map.
@@ -349,10 +348,10 @@ void NewMenuController::fillPopupMenu( Reference< css::awt::XPopupMenu >& rPopup
                 MenuConfiguration::Attributes* pBmkAttributes = reinterpret_cast<MenuConfiguration::Attributes *>(pSubMenu->GetUserValue( nItemId ));
                 if ( pBmkAttributes != 0 )
                 {
-                    aAddInfo.aTargetFrame = pBmkAttributes->aTargetFrame;
-                    aAddInfo.aImageId     = pBmkAttributes->aImageId;
-
-                    m_aAddInfoForItem.insert( AddInfoForId::value_type( nItemId, aAddInfo ));
+                    auto result = m_aAddInfoForItem.insert(
+                        std::make_pair(nItemId, std::unique_ptr<AddInfo>(new AddInfo(*pBmkAttributes))));
+                    MenuConfiguration::Attributes *pNewInfo = result.first->second.get();
+                    pVCLPopupMenu->SetUserValue(nItemId, reinterpret_cast<sal_uIntPtr>(pNewInfo));
                 }
             }
         }
@@ -420,7 +419,7 @@ void SAL_CALL NewMenuController::itemSelected( const css::awt::MenuEvent& rEvent
             OUString aTargetFrame( m_aTargetFrame );
             AddInfoForId::const_iterator pItem = m_aAddInfoForItem.find( rEvent.MenuId );
             if ( pItem != m_aAddInfoForItem.end() )
-                aTargetFrame = pItem->second.aTargetFrame;
+                aTargetFrame = pItem->second->aTargetFrame;
 
             xDispatch = xDispatchProvider->queryDispatch( aTargetURL, aTargetFrame, 0 );
         }

framework/source/uielement/popuptoolbarcontroller.cxx
@@ -438,6 +438,7 @@ throw ( css::uno::RuntimeException, std::exception )
         SolarMutexGuard aSolarMutexGuard;
         PopupMenu* pVCLPopupMenu = pTkPopupMenu ?
             dynamic_cast< PopupMenu * >( pTkPopupMenu->GetMenu() ) : NULL;
+
         if ( pVCLPopupMenu )
             pMenuAttributes = reinterpret_cast< MenuConfiguration::Attributes* >(
                 pVCLPopupMenu->GetUserValue( pVCLPopupMenu->GetCurItemId() ) );