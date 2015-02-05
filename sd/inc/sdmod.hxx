LemmingAvalanche/libreoffice-repaired

commit 03c8935d8ddb1197edfd7bfb8762bb8861d6e6c5

hard to find accidental leaks with all the deliberate ones
so make these globals belong to the SfxModule which gets
torn down before exit

Change-Id: Iad655f8726ba6784a5d606ef469522950906360a

sd/inc/sdmod.hxx
@@ -26,11 +26,13 @@
 #include <sot/storage.hxx>
 #include <tools/shl.hxx>
 #include "sddllapi.h"
+#include <svl/itemprop.hxx>
 #include <svl/lstner.hxx>
 #include <com/sun/star/text/WritingMode.hpp>
 #include <sfx2/module.hxx>
 #include <vcl/vclevent.hxx>
 #include <sal/types.h>
+#include <boost/ptr_container/ptr_map.hpp>
 #include <memory>
 
 class SdOptions;
@@ -57,6 +59,9 @@ enum SdOptionStreamMode
     SD_OPTION_LOAD = 0,
     SD_OPTION_STORE = 1
 };
+
+typedef boost::ptr_map< sal_uIntPtr, SfxExtItemPropertySetInfo > SdExtPropertySetInfoCache;
+typedef boost::ptr_map< sal_uInt32, css::uno::Sequence< css::uno::Type> > SdTypesCache;
 
 /*
 
@@ -121,6 +126,10 @@ public:
     virtual SfxItemSet*  CreateItemSet( sal_uInt16 nId ) SAL_OVERRIDE;
     virtual void         ApplyItemSet( sal_uInt16 nId, const SfxItemSet& rSet ) SAL_OVERRIDE;
     virtual SfxTabPage*  CreateTabPage( sal_uInt16 nId, vcl::Window* pParent, const SfxItemSet& rSet ) SAL_OVERRIDE;
+
+    SdExtPropertySetInfoCache gImplImpressPropertySetInfoCache;
+    SdExtPropertySetInfoCache gImplDrawPropertySetInfoCache;
+    SdTypesCache gImplTypesCache;
 
 protected:
 

sd/source/ui/unoidl/unoobj.cxx
@@ -85,13 +85,6 @@ using ::com::sun::star::uno::Any;
 using ::com::sun::star::uno::Reference;
 using ::com::sun::star::drawing::XShape;
 
-typedef std::map<sal_uIntPtr, SfxExtItemPropertySetInfo*> SdExtPropertySetInfoCache;
-static SdExtPropertySetInfoCache gImplImpressPropertySetInfoCache;
-static SdExtPropertySetInfoCache gImplDrawPropertySetInfoCache;
-
-typedef std::map<sal_uInt32, uno::Sequence< uno::Type >*> SdTypesCache;
-static SdTypesCache gImplTypesCache;
-
 #define WID_EFFECT          1
 #define WID_SPEED           2
 #define WID_TEXTEFFECT      3
@@ -344,8 +337,9 @@ uno::Sequence< uno::Type > SAL_CALL SdXShape::getTypes()
     }
     else
     {
-        const sal_uInt32 nObjId = mpShape->getShapeKind();
+        sal_uInt32 nObjId = mpShape->getShapeKind();
         uno::Sequence< uno::Type >* pTypes;
+        SdTypesCache& gImplTypesCache = SD_MOD()->gImplTypesCache;
         SdTypesCache::iterator aIter( gImplTypesCache.find( nObjId ) );
         if( aIter == gImplTypesCache.end() )
         {
@@ -354,7 +348,7 @@ uno::Sequence< uno::Type > SAL_CALL SdXShape::getTypes()
             pTypes->realloc( nCount+1 );
             (*pTypes)[nCount] = cppu::UnoType<lang::XTypeProvider>::get();
 
-            gImplTypesCache[ nObjId ] = pTypes;
+            gImplTypesCache.insert(nObjId, pTypes);
         }
         else
         {
@@ -430,17 +424,17 @@ uno::Any SAL_CALL SdXShape::getPropertyDefault( const OUString& aPropertyName ) 
     sal_uIntPtr nObjId = reinterpret_cast<sal_uIntPtr>(mpShape->getPropertyMapEntries());
     SfxExtItemPropertySetInfo* pInfo = NULL;
 
-    SdExtPropertySetInfoCache* pCache = (mpModel && mpModel->IsImpressDocument()) ?
-        &gImplImpressPropertySetInfoCache : &gImplDrawPropertySetInfoCache;
+    SdExtPropertySetInfoCache& rCache = (mpModel && mpModel->IsImpressDocument()) ?
+        SD_MOD()->gImplImpressPropertySetInfoCache : SD_MOD()->gImplDrawPropertySetInfoCache;
 
-    SdExtPropertySetInfoCache::iterator aIter( pCache->find( nObjId ) );
-    if( aIter == pCache->end() )
+    SdExtPropertySetInfoCache::iterator aIter( rCache.find( nObjId ) );
+    if( aIter == rCache.end() )
     {
         uno::Reference< beans::XPropertySetInfo > xInfo( mpShape->_getPropertySetInfo() );
         pInfo = new SfxExtItemPropertySetInfo( mpMap, xInfo->getProperties() );
         pInfo->acquire();
 
-        (*pCache)[ nObjId ] = pInfo;
+        rCache.insert(nObjId, pInfo);
     }
     else
     {

commit 3bd4c04bcc8c6a03ccfbe033c51577373ae6699f

Don't hold refcounted objects by naked pointer
(and no need to hold css::uno::Sequence objects by pointer either).  Regression
from 03c8935d8ddb1197edfd7bfb8762bb8861d6e6c5 "hard to find accidental leaks
with all the deliberate ones" caused JunitTest_svx_unoapi to sometimes crash.

Change-Id: I3579c4bbac58d7e341c8acb52dd3f0f06f7b2245

sd/inc/sdmod.hxx
@@ -23,6 +23,7 @@
 #include "glob.hxx"
 #include "pres.hxx"
 
+#include <rtl/ref.hxx>
 #include <sot/storage.hxx>
 #include <tools/shl.hxx>
 #include "sddllapi.h"
@@ -32,7 +33,7 @@
 #include <sfx2/module.hxx>
 #include <vcl/vclevent.hxx>
 #include <sal/types.h>
-#include <boost/ptr_container/ptr_map.hpp>
+#include <map>
 #include <memory>
 
 class SdOptions;
@@ -60,8 +61,8 @@ enum SdOptionStreamMode
     SD_OPTION_STORE = 1
 };
 
-typedef boost::ptr_map< sal_uIntPtr, SfxExtItemPropertySetInfo > SdExtPropertySetInfoCache;
-typedef boost::ptr_map< sal_uInt32, css::uno::Sequence< css::uno::Type> > SdTypesCache;
+typedef std::map< sal_uIntPtr, rtl::Reference<SfxExtItemPropertySetInfo> > SdExtPropertySetInfoCache;
+typedef std::map< sal_uInt32, css::uno::Sequence< css::uno::Type> > SdTypesCache;
 
 /*
 

sd/source/ui/unoidl/unoobj.cxx
@@ -19,7 +19,7 @@
 
 #include <sal/config.h>
 
-#include <map>
+#include <utility>
 
 #include <com/sun/star/style/XStyle.hpp>
 #include <com/sun/star/presentation/ClickAction.hpp>
@@ -338,24 +338,24 @@ uno::Sequence< uno::Type > SAL_CALL SdXShape::getTypes()
     else
     {
         sal_uInt32 nObjId = mpShape->getShapeKind();
-        uno::Sequence< uno::Type >* pTypes;
+        uno::Sequence< uno::Type > aTypes;
         SdTypesCache& gImplTypesCache = SD_MOD()->gImplTypesCache;
         SdTypesCache::iterator aIter( gImplTypesCache.find( nObjId ) );
         if( aIter == gImplTypesCache.end() )
         {
-            pTypes = new uno::Sequence< uno::Type >( mpShape->_getTypes() );
-            sal_uInt32 nCount = pTypes->getLength();
-            pTypes->realloc( nCount+1 );
-            (*pTypes)[nCount] = cppu::UnoType<lang::XTypeProvider>::get();
+            aTypes = mpShape->_getTypes();
+            sal_uInt32 nCount = aTypes.getLength();
+            aTypes.realloc( nCount+1 );
+            aTypes[nCount] = cppu::UnoType<lang::XTypeProvider>::get();
 
-            gImplTypesCache.insert(nObjId, pTypes);
+            gImplTypesCache.insert(std::make_pair(nObjId, aTypes));
         }
         else
         {
             // use the already computed implementation id
-            pTypes = (*aIter).second;
+            aTypes = (*aIter).second;
         }
-        return *pTypes;
+        return aTypes;
     }
 }
 
@@ -422,7 +422,7 @@ uno::Any SAL_CALL SdXShape::getPropertyDefault( const OUString& aPropertyName ) 
     throw(::com::sun::star::uno::RuntimeException)
 {
     sal_uIntPtr nObjId = reinterpret_cast<sal_uIntPtr>(mpShape->getPropertyMapEntries());
-    SfxExtItemPropertySetInfo* pInfo = NULL;
+    rtl::Reference<SfxExtItemPropertySetInfo> pInfo;
 
     SdExtPropertySetInfoCache& rCache = (mpModel && mpModel->IsImpressDocument()) ?
         SD_MOD()->gImplImpressPropertySetInfoCache : SD_MOD()->gImplDrawPropertySetInfoCache;
@@ -432,9 +432,8 @@ uno::Any SAL_CALL SdXShape::getPropertyDefault( const OUString& aPropertyName ) 
     {
         uno::Reference< beans::XPropertySetInfo > xInfo( mpShape->_getPropertySetInfo() );
         pInfo = new SfxExtItemPropertySetInfo( mpMap, xInfo->getProperties() );
-        pInfo->acquire();
 
-        rCache.insert(nObjId, pInfo);
+        rCache.insert(std::make_pair(nObjId, pInfo));
     }
     else
     {
@@ -442,8 +441,7 @@ uno::Any SAL_CALL SdXShape::getPropertyDefault( const OUString& aPropertyName ) 
         pInfo = (*aIter).second;
     }
 
-    uno::Reference< beans::XPropertySetInfo > xInfo( pInfo );
-    return pInfo;
+    return pInfo.get();
 }
 
 void SAL_CALL SdXShape::setPropertyValue( const OUString& aPropertyName, const ::com::sun::star::uno::Any& aValue )