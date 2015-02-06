LemmingAvalanche/libreoffice-repaired

commit 39c1814940ea27b89d5a8facceab7299fd927029

Unmap memory object right after the buffer is read.
Change-Id: Ic7c355dc005b8071a3b5db347f51882d89d2160f

sc/source/core/opencl/formulagroupcl.cxx
@@ -3692,6 +3692,14 @@ public:
         {
             SAL_WARN("sc.opencl", "Dynamic formula compiler: OpenCL error: " << err);
             mpResBuf = NULL;
+            return;
+        }
+
+        err = clEnqueueUnmapMemObject(kEnv.mpkCmdQueue, mpCLResBuf, mpResBuf, 0, NULL, NULL);
+        if (err != CL_SUCCESS)
+        {
+            SAL_WARN("sc.opencl", "Dynamic formula compiler: OpenCL error: " << err);
+            mpResBuf = NULL;
         }
     }
 
@@ -3701,18 +3709,6 @@ public:
             return false;
 
         rDoc.SetFormulaResults(rTopPos, mpResBuf, mnGroupLength);
-
-        // Obtain cl context
-        ::opencl::KernelEnv kEnv;
-        ::opencl::setKernelEnv(&kEnv);
-
-        cl_int err = clEnqueueUnmapMemObject(kEnv.mpkCmdQueue, mpCLResBuf, mpResBuf, 0, NULL, NULL);
-        if (err != CL_SUCCESS)
-        {
-            SAL_WARN("sc.opencl", "Dynamic formula compiler: OpenCL error: " << err);
-            return false;
-        }
-
         return true;
     }
 };

commit 364f183ded934ca5c136681943f5e1a07b05b98f

Unmap the host buffer only after done accessing it
Most likely 39c1814940ea27b89d5a8facceab7299fd927029 accidentally made the
code unmap the host buffer before it is accessed, but the code continued to
work by accident in many (most?) cases. Either because in the case of OpenCL
devices that share memory with the CPU, the host buffer *is* the OpenCL
buffer, so even if the host buffer is "unmapped", it still exists. In the
case of GPU device with separate memory, using the host buffer after unmapping
corresponds simply to a case of use after free of a heap-allocated buffer,
which often happens to work.

Found by code reading.

Change-Id: I9e2b4574077a267938702c0f81c4b1cba9c9a183

sc/source/core/opencl/formulagroupcl.cxx
@@ -3965,13 +3965,6 @@ public:
             mpResBuf = NULL;
             return;
         }
-
-        err = clEnqueueUnmapMemObject(kEnv.mpkCmdQueue, mpCLResBuf, mpResBuf, 0, NULL, NULL);
-        if (err != CL_SUCCESS)
-        {
-            SAL_WARN("sc.opencl", "clEnqueueUnmapMemObject failed: " << ::opencl::errorString(err));
-            mpResBuf = NULL;
-        }
     }
 
     bool pushResultToDocument( ScDocument& rDoc, const ScAddress& rTopPos )
@@ -3980,6 +3973,20 @@ public:
             return false;
 
         rDoc.SetFormulaResults(rTopPos, mpResBuf, mnGroupLength);
+
+        // Obtain cl context
+        ::opencl::KernelEnv kEnv;
+        ::opencl::setKernelEnv(&kEnv);
+
+        cl_int err;
+        err = clEnqueueUnmapMemObject(kEnv.mpkCmdQueue, mpCLResBuf, mpResBuf, 0, NULL, NULL);
+
+        if (err != CL_SUCCESS)
+        {
+            SAL_WARN("sc.opencl", "clEnqueueUnmapMemObject failed: " << ::opencl::errorString(err));
+            return false;
+        }
+
         return true;
     }
 };