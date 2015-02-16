LemmingAvalanche/libreoffice-repaired

commit a5066fa046a5e6618845256009a373e77eccaa8c

sw: fix regex search/replace of $ in selection
SwView::Replace() nowadays first searches for the to-be-replaced text in
the current selection, which does not work in the corner case of "$"
when the selection starts at end of a paragraph, as it will when
clicking "Find" to look for "$" and then clicking "Replace".

The problem is that there is some antique code in SwPaM::Find() to
move the cursor forward if it's at the end of a paragraph, which is
presumably some pointless micro-optimization; the result is that
"Replace" does not replace the selected paragraph break but the
one in the following paragraph.

(regression from 68a014dbca8bbd25056f75ef551fb81a0dbfb1b7)

Change-Id: I5aae9c272d102a48166c63e01775dc6322f9f02d

sw/source/core/crsr/findtxt.cxx
@@ -221,22 +221,6 @@ bool SwPaM::Find( const SearchOptions& rSearchOpt, bool bSearchInNotes , utl::Te
     SwNodeIndex& rNdIdx = pPam->GetPoint()->nNode;
     SwIndex& rCntntIdx = pPam->GetPoint()->nContent;
 
-    // If a beginning/end, from out of node; stop if empty node
-    if( bSrchForward
-        ? ( rCntntIdx.GetIndex() == pPam->GetCntntNode()->Len() &&
-            rCntntIdx.GetIndex() )
-        : !rCntntIdx.GetIndex() && pPam->GetCntntNode()->Len() )
-    {
-        if( !(*fnMove->fnNds)( &rNdIdx, sal_False ))
-        {
-            delete pPam;
-            return false;
-        }
-        SwCntntNode *pNd = rNdIdx.GetNode().GetCntntNode();
-        xub_StrLen nTmpPos = bSrchForward ? 0 : pNd->Len();
-        rCntntIdx.Assign( pNd, nTmpPos );
-    }
-
     // If bFound is true then the string was found and is between nStart and nEnd
     bool bFound = false;
     // start position in text or initial position

commit 84a096dce4c4d53b07432bf8db6dd641d5912774

tdf#80715: fix infinite loop in find-and-replace if SwTxtNode full
In a regex searching for "$" to remove the paragraph break, sw_JoinText()
may fail to join the nodes because the maximum size is reached; in this
case the cursor has to be moved to the next node, otherwise the search
will be stuck on that node forever.

This would fix the problem in LO 4.2 but it's actually rather harder to
trigger in 4.3+ because the 16-bit size limit of SwTxtNodes is gone.

(regression from a5066fa046a5e6618845256009a373e77eccaa8c)

Change-Id: Ie047cad37835adf95afe0d12b94a16ff4aecb17a

sw/source/core/crsr/findtxt.cxx
@@ -583,9 +583,11 @@ int SwFindParaText::Find( SwPaM* pCrsr, SwMoveFn fnMove,
 
         boost::scoped_ptr<OUString> pRepl( (bRegExp)
                 ? ReplaceBackReferences( rSearchOpt, pCrsr ) : 0 );
-        rCursor.GetDoc()->getIDocumentContentOperations().ReplaceRange( *pCrsr,
-            (pRepl.get()) ? *pRepl : rSearchOpt.replaceString,
-            bRegExp );
+        bool const bReplaced =
+            rCursor.GetDoc()->getIDocumentContentOperations().ReplaceRange(
+                *pCrsr,
+                (pRepl.get()) ? *pRepl : rSearchOpt.replaceString,
+                bRegExp );
         rCursor.SaveTblBoxCntnt( pCrsr->GetPoint() );
 
         if( bRegExp )
@@ -599,7 +601,15 @@ int SwFindParaText::Find( SwPaM* pCrsr, SwMoveFn fnMove,
                 p->MoveTo( const_cast<SwPaM*>(pRegion) );
             } while( p != pPrev );
         }
-        pCrsr->Start()->nContent = nSttCnt;
+        if (bRegExp && !bReplaced)
+        {   // fdo#80715 avoid infinite loop if join failed
+            bool bRet = ((fnMoveForward == fnMove) ? &GoNextPara : &GoPrevPara)
+                (*pCrsr, fnMove);
+            (void) bRet;
+            assert(bRet); // if join failed, next node must be SwTxtNode
+        }
+        else
+            pCrsr->Start()->nContent = nSttCnt;
         return FIND_NO_RING;
     }
     return bFnd ? FIND_FOUND : FIND_NOT_FOUND;

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -3982,11 +3982,14 @@ bool DocumentContentOperationsManager::ReplaceRangeImpl( SwPaM& rPam, const OUSt
         }
     }
 
-    if( bJoinTxt )
-        ::sw_JoinText( rPam, bJoinPrev );
+    bool bRet(true);
+    if (bJoinTxt)
+    {
+        bRet = ::sw_JoinText(rPam, bJoinPrev);
+    }
 
     m_rDoc.getIDocumentState().SetModified();
-    return true;
+    return bRet;
 }
 
 SwFlyFrmFmt* DocumentContentOperationsManager::_InsNoTxtNode( const SwPosition& rPos, SwNoTxtNode* pNode,

sw/source/core/doc/docedt.cxx
@@ -323,7 +323,7 @@ void sw_GetJoinFlags( SwPaM& rPam, bool& rJoinTxt, bool& rJoinPrev )
     }
 }
 
-void sw_JoinText( SwPaM& rPam, bool bJoinPrev )
+bool sw_JoinText( SwPaM& rPam, bool bJoinPrev )
 {
     SwNodeIndex aIdx( rPam.GetPoint()->nNode );
     SwTxtNode *pTxtNd = aIdx.GetNode().GetTxtNode();
@@ -445,7 +445,9 @@ void sw_JoinText( SwPaM& rPam, bool bJoinPrev )
             }
             pTxtNd->JoinNext();
         }
+        return true;
     }
+    else return false;
 }
 
 static void lcl_syncGrammarError( SwTxtNode &rTxtNode, linguistic2::ProofreadingResult& rResult,

sw/source/core/inc/docedt.hxx
@@ -20,7 +20,7 @@
 #ifndef INCLUDED_SW_SOURCE_CORE_INC_DOCEDT_HXX
 #define INCLUDED_SW_SOURCE_CORE_INC_DOCEDT_HXX
 
-void sw_JoinText( SwPaM& rPam, bool bJoinPrev );
+bool sw_JoinText( SwPaM& rPam, bool bJoinPrev );
 
 void sw_GetJoinFlags( SwPaM& rPam, bool& rJoinTxt, bool& rJoinPrev );
 