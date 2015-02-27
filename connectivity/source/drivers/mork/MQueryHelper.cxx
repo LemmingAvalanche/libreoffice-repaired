LemmingAvalanche/libreoffice-repaired

commit 6bab9cb280801c62a0be267d7ead8e31547b8ce5

Resolves fdo#87789: show groups in collected addresses
Retrieves lists in CollectedAddressBook and retrieves the right mork parser
However, it doesn't take into account addresses books created by user

Change-Id: I2cee6ba7afd43dd772f69af6686cc4308e753e66
Reviewed-on: https://gerrit.libreoffice.org/13760
Reviewed-by: Lionel Elie Mamane <lionel@mamane.lu>
Tested-by: Lionel Elie Mamane <lionel@mamane.lu>

connectivity/source/drivers/mork/MQueryHelper.cxx
@@ -195,7 +195,15 @@ sal_Int32 MQueryHelper::executeQuery(OConnection* xConnection, MQueryExpression 
     OString oStringTable = OUStringToOString( m_aAddressbook, RTL_TEXTENCODING_UTF8 );
     std::set<int> listRecords;
     bool handleListTable = false;
-    MorkParser* xMork = xConnection->getMorkParser(oStringTable);
+
+    // Let's try to retrieve the list in Collected Addresses book
+    MorkParser* xMork = xConnection->getMorkParser(OString("CollectedAddressBook"));
+    if (std::find(xMork->lists_.begin(), xMork->lists_.end(), m_aAddressbook) == xMork->lists_.end())
+    {
+        // so the list is in Address book
+        // TODO : manage case where an address book has been created
+        xMork = xConnection->getMorkParser(OString("AddressBook"));
+    }
 
     // check if we are retrieving the default table
     if (oStringTable != "AddressBook" && oStringTable != "CollectedAddressBook")

connectivity/source/drivers/mork/MConnection.cxx
@@ -169,6 +169,17 @@ void OConnection::construct(const OUString& url,const Sequence< PropertyValue >&
             SAL_INFO("connectivity.mork", "table->first : " << tableIter->first);
         }
     }
+    // check that we can retrieve the history tables:
+    MorkTableMap *Tables_hist = m_pHistory->getTables( defaultScope );
+    if (Tables_hist)
+    {
+        // Iterate all tables
+        for ( tableIter = Tables_hist->begin(); tableIter != Tables_hist->end(); ++tableIter )
+        {
+            if ( 0 == tableIter->first ) continue;
+            SAL_INFO("connectivity.mork", "table->first : " << tableIter->first);
+        }
+    }
 }
 
 // XServiceInfo

connectivity/source/drivers/mork/MDatabaseMetaDataHelper.cxx
@@ -58,12 +58,27 @@ bool MDatabaseMetaDataHelper::getTableStrings( OConnection* _pCon,
 
     /* retrieve list table names (not from collected ab) */
     std::set<std::string> lists;
-    _pCon->getMorkParser("AddressBook")->retrieveLists(lists);
+    MorkParser* pMork = _pCon->getMorkParser("AddressBook");
+    pMork->retrieveLists(lists);
     for (::std::set<std::string>::iterator iter = lists.begin(); iter != lists.end(); ++iter) {
         OUString groupTableName = OStringToOUString((*iter).c_str(), RTL_TEXTENCODING_UTF8);
         SAL_INFO("connectivity.mork", "add Table " << groupTableName);
 
         _rStrings.push_back(groupTableName);
+        // remember the list in the mork parser, we'll use it later
+        pMork->lists_.push_back(groupTableName);
+    }
+
+    std::set<std::string> lists_history;
+    pMork = _pCon->getMorkParser("CollectedAddressBook");
+    pMork->retrieveLists(lists_history);
+    for (::std::set<std::string>::iterator iter = lists_history.begin(); iter != lists_history.end(); ++iter) {
+        OUString groupTableName = OStringToOUString((*iter).c_str(), RTL_TEXTENCODING_UTF8);
+        SAL_INFO("connectivity.mork", "add Table " << groupTableName);
+
+        _rStrings.push_back(groupTableName);
+        // remember the list in the mork parser, we'll use it later
+        pMork->lists_.push_back(groupTableName);
     }
 
     return true;

connectivity/source/drivers/mork/MorkParser.hxx
@@ -36,6 +36,7 @@
 #define INCLUDED_CONNECTIVITY_SOURCE_DRIVERS_MORK_MORKPARSER_HXX
 
 #include <sal/types.h>
+#include <rtl/ustring.hxx>
 
 #include <string>
 #include <map>
@@ -113,6 +114,9 @@ public:
 
     void dump();
 
+    // All lists
+    std::vector<OUString> lists_;
+
 protected: // Members
 
     void initVars();
@@ -148,6 +152,7 @@ protected: // Data
 
     // All Mork data
     std::string morkData_;
+
 
     unsigned morkPos_;
     int nextAddValueId_;

commit 939433bba735cc7bb16a051b50107d1b041712ea

Re tdf#87789: Fix opening mork CollectedAddressBook Table
...which now opened the AddressBook Table instead.  Apparently a regression from
6bab9cb280801c62a0be267d7ead8e31547b8ce5 "Resolves fdo#87789: show groups in
collected addresses."

Change-Id: I413adb3ee963262cac71547cde6e5ccc5a9a19ab

connectivity/source/drivers/mork/MQueryHelper.cxx
@@ -195,19 +195,23 @@ sal_Int32 MQueryHelper::executeQuery(OConnection* xConnection, MQueryExpression 
     OString oStringTable = OUStringToOString( m_aAddressbook, RTL_TEXTENCODING_UTF8 );
     std::set<int> listRecords;
     bool handleListTable = false;
-
-    // Let's try to retrieve the list in Collected Addresses book
-    MorkParser* xMork = xConnection->getMorkParser(OString("CollectedAddressBook"));
-    if (std::find(xMork->lists_.begin(), xMork->lists_.end(), m_aAddressbook) == xMork->lists_.end())
-    {
-        // so the list is in Address book
-        // TODO : manage case where an address book has been created
-        xMork = xConnection->getMorkParser(OString("AddressBook"));
-    }
+    MorkParser* xMork;
 
     // check if we are retrieving the default table
-    if (oStringTable != "AddressBook" && oStringTable != "CollectedAddressBook")
+    if (oStringTable == "AddressBook" || oStringTable == "CollectedAddressBook")
     {
+        xMork = xConnection->getMorkParser(oStringTable);
+    }
+    else
+    {
+        // Let's try to retrieve the list in Collected Addresses book
+        xMork = xConnection->getMorkParser(OString("CollectedAddressBook"));
+        if (std::find(xMork->lists_.begin(), xMork->lists_.end(), m_aAddressbook) == xMork->lists_.end())
+        {
+            // so the list is in Address book
+            // TODO : manage case where an address book has been created
+            xMork = xConnection->getMorkParser(OString("AddressBook"));
+        }
         handleListTable = true;
         // retrieve row ids for that list table
         std::string listTable = oStringTable.getStr();