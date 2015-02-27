LemmingAvalanche/libreoffice-repaired

commit 5bb5137d9194e791e707b22a2085f229dd80645a

forms: use constructor feature for form modules
Change-Id: I8f1588cbd338e2f8b5bc6047d84b17c697a7d2d3

forms/source/component/FormattedFieldWrapper.cxx
@@ -355,5 +355,18 @@ void OFormattedFieldWrapper::ensureAggregate()
 
 }
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OFormattedFieldWrapper_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OFormattedFieldWrapper(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_comp_form_OFormattedFieldWrapper_ForcedFormatted_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OFormattedFieldWrapper(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/FormattedFieldWrapper.hxx
@@ -25,7 +25,6 @@
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/util/XCloneable.hpp>
 
-
 namespace frm
 {
 
@@ -42,6 +41,9 @@ class OFormattedFieldWrapper : public OFormattedFieldWrapper_Base
 {
     ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext> m_xContext;
 
+public:
+    OFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory);
+
 protected:
     ::com::sun::star::uno::Reference< ::com::sun::star::uno::XAggregation>      m_xAggregate;
 
@@ -53,9 +55,6 @@ protected:
     // if bActAsFormatted is false, the state is undetermined until somebody calls
     // ::read or does anything which requires a living aggregate
     static InterfaceRef createFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory, bool bActAsFormatted);
-
-private:
-    OFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory);
 
 protected:
     virtual ~OFormattedFieldWrapper();
@@ -86,9 +85,7 @@ protected:
     void ensureAggregate();
 };
 
-
 }
-
 
 #endif // INCLUDED_FORMS_SOURCE_COMPONENT_FORMATTEDFIELDWRAPPER_HXX
 

forms/source/component/Button.cxx
@@ -31,7 +31,6 @@
 #include <vcl/svapp.hxx>
 #include <osl/mutex.hxx>
 
-
 namespace frm
 {
 
@@ -51,12 +50,6 @@ using ::com::sun::star::frame::XDispatchProviderInterceptor;
 
 
 //= OButtonModel
-
-
-InterfaceRef SAL_CALL OButtonModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OButtonModel( comphelper::getComponentContext(_rxFactory) ));
-}
 
 
 OButtonModel::OButtonModel(const Reference<XComponentContext>& _rxFactory)
@@ -332,12 +325,6 @@ void OButtonModel::impl_resetNoBroadcast_nothrow()
 
 
 // OButtonControl
-
-
-InterfaceRef SAL_CALL OButtonControl_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OButtonControl( comphelper::getComponentContext(_rxFactory) ));
-}
 
 
 Sequence<Type> OButtonControl::_getTypes()
@@ -759,8 +746,20 @@ void SAL_CALL OButtonControl::releaseDispatchProviderInterceptor( const Referenc
     OFormNavigationHelper::releaseDispatchProviderInterceptor( _rxInterceptor );
 }
 
-
 }   // namespace frm
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OButtonModel_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OButtonModel(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OButtonControl_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OButtonControl(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/CheckBox.cxx
@@ -24,7 +24,6 @@
 #include <comphelper/basicio.hxx>
 #include <comphelper/processfactory.hxx>
 
-
 namespace frm
 {
 using namespace ::com::sun::star::uno;
@@ -46,12 +45,6 @@ OCheckBoxControl::OCheckBoxControl(const Reference<XComponentContext>& _rxFactor
 }
 
 
-InterfaceRef SAL_CALL OCheckBoxControl_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OCheckBoxControl( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 StringSequence SAL_CALL OCheckBoxControl::getSupportedServiceNames() throw(::com::sun::star::uno::RuntimeException, std::exception)
 {
     StringSequence aSupported = OBoundControl::getSupportedServiceNames();
@@ -64,15 +57,6 @@ StringSequence SAL_CALL OCheckBoxControl::getSupportedServiceNames() throw(::com
 
 
 //= OCheckBoxModel
-
-
-
-InterfaceRef SAL_CALL OCheckBoxModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OCheckBoxModel( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 
 OCheckBoxModel::OCheckBoxModel(const Reference<XComponentContext>& _rxFactory)
     :OReferenceValueComponent( _rxFactory, VCL_CONTROLMODEL_CHECKBOX, FRM_SUN_CONTROL_CHECKBOX, true )
@@ -281,8 +265,20 @@ bool OCheckBoxModel::commitControlValueToDbColumn( bool /*_bPostReset*/ )
     return true;
 }
 
-
 }
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OCheckBoxModel_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OCheckBoxModel(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OCheckBoxControl_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OCheckBoxControl(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/ComboBox.cxx
@@ -71,14 +71,6 @@ using namespace ::com::sun::star::form::binding;
 
 
 // class OComboBoxModel
-
-
-InterfaceRef SAL_CALL OComboBoxModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return (*new OComboBoxModel( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 Sequence<Type> OComboBoxModel::_getTypes()
 {
     return ::comphelper::concatSequences(
@@ -867,14 +859,6 @@ void SAL_CALL OComboBoxModel::disposing( const EventObject& _rSource ) throw ( R
 
 //= OComboBoxControl
 
-
-
-InterfaceRef SAL_CALL OComboBoxControl_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OComboBoxControl( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 OComboBoxControl::OComboBoxControl(const Reference<XComponentContext>& _rxContext)
     :OBoundControl(_rxContext, VCL_CONTROL_COMBOBOX)
 {
@@ -891,8 +875,20 @@ StringSequence SAL_CALL OComboBoxControl::getSupportedServiceNames() throw(Runti
     return aSupported;
 }
 
-
 }
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OComboBoxModel_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OComboBoxModel(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OComboBoxControl_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OComboBoxControl(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/Currency.cxx
@@ -24,6 +24,7 @@
 #include <unotools/syslocale.hxx>
 #include <comphelper/processfactory.hxx>
 
+
 namespace frm
 {
 
@@ -44,13 +45,6 @@ OCurrencyControl::OCurrencyControl(const Reference<XComponentContext>& _rxFactor
 {
 }
 
-
-InterfaceRef SAL_CALL OCurrencyControl_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OCurrencyControl( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 Sequence<Type> OCurrencyControl::_getTypes()
 {
     return OBoundControl::_getTypes();
@@ -69,13 +63,6 @@ StringSequence SAL_CALL OCurrencyControl::getSupportedServiceNames() throw(std::
 
 
 // OCurrencyModel
-
-
-InterfaceRef SAL_CALL OCurrencyModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OCurrencyModel( comphelper::getComponentContext(_rxFactory) ));
-}
-
 
 Sequence<Type> OCurrencyModel::_getTypes()
 {
@@ -247,5 +234,18 @@ void OCurrencyModel::resetNoBroadcast()
 
 }   // namespace frm
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OCurrencyModel_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OCurrencyModel(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OCurrencyControl_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OCurrencyControl(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/Date.cxx
@@ -26,7 +26,6 @@
 
 using namespace dbtools;
 
-
 namespace frm
 {
 
@@ -51,12 +50,6 @@ ODateControl::ODateControl(const Reference<XComponentContext>& _rxFactory)
 }
 
 
-InterfaceRef SAL_CALL ODateControl_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new ODateControl( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 Sequence<Type> ODateControl::_getTypes()
 {
     return OBoundControl::_getTypes();
@@ -71,11 +64,6 @@ StringSequence SAL_CALL ODateControl::getSupportedServiceNames() throw(std::exce
     OUString*pArray = aSupported.getArray();
     pArray[aSupported.getLength()-1] = FRM_SUN_CONTROL_DATEFIELD;
     return aSupported;
-}
-
-InterfaceRef SAL_CALL ODateModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new ODateModel( comphelper::getComponentContext(_rxFactory) ));
 }
 
 
@@ -318,8 +306,20 @@ Sequence< Type > ODateModel::getSupportedBindingTypes()
     return Sequence< Type >( & cppu::UnoType<util::Date>::get(), 1 );
 }
 
-
 }   // namespace frm
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_ODateModel_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::ODateModel(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_ODateControl_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::ODateControl(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/Edit.cxx
@@ -60,12 +60,6 @@ using namespace ::com::sun::star::util;
 using namespace ::com::sun::star::form::binding;
 
 
-InterfaceRef SAL_CALL OEditControl_CreateInstance(const Reference< XMultiServiceFactory > & _rxFactory)
-{
-    return *(new OEditControl( comphelper::getComponentContext(_rxFactory) ));
-}
-
-
 Sequence<Type> OEditControl::_getTypes()
 {
     static Sequence<Type> aTypes;
@@ -267,11 +261,6 @@ IMPL_LINK(OEditControl, OnKeyPressed, void*, /*EMPTYARG*/)
 void SAL_CALL OEditControl::createPeer( const Reference< XToolkit>& _rxToolkit, const Reference< XWindowPeer>& _rxParent ) throw ( RuntimeException, std::exception )
 {
     OBoundControl::createPeer(_rxToolkit, _rxParent);
-}
-
-InterfaceRef SAL_CALL OEditModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
-{
-    return *(new OEditModel( comphelper::getComponentContext(_rxFactory) ));
 }
 
 
@@ -734,8 +723,20 @@ Any OEditModel::getDefaultForReset() const
     return makeAny( m_aDefaultText );
 }
 
-
 }
 
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OEditModel_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OEditModel(component));
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
+com_sun_star_form_OEditControl_get_implementation(::com::sun::star::uno::XComponentContext* component,
+        ::com::sun::star::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new frm::OEditControl(component));
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 5081bdb107fc832c793db9d6af2ca7a4ecc1c0e7

Fix OFormattedFieldWrapper[_ForcedFormatted] instance creation
broken with 5bb5137d9194e791e707b22a2085f229dd80645a "forms: use constructor
feature for form modules" and follow-up misguided clean up

Change-Id: If9447740e6408472722b81d112cd85349020eabe

forms/source/component/FormattedFieldWrapper.cxx
@@ -48,6 +48,40 @@ OFormattedFieldWrapper::OFormattedFieldWrapper(const Reference<XComponentContext
 {
 }
 
+InterfaceRef OFormattedFieldWrapper::createFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory, bool bActAsFormatted)
+{
+    OFormattedFieldWrapper *pRef = new OFormattedFieldWrapper(_rxFactory);
+
+    if (bActAsFormatted)
+    {
+        // instantiate an FormattedModel
+        InterfaceRef  xFormattedModel;
+        // (instantiate it directly ..., as the OFormattedModel isn't
+        // registered for any service names anymore)
+        OFormattedModel* pModel = new OFormattedModel(pRef->m_xContext);
+        query_interface(static_cast<XWeak*>(pModel), xFormattedModel);
+
+        pRef->m_xAggregate = Reference<XAggregation> (xFormattedModel, UNO_QUERY);
+        OSL_ENSURE(pRef->m_xAggregate.is(), "the OFormattedModel didn't have an XAggregation interface !");
+
+        // _before_ setting the delegator, give it to the member references
+        query_interface(xFormattedModel, pRef->m_xFormattedPart);
+        pRef->m_pEditPart = rtl::Reference< OEditModel >(new OEditModel(pRef->m_xContext));
+    }
+
+    increment(pRef->m_refCount);
+
+    if (pRef->m_xAggregate.is())
+    {   // has to be in it's own block because of the temporary variable created by *this
+        pRef->m_xAggregate->setDelegator(static_cast<XWeak*>(pRef));
+    }
+
+    InterfaceRef xRef(*pRef);
+    decrement(pRef->m_refCount);
+
+    return xRef;
+}
+
 Reference< XCloneable > SAL_CALL OFormattedFieldWrapper::createClone() throw (RuntimeException, std::exception)
 {
     ensureAggregate();
@@ -311,14 +345,20 @@ extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
 com_sun_star_form_OFormattedFieldWrapper_get_implementation(::com::sun::star::uno::XComponentContext* component,
         ::com::sun::star::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(new frm::OFormattedFieldWrapper(component));
+    css::uno::Reference<css::uno::XInterface> inst(
+        OFormattedFieldWrapper::createFormattedFieldWrapper(component, false));
+    inst->acquire();
+    return inst.get();
 }
 
 extern "C" SAL_DLLPUBLIC_EXPORT ::com::sun::star::uno::XInterface* SAL_CALL
 com_sun_star_comp_forms_OFormattedFieldWrapper_ForcedFormatted_get_implementation(::com::sun::star::uno::XComponentContext* component,
         ::com::sun::star::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(new frm::OFormattedFieldWrapper(component));
+    css::uno::Reference<css::uno::XInterface> inst(
+        OFormattedFieldWrapper::createFormattedFieldWrapper(component, true));
+    inst->acquire();
+    return inst.get();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

forms/source/component/FormattedFieldWrapper.hxx
@@ -41,9 +41,6 @@ class OFormattedFieldWrapper : public OFormattedFieldWrapper_Base
 {
     ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext> m_xContext;
 
-public:
-    OFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory);
-
 protected:
     ::com::sun::star::uno::Reference< ::com::sun::star::uno::XAggregation>      m_xAggregate;
 
@@ -51,10 +48,19 @@ protected:
     // if we act as formatted this is used to write the EditModel part
     ::com::sun::star::uno::Reference< ::com::sun::star::io::XPersistObject>     m_xFormattedPart;
 
+private:
+    OFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory);
+
 protected:
     virtual ~OFormattedFieldWrapper();
 
 public:
+    // if we act as formatted, this is the PersistObject interface of our aggregate, used
+    // to read and write the FormattedModel part
+    // if bActAsFormatted is false, the state is undetermined until somebody calls
+    // ::read or does anything which requires a living aggregate
+    static InterfaceRef createFormattedFieldWrapper(const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext>& _rxFactory, bool bActAsFormatted);
+
     // UNO
     DECLARE_UNO3_AGG_DEFAULTS(OFormattedFieldWrapper, OWeakAggObject)
     virtual ::com::sun::star::uno::Any SAL_CALL queryAggregation(const ::com::sun::star::uno::Type& _rType) throw(::com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;