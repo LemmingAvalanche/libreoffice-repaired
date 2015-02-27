LemmingAvalanche/libreoffice-repaired

commit 6a275a1392892c0139777eec78a95b6b88e943ec

make use of startsWith()
Change-Id: Ie70097de550ddd1cddc6714f8f86f9723cb36679
Reviewed-on: https://gerrit.libreoffice.org/2599
Tested-by: Fridrich Strba <fridrich@documentfoundation.org>
Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>

connectivity/source/drivers/kab/KDriver.cxx
@@ -408,7 +408,7 @@ sal_Bool SAL_CALL KabDriver::acceptsURL( const ::rtl::OUString& url )
         return sal_False;
 
     // here we have to look whether we support this URL format
-    return (!url.compareTo(::rtl::OUString("sdbc:address:kab:"), 16));
+    return url.startsWith("sdbc:address:kab:");
 }
 // --------------------------------------------------------------------------------
 Sequence< DriverPropertyInfo > SAL_CALL KabDriver::getPropertyInfo( const ::rtl::OUString&, const Sequence< PropertyValue >& ) throw(SQLException, RuntimeException)

connectivity/source/drivers/ado/ADriver.cxx
@@ -141,7 +141,7 @@ Reference< XConnection > SAL_CALL ODriver::connect( const ::rtl::OUString& url, 
 sal_Bool SAL_CALL ODriver::acceptsURL( const ::rtl::OUString& url )
         throw(SQLException, RuntimeException)
 {
-    return (!url.compareTo(::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("sdbc:ado:")),9));
+    return url.startsWith("sdbc:ado:");
 }
 // -----------------------------------------------------------------------------
 void ODriver::impl_checkURL_throw(const ::rtl::OUString& _sUrl)

connectivity/source/drivers/calc/CDriver.cxx
@@ -78,7 +78,7 @@ Reference< XConnection > SAL_CALL ODriver::connect( const ::rtl::OUString& url,
 sal_Bool SAL_CALL ODriver::acceptsURL( const ::rtl::OUString& url )
                 throw(SQLException, RuntimeException)
 {
-    return url.compareTo(::rtl::OUString("sdbc:calc:"),10) == 0;
+    return url.startsWith("sdbc:calc:");
 }
 
 Sequence< DriverPropertyInfo > SAL_CALL ODriver::getPropertyInfo( const ::rtl::OUString& url, const Sequence< PropertyValue >& /*info*/ ) throw(SQLException, RuntimeException)

connectivity/source/drivers/dbase/DDriver.cxx
@@ -70,7 +70,7 @@ Reference< XConnection > SAL_CALL ODriver::connect( const ::rtl::OUString& url, 
 // --------------------------------------------------------------------------------
 sal_Bool SAL_CALL ODriver::acceptsURL( const ::rtl::OUString& url ) throw(SQLException, RuntimeException)
 {
-    return !url.compareTo(::rtl::OUString("sdbc:dbase:"),11);
+    return url.startsWith("sdbc:dbase:");
 }
 // -----------------------------------------------------------------------------
 Sequence< DriverPropertyInfo > SAL_CALL ODriver::getPropertyInfo( const ::rtl::OUString& url, const Sequence< PropertyValue >& /*info*/ ) throw(SQLException, RuntimeException)

connectivity/source/drivers/file/FDriver.cxx
@@ -118,7 +118,7 @@ sal_Bool SAL_CALL OFileDriver::acceptsURL( const ::rtl::OUString& url )
                 throw(SQLException, RuntimeException)
 {
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "file", "Ocke.Janssen@sun.com", "OFileDriver::acceptsURL" );
-    return (!url.compareTo(::rtl::OUString("sdbc:file:"),10));
+    return url.startsWith("sdbc:file:");
 }
 // --------------------------------------------------------------------------------
 Sequence< DriverPropertyInfo > SAL_CALL OFileDriver::getPropertyInfo( const ::rtl::OUString& url, const Sequence< PropertyValue >& /*info*/ ) throw(SQLException, RuntimeException)

connectivity/source/drivers/flat/EDriver.cxx
@@ -74,7 +74,7 @@ Reference< XConnection > SAL_CALL ODriver::connect( const ::rtl::OUString& url, 
 sal_Bool SAL_CALL ODriver::acceptsURL( const ::rtl::OUString& url )
                 throw(SQLException, RuntimeException)
 {
-    return url.compareTo(::rtl::OUString("sdbc:flat:"),10) == 0;
+    return url.startsWith("sdbc:flat:");
 }
 // -----------------------------------------------------------------------------
 Sequence< DriverPropertyInfo > SAL_CALL ODriver::getPropertyInfo( const ::rtl::OUString& url, const Sequence< PropertyValue >& info ) throw(SQLException, RuntimeException)

connectivity/source/drivers/jdbc/JDriver.cxx
@@ -118,8 +118,7 @@ sal_Bool SAL_CALL java_sql_Driver::acceptsURL( const ::rtl::OUString& url ) thro
     // I feel responsible for all jdbc url's
     sal_Bool bEnabled = sal_False;
     OSL_VERIFY_EQUALS( jfw_getEnabled( &bEnabled ), JFW_E_NONE, "error in jfw_getEnabled" );
-    static const ::rtl::OUString s_sJdbcPrefix(  "jdbc:" );
-    return bEnabled && 0 == url.compareTo(s_sJdbcPrefix, 5);
+    return bEnabled && url.startsWith("jdbc:");
 }
 // -------------------------------------------------------------------------
 Sequence< DriverPropertyInfo > SAL_CALL java_sql_Driver::getPropertyInfo( const ::rtl::OUString& url,

connectivity/source/drivers/macab/MacabDriver.cxx
@@ -276,7 +276,7 @@ sal_Bool SAL_CALL MacabDriver::acceptsURL( const ::rtl::OUString& url )
         return sal_False;
 
     // here we have to look whether we support this URL format
-    return (!url.compareTo(::rtl::OUString("sdbc:address:macab:"), 18));
+    return url.startsWith("sdbc:address:macab:");
 }
 // --------------------------------------------------------------------------------
 Sequence< DriverPropertyInfo > SAL_CALL MacabDriver::getPropertyInfo( const ::rtl::OUString&, const Sequence< PropertyValue >& ) throw(SQLException, RuntimeException)

commit b851ee65f616dca9e7069a868429da90ebdfc224

sdbc:address:kab URLs don't have a trailing ":"
broken since 6a275a1392892c0139777eec78a95b6b88e943ec "make use of startsWith()"
where the tricky

  !url.compareTo(::rtl::OUString("sdbc:address:kab:"), 16)

was replaced with

  url.startsWith("sdbc:address:kab:")

even though the original exempted the trailing ":" from the comparison

Change-Id: I45175b2669d61eb4cee36d4456ccb69d0f11ef63

connectivity/source/drivers/kab/KDriver.cxx
@@ -414,7 +414,7 @@ sal_Bool SAL_CALL KabDriver::acceptsURL( const OUString& url )
         return sal_False;
 
     // here we have to look whether we support this URL format
-    return url.startsWith("sdbc:address:" KAB_SERVICE_NAME ":");
+    return url.startsWith("sdbc:address:" KAB_SERVICE_NAME);
 }
 
 Sequence< DriverPropertyInfo > SAL_CALL KabDriver::getPropertyInfo( const OUString&, const Sequence< PropertyValue >& ) throw(SQLException, RuntimeException, std::exception)