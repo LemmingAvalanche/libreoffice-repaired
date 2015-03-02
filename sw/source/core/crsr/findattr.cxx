LemmingAvalanche/libreoffice-repaired

commit a470e43ae083dd3a1309d021cf14c2877dbfe855

fdo#44689: fix for specific case of page restart-value 0
This bug fix is for roundtripping a DOCX that has
a specific 'start value' for the page numbers.
In most cases LO imports it ok.
However - until now - Word allowed you to start page number
from 0, while LO only allowed starting page numbers from 1.
This was because the 'start value' was stored in an 'unsigned int',
and the value '0' was used to mark 'there is no start value'.
This patch changes the way the 'start value' is stored
from 'unsigned int' to 'optional unsigned int'.
This way - if there is no value applied - the variable will hold NULL.
However - if a value is set - it can be 0 or more.
This meant also tweaking all the places that used to get this value,
so that now they handle an 'optional uint', instead of a 'uint'.

Conflicts:
	sw/source/ui/inc/break.hxx
	sw/source/ui/inc/wrtsh.hxx
	sw/source/ui/shells/textsh1.cxx
	sw/source/ui/utlui/uitool.cxx
	sw/source/ui/wrtsh/wrtsh1.cxx

Change-Id: I6ad9d90e03b42c58eed2271477df43c20ad6f20a
Reviewed-on: https://gerrit.libreoffice.org/5681

sw/source/core/crsr/findattr.cxx
@@ -40,6 +40,7 @@
 #include <pamtyp.hxx>
 #include <swundo.hxx>
 #include <crsskip.hxx>
+#include <boost/optional.hpp>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::lang;
@@ -60,10 +61,26 @@ int CmpAttr( const SfxPoolItem& rItem1, const SfxPoolItem& rItem2 )
         return ((SvxColorItem&)rItem1).GetValue().IsRGBEqual(
                                 ((SvxColorItem&)rItem2).GetValue() );
     case RES_PAGEDESC:
-        return ((SwFmtPageDesc&)rItem1).GetNumOffset() ==
-                        ((SwFmtPageDesc&)rItem2).GetNumOffset() &&
-                ((SwFmtPageDesc&)rItem1).GetPageDesc() ==
-                        ((SwFmtPageDesc&)rItem2).GetPageDesc();
+        bool bNumOffsetEqual = false;
+        ::boost::optional<sal_uInt16> oNumOffset1 = ((SwFmtPageDesc&)rItem1).GetNumOffset();
+        ::boost::optional<sal_uInt16> oNumOffset2 = ((SwFmtPageDesc&)rItem1).GetNumOffset();
+        if (!oNumOffset1 && !oNumOffset2)
+        {
+            bNumOffsetEqual = true;
+        }
+        else if (oNumOffset1 && oNumOffset2)
+        {
+            bNumOffsetEqual = oNumOffset1.get() == oNumOffset2.get();
+        }
+        else
+        {
+            bNumOffsetEqual = false;
+        }
+
+        if (bNumOffsetEqual == false)
+            return false;
+
+        return ((SwFmtPageDesc&)rItem1).GetPageDesc() == ((SwFmtPageDesc&)rItem2).GetPageDesc();
     }
     return rItem1 == rItem2;
 }

sw/inc/fmtpdsc.hxx
@@ -25,6 +25,7 @@
 #include <hintids.hxx>
 #include <format.hxx>
 #include <calbck.hxx>
+#include <boost/optional.hpp>
 
 class SwPageDesc;
 class SwHistory;
@@ -41,7 +42,7 @@ class SW_DLLPUBLIC SwFmtPageDesc : public SfxPoolItem, public SwClient
      to set the auto-flag after copying!! */
     friend sal_Bool InsAttr( SwDoc*, const SwPaM &, const SfxItemSet&, sal_uInt16,
                         SwHistory* );
-    sal_uInt16 nNumOffset;          ///< Offset page number.
+    ::boost::optional<sal_uInt16> oNumOffset;          ///< Offset page number.
     sal_uInt16 nDescNameIdx;        ///< SW3-Reader: stringpool-index of style name.
     SwModify* pDefinedIn;       /**< Points to the object in which the
                                  attribute was set (CntntNode/Format). */
@@ -71,8 +72,8 @@ public:
           SwPageDesc *GetPageDesc() { return (SwPageDesc*)GetRegisteredIn(); }
     const SwPageDesc *GetPageDesc() const { return (SwPageDesc*)GetRegisteredIn(); }
 
-    sal_uInt16  GetNumOffset() const        { return nNumOffset; }
-    void    SetNumOffset( sal_uInt16 nNum ) { nNumOffset = nNum; }
+    ::boost::optional<sal_uInt16>  GetNumOffset() const        { return oNumOffset; }
+    void    SetNumOffset( ::boost::optional<sal_uInt16> oNum ) { oNumOffset = oNum; }
 
     /// Query / set where attribute is anchored.
     inline const SwModify* GetDefinedIn() const { return pDefinedIn; }

sw/inc/swabstdlg.hxx
@@ -34,6 +34,7 @@
 #include <com/sun/star/awt/XControl.hpp>
 #include <com/sun/star/container/XNamed.hpp>
 #include "itabenum.hxx"
+#include <boost/optional.hpp>
 
 class SfxViewFrame;
 class SfxBindings;
@@ -205,9 +206,9 @@ public:
 class AbstractSwBreakDlg : public VclAbstractDialog
 {
 public:
-    virtual OUString    GetTemplateName() = 0;
-    virtual sal_uInt16  GetKind() = 0;
-    virtual sal_uInt16  GetPageNumber() = 0;
+    virtual OUString                        GetTemplateName() = 0;
+    virtual sal_uInt16                      GetKind() = 0;
+    virtual ::boost::optional<sal_uInt16>   GetPageNumber() = 0;
 
 };
 

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -110,6 +110,7 @@ public:
     void testParaShadow();
     void testTableFloating();
     void testTableFloatingMargins();
+    void testFdo44689_start_page_0();
     void testFdo44689_start_page_7();
     void testFdo67737();
     void testTransparentShadow();
@@ -229,6 +230,7 @@ void Test::run()
         {"para-shadow.docx", &Test::testParaShadow},
         {"table-floating.docx", &Test::testTableFloating},
         {"table-floating-margins.docx", &Test::testTableFloatingMargins},
+        {"fdo44689_start_page_0.docx", &Test::testFdo44689_start_page_0},
         {"fdo44689_start_page_7.docx", &Test::testFdo44689_start_page_7},
         {"fdo67737.docx", &Test::testFdo67737},
         {"transparent-shadow.docx", &Test::testTransparentShadow},
@@ -1240,6 +1242,13 @@ void Test::testTableFloatingMargins()
     // Paragraph bottom margin wasn't 0 in the A1 cell of the floating table.
     xmlDocPtr pXmlDoc = parseExport();
     assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:r/w:pict/v:rect/v:textbox/w:txbxContent/w:tbl/w:tr[1]/w:tc[1]/w:p/w:pPr/w:spacing", "after", "0");
+}
+
+void Test::testFdo44689_start_page_0()
+{
+    // The problem was that the import & export process did not analyze the 'start from page' attribute of a section
+    uno::Reference<beans::XPropertySet> xPara(getParagraph(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(0), getProperty<sal_Int16>(xPara, "PageNumberOffset"));
 }
 
 void Test::testFdo44689_start_page_7()

sw/source/core/fields/docufld.cxx
@@ -115,7 +115,7 @@ OUString SwPageNumberFieldType::Expand( sal_uInt32 nFmt, short nOff,
     sal_uInt32 nTmpFmt = (SVX_NUM_PAGEDESC == nFmt) ? (sal_uInt32)nNumberingType : nFmt;
     int const nTmp = nPageNumber + nOff;
 
-    if (0 >= nTmp || SVX_NUM_NUMBER_NONE == nTmpFmt || (!bVirtuell && nTmp > nMaxPage))
+    if (0 > nTmp || SVX_NUM_NUMBER_NONE == nTmpFmt || (!bVirtuell && nTmp > nMaxPage))
         return OUString();
 
     if( SVX_NUM_CHAR_SPECIAL == nTmpFmt )

sw/source/core/frmedt/fedesc.cxx
@@ -65,7 +65,7 @@ void SwFEShell::ChgCurPageDesc( const SwPageDesc& rDesc )
 
     SwPageFrm *pPage = GetCurrFrm()->FindPageFrm();
     const SwFrm *pFlow = 0;
-    sal_uInt16 nPageNmOffset = 0;
+    ::boost::optional<sal_uInt16> oPageNumOffset;
 
     OSL_ENSURE( !GetCrsr()->HasMark(), "ChgCurPageDesc only without selection!");
 
@@ -81,7 +81,7 @@ void SwFEShell::ChgCurPageDesc( const SwPageDesc& rDesc )
             if( rPgDesc.GetPageDesc() )
             {
                 // wir haben ihn den Schlingel
-                nPageNmOffset = rPgDesc.GetNumOffset();
+                oPageNumOffset = rPgDesc.GetNumOffset();
                 break;
             }
         }
@@ -101,7 +101,7 @@ void SwFEShell::ChgCurPageDesc( const SwPageDesc& rDesc )
 
     // use pagenumber
     SwFmtPageDesc aNew( &rDesc );
-    aNew.SetNumOffset( nPageNmOffset );
+    aNew.SetNumOffset( oPageNumOffset );
 
     if ( pFlow->IsInTab() )
         GetDoc()->SetAttr( aNew, *(SwFmt*)pFlow->FindTabFrm()->GetFmt() );

sw/source/core/frmedt/fews.cxx
@@ -427,9 +427,9 @@ sal_uInt16 SwFEShell::GetPageOffset() const
         {
             if ( pFlow->IsInTab() )
                 pFlow = pFlow->FindTabFrm();
-            const sal_uInt16 nOffset = pFlow->GetAttrSet()->GetPageDesc().GetNumOffset();
-            if ( nOffset )
-                return nOffset;
+            ::boost::optional<sal_uInt16> oNumOffset = pFlow->GetAttrSet()->GetPageDesc().GetNumOffset();
+            if ( oNumOffset )
+                return oNumOffset.get();
         }
         pPage = (SwPageFrm*)pPage->GetPrev();
     }

commit 013502bb6cb99b982377c6d3db4fdb9992b2c92b

sw: PVS V656 Variables initialized through call to same function
(regression from a470e43ae083dd3a1309d021cf14c2877dbfe855)

Change-Id: I78ac92869b0e9b56df708aa7d2d35b45bfb5f751

sw/source/core/crsr/findattr.cxx
@@ -65,8 +65,10 @@ bool CmpAttr( const SfxPoolItem& rItem1, const SfxPoolItem& rItem2 )
                                 static_cast<const SvxColorItem&>(rItem2).GetValue() );
     case RES_PAGEDESC:
         bool bNumOffsetEqual = false;
-        ::boost::optional<sal_uInt16> oNumOffset1 = static_cast<const SwFmtPageDesc&>(rItem1).GetNumOffset();
-        ::boost::optional<sal_uInt16> oNumOffset2 = static_cast<const SwFmtPageDesc&>(rItem1).GetNumOffset();
+        ::boost::optional<sal_uInt16> const oNumOffset1 =
+            static_cast<const SwFmtPageDesc&>(rItem1).GetNumOffset();
+        ::boost::optional<sal_uInt16> const oNumOffset2 =
+            static_cast<const SwFmtPageDesc&>(rItem2).GetNumOffset();
         if (!oNumOffset1 && !oNumOffset2)
         {
             bNumOffsetEqual = true;