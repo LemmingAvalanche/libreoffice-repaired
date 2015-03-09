LemmingAvalanche/libreoffice-repaired

commit d789e826a8b85c2ffc84ab180b4aea2e8e9dff19

cppcheck: Variable is assigned a value that is never used
Change-Id: Iab6eaf148af6219a7c60af23c56ee088f6438a63

dbaccess/source/ui/misc/HtmlReader.cxx
@@ -412,7 +412,6 @@ bool OHTMLReader::CreateTable(int nToken)
     aTempName = aTempName.getToken(0,' ');
     aTempName = ::dbtools::createUniqueName(m_xTables, aTempName);
 
-    int nTmpToken2 = nToken;
     bool bCaption = false;
     bool bTableHeader = false;
     OUString aColumnName;
@@ -423,7 +422,7 @@ bool OHTMLReader::CreateTable(int nToken)
     sal_Int32 nTextColor = 0;
     do
     {
-        switch(nTmpToken2)
+        switch(nToken)
         {
             case HTML_TEXTTOKEN:
             case HTML_SINGLECHAR:
@@ -492,7 +491,7 @@ bool OHTMLReader::CreateTable(int nToken)
                 break;
         }
     }
-    while((nTmpToken2 = GetNextToken()) != HTML_TABLEROW_OFF);
+    while( GetNextToken() != HTML_TABLEROW_OFF );
 
     if ( !m_sCurrent.isEmpty() )
         aColumnName = m_sCurrent;

dbaccess/source/ui/misc/RtfReader.cxx
@@ -253,13 +253,12 @@ bool ORTFReader::CreateTable(int nToken)
     aTableName = aTableName.getToken(0,' ');
     aTableName = ::dbtools::createUniqueName(m_xTables, aTableName);
 
-    int nTmpToken2 = nToken;
     OUString aColumnName;
 
     FontDescriptor aFont = VCLUnoHelper::CreateFontDescriptor(Application::GetSettings().GetStyleSettings().GetAppFont());
     do
     {
-        switch(nTmpToken2)
+        switch(nToken)
         {
             case RTF_UNKNOWNCONTROL:
             case RTF_UNKNOWNDATA:
@@ -303,7 +302,7 @@ bool ORTFReader::CreateTable(int nToken)
                 break;
         }
     }
-    while((nTmpToken2 = GetNextToken()) != RTF_TROWD && eState != SVPAR_ERROR && eState != SVPAR_ACCEPTED);
+    while( GetNextToken() != RTF_TROWD && eState != SVPAR_ERROR && eState != SVPAR_ACCEPTED );
 
     bool bOk = !m_vDestVector.empty();
     if(bOk)

chart2/source/controller/main/SelectionHelper.cxx
@@ -256,14 +256,13 @@ void Selection::adaptSelectionToNewPos( const Point& rMousePos, DrawViewWrapper*
                 bool bBackGroundHit = m_aSelectedOID.getObjectCID().equals( aPageCID ) || m_aSelectedOID.getObjectCID().equals( aWallCID ) || !m_aSelectedOID.isAutoGeneratedObject();
                 if( bBackGroundHit )
                 {
-                    //todo: if more than one diagram is available in future do chack the list of all diagrams here
+                    //todo: if more than one diagram is available in future do check the list of all diagrams here
                     SdrObject* pDiagram = pDrawViewWrapper->getNamedSdrObject( aDiagramCID );
                     if( pDiagram )
                     {
                         if( pDrawViewWrapper->IsObjectHit( pDiagram, rMousePos ) )
                         {
                             m_aSelectedOID = ObjectIdentifier( aDiagramCID );
-                            pNewObj = pDiagram;
                         }
                     }
                 }

commit 6cb4fa1a8d36da90e221af3667bcced94da98acc

coverity#1287398 Logically dead code
and

coverity#1287399 Logically dead code

regression from

commit d789e826a8b85c2ffc84ab180b4aea2e8e9dff19
Date:   Mon Mar 2 09:36:39 2015 +0200
    cppcheck: Variable is assigned a value that is never used

Change-Id: Iaee21f8c120a09aaeed2f55da16a7ef83bea2b07

dbaccess/source/ui/misc/HtmlReader.cxx
@@ -422,7 +422,7 @@ bool OHTMLReader::CreateTable(int nToken)
     sal_Int32 nTextColor = 0;
     do
     {
-        switch(nToken)
+        switch (nToken)
         {
             case HTML_TEXTTOKEN:
             case HTML_SINGLECHAR:
@@ -490,8 +490,9 @@ bool OHTMLReader::CreateTable(int nToken)
                 aFont.Strikeout = ::com::sun::star::awt::FontStrikeout::SINGLE;
                 break;
         }
+        nToken = GetNextToken();
     }
-    while( GetNextToken() != HTML_TABLEROW_OFF );
+    while (nToken != HTML_TABLEROW_OFF);
 
     if ( !m_sCurrent.isEmpty() )
         aColumnName = m_sCurrent;

dbaccess/source/ui/misc/RtfReader.cxx
@@ -258,7 +258,7 @@ bool ORTFReader::CreateTable(int nToken)
     FontDescriptor aFont = VCLUnoHelper::CreateFontDescriptor(Application::GetSettings().GetStyleSettings().GetAppFont());
     do
     {
-        switch(nToken)
+        switch (nToken)
         {
             case RTF_UNKNOWNCONTROL:
             case RTF_UNKNOWNDATA:
@@ -301,8 +301,9 @@ bool ORTFReader::CreateTable(int nToken)
                 aFont.Strikeout = ::com::sun::star::awt::FontStrikeout::SINGLE;
                 break;
         }
+        nToken = GetNextToken();
     }
-    while( GetNextToken() != RTF_TROWD && eState != SVPAR_ERROR && eState != SVPAR_ACCEPTED );
+    while(nToken != RTF_TROWD && eState != SVPAR_ERROR && eState != SVPAR_ACCEPTED);
 
     bool bOk = !m_vDestVector.empty();
     if(bOk)