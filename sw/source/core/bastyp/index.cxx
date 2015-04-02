LemmingAvalanche/libreoffice-repaired

commit 655b604ecc608bd3b1afecb3d51bb5e968e87dd4

SwIndex: remove global EmptyIndexArray

sw/source/core/bastyp/index.cxx
@@ -44,12 +44,6 @@ SwIndex::SwIndex(SwIndexReg *const pReg, xub_StrLen const nIdx)
     , m_pNext( 0 )
     , m_pPrev( 0 )
 {
-    if (!m_pIndexReg)
-    {
-        m_pIndexReg = &EmptyIndexArray::get();
-        m_nIndex = 0; // always 0 if no IndexReg
-    }
-
     Init(m_nIndex);
 }
 
@@ -72,7 +66,11 @@ SwIndex::SwIndex( const SwIndex& rIdx )
 
 void SwIndex::Init(xub_StrLen const nIdx)
 {
-    if (!m_pIndexReg->m_pFirst) // first Index?
+    if (!m_pIndexReg)
+    {
+        m_nIndex = 0; // always 0 if no IndexReg
+    }
+    else if (!m_pIndexReg->m_pFirst) // first Index?
     {
         OSL_ASSERT(!m_pIndexReg->m_pLast);
         m_pIndexReg->m_pFirst = m_pIndexReg->m_pLast = this;
@@ -91,6 +89,11 @@ void SwIndex::Init(xub_StrLen const nIdx)
 
 SwIndex& SwIndex::ChgValue( const SwIndex& rIdx, xub_StrLen nNewValue )
 {
+    OSL_ASSERT(m_pIndexReg == rIdx.m_pIndexReg);
+    if (!m_pIndexReg)
+    {
+        return *this; // no IndexReg => no list to sort into; m_nIndex is 0
+    }
     SwIndex* pFnd = const_cast<SwIndex*>(&rIdx);
     if (rIdx.m_nIndex > nNewValue) // move forwards
     {
@@ -146,7 +149,6 @@ SwIndex& SwIndex::ChgValue( const SwIndex& rIdx, xub_StrLen nNewValue )
         else
             m_pNext->m_pPrev = this;
     }
-    m_pIndexReg = rIdx.m_pIndexReg;
 
     if (m_pIndexReg->m_pFirst == m_pNext)
         m_pIndexReg->m_pFirst = this;
@@ -160,6 +162,12 @@ SwIndex& SwIndex::ChgValue( const SwIndex& rIdx, xub_StrLen nNewValue )
 
 void SwIndex::Remove()
 {
+    if (!m_pIndexReg)
+    {
+        OSL_ASSERT(!m_pPrev && !m_pNext);
+        return;
+    }
+
     if (m_pPrev)
     {
         m_pPrev->m_pNext = m_pNext;
@@ -205,12 +213,6 @@ SwIndex& SwIndex::operator=( const SwIndex& rIdx )
 *************************************************************************/
 SwIndex& SwIndex::Assign( SwIndexReg* pArr, xub_StrLen nIdx )
 {
-    if( !pArr )
-    {
-        pArr = &EmptyIndexArray::get();
-        nIdx = 0; // always 0 if no IndexReg
-    }
-
     if (pArr != m_pIndexReg) // unregister!
     {
         Remove();

sw/inc/index.hxx
@@ -145,9 +145,6 @@ public:
     void MoveTo( SwIndexReg& rArr );
 };
 
-// A global array for holding indices that need to be "swapped" temporarily or
-// do not know a valid array (SwPaM/SwPosition!):
-struct EmptyIndexArray: rtl::Static< SwIndexReg, EmptyIndexArray > {};
 
 #ifndef DBG_UTIL
 

sw/source/core/crsr/crsrsh.cxx
@@ -3156,7 +3156,7 @@ const SwStartNode* lcl_NodeContext( const SwNode& rNode )
 bool lcl_PosOk(const SwPosition & aPos)
 {
     return NULL != aPos.nNode.GetNode().GetCntntNode() &&
-           &EmptyIndexArray::get() != aPos.nContent.GetIdxReg();
+           aPos.nContent.GetIdxReg();
 }
 
 /**

commit 4bc2e72a61569548b9c990069e32a5141ebc828f

sw: fix crash on SwUndoDelete after select-all with table at start
Somehow the Redo leaves the shell cursor at an invalid offset into the
paragraph, which then leads to out-of-bounds string access.

Noticed that SwUndRng::SetPaM() leaves an invalid nContent.m_nIndex on
the start position, due to a surprising omission in SwIndex::ChgValue().

(regression from 655b604ecc608bd3b1afecb3d51bb5e968e87dd4)

Change-Id: I6e6ad7f70835d7e9d6da1fb680e2ae15469fad71

sw/source/core/bastyp/index.cxx
@@ -81,6 +81,7 @@ SwIndex& SwIndex::ChgValue( const SwIndex& rIdx, sal_Int32 nNewValue )
     assert(m_pIndexReg == rIdx.m_pIndexReg);
     if (!m_pIndexReg)
     {
+        m_nIndex = 0;
         return *this; // no IndexReg => no list to sort into; m_nIndex is 0
     }
     SwIndex* pFnd = const_cast<SwIndex*>(&rIdx);

sw/source/core/edit/eddel.cxx
@@ -99,6 +99,7 @@ void SwEditShell::DeleteSel( SwPaM& rPam, bool* pUndo )
             // Selection starts at the first para of the first cell, but we
             // want to delete the table node before the first cell as well.
             pNewPam->Start()->nNode = pNewPam->Start()->nNode.GetNode().FindTableNode()->GetIndex();
+            pNewPam->Start()->nContent.Assign(nullptr, 0);
             pPam = pNewPam.get();
         }
         // delete everything