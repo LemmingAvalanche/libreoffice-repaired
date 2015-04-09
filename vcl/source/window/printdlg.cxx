LibreOffice/core

manage VCL widgets using rtl::Reference
Change-Id: Ie95790cbaa5d459c8e849d7333098d857d31ed0a

vcl/source/window/printdlg.cxx
@@ -67,17 +67,17 @@ PrintDialog::PrintPreviewWindow::PrintPreviewWindow( vcl::Window* i_pParent )
     , maPageVDev( *this )
     , maToolTipString(VclResId( SV_PRINT_PRINTPREVIEW_TXT).toString())
     , mbGreyscale( false )
-    , maHorzDim( this, WB_HORZ | WB_CENTER  )
-    , maVertDim( this, WB_VERT | WB_VCENTER )
+    , maHorzDim(new FixedLine(this, WB_HORZ | WB_CENTER))
+    , maVertDim(new FixedLine(this, WB_VERT | WB_VCENTER))
 {
     SetPaintTransparent( true );
     SetBackground();
     maPageVDev.SetBackground( Color( COL_WHITE ) );
-    maHorzDim.Show();
-    maVertDim.Show();
+    maHorzDim->Show();
+    maVertDim->Show();
 
-    maHorzDim.SetText( OUString( "2.0in" ) );
-    maVertDim.SetText( OUString( "2.0in" ) );
+    maHorzDim->SetText( OUString( "2.0in" ) );
+    maVertDim->SetText( OUString( "2.0in" ) );
 }
 
 PrintDialog::PrintPreviewWindow::~PrintPreviewWindow()
@@ -99,7 +99,7 @@ void PrintDialog::PrintPreviewWindow::DataChanged( const DataChangedEvent& i_rDC
 void PrintDialog::PrintPreviewWindow::Resize()
 {
     Size aNewSize( GetSizePixel() );
-    long nTextHeight = maHorzDim.GetTextHeight();
+    long nTextHeight = maHorzDim->GetTextHeight();
     // leave small space for decoration
     aNewSize.Width() -= nTextHeight + 2;
     aNewSize.Height() -= nTextHeight + 2;
@@ -145,16 +145,16 @@ void PrintDialog::PrintPreviewWindow::Resize()
     // position dimension lines
     Point aRef( nTextHeight + (aNewSize.Width() - maPreviewSize.Width())/2,
                 nTextHeight + (aNewSize.Height() - maPreviewSize.Height())/2 );
-    maHorzDim.SetPosSizePixel( Point( aRef.X(), aRef.Y() - nTextHeight ),
+    maHorzDim->SetPosSizePixel( Point( aRef.X(), aRef.Y() - nTextHeight ),
                                Size( maPreviewSize.Width(), nTextHeight ) );
-    maVertDim.SetPosSizePixel( Point( aRef.X() - nTextHeight, aRef.Y() ),
+    maVertDim->SetPosSizePixel( Point( aRef.X() - nTextHeight, aRef.Y() ),
                                Size( nTextHeight, maPreviewSize.Height() ) );
 
 }
 
 void PrintDialog::PrintPreviewWindow::Paint( const Rectangle& )
 {
-    long nTextHeight = maHorzDim.GetTextHeight();
+    long nTextHeight = maHorzDim->GetTextHeight();
     Size aSize( GetSizePixel() );
     Point aOffset( (aSize.Width()  - maPreviewSize.Width()  + nTextHeight) / 2 ,
                    (aSize.Height() - maPreviewSize.Height() + nTextHeight) / 2 );
@@ -241,17 +241,24 @@ void PrintDialog::PrintPreviewWindow::setPreview( const GDIMetaFile& i_rNewPrevi
         aBuf.append( i_rPaperName );
         aBuf.append( ')' );
     }
-    maHorzDim.SetText( aBuf.makeStringAndClear() );
+    maHorzDim->SetText( aBuf.makeStringAndClear() );
 
     aNumText = rLocWrap.getNum( aLogicPaperSize.Height(), nDigits );
     aBuf.append( aNumText )
         .append( sal_Unicode( ' ' ) );
     aBuf.appendAscii( eUnit == MAP_MM ? "mm" : "in" );
-    maVertDim.SetText( aBuf.makeStringAndClear() );
+    maVertDim->SetText( aBuf.makeStringAndClear() );
 
     Resize();
     preparePreviewBitmap();
     Invalidate();
+}
+
+void PrintDialog::PrintPreviewWindow::dispose()
+{
+    maHorzDim.clear();
+    maVertDim.clear();
+    Window::dispose();
 }
 
 void PrintDialog::PrintPreviewWindow::preparePreviewBitmap()

vcl/workben/svpclient.cxx
@@ -92,10 +92,10 @@ SAL_IMPLEMENT_MAIN()
 
 class MyWin : public WorkWindow
 {
-    PushButton      m_aListButton;
-    ListBox         m_aSvpBitmaps;
-    ImageControl    m_aImage;
-    PushButton      m_aQuitButton;
+    PushButtonPtr   m_aListButton;
+    ListBoxPtr      m_aSvpBitmaps;
+    ImageControlPtr m_aImage;
+    PushButtonPtr   m_aQuitButton;
 public:
                  MyWin( vcl::Window* pParent, WinBits nWinStyle );
 
@@ -108,6 +108,7 @@ public:
     virtual void Resize() SAL_OVERRIDE;
 
     virtual bool Close() SAL_OVERRIDE;
+    virtual void dispose() SAL_OVERRIDE;
 
     void parseList( const OString& rList );
     OString processCommand( const OString& rCommand );
@@ -128,28 +129,28 @@ void Main()
 
 MyWin::MyWin( vcl::Window* pParent, WinBits nWinStyle ) :
     WorkWindow( pParent, nWinStyle ),
-    m_aListButton( this, 0 ),
-    m_aSvpBitmaps( this, WB_BORDER ),
-    m_aImage( this, WB_BORDER ),
-    m_aQuitButton( this, 0 )
+    m_aListButton(new PushButton(this, 0)),
+    m_aSvpBitmaps(new ListBox(this, WB_BORDER)),
+    m_aImage(new ImageControl(this, WB_BORDER)),
+    m_aQuitButton(new PushButton(this, 0))
 {
-    m_aListButton.SetPosSizePixel( Point( 10, 10 ), Size( 120, 25 ) );
-    m_aListButton.SetText( OUString( "List Elements" ) );
-    m_aListButton.SetClickHdl( LINK( this, MyWin, ListHdl ) );
-    m_aListButton.Show();
+    m_aListButton->SetPosSizePixel( Point( 10, 10 ), Size( 120, 25 ) );
+    m_aListButton->SetText( OUString( "List Elements" ) );
+    m_aListButton->SetClickHdl( LINK( this, MyWin, ListHdl ) );
+    m_aListButton->Show();
 
-    m_aSvpBitmaps.SetPosSizePixel( Point( 10, 40 ), Size( 150, 150 ) );
-    m_aSvpBitmaps.SetSelectHdl( LINK( this, MyWin, SelectHdl ) );
-    m_aSvpBitmaps.Show();
+    m_aSvpBitmaps->SetPosSizePixel( Point( 10, 40 ), Size( 150, 150 ) );
+    m_aSvpBitmaps->SetSelectHdl( LINK( this, MyWin, SelectHdl ) );
+    m_aSvpBitmaps->Show();
 
-    m_aImage.SetPosSizePixel( Point( 170, 10 ), Size( 400, 400 ) );
-    m_aImage.SetScaleMode( com::sun::star::awt::ImageScaleMode::NONE );
-    m_aImage.Show();
+    m_aImage->SetPosSizePixel( Point( 170, 10 ), Size( 400, 400 ) );
+    m_aImage->SetScaleMode( com::sun::star::awt::ImageScaleMode::NONE );
+    m_aImage->Show();
 
-    m_aQuitButton.SetPosSizePixel( Point( 10, 300 ), Size( 120,25 ) );
-    m_aQuitButton.SetText( OUString( "Quit SVP server" ) );
-    m_aQuitButton.SetClickHdl( LINK( this, MyWin, QuitHdl ) );
-    m_aQuitButton.Show();
+    m_aQuitButton->SetPosSizePixel( Point( 10, 300 ), Size( 120,25 ) );
+    m_aQuitButton->SetText( OUString( "Quit SVP server" ) );
+    m_aQuitButton->SetClickHdl( LINK( this, MyWin, QuitHdl ) );
+    m_aQuitButton->Show();
 }
 
 bool MyWin::Close()
@@ -160,11 +161,20 @@ bool MyWin::Close()
     return bRet;
 }
 
+void MyWin::dispose()
+{
+    m_aListButton.clear();
+    m_aSvpBitmaps.clear();
+    m_aImage.clear();
+    m_aQuitButton.clear();
+    WorkWindow::dispose();
+}
+
 void MyWin::parseList( const OString& rList )
 {
     sal_Int32 nTokenPos = 0;
     OUString aElementType;
-    m_aSvpBitmaps.Clear();
+    m_aSvpBitmaps->Clear();
     while( nTokenPos >= 0 )
     {
         OString aLine = rList.getToken( 0, '\n', nTokenPos );
@@ -179,7 +189,7 @@ void MyWin::parseList( const OString& rList )
             aNewElement.append( aElementType );
             aNewElement.appendAscii( ": " );
             aNewElement.append( OStringToOUString( aLine, RTL_TEXTENCODING_ASCII_US ) );
-            m_aSvpBitmaps.InsertEntry( aNewElement.makeStringAndClear() );
+            m_aSvpBitmaps->InsertEntry( aNewElement.makeStringAndClear() );
         }
     }
 }
@@ -235,7 +245,7 @@ IMPL_LINK( MyWin, QuitHdl, Button*, )
 
 IMPL_LINK( MyWin, SelectHdl, ListBox*, )
 {
-    OUString aEntry = m_aSvpBitmaps.GetSelectEntry();
+    OUString aEntry = m_aSvpBitmaps->GetSelectEntry();
     sal_Int32 nPos = aEntry.indexOf( ": " );
     if( nPos != -1 )
     {
@@ -259,8 +269,8 @@ IMPL_LINK( MyWin, SelectHdl, ListBox*, )
         Size aFixedSize( aBitmap.GetSizePixel() );
         aFixedSize.Width() += 10;
         aFixedSize.Height() += 10;
-        m_aImage.SetSizePixel( aFixedSize );
-        m_aImage.SetImage( Image( BitmapEx( aBitmap ) ) );
+        m_aImage->SetSizePixel( aFixedSize );
+        m_aImage->SetImage( Image( BitmapEx( aBitmap ) ) );
     }
     return 0;
 }

LemmingAvalanche/libreoffice-repaired

vcl/source/window/printdlg.cxx
@@ -67,17 +67,17 @@ PrintDialog::PrintPreviewWindow::PrintPreviewWindow( vcl::Window* i_pParent )
     , maPageVDev( *this )
     , maToolTipString(VclResId( SV_PRINT_PRINTPREVIEW_TXT).toString())
     , mbGreyscale( false )
-    , maHorzDim( this, WB_HORZ | WB_CENTER  )
-    , maVertDim( this, WB_VERT | WB_VCENTER )
+    , maHorzDim(new FixedLine(this, WB_HORZ | WB_CENTER))
+    , maVertDim(new FixedLine(this, WB_VERT | WB_VCENTER))
 {
     SetPaintTransparent( true );
     SetBackground();
     maPageVDev.SetBackground( Color( COL_WHITE ) );
-    maHorzDim.Show();
-    maVertDim.Show();
+    maHorzDim->Show();
+    maVertDim->Show();
 
-    maHorzDim.SetText( OUString( "2.0in" ) );
-    maVertDim.SetText( OUString( "2.0in" ) );
+    maHorzDim->SetText( OUString( "2.0in" ) );
+    maVertDim->SetText( OUString( "2.0in" ) );
 }
 
 PrintDialog::PrintPreviewWindow::~PrintPreviewWindow()
@@ -99,7 +99,7 @@ void PrintDialog::PrintPreviewWindow::DataChanged( const DataChangedEvent& i_rDC
 void PrintDialog::PrintPreviewWindow::Resize()
 {
     Size aNewSize( GetSizePixel() );
-    long nTextHeight = maHorzDim.GetTextHeight();
+    long nTextHeight = maHorzDim->GetTextHeight();
     // leave small space for decoration
     aNewSize.Width() -= nTextHeight + 2;
     aNewSize.Height() -= nTextHeight + 2;
@@ -145,16 +145,16 @@ void PrintDialog::PrintPreviewWindow::Resize()
     // position dimension lines
     Point aRef( nTextHeight + (aNewSize.Width() - maPreviewSize.Width())/2,
                 nTextHeight + (aNewSize.Height() - maPreviewSize.Height())/2 );
-    maHorzDim.SetPosSizePixel( Point( aRef.X(), aRef.Y() - nTextHeight ),
+    maHorzDim->SetPosSizePixel( Point( aRef.X(), aRef.Y() - nTextHeight ),
                                Size( maPreviewSize.Width(), nTextHeight ) );
-    maVertDim.SetPosSizePixel( Point( aRef.X() - nTextHeight, aRef.Y() ),
+    maVertDim->SetPosSizePixel( Point( aRef.X() - nTextHeight, aRef.Y() ),
                                Size( nTextHeight, maPreviewSize.Height() ) );
 
 }
 
 void PrintDialog::PrintPreviewWindow::Paint( const Rectangle& )
 {
-    long nTextHeight = maHorzDim.GetTextHeight();
+    long nTextHeight = maHorzDim->GetTextHeight();
     Size aSize( GetSizePixel() );
     Point aOffset( (aSize.Width()  - maPreviewSize.Width()  + nTextHeight) / 2 ,
                    (aSize.Height() - maPreviewSize.Height() + nTextHeight) / 2 );
@@ -241,17 +241,24 @@ void PrintDialog::PrintPreviewWindow::setPreview( const GDIMetaFile& i_rNewPrevi
         aBuf.append( i_rPaperName );
         aBuf.append( ')' );
     }
-    maHorzDim.SetText( aBuf.makeStringAndClear() );
+    maHorzDim->SetText( aBuf.makeStringAndClear() );
 
     aNumText = rLocWrap.getNum( aLogicPaperSize.Height(), nDigits );
     aBuf.append( aNumText )
         .append( sal_Unicode( ' ' ) );
     aBuf.appendAscii( eUnit == MAP_MM ? "mm" : "in" );
-    maVertDim.SetText( aBuf.makeStringAndClear() );
+    maVertDim->SetText( aBuf.makeStringAndClear() );
 
     Resize();
     preparePreviewBitmap();
     Invalidate();
+}
+
+void PrintDialog::PrintPreviewWindow::dispose()
+{
+    maHorzDim.disposeAndClear();
+    maVertDim.disposeAndClear();
+    Window::dispose();
 }
 
 void PrintDialog::PrintPreviewWindow::preparePreviewBitmap()

vcl/workben/svpclient.cxx
@@ -92,10 +92,10 @@ SAL_IMPLEMENT_MAIN()
 
 class MyWin : public WorkWindow
 {
-    PushButton      m_aListButton;
-    ListBox         m_aSvpBitmaps;
-    ImageControl    m_aImage;
-    PushButton      m_aQuitButton;
+    PushButtonPtr   m_aListButton;
+    ListBoxPtr      m_aSvpBitmaps;
+    ImageControlPtr m_aImage;
+    PushButtonPtr   m_aQuitButton;
 public:
                  MyWin( vcl::Window* pParent, WinBits nWinStyle );
 
@@ -108,6 +108,7 @@ public:
     virtual void Resize() SAL_OVERRIDE;
 
     virtual bool Close() SAL_OVERRIDE;
+    virtual void dispose() SAL_OVERRIDE;
 
     void parseList( const OString& rList );
     OString processCommand( const OString& rCommand );
@@ -128,28 +129,28 @@ void Main()
 
 MyWin::MyWin( vcl::Window* pParent, WinBits nWinStyle ) :
     WorkWindow( pParent, nWinStyle ),
-    m_aListButton( this, 0 ),
-    m_aSvpBitmaps( this, WB_BORDER ),
-    m_aImage( this, WB_BORDER ),
-    m_aQuitButton( this, 0 )
+    m_aListButton(new PushButton(this, 0)),
+    m_aSvpBitmaps(new ListBox(this, WB_BORDER)),
+    m_aImage(new ImageControl(this, WB_BORDER)),
+    m_aQuitButton(new PushButton(this, 0))
 {
-    m_aListButton.SetPosSizePixel( Point( 10, 10 ), Size( 120, 25 ) );
-    m_aListButton.SetText( OUString( "List Elements" ) );
-    m_aListButton.SetClickHdl( LINK( this, MyWin, ListHdl ) );
-    m_aListButton.Show();
+    m_aListButton->SetPosSizePixel( Point( 10, 10 ), Size( 120, 25 ) );
+    m_aListButton->SetText( OUString( "List Elements" ) );
+    m_aListButton->SetClickHdl( LINK( this, MyWin, ListHdl ) );
+    m_aListButton->Show();
 
-    m_aSvpBitmaps.SetPosSizePixel( Point( 10, 40 ), Size( 150, 150 ) );
-    m_aSvpBitmaps.SetSelectHdl( LINK( this, MyWin, SelectHdl ) );
-    m_aSvpBitmaps.Show();
+    m_aSvpBitmaps->SetPosSizePixel( Point( 10, 40 ), Size( 150, 150 ) );
+    m_aSvpBitmaps->SetSelectHdl( LINK( this, MyWin, SelectHdl ) );
+    m_aSvpBitmaps->Show();
 
-    m_aImage.SetPosSizePixel( Point( 170, 10 ), Size( 400, 400 ) );
-    m_aImage.SetScaleMode( com::sun::star::awt::ImageScaleMode::NONE );
-    m_aImage.Show();
+    m_aImage->SetPosSizePixel( Point( 170, 10 ), Size( 400, 400 ) );
+    m_aImage->SetScaleMode( com::sun::star::awt::ImageScaleMode::NONE );
+    m_aImage->Show();
 
-    m_aQuitButton.SetPosSizePixel( Point( 10, 300 ), Size( 120,25 ) );
-    m_aQuitButton.SetText( OUString( "Quit SVP server" ) );
-    m_aQuitButton.SetClickHdl( LINK( this, MyWin, QuitHdl ) );
-    m_aQuitButton.Show();
+    m_aQuitButton->SetPosSizePixel( Point( 10, 300 ), Size( 120,25 ) );
+    m_aQuitButton->SetText( OUString( "Quit SVP server" ) );
+    m_aQuitButton->SetClickHdl( LINK( this, MyWin, QuitHdl ) );
+    m_aQuitButton->Show();
 }
 
 bool MyWin::Close()
@@ -160,11 +161,20 @@ bool MyWin::Close()
     return bRet;
 }
 
+void MyWin::dispose()
+{
+    m_aListButton.disposeAndClear();
+    m_aSvpBitmaps.disposeAndClear();
+    m_aImage.disposeAndClear();
+    m_aQuitButton.disposeAndClear();
+    WorkWindow::dispose();
+}
+
 void MyWin::parseList( const OString& rList )
 {
     sal_Int32 nTokenPos = 0;
     OUString aElementType;
-    m_aSvpBitmaps.Clear();
+    m_aSvpBitmaps->Clear();
     while( nTokenPos >= 0 )
     {
         OString aLine = rList.getToken( 0, '\n', nTokenPos );
@@ -179,7 +189,7 @@ void MyWin::parseList( const OString& rList )
             aNewElement.append( aElementType );
             aNewElement.appendAscii( ": " );
             aNewElement.append( OStringToOUString( aLine, RTL_TEXTENCODING_ASCII_US ) );
-            m_aSvpBitmaps.InsertEntry( aNewElement.makeStringAndClear() );
+            m_aSvpBitmaps->InsertEntry( aNewElement.makeStringAndClear() );
         }
     }
 }
@@ -235,7 +245,7 @@ IMPL_LINK( MyWin, QuitHdl, Button*, )
 
 IMPL_LINK( MyWin, SelectHdl, ListBox*, )
 {
-    OUString aEntry = m_aSvpBitmaps.GetSelectEntry();
+    OUString aEntry = m_aSvpBitmaps->GetSelectEntry();
     sal_Int32 nPos = aEntry.indexOf( ": " );
     if( nPos != -1 )
     {
@@ -259,8 +269,8 @@ IMPL_LINK( MyWin, SelectHdl, ListBox*, )
         Size aFixedSize( aBitmap.GetSizePixel() );
         aFixedSize.Width() += 10;
         aFixedSize.Height() += 10;
-        m_aImage.SetSizePixel( aFixedSize );
-        m_aImage.SetImage( Image( BitmapEx( aBitmap ) ) );
+        m_aImage->SetSizePixel( aFixedSize );
+        m_aImage->SetImage( Image( BitmapEx( aBitmap ) ) );
     }
     return 0;
 }