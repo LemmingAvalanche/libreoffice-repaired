chidelmun/core

vclptr: switch printers away from boost_shared.
Also continue to move to extended toolkit/ API.

Change-Id: Ibd4337b014ee096c84d0ad4435e2c5de1b258afe

vcl/generic/print/genprnpsp.cxx
@@ -1078,7 +1078,7 @@ bool PspSalPrinter::StartJob( const OUString* i_pFileName, const OUString& i_rJo
 
     boost::shared_ptr<vcl::PDFWriter> pWriter;
     std::vector< PDFPrintFile > aPDFFiles;
-    boost::shared_ptr<Printer> pPrinter( i_rController.getPrinter() );
+    VclPtr<Printer> pPrinter( i_rController.getPrinter() );
     int nAllPages = i_rController.getFilteredPageCount();
     i_rController.createProgressDialog();
     bool bAborted = false;

vcl/source/gdi/oldprintadaptor.cxx
@@ -44,7 +44,7 @@ using namespace com::sun::star;
 using namespace com::sun::star::uno;
 using namespace com::sun::star::beans;
 
-OldStylePrintAdaptor::OldStylePrintAdaptor( const boost::shared_ptr< Printer >& i_pPrinter )
+OldStylePrintAdaptor::OldStylePrintAdaptor( const VclPtr< Printer >& i_pPrinter )
     : PrinterController( i_pPrinter )
     , mpData( new ImplOldStyleAdaptorData() )
 {
@@ -64,7 +64,7 @@ void OldStylePrintAdaptor::StartPage()
     getPrinter()->SetConnectMetaFile( &mpData->maPages.back().maPage );
 
     // copy state into metafile
-    boost::shared_ptr<Printer> pPrinter( getPrinter() );
+    VclPtr<Printer> pPrinter( getPrinter() );
     pPrinter->SetMapMode( pPrinter->GetMapMode() );
     pPrinter->SetFont( pPrinter->GetFont() );
     pPrinter->SetDrawMode( pPrinter->GetDrawMode() );

vcl/source/gdi/print3.cxx
@@ -140,7 +140,7 @@ public:
     typedef boost::unordered_map< OUString, ControlDependency, OUStringHash > ControlDependencyMap;
     typedef boost::unordered_map< OUString, Sequence< sal_Bool >, OUStringHash > ChoiceDisableMap;
 
-    boost::shared_ptr<Printer>                                  mpPrinter;
+    VclPtr< Printer >                                           mpPrinter;
     Sequence< PropertyValue >                                   maUIOptions;
     std::vector< PropertyValue >                                maUIProperties;
     std::vector< bool >                                         maUIPropertyEnabled;
@@ -206,7 +206,7 @@ public:
     void resetPaperToLastConfigured();
 };
 
-PrinterController::PrinterController( const boost::shared_ptr<Printer>& i_pPrinter )
+PrinterController::PrinterController( const VclPtr<Printer>& i_pPrinter )
     : mpImplData( new ImplPrinterControllerData )
 {
     mpImplData->mpPrinter = i_pPrinter;
@@ -326,7 +326,7 @@ void Printer::ImplPrintJob( const boost::shared_ptr<PrinterController>& i_pContr
     if( ! pController->getPrinter() )
     {
         OUString aPrinterName( i_rInitSetup.GetPrinterName() );
-        boost::shared_ptr<Printer> pPrinter( new Printer( aPrinterName ) );
+        VclPtr<Printer> pPrinter( new Printer( aPrinterName ) );
         pPrinter->SetJobSetup( i_rInitSetup );
         pController->setPrinter( pPrinter );
     }
@@ -763,12 +763,12 @@ void PrinterController::setJobState( view::PrintableState i_eState )
     mpImplData->meJobState = i_eState;
 }
 
-const boost::shared_ptr<Printer>& PrinterController::getPrinter() const
+const VclPtr<Printer>& PrinterController::getPrinter() const
 {
     return mpImplData->mpPrinter;
 }
 
-void PrinterController::setPrinter( const boost::shared_ptr<Printer>& i_rPrinter )
+void PrinterController::setPrinter( const VclPtr<Printer>& i_rPrinter )
 {
     mpImplData->mpPrinter = i_rPrinter;
     setValue( OUString( "Name" ),

LemmingAvalanche/libreoffice-repaired

vcl/generic/print/genprnpsp.cxx
@@ -1079,7 +1079,7 @@ bool PspSalPrinter::StartJob( const OUString* i_pFileName, const OUString& i_rJo
 
     std::shared_ptr<vcl::PDFWriter> xWriter;
     std::vector< PDFPrintFile > aPDFFiles;
-    std::shared_ptr<Printer> xPrinter(i_rController.getPrinter());
+    VclPtr<Printer> xPrinter( i_rController.getPrinter() );
     int nAllPages = i_rController.getFilteredPageCount();
     i_rController.createProgressDialog();
     bool bAborted = false;

vcl/source/gdi/oldprintadaptor.cxx
@@ -44,8 +44,8 @@ using namespace com::sun::star;
 using namespace com::sun::star::uno;
 using namespace com::sun::star::beans;
 
-OldStylePrintAdaptor::OldStylePrintAdaptor(const std::shared_ptr< Printer >& i_xPrinter)
-    : PrinterController(i_xPrinter)
+OldStylePrintAdaptor::OldStylePrintAdaptor( const VclPtr< Printer >& i_xPrinter )
+    : PrinterController( i_xPrinter )
     , mpData( new ImplOldStyleAdaptorData() )
 {
 }
@@ -64,7 +64,7 @@ void OldStylePrintAdaptor::StartPage()
     getPrinter()->SetConnectMetaFile( &mpData->maPages.back().maPage );
 
     // copy state into metafile
-    std::shared_ptr<Printer> xPrinter(getPrinter());
+    VclPtr<Printer> xPrinter( getPrinter() );
     xPrinter->SetMapMode(xPrinter->GetMapMode());
     xPrinter->SetFont(xPrinter->GetFont());
     xPrinter->SetDrawMode(xPrinter->GetDrawMode());

vcl/source/gdi/print3.cxx
@@ -140,7 +140,7 @@ public:
     typedef std::unordered_map< OUString, ControlDependency, OUStringHash > ControlDependencyMap;
     typedef std::unordered_map< OUString, Sequence< sal_Bool >, OUStringHash > ChoiceDisableMap;
 
-    std::shared_ptr<Printer>                                    mxPrinter;
+    VclPtr< Printer >                                           mxPrinter;
     Sequence< PropertyValue >                                   maUIOptions;
     std::vector< PropertyValue >                                maUIProperties;
     std::vector< bool >                                         maUIPropertyEnabled;
@@ -206,7 +206,7 @@ public:
     void resetPaperToLastConfigured();
 };
 
-PrinterController::PrinterController(const std::shared_ptr<Printer>& i_xPrinter)
+PrinterController::PrinterController( const VclPtr<Printer>& i_xPrinter )
     : mpImplData( new ImplPrinterControllerData )
 {
     mpImplData->mxPrinter = i_xPrinter;
@@ -319,7 +319,7 @@ void Printer::PreparePrintJob(std::shared_ptr<PrinterController> xController,
     if (!xController->getPrinter())
     {
         OUString aPrinterName( i_rInitSetup.GetPrinterName() );
-        std::shared_ptr<Printer> xPrinter(std::make_shared<Printer>(aPrinterName));
+        VclPtr<Printer> xPrinter( new Printer( aPrinterName ) );
         xPrinter->SetJobSetup(i_rInitSetup);
         xController->setPrinter(xPrinter);
     }
@@ -770,12 +770,12 @@ void PrinterController::setJobState( view::PrintableState i_eState )
     mpImplData->meJobState = i_eState;
 }
 
-const std::shared_ptr<Printer>& PrinterController::getPrinter() const
+const VclPtr<Printer>& PrinterController::getPrinter() const
 {
     return mpImplData->mxPrinter;
 }
 
-void PrinterController::setPrinter(const std::shared_ptr<Printer>& i_rPrinter)
+void PrinterController::setPrinter( const VclPtr<Printer>& i_rPrinter )
 {
     mpImplData->mxPrinter = i_rPrinter;
     setValue( OUString( "Name" ),