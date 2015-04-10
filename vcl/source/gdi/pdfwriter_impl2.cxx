bihai/core

automated VclPtrInstance conversion.
Change-Id: I9a1d47202e2794461f6ec44f3e72ee1dd2fde09d

vcl/source/gdi/pdfwriter_impl2.cxx
@@ -431,7 +431,7 @@ void PDFWriterImpl::playMetafile( const GDIMetaFile& i_rMtf, vcl::PDFExtOutDevDa
                         if ( nPixelX && nPixelY )
                         {
                             Size aDstSizePixel( nPixelX, nPixelY );
-                            ScopedVclPtr<VirtualDevice> pVDev(new VirtualDevice);
+                            ScopedVclPtrInstance< VirtualDevice > pVDev;
                             if( pVDev->SetOutputSizePixel( aDstSizePixel ) )
                             {
                                 Bitmap          aPaint, aMask;

vcl/source/outdev/transparent.cxx
@@ -431,7 +431,7 @@ void OutputDevice::EmulateDrawTransparent ( const tools::PolyPolygon& rPolyPoly,
 
         if( !bDrawn )
         {
-            ScopedVclPtr<VirtualDevice> aVDev( new VirtualDevice( *this, 1 ) );
+            ScopedVclPtrInstance< VirtualDevice > aVDev(  *this, 1  );
             const Size aDstSz( aDstRect.GetSize() );
             const sal_uInt8 cTrans = (sal_uInt8) MinMax( FRound( nTransparencePercent * 2.55 ), 0, 255 );
 
@@ -682,7 +682,7 @@ void OutputDevice::DrawTransparent( const GDIMetaFile& rMtf, const Point& rPos,
 
         if( !aDstRect.IsEmpty() )
         {
-            ScopedVclPtr<VirtualDevice> pVDev(new VirtualDevice);
+            ScopedVclPtrInstance< VirtualDevice > pVDev;
 
             ((OutputDevice*)pVDev.get())->mnDPIX = mnDPIX;
             ((OutputDevice*)pVDev.get())->mnDPIY = mnDPIY;

LemmingAvalanche/libreoffice-repaired

vcl/source/gdi/pdfwriter_impl2.cxx
@@ -431,7 +431,7 @@ void PDFWriterImpl::playMetafile( const GDIMetaFile& i_rMtf, vcl::PDFExtOutDevDa
                         if ( nPixelX && nPixelY )
                         {
                             Size aDstSizePixel( nPixelX, nPixelY );
-                            ScopedVclPtr<VirtualDevice> xVDev(new VirtualDevice);
+                            ScopedVclPtrInstance<VirtualDevice> xVDev;
                             if( xVDev->SetOutputSizePixel( aDstSizePixel ) )
                             {
                                 Bitmap          aPaint, aMask;

vcl/source/outdev/transparent.cxx
@@ -431,7 +431,7 @@ void OutputDevice::EmulateDrawTransparent ( const tools::PolyPolygon& rPolyPoly,
 
         if( !bDrawn )
         {
-            ScopedVclPtr<VirtualDevice> aVDev( new VirtualDevice( *this, 1 ) );
+            ScopedVclPtrInstance< VirtualDevice > aVDev(  *this, 1  );
             const Size aDstSz( aDstRect.GetSize() );
             const sal_uInt8 cTrans = (sal_uInt8) MinMax( FRound( nTransparencePercent * 2.55 ), 0, 255 );
 
@@ -682,7 +682,7 @@ void OutputDevice::DrawTransparent( const GDIMetaFile& rMtf, const Point& rPos,
 
         if( !aDstRect.IsEmpty() )
         {
-            ScopedVclPtrInstance<VirtualDevice> xVDev;
+            ScopedVclPtrInstance< VirtualDevice > xVDev;
 
             ((OutputDevice*)xVDev.get())->mnDPIX = mnDPIX;
             ((OutputDevice*)xVDev.get())->mnDPIY = mnDPIY;