LemmingAvalanche/libreoffice-repaired

commit 49d93d854111791c0fe00155463e5d86faaef483

loplugin:staticmethods
Change-Id: Id7fc12bc61c6e803f45cc60556d9cf54ac2e529f

extensions/source/scanner/scanner.hxx
@@ -47,8 +47,8 @@ protected:
     osl::Mutex                              maProtector;
     void*                                   mpData;
 
-    void                                    AcquireData();
-    void                                    ReleaseData();
+    static void                             AcquireData();
+    static void                             ReleaseData();
 
 public:
 

basctl/source/basicide/basicbox.hxx
@@ -79,7 +79,7 @@ private:
     bool            bFillBox;
     com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame > m_xFrame;
 
-    void            ReleaseFocus();
+    static void     ReleaseFocus();
     void            InsertEntries( const ScriptDocument& rDocument, LibraryLocation eLocation );
 
     void            ClearBox();

basctl/source/basicide/baside2.hxx
@@ -311,7 +311,7 @@ private:
     void                CheckCompileBasic();
     bool                BasicExecute();
 
-    void                GoOnTop();
+    static void         GoOnTop();
 
     sal_Int32           FormatAndPrint( Printer* pPrinter, sal_Int32 nPage = -1 );
     SbModuleRef         XModule();

basctl/source/basicide/baside3.cxx
@@ -1346,7 +1346,7 @@ sal_Int32 DialogWindow::countPages( Printer* )
 
 void DialogWindow::printPage( sal_Int32 nPage, Printer* pPrinter )
 {
-    pEditor->printPage( nPage, pPrinter, CreateQualifiedName() );
+    DlgEditor::printPage( nPage, pPrinter, CreateQualifiedName() );
 }
 
 void DialogWindow::DataChanged( const DataChangedEvent& rDCEvt )

basctl/source/basicide/bastype2.hxx
@@ -225,7 +225,7 @@ public:
 
     EntryDescriptor GetEntryDescriptor( SvTreeListEntry* pEntry );
 
-    ItemType        ConvertType (EntryType eType);
+    static ItemType ConvertType (EntryType eType);
     bool            IsValidEntry( SvTreeListEntry* pEntry );
 
     SvTreeListEntry*    AddEntry(
@@ -236,8 +236,8 @@ public:
     void            RemoveEntry (SvTreeListEntry*);
     void            RemoveEntry (ScriptDocument const&);
 
-    OUString GetRootEntryName( const ScriptDocument& rDocument, LibraryLocation eLocation ) const;
-    void            GetRootEntryBitmaps( const ScriptDocument& rDocument, Image& rImage );
+    OUString        GetRootEntryName( const ScriptDocument& rDocument, LibraryLocation eLocation ) const;
+    static void     GetRootEntryBitmaps( const ScriptDocument& rDocument, Image& rImage );
 
     void            SetCurrentEntry (EntryDescriptor&);
 

basctl/source/basicide/macrodlg.hxx
@@ -79,7 +79,7 @@ private:
 
     void                EnableButton( Button& rButton, bool bEnable );
 
-    OUString            GetInfo( SbxVariable* pVar );
+    static OUString     GetInfo( SbxVariable* pVar );
 
     void                StoreMacroDescription();
     void                RestoreMacroDescription();

basctl/source/basicide/moduldlg.cxx
@@ -695,7 +695,7 @@ IMPL_LINK( ObjectPage, ButtonHdl, Button *, pButton )
                     aModName = aModName.getToken( 0, ' ', nIndex );
                 }
                 SbxItem aSbxItem( SID_BASICIDE_ARG_SBX, aDesc.GetDocument(), aDesc.GetLibName(),
-                                  aModName, m_pBasicBox->ConvertType( aDesc.GetType() ) );
+                                  aModName, TreeListBox::ConvertType( aDesc.GetType() ) );
                 pDispatcher->Execute( SID_BASICIDE_SHOWSBX, SfxCallMode::SYNCHRON, &aSbxItem, 0L );
             }
         }
@@ -876,7 +876,7 @@ void ObjectPage::DeleteCurrent()
             m_pBasicBox->Select( m_pBasicBox->GetCurEntry() );
         if (SfxDispatcher* pDispatcher = GetDispatcher())
         {
-            SbxItem aSbxItem( SID_BASICIDE_ARG_SBX, aDocument, aLibName, aName, m_pBasicBox->ConvertType( eType ) );
+            SbxItem aSbxItem( SID_BASICIDE_ARG_SBX, aDocument, aLibName, aName, TreeListBox::ConvertType( eType ) );
             pDispatcher->Execute( SID_BASICIDE_SBXDELETED,
                                   SfxCallMode::SYNCHRON, &aSbxItem, 0L );
         }

basctl/source/basicide/scriptdocument.cxx
@@ -132,13 +132,13 @@ namespace basctl
             virtual bool    includeDocument( const docs::DocumentDescriptor& _rDocument ) const SAL_OVERRIDE;
 
         private:
-            bool    impl_isDocumentVisible_nothrow( const docs::DocumentDescriptor& _rDocument ) const;
+            static bool    impl_isDocumentVisible_nothrow( const docs::DocumentDescriptor& _rDocument );
 
         private:
             bool    m_bFilterInvisible;
         };
 
-        bool FilterDocuments::impl_isDocumentVisible_nothrow( const docs::DocumentDescriptor& _rDocument ) const
+        bool FilterDocuments::impl_isDocumentVisible_nothrow( const docs::DocumentDescriptor& _rDocument )
         {
             try
             {

commit ca91e31fdfd93440b0b150e6df74befe680e18f0

fix windows build
caused by my commit 49d93d854111791c0fe00155463e5d86faaef483
"loplugin:staticmethods"

Change-Id: I525ed6f6e4f82af58de2c994a610e00f49241bfa

extensions/source/scanner/scanner.hxx
@@ -48,7 +48,7 @@ protected:
     void*                                   mpData;
 
     static void                             AcquireData();
-    static void                             ReleaseData();
+    void                                    ReleaseData();
 
 public:
 