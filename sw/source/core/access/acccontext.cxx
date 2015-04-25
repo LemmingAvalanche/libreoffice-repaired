LemmingAvalanche/libreoffice-repaired

commit 15a1104df509af780cbe45c6e3e299afe9b8606c

Integrate branch of IAccessible2
Conflicts:
	everything

Change-Id: I9619634ee1e60d449025c006803da29c1e9d14b3

sw/source/core/access/acccontext.cxx
@@ -44,6 +44,7 @@
 #include <acccontext.hxx>
 #include <svx/AccessibleShape.hxx>
 #include <comphelper/accessibleeventnotifier.hxx>
+#include "accpara.hxx"
 #include <PostItMgr.hxx>
 
 using namespace sw::access;
@@ -185,8 +186,7 @@ void SwAccessibleContext::ChildrenScrolled( const SwFrm *pFrm,
                             "<SwAccessibleContext::ChildrenScrolled(..)> - always included child not considered!" );
                     const SwFrm* pLower( rLower.GetSwFrm() );
                     ::rtl::Reference< SwAccessibleContext > xAccImpl =
-                        GetMap()->GetContextImpl( pLower, SCROLLED_OUT == eAction ||
-                                                SCROLLED_IN == eAction );
+                        GetMap()->GetContextImpl( pLower, true );
                     if( xAccImpl.is() )
                     {
                         switch( eAction )
@@ -219,8 +219,7 @@ void SwAccessibleContext::ChildrenScrolled( const SwFrm *pFrm,
                     ::rtl::Reference< ::accessibility::AccessibleShape > xAccImpl =
                         GetMap()->GetContextImpl( rLower.GetDrawObject(),
                                                   this,
-                                                  SCROLLED_OUT == eAction ||
-                                                  SCROLLED_IN == eAction );
+                                                  true );
                     if( xAccImpl.is() )
                     {
                         switch( eAction )
@@ -240,8 +239,6 @@ void SwAccessibleContext::ChildrenScrolled( const SwFrm *pFrm,
                                 xAccImpl->ViewForwarderChanged(
                                     ::accessibility::IAccessibleViewForwarderListener::VISIBLE_AREA,
                                     GetMap() );
-                                DisposeShape( rLower.GetDrawObject(),
-                                              xAccImpl.get() );
                             }
                             break;
                         case NONE:
@@ -348,9 +345,6 @@ void SwAccessibleContext::ScrolledOut( const SwRect& rOldVisArea )
     // It might be that the child is freshly created just to send
     // the child event. In this case no listener will exist.
     FireStateChangedEvent( AccessibleStateType::SHOWING, sal_False );
-
-    // We now dispose the frame
-    Dispose( sal_True );
 }
 
 // #i27301# - use new type definition for <_nStates>
@@ -483,8 +477,12 @@ void SwAccessibleContext::GetStates(
 
     // EDITABLE
     if( bIsEditableState )
+    //Set editable state to graphic and other object when the document is editable
+    {
         rStateSet.AddState( AccessibleStateType::EDITABLE );
-
+        rStateSet.AddState( AccessibleStateType::RESIZABLE );
+        rStateSet.AddState( AccessibleStateType::MOVEABLE );
+    }
     // ENABLED
     rStateSet.AddState( AccessibleStateType::ENABLED );
 
@@ -520,6 +518,10 @@ SwAccessibleContext::SwAccessibleContext( SwAccessibleMap *pM,
     , nRole( nR )
     , bDisposing( sal_False )
     , bRegisteredAtAccessibleMap( true )
+    //Initialize the begin document load and IfAsynLoad to true
+    , bBeginDocumentLoad( sal_True )
+    , isIfAsynLoad( sal_True )
+    , bIsSeletedInDoc( sal_False)
 {
     InitStates();
 }
@@ -544,6 +546,9 @@ sal_Int32 SAL_CALL SwAccessibleContext::getAccessibleChildCount( void )
     SolarMutexGuard aGuard;
 
     CHECK_FOR_DEFUNC( XAccessibleContext )
+    //Notify the frame is a document
+    if( nRole == AccessibleRole::DOCUMENT )
+        bIsAccDocUse = sal_True;
 
     return bDisposing ? 0 : GetChildCount( *(GetMap()) );
 }
@@ -555,6 +560,10 @@ uno::Reference< XAccessible> SAL_CALL
     SolarMutexGuard aGuard;
 
     CHECK_FOR_DEFUNC( XAccessibleContext )
+
+    //Notify the frame is a document
+    if( nRole == AccessibleRole::DOCUMENT )
+        bIsAccDocUse = sal_True;
 
     const SwAccessibleChild aChild( GetChild( *(GetMap()), nIndex ) );
     if( !aChild.IsValid() )
@@ -571,6 +580,24 @@ uno::Reference< XAccessible> SAL_CALL
     {
         ::rtl::Reference < SwAccessibleContext > xChildImpl(
                 GetMap()->GetContextImpl( aChild.GetSwFrm(), !bDisposing )  );
+        //Send out accessible event when begin load.
+        if( bBeginDocumentLoad && nRole == AccessibleRole::DOCUMENT )
+        {
+
+            FireStateChangedEvent( AccessibleStateType::FOCUSABLE,sal_True );
+            FireStateChangedEvent( AccessibleStateType::BUSY,sal_True );
+            if( !isIfAsynLoad )
+            {
+                FireStateChangedEvent( AccessibleStateType::FOCUSED,sal_True );
+                // OFFSCREEN == !SHOWING, should stay consistent
+                // FireStateChangedEvent( AccessibleStateType::OFFSCREEN,sal_True );
+                FireStateChangedEvent( AccessibleStateType::SHOWING,sal_True );
+                FireStateChangedEvent( AccessibleStateType::BUSY,sal_False );
+                // OFFSCREEN again?
+                // FireStateChangedEvent( AccessibleStateType::OFFSCREEN,sal_False );
+            }
+            bBeginDocumentLoad = sal_False;
+        }
         if( xChildImpl.is() )
         {
             xChildImpl->SetParent( this );
@@ -679,6 +706,9 @@ uno::Reference<XAccessibleStateSet> SAL_CALL
 
     ::utl::AccessibleStateSetHelper *pStateSet =
         new ::utl::AccessibleStateSetHelper;
+
+    if( bIsSeletedInDoc )
+        pStateSet->AddState( AccessibleStateType::SELECTED );
 
     uno::Reference<XAccessibleStateSet> xStateSet( pStateSet );
     GetStates( *pStateSet );
@@ -941,13 +971,13 @@ uno::Any SAL_CALL SwAccessibleContext::getAccessibleKeyBinding()
 sal_Int32 SAL_CALL SwAccessibleContext::getForeground()
         throw (uno::RuntimeException)
 {
-    return 0;
+    return COL_BLACK;
 }
 
 sal_Int32 SAL_CALL SwAccessibleContext::getBackground()
         throw (uno::RuntimeException)
 {
-    return 0xffffff;
+    return COL_WHITE;
 }
 
 OUString SAL_CALL SwAccessibleContext::getImplementationName()
@@ -993,6 +1023,10 @@ void SwAccessibleContext::DisposeShape( const SdrObject *pObj,
 void SwAccessibleContext::ScrolledInShape( const SdrObject* ,
                                 ::accessibility::AccessibleShape *pAccImpl )
 {
+    if(NULL == pAccImpl)
+    {
+        return ;
+    }
     AccessibleEventObject aEvent;
     aEvent.EventId = AccessibleEventId::CHILD;
     uno::Reference< XAccessible > xAcc( pAccImpl );
@@ -1133,13 +1167,10 @@ void SwAccessibleContext::InvalidatePosOrSize( const SwRect& )
     if( !bIsNewShowingState &&
         SwAccessibleChild( GetParent() ).IsVisibleChildrenOnly() )
     {
-        // The frame is now invisible -> dispose it
-        Dispose( sal_True );
+        return;
     }
-    else
-    {
-        _InvalidateContent( sal_True );
-    }
+
+    _InvalidateContent( sal_True );
 }
 
 void SwAccessibleContext::InvalidateChildPosOrSize(
@@ -1464,5 +1495,16 @@ void SwAccessibleContext::GetAdditionalAccessibleChildren( std::vector< Window* 
         }
     }
 }
+
+sal_Bool SwAccessibleContext::SetSelectedState(sal_Bool bSeleted)
+{
+    if(bIsSeletedInDoc != bSeleted)
+    {
+        bIsSeletedInDoc = bSeleted;
+        FireStateChangedEvent( AccessibleStateType::SELECTED, bSeleted );
+        return sal_True;
+    }
+    return sal_False;
+};
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 276e0b65ced5d10354f8a3385b4892dfc2b30111

sw: fix SwAccessibleParagraph crash on delete
It happens that SwAccessibleParagraph::getCharacterBounds() is called
from ATK with a clearly invalid pPortionData member; the SwTxtNode has
no text but there are portions.

This is because after a deletion both a POS_CHANGED and a
INVALID_CONTENT event are created for the same SwAccessibleParagraph,
and they are merged into one POS_CHANGED event by
SwAccessibleMap::AppendEvent(), but
SwAccessibleContext::InvalidatePosOrSize() returns early if the
paragraph happens to not be visible, skipping the invalidation.

(regression from 15a1104df509af780cbe45c6e3e299afe9b8606c, which
 removed a Dispose() call; not sure if the Dispose() is needed here)

Change-Id: I985e9a439ee6c7024963eace876186f2247b9e03

sw/source/core/access/acccontext.cxx
@@ -1160,12 +1160,9 @@ void SwAccessibleContext::InvalidatePosOrSize( const SwRect& )
         FireVisibleDataEvent();
     }
 
-    if( !bIsNewShowingState &&
-        SwAccessibleChild( GetParent() ).IsVisibleChildrenOnly() )
-    {
-        return;
-    }
-
+    // note: InvalidatePosOrSize must call _InvalidateContent so that
+    // SwAccessibleParagraph updates its portions, or dispose it
+    // (see accmap.cxx: INVALID_CONTENT is contained in POS_CHANGED)
     _InvalidateContent( true );
 }
 