LemmingAvalanche/libreoffice-repaired

commit 5799e1edd920f8bff31e60cac3c6dc3154095b96

cleanup DIALOG_NO_PARENT, follow on to tdf#91090
remove the DIALOG_NO_PARENT abomination and replace it with a flags
parameter

Change-Id: I71b7dc46c619f2db56af6d4dc2c17daf0a2c8534
Reviewed-on: https://gerrit.libreoffice.org/15645
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Michael Meeks <michael.meeks@collabora.com>

vcl/source/window/dialog.cxx
@@ -353,7 +353,7 @@ void Dialog::ImplInitDialogData()
     mpDialogImpl            = new DialogImpl;
 }
 
-void Dialog::ImplInit( vcl::Window* pParent, WinBits nStyle )
+void Dialog::ImplInit( vcl::Window* pParent, WinBits nStyle, InitFlag eFlag )
 {
     SystemWindowFlags nSysWinMode = Application::GetSystemWindowMode();
 
@@ -392,8 +392,7 @@ void Dialog::ImplInit( vcl::Window* pParent, WinBits nStyle )
             }
         }
     }
-    // DIALOG_NO_PARENT: explicitly don't have a parent for this Dialog
-    else if( pParent == DIALOG_NO_PARENT )
+    else if( eFlag == InitFlag::Default )
         pParent = NULL;
 
     if ( !pParent || (nStyle & WB_SYSTEMWINDOW) ||
@@ -490,7 +489,7 @@ void Dialog::doDeferredInit(WinBits nBits)
 {
     VclPtr<vcl::Window> pParent = mpDialogParent;
     mpDialogParent = NULL;
-    ImplInit(pParent, nBits);
+    ImplInit(pParent, nBits, mnInitFlag);
     mbIsDefferedInit = false;
 }
 
@@ -501,18 +500,19 @@ Dialog::Dialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXML
     loadUI(pParent, OUStringToOString(rID, RTL_TEXTENCODING_UTF8), rUIXMLDescription);
 }
 
-Dialog::Dialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXMLDescription, WindowType nType)
+Dialog::Dialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXMLDescription, WindowType nType, InitFlag eFlag)
     : SystemWindow(nType)
 {
     ImplInitDialogData();
     loadUI(pParent, OUStringToOString(rID, RTL_TEXTENCODING_UTF8), rUIXMLDescription);
+    mnInitFlag = eFlag;
 }
 
-Dialog::Dialog(vcl::Window* pParent, WinBits nStyle)
+Dialog::Dialog(vcl::Window* pParent, WinBits nStyle, InitFlag eFlag)
     : SystemWindow(WINDOW_DIALOG)
 {
     ImplInitDialogData();
-    ImplInit( pParent, nStyle );
+    ImplInit( pParent, nStyle, eFlag );
 }
 
 void Dialog::set_action_area(VclButtonBox* pBox)
@@ -1130,8 +1130,8 @@ VclBuilderContainer::~VclBuilderContainer()
     delete m_pUIBuilder;
 }
 
-ModelessDialog::ModelessDialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXMLDescription)
-    : Dialog(pParent, rID, rUIXMLDescription, WINDOW_MODELESSDIALOG)
+ModelessDialog::ModelessDialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXMLDescription, InitFlag eFlag)
+    : Dialog(pParent, rID, rUIXMLDescription, WINDOW_MODELESSDIALOG, eFlag)
 {
 }
 

desktop/source/deployment/gui/dp_gui_dialog2.cxx
@@ -668,8 +668,8 @@ void DialogHelper::PostUserEvent( const Link<>& rLink, void* pCaller )
 }
 
 //                             ExtMgrDialog
-ExtMgrDialog::ExtMgrDialog(vcl::Window *pParent, TheExtensionManager *pManager)
-    : ModelessDialog(pParent, "ExtensionManagerDialog", "desktop/ui/extensionmanager.ui")
+ExtMgrDialog::ExtMgrDialog(vcl::Window *pParent, TheExtensionManager *pManager, Dialog::InitFlag eFlag)
+    : ModelessDialog(pParent, "ExtensionManagerDialog", "desktop/ui/extensionmanager.ui", eFlag)
     , DialogHelper(pManager->getContext(), (Dialog*) this)
     , m_sAddPackages(getResourceString(RID_STR_ADD_PACKAGES))
     , m_bHasProgress(false)

desktop/source/deployment/gui/dp_gui_dialog2.hxx
@@ -139,7 +139,7 @@ class ExtMgrDialog : public ModelessDialog,
     DECL_DLLPRIVATE_LINK( startProgress, void * );
 
 public:
-                    ExtMgrDialog( vcl::Window * pParent, TheExtensionManager *pManager );
+                    ExtMgrDialog( vcl::Window * pParent, TheExtensionManager *pManager, Dialog::InitFlag eFlag = Dialog::InitFlag::Default );
     virtual        ~ExtMgrDialog();
     virtual void    dispose() SAL_OVERRIDE;
 

desktop/source/deployment/gui/dp_gui_theextmgr.cxx
@@ -120,12 +120,10 @@ void TheExtensionManager::createDialog( const bool bCreateUpdDlg )
     }
     else if ( !m_pExtMgrDialog )
     {
-        // FIXME: horrible ...
-        vcl::Window* pParent = DIALOG_NO_PARENT;
         if (m_xParent.is())
-            pParent = VCLUnoHelper::GetWindow(m_xParent);
-
-        m_pExtMgrDialog = VclPtr<ExtMgrDialog>::Create( pParent, this );
+            m_pExtMgrDialog = VclPtr<ExtMgrDialog>::Create( VCLUnoHelper::GetWindow(m_xParent), this );
+        else
+            m_pExtMgrDialog = VclPtr<ExtMgrDialog>::Create( nullptr, this, Dialog::InitFlag::NoParent );
         delete m_pExecuteCmdQueue;
         m_pExecuteCmdQueue = new ExtensionCmdQueue( m_pExtMgrDialog.get(), this, m_xContext );
         m_pExtMgrDialog->setGetExtensionsURL( m_sGetExtensionsURL );

filter/source/xsltdialog/xmlfilterdialogcomponent.cxx
@@ -326,12 +326,11 @@ sal_Int16 SAL_CALL XMLFilterDialogComponent::execute(  ) throw(RuntimeException,
 
     if( nullptr == mpDialog )
     {
-        vcl::Window* pParent = DIALOG_NO_PARENT;
-        if (mxParent.is())
-            pParent = VCLUnoHelper::GetWindow(mxParent);
-
         Reference< XComponent > xComp( this );
-        mpDialog = VclPtr<XMLFilterSettingsDialog>::Create(pParent, mxContext);
+        if (mxParent.is())
+            mpDialog = VclPtr<XMLFilterSettingsDialog>::Create(VCLUnoHelper::GetWindow(mxParent), mxContext);
+        else
+            mpDialog = VclPtr<XMLFilterSettingsDialog>::Create(nullptr, mxContext, Dialog::InitFlag::NoParent);
         mpDialog->Execute();
     }
     else if( !mpDialog->IsVisible() )

filter/source/xsltdialog/xmlfiltersettingsdialog.cxx
@@ -56,8 +56,9 @@ using namespace com::sun::star::util;
 using ::rtl::Uri;
 
 XMLFilterSettingsDialog::XMLFilterSettingsDialog(vcl::Window* pParent,
-    const com::sun::star::uno::Reference< com::sun::star::uno::XComponentContext >& rxContext)
-    : ModelessDialog(pParent, "XMLFilterSettingsDialog", "filter/ui/xmlfiltersettings.ui")
+    const com::sun::star::uno::Reference< com::sun::star::uno::XComponentContext >& rxContext,
+    Dialog::InitFlag eFlag)
+    : ModelessDialog(pParent, "XMLFilterSettingsDialog", "filter/ui/xmlfiltersettings.ui", eFlag)
     , mxContext( rxContext )
     , m_bIsClosable(true)
     , m_sTemplatePath("$(user)/template/")

filter/source/xsltdialog/xmlfiltersettingsdialog.hxx
@@ -86,7 +86,8 @@ class XMLFilterSettingsDialog : public ModelessDialog
 {
 public:
     XMLFilterSettingsDialog(vcl::Window* pParent,
-        const com::sun::star::uno::Reference< com::sun::star::uno::XComponentContext >& rxContext);
+        const com::sun::star::uno::Reference< com::sun::star::uno::XComponentContext >& rxContext,
+        Dialog::InitFlag eFlag = Dialog::InitFlag::Default);
     virtual ~XMLFilterSettingsDialog();
     virtual void dispose() SAL_OVERRIDE;
 

include/toolkit/awt/scrollabledialog.hxx
@@ -57,7 +57,7 @@ namespace toolkit
         ScrollBarVisibility maScrollVis;
         void    lcl_Scroll( long nX, long nY );
     public:
-        ScrollableWrapper( vcl::Window* pParent, WinBits nStyle = WB_STDDIALOG );
+        ScrollableWrapper( vcl::Window* pParent, WinBits nStyle = WB_STDDIALOG, Dialog::InitFlag eFlag = Dialog::InitFlag::Default );
         virtual ~ScrollableWrapper();
         virtual void dispose() SAL_OVERRIDE;
         virtual void    SetScrollWidth( long nWidth ) SAL_OVERRIDE;

commit fb2b131ebe29604b8e90ce122693e36f1519ca89

vcl: valgrind - initialize Dialog::mnInitFlag
The assignment in the 3rd ctor is actually executed after the use from
loadUI().

(regression from 5799e1edd920f8bff31e60cac3c6dc3154095b96)

Change-Id: I34b3d7fb9d6c4637d15c7865e6034f2c9d968b99

vcl/source/window/dialog.cxx
@@ -451,6 +451,7 @@ void Dialog::ImplInitSettings()
 
 Dialog::Dialog( WindowType nType )
     : SystemWindow( nType )
+    , mnInitFlag(InitFlag::Default)
 {
     ImplInitDialogData();
 }
@@ -495,6 +496,7 @@ void Dialog::doDeferredInit(WinBits nBits)
 
 Dialog::Dialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXMLDescription)
     : SystemWindow(WINDOW_DIALOG)
+    , mnInitFlag(InitFlag::Default)
 {
     ImplInitDialogData();
     loadUI(pParent, OUStringToOString(rID, RTL_TEXTENCODING_UTF8), rUIXMLDescription);
@@ -502,14 +504,15 @@ Dialog::Dialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXML
 
 Dialog::Dialog(vcl::Window* pParent, const OUString& rID, const OUString& rUIXMLDescription, WindowType nType, InitFlag eFlag)
     : SystemWindow(nType)
+    , mnInitFlag(eFlag)
 {
     ImplInitDialogData();
     loadUI(pParent, OUStringToOString(rID, RTL_TEXTENCODING_UTF8), rUIXMLDescription);
-    mnInitFlag = eFlag;
 }
 
 Dialog::Dialog(vcl::Window* pParent, WinBits nStyle, InitFlag eFlag)
     : SystemWindow(WINDOW_DIALOG)
+    , mnInitFlag(eFlag)
 {
     ImplInitDialogData();
     ImplInit( pParent, nStyle, eFlag );