zzz99977/core

CntntIdxStore should also handle new unocrsrs
Change-Id: I37a120f891a07acdc467d0197d112139ec9b8c01

sw/source/core/doc/CntntIdxStore.cxx
@@ -394,6 +394,24 @@ void CntntIdxStoreImpl::SaveUnoCrsrs(SwDoc* pDoc, sal_uLong nNode, sal_Int32 nCn
             }
         }
     }
+    for (auto pWeakUnoCrsr : pDoc->mvUnoCrsrTbl2)
+    {
+        auto pUnoCrsr(pWeakUnoCrsr.lock());
+        if(!pUnoCrsr)
+            continue;
+        for(SwPaM& rPaM : (const_cast<SwUnoCrsr*>(pUnoCrsr.get()))->GetRingContainer())
+        {
+            lcl_ChkPaMBoth( m_aUnoCrsrEntries, nNode, nCntnt, rPaM);
+        }
+        const SwUnoTableCrsr* pUnoTblCrsr = dynamic_cast<const SwUnoTableCrsr*>(pUnoCrsr.get());
+        if( pUnoTblCrsr )
+        {
+            for(SwPaM& rPaM : (&(const_cast<SwUnoTableCrsr*>(pUnoTblCrsr))->GetSelRing())->GetRingContainer())
+            {
+                lcl_ChkPaMBoth( m_aUnoCrsrEntries, nNode, nCntnt, rPaM);
+            }
+        }
+    }
 }
 
 void CntntIdxStoreImpl::RestoreUnoCrsrs(updater_t& rUpdater)

LemmingAvalanche/libreoffice-repaired

sw/source/core/doc/CntntIdxStore.cxx
@@ -394,6 +394,24 @@ void ContentIdxStoreImpl::SaveUnoCrsrs(SwDoc* pDoc, sal_uLong nNode, sal_Int32 n
             }
         }
     }
+    for (auto pWeakUnoCrsr : pDoc->mvUnoCrsrTbl2)
+    {
+        auto pUnoCrsr(pWeakUnoCrsr.lock());
+        if(!pUnoCrsr)
+            continue;
+        for(SwPaM& rPaM : (const_cast<SwUnoCrsr*>(pUnoCrsr.get()))->GetRingContainer())
+        {
+            lcl_ChkPaMBoth( m_aUnoCrsrEntries, nNode, nContent, rPaM);
+        }
+        const SwUnoTableCrsr* pUnoTblCrsr = dynamic_cast<const SwUnoTableCrsr*>(pUnoCrsr.get());
+        if( pUnoTblCrsr )
+        {
+            for(SwPaM& rPaM : (&(const_cast<SwUnoTableCrsr*>(pUnoTblCrsr))->GetSelRing())->GetRingContainer())
+            {
+                lcl_ChkPaMBoth( m_aUnoCrsrEntries, nNode, nContent, rPaM);
+            }
+        }
+    }
 }
 
 void ContentIdxStoreImpl::RestoreUnoCrsrs(updater_t& rUpdater)