zzz99977/core

use new unocrsrs in ww8
Change-Id: I7f19c4002f471981b75b88998b31afcd4f30fa8c

sw/source/filter/ww8/ww8par.cxx
@@ -4898,7 +4898,9 @@ sal_uLong SwWW8ImplReader::CoreLoad(WW8Glossary *pGloss, const SwPosition &rPos)
             pDocShell->SetReadOnlyUI(true);
     }
 
-    pPaM = rDoc.CreateUnoCrsr(rPos);
+    mpCrsr = rDoc.CreateUnoCrsr2(rPos);
+    pPaM = mpCrsr.get();
+
 
     pCtrlStck = new SwWW8FltControlStack( &rDoc, nFieldFlags, *this );
 
@@ -5342,7 +5344,8 @@ sal_uLong SwWW8ImplReader::CoreLoad(WW8Glossary *pGloss, const SwPosition &rPos)
 
     SAL_WARN_IF(mpTableEndPaM, "sw.ww8", "document ended without table ending");
     mpTableEndPaM.reset();  //ensure this is deleted before pPaM
-    delete pPaM, pPaM = 0;
+    mpCrsr.reset();
+    pPaM = nullptr;
     mpLastAnchorPos.reset();//ensure this is deleted before UpdatePageDescs
 
     UpdatePageDescs(rDoc, nPageDescOffset);

LemmingAvalanche/libreoffice-repaired

sw/source/filter/ww8/ww8par.cxx
@@ -4900,7 +4900,8 @@ sal_uLong SwWW8ImplReader::CoreLoad(WW8Glossary *pGloss, const SwPosition &rPos)
             pDocShell->SetReadOnlyUI(true);
     }
 
-    m_pPaM = m_rDoc.CreateUnoCrsr(rPos);
+    mpCrsr = m_rDoc.CreateUnoCrsr2(rPos);
+    m_pPaM = mpCrsr.get();
 
     m_pCtrlStck = new SwWW8FltControlStack( &m_rDoc, m_nFieldFlags, *this );
 
@@ -5344,7 +5345,8 @@ sal_uLong SwWW8ImplReader::CoreLoad(WW8Glossary *pGloss, const SwPosition &rPos)
 
     SAL_WARN_IF(m_pTableEndPaM, "sw.ww8", "document ended without table ending");
     m_pTableEndPaM.reset();  //ensure this is deleted before pPaM
-    delete m_pPaM, m_pPaM = 0;
+    mpCrsr.reset();
+    m_pPaM = nullptr;
     m_pLastAnchorPos.reset();//ensure this is deleted before UpdatePageDescs
 
     UpdatePageDescs(m_rDoc, nPageDescOffset);