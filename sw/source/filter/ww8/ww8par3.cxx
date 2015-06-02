LibreOffice/core

loplugin: loopvartoosmall
Change-Id: Icb30dac9cdcba493752623ccafff5f06ccafc31f

sw/source/filter/ww8/ww8par3.cxx
@@ -1246,7 +1246,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
         std::deque<bool> aNotReallyThere;
         aNotReallyThere.resize(nMaxLevel);
         pListInfo->maParaSprms.resize(nMaxLevel);
-        for (sal_uInt8 nLevel = 0; nLevel < nLvlCount; ++nLevel)
+        for (sal_uInt16 nLevel = 0; nLevel < nLvlCount; ++nLevel)
         {
             SwNumFormat aNumFormat( rMyNumRule.Get( nLevel ) );
             // LVLF einlesen
@@ -1264,7 +1264,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
         // 1.2.2 die ItemPools mit den CHPx Einstellungen der verschiedenen
         //       Level miteinander vergleichen und ggfs. Style(s) erzeugen
 
-        for (sal_uInt8 nLevel = 0; nLevel < nLvlCount; ++nLevel)
+        for (sal_uInt16 nLevel = 0; nLevel < nLvlCount; ++nLevel)
         {
             bool bDummy;
             AdjustLVL( nLevel, rMyNumRule, pListInfo->aItemSet,
@@ -1273,7 +1273,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
 
         // 1.2.3 ItemPools leeren und loeschen
 
-        for (sal_uInt8 nLevel = 0; nLevel < nLvlCount; ++nLevel)
+        for (sal_uInt16 nLevel = 0; nLevel < nLvlCount; ++nLevel)
             delete pListInfo->aItemSet[ nLevel ];
     }
 
@@ -1293,7 +1293,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
 
     // 2.1 alle LFO einlesen
 
-    for (sal_uInt16 nLfo = 0; nLfo < nLfoCount; ++nLfo)
+    for (sal_uInt32 nLfo = 0; nLfo < nLfoCount; ++nLfo)
     {
         bOk = false;
 

sw/source/uibase/app/applab.cxx
@@ -308,9 +308,9 @@ void SwModule::InsertLab(SfxRequest& rReq, bool bLabel)
                 aFrmVertOrient.SetVertOrient( text::VertOrientation::TOP );
                 pFormat->SetFormatAttr(aFrmVertOrient);
 
-                for ( sal_uInt16 i = 0; i < rItem.nRows; ++i )
+                for ( sal_uInt32 i = 0; i < rItem.nRows; ++i )
                 {
-                    for ( sal_uInt16 j = 0; j < rItem.nCols; ++j )
+                    for ( sal_uInt32 j = 0; j < rItem.nCols; ++j )
                     {
                         pSh->Push();
                         const SwFrameFormat *pTmp = ( bLabel ?

sw/source/uibase/lingu/olmenu.cxx
@@ -181,7 +181,7 @@ void SwSpellPopup::fillLangPopupMenu(
         uno::Sequence< lang::Locale > rLocales( xDocumentLanguages->getDocumentLanguages( static_cast<sal_Int16>(nScriptType), nMaxCount ) );
         if (rLocales.getLength() > 0)
         {
-            for (sal_uInt16 i = 0; i < rLocales.getLength(); ++i)
+            for (sal_uInt32 i = 0; i < rLocales.getLength(); ++i)
             {
                 if (aLangItems.size() == (size_t)nMaxCount)
                     break;
@@ -514,7 +514,7 @@ m_aInfo16( SW_RES(IMG_INFO_16) )
         }
 
         sal_uInt16 nItemId = MN_SUGGESTION_START;
-        for (sal_uInt16 i = 0;  i < nStringCount;  ++i)
+        for (sal_uInt32 i = 0;  i < nStringCount;  ++i)
         {
             const OUString aEntry = m_aSuggestions[ i ];
             InsertItem(nItemId, aEntry, MenuItemBits::NONE, OString(), nPos++);

LemmingAvalanche/libreoffice-repaired

sw/source/filter/ww8/ww8par3.cxx
@@ -1246,7 +1246,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
         std::deque<bool> aNotReallyThere;
         aNotReallyThere.resize(nMaxLevel);
         pListInfo->maParaSprms.resize(nMaxLevel);
-        for (sal_uInt8 nLevel = 0; nLevel < nLvlCount; ++nLevel)
+        for (sal_uInt16 nLevel = 0; nLevel < nLvlCount; ++nLevel)
         {
             SwNumFormat aNumFormat( rMyNumRule.Get( nLevel ) );
             // LVLF einlesen
@@ -1264,7 +1264,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
         // 1.2.2 die ItemPools mit den CHPx Einstellungen der verschiedenen
         //       Level miteinander vergleichen und ggfs. Style(s) erzeugen
 
-        for (sal_uInt8 nLevel = 0; nLevel < nLvlCount; ++nLevel)
+        for (sal_uInt16 nLevel = 0; nLevel < nLvlCount; ++nLevel)
         {
             bool bDummy;
             AdjustLVL( nLevel, rMyNumRule, pListInfo->aItemSet,
@@ -1273,7 +1273,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
 
         // 1.2.3 ItemPools leeren und loeschen
 
-        for (sal_uInt8 nLevel = 0; nLevel < nLvlCount; ++nLevel)
+        for (sal_uInt16 nLevel = 0; nLevel < nLvlCount; ++nLevel)
             delete pListInfo->aItemSet[ nLevel ];
     }
 
@@ -1293,7 +1293,7 @@ WW8ListManager::WW8ListManager(SvStream& rSt_, SwWW8ImplReader& rReader_)
 
     // 2.1 alle LFO einlesen
 
-    for (sal_uInt16 nLfo = 0; nLfo < nLfoCount; ++nLfo)
+    for (sal_Int32 nLfo = 0; nLfo < nLfoCount; ++nLfo)
     {
         bOk = false;
 

sw/source/uibase/app/applab.cxx
@@ -308,9 +308,9 @@ void SwModule::InsertLab(SfxRequest& rReq, bool bLabel)
                 aFrmVertOrient.SetVertOrient( text::VertOrientation::TOP );
                 pFormat->SetFormatAttr(aFrmVertOrient);
 
-                for ( sal_uInt16 i = 0; i < rItem.nRows; ++i )
+                for ( sal_Int32 i = 0; i < rItem.nRows; ++i )
                 {
-                    for ( sal_uInt16 j = 0; j < rItem.nCols; ++j )
+                    for ( sal_Int32 j = 0; j < rItem.nCols; ++j )
                     {
                         pSh->Push();
                         const SwFrameFormat *pTmp = ( bLabel ?

sw/source/uibase/lingu/olmenu.cxx
@@ -181,7 +181,7 @@ void SwSpellPopup::fillLangPopupMenu(
         uno::Sequence< lang::Locale > rLocales( xDocumentLanguages->getDocumentLanguages( static_cast<sal_Int16>(nScriptType), nMaxCount ) );
         if (rLocales.getLength() > 0)
         {
-            for (sal_uInt16 i = 0; i < rLocales.getLength(); ++i)
+            for (sal_Int32 i = 0; i < rLocales.getLength(); ++i)
             {
                 if (aLangItems.size() == (size_t)nMaxCount)
                     break;
@@ -514,7 +514,7 @@ m_aInfo16( SW_RES(IMG_INFO_16) )
         }
 
         sal_uInt16 nItemId = MN_SUGGESTION_START;
-        for (sal_uInt16 i = 0;  i < nStringCount;  ++i)
+        for (sal_Int32 i = 0;  i < nStringCount;  ++i)
         {
             const OUString aEntry = m_aSuggestions[ i ];
             InsertItem(nItemId, aEntry, MenuItemBits::NONE, OString(), nPos++);