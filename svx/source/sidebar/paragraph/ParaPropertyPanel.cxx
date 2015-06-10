LemmingAvalanche/libreoffice-repaired

commit d373458b4f9859508d62d1161e39cb0c0488a42b

Fix indent button icons in impress/draw sidebar
Change-Id: Ib21c2489e1ff420e651e50a2731dfd3f16c9cef8
Reviewed-on: https://gerrit.libreoffice.org/16028
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

svx/uiconfig/ui/sidebarparagraph.ui
@@ -535,7 +535,7 @@
                         <property name="can_focus">False</property>
                         <property name="has_tooltip">True</property>
                         <property name="tooltip_text" translatable="yes">Increase Indent</property>
-                        <property name="action_name">.uno:Promote</property>
+                        <property name="action_name">.uno:IncrementIndent</property>
                         <property name="use_underline">True</property>
                       </object>
                       <packing>
@@ -549,7 +549,7 @@
                         <property name="can_focus">False</property>
                         <property name="has_tooltip">True</property>
                         <property name="tooltip_text" translatable="yes">Decrease Indent</property>
-                        <property name="action_name">.uno:Demote</property>
+                        <property name="action_name">.uno:DecrementIndent</property>
                         <property name="use_underline">True</property>
                       </object>
                       <packing>

commit c9ebe6e8a3f7663b346aae3bbccc6d149261463c

tdf#91944 Fix inc/dec indent buttons in impress
Regression from d373458b4f9859508d62d1161e39cb0c0488a42b.
These buttons are implemented inside the sidebar, so just
changing command names in the ui file won't work.
Also, following the decision to use indent buttons across all
modules, there is no need for 2 separate toolboxes anymore.

Change-Id: Id9af0cd10fd0517be86b9793c187cd6508029732
Reviewed-on: https://gerrit.libreoffice.org/16199
Reviewed-by: Maxim Monastirsky <momonasmon@gmail.com>
Tested-by: Maxim Monastirsky <momonasmon@gmail.com>

svx/source/sidebar/paragraph/ParaPropertyPanel.cxx
@@ -40,10 +40,6 @@ const char UNO_INCREMENTINDENT[]  = ".uno:IncrementIndent";
 const char UNO_DECREMENTINDENT[]  = ".uno:DecrementIndent";
 const char UNO_HANGINGINDENT[]    = ".uno:HangingIndent";
 
-const char UNO_PROMOTE[]          = ".uno:Promote";
-const char UNO_DEMOTE[]           = ".uno:Demote";
-const char UNO_HANGINGINDENT2[]   = ".uno:HangingIndent2";
-
 namespace svx {namespace sidebar {
 #define DEFAULT_VALUE          0
 
@@ -88,12 +84,11 @@ void ParaPropertyPanel::HandleContextChange (
     switch (maContext.GetCombinedContext_DI())
     {
         case CombinedEnumContext(Application_Calc, Context_DrawText):
+        case CombinedEnumContext(Application_WriterVariants, Context_DrawText):
             mpTBxVertAlign->Show();
             mpTBxBackColor->Hide();
             mpTBxNumBullet->Hide();
             ReSize(false);
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
             break;
 
         case CombinedEnumContext(Application_DrawImpress, Context_Draw):
@@ -103,26 +98,14 @@ void ParaPropertyPanel::HandleContextChange (
             mpTBxBackColor->Hide();
             mpTBxNumBullet->Show();
             ReSize(true);
-            mpTbxIndent_IncDec->Hide();
-            mpTbxProDemote->Show();
             break;
 
         case CombinedEnumContext(Application_DrawImpress, Context_DrawText):
-            mpTBxVertAlign->Show();
-            mpTBxBackColor->Hide();
-            mpTBxNumBullet->Show();
-            ReSize(true);
-            mpTbxIndent_IncDec->Hide();
-            mpTbxProDemote->Show();
-            break;
-
         case CombinedEnumContext(Application_DrawImpress, Context_Table):
             mpTBxVertAlign->Show();
             mpTBxBackColor->Hide();
             mpTBxNumBullet->Show();
             ReSize(true);
-            mpTbxIndent_IncDec->Hide();
-            mpTbxProDemote->Show();
             break;
 
         case CombinedEnumContext(Application_WriterVariants, Context_Default):
@@ -130,10 +113,7 @@ void ParaPropertyPanel::HandleContextChange (
             mpTBxVertAlign->Hide();
             mpTBxBackColor->Show();
             mpTBxNumBullet->Show();
-
             ReSize(true);
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
             break;
 
         case CombinedEnumContext(Application_WriterVariants, Context_Table):
@@ -141,17 +121,6 @@ void ParaPropertyPanel::HandleContextChange (
             mpTBxBackColor->Show();
             mpTBxNumBullet->Show();
             ReSize(true);
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
-            break;
-
-        case CombinedEnumContext(Application_WriterVariants, Context_DrawText):
-            mpTBxVertAlign->Show();
-            mpTBxBackColor->Hide();
-            mpTBxNumBullet->Hide();
-            ReSize(false);
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
             break;
 
         case CombinedEnumContext(Application_WriterVariants, Context_Annotation):
@@ -159,8 +128,6 @@ void ParaPropertyPanel::HandleContextChange (
             mpTBxBackColor->Hide();
             mpTBxNumBullet->Hide();
             ReSize(false);
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
             break;
 
         case CombinedEnumContext(Application_Calc, Context_EditCell):
@@ -217,14 +184,6 @@ void ParaPropertyPanel::InitToolBoxIndent()
 
     mpTbxIndent_IncDec->SetSelectHdl(LINK( this, ParaPropertyPanel, ClickIndent_IncDec_Hdl_Impl ));
     m_eLRSpaceUnit = maLRSpaceControl.GetCoreMetric();
-
-    const sal_uInt16 nIdPromote  = mpTbxProDemote->GetItemId(UNO_PROMOTE);
-    const sal_uInt16 nIdDemote   = mpTbxProDemote->GetItemId(UNO_DEMOTE);
-    const sal_uInt16 nIdHanging2 = mpTbxProDemote->GetItemId(UNO_HANGINGINDENT2);
-    mpTbxProDemote->SetItemImage(nIdPromote, maOutLineLeftControl.GetIcon());
-    mpTbxProDemote->SetItemImage(nIdDemote, maOutLineRightControl.GetIcon());
-    mpTbxProDemote->SetItemImage(nIdHanging2, maIndHang);
-    mpTbxProDemote->SetSelectHdl(LINK( this, ParaPropertyPanel, ClickProDemote_Hdl_Impl ));
     m_eLRSpaceUnit = maLRSpaceControl.GetCoreMetric();
 }
 
@@ -338,29 +297,6 @@ IMPL_LINK_TYPED(ParaPropertyPanel, ClickIndent_IncDec_Hdl_Impl, ToolBox *, pCont
         }
 }
 
-IMPL_LINK_TYPED(ParaPropertyPanel, ClickProDemote_Hdl_Impl, ToolBox *, pControl, void)
-{
-    const OUString aCommand(pControl->GetItemCommand(pControl->GetCurItemId()));
-
-        if (aCommand == UNO_PROMOTE)
-        {
-            GetBindings()->GetDispatcher()->Execute( SID_OUTLINE_RIGHT, SfxCallMode::RECORD );
-        }
-        else if (aCommand == UNO_DEMOTE)
-        {
-            GetBindings()->GetDispatcher()->Execute( SID_OUTLINE_LEFT, SfxCallMode::RECORD );
-        }
-        else if (aCommand == UNO_HANGINGINDENT2)
-        {
-            SvxLRSpaceItem aMargin( SID_ATTR_PARA_LRSPACE );
-            aMargin.SetTextLeft( (const long)GetCoreValue( *mpLeftIndent, m_eLRSpaceUnit ) + (const short)GetCoreValue( *mpFLineIndent, m_eLRSpaceUnit ) );
-            aMargin.SetRight( (const long)GetCoreValue( *mpRightIndent, m_eLRSpaceUnit ) );
-            aMargin.SetTextFirstLineOfst( ((const short)GetCoreValue( *mpFLineIndent, m_eLRSpaceUnit ))*(-1) );
-
-            GetBindings()->GetDispatcher()->Execute( SID_ATTR_PARA_LRSPACE, SfxCallMode::RECORD, &aMargin, 0L);
-        }
-}
-
 // for Paragraph Spacing
 IMPL_LINK_NOARG( ParaPropertyPanel, ULSpaceHdl_Impl)
 {
@@ -407,11 +343,6 @@ void ParaPropertyPanel::NotifyItemUpdate(
         StateChangedULImpl( nSID, eState, pState );
         break;
 
-    case SID_OUTLINE_LEFT:
-    case SID_OUTLINE_RIGHT:
-        StateChangeOutLineImpl( nSID, eState, pState );
-        break;
-
     case SID_INC_INDENT:
     case SID_DEC_INDENT:
         StateChangeIncDecImpl( nSID, eState, pState );
@@ -427,14 +358,6 @@ void ParaPropertyPanel::StateChangedIndentImpl( sal_uInt16 /*nSID*/, SfxItemStat
     case CombinedEnumContext(Application_WriterVariants, Context_DrawText):
     case CombinedEnumContext(Application_WriterVariants, Context_Annotation):
     case CombinedEnumContext(Application_Calc, Context_DrawText):
-        {
-            mpLeftIndent->SetMin( DEFAULT_VALUE );
-            mpRightIndent->SetMin( DEFAULT_VALUE );
-            mpFLineIndent->SetMin( DEFAULT_VALUE );
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
-        }
-        break;
     case CombinedEnumContext(Application_DrawImpress, Context_DrawText):
     case CombinedEnumContext(Application_DrawImpress, Context_Draw):
     case CombinedEnumContext(Application_DrawImpress, Context_TextObject):
@@ -444,8 +367,6 @@ void ParaPropertyPanel::StateChangedIndentImpl( sal_uInt16 /*nSID*/, SfxItemStat
             mpLeftIndent->SetMin( DEFAULT_VALUE );
             mpRightIndent->SetMin( DEFAULT_VALUE );
             mpFLineIndent->SetMin( DEFAULT_VALUE );
-            mpTbxIndent_IncDec->Hide();
-            mpTbxProDemote->Show();
         }
         break;
     case CombinedEnumContext(Application_WriterVariants, Context_Default):
@@ -455,14 +376,11 @@ void ParaPropertyPanel::StateChangedIndentImpl( sal_uInt16 /*nSID*/, SfxItemStat
             mpLeftIndent->SetMin( NEGA_MAXVALUE, FUNIT_100TH_MM );
             mpRightIndent->SetMin( NEGA_MAXVALUE, FUNIT_100TH_MM );
             mpFLineIndent->SetMin( NEGA_MAXVALUE, FUNIT_100TH_MM );
-            mpTbxIndent_IncDec->Show();
-            mpTbxProDemote->Hide();
         }
         break;
     }
 
     const sal_uInt16 nIdHangingIndent   = mpTbxIndent_IncDec->GetItemId(UNO_HANGINGINDENT);
-    const sal_uInt16 nIdHangingIndent2  = mpTbxIndent_IncDec->GetItemId(UNO_HANGINGINDENT2);
     if( pState && eState >= SfxItemState::DEFAULT )
     {
         const SvxLRSpaceItem* pSpace = static_cast<const SvxLRSpaceItem*>(pState);
@@ -536,24 +454,18 @@ void ParaPropertyPanel::StateChangedIndentImpl( sal_uInt16 /*nSID*/, SfxItemStat
             mpTbxIndent_IncDec->EnableItem(nIdIncrIndent, true);
             mpTbxIndent_IncDec->EnableItem(nIdDecrIndent, true);
         }
-
-        mpTbxProDemote->EnableItem(nIdHangingIndent2, true);
     }
     else if( eState == SfxItemState::DISABLED )
     {
         mpLeftIndent-> Disable();
         mpRightIndent->Disable();
         mpFLineIndent->Disable();
-        mpTbxIndent_IncDec->Disable();
         if( maContext.GetCombinedContext_DI() != CombinedEnumContext(Application_WriterVariants, Context_Text)  &&
             maContext.GetCombinedContext_DI() != CombinedEnumContext(Application_WriterVariants, Context_Default) &&
             maContext.GetCombinedContext_DI() !=  CombinedEnumContext(Application_WriterVariants, Context_Table) )
             mpTbxIndent_IncDec->Disable();
         else
             mpTbxIndent_IncDec->EnableItem(nIdHangingIndent, false);
-
-        //      maTbxProDemote->Disable();
-        mpTbxProDemote->EnableItem(nIdHangingIndent2, false);
     }
     else
     {
@@ -566,7 +478,6 @@ void ParaPropertyPanel::StateChangedIndentImpl( sal_uInt16 /*nSID*/, SfxItemStat
             mpTbxIndent_IncDec->Disable();
         else
             mpTbxIndent_IncDec->EnableItem(nIdHangingIndent, false);
-        mpTbxProDemote->EnableItem(nIdHangingIndent2, false);
     }
 }
 
@@ -605,37 +516,6 @@ void ParaPropertyPanel::StateChangedULImpl( sal_uInt16 /*nSID*/, SfxItemState eS
         mpTopDist->SetEmptyFieldValue();
         mpBottomDist->SetEmptyFieldValue();
     }
-}
-
-void ParaPropertyPanel::StateChangeOutLineImpl( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState )
-{
-    if (nSID==SID_OUTLINE_LEFT)
-    {
-        if( pState && eState == SfxItemState::UNKNOWN )
-            mbOutLineLeft = true;
-        else
-            mbOutLineLeft = false;
-    }
-    if (nSID==SID_OUTLINE_RIGHT)
-    {
-        if( pState && eState == SfxItemState::UNKNOWN )
-            mbOutLineRight = true;
-        else
-            mbOutLineRight = false;
-    }
-
-    const sal_uInt16 nIdDemote = mpTbxProDemote->GetItemId(UNO_DEMOTE);
-    if(mbOutLineLeft)
-        mpTbxProDemote->EnableItem(nIdDemote, true);
-    else
-        mpTbxProDemote->EnableItem(nIdDemote, false);
-
-    const sal_uInt16 nIdPromote = mpTbxProDemote->GetItemId(UNO_PROMOTE);
-    if(mbOutLineRight)
-        mpTbxProDemote->EnableItem(nIdPromote, true);
-    else
-        mpTbxProDemote->EnableItem(nIdPromote, false);
-
 }
 
 void ParaPropertyPanel::StateChangeIncDecImpl( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState )
@@ -696,8 +576,6 @@ ParaPropertyPanel::ParaPropertyPanel(vcl::Window* pParent,
       maSpace3 (SVX_RES(IMG_SPACE3)),
       maIndHang (SVX_RES(IMG_INDENT_HANG)),
       maTxtLeft (0),
-      mbOutLineLeft (false),
-      mbOutLineRight (false),
       maUpper (0),
       maLower (0),
       m_eMetricUnit(FUNIT_NONE),
@@ -706,8 +584,6 @@ ParaPropertyPanel::ParaPropertyPanel(vcl::Window* pParent,
       m_eULSpaceUnit(),
       maLRSpaceControl (SID_ATTR_PARA_LRSPACE,*pBindings,*this),
       maULSpaceControl (SID_ATTR_PARA_ULSPACE, *pBindings,*this),
-      maOutLineLeftControl(SID_OUTLINE_LEFT, *pBindings, *this, OUString("OutlineRight"), rxFrame),
-      maOutLineRightControl(SID_OUTLINE_RIGHT, *pBindings, *this, OUString("OutlineLeft"), rxFrame),
       maDecIndentControl(SID_DEC_INDENT, *pBindings,*this, OUString("DecrementIndent"), rxFrame),
       maIncIndentControl(SID_INC_INDENT, *pBindings,*this, OUString("IncrementIndent"), rxFrame),
       m_aMetricCtl (SID_ATTR_METRIC, *pBindings,*this),
@@ -732,9 +608,7 @@ ParaPropertyPanel::ParaPropertyPanel(vcl::Window* pParent,
     mpRightIndent->set_width_request(mpRightIndent->get_preferred_size().Width());
     get(mpFLineIndent,  "firstlineindent");
     mpFLineIndent->set_width_request(mpFLineIndent->get_preferred_size().Width());
-
     get(mpTbxIndent_IncDec, "indent");
-    get(mpTbxProDemote, "promotedemote");
 
     initial();
     m_aMetricCtl.RequestUpdate();
@@ -753,15 +627,12 @@ void ParaPropertyPanel::dispose()
     mpTopDist.clear();
     mpBottomDist.clear();
     mpTbxIndent_IncDec.clear();
-    mpTbxProDemote.clear();
     mpLeftIndent.clear();
     mpRightIndent.clear();
     mpFLineIndent.clear();
 
     maLRSpaceControl.dispose();
     maULSpaceControl.dispose();
-    maOutLineLeftControl.dispose();
-    maOutLineRightControl.dispose();
     maDecIndentControl.dispose();
     maIncIndentControl.dispose();
     m_aMetricCtl.dispose();

svx/source/sidebar/paragraph/ParaPropertyPanel.hxx
@@ -83,7 +83,6 @@ private:
     VclPtr<SvxRelativeField>   mpTopDist;
     VclPtr<SvxRelativeField>   mpBottomDist;
     VclPtr<ToolBox>            mpTbxIndent_IncDec;
-    VclPtr<ToolBox>            mpTbxProDemote;
     VclPtr<SvxRelativeField>   mpLeftIndent;
     VclPtr<SvxRelativeField>   mpRightIndent;
     VclPtr<SvxRelativeField>   mpFLineIndent;
@@ -94,8 +93,6 @@ private:
 
     // Data Member
     long                maTxtLeft;
-    bool                    mbOutLineLeft;
-    bool                    mbOutLineRight;
     long                    maUpper;
     long                    maLower;
 
@@ -106,8 +103,6 @@ private:
     // Control Items
     ::sfx2::sidebar::ControllerItem  maLRSpaceControl;
     ::sfx2::sidebar::ControllerItem  maULSpaceControl;
-    ::sfx2::sidebar::ControllerItem  maOutLineLeftControl;
-    ::sfx2::sidebar::ControllerItem  maOutLineRightControl;
     ::sfx2::sidebar::ControllerItem  maDecIndentControl;
     ::sfx2::sidebar::ControllerItem  maIncIndentControl;
     ::sfx2::sidebar::ControllerItem  m_aMetricCtl;
@@ -119,12 +114,10 @@ private:
 
     DECL_LINK(ModifyIndentHdl_Impl, void*);
     DECL_LINK_TYPED(ClickIndent_IncDec_Hdl_Impl, ToolBox*, void);
-    DECL_LINK_TYPED(ClickProDemote_Hdl_Impl, ToolBox*, void);
     DECL_LINK(ULSpaceHdl_Impl, void*);
 
     void StateChangedIndentImpl( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState );
     void StateChangedULImpl( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState );
-    void StateChangeOutLineImpl( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState );
     void StateChangeIncDecImpl( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState );
 
     void initial();

svx/uiconfig/ui/sidebarparagraph.ui
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- Generated with glade 3.16.1 -->
+<!-- Generated with glade 3.18.3 -->
 <interface>
   <requires lib="gtk+" version="3.4"/>
-  <!-- interface-requires LibreOffice 1.0 -->
+  <requires lib="LibreOffice" version="1.0"/>
   <object class="GtkAdjustment" id="adjustment1">
     <property name="upper">100</property>
     <property name="step_increment">1</property>
@@ -56,8 +56,6 @@
           <packing>
             <property name="left_attach">0</property>
             <property name="top_attach">1</property>
-            <property name="width">1</property>
-            <property name="height">1</property>
           </packing>
         </child>
         <child>
@@ -84,8 +82,6 @@
           <packing>
             <property name="left_attach">1</property>
             <property name="top_attach">1</property>
-            <property name="width">1</property>
-            <property name="height">1</property>
           </packing>
         </child>
         <child>
@@ -252,7 +248,6 @@
             <property name="left_attach">0</property>
             <property name="top_attach">0</property>
             <property name="width">2</property>
-            <property name="height">1</property>
           </packing>
         </child>
         <child>
@@ -266,10 +261,10 @@
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
                 <property name="hexpand">True</property>
-                <property name="xalign">0</property>
                 <property name="label" translatable="yes">_Spacing:</property>
                 <property name="use_underline">True</property>
                 <property name="mnemonic_widget">paraspacing</property>
+                <property name="xalign">0</property>
               </object>
               <packing>
                 <property name="expand">False</property>
@@ -432,8 +427,6 @@
           <packing>
             <property name="left_attach">0</property>
             <property name="top_attach">2</property>
-            <property name="width">1</property>
-            <property name="height">1</property>
           </packing>
         </child>
         <child>
@@ -447,10 +440,10 @@
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
                 <property name="hexpand">True</property>
-                <property name="xalign">0</property>
                 <property name="label" translatable="yes">_Indent:</property>
                 <property name="use_underline">True</property>
-                <property name="mnemonic_widget">box2</property>
+                <property name="mnemonic_widget">indent</property>
+                <property name="xalign">0</property>
               </object>
               <packing>
                 <property name="expand">False</property>
@@ -459,123 +452,52 @@
               </packing>
             </child>
             <child>
-              <object class="GtkBox" id="box2">
+              <object class="GtkToolbar" id="indent">
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
+                <property name="has_tooltip">True</property>
+                <property name="tooltip_text" translatable="yes">Indent</property>
+                <property name="show_arrow">False</property>
                 <child>
-                  <object class="GtkToolbar" id="indent">
+                  <object class="GtkToolButton" id="increaseindent">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
                     <property name="has_tooltip">True</property>
-                    <property name="tooltip_text" translatable="yes">Indent</property>
-                    <property name="show_arrow">False</property>
-                    <child>
-                      <object class="GtkToolButton" id="increaseindent">
-                        <property name="visible">True</property>
-                        <property name="can_focus">False</property>
-                        <property name="has_tooltip">True</property>
-                        <property name="tooltip_text" translatable="yes">Increase Indent</property>
-                        <property name="action_name">.uno:IncrementIndent</property>
-                        <property name="use_underline">True</property>
-                      </object>
-                      <packing>
-                        <property name="expand">False</property>
-                        <property name="homogeneous">True</property>
-                      </packing>
-                    </child>
-                    <child>
-                      <object class="GtkToolButton" id="decreaseindent">
-                        <property name="width_request">15</property>
-                        <property name="height_request">25</property>
-                        <property name="visible">True</property>
-                        <property name="can_focus">False</property>
-                        <property name="has_tooltip">True</property>
-                        <property name="tooltip_text" translatable="yes">Decrease Indent</property>
-                        <property name="action_name">.uno:DecrementIndent</property>
-                        <property name="use_underline">True</property>
-                      </object>
-                      <packing>
-                        <property name="expand">False</property>
-                        <property name="homogeneous">True</property>
-                      </packing>
-                    </child>
-                    <child>
-                      <object class="GtkToolButton" id="hangingindent">
-                        <property name="visible">True</property>
-                        <property name="can_focus">False</property>
-                        <property name="has_tooltip">True</property>
-                        <property name="tooltip_text" translatable="yes">Switch to Hanging Indent</property>
-                        <property name="action_name">.uno:HangingIndent</property>
-                        <property name="use_underline">True</property>
-                      </object>
-                      <packing>
-                        <property name="expand">False</property>
-                        <property name="homogeneous">True</property>
-                      </packing>
-                    </child>
+                    <property name="tooltip_text" translatable="yes">Increase Indent</property>
+                    <property name="action_name">.uno:IncrementIndent</property>
+                    <property name="use_underline">True</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>
-                    <property name="fill">True</property>
-                    <property name="position">0</property>
+                    <property name="homogeneous">True</property>
                   </packing>
                 </child>
                 <child>
-                  <object class="GtkToolbar" id="promotedemote">
+                  <object class="GtkToolButton" id="decreaseindent">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
                     <property name="has_tooltip">True</property>
-                    <property name="tooltip_text" translatable="yes">Indent</property>
-                    <property name="show_arrow">False</property>
-                    <child>
-                      <object class="GtkToolButton" id="promote">
-                        <property name="width_request">15</property>
-                        <property name="height_request">25</property>
-                        <property name="visible">True</property>
-                        <property name="can_focus">False</property>
-                        <property name="has_tooltip">True</property>
-                        <property name="tooltip_text" translatable="yes">Increase Indent</property>
-                        <property name="action_name">.uno:IncrementIndent</property>
-                        <property name="use_underline">True</property>
-                      </object>
-                      <packing>
-                        <property name="expand">False</property>
-                        <property name="homogeneous">True</property>
-                      </packing>
-                    </child>
-                    <child>
-                      <object class="GtkToolButton" id="demote">
-                        <property name="visible">True</property>
-                        <property name="can_focus">False</property>
-                        <property name="has_tooltip">True</property>
-                        <property name="tooltip_text" translatable="yes">Decrease Indent</property>
-                        <property name="action_name">.uno:DecrementIndent</property>
-                        <property name="use_underline">True</property>
-                      </object>
-                      <packing>
-                        <property name="expand">False</property>
-                        <property name="homogeneous">True</property>
-                      </packing>
-                    </child>
-                    <child>
-                      <object class="GtkToolButton" id="hangingindent2">
-                        <property name="visible">True</property>
-                        <property name="can_focus">False</property>
-                        <property name="has_tooltip">True</property>
-                        <property name="tooltip_text" translatable="yes">Switch to Hanging Indent</property>
-                        <property name="action_name">.uno:HangingIndent2</property>
-                        <property name="use_underline">True</property>
-                      </object>
-                      <packing>
-                        <property name="expand">False</property>
-                        <property name="homogeneous">True</property>
-                      </packing>
-                    </child>
+                    <property name="tooltip_text" translatable="yes">Decrease Indent</property>
+                    <property name="action_name">.uno:DecrementIndent</property>
+                    <property name="use_underline">True</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>
-                    <property name="fill">True</property>
-                    <property name="position">1</property>
+                    <property name="homogeneous">True</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkToolButton" id="hangingindent">
+                    <property name="visible">True</property>
+                    <property name="can_focus">False</property>
+                    <property name="has_tooltip">True</property>
+                    <property name="tooltip_text" translatable="yes">Switch to Hanging Indent</property>
+                    <property name="action_name">.uno:HangingIndent</property>
+                    <property name="use_underline">True</property>
+                  </object>
+                  <packing>
+                    <property name="expand">False</property>
+                    <property name="homogeneous">True</property>
                   </packing>
                 </child>
               </object>
@@ -715,16 +637,12 @@
           <packing>
             <property name="left_attach">1</property>
             <property name="top_attach">2</property>
-            <property name="width">1</property>
-            <property name="height">1</property>
           </packing>
         </child>
       </object>
       <packing>
         <property name="left_attach">0</property>
         <property name="top_attach">0</property>
-        <property name="width">1</property>
-        <property name="height">1</property>
       </packing>
     </child>
   </object>