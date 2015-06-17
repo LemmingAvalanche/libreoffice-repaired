LemmingAvalanche/libreoffice-repaired

commit d68d122b9c402eb4a5b6caf66b191302813abeda

sw: fix assert on exporting ooo24533-1.sxw to WW8
There's a fly anchored at a paragraph inside of a table inside of a
redline, and for some reason MoveRange() calls SwTableNode::MakeFrms(),
which asserts in frmtool.cxx AppendObjs().

The problem is that _SaveFlyInRange() removes the frame format from the
SpzFrmFmts but it is still reachable from the SwNode::m_pAnchoredFlys.

Change-Id: I3853c0072662bfb4f71f78451d3604ed567cd584

sw/source/core/doc/docedt.cxx
@@ -44,6 +44,7 @@
 #include <unoflatpara.hxx>
 #include <SwGrammarMarkUp.hxx>
 #include <docedt.hxx>
+#include <atrfrm.hxx>
 
 #include <vector>
 
@@ -77,10 +78,13 @@ void _RestFlyInRange( _SaveFlyArr & rArr, const SwNodeIndex& rSttIdx,
         aAnchor.SetAnchor( &aPos );
         pFormat->GetDoc()->GetSpzFrameFormats()->push_back( pFormat );
         pFormat->SetFormatAttr( aAnchor );
+        // SetFormatAttr will not call Modify() because the node is the same :-/
+        aPos.nNode.GetNode().AddAnchoredFly(pFormat);
         SwContentNode* pCNd = aPos.nNode.GetNode().GetContentNode();
         if( pCNd && pCNd->getLayoutFrm( pFormat->GetDoc()->getIDocumentLayoutAccess().GetCurrentLayout(), 0, 0, false ) )
             pFormat->MakeFrms();
     }
+    sw::CheckAnchoredFlyConsistency(*rSttIdx.GetNode().GetDoc());
 }
 
 void _SaveFlyInRange( const SwNodeRange& rRg, _SaveFlyArr& rArr )
@@ -100,9 +104,11 @@ void _SaveFlyInRange( const SwNodeRange& rRg, _SaveFlyArr& rArr )
                             pFormat, false );
             rArr.push_back( aSave );
             pFormat->DelFrms();
+            pAPos->nNode.GetNode().RemoveAnchoredFly(pFormat);
             rFormats.erase( rFormats.begin() + n-- );
         }
     }
+    sw::CheckAnchoredFlyConsistency(*rRg.aStart.GetNode().GetDoc());
 }
 
 void _SaveFlyInRange( const SwPaM& rPam, const SwNodeIndex& rInsPos,
@@ -162,10 +168,12 @@ void _SaveFlyInRange( const SwPaM& rPam, const SwNodeIndex& rInsPos,
                                 pFormat, bInsPos );
                 rArr.push_back( aSave );
                 pFormat->DelFrms();
+                pAPos->nNode.GetNode().RemoveAnchoredFly(pFormat);
                 rFormats.erase( rFormats.begin() + n-- );
             }
         }
     }
+    sw::CheckAnchoredFlyConsistency(*rPam.GetPoint()->nNode.GetNode().GetDoc());
 }
 
 /// Delete and move all Flys at the paragraph, that are within the selection.

commit 0c653cb9a8be52e57cf887f368c4bdcd514d418a

sw: fix assert on exporting ooo75410-1.doc to WW8
Apparently it may happen that some fly does get a different anchor node
than before, which means the RemoveAnchoredFly() will be called twice.

Prevent the problem differently by clearing the anchor in
_SaveFlyInRange().

(regression from d68d122b9c402eb4a5b6caf66b191302813abeda)

Change-Id: Id796d2e230de065113bcf1e6164d6b8a01ab933c

sw/source/core/doc/docedt.cxx
@@ -77,9 +77,8 @@ void _RestFlyInRange( _SaveFlyArr & rArr, const SwNodeIndex& rSttIdx,
         SwFormatAnchor aAnchor( pFormat->GetAnchor() );
         aAnchor.SetAnchor( &aPos );
         pFormat->GetDoc()->GetSpzFrameFormats()->push_back( pFormat );
+        // SetFormatAttr should call Modify() and add it to the node
         pFormat->SetFormatAttr( aAnchor );
-        // SetFormatAttr will not call Modify() because the node is the same :-/
-        aPos.nNode.GetNode().AddAnchoredFly(pFormat);
         SwContentNode* pCNd = aPos.nNode.GetNode().GetContentNode();
         if( pCNd && pCNd->getLayoutFrm( pFormat->GetDoc()->getIDocumentLayoutAccess().GetCurrentLayout(), 0, 0, false ) )
             pFormat->MakeFrms();
@@ -104,7 +103,10 @@ void _SaveFlyInRange( const SwNodeRange& rRg, _SaveFlyArr& rArr )
                             pFormat, false );
             rArr.push_back( aSave );
             pFormat->DelFrms();
-            pAPos->nNode.GetNode().RemoveAnchoredFly(pFormat);
+            // set a dummy anchor position to maintain anchoring invariants
+            SwFormatAnchor aAnchor( pFormat->GetAnchor() );
+            aAnchor.SetAnchor(nullptr);
+            pFormat->SetFormatAttr(aAnchor);
             rFormats.erase( rFormats.begin() + n-- );
         }
     }
@@ -168,7 +170,10 @@ void _SaveFlyInRange( const SwPaM& rPam, const SwNodeIndex& rInsPos,
                                 pFormat, bInsPos );
                 rArr.push_back( aSave );
                 pFormat->DelFrms();
-                pAPos->nNode.GetNode().RemoveAnchoredFly(pFormat);
+                // set a dummy anchor position to maintain anchoring invariants
+                SwFormatAnchor aAnchor( pFormat->GetAnchor() );
+                aAnchor.SetAnchor(nullptr);
+                pFormat->SetFormatAttr(aAnchor);
                 rFormats.erase( rFormats.begin() + n-- );
             }
         }