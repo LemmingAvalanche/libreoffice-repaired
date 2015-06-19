LemmingAvalanche/libreoffice-repaired

commit bbdb3c009add537c27b622c4da30621cbb963dd6

Select the whole text when the named range box gets focus.
Not doing this had been bugging me for a long time since I use
Shift-Ctrl-F5 to define named ranges a lot.  Small usability improvement.

Change-Id: Ib1a4e4e26e15667598a603443dde05e08fdbe3b7

sc/source/ui/app/inputwin.cxx
@@ -2484,43 +2484,55 @@ void ScPosWnd::DoEnter()
 
 bool ScPosWnd::Notify( NotifyEvent& rNEvt )
 {
-    bool nHandled = false;
+    bool bHandled = true;
 
-    if ( rNEvt.GetType() == EVENT_KEYINPUT )
+    switch (rNEvt.GetType())
     {
-        const KeyEvent* pKEvt = rNEvt.GetKeyEvent();
-
-        switch ( pKEvt->GetKeyCode().GetCode() )
+        case EVENT_KEYINPUT:
         {
-            case KEY_RETURN:
-                DoEnter();
-                nHandled = true;
-                break;
+            const KeyEvent* pKEvt = rNEvt.GetKeyEvent();
 
-            case KEY_ESCAPE:
-                if (nTipVisible)
-                {
-                    // escape when the tip help is shown: only hide the tip
-                    HideTip();
-                }
-                else
-                {
-                    if (!bFormulaMode)
-                        SetText( aPosStr );
-                    ReleaseFocus_Impl();
-                }
-                nHandled = true;
-                break;
+            switch ( pKEvt->GetKeyCode().GetCode() )
+            {
+                case KEY_RETURN:
+                    DoEnter();
+                    break;
+
+                case KEY_ESCAPE:
+                    if (nTipVisible)
+                    {
+                        // escape when the tip help is shown: only hide the tip
+                        HideTip();
+                    }
+                    else
+                    {
+                        if (!bFormulaMode)
+                            SetText( aPosStr );
+                        ReleaseFocus_Impl();
+                    }
+                    break;
+            }
         }
+        break;
+        case EVENT_GETFOCUS:
+        {
+            // Select the whole text upon focus.
+            OUString aStr = GetText();
+            SetSelection(Selection(0, aStr.getLength()));
+        }
+        break;
+        case EVENT_LOSEFOCUS:
+            HideTip();
+            bHandled = false;
+        break;
+        default:
+            bHandled = false;
     }
 
-    if ( !nHandled )
-        nHandled = ComboBox::Notify( rNEvt );
+    if (!bHandled)
+        bHandled = ComboBox::Notify(rNEvt);
 
-    if ( rNEvt.GetType() == EVENT_LOSEFOCUS )
-        HideTip();
-
-    return nHandled;
+    return bHandled;
 }
 
 void ScPosWnd::ReleaseFocus_Impl()

commit b36b87678842600d6a0ca30715a2ffb560780d30

tdf#83859: Make arrow keys work again in named ranges dropdown
Regression from bbdb3c009add537c2 which slightly changed the logic
and set bHandled to true by default, thus effectively throwing all
key press events except for Enter and Esc away.

Set key press events (minus Enter & Esc) to bHandled = false and let
them fall through to be dealt with in the base class (ComboBox)

Change-Id: Ie1d857725a7de1d0bed1fd59df7eef5390ae5f3d

sc/source/ui/app/inputwin.cxx
@@ -2504,6 +2504,10 @@ bool ScPosWnd::Notify( NotifyEvent& rNEvt )
                         ReleaseFocus_Impl();
                     }
                     break;
+
+                default:
+                    bHandled = false;
+                    break;
             }
         }
         break;