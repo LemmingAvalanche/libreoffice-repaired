LemmingAvalanche/libreoffice-repaired

commit e1696d14108f26fd4665f1d9020753c244a9fa8c

SwViewShell::ImplEndAction: avoid direct paint
Commit f04274dd2ea73e17a9c4b76fb69fbd59e4bb4231 (sw: invalidate after
keypresses when we have a callback, 2015-01-13) only enabled
invalidate-then-paint for the tiled rendering case, because the main
loop had no priorities back then.

This limitation went away with bc9316412ca9bfd4981d467fa77d6d407e285803
(tdf#91727 - Unwind non-dispatch of idle handlers., 2015-06-10), so it's
not a problem to always do this panting inside the Paint() call.

Change-Id: I98ddfcc0f20f1553974b74ec3c859dde315612e4
Reviewed-on: https://gerrit.libreoffice.org/16587
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

sw/source/core/view/viewsh.cxx
@@ -410,10 +410,7 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
 
                         if ( bPaintsFromSystem )
                             PaintDesktop(*GetOut(), aRect);
-                        if (!isTiledRendering())
-                            pCurrentLayout->Paint( *mpOut, aRect );
-                        else
-                            pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
+                        pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
 
                         // #i75172# end DrawingLayer paint
                         DLPostPaint2(true);

commit fbde824ea6ff02f6e920d80c1ff1e2a4cf2bf0f9

tdf#92577 sw rendercontext: paint transparent from controls conditionally
Regression from commit e1696d14108f26fd4665f1d9020753c244a9fa8c
(SwViewShell::ImplEndAction: avoid direct paint, 2015-06-29), the
problem was that the lcl_PaintTransparentFormControls() call performs
direct paint, so it should be called only in case we don't do an async
paint via invalidate.

As expected, this call is no longer needed for the original i#107365
bug document in case SwViewShell::Paint() will be called by the main
loop shortly.

Change-Id: Ia27c551ed838d610f972f20abbb4ae9f0e1241b6

sw/source/core/view/viewsh.cxx
@@ -407,8 +407,8 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
                     {
                         InvalidateWindows(aRect.SVRect());
                     }
-
-                    lcl_PaintTransparentFormControls(*this, aRect); // i#107365
+                    else
+                        lcl_PaintTransparentFormControls(*this, aRect); // i#107365
                 }
 
                 pVout.disposeAndClear();