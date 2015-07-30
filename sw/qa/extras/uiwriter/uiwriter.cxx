LemmingAvalanche/libreoffice-repaired

commit 146a0e6b6fe8fd78b806121a1ac54368927f781e

123792: complete annotations on text ranges feature
- rely annotations on text ranges on new annotation marks
- support arbitrary text ranges for annotations
- fix undo/redo regarding annotations an text ranges
- support annotations on overlapping text ranges
- fix *.docx import for annotations on overlapping text ranges
- fix ODF import of annotations on text ranges

cherry-picked from branch ooxml-osba

(cherry picked from commit ebd2922ebd3cf2d3899fb80780d38f367197aadf)

Conflicts:
	sw/Library_sw.mk
	sw/inc/IDocumentMarkAccess.hxx
	sw/inc/SidebarWin.hxx
	sw/inc/doc.hxx
	sw/inc/docufld.hxx
	sw/inc/hintids.hxx
	sw/inc/ndhints.hxx
	sw/inc/txatbase.hxx
	sw/inc/unobookmark.hxx
	sw/inc/unofield.hxx
	sw/inc/unoport.hxx
	sw/source/core/access/accpara.cxx
	sw/source/core/bastyp/init.cxx
	sw/source/core/crsr/bookmrk.cxx
	sw/source/core/crsr/findtxt.cxx
	sw/source/core/doc/dbgoutsw.cxx
	sw/source/core/doc/docbm.cxx
	sw/source/core/doc/doccorr.cxx
	sw/source/core/doc/docdde.cxx
	sw/source/core/doc/docedt.cxx
	sw/source/core/doc/docfld.cxx
	sw/source/core/doc/doclay.cxx
	sw/source/core/doc/tblrwcl.cxx
	sw/source/core/docnode/ndcopy.cxx
	sw/source/core/docnode/nodes.cxx
	sw/source/core/docnode/section.cxx
	sw/source/core/edit/edfld.cxx
	sw/source/core/fields/docufld.cxx
	sw/source/core/fields/postithelper.cxx
	sw/source/core/fields/reffld.cxx
	sw/source/core/inc/MarkManager.hxx
	sw/source/core/inc/crossrefbookmark.hxx
	sw/source/core/text/inftxt.cxx
	sw/source/core/text/itratr.cxx
	sw/source/core/text/txtfrm.cxx
	sw/source/core/txtnode/atrfld.cxx
	sw/source/core/txtnode/ndtxt.cxx
	sw/source/core/txtnode/thints.cxx
	sw/source/core/undo/undel.cxx
	sw/source/core/undo/undobj.cxx
	sw/source/core/unocore/unofield.cxx
	sw/source/core/unocore/unoport.cxx
	sw/source/core/unocore/unoportenum.cxx
	sw/source/filter/html/htmlatr.cxx
	sw/source/filter/html/htmlgrin.cxx
	sw/source/filter/html/wrthtml.cxx
	sw/source/filter/writer/writer.cxx
	sw/source/filter/ww1/fltshell.cxx

sw/source/ui/docvw/SidebarWin.cxx
@@ -25,6 +25,7 @@
 #include <SidebarTxtControl.hxx>
 #include <AnchorOverlayObject.hxx>
 #include <ShadowOverlayObject.hxx>
+#include <OverlayRanges.hxx>
 
 #include <annotation.hrc>
 #include <popup.hrc>
@@ -63,6 +64,9 @@
 #include <swmodule.hxx>
 #include <langhelper.hxx>
 
+#include <txtannotationfld.hxx>
+#include <ndtxt.hxx>
+
 #include <sw_primitivetypes2d.hxx>
 #include <drawinglayer/primitive2d/primitivetools2d.hxx>
 #include <drawinglayer/primitive2d/fillgradientprimitive2d.hxx>
@@ -95,8 +99,9 @@ SwSidebarWin::SwSidebarWin( SwEditWin& rEditWin,
     , mpMetadataAuthor(0)
     , mpMetadataDate(0)
     , mpMenuButton(0)
-    , mpAnchor(0)
-    , mpShadow(0)
+    , mpAnchor( NULL )
+    , mpShadow( NULL )
+    , mpTextRangeOverlay( NULL )
     , mColorAnchor()
     , mColorDark()
     , mColorLight()
@@ -119,7 +124,7 @@ SwSidebarWin::SwSidebarWin( SwEditWin& rEditWin,
     }
 
     mrMgr.ConnectSidebarWinToFrm( *(mrSidebarItem.maLayoutInfo.mpAnchorFrm),
-                                  *(mrSidebarItem.GetFmtFld()),
+                                  mrSidebarItem.GetFmtFld(),
                                   *this );
 }
 
@@ -174,10 +179,13 @@ SwSidebarWin::~SwSidebarWin()
     }
 
     AnchorOverlayObject::DestroyAnchorOverlayObject( mpAnchor );
-    mpAnchor = 0;
+    mpAnchor = NULL;
 
     ShadowOverlayObject::DestroyShadowOverlayObject( mpShadow );
-    mpShadow = 0;
+    mpShadow = NULL;
+
+    delete mpTextRangeOverlay;
+    mpTextRangeOverlay = NULL;
 
     delete mpMenuButton;
     mpMenuButton = 0;
@@ -216,11 +224,11 @@ void SwSidebarWin::SetPosSizePixelRect( long nX,
                                         long nY,
                                         long nWidth,
                                         long nHeight,
-                                        const SwRect &aRect,
+                                        const SwRect& aAnchorRect,
                                         const long aPageBorder)
 {
     mPosSize = Rectangle(Point(nX,nY),Size(nWidth,nHeight));
-    mAnchorRect = aRect;
+    mAnchorRect = aAnchorRect;
     mPageBorder = aPageBorder;
 }
 
@@ -559,6 +567,69 @@ void SwSidebarWin::SetPosAndSize()
                 pWin->Anchor()->SetAnchorState(AS_END);
             }
         }
+    }
+
+    // text range overlay
+    if ( mrSidebarItem.maLayoutInfo.mnStartNodeIdx != 0
+         && mrSidebarItem.maLayoutInfo.mnStartContent != -1 )
+    {
+        std::vector< basegfx::B2DRange > aAnnotationTextRanges;
+        {
+            const SwTxtAnnotationFld* pTxtAnnotationFld =
+                dynamic_cast< const SwTxtAnnotationFld* >( mrSidebarItem.GetFmtFld().GetTxtFld() );
+            if ( pTxtAnnotationFld != NULL
+                 && pTxtAnnotationFld->GetpTxtNode() != NULL )
+            {
+                SwTxtNode* pTxtNode = pTxtAnnotationFld->GetpTxtNode();
+                SwNodes& rNds = pTxtNode->GetDoc()->GetNodes();
+                SwCntntNode* const pCntntNd = rNds[mrSidebarItem.maLayoutInfo.mnStartNodeIdx]->GetCntntNode();
+                SwPosition aStartPos( *pCntntNd, mrSidebarItem.maLayoutInfo.mnStartContent );
+                ::boost::scoped_ptr<SwShellCrsr> pTmpCrsrForAnnotationTextRange(
+                    new SwShellCrsr( DocView().GetWrtShell(), aStartPos ) );
+                pTmpCrsrForAnnotationTextRange->SetMark();
+                pTmpCrsrForAnnotationTextRange->GetMark()->nNode = *pTxtNode;
+                pTmpCrsrForAnnotationTextRange->GetMark()->nContent.Assign( pTxtNode, *(pTxtAnnotationFld->GetStart())+1 );
+
+                pTmpCrsrForAnnotationTextRange->FillRects();
+
+                for( sal_uInt16 a(0); a < pTmpCrsrForAnnotationTextRange->size(); ++a )
+                {
+                    const SwRect aNextRect((*pTmpCrsrForAnnotationTextRange)[a]);
+                    const Rectangle aPntRect(aNextRect.SVRect());
+
+                    aAnnotationTextRanges.push_back(basegfx::B2DRange(
+                        aPntRect.Left(), aPntRect.Top(),
+                        aPntRect.Right() + 1, aPntRect.Bottom() + 1));
+                }
+            }
+        }
+
+        if ( mpTextRangeOverlay != NULL )
+        {
+            mpTextRangeOverlay->setRanges( aAnnotationTextRanges );
+            if ( mpAnchor != NULL && mpAnchor->getLineSolid() )
+            {
+                mpTextRangeOverlay->ShowSolidBorder();
+            }
+            else
+            {
+                mpTextRangeOverlay->HideSolidBorder();
+            }
+        }
+        else
+        {
+            mpTextRangeOverlay =
+                sw::overlay::OverlayRanges::CreateOverlayRange(
+                    DocView(),
+                    mColorAnchor,
+                    aAnnotationTextRanges,
+                    mpAnchor != NULL ? mpAnchor->getLineSolid() : false );
+        }
+    }
+    else
+    {
+        delete mpTextRangeOverlay;
+        mpTextRangeOverlay = NULL;
     }
 }
 
@@ -1106,6 +1177,10 @@ void SwSidebarWin::SetViewState(ViewState bViewState)
                     pWin->Anchor()->SetAnchorState(AS_END);
                 }
                 mpAnchor->setLineSolid(true);
+                if ( mpTextRangeOverlay != NULL )
+                {
+                    mpTextRangeOverlay->ShowSolidBorder();
+                }
             }
             if (mpShadow)
                 mpShadow->SetShadowState(SS_EDIT);
@@ -1114,7 +1189,13 @@ void SwSidebarWin::SetViewState(ViewState bViewState)
         case VS_VIEW:
         {
             if (mpAnchor)
+            {
                 mpAnchor->setLineSolid(true);
+                if ( mpTextRangeOverlay != NULL )
+                {
+                    mpTextRangeOverlay->ShowSolidBorder();
+                }
+            }
             if (mpShadow)
                 mpShadow->SetShadowState(SS_VIEW);
             break;
@@ -1139,11 +1220,19 @@ void SwSidebarWin::SetViewState(ViewState bViewState)
                         if ( pTopWinSelf != mrMgr.GetActiveSidebarWin() )
                         {
                             pTopWinSelf->Anchor()->setLineSolid(false);
+                            if ( pTopWinSelf->TextRange() != NULL )
+                            {
+                                pTopWinSelf->TextRange()->HideSolidBorder();
+                            }
                         }
                         pTopWinSelf->Anchor()->SetAnchorState(AS_ALL);
                     }
                 }
                 mpAnchor->setLineSolid(false);
+                if ( mpTextRangeOverlay != NULL )
+                {
+                    mpTextRangeOverlay->HideSolidBorder();
+                }
             }
             if ( mpShadow )
             {
@@ -1227,7 +1316,7 @@ void SwSidebarWin::ChangeSidebarItem( SwSidebarItem& rSidebarItem )
     if ( bAnchorChanged )
     {
         mrMgr.ConnectSidebarWinToFrm( *(mrSidebarItem.maLayoutInfo.mpAnchorFrm),
-                                      *(mrSidebarItem.GetFmtFld()),
+                                      mrSidebarItem.GetFmtFld(),
                                       *this );
     }
 }

commit a9ac70223b87ec599ab9be310f1321820c761301

tdf#89720 sw: do not create text range overlay for reply annotations
Regression from commit 146a0e6b6fe8fd78b806121a1ac54368927f781e (123792:
complete annotations on text ranges feature, 2013-12-19), the problem
was that in case a comment had a reply, then both comments produced its
range overlay, and this repeated semi-transparent overlay resulted in
dark color.

Go back to the old behavior of just painting the overlay for the
non-reply comment; for real, partly overlapping comments showing the
exact boundaries of the overlapping overlays still makes sense.

Change-Id: Ic2673a1a41146ff547262ba681b34a63f41aa8ff

sw/source/uibase/docvw/SidebarWin.cxx
@@ -703,8 +703,9 @@ void SwSidebarWin::SetPosAndSize()
                 mpTextRangeOverlay->HideSolidBorder();
             }
         }
-        else
+        else if (!IsFollow())
         {
+            // This window is not a reply, then draw its range overlay.
             mpTextRangeOverlay =
                 sw::overlay::OverlayRanges::CreateOverlayRange(
                     DocView(),

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -37,6 +37,9 @@
 #include <unocrsrhelper.hxx>
 #include <unotbl.hxx>
 #include <pagedesc.hxx>
+#include <postithelper.hxx>
+#include <PostItMgr.hxx>
+#include <SidebarWin.hxx>
 #include "com/sun/star/text/XDefaultNumberingProvider.hpp"
 #include "com/sun/star/awt/FontUnderline.hpp"
 
@@ -138,6 +141,7 @@ public:
     void testShapeAnchorUndo();
     void testDde();
     void testTdf89954();
+    void testTdf89720();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -200,6 +204,7 @@ public:
     CPPUNIT_TEST(testShapeAnchorUndo);
     CPPUNIT_TEST(testDde);
     CPPUNIT_TEST(testTdf89954);
+    CPPUNIT_TEST(testTdf89720);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -2061,6 +2066,20 @@ void SwUiWriterTest::testTdf89954()
     // As a result, autocorrect did not turn the 't' input into 'T'.
     OUString aExpected("Tes\xef\xbf\xb9t. Test.", 14, RTL_TEXTENCODING_UTF8);
     CPPUNIT_ASSERT_EQUAL(aExpected, aNodeIndex.GetNode().GetTextNode()->GetText());
+}
+
+void SwUiWriterTest::testTdf89720()
+{
+    SwDoc* pDoc = createDoc("tdf89720.odt");
+    SwView* pView = pDoc->GetDocShell()->GetView();
+    SwPostItMgr* pPostItMgr = pView->GetPostItMgr();
+    for (SwSidebarItem* pItem : *pPostItMgr)
+    {
+        if (pItem->pPostIt->IsFollow())
+            // This was non-0: reply comments had a text range overlay,
+            // resulting in unexpected dark color.
+            CPPUNIT_ASSERT(!pItem->pPostIt->TextRange());
+    }
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);