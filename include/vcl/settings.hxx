LemmingAvalanche/libreoffice-repaired

commit d4cec5730f6a8d14e1725a23eb13108b975cae98

convert SETTINGS_ #defines to 'enum class'
and dump the ones that nothing is listening to

Change-Id: I253ef284df785812a439dd160edba1b07fdbaac4

include/vcl/settings.hxx
@@ -29,6 +29,7 @@
 #include <vcl/wall.hxx>
 #include <i18nlangtag/languagetag.hxx>
 #include <unotools/syslocale.hxx>
+#include <o3tl/typed_flags_set.hxx>
 
 #include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
@@ -618,21 +619,16 @@ public:
 
 // - AllSettings -
 
-const int SETTINGS_MOUSE = 0x00000001;
-const int SETTINGS_STYLE = 0x00000002;
-const int SETTINGS_MISC = 0x00000004;
-const int SETTINGS_SOUND = 0x00000008;
-const int SETTINGS_HELP = 0x00000010;
-const int SETTINGS_LOCALE = 0x00000020;
-const int SETTINGS_UILOCALE = 0x00000040;
-const int SETTINGS_ALLSETTINGS =   ( SETTINGS_MOUSE |
-                                     SETTINGS_STYLE | SETTINGS_MISC |
-                                     SETTINGS_SOUND |
-                                     SETTINGS_HELP |
-                                     SETTINGS_LOCALE | SETTINGS_UILOCALE );
-const int SETTINGS_IN_UPDATE_SETTINGS = 0x00000800;   // this flag indicates that the data changed event was created
-                                                          // in Windows::UpdateSettings probably because of a global
-                                                          // settings changed
+enum class AllSettingsFlags {
+    NONE     = 0x0000,
+    STYLE    = 0x0002,
+    MISC     = 0x0004,
+    LOCALE   = 0x0020,
+};
+namespace o3tl
+{
+    template<> struct typed_flags<AllSettingsFlags> : is_typed_flags<AllSettingsFlags, 0x026> {};
+}
 
 class VCL_DLLPUBLIC AllSettings
 {
@@ -669,10 +665,10 @@ public:
     const vcl::I18nHelper&                  GetLocaleI18nHelper() const;
     const vcl::I18nHelper&                  GetUILocaleI18nHelper() const;
 
-    sal_uLong                               GetWindowUpdate() const;
+    AllSettingsFlags                        GetWindowUpdate() const;
 
-    sal_uLong                               Update( sal_uLong nFlags, const AllSettings& rSettings );
-    sal_uLong                               GetChangeFlags( const AllSettings& rSettings ) const;
+    AllSettingsFlags                        Update( AllSettingsFlags nFlags, const AllSettings& rSettings );
+    AllSettingsFlags                        GetChangeFlags( const AllSettings& rSettings ) const;
 
     bool                                    operator ==( const AllSettings& rSet ) const;
     bool                                    operator !=( const AllSettings& rSet ) const;

vcl/source/app/settings.cxx
@@ -240,7 +240,7 @@ struct ImplAllSettingsData
     MiscSettings                            maMiscSettings;
     HelpSettings                            maHelpSettings;
     LanguageTag                             maLocale;
-    sal_uLong                               mnWindowUpdate;
+    AllSettingsFlags                        mnWindowUpdate;
     LanguageTag                             maUILocale;
     LocaleDataWrapper*                      mpLocaleDataWrapper;
     LocaleDataWrapper*                      mpUILocaleDataWrapper;
@@ -2640,7 +2640,7 @@ ImplAllSettingsData::ImplAllSettingsData()
         maLocale( LANGUAGE_SYSTEM ),
         maUILocale( LANGUAGE_SYSTEM )
 {
-    mnWindowUpdate              = SETTINGS_ALLSETTINGS;
+    mnWindowUpdate              = AllSettingsFlags::STYLE | AllSettingsFlags::MISC | AllSettingsFlags::LOCALE;
     mpLocaleDataWrapper         = NULL;
     mpUILocaleDataWrapper       = NULL;
     mpI18nHelper                = NULL;
@@ -2699,87 +2699,56 @@ void AllSettings::CopyData()
 
 }
 
-sal_uLong AllSettings::Update( sal_uLong nFlags, const AllSettings& rSet )
+AllSettingsFlags AllSettings::Update( AllSettingsFlags nFlags, const AllSettings& rSet )
 {
 
-    sal_uLong nChangeFlags = 0;
+    AllSettingsFlags nChangeFlags = AllSettingsFlags::NONE;
 
-    if ( nFlags & SETTINGS_MOUSE )
-    {
-        if ( mpData->maMouseSettings != rSet.mpData->maMouseSettings )
-        {
-            CopyData();
-            mpData->maMouseSettings = rSet.mpData->maMouseSettings;
-            nChangeFlags |= SETTINGS_MOUSE;
-        }
-    }
-
-    if ( nFlags & SETTINGS_STYLE )
+    if ( nFlags & AllSettingsFlags::STYLE )
     {
         if ( mpData->maStyleSettings != rSet.mpData->maStyleSettings )
         {
             CopyData();
             mpData->maStyleSettings = rSet.mpData->maStyleSettings;
-            nChangeFlags |= SETTINGS_STYLE;
+            nChangeFlags |= AllSettingsFlags::STYLE;
         }
     }
 
-    if ( nFlags & SETTINGS_MISC )
+    if ( nFlags & AllSettingsFlags::MISC )
     {
         if ( mpData->maMiscSettings != rSet.mpData->maMiscSettings )
         {
             CopyData();
             mpData->maMiscSettings = rSet.mpData->maMiscSettings;
-            nChangeFlags |= SETTINGS_MISC;
+            nChangeFlags |= AllSettingsFlags::MISC;
         }
     }
 
-    if ( nFlags & SETTINGS_HELP )
-    {
-        if ( mpData->maHelpSettings != rSet.mpData->maHelpSettings )
-        {
-            CopyData();
-            mpData->maHelpSettings = rSet.mpData->maHelpSettings;
-            nChangeFlags |= SETTINGS_HELP;
-        }
-    }
-
-    if ( nFlags & SETTINGS_LOCALE )
+    if ( nFlags & AllSettingsFlags::LOCALE )
     {
         if ( mpData->maLocale != rSet.mpData->maLocale )
         {
             SetLanguageTag( rSet.mpData->maLocale );
-            nChangeFlags |= SETTINGS_LOCALE;
+            nChangeFlags |= AllSettingsFlags::LOCALE;
         }
-    }
-
-    if ( nFlags & SETTINGS_UILOCALE )
-    {
-        // UILocale can't be changed
     }
 
     return nChangeFlags;
 }
 
-sal_uLong AllSettings::GetChangeFlags( const AllSettings& rSet ) const
+AllSettingsFlags AllSettings::GetChangeFlags( const AllSettings& rSet ) const
 {
 
-    sal_uLong nChangeFlags = 0;
-
-    if ( mpData->maMouseSettings != rSet.mpData->maMouseSettings )
-        nChangeFlags |= SETTINGS_MOUSE;
+    AllSettingsFlags nChangeFlags = AllSettingsFlags::NONE;
 
     if ( mpData->maStyleSettings != rSet.mpData->maStyleSettings )
-        nChangeFlags |= SETTINGS_STYLE;
+        nChangeFlags |= AllSettingsFlags::STYLE;
 
     if ( mpData->maMiscSettings != rSet.mpData->maMiscSettings )
-        nChangeFlags |= SETTINGS_MISC;
-
-    if ( mpData->maHelpSettings != rSet.mpData->maHelpSettings )
-        nChangeFlags |= SETTINGS_HELP;
+        nChangeFlags |= AllSettingsFlags::MISC;
 
     if ( mpData->maLocale != rSet.mpData->maLocale )
-        nChangeFlags |= SETTINGS_LOCALE;
+        nChangeFlags |= AllSettingsFlags::LOCALE;
 
     return nChangeFlags;
 }
@@ -3076,7 +3045,7 @@ AllSettings::GetHelpSettings() const
     return mpData->maHelpSettings;
 }
 
-sal_uLong
+AllSettingsFlags
 AllSettings::GetWindowUpdate() const
 {
     return mpData->mnWindowUpdate;

vcl/source/window/settings.cxx
@@ -57,12 +57,12 @@ void Window::SetSettings( const AllSettings& rSettings, bool bChild )
 
     AllSettings aOldSettings(*mxSettings);
     OutputDevice::SetSettings( rSettings );
-    sal_uLong nChangeFlags = aOldSettings.GetChangeFlags( rSettings );
+    AllSettingsFlags nChangeFlags = aOldSettings.GetChangeFlags( rSettings );
 
     // recalculate AppFont-resolution and DPI-resolution
     ImplInitResolutionSettings();
 
-    if ( nChangeFlags )
+    if ( bool(nChangeFlags) )
     {
         DataChangedEvent aDCEvt( DataChangedEventType::SETTINGS, &aOldSettings, nChangeFlags );
         DataChanged( aDCEvt );
@@ -91,10 +91,7 @@ void Window::UpdateSettings( const AllSettings& rSettings, bool bChild )
     }
 
     AllSettings aOldSettings(*mxSettings);
-    sal_uLong nChangeFlags = mxSettings->Update( mxSettings->GetWindowUpdate(), rSettings );
-    nChangeFlags |= SETTINGS_IN_UPDATE_SETTINGS; // Set this flag so the receiver of the data changed
-                                                 // event can distinguish between the changing of global
-                                                 // setting and a local change ( with SetSettings )
+    AllSettingsFlags nChangeFlags = mxSettings->Update( mxSettings->GetWindowUpdate(), rSettings );
 
     // recalculate AppFont-resolution and DPI-resolution
     ImplInitResolutionSettings();
@@ -110,7 +107,7 @@ void Window::UpdateSettings( const AllSettings& rSettings, bool bChild )
     aSet.SetWheelBehavior( aOldSettings.GetMouseSettings().GetWheelBehavior() );
     mxSettings->SetMouseSettings( aSet );
 
-    if( (nChangeFlags & SETTINGS_STYLE) && IsBackground() )
+    if( (nChangeFlags & AllSettingsFlags::STYLE) && IsBackground() )
     {
         Wallpaper aWallpaper = GetBackground();
         if( !aWallpaper.IsBitmap() && !aWallpaper.IsGradient() )
@@ -128,7 +125,7 @@ void Window::UpdateSettings( const AllSettings& rSettings, bool bChild )
         }
     }
 
-    if ( nChangeFlags )
+    if ( bool(nChangeFlags) )
     {
         DataChangedEvent aDCEvt( DataChangedEventType::SETTINGS, &aOldSettings, nChangeFlags );
         DataChanged( aDCEvt );

commit 2d5a6dcf75558f2b919e5980b069534f670d904b

tdf#92788 - Middle Button on Mouse Paste Option Broken for X11
caused by commit d4cec5730f6a8d14e1725a23eb13108b975cae98
"convert SETTINGS_ #defines to 'enum class'"

Change-Id: Id0c2738a61f73223f6c8716f04a619c8cb84c0a9

include/vcl/settings.hxx
@@ -682,13 +682,14 @@ public:
 
 enum class AllSettingsFlags {
     NONE     = 0x0000,
+    MOUSE    = 0x0001,
     STYLE    = 0x0002,
     MISC     = 0x0004,
     LOCALE   = 0x0020,
 };
 namespace o3tl
 {
-    template<> struct typed_flags<AllSettingsFlags> : is_typed_flags<AllSettingsFlags, 0x026> {};
+    template<> struct typed_flags<AllSettingsFlags> : is_typed_flags<AllSettingsFlags, 0x0027> {};
 }
 
 class VCL_DLLPUBLIC AllSettings

vcl/source/app/settings.cxx
@@ -2701,7 +2701,8 @@ ImplAllSettingsData::ImplAllSettingsData()
         maLocale( LANGUAGE_SYSTEM ),
         maUILocale( LANGUAGE_SYSTEM )
 {
-    mnWindowUpdate              = AllSettingsFlags::STYLE | AllSettingsFlags::MISC | AllSettingsFlags::LOCALE;
+    mnWindowUpdate              = AllSettingsFlags::MOUSE | AllSettingsFlags::STYLE |
+                                  AllSettingsFlags::MISC | AllSettingsFlags::LOCALE;
     mpLocaleDataWrapper         = NULL;
     mpUILocaleDataWrapper       = NULL;
     mpI18nHelper                = NULL;
@@ -2762,6 +2763,16 @@ AllSettingsFlags AllSettings::Update( AllSettingsFlags nFlags, const AllSettings
 {
 
     AllSettingsFlags nChangeFlags = AllSettingsFlags::NONE;
+
+    if ( nFlags & AllSettingsFlags::MOUSE )
+    {
+        if ( mxData->maMouseSettings != rSet.mxData->maMouseSettings )
+        {
+            CopyData();
+            mxData->maMouseSettings = rSet.mxData->maMouseSettings;
+            nChangeFlags |= AllSettingsFlags::MOUSE;
+        }
+    }
 
     if ( nFlags & AllSettingsFlags::STYLE )
     {