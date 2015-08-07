LemmingAvalanche/libreoffice-repaired

commit 0df7a9f7c2fdaad35b8f35ca808edfcfb3ac5dc9

fdo#82191 sw::DocumentLayoutManager: copy textbox content of draw formats
The SwFmtCntnt, i.e. the content of the draw format was already copied,
but that's only a pointer to the real content: instead duplicate the
real contents on copy&paste, that's how we copy fly frames as well.

Change-Id: I42475e356aaa1c54c08fb23a6a395d1726e5f33e

sw/source/core/doc/DocumentLayoutManager.cxx
@@ -39,6 +39,7 @@
 #include <unoframe.hxx>
 #include <docary.hxx>
 #include <dcontact.hxx>
+#include <textboxhelper.hxx>
 
 using namespace ::com::sun::star;
 
@@ -492,6 +493,16 @@ SwFrmFmt *DocumentLayoutManager::CopyLayoutFmt(
 
     if( bMakeFrms )
         pDest->MakeFrms();
+
+    // If the draw format has a TextBox, then copy its fly format as well.
+    if (SwFrmFmt* pSourceTextBox = SwTextBoxHelper::findTextBox(&rSource))
+    {
+        SwFrmFmt* pDestTextBox = CopyLayoutFmt(*pSourceTextBox, rNewAnchor, bSetTxtFlyAtt, bMakeFrms);
+        SwAttrSet aSet(pDest->GetAttrSet());
+        SwFmtCntnt aCntnt(pDestTextBox->GetCntnt().GetCntntIdx()->GetNode().GetStartNode());
+        aSet.Put(aCntnt);
+        pDest->SetFmtAttr(aSet);
+    }
 
     return pDest;
 }

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -18,6 +18,7 @@
 #include <section.hxx>
 #include <fmtclds.hxx>
 #include <dcontact.hxx>
+#include <textboxhelper.hxx>
 
 #include <svx/svdpage.hxx>
 #include <svx/svdview.hxx>
@@ -46,6 +47,7 @@ public:
     void testCp1000071();
     void testShapeTextboxVertadjust();
     void testShapeTextboxAutosize();
+    void testFdo82191();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -62,6 +64,7 @@ public:
     CPPUNIT_TEST(testCp1000071);
     CPPUNIT_TEST(testShapeTextboxVertadjust);
     CPPUNIT_TEST(testShapeTextboxAutosize);
+    CPPUNIT_TEST(testFdo82191);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -388,6 +391,27 @@ void SwUiWriterTest::testShapeTextboxAutosize()
     // same height as the first, even though the first contained 1 paragraph
     // and the other 2 ones.
     CPPUNIT_ASSERT(pFirst->GetSnapRect().getHeight() < pSecond->GetSnapRect().getHeight());
+}
+
+void SwUiWriterTest::testFdo82191()
+{
+    SwDoc* pDoc = createDoc("fdo82191.odt");
+    SdrPage* pPage = pDoc->getIDocumentDrawModelAccess().GetDrawModel()->GetPage(0);
+    std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pDoc);
+    // Make sure we have a single draw shape.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), SwTextBoxHelper::getCount(pPage, aTextBoxes));
+
+    SwDoc aClipboard;
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SdrObject* pObject = pPage->GetObj(0);
+    // Select it, then copy and paste.
+    pWrtShell->SelectObj(Point(), 0, pObject);
+    pWrtShell->Copy(&aClipboard);
+    pWrtShell->Paste(&aClipboard);
+
+    aTextBoxes = SwTextBoxHelper::findTextBoxes(pDoc);
+    // This was one: the textbox of the shape wasn't copied.
+    CPPUNIT_ASSERT_EQUAL(size_t(2), aTextBoxes.size());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);

sw/source/core/frmedt/fecopy.cxx
@@ -75,6 +75,7 @@
 #include <docsh.hxx>
 #include <pagedesc.hxx>
 #include <mvsave.hxx>
+#include <textboxhelper.hxx>
 #include <vcl/virdev.hxx>
 #include <svx/svdundo.hxx>
 
@@ -884,6 +885,7 @@ bool SwFEShell::Paste( SwDoc* pClpDoc, bool bIncludingPageFrames )
             if( !Imp()->GetDrawView() )
                 MakeDrawView();
 
+            std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pClpDoc);
             for ( sal_uInt16 i = 0; i < pClpDoc->GetSpzFrmFmts()->size(); ++i )
             {
                 bool bInsWithFmt = true;
@@ -952,6 +954,10 @@ bool SwFEShell::Paste( SwDoc* pClpDoc, bool bIncludingPageFrames )
                             if (pCpyObj && CheckControlLayer(pCpyObj))
                                 continue;
                         }
+
+                        // Ignore TextBoxes, they are already handled in sw::DocumentLayoutManager::CopyLayoutFmt().
+                        if (aTextBoxes.find(&rCpyFmt) != aTextBoxes.end())
+                            continue;
 
                         aAnchor.SetAnchor( pPos );
                     }

commit fb938d6e4b70af14989c4dd70ac8389ea7d610e7

sw: fix ~SwIndexReg() assertion on import of tdf92157-1.docx
Problem is that the TextBox SwFrmFmt of a draw object gets a FLY_AS_CHAR
anchor set, which is then not updated later before nodes are deleted.

(regression from 0df7a9f7c2fdaad35b8f35ca808edfcfb3ac5dc9)

Change-Id: I906ee47b5c30944a08fd8a0d7d6ea8b8c9e6a6a8

sw/source/core/doc/DocumentLayoutManager.cxx
@@ -496,7 +496,16 @@ SwFrameFormat *DocumentLayoutManager::CopyLayoutFormat(
     // If the draw format has a TextBox, then copy its fly format as well.
     if (SwFrameFormat* pSourceTextBox = SwTextBoxHelper::findTextBox(&rSource))
     {
-        SwFrameFormat* pDestTextBox = CopyLayoutFormat(*pSourceTextBox, rNewAnchor, bSetTextFlyAtt, bMakeFrms);
+        SwFormatAnchor boxAnchor(rNewAnchor);
+        if (FLY_AS_CHAR == boxAnchor.GetAnchorId())
+        {
+            // AS_CHAR *must not* be set on textbox fly-frame
+            boxAnchor.SetType(FLY_AT_CHAR);
+        }
+        // presumably these anchors are supported though not sure
+        assert(FLY_AT_CHAR == boxAnchor.GetAnchorId() || FLY_AT_PARA == boxAnchor.GetAnchorId());
+        SwFrameFormat* pDestTextBox = CopyLayoutFormat(*pSourceTextBox,
+                boxAnchor, bSetTextFlyAtt, bMakeFrms);
         SwAttrSet aSet(pDest->GetAttrSet());
         SwFormatContent aContent(pDestTextBox->GetContent().GetContentIdx()->GetNode().GetStartNode());
         aSet.Put(aContent);