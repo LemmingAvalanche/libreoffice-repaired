LemmingAvalanche/libreoffice-repaired

commit 158852f2dffeda210605404e2de9179f0eed6e49

tdf#76649 Skip NaN initial values for min and max
min and max were initiated as aValuesX[0] which could be NaN

Change-Id: I229f4c8f8fda54684e1c817ea7da06fd87eb79b9
Reviewed-on: https://gerrit.libreoffice.org/17343
Reviewed-by: Jan Holesovsky <kendy@collabora.com>
Tested-by: Jan Holesovsky <kendy@collabora.com>

chart2/source/view/main/VDataSeries.cxx
@@ -520,9 +520,13 @@ void VDataSeries::getMinMaxXValue(double& fMin, double& fMax) const
 
     if(aValuesX.getLength() > 0)
     {
-        fMax = fMin = aValuesX[0];
+        sal_Int32 i = 0;
+        while ( ::rtl::math::isNan( aValuesX[i] ) && i < aValuesX.getLength() )
+            i++;
+        if ( i < aValuesX.getLength() )
+            fMax = fMin = aValuesX[i++];
 
-        for (sal_Int32 i = 1; i < aValuesX.getLength(); i++)
+        for ( ; i < aValuesX.getLength(); i++)
         {
             const double aValue = aValuesX[i];
             if ( aValue > fMax)

commit b5625af367e48df2ae2babb1e9e911b0b173998c

crashtesting: failure on ooo82676-1.sxc import
regression from

commit 158852f2dffeda210605404e2de9179f0eed6e49
Author: Laurent Balland-Poirier <laurent.balland-poirier@laposte.net>
Date:   Wed Jun 17 15:15:07 2015 +0200

Change-Id: I44cfd85e9f7be65929281169fd297252e00c7317

chart2/source/view/main/VDataSeries.cxx
@@ -521,7 +521,7 @@ void VDataSeries::getMinMaxXValue(double& fMin, double& fMax) const
     if(aValuesX.getLength() > 0)
     {
         sal_Int32 i = 0;
-        while ( ::rtl::math::isNan( aValuesX[i] ) && i < aValuesX.getLength() )
+        while ( i < aValuesX.getLength() && ::rtl::math::isNan(aValuesX[i]) )
             i++;
         if ( i < aValuesX.getLength() )
             fMax = fMin = aValuesX[i++];