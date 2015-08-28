LemmingAvalanche/libreoffice-repaired

commit 7b350db9ebf50a953b2295a166cbf10ad57429bd

expand complex cascading conditional operator, step 5
Change-Id: Ia96f59911f20e4baf342a512a010c84e91b1f21d
Reviewed-on: https://gerrit.libreoffice.org/13012
Reviewed-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>
Tested-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -3182,7 +3182,7 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
     const SwNodeRange& rRg,
     const sal_Int32 nEndContentIndex,
     const SwNodeIndex& rStartIdx,
-    const bool bCopyFlyAtFly,
+    bool bCopyFlyAtFly,
     const bool bMergedFirstNode ) const
 {
     // First collect all Flys, sort them according to their ordering number,
@@ -3207,24 +3207,20 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
         switch ( pAnchor->GetAnchorId() )
         {
             case FLY_AT_FLY:
+                if( bCopyFlyAtFly && rRg.aStart > pAPos->nNode.GetIndex() + 1 )
+                    continue;
+            break;
             case FLY_AT_CHAR:
             case FLY_AT_PARA:
-                break;
+                bCopyFlyAtFly = false;
+            break;
             default:
                 continue;
         }
-        if ( bCopyFlyAtFly && FLY_AT_FLY == pAnchor->GetAnchorId() )
-        {
-            if( rRg.aStart > pAPos->nNode.GetIndex() + 1 )
-                continue;
-        }
-        else
-        {
-            if ( m_rDoc.getIDocumentRedlineAccess().IsRedlineMove()
+        if ( !bCopyFlyAtFly && ( m_rDoc.getIDocumentRedlineAccess().IsRedlineMove()
                     ? rRg.aStart >= pAPos->nNode
-                    : rRg.aStart > pAPos->nNode )
-                continue;
-        }
+                    : rRg.aStart > pAPos->nNode ))
+            continue;
         if ( pAPos->nNode > rRg.aEnd )
             continue;
         //frames at the last source node are not always copied:

commit 838ea9b39fb13e445ef7511cf04db406948f291f

tdf#93353: fix copy fluy in fly
- prevents flys in inlcuded docs in a master doc from disappearing
- fixes a regression from 7b350db9ebf50a953b2295a166cbf10ad57429bd

Change-Id: If211d8dbf423e2a33a258b43ab7092e1a010206f

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -3201,7 +3201,7 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
     const SwNodeRange& rRg,
     const sal_Int32 nEndContentIndex,
     const SwNodeIndex& rStartIdx,
-    bool bCopyFlyAtFly,
+    const bool bCopyFlyAtFly,
     const bool bMergedFirstNode ) const
 {
     // First collect all Flys, sort them according to their ordering number,
@@ -3223,22 +3223,25 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
         bool bAtContent = (pAnchor->GetAnchorId() == FLY_AT_PARA);
         if ( !pAPos )
             continue;
+        sal_uLong nSkipAfter = pAPos->nNode.GetIndex();
+        sal_uLong nStart = rRg.aStart.GetIndex();
         switch ( pAnchor->GetAnchorId() )
         {
             case FLY_AT_FLY:
-                if( bCopyFlyAtFly && rRg.aStart > pAPos->nNode.GetIndex() + 1 )
-                    continue;
+                if(bCopyFlyAtFly)
+                    ++nSkipAfter;
+                else if(m_rDoc.getIDocumentRedlineAccess().IsRedlineMove())
+                    ++nStart;
             break;
             case FLY_AT_CHAR:
             case FLY_AT_PARA:
-                bCopyFlyAtFly = false;
+                if(m_rDoc.getIDocumentRedlineAccess().IsRedlineMove())
+                    ++nStart;
             break;
             default:
                 continue;
         }
-        if ( !bCopyFlyAtFly && ( m_rDoc.getIDocumentRedlineAccess().IsRedlineMove()
-                    ? rRg.aStart >= pAPos->nNode
-                    : rRg.aStart > pAPos->nNode ))
+        if ( nStart > nSkipAfter )
             continue;
         if ( pAPos->nNode > rRg.aEnd )
             continue;