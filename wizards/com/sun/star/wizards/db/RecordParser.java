LemmingAvalanche/libreoffice-repaired

commit d85d336969ccbe74a6064147ea7fb6c84e5c43c6

java: Method uses the same code for two switch clauses
Change-Id: Ied82b0a39ff9dc9c7cc9539dec2b963059f3d4a5
Reviewed-on: https://gerrit.libreoffice.org/12139
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

wizards/com/sun/star/wizards/db/RecordParser.java
@@ -132,70 +132,27 @@ public class RecordParser extends QueryMetaData
         {
             case DataType.BIT: // ==  -7;
             case DataType.BOOLEAN:
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
             case DataType.TINYINT: // ==  -6;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
             case DataType.BIGINT: // ==  -5;
+            case DataType.NUMERIC: // ==   2;
+            case DataType.INTEGER: // ==   4;
+            case DataType.SMALLINT: // ==   5;
+            case DataType.DECIMAL: // ==   3;  [mit Nachkommastellen]
+            case DataType.FLOAT: // ==   6;
+            case DataType.REAL: // ==   7;
+            case DataType.DOUBLE: // ==   8;
+            case DataType.DATE: // ==  91;
+            case DataType.TIME: // ==  92;
+            case DataType.TIMESTAMP: // ==  93;
                 oAny = getColumnDoubleValue(ColIndex, false);
                 break;
 
             case DataType.CHAR: // ==   1;
-                oAny = getColumnStringValue(ColIndex);
-                break;
-
             case DataType.VARCHAR: // ==  12;
-                oAny = getColumnStringValue(ColIndex);
-                //      double sResult = xResultSetRow.getClob(ColIndex);       //???
-                break;
-
             case DataType.LONGVARCHAR: // ==  -1;
                 oAny = getColumnStringValue(ColIndex);
-                //      double sResult = xResultSetRow.getCharacterStream(ColIndex);
                 break;
 
-            case DataType.NUMERIC: // ==   2;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.INTEGER: // ==   4;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.SMALLINT: // ==   5;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.DECIMAL: // ==   3;  [mit Nachkommastellen]
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.FLOAT: // ==   6;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.REAL: // ==   7;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.DOUBLE: // ==   8;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.DATE: // ==  91;
-                oAny = getColumnDoubleValue(ColIndex, true);
-                break;
-
-            case DataType.TIME: // ==  92;
-                oAny = getColumnDoubleValue(ColIndex, false);
-                break;
-
-            case DataType.TIMESTAMP: // ==  93;
-                oAny = getColumnDoubleValue(ColIndex, true);
-                break;
         }
 
         // Is the index OK; increment?

reportbuilder/java/org/libreoffice/report/pentaho/output/OfficeDocumentReportTarget.java
@@ -703,11 +703,6 @@ public abstract class OfficeDocumentReportTarget extends AbstractReportTarget
                     break;
                 }
                 case OfficeDocumentReportTarget.STATE_IN_SECTION:
-                {
-                    states.push(IntegerCache.getInteger(OfficeDocumentReportTarget.STATE_IN_OTHER));
-                    startOther(attrs);
-                    break;
-                }
                 case OfficeDocumentReportTarget.STATE_IN_OTHER:
                 {
                     states.push(IntegerCache.getInteger(OfficeDocumentReportTarget.STATE_IN_OTHER));

toolkit/test/accessibility/EventListener.java
@@ -80,13 +80,7 @@ public class EventListener
                 break;
 
             case AccessibleEventId.ACTION_CHANGED:
-                aHandler = new EventHandler (aEvent, maTreeModel);
-                break;
-
             case AccessibleEventId.HYPERTEXT_CHANGED:
-                aHandler = new EventHandler (aEvent, maTreeModel);
-                break;
-
             case AccessibleEventId.ACTIVE_DESCENDANT_CHANGED:
             case AccessibleEventId.CARET_CHANGED:
             case AccessibleEventId.TEXT_CHANGED:

wizards/com/sun/star/wizards/common/NumericalHelper.java
@@ -608,31 +608,17 @@ public class NumericalHelper
         switch (aTypeObject.iType)
         {
             case BYTE_TYPE:
-                retValue = aTypeObject.aValue.toString();
-                break;
             case CHAR_TYPE:
-                retValue = aTypeObject.aValue.toString();
-                break;
             case SHORT_TYPE:
-                retValue = aTypeObject.aValue.toString();
-                break;
             case INT_TYPE:
-                retValue = aTypeObject.aValue.toString();
-                break;
             case LONG_TYPE:
-                retValue = aTypeObject.aValue.toString();
-                break;
             case FLOAT_TYPE:
-                retValue = aTypeObject.aValue.toString();
-                break;
             case DOUBLE_TYPE:
+            case BOOLEAN_TYPE:
                 retValue = aTypeObject.aValue.toString();
                 break;
             case STRING_TYPE:
                 retValue = (String) aTypeObject.aValue;
-                break;
-            case BOOLEAN_TYPE:
-                retValue = aTypeObject.aValue.toString();
                 break;
             case SEQUENCE_TYPE:
                 retValue = new String(toByteArray((aValue)));

wizards/com/sun/star/wizards/db/FieldColumn.java
@@ -256,11 +256,6 @@ public class FieldColumn
                     break;
 
                 case DataType.TINYINT: // ==  -6;
-                    StandardFormatKey = iNumberFormatKey;
-                    FieldWidth = 5;
-                    bIsNumberFormat = true;
-                    break;
-
                 case DataType.SMALLINT: // ==   5;
                     StandardFormatKey = iNumberFormatKey;
                     FieldWidth = 5;
@@ -304,23 +299,8 @@ public class FieldColumn
                     break;
 
                 case DataType.DECIMAL: // ==   3;  [mit Nachkommastellen]
-                    StandardFormatKey = iNumberFormatKey;
-                    FieldWidth = 10 + AnyConverter.toInt(m_xColPropertySet.getPropertyValue("Scale")) + 1;
-                    bIsNumberFormat = true;
-                    break;
-
                 case DataType.FLOAT: // ==   6;
-                    StandardFormatKey = iNumberFormatKey;
-                    FieldWidth = 10 + AnyConverter.toInt(m_xColPropertySet.getPropertyValue("Scale")) + 1;
-                    bIsNumberFormat = true;
-                    break;
-
                 case DataType.REAL: // ==   7;
-                    StandardFormatKey = iNumberFormatKey;
-                    FieldWidth = 10 + AnyConverter.toInt(m_xColPropertySet.getPropertyValue("Scale")) + 1;
-                    bIsNumberFormat = true;
-                    break;
-
                 case DataType.DOUBLE: // ==   8;
                     StandardFormatKey = iNumberFormatKey;
                     FieldWidth = 10 + AnyConverter.toInt(m_xColPropertySet.getPropertyValue("Scale")) + 1;
@@ -416,29 +396,14 @@ public class FieldColumn
                 break;
 
             case DataType.VARCHAR: // ==  12;
-                DefaultValue = BlindtextCreator.getBlindTextString(FieldTitle, FieldWidth, FieldWidth);
-                break;
-
             case DataType.LONGVARCHAR: // ==  -1;
                 DefaultValue = BlindtextCreator.getBlindTextString(FieldTitle, FieldWidth, FieldWidth);
                 break;
 
             case DataType.NUMERIC: // ==   2;
-                DefaultValue = Double.valueOf("9876.5");
-                break;
-
             case DataType.DECIMAL: // ==   3;  [mit Nachkommastellen]
-                DefaultValue = Double.valueOf("9876.5");
-                break;
-
             case DataType.FLOAT: // ==   6;
-                DefaultValue = Double.valueOf("9876.5");
-                break;
-
             case DataType.REAL: // ==   7;
-                DefaultValue = Double.valueOf("9876.5");
-                break;
-
             case DataType.DOUBLE: // ==   8;
                 DefaultValue = Double.valueOf("9876.54");
                 break;

wizards/com/sun/star/wizards/ui/FieldSelection.java
@@ -67,14 +67,11 @@ public class FieldSelection
             int iKey = CurUnoDialog.getControlKey(EventObject.Source, CurUnoDialog.ControlList);
             switch (iKey)
             {
-
                 case SOFLDSLST:
-                    toggleListboxButtons((short) - 1, (short) - 1);
-                    break;
-
                 case SOSELFLDSLST:
                     toggleListboxButtons((short) - 1, (short) - 1);
                     break;
+
                 default:
                     break;
             }
@@ -100,15 +97,8 @@ public class FieldSelection
                 int iKey = CurUnoDialog.getControlKey(actionEvent.Source, CurUnoDialog.ControlList);
                 switch (iKey)
                 {
-                    case SOFLDSLST:
-                        selectFields(false);
-                        break;
-
-                    case SOSELFLDSLST:
-                        deselectFields(false);
-                        break;
-
                     case SOCMDMOVESEL:
+                    case SOFLDSLST:
                         selectFields(false);
                         break;
 
@@ -117,6 +107,7 @@ public class FieldSelection
                         break;
 
                     case SOCMDREMOVESEL:
+                    case SOSELFLDSLST:
                         deselectFields(false);
                         break;
 

wizards/com/sun/star/wizards/ui/FilterComponent.java
@@ -139,22 +139,21 @@ public class FilterComponent
                     aNumberFormatter.setNumberFormat(xValueModel, CurFieldColumn.getDBFormatKey(), aNumberFormatter);
 
                     break;
+
                 case SO_FIRSTCONDITION:
                 case SO_SECONDCONDITION:
                 case SO_THIRDCONDITION:
                 case SO_FOURTHCONDITION:
-                    sControlName = getControlName(EventObject.Source);
-                    break;
-                case SOOPTORMODE:
-                case SOOPTANDMODE:
-                    return;
-
                 case SO_FIRSTBOOLFIELDNAME:
                 case SO_SECONDBOOLFIELDNAME:
                 case SO_THIRDBOOLFIELDNAME:
                 case SO_FOURTHBOOLFIELDNAME:
                     sControlName = getControlName(EventObject.Source);
                     break;
+
+                case SOOPTORMODE:
+                case SOOPTANDMODE:
+                    return;
 
                 default:
                     break;

commit cc146cfdf07aa1c0e6a7cfbc2dd2b5908b0c282a

tdf#91022: Incorrect Dates in Report created with legacy report wizard
Regression from http://cgit.freedesktop.org/libreoffice/core/commit/?id=d85d336969ccbe74a6064147ea7fb6c84e5c43c6
Thank you Matthew Francis for having found the root cause!

Change-Id: I4128368a5f5df6a9fc5f7668fb1f8f330b5795b9
Reviewed-on: https://gerrit.libreoffice.org/18210
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

wizards/com/sun/star/wizards/db/RecordParser.java
@@ -131,12 +131,14 @@ public class RecordParser extends QueryMetaData
             case DataType.FLOAT: // ==   6;
             case DataType.REAL: // ==   7;
             case DataType.DOUBLE: // ==   8;
-            case DataType.DATE: // ==  91;
             case DataType.TIME: // ==  92;
-            case DataType.TIMESTAMP: // ==  93;
                 oAny = getColumnDoubleValue(ColIndex, false);
                 break;
 
+            case DataType.DATE: // ==  91;
+            case DataType.TIMESTAMP: // ==  93;
+                oAny = getColumnDoubleValue(ColIndex, true);
+                break;
             case DataType.CHAR: // ==   1;
             case DataType.VARCHAR: // ==  12;
             case DataType.LONGVARCHAR: // ==  -1;