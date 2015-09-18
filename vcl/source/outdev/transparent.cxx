LemmingAvalanche/libreoffice-repaired

commit 4691bff6f504fc8ba44a54b531c07f039e01c834

start wrapping OutputDevice in VclPtr
Change-Id: If3ecbb0599b50d50ce6b3997ca7892200c332ffe

vcl/source/outdev/transparent.cxx
@@ -116,7 +116,7 @@ sal_uInt16 OutputDevice::GetAlphaBitCount() const
 
 bool OutputDevice::HasAlpha()
 {
-    return mpAlphaVDev != NULL;
+    return mpAlphaVDev != nullptr;
 }
 
 void OutputDevice::ImplPrintTransparent( const Bitmap& rBmp, const Bitmap& rMask,
@@ -431,25 +431,25 @@ void OutputDevice::EmulateDrawTransparent ( const tools::PolyPolygon& rPolyPoly,
 
         if( !bDrawn )
         {
-            VirtualDevice aVDev( *this, 1 );
+            ScopedVclPtr<VirtualDevice> aVDev = new VirtualDevice( *this, 1 );
             const Size aDstSz( aDstRect.GetSize() );
             const sal_uInt8 cTrans = (sal_uInt8) MinMax( FRound( nTransparencePercent * 2.55 ), 0, 255 );
 
             if( aDstRect.Left() || aDstRect.Top() )
                 aPolyPoly.Move( -aDstRect.Left(), -aDstRect.Top() );
 
-            if( aVDev.SetOutputSizePixel( aDstSz ) )
+            if( aVDev->SetOutputSizePixel( aDstSz ) )
             {
                 const bool bOldMap = mbMap;
 
                 EnableMapMode( false );
 
-                aVDev.SetLineColor( COL_BLACK );
-                aVDev.SetFillColor( COL_BLACK );
-                aVDev.DrawPolyPolygon( aPolyPoly );
+                aVDev->SetLineColor( COL_BLACK );
+                aVDev->SetFillColor( COL_BLACK );
+                aVDev->DrawPolyPolygon( aPolyPoly );
 
                 Bitmap aPaint( GetBitmap( aDstRect.TopLeft(), aDstSz ) );
-                Bitmap aPolyMask( aVDev.GetBitmap( Point(), aDstSz ) );
+                Bitmap aPolyMask( aVDev->GetBitmap( Point(), aDstSz ) );
 
                 // #107766# check for non-empty bitmaps before accessing them
                 if( !!aPaint && !!aPolyMask )
@@ -682,7 +682,7 @@ void OutputDevice::DrawTransparent( const GDIMetaFile& rMtf, const Point& rPos,
 
         if( !aDstRect.IsEmpty() )
         {
-            std::unique_ptr<VirtualDevice> xVDev(new VirtualDevice);
+            ScopedVclPtrInstance<VirtualDevice> xVDev;
 
             ((OutputDevice*)xVDev.get())->mnDPIX = mnDPIX;
             ((OutputDevice*)xVDev.get())->mnDPIY = mnDPIY;

commit 61960dedfcc31a6c3369f4ab9618670901eaca07

valgrind: memory leak in scoped_ptr/unique_ptr -> VclPtr convert
regression from

commit 4691bff6f504fc8ba44a54b531c07f039e01c834
Author: Noel Grandin <noel@peralex.com>
Date:   Thu Mar 19 13:54:12 2015 +0200

    start wrapping OutputDevice in VclPtr

 9,800 (568 direct, 9,232 indirect) bytes in 1 blocks are definitely lost in loss record 12,696 of 12,898
    by 0xC602E72: ScopedVclPtrInstance<VirtualDevice>::ScopedVclPtrInstance<>() (vclptr.hxx:375)

Change-Id: I356f39c339fd28a9e19a00d61b6f1bee492f2638

vcl/source/outdev/transparent.cxx
@@ -741,7 +741,7 @@ void OutputDevice::DrawTransparent( const GDIMetaFile& rMtf, const Point& rPos,
 
                     const AlphaMask aAlpha(xVDev->GetBitmap(aPoint, xVDev->GetOutputSizePixel()));
 
-                    xVDev.reset();
+                    xVDev.disposeAndClear();
 
                     // draw masked content to target and restore MapMode
                     DrawBitmapEx(aDstRect.TopLeft(), BitmapEx(aPaint, aAlpha));
@@ -789,7 +789,7 @@ void OutputDevice::DrawTransparent( const GDIMetaFile& rMtf, const Point& rPos,
 
                     aAlpha = xVDev->GetBitmap( Point(), xVDev->GetOutputSizePixel() );
 
-                    xVDev.reset();
+                    xVDev.disposeAndClear();
 
                     EnableMapMode( false );
                     DrawBitmapEx( aDstRect.TopLeft(), BitmapEx( aPaint, aAlpha ) );