LemmingAvalanche/libreoffice-repaired

commit ce5b019ee3db529a4c4a60a80b5f0ea7581bcef9

sd: dispose SdStyleSheet properly
If it's disposed during document destruction it still retained pointers
to SfxItemSet and SfxStyleSheetBasePool.

Change-Id: I7b986b4fe5ca466f0fa86a99b744f5e2f85e08be

sd/source/core/stlsheet.cxx
@@ -773,6 +773,13 @@ void SAL_CALL SdStyleSheet::dispose(  ) throw (RuntimeException, std::exception)
 
 void SdStyleSheet::disposing()
 {
+    SolarMutexGuard aGuard;
+    if (bMySet)
+    {
+        delete pSet;
+    }
+    pSet = nullptr;
+    pPool = nullptr;
     mxPool.clear();
 }
 

commit 05ea49bdb5b5d692db8617c2c1f0983f25638558

valgrind, leak in SdStyleSheet
this is presumably a regression from

commit ce5b019ee3db529a4c4a60a80b5f0ea7581bcef9
Author: Michael Stahl <mstahl@redhat.com>
Date:   Tue Jul 7 23:57:07 2015 +0200

    sd: dispose SdStyleSheet properly

    If it's disposed during document destruction it still retained pointers
    to SfxItemSet and SfxStyleSheetBasePool.

where it was always deleted in the dtor, but now
is optional in ::dispose

lets set bMySet to true when pSet is created like sc does, rather
try going to an unconditional delete in ::dispose

Change-Id: I214675805e513f657e51c779758106194f6d14e3

sd/source/core/stlsheet.cxx
@@ -245,6 +245,7 @@ SfxItemSet& SdStyleSheet::GetItemSet()
                                         0, 0 };
 
             pSet = new SfxItemSet(GetPool().GetPool(), nWhichPairTable);
+            bMySet = true;
         }
 
         return *pSet;
@@ -270,6 +271,7 @@ SfxItemSet& SdStyleSheet::GetItemSet()
                                         0, 0 };
 
             pSet = new SfxItemSet(GetPool().GetPool(), nWhichPairTable);
+            bMySet = true;
         }
 
         return *pSet;
@@ -309,6 +311,7 @@ SfxItemSet& SdStyleSheet::GetItemSet()
                                              0, 0 };
 
                 pSet = new SfxItemSet(GetPool().GetPool(), nWhichPairTable);
+                bMySet = true;
             }
 
             return(*pSet);