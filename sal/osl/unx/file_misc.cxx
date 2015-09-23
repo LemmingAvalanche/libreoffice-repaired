LemmingAvalanche/libreoffice-repaired

commit f273d98ce5e4cb3b598b4ae8639f02caadab879b

cid#1242936 handle rename failure
Change-Id: I92ce690a6f8183ce77c8ed53bd2fcb45d39d756e

sal/osl/unx/file_misc.cxx
@@ -810,6 +810,8 @@ static oslFileError oslDoCopy(const sal_Char* pszSourceFileName, const sal_Char*
 
     if ( DestFileExists )
     {
+        //TODO: better pick a temp file name instead of adding .osl-tmp:
+
         strncpy(pszTmpDestFile, pszDestFileName, size_tmp_dest_buff - 1);
 
         if ((strlen(pszTmpDestFile) + strlen(TMP_DEST_FILE_EXTENSION)) >= size_tmp_dest_buff)
@@ -817,9 +819,22 @@ static oslFileError oslDoCopy(const sal_Char* pszSourceFileName, const sal_Char*
 
         strncat(pszTmpDestFile, TMP_DEST_FILE_EXTENSION, strlen(TMP_DEST_FILE_EXTENSION));
 
-        /* FIXME: what if pszTmpDestFile already exists? */
-        /*        with getcanonical??? */
-        nRet=rename(pszDestFileName,pszTmpDestFile);
+        if (rename(pszDestFileName,pszTmpDestFile) != 0)
+        {
+            if (errno == ENOENT)
+            {
+                DestFileExists = 0;
+            }
+            else
+            {
+                int e = errno;
+                SAL_INFO(
+                    "sal.osl",
+                    "rename(" << pszDestFileName << ", " << pszTmpDestFile
+                        << ") failed with errno " << e);
+                return osl_File_E_BUSY; // for want of a better error code
+            }
+        }
     }
 
     /* mfe: should be S_ISREG */

include/osl/file.h
@@ -1309,7 +1309,9 @@ SAL_DLLPUBLIC oslFileError SAL_CALL osl_removeFile(
     osl_File_E_NAMETOOLONG file name too long<br>
     osl_File_E_NOENT no such file or directory<br>
     osl_File_E_ISDIR is a directory<br>
-    osl_File_E_ROFS read-only file system<p>
+    osl_File_E_ROFS read-only file system<br>
+    osl_File_E_BUSY if the implementation internally requires resources that are
+        (temporarily) unavailable (added with LibreOffice 4.4)
 
     @see    osl_moveFile()
     @see    osl_removeFile()
@@ -1339,6 +1341,8 @@ SAL_DLLPUBLIC oslFileError SAL_CALL osl_copyFile(
     osl_File_E_NAMETOOLONG file name too long<br>
     osl_File_E_NOENT no such file or directory<br>
     osl_File_E_ROFS read-only file system<br>
+    osl_File_E_BUSY if the implementation internally requires resources that are
+        (temporarily) unavailable (added with LibreOffice 4.4)
 
     @see osl_copyFile()
 */

commit cc46ae21608f64ed140a50a179572fabdf126e55

Better fail with E_EXIST if rename(3) fails
...it leads to better handling of the failure downstream, if e.g. this is an
attempt to copy a file from the presets to an existing file of a previously
partly generated UserInstallation, and the presets and the UserInstallation are
on different drives and rename(3) fails with EXDEV.  (Without this fix, removing
the UserInstallation's registrymodifications.xcu caused restarting soffice to
fail with "User installation could not be completed" in this case; regression
introduced with f273d98ce5e4cb3b598b4ae8639f02caadab879b "cid#1242936 handle
rename failure.")

Change-Id: I409698c4e2e6cc6ccd8b2be80a393c9657b5125d

sal/osl/unx/file_misc.cxx
@@ -816,7 +816,7 @@ static oslFileError oslDoCopy(const sal_Char* pszSourceFileName, const sal_Char*
                     "sal.osl",
                     "rename(" << pszDestFileName << ", " << tmpDestFile
                         << ") failed with errno " << e);
-                return osl_File_E_BUSY; // for want of a better error code
+                return osl_File_E_EXIST; // for want of a better error code
             }
         }
     }