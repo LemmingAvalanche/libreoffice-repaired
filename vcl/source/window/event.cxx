LemmingAvalanche/libreoffice-repaired

commit 53322e3ea31db07cc9e74e56a55a9e274ba7aa51

convert Link<> to typed
Change-Id: If3e2b00092440ebd42ae5b73ae2b0e44c3702683

vcl/source/window/event.cxx
@@ -214,8 +214,8 @@ void Window::CallEventListeners( sal_uLong nEvent, void* pData )
 
     if (!mpWindowImpl->maEventListeners.empty())
     {
-         // Copy the list, because this can be destroyed when calling a Link...
-         std::vector<Link<VclWindowEvent&,void>> aCopy( mpWindowImpl->maEventListeners );
+        // Copy the list, because this can be destroyed when calling a Link...
+        std::vector<Link<VclWindowEvent&,void>> aCopy( mpWindowImpl->maEventListeners );
         for ( Link<VclWindowEvent&,void>& rLink : aCopy )
         {
             if (aDelData.IsDead()) break;
@@ -238,7 +238,19 @@ void Window::CallEventListeners( sal_uLong nEvent, void* pData )
         if ( aDelData.IsDead() )
             return;
 
-        pWindow->mpWindowImpl->maChildEventListeners.Call( &aEvent );
+        if (!mpWindowImpl->maEventListeners.empty())
+        {
+            // Copy the list, because this can be destroyed when calling a Link...
+            std::vector<Link<VclWindowEvent&,void>> aCopy( mpWindowImpl->maChildEventListeners );
+            for ( Link<VclWindowEvent&,void>& rLink : aCopy )
+            {
+                if (aDelData.IsDead())
+                    return;
+                // check this hasn't been removed in some re-enterancy scenario fdo#47368
+                if( std::find(mpWindowImpl->maChildEventListeners.begin(), mpWindowImpl->maChildEventListeners.end(), rLink) != mpWindowImpl->maChildEventListeners.end() )
+                    rLink.Call( aEvent );
+            }
+        }
 
         if ( aDelData.IsDead() )
             return;
@@ -268,15 +280,18 @@ void Window::RemoveEventListener( const Link<VclWindowEvent&,void>& rEventListen
     }
 }
 
-void Window::AddChildEventListener( const Link<>& rEventListener )
+void Window::AddChildEventListener( const Link<VclWindowEvent&,void>& rEventListener )
 {
-    mpWindowImpl->maChildEventListeners.addListener( rEventListener );
+    mpWindowImpl->maChildEventListeners.push_back( rEventListener );
 }
 
-void Window::RemoveChildEventListener( const Link<>& rEventListener )
+void Window::RemoveChildEventListener( const Link<VclWindowEvent&,void>& rEventListener )
 {
     if (mpWindowImpl)
-        mpWindowImpl->maChildEventListeners.removeListener( rEventListener );
+    {
+        auto& rListeners = mpWindowImpl->maChildEventListeners;
+        rListeners.erase( std::remove(rListeners.begin(), rListeners.end(), rEventListener ), rListeners.end() );
+    }
 }
 
 ImplSVEvent * Window::PostUserEvent( const Link<void*,void>& rLink, void* pCaller, bool bReferenceLink )

dbaccess/source/ui/misc/ToolBoxHelper.cxx
@@ -74,19 +74,17 @@ namespace dbaui
                 m_pToolBox->SetOutStyle(aOptions.GetToolboxStyle());
         }
     }
-    IMPL_LINK(OToolBoxHelper, SettingsChanged, VclWindowEvent*, _pEvt)
+    IMPL_LINK_TYPED(OToolBoxHelper, SettingsChanged, VclSimpleEvent&, _rEvt, void)
     {
-        if ( m_pToolBox && _pEvt && _pEvt->GetId() == VCLEVENT_APPLICATION_DATACHANGED )
+        if ( m_pToolBox && _rEvt.GetId() == VCLEVENT_APPLICATION_DATACHANGED )
         {
-            DataChangedEvent* pData = static_cast<DataChangedEvent*>(_pEvt->GetData());
+            DataChangedEvent* pData = static_cast<DataChangedEvent*>(static_cast<VclWindowEvent&>(_rEvt).GetData());
             if ( pData && ((( pData->GetType() == DataChangedEventType::SETTINGS  )   ||
             ( pData->GetType() == DataChangedEventType::DISPLAY   ))  &&
             ( pData->GetFlags() & AllSettingsFlags::STYLE        )))
                 // check if imagelist changed
                 checkImageList();
         }
-
-        return 0L;
     }
     void OToolBoxHelper::setToolBox(ToolBox* _pTB)
     {

desktop/source/splash/splash.cxx
@@ -72,7 +72,7 @@ private:
 
     VclPtr<SplashScreenWindow> pWindow;
 
-    DECL_LINK( AppEventListenerHdl, VclWindowEvent * );
+    DECL_LINK_TYPED( AppEventListenerHdl, VclSimpleEvent&, void );
     virtual ~SplashScreen();
     void loadConfig();
     void updateStatus();
@@ -366,11 +366,11 @@ void SplashScreen::updateStatus()
 }
 
 // internal private methods
-IMPL_LINK( SplashScreen, AppEventListenerHdl, VclWindowEvent *, inEvent )
+IMPL_LINK_TYPED( SplashScreen, AppEventListenerHdl, VclSimpleEvent&, inEvent, void )
 {
-    if (inEvent != 0 && inEvent->GetWindow() == pWindow)
+    if (static_cast<VclWindowEvent&>(inEvent).GetWindow() == pWindow)
     {
-        switch ( inEvent->GetId() )
+        switch ( inEvent.GetId() )
         {
             case VCLEVENT_WINDOW_SHOW:
                 pWindow->Redraw();
@@ -379,7 +379,6 @@ IMPL_LINK( SplashScreen, AppEventListenerHdl, VclWindowEvent *, inEvent )
                 break;
         }
     }
-    return 0;
 }
 
 // Read keys from soffice{.ini|rc}:

extensions/source/bibliography/toolbar.cxx
@@ -555,7 +555,7 @@ IMPL_LINK_NOARG_TYPED( BibToolBar, OptionsChanged_Impl, LinkParamNone*, void )
 
 
 
-IMPL_LINK_NOARG( BibToolBar, SettingsChanged_Impl )
+IMPL_LINK_NOARG_TYPED( BibToolBar, SettingsChanged_Impl, VclSimpleEvent&, void )
 {
     // Check if toolbar button size have changed and we have to use system settings
     sal_Int16 eSymbolsSize = SvtMiscOptions().GetCurrentSymbolsSize();
@@ -564,8 +564,6 @@ IMPL_LINK_NOARG( BibToolBar, SettingsChanged_Impl )
         nSymbolsSize = eSymbolsSize;
         RebuildToolbar();
     }
-
-    return 0L;
 }
 
 

extensions/source/bibliography/toolbar.hxx
@@ -132,7 +132,7 @@ class BibToolBar:   public ToolBox
         DECL_LINK_TYPED( SendSelHdl, Idle*, void );
         DECL_LINK_TYPED( MenuHdl, ToolBox*, void );
         DECL_LINK_TYPED( OptionsChanged_Impl, LinkParamNone*, void );
-        DECL_LINK( SettingsChanged_Impl, void* );
+        DECL_LINK_TYPED( SettingsChanged_Impl, VclSimpleEvent&, void );
 
         void                    ApplyImageList();
         void                    RebuildToolbar();

extensions/source/update/ui/updatecheckui.cxx
@@ -136,7 +136,7 @@ class UpdateCheckUI : public ::cppu::WeakImplHelper
     Idle                maWaitIdle;
     Timer               maTimeoutTimer;
     Link<VclWindowEvent&,void> maWindowEventHdl;
-    Link<>              maApplicationEventHdl;
+    Link<VclSimpleEvent&,void> maApplicationEventHdl;
     bool                mbShowBubble;
     bool                mbShowMenuIcon;
     bool                mbBubbleChanged;
@@ -149,7 +149,7 @@ private:
                     DECL_LINK_TYPED(TimeOutHdl, Timer *, void);
                     DECL_LINK_TYPED(UserEventHdl, void *, void);
                     DECL_LINK_TYPED(WindowEventHdl, VclWindowEvent&, void);
-                    DECL_LINK( ApplicationEventHdl, VclSimpleEvent* );
+                    DECL_LINK_TYPED(ApplicationEventHdl, VclSimpleEvent&, void);
 
     BubbleWindow*   GetBubbleWindow();
     void            RemoveBubbleWindow( bool bRemoveIcon );
@@ -712,16 +712,16 @@ IMPL_LINK_TYPED( UpdateCheckUI, WindowEventHdl, VclWindowEvent&, rEvent, void )
 }
 
 
-IMPL_LINK( UpdateCheckUI, ApplicationEventHdl, VclSimpleEvent *, pEvent)
+IMPL_LINK_TYPED( UpdateCheckUI, ApplicationEventHdl, VclSimpleEvent&, rEvent, void)
 {
-    switch (pEvent->GetId())
+    switch (rEvent.GetId())
     {
         case VCLEVENT_WINDOW_SHOW:
         case VCLEVENT_WINDOW_ACTIVATE:
         case VCLEVENT_WINDOW_GETFOCUS: {
             SolarMutexGuard aGuard;
 
-            vcl::Window *pWindow = static_cast< VclWindowEvent * >(pEvent)->GetWindow();
+            vcl::Window *pWindow = static_cast< VclWindowEvent * >(&rEvent)->GetWindow();
             if ( pWindow && pWindow->IsTopWindow() )
             {
                 SystemWindow *pSysWin = pWindow->GetSystemWindow();
@@ -734,7 +734,6 @@ IMPL_LINK( UpdateCheckUI, ApplicationEventHdl, VclSimpleEvent *, pEvent)
             break;
         }
     }
-    return 0;
 }
 
 

framework/inc/services/layoutmanager.hxx
@@ -169,7 +169,7 @@ namespace framework
             virtual void SAL_CALL removeLayoutManagerEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XLayoutManagerListener >& aLayoutManagerListener ) throw (::com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
 
             DECL_LINK_TYPED( MenuBarClose, void *, void);
-            DECL_LINK( WindowEventListener, VclSimpleEvent* );
+            DECL_LINK_TYPED( WindowEventListener, VclWindowEvent&, void );
 
             //  ILayoutNotifications
 

framework/source/layoutmanager/layoutmanager.cxx
@@ -1398,25 +1398,18 @@ uno::Reference< ui::XUIElement > LayoutManager::implts_createDockingWindow( cons
     return xUIElement;
 }
 
-IMPL_LINK( LayoutManager, WindowEventListener, VclSimpleEvent*, pEvent )
+IMPL_LINK_TYPED( LayoutManager, WindowEventListener, VclWindowEvent&, rEvent, void )
 {
-    long nResult( 1 );
-
-    if ( pEvent && pEvent->ISA( VclWindowEvent ))
+    vcl::Window* pWindow = rEvent.GetWindow();
+    if ( pWindow && pWindow->GetType() == WINDOW_TOOLBOX )
     {
-        vcl::Window* pWindow = static_cast< VclWindowEvent* >(pEvent)->GetWindow();
-        if ( pWindow && pWindow->GetType() == WINDOW_TOOLBOX )
-        {
-            SolarMutexClearableGuard aReadLock;
-            ToolbarLayoutManager* pToolbarManager( m_pToolbarManager );
-            aReadLock.clear();
+        SolarMutexClearableGuard aReadLock;
+        ToolbarLayoutManager* pToolbarManager( m_pToolbarManager );
+        aReadLock.clear();
 
-            if ( pToolbarManager )
-                nResult = pToolbarManager->childWindowEvent( pEvent );
-        }
+        if ( pToolbarManager )
+            pToolbarManager->childWindowEvent( &rEvent );
     }
-
-    return nResult;
 }
 
 void SAL_CALL LayoutManager::createElement( const OUString& aName )

commit 62a4f31768bb615d676f4e850c3255506c37591c

fix for tdf#94827 - ToolBox events aren't delivered
regression from 53322e3ea31db07cc9e74e56a55a9e274ba7aa51
"convert Link<> to typed"

Change-Id: Ibb20366f0edcb9425d61ce5e9d5fb1a34533785b

vcl/source/window/event.cxx
@@ -238,16 +238,17 @@ void Window::CallEventListeners( sal_uLong nEvent, void* pData )
         if ( aDelData.IsDead() )
             return;
 
-        if (!mpWindowImpl->maEventListeners.empty())
+        auto& rChildListeners = pWindow->mpWindowImpl->maChildEventListeners;
+        if (!rChildListeners.empty())
         {
             // Copy the list, because this can be destroyed when calling a Link...
-            std::vector<Link<VclWindowEvent&,void>> aCopy( mpWindowImpl->maChildEventListeners );
+            std::vector<Link<VclWindowEvent&,void>> aCopy( rChildListeners );
             for ( Link<VclWindowEvent&,void>& rLink : aCopy )
             {
                 if (aDelData.IsDead())
                     return;
                 // check this hasn't been removed in some re-enterancy scenario fdo#47368
-                if( std::find(mpWindowImpl->maChildEventListeners.begin(), mpWindowImpl->maChildEventListeners.end(), rLink) != mpWindowImpl->maChildEventListeners.end() )
+                if( std::find(rChildListeners.begin(), rChildListeners.end(), rLink) != rChildListeners.end() )
                     rLink.Call( aEvent );
             }
         }