LemmingAvalanche/libreoffice-repaired

commit d0dd91ed30aecbf6825b222022bcf31976105b9d

CppCheck : reduce variables scope
Change-Id: Ief402017b693a4337f330fb07bb7a6dc6e749f72
Reviewed-on: https://gerrit.libreoffice.org/18753
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

drawinglayer/source/primitive3d/sdrextrudelathetools3d.cxx
@@ -596,9 +596,6 @@ namespace drawinglayer
 
                 // tetxture parameters
                 double fInvTexHeight(1.0);
-                double fTexHeightPos(0.0);
-                double fTexStart(0.0);
-                double fTexStop(1.0);
                 ::std::vector<double> aTexHeightArray;
                 basegfx::B3DRange aTexRangeFront;
                 basegfx::B3DRange aTexRangeBack;
@@ -640,6 +637,7 @@ namespace drawinglayer
                 {
                     for(a = 0L; a < nLoopCount; a++)
                     {
+                        double fTexHeightPos(0.0);
                         const Slice3D& rSliceA(rSliceVector[a]);
                         const Slice3D& rSliceB(rSliceVector[(a + 1L) % nNumSlices]);
                         const bool bAcceptPair(SLICETYPE3D_REGULAR == rSliceA.getSliceType() && SLICETYPE3D_REGULAR == rSliceB.getSliceType());
@@ -648,6 +646,9 @@ namespace drawinglayer
 
                         if(bAcceptPair)
                         {
+                            double fTexStart(0.0);
+                            double fTexStop(1.0);
+
                             if(bCreateNormals)
                             {
                                 impCreateInBetweenNormals(aPolB, aPolA, bSmoothHorizontalNormals);

chart2/source/controller/itemsetwrapper/DataPointItemConverter.cxx
@@ -629,9 +629,9 @@ void DataPointItemConverter::FillSpecialItem(
 
         case SCHATTR_DATADESCR_WRAP_TEXT:
         {
-            bool bValue = false;
             try
             {
+                bool bValue = false;
                 GetPropertySet()->getPropertyValue( "TextWordWrap" ) >>= bValue;
                 rOutItemSet.Put( SfxBoolItem( nWhichId, bValue ));
             }

chart2/source/controller/itemsetwrapper/TextLabelItemConverter.cxx
@@ -576,9 +576,9 @@ void TextLabelItemConverter::FillSpecialItem( sal_uInt16 nWhichId, SfxItemSet& r
         break;
         case SCHATTR_DATADESCR_WRAP_TEXT:
         {
-            bool bValue = false;
             try
             {
+                bool bValue = false;
                 GetPropertySet()->getPropertyValue( "TextWordWrap" ) >>= bValue;
                 rOutItemSet.Put( SfxBoolItem( nWhichId, bValue ));
             }

connectivity/source/drivers/macab/MacabRecords.cxx
@@ -381,7 +381,6 @@ MacabHeader *MacabRecords::createHeaderForRecordType(const CFArrayRef _records, 
     /* While searching through the properties for required properties, these
      * sal_Bools will keep track of what we have found.
      */
-    bool bFoundProperty;
     bool bFoundRequiredProperties[numRequiredProperties];
 
 
@@ -411,8 +410,8 @@ MacabHeader *MacabRecords::createHeaderForRecordType(const CFArrayRef _records, 
     /* Determine the non-required properties... */
     for(i = 0; i < numProperties; i++)
     {
+        bool bFoundProperty = false;
         property = static_cast<CFStringRef>(CFArrayGetValueAtIndex(allProperties, i));
-        bFoundProperty = false;
         for(j = 0; j < numRequiredProperties; j++)
         {
             if(CFEqual(property, requiredProperties[j]))

desktop/source/lib/init.cxx
@@ -563,7 +563,6 @@ static void doc_iniUnoCommands ()
     };
 
     util::URL aCommandURL;
-    const SfxSlot* pSlot = NULL;
     SfxViewShell* pViewShell = SfxViewShell::Current();
     SfxViewFrame* pViewFrame = pViewShell? pViewShell->GetViewFrame(): NULL;
 
@@ -579,6 +578,8 @@ static void doc_iniUnoCommands ()
 
     for (sal_uInt32 nIterator = 0; nIterator < SAL_N_ELEMENTS(sUnoCommands); nIterator++)
     {
+        const SfxSlot* pSlot = NULL;
+
         aCommandURL.Complete = sUnoCommands[nIterator];
         xParser->parseStrict(aCommandURL);
         pSlot = rSlotPool.GetUnoSlot(aCommandURL.Path);

desktop/unx/source/start.c
@@ -105,7 +105,6 @@ child_spawn ( Args *args, sal_Bool bAllArgs, sal_Bool bWithStatus )
     rtl_uString *pApp = NULL, *pTmp = NULL;
     rtl_uString **ppArgs;
     sal_uInt32 nArgs, i;
-    char buffer[64];
     ChildInfo *info;
     int status_pipe[2];
     oslProcessError nError;
@@ -136,6 +135,8 @@ child_spawn ( Args *args, sal_Bool bAllArgs, sal_Bool bWithStatus )
 
     if( bWithStatus )
     {
+        char buffer[64];
+
         /* add the pipe arg */
         snprintf (buffer, 63, "--splash-pipe=%d", status_pipe[1]);
         rtl_uString_newFromAscii( &pTmp, buffer );
@@ -754,7 +755,6 @@ void sigterm_handler(int ignored)
 
 SAL_IMPLEMENT_MAIN_WITH_ARGS( argc, argv )
 {
-    int fd = 0;
     sal_Bool bSentArgs = sal_False;
     const char* pUsePlugin;
     rtl_uString *pPipePath = NULL;
@@ -794,6 +794,7 @@ SAL_IMPLEMENT_MAIN_WITH_ARGS( argc, argv )
 
     if ( !args->bInhibitPipe )
     {
+        int fd = 0;
         pPipePath = get_pipe_path( args->pAppPath );
 
         if ( ( fd = connect_pipe( pPipePath ) ) >= 0 )

editeng/source/items/numitem.cxx
@@ -870,9 +870,9 @@ bool SvxNumRule::UnLinkGraphics()
     {
         SvxNumberFormat aFmt(GetLevel(i));
         const SvxBrushItem* pBrush = aFmt.GetBrush();
-        const Graphic* pGraphic = NULL;
         if(SVX_NUM_BITMAP == aFmt.GetNumberingType())
         {
+            const Graphic* pGraphic = NULL;
             if(pBrush &&
                 !pBrush->GetGraphicLink().isEmpty() &&
                     0 != (pGraphic = pBrush->GetGraphic()))

editeng/source/rtf/svxrtf.cxx
@@ -446,7 +446,6 @@ void SvxRTFParser::ReadColorTable()
 
 void SvxRTFParser::ReadFontTable()
 {
-    int nToken = 0;
     int _nOpenBrakets = 1;      // the first was already detected earlier!!
     std::unique_ptr<vcl::Font> pFont(new vcl::Font);
     short nFontNo(0), nInsFontNo (0);
@@ -459,6 +458,7 @@ void SvxRTFParser::ReadFontTable()
 
     while( _nOpenBrakets && IsParserWorking() )
     {
+        int nToken = 0;
         bool bCheckNewFont = false;
         switch( ( nToken = GetNextToken() ))
         {
@@ -593,10 +593,11 @@ void SvxRTFParser::ReadOLEData()
 OUString& SvxRTFParser::GetTextToEndGroup( OUString& rStr )
 {
     rStr.clear();
-    int _nOpenBrakets = 1, nToken = 0;  // the first was already detected earlier!!
+    int _nOpenBrakets = 1;  // the first was already detected earlier!!
 
     while( _nOpenBrakets && IsParserWorking() )
     {
+        int nToken = 0;
         switch( nToken = GetNextToken() )
         {
         case '}':       --_nOpenBrakets;    break;
@@ -653,7 +654,7 @@ util::DateTime SvxRTFParser::GetDateTimeStamp( )
 
 void SvxRTFParser::ReadInfo( const sal_Char* pChkForVerNo )
 {
-    int _nOpenBrakets = 1, nToken = 0;  // the first was already detected earlier!!
+    int _nOpenBrakets = 1;  // the first was already detected earlier!!
     DBG_ASSERT(m_xDocProps.is(),
         "SvxRTFParser::ReadInfo: no DocumentProperties");
     OUString sStr, sComment;
@@ -661,6 +662,7 @@ void SvxRTFParser::ReadInfo( const sal_Char* pChkForVerNo )
 
     while( _nOpenBrakets && IsParserWorking() )
     {
+        int nToken = 0;
         switch( nToken = GetNextToken() )
         {
         case '}':       --_nOpenBrakets;    break;

commit 591253cb0ae730e6045c08b21293bb8fd833f3f7

fTexHeightPos shall be incremented across loop iterations
Regression introduced with d0dd91ed30aecbf6825b222022bcf31976105b9d "CppCheck :
reduce variables scope," found by clang-analyzer-deadcode.DeadStores.

Change-Id: I3758a9827d4d242d4fd57af7bbb77decf2d0f327

drawinglayer/source/primitive3d/sdrextrudelathetools3d.cxx
@@ -635,9 +635,9 @@ namespace drawinglayer
 
                 if(nLoopCount)
                 {
+                    double fTexHeightPos(0.0);
                     for(a = 0L; a < nLoopCount; a++)
                     {
-                        double fTexHeightPos(0.0);
                         const Slice3D& rSliceA(rSliceVector[a]);
                         const Slice3D& rSliceB(rSliceVector[(a + 1L) % nNumSlices]);
                         const bool bAcceptPair(SLICETYPE3D_REGULAR == rSliceA.getSliceType() && SLICETYPE3D_REGULAR == rSliceB.getSliceType());