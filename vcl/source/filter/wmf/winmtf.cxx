LemmingAvalanche/libreoffice-repaired

commit c778efaed1e88ab77093b1a265c633ec1cf615ab

bnc#881024 Don't world transform font size in WMF/EMF import
Change-Id: Ia865b84ee2b159ff7251ab5a769a2b635dd2a1ea

vcl/source/filter/wmf/winmtf.cxx
@@ -389,13 +389,22 @@ Point WinMtfOutput::ImplMap( const Point& rPt )
         return Point();
 };
 
-Size WinMtfOutput::ImplMap( const Size& rSz )
+Size WinMtfOutput::ImplMap(const Size& rSz, bool bDoWorldTransform)
 {
     if ( mnWinExtX && mnWinExtY )
     {
         // #i121382# apply the whole WorldTransform, else a rotation will be misinterpreted
-        double fWidth = rSz.Width() * maXForm.eM11 + rSz.Height() * maXForm.eM21;
-        double fHeight = rSz.Width() * maXForm.eM12 + rSz.Height() * maXForm.eM22;
+        double fWidth, fHeight;
+        if (bDoWorldTransform)
+        {
+            fWidth = rSz.Width() * maXForm.eM11 + rSz.Height() * maXForm.eM21;
+            fHeight = rSz.Width() * maXForm.eM12 + rSz.Height() * maXForm.eM22;
+        }
+        else
+        {
+            fWidth = rSz.Width();
+            fHeight = rSz.Height();
+        }
 
         if ( mnGfxMode == GM_COMPATIBLE )
         {
@@ -457,7 +466,7 @@ void WinMtfOutput::ImplMap( Font& rFont )
 {
     // !!! HACK: we now always set the width to zero because the OS width is interpreted differently;
     // must later be made portable in SV (KA 1996-02-08)
-    Size  aFontSize = ImplMap (rFont.GetSize());
+    Size  aFontSize = ImplMap (rFont.GetSize(), false);
 
     if( aFontSize.Height() < 0 )
         aFontSize.Height() *= -1;

vcl/source/filter/wmf/winmtf.hxx
@@ -635,7 +635,7 @@ class WinMtfOutput
 
     Point               ImplMap( const Point& rPt );
     Point               ImplScale( const Point& rPt );
-    Size                ImplMap( const Size& rSz );
+    Size                ImplMap( const Size& rSize, bool bDoWorldTransform = true);
     Rectangle           ImplMap( const Rectangle& rRectangle );
     void                ImplMap( Font& rFont );
     Polygon&            ImplMap( Polygon& rPolygon );

commit 7848ae910ddaf97c826a42d8d11e2e3c39328261

take the world scaling factor for fonts, but not the rotation
regression from

commit c778efaed1e88ab77093b1a265c633ec1cf615ab
Author: Tomaž Vajngerl <tomaz.vajngerl@collabora.com>
Date:   Fri Jul 18 13:36:13 2014 +0200

    bnc#881024 Don't world transform font size in WMF/EMF import

which seems to be a regression from

commit b4c5a2fa089e2bed52e326da52db399a811662b1
Author: Armin Le Grand <alg@apache.org>
Date:   Mon Dec 10 16:08:58 2012 +0000

    Resolves: #i121382# Corrected size handling including font size handling...

    for EMF/WMF imports

Change-Id: I87307c9993d3343b4db636257e2567b10a73b79d

vcl/source/filter/wmf/winmtf.cxx
@@ -408,8 +408,18 @@ Size WinMtfOutput::ImplMap(const Size& rSz, bool bDoWorldTransform)
         }
         else
         {
-            fWidth = rSz.Width();
-            fHeight = rSz.Height();
+            //take the scale, but not the rotation
+            basegfx::B2DHomMatrix aMatrix(maXForm.eM11, maXForm.eM12, 0,
+                                          maXForm.eM21, maXForm.eM22, 0);
+            basegfx::B2DTuple aScale, aTranslate;
+            double fRotate, fShearX;
+            if (!aMatrix.decompose(aScale, aTranslate, fRotate, fShearX))
+            {
+                aScale.setX(1.0);
+                aScale.setY(1.0);
+            }
+            fWidth = rSz.Width() * aScale.getX();
+            fHeight = rSz.Height() * aScale.getY();
         }
 
         if ( mnGfxMode == GM_COMPATIBLE )