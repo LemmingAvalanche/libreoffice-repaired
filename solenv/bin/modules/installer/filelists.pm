LemmingAvalanche/libreoffice-repaired

commit d7c7d48c05617d4b5cdc496bcd2f2ac86468fd98

allow file names with a space in filelists
Change-Id: If83c73c3a8a0aab24c31199e3c31d90c215f3eac

solenv/bin/modules/installer/filelists.pm
@@ -118,7 +118,7 @@ sub read_filelist
     foreach my $line (@{$content})
     {
         chomp $line;
-        foreach my $file (split /\s+/, $line)
+        foreach my $file (split /\s+(?=\/)/, $line)
         {
             if ($file ne "")
             {

commit e0f902de75c47882b3216c52a4ecade110d1accd

solenv: hack to maybe fix WNT instset creation
Apparently that regex doesn't work on WNT for non-obvious reasons;
clearly this should be fixed properly by somebody with actual Perl
knowledge.

(regression from d7c7d48c05617d4b5cdc496bcd2f2ac86468fd98)

Change-Id: Ifed4ff3305e4961709a45f6a0ce40dc0683ccf28

solenv/bin/modules/installer/filelists.pm
@@ -118,11 +118,24 @@ sub read_filelist
     foreach my $line (@{$content})
     {
         chomp $line;
-        foreach my $file (split /\s+(?=\/)/, $line)
+        if ($installer::globals::os eq "WNT") # FIXME hack
         {
-            if ($file ne "")
+            foreach my $file (split /\s+/, $line)
             {
-                push @filelist, $file;
+                if ($file ne "")
+                {
+                    push @filelist, $file;
+                }
+            }
+        }
+        else
+        {
+            foreach my $file (split /\s+(?=\/)/, $line)
+            {
+                if ($file ne "")
+                {
+                    push @filelist, $file;
+                }
             }
         }
     }