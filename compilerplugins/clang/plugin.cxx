LibreOffice/core

tdf#94269: Replace "n" prefix for bool variables with "b"
Change-Id: I178545792c7354a362658ac7ef8b1d4cf0865797

compilerplugins/clang/plugin.cxx
@@ -249,18 +249,18 @@ bool RewritePlugin::adjustRangeForOptions( CharSourceRange* range, RewriteOption
     SourceLocation fileStartLoc = SM.getLocForStartOfFile( SM.getFileID( range->getBegin()));
     if( fileStartLoc.isInvalid())
         return false;
-    bool invalid = false;
-    const char* fileBuf = SM.getCharacterData( fileStartLoc, &invalid );
-    if( invalid )
+    bool bInvalid = false;
+    const char* fileBuf = SM.getCharacterData( fileStartLoc, &bInvalid );
+    if( bInvalid )
         return false;
-    const char* startBuf = SM.getCharacterData( range->getBegin(), &invalid );
-    if( invalid )
+    const char* startBuf = SM.getCharacterData( range->getBegin(), &bInvalid );
+    if( bInvalid )
         return false;
     SourceLocation locationEnd = range->getEnd();
     if( range->isTokenRange())
         locationEnd = locationAfterToken( locationEnd );
-    const char* endBuf = SM.getCharacterData( locationEnd, &invalid );
-    if( invalid )
+    const char* endBuf = SM.getCharacterData( locationEnd, &bInvalid );
+    if( bInvalid )
         return false;
     const char* startPos = startBuf;
     --startPos;

LemmingAvalanche/libreoffice-repaired

Signed-off-by: Michael Stahl <mstahl@redhat.com>

compilerplugins/clang/plugin.cxx
@@ -249,18 +249,18 @@ bool RewritePlugin::adjustRangeForOptions( CharSourceRange* range, RewriteOption
     SourceLocation fileStartLoc = SM.getLocForStartOfFile( SM.getFileID( range->getBegin()));
     if( fileStartLoc.isInvalid())
         return false;
-    bool invalid = false;
-    const char* fileBuf = SM.getCharacterData( fileStartLoc, &invalid );
-    if( invalid )
+    bool isInvalid = false;
+    const char* fileBuf = SM.getCharacterData( fileStartLoc, &isInvalid );
+    if( isInvalid )
         return false;
-    const char* startBuf = SM.getCharacterData( range->getBegin(), &invalid );
-    if( invalid )
+    const char* startBuf = SM.getCharacterData( range->getBegin(), &isInvalid );
+    if( isInvalid )
         return false;
     SourceLocation locationEnd = range->getEnd();
     if( range->isTokenRange())
         locationEnd = locationAfterToken( locationEnd );
-    const char* endBuf = SM.getCharacterData( locationEnd, &invalid );
-    if( invalid )
+    const char* endBuf = SM.getCharacterData( locationEnd, &isInvalid );
+    if( isInvalid )
         return false;
     const char* startPos = startBuf;
     --startPos;