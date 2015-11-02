LemmingAvalanche/libreoffice-repaired

commit febafe04553a791cb5b4cbe0733b75937f51448d

oox: handle textboxes in ShapeContextHandler::endFastElement()
DOCX shape import normally works by oox creating the shape, then
writerfilter handling the shape text. For drawingML shapes, having shape
text, this a bit more complicated, as there are shape properties after
the shape text as well.

ShapeContextHandler::endFastElement() assumed that shape text is only
possible on css.text.TextFrame shapes: also handle shapes having a
TextBox as well.

sw/qa/extras/ooxmlimport/data/mce-nested.docx is a reproducer for this
problem (group shape missing), when TextBoxes are enabled by default in
oox.

Change-Id: I7a412b31965cf363da0b0c7fcc732741f2037542

oox/source/shape/ShapeContextHandler.cxx
@@ -331,7 +331,15 @@ void SAL_CALL ShapeContextHandler::endFastElement(::sal_Int32 Element)
     if (Element == (NMSP_wps | XML_wsp))
     {
         uno::Reference<lang::XServiceInfo> xServiceInfo(mxSavedShape, uno::UNO_QUERY);
-        if (xServiceInfo.is() && xServiceInfo->supportsService("com.sun.star.text.TextFrame"))
+        bool bTextFrame = xServiceInfo.is() && xServiceInfo->supportsService("com.sun.star.text.TextFrame");
+        bool bTextBox = false;
+        if (!bTextFrame)
+        {
+            uno::Reference<beans::XPropertySet> xPropertySet(mxSavedShape, uno::UNO_QUERY);
+            if (xPropertySet.is())
+                bTextBox = xPropertySet->getPropertyValue("TextBox").get<bool>();
+        }
+        if (bTextFrame || bTextBox)
             mxWpsContext.clear();
         mxSavedShape.clear();
     }

commit af7022e2f89eca79c8370e43673a86ec9030db47

tdf#89088 DOCX import: fix missing text due to throwing ShapeContextHandler
Regression from commit febafe04553a791cb5b4cbe0733b75937f51448d (oox:
handle textboxes in ShapeContextHandler::endFastElement(), 2014-06-04),
the problem was that code in ShapeContextHandler::endFastElement()
assumed that all the XShape implementation it may see provides a TextBox
UNO property (which is specific to Writer) of type bool, but this is not
true.

Fix the problem by assuming false in case it has an other type.

Testcase can be a simple "does it load" one, as in the meantime
exceptions such as trying to read a bool from an uno::Any make the DOCX
XFilter::filter() implementation return false, unlike in the originally
reported LibreOffice 4.4 version that just silently discarded the
remaining content of the document.

Change-Id: I81781a7ec28d085ceee50d386b8eda023f83c29f

oox/source/shape/ShapeContextHandler.cxx
@@ -356,7 +356,7 @@ void SAL_CALL ShapeContextHandler::endFastElement(::sal_Int32 Element)
         {
             uno::Reference<beans::XPropertySet> xPropertySet(mxSavedShape, uno::UNO_QUERY);
             if (xPropertySet.is())
-                bTextBox = xPropertySet->getPropertyValue("TextBox").get<bool>();
+                xPropertySet->getPropertyValue("TextBox") >>= bTextBox;
         }
         if (bTextFrame || bTextBox)
             mxWpsContext.clear();