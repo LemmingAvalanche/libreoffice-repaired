LemmingAvalanche/libreoffice-repaired

commit f8dae9d3d6f9daf18e21d191ccb9c1cbb5ed71b9

convert Link<> to typed
Change-Id: I59d325c3b051690303a5841907317122fa1ec98b
Reviewed-on: https://gerrit.libreoffice.org/18825
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

cui/source/inc/numpages.hxx
@@ -429,6 +429,7 @@ class SvxNumPositionTabPage : public SfxTabPage
     DECL_LINK( LevelHdl_Impl, ListBox * );
     DECL_LINK( EditModifyHdl_Impl, void *);
     DECL_LINK( DistanceHdl_Impl, MetricField * );
+    DECL_LINK_TYPED( DistanceFocusHdl_Impl, Control&, void );
     DECL_LINK_TYPED( RelativeHdl_Impl, Button*, void );
     DECL_LINK_TYPED( StandardHdl_Impl, Button*, void);
 

cui/source/tabpages/numpages.cxx
@@ -2792,18 +2792,19 @@ SvxNumPositionTabPage::SvxNumPositionTabPage(vcl::Window* pParent,
     m_pAlign2LB->SetDropDownLineCount( m_pAlign2LB->GetEntryCount() );
 
     Link<> aLk = LINK(this, SvxNumPositionTabPage, DistanceHdl_Impl);
+    Link<Control&,void> aLk2 = LINK(this, SvxNumPositionTabPage, DistanceFocusHdl_Impl);
 
     m_pDistBorderMF->SetUpHdl(aLk);
     m_pDistBorderMF->SetDownHdl(aLk);
-    m_pDistBorderMF->SetLoseFocusHdl(aLk);
+    m_pDistBorderMF->SetLoseFocusHdl(aLk2);
 
     m_pDistNumMF->SetUpHdl(aLk);
     m_pDistNumMF->SetDownHdl(aLk);
-    m_pDistNumMF->SetLoseFocusHdl(aLk);
+    m_pDistNumMF->SetLoseFocusHdl(aLk2);
 
     m_pIndentMF->SetUpHdl(aLk);
     m_pIndentMF->SetDownHdl(aLk);
-    m_pIndentMF->SetLoseFocusHdl(aLk);
+    m_pIndentMF->SetLoseFocusHdl(aLk2);
 
     m_pLabelFollowedByLB->SetDropDownLineCount( m_pLabelFollowedByLB->GetEntryCount() );
     m_pLabelFollowedByLB->SetSelectHdl( LINK(this, SvxNumPositionTabPage, LabelFollowedByHdl_Impl) );
@@ -2811,17 +2812,17 @@ SvxNumPositionTabPage::SvxNumPositionTabPage(vcl::Window* pParent,
     aLk = LINK(this, SvxNumPositionTabPage, ListtabPosHdl_Impl);
     m_pListtabMF->SetUpHdl(aLk);
     m_pListtabMF->SetDownHdl(aLk);
-    m_pListtabMF->SetLoseFocusHdl(aLk);
+    m_pListtabMF->SetLoseFocusHdl(aLk2);
 
     aLk = LINK(this, SvxNumPositionTabPage, AlignAtHdl_Impl);
     m_pAlignedAtMF->SetUpHdl(aLk);
     m_pAlignedAtMF->SetDownHdl(aLk);
-    m_pAlignedAtMF->SetLoseFocusHdl(aLk);
+    m_pAlignedAtMF->SetLoseFocusHdl(aLk2);
 
     aLk = LINK(this, SvxNumPositionTabPage, IndentAtHdl_Impl);
     m_pIndentAtMF->SetUpHdl(aLk);
     m_pIndentAtMF->SetDownHdl(aLk);
-    m_pIndentAtMF->SetLoseFocusHdl(aLk);
+    m_pIndentAtMF->SetLoseFocusHdl(aLk2);
 
     m_pLevelLB->EnableMultiSelection(true);
     m_pLevelLB->SetSelectHdl(LINK(this, SvxNumPositionTabPage, LevelHdl_Impl));
@@ -3388,6 +3389,10 @@ IMPL_LINK( SvxNumPositionTabPage, LevelHdl_Impl, ListBox *, pBox )
     return 0;
 }
 
+IMPL_LINK_TYPED( SvxNumPositionTabPage, DistanceFocusHdl_Impl, Control&, rControl, void )
+{
+    DistanceHdl_Impl(static_cast<MetricField*>(&rControl));
+}
 IMPL_LINK( SvxNumPositionTabPage, DistanceHdl_Impl, MetricField *, pFld )
 {
     if(bInInintControl)

commit 472b8050830fb762a0ca72f9248bda8ce86c2e6b

fix tdf#95689 - List indent is broken
caused by my commit f8dae9d3d6f9daf18e21d191ccb9c1cbb5ed71b9
"convert Link<> to typed"

Change-Id: Ieeb9e6fa5d5be3789ea70549e85d4bd3ffd51e59

cui/source/inc/numpages.hxx
@@ -440,8 +440,11 @@ class SvxNumPositionTabPage : public SfxTabPage
 
     DECL_LINK_TYPED(LabelFollowedByHdl_Impl, ListBox&, void);
     DECL_LINK_TYPED( ListtabPosHdl_Impl, SpinField&, void );
+    DECL_LINK_TYPED( ListtabPosFocusHdl_Impl, Control&, void );
     DECL_LINK_TYPED( AlignAtHdl_Impl, SpinField&, void );
+    DECL_LINK_TYPED( AlignAtFocusHdl_Impl, Control&, void );
     DECL_LINK_TYPED( IndentAtHdl_Impl, SpinField&, void );
+    DECL_LINK_TYPED( IndentAtFocusHdl_Impl, Control&, void );
 
 public:
         SvxNumPositionTabPage(vcl::Window* pParent,

cui/source/tabpages/numpages.cxx
@@ -2803,16 +2803,19 @@ SvxNumPositionTabPage::SvxNumPositionTabPage(vcl::Window* pParent,
     m_pLabelFollowedByLB->SetSelectHdl( LINK(this, SvxNumPositionTabPage, LabelFollowedByHdl_Impl) );
 
     aLk3 = LINK(this, SvxNumPositionTabPage, ListtabPosHdl_Impl);
+    aLk2 = LINK(this, SvxNumPositionTabPage, ListtabPosFocusHdl_Impl);
     m_pListtabMF->SetUpHdl(aLk3);
     m_pListtabMF->SetDownHdl(aLk3);
     m_pListtabMF->SetLoseFocusHdl(aLk2);
 
     aLk3 = LINK(this, SvxNumPositionTabPage, AlignAtHdl_Impl);
+    aLk2 = LINK(this, SvxNumPositionTabPage, AlignAtFocusHdl_Impl);
     m_pAlignedAtMF->SetUpHdl(aLk3);
     m_pAlignedAtMF->SetDownHdl(aLk3);
     m_pAlignedAtMF->SetLoseFocusHdl(aLk2);
 
     aLk3 = LINK(this, SvxNumPositionTabPage, IndentAtHdl_Impl);
+    aLk2 = LINK(this, SvxNumPositionTabPage, IndentAtFocusHdl_Impl);
     m_pIndentAtMF->SetUpHdl(aLk3);
     m_pIndentAtMF->SetDownHdl(aLk3);
     m_pIndentAtMF->SetLoseFocusHdl(aLk2);
@@ -3542,6 +3545,10 @@ IMPL_LINK_NOARG_TYPED(SvxNumPositionTabPage, LabelFollowedByHdl_Impl, ListBox&, 
     SetModified();
 }
 
+IMPL_LINK_TYPED( SvxNumPositionTabPage, ListtabPosFocusHdl_Impl, Control&, rControl, void )
+{
+    ListtabPosHdl_Impl(static_cast<SpinField&>(rControl));
+}
 IMPL_LINK_TYPED( SvxNumPositionTabPage, ListtabPosHdl_Impl, SpinField&, rFld, void )
 {
     // determine value to be set at the chosen list levels
@@ -3563,6 +3570,10 @@ IMPL_LINK_TYPED( SvxNumPositionTabPage, ListtabPosHdl_Impl, SpinField&, rFld, vo
     SetModified();
 }
 
+IMPL_LINK_TYPED( SvxNumPositionTabPage, AlignAtFocusHdl_Impl, Control&, rControl, void )
+{
+    AlignAtHdl_Impl(static_cast<SpinField&>(rControl));
+}
 IMPL_LINK_TYPED( SvxNumPositionTabPage, AlignAtHdl_Impl, SpinField&, rFld, void )
 {
     // determine value to be set at the chosen list levels
@@ -3585,6 +3596,10 @@ IMPL_LINK_TYPED( SvxNumPositionTabPage, AlignAtHdl_Impl, SpinField&, rFld, void 
     SetModified();
 }
 
+IMPL_LINK_TYPED( SvxNumPositionTabPage, IndentAtFocusHdl_Impl, Control&, rControl, void )
+{
+    IndentAtHdl_Impl(static_cast<SpinField&>(rControl));
+}
 IMPL_LINK_TYPED( SvxNumPositionTabPage, IndentAtHdl_Impl, SpinField&, rFld, void )
 {
     // determine value to be set at the chosen list levels