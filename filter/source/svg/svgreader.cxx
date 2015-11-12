LemmingAvalanche/libreoffice-repaired

commit fcd2b149e12c98434721c9af411c128cf2bf8d8c

SVG: tdf#95332 defs can contain shapes so fix tdf#65864 in...
a different way.
This is just a temporal fix as this should be fix by implementing support for 'clipPath'

Change-Id: I451e0a9ae0466c027505672393d623d7ac363e79
Reviewed-on: https://gerrit.libreoffice.org/19615
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Christina Roßmanith <ChrRossmanith@web.de>

filter/source/svg/svgreader.cxx
@@ -90,19 +90,24 @@ template<typename Func> void visitElements(Func& rFunc,
     rFunc.push();
 
     // recurse over children
-    if (eCaller == SHAPE_WRITER && rElem->getTagName() == "defs") {
-        return;
-    }
     uno::Reference<xml::dom::XNodeList> xChildren( rElem->getChildNodes() );
     const sal_Int32 nNumNodes( xChildren->getLength() );
     for( sal_Int32 i=0; i<nNumNodes; ++i )
     {
-        if( xChildren->item(i)->getNodeType() == xml::dom::NodeType_ELEMENT_NODE )
-            visitElements( rFunc,
-                           uno::Reference<xml::dom::XElement>(
-                               xChildren->item(i),
-                               uno::UNO_QUERY_THROW),
-                           eCaller );
+        if( xChildren->item(i)->getNodeType() == xml::dom::NodeType_ELEMENT_NODE ){
+            //tdf#65864
+            //TODO: support clipPath
+            if( !(eCaller == SHAPE_WRITER &&
+                xChildren->item(i)->getNodeName() == "path" &&
+                rElem->getTagName() == "clipPath" &&
+                rElem->getParentNode()->getNodeName() == "defs") ){
+                    visitElements( rFunc,
+                                   uno::Reference<xml::dom::XElement>(
+                                       xChildren->item(i),
+                                       uno::UNO_QUERY_THROW),
+                                   eCaller );
+            }
+        }
     }
 
     // children processing done

commit 36c87b842e50807ac65e716d864023f862e704b8

tdf#95733: SVG: clipPath can be defined outside path...
... so just ignore all clipPath until its support is implemented in filter/source/svg/

Regression introduced by fcd2b149e12c98434721c9af411c128cf2bf8d8c.
Verified that tdf#95332 and tdf#65864 are still rendered correctly

Change-Id: Ie3bdee545a81554ef2a95d373e2559e2540ddaa5
Reviewed-on: https://gerrit.libreoffice.org/19889
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

filter/source/svg/svgreader.cxx
@@ -98,9 +98,7 @@ template<typename Func> void visitElements(Func& rFunc,
             //tdf#65864
             //TODO: support clipPath
             if( !(eCaller == SHAPE_WRITER &&
-                xChildren->item(i)->getNodeName() == "path" &&
-                rElem->getTagName() == "clipPath" &&
-                rElem->getParentNode()->getNodeName() == "defs") ){
+                rElem->getTagName() == "clipPath")){
                     visitElements( rFunc,
                                    uno::Reference<xml::dom::XElement>(
                                        xChildren->item(i),