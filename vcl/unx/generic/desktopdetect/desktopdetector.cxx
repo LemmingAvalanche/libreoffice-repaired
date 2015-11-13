LemmingAvalanche/libreoffice-repaired

commit 4bc28b8b5fb98c7c06c60f5b86c8f3d91c5ed96b

new loplugin: oncevar
Change-Id: If57390510dde4d166be3141b9f658a7453755d3f
Reviewed-on: https://gerrit.libreoffice.org/19815
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

vcl/unx/generic/desktopdetect/desktopdetector.cxx
@@ -231,18 +231,18 @@ static bool is_tde_desktop( Display* pDisplay )
     return false;
 }
 
-static bool is_kde_desktop( Display* pDisplay )
+static bool is_kde3_desktop( Display* pDisplay )
 {
-    if ( NULL != getenv( "KDE_FULL_SESSION" ) )
+    static const char * pFullVersion = getenv( "KDE_FULL_SESSION" );
+    static const char * pSessionVersion = getenv( "KDE_SESSION_VERSION" );
+    if ( pFullVersion )
     {
-        const char *pVer = getenv( "KDE_SESSION_VERSION" );
-        if ( !pVer || pVer[0] == '0' )
+        if ( !pSessionVersion || pSessionVersion[0] == '0' )
         {
             return true; // does not exist => KDE3
         }
 
-        OUString aVer( "3" );
-        if ( aVer.equalsIgnoreAsciiCaseAscii( pVer ) )
+        if ( strcmp(pSessionVersion, "3" ) == 0 )
         {
             return true;
         }
@@ -256,14 +256,10 @@ static bool is_kde_desktop( Display* pDisplay )
 
 static bool is_kde4_desktop( Display* pDisplay )
 {
-    if ( NULL != getenv( "KDE_FULL_SESSION" ) )
-    {
-        OUString aVer( "4" );
-
-        const char *pVer = getenv( "KDE_SESSION_VERSION" );
-        if ( pVer && aVer.equalsIgnoreAsciiCaseAscii( pVer ) )
-            return true;
-    }
+    static const char * pFullVersion = getenv( "KDE_FULL_SESSION" );
+    static const char * pSessionVersion = getenv( "KDE_SESSION_VERSION" );
+    if ( pFullVersion && pSessionVersion && strcmp(pSessionVersion, "4") == 0 )
+        return true;
 
     if ( KDEVersion( pDisplay ) == 4 )
         return true;
@@ -273,14 +269,10 @@ static bool is_kde4_desktop( Display* pDisplay )
 
 static bool is_kde5_desktop( Display* pDisplay )
 {
-    if ( NULL != getenv( "KDE_FULL_SESSION" ) )
-    {
-        OUString aVer( "5" );
-
-        const char *pVer = getenv( "KDE_SESSION_VERSION" );
-        if ( pVer && aVer.equalsIgnoreAsciiCaseAscii( pVer ) )
-            return true;
-    }
+    static const char * pFullVersion = getenv( "KDE_FULL_SESSION" );
+    static const char * pSessionVersion = getenv( "KDE_SESSION_VERSION" );
+    if ( pFullVersion && pSessionVersion && strcmp(pSessionVersion, "5") )
+        return true;
 
     if ( KDEVersion( pDisplay ) == 5 )
         return true;
@@ -400,7 +392,7 @@ DESKTOP_DETECTOR_PUBLIC DesktopType get_desktop_environment()
             ret = DESKTOP_KDE4;
         else if ( is_gnome_desktop( pDisplay ) )
             ret = DESKTOP_GNOME;
-        else if ( is_kde_desktop( pDisplay ) )
+        else if ( is_kde3_desktop( pDisplay ) )
             ret = DESKTOP_KDE3;
         else if ( is_tde_desktop( pDisplay ) )
             ret = DESKTOP_TDE;

commit e78fa59d7be54471e1b29a7dd2e37610568f9174

tdf#95795 vcl: fix KDE5 desktop detection
Regression from commit 4bc28b8b5fb98c7c06c60f5b86c8f3d91c5ed96b (new
loplugin: oncevar, 2015-11-06), KDE4 was detected as KDE5.

Change-Id: If4a7a074052a31c7505f53c03a36a0975d71b3a7

vcl/unx/generic/desktopdetect/desktopdetector.cxx
@@ -271,7 +271,7 @@ static bool is_kde5_desktop( Display* pDisplay )
 {
     static const char * pFullVersion = getenv( "KDE_FULL_SESSION" );
     static const char * pSessionVersion = getenv( "KDE_SESSION_VERSION" );
-    if ( pFullVersion && pSessionVersion && strcmp(pSessionVersion, "5") )
+    if ( pFullVersion && pSessionVersion && strcmp(pSessionVersion, "5") == 0)
         return true;
 
     if ( KDEVersion( pDisplay ) == 5 )