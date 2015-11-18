LemmingAvalanche/libreoffice-repaired

commit 5cd01aa7dd6634286b9d26ef74d6e70085891a8f

tdf#39080 Interactive hide-whitespace mode
Support for enabling and disabling hide-whitespace
by clicking between page frames.

Disabled header and footer decorators when
hide-whitepsace is enabled, and page size
is trimmed to the contents of each page.

Experimental/suggestive mouse pointers added
but only tested on Windows.

Change-Id: Ia2faeeda9e3e783ffaf5340aa69303a4218892a7
Reviewed-on: https://gerrit.libreoffice.org/18156
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sw/source/core/layout/pagechg.cxx
@@ -68,9 +68,9 @@ SwBodyFrm::SwBodyFrm( SwFrameFormat *pFormat, SwFrm* pSib ):
 
 void SwBodyFrm::Format( vcl::RenderContext* /*pRenderContext*/, const SwBorderAttrs * )
 {
-    // Formatting of the body is too simple, thus, it gets an own format method.
+    // Formatting of the body is too simple, thus, it gets its own format method.
     // Borders etc. are not taken into account here.
-    // With is taken from the PrtArea of the Upper, height is the height of the
+    // Width is taken from the PrtArea of the Upper. Height is the height of the
     // PrtArea of the Upper minus any neighbors (for robustness).
     // The PrtArea has always the size of the frame.
 
@@ -597,7 +597,7 @@ void SwPageFrm::_UpdateAttr( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
                 mbValidSize = false;
                 // OD 28.10.2002 #97265# - Don't call <SwPageFrm::MakeAll()>
                 // Calculation of the page is not necessary, because its size is
-                // is invalidated here and further invalidation is done in the
+                // invalidated here and further invalidation is done in the
                 // calling method <SwPageFrm::Modify(..)> and probably by calling
                 // <SwLayoutFrm::Modify(..)> at the end.
                 // It can also causes inconsistences, because the lowers are
@@ -2267,15 +2267,19 @@ bool SwPageFrm::IsOverHeaderFooterArea( const Point& rPt, FrameControlType &rCon
 
     if ( aHeaderArea.IsInside( rPt ) )
     {
-        rControl = Header;
-        return true;
+        if (static_cast<const SwFrameFormat*>(GetRegisteredIn())->GetHeader().IsActive())
+        {
+            rControl = Header;
+            return true;
+        }
     }
     else
     {
         SwRect aFooterArea( Point( Frm().Left(), nLowerLimit ),
                 Size( Frm().Width(), Frm().Bottom() - nLowerLimit ) );
 
-        if ( aFooterArea.IsInside( rPt ) )
+        if ( aFooterArea.IsInside( rPt ) &&
+             static_cast<const SwFrameFormat*>(GetRegisteredIn())->GetFooter().IsActive() )
         {
             rControl = Footer;
             return true;

include/vcl/ptrstyle.hxx
@@ -116,7 +116,9 @@ enum class PointerStyle
     TabSelectW         = 91,
     TabSelectSW        = 92,
     Paintbrush         = 93,
-    LAST               = Paintbrush,
+    HideWhitespace     = 94,
+    ShowWhitespace     = 95,
+    LAST               = ShowWhitespace,
 };
 
 #endif // INCLUDED_VCL_PTRSTYLE_HXX

sw/inc/swtypes.hxx
@@ -58,6 +58,13 @@ typedef long SwTwips;
 #define INVALID_TWIPS   LONG_MAX
 #define TWIPS_MAX       (LONG_MAX - 1)
 
+// Converts Twips to Millimeters (1 twip == 17.573 um).
+template <typename T = SwTwips>
+static SAL_CONSTEXPR T TwipsToMm(const double twips) { return static_cast<T>(twips * 0.017573); }
+// Converts Millimeters to Twips (1 mm == 56.905479 twips).
+template <typename T = SwTwips>
+static SAL_CONSTEXPR T MmToTwips(const double mm) { return static_cast<T>(mm / 0.017573); }
+
 #define MM50   283  // 1/2 cm in TWIPS.
 
 const sal_Int32 COMPLETE_STRING = SAL_MAX_INT32;

sw/inc/viewopt.hxx
@@ -375,11 +375,11 @@ public:
     static SAL_CONSTEXPR sal_uInt16 GetDefDocumentBorder() { return 284; }
     // Default gap between pages: 284 twips == 5.0 mm.
     static SAL_CONSTEXPR sal_uInt16 GetDefGapBetweenPages() { return 284; }
-    // Minimum edge-to-text distance: 114 twips == 2.0 mm.
-    static SAL_CONSTEXPR sal_uInt16 GetMinGapBetweenPages() { return 114; }
+    // Minimum edge-to-text distance: 22 twips == 0.4 mm.
+    static SAL_CONSTEXPR sal_uInt16 GetMinGapBetweenPages() { return 22; }
 
-    inline sal_uInt16 GetDocumentBorder() const { return IsHideWhitespaceMode() ? GetMinGapBetweenPages() : GetDefDocumentBorder(); }
-    inline sal_uInt16 GetGapBetweenPages() const { return IsHideWhitespaceMode() ? GetMinGapBetweenPages() : GetDefGapBetweenPages(); }
+    inline sal_uInt16 GetDocumentBorder() const { return IsWhitespaceHidden() ? GetMinGapBetweenPages() : GetDefDocumentBorder(); }
+    inline sal_uInt16 GetGapBetweenPages() const { return IsWhitespaceHidden() ? GetMinGapBetweenPages() : GetDefGapBetweenPages(); }
 
     inline sal_uInt8  GetPagePrevRow() const      { return nPagePrevRow; }
     inline void  SetPagePrevRow( sal_uInt8 n ) { nPagePrevRow = n; }
@@ -408,6 +408,9 @@ public:
     bool   IsHideWhitespaceMode() const { return mbHideWhitespaceMode; }
     void   SetHideWhitespaceMode( bool bMode ) { mbHideWhitespaceMode = bMode; }
 
+    /// The rules that allow for hiding whitespace.
+    bool   CanHideWhitespace() const { return !IsMultipageView(); }
+    bool   IsWhitespaceHidden() const { return IsHideWhitespaceMode() && !IsMultipageView(); }
     bool   IsMultipageView() const { return IsViewLayoutBookMode() ||
                                             GetViewLayoutColumns() == 0; }
 

sw/source/core/inc/pagefrm.hxx
@@ -324,6 +324,7 @@ public:
     const SwPageFrm& GetFormatPage() const;
 
     /// If in header or footer area, it also indicates the exact area in rControl.
+    /// Header or footer must be active, otherwise returns false.
     bool IsOverHeaderFooterArea( const Point& rPt, FrameControlType &rControl ) const;
 
     // return font used to paint the "empty page" string

sw/source/core/inc/rootfrm.hxx
@@ -291,6 +291,15 @@ public:
      */
     const SwPageFrm* GetPageAtPos( const Point& rPt, const Size* pSize = 0, bool bExtend = false ) const;
 
+    /**
+    * Point rPt: The point to test
+    * @returns true: if rPt is between top/bottom margins of two pages
+    *                in hide-whitespace, rPt can be near the gap, but
+    *                not strictly between pages (in a page) as gap is small.
+    * @returns false: if rPt is in a page or not strictly between two pages
+    */
+    bool IsBetweenPages(const Point& rPt) const;
+
     void CalcFrmRects( SwShellCrsr& );
 
     /**

sw/source/core/layout/calcmove.cxx
@@ -764,6 +764,7 @@ void SwPageFrm::MakeAll(vcl::RenderContext* pRenderContext)
                 else
                 {   // Set FixSize. For pages, this is not done from Upper, but from
                     // the attribute.
+                    //FIXME: This resets the size when (mbValidSize && !mbValidPrtArea).
                     Frm().SSize( pAttrs->GetSize() );
                     Format( pRenderContext, pAttrs );
                 }

sw/source/core/layout/findfrm.cxx
@@ -31,6 +31,7 @@
 #include "fmtftn.hxx"
 #include <txtfrm.hxx>
 #include <calbck.hxx>
+#include <viewopt.hxx>
 
 /// Searches the first ContentFrm in BodyText below the page.
 SwLayoutFrm *SwFootnoteBossFrm::FindBodyCont()
@@ -578,6 +579,46 @@ const SwPageFrm* SwRootFrm::GetPageAtPos( const Point& rPt, const Size* pSize, b
     }
 
     return pRet;
+}
+
+bool SwRootFrm::IsBetweenPages(const Point& rPt) const
+{
+    if (!Frm().IsInside(rPt))
+        return false;
+
+    // top visible page
+    const SwFrm* pPage = Lower();
+    if (pPage == nullptr)
+        return false;
+
+    // skip pages above point:
+    while (pPage && rPt.Y() > pPage->Frm().Bottom())
+        pPage = pPage->GetNext();
+
+    if (pPage &&
+        rPt.X() >= pPage->Frm().Left() &&
+        rPt.X() <= pPage->Frm().Right())
+    {
+        // Trivial case when we're right in between.
+        if (!pPage->Frm().IsInside(rPt))
+            return true;
+
+        // In normal mode the gap is large enough and
+        // header/footer mouse interaction competes with
+        // handling hide-whitespace within them.
+        // In hide-whitespace, however, the gap is too small
+        // for convenience and there are no headers/footers.
+        const SwViewShell *pSh = GetCurrShell();
+        if (pSh && pSh->GetViewOptions()->IsWhitespaceHidden())
+        {
+            // If we are really close to the bottom or top of a page.
+            const auto toEdge = std::min(std::abs(pPage->Frm().Top() - rPt.Y()),
+                                         std::abs(pPage->Frm().Bottom() - rPt.Y()));
+            return toEdge <= MmToTwips(2.0);
+        }
+    }
+
+    return false;
 }
 
 const SwAttrSet* SwFrm::GetAttrSet() const

commit 4c00f66b62e4e8aaab3067201006b8b07fac9c93

tdf#95888 sw: make click to header area show header controls again
Regression from commit 5cd01aa7dd6634286b9d26ef74d6e70085891a8f
(tdf#39080 Interactive hide-whitespace mode, 2015-08-30), the problem
was that SwPageFrm::IsOverHeaderFooterArea() checked if the header is
active before accepting that the user clicked in that area.

Don't require an active header at least in non-hide-whitespace mode, as
the member function is used in the "could there be a header here" sense
in SwEditWin::MouseButtonDown() to show the controls that allows
actually adding a header.

Change-Id: I6f905920113aed1512e333e718a8f26d88a3245d

sw/source/core/layout/pagechg.cxx
@@ -2261,9 +2261,11 @@ bool SwPageFrm::IsOverHeaderFooterArea( const Point& rPt, FrameControlType &rCon
     SwRect aHeaderArea( Frm().TopLeft(),
            Size( Frm().Width(), nUpperLimit - Frm().Top() ) );
 
+    SwViewShell* pViewShell = getRootFrm()->GetCurrShell();
+    bool bHideWhitespaceMode = pViewShell->GetViewOptions()->IsHideWhitespaceMode();
     if ( aHeaderArea.IsInside( rPt ) )
     {
-        if (static_cast<const SwFrameFormat*>(GetRegisteredIn())->GetHeader().IsActive())
+        if (static_cast<const SwFrameFormat*>(GetRegisteredIn())->GetHeader().IsActive() || !bHideWhitespaceMode)
         {
             rControl = Header;
             return true;
@@ -2275,7 +2277,7 @@ bool SwPageFrm::IsOverHeaderFooterArea( const Point& rPt, FrameControlType &rCon
                 Size( Frm().Width(), Frm().Bottom() - nLowerLimit ) );
 
         if ( aFooterArea.IsInside( rPt ) &&
-             static_cast<const SwFrameFormat*>(GetRegisteredIn())->GetFooter().IsActive() )
+             (static_cast<const SwFrameFormat*>(GetRegisteredIn())->GetFooter().IsActive() || !bHideWhitespaceMode) )
         {
             rControl = Footer;
             return true;