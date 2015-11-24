LemmingAvalanche/libreoffice-repaired

commit 9c64857d7d7522f0ad554449f8b230c3208ff208

rhbz#988516: DOCX import: fix context stack when importing header/footer
When a header/footer substream is parsed, a ParagraphGroup is started,
but not ended; so the properties of the last paragraph in the
header/footer are applied to a paragraph in the body.

The obvious fix to add a call to endParagraphGroup() at the end of w:p
element breaks table cells.  So add a call to endParagraphGroup() at
the end of the "hdr"/"ftr" element.

(The problem in the bugdoc became much more visible with commit
ae92778ab7245e2f0fe916c72cf5e4c5054cf4b6, but was there before)

Change-Id: Ib054f1882793049b39424c1076ba5d4b319cd027

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -198,6 +198,18 @@ DECLARE_OOXMLIMPORT_TEST(testN757890, "n757890.docx")
     CPPUNIT_ASSERT_EQUAL(text::HoriOrientation::CENTER, nValue);
 }
 
+DECLARE_OOXMLIMPORT_TEST(testRhbz988516, "rhbz988516.docx")
+{
+    // The problem was that the list properties of the footer leaked into body
+    CPPUNIT_ASSERT_EQUAL(OUString(),
+            getProperty<OUString>(getParagraph(1), "NumberingStyleName"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Enclosure 3"), getParagraph(2)->getString());
+    CPPUNIT_ASSERT_EQUAL(OUString(),
+            getProperty<OUString>(getParagraph(2), "NumberingStyleName"));
+    CPPUNIT_ASSERT_EQUAL(OUString(),
+            getProperty<OUString>(getParagraph(3), "NumberingStyleName"));
+}
+
 DECLARE_OOXMLIMPORT_TEST(testFdo49940, "fdo49940.docx")
 {
     uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);

writerfilter/source/ooxml/model.xml
@@ -24210,7 +24210,9 @@
       <element name="headerReference" tokenid="ooxml:EG_HdrFtrReferences_headerReference"/>
       <element name="footerReference" tokenid="ooxml:EG_HdrFtrReferences_footerReference"/>
     </resource>
-    <resource name="CT_HdrFtr" resource="Stream" tag="header"/>
+    <resource name="CT_HdrFtr" resource="Stream" tag="header">
+      <action name="end" action="endParagraphGroup"/>
+    </resource>
     <resource name="EG_SectPrContents" resource="Properties" tag="section">
       <element name="bidi" tokenid="ooxml:EG_SectPrContents_bidi"/>
       <element name="cols" tokenid="ooxml:EG_SectPrContents_cols"/>

commit ee85e9e4c45072ea5f5083c90086b3afe1695fc6

tdf#78902 VML import: workaround for extreme top margin
Regression from commit 9c64857d7d7522f0ad554449f8b230c3208ff208 (rhbz#988516:
DOCX import: fix context stack when importing header/footer, 2014-03-05),
though that just made an existing Writer layout problem visible.

RTF/WW8/newer (drawingML) DOCX import doesn't have this problem, as those
import pictures as sw graphics, not draw ones.

<w10:wrap type="through"/> is normally mapped to our page wrap (as it uses
"through" in the "not only wrap around, but also in the holes of the shape, if
it has any" context, not in our "text should go through it, so no wrapping"
one), but for some reason in this case (most probably due to the extreme large
negative margins) Word handles the situation as our through, i.e. the text
should not go to the second page, as it would normally happen with a "Word
through" wrapping.

Work around the strange situation by ignoring the wrapping request for extreme
top margin values.

Change-Id: I20555b1fa7a769e20c40a3a5ff3873807403e937

oox/source/vml/vmlshape.cxx
@@ -530,11 +530,18 @@ SimpleShape::SimpleShape( Drawing& rDrawing, const OUString& rService ) :
 {
 }
 
-void lcl_setSurround(PropertySet& rPropSet, const ShapeTypeModel& rTypeModel)
+void lcl_setSurround(PropertySet& rPropSet, const ShapeTypeModel& rTypeModel, const GraphicHelper& rGraphicHelper)
 {
+    OUString aWrapType = rTypeModel.moWrapType.get();
+
+    // Extreme negative top margin? Then the shape will end up at the top of the page, it's pointless to perform any kind of wrapping.
+    sal_Int32 nMarginTop = ConversionHelper::decodeMeasureToHmm(rGraphicHelper, rTypeModel.maMarginTop, 0, false, true);
+    if (nMarginTop < -35277) // Less than 1000 points.
+        aWrapType.clear();
+
     sal_Int32 nSurround = css::text::WrapTextMode_THROUGHT;
-    if ( rTypeModel.moWrapType.get() == "square" || rTypeModel.moWrapType .get()== "tight" ||
-         rTypeModel.moWrapType.get() == "through" )
+    if ( aWrapType == "square" || aWrapType == "tight" ||
+         aWrapType == "through" )
     {
         nSurround = css::text::WrapTextMode_PARALLEL;
         if ( rTypeModel.moWrapSide.get() == "left" )
@@ -542,13 +549,13 @@ void lcl_setSurround(PropertySet& rPropSet, const ShapeTypeModel& rTypeModel)
         else if ( rTypeModel.moWrapSide.get() == "right" )
             nSurround = css::text::WrapTextMode_RIGHT;
     }
-    else if ( rTypeModel.moWrapType.get() == "topAndBottom" )
+    else if ( aWrapType == "topAndBottom" )
         nSurround = css::text::WrapTextMode_NONE;
 
     rPropSet.setProperty(PROP_Surround, nSurround);
 }
 
-void lcl_SetAnchorType(PropertySet& rPropSet, const ShapeTypeModel& rTypeModel)
+void lcl_SetAnchorType(PropertySet& rPropSet, const ShapeTypeModel& rTypeModel, const GraphicHelper& rGraphicHelper)
 {
     if ( rTypeModel.maPositionHorizontal == "center" )
         rPropSet.setAnyProperty(PROP_HoriOrient, makeAny(text::HoriOrientation::CENTER));
@@ -611,7 +618,7 @@ void lcl_SetAnchorType(PropertySet& rPropSet, const ShapeTypeModel& rTypeModel)
     {
         rPropSet.setProperty(PROP_AnchorType, text::TextContentAnchorType_AS_CHARACTER);
     }
-    lcl_setSurround( rPropSet, rTypeModel );
+    lcl_setSurround( rPropSet, rTypeModel, rGraphicHelper );
 }
 
 void lcl_SetRotation(PropertySet& rPropSet, const sal_Int32 nRotation)
@@ -813,7 +820,7 @@ Reference< XShape > SimpleShape::implConvertAndInsert( const Reference< XShapes 
         }
     }
 
-    lcl_SetAnchorType(aPropertySet, maTypeModel);
+    lcl_SetAnchorType(aPropertySet, maTypeModel, rGraphicHelper );
 
     return xShape;
 }
@@ -842,7 +849,8 @@ Reference< XShape > SimpleShape::createPictureObject( const Reference< XShapes >
         if ( !maTypeModel.maRotation.isEmpty() )
             lcl_SetRotation( aPropSet, maTypeModel.maRotation.toInt32() );
 
-        lcl_SetAnchorType(aPropSet, maTypeModel);
+        const GraphicHelper& rGraphicHelper = mrDrawing.getFilter().getGraphicHelper();
+        lcl_SetAnchorType(aPropSet, maTypeModel, rGraphicHelper);
     }
     return xShape;
 }
@@ -1231,7 +1239,8 @@ Reference< XShape > GroupShape::implConvertAndInsert( const Reference< XShapes >
     }
     // Make sure group shapes are inline as well, unless there is an explicit different style.
     PropertySet aPropertySet(xGroupShape);
-    lcl_SetAnchorType(aPropertySet, maTypeModel);
+    const GraphicHelper& rGraphicHelper = mrDrawing.getFilter().getGraphicHelper();
+    lcl_SetAnchorType(aPropertySet, maTypeModel, rGraphicHelper);
     if (!maTypeModel.maRotation.isEmpty())
         lcl_SetRotation(aPropertySet, maTypeModel.maRotation.toInt32());
     return xGroupShape;

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2926,6 +2926,12 @@ DECLARE_OOXMLIMPORT_TEST(testTdf85232, "tdf85232.docx")
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2267), xShape->getPosition().X);
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf78902, "tdf78902.docx")
+{
+    // This hung in layout.
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 DECLARE_OOXMLIMPORT_TEST(testTdf95775, "tdf95775.docx")
 {
     // This must not fail in layout