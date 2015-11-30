LemmingAvalanche/libreoffice-repaired

commit f78b679130232312fbc659f865af8dc143d23619

new loplugin: memoryvar
detect when we can convert a new/delete sequence on a local variable to
use std::unique_ptr

Change-Id: Iecae4e4197eccdfacfce2eed39aa4a69e4a660bc
Reviewed-on: https://gerrit.libreoffice.org/19884
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

dbaccess/source/ui/misc/UITools.cxx
@@ -107,6 +107,7 @@
 #include <connectivity/FValue.hxx>
 
 #include <editeng/justifyitem.hxx>
+#include <memory>
 
 namespace dbaui
 {
@@ -825,7 +826,7 @@ bool callColumnFormatDialog(vcl::Window* _pParent,
     pPool->SetDefaultMetric( SFX_MAPUNIT_TWIP );    // ripped, don't understand why
     pPool->FreezeIdRanges();                        // the same
 
-    SfxItemSet* pFormatDescriptor = new SfxItemSet(*pPool, aAttrMap);
+    std::unique_ptr<SfxItemSet> pFormatDescriptor(new SfxItemSet(*pPool, aAttrMap));
     // fill it
     pFormatDescriptor->Put(SvxHorJustifyItem(_eJustify, SBA_ATTR_ALIGN_HOR_JUSTIFY));
     bool bText = false;
@@ -852,7 +853,7 @@ bool callColumnFormatDialog(vcl::Window* _pParent,
     }
 
     {   // want the dialog to be destroyed before our set
-        ScopedVclPtrInstance< SbaSbAttrDlg > aDlg(_pParent, pFormatDescriptor, _pFormatter, _bHasFormat);
+        ScopedVclPtrInstance< SbaSbAttrDlg > aDlg(_pParent, pFormatDescriptor.get(), _pFormatter, _bHasFormat);
         if (RET_OK == aDlg->Execute())
         {
             // ItemSet->UNO
@@ -890,7 +891,6 @@ bool callColumnFormatDialog(vcl::Window* _pParent,
         }
     }
 
-    delete pFormatDescriptor;
     SfxItemPool::Free(pPool);
     for (sal_uInt16 i=0; i<sizeof(pDefaults)/sizeof(pDefaults[0]); ++i)
         delete pDefaults[i];

basegfx/source/polygon/b2dpolygoncutandtouch.cxx
@@ -29,6 +29,7 @@
 
 #include <vector>
 #include <algorithm>
+#include <memory>
 
 #define SUBDIVIDE_FOR_CUT_TEST_COUNT        (50)
 
@@ -887,7 +888,7 @@ namespace basegfx
                 else
                 {
                     // first solve self cuts and self touches for all contained single polygons
-                    temporaryPolygonData *pTempData = new temporaryPolygonData[nCount];
+                    std::unique_ptr<temporaryPolygonData[]> pTempData(new temporaryPolygonData[nCount]);
                     sal_uInt32 a, b;
 
                     for(a = 0L; a < nCount; a++)
@@ -934,8 +935,6 @@ namespace basegfx
                     {
                         aRetval.append(mergeTemporaryPointsAndPolygon(pTempData[a].getPolygon(), pTempData[a].getTemporaryPointVector()));
                     }
-
-                    delete[] pTempData;
                 }
 
                 return aRetval;

basic/source/runtime/methods.cxx
@@ -3718,10 +3718,10 @@ RTLFUNC(Shell)
         ++iter;
 
         sal_uInt16 nParamCount = sal::static_int_cast< sal_uInt16 >(aTokenList.size() - 1 );
-        rtl_uString** pParamList = nullptr;
+        std::unique_ptr<rtl_uString*[]> pParamList;
         if( nParamCount )
         {
-            pParamList = new rtl_uString*[nParamCount];
+            pParamList.reset( new rtl_uString*[nParamCount]);
             for(int iList = 0; iter != aTokenList.end(); ++iList, ++iter)
             {
                 const OUString& rParamStr = (*iter);
@@ -3734,7 +3734,7 @@ RTLFUNC(Shell)
         oslProcess pApp;
         bool bSucc = osl_executeProcess(
                     aOUStrProgURL.pData,
-                    pParamList,
+                    pParamList.get(),
                     nParamCount,
                     nOptions,
                     nullptr,
@@ -3752,8 +3752,6 @@ RTLFUNC(Shell)
         {
             rtl_uString_release(pParamList[j]);
         }
-
-        delete [] pParamList;
 
         if( !bSucc )
         {
@@ -4352,7 +4350,7 @@ RTLFUNC(StrConv)
         // convert the string to byte string, preserving unicode (2 bytes per character)
         sal_Int32 nSize = aNewStr.getLength()*2;
         const sal_Unicode* pSrc = aNewStr.getStr();
-        sal_Char* pChar = new sal_Char[nSize+1];
+        std::unique_ptr<sal_Char[]> pChar(new sal_Char[nSize+1]);
         for( sal_Int32 i=0; i < nSize; i++ )
         {
             pChar[i] = static_cast< sal_Char >( (i%2) ? ((*pSrc) >> 8) & 0xff : (*pSrc) & 0xff );
@@ -4362,8 +4360,7 @@ RTLFUNC(StrConv)
             }
         }
         pChar[nSize] = '\0';
-        OString aOStr(pChar);
-        delete[] pChar;
+        OString aOStr(pChar.get());
 
         // there is no concept about default codepage in unix. so it is incorrectly in unix
         OUString aOUStr = OStringToOUString(aOStr, osl_getThreadTextEncoding());

basic/source/sbx/sbxstr.cxx
@@ -25,6 +25,7 @@
 #include "sbxres.hxx"
 #include "runtime.hxx"
 #include <rtl/ustrbuf.hxx>
+#include <memory>
 
 // The conversion of an item onto String was handled via the Put-Methods
 // of the several data types to avoid duplicated code.
@@ -161,10 +162,13 @@ void ImpPutString( SbxValues* p, const OUString* n )
 {
     SbxValues aTmp;
     aTmp.eType = SbxSTRING;
-    OUString* pTmp = nullptr;
+    std::unique_ptr<OUString> pTmp;
     // as a precaution, if a NULL-Ptr appears
     if( !n )
-        n = pTmp = new OUString;
+    {
+        pTmp.reset(new OUString);
+        n = pTmp.get();
+    }
     aTmp.pOUString = const_cast<OUString*>(n);
     switch( +p->eType )
     {
@@ -252,7 +256,6 @@ void ImpPutString( SbxValues* p, const OUString* n )
         default:
             SbxBase::SetError( ERRCODE_SBX_CONVERSION );
     }
-    delete pTmp;
 }
 
 

compilerplugins/clang/memoryvar.cxx
@@ -1,0 +1,239 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <set>
+
+#include "plugin.hxx"
+#include "compat.hxx"
+#include "clang/AST/CXXInheritance.h"
+
+// Check for local variables that we are calling delete on
+
+namespace
+{
+
+class MemoryVar:
+    public RecursiveASTVisitor<MemoryVar>, public loplugin::Plugin
+{
+public:
+    explicit MemoryVar(InstantiationData const & data): Plugin(data) {}
+
+    virtual void run() override {
+        TraverseDecl(compiler.getASTContext().getTranslationUnitDecl());
+    }
+
+    bool TraverseFunctionDecl(FunctionDecl*);
+    bool VisitCXXDeleteExpr(const CXXDeleteExpr*);
+    bool VisitCXXNewExpr(const CXXNewExpr* );
+    bool VisitBinaryOperator(const BinaryOperator*);
+    bool VisitReturnStmt(const ReturnStmt*);
+
+private:
+    bool mbChecking;
+    std::set<SourceLocation> maVarUsesSet;
+    std::set<SourceLocation> maVarNewSet;
+    std::set<SourceLocation> maVarIgnoreSet;
+    std::map<SourceLocation,SourceRange> maVarDeclSourceRangeMap;
+    std::map<SourceLocation,SourceRange> maVarDeleteSourceRangeMap;
+    StringRef getFilename(SourceLocation loc);
+};
+
+StringRef MemoryVar::getFilename(SourceLocation loc)
+{
+    SourceLocation spellingLocation = compiler.getSourceManager().getSpellingLoc(loc);
+    StringRef name { compiler.getSourceManager().getFilename(spellingLocation) };
+    return name;
+}
+
+bool MemoryVar::TraverseFunctionDecl(FunctionDecl * decl)
+{
+    if (ignoreLocation(decl)) {
+        return true;
+    }
+    if (!decl->hasBody() || !decl->isThisDeclarationADefinition()) {
+        return true;
+    }
+
+    maVarUsesSet.clear();
+    maVarNewSet.clear();
+    maVarIgnoreSet.clear();
+    maVarDeclSourceRangeMap.clear();
+    maVarDeleteSourceRangeMap.clear();
+
+    assert(!mbChecking);
+    mbChecking = true;
+    TraverseStmt(decl->getBody());
+    mbChecking = false;
+
+    for (const auto& varLoc : maVarUsesSet)
+    {
+        // checking the location of the var instead of the function because for some reason
+        // I'm not getting accurate results from clang right now
+        StringRef aFileName = getFilename(varLoc);
+        // TODO these files are doing some weird stuff I don't know how to ignore yet
+        if (aFileName.startswith(SRCDIR "/vcl/source/filter")) {
+           return true;
+        }
+        if (aFileName.startswith(SRCDIR "/sw/source/core/layout/frmtool.cxx")) {
+           return true;
+        }
+
+
+        if (maVarNewSet.find(varLoc) == maVarNewSet.end())
+            continue;
+        if (maVarIgnoreSet.find(varLoc) != maVarIgnoreSet.end())
+            continue;
+
+        report(DiagnosticsEngine::Warning,
+                "calling new and delete on a local var, rather use std::unique_ptr",
+               varLoc)
+            << maVarDeclSourceRangeMap[varLoc];
+        report(DiagnosticsEngine::Note,
+               "delete called here",
+               maVarDeleteSourceRangeMap[varLoc].getBegin())
+            << maVarDeleteSourceRangeMap[varLoc];
+        cout << "xxxx " << aFileName.str() << endl;
+    }
+    return true;
+}
+
+bool MemoryVar::VisitCXXDeleteExpr(const CXXDeleteExpr *deleteExpr)
+{
+    if (!mbChecking)
+        return true;
+    if (ignoreLocation(deleteExpr)) {
+        return true;
+    }
+    const Expr* argumentExpr = deleteExpr->getArgument();
+    if (isa<CastExpr>(argumentExpr)) {
+        argumentExpr = dyn_cast<CastExpr>(argumentExpr)->getSubExpr();
+    }
+    const DeclRefExpr* declRefExpr = dyn_cast<DeclRefExpr>(argumentExpr);
+    if (!declRefExpr)
+        return true;
+    const Decl* decl = declRefExpr->getDecl();
+    if (!isa<VarDecl>(decl) || isa<ParmVarDecl>(decl)) {
+        return true;
+    }
+    const VarDecl * varDecl = dyn_cast<VarDecl>(decl)->getCanonicalDecl();
+    if (varDecl->hasGlobalStorage()) {
+        return true;
+    }
+
+    SourceLocation loc = varDecl->getLocation();
+
+    if (maVarUsesSet.find(loc) == maVarUsesSet.end()) {
+        maVarUsesSet.insert(loc);
+        maVarDeclSourceRangeMap[loc] = varDecl->getSourceRange();
+        maVarDeleteSourceRangeMap[loc] = declRefExpr->getSourceRange();
+    }
+    return true;
+}
+
+bool MemoryVar::VisitCXXNewExpr(const CXXNewExpr *newExpr)
+{
+    if (!mbChecking)
+        return true;
+    if (ignoreLocation(newExpr)) {
+        return true;
+    }
+    const Stmt* stmt = parentStmt(newExpr);
+
+    const DeclStmt* declStmt = dyn_cast<DeclStmt>(stmt);
+    if (declStmt) {
+        const VarDecl* varDecl = dyn_cast<VarDecl>(declStmt->getSingleDecl());
+        if (varDecl) {
+            varDecl = varDecl->getCanonicalDecl();
+            SourceLocation loc = varDecl->getLocation();
+            maVarNewSet.insert(loc);
+        }
+        return true;
+    }
+
+    const BinaryOperator* binaryOp = dyn_cast<BinaryOperator>(stmt);
+    if (binaryOp && binaryOp->getOpcode() == BO_Assign) {
+        const DeclRefExpr* declRefExpr = dyn_cast<DeclRefExpr>(binaryOp->getLHS());
+        if (declRefExpr) {
+            const VarDecl* varDecl = dyn_cast<VarDecl>(declRefExpr->getDecl());
+            if (varDecl) {
+                varDecl = varDecl->getCanonicalDecl();
+                SourceLocation loc = varDecl->getLocation();
+                maVarNewSet.insert(loc);
+            }
+        }
+    }
+    return true;
+}
+
+// Ignore cases where the variable in question is assigned to another variable
+bool MemoryVar::VisitBinaryOperator(const BinaryOperator *binaryOp)
+{
+    if (!mbChecking)
+        return true;
+    if (ignoreLocation(binaryOp)) {
+        return true;
+    }
+    if (binaryOp->getOpcode() != BO_Assign) {
+        return true;
+    }
+    const Expr* expr = binaryOp->getRHS();
+    // unwrap casts
+    while (isa<CastExpr>(expr)) {
+        expr = dyn_cast<CastExpr>(expr)->getSubExpr();
+    }
+    const DeclRefExpr* declRefExpr = dyn_cast<DeclRefExpr>(expr);
+    if (!declRefExpr) {
+        return true;
+    }
+    const VarDecl* varDecl = dyn_cast<VarDecl>(declRefExpr->getDecl());
+    if (!varDecl) {
+        return true;
+    }
+    varDecl = varDecl->getCanonicalDecl();
+    maVarIgnoreSet.insert(varDecl->getLocation());
+    return true;
+}
+
+// Ignore cases where the variable in question is returned from a function
+bool MemoryVar::VisitReturnStmt(const ReturnStmt *returnStmt)
+{
+    if (!mbChecking)
+        return true;
+    if (ignoreLocation(returnStmt)) {
+        return true;
+    }
+    const Expr* expr = returnStmt->getRetValue();
+    if (!expr) {
+        return true;
+    }
+    // unwrap casts
+    while (isa<CastExpr>(expr)) {
+        expr = dyn_cast<CastExpr>(expr)->getSubExpr();
+    }
+    const DeclRefExpr* declRefExpr = dyn_cast<DeclRefExpr>(expr);
+    if (!declRefExpr) {
+        return true;
+    }
+    const VarDecl* varDecl = dyn_cast<VarDecl>(declRefExpr->getDecl());
+    if (!varDecl) {
+        return true;
+    }
+    varDecl = varDecl->getCanonicalDecl();
+    maVarIgnoreSet.insert(varDecl->getLocation());
+    return true;
+}
+
+loplugin::Plugin::Registration< MemoryVar > X("memoryvar");
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

connectivity/source/drivers/dbase/dindexnode.cxx
@@ -895,11 +895,10 @@ SvStream& connectivity::dbase::WriteONDXPage(SvStream &rStream, const ONDXPage& 
         sal_Size nRemainSize = nBufferSize - nTell;
         if ( nRemainSize <= nBufferSize )
         {
-            char* pEmptyData = new char[nRemainSize];
-            memset(pEmptyData,0x00,nRemainSize);
-            rStream.Write(pEmptyData, nRemainSize);
+            std::unique_ptr<char[]> pEmptyData( new char[nRemainSize] );
+            memset(pEmptyData.get(), 0x00, nRemainSize);
+            rStream.Write(pEmptyData.get(), nRemainSize);
             rStream.Seek(nTell);
-            delete [] pEmptyData;
         }
     }
     return rStream;

extensions/source/plugin/unx/npnapi.cxx
@@ -44,6 +44,7 @@
 #include <config_vclplug.h>
 
 #include <npwrap.hxx>
+#include <memory>
 
 extern PluginConnector* pConnector;
 extern XtAppContext app_context;
@@ -270,7 +271,7 @@ static NPError l_NPN_RequestRead( NPStream* stream, NPByteRange* rangeList )
         pRange = pRange->next;
     }
 
-    sal_uInt32* pArray = new sal_uInt32[ 2 * nRanges ];
+    std::unique_ptr<sal_uInt32[]> pArray(new sal_uInt32[ 2 * nRanges ]);
     pRange = rangeList;
     sal_uInt32 n = 0;
     while( pRange )
@@ -285,17 +286,15 @@ static NPError l_NPN_RequestRead( NPStream* stream, NPByteRange* rangeList )
         Transact( eNPN_RequestRead,
                   &nFileID, sizeof( nFileID ),
                   &nRanges, sizeof( nRanges ),
-                  pArray, sizeof( sal_uInt32 ) * 2 * nRanges,
+                  pArray.get(), sizeof( sal_uInt32 ) * 2 * nRanges,
                   NULL );
 
     if( ! pMes )
     {
-        delete[] pArray;
         return NPERR_GENERIC_ERROR;
     }
 
     NPError aRet = PluginConnector::GetNPError( pMes );
-    delete [] pArray;
     delete pMes;
     return aRet;
 }

filter/source/graphicfilter/ieps/ieps.cxx
@@ -581,19 +581,17 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
         nPSStreamPos = nOrigPos;            // no preview available _>so we must get the size manually
         nPSSize = rStream.Seek( STREAM_SEEK_TO_END ) - nOrigPos;
     }
-    sal_uInt8* pHeader = new sal_uInt8[ 22 ];
+    std::unique_ptr<sal_uInt8[]> pHeader( new sal_uInt8[ 22 ] );
     rStream.Seek( nPSStreamPos );
-    rStream.Read( pHeader, 22 );    // check PostScript header
-    if ( ImplSearchEntry( pHeader, reinterpret_cast<sal_uInt8 const *>("%!PS-Adobe"), 10, 10 ) &&
+    rStream.Read( pHeader.get(), 22 );    // check PostScript header
+    if ( ImplSearchEntry( pHeader.get(), reinterpret_cast<sal_uInt8 const *>("%!PS-Adobe"), 10, 10 ) &&
         ImplSearchEntry( &pHeader[ 15 ], reinterpret_cast<sal_uInt8 const *>("EPS"), 3, 3 ) )
     {
-        bool bGraphicLinkCreated = false;
-
         rStream.Seek( nPSStreamPos );
-        sal_uInt8* pBuf = new sal_uInt8[ nPSSize ];
+        std::unique_ptr<sal_uInt8[]> pBuf( new sal_uInt8[ nPSSize ] );
 
         sal_uInt32 nBufStartPos = rStream.Tell();
-        sal_uInt32 nBytesRead = rStream.Read( pBuf, nPSSize );
+        sal_uInt32 nBytesRead = rStream.Read( pBuf.get(), nPSSize );
         if ( nBytesRead == nPSSize )
         {
             sal_uInt32 nSecurityCount = 32;
@@ -601,7 +599,7 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
             // the eps prolog
             if (!bHasPreview && nBytesRead >= nSecurityCount)
             {
-                sal_uInt8* pDest = ImplSearchEntry( pBuf, reinterpret_cast<sal_uInt8 const *>("%%BeginPreview:"), nBytesRead - nSecurityCount, 15 );
+                sal_uInt8* pDest = ImplSearchEntry( pBuf.get(), reinterpret_cast<sal_uInt8 const *>("%%BeginPreview:"), nBytesRead - nSecurityCount, 15 );
                 if ( pDest  )
                 {
                     pDest += 15;
@@ -612,7 +610,7 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
                     pDest = ImplSearchEntry( pDest, reinterpret_cast<sal_uInt8 const *>("%"), 16, 1 );       // go to the first Scanline
                     if ( nSecurityCount && pDest && nWidth && nHeight && ( ( nBitDepth == 1 ) || ( nBitDepth == 8 ) ) && nScanLines )
                     {
-                        rStream.Seek( nBufStartPos + ( pDest - pBuf ) );
+                        rStream.Seek( nBufStartPos + ( pDest - pBuf.get() ) );
 
                         Bitmap aBitmap( Size( nWidth, nHeight ), 1 );
                         BitmapWriteAccess* pAcc = aBitmap.AcquireWriteAccess();
@@ -700,7 +698,7 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
                 }
             }
 
-            sal_uInt8* pDest = ImplSearchEntry( pBuf, reinterpret_cast<sal_uInt8 const *>("%%BoundingBox:"), nBytesRead, 14 );
+            sal_uInt8* pDest = ImplSearchEntry( pBuf.get(), reinterpret_cast<sal_uInt8 const *>("%%BoundingBox:"), nBytesRead, 14 );
             if ( pDest )
             {
                 nSecurityCount = 100;
@@ -713,8 +711,6 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
                 }
                 if ( nSecurityCount)
                 {
-                    bGraphicLinkCreated = true;
-                    GfxLink     aGfxLink( pBuf, nPSSize, GFX_LINK_TYPE_EPS_BUFFER, true ) ;
                     GDIMetaFile aMtf;
 
                     long nWidth =  nNumb[2] - nNumb[0] + 1;
@@ -723,18 +719,20 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
                     // if there is no preview -> try with gs to make one
                     if (!bHasPreview && !utl::ConfigManager::IsAvoidConfig())
                     {
-                        bHasPreview = RenderAsEMF(pBuf, nBytesRead, aGraphic);
+                        bHasPreview = RenderAsEMF(pBuf.get(), nBytesRead, aGraphic);
                         if (!bHasPreview)
-                            bHasPreview = RenderAsBMP(pBuf, nBytesRead, aGraphic);
+                            bHasPreview = RenderAsBMP(pBuf.get(), nBytesRead, aGraphic);
                     }
 
                     // if there is no preview -> make a red box
                     if( !bHasPreview )
                     {
-                        MakePreview(pBuf, nBytesRead, nWidth, nHeight,
+                        MakePreview(pBuf.get(), nBytesRead, nWidth, nHeight,
                             aGraphic);
                     }
 
+                    GfxLink     aGfxLink( pBuf.get(), nPSSize, GFX_LINK_TYPE_EPS_BUFFER, true ) ;
+                    pBuf.release();
                     aMtf.AddAction( static_cast<MetaAction*>( new MetaEPSAction( Point(), Size( nWidth, nHeight ),
                                                                       aGfxLink, aGraphic.GetGDIMetaFile() ) ) );
                     CreateMtfReplacementAction( aMtf, rStream, nOrigPos, nPSSize, nPosWMF, nSizeWMF, nPosTIFF, nSizeTIFF );
@@ -746,11 +744,7 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
                 }
             }
         }
-
-        if ( !bGraphicLinkCreated )
-            delete[] pBuf;
     }
-    delete[] pHeader;
     rStream.SetEndian(nOldFormat);
     rStream.Seek( nOrigPos );
     return bRetValue;

commit 2d2fdb9912d713bae6fecf463ad096d3543e4d77

tdf#96135 - Crash : When Field Format Window Close
regression from commit f78b679130232312fbc659f865af8dc143d23619
"new loplugin: memoryvar"

Change-Id: Id37226646a561579a375110bf684140d1fa8cb92

dbaccess/source/ui/misc/UITools.cxx
@@ -891,6 +891,7 @@ bool callColumnFormatDialog(vcl::Window* _pParent,
         }
     }
 
+    pFormatDescriptor.reset();
     SfxItemPool::Free(pPool);
     for (sal_uInt16 i=0; i<sizeof(pDefaults)/sizeof(pDefaults[0]); ++i)
         delete pDefaults[i];