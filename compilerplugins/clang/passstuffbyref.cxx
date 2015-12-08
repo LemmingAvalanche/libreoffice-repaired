LemmingAvalanche/libreoffice-repaired

commit 09fe85eebac3c2d7ed59ba2a0bddf465336df51f

cppcheck:stlIfStrFind
"Inefficient usage of string::find() in condition; string::compare() would be faster."

Change-Id: I90403b1d05eff6499c10be33068e5fd4fed30b62
Reviewed-on: https://gerrit.libreoffice.org/19966
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

compilerplugins/clang/passstuffbyref.cxx
@@ -103,7 +103,7 @@ bool PassStuffByRef::isFat(QualType type, std::string * name) {
     }
     *name = type.getUnqualifiedType().getCanonicalType().getAsString();
     if (*name == "class rtl::OUString" || *name == "class rtl::OString"
-        || name->find("class com::sun::star::uno::Sequence") == 0)
+        || name->compare("class com::sun::star::uno::Sequence") == 0)
     {
         return true;
     }

helpcompiler/source/HelpCompiler.cxx
@@ -335,7 +335,7 @@ void myparser::traverse( xmlNodePtr parentNode )
 
             std::string hid;
 
-            if (branch.find("hid") == 0)
+            if (branch.compare("hid") == 0)
             {
                 size_t index = branch.find('/');
                 if (index != std::string::npos)
@@ -493,7 +493,7 @@ bool HelpCompiler::compile()
 
     if ( !bExtensionMode && !fileName.empty())
     {
-        if (fileName.find("/text/") == 0)
+        if (fileName.compare("/text/") == 0)
         {
             int len = strlen("/text/");
             actMod = fileName.substr(len);

helpcompiler/source/HelpLinker.cxx
@@ -400,7 +400,7 @@ void HelpLinker::link() throw(HelpProcessingException, BasicCodeTagger::TaggerEx
                 continue;
 
             std::string documentPath = streamTable.document_path;
-            if (documentPath.find("/") == 0)
+            if (documentPath.compare("/") == 0)
                 documentPath = documentPath.substr(1);
 
             std::string documentJarfile = streamTable.document_module + ".jar";

commit db168bab122863dc328d165bbccd7bb7f58d30ba

tdf#96042: 'std::string::find("something") == 0' means "startsWith()".
This should fix a regression from 09fe85eebac3c2d7ed59ba2a0bddf465336df51f,
apparently the cppcheck's advice is misleading.

Change-Id: I427ecaa1eb3c9841cb6112997b9b51feda4583d0

compilerplugins/clang/passstuffbyref.cxx
@@ -103,7 +103,7 @@ bool PassStuffByRef::isFat(QualType type, std::string * name) {
     }
     *name = type.getUnqualifiedType().getCanonicalType().getAsString();
     if (*name == "class rtl::OUString" || *name == "class rtl::OString"
-        || name->compare("class com::sun::star::uno::Sequence") == 0)
+        || name->compare(0, 35, "class com::sun::star::uno::Sequence") == 0)
     {
         return true;
     }

helpcompiler/source/HelpCompiler.cxx
@@ -334,7 +334,7 @@ void myparser::traverse( xmlNodePtr parentNode )
 
             std::string hid;
 
-            if (branch.compare("hid") == 0)
+            if (branch.compare(0, 3, "hid") != 0)
             {
                 size_t index = branch.find('/');
                 if (index != std::string::npos)
@@ -492,7 +492,7 @@ bool HelpCompiler::compile()
 
     if ( !bExtensionMode && !fileName.empty())
     {
-        if (fileName.compare("/text/") == 0)
+        if (fileName.compare(0, 6, "/text/") == 0)
         {
             int len = strlen("/text/");
             actMod = fileName.substr(len);

helpcompiler/source/HelpLinker.cxx
@@ -396,7 +396,7 @@ void HelpLinker::link() throw(HelpProcessingException, BasicCodeTagger::TaggerEx
                 continue;
 
             std::string documentPath = streamTable.document_path;
-            if (documentPath.compare("/") == 0)
+            if (documentPath.compare(0, 1, "/") == 0)
                 documentPath = documentPath.substr(1);
 
             std::string documentJarfile = streamTable.document_module + ".jar";