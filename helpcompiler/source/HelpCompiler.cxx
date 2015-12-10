LemmingAvalanche/libreoffice-repaired

commit db168bab122863dc328d165bbccd7bb7f58d30ba

tdf#96042: 'std::string::find("something") == 0' means "startsWith()".
This should fix a regression from 09fe85eebac3c2d7ed59ba2a0bddf465336df51f,
apparently the cppcheck's advice is misleading.

Change-Id: I427ecaa1eb3c9841cb6112997b9b51feda4583d0

helpcompiler/source/HelpCompiler.cxx
@@ -334,7 +334,7 @@ void myparser::traverse( xmlNodePtr parentNode )
 
             std::string hid;
 
-            if (branch.compare("hid") == 0)
+            if (branch.compare(0, 3, "hid") != 0)
             {
                 size_t index = branch.find('/');
                 if (index != std::string::npos)
@@ -492,7 +492,7 @@ bool HelpCompiler::compile()
 
     if ( !bExtensionMode && !fileName.empty())
     {
-        if (fileName.compare("/text/") == 0)
+        if (fileName.compare(0, 6, "/text/") == 0)
         {
             int len = strlen("/text/");
             actMod = fileName.substr(len);

compilerplugins/clang/passstuffbyref.cxx
@@ -103,7 +103,7 @@ bool PassStuffByRef::isFat(QualType type, std::string * name) {
     }
     *name = type.getUnqualifiedType().getCanonicalType().getAsString();
     if (*name == "class rtl::OUString" || *name == "class rtl::OString"
-        || name->compare("class com::sun::star::uno::Sequence") == 0)
+        || name->compare(0, 35, "class com::sun::star::uno::Sequence") == 0)
     {
         return true;
     }

helpcompiler/source/HelpLinker.cxx
@@ -396,7 +396,7 @@ void HelpLinker::link() throw(HelpProcessingException, BasicCodeTagger::TaggerEx
                 continue;
 
             std::string documentPath = streamTable.document_path;
-            if (documentPath.compare("/") == 0)
+            if (documentPath.compare(0, 1, "/") == 0)
                 documentPath = documentPath.substr(1);
 
             std::string documentJarfile = streamTable.document_module + ".jar";

commit 8f9694f2571980502d1fedd17950f52fc9237eb0

tdf#96042: Oops, fix a typo.
This fixes a regression from commit db168bab122863dc328d165bbccd7bb7f58d30ba.

Change-Id: Ie8496392f4811cc1ff146d7a5c43988da0d539c2

helpcompiler/source/HelpCompiler.cxx
@@ -334,7 +334,7 @@ void myparser::traverse( xmlNodePtr parentNode )
 
             std::string hid;
 
-            if (branch.compare(0, 3, "hid") != 0)
+            if (branch.compare(0, 3, "hid") == 0)
             {
                 size_t index = branch.find('/');
                 if (index != std::string::npos)