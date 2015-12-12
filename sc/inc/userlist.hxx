LemmingAvalanche/libreoffice-repaired

commit 23f1a39dd5a1fb360fe8ae53f021393fedb94b59

sc: boost::ptr_vector->std::vector
Change-Id: Ic4af0579f31c2076fcbc4ac76266e0ae49cb2be8

sc/inc/userlist.hxx
@@ -22,7 +22,6 @@
 
 #include "scdllapi.h"
 
-#include <boost/ptr_container/ptr_vector.hpp>
 #include <vector>
 
 /**
@@ -47,7 +46,6 @@ private:
 public:
     ScUserListData(const OUString& rStr);
     ScUserListData(const ScUserListData& rData);
-    ~ScUserListData();
 
     const OUString& GetString() const { return aStr; }
     void SetString(const OUString& rStr);
@@ -63,7 +61,7 @@ public:
  */
 class SC_DLLPUBLIC ScUserList
 {
-    typedef ::boost::ptr_vector<ScUserListData> DataType;
+    typedef std::vector<ScUserListData> DataType;
     DataType maData;
 public:
     typedef DataType::iterator iterator;
@@ -85,8 +83,9 @@ public:
     iterator begin();
     const_iterator begin() const;
     void clear();
+    void reserve(size_t size) { maData.reserve(size); }
     size_t size() const;
-    void push_back(ScUserListData* p);
+    void push_back(const ScUserListData& r) { maData.push_back(r); }
     void erase(iterator itr);
 };
 

sc/source/core/tool/appoptio.cxx
@@ -180,11 +180,11 @@ static void lcl_SetSortList( const Any& rValue )
         if (!bDefault)
         {
             aList.clear();
+            aList.reserve(nCount);
 
             for (long i=0; i<nCount; i++)
             {
-                ScUserListData* pNew = new ScUserListData( pArray[i] );
-                aList.push_back(pNew);
+                aList.push_back( ScUserListData( pArray[i] ) );
             }
         }
 

sc/source/core/tool/userlist.cxx
@@ -96,10 +96,6 @@ ScUserListData::ScUserListData(const ScUserListData& rData) :
     InitTokens();
 }
 
-ScUserListData::~ScUserListData()
-{
-}
-
 void ScUserListData::SetString( const OUString& rStr )
 {
     aStr = rStr;
@@ -237,9 +233,9 @@ ScUserList::ScUserList()
             OUString aDayLong = aDayLongBuf.makeStringAndClear();
 
             if ( !HasEntry( aDayShort ) )
-                maData.push_back( new ScUserListData( aDayShort ));
+                maData.push_back( ScUserListData( aDayShort ));
             if ( !HasEntry( aDayLong ) )
-                maData.push_back( new ScUserListData( aDayLong ));
+                maData.push_back( ScUserListData( aDayLong ));
         }
 
         xCal = xCalendars[j].Months;
@@ -262,9 +258,9 @@ ScUserList::ScUserList()
             OUString aMonthLong = aMonthLongBuf.makeStringAndClear();
 
             if ( !HasEntry( aMonthShort ) )
-                maData.push_back( new ScUserListData( aMonthShort ));
+                maData.push_back( ScUserListData( aMonthShort ));
             if ( !HasEntry( aMonthLong ) )
-                maData.push_back( new ScUserListData( aMonthLong ));
+                maData.push_back( ScUserListData( aMonthLong ));
         }
     }
 }
@@ -348,11 +344,6 @@ void ScUserList::clear()
 size_t ScUserList::size() const
 {
     return maData.size();
-}
-
-void ScUserList::push_back(ScUserListData* p)
-{
-    maData.push_back(p);
 }
 
 void ScUserList::erase(iterator itr)

sc/source/ui/optdlg/tpusrlst.cxx
@@ -344,7 +344,7 @@ void ScTpUserLists::AddNewList( const OUString& rEntriesStr )
 
     MakeListStr( theEntriesStr );
 
-    pUserLists->push_back(new ScUserListData(theEntriesStr));
+    pUserLists->push_back(ScUserListData(rEntriesStr));
 }
 
 void ScTpUserLists::CopyListFromArea( const ScRefAddress& rStartPos,

sc/source/ui/unoobj/appluno.cxx
@@ -358,12 +358,12 @@ void SAL_CALL ScSpreadsheetSettings::setPropertyValue(
 
             pUserList->clear();                 // alle Eintraege raus
             sal_uInt16 nCount = (sal_uInt16)aSeq.getLength();
+            pUserList->reserve(nCount);
             const OUString* pAry = aSeq.getConstArray();
             for (sal_uInt16 i=0; i<nCount; i++)
             {
                 OUString aEntry = pAry[i];
-                ScUserListData* pData = new ScUserListData(aEntry);
-                pUserList->push_back(pData);
+                pUserList->push_back( ScUserListData(aEntry) );
             }
             bSaveApp = true;    // Liste wird mit den App-Optionen gespeichert
         }

commit 9bbada9023a5771aa32babb4de0f3a763dd56957

tdf#96339 fix bug in sort list
this bug was caused by commit
23f1a39dd5a1fb360fe8ae53f021393fedb94b59
"sc: boost::ptr_vector->std::vector"

Since the code in question likes passing around a pointer
to the element of the vector, convert to using
std::vector<std::unique_ptr>

Change-Id: I9e9676fe7c2dc32e23ba6708aaea1f16c1bf2ff8

sc/inc/userlist.hxx
@@ -22,6 +22,7 @@
 
 #include "scdllapi.h"
 
+#include <memory>
 #include <vector>
 
 /**
@@ -46,6 +47,7 @@ private:
 public:
     ScUserListData(const OUString& rStr);
     ScUserListData(const ScUserListData& rData);
+    ~ScUserListData();
 
     const OUString& GetString() const { return aStr; }
     void SetString(const OUString& rStr);
@@ -61,7 +63,7 @@ public:
  */
 class SC_DLLPUBLIC ScUserList
 {
-    typedef std::vector<ScUserListData> DataType;
+    typedef std::vector< std::unique_ptr<ScUserListData> > DataType;
     DataType maData;
 public:
     typedef DataType::iterator iterator;
@@ -83,9 +85,8 @@ public:
     iterator begin();
     const_iterator begin() const;
     void clear();
-    void reserve(size_t nSize) { maData.reserve(nSize); }
     size_t size() const;
-    void push_back(const ScUserListData& r) { maData.push_back(r); }
+    void push_back(ScUserListData* p);
     void erase(iterator itr);
 };
 

sc/source/core/tool/appoptio.cxx
@@ -180,11 +180,11 @@ static void lcl_SetSortList( const Any& rValue )
         if (!bDefault)
         {
             aList.clear();
-            aList.reserve(nCount);
 
             for (long i=0; i<nCount; i++)
             {
-                aList.push_back( ScUserListData( pArray[i] ) );
+                ScUserListData* pNew = new ScUserListData( pArray[i] );
+                aList.push_back(pNew);
             }
         }
 

sc/source/core/tool/userlist.cxx
@@ -24,6 +24,7 @@
 #include <unotools/localedatawrapper.hxx>
 #include <unotools/calendarwrapper.hxx>
 #include <unotools/transliterationwrapper.hxx>
+#include <o3tl/make_unique.hxx>
 
 #include <boost/bind.hpp>
 #include <algorithm>
@@ -94,6 +95,10 @@ ScUserListData::ScUserListData(const ScUserListData& rData) :
     aStr(rData.aStr)
 {
     InitTokens();
+}
+
+ScUserListData::~ScUserListData()
+{
 }
 
 void ScUserListData::SetString( const OUString& rStr )
@@ -233,9 +238,9 @@ ScUserList::ScUserList()
             OUString aDayLong = aDayLongBuf.makeStringAndClear();
 
             if ( !HasEntry( aDayShort ) )
-                maData.push_back( ScUserListData( aDayShort ));
+                maData.push_back( o3tl::make_unique<ScUserListData>( aDayShort ));
             if ( !HasEntry( aDayLong ) )
-                maData.push_back( ScUserListData( aDayLong ));
+                maData.push_back( o3tl::make_unique<ScUserListData>( aDayLong ));
         }
 
         xCal = xCalendars[j].Months;
@@ -258,15 +263,18 @@ ScUserList::ScUserList()
             OUString aMonthLong = aMonthLongBuf.makeStringAndClear();
 
             if ( !HasEntry( aMonthShort ) )
-                maData.push_back( ScUserListData( aMonthShort ));
+                maData.push_back( o3tl::make_unique<ScUserListData>( aMonthShort ));
             if ( !HasEntry( aMonthLong ) )
-                maData.push_back( ScUserListData( aMonthLong ));
+                maData.push_back( o3tl::make_unique<ScUserListData>( aMonthLong ));
         }
     }
 }
 
-ScUserList::ScUserList(const ScUserList& r) :
-    maData(r.maData) {}
+ScUserList::ScUserList(const ScUserList& rOther)
+{
+    for (const std::unique_ptr<ScUserListData>& rData : rOther.maData)
+        maData.push_back( o3tl::make_unique<ScUserListData>(*rData.get()) );
+}
 
 const ScUserListData* ScUserList::GetData(const OUString& rSubStr) const
 {
@@ -277,12 +285,12 @@ const ScUserListData* ScUserList::GetData(const OUString& rSubStr) const
 
     for (; itr != itrEnd; ++itr)
     {
-        if (itr->GetSubIndex(rSubStr, nIndex, bMatchCase))
+        if ((*itr)->GetSubIndex(rSubStr, nIndex, bMatchCase))
         {
             if (bMatchCase)
-                return &(*itr);
+                return itr->get();
             if (!pFirstCaseInsensitive)
-                pFirstCaseInsensitive = &(*itr);
+                pFirstCaseInsensitive = itr->get();
         }
     }
 
@@ -291,17 +299,19 @@ const ScUserListData* ScUserList::GetData(const OUString& rSubStr) const
 
 const ScUserListData& ScUserList::operator[](size_t nIndex) const
 {
-    return maData[nIndex];
+    return *maData[nIndex].get();
 }
 
 ScUserListData& ScUserList::operator[](size_t nIndex)
 {
-    return maData[nIndex];
+    return *maData[nIndex].get();
 }
 
-ScUserList& ScUserList::operator=( const ScUserList& r )
+ScUserList& ScUserList::operator=( const ScUserList& rOther )
 {
-    maData = r.maData;
+    maData.clear();
+    for (const std::unique_ptr<ScUserListData>& rData : rOther.maData)
+        maData.push_back( o3tl::make_unique<ScUserListData>(*rData.get()) );
     return *this;
 }
 
@@ -313,8 +323,8 @@ bool ScUserList::operator==( const ScUserList& r ) const
     DataType::const_iterator itr1 = maData.begin(), itr2 = r.maData.begin(), itrEnd = maData.end();
     for (; itr1 != itrEnd; ++itr1, ++itr2)
     {
-        const ScUserListData& v1 = *itr1;
-        const ScUserListData& v2 = *itr2;
+        const ScUserListData& v1 = *itr1->get();
+        const ScUserListData& v2 = *itr2->get();
         if (v1.GetString() != v2.GetString() || v1.GetSubCount() != v2.GetSubCount())
             return false;
     }
@@ -344,6 +354,11 @@ void ScUserList::clear()
 size_t ScUserList::size() const
 {
     return maData.size();
+}
+
+void ScUserList::push_back(ScUserListData* p)
+{
+    maData.push_back(std::unique_ptr<ScUserListData>(p));
 }
 
 void ScUserList::erase(iterator itr)

sc/source/ui/optdlg/tpusrlst.cxx
@@ -344,7 +344,7 @@ void ScTpUserLists::AddNewList( const OUString& rEntriesStr )
 
     MakeListStr( theEntriesStr );
 
-    pUserLists->push_back(ScUserListData(rEntriesStr));
+    pUserLists->push_back(new ScUserListData(theEntriesStr));
 }
 
 void ScTpUserLists::CopyListFromArea( const ScRefAddress& rStartPos,

sc/source/ui/unoobj/appluno.cxx
@@ -356,12 +356,12 @@ void SAL_CALL ScSpreadsheetSettings::setPropertyValue(
 
             pUserList->clear();                 // alle Eintraege raus
             sal_uInt16 nCount = (sal_uInt16)aSeq.getLength();
-            pUserList->reserve(nCount);
             const OUString* pAry = aSeq.getConstArray();
             for (sal_uInt16 i=0; i<nCount; i++)
             {
                 OUString aEntry = pAry[i];
-                pUserList->push_back( ScUserListData(aEntry) );
+                ScUserListData* pData = new ScUserListData(aEntry);
+                pUserList->push_back(pData);
             }
             bSaveApp = true;    // Liste wird mit den App-Optionen gespeichert
         }