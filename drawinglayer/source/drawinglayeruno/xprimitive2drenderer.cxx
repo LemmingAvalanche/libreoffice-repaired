LemmingAvalanche/libreoffice-repaired

commit 25975a73d4e17c515b3898443e2ab917c03683bd

tdf#69977: uno::Sequence is expensive
when used as a mutable data-structure. Plain std::vector halves the time
taken to display the chart dialog

Create a class to represent the std::vector we are going to be passing
around, and move some of the utility methods into it to make the code
prettier.
Also create an optimised append(&&) method for the common case of
appending small temporaries.

Change-Id: I7f5b43fb4a8a84e40e6a52fcb7e9f974091b4485

drawinglayer/source/drawinglayeruno/xprimitive2drenderer.cxx
@@ -161,8 +161,8 @@ namespace drawinglayer
                     const primitive2d::Primitive2DReference xEmbedRef(
                         new primitive2d::TransformPrimitive2D(
                             aEmbedding,
-                            Primitive2DSequence));
-                    const primitive2d::Primitive2DSequence xEmbedSeq(&xEmbedRef, 1);
+                            primitive2d::Primitive2DContainer()));
+                    const primitive2d::Primitive2DContainer xEmbedSeq { xEmbedRef };
 
                     BitmapEx aBitmapEx(
                         tools::convertToBitmapEx(

commit 984ee9d4cd3b88897c7a07f5ebb82bc5c7903fa8

tdf#96541 - dialog "About LibreOfficeDev" has lost its logo
regression from 25975a7 "tdf#69977: uno::Sequence is expensive"

Change-Id: I792b5971a7a3a9532cccb36add5b558c68e397ef

drawinglayer/source/drawinglayeruno/xprimitive2drenderer.cxx
@@ -25,6 +25,7 @@
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <cppuhelper/implbase2.hxx>
 #include <cppuhelper/supportsservice.hxx>
+#include <comphelper/sequence.hxx>
 #include <com/sun/star/xml/sax/XParser.hpp>
 #include <com/sun/star/xml/sax/InputSource.hpp>
 #include <comphelper/processfactory.hxx>
@@ -112,7 +113,7 @@ namespace drawinglayer
         }
 
         uno::Reference< rendering::XBitmap > XPrimitive2DRenderer::rasterize(
-            const uno::Sequence< uno::Reference< graphic::XPrimitive2D > >& Primitive2DSequence,
+            const uno::Sequence< uno::Reference< graphic::XPrimitive2D > >& aPrimitive2DSequence,
             const uno::Sequence< beans::PropertyValue >& aViewInformationSequence,
             ::sal_uInt32 DPI_X,
             ::sal_uInt32 DPI_Y,
@@ -121,7 +122,7 @@ namespace drawinglayer
         {
             uno::Reference< rendering::XBitmap > XBitmap;
 
-            if(Primitive2DSequence.hasElements())
+            if(aPrimitive2DSequence.hasElements())
             {
                 const basegfx::B2DRange aRange(Range.X1, Range.Y1, Range.X2, Range.Y2);
                 const double fWidth(aRange.getWidth());
@@ -161,7 +162,7 @@ namespace drawinglayer
                     const primitive2d::Primitive2DReference xEmbedRef(
                         new primitive2d::TransformPrimitive2D(
                             aEmbedding,
-                            primitive2d::Primitive2DContainer()));
+                            comphelper::sequenceToContainer<primitive2d::Primitive2DContainer>(aPrimitive2DSequence)));
                     const primitive2d::Primitive2DContainer xEmbedSeq { xEmbedRef };
 
                     BitmapEx aBitmapEx(