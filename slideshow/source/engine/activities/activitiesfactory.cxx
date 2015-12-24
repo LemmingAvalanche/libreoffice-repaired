LemmingAvalanche/libreoffice-repaired

commit a578d7f26562598b0801dd0726b1ced1f986e5f7

loplugin:unusedmethods
Change-Id: Id3b5cd75d4357336ed592ef11a3f34d209f8e95f
Reviewed-on: https://gerrit.libreoffice.org/19636
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

slideshow/source/engine/activities/activitiesfactory.cxx
@@ -170,74 +170,6 @@ public:
             "From and one of To or By, or To or By alone must be valid" );
     }
 
-    virtual void startAnimation()
-    {
-        if (this->isDisposed() || !mpAnim)
-            return;
-        BaseType::startAnimation();
-
-        // start animation
-        mpAnim->start( BaseType::getShape(),
-                       BaseType::getShapeAttributeLayer() );
-
-        // setup start and end value. Determine animation
-        // start value only when animation actually
-        // started up (this order is part of the Animation
-        // interface contract)
-        const ValueType aAnimationStartValue( mpAnim->getUnderlyingValue() );
-
-        // first of all, determine general type of
-        // animation, by inspecting which of the FromToBy values
-        // are actually valid.
-        // See http://www.w3.org/TR/smil20/animation.html#AnimationNS-FromToBy
-        // for a definition
-        if( maFrom )
-        {
-            // From-to or From-by animation. According to
-            // SMIL spec, the To value takes precedence
-            // over the By value, if both are specified
-            if( maTo )
-            {
-                // From-To animation
-                maStartValue = *maFrom;
-                maEndValue = *maTo;
-            }
-            else if( maBy )
-            {
-                // From-By animation
-                maStartValue = *maFrom;
-                maEndValue = maStartValue + *maBy;
-            }
-        }
-        else
-        {
-            maStartValue = aAnimationStartValue;
-            maStartInterpolationValue = maStartValue;
-
-            // By or To animation. According to SMIL spec,
-            // the To value takes precedence over the By
-            // value, if both are specified
-            if( maTo )
-            {
-                // To animation
-
-                // According to the SMIL spec
-                // (http://www.w3.org/TR/smil20/animation.html#animationNS-ToAnimation),
-                // the to animation interpolates between
-                // the _running_ underlying value and the to value (as the end value)
-                mbDynamicStartValue = true;
-                maPreviousValue = maStartValue;
-                maEndValue = *maTo;
-            }
-            else if( maBy )
-            {
-                // By animation
-                maStartValue = aAnimationStartValue;
-                maEndValue = maStartValue + *maBy;
-            }
-        }
-    }
-
     virtual void endAnimation()
     {
         // end animation
@@ -338,13 +270,6 @@ public:
             else
                 (*mpAnim)( getPresentationValue( maEndValue ) );
         }
-    }
-
-    /// Disposable:
-    virtual void dispose()
-    {
-        mpAnim.reset();
-        BaseType::dispose();
     }
 
 private:
@@ -521,17 +446,6 @@ public:
     {
         ENSURE_OR_THROW( mpAnim, "Invalid animation object" );
         ENSURE_OR_THROW( !rValues.empty(), "Empty value vector" );
-    }
-
-    virtual void startAnimation()
-    {
-        if (this->isDisposed() || !mpAnim)
-            return;
-        BaseType::startAnimation();
-
-        // start animation
-        mpAnim->start( BaseType::getShape(),
-                       BaseType::getShapeAttributeLayer() );
     }
 
     virtual void endAnimation()

canvas/source/tools/canvastools.cxx
@@ -62,45 +62,6 @@
 
 using namespace ::com::sun::star;
 
-namespace com { namespace sun { namespace star { namespace rendering
-{
-    bool operator==( const RenderState& renderState1,
-                     const RenderState& renderState2 )
-    {
-        if( renderState1.Clip != renderState2.Clip )
-            return false;
-
-        if( renderState1.DeviceColor != renderState2.DeviceColor )
-            return false;
-
-        if( renderState1.CompositeOperation != renderState2.CompositeOperation )
-            return false;
-
-        ::basegfx::B2DHomMatrix mat1, mat2;
-        ::canvas::tools::getRenderStateTransform( mat1, renderState1 );
-        ::canvas::tools::getRenderStateTransform( mat2, renderState2 );
-        if( mat1 != mat2 )
-            return false;
-
-        return true;
-    }
-
-    bool operator==( const ViewState& viewState1,
-                     const ViewState& viewState2 )
-    {
-        if( viewState1.Clip != viewState2.Clip )
-            return false;
-
-        ::basegfx::B2DHomMatrix mat1, mat2;
-        ::canvas::tools::getViewStateTransform( mat1, viewState1 );
-        ::canvas::tools::getViewStateTransform( mat2, viewState2 );
-        if( mat1 != mat2 )
-            return false;
-
-        return true;
-    }
-}}}}
-
 namespace canvas
 {
     namespace tools

configmgr/source/modifications.hxx
@@ -45,8 +45,6 @@ public:
 
     void remove(Path const & path);
 
-    void clear() { root_.children.clear(); }
-
     bool empty() const { return root_.children.empty(); }
 
     Node const & getRoot() const { return root_;}

connectivity/source/drivers/evoab2/NTable.hxx
@@ -29,8 +29,6 @@ namespace connectivity
     {
         typedef connectivity::sdbcx::OTable OEvoabTable_TYPEDEF;
 
-        OUString getTypeString(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xColProp);
-
         class OEvoabTable : public OEvoabTable_TYPEDEF
         {
             ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDatabaseMetaData > m_xMetaData;

connectivity/source/drivers/kab/KTable.hxx
@@ -29,8 +29,6 @@ namespace connectivity
     {
         typedef connectivity::sdbcx::OTable KabTable_TYPEDEF;
 
-        OUString getTypeString(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xColProp);
-
         class KabTable : public KabTable_TYPEDEF
         {
             ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDatabaseMetaData > m_xMetaData;

connectivity/source/drivers/macab/MacabTable.hxx
@@ -29,8 +29,6 @@ namespace connectivity
     {
         typedef connectivity::sdbcx::OTable MacabTable_TYPEDEF;
 
-        OUString getTypeString(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xColProp);
-
         class MacabTable : public MacabTable_TYPEDEF
         {
             ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDatabaseMetaData > m_xMetaData;

connectivity/source/inc/hsqldb/HTable.hxx
@@ -29,8 +29,6 @@ namespace connectivity
     namespace hsqldb
     {
 
-        OUString getTypeString(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xColProp);
-
         class OHSQLTable;
         typedef ::comphelper::OIdPropertyArrayUsageHelper< OHSQLTable > OHSQLTable_PROP;
         class OHSQLTable :  public OTableHelper

connectivity/source/inc/mysql/YTable.hxx
@@ -29,8 +29,6 @@ namespace connectivity
     namespace mysql
     {
 
-        OUString getTypeString(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xColProp);
-
         class OMySQLTable;
         typedef ::comphelper::OIdPropertyArrayUsageHelper< OMySQLTable >    OMySQLTable_PROP;
         class OMySQLTable : public OTableHelper

commit fb2dbd8db9852e58e377d0ba0d90d32522b1fc1c

tdf#96522 - [ANIMATION] Animation Spiral in does not work
blind fix for this since I cannot seem to make it work on my dev box no
matter how far back I go.
This seems to be a regression from commit
a578d7f26562598b0801dd0726b1ced1f986e5f7 "loplugin:unusedmethods"

Change-Id: I99f0f0ca662a5fe246057b1bb8da3bd3d3c7bac6

slideshow/source/engine/activities/activitiesfactory.cxx
@@ -141,6 +141,7 @@ public:
         value, or start fresh each time.
     */
     FromToByActivity(
+        const OptionalValueType&                      rFrom,
         const OptionalValueType&                      rTo,
         const OptionalValueType&                      rBy,
         const ActivityParameters&                     rParms,
@@ -148,6 +149,9 @@ public:
         const Interpolator< ValueType >&              rInterpolator,
         bool                                          bCumulative )
         : BaseType( rParms ),
+          maFrom( rFrom ),
+          maTo( rTo ),
+          maBy( rBy ),
           mpFormula( rParms.mpFormula ),
           maStartValue(),
           maEndValue(),
@@ -164,6 +168,74 @@ public:
         ENSURE_OR_THROW(
             rTo || rBy,
             "From and one of To or By, or To or By alone must be valid" );
+    }
+
+    virtual void startAnimation()
+    {
+        if (this->isDisposed() || !mpAnim)
+            return;
+        BaseType::startAnimation();
+
+        // start animation
+        mpAnim->start( BaseType::getShape(),
+                       BaseType::getShapeAttributeLayer() );
+
+        // setup start and end value. Determine animation
+        // start value only when animation actually
+        // started up (this order is part of the Animation
+        // interface contract)
+        const ValueType aAnimationStartValue( mpAnim->getUnderlyingValue() );
+
+        // first of all, determine general type of
+        // animation, by inspecting which of the FromToBy values
+        // are actually valid.
+        // See http://www.w3.org/TR/smil20/animation.html#AnimationNS-FromToBy
+        // for a definition
+        if( maFrom )
+        {
+            // From-to or From-by animation. According to
+            // SMIL spec, the To value takes precedence
+            // over the By value, if both are specified
+            if( maTo )
+            {
+                // From-To animation
+                maStartValue = *maFrom;
+                maEndValue = *maTo;
+            }
+            else if( maBy )
+            {
+                // From-By animation
+                maStartValue = *maFrom;
+                maEndValue = maStartValue + *maBy;
+            }
+        }
+        else
+        {
+            maStartValue = aAnimationStartValue;
+            maStartInterpolationValue = maStartValue;
+
+            // By or To animation. According to SMIL spec,
+            // the To value takes precedence over the By
+            // value, if both are specified
+            if( maTo )
+            {
+                // To animation
+
+                // According to the SMIL spec
+                // (http://www.w3.org/TR/smil20/animation.html#animationNS-ToAnimation),
+                // the to animation interpolates between
+                // the _running_ underlying value and the to value (as the end value)
+                mbDynamicStartValue = true;
+                maPreviousValue = maStartValue;
+                maEndValue = *maTo;
+            }
+            else if( maBy )
+            {
+                // By animation
+                maStartValue = aAnimationStartValue;
+                maEndValue = maStartValue + *maBy;
+            }
+        }
     }
 
     virtual void endAnimation()
@@ -268,7 +340,18 @@ public:
         }
     }
 
+    /// Disposable:
+    virtual void dispose()
+    {
+        mpAnim.reset();
+        BaseType::dispose();
+    }
+
 private:
+    const OptionalValueType                 maFrom;
+    const OptionalValueType                 maTo;
+    const OptionalValueType                 maBy;
+
     ExpressionNodeSharedPtr                 mpFormula;
 
     ValueType                               maStartValue;
@@ -338,6 +421,7 @@ AnimationActivitySharedPtr createFromToByActivity(
 
     return AnimationActivitySharedPtr(
         new FromToByActivity<BaseType, AnimationType>(
+            aFrom,
             aTo,
             aBy,
             rParms,
@@ -437,6 +521,17 @@ public:
     {
         ENSURE_OR_THROW( mpAnim, "Invalid animation object" );
         ENSURE_OR_THROW( !rValues.empty(), "Empty value vector" );
+    }
+
+    virtual void startAnimation()
+    {
+        if (this->isDisposed() || !mpAnim)
+            return;
+        BaseType::startAnimation();
+
+        // start animation
+        mpAnim->start( BaseType::getShape(),
+                       BaseType::getShapeAttributeLayer() );
     }
 
     virtual void endAnimation()