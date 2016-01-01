LemmingAvalanche/libreoffice-repaired

commit 08bcf4b3b0b5182da527af3e9e383a309e8de70f

vcl: create accessor functions for ImplDevFontAttributes
Change-Id: I0494a1db1a1e5c164bd5da2d0a221a8db6c494d6
Reviewed-on: https://gerrit.libreoffice.org/21016
Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
Tested-by: Chris Sherlock <chris.sherlock79@gmail.com>

vcl/inc/outfont.hxx
@@ -82,16 +82,46 @@ private:
 class ImplDevFontAttributes : public ImplFontAttributes
 {
 public:
-    bool               IsEmbeddable() const      { return mbEmbeddable; }
-    bool               IsSubsettable() const     { return mbSubsettable; }
+    int                 GetQuality() const      { return mnQuality; }
+    OUString            GetMapNames() const     { return maMapNames; }
 
-public: // TODO: hide members behind accessor methods
-    OUString           maMapNames;       // List of family name aliases separated with ';'
-    int                mnQuality;        // Quality (used when similar fonts compete)
-    bool               mbOrientation;    // true: physical font can be rotated
-    bool               mbDevice;         // true: built in font
-    bool               mbSubsettable;    // true: a subset of the font can be created
-    bool               mbEmbeddable;     // true: the font can be embedded
+    bool                IsBuiltInFont() const   { return mbDevice; }
+    bool                CanEmbed() const        { return mbEmbeddable; }
+    bool                CanSubset() const       { return mbSubsettable; }
+    bool                CanRotate() const       { return mbOrientation; }
+    bool                HasMapNames() const     { return (maMapNames.getLength() > 0); }
+
+    void                SetQuality( int nQuality )                  { mnQuality = nQuality; }
+    void                IncreaseQualityBy( int nQualityAmount )     { mnQuality += nQualityAmount; }
+    void                DecreaseQualityBy( int nQualityAmount )     { mnQuality -= nQualityAmount; }
+    void                SetMapNames( OUString aMapNames )           { maMapNames = aMapNames; }
+    void                AddMapName( OUString aMapName )
+                            {
+                                if( maMapNames.getLength() > 0 )
+                                {
+                                    maMapNames += ";";
+                                }
+                                else
+                                {
+                                    SAL_WARN("vcl.fonts", "New map name is empty");
+                                    return;
+                                }
+
+                                maMapNames += aMapName;
+                            }
+
+    void                SetBuiltInFontFlag( bool bIsBuiltInFont )   { mbDevice = bIsBuiltInFont; }
+    void                SetEmbeddableFlag ( bool bEmbeddable )      { mbEmbeddable = bEmbeddable; }
+    void                SetSubsettableFlag( bool bSubsettable )     { mbSubsettable = bSubsettable; }
+    void                SetOrientationFlag( bool bCanRotate )       { mbOrientation = bCanRotate; }
+
+private:
+    OUString            maMapNames;       // List of family name aliases separated with ';'
+    int                 mnQuality;        // Quality (used when similar fonts compete)
+    bool                mbOrientation;    // true: physical font can be rotated
+    bool                mbDevice;         // true: built in font
+    bool                mbSubsettable;    // true: a subset of the font can be created
+    bool                mbEmbeddable;     // true: the font can be embedded
 };
 
 class FontSelectPatternAttributes : public ImplFontAttributes

vcl/generic/glyphs/gcach_ftyp.cxx
@@ -209,9 +209,9 @@ FtFontInfo::FtFontInfo( const ImplDevFontAttributes& rDevFontAttributes,
     mpGlyph2Char( nullptr )
 {
     // prefer font with low ID
-    maDevFontAttributes.mnQuality += 10000 - nFontId;
+    maDevFontAttributes.IncreaseQualityBy( 10000 - nFontId );
     // prefer font with matching file names
-    maDevFontAttributes.mnQuality += mpFontFile->GetLangBoost();
+    maDevFontAttributes.IncreaseQualityBy( mpFontFile->GetLangBoost() );
 }
 
 FtFontInfo::~FtFontInfo()
@@ -423,8 +423,8 @@ ImplFTSFontData::ImplFTSFontData( FtFontInfo* pFI, const ImplDevFontAttributes& 
 :   PhysicalFontFace( rDFA, IFTSFONT_MAGIC ),
     mpFtFontInfo( pFI )
 {
-    mbDevice        = false;
-    mbOrientation   = true;
+    SetBuiltInFontFlag( false );
+    SetOrientationFlag( true );
 }
 
 ImplFontEntry* ImplFTSFontData::CreateFontInstance( FontSelectPattern& rFSD ) const

vcl/generic/print/genpspgraphics.cxx
@@ -878,7 +878,7 @@ bool GenPspGraphics::AddTempDevFontHelper( PhysicalFontCollection* pFontCollecti
 
         // inform glyph cache of new font
         ImplDevFontAttributes aDFA = GenPspGraphics::Info2DevFontAttributes( aInfo );
-        aDFA.mnQuality += 5800;
+        aDFA.IncreaseQualityBy( 5800 );
 
         int nFaceNum = rMgr.getFontFaceNumber( aInfo.m_nID );
 
@@ -921,7 +921,7 @@ void GenPspGraphics::GetFontMetric( ImplFontMetricData *pMetric, int )
     {
         ImplDevFontAttributes aDFA = Info2DevFontAttributes( aInfo );
         static_cast<ImplFontAttributes&>(*pMetric) = aDFA;
-        pMetric->mbDevice       = aDFA.mbDevice;
+        pMetric->mbDevice       = aDFA.IsBuiltInFont();
         pMetric->mbScalableFont = true;
         pMetric->mbTrueTypeFont = false; // FIXME, needed?
 
@@ -1093,40 +1093,34 @@ ImplDevFontAttributes GenPspGraphics::Info2DevFontAttributes( const psp::FastPri
     aDFA.SetWidthType( rInfo.m_eWidth );
     aDFA.SetPitch( rInfo.m_ePitch );
     aDFA.SetSymbolFlag( (rInfo.m_aEncoding == RTL_TEXTENCODING_SYMBOL) );
-    aDFA.mbSubsettable  = rInfo.m_bSubsettable;
-    aDFA.mbEmbeddable   = rInfo.m_bEmbeddable;
+    aDFA.SetSubsettableFlag( rInfo.m_bSubsettable );
+    aDFA.SetEmbeddableFlag( rInfo.m_bEmbeddable );
 
     switch( rInfo.m_eType )
     {
         case psp::fonttype::TrueType:
-            aDFA.mnQuality       = 512;
-            aDFA.mbDevice        = false;
+            aDFA.SetQuality( 512 );
+            aDFA.SetBuiltInFontFlag( false );
             break;
         case psp::fonttype::Type1:
-            aDFA.mnQuality       = 0;
-            aDFA.mbDevice        = false;
+            aDFA.SetQuality( 0 );
+            aDFA.SetBuiltInFontFlag( false );
             break;
         default:
-            aDFA.mnQuality       = 0;
-            aDFA.mbDevice        = false;
+            aDFA.SetQuality( 0 );
+            aDFA.SetBuiltInFontFlag( false );
             break;
     }
 
-    aDFA.mbOrientation   = true;
+    aDFA.SetOrientationFlag( true );
 
     // add font family name aliases
     ::std::list< OUString >::const_iterator it = rInfo.m_aAliases.begin();
-    bool bHasMapNames = false;
     for(; it != rInfo.m_aAliases.end(); ++it )
-    {
-        if( bHasMapNames )
-            aDFA.maMapNames += ";";
-        aDFA.maMapNames += *it;
-        bHasMapNames = true;
-    }
+        aDFA.AddMapName( *it );
 
 #if OSL_DEBUG_LEVEL > 2
-    if( bHasMapNames )
+    if( aDFA.HasMapNames() )
     {
         OString aOrigName(OUStringToOString(aDFA.GetFamilyName(), osl_getThreadTextEncoding()));
         OString aAliasNames(OUStringToOString(aDFA.GetAliasNames(), osl_getThreadTextEncoding()));
@@ -1186,7 +1180,7 @@ void GenPspGraphics::AnnounceFonts( PhysicalFontCollection* pFontCollection, con
     }
 
     ImplPspFontData* pFD = new ImplPspFontData( aInfo );
-    pFD->mnQuality += nQuality;
+    pFD->IncreaseQualityBy( nQuality );
     pFontCollection->Add( pFD );
 }
 

vcl/quartz/ctfonts.cxx
@@ -277,9 +277,9 @@ ImplDevFontAttributes DevFontFromCTFontDescriptor( CTFontDescriptorRef pFD, bool
 {
     // all CoreText fonts are device fonts that can rotate just fine
     ImplDevFontAttributes rDFA;
-    rDFA.mbOrientation = true;
-    rDFA.mbDevice = true;
-    rDFA.mnQuality = 0;
+    rDFA.SetOrientationFlag( true );
+    rDFA.SetBuiltInFontFlag( true );
+    rDFA.SetQuality( 0 );
 
     // reset the font attributes
     rDFA.SetFamilyType( FAMILY_DONTKNOW );
@@ -290,8 +290,8 @@ ImplDevFontAttributes DevFontFromCTFontDescriptor( CTFontDescriptorRef pFD, bool
     rDFA.SetSymbolFlag( false );
 
     // all scalable fonts on this platform are subsettable
-    rDFA.mbEmbeddable = false;
-    rDFA.mbSubsettable = true;
+    rDFA.SetEmbeddableFlag( false );
+    rDFA.SetSubsettableFlag( true );
 
     // get font name
 #ifdef MACOSX

vcl/quartz/salgdi.cxx
@@ -694,9 +694,9 @@ void AquaSalGraphics::GetGlyphWidths( const PhysicalFontFace* pFontData, bool bV
     rGlyphWidths.clear();
     rUnicodeEnc.clear();
 
-    if( !pFontData->IsSubsettable() )
+    if( !pFontData->CanSubset() )
     {
-        if( pFontData->IsEmbeddable() )
+        if( pFontData->CanEmbed() )
         {
             // get individual character widths
             OSL_FAIL("not implemented for non-subsettable fonts!\n");

vcl/source/font/PhysicalFontFace.cxx
@@ -158,7 +158,7 @@ bool PhysicalFontFace::IsBetterMatch( const FontSelectPattern& rFSD, FontMatchSt
             nMatch += 600;
     }
 
-    if( mbDevice )
+    if( IsBuiltInFont() )
         nMatch += 1;
 
     int nHeightMatch = 0;

vcl/source/font/PhysicalFontFamily.cxx
@@ -116,10 +116,10 @@ bool PhysicalFontFamily::AddFontFace( PhysicalFontFace* pNewFontFace )
     if( maFontFaces.empty() )
     {
         maFamilyName   = pNewFontFace->GetFamilyName();
-        maMapNames     = pNewFontFace->maMapNames;
+        maMapNames     = pNewFontFace->GetMapNames();
         meFamily       = pNewFontFace->GetFamilyType();
         mePitch        = pNewFontFace->GetPitch();
-        mnMinQuality   = pNewFontFace->mnQuality;
+        mnMinQuality   = pNewFontFace->GetQuality();
     }
     else
     {
@@ -127,8 +127,8 @@ bool PhysicalFontFamily::AddFontFace( PhysicalFontFace* pNewFontFace )
             meFamily = pNewFontFace->GetFamilyType();
         if( mePitch == PITCH_DONTKNOW )
             mePitch = pNewFontFace->GetPitch();
-        if( mnMinQuality > pNewFontFace->mnQuality )
-            mnMinQuality = pNewFontFace->mnQuality;
+        if( mnMinQuality > pNewFontFace->GetQuality() )
+            mnMinQuality = pNewFontFace->GetQuality();
     }
 
     // set attributes for attribute based font matching
@@ -172,11 +172,11 @@ bool PhysicalFontFamily::AddFontFace( PhysicalFontFace* pNewFontFace )
             break;
 
         // ignore duplicate if its quality is worse
-        if( pNewFontFace->mnQuality < pFoundFontFace->mnQuality )
+        if( pNewFontFace->GetQuality() < pFoundFontFace->GetQuality() )
             return false;
 
         // keep the device font if its quality is good enough
-        if( (pNewFontFace->mnQuality == pFoundFontFace->mnQuality) && (pFoundFontFace->mbDevice || !pNewFontFace->mbDevice) )
+        if( (pNewFontFace->GetQuality() == pFoundFontFace->GetQuality()) && (pFoundFontFace->IsBuiltInFont() || !pNewFontFace->IsBuiltInFont()) )
             return false;
 
         // replace existing font face with a better one
@@ -277,7 +277,7 @@ void PhysicalFontFamily::UpdateCloneFontList( PhysicalFontCollection& rFontColle
 
         if( bScalable && !pFoundFontFace->IsScalable() )
             continue;
-        if( bEmbeddable && !pFoundFontFace->IsEmbeddable() && !pFoundFontFace->IsSubsettable() )
+        if( bEmbeddable && !pFoundFontFace->CanEmbed() && !pFoundFontFace->CanSubset() )
             continue;
 
         PhysicalFontFace* pClonedFace = pFoundFontFace->Clone();

vcl/source/gdi/pdfwriter_impl.cxx
@@ -2274,11 +2274,11 @@ static ImplDevFontAttributes GetDevFontAttributes( const PDFWriterImpl::BuiltinF
     aDFA.SetItalic( rBuiltin.m_eItalic );
     aDFA.SetWidthType( rBuiltin.m_eWidthType );
 
-    aDFA.mbOrientation  = true;
-    aDFA.mbDevice       = true;
-    aDFA.mnQuality      = 50000;
-    aDFA.mbSubsettable  = false;
-    aDFA.mbEmbeddable   = false;
+    aDFA.SetOrientationFlag( true );
+    aDFA.SetBuiltInFontFlag( true );
+    aDFA.SetQuality( 50000 );
+    aDFA.SetSubsettableFlag( false );
+    aDFA.SetEmbeddableFlag( false );
     return aDFA;
 }
 
@@ -2982,7 +2982,7 @@ std::map< sal_Int32, sal_Int32 > PDFWriterImpl::emitSystemFont( const PhysicalFo
 
     assert(pGraphics);
 
-    if( pFont->IsEmbeddable() )
+    if( pFont->CanEmbed() )
     {
         const unsigned char* pFontData = nullptr;
         long nFontLen = 0;
@@ -3004,7 +3004,7 @@ std::map< sal_Int32, sal_Int32 > PDFWriterImpl::emitSystemFont( const PhysicalFo
             }
         }
     }
-    else if( pFont->mbSubsettable )
+    else if( pFont->CanSubset() )
     {
         aSubType = OString( "/TrueType" );
         Int32Vector aGlyphWidths;
@@ -8441,7 +8441,7 @@ bool PDFWriterImpl::registerGlyphs( int nGlyphs,
         const int nFontGlyphId = pGlyphs[i] & (GF_IDXMASK | GF_ISCHAR | GF_GSUB);
         const PhysicalFontFace* pCurrentFont = pFallbackFonts[i] ? pFallbackFonts[i] : pDevFont;
 
-        if( pCurrentFont->mbSubsettable )
+        if( pCurrentFont->CanSubset() )
         {
             FontSubset& rSubset = m_aSubsets[ pCurrentFont ];
             // search for font specific glyphID
@@ -8485,7 +8485,7 @@ bool PDFWriterImpl::registerGlyphs( int nGlyphs,
                                                           bVertical,
                                                           pGraphics );
         }
-        else if( pCurrentFont->IsEmbeddable() )
+        else if( pCurrentFont->CanEmbed() )
         {
             sal_Int32 nFontID = 0;
             FontEmbedData::iterator it = m_aEmbeddedFonts.find( pCurrentFont );

commit 0be1e670e58638d8a36863589c293a9807e26612

vcl: Regression from 08bcf4b3b
Very silly mistake - I put in an else, where it should have been an
entirely new if statement... this will have broken font mapping :(

Change-Id: I026b1f3808cb3a3793467fa3b2670bf9b56e3c5c

vcl/inc/outfont.hxx
@@ -88,7 +88,8 @@ public:
                         {
                             maMapNames += ";";
                         }
-                        else
+
+                        if (aMapName.getLength() == 0)
                         {
                             SAL_WARN("vcl.fonts", "New map name is empty");
                             return;