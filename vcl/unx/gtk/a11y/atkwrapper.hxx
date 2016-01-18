LemmingAvalanche/libreoffice-repaired

commit 9a23f2320e4b32c8d5fdd5896c3c7c1def8ecca8

loplugin:unusedfields
Change-Id: I321b594f1190cdd997b4769161e1c02d31f46213
Reviewed-on: https://gerrit.libreoffice.org/21566
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

vcl/unx/gtk/a11y/atkwrapper.hxx
@@ -45,8 +45,6 @@ namespace com { namespace sun { namespace star { namespace accessibility {
 
 struct _AtkObjectWrapper
 {
-    AtkObject aParent;
-
     css::uno::Reference<css::accessibility::XAccessible> mpAccessible;
     css::uno::Reference<css::accessibility::XAccessibleContext> mpContext;
     css::uno::Reference<css::accessibility::XAccessibleAction> mpAction;

avmedia/source/gstreamer/gstmanager.cxx
@@ -31,8 +31,7 @@ using namespace ::com::sun::star;
 
 namespace avmedia { namespace gstreamer {
 
-Manager::Manager( const uno::Reference< lang::XMultiServiceFactory >& rxMgr ) :
-    mxMgr( rxMgr )
+Manager::Manager()
 {
 }
 

avmedia/source/gstreamer/gstmanager.hxx
@@ -35,7 +35,7 @@ class Manager : public ::cppu::WeakImplHelper< css::media::XManager,
 {
 public:
 
-    explicit Manager( const css::uno::Reference< css::lang::XMultiServiceFactory >& rxMgr );
+    explicit Manager();
     virtual ~Manager();
 
     // XManager
@@ -45,9 +45,6 @@ public:
     virtual OUString SAL_CALL getImplementationName(  ) throw (css::uno::RuntimeException, std::exception) override;
     virtual sal_Bool SAL_CALL supportsService( const OUString& ServiceName ) throw (css::uno::RuntimeException, std::exception) override;
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw (css::uno::RuntimeException, std::exception) override;
-private:
-
-    css::uno::Reference< css::lang::XMultiServiceFactory > mxMgr;
 };
 
 } // namespace gstreamer

avmedia/source/gstreamer/gstuno.cxx
@@ -34,9 +34,9 @@ using namespace ::com::sun::star;
 #  define SERVICE_NAME AVMEDIA_MANAGER_SERVICE_NAME // "com.sun.star.comp.avmedia.Manager_GStreamer"
 #endif
 
-static uno::Reference< uno::XInterface > SAL_CALL create_MediaPlayer( const uno::Reference< lang::XMultiServiceFactory >& rxFact )
+static uno::Reference< uno::XInterface > SAL_CALL create_MediaPlayer( const uno::Reference< lang::XMultiServiceFactory >&  )
 {
-    return uno::Reference< uno::XInterface >( *new ::avmedia::gstreamer::Manager( rxFact ) );
+    return uno::Reference< uno::XInterface >( *new ::avmedia::gstreamer::Manager );
 }
 
 extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL avmediagst_component_getFactory( const sal_Char* pImplName, void* pServiceManager, void* /*pRegistryKey*/ )

include/sfx2/fcontnr.hxx
@@ -53,17 +53,6 @@ public:
     {   return static_cast<const SfxRefItem&>(rL).maRef == maRef; }
 };
 
-class SfxFrameWindow
-{
-    ScopedVclPtr<vcl::Window> pWindow;
-public:
-                SfxFrameWindow( vcl::Window *pWin )
-                 : pWindow( pWin )
-                {}
-
-    virtual     ~SfxFrameWindow()  {}
-};
-
 typedef sal_uIntPtr (*SfxDetectFilter)( SfxMedium& rMedium, const SfxFilter **, SfxFilterFlags nMust, SfxFilterFlags nDont );
 
 class SFX2_DLLPUBLIC SfxFilterContainer

l10ntools/inc/export.hxx
@@ -287,14 +287,13 @@ class MergeData
     friend class MergeDataHashMap;
 
 public:
-    OString sTyp;
     OString sGID;
     OString sLID;
     MergeEntrys* pMergeEntrys;
 private:
     MergeDataHashMap::iterator m_aNextData;
 public:
-    MergeData( const OString &rTyp, const OString &rGID, const OString &rLID );
+    MergeData( const OString &rGID, const OString &rLID );
     ~MergeData();
     MergeEntrys* GetMergeEntries() { return pMergeEntrys;}
 

l10ntools/source/merge.cxx
@@ -190,11 +190,9 @@ MergeDataHashMap::iterator MergeDataHashMap::find(const OString& rKey)
 // class MergeData
 
 
-MergeData::MergeData(
-    const OString &rTyp, const OString &rGID,
+MergeData::MergeData(const OString &rGID,
     const OString &rLID )
-    : sTyp( rTyp ),
-    sGID( rGID ),
+    : sGID( rGID ),
     sLID( rLID ) ,
     pMergeEntrys( new MergeEntrys() )
 {
@@ -403,7 +401,7 @@ void MergeDataFile::InsertEntry(
 
     if( !pData )
     {
-        pData = new MergeData( rTYP, rGID, rLID );
+        pData = new MergeData( rGID, rLID );
         aMap.insert( sKey, pData );
     }
 

scripting/source/stringresource/stringresource.hxx
@@ -98,7 +98,6 @@ class StringResourceImpl : public StringResourceImpl_BASE
 {
 protected:
     css::uno::Reference< css::uno::XComponentContext >        m_xContext;
-    css::uno::Reference< css::lang::XMultiComponentFactory >  m_xMCF;
 
     LocaleItem*                                               m_pCurrentLocaleItem;
     LocaleItem*                                               m_pDefaultLocaleItem;

commit 74bdbc92223c543c5b5d56cfb4994ae1d0f37ac8

fix recently broken gtk a11y
since

commit 9a23f2320e4b32c8d5fdd5896c3c7c1def8ecca8
Date:   Mon Jan 18 09:01:15 2016 +0200

    loplugin:unusedfields

structure is this way for gobject style inheritence

Change-Id: Ia069333aa5cfd913a2e8f85c967dabdcf4de6615

vcl/unx/gtk/a11y/atkwrapper.hxx
@@ -45,6 +45,8 @@ namespace com { namespace sun { namespace star { namespace accessibility {
 
 struct _AtkObjectWrapper
 {
+    AtkObject aParent;
+
     css::uno::Reference<css::accessibility::XAccessible> mpAccessible;
     css::uno::Reference<css::accessibility::XAccessibleContext> mpContext;
     css::uno::Reference<css::accessibility::XAccessibleAction> mpAction;