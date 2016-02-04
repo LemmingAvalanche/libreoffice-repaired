LemmingAvalanche/libreoffice-repaired

commit 0bae70680314cfd18baac7380cf19de74d895f09

tdf#92630 Enable auto-accelerator behaviour for gtk
Change-Id: I671177dd1f9e535c28a29bcbd6b74f1c789371ea
Reviewed-on: https://gerrit.libreoffice.org/16883
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/window/winproc.cxx
@@ -2165,17 +2165,22 @@ static void ImplHandleSalKeyMod( vcl::Window* pWindow, SalKeyModEvent* pEvent )
 
     // #105224# send commandevent to allow special treatment of Ctrl-LeftShift/Ctrl-RightShift etc.
 
-    // find window
-    vcl::Window* pChild = ImplGetKeyInputWindow( pWindow );
-    if ( !pChild )
-        return;
+    // find window - first look to see if the system window is available
+    vcl::Window* pChild = pWindow->ImplGetWindowImpl()->mpFirstChild;
 
-    // send modkey events only if useful data is available
-    if( pEvent->mnModKeyCode != 0 )
+    while ( pChild )
     {
-        CommandModKeyData data( pEvent->mnModKeyCode );
-        ImplCallCommand( pChild, CommandEventId::ModKeyChange, &data );
+        if ( pChild->ImplGetWindowImpl()->mbSysWin )
+            break;
+        pChild = pChild->ImplGetWindowImpl()->mpNext;
     }
+    //...if not, try to find a key input window...
+    if (!pChild) ImplGetKeyInputWindow( pWindow );
+    //...otherwise fail safe...
+    if (!pChild) pChild = pWindow;
+
+    CommandModKeyData data( pEvent->mnModKeyCode );
+    ImplCallCommand( pChild, CommandEventId::ModKeyChange, &data );
 }
 
 static void ImplHandleInputLanguageChange( vcl::Window* pWindow )

include/vcl/ctrl.hxx
@@ -43,6 +43,7 @@ private:
     bool                    mbHasControlFocus;
     bool                    mbFont;
     bool                    mbForeground;
+    bool                    mbShowAccelerator;
     Link<>                  maGetFocusHdl;
     Link<>                  maLoseFocusHdl;
 
@@ -184,6 +185,8 @@ public:
     OutputDevice*   GetReferenceDevice() const;
 
     vcl::Font       GetUnzoomedControlPointFont() const;
+    void            SetShowAccelerator (bool val);
+    bool            GetShowAccelerator (void) const;
 };
 
 #endif // INCLUDED_VCL_CTRL_HXX

include/vcl/dialog.hxx
@@ -107,6 +107,7 @@ public:
 private:
     bool            ImplStartExecuteModal();
     static void     ImplEndExecuteModal();
+    bool            ImplHandleCmdEvent ( const CommandEvent& rCEvent );
 public:
 
     // Dialog::Execute replacement API

include/vcl/layout.hxx
@@ -786,6 +786,7 @@ inline bool isContainerWindow(const vcl::Window &rWindow)
 {
     WindowType eType = rWindow.GetType();
     return eType == WINDOW_CONTAINER || eType == WINDOW_SCROLLWINDOW ||
+            eType == WINDOW_TABCONTROL || eType == WINDOW_TABPAGE ||
            (eType == WINDOW_DOCKINGWINDOW && ::isLayoutEnabled(&rWindow));
 }
 

include/vcl/menu.hxx
@@ -44,6 +44,7 @@ class HelpEvent;
 class Image;
 class PopupMenu;
 class KeyEvent;
+class CommandEvent;
 class MenuFloatingWindow;
 namespace vcl { class Window; }
 class SalMenu;
@@ -440,6 +441,7 @@ class VCL_DLLPUBLIC MenuBar : public Menu
                                                   MenuBar* pMenu, const css::uno::Reference<css::frame::XFrame> &rFrame);
     SAL_DLLPRIVATE static void ImplDestroy(MenuBar* pMenu, bool bDelete);
     SAL_DLLPRIVATE bool ImplHandleKeyEvent(const KeyEvent& rKEvent, bool bFromMenu = true);
+    SAL_DLLPRIVATE bool ImplHandleCmdEvent(const CommandEvent& rCEvent);
 
 protected:
 

include/vcl/outdev.hxx
@@ -160,30 +160,29 @@ typedef std::vector< Rectangle > MetricVector;
 // Flags for DrawText()
 enum class DrawTextFlags
 {
-    NONE                  = 0x0000,
-    Disable               = 0x0001,
-    Mnemonic              = 0x0002,
-    Mono                  = 0x0004,
-    Clip                  = 0x0008,
-    Left                  = 0x0010,
-    Center                = 0x0020,
-    Right                 = 0x0040,
-    Top                   = 0x0080,
-    VCenter               = 0x0100,
-    Bottom                = 0x0200,
-    EndEllipsis           = 0x0400,
-    PathEllipsis          = 0x0800,
-    MultiLine             = 0x1000,
-    WordBreak             = 0x2000,
-    NewsEllipsis          = 0x4000,
-    // in the long run we should make text style flags longer
-    // but at the moment we can get away with this 2 bit field for ellipsis style
-    CenterEllipsis        = EndEllipsis | PathEllipsis,
-    WordBreakHyphenation  = 0x8000 | WordBreak,
+    NONE                  = 0x00000000,
+    Disable               = 0x00000001,
+    Mnemonic              = 0x00000002,
+    Mono                  = 0x00000004,
+    Clip                  = 0x00000008,
+    Left                  = 0x00000010,
+    Center                = 0x00000020,
+    Right                 = 0x00000040,
+    Top                   = 0x00000080,
+    VCenter               = 0x00000100,
+    Bottom                = 0x00000200,
+    EndEllipsis           = 0x00000400,
+    PathEllipsis          = 0x00000800,
+    MultiLine             = 0x00001000,
+    WordBreak             = 0x00002000,
+    NewsEllipsis          = 0x00004000,
+    WordBreakHyphenation  = 0x00008000 | WordBreak,
+    CenterEllipsis        = 0x00010000,
+    HideMnemonic          = 0x00020000,
 };
 namespace o3tl
 {
-    template<> struct typed_flags<DrawTextFlags> : is_typed_flags<DrawTextFlags, 0xffff> {};
+    template<> struct typed_flags<DrawTextFlags> : is_typed_flags<DrawTextFlags, 0x3ffff> {};
 }
 
 // Flags for DrawImage(), these must match the definitions in css::awt::ImageDrawMode

vcl/inc/svdata.hxx
@@ -280,6 +280,8 @@ struct ImplSVNWFData
 
     /// entire drop down listbox resembles a button, no textarea/button parts (as currently on Windows)
     bool                    mbDDListBoxNoTextArea:1;
+    bool                    mbEnableAccel:1;                // whether or not accelerators are shown
+    bool                    mbAutoAccel:1;                  // whether accelerators are only shown when Alt is held down
 };
 
 struct BlendFrameCache

vcl/source/app/svdata.cxx
@@ -84,6 +84,8 @@ ImplSVData::ImplSVData()
     memset( this, 0, sizeof( ImplSVData ) );
     maHelpData.mbAutoHelpId = true;
     maNWFData.maMenuBarHighlightTextColor = Color( COL_TRANSPARENT );
+    maNWFData.mbEnableAccel = 1;
+    maNWFData.mbAutoAccel = 0;
 }
 
 ImplSVGDIData::~ImplSVGDIData()

commit 3af8f4fa79447916b3af14069bc649b667f41931

tdf#96739: Send Ctrl-Left/RightShift events to correct window
Another regression from commit 0bae70680314cfd1. SystemWindow should
consume only Alt pressed/released events and let key input window
handle the rest of modkey events

Change-Id: I304701fe63ee86bad534ba003ea05991d06f433d
Reviewed-on: https://gerrit.libreoffice.org/21854
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

vcl/source/window/winproc.cxx
@@ -2098,20 +2098,30 @@ static void ImplHandleSalKeyMod( vcl::Window* pWindow, SalKeyModEvent* pEvent )
     }
 
     // #105224# send commandevent to allow special treatment of Ctrl-LeftShift/Ctrl-RightShift etc.
+    // + auto-accelerator feature, tdf#92630
 
-    // find window - first look to see if the system window is available
-    vcl::Window* pChild = pWindow->ImplGetWindowImpl()->mpFirstChild;
+    vcl::Window *pChild = nullptr;
 
-    while ( pChild )
+    // Alt pressed or released => give SystemWindow a chance to handle auto-accelerator
+    if ( pEvent->mnCode == KEY_MOD2 || (pEvent->mnModKeyCode & MODKEY_MOD2) != 0 )
     {
-        if ( pChild->ImplGetWindowImpl()->mbSysWin )
-            break;
-        pChild = pChild->ImplGetWindowImpl()->mpNext;
+        // find window - first look to see if the system window is available
+        pChild = pWindow->ImplGetWindowImpl()->mpFirstChild;
+
+        while ( pChild )
+        {
+            if ( pChild->ImplGetWindowImpl()->mbSysWin )
+                break;
+            pChild = pChild->ImplGetWindowImpl()->mpNext;
+        }
     }
+
     //...if not, try to find a key input window...
-    if (!pChild) ImplGetKeyInputWindow( pWindow );
+    if (!pChild)
+        pChild = ImplGetKeyInputWindow( pWindow );
     //...otherwise fail safe...
-    if (!pChild) pChild = pWindow;
+    if (!pChild)
+        pChild = pWindow;
 
     CommandModKeyData data( pEvent->mnModKeyCode );
     ImplCallCommand( pChild, CommandEventId::ModKeyChange, &data );