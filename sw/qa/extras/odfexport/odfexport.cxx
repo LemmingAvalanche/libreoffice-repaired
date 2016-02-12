LibreOffice/core

commit 83777cd6e0f3f1a4458af896fd13344c696ecb1e

#i119492# Reset the "modified" flag for all OLE objects after loading.
Patch by: Clarence GUO
Review by: Andre Fischer

sfx2/source/doc/objstor.cxx
@@ -2351,7 +2351,29 @@ sal_Bool SfxObjectShell::InsertFrom( SfxMedium& rMedium )
         aArgs[nEnd-1].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( "InsertMode" ) );
         aArgs[nEnd-1].Value <<= (sal_Bool) sal_True;
 
-        return xLoader->filter( aArgs );
+//-> #i119492
+//        return xLoader->filter( aArgs );
+// During loading, some OLE objects like chart will be set modified flag, so needs to reset the flag to false after loading
+        sal_Bool bRtn = xLoader->filter( aArgs );
+        uno::Sequence < ::rtl::OUString > aNames = GetEmbeddedObjectContainer().GetObjectNames();
+        for ( sal_Int32 n = 0; n < aNames.getLength(); n++ )
+        {
+            ::rtl::OUString aName = aNames[n];
+            uno::Reference < embed::XEmbeddedObject > xObj = GetEmbeddedObjectContainer().GetEmbeddedObject( aName );
+            OSL_ENSURE( xObj.is(), "An empty entry in the embedded objects list!\n" );
+            if ( xObj.is() )
+            {
+                sal_Int32 nState = xObj->getCurrentState();
+                if ( nState == embed::EmbedStates::LOADED || nState == embed::EmbedStates::RUNNING )    // means that the object is not active
+                {
+                    uno::Reference< util::XModifiable > xModifiable( xObj->getComponent(), uno::UNO_QUERY );
+                    if ( xModifiable.is() )
+                        xModifiable->setModified(sal_False);
+                }
+            }
+        }
+        return bRtn;
+//<- #i119492
         }catch(const uno::Exception&)
         {}
     }

LemmingAvalanche/libreoffice-repaired

commit 6d2c2836c279f7eb30116d8205c424c76c487085

sfx2: related tdf#56270: loss of embedded objects imported from DOCX
After the import some of these are kept in RUNNING state.  For Math
objects imported from MathType3 OLEs in particular, first a new
Math object is created and stored to the XStorage, only then is the
MathType3 stream imported.  This means the Math object is modified and
contains data that must be stored.

The problem is then that SfxObjectShell::ImportFrom() simply calls
setModified(false), clearing the flag without storing the object.

For Flat ODF export we lose all the objects that are cached in sw's
SwOLELRUCache; for the bugdoc something more inexplicable happens for
ODT export where we lose "Object 214" (which is the first one in the
cache) but no other ones.

(The main difference is that for ODF there is an optimization to copy the
embedded object's storage without loading the object, but for Flat ODF
every object must be loaded and exported.)

(regression from 83777cd6e0f3f1a4458af896fd13344c696ecb1e)

Change-Id: Id1474fba9f4da2d5247c7ff4dc6819ddb9829fe8

sfx2/source/doc/objstor.cxx
@@ -2275,8 +2275,19 @@ bool SfxObjectShell::ImportFrom(SfxMedium& rMedium,
                 if ( nState == embed::EmbedStates::LOADED || nState == embed::EmbedStates::RUNNING )    // means that the object is not active
                 {
                     uno::Reference< util::XModifiable > xModifiable( xObj->getComponent(), uno::UNO_QUERY );
-                    if ( xModifiable.is() )
+                    if (xModifiable.is() && xModifiable->isModified())
+                    {
+                        uno::Reference<embed::XEmbedPersist> const xPers(xObj, uno::UNO_QUERY);
+                        if (xPers.is())
+                        {   // store it before resetting modified!
+                            xPers->storeOwn();
+                        }
+                        else
+                        {
+                            SAL_WARN("sfx.doc", "Modified object without persistence!");
+                        }
                         xModifiable->setModified(sal_False);
+                    }
                 }
             }
         }

sw/CppunitTest_sw_odfexport.mk
@@ -87,6 +87,7 @@ $(eval $(call gb_CppunitTest_use_components,sw_odfexport,\
     sd/util/sd \
     svx/util/svxcore \
     xmloff/source/transform/xof \
+    xmlscript/util/xmlscript \
 ))
 
 $(eval $(call gb_CppunitTest_use_custom_headers,sw_odfexport,\

sw/qa/extras/odfexport/odfexport.cxx
@@ -73,9 +73,39 @@ public:
             pBatch->commit();
             return pResetter;
         }
+        if (OString(pFilename) == "2_MathType3.docx")
+        {
+            std::unique_ptr<Resetter> pResetter(new Resetter(
+                [this] () {
+                    mpFilter = "writer8";
+                    std::shared_ptr<comphelper::ConfigurationChanges> pBatch(
+                            comphelper::ConfigurationChanges::create());
+                    officecfg::Office::Common::Cache::Writer::OLE_Objects::set(20, pBatch);
+                    return pBatch->commit();
+                }));
+            mpFilter = "OpenDocument Text Flat XML"; // doesn't happen with ODF package
+            std::shared_ptr<comphelper::ConfigurationChanges> pBatch(
+                    comphelper::ConfigurationChanges::create());
+            officecfg::Office::Common::Cache::Writer::OLE_Objects::set(1, pBatch);
+            pBatch->commit();
+            return pResetter;
+        }
         return nullptr;
     }
 };
+
+DECLARE_ODFEXPORT_TEST(testMathObjectFlatExport, "2_MathType3.docx")
+{
+    uno::Reference<util::XModifiable> xModifiable(mxComponent, uno::UNO_QUERY);
+    CPPUNIT_ASSERT(!xModifiable->isModified());
+    // see preTest(), set the OLE cache to 1 for this test
+    // and the problem was that the formulas that were in the cache
+    // (the second one) were lost
+    OUString formula1(getFormula(getRun(getParagraph(1), 1)));
+    CPPUNIT_ASSERT_EQUAL(OUString(" size 12{1+1=2} {}"), formula1);
+    OUString formula2(getFormula(getRun(getParagraph(2), 1)));
+    CPPUNIT_ASSERT_EQUAL(OUString(" size 12{2+2=4} {}"), formula2);
+}
 
 DECLARE_ODFEXPORT_TEST(testFramebackgrounds, "framebackgrounds.odt")
 {