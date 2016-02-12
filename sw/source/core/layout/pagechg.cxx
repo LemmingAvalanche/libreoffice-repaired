LemmingAvalanche/libreoffice-repaired

commit f84f491245a86438c1a44fd2e8509ad204f256aa

Misc tweaks and cleanups.
Streamlined a couple of hotspots and some
minor cleanups.

Change-Id: I85a9423e03f7e20ce78736a44d668da859a443cd
Reviewed-on: https://gerrit.libreoffice.org/19720
Reviewed-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>
Tested-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>

sw/source/core/layout/pagechg.cxx
@@ -1754,44 +1754,41 @@ static void lcl_MoveAllLowers( SwFrm* pFrm, const Point& rOffset );
 
 static void lcl_MoveAllLowerObjs( SwFrm* pFrm, const Point& rOffset )
 {
-    SwSortedObjs* pSortedObj = 0;
     const bool bPage = pFrm->IsPageFrm();
+    const SwSortedObjs* pSortedObj = bPage
+                        ? static_cast<SwPageFrm*>(pFrm)->GetSortedObjs()
+                        : pFrm->GetDrawObjs();
+    if (pSortedObj == nullptr)
+        return;
 
-    if ( bPage )
-        pSortedObj = static_cast<SwPageFrm*>(pFrm)->GetSortedObjs();
-    else
-        pSortedObj = pFrm->GetDrawObjs();
-
-    for ( size_t i = 0; pSortedObj && i < pSortedObj->size(); ++i)
+    for (SwAnchoredObject* pAnchoredObj : *pSortedObj)
     {
-        SwAnchoredObject* pAnchoredObj = (*pSortedObj)[i];
-
         const SwFrameFormat& rObjFormat = pAnchoredObj->GetFrameFormat();
         const SwFormatAnchor& rAnchor = rObjFormat.GetAnchor();
 
         // all except from the as character anchored objects are moved
         // when processing the page frame:
-        const bool bAsChar = (rAnchor.GetAnchorId() == FLY_AS_CHAR);
-        if ( !bPage && !bAsChar )
+        if ( !bPage && (rAnchor.GetAnchorId() != FLY_AS_CHAR) )
             continue;
 
         SwObjPositioningInProgress aPosInProgress( *pAnchoredObj );
 
-        if ( dynamic_cast< const SwFlyFrm *>( pAnchoredObj ) !=  nullptr )
+        if ( dynamic_cast< const SwFlyFrm *>( pAnchoredObj ) != nullptr )
         {
             SwFlyFrm* pFlyFrm( static_cast<SwFlyFrm*>(pAnchoredObj) );
             lcl_MoveAllLowers( pFlyFrm, rOffset );
             pFlyFrm->NotifyDrawObj();
             // --> let the active embedded object be moved
-            if ( pFlyFrm->Lower() )
+            SwFrm* pLower = pFlyFrm->Lower();
+            if ( pLower )
             {
-                if ( pFlyFrm->Lower()->IsNoTextFrm() )
+                if ( pLower->IsNoTextFrm() )
                 {
-                    SwContentFrm* pContentFrm = static_cast<SwContentFrm*>(pFlyFrm->Lower());
-                    SwRootFrm* pRoot = pFlyFrm->Lower()->getRootFrm();
-                    SwViewShell *pSh = pRoot ? pRoot->GetCurrShell() : 0;
+                    SwRootFrm* pRoot = pLower->getRootFrm();
+                    SwViewShell *pSh = pRoot ? pRoot->GetCurrShell() : nullptr;
                     if ( pSh )
                     {
+                        SwContentFrm* pContentFrm = static_cast<SwContentFrm*>(pLower);
                         SwOLENode* pNode = pContentFrm->GetNode()->GetOLENode();
                         if ( pNode )
                         {
@@ -1815,11 +1812,10 @@ static void lcl_MoveAllLowerObjs( SwFrm* pFrm, const Point& rOffset )
             SwAnchoredDrawObject* pAnchoredDrawObj( static_cast<SwAnchoredDrawObject*>(pAnchoredObj) );
 
             // don't touch objects that are not yet positioned:
-            const bool bNotYetPositioned = pAnchoredDrawObj->NotYetPositioned();
-            if ( bNotYetPositioned )
+            if ( pAnchoredDrawObj->NotYetPositioned() )
                 continue;
 
-            const Point aCurrAnchorPos = pAnchoredDrawObj->GetDrawObj()->GetAnchorPos();
+            const Point& aCurrAnchorPos = pAnchoredDrawObj->GetDrawObj()->GetAnchorPos();
             const Point aNewAnchorPos( ( aCurrAnchorPos + rOffset ) );
             pAnchoredDrawObj->DrawObj()->SetAnchorPos( aNewAnchorPos );
             pAnchoredDrawObj->SetLastObjRect( pAnchoredDrawObj->GetObjRect().SVRect() );
@@ -1874,12 +1870,13 @@ static void lcl_MoveAllLowers( SwFrm* pFrm, const Point& rOffset )
 // Calculate how the pages have to be positioned
 void SwRootFrm::CheckViewLayout( const SwViewOption* pViewOpt, const SwRect* pVisArea )
 {
-    vcl::RenderContext* pRenderContext = GetCurrShell() ? GetCurrShell()->GetOut() : 0;
+    SwViewShell* pSh = GetCurrShell();
+    vcl::RenderContext* pRenderContext = pSh ? pSh->GetOut() : nullptr;
     // #i91432#
     // No calculation of page positions, if only an empty page is present.
     // This situation occurs when <SwRootFrm> instance is in construction
     // and the document contains only left pages.
-    if ( Lower()->GetNext() == 0 &&
+    if ( Lower()->GetNext() == nullptr &&
          static_cast<SwPageFrm*>(Lower())->IsEmptyPage() )
     {
         return;
@@ -1922,7 +1919,6 @@ void SwRootFrm::CheckViewLayout( const SwViewOption* pViewOpt, const SwRect* pVi
 
     const long nBorder = Frm().Pos().getX();
     const long nVisWidth = mnViewWidth - 2 * nBorder;
-    SwViewShell* pSh = GetCurrShell();
     const long nGapBetweenPages = pViewOpt ? pViewOpt->GetGapBetweenPages()
                                            : (pSh ? pSh->GetViewOptions()->GetGapBetweenPages()
                                                   : SwViewOption::GetDefGapBetweenPages());
@@ -1950,7 +1946,7 @@ void SwRootFrm::CheckViewLayout( const SwViewOption* pViewOpt, const SwRect* pVi
 
     bool bPageChanged = false;
     const bool bRTL = !IsLeftToRightViewLayout();
-    const SwTwips nSidebarWidth = SwPageFrm::GetSidebarBorderWidth( GetCurrShell() );
+    const SwTwips nSidebarWidth = SwPageFrm::GetSidebarBorderWidth( pSh );
 
     while ( pPageFrm )
     {

sw/inc/anchoredobject.hxx
@@ -214,12 +214,12 @@ class SW_DLLPUBLIC SwAnchoredObject
 
         // accessors to member <mpDrawObj>
         void SetDrawObj( SdrObject& _rDrawObj );
-        const SdrObject* GetDrawObj() const { return mpDrawObj;}
-        SdrObject* DrawObj() { return mpDrawObj;}
+        const SdrObject* GetDrawObj() const { return mpDrawObj; }
+        SdrObject* DrawObj() { return mpDrawObj; }
 
         // accessors to member <mpAnchorFrm>
-        const SwFrm* GetAnchorFrm() const { return mpAnchorFrm;}
-        SwFrm* AnchorFrm() { return mpAnchorFrm;}
+        const SwFrm* GetAnchorFrm() const { return mpAnchorFrm; }
+        SwFrm* AnchorFrm() { return mpAnchorFrm; }
         void ChgAnchorFrm( SwFrm* _pNewAnchorFrm );
         /** determine anchor frame containing the anchor position
 
@@ -232,8 +232,8 @@ class SW_DLLPUBLIC SwAnchoredObject
         */
         SwFrm* GetAnchorFrmContainingAnchPos();
 
-        SwPageFrm* GetPageFrm() { return mpPageFrm;}
-        const SwPageFrm* GetPageFrm() const { return mpPageFrm;}
+        SwPageFrm* GetPageFrm() { return mpPageFrm; }
+        const SwPageFrm* GetPageFrm() const { return mpPageFrm; }
         void SetPageFrm( SwPageFrm* _pNewPageFrm );
 
         /** method to determine the page frame, on which the 'anchor' of

sw/inc/crstate.hxx
@@ -161,9 +161,9 @@ struct SwCrsrMoveState
     int m_nInNumPostionOffset;        ///< distance from number portion's start
 
     SwCrsrMoveState( CrsrMoveState eSt = MV_NONE ) :
-        m_pFill( NULL ),
-        m_p2Lines( NULL ),
-        m_pSpecialPos( NULL ),
+        m_pFill( nullptr ),
+        m_p2Lines( nullptr ),
+        m_pSpecialPos( nullptr ),
         m_eState( eSt ),
         m_nCursorBidiLevel( 0 ),
         m_bStop( false ),
@@ -185,8 +185,8 @@ struct SwCrsrMoveState
     {}
     SwCrsrMoveState( SwFillCrsrPos *pInitFill ) :
         m_pFill( pInitFill ),
-        m_p2Lines( NULL ),
-        m_pSpecialPos( NULL ),
+        m_p2Lines( nullptr ),
+        m_pSpecialPos( nullptr ),
         m_eState( MV_SETONLYTEXT ),
         m_nCursorBidiLevel( 0 ),
         m_bStop( false ),

sw/inc/dbfld.hxx
@@ -109,7 +109,7 @@ public:
     virtual bool        PutValue( const css::uno::Any& rVal, sal_uInt16 nWhich ) override;
 
     static bool FormatValue( SvNumberFormatter* pDocFormatter, OUString &aString, sal_uInt32 nFormat,
-                             double &aNumber, sal_Int32 nColumnType, SwDBField *pField = NULL );
+                             double &aNumber, sal_Int32 nColumnType, SwDBField *pField = nullptr );
 };
 
 inline  void SwDBField::SetExpansion(const OUString& rStr)

sw/inc/dbmgr.hxx
@@ -250,7 +250,7 @@ public:
     inline void     SetMergeSilent( bool bVal )     { bMergeSilent = bVal; }
 
     /// Merging of data records into fields.
-    bool            MergeNew( const SwMergeDescriptor& rMergeDesc, vcl::Window* pParent = NULL );
+    bool            MergeNew( const SwMergeDescriptor& rMergeDesc, vcl::Window* pParent = nullptr );
     static bool     Merge(SwWrtShell* pSh);
     void            MergeCancel();
 
@@ -335,7 +335,7 @@ public:
     static OUString GetDBField(
                     css::uno::Reference< css::beans::XPropertySet > xColumnProp,
                     const SwDBFormatData& rDBFormatData,
-                    double *pNumber = NULL);
+                    double *pNumber = nullptr);
 
     static css::uno::Reference< css::sdbc::XConnection>
             GetConnection(const OUString& rDataSource,

sw/inc/doc.hxx
@@ -920,9 +920,9 @@ public:
     size_t GetPageDescCnt() const { return m_PageDescs.size(); }
     const SwPageDesc& GetPageDesc(const size_t i) const { return *m_PageDescs[i]; }
     SwPageDesc& GetPageDesc(size_t const i) { return *m_PageDescs[i]; }
-    SwPageDesc* FindPageDesc(const OUString& rName, size_t* pPos = NULL) const;
+    SwPageDesc* FindPageDesc(const OUString& rName, size_t* pPos = nullptr) const;
     // Just searches the pointer in the m_PageDescs vector!
-    bool        ContainsPageDesc(const SwPageDesc *pDesc, size_t* pPos = NULL);
+    bool        ContainsPageDesc(const SwPageDesc *pDesc, size_t* pPos = nullptr);
 
     /** Copy the complete PageDesc - beyond document and "deep"!
      Optionally copying of PoolFormatId, -HlpId can be prevented. */

sw/inc/editsh.hxx
@@ -234,7 +234,7 @@ public:
     bool GetCurAttr( SfxItemSet& ,
                      const bool bMergeIndentValuesOfNumRule = false ) const;
     void SetAttrItem( const SfxPoolItem&, SetAttrMode nFlags = SetAttrMode::DEFAULT );
-    void SetAttrSet( const SfxItemSet&, SetAttrMode nFlags = SetAttrMode::DEFAULT, SwPaM* pCrsr = NULL );
+    void SetAttrSet( const SfxItemSet&, SetAttrMode nFlags = SetAttrMode::DEFAULT, SwPaM* pCrsr = nullptr );
 
     /** Get RES_CHRATR_* items of one type in the current selection.
      * @param nWhich WhichId of the collected items.
@@ -276,7 +276,7 @@ public:
     /// Query default attribute of document.
     const SfxPoolItem& GetDefault( sal_uInt16 nFormatHint ) const;
 
-    void ResetAttr( const std::set<sal_uInt16> &attrs = std::set<sal_uInt16>(), SwPaM* pCrsr = NULL );
+    void ResetAttr( const std::set<sal_uInt16> &attrs = std::set<sal_uInt16>(), SwPaM* pCrsr = nullptr );
     void GCAttr();
 
     /// @return the scripttpye of the selection.
@@ -525,11 +525,11 @@ public:
     void ChgNumRuleFormats( const SwNumRule& rRule );
 
     /// Set (and query if) a numbering with StartFlag starts at current PointPos.
-    void SetNumRuleStart( bool bFlag = true, SwPaM* pCrsr = NULL );
-    bool IsNumRuleStart( SwPaM* pPaM = NULL ) const;
-    void SetNodeNumStart( sal_uInt16 nStt, SwPaM* = NULL );
+    void SetNumRuleStart( bool bFlag = true, SwPaM* pCrsr = nullptr );
+    bool IsNumRuleStart( SwPaM* pPaM = nullptr ) const;
+    void SetNodeNumStart( sal_uInt16 nStt, SwPaM* = nullptr );
 
-    sal_uInt16 GetNodeNumStart( SwPaM* pPaM = NULL ) const;
+    sal_uInt16 GetNodeNumStart( SwPaM* pPaM = nullptr ) const;
 
     bool ReplaceNumRule( const OUString& rOldRule, const OUString& rNewRule );
 
@@ -793,7 +793,7 @@ public:
     void GetINetAttrs( SwGetINetAttrs& rArr );
 
     OUString GetDropText( const sal_Int32 nChars ) const;
-    void   ReplaceDropText( const OUString &rStr, SwPaM* pPaM = NULL );
+    void   ReplaceDropText( const OUString &rStr, SwPaM* pPaM = nullptr );
 
     /** May an outline be moved or copied?
      Check whether it's in text body, not in table, and not read-only (move). */

sw/inc/fesh.hxx
@@ -330,11 +330,11 @@ public:
                         const RndStdIds _nAnchorId,
                         const sal_Int16 _eHoriRelOrient = css::text::RelOrientation::FRAME,
                         const sal_Int16 _eVertRelOrient = css::text::RelOrientation::FRAME,
-                        const SwPosition* _pToCharContentPos = NULL,
+                        const SwPosition* _pToCharContentPos = nullptr,
                         const bool _bFollowTextFlow = false,
                         bool _bMirror = false,
-                        Point* _opRef = NULL,
-                        Size* _opPercent = NULL,
+                        Point* _opRef = nullptr,
+                        Size* _opPercent = nullptr,
                         const SwFormatFrmSize* pFormatFrmSize = 0 ) const;
 
     /// Set size of draw objects.
@@ -492,7 +492,7 @@ public:
     bool GetObjAttr( SfxItemSet &rSet ) const;
     bool SetObjAttr( const SfxItemSet &rSet );
 
-    const SdrObject* GetBestObject( bool bNext, GotoObjFlags eType = GotoObjFlags::DrawAny, bool bFlat = true, const svx::ISdrObjectFilter* pFilter = NULL );
+    const SdrObject* GetBestObject( bool bNext, GotoObjFlags eType = GotoObjFlags::DrawAny, bool bFlat = true, const svx::ISdrObjectFilter* pFilter = nullptr );
     bool GotoObj( bool bNext, GotoObjFlags eType = GotoObjFlags::DrawAny);
 
     /// Set DragMode (e.g. Rotate), but do nothing when frame is selected.
@@ -664,7 +664,7 @@ public:
 
     void SetTabBorders( const SfxItemSet& rSet );
     void GetTabBorders(       SfxItemSet& rSet) const;
-    void SetTabLineStyle(const Color* pColor, bool bSetLine = false, const editeng::SvxBorderLine* pBorderLine = NULL);
+    void SetTabLineStyle(const Color* pColor, bool bSetLine = false, const editeng::SvxBorderLine* pBorderLine = nullptr);
 
     void SetTabBackground( const SvxBrushItem &rNew );
     void GetTabBackground( SvxBrushItem &rToFill ) const;

commit 6d0246f8be0167fc2fad3813bbc3f13d35aaab72

sw: fix a STL assertion in lcl_MoveAllLowerObjs()
For OLE objects MoveObjectIfActive() ends up calling
SwSortedObjs::Update() which removes and re-inserts objects,
thus invalidating the pSortedObj iterator.

(regression from f84f491245a86438c1a44fd2e8509ad204f256aa)

Change-Id: I2628f1b4ecd1c20ebbc4e9dda4e4befc9fad4644

sw/source/core/layout/pagechg.cxx
@@ -1761,8 +1761,11 @@ static void lcl_MoveAllLowerObjs( SwFrame* pFrame, const Point& rOffset )
     if (pSortedObj == nullptr)
         return;
 
-    for (SwAnchoredObject* pAnchoredObj : *pSortedObj)
+    // note: pSortedObj elements may be removed and inserted from
+    // MoveObjectIfActive(), invalidating iterators
+    for (size_t i = 0; i < pSortedObj->size(); ++i)
     {
+        SwAnchoredObject *const pAnchoredObj = (*pSortedObj)[i];
         const SwFrameFormat& rObjFormat = pAnchoredObj->GetFrameFormat();
         const SwFormatAnchor& rAnchor = rObjFormat.GetAnchor();
 