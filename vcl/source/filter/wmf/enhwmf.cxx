LemmingAvalanche/libreoffice-repaired

commit 31a933e4f6a81aa2c3f9125f7fb01fcac3b638c1

EMF/WMF: fix rendering of pen styles (dash, dot, dashdot, dashdotdot)
Change-Id: I226bac370601b75f2589f7a7c5e8830746b31e2e

vcl/source/filter/wmf/enhwmf.cxx
@@ -730,19 +730,37 @@ sal_Bool EnhWMFReader::ReadEnhWMF()
                             aLineInfo.SetWidth( aSize.Width() );
 
                         sal_Bool bTransparent = sal_False;
-                        sal_uInt16 nDashCount = 0;
-                        sal_uInt16 nDotCount = 0;
                         switch( nStyle & 0xFF )
                         {
                             case PS_DASHDOTDOT :
-                                nDotCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 1 );
+                                aLineInfo.SetDotCount( 2 );
+                                aLineInfo.SetDashLen( 150 );
+                                aLineInfo.SetDotLen( 30 );
+                                aLineInfo.SetDistance( 50 );
+                            break;
                             case PS_DASHDOT :
-                                nDashCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 1 );
+                                aLineInfo.SetDotCount( 1 );
+                                aLineInfo.SetDashLen( 150 );
+                                aLineInfo.SetDotLen( 30 );
+                                aLineInfo.SetDistance( 90 );
+                            break;
                             case PS_DOT :
-                                nDotCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 0 );
+                                aLineInfo.SetDotCount( 1 );
+                                aLineInfo.SetDotLen( 30 );
+                                aLineInfo.SetDistance( 50 );
                             break;
                             case PS_DASH :
-                                nDashCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 1 );
+                                aLineInfo.SetDotCount( 0 );
+                                aLineInfo.SetDashLen( 225 );
+                                aLineInfo.SetDistance( 100 );
                             break;
                             case PS_NULL :
                                 bTransparent = sal_True;
@@ -778,12 +796,6 @@ sal_Bool EnhWMFReader::ReadEnhWMF()
                             break;
                             default :
                                 aLineInfo.SetLineJoin ( basegfx::B2DLINEJOIN_NONE );
-                        }
-                        if ( nDashCount | nDotCount )
-                        {
-                            aLineInfo.SetStyle( LINE_DASH );
-                            aLineInfo.SetDashCount( nDashCount );
-                            aLineInfo.SetDotCount( nDotCount );
                         }
                         pOut->CreateObject( nIndex, GDI_PEN, new WinMtfLineStyle( ReadColor(), aLineInfo, bTransparent ) );
                     }

vcl/source/filter/wmf/winmtf.cxx
@@ -713,14 +713,6 @@ void WinMtfOutput::CreateObject( GDIObjectType eType, void* pStyle )
         {
             Size aSize( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetWidth(), 0 );
             ((WinMtfLineStyle*)pStyle)->aLineInfo.SetWidth( ImplMap( aSize ).Width() );
-            if ( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetStyle() == LINE_DASH )
-            {
-                aSize.Width() += 1;
-                long nDotLen = ImplMap( aSize ).Width();
-                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDistance( nDotLen );
-                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDotLen( nDotLen );
-                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDashLen( nDotLen * 4 );
-            }
         }
     }
     sal_uInt32 nIndex;
@@ -749,14 +741,6 @@ void WinMtfOutput::CreateObject( sal_Int32 nIndex, GDIObjectType eType, void* pS
             {
                 Size aSize( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetWidth(), 0 );
                 ((WinMtfLineStyle*)pStyle)->aLineInfo.SetWidth( ImplMap( aSize ).Width() );
-                if ( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetStyle() == LINE_DASH )
-                {
-                    aSize.Width() += 1;
-                    long nDotLen = ImplMap( aSize ).Width();
-                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDistance( nDotLen );
-                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDotLen( nDotLen );
-                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDashLen( nDotLen * 4 );
-                }
             }
         }
         if ( (sal_uInt32)nIndex >= vGDIObj.size() )

vcl/source/filter/wmf/winwmf.cxx
@@ -749,19 +749,37 @@ void WMFReader::ReadRecordParams( sal_uInt16 nFunc )
                 aLineInfo.SetWidth( nWidth );
 
             sal_Bool bTransparent = sal_False;
-            sal_uInt16 nDashCount = 0;
-            sal_uInt16 nDotCount = 0;
             switch( nStyle & 0xFF )
             {
                 case PS_DASHDOTDOT :
-                    nDotCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 1 );
+                    aLineInfo.SetDotCount( 2 );
+                    aLineInfo.SetDashLen( 150 );
+                    aLineInfo.SetDotLen( 30 );
+                    aLineInfo.SetDistance( 50 );
+                break;
                 case PS_DASHDOT :
-                    nDashCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 1 );
+                    aLineInfo.SetDotCount( 1 );
+                    aLineInfo.SetDashLen( 150 );
+                    aLineInfo.SetDotLen( 30 );
+                    aLineInfo.SetDistance( 90 );
+                break;
                 case PS_DOT :
-                    nDotCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 0 );
+                    aLineInfo.SetDotCount( 1 );
+                    aLineInfo.SetDotLen( 30 );
+                    aLineInfo.SetDistance( 50 );
                 break;
                 case PS_DASH :
-                    nDashCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 1 );
+                    aLineInfo.SetDotCount( 0 );
+                    aLineInfo.SetDashLen( 225 );
+                    aLineInfo.SetDistance( 100 );
                 break;
                 case PS_NULL :
                     bTransparent = sal_True;
@@ -797,12 +815,6 @@ void WMFReader::ReadRecordParams( sal_uInt16 nFunc )
                 break;
                 default :
                     aLineInfo.SetLineJoin ( basegfx::B2DLINEJOIN_NONE );
-            }
-            if ( nDashCount | nDotCount )
-            {
-                aLineInfo.SetStyle( LINE_DASH );
-                aLineInfo.SetDashCount( nDashCount );
-                aLineInfo.SetDotCount( nDotCount );
             }
             pOut->CreateObject( GDI_PEN, new WinMtfLineStyle( ReadColor(), aLineInfo, bTransparent ) );
         }

commit 29c8f5c70b7be29775ffac226050c7f1ac017706

tdf#79679 vcl: dashed lines show as solid lines when importing EMF files
Issue is a regression in commit 31a933e4f6a81aa2c3f9125f7fb01fcac3b638c1.
(EMF/WMF: fix rendering of pen styles (dash, dot, dashdot, dashdotdot).

I've looked at how the latest version of Word on the Mac works, and it
turns out that the spacings for the PenStyle enumerations in the LogPen
objects for all the create pen EMF records are as follows:

* PS_DOT           - ■ □ ■ □ ■ □ ■ □ ■ □ ■
* PS_DASHDOT       - ■ ■ ■ □ ■ □ ■ ■ ■ □ ■
* PS_DASHDOTDOT    - ■ ■ ■ □ ■ □ ■ □ ■ ■ ■

(where ■ is the actual filled in area, and □ is the space between the
filled in areas)

In other words, each dash fills in the space of three dots, and there
is the one dot worth of empty space between the dashes and dots. Each
"dot" has a width and height equal to the width specified in the pen.

So basically, we seem to be arbitrarily setting the dot, dash and
distance lengths arbitrarily, which were reasonable guesses but tended
to produce very odd lines at different zoom levels.

Change-Id: Ie8b5fa396e4fb0f480cb3594c8129a59f472c1b8
Reviewed-on: https://gerrit.libreoffice.org/22886
Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
Tested-by: Chris Sherlock <chris.sherlock79@gmail.com>

vcl/source/filter/wmf/enhwmf.cxx
@@ -17,10 +17,12 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include "winmtf.hxx"
 #include <osl/endian.h>
 #include <basegfx/matrix/b2dhommatrix.hxx>
 #include <vcl/dibtools.hxx>
+
+#include "winmtf.hxx"
+
 #include <memory>
 
 #ifdef DBG_UTIL
@@ -912,37 +914,27 @@ bool EnhWMFReader::ReadEnhWMF()
                             aLineInfo.SetWidth( aSize.Width() );
 
                         bool bTransparent = false;
-                        switch( nStyle & 0xFF )
+                        switch( nStyle & PS_STYLE_MASK )
                         {
                             case PS_DASHDOTDOT :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 1 );
                                 aLineInfo.SetDotCount( 2 );
-                                aLineInfo.SetDashLen( 150 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 50 );
                             break;
                             case PS_DASHDOT :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 1 );
                                 aLineInfo.SetDotCount( 1 );
-                                aLineInfo.SetDashLen( 150 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 90 );
                             break;
                             case PS_DOT :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 0 );
                                 aLineInfo.SetDotCount( 1 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 50 );
                             break;
                             case PS_DASH :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 1 );
                                 aLineInfo.SetDotCount( 0 );
-                                aLineInfo.SetDashLen( 225 );
-                                aLineInfo.SetDistance( 100 );
                             break;
                             case PS_NULL :
                                 bTransparent = true;
@@ -1015,31 +1007,21 @@ bool EnhWMFReader::ReadEnhWMF()
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 1 );
                                 aLineInfo.SetDotCount( 2 );
-                                aLineInfo.SetDashLen( 150 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 50 );
                             break;
                             case PS_DASHDOT :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 1 );
                                 aLineInfo.SetDotCount( 1 );
-                                aLineInfo.SetDashLen( 150 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 90 );
                             break;
                             case PS_DOT :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 0 );
                                 aLineInfo.SetDotCount( 1 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 50 );
                             break;
                             case PS_DASH :
                                 aLineInfo.SetStyle( LINE_DASH );
                                 aLineInfo.SetDashCount( 1 );
                                 aLineInfo.SetDotCount( 0 );
-                                aLineInfo.SetDashLen( 225 );
-                                aLineInfo.SetDistance( 100 );
                             break;
                             case PS_NULL :
                                 bTransparent = true;

vcl/source/filter/wmf/winmtf.cxx
@@ -729,8 +729,16 @@ void WinMtfOutput::CreateObject( sal_Int32 nIndex, GDIObjectType eType, void* pS
             {
                 WinMtfLineStyle* pLineStyle = static_cast<WinMtfLineStyle*>(pStyle);
                 Size aSize(pLineStyle->aLineInfo.GetWidth(), 0);
-                aSize = ImplMap(aSize);
-                pLineStyle->aLineInfo.SetWidth(aSize.Width());
+                pLineStyle->aLineInfo.SetWidth( ImplMap(aSize).Width() );
+
+                if ( pLineStyle->aLineInfo.GetStyle() == LINE_DASH )
+                {
+                    aSize.Width() += 1;
+                    long nDotLen = ImplMap( aSize ).Width();
+                    pLineStyle->aLineInfo.SetDistance( nDotLen );
+                    pLineStyle->aLineInfo.SetDotLen( nDotLen );
+                    pLineStyle->aLineInfo.SetDashLen( nDotLen * 3 );
+                }
             }
         }
         if ( (sal_uInt32)nIndex >= vGDIObj.size() )

vcl/source/filter/wmf/winwmf.cxx
@@ -811,37 +811,28 @@ void WMFReader::ReadRecordParams( sal_uInt16 nFunc )
                 aLineInfo.SetWidth(nWidth);
 
             bool bTransparent = false;
+
             switch( nStyle & 0xFF )
             {
                 case PS_DASHDOTDOT :
                     aLineInfo.SetStyle( LINE_DASH );
                     aLineInfo.SetDashCount( 1 );
                     aLineInfo.SetDotCount( 2 );
-                    aLineInfo.SetDashLen( 150 );
-                    aLineInfo.SetDotLen( 30 );
-                    aLineInfo.SetDistance( 50 );
                 break;
                 case PS_DASHDOT :
                     aLineInfo.SetStyle( LINE_DASH );
                     aLineInfo.SetDashCount( 1 );
                     aLineInfo.SetDotCount( 1 );
-                    aLineInfo.SetDashLen( 150 );
-                    aLineInfo.SetDotLen( 30 );
-                    aLineInfo.SetDistance( 90 );
                 break;
                 case PS_DOT :
                     aLineInfo.SetStyle( LINE_DASH );
                     aLineInfo.SetDashCount( 0 );
                     aLineInfo.SetDotCount( 1 );
-                    aLineInfo.SetDotLen( 30 );
-                    aLineInfo.SetDistance( 50 );
                 break;
                 case PS_DASH :
                     aLineInfo.SetStyle( LINE_DASH );
                     aLineInfo.SetDashCount( 1 );
                     aLineInfo.SetDotCount( 0 );
-                    aLineInfo.SetDashLen( 225 );
-                    aLineInfo.SetDistance( 100 );
                 break;
                 case PS_NULL :
                     bTransparent = true;

vcl/qa/cppunit/wmf/wmfimporttest.cxx
@@ -146,38 +146,38 @@ void WmfTest::testEmfLineStyles()
     assertXPath(pDoc, "/metafile/linecolor[5]", "color", "#0000ff");
 
     assertXPath(pDoc, "/metafile/line[1]", "style", "dash");
-    assertXPath(pDoc, "/metafile/line[1]", "dashlen", "225");
+    assertXPath(pDoc, "/metafile/line[1]", "dashlen", "528");
     assertXPath(pDoc, "/metafile/line[1]", "dashcount", "1");
-    assertXPath(pDoc, "/metafile/line[1]", "dotlen", "0");
+    assertXPath(pDoc, "/metafile/line[1]", "dotlen", "176");
     assertXPath(pDoc, "/metafile/line[1]", "dotcount", "0");
-    assertXPath(pDoc, "/metafile/line[1]", "distance", "100");
+    assertXPath(pDoc, "/metafile/line[1]", "distance", "176");
     assertXPath(pDoc, "/metafile/line[1]", "join", "miter");
     assertXPath(pDoc, "/metafile/line[1]", "cap", "butt");
 
     assertXPath(pDoc, "/metafile/line[2]", "style", "dash");
-    assertXPath(pDoc, "/metafile/line[2]", "dashlen", "0");
+    assertXPath(pDoc, "/metafile/line[2]", "dashlen", "528");
     assertXPath(pDoc, "/metafile/line[2]", "dashcount", "0");
-    assertXPath(pDoc, "/metafile/line[2]", "dotlen", "30");
+    assertXPath(pDoc, "/metafile/line[2]", "dotlen", "176");
     assertXPath(pDoc, "/metafile/line[2]", "dotcount", "1");
-    assertXPath(pDoc, "/metafile/line[2]", "distance", "50");
+    assertXPath(pDoc, "/metafile/line[2]", "distance", "176");
     assertXPath(pDoc, "/metafile/line[2]", "join", "miter");
     assertXPath(pDoc, "/metafile/line[2]", "cap", "butt");
 
     assertXPath(pDoc, "/metafile/line[3]", "style", "dash");
-    assertXPath(pDoc, "/metafile/line[3]", "dashlen", "150");
+    assertXPath(pDoc, "/metafile/line[3]", "dashlen", "528");
     assertXPath(pDoc, "/metafile/line[3]", "dashcount", "1");
-    assertXPath(pDoc, "/metafile/line[3]", "dotlen", "30");
+    assertXPath(pDoc, "/metafile/line[3]", "dotlen", "176");
     assertXPath(pDoc, "/metafile/line[3]", "dotcount", "1");
-    assertXPath(pDoc, "/metafile/line[3]", "distance", "90");
+    assertXPath(pDoc, "/metafile/line[3]", "distance", "176");
     assertXPath(pDoc, "/metafile/line[3]", "join", "miter");
     assertXPath(pDoc, "/metafile/line[3]", "cap", "butt");
 
     assertXPath(pDoc, "/metafile/line[4]", "style", "dash");
-    assertXPath(pDoc, "/metafile/line[4]", "dashlen", "150");
+    assertXPath(pDoc, "/metafile/line[4]", "dashlen", "528");
     assertXPath(pDoc, "/metafile/line[4]", "dashcount", "1");
-    assertXPath(pDoc, "/metafile/line[4]", "dotlen", "30");
+    assertXPath(pDoc, "/metafile/line[4]", "dotlen", "176");
     assertXPath(pDoc, "/metafile/line[4]", "dotcount", "2");
-    assertXPath(pDoc, "/metafile/line[4]", "distance", "50");
+    assertXPath(pDoc, "/metafile/line[4]", "distance", "176");
     assertXPath(pDoc, "/metafile/line[4]", "join", "miter");
     assertXPath(pDoc, "/metafile/line[4]", "cap", "butt");
 };