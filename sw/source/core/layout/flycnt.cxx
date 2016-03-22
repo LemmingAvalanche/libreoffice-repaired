LemmingAvalanche/libreoffice-repaired

commit c89408017af254dd68110aca66ab09c8e58af248

simplify
Change-Id: Ib7bfe8d479ab4f0445f5a95b3108b48531f70101
Reviewed-on: https://gerrit.libreoffice.org/12997
Reviewed-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>
Tested-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>

sw/source/core/layout/flycnt.cxx
@@ -1202,30 +1202,15 @@ void SwFlyAtCntFrm::SetAbsPos( const Point &rNew )
     if ( pCnt->Frm().IsInside( aNew ) )
     {
         // #i70582#
-        SwTwips nTopForObjPos;
         if ( bVert )
         {
-            nTopForObjPos = pCnt->Frm().Left();
+            nY = pCnt->Frm().Left() - rNew.X();
             if ( bVertL2R )
-                nTopForObjPos += pCnt->GetUpperSpaceAmountConsideredForPrevFrmAndPageGrid();
-            else
-                nTopForObjPos += pCnt->Frm().Width() - pCnt->GetUpperSpaceAmountConsideredForPrevFrmAndPageGrid();
+                nY = -nY;
+            nY -= pCnt->GetUpperSpaceAmountConsideredForPrevFrmAndPageGrid();
         }
         else
-        {
-            nTopForObjPos = pCnt->Frm().Top() + pCnt->GetUpperSpaceAmountConsideredForPrevFrmAndPageGrid();
-        }
-        if( bVert )
-        {
-            if ( bVertL2R )
-                nY = rNew.X() - nTopForObjPos;
-            else
-                nY = nTopForObjPos - rNew.X() - Frm().Width();
-        }
-        else
-        {
-            nY = rNew.Y() - nTopForObjPos;
-        }
+            nY = rNew.Y() - pCnt->Frm().Top() + pCnt->GetUpperSpaceAmountConsideredForPrevFrmAndPageGrid();
     }
     else
     {

commit a0211135994b110fe387c368b58e8643bcdfaf31

tdf#95977: fix fly positioning regression from c8940801
Change-Id: I7e11c7072ba0ec82237985da79f8572fdda05778
Reviewed-on: https://gerrit.libreoffice.org/23428
Reviewed-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>
Tested-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>

sw/source/core/layout/flycnt.cxx
@@ -1220,7 +1220,7 @@ void SwFlyAtContentFrame::SetAbsPos( const Point &rNew )
             nY -= pCnt->GetUpperSpaceAmountConsideredForPrevFrameAndPageGrid();
         }
         else
-            nY = rNew.Y() - pCnt->Frame().Top() + pCnt->GetUpperSpaceAmountConsideredForPrevFrameAndPageGrid();
+            nY = rNew.Y() - pCnt->Frame().Top() - pCnt->GetUpperSpaceAmountConsideredForPrevFrameAndPageGrid();
     }
     else
     {