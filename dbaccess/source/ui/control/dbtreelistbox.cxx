LemmingAvalanche/libreoffice-repaired

commit 510f8c910429aeb48ccf4993da18562f42906b6b

convert Link<> to typed
Change-Id: I85658fa35b9b85106a3b9c8ef303584cad6f39b0

dbaccess/source/ui/control/dbtreelistbox.cxx
@@ -400,9 +400,8 @@ bool DBTreeListBox::EditedEntry( SvTreeListEntry* pEntry, const OUString& rNewTe
 
 bool DBTreeListBox::DoubleClickHdl()
 {
-    long nResult = aDoubleClickHdl.Call( this );
     // continue default processing if the DoubleClickHandler didn't handle it
-    return nResult == 0;
+    return aDoubleClickHdl.Call( this );
 }
 
 void scrollWindow(DBTreeListBox* _pListBox, const Point& _rPos,bool _bUp)

commit 374e86c7bfb9c74bc3c3d1d517e923af3425b60d

tdf#99073 fix double-click tree node expansion in base
fallout from
commit	510f8c910429aeb48ccf4993da18562f42906b6b
"convert Link<> to typed"

Change-Id: I3452b76320c5355aaf84a0bd67c99b281bb46d0f

dbaccess/source/ui/control/dbtreelistbox.cxx
@@ -386,7 +386,7 @@ bool DBTreeListBox::EditedEntry( SvTreeListEntry* pEntry, const OUString& rNewTe
 bool DBTreeListBox::DoubleClickHdl()
 {
     // continue default processing if the DoubleClickHandler didn't handle it
-    return aDoubleClickHdl.Call( this );
+    return !aDoubleClickHdl.Call( this );
 }
 
 void scrollWindow(DBTreeListBox* _pListBox, const Point& _rPos,bool _bUp)