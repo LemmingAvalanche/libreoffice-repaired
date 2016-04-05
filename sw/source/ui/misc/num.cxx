LemmingAvalanche/libreoffice-repaired

commit f8dae9d3d6f9daf18e21d191ccb9c1cbb5ed71b9

convert Link<> to typed
Change-Id: I59d325c3b051690303a5841907317122fa1ec98b
Reviewed-on: https://gerrit.libreoffice.org/18825
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

sw/source/ui/misc/num.cxx
@@ -116,15 +116,16 @@ SwNumPositionTabPage::SwNumPositionTabPage(vcl::Window* pParent,
     m_pAlign2FT->SetText( m_pAlignFT->GetText() );
 
     Link<> aLk = LINK(this, SwNumPositionTabPage, DistanceHdl);
+    Link<Control&,void> aLk2 = LINK(this, SwNumPositionTabPage, DistanceLoseFocusHdl);
     m_pDistBorderMF->SetUpHdl(aLk);
     m_pDistNumMF->SetUpHdl(aLk);
     m_pIndentMF->SetUpHdl(aLk);
     m_pDistBorderMF->SetDownHdl(aLk);
     m_pDistNumMF->SetDownHdl(aLk);
     m_pIndentMF->SetDownHdl(aLk);
-    m_pDistBorderMF->SetLoseFocusHdl(aLk);
-    m_pDistNumMF->SetLoseFocusHdl(aLk);
-    m_pIndentMF->SetLoseFocusHdl(aLk);
+    m_pDistBorderMF->SetLoseFocusHdl(aLk2);
+    m_pDistNumMF->SetLoseFocusHdl(aLk2);
+    m_pIndentMF->SetLoseFocusHdl(aLk2);
 
     m_pLabelFollowedByLB->SetDropDownLineCount( m_pLabelFollowedByLB->GetEntryCount() );
     m_pLabelFollowedByLB->SetSelectHdl( LINK(this, SwNumPositionTabPage, LabelFollowedByHdl_Impl) );
@@ -132,17 +133,17 @@ SwNumPositionTabPage::SwNumPositionTabPage(vcl::Window* pParent,
     aLk = LINK(this, SwNumPositionTabPage, ListtabPosHdl_Impl);
     m_pListtabMF->SetUpHdl(aLk);
     m_pListtabMF->SetDownHdl(aLk);
-    m_pListtabMF->SetLoseFocusHdl(aLk);
+    m_pListtabMF->SetLoseFocusHdl(aLk2);
 
     aLk = LINK(this, SwNumPositionTabPage, AlignAtHdl_Impl);
     m_pAlignedAtMF->SetUpHdl(aLk);
     m_pAlignedAtMF->SetDownHdl(aLk);
-    m_pAlignedAtMF->SetLoseFocusHdl(aLk);
+    m_pAlignedAtMF->SetLoseFocusHdl(aLk2);
 
     aLk = LINK(this, SwNumPositionTabPage, IndentAtHdl_Impl);
     m_pIndentAtMF->SetUpHdl(aLk);
     m_pIndentAtMF->SetDownHdl(aLk);
-    m_pIndentAtMF->SetLoseFocusHdl(aLk);
+    m_pIndentAtMF->SetLoseFocusHdl(aLk2);
 
     m_pLevelLB->SetSelectHdl(LINK(this, SwNumPositionTabPage, LevelHdl));
     m_pRelativeCB->SetClickHdl(LINK(this, SwNumPositionTabPage, RelativeHdl));
@@ -659,6 +660,10 @@ IMPL_LINK( SwNumPositionTabPage, LevelHdl, ListBox *, pBox )
     return 0;
 }
 
+IMPL_LINK_TYPED( SwNumPositionTabPage, DistanceLoseFocusHdl, Control&, rControl, void )
+{
+    DistanceHdl(static_cast<MetricField*>(&rControl));
+}
 IMPL_LINK( SwNumPositionTabPage, DistanceHdl, MetricField *, pField )
 {
     if(bInInintControl)

sw/source/uibase/inc/num.hxx
@@ -94,6 +94,7 @@ class SwNumPositionTabPage : public SfxTabPage
     DECL_LINK( LevelHdl, ListBox * );
     DECL_LINK(EditModifyHdl, void *);
     DECL_LINK( DistanceHdl, MetricField * );
+    DECL_LINK_TYPED( DistanceLoseFocusHdl, Control&, void );
     DECL_LINK_TYPED( RelativeHdl, Button*, void );
     DECL_LINK_TYPED(StandardHdl, Button*, void);
 

commit ed6026f84f125b85ed4e143bb9c921f05d8077a4

tdf#98647 fix bug in tools->outline-numbering
fallout from
commit f8dae9d3d6f9daf18e21d191ccb9c1cbb5ed71b9
"convert Link<> to typed"

Change-Id: I8f98bcf6d1eff596e139f9f3a2e3d32fcce67d11

sw/source/ui/misc/num.cxx
@@ -131,16 +131,19 @@ SwNumPositionTabPage::SwNumPositionTabPage(vcl::Window* pParent,
     m_pLabelFollowedByLB->SetSelectHdl( LINK(this, SwNumPositionTabPage, LabelFollowedByHdl_Impl) );
 
     aLk = LINK(this, SwNumPositionTabPage, ListtabPosHdl_Impl);
+    aLk2 = LINK(this, SwNumPositionTabPage, ListtabPosFocusHdl_Impl);
     m_pListtabMF->SetUpHdl(aLk);
     m_pListtabMF->SetDownHdl(aLk);
     m_pListtabMF->SetLoseFocusHdl(aLk2);
 
     aLk = LINK(this, SwNumPositionTabPage, AlignAtHdl_Impl);
+    aLk2 = LINK(this, SwNumPositionTabPage, AlignAtFocusHdl_Impl);
     m_pAlignedAtMF->SetUpHdl(aLk);
     m_pAlignedAtMF->SetDownHdl(aLk);
     m_pAlignedAtMF->SetLoseFocusHdl(aLk2);
 
     aLk = LINK(this, SwNumPositionTabPage, IndentAtHdl_Impl);
+    aLk2 = LINK(this, SwNumPositionTabPage, IndentAtFocusHdl_Impl);
     m_pIndentAtMF->SetUpHdl(aLk);
     m_pIndentAtMF->SetDownHdl(aLk);
     m_pIndentAtMF->SetLoseFocusHdl(aLk2);
@@ -818,6 +821,10 @@ IMPL_LINK_NOARG_TYPED(SwNumPositionTabPage, LabelFollowedByHdl_Impl, ListBox&, v
     SetModified();
 }
 
+IMPL_LINK_TYPED( SwNumPositionTabPage, ListtabPosFocusHdl_Impl, Control&, rControl, void )
+{
+    ListtabPosHdl_Impl(static_cast<SpinField&>(rControl));
+}
 IMPL_LINK_TYPED( SwNumPositionTabPage, ListtabPosHdl_Impl, SpinField&, rSpin, void )
 {
     MetricField& rField = static_cast<MetricField&>(rSpin);
@@ -840,6 +847,10 @@ IMPL_LINK_TYPED( SwNumPositionTabPage, ListtabPosHdl_Impl, SpinField&, rSpin, vo
     SetModified();
 }
 
+IMPL_LINK_TYPED( SwNumPositionTabPage, AlignAtFocusHdl_Impl, Control&, rControl, void )
+{
+    AlignAtHdl_Impl(static_cast<SpinField&>(rControl));
+}
 IMPL_LINK_TYPED( SwNumPositionTabPage, AlignAtHdl_Impl, SpinField&, rSpin, void )
 {
     MetricField& rField = static_cast<MetricField&>(rSpin);
@@ -863,6 +874,10 @@ IMPL_LINK_TYPED( SwNumPositionTabPage, AlignAtHdl_Impl, SpinField&, rSpin, void 
     SetModified();
 }
 
+IMPL_LINK_TYPED( SwNumPositionTabPage, IndentAtFocusHdl_Impl, Control&, rControl, void )
+{
+    IndentAtHdl_Impl(static_cast<SpinField&>(rControl));
+}
 IMPL_LINK_TYPED( SwNumPositionTabPage, IndentAtHdl_Impl, SpinField&, rSpin, void )
 {
     MetricField& rField = static_cast<MetricField&>(rSpin);

sw/source/uibase/inc/num.hxx
@@ -96,8 +96,11 @@ class SwNumPositionTabPage : public SfxTabPage
 
     DECL_LINK_TYPED(LabelFollowedByHdl_Impl, ListBox&, void);
     DECL_LINK_TYPED( ListtabPosHdl_Impl, SpinField&, void );
+    DECL_LINK_TYPED( ListtabPosFocusHdl_Impl, Control&, void );
     DECL_LINK_TYPED( AlignAtHdl_Impl, SpinField&, void );
+    DECL_LINK_TYPED( AlignAtFocusHdl_Impl, Control&, void );
     DECL_LINK_TYPED( IndentAtHdl_Impl, SpinField&, void );
+    DECL_LINK_TYPED( IndentAtFocusHdl_Impl, Control&, void );
 
     using SfxTabPage::ActivatePage;
     using SfxTabPage::DeactivatePage;