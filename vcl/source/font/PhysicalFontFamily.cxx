LemmingAvalanche/libreoffice-repaired

commit fc02708a34964dfe4fc81b3d1c2890098669ee15

fdo#79761 - avoid re-canonicalising each font name on clone repeatedly.
Change-Id: I5b32787943ab3f824249359e9924eae25ba5783b

vcl/source/font/PhysicalFontFamily.cxx
@@ -281,6 +281,10 @@ void PhysicalFontFamily::GetFontHeights( std::set<int>& rHeights ) const
 void PhysicalFontFamily::UpdateCloneFontList( PhysicalFontCollection& rFontCollection,
                                               bool bScalable, bool bEmbeddable ) const
 {
+    // This is rather expensive to do per face.
+    OUString aFamilyName = GetEnglishSearchFontName( GetFamilyName() );
+    PhysicalFontFamily* pFamily = rFontCollection.FindOrCreateFamily( aFamilyName );
+
     for( PhysicalFontFace* pFace = mpFirst; pFace; pFace = pFace->GetNextFace() )
     {
         if( bScalable && !pFace->IsScalable() )
@@ -289,7 +293,12 @@ void PhysicalFontFamily::UpdateCloneFontList( PhysicalFontCollection& rFontColle
             continue;
 
         PhysicalFontFace* pClonedFace = pFace->Clone();
-        rFontCollection.Add( pClonedFace );
+
+        assert( pClonedFace->GetFamilyName() == GetFamilyName() );
+        assert( rFontCollection.FindOrCreateFamily( GetEnglishSearchFontName( pClonedFace->GetFamilyName() ) ) == pFamily );
+
+        if (! pFamily->AddFontFace( pClonedFace ) )
+            delete pClonedFace;
     }
 }
 

commit b14dc7642fca1c80f99944aa82281aba3b0d5c8e

tdf#98989: vcl: fix handling of non-scalable fonts like "Courier"
For a VirtualDevice only scalable fonts are cloned, but for non-scalable
bitmap fonts still an empty PhysicalFontFamily with no PhysicalFontFace
is created, which causes text to disappear (height 0).

Suppress creation of such families like it was done in LO 4.3, so that
the fall-back can handle it and map "Courier" to "Courier New".

(regression from fc02708a34964dfe4fc81b3d1c2890098669ee15)

Change-Id: I6542a3f7a01bdf46ae2bcf328fa04064f7f86332

vcl/source/font/PhysicalFontFamily.cxx
@@ -264,7 +264,7 @@ void PhysicalFontFamily::UpdateCloneFontList( PhysicalFontCollection& rFontColle
                                               bool bEmbeddable ) const
 {
     OUString aFamilyName = GetEnglishSearchFontName( GetFamilyName() );
-    PhysicalFontFamily* pFamily = rFontCollection.FindOrCreateFontFamily( aFamilyName );
+    PhysicalFontFamily* pFamily(nullptr);
 
     for( std::vector< PhysicalFontFace* >::const_iterator it=maFontFaces.begin(); it != maFontFaces.end(); ++it )
     {
@@ -275,6 +275,11 @@ void PhysicalFontFamily::UpdateCloneFontList( PhysicalFontCollection& rFontColle
         if( bEmbeddable && !pFoundFontFace->CanEmbed() && !pFoundFontFace->CanSubset() )
             continue;
 
+        if (!pFamily)
+        {   // tdf#98989 lazy create as family without faces won't work
+            pFamily = rFontCollection.FindOrCreateFontFamily(aFamilyName);
+        }
+        assert(pFamily);
         PhysicalFontFace* pClonedFace = pFoundFontFace->Clone();
 
 #if OSL_DEBUG_LEVEL > 0