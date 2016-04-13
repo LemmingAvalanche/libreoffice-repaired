LemmingAvalanche/libreoffice-repaired

commit 476e7e434acee7a6a14928e65ef54f05df46bbcf

sfx2: make SfxDispatcher::Execute() less variadic ...
... and less overloaded; C++11 initializer_list should make undefined
behavior as fixed in 753d219b623993dc803e696bffa5e647ad4d6e9e less
likely.

Change-Id: I15cc0804451b6a4fcbfaa623f9a30db222207865
Reviewed-on: https://gerrit.libreoffice.org/23666
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Stahl <mstahl@redhat.com>

svx/source/sidebar/area/AreaPropertyPanel.cxx
@@ -105,46 +105,48 @@ VclPtr<vcl::Window> AreaPropertyPanel::Create (
 
 void AreaPropertyPanel::setFillTransparence(const XFillTransparenceItem& rItem)
 {
-    GetBindings()->GetDispatcher()->Execute( SID_ATTR_FILL_TRANSPARENCE, SfxCallMode::RECORD, &rItem, 0L );
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_TRANSPARENCE,
+            SfxCallMode::RECORD, { &rItem });
 }
 
 void AreaPropertyPanel::setFillFloatTransparence(const XFillFloatTransparenceItem& rItem)
 {
-    GetBindings()->GetDispatcher()->Execute( SID_ATTR_FILL_FLOATTRANSPARENCE, SfxCallMode::RECORD, &rItem, 0L );
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_FLOATTRANSPARENCE,
+            SfxCallMode::RECORD, { &rItem });
 }
 
 void AreaPropertyPanel::setFillStyle(const XFillStyleItem& rItem)
 {
-    GetBindings()->GetDispatcher()->Execute(
-            SID_ATTR_FILL_STYLE, SfxCallMode::RECORD, &rItem, 0L);
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_STYLE,
+            SfxCallMode::RECORD, { &rItem });
 }
 
 void AreaPropertyPanel::setFillStyleAndColor(const XFillStyleItem* pStyleItem,
         const XFillColorItem& aColorItem)
 {
-    GetBindings()->GetDispatcher()->Execute(
-            SID_ATTR_FILL_COLOR, SfxCallMode::RECORD, &aColorItem, pStyleItem, 0L);
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_COLOR,
+            SfxCallMode::RECORD, { &aColorItem, pStyleItem });
 }
 
 void AreaPropertyPanel::setFillStyleAndGradient(const XFillStyleItem* pStyleItem,
         const XFillGradientItem& rGradientItem)
 {
-    GetBindings()->GetDispatcher()->Execute(
-            SID_ATTR_FILL_GRADIENT, SfxCallMode::RECORD, &rGradientItem, pStyleItem, 0L);
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_GRADIENT,
+            SfxCallMode::RECORD, { &rGradientItem, pStyleItem });
 }
 
 void AreaPropertyPanel::setFillStyleAndHatch(const XFillStyleItem* pStyleItem,
         const XFillHatchItem& rHatchItem)
 {
-    GetBindings()->GetDispatcher()->Execute(
-            SID_ATTR_FILL_HATCH, SfxCallMode::RECORD, &rHatchItem, pStyleItem, 0L);
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_HATCH,
+            SfxCallMode::RECORD, { &rHatchItem, pStyleItem });
 }
 
 void AreaPropertyPanel::setFillStyleAndBitmap(const XFillStyleItem* pStyleItem,
         const XFillBitmapItem& rBitmapItem)
 {
-    GetBindings()->GetDispatcher()->Execute(
-            SID_ATTR_FILL_BITMAP, SfxCallMode::RECORD, &rBitmapItem, pStyleItem, 0L);
+    GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_BITMAP,
+            SfxCallMode::RECORD, { &rBitmapItem, pStyleItem });
 }
 
 } } // end of namespace svx::sidebar

commit fbe13be08ae17237af4334759a3bb83e3ece0a0b

tdf#99264 svx: don't pass null pointers to SfxDispatcher
(regression from 476e7e434acee7a6a14928e65ef54f05df46bbcf)

Change-Id: I79863f8e5767acc90dc3c6ef5e94d2d90a03dd0e

svx/source/sidebar/area/AreaPropertyPanel.cxx
@@ -122,31 +122,39 @@ void AreaPropertyPanel::setFillStyle(const XFillStyleItem& rItem)
 }
 
 void AreaPropertyPanel::setFillStyleAndColor(const XFillStyleItem* pStyleItem,
-        const XFillColorItem& aColorItem)
+        const XFillColorItem& rColorItem)
 {
     GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_COLOR,
-            SfxCallMode::RECORD, { &aColorItem, pStyleItem });
+        SfxCallMode::RECORD, (pStyleItem)
+            ? std::initializer_list<SfxPoolItem const*>({ &rColorItem, pStyleItem })
+            : std::initializer_list<SfxPoolItem const*>({ &rColorItem }));
 }
 
 void AreaPropertyPanel::setFillStyleAndGradient(const XFillStyleItem* pStyleItem,
         const XFillGradientItem& rGradientItem)
 {
     GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_GRADIENT,
-            SfxCallMode::RECORD, { &rGradientItem, pStyleItem });
+        SfxCallMode::RECORD, (pStyleItem)
+            ? std::initializer_list<SfxPoolItem const*>({ &rGradientItem, pStyleItem })
+            : std::initializer_list<SfxPoolItem const*>({ &rGradientItem }));
 }
 
 void AreaPropertyPanel::setFillStyleAndHatch(const XFillStyleItem* pStyleItem,
         const XFillHatchItem& rHatchItem)
 {
     GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_HATCH,
-            SfxCallMode::RECORD, { &rHatchItem, pStyleItem });
+        SfxCallMode::RECORD, (pStyleItem)
+            ? std::initializer_list<SfxPoolItem const*>({ &rHatchItem, pStyleItem })
+            : std::initializer_list<SfxPoolItem const*>({ &rHatchItem }));
 }
 
 void AreaPropertyPanel::setFillStyleAndBitmap(const XFillStyleItem* pStyleItem,
         const XFillBitmapItem& rBitmapItem)
 {
     GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_BITMAP,
-            SfxCallMode::RECORD, { &rBitmapItem, pStyleItem });
+        SfxCallMode::RECORD, (pStyleItem)
+            ? std::initializer_list<SfxPoolItem const*>({ &rBitmapItem, pStyleItem })
+            : std::initializer_list<SfxPoolItem const*>({ &rBitmapItem }));
 }
 
 } } // end of namespace svx::sidebar