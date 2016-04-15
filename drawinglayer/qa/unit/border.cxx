LemmingAvalanche/libreoffice-repaired

commit 5f158beb27f368564fd02b322edf926847053467

fdo#75260: Correctly draw double lines for both Writer and Calc.
Fix all sorts of incorrect double line handling in drawinglayer in order to
draw thick-thin double line types correctly.  Also change handling of border
lines in writer tables. There are still some outstanding issues but it's
much better than how it was before.

Also realized that Word and Excel handle simple thin double lines differently;
Word varies widths of all of the lines and the gap whereas Excel only has one
fixed size for its double line.  For this reason I decided to add a separate
double line type (DOUBLE_THIN) to handle Excel's double line.

Change-Id: Iaaa353b6e4f998b524262bea59260b4333e0cdb4

drawinglayer/source/primitive2d/borderlineprimitive2d.cxx
@@ -28,6 +28,8 @@
 #include <numeric>
 #include <algorithm>
 
+namespace drawinglayer {
+
 namespace {
 
 void moveLine(basegfx::B2DPolygon& rPoly, double fGap, const basegfx::B2DVector& rVector)
@@ -44,10 +46,46 @@ void moveLine(basegfx::B2DPolygon& rPoly, double fGap, const basegfx::B2DVector&
     }
 }
 
+primitive2d::Primitive2DReference makeHairLinePrimitive(
+    const basegfx::B2DPoint& rStart, const basegfx::B2DPoint& rEnd, const basegfx::B2DVector& rVector,
+    const basegfx::BColor& rColor, double fGap)
+{
+    basegfx::B2DPolygon aPolygon;
+    aPolygon.append(rStart);
+    aPolygon.append(rEnd);
+    moveLine(aPolygon, fGap, rVector);
+
+    return primitive2d::Primitive2DReference(new primitive2d::PolygonHairlinePrimitive2D(aPolygon, rColor));
 }
 
-namespace drawinglayer
+primitive2d::Primitive2DReference makeSolidLinePrimitive(
+    const basegfx::B2DPolyPolygon& rClipRegion, const basegfx::B2DPoint& rStart, const basegfx::B2DPoint& rEnd,
+    const basegfx::B2DVector& rVector, const basegfx::BColor& rColor, double fLineWidth, double fGap)
 {
+    const basegfx::B2DVector aPerpendicular = basegfx::getPerpendicular(rVector);
+    const basegfx::B2DVector aLineWidthOffset = ((fLineWidth + 1.0) * 0.5) * aPerpendicular;
+
+    basegfx::B2DPolygon aPolygon;
+    aPolygon.append(rStart + aLineWidthOffset);
+    aPolygon.append(rEnd + aLineWidthOffset);
+    aPolygon.append(rEnd - aLineWidthOffset);
+    aPolygon.append(rStart - aLineWidthOffset);
+    aPolygon.setClosed(true);
+
+    moveLine(aPolygon, fGap, rVector);
+
+    basegfx::B2DPolyPolygon aClipped =
+        basegfx::tools::clipPolygonOnPolyPolygon(aPolygon, rClipRegion, true, false);
+
+    if (aClipped.count())
+        aPolygon = aClipped.getB2DPolygon(0);
+
+    return primitive2d::Primitive2DReference(
+        new primitive2d::PolyPolygonColorPrimitive2D(basegfx::B2DPolyPolygon(aPolygon), rColor));
+}
+
+}
+
     // fdo#49438: heuristic pseudo hack
     static bool lcl_UseHairline(double const fW,
             basegfx::B2DPoint const& rStart, basegfx::B2DPoint const& rEnd,
@@ -139,66 +177,42 @@ namespace drawinglayer
 
                 if(isOutsideUsed() && isInsideUsed())
                 {
-                    basegfx::B2DPolygon aPolygon;
                     const double fExt = getWidth(rViewInformation);  // Extend a lot: it'll be clipped later.
                     const basegfx::B2DPoint aTmpStart(getStart() - (fExt * aVector));
                     const basegfx::B2DPoint aTmpEnd(getEnd() + (fExt * aVector));
 
-                    // Get which is the line to show
-                    double nWidth = getLeftWidth();
-                    basegfx::BColor aColor = getRGBColorLeft();
+                    xRetval.realloc(2);
 
-                    bool const bIsHairline = lcl_UseHairline(
-                            nWidth, getStart(), getEnd(), rViewInformation);
-                    nWidth = lcl_GetCorrectedWidth(nWidth,
-                                getStart(), getEnd(), rViewInformation);
-
-                    // distance is already scaled.
-                    double fGap = mfDistance*8.0;
-
-                    if (bIsHairline)
                     {
-                        // create hairline primitive
-                        aPolygon.append( getStart() );
-                        aPolygon.append( getEnd() );
+                        // "inside" line
+                        double fWidth = getLeftWidth();
+                        basegfx::BColor aColor = getRGBColorLeft();
+                        bool bIsHairline = lcl_UseHairline(
+                                fWidth, getStart(), getEnd(), rViewInformation);
+                        fWidth = lcl_GetCorrectedWidth(fWidth,
+                                    getStart(), getEnd(), rViewInformation);
 
-                        basegfx::B2DPolygon aPolygon2 = aPolygon;
-                        moveLine(aPolygon2, fGap, aVector);
-
-                        xRetval.realloc(2);
-                        xRetval[0] = Primitive2DReference(new PolygonHairlinePrimitive2D(
-                            aPolygon,
-                            aColor));
-
-                        xRetval[1] = Primitive2DReference(new PolygonHairlinePrimitive2D(
-                            aPolygon2,
-                            aColor));
+                        if (bIsHairline)
+                            xRetval[0] = makeHairLinePrimitive(getStart(), getEnd(), aVector, aColor, 0.0);
+                        else
+                            xRetval[0] = makeSolidLinePrimitive(
+                                aClipRegion, aTmpStart, aTmpEnd, aVector, aColor, fWidth, 0.0);
                     }
-                    else
+
                     {
-                        // create filled polygon primitive
-                        const basegfx::B2DVector aLineWidthOffset(((nWidth + 1) * 0.5) * aPerpendicular);
+                        // "outside" line
+                        double fWidth = getRightWidth();
+                        basegfx::BColor aColor = getRGBColorRight();
+                        bool bIsHairline = lcl_UseHairline(
+                                fWidth, getStart(), getEnd(), rViewInformation);
+                        fWidth = lcl_GetCorrectedWidth(fWidth,
+                                    getStart(), getEnd(), rViewInformation);
 
-                        aPolygon.append( aTmpStart + aLineWidthOffset );
-                        aPolygon.append( aTmpEnd + aLineWidthOffset );
-                        aPolygon.append( aTmpEnd - aLineWidthOffset );
-                        aPolygon.append( aTmpStart - aLineWidthOffset );
-                        aPolygon.setClosed( true );
-
-                        basegfx::B2DPolyPolygon aClipped = basegfx::tools::clipPolygonOnPolyPolygon(
-                            aPolygon, aClipRegion, true, false );
-
-                        if ( aClipped.count() )
-                            aPolygon = aClipped.getB2DPolygon(0);
-
-                        basegfx::B2DPolygon aPolygon2 = aPolygon;
-                        moveLine(aPolygon2, fGap, aVector);
-
-                        xRetval.realloc(2);
-                        xRetval[0] = Primitive2DReference(
-                            new PolyPolygonColorPrimitive2D(basegfx::B2DPolyPolygon(aPolygon), aColor));
-                        xRetval[1] = Primitive2DReference(
-                            new PolyPolygonColorPrimitive2D(basegfx::B2DPolyPolygon(aPolygon2), aColor));
+                        if (bIsHairline)
+                            xRetval[1] = makeHairLinePrimitive(getStart(), getEnd(), aVector, aColor, mfDistance);
+                        else
+                            xRetval[1] = makeSolidLinePrimitive(
+                                aClipRegion, aTmpStart, aTmpEnd, aVector, aColor, fWidth, mfDistance);
                     }
                 }
                 else

cui/source/tabpages/border.cxx
@@ -1045,6 +1045,7 @@ void SvxBorderTabPage::FillLineListBox_Impl()
 
         // Double lines
         { DOUBLE,              10, &sameColor, &sameColor, &sameDistColor },
+        { DOUBLE_THIN,         10, &sameColor, &sameColor, &sameDistColor },
         { THINTHICK_SMALLGAP,  20, &sameColor, &sameColor, &sameDistColor },
         { THINTHICK_MEDIUMGAP,  0, &sameColor, &sameColor, &sameDistColor },
         { THINTHICK_LARGEGAP,   0, &sameColor, &sameColor, &sameDistColor },

drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -297,13 +297,13 @@ namespace drawinglayer
             switch (rSource.getStyle())
             {
                 case table::BorderLineStyle::SOLID:
-                case table::BorderLineStyle::DOUBLE:
+                case table::BorderLineStyle::DOUBLE_THIN:
                 {
                     const basegfx::BColor aLineColor =
                         maBColorModifierStack.getModifiedColor(rSource.getRGBColorLeft());
                     double nThick = rtl::math::round(rSource.getLeftWidth());
 
-                    bool bDouble = rSource.getStyle() == table::BorderLineStyle::DOUBLE;
+                    bool bDouble = rSource.getStyle() == table::BorderLineStyle::DOUBLE_THIN;
 
                     basegfx::B2DPolygon aTarget;
 

editeng/source/items/borderline.cxx
@@ -316,6 +316,10 @@ BorderWidthImpl SvxBorderLine::getWidthImpl( SvxBorderStyle nStyle )
                     1.0/3.0, 1.0/3.0, 1.0/3.0 );
             break;
 
+        case DOUBLE_THIN:
+            aImpl = BorderWidthImpl(CHANGE_DIST, 10.0, 10.0, 1.0);
+            break;
+
         case THINTHICK_SMALLGAP:
             aImpl = BorderWidthImpl( CHANGE_LINE1, 1.0,
                     THINTHICK_SMALLGAP_line2, THINTHICK_SMALLGAP_gap );
@@ -429,6 +433,7 @@ void SvxBorderLine::GuessLinesWidths( SvxBorderStyle nStyle, sal_uInt16 nOut, sa
         static const SvxBorderStyle aDoubleStyles[] =
         {
             DOUBLE,
+            DOUBLE_THIN,
             THINTHICK_SMALLGAP,
             THINTHICK_MEDIUMGAP,
             THINTHICK_LARGEGAP,

editeng/source/items/frmitems.cxx
@@ -1834,7 +1834,7 @@ SvxBoxItem::LineToSvxLine(const ::com::sun::star::table::BorderLine2& rLine, Svx
         rSvxLine.SetWidth( bConvert? MM100_TO_TWIP_UNSIGNED( rLine.LineWidth ) : rLine.LineWidth );
         // fdo#46112: double does not necessarily mean symmetric
         // for backwards compatibility
-        bGuessWidth = (DOUBLE == nStyle) &&
+        bGuessWidth = ((DOUBLE == nStyle || DOUBLE_THIN == nStyle)) &&
             (rLine.InnerLineWidth > 0) && (rLine.OuterLineWidth > 0);
     }
 

include/xmloff/xmltoken.hxx
@@ -647,6 +647,7 @@ namespace xmloff { namespace token {
         XML_DOTTED,
         XML_DOUBLE,
         XML_DOUBLE_SIDED,
+        XML_DOUBLE_THIN,
         XML_DOWN,
         XML_DRAFT,
         XML_DRAW,

offapi/com/sun/star/table/BorderLineStyle.idl
@@ -41,7 +41,8 @@ constants BorderLineStyle
      */
     const short DASHED = 2;
 
-    /** Double border line.
+    /** Double border line. Widths of the lines and the gap are all equal,
+        and vary equally with the total width.
      */
     const short DOUBLE = 3;
 
@@ -95,9 +96,14 @@ constants BorderLineStyle
      */
     const short FINE_DASHED = 14;
 
+    /** Double border line consisting of two fixed thin lines separated by a
+        variable gap.
+     */
+    const short DOUBLE_THIN = 15;
+
     /** Maximum valid border line style value.
      */
-    const short BORDER_LINE_STYLE_MAX = 14;
+    const short BORDER_LINE_STYLE_MAX = 15;
 };
 
 

sc/qa/unit/subsequent_export-test.cxx
@@ -1015,7 +1015,7 @@ const char* toBorderName( sal_Int16 eStyle )
         case table::BorderLineStyle::SOLID: return "SOLID";
         case table::BorderLineStyle::DOTTED: return "DOTTED";
         case table::BorderLineStyle::DASHED: return "DASHED";
-        case table::BorderLineStyle::DOUBLE: return "DOUBLE";
+        case table::BorderLineStyle::DOUBLE_THIN: return "DOUBLE_THIN";
         case table::BorderLineStyle::FINE_DASHED: return "FINE_DASHED";
         default:
             ;
@@ -1046,7 +1046,7 @@ void ScExportTest::testExcelCellBorders( sal_uLong nFormatType )
         { 19, table::BorderLineStyle::DASHED,      35L }, // medium dashed
         { 21, table::BorderLineStyle::SOLID,       35L }, // medium
         { 23, table::BorderLineStyle::SOLID,       50L }, // thick
-        { 25, table::BorderLineStyle::DOUBLE,      -1L }, // double (don't check width)
+        { 25, table::BorderLineStyle::DOUBLE_THIN, -1L }, // double (don't check width)
     };
 
     for (size_t i = 0; i < SAL_N_ELEMENTS(aChecks); ++i)

commit f473cf37ed85f3e38c36348eebd2edae2f984e2b

Related: tdf#99315 BorderLinePrimitive2D: fix solid line primitive width
Regression from commit 5f158beb27f368564fd02b322edf926847053467
(fdo#75260: Correctly draw double lines for both Writer and Calc.,
2014-03-03), the problem was that when decomposing a double border line
promitive to solid line primitives (which happens for the legacy
"border" type, while both the Calc UI and Excel importer creates
"border-thin" ones), the decomposed inner line's height was larger than
the requested width.

As a result there was no gap between the polygon of the inner and the
outer line of the double border, looking like a non-double border.

The width of the outer border is still incorrect on the screen, though.

Change-Id: Ia9713c315ce8f23e2579b257169798e7c82c0a64
Reviewed-on: https://gerrit.libreoffice.org/24115
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

drawinglayer/CppunitTest_drawinglayer_border.mk
@@ -1,0 +1,35 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,drawinglayer_border))
+
+$(eval $(call gb_CppunitTest_use_api,drawinglayer_border,\
+	offapi \
+	udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,drawinglayer_border, \
+	basegfx \
+	cppu \
+	sal \
+	salhelper \
+	drawinglayer \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,drawinglayer_border,\
+	boost_headers \
+	libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,drawinglayer_border, \
+	drawinglayer/qa/unit/border \
+))
+
+# vim: set noet sw=4 ts=4:

drawinglayer/Module_drawinglayer.mk
@@ -13,4 +13,8 @@ $(eval $(call gb_Module_add_targets,drawinglayer,\
     Library_drawinglayer \
 ))
 
+$(eval $(call gb_Module_add_slowcheck_targets,drawinglayer,\
+    CppunitTest_drawinglayer_border \
+))
+
 # vim: set noet sw=4 ts=4:

drawinglayer/source/primitive2d/borderlineprimitive2d.cxx
@@ -63,7 +63,7 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
     const basegfx::B2DVector& rVector, const basegfx::BColor& rColor, double fLineWidth, double fGap)
 {
     const basegfx::B2DVector aPerpendicular = basegfx::getPerpendicular(rVector);
-    const basegfx::B2DVector aLineWidthOffset = ((fLineWidth + 1.0) * 0.5) * aPerpendicular;
+    const basegfx::B2DVector aLineWidthOffset = (fLineWidth * 0.5) * aPerpendicular;
 
     basegfx::B2DPolygon aPolygon;
     aPolygon.append(rStart + aLineWidthOffset);

drawinglayer/qa/unit/border.cxx
@@ -1,0 +1,82 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include <com/sun/star/table/BorderLineStyle.hpp>
+
+#include <drawinglayer/geometry/viewinformation2d.hxx>
+#include <drawinglayer/primitive2d/borderlineprimitive2d.hxx>
+#include <drawinglayer/primitive2d/polypolygonprimitive2d.hxx>
+
+using namespace com::sun::star;
+
+namespace
+{
+
+class DrawinglayerBorderTest : public CppUnit::TestFixture
+{
+public:
+    void testDoubleDecompositionSolid();
+
+    CPPUNIT_TEST_SUITE(DrawinglayerBorderTest);
+    CPPUNIT_TEST(testDoubleDecompositionSolid);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void DrawinglayerBorderTest::testDoubleDecompositionSolid()
+{
+    // Create a border line primitive that's similar to the one from the bugdoc:
+    // 1.47 pixels is 0.03cm at 130% zoom and 96 DPI.
+    basegfx::B2DPoint aStart(0, 20);
+    basegfx::B2DPoint aEnd(100, 20);
+    double fLeftWidth = 1.47;
+    double fDistance = 1.47;
+    double fRightWidth = 1.47;
+    double fExtendLeftStart = 0;
+    double fExtendLeftEnd = 0;
+    double fExtendRightStart = 0;
+    double fExtendRightEnd = 0;
+    basegfx::BColor aColorRight;
+    basegfx::BColor aColorLeft;
+    basegfx::BColor aColorGap;
+    bool bHasGapColor = false;
+    sal_Int16 nStyle = table::BorderLineStyle::DOUBLE;
+    drawinglayer::primitive2d::BorderLinePrimitive2D aBorder(aStart, aEnd, fLeftWidth, fDistance, fRightWidth, fExtendLeftStart, fExtendLeftEnd, fExtendRightStart, fExtendRightEnd, aColorRight, aColorLeft, aColorGap, bHasGapColor, nStyle);
+
+    // Decompose it into polygons.
+    drawinglayer::geometry::ViewInformation2D aView;
+    drawinglayer::primitive2d::Primitive2DContainer aContainer = aBorder.get2DDecomposition(aView);
+
+    // Make sure it results in two borders as it's a double one.
+    CPPUNIT_ASSERT_EQUAL(static_cast<std::size_t>(2), aContainer.size());
+
+    // Get the inside line.
+    auto pInside = dynamic_cast<const drawinglayer::primitive2d::PolyPolygonColorPrimitive2D*>(aContainer[0].get());
+    CPPUNIT_ASSERT(pInside);
+
+    // Make sure the inside line's height is fLeftWidth.
+    const basegfx::B2DPolyPolygon& rPolyPolygon = pInside->getB2DPolyPolygon();
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt32>(1), rPolyPolygon.count());
+    const basegfx::B2DPolygon& rPolygon = rPolyPolygon.getB2DPolygon(0);
+    const basegfx::B2DRange& rRange = rPolygon.getB2DRange();
+    // This was 2.47, i.e. the width of the inner line was 1 unit (in the bugdoc's case: 1 pixel) wider than expected.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(fLeftWidth, rRange.getHeight(), basegfx::fTools::getSmallValue());
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DrawinglayerBorderTest);
+
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */