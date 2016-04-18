LemmingAvalanche/libreoffice-repaired

commit 8cb374a7716179cf545db211c93b3af63c5943f0

better drawing support for borders of different width, fdo#33634

drawinglayer/inc/drawinglayer/primitive2d/borderlineprimitive2d.hxx
@@ -85,17 +85,17 @@ namespace drawinglayer
             /// local helpers
             double getCorrectedLeftWidth() const
             {
-                return basegfx::fTools::equal(1.0, mfLeftWidth) ? 0.0 : mfLeftWidth;
+                return mfLeftWidth <= 0.1 ? 0.0 : mfLeftWidth;
             }
 
             double getCorrectedDistance() const
             {
-                return basegfx::fTools::equal(1.0, mfDistance) ? 0.0 : mfDistance;
+                return mfDistance <= 0.1 ? 0.0 : mfDistance;
             }
 
             double getCorrectedRightWidth() const
             {
-                return basegfx::fTools::equal(1.0, mfRightWidth) ? 0.0 : mfRightWidth;
+                return mfRightWidth <= 0.1 ? 0.0 : mfRightWidth;
             }
 
             double getWidth() const
@@ -105,12 +105,12 @@ namespace drawinglayer
 
             bool leftIsHairline() const
             {
-                return basegfx::fTools::equal(1.0, mfLeftWidth);
+                return 0 < mfLeftWidth && mfLeftWidth <= 0.1;
             }
 
             bool rightIsHairline() const
             {
-                return basegfx::fTools::equal(1.0, mfRightWidth);
+                return 0 < mfRightWidth && mfRightWidth <= 0.1;
             }
 
             bool isSolidLine() const

svx/inc/svx/framelink.hxx
@@ -118,12 +118,12 @@ public:
     /** Constructs an invisible frame style. */
     inline explicit     Style() : meRefMode( REFMODE_CENTERED ), mnType( editeng::SOLID ) { Clear(); }
     /** Constructs a frame style with passed line widths. */
-    inline explicit     Style( sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS, editeng::SvxBorderStyle nType ) :
+    inline explicit     Style( double nP, double nD, double nS, editeng::SvxBorderStyle nType ) :
                             meRefMode( REFMODE_CENTERED ), mnType( nType )
                             { Clear(); Set( nP, nD, nS ); }
     /** Constructs a frame style with passed color and line widths. */
     inline explicit     Style( const Color& rColorPrim, const Color& rColorSecn, const Color& rColorGap, bool bUseGapColor,
-                            sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS, editeng::SvxBorderStyle nType ) :
+                            double nP, double nD, double nS, editeng::SvxBorderStyle nType ) :
                             meRefMode( REFMODE_CENTERED ), mnType( nType )
                             { Set( rColorPrim, rColorSecn, rColorGap, bUseGapColor, nP, nD, nS ); }
     /** Constructs a frame style from the passed SvxBorderLine struct. */
@@ -138,21 +138,21 @@ public:
     inline const Color& GetColorSecn() const { return maColorSecn; }
     inline const Color& GetColorGap() const { return maColorGap; }
     inline bool         UseGapColor() const { return mbUseGapColor; }
-    inline sal_uInt16   Prim() const { return mnPrim; }
-    inline sal_uInt16   Dist() const { return mnDist; }
-    inline sal_uInt16   Secn() const { return mnSecn; }
+    inline double       Prim() const { return mnPrim; }
+    inline double       Dist() const { return mnDist; }
+    inline double       Secn() const { return mnSecn; }
     inline editeng::SvxBorderStyle Type() const { return mnType; }
 
     /** Returns the total width of this frame style. */
-    inline sal_uInt16   GetWidth() const { return mnPrim + mnDist + mnSecn; }
+    inline double       GetWidth() const { return mnPrim + mnDist + mnSecn; }
 
     /** Sets the frame style to invisible state. */
     void                Clear();
     /** Sets the frame style to the passed line widths. */
-    void                Set( sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS );
+    void                Set( double nP, double nD, double nS );
     /** Sets the frame style to the passed line widths. */
     void                Set( const Color& rColorPrim, const Color& rColorSecn, const Color& rColorGap, bool bUseGapColor,
-                            sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS );
+                            double nP, double nD, double nS );
     /** Sets the frame style to the passed SvxBorderLine struct. */
     void                Set( const editeng::SvxBorderLine& rBorder, double fScale = 1.0, sal_uInt16 nMaxWidth = SAL_MAX_UINT16 );
     /** Sets the frame style to the passed SvxBorderLine struct. Clears the style, if pBorder is 0. */
@@ -178,9 +178,9 @@ private:
     Color               maColorGap;
     bool                mbUseGapColor;
     RefMode             meRefMode;  /// Reference point handling for this frame border.
-    sal_uInt16          mnPrim;     /// Width of primary (single, left, or top) line.
-    sal_uInt16          mnDist;     /// Distance between primary and secondary line.
-    sal_uInt16          mnSecn;     /// Width of secondary (right or bottom) line.
+    double              mnPrim;     /// Width of primary (single, left, or top) line.
+    double              mnDist;     /// Distance between primary and secondary line.
+    double              mnSecn;     /// Width of secondary (right or bottom) line.
     editeng::SvxBorderStyle      mnType;
 };
 

svx/source/dialog/framelink.cxx
@@ -215,12 +215,9 @@ inline long lclD2L( double fValue )
 }
 
 /** Converts a width in twips to a width in another map unit (specified by fScale). */
-sal_uInt16 lclScaleValue( long nValue, double fScale, sal_uInt16 nMaxWidth )
+double lclScaleValue( double nValue, double fScale, sal_uInt16 nMaxWidth )
 {
-    // convert any width except 0 to at least 1 unit
-    // #i61324# 1 twip must scale to 1/100mm
-    return nValue ? static_cast< sal_uInt16 >( std::min< long >( std::max(
-        static_cast< long >( nValue * fScale ), 1L ), nMaxWidth ) ) : 0;
+    return std::min<double>(nValue * fScale, nMaxWidth);
 }
 
 // ----------------------------------------------------------------------------
@@ -1154,7 +1151,7 @@ void Style::Clear()
     Set( Color(), Color(), Color(), false, 0, 0, 0 );
 }
 
-void Style::Set( sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS )
+void Style::Set( double nP, double nD, double nS )
 {
     /*  nP  nD  nS  ->  mnPrim  mnDist  mnSecn
         --------------------------------------
@@ -1163,12 +1160,12 @@ void Style::Set( sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS )
         >0  0   >0      nP      0       0
         >0  >0  >0      nP      nD      nS
      */
-    mnPrim = nP ? nP : nS;
-    mnDist = (nP && nS) ? nD : 0;
-    mnSecn = (nP && nD) ? nS : 0;
+    mnPrim = rtl::math::round(nP ? nP : nS, 2);
+    mnDist = rtl::math::round((nP && nS) ? nD : 0, 2);
+    mnSecn = rtl::math::round((nP && nD) ? nS : 0, 2);
 }
 
-void Style::Set( const Color& rColorPrim, const Color& rColorSecn, const Color& rColorGap, bool bUseGapColor, sal_uInt16 nP, sal_uInt16 nD, sal_uInt16 nS )
+void Style::Set( const Color& rColorPrim, const Color& rColorSecn, const Color& rColorGap, bool bUseGapColor, double nP, double nD, double nS )
 {
     maColorPrim = rColorPrim;
     maColorSecn = rColorSecn;
@@ -1197,7 +1194,7 @@ void Style::Set( const SvxBorderLine& rBorder, double fScale, sal_uInt16 nMaxWid
     {
         Set( SCALEVALUE( nPrim ), SCALEVALUE( nDist ), SCALEVALUE( nSecn ) );
         // Enlarge the style if distance is too small due to rounding losses.
-        sal_uInt16 nPixWidth = SCALEVALUE( nPrim + nDist + nSecn );
+        double nPixWidth = SCALEVALUE( nPrim + nDist + nSecn );
         if( nPixWidth > GetWidth() )
             mnDist = nPixWidth - mnPrim - mnSecn;
         // Shrink the style if it is too thick for the control.
@@ -1209,7 +1206,7 @@ void Style::Set( const SvxBorderLine& rBorder, double fScale, sal_uInt16 nMaxWid
             // Still too thick? Decrease the line widths.
             if( GetWidth() > nMaxWidth )
             {
-                if( mnPrim && (mnPrim == mnSecn) )
+                if( rtl::math::approxEqual(mnPrim, 0.0) && rtl::math::approxEqual(mnPrim, mnSecn) )
                 {
                     // Both lines equal - decrease both to keep symmetry.
                     --mnPrim;
@@ -1220,7 +1217,7 @@ void Style::Set( const SvxBorderLine& rBorder, double fScale, sal_uInt16 nMaxWid
                     // Decrease each line for itself
                     if( mnPrim )
                         --mnPrim;
-                    if( (GetWidth() > nMaxWidth) && mnSecn )
+                    if( (GetWidth() > nMaxWidth) && !rtl::math::approxEqual(mnSecn, 0.0) )
                         --mnSecn;
                 }
             }
@@ -1264,8 +1261,8 @@ bool operator==( const Style& rL, const Style& rR )
 bool operator<( const Style& rL, const Style& rR )
 {
     // different total widths -> rL<rR, if rL is thinner
-    sal_uInt16 nLW = rL.GetWidth();
-    sal_uInt16 nRW = rR.GetWidth();
+    double nLW = rL.GetWidth();
+    double nRW = rR.GetWidth();
     if( nLW != nRW ) return nLW < nRW;
 
     // one line double, the other single -> rL<rR, if rL is single

commit 94461380eddd2053daa7af2a2c32bab7bbdf36cb

tdf#99315 VclPixelProcessor2D: fix double border line width
Regression from commit 8cb374a7716179cf545db211c93b3af63c5943f0 (better
drawing support for borders of different width, fdo#33634, 2012-04-04),
the problem is that previously the width of inner/outer double border
lines got rounded to integer values quite early, but after the commit
they are kept at a double precision for much longer, which needs pixel
correction in VclPixelProcessor2D.

Example: if the border with is 1.47, and the line gets moved by 0.2
pixels, then the inner and outer edge of the line will be 0.2 and 1.67,
which gets rounded to 0 -> 2 in the pixel processor. Previously the
input was rounded to 1, so moving by 0.2 resulted in 0.2 -> 1.2, which
got rounded to 0 -> 1. The result is that sometimes the line width is 1
pixel wider than expected.

Fix the problem by allowing VclPixelProcessor2D to request pixel
correction from BorderLinePrimitive2D. It wouldn't be possible to do
pixel correction only in VclPixelProcessor2D, as it has no idea what to
correct: it only gets polygons, so it has no idea if e.g. the top of a
polygon is the outer edge of a top border line or an inner edge of a
bottom border line.

Change-Id: I1971f3a952fbcdc598ab46c659e12d976c13cbe6
Reviewed-on: https://gerrit.libreoffice.org/24221
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

drawinglayer/CppunitTest_drawinglayer_border.mk
@@ -21,6 +21,9 @@ $(eval $(call gb_CppunitTest_use_libraries,drawinglayer_border, \
 	sal \
 	salhelper \
 	drawinglayer \
+	vcl \
+	test \
+	tl \
 	$(gb_UWINAPI) \
 ))
 
@@ -32,5 +35,18 @@ $(eval $(call gb_CppunitTest_use_externals,drawinglayer_border,\
 $(eval $(call gb_CppunitTest_add_exception_objects,drawinglayer_border, \
 	drawinglayer/qa/unit/border \
 ))
+
+$(eval $(call gb_CppunitTest_use_ure,drawinglayer_border))
+
+$(eval $(call gb_CppunitTest_use_vcl,drawinglayer_border))
+
+$(eval $(call gb_CppunitTest_use_components,drawinglayer_border,\
+    configmgr/source/configmgr \
+    i18npool/util/i18npool \
+    ucb/source/core/ucb1 \
+    ucb/source/ucp/file/ucpfile1 \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,drawinglayer_border))
 
 # vim: set noet sw=4 ts=4:

drawinglayer/source/primitive2d/borderlineprimitive2d.cxx
@@ -163,6 +163,11 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
 
         Primitive2DContainer BorderLinePrimitive2D::create2DDecomposition(const geometry::ViewInformation2D& rViewInformation) const
         {
+            return createDecomposition(rViewInformation, false);
+        }
+
+        Primitive2DContainer BorderLinePrimitive2D::createDecomposition(const geometry::ViewInformation2D& rViewInformation, bool bPixelCorrection) const
+        {
             Primitive2DContainer xRetval;
 
             if(!getStart().equal(getEnd()) && ( isInsideUsed() || isOutsideUsed() ) )
@@ -199,8 +204,11 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
                         xRetval[0] = makeHairLinePrimitive(
                             getStart(), getEnd(), aVector, getRGBColorLeft(), 0.0);
                     else
+                    {
+                        double fWidth = bPixelCorrection ? std::round(fLeftWidth) : fLeftWidth;
                         xRetval[0] = makeSolidLinePrimitive(
-                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorLeft(), fLeftWidth, -fLeftWidth/2.0);
+                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorLeft(), fWidth, -fLeftWidth/2.0);
+                    }
 
                     // "outside" line
 
@@ -208,8 +216,11 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
                         xRetval[1] = makeHairLinePrimitive(
                             getStart(), getEnd(), aVector, getRGBColorRight(), fLeftWidth+mfDistance);
                     else
+                    {
+                        double fWidth = bPixelCorrection ? std::round(fRightWidth) : fRightWidth;
                         xRetval[1] = makeSolidLinePrimitive(
-                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorRight(), fRightWidth, mfDistance+fRightWidth/2.0);
+                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorRight(), fWidth, mfDistance+fRightWidth/2.0);
+                    }
                 }
                 else
                 {

drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -1232,7 +1232,12 @@ namespace drawinglayer
                         static_cast<const drawinglayer::primitive2d::BorderLinePrimitive2D&>(rCandidate);
 
                     if (!tryDrawBorderLinePrimitive2DDirect(rBorder))
-                        process(rCandidate.get2DDecomposition(getViewInformation2D()));
+                    {
+                        if (rBorder.getStyle() == table::BorderLineStyle::DOUBLE)
+                            process(rBorder.createDecomposition(getViewInformation2D(), true));
+                        else
+                            process(rCandidate.get2DDecomposition(getViewInformation2D()));
+                    }
 
                     mpOutputDevice->SetAntialiasing(nAntiAliasing);
                     break;

include/drawinglayer/primitive2d/borderlineprimitive2d.hxx
@@ -131,6 +131,8 @@ namespace drawinglayer
             bool hasGapColor( ) const { return mbHasGapColor; }
             short getStyle () const { return mnStyle; }
             double getPatternScale() const { return mfPatternScale; }
+            /// Same as create2DDecomposition(), but can do pixel correction if requested.
+            Primitive2DContainer createDecomposition(const geometry::ViewInformation2D& rViewInformation, bool bPixelCorrection) const;
 
             /// compare operator
             virtual bool operator==(const BasePrimitive2D& rPrimitive) const override;

drawinglayer/qa/unit/border.cxx
@@ -8,7 +8,6 @@
  */
 
 #include <cppunit/TestAssert.h>
-#include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/plugin/TestPlugIn.h>
 
@@ -17,20 +16,27 @@
 #include <drawinglayer/geometry/viewinformation2d.hxx>
 #include <drawinglayer/primitive2d/borderlineprimitive2d.hxx>
 #include <drawinglayer/primitive2d/polypolygonprimitive2d.hxx>
+#include <drawinglayer/processor2d/baseprocessor2d.hxx>
+#include <drawinglayer/processor2d/processorfromoutputdevice.hxx>
 #include <rtl/ref.hxx>
+#include <test/bootstrapfixture.hxx>
+#include <vcl/vclptr.hxx>
+#include <vcl/virdev.hxx>
 
 using namespace com::sun::star;
 
 namespace
 {
 
-class DrawinglayerBorderTest : public CppUnit::TestFixture
+class DrawinglayerBorderTest : public test::BootstrapFixture
 {
 public:
     void testDoubleDecompositionSolid();
+    void testDoublePixelProcessing();
 
     CPPUNIT_TEST_SUITE(DrawinglayerBorderTest);
     CPPUNIT_TEST(testDoubleDecompositionSolid);
+    CPPUNIT_TEST(testDoublePixelProcessing);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -72,6 +78,67 @@ void DrawinglayerBorderTest::testDoubleDecompositionSolid()
     const basegfx::B2DRange& rRange = rPolygon.getB2DRange();
     // This was 2.47, i.e. the width of the inner line was 1 unit (in the bugdoc's case: 1 pixel) wider than expected.
     CPPUNIT_ASSERT_DOUBLES_EQUAL(fLeftWidth, rRange.getHeight(), basegfx::fTools::getSmallValue());
+}
+
+void DrawinglayerBorderTest::testDoublePixelProcessing()
+{
+    // Create a pixel processor.
+    ScopedVclPtrInstance<VirtualDevice> pDev;
+    drawinglayer::geometry::ViewInformation2D aView;
+    std::unique_ptr<drawinglayer::processor2d::BaseProcessor2D> pProcessor(drawinglayer::processor2d::createBaseProcessor2DFromOutputDevice(*pDev, aView));
+    CPPUNIT_ASSERT(pProcessor);
+    GDIMetaFile aMetaFile;
+    // Start recording after the processor is created, so we can test the pixel processor.
+    aMetaFile.Record(pDev);
+
+    // Create a border line primitive that's similar to the one from the bugdoc:
+    // 1.47 pixels is 0.03cm at 130% zoom and 96 DPI.
+    basegfx::B2DPoint aStart(0, 20);
+    basegfx::B2DPoint aEnd(100, 20);
+    double fLeftWidth = 1.47;
+    double fDistance = 1.47;
+    double fRightWidth = 1.47;
+    double fExtendLeftStart = 0;
+    double fExtendLeftEnd = 0;
+    double fExtendRightStart = 0;
+    double fExtendRightEnd = 0;
+    basegfx::BColor aColorRight;
+    basegfx::BColor aColorLeft;
+    basegfx::BColor aColorGap;
+    bool bHasGapColor = false;
+    sal_Int16 nStyle = table::BorderLineStyle::DOUBLE;
+    rtl::Reference<drawinglayer::primitive2d::BorderLinePrimitive2D> xBorder(new drawinglayer::primitive2d::BorderLinePrimitive2D(aStart, aEnd, fLeftWidth, fDistance, fRightWidth, fExtendLeftStart, fExtendLeftEnd, fExtendRightStart, fExtendRightEnd, aColorRight, aColorLeft, aColorGap, bHasGapColor, nStyle));
+    drawinglayer::primitive2d::Primitive2DContainer aPrimitives;
+    aPrimitives.push_back(drawinglayer::primitive2d::Primitive2DReference(xBorder.get()));
+
+    // Process the primitives.
+    pProcessor->process(aPrimitives);
+
+    // Now assert the height of the outer (second) border polygon.
+    aMetaFile.Stop();
+    aMetaFile.WindStart();
+    bool bFirst = true;
+    sal_Int32 nHeight = 0;
+    for(std::size_t nAction = 0; nAction < aMetaFile.GetActionSize(); ++nAction)
+    {
+        MetaAction* pAction = aMetaFile.GetAction(nAction);
+        if (pAction->GetType() == MetaActionType::POLYPOLYGON)
+        {
+            if (bFirst)
+            {
+                bFirst = false;
+                continue;
+            }
+
+            auto pMPPAction = static_cast<MetaPolyPolygonAction*>(pAction);
+            const tools::PolyPolygon& rPolyPolygon = pMPPAction->GetPolyPolygon();
+            nHeight = rPolyPolygon.GetBoundRect().getHeight();
+        }
+    }
+    sal_Int32 nExpectedHeight = std::round(fRightWidth);
+    // This was 2, and should be 1: if the logical requested width is 1.47,
+    // then that must be 1 px on the screen, not 2.
+    CPPUNIT_ASSERT_EQUAL(nExpectedHeight, nHeight);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(DrawinglayerBorderTest);