LemmingAvalanche/libreoffice-repaired

commit e5635aa5e37bb698b9e98c6a11399da917215201

fdo#51296 Patch accounts security setting requiring ctrl+click for hyperlinks.
Have used a boolean variable that acts as an entry condition to OpenURL
methods main body/code and is set on qualification of security check.

Change-Id: I1280e7cf7e8923282fda17088fd7c61fbf503b9d

sc/source/core/data/global.cxx
@@ -38,6 +38,7 @@
 #include <sal/macros.h>
 #include <tools/rcid.h>
 #include <unotools/charclass.hxx>
+#include <unotools/securityoptions.hxx>
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
@@ -924,13 +925,25 @@ void ScGlobal::OpenURL( const String& rURL, const String& rTarget )
 {
     //  OpenURL wird immer ueber irgendwelche Umwege durch Mausklicks im GridWindow
     //  aufgerufen, darum stimmen pScActiveViewShell und nScClickMouseModifier.
+    //SvtSecurityOptions to access Libreoffice global security parameters
+    SvtSecurityOptions aSecOpt;
+    bool bProceedHyperlink = false;
+    if ( (nScClickMouseModifier & KEY_MOD1) && aSecOpt.IsOptionSet( SvtSecurityOptions::E_CTRLCLICK_HYPERLINK ))     // control-click -> into new window
+    {
+        //Ctrl key is pressed and ctrl+click hyperlink security control is set
+        bProceedHyperlink = true;
+    }
+    else if( !aSecOpt.IsOptionSet( SvtSecurityOptions::E_CTRLCLICK_HYPERLINK ) )
+    {
+        //ctrl+click hyperlink security control is disabled just click will do
+        bProceedHyperlink = true;
+    }
+    if ( !bProceedHyperlink )
+        return;
 
     SfxStringItem aUrl( SID_FILE_NAME, rURL );
     SfxStringItem aTarget( SID_TARGETNAME, rTarget );
-
-    if ( nScClickMouseModifier & KEY_MOD1 )     // control-click -> into new window
-        aTarget.SetValue(rtl::OUString("_blank"));
-
+    aTarget.SetValue(rtl::OUString("_blank"));
     SfxViewFrame* pFrame = NULL;
     String aReferName;
     if ( pScActiveViewShell )

commit b49e027198357af6415e1027e4571a1ceffa3c7b

don't try to open in new window by default, tdf#70959
This fixes a regression from e5635aa5e37bb698b9e98c6a11399da917215201

Change-Id: Idcee34c97173fb79503a560f5246230a9bfaeb51

sc/source/core/data/global.cxx
@@ -901,7 +901,8 @@ void ScGlobal::OpenURL(const OUString& rURL, const OUString& rTarget, const SdrM
     }
     SfxStringItem aUrl( SID_FILE_NAME, rURL );
     SfxStringItem aTarget( SID_TARGETNAME, rTarget );
-    aTarget.SetValue("_blank");
+    if ( nScClickMouseModifier & KEY_SHIFT )     // control-click -> into new window
+        aTarget.SetValue("_blank");
     SfxViewFrame* pFrame = nullptr;
     OUString aReferName;
     if ( pScActiveViewShell )