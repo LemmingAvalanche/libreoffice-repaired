LemmingAvalanche/libreoffice-repaired

commit 1da46d960c1cd59ebb8bd1b85fa39008bcaf9d63

Remove Edit -> Plug-in as no longer used
This functionality seems to only have been used with NPAPI plugins.
They've been previously removed.  Per the help text, if it is still
used by anything, you can use the context menu instead.

MN_READONLY_PLUGINOFF also appeared to be unused, so removed.

More code might be removable, but would affect activate on click/visible.

Change-Id: I12d5bf54edd3a2f716912179b87e798b47b3cc3d
Reviewed-on: https://gerrit.libreoffice.org/24500
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

include/sfx2/viewsh.hxx
@@ -315,7 +315,7 @@ public:
     SAL_DLLPRIVATE void ExecPrint_Impl(SfxRequest &);
     SAL_DLLPRIVATE void ExecMisc_Impl(SfxRequest &);
     SAL_DLLPRIVATE void GetState_Impl(SfxItemSet&);
-    SAL_DLLPRIVATE void CheckIPClient_Impl( SfxInPlaceClient*, const Rectangle& );
+    SAL_DLLPRIVATE void CheckIPClient_Impl( SfxInPlaceClient* );
     SAL_DLLPRIVATE void PushSubShells_Impl( bool bPush=true );
     SAL_DLLPRIVATE void PopSubShells_Impl() { PushSubShells_Impl( false ); }
     SAL_DLLPRIVATE void TakeOwnership_Impl();

sfx2/source/view/ipclient.cxx
@@ -593,7 +593,7 @@ void SfxInPlaceClient_Impl::SizeHasChanged()
 IMPL_LINK_NOARG_TYPED(SfxInPlaceClient_Impl, TimerHdl, Timer *, void)
 {
     if ( m_pClient && m_xObject.is() )
-        m_pClient->GetViewShell()->CheckIPClient_Impl( m_pClient, m_pClient->GetViewShell()->GetObjectShell()->GetVisArea() );
+        m_pClient->GetViewShell()->CheckIPClient_Impl( m_pClient );
 }
 
 

sfx2/source/view/viewsh.cxx
@@ -242,7 +242,6 @@ SfxViewShell_Impl::SfxViewShell_Impl(SfxViewShellFlags const nFlags)
 ,   m_nPrinterLocks(0)
 ,   m_bCanPrint(nFlags & SfxViewShellFlags::CAN_PRINT)
 ,   m_bHasPrintOptions(nFlags & SfxViewShellFlags::HAS_PRINTOPTIONS)
-,   m_bPlugInsActive(true)
 ,   m_bIsShowView(!(nFlags & SfxViewShellFlags::NO_SHOW))
 ,   m_bGotOwnership(false)
 ,   m_bGotFrameOwnership(false)
@@ -680,70 +679,6 @@ void SfxViewShell::ExecMisc_Impl( SfxRequest &rReq )
                 return;
             }
         }
-
-        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        case SID_PLUGINS_ACTIVE:
-        {
-            const SfxBoolItem* pShowItem = rReq.GetArg<SfxBoolItem>(nId);
-            bool const bActive = (pShowItem)
-                ? pShowItem->GetValue()
-                : !pImp->m_bPlugInsActive;
-            // ggf. recorden
-            if ( !rReq.IsAPI() )
-                rReq.AppendItem( SfxBoolItem( nId, bActive ) );
-
-            // Jetzt schon DONE aufrufen, da die Argumente evtl. einen Pool
-            // benutzen, der demn"achst weg ist
-            rReq.Done(true);
-
-            // ausfuehren
-            if (!pShowItem || (bActive != pImp->m_bPlugInsActive))
-            {
-                SfxFrame* pTopFrame = &GetFrame()->GetTopFrame();
-                if ( pTopFrame != &GetFrame()->GetFrame() )
-                {
-                    // FramesetDocument
-                    SfxViewShell *pShell = pTopFrame->GetCurrentViewFrame()->GetViewShell();
-                    if ( pShell->GetInterface()->GetSlot( nId ) )
-                        pShell->ExecuteSlot( rReq );
-                    break;
-                }
-
-                SfxFrameIterator aIter( *pTopFrame );
-                while ( pTopFrame )
-                {
-                    if ( pTopFrame->GetCurrentViewFrame() )
-                    {
-                        SfxViewShell *pView = pTopFrame->GetCurrentViewFrame()->GetViewShell();
-                        if ( pView )
-                        {
-                            pView->pImp->m_bPlugInsActive = bActive;
-                            Rectangle aVisArea = GetObjectShell()->GetVisArea();
-                            VisAreaChanged(aVisArea);
-
-                            // the plugins might need change in their state
-                            SfxInPlaceClientList *pClients = pView->pImp->GetIPClientList_Impl(false);
-                            if ( pClients )
-                            {
-                                for ( size_t n = 0; n < pClients->size(); n++)
-                                {
-                                    SfxInPlaceClient* pIPClient = pClients->at( n );
-                                    if ( pIPClient )
-                                        pView->CheckIPClient_Impl( pIPClient, aVisArea );
-                                }
-                            }
-                        }
-                    }
-
-                    if ( !pTopFrame->GetParentFrame() )
-                        pTopFrame = aIter.FirstFrame();
-                    else
-                        pTopFrame = aIter.NextFrame( *pTopFrame );
-                }
-            }
-
-            break;
-        }
     }
 }
 
@@ -801,13 +736,6 @@ void SfxViewShell::GetState_Impl( SfxItemSet &rSet )
                         }
                     }
                 }
-                break;
-            }
-
-            // PlugIns running
-            case SID_PLUGINS_ACTIVE:
-            {
-                rSet.Put( SfxBoolItem( SID_PLUGINS_ACTIVE, !pImp->m_bPlugInsActive) );
                 break;
             }
             case SID_STYLE_FAMILY :
@@ -1175,11 +1103,6 @@ SfxViewShell::SfxViewShell
 ,   mbPrinterSettingsModified(false)
 {
 
-    if ( pViewFrame->GetParentViewFrame() )
-    {
-        pImp->m_bPlugInsActive = pViewFrame->GetParentViewFrame()
-            ->GetViewShell()->pImp->m_bPlugInsActive;
-    }
     SetMargin( pViewFrame->GetMargin_Impl() );
 
     SetPool( &pViewFrame->GetObjectShell()->GetPool() );
@@ -1641,7 +1564,7 @@ void SfxViewShell::VisAreaChanged(const Rectangle& /*rVisArea*/)
 }
 
 
-void SfxViewShell::CheckIPClient_Impl( SfxInPlaceClient *pIPClient, const Rectangle& rVisArea )
+void SfxViewShell::CheckIPClient_Impl( SfxInPlaceClient *pIPClient )
 {
     if ( GetObjectShell()->IsInClose() )
         return;
@@ -1651,32 +1574,11 @@ void SfxViewShell::CheckIPClient_Impl( SfxInPlaceClient *pIPClient, const Rectan
     bool bActiveWhenVisible =
         ( (( pIPClient->GetObjectMiscStatus() & embed::EmbedMisc::MS_EMBED_ACTIVATEWHENVISIBLE ) != 0 ) ||
          svt::EmbeddedObjectRef::IsGLChart(pIPClient->GetObject()));
-
-    // this method is called when either a client is created or the "Edit/Plugins" checkbox is checked
-    if ( !pIPClient->IsObjectInPlaceActive() && pImp->m_bPlugInsActive )
-    {
-        // object in client is currently not active
-        // check if the object wants to be activated always or when it becomes at least partially visible
-        // TODO/LATER: maybe we should use the scaled area instead of the ObjArea?!
-        if ( bAlwaysActive || (bActiveWhenVisible && rVisArea.IsOver(pIPClient->GetObjArea())) )
-        {
-            try
-            {
-                pIPClient->GetObject()->changeState( embed::EmbedStates::INPLACE_ACTIVE );
-            }
-            catch (const uno::Exception&)
-            {
-            }
-        }
-    }
-    else if (!pImp->m_bPlugInsActive)
-    {
-        // object in client is currently active and "Edit/Plugins" checkbox is selected
-        // check if the object wants to be activated always or when it becomes at least partially visible
-        // in this case selecting of the "Edit/Plugin" checkbox should let such objects deactivate
-        if ( bAlwaysActive || bActiveWhenVisible )
-            pIPClient->GetObject()->changeState( embed::EmbedStates::RUNNING );
-    }
+    // object in client is currently active
+    // check if the object wants to be activated always or when it becomes at least partially visible
+    // in this case selecting of the "Edit/Plugin" checkbox should let such objects deactivate
+    if ( bAlwaysActive || bActiveWhenVisible )
+        pIPClient->GetObject()->changeState( embed::EmbedStates::RUNNING );
 }
 
 

configmgr/qa/unit/data.xcd
@@ -2421,14 +2421,6 @@
           <value>1</value>
         </prop>
       </node>
-      <node oor:name=".uno:PlugInsActive" oor:op="replace">
-        <prop oor:name="Label" oor:type="xs:string">
-          <value xml:lang="x-no-translate"></value><value xml:lang="en-US">P~lug-in</value>
-        </prop>
-        <prop oor:name="Properties" oor:type="xs:int">
-          <value>1</value>
-        </prop>
-      </node>
       <node oor:name=".uno:HFixedLine" oor:op="replace">
         <prop oor:name="Label" oor:type="xs:string">
           <value xml:lang="x-no-translate"></value><value xml:lang="en-US">Horizontal Line</value>

include/sfx2/sfxsids.hrc
@@ -154,7 +154,7 @@
 #define SID_BOOKMARK_URL                    (SID_SFX_START + 1311)
 #define SID_EDITDOC                         (SID_SFX_START + 1312)
 #define SID_BROWSER_MODE                    (SID_SFX_START + 1313)
-#define SID_PLUGINS_ACTIVE                  (SID_SFX_START + 1314)
+// FREE (was SID_PLUGINS_ACTIVE)                  (SID_SFX_START + 1314)
 #define SID_BROWSER                         (SID_SFX_START + 1318)
 #define SID_MACROEXECMODE                   (SID_SFX_START + 1319)
 

officecfg/registry/data/org/openoffice/Office/UI/GenericCommands.xcu
@@ -2634,14 +2634,6 @@
           <value>1</value>
         </prop>
       </node>
-      <node oor:name=".uno:PlugInsActive" oor:op="replace">
-        <prop oor:name="Label" oor:type="xs:string">
-          <value xml:lang="en-US">Plug-in</value>
-        </prop>
-        <prop oor:name="Properties" oor:type="xs:int">
-          <value>1</value>
-        </prop>
-      </node>
       <node oor:name=".uno:HFixedLine" oor:op="replace">
         <prop oor:name="Label" oor:type="xs:string">
           <value xml:lang="en-US">Horizontal Line</value>

sc/uiconfig/scalc/menubar/menubar.xml
@@ -98,7 +98,6 @@
             <menu:menuitem menu:id=".uno:MergeDocuments"/>
             <menu:menuseparator/>
             <menu:menuitem menu:id=".uno:EditLinks"/>
-            <menu:menuitem menu:id=".uno:PlugInsActive"/>
             <menu:menuitem menu:id=".uno:ImageMapDialog"/>
             <menu:menuitem menu:id=".uno:ObjectMenue"/>
             <menu:menuseparator/>

sd/uiconfig/sdraw/menubar/menubar.xml
@@ -90,7 +90,6 @@
       <menu:menuitem menu:id=".uno:DeletePage"/>
       <menu:menuseparator/>
       <menu:menuitem menu:id=".uno:ManageLinks"/>
-      <menu:menuitem menu:id=".uno:PlugInsActive"/>
       <menu:menuitem menu:id=".uno:ImageMapDialog"/>
       <menu:menuitem menu:id=".uno:ObjectMenue"/>
       <menu:menuitem menu:id=".uno:EditHyperlink"/>

commit 9c67dec9f0043bee47c2576e45f6d7a456f48d40

sfx2: fix SfxViewShell::CheckIPClient_Impl()
Probably the Edit->Plug-in removal removed the wrong branch there since
m_bPlugInsActive was always true before.

This caused embded::UnreachableStateException to be thrown out of a
Timer and that was only handled as a FatalError in desktop::Main,
for example on kde84498-1.html.

(regression from 1da46d960c1cd59ebb8bd1b85fa39008bcaf9d63)

Change-Id: If9e2578f722ea5c5b0821d3d894bb439dbf3c40c

include/sfx2/viewsh.hxx
@@ -315,7 +315,7 @@ public:
     SAL_DLLPRIVATE void ExecPrint_Impl(SfxRequest &);
     SAL_DLLPRIVATE void ExecMisc_Impl(SfxRequest &);
     SAL_DLLPRIVATE void GetState_Impl(SfxItemSet&);
-    SAL_DLLPRIVATE void CheckIPClient_Impl( SfxInPlaceClient* );
+    SAL_DLLPRIVATE void CheckIPClient_Impl(SfxInPlaceClient*, const Rectangle&);
     SAL_DLLPRIVATE void PushSubShells_Impl( bool bPush=true );
     SAL_DLLPRIVATE void PopSubShells_Impl() { PushSubShells_Impl( false ); }
     SAL_DLLPRIVATE void TakeOwnership_Impl();

sfx2/source/view/ipclient.cxx
@@ -593,7 +593,10 @@ void SfxInPlaceClient_Impl::SizeHasChanged()
 IMPL_LINK_NOARG_TYPED(SfxInPlaceClient_Impl, TimerHdl, Timer *, void)
 {
     if ( m_pClient && m_xObject.is() )
-        m_pClient->GetViewShell()->CheckIPClient_Impl( m_pClient );
+    {
+        m_pClient->GetViewShell()->CheckIPClient_Impl(m_pClient,
+                m_pClient->GetViewShell()->GetObjectShell()->GetVisArea());
+    }
 }
 
 

sfx2/source/view/viewsh.cxx
@@ -1564,7 +1564,8 @@ void SfxViewShell::VisAreaChanged(const Rectangle& /*rVisArea*/)
 }
 
 
-void SfxViewShell::CheckIPClient_Impl( SfxInPlaceClient *pIPClient )
+void SfxViewShell::CheckIPClient_Impl(
+        SfxInPlaceClient *const pIPClient, const Rectangle& rVisArea)
 {
     if ( GetObjectShell()->IsInClose() )
         return;
@@ -1574,11 +1575,25 @@ void SfxViewShell::CheckIPClient_Impl( SfxInPlaceClient *pIPClient )
     bool bActiveWhenVisible =
         ( (( pIPClient->GetObjectMiscStatus() & embed::EmbedMisc::MS_EMBED_ACTIVATEWHENVISIBLE ) != 0 ) ||
          svt::EmbeddedObjectRef::IsGLChart(pIPClient->GetObject()));
-    // object in client is currently active
-    // check if the object wants to be activated always or when it becomes at least partially visible
-    // in this case selecting of the "Edit/Plugin" checkbox should let such objects deactivate
-    if ( bAlwaysActive || bActiveWhenVisible )
-        pIPClient->GetObject()->changeState( embed::EmbedStates::RUNNING );
+
+    // this method is called when a client is created
+    if (!pIPClient->IsObjectInPlaceActive())
+    {
+        // object in client is currently not active
+        // check if the object wants to be activated always or when it becomes at least partially visible
+        // TODO/LATER: maybe we should use the scaled area instead of the ObjArea?!
+        if (bAlwaysActive || (bActiveWhenVisible && rVisArea.IsOver(pIPClient->GetObjArea())))
+        {
+            try
+            {
+                pIPClient->GetObject()->changeState( embed::EmbedStates::INPLACE_ACTIVE );
+            }
+            catch (const uno::Exception& e)
+            {
+                SAL_WARN("sfx.view", "SfxViewShell::CheckIPClient_Impl exception: " << e.Message);
+            }
+        }
+    }
 }
 
 