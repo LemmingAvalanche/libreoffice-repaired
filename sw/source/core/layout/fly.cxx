LemmingAvalanche/libreoffice-repaired

commit 03d6298760fc85d2b1b85b73063038d3f8e5204e

coverity#704901 Dereference after null check
Change-Id: I3e2ad2d3dd1713336b1a01cb7a2b1ff28bb833ea

sw/source/core/layout/fly.cxx
@@ -805,6 +805,7 @@ void SwFlyFrm::_UpdateAttr( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
             }
 
         case RES_COL:
+            if (pOld && pNew)
             {
                 ChgColumns( *(const SwFmtCol*)pOld, *(const SwFmtCol*)pNew );
                 const SwFmtFrmSize &rNew = GetFmt()->GetFrmSize();

commit 480689e6c1054fccf82ac2670d663e07ec2e72d4

Missing break in switch
caused by 03d6298760fc85d2b1b85b73063038d3f8e5204e "coverity#704901 Dereference
after null check"

Change-Id: I7e53493bcb2857ccc840429cd2fd2e802180aa19

sw/source/core/layout/fly.cxx
@@ -818,8 +818,8 @@ void SwFlyFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
                 if ( FrameSizeChg( rNew ) )
                     NotifyDrawObj();
                 rInvFlags |= 0x1A;
-                break;
             }
+            break;
 
         case RES_FRM_SIZE:
         case RES_FMT_CHG: