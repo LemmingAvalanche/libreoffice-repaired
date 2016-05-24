LemmingAvalanche/libreoffice-repaired

commit 236d9c74b2564bc1cc9ff6d0c5ac8ebac8c5cb4f

clang-tidy modernize-loop-convert in toolkit to uui
Change-Id: I805aa1389ef8dde158f0b776d6b59579fa3082e4
Reviewed-on: https://gerrit.libreoffice.org/24921
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

unotools/source/config/options.cxx
@@ -68,8 +68,8 @@ void ConfigurationBroadcaster::NotifyListeners( sal_uInt32 nHint )
         nHint |= m_nBlockedHint;
         m_nBlockedHint = 0;
         if ( mpList ) {
-            for ( size_t n = 0; n < mpList->size(); n++ ) {
-                (*mpList)[ n ]->ConfigurationChanged( this, nHint );
+            for (ConfigurationListener* n : *mpList) {
+                n->ConfigurationChanged( this, nHint );
             }
         }
     }

toolkit/source/awt/vclxtoolkit.cxx
@@ -1787,10 +1787,10 @@ void VCLXToolkit::callTopWindowListeners(
         {
             css::lang::EventObject aAwtEvent(
                 static_cast< css::awt::XWindow * >(pWindow->GetWindowPeer()));
-            for (::sal_Int32 i = 0; i < (sal_Int32)aListeners.size(); ++i)
+            for (css::uno::Reference<XInterface> & i : aListeners)
             {
                 css::uno::Reference< css::awt::XTopWindowListener >
-                      xListener(aListeners[i], css::uno::UNO_QUERY);
+                      xListener(i, css::uno::UNO_QUERY);
                 try
                 {
                     (xListener.get()->*pFn)(aAwtEvent);
@@ -1833,10 +1833,10 @@ bool VCLXToolkit::callKeyHandlers(::VclSimpleEvent const * pEvent,
             pKeyEvent->GetKeyCode().GetCode(), pKeyEvent->GetCharCode(),
             sal::static_int_cast< sal_Int16 >(
                 pKeyEvent->GetKeyCode().GetFunction()));
-        for (::sal_Int32 i = 0; i < (sal_Int32)aHandlers.size(); ++i)
+        for (css::uno::Reference<XInterface> & i : aHandlers)
         {
             css::uno::Reference< css::awt::XKeyHandler > xHandler(
-                aHandlers[i], css::uno::UNO_QUERY);
+                i, css::uno::UNO_QUERY);
             try
             {
                 if ((bPressed ? xHandler->keyPressed(aAwtEvent)
@@ -1883,10 +1883,10 @@ void VCLXToolkit::callFocusListeners(::VclSimpleEvent const * pEvent,
                 static_cast< css::awt::XWindow * >(pWindow->GetWindowPeer()),
                 static_cast<sal_Int16>(pWindow->GetGetFocusFlags()),
                 xNext, false);
-            for (::sal_Int32 i = 0; i < (sal_Int32)aListeners.size(); ++i)
+            for (css::uno::Reference<XInterface> & i : aListeners)
             {
                 css::uno::Reference< css::awt::XFocusListener > xListener(
-                    aListeners[i], css::uno::UNO_QUERY);
+                    i, css::uno::UNO_QUERY);
                 try
                 {
                     bGained ? xListener->focusGained(aAwtEvent)

toolkit/source/controls/unocontrol.cxx
@@ -586,9 +586,9 @@ void UnoControl::ImplModelPropertiesChanged( const Sequence< PropertyChangeEvent
             while ( pLangDepProp->pPropName != nullptr )
             {
                 bool bMustBeInserted( true );
-                for ( size_t i = 0; i < aPeerPropertiesToSet.size(); i++ )
+                for (PropertyValue & i : aPeerPropertiesToSet)
                 {
-                    if ( aPeerPropertiesToSet[i].Name.equalsAsciiL(
+                    if ( i.Name.equalsAsciiL(
                             pLangDepProp->pPropName, pLangDepProp->nPropNameLength ))
                     {
                         bMustBeInserted = false;

toolkit/source/helper/unowrapper.cxx
@@ -197,9 +197,8 @@ void UnoWrapper::ReleaseAllGraphics( OutputDevice* pOutDev )
     std::vector< VCLXGraphics* > *pLst = pOutDev->GetUnoGraphicsList();
     if ( pLst )
     {
-        for ( size_t n = 0; n < pLst->size(); n++ )
+        for (VCLXGraphics* pGrf : *pLst)
         {
-            VCLXGraphics* pGrf = (*pLst)[ n ];
             pGrf->SetOutputDevice( nullptr );
         }
     }

toolkit/source/helper/vclunohelper.cxx
@@ -544,19 +544,19 @@ namespace
             { FUNIT_FOOT,       MeasureUnit::FOOT,          1 },
             { FUNIT_MILE,       MeasureUnit::MILE,          1 },
         };
-        for ( size_t i = 0; i < SAL_N_ELEMENTS( aUnits ); ++i )
+        for (auto & aUnit : aUnits)
         {
             if ( eDirection == FieldUnitToMeasurementUnit )
             {
-                if ( ( aUnits[ i ].eFieldUnit == (FieldUnit)_nUnit ) && ( aUnits[ i ].nFieldToMeasureFactor == _rFieldToUNOValueFactor ) )
-                    return aUnits[ i ].nMeasurementUnit;
+                if ( ( aUnit.eFieldUnit == (FieldUnit)_nUnit ) && ( aUnit.nFieldToMeasureFactor == _rFieldToUNOValueFactor ) )
+                    return aUnit.nMeasurementUnit;
             }
             else
             {
-                if ( aUnits[ i ].nMeasurementUnit == _nUnit )
+                if ( aUnit.nMeasurementUnit == _nUnit )
                 {
-                    _rFieldToUNOValueFactor = aUnits[ i ].nFieldToMeasureFactor;
-                    return (sal_Int16)aUnits[ i ].eFieldUnit;
+                    _rFieldToUNOValueFactor = aUnit.nFieldToMeasureFactor;
+                    return (sal_Int16)aUnit.eFieldUnit;
                 }
             }
         }

tools/source/inet/inetmime.cxx
@@ -2224,9 +2224,9 @@ static EncodingEntry const aEncodingMap[]
 rtl_TextEncoding getCharsetEncoding(sal_Char const * pBegin,
                                               sal_Char const * pEnd)
 {
-    for (sal_Size i = 0; i < SAL_N_ELEMENTS(aEncodingMap); ++i)
-        if (equalIgnoreCase(pBegin, pEnd, aEncodingMap[i].m_aName))
-            return aEncodingMap[i].m_eEncoding;
+    for (const EncodingEntry& i : aEncodingMap)
+        if (equalIgnoreCase(pBegin, pEnd, i.m_aName))
+            return i.m_eEncoding;
     return RTL_TEXTENCODING_DONTKNOW;
 }
 

tools/source/memtools/multisel.cxx
@@ -27,8 +27,8 @@ void MultiSelection::ImplClear()
     // no selected indexes
     nSelCount = 0;
 
-    for ( size_t i = 0, n = aSels.size(); i < n; ++i ) {
-        delete aSels[ i ];
+    for (Range* pSel : aSels) {
+        delete pSel;
     }
     aSels.clear();
 }
@@ -92,8 +92,8 @@ MultiSelection::MultiSelection( const MultiSelection& rOrig ) :
     }
 
     // copy the sub selections
-    for ( size_t n = 0; n < rOrig.aSels.size(); ++n )
-        aSels.push_back( new Range( *rOrig.aSels[ n ] ) );
+    for (const Range* pSel : rOrig.aSels)
+        aSels.push_back( new Range( *pSel ) );
 }
 
 MultiSelection::MultiSelection( const Range& rRange ):
@@ -109,8 +109,8 @@ MultiSelection::MultiSelection( const Range& rRange ):
 
 MultiSelection::~MultiSelection()
 {
-    for ( size_t i = 0, n = aSels.size(); i < n; ++i )
-        delete aSels[ i ];
+    for (Range* pSel : aSels)
+        delete pSel;
     aSels.clear();
 }
 
@@ -126,8 +126,8 @@ MultiSelection& MultiSelection::operator= ( const MultiSelection& rOrig )
 
     // clear the old and copy the sub selections
     ImplClear();
-    for ( size_t n = 0; n < rOrig.aSels.size(); ++n )
-        aSels.push_back( new Range( *rOrig.aSels[ n ] ) );
+    for (const Range* pSel : rOrig.aSels)
+        aSels.push_back( new Range( *pSel ) );
     nSelCount = rOrig.nSelCount;
 
     return *this;
@@ -527,8 +527,8 @@ void MultiSelection::SetTotalRange( const Range& rTotRange )
 
     // re-calculate selection count
     nSelCount = 0;
-    for ( size_t i = 0, n = aSels.size(); i < n; ++ i )
-        nSelCount += aSels[i]->Len();
+    for (Range* pSel : aSels)
+        nSelCount += pSel->Len();
 
     bCurValid = false;
     nCurIndex = 0;

tools/source/ref/errinf.cxx
@@ -63,8 +63,8 @@ EDcrData::EDcrData()
     , bIsWindowDsp(false)
     , nNextDcr(0)
 {
-    for(sal_uInt16 n=0;n<ERRCODE_DYNAMIC_COUNT;n++)
-        ppDcr[n]=nullptr;
+    for(DynamicErrorInfo*& rp : ppDcr)
+        rp = nullptr;
 }
 
 void DynamicErrorInfo_Impl::RegisterEDcr(DynamicErrorInfo *pDcr)

commit 5ae2e6f043d94cd74448700948d134e9b92660ae

tdf#99973 - Crash when changing Locale Setting in options
regression from commit 236d9c74 "clang-tidy modernize-loop-convert in
toolkit to uui", probably because the list is being modified while the
loop is executing

Change-Id: I18cd21001aee132e06d577c469aed5eb46a16a7f
Reviewed-on: https://gerrit.libreoffice.org/25368
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

unotools/source/config/options.cxx
@@ -68,9 +68,8 @@ void ConfigurationBroadcaster::NotifyListeners( sal_uInt32 nHint )
         nHint |= m_nBlockedHint;
         m_nBlockedHint = 0;
         if ( mpList ) {
-            for (ConfigurationListener* n : *mpList) {
-                n->ConfigurationChanged( this, nHint );
-            }
+            for ( size_t n = 0; n < mpList->size(); n++ )
+                (*mpList)[ n ]->ConfigurationChanged( this, nHint );
         }
     }
 }