LemmingAvalanche/libreoffice-repaired

commit 66caed685129c9e2a05a0478be2b1c9f8002d9c1

cppcheck:variableScope
Change-Id: I9b671637fbe0f32e695d22b74bfb40a39a9fe884
Reviewed-on: https://gerrit.libreoffice.org/19364
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

sw/source/core/layout/frmtool.cxx
@@ -1248,8 +1248,6 @@ void _InsertCnt( SwLayoutFrm *pLay, SwDoc *pDoc,
     SwPageFrm *pPage = pLay->FindPageFrm();
     const SwFrameFormats *pTable = pDoc->GetSpzFrameFormats();
     SwFrm       *pFrm = 0;
-    bool   bBreakAfter   = false;
-
     SwActualSection *pActualSection = 0;
     SwLayHelper *pPageMaker;
 
@@ -1260,6 +1258,7 @@ void _InsertCnt( SwLayoutFrm *pLay, SwDoc *pDoc,
     {
         // Attention: the SwLayHelper class uses references to the content-,
         // page-, layout-frame etc. and may change them!
+        bool   bBreakAfter   = false;
         pPageMaker = new SwLayHelper( pDoc, pFrm, pPrv, pPage, pLay,
                 pActualSection, bBreakAfter, nIndex, 0 == nEndIndex );
         if( bStartPercent )

sw/source/core/doc/doccomp.cxx
@@ -1699,10 +1699,10 @@ void CompareData::SetRedlinesToDoc( bool bUseDocInfo )
         // combine consecutive
         if( pTmp->GetNext() != pInsRing )
         {
-            const SwContentNode* pCNd;
             do {
                 SwPosition& rSttEnd = *pTmp->End(),
                           & rEndStt = *(pTmp->GetNext())->Start();
+                const SwContentNode* pCNd;
                 if( rSttEnd == rEndStt ||
                     (!rEndStt.nContent.GetIndex() &&
                     rEndStt.nNode.GetIndex() - 1 == rSttEnd.nNode.GetIndex() &&

sw/source/core/doc/docedt.cxx
@@ -131,13 +131,12 @@ void _SaveFlyInRange( const SwPaM& rPam, const SwNodeIndex& rInsPos,
                 pPos->nContent == rEndNdIdx.GetNode().GetContentNode()->Len() ))
                     ? 0 : 1;
 
-    const SwNodeIndex* pContentIdx;
-
     for( SwFrameFormats::size_type n = 0; n < rFormats.size(); ++n )
     {
         pFormat = rFormats[n];
         pAnchor = &pFormat->GetAnchor();
         const SwPosition* pAPos = pAnchor->GetContentAnchor();
+        const SwNodeIndex* pContentIdx;
         if (pAPos &&
             ((FLY_AT_PARA == pAnchor->GetAnchorId()) ||
              (FLY_AT_CHAR == pAnchor->GetAnchorId())) &&

sw/source/core/layout/calcmove.cxx
@@ -1093,7 +1093,6 @@ void SwContentFrm::MakeAll(vcl::RenderContext* /*pRenderContext*/)
                                             // this flag is set. If it turns out that it
                                             // didn't keep it's promise, we can act in a
                                             // controlled fashion.
-    bool bMoveable;
     const bool bFly = IsInFly();
     const bool bTab = IsInTab();
     const bool bFootnote = IsInFootnote();
@@ -1196,6 +1195,8 @@ void SwContentFrm::MakeAll(vcl::RenderContext* /*pRenderContext*/)
     }
 
     SWRECTFN( this )
+
+    bool bMoveable;
 
     while ( !mbValidPos || !mbValidSize || !mbValidPrtArea )
     {

sw/source/core/layout/fly.cxx
@@ -1496,10 +1496,10 @@ void CalcContent( SwLayoutFrm *pLay,
                                 pTmpPrevFlowFrm->IsKeepFwdMoveAllowed();
 
             // format floating screen objects anchored to the frame.
-            bool bRestartLayoutProcess = false;
             if ( !bPrevInvalid && pFrm->GetDrawObjs() && pLay->IsAnLower( pFrm ) )
             {
                 bool bAgain = false;
+                bool bRestartLayoutProcess = false;
                 SwPageFrm* pPageFrm = pFrm->FindPageFrm();
                 size_t nCnt = pFrm->GetDrawObjs()->size();
                 size_t i = 0;

sw/source/core/table/swnewtable.cxx
@@ -381,14 +381,13 @@ SwBoxSelection* SwTable::CollectBoxSelection( const SwPaM& rPam ) const
         SwTableLine* pLine = aLines[nRow];
         OSL_ENSURE( pLine, "Missing table line" );
         SwSelBoxes *pBoxes = new SwSelBoxes();
-        long nLeft = 0;
         long nRight = 0;
         const size_t nCount = pLine->GetTabBoxes().size();
         for( size_t nCurrBox = 0; nCurrBox < nCount; ++nCurrBox )
         {
             SwTableBox* pBox = pLine->GetTabBoxes()[nCurrBox];
             OSL_ENSURE( pBox, "Missing table box" );
-            nLeft = nRight;
+            long nLeft = nRight;
             nRight += pBox->GetFrameFormat()->GetFrmSize().GetWidth();
             long nRowSpan = pBox->getRowSpan();
             if( nRight <= nMin )

sw/source/core/text/txtdrop.cxx
@@ -678,7 +678,6 @@ void SwDropCapCache::CalcFontSize( SwDropPortion* pDrop, SwTextFormatInfo &rInf 
     sal_Int32 nIdx = rInf.GetIdx();
     OUString aStr(rInf.GetText().copy(nIdx, pCurrPart->GetLen()));
 
-    long nAscent = 0;
     long nDescent = 0;
     long nFactor = -1;
 
@@ -707,6 +706,7 @@ void SwDropCapCache::CalcFontSize( SwDropPortion* pDrop, SwTextFormatInfo &rInf 
         nTmpIdx = nIndex;
 
         long nWishedHeight = pDrop->GetDropHeight();
+        long nAscent = 0;
 
         // find out biggest font size for initial scaling factor
         long nMaxFontHeight = 1;

sw/source/core/txtnode/thints.cxx
@@ -3111,10 +3111,10 @@ bool SwpHints::TryInsertHint(
         {
             // search for a reference with the same name
             SwTextAttr* pTmpHt;
-            sal_Int32 *pTmpHtEnd;
-            sal_Int32 *pTmpHintEnd;
             for( size_t n = 0, nEnd = Count(); n < nEnd; ++n )
             {
+                sal_Int32 *pTmpHtEnd;
+                sal_Int32 *pTmpHintEnd;
                 if (RES_TXTATR_REFMARK == (pTmpHt = Get(n))->Which() &&
                     pHint->GetAttr() == pTmpHt->GetAttr() &&
                     0 != ( pTmpHtEnd = pTmpHt->GetEnd() ) &&

commit 0e5a577dfa38085c9f9f1b8c3b318277e875d3cd

tdf#96089 sw: fix scope of bBreakAfter in InsertCnt_()
The problem is that bBreakAfter is passed by reference to SwLayHelper
and stored as a reference member there, so it has to live at least as
long as pPageMaker.  (Unfortunately C++ can't statically check that.)

This then somehow caused the number of pages created after initial load
to be 812 instead of the correct 396 determined from the layout-cache in
the bugdoc, and that then caused Drawing objects to move backward during
the following re-pagination, and then SwDrawContact::Changed_() calls
SetFlyFrmAttr() and that sets the document to modified, which triggers the
AutoSave that was reported in the bug.

(regression from 66caed685129c9e2a05a0478be2b1c9f8002d9c1)

Change-Id: I14dc4644c2e127b3c3ff0e6876eedcc534e1a68e

sw/source/core/layout/frmtool.cxx
@@ -1246,6 +1246,7 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
     SwPageFrame *pPage = pLay->FindPageFrame();
     const SwFrameFormats *pTable = pDoc->GetSpzFrameFormats();
     SwFrame       *pFrame = nullptr;
+    bool bBreakAfter = false;
     SwActualSection *pActualSection = nullptr;
     SwLayHelper *pPageMaker;
 
@@ -1256,7 +1257,6 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
     {
         // Attention: the SwLayHelper class uses references to the content-,
         // page-, layout-frame etc. and may change them!
-        bool   bBreakAfter   = false;
         pPageMaker = new SwLayHelper( pDoc, pFrame, pPrv, pPage, pLay,
                 pActualSection, bBreakAfter, nIndex, 0 == nEndIndex );
         if( bStartPercent )