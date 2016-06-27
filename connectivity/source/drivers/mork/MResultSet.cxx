LemmingAvalanche/libreoffice-repaired

commit 590e0561a2cbd85455561999f7ad788037c703fa

clang-tidy modernize-loop-convert in c*
Change-Id: I77d2548f8be97792660761e6156cd24734a95aaf

connectivity/source/drivers/mork/MResultSet.cxx
@@ -1248,12 +1248,12 @@ void SAL_CALL OResultSet::executeQuery() throw( ::com::sun::star::sdbc::SQLExcep
                 {
                     OValueRow aSearchRow = new OValueVector( m_aRow->get().size() );
 
-                    for( OKeySet::Vector::size_type i = 0; i < m_pKeySet->get().size(); i++ )
+                    for(sal_Int32 i : m_pKeySet->get())
                     {
-                        fetchRow( (m_pKeySet->get())[i] );        // Fills m_aRow
+                        fetchRow( i );        // Fills m_aRow
                         if ( matchRow( m_aRow, aSearchRow ) )
                         {
-                            (m_pKeySet->get())[i] = 0;   // Marker for later to be removed
+                            i = 0;   // Marker for later to be removed
                         }
                         else
                         {

canvas/source/cairo/cairo_textlayout.cxx
@@ -512,10 +512,10 @@ namespace cairocanvas
                 // loop to draw the text for every half pixel of displacement
                 for (int nSteps = 0; nSteps < total_steps; nSteps++)
                 {
-                    for(int nGlyphIdx = 0; nGlyphIdx < (int) cairo_glyphs.size(); nGlyphIdx++)
+                    for(cairo_glyph_t & cairo_glyph : cairo_glyphs)
                     {
-                        cairo_glyphs[nGlyphIdx].x += (bold_dx * nSteps / total_steps) / 4;
-                        cairo_glyphs[nGlyphIdx].y -= (bold_dx * nSteps / total_steps) / 4;
+                        cairo_glyph.x += (bold_dx * nSteps / total_steps) / 4;
+                        cairo_glyph.y -= (bold_dx * nSteps / total_steps) / 4;
                     }
                     cairo_show_glyphs(pSCairo.get(), &cairo_glyphs[0], cairo_glyphs.size());
                 }

canvas/source/opengl/ogl_canvastools.cxx
@@ -113,14 +113,14 @@ namespace oglcanvas
         aScaleShear.shearX(-0.1);
         aScaleShear.scale(scale,scale);
 
-        for( size_t i=0; i<rNumbers.size(); ++i )
+        for(double rNumber : rNumbers)
         {
             aTmp.identity();
             aTmp.translate(0,y);
             y += 1.2*scale;
 
             basegfx::B2DPolyPolygon aPoly=
-                basegfx::tools::number2PolyPolygon(rNumbers[i],10,3);
+                basegfx::tools::number2PolyPolygon(rNumber,10,3);
 
             aTmp=aTmp*aScaleShear;
             aPoly.transform(aTmp);

chart2/source/controller/dialogs/tp_SeriesToAxis.cxx
@@ -199,9 +199,8 @@ void SchOptionTabPage::Reset(const SfxItemSet* rInAttrs)
             m_pRB_AssumeZero->Enable(false);
             m_pRB_ContinueLine->Enable(false);
 
-            for( size_t nN =0; nN<aMissingValueTreatments.size(); nN++ )
+            for(int nVal : aMissingValueTreatments)
             {
-                sal_Int32 nVal = aMissingValueTreatments[nN];
                 if(nVal==css::chart::MissingValueTreatment::LEAVE_GAP)
                     m_pRB_DontPaint->Enable();
                 else if(nVal==css::chart::MissingValueTreatment::USE_ZERO)

chart2/source/controller/sidebar/ChartAxisPanel.cxx
@@ -100,10 +100,10 @@ sal_Int32 getLabelPosition(const css::uno::Reference<css::frame::XModel>& xModel
 
     css::chart::ChartAxisLabelPosition ePos;
     aAny >>= ePos;
-    for (size_t i = 0; i < SAL_N_ELEMENTS(aLabelPosMap); ++i)
+    for (AxisLabelPosMap & i : aLabelPosMap)
     {
-        if (aLabelPosMap[i].ePos == ePos)
-            return aLabelPosMap[i].nPos;
+        if (i.ePos == ePos)
+            return i.nPos;
     }
 
     return 0;
@@ -119,10 +119,10 @@ void setLabelPosition(const css::uno::Reference<css::frame::XModel>& xModel,
         return;
 
     css::chart::ChartAxisLabelPosition ePos;
-    for (size_t i = 0; i < SAL_N_ELEMENTS(aLabelPosMap); ++i)
+    for (AxisLabelPosMap & i : aLabelPosMap)
     {
-        if (aLabelPosMap[i].nPos == nPos)
-            ePos = aLabelPosMap[i].ePos;
+        if (i.nPos == nPos)
+            ePos = i.ePos;
     }
 
     xAxis->setPropertyValue("LabelPosition", css::uno::makeAny(ePos));

chart2/source/controller/sidebar/ChartErrorBarPanel.cxx
@@ -150,10 +150,10 @@ sal_Int32 getTypePos(const css::uno::Reference<css::frame::XModel>& xModel,
     sal_Int32 nApi = 0;
     aAny >>= nApi;
 
-    for (size_t i = 0; i < SAL_N_ELEMENTS(aErrorBarType); ++i)
+    for (ErrorBarTypeMap & i : aErrorBarType)
     {
-        if (aErrorBarType[i].nApi == nApi)
-            return aErrorBarType[i].nPos;
+        if (i.nApi == nApi)
+            return i.nPos;
     }
 
     return 0;
@@ -169,10 +169,10 @@ void setTypePos(const css::uno::Reference<css::frame::XModel>& xModel,
         return;
 
     sal_Int32 nApi = 0;
-    for (size_t i = 0; i < SAL_N_ELEMENTS(aErrorBarType); ++i)
+    for (ErrorBarTypeMap & i : aErrorBarType)
     {
-        if (aErrorBarType[i].nPos == nPos)
-            nApi = aErrorBarType[i].nApi;
+        if (i.nPos == nPos)
+            nApi = i.nApi;
     }
 
     xPropSet->setPropertyValue("ErrorBarStyle", css::uno::makeAny(nApi));

chart2/source/controller/sidebar/ChartSeriesPanel.cxx
@@ -110,10 +110,10 @@ sal_Int32 getDataLabelPlacement(const css::uno::Reference<css::frame::XModel>& x
     sal_Int32 nPlacement = 0;
     aAny >>= nPlacement;
 
-    for (size_t i = 0; i < SAL_N_ELEMENTS(aLabelPlacementMap); ++i)
+    for (LabelPlacementMap & i : aLabelPlacementMap)
     {
-        if (aLabelPlacementMap[i].nApi == nPlacement)
-            return aLabelPlacementMap[i].nPos;
+        if (i.nApi == nPlacement)
+            return i.nPos;
     }
 
     return 0;
@@ -129,11 +129,11 @@ void setDataLabelPlacement(const css::uno::Reference<css::frame::XModel>& xModel
         return;
 
     sal_Int32 nApi = 0;
-    for (size_t i = 0; i < SAL_N_ELEMENTS(aLabelPlacementMap); ++i)
+    for (LabelPlacementMap & i : aLabelPlacementMap)
     {
-        if (aLabelPlacementMap[i].nPos == nPos)
+        if (i.nPos == nPos)
         {
-            nApi = aLabelPlacementMap[i].nApi;
+            nApi = i.nApi;
             break;
         }
     }

chart2/source/model/main/BaseCoordinateSystem.cxx
@@ -184,8 +184,8 @@ BaseCoordinateSystem::~BaseCoordinateSystem()
 {
     try
     {
-        for( tAxisVecVecType::size_type nN=0; nN<m_aAllAxis.size(); nN++ )
-            ModifyListenerHelper::removeListenerFromAllElements( m_aAllAxis[nN], m_xModifyEventForwarder );
+        for(tAxisVecVecType::value_type & m_aAllAxi : m_aAllAxis)
+            ModifyListenerHelper::removeListenerFromAllElements( m_aAllAxi, m_xModifyEventForwarder );
         ModifyListenerHelper::removeListenerFromAllElements( m_aChartTypes, m_xModifyEventForwarder );
     }
     catch( const uno::Exception & ex )

commit 7c42517a8f0bfaecdf3239d0a95c6475f8cea014

clang-analyzer-deadcode.DeadStores
regression introduced by 590e0561a2cbd85455561999f7ad788037c703fa "clang-tidy
modernize-loop-convert in c*"

Change-Id: I2f1fcb324f9bf7e97a8a514f41ac1208f1f1ca74

connectivity/source/drivers/mork/MResultSet.cxx
@@ -1248,7 +1248,7 @@ void SAL_CALL OResultSet::executeQuery() throw( css::sdbc::SQLException,
                 {
                     OValueRow aSearchRow = new OValueVector( m_aRow->get().size() );
 
-                    for(sal_Int32 i : m_pKeySet->get())
+                    for(sal_Int32 & i : m_pKeySet->get())
                     {
                         fetchRow( i );        // Fills m_aRow
                         if ( matchRow( m_aRow, aSearchRow ) )