LemmingAvalanche/libreoffice-repaired

commit f08dcd616ddc45df515c4fae07e0fb9face02a2d

keep disable image at same depth as original
that way the "preferred" image format for a platform
for icons etc remains sticky

Change-Id: Ia76af0b7c4f27650038cab214b6406b8ef1a5fc4

vcl/source/bitmap/BitmapProcessor.cxx
@@ -59,7 +59,7 @@ BitmapEx BitmapProcessor::createDisabledImage(const BitmapEx& rBitmapEx)
 {
     const Size aSize(rBitmapEx.GetSizePixel());
 
-    Bitmap aGrey(aSize, 8, &Bitmap::GetGreyPalette(256));
+    Bitmap aGrey(aSize, rBitmapEx.GetBitCount());
     AlphaMask aGreyAlpha(aSize);
 
     Bitmap aBitmap(rBitmapEx.GetBitmap());
@@ -77,14 +77,14 @@ BitmapEx BitmapProcessor::createDisabledImage(const BitmapEx& rBitmapEx)
 
         if (pRead && pReadAlpha && pGrey && pGreyAlpha)
         {
-            BitmapColor aGreyValue(0);
             BitmapColor aGreyAlphaValue(0);
 
             for (long nY = 0; nY < aSize.Height(); ++nY)
             {
                 for (long nX = 0; nX < aSize.Width(); ++nX)
                 {
-                    aGreyValue.SetIndex(pRead->GetLuminance(nY, nX));
+                    const sal_uInt8 nLum(pRead->GetLuminance(nY, nX));
+                    BitmapColor aGreyValue(nLum, nLum, nLum);
                     pGrey->SetPixel(nY, nX, aGreyValue);
 
                     const BitmapColor aBitmapAlphaValue(pReadAlpha->GetPixel(nY, nX));
@@ -101,14 +101,14 @@ BitmapEx BitmapProcessor::createDisabledImage(const BitmapEx& rBitmapEx)
     {
         if (pRead && pGrey && pGreyAlpha)
         {
-            BitmapColor aGreyValue(0);
             BitmapColor aGreyAlphaValue(0);
 
             for (long nY = 0; nY < aSize.Height(); ++nY)
             {
                 for (long nX = 0; nX < aSize.Width(); ++nX)
                 {
-                    aGreyValue.SetIndex(pRead->GetLuminance(nY, nX));
+                    const sal_uInt8 nLum(pRead->GetLuminance(nY, nX));
+                    BitmapColor aGreyValue(nLum, nLum, nLum);
                     pGrey->SetPixel(nY, nX, aGreyValue);
 
                     aGreyAlphaValue.SetIndex(128);

commit 8e6f78e137d16f7ecf71fec057017b3abe62b1fa

making a disabled image from a 1 bit depth icon gives index asserts
cause the disabled color cannot be mapped to the 2 entries in the
palette, so use 8bit dests if the src is 1bit

regression from

commit f08dcd616ddc45df515c4fae07e0fb9face02a2d
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Tue Dec 1 12:18:57 2015 +0000

    keep disable image at same depth as original

seen when clicking on the image loaded from tdf#100632

Change-Id: Ic69bccfa0fc86707b4fd9e757d8374d80e4b0071

vcl/source/bitmap/BitmapProcessor.cxx
@@ -59,7 +59,14 @@ BitmapEx BitmapProcessor::createDisabledImage(const BitmapEx& rBitmapEx)
 {
     const Size aSize(rBitmapEx.GetSizePixel());
 
-    Bitmap aGrey(aSize, rBitmapEx.GetBitCount());
+    //keep disable image at same depth as original where possible, otherwise
+    //use 8 bit
+    sal_uInt16 nBitCount = rBitmapEx.GetBitCount();
+    if (nBitCount < 8)
+        nBitCount = 8;
+    const BitmapPalette* pPal = nBitCount == 8 ? &Bitmap::GetGreyPalette(256) : nullptr;
+    Bitmap aGrey(aSize, nBitCount, pPal);
+
     AlphaMask aGreyAlpha(aSize);
 
     Bitmap aBitmap(rBitmapEx.GetBitmap());