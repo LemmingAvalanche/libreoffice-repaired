LemmingAvalanche/libreoffice-repaired

commit 70cec05a56356a593ac233f1835b47e43760331e

Resolves: tdf#86502 split formula groups referring bounds shifted into
So references can be updated or invalidated individually when deleting
cells with shifting cells up or left.

Change-Id: I03a57e94cf0fa9bb0716ffec21960e85ad5c7094

sc/source/core/tool/token.cxx
@@ -4365,18 +4365,18 @@ namespace {
 
 void checkBounds(
     const ScAddress& rPos, SCROW nGroupLen, const ScRange& rCheckRange,
-    const ScSingleRefData& rRef, std::vector<SCROW>& rBounds )
+    const ScSingleRefData& rRef, std::vector<SCROW>& rBounds, const ScRange* pDeletedRange )
 {
     if (!rRef.IsRowRel())
         return;
 
     ScRange aAbs(rRef.toAbs(rPos));
     aAbs.aEnd.IncRow(nGroupLen-1);
-    if (!rCheckRange.Intersects(aAbs))
+    if (!rCheckRange.Intersects(aAbs) && (!pDeletedRange || !pDeletedRange->Intersects(aAbs)))
         return;
 
     // Get the boundary row positions.
-    if (aAbs.aEnd.Row() < rCheckRange.aStart.Row())
+    if (aAbs.aEnd.Row() < rCheckRange.aStart.Row() && (!pDeletedRange || aAbs.aEnd.Row() < pDeletedRange->aStart.Row()))
         // No intersections.
         return;
 
@@ -4391,6 +4391,12 @@ void checkBounds(
 
         // Add offset from the reference top to the cell position.
         SCROW nOffset = rCheckRange.aStart.Row() - aAbs.aStart.Row();
+        rBounds.push_back(rPos.Row()+nOffset);
+    }
+    // Same for deleted range.
+    if (pDeletedRange && aAbs.aStart.Row() <= pDeletedRange->aStart.Row())
+    {
+        SCROW nOffset = pDeletedRange->aStart.Row() - aAbs.aStart.Row();
         rBounds.push_back(rPos.Row()+nOffset);
     }
 
@@ -4409,6 +4415,12 @@ void checkBounds(
         SCROW nOffset = rCheckRange.aEnd.Row() + 1 - aAbs.aStart.Row();
         rBounds.push_back(rPos.Row()+nOffset);
     }
+    // Same for deleted range.
+    if (pDeletedRange && aAbs.aEnd.Row() >= pDeletedRange->aEnd.Row())
+    {
+        SCROW nOffset = pDeletedRange->aEnd.Row() + 1 - aAbs.aStart.Row();
+        rBounds.push_back(rPos.Row()+nOffset);
+    }
 }
 
 void checkBounds(
@@ -4417,6 +4429,9 @@ void checkBounds(
 {
     if (!rRef.IsRowRel())
         return;
+
+    ScRange aDeletedRange( ScAddress::UNINITIALIZED );
+    const ScRange* pDeletedRange = nullptr;
 
     ScRange aCheckRange = rCxt.maRange;
     if (rCxt.meMode == URM_MOVE)
@@ -4428,8 +4443,17 @@ void checkBounds(
             assert(!"can't move");
         }
     }
+    else if (rCxt.meMode == URM_INSDEL &&
+            ((rCxt.mnColDelta < 0 && rCxt.maRange.aStart.Col() > 0) ||
+             (rCxt.mnRowDelta < 0 && rCxt.maRange.aStart.Row() > 0)))
+    {
+        // Check bounds also against deleted range where cells are shifted
+        // into and references need to be invalidated.
+        aDeletedRange = getSelectedRange( rCxt);
+        pDeletedRange = &aDeletedRange;
+    }
 
-    checkBounds(rPos, nGroupLen, aCheckRange, rRef, rBounds);
+    checkBounds(rPos, nGroupLen, aCheckRange, rRef, rBounds, pDeletedRange);
 }
 
 }
@@ -4488,14 +4512,14 @@ void ScTokenArray::CheckRelativeReferenceBounds(
                 case svSingleRef:
                     {
                         const ScSingleRefData& rRef = *p->GetSingleRef();
-                        checkBounds(rPos, nGroupLen, rRange, rRef, rBounds);
+                        checkBounds(rPos, nGroupLen, rRange, rRef, rBounds, nullptr);
                     }
                     break;
                 case svDoubleRef:
                     {
                         const ScComplexRefData& rRef = *p->GetDoubleRef();
-                        checkBounds(rPos, nGroupLen, rRange, rRef.Ref1, rBounds);
-                        checkBounds(rPos, nGroupLen, rRange, rRef.Ref2, rBounds);
+                        checkBounds(rPos, nGroupLen, rRange, rRef.Ref1, rBounds, nullptr);
+                        checkBounds(rPos, nGroupLen, rRange, rRef.Ref2, rBounds, nullptr);
                     }
                     break;
                 default:

commit 7d03980f6b3ad2679bb76d88849391f45ea37b59

Resolves: tdf#100764 check row when determining bounds for deleted range
Regression introduced with 70cec05a56356a593ac233f1835b47e43760331e for tdf#86502

Change-Id: I493c458faedccfd6e2ddf849073a52661ef2e4d0

sc/source/core/tool/token.cxx
@@ -4414,7 +4414,10 @@ void checkBounds(
     if (pDeletedRange && aAbs.aStart.Row() <= pDeletedRange->aStart.Row())
     {
         SCROW nOffset = pDeletedRange->aStart.Row() - aAbs.aStart.Row();
-        rBounds.push_back(rPos.Row()+nOffset);
+        SCROW nRow = rPos.Row() + nOffset;
+        // Unlike for rCheckRange, for pDeletedRange nRow can be anywhere>=0.
+        if (ValidRow(nRow))
+            rBounds.push_back(nRow);
     }
 
     if (aAbs.aEnd.Row() >= rCheckRange.aEnd.Row())
@@ -4436,7 +4439,10 @@ void checkBounds(
     if (pDeletedRange && aAbs.aEnd.Row() >= pDeletedRange->aEnd.Row())
     {
         SCROW nOffset = pDeletedRange->aEnd.Row() + 1 - aAbs.aStart.Row();
-        rBounds.push_back(rPos.Row()+nOffset);
+        SCROW nRow = rPos.Row() + nOffset;
+        // Unlike for rCheckRange, for pDeletedRange nRow can be ~anywhere.
+        if (ValidRow(nRow))
+            rBounds.push_back(nRow);
     }
 }
 