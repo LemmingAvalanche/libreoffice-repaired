LemmingAvalanche/libreoffice-repaired

commit 5c9fdf60dc8773581fda373d1dd6920197625936

writerfilter: import paragraph color as fill attributes
In theory this is to be in sync with the ODF import. In practice the old
UNO property seems not to have a proper fallback to populate the doc
model with the fillattributes, so without this even if the import result
is visible, it would be lost on ODF export.

Additionally, this detected a bug in SwUnoCursorHelper::makeRedline(),
where paragraph format redline tried to use the map of a text portion
instead of a paragraph.

Change-Id: I026e38e1990ed2a460624a8d967a16ae3fb6c512

writerfilter/source/dmapper/CellColorHandler.cxx
@@ -21,6 +21,7 @@
 #include <ConversionHelper.hxx>
 #include <TDefTableHandler.hxx>
 #include <ooxml/resourceids.hxx>
+#include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/drawing/ShadingPattern.hpp>
 #include <sal/macros.h>
 #include <filter/msfilter/util.hxx>
@@ -268,9 +269,14 @@ TablePropertyMapPtr  CellColorHandler::getProperties()
         pPropertyMap->Insert(PROP_CHAR_SHADING_VALUE, uno::makeAny( nShadingPattern ));
     }
 
-    pPropertyMap->Insert( m_OutputFormat == Form ? PROP_BACK_COLOR
-                        : m_OutputFormat == Paragraph ? PROP_PARA_BACK_COLOR
-                        : PROP_CHAR_BACK_COLOR, uno::makeAny( nApplyColor ));
+    if (m_OutputFormat == Paragraph)
+    {
+        pPropertyMap->Insert(PROP_FILL_STYLE, uno::makeAny(drawing::FillStyle_SOLID));
+        pPropertyMap->Insert(PROP_FILL_COLOR, uno::makeAny(nApplyColor));
+    }
+    else
+        pPropertyMap->Insert( m_OutputFormat == Form ? PROP_BACK_COLOR
+                            : PROP_CHAR_BACK_COLOR, uno::makeAny( nApplyColor ));
 
     createGrabBag("originalColor", uno::makeAny(OUString::fromUtf8(msfilter::util::ConvertColor(nApplyColor, true))));
 

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -10,6 +10,7 @@
 #include <swmodeltestbase.hxx>
 
 #include <com/sun/star/awt/XBitmap.hpp>
+#include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/graphic/XGraphic.hpp>
 #include <com/sun/star/style/BreakType.hpp>
 #include <com/sun/star/text/FontEmphasis.hpp>
@@ -631,9 +632,8 @@ DECLARE_OOXMLEXPORT_TEST(testEffectExtentMargin, "effectextent-margin.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testTdf88583, "tdf88583.odt")
 {
-    if (xmlDocPtr pXmlDoc = parseExport())
-        // <w:pPr> had no <w:shd> child element, paragraph background was lost.
-        assertXPath(pXmlDoc, "//w:pPr/w:shd", "fill", "00CC00");
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_SOLID, getProperty<drawing::FillStyle>(getParagraph(1), "FillStyle"));
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0x00cc00), getProperty<sal_Int32>(getParagraph(1), "FillColor"));
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/core/unocore/unocrsrhelper.cxx
@@ -1217,9 +1217,13 @@ void makeRedline( SwPaM& rPaM,
     // Check if the value exists
     if ( aRevertPropertiesValue >>= aRevertProperties )
     {
-        // sw/source/core/unocore/unoport.cxx#83 is where it's decided what map gets used for a text portion
-        // so it's PROPERTY_MAP_TEXTPORTION_EXTENSIONS, unless it's a redline portion
-        SfxItemPropertySet const& rPropSet = (*aSwMapProvider.GetPropertySet(PROPERTY_MAP_TEXTPORTION_EXTENSIONS));
+        int nMap = 0;
+        // Make sure that paragraph format gets its own map, otherwise e.g. fill attributes are not preserved.
+        if (eType == nsRedlineType_t::REDLINE_PARAGRAPH_FORMAT)
+            nMap = PROPERTY_MAP_PARAGRAPH;
+        else
+            nMap = PROPERTY_MAP_TEXTPORTION_EXTENSIONS;
+        SfxItemPropertySet const& rPropSet = (*aSwMapProvider.GetPropertySet(nMap));
 
         // Check if there are any properties
         if (aRevertProperties.getLength())

writerfilter/source/dmapper/PropertyIds.cxx
@@ -401,6 +401,8 @@ OUString PropertyNameSupplier::GetName( PropertyIds eId ) const
             case PROP_ROW_CNF_STYLE: sName = "RowCnfStyle"; break;
             case PROP_CELL_HIDE_MARK: sName = "CellHideMark"; break;
             case PROP_FOLLOW_TEXT_FLOW: sName = "IsFollowingTextFlow"; break;
+            case PROP_FILL_STYLE: sName = "FillStyle"; break;
+            case PROP_FILL_COLOR: sName = "FillColor"; break;
         }
         ::std::pair<PropertyNameMap_t::iterator,bool> aInsertIt =
                 m_pImpl->aNameMap.insert( PropertyNameMap_t::value_type( eId, sName ));

writerfilter/source/dmapper/PropertyIds.hxx
@@ -373,6 +373,8 @@ enum PropertyIds
         ,PROP_ROW_CNF_STYLE
         ,PROP_CELL_HIDE_MARK
         ,PROP_FOLLOW_TEXT_FLOW
+        ,PROP_FILL_STYLE
+        ,PROP_FILL_COLOR
     };
 struct PropertyNameSupplier_Impl;
 class PropertyNameSupplier

commit 75f4f0427935e7b29fbebe1526c560e8f44f5810

tdf#97090 writerfilter - don't fill_SOLID with auto color
fixes a regression from 5c9fdf60dc8773581fda373d1dd6920197625936

<w:shd w:val="clear" w:color="auto" w:fill="auto"> seems to be
the default "fill disabled" state, so don't force a solid white
fill in that case.

Change-Id: Ia421e52e228bbf0d3a2cd9af72e0a580042e5dcd
Reviewed-on: https://gerrit.libreoffice.org/27915
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Justin Luth <justin_luth@sil.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

writerfilter/source/dmapper/CellColorHandler.cxx
@@ -37,6 +37,7 @@ LoggedProperties("CellColorHandler"),
 m_nShadingPattern( drawing::ShadingPattern::CLEAR ),
 m_nColor( 0xffffffff ),
 m_nFillColor( 0xffffffff ),
+m_bAutoFillColor( true ),
     m_OutputFormat( Form )
 {
 }
@@ -110,6 +111,9 @@ void CellColorHandler::lcl_attribute(Id rName, Value & rVal)
             createGrabBag("fill", uno::makeAny(OUString::fromUtf8(msfilter::util::ConvertColor(nIntValue, /*bAutoColor=*/true))));
             if( nIntValue == OOXML_COLOR_AUTO )
                 nIntValue = 0xffffff; //fill color auto means white
+            else
+                m_bAutoFillColor = false;
+
             m_nFillColor = nIntValue;
         break;
         case NS_ooxml::LN_CT_Shd_color:
@@ -271,7 +275,10 @@ TablePropertyMapPtr  CellColorHandler::getProperties()
 
     if (m_OutputFormat == Paragraph)
     {
-        pPropertyMap->Insert(PROP_FILL_STYLE, uno::makeAny(drawing::FillStyle_SOLID));
+        // If brush style = clear and FillColor = COLOR_AUTO, then don't enable the fill style - just pre-select the default color
+        if (nWW8BrushStyle || !m_bAutoFillColor)
+            pPropertyMap->Insert(PROP_FILL_STYLE, uno::makeAny(drawing::FillStyle_SOLID));
+
         pPropertyMap->Insert(PROP_FILL_COLOR, uno::makeAny(nApplyColor));
     }
     else

writerfilter/source/dmapper/CellColorHandler.hxx
@@ -37,6 +37,7 @@ private:
     sal_Int32 m_nShadingPattern;
     sal_Int32 m_nColor;
     sal_Int32 m_nFillColor;
+    bool      m_bAutoFillColor;
     OutputFormat m_OutputFormat;
 
     OUString m_aInteropGrabBagName;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -718,6 +718,24 @@ DECLARE_OOXMLEXPORT_TEST(testTdf88583, "tdf88583.odt")
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0x00cc00), getProperty<sal_Int32>(getParagraph(1), "FillColor"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf97090, "tdf97090.docx")
+{
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0x95B3D7), getProperty<sal_Int32>(xTable->getCellByName("A1"), "BackColor"));
+
+    uno::Reference<container::XEnumerationAccess> paraEnumAccess(xTable->getCellByName("A1"), uno::UNO_QUERY);
+    assert( paraEnumAccess.is() );
+    uno::Reference<container::XEnumeration> paraEnum = paraEnumAccess->createEnumeration();
+
+    assert( paraEnum.is() );
+    uno::Reference<beans::XPropertySet> paragraphProperties(paraEnum->nextElement(), uno::UNO_QUERY);
+    assert( paragraphProperties.is() );
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_NONE, getProperty<drawing::FillStyle>(paragraphProperties, "FillStyle"));
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xffffff), getProperty<sal_Int32>(paragraphProperties, "FillColor"));
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf89791, "tdf89791.docx")
 {
     if (mbExported)