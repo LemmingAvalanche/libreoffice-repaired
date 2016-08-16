LemmingAvalanche/libreoffice-repaired

commit 0016cd3ccb955a7b5188a4e0e9559f871d95cf25

n#825305: writerfilter RTF import: override style properties like Word
It would certainly be immediately obvious to any reader of the RTF spec
that \sN will apply the style with index N to the current paragraph.

But actually, that is not what Word does when it reads \sN...
what it really does is to apply the style with index N, and then for
every attribute in that style, apply the same attribute with a default
value to the paragraph, effectively overriding what's in the style.

If that doesn't make any sense to you, well, have you heard the joke
about how many Microsoft engineers it takes to change a light bulb?

Also, \pard apparently implies \s0.

To implement that, change RTFSprms::deduplicate() to recursively
look for style SPRMs that are missing in the properties, and put
in default ones, currently just for 2 keywords \sa and \sb.

This requires changing deduplicate() to be const and return a new value,
since it is no longer idempotent, as the erased SPRMs would get
defaulted on the next run.

While at it, fix RTFValue::equals() which did not compare m_sValue.

This fixes the testParaBottomMargin test that was broken by the fix
for fdo#70578.

Change-Id: I4ced38628d76f6c41b488d608a804883493ff00b

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -443,8 +443,12 @@ writerfilter::Reference<Properties>::Pointer_t RTFDocumentImpl::getProperties(RT
     {
         RTFReferenceProperties& rProps = *(RTFReferenceProperties*)it->second.get();
         // Get rid of direct formatting what is already in the style.
-        rSprms.deduplicate(rProps.getSprms());
-        rAttributes.deduplicate(rProps.getAttributes());
+        RTFSprms const sprms(
+                rSprms.cloneAndDeduplicate(rProps.getSprms()));
+        RTFSprms const attributes(
+                rAttributes.cloneAndDeduplicate(rProps.getAttributes()));
+        return writerfilter::Reference<Properties>::Pointer_t(
+                new RTFReferenceProperties(attributes, sprms));
     }
     writerfilter::Reference<Properties>::Pointer_t pRet(new RTFReferenceProperties(rAttributes, rSprms));
     return pRet;
@@ -2759,6 +2763,7 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         // \pard is allowed between \cell and \row, but in that case it should not reset the fact that we're inside a table.
         m_aStates.top().aParagraphSprms = m_aDefaultState.aParagraphSprms;
         m_aStates.top().aParagraphAttributes = m_aDefaultState.aParagraphAttributes;
+
         if (m_nTopLevelCells == 0 && m_nNestedCells == 0)
         {
             // Reset that we're in a table.
@@ -2772,7 +2777,20 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         m_aStates.top().resetFrame();
 
         // Reset currently selected paragraph style as well.
-        m_aStates.top().nCurrentStyleIndex = -1;
+        // By default the style with index 0 is applied.
+        {
+            OUString const aName = getStyleName(0);
+            if (!aName.isEmpty())
+            {
+                m_aStates.top().aParagraphSprms.set(NS_ooxml::LN_CT_PPrBase_pStyle,
+                    RTFValue::Pointer_t(new RTFValue(aName)));
+                m_aStates.top().nCurrentStyleIndex = 0;
+            }
+            else
+            {
+                m_aStates.top().nCurrentStyleIndex = -1;
+            }
+        }
         break;
     case RTF_SECTD:
     {

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1173,8 +1173,18 @@ DECLARE_RTFIMPORT_TEST(testN825305, "n825305.rtf")
 
 DECLARE_RTFIMPORT_TEST(testParaBottomMargin, "para-bottom-margin.rtf")
 {
+    uno::Reference<beans::XPropertySet> xPropertySet(
+        getStyles("ParagraphStyles")->getByName("Standard"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(353),
+            getProperty<sal_Int32>(xPropertySet, "ParaBottomMargin"));
+
     // This was 353, i.e. bottom margin of the paragraph was 0.35cm instead of 0.
+    // The reason why this is 0 despite the default style containing \sa200
+    // is that Word will actually interpret \sN (or \pard which apparently
+    // implies \s0) as "set style N and for every attribute of that style,
+    // set an attribute with default value on the paragraph"
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(getParagraph(1), "ParaBottomMargin"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), getProperty<sal_Int32>(getParagraph(1), "ParaTopMargin"));
 }
 
 DECLARE_RTFIMPORT_TEST(testN823655, "n823655.rtf")

writerfilter/source/rtftok/rtfsprm.cxx
@@ -8,8 +8,10 @@
  */
 
 #include <rtfsprm.hxx>
+
 #include <rtl/strbuf.hxx>
 
+#include <ooxml/resourceids.hxx>
 #include <resourcemodel/QNameToString.hxx>
 
 
@@ -138,22 +140,68 @@ bool RTFSprms::erase(Id nKeyword)
     return false;
 }
 
-void RTFSprms::deduplicate(RTFSprms& rReference)
+static RTFValue::Pointer_t getDefaultSPRM(Id const id)
 {
-    ensureCopyBeforeWrite();
-
-    RTFSprms::Iterator_t i = m_pSprms->begin();
-    while (i != m_pSprms->end())
+    switch (id)
     {
-        bool bIgnore = false;
-        RTFValue::Pointer_t pValue(rReference.find(i->first));
-        if (pValue.get() && i->second->equals(*pValue))
-            bIgnore = true;
-        if (bIgnore)
-            i = m_pSprms->erase(i);
-        else
-            ++i;
+        case NS_ooxml::LN_CT_Spacing_before:
+        case NS_ooxml::LN_CT_Spacing_after:
+            return RTFValue::Pointer_t(new RTFValue(0));
+
+        default:
+            return 0;
     }
+}
+
+RTFSprms RTFSprms::cloneAndDeduplicate(RTFSprms& rReference) const
+{
+    RTFSprms ret(*this);
+    ret.ensureCopyBeforeWrite();
+
+    // Note: apparently some attributes are set with OVERWRITE_NO_APPEND;
+    // it is probably a bad idea to mess with those in any way here?
+    for (RTFSprms::Iterator_t i = rReference.begin(); i != rReference.end(); ++i)
+    {
+        RTFValue::Pointer_t const pValue(ret.find(i->first));
+        if (pValue)
+        {
+            if (i->second->equals(*pValue))
+            {
+                ret.erase(i->first); // duplicate to style
+            }
+            else if (!i->second->getSprms().empty() || !i->second->getAttributes().empty())
+            {
+                RTFSprms const sprms(
+                    pValue->getSprms().cloneAndDeduplicate(i->second->getSprms()));
+                RTFSprms const attributes(
+                    pValue->getAttributes().cloneAndDeduplicate(i->second->getAttributes()));
+                ret.set(i->first, RTFValue::Pointer_t(
+                            pValue->CloneWithSprms(attributes, sprms)));
+            }
+        }
+        else
+        {
+            // not found - try to override style with default
+            RTFValue::Pointer_t const pDefault(getDefaultSPRM(i->first));
+            if (pDefault)
+            {
+                ret.set(i->first, pDefault);
+            }
+            else if (!i->second->getSprms().empty() || !i->second->getAttributes().empty())
+            {
+                RTFSprms const sprms(
+                    RTFSprms().cloneAndDeduplicate(i->second->getSprms()));
+                RTFSprms const attributes(
+                    RTFSprms().cloneAndDeduplicate(i->second->getAttributes()));
+                if (!sprms.empty() || !attributes.empty())
+                {
+                    ret.set(i->first,
+                        RTFValue::Pointer_t(new RTFValue(attributes, sprms)));
+                }
+            }
+        }
+    }
+    return ret;
 }
 
 bool RTFSprms::equals(RTFValue& rOther)

writerfilter/source/rtftok/rtfsprm.hxx
@@ -67,8 +67,10 @@ public:
     /// Does the same as ->push_back(), except that it can overwrite or ignore existing entries.
     void set(Id nKeyword, RTFValue::Pointer_t pValue, RTFOverwrite eOverwrite = OVERWRITE_YES);
     bool erase(Id nKeyword);
-    /// Removes elements, which are already in the reference set.
-    void deduplicate(RTFSprms& rReference);
+    /// Removes elements which are already in the reference set.
+    /// Also insert default values to override attributes of style
+    /// (yes, really; that's what Word does).
+    RTFSprms cloneAndDeduplicate(RTFSprms& rReference) const;
     size_t size() const
     {
         return m_pSprms->size();

writerfilter/source/rtftok/rtfvalue.cxx
@@ -223,9 +223,17 @@ RTFValue* RTFValue::Clone()
     return new RTFValue(m_nValue, m_sValue, *m_pAttributes, *m_pSprms, m_xShape, m_xStream, m_xObject, m_bForceString, *m_pShape);
 }
 
+RTFValue* RTFValue::CloneWithSprms(
+        RTFSprms const& rAttributes, RTFSprms const& rSprms)
+{
+    return new RTFValue(m_nValue, m_sValue, rAttributes, rSprms, m_xShape, m_xStream, m_xObject, m_bForceString, *m_pShape);
+}
+
 bool RTFValue::equals(RTFValue& rOther)
 {
     if (m_nValue != rOther.m_nValue)
+        return false;
+    if (m_sValue != rOther.m_sValue)
         return false;
     if (m_pAttributes->size() != rOther.m_pAttributes->size())
         return false;

writerfilter/source/rtftok/rtfvalue.hxx
@@ -48,6 +48,7 @@ public:
     virtual writerfilter::Reference<BinaryObj>::Pointer_t getBinary() SAL_OVERRIDE;
     virtual std::string toString() const SAL_OVERRIDE;
     virtual RTFValue* Clone();
+    virtual RTFValue* CloneWithSprms(RTFSprms const& rAttributes, RTFSprms const& rSprms);
     RTFSprms& getAttributes();
     RTFSprms& getSprms();
     RTFShape& getShape() const;

commit cecd6c053aaa0c1bbd086fba0a9f23a0375f6f99

tdf#100507 RTF import: don't set default para style to the 0th char style
Regression from commit 0016cd3ccb955a7b5188a4e0e9559f871d95cf25
(n#825305: writerfilter RTF import: override style properties like Word,
2014-06-17), the problem was that the RTF_PARD handler wanted to set a
default paragraph style, but it didn't check if the 0th style is
actually a paragraph one. This resulted in using a character style name
as a paragraph one, throwing in SwUnoCursorHelper::SetTextFormatColl()
-> all paragraph properties were lost, including the left indent.

Fix this by tracking the style type, and filtering out character styles
when looking up a default paragraph style.

Change-Id: I41faab0e72667b89ec9a507014b395a675847abf

writerfilter/source/rtftok/rtfdispatchflag.cxx
@@ -485,7 +485,8 @@ RTFError RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         // By default the style with index 0 is applied.
         {
             OUString const aName = getStyleName(0);
-            if (!aName.isEmpty())
+            // But only in case it's not a character style.
+            if (!aName.isEmpty() && getStyleType(0) != NS_ooxml::LN_Value_ST_StyleType_character)
             {
                 m_aStates.top().aParagraphSprms.set(NS_ooxml::LN_CT_PPrBase_pStyle, std::make_shared<RTFValue>(aName));
                 m_aStates.top().nCurrentStyleIndex = 0;

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -700,6 +700,19 @@ OUString RTFDocumentImpl::getStyleName(int nIndex)
         return m_pSuperstream->getStyleName(nIndex);
 }
 
+Id RTFDocumentImpl::getStyleType(int nIndex)
+{
+    if (!m_pSuperstream)
+    {
+        Id nRet = 0;
+        if (m_aStyleTypes.find(nIndex) != m_aStyleTypes.end())
+            nRet = m_aStyleTypes[nIndex];
+        return nRet;
+    }
+    else
+        return m_pSuperstream->getStyleType(nIndex);
+}
+
 RTFParserState& RTFDocumentImpl::getDefaultState()
 {
     if (!m_pSuperstream)
@@ -1247,10 +1260,13 @@ void RTFDocumentImpl::text(OUString& rString)
             }
             break;
             case Destination::STYLEENTRY:
-                if (m_aStates.top().aTableAttributes.find(NS_ooxml::LN_CT_Style_type))
+            {
+                RTFValue::Pointer_t pType = m_aStates.top().aTableAttributes.find(NS_ooxml::LN_CT_Style_type);
+                if (pType)
                 {
                     // Word strips whitespace around style names.
                     m_aStyleNames[m_nCurrentStyleIndex] = aName.trim();
+                    m_aStyleTypes[m_nCurrentStyleIndex] = pType->getInt();
                     auto pValue = std::make_shared<RTFValue>(aName.trim());
                     m_aStates.top().aTableAttributes.set(NS_ooxml::LN_CT_Style_styleId, pValue);
                     m_aStates.top().aTableSprms.set(NS_ooxml::LN_CT_Style_name, pValue);
@@ -1261,6 +1277,7 @@ void RTFDocumentImpl::text(OUString& rString)
                 else
                     SAL_INFO("writerfilter", "no RTF style type defined, ignoring");
                 break;
+            }
             case Destination::LISTNAME:
                 // TODO: what can be done with a list name?
                 break;

writerfilter/source/rtftok/rtfdocumentimpl.hxx
@@ -438,6 +438,8 @@ public:
     OUString getFontName(int nIndex);
     /// Return the style name of an RTF style index.
     OUString getStyleName(int nIndex);
+    /// Return the style type of an RTF style index.
+    Id getStyleType(int nIndex);
     /// Return the encoding associated with a font index.
     rtl_TextEncoding getEncoding(int nFontIndex);
     /// Get the default parser state.
@@ -519,6 +521,8 @@ private:
     std::vector<int> m_aFontIndexes;
     /// Maps style indexes to style names.
     std::map<int, OUString> m_aStyleNames;
+    /// Maps style indexes to style types.
+    std::map<int, Id> m_aStyleTypes;
     /// Color index <-> RGB color value map
     std::vector<sal_uInt32> m_aColorTable;
     bool m_bFirstRun;

sw/qa/extras/rtfimport/data/tdf100507.rtf
@@ -1,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\deff0\dntblnsbdb\viewkind1
+{\fonttbl
+{\f0\froman\fcharset0 Times New Roman;}
+{\f1\fnil\fcharset0 Arial;}
+{\f2\fnil\fcharset0 Arial;}
+{\f3\fnil\fcharset0 Arial;}
+{\f4\fnil\fcharset0 Arial;}
+{\f5\fnil\fcharset0 Arial;}
+{\f6\fnil\fcharset0 Arial;}
+{\f7\fnil\fcharset0 Arial;}
+{\f8\fnil\fcharset0 Arial;}
+}
+{\colortbl;\red255\green255\blue0;\red0\green0\blue255;\red255\green255\blue255;}
+{\stylesheet
+{\*\cs0 Default Paragraph Font;}
+}
+\jexpand\pgwsxn12240\pghsxn15840
+\margl1748\margr1460\margt678\margb478\marglsxn1748\margrsxn1460\cols1\colno1\colw9032
+{\pard\plain \li3752\ql
+{\f2\b\fs20 Generation 1}
+\par}
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -2678,6 +2678,12 @@ DECLARE_RTFIMPORT_TEST(testTdf78506, "tdf78506.rtf")
     }
 }
 
+DECLARE_RTFIMPORT_TEST(testTdf100507, "tdf100507.rtf")
+{
+    // This was 0: left margin of the first paragraph was lost on import.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(6618), getProperty<sal_Int32>(getParagraph(1), "ParaLeftMargin"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */