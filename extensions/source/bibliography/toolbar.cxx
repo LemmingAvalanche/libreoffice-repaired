LemmingAvalanche/libreoffice-repaired

commit 377d06c042d9acb93d4c970d72b54003ca902e88

tdf#97527 - vcl: reference-count Menu
some places are marked with "dodgy"- need to check those to see
what is going on, because they are leaving dangling pointers behind
in the Menu class

Change-Id: I41d5c7c0fec2f70ce9e3ffdc48cd03d26c0a869b
Reviewed-on: https://gerrit.libreoffice.org/26516
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

extensions/source/bibliography/toolbar.cxx
@@ -383,21 +383,21 @@ void BibToolBar::Click()
 
 void BibToolBar::ClearFilterMenu()
 {
-    aPopupMenu.Clear();
+    pPopupMenu->Clear();
     nMenuId=0;
 }
 sal_uInt16 BibToolBar::InsertFilterItem(const OUString& aMenuEntry)
 {
     nMenuId++;
-    aPopupMenu.InsertItem(nMenuId,aMenuEntry);
+    pPopupMenu->InsertItem(nMenuId,aMenuEntry);
 
     return nMenuId;
 }
 void BibToolBar::SelectFilterItem(sal_uInt16    nId)
 {
-    aPopupMenu.CheckItem(nId);
+    pPopupMenu->CheckItem(nId);
     nSelMenuItem=nId;
-    aQueryField = MnemonicGenerator::EraseAllMnemonicChars( aPopupMenu.GetItemText(nId) );
+    aQueryField = MnemonicGenerator::EraseAllMnemonicChars( pPopupMenu->GetItemText(nId) );
 }
 
 void BibToolBar::EnableSourceList(bool bFlag)
@@ -490,15 +490,15 @@ IMPL_LINK_NOARG_TYPED( BibToolBar, MenuHdl, ToolBox*, void)
         EndSelection();     // vor SetDropMode (SetDropMode ruft SetItemImage)
 
         SetItemDown(nTBC_BT_AUTOFILTER, true);
-        nId = aPopupMenu.Execute(this, GetItemRect(nTBC_BT_AUTOFILTER));
+        nId = pPopupMenu->Execute(this, GetItemRect(nTBC_BT_AUTOFILTER));
 
 
         if(nId>0)
         {
-            aPopupMenu.CheckItem(nSelMenuItem,false);
-            aPopupMenu.CheckItem(nId);
+            pPopupMenu->CheckItem(nSelMenuItem,false);
+            pPopupMenu->CheckItem(nId);
             nSelMenuItem=nId;
-            aQueryField = MnemonicGenerator::EraseAllMnemonicChars( aPopupMenu.GetItemText(nId) );
+            aQueryField = MnemonicGenerator::EraseAllMnemonicChars( pPopupMenu->GetItemText(nId) );
             Sequence<PropertyValue> aPropVal(2);
             PropertyValue* pPropertyVal = const_cast<PropertyValue*>(aPropVal.getConstArray());
             pPropertyVal[0].Name = "QueryText";

commit 8d83549f8a326026d8bdfbb406818189516a7229

tdf#101810 - Crash when selecting Bibliography from Tools
fallout from commit 377d06c0 "tdf#97527 - vcl: reference-count Menu"

Change-Id: I996a5b739f35d034b15fd5d0b4c94fd57688bf12

extensions/source/bibliography/toolbar.cxx
@@ -181,6 +181,7 @@ BibToolBar::BibToolBar(vcl::Window* pParent, Link<void*,void> aLink)
     aLBSource(VclPtr<ListBox>::Create(this,WB_DROPDOWN)),
     aFtQuery(VclPtr<FixedText>::Create(this,WB_VCENTER)),
     aEdQuery(VclPtr<Edit>::Create(this)),
+    pPopupMenu(VclPtr<PopupMenu>::Create()),
     nMenuId(0),
     nSelMenuItem(0),
     aLayoutManager( aLink ),