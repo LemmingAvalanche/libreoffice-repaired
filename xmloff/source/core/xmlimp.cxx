LemmingAvalanche/libreoffice-repaired

commit 9893470f581c6ca89108ed4491c8e70ede30d68c

resolve the snafu with 2 separate refcounted bases
Change-Id: Ia275596d54ea27436f03d01297fb78b6ca09e8a6

xmloff/source/core/xmlimp.cxx
@@ -627,14 +627,14 @@ void SAL_CALL SvXMLImport::endDocument()
         }
     }
 
-    if( mxFontDecls.Is() )
-        static_cast<SvXMLStylesContext *>(&mxFontDecls)->Clear();
-    if( mxStyles.Is() )
-        static_cast<SvXMLStylesContext *>(&mxStyles)->Clear();
-    if( mxAutoStyles.Is() )
-        static_cast<SvXMLStylesContext *>(&mxAutoStyles)->Clear();
-    if( mxMasterStyles.Is() )
-        static_cast<SvXMLStylesContext *>(&mxMasterStyles)->Clear();
+    if( mxFontDecls.is() )
+        static_cast<SvXMLStylesContext *>(mxFontDecls.get())->Clear();
+    if( mxStyles.is() )
+        static_cast<SvXMLStylesContext *>(mxStyles.get())->Clear();
+    if( mxAutoStyles.is() )
+        static_cast<SvXMLStylesContext *>(mxAutoStyles.get())->Clear();
+    if( mxMasterStyles.is() )
+        static_cast<SvXMLStylesContext *>(mxMasterStyles.get())->Clear();
 
     // possible form-layer related knittings which can only be done when
     // the whole document exists
@@ -1690,32 +1690,32 @@ void SvXMLImport::SetMasterStyles( SvXMLStylesContext *pMasterStyles )
 
 XMLFontStylesContext *SvXMLImport::GetFontDecls()
 {
-    return static_cast<XMLFontStylesContext *>(&mxFontDecls);
+    return static_cast<XMLFontStylesContext *>(mxFontDecls.get());
 }
 
 SvXMLStylesContext *SvXMLImport::GetStyles()
 {
-    return static_cast<SvXMLStylesContext *>(&mxStyles);
+    return static_cast<SvXMLStylesContext *>(mxStyles.get());
 }
 
 SvXMLStylesContext *SvXMLImport::GetAutoStyles()
 {
-    return static_cast<SvXMLStylesContext *>(&mxAutoStyles);
+    return static_cast<SvXMLStylesContext *>(mxAutoStyles.get());
 }
 
 const XMLFontStylesContext *SvXMLImport::GetFontDecls() const
 {
-    return static_cast<const XMLFontStylesContext *>(&mxFontDecls);
+    return static_cast<const XMLFontStylesContext *>(mxFontDecls.get());
 }
 
 const SvXMLStylesContext *SvXMLImport::GetStyles() const
 {
-    return static_cast<const SvXMLStylesContext *>(&mxStyles);
+    return static_cast<const SvXMLStylesContext *>(mxStyles.get());
 }
 
 const SvXMLStylesContext *SvXMLImport::GetAutoStyles() const
 {
-    return static_cast<const SvXMLStylesContext *>(&mxAutoStyles);
+    return static_cast<const SvXMLStylesContext *>(mxAutoStyles.get());
 }
 
 OUString SvXMLImport::GetAbsoluteReference(const OUString& rValue) const
@@ -1903,14 +1903,14 @@ void SvXMLImport::SetError(
 
 void SvXMLImport::DisposingModel()
 {
-    if( mxFontDecls.Is() )
-        static_cast<SvXMLStylesContext *>(&mxFontDecls)->Clear();
-    if( mxStyles.Is() )
-        static_cast<SvXMLStylesContext *>(&mxStyles)->Clear();
-    if( mxAutoStyles.Is() )
-        static_cast<SvXMLStylesContext *>(&mxAutoStyles)->Clear();
-    if( mxMasterStyles.Is() )
-        static_cast<SvXMLStylesContext *>(&mxMasterStyles)->Clear();
+    if( mxFontDecls.is() )
+        static_cast<SvXMLStylesContext *>(mxFontDecls.get())->Clear();
+    if( mxStyles.is() )
+        static_cast<SvXMLStylesContext *>(mxStyles.get())->Clear();
+    if( mxAutoStyles.is() )
+        static_cast<SvXMLStylesContext *>(mxAutoStyles.get())->Clear();
+    if( mxMasterStyles.is() )
+        static_cast<SvXMLStylesContext *>(mxMasterStyles.get())->Clear();
 
     mxModel.set(nullptr);
     mxEventListener.set(nullptr);

include/xmloff/prstylei.hxx
@@ -80,7 +80,7 @@ protected:
     virtual void SetAttribute( sal_uInt16 nPrefixKey,
                                const OUString& rLocalName,
                                const OUString& rValue ) override;
-    SvXMLStylesContext *GetStyles() { return static_cast<SvXMLStylesContext *>(&mxStyles); }
+    SvXMLStylesContext *GetStyles() { return static_cast<SvXMLStylesContext *>(mxStyles.get()); }
     ::std::vector< XMLPropertyState > & GetProperties() { return maProperties; }
 
     // Override this method to create a new style. It's called by
@@ -106,7 +106,7 @@ public:
     virtual void FillPropertySet(
             const css::uno::Reference< css::beans::XPropertySet > & rPropSet );
 
-    const SvXMLStylesContext *GetStyles() const { return static_cast<const SvXMLStylesContext *>(&mxStyles); }
+    const SvXMLStylesContext *GetStyles() const { return static_cast<const SvXMLStylesContext *>(mxStyles.get()); }
     const ::std::vector< XMLPropertyState > & GetProperties() const { return maProperties; }
 
     const css::uno::Reference< css::style::XStyle >&

include/xmloff/xmlictxt.hxx
@@ -34,8 +34,7 @@
 class SvXMLNamespaceMap;
 class SvXMLImport;
 
-class XMLOFF_DLLPUBLIC SvXMLImportContext : public SvRefBase,
-        public ::cppu::WeakImplHelper1< ::css::xml::sax::XFastContextHandler >
+class XMLOFF_DLLPUBLIC SvXMLImportContext : public ::cppu::WeakImplHelper1< ::css::xml::sax::XFastContextHandler >
 {
     friend class SvXMLImport;
 
@@ -125,9 +124,13 @@ public:
     // support multiple images and to rescue evtl. GluePoints imported with one of the
     // to be deprecated contents
     virtual void onDemandRescueUsefulDataFromTemporary( const SvXMLImportContext& rCandidate );
+
+    void AddFirstRef();
+    void AddNextRef();
+    void ReleaseRef();
 };
 
-typedef tools::SvRef<SvXMLImportContext> SvXMLImportContextRef;
+typedef css::uno::Reference<SvXMLImportContext> SvXMLImportContextRef;
 
 #endif // INCLUDED_XMLOFF_XMLICTXT_HXX
 

sc/source/filter/xml/xmlcvali.cxx
@@ -403,11 +403,11 @@ void ScXMLContentValidationContext::GetCondition( ScMyImportValidation& rValidat
 void ScXMLContentValidationContext::EndElement()
 {
     // #i36650# event-listeners element moved up one level
-    if (xEventContext.Is())
+    if (xEventContext.is())
     {
         OUString sOnError("OnError");
         XMLEventsImportContext* pEvents =
-            static_cast<XMLEventsImportContext*>(&xEventContext);
+            static_cast<XMLEventsImportContext*>(xEventContext.get());
         uno::Sequence<beans::PropertyValue> aValues;
         pEvents->GetEventSequence( sOnError, aValues );
 

sw/source/filter/xml/xmlitemi.cxx
@@ -278,10 +278,10 @@ SwXMLItemSetContext_Impl::SwXMLItemSetContext_Impl(
 
 SwXMLItemSetContext_Impl::~SwXMLItemSetContext_Impl()
 {
-    if( xBackground.Is() )
+    if( xBackground.is() )
     {
         const SvxBrushItem& rItem =
-            static_cast<SwXMLBrushItemImportContext*>(&xBackground)->GetItem();
+            static_cast<SwXMLBrushItemImportContext*>(xBackground.get())->GetItem();
         rItemSet.Put( rItem );
     }
 }

sw/source/filter/xml/xmltbli.cxx
@@ -210,7 +210,7 @@ public:
                      OUString const& i_rXmlId);
 
     bool IsUsed() const { return pStartNode!=nullptr ||
-                                     xSubTable.Is() || bProtected;}
+                                     xSubTable.is() || bProtected;}
 
     sal_uInt32 GetRowSpan() const { return nRowSpan; }
     void SetRowSpan( sal_uInt32 nSet ) { nRowSpan = nSet; }
@@ -281,12 +281,12 @@ inline void SwXMLTableCell_Impl::SetStartNode( const SwStartNode *pSttNd )
 
 inline SwXMLTableContext *SwXMLTableCell_Impl::GetSubTable() const
 {
-    return static_cast<SwXMLTableContext *>(&xSubTable);
+    return static_cast<SwXMLTableContext *>(xSubTable.get());
 }
 
 inline void SwXMLTableCell_Impl::Dispose()
 {
-    if( xSubTable.Is() )
+    if( xSubTable.is() )
         xSubTable = nullptr;
 }
 
@@ -412,7 +412,7 @@ class SwXMLTableCellContext_Impl : public SvXMLImportContext
     bool                    bHasTextContent : 1;
     bool                    bHasTableContent : 1;
 
-    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(&xMyTable); }
+    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(xMyTable.get()); }
 
     bool HasContent() const { return bHasTextContent || bHasTableContent; }
     inline void InsertContent_();
@@ -738,7 +738,7 @@ class SwXMLTableColContext_Impl : public SvXMLImportContext
 {
     SvXMLImportContextRef   xMyTable;
 
-    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(&xMyTable); }
+    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(xMyTable.get()); }
 
 public:
 
@@ -823,7 +823,7 @@ class SwXMLTableColsContext_Impl : public SvXMLImportContext
 {
     SvXMLImportContextRef   xMyTable;
 
-    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(&xMyTable); }
+    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(xMyTable.get()); }
 
 public:
 
@@ -881,7 +881,7 @@ class SwXMLTableRowContext_Impl : public SvXMLImportContext
 
     sal_uInt32                  nRowRepeat;
 
-    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(&xMyTable); }
+    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(xMyTable.get()); }
 
 public:
 
@@ -1001,7 +1001,7 @@ class SwXMLTableRowsContext_Impl : public SvXMLImportContext
 
     bool bHeader;
 
-    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(&xMyTable); }
+    SwXMLTableContext *GetTable() { return static_cast<SwXMLTableContext *>(xMyTable.get()); }
 
 public:
 
@@ -1831,8 +1831,8 @@ SwTableBox *SwXMLTableContext::NewTableBox( const SwStartNode *pStNd,
 {
     // The topmost table is the only table that maintains the two members
     // pBox1 and bFirstSection.
-    if( m_xParentTable.Is() )
-        return static_cast<SwXMLTableContext *>(&m_xParentTable)->NewTableBox( pStNd,
+    if( m_xParentTable.is() )
+        return static_cast<SwXMLTableContext *>(m_xParentTable.get())->NewTableBox( pStNd,
                                                                   pUpper );
 
     SwTableBox *pBox;
@@ -2839,8 +2839,8 @@ const SwStartNode *SwXMLTableContext::InsertTableSection(
 {
     // The topmost table is the only table that maintains the two members
     // pBox1 and bFirstSection.
-    if( m_xParentTable.Is() )
-        return static_cast<SwXMLTableContext *>(&m_xParentTable)
+    if( m_xParentTable.is() )
+        return static_cast<SwXMLTableContext *>(m_xParentTable.get())
                     ->InsertTableSection(pPrevSttNd, pStringValueStyleName);
 
     const SwStartNode *pStNd;
@@ -2907,7 +2907,7 @@ const SwStartNode *SwXMLTableContext::InsertTableSection(
 
 void SwXMLTableContext::EndElement()
 {
-    if( IsValid() && !m_xParentTable.Is() )
+    if( IsValid() && !m_xParentTable.is() )
     {
         MakeTable();
         GetImport().GetTextImport()->SetCursor( m_xOldCursor );

sw/source/filter/xml/xmltbli.hxx
@@ -185,7 +185,7 @@ public:
 
 inline SwXMLTableContext *SwXMLTableContext::GetParentTable() const
 {
-    return static_cast<SwXMLTableContext *>(&m_xParentTable);
+    return static_cast<SwXMLTableContext *>(m_xParentTable.get());
 }
 
 inline sal_uInt32 SwXMLTableContext::GetColumnCount() const

xmloff/source/core/xmlictxt.cxx
@@ -167,4 +167,19 @@ void SvXMLImportContext::onDemandRescueUsefulDataFromTemporary( const SvXMLImpor
 {
 }
 
+void SvXMLImportContext::AddFirstRef()
+{
+    acquire();
+}
+
+void SvXMLImportContext::AddNextRef()
+{
+    acquire();
+}
+
+void SvXMLImportContext::ReleaseRef()
+{
+    release();
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit d31904dcbb7a5bf1101ad2db5af96f8ee025e52f

xmloff: invoke SvXMLImportContext destructor with proper namespace map
The recent pRewindMap fix revealed that the xContext object is destroyed
too late so the destructors containing half the import code (because
where else would you put complex logic that can fail?) are invoked with
the namespace map of the parent element, which breaks RDFa import and
JunitTest_sfx2_complex.

(regression from 9893470f581c6ca89108ed4491c8e70ede30d68c)

Change-Id: I0c897ea0cae2db4d35f2ba19ba2f6e8026f1b3ec

xmloff/source/core/xmlimp.cxx
@@ -775,9 +775,15 @@ rName
 )
     throw(xml::sax::SAXException, uno::RuntimeException, std::exception)
 {
-    sal_uInt16 nCount = maContexts.size();
-    SAL_WARN_IF( nCount == 0, "xmloff.core", "SvXMLImport::endElement: no context left" );
-    if( nCount > 0 )
+    auto const nCount = maContexts.size();
+    if (nCount == 0)
+    {
+        SAL_WARN("xmloff.core", "SvXMLImport::endElement: no context left");
+        return;
+    }
+
+    SvXMLNamespaceMap * pRewindMap(nullptr);
+
     {
         // Get topmost context and remove it from the stack.
         SvXMLImportContextRef xContext = maContexts.back();
@@ -794,16 +800,16 @@ rName
 
         // Call a EndElement at the current context.
         xContext->EndElement();
-
         // Get a namespace map to rewind.
-        SvXMLNamespaceMap *pRewindMap = xContext->TakeRewindMap();
+        pRewindMap = xContext->TakeRewindMap();
+        // note: delete xContext *before* rewinding namespace map!
+    }
 
-        // Rewind a namespace map.
-        if( pRewindMap )
-        {
-            delete mpNamespaceMap;
-            mpNamespaceMap = pRewindMap;
-        }
+    // Rewind a namespace map.
+    if (pRewindMap)
+    {
+        delete mpNamespaceMap;
+        mpNamespaceMap = pRewindMap;
     }
 }
 