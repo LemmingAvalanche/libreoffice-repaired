LemmingAvalanche/libreoffice-repaired

commit c2330b9c19621187c2b882c96ad2817cfe8748e6

tdf#99827 sw: don't create AutoText dialog's preview doc ReadOnly
Since commit 72a27f75f507de896adb4105c02d90182b1f9d0c the framework
refuses to create new documents as ReadOnly, so don't do that.

Change-Id: I9d94ca9859baaf5407bad88a2b4b5b2e26b99b94

sw/source/uibase/utlui/unotools.cxx
@@ -137,14 +137,12 @@ void SwOneExampleFrame::CreateControl()
             sTempURL = sArgumentURL;
         aURL <<= sTempURL;
 
-        uno::Sequence<beans::PropertyValue> aSeq(3);
+        uno::Sequence<beans::PropertyValue> aSeq(2);
         beans::PropertyValue* pValues = aSeq.getArray();
-        pValues[0].Name = "ReadOnly";
-        pValues[0].Value <<= true;
-        pValues[1].Name = "OpenFlags";
-        pValues[1].Value <<= OUString("-RB");
-        pValues[2].Name = "Referer";
-        pValues[2].Value <<= OUString("private:user");
+        pValues[0].Name = "OpenFlags";
+        pValues[0].Value <<= OUString("-RB");
+        pValues[1].Name = "Referer";
+        pValues[1].Value <<= OUString("private:user");
         uno::Any aArgs(aSeq);
 
         xPrSet->setPropertyValue( "LoaderArguments", aArgs );

commit d4113973c6233ff918469716560d6330535a9260

tdf#101536 sw: do load Insert Index dialog's document ReadOnly
Turns out SwOneExampleFrame is used for different things, so load doc
read-only only if it's an existing document.

(regression from c2330b9c19621187c2b882c96ad2817cfe8748e6)

Change-Id: I0a1ab0725ed9dccca23742c9d83cd9635a188edf

sw/source/uibase/utlui/unotools.cxx
@@ -137,12 +137,14 @@ void SwOneExampleFrame::CreateControl()
             sTempURL = sArgumentURL;
         aURL <<= sTempURL;
 
-        uno::Sequence<beans::PropertyValue> aSeq(2);
+        uno::Sequence<beans::PropertyValue> aSeq(3);
         beans::PropertyValue* pValues = aSeq.getArray();
         pValues[0].Name = "OpenFlags";
         pValues[0].Value <<= OUString("-RB");
         pValues[1].Name = "Referer";
         pValues[1].Value <<= OUString("private:user");
+        pValues[2].Name = "ReadOnly";
+        pValues[2].Value <<= (sTempURL != cFactory);
         uno::Any aArgs(aSeq);
 
         xPrSet->setPropertyValue( "LoaderArguments", aArgs );