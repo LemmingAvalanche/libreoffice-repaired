LemmingAvalanche/libreoffice-repaired

commit 2cb2e5e5b3806c28a113e35ae75c870356741aab

tdf#98407 PathSubstitution: Add substitution for $(username)
This allows to use the username as a placeholder in the config paths (Autotext, Gallery, etc)

Change-Id: I76434e980cd8ec8785a5587d0bc5fdd67dc42de2
Reviewed-on: https://gerrit.libreoffice.org/22901
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
Tested-by: Stephan Bergmann <sbergman@redhat.com>

framework/source/services/substitutepathvars.cxx
@@ -167,6 +167,7 @@ enum PreDefVariable
     PREDEFVAR_HOME,
     PREDEFVAR_TEMP,
     PREDEFVAR_PATH,
+    PREDEFVAR_USERNAME,
     PREDEFVAR_LANGID,
     PREDEFVAR_VLANG,
     PREDEFVAR_INSTPATH,
@@ -350,6 +351,7 @@ static const FixedVariable aFixedVarTable[] =
     { "$(home)",        PREDEFVAR_HOME,         true  },
     { "$(temp)",        PREDEFVAR_TEMP,         true  },
     { "$(path)",        PREDEFVAR_PATH,         true  },
+    { "$(username)",    PREDEFVAR_USERNAME,     false },
     { "$(langid)",      PREDEFVAR_LANGID,       false },
     { "$(vlang)",       PREDEFVAR_VLANG,        false },
     { "$(instpath)",    PREDEFVAR_INSTPATH,     true  },
@@ -1238,6 +1240,12 @@ void SubstitutePathVariables::SetPredefinedPathVariables()
         m_aPreDefVars.m_FixedVar[ PREDEFVAR_PROGURL ]  = m_aPreDefVars.m_FixedVar[ PREDEFVAR_PROGPATH ];
         m_aPreDefVars.m_FixedVar[ PREDEFVAR_PROG ]     = m_aPreDefVars.m_FixedVar[ PREDEFVAR_PROGPATH ];
     }
+
+    // Set $(username)
+    OUString aSystemUser;
+    ::osl::Security aSecurity;
+    aSecurity.getUserName( aSystemUser, false );
+    m_aPreDefVars.m_FixedVar[ PREDEFVAR_USERNAME ]   = aSystemUser;
 
     // Detect the language type of the current office
     m_aPreDefVars.m_eLanguageType = LANGUAGE_ENGLISH_US;

framework/qa/complex/path_substitution/PathSubstitutionTest.java
@@ -54,6 +54,7 @@ public class PathSubstitutionTest
         substVars.add("$(home)", true, true);
         substVars.add("$(temp)", true, true);
         substVars.add("$(lang)", false, false);
+        substVars.add("$(username)", false, false);
         substVars.add("$(langid)", false, false);
         substVars.add("$(vlang)", false, false);
         // path won't resubstitute

include/osl/security.h
@@ -118,6 +118,16 @@ SAL_DLLPUBLIC sal_Bool SAL_CALL osl_getUserIdent(
 SAL_DLLPUBLIC sal_Bool SAL_CALL osl_getUserName(
         oslSecurity Security, rtl_uString **strName);
 
+/** Get the login name for the user of this security handle,
+    excluding the domain name on Windows.
+    @param[in] Security the security handle.
+    @param[out] strName the string that receives the user name on success.
+    @return True, if the security handle is valid, otherwise False.
+    @since LibreOffice 5.2
+*/
+SAL_DLLPUBLIC sal_Bool SAL_CALL osl_getShortUserName(
+        oslSecurity Security, rtl_uString **strName);
+
 /** Get the home directory of the user of this security handle.
     @param[in] Security the security handle.
     @param[out] strDirectory the string that receives the directory path on success.

include/osl/security.hxx
@@ -69,9 +69,11 @@ inline bool Security::getUserIdent( rtl::OUString& strIdent) const
 }
 
 
-inline bool Security::getUserName( rtl::OUString& strName ) const
+inline bool Security::getUserName( rtl::OUString& strName, bool bIncludeDomain ) const
 {
-    return osl_getUserName( m_handle, &strName.pData );
+    if (bIncludeDomain)
+        return osl_getUserName( m_handle, &strName.pData );
+    return osl_getShortUserName( m_handle, &strName.pData );
 }
 
 

include/osl/security_decl.hxx
@@ -76,9 +76,11 @@ public:
 
     /** get the name of the logged in user.
         @param[out] strName is the OUString which returns the name
+        @param[in] bIncludeDomain Include the Domain name (like "ORG\username"). Affects Windows only.
+                                  This parameter is available since LibreOffice 5.2.
         @return True, if any user is successfully logged in, otherwise False
     */
-    inline bool SAL_CALL getUserName( rtl::OUString& strName) const;
+    inline bool SAL_CALL getUserName( rtl::OUString& strName, bool bIncludeDomain=true ) const;
 
     /** get the home directory of the logged in user.
         @param[out] strDirectory is the OUString which returns the directory name

odk/examples/DevelopersGuide/OfficeDev/PathSubstitution/PathSubstitutionTest.java
@@ -44,7 +44,7 @@ public class PathSubstitutionTest {
      * the path substitution service.
      */
     private static String[] predefinedPathVariables = {
-        "$(home)","$(inst)","$(prog)","$(temp)","$(user)",
+        "$(home)","$(inst)","$(prog)","$(temp)","$(user)", "$(username)",
         "$(work)","$(path)","$(langid)","$(vlang)"
     };
 

offapi/com/sun/star/util/PathSubstitution.idl
@@ -58,6 +58,8 @@ module com {  module sun {  module star {  module  util {
     <dd>The current temporary directory.</dd>
     <dt>\$(path)</dt>
     <dd>The value of PATH environment variable.</dd>
+    <dt>\$(username)</dt>
+    <dd>The username (login name) of the currently active user, excluding the domain name on Windows. (Available since LibreOffice 5.2)</dd>
     <dt>\$(langid)</dt>
     <dd>The language code used by the Office, like 0x0009=English, 0x0409=English US.</dd>
     <dt>\$(vlang)</dt>
@@ -65,7 +67,7 @@ module com {  module sun {  module star {  module  util {
     </dl>
     <p>
         Attention: Most predefined variables describe an absolute path.
-        The only exceptions are: \$(langid) and \$(vlang).
+        The only exceptions are: \$(username), \$(langid) and \$(vlang).
         Therefore the service implementation should only substitute variables which
         are located at the start of a provided path string or are part of a multi-path.
         This special service is not designed to be a text substiution but shall

sal/osl/unx/security.cxx
@@ -66,7 +66,7 @@ static bool sysconf_SC_GETPW_R_SIZE_MAX(std::size_t * value) {
            way and always set EINVAL, so be resilient here: */
         return false;
     } else {
-        SAL_WARN_IF( m < 0 || (unsigned long) m >= std::numeric_limits<std::size_t>::max(), "sal.osl", 
+        SAL_WARN_IF( m < 0 || (unsigned long) m >= std::numeric_limits<std::size_t>::max(), "sal.osl",
                 "m < 0 || (unsigned long) m >= std::numeric_limits<std::size_t>::max()");
         *value = (std::size_t) m;
         return true;
@@ -259,6 +259,11 @@ sal_Bool SAL_CALL osl_getUserName(oslSecurity Security, rtl_uString **ustrName)
     SAL_WARN_IF(*ustrName == nullptr, "sal.osl", "ustrName == NULL");
 
     return bRet;
+}
+
+sal_Bool SAL_CALL osl_getShortUserName(oslSecurity Security, rtl_uString **ustrName)
+{
+    return osl_getUserName(Security, ustrName); // No domain name on unix
 }
 
 static bool SAL_CALL osl_psz_getUserName(oslSecurity Security, sal_Char* pszName, sal_uInt32  nMax)

commit 01685436d20e787563d12c0395396fc92bc101d1

tdf#101898: Only re-substitute whole segments with $(username)
...otherwise, if $(username) happens to be "user", it will endlessly re-
substitute inside a (already partly re-substituted) URL of the form

  $(user)/...

Regression introduced with 2cb2e5e5b3806c28a113e35ae75c870356741aab "tdf#98407
PathSubstitution: Add substitution for $(username)".

Change-Id: I1c8b64f383fdfd97fa5edc192e9ca4b46944d6f1

framework/source/services/substitutepathvars.cxx
@@ -1096,7 +1096,8 @@ throw ( RuntimeException )
             if ( nPos >= 0 )
             {
                 bool bMatch = true;
-                if ( i->eVariable == PREDEFVAR_LANGID ||
+                if ( i->eVariable == PREDEFVAR_USERNAME ||
+                     i->eVariable == PREDEFVAR_LANGID ||
                      i->eVariable == PREDEFVAR_VLANG )
                 {
                     // Special path variables as they can occur in the middle of a path. Only match if they