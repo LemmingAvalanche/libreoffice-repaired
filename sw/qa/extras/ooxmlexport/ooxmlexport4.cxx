LemmingAvalanche/libreoffice-repaired

commit d0fe508b3a4fd6ceee7a0980c94a019cad9e97bc

tdf#83227 oox: reuse RelId in DML/VML export for the same graphic
So that large images are written only once to the ZIP container when
they are exported using both markups. This affects drawinglayer images,
the Writer ones are handled directly in sw and were already
deduplicated.

Change-Id: Iff7c769329b42939833056b727b070f6a60da5e3

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -4125,6 +4125,22 @@ void DocxAttributeOutput::WriteSrcRect(const SdrObject* pSdrObj )
 void DocxAttributeOutput::ClearRelIdCache()
 {
     m_aRelIdCache.clear();
+    m_aSdrRelIdCache.clear();
+}
+
+OUString DocxAttributeOutput::FindRelId(BitmapChecksum nChecksum)
+{
+    OUString aRet;
+
+    if (m_aSdrRelIdCache.find(nChecksum) != m_aSdrRelIdCache.end())
+        aRet = m_aSdrRelIdCache[nChecksum];
+
+    return aRet;
+}
+
+void DocxAttributeOutput::CacheRelId(BitmapChecksum nChecksum, const OUString& rRelId)
+{
+     m_aSdrRelIdCache[nChecksum] = rRelId;
 }
 
 void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size& rSize, const SwFlyFrameFormat* pOLEFrameFormat, SwOLENode* pOLENode, const SdrObject* pSdrObj )

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -906,6 +906,8 @@ private:
 
     /// RelId <-> Graphic* cache, so that in case of alternate content, the same graphic only gets written once.
     std::map<const Graphic*, OString> m_aRelIdCache;
+    /// RelId <-> BitmapChecksum cache, similar to m_aRelIdCache, but used for non-Writer graphics, handled in oox.
+    std::map<BitmapChecksum, OUString> m_aSdrRelIdCache;
 
     /// members to control the existence of grabbagged SDT properties in the paragraph
     sal_Int32 m_nParagraphSdtPrToken;
@@ -963,6 +965,8 @@ public:
     virtual void WriteVMLTextBox(css::uno::Reference<css::drawing::XShape> xShape) SAL_OVERRIDE;
     /// DMLTextExport
     virtual void WriteTextBox(css::uno::Reference<css::drawing::XShape> xShape) SAL_OVERRIDE;
+    virtual OUString FindRelId(BitmapChecksum nChecksum) SAL_OVERRIDE;
+    virtual void CacheRelId(BitmapChecksum nChecksum, const OUString& rRelId) SAL_OVERRIDE;
     virtual oox::drawingml::DrawingML& GetDrawingML() SAL_OVERRIDE;
 
     void BulletDefinition(int nId, const Graphic& rGraphic, Size aSize) SAL_OVERRIDE;

sw/qa/extras/ooxmlexport/ooxmlexport4.cxx
@@ -800,6 +800,18 @@ DECLARE_OOXMLEXPORT_TEST(testSimpleSdts, "simple-sdts.docx")
 
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf83227, "tdf83227.docx")
+{
+    // Bug document contains a rotated image, which is handled as a draw shape (not as a Writer image) on export.
+    if (!mbExported)
+        return;
+
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(comphelper::getComponentContext(m_xSFactory), maTempFile.GetURL());
+    CPPUNIT_ASSERT_EQUAL(true, bool(xNameAccess->hasByName("word/media/image1.png")));
+    // This was also true, image was written twice.
+    CPPUNIT_ASSERT_EQUAL(false, bool(xNameAccess->hasByName("word/media/image2.png")));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/oox/export/drawingml.hxx
@@ -74,6 +74,10 @@ public:
     virtual void WriteOutliner(const OutlinerParaObject& rParaObj) = 0;
     /// Write the contents of the textbox that is associated to this shape.
     virtual void WriteTextBox(css::uno::Reference<css::drawing::XShape> xShape) = 0;
+    /// Look up the RelId of a graphic based on its checksum.
+    virtual OUString FindRelId(BitmapChecksum nChecksum) = 0;
+    /// Store the RelId of a graphic based on its checksum.
+    virtual void CacheRelId(BitmapChecksum nChecksum, const OUString& rRelId) = 0;
 protected:
     DMLTextExport() {}
     virtual ~DMLTextExport() {}

include/oox/export/vmlexport.hxx
@@ -39,6 +39,10 @@ public:
     virtual oox::drawingml::DrawingML& GetDrawingML() = 0;
     /// Write the contents of the textbox that is associated to this shape in VML format.
     virtual void WriteVMLTextBox(css::uno::Reference<css::drawing::XShape> xShape) = 0;
+    /// Look up the RelId of a graphic based on its checksum.
+    virtual OUString FindRelId(BitmapChecksum nChecksum) = 0;
+    /// Store the RelId of a graphic based on its checksum.
+    virtual void CacheRelId(BitmapChecksum nChecksum, const OUString& rRelId) = 0;
 protected:
     VMLTextExport() {}
     virtual ~VMLTextExport() {}

oox/source/export/drawingml.cxx
@@ -772,7 +772,7 @@ void DrawingML::WriteOutline( Reference<XPropertySet> rXPropSet )
     mpFS->endElementNS( XML_a, XML_ln );
 }
 
-OUString DrawingML::WriteImage( const OUString& rURL, bool bRelPathToMedia )
+bool lcl_URLToGraphic(const OUString& rURL, Graphic& rGraphic)
 {
     OString aURLBS(OUStringToOString(rURL, RTL_TEXTENCODING_UTF8));
 
@@ -781,9 +781,18 @@ OUString DrawingML::WriteImage( const OUString& rURL, bool bRelPathToMedia )
 
     if ( index != -1 )
     {
-        SAL_INFO("oox.shape", "begin: " << sizeof( aURLBegin ) << " " <<  USS( rURL ) + RTL_CONSTASCII_LENGTH( aURLBegin ));
-        Graphic aGraphic = GraphicObject( aURLBS.copy(RTL_CONSTASCII_LENGTH(aURLBegin)) ).GetTransformedGraphic ();
+        rGraphic = GraphicObject(aURLBS.copy(RTL_CONSTASCII_LENGTH(aURLBegin))).GetTransformedGraphic();
+        return true;
+    }
 
+    return false;
+}
+
+OUString DrawingML::WriteImage( const OUString& rURL, bool bRelPathToMedia )
+{
+    Graphic aGraphic;
+    if (lcl_URLToGraphic(rURL, aGraphic))
+    {
         return WriteImage( aGraphic , bRelPathToMedia );
     }
     else
@@ -933,7 +942,23 @@ OUString DrawingML::WriteImage( const Graphic& rGraphic , bool bRelPathToMedia )
 
 OUString DrawingML::WriteBlip( Reference< XPropertySet > rXPropSet, const OUString& rURL, bool bRelPathToMedia, const Graphic *pGraphic )
 {
-    OUString sRelId = pGraphic ? WriteImage( *pGraphic, bRelPathToMedia ) : WriteImage( rURL, bRelPathToMedia );
+    OUString sRelId;
+    BitmapChecksum nChecksum = 0;
+    if (!rURL.isEmpty() && mpTextExport)
+    {
+        Graphic aGraphic;
+        if (lcl_URLToGraphic(rURL, aGraphic))
+        {
+            nChecksum = aGraphic.GetChecksum();
+            sRelId = mpTextExport->FindRelId(nChecksum);
+        }
+    }
+    if (sRelId.isEmpty())
+    {
+        sRelId = pGraphic ? WriteImage( *pGraphic, bRelPathToMedia ) : WriteImage( rURL, bRelPathToMedia );
+        if (!rURL.isEmpty() && mpTextExport)
+            mpTextExport->CacheRelId(nChecksum, sRelId);
+    }
     sal_Int16 nBright = 0;
     sal_Int32 nContrast = 0;
 

oox/source/export/vmlexport.cxx
@@ -586,7 +586,14 @@ void VMLExport::Commit( EscherPropertyContainer& rProps, const Rectangle& rRect 
                         aStream.Seek(0);
                         Graphic aGraphic;
                         GraphicConverter::Import(aStream, aGraphic);
-                        OUString aImageId = m_pTextExport->GetDrawingML().WriteImage( aGraphic );
+
+                        BitmapChecksum nChecksum = aGraphic.GetChecksum();
+                        OUString aImageId = m_pTextExport->FindRelId(nChecksum);
+                        if (aImageId.isEmpty())
+                        {
+                            aImageId = m_pTextExport->GetDrawingML().WriteImage( aGraphic );
+                            m_pTextExport->CacheRelId(nChecksum, aImageId);
+                        }
                         pAttrList->add(FSNS(XML_r, XML_id), OUStringToOString(aImageId, RTL_TEXTENCODING_UTF8));
                         imageData = true;
                     }

commit b9e336157c6dd49f074a5330f090a9ed1082b564

tdf#103001 DOCX export: fix RelId cache when switching streams
RelIds are used to refer to external streams, e.g. images. A RelId cache
tries to avoid storing the same image more than once in the export
result. RelIds are relative to an XML stream, so caching them across
stream switches is problematic. There was code already to notify the
cache after a header or footer was written, but:

1) It was only done after a switch, not before and

2) It cleared the whole cache, instead of stashing it away, and
restoring it when the write of the special stream is done.

Regression from commit d0fe508b3a4fd6ceee7a0980c94a019cad9e97bc
(tdf#83227 oox: reuse RelId in DML/VML export for the same graphic,
2015-09-07), this existing problem became more visible when caching
started to include draw images, not just writer ones.

Fix both problems by turning the cache into a stack that is
pushed/popped around stream changes.

Change-Id: If9ec168823eea5e272197e28f6125ba626605550
Reviewed-on: https://gerrit.libreoffice.org/29684
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -4166,25 +4166,34 @@ void DocxAttributeOutput::WriteSrcRect(const SdrObject* pSdrObj )
     }
 }
 
-void DocxAttributeOutput::ClearRelIdCache()
+void DocxAttributeOutput::PopRelIdCache()
 {
-    m_aRelIdCache.clear();
-    m_aSdrRelIdCache.clear();
+    if (!m_aRelIdCache.empty())
+        m_aRelIdCache.pop();
+    if (!m_aSdrRelIdCache.empty())
+        m_aSdrRelIdCache.pop();
+}
+
+void DocxAttributeOutput::PushRelIdCache()
+{
+    m_aRelIdCache.push(std::map<const Graphic*, OString>());
+    m_aSdrRelIdCache.push(std::map<BitmapChecksum, OUString>());
 }
 
 OUString DocxAttributeOutput::FindRelId(BitmapChecksum nChecksum)
 {
     OUString aRet;
 
-    if (m_aSdrRelIdCache.find(nChecksum) != m_aSdrRelIdCache.end())
-        aRet = m_aSdrRelIdCache[nChecksum];
+    if (!m_aSdrRelIdCache.empty() && m_aSdrRelIdCache.top().find(nChecksum) != m_aSdrRelIdCache.top().end())
+        aRet = m_aSdrRelIdCache.top()[nChecksum];
 
     return aRet;
 }
 
 void DocxAttributeOutput::CacheRelId(BitmapChecksum nChecksum, const OUString& rRelId)
 {
-     m_aSdrRelIdCache[nChecksum] = rRelId;
+    if (!m_aSdrRelIdCache.empty())
+        m_aSdrRelIdCache.top()[nChecksum] = rRelId;
 }
 
 void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size& rSize, const SwFlyFrameFormat* pOLEFrameFormat, SwOLENode* pOLENode, const SdrObject* pSdrObj )
@@ -4222,9 +4231,9 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
         else
             pGraphic = pOLENode->GetGraphic();
 
-        if (m_aRelIdCache.find(pGraphic) != m_aRelIdCache.end())
+        if (!m_aRelIdCache.empty() && m_aRelIdCache.top().find(pGraphic) != m_aRelIdCache.top().end())
             // We already have a RelId for this Graphic.
-            aRelId = m_aRelIdCache[pGraphic];
+            aRelId = m_aRelIdCache.top()[pGraphic];
         else
         {
             // Not in cache, then need to write it.
@@ -4233,7 +4242,8 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
             OUString aImageId = m_rDrawingML.WriteImage( *pGraphic );
 
             aRelId = OUStringToOString( aImageId, RTL_TEXTENCODING_UTF8 );
-            m_aRelIdCache[pGraphic] = aRelId;
+            if (!m_aRelIdCache.empty())
+                m_aRelIdCache.top()[pGraphic] = aRelId;
         }
 
         nImageType = XML_embed;
@@ -8497,6 +8507,10 @@ DocxAttributeOutput::DocxAttributeOutput( DocxExport &rExport, FSHelperPtr pSeri
     , m_nStateOfFlyFrame( FLY_NOT_PROCESSED )
     , m_bParagraphSdtHasId(false)
 {
+    // Push initial items to the RelId cache. In case the document contains no
+    // special streams (headers, footers, etc.) then these items are used
+    // during the full export.
+    PushRelIdCache();
 }
 
 DocxAttributeOutput::~DocxAttributeOutput()

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -371,7 +371,8 @@ public:
 
     void WriteBookmarks_Impl( std::vector< OUString >& rStarts, std::vector< OUString >& rEnds );
     void WriteAnnotationMarks_Impl( std::vector< OUString >& rStarts, std::vector< OUString >& rEnds );
-    void ClearRelIdCache();
+    void PushRelIdCache();
+    void PopRelIdCache();
     /// End possibly opened paragraph sdt block.
     void EndParaSdtBlock();
 
@@ -915,9 +916,9 @@ private:
     bool m_setFootnote;
 
     /// RelId <-> Graphic* cache, so that in case of alternate content, the same graphic only gets written once.
-    std::map<const Graphic*, OString> m_aRelIdCache;
+    std::stack< std::map<const Graphic*, OString> > m_aRelIdCache;
     /// RelId <-> BitmapChecksum cache, similar to m_aRelIdCache, but used for non-Writer graphics, handled in oox.
-    std::map<BitmapChecksum, OUString> m_aSdrRelIdCache;
+    std::stack< std::map<BitmapChecksum, OUString> > m_aSdrRelIdCache;
 
     /// members to control the existence of grabbagged SDT properties in the paragraph
     sal_Int32 m_nParagraphSdtPrToken;

sw/source/filter/ww8/docxexport.cxx
@@ -729,14 +729,14 @@ void DocxExport::WriteHeaderFooter( const SwFormat& rFormat, bool bHeader, const
 
     DocxTableExportContext aTableExportContext;
     m_pAttrOutput->pushToTableExportContext(aTableExportContext);
-    // do the work
-    WriteHeaderFooterText( rFormat, bHeader );
-    m_pAttrOutput->popFromTableExportContext(aTableExportContext);
-    m_pAttrOutput->EndParaSdtBlock();
-
     //When the stream changes the cache which is maintained for the graphics in case of alternate content is not cleared.
     //So clearing the alternate content graphic cache.
-    m_pAttrOutput->ClearRelIdCache();
+    m_pAttrOutput->PushRelIdCache();
+    // do the work
+    WriteHeaderFooterText( rFormat, bHeader );
+    m_pAttrOutput->PopRelIdCache();
+    m_pAttrOutput->popFromTableExportContext(aTableExportContext);
+    m_pAttrOutput->EndParaSdtBlock();
 
     // switch the serializer back
     m_pAttrOutput->SetSerializer( m_pDocumentFS );

sw/qa/extras/ooxmlexport/ooxmlexport4.cxx
@@ -958,6 +958,19 @@ DECLARE_OOXMLEXPORT_TEST(testTdf83227, "tdf83227.docx")
     CPPUNIT_ASSERT_EQUAL(false, bool(xNameAccess->hasByName("word/media/image2.png")));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf103001, "tdf103001.docx")
+{
+    // The same image is featured in the header and in the body text, make sure
+    // the header relation is still written, even when caching is enabled.
+    if (!mbExported)
+        return;
+
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(comphelper::getComponentContext(m_xSFactory), maTempFile.GetURL());
+    // This failed: header reused the RelId of the body text, even if RelIds
+    // are local to their stream.
+    CPPUNIT_ASSERT(xNameAccess->hasByName("word/_rels/header1.xml.rels"));
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf92521, "tdf92521.odt")
 {
     if (xmlDocPtr pXmlDoc = parseExport("word/document.xml"))