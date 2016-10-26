LemmingAvalanche/libreoffice-repaired

commit fda66dc3aee65515348544a2645964575bbff99c

tdf#87905: Use darker background colour for master view
for the area behind the slide, that is

Change-Id: Ie020f43a81e5eaa257c20d0a7b6feffce1614205
Reviewed-on: https://gerrit.libreoffice.org/16466
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Philippe Jung <phil.jung@free.fr>

sd/source/ui/inc/DrawViewShell.hxx
@@ -388,6 +388,9 @@ public:
 
     OUString GetSidebarContextName() const;
 
+    const Color& GetAppBackgroundColor() const { return mnAppBackgroundColor; }
+    void SetAppBackgroundColor( Color nNewColor )  { mnAppBackgroundColor = nNewColor; }
+
     //move this method to ViewShell.
     //void  NotifyAccUpdate();
 protected:
@@ -519,6 +522,9 @@ private:
     ::std::unique_ptr< ViewOverlayManager > mpViewOverlayManager;
 
     std::vector<std::unique_ptr<SdrExternalToolEdit>> m_ExternalEdits;
+
+    // The colour of the area behind the slide (used to be called "Wiese")
+    Color mnAppBackgroundColor;
 };
 
 } // end of namespace sd

sd/source/ui/view/drviews1.cxx
@@ -316,6 +316,7 @@ bool DrawViewShell::PrepareClose( bool bUI )
     return true;
 }
 
+
 /**
  * Set status (enabled/disabled) of menu SfxSlots
  */
@@ -377,6 +378,9 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
                 GetViewShellBase().GetToolBarManager()->ResetToolBars(ToolBarManager::TBG_COMMON_TASK);
         }
 
+        svtools::ColorConfig aColorConfig;
+        Color aFillColor = Color( aColorConfig.GetColorValue( svtools::APPBACKGROUND ).nColor );
+
         if (meEditMode == EM_PAGE)
         {
             /******************************************************************
@@ -401,6 +405,8 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
             }
 
             maTabControl->SetCurPageId(nActualPageNum + 1);
+
+            SetAppBackgroundColor( aFillColor );
 
             SwitchPage(nActualPageNum);
         }
@@ -437,6 +443,9 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
                     nActualMasterPageNum = i;
                 }
             }
+
+            aFillColor.DecreaseLuminance( 64 );
+            SetAppBackgroundColor( aFillColor );
 
             maTabControl->SetCurPageId(nActualMasterPageNum + 1);
             SwitchPage(nActualMasterPageNum);
@@ -477,6 +486,7 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
         Invalidate( SID_TITLE_MASTERPAGE );
         Invalidate( SID_NOTES_MASTERPAGE );
         Invalidate( SID_HANDOUT_MASTERPAGE );
+        InvalidateWindows();
 
         SetContextName(GetSidebarContextName());
     }

sd/source/ui/view/drviews5.cxx
@@ -406,19 +406,13 @@ void DrawViewShell::PrePaint()
  */
 void DrawViewShell::Paint(const Rectangle& rRect, ::sd::Window* pWin)
 {
-    // Fill var FillColor here to have it available on later call
-    svtools::ColorConfig aColorConfig;
-    Color aFillColor;
-
-    aFillColor = Color( aColorConfig.GetColorValue( svtools::APPBACKGROUND ).nColor );
-
     /* This is done before each text edit, so why not do it before every paint.
                 The default language is only used if the outliner only contains one
                 character in a symbol font */
     GetDoc()->GetDrawOutliner( NULL ).SetDefaultLanguage( GetDoc()->GetLanguage( EE_CHAR_LANGUAGE ) );
 
     // Set Application Background color for usage in SdrPaintView(s)
-    mpDrawView->SetApplicationBackgroundColor(aFillColor);
+    mpDrawView->SetApplicationBackgroundColor(GetAppBackgroundColor());
 
     /* This is done before each text edit, so why not do it before every paint.
                 The default language is only used if the outliner only contains one

sd/source/ui/view/drviewsa.cxx
@@ -209,6 +209,9 @@ void DrawViewShell::Construct(DrawDocShell* pDocSh, PageKind eInitialPageKind)
     mbPastePossible = false;
     mbIsLayerModeActive = false;
 
+    svtools::ColorConfig aColorConfig;
+    mnAppBackgroundColor = Color( aColorConfig.GetColorValue( svtools::APPBACKGROUND ).nColor );
+
     mpFrameView->Connect();
 
     OSL_ASSERT (GetViewShell()!=NULL);

commit 6887fa24ea56beb4cad52f1fc7627056e298d887

tdf#96948 Change color of the area behind Impress slide immediately
after saving Options > Application colours > Application background.

This fixes a regression from fda66dc3aee65515348544a2645964575bbff99c.

Change-Id: I8dc0caf9e351c0b1bf2a10e9097d8f3e895f1471
Reviewed-on: https://gerrit.libreoffice.org/29957
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

sd/Library_sd.mk
@@ -475,6 +475,7 @@ $(eval $(call gb_Library_add_exception_objects,sd,\
 	sd/source/ui/view/drviewsh \
 	sd/source/ui/view/drviewsi \
 	sd/source/ui/view/drviewsj \
+	sd/source/ui/view/drviewsk \
 	sd/source/ui/view/drvwshrg \
 	sd/source/ui/view/frmview \
 	sd/source/ui/view/grviewsh \

sd/inc/sdmod.hxx
@@ -43,6 +43,7 @@ class SvNumberFormatter;
 class SfxErrorHandler;
 class SdDrawDocument;
 class SfxFrame;
+namespace svtools { class ColorConfig; }
 
 namespace sd {
 class DrawDocShell;
@@ -129,6 +130,8 @@ public:
     SdExtPropertySetInfoCache gImplDrawPropertySetInfoCache;
     SdTypesCache gImplTypesCache;
 
+    svtools::ColorConfig& GetColorConfig();
+
 protected:
 
     SdOptions*              pImpressOptions;
@@ -176,6 +179,7 @@ private:
     */
     DECL_STATIC_LINK( SdModule, EventListenerHdl, VclSimpleEvent&, void );
 
+    std::unique_ptr<svtools::ColorConfig> mpColorConfig;
 };
 
 #define SD_MOD() ( static_cast<SdModule*>(SfxApplication::GetModule(SfxToolsModule::Draw)) )

sd/source/ui/app/sdmod.cxx
@@ -40,6 +40,7 @@
 #include <svx/svxerr.hxx>
 
 #include <svx/xmlsecctrl.hxx>
+#include <svtools/colorcfg.hxx>
 
 #include "sderror.hxx"
 #include "sdmod.hxx"
@@ -77,7 +78,8 @@ SdModule::SdModule(SfxObjectFactory* pFact1, SfxObjectFactory* pFact2 )
     pNumberFormatter( nullptr ),
     bWaterCan(false),
     mpResourceContainer(new ::sd::SdGlobalResourceContainer()),
-    mbEventListenerAdded(false)
+    mbEventListenerAdded(false),
+    mpColorConfig(new svtools::ColorConfig)
 {
     SetName( "StarDraw" );  // Do not translate!
     pSearchItem = new SvxSearchItem(SID_SEARCH_ITEM);
@@ -218,6 +220,11 @@ SvNumberFormatter* SdModule::GetNumberFormatter()
 OutputDevice* SdModule::GetRefDevice (::sd::DrawDocShell& )
 {
     return GetVirtualRefDevice();
+}
+
+svtools::ColorConfig& SdModule::GetColorConfig()
+{
+    return *mpColorConfig;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sd/source/ui/inc/DrawViewShell.hxx
@@ -30,6 +30,7 @@
 #include <com/sun/star/lang/XEventListener.hpp>
 #include <com/sun/star/scanner/XScannerManager2.hpp>
 #include <unotools/caserotate.hxx>
+#include <unotools/options.hxx>
 
 class Outliner;
 class SdPage;
@@ -65,7 +66,8 @@ class ViewOverlayManager;
 */
 class DrawViewShell
     : public ViewShell,
-      public SfxListener
+      public SfxListener,
+      public utl::ConfigurationListener
 {
 public:
     SFX_DECL_INTERFACE(SD_IF_SDDRAWVIEWSHELL)
@@ -361,8 +363,6 @@ public:
 
     OUString GetSidebarContextName() const;
 
-    const Color& GetAppBackgroundColor() const { return mnAppBackgroundColor; }
-    void SetAppBackgroundColor( Color nNewColor )  { mnAppBackgroundColor = nNewColor; }
     bool IsInSwitchPage() { return mbIsInSwitchPage; }
 
     //move this method to ViewShell.
@@ -488,6 +488,10 @@ private:
     ::std::unique_ptr< ViewOverlayManager > mpViewOverlayManager;
 
     std::vector<std::unique_ptr<SdrExternalToolEdit>> m_ExternalEdits;
+
+    virtual void ConfigurationChanged( utl::ConfigurationBroadcaster* pCb, sal_uInt32 ) override;
+
+    void ConfigureAppBackgroundColor( svtools::ColorConfig* pColorConfig = nullptr );
 
     // The colour of the area behind the slide (used to be called "Wiese")
     Color mnAppBackgroundColor;

sd/source/ui/view/drviews1.cxx
@@ -380,10 +380,7 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
                 GetViewShellBase().GetToolBarManager()->ResetToolBars(ToolBarManager::TBG_COMMON_TASK);
         }
 
-        svtools::ColorConfig aColorConfig;
-        Color aFillColor( aColorConfig.GetColorValue( svtools::APPBACKGROUND ).nColor );
-        if (comphelper::LibreOfficeKit::isActive())
-            aFillColor = COL_TRANSPARENT;
+        ConfigureAppBackgroundColor();
 
         if (meEditMode == EditMode::Page)
         {
@@ -409,8 +406,6 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
             }
 
             maTabControl->SetCurPageId(nActualPageNum + 1);
-
-            SetAppBackgroundColor( aFillColor );
 
             SwitchPage(nActualPageNum);
 
@@ -450,9 +445,6 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
                     nActualMasterPageNum = i;
                 }
             }
-
-            aFillColor.DecreaseLuminance( 64 );
-            SetAppBackgroundColor( aFillColor );
 
             maTabControl->SetCurPageId(nActualMasterPageNum + 1);
             SwitchPage(nActualMasterPageNum);

sd/source/ui/view/drviews4.cxx
@@ -20,7 +20,6 @@
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
 
 #include "DrawViewShell.hxx"
-#include <comphelper/lok.hxx>
 #include <vcl/msgbox.hxx>
 #include <svl/urlbmk.hxx>
 #include <svx/svdpagv.hxx>
@@ -342,14 +341,8 @@ void DrawViewShell::MouseMove(const MouseEvent& rMEvt, ::sd::Window* pWin)
         // is needed it is necessary to set it here.
         if (GetDoc())
         {
-            svtools::ColorConfig aColorConfig;
-            Color aFillColor;
-
-            aFillColor = Color( aColorConfig.GetColorValue( svtools::APPBACKGROUND ).nColor );
-            if (comphelper::LibreOfficeKit::isActive())
-                aFillColor = COL_TRANSPARENT;
-
-            mpDrawView->SetApplicationBackgroundColor(aFillColor);
+            ConfigureAppBackgroundColor();
+            mpDrawView->SetApplicationBackgroundColor( mnAppBackgroundColor );
         }
 
         ViewShell::MouseMove(rMEvt, pWin);

sd/source/ui/view/drviews5.cxx
@@ -414,7 +414,7 @@ void DrawViewShell::Paint(const Rectangle& rRect, ::sd::Window* pWin)
     GetDoc()->GetDrawOutliner().SetDefaultLanguage( GetDoc()->GetLanguage( EE_CHAR_LANGUAGE ) );
 
     // Set Application Background color for usage in SdrPaintView(s)
-    mpDrawView->SetApplicationBackgroundColor(GetAppBackgroundColor());
+    mpDrawView->SetApplicationBackgroundColor( mnAppBackgroundColor );
 
     /* This is done before each text edit, so why not do it before every paint.
                 The default language is only used if the outliner only contains one

sd/source/ui/view/drviewsa.cxx
@@ -20,7 +20,6 @@
 #include "DrawViewShell.hxx"
 #include <com/sun/star/scanner/ScannerManager.hpp>
 #include <cppuhelper/implbase.hxx>
-#include <comphelper/lok.hxx>
 #include <comphelper/processfactory.hxx>
 #include <editeng/sizeitem.hxx>
 #include <svx/svdlayer.hxx>
@@ -128,10 +127,15 @@ DrawViewShell::DrawViewShell( SfxViewFrame* pFrame, ViewShellBase& rViewShellBas
     SetContextName(GetSidebarContextName());
 
     doShow();
+
+    ConfigureAppBackgroundColor();
+    SD_MOD()->GetColorConfig().AddListener(this);
 }
 
 DrawViewShell::~DrawViewShell()
 {
+    SD_MOD()->GetColorConfig().RemoveListener(this);
+
     mpSelectionChangeHandler->Disconnect();
 
     mpAnnotationManager.reset();
@@ -204,11 +208,6 @@ void DrawViewShell::Construct(DrawDocShell* pDocSh, PageKind eInitialPageKind)
     mxClipEvtLstnr.clear();
     mbPastePossible = false;
     mbIsLayerModeActive = false;
-
-    svtools::ColorConfig aColorConfig;
-    mnAppBackgroundColor = Color( aColorConfig.GetColorValue( svtools::APPBACKGROUND ).nColor );
-    if (comphelper::LibreOfficeKit::isActive())
-        mnAppBackgroundColor = COL_TRANSPARENT;
 
     mpFrameView->Connect();
 

sd/source/ui/view/drviewsk.cxx
@@ -1,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "DrawViewShell.hxx"
+#include "sdmod.hxx"
+
+#include <comphelper/lok.hxx>
+
+namespace sd {
+
+void DrawViewShell::ConfigurationChanged( utl::ConfigurationBroadcaster* pCb, sal_uInt32 )
+{
+    ConfigureAppBackgroundColor( dynamic_cast<svtools::ColorConfig*>(pCb) );
+}
+
+void DrawViewShell::ConfigureAppBackgroundColor( svtools::ColorConfig *pColorConfig )
+{
+    if (!pColorConfig)
+        pColorConfig = &SD_MOD()->GetColorConfig();
+    Color aFillColor( pColorConfig->GetColorValue( svtools::APPBACKGROUND ).nColor );
+    if (comphelper::LibreOfficeKit::isActive())
+        aFillColor = COL_TRANSPARENT;
+    // tdf#87905 Use darker background color for master view
+    if (meEditMode == EditMode::MasterPage)
+        aFillColor.DecreaseLuminance( 64 );
+    mnAppBackgroundColor = aFillColor;
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */