LemmingAvalanche/libreoffice-repaired

commit 70ffb012898fa768705f7ece3086710f67f484a1

tdf#76349 SpacingWithoutBorders: enable .doc RT
Microsoft allows spacing in textboxes even if the borders are not
shown, but LO does not.  Added a compatible setting, used an existing
setting to allow the spacing, and changed .doc export not to zero
out the spacing if the border was zero-width.

Using the compatible setting in the export code is almost redundant,
but it does require that the document was LOADED as .doc, and not
"save as" from another format.

This patch simply allows round-tripping - any user attempt to modify the
border settings will enforce normal LO border rules.

Change-Id: I60ac036e1bfac381eea15e33c21495ad3800277a
Reviewed-on: https://gerrit.libreoffice.org/28601
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Justin Luth <justin_luth@sil.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/filter/ww8/wrtw8esh.cxx
@@ -1967,6 +1967,7 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
     sal_Int32 nLineWidth=0;
     const SfxPoolItem* pItem;
     bool bFirstLine = true;
+    const bool bAllowSpacingWithoutBorders = rFormat.getIDocumentSettingAccess().get(DocumentSettingId::ALLOW_SPACING_WITHOUT_BORDERS);
     if (SfxItemState::SET == rFormat.GetItemState(RES_BOX, true, &pItem))
     {
         static const o3tl::enumarray<SvxBoxItemLine, sal_uInt16> aExhperProp =
@@ -2034,6 +2035,10 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
                 rPropOpt.AddOpt( aExhperProp[ n ], DrawModelToEmu(
                     static_cast<const SvxBoxItem*>(pItem)->GetDistance( n ) ));
             }
+            else if( bAllowSpacingWithoutBorders )
+            {
+                rPropOpt.AddOpt( aExhperProp[ n ], DrawModelToEmu(static_cast<const SvxBoxItem*>(pItem)->GetDistance( n )) );
+            }
             else
                 // MM If there is no line the distance should be set to 0
                 rPropOpt.AddOpt( aExhperProp[ n ], DrawModelToEmu(0));
@@ -2041,10 +2046,13 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
     if( bFirstLine )                // no valid line found
     {
         rPropOpt.AddOpt( ESCHER_Prop_fNoLineDrawDash, 0x80000 );
-        rPropOpt.AddOpt( ESCHER_Prop_dyTextTop, 0 );
-        rPropOpt.AddOpt( ESCHER_Prop_dyTextBottom, 0 );
-        rPropOpt.AddOpt( ESCHER_Prop_dxTextLeft, 0 );
-        rPropOpt.AddOpt( ESCHER_Prop_dxTextRight, 0 );
+        if( !bAllowSpacingWithoutBorders )
+        {
+            rPropOpt.AddOpt( ESCHER_Prop_dyTextTop, 0 );
+            rPropOpt.AddOpt( ESCHER_Prop_dyTextBottom, 0 );
+            rPropOpt.AddOpt( ESCHER_Prop_dxTextLeft, 0 );
+            rPropOpt.AddOpt( ESCHER_Prop_dxTextRight, 0 );
+        }
     }
     const SwAttrSet& rAttrSet = rFormat.GetAttrSet();
     if (SfxItemState::SET == rAttrSet.GetItemState(RES_BOX, false, &pItem))

sw/inc/IDocumentSettingAccess.hxx
@@ -39,6 +39,7 @@ enum class DocumentSettingId
     ADD_FLY_OFFSETS,
     ADD_VERTICAL_FLY_OFFSETS,
 
+    ALLOW_SPACING_WITHOUT_BORDERS,
     OLD_NUMBERING,
 
     ADD_EXT_LEADING,

sw/qa/extras/ww8export/ww8export.cxx
@@ -734,6 +734,12 @@ DECLARE_WW8EXPORT_TEST(testTdf91083_tableKeep, "tdf91083_tableKeep.doc")
     CPPUNIT_ASSERT_EQUAL(false, getProperty<bool>(xTable, "Split"));
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf76349_textboxMargins, "tdf76349_textboxMargins.doc")
+{
+    // textboxes without borders were losing their spacing items in round-tripping
+    CPPUNIT_ASSERT( 0 < parseDump("/root/page/body/txt/anchored/fly/infos/prtBounds", "left").toInt32() );
+}
+
 DECLARE_WW8EXPORT_TEST(testMoveRange, "fdo66304-1.odt")
 {
     //the save must survive without asserting

sw/source/core/doc/DocumentSettingManager.cxx
@@ -55,6 +55,7 @@ sw::DocumentSettingManager::DocumentSettingManager(SwDoc &rDoc)
 
     mbAddFlyOffsets(false),
     mbAddVerticalFlyOffsets(false),
+    mbAllowSpacingWithoutBorders( false ),
     mbUseHiResolutionVirtualDevice(true),
     mbMathBaselineAlignment(false), // default for *old* documents is 'off'
     mbStylesNoDefault(false),
@@ -146,6 +147,7 @@ bool sw::DocumentSettingManager::get(/*[in]*/ DocumentSettingId id) const
         case DocumentSettingId::TAB_COMPAT: return mbTabCompat; //(n8Dummy1 & DUMMY_TAB_COMPAT);
         case DocumentSettingId::ADD_FLY_OFFSETS: return mbAddFlyOffsets; //(n8Dummy2 & DUMMY_ADD_FLY_OFFSETS);
         case DocumentSettingId::ADD_VERTICAL_FLY_OFFSETS: return mbAddVerticalFlyOffsets;
+        case DocumentSettingId::ALLOW_SPACING_WITHOUT_BORDERS: return mbAllowSpacingWithoutBorders;
         case DocumentSettingId::ADD_EXT_LEADING: return mbAddExternalLeading; //(n8Dummy2 & DUMMY_ADD_EXTERNAL_LEADING);
         case DocumentSettingId::USE_VIRTUAL_DEVICE: return mbUseVirtualDevice; //(n8Dummy1 & DUMMY_USE_VIRTUAL_DEVICE);
         case DocumentSettingId::USE_HIRES_VIRTUAL_DEVICE: return mbUseHiResolutionVirtualDevice; //(n8Dummy2 & DUMMY_USE_HIRES_VIR_DEV);
@@ -220,6 +222,9 @@ void sw::DocumentSettingManager::set(/*[in]*/ DocumentSettingId id, /*[in]*/ boo
             break;
         case DocumentSettingId::ADD_VERTICAL_FLY_OFFSETS:
             mbAddVerticalFlyOffsets = value;
+            break;
+        case DocumentSettingId::ALLOW_SPACING_WITHOUT_BORDERS:
+            mbAllowSpacingWithoutBorders = value;
             break;
         case DocumentSettingId::ADD_EXT_LEADING:
             mbAddExternalLeading = value;

sw/source/core/inc/DocumentSettingManager.hxx
@@ -109,6 +109,7 @@ class DocumentSettingManager :
     bool mbUseVirtualDevice                 : 1;
     bool mbAddFlyOffsets                    : 1;
     bool mbAddVerticalFlyOffsets            : 1;
+    bool mbAllowSpacingWithoutBorders       : 1;    // tdf#41542
     bool mbAddExternalLeading               : 1;
     bool mbUseHiResolutionVirtualDevice     : 1;
     bool mbOldLineSpacing                   : 1;    // #i11859#
@@ -147,7 +148,7 @@ class DocumentSettingManager :
     bool mbClippedPictures;
     bool mbBackgroundParaOverDrawings;
     bool mbTabOverMargin;
-    bool mbTreatSingleColumnBreakAsPageBreak;
+    bool mbTreatSingleColumnBreakAsPageBreak;              // tdf#76349
     bool mbSurroundTextWrapSmall;
     bool mbPropLineSpacingShrinksFirstLine; // fdo#79602
     bool mbSubtractFlys; // tdf#86578

sw/source/core/layout/frmtool.cxx
@@ -1834,7 +1834,15 @@ SwBorderAttrs::SwBorderAttrs(const SwModify *pMod, const SwFrame *pConstructor)
     m_bCachedJoinedWithPrev = false;
     m_bCachedJoinedWithNext = false;
 
-    m_bBorderDist = bool(pConstructor->GetType() & SwFrameType::Cell);
+    bool bAllowSpacingWithoutBorders = false;
+    if( (pConstructor->GetType() & SwFrameType::Fly)
+        && pConstructor->IsLayoutFrame()
+        && pConstructor->GetLower() && pConstructor->GetLower()->IsTextFrame() )
+    {
+        const SwFrameFormat* pFormat = static_cast<const SwLayoutFrame*>(pConstructor)->GetFormat();
+        bAllowSpacingWithoutBorders = pFormat->getIDocumentSettingAccess().get(DocumentSettingId::ALLOW_SPACING_WITHOUT_BORDERS);
+    }
+    m_bBorderDist = bool(pConstructor->GetType() & SwFrameType::Cell) || bAllowSpacingWithoutBorders;
 }
 
 SwBorderAttrs::~SwBorderAttrs()

sw/source/filter/ww8/ww8par.cxx
@@ -1959,6 +1959,7 @@ void SwWW8ImplReader::ImportDop()
     m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::TAB_OVER_MARGIN, true);
     m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::SURROUND_TEXT_WRAP_SMALL, true);
     m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::PROP_LINE_SPACING_SHRINKS_FIRST_LINE, true);
+    m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::ALLOW_SPACING_WITHOUT_BORDERS, true);
 
     // COMPATIBILITY FLAGS END
 

commit deb14d0fe6690db8585ff46d973c6fa8870a3358

fix logic error - still set zero padding if no border set
caused by commit 70ffb012898fa768705f7ece3086710f67f484a1

originally, if there were no visible lines (bFirstLine), then any
line distance was reset to zero, but with AllowPaddingWithoutBorders
that should not be done.

However, the case were there is no boxItem at all was missed - padding
should still be initialized to zero in that case.

Change-Id: I0a95ee1de6781089196a1ba40d2c0365d15926e2
Reviewed-on: https://gerrit.libreoffice.org/30412
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Justin Luth <justin_luth@sil.org>

sw/source/filter/ww8/wrtw8esh.cxx
@@ -2043,6 +2043,14 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
                 // MM If there is no line the distance should be set to 0
                 rPropOpt.AddOpt( aExhperProp[ n ], DrawModelToEmu(0));
     }
+    else
+    {
+        rPropOpt.AddOpt( ESCHER_Prop_dyTextTop, 0 );
+        rPropOpt.AddOpt( ESCHER_Prop_dyTextBottom, 0 );
+        rPropOpt.AddOpt( ESCHER_Prop_dxTextLeft, 0 );
+        rPropOpt.AddOpt( ESCHER_Prop_dxTextRight, 0 );
+    }
+
     if( bFirstLine )                // no valid line found
     {
         rPropOpt.AddOpt( ESCHER_Prop_fNoLineDrawDash, 0x80000 );