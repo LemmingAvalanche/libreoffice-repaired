LemmingAvalanche/libreoffice-repaired

commit fb510a594d0028c48de050d388737ae655519008

tdf#92702 Unable to select menu items that were initially off-screen
Revert "Last item of menu with title cannot be hilighted"

This reverts commit 7c05c3e98cff5ceaad5f55ffd2b53ba46ce1b457.

vcl/source/window/menufloatingwindow.cxx
@@ -191,7 +191,7 @@ void MenuFloatingWindow::ImplHighlightItem( const MouseEvent& rMEvt, bool bMBDow
     long nY = GetInitialItemY();
     long nMouseY = rMEvt.GetPosPixel().Y();
     Size aOutSz = GetOutputSizePixel();
-    if ( ( nMouseY >= nY ) && ( nMouseY < ( aOutSz.Height() + nY ) ) )
+    if ( ( nMouseY >= nY ) && ( nMouseY < ( aOutSz.Height() - nY ) ) )
     {
         bool bHighlighted = false;
         size_t nCount = pMenu->pItemList->size();

commit 6756f5bddcd3e446d8e3c33f53c88b10c3f20568

tdf#95341: Make last item of menus clickable again
Regression from fb510a594d0028c48de050d388737ae655519008

Change-Id: I7b7f3594499cd049ae59064da244e3f82dff7757
Reviewed-on: https://gerrit.libreoffice.org/28617
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: jan iversen <jani@documentfoundation.org>

vcl/source/window/menufloatingwindow.cxx
@@ -197,7 +197,7 @@ void MenuFloatingWindow::ImplHighlightItem( const MouseEvent& rMEvt, bool bMBDow
     long nY = GetInitialItemY();
     long nMouseY = rMEvt.GetPosPixel().Y();
     Size aOutSz = GetOutputSizePixel();
-    if ( ( nMouseY >= nY ) && ( nMouseY < ( aOutSz.Height() - nY ) ) )
+    if ( ( nMouseY >= nY ) && ( nMouseY < aOutSz.Height() ) )
     {
         bool bHighlighted = false;
         size_t nCount = pMenu->pItemList->size();