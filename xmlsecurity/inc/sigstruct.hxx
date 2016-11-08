LemmingAvalanche/libreoffice-repaired

commit 25ac5b75b401a7c621323bc9934578d782f595c8

Bin pointless (overloaded) member function
The XSecController::setX509Certificate() overload that took a
sal_Int32 nSecurityEnvironmentIndex was called in just one place, and
-1 was always passed for the nSecurityEnvironmentIndex.

Change-Id: Ic37b7053c90a6fbb8b4e5b6485483d34c605b965

xmlsecurity/source/helper/xsecsign.cxx
@@ -234,23 +234,12 @@ void XSecController::setX509Certificate(
     const OUString& ouX509Cert,
     const OUString& ouX509CertDigest)
 {
-    setX509Certificate(nSecurityId, -1, ouX509IssuerName, ouX509SerialNumber, ouX509Cert, ouX509CertDigest);
-}
-
-void XSecController::setX509Certificate(
-    sal_Int32 nSecurityId,
-    const sal_Int32 nSecurityEnvironmentIndex,
-    const OUString& ouX509IssuerName,
-    const OUString& ouX509SerialNumber,
-    const OUString& ouX509Cert,
-    const OUString& ouX509CertDigest)
-{
     int index = findSignatureInfor( nSecurityId );
 
     if ( index == -1 )
     {
         InternalSignatureInformation isi(nSecurityId, nullptr);
-        isi.signatureInfor.nSecurityEnvironmentIndex = nSecurityEnvironmentIndex;
+        isi.signatureInfor.nSecurityEnvironmentIndex = -1;
         isi.signatureInfor.ouX509IssuerName = ouX509IssuerName;
         isi.signatureInfor.ouX509SerialNumber = ouX509SerialNumber;
         isi.signatureInfor.ouX509Certificate = ouX509Cert;
@@ -265,7 +254,7 @@ void XSecController::setX509Certificate(
         si.ouX509SerialNumber = ouX509SerialNumber;
         si.ouX509Certificate = ouX509Cert;
         si.ouCertDigest = ouX509CertDigest;
-        si.nSecurityEnvironmentIndex = nSecurityEnvironmentIndex;
+        si.nSecurityEnvironmentIndex = -1;
     }
 }
 

xmlsecurity/source/helper/xsecctl.hxx
@@ -381,14 +381,6 @@ public:
         const OUString& ouX509SerialNumber,
         const OUString& ouX509Cert,
         const OUString& ouX509CertDigest);
-    // see the other setX509Certifcate function
-    void setX509Certificate(
-        sal_Int32 nSecurityId,
-        const sal_Int32 nSecurityEnvironmentIndex,
-        const OUString& ouX509IssuerName,
-        const OUString& ouX509SerialNumber,
-        const OUString& ouX509Cert,
-        const OUString& ouX509CertDigest);
 
     void setDate(
         sal_Int32 nSecurityId,

commit 558b2fbde236711db4c27d1dd2307b15d3c79123

Fallout from previous commit 25ac5b75b401a7c621323bc9934578d782f595c8
Remove SignatureInformation::nSecurityEnvironmentIndex as it was
always -1.

Change-Id: I39247e5c40ce36e394c30b450626eca533f46267

xmlsecurity/inc/sigstruct.hxx
@@ -68,7 +68,6 @@ typedef ::std::vector< SignatureReferenceInformation > SignatureReferenceInforma
 struct SignatureInformation
 {
     sal_Int32 nSecurityId;
-    sal_Int32 nSecurityEnvironmentIndex;
     css::xml::crypto::SecurityOperationStatus nStatus;
     // For ODF: XAdES digests (SHA256) or the old SHA1, from css::xml::crypto::DigestID
     sal_Int32 nDigestID;
@@ -106,7 +105,6 @@ struct SignatureInformation
     {
         nSecurityId = nId;
         nStatus = css::xml::crypto::SecurityOperationStatus_UNKNOWN;
-        nSecurityEnvironmentIndex = -1;
     }
 };
 

xmlsecurity/source/helper/xsecsign.cxx
@@ -90,15 +90,7 @@ cssu::Reference< cssxc::sax::XReferenceResolvedListener > XSecController::prepar
     args[2] = cssu::makeAny(OUString::number(nIdOfSignatureElementCollector));
 
     //for nss, the internal module is used for signing, which needs to be improved later
-    sal_Int32 nEnvIndex = internalSignatureInfor.signatureInfor.nSecurityEnvironmentIndex;
-    if( nEnvIndex < 0 || nEnvIndex >= m_xSecurityContext->getSecurityEnvironmentNumber())
-    {// set defaultEnv
-        args[3] = cssu::makeAny(m_xSecurityContext->getSecurityEnvironment());
-    }
-    else
-    {
-        args[3] = cssu::makeAny(m_xSecurityContext->getSecurityEnvironmentByIndex(nEnvIndex));
-    }
+    args[3] = cssu::makeAny(m_xSecurityContext->getSecurityEnvironment());
 
     args[4] = cssu::makeAny(m_xXMLSignature);
     xInitialization->initialize(args);
@@ -239,7 +231,6 @@ void XSecController::setX509Certificate(
     if ( index == -1 )
     {
         InternalSignatureInformation isi(nSecurityId, nullptr);
-        isi.signatureInfor.nSecurityEnvironmentIndex = -1;
         isi.signatureInfor.ouX509IssuerName = ouX509IssuerName;
         isi.signatureInfor.ouX509SerialNumber = ouX509SerialNumber;
         isi.signatureInfor.ouX509Certificate = ouX509Cert;
@@ -254,7 +245,6 @@ void XSecController::setX509Certificate(
         si.ouX509SerialNumber = ouX509SerialNumber;
         si.ouX509Certificate = ouX509Cert;
         si.ouCertDigest = ouX509CertDigest;
-        si.nSecurityEnvironmentIndex = -1;
     }
 }
 