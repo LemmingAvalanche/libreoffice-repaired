LemmingAvalanche/libreoffice-repaired

commit a7b525d07112aaa4c347989c6c0e70a50e727226

clang-tidy performance-unnecessary-copy-initialization
probably not much performance benefit, but it sure is good at
identifying leftover intermediate variables from previous
refactorings.

Change-Id: I3ce16fe496ac2733c1cb0a35f74c0fc9193cc657
Reviewed-on: https://gerrit.libreoffice.org/24026
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

jvmfwk/plugins/sunmajor/pluginlib/util.cxx
@@ -729,9 +729,8 @@ void bubbleSortVersion(vector<rtl::Reference<VendorBase> >& vec)
             }
             if(nCmp == 1) // cur > next
             {
-                rtl::Reference<VendorBase> less = next;
                 vec.at(j-1)= cur;
-                vec.at(j)= less;
+                vec.at(j)= next;
             }
         }
         ++cIter;

commit d239d929e738872bac85e7969ec0910835b04431

Fix regression in bubbleSortVersion
...introduced with a7b525d07112aaa4c347989c6c0e70a50e727226 "clang-tidy
performance-unnecessary-copy-initialization" (so partially revert it).  Whatever
clang-tidy erroneously reported there, cur and next are lvalue references into
vec, so this attempted copy now actually overwrote one with the other.  The
result was that if multiple JREs are detected on the system, "Options -
LibreOffice - Advanced" would list a single one multiple times.

Change-Id: I7ef454c0f37669722812383848602dc2bacf7cd1

jvmfwk/plugins/sunmajor/pluginlib/util.cxx
@@ -731,8 +731,9 @@ void bubbleSortVersion(vector<rtl::Reference<VendorBase> >& vec)
             }
             if(nCmp == 1) // cur > next
             {
+                rtl::Reference<VendorBase> less = next;
                 vec.at(j-1)= cur;
-                vec.at(j)= next;
+                vec.at(j)= less;
             }
         }
         ++cIter;