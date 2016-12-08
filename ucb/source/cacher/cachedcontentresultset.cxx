LemmingAvalanche/libreoffice-repaired

commit 670efa69180dd1cc9f29a0113bc3ede98e8c915b

Clean up uses of Any::getValue() in ucb
Change-Id: Ifeae43257ce9c2ba8513652c21b512bcffd87841

ucb/source/cacher/cachedcontentresultset.cxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/sdbc/ResultSetType.hpp>
 #include <com/sun/star/lang/WrappedTargetRuntimeException.hpp>
 #include <rtl/ustring.hxx>
+#include <o3tl/any.hxx>
 #include <osl/diagnose.h>
 #include <comphelper/processfactory.hxx>
 #include <cppuhelper/exc_hlp.hxx>
@@ -286,13 +287,12 @@ const Any& SAL_CALL CachedContentResultSet::CCRS_Cache
         else
             m_xContentIdentifierMapping.clear();
     }
-    const Sequence< Any >& rRow =
-        (* static_cast< const Sequence< Any > * >
-        (getRowAny( nRow ).getValue() ));
+    auto & rowAny = getRowAny(nRow);
+    auto rRow = o3tl::doAccess<Sequence<Any>>(rowAny);
 
-    if( nColumnIndex > rRow.getLength() )
+    if( nColumnIndex > rRow->getLength() )
         throw SQLException();
-    return rRow[nColumnIndex-1];
+    return (*rRow)[nColumnIndex-1];
 }
 
 const OUString& SAL_CALL CachedContentResultSet::CCRS_Cache
@@ -309,8 +309,7 @@ const OUString& SAL_CALL CachedContentResultSet::CCRS_Cache
             rRow <<= m_xContentIdentifierMapping->mapContentIdentifierString( aValue );
             remindMapped( nRow );
         }
-        return (* static_cast< const OUString * >
-                (getRowAny( nRow ).getValue() ));
+        return *o3tl::doAccess<OUString>(getRowAny(nRow));
     }
     catch(const SQLException&)
     {
@@ -332,8 +331,7 @@ const Reference< XContentIdentifier >& SAL_CALL CachedContentResultSet::CCRS_Cac
             rRow <<= m_xContentIdentifierMapping->mapContentIdentifier( aValue );
             remindMapped( nRow );
         }
-        return (* static_cast< const Reference< XContentIdentifier > * >
-                (getRowAny( nRow ).getValue() ));
+        return *o3tl::doAccess<Reference<XContentIdentifier>>(getRowAny(nRow));
     }
     catch(const SQLException&)
     {
@@ -355,8 +353,7 @@ const Reference< XContent >& SAL_CALL CachedContentResultSet::CCRS_Cache
             rRow <<= m_xContentIdentifierMapping->mapContent( aValue );
             remindMapped( nRow );
         }
-        return (* static_cast< const Reference< XContent > * >
-                (getRowAny( nRow ).getValue() ));
+        return *o3tl::doAccess<Reference<XContent>>(getRowAny(nRow));
     }
     catch (const SQLException&)
     {

commit f7db8b43c336b0929e18b67c63fcd1d8f4069703

-Werror=return-local-addr
(the first case with OUString would technically be OK, but better change all
three of them)

regression introduced with 670efa69180dd1cc9f29a0113bc3ede98e8c915b "Clean up
uses of Any::getValue() in ucb"; thanks to tml for finding it via
--enable-fuzz-options

Change-Id: I79e70cff8d8b96e1eb6b0bacda52be06eb0f108b

ucb/source/cacher/cachedcontentresultset.cxx
@@ -295,7 +295,7 @@ const Any& SAL_CALL CachedContentResultSet::CCRS_Cache
     return (*rRow)[nColumnIndex-1];
 }
 
-const OUString& SAL_CALL CachedContentResultSet::CCRS_Cache
+OUString SAL_CALL CachedContentResultSet::CCRS_Cache
     ::getContentIdentifierString( sal_Int32 nRow )
     throw( css::uno::RuntimeException )
 {
@@ -317,7 +317,7 @@ const OUString& SAL_CALL CachedContentResultSet::CCRS_Cache
     }
 }
 
-const Reference< XContentIdentifier >& SAL_CALL CachedContentResultSet::CCRS_Cache
+Reference< XContentIdentifier > SAL_CALL CachedContentResultSet::CCRS_Cache
     ::getContentIdentifier( sal_Int32 nRow )
     throw( css::uno::RuntimeException )
 {
@@ -339,7 +339,7 @@ const Reference< XContentIdentifier >& SAL_CALL CachedContentResultSet::CCRS_Cac
     }
 }
 
-const Reference< XContent >& SAL_CALL CachedContentResultSet::CCRS_Cache
+Reference< XContent > SAL_CALL CachedContentResultSet::CCRS_Cache
     ::getContent( sal_Int32 nRow )
     throw( css::uno::RuntimeException )
 {

ucb/source/cacher/cachedcontentresultset.hxx
@@ -94,15 +94,15 @@ class CachedContentResultSet
             throw( css::sdbc::SQLException,
             css::uno::RuntimeException );
 
-        const OUString& SAL_CALL
+        OUString SAL_CALL
         getContentIdentifierString( sal_Int32 nRow )
             throw( css::uno::RuntimeException );
 
-        const css::uno::Reference< css::ucb::XContentIdentifier >& SAL_CALL
+        css::uno::Reference< css::ucb::XContentIdentifier > SAL_CALL
         getContentIdentifier( sal_Int32 nRow )
             throw( css::uno::RuntimeException );
 
-        const css::uno::Reference< css::ucb::XContent >& SAL_CALL
+        css::uno::Reference< css::ucb::XContent > SAL_CALL
         getContent( sal_Int32 nRow )
             throw( css::uno::RuntimeException );
     };