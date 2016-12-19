LemmingAvalanche/libreoffice-repaired

commit 134c4c2dbf208bbd0650b189a9bb8875f7834cff

tdf#53856 writerfilter set parentless style based on default
docx import of base-less style "Text" was conflicting with
LibreOffice's builtin "Caption"->"Text" paragraph style.

This only worked when the parent .isEmpty(). Style names can
use localization, so forcing the imported parent-name will not
match between different language versions.

Change-Id: I86d9965abb4ee90b04c713371529a679d045660c
Reviewed-on: https://gerrit.libreoffice.org/28894
Reviewed-by: Justin Luth <justin_luth@sil.org>
Tested-by: Justin Luth <justin_luth@sil.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -943,7 +943,13 @@ void StyleSheetTable::ApplyStyleSheets( const FontTablePtr& rFontTable )
 
                         // Standard is handled already in applyDefaults().
                         if (sConvertedStyleName != "Standard")
+                        {
                             StyleSheetTable_Impl::SetPropertiesToDefault(xStyle);
+
+                            // resolve import conflicts with built-in styles
+                            if( pEntry->sBaseStyleIdentifier.isEmpty() && !xStyle->getParentStyle().isEmpty() )
+                                xStyle->setParentStyle( "Standard" );
+                        }
                     }
                     else
                     {

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -9,6 +9,7 @@
 
 #include <swmodeltestbase.hxx>
 
+#include <com/sun/star/awt/FontSlant.hpp>
 #include <com/sun/star/awt/XBitmap.hpp>
 #include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/graphic/XGraphic.hpp>
@@ -349,6 +350,14 @@ DECLARE_OOXMLEXPORT_TEST(testNumberingFont, "numbering-font.docx")
     uno::Reference<beans::XPropertySet> xStyle(getStyles("CharacterStyles")->getByName("ListLabel 1"), uno::UNO_QUERY);
     // This was Calibri, i.e. custom font of the numbering itself ("1.\t") was lost on import.
     CPPUNIT_ASSERT_EQUAL(OUString("Verdana"), getProperty<OUString>(xStyle, "CharFontName"));
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf53856_conflictingStyle, "tdf53856_conflictingStyle.docx")
+{
+    // The "Text" style conflicted with builtin paragraph style Caption -> Text
+    uno::Reference<beans::XPropertySet> xStyle(getStyles("ParagraphStyles")->getByName("Text"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Times New Roman"), getProperty<OUString>(xStyle, "CharFontName"));
+    CPPUNIT_ASSERT_EQUAL(awt::FontSlant_NONE, getProperty<awt::FontSlant>(xStyle, "CharPosture"));
 }
 
 DECLARE_OOXMLEXPORT_TEST(testDrawingmlFlipv, "drawingml-flipv.docx")

commit 4b3b0a7a888ab30aa508bf077a0108cb0f21f786

tdf#104713 writerfilter: parentless styles - only if default defined
LibreOffice's default paragraph style is very different from Word's normal
style? Anyway, if normal is not defined in styles.xml, then don't
re-assign any other automatic styles to be based off of it, since that
indicates that the style information being imported is not complete.

avoids regression from commit 134c4c2dbf208bbd0650b189a9bb8875f7834cff

Change-Id: I1bfa8505d6b89b2bba255ad727ebadbacc8d3651
Reviewed-on: https://gerrit.libreoffice.org/32103
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Justin Luth <justin_luth@sil.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -267,6 +267,7 @@ struct StyleSheetTable_Impl
     /// Style names which should not be used without a " (user)" suffix.
     std::set<OUString>                      m_aReservedStyleNames;
     ListCharStylePropertyVector_t           m_aListCharStylePropertyVector;
+    bool                                    m_bHasImportedDefaultParaStyle;
     bool                                    m_bIsNewDoc;
 
     StyleSheetTable_Impl(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> const& xTextDocument, bool bIsNewDoc);
@@ -289,6 +290,7 @@ StyleSheetTable_Impl::StyleSheetTable_Impl(DomainMapper& rDMapper,
             m_pCurrentEntry(),
             m_pDefaultParaProps(new PropertyMap),
             m_pDefaultCharProps(new PropertyMap),
+            m_bHasImportedDefaultParaStyle(false),
             m_bIsNewDoc(bIsNewDoc)
 {
     //set font height default to 10pt
@@ -456,6 +458,9 @@ void StyleSheetTable::lcl_attribute(Id Name, Value & val)
         break;
         case NS_ooxml::LN_CT_Style_default:
             m_pImpl->m_pCurrentEntry->bIsDefaultStyle = (nIntValue != 0);
+            if (m_pImpl->m_pCurrentEntry->bIsDefaultStyle && m_pImpl->m_pCurrentEntry->nStyleTypeCode == STYLE_TYPE_PARA)
+                m_pImpl->m_bHasImportedDefaultParaStyle = true;
+
             if (m_pImpl->m_pCurrentEntry->nStyleTypeCode != STYLE_TYPE_UNKNOWN)
             {
                 beans::PropertyValue aValue;
@@ -946,9 +951,13 @@ void StyleSheetTable::ApplyStyleSheets( const FontTablePtr& rFontTable )
                         {
                             StyleSheetTable_Impl::SetPropertiesToDefault(xStyle);
 
-                            // resolve import conflicts with built-in styles
-                            if( pEntry->sBaseStyleIdentifier.isEmpty() && !xStyle->getParentStyle().isEmpty() )
+                            // resolve import conflicts with built-in styles (only if normal style has been defined)
+                            if( m_pImpl->m_bHasImportedDefaultParaStyle
+                                && pEntry->sBaseStyleIdentifier.isEmpty()
+                                && !xStyle->getParentStyle().isEmpty() )
+                            {
                                 xStyle->setParentStyle( "Standard" );
+                            }
                         }
                     }
                     else

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -357,6 +357,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf53856_conflictingStyle, "tdf53856_conflictingSty
     CPPUNIT_ASSERT_EQUAL(awt::FontSlant_NONE, getProperty<awt::FontSlant>(xStyle, "CharPosture"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf104713_undefinedStyles, "tdf104713_undefinedStyles.docx")
+{
+    // Normal paragraph style was not defined, so don't replace conflicting styles
+    uno::Reference<beans::XPropertySet> xStyle(getStyles("ParagraphStyles")->getByName("Heading 1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(212), getProperty<sal_Int32>(xStyle, "ParaBottomMargin"));
+}
+
 DECLARE_OOXMLEXPORT_TEST(testDrawingmlFlipv, "drawingml-flipv.docx")
 {
     // The problem was that the shape had vertical flip only, but then we added rotation as well on export.