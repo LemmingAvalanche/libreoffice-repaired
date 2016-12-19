LemmingAvalanche/libreoffice-repaired

commit a39fa9d42b3371208e8c97ee8f8c52016791ebca

Resolves: tdf#100272 row ally index is ROW_INVALID
which is < 0 so bad things happen, consider ROW_INVALID/COL_INVALID before
looking for a11y child at that index

Change-Id: I16a9d86879e1a894c63932705c056268e98db7aa

accessibility/source/extended/AccessibleGridControl.cxx
@@ -345,7 +345,14 @@ void AccessibleGridControl::commitTableEvent(sal_Int16 _nEventId,const Any& _rNe
     {
         if(_nEventId == AccessibleEventId::ACTIVE_DESCENDANT_CHANGED)
         {
-            css::uno::Reference< css::accessibility::XAccessible > xChild = m_xImpl->m_pTable->getAccessibleChild(m_aTable.GetCurrentRow()*m_aTable.GetColumnCount()+m_aTable.GetCurrentColumn());
+            const sal_Int32 nCurrentRow = m_aTable.GetCurrentRow();
+            const sal_Int32 nCurrentCol = m_aTable.GetCurrentColumn();
+            css::uno::Reference< css::accessibility::XAccessible > xChild;
+            if (nCurrentRow > -1 && nCurrentCol > -1)
+            {
+                sal_Int32 nColumnCount = m_aTable.GetColumnCount();
+                xChild = m_xImpl->m_pTable->getAccessibleChild(nCurrentRow * nCurrentCol + nColumnCount);
+            }
             m_xImpl->m_pTable->commitEvent(_nEventId, makeAny(xChild),_rOldValue);
         }
         else if(_nEventId == AccessibleEventId::TABLE_MODEL_CHANGED)

commit 0238c64210bed7eee133e21ee1fa735a435910fd

tdf#104767: Fix calculus of child index in commitTableEvent (accessibility)
bt:
1  0x00002aaade2fea5b in accessibility::AccessibleGridControlTableBase::ensureIsValidIndex (this=0x5555586abdb0, nChildIndex=11)
    at /home/julien/lo/libreoffice/accessibility/source/extended/AccessibleGridControlTableBase.cxx:257
2  0x00002aaade2f97f2 in accessibility::AccessibleGridControlTable::getAccessibleChild (this=0x5555586abdb0, nChildIndex=11)
    at /home/julien/lo/libreoffice/accessibility/source/extended/AccessibleGridControlTable.cxx:60
3  0x00002aaade2ee054 in accessibility::AccessibleGridControl::commitTableEvent (this=0x555557e97500, _nEventId=5, _rNewValue=uno::Any(void), _rOldValue=uno::Any(void))
    at /home/julien/lo/libreoffice/accessibility/source/extended/AccessibleGridControl.cxx:354

Seems a regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=a39fa9d42b3371208e8c97ee8f8c52016791ebca

Change-Id: Ia973eb381832da27ecc39fc5273f67a685d5a7bb
Reviewed-on: https://gerrit.libreoffice.org/32205
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Julien Nabet <serval2412@yahoo.fr>

accessibility/source/extended/AccessibleGridControl.cxx
@@ -351,7 +351,7 @@ void AccessibleGridControl::commitTableEvent(sal_Int16 _nEventId,const Any& _rNe
             if (nCurrentRow > -1 && nCurrentCol > -1)
             {
                 sal_Int32 nColumnCount = m_aTable.GetColumnCount();
-                xChild = m_xImpl->m_pTable->getAccessibleChild(nCurrentRow * nCurrentCol + nColumnCount);
+                xChild = m_xImpl->m_pTable->getAccessibleChild(nCurrentRow * nColumnCount + nCurrentCol);
             }
             m_xImpl->m_pTable->commitEvent(_nEventId, makeAny(xChild),_rOldValue);
         }