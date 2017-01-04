LemmingAvalanche/libreoffice-repaired

commit 7393a068825d10898b5bbda7221f6cab8a76e298

unwind RID_SVXIL_FRAME ImageList
Change-Id: I18f567d3ff7f415c75e29043ef29a460be8a21c5

svx/source/tbxctrls/tbcontrl.cxx
@@ -255,7 +255,7 @@ class SvxFrameWindow_Impl : public svtools::ToolbarPopup
 private:
     VclPtr<SvxFrmValueSet_Impl> aFrameSet;
     svt::ToolboxController&     mrController;
-    ImageList                   aImgList;
+    std::vector<BitmapEx>       aImgVec;
     bool                        bParagraphMode;
 
     void InitImageList();
@@ -272,7 +272,6 @@ public:
     virtual void dispose() override;
 
     virtual void    statusChanged( const css::frame::FeatureStateEvent& rEvent ) throw ( css::uno::RuntimeException ) override;
-    virtual void    DataChanged( const DataChangedEvent& rDCEvt ) override;
 };
 
 class SvxLineWindow_Impl : public svtools::ToolbarPopup
@@ -1746,12 +1745,12 @@ SvxFrameWindow_Impl::SvxFrameWindow_Impl ( svt::ToolboxController& rController, 
     sal_uInt16 i = 0;
 
     for ( i=1; i<9; i++ )
-        aFrameSet->InsertItem( i, aImgList.GetImage(i) );
+        aFrameSet->InsertItem(i, Image(aImgVec[i-1]));
 
     //bParagraphMode should have been set in StateChanged
     if ( !bParagraphMode )
         for ( i = 9; i < 13; i++ )
-            aFrameSet->InsertItem( i, aImgList.GetImage(i) );
+            aFrameSet->InsertItem(i, Image(aImgVec[i-1]));
 
     aFrameSet->SetColCount( 4 );
     aFrameSet->SetSelectHdl( LINK( this, SvxFrameWindow_Impl, SelectHdl ) );
@@ -1784,21 +1783,6 @@ void SvxFrameWindow_Impl::KeyInput( const KeyEvent& rKEvt )
 {
     aFrameSet->GrabFocus();
     aFrameSet->KeyInput( rKEvt );
-}
-
-void SvxFrameWindow_Impl::DataChanged( const DataChangedEvent& rDCEvt )
-{
-    ToolbarPopup::DataChanged( rDCEvt );
-
-    if( ( rDCEvt.GetType() == DataChangedEventType::SETTINGS ) && ( rDCEvt.GetFlags() & AllSettingsFlags::STYLE ) )
-    {
-        InitImageList();
-
-        sal_uInt16  nNumOfItems = aFrameSet->GetItemCount();
-
-        for( sal_uInt16 i = 1 ; i <= nNumOfItems ; ++i )
-            aFrameSet->SetItemImage( i, aImgList.GetImage( i ) );
-    }
 }
 
 enum class FrmValidFlags {
@@ -1956,7 +1940,7 @@ void SvxFrameWindow_Impl::statusChanged( const css::frame::FeatureStateEvent& rE
                 else if ( !bTableMode && !bParagraphMode )
                 {
                     for ( sal_uInt16 i = 9; i < 13; i++ )
-                        aFrameSet->InsertItem( i, aImgList.GetImage(i) );
+                        aFrameSet->InsertItem(i, Image(aImgVec[i-1]));
                     bResize = true;
                 }
 
@@ -1981,16 +1965,25 @@ void SvxFrameWindow_Impl::CalcSizeValueSet()
 
 void SvxFrameWindow_Impl::InitImageList()
 {
-    aImgList = ImageList( SVX_RES( RID_SVXIL_FRAME ) );
+    aImgVec.clear();
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME1)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME2)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME3)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME4)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME5)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME6)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME7)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME8)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME9)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME10)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME11)));
+    aImgVec.push_back(BitmapEx(SVX_RES(RID_SVXBMP_FRAME12)));
 
-    if( GetParent()->GetDPIScaleFactor() > 1 )
+    if (GetParent()->GetDPIScaleFactor() > 1)
     {
-        for (short i = 0; i < aImgList.GetImageCount(); i++)
+        for (size_t i = 0; i < aImgVec.size(); ++i)
         {
-            OUString rImageName = aImgList.GetImageName(i);
-            BitmapEx b = aImgList.GetImage(rImageName).GetBitmapEx();
-            b.Scale(GetParent()->GetDPIScaleFactor(), GetParent()->GetDPIScaleFactor());
-            aImgList.ReplaceImage(rImageName, Image(b));
+            aImgVec[i].Scale(GetParent()->GetDPIScaleFactor(), GetParent()->GetDPIScaleFactor());
         }
     }
 }

include/svx/dialogs.hrc
@@ -141,8 +141,6 @@
 // ResId's fuer Dialoge aus StarDraw
 #define RID_SVXSW_FRAMEPOSITIONS            (RID_SVX_START + 289)
 
-#define RID_SVXIL_FRAME                     (RID_SVX_START +  65)
-
 #define RID_SVXIMAGE_LIGHT_ON               (RID_SVX_START + 210)
 #define RID_SVXIMAGE_LIGHT_OFF              (RID_SVX_START + 211)
 #define RID_SVXIMAGE_COLORDLG               (RID_SVX_START + 214)
@@ -177,6 +175,19 @@
 #define RID_SVXBMP_ATTRIBUTE                (RID_SVX_START +  72)
 #define RID_SVXBMP_TEXT                     (RID_SVX_START +  73)
 #define RID_SVXBMP_OTHER                    (RID_SVX_START +  74)
+
+#define RID_SVXBMP_FRAME1                   (RID_SVX_START +  75)
+#define RID_SVXBMP_FRAME2                   (RID_SVX_START +  76)
+#define RID_SVXBMP_FRAME3                   (RID_SVX_START +  77)
+#define RID_SVXBMP_FRAME4                   (RID_SVX_START +  78)
+#define RID_SVXBMP_FRAME5                   (RID_SVX_START +  79)
+#define RID_SVXBMP_FRAME6                   (RID_SVX_START +  80)
+#define RID_SVXBMP_FRAME7                   (RID_SVX_START +  81)
+#define RID_SVXBMP_FRAME8                   (RID_SVX_START +  82)
+#define RID_SVXBMP_FRAME9                   (RID_SVX_START +  83)
+#define RID_SVXBMP_FRAME10                  (RID_SVX_START +  84)
+#define RID_SVXBMP_FRAME11                  (RID_SVX_START +  85)
+#define RID_SVXBMP_FRAME12                  (RID_SVX_START +  86)
 
 // Menu for the ruler
 #define RID_SVXMN_RULER                     (RID_SVX_START +  92)

svx/source/tbxctrls/tbcontrl.src
@@ -34,11 +34,64 @@ Image RID_SVX_RELOAD_SPECIAL
     ImageBitmap = Bitmap { File = "reloads.png" ; };
 };
 
-ImageList RID_SVXIL_FRAME
+Bitmap RID_SVXBMP_FRAME1
 {
-    Prefix = "fr";
-    IdList = { 1 ; 2 ; 3 ; 4 ; 5 ; 6 ; 7 ; 8 ; 9 ; 10 ; 11 ; 12 ; };
-    IdCount = { 12 ; };
+    File = "fr01.png";
+};
+
+Bitmap RID_SVXBMP_FRAME2
+{
+    File = "fr02.png";
+};
+
+Bitmap RID_SVXBMP_FRAME3
+{
+    File = "fr03.png";
+};
+
+Bitmap RID_SVXBMP_FRAME4
+{
+    File = "fr04.png";
+};
+
+Bitmap RID_SVXBMP_FRAME5
+{
+    File = "fr05.png";
+};
+
+Bitmap RID_SVXBMP_FRAME6
+{
+    File = "fr06.png";
+};
+
+Bitmap RID_SVXBMP_FRAME7
+{
+    File = "fr07.png";
+};
+
+Bitmap RID_SVXBMP_FRAME8
+{
+    File = "fr08.png";
+};
+
+Bitmap RID_SVXBMP_FRAME9
+{
+    File = "fr09.png";
+};
+
+Bitmap RID_SVXBMP_FRAME10
+{
+    File = "fr010.png";
+};
+
+Bitmap RID_SVXBMP_FRAME11
+{
+    File = "fr011.png";
+};
+
+Bitmap RID_SVXBMP_FRAME12
+{
+    File = "fr012.png";
 };
 
 String RID_SVXSTR_NOFILL

commit 58d752b506a68c7d7c6861f80aa4cc7a608d8813

Support icon theme change in a floating border control
was unintentionally broken by
7393a068825d10898b5bbda7221f6cab8a76e298

Change-Id: I3358d8104dd1e2200f84019a2c4c41f1e0019399

svx/source/tbxctrls/tbcontrl.cxx
@@ -273,6 +273,7 @@ public:
     virtual void dispose() override;
 
     virtual void    statusChanged( const css::frame::FeatureStateEvent& rEvent ) throw ( css::uno::RuntimeException ) override;
+    virtual void    DataChanged( const DataChangedEvent& rDCEvt ) override;
 };
 
 class SvxLineWindow_Impl : public svtools::ToolbarPopup
@@ -1752,6 +1753,20 @@ void SvxFrameWindow_Impl::KeyInput( const KeyEvent& rKEvt )
 {
     aFrameSet->GrabFocus();
     aFrameSet->KeyInput( rKEvt );
+}
+
+void SvxFrameWindow_Impl::DataChanged( const DataChangedEvent& rDCEvt )
+{
+    ToolbarPopup::DataChanged( rDCEvt );
+
+    if ( ( rDCEvt.GetType() == DataChangedEventType::SETTINGS ) && ( rDCEvt.GetFlags() & AllSettingsFlags::STYLE ) )
+    {
+        InitImageList();
+
+        sal_uInt16 nNumOfItems = aFrameSet->GetItemCount();
+        for ( sal_uInt16 i = 1 ; i <= nNumOfItems ; ++i )
+            aFrameSet->SetItemImage( i, Image(aImgVec[i-1]) );
+    }
 }
 
 enum class FrmValidFlags {