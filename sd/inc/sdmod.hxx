LemmingAvalanche/libreoffice-repaired

commit 311077e2df872dd00c6d243063aaff810f3f9e3d

Moved SfxModule owner to SfxApplication
::GetAppData replaced with SfxApplication::GetModule
that now returns SfxModule*

SfxModule no longer registers self for ownership
instead it is now registered using SfxApplication::SetModule

Change-Id: Ifbbe1b2b4c5122da8e643b7926d47878d116c6c8
Reviewed-on: https://gerrit.libreoffice.org/26914
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

sd/inc/sdmod.hxx
@@ -24,7 +24,6 @@
 #include "pres.hxx"
 
 #include <sot/storage.hxx>
-#include <tools/shl.hxx>
 #include "sddllapi.h"
 #include <svl/itemprop.hxx>
 #include <svl/lstner.hxx>
@@ -192,7 +191,7 @@ private:
 
 };
 
-#define SD_MOD() ( *reinterpret_cast<SdModule**>(GetAppData(SHL_DRAW)) )
+#define SD_MOD() ( static_cast<SdModule*>(SfxApplication::GetModule(SfxToolsModule::Draw)) )
 
 #endif // INCLUDED_SD_INC_SDMOD_HXX
 

sd/source/ui/app/sdmod.cxx
@@ -110,11 +110,6 @@ SdModule::~SdModule()
 
     mpResourceContainer.reset();
 
-    // Mark the module in the global AppData structure as deleted.
-    SdModule** ppShellPointer = reinterpret_cast<SdModule**>(GetAppData(SHL_DRAW));
-    if (ppShellPointer != nullptr)
-        (*ppShellPointer) = nullptr;
-
     delete mpErrorHdl;
     mpVirtualRefDevice.disposeAndClear();
 }

basctl/source/basicide/basicmod.hxx
@@ -28,13 +28,10 @@ namespace basctl
 
 class Module : public SfxModule
 {
-    static Module* mpModule;
 public:
     Module ( ResMgr *pMgr, SfxObjectFactory *pObjFact) :
         SfxModule( pMgr, {pObjFact} )
     { }
-public:
-    static Module*& Get () { return mpModule; }
 };
 
 } // namespace basctl

basctl/source/basicide/iderdll.cxx
@@ -32,15 +32,13 @@
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/script/XLibraryContainerPassword.hpp>
 #include <vcl/settings.hxx>
-
+#include <o3tl/make_unique.hxx>
 
 namespace basctl
 {
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
-
-Module* Module::mpModule = nullptr;
 
 namespace
 {
@@ -106,7 +104,7 @@ ExtraData* GetExtraData()
 
 
 IDEResId::IDEResId( sal_uInt16 nId ):
-    ResId(nId, *Module::Get()->GetResMgr())
+    ResId(nId, *SfxApplication::GetModule(SfxToolsModule::Basic)->GetResMgr())
 { }
 
 namespace
@@ -121,11 +119,11 @@ Dll::Dll () :
     ResMgr* pMgr = ResMgr::CreateResMgr(
         "basctl", Application::GetSettings().GetUILanguageTag());
 
-    Module::Get() = new Module( pMgr, &DocShell::Factory() );
+    auto pModule = o3tl::make_unique<Module>( pMgr, &DocShell::Factory() );
+    SfxModule* pMod = pModule.get();
+    SfxApplication::SetModule(SfxToolsModule::Basic, std::move(pModule));
 
     GetExtraData(); // to cause GlobalErrorHdl to be set
-
-    SfxModule* pMod = Module::Get();
 
     SfxObjectFactory& rFactory = DocShell::Factory();
     rFactory.SetDocumentServiceName( "com.sun.star.script.BasicIDE" );

cui/source/options/treeopt.cxx
@@ -89,7 +89,6 @@
 #include <svx/xtable.hxx>
 #include <svx/xpool.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
-#include <tools/shl.hxx>
 #include <tools/urlobj.hxx>
 #include <unotools/linguprops.hxx>
 #include <unotools/misccfg.hxx>
@@ -1586,7 +1585,7 @@ void OfaTreeOptionsDialog::Initialize( const Reference< XFrame >& _xFrame )
             || aFactory == "com.sun.star.text.WebDocument"
             || aFactory == "com.sun.star.text.GlobalDocument" )
         {
-            SfxModule* pSwMod = *reinterpret_cast<SfxModule**>(GetAppData(SHL_WRITER));
+            SfxModule* pSwMod = SfxApplication::GetModule(SfxToolsModule::Writer);
             if ( !lcl_isOptionHidden( SID_SW_EDITOPTIONS, aOptionsDlgOpt ) )
             {
                 if ( aFactory == "com.sun.star.text.WebDocument" )
@@ -1635,7 +1634,7 @@ void OfaTreeOptionsDialog::Initialize( const Reference< XFrame >& _xFrame )
             if ( !lcl_isOptionHidden( SID_SC_EDITOPTIONS, aOptionsDlgOpt ) )
             {
                 ResStringArray& rCalcArray = aDlgResource.GetCalcArray();
-                SfxModule* pScMod = *reinterpret_cast<SfxModule**>(GetAppData( SHL_CALC ));
+                SfxModule* pScMod = SfxApplication::GetModule( SfxToolsModule::Calc );
                 setGroupName( "Calc", rCalcArray.GetString(0) );
                 nGroup = AddGroup( rCalcArray.GetString( 0 ), pScMod, pScMod, SID_SC_EDITOPTIONS );
                 const sal_uInt16 nCount = static_cast< const sal_uInt16 >( rCalcArray.Count() );
@@ -1652,7 +1651,7 @@ void OfaTreeOptionsDialog::Initialize( const Reference< XFrame >& _xFrame )
     }
 
     // Impress options
-    SfxModule* pSdMod = *reinterpret_cast<SfxModule**>(GetAppData( SHL_DRAW ));
+    SfxModule* pSdMod = SfxApplication::GetModule( SfxToolsModule::Draw );
     if ( aModuleOpt.IsModuleInstalled( SvtModuleOptions::EModule::IMPRESS ) )
     {
         if ( aFactory == "com.sun.star.presentation.PresentationDocument" )
@@ -1706,7 +1705,7 @@ void OfaTreeOptionsDialog::Initialize( const Reference< XFrame >& _xFrame )
             if ( !lcl_isOptionHidden( SID_SM_EDITOPTIONS, aOptionsDlgOpt ) )
             {
                 ResStringArray& rStarMathArray = aDlgResource.GetStarMathArray();
-                SfxModule* pSmMod = *reinterpret_cast<SfxModule**>(GetAppData(SHL_SM));
+                SfxModule* pSmMod = SfxApplication::GetModule(SfxToolsModule::Math);
                 setGroupName( "Math", rStarMathArray.GetString(0) );
                 nGroup = AddGroup(rStarMathArray.GetString(0), pSmMod, pSmMod, SID_SM_EDITOPTIONS );
                 for ( i = 1; i < rStarMathArray.Count(); ++i )

cui/source/tabpages/autocdlg.cxx
@@ -25,7 +25,6 @@
 #include <vcl/settings.hxx>
 #include <sot/exchange.hxx>
 #include <svtools/transfer.hxx>
-#include <tools/shl.hxx>
 #include <unotools/syslocale.hxx>
 #include <sfx2/objsh.hxx>
 #include <sfx2/viewsh.hxx>
@@ -882,7 +881,7 @@ OfaAutocorrReplacePage::OfaAutocorrReplacePage( vcl::Window* pParent,
     get(m_pReplaceTLB, "tabview");
     m_pReplaceTLB->set_height_request(16 * GetTextHeight());
 
-    SfxModule *pMod = *reinterpret_cast<SfxModule**>(GetAppData(SHL_WRITER));
+    SfxModule *pMod = SfxApplication::GetModule(SfxToolsModule::Writer);
     bSWriter = pMod == SfxModule::GetActiveModule();
 
     LanguageTag aLanguageTag( eLastDialogLanguage );

include/sfx2/app.hxx
@@ -88,6 +88,16 @@ namespace sfx2
     }
 }
 
+enum class SfxToolsModule
+{
+    Math = 0,
+    Calc = 1,
+    Draw = 2,
+    Writer = 3,
+    Basic = 4,
+    LAST = Basic
+};
+
 class SfxLinkItem : public SfxPoolItem
 {
     Link<SfxPoolItem*, void> aLink;
@@ -223,6 +233,9 @@ public:
     // already removed, or the below methods?
     SAL_DLLPRIVATE SfxSlotPool& GetAppSlotPool_Impl() const;
     SAL_DLLPRIVATE SfxModule*   GetModule_Impl();
+
+    static void                 SetModule(SfxToolsModule nSharedLib, std::unique_ptr<SfxModule> pModule);
+    static SfxModule*           GetModule(SfxToolsModule nSharedLib);
 
     static bool loadBrandSvg(const char *pName, BitmapEx &rBitmap, int nWidth);
 

include/sfx2/module.hxx
@@ -96,8 +96,6 @@ public:
     static FieldUnit            GetModuleFieldUnit( css::uno::Reference< css::frame::XFrame > const & i_frame );
     FieldUnit                   GetFieldUnit() const;
 
-    SAL_DLLPRIVATE static std::vector<SfxModule*>& GetModules_Impl();
-    SAL_DLLPRIVATE static void DestroyModules_Impl();
     SAL_DLLPRIVATE SfxTbxCtrlFactArr_Impl* GetTbxCtrlFactories_Impl() const;
     SAL_DLLPRIVATE SfxStbCtrlFactArr_Impl* GetStbCtrlFactories_Impl() const;
     SAL_DLLPRIVATE SfxChildWinFactArr_Impl* GetChildWinFactories_Impl() const;

commit 213b34045c4b88220209ed0a8e294e398ccc2865

tdf#105188 sd: fix shutdown crash after accessing master pages
The problem here is that the destructor of SdModule does a lot of
things, including destroying an SdXImpressDocument that is referenced
from some SdGlobalResourceContainer.

This calls SD_MOD() to get the SdModule to get some resource, but at
that point SfxApplication::GetModule() returns null, because the
sequence was changed from first deleting the SfxModules, then clearing
the pointer in ~SfxModule to null, to unique_ptr::reset(), which, at
least in libstdc++, is implemented via std::swap, so it clears the
pointer before deleting the SfxModule.

It appears rather brittle to rely on such a subtle detail, so refactor
things so that SdGlobalResourceContainer is no longer owned by SdModule
but has its own pet XTerminationListener, which means it will be
destroyed earlier, while the SdModule is still fully alive.

(regression from 311077e2df872dd00c6d243063aaff810f3f9e3d)

Change-Id: I7f03f3adf431be8728ef3d65a078b536cb96f959

sd/inc/sdmod.hxx
@@ -47,7 +47,6 @@ namespace svtools { class ColorConfig; }
 
 namespace sd {
 class DrawDocShell;
-class SdGlobalResourceContainer;
 }
 
 namespace com { namespace sun { namespace star { namespace frame {
@@ -155,10 +154,6 @@ private:
 
     static SfxFrame* CreateEmptyDocument( const css::uno::Reference< css::frame::XFrame >& i_rFrame );
     static SfxFrame* CreateFromTemplate( const OUString& rTemplatePath, const css::uno::Reference< css::frame::XFrame >& i_rFrame );
-
-    /** The resource container controls the lifetime of some singletons.
-    */
-    ::std::unique_ptr< ::sd::SdGlobalResourceContainer> mpResourceContainer;
 
     bool mbEventListenerAdded;
 

sd/source/ui/app/sdmod.cxx
@@ -53,7 +53,6 @@
 #include "strings.hrc"
 #include "res_bmp.hrc"
 #include "cfgids.hxx"
-#include "tools/SdGlobalResourceContainer.hxx"
 
 
 #define SdModule
@@ -77,7 +76,6 @@ SdModule::SdModule(SfxObjectFactory* pFact1, SfxObjectFactory* pFact2 )
     pSearchItem(nullptr),
     pNumberFormatter( nullptr ),
     bWaterCan(false),
-    mpResourceContainer(new ::sd::SdGlobalResourceContainer()),
     mbEventListenerAdded(false),
     mpColorConfig(new svtools::ColorConfig)
 {
@@ -109,8 +107,6 @@ SdModule::~SdModule()
     {
         Application::RemoveEventListener( LINK( this, SdModule, EventListenerHdl ) );
     }
-
-    mpResourceContainer.reset();
 
     delete mpErrorHdl;
     mpVirtualRefDevice.disposeAndClear();

sd/source/ui/inc/tools/SdGlobalResourceContainer.hxx
@@ -82,7 +82,7 @@ public:
     void AddResource (const css::uno::Reference<css::uno::XInterface>& rxResource);
 
 protected:
-    friend class ::SdModule;
+    friend class SdGlobalResourceContainerInstance;
     friend struct ::std::default_delete<SdGlobalResourceContainer>;
 
     class Implementation;

sd/source/ui/tools/SdGlobalResourceContainer.cxx
@@ -19,6 +19,13 @@
 
 #include "tools/SdGlobalResourceContainer.hxx"
 
+#include <comphelper/processfactory.hxx>
+#include <comphelper/unique_disposing_ptr.hxx>
+
+#include <com/sun/star/frame/Desktop.hpp>
+
+#include <rtl/instance.hxx>
+
 #include <algorithm>
 #include <vector>
 
@@ -27,13 +34,30 @@ using namespace ::com::sun::star::uno;
 
 namespace sd {
 
+class SdGlobalResourceContainerInstance
+    : public comphelper::unique_disposing_solar_mutex_reset_ptr<SdGlobalResourceContainer>
+{
+public:
+    SdGlobalResourceContainerInstance()
+        : comphelper::unique_disposing_solar_mutex_reset_ptr<SdGlobalResourceContainer>(
+            uno::Reference<lang::XComponent>(frame::Desktop::create(comphelper::getProcessComponentContext()), uno::UNO_QUERY_THROW),
+            new SdGlobalResourceContainer, true)
+    {
+    }
+};
+
+namespace {
+
+struct theSdGlobalResourceContainerInstance : public rtl::Static<SdGlobalResourceContainerInstance, theSdGlobalResourceContainerInstance> {};
+
+} // namespace
+
 //===== SdGlobalResourceContainer::Implementation =============================
 
 class SdGlobalResourceContainer::Implementation
 {
 private:
     friend class SdGlobalResourceContainer;
-    static SdGlobalResourceContainer* mpInstance;
 
     ::osl::Mutex maMutex;
 
@@ -53,14 +77,10 @@ private:
 // static
 SdGlobalResourceContainer& SdGlobalResourceContainer::Instance()
 {
-    DBG_ASSERT(Implementation::mpInstance!=nullptr,
-        "SdGlobalResourceContainer::Instance(): instance has been deleted");
-    // Maybe we should throw an exception when the instance has been deleted.
-    return *Implementation::mpInstance;
+    SdGlobalResourceContainer *const pRet(theSdGlobalResourceContainerInstance::get().get());
+    assert(pRet); // error if it has been deleted and is null
+    return *pRet;
 }
-
-SdGlobalResourceContainer*
-    SdGlobalResourceContainer::Implementation::mpInstance = nullptr;
 
 //===== SdGlobalResourceContainer =============================================
 
@@ -128,7 +148,6 @@ void SdGlobalResourceContainer::AddResource (const Reference<XInterface>& rxReso
 SdGlobalResourceContainer::SdGlobalResourceContainer()
     : mpImpl (new SdGlobalResourceContainer::Implementation())
 {
-    Implementation::mpInstance = this;
 }
 
 SdGlobalResourceContainer::~SdGlobalResourceContainer()
@@ -174,10 +193,6 @@ SdGlobalResourceContainer::~SdGlobalResourceContainer()
         if (xComponent.is())
             xComponent->dispose();
     }
-
-    DBG_ASSERT(Implementation::mpInstance == this,
-        "~SdGlobalResourceContainer(): more than one instance of singleton");
-    Implementation::mpInstance = nullptr;
 }
 
 } // end of namespace sd