LibreOffice/core

Convert bTimer => bIdle
All other places already refer to being Idle, so change the
Scheduler::ProcessTaskScheduling argument to bIdle and adapt
all other scheduler-related functions.

Change-Id: If5a605abbc3e620092127b65ada29f11215a0343

vcl/source/app/scheduler.cxx
@@ -49,14 +49,14 @@ void ImplSchedulerData::Invoke()
     mbInScheduler = false;
 }
 
-ImplSchedulerData *ImplSchedulerData::GetMostImportantTask( const sal_uInt64 nTime, const bool bTimerOnly )
+ImplSchedulerData *ImplSchedulerData::GetMostImportantTask( const sal_uInt64 nTime, const bool bIdle )
 {
     ImplSVData*     pSVData = ImplGetSVData();
     ImplSchedulerData *pMostUrgent = nullptr;
 
     for ( ImplSchedulerData *pSchedulerData = pSVData->mpFirstSchedulerData; pSchedulerData; pSchedulerData = pSchedulerData->mpNext )
     {
-        if ( !pSchedulerData->mpScheduler || !pSchedulerData->mpScheduler->ReadyForSchedule( nTime, bTimerOnly ) )
+        if ( !pSchedulerData->mpScheduler || !pSchedulerData->mpScheduler->ReadyForSchedule( nTime, bIdle ) )
             continue;
         if (!pMostUrgent)
             pMostUrgent = pSchedulerData;
@@ -153,18 +153,18 @@ void InitSystemTimer(ImplSVData* pSVData)
 
 }
 
-void Scheduler::CallbackTaskScheduling(bool)
+void Scheduler::CallbackTaskScheduling( bool bIdle )
 {
     // this function is for the saltimer callback
-    Scheduler::ProcessTaskScheduling( false );
+    Scheduler::ProcessTaskScheduling( bIdle );
 }
 
-bool Scheduler::ProcessTaskScheduling( bool bTimerOnly )
+bool Scheduler::ProcessTaskScheduling( bool bIdle )
 {
     ImplSchedulerData* pSchedulerData;
     sal_uInt64         nTime = tools::Time::GetSystemTicks();
 
-    if ((pSchedulerData = ImplSchedulerData::GetMostImportantTask(nTime, bTimerOnly)))
+    if ((pSchedulerData = ImplSchedulerData::GetMostImportantTask(nTime, bIdle)))
     {
         SAL_INFO("vcl.schedule", "Invoke task " << pSchedulerData->GetDebugName());
 
@@ -190,8 +190,6 @@ bool Scheduler::GetDeterministicMode()
 
 sal_uInt64 Scheduler::CalculateMinimumTimeout( bool &bHasActiveIdles )
 {
-    // process all pending Tasks
-    // if bTimer True, only handle timer
     ImplSchedulerData* pSchedulerData = nullptr;
     ImplSchedulerData* pPrevSchedulerData = nullptr;
     ImplSVData*        pSVData = ImplGetSVData();

LemmingAvalanche/libreoffice-repaired

vcl/source/app/scheduler.cxx
@@ -49,7 +49,7 @@ void ImplSchedulerData::Invoke()
     mbInScheduler = false;
 }
 
-ImplSchedulerData *ImplSchedulerData::GetMostImportantTask( bool bTimerOnly, sal_uInt64 nTimeNow )
+ImplSchedulerData *ImplSchedulerData::GetMostImportantTask( bool bIdle, sal_uInt64 nTimeNow )
 {
     ImplSVData*     pSVData = ImplGetSVData();
     ImplSchedulerData *pMostUrgent = nullptr;
@@ -57,7 +57,7 @@ ImplSchedulerData *ImplSchedulerData::GetMostImportantTask( bool bTimerOnly, sal
     for ( ImplSchedulerData *pSchedulerData = pSVData->mpFirstSchedulerData; pSchedulerData; pSchedulerData = pSchedulerData->mpNext )
     {
         if ( !pSchedulerData->mpScheduler || pSchedulerData->mbDelete || pSchedulerData->mbInScheduler ||
-             !pSchedulerData->mpScheduler->ReadyForSchedule( bTimerOnly, nTimeNow ) ||
+             !pSchedulerData->mpScheduler->ReadyForSchedule( bIdle, nTimeNow ) ||
              !pSchedulerData->mpScheduler->IsActive())
             continue;
         if (!pMostUrgent)
@@ -164,20 +164,20 @@ void InitSystemTimer(ImplSVData* pSVData)
 
 }
 
-void Scheduler::CallbackTaskScheduling(bool)
+void Scheduler::CallbackTaskScheduling( bool bIdle )
 {
     // this function is for the saltimer callback
-    Scheduler::ProcessTaskScheduling( false );
+    Scheduler::ProcessTaskScheduling( bIdle );
 }
 
-bool Scheduler::ProcessTaskScheduling( bool bTimerOnly )
+bool Scheduler::ProcessTaskScheduling( bool bIdle )
 {
     ImplSchedulerData* pSchedulerData;
     sal_uInt64         nTime = tools::Time::GetSystemTicks();
 
     DBG_TESTSOLARMUTEX();
 
-    if ((pSchedulerData = ImplSchedulerData::GetMostImportantTask(bTimerOnly, nTime)))
+    if ((pSchedulerData = ImplSchedulerData::GetMostImportantTask(bIdle, nTime)))
     {
         SAL_INFO("vcl.schedule", "Invoke task " << pSchedulerData->GetDebugName());
 
@@ -203,8 +203,6 @@ bool Scheduler::GetDeterministicMode()
 
 sal_uInt64 Scheduler::CalculateMinimumTimeout( bool &bHasActiveIdles )
 {
-    // process all pending Tasks
-    // if bTimer True, only handle timer
     ImplSchedulerData* pSchedulerData = nullptr;
     ImplSchedulerData* pPrevSchedulerData = nullptr;
     ImplSVData*        pSVData = ImplGetSVData();