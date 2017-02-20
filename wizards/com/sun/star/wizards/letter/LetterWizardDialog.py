LemmingAvalanche/libreoffice-repaired

commit 8ea1a6b6e341a8f4b3df51ab6a8653f8fceb9366

use the precanned constant for this
Change-Id: Iaa6283029f9de41e2506051c49dc18f876b28587

wizards/com/sun/star/wizards/letter/LetterWizardDialog.py
@@ -1094,7 +1094,7 @@ class LetterWizardDialog(WizardDialog):
                 PropertyNames.PROPERTY_TABINDEX,
                 PropertyNames.PROPERTY_WIDTH),
             (0, 10,
-                "private:resource/dbu/image/19205",
+                UIConsts.INFOIMAGEURL,
                 "ImageControl2", 92, 145,  False, 6, 66, 10))
         self.insertLabel("lblTemplateName",
             (PropertyNames.PROPERTY_HEIGHT,

commit e868ae444530bec608854ce41d84b20892214e9c

Import UIConsts to use INFOIMAGEURL
Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=8ea1a6b6e341a8f4b3df51ab6a8653f8fceb9366

Change-Id: I4f25b5df6a657ad22080b039eaa1746ce4b82b9a
Reviewed-on: https://gerrit.libreoffice.org/34495
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Julien Nabet <serval2412@yahoo.fr>

wizards/com/sun/star/wizards/letter/LetterWizardDialog.py
@@ -18,7 +18,7 @@
 from .LetterWizardDialogConst import LetterWizardDialogConst, HIDMAIN, HID
 from .LetterWizardDialogResources import LetterWizardDialogResources
 from ..common.HelpIds import HelpIds
-from ..ui.WizardDialog import WizardDialog, uno, PropertyNames
+from ..ui.WizardDialog import WizardDialog, uno, UIConsts, PropertyNames
 
 from com.sun.star.awt.FontUnderline import SINGLE
 