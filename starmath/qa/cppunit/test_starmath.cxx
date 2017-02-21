LemmingAvalanche/libreoffice-repaired

commit 9f5091db200168a9cfbb673404cecfaaae78f868

resolved fdo#35756 import more than 64k HTML table cells
Enhanced EditEngine to be able to hold more than 64k paragraphs. Used
also in RTF import Calc and Writer, so that could benefit as well.

* changed all EditEngine,Outliner,... related paragraph index/count
  variables from sal_uInt16 to sal_Int32
  * sal_Int32 instead of sal_uInt32 to match accessibility API
  * matched some Outliner methods' paragraph parameters from sal_uLong
    to sal_Int32
  * containers capable to hold size_t nevertheless are limited to a
    maximum of sal_Int32
* changed definition of EE_PARA_NOT_FOUND and EE_PARA_ALL to
  SAL_MAX_INT32
+ added EE_PARA_MAX_COUNT and EE_TEXTPOS_MAX_COUNT to initialize
  ESelection with what previously were hard coded 0xFFFF all over the
  place
+ for similar reason added EE_TEXTPOS_ALL corresponding to EE_PARA_ALL
  to initialize an ESelection spanning all available text like
  aSel(0,0,EE_PARA_ALL,EE_TEXTPOS_ALL)

Change-Id: I62d1b9c399cd709a4f93dbac31b219a61c46ec00
Reviewed-on: https://gerrit.libreoffice.org/3838
Tested-by: LibreOffice gerrit bot <gerrit@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

starmath/source/edit.cxx
@@ -59,7 +59,7 @@ using namespace com::sun::star::uno;
 
 
 void SmGetLeftSelectionPart(const ESelection &rSel,
-                            sal_uInt16 &nPara, sal_uInt16 &nPos)
+                            sal_Int32 &nPara, sal_uInt16 &nPos)
     // returns paragraph number and position of the selections left part
 {
     // compare start and end of selection and use the one that comes first
@@ -266,11 +266,12 @@ IMPL_LINK(SmEditWindow, CursorMoveTimerHdl, Timer *, EMPTYARG /*pTimer*/)
         if (pView)
         {
             // get row and column to look for
-            sal_uInt16  nRow, nCol;
+            sal_Int32  nRow;
+            sal_uInt16 nCol;
             SmGetLeftSelectionPart(aNewSelection, nRow, nCol);
             nRow++;
             nCol++;
-            pView->GetGraphicWindow().SetCursorPos(nRow, nCol);
+            pView->GetGraphicWindow().SetCursorPos(static_cast<sal_uInt16>(nRow), nCol);
             aOldSelection = aNewSelection;
         }
     }
@@ -697,8 +698,8 @@ void SmEditWindow::SelectAll()
     OSL_ENSURE( pEditView, "NULL pointer" );
     if (pEditView)
     {
-        // 0xFFFF as last two parameters refers to the end of the text
-        pEditView->SetSelection( ESelection( 0, 0, 0xFFFF, 0xFFFF ) );
+        // ALL as last two parameters refers to the end of the text
+        pEditView->SetSelection( ESelection( 0, 0, EE_PARA_ALL, EE_TEXTPOS_ALL ) );
     }
 }
 
@@ -757,7 +758,7 @@ void SmEditWindow::SelNextMark()
     {
         ESelection eSelection = pEditView->GetSelection();
         sal_Int32 nPos = eSelection.nEndPos;
-        sal_uInt16     nCounts    = pEditEngine->GetParagraphCount();
+        sal_Int32 nCounts = pEditEngine->GetParagraphCount();
 
         while (eSelection.nEndPara < nCounts)
         {
@@ -788,7 +789,7 @@ void SmEditWindow::SelPrevMark()
         sal_Int32 nMax = eSelection.nStartPos;
         OUString aText(pEditEngine->GetText(eSelection.nStartPara));
         OUString aMark("<?>");
-        sal_uInt16     nCounts    = pEditEngine->GetParagraphCount();
+        sal_Int32 nCounts = pEditEngine->GetParagraphCount();
 
         do
         {

commit 7d0464b6739f8f9dad1d1c811f1b05a0dc604d55

tdf#106116 "Previous Marker" changes selection only when needed
This fixes a hang caused by repeated Shift+F4. Although it seems
a regression from 9f5091db200168a9cfbb673404cecfaaae78f868,
the original code of SmEditWindow::SelPrevMark() had been doomed
due to conditional loop depending on obscure signed/unsigned
conversion.

Change-Id: I61f630eec44b285dc1f1c27097acde4b48ed2991
Reviewed-on: https://gerrit.libreoffice.org/34503
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Takeshi Abe <tabe@fixedpoint.jp>

starmath/source/edit.cxx
@@ -878,36 +878,20 @@ void SmEditWindow::SelPrevMark()
     if (pEditEngine  &&  pEditView)
     {
         ESelection eSelection = pEditView->GetSelection();
-        sal_Int32 nPos = -1;
+        sal_Int32 nPara = eSelection.nStartPara;
         sal_Int32 nMax = eSelection.nStartPos;
-        OUString aText(pEditEngine->GetText(eSelection.nStartPara));
-        OUString aMark("<?>");
-        sal_Int32 nCounts = pEditEngine->GetParagraphCount();
+        OUString aText(pEditEngine->GetText(nPara));
+        const OUString aMark("<?>");
+        sal_Int32 nPos;
 
-        do
+        while ( (nPos = aText.lastIndexOf(aMark, nMax)) < 0 )
         {
-            sal_Int32 nMarkIndex = aText.indexOf(aMark);
-            while ((nMarkIndex < nMax) && (nMarkIndex != -1))
-            {
-                nPos = nMarkIndex;
-                nMarkIndex = aText.indexOf(aMark, nMarkIndex + 1);
-            }
-
-            if (nPos == -1)
-            {
-                eSelection.nStartPara--;
-                aText = pEditEngine->GetText(eSelection.nStartPara);
-                nMax = aText.getLength();
-            }
+            if (--nPara < 0)
+                return;
+            aText = pEditEngine->GetText(nPara);
+            nMax = aText.getLength();
         }
-        while ((eSelection.nStartPara < nCounts) &&
-            (nPos == -1));
-
-        if (nPos != -1)
-        {
-            pEditView->SetSelection(ESelection(
-                eSelection.nStartPara, nPos, eSelection.nStartPara, nPos + 3));
-        }
+        pEditView->SetSelection(ESelection(nPara, nPos, nPara, nPos + 3));
     }
 }
 

starmath/qa/cppunit/test_starmath.cxx
@@ -133,6 +133,14 @@ void Test::editMarker()
         m_pEditWindow->Delete();
         m_pEditWindow->InsertText("b");
 
+        // tdf#106116: should be safe i.e. do nothing
+        m_pEditWindow->SelPrevMark();
+        auto aSelection = m_pEditWindow->GetSelection();
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), aSelection.nStartPara);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(9), aSelection.nStartPos);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), aSelection.nEndPara);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(9), aSelection.nEndPos);
+
         m_pEditWindow->Flush();
         OUString sFinalText = m_pEditWindow->GetText();
         CPPUNIT_ASSERT_EQUAL_MESSAGE("Should be a under b under c", sTargetText, sFinalText);