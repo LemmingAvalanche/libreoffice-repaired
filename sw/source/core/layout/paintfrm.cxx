LemmingAvalanche/libreoffice-repaired

commit 91859e4ae1212df5a85ad81614a93b4446976f1e

convert SwFootnoteAdj to scoped enum
and drop read-only FTNADJ_CENTER enumerator

Change-Id: Id059a8e84aed703a86f0ef4d66c24e9401dfc3a9

sw/source/core/layout/paintfrm.cxx
@@ -5573,14 +5573,12 @@ void SwFootnoteContFrame::PaintLine( const SwRect& rRect,
     SwTwips nX = aRectFnSet.GetPrtLeft(*this);
     switch ( rInf.GetAdj() )
     {
-        case FTNADJ_CENTER:
-            nX += nPrtWidth/2 - nWidth/2; break;
-        case FTNADJ_RIGHT:
+        case SwFootnoteAdj::Right:
             nX += nPrtWidth - nWidth; break;
-        case FTNADJ_LEFT:
+        case SwFootnoteAdj::Left:
             /* do nothing */; break;
         default:
-            OSL_ENSURE( false, "New adjustment for footnote lines?" );
+            assert(false);
     }
     SwTwips nLineWidth = rInf.GetLineWidth();
     const SwRect aLineRect = aRectFnSet.IsVert() ?

sw/inc/pagedesc.hxx
@@ -43,11 +43,9 @@ class SwNode;
 class SwPageDescs;
 
 /// Separator line adjustment.
-enum SwFootnoteAdj
+enum class SwFootnoteAdj
 {
-    FTNADJ_LEFT,
-    FTNADJ_CENTER,
-    FTNADJ_RIGHT
+    Left, Right
 };
 
 /// Footnote information.

sw/source/core/layout/pagedesc.cxx
@@ -383,8 +383,8 @@ SwPageFootnoteInfo::SwPageFootnoteInfo()
     , m_nBottomDist( 57 )
 {
     m_eAdjust = FRMDIR_HORI_RIGHT_TOP == GetDefaultFrameDirection(GetAppLanguage()) ?
-           FTNADJ_RIGHT :
-           FTNADJ_LEFT;
+           SwFootnoteAdj::Right :
+           SwFootnoteAdj::Left;
 }
 
 SwPageFootnoteInfo::SwPageFootnoteInfo( const SwPageFootnoteInfo &rCpy )

commit e812890d783ce5ad4b0e8e9e1706c9a40911196b

sw: re-add handling of Center footnote alignment
... that was erroneously removed from SwFootnoteContFrame::PaintLine().

Fixes the assert when loading tdf96579-1.odt.

(regression from 91859e4ae1212df5a85ad81614a93b4446976f1e)

Change-Id: I33549bea3e5954fc347b05aa713e5982cde401ec

sw/source/core/layout/paintfrm.cxx
@@ -5573,6 +5573,8 @@ void SwFootnoteContFrame::PaintLine( const SwRect& rRect,
     SwTwips nX = aRectFnSet.GetPrtLeft(*this);
     switch ( rInf.GetAdj() )
     {
+        case css::text::HorizontalAdjust_CENTER:
+            nX += nPrtWidth/2 - nWidth/2; break;
         case css::text::HorizontalAdjust_RIGHT:
             nX += nPrtWidth - nWidth; break;
         case css::text::HorizontalAdjust_LEFT: