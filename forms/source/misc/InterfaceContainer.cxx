LemmingAvalanche/libreoffice-repaired

commit 3b6320834a00b4096f1c8342e319f3fa79700c83

new loplugin: use more efficient find() methods
(Original idea from Kendy)
Look for code that is calling std::find on a sorted container
(set/map/vector) and warn about it - the code should be using
the find method on the container itself, since that is considerably faster.

Change-Id: Ib74e5d3faa836eeb0df16a736d202696626bdfd2

forms/source/misc/InterfaceContainer.cxx
@@ -680,9 +680,8 @@ throw (::com::sun::star::uno::RuntimeException, std::exception) {
     if (evt.PropertyName == PROPERTY_NAME)
     {
         ::osl::MutexGuard aGuard( m_rMutex );
-        OInterfaceMap::iterator i = ::std::find(m_aMap.begin(), m_aMap.end(),
-            ::std::pair<const OUString, InterfaceRef >(::comphelper::getString(evt.OldValue),evt.Source));
-        if (i != m_aMap.end())
+        OInterfaceMap::iterator i = m_aMap.find(::comphelper::getString(evt.OldValue));
+        if (i != m_aMap.end() && (*i).second != evt.Source)
         {
             InterfaceRef  xCorrectType((*i).second);
             m_aMap.erase(i);

compilerplugins/clang/store/findoncontainer.cxx
@@ -1,0 +1,77 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <string>
+#include <set>
+
+#include "plugin.hxx"
+
+// Look for places calling std::find on a standard container where it should be using the container find method, which
+// is more efficient.
+//
+// This lives in /store because the implementation is a hack and is highly dependant on the inwards
+// of the libc++ library on the machine it runs on.
+//
+
+namespace {
+
+class FindOnContainer:
+    public RecursiveASTVisitor<FindOnContainer>, public loplugin::Plugin
+{
+public:
+    explicit FindOnContainer(InstantiationData const & data): Plugin(data) {}
+
+    virtual void run() override { TraverseDecl(compiler.getASTContext().getTranslationUnitDecl()); }
+
+    bool VisitCallExpr(const CallExpr * expr);
+};
+
+bool FindOnContainer::VisitCallExpr(const CallExpr * expr) {
+    if (ignoreLocation(expr)) {
+        return true;
+    }
+    FunctionDecl const * fdecl = expr->getDirectCallee();
+    if (fdecl == nullptr) {
+        return true;
+    }
+    std::string qname { fdecl->getQualifiedNameAsString() };
+    if (qname == "std::find")
+    {
+        std::string tname = expr->getArg(0)->getType().getAsString();
+        if (tname.find("std::_List_iterator") != std::string::npos
+            || tname.find("std::_List_const_iterator") != std::string::npos
+            || tname.find("std::vector") != std::string::npos
+            || tname.find("std::_Deque_iterator") != std::string::npos
+            || tname == "const int *"
+            || tname == "struct std::_Bit_const_iterator"
+            || tname == "const rtl::OUString *"
+            || tname == "class rtl::OUString *"
+            || tname == "const class rtl::OUString *"
+            || tname == "const sal_Int8 *"
+            || tname == "const sal_Int32 *"
+            || tname == "sal_Int32 *"
+            || tname == "sal_uInt16 *" )
+        {
+            return true;
+        }
+        expr->dump();
+        report(
+            DiagnosticsEngine::Warning,
+            ("rather use the more specific find method " + tname),
+            expr->getExprLoc());
+        return true;
+    }
+    return true;
+}
+
+loplugin::Plugin::Registration< FindOnContainer > X("findoncontainer");
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

connectivity/source/drivers/mork/MQueryHelper.cxx
@@ -228,7 +228,7 @@ sal_Int32 MQueryHelper::executeQuery(OConnection* xConnection, MQueryExpression 
                 {
                     int rowId = rowIter->first;
                     // belongs this row id to the list table?
-                    if (listRecords.end() == std::find(listRecords.begin(), listRecords.end(), rowId))
+                    if (listRecords.end() == listRecords.find(rowId))
                     {
                         // no, skip it
                         continue;

commit 4f8c03be4de5aeb742eb0e86f2ab1141608fe6d7

tdf#103830 Form field names not update in python macro
regression from commit 3b6320834a00b4096f1c8342e319f3fa79700c83
"new loplugin: use more efficient find() methods"

Regression found by Aron Budea.

Change-Id: I0e84376dfd754738de97882979058f86c2476bd6
Reviewed-on: https://gerrit.libreoffice.org/34600
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Julien Nabet <serval2412@yahoo.fr>

forms/source/misc/InterfaceContainer.cxx
@@ -662,9 +662,9 @@ void OInterfaceContainer::propertyChange(const PropertyChangeEvent& evt) {
     {
         ::osl::MutexGuard aGuard( m_rMutex );
         OInterfaceMap::iterator i = m_aMap.find(::comphelper::getString(evt.OldValue));
-        if (i != m_aMap.end() && (*i).second != evt.Source)
+        if (i != m_aMap.end() && i->second == evt.Source)
         {
-            css::uno::Reference<css::uno::XInterface>  xCorrectType((*i).second);
+            css::uno::Reference<css::uno::XInterface>  xCorrectType(i->second);
             m_aMap.erase(i);
             m_aMap.insert(::std::pair<const OUString, css::uno::Reference<css::uno::XInterface> >(::comphelper::getString(evt.NewValue),xCorrectType));
         }