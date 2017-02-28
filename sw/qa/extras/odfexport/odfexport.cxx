LemmingAvalanche/libreoffice-repaired

commit b8ba29282b892c25d9bf308cb4d087e1314f4c8a

Related: #i124638# Second step of DrawingLayer FillAttributes...
for Writer objects, now added support for Paragraph and PageStyle (including
Header and Footer) for direct attributes and style attributes

(cherry picked from commit cc25c58f7052827bfebdc9fbeec668c8fa29ed1b)

Conflicts:
	cui/source/factory/dlgfact.cxx
	cui/source/factory/dlgfact.hxx
	cui/source/inc/bbdlg.hxx
	cui/source/inc/cuires.hrc
	cui/source/tabpages/bbdlg.cxx
	cui/source/tabpages/bbdlg.src
	cui/source/tabpages/page.cxx
	cui/source/tabpages/tparea.cxx
	include/svx/pagectrl.hxx
	include/svx/svxdlg.hxx
	include/svx/svxids.hrc
	include/xmloff/PageMasterStyleMap.hxx
	include/xmloff/prstylei.hxx
	include/xmloff/txtprmap.hxx
	svx/Package_inc.mk
	svx/inc/svx/hdft.hxx
	svx/source/dialog/hdft.cxx
	svx/source/dialog/pagectrl.cxx
	svx/source/tbxctrls/tbxcolorupdate.cxx
	svx/source/unodraw/unobrushitemhelper.cxx
	sw/Library_sw.mk
	sw/inc/fillattributes.hxx
	sw/inc/format.hxx
	sw/inc/frmatr.hxx
	sw/inc/frmfmt.hxx
	sw/inc/hintids.hxx
	sw/inc/hints.hxx
	sw/inc/ndtxt.hxx
	sw/inc/node.hxx
	sw/inc/swunohelper.hxx
	sw/inc/unobrushitemhelper.hxx
	sw/inc/unoprnms.hxx
	sw/source/core/attr/format.cxx
	sw/source/core/attr/hints.cxx
	sw/source/core/doc/docdesc.cxx
	sw/source/core/doc/docdraw.cxx
	sw/source/core/doc/docfmt.cxx
	sw/source/core/doc/docnew.cxx
	sw/source/core/doc/docredln.cxx
	sw/source/core/doc/poolfmt.cxx
	sw/source/core/doc/visiturl.cxx
	sw/source/core/docnode/node.cxx
	sw/source/core/inc/frame.hxx
	sw/source/core/inc/frmtool.hxx
	sw/source/core/inc/rolbck.hxx
	sw/source/core/layout/atrfrm.cxx
	sw/source/core/layout/fillattributes.cxx
	sw/source/core/layout/findfrm.cxx
	sw/source/core/layout/paintfrm.cxx
	sw/source/core/txtnode/ndtxt.cxx
	sw/source/core/txtnode/thints.cxx
	sw/source/core/txtnode/txtedt.cxx
	sw/source/core/undo/rolbck.cxx
	sw/source/core/unocore/swunohelper.cxx
	sw/source/core/unocore/unoframe.cxx
	sw/source/core/unocore/unomap.cxx
	sw/source/core/unocore/unoparagraph.cxx

sw/source/core/unocore/unoparagraph.cxx
@@ -47,7 +47,16 @@
 #include <comphelper/servicehelper.hxx>
 #include <boost/scoped_ptr.hpp>
 
+//UUUU
+#include <swunohelper.hxx>
+#include <svx/unobrushitemhelper.hxx>
+#include <editeng/unoipset.hxx>
+#include <svx/xflbstit.hxx>
+#include <svx/xflbmtit.hxx>
+#include <com/sun/star/drawing/BitmapMode.hpp>
+
 using namespace ::com::sun::star;
+
 
 class SwParaSelection
 {
@@ -150,6 +159,13 @@ public:
             const uno::Sequence< OUString >& rPropertyNames)
         throw (beans::UnknownPropertyException, lang::WrappedTargetException,
                 uno::RuntimeException);
+
+    //UUUU
+    void GetSinglePropertyValue_Impl(
+        const SfxItemPropertySimpleEntry& rEntry,
+        const SfxItemSet& rSet,
+        uno::Any& rAny ) const
+    throw(uno::RuntimeException);
 
     uno::Sequence< beans::GetDirectPropertyTolerantResult >
         GetPropertyValuesTolerant_Impl(
@@ -424,6 +440,98 @@ throw (beans::PropertyVetoException, lang::IllegalArgumentException,
     }
 }
 
+//UUUU Support for DrawingLayer FillStyles for GetPropertyValue() usages
+void SwXParagraph::Impl::GetSinglePropertyValue_Impl(
+    const SfxItemPropertySimpleEntry& rEntry,
+    const SfxItemSet& rSet,
+    uno::Any& rAny ) const
+throw(uno::RuntimeException)
+{
+    bool bDone(false);
+
+    switch(rEntry.nWID)
+    {
+        case RES_BACKGROUND:
+        {
+            const SvxBrushItem aOriginalBrushItem(getSvxBrushItemFromSourceSet(rSet, RES_BACKGROUND));
+            const sal_uInt8 nMemberId(rEntry.nMemberId & (~SFX_METRIC_ITEM));
+
+            if(!aOriginalBrushItem.QueryValue(rAny, nMemberId))
+            {
+                OSL_ENSURE(false, "Error getting attribute from RES_BACKGROUND (!)");
+            }
+
+            bDone = true;
+            break;
+        }
+        case OWN_ATTR_FILLBMP_MODE:
+        {
+            const XFillBmpStretchItem* pStretchItem = dynamic_cast< const XFillBmpStretchItem* >(&rSet.Get(XATTR_FILLBMP_STRETCH));
+            const XFillBmpTileItem* pTileItem = dynamic_cast< const XFillBmpTileItem* >(&rSet.Get(XATTR_FILLBMP_TILE));
+
+            if( pTileItem && pTileItem->GetValue() )
+            {
+                rAny <<= drawing::BitmapMode_REPEAT;
+            }
+            else if( pStretchItem && pStretchItem->GetValue() )
+            {
+                rAny <<= drawing::BitmapMode_STRETCH;
+            }
+            else
+            {
+                rAny <<= drawing::BitmapMode_NO_REPEAT;
+            }
+
+            bDone = true;
+            break;
+        }
+        default: break;
+    }
+
+    if(!bDone)
+    {
+        // fallback to standard get value implementation used before this helper was created
+        m_rPropSet.getPropertyValue(rEntry, rSet, rAny);
+
+        if(rEntry.aType == ::getCppuType((const sal_Int16*)0) && rEntry.aType != rAny.getValueType())
+        {
+            // since the sfx uInt16 item now exports a sal_Int32, we may have to fix this here
+            sal_Int32 nValue(0);
+
+            rAny >>= nValue;
+            rAny <<= static_cast< sal_Int16 >(nValue);
+        }
+
+        //UUUU check for needed metric translation
+        if(rEntry.nMemberId & SFX_METRIC_ITEM)
+        {
+            bool bDoIt(true);
+
+            if(XATTR_FILLBMP_SIZEX == rEntry.nWID || XATTR_FILLBMP_SIZEY == rEntry.nWID)
+            {
+                // exception: If these ItemTypes are used, do not convert when these are negative
+                // since this means they are intended as percent values
+                sal_Int32 nValue = 0;
+
+                if(rAny >>= nValue)
+                {
+                    bDoIt = nValue > 0;
+                }
+            }
+
+            if(bDoIt)
+            {
+                const SfxMapUnit eMapUnit(rSet.GetPool()->GetMetric(rEntry.nWID));
+
+                if(eMapUnit != SFX_MAPUNIT_100TH_MM)
+                {
+                    SvxUnoConvertToMM(eMapUnit, rAny);
+                }
+            }
+        }
+    }
+}
+
 uno::Sequence< uno::Any > SwXParagraph::Impl::GetPropertyValues_Impl(
         const uno::Sequence< OUString > & rPropertyNames )
 throw (beans::UnknownPropertyException, lang::WrappedTargetException,
@@ -456,8 +564,8 @@ throw (beans::UnknownPropertyException, lang::WrappedTargetException,
                 *pEntry, aPam, &(pValues[nProp]), eTemp, &rTxtNode );
             if (!bDone)
             {
-                m_rPropSet.getPropertyValue(
-                    *pEntry, rAttrSet, pValues[nProp]);
+                //UUUU
+                GetSinglePropertyValue_Impl(*pEntry, rAttrSet, pValues[nProp]);
             }
         }
     }
@@ -717,8 +825,8 @@ throw (uno::RuntimeException)
                         // if not found try the real paragraph attributes...
                         if (!bDone)
                         {
-                            m_rPropSet.getPropertyValue(
-                                *pEntry, rValueAttrSet, aValue );
+                            //UUUU
+                            GetSinglePropertyValue_Impl(*pEntry, rValueAttrSet, aValue);
                         }
                     }
 
@@ -832,61 +940,108 @@ throw (beans::UnknownPropertyException, lang::WrappedTargetException,
 }
 
 static beans::PropertyState lcl_SwXParagraph_getPropertyState(
-//                          SwUnoCrsr& rUnoCrsr,
-                            const SwTxtNode& rTxtNode,
-                            const SwAttrSet** ppSet,
-                            const SfxItemPropertySimpleEntry& rEntry,
-                            bool &rAttrSetFetched )
+    // SwUnoCrsr& rUnoCrsr,
+    const SwTxtNode& rTxtNode,
+    const SwAttrSet** ppSet,
+    const SfxItemPropertySimpleEntry& rEntry,
+    bool &rAttrSetFetched)
     throw (beans::UnknownPropertyException, uno::RuntimeException, std::exception)
 {
-    beans::PropertyState eRet = beans::PropertyState_DEFAULT_VALUE;
+    beans::PropertyState eRet(beans::PropertyState_DEFAULT_VALUE);
 
-    if(!(*ppSet) && !rAttrSetFetched )
+    if(!(*ppSet) && !rAttrSetFetched)
     {
         (*ppSet) = rTxtNode.GetpSwAttrSet();
         rAttrSetFetched = true;
     }
-    SwPosition aPos( rTxtNode );
-    SwPaM aPam( aPos );
-    switch( rEntry.nWID )
+
+    SwPosition aPos(rTxtNode);
+    SwPaM aPam(aPos);
+    bool bDone(false);
+
+    switch(rEntry.nWID)
     {
-    case FN_UNO_NUM_RULES:
-        // if numbering is set, return it; else do nothing
-        SwUnoCursorHelper::getNumberingProperty( aPam, eRet, NULL );
-        break;
-    case FN_UNO_ANCHOR_TYPES:
-        break;
-    case RES_ANCHOR:
-        if ( MID_SURROUND_SURROUNDTYPE != rEntry.nMemberId )
-            goto lcl_SwXParagraph_getPropertyStateDEFAULT;
-        break;
-    case RES_SURROUND:
-        if ( MID_ANCHOR_ANCHORTYPE != rEntry.nMemberId )
-            goto lcl_SwXParagraph_getPropertyStateDEFAULT;
-        break;
-    case FN_UNO_PARA_STYLE:
-    case FN_UNO_PARA_CONDITIONAL_STYLE_NAME:
+        case FN_UNO_NUM_RULES:
         {
-            SwFmtColl* pFmt = SwUnoCursorHelper::GetCurTxtFmtColl(
-                aPam, rEntry.nWID == FN_UNO_PARA_CONDITIONAL_STYLE_NAME);
-            eRet = pFmt ? beans::PropertyState_DIRECT_VALUE
-                        : beans::PropertyState_AMBIGUOUS_VALUE;
+            // if numbering is set, return it; else do nothing
+            SwUnoCursorHelper::getNumberingProperty(aPam,eRet,NULL);
+            bDone = true;
+            break;
         }
-        break;
-    case FN_UNO_PAGE_STYLE:
+        case FN_UNO_ANCHOR_TYPES:
+        {
+            bDone = true;
+            break;
+        }
+        case RES_ANCHOR:
+        {
+            bDone = (MID_SURROUND_SURROUNDTYPE == rEntry.nMemberId);
+            break;
+        }
+        case RES_SURROUND:
+        {
+            bDone = (MID_ANCHOR_ANCHORTYPE == rEntry.nMemberId);
+            break;
+        }
+        case FN_UNO_PARA_STYLE:
+        case FN_UNO_PARA_CONDITIONAL_STYLE_NAME:
+        {
+            SwFmtColl* pFmt = SwUnoCursorHelper::GetCurTxtFmtColl(aPam,rEntry.nWID == FN_UNO_PARA_CONDITIONAL_STYLE_NAME);
+            eRet = pFmt ? beans::PropertyState_DIRECT_VALUE : beans::PropertyState_AMBIGUOUS_VALUE;
+            bDone = true;
+            break;
+        }
+        case FN_UNO_PAGE_STYLE:
         {
             OUString sVal;
             SwUnoCursorHelper::GetCurPageStyle( aPam, sVal );
             eRet = !sVal.isEmpty() ? beans::PropertyState_DIRECT_VALUE
                               : beans::PropertyState_AMBIGUOUS_VALUE;
+            bDone = true;
+            break;
         }
-        break;
-    lcl_SwXParagraph_getPropertyStateDEFAULT:
-    default:
-        if((*ppSet) && SFX_ITEM_SET == (*ppSet)->GetItemState(rEntry.nWID, false))
-            eRet = beans::PropertyState_DIRECT_VALUE;
-        break;
+
+        //UUUU DrawingLayer PropertyStyle support
+        case OWN_ATTR_FILLBMP_MODE:
+        {
+            if(*ppSet)
+            {
+                if(SFX_ITEM_SET == (*ppSet)->GetItemState(XATTR_FILLBMP_STRETCH, false)
+                    || SFX_ITEM_SET == (*ppSet)->GetItemState(XATTR_FILLBMP_TILE, false))
+                {
+                    eRet = beans::PropertyState_DIRECT_VALUE;
+                }
+                else
+                {
+                    eRet = beans::PropertyState_AMBIGUOUS_VALUE;
+                }
+
+                bDone = true;
+            }
+            break;
+        }
+        case RES_BACKGROUND:
+        {
+            if(*ppSet)
+            {
+                if(SWUnoHelper::needToMapFillItemsToSvxBrushItemTypes(**ppSet))
+                {
+                    eRet = beans::PropertyState_DIRECT_VALUE;
+                    bDone = true;
+                }
+            }
+            break;
+        }
     }
+
+    if(!bDone)
+    {
+        if((*ppSet) && SFX_ITEM_SET == (*ppSet)->GetItemState(rEntry.nWID, false))
+        {
+            eRet = beans::PropertyState_DIRECT_VALUE;
+        }
+    }
+
     return eRet;
 }
 
@@ -990,10 +1145,24 @@ throw (beans::UnknownPropertyException, uno::RuntimeException, std::exception)
             static_cast<cppu::OWeakObject *>(this));
     }
 
-    if (pEntry->nWID < RES_FRMATR_END)
+    const bool bBelowFrmAtrEnd(pEntry->nWID < RES_FRMATR_END);
+    const bool bDrawingLayerRange(XATTR_FILL_FIRST <= pEntry->nWID && XATTR_FILL_LAST >= pEntry->nWID);
+
+    if(bBelowFrmAtrEnd || bDrawingLayerRange)
     {
         std::set<sal_uInt16> aWhichIds;
-        aWhichIds.insert( pEntry->nWID );
+
+        //UUUU For FillBitmapMode two IDs have to be reset (!)
+        if(OWN_ATTR_FILLBMP_MODE == pEntry->nWID)
+        {
+            aWhichIds.insert(XATTR_FILLBMP_STRETCH);
+            aWhichIds.insert(XATTR_FILLBMP_TILE);
+        }
+        else
+        {
+            aWhichIds.insert(pEntry->nWID);
+        }
+
         if (pEntry->nWID < RES_PARATR_BEGIN)
         {
             aCursor.GetDoc()->ResetAttrs(aCursor, true, aWhichIds);
@@ -1010,14 +1179,19 @@ throw (beans::UnknownPropertyException, uno::RuntimeException, std::exception)
             {
                 pTemp->MovePara(fnParaCurr, fnParaStart);
             }
+
             pTemp->SetMark();
             *pTemp->GetPoint() = aEnd;
             //pTemp->Exchange();
+
             SwUnoCursorHelper::SelectPam(*pTemp, true);
+
             if (!SwUnoCursorHelper::IsEndOfPara(*pTemp))
             {
                 pTemp->MovePara(fnParaCurr, fnParaEnd);
             }
+
+
             pTemp->GetDoc()->ResetAttrs(*pTemp, true, aWhichIds);
         }
     }
@@ -1051,10 +1225,13 @@ throw (beans::UnknownPropertyException, lang::WrappedTargetException,
             static_cast<cppu::OWeakObject *>(this));
     }
 
-    if (pEntry->nWID < RES_FRMATR_END)
+    const bool bBelowFrmAtrEnd(pEntry->nWID < RES_FRMATR_END);
+    const bool bDrawingLayerRange(XATTR_FILL_FIRST <= pEntry->nWID && XATTR_FILL_LAST >= pEntry->nWID);
+
+    if(bBelowFrmAtrEnd || bDrawingLayerRange)
     {
-        const SfxPoolItem& rDefItem =
-            rTxtNode.GetDoc()->GetAttrPool().GetDefaultItem(pEntry->nWID);
+        const SfxPoolItem& rDefItem = rTxtNode.GetDoc()->GetAttrPool().GetDefaultItem(pEntry->nWID);
+
         rDefItem.QueryValue(aRet, pEntry->nMemberId);
     }
 

commit dcb04ba805dc396b82ac2d7ea1a34f0575af7c58

tdf#77111 sw: add unit test, fix getting PageNumberOffset property
There are 2 places where the style:page-number attribute is handled, in
XMLNumberWithAutoInsteadZeroPropHdl (for paragraphs) and
SvXMLExportItemMapper/SvXMLImportItemMapper (for tables)

Apparently for the paragraph case, 0 was never written to mean "auto",
this happened only for tables (see
7edaf190e2b18fe5dd9b7dd8d8e7e24b2ff26520).

The test reveals that SwXParagraph::Impl::GetSinglePropertyValue_Impl()
was kinda broken by b8ba29282b892c25d9bf308cb4d087e1314f4c8a, which
converts void values of "short" properties to short(0), wrongly assuming
that the Any contains a long.  (Fortunately this is then mapped to
"auto" in XMLNumberWithAutoInsteadZeroPropHdl.)

Change-Id: I3dc6d5533ac96955440b1589f1999d06750101af

sw/source/core/unocore/unoparagraph.cxx
@@ -495,8 +495,10 @@ void SwXParagraph::Impl::GetSinglePropertyValue_Impl(
             // since the sfx uInt16 item now exports a sal_Int32, we may have to fix this here
             sal_Int32 nValue(0);
 
-            rAny >>= nValue;
-            rAny <<= static_cast< sal_Int16 >(nValue);
+            if (rAny >>= nValue)
+            {
+                rAny <<= static_cast<sal_Int16>(nValue);
+            }
         }
 
         //UUUU check for needed metric translation

sw/source/core/unocore/unostyle.cxx
@@ -3989,8 +3989,10 @@ uno::Sequence< uno::Any > SwXAutoStyle::GetPropertyValues_Impl(
             {
                 // since the sfx uint16 item now exports a sal_Int32, we may have to fix this here
                 sal_Int32 nValue = 0;
-                aTarget >>= nValue;
-                aTarget <<= (sal_Int16)nValue;
+                if (aTarget >>= nValue)
+                {
+                    aTarget <<= static_cast<sal_Int16>(nValue);
+                }
             }
 
             // check for needed metric translation

sw/source/filter/xml/xmlimpit.cxx
@@ -786,6 +786,9 @@ bool SvXMLImportItemMapper::PutXMLValue(
                 sal_Int32 nVal;
                 bOk = ::sax::Converter::convertNumber(
                         nVal, rValue, 0, USHRT_MAX);
+                // i#114163 tdf#77111: OOo < 3.3 had a bug where it wrote
+                // "auto" as "0" for tables - now that we support a real offset
+                //  0, this fake "0" MUST NOT be imported as offset 0!
                 if( bOk && nVal > 0 )
                     rPageDesc.SetNumOffset( (sal_uInt16)nVal );
             }

sw/qa/extras/odfexport/odfexport.cxx
@@ -548,6 +548,37 @@ DECLARE_ODFEXPORT_TEST(testFdo58949, "fdo58949.docx")
     CPPUNIT_ASSERT_EQUAL(true, bool(xNameAccess->hasByName("Obj102")));
 }
 
+DECLARE_ODFEXPORT_TEST(testStylePageNumber, "ooo321_stylepagenumber.odt")
+{
+    uno::Reference<text::XTextContent> xTable1(getParagraphOrTable(1));
+// actually no break attribute is written in this case
+//    CPPUNIT_ASSERT_EQUAL(style::BreakType_PAGE_BEFORE, getProperty<style::BreakType>(xTable1, "BreakType"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Left Page"), getProperty<OUString>(xTable1, "PageDescName"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(1), getProperty<sal_Int16>(xTable1, "PageNumberOffset"));
+
+    uno::Reference<text::XTextContent> xPara1(getParagraphOrTable(2));
+    CPPUNIT_ASSERT_EQUAL(OUString("Right Page"), getProperty<OUString>(xPara1, "PageDescName"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(1), getProperty<sal_Int16>(xPara1, "PageNumberOffset"));
+
+    // i#114163 tdf#77111: OOo < 3.3 bug, it wrote "auto" as "0" for tables
+    uno::Reference<beans::XPropertySet> xTable0(getParagraphOrTable(3), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Left Page"), getProperty<OUString>(xTable0, "PageDescName"));
+    CPPUNIT_ASSERT_EQUAL(uno::Any(), xTable0->getPropertyValue("PageNumberOffset"));
+
+    uno::Reference<beans::XPropertySet> xPara0(getParagraphOrTable(4), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Right Page"), getProperty<OUString>(xPara0, "PageDescName"));
+    CPPUNIT_ASSERT_EQUAL(uno::Any(), xPara0->getPropertyValue("PageNumberOffset"));
+
+    uno::Reference<container::XNameAccess> xParaStyles(getStyles("ParagraphStyles"), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xStyle1(xParaStyles->getByName("stylewithbreak1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Right Page"), getProperty<OUString>(xStyle1, "PageDescName"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(1), getProperty<sal_Int16>(xStyle1, "PageNumberOffset"));
+
+    uno::Reference<beans::XPropertySet> xStyle0(xParaStyles->getByName("stylewithbreak0"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("First Page"), getProperty<OUString>(xStyle0, "PageDescName"));
+    CPPUNIT_ASSERT_EQUAL(uno::Any(), xStyle0->getPropertyValue("PageNumberOffset"));
+}
+
 DECLARE_ODFEXPORT_TEST(testCharacterBorder, "charborder.odt")
 {
     // Make sure paragraph and character attributes don't interfere