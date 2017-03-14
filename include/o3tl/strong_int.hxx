LemmingAvalanche/libreoffice-repaired

commit 162ecc22e15d5de86e7c57acd3a3f64ccfa0270a

Use o3tl::strong_int on RESOURCE_TYPE
Change-Id: I42690c07a611e031963ff41942f530af4a5c672a
Reviewed-on: https://gerrit.libreoffice.org/34220
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/tools/resid.hxx
@@ -24,10 +24,12 @@
 #include <rtl/ustring.hxx>
 #include <tools/solar.h>
 #include <tools/toolsdllapi.h>
+#include <o3tl/strong_int.hxx>
 
 struct RSHEADER_TYPE;
-typedef sal_uInt32 RESOURCE_TYPE;
-#define RSC_NOTYPE              0x100
+struct RESOURCE_TYPE_Tag {};
+typedef o3tl::strong_int<sal_uInt32, RESOURCE_TYPE_Tag> RESOURCE_TYPE;
+#define RSC_NOTYPE              RESOURCE_TYPE(0x100)
 #define RSC_DONTRELEASE         (sal_uInt32(1U << 31))
 
 class ResMgr;
@@ -45,11 +47,11 @@ class SAL_WARN_UNUSED ResId
         release the Resource context after loading this id.
     */
     RSHEADER_TYPE*          m_pResource;
-    mutable sal_uInt32      m_nResId;      // Resource Identifier
+    mutable RESOURCE_TYPE   m_nResId;      // Resource Identifier
     mutable RESOURCE_TYPE   m_nRT;         // type for loading (mutable to be set later)
     mutable ResMgr *        m_pResMgr;     // load from this ResMgr (mutable for setting on demand)
 
-    void ImplInit( sal_uInt32 nId, ResMgr& rMgr, RSHEADER_TYPE* pRes )
+    void ImplInit( RESOURCE_TYPE nId, ResMgr& rMgr, RSHEADER_TYPE* pRes )
     {
         m_pResource = pRes; m_nResId = nId; m_nRT = RSC_NOTYPE; m_pResMgr = &rMgr;
         OSL_ENSURE( m_pResMgr != nullptr, "ResId without ResMgr created" );
@@ -60,7 +62,7 @@ public:
     {
         ImplInit( 0, rMgr, pRc );
     }
-    ResId( sal_uInt32 nId, ResMgr& rMgr )
+    ResId( RESOURCE_TYPE nId, ResMgr& rMgr )
     {
         ImplInit( nId, rMgr, nullptr );
     }
@@ -87,9 +89,9 @@ public:
     ResMgr *        GetResMgr() const { return m_pResMgr; }
     void            ClearResMgr() const { m_pResMgr = nullptr; }
 
-    bool            IsAutoRelease()  const { return !(m_nResId & RSC_DONTRELEASE); }
+    bool            IsAutoRelease()  const { return !(sal_uInt32(m_nResId) & RSC_DONTRELEASE); }
 
-    sal_uInt32      GetId()          const { return m_nResId & ~RSC_DONTRELEASE; }
+    sal_uInt32      GetId()          const { return sal_uInt32(m_nResId) & ~RSC_DONTRELEASE; }
     RSHEADER_TYPE*  GetpResource()   const { return m_pResource; }
 
     TOOLS_DLLPUBLIC OUString toString() const;

rsc/inc/rsctop.hxx
@@ -25,6 +25,7 @@
 #include <rscclobj.hxx>
 #include <rsc/rscsfx.hxx>
 #include <o3tl/typed_flags_set.hxx>
+#include <tools/resid.hxx>
 
 enum class RSCVAR {
     NONE        = 0x0000,
@@ -47,7 +48,7 @@ class RscTop : public RefNode
     RscTop *        pRefClass;
 
 protected:
-                    RscTop( Atom nId, sal_uInt32 nTypIdent,
+                    RscTop( Atom nId, RESOURCE_TYPE nTypIdent,
                             RscTop * pSuperCl = nullptr );
 
 public:

rsc/source/parser/rscdb.cxx
@@ -44,7 +44,7 @@ RscTypCont::RscTypCont( RscError * pErrHdl,
     , aSearchPath( rSearchPath )
     , nUniqueId(256)
     , nFilePos( 0 )
-    , nPMId(RSC_VERSIONCONTROL +1) // at least one more
+    , nPMId(RSC_VERSIONCONTROL + RESOURCE_TYPE(1)) // at least one more
     , aBool( pHS->getID( "sal_Bool" ), RSC_NOTYPE )
     , aShort( pHS->getID( "short" ), RSC_NOTYPE )
     , aUShort( pHS->getID( "sal_uInt16" ), RSC_NOTYPE )
@@ -300,17 +300,17 @@ void RscEnumerateObj::WriteRcFile( RscWriteRc & rMem, FILE * fOut )
     /*
     struct RSHEADER_TYPE{
         sal_uInt32          nId;        // resource identifier
-        sal_uInt32          nRT;        // resource type
+        RESOURCE_TYPE       nRT;        // resource type
         sal_uInt32          nGlobOff;   // global offset
         sal_uInt32          nLocalOff;  // local offset
     } aHeader;
     */
 
     sal_uInt32 nId = rMem.GetLong( 0 );
-    sal_uInt32 nRT = rMem.GetLong( 4 );
+    RESOURCE_TYPE nRT(rMem.GetLong( 4 ));
 
     // table is filled with nId and nRT
-    pTypCont->PutTranslatorKey( (sal_uInt64(nRT) << 32) + sal_uInt64(nId) );
+    pTypCont->PutTranslatorKey( (sal_uInt64(sal_uInt32(nRT)) << 32) + sal_uInt64(nId) );
 
     if( nRT == RSC_VERSIONCONTROL )
     { // always comes last

tools/source/rc/resmgr.cxx
@@ -544,7 +544,7 @@ bool InternalResMgr::IsGlobalAvailable( RESOURCE_TYPE nRT, sal_uInt32 nId ) cons
 {
     // Anfang der Strings suchen
     ImpContent aValue;
-    aValue.nTypeAndId = ((sal_uInt64(nRT) << 32) | nId);
+    aValue.nTypeAndId = ((sal_uInt64(sal_uInt32(nRT)) << 32) | nId);
     ImpContent * pFind = ::std::lower_bound(pContent,
                                             pContent + nEntries,
                                             aValue,
@@ -558,11 +558,11 @@ void* InternalResMgr::LoadGlobalRes( RESOURCE_TYPE nRT, sal_uInt32 nId,
 {
 #ifdef DBG_UTIL
     if( pResUseDump )
-        pResUseDump->erase( (sal_uInt64(nRT) << 32) | nId );
+        pResUseDump->erase( (sal_uInt64(sal_uInt32(nRT)) << 32) | nId );
 #endif
     // search beginning of string
     ImpContent aValue;
-    aValue.nTypeAndId = ((sal_uInt64(nRT) << 32) | nId);
+    aValue.nTypeAndId = ((sal_uInt64(sal_uInt32(nRT)) << 32) | nId);
     ImpContent* pEnd = (pContent + nEntries);
     ImpContent* pFind = ::std::lower_bound( pContent,
                                             pEnd,
@@ -578,9 +578,9 @@ void* InternalResMgr::LoadGlobalRes( RESOURCE_TYPE nRT, sal_uInt32 nId,
                 // search beginning of string
                 ImpContent * pFirst = pFind;
                 ImpContent * pLast = pFirst;
-                while( pFirst > pContent && ((pFirst -1)->nTypeAndId >> 32) == RSC_STRING )
+                while( pFirst > pContent && RESOURCE_TYPE((pFirst -1)->nTypeAndId >> 32) == RSC_STRING )
                     --pFirst;
-                while( pLast < pEnd && (pLast->nTypeAndId >> 32) == RSC_STRING )
+                while( pLast < pEnd && RESOURCE_TYPE(pLast->nTypeAndId >> 32) == RSC_STRING )
                     ++pLast;
                 nOffCorrection = pFirst->nOffset;
                 sal_uInt32 nSize;

include/tools/rcid.h
@@ -30,15 +30,15 @@
 // Resource types
 // Minimum is 0x100 due to MS-Windows resource types
 // (RSC_NOTYPE=0x100) is defined in resid.hxx
-#define RSC_VERSIONCONTROL      (RSC_NOTYPE + 0x02) // Version control
+#define RSC_VERSIONCONTROL      (RSC_NOTYPE + RESOURCE_TYPE(0x02)) // Version control
 
-#define RSC_RESOURCE            (RSC_NOTYPE + 0x10)
-#define RSC_STRING              (RSC_NOTYPE + 0x11)
-#define RSC_BITMAP              (RSC_NOTYPE + 0x13)
-#define RSC_MENU                (RSC_NOTYPE + 0x1c)
-#define RSC_MENUITEM            (RSC_NOTYPE + 0x1d) // only used internally
+#define RSC_RESOURCE            (RSC_NOTYPE + RESOURCE_TYPE(0x10))
+#define RSC_STRING              (RSC_NOTYPE + RESOURCE_TYPE(0x11))
+#define RSC_BITMAP              (RSC_NOTYPE + RESOURCE_TYPE(0x13))
+#define RSC_MENU                (RSC_NOTYPE + RESOURCE_TYPE(0x1c))
+#define RSC_MENUITEM            (RSC_NOTYPE + RESOURCE_TYPE(0x1d)) // only used internally
 
-#define RSC_STRINGARRAY         (RSC_NOTYPE + 0x79)
+#define RSC_STRINGARRAY         (RSC_NOTYPE + RESOURCE_TYPE(0x79))
 
 // (RSC_NOTYPE + 0x200) - (RSC_NOTYPE + 0x300) reserved for Sfx
 

include/tools/resmgr.hxx
@@ -202,7 +202,7 @@ inline sal_uInt32 RSHEADER_TYPE::GetId()
 
 inline RESOURCE_TYPE RSHEADER_TYPE::GetRT()
 {
-    return (RESOURCE_TYPE)ResMgr::GetLong( &nRT );
+    return RESOURCE_TYPE(ResMgr::GetLong( &nRT ));
 }
 
 inline sal_uInt32 RSHEADER_TYPE::GetGlobOff()

rsc/inc/rscarray.hxx
@@ -59,7 +59,7 @@ protected:
     void            WriteSrcArray( const RSCINST & rInst, FILE * fOutput,
                                    RscTypCont * pTC, sal_uInt32 nTab, const char * );
 public:
-                    RscArray( Atom nId, sal_uInt32 nTypId,
+                    RscArray( Atom nId, RESOURCE_TYPE nTypId,
                               RscTop * pSuper, RscEnum * pTypeClass );
                     virtual ~RscArray() override;
     virtual RSCCLASS_TYPE   GetClassType() const override;
@@ -94,7 +94,7 @@ public:
 class RscLangArray : public RscArray
 {
 public:
-                    RscLangArray( Atom nId, sal_uInt32 nTypId,
+                    RscLangArray( Atom nId, RESOURCE_TYPE nTypId,
                                   RscTop * pSuper, RscEnum * pTypeClass );
     virtual RSCCLASS_TYPE   GetClassType() const override;
 };

rsc/inc/rscclass.hxx
@@ -54,7 +54,7 @@ protected:
     void                SetVarDflt( CLASS_DATA pData, sal_uInt32 nEle,
                                     bool bSet );
 public:
-                    RscClass( Atom nId, sal_uInt32 nTypId, RscTop * pSuperCl );
+                    RscClass( Atom nId, RESOURCE_TYPE nTypId, RscTop * pSuperCl );
                     virtual ~RscClass() override;
 
     virtual RSCCLASS_TYPE   GetClassType() const override;
@@ -95,7 +95,7 @@ public:
 class RscSysDepend : public RscClass
 {
 public:
-                    RscSysDepend( Atom nId, sal_uInt32 nTypId, RscTop * pSuper );
+                    RscSysDepend( Atom nId, RESOURCE_TYPE nTypId, RscTop * pSuper );
     ERRTYPE         WriteSysDependRc( const RSCINST &, RscWriteRc & aMem,
                                       RscTypCont * pTC, sal_uInt32, bool bExtra );
     ERRTYPE         WriteRc( const RSCINST &, RscWriteRc & aMem,
@@ -105,7 +105,7 @@ public:
 class RscTupel : public RscClass
 {
 public:
-    RscTupel( Atom nId, sal_uInt32 nTypId );
+    RscTupel( Atom nId, RESOURCE_TYPE nTypId );
     RSCINST         GetTupelVar( const RSCINST & rInst, sal_uInt32 nPos,
                                  const RSCINST & rInitInst ) override;
     void            WriteSrc( const RSCINST & rInst, FILE * fOutput,

commit 325b077947cca4ee8c7d4a413e65f9919f8ba44c

make o3tl::strong_int constructor explicit
Could have sworn I did this originally, somehow it got lost.
Also fix a thinko in tools::ResId I introduced in commit
162ecc22e15d5de86e7c57acd3a3f64ccfa0270a
"Use o3tl::strong_int on RESOURCE_TYPE"

Change-Id: Id3b39962255297010cd1feaaca6d822168311081
Reviewed-on: https://gerrit.libreoffice.org/35108
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/o3tl/strong_int.hxx
@@ -40,7 +40,7 @@ template <typename UNDERLYING_TYPE, typename PHANTOM_TYPE>
 struct strong_int
 {
 public:
-    strong_int(UNDERLYING_TYPE value) : m_value(value) {}
+    explicit strong_int(UNDERLYING_TYPE value) : m_value(value) {}
     strong_int() : m_value(0) {}
 
     explicit operator UNDERLYING_TYPE() const { return m_value; }

include/tools/resid.hxx
@@ -47,11 +47,11 @@ class SAL_WARN_UNUSED ResId
         release the Resource context after loading this id.
     */
     RSHEADER_TYPE*          m_pResource;
-    mutable RESOURCE_TYPE   m_nResId;      // Resource Identifier
+    mutable sal_uInt32      m_nResId;      // Resource Identifier
     mutable RESOURCE_TYPE   m_nRT;         // type for loading (mutable to be set later)
     mutable ResMgr *        m_pResMgr;     // load from this ResMgr (mutable for setting on demand)
 
-    void ImplInit( RESOURCE_TYPE nId, ResMgr& rMgr, RSHEADER_TYPE* pRes )
+    void ImplInit( sal_uInt32 nId, ResMgr& rMgr, RSHEADER_TYPE* pRes )
     {
         m_pResource = pRes; m_nResId = nId; m_nRT = RSC_NOTYPE; m_pResMgr = &rMgr;
         OSL_ENSURE( m_pResMgr != nullptr, "ResId without ResMgr created" );
@@ -62,7 +62,7 @@ public:
     {
         ImplInit( 0, rMgr, pRc );
     }
-    ResId( RESOURCE_TYPE nId, ResMgr& rMgr )
+    ResId( sal_uInt32 nId, ResMgr& rMgr )
     {
         ImplInit( nId, rMgr, nullptr );
     }
@@ -89,9 +89,9 @@ public:
     ResMgr *        GetResMgr() const { return m_pResMgr; }
     void            ClearResMgr() const { m_pResMgr = nullptr; }
 
-    bool            IsAutoRelease()  const { return !(sal_uInt32(m_nResId) & RSC_DONTRELEASE); }
+    bool            IsAutoRelease()  const { return !(m_nResId & RSC_DONTRELEASE); }
 
-    sal_uInt32      GetId()          const { return sal_uInt32(m_nResId) & ~RSC_DONTRELEASE; }
+    sal_uInt32      GetId()          const { return m_nResId & ~RSC_DONTRELEASE; }
     RSHEADER_TYPE*  GetpResource()   const { return m_pResource; }
 
     TOOLS_DLLPUBLIC OUString toString() const;

include/tools/unqidx.hxx
@@ -36,7 +36,7 @@ private:
     Index nUniqIndex;
 
 public:
-    UniqueIndexImpl( Index nStartIndex = 0 )
+    UniqueIndexImpl( Index nStartIndex = Index(0) )
         : maMap(), nUniqIndex(nStartIndex) {}
 
     Index Insert( void* p );
@@ -57,7 +57,7 @@ public:
     using UniqueIndexImpl::Index;
     using UniqueIndexImpl::IndexNotFound;
 
-    UniqueIndex<T>( Index _nStartIndex = 0 ) : UniqueIndexImpl(_nStartIndex) {}
+    UniqueIndex<T>( Index _nStartIndex = Index(0) ) : UniqueIndexImpl(_nStartIndex) {}
 
     Index Insert(T* p) { return UniqueIndexImpl::Insert(p); }
     T*    Get(Index idx) const { return static_cast<T*>( UniqueIndexImpl::Get(idx) ); }

rsc/inc/rscdb.hxx
@@ -41,18 +41,18 @@ enum class MapUnit;
 
 struct WriteRcContext
 {
-    FILE *              fOutput;
+    FILE *         fOutput;
     OString        aOutputRc;
     OString        aOutputSysList;
-    RscCmdLine*         pCmdLine;
+    RscCmdLine*    pCmdLine;
 };
 
 // table for system dependent resources
 struct RscSysEntry
 {
     sal_uInt32      nKey;
-    sal_uInt32      nRscTyp;
-    OString    aFileName;
+    RESOURCE_TYPE   nRscTyp;
+    OString         aFileName;
     sal_uInt32      nTyp;
     sal_uInt32      nRefId;
 };
@@ -129,7 +129,7 @@ public:
     void              SetSysSearchPath( const OString& rStr ) { aSysSearchPath = rStr; }
                       // deletes all resource objects of this file
     void              Delete( RscFileTab::Index lFileKey );
-    sal_uInt32        PutSysName( sal_uInt32 nRscTyp, char * pName );
+    sal_uInt32        PutSysName( RESOURCE_TYPE nRscTyp, char * pName );
     void              ClearSysNames();
     ERRTYPE           WriteRc( WriteRcContext& rContext );
     void              WriteSrc( FILE * fOutput, RscFileTab::Index nFileIndex );

rsc/inc/rsctop.hxx
@@ -44,7 +44,7 @@ class RscTop : public RefNode
 {
     RscTop *        pSuperClass;
     RSCINST         aDfltInst;
-    sal_uInt32          nTypId;
+    RESOURCE_TYPE   nTypId;
     RscTop *        pRefClass;
 
 protected:
@@ -59,7 +59,7 @@ public:
             RscTop* GetSuperClass() const
                     { return pSuperClass; }
                     // returns the type identifier
-            sal_uInt32  GetTypId() const
+            RESOURCE_TYPE GetTypId() const
                     { return nTypId; };
                     // returns the super class
             bool    InHierarchy( RscTop * pClass );

rsc/source/parser/rscdb.cxx
@@ -200,7 +200,7 @@ void RscTypCont::ClearSysNames()
     aSysLst.clear();
 }
 
-sal_uInt32 RscTypCont::PutSysName( sal_uInt32 nRscTyp, char * pFileName )
+sal_uInt32 RscTypCont::PutSysName( RESOURCE_TYPE nRscTyp, char * pFileName )
 {
     RscSysEntry *pFoundEntry = nullptr;
 

sw/source/uibase/uiview/view2.cxx
@@ -467,7 +467,7 @@ bool SwView::InsertGraphicDlg( SfxRequest& rReq )
             rSh.SetFrameFormat( pFormat );
         }
 
-        RESOURCE_TYPE nResId = 0;
+        sal_uInt32 nResId(0);
         switch( nError )
         {
             case GRFILTER_OPENERROR:

tools/source/rc/resmgr.cxx
@@ -671,7 +671,7 @@ void ResMgr::RscError_Impl( const sal_Char* pMessage, ResMgr* pResMgr,
     aStr.append('\n');
 
     aStr.append("    Class: ");
-    aStr.append(OUStringToOString(GetTypeRes_Impl(ResId(nRT, *pNewResMgr)),
+    aStr.append(OUStringToOString(GetTypeRes_Impl(ResId((sal_uInt32)nRT, *pNewResMgr)),
         RTL_TEXTENCODING_UTF8));
     aStr.append(", Id: ");
     aStr.append(static_cast<sal_Int32>(nId));
@@ -682,7 +682,7 @@ void ResMgr::RscError_Impl( const sal_Char* pMessage, ResMgr* pResMgr,
     {
         aStr.append(" [ Class: ");
         aStr.append(OUStringToOString(GetTypeRes_Impl(
-            ResId(rResStack[nDepth].pResource->GetRT(), *pNewResMgr)),
+            ResId((sal_uInt32)rResStack[nDepth].pResource->GetRT(), *pNewResMgr)),
             RTL_TEXTENCODING_UTF8));
         aStr.append(", Id: ");
         aStr.append(static_cast<sal_Int32>(

tools/source/ref/pstm.cxx
@@ -54,10 +54,10 @@ SvCreateInstancePersist SvClassManager::Get( sal_Int32 nClassId )
 SvPersistStream::SvPersistStream( SvClassManager & rMgr, SvStream * pStream )
     : rClassMgr( rMgr )
     , pStm( pStream )
-    , aPUIdx( 1 )
+    , aPUIdx( UniqueIndex<SvPersistBase>::Index(1) )
     , nStartIdx( 1 )
 {
-    DBG_ASSERT( nStartIdx != 0, "zero index not allowed" );
+    DBG_ASSERT( nStartIdx != Index(0), "zero index not allowed" );
     m_isWritable = true;
     if( pStm )
     {
@@ -117,7 +117,7 @@ SvPersistStream::Index SvPersistStream::GetIndex( SvPersistBase * pObj ) const
     PersistBaseMap::const_iterator it = aPTable.find( pObj );
     if( it == aPTable.end() )
     {
-        return 0;
+        return Index(0);
     }
     return it->second;
 }
@@ -357,18 +357,18 @@ static void ReadId
     nClassId = 0;
     rStm.ReadUChar( nHdr );
     if( nHdr & P_ID_0 )
-        nId = 0;
+        nId = SvPersistStream::Index(0);
     else
     {
         if( (nHdr & P_VER_MASK) == 0 )
         {
             if( (nHdr & P_DBGUTIL) || !(nHdr & P_OBJ) )
-                nId = SvPersistStream::ReadCompressed( rStm );
+                nId = SvPersistStream::Index(SvPersistStream::ReadCompressed( rStm ));
             else
-                nId = 0;
+                nId = SvPersistStream::Index(0);
         }
         else if( nHdr & P_ID )
-            nId = SvPersistStream::ReadCompressed( rStm );
+            nId = SvPersistStream::Index(SvPersistStream::ReadCompressed( rStm ));
 
         if( (nHdr & P_DBGUTIL) || (nHdr & P_OBJ) )
             nClassId = (sal_uInt16)SvPersistStream::ReadCompressed( rStm );
@@ -429,7 +429,7 @@ void SvPersistStream::ReadObj
 )
 {
     sal_uInt8   nHdr;
-    Index       nId = 0;
+    Index       nId(0);
     sal_uInt16  nClassId;
 
     rpObj = nullptr; // specification: 0 in case of error