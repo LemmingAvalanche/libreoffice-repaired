LemmingAvalanche/libreoffice-repaired

commit 336e19e1493476f311b7b31caf2fa5bebdd66f02

fdo#60513: SwCrsrShell::Push(): take position from table cursor
If there is a table cursor, store its point/mark position on the cursor
stack instead of pCurCrsr, which in this case is just one cell in a ring
that contains a SwShellCrsr for every selected cell.

SwCrsrShell::Pop() is surprisingly able to restore the prior state,
because it calls UpdateCrsr(), which detects that the pCurCrsr spans
multiple table cells and re-creates pTblCrsr, and then later on the
first GetCrsr() call SwTableCursor::MakeBoxSels() will restore the ring
of SwShellCrsrs from the pTblCrsr selection.

Change-Id: Ibe9a7f379ece61c7dab1a6f69abaebf42a4c85ec

sw/source/core/crsr/crsrsh.cxx
@@ -1859,13 +1859,17 @@ void SwCrsrShell::RefreshBlockCursor()
 /// create a copy of the cursor and save it in the stack
 void SwCrsrShell::Push()
 {
-    pCrsrStk = new SwShellCrsr( *this, *pCurCrsr->GetPoint(),
-                                    pCurCrsr->GetPtPos(), pCrsrStk );
+    // fdo#60513: if we have a table cursor, copy that; else copy current.
+    // This seems to work because UpdateCrsr() will fix this up on Pop(),
+    // then MakeBoxSels() will re-create the current pCurCrsr cell ring.
+    SwShellCrsr *const pCurrent((pTblCrsr) ? pTblCrsr : pCurCrsr);
+    pCrsrStk = new SwShellCrsr( *this, *pCurrent->GetPoint(),
+                                    pCurrent->GetPtPos(), pCrsrStk );
 
-    if( pCurCrsr->HasMark() )
+    if (pCurrent->HasMark())
     {
         pCrsrStk->SetMark();
-        *pCrsrStk->GetMark() = *pCurCrsr->GetMark();
+        *pCrsrStk->GetMark() = *pCurrent->GetMark();
     }
 }
 

commit ced35811e7593d6a745707a05866bc02cad82898

tdf#106929 sw: SwCursorShell::Pop() ensure cursor ring is recreated
... from table cursor.  The problem is that in this case, between
Push() and Pop() there is no call to ClearMark() or any of the other
functions that sets the SwTableCursor::m_bChanged flag, so MakeBoxSels()
is never called for the m_pCurrentCursor after Pop() and there is no
cursor ring selecting all table cells.

(regression from 336e19e1493476f311b7b31caf2fa5bebdd66f02)

Change-Id: I5bce18b71fed60a926f4ddf9938dfb7d1ddf4e92

sw/inc/swcrsr.hxx
@@ -300,6 +300,7 @@ public:
     }
 
     bool IsChgd() const { return m_bChanged; }
+    void SetChgd() { m_bChanged = true; }
 
     // Park table cursor at start node of boxes.
     void ParkCursor();

sw/source/core/crsr/crsrsh.cxx
@@ -2078,7 +2078,13 @@ bool SwCursorShell::Pop( bool bOldCursor )
         if( !m_pCurrentCursor->IsInProtectTable( true ) &&
             !m_pCurrentCursor->IsSelOvr( SwCursorSelOverFlags::Toggle |
                                  SwCursorSelOverFlags::ChangePos ) )
+        {
             UpdateCursor(); // update current cursor
+            if (m_pTableCursor)
+            { // tdf#106929 ensure m_pCurrentCursor ring is recreated from table
+                m_pTableCursor->SetChgd();
+            }
+        }
     }
     return true;
 }