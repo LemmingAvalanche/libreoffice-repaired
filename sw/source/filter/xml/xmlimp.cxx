LemmingAvalanche/libreoffice-repaired

commit 15ab72e1d32063d1dd8f1a97febd774cbfac8959

Rename style called "Default" to "Default Style" + First Cap Some Others too.
Change-Id: I71b74ceabfdaced541529143d15d020ac17d2e26

sw/source/ui/utlui/poolfmt.src
@@ -70,19 +70,19 @@ String STR_POOLPAGE_LANDSCAPE
 };
 String STR_POOLCHR_LINENUM
 {
-    Text [ en-US ] = "Line numbering" ;
+    Text [ en-US ] = "Line Numbering" ;
 };
 String STR_POOLCHR_IDX_MAIN_ENTRY
 {
-    Text [ en-US ] = "Main index entry";
+    Text [ en-US ] = "Main Index Entry";
 };
 String STR_POOLCHR_FOOTNOTE_ANCHOR
 {
-    Text [ en-US ] = "Footnote anchor";
+    Text [ en-US ] = "Footnote Anchor";
 };
 String STR_POOLCHR_ENDNOTE_ANCHOR
 {
-    Text [ en-US ] = "Endnote anchor";
+    Text [ en-US ] = "Endnote Anchor";
 };
 
  // Zeichenvorlagen fuer HTML
@@ -157,27 +157,27 @@ String STR_POOLFRM_LABEL
  // -----------------
 String STR_POOLCOLL_STANDARD
 {
-    Text [ en-US ] = "Default" ;
+    Text [ en-US ] = "Default Style" ;
 };
 String STR_POOLCOLL_TEXT
 {
-    Text [ en-US ] = "Text body" ;
+    Text [ en-US ] = "Text Body" ;
 };
 String STR_POOLCOLL_TEXT_IDENT
 {
-    Text [ en-US ] = "First line indent" ;
+    Text [ en-US ] = "First Line Indent" ;
 };
 String STR_POOLCOLL_TEXT_NEGIDENT
 {
-    Text [ en-US ] = "Hanging indent" ;
+    Text [ en-US ] = "Hanging Indent" ;
 };
 String STR_POOLCOLL_TEXT_MOVE
 {
-    Text [ en-US ] = "Text body indent" ;
+    Text [ en-US ] = "Text Body Indent" ;
 };
 String STR_POOLCOLL_GREETING
 {
-    Text [ en-US ] = "Complimentary close" ;
+    Text [ en-US ] = "Complimentary Close" ;
 };
 String STR_POOLCOLL_SIGNATURE
 {
@@ -409,11 +409,11 @@ String STR_POOLCOLL_HEADER
 };
 String STR_POOLCOLL_HEADERL
 {
-    Text [ en-US ] = "Header left" ;
+    Text [ en-US ] = "Header Left" ;
 };
 String STR_POOLCOLL_HEADERR
 {
-    Text [ en-US ] = "Header right" ;
+    Text [ en-US ] = "Header Right" ;
 };
 String STR_POOLCOLL_FOOTER
 {
@@ -421,11 +421,11 @@ String STR_POOLCOLL_FOOTER
 };
 String STR_POOLCOLL_FOOTERL
 {
-    Text [ en-US ] = "Footer left" ;
+    Text [ en-US ] = "Footer Left" ;
 };
 String STR_POOLCOLL_FOOTERR
 {
-    Text [ en-US ] = "Footer right" ;
+    Text [ en-US ] = "Footer Right" ;
 };
 String STR_POOLCOLL_TABLE
 {
@@ -437,7 +437,7 @@ String STR_POOLCOLL_TABLE_HDLN
 };
 String STR_POOLCOLL_FRAME
 {
-    Text [ en-US ] = "Frame contents" ;
+    Text [ en-US ] = "Frame Contents" ;
 };
 String STR_POOLCOLL_FOOTNOTE
 {
@@ -593,19 +593,19 @@ String STR_POOLCOLL_TOX_ILLUS1
 };
 String STR_POOLCOLL_TOX_OBJECTH
 {
-    Text [ en-US ] = "Object index heading";
+    Text [ en-US ] = "Object Index Heading";
 };
 String STR_POOLCOLL_TOX_OBJECT1
 {
-    Text [ en-US ] = "Object index 1";
+    Text [ en-US ] = "Object Index 1";
 };
 String STR_POOLCOLL_TOX_TABLESH
 {
-    Text [ en-US ] = "Table index heading";
+    Text [ en-US ] = "Table Index Heading";
 };
 String STR_POOLCOLL_TOX_TABLES1
 {
-    Text [ en-US ] = "Table index 1";
+    Text [ en-US ] = "Table Index 1";
 };
 String STR_POOLCOLL_TOX_AUTHORITIESH
 {

commit 125738fc84ff18fff35d4e47ce4645a3f0fcb3be

tdf#107211 sw: ODF import: add a horrible hack to import "Default"
... character style for hyperlinks and index-entry-templates.

The problem is that the "Default" character style doesn't actually
exist, it is just displayed in the UI so you can remove a character
style that is set.  So for the most part there is no need to store
"Default" in ODF files, except that for hyperlinks the default isn't
"Default" but "Internet Link"/"Visited Internet Link".

Hence it was not really a good idea to rename "Default" to "Default
Style", because when importing existing documents the
text:style-name="Default" is not found, and for new documents
text:style-name="Default_20_Style" is not found either because
there is no style:style element that sets up the mapping between the
encoded style-name and the unencoded display-style-name.

Add some hack in SwXMLBodyContext_Impl::SwXMLBodyContext_Impl(), when
all style:style elements have been read (so we don't override what's
in the document) to create the mapping for "Default", "Default_20_Style"
and whatever the translation of that is.

(regression from 15ab72e1d32063d1dd8f1a97febd774cbfac8959)

Change-Id: I8a80847571f194204c5df267cb48e826a82328b2

sw/source/filter/xml/xmlimp.cxx
@@ -50,6 +50,7 @@
 #include <poolfmt.hxx>
 #include <ndtxt.hxx>
 #include <editsh.hxx>
+#include <poolfmt.hrc>
 #include "xmlimp.hxx"
 #include "xmltexti.hxx"
 #include <xmloff/DocumentSettingsContext.hxx>
@@ -133,6 +134,29 @@ SwXMLBodyContext_Impl::SwXMLBodyContext_Impl( SwXMLImport& rImport,
                 const Reference< xml::sax::XAttributeList > & /*xAttrList*/ ) :
     SvXMLImportContext( rImport, nPrfx, rLName )
 {
+    // tdf#107211: if at this point we don't have a defined char style "Default"
+    // or "Default Style", add a mapping for it as it is not written
+    // into the file since it's not really a style but "no style"
+    // (hence referencing it actually makes no sense except for hyperlinks
+    // which default to something other than "Default")
+    OUString const sDefault(SW_RES(STR_POOLCOLL_STANDARD));
+    uno::Reference<container::XNameContainer> const& xStyles(
+            rImport.GetTextImport()->GetTextStyles());
+    if (!xStyles->hasByName("Default"))
+    {   // this old name was used before LO 4.0
+        rImport.AddStyleDisplayName(XML_STYLE_FAMILY_TEXT_TEXT, "Default", sDefault);
+    }
+    if (!xStyles->hasByName("Default_20_Style"))
+    {   // this new name contains a space which is converted to _20_ on export
+        rImport.AddStyleDisplayName(XML_STYLE_FAMILY_TEXT_TEXT, "Default_20_Style", sDefault);
+    }
+    bool isEncoded(false);
+    OUString const defaultEncoded(
+        rImport.GetMM100UnitConverter().encodeStyleName(sDefault, &isEncoded));
+    if (isEncoded && !xStyles->hasByName(defaultEncoded))
+    {   // new name may contain a space which is converted to _20_ on export
+        rImport.AddStyleDisplayName(XML_STYLE_FAMILY_TEXT_TEXT, defaultEncoded, sDefault);
+    }
 }
 
 SvXMLImportContext *SwXMLBodyContext_Impl::CreateChildContext(

sw/source/ui/utlui/poolfmt.src
@@ -155,6 +155,7 @@ String STR_POOLFRM_LABEL
 
  // Template names
 
+// tdf#107211 please don't change STANDARD, except back to "Default"
 String STR_POOLCOLL_STANDARD
 {
     Text [ en-US ] = "Default Style" ;