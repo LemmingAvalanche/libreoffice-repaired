LemmingAvalanche/libreoffice-repaired

commit 008aebdcd97dccab6dc41442ac8c14ff3a70a237

tdf#106933 vcl: delete D2DWriteTextOutRenderer before exit()
As it happens this DirectWrite stuff is using some thread pool
internally, and that must be shutdown before exit(), as Win32 will
terminate all other threads at that point, and then the thread pool
wants to talk to threads that don't exist any more.

https://blogs.msdn.microsoft.com/oldnewthing/20120427-00/?p=7763/

So convert this from a global variable that is deleted from DllMain()
to a member of SalData, so it is deleted from DeInitVCL().

Change-Id: I51408a07c78758cf0c193ab66b9214d0c9dbd9e3

vcl/inc/win/saldata.hxx
@@ -39,6 +39,7 @@ class WinSalPrinter;
 namespace vcl { class Font; }
 struct HDCCache;
 struct TempFontItem;
+class TextOutRenderer;
 
 #define MAX_STOCKPEN            4
 #define MAX_STOCKBRUSH          4
@@ -118,6 +119,9 @@ public:
 
     std::set< HMENU >       mhMenuSet;              // keeps track of menu handles created by VCL, used by IsKnownMenuHandle()
     std::map< UINT,sal_uInt16 > maVKMap;      // map some dynamic VK_* entries
+
+    // must be deleted before exit(), so delete it in DeInitSalData()
+    std::unique_ptr<TextOutRenderer> m_pTextOutRenderer;
 };
 
 inline void SetSalData( SalData* pData ) { ImplGetSVData()->mpSalData = pData; }

vcl/win/gdi/winlayout.cxx
@@ -245,20 +245,26 @@ void D2DWriteTextOutRenderer::CleanupModules()
 
 TextOutRenderer & TextOutRenderer::get(bool bUseDWrite)
 {
-    if (bUseDWrite)
-    {
-        static std::unique_ptr<TextOutRenderer> _impl(D2DWriteTextOutRenderer::InitModules()
-            ? static_cast<TextOutRenderer*>(new D2DWriteTextOutRenderer())
-            : static_cast<TextOutRenderer*>(new ExTextOutRenderer));
+    SalData *const pSalData = GetSalData();
 
-        return *_impl;
+    if (!pSalData)
+    {   // don't call this after DeInitVCL()
+        fprintf(stderr, "TextOutRenderer fatal error: no SalData");
+        abort();
     }
-    else
-    {
-        static std::unique_ptr<TextOutRenderer> _impl(new ExTextOutRenderer);
 
-        return *_impl;
+    if (!pSalData->m_pTextOutRenderer)
+    {
+        if (bUseDWrite && D2DWriteTextOutRenderer::InitModules())
+        {
+            pSalData->m_pTextOutRenderer.reset(new D2DWriteTextOutRenderer());
+        }
+        else
+        {
+            pSalData->m_pTextOutRenderer.reset(new ExTextOutRenderer());
+        }
     }
+    return *pSalData->m_pTextOutRenderer;
 }
 
 

vcl/win/app/salinst.cxx
@@ -39,6 +39,7 @@
 #include "win/salobj.h"
 #include "win/saltimer.h"
 #include "win/salbmp.h"
+#include "win/winlayout.hxx"
 
 #include "salimestatus.hxx"
 #include "salsys.hxx"

commit e2dd467d32526df9aa858c5f51c3fbd7aac7c154

tdf#107205 vcl: don't always use D2DWriteTextOutRenderer
... because it cannot rotate text.

(regression from commit 008aebdcd97dccab6dc41442ac8c14ff3a70a237)

Change-Id: If9bc6e98b8979c5eb02f1a5cfc12b4d19bdc481b

vcl/inc/win/saldata.hxx
@@ -123,7 +123,9 @@ public:
     std::map< UINT,sal_uInt16 > maVKMap;      // map some dynamic VK_* entries
 
     // must be deleted before exit(), so delete it in DeInitSalData()
-    std::unique_ptr<TextOutRenderer> m_pTextOutRenderer;
+    std::unique_ptr<TextOutRenderer> m_pD2DWriteTextOutRenderer;
+    // tdf#107205 need 2 instances because D2DWrite can't rotate text
+    std::unique_ptr<TextOutRenderer> m_pExTextOutRenderer;
     std::unique_ptr<GlobalGlyphCache> m_pGlobalGlyphCache;
     std::unique_ptr<TheTextureCache> m_pTextureCache;
 };

vcl/win/gdi/winlayout.cxx
@@ -258,18 +258,24 @@ TextOutRenderer & TextOutRenderer::get(bool bUseDWrite)
         abort();
     }
 
-    if (!pSalData->m_pTextOutRenderer)
+    if (bUseDWrite)
     {
-        if (bUseDWrite && D2DWriteTextOutRenderer::InitModules())
+        static bool const bSuccess(D2DWriteTextOutRenderer::InitModules());
+        if (bSuccess && !pSalData->m_pD2DWriteTextOutRenderer)
         {
-            pSalData->m_pTextOutRenderer.reset(new D2DWriteTextOutRenderer());
+            pSalData->m_pD2DWriteTextOutRenderer.reset(new D2DWriteTextOutRenderer());
         }
-        else
+        if (pSalData->m_pD2DWriteTextOutRenderer)
         {
-            pSalData->m_pTextOutRenderer.reset(new ExTextOutRenderer);
+            return *pSalData->m_pD2DWriteTextOutRenderer;
         }
+        // else: fall back to GDI
     }
-    return *pSalData->m_pTextOutRenderer;
+    if (!pSalData->m_pExTextOutRenderer)
+    {
+        pSalData->m_pExTextOutRenderer.reset(new ExTextOutRenderer);
+    }
+    return *pSalData->m_pExTextOutRenderer;
 }
 
 