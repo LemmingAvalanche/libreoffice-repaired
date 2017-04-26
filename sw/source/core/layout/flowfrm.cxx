LibreOffice/core

commit af41b7f91f22052d49654d41ae9916d6981db3f6

INTEGRATION: CWS swqbf64 (1.54.102); FILE MERGED 2006/05/17 11:06:48 od 1.54.102.1: #i65250# method <SwFlowFrm::MoveBwd(..)> 	 - apply <Move Backward> layout loop control

sw/source/core/layout/flowfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: flowfrm.cxx,v $
  *
- *  $Revision: 1.56 $
+ *  $Revision: 1.57 $
  *
- *  last change: $Author: vg $ $Date: 2006-05-24 13:54:43 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:11:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -2442,6 +2442,30 @@ BOOL SwFlowFrm::MoveBwd( BOOL &rbReformat )
         }
         // <--
     }
+
+    // --> OD 2006-05-10 #i65250#
+    // layout loop control for flowing content again and again moving
+    // backward under the same layout condition.
+    if ( pNewUpper && !IsFollow() &&
+         SwLayouter::MoveBwdSuppressed( *(pOldPage->GetFmt()->GetDoc()),
+                                        *this, *pNewUpper ) )
+    {
+        SwLayoutFrm* pNextNewUpper = pNewUpper->GetLeaf(
+                                    ( !rThis.IsSctFrm() && rThis.IsInSct() )
+                                    ? MAKEPAGE_NOSECTION
+                                    : MAKEPAGE_NONE,
+                                    TRUE );
+        if ( pNextNewUpper == rThis.GetUpper() ||
+             pNextNewUpper->GetType() != rThis.GetUpper()->GetType() )
+        {
+            pNewUpper = 0L;
+#if OSL_DEBUG_LEVEL > 1
+            ASSERT( false,
+                    "<SwFlowFrm::MoveBwd(..)> - layout loop control for layout action <Move Backward> applied!" );
+#endif
+        }
+    }
+    // <--
 
     if ( pNewUpper )
     {

LemmingAvalanche/libreoffice-repaired

commit 18741e671990112456685bbea15c173e6b4e4ded

tdf#107398 sw: do not leave empty footnote container in layout
... when applying loop control in SwFlowFrame::MoveBwd().

The SwFootnoteContFrame is newly created in MoveBwd(), line 2062:
    pNewUpper = m_rThis.GetLeaf( MAKEPAGE_FTN, false );

If it stays empty, that is not a valid layout so delete it again.

Since the idle/timer refactoring in VCL the invalid layout stays
until the document is closed; presumably before LO 5.0 the timer
based layout would reformat things again.

(regression from af41b7f91f22052d49654d41ae9916d6981db3f6)

Change-Id: I841f42b465f8123f9246f1fa70d1417ffdd57700

sw/source/core/layout/flowfrm.cxx
@@ -2402,6 +2402,12 @@ bool SwFlowFrame::MoveBwd( bool &rbReformat )
              ( pNextNewUpper == m_rThis.GetUpper() ||
                pNextNewUpper->GetType() != m_rThis.GetUpper()->GetType() ) )
         {
+            // tdf#107398 do not leave empty footnote container around
+            if (!pNewUpper->Lower() && pNewUpper->IsFootnoteContFrame())
+            {
+                pNewUpper->Cut();
+                SwFrame::DestroyFrame(pNewUpper);
+            }
             pNewUpper = nullptr;
             OSL_FAIL( "<SwFlowFrame::MoveBwd(..)> - layout loop control for layout action <Move Backward> applied!" );
         }