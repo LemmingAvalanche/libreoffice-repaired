LemmingAvalanche/libreoffice-repaired

commit 23de0dbe6118417f7846dfb39e59dc360ffb5281

crashtesting: "use" loop in moz325427-1.svg
Change-Id: I6ea2aaa5ca857d89342b9d18bd4cbedbeeea2b2f

filter/source/svg/svgreader.cxx
@@ -189,6 +189,33 @@ struct AnnotatingVisitor
         writeStyle(xElem,nTagId);
     }
 
+    bool IsAncestorId(const uno::Reference<xml::dom::XNode>& xParentNode, const OUString& rValue)
+    {
+        bool bSelfCycle = false;
+        if (xParentNode.is())
+        {
+            if (xParentNode->hasAttributes())
+            {
+                const uno::Reference<xml::dom::XNamedNodeMap> xParentAttributes = xParentNode->getAttributes();
+                const sal_Int32 nFooNumAttrs(xParentAttributes->getLength());
+                for (sal_Int32 i=0; i < nFooNumAttrs; ++i)
+                {
+                    const sal_Int32 nTokenId(getTokenId(xParentAttributes->item(i)->getNodeName()));
+                    if (XML_ID == nTokenId)
+                    {
+                        OUString sParentID = xParentAttributes->item(i)->getNodeValue();
+                        bSelfCycle = sParentID == rValue;
+                        break;
+                    }
+                }
+            }
+
+            if (!bSelfCycle)
+                bSelfCycle = IsAncestorId(xParentNode->getParentNode(), rValue);
+        }
+        return bSelfCycle;
+    }
+
     void operator()( const uno::Reference<xml::dom::XElement>&      xElem,
                      const uno::Reference<xml::dom::XNamedNodeMap>& xAttributes )
     {
@@ -294,27 +321,10 @@ struct AnnotatingVisitor
                     bool bFound = aFound != maElementIdMap.end();
                     if (bFound)
                     {
-                        bool bSelfCycle = false;
-
-                        uno::Reference<xml::dom::XNode> xParentNode(xElem->getParentNode());
-                        if (xParentNode.is() && xParentNode->hasAttributes())
-                        {
-                            const uno::Reference<xml::dom::XNamedNodeMap> xParentAttributes = xParentNode->getAttributes();
-                            const sal_Int32 nFooNumAttrs(xParentAttributes->getLength());
-                            for (sal_Int32 i=0; i < nFooNumAttrs; ++i)
-                            {
-                                const sal_Int32 nTokenId(getTokenId(xParentAttributes->item(i)->getNodeName()));
-                                if (XML_ID == nTokenId)
-                                {
-                                    OUString sParentID = xParentAttributes->item(i)->getNodeValue();
-                                    bSelfCycle = sParentID == sValue;
-                                    break;
-                                }
-                            }
-                        }
-
+                        const bool bSelfCycle = IsAncestorId(xElem->getParentNode(), sValue);
                         if (bSelfCycle)
                         {
+                            SAL_WARN("filter.svg", "\"use\" declaration with target of ancestor node, causing use cycle");
                             //drop this invalid self-referencing "use" node
                             maElementIdMap.erase(aFound);
                             bFound = false;

commit 5d8779b8fd8ab4c378e1f0bee9c52e210f0a93bf

tdf#107142 Fix crash when opening SVG with <use>
which is a regression from 23de0dbe6118417f7846dfb39e59dc360ffb5281.

Note that css::xml::dom::XNode::hasAttributes() is meaningful
only when it is an element.

Change-Id: Ib0a8b467012e9cb42a4b018967fb2740bcd10c5c
Reviewed-on: https://gerrit.libreoffice.org/37070
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

filter/source/svg/svgreader.cxx
@@ -192,7 +192,7 @@ struct AnnotatingVisitor
     bool IsAncestorId(const uno::Reference<xml::dom::XNode>& xParentNode, const OUString& rValue)
     {
         bool bSelfCycle = false;
-        if (xParentNode.is())
+        if (xParentNode.is() && xParentNode->getNodeType() == xml::dom::NodeType_ELEMENT_NODE)
         {
             if (xParentNode->hasAttributes())
             {