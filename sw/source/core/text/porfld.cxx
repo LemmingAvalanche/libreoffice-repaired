LemmingAvalanche/libreoffice-repaired

commit 2672f8ce13b9351401e8905e69c45ec379058a06

tdf#107204: Make sure we always do Unicode Bidi
Writer seems to have an “optimization” that only applies bidi algorithm
if the text direction is nor LTR and there is some characters that we
classify as complex script. This is a very simplistic optimization as
evidenced by the bug above. We certainly don't want to skip bidi the
text differently based on the default direction (whatever that means),
and not all RTL scripts are complex ones, even if they are we might be
behind Unicode in our script classification.

Just trust that bidi algorithm does the right thing and don’t do
premature optimization. If this turns out to be a real performance
issue, we will need to find a better optimization.

Change-Id: Ic0423b61d54de937bded95b369dab50e8fdca0df
Reviewed-on: https://gerrit.libreoffice.org/36704
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>

sw/source/core/text/porfld.cxx
@@ -228,8 +228,6 @@ void SwFieldPortion::CheckScript( const SwTextSizeInfo &rInf )
                              rSI.GetDefaultDir() :
                              rSI.DirType( IsFollow() ? rInf.GetIdx() - 1 : rInf.GetIdx() );
 
-        bool bPerformUBA = UBIDI_LTR != nFieldDir || i18n::ScriptType::COMPLEX == nScript;
-        if (bPerformUBA)
         {
             UErrorCode nError = U_ZERO_ERROR;
             UBiDi* pBidi = ubidi_openSized( aText.getLength(), 0, &nError );

sw/source/core/text/porlay.cxx
@@ -1184,16 +1184,6 @@ void SwScriptInfo::InitScriptInfo( const SwTextNode& rNode, bool bRTL )
     aDirectionChanges.clear();
 
     // Perform Unicode Bidi Algorithm for text direction information
-    bool bPerformUBA = UBIDI_LTR != nDefaultDir;
-    nCnt = 0;
-    while( !bPerformUBA && nCnt < CountScriptChg() )
-    {
-        if ( i18n::ScriptType::COMPLEX == GetScriptType( nCnt++ ) )
-            bPerformUBA = true;
-    }
-
-    // do not call the unicode bidi algorithm if not required
-    if ( bPerformUBA )
     {
         UpdateBidiInfo( rText );
 

commit 90bf0fc2d0267b7eb352cd609bdd28da72ef15f2

Don’t mark any field without strong LTR text CTL
Regression from 2672f8ce13b9351401e8905e69c45ec379058a06 which removed
the condition this block was implying, so putting it back here.

This should fix Jenkins_Callgrind tinderbox, we probably should have
some unit tests based on the file from [1] and [2], but I don’t know
how…

1. https://bz.apache.org/ooo/show_bug.cgi?id=89825
2. sw/qa/extras/ww8export/data/tdf91083.odt

Change-Id: I88ad2564908b3a269e95d1969366c8506801818b
Reviewed-on: https://gerrit.libreoffice.org/37050
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/core/text/porfld.cxx
@@ -241,7 +241,8 @@ void SwFieldPortion::CheckScript( const SwTextSizeInfo &rInf )
 
             // #i89825# change the script type also to CTL
             // if there is no strong LTR char in the LTR run (numbers)
-            if ( nCurrDir != UBIDI_RTL )
+            if (nCurrDir != UBIDI_RTL &&
+                (UBIDI_LTR != nFieldDir || i18n::ScriptType::COMPLEX == nScript))
             {
                 nCurrDir = UBIDI_RTL;
                 for( sal_Int32 nCharIdx = 0; nCharIdx < nEnd; ++nCharIdx )