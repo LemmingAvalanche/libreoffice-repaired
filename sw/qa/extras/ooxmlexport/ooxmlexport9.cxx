LemmingAvalanche/libreoffice-repaired

commit 011524d3d7f754d1e09969cabceba3f183527bb7

fdo#68787 DOCX import: handle when w:separator is missing for footnotes
There were two problems here:

1) OOXML has no way to explicitly disable the footnote separator, what
is does is that it omits the <w:separator/> element in that case. We
didn't parse that previously -- now we do, and if it's missing, the
separator is disabled.

2) The footnote stream isn't read by the importer, only when the main
stream references the footnote one, the relevant part of it is parsed.
At the moment we always parse the first (special, "separator") entry in
the footnote stream, that may be optimized later if it becomes a
bottleneck.

Change-Id: Ie588270a212fc90fc41095029a362cfd832b24f8

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -175,7 +175,9 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bIsNewDoc(bIsNewDoc),
         m_bInTableStyleRunProps(false),
         m_pSdtHelper(0),
-        m_nTableDepth(0)
+        m_nTableDepth(0),
+        m_bHasFtnSep(false),
+        m_bIgnoreNextPara(false)
 
 {
     appendTableManager( );

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -701,6 +701,12 @@ public:
      * PFInTable SPRM or not).
      */
     sal_Int32 m_nTableDepth;
+
+    /// If the document has a footnote separator.
+    bool m_bHasFtnSep;
+
+    /// If the next newline should be ignored, used by the special footnote separator paragraph.
+    bool m_bIgnoreNextPara;
 };
 } //namespace dmapper
 } //namespace writerfilter

writerfilter/source/dmapper/PropertyMap.cxx
@@ -864,6 +864,9 @@ void SectionPropertyMap::HandleMarginsHeaderFooter(DomainMapper_Impl& rDM_Impl)
 
     if (rDM_Impl.m_oBackgroundColor)
         operator[](PropertyDefinition(PROP_BACK_COLOR )) = uno::makeAny(*rDM_Impl.m_oBackgroundColor);
+    if (!rDM_Impl.m_bHasFtnSep)
+        // Set footnote line width to zero, document has no footnote separator.
+        operator[](PropertyDefinition(PROP_FOOTNOTE_LINE_RELATIVE_WIDTH)) = uno::makeAny(sal_Int32(0));
 
     /*** if headers/footers are available then the top/bottom margins of the
       header/footer are copied to the top/bottom margin of the page

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -131,6 +131,7 @@ public:
     void testTableStyleParprop();
     void testTablePagebreak();
     void testFdo68607();
+    void testFdo68787();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -228,6 +229,7 @@ void Test::run()
         {"table-style-parprop.docx", &Test::testTableStyleParprop},
         {"table-pagebreak.docx", &Test::testTablePagebreak},
         {"fdo68607.docx", &Test::testFdo68607},
+        {"fdo68787.docx", &Test::testFdo68787},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1538,6 +1540,13 @@ void Test::testFdo68607()
     // table in a frame. Exact layout may depend on fonts available, etc. --
     // but at least make sure that our table spans over multiple pages now.
     CPPUNIT_ASSERT(getPages() > 1);
+}
+
+void Test::testFdo68787()
+{
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
+    // This was 25, the 'lack of w:separator' <-> '0 line width' mapping was missing.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xPageStyle, "FootnoteLineRelativeWidth"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1465,6 +1465,11 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
             if (pSectionContext != NULL)
                 pSectionContext->SetPageNumber(nIntValue);
         break;
+        case NS_ooxml::LN_CT_FtnEdn_type:
+            // This is the "separator" footnote, ignore its linebreak.
+            if (static_cast<sal_uInt32>(nIntValue) == NS_ooxml::LN_Value_wordprocessingml_ST_FtnEdn_separator)
+                m_pImpl->m_bIgnoreNextPara = true;
+        break;
         default:
             {
 #if OSL_DEBUG_LEVEL > 0
@@ -3728,6 +3733,12 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
         m_pImpl->m_pSdtHelper->createDateControl(sText);
         return;
     }
+    else if (len == 1 && sText[0] == 0x03)
+    {
+        // This is the uFtnEdnSep, remember that the document has a separator.
+        m_pImpl->m_bHasFtnSep = true;
+        return;
+    }
 
     try
     {
@@ -3735,6 +3746,11 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
 
         if(len == 1 && (sText[0] == 0x0d || sText[0] == 0x07))
         {
+            if (m_pImpl->m_bIgnoreNextPara)
+            {
+                m_pImpl->m_bIgnoreNextPara = false;
+                return;
+            }
             PropertyMapPtr pContext = m_pImpl->GetTopContextOfType(CONTEXT_PARAGRAPH);
             if (pContext && m_pImpl->GetSettingsTable()->GetSplitPgBreakAndParaMark())
             {

writerfilter/source/dmapper/PropertyIds.cxx
@@ -331,6 +331,7 @@ const OUString& PropertyNameSupplier::GetName( PropertyIds eId ) const
             case PROP_MIRROR_INDENTS : sName = "MirrorIndents"; break;
             case PROP_SURROUND_TEXT_WRAP_SMALL: sName = "SurroundTextWrapSmall"; break;
             case PROP_PARA_SHADOW_FORMAT: sName = "ParaShadowFormat"; break;
+            case PROP_FOOTNOTE_LINE_RELATIVE_WIDTH: sName = "FootnoteLineRelativeWidth"; break;
         }
         ::std::pair<PropertyNameMap_t::iterator,bool> aInsertIt =
                 m_pImpl->aNameMap.insert( PropertyNameMap_t::value_type( eId, sName ));

writerfilter/source/dmapper/PropertyIds.hxx
@@ -302,6 +302,7 @@ enum PropertyIds
         ,PROP_MIRROR_INDENTS
         ,PROP_SURROUND_TEXT_WRAP_SMALL
         ,PROP_PARA_SHADOW_FORMAT
+        ,PROP_FOOTNOTE_LINE_RELATIVE_WIDTH
     };
 struct PropertyNameSupplier_Impl;
 class PropertyNameSupplier

commit 896631807896afe25c371ab4571ce57137948c09

tdf#107033 DOCX import: fix unexpected missing footnote separator
Regression from commit 011524d3d7f754d1e09969cabceba3f183527bb7
(fdo#68787 DOCX import: handle when w:separator is missing for
footnotes, 2013-09-04), the problem was footnote settings were modified
also in case there were no footnotes at all in the document.

Make the bug scenario and the original one working at the same time by
touching footnote settings only in case there is at least one footnote
in the current section.

Change-Id: I163d11769cbd97957662607fbedfba404181e002

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -236,6 +236,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_nTableDepth(0),
         m_nTableCellDepth(0),
         m_nLastTableCellParagraphDepth(0),
+        m_bHasFtn(false),
         m_bHasFtnSep(false),
         m_bIgnoreNextPara(false),
         m_bIgnoreNextTab(false),
@@ -5444,6 +5445,9 @@ void DomainMapper_Impl::substream(Id rName,
         propSize[i] = m_aPropertyStacks[i].size();
     }
 #endif
+    // Save "has footnote" state, which is specific to a section in the body
+    // text, so state from substreams is not relevant.
+    bool bHasFtn = m_bHasFtn;
 
     //finalize any waiting frames before starting alternate streams
     CheckUnregisteredFrameConversion();
@@ -5513,12 +5517,14 @@ void DomainMapper_Impl::substream(Id rName,
 
     getTableManager().endLevel();
     popTableManager();
+    m_bHasFtn = bHasFtn;
 
     switch(rName)
     {
     case NS_ooxml::LN_footnote:
     case NS_ooxml::LN_endnote:
         m_pTableHandler->setHadFootOrEndnote(true);
+        m_bHasFtn = true;
         break;
     }
 

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -841,7 +841,9 @@ public:
     /// Table cell depth of the last finished paragraph.
     sal_Int32 m_nLastTableCellParagraphDepth;
 
-    /// If the document has a footnote separator.
+    /// If the current section has footnotes.
+    bool m_bHasFtn;
+    /// If the current section has a footnote separator.
     bool m_bHasFtnSep;
 
     /// If the next newline should be ignored, used by the special footnote separator paragraph.

writerfilter/source/dmapper/PropertyMap.cxx
@@ -1001,9 +1001,18 @@ void SectionPropertyMap::HandleMarginsHeaderFooter( bool bFirstPage, DomainMappe
 
     if ( rDM_Impl.m_oBackgroundColor )
         Insert( PROP_BACK_COLOR, uno::makeAny( *rDM_Impl.m_oBackgroundColor ) );
-    if ( !rDM_Impl.m_bHasFtnSep )
-        // Set footnote line width to zero, document has no footnote separator.
-        Insert( PROP_FOOTNOTE_LINE_RELATIVE_WIDTH, uno::makeAny( sal_Int32( 0 ) ) );
+    if (rDM_Impl.m_bHasFtn)
+    {
+        // Check for missing footnote separator only in case there is at least
+        // one footnote.
+        if (!rDM_Impl.m_bHasFtnSep)
+        {
+            // Set footnote line width to zero, document has no footnote separator.
+            Insert(PROP_FOOTNOTE_LINE_RELATIVE_WIDTH, uno::makeAny(sal_Int32(0)));
+            rDM_Impl.m_bHasFtn = false;
+        }
+        rDM_Impl.m_bHasFtnSep = false;
+    }
 
     /*** if headers/footers are available then the top/bottom margins of the
     header/footer are copied to the top/bottom margin of the page

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -460,8 +460,16 @@ DECLARE_OOXMLEXPORT_TEST(testTdf106492, "tdf106492.docx")
 DECLARE_OOXMLEXPORT_TEST(testTdf107104, "tdf107104.docx")
 {
     CPPUNIT_ASSERT(getShape(1)->getSize().Width > 0);
-    // This failed: the second arrow was invisible because it had zero height.
+    // This failed: the second arrow was invisible because it had zero width.
     CPPUNIT_ASSERT(getShape(2)->getSize().Width > 0);
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf107033, "tdf107033.docx")
+{
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName("Standard"), uno::UNO_QUERY);
+    // This was 0: footnote separator was disabled even in case the document
+    // had no footnotes.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(25), getProperty<sal_Int32>(xPageStyle, "FootnoteLineRelativeWidth"));
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();