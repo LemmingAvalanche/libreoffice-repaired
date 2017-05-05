LemmingAvalanche/libreoffice-repaired

commit 7a2a6a561258596262595ccad713836366b05290

drop GetId() from SfxUndoAction
mostly so that the subclasses can use proper enum types for their own
IDs.
It turns out that nothing at the svl/sfx2 level needs an ID anyhow.

Change-Id: I3c020aeafb812fa30d896216d4e3bc1a82cbfeab
Reviewed-on: https://gerrit.libreoffice.org/35222
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/undo/docundo.cxx
@@ -387,8 +387,7 @@ UndoManager::GetLastUndoInfo(
     }
     if (o_pId)
     {
-        sal_uInt16 const nId(pAction->GetId());
-        *o_pId = static_cast<SwUndoId>(nId);
+        *o_pId = static_cast<const SwUndo*>(pAction)->GetId();
     }
 
     return true;
@@ -446,8 +445,7 @@ bool UndoManager::GetFirstRedoInfo(OUString *const o_pStr,
     }
     if (o_pId)
     {
-        sal_uInt16 const nId(pAction->GetId());
-        *o_pId = static_cast<SwUndoId>(nId);
+        *o_pId = static_cast<const SwUndo*>(pAction)->GetId();
     }
 
     return true;
@@ -655,7 +653,7 @@ bool UndoManager::Repeat(::sw::RepeatContext & rContext,
 
     OUString const comment(pRepeatAction->GetComment());
     OUString const rcomment(pRepeatAction->GetRepeatComment(rContext));
-    sal_uInt16 const nId(pRepeatAction->GetId());
+    SwUndoId const nId(static_cast<const SwUndo*>(pRepeatAction)->GetId());
     if (DoesUndo())
     {
         int nViewShellId = -1;

cui/source/dialogs/SpellDialog.cxx
@@ -119,7 +119,7 @@ public:
         {}
 
     virtual void            Undo() override;
-    virtual sal_uInt16          GetId() const override;
+    sal_uInt16              GetId() const;
 
     void                    SetEnableChangePB(){m_bEnableChangePB = true;}
     bool                    IsEnableChangePB(){return m_bEnableChangePB;}
@@ -1931,15 +1931,15 @@ void SentenceEditWindow_Impl::Undo()
     if(!GetUndoActionCount())
         return;
     bool bSaveUndoEdit = IsUndoEditMode();
-    sal_uInt16 nId;
+    SpellUndoAction_Impl* pUndoAction;
     //if the undo edit mode is active then undo all changes until the UNDO_EDIT_MODE action has been found
     do
     {
-        nId = rUndoMgr.GetUndoActionId();
+        pUndoAction = static_cast<SpellUndoAction_Impl*>(rUndoMgr.GetUndoAction());
         rUndoMgr.Undo();
-    }while(bSaveUndoEdit && SPELLUNDO_UNDO_EDIT_MODE != nId && GetUndoActionCount());
+    }while(bSaveUndoEdit && SPELLUNDO_UNDO_EDIT_MODE != pUndoAction->GetId() && GetUndoActionCount());
 
-    if(bSaveUndoEdit || SPELLUNDO_CHANGE_GROUP == nId)
+    if(bSaveUndoEdit || SPELLUNDO_CHANGE_GROUP == pUndoAction->GetId())
         GetSpellDialog()->UpdateBoxes_Impl();
 }
 

include/editeng/editund2.hxx
@@ -63,7 +63,7 @@ public:
     virtual OUString GetComment() const override;
     /// See SfxUndoAction::GetViewShellId().
     sal_Int32 GetViewShellId() const override;
-    virtual sal_uInt16  GetId() const override;
+    sal_uInt16  GetId() const;
 };
 
 #endif // INCLUDED_EDITENG_EDITUND2_HXX

include/svl/undo.hxx
@@ -59,7 +59,6 @@ public:
 
     virtual OUString    GetComment() const;
     virtual OUString    GetRepeatComment(SfxRepeatTarget&) const;
-    virtual sal_uInt16  GetId() const;
     /// ID of the view shell that created this undo action.
     virtual sal_Int32 GetViewShellId() const;
     /// Timestamp when this undo item was created.
@@ -151,7 +150,7 @@ public:
     /// See SfxUndoAction::GetViewShellId().
     sal_Int32 GetViewShellId() const override;
     virtual OUString        GetRepeatComment(SfxRepeatTarget&) const override;
-    virtual sal_uInt16      GetId() const override;
+    virtual sal_uInt16      GetId() const;
 
     void SetComment(const OUString& rComment);
     void dumpAsXml(struct _xmlTextWriter* pWriter) const override;
@@ -194,7 +193,6 @@ namespace svl
         virtual void            AddUndoAction( SfxUndoAction *pAction, bool bTryMerg=false ) = 0;
 
         virtual size_t          GetUndoActionCount( bool const i_currentLevel = CurrentLevel ) const = 0;
-        virtual sal_uInt16      GetUndoActionId() const = 0;
         virtual OUString        GetUndoActionComment( size_t nNo=0, bool const i_currentLevel = CurrentLevel ) const = 0;
         virtual SfxUndoAction*  GetUndoAction( size_t nNo=0 ) const = 0;
         /// Get info about all undo actions (comment, view shell id, etc.)
@@ -314,7 +312,6 @@ public:
     virtual void            SetMaxUndoActionCount( size_t nMaxUndoActionCount ) override;
     virtual void            AddUndoAction( SfxUndoAction *pAction, bool bTryMerg=false ) override;
     virtual size_t          GetUndoActionCount( bool const i_currentLevel = CurrentLevel ) const override;
-    virtual sal_uInt16      GetUndoActionId() const override;
     virtual OUString        GetUndoActionComment( size_t nNo=0, bool const i_currentLevel = CurrentLevel ) const override;
     virtual SfxUndoAction*  GetUndoAction( size_t nNo=0 ) const override;
     OUString                GetUndoActionsInfo() const override;

sc/source/ui/inc/undobase.hxx
@@ -183,7 +183,6 @@ public:
     virtual bool            Merge( SfxUndoAction *pNextAction ) override;
     virtual OUString        GetComment() const override;
     virtual OUString        GetRepeatComment(SfxRepeatTarget&) const override;
-    virtual sal_uInt16      GetId() const override;
     /// See SfxUndoAction::GetViewShellId().
     sal_Int32 GetViewShellId() const override;
 };

sc/source/ui/inc/undoblk.hxx
@@ -671,7 +671,6 @@ public:
     virtual bool    CanRepeat(SfxRepeatTarget& rTarget) const override;
 
     virtual OUString GetComment() const override;
-    virtual sal_uInt16  GetId() const override;
 
 private:
     ScMarkData      aMarkData;

sc/source/ui/inc/undodraw.hxx
@@ -45,7 +45,6 @@ public:
     virtual bool            Merge( SfxUndoAction *pNextAction ) override;
     virtual OUString        GetComment() const override;
     virtual OUString        GetRepeatComment(SfxRepeatTarget&) const override;
-    virtual sal_uInt16      GetId() const override;
     /// See SfxUndoAction::GetViewShellId().
     sal_Int32 GetViewShellId() const override;
 };

sc/source/ui/undo/undobase.cxx
@@ -642,14 +642,6 @@ OUString ScUndoWrapper::GetRepeatComment(SfxRepeatTarget& rTarget) const
     return OUString();
 }
 
-sal_uInt16 ScUndoWrapper::GetId() const
-{
-    if (pWrappedUndo)
-        return pWrappedUndo->GetId();
-    else
-        return 0;
-}
-
 bool ScUndoWrapper::Merge( SfxUndoAction* pNextAction )
 {
     if (pWrappedUndo)

commit d66a82278c4a7f98f807ec366dd7e96da0a73f24

sw: fix invalid downcast in sw::UndoManager::Repeat()
(regression from 7a2a6a561258596262595ccad713836366b05290)

Change-Id: Ie5343b9911feea329fc176f0abe31edc9e68a33e

sw/source/core/undo/docundo.cxx
@@ -661,7 +661,15 @@ bool UndoManager::Repeat(::sw::RepeatContext & rContext,
 
     OUString const comment(pRepeatAction->GetComment());
     OUString const rcomment(pRepeatAction->GetRepeatComment(rContext));
-    SwUndoId const nId(static_cast<const SwUndo*>(pRepeatAction)->GetId());
+    auto const*const pListAction(dynamic_cast<SfxListUndoAction *>(pRepeatAction));
+    auto const*const pSwAction(dynamic_cast<SwUndo *>(pRepeatAction));
+    if (!pListAction && !pSwAction)
+    {
+        return false;
+    }
+    SwUndoId const nId((pSwAction)
+            ? pSwAction->GetId()
+            : static_cast<SwUndoId>(pListAction->GetId()));
     if (DoesUndo())
     {
         ViewShellId nViewShellId(-1);