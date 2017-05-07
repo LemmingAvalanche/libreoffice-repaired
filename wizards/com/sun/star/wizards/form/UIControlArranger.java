LemmingAvalanche/libreoffice-repaired

commit 7850838d6106cb0bb91ca088671b9c01261f3e63

remove wizards bitmaps from .src files
Change-Id: I4e334ac88e1e060bc07af9b174b9b8fb1401fc80

wizards/com/sun/star/wizards/form/UIControlArranger.java
@@ -24,6 +24,7 @@ import com.sun.star.awt.XItemListener;
 import com.sun.star.awt.XRadioButton;
 import com.sun.star.lang.EventObject;
 import com.sun.star.wizards.common.Helper;
+import com.sun.star.wizards.common.IRenderer;
 import com.sun.star.wizards.common.PropertyNames;
 import com.sun.star.wizards.document.Control;
 import com.sun.star.wizards.ui.ButtonList;
@@ -107,15 +108,16 @@ public class UIControlArranger
         HelpTexts[3] = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 39); // In Blocks - Labels Above"
 
         DefaultListModel imageModel = new DefaultListModel();
-        for (int i = 0; i < HelpTexts.length; i++)
-        {
-            imageModel.addElement(Integer.valueOf(i));
-        }
-         String sMainArrangementHeader = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 41); // "Arrangement of the main form"
-         m_aArrangeList[0] = new ArrangeButtonList(0, imageModel, sMainArrangementHeader);
+        imageModel.addElement("private:graphicrepository/wizards/res/formarrangelistside_42.png");
+        imageModel.addElement("private:graphicrepository/wizards/res/formarrangelisttop_42.png");
+        imageModel.addElement("private:graphicrepository/wizards/res/formarrangetable_42.png");
+        imageModel.addElement("private:graphicrepository/wizards/res/formarrangefree_42.png");
 
-         String sSubArrangementHeader = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 42); // "Arrangement of the sub form"
-         m_aArrangeList[1] = new ArrangeButtonList(1, imageModel, sSubArrangementHeader);
+        String sMainArrangementHeader = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 41); // "Arrangement of the main form"
+        m_aArrangeList[0] = new ArrangeButtonList(0, imageModel, sMainArrangementHeader);
+
+        String sSubArrangementHeader = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 42); // "Arrangement of the sub form"
+        m_aArrangeList[1] = new ArrangeButtonList(1, imageModel, sSubArrangementHeader);
         enableAlignControlGroup(false);
     }
 
@@ -124,14 +126,8 @@ public class UIControlArranger
         return m_aArrangeList[_formindex].m_aButtonList.getSelected() + 1;
     }
 
-    private class LayoutRenderer implements ButtonList.IImageRenderer
+    private class LayoutRenderer implements IRenderer
     {
-        public Object getImageUrl(Object listitem)
-        {
-            int ResId = UIConsts.RID_IMG_FORM + (2 * ((Integer) listitem).intValue());
-            return Integer.valueOf(ResId);
-        }
-
         public String render(Object listItem)
         {
             if (listItem == null)

Repository.mk
@@ -1103,7 +1103,6 @@ $(eval $(call gb_Helper_register_resources,\
 	uui \
 	vcl \
 	writerperfect \
-	wzi \
 	$(if $(ENABLE_NSS),xmlsec) \
 	xsltdlg \
 ))

postprocess/CustomTarget_images.mk
@@ -28,6 +28,7 @@ $(packimages_DIR)/%.zip : \
 		$(packimages_DIR)/sorted.lst \
 		$(packimages_DIR)/commandimagelist.ilst \
 		$(packimages_DIR)/sourceimagelist.ilst \
+		$(SRCDIR)/wizards/source/imagelists/imagelists.ilst \
 		$(call gb_Helper_optional,HELP,$(helpimages_DIR)/helpimg.ilst) \
 		$(call gb_Helper_optional,HELP,$(helpimages_DIR)/screenshotimg.ilst) \
 		$(call gb_Helper_get_imagelists)

scp2/source/ooo/file_resource_ooo.scp
@@ -93,8 +93,6 @@ STD_RES_FILE( gid_File_Res_Vcl, vcl )
 
 STD_RES_FILE( gid_File_Res_Writerperfect, writerperfect )
 
-STD_RES_FILE( gid_File_Res_Wzi, wzi )
-
 STD_RES_FILE( gid_File_Res_Xsltdlg, xsltdlg )
 
 STD_RES_FILE( gid_File_Res_AVMedia, avmedia )

wizards/AllLangResTarget_wzi.mk
@@ -1,34 +1,0 @@
-# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
-#
-# This file is part of the LibreOffice project.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# This file incorporates work covered by the following license notice:
-#
-#   Licensed to the Apache Software Foundation (ASF) under one or more
-#   contributor license agreements. See the NOTICE file distributed
-#   with this work for additional information regarding copyright
-#   ownership. The ASF licenses this file to you under the Apache
-#   License, Version 2.0 (the "License"); you may not use this file
-#   except in compliance with the License. You may obtain a copy of
-#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
-#
-
-$(eval $(call gb_AllLangResTarget_AllLangResTarget,wzi))
-
-$(eval $(call gb_AllLangResTarget_set_reslocation,wzi,wizards))
-
-$(eval $(call gb_AllLangResTarget_add_srs,wzi,\
-	wzi/res \
-))
-
-$(eval $(call gb_SrsTarget_SrsTarget,wzi/res))
-
-$(eval $(call gb_SrsTarget_add_nonlocalizable_files,wzi/res,\
-	wizards/source/imagelists/imagelists.src \
-))
-
-# vim: set noet sw=4 ts=4:

wizards/Module_wizards.mk
@@ -44,7 +44,6 @@ $(eval $(call gb_Module_add_l10n_targets,wizards,\
 	AllLangResTarget_eur \
 	AllLangResTarget_imp \
 	AllLangResTarget_tpl \
-	AllLangResTarget_wzi \
 ))
 
 ifeq ($(ENABLE_JAVA),TRUE)

wizards/com/sun/star/wizards/report/ReportLayouter.java
@@ -191,11 +191,11 @@ public class ReportLayouter
                     });
             if (m_nLandscapeState == 1)
             {
-                CurUnoDialog.getPeerConfiguration().setImageUrl(aOrientationImage, 1002);
+                CurUnoDialog.getPeerConfiguration().setImageUrl(aOrientationImage, "private:graphicrepository/wizards/res/landscape_32.png");
             }
             else
             {
-                CurUnoDialog.getPeerConfiguration().setImageUrl(aOrientationImage, 1000);
+                CurUnoDialog.getPeerConfiguration().setImageUrl(aOrientationImage, "private:graphicrepository/wizards/res/portrait_32.png");
             }
         }
         catch (Exception exception)
@@ -267,7 +267,7 @@ public class ReportLayouter
                         catch (com.sun.star.lang.IllegalArgumentException e)
                         {
                         }
-                        Helper.setUnoPropertyValue(aOrientationImage, PropertyNames.PROPERTY_IMAGEURL, CurUnoDialog.getWizardImageUrl(1002));
+                        Helper.setUnoPropertyValue(aOrientationImage, PropertyNames.PROPERTY_IMAGEURL, "private:graphicrepository/wizards/res/landscape_32.png");
 
                         break;
 
@@ -282,7 +282,7 @@ public class ReportLayouter
                         }
 
                         CurUnoDialog.setControlProperty("imgOrientation", PropertyNames.PROPERTY_IMAGEURL, Desktop.getBitmapPath(m_xMSF) + "/portrait.gif");
-                        Helper.setUnoPropertyValue(aOrientationImage, PropertyNames.PROPERTY_IMAGEURL, CurUnoDialog.getWizardImageUrl(1000));
+                        Helper.setUnoPropertyValue(aOrientationImage, PropertyNames.PROPERTY_IMAGEURL, "private:graphicrepository/wizards/res/portrait_32.png");
                         break;
 
                     default:

wizards/com/sun/star/wizards/ui/ButtonList.java
@@ -62,7 +62,7 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
     private int m_nCurrentSelection = -1;
     private int pageStart = 0;
     public int helpURL = 0;
-    private IImageRenderer renderer;
+    private IRenderer renderer;
     private ListModel listModel;
     private IRenderer counterRenderer = new SimpleCounterRenderer();
     private static final int LINE_HEIGHT = 8;
@@ -195,7 +195,6 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
 
         m_aButtons = new XControl[rows * cols];
 
-
         m_aButtonHeight = Integer.valueOf(m_aButtonSize.Height);
         m_aButtonWidth = Integer.valueOf(m_aButtonSize.Width);
 
@@ -285,10 +284,7 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
         }
         for (int i = 0; i < m_aButtons.length; i++)
         {
-            Object oObj = getObjectFor(i);
-            if (oObj == null)
-                continue;
-            Object oResource = renderer.getImageUrl(oObj);
+            String oResource = getObjectFor(i);
             if (oResource == null)
                 continue;
             oUnoDialog.getPeerConfiguration().setImageUrl(m_aButtons[i].getModel(), oResource);
@@ -329,9 +325,9 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
 
     /**
      * @param i
-     * @return the Object in the list model corresponding to the given image index.
+     * @return the String in the list model corresponding to the given image url
      */
-    private Object getObjectFor(int i)
+    private String getObjectFor(int i)
     {
         int ii = getIndexFor(i);
         if (listModel.getSize() <= ii)
@@ -340,7 +336,7 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
         }
         else
         {
-            return listModel.getElementAt(ii);
+            return (String)listModel.getElementAt(ii);
         }
     }
 
@@ -442,7 +438,7 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
         pos = _size;
     }
 
-    public void setRenderer(IImageRenderer _renderer)
+    public void setRenderer(IRenderer _renderer)
     {
         this.renderer = _renderer;
     }
@@ -520,15 +516,6 @@ public class ButtonList implements XItemEventBroadcaster, XActionListener
     private Object getModel(Object control)
     {
         return UnoRuntime.queryInterface(XControl.class, control).getModel();
-    }
-
-    public interface IImageRenderer extends IRenderer
-    {
-
-        /**
-         * @return a resource ids for an image referenced in the resourcefile of the wizards project
-         */
-        Object getImageUrl(Object listItem);
     }
 
     private static class SimpleCounterRenderer implements IRenderer

commit 3f14fc51d91f0f83a992ee163150e182617986f7

tdf#107668: fix form wizard
Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=7850838d6106cb0bb91ca088671b9c01261f3e63
Use an hashmap to find the right helpText

Change-Id: Ie3389406525fb068fb24262a40b8e6af4c744100
Reviewed-on: https://gerrit.libreoffice.org/37334
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

wizards/com/sun/star/wizards/form/UIControlArranger.java
@@ -17,6 +17,9 @@
  */
 package com.sun.star.wizards.form;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import com.sun.star.awt.ItemEvent;
 import com.sun.star.awt.Size;
 import com.sun.star.awt.XControl;
@@ -44,11 +47,15 @@ public class UIControlArranger
     private final XRadioButton optAlignLeft;
     private final XRadioButton optAlignRight;
     private final XControl flnLabelPlacement;
-    private final String[] HelpTexts = new String[4];
+    private final Map helpTexts = new HashMap(4);
     private final ArrangeButtonList[] m_aArrangeList = new ArrangeButtonList[2];
     private final Integer IControlStep;
     private static final int SOBASEIMAGEYPOSITION = 66;
     private static final int SOIMAGELISTHEIGHT = 60;
+    private static final String ARRANGELISTSIDE = "private:graphicrepository/wizards/res/formarrangelistside_42.png";
+    private static final String ARRANGELISTTOP = "private:graphicrepository/wizards/res/formarrangelisttop_42.png";
+    private static final String ARRANGETABLE = "private:graphicrepository/wizards/res/formarrangetable_42.png";
+    private static final String ARRANGEFREE = "private:graphicrepository/wizards/res/formarrangefree_42.png";
 
     public UIControlArranger(FormWizard _CurUnoDialog, FormDocument _curFormDocument)
     {
@@ -102,16 +109,23 @@ public class UIControlArranger
                 });
 
 
-        HelpTexts[0] = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 36); // "Columnar - Labels Left"
-        HelpTexts[1] = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 37); // "Columnar - Labels of Top"
-        HelpTexts[2] = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 40); // "As Data Sheet"
-        HelpTexts[3] = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 39); // In Blocks - Labels Above"
-
         DefaultListModel imageModel = new DefaultListModel();
-        imageModel.addElement("private:graphicrepository/wizards/res/formarrangelistside_42.png");
-        imageModel.addElement("private:graphicrepository/wizards/res/formarrangelisttop_42.png");
-        imageModel.addElement("private:graphicrepository/wizards/res/formarrangetable_42.png");
-        imageModel.addElement("private:graphicrepository/wizards/res/formarrangefree_42.png");
+        imageModel.addElement(ARRANGELISTSIDE);
+        imageModel.addElement(ARRANGELISTTOP);
+        imageModel.addElement(ARRANGETABLE);
+        imageModel.addElement(ARRANGEFREE);
+
+        // "Columnar - Labels Left"
+        helpTexts.put(ARRANGELISTSIDE, CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 36));
+
+        // "Columnar - Labels of Top"
+        helpTexts.put(ARRANGELISTTOP, CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 37));
+
+        // "As Data Sheet"
+        helpTexts.put(ARRANGETABLE, CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 40));
+
+        // "In Blocks - Labels Above"
+        helpTexts.put(ARRANGEFREE, CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 39));
 
         String sMainArrangementHeader = CurUnoDialog.m_oResource.getResText(UIConsts.RID_FORM + 41); // "Arrangement of the main form"
         m_aArrangeList[0] = new ArrangeButtonList(0, imageModel, sMainArrangementHeader);
@@ -134,7 +148,7 @@ public class UIControlArranger
             {
                 return PropertyNames.EMPTY_STRING;
             }
-            return HelpTexts[((Integer) listItem).intValue()];
+            return (String) helpTexts.get(listItem);
 
         }
     }