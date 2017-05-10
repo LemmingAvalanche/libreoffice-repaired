LemmingAvalanche/libreoffice-repaired

commit 3f9e6b7068cc58a3d7d2070cac7425bdfaae5237

tdf#94173: Calc doesn't save your own created autoformat presets
Auformat list (maData) is defined as "MapType" which itself is defined like this:
boost::ptr_map<OUString, ScAutoFormatData>
so default sorting is ascii

2 consequences:
1) Default didn't appear first
2) When adding a new autoformat entry when it was new first one of the list
wasn't saved because of iterator was incremented first before looping
See https://bugs.documentfoundation.org/show_bug.cgi?id=94173#c5
There were some other weird behaviors too according to comments of the bugtracker

Regression from http://cgit.freedesktop.org/libreoffice/core/commit/?id=973aa731c8b35aec807e9a318b0faf1c57fd6fdd

Solution:
Add a Compare so Default entry is always first one so the first time incremented iterator is ok and
new entry (even if new first one in list) is saved

Thank you Markus for the idea! (I was lost in Compare syntax)

Change-Id: I9ba0cdc63c66b747db102bb661cd09fbfe5996ae
Reviewed-on: https://gerrit.libreoffice.org/18598
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sc/source/core/tool/autoform.cxx
@@ -896,6 +896,16 @@ ScAutoFormat::ScAutoFormat() :
     insert(pData);
 }
 
+bool DefaultFirstEntry::operator() (const OUString& left, const OUString& right) const
+{
+    OUString aStrStandard(ScGlobal::GetRscString(STR_STYLENAME_STANDARD));
+    if ( ScGlobal::GetpTransliteration()->isEqual( left, aStrStandard ) )
+        return true;
+    if ( ScGlobal::GetpTransliteration()->isEqual( right, aStrStandard ) )
+        return false;
+    return left < right;
+}
+
 ScAutoFormat::ScAutoFormat(const ScAutoFormat& r) :
     maData(r.maData),
     mbSaveLater(false) {}

sc/inc/autoform.hxx
@@ -307,9 +307,13 @@ public:
     bool                        Save( SvStream& rStream, sal_uInt16 fileVersion );
 };
 
+struct DefaultFirstEntry {
+    bool operator() (const OUString& left, const OUString& right) const;
+};
+
 class SC_DLLPUBLIC ScAutoFormat
 {
-    typedef boost::ptr_map<OUString, ScAutoFormatData> MapType;
+    typedef boost::ptr_map<OUString, ScAutoFormatData, DefaultFirstEntry> MapType;
     MapType maData;
     bool mbSaveLater;
     ScAfVersions m_aVersions;

commit 17395cdcb7b31b63a3d12265ec601ab442b6c92e

tdf#107572: retrieve Default autoformat
by fixing DefaultFirstEntry comparison operator.
From http://www.cplusplus.com/reference/map/map/key_comp/:
"...
Two keys are considered equivalent if key_comp returns false reflexively
(i.e., no matter the order in which the keys are passed as arguments).
..."

Certainly regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=3f9e6b7068cc58a3d7d2070cac7425bdfaae5237
author	Julien Nabet <serval2412@yahoo.fr>	2015-09-15 19:41:48 (GMT)
committer	Julien Nabet <serval2412@yahoo.fr>	2015-09-30 12:11:35 (GMT)
commit	3f9e6b7068cc58a3d7d2070cac7425bdfaae5237 (patch)
tree	13ad29a110565f24e7010a68d902cadf1c441b62
parent	23367d189047a9b2d97b217f2269e2888f725926 (diff)
tdf#94173: Calc doesn't save your own created autoformat presets

This new patch has also been tested for tdf#94173

Change-Id: I60de5fffe39be9b3615c32a317611812b42bafc7
Reviewed-on: https://gerrit.libreoffice.org/37362
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/core/tool/autoform.cxx
@@ -911,6 +911,8 @@ ScAutoFormat::ScAutoFormat() :
 bool DefaultFirstEntry::operator() (const OUString& left, const OUString& right) const
 {
     OUString aStrStandard(ScGlobal::GetRscString(STR_STYLENAME_STANDARD));
+    if (ScGlobal::GetpTransliteration()->isEqual( left, right ) )
+        return false;
     if ( ScGlobal::GetpTransliteration()->isEqual( left, aStrStandard ) )
         return true;
     if ( ScGlobal::GetpTransliteration()->isEqual( right, aStrStandard ) )