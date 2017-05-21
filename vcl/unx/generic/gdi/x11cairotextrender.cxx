LemmingAvalanche/libreoffice-repaired

commit 813fe9a98b9093feeb3025dfed87fc94dcb8199e

loplugin:unusedfields in vcl/
and remove the unused SALEVENT_MOUSEACTIVATE stuff

Change-Id: Ieb85872eca68621c6a7be47ff5dbea12f7690507
Reviewed-on: https://gerrit.libreoffice.org/20140
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

vcl/unx/generic/gdi/x11cairotextrender.cxx
@@ -36,7 +36,6 @@ struct BOX
 };
 struct _XRegion
 {
-    long size;
     long numRects;
     BOX *rects;
     BOX extents;

compilerplugins/clang/unusedfields.py
@@ -60,7 +60,11 @@ for d in definitionSet:
         continue
     # this is all representations of on-disk data structures
     if (srcLoc.startswith("sc/source/filter/inc/scflt.hxx")
-        or srcLoc.startswith("sw/source/filter/ww8/")):
+        or srcLoc.startswith("sw/source/filter/ww8/")
+        or srcLoc.startswith("vcl/source/filter/sgvmain.hxx")
+        or srcLoc.startswith("vcl/source/filter/sgfbram.hxx")
+        or srcLoc.startswith("vcl/inc/unx/XIM.h")
+        or srcLoc.startswith("vcl/inc/unx/gtk/gloactiongroup.h")):
         continue
 
     tmp1set.add((clazz, srcLoc))

vcl/generic/fontmanager/parseAFM.hxx
@@ -203,9 +203,8 @@ typedef struct _t_ligature
 typedef struct
 {
     int code,       /* key: C */
-        wx,     /* key: WX */
-        w0x,        /* key: W0X */
-        wy;     /* together wx and wy are associated with key: W */
+        wx,         /* key: WX */
+        wy;         /* together wx and wy are associated with key: W */
     char *name;     /* key: N */
     BBox charBBox;  /* key: B */
     Ligature *ligs; /* key: L (linked list; not a fixed number of Ls */

vcl/generic/glyphs/gcach_ftyp.cxx
@@ -444,7 +444,6 @@ ServerFont::ServerFont( const FontSelectPattern& rFSD, FtFontInfo* pFI )
     mpNextGCFont( nullptr ),
     mnCos( 0x10000),
     mnSin( 0 ),
-    mbCollectedZW( false ),
     mnPrioEmbedded(nDefaultPrioEmbedded),
     mnPrioAntiAlias(nDefaultPrioAntiAlias),
     mnPrioAutoHint(nDefaultPrioAutoHint),

vcl/generic/glyphs/scrptrun.h
@@ -47,13 +47,6 @@
 
 namespace vcl {
 
-struct ScriptRecord
-{
-    UChar32 startChar;
-    UChar32 endChar;
-    UScriptCode scriptCode;
-};
-
 struct ParenStackEntry
 {
     int32_t pairIndex;

vcl/inc/brdwin.hxx
@@ -198,7 +198,6 @@ struct ImplBorderFrameData
     long                     mnNoTitleTop;
     long                     mnBorderSize;
     long                     mnTitleHeight;
-    long                     mnTitleOff;
     sal_uInt16               mnHitTest;
     DrawButtonFlags          mnPinState;
     DrawButtonFlags          mnCloseState;

vcl/inc/fontmanager.hxx
@@ -246,7 +246,6 @@ class VCL_PLUGIN_PUBLIC PrintFontManager
     fontID                                      m_nNextFontID;
     std::unordered_map< fontID, PrintFont* >       m_aFonts;
     std::unordered_map< int, FontFamily >        m_aFamilyTypes;
-    std::list< OUString >              m_aPrinterDrivers;
     std::list< OString >               m_aFontDirectories;
     std::list< int >                            m_aPrivateFontDirectories;
     utl::MultiAtomProvider*                   m_pAtoms;
@@ -319,7 +318,6 @@ class VCL_PLUGIN_PUBLIC PrintFontManager
     static bool addFontconfigDir(const OString& rDirectory);
 
     std::set<OString> m_aPreviousLangSupportRequests;
-    std::vector<OString> m_aCurrentRequests;
     Timer m_aFontInstallerTimer;
 
 #if defined(ENABLE_DBUS) && defined(ENABLE_PACKAGEKIT)

vcl/inc/generic/glyphcache.hxx
@@ -125,18 +125,6 @@ private:
     Size                    maSize;
 };
 
-// the glyph specific data needed by a GlyphCachePeer is usually trivial,
-// not attaching it to the corresponding GlyphData would be overkill;
-// this is currently only used by the headless (aka svp) plugin, where meInfo is
-// basebmp::Format and mpData is SvpGcpHelper*
-struct ExtGlyphData
-{
-    basebmp::Format  meInfo;
-    SvpGcpHelper*    mpData;
-
-    ExtGlyphData() : meInfo(basebmp::Format::NONE), mpData(nullptr) {}
-};
-
 class GlyphData
 {
 public:
@@ -233,8 +221,6 @@ private:
     // 16.16 fixed point values used for a rotated font
     long                    mnCos;
     long                    mnSin;
-
-    bool                    mbCollectedZW;
 
     int                     mnWidth;
     int                     mnPrioEmbedded;

commit 9f4f1dc7e83a560f4c5cb59819a47d57aad74322

XRegion has to match the "real" XRegion structure
i.e. same as...

commit 48a9c12daafa394b8f9cc94e86ddd0278867ec6a
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Wed Nov 25 10:04:37 2015 +0000

    XRegion has to match the "real" XRegion structure, even if we don't use

    some of the fields

    regression from

    commit 813fe9a98b9093feeb3025dfed87fc94dcb8199e
    Date:   Tue Nov 24 08:50:39 2015 +0200

        loplugin:unusedfields in vcl/

Change-Id: I8314eae6d507b6a7fb847d593458e15e666230c8

vcl/unx/generic/gdi/x11cairotextrender.cxx
@@ -29,10 +29,22 @@ struct BOX
 {
     short x1, x2, y1, y2;
 };
+
+/*
+ * Xutil.h contains the declaration:
+ * typedef struct _XRegion *Region;
+ *
+ * this struct has to match the real _Xregion
+ * definition, don't change this is remove apparently
+ * unused fields
+ */
+
 struct _XRegion
 {
+    long size;
     long numRects;
     BOX *rects;
+    BOX extents;
 };
 
 X11CairoTextRender::X11CairoTextRender(X11SalGraphics& rParent)