LemmingAvalanche/libreoffice-repaired

commit eabcd10e4467d52ec03d0369e3f88334e51a698d

remove unnecessary use of OString::getStr
Change-Id: I0490efedf459190521f4339854b3394d57765fdb
Reviewed-on: https://gerrit.libreoffice.org/38058
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

i18nutil/source/utility/paper.cxx
@@ -271,8 +271,7 @@ PaperInfo PaperInfo::getSystemDefaultPaper()
 
                 if (ePaper == PAPER_USER)
                 {
-                    bHalve = !rtl_str_shortenedCompareIgnoreAsciiCase_WithLength(
-                        aPaper.getStr(), aPaper.getLength(),  "half", 4, 4);
+                    bHalve = !aPaper.startsWith("half");
                     if (bHalve)
                         aPaper = aPaper.copy(4);
                     ePaper = PaperInfo::fromPSName(aPaper);

commit 1fa0909f0d4fd475e177113226150975d7a9ab0b

i18nutil: fix invalid string copy that prevents startup
(regression from eabcd10e4467d52ec03d0369e3f88334e51a698d)

Change-Id: Ia3ba5ba853648a4b180d6cc2318a4780fc87bf4d

i18nutil/source/utility/paper.cxx
@@ -271,9 +271,10 @@ PaperInfo PaperInfo::getSystemDefaultPaper()
 
                 if (ePaper == PAPER_USER)
                 {
-                    bHalve = !aPaper.startsWith("half");
-                    if (bHalve)
+                    if (aPaper.startsWith("half"))
+                    {
                         aPaper = aPaper.copy(4);
+                    }
                     ePaper = PaperInfo::fromPSName(aPaper);
                 }
 