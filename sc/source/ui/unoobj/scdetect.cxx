LemmingAvalanche/libreoffice-repaired

commit d8a731d286c18f7987f9154c5f7fc1bed7361ff6

tdf#84834 sc: stricter type detection for dBASE files
The detection is rather sloppy, the bugdoc gets erroneously detected as
dBASE because it starts with '0', has a not-too-large header size at offset 4,
and a '\r' at a 32-byte alignment towards the start of the the not-too-large
header.

Add a plausibility check for the number of records in the file, which
helps for this bugdoc.

Change-Id: I466dfae18aa32fd62b79b9f524f22bea719721be

sc/source/ui/unoobj/scdetect.cxx
@@ -219,12 +219,35 @@ static bool lcl_MayBeDBase( SvStream& rStream )
     if ( nSize < nEmptyDbf )
         return false;
 
+    // count of records at 4
+    rStream.Seek(4);
+    sal_uInt32 nRecords(0);
+    rStream.ReadUInt32(nRecords);
+
     // length of header starts at 8
     rStream.Seek(8);
     sal_uInt16 nHeaderLen;
     rStream.ReadUInt16( nHeaderLen );
 
+    // size of record at 10
+    sal_uInt16 nRecordSize(0);
+    rStream.ReadUInt16(nRecordSize);
+
     if ( nHeaderLen < nEmptyDbf || nSize < nHeaderLen )
+        return false;
+
+    // see DTable.cxx ODbaseTable::readHeader()
+    if (0 == nRecordSize)
+        return false;
+
+    // see DTable.cxx ODbaseTable::construct() line 546
+    if (0 == nRecords)
+    {
+        nRecords = (nSize - nHeaderLen) / nRecordSize;
+    }
+
+    // tdf#84834 sanity check of size
+    if (0 == nRecords || nSize < nHeaderLen + nRecords * sal_uInt64(nRecordSize))
         return false;
 
     // Last byte of header must be 0x0d, this is how it's specified.

commit 1798ed0baf643c4aa360508f5dbe05724054b87f

tdf#106423: dbase file can have no records
Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=d8a731d286c18f7987f9154c5f7fc1bed7361ff6
tdf#84834 sc: stricter type detection for dBASE files

Log test file from tdf#84834 still opens in Writer as expected

Change-Id: Ibf5a0f586e3a132455d2363e61e5cc7ea249002d
Reviewed-on: https://gerrit.libreoffice.org/38660
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Stahl <mstahl@redhat.com>

sc/source/ui/unoobj/scdetect.cxx
@@ -241,7 +241,8 @@ static bool lcl_MayBeDBase( SvStream& rStream )
     }
 
     // tdf#84834 sanity check of size
-    if (0 == nRecords || nSize < nHeaderLen + nRecords * sal_uInt64(nRecordSize))
+    // tdf#106423: a dbf file can have 0 record, so no need to check nRecords
+    if (nSize < nHeaderLen + nRecords * sal_uInt64(nRecordSize))
         return false;
 
     // Last byte of header must be 0x0d, this is how it's specified.