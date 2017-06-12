LemmingAvalanche/libreoffice-repaired

commit 3af5d2c7113df64d4c38c4ea6b398f7e1f172ca6

clang-tidy readability-delete-null-pointer
which in turn triggered some loplugin:useuniqueptr

Change-Id: I0c38561fc9b68dac44e8cf58c8aa1f582196cc64
Reviewed-on: https://gerrit.libreoffice.org/38281
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/viewobjectcontactofgraphic.cxx
@@ -112,7 +112,7 @@ namespace sdr
                             // Trigger asynchronious SwapIn.
                             sdr::event::TimerEventHandler& rEventHandler = rObjectContact.GetEventHandler();
 
-                            mpAsynchLoadEvent = new sdr::event::AsynchGraphicLoadingEvent(rEventHandler, *this);
+                            mpAsynchLoadEvent.reset( new sdr::event::AsynchGraphicLoadingEvent(rEventHandler, *this) );
                         }
                     }
                     else
@@ -142,7 +142,7 @@ namespace sdr
                 {
                     // just delete it, this will remove it from the EventHandler and
                     // will trigger forgetAsynchGraphicLoadingEvent from the destructor
-                    delete mpAsynchLoadEvent;
+                    mpAsynchLoadEvent.reset();
 
                     // Invalidate paint areas.
                     // [1] If a calc document with graphics is loaded then OnLoad will
@@ -222,7 +222,7 @@ namespace sdr
 
             if(mpAsynchLoadEvent)
             {
-                OSL_ENSURE(!pEvent || mpAsynchLoadEvent == pEvent,
+                OSL_ENSURE(!pEvent || mpAsynchLoadEvent.get() == pEvent,
                     "ViewObjectContactOfGraphic::forgetAsynchGraphicLoadingEvent: Forced to forget another event then I have scheduled (?)");
 
                 // forget event
@@ -302,13 +302,6 @@ namespace sdr
 
         ViewObjectContactOfGraphic::~ViewObjectContactOfGraphic()
         {
-            // evtl. delete the asynch loading event
-            if(mpAsynchLoadEvent)
-            {
-                // just delete it, this will remove it from the EventHandler and
-                // will trigger forgetAsynchGraphicLoadingEvent from the destructor
-                delete mpAsynchLoadEvent;
-            }
         }
     } // end of namespace contact
 } // end of namespace sdr

commit 6630d39db7420b90ee25c8fb6dbd57775400e6ff

svx: fix the std::unique_ptr that deletes itself twice
It's good that the obviously error prone C++98 bare pointer was replaced
with a modern C++11 std::unique_ptr - everyone knows that with such modern
idioms memory management errors will be a thing of the past!

(regression from commit 3af5d2c7113df64d4c38c4ea6b398f7e1f172ca6)

    svxcorelo.dll!sdr::event::AsynchGraphicLoadingEvent::~AsynchGraphicLoadingEvent() Line 57       C++
    svxcorelo.dll!sdr::event::AsynchGraphicLoadingEvent::`scalar deleting destructor'(unsigned int)     C++
    svxcorelo.dll!std::default_delete<sdr::event::AsynchGraphicLoadingEvent>::operator()(sdr::event::AsynchGraphicLoadingEvent * _Ptr) Line 1608        C++
    svxcorelo.dll!std::unique_ptr<sdr::event::AsynchGraphicLoadingEvent,std::default_delete<sdr::event::AsynchGraphicLoadingEvent> >::reset(sdr::event::AsynchGraphicLoadingEvent * _Ptr) Line 1855     C++
    svxcorelo.dll!std::unique_ptr<sdr::event::AsynchGraphicLoadingEvent,std::default_delete<sdr::event::AsynchGraphicLoadingEvent> >::operator=(void * __formal) Line 1728      C++
    svxcorelo.dll!sdr::contact::ViewObjectContactOfGraphic::forgetAsynchGraphicLoadingEvent(sdr::event::AsynchGraphicLoadingEvent * pEvent) Line 231    C++
    svxcorelo.dll!sdr::event::AsynchGraphicLoadingEvent::~AsynchGraphicLoadingEvent() Line 59   C++
    svxcorelo.dll!sdr::event::AsynchGraphicLoadingEvent::`scalar deleting destructor'(unsigned int)     C++
    svxcorelo.dll!std::default_delete<sdr::event::AsynchGraphicLoadingEvent>::operator()(sdr::event::AsynchGraphicLoadingEvent * _Ptr) Line 1608        C++
    svxcorelo.dll!std::unique_ptr<sdr::event::AsynchGraphicLoadingEvent,std::default_delete<sdr::event::AsynchGraphicLoadingEvent> >::~unique_ptr<sdr::event::AsynchGraphicLoadingEvent,std::default_delete<sdr::event::AsynchGraphicLoadingEvent> >() Line 1820        C++
    svxcorelo.dll!sdr::contact::ViewObjectContactOfGraphic::~ViewObjectContactOfGraphic() Line 306      C++

svx/source/sdr/contact/viewobjectcontactofgraphic.cxx
@@ -302,6 +302,8 @@ namespace sdr
 
         ViewObjectContactOfGraphic::~ViewObjectContactOfGraphic()
         {
+            // explicitly call this to avoid it double-freeing itself
+            forgetAsynchGraphicLoadingEvent(mpAsynchLoadEvent.get());
         }
     } // end of namespace contact
 } // end of namespace sdr