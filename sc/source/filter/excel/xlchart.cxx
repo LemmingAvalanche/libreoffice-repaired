LemmingAvalanche/libreoffice-repaired

commit 4bca29d97670efaf465507628036fa3209c68542

loplugin:oncevar in sc
Change-Id: Ice59e286debb6bd0eb692f2b2b0c2c5087c069c0
Reviewed-on: https://gerrit.libreoffice.org/39239
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/excel/xlchart.cxx
@@ -992,7 +992,6 @@ void XclChPropSetHelper::WriteAreaProperties( ScfPropertySet& rPropSet,
     namespace cssd = ::com::sun::star::drawing;
     cssd::FillStyle eFillStyle = cssd::FillStyle_NONE;
     Color aColor;
-    sal_Int16 nTransparency = 0;
 
     // fill color
     if( rAreaFmt.mnPattern != EXC_PATT_NONE )
@@ -1004,7 +1003,7 @@ void XclChPropSetHelper::WriteAreaProperties( ScfPropertySet& rPropSet,
     // write the properties
     ScfPropSetHelper& rAreaHlp = GetAreaHelper( ePropMode );
     rAreaHlp.InitializeWrite();
-    rAreaHlp << eFillStyle << aColor << nTransparency;
+    rAreaHlp << eFillStyle << aColor << 0/*nTransparency*/;
     rAreaHlp.WriteToPropertySet( rPropSet );
 }
 

commit 5e01359ae8e0a1edd153da4be963b91466166b2f

nTransparency of type sal_Int16
regression introduced with 4bca29d97670efaf465507628036fa3209c68542
"loplugin:oncevar in sc"

Change-Id: I521053a59bb16c78d97129e4de5ebbf4820d6640

sc/source/filter/excel/xlchart.cxx
@@ -1003,7 +1003,7 @@ void XclChPropSetHelper::WriteAreaProperties( ScfPropertySet& rPropSet,
     // write the properties
     ScfPropSetHelper& rAreaHlp = GetAreaHelper( ePropMode );
     rAreaHlp.InitializeWrite();
-    rAreaHlp << eFillStyle << aColor << 0/*nTransparency*/;
+    rAreaHlp << eFillStyle << aColor << sal_Int16(0)/*nTransparency*/;
     rAreaHlp.WriteToPropertySet( rPropSet );
 }
 