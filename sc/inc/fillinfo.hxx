LemmingAvalanche/libreoffice-repaired

commit eadc44e26cf5ba16ad3be75a70a4faa4b3d49fca

loplugin:unusedfields in sc part3
Change-Id: I7323e8644e5d336d52fa21b063264b0847b5e780
Reviewed-on: https://gerrit.libreoffice.org/39064
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/inc/fillinfo.hxx
@@ -114,9 +114,7 @@ struct CellInfo
         , nClipMark(ScClipMark::NONE)
         , nWidth(0)
         , nRotateDir(ScRotateDir::NONE)
-        , bMarked(false)
         , bEmptyCellText(false)
-        , bMerged(false)
         , bHOverlapped(false)
         , bVOverlapped(false)
         , bAutoFilter(false)
@@ -157,7 +155,6 @@ struct CellInfo
     sal_uInt16                  nWidth;
     ScRotateDir                 nRotateDir;
 
-    bool                        bMarked : 1;
     bool                        bEmptyCellText : 1;
     bool                        bMerged : 1;
     bool                        bHOverlapped : 1;
@@ -185,7 +182,6 @@ struct RowInfo
     SCCOL               nRotMaxCol;         // SC_ROTMAX_NONE, if nothing
 
     bool                bEmptyBack:1;
-    bool                bEmptyText:1;
     bool                bAutoFilter:1;
     bool                bPivotButton:1;
     bool                bChanged:1;           // TRUE, if not tested

sc/inc/AccessibleFilterMenu.hxx
@@ -119,9 +119,8 @@ public:
 
     /// non-UNO methods
 
-    void appendMenuItem(const OUString& rName, bool bEnabled, size_t nMenuPos);
+    void appendMenuItem(const OUString& rName, size_t nMenuPos);
     void setMenuPos(size_t nMenuPos);
-    void setEnabled(bool bEnabled);
 
 protected:
 
@@ -142,8 +141,6 @@ private:
 
     size_t mnMenuPos;
     VclPtr<ScMenuFloatingWindow> mpWindow;
-
-    bool mbEnabled:1;
 };
 
 #endif

sc/inc/AccessibleFilterMenuItem.hxx
@@ -75,8 +75,6 @@ public:
 
     /// Non-UNO Methods
 
-    void setEnabled(bool bEnabled);
-
 protected:
 
     virtual tools::Rectangle GetBoundingBoxOnScreen() const override;
@@ -92,7 +90,6 @@ private:
 
     VclPtr<ScMenuFloatingWindow> mpWindow;
     size_t mnMenuPos;
-    bool mbEnabled;
 };
 
 #endif

sc/inc/colcontainer.hxx
@@ -32,10 +32,9 @@ class ScColContainer
 {
     typedef std::vector<ScColumn*> ScColumnVector;
     ScColumnVector    aCols;
-    ScDocument*       pDocument;
 
 public:
-    ScColContainer( ScDocument* pDoc, const size_t nSize );
+    ScColContainer( const size_t nSize );
     ~ScColContainer() COVERITY_NOEXCEPT_FALSE;
 
     const ScColumn& operator[] ( const size_t nIndex ) const

sc/inc/compiler.hxx
@@ -281,7 +281,6 @@ private:
 
     const CharClass*    pCharClass;         // which character classification is used for parseAnyToken
     sal_uInt16      mnPredetectedReference;     // reference when reading ODF, 0 (none), 1 (single) or 2 (double)
-    SCTAB       nMaxTab;                    // last sheet in document
     sal_Int32   mnRangeOpPosInSymbol;       // if and where a range operator is in symbol
     const Convention *pConv;
     ExtendedErrorDetection  meExtendedErrorDetection;

sc/inc/dociter.hxx
@@ -165,7 +165,6 @@ private:
         ScDBQueryParamMatrix* mpParam;
         SCROW mnCurRow;
         SCROW mnRows;
-        SCCOL mnCols;
     };
 
     ::std::unique_ptr<ScDBQueryParamBase> mpParam;
@@ -422,8 +421,6 @@ class ScHorizontalCellIterator      // walk through all non empty cells in an ar
     SCCOL           nEndCol;
     SCROW           nStartRow;
     SCROW           nEndRow;
-    SCROW*          pNextRows;
-    SCSIZE*         pNextIndices;
     SCCOL           mnCol;
     SCROW           mnRow;
     ScRefCellValue  maCurCell;

sc/inc/dpoutput.hxx
@@ -76,7 +76,6 @@ private:
     SCROW                   nTabEndRow;
     bool                    bDoFilter:1;
     bool                    bResultsError:1;
-    bool                    mbHasDataLayout:1;
     bool                    bSizesValid:1;
     bool                    bSizeOverflow:1;
     bool                    mbHeaderLayout:1;  // true : grid, false : standard

sc/inc/hints.hxx
@@ -78,7 +78,6 @@ class ScLinkRefreshedHint : public SfxHint
     OUString    aDdeAppl;   // used for dde links:
     OUString    aDdeTopic;
     OUString    aDdeItem;
-    sal_uInt8   nDdeMode;
     ScAddress   aDestPos;   // used to identify area links
                             //! also use source data for area links?
 
@@ -87,7 +86,7 @@ public:
                     virtual ~ScLinkRefreshedHint() override;
 
     void            SetSheetLink( const OUString& rSourceUrl );
-    void            SetDdeLink( const OUString& rA, const OUString& rT, const OUString& rI, sal_uInt8 nM );
+    void            SetDdeLink( const OUString& rA, const OUString& rT, const OUString& rI );
     void            SetAreaLink( const ScAddress& rPos );
 
     ScLinkRefType       GetLinkType() const { return nLinkType; }

commit 7e72d89e68c87869714337b331bcc452aed17154

Init CellInfo::bMerged again
Fallout from

    commit eadc44e26cf5ba16ad3be75a70a4faa4b3d49fca
    Date:   Wed Jun 21 16:15:42 2017 +0200

        loplugin:unusedfields in sc part3

that (luckily) led to massive

warn:legacy.osl:26775:1:sc/source/core/data/document.cxx:4677: Attribut Null
warn:legacy.tools:26775:1:svx/source/dialog/framelinkarray.cxx:621: svx::frame::Array::SetMergedRange - invalid cell ind
ex
warn:legacy.tools:26775:1:svx/source/dialog/framelinkarray.cxx:344: svx::frame::MergedCellIterator::MergedCellIterator -
 not in merged range

output.

Change-Id: Idd9c581490249efb4198b4f09db5dbef331f2b1c

sc/inc/fillinfo.hxx
@@ -115,6 +115,7 @@ struct CellInfo
         , nWidth(0)
         , nRotateDir(ScRotateDir::NONE)
         , bEmptyCellText(false)
+        , bMerged(false)
         , bHOverlapped(false)
         , bVOverlapped(false)
         , bAutoFilter(false)