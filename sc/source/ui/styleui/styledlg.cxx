LemmingAvalanche/libreoffice-repaired

commit 71f60bd448c0983d707f40ab6d9222abc388eada

loplugin:unusedfields in sc part1
in ScNavigatorDlg::ScNavigatorDlg, we replace the assignment of nAreaId
with an assignment to nDataId instead, which is never initialized but
used, whereas nAreaId was initialized but never used.

Change-Id: I9681af7f7d515d4891c9b52d3809e88e3d5e8256
Reviewed-on: https://gerrit.libreoffice.org/39062
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/styleui/styledlg.cxx
@@ -54,16 +54,9 @@ ScStyleDlg::ScStyleDlg( vcl::Window*             pParent,
     , nDlgRsc         ( nRscId )
     , m_nNumberId(0)
     , m_nFontId(0)
-    , m_nFontEffectId(0)
-    , m_nAlignmentId(0)
-    , m_nAsianId(0)
-    , m_nBorderId(0)
     , m_nBackgroundId(0)
-    , m_nProtectId(0)
     , m_nPageId(0)
     , m_nHeaderId(0)
-    , m_nFooterId(0)
-    , m_nSheetId(0)
 {
     SfxAbstractDialogFactory* pFact = SfxAbstractDialogFactory::Create();
     OSL_ENSURE(pFact, "Dialog creation failed!");
@@ -80,25 +73,25 @@ ScStyleDlg::ScStyleDlg( vcl::Window*             pParent,
                 m_nFontId = AddTabPage("font", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_CHAR_NAME ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_CHAR_NAME ));
                 OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_CHAR_EFFECTS ), "GetTabPageCreatorFunc fail!");
                 OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_CHAR_EFFECTS ), "GetTabPageRangesFunc fail!");
-                m_nFontEffectId = AddTabPage("fonteffects", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_CHAR_EFFECTS ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_CHAR_EFFECTS ));
+                AddTabPage("fonteffects", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_CHAR_EFFECTS ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_CHAR_EFFECTS ));
                 OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_ALIGNMENT ), "GetTabPageCreatorFunc fail!");
                 OSL_ENSURE( pFact->GetTabPageRangesFunc( RID_SVXPAGE_ALIGNMENT ), "GetTabPageRangesFunc fail!");
-                m_nAlignmentId = AddTabPage("alignment", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_ALIGNMENT ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_ALIGNMENT ));
+                 AddTabPage("alignment", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_ALIGNMENT ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_ALIGNMENT ));
                 if ( aCJKOptions.IsAsianTypographyEnabled() )
                 {
                     OSL_ENSURE(pFact->GetTabPageCreatorFunc(RID_SVXPAGE_PARA_ASIAN), "GetTabPageCreatorFunc fail!");
                     OSL_ENSURE(pFact->GetTabPageRangesFunc(RID_SVXPAGE_PARA_ASIAN), "GetTabPageRangesFunc fail!");
-                    m_nAsianId = AddTabPage("asiantypo", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_PARA_ASIAN),       pFact->GetTabPageRangesFunc(RID_SVXPAGE_PARA_ASIAN));
+                     AddTabPage("asiantypo", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_PARA_ASIAN),       pFact->GetTabPageRangesFunc(RID_SVXPAGE_PARA_ASIAN));
                 }
                 else
                     RemoveTabPage("asiantypo");
                 OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), "GetTabPageCreatorFunc fail!");
                 OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ), "GetTabPageRangesFunc fail!");
-                m_nBorderId = AddTabPage("borders", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ));
+                 AddTabPage("borders", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ));
                 OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageCreatorFunc fail!");
                 OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageRangesFunc fail!");
                 m_nBackgroundId = AddTabPage("background", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ));
-                m_nProtectId = AddTabPage("protection", &ScTabPageProtection::Create,    &ScTabPageProtection::GetRanges);
+                AddTabPage("protection", &ScTabPageProtection::Create,    &ScTabPageProtection::GetRanges);
             }
             break;
 
@@ -109,13 +102,13 @@ ScStyleDlg::ScStyleDlg( vcl::Window*             pParent,
                 m_nPageId = AddTabPage("page", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_PAGE ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_PAGE ) );
                 OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), "GetTabPageCreatorFunc fail!");
                 OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ), "GetTabPageRangesFunc fail!");
-                m_nBorderId = AddTabPage("borders", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ) );
+                AddTabPage("borders", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ) );
                 OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageCreatorFunc fail!");
                 OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageRangesFunc fail!");
                 m_nBackgroundId = AddTabPage("background", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ) );
                 m_nHeaderId = AddTabPage("header", &ScHeaderPage::Create,      &ScHeaderPage::GetRanges );
                 m_nFooterId = AddTabPage("footer", &ScFooterPage::Create,      &ScFooterPage::GetRanges );
-                m_nSheetId = AddTabPage("sheet", &ScTablePage::Create,     &ScTablePage::GetRanges );
+                AddTabPage("sheet", &ScTablePage::Create,     &ScTablePage::GetRanges );
             }
             break;
 

sc/source/filter/xml/XMLTrackedChangesContext.cxx
@@ -89,11 +89,9 @@ class ScXMLCellContentDeletionContext : public ScXMLImportContext
     sal_uInt32                          nID;
     sal_Int32                           nMatrixCols;
     sal_Int32                           nMatrixRows;
-    formula::FormulaGrammar::Grammar                  eGrammar;
+    formula::FormulaGrammar::Grammar    eGrammar;
     sal_uInt16                          nType;
     ScMatrixMode                        nMatrixFlag;
-    bool                            bBigRange;
-    bool                            bContainsCell;
 
 public:
     ScXMLCellContentDeletionContext( ScXMLImport& rImport, sal_uInt16 nPrfx, const OUString& rLName,
@@ -629,9 +627,7 @@ ScXMLCellContentDeletionContext::ScXMLCellContentDeletionContext(  ScXMLImport& 
     nMatrixRows(0),
     eGrammar( formula::FormulaGrammar::GRAM_STORAGE_DEFAULT),
     nType(css::util::NumberFormat::ALL),
-    nMatrixFlag(ScMatrixMode::NONE),
-    bBigRange(false),
-    bContainsCell(false)
+    nMatrixFlag(ScMatrixMode::NONE)
 {
     sal_Int16 nAttrCount(xAttrList.is() ? xAttrList->getLength() : 0);
     for( sal_Int16 i=0; i < nAttrCount; ++i )
@@ -660,14 +656,12 @@ SvXMLImportContext *ScXMLCellContentDeletionContext::CreateChildContext( sal_uIn
     {
         if (IsXMLToken(rLocalName, XML_CHANGE_TRACK_TABLE_CELL))
         {
-            bContainsCell = true;
             pContext = new ScXMLChangeCellContext(GetScImport(), nPrefix, rLocalName, xAttrList,
                 maCell, sFormulaAddress, sFormula, sFormulaNmsp, eGrammar, sInputString, fValue, nType, nMatrixFlag, nMatrixCols, nMatrixRows );
         }
         else if (IsXMLToken(rLocalName, XML_CELL_ADDRESS))
         {
             OSL_ENSURE(!nID, "a action with a ID should not contain a BigRange");
-            bBigRange = true;
             pContext = new ScXMLBigRangeContext(GetScImport(), nPrefix, rLocalName, xAttrList, aBigRange);
         }
     }

sc/source/ui/Accessibility/AccessibleSpreadsheet.cxx
@@ -290,7 +290,6 @@ void ScAccessibleSpreadsheet::ConstructScAccessibleSpreadsheet(
     mpAccCell.clear();
     meSplitPos = eSplitPos;
     mnTab = nTab;
-    mbHasSelection = false;
     mbDelIns = false;
     mbIsFocusSend = false;
     maVisCells = GetVisCells(GetVisArea(mpViewShell, meSplitPos));
@@ -299,10 +298,7 @@ void ScAccessibleSpreadsheet::ConstructScAccessibleSpreadsheet(
         mpViewShell->AddAccessibilityObject(*this);
 
         const ScViewData& rViewData = mpViewShell->GetViewData();
-        const ScMarkData& rMarkData = rViewData.GetMarkData();
         maActiveCell = rViewData.GetCurPos();
-        mbHasSelection = rMarkData.GetTableSelect(maActiveCell.Tab()) &&
-                    (rMarkData.IsMarked() || rMarkData.IsMultiMarked());
         mpAccCell = GetAccessibleCellAt(maActiveCell.Row(), maActiveCell.Col());
         ScDocument* pScDoc= GetDocument(mpViewShell);
         if (pScDoc)
@@ -333,7 +329,6 @@ void ScAccessibleSpreadsheet::CompleteSelectionChanged(bool bNewState)
     }
     if (mpMarkedRanges)
         DELETEZ(mpMarkedRanges);
-    mbHasSelection = bNewState;
 
     AccessibleEventObject aEvent;
     aEvent.EventId = AccessibleEventId::STATE_CHANGED;

sc/source/ui/app/client.cxx
@@ -41,8 +41,7 @@ using namespace com::sun::star;
 
 ScClient::ScClient( ScTabViewShell* pViewShell, vcl::Window* pDraw, SdrModel* pSdrModel, SdrOle2Obj* pObj ) :
     SfxInPlaceClient( pViewShell, pDraw, pObj->GetAspect() ),
-    pModel( pSdrModel ),
-    pGrafEdit( nullptr )
+    pModel( pSdrModel )
 {
     SetObject( pObj->GetObjRef() );
 }

sc/source/ui/app/drwtrans.cxx
@@ -84,7 +84,6 @@ ScDrawTransferObj::ScDrawTransferObj( SdrModel* pClipModel, ScDocShell* pContain
     pDragSourceView( nullptr ),
     nDragSourceFlags( ScDragSrc::Undefined ),
     bDragWasInternal( false ),
-    nSourceDocID( 0 ),
     maShellID(SfxObjectShell::CreateShellID(pContainerShell))
 {
 
@@ -210,7 +209,6 @@ ScDrawTransferObj::ScDrawTransferObj( SdrModel* pClipModel, ScDocShell* pContain
     if ( pContainerShell )
     {
         ScDocument& rDoc = pContainerShell->GetDocument();
-        nSourceDocID = rDoc.GetDocumentID();
         if ( pPage )
         {
             ScChartHelper::FillProtectedChartRangesVector( m_aProtectedChartRangesVector, &rDoc, pPage );

sc/source/ui/condformat/condformatdlgentry.cxx
@@ -41,7 +41,6 @@
 ScCondFrmtEntry::ScCondFrmtEntry(vcl::Window* pParent, ScDocument* pDoc, const ScAddress& rPos)
     : VclContainer(pParent, WB_CLIPCHILDREN | WB_BORDER)
     , mbActive(false)
-    , mnIndex(0)
     , maStrCondition(ScResId(SCSTR_CONDITION))
     , mpDoc(pDoc)
     , maPos(rPos)
@@ -106,10 +105,7 @@ bool ScCondFrmtEntry::EventNotify( NotifyEvent& rNEvt )
 
 void ScCondFrmtEntry::SetIndex(sal_Int32 nIndex)
 {
-    mnIndex = nIndex;
-    OUStringBuffer aBuffer(maStrCondition);
-    aBuffer.append(OUString::number(nIndex));
-    maFtCondNr->SetText(aBuffer.makeStringAndClear());
+    maFtCondNr->SetText(maStrCondition + OUString::number(nIndex));
 }
 
 void ScCondFrmtEntry::SetHeight()

sc/source/ui/dbgui/consdlg.cxx
@@ -40,20 +40,17 @@ class ScAreaData
 {
 public:
     ScAreaData()
-        : bIsDbArea(false)
     {
     }
 
-    void Set( const OUString& rName, const OUString& rArea, bool bDb )
-                {
-                    aStrName  = rName;
-                    aStrArea  = rArea;
-                    bIsDbArea = bDb;
-                }
+    void Set( const OUString& rName, const OUString& rArea )
+    {
+        aStrName  = rName;
+        aStrArea  = rArea;
+    }
 
     OUString  aStrName;
     OUString  aStrArea;
-    bool  bIsDbArea;
 };
 
 ScConsolidateDlg::ScConsolidateDlg( SfxBindings* pB, SfxChildWindow* pCW, vcl::Window* pParent,
@@ -218,7 +215,7 @@ void ScConsolidateDlg::Init()
         while ( aIter.Next( aStrName, aRange ) )
         {
             OUString aStrArea(aRange.Format(ScRefFlags::ADDR_ABS_3D, pDoc, eConv));
-            pAreaData[nAt++].Set( aStrName, aStrArea, aIter.WasDBName() );
+            pAreaData[nAt++].Set( aStrName, aStrArea );
         }
     }
 

sc/source/ui/dbgui/pfiltdlg.cxx
@@ -55,8 +55,7 @@ ScPivotFilterDlg::ScPivotFilterDlg(vcl::Window* pParent, const SfxItemSet& rArgS
         pOutItem        ( nullptr ),
         pViewData       ( nullptr ),
         pDoc            ( nullptr ),
-        nSrcTab         ( nSourceTab ),     // is not in QueryParam
-        nFieldCount     ( 0 )
+        nSrcTab         ( nSourceTab )     // is not in QueryParam
 {
     get(m_pLbField1, "field1");
     get(m_pLbCond1, "cond1");
@@ -285,7 +284,6 @@ void ScPivotFilterDlg::FillFieldLists()
             m_pLbField3->InsertEntry( aFieldName, i );
             i++;
         }
-        nFieldCount = i;
     }
 }
 

commit 0638fd7dbfdf428dc73508d443ff368f6bed1933

coverity#1414481 Uninitialized scalar field
regression from

commit 71f60bd448c0983d707f40ab6d9222abc388eada
Date:   Wed Jun 21 15:32:03 2017 +0200

    loplugin:unusedfields in sc part1

Change-Id: Ibd3a8410f7916bf7c03b436f45bf382ff9eb3798
Reviewed-on: https://gerrit.libreoffice.org/39571
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sc/source/ui/styleui/styledlg.cxx
@@ -57,6 +57,7 @@ ScStyleDlg::ScStyleDlg( vcl::Window*             pParent,
     , m_nBackgroundId(0)
     , m_nPageId(0)
     , m_nHeaderId(0)
+    , m_nFooterId(0)
 {
     SfxAbstractDialogFactory* pFact = SfxAbstractDialogFactory::Create();
     OSL_ENSURE(pFact, "Dialog creation failed!");