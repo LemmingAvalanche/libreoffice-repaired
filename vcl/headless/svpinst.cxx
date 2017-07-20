LemmingAvalanche/libreoffice-repaired

commit 6e8356c3d9e6f741e194c9ffc116fea129aebb83

Add some Scheduler unit tests and logging
 1. calling Start() for invoked tasks
 2. correctly schedule by priority
 3. self-stopping AutoTimer

This also adds SAL_INFO output to Scheduler and Task to log the
scheduling processing tasks.

Change-Id: I3c8a708d1fd51c550320f8af3f9486c43c32e358

vcl/osx/salinst.cxx
@@ -445,7 +445,7 @@ bool AquaSalInstance::CheckYieldMutex()
     return bRet;
 }
 
-bool AquaSalInstance::isNSAppThread() const
+bool AquaSalInstance::IsMainThread() const
 {
     return osl::Thread::getCurrentIdentifier() == maMainThread;
 }
@@ -607,7 +607,7 @@ SalYieldResult AquaSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents
     // handle cocoa event queue
     // cocoa events may be only handled in the thread the NSApp was created
     bool bHadEvent = false;
-    if( isNSAppThread() && mnActivePrintJobs == 0 )
+    if( IsMainThread() && mnActivePrintJobs == 0 )
     {
         // we need to be woken up by a cocoa-event
         // if a user event should be posted by the event handling below
@@ -742,7 +742,7 @@ bool AquaSalInstance::AnyInput( VclInputFlags nType )
         }
     }
 
-    if (![NSThread isMainThread])
+    if (!IsMainThread())
         return false;
 
     unsigned/*NSUInteger*/ nEventMask = 0;
@@ -1019,7 +1019,7 @@ OUString AquaSalInstance::getOSVersion()
 YieldMutexReleaser::YieldMutexReleaser() : mnCount( 0 )
 {
     SalData* pSalData = GetSalData();
-    if( ! pSalData->mpFirstInstance->isNSAppThread() )
+    if( ! pSalData->mpFirstInstance->IsMainThread() )
     {
         SalData::ensureThreadAutoreleasePool();
         mnCount = pSalData->mpFirstInstance->ReleaseYieldMutex();

vcl/win/app/salinst.cxx
@@ -188,10 +188,7 @@ void ImplSalYieldMutexAcquireWithWait()
     if ( !pInst )
         return;
 
-    DWORD nThreadId = GetCurrentThreadId();
-    SalData* pSalData = GetSalData();
-
-    if ( pSalData->mnAppThreadId == nThreadId )
+    if ( pInst->IsMainThread() )
     {
         // tdf#96887 If this is the main thread, then we must wait for two things:
         // - the mpSalYieldMutex being freed
@@ -594,13 +591,18 @@ ImplSalYield( bool bWait, bool bHandleAllCurrentEvents )
                      SalYieldResult::TIMEOUT;
 }
 
+bool WinSalInstance::IsMainThread() const
+{
+    const SalData* pSalData = GetSalData();
+    return pSalData->mnAppThreadId == GetCurrentThreadId();
+}
+
 SalYieldResult WinSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLong const nReleased)
 {
     SalYieldResult eDidWork = SalYieldResult::TIMEOUT;
     // NOTE: if nReleased != 0 this will be called without SolarMutex
     //       so don't do anything dangerous before releasing it here
     SalYieldMutex*  pYieldMutex = mpSalYieldMutex;
-    SalData*        pSalData = GetSalData();
     DWORD           nCurThreadId = GetCurrentThreadId();
     sal_uLong const nCount = (nReleased != 0)
                                 ? nReleased
@@ -611,7 +613,7 @@ SalYieldResult WinSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents,
         pYieldMutex->release();
         n--;
     }
-    if ( pSalData->mnAppThreadId != nCurThreadId )
+    if ( !IsMainThread() )
     {
         // #97739# A SendMessage call blocks until the called thread (here: the main thread)
         // returns. During a yield however, messages are processed in the main thread that might

include/vcl/svapp.hxx
@@ -462,18 +462,25 @@ public:
     */
     static void                 Quit();
 
-    /** Attempt to reschedule in processing of current event(s)
+    /** Attempt to process current pending event(s)
+
+     It doesn't sleep if no events are available for processing.
 
      @param bAllEvents  If set to true, then try to process all the
         events. If set to false, then only process the current
         event. Defaults to false.
 
+     @returns true if any event was processed.
+
      @see Execute, Quit, Yield, EndYield, GetSolarMutex,
           GetMainThreadIdentifier, ReleaseSolarMutex, AcquireSolarMutex,
      */
-    static void                 Reschedule( bool bAllEvents = false );
+    static bool                 Reschedule( bool bAllEvents = false );
 
-    /** Allow processing of the next event.
+    /** Process the next event.
+
+     It sleeps if no event is available for processing and just returns
+     if an event was processed.
 
      @see Execute, Quit, Reschedule, EndYield, GetSolarMutex,
           GetMainThreadIdentifier, ReleaseSolarMutex, AcquireSolarMutex,

vcl/inc/headless/svpinst.hxx
@@ -157,6 +157,7 @@ public:
     // and timer
     virtual SalYieldResult  DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLong nReleased) override;
     virtual bool            AnyInput( VclInputFlags nType ) override;
+    virtual bool            IsMainThread() const override { return true; }
 
     // may return NULL to disable session management
     virtual SalSession*     CreateSalSession() override;

vcl/inc/osx/salinst.h
@@ -142,7 +142,8 @@ public:
     void PostUserEvent( AquaSalFrame* pFrame, SalEvent nType, void* pData );
     void delayedSettingsChanged( bool bInvalidate );
 
-    bool isNSAppThread() const;
+    // Is this the NSAppThread?
+    virtual bool IsMainThread() const override;
 
     void startedPrintJob() { mnActivePrintJobs++; }
     void endedPrintJob() { mnActivePrintJobs--; }

vcl/inc/salinst.hxx
@@ -127,6 +127,7 @@ public:
     virtual void            AcquireYieldMutex( sal_uLong nCount ) = 0;
     // return true, if yield mutex is owned by this thread, else false
     virtual bool            CheckYieldMutex() = 0;
+    virtual bool            IsMainThread() const = 0;
 
     /**
      * Wait for the next event (if bWait) and dispatch it,

vcl/inc/saltimer.hxx
@@ -21,10 +21,9 @@
 #define INCLUDED_VCL_INC_SALTIMER_HXX
 
 #include <sal/config.h>
-
 #include <vcl/dllapi.h>
-
 #include <salwtype.hxx>
+#include <iostream>
 
 /*
  * note: there will be only a single instance of SalTimer
@@ -71,6 +70,15 @@ struct ImplSchedulerData
 
     const char *GetDebugName() const;
 };
+
+template< typename charT, typename traits >
+inline std::basic_ostream<charT, traits> & operator <<(
+    std::basic_ostream<charT, traits> & stream, const ImplSchedulerData& data )
+{
+    stream << " i: " << data.mbInScheduler
+           << " d: " << data.mbDelete;
+    return stream;
+}
 
 #endif // INCLUDED_VCL_INC_SALTIMER_HXX
 

vcl/inc/unx/gtk/gtkinst.hxx
@@ -210,6 +210,8 @@ public:
 
     virtual SalYieldResult      DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLong nReleased) override;
     virtual bool                AnyInput( VclInputFlags nType ) override;
+    // impossible to handle correctly, as "main thread" depends on the dispatch mutex
+    virtual bool                IsMainThread() const override { return false; }
 
     virtual GenPspGraphics     *CreatePrintGraphics() override;
 

commit bcc42f47695915a9a782de9c8f64d3a9a01751e0

SalInstance::DoYield: Don't drop SolarMutex when accessing user event queue
<https://ci.libreoffice.org/job/lo_tb_master_linux_dbg/14468/console> fails with
the assert in the OSL_DEBUG_LEVEL > 0 test code in
Scheduler::ProcessEventsToIdle firing (see below), indicating that (even though
the SolarMutex is locked one frame up in VCLXToolkit::processEventsToIdle), when
Application::Reschedule(true) returned, there are unprocessed events in the
queue.

That failure is with the headless plugin, and looking at
SvpSalInstance::DoYield, there was a window of time after m_aEventGuard has been
released and before the SolarMutex has been re-acquired, where other threads
could enqueue events.

If Application::Reschedule(true) can thus not guarantee that the queue is empty
when it returned, that would mean that the assert in
Scheduler::ProcessEventsToIdle (introduced with
6e8356c3d9e6f741e194c9ffc116fea129aebb83 "Add some Scheduler unit tests and
logging") is bogus.

However, at least the two SalInstance::DoYield implementations
SvpSalInstance::DoYield and AquaSalInstance::DoYield appear to have no reason
in the first place to drop the SolarMutex when accessing the user event queue.
So removing that dropping should make the assert in
Scheduler::ProcessEventsToIdle faithful, at least for these two implementations.

But I have no idea whether the other three implementations
(X11SalInstance::DoYield in vcl/unx/generic/app/salinst.cxx,
GtkInstance::DoYield in vcl/unx/gtk/gtkinst.cxx, and WinSalInstance::DoYield in
vcl/win/app/salinst.cxx) have similar issues---i.e., whether everything is good
after this change, whether those other implementations need similar changes, or
whether ultimately Application::Reschedule(true) cannot guarantee the queue to
be empty upon return (in which case the assert in Scheduler::ProcessEventsToIdle
would need to go).

> warn:vcl.schedule:27575:54:vcl/source/app/svapp.cxx:535: Unprocessed Idle: vcl::Window maPaintIdle

vcl/headless/svpinst.cxx
@@ -310,9 +310,7 @@ bool SvpSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLong
     assert(nReleased == 0); // not implemented
     // first, check for already queued events.
 
-    // release yield mutex
     std::list< SalUserEvent > aEvents;
-    sal_uLong nAcquireCount = ReleaseYieldMutex();
     {
         osl::MutexGuard g(m_aEventGuard);
         if( ! m_aUserEvents.empty() )
@@ -329,8 +327,6 @@ bool SvpSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLong
             }
         }
     }
-    // acquire yield mutex again
-    AcquireYieldMutex( nAcquireCount );
 
     bool bEvent = !aEvents.empty();
     if( bEvent )

vcl/osx/salinst.cxx
@@ -556,8 +556,6 @@ bool AquaSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLon
     bool bDispatchUser = true;
     while( bDispatchUser )
     {
-        sal_uLong nCount = ReleaseYieldMutex();
-
         // get one user event
         SalUserEvent aEvent( nullptr, nullptr, SalEvent::NONE );
         {
@@ -571,7 +569,6 @@ bool AquaSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents, sal_uLon
             else
                 bDispatchUser = false;
         }
-        AcquireYieldMutex( nCount );
 
         // dispatch it
         if( aEvent.mpFrame && AquaSalFrame::isAlive( aEvent.mpFrame ) )