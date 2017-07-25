LemmingAvalanche/libreoffice-repaired

commit 4d0d97d599bc6dd2dd585225c48bce162bf2876a

tdf#106953 RTF import: fix missing paragraph left margin
See commit 2f1ffe926c09f16caa98d5ad6f0539d1bd110025 (tdf#95376 DOCX
import: fix incorrectly indented tab stops, 2016-01-26) for the various
sources that can determine the paragraph indentation.

In this case the problem was that too aggressive RTF style deduplication
removed a direct indent, which then meant a fallback to the ind-from-num
value, not to the ind-from-parastyle one.

Change-Id: I3b47b2bbeaaedf405baef24505d23dc49bd01865
Reviewed-on: https://gerrit.libreoffice.org/37660
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1333,6 +1333,15 @@ DECLARE_RTFIMPORT_TEST(testN825305, "n825305.rtf")
     CPPUNIT_ASSERT_EQUAL(beans::PropertyState_DIRECT_VALUE, ePropertyState);
 }
 
+DECLARE_RTFIMPORT_TEST(testTdf106953, "tdf106953.rtf")
+{
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1270), getProperty<sal_Int32>(getParagraph(1), "ParaLeftMargin"));
+    uno::Reference<beans::XPropertyState> xPropertyState(getParagraph(1), uno::UNO_QUERY);
+    beans::PropertyState ePropertyState = xPropertyState->getPropertyState("ParaLeftMargin");
+    // Was beans::PropertyState_DEFAULT_VALUE.
+    CPPUNIT_ASSERT_EQUAL(beans::PropertyState_DIRECT_VALUE, ePropertyState);
+}
+
 DECLARE_RTFIMPORT_TEST(testParaBottomMargin, "para-bottom-margin.rtf")
 {
     uno::Reference<beans::XPropertySet> xPropertySet(getStyles("ParagraphStyles")->getByName("Standard"), uno::UNO_QUERY);

sw/qa/extras/rtfimport/data/tdf106953.rtf
@@ -1,0 +1,23 @@
+{\rtf \ansi \ansicpg0 \deff0 \stshfdbch1 \stshfloch0 \stshfhich0 \deflang1033 \deflangfe1033
+{\stylesheet
+{\s19 \li720
+style;
+}
+}
+{\*\listtable
+{\list
+{\listlevel \levelnfc0 \levelnfcn0 \leveljc0 \leveljcn0 \levelstartat1 \levelfollow0 \levelspace0 \levelindent0 \levellegal0 \levelnorestart0
+{\leveltext\'02\'00.;
+}
+{\levelnumbers \'01
+{\uc1 \u59 ?}
+}
+\li720 \fi-360 }
+\listid697112958 }
+}
+{\*\listoverridetable
+{\listoverride \listid697112958 \listoverridecount0 \ls1 }
+}
+\pard \plain \s19 \fi-360 \li720 \ls1
+{hello\par }
+}

writerfilter/source/rtftok/rtfdispatchvalue.cxx
@@ -628,7 +628,7 @@ RTFError RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
         if (m_aStates.top().eDestination == Destination::LISTOVERRIDEENTRY)
             m_aStates.top().aTableAttributes.set(NS_ooxml::LN_CT_AbstractNum_nsid, pIntValue);
         else
-            putNestedSprm(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_tabs, NS_ooxml::LN_CT_NumPr_numId, pIntValue);
+            putNestedSprm(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_numPr, NS_ooxml::LN_CT_NumPr_numId, pIntValue);
     }
     break;
     case RTF_UC:

writerfilter/source/rtftok/rtfsprm.cxx
@@ -158,11 +158,16 @@ static bool isSPRMDeduplicateBlacklist(Id nId)
 {
     switch (nId)
     {
+    // See the NS_ooxml::LN_CT_PPrBase_tabs handler in DomainMapper,
+    // deduplication is explicitly not wanted for these tokens.
     case NS_ooxml::LN_CT_TabStop_val:
     case NS_ooxml::LN_CT_TabStop_leader:
     case NS_ooxml::LN_CT_TabStop_pos:
-        // See the NS_ooxml::LN_CT_PPrBase_tabs handler in DomainMapper,
-        // deduplication is explicitly not wanted for these tokens.
+    // Erasing these just because they equal to the style one is
+    // problematic, as then the used value won't be from the style, but
+    // possibly from the numbering.
+    case NS_ooxml::LN_CT_Ind_left:
+    case NS_ooxml::LN_CT_Ind_right:
         return true;
 
     default:

commit 04d811e7c346dff982e7f4884cdcfd3b46ff221b

tdf#108951 RTF import: fix missing custom bullet types
Regression from commit 4d0d97d599bc6dd2dd585225c48bce162bf2876a
(tdf#106953 RTF import: fix missing paragraph left margin, 2017-05-16)
where I fixed the parent of the ooxml::CT_NumPr_numId token at one
place, but forgot to adapt RTFDocumentImpl::popState() accordingly.

Change-Id: Ibddd2486a50f284e11c2cd96339e7140b427aae1
Reviewed-on: https://gerrit.libreoffice.org/40381
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2906,7 +2906,7 @@ RTFError RTFDocumentImpl::popState()
 
             // Use it
             putNestedSprm(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_numPr, NS_ooxml::LN_CT_NumPr_ilvl, pIlvlValue);
-            putNestedSprm(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_tabs, NS_ooxml::LN_CT_NumPr_numId, pIdValue);
+            putNestedSprm(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_numPr, NS_ooxml::LN_CT_NumPr_numId, pIdValue);
         }
     }
     break;

sw/qa/extras/rtfimport/data/tdf108951.rtf
@@ -1,0 +1,38 @@
+{\rtf1\ansi\ansicpg1252\deff0\deflang1033
+{\fonttbl
+{\f0\fnil\fcharset0\fprq0\fttruetype Times New Roman;}
+{\f1\fnil\fcharset0\fprq0\fttruetype NULL;}
+{\f2\fnil\fcharset0\fprq0\fttruetype Symbol;}
+{\f3\fnil\fcharset0\fprq0\fttruetype Dingbats;}
+{\f4\fnil\fcharset0\fprq0\fttruetype Arial;}
+{\f5\fnil\fcharset0\fprq0\fttruetype Courier New;}
+}
+{\stylesheet
+{\s15 Normal;}
+}
+{\*\listtable
+{\list\listtemplateid372490334\listsimple
+{\listlevel\levelnfc23\levelstartat0\levelspace0\levelfollow0\fi-431\li720
+{\leveltext\'01\'b7 ;}
+{\levelnumbers;}
+\f2}
+\listid616308743}
+{\list\listtemplateid1541166935\listsimple
+{\listlevel\levelnfc23\levelstartat0\levelspace0\levelfollow0\fi-431\li720
+{\leveltext\'01\'2d ;}
+{\levelnumbers;}
+\f0}
+\listid1601185492}
+}
+{\*\listoverridetable
+{\listoverride\listoverridecount0\listid616308743\ls1}
+{\listoverride\listoverridecount0\listid1601185492\ls2}
+}
+\pard\fi-431\li720\s15
+{\*\pn\pnql\pnstart0\pnlvlblt
+{\pntxtb -}
+}
+\ls2\ilvl0
+{\lang1033
+dashed}
+\par}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -143,6 +143,15 @@ DECLARE_RTFIMPORT_TEST(testFdo46662, "fdo46662.rtf")
     }
 }
 
+DECLARE_RTFIMPORT_TEST(testTdf108951, "tdf108951.rtf")
+{
+    // This test is import-only, as we assert the list ID, which is OK to
+    // re-order on export, but then this test would not pass anymore.
+
+    // \ls2 was imported as WWNum1.
+    CPPUNIT_ASSERT_EQUAL(OUString("WWNum2"), getProperty<OUString>(getParagraph(1), "NumberingStyleName"));
+}
+
 DECLARE_RTFIMPORT_TEST(testFdo47036, "fdo47036.rtf")
 {
     uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);