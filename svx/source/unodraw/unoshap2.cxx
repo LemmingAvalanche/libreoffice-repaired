LemmingAvalanche/libreoffice-repaired

commit 02413c094f59e98712e2bb2a22937b27cc6bf4a6

use actual UNO enums in svx
Change-Id: I00f53260667861ca2595892b5605479da3401adb
Reviewed-on: https://gerrit.libreoffice.org/35913
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/unodraw/unoshap2.cxx
@@ -662,8 +662,8 @@ namespace
 
     struct EnumConversionMap
     {
-        sal_Int16   nAPIValue;
-        sal_Int16   nFormValue;
+        style::ParagraphAdjust   nAPIValue;
+        sal_Int16                nFormValue;
     };
 
     EnumConversionMap aMapAdjustToAlign[] =
@@ -676,51 +676,33 @@ namespace
         {style::ParagraphAdjust_RIGHT,          (sal_Int16)awt::TextAlign::RIGHT},
         {style::ParagraphAdjust_BLOCK,          (sal_Int16)awt::TextAlign::RIGHT},
         {style::ParagraphAdjust_STRETCH,        (sal_Int16)awt::TextAlign::LEFT},
-        {-1,-1}
+        {(style::ParagraphAdjust)-1,-1}
     };
 
-    void lcl_mapFormToAPIValue( Any& _rValue, const EnumConversionMap* _pMap )
+    void lcl_convertTextAlignmentToParaAdjustment( Any& _rValue )
     {
         sal_Int16 nValue = sal_Int16();
         OSL_VERIFY( _rValue >>= nValue );
 
-        const EnumConversionMap* pEntry = _pMap;
-        while ( pEntry && ( pEntry->nFormValue != -1 ) )
-        {
-            if ( nValue == pEntry->nFormValue )
+        for ( auto const & rEntry : aMapAdjustToAlign )
+            if ( nValue == rEntry.nFormValue )
             {
-                _rValue <<= pEntry->nAPIValue;
+                _rValue <<= rEntry.nAPIValue;
                 return;
             }
-            ++pEntry;
-        }
     }
 
-    void lcl_mapAPIToFormValue( Any& _rValue, const EnumConversionMap* _pMap )
+    void lcl_convertParaAdjustmentToTextAlignment( Any& _rValue )
     {
         sal_Int32 nValue = 0;
         OSL_VERIFY( _rValue >>= nValue );
 
-        const EnumConversionMap* pEntry = _pMap;
-        while ( pEntry && ( pEntry->nAPIValue != -1 ) )
-        {
-            if ( nValue == pEntry->nAPIValue )
+        for ( auto const & rEntry : aMapAdjustToAlign )
+            if ( (style::ParagraphAdjust)nValue == rEntry.nAPIValue )
             {
-                _rValue <<= pEntry->nFormValue;
+                _rValue <<= rEntry.nFormValue;
                 return;
             }
-            ++pEntry;
-        }
-    }
-
-    void lcl_convertTextAlignmentToParaAdjustment( Any& rValue )
-    {
-        lcl_mapFormToAPIValue( rValue, aMapAdjustToAlign );
-    }
-
-    void lcl_convertParaAdjustmentToTextAlignment( Any& rValue )
-    {
-        lcl_mapAPIToFormValue( rValue, aMapAdjustToAlign );
     }
 
     void convertVerticalAdjustToVerticalAlign( Any& _rValue )

cui/source/inc/swpossizetabpage.hxx
@@ -26,6 +26,7 @@
 #include <vcl/lstbox.hxx>
 #include <svx/swframeposstrings.hxx>
 #include <svx/swframeexample.hxx>
+#include <com/sun/star/text/TextContentAnchorType.hpp>
 
 // SvxSwPosSizeTabPage - position and size page for Writer drawing objects
 struct FrmMap;
@@ -109,13 +110,13 @@ class SvxSwPosSizeTabPage : public SfxTabPage
     DECL_LINK( ModifyHdl, Edit&, void );
     DECL_LINK(ProtectHdl, Button*, void);
 
-    void            InitPos(short nAnchorType, sal_uInt16 nH, sal_uInt16 nHRel,
+    void            InitPos(css::text::TextContentAnchorType nAnchorType, sal_uInt16 nH, sal_uInt16 nHRel,
                             sal_uInt16 nV,  sal_uInt16 nVRel,
                             long   nX,  long   nY);
     sal_uInt16          GetMapPos(FrmMap *pMap, ListBox &rAlignLB);
     static short        GetAlignment(FrmMap *pMap, sal_uInt16 nMapPos, ListBox &rAlignLB, ListBox &rRelationLB);
     static short        GetRelation(FrmMap *pMap, ListBox &rRelationLB);
-    short               GetAnchorType(bool* pbHasChanged = nullptr);
+    css::text::TextContentAnchorType GetAnchorType(bool* pbHasChanged = nullptr);
     void                FillRelLB(FrmMap *pMap, sal_uInt16 nLBSelPos, sal_uInt16 nAlign, sal_uInt16 nRel, ListBox &rLB, FixedText &rFT);
     sal_uInt16          FillPosLB(FrmMap *pMap, sal_uInt16 nAlign, const sal_uInt16 _nRel, ListBox &rLB);
 

cui/source/tabpages/swpossizetabpage.cxx
@@ -30,7 +30,6 @@
 #include <svx/svdpagv.hxx>
 #include <svx/rectenum.hxx>
 #include <sal/macros.h>
-#include <com/sun/star/text/TextContentAnchorType.hpp>
 #include <com/sun/star/text/HoriOrientation.hpp>
 #include <com/sun/star/text/VertOrientation.hpp>
 #include <com/sun/star/text/RelOrientation.hpp>
@@ -772,11 +771,11 @@ const sal_uInt16* SvxSwPosSizeTabPage::GetRanges()
 bool SvxSwPosSizeTabPage::FillItemSet( SfxItemSet* rSet)
 {
     bool bAnchorChanged = false;
-    short nAnchor = GetAnchorType(&bAnchorChanged);
+    css::text::TextContentAnchorType nAnchor = GetAnchorType(&bAnchorChanged);
     bool bModified = false;
     if(bAnchorChanged)
     {
-        rSet->Put(SfxInt16Item(SID_ATTR_TRANSFORM_ANCHOR, nAnchor));
+        rSet->Put(SfxInt16Item(SID_ATTR_TRANSFORM_ANCHOR, (sal_Int16)nAnchor));
         bModified = true;
     }
     if ( m_pPositionCB->IsValueChangedFromSaved() )
@@ -926,10 +925,10 @@ void SvxSwPosSizeTabPage::Reset( const SfxItemSet* rSet)
 {
     const SfxPoolItem* pItem = GetItem( *rSet, SID_ATTR_TRANSFORM_ANCHOR );
     bool bInvalidateAnchor = false;
-    short nAnchorType = TextContentAnchorType_AT_PARAGRAPH;
+    TextContentAnchorType nAnchorType = TextContentAnchorType_AT_PARAGRAPH;
     if(pItem)
     {
-        nAnchorType = static_cast<const SfxInt16Item*>(pItem)->GetValue();
+        nAnchorType = (TextContentAnchorType) static_cast<const SfxInt16Item*>(pItem)->GetValue();
         switch(nAnchorType)
         {
             case  TextContentAnchorType_AT_PAGE:        m_pToPageRB->Check();  break;
@@ -1102,9 +1101,9 @@ void SvxSwPosSizeTabPage::EnableAnchorTypes(SvxAnchorIds nAnchorEnable)
         m_pToPageRB->Enable(false);
 }
 
-short SvxSwPosSizeTabPage::GetAnchorType(bool* pbHasChanged)
+TextContentAnchorType SvxSwPosSizeTabPage::GetAnchorType(bool* pbHasChanged)
 {
-    short nRet = -1;
+    TextContentAnchorType nRet = (TextContentAnchorType)-1;
     RadioButton* pCheckedButton = nullptr;
     if(m_pToParaRB->IsEnabled())
     {
@@ -1234,7 +1233,7 @@ IMPL_LINK_NOARG(SvxSwPosSizeTabPage, AnchorTypeHdl, Button*, void)
     // type to-paragraph' and to-character
     m_pFollowCB->Enable( m_pToParaRB->IsChecked() || m_pToCharRB->IsChecked() );
 
-    short nId = GetAnchorType();
+    TextContentAnchorType nId = GetAnchorType();
 
     InitPos( nId, USHRT_MAX, 0, USHRT_MAX, 0, LONG_MAX, LONG_MAX);
     RangeModifyHdl(*m_pWidthMF);
@@ -1248,7 +1247,7 @@ IMPL_LINK_NOARG(SvxSwPosSizeTabPage, AnchorTypeHdl, Button*, void)
 
 IMPL_LINK_NOARG(SvxSwPosSizeTabPage, MirrorHdl, Button*, void)
 {
-    short nId = GetAnchorType();
+    TextContentAnchorType nId = GetAnchorType();
     InitPos( nId, USHRT_MAX, 0, USHRT_MAX, 0, LONG_MAX, LONG_MAX);
 }
 
@@ -1490,7 +1489,7 @@ sal_uInt16 SvxSwPosSizeTabPage::GetMapPos(FrmMap *pMap, ListBox &rAlignLB)
     return nMapPos;
 }
 
-void SvxSwPosSizeTabPage::InitPos(short nAnchor,
+void SvxSwPosSizeTabPage::InitPos(TextContentAnchorType nAnchor,
                                 sal_uInt16 nH,
                                 sal_uInt16 nHRel,
                                 sal_uInt16 nV,

cui/source/tabpages/tpline.cxx
@@ -1417,7 +1417,7 @@ void SvxLineTabPage::Reset( const SfxItemSet* rAttrs )
 
         switch(eLineJoint)
         {
-            case css::drawing::LineJoint_MAKE_FIXED_SIZE: // fallback to round, unused value
+            case css::drawing::LineJoint::LineJoint_MAKE_FIXED_SIZE: // fallback to round, unused value
             case css::drawing::LineJoint_ROUND : m_pLBEdgeStyle->SelectEntryPos(0); break;
             case css::drawing::LineJoint_NONE : m_pLBEdgeStyle->SelectEntryPos(1); break;
             case css::drawing::LineJoint_MIDDLE : // fallback to mitre, unused value

include/svx/fillctrl.hxx
@@ -55,7 +55,7 @@ private:
     VclPtr<ToolBox>            mpToolBoxColor;
     VclPtr<SvxFillAttrBox>     mpLbFillAttr;
 
-    sal_uInt16          meLastXFS;
+    css::drawing::FillStyle    meLastXFS;
     sal_Int32           mnLastPosGradient;
     sal_Int32           mnLastPosHatch;
     sal_Int32           mnLastPosBitmap;

include/svx/swframeexample.hxx
@@ -24,6 +24,8 @@
 #include <tools/color.hxx>
 #include <tools/gen.hxx>
 #include <svx/svxdllapi.h>
+#include <com/sun/star/text/TextContentAnchorType.hpp>
+#include <com/sun/star/text/WrapTextMode.hpp>
 
 // class SwFrmPagePreview -------------------------------------------------------
 
@@ -55,8 +57,10 @@ class SVX_DLLPUBLIC SvxSwFrameExample : public vcl::Window
     short       nVAlign;
     short       nVRel;
 
-    short       nWrap;
-    short       nAnchor;
+    css::text::WrapTextMode
+                nWrap;
+    css::text::TextContentAnchorType
+                nAnchor;
     bool        bTrans;
 
     Point       aRelPos;
@@ -74,7 +78,7 @@ public:
 
     SvxSwFrameExample(vcl::Window* pParent, WinBits nStyle);
 
-    void SetWrap(sal_uInt16 nW)          { nWrap     = nW; }
+    void SetWrap(css::text::WrapTextMode nW) { nWrap     = nW; }
 
     void SetHAlign(short nH)          { nHAlign   = nH; }
     void SetHoriRel(short nR)         { nHRel     = nR; }
@@ -83,7 +87,7 @@ public:
     void SetVertRel(short nR)         { nVRel     = nR; }
 
     void SetTransparent(bool bT)      { bTrans    = bT; }
-    void SetAnchor(short nA)          { nAnchor   = nA; }
+    void SetAnchor(css::text::TextContentAnchorType nA) { nAnchor   = nA; }
 
     void SetRelPos(const Point& rP);
 };

include/svx/swframevalidation.hxx
@@ -18,16 +18,18 @@
  */
 #ifndef INCLUDED_SVX_SWFRAMEVALIDATION_HXX
 #define INCLUDED_SVX_SWFRAMEVALIDATION_HXX
+
 #include <sal/types.h>
 #include <tools/gen.hxx>
 #include <limits.h>
+#include <com/sun/star/text/TextContentAnchorType.hpp>
 
 /*
   struct to determine min/max values for fly frame positioning in Writer
  */
 struct SvxSwFrameValidation
 {
-    sal_Int16           nAnchorType; //css::text::TextContentAnchorType
+    css::text::TextContentAnchorType nAnchorType;
     sal_Int16           nHoriOrient; //css::text::HoriOrientation
     sal_Int16           nVertOrient; //css::text::VertOrientation
     sal_Int16           nHRelOrient; //css::text::RelOrientation
@@ -56,7 +58,7 @@ struct SvxSwFrameValidation
     Size    aPercentSize;   // Size for 100% value
 
     SvxSwFrameValidation() :
-        nAnchorType(0),
+        nAnchorType(css::text::TextContentAnchorType_AT_PARAGRAPH),
         nHoriOrient(0),
         nVertOrient(0),
         nHRelOrient(0),

svx/source/accessibility/DescriptionGenerator.cxx
@@ -339,7 +339,7 @@ void DescriptionGenerator::AddFillStyle (const OUString& sPropertyName,
                     case drawing::FillStyle_BITMAP:
                         sFillStyleName = SVX_RESSTR(RID_SVXSTR_A11Y_FILLSTYLE_BITMAP);
                         break;
-                    case drawing::FillStyle_MAKE_FIXED_SIZE:
+                    default:
                         break;
                 }
             }
@@ -366,7 +366,7 @@ void DescriptionGenerator::AddFillStyle (const OUString& sPropertyName,
                     AddProperty ("FillBitmapName", PropertyType::String, SIP_XA_FILLBITMAP,
                         XATTR_FILLBITMAP);
                     break;
-                case drawing::FillStyle_MAKE_FIXED_SIZE:
+                default:
                     break;
             }
         }

commit 673b76149b635ab26cd0694d365b76e18f9e944c

tdf#109242 fix save/load of form button alignment
Regression introduced by commit
02413c094f59e98712e2bb2a22937b27cc6bf4a6
"use actual UNO enums in svx"

Change-Id: Ie3bb025fa0ef52d2e746e4556cf29dbd3c3d55a9
Reviewed-on: https://gerrit.libreoffice.org/40416
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/unodraw/unoshap2.cxx
@@ -686,7 +686,7 @@ namespace
         for ( auto const & rEntry : aMapAdjustToAlign )
             if ( nValue == rEntry.nFormValue )
             {
-                _rValue <<= rEntry.nAPIValue;
+                _rValue <<= (sal_uInt16)rEntry.nAPIValue;
                 return;
             }
     }