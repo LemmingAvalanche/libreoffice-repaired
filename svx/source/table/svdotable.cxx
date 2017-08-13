LemmingAvalanche/libreoffice-repaired

commit 4ea9c915ad7dd93f230d1775ecb6d25edda22408

avoid repeated table layouting (fdo#75622)
With the document from fdo#75622, this saves 3775 calls and leaves only 13.
6210b287495688f743823eac585252aa32232690 removed avoiding the call
to LayoutTable(), which made loading slow. I checked that the doc from that
bugreport still works, so if very original code was correct in avoiding
the call sometimes, this should be ok too.

Change-Id: Ia80f974d4497e5cb04612331527eb87b579ddb76

svx/source/table/svdotable.cxx
@@ -242,9 +242,19 @@ public:
     void connectTableStyle();
     void disconnectTableStyle();
     virtual bool isInUse();
+private:
+    static SdrTableObjImpl* lastLayoutTable;
+    static Rectangle lastLayoutRectangle;
+    static bool lastLayoutFitWidth;
+    static bool lastLayoutFitHeight;
+    static WritingMode lastLayoutMode;
 };
 
-
+SdrTableObjImpl* SdrTableObjImpl::lastLayoutTable = NULL;
+Rectangle SdrTableObjImpl::lastLayoutRectangle;
+bool SdrTableObjImpl::lastLayoutFitWidth;
+bool SdrTableObjImpl::lastLayoutFitHeight;
+WritingMode SdrTableObjImpl::lastLayoutMode;
 
 SdrTableObjImpl::SdrTableObjImpl()
 : mpTableObj( 0 )
@@ -257,6 +267,8 @@ SdrTableObjImpl::SdrTableObjImpl()
 
 SdrTableObjImpl::~SdrTableObjImpl()
 {
+    if( lastLayoutTable == this )
+        lastLayoutTable = NULL;
 }
 
 
@@ -677,8 +689,21 @@ void SdrTableObjImpl::LayoutTable( Rectangle& rArea, bool bFitWidth, bool bFitHe
 {
     if( mpLayouter && mpTableObj->GetModel() )
     {
-        TableModelNotifyGuard aGuard( mxTable.get() );
-        mpLayouter->LayoutTable( rArea, bFitWidth, bFitHeight );
+        // Optimization: SdrTableObj::SetChanged() can call this very often, repeatedly
+        // with the same settings, noticeably increasing load time. Skip if already done.
+        WritingMode writingMode = mpTableObj->GetWritingMode();
+        if( lastLayoutTable != this || lastLayoutRectangle != rArea
+            || lastLayoutFitWidth != bFitWidth || lastLayoutFitHeight != bFitHeight
+            || lastLayoutMode != writingMode )
+        {
+            lastLayoutTable = this;
+            lastLayoutRectangle = rArea;
+            lastLayoutFitWidth = bFitWidth;
+            lastLayoutFitHeight = bFitHeight;
+            lastLayoutMode = writingMode;
+            TableModelNotifyGuard aGuard( mxTable.get() );
+            mpLayouter->LayoutTable( rArea, bFitWidth, bFitHeight );
+        }
     }
 }
 

commit 122b3161361dc8fa819f8bd9ede378a972ffbe2f

tdf#85909: EDITING: Native tables visualization not refreshed
... after changes to borders

Regression from:
4ea9c915ad7dd93f230d1775ecb6d25edda22408
"avoid repeated table layouting (fdo#75622)"

It was a performance change, but it was a bad idea to avoid
table layout refresh on this way

Anyway I added a call for border update even if not all
the table layouting is done. I tested with the test
document attached to fdo#75622, import time seems similar
so it does not cause perfromance issue to do that.

Change-Id: I7c6fcf105c89233512390dc2ecbd111a32f6779a
Reviewed-on: https://gerrit.libreoffice.org/41116
Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>

svx/source/table/svdotable.cxx
@@ -716,6 +716,7 @@ void SdrTableObjImpl::LayoutTable( tools::Rectangle& rArea, bool bFitWidth, bool
         else
         {
             rArea = lastLayoutResultRectangle;
+            mpLayouter->UpdateBorderLayout();
         }
     }
 }