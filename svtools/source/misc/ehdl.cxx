LemmingAvalanche/libreoffice-repaired

commit fb19718bc5348f07c231462b0878f3843f022306

convert ErrCode to strong typedef
would have preferred to re-use o3tl::strong_int, of which this
is a modified copy, but there are lots of convenience accessors
which are nice to define on the class.

Change-Id: I301b807aaf02fbced3bf75de1e1692cde6c0340a
Reviewed-on: https://gerrit.libreoffice.org/38497
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svtools/source/misc/ehdl.cxx
@@ -139,7 +139,7 @@ static DialogMask aWndFunc(
 }
 
 
-SfxErrorHandler::SfxErrorHandler(sal_uInt16 nIdP, sal_uLong lStartP, sal_uLong lEndP, ResMgr *pMgrP) :
+SfxErrorHandler::SfxErrorHandler(sal_uInt16 nIdP, ErrCode lStartP, ErrCode lEndP, ResMgr *pMgrP) :
 
     lStart(lStartP), lEnd(lEndP), nId(nIdP), pMgr(pMgrP), pFreeMgr( nullptr )
 
@@ -167,7 +167,7 @@ bool SfxErrorHandler::CreateString(const ErrorInfo *pErr, OUString &rStr) const
     */
 
 {
-    sal_uLong nErrCode = pErr->GetErrorCode() & ERRCODE_ERROR_MASK;
+    ErrCode nErrCode = pErr->GetErrorCode().IgnoreWarning();
     if( nErrCode>=lEnd || nErrCode<=lStart )
         return false;
     if(GetErrorString(nErrCode, rStr))
@@ -213,7 +213,7 @@ void SfxErrorHandler::GetClassString(sal_uLong lClassId, OUString &rStr)
     }
 }
 
-bool SfxErrorHandler::GetErrorString(sal_uLong lErrId, OUString &rStr) const
+bool SfxErrorHandler::GetErrorString(ErrCode lErrId, OUString &rStr) const
 
 /*  [Description]
 
@@ -229,7 +229,7 @@ bool SfxErrorHandler::GetErrorString(sal_uLong lErrId, OUString &rStr) const
     rStr = RID_ERRHDL_CLASS;
 
     ResStringArray aEr(ResId(nId, *pMgr));
-    sal_uInt32 nErrIdx = aEr.FindIndex((sal_uInt16)lErrId);
+    sal_uInt32 nErrIdx = aEr.FindIndex((sal_uInt16)(sal_uInt32)lErrId);
     if (nErrIdx != RESARRAY_INDEX_NOTFOUND)
     {
         rStr = rStr.replaceAll("$(ERROR)", aEr.GetString(nErrIdx));
@@ -241,7 +241,7 @@ bool SfxErrorHandler::GetErrorString(sal_uLong lErrId, OUString &rStr) const
     if( bRet )
     {
         OUString aErrStr;
-        GetClassString(lErrId & ERRCODE_CLASS_MASK,
+        GetClassString((sal_uInt32)lErrId & ERRCODE_CLASS_MASK,
                        aErrStr);
         if(!aErrStr.isEmpty())
             aErrStr += ".\n";
@@ -271,7 +271,7 @@ SfxErrorContext::SfxErrorContext(
 }
 
 
-bool SfxErrorContext::GetString(sal_uInt32 nErrId, OUString &rStr)
+bool SfxErrorContext::GetString(ErrCode nErrId, OUString &rStr)
 
 /*  [Description]
 
@@ -305,7 +305,7 @@ bool SfxErrorContext::GetString(sal_uInt32 nErrId, OUString &rStr)
 
         if ( bRet )
         {
-            sal_uInt16 nId = ( nErrId & ERRCODE_WARNING_MASK ) ? ERRCTX_WARNING : ERRCTX_ERROR;
+            sal_uInt16 nId = nErrId.IsWarning() ? ERRCTX_WARNING : ERRCTX_ERROR;
             ResStringArray aEr(ResId(RID_ERRCTX, *pMgr));
             rStr = rStr.replaceAll("$(ERR)", aEr.GetString(nId));
         }

uui/source/iahndl.cxx
@@ -486,7 +486,7 @@ UUIInteractionHelper::handleRequest_impl(
         {
             std::vector< OUString > aArguments;
             handleErrorHandlerRequest( aAppException.Classification,
-                                       aAppException.Code,
+                                       ErrCode(aAppException.Code),
                                        aArguments,
                                        rRequest->getContinuations(),
                                        bObtainErrorStringOnly,
@@ -725,7 +725,7 @@ UUIInteractionHelper::handleRequest_impl(
         task::ErrorCodeRequest aErrorCodeRequest;
         if (aAnyRequest >>= aErrorCodeRequest)
         {
-            handleGenericErrorRequest( aErrorCodeRequest.ErrCode,
+            handleGenericErrorRequest( ErrCode(aErrorCodeRequest.ErrCode),
                     rRequest->getContinuations(),
                     bObtainErrorStringOnly,
                     bHasErrorString,
@@ -736,7 +736,7 @@ UUIInteractionHelper::handleRequest_impl(
         task::ErrorCodeIOException aErrorCodeIOException;
         if (aAnyRequest >>= aErrorCodeIOException)
         {
-            handleGenericErrorRequest( aErrorCodeIOException.ErrCode,
+            handleGenericErrorRequest( ErrCode(aErrorCodeIOException.ErrCode),
                                        rRequest->getContinuations(),
                                        bObtainErrorStringOnly,
                                        bHasErrorString,
@@ -1075,7 +1075,7 @@ UUIInteractionHelper::handleNameClashResolveRequest(
 
 void
 UUIInteractionHelper::handleGenericErrorRequest(
-    sal_Int32 nErrorCode,
+    ErrCode nErrorCode,
     uno::Sequence< uno::Reference<
         task::XInteractionContinuation > > const & rContinuations,
     bool bObtainErrorStringOnly,
@@ -1101,8 +1101,8 @@ UUIInteractionHelper::handleGenericErrorRequest(
         // Note: It's important to convert the transported long to the
         // required  unsigned long value. Otherwhise using as flag field
         // can fail ...
-        ErrCode  nError   = static_cast< ErrCode >(nErrorCode);
-        bool bWarning = !ERRCODE_TOERROR(nError);
+        ErrCode  nError(nErrorCode);
+        bool bWarning = !nError.IgnoreWarning();
 
         if ( nError == ERRCODE_SFX_INCOMPLETE_ENCRYPTION )
         {
@@ -1274,7 +1274,7 @@ bool
 ErrorResource::getString(ErrCode nErrorCode, OUString &rString)
     const
 {
-    sal_uInt32 nIdx = m_aStringArray.FindIndex(nErrorCode & ERRCODE_RES_MASK);
+    sal_uInt32 nIdx = m_aStringArray.FindIndex(nErrorCode.GetRest());
     if (nIdx == RESARRAY_INDEX_NOTFOUND)
         return false;
     rString = m_aStringArray.GetString(nIdx);

commit 2a07dbb0d5539988fa1feff4c19c1f8830b1b11f

tdf#111696 No macro security dialog with High setting
Regression from commit fb19718bc5348f07c231462b0878f3843f022306
"convert ErrCode to strong typedef"

Why this is necessary I don't know. Only the change in ehdl.cxx is
necessary to fix the reported bug, bug fixing the other one too because
it operates in the same way.

I know that the other arrays that get passed here look like:

{ NC_("RID_UUI_ERRHDL", "....."),
ErrCode(sal_uInt32(ERRCODE_UUI_IO_ABORT) & ERRCODE_RES_MASK) },

but RID_ERRHDL in svtools/inc/errtxt.hrc does not, it looks like:

{ NC_("RID_ERRHDL", "...."),
ErrCode(ERRCODE_SFX_MACROS_SUPPORT_DISABLED) },

I'm guessing that somehow before my conversion somehow the code was
stripping the top bits off the error codes?

And why we need to strip the top half of the error code off at all is
also a mystery.

Change-Id: Iebca026ddf80e24ed74802410693adf90ea36047
Reviewed-on: https://gerrit.libreoffice.org/41172
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svtools/source/misc/ehdl.cxx
@@ -215,11 +215,9 @@ bool SfxErrorHandler::GetErrorString(ErrCode lErrId, OUString &rStr) const
     bool bRet = false;
     rStr = RID_ERRHDL_CLASS;
 
-    ErrCode nErrId((sal_uInt16)(sal_uInt32)lErrId);
-
     for (const ErrMsgCode* pItem = pIds; pItem->second; ++pItem)
     {
-        if (pItem->second == nErrId)
+        if (pItem->second.GetRest() == lErrId.GetRest())
         {
             rStr = rStr.replaceAll("$(ERROR)", Translate::get(pItem->first, rResLocale));
             bRet = true;

uui/source/iahndl.cxx
@@ -1255,7 +1255,7 @@ bool ErrorResource::getString(ErrCode nErrorCode, OUString &rString) const
 {
     for (const std::pair<const char*, ErrCode>* pStringArray = m_pStringArray; pStringArray->first != nullptr; ++pStringArray)
     {
-        if (ErrCode(nErrorCode.GetRest()) == pStringArray->second)
+        if (nErrorCode.GetRest() == pStringArray->second.GetRest())
         {
             rString = Translate::get(pStringArray->first, m_rResLocale);
             return true;