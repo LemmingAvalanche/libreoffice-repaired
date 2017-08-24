LemmingAvalanche/libreoffice-repaired

commit 6620f095f2a3b0150ef819ff2afbb61adf32bc74

Convert ErrCode area to scoped enum
Change-Id: Ifa365bbac3c7eb21da9381ec8bb3be6b6785016b
Reviewed-on: https://gerrit.libreoffice.org/41402
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svtools/source/misc/ehdl.cxx
@@ -139,7 +139,7 @@ static DialogMask aWndFunc(
     return nRet;
 }
 
-SfxErrorHandler::SfxErrorHandler(const ErrMsgCode* pIdPs, ErrCode lStartP, ErrCode lEndP, const std::locale& rLocale)
+SfxErrorHandler::SfxErrorHandler(const ErrMsgCode* pIdPs, ErrCodeArea lStartP, ErrCodeArea lEndP, const std::locale& rLocale)
     : lStart(lStartP), lEnd(lEndP), pIds(pIdPs), rResLocale(rLocale)
 {
     ErrorRegistry::RegisterDisplay(&aWndFunc);
@@ -159,7 +159,7 @@ bool SfxErrorHandler::CreateString(const ErrorInfo *pErr, OUString &rStr) const
 
 {
     ErrCode nErrCode(sal_uInt32(pErr->GetErrorCode()) & ERRCODE_ERROR_MASK);
-    if( nErrCode>=lEnd || nErrCode<=lStart )
+    if( pErr->GetErrorCode().GetArea() > lEnd || pErr->GetErrorCode().GetArea() <= lStart )
         return false;
     if(GetErrorString(nErrCode, rStr))
     {

include/basic/sbxdef.hxx
@@ -124,36 +124,36 @@ enum class SbxNameType {          // Type of the questioned name of a variable
 };
 
 // New error codes per define
-#define ERRCODE_SBX_SYNTAX              ErrCode(1UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_COMPILER)
-#define ERRCODE_SBX_NOTIMP              ErrCode(2UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_NOTSUPPORTED)
-#define ERRCODE_SBX_OVERFLOW            ErrCode(3UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_SBX)        // overflow
-#define ERRCODE_SBX_BOUNDS              ErrCode(4UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_SBX)        // Invalid array index
-#define ERRCODE_SBX_ZERODIV             ErrCode(5UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_SBX)        // Division by zero
-#define ERRCODE_SBX_CONVERSION          ErrCode(6UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_SBX)        // wrong data type
-#define ERRCODE_SBX_BAD_PARAMETER       ErrCode(7UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)    // invalid Parameter
-#define ERRCODE_SBX_PROC_UNDEFINED      ErrCode(8UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)    // Sub or Func not def
-#define ERRCODE_SBX_ERROR               ErrCode(9UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_UNKNOWN)    // generic object error
-#define ERRCODE_SBX_NO_OBJECT           ErrCode(10UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Object var not object
-#define ERRCODE_SBX_CANNOT_LOAD         ErrCode(11UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_CREATE)    // Object init/load fail
-#define ERRCODE_SBX_BAD_INDEX           ErrCode(12UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_SBX)       // Invalid object index
-#define ERRCODE_SBX_NO_ACTIVE_OBJECT    ErrCode(13UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_ACCESS)    // Object not active
-#define ERRCODE_SBX_BAD_PROP_VALUE      ErrCode(14UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Bad property value
-#define ERRCODE_SBX_PROP_READONLY       ErrCode(15UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_READ)      // Property is read only
-#define ERRCODE_SBX_PROP_WRITEONLY      ErrCode(16UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_WRITE)     // Property is write only
-#define ERRCODE_SBX_INVALID_OBJECT      ErrCode(17UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_ACCESS)    // Invalid object reference
-#define ERRCODE_SBX_NO_METHOD           ErrCode(18UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Property or Method unknown
-#define ERRCODE_SBX_INVALID_USAGE_OBJECT ErrCode(19UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_ACCESS)   // Invalid object usage
-#define ERRCODE_SBX_NO_OLE              ErrCode(20UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_ACCESS)    // No OLE-Object
-#define ERRCODE_SBX_BAD_METHOD          ErrCode(21UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Method not supported
-#define ERRCODE_SBX_OLE_ERROR           ErrCode(22UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // OLE Automation Error
-#define ERRCODE_SBX_BAD_ACTION          ErrCode(23UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_NOTSUPPORTED)  // Action not supported
-#define ERRCODE_SBX_NO_NAMED_ARGS       ErrCode(24UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // No named arguments
-#define ERRCODE_SBX_BAD_LOCALE          ErrCode(25UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_NOTSUPPORTED)  // Locale not supported
-#define ERRCODE_SBX_NAMED_NOT_FOUND     ErrCode(26UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Unknown named argument
-#define ERRCODE_SBX_NOT_OPTIONAL        ErrCode(27UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Argument not optional
-#define ERRCODE_SBX_WRONG_ARGS          ErrCode(28UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_SBX)       // Invalid number of arguments
-#define ERRCODE_SBX_NOT_A_COLL          ErrCode(29UL | ERRCODE_AREA_SBX | ERRCODE_CLASS_RUNTIME)   // Object contains no elements
-#define LAST_SBX_ERROR_ID                        29UL
+#define ERRCODE_SBX_SYNTAX               ErrCode( ErrCodeArea::Sbx,  1UL | ERRCODE_CLASS_COMPILER)
+#define ERRCODE_SBX_NOTIMP               ErrCode( ErrCodeArea::Sbx,  2UL | ERRCODE_CLASS_NOTSUPPORTED)
+#define ERRCODE_SBX_OVERFLOW             ErrCode( ErrCodeArea::Sbx,  3UL | ERRCODE_CLASS_SBX)        // overflow
+#define ERRCODE_SBX_BOUNDS               ErrCode( ErrCodeArea::Sbx,  4UL | ERRCODE_CLASS_SBX)        // Invalid array index
+#define ERRCODE_SBX_ZERODIV              ErrCode( ErrCodeArea::Sbx,  5UL | ERRCODE_CLASS_SBX)        // Division by zero
+#define ERRCODE_SBX_CONVERSION           ErrCode( ErrCodeArea::Sbx,  6UL | ERRCODE_CLASS_SBX)        // wrong data type
+#define ERRCODE_SBX_BAD_PARAMETER        ErrCode( ErrCodeArea::Sbx,  7UL | ERRCODE_CLASS_RUNTIME)    // invalid Parameter
+#define ERRCODE_SBX_PROC_UNDEFINED       ErrCode( ErrCodeArea::Sbx,  8UL | ERRCODE_CLASS_RUNTIME)    // Sub or Func not def
+#define ERRCODE_SBX_ERROR                ErrCode( ErrCodeArea::Sbx,  9UL | ERRCODE_CLASS_UNKNOWN)    // generic object error
+#define ERRCODE_SBX_NO_OBJECT            ErrCode( ErrCodeArea::Sbx, 10UL | ERRCODE_CLASS_RUNTIME)   // Object var not object
+#define ERRCODE_SBX_CANNOT_LOAD          ErrCode( ErrCodeArea::Sbx, 11UL | ERRCODE_CLASS_CREATE)    // Object init/load fail
+#define ERRCODE_SBX_BAD_INDEX            ErrCode( ErrCodeArea::Sbx, 12UL | ERRCODE_CLASS_SBX)       // Invalid object index
+#define ERRCODE_SBX_NO_ACTIVE_OBJECT     ErrCode( ErrCodeArea::Sbx, 13UL | ERRCODE_CLASS_ACCESS)    // Object not active
+#define ERRCODE_SBX_BAD_PROP_VALUE       ErrCode( ErrCodeArea::Sbx, 14UL | ERRCODE_CLASS_RUNTIME)   // Bad property value
+#define ERRCODE_SBX_PROP_READONLY        ErrCode( ErrCodeArea::Sbx, 15UL | ERRCODE_CLASS_READ)      // Property is read only
+#define ERRCODE_SBX_PROP_WRITEONLY       ErrCode( ErrCodeArea::Sbx, 16UL | ERRCODE_CLASS_WRITE)     // Property is write only
+#define ERRCODE_SBX_INVALID_OBJECT       ErrCode( ErrCodeArea::Sbx, 17UL | ERRCODE_CLASS_ACCESS)    // Invalid object reference
+#define ERRCODE_SBX_NO_METHOD            ErrCode( ErrCodeArea::Sbx, 18UL | ERRCODE_CLASS_RUNTIME)   // Property or Method unknown
+#define ERRCODE_SBX_INVALID_USAGE_OBJECT ErrCode( ErrCodeArea::Sbx, 19UL | ERRCODE_CLASS_ACCESS)   // Invalid object usage
+#define ERRCODE_SBX_NO_OLE               ErrCode( ErrCodeArea::Sbx, 20UL | ERRCODE_CLASS_ACCESS)    // No OLE-Object
+#define ERRCODE_SBX_BAD_METHOD           ErrCode( ErrCodeArea::Sbx, 21UL | ERRCODE_CLASS_RUNTIME)   // Method not supported
+#define ERRCODE_SBX_OLE_ERROR            ErrCode( ErrCodeArea::Sbx, 22UL | ERRCODE_CLASS_RUNTIME)   // OLE Automation Error
+#define ERRCODE_SBX_BAD_ACTION           ErrCode( ErrCodeArea::Sbx, 23UL | ERRCODE_CLASS_NOTSUPPORTED)  // Action not supported
+#define ERRCODE_SBX_NO_NAMED_ARGS        ErrCode( ErrCodeArea::Sbx, 24UL | ERRCODE_CLASS_RUNTIME)   // No named arguments
+#define ERRCODE_SBX_BAD_LOCALE           ErrCode( ErrCodeArea::Sbx, 25UL | ERRCODE_CLASS_NOTSUPPORTED)  // Locale not supported
+#define ERRCODE_SBX_NAMED_NOT_FOUND      ErrCode( ErrCodeArea::Sbx, 26UL | ERRCODE_CLASS_RUNTIME)   // Unknown named argument
+#define ERRCODE_SBX_NOT_OPTIONAL         ErrCode( ErrCodeArea::Sbx, 27UL | ERRCODE_CLASS_RUNTIME)   // Argument not optional
+#define ERRCODE_SBX_WRONG_ARGS           ErrCode( ErrCodeArea::Sbx, 28UL | ERRCODE_CLASS_SBX)       // Invalid number of arguments
+#define ERRCODE_SBX_NOT_A_COLL           ErrCode( ErrCodeArea::Sbx, 29UL | ERRCODE_CLASS_RUNTIME)   // Object contains no elements
+#define LAST_SBX_ERROR_ID                29UL
 
 // Flag-Bits:
 enum class SbxFlagBits {

include/editeng/editdata.hxx
@@ -56,7 +56,7 @@ enum class EEAnchorMode {
 EDITENG_DLLPUBLIC extern const size_t EE_APPEND;
 
 // Error messages for Read / Write Method
-#define EE_READWRITE_WRONGFORMAT     ErrCode(ERRCODE_AREA_SVX+1)
+#define EE_READWRITE_WRONGFORMAT     ErrCode(ErrCodeArea::Svx, 1)
 
 #define EDITUNDO_REMOVECHARS        100
 #define EDITUNDO_CONNECTPARAS       101

include/editeng/editerr.hxx
@@ -21,10 +21,9 @@
 
 #include <vcl/errcode.hxx>
 
-#define ERRCODE_SVX_LINGU_LANGUAGENOTEXISTS    ErrCode(2UL | ERRCODE_AREA_SVX)
+#define ERRCODE_SVX_LINGU_LANGUAGENOTEXISTS    ErrCode(ErrCodeArea::Svx, 2UL)
+#define ERRCODE_SVX_GRAPHIC_WRONG_FILEFORMAT   ErrCode(ErrCodeArea::Svx, 8UL | ERRCODE_CLASS_READ)
 
-#define ERRCODE_SVX_GRAPHIC_WRONG_FILEFORMAT   ErrCode(8UL | ERRCODE_AREA_SVX | \
-                                                ERRCODE_CLASS_READ )
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/svtools/ehdl.hxx
@@ -52,7 +52,7 @@ private:
 class SVT_DLLPUBLIC SfxErrorHandler : private ErrorHandler
 {
 public:
-    SfxErrorHandler(const ErrMsgCode* pIds, ErrCode lStart, ErrCode lEnd, const std::locale& rResLocale = SvtResLocale());
+    SfxErrorHandler(const ErrMsgCode* pIds, ErrCodeArea lStart, ErrCodeArea lEnd, const std::locale& rResLocale = SvtResLocale());
     virtual ~SfxErrorHandler() override;
 
 protected:
@@ -60,8 +60,8 @@ protected:
 
 private:
 
-    ErrCode              lStart;
-    ErrCode              lEnd;
+    ErrCodeArea          lStart;
+    ErrCodeArea          lEnd;
     const ErrMsgCode*    pIds;
     const std::locale&   rResLocale;
 

include/svtools/sfxecode.hxx
@@ -21,27 +21,28 @@
 
 #include <vcl/errcode.hxx>
 
-#define ERRCODE_SFX_NOTATEMPLATE            ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_FORMAT|2)
-#define ERRCODE_SFX_GENERAL                 ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_GENERAL|3)
-#define ERRCODE_SFX_DOLOADFAILED            ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_READ|4)
-#define ERRCODE_SFX_ALREADYOPEN             ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_ALREADYEXISTS|10)
-#define ERRCODE_SFX_WRONGPASSWORD           ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_READ|11)
-#define ERRCODE_SFX_DOCUMENTREADONLY        ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_WRITE|12)
-#define ERRCODE_SFX_OLEGENERAL              ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_NONE|14)
-#define ERRCODE_SFX_TEMPLATENOTFOUND        ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_NOTEXISTS|15)
+#define ERRCODE_SFX_NOTATEMPLATE            ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_FORMAT | 2)
+#define ERRCODE_SFX_GENERAL                 ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_GENERAL | 3)
+#define ERRCODE_SFX_DOLOADFAILED            ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_READ | 4)
+#define ERRCODE_SFX_ALREADYOPEN             ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_ALREADYEXISTS | 10)
+#define ERRCODE_SFX_WRONGPASSWORD           ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_READ | 11)
+#define ERRCODE_SFX_DOCUMENTREADONLY        ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_WRITE | 12)
+#define ERRCODE_SFX_OLEGENERAL              ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_NONE | 14)
+#define ERRCODE_SFX_TEMPLATENOTFOUND        ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_NOTEXISTS | 15)
 
-#define ERRCODE_SFX_CANTCREATECONTENT       ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_CREATE|21)
-#define ERRCODE_SFX_INVALIDSYNTAX           ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_PATH|36)
-#define ERRCODE_SFX_CANTCREATEFOLDER        ErrCode(ERRCODE_AREA_SFX|ERRCODE_CLASS_CREATE|37)
-#define ERRCODE_SFX_CANTGETPASSWD           ErrCode(ERRCODE_AREA_SFX| ERRCODE_CLASS_READ | 42)
-#define ERRCODE_SFX_NOMOREDOCUMENTSALLOWED  ErrCode(ERRCODE_WARNING_MASK | ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 44)
-#define ERRCODE_SFX_CANTCREATEBACKUP        ErrCode(ERRCODE_AREA_SFX | ERRCODE_CLASS_CREATE | 50)
-#define ERRCODE_SFX_MACROS_SUPPORT_DISABLED ErrCode(ERRCODE_WARNING_MASK | ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 51)
-#define ERRCODE_SFX_DOCUMENT_MACRO_DISABLED ErrCode(ERRCODE_WARNING_MASK | ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 52)
-#define ERRCODE_SFX_SHARED_NOPASSWORDCHANGE ErrCode(ERRCODE_WARNING_MASK | ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 54)
-#define ERRCODE_SFX_INCOMPLETE_ENCRYPTION    ErrCode(ERRCODE_WARNING_MASK | ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 55)
-#define ERRCODE_SFX_DOCUMENT_MACRO_DISABLED_MAC ErrCode(ERRCODE_WARNING_MASK | ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 56)
-#define ERRCODE_SFX_FORMAT_ROWCOL           ErrCode(ERRCODE_AREA_SFX | ERRCODE_CLASS_NONE | 57)
+#define ERRCODE_SFX_CANTCREATECONTENT       ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_CREATE | 21)
+#define ERRCODE_SFX_INVALIDSYNTAX           ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_PATH | 36)
+#define ERRCODE_SFX_CANTCREATEFOLDER        ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_CREATE | 37)
+#define ERRCODE_SFX_CANTGETPASSWD           ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_READ | 42)
+#define ERRCODE_SFX_NOMOREDOCUMENTSALLOWED  ErrCode(ErrCodeArea::Sfx, ERRCODE_WARNING_MASK | ERRCODE_CLASS_NONE | 44)
+#define ERRCODE_SFX_CANTCREATEBACKUP        ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_CREATE | 50)
+#define ERRCODE_SFX_MACROS_SUPPORT_DISABLED ErrCode(ErrCodeArea::Sfx, ERRCODE_WARNING_MASK | ERRCODE_CLASS_NONE | 51)
+#define ERRCODE_SFX_DOCUMENT_MACRO_DISABLED ErrCode(ErrCodeArea::Sfx, ERRCODE_WARNING_MASK | ERRCODE_CLASS_NONE | 52)
+#define ERRCODE_SFX_SHARED_NOPASSWORDCHANGE ErrCode(ErrCodeArea::Sfx, ERRCODE_WARNING_MASK | ERRCODE_CLASS_NONE | 54)
+#define ERRCODE_SFX_INCOMPLETE_ENCRYPTION   ErrCode(ErrCodeArea::Sfx, ERRCODE_WARNING_MASK | ERRCODE_CLASS_NONE | 55)
+#define ERRCODE_SFX_DOCUMENT_MACRO_DISABLED_MAC \
+                                            ErrCode(ErrCodeArea::Sfx, ERRCODE_WARNING_MASK | ERRCODE_CLASS_NONE | 56)
+#define ERRCODE_SFX_FORMAT_ROWCOL           ErrCode(ErrCodeArea::Sfx, ERRCODE_CLASS_NONE | 57)
 
 
 // Various

include/svtools/soerr.hxx
@@ -24,8 +24,8 @@
 #include <svtools/svtdllapi.h>
 
 // error codes
-#define SO_ERR(x) ErrCode(ERRCODE_AREA_SO | ERRCODE_CLASS_SO | x)
-#define SO_WRN(x) ErrCode(ERRCODE_AREA_SO | ERRCODE_CLASS_SO | ERRCODE_WARNING_MASK | x)
+#define SO_ERR(x) ErrCode(ErrCodeArea::So, ERRCODE_CLASS_SO | x)
+#define SO_WRN(x) ErrCode(ErrCodeArea::So, ERRCODE_CLASS_SO | ERRCODE_WARNING_MASK | x)
 
 #define ERRCODE_SO_GENERALERROR             SO_ERR(1)
 #define ERRCODE_SO_FALSE                    SO_WRN(23)

commit 4d59da32c8ea629a203c1d86d1d53f325c380476

svtools: fix checking of ErrCodeArea so error dialogs are shown again
Previously SfxErrorHandler::CreateString() compared the entire error
code so >=/<= worked but now only the ErrCodeArea is compared,
so only </> works.

(regression from 6620f095f2a3b0150ef819ff2afbb61adf32bc74)

Change-Id: I8ac46de154f93a7109ec29d899c71bfb470d008c

svtools/source/misc/ehdl.cxx
@@ -159,7 +159,7 @@ bool SfxErrorHandler::CreateString(const ErrorInfo *pErr, OUString &rStr) const
 
 {
     ErrCode nErrCode(sal_uInt32(pErr->GetErrorCode()) & ERRCODE_ERROR_MASK);
-    if( pErr->GetErrorCode().GetArea() > lEnd || pErr->GetErrorCode().GetArea() <= lStart )
+    if (pErr->GetErrorCode().GetArea() < lStart || lEnd < pErr->GetErrorCode().GetArea())
         return false;
     if(GetErrorString(nErrCode, rStr))
     {