LemmingAvalanche/libreoffice-repaired

commit 427a10e267186172541cf37c33d04a56d6da35cb

get rid of Svx...Locale...() double conversion nonsense
Change-Id: I21d14ff6087d1adb0ce769f2e8f8060a005250cc

editeng/source/items/numitem.cxx
@@ -100,9 +100,8 @@ SvxNumberType::~SvxNumberType()
 
 String SvxNumberType::GetNumStr( sal_uLong nNo ) const
 {
-    LanguageType eLang = Application::GetSettings().GetLanguageTag().getLanguageType();
-    Locale aLocale = SvxCreateLocale(eLang);
-    return GetNumStr( nNo, aLocale );
+    const LanguageTag& rLang = Application::GetSettings().GetLanguageTag();
+    return GetNumStr( nNo, rLang.getLocale() );
 }
 
 String  SvxNumberType::GetNumStr( sal_uLong nNo, const Locale& rLocale ) const
@@ -562,8 +561,6 @@ SvxNumRule::SvxNumRule( sal_uLong nFeatures,
       bContinuousNumbering(bCont)
 {
     ++nRefCount;
-    LanguageType eLang = Application::GetSettings().GetLanguageTag().getLanguageType();
-    aLocale = SvxCreateLocale(eLang);
     for(sal_uInt16 i = 0; i < SVX_MAX_NUM; i++)
     {
         if(i < nLevels)

commit 858e23d40b4619d4fdc74f98d7cfdf87de7ff935

remove unused aLocale from SvxNumRule
and fix usage site, regression from

    commit 427a10e267186172541cf37c33d04a56d6da35cb
    Date:   Sat Nov 24 18:54:09 2012 +0100
    get rid of Svx...Locale...() double conversion nonsense

Change-Id: Ic70abf5f52dce1db9cffd9c819e57f1a6c2a3df3

editeng/source/items/numitem.cxx
@@ -604,7 +604,6 @@ SvxNumRule::SvxNumRule( SvxNumRuleFlags nFeatures,
 SvxNumRule::SvxNumRule(const SvxNumRule& rCopy)
 {
     ++nRefCount;
-    aLocale             = rCopy.aLocale;
     nLevelCount          = rCopy.nLevelCount         ;
     nFeatureFlags        = rCopy.nFeatureFlags       ;
     bContinuousNumbering = rCopy.bContinuousNumbering;
@@ -853,7 +852,10 @@ OUString SvxNumRule::MakeNumString( const SvxNodeNum& rNum ) const
                 if( rNum.GetLevelVal()[ i ] )
                 {
                     if(SVX_NUM_BITMAP != rNFmt.GetNumberingType())
-                        aStr += rNFmt.GetNumStr( rNum.GetLevelVal()[ i ], aLocale );
+                    {
+                        const LanguageTag& rLang = Application::GetSettings().GetLanguageTag();
+                        aStr += rNFmt.GetNumStr( rNum.GetLevelVal()[ i ], rLang.getLocale()  );
+                    }
                     else
                         bDot = false;
                 }

include/editeng/numitem.hxx
@@ -248,7 +248,6 @@ class EDITENG_DLLPUBLIC SvxNumRule final
     bool                aFmtsSet[SVX_MAX_NUM]; // Flags indicating valid levels
 
     static sal_Int32    nRefCount;
-    css::lang::Locale aLocale;
 public:
     SvxNumRule( SvxNumRuleFlags nFeatures,
                 sal_uInt16 nLevels,