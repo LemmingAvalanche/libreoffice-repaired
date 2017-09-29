LemmingAvalanche/libreoffice-repaired

commit 6b807067133198221ce90792efa6a25e4956fd77

fdo#61429: Stylist: try a little harder to restore "hierarchical"
Change-Id: I30972224c5a9aa12c4ef4c2b5ca80e7aece0baad

sfx2/source/dialog/templdlg.cxx
@@ -1038,6 +1038,7 @@ void SfxCommonTemplateDialog_Impl::Initialize()
     ReadResource();
     pBindings->Invalidate( SID_STYLE_FAMILY );
     pBindings->Update( SID_STYLE_FAMILY );
+
     Update_Impl();
 
     aFilterLb.SetSelectHdl( LINK( this, SfxCommonTemplateDialog_Impl, FilterSelectHdl ) );
@@ -1045,8 +1046,10 @@ void SfxCommonTemplateDialog_Impl::Initialize()
     aFmtLb.SetSelectHdl( LINK( this, SfxCommonTemplateDialog_Impl, FmtSelectHdl ) );
     aFmtLb.SetSelectionMode(MULTIPLE_SELECTION);
 
+
     aFilterLb.Show();
-    aFmtLb.Show();
+    if (!bHierarchical)
+        aFmtLb.Show();
 }
 
 //-------------------------------------------------------------------------
@@ -1367,6 +1370,7 @@ void SfxCommonTemplateDialog_Impl::UpdateStyles_Impl(sal_uInt16 nFlags)
             if (pTreeBox || m_bWantHierarchical)
             {
                 aFilterLb.SelectEntry(SfxResId(STR_STYLE_FILTER_HIERARCHICAL).toString());
+                EnableHierarchical(true);
             }
 
             // show maximum 14 entries
@@ -1375,21 +1379,12 @@ void SfxCommonTemplateDialog_Impl::UpdateStyles_Impl(sal_uInt16 nFlags)
         }
         else
         {
-            if (m_bWantHierarchical)
-            {
-                nActFilter = 0;
-                aFilterLb.SelectEntry(SfxResId(
-                            STR_STYLE_FILTER_HIERARCHICAL).toString());
-            }
+            if (nActFilter < aFilterLb.GetEntryCount() - 1)
+                aFilterLb.SelectEntryPos(nActFilter + 1);
             else
             {
-                if (nActFilter < aFilterLb.GetEntryCount() - 1)
-                    aFilterLb.SelectEntryPos(nActFilter + 1);
-                else
-                {
-                    nActFilter = 0;
-                    aFilterLb.SelectEntryPos(1);
-                }
+                nActFilter = 0;
+                aFilterLb.SelectEntryPos(1);
             }
         }
 
@@ -1827,11 +1822,11 @@ sal_Bool SfxCommonTemplateDialog_Impl::Execute_Impl(
 //-------------------------------------------------------------------------
 
 // Handler der Listbox der Filter
-IMPL_LINK( SfxCommonTemplateDialog_Impl, FilterSelectHdl, ListBox *, pBox )
+void SfxCommonTemplateDialog_Impl::EnableHierarchical(bool const bEnable)
 {
-    if ( SfxResId(STR_STYLE_FILTER_HIERARCHICAL).toString().equals(pBox->GetSelectEntry()) )
+    if (bEnable)
     {
-        if ( !bHierarchical )
+        if (!bHierarchical)
         {
             // Turn on treeView
             bHierarchical=sal_True;
@@ -1862,7 +1857,6 @@ IMPL_LINK( SfxCommonTemplateDialog_Impl, FilterSelectHdl, ListBox *, pBox )
             pTreeBox->Show();
         }
     }
-
     else
     {
         DELETEZ(pTreeBox);
@@ -1870,8 +1864,20 @@ IMPL_LINK( SfxCommonTemplateDialog_Impl, FilterSelectHdl, ListBox *, pBox )
         // If bHierarchical, then the family can have changed
         // minus one since hierarchical is inserted at the start
         m_bWantHierarchical = sal_False; // before FilterSelect
-        FilterSelect(pBox->GetSelectEntryPos() - 1, bHierarchical );
+        FilterSelect(aFilterLb.GetSelectEntryPos() - 1, bHierarchical );
         bHierarchical=sal_False;
+    }
+}
+
+IMPL_LINK( SfxCommonTemplateDialog_Impl, FilterSelectHdl, ListBox *, pBox )
+{
+    if (SfxResId(STR_STYLE_FILTER_HIERARCHICAL).toString() == pBox->GetSelectEntry())
+    {
+        EnableHierarchical(true);
+    }
+    else
+    {
+        EnableHierarchical(false);
     }
 
     return 0;

sfx2/source/inc/templdgi.hxx
@@ -201,6 +201,7 @@ protected:
     void                        ResetFocus();
     void                        EnableDelete();
     void                        Initialize();
+    void                        EnableHierarchical(bool);
 
     void                FilterSelect( sal_uInt16 nFilterIdx, sal_Bool bForce = sal_False );
     void                SetFamilyState( sal_uInt16 nSlotId, const SfxTemplateItem* );

commit b28c08f71e7c7306b1c7f25a4178278a54f5ec1d

tdf#91106 sfx2: Stylist: sort top-level styles in hierarchical mode
(regression from 6b807067133198221ce90792efa6a25e4956fd77)

Change-Id: I111d12a622390afc02eb73a42b1d3c714f046bd2

sfx2/source/dialog/templdlg.cxx
@@ -558,6 +558,12 @@ StyleTreeArr_Impl& MakeTree_Impl(StyleTreeArr_Impl& rArr)
     // Only keep tree roots in rArr, child elements can be accessed through the hierarchy
     rArr.erase(std::remove_if(rArr.begin(), rArr.end(), [](StyleTree_Impl* pEntry) { return pEntry->HasParent(); }), rArr.end());
 
+    // tdf#91106 sort top level styles
+    std::sort(rArr.begin(), rArr.end(),
+        [&aSorter](StyleTree_Impl* pEntry1, StyleTree_Impl* pEntry2) {
+            return aSorter.compare(pEntry1->getName(), pEntry2->getName()) < 0;
+        });
+
     return rArr;
 }
 