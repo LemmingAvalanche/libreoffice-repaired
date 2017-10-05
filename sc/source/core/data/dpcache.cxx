LemmingAvalanche/libreoffice-repaired

commit 36327746bb81e46361c892ee992b77d0de647c31

tdf#107945: properly iterate over mtv during pivot cache loading.
This reduces the total time required for populating the pivot cache
by ~60%.

Change-Id: I6a8511959c20231a8a5dbd0b0a9a3d0930a1fa0c
Reviewed-on: https://gerrit.libreoffice.org/37971
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>

sc/source/core/data/dpcache.cxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <memory>
 #include "dpcache.hxx"
 
 #include "document.hxx"
@@ -30,6 +29,8 @@
 #include "dpitemdata.hxx"
 #include "dputil.hxx"
 #include "dpnumgroupinfo.hxx"
+#include <columniterator.hxx>
+#include <cellvalue.hxx>
 
 #include <rtl/math.hxx>
 #include <unotools/textsearch.hxx>
@@ -105,9 +106,10 @@ private:
     ScDocument* mpDoc;
 };
 
-OUString createLabelString(ScDocument* pDoc, SCCOL nCol, SCROW nRow, SCTAB nTab)
+OUString createLabelString( const ScDocument* pDoc, SCCOL nCol, const ScRefCellValue& rCell )
 {
-    OUString aDocStr = pDoc->GetString(nCol, nRow, nTab);
+    OUString aDocStr = rCell.getRawString(pDoc);
+
     if (aDocStr.isEmpty())
     {
         // Replace an empty label string with column name.
@@ -123,25 +125,24 @@ OUString createLabelString(ScDocument* pDoc, SCCOL nCol, SCROW nRow, SCTAB nTab)
 }
 
 void initFromCell(
-    ScDPCache& rCache, ScDocument* pDoc, SCCOL nCol, SCROW nRow, SCTAB nTab,
+    ScDPCache& rCache, ScDocument* pDoc, const ScAddress& rPos,
+    const ScRefCellValue& rCell,
     ScDPItemData& rData, sal_uInt32& rNumFormat)
 {
-    OUString aDocStr = pDoc->GetString(nCol, nRow, nTab);
+    OUString aDocStr = rCell.getRawString(pDoc);
     rNumFormat = 0;
 
-    ScAddress aPos(nCol, nRow, nTab);
-
-    if (pDoc->GetErrCode(aPos) != FormulaError::NONE)
+    if (rCell.hasError())
     {
         rData.SetErrorString(rCache.InternString(aDocStr));
     }
-    else if (pDoc->HasValueData(nCol, nRow, nTab))
+    else if (rCell.hasNumeric())
     {
-        double fVal = pDoc->GetValue(aPos);
-        rNumFormat = pDoc->GetNumberFormat(aPos);
+        double fVal = rCell.getRawValue();
+        rNumFormat = pDoc->GetNumberFormat(rPos);
         rData.SetValue(fVal);
     }
-    else if (pDoc->HasData(nCol, nRow, nTab))
+    else if (!rCell.isEmpty())
     {
         rData.SetString(rCache.InternString(aDocStr));
     }
@@ -322,21 +323,33 @@ void ScDPCache::InitFromDoc(ScDocument* pDoc, const ScRange& rRange)
 
     maLabelNames.reserve(mnColumnCount+1);
 
+    // Ensure that none of the formula cells in the data range are dirty.
+    pDoc->EnsureFormulaCellResults(rRange);
+
     ScDPItemData aData;
     for (sal_uInt16 nCol = nStartCol; nCol <= nEndCol; ++nCol)
     {
-        AddLabel(createLabelString(pDoc, nCol, nStartRow, nDocTab));
+        std::unique_ptr<sc::ColumnIterator> pIter =
+            pDoc->GetColumnIterator(nDocTab, nCol, nStartRow, nEndRow);
+        assert(pIter);
+        assert(pIter->hasCell());
+
+        AddLabel(createLabelString(pDoc, nCol, pIter->getCell()));
+        pIter->next();
+
         Field& rField = *maFields[nCol-nStartCol].get();
         std::vector<Bucket> aBuckets;
         aBuckets.reserve(nEndRow-nStartRow); // skip the topmost label cell.
 
         // Push back all original values.
-        SCROW nOffset = nStartRow + 1;
-        for (SCROW i = 0, n = nEndRow-nStartRow; i < n; ++i)
+        for (SCROW i = 0, n = nEndRow-nStartRow; i < n; ++i, pIter->next())
         {
-            SCROW nRow = i + nOffset;
+            assert(pIter->hasCell());
+
             sal_uInt32 nNumFormat = 0;
-            initFromCell(*this, pDoc, nCol, nRow, nDocTab, aData, nNumFormat);
+            ScAddress aPos(nCol, pIter->getRow(), nDocTab);
+            initFromCell(*this, pDoc, aPos, pIter->getCell(), aData, nNumFormat);
+
             aBuckets.push_back(Bucket(aData, i));
 
             if (!aData.IsEmpty())

sc/Library_sc.mk
@@ -157,6 +157,7 @@ $(eval $(call gb_Library_add_exception_objects,sc,\
     sc/source/core/data/markarr \
     sc/source/core/data/markdata \
     sc/source/core/data/markmulti \
+    sc/source/core/data/mtvcellfunc \
     sc/source/core/data/mtvelements \
     sc/source/core/data/olinetab \
     sc/source/core/data/pagepar \

sc/inc/cellvalue.hxx
@@ -132,18 +132,37 @@ struct SC_DLLPUBLIC ScRefCellValue
 
     bool hasNumeric() const;
 
+    bool hasError() const;
+
     double getValue();
 
-    /** Retrieve string value.
+    /**
+     * Retrieve a numeric value without modifying the states of any objects in
+     * the referenced document store.
+     */
+    double getRawValue() const;
 
-        @param  pDoc
-                Needed to resolve EditCells' field contents, obtain a
-                ScFieldEditEngine from that document. May be NULL if there is
-                no ScDocument in the calling context but then the document
-                specific fields can not be resolved. See
-                ScEditUtil::GetString().
+    /**
+     *  Retrieve string value.
+     *
+     *  Note that this method is NOT thread-safe.
+     *
+     *  @param  pDoc
+     *          Needed to resolve EditCells' field contents, obtain a
+     *          ScFieldEditEngine from that document. May be NULL if there is
+     *          no ScDocument in the calling context but then the document
+     *          specific fields can not be resolved. See
+     *          ScEditUtil::GetString().
      */
     OUString getString( const ScDocument* pDoc );
+
+    /**
+     * Retrieve a string value without modifying the states of any objects in
+     * the referenced document store.
+     *
+     * This method is thread-safe.
+     */
+    OUString getRawString( const ScDocument* pDoc ) const;
 
     bool isEmpty() const;
 

sc/inc/column.hxx
@@ -67,6 +67,7 @@ class CompileFormulaContext;
 struct SetFormulaDirtyContext;
 class RefMovedHint;
 enum class MatrixEdge;
+class ColumnIterator;
 
 }
 
@@ -665,6 +666,10 @@ public:
 
     void SwapNonEmpty(
         sc::TableValues& rValues, sc::StartListeningContext& rStartCxt, sc::EndListeningContext& rEndCxt );
+
+    std::unique_ptr<sc::ColumnIterator> GetColumnIterator( SCROW nRow1, SCROW nRow2 ) const;
+
+    void EnsureFormulaCellResults( SCROW nRow1, SCROW nRow2 );
 
 #if DUMP_COLUMN_STORAGE
     void DumpColumnStorage() const;

sc/inc/columniterator.hxx
@@ -49,6 +49,35 @@ private:
     void checkEndRow();
 };
 
+namespace sc {
+
+/**
+ * This iterator lets you iterate over cells over specified range in a
+ * single column.  It does not modify the state of the cells, and therefore
+ * is thread safe.
+ */
+class ColumnIterator
+{
+    CellStoreType::const_position_type maPos;
+    CellStoreType::const_position_type maPosEnd;
+
+public:
+    ColumnIterator( const CellStoreType& rCells, SCROW nRow1, SCROW nRow2 );
+    ~ColumnIterator();
+
+    void next();
+
+    SCROW getRow() const;
+
+    bool hasCell() const;
+
+    mdds::mtv::element_t getType() const;
+
+    ScRefCellValue getCell() const;
+};
+
+}
+
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/inc/document.hxx
@@ -85,6 +85,7 @@ class FormulaGroupAreaListener;
 class ColumnSet;
 class UpdatedRangeNames;
 class TableColumnBlockPositionSet;
+class ColumnIterator;
 
 }
 
@@ -2281,9 +2282,21 @@ public:
      */
     void PrepareFormulaCalc();
 
+    /**
+     * Make sure all of the formula cells in the specified range have been
+     * fully calculated.  This method only re-calculates those formula cells
+     * that have been flagged dirty.
+     *
+     * @param rRange range in which to potentially calculate the formula
+     *               cells.
+     */
+    void EnsureFormulaCellResults( const ScRange& rRange );
+
     SvtBroadcaster*         GetBroadcaster( const ScAddress& rPos );
     const SvtBroadcaster*   GetBroadcaster( const ScAddress& rPos ) const;
     void                    DeleteBroadcasters( sc::ColumnBlockPosition& rBlockPos, const ScAddress& rTopPos, SCROW nLength );
+
+    std::unique_ptr<sc::ColumnIterator> GetColumnIterator( SCTAB nTab, SCCOL nCol, SCROW nRow1, SCROW nRow2 ) const;
 
 #if DUMP_COLUMN_STORAGE
     SC_DLLPUBLIC void DumpColumnStorage( SCTAB nTab, SCCOL nCol ) const;

sc/inc/formulacell.hxx
@@ -298,6 +298,16 @@ public:
     bool            IsValueNoError() const;
     double          GetValue();
     svl::SharedString GetString();
+
+    /**
+     * Get a numeric value without potentially triggering re-calculation.
+     */
+    double GetRawValue() const;
+
+    /**
+     * Get a string value without potentially triggering re-calculation.
+     */
+    svl::SharedString GetRawString() const;
     const ScMatrix* GetMatrix();
     bool            GetMatrixOrigin( ScAddress& rPos ) const;
     void            GetResultDimensions( SCSIZE& rCols, SCSIZE& rRows );

sc/inc/mtvcellfunc.hxx
@@ -13,6 +13,10 @@
 #include "mtvelements.hxx"
 #include "mtvfunctions.hxx"
 
+#include <functional>
+
+class ScFormulaCell;
+
 namespace sc {
 
 template<typename Func>
@@ -31,6 +35,14 @@ ProcessFormula(
     return ProcessElements1<
         CellStoreType, formula_block, FuncElem, FuncElseNoOp<size_t> >(it, rStore, nRow1, nRow2, rFuncElem, aElse);
 }
+
+/**
+ * Process formula cells found within specified row range. This function
+ * allows modifications of the states of the formula function objects.
+ */
+CellStoreType::iterator ProcessFormula(
+    const CellStoreType::iterator& it, CellStoreType& rStore, SCROW nRow1, SCROW nRow2,
+    std::function<void(size_t,ScFormulaCell*)> aFuncElem );
 
 template<typename FuncElem, typename FuncElse>
 typename CellStoreType::iterator

commit 78c5ce15a2356ab9d75e93b5c426c5db2b0d70b4

tdf#112884: Error string is displayed as empty item in pivot table
Need to get the right string representation of error cells.

Based on the code it seems a regression from:
36327746bb81e46361c892ee992b77d0de647c31

Change-Id: I30001e63cd17ae68ba4988d5825de479a4496326
Reviewed-on: https://gerrit.libreoffice.org/43146
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

sc/source/core/data/dpcache.cxx
@@ -155,7 +155,7 @@ void initFromCell(
 
     if (rCell.hasError())
     {
-        rData.SetErrorStringInterned(internString(rStrPool, aDocStr));
+        rData.SetErrorStringInterned(internString(rStrPool, pDoc->GetString(rPos.Col(), rPos.Row(), rPos.Tab())));
     }
     else if (rCell.hasNumeric())
     {