LemmingAvalanche/libreoffice-repaired

commit fb9b11e21b6a34052c9f70dfec76c18c5ad10422

Resolves: ofz#2833 null deref
Change-Id: I021a716aa76d430a1d3c6fac2dddec4daa01e563
Reviewed-on: https://gerrit.libreoffice.org/40667
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

svl/source/numbers/zforscan.cxx
@@ -2412,17 +2412,20 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
                         nTypeArray[i] = NF_SYMBOLTYPE_DIGIT;
                         OUString& rStr = sStrArray[i];
                         i++;
-                        nPos = nPos + sStrArray[i].getLength();
-                        nCounter++;
-                        while (i < nAnzStrings &&
-                               sStrArray[i][0] == '0')
+                        if (i < nAnzStrings)
                         {
-                            rStr += sStrArray[i];
                             nPos = nPos + sStrArray[i].getLength();
-                            nTypeArray[i] = NF_SYMBOLTYPE_EMPTY;
-                            nAnzResStrings--;
                             nCounter++;
-                            i++;
+                            while (i < nAnzStrings &&
+                                   sStrArray[i][0] == '0')
+                            {
+                                rStr += sStrArray[i];
+                                nPos = nPos + sStrArray[i].getLength();
+                                nTypeArray[i] = NF_SYMBOLTYPE_EMPTY;
+                                nAnzResStrings--;
+                                nCounter++;
+                                i++;
+                            }
                         }
                     }
                     else

sc/qa/unit/data/slk/pass/numfmt.slk
@@ -1,0 +1,2 @@
+
+P;Pÿ                           s  ÿ0                                                                  0

commit e09cebb934dd3a8088f746cc6433ffd24f1af9aa

Resolves: tdf#112933 correct number of one decimal in fraction of seconds
Regression from

    commit fb9b11e21b6a34052c9f70dfec76c18c5ad10422
    Date:   Wed Aug 2 09:39:43 2017 +0100

        Resolves: ofz#2833 null deref

and similar for date+time

    commit 99d9099fa8128cf9c67ad3c7e973291b96e1a80f
    Date:   Sat Aug 5 19:15:36 2017 +0100

        ofz#2867 null deref

Effectively revert the code changes of those, leaving the test
case files intact, and "do the right thing" (switching two lines,
to

    nPos = nPos + sStrArray[i].getLength();
    i++;

nPos actually had always to be incremented by the length of the
*current* string position but was used after only as an error
position indicator, meaningless if there was no error.

Change-Id: I9bc4e5e56d70a275d26df0b72ed6a19e9297e0fa

svl/source/numbers/zforscan.cxx
@@ -2411,21 +2411,18 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
                         bDecSep = true;
                         nTypeArray[i] = NF_SYMBOLTYPE_DIGIT;
                         OUString& rStr = sStrArray[i];
+                        nPos = nPos + sStrArray[i].getLength();
+                        nCounter++;
                         i++;
-                        if (i < nStringsCnt)
+                        while (i < nStringsCnt &&
+                               sStrArray[i][0] == '0')
                         {
+                            rStr += sStrArray[i];
                             nPos = nPos + sStrArray[i].getLength();
+                            nTypeArray[i] = NF_SYMBOLTYPE_EMPTY;
+                            nResultStringsCnt--;
                             nCounter++;
-                            while (i < nStringsCnt &&
-                                   sStrArray[i][0] == '0')
-                            {
-                                rStr += sStrArray[i];
-                                nPos = nPos + sStrArray[i].getLength();
-                                nTypeArray[i] = NF_SYMBOLTYPE_EMPTY;
-                                nResultStringsCnt--;
-                                nCounter++;
-                                i++;
-                            }
+                            i++;
                         }
                     }
                     else
@@ -2564,21 +2561,18 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
                             bDecSep = true;
                             nTypeArray[i] = NF_SYMBOLTYPE_DIGIT;
                             OUString& rStr = sStrArray[i];
+                            nPos = nPos + sStrArray[i].getLength();
                             i++;
-                            if (i < nStringsCnt)
+                            nCounter++;
+                            while (i < nStringsCnt &&
+                                   sStrArray[i][0] == '0')
                             {
+                                rStr += sStrArray[i];
                                 nPos = nPos + sStrArray[i].getLength();
+                                nTypeArray[i] = NF_SYMBOLTYPE_EMPTY;
+                                nResultStringsCnt--;
                                 nCounter++;
-                                while (i < nStringsCnt &&
-                                       sStrArray[i][0] == '0')
-                                {
-                                    rStr += sStrArray[i];
-                                    nPos = nPos + sStrArray[i].getLength();
-                                    nTypeArray[i] = NF_SYMBOLTYPE_EMPTY;
-                                    nResultStringsCnt--;
-                                    nCounter++;
-                                    i++;
-                                }
+                                i++;
                             }
                         }
                         else