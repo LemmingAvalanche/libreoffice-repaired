LibreOffice/core

RotGrfFlyFrame: Minimal working rotation solution
This version allows rotation (in 10th degrees) and perserves
it over save/load cycles. Rotation of multiples of 90 degree
behave close to original except not changing the contained
Graphic and being adaptable to all kinds of graphic. The
rotated Graphic is displayed centered and under preserved
AspectRatio in the available frame space (so no rotation,
180 degree is identical, 90/-90 is identical with 1:1 ratio
of the graphic)

Change-Id: I54b3385f709ee0d34a55324aca919dcd2ce0c009

sw/source/core/doc/notxtfrm.cxx
@@ -773,16 +773,36 @@ void paintGraphicUsingPrimitivesHelper(vcl::RenderContext & rOutputDevice,
     // RotGrfFlyFrame: Take rotation into account. Rotation is in 10th degrees
     if(0 != rGraphicAttr.GetRotation())
     {
+        // Fit rotated graphic to center of available space, keeping page ratio:
+        // Adapt scaling ratio of unit object and rotate it
         const double fRotate(static_cast< double >(-rGraphicAttr.GetRotation()) * (M_PI/1800.0));
-        aTargetTransform.translate(-0.5, -0.5);
+        aTargetTransform.scale(1.0, aTargetRange.getHeight() / aTargetRange.getWidth());
         aTargetTransform.rotate(fRotate);
-        aTargetTransform.translate(0.5, 0.5);
-    }
 
-    // needed scale/translate
-    aTargetTransform *= basegfx::tools::createScaleTranslateB2DHomMatrix(
-        aTargetRange.getRange(),
-        aTargetRange.getMinimum());
+        // get the range to see where we are in unit coordinates
+        basegfx::B2DRange aFullRange(0.0, 0.0, 1.0, 1.0);
+        aFullRange.transform(aTargetTransform);
+
+        // detect needed scales in X/Y and choose the smallest for staying inside the
+        // available space while keeping aspect ratio of the source
+        const double fScaleX(aTargetRange.getWidth() / aFullRange.getWidth());
+        const double fScaleY(aTargetRange.getHeight() / aFullRange.getHeight());
+        const double fScaleMin(std::min(fScaleX, fScaleY));
+
+        // TopLeft to zero, then scale, then move to center of available space
+        aTargetTransform.translate(-aFullRange.getMinX(), -aFullRange.getMinY());
+        aTargetTransform.scale(fScaleMin, fScaleMin);
+        aTargetTransform.translate(
+            aTargetRange.getCenterX() - (0.5 * fScaleMin * aFullRange.getWidth()),
+            aTargetRange.getCenterY() - (0.5 * fScaleMin * aFullRange.getHeight()));
+    }
+    else
+    {
+        // just scale/translate needed
+        aTargetTransform *= basegfx::tools::createScaleTranslateB2DHomMatrix(
+            aTargetRange.getRange(),
+            aTargetRange.getMinimum());
+    }
 
     drawinglayer::primitive2d::Primitive2DContainer aContent(1);
     bool bDone(false);

LemmingAvalanche/libreoffice-repaired

sw/source/core/doc/notxtfrm.cxx
@@ -773,16 +773,36 @@ void paintGraphicUsingPrimitivesHelper(vcl::RenderContext & rOutputDevice,
     // RotGrfFlyFrame: Take rotation into account. Rotation is in 10th degrees
     if(0 != rGraphicAttr.GetRotation())
     {
+        // Fit rotated graphic to center of available space, keeping page ratio:
+        // Adapt scaling ratio of unit object and rotate it
         const double fRotate(static_cast< double >(-rGraphicAttr.GetRotation()) * (M_PI/1800.0));
-        aTargetTransform.translate(-0.5, -0.5);
+        aTargetTransform.scale(1.0, aTargetRange.getHeight() / aTargetRange.getWidth());
         aTargetTransform.rotate(fRotate);
-        aTargetTransform.translate(0.5, 0.5);
-    }
 
-    // needed scale/translate
-    aTargetTransform *= basegfx::utils::createScaleTranslateB2DHomMatrix(
-        aTargetRange.getRange(),
-        aTargetRange.getMinimum());
+        // get the range to see where we are in unit coordinates
+        basegfx::B2DRange aFullRange(0.0, 0.0, 1.0, 1.0);
+        aFullRange.transform(aTargetTransform);
+
+        // detect needed scales in X/Y and choose the smallest for staying inside the
+        // available space while keeping aspect ratio of the source
+        const double fScaleX(aTargetRange.getWidth() / aFullRange.getWidth());
+        const double fScaleY(aTargetRange.getHeight() / aFullRange.getHeight());
+        const double fScaleMin(std::min(fScaleX, fScaleY));
+
+        // TopLeft to zero, then scale, then move to center of available space
+        aTargetTransform.translate(-aFullRange.getMinX(), -aFullRange.getMinY());
+        aTargetTransform.scale(fScaleMin, fScaleMin);
+        aTargetTransform.translate(
+            aTargetRange.getCenterX() - (0.5 * fScaleMin * aFullRange.getWidth()),
+            aTargetRange.getCenterY() - (0.5 * fScaleMin * aFullRange.getHeight()));
+    }
+    else
+    {
+        // just scale/translate needed
+        aTargetTransform *= basegfx::utils::createScaleTranslateB2DHomMatrix(
+            aTargetRange.getRange(),
+            aTargetRange.getMinimum());
+    }
 
     drawinglayer::primitive2d::Primitive2DContainer aContent(1);
     bool bDone(false);