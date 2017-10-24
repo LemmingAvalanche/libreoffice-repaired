LemmingAvalanche/libreoffice-repaired

commit cfa39685602e9ca8f2dc2b5c0fabd240aeb2f623

use SvxExtNumType in SvxNumberType
and follow the rabbit trail, fixing various other types in the process

Change-Id: I8fc7df3b585df21578ffb28fdd7e5e427b3bfd9d
Reviewed-on: https://gerrit.libreoffice.org/34814
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/filter/eppt/pptx-text.cxx
@@ -765,7 +765,7 @@ void ParagraphObj::ImplClear()
 
 void ParagraphObj::CalculateGraphicBulletSize( sal_uInt16 nFontHeight )
 {
-    if ( ( (SvxExtNumType)nNumberingType == SVX_NUM_BITMAP ) && ( nBulletId != 0xffff ) )
+    if ( ( nNumberingType == SVX_NUM_BITMAP ) && ( nBulletId != 0xffff ) )
     {
         // calculate the bulletrealsize for this grafik
         if ( aBuGraSize.Width() && aBuGraSize.Height() )
@@ -821,7 +821,7 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider* pBuProv, sal_Int1
                 {
                     OUString aPropName( pPropValue[ i ].Name );
                     if ( aPropName == "NumberingType" )
-                        nNumberingType = *o3tl::doAccess<sal_Int16>(pPropValue[i].Value);
+                        nNumberingType = (SvxExtNumType)*o3tl::doAccess<sal_Int16>(pPropValue[i].Value);
                     else if ( aPropName == "Adjust" )
                         nHorzAdjust = *o3tl::doAccess<sal_Int16>(pPropValue[i].Value);
                     else if ( aPropName == "BulletChar" )
@@ -959,7 +959,7 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider* pBuProv, sal_Int1
                             else
                                 cBulletId = 0x2022;
 
-                            switch( (SvxExtNumType)nNumberingType )
+                            switch( nNumberingType )
                             {
                                 case SVX_NUM_CHARS_UPPER_LETTER :
                                 case SVX_NUM_CHARS_UPPER_LETTER_N :
@@ -1075,6 +1075,8 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider* pBuProv, sal_Int1
                                 default:
                                     break;
                             }
+                            break;
+                            default: break;
                         }
                         nParaFlags |= 0x2f;
                         nBulletFlags |= 6;

cui/source/inc/numpages.hxx
@@ -31,6 +31,7 @@
 #include <vcl/edit.hxx>
 #include <vcl/field.hxx>
 #include <editeng/numdef.hxx>
+#include <editeng/svxenum.hxx>
 #include <svtools/ctrlbox.hxx>
 #include <vcl/dialog.hxx>
 
@@ -65,14 +66,14 @@ class SvxNumberingPreview : public vcl::Window
 
 struct SvxNumSettings_Impl
 {
-    short           nNumberType;
+    SvxExtNumType   nNumberType;
     short           nParentNumbering;
     OUString   sPrefix;
     OUString   sSuffix;
     OUString   sBulletChar;
     OUString   sBulletFont;
     SvxNumSettings_Impl() :
-        nNumberType(0),
+        nNumberType(SVX_NUM_CHARS_UPPER_LETTER),
         nParentNumbering(0)
         {}
 };

cui/source/tabpages/numpages.cxx
@@ -111,7 +111,11 @@ static SvxNumSettings_Impl* lcl_CreateNumSettingsPtr(const Sequence<PropertyValu
     for(sal_Int32 j = 0; j < rLevelProps.getLength(); j++)
     {
         if ( pValues[j].Name == "NumberingType" )
-            pValues[j].Value >>= pNew->nNumberType;
+        {
+            sal_Int16 nTmp;
+            if (pValues[j].Value >>= nTmp)
+                pNew->nNumberType = (SvxExtNumType)nTmp;
+        }
         else if ( pValues[j].Name == "Prefix" )
             pValues[j].Value >>= pNew->sPrefix;
         else if ( pValues[j].Name == "Suffix" )
@@ -326,7 +330,7 @@ IMPL_LINK_NOARG(SvxSingleNumPickTabPage, NumSelectHdl_Impl, ValueSet*, void)
         if(aNumSettingsArr.size() <= nIdx)
             return;
         SvxNumSettings_Impl* _pSet = aNumSettingsArr[nIdx].get();
-        sal_Int16 eNewType = _pSet->nNumberType;
+        SvxExtNumType eNewType = _pSet->nNumberType;
         const sal_Unicode cLocalPrefix = !_pSet->sPrefix.isEmpty() ? _pSet->sPrefix[0] : 0;
         const sal_Unicode cLocalSuffix = !_pSet->sSuffix.isEmpty() ? _pSet->sSuffix[0] : 0;
 
@@ -945,7 +949,7 @@ IMPL_LINK_NOARG(SvxBitmapPickTabPage, NumSelectHdl_Impl, ValueSet*, void)
         sal_uInt16 nIdx = m_pExamplesVS->GetSelectItemId() - 1;
 
         sal_uInt16 nMask = 1;
-        sal_uInt16 nSetNumberingType = SVX_NUM_BITMAP;
+        SvxExtNumType nSetNumberingType = SVX_NUM_BITMAP;
         for(sal_uInt16 i = 0; i < pActNum->GetLevelCount(); i++)
         {
             if(nActNumLvl & nMask)
@@ -1780,8 +1784,8 @@ IMPL_LINK( SvxNumOptionsTabPage, NumberTypeSelectHdl_Impl, ListBox&, rBox, void 
         {
             SvxNumberFormat aNumFmt(pActNum->GetLevel(i));
             // PAGEDESC does not exist
-            sal_uInt16 nNumType = (sal_uInt16)reinterpret_cast<sal_uLong>(rBox.GetSelectEntryData());
-            aNumFmt.SetNumberingType((sal_Int16)nNumType);
+            SvxExtNumType nNumType = (SvxExtNumType)reinterpret_cast<sal_uLong>(rBox.GetSelectEntryData());
+            aNumFmt.SetNumberingType(nNumType);
             sal_uInt16 nNumberingType = aNumFmt.GetNumberingType();
             if(SVX_NUM_BITMAP == (nNumberingType&(~LINK_TOKEN)))
             {

editeng/source/items/numitem.cxx
@@ -78,7 +78,7 @@ static void lcl_getFormatter(css::uno::Reference<css::text::XNumberingFormatter>
     }
 }
 
-SvxNumberType::SvxNumberType(sal_Int16 nType) :
+SvxNumberType::SvxNumberType(SvxExtNumType nType) :
     nNumType(nType),
     bShowSymbol(true)
 {
@@ -129,7 +129,7 @@ OUString SvxNumberType::GetNumStr( sal_uLong nNo, const css::lang::Locale& rLoca
                         Sequence< PropertyValue > aProperties(2);
                         PropertyValue* pValues = aProperties.getArray();
                         pValues[0].Name = "NumberingType";
-                        pValues[0].Value <<= nNumType;
+                        pValues[0].Value <<= (sal_uInt16)nNumType;
                         pValues[1].Name = "Value";
                         pValues[1].Value <<= (sal_Int32)nNo;
 
@@ -147,7 +147,7 @@ OUString SvxNumberType::GetNumStr( sal_uLong nNo, const css::lang::Locale& rLoca
     return OUString();
 }
 
-SvxNumberFormat::SvxNumberFormat( sal_Int16 eType,
+SvxNumberFormat::SvxNumberFormat( SvxExtNumType eType,
                                   SvxNumPositionAndSpaceMode ePositionAndSpaceMode )
     : SvxNumberType(eType),
       eNumAdjust(SVX_ADJUST_LEFT),
@@ -190,7 +190,7 @@ SvxNumberFormat::SvxNumberFormat( SvStream &rStream )
     sal_Int32  nTmp32(0);
     rStream.ReadUInt16( nTmp16 ); // Version number
 
-    rStream.ReadUInt16( nTmp16 ); SetNumberingType( nTmp16 );
+    rStream.ReadUInt16( nTmp16 ); SetNumberingType( (SvxExtNumType)nTmp16 );
     rStream.ReadUInt16( nTmp16 ); eNumAdjust = ( SvxAdjust )nTmp16;
     rStream.ReadUInt16( nTmp16 ); nInclUpperLevels = nTmp16;
     rStream.ReadUInt16( nStart );
@@ -891,7 +891,7 @@ void SvxNumRule::UnLinkGraphics()
                 aFmt.SetGraphicBrush( &aTempItem, &aFmt.GetGraphicSize(), &eOrient );
             }
         }
-        else if((SVX_NUM_BITMAP|LINK_TOKEN) == aFmt.GetNumberingType())
+        else if((SVX_NUM_BITMAP|LINK_TOKEN) == (int)aFmt.GetNumberingType())
             aFmt.SetNumberingType(SVX_NUM_BITMAP);
         SetLevel(i, aFmt);
     }

editeng/source/uno/unonrule.cxx
@@ -178,7 +178,7 @@ Sequence<beans::PropertyValue> SvxUnoNumberingRules::getNumberingRuleByIndex(sal
 
     Any aVal;
     {
-        aVal <<= rFmt.GetNumberingType();
+        aVal <<= (sal_uInt16)rFmt.GetNumberingType();
         beans::PropertyValue aAlignProp( UNO_NAME_NRULE_NUMBERINGTYPE, -1, aVal, beans::PropertyState_DIRECT_VALUE);
         pArray[nIdx++] = aAlignProp;
     }
@@ -282,7 +282,7 @@ void SvxUnoNumberingRules::setNumberingRuleByIndex(const Sequence<beans::Propert
             // There is no reason to limit numbering types.
             if ( nSet>=0 )
             {
-                aFmt.SetNumberingType(nSet);
+                aFmt.SetNumberingType((SvxExtNumType)nSet);
                 continue;
             }
         }

filter/source/msfilter/svdfppt.cxx
@@ -3663,6 +3663,7 @@ void PPTNumberFormatCreator::GetNumberFormat( SdrPowerPointImport& rManager, Svx
             }
         }
         break;
+        default: break;
     }
 }
 
@@ -3723,6 +3724,7 @@ bool PPTNumberFormatCreator::GetNumberFormat( SdrPowerPointImport& rManager, Svx
                 }
             }
             break;
+            default: break;
         }
     }
     return nHardCount != 0;

include/editeng/numitem.hxx
@@ -55,19 +55,19 @@ class EDITENG_DLLPUBLIC SvxNumberType
     static sal_Int32 nRefCount;
     static css::uno::Reference<css::text::XNumberingFormatter> xFormatter;
 
-    sal_Int16       nNumType;
+    SvxExtNumType   nNumType;
     bool            bShowSymbol;        // Also show Symbol ?
 
 public:
-    explicit SvxNumberType(sal_Int16 nType = css::style::NumberingType::ARABIC);
+    explicit SvxNumberType(SvxExtNumType nType = SVX_NUM_ARABIC);
     SvxNumberType(const SvxNumberType& rType);
     ~SvxNumberType();
 
     OUString        GetNumStr( sal_uLong nNo ) const;
     OUString        GetNumStr( sal_uLong nNo, const css::lang::Locale& rLocale ) const;
 
-    void            SetNumberingType(sal_Int16 nSet) {nNumType = nSet;}
-    sal_Int16       GetNumberingType() const {return nNumType;}
+    void            SetNumberingType(SvxExtNumType nSet) {nNumType = nSet;}
+    SvxExtNumType   GetNumberingType() const {return nNumType;}
 
     void            SetShowSymbol(bool bSet) {bShowSymbol = bSet;}
     bool            IsShowSymbol()const{return bShowSymbol;}
@@ -96,8 +96,8 @@ public:
     };
 
 private:
-    OUString       sPrefix;
-    OUString       sSuffix;
+    OUString            sPrefix;
+    OUString            sSuffix;
 
     SvxAdjust           eNumAdjust;
 
@@ -125,13 +125,13 @@ private:
 
     // specifies what follows the list label before the text of the first line
     // of the list item starts
-    LabelFollowedBy       meLabelFollowedBy;
+    LabelFollowedBy     meLabelFollowedBy;
     // specifies an additional list tab stop position for meLabelFollowedBy = LISTTAB
-    long                        mnListtabPos;
+    long                mnListtabPos;
     // specifies the first line indent
-    long                        mnFirstLineIndent;
+    long                mnFirstLineIndent;
     // specifies the indent before the text, e.g. in L2R-layout the left margin
-    long                        mnIndentAt;
+    long                mnIndentAt;
 
     SvxBrushItem*       pGraphicBrush;
     sal_Int16           eVertOrient;        // vertical alignment of a bitmap
@@ -142,7 +142,7 @@ private:
     OUString            sCharStyleName;     // Character Style
 
 public:
-    explicit SvxNumberFormat( sal_Int16 nNumberingType,
+    explicit SvxNumberFormat( SvxExtNumType nNumberingType,
                      SvxNumPositionAndSpaceMode ePositionAndSpaceMode = LABEL_WIDTH_AND_POSITION );
     SvxNumberFormat(const SvxNumberFormat& rFormat);
     SvxNumberFormat( SvStream & rStream );
@@ -158,9 +158,9 @@ public:
     void            SetNumAdjust(SvxAdjust eSet) {eNumAdjust = eSet;}
     SvxAdjust       GetNumAdjust() const {return eNumAdjust;}
     void            SetPrefix(const OUString& rSet) { sPrefix = rSet;}
-    const OUString&   GetPrefix() const { return sPrefix;}
+    const OUString& GetPrefix() const { return sPrefix;}
     void            SetSuffix(const OUString& rSet) { sSuffix = rSet;}
-    const OUString&   GetSuffix() const { return sSuffix;}
+    const OUString& GetSuffix() const { return sSuffix;}
 
     void                    SetCharFormatName(const OUString& rSet){ sCharStyleName = rSet; }
     virtual OUString        GetCharFormatName()const;
@@ -170,14 +170,14 @@ public:
     void            SetBulletChar(sal_Unicode cSet){cBullet = cSet;}
     sal_Unicode     GetBulletChar()const {return cBullet;}
     void            SetBulletRelSize(sal_uInt16 nSet) {nBulletRelSize = nSet;}
-    sal_uInt16          GetBulletRelSize() const { return nBulletRelSize;}
+    sal_uInt16      GetBulletRelSize() const { return nBulletRelSize;}
     void            SetBulletColor(Color nSet){nBulletColor = nSet;}
     const Color&    GetBulletColor()const {return nBulletColor;}
 
     void            SetIncludeUpperLevels( sal_uInt8 nSet ) { nInclUpperLevels = nSet;}
-    sal_uInt8           GetIncludeUpperLevels()const  { return nInclUpperLevels;}
+    sal_uInt8       GetIncludeUpperLevels()const  { return nInclUpperLevels;}
     void            SetStart(sal_uInt16 nSet) {nStart = nSet;}
-    sal_uInt16          GetStart() const {return nStart;}
+    sal_uInt16      GetStart() const {return nStart;}
 
     virtual void    SetGraphicBrush( const SvxBrushItem* pBrushItem, const Size* pSize = nullptr, const sal_Int16* pOrient = nullptr);
     const SvxBrushItem*         GetBrush() const {return pGraphicBrush;}

include/editeng/svxenum.hxx
@@ -175,7 +175,11 @@ enum SvxExtNumType
     SVX_NUM_AIU_FULLWIDTH_JA      = css::style::NumberingType::AIU_FULLWIDTH_JA,
     SVX_NUM_AIU_HALFWIDTH_JA      = css::style::NumberingType::AIU_HALFWIDTH_JA,
     SVX_NUM_IROHA_FULLWIDTH_JA    = css::style::NumberingType::IROHA_FULLWIDTH_JA,
-    SVX_NUM_IROHA_HALFWIDTH_JA    = css::style::NumberingType::IROHA_HALFWIDTH_JA
+    SVX_NUM_IROHA_HALFWIDTH_JA    = css::style::NumberingType::IROHA_HALFWIDTH_JA,
+    SVX_NUM_HANGUL_SYLLABLE_KO    = css::style::NumberingType::HANGUL_SYLLABLE_KO,
+    SVX_NUM_HANGUL_JAMO_KO        = css::style::NumberingType::HANGUL_JAMO_KO,
+    SVX_NUM_NUMBER_HANGUL_KO      = css::style::NumberingType::NUMBER_HANGUL_KO,
+    SVX_NUM_NUMBER_UPPER_KO       = css::style::NumberingType::NUMBER_UPPER_KO,
 };
 
 #endif

commit fdf978aa3ad35b4be70f486363acad60e5fe61c1

tdf#113227 FILESAVE: PPT: bullets disappear after RT
regression from

	commit cfa39685602e9ca8f2dc2b5c0fabd240aeb2f623
	use SvxExtNumType in SvxNumberType

where I managed to put a "default: break" statement in the wrong place

Change-Id: Ic821a03208a75363c538c4641077b084f1e2d242
Reviewed-on: https://gerrit.libreoffice.org/43737
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/filter/eppt/pptx-text.cxx
@@ -1076,14 +1076,15 @@ void ParagraphObj::ImplGetNumberingLevel( PPTExBulletProvider* pBuProv, sal_Int1
                                 default:
                                     break;
                             }
-                            break;
-                            default: break;
                         }
                         nParaFlags |= 0x2f;
                         nBulletFlags |= 6;
                         if ( mbIsBullet && bNumberingIsNumber )
                             nBulletFlags |= 1;
+                        break;
                     }
+                    default:
+                        break;
                 }
             }
         }