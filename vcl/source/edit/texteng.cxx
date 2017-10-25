LemmingAvalanche/libreoffice-repaired

commit cd6b82704472a30e0bc22071f30dbcb8b51d76d4

tdf#112658: fix leak when calling TextEngine::SetAttrib
TextCharAttribList::RemoveAttrib lets a dangling pointer
when release unique_ptr obj maAttribs[n]
So retrieve a unique_ptr from the different layers until
SentenceEditWindow_Impl::ChangeMarkedWord (SpellDialog.cxx).

Change-Id: I734909dce86ec28d69c09b2a8c0fc4a6941f422a
Reviewed-on: https://gerrit.libreoffice.org/42881
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Stahl <mstahl@redhat.com>

vcl/source/edit/texteng.cxx
@@ -2504,8 +2504,9 @@ void TextEngine::RemoveAttribs( sal_uInt32 nPara, sal_uInt16 nWhich )
     }
 }
 
-void TextEngine::RemoveAttrib( sal_uInt32 nPara, const TextCharAttrib& rAttrib )
+std::unique_ptr<TextCharAttrib> TextEngine::RemoveAttrib( sal_uInt32 nPara, const TextCharAttrib& rAttrib )
 {
+    std::unique_ptr<TextCharAttrib> pRet;
     if ( nPara < mpDoc->GetNodes().size() )
     {
         TextNode* pNode = mpDoc->GetNodes()[ nPara ];
@@ -2516,7 +2517,7 @@ void TextEngine::RemoveAttrib( sal_uInt32 nPara, const TextCharAttrib& rAttrib )
             {
                 if(&(rAttribs.GetAttrib( nAttr - 1 )) == &rAttrib)
                 {
-                    rAttribs.RemoveAttrib( nAttr -1 );
+                    pRet = rAttribs.RemoveAttrib( nAttr -1 );
                     break;
                 }
             }
@@ -2526,6 +2527,7 @@ void TextEngine::RemoveAttrib( sal_uInt32 nPara, const TextCharAttrib& rAttrib )
             FormatAndUpdate();
         }
     }
+    return pRet;
 }
 
 void TextEngine::SetAttrib( const TextAttrib& rAttr, sal_uInt32 nPara, sal_Int32 nStart, sal_Int32 nEnd, bool bIdleFormatAndUpdate )

cui/source/dialogs/SpellDialog.cxx
@@ -1643,11 +1643,14 @@ void SentenceEditWindow_Impl::ChangeMarkedWord(const OUString& rNewWord, Languag
     ExtTextEngine* pTextEngine = GetTextEngine();
     pTextEngine->UndoActionStart();
     const TextCharAttrib*  pErrorAttrib = pTextEngine->FindCharAttrib( TextPaM(0, m_nErrorStart), TEXTATTR_SPELL_ERROR );
+    std::unique_ptr<TextCharAttrib> pReleasedErrorAttrib;
+    std::unique_ptr<TextCharAttrib> pReleasedLangAttrib;
+    std::unique_ptr<TextCharAttrib> pReleasedBackAttrib;
     DBG_ASSERT(pErrorAttrib, "no error attribute found");
     const SpellErrorDescription* pSpellErrorDescription = nullptr;
     if(pErrorAttrib)
     {
-        pTextEngine->RemoveAttrib(0, *pErrorAttrib);
+        pReleasedErrorAttrib = pTextEngine->RemoveAttrib(0, *pErrorAttrib);
         pSpellErrorDescription = &static_cast<const SpellErrorAttrib&>(pErrorAttrib->GetAttr()).GetErrorDescription();
     }
     const TextCharAttrib*  pBackAttrib = pTextEngine->FindCharAttrib( TextPaM(0, m_nErrorStart), TEXTATTR_SPELL_BACKGROUND );
@@ -1666,7 +1669,7 @@ void SentenceEditWindow_Impl::ChangeMarkedWord(const OUString& rNewWord, Languag
             nTextLen)
         {
             SpellLanguageAttrib aNewLangAttrib( static_cast<const SpellLanguageAttrib&>(pLangAttrib->GetAttr()).GetLanguage());
-            pTextEngine->RemoveAttrib(0, *pLangAttrib);
+            pReleasedLangAttrib = pTextEngine->RemoveAttrib(0, *pLangAttrib);
             pTextEngine->SetAttrib( aNewLangAttrib, 0, m_nErrorEnd + nDiffLen, nTextLen );
         }
     }
@@ -1675,7 +1678,7 @@ void SentenceEditWindow_Impl::ChangeMarkedWord(const OUString& rNewWord, Languag
     {
         std::unique_ptr<TextAttrib> pNewBackground(pBackAttrib->GetAttr().Clone());
         const sal_Int32 nStart = pBackAttrib->GetStart();
-        pTextEngine->RemoveAttrib(0, *pBackAttrib);
+        pReleasedBackAttrib = pTextEngine->RemoveAttrib(0, *pBackAttrib);
         pTextEngine->SetAttrib(*pNewBackground, 0, nStart, m_nErrorStart);
     }
     pTextEngine->SetModified(true);

include/vcl/texteng.hxx
@@ -297,7 +297,7 @@ public:
     const TextCharAttrib*   FindCharAttrib( const TextPaM& rPaM, sal_uInt16 nWhich ) const;
 
     void                RemoveAttribs( sal_uInt32 nPara, sal_uInt16 nWhich );
-    void                RemoveAttrib( sal_uInt32 nPara, const TextCharAttrib& rAttrib );
+    std::unique_ptr<TextCharAttrib>     RemoveAttrib( sal_uInt32 nPara, const TextCharAttrib& rAttrib );
     void                RemoveAttribs( sal_uInt32 nPara );
     void                SetAttrib( const TextAttrib& rAttr, sal_uInt32 nPara, sal_Int32 nStart, sal_Int32 nEnd, bool bIdleFormatAndUpdate = true );
 

vcl/source/edit/textdoc.hxx
@@ -44,7 +44,12 @@ public:
 
     const TextCharAttrib& GetAttrib( sal_uInt16 n ) const { return *maAttribs[n].get(); }
     TextCharAttrib& GetAttrib( sal_uInt16 n )       { return *maAttribs[n].get(); }
-    void            RemoveAttrib( sal_uInt16 n )    { maAttribs[n].release(); maAttribs.erase( maAttribs.begin() + n ); }
+    std::unique_ptr<TextCharAttrib>  RemoveAttrib( sal_uInt16 n )
+    {
+        std::unique_ptr<TextCharAttrib> pReleased = std::move(maAttribs[n]);
+        maAttribs.erase( maAttribs.begin() + n );
+        return pReleased;
+    }
 
     void            InsertAttrib( TextCharAttrib* pAttrib );
 

commit e4ce3745bf9737561243163f914ceba123687109

tdf#113400: fix crash by destroying returned unique_ptr
Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=cd6b82704472a30e0bc22071f30dbcb8b51d76d4
tdf#112658: fix leak when calling TextEngine::SetAttrib

Change-Id: I4f1edf41e11f3cdfda6071b30a84372db68cd59d
Reviewed-on: https://gerrit.libreoffice.org/43795
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

vcl/source/edit/texteng.cxx
@@ -2494,7 +2494,10 @@ void TextEngine::RemoveAttribs( sal_uInt32 nPara, sal_uInt16 nWhich )
             for(sal_uInt16 nAttr = rAttribs.Count(); nAttr; --nAttr)
             {
                 if(rAttribs.GetAttrib( nAttr - 1 ).Which() == nWhich)
-                    rAttribs.RemoveAttrib( nAttr -1 );
+                {
+                    // tdf#113400 destroy unique_ptr returned
+                    rAttribs.RemoveAttrib( nAttr -1 ).reset();
+                }
             }
             TEParaPortion* pTEParaPortion = mpTEParaPortions->GetObject( nPara );
             pTEParaPortion->MarkSelectionInvalid( 0 );