LemmingAvalanche/libreoffice-repaired

commit 284fb714cf921da2b8636b62f56940e800136546

gpg4libre: enable OpenPGP support for Mac
Change-Id: Iad7d6b045be90efb32da3ffb1c19753b58b1753d
Reviewed-on: https://gerrit.libreoffice.org/43616
Reviewed-by: Siegmund Gorr <siegmund.gorr@cib.de>
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

RepositoryExternal.mk
@@ -3467,7 +3467,7 @@ $(call gb_LinkTarget_use_package,$(1),gpgme)
 
 endef
 
-ifeq ($(OS),LINUX)
+ifneq ($(filter-out MACOSX LINUX,$(OS)),)
 
 $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	gpgme \

configure.ac
@@ -10233,7 +10233,7 @@ fi
 AC_SUBST(ENABLE_PDFIUM)
 
 SYSTEM_GPGMEPP=
-if test "$_os" = "Linux"; then
+if test "$_os" = "Linux" -o "$_os" = "Darwin"; then
     dnl ===================================================================
     dnl Check for system gpgme
     dnl ===================================================================

external/gpgme/ExternalPackage_gpgme.mk
@@ -13,8 +13,17 @@ $(eval $(call gb_ExternalPackage_use_external_project,gpgme,gpgme))
 
 ifneq ($(DISABLE_DYNLOADING),TRUE)
 
+ifeq ($(OS),LINUX)
+
 $(eval $(call gb_ExternalPackage_add_file,gpgme,$(LIBO_LIB_FOLDER)/libgpgmepp.so.6,lang/cpp/src/.libs/libgpgmepp.so.6.4.0))
 $(eval $(call gb_ExternalPackage_add_file,gpgme,$(LIBO_LIB_FOLDER)/libgpgme.so.11,src/.libs/libgpgme.so.11.18.0))
+
+else
+
+$(eval $(call gb_ExternalPackage_add_file,gpgme,$(LIBO_LIB_FOLDER)/libgpgmepp.6.dylib,lang/cpp/src/.libs/libgpgmepp.6.dylib))
+$(eval $(call gb_ExternalPackage_add_file,gpgme,$(LIBO_LIB_FOLDER)/libgpgme.11.dylib,src/.libs/libgpgme.11.dylib))
+
+endif
 
 endif # $(DISABLE_DYNLOADING)
 

external/libassuan/ExternalPackage_libassuan.mk
@@ -13,7 +13,15 @@ $(eval $(call gb_ExternalPackage_use_external_project,libassuan,libassuan))
 
 ifneq ($(DISABLE_DYNLOADING),TRUE)
 
+ifeq ($(OS),LINUX)
+
 $(eval $(call gb_ExternalPackage_add_file,libassuan,$(LIBO_LIB_FOLDER)/libassuan.so.0,src/.libs/libassuan.so.0.7.3))
+
+else
+
+$(eval $(call gb_ExternalPackage_add_file,libassuan,$(LIBO_LIB_FOLDER)/libassuan.0.dylib,src/.libs/libassuan.0.dylib))
+
+endif
 
 endif # $(DISABLE_DYNLOADING)
 

external/libgpg-error/ExternalPackage_libgpg-error.mk
@@ -13,7 +13,16 @@ $(eval $(call gb_ExternalPackage_use_external_project,libgpg-error,libgpg-error)
 
 ifneq ($(DISABLE_DYNLOADING),TRUE)
 
+ifeq ($(OS),LINUX)
+
 $(eval $(call gb_ExternalPackage_add_file,libgpg-error,$(LIBO_LIB_FOLDER)/libgpg-error.so.0,src/.libs/libgpg-error.so.0.22.0))
+
+else
+
+$(eval $(call gb_ExternalPackage_add_file,libgpg-error,$(LIBO_LIB_FOLDER)/libgpg-error.0.dylib,src/.libs/libgpg-error.0.dylib))
+
+
+endif
 
 endif # $(DISABLE_DYNLOADING)
 

commit bcd98fea245a09c0d6cb0bc2c02a976259c5e7ea

RepositoryExternal.mk: fix inverted gpg condition
(regression from 284fb714cf921da2b8636b62f56940e800136546)

Change-Id: Ic621979926a9629aa8d8fb95a80957850ae0661b

RepositoryExternal.mk
@@ -3467,7 +3467,7 @@ $(call gb_LinkTarget_use_package,$(1),gpgme)
 
 endef
 
-ifneq ($(filter-out MACOSX LINUX,$(OS)),)
+ifneq ($(filter MACOSX LINUX,$(OS)),)
 
 $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	gpgme \