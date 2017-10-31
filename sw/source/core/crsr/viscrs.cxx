LibreOffice/core

RotateFlyFrame2: Unified FrameAreaDefinition
Isolated all Frame AreaDefinition and it's layout flags to
SwFrameAreaDefinition class which is now base for SwFrame.
Adapted calls to get/set and WriteAccess hekper classes accordingly.
This allows much deeper understanding what Writer is doing when
layouting it's frames and needed when reaction on such changes
is necessary

Change-Id: I96220a1d140e69c76cc63023aae26e4ed17f3504

sw/source/core/crsr/viscrs.cxx
@@ -779,7 +779,7 @@ void SwShellTableCursor::FillRects()
     bool bStart = true;
     SwRegionRects aReg( GetShell()->VisArea() );
     if (comphelper::LibreOfficeKit::isActive())
-        aReg = GetShell()->getIDocumentLayoutAccess().GetCurrentLayout()->getSwFrame();
+        aReg = GetShell()->getIDocumentLayoutAccess().GetCurrentLayout()->geFrameArea();
     SwNodes& rNds = GetDoc()->GetNodes();
     SwFrame* pEndFrame = nullptr;
     for (size_t n = 0; n < m_SelectedBoxes.size(); ++n)
@@ -812,13 +812,13 @@ void SwShellTableCursor::FillRects()
 
         while ( pFrame )
         {
-            if( aReg.GetOrigin().IsOver( pFrame->getSwFrame() ) )
+            if( aReg.GetOrigin().IsOver( pFrame->geFrameArea() ) )
             {
-                aReg -= pFrame->getSwFrame();
+                aReg -= pFrame->geFrameArea();
                 if (bStart)
                 {
                     bStart = false;
-                    m_aStart = SwRect(pFrame->getSwFrame().Left(), pFrame->getSwFrame().Top(), 1, pFrame->getSwFrame().Height());
+                    m_aStart = SwRect(pFrame->geFrameArea().Left(), pFrame->geFrameArea().Top(), 1, pFrame->geFrameArea().Height());
                 }
             }
 
@@ -827,7 +827,7 @@ void SwShellTableCursor::FillRects()
         }
     }
     if (pEndFrame)
-        m_aEnd = SwRect(pEndFrame->getSwFrame().Right(), pEndFrame->getSwFrame().Top(), 1, pEndFrame->getSwFrame().Height());
+        m_aEnd = SwRect(pEndFrame->geFrameArea().Right(), pEndFrame->geFrameArea().Top(), 1, pEndFrame->geFrameArea().Height());
     aReg.Invert();
     insert( begin(), aReg.begin(), aReg.end() );
 }
@@ -857,7 +857,7 @@ bool SwShellTableCursor::IsInside( const Point& rPt ) const
         while( pFrame && !pFrame->IsCellFrame() )
             pFrame = pFrame->GetUpper();
         OSL_ENSURE( pFrame, "Node not in a table" );
-        if( pFrame && pFrame->getSwFrame().IsInside( rPt ) )
+        if( pFrame && pFrame->geFrameArea().IsInside( rPt ) )
             return true;
     }
     return false;

sw/source/core/doc/docfly.cxx
@@ -223,7 +223,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
             {
                 const SwFrame* pOld = static_cast<const SwFlyFrameFormat*>(pFlyFormat)->GetFrame( &aRet );
                 if( pOld )
-                    aRet = pOld->getSwFrame().Pos();
+                    aRet = pOld->geFrameArea().Pos();
             }
             break;
 
@@ -235,7 +235,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
                 const SwContentNode* pNd = pPos->nNode.GetNode().GetContentNode();
                 const SwFrame* pOld = pNd ? pNd->getLayoutFrame( rDoc.getIDocumentLayoutAccess().GetCurrentLayout(), &aRet, nullptr, false ) : nullptr;
                 if( pOld )
-                    aRet = pOld->getSwFrame().Pos();
+                    aRet = pOld->geFrameArea().Pos();
             }
             break;
 
@@ -246,7 +246,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
                                                 nNode.GetNode().GetFlyFormat());
                 const SwFrame* pOld = pFormat ? pFormat->GetFrame( &aRet ) : nullptr;
                 if( pOld )
-                    aRet = pOld->getSwFrame().Pos();
+                    aRet = pOld->geFrameArea().Pos();
             }
             break;
 
@@ -258,7 +258,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
                                     pPage =static_cast<const SwPageFrame*>(pPage->GetNext()) )
                     if( i == nPgNum )
                     {
-                        aRet = pPage->getSwFrame().Pos();
+                        aRet = pPage->geFrameArea().Pos();
                         break;
                     }
             }
@@ -840,7 +840,7 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
             case RndStdIds::FLY_AT_PAGE:
                 {
                     pNewAnchorFrame = getIDocumentLayoutAccess().GetCurrentLayout()->Lower();
-                    while ( pNewAnchorFrame && !pNewAnchorFrame->getSwFrame().IsInside( aPt ) )
+                    while ( pNewAnchorFrame && !pNewAnchorFrame->geFrameArea().IsInside( aPt ) )
                         pNewAnchorFrame = pNewAnchorFrame->GetNext();
                     if ( !pNewAnchorFrame )
                         continue;
@@ -873,7 +873,7 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
                     aPoint.setX(aPoint.getX() - 1);    // Do not load in the DrawObj!
                     aNewAnch.SetType( RndStdIds::FLY_AS_CHAR );
                     SwPosition aPos( *static_cast<const SwContentFrame*>(pNewAnchorFrame)->GetNode() );
-                    if ( pNewAnchorFrame->getSwFrame().IsInside( aPoint ) )
+                    if ( pNewAnchorFrame->geFrameArea().IsInside( aPoint ) )
                     {
                     // We need to find a TextNode, because only there we can anchor a
                     // content-bound DrawObject.
@@ -884,7 +884,7 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
                     {
                         SwContentNode &rCNd = const_cast<SwContentNode&>(
                             *static_cast<const SwContentFrame*>(pNewAnchorFrame)->GetNode());
-                        if ( pNewAnchorFrame->getSwFrame().Bottom() < aPt.Y() )
+                        if ( pNewAnchorFrame->geFrameArea().Bottom() < aPt.Y() )
                             rCNd.MakeStartIndex( &aPos.nContent );
                         else
                             rCNd.MakeEndIndex( &aPos.nContent );
@@ -1096,7 +1096,7 @@ SwChainRet SwDoc::Chain( SwFrameFormat &rSource, const SwFrameFormat &rDest )
         {
             SwFlyFrame *pFly = SwIterator<SwFlyFrame,SwFormat>( rSource ).First();
             if ( pFly )
-                aSize.SetHeight( pFly->getSwFrame().Height() );
+                aSize.SetHeight( pFly->geFrameArea().Height() );
             aSize.SetHeightSizeType( ATT_FIX_SIZE );
             aSet.Put( aSize );
         }

sw/source/core/layout/anchoredobject.cxx
@@ -163,12 +163,12 @@ void SwAnchoredObject::SetPageFrame( SwPageFrame* _pNewPageFrame )
 
 SwTwips SwAnchoredObject::GetRelCharX( const SwFrame* pFrame ) const
 {
-    return maLastCharRect.Left() - pFrame->getSwFrame().Left();
+    return maLastCharRect.Left() - pFrame->geFrameArea().Left();
 }
 
 SwTwips SwAnchoredObject::GetRelCharY( const SwFrame* pFrame ) const
 {
-    return maLastCharRect.Bottom() - pFrame->getSwFrame().Top();
+    return maLastCharRect.Bottom() - pFrame->geFrameArea().Top();
 }
 
 void SwAnchoredObject::AddLastCharY( long nDiff )
@@ -783,7 +783,7 @@ bool SwAnchoredObject::OverlapsPrevColumn() const
             SwRect aChkRect;
             while ( pTmpColFrame )
             {
-                aChkRect.Union( pTmpColFrame->getSwFrame() );
+                aChkRect.Union( pTmpColFrame->geFrameArea() );
                 pTmpColFrame = pTmpColFrame->GetPrev();
             }
             bOverlapsPrevColumn = GetObjRect().IsOver( aChkRect );
@@ -805,7 +805,7 @@ Point SwAnchoredObject::GetRelPosToAnchorFrame() const
 
     assert(GetAnchorFrame());
     aRelPos = GetObjRect().Pos();
-    aRelPos -= GetAnchorFrame()->getSwFrame().Pos();
+    aRelPos -= GetAnchorFrame()->geFrameArea().Pos();
 
     return aRelPos;
 }
@@ -848,12 +848,12 @@ Point SwAnchoredObject::GetRelPosToPageFrame( const bool _bFollowTextFlow,
     }
     if ( pFrame->IsCellFrame() )
     {
-        aRelPos -= ( pFrame->getSwFrame().Pos() + pFrame->getSwPrint().Pos() );
+        aRelPos -= ( pFrame->geFrameArea().Pos() + pFrame->getFramePrintArea().Pos() );
         _obRelToTableCell = true;
     }
     else
     {
-        aRelPos -= pFrame->getSwFrame().Pos();
+        aRelPos -= pFrame->geFrameArea().Pos();
     }
 
     return aRelPos;

sw/source/core/layout/laycache.cxx
@@ -281,7 +281,7 @@ void SwLayoutCache::Write( SvStream &rStream, const SwDoc& rDoc )
                 {
                     if (SwFlyFrame *pFly = dynamic_cast<SwFlyFrame*>(pAnchoredObj))
                     {
-                        if( pFly->getSwFrame().Left() != FAR_AWAY &&
+                        if( pFly->geFrameArea().Left() != FAR_AWAY &&
                             !pFly->GetAnchorFrame()->FindFooterOrHeader() )
                         {
                             const SwContact *pC =
@@ -294,9 +294,9 @@ void SwLayoutCache::Write( SvStream &rStream, const SwDoc& rDoc )
                                 aIo.OpenRec( SW_LAYCACHE_IO_REC_FLY );
                                 aIo.OpenFlagRec( 0, 0 );
                                 aIo.CloseFlagRec();
-                                const SwRect& rRct = pFly->getSwFrame();
-                                sal_Int32 nX = rRct.Left() - pPage->getSwFrame().Left();
-                                sal_Int32 nY = rRct.Top() - pPage->getSwFrame().Top();
+                                const SwRect& rRct = pFly->geFrameArea();
+                                sal_Int32 nX = rRct.Left() - pPage->geFrameArea().Left();
+                                sal_Int32 nY = rRct.Top() - pPage->geFrameArea().Top();
                                 aIo.GetStream().WriteUInt16( nPageNum ).WriteUInt32( nOrdNum )
                                                .WriteInt32( nX ).WriteInt32( nY )
                                                .WriteInt32( rRct.Width() )
@@ -807,8 +807,8 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
                         mrpFrame->InsertBehind( mrpLay, mrpPrv );
 
                         {
-                            SwFrameRect::FrameWriteAccess aFrm(*mrpFrame);
-                            aFrm.Pos() = mrpLay->getSwFrame().Pos();
+                            SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*mrpFrame);
+                            aFrm.Pos() = mrpLay->geFrameArea().Pos();
                             aFrm.Pos().Y() += 1;
                         }
 
@@ -880,10 +880,10 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
             if( CheckInsertPage() )
             {
                 CheckFlyCache_( pLastPage );
-                if( mrpPrv && mrpPrv->IsTextFrame() && !mrpPrv->GetValidSizeFlag() )
+                if( mrpPrv && mrpPrv->IsTextFrame() && !mrpPrv->isFrameAreaSizeValid() )
                 {
-                    SwFrameRect::FrameWriteAccess aFrm(*mrpPrv);
-                    aFrm.Height( mrpPrv->GetUpper()->getSwPrint().Height() );
+                    SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*mrpPrv);
+                    aFrm.Height( mrpPrv->GetUpper()->getFramePrintArea().Height() );
                 }
 
                 bRet = true;
@@ -917,8 +917,8 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
                     }
 
                     {
-                        SwFrameRect::FrameWriteAccess aFrm(*pSct);
-                        aFrm.Pos() = mrpLay->getSwFrame().Pos();
+                        SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*pSct);
+                        aFrm.Pos() = mrpLay->geFrameArea().Pos();
                         aFrm.Pos().Y() += 1; //because of the notifications
                     }
 
@@ -1016,12 +1016,12 @@ void SwLayHelper::CheckFlyCache_( SwPageFrame* pPage )
                 const SwFlyCache* pFlyCache = *aFlyCacheSetIt;
                 SwFlyFrame* pFly = const_cast<SwVirtFlyDrawObj*>(static_cast<const SwVirtFlyDrawObj*>(*aFlySetIt))->GetFlyFrame();
 
-                if ( pFly->getSwFrame().Left() == FAR_AWAY )
+                if ( pFly->geFrameArea().Left() == FAR_AWAY )
                 {
                     // we get the stored information
-                    SwFrameRect::FrameWriteAccess aFrm(*pFly);
-                    aFrm.Pos().X() = pFlyCache->Left() + pPage->getSwFrame().Left();
-                    aFrm.Pos().Y() = pFlyCache->Top() + pPage->getSwFrame().Top();
+                    SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*pFly);
+                    aFrm.Pos().X() = pFlyCache->Left() + pPage->geFrameArea().Left();
+                    aFrm.Pos().Y() = pFlyCache->Top() + pPage->geFrameArea().Top();
 
                     if ( mpImpl->IsUseFlyCache() )
                     {

sw/source/core/text/widorp.cxx
@@ -74,8 +74,8 @@ SwTextFrameBreak::SwTextFrameBreak( SwTextFrame *pNewFrame, const SwTwips nRst )
     if( !m_nRstHeight && !m_pFrame->IsFollow() && m_pFrame->IsInFootnote() && m_pFrame->HasPara() )
     {
         m_nRstHeight = m_pFrame->GetFootnoteFrameHeight();
-        m_nRstHeight += aRectFnSet.GetHeight(m_pFrame->getSwPrint()) -
-                      aRectFnSet.GetHeight(m_pFrame->getSwFrame());
+        m_nRstHeight += aRectFnSet.GetHeight(m_pFrame->getFramePrintArea()) -
+                      aRectFnSet.GetHeight(m_pFrame->geFrameArea());
         if( m_nRstHeight < 0 )
             m_nRstHeight = 0;
     }
@@ -382,10 +382,10 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
     if ( aRectFnSet.IsVert() )
     {
         nTmpY = pFrame->SwitchHorizontalToVertical( nTmpY );
-        nOldHeight = -aRectFnSet.GetHeight(pFrame->getSwPrint());
+        nOldHeight = -aRectFnSet.GetHeight(pFrame->getFramePrintArea());
     }
     else
-        nOldHeight = aRectFnSet.GetHeight(pFrame->getSwPrint());
+        nOldHeight = aRectFnSet.GetHeight(pFrame->getFramePrintArea());
 
     const SwTwips nChg = aRectFnSet.YDiff( nTmpY, nDocPrtTop + nOldHeight );
 
@@ -404,7 +404,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
             // multiple lines (e.g. via frames).
             if( !pMaster->IsLocked() && pMaster->GetUpper() )
             {
-                const SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->getSwFrame(),
+                const SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->geFrameArea(),
                     aRectFnSet.GetPrtBottom(*pMaster->GetUpper()) );
                 if ( nTmpRstHeight >=
                      SwTwips(rLine.GetInfo().GetParaPortion()->Height() ) )
@@ -427,7 +427,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
 
     if( 0 > nChg && !pMaster->IsLocked() && pMaster->GetUpper() )
     {
-        SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->getSwFrame(),
+        SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->geFrameArea(),
             aRectFnSet.GetPrtBottom(*pMaster->GetUpper()) );
         if( nTmpRstHeight >= SwTwips(rLine.GetInfo().GetParaPortion()->Height() ) )
         {

LemmingAvalanche/libreoffice-repaired

sw/source/core/crsr/viscrs.cxx
@@ -779,7 +779,7 @@ void SwShellTableCursor::FillRects()
     bool bStart = true;
     SwRegionRects aReg( GetShell()->VisArea() );
     if (comphelper::LibreOfficeKit::isActive())
-        aReg = GetShell()->getIDocumentLayoutAccess().GetCurrentLayout()->getSwFrame();
+        aReg = GetShell()->getIDocumentLayoutAccess().GetCurrentLayout()->getFrameArea();
     SwNodes& rNds = GetDoc()->GetNodes();
     SwFrame* pEndFrame = nullptr;
     for (size_t n = 0; n < m_SelectedBoxes.size(); ++n)
@@ -812,13 +812,13 @@ void SwShellTableCursor::FillRects()
 
         while ( pFrame )
         {
-            if( aReg.GetOrigin().IsOver( pFrame->getSwFrame() ) )
+            if( aReg.GetOrigin().IsOver( pFrame->getFrameArea() ) )
             {
-                aReg -= pFrame->getSwFrame();
+                aReg -= pFrame->getFrameArea();
                 if (bStart)
                 {
                     bStart = false;
-                    m_aStart = SwRect(pFrame->getSwFrame().Left(), pFrame->getSwFrame().Top(), 1, pFrame->getSwFrame().Height());
+                    m_aStart = SwRect(pFrame->getFrameArea().Left(), pFrame->getFrameArea().Top(), 1, pFrame->getFrameArea().Height());
                 }
             }
 
@@ -827,7 +827,7 @@ void SwShellTableCursor::FillRects()
         }
     }
     if (pEndFrame)
-        m_aEnd = SwRect(pEndFrame->getSwFrame().Right(), pEndFrame->getSwFrame().Top(), 1, pEndFrame->getSwFrame().Height());
+        m_aEnd = SwRect(pEndFrame->getFrameArea().Right(), pEndFrame->getFrameArea().Top(), 1, pEndFrame->getFrameArea().Height());
     aReg.Invert();
     insert( begin(), aReg.begin(), aReg.end() );
 }
@@ -857,7 +857,7 @@ bool SwShellTableCursor::IsInside( const Point& rPt ) const
         while( pFrame && !pFrame->IsCellFrame() )
             pFrame = pFrame->GetUpper();
         OSL_ENSURE( pFrame, "Node not in a table" );
-        if( pFrame && pFrame->getSwFrame().IsInside( rPt ) )
+        if( pFrame && pFrame->getFrameArea().IsInside( rPt ) )
             return true;
     }
     return false;

sw/source/core/doc/docfly.cxx
@@ -223,7 +223,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
             {
                 const SwFrame* pOld = static_cast<const SwFlyFrameFormat*>(pFlyFormat)->GetFrame( &aRet );
                 if( pOld )
-                    aRet = pOld->getSwFrame().Pos();
+                    aRet = pOld->getFrameArea().Pos();
             }
             break;
 
@@ -235,7 +235,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
                 const SwContentNode* pNd = pPos->nNode.GetNode().GetContentNode();
                 const SwFrame* pOld = pNd ? pNd->getLayoutFrame( rDoc.getIDocumentLayoutAccess().GetCurrentLayout(), &aRet, nullptr, false ) : nullptr;
                 if( pOld )
-                    aRet = pOld->getSwFrame().Pos();
+                    aRet = pOld->getFrameArea().Pos();
             }
             break;
 
@@ -246,7 +246,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
                                                 nNode.GetNode().GetFlyFormat());
                 const SwFrame* pOld = pFormat ? pFormat->GetFrame( &aRet ) : nullptr;
                 if( pOld )
-                    aRet = pOld->getSwFrame().Pos();
+                    aRet = pOld->getFrameArea().Pos();
             }
             break;
 
@@ -258,7 +258,7 @@ static Point lcl_FindAnchorLayPos( SwDoc& rDoc, const SwFormatAnchor& rAnch,
                                     pPage =static_cast<const SwPageFrame*>(pPage->GetNext()) )
                     if( i == nPgNum )
                     {
-                        aRet = pPage->getSwFrame().Pos();
+                        aRet = pPage->getFrameArea().Pos();
                         break;
                     }
             }
@@ -840,7 +840,7 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
             case RndStdIds::FLY_AT_PAGE:
                 {
                     pNewAnchorFrame = getIDocumentLayoutAccess().GetCurrentLayout()->Lower();
-                    while ( pNewAnchorFrame && !pNewAnchorFrame->getSwFrame().IsInside( aPt ) )
+                    while ( pNewAnchorFrame && !pNewAnchorFrame->getFrameArea().IsInside( aPt ) )
                         pNewAnchorFrame = pNewAnchorFrame->GetNext();
                     if ( !pNewAnchorFrame )
                         continue;
@@ -873,7 +873,7 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
                     aPoint.setX(aPoint.getX() - 1);    // Do not load in the DrawObj!
                     aNewAnch.SetType( RndStdIds::FLY_AS_CHAR );
                     SwPosition aPos( *static_cast<const SwContentFrame*>(pNewAnchorFrame)->GetNode() );
-                    if ( pNewAnchorFrame->getSwFrame().IsInside( aPoint ) )
+                    if ( pNewAnchorFrame->getFrameArea().IsInside( aPoint ) )
                     {
                     // We need to find a TextNode, because only there we can anchor a
                     // content-bound DrawObject.
@@ -884,7 +884,7 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
                     {
                         SwContentNode &rCNd = const_cast<SwContentNode&>(
                             *static_cast<const SwContentFrame*>(pNewAnchorFrame)->GetNode());
-                        if ( pNewAnchorFrame->getSwFrame().Bottom() < aPt.Y() )
+                        if ( pNewAnchorFrame->getFrameArea().Bottom() < aPt.Y() )
                             rCNd.MakeStartIndex( &aPos.nContent );
                         else
                             rCNd.MakeEndIndex( &aPos.nContent );
@@ -1096,7 +1096,7 @@ SwChainRet SwDoc::Chain( SwFrameFormat &rSource, const SwFrameFormat &rDest )
         {
             SwFlyFrame *pFly = SwIterator<SwFlyFrame,SwFormat>( rSource ).First();
             if ( pFly )
-                aSize.SetHeight( pFly->getSwFrame().Height() );
+                aSize.SetHeight( pFly->getFrameArea().Height() );
             aSize.SetHeightSizeType( ATT_FIX_SIZE );
             aSet.Put( aSize );
         }

sw/source/core/layout/anchoredobject.cxx
@@ -163,12 +163,12 @@ void SwAnchoredObject::SetPageFrame( SwPageFrame* _pNewPageFrame )
 
 SwTwips SwAnchoredObject::GetRelCharX( const SwFrame* pFrame ) const
 {
-    return maLastCharRect.Left() - pFrame->getSwFrame().Left();
+    return maLastCharRect.Left() - pFrame->getFrameArea().Left();
 }
 
 SwTwips SwAnchoredObject::GetRelCharY( const SwFrame* pFrame ) const
 {
-    return maLastCharRect.Bottom() - pFrame->getSwFrame().Top();
+    return maLastCharRect.Bottom() - pFrame->getFrameArea().Top();
 }
 
 void SwAnchoredObject::AddLastCharY( long nDiff )
@@ -783,7 +783,7 @@ bool SwAnchoredObject::OverlapsPrevColumn() const
             SwRect aChkRect;
             while ( pTmpColFrame )
             {
-                aChkRect.Union( pTmpColFrame->getSwFrame() );
+                aChkRect.Union( pTmpColFrame->getFrameArea() );
                 pTmpColFrame = pTmpColFrame->GetPrev();
             }
             bOverlapsPrevColumn = GetObjRect().IsOver( aChkRect );
@@ -805,7 +805,7 @@ Point SwAnchoredObject::GetRelPosToAnchorFrame() const
 
     assert(GetAnchorFrame());
     aRelPos = GetObjRect().Pos();
-    aRelPos -= GetAnchorFrame()->getSwFrame().Pos();
+    aRelPos -= GetAnchorFrame()->getFrameArea().Pos();
 
     return aRelPos;
 }
@@ -848,12 +848,12 @@ Point SwAnchoredObject::GetRelPosToPageFrame( const bool _bFollowTextFlow,
     }
     if ( pFrame->IsCellFrame() )
     {
-        aRelPos -= ( pFrame->getSwFrame().Pos() + pFrame->getSwPrint().Pos() );
+        aRelPos -= ( pFrame->getFrameArea().Pos() + pFrame->getFramePrintArea().Pos() );
         _obRelToTableCell = true;
     }
     else
     {
-        aRelPos -= pFrame->getSwFrame().Pos();
+        aRelPos -= pFrame->getFrameArea().Pos();
     }
 
     return aRelPos;

sw/source/core/layout/laycache.cxx
@@ -281,7 +281,7 @@ void SwLayoutCache::Write( SvStream &rStream, const SwDoc& rDoc )
                 {
                     if (SwFlyFrame *pFly = dynamic_cast<SwFlyFrame*>(pAnchoredObj))
                     {
-                        if( pFly->getSwFrame().Left() != FAR_AWAY &&
+                        if( pFly->getFrameArea().Left() != FAR_AWAY &&
                             !pFly->GetAnchorFrame()->FindFooterOrHeader() )
                         {
                             const SwContact *pC =
@@ -294,9 +294,9 @@ void SwLayoutCache::Write( SvStream &rStream, const SwDoc& rDoc )
                                 aIo.OpenRec( SW_LAYCACHE_IO_REC_FLY );
                                 aIo.OpenFlagRec( 0, 0 );
                                 aIo.CloseFlagRec();
-                                const SwRect& rRct = pFly->getSwFrame();
-                                sal_Int32 nX = rRct.Left() - pPage->getSwFrame().Left();
-                                sal_Int32 nY = rRct.Top() - pPage->getSwFrame().Top();
+                                const SwRect& rRct = pFly->getFrameArea();
+                                sal_Int32 nX = rRct.Left() - pPage->getFrameArea().Left();
+                                sal_Int32 nY = rRct.Top() - pPage->getFrameArea().Top();
                                 aIo.GetStream().WriteUInt16( nPageNum ).WriteUInt32( nOrdNum )
                                                .WriteInt32( nX ).WriteInt32( nY )
                                                .WriteInt32( rRct.Width() )
@@ -807,8 +807,8 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
                         mrpFrame->InsertBehind( mrpLay, mrpPrv );
 
                         {
-                            SwFrameRect::FrameWriteAccess aFrm(*mrpFrame);
-                            aFrm.Pos() = mrpLay->getSwFrame().Pos();
+                            SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*mrpFrame);
+                            aFrm.Pos() = mrpLay->getFrameArea().Pos();
                             aFrm.Pos().Y() += 1;
                         }
 
@@ -880,10 +880,10 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
             if( CheckInsertPage() )
             {
                 CheckFlyCache_( pLastPage );
-                if( mrpPrv && mrpPrv->IsTextFrame() && !mrpPrv->GetValidSizeFlag() )
+                if( mrpPrv && mrpPrv->IsTextFrame() && !mrpPrv->isFrameAreaSizeValid() )
                 {
-                    SwFrameRect::FrameWriteAccess aFrm(*mrpPrv);
-                    aFrm.Height( mrpPrv->GetUpper()->getSwPrint().Height() );
+                    SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*mrpPrv);
+                    aFrm.Height( mrpPrv->GetUpper()->getFramePrintArea().Height() );
                 }
 
                 bRet = true;
@@ -917,8 +917,8 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
                     }
 
                     {
-                        SwFrameRect::FrameWriteAccess aFrm(*pSct);
-                        aFrm.Pos() = mrpLay->getSwFrame().Pos();
+                        SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*pSct);
+                        aFrm.Pos() = mrpLay->getFrameArea().Pos();
                         aFrm.Pos().Y() += 1; //because of the notifications
                     }
 
@@ -1016,12 +1016,12 @@ void SwLayHelper::CheckFlyCache_( SwPageFrame* pPage )
                 const SwFlyCache* pFlyCache = *aFlyCacheSetIt;
                 SwFlyFrame* pFly = const_cast<SwVirtFlyDrawObj*>(static_cast<const SwVirtFlyDrawObj*>(*aFlySetIt))->GetFlyFrame();
 
-                if ( pFly->getSwFrame().Left() == FAR_AWAY )
+                if ( pFly->getFrameArea().Left() == FAR_AWAY )
                 {
                     // we get the stored information
-                    SwFrameRect::FrameWriteAccess aFrm(*pFly);
-                    aFrm.Pos().X() = pFlyCache->Left() + pPage->getSwFrame().Left();
-                    aFrm.Pos().Y() = pFlyCache->Top() + pPage->getSwFrame().Top();
+                    SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*pFly);
+                    aFrm.Pos().X() = pFlyCache->Left() + pPage->getFrameArea().Left();
+                    aFrm.Pos().Y() = pFlyCache->Top() + pPage->getFrameArea().Top();
 
                     if ( mpImpl->IsUseFlyCache() )
                     {

sw/source/core/text/widorp.cxx
@@ -74,8 +74,8 @@ SwTextFrameBreak::SwTextFrameBreak( SwTextFrame *pNewFrame, const SwTwips nRst )
     if( !m_nRstHeight && !m_pFrame->IsFollow() && m_pFrame->IsInFootnote() && m_pFrame->HasPara() )
     {
         m_nRstHeight = m_pFrame->GetFootnoteFrameHeight();
-        m_nRstHeight += aRectFnSet.GetHeight(m_pFrame->getSwPrint()) -
-                      aRectFnSet.GetHeight(m_pFrame->getSwFrame());
+        m_nRstHeight += aRectFnSet.GetHeight(m_pFrame->getFramePrintArea()) -
+                      aRectFnSet.GetHeight(m_pFrame->getFrameArea());
         if( m_nRstHeight < 0 )
             m_nRstHeight = 0;
     }
@@ -382,10 +382,10 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
     if ( aRectFnSet.IsVert() )
     {
         nTmpY = pFrame->SwitchHorizontalToVertical( nTmpY );
-        nOldHeight = -aRectFnSet.GetHeight(pFrame->getSwPrint());
+        nOldHeight = -aRectFnSet.GetHeight(pFrame->getFramePrintArea());
     }
     else
-        nOldHeight = aRectFnSet.GetHeight(pFrame->getSwPrint());
+        nOldHeight = aRectFnSet.GetHeight(pFrame->getFramePrintArea());
 
     const SwTwips nChg = aRectFnSet.YDiff( nTmpY, nDocPrtTop + nOldHeight );
 
@@ -404,7 +404,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
             // multiple lines (e.g. via frames).
             if( !pMaster->IsLocked() && pMaster->GetUpper() )
             {
-                const SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->getSwFrame(),
+                const SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->getFrameArea(),
                     aRectFnSet.GetPrtBottom(*pMaster->GetUpper()) );
                 if ( nTmpRstHeight >=
                      SwTwips(rLine.GetInfo().GetParaPortion()->Height() ) )
@@ -427,7 +427,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
 
     if( 0 > nChg && !pMaster->IsLocked() && pMaster->GetUpper() )
     {
-        SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->getSwFrame(),
+        SwTwips nTmpRstHeight = aRectFnSet.BottomDist( pMaster->getFrameArea(),
             aRectFnSet.GetPrtBottom(*pMaster->GetUpper()) );
         if( nTmpRstHeight >= SwTwips(rLine.GetInfo().GetParaPortion()->Height() ) )
         {