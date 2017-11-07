LemmingAvalanche/libreoffice-repaired

commit 97f61dc6e2cad79e94dd9faa83739f0d9d99408a

QT5 first stab on implementing CommonSalLayout
CommonSalLayout doesn't rally have an interface. It's cluttered
with #ifdefs. Currently we have to move the Qt5Font into the
VCL library. Someone should refactor this...

Doen't render any text yet, but reports some sizes.

Eventually that would cut down the public interface again.

Change-Id: I12f32affb05b37e070c6cbc80db01779f84590b6

vcl/inc/CommonSalLayout.hxx
@@ -20,6 +20,8 @@
 #ifndef INCLUDED_VCL_INC_COMMONSALLAYOUT_HXX
 #define INCLUDED_VCL_INC_COMMONSALLAYOUT_HXX
 
+#include <config_qt5.h>
+
 #include <com/sun/star/i18n/XBreakIterator.hpp>
 
 #ifdef _WIN32
@@ -37,8 +39,19 @@
 #include <hb-icu.h>
 #include <hb-ot.h>
 
-class CommonSalLayout : public GenericSalLayout
+#if ENABLE_QT5
+class Qt5Font;
+#endif
+
+class VCL_DLLPUBLIC CommonSalLayout : public GenericSalLayout
 {
+#if ENABLE_QT5
+    friend hb_blob_t*       getFontTable(hb_face_t*, hb_tag_t, void*);
+    explicit                CommonSalLayout(const FontSelectPattern &rFSP,
+                                            FreetypeFont *pFreetypeFont,
+                                            Qt5Font *pFont, bool bUseQt5);
+#endif
+
     hb_font_t*              mpHbFont;
     const FontSelectPattern& mrFontSelData;
     css::uno::Reference<css::i18n::XBreakIterator> mxBreak;
@@ -50,7 +63,11 @@ class CommonSalLayout : public GenericSalLayout
 #elif defined(MACOSX) || defined(IOS)
     const CoreTextStyle&    mrCoreTextStyle;
 #else
-    FreetypeFont&           mrFreetypeFont;
+    FreetypeFont*           mpFreetypeFont;
+#if ENABLE_QT5
+    const bool              mbUseQt5;
+    Qt5Font*                mpQFont;
+#endif
 #endif
 
     void                    ParseFeatures(const OUString& name);
@@ -76,7 +93,11 @@ public:
     const CoreTextStyle&    getFontData() const { return mrCoreTextStyle; };
 #else
     explicit                CommonSalLayout(FreetypeFont&);
-    const FreetypeFont&     getFontData() const { return mrFreetypeFont; };
+    const FreetypeFont*     getFreetypeFont() const { return mpFreetypeFont; };
+#if ENABLE_QT5
+    explicit                CommonSalLayout(Qt5Font&);
+    const Qt5Font*          getQt5Font() const { return mpQFont; };
+#endif
 #endif
 
     virtual void            InitFont() const override;

vcl/source/gdi/CommonSalLayout.cxx
@@ -29,7 +29,14 @@
 #include <unicode/uchar.h>
 #include <android/compatibility.hxx>
 
-static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pUserData)
+#if ENABLE_QT5
+#include <qt5/Qt5Font.hxx>
+#include <QtGui/QRawFont>
+#else
+class Qt5Font;
+#endif
+
+hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pUserData)
 {
     char pTagName[5];
     pTagName[0] = (char)(nTableTag >> 24);
@@ -62,9 +69,23 @@ static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pU
         pFont->GetFontTable(pTagName, pBuffer);
     }
 #else
-    const unsigned char* pBuffer = nullptr;
-    FreetypeFont* pFont = static_cast<FreetypeFont*>(pUserData);
-    pBuffer = pFont->GetTable(pTagName, &nLength);
+    const char* pBuffer = nullptr;
+    CommonSalLayout *pLayout = static_cast<CommonSalLayout*>( pUserData );
+#if ENABLE_QT5
+    QByteArray aTable;
+    if ( pLayout->mbUseQt5 )
+    {
+        QRawFont aRawFont( QRawFont::fromFont( *pLayout->mpQFont ) );
+        aTable = aRawFont.fontTable( pTagName );
+        pBuffer = reinterpret_cast<const char*>( aTable.data() );
+        nLength = aTable.size();
+    }
+    else
+#endif
+    {
+        pBuffer = reinterpret_cast<const char*>(
+            pLayout->mpFreetypeFont->GetTable(pTagName, &nLength) );
+    }
 #endif
 
     hb_blob_t* pBlob = nullptr;
@@ -73,7 +94,7 @@ static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pU
         pBlob = hb_blob_create(reinterpret_cast<const char*>(pBuffer), nLength, HB_MEMORY_MODE_READONLY,
                                pBuffer, [](void* data){ delete[] static_cast<unsigned char*>(data); });
 #else
-        pBlob = hb_blob_create(reinterpret_cast<const char*>(pBuffer), nLength, HB_MEMORY_MODE_READONLY, nullptr, nullptr);
+        pBlob = hb_blob_create(pBuffer, nLength, HB_MEMORY_MODE_READONLY, nullptr, nullptr);
 #endif
 
     return pBlob;
@@ -234,20 +255,51 @@ CommonSalLayout::CommonSalLayout(const CoreTextStyle& rCoreTextStyle)
 }
 
 #else
-CommonSalLayout::CommonSalLayout(FreetypeFont& rFreetypeFont)
-:   mrFontSelData(rFreetypeFont.GetFontSelData())
-,   mrFreetypeFont(rFreetypeFont)
-,   mpVertGlyphs(nullptr)
+
+CommonSalLayout::CommonSalLayout(const FontSelectPattern &rFSP,
+                                 FreetypeFont *pFreetypeFont,
+                                 Qt5Font *pQt5Font, bool bUseQt5)
+    : mrFontSelData(rFSP)
+    , mpFreetypeFont(pFreetypeFont)
+#if ENABLE_QT5
+    , mbUseQt5(bUseQt5)
+    , mpQFont(pQt5Font)
+#endif
+    , mpVertGlyphs(nullptr)
 {
-    mpHbFont = rFreetypeFont.GetHbFont();
+#if ENABLE_QT5
+    if (mbUseQt5)
+        mpHbFont = mpQFont->GetHbFont();
+    else
+#endif
+        mpHbFont = mpFreetypeFont->GetHbFont();
     if (!mpHbFont)
     {
-        hb_face_t* pHbFace = hb_face_create_for_tables(getFontTable, &rFreetypeFont, nullptr);
+        hb_face_t* pHbFace = hb_face_create_for_tables(getFontTable, this, nullptr);
 
         mpHbFont = createHbFont(pHbFace);
-        mrFreetypeFont.SetHbFont(mpHbFont);
+#if ENABLE_QT5
+        if (mbUseQt5)
+            mpQFont->SetHbFont(mpHbFont);
+        else
+#endif
+            mpFreetypeFont->SetHbFont(mpHbFont);
     }
 }
+
+CommonSalLayout::CommonSalLayout(FreetypeFont& rFreetypeFont)
+    : CommonSalLayout(rFreetypeFont.GetFontSelData(),
+                      &rFreetypeFont, nullptr, false)
+{
+}
+
+#if ENABLE_QT5
+CommonSalLayout::CommonSalLayout(Qt5Font& rQFont)
+    : CommonSalLayout(rQFont.GetFontSelData(),
+                      nullptr, &rQFont, true)
+{
+}
+#endif
 #endif
 
 void CommonSalLayout::InitFont() const

config_host/config_qt5.h.in
@@ -5,6 +5,7 @@ Settings for QT5 integration.
 #ifndef CONFIG_QT5_H
 #define CONFIG_QT5_H
 
+#define ENABLE_QT5 0
 #define QT5_HAVE_GLIB 0
 
 #endif

vcl/CustomTarget_qt5_moc.mk
@@ -9,12 +9,12 @@
 
 $(eval $(call gb_CustomTarget_CustomTarget,vcl/qt5))
 
-$(call gb_CustomTarget_get_target,vcl/unx/qt5) : \
+$(call gb_CustomTarget_get_target,vcl/qt5) : \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Instance.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Timer.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Widget.moc \
 
-$(call gb_CustomTarget_get_workdir,vcl/unx/qt5)/%.moc : \
+$(call gb_CustomTarget_get_workdir,vcl/qt5)/%.moc : \
 		$(SRCDIR)/vcl/qt5/%.hxx \
 		| $(call gb_CustomTarget_get_workdir,vcl/qt5)/.dir
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),MOC,1)

vcl/Library_vcl.mk
@@ -719,4 +719,22 @@ ifeq ($(OS),WNT)
 $(eval $(call gb_Library_use_package,vcl,postprocess_images))
 endif
 
+ifeq ($(ENABLE_QT5),TRUE)
+$(eval $(call gb_Library_use_externals,vcl,\
+	qt5 \
+))
+$(eval $(call gb_Library_add_defs,vcl,\
+    $(QT5_CFLAGS) \
+))
+$(eval $(call gb_Library_add_libs,vcl,\
+    $(QT5_LIBS) \
+))
+$(eval $(call gb_Library_add_cxxflags,vcl,\
+    $(QT5_CFLAGS) \
+))
+$(eval $(call gb_Library_add_exception_objects,vcl,\
+    vcl/qt5/Qt5Font \
+))
+endif
+
 # vim: set noet sw=4 ts=4:

vcl/Library_vclplug_qt5.mk
@@ -53,11 +53,14 @@ $(eval $(call gb_Library_use_libraries,vclplug_qt5,\
 ))
 
 $(eval $(call gb_Library_use_externals,vclplug_qt5,\
-	boost_headers \
-	cairo \
-	icuuc \
-	qt5 \
-	epoxy \
+    boost_headers \
+    cairo \
+    epoxy \
+    graphite \
+    harfbuzz \
+    icu_headers \
+    icuuc \
+    qt5 \
 ))
 
 $(eval $(call gb_Library_add_defs,vclplug_qt5,\

vcl/inc/impfontmetricdata.hxx
@@ -32,7 +32,7 @@ typedef tools::SvRef<ImplFontMetricData> ImplFontMetricDataRef;
 class OutputDevice;
 class FontSelectPattern;
 
-class ImplFontMetricData : public FontAttributes, public SvRefBase
+class VCL_DLLPUBLIC ImplFontMetricData : public FontAttributes, public SvRefBase
 {
 public:
     explicit        ImplFontMetricData( const FontSelectPattern& );

vcl/inc/qt5/Qt5Font.hxx
@@ -1,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <vcl/dllapi.h>
+#include <QtGui/QFont>
+
+#include <fontselect.hxx>
+#include <hb-ot.h>
+
+class VCL_DLLPUBLIC Qt5Font : public QFont
+{
+    const FontSelectPattern m_aFontSelData;
+    hb_font_t* m_pHbFont;
+
+public:
+    Qt5Font( const FontSelectPattern& rFSP)
+        : m_aFontSelData( rFSP ), m_pHbFont( nullptr ) {}
+    virtual ~Qt5Font();
+
+    hb_font_t*                GetHbFont() const { return m_pHbFont; }
+    void                      SetHbFont( hb_font_t* pHbFont ) { m_pHbFont = pHbFont; }
+    const FontSelectPattern&  GetFontSelData() const { return m_aFontSelData; }
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit ecd24ad34c4fe68fb95698bae8eda2976ea2d0dc

Fix harfbuzz font lifecycle in CommonSalLayout
The harfbuzz font is attached to the system font face and
therefore inherits its lifecycle. This means it can be used in
multiple CommonSalLayout objects, so the user data parameter of
hb_face_create_for_tables can't be the layout, but must be the
font.

This moves the special Qt5Font handling into it's own function,
so accessing the switching parameter mbUseQt5 is not needed.

Regression from commit 97f61dc6e2cad79e94dd9faa83739f0d9d99408a.

Change-Id: Ic34cc5b60e401562c73b239a58176a59fe4bf9be
Reviewed-on: https://gerrit.libreoffice.org/44398
Reviewed-by: Khaled Hosny <khaledhosny@eglug.org>
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

vcl/inc/CommonSalLayout.hxx
@@ -60,7 +60,12 @@ class VCL_DLLPUBLIC CommonSalLayout : public GenericSalLayout
 #if ENABLE_QT5
     const bool              mbUseQt5;
     Qt5Font*                mpQFont;
+
+    explicit                CommonSalLayout(const FontSelectPattern &rFSP,
+                                            FreetypeFont *pFreetypeFont,
+                                            Qt5Font *pFont, bool bUseQt5);
 #endif
+    void                    InitFromFreetypeFont();
 #endif
 
     void                    ParseFeatures(const OUString& name);
@@ -88,9 +93,6 @@ public:
     explicit                CommonSalLayout(FreetypeFont&);
     const FreetypeFont*     getFreetypeFont() const { return mpFreetypeFont; }
 #if ENABLE_QT5
-    explicit                CommonSalLayout(const FontSelectPattern &rFSP,
-                                            FreetypeFont *pFreetypeFont,
-                                            Qt5Font *pFont, bool bUseQt5);
     explicit                CommonSalLayout(Qt5Font&);
     const Qt5Font*          getQt5Font() const { return mpQFont; }
     bool                    useQt5() const { return mbUseQt5; }

vcl/source/gdi/CommonSalLayout.cxx
@@ -34,14 +34,19 @@
 #include <QtGui/QRawFont>
 #endif
 
-static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pUserData)
+static inline void decode_hb_tag( const hb_tag_t nTableTag, char *pTagName )
 {
-    char pTagName[5];
     pTagName[0] = (char)(nTableTag >> 24);
     pTagName[1] = (char)(nTableTag >> 16);
     pTagName[2] = (char)(nTableTag >>  8);
     pTagName[3] = (char)nTableTag;
     pTagName[4] = 0;
+}
+
+static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pUserData)
+{
+    char pTagName[5];
+    decode_hb_tag( nTableTag, pTagName );
 
     sal_uLong nLength = 0;
 #if defined(_WIN32)
@@ -67,23 +72,9 @@ static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pU
         pFont->GetFontTable(pTagName, pBuffer);
     }
 #else
-    const char* pBuffer = nullptr;
-    CommonSalLayout *pLayout = static_cast<CommonSalLayout*>( pUserData );
-#if ENABLE_QT5
-    QByteArray aTable;
-    if ( pLayout->useQt5() )
-    {
-        QRawFont aRawFont( QRawFont::fromFont( *pLayout->getQt5Font() ) );
-        aTable = aRawFont.fontTable( pTagName );
-        pBuffer = reinterpret_cast<const char*>( aTable.data() );
-        nLength = aTable.size();
-    }
-    else
-#endif
-    {
-        pBuffer = reinterpret_cast<const char*>(
-            pLayout->getFreetypeFont()->GetTable(pTagName, &nLength) );
-    }
+    FreetypeFont* pFont = static_cast<FreetypeFont*>( pUserData );
+    const char* pBuffer = reinterpret_cast<const char*>(
+        pFont->GetTable(pTagName, &nLength) );
 #endif
 
     hb_blob_t* pBlob = nullptr;
@@ -97,6 +88,25 @@ static hb_blob_t* getFontTable(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pU
 
     return pBlob;
 }
+
+#if ENABLE_QT5
+static hb_blob_t* getFontTable_Qt5Font(hb_face_t* /*face*/, hb_tag_t nTableTag, void* pUserData)
+{
+    char pTagName[5];
+    decode_hb_tag( nTableTag, pTagName );
+
+    Qt5Font *pFont = static_cast<Qt5Font*>( pUserData );
+    QRawFont aRawFont( QRawFont::fromFont( *pFont ) );
+    QByteArray aTable = aRawFont.fontTable( pTagName );
+    const sal_uLong nLength = aTable.size();
+
+    hb_blob_t* pBlob = nullptr;
+    if (nLength > 0)
+        pBlob = hb_blob_create(reinterpret_cast<const char*>( aTable.data() ),
+                               nLength, HB_MEMORY_MODE_READONLY, nullptr, nullptr);
+    return pBlob;
+}
+#endif
 
 static hb_font_t* createHbFont(hb_face_t* pHbFace)
 {
@@ -254,6 +264,17 @@ CommonSalLayout::CommonSalLayout(const CoreTextStyle& rCoreTextStyle)
 
 #else
 
+void CommonSalLayout::InitFromFreetypeFont()
+{
+    mpHbFont = mpFreetypeFont->GetHbFont();
+    if (!mpHbFont)
+    {
+        hb_face_t* pHbFace = hb_face_create_for_tables(getFontTable, mpFreetypeFont, nullptr);
+        mpHbFont = createHbFont(pHbFace);
+        mpFreetypeFont->SetHbFont(mpHbFont);
+    }
+}
+
 #if ENABLE_QT5
 CommonSalLayout::CommonSalLayout(const FontSelectPattern &rFSP,
                                  FreetypeFont *pFreetypeFont,
@@ -265,18 +286,21 @@ CommonSalLayout::CommonSalLayout(const FontSelectPattern &rFSP,
     , mpVertGlyphs(nullptr)
 {
     if (mbUseQt5)
-        mpHbFont = mpQFont->GetHbFont();
-    else
-        mpHbFont = mpFreetypeFont->GetHbFont();
-    if (!mpHbFont)
     {
-        hb_face_t* pHbFace = hb_face_create_for_tables(getFontTable, this, nullptr);
-
-        mpHbFont = createHbFont(pHbFace);
-        if (mbUseQt5)
+        assert( pQt5Font && !pFreetypeFont );
+        mpHbFont = mpQFont->GetHbFont();
+        if (!mpHbFont)
+        {
+            hb_face_t* pHbFace = hb_face_create_for_tables(
+                getFontTable_Qt5Font, pQt5Font, nullptr);
+            mpHbFont = createHbFont(pHbFace);
             mpQFont->SetHbFont(mpHbFont);
-        else
-            mpFreetypeFont->SetHbFont(mpHbFont);
+        }
+    }
+    else
+    {
+        assert( !pQt5Font && pFreetypeFont );
+        InitFromFreetypeFont();
     }
 }
 
@@ -299,13 +323,7 @@ CommonSalLayout::CommonSalLayout(FreetypeFont& rFreetypeFont)
     , mpFreetypeFont(&rFreetypeFont)
     , mpVertGlyphs(nullptr)
 {
-    mpHbFont = mpFreetypeFont->GetHbFont();
-    if (!mpHbFont)
-    {
-        hb_face_t* pHbFace = hb_face_create_for_tables(getFontTable, this, nullptr);
-        mpHbFont = createHbFont(pHbFace);
-        mpFreetypeFont->SetHbFont(mpHbFont);
-    }
+    InitFromFreetypeFont();
 }
 
 #endif