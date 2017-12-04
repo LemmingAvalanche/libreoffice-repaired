LemmingAvalanche/libreoffice-repaired

commit dc3dc406e8855aaa586a79ac25cd2c6b83eb8a43

lok - add support for in place chart editing
This commit add a minimal support for editing chart embedded in a
spreadsheet or a text document or a presentation.
Graphic objects can be moved and resized, text objects can be edited.

Change-Id: I8e637dabf328a94bd6bb0e309a245302cff421d8
Reviewed-on: https://gerrit.libreoffice.org/39342
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Marco Cecchetti <mrcekets@gmail.com>

sfx2/source/view/lokcharthelper.cxx
@@ -1,0 +1,373 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sfx2/lokcharthelper.hxx>
+
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
+#include <sfx2/ipclient.hxx>
+#include <sfx2/lokhelper.hxx>
+#include <toolkit/helper/vclunohelper.hxx>
+#include <tools/fract.hxx>
+#include <tools/mapunit.hxx>
+#include <vcl/virdev.hxx>
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/embed/XEmbeddedObject.hpp>
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/chart2/XChartDocument.hpp>
+
+
+#define TWIPS_PER_PIXEL 15
+
+using namespace com::sun::star;
+
+namespace {
+
+inline Point lcl_TwipsToHMM( const Point& rPoint )
+{
+    return Point(convertTwipToMm100(rPoint.getX()), convertTwipToMm100(rPoint.getY()));
+}
+
+inline Size lcl_TwipsToHMM( const Size& rSize )
+{
+    return Size(convertTwipToMm100(rSize.getWidth()), convertTwipToMm100(rSize.getHeight()));
+}
+
+} // end anonymous ns
+
+css::uno::Reference<css::frame::XController>& LokChartHelper::GetXController()
+{
+    if(!mxController.is() )
+    {
+        if (mpViewShell)
+        {
+            SfxInPlaceClient* pIPClient = mpViewShell->GetIPClient();
+            if (pIPClient)
+            {
+                css::uno::Reference< ::css::embed::XEmbeddedObject > xEmbObj = pIPClient->GetObject();
+                if( xEmbObj.is() )
+                {
+                    ::css::uno::Reference< ::css::chart2::XChartDocument > xChart( xEmbObj->getComponent(), uno::UNO_QUERY );
+                    if( xChart.is() )
+                    {
+                        ::css::uno::Reference< ::css::frame::XController > xChartController = xChart->getCurrentController();
+                        if( xChartController.is() )
+                        {
+                            mxController = xChartController;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return mxController;
+}
+
+css::uno::Reference<css::frame::XDispatch>& LokChartHelper::GetXDispatcher()
+{
+    if( !mxDispatcher.is() )
+    {
+        ::css::uno::Reference< ::css::frame::XController >& xChartController = GetXController();
+        if( xChartController.is() )
+        {
+            ::css::uno::Reference< ::css::frame::XDispatch > xDispatcher( xChartController, uno::UNO_QUERY );
+            if( xDispatcher.is() )
+            {
+                mxDispatcher = xDispatcher;
+            }
+        }
+    }
+
+    return mxDispatcher;
+}
+
+vcl::Window* LokChartHelper::GetWindow()
+{
+    if (!mpWindow)
+    {
+        ::css::uno::Reference< ::css::frame::XController >& xChartController = GetXController();
+        if( xChartController.is() )
+        {
+            ::css::uno::Reference< ::css::frame::XFrame > xFrame = xChartController->getFrame();
+            if (xFrame.is())
+            {
+                ::css::uno::Reference< ::css::awt::XWindow > xDockerWin = xFrame->getContainerWindow();
+                vcl::Window* pParent = VCLUnoHelper::GetWindow( xDockerWin ).get();
+                if (pParent)
+                {
+                    sal_uInt16 nTotChildren = pParent->GetChildCount();
+                    while (nTotChildren--)
+                    {
+                        vcl::Window* pChildWin = pParent->GetChild(nTotChildren);
+                        if (pChildWin && pChildWin->IsChart())
+                        {
+                            mpWindow = pChildWin;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return mpWindow.get();
+}
+
+tools::Rectangle LokChartHelper::GetChartBoundingBox()
+{
+    tools::Rectangle aBBox;
+    if (mpViewShell)
+    {
+        SfxInPlaceClient* pIPClient = mpViewShell->GetIPClient();
+        if (pIPClient)
+        {
+            vcl::Window* pRootWin = pIPClient->GetEditWin();
+            if (pRootWin)
+            {
+                vcl::Window* pWindow = GetWindow();
+                if (pWindow)
+                {
+                    // In all cases, the following code fragment
+                    // returns the chart bounding box in twips.
+                    MapMode aCWMapMode = pWindow->GetMapMode();
+                    double fXScale = aCWMapMode.GetScaleX();
+                    double fYScale = aCWMapMode.GetScaleY();
+                    Point aOffset = pWindow->GetOffsetPixelFrom(*pRootWin);
+                    aOffset.X() *= (TWIPS_PER_PIXEL / fXScale);
+                    aOffset.Y() *= (TWIPS_PER_PIXEL / fYScale);
+                    Size aSize = pWindow->GetSizePixel();
+                    aSize.Width() *= (TWIPS_PER_PIXEL / fXScale);
+                    aSize.Height() *= (TWIPS_PER_PIXEL / fYScale);
+                    aBBox = tools::Rectangle(aOffset, aSize);
+                }
+            }
+        }
+    }
+    return aBBox;
+}
+
+void LokChartHelper::Invalidate()
+{
+    mpWindow = nullptr;
+    mxDispatcher.clear();
+    mxController.clear();
+}
+
+bool LokChartHelper::Hit(const Point& aPos)
+{
+    if (mpViewShell)
+    {
+        vcl::Window* pChartWindow = GetWindow();
+        if (pChartWindow)
+        {
+            tools::Rectangle rChartBBox = GetChartBoundingBox();
+            return rChartBBox.IsInside(aPos);
+        }
+    }
+    return false;
+}
+
+bool LokChartHelper::HitAny(const Point& aPos)
+{
+    SfxViewShell* pViewShell = SfxViewShell::GetFirst();
+    while (pViewShell)
+    {
+        LokChartHelper aChartHelper(pViewShell);
+        if (aChartHelper.Hit(aPos))
+            return true;
+        pViewShell = SfxViewShell::GetNext(*pViewShell);
+    }
+    return false;
+}
+
+void LokChartHelper::PaintTile(VirtualDevice& rRenderContext, const tools::Rectangle& rTileRect)
+{
+    if (mpViewShell)
+    {
+        vcl::Window* pChartWindow = GetWindow();
+        if (pChartWindow)
+        {
+            tools::Rectangle aChartRect = GetChartBoundingBox();
+            tools::Rectangle aTestRect = rTileRect;
+            aTestRect.Intersection( aChartRect );
+            if (!aTestRect.IsEmpty())
+            {
+                Point aOffset( aChartRect.Left() - rTileRect.Left(), aChartRect.Top() - rTileRect.Top() );
+                Point aOffsetFromTile = lcl_TwipsToHMM(aOffset);
+                Size aSize = lcl_TwipsToHMM(aChartRect.GetSize());
+                tools::Rectangle aRectangle(Point(0,0), aSize);
+
+                bool bEnableMapMode = !pChartWindow->IsMapModeEnabled();
+                pChartWindow->EnableMapMode();
+                bool bRenderContextEnableMapMode = !rRenderContext.IsMapModeEnabled();
+                rRenderContext.EnableMapMode();
+
+                rRenderContext.Push(PushFlags::MAPMODE);
+
+                MapMode aCWMapMode = pChartWindow->GetMapMode();
+                aCWMapMode.SetScaleX(rRenderContext.GetMapMode().GetScaleX());
+                aCWMapMode.SetScaleY(rRenderContext.GetMapMode().GetScaleY());
+
+                aCWMapMode.SetOrigin(aOffsetFromTile);
+                rRenderContext.SetMapMode(aCWMapMode);
+
+                pChartWindow->Paint(rRenderContext, aRectangle);
+
+                rRenderContext.Pop();
+
+                if (bRenderContextEnableMapMode)
+                    rRenderContext.EnableMapMode(false);
+                if (bEnableMapMode)
+                    pChartWindow->EnableMapMode(false);
+            }
+        }
+    }
+}
+
+void LokChartHelper::PaintAllChartsOnTile(VirtualDevice& rDevice,
+                                          int nOutputWidth, int nOutputHeight,
+                                          int nTilePosX, int nTilePosY,
+                                          long nTileWidth, long nTileHeight)
+{
+    // Resizes the virtual device so to contain the entries context
+    rDevice.SetOutputSizePixel(Size(nOutputWidth, nOutputHeight));
+
+    rDevice.Push(PushFlags::MAPMODE);
+    MapMode aMapMode(rDevice.GetMapMode());
+
+    // Scaling. Must convert from pixels to twips. We know
+    // that VirtualDevices use a DPI of 96.
+    Fraction scaleX = Fraction(nOutputWidth, 96) * Fraction(1440L) / Fraction(nTileWidth);
+    Fraction scaleY = Fraction(nOutputHeight, 96) * Fraction(1440L) / Fraction(nTileHeight);
+    aMapMode.SetScaleX(scaleX);
+    aMapMode.SetScaleY(scaleY);
+    rDevice.SetMapMode(aMapMode);
+
+    tools::Rectangle aTileRect(Point(nTilePosX, nTilePosY), Size(nTileWidth, nTileHeight));
+    SfxViewShell* pViewShell = SfxViewShell::GetFirst();
+    while (pViewShell)
+    {
+        LokChartHelper aChartHelper(pViewShell);
+        aChartHelper.PaintTile(rDevice, aTileRect);
+        pViewShell = SfxViewShell::GetNext(*pViewShell);
+    }
+    rDevice.Pop();
+}
+
+bool LokChartHelper::postMouseEvent(int nType, int nX, int nY,
+                                    int nCount, int nButtons, int nModifier,
+                                    double fScaleX, double fScaleY)
+{
+    Point aMousePos(nX, nY);
+    vcl::Window* pChartWindow = GetWindow();
+    if (pChartWindow)
+    {
+        tools::Rectangle rChartBBox = GetChartBoundingBox();
+        if (rChartBBox.IsInside(aMousePos))
+        {
+            int nChartWinX = nX - rChartBBox.Left();
+            int nChartWinY = nY - rChartBBox.Top();
+
+            // chart window expects pixels, but the conversion factor
+            // can depend on the client zoom
+            Point aPos(nChartWinX * fScaleX, nChartWinY * fScaleY);
+            MouseEvent aEvent(aPos, nCount,
+                    MouseEventModifiers::SIMPLECLICK, nButtons, nModifier);
+
+            switch (nType)
+            {
+            case LOK_MOUSEEVENT_MOUSEBUTTONDOWN:
+                pChartWindow->MouseButtonDown(aEvent);
+                break;
+            case LOK_MOUSEEVENT_MOUSEBUTTONUP:
+                pChartWindow->MouseButtonUp(aEvent);
+                if (pChartWindow->IsTracking())
+                    pChartWindow->EndTracking(TrackingEventFlags::DontCallHdl);
+                break;
+            case LOK_MOUSEEVENT_MOUSEMOVE:
+                pChartWindow->MouseMove(aEvent);
+                break;
+            default:
+                assert(false);
+                break;
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+bool LokChartHelper::setTextSelection(int nType, int nX, int nY)
+{
+    tools::Rectangle rChartBBox = GetChartBoundingBox();
+    if (rChartBBox.IsInside(Point(nX, nY)))
+    {
+        css::uno::Reference<css::frame::XDispatch> xDispatcher = GetXDispatcher();
+        if (xDispatcher.is())
+        {
+            int nChartWinX = nX - rChartBBox.Left();
+            int nChartWinY = nY - rChartBBox.Top();
+
+            // no scale here the chart controller expects twips
+            // that are converted to hmm
+            util::URL aURL;
+            aURL.Path = "LOKSetTextSelection";
+            uno::Sequence< beans::PropertyValue > aArgs(3);
+            aArgs[0].Value <<= static_cast<sal_Int32>(nType);
+            aArgs[1].Value <<= static_cast<sal_Int32>(nChartWinX);
+            aArgs[2].Value <<= static_cast<sal_Int32>(nChartWinY);
+            xDispatcher->dispatch(aURL, aArgs);
+        }
+        return true;
+    }
+    return false;
+}
+
+bool LokChartHelper::setGraphicSelection(int nType, int nX, int nY,
+                                         double fScaleX, double fScaleY)
+{
+    tools::Rectangle rChartBBox = GetChartBoundingBox();
+    if (rChartBBox.IsInside(Point(nX, nY)))
+    {
+        int nChartWinX = nX - rChartBBox.Left();
+        int nChartWinY = nY - rChartBBox.Top();
+
+        vcl::Window* pChartWindow = GetWindow();
+
+        Point aPos(nChartWinX * fScaleX, nChartWinY * fScaleY);
+        switch (nType)
+        {
+        case LOK_SETGRAPHICSELECTION_START:
+            {
+                MouseEvent aClickEvent(aPos, 1, MouseEventModifiers::SIMPLECLICK, MOUSE_LEFT);
+                pChartWindow->MouseButtonDown(aClickEvent);
+                MouseEvent aMoveEvent(aPos, 0, MouseEventModifiers::SIMPLEMOVE, MOUSE_LEFT);
+                pChartWindow->MouseMove(aMoveEvent);
+            }
+            break;
+        case LOK_SETGRAPHICSELECTION_END:
+            {
+                MouseEvent aMoveEvent(aPos, 0, MouseEventModifiers::SIMPLEMOVE, MOUSE_LEFT);
+                pChartWindow->MouseMove(aMoveEvent);
+                MouseEvent aClickEvent(aPos, 1, MouseEventModifiers::SIMPLECLICK, MOUSE_LEFT);
+                pChartWindow->MouseButtonUp(aClickEvent);
+            }
+            break;
+        default:
+            assert(false);
+            break;
+        }
+        return true;
+    }
+    return false;
+}
+
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

chart2/source/controller/inc/ChartController.hxx
@@ -488,6 +488,8 @@ private:
     void executeDispatch_ToggleGridHorizontal();
     void executeDispatch_ToggleGridVertical();
 
+    void executeDispatch_LOKSetTextSelection(int nType, int nX, int nY);
+
     void sendPopupRequest(OUString const & rCID, tools::Rectangle aRectangle);
 
     void impl_ShapeControllerDispatch( const css::util::URL& rURL,

chart2/source/controller/inc/ChartWindow.hxx
@@ -58,6 +58,7 @@ public:
     virtual void Invalidate( InvalidateFlags nFlags = InvalidateFlags::NONE ) override;
     virtual void Invalidate( const tools::Rectangle& rRect, InvalidateFlags nFlags = InvalidateFlags::NONE ) override;
     virtual void Invalidate( const vcl::Region& rRegion, InvalidateFlags nFlags = InvalidateFlags::NONE ) override;
+    virtual void LogicInvalidate( const tools::Rectangle* pRectangle ) override;
 
     virtual css::uno::Reference< css::accessibility::XAccessible > CreateAccessible() override;
 
@@ -65,9 +66,17 @@ public:
 
     ChartController* GetController();
 
+    virtual bool IsChart() const override { return true; }
+    vcl::Window* GetParentEditWin();
+
+private:
+    // returns the chart bounding box in twips
+    tools::Rectangle GetBoundingBox();
+
 private:
     ChartController* m_pWindowController;
     bool m_bInPaint;
+    VclPtr<vcl::Window> m_pViewShellWindow;
     VclPtr<vcl::Window> m_pOpenGLWindow;
 
     void adjustHighContrastMode();

chart2/source/controller/main/ChartController.cxx
@@ -1091,7 +1091,20 @@ void SAL_CALL ChartController::dispatch(
 {
     OUString aCommand = rURL.Path;
 
-    if(aCommand == "Paste")
+    if(aCommand == "LOKSetTextSelection")
+    {
+        if (rArgs.getLength() == 3)
+        {
+            sal_Int32 nType = -1;
+            rArgs[0].Value >>= nType;
+            sal_Int32 nX = 0;
+            rArgs[1].Value >>= nX;
+            sal_Int32 nY = 0;
+            rArgs[2].Value >>= nY;
+            executeDispatch_LOKSetTextSelection(nType, nX, nY);
+        }
+    }
+    else if(aCommand == "Paste")
         this->executeDispatch_Paste();
     else if(aCommand == "Copy" )
         this->executeDispatch_Copy();

chart2/source/controller/main/ChartController_Tools.cxx
@@ -52,6 +52,8 @@
 #include <com/sun/star/drawing/TextHorizontalAdjust.hpp>
 #include <com/sun/star/chart/ErrorBarStyle.hpp>
 
+#include <editeng/editview.hxx>
+#include <editeng/outliner.hxx>
 #include <svx/ActionDescriptionProvider.hxx>
 #include <svtools/transfer.hxx>
 #include <sot/storage.hxx>
@@ -69,6 +71,8 @@
 #include <svx/svdundo.hxx>
 #include <svx/unoapi.hxx>
 #include <svx/unopage.hxx>
+
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
 
 #include <memory>
 
@@ -920,6 +924,37 @@ void ChartController::executeDispatch_ToggleGridVertical()
         }
 
         aUndoGuard.commit();
+    }
+}
+
+void ChartController::executeDispatch_LOKSetTextSelection(int nType, int nX, int nY)
+{
+    if (m_pDrawViewWrapper)
+    {
+        if (m_pDrawViewWrapper->IsTextEdit())
+        {
+            OutlinerView* pOutlinerView = m_pDrawViewWrapper->GetTextEditOutlinerView();
+            if (pOutlinerView)
+            {
+                EditView& rEditView = pOutlinerView->GetEditView();
+                Point aPoint(convertTwipToMm100(nX), convertTwipToMm100(nY));
+                switch (nType)
+                {
+                    case LOK_SETTEXTSELECTION_START:
+                        rEditView.SetCursorLogicPosition(aPoint, /*bPoint=*/false, /*bClearMark=*/false);
+                        break;
+                    case LOK_SETTEXTSELECTION_END:
+                        rEditView.SetCursorLogicPosition(aPoint, /*bPoint=*/true, /*bClearMark=*/false);
+                        break;
+                    case LOK_SETTEXTSELECTION_RESET:
+                        rEditView.SetCursorLogicPosition(aPoint, /*bPoint=*/true, /*bClearMark=*/true);
+                        break;
+                    default:
+                        assert(false);
+                        break;
+                }
+            }
+        }
     }
 }
 

chart2/source/controller/main/ChartController_Window.cxx
@@ -888,7 +888,10 @@ void ChartController::execute_MouseButtonUp( const MouseEvent& rMEvt )
             else
                 m_aSelection.resetPossibleSelectionAfterSingleClickWasEnsured();
         }
-        else if( isDoubleClick(rMEvt) && !bMouseUpWithoutMouseDown /*#i106966#*/ )
+        // In tiled rendering drag mode could be not yet over on the call
+        // that should handle the double-click, so better to perform this check
+        // always.
+        if( isDoubleClick(rMEvt) && !bMouseUpWithoutMouseDown /*#i106966#*/ )
         {
             Point aMousePixel = rMEvt.GetPosPixel();
             execute_DoubleClick( &aMousePixel );

chart2/source/controller/main/ChartWindow.cxx
@@ -28,6 +28,13 @@
 #include <config_features.h>
 #include <com/sun/star/chart2/X3DChartWindowProvider.hpp>
 
+#include <sfx2/ipclient.hxx>
+#include <sfx2/viewsh.hxx>
+#include <sfx2/lokhelper.hxx>
+#include <comphelper/lok.hxx>
+
+#define TWIPS_PER_PIXEL 15
+
 using namespace ::com::sun::star;
 
 namespace
@@ -50,6 +57,7 @@ ChartWindow::ChartWindow( ChartController* pController, vcl::Window* pParent, Wi
         : Window(pParent, nStyle)
         , m_pWindowController( pController )
         , m_bInPaint(false)
+        , m_pViewShellWindow( nullptr )
 #if HAVE_FEATURE_OPENGL
         , m_pOpenGLWindow(VclPtr<OpenGLWindow>::Create(this, false))
 #else
@@ -94,6 +102,7 @@ void ChartWindow::dispose()
         xUpdatable->update();
     }
     m_pOpenGLWindow.disposeAndClear();
+    m_pViewShellWindow.clear();
     vcl::Window::dispose();
 }
 
@@ -108,6 +117,10 @@ void ChartWindow::PrePaint(vcl::RenderContext& )
 
 void ChartWindow::Paint(vcl::RenderContext& rRenderContext, const tools::Rectangle& rRect)
 {
+    if (comphelper::LibreOfficeKit::isActive()
+        && rRenderContext.GetOutDevType() != OutDevType::OUTDEV_VIRDEV)
+        return;
+
     m_bInPaint = true;
     if (m_pOpenGLWindow && m_pOpenGLWindow->IsVisible())
     {
@@ -301,6 +314,56 @@ void ChartWindow::Invalidate( const vcl::Region& rRegion, InvalidateFlags nFlags
     }
 }
 
+void ChartWindow::LogicInvalidate(const tools::Rectangle* pRectangle)
+{
+    OString sRectangle;
+    if (!pRectangle)
+    {
+        // we have to invalidate the whole chart area not the whole document
+        sRectangle = GetBoundingBox().toString();
+    }
+    else
+    {
+        tools::Rectangle aRectangle(*pRectangle);
+        // When dragging shapes the map mode is disabled.
+        if (IsMapModeEnabled())
+        {
+            if (GetMapMode().GetMapUnit() == MapUnit::Map100thMM)
+                aRectangle = OutputDevice::LogicToLogic(aRectangle, MapUnit::Map100thMM, MapUnit::MapTwip);
+        }
+        else
+        {
+            aRectangle = PixelToLogic(aRectangle, MapMode(MapUnit::MapTwip));
+        }
+
+        vcl::Window* pEditWin = GetParentEditWin();
+        if (pEditWin)
+        {
+            MapMode aCWMapMode = GetMapMode();
+            double fXScale = aCWMapMode.GetScaleX();
+            double fYScale = aCWMapMode.GetScaleY();
+
+            if (!IsMapModeEnabled())
+            {
+                aRectangle.Left() /= fXScale;
+                aRectangle.Right() /= fXScale;
+                aRectangle.Top() /= fYScale;
+                aRectangle.Bottom() /= fYScale;
+            }
+
+            Point aOffset = this->GetOffsetPixelFrom(*pEditWin);
+            aOffset.X() *= (TWIPS_PER_PIXEL / fXScale);
+            aOffset.Y() *= (TWIPS_PER_PIXEL / fYScale);
+
+            aRectangle = tools::Rectangle(aRectangle.TopLeft() + aOffset, aRectangle.GetSize());
+        }
+
+        sRectangle = aRectangle.toString();
+    }
+    SfxViewShell* pCurrentShell = SfxViewShell::Current();
+    SfxLokHelper::notifyInvalidation(pCurrentShell, sRectangle);
+}
+
 FactoryFunction ChartWindow::GetUITestFactory() const
 {
     return ChartWindowUIObject::create;
@@ -309,6 +372,53 @@ FactoryFunction ChartWindow::GetUITestFactory() const
 ChartController* ChartWindow::GetController()
 {
     return m_pWindowController;
+}
+
+vcl::Window* ChartWindow::GetParentEditWin()
+{
+    if (m_pViewShellWindow)
+        return m_pViewShellWindow.get();
+
+    // So, you are thinking, why do not invoke pCurrentShell->GetWindow() ?
+    // Because in Impress the parent edit win is not view shell window.
+    SfxViewShell* pCurrentShell = SfxViewShell::Current();
+    if( pCurrentShell )
+    {
+        SfxInPlaceClient* pIPClient = pCurrentShell->GetIPClient();
+        if (pIPClient)
+        {
+            vcl::Window* pRootWin = pIPClient->GetEditWin();
+            if(pRootWin && pRootWin->IsAncestorOf(*this))
+            {
+                m_pViewShellWindow = pRootWin;
+                return m_pViewShellWindow.get();
+            }
+        }
+    }
+    return nullptr;
+}
+
+tools::Rectangle ChartWindow::GetBoundingBox()
+{
+    tools::Rectangle aBBox;
+
+    vcl::Window* pRootWin = GetParentEditWin();
+    if (pRootWin)
+    {
+        // In all cases, the following code fragment
+        // returns the chart bounding box in twips.
+        MapMode aCWMapMode = GetMapMode();
+        double fXScale = aCWMapMode.GetScaleX();
+        double fYScale = aCWMapMode.GetScaleY();
+        Point aOffset = GetOffsetPixelFrom(*pRootWin);
+        aOffset.X() *= (TWIPS_PER_PIXEL / fXScale);
+        aOffset.Y() *= (TWIPS_PER_PIXEL / fYScale);
+        Size aSize = GetSizePixel();
+        aSize.Width() *= (TWIPS_PER_PIXEL / fXScale);
+        aSize.Height() *= (TWIPS_PER_PIXEL / fYScale);
+        aBBox = tools::Rectangle(aOffset, aSize);
+    }
+    return aBBox;
 }
 
 } //namespace chart

editeng/source/editeng/impedit.cxx
@@ -376,6 +376,17 @@ void ImpEditView::DrawSelection( EditSelection aTmpSel, vcl::Region* pRegion, Ou
             {
                 std::vector<tools::Rectangle> aRectangles;
                 pRegion->GetRegionRectangles(aRectangles);
+                if (pOutWin->IsChart())
+                {
+                    const vcl::Window* pViewShellWindow = mpViewShell->GetEditWindowForActiveOLEObj();
+                    if (pViewShellWindow && pViewShellWindow->IsAncestorOf(*pOutWin))
+                    {
+                        Point aOffsetPx = pOutWin->GetOffsetPixelFrom(*pViewShellWindow);
+                        Point aLogicOffset = pOutWin->PixelToLogic(aOffsetPx);
+                        for (tools::Rectangle& rRect : aRectangles)
+                            rRect.Move(aLogicOffset.getX(), aLogicOffset.getY());
+                    }
+                }
 
                 if (!aRectangles.empty())
                 {
@@ -1017,8 +1028,19 @@ void ImpEditView::ShowCursor( bool bGotoCursor, bool bForceVisCursor )
 
         if (comphelper::LibreOfficeKit::isActive() && mpViewShell)
         {
-            const Point& rPos = GetCursor()->GetPos();
-            tools::Rectangle aRect(rPos.getX(), rPos.getY(), rPos.getX() + GetCursor()->GetWidth(), rPos.getY() + GetCursor()->GetHeight());
+            Point aPos = GetCursor()->GetPos();
+            if (pOutWin->IsChart())
+            {
+                const vcl::Window* pViewShellWindow = mpViewShell->GetEditWindowForActiveOLEObj();
+                if (pViewShellWindow && pViewShellWindow->IsAncestorOf(*pOutWin))
+                {
+                    Point aOffsetPx = pOutWin->GetOffsetPixelFrom(*pViewShellWindow);
+                    Point aLogicOffset = pOutWin->PixelToLogic(aOffsetPx);
+                    aPos.Move(aLogicOffset.getX(), aLogicOffset.getY());
+                }
+            }
+
+            tools::Rectangle aRect(aPos.getX(), aPos.getY(), aPos.getX() + GetCursor()->GetWidth(), aPos.getY() + GetCursor()->GetHeight());
 
             // LOK output is always in twips, convert from mm100 if necessary.
             if (pOutWin->GetMapMode().GetMapUnit() == MapUnit::Map100thMM)

commit 8496273c18bba56009ac0d1b6e579357b6631498

android: hackaround for rendering regression (disable charts)
dc3dc406e8855aaa586a79ac25cd2c6b83eb8a43 broke rendering in the Android
Viewer - only blank pages were the result.
This hack just makes the offending function a non-op, allowing the
Viewer to render something to the viewer again.

Change-Id: Iaa206ff92b2e7642cb921c8e2d61ffd930a59463
Reviewed-on: https://gerrit.libreoffice.org/45493
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

sfx2/source/view/lokcharthelper.cxx
@@ -236,6 +236,10 @@ void LokChartHelper::PaintAllChartsOnTile(VirtualDevice& rDevice,
                                           int nTilePosX, int nTilePosY,
                                           long nTileWidth, long nTileHeight)
 {
+    // TODO: Hack to make Android Viewer render at least *something* again, but of
+    // course it's just a crude hack to just disable this instead of debugging further
+    // and fixing it for Android case
+#if !defined(ANDROID)
     // Resizes the virtual device so to contain the entries context
     rDevice.SetOutputSizePixel(Size(nOutputWidth, nOutputHeight));
 
@@ -259,6 +263,7 @@ void LokChartHelper::PaintAllChartsOnTile(VirtualDevice& rDevice,
         pViewShell = SfxViewShell::GetNext(*pViewShell);
     }
     rDevice.Pop();
+#endif
 }
 
 bool LokChartHelper::postMouseEvent(int nType, int nX, int nY,