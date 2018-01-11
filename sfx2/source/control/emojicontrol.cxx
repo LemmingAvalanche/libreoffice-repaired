LemmingAvalanche/libreoffice-repaired

commit 3faa4db0544a8dde87562d6f560b9b2f4f1680dc

use comphelper::InitPropertySequence in more places
Change-Id: I72d7b13a23ce306b752b39187a0e9fbb7028643a
Reviewed-on: https://gerrit.libreoffice.org/38606
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/control/emojicontrol.cxx
@@ -160,18 +160,16 @@ IMPL_LINK_NOARG(SfxEmojiControl, ActivatePageHdl, TabControl*, void)
 
 IMPL_STATIC_LINK(SfxEmojiControl, InsertHdl, ThumbnailViewItem*, pItem, void)
 {
-    OUStringBuffer sHexText = "";
-    sHexText.appendUtf32(pItem->getTitle().toUInt32(16));
+    OUString sHexText = OUString::number(pItem->getTitle().toUInt32(16));
 
     uno::Reference< uno::XComponentContext > xContext( comphelper::getProcessComponentContext() );
     OUString sFontName(officecfg::Office::Common::Misc::EmojiFont::get(xContext));
 
-    uno::Sequence<beans::PropertyValue> aArgs(2);
-    aArgs[0].Name = OUString::fromUtf8("Symbols");
-    aArgs[0].Value <<= sHexText.toString();
-    //add font settings here
-    aArgs[1].Name = OUString::fromUtf8("FontName");
-    aArgs[1].Value <<= sFontName;
+    uno::Sequence<beans::PropertyValue> aArgs( comphelper::InitPropertySequence({
+            { "Symbols", uno::Any(sHexText) },
+            // add font settings here
+            { "FontName", uno::Any(sFontName) }
+        }));
 
     comphelper::dispatchCommand(".uno:InsertSymbol", aArgs);
 }

chart2/qa/extras/PivotChartTest.cxx
@@ -188,10 +188,8 @@ uno::Reference<sheet::XDataPilotTable> lclGetPivotTableByName(sal_Int32 nIndex, 
 uno::Sequence<uno::Reference<chart2::data::XLabeledDataSequence>>
     lclGetCategories(Reference<chart2::XChartDocument> const & xChartDoc)
 {
-    uno::Sequence<beans::PropertyValue> aArguments(1);
-    aArguments[0] = beans::PropertyValue("CellRangeRepresentation", -1,
-                                         uno::makeAny<OUString>("PT@categories"),
-                                         beans::PropertyState_DIRECT_VALUE);
+    uno::Sequence<beans::PropertyValue> aArguments( comphelper::InitPropertySequence(
+            {{"CellRangeRepresentation", uno::Any(OUString("PT@categories"))}} ));
 
     uno::Reference<chart2::data::XDataProvider> xDataProvider(xChartDoc->getDataProvider(), uno::UNO_QUERY_THROW);
     return xDataProvider->createDataSource(aArguments)->getDataSequences();

chart2/source/model/main/ChartModel.cxx
@@ -41,6 +41,7 @@
 #include <com/sun/star/chart2/data/XPivotTableDataProvider.hpp>
 
 #include <comphelper/processfactory.hxx>
+#include <comphelper/propertysequence.hxx>
 #include <cppuhelper/supportsservice.hxx>
 
 #include <svl/numuno.hxx>
@@ -744,25 +745,12 @@ Reference< chart2::data::XDataSource > ChartModel::impl_createDefaultData()
                 xIni->initialize(aArgs);
             }
             //create data
-            uno::Sequence<beans::PropertyValue> aArgs(4);
-            aArgs[0] = beans::PropertyValue(
-                "CellRangeRepresentation", -1,
-                uno::Any( OUString("all") ), beans::PropertyState_DIRECT_VALUE );
-            aArgs[1] = beans::PropertyValue(
-                "HasCategories",
-                -1,
-                uno::Any( true ),
-                beans::PropertyState_DIRECT_VALUE );
-            aArgs[2] = beans::PropertyValue(
-                "FirstCellAsLabel",
-                -1,
-                uno::Any( true ),
-                beans::PropertyState_DIRECT_VALUE );
-            aArgs[3] = beans::PropertyValue(
-                "DataRowSource",
-                -1,
-                uno::Any( css::chart::ChartDataRowSource_COLUMNS ),
-                beans::PropertyState_DIRECT_VALUE );
+            uno::Sequence<beans::PropertyValue> aArgs( comphelper::InitPropertySequence({
+                { "CellRangeRepresentation", uno::Any( OUString("all") ) },
+                { "HasCategories", uno::Any( true ) },
+                { "FirstCellAsLabel", uno::Any( true ) },
+                { "DataRowSource", uno::Any( css::chart::ChartDataRowSource_COLUMNS ) }
+                }));
             xDataSource = m_xInternalDataProvider->createDataSource( aArgs );
         }
     }

i18npool/source/localedata/localedata.cxx
@@ -1293,8 +1293,7 @@ LocaleDataImpl::getContinuousNumberingLevels( const lang::Locale& rLocale )
         return pv;
     }
 
-    Sequence< Sequence<beans::PropertyValue> > seq1(0);
-    return seq1;
+    return Sequence< Sequence<beans::PropertyValue> >();
 }
 
 // OutlineNumbering helper class

linguistic/source/gciterator.cxx
@@ -54,6 +54,7 @@
 #include <cppuhelper/supportsservice.hxx>
 #include <i18nlangtag/languagetag.hxx>
 #include <comphelper/processfactory.hxx>
+#include <comphelper/propertysequence.hxx>
 #include <comphelper/extract.hxx>
 
 #include <deque>
@@ -490,16 +491,12 @@ uno::Reference< linguistic2::XProofreader > GrammarCheckingIterator::GetGrammarC
 static uno::Sequence<beans::PropertyValue>
 lcl_makeProperties(uno::Reference<text::XFlatParagraph> const& xFlatPara)
 {
-    uno::Sequence<beans::PropertyValue> ret(2);
     uno::Reference<beans::XPropertySet> const xProps(
             xFlatPara, uno::UNO_QUERY_THROW);
-    ret[0] = beans::PropertyValue("FieldPositions", -1,
-        xProps->getPropertyValue("FieldPositions"),
-        beans::PropertyState_DIRECT_VALUE);
-    ret[1] = beans::PropertyValue("FootnotePositions", -1,
-        xProps->getPropertyValue("FootnotePositions"),
-        beans::PropertyState_DIRECT_VALUE);
-    return ret;
+    return comphelper::InitPropertySequence({
+        { "FieldPositions", xProps->getPropertyValue("FieldPositions") },
+        { "FootnotePositions", xProps->getPropertyValue("FootnotePositions") }
+    });
 }
 
 void GrammarCheckingIterator::DequeueAndCheck()

oox/source/export/shapes.cxx
@@ -79,6 +79,7 @@
 #include <tools/stream.hxx>
 #include <tools/globname.hxx>
 #include <comphelper/classids.hxx>
+#include <comphelper/propertysequence.hxx>
 #include <comphelper/storagehelper.hxx>
 #include <sot/exchange.hxx>
 #include <utility>
@@ -275,11 +276,10 @@ static uno::Reference<io::XInputStream> lcl_StoreOwnAsOOXML(
         xContext->getServiceManager()->createInstanceWithContext(
             "com.sun.star.comp.MemoryStream", xContext),
         uno::UNO_QUERY_THROW);
-    uno::Sequence<beans::PropertyValue> args(2);
-    args[0].Name = "OutputStream";
-    args[0].Value <<= xTempStream->getOutputStream();
-    args[1].Name = "FilterName";
-    args[1].Value <<= OUString::createFromAscii(pFilterName);
+    uno::Sequence<beans::PropertyValue> args( comphelper::InitPropertySequence({
+            { "OutputStream", Any(xTempStream->getOutputStream()) },
+            { "FilterName", Any(OUString::createFromAscii(pFilterName)) }
+        }));
     uno::Reference<frame::XStorable> xStorable(xObj->getComponent(), uno::UNO_QUERY);
     try
     {

oox/source/vml/vmlformatting.cxx
@@ -35,6 +35,7 @@
 #include <oox/token/properties.hxx>
 #include <oox/token/tokens.hxx>
 #include <svx/svdtrans.hxx>
+#include <comphelper/propertysequence.hxx>
 
 namespace oox {
 namespace vml {
@@ -864,15 +865,12 @@ TextpathModel::TextpathModel()
 
 beans::PropertyValue lcl_createTextpathProps()
 {
-    uno::Sequence<beans::PropertyValue> aTextpathPropSeq(4);
-    aTextpathPropSeq[0].Name = "TextPath";
-    aTextpathPropSeq[0].Value <<= true;
-    aTextpathPropSeq[1].Name = "TextPathMode";
-    aTextpathPropSeq[1].Value <<= drawing::EnhancedCustomShapeTextPathMode_SHAPE;
-    aTextpathPropSeq[2].Name = "ScaleX";
-    aTextpathPropSeq[2].Value <<= false;
-    aTextpathPropSeq[3].Name = "SameLetterHeights";
-    aTextpathPropSeq[3].Value <<= false;
+    uno::Sequence<beans::PropertyValue> aTextpathPropSeq( comphelper::InitPropertySequence({
+            { "TextPath", uno::Any(true) },
+            { "TextPathMode", uno::Any(drawing::EnhancedCustomShapeTextPathMode_SHAPE) },
+            { "ScaleX", uno::Any(false) },
+            { "SameLetterHeights", uno::Any(false) }
+        }));
 
     beans::PropertyValue aRet;
     aRet.Name = "TextPath";

reportdesign/source/ui/dlg/PageNumber.cxx
@@ -29,6 +29,7 @@
 #include "UITools.hxx"
 #include "uistrings.hrc"
 #include "ReportController.hxx"
+#include <comphelper/propertysequence.hxx>
 #include <algorithm>
 
 namespace rptui
@@ -108,16 +109,11 @@ short OPageNumberDialog::Execute()
             if ( m_pAlignmentLst->GetSelectEntryPos() > 2 )
                 nPosX = nPos2X;
 
-            sal_Int32 nLength = 0;
-            uno::Sequence<beans::PropertyValue> aValues( 3 );
-            aValues[nLength].Name = PROPERTY_POSITION;
-            aValues[nLength++].Value <<= awt::Point(nPosX,0);
-
-            aValues[nLength].Name = PROPERTY_PAGEHEADERON;
-            aValues[nLength++].Value <<= m_pTopPage->IsChecked();
-
-            aValues[nLength].Name = PROPERTY_STATE;
-            aValues[nLength++].Value <<= m_pPageNofM->IsChecked();
+            uno::Sequence<beans::PropertyValue> aValues( comphelper::InitPropertySequence({
+                    { PROPERTY_POSITION, uno::Any(awt::Point(nPosX,0)) },
+                    { PROPERTY_PAGEHEADERON, uno::Any(m_pTopPage->IsChecked()) },
+                    { PROPERTY_STATE, uno::Any(m_pPageNofM->IsChecked()) }
+                }));
 
             m_pController->executeChecked(SID_INSERT_FLD_PGNUMBER,aValues);
         }

commit 75cbbf0eaea24cde2126191a9b5d7b864d5fe52c

tdf#114802 Emojis palette inserts code instead of emoji
regression from

    commit 3faa4db0544a8dde87562d6f560b9b2f4f1680dc
    use comphelper::InitPropertySequence in more places

Change-Id: I587242427c00ebf1faf44ad6b12090a39a1a3ef7
Reviewed-on: https://gerrit.libreoffice.org/47704
Reviewed-by: Jean-Baptiste Faure <jbfaure@libreoffice.org>
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/control/emojicontrol.cxx
@@ -160,13 +160,14 @@ IMPL_LINK_NOARG(SfxEmojiControl, ActivatePageHdl, TabControl*, void)
 
 IMPL_STATIC_LINK(SfxEmojiControl, InsertHdl, ThumbnailViewItem*, pItem, void)
 {
-    OUString sHexText = OUString::number(pItem->getTitle().toUInt32(16));
+    OUString sHexText = pItem->getTitle();
+    sal_uInt32 cEmojiChar = sHexText.toUInt32(16);
 
     uno::Reference< uno::XComponentContext > xContext( comphelper::getProcessComponentContext() );
     OUString sFontName(officecfg::Office::Common::Misc::EmojiFont::get(xContext));
 
     uno::Sequence<beans::PropertyValue> aArgs( comphelper::InitPropertySequence({
-            { "Symbols", uno::Any(sHexText) },
+            { "Symbols", uno::Any(OUString(&cEmojiChar, 1)) },
             // add font settings here
             { "FontName", uno::Any(sFontName) }
         }));