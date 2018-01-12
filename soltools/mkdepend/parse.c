LemmingAvalanche/libreoffice-repaired

commit 8f5a1116759fb5fa261dd135d94bea395db18cac

loplugin:unusedfields store..svl
Change-Id: I6070a683e5128271b84a10caccb548d07c950927
Reviewed-on: https://gerrit.libreoffice.org/39021
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

soltools/mkdepend/parse.c
@@ -34,11 +34,9 @@ char *hash_lookup( char *symbol, struct symhash *symbols );
 void hash_undefine( char *symbol, struct symhash *symbols );
 int gobble( struct filepointer *filep, struct inclist *file,
     struct inclist *file_red, struct symhash *symbols );
-int deftype ( char *line, struct filepointer *filep,
-    struct inclist *file_red, struct inclist *file,
+int deftype ( char *line, struct inclist *file,
     int parse_it, struct symhash *symbols);
-int zero_value(char *exp, struct filepointer *filep,
-    struct inclist *file_red, struct symhash *symbols);
+int zero_value(char *exp, struct symhash *symbols);
 
 extern struct symhash *maininclist;
 
@@ -49,7 +47,7 @@ int find_includes(struct filepointer *filep, struct inclist *file, struct inclis
     boolean recfailOK;
 
     while ((line = get_line(filep))) {
-        switch(type = deftype(line, filep, file_red, file, TRUE, symbols)) {
+        switch(type = deftype(line, file, TRUE, symbols)) {
         case IF:
         doif:
             type = find_includes(filep, file,
@@ -172,7 +170,7 @@ int gobble(struct filepointer *filep,
     int    type;
 
     while ((line = get_line(filep))) {
-        switch(type = deftype(line, filep, file_red, file, FALSE, symbols)) {
+        switch(type = deftype(line, file, FALSE, symbols)) {
         case IF:
         case IFFALSE:
         case IFGUESSFALSE:
@@ -217,7 +215,7 @@ int gobble(struct filepointer *filep,
 /*
  * Decide what type of # directive this line is.
  */
-int deftype (char *line, struct filepointer *filep, struct inclist *file_red, struct inclist *file, int parse_it, struct symhash *symbols)
+int deftype (char *line, struct inclist *file, int parse_it, struct symhash *symbols)
 {
     char   *p;
     char    *directive, savechar;
@@ -253,7 +251,7 @@ int deftype (char *line, struct filepointer *filep, struct inclist *file_red, st
          */
         debug(0,("%s, line %d: #elif %s ",
            file->i_file, filep->f_line, p));
-        ret = zero_value(p, filep, file_red, symbols);
+        ret = zero_value(p, symbols);
         if (ret != IF)
         {
           debug(0,("false...\n"));
@@ -282,7 +280,7 @@ int deftype (char *line, struct filepointer *filep, struct inclist *file_red, st
         /*
          * parse an expression.
          */
-        ret = zero_value(p, filep, file_red, symbols);
+        ret = zero_value(p, symbols);
         debug(0,("%s, line %d: %s #if %s\n",
              file->i_file, filep->f_line, ret?"false":"true", p));
         break;
@@ -372,10 +370,10 @@ char * isdefined( char *symbol )
 /*
  * Return type based on if the #if expression evaluates to 0
  */
-int zero_value(char *exp, struct filepointer *filep, struct inclist *file_red, struct symhash *symbols)
+int zero_value(char *exp, struct symhash *symbols)
 {
     global_symbols = symbols; /* HACK! see above */
-    if (cppsetup(exp, filep, file_red))
+    if (cppsetup(exp))
         return IFFALSE;
     else
         return IF;

include/store/types.h
@@ -120,12 +120,6 @@ typedef struct
      */
     sal_uInt32 m_nAttrib;
 
-    /** Size.
-        @see store_getStreamSize()
-        @see store_setStreamSize()
-     */
-    sal_uInt32 m_nSize;
-
     /** Reserved for internal use.
      */
     sal_uInt32 m_nReserved;

include/svl/ondemand.hxx
@@ -141,13 +141,11 @@ class OnDemandCalendarWrapper
     mutable std::unique_ptr<CalendarWrapper>
                                 pPtr;
     mutable bool                bValid;
-            bool                bInitialized;
 
 public:
                                 OnDemandCalendarWrapper()
                                     : pPtr(nullptr)
                                     , bValid(false)
-                                    , bInitialized(false)
                                     {}
 
             void                init(
@@ -158,7 +156,6 @@ public:
                                         m_xContext = rxContext;
                                         changeLocale( rLocale );
                                         pPtr.reset();
-                                        bInitialized = true;
                                     }
 
             void                changeLocale( const css::lang::Locale& rLocale )
@@ -252,12 +249,10 @@ class OnDemandNativeNumberWrapper
             css::uno::Reference< css::uno::XComponentContext > m_xContext;
     mutable std::unique_ptr<NativeNumberWrapper>
                                     pPtr;
-            bool                    bInitialized;
 
 public:
                                 OnDemandNativeNumberWrapper()
                                     : pPtr(nullptr)
-                                    , bInitialized(false)
                                     {}
 
             void                init(
@@ -266,7 +261,6 @@ public:
                                     {
                                         m_xContext = rxContext;
                                         pPtr.reset();
-                                        bInitialized = true;
                                     }
 
             NativeNumberWrapper*    get() const

soltools/mkdepend/cppsetup.c
@@ -114,11 +114,6 @@ yyerror(s)
 #else /* not CPP */
 
 #include "ifparser.h"
-struct parse_data {
-    struct filepointer *filep;
-    struct inclist *inc;
-    const char *line;
-};
 
 static const char *
 my_if_errors (IfParser *ip, const char *cp, const char *expecting)
@@ -199,19 +194,14 @@ my_eval_variable (IfParser *ip, const char *var, size_t len)
 }
 
 
-int cppsetup(char *line, struct filepointer *filep, struct inclist *inc)
+int cppsetup(char *line)
 {
     IfParser ip;
-    struct parse_data pd;
     int val = 0;
 
-    pd.filep = filep;
-    pd.inc = inc;
-    pd.line = line;
     ip.funcs.handle_error = my_if_errors;
     ip.funcs.eval_defined = my_eval_defined;
     ip.funcs.eval_variable = my_eval_variable;
-    ip.data = (char *) &pd;
 
     (void) ParseIfExpression (&ip, line, &val);
     if (val)

soltools/mkdepend/def.h
@@ -138,7 +138,6 @@ struct filepointer {
     char    *f_p;
     char    *f_base;
     char    *f_end;
-    long    f_len;
     long    f_line;
 };
 
@@ -172,8 +171,7 @@ int find_includes(struct filepointer *filep, struct inclist *file,
     struct IncludesCollection* incCollection, struct symhash *symbols);
 void included_by(struct inclist *ip,
     struct inclist * newfile);
-int cppsetup(char *line,
-    struct filepointer *filep, struct inclist *inc);
+int cppsetup(char *line);
 void add_include(struct filepointer *filep, struct inclist *file,
     struct inclist *file_red, char *include, boolean dot, boolean failOK,
     struct IncludesCollection* incCollection, struct symhash *symbols);

soltools/mkdepend/ifparser.h
@@ -68,7 +68,6 @@ typedef struct if_parser {
       int         (*eval_variable) (struct if_parser *, const char *, size_t);
       int         (*eval_defined)  (struct if_parser *, const char *, size_t);
     } funcs;
-    char *data;
 } IfParser;
 
 const char *ParseIfExpression (IfParser *, const char *, int *);

soltools/mkdepend/main.c
@@ -512,7 +512,6 @@ struct filepointer *getfile(char *file)
             fatalerr("makedepend:  Failed to read file \"%s\"\n", file);
 
     close(fd);
-    content->f_len = bytes_read+1;
     content->f_p = content->f_base;
     content->f_end = content->f_base + bytes_read;
     *content->f_end = '\0';

sot/source/sdstor/stgdir.cxx
@@ -67,7 +67,6 @@ void StgDirEntry::InitMembers()
     m_aSave       = m_aEntry;
     m_pUp         =
     m_pDown       = nullptr;
-    m_ppRoot      = nullptr;
     m_pStgStrm    = nullptr;
     m_pCurStrm    =
     m_pTmpStrm    = nullptr;
@@ -727,11 +726,9 @@ void StgDirEntry::Invalidate( bool bDel )
 StgDirStrm::StgDirStrm( StgIo& r )
           : StgDataStrm( r, r.m_aHdr.GetTOCStart(), -1 )
           , m_pRoot( nullptr )
-          , m_nEntries( 0 )
 {
     if( r.GetError() )
         return;
-    m_nEntries = m_nPageSize / STGENTRY_SIZE;
     if( m_nStart == STG_EOF )
     {
         StgEntry aRoot;
@@ -817,7 +814,6 @@ void StgDirStrm::SetupEntry( sal_Int32 n, StgDirEntry* pUpper )
                 ( reinterpret_cast<StgAvlNode**>( pUpper ? &pUpper->m_pDown : &m_pRoot ), pCur ) )
             {
                 pCur->m_pUp    = pUpper;
-                pCur->m_ppRoot = &m_pRoot;
             }
             else
             {
@@ -972,7 +968,6 @@ StgDirEntry* StgDirStrm::Create( StgDirEntry& rStg, const OUString& rName, StgEn
         if( StgAvlNode::Insert( reinterpret_cast<StgAvlNode**>(&rStg.m_pDown), pRes ) )
         {
             pRes->m_pUp    = &rStg;
-            pRes->m_ppRoot = &m_pRoot;
             pRes->m_bCreated =
             pRes->m_bDirty = true;
         }

commit a4cd74a469219a5d9246a212169328d00d1ffaec

tdf#114952 filep is still used in soltools/mkdepend/parse.c
regression from

    commit 8f5a1116759fb5fa261dd135d94bea395db18cac
    Date:   Tue Jun 20 19:54:23 2017 +0200
    loplugin:unusedfields store..svl

somewhat tricky to reproduce on linux, I had to do

    make soltools CPPFLAGS=-DDEBUG

Change-Id: I4b1989efc02b9e864432b96cf4f71a29e581bcdb
Reviewed-on: https://gerrit.libreoffice.org/47755
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

soltools/mkdepend/parse.c
@@ -34,7 +34,7 @@ char *hash_lookup( char *symbol, struct symhash *symbols );
 void hash_undefine( char *symbol, struct symhash *symbols );
 int gobble( struct filepointer *filep, struct inclist *file,
     struct inclist *file_red, struct symhash *symbols );
-int deftype ( char *line, struct inclist *file,
+int deftype ( char *line, struct filepointer *filep, struct inclist *file,
     int parse_it, struct symhash *symbols);
 int zero_value(char const *exp, struct symhash *symbols);
 
@@ -47,7 +47,7 @@ int find_includes(struct filepointer *filep, struct inclist *file, struct inclis
     boolean recfailOK;
 
     while ((line = get_line(filep))) {
-        switch(type = deftype(line, file, TRUE, symbols)) {
+        switch(type = deftype(line, filep, file, TRUE, symbols)) {
         case IF:
         doif:
             type = find_includes(filep, file,
@@ -170,7 +170,7 @@ int gobble(struct filepointer *filep,
     int    type;
 
     while ((line = get_line(filep))) {
-        switch(type = deftype(line, file, FALSE, symbols)) {
+        switch(type = deftype(line, filep, file, FALSE, symbols)) {
         case IF:
         case IFFALSE:
         case IFGUESSFALSE:
@@ -215,12 +215,12 @@ int gobble(struct filepointer *filep,
 /*
  * Decide what type of # directive this line is.
  */
-int deftype (char *line, struct inclist *file, int parse_it, struct symhash *symbols)
+int deftype (char *line, struct filepointer *filep, struct inclist *file, int parse_it, struct symhash *symbols)
 {
     char   *p;
     char    *directive, savechar;
     int    ret;
-
+    (void)filep;
     /*
      * Parse the directive...
      */