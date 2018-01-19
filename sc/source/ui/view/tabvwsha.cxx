LemmingAvalanche/libreoffice-repaired

commit 0f8bc804ecd192d107bfa075393c9af6ccb0df4f

lokdialog: Allow closing the Format Cell dialogs in any order.
Includes also lots of infrastructural changes, making the
conversion of the rest of the dialogs much easier.

StartExecuteAsync should be used in-place of StartExecuteModal
and the latter removed from the code-base incrementally. More
common code from Dialog::Execute should be moved to
ImplStartExecuteModal in a next step, as this is used more widely.

Change-Id: Idb2c1ec790e38f582438471a0419a56cdcf1439d
Reviewed-on: https://gerrit.libreoffice.org/47722
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Jan Holesovsky <kendy@collabora.com>
Tested-by: Jan Holesovsky <kendy@collabora.com>

sc/source/ui/view/tabvwsha.cxx
@@ -478,8 +478,8 @@ void ScTabViewShell::ExecuteCellFormatDlg(SfxRequest& rReq, const OString &rName
 
     const ScPatternAttr*    pOldAttrs       = GetSelectionPattern();
 
-    std::unique_ptr<SfxItemSet> pOldSet(new SfxItemSet(pOldAttrs->GetItemSet()));
-    std::unique_ptr<SvxNumberInfoItem> pNumberInfoItem;
+    std::shared_ptr<SfxItemSet> pOldSet(new SfxItemSet(pOldAttrs->GetItemSet()));
+    std::shared_ptr<SvxNumberInfoItem> pNumberInfoItem;
 
     pOldSet->MergeRange(SID_ATTR_BORDER_STYLES, SID_ATTR_BORDER_DEFAULT_WIDTH);
 
@@ -536,28 +536,30 @@ void ScTabViewShell::ExecuteCellFormatDlg(SfxRequest& rReq, const OString &rName
     ScAbstractDialogFactory* pFact = ScAbstractDialogFactory::Create();
     OSL_ENSURE(pFact, "ScAbstractFactory create fail!");
 
-    ScopedVclPtr<SfxAbstractTabDialog> pDlg(pFact->CreateScAttrDlg(GetDialogParent(), pOldSet.get()));
+    VclPtr<SfxAbstractTabDialog> pDlg(pFact->CreateScAttrDlg(GetDialogParent(), pOldSet.get()));
 
     if (!rName.isEmpty())
         pDlg->SetCurPageId(rName);
-    short nResult = pDlg->Execute();
-    bInFormatDialog = false;
 
-    if ( nResult == RET_OK )
-    {
-        const SfxItemSet* pOutSet = pDlg->GetOutputItemSet();
+    std::shared_ptr<SfxRequest> pRequest(new SfxRequest(rReq));
+    pDlg->StartExecuteAsync([=](sal_Int32 nResult){
+            bInFormatDialog = false;
 
-        const SfxPoolItem* pItem=nullptr;
-        if(pOutSet->GetItemState(SID_ATTR_NUMBERFORMAT_INFO,true,&pItem)==SfxItemState::SET)
-        {
+            if ( nResult == RET_OK )
+            {
+                const SfxItemSet* pOutSet = pDlg->GetOutputItemSet();
 
-            UpdateNumberFormatter(static_cast<const SvxNumberInfoItem&>(*pItem));
-        }
+                const SfxPoolItem* pItem=nullptr;
+                if(pOutSet->GetItemState(SID_ATTR_NUMBERFORMAT_INFO,true,&pItem)==SfxItemState::SET)
+                {
+                    UpdateNumberFormatter(static_cast<const SvxNumberInfoItem&>(*pItem));
+                }
 
-        ApplyAttributes(pOutSet, pOldSet.get());
+                ApplyAttributes(pOutSet, pOldSet.get());
 
-        rReq.Done( *pOutSet );
-    }
+                pRequest->Done(pOutSet);
+            }
+        }, pDlg);
 }
 
 bool ScTabViewShell::IsRefInputMode() const

include/sfx2/tabdlg.hxx
@@ -190,6 +190,7 @@ public:
 
     short               Execute() override;
     void                StartExecuteModal( const Link<Dialog&,void>& rEndDialogHdl ) override;
+    bool                StartExecuteAsync( VclAbstractDialog::AsyncContext &rCtx ) override;
     void                Start();
 
     const SfxItemSet*   GetExampleSet() const { return m_pExampleSet; }

include/vcl/abstdlg.hxx
@@ -25,6 +25,7 @@
 #include <vcl/vclptr.hxx>
 #include <vcl/vclreferencebase.hxx>
 #include <vector>
+#include <functional>
 
 namespace vcl { class Window; }
 class Dialog;
@@ -41,6 +42,23 @@ protected:
     virtual             ~VclAbstractDialog() override;
 public:
     virtual short       Execute() = 0;
+
+    struct AsyncContext {
+        VclPtr<VclReferenceBase> mxOwner;
+        std::function<void(sal_Int32)> maEndDialogFn;
+        bool isSet() { return !!maEndDialogFn; }
+    };
+
+    bool StartExecuteAsync(const std::function<void(sal_Int32)> &rEndDialogFn, VclPtr<VclReferenceBase> xOwner)
+    {
+        AsyncContext aCtx;
+        aCtx.mxOwner = xOwner;
+        aCtx.maEndDialogFn = rEndDialogFn;
+        return StartExecuteAsync(aCtx);
+    }
+
+    /// Commence execution of a modal dialog.
+    virtual bool StartExecuteAsync(AsyncContext &);
 
     // Screenshot interface
     virtual std::vector<OString> getAllPageUIXMLDescriptions() const;

include/vcl/dialog.hxx
@@ -26,6 +26,7 @@
 #include <vcl/syswin.hxx>
 #include <vcl/vclptr.hxx>
 #include <vcl/IDialogRenderable.hxx>
+#include <vcl/abstdlg.hxx>
 
 struct DialogImpl;
 class VclBox;
@@ -45,6 +46,7 @@ public:
 
 private:
     VclPtr<Dialog>  mpPrevExecuteDlg;
+    VclPtr<Dialog>  mpNextExecuteDlg;
     std::unique_ptr<DialogImpl>     mpDialogImpl;
     long            mnMousePositioned;
     bool            mbInExecute;
@@ -57,6 +59,7 @@ private:
     VclPtr<VclButtonBox> mpActionArea;
     VclPtr<VclBox>       mpContentArea;
 
+    SAL_DLLPRIVATE void    RemoveFromDlgList();
     SAL_DLLPRIVATE void    ImplInitDialogData();
     SAL_DLLPRIVATE void    ImplInitSettings();
     SAL_DLLPRIVATE VclPtr<vcl::Window> AddBorderWindow(vcl::Window* pParent, WinBits nBits);
@@ -137,6 +140,20 @@ private:
     static void     ImplEndExecuteModal();
     void            ImplSetModalInputMode(bool bModal);
 public:
+
+    // FIXME: Need to remove old StartExecuteModal in favour of this one.
+    /// Returns true of the dialog successfully starts
+    bool StartExecuteAsync(const std::function<void(sal_Int32)> &rEndDialogFn,
+                           VclPtr<VclReferenceBase> xOwner = VclPtr<VclReferenceBase>())
+    {
+        VclAbstractDialog::AsyncContext aCtx;
+        aCtx.mxOwner = xOwner;
+        aCtx.maEndDialogFn = rEndDialogFn;
+        return StartExecuteAsync(aCtx);
+    }
+
+    /// Commence execution of a modal dialog, disposes owner on failure
+    virtual bool    StartExecuteAsync(VclAbstractDialog::AsyncContext &rCtx);
 
     // Dialog::Execute replacement API
 

sc/source/ui/attrdlg/scdlgfact.hxx
@@ -65,6 +65,7 @@ public:                                             \
                      {}                             \
     virtual         ~Class() override;                       \
     virtual short   Execute() override ;            \
+    virtual bool    StartExecuteAsync(VclAbstractDialog::AsyncContext &rCtx) override; \
     std::vector<OString> getAllPageUIXMLDescriptions() const override; \
     bool selectPageByUIXMLDescription(const OString& rUIXMLDescription) override; \
     virtual Bitmap  createScreenshot() const override; \
@@ -78,6 +79,10 @@ short Class::Execute()                              \
 {                                                   \
     return pDlg->Execute();                         \
 }                                                   \
+bool Class::StartExecuteAsync(VclAbstractDialog::AsyncContext &rCtx)\
+{ \
+    return pDlg->StartExecuteAsync( rCtx ); \
+} \
 std::vector<OString> Class::getAllPageUIXMLDescriptions() const \
 {                                                   \
     return pDlg->getAllPageUIXMLDescriptions();     \

sfx2/source/control/request.cxx
@@ -155,6 +155,23 @@ SfxRequest::SfxRequest
         pImpl->SetPool( pArgs->GetPool() );
     else
         pImpl->SetPool( rOrig.pImpl->pPool );
+
+    // setup macro recording if it was in the original SfxRequest
+    if (rOrig.pImpl->pViewFrame && rOrig.pImpl->xRecorder.is())
+    {
+        nSlot = rOrig.nSlot;
+        pImpl->pViewFrame = rOrig.pImpl->pViewFrame;
+        if (pImpl->pViewFrame->GetDispatcher()->GetShellAndSlot_Impl(nSlot, &pImpl->pShell, &pImpl->pSlot, true, true))
+        {
+            pImpl->SetPool( &pImpl->pShell->GetPool() );
+            pImpl->xRecorder = SfxRequest::GetMacroRecorder(pImpl->pViewFrame);
+            pImpl->aTarget = pImpl->pShell->GetName();
+        }
+        else
+        {
+            SAL_WARN("sfx", "Recording unsupported slot: " << pImpl->pPool->GetSlotId(nSlot));
+        }
+    }
 }
 
 

sfx2/source/dialog/tabdlg.cxx
@@ -518,22 +518,19 @@ short SfxTabDialog::Execute()
         return RET_CANCEL;
     Start_Impl();
 
-    SfxViewShell* pViewShell = SfxViewShell::Current();
-    if (comphelper::LibreOfficeKit::isActive() && pViewShell && !GetLOKNotifier())
-    {
-        SetLOKNotifier(pViewShell);
-        const Size aSize = GetOptimalSize();
-        std::vector<vcl::LOKPayloadItem> aItems;
-        aItems.emplace_back("type", "dialog");
-        aItems.emplace_back("size", aSize.toString());
-        if (!GetText().isEmpty())
-            aItems.emplace_back("title", GetText().toUtf8());
-        pViewShell->notifyWindow(GetLOKWindowId(), "created", aItems);
-    }
-
     return TabDialog::Execute();
 }
 
+bool SfxTabDialog::StartExecuteAsync( VclAbstractDialog::AsyncContext &rCtx )
+{
+    if ( !m_pTabCtrl->GetPageCount() )
+    {
+        rCtx.mxOwner.disposeAndClear();
+        return false;
+    }
+    Start_Impl();
+    return TabDialog::StartExecuteAsync( rCtx );
+}
 
 void SfxTabDialog::StartExecuteModal( const Link<Dialog&,void>& rEndDialogHdl )
 {
@@ -590,6 +587,20 @@ void SfxTabDialog::Start_Impl()
 
     m_pTabCtrl->SetCurPageId( nActPage );
     ActivatePageHdl( m_pTabCtrl );
+
+    SfxViewShell* pViewShell = SfxViewShell::Current();
+
+    if (comphelper::LibreOfficeKit::isActive() && pViewShell && !GetLOKNotifier())
+    {
+        SetLOKNotifier(pViewShell);
+        const Size aSize = GetOptimalSize();
+        std::vector<vcl::LOKPayloadItem> aItems;
+        aItems.emplace_back("type", "dialog");
+        aItems.emplace_back("size", aSize.toString());
+        if (!GetText().isEmpty())
+            aItems.emplace_back("title", GetText().toUtf8());
+        pViewShell->notifyWindow(GetLOKWindowId(), "created", aItems);
+    }
 }
 
 void SfxTabDialog::AddTabPage( sal_uInt16 nId, const OUString &rRiderText )

vcl/source/window/abstdlg.cxx
@@ -58,6 +58,12 @@ VclAbstractDialog::~VclAbstractDialog()
 {
 }
 
+bool VclAbstractDialog::StartExecuteAsync(AsyncContext &)
+{
+    assert(false);
+    return false;
+}
+
 std::vector<OString> VclAbstractDialog::getAllPageUIXMLDescriptions() const
 {
     // default has no pages

commit e7ad82dabce2fb8cc7975f73d370d0cab0b90ec7

SfxReqest::Done takes a SfxItemSet& or a boolean, not a SfxItemSet*
regression from

commit 0f8bc804ecd192d107bfa075393c9af6ccb0df4f
Date:   Fri Jan 12 11:57:22 2018 +0000

Change-Id: Ia97ba286a2da0584a2e806ad7396a3633f9d913e
Reviewed-on: https://gerrit.libreoffice.org/48160
Reviewed-by: Jan Holesovsky <kendy@collabora.com>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sc/source/ui/view/tabvwsha.cxx
@@ -551,8 +551,6 @@ void ScTabViewShell::ExecuteCellFormatDlg(SfxRequest& rReq, const OString &rName
             {
                 const SfxItemSet* pOutSet = pDlg->GetOutputItemSet();
 
-                assert(pOutSet);
-
                 const SfxPoolItem* pItem=nullptr;
                 if(pOutSet->GetItemState(SID_ATTR_NUMBERFORMAT_INFO,true,&pItem)==SfxItemState::SET)
                 {
@@ -561,7 +559,7 @@ void ScTabViewShell::ExecuteCellFormatDlg(SfxRequest& rReq, const OString &rName
 
                 ApplyAttributes(pOutSet, pOldSet.get());
 
-                pRequest->Done(pOutSet);
+                pRequest->Done(*pOutSet);
             }
         }, pDlg);
 }