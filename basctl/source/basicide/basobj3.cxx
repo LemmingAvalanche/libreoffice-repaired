LemmingAvalanche/libreoffice-repaired

commit 81475a99aeb4ab48afde837b6cfdbf185cb0455c

loplugin:constparams in basctl
Change-Id: If6c2b980a2916c4ee8ac108fbb84b006a35f49c5
Reviewed-on: https://gerrit.libreoffice.org/40570
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

basctl/source/basicide/basobj3.cxx
@@ -44,9 +44,9 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::container;
 
 extern "C" {
-    SAL_DLLPUBLIC_EXPORT long basicide_handle_basic_error( void* pPtr )
+    SAL_DLLPUBLIC_EXPORT long basicide_handle_basic_error( void const * pPtr )
     {
-        return HandleBasicError( static_cast<StarBASIC*>(pPtr) );
+        return HandleBasicError( static_cast<StarBASIC const *>(pPtr) );
     }
 }
 
@@ -212,7 +212,7 @@ StarBASIC* FindBasic( const SbxVariable* pVar )
     return const_cast<StarBASIC*>(static_cast<const StarBASIC*>(pSbx));
 }
 
-BasicManager* FindBasicManager( StarBASIC* pLib )
+BasicManager* FindBasicManager( StarBASIC const * pLib )
 {
     ScriptDocuments aDocuments( ScriptDocument::getAllScriptDocuments( ScriptDocument::AllWithApplication ) );
     for (   ScriptDocuments::const_iterator doc = aDocuments.begin();
@@ -263,7 +263,7 @@ void MarkDocumentModified( const ScriptDocument& rDocument )
     }
 }
 
-void RunMethod( SbMethod* pMethod )
+void RunMethod( SbMethod const * pMethod )
 {
     SbxValues aRes;
     aRes.eType = SbxVOID;
@@ -355,7 +355,7 @@ void InvalidateDebuggerSlots()
     }
 }
 
-long HandleBasicError( StarBASIC* pBasic )
+long HandleBasicError( StarBASIC const * pBasic )
 {
     EnsureIde();
     BasicStopped();

basctl/inc/iderdll.hxx
@@ -30,7 +30,7 @@ void EnsureIde ();
 
 void ShellCreated (Shell* pShell);
 Shell* GetShell();
-void ShellDestroyed (Shell* pShell);
+void ShellDestroyed (Shell const * pShell);
 
 ExtraData* GetExtraData();
 

basctl/source/basicide/baside2.cxx
@@ -177,7 +177,7 @@ void lcl_ConvertTabsToSpaces( OUString& rLine )
 } // namespace
 
 ModulWindow::ModulWindow (ModulWindowLayout* pParent, ScriptDocument const& rDocument,
-                          const OUString& aLibName, const OUString& aName, OUString& aModule)
+                          const OUString& aLibName, const OUString& aName, OUString const & aModule)
     : BaseWindow(pParent, rDocument, aLibName, aName)
     , m_rLayout(*pParent)
     , m_nValid(ValidWindow)
@@ -587,7 +587,7 @@ void ModulWindow::ManageBreakPoints()
 }
 
 
-bool ModulWindow::BasicErrorHdl( StarBASIC * pBasic )
+bool ModulWindow::BasicErrorHdl( StarBASIC const * pBasic )
 {
     GetShell()->GetViewFrame()->ToTop();
 

basctl/source/basicide/baside2.hxx
@@ -157,7 +157,7 @@ public:
     void            ChangeFontColor( Color aColor );
     void            UpdateSyntaxHighlighting ();
 
-    bool            GetProcedureName(OUString& rLine, OUString& rProcType, OUString& rProcName) const;
+    bool            GetProcedureName(OUString const & rLine, OUString& rProcType, OUString& rProcName) const;
 };
 
 
@@ -322,7 +322,7 @@ protected:
     virtual void    DoScroll( ScrollBar* pCurScrollBar ) override;
 
 public:
-    ModulWindow( ModulWindowLayout* pParent, const ScriptDocument& rDocument, const OUString& aLibName, const OUString& aName, OUString& aModule );
+    ModulWindow( ModulWindowLayout* pParent, const ScriptDocument& rDocument, const OUString& aLibName, const OUString& aName, OUString const & aModule );
 
                     virtual ~ModulWindow() override;
     virtual void    dispose() override;
@@ -361,7 +361,7 @@ public:
     void            UpdateBreakPoint( const BreakPoint& rBrk );
     void            BasicAddWatch();
 
-    bool            BasicErrorHdl( StarBASIC* pBasic );
+    bool            BasicErrorHdl( StarBASIC const * pBasic );
     BasicDebugFlags BasicBreakHdl();
     void            AssertValidEditEngine();
 

basctl/source/basicide/baside2b.cxx
@@ -122,7 +122,7 @@ void setTextEngineText (ExtTextEngine& rEngine, OUString const& aStr)
 namespace
 {
 
-void lcl_DrawIDEWindowFrame(DockingWindow* pWin, vcl::RenderContext& rRenderContext)
+void lcl_DrawIDEWindowFrame(DockingWindow const * pWin, vcl::RenderContext& rRenderContext)
 {
     if (pWin->IsFloatingMode())
         return;
@@ -783,7 +783,7 @@ void EditorWindow::HandleProcedureCompletion()
     }
 }
 
-bool EditorWindow::GetProcedureName(OUString& rLine, OUString& rProcType, OUString& rProcName) const
+bool EditorWindow::GetProcedureName(OUString const & rLine, OUString& rProcType, OUString& rProcName) const
 {
     std::vector<HighlightPortion> aPortions;
     aHighlighter.getHighlightPortions(rLine, aPortions);

basctl/source/basicide/basides1.cxx
@@ -1162,7 +1162,7 @@ VclPtr<BaseWindow> Shell::FindWindow(
     return nullptr;
 }
 
-bool Shell::CallBasicErrorHdl( StarBASIC* pBasic )
+bool Shell::CallBasicErrorHdl( StarBASIC const * pBasic )
 {
     bool bRet = false;
     VclPtr<ModulWindow> pModWin = ShowActiveModuleWindow( pBasic );
@@ -1171,7 +1171,7 @@ bool Shell::CallBasicErrorHdl( StarBASIC* pBasic )
     return bRet;
 }
 
-BasicDebugFlags Shell::CallBasicBreakHdl( StarBASIC* pBasic )
+BasicDebugFlags Shell::CallBasicBreakHdl( StarBASIC const * pBasic )
 {
     BasicDebugFlags nRet = BasicDebugFlags::NONE;
     VclPtr<ModulWindow> pModWin = ShowActiveModuleWindow( pBasic );
@@ -1201,7 +1201,7 @@ BasicDebugFlags Shell::CallBasicBreakHdl( StarBASIC* pBasic )
     return nRet;
 }
 
-VclPtr<ModulWindow> Shell::ShowActiveModuleWindow( StarBASIC* pBasic )
+VclPtr<ModulWindow> Shell::ShowActiveModuleWindow( StarBASIC const * pBasic )
 {
     SetCurLib( ScriptDocument::getApplicationScriptDocument(), OUString(), false );
 

basctl/source/basicide/bastype2.cxx
@@ -581,7 +581,7 @@ void TreeListBox::UpdateEntries()
 }
 
 // Removes the entry from the tree.
-void TreeListBox::RemoveEntry (SvTreeListEntry* pEntry)
+void TreeListBox::RemoveEntry (SvTreeListEntry const * pEntry)
 {
     // removing the associated user data
     delete static_cast<Entry*>(pEntry->GetUserData());
@@ -780,7 +780,7 @@ void TreeListBox::GetRootEntryBitmaps( const ScriptDocument& rDocument, Image& r
     }
 }
 
-void TreeListBox::SetCurrentEntry (EntryDescriptor& rDesc)
+void TreeListBox::SetCurrentEntry (EntryDescriptor const & rDesc)
 {
     SvTreeListEntry* pCurEntry = nullptr;
     EntryDescriptor aDesc = rDesc;

basctl/source/basicide/iderdll.cxx
@@ -91,7 +91,7 @@ void ShellCreated (Shell* pShell)
         pDll->SetShell(pShell);
 }
 
-void ShellDestroyed (Shell* pShell)
+void ShellDestroyed (Shell const * pShell)
 {
     Dll* pDll = theDllInstance::get().get();
     if (pDll && pDll->GetShell() == pShell)

commit 28de77e85a2b38812dd41d859a3a868bfe8eee08

Fix callback function type
...that got broken with 81475a99aeb4ab48afde837b6cfdbf185cb0455c
"loplugin:constparams in basctl", causing -fsanitize=function to complain:

> sfx2/source/appl/app.cxx:452:28: runtime error: call to function basicide_handle_basic_error through pointer to incorrect function type 'long (*)(void *)'
> basctl/source/basicide/basobj3.cxx:48: note: basicide_handle_basic_error defined here
>     #0 0x7ff6340234aa in SfxApplication::GlobalBasicErrorHdl_Impl(SfxApplication*, StarBASIC*) sfx2/source/appl/app.cxx:452:28
>     #1 0x7ff63401cbef in SfxApplication::LinkStubGlobalBasicErrorHdl_Impl(void*, StarBASIC*) sfx2/source/appl/app.cxx:434:1
>     #2 0x7ff639142148 in Link<StarBASIC*, bool>::Call(StarBASIC*) const include/tools/link.hxx:84:45
>     #3 0x7ff63911f350 in StarBASIC::RTError(ErrCode, rtl::OUString const&, int, int, int) basic/source/classes/sb.cxx:1681:37
>     #4 0x7ff63965aafe in SbiInstance::Abort() basic/source/runtime/runtime.cxx:518:16
>     #5 0x7ff639664363 in SbiRuntime::Step() basic/source/runtime/runtime.cxx:865:28
>     #6 0x7ff6392ec321 in SbModule::Run(SbMethod*) basic/source/classes/sbxmod.cxx:1141:25
[...]

Change-Id: I66bd1ca720cebca4fb5fd52d86920de3fc7a604e
Reviewed-on: https://gerrit.libreoffice.org/48142
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

basctl/source/basicide/basobj3.cxx
@@ -44,7 +44,7 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::container;
 
 extern "C" {
-    SAL_DLLPUBLIC_EXPORT long basicide_handle_basic_error( void const * pPtr )
+    SAL_DLLPUBLIC_EXPORT long basicide_handle_basic_error( void * pPtr )
     {
         return HandleBasicError( static_cast<StarBASIC const *>(pPtr) );
     }