LemmingAvalanche/libreoffice-repaired

commit feb75e69efde5f46e5ec7ef1fdbf01ca857c7411

Optimize plain-text import.
Cost of managing rsids is very significant >50% of import time, new
Rsids will be generated for changes anyway.
Also avoid un-necessary splitting of paragraphs on new-line.

Change-Id: Ib959306f187f93bd3a85ead11fd2342b99d4ece3
Reviewed-on: https://gerrit.libreoffice.org/36938
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

sw/source/filter/ascii/parasc.cxx
@@ -481,7 +481,7 @@ sal_uLong SwASCIIParser::ReadChars()
         {
             // We found a CR/LF, thus save the text
             InsertText( OUString( pLastStt ));
-            pDoc->getIDocumentContentOperations().SplitNode( *pPam->GetPoint(), false );
+            pDoc->getIDocumentContentOperations().AppendTextNode( *pPam->GetPoint() );
             pLastStt = pStt;
             nLineLen = 0;
         }
@@ -498,8 +498,6 @@ sal_uLong SwASCIIParser::ReadChars()
 void SwASCIIParser::InsertText( const OUString& rStr )
 {
     pDoc->getIDocumentContentOperations().InsertString( *pPam, rStr );
-    pDoc->UpdateRsid( *pPam, rStr.getLength() );
-    pDoc->UpdateParRsid( pPam->GetPoint()->nNode.GetNode().GetTextNode() );
 
     if( pItemSet && g_pBreakIt && nScript != ( SvtScriptType::LATIN |
                                              SvtScriptType::ASIAN |

commit 709744780d980c6d1e61fbf374399f3262838590

tdf#115088: Pasting multi-line text from external source behaves strangely
Regression from:
feb75e69efde5f46e5ec7ef1fdbf01ca857c7411

AppendTextNode might be sufficient for import (when there is no
content in the document), but for clipboard operations better to
use the original SplitNode method.

Change-Id: I7de3e9bc9c5356acb0b4cc43927a0d39079522b2
Reviewed-on: https://gerrit.libreoffice.org/48124
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

sw/source/filter/ascii/parasc.cxx
@@ -472,7 +472,10 @@ ErrCode SwASCIIParser::ReadChars()
         {
             // We found a CR/LF, thus save the text
             InsertText( OUString( pLastStt ));
-            pDoc->getIDocumentContentOperations().AppendTextNode( *pPam->GetPoint() );
+            if(bNewDoc)
+                pDoc->getIDocumentContentOperations().AppendTextNode( *pPam->GetPoint() );
+            else
+                pDoc->getIDocumentContentOperations().SplitNode( *pPam->GetPoint(), false );
             pLastStt = pStt;
             nLineLen = 0;
         }