LemmingAvalanche/libreoffice-repaired

commit b25f62e910bb5e9ab8d3fc4cbd307eeaea96e7af

new loplugin: refcounting
This was a feature requested by mmeeks, as a result of
tdf#92611.

It validates that things that extend XInterface are not
directly heap/stack-allocated, but have their lifecycle managed
via css::uno::Reference or rtl::Reference.

Change-Id: I28e3b8b236f6a4a56d0a6d6f26ad54e44b36e692
Reviewed-on: https://gerrit.libreoffice.org/16924
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

dbaccess/source/core/dataaccess/datasource.cxx
@@ -491,7 +491,7 @@ ODatabaseSource::ODatabaseSource(const ::rtl::Reference<ODatabaseModelImpl>& _pI
             :ModelDependentComponent( _pImpl )
             ,ODatabaseSource_Base( getMutex() )
             ,OPropertySetHelper( ODatabaseSource_Base::rBHelper )
-            ,m_aBookmarks( *this, getMutex() )
+            ,m_xBookmarks( new OBookmarkContainer( *this, getMutex() ) )
             ,m_aFlushListeners( getMutex() )
 {
     // some kind of default
@@ -1200,7 +1200,7 @@ Reference< XConnection > ODatabaseSource::getConnection(const OUString& user, co
 Reference< XNameAccess > SAL_CALL ODatabaseSource::getBookmarks(  ) throw (RuntimeException, std::exception)
 {
     ModelMethodGuard aGuard( *this );
-    return static_cast< XNameContainer* >(&m_aBookmarks);
+    return static_cast< XNameContainer* >(m_xBookmarks.get());
 }
 
 Reference< XNameAccess > SAL_CALL ODatabaseSource::getQueryDefinitions( ) throw(RuntimeException, std::exception)

dbaccess/source/core/dataaccess/datasource.hxx
@@ -82,7 +82,7 @@ class ODatabaseSource   :public ModelDependentComponent // must be first
 
 private:
     using ODatabaseSource_Base::rBHelper;
-    OBookmarkContainer                      m_aBookmarks;
+    css::uno::Reference<OBookmarkContainer> m_xBookmarks;
     ::cppu::OInterfaceContainerHelper       m_aFlushListeners;
 
 private:

commit 6e753c4ca0abe4aa631b547c27fd5378600423bb

tdf#114596 dbaccess: fix mysterious dataloss bug
OBookmarkContainer actually re-uses the reference count of
ODatabaseSource, so converting ODatabaseSource::m_aBookmark to Reference
created a cycle, which somehow causes the dataloss, because evidently
something as important as storing the data must be done in the
destructor.

(regression from b25f62e910bb5e9ab8d3fc4cbd307eeaea96e7af)

Change-Id: I4cad01dc9cdaf405c1eb31d6c0e161eb6712b78f

dbaccess/source/core/dataaccess/datasource.cxx
@@ -475,7 +475,7 @@ ODatabaseSource::ODatabaseSource(const ::rtl::Reference<ODatabaseModelImpl>& _pI
             :ModelDependentComponent( _pImpl )
             ,ODatabaseSource_Base( getMutex() )
             ,OPropertySetHelper( ODatabaseSource_Base::rBHelper )
-            ,m_xBookmarks( new OBookmarkContainer( *this, getMutex() ) )
+            , m_Bookmarks(*this, getMutex())
             ,m_aFlushListeners( getMutex() )
 {
     // some kind of default
@@ -1179,7 +1179,8 @@ Reference< XConnection > ODatabaseSource::getConnection(const OUString& user, co
 Reference< XNameAccess > SAL_CALL ODatabaseSource::getBookmarks(  )
 {
     ModelMethodGuard aGuard( *this );
-    return static_cast< XNameContainer* >(m_xBookmarks.get());
+    // tdf#114596 this may look nutty but see OBookmarkContainer::aquire()
+    return static_cast<XNameContainer*>(&m_Bookmarks);
 }
 
 Reference< XNameAccess > SAL_CALL ODatabaseSource::getQueryDefinitions( )

dbaccess/source/core/dataaccess/datasource.hxx
@@ -82,7 +82,8 @@ class ODatabaseSource   :public ModelDependentComponent // must be first
 
 private:
     using ODatabaseSource_Base::rBHelper;
-    rtl::Reference<OBookmarkContainer> m_xBookmarks;
+    // note: this thing uses the ref-count of "this", see OBookmarkContainer::acquire!
+    OBookmarkContainer m_Bookmarks;
     ::comphelper::OInterfaceContainerHelper2       m_aFlushListeners;
 
 private: