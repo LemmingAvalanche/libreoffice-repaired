LemmingAvalanche/libreoffice-repaired

commit ed61a3148399b5296ab624836f4a7e8321b51693

ofz: divide-by-zero
Change-Id: Ie9a21a1432a98af3dca9a397057b7887ff30375f

filter/source/graphicfilter/itiff/itiff.cxx
@@ -1345,11 +1345,21 @@ bool TIFFReader::ReadTIFF(SvStream & rTIFF, Graphic & rGraphic )
                 else
                     nPlanes = nSamplesPerPixel;
 
+                bStatus = nPlanes != 0;
+            }
+
+            sal_uInt32 nDiv = GetRowsPerStrip() + 1;
+
+            if ( bStatus )
+            {
+                bStatus = (nDiv != 0);
+            }
+
+            if ( bStatus )
+            {
                 if ( ( nFillOrder == 2 ) && ( nCompression != 5 ) )     // in the LZW mode bits are already being inverted
                     bByteSwap = true;
-
-                nStripsPerPlane = ( nImageLength - 1 ) / GetRowsPerStrip() + 1;
-                bStatus = nPlanes != 0;
+                nStripsPerPlane = ( nImageLength - 1 ) / nDiv;
             }
 
             if ( bStatus )

commit d70ce7c831b3415a1a413140480ab21afa9d6a41

tdf#115863: fix TIFF import
Regression from ed61a3148399b5296ab624836f4a7e8321b51693

Change-Id: Id1604fb8eb11ea4e39846591e3912b40a7e7cc96
Reviewed-on: https://gerrit.libreoffice.org/50269
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Aron Budea <aron.budea@collabora.com>

filter/source/graphicfilter/itiff/itiff.cxx
@@ -1394,7 +1394,7 @@ bool TIFFReader::ReadTIFF(SvStream & rTIFF, Graphic & rGraphic )
                 bStatus = nPlanes != 0;
             }
 
-            sal_uInt32 nDiv = GetRowsPerStrip() + 1;
+            sal_uInt32 nDiv = GetRowsPerStrip();
 
             if ( bStatus )
             {
@@ -1405,7 +1405,7 @@ bool TIFFReader::ReadTIFF(SvStream & rTIFF, Graphic & rGraphic )
             {
                 if ( ( nFillOrder == 2 ) && ( nCompression != 5 ) )     // in the LZW mode bits are already being inverted
                     bByteSwap = true;
-                nStripsPerPlane = ( nImageLength - 1 ) / nDiv;
+                nStripsPerPlane = ( nImageLength - 1 ) / nDiv + 1;
             }
 
             if ( bStatus )