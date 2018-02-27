LemmingAvalanche/libreoffice-repaired

commit 4125c6ee21b6bffc17d38fad6d8f864faa365ec0

templatize SvXMLEnumMapEntry
in preparation for "scoped UNO enums".

This is a little hacky: In order to limit the scope of this change,
the templated SvXMLEnumMapEntry struct actually has a fixed size field,
and we cast it to SvXMLEnumMapEntry<sal_uInt16>* in various
places, to avoid carrying the type param around.

Change-Id: Idfbc5561303c557598dd5564b7a7259ae5261d83
Reviewed-on: https://gerrit.libreoffice.org/34987
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmloff/source/style/XMLFootnoteSeparatorExport.cxx
@@ -55,7 +55,7 @@ void XMLFootnoteSeparatorExport::exportXML(
     assert(pProperties);
 
     // initialize values
-    sal_Int16 eLineAdjust = text::HorizontalAdjust_LEFT;
+    text::HorizontalAdjust eLineAdjust = text::HorizontalAdjust_LEFT;
     sal_Int32 nLineColor = 0;
     sal_Int32 nLineDistance = 0;
     sal_Int8 nLineRelWidth = 0;
@@ -129,7 +129,7 @@ void XMLFootnoteSeparatorExport::exportXML(
     }
 
     // line style
-    static const SvXMLEnumMapEntry aXML_LineStyle_Enum[] =
+    static const SvXMLEnumMapEntry<sal_Int8> aXML_LineStyle_Enum[] =
     {
         { XML_NONE,     0 },
         { XML_SOLID,    1 },
@@ -145,12 +145,12 @@ void XMLFootnoteSeparatorExport::exportXML(
     }
 
     // adjustment
-    static const SvXMLEnumMapEntry aXML_HorizontalAdjust_Enum[] =
+    static const SvXMLEnumMapEntry<text::HorizontalAdjust> aXML_HorizontalAdjust_Enum[] =
     {
         { XML_LEFT,     text::HorizontalAdjust_LEFT },
         { XML_CENTER,   text::HorizontalAdjust_CENTER },
         { XML_RIGHT,    text::HorizontalAdjust_RIGHT },
-        { XML_TOKEN_INVALID, 0 }
+        { XML_TOKEN_INVALID, (text::HorizontalAdjust)0 }
     };
 
     if (SvXMLUnitConverter::convertEnum(

commit 3519ee6e9c0be404fb0c2246e3287fa63bc20dea

tdf#115432 footnote separator position remain in the left side
regression from
    commit 4125c6ee21b6bffc17d38fad6d8f864faa365ec0
    "templatize SvXMLEnumMapEntry"

Change-Id: Id29e7c07be637adcdc7fe624a1673beca9829d4d
Reviewed-on: https://gerrit.libreoffice.org/50420
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/unocore/unomap1.cxx
@@ -58,6 +58,7 @@
 #include <com/sun/star/table/TableBorder2.hpp>
 #include <com/sun/star/table/TableBorderDistances.hpp>
 #include <com/sun/star/table/XCell.hpp>
+#include <com/sun/star/text/HorizontalAdjust.hpp>
 #include <com/sun/star/text/GraphicCrop.hpp>
 #include <com/sun/star/text/PageNumberType.hpp>
 #include <com/sun/star/text/SectionFileLink.hpp>
@@ -583,7 +584,7 @@ const SfxItemPropertyMapEntry*  SwUnoPropertyMapProvider::GetPageStylePropertyMa
         { OUString(UNO_NAME_FOOTNOTE_LINE_COLOR), FN_PARAM_FTN_INFO,        cppu::UnoType<sal_Int32>::get(),   PROPERTY_NONE ,     MID_LINE_COLOR},
         { OUString(UNO_NAME_FOOTNOTE_LINE_STYLE), FN_PARAM_FTN_INFO,        cppu::UnoType<sal_Int8>::get(),    PROPERTY_NONE , MID_FTN_LINE_STYLE},
         { OUString(UNO_NAME_FOOTNOTE_LINE_RELATIVE_WIDTH), FN_PARAM_FTN_INFO,       cppu::UnoType<sal_Int8>::get(),        PROPERTY_NONE ,     MID_LINE_RELWIDTH    },
-        { OUString(UNO_NAME_FOOTNOTE_LINE_ADJUST), FN_PARAM_FTN_INFO,       cppu::UnoType<sal_Int16>::get(),   PROPERTY_NONE ,     MID_LINE_ADJUST     },
+        { OUString(UNO_NAME_FOOTNOTE_LINE_ADJUST), FN_PARAM_FTN_INFO,       cppu::UnoType<css::text::HorizontalAdjust>::get(),   PROPERTY_NONE ,     MID_LINE_ADJUST     },
         { OUString(UNO_NAME_FOOTNOTE_LINE_TEXT_DISTANCE), FN_PARAM_FTN_INFO,        cppu::UnoType<sal_Int32>::get(),   PROPERTY_NONE ,     MID_LINE_TEXT_DIST   |CONVERT_TWIPS },
         { OUString(UNO_NAME_FOOTNOTE_LINE_DISTANCE), FN_PARAM_FTN_INFO,         cppu::UnoType<sal_Int32>::get(),   PROPERTY_NONE ,     MID_LINE_FOOTNOTE_DIST|CONVERT_TWIPS},
         { OUString(UNO_NAME_WRITING_MODE), RES_FRAMEDIR, cppu::UnoType<sal_Int16>::get(), PROPERTY_NONE, 0 },

sw/source/uibase/utlui/uiitems.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <editeng/itemtype.hxx>
+#include <o3tl/any.hxx>
 #include <unosett.hxx>
 
 #include <swtypes.hxx>
@@ -86,7 +87,7 @@ bool SwPageFootnoteInfoItem::QueryValue( Any& rVal, sal_uInt8 nMemberId ) const
             rVal <<= static_cast<sal_Int8>(static_cast<long>(aTmp));
         }
         break;
-        case MID_LINE_ADJUST       :     rVal <<= static_cast<sal_Int16>(aFootnoteInfo.GetAdj());break;//text::HorizontalAdjust
+        case MID_LINE_ADJUST       :     rVal <<= aFootnoteInfo.GetAdj();break;//text::HorizontalAdjust
         case MID_LINE_TEXT_DIST    :     rVal <<= static_cast<sal_Int32>(convertTwipToMm100(aFootnoteInfo.GetTopDist()));break;
         case MID_LINE_FOOTNOTE_DIST:     rVal <<= static_cast<sal_Int32>(convertTwipToMm100(aFootnoteInfo.GetBottomDist()));break;
         case MID_FTN_LINE_STYLE    :
@@ -156,12 +157,8 @@ bool SwPageFootnoteInfoItem::PutValue(const Any& rVal, sal_uInt8 nMemberId)
         break;
         case MID_LINE_ADJUST       :
         {
-            sal_Int16 nSet = 0;
-            rVal >>= nSet;
-            if(nSet >= 0 && nSet < 3) //text::HorizontalAdjust
-                aFootnoteInfo.SetAdj(static_cast<css::text::HorizontalAdjust>(nSet));
-            else
-                bRet = false;
+            text::HorizontalAdjust nSet = *o3tl::forceAccess<text::HorizontalAdjust>(rVal);
+            aFootnoteInfo.SetAdj(nSet);
         }
         break;
         case MID_FTN_LINE_STYLE:

xmloff/source/style/XMLFootnoteSeparatorExport.cxx
@@ -31,6 +31,7 @@
 #include <xmloff/PageMasterStyleMap.hxx>
 #include <com/sun/star/text/HorizontalAdjust.hpp>
 #include <rtl/ustrbuf.hxx>
+#include <o3tl/any.hxx>
 
 
 using namespace ::com::sun::star;
@@ -75,7 +76,7 @@ void XMLFootnoteSeparatorExport::exportXML(
         switch (rMapper->GetEntryContextId(rState.mnIndex))
         {
         case CTF_PM_FTN_LINE_ADJUST:
-            rState.maValue >>= eLineAdjust;
+            eLineAdjust = *o3tl::forceAccess<text::HorizontalAdjust>(rState.maValue);
             break;
         case CTF_PM_FTN_LINE_COLOR:
             rState.maValue >>= nLineColor;