LemmingAvalanche/libreoffice-repaired

commit a54401709768c23e0ede79483e54709610c61999

use RawBitmap in BPixelRasterToBitmapEx
part of making BitmapWriteAccess an internal detail of vcl/

Change-Id: I28b9e0e0b414489c564fb2f23c4de2390ad175c1
Reviewed-on: https://gerrit.libreoffice.org/49964
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/primitive2d/sceneprimitive2d.cxx
@@ -33,7 +33,7 @@
 #include <drawinglayer/processor3d/geometry2dextractor.hxx>
 #include <drawinglayer/primitive2d/polygonprimitive2d.hxx>
 #include <basegfx/raster/bzpixelraster.hxx>
-#include <vcl/bitmapaccess.hxx>
+#include <vcl/BitmapTools.hxx>
 #include <comphelper/threadpool.hxx>
 
 using namespace com::sun::star;
@@ -49,83 +49,68 @@ namespace
         if(nWidth && nHeight)
         {
             const Size aDestSize(nWidth, nHeight);
-            sal_uInt8 nInitAlpha(255);
-            Bitmap aContent(aDestSize, 24);
-            AlphaMask aAlpha(aDestSize, &nInitAlpha);
-            Bitmap::ScopedWriteAccess pContent(aContent);
-            AlphaMask::ScopedWriteAccess pAlpha(aAlpha);
+            vcl::bitmap::RawBitmap aContent(aDestSize);
 
-            if (pContent && pAlpha)
+            if(mnAntiAlialize)
             {
-                if(mnAntiAlialize)
+                const sal_uInt16 nDivisor(mnAntiAlialize * mnAntiAlialize);
+
+                for(sal_uInt32 y(0); y < nHeight; y++)
                 {
-                    const sal_uInt16 nDivisor(mnAntiAlialize * mnAntiAlialize);
-
-                    for(sal_uInt32 y(0); y < nHeight; y++)
+                    for(sal_uInt32 x(0); x < nWidth; x++)
                     {
-                        Scanline pScanlineContent = pContent->GetScanline( y );
-                        Scanline pScanlineAlpha = pAlpha->GetScanline( y );
-                        for(sal_uInt32 x(0); x < nWidth; x++)
+                        sal_uInt16 nRed(0);
+                        sal_uInt16 nGreen(0);
+                        sal_uInt16 nBlue(0);
+                        sal_uInt16 nOpacity(0);
+                        sal_uInt32 nIndex(rRaster.getIndexFromXY(x * mnAntiAlialize, y * mnAntiAlialize));
+
+                        for(sal_uInt32 c(0); c < mnAntiAlialize; c++)
                         {
-                            sal_uInt16 nRed(0);
-                            sal_uInt16 nGreen(0);
-                            sal_uInt16 nBlue(0);
-                            sal_uInt16 nOpacity(0);
-                            sal_uInt32 nIndex(rRaster.getIndexFromXY(x * mnAntiAlialize, y * mnAntiAlialize));
-
-                            for(sal_uInt32 c(0); c < mnAntiAlialize; c++)
+                            for(sal_uInt32 d(0); d < mnAntiAlialize; d++)
                             {
-                                for(sal_uInt32 d(0); d < mnAntiAlialize; d++)
-                                {
-                                    const basegfx::BPixel& rPixel(rRaster.getBPixel(nIndex++));
-                                    nRed = nRed + rPixel.getRed();
-                                    nGreen = nGreen + rPixel.getGreen();
-                                    nBlue = nBlue + rPixel.getBlue();
-                                    nOpacity = nOpacity + rPixel.getOpacity();
-                                }
-
-                                nIndex += rRaster.getWidth() - mnAntiAlialize;
+                                const basegfx::BPixel& rPixel(rRaster.getBPixel(nIndex++));
+                                nRed = nRed + rPixel.getRed();
+                                nGreen = nGreen + rPixel.getGreen();
+                                nBlue = nBlue + rPixel.getBlue();
+                                nOpacity = nOpacity + rPixel.getOpacity();
                             }
 
-                            nOpacity = nOpacity / nDivisor;
+                            nIndex += rRaster.getWidth() - mnAntiAlialize;
+                        }
 
-                            if(nOpacity)
-                            {
-                                pContent->SetPixelOnData(pScanlineContent, x, BitmapColor(
-                                    static_cast<sal_uInt8>(nRed / nDivisor),
-                                    static_cast<sal_uInt8>(nGreen / nDivisor),
-                                    static_cast<sal_uInt8>(nBlue / nDivisor)));
-                                pAlpha->SetPixelOnData(pScanlineAlpha, x, BitmapColor(255 - static_cast<sal_uInt8>(nOpacity)));
-                            }
+                        nOpacity = nOpacity / nDivisor;
+
+                        if(nOpacity)
+                        {
+                            aContent.SetPixel(y, x, Color(
+                                static_cast<sal_uInt8>(nRed / nDivisor),
+                                static_cast<sal_uInt8>(nGreen / nDivisor),
+                                static_cast<sal_uInt8>(nBlue / nDivisor),
+                                255 - static_cast<sal_uInt8>(nOpacity)));
                         }
                     }
                 }
-                else
+            }
+            else
+            {
+                sal_uInt32 nIndex(0);
+
+                for(sal_uInt32 y(0); y < nHeight; y++)
                 {
-                    sal_uInt32 nIndex(0);
-
-                    for(sal_uInt32 y(0); y < nHeight; y++)
+                    for(sal_uInt32 x(0); x < nWidth; x++)
                     {
-                        Scanline pScanlineContent = pContent->GetScanline( y );
-                        Scanline pScanlineAlpha = pAlpha->GetScanline( y );
-                        for(sal_uInt32 x(0); x < nWidth; x++)
-                        {
-                            const basegfx::BPixel& rPixel(rRaster.getBPixel(nIndex++));
+                        const basegfx::BPixel& rPixel(rRaster.getBPixel(nIndex++));
 
-                            if(rPixel.getOpacity())
-                            {
-                                pContent->SetPixelOnData(pScanlineContent, x, BitmapColor(rPixel.getRed(), rPixel.getGreen(), rPixel.getBlue()));
-                                pAlpha->SetPixelOnData(pScanlineAlpha, x, BitmapColor(255 - rPixel.getOpacity()));
-                            }
+                        if(rPixel.getOpacity())
+                        {
+                            aContent.SetPixel(y, x, Color(rPixel.getRed(), rPixel.getGreen(), rPixel.getBlue(), 255 - rPixel.getOpacity()));
                         }
                     }
                 }
             }
 
-            pAlpha.reset();
-            pContent.reset();
-
-            aRetval = BitmapEx(aContent, aAlpha);
+            aRetval = vcl::bitmap::CreateFromData(std::move(aContent));
 
             // #i101811# set PrefMapMode and PrefSize at newly created Bitmap
             aRetval.SetPrefMapMode(MapMode(MapUnit::MapPixel));

commit 115d2c2cffa2773feb1b77fa85ebb50770bc2573

tdf#116001 Incorrect Colors in Chart Wall Area
regression from

    commit a54401709768c23e0ede79483e54709610c61999
    "use RawBitmap in BPixelRasterToBitmapEx"

we need to implement alpha support in vcl::bitmap::CreateFromData

Change-Id: I68bebbe1dd15e3bb04de312309c1631d6bb7fe2d
Reviewed-on: https://gerrit.libreoffice.org/50556
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/primitive2d/sceneprimitive2d.cxx
@@ -49,7 +49,7 @@ namespace
         if(nWidth && nHeight)
         {
             const Size aDestSize(nWidth, nHeight);
-            vcl::bitmap::RawBitmap aContent(aDestSize);
+            vcl::bitmap::RawBitmap aContent(aDestSize, 32);
 
             if(mnAntiAlialize)
             {

emfio/source/reader/wmfreader.cxx
@@ -715,7 +715,7 @@ namespace emfio
                 }
                 if (bOk)
                 {
-                    vcl::bitmap::RawBitmap aBmp( Size( nWidth, nHeight ) );
+                    vcl::bitmap::RawBitmap aBmp( Size( nWidth, nHeight ), 24 );
                     for (sal_uInt16 y = 0; y < nHeight && mpInputStream->good(); ++y)
                     {
                         sal_uInt16 x = 0;

filter/source/graphicfilter/icgm/bitmap.cxx
@@ -69,7 +69,7 @@ void CGMBitmap::ImplGetBitmap( CGMBitmapDescriptor& rDesc )
     if (!(ImplGetDimensions(rDesc) && rDesc.mpBuf && isLegalBitsPerPixel(rDesc.mnDstBitsPerPixel)))
         return;
 
-    vcl::bitmap::RawBitmap aBitmap( Size( rDesc.mnX, rDesc.mnY ) );
+    vcl::bitmap::RawBitmap aBitmap( Size( rDesc.mnX, rDesc.mnY ), 24 );
 
     // the picture may either be read from left to right or right to left, from top to bottom ...
 

filter/source/graphicfilter/ieps/ieps.cxx
@@ -662,7 +662,7 @@ ipsGraphicImport( SvStream & rStream, Graphic & rGraphic, FilterConfigItem* )
                     {
                         rStream.Seek( nBufStartPos + ( pDest - pBuf.get() ) );
 
-                        vcl::bitmap::RawBitmap aBitmap( Size( nWidth, nHeight ) );
+                        vcl::bitmap::RawBitmap aBitmap( Size( nWidth, nHeight ), 24 );
                         {
                             bool bIsValid = true;
                             sal_uInt8 nDat = 0;

filter/source/graphicfilter/ipbm/ipbm.cxx
@@ -92,7 +92,7 @@ bool PBMReader::ReadPBM(Graphic & rGraphic )
             if (nRemainingSize < nPixelsRequired / 8)
                 return false;
 
-            mpRawBmp.reset( new vcl::bitmap::RawBitmap( Size( mnWidth, mnHeight ) ) );
+            mpRawBmp.reset( new vcl::bitmap::RawBitmap( Size( mnWidth, mnHeight ), 24 ) );
             mvPalette.resize( 2 );
             mvPalette[0] = Color( 0xff, 0xff, 0xff );
             mvPalette[1] = Color( 0x00, 0x00, 0x00 );
@@ -102,7 +102,7 @@ bool PBMReader::ReadPBM(Graphic & rGraphic )
             if (nRemainingSize < nPixelsRequired)
                 return false;
 
-            mpRawBmp.reset( new vcl::bitmap::RawBitmap( Size( mnWidth, mnHeight ) ) );
+            mpRawBmp.reset( new vcl::bitmap::RawBitmap( Size( mnWidth, mnHeight ), 24 ) );
             mnCol = static_cast<sal_uInt16>(mnMaxVal) + 1;
             if ( mnCol > 256 )
                 mnCol = 256;
@@ -118,7 +118,7 @@ bool PBMReader::ReadPBM(Graphic & rGraphic )
             if (nRemainingSize / 3 < nPixelsRequired)
                 return false;
 
-            mpRawBmp.reset( new vcl::bitmap::RawBitmap( Size( mnWidth, mnHeight ) ) );
+            mpRawBmp.reset( new vcl::bitmap::RawBitmap( Size( mnWidth, mnHeight ), 24 ) );
             break;
     }
 

filter/source/graphicfilter/ipcd/ipcd.cxx
@@ -148,7 +148,7 @@ bool PCDReader::ReadPCD( Graphic & rGraphic, FilterConfigItem* pConfigItem )
             nBMPWidth = nHeight;
             nBMPHeight = nWidth;
         }
-        mpBitmap.reset(new vcl::bitmap::RawBitmap( Size( nBMPWidth, nBMPHeight ) ));
+        mpBitmap.reset(new vcl::bitmap::RawBitmap( Size( nBMPWidth, nBMPHeight ), 24 ));
 
         ReadImage();
 

filter/source/graphicfilter/ipcx/ipcx.cxx
@@ -105,7 +105,7 @@ bool PCXReader::ReadPCX(Graphic & rGraphic)
     // Write BMP header and conditionally (maybe invalid for now) color palette:
     if (bStatus)
     {
-        mpBitmap.reset( new vcl::bitmap::RawBitmap( Size( nWidth, nHeight ) ) );
+        mpBitmap.reset( new vcl::bitmap::RawBitmap( Size( nWidth, nHeight ), 24 ) );
 
         if ( nDestBitsPerPixel <= 8 )
         {

filter/source/graphicfilter/ipict/ipict.cxx
@@ -850,7 +850,7 @@ sal_uLong PictReader::ReadPixMapEtc( BitmapEx &rBitmap, bool bBaseAddr, bool bCo
                 return 0xffffffff;
         }
 
-        pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight) ));
+        pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight), 24 ));
 
         for (sal_uInt16 ny = 0; ny < nHeight; ++ny)
         {
@@ -932,7 +932,7 @@ sal_uLong PictReader::ReadPixMapEtc( BitmapEx &rBitmap, bool bBaseAddr, bool bCo
                 return 0xffffffff;
         }
 
-        pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight) ));
+        pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight), 24 ));
 
         for (sal_uInt16 ny = 0; ny < nHeight; ++ny)
         {
@@ -1027,7 +1027,7 @@ sal_uLong PictReader::ReadPixMapEtc( BitmapEx &rBitmap, bool bBaseAddr, bool bCo
             if (nWidth > nMaxCols)
                 return 0xffffffff;
 
-            pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight) ));
+            pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight), 24 ));
 
             for (sal_uInt16 ny = 0; ny < nHeight; ++ny)
             {
@@ -1050,7 +1050,7 @@ sal_uLong PictReader::ReadPixMapEtc( BitmapEx &rBitmap, bool bBaseAddr, bool bCo
             if (nWidth > nMaxCols)
                 return 0xffffffff;
 
-            pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight) ));
+            pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight), 24 ));
 
             for (sal_uInt16 ny = 0; ny < nHeight; ++ny)
             {
@@ -1072,7 +1072,7 @@ sal_uLong PictReader::ReadPixMapEtc( BitmapEx &rBitmap, bool bBaseAddr, bool bCo
                 if (nHeight > pPict->remainingSize() / nByteCountSize)
                     return 0xffffffff;
 
-                pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight) ));
+                pBitmap.reset(new vcl::bitmap::RawBitmap( Size(nWidth, nHeight), 24 ));
 
                 size_t nByteWidth = static_cast<size_t>(nWidth) * nCmpCount;
                 std::vector<sal_uInt8> aScanline(nByteWidth);

filter/source/graphicfilter/ipsd/ipsd.cxx
@@ -115,7 +115,7 @@ bool PSDReader::ReadPSD(Graphic & rGraphic )
     }
 
     Size aBitmapSize( mpFileHeader->nColumns, mpFileHeader->nRows );
-    mpBitmap.reset( new vcl::bitmap::RawBitmap( aBitmapSize ) );
+    mpBitmap.reset( new vcl::bitmap::RawBitmap( aBitmapSize, 24 ) );
     if ( mpPalette && mbStatus )
     {
         mvPalette.resize( 256 );

filter/source/graphicfilter/iras/iras.cxx
@@ -173,7 +173,7 @@ bool RASReader::ReadRAS(Graphic & rGraphic)
     if (m_rRAS.remainingSize() * nMaxCompression < nBitSize / 8)
         return false;
 
-    vcl::bitmap::RawBitmap aBmp(Size(mnWidth, mnHeight));
+    vcl::bitmap::RawBitmap aBmp(Size(mnWidth, mnHeight), 24);
 
     // read in the bitmap data
     mbStatus = ImplReadBody(aBmp, aPalette);

filter/source/graphicfilter/itga/itga.cxx
@@ -146,7 +146,7 @@ bool TGAReader::ReadTGA(Graphic & rGraphic)
             if (nSize > SAL_MAX_INT32/2/3)
                 return false;
 
-            mpBitmap.reset( new vcl::bitmap::RawBitmap( Size( mpFileHeader->nImageWidth, mpFileHeader->nImageHeight ) ) );
+            mpBitmap.reset( new vcl::bitmap::RawBitmap( Size( mpFileHeader->nImageWidth, mpFileHeader->nImageHeight ), 24 ) );
             if ( mbIndexing )
                 mbStatus = ImplReadPalette();
             if ( mbStatus )

filter/source/msfilter/msdffimp.cxx
@@ -1109,7 +1109,7 @@ void ApplyRectangularGradientAsBitmap( const SvxMSDffManager& rManager, SvStream
         double fFocusX = rManager.GetPropertyValue( DFF_Prop_fillToRight, 0 ) / 65536.0;
         double fFocusY = rManager.GetPropertyValue( DFF_Prop_fillToBottom, 0 ) / 65536.0;
 
-        vcl::bitmap::RawBitmap aBitmap(aBitmapSizePixel);
+        vcl::bitmap::RawBitmap aBitmap(aBitmapSizePixel, 24);
 
         for ( long nY = 0; nY < aBitmapSizePixel.Height(); nY++ )
         {
@@ -1364,7 +1364,7 @@ void DffPropertyReader::ApplyFillAttributes( SvStream& rIn, SfxItemSet& rSet, co
                                 aCol2 = rManager.MSO_CLR_ToColor( GetPropertyValue( DFF_Prop_fillBackColor, 0 ), DFF_Prop_fillBackColor );
 
                             // Create a bitmap for the pattern with expected colors
-                            vcl::bitmap::RawBitmap aResult(Size(8, 8));
+                            vcl::bitmap::RawBitmap aResult(Size(8, 8), 24);
                             {
                                 Bitmap::ScopedReadAccess pRead(aBmp);
 

include/vcl/BitmapTools.hxx
@@ -30,28 +30,37 @@ class VCL_DLLPUBLIC RawBitmap
 friend BitmapEx VCL_DLLPUBLIC CreateFromData( RawBitmap&& rawBitmap );
     std::unique_ptr<sal_uInt8[]> mpData;
     Size maSize;
+    sal_uInt8 mnBitCount;
 public:
-    RawBitmap(Size const & rSize)
-        : mpData(new sal_uInt8[ rSize.getWidth() * 3 * rSize.getHeight()]),
-          maSize(rSize)
+    RawBitmap(Size const & rSize, sal_uInt8 nBitCount)
+        : mpData(new sal_uInt8[ rSize.getWidth() * nBitCount/8 * rSize.getHeight()]),
+          maSize(rSize),
+          mnBitCount(nBitCount)
     {
+        assert(nBitCount == 24 || nBitCount == 32);
     }
     void SetPixel(long nY, long nX, Color nColor)
     {
-        long p = (nY * maSize.getWidth() + nX) * 3;
+        long p = (nY * maSize.getWidth() + nX) * (mnBitCount/8);
         mpData[ p++ ] = nColor.GetRed();
         mpData[ p++ ] = nColor.GetGreen();
-        mpData[ p   ] = nColor.GetBlue();
+        mpData[ p++ ] = nColor.GetBlue();
+        if (mnBitCount == 32)
+            mpData[ p ] = nColor.GetTransparency();
     }
     Color GetPixel(long nY, long nX) const
     {
-        long p = (nY * maSize.getWidth() + nX) * 3;
-        return Color( mpData[p], mpData[p+1], mpData[p+2]);
+        long p = (nY * maSize.getWidth() + nX) * mnBitCount/8;
+        if (mnBitCount == 24)
+            return Color( mpData[p], mpData[p+1], mpData[p+2]);
+        else
+            return Color( mpData[p+3], mpData[p], mpData[p+1], mpData[p+2]);
     }
     // so we don't accidentally leave any code in that uses palette color indexes
     void SetPixel(long nY, long nX, BitmapColor nColor) = delete;
     long Height() { return maSize.Height(); }
     long Width() { return maSize.Width(); }
+    sal_uInt8 GetBitCount() { return mnBitCount; }
 };
 
 BitmapEx VCL_DLLPUBLIC loadFromName(const OUString& rFileName, const ImageLoadFlags eFlags = ImageLoadFlags::NONE);

svx/source/xoutdev/xattrbmp.cxx
@@ -145,7 +145,7 @@ XFillBitmapItem::XFillBitmapItem(const XFillBitmapItem& rItem)
 
 BitmapEx createHistorical8x8FromArray(std::array<sal_uInt8,64> const & pArray, Color aColorPix, Color aColorBack)
 {
-    vcl::bitmap::RawBitmap aBitmap(Size(8, 8));
+    vcl::bitmap::RawBitmap aBitmap(Size(8, 8), 24);
 
     for(sal_uInt16 a(0); a < 8; a++)
     {

vcl/source/bitmap/BitmapTools.cxx
@@ -148,7 +148,7 @@ BitmapEx CreateFromData( sal_uInt8 const *pData, sal_Int32 nWidth, sal_Int32 nHe
     {
         for( long y = 0; y < nHeight; ++y )
         {
-            sal_uInt8 const *p = pData + y * nStride;
+            sal_uInt8 const *p = pData + (y * nStride);
             Scanline pScanline = pWrite->GetScanline(y);
             for (long x = 0; x < nWidth; ++x)
             {
@@ -158,7 +158,7 @@ BitmapEx CreateFromData( sal_uInt8 const *pData, sal_Int32 nWidth, sal_Int32 nHe
             }
             if (nBitCount == 32)
             {
-                p = pData + y * nStride + 3;
+                p = pData + (y * nStride) + 3;
                 Scanline pMaskScanLine = xMaskAcc->GetScanline(y);
                 for (long x = 0; x < nWidth; ++x)
                 {
@@ -179,27 +179,50 @@ BitmapEx CreateFromData( sal_uInt8 const *pData, sal_Int32 nWidth, sal_Int32 nHe
 */
 BitmapEx CreateFromData( RawBitmap&& rawBitmap )
 {
-    Bitmap aBmp( rawBitmap.maSize, /*nBitCount*/24 );
+    auto nBitCount = rawBitmap.GetBitCount();
+    assert( nBitCount == 24 || nBitCount == 32);
+    Bitmap aBmp( rawBitmap.maSize, nBitCount );
 
     Bitmap::ScopedWriteAccess pWrite(aBmp);
     assert(pWrite.get());
     if( !pWrite )
         return BitmapEx();
+    std::unique_ptr<AlphaMask> pAlphaMask;
+    AlphaMask::ScopedWriteAccess xMaskAcc;
+    if (nBitCount == 32)
+    {
+        pAlphaMask.reset( new AlphaMask( rawBitmap.maSize ) );
+        xMaskAcc = AlphaMask::ScopedWriteAccess(*pAlphaMask);
+    }
 
     auto nHeight = rawBitmap.maSize.getHeight();
     auto nWidth = rawBitmap.maSize.getWidth();
+    auto nStride = nWidth * nBitCount / 8;
     for( long y = 0; y < nHeight; ++y )
     {
-        sal_uInt8 const *p = rawBitmap.mpData.get() + (y * nWidth * 3);
+        sal_uInt8 const *p = rawBitmap.mpData.get() + (y * nStride);
         Scanline pScanline = pWrite->GetScanline(y);
         for (long x = 0; x < nWidth; ++x)
         {
             BitmapColor col(p[0], p[1], p[2]);
             pWrite->SetPixelOnData(pScanline, x, col);
-            p += 3;
+            p += nBitCount/8;
+        }
+        if (nBitCount == 32)
+        {
+            p = rawBitmap.mpData.get() + (y * nStride) + 3;
+            Scanline pMaskScanLine = xMaskAcc->GetScanline(y);
+            for (long x = 0; x < nWidth; ++x)
+            {
+                xMaskAcc->SetPixelOnData(pMaskScanLine, x, BitmapColor(*p));
+                p += 4;
+            }
         }
     }
-    return aBmp;
+    if (nBitCount == 32)
+        return BitmapEx(aBmp, *pAlphaMask);
+    else
+        return aBmp;
 }
 
 #if ENABLE_CAIRO_CANVAS