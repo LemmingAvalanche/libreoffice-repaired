LemmingAvalanche/libreoffice-repaired

commit 4fb6e27e93a6883a82908aa9496b1b040fc47cd4

More on fixing build by eliminating use of ScMatrixValue.

sc/source/core/tool/interpr1.cxx
@@ -126,23 +126,20 @@ void ScInterpreter::ScIfJump()
                         {
                             double fVal;
                             bool bTrue;
-                            ScMatValType nType = 0;
-                            const ScMatrixValue* pMatVal = pMat->Get( nC, nR,
-                                    nType);
-                            bool bIsValue = ScMatrix::IsValueType( nType);
-                            if ( bIsValue )
+                            bool bIsValue = pMat->IsValue(nC, nR);
+                            if (bIsValue)
                             {
-                                fVal = pMatVal->fVal;
-                                bIsValue = ::rtl::math::isFinite( fVal );
+                                fVal = pMat->GetDouble(nC, nR);
+                                bIsValue = ::rtl::math::isFinite(fVal);
                                 bTrue = bIsValue && (fVal != 0.0);
-                                if ( bTrue )
+                                if (bTrue)
                                     fVal = 1.0;
                             }
                             else
                             {
                                 // Treat empty and empty path as 0, but string
                                 // as error.
-                                bIsValue = !ScMatrix::IsRealStringType( nType);
+                                bIsValue = (!pMat->IsString(nC, nR) || pMat->IsEmpty(nC, nR));
                                 bTrue = false;
                                 fVal = (bIsValue ? 0.0 : CreateDoubleError( errNoValue));
                             }
@@ -258,13 +255,10 @@ void ScInterpreter::ScChoseJump()
                         for ( SCSIZE nR=0; nR < nRows; ++nR )
                         {
                             double fVal;
-                            ScMatValType nType;
-                            const ScMatrixValue* pMatVal = pMat->Get( nC, nR,
-                                    nType);
-                            bool bIsValue = ScMatrix::IsValueType( nType);
+                            bool bIsValue = pMat->IsValue(nC, nR);
                             if ( bIsValue )
                             {
-                                fVal = pMatVal->fVal;
+                                fVal = pMat->GetDouble(nC, nR);
                                 bIsValue = ::rtl::math::isFinite( fVal );
                                 if ( bIsValue )
                                 {

sc/source/core/tool/compiler.cxx
@@ -5088,18 +5088,15 @@ void ScCompiler::CreateStringFromMatrix( rtl::OUStringBuffer& rBuffer,
 
             if( pMatrix->IsValue( nC, nR ) )
             {
-                ScMatValType nType;
-                const ScMatrixValue* pVal = pMatrix->Get( nC, nR, nType);
-
-                if( nType == SC_MATVAL_BOOLEAN )
-                    AppendBoolean( rBuffer, pVal->GetBoolean() );
+                if (pMatrix->IsBoolean(nC, nR))
+                    AppendBoolean(rBuffer, pMatrix->GetDouble(nC, nR) != 0.0);
                 else
                 {
-                    USHORT nErr = pVal->GetError();
-                    if( nErr )
-                        rBuffer.append( ScGlobal::GetErrorString( nErr ) );
+                    USHORT nErr = pMatrix->GetError(nC, nR);
+                    if (nErr)
+                        rBuffer.append(ScGlobal::GetErrorString(nErr));
                     else
-                        AppendDouble( rBuffer, pVal->fVal );
+                        AppendDouble(rBuffer, pMatrix->GetDouble(nC, nR));
                 }
             }
             else if( pMatrix->IsEmpty( nC, nR ) )

commit 1ac97620fcd06148d4e1d3989787aa3615093341

Resolves: tdf#116324 treat jump empty path as 0 when nested
Apparently a fallout from

    commit 4fb6e27e93a6883a82908aa9496b1b040fc47cd4
    AuthorDate: Mon Jun 28 11:40:39 2010 -0400
    CommitDate: Sat Nov 6 23:40:00 2010 -0400

        More on fixing build by eliminating use of ScMatrixValue.

or other commits related to ScMatrix handling.

Change-Id: I10e8b58aed51cd707b1503dbbc0b369aea4a2805

sc/source/core/tool/interpr1.cxx
@@ -130,8 +130,10 @@ void ScInterpreter::ScIfJump()
                             else
                             {
                                 // Treat empty and empty path as 0, but string
-                                // as error.
-                                bIsValue = (!pMat->IsString(nC, nR) || pMat->IsEmpty(nC, nR));
+                                // as error. ScMatrix::IsValueOrEmpty() returns
+                                // true for any empty, empty path, empty cell,
+                                // empty result.
+                                bIsValue = pMat->IsValueOrEmpty(nC, nR);
                                 bTrue = false;
                                 fVal = (bIsValue ? 0.0 : CreateDoubleError( FormulaError::NoValue));
                             }