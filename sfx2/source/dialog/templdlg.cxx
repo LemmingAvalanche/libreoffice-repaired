LemmingAvalanche/libreoffice-repaired

commit b28c08f71e7c7306b1c7f25a4178278a54f5ec1d

tdf#91106 sfx2: Stylist: sort top-level styles in hierarchical mode
(regression from 6b807067133198221ce90792efa6a25e4956fd77)

Change-Id: I111d12a622390afc02eb73a42b1d3c714f046bd2

sfx2/source/dialog/templdlg.cxx
@@ -558,6 +558,12 @@ StyleTreeArr_Impl& MakeTree_Impl(StyleTreeArr_Impl& rArr)
     // Only keep tree roots in rArr, child elements can be accessed through the hierarchy
     rArr.erase(std::remove_if(rArr.begin(), rArr.end(), [](StyleTree_Impl* pEntry) { return pEntry->HasParent(); }), rArr.end());
 
+    // tdf#91106 sort top level styles
+    std::sort(rArr.begin(), rArr.end(),
+        [&aSorter](StyleTree_Impl* pEntry1, StyleTree_Impl* pEntry2) {
+            return aSorter.compare(pEntry1->getName(), pEntry2->getName()) < 0;
+        });
+
     return rArr;
 }
 

commit b4d07278286deaed838900485f48a48472aff4f5

tdf#91106 sfx2: Stylist hierarchical: sort the Default Style first
(regression from b28c08f71e7c7306b1c7f25a4178278a54f5ec1d)

Change-Id: I47377447dd1e04a44033dea27ce7307a65821737

sfx2/source/dialog/templdlg.cxx
@@ -555,6 +555,10 @@ void MakeTree_Impl(StyleTreeArr_Impl& rArr)
     // tdf#91106 sort top level styles
     std::sort(rArr.begin(), rArr.end(),
         [&aSorter](std::unique_ptr<StyleTree_Impl> const & pEntry1, std::unique_ptr<StyleTree_Impl> const & pEntry2) {
+            if (pEntry2->getName() == "Default Style")
+                return false;
+            if (pEntry1->getName() == "Default Style")
+                return true; // default always first
             return aSorter.compare(pEntry1->getName(), pEntry2->getName()) < 0;
         });
 }