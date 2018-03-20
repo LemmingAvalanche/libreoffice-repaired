LemmingAvalanche/libreoffice-repaired

commit e30bee2c23da2f0a5b866881a1b4c4adba39ee40

shell: use Unicode on Windows
Change-Id: I9f7962db320f8832879ba327108425a7592b4b77
Reviewed-on: https://gerrit.libreoffice.org/42885
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

shell/source/backends/wininetbe/wininetbackend.cxx
@@ -35,7 +35,7 @@
 #pragma warning(pop)
 #endif
 
-#define WININET_DLL_NAME "wininet.dll"
+#define WININET_DLL_NAME L"wininet.dll"
 #define EQUAL_SIGN '='
 #define COLON      ':'
 #define SPACE      ' '
@@ -102,14 +102,14 @@ namespace
 
 WinInetBackend::WinInetBackend()
 {
-    Library hWinInetDll( LoadLibrary( WININET_DLL_NAME ) );
+    Library hWinInetDll( LoadLibraryW( WININET_DLL_NAME ) );
     if( hWinInetDll.module )
     {
         typedef BOOL ( WINAPI *InternetQueryOption_Proc_T )( HINTERNET, DWORD, LPVOID, LPDWORD );
 
         InternetQueryOption_Proc_T lpfnInternetQueryOption =
             reinterpret_cast< InternetQueryOption_Proc_T >(
-                GetProcAddress( hWinInetDll.module, "InternetQueryOptionA" ) );
+                GetProcAddress( hWinInetDll.module, "InternetQueryOptionW" ) );
         if (lpfnInternetQueryOption)
         {
             // Some Windows versions would fail the InternetQueryOption call
@@ -120,7 +120,7 @@ WinInetBackend::WinInetBackend()
             // reallocation:
             INTERNET_PROXY_INFO pi;
             LPINTERNET_PROXY_INFO lpi = &pi;
-            DWORD dwLength = sizeof (INTERNET_PROXY_INFO);
+            DWORD dwLength = sizeof (pi);
             BOOL ok = lpfnInternetQueryOption(
                 nullptr,
                 INTERNET_OPTION_PROXY,
@@ -165,8 +165,10 @@ WinInetBackend::WinInetBackend()
             // an empty proxy list, so we don't have to check if
             // proxy is enabled or not
 
-            OUString aProxyList       = OUString::createFromAscii( lpi->lpszProxy );
-            OUString aProxyBypassList = OUString::createFromAscii( lpi->lpszProxyBypass );
+            // We use a W-version of InternetQueryOption; it returns struct with pointers to wide strings
+            // There's no INTERNET_PROXY_INFOW, so we simply cast returned struct's members
+            OUString aProxyList       = reinterpret_cast<const sal_Unicode*>( lpi->lpszProxy );
+            OUString aProxyBypassList = reinterpret_cast<const sal_Unicode*>( lpi->lpszProxyBypass );
 
             // override default for ProxyType, which is "0" meaning "No proxies".
             valueProxyType_.IsPresent = true;

shell/inc/basereader.hxx
@@ -28,6 +28,7 @@
 
 #include "xml_parser.hxx"
 #include "zipfile.hxx"
+#include <filepath.hxx>
 
 class CBaseReader : public i_xml_parser_event_handler
 {
@@ -35,7 +36,7 @@ public:
     virtual ~CBaseReader() override;
 
 protected: // protected because its only an implementation relevant class
-    CBaseReader( const std::string& DocumentName );
+    CBaseReader( const Filepath_t& DocumentName );
 
     CBaseReader( StreamInterface *stream );
 

shell/inc/contentreader.hxx
@@ -22,6 +22,7 @@
 
 #include "basereader.hxx"
 #include <stack>
+#include <filepath.hxx>
 
 class ITag;
 class StreamInterface;
@@ -31,7 +32,7 @@ class CContentReader : public CBaseReader
 public:
     virtual ~CContentReader() override;
 
-    CContentReader( const std::string& DocumentName, LocaleSet_t const & DocumentLocale );
+    CContentReader( const Filepath_t& DocumentName, LocaleSet_t const & DocumentLocale );
 
     CContentReader( StreamInterface* stream, LocaleSet_t const & DocumentLocale );
 

shell/inc/fileextensions.hxx
@@ -23,6 +23,7 @@
 #include "global.hxx"
 
 #include <string>
+#include <filepath.hxx>
 
 // A simple table with information about the currently used OO file extensions
 // for instance ".sxw" and information about windows registry keys which are
@@ -34,20 +35,19 @@
 */
 struct FileExtensionEntry
 {
-    const char*    ExtensionAnsi;         // e.g. ".sxw"
-    const wchar_t* ExtensionUnicode;      // e.g. L".sxw"
-    const char*    RegistryForwardKey;    // e.g. "soffice.StarWriterDocument.6"
+    const char*    ExtensionA;            // e.g. ".sxw"
+    const wchar_t* ExtensionU;            // e.g. L".sxw"
+    const wchar_t* RegistryForwardKey;    // e.g. "soffice.StarWriterDocument.6"
 };
 
 extern const FileExtensionEntry OOFileExtensionTable[];
 
 extern const size_t OOFileExtensionTableSize;
 
-
 /** Return the extension of a file
     name without the '.'
 */
-std::string get_file_name_extension(const std::string& file_name);
+Filepath_t get_file_name_extension(const Filepath_t& file_name);
 
 
 /** Return the type of a file
@@ -55,7 +55,7 @@ std::string get_file_name_extension(const std::string& file_name);
 
 enum File_Type_t { UNKNOWN, WRITER, CALC, DRAW, IMPRESS, MATH, WEB, DATABASE };
 
-File_Type_t get_file_type(const std::string& file_name);
+File_Type_t get_file_type(const Filepath_t& file_name);
 
 #endif
 

shell/inc/filepath.hxx
@@ -1,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SHELL_INC_INTERNAL_FILEPATH_HXX
+#define INCLUDED_SHELL_INC_INTERNAL_FILEPATH_HXX
+
+
+// typedefs to allow using Unicode paths on Windows
+
+
+#include <string>
+
+#if defined _WIN32
+typedef wchar_t Filepath_char_t;
+typedef std::wstring Filepath_t;
+#else
+typedef char Filepath_char_t;
+typedef std::string Filepath_t;
+#endif
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

shell/inc/infotips.hxx
@@ -30,6 +30,7 @@
 #pragma warning(pop)
 #endif
 #include <string>
+#include <filepath.hxx>
 
 class CInfoTip : public IQueryInfo, public IPersistFile
 {
@@ -84,9 +85,9 @@ public:
             /* [out] */ LPOLESTR __RPC_FAR *ppszFileName) override;
 
 private:
-    long         m_RefCnt;
-    char         m_szFileName[MAX_PATH];
-    std::wstring m_FileNameOnly;
+    long            m_RefCnt;
+    Filepath_char_t m_szFileName[MAX_PATH];
+    std::wstring    m_FileNameOnly;
 };
 
 #endif

shell/inc/metainforeader.hxx
@@ -23,6 +23,7 @@
 #include "basereader.hxx"
 #include "types.hxx"
 #include <stack>
+#include <filepath.hxx>
 
 class ITag;
 class CKeywordsTag;
@@ -34,7 +35,7 @@ class CMetaInfoReader : public CBaseReader
 public:
     virtual ~CMetaInfoReader() override;
 
-    CMetaInfoReader( const std::string& DocumentName );
+    CMetaInfoReader( const Filepath_t& DocumentName );
 
     CMetaInfoReader( StreamInterface* stream );
 

shell/inc/propsheets.hxx
@@ -31,6 +31,7 @@
 #endif
 #include <string>
 #include <memory>
+#include <filepath.hxx>
 
 class CPropertySheet : public IShellExtInit, public IShellPropSheetExt
 {
@@ -80,7 +81,7 @@ private:
 
 private:
     long m_RefCnt;
-    char m_szFileName[MAX_PATH];
+    Filepath_char_t m_szFileName[MAX_PATH];
 };
 
 #endif

commit 8cb40f414bb2d23a3c628d167dbd15fd344947c3

tdf#116516: INTERNET_PROXY_INFO always contains char* data
Regression from e30bee2c23da2f0a5b866881a1b4c4adba39ee40

The InternetQueryOption has two variants: ANSI (A) and Unicode (W)
(see https://msdn.microsoft.com/en-us/library/aa385101).
INTERNET_PROXY_INFO struct we are using is defined to contain two
LPCTSTR members (see https://msdn.microsoft.com/en-us/library/aa385148).
When UNICODE is defined, InternetQueryOption expands to W variant,
and at the same time, all MS-specific generic string types (like
TCHAR or LPCTSTR) are expanded to wchar_t-based types.

So, the expectation is that InternetQueryOptionW fills the struct
with pointers to widechar strings.

But actually this is not true: InternetQueryOptionW still returns
char-based strings in the struct; so just revert partially commit
above.

Change-Id: I0facb1baf2a191f7bafdf185e684bfe741ca677a
Reviewed-on: https://gerrit.libreoffice.org/51629
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

shell/source/backends/wininetbe/wininetbackend.cxx
@@ -159,10 +159,14 @@ WinInetBackend::WinInetBackend()
             // an empty proxy list, so we don't have to check if
             // proxy is enabled or not
 
-            // We use a W-version of InternetQueryOption; it returns struct with pointers to wide strings
-            // There's no INTERNET_PROXY_INFOW, so we simply cast returned struct's members
-            OUString aProxyList       = reinterpret_cast<const sal_Unicode*>( lpi->lpszProxy );
-            OUString aProxyBypassList = reinterpret_cast<const sal_Unicode*>( lpi->lpszProxyBypass );
+            // We use InternetQueryOptionW (see https://msdn.microsoft.com/en-us/library/aa385101);
+            // it fills INTERNET_PROXY_INFO struct which is definned in WinInet.h to have LPCTSTR
+            // (i.e., the UNICODE-dependent generic string type expanding to const wchar_t* when
+            // UNICODE is defined, and InternetQueryOption macro expands to InternetQueryOptionW).
+            // Thus, it's natural to expect that W version would return wide strings. But it's not
+            // true. The W version still returns const char* in INTERNET_PROXY_INFO.
+            OUString aProxyList       = OUString::createFromAscii( lpi->lpszProxy );
+            OUString aProxyBypassList = OUString::createFromAscii( lpi->lpszProxyBypass );
 
             // override default for ProxyType, which is "0" meaning "No proxies".
             valueProxyType_.IsPresent = true;