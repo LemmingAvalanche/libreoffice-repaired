LemmingAvalanche/libreoffice-repaired

commit e03efde345139561e098ef28e244965ee2e1adcb

teach redundantcast plugin about functional casts
Change-Id: Iac8ccd17d9e46ebb2cb55db7adb06c469bbd4ea0
Reviewed-on: https://gerrit.libreoffice.org/37910
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/paintfrm.cxx
@@ -5107,7 +5107,7 @@ void PaintCharacterBorder(
 
         if( aShadow.GetLocation() != SvxShadowLocation::NONE )
         {
-            lcl_PaintShadow( SwRect(aAlignedRect), aAlignedRect, aShadow,
+            lcl_PaintShadow( aAlignedRect, aAlignedRect, aShadow,
                              false, bTop, bBottom, bLeft, bRight, gProp);
         }
     }

commit acb5e3c3aac4b3e21336767ccead1a98e0503e24

tdf#116525 writer, no shadow around box
regression from

    commit e03efde345139561e098ef28e244965ee2e1adcb
    teach redundantcast plugin about functional casts

because we are now passing the same variable as a both an in-parameter
and an out-parameter

Change-Id: I6454103fbc46530180cde5f725273e062eb2e25d
Reviewed-on: https://gerrit.libreoffice.org/51757
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/paintfrm.cxx
@@ -4755,7 +4755,7 @@ void PaintCharacterBorder(
 
         if( aShadow.GetLocation() != SvxShadowLocation::NONE )
         {
-            lcl_PaintShadow( aAlignedRect, aAlignedRect, aShadow,
+            lcl_PaintShadow( rPaintArea, aAlignedRect, aShadow,
                              false, bTop, bBottom, bLeft, bRight, gProp);
         }
     }