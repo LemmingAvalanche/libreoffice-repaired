LemmingAvalanche/libreoffice-repaired

commit 93e23ed1989143418d1488108bde1db926444529

tdf#116581: also read UTF16-encoded version string from TTF
Change-Id: I47d8a755450b085be39346615f06cee4f920859f
Reviewed-on: https://gerrit.libreoffice.org/51781
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

solenv/bin/modules/installer/windows/file.pm
@@ -560,10 +560,17 @@ sub get_fileversion
         close TTF;
 
         my $ttfversion = "(Version )([0-9]+[.]*([0-9][.])*[0-9]+)";
+        # UTF16-encoded version string
+        my $ttfversionU = "(V\0e\0r\0s\0i\0o\0n\0 \0)(([0-9]\0)+([.]\0([0-9]\0)+)*)";
 
         if ($ttfdata =~ /$ttfversion/ms)
         {
             my ($version, $subversion, $microversion, $vervariant) = split(/\./,$2);
+            $fileversion = int($version) . "." . int($subversion) . "." . int($microversion) . "." . int($vervariant);
+        }
+        elsif ($ttfdata =~ /$ttfversionU/ms)
+        {
+            my ($version, $subversion, $microversion, $vervariant) = split(/\./,$2 =~ s/\0//gr);
             $fileversion = int($version) . "." . int($subversion) . "." . int($microversion) . "." . int($vervariant);
         }
         else

commit 077621dfa78f721033accc9f684c4aaac646e89b

file.pm : quick build fix
Regression from commit 93e23ed1989143418d1488108bde1db926444529

Change-Id: If2d11856db8f7f341a8f3c2b3a73f7afdff6a621
Reviewed-on: https://gerrit.libreoffice.org/51792
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

solenv/bin/modules/installer/windows/file.pm
@@ -570,7 +570,7 @@ sub get_fileversion
         }
         elsif ($ttfdata =~ /$ttfversionU/ms)
         {
-            my ($version, $subversion, $microversion, $vervariant) = split(/\./,$2 =~ s/\0//gr);
+            my ($version, $subversion, $microversion, $vervariant) = split(/\./,$2 =~ s/\0//g);
             $fileversion = int($version) . "." . int($subversion) . "." . int($microversion) . "." . int($vervariant);
         }
         else