LemmingAvalanche/libreoffice-repaired

commit 25d920cd3b2257d9a97ba085276110a93bea982b

enhance pass-by-ref plugin to detect large arguments
Detect arguments larger than 64 chars passed by value.

Change-Id: I9b0ea9ccb99d115984a26eab67c9cf6afd5f6cae
Signed-off-by: Stephan Bergmann <sbergman@redhat.com>

sc/inc/column.hxx
@@ -499,7 +499,7 @@ public:
      * update script types on demand if they have not been determined.
      */
     sal_uInt8 GetRangeScriptType( sc::CellTextAttrStoreType::iterator& itPos, SCROW nRow1, SCROW nRow2,
-            sc::CellStoreType::iterator itr);
+            const sc::CellStoreType::iterator& itr);
 
     void SetScriptType( SCROW nRow, sal_uInt8 nType );
     void UpdateScriptTypes( SCROW nRow1, SCROW nRow2 );
@@ -611,7 +611,7 @@ private:
     void ActivateNewFormulaCell( const sc::CellStoreType::position_type& aPos, ScFormulaCell& rCell, bool bJoin = true );
     void AttachNewFormulaCells( const sc::CellStoreType::position_type& aPos, size_t nLength );
     void BroadcastNewCell( SCROW nRow );
-    bool UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, sc::CellStoreType::iterator& itr );
+    bool UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, const sc::CellStoreType::iterator& itr );
 
     const ScFormulaCell* FetchFormulaCell( SCROW nRow ) const;
 

sc/source/core/data/column2.cxx
@@ -1830,7 +1830,7 @@ sal_uInt8 ScColumn::GetScriptType( SCROW nRow ) const
 }
 
 sal_uInt8 ScColumn::GetRangeScriptType(
-    sc::CellTextAttrStoreType::iterator& itPos, SCROW nRow1, SCROW nRow2, sc::CellStoreType::iterator itrCells )
+    sc::CellTextAttrStoreType::iterator& itPos, SCROW nRow1, SCROW nRow2, const sc::CellStoreType::iterator& itrCells )
 {
     if (!ValidRow(nRow1) || !ValidRow(nRow2) || nRow1 > nRow2)
         return 0;
@@ -2183,7 +2183,7 @@ namespace {
 
 template<typename _Blk>
 void getBlockIterators(
-    sc::CellStoreType::iterator it, size_t& rLenRemain,
+    const sc::CellStoreType::iterator& it, size_t& rLenRemain,
     typename _Blk::iterator& rData, typename _Blk::iterator& rDataEnd )
 {
     rData = _Blk::begin(*it->data);
@@ -2203,14 +2203,14 @@ void getBlockIterators(
 
 bool appendToBlock(
     ScDocument* pDoc, sc::FormulaGroupContext& rCxt, sc::FormulaGroupContext::ColArray& rColArray,
-    size_t nPos, size_t nArrayLen, sc::CellStoreType::iterator it, const sc::CellStoreType::iterator& itEnd )
+    size_t nPos, size_t nArrayLen, const sc::CellStoreType::iterator& _it, const sc::CellStoreType::iterator& itEnd )
 {
     svl::SharedStringPool& rPool = pDoc->GetSharedStringPool();
     size_t nLenRemain = nArrayLen - nPos;
     double fNan;
     rtl::math::setNan(&fNan);
 
-    for (; it != itEnd; ++it)
+    for (sc::CellStoreType::iterator it = _it; it != itEnd; ++it)
     {
         switch (it->type)
         {
@@ -2343,7 +2343,7 @@ void copyFirstStringBlock(
 
 sc::FormulaGroupContext::ColArray*
 copyFirstFormulaBlock(
-    sc::FormulaGroupContext& rCxt, sc::CellStoreType::iterator itBlk, size_t nArrayLen,
+    sc::FormulaGroupContext& rCxt, const sc::CellStoreType::iterator& itBlk, size_t nArrayLen,
     SCTAB nTab, SCCOL nCol )
 {
     double fNan;

sc/source/core/data/column3.cxx
@@ -520,7 +520,7 @@ void ScColumn::BroadcastNewCell( SCROW nRow )
     Broadcast(nRow);
 }
 
-bool ScColumn::UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, sc::CellStoreType::iterator& itr )
+bool ScColumn::UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, const sc::CellStoreType::iterator& itr )
 {
     if (rAttr.mnScriptType != SC_SCRIPTTYPE_UNKNOWN)
         // Already updated. Nothing to do.
@@ -533,9 +533,9 @@ bool ScColumn::UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, sc::CellSt
         return false;
 
     sc::CellStoreType::position_type pos = maCells.position(itr, nRow);
-    itr = pos.first;
+    sc::CellStoreType::iterator itr2 = pos.first;
     size_t nOffset = pos.second;
-    ScRefCellValue aCell = GetCellValue( itr, nOffset );
+    ScRefCellValue aCell = GetCellValue( itr2, nOffset );
     ScAddress aPos(nCol, nRow, nTab);
 
     const SfxItemSet* pCondSet = NULL;

commit 6ad87af9feb2898adee2c11b697d05cd068ab40b

tdf#106667 Hang when trying to open a dialog and the whole sheet is selected
regression from

    commit 25d920cd3b2257d9a97ba085276110a93bea982b (patch)
    enhance pass-by-ref plugin to detect large arguments

which converted this peice of code into an O(n^2) loop

Change-Id: I0473a084518c0d10411615e1b3901b9b90144bc9
Reviewed-on: https://gerrit.libreoffice.org/51956
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins <ci@libreoffice.org>

sc/inc/column.hxx
@@ -713,7 +713,7 @@ private:
 
     void AttachNewFormulaCells( const sc::CellStoreType::position_type& aPos, size_t nLength );
     void BroadcastNewCell( SCROW nRow );
-    bool UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, const sc::CellStoreType::iterator& itr );
+    bool UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, sc::CellStoreType::iterator& itr );
 
     const ScFormulaCell* FetchFormulaCell( SCROW nRow ) const;
 

sc/source/core/data/column2.cxx
@@ -1990,7 +1990,7 @@ SvtScriptType ScColumn::GetScriptType( SCROW nRow ) const
 }
 
 SvtScriptType ScColumn::GetRangeScriptType(
-    sc::CellTextAttrStoreType::iterator& itPos, SCROW nRow1, SCROW nRow2, const sc::CellStoreType::iterator& itrCells )
+    sc::CellTextAttrStoreType::iterator& itPos, SCROW nRow1, SCROW nRow2, const sc::CellStoreType::iterator& itrCells_ )
 {
     if (!ValidRow(nRow1) || !ValidRow(nRow2) || nRow1 > nRow2)
         return SvtScriptType::NONE;
@@ -2000,6 +2000,7 @@ SvtScriptType ScColumn::GetRangeScriptType(
         maCellTextAttrs.position(itPos, nRow1);
 
     itPos = aRet.first; // Track the position of cell text attribute array.
+    sc::CellStoreType::iterator itrCells = itrCells_;
 
     SvtScriptType nScriptType = SvtScriptType::NONE;
     bool bUpdated = false;

sc/source/core/data/column3.cxx
@@ -502,7 +502,7 @@ void ScColumn::BroadcastNewCell( SCROW nRow )
     Broadcast(nRow);
 }
 
-bool ScColumn::UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, const sc::CellStoreType::iterator& itr )
+bool ScColumn::UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, sc::CellStoreType::iterator& itr )
 {
     if (rAttr.mnScriptType != SvtScriptType::UNKNOWN)
         // Already updated. Nothing to do.
@@ -515,9 +515,9 @@ bool ScColumn::UpdateScriptType( sc::CellTextAttr& rAttr, SCROW nRow, const sc::
         return false;
 
     sc::CellStoreType::position_type pos = maCells.position(itr, nRow);
-    sc::CellStoreType::iterator itr2 = pos.first;
+    itr = pos.first;
     size_t nOffset = pos.second;
-    ScRefCellValue aCell = GetCellValue( itr2, nOffset );
+    ScRefCellValue aCell = GetCellValue( itr, nOffset );
     ScAddress aPos(nCol, nRow, nTab);
 
     ScDocument* pDocument = GetDoc();