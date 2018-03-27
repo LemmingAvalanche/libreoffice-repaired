LemmingAvalanche/libreoffice-repaired

commit 2215ed8a5aea1a0142258400da561f35ade9075a

Add x64 Windows here, too

desktop/source/deployment/misc/dp_platform.cxx
@@ -38,6 +38,7 @@
 
 #define PLATFORM_ALL                "all"
 #define PLATFORM_WIN_X86            "windows_x86"
+#define PLATFORM_WIN_X86_64         "windows_x86_64"
 #define PLATFORM_LINUX_X86          "linux_x86"
 #define PLATFORM_LINUX_X86_64       "linux_x86_64"
 #define PLATFORM_KFREEBSD_X86       "kfreebsd_x86"
@@ -133,6 +134,8 @@ namespace
             ret = true;
         else if (token.equals(OUSTR(PLATFORM_WIN_X86)))
             ret = checkOSandCPU(OUSTR("Windows"), OUSTR("x86"));
+        else if (token.equals(OUSTR(PLATFORM_WIN_X86_64)))
+            ret = checkOSandCPU(OUSTR("Windows"), OUSTR("x86_64"));
         else if (token.equals(OUSTR(PLATFORM_LINUX_X86)))
             ret = checkOSandCPU(OUSTR("Linux"), OUSTR("x86"));
         else if (token.equals(OUSTR(PLATFORM_LINUX_X86_64)))

commit b5875d1a0d9d515303979c8d3c34df1c041f64d7

Fix typo ("x86_64" -> "X86_64")
...introduced with 2215ed8a5aea1a0142258400da561f35ade9075a "Add x64 Windows
here, too".  As reported by Richard Webb at
<https://lists.freedesktop.org/archives/libreoffice/2018-March/079870.html>
"Re: Manifest.xml for 64bit LO".

Change-Id: I1681908012e73e520bebd79b8149788358ea49a3
Reviewed-on: https://gerrit.libreoffice.org/51946
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

desktop/source/deployment/misc/dp_platform.cxx
@@ -77,7 +77,7 @@ namespace
         else if (token == "windows_x86")
             ret = checkOSandCPU("Windows", "x86");
         else if (token == "windows_x86_64")
-            ret = checkOSandCPU("Windows", "x86_64");
+            ret = checkOSandCPU("Windows", "X86_64");
         else if (token == "solaris_sparc")
             ret = checkOSandCPU("Solaris", "SPARC");
         else if (token == "solaris_sparc64")