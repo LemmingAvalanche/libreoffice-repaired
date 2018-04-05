LemmingAvalanche/libreoffice-repaired

commit 82879d224752fdba685d9620c5c0dc8b6931de3e

lok: enable pre-loading with Vcl's builder
Cache vclbuilder's loaded modules globally & simplify logic.

Pre-populate that cache on preload for LOK vs. a somewhat
inelegant pre-canned list of dialog libraries.

Change-Id: I86d936862a41495fd37908f3ee7eb2e0c363d651
Reviewed-on: https://gerrit.libreoffice.org/49550
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

vcl/source/window/builder.cxx
@@ -1099,6 +1099,49 @@ void VclBuilder::cleanupWidgetOwnScrolling(vcl::Window *pScrollParent, vcl::Wind
 extern "C" { static void thisModule() {} }
 #endif
 
+// We store these forever, closing modules is non-ideal from a performance
+// perspective, code pages will be freed up by the OS anyway if unused for
+// a while in many cases, and this helps us pre-init.
+typedef std::map<OUString, std::unique_ptr<osl::Module>> ModuleMap;
+static ModuleMap g_aModuleMap;
+static osl::Module g_aMergedLib;
+
+#ifndef SAL_DLLPREFIX
+#  define SAL_DLLPREFIX ""
+#endif
+
+void VclBuilder::preload()
+{
+#ifndef DISABLE_DYNLOADING
+
+#if ENABLE_MERGELIBS
+    g_aMergedLibs->loadRelative(&thisModule, SVLIBRARY("merged"));
+#endif
+// find -name '*ui*' | xargs grep 'class=".*lo-' |
+//     sed 's/.*class="//' | sed 's/-.*$//' | sort | uniq
+    static const char *aWidgetLibs[] = {
+        "sfxlo",  "svtlo",     "svxcorelo", "foruilo",
+        "vcllo",  "svxlo",     "cuilo",     "swlo",
+        "swuilo", "sclo",      "sdlo",      "chartcontrollerlo",
+        "smlo",   "scuilo",    "basctllo",  "sduilo",
+        "scnlo",  "xsltdlglo", "pcrlo" // "dbulo"
+    };
+    for (auto & lib : aWidgetLibs)
+    {
+        OUStringBuffer sModuleBuf;
+        sModuleBuf.append(SAL_DLLPREFIX);
+        sModuleBuf.append(OUString::createFromAscii(lib));
+        sModuleBuf.append(SAL_DLLEXTENSION);
+        osl::Module* pModule = new osl::Module;
+        OUString sModule = sModuleBuf.makeStringAndClear();
+        if (pModule->loadRelative(&thisModule, sModule))
+            g_aModuleMap.insert(std::make_pair(sModule, std::unique_ptr<osl::Module>(pModule)));
+        else
+            delete pModule;
+    }
+#endif // DISABLE_DYNLOADING
+}
+
 VclPtr<vcl::Window> VclBuilder::makeObject(vcl::Window *pParent, const OString &name, const OString &id,
     stringmap &rMap)
 {
@@ -1581,40 +1624,32 @@ VclPtr<vcl::Window> VclBuilder::makeObject(vcl::Window *pParent, const OString &
     }
     else
     {
-#ifndef SAL_DLLPREFIX
-#define SAL_DLLPREFIX ""
-#endif
         sal_Int32 nDelim = name.indexOf('-');
         if (nDelim != -1)
         {
+            OUString sFunction(OStringToOUString(OString("make") + name.copy(nDelim+1), RTL_TEXTENCODING_UTF8));
+
 #ifndef DISABLE_DYNLOADING
             OUStringBuffer sModuleBuf;
             sModuleBuf.append(SAL_DLLPREFIX);
             sModuleBuf.append(OStringToOUString(name.copy(0, nDelim), RTL_TEXTENCODING_UTF8));
             sModuleBuf.append(SAL_DLLEXTENSION);
-#endif
-            OUString sFunction(OStringToOUString(OString("make") + name.copy(nDelim+1), RTL_TEXTENCODING_UTF8));
-#ifndef DISABLE_DYNLOADING
+
             OUString sModule = sModuleBuf.makeStringAndClear();
-            ModuleMap::iterator aI = m_aModuleMap.find(sModule);
-            if (aI == m_aModuleMap.end())
+            ModuleMap::iterator aI = g_aModuleMap.find(sModule);
+            if (aI == g_aModuleMap.end())
             {
                 osl::Module* pModule = new osl::Module;
+                bool ok = false;
 #if ENABLE_MERGELIBS
-                sModuleBuf.append(SAL_DLLPREFIX);
-                sModuleBuf.append("mergedlo");
-                sModuleBuf.append(SAL_DLLEXTENSION);
-                OUString sMergedModule = sModuleBuf.makeStringAndClear();
-                bool ok = pModule->loadRelative(&thisModule, sMergedModule);
-                if (!pModule->getFunctionSymbol(sFunction))
-                {
-                    ok = pModule->loadRelative(&thisModule, sModule);
-                }
-#else
-                bool ok = pModule->loadRelative(&thisModule, sModule);
+                if (!g_aMergedLib.is())
+                    g_aMergedLib->loadRelative(&thisModule, SVLIBRARY("merged"));
+                ok = g_aMergedLib->getFunctionSymbol(sFunction);
 #endif
-                assert(ok && "bad module name in .ui"); (void)ok;
-                aI = m_aModuleMap.insert(std::make_pair(sModule, std::unique_ptr<osl::Module>(pModule))).first;
+                if (!ok)
+                    ok = pModule->loadRelative(&thisModule, sModule);
+                assert(ok && "bad module name in .ui");
+                aI = g_aModuleMap.insert(std::make_pair(sModule, std::unique_ptr<osl::Module>(pModule))).first;
             }
             customMakeWidget pFunction = reinterpret_cast<customMakeWidget>(aI->second->getFunctionSymbol(sFunction));
 #else

desktop/source/lib/init.cxx
@@ -98,6 +98,8 @@
 #include <svl/undo.hxx>
 #include <unotools/datetime.hxx>
 #include <i18nlangtag/languagetag.hxx>
+#include <vcl/builder.hxx>
+#include <vcl/abstdlg.hxx>
 
 #include <app.hxx>
 
@@ -3811,7 +3813,9 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
                 // 2) comphelper::setProcessServiceFactory(xSFactory);
                 // 3) InitVCL()
                 aService->initialize({css::uno::makeAny<OUString>("preload")});
-
+                // Force load some modules
+                VclBuilder::preload();
+                VclAbstractDialogFactory::Create();
                 preloadData();
 
                 // Release Solar Mutex, lo_startmain thread should acquire it.

include/vcl/builder.hxx
@@ -94,16 +94,12 @@ public:
         return m_sHelpRoot;
     }
 
+    /// Pre-loads all modules containing UI information
+    static void preload();
+
 private:
     VclBuilder(const VclBuilder&) = delete;
     VclBuilder& operator=(const VclBuilder&) = delete;
-
-    typedef std::map<OUString, std::unique_ptr<osl::Module>> ModuleMap;
-
-    //We store these until the builder is deleted, that way we can use the
-    //ui-previewer on custom widgets and guarantee the modules they are from
-    //exist for the duration of the dialog
-    ModuleMap       m_aModuleMap;
 
     //If the toplevel window has any properties which need to be set on it,
     //but the toplevel is the owner of the builder, then its ctor

commit 5c3115c2e05fe53694541c25ee0f2b8a648182d0

If the function is in mergelib, store mergelib module
Regression from commit 82879d224752fdba685d9620c5c0dc8b6931de3e
It didn't ensure that we store mergelib when required, thus still
trying to get function from stubs and failing (with mergelibs
enabled), leading to SEGFAULT later.

Change-Id: Ib15528795f3d65b1ae5c31be2aa5284ce5ff04cf
Reviewed-on: https://gerrit.libreoffice.org/52428
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Michael Meeks <michael.meeks@collabora.com>

vcl/source/window/builder.cxx
@@ -1223,18 +1223,23 @@ void VclBuilder::cleanupWidgetOwnScrolling(vcl::Window *pScrollParent, vcl::Wind
 }
 
 #ifndef DISABLE_DYNLOADING
+
 extern "C" { static void thisModule() {} }
-#endif
 
 // We store these forever, closing modules is non-ideal from a performance
 // perspective, code pages will be freed up by the OS anyway if unused for
 // a while in many cases, and this helps us pre-init.
-typedef std::map<OUString, std::unique_ptr<osl::Module>> ModuleMap;
+typedef std::map<OUString, std::shared_ptr<osl::Module>> ModuleMap;
 static ModuleMap g_aModuleMap;
-static osl::Module g_aMergedLib;
+
+#if ENABLE_MERGELIBS
+static std::shared_ptr<osl::Module> g_pMergedLib = std::make_shared<osl::Module>();
+#endif
 
 #ifndef SAL_DLLPREFIX
 #  define SAL_DLLPREFIX ""
+#endif
+
 #endif
 
 void VclBuilder::preload()
@@ -1242,8 +1247,8 @@ void VclBuilder::preload()
 #ifndef DISABLE_DYNLOADING
 
 #if ENABLE_MERGELIBS
-    g_aMergedLib.loadRelative(&thisModule, SVLIBRARY("merged"));
-#endif
+    g_pMergedLib->loadRelative(&thisModule, SVLIBRARY("merged"));
+#else
 // find -name '*ui*' | xargs grep 'class=".*lo-' |
 //     sed 's/.*class="//' | sed 's/-.*$//' | sort | uniq
     static const char *aWidgetLibs[] = {
@@ -1266,6 +1271,7 @@ void VclBuilder::preload()
         else
             delete pModule;
     }
+#endif // ENABLE_MERGELIBS
 #endif // DISABLE_DYNLOADING
 }
 
@@ -1768,6 +1774,7 @@ VclPtr<vcl::Window> VclBuilder::makeObject(vcl::Window *pParent, const OString &
         {
             OUString sFunction(OStringToOUString(OString("make") + name.copy(nDelim+1), RTL_TEXTENCODING_UTF8));
 
+            customMakeWidget pFunction = nullptr;
 #ifndef DISABLE_DYNLOADING
             OUStringBuffer sModuleBuf;
             sModuleBuf.append(SAL_DLLPREFIX);
@@ -1778,21 +1785,26 @@ VclPtr<vcl::Window> VclBuilder::makeObject(vcl::Window *pParent, const OString &
             ModuleMap::iterator aI = g_aModuleMap.find(sModule);
             if (aI == g_aModuleMap.end())
             {
-                osl::Module* pModule = new osl::Module;
-                bool ok = false;
+                std::shared_ptr<osl::Module> pModule;
 #if ENABLE_MERGELIBS
-                if (!g_aMergedLib.is())
-                    g_aMergedLib.loadRelative(&thisModule, SVLIBRARY("merged"));
-                ok = g_aMergedLib.getFunctionSymbol(sFunction);
+                if (!g_pMergedLib->is())
+                    g_pMergedLib->loadRelative(&thisModule, SVLIBRARY("merged"));
+                if ((pFunction = reinterpret_cast<customMakeWidget>(g_pMergedLib->getFunctionSymbol(sFunction))))
+                    pModule = g_pMergedLib;
 #endif
-                if (!ok)
-                    ok = pModule->loadRelative(&thisModule, sModule);
-                assert(ok && "bad module name in .ui");
-                aI = g_aModuleMap.insert(std::make_pair(sModule, std::unique_ptr<osl::Module>(pModule))).first;
+                if (!pFunction)
+                {
+                    pModule.reset(new osl::Module);
+                    bool ok = pModule->loadRelative(&thisModule, sModule);
+                    assert(ok && "bad module name in .ui");
+                    pFunction = reinterpret_cast<customMakeWidget>(pModule->getFunctionSymbol(sFunction));
+                }
+                g_aModuleMap.insert(std::make_pair(sModule, pModule));
             }
-            customMakeWidget pFunction = reinterpret_cast<customMakeWidget>(aI->second->getFunctionSymbol(sFunction));
+            else
+                pFunction = reinterpret_cast<customMakeWidget>(aI->second->getFunctionSymbol(sFunction));
 #else
-            customMakeWidget pFunction = reinterpret_cast<customMakeWidget>(osl_getFunctionSymbol((oslModule) RTLD_DEFAULT, sFunction.pData));
+            pFunction = reinterpret_cast<customMakeWidget>(osl_getFunctionSymbol((oslModule) RTLD_DEFAULT, sFunction.pData));
 #endif
             if (pFunction)
             {