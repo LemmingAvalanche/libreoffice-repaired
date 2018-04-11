LemmingAvalanche/libreoffice-repaired

commit 0966edeb8defa604fd67e1796d7e46f47403c6d7

loplugin:staticmethods
Change-Id: Ie7a1a5bc61d19aebd11a00c63c1f4104d893efbe

sd/source/filter/ppt/pptinanimations.cxx
@@ -1449,7 +1449,7 @@ int AnimationImporter::importTimeContainer( const Atom* pAtom, const Reference< 
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 
@@ -1523,7 +1523,7 @@ int AnimationImporter::importAnimationNodeContainer( const Atom* pAtom, const Re
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 
@@ -1602,7 +1602,7 @@ void AnimationImporter::importAnimateFilterContainer( const Atom* pAtom, const R
 
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -1733,7 +1733,7 @@ void AnimationImporter::importAnimateAttributeTargetContainer( const Atom* pAtom
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 
@@ -1880,7 +1880,7 @@ void AnimationImporter::importAnimateColorContainer( const Atom* pAtom, const Re
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -1937,7 +1937,7 @@ void AnimationImporter::importAnimateSetContainer( const Atom* pAtom, const Refe
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -2014,7 +2014,7 @@ void AnimationImporter::importAnimateContainer( const Atom* pAtom, const Referen
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -2094,7 +2094,7 @@ void AnimationImporter::importAnimateMotionContainer( const Atom* pAtom, const R
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -2158,7 +2158,7 @@ void AnimationImporter::importCommandContainer( const Atom* pAtom, const Referen
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
 
         if( nBits & 3 )
@@ -2281,7 +2281,7 @@ int AnimationImporter::importAudioContainer( const Atom* pAtom, const Reference<
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
 
         // TODO: What to do with them?
@@ -2386,7 +2386,7 @@ void AnimationImporter::importAnimateScaleContainer( const Atom* pAtom, const Re
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -2454,7 +2454,7 @@ void AnimationImporter::importAnimateRotationContainer( const Atom* pAtom, const
                 break;
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -2601,13 +2601,13 @@ void AnimationImporter::importAnimateKeyPoints( const Atom* pAtom, const Referen
                 double fTemp = (double)nTemp / 1000.0;
                 aKeyTimes[nKeyTime] = fTemp;
 
-                const Atom* pValue = pAtom->findNextChildAtom(pIter);
+                const Atom* pValue = Atom::findNextChildAtom(pIter);
                 if( pValue && pValue->getType() == DFF_msofbtAnimAttributeValue )
                 {
                     Any aValue1, aValue2;
                     if( importAttributeValue( pValue, aValue1 ) )
                     {
-                        pValue = pAtom->findNextChildAtom(pValue);
+                        pValue = Atom::findNextChildAtom(pValue);
                         if( pValue && pValue->getType() == DFF_msofbtAnimAttributeValue )
                             (void)importAttributeValue( pValue, aValue2 );
 
@@ -2847,7 +2847,7 @@ void AnimationImporter::importAnimationEvents( const Atom* pAtom, const Referenc
                 }
                 }
 
-                pChildAtom = pEventAtom->findNextChildAtom( pChildAtom );
+                pChildAtom = Atom::findNextChildAtom( pChildAtom );
             }
 
             *pEvents = addToSequence( *pEvents, (aEvent.Trigger == EventTrigger::NONE) ? aEvent.Offset : makeAny( aEvent ) );
@@ -3042,7 +3042,7 @@ sal_Int32 AnimationImporter::importTargetElementContainer( const Atom* pAtom, An
                 break;
             }
 
-        pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+        pChildAtom = Atom::findNextChildAtom( pChildAtom );
 
         }
     }
@@ -3070,7 +3070,7 @@ void AnimationImporter::importPropertySetContainer( const Atom* pAtom, PropertyS
                 OSL_FAIL("unknown atom inside ppt::AnimationImporter::importPropertySetContainer()!");
             }
 
-            pChildAtom = pAtom->findNextChildAtom( pChildAtom );
+            pChildAtom = Atom::findNextChildAtom( pChildAtom );
         }
     }
 }
@@ -3214,7 +3214,7 @@ void AnimationImporter::dump_atom( const Atom* pAtom, bool bNewLine )
                     dump_atom_header( pChildAtom, false, pChildAtom->getType() == DFF_msofbtAnimAttributeValue );
                 }
 
-                pChildAtom = pAtom->findNextChildAtom(pChildAtom);
+                pChildAtom = Atom::findNextChildAtom(pChildAtom);
             }
         }
         else if( pAtom->seekToContent() )

sd/source/filter/ppt/pptinanimations.hxx
@@ -75,19 +75,19 @@ private:
     bool importAttributeNamesContainer( const Atom* pAtom, OUString& rAttributeNames );
     sal_Int32 importTargetElementContainer( const Atom* pAtom, ::com::sun::star::uno::Any& rTarget, sal_Int16& nSubType );
 
-    void fillNode( ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xTiming, const AnimationNode& rNode, const PropertySet& rSet );
-    ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode > createNode( const Atom* pAtom, const AnimationNode& rNode );
+    static void fillNode( ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xTiming, const AnimationNode& rNode, const PropertySet& rSet );
+    static ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode > createNode( const Atom* pAtom, const AnimationNode& rNode );
 
     bool convertAnimationNode( const ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xNode, const ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xParent );
-    bool convertAnimationValue( MS_AttributeNames eAttribute, com::sun::star::uno::Any& rValue );
+    static bool convertAnimationValue( MS_AttributeNames eAttribute, com::sun::star::uno::Any& rValue );
 
-    void fixMainSequenceTiming( const ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xNode );
-    void fixInteractiveSequenceTiming( const ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xNode );
+    static void fixMainSequenceTiming( const ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xNode );
+    static void fixInteractiveSequenceTiming( const ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode >& xNode );
 
     void processAfterEffectNodes();
 
     ::com::sun::star::uno::Any  implGetColorAny( sal_Int32 nMode, sal_Int32  nA, sal_Int32 nB, sal_Int32 nC );
-    sal_Int16                   implGetColorSpace( sal_Int32 nMode, sal_Int32  nA, sal_Int32 nB, sal_Int32 nC );
+    static sal_Int16            implGetColorSpace( sal_Int32 nMode, sal_Int32  nA, sal_Int32 nB, sal_Int32 nC );
 
 private:
     ::com::sun::star::uno::Reference< ::com::sun::star::animations::XAnimationNode > mxRootNode;
@@ -104,18 +104,18 @@ private:
     void dump( sal_uInt32 nLen, bool bNewLine = true );
 #endif
 
-    void dump_atom_header( const Atom* pAtom, bool bOpen, bool bAppend );
-    void dump_atom( const Atom* pAtom, bool bNewLine = true );
-    void dump_target( ::com::sun::star::uno::Any& rAny );
-    void dump( ::com::sun::star::uno::Any& rAny );
-    void dump( const PropertySet& rSet );
-    void dump( const AnimationNode& rNode );
-    void dump( const char * pText );
-    void dump( const char * pText, sal_Int32 nInt );
+    static void dump_atom_header( const Atom* pAtom, bool bOpen, bool bAppend );
+    static void dump_atom( const Atom* pAtom, bool bNewLine = true );
+    static void dump_target( ::com::sun::star::uno::Any& rAny );
+    static void dump( ::com::sun::star::uno::Any& rAny );
+    static void dump( const PropertySet& rSet );
+    static void dump( const AnimationNode& rNode );
+    static void dump( const char * pText );
+    static void dump( const char * pText, sal_Int32 nInt );
     void dump( const char * pText, sal_Int64 nInt );
-    void dump( const char * pText, double fDouble );
-    void dump( const char * pText, const char * pText2 );
-    void dump( const char * pText, const OUString& rString );
+    static void dump( const char * pText, double fDouble );
+    static void dump( const char * pText, const char * pText2 );
+    static void dump( const char * pText, const OUString& rString );
 };
 
 } // namespace ppt

commit bf9c69fae1c1183b82d310fdfd05741ed7e7c0fa

Fix building with DBG_ANIM_LOG defined
Partial regression from 0966edeb8defa604fd67e1796d7e46f47403c6d7

Change-Id: Ie9eded8673131e19428411e430951feaf33218bb
Reviewed-on: https://gerrit.libreoffice.org/52701
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/filter/ppt/pptinanimations.cxx
@@ -1227,7 +1227,7 @@ int AnimationImporter::importTimeContainer( const Atom* pAtom, const Reference< 
                 {
 #ifdef DBG_ANIM_LOG
                     sal_uInt32 nU1, nU2;
-                    mrStCtrl >> nU1 >> nU2;
+                    mrStCtrl.ReadUInt32(nU1).ReadUInt32(nU2);
 
                     fprintf( mpFile, "<unknown_0xf136 nU1=\"%ld\" nU2=\"%ld\"/>\n", nU1, nU2 );
 #endif
@@ -2932,11 +2932,11 @@ void AnimationImporter::dump( sal_uInt32 nLen, bool bNewLine )
 
     sal_uInt32 i = 0;
     int b = 0;
-    sal_Int8 nData;
+    char nData;
 
     for( i = 0; i < nLen; i++ )
     {
-        mrStCtrl >> nData;
+        mrStCtrl.ReadChar(nData);
 
         fprintf( mpFile, "%c%c ", faul[ (nData >> 4) & 0x0f ], faul[ nData & 0x0f ] );
 

sd/source/filter/ppt/pptinanimations.hxx
@@ -78,7 +78,7 @@ private:
     static css::uno::Reference< css::animations::XAnimationNode > createNode( const Atom* pAtom, const AnimationNode& rNode );
 
     bool convertAnimationNode( const css::uno::Reference< css::animations::XAnimationNode >& xNode, const css::uno::Reference< css::animations::XAnimationNode >& xParent );
-    static bool convertAnimationValue( oox::ppt::MS_AttributeNames eAttribute, css::uno::Any& rValue );
+    bool convertAnimationValue( oox::ppt::MS_AttributeNames eAttribute, css::uno::Any& rValue );
 
     css::uno::Any  implGetColorAny( sal_Int32 nMode, sal_Int32  nA, sal_Int32 nB, sal_Int32 nC );
     static sal_Int16            implGetColorSpace( sal_Int32 nMode, sal_Int32  nA, sal_Int32 nB, sal_Int32 nC );
@@ -98,18 +98,18 @@ private:
     void dump( sal_uInt32 nLen, bool bNewLine = true );
 #endif
 
-    static void dump_atom_header( const Atom* pAtom, bool bOpen, bool bAppend );
-    static void dump_atom( const Atom* pAtom, bool bNewLine = true );
-    static void dump_target( css::uno::Any& rAny );
-    static void dump( css::uno::Any& rAny );
-    static void dump( const PropertySet& rSet );
-    static void dump( const AnimationNode& rNode );
-    static void dump( const char * pText );
-    static void dump( const char * pText, sal_Int32 nInt );
+    void dump_atom_header( const Atom* pAtom, bool bOpen, bool bAppend );
+    void dump_atom( const Atom* pAtom, bool bNewLine = true );
+    void dump_target( css::uno::Any& rAny );
+    void dump( css::uno::Any& rAny );
+    void dump( const PropertySet& rSet );
+    void dump( const AnimationNode& rNode );
+    void dump( const char * pText );
+    void dump( const char * pText, sal_Int32 nInt );
     void dump( const char * pText, sal_Int64 nInt );
-    static void dump( const char * pText, double fDouble );
-    static void dump( const char * pText, const char * pText2 );
-    static void dump( const char * pText, const OUString& rString );
+    void dump( const char * pText, double fDouble );
+    void dump( const char * pText, const char * pText2 );
+    void dump( const char * pText, const OUString& rString );
 };
 
 } // namespace ppt