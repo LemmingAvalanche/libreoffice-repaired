LemmingAvalanche/libreoffice-repaired

commit 619ebaf45686ddb07d109a760a7540a23bb6444d

move some mask code from SvxMSDffManager::ImportGraphic inside BitmapEx
part of moving GetMask/GetAlpha touching code inside vcl

Change-Id: I46e908ed6216dd521143ca94d2dcbd7e95d8b9ce
Reviewed-on: https://gerrit.libreoffice.org/51408
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Jenkins <ci@libreoffice.org>

vcl/source/gdi/bitmapex.cxx
@@ -1441,4 +1441,12 @@ void BitmapEx::GetSplitData( std::vector<sal_uInt8>& rvColorData, std::vector<sa
     }
 }
 
+void BitmapEx::CombineMaskOr(Color maskColor, sal_uInt8 nTol)
+{
+    Bitmap aNewMask = maBitmap.CreateMask( maskColor, nTol );
+    if ( IsTransparent() )
+         aNewMask.CombineSimple( maMask, BmpCombine::Or );
+    maMask = aNewMask;
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

filter/source/msfilter/msdffimp.cxx
@@ -3822,12 +3822,9 @@ SdrObject* SvxMSDffManager::ImportGraphic( SvStream& rSt, SfxItemSet& rSet, cons
 
                 if ( aGraf.GetType() == GraphicType::Bitmap )
                 {
-                    BitmapEx    aBitmapEx( aGraf.GetBitmapEx() );
-                    Bitmap      aBitmap( aBitmapEx.GetBitmap() );
-                    Bitmap      aMask( aBitmap.CreateMask( MSO_CLR_ToColor( nTransColor, DFF_Prop_pictureTransparent ), 9 ) );
-                    if ( aBitmapEx.IsTransparent() )
-                        aMask.CombineSimple( aBitmapEx.GetMask(), BmpCombine::Or );
-                    aGraf = BitmapEx( aBitmap, aMask );
+                    BitmapEx aBitmapEx( aGraf.GetBitmapEx() );
+                    aBitmapEx.CombineMaskOr( MSO_CLR_ToColor( nTransColor, DFF_Prop_pictureTransparent ), 9 );
+                    aGraf = aBitmapEx;
                 }
             }
 

include/vcl/bitmapex.hxx
@@ -448,6 +448,7 @@ public:
 
     void                AdjustTransparency( sal_uInt8 cTrans );
 
+    void                CombineMaskOr(Color maskColor, sal_uInt8 nTol);
 public:
 
     SAL_DLLPRIVATE std::shared_ptr<ImpBitmap> const & ImplGetBitmapImpBitmap() const { return maBitmap.ImplGetImpBitmap(); }

commit 88907d0d174636bfbe523a5b47fd0ec14e1d48f5

tdf#116929 Transparent mask imported incorrectly
regression from
    commit 619ebaf45686ddb07d109a760a7540a23bb6444d
    Date:   Fri Mar 16 13:43:08 2018 +0200
    move some mask code from SvxMSDffManager::ImportGraphic inside
BitmapEx

Change-Id: Idee9728f1274f487f5c085f1756ccb5cf28430ec
Reviewed-on: https://gerrit.libreoffice.org/52716
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/bitmapex.cxx
@@ -1454,6 +1454,7 @@ void BitmapEx::CombineMaskOr(Color maskColor, sal_uInt8 nTol)
     if ( IsTransparent() )
          aNewMask.CombineSimple( maMask, BmpCombine::Or );
     maMask = aNewMask;
+    meTransparent = TransparentType::Bitmap;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */