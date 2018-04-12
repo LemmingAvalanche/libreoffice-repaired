LemmingAvalanche/libreoffice-repaired

commit 9f95f2e8ab7eaa25dc85a2afc43c0f930eccfe98

new loplugin:unusedvariablemore
collection of heuristics to look for local variables that are never read
from i.e. do not contribute to the surrounding logic

This is an expensive plugin, since it walks up the parent tree,
so it is off by default.

Change-Id: Ib8ba292241bd16adf299e8bba4502cb473513a06
Reviewed-on: https://gerrit.libreoffice.org/52450
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

jvmfwk/source/framework.cxx
@@ -275,11 +275,9 @@ javaFrameworkError jfw_startVM(
             index ++;
         }
         //add all options of the arOptions argument
-        std::vector<OString> convertedOptions;
         for (auto const & ii: arOptions)
         {
             OString conv = OUStringToOString(ii, osl_getThreadTextEncoding());
-            convertedOptions.push_back(conv);
             arOpt[index].optionString = const_cast<char *>(conv.getStr());
             arOpt[index].extraInfo = nullptr;
             index++;

commit 7997ad29b2aaa42b12b63c032bb4ce9bbb3bd22c

Keep strings alive again as necessary
...fixing regression introduced with 9f95f2e8ab7eaa25dc85a2afc43c0f930eccfe98
"new loplugin:unusedvariablemore"

Change-Id: Ib598d67add50031937c9c9f3e23ea16d0a1da107

jvmfwk/source/framework.cxx
@@ -275,9 +275,13 @@ javaFrameworkError jfw_startVM(
             index ++;
         }
         //add all options of the arOptions argument
+        std::vector<OString> convertedOptions;
         for (auto const & ii: arOptions)
         {
             OString conv = OUStringToOString(ii, osl_getThreadTextEncoding());
+            convertedOptions.push_back(conv);
+                // keep conv.getStr() alive until after the call to
+                // jfw_plugin_startJavaVirtualMachine below
             arOpt[index].optionString = const_cast<char *>(conv.getStr());
             arOpt[index].extraInfo = nullptr;
             index++;