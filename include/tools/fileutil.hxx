LemmingAvalanche/libreoffice-repaired

commit 7c95f1f5247cbaeb38c28f919e2795e6884e8a99

tdf#106942 Wrong message when lock file is empty or corrupt
1. Erase empty or corrupt lockfile, when reading it fails and
   skip "Locked Document Dialog" in that case
2. Show Dialog (use LockFileIgnoreRequest -> LockFailedQueryBox)
   when create lock file is not possible.
   Two Dialogs:  "lock file create error" and
                 "empty lockfile is present"
   Set the document to read-only when creating lockfile is not
   possible. If lockfile is corrupt or empty, inform the user.
   They can interrupt loading the document or open it read-only,
   which also erases the corrupt lock file after closing
   the document
3. Handling for the two Dialogs
   Use LockFileIgnoreRequst for create lockfile error and
   add a new LockFileCorruptRequest, (lock file is corrupt).
   Change and generate new messages for both dialogs in english

Change-Id: I35c377f85b5113e8ffb89d83b9544b8ebc81d89f
Reviewed-on: https://gerrit.libreoffice.org/36658
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

sfx2/source/doc/docfile.cxx
@@ -31,6 +31,7 @@
 #include <com/sun/star/document/LockedDocumentRequest.hpp>
 #include <com/sun/star/document/OwnLockOnDocumentRequest.hpp>
 #include <com/sun/star/document/LockFileIgnoreRequest.hpp>
+#include <com/sun/star/document/LockFileCorruptRequest.hpp>
 #include <com/sun/star/document/ChangedByOthersRequest.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/embed/XTransactedObject.hpp>
@@ -119,6 +120,8 @@
 #include "openflag.hxx"
 #include <sfx2/sfxresid.hxx>
 #include <officecfg/Office/Common.hxx>
+
+#include <com/sun/star/io/WrongFormatException.hpp>
 
 #include <memory>
 
@@ -817,12 +820,12 @@ void SfxMedium::SetEncryptionDataToStorage_Impl()
 // not for some URL scheme belongs in UCB, not here.
 
 
-SfxMedium::ShowLockResult SfxMedium::ShowLockedDocumentDialog( const LockFileEntry& aData, bool bIsLoading, bool bOwnLock )
+SfxMedium::ShowLockResult SfxMedium::ShowLockedDocumentDialog( const LockFileEntry& aData, bool bIsLoading, bool bOwnLock, bool bHandleSysLocked )
 {
     ShowLockResult nResult = ShowLockResult::NoLock;
 
-    // tdf#92817: Simple check for empty lock file that needs to be deleted
-    if( aData[LockFileComponent::OOOUSERNAME].isEmpty() && aData[LockFileComponent::SYSUSERNAME].isEmpty() )
+    // tdf#92817: Simple check for empty lock file that needs to be deleted, when system locking is enabled
+    if( aData[LockFileComponent::OOOUSERNAME].isEmpty() && aData[LockFileComponent::SYSUSERNAME].isEmpty() && !bHandleSysLocked )
         bOwnLock=true;
 
     // show the interaction regarding the document opening
@@ -914,6 +917,51 @@ SfxMedium::ShowLockResult SfxMedium::ShowLockedDocumentDialog( const LockFileEnt
     }
 
     return nResult;
+}
+
+
+bool SfxMedium::ShowLockFileProblemDialog(MessageDlg nWhichDlg)
+{
+    // system file locking is not active, ask user whether he wants to open the document without any locking
+    uno::Reference< task::XInteractionHandler > xHandler = GetInteractionHandler();
+
+    if (xHandler.is())
+    {
+        ::rtl::Reference< ::ucbhelper::InteractionRequest > xIgnoreRequestImpl;
+
+        switch (nWhichDlg)
+        {
+            case MessageDlg::LockFileIgnore:
+                xIgnoreRequestImpl = new ::ucbhelper::InteractionRequest(uno::makeAny( document::LockFileIgnoreRequest() ));
+                break;
+            case MessageDlg::LockFileCorrupt:
+                xIgnoreRequestImpl = new ::ucbhelper::InteractionRequest(uno::makeAny( document::LockFileCorruptRequest() ));
+                break;
+        }
+
+        uno::Sequence< uno::Reference< task::XInteractionContinuation > > aContinuations(2);
+        aContinuations[0] = new ::ucbhelper::InteractionAbort(xIgnoreRequestImpl.get());
+        aContinuations[1] = new ::ucbhelper::InteractionApprove(xIgnoreRequestImpl.get());
+        xIgnoreRequestImpl->setContinuations(aContinuations);
+
+        xHandler->handle(xIgnoreRequestImpl.get());
+
+        ::rtl::Reference< ::ucbhelper::InteractionContinuation > xSelected = xIgnoreRequestImpl->getSelection();
+        bool bReadOnly = uno::Reference< task::XInteractionApprove >(xSelected.get(), uno::UNO_QUERY).is();
+
+        if (bReadOnly)
+        {
+            GetItemSet()->Put(SfxBoolItem(SID_DOC_READONLY, true));
+        }
+        else
+        {
+            SetError(ERRCODE_ABORT);
+        }
+
+        return bReadOnly;
+    }
+
+    return false;
 }
 
 namespace
@@ -1011,7 +1059,7 @@ void SfxMedium::LockOrigFileOnDemand( bool bLoading, bool bNoUI )
 
                             if ( !bResult && !bNoUI )
                             {
-                                bUIStatus = ShowLockedDocumentDialog( aLockData, bLoading, false );
+                                bUIStatus = ShowLockedDocumentDialog( aLockData, bLoading, false , false );
                             }
                         }
                         catch( ucb::InteractiveNetworkWriteException& )
@@ -1136,59 +1184,34 @@ void SfxMedium::LockOrigFileOnDemand( bool bLoading, bool bNoUI )
                         try
                         {
                             ::svt::DocumentLockFile aLockFile( pImpl->m_aLogicName );
-                            if ( !bHandleSysLocked )
+                            bool  bIoErr = false;
+
+                            if (!bHandleSysLocked)
                             {
                                 try
                                 {
                                     bResult = aLockFile.CreateOwnLockFile();
                                 }
-                                catch ( const ucb::InteractiveIOException& e )
+                                catch (const ucb::InteractiveIOException&)
                                 {
-                                    // exception means that the lock file can not be successfully accessed
-                                    // in this case it should be ignored if system file locking is anyway active
-                                    if ( bUseSystemLock || !IsOOoLockFileUsed() )
+                                    if (bLoading && !bNoUI)
                                     {
-                                        bResult = true;
-                                        // take the ownership over the lock file
-                                        aLockFile.OverwriteOwnLockFile();
-                                    }
-                                    else if ( e.Code == IOErrorCode_INVALID_PARAMETER )
-                                    {
-                                        // system file locking is not active, ask user whether he wants to open the document without any locking
-                                        uno::Reference< task::XInteractionHandler > xHandler = GetInteractionHandler();
-
-                                        if ( xHandler.is() )
-                                        {
-                                            ::rtl::Reference< ::ucbhelper::InteractionRequest > xIgnoreRequestImpl
-                                                = new ::ucbhelper::InteractionRequest( uno::makeAny( document::LockFileIgnoreRequest() ) );
-
-                                            uno::Sequence< uno::Reference< task::XInteractionContinuation > > aContinuations( 2 );
-                                            aContinuations[0] = new ::ucbhelper::InteractionAbort( xIgnoreRequestImpl.get() );
-                                            aContinuations[1] = new ::ucbhelper::InteractionApprove( xIgnoreRequestImpl.get() );
-                                            xIgnoreRequestImpl->setContinuations( aContinuations );
-
-                                            xHandler->handle( xIgnoreRequestImpl.get() );
-
-                                            ::rtl::Reference< ::ucbhelper::InteractionContinuation > xSelected = xIgnoreRequestImpl->getSelection();
-                                            bResult = uno::Reference< task::XInteractionApprove >( xSelected.get(), uno::UNO_QUERY ).is();
-                                        }
+                                        bIoErr = true;
+                                        bResult = ShowLockFileProblemDialog(MessageDlg::LockFileIgnore);
                                     }
                                 }
-                                catch ( const uno::Exception& )
+                                catch (const uno::Exception&)
                                 {
-                                    // exception means that the lock file can not be successfully accessed
-                                    // in this case it should be ignored if system file locking is anyway active
-                                    if ( bUseSystemLock || !IsOOoLockFileUsed() )
+                                    if (bLoading && !bNoUI)
                                     {
-                                        bResult = true;
-                                        // take the ownership over the lock file
-                                        aLockFile.OverwriteOwnLockFile();
+                                        bIoErr = true;
+                                        bResult = ShowLockFileProblemDialog(MessageDlg::LockFileIgnore);
                                     }
                                 }
 
                                 // in case OOo locking is turned off the lock file is still written if possible
                                 // but it is ignored while deciding whether the document should be opened for editing or not
-                                if ( !bResult && !IsOOoLockFileUsed() )
+                                if (!bResult && !IsOOoLockFileUsed() && !bIoErr)
                                 {
                                     bResult = true;
                                     // take the ownership over the lock file
@@ -1196,46 +1219,54 @@ void SfxMedium::LockOrigFileOnDemand( bool bLoading, bool bNoUI )
                                 }
                             }
 
-
                             if ( !bResult )
                             {
                                 LockFileEntry aData;
                                 try
                                 {
-                                    // impossibility to get data is no real problem
                                     aData = aLockFile.GetLockData();
+                                }
+                                catch (const io::WrongFormatException&)
+                                {
+                                    // we get empty or corrupt data
+                                    // info to the user
+                                    if (!bIoErr && bLoading && !bNoUI )
+                                        bResult = ShowLockFileProblemDialog(MessageDlg::LockFileCorrupt);
+
+                                    // not show the Lock Document Dialog
+                                    bIoErr = true;
                                 }
                                 catch( const uno::Exception& )
                                 {
+                                    // show the Lock Document Dialog, when locked from other app
+                                    bIoErr = !bHandleSysLocked;
                                 }
 
                                 bool bOwnLock = false;
 
-                                if ( !bHandleSysLocked )
+                                if (!bHandleSysLocked)
                                 {
                                     LockFileEntry aOwnData = svt::LockFileCommon::GenerateOwnEntry();
-                                    bOwnLock = aOwnData[LockFileComponent::SYSUSERNAME].equals( aData[LockFileComponent::SYSUSERNAME] );
+                                    bOwnLock = aOwnData[LockFileComponent::SYSUSERNAME].equals(aData[LockFileComponent::SYSUSERNAME]);
 
-                                    if ( bOwnLock
-                                      && aOwnData[LockFileComponent::LOCALHOST].equals( aData[LockFileComponent::LOCALHOST] )
-                                      && aOwnData[LockFileComponent::USERURL].equals( aData[LockFileComponent::USERURL] ) )
+                                    if (bOwnLock
+                                        && aOwnData[LockFileComponent::LOCALHOST].equals(aData[LockFileComponent::LOCALHOST])
+                                        && aOwnData[LockFileComponent::USERURL].equals(aData[LockFileComponent::USERURL]))
                                     {
                                         // this is own lock from the same installation, it could remain because of crash
                                         bResult = true;
                                     }
                                 }
 
-                                if ( !bResult && !bNoUI )
+                                if ( !bResult && !bNoUI && !bIoErr)
                                 {
-                                    bUIStatus = ShowLockedDocumentDialog( aData, bLoading, bOwnLock );
+                                    bUIStatus = ShowLockedDocumentDialog( aData, bLoading, bOwnLock, bHandleSysLocked );
                                     if ( bUIStatus == ShowLockResult::Succeeded )
                                     {
                                         // take the ownership over the lock file
                                         bResult = aLockFile.OverwriteOwnLockFile();
                                     }
                                 }
-
-                                bHandleSysLocked = false;
                             }
                         }
                         catch( const uno::Exception& )
@@ -2736,12 +2767,23 @@ void SfxMedium::UnlockFile( bool bReleaseLockStream )
 
     if ( pImpl->m_bLocked )
     {
+        ::svt::DocumentLockFile aLockFile( pImpl->m_aLogicName );
+
         try
         {
             pImpl->m_bLocked = false;
-            ::svt::DocumentLockFile aLockFile( pImpl->m_aLogicName );
             // TODO/LATER: A warning could be shown in case the file is not the own one
             aLockFile.RemoveFile();
+        }
+        catch( const io::WrongFormatException& )
+        {
+            try
+            {
+                // erase the empty or corrupt file
+                aLockFile.RemoveFileDirectly();
+            }
+            catch( const uno::Exception& )
+            {}
         }
         catch( const uno::Exception& )
         {}

include/sfx2/docfile.hxx
@@ -163,8 +163,10 @@ public:
     bool                IsStorage();
 
     enum class ShowLockResult { NoLock, Succeeded,Try };
-    ShowLockResult      ShowLockedDocumentDialog( const LockFileEntry& aData, bool bIsLoading, bool bOwnLock );
+    ShowLockResult      ShowLockedDocumentDialog( const LockFileEntry& aData, bool bIsLoading, bool bOwnLock, bool bHandleSysLocked);
     void                LockOrigFileOnDemand( bool bLoading, bool bNoUI );
+    enum class MessageDlg    { LockFileIgnore, LockFileCorrupt };
+    bool                ShowLockFileProblemDialog(MessageDlg nWhichDlg);
     void                DisableUnlockWebDAV( bool bDisableUnlockWebDAV = true );
     void                UnlockFile( bool bReleaseLockStream );
 

include/svl/documentlockfile.hxx
@@ -48,7 +48,10 @@ public:
     bool CreateOwnLockFile();
     LockFileEntry GetLockData();
     bool OverwriteOwnLockFile();
+    /// Delete the Lockfile, if current user is the owner
     void RemoveFile();
+    /// Only delete lockfile, disregarding ownership
+    void RemoveFileDirectly();
 
     static bool IsInteractionAllowed() { return m_bAllowInteraction; }
 };

l10ntools/source/gRun.sh
@@ -490,7 +490,7 @@ ${MYCMD} --base sw/source/uibase/utlui --files attrdesc.src initui.src navipi.sr
 
 ${MYCMD} --files sw/source/uibase/wrtsh/wrtsh.src
 
-${MYCMD} --base uui/source --files alreadyopen.src filechanged.src ids.src lockfailed.src nameclashdlg.src openlocked.src passworddlg.src passworderrs.src trylater.src
+${MYCMD} --base uui/source --files alreadyopen.src filechanged.src ids.src lockfailed.src lockcorrupt.src nameclashdlg.src openlocked.src passworddlg.src passworderrs.src trylater.src
 
 ${MYCMD} --files vcl/source/edit/textundo.src
 

offapi/UnoApi_offapi.mk
@@ -2190,6 +2190,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/document,\
 	FilterOptionsRequest \
 	LinkUpdateModes \
 	LockFileIgnoreRequest \
+	LockFileCorruptRequest \
 	LockedDocumentRequest \
 	LockedOnSavingRequest \
 	MacroExecMode \

offapi/com/sun/star/document/LockFileCorruptRequest.idl
@@ -16,20 +16,28 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
+#ifndef __com_sun_star_document_LockFileCorruptRequest_idl__
+#define __com_sun_star_document_LockFileCorruptRequest_idl__
 
-#include "ids.hrc"
+#include <com/sun/star/io/IOException.idl>
+#include <com/sun/star/beans/PropertyValue.idl>
+#include <com/sun/star/frame/XModel.idl>
 
-String STR_LOCKFAILED_TITLE
+
+module com { module sun { module star { module document {
+
+/** Is used for interaction handle to query user's decision
+    when the lock file is corrupt.
+
+    @since OOo 5.5
+*/
+exception LockFileCorruptRequest : ::com::sun::star::io::IOException
 {
-    Text [ en-US ] = "Document Could Not Be Locked";
 };
-String STR_LOCKFAILED_MSG
-{
-    Text [ en-US ] = "The lock file could not be created for exclusive access by %PRODUCTNAME, due to missing permission to create a lock file on that file location or lack of free disk space.";
-};
-String STR_LOCKFAILED_OPENREADONLY_BTN
-{
-    Text [ en-US ] = "Open ~Read-Only";
-};
+
+
+}; }; }; };
+
+#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svl/source/misc/documentlockfile.cxx
@@ -207,11 +207,18 @@ void DocumentLockFile::RemoveFile()
       || !aFileData[LockFileComponent::USERURL].equals( aNewEntry[LockFileComponent::USERURL] ) )
         throw io::IOException(); // not the owner, access denied
 
+    RemoveFileDirectly();
+}
+
+void DocumentLockFile::RemoveFileDirectly()
+{
     uno::Reference < css::ucb::XCommandEnvironment > xEnv;
     ::ucbhelper::Content aCnt(m_aURL, xEnv, comphelper::getProcessComponentContext());
     aCnt.executeCommand("delete",
         uno::makeAny(true));
 }
+
+
 
 } // namespace svt
 

uui/AllLangResTarget_uui.mk
@@ -25,6 +25,7 @@ $(eval $(call gb_SrsTarget_add_files,uui/res,\
 	uui/source/filechanged.src \
 	uui/source/ids.src \
 	uui/source/lockfailed.src \
+	uui/source/lockcorrupt.src \
 	uui/source/nameclashdlg.src \
 	uui/source/openlocked.src \
 	uui/source/passworddlg.src \

commit d9f778f97103e80b9d385f17068d8e13778a77a8

tdf#116420: Windows: Test if a filepath redirects to a WebDAV resource
In Windows, filesystem redirectors can map WebDAV resources to UNC
paths, or to drive-based "local" paths; so a WebDAV URI of the form
"http://WebDADServer/root/directory/File.ext" may be accessed using
"\\WebDADServer\root\directory\File.ext" or "Z:\directory\File.ext".

When using these paths, failure to create a lockfile aside the opened
document should not be considered an error; so this patch checks for
this.

Regression from commit 7c95f1f5247cbaeb38c28f919e2795e6884e8a99.

Change-Id: I1de55b66447dc91d22b6d2b5b121de96bf32e4ee
Reviewed-on: https://gerrit.libreoffice.org/53070
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

include/tools/fileutil.hxx
@@ -1,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TOOLS_FILEUTIL_HXX
+#define INCLUDED_TOOLS_FILEUTIL_HXX
+
+#include <tools/toolsdllapi.h>
+
+class INetURLObject;
+
+namespace tools
+{
+// Tests if the path is a UNC or local (drive-based) path that redirects to
+// a WebDAV resource (e.g., using redirectors on Windows).
+// Currently only implemented for Windows; on other platforms, returns false.
+TOOLS_DLLPUBLIC bool IsMappedWebDAVPath(const INetURLObject& aURL);
+}
+
+#endif // INCLUDED_TOOLS_FILEUTIL_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/doc/docfile.cxx
@@ -72,6 +72,7 @@
 #include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 #include <o3tl/make_unique.hxx>
 #include <tools/urlobj.hxx>
+#include <tools/fileutil.hxx>
 #include <unotools/configmgr.hxx>
 #include <unotools/tempfile.hxx>
 #include <comphelper/fileurl.hxx>
@@ -1305,7 +1306,13 @@ SfxMedium::LockFileResult SfxMedium::LockOrigFileOnDemand( bool bLoading, bool b
                                 }
                                 catch (const uno::Exception&)
                                 {
-                                    if (bLoading && !bNoUI)
+                                    if (tools::IsMappedWebDAVPath(GetURLObject()))
+                                    {
+                                        // This is a path that redirects to a WebDAV resource;
+                                        // so failure creating lockfile is not an error here.
+                                        bResult = true;
+                                    }
+                                    else if (bLoading && !bNoUI)
                                     {
                                         bIoErr = true;
                                         ShowLockFileProblemDialog(MessageDlg::LockFileIgnore);

tools/Library_tl.mk
@@ -47,6 +47,7 @@ $(eval $(call gb_Library_add_exception_objects,tl,\
     tools/source/datetime/tdate \
     tools/source/datetime/ttime \
     tools/source/debug/debug \
+    tools/source/fsys/fileutil \
     tools/source/fsys/urlobj \
     tools/source/fsys/wldcrd \
     tools/source/generic/b3dtrans \

tools/source/fsys/fileutil.cxx
@@ -1,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <tools/fileutil.hxx>
+#include <tools/urlobj.hxx>
+#if defined _WIN32
+#include <osl/file.hxx>
+#include <string.h>
+#include <o3tl/char16_t2wchar_t.hxx>
+#include <o3tl/make_unique.hxx>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+namespace tools
+{
+bool IsMappedWebDAVPath(const INetURLObject& aURL)
+{
+#if defined _WIN32
+    if (aURL.GetProtocol() == INetProtocol::File)
+    {
+        OUString sURL = aURL.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+        OUString aSystemPath;
+        if (osl::FileBase::getSystemPathFromFileURL(sURL, aSystemPath) == osl::FileBase::E_None)
+        {
+            DWORD nSize = MAX_PATH;
+            auto bufUNC(o3tl::make_unique<char[]>(nSize));
+            DWORD nResult = WNetGetUniversalNameW(o3tl::toW(aSystemPath.getStr()),
+                                                  UNIVERSAL_NAME_INFO_LEVEL, bufUNC.get(), &nSize);
+            if (nResult == ERROR_MORE_DATA)
+            {
+                bufUNC = o3tl::make_unique<char[]>(nSize);
+                nResult = WNetGetUniversalNameW(o3tl::toW(aSystemPath.getStr()),
+                                                UNIVERSAL_NAME_INFO_LEVEL, bufUNC.get(), &nSize);
+            }
+            if (nResult == NO_ERROR || nResult == ERROR_BAD_DEVICE)
+            {
+                NETRESOURCEW aReq{};
+                if (nResult == ERROR_BAD_DEVICE) // The path could already be an UNC
+                    aReq.lpRemoteName = const_cast<LPWSTR>(o3tl::toW(aSystemPath.getStr()));
+                else
+                {
+                    auto pInfo = reinterpret_cast<LPUNIVERSAL_NAME_INFOW>(bufUNC.get());
+                    aReq.lpRemoteName = pInfo->lpUniversalName;
+                }
+                nSize = 1024;
+                auto bufInfo(o3tl::make_unique<char[]>(nSize));
+                LPWSTR pSystem = nullptr;
+                nResult = WNetGetResourceInformationW(&aReq, bufInfo.get(), &nSize, &pSystem);
+                if (nResult == ERROR_MORE_DATA)
+                {
+                    bufInfo = o3tl::make_unique<char[]>(nSize);
+                    nResult = WNetGetResourceInformationW(&aReq, bufInfo.get(), &nSize, &pSystem);
+                }
+                if (nResult == NO_ERROR)
+                {
+                    LPNETRESOURCEW pInfo = reinterpret_cast<LPNETRESOURCEW>(bufInfo.get());
+                    if (wcscmp(pInfo->lpProvider, L"Web Client Network") == 0)
+                        return true;
+                }
+            }
+        }
+    }
+#else
+    (void)aURL;
+#endif
+    return false;
+}
+
+} // namespace tools
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */