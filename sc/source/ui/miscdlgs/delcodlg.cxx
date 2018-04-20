LemmingAvalanche/libreoffice-repaired

commit f5031d274376de856f44e27899f145249e7d6eac

drop IS_SET macro
Change-Id: I876462e3f436b1540bb1a7ef92f184fa1c0be0e8
Reviewed-on: https://gerrit.libreoffice.org/53102
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/miscdlgs/delcodlg.cxx
@@ -46,20 +46,13 @@ ScDeleteContentsDlg::ScDeleteContentsDlg(weld::Window* pParent, InsertDeleteFlag
     }
 
     m_xBtnDelAll->set_active( ScDeleteContentsDlg::bPreviousAllCheck );
-    m_xBtnDelStrings->set_active( IS_SET( InsertDeleteFlags::STRING,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
-    m_xBtnDelNumbers->set_active( IS_SET( InsertDeleteFlags::VALUE,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
-    m_xBtnDelDateTime->set_active( IS_SET( InsertDeleteFlags::DATETIME,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
-    m_xBtnDelFormulas->set_active( IS_SET( InsertDeleteFlags::FORMULA,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
-    m_xBtnDelNotes->set_active( IS_SET( InsertDeleteFlags::NOTE,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
-    m_xBtnDelAttrs->set_active( IS_SET( InsertDeleteFlags::ATTRIB,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
-    m_xBtnDelObjects->set_active( IS_SET( InsertDeleteFlags::OBJECTS,
-                                   ScDeleteContentsDlg::nPreviousChecks ) );
+    m_xBtnDelStrings->set_active( bool(InsertDeleteFlags::STRING & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelNumbers->set_active( bool(InsertDeleteFlags::VALUE & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelDateTime->set_active( bool(InsertDeleteFlags::DATETIME & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelFormulas->set_active( bool(InsertDeleteFlags::FORMULA & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelNotes->set_active( bool(InsertDeleteFlags::NOTE & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelAttrs->set_active( bool(InsertDeleteFlags::ATTRIB & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelObjects->set_active( bool(InsertDeleteFlags::OBJECTS & ScDeleteContentsDlg::nPreviousChecks) );
 
     DisableChecks( m_xBtnDelAll->get_active() );
 

sc/source/ui/miscdlgs/inscodlg.cxx
@@ -76,20 +76,13 @@ ScInsertContentsDlg::ScInsertContentsDlg( vcl::Window*       pParent,
     }
 
     mpBtnInsAll->Check     ( ScInsertContentsDlg::bPreviousAllCheck );
-    mpBtnInsStrings->Check ( IS_SET( InsertDeleteFlags::STRING,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
-    mpBtnInsNumbers->Check ( IS_SET( InsertDeleteFlags::VALUE,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
-    mpBtnInsDateTime->Check( IS_SET( InsertDeleteFlags::DATETIME,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
-    mpBtnInsFormulas->Check( IS_SET( InsertDeleteFlags::FORMULA,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
-    mpBtnInsNotes->Check   ( IS_SET( InsertDeleteFlags::NOTE,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
-    mpBtnInsAttrs->Check   ( IS_SET( InsertDeleteFlags::ATTRIB,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
-    mpBtnInsObjects->Check ( IS_SET( InsertDeleteFlags::OBJECTS,
-                                   ScInsertContentsDlg::nPreviousChecks ) );
+    mpBtnInsStrings->Check ( bool(InsertDeleteFlags::STRING & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsNumbers->Check ( bool(InsertDeleteFlags::VALUE & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsDateTime->Check( bool(InsertDeleteFlags::DATETIME & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsFormulas->Check( bool(InsertDeleteFlags::FORMULA & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsNotes->Check   ( bool(InsertDeleteFlags::NOTE & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsAttrs->Check   ( bool(InsertDeleteFlags::ATTRIB & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsObjects->Check ( bool(InsertDeleteFlags::OBJECTS & ScInsertContentsDlg::nPreviousChecks) );
 
     switch( ScInsertContentsDlg::nPreviousFormulaChecks )
     {

sc/inc/global.hxx
@@ -276,9 +276,6 @@ namespace o3tl
 #define DELETEZ(pPtr) { delete pPtr; pPtr = 0; }
 #endif
 
-                                    // is bit set in set?
-#define IS_SET(bit,set)(((set)&(bit))==(bit))
-
 enum CellType
     {
         CELLTYPE_NONE,

sc/source/core/data/stlpool.cxx
@@ -120,7 +120,7 @@ void ScStyleSheetPool::Remove( SfxStyleSheetBase* pStyle )
 {
     if ( pStyle )
     {
-        OSL_ENSURE( IS_SET( SfxStyleSearchBits::UserDefined, pStyle->GetMask() ),
+        OSL_ENSURE( SfxStyleSearchBits::UserDefined & pStyle->GetMask(),
                     "SfxStyleSearchBits::UserDefined not set!" );
 
         static_cast<ScDocumentPool&>(rPool).StyleDeleted(static_cast<ScStyleSheet*>(pStyle));

commit 0139bacda2ab021deb4e731b52a84634e68ff66c

fix for "drop IS_SET macro"
regression from

    commit f5031d274376de856f44e27899f145249e7d6eac
    Date:   Wed Apr 18 14:12:06 2018 +0200
    drop IS_SET macro

Jochen Nitschke: InsertDeleteFlags::ATTRIB sets 2 bits, before it
checked both bits and now it's true if only one of them is set.

Change-Id: I169ffae10d219063ce378257e95fbb4f863ca525
Reviewed-on: https://gerrit.libreoffice.org/53211
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Jochen Nitschke <j.nitschke+logerrit@ok.de>

sc/source/ui/miscdlgs/delcodlg.cxx
@@ -51,7 +51,7 @@ ScDeleteContentsDlg::ScDeleteContentsDlg(weld::Window* pParent, InsertDeleteFlag
     m_xBtnDelDateTime->set_active( bool(InsertDeleteFlags::DATETIME & ScDeleteContentsDlg::nPreviousChecks) );
     m_xBtnDelFormulas->set_active( bool(InsertDeleteFlags::FORMULA & ScDeleteContentsDlg::nPreviousChecks) );
     m_xBtnDelNotes->set_active( bool(InsertDeleteFlags::NOTE & ScDeleteContentsDlg::nPreviousChecks) );
-    m_xBtnDelAttrs->set_active( bool(InsertDeleteFlags::ATTRIB & ScDeleteContentsDlg::nPreviousChecks) );
+    m_xBtnDelAttrs->set_active( (InsertDeleteFlags::ATTRIB & ScDeleteContentsDlg::nPreviousChecks) == InsertDeleteFlags::ATTRIB );
     m_xBtnDelObjects->set_active( bool(InsertDeleteFlags::OBJECTS & ScDeleteContentsDlg::nPreviousChecks) );
 
     DisableChecks( m_xBtnDelAll->get_active() );

sc/source/ui/miscdlgs/inscodlg.cxx
@@ -81,7 +81,7 @@ ScInsertContentsDlg::ScInsertContentsDlg( vcl::Window*       pParent,
     mpBtnInsDateTime->Check( bool(InsertDeleteFlags::DATETIME & ScInsertContentsDlg::nPreviousChecks) );
     mpBtnInsFormulas->Check( bool(InsertDeleteFlags::FORMULA & ScInsertContentsDlg::nPreviousChecks) );
     mpBtnInsNotes->Check   ( bool(InsertDeleteFlags::NOTE & ScInsertContentsDlg::nPreviousChecks) );
-    mpBtnInsAttrs->Check   ( bool(InsertDeleteFlags::ATTRIB & ScInsertContentsDlg::nPreviousChecks) );
+    mpBtnInsAttrs->Check   ( (InsertDeleteFlags::ATTRIB & ScInsertContentsDlg::nPreviousChecks) == InsertDeleteFlags::ATTRIB );
     mpBtnInsObjects->Check ( bool(InsertDeleteFlags::OBJECTS & ScInsertContentsDlg::nPreviousChecks) );
 
     switch( ScInsertContentsDlg::nPreviousFormulaChecks )