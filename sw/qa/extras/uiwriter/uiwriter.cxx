LemmingAvalanche/libreoffice-repaired

commit fd3a99763a11cda79332d5a801826d358ebfde45

sfx2 store: create temp files next to local files (storage case)
This is similar to commit 415d72fb7384bd5e4ce787eaf754187df42074e8 (sfx2
store: create temp files next to local files, 2018-01-08), except there
only the stream case was handled (e.g. sw HTML export), while this is
the storage case (e.g. ODT export).

Change-Id: I88d78aa0f09e9fdfdd27cd0fad1a2633ff54d576
Reviewed-on: https://gerrit.libreoffice.org/48071
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

sfx2/source/doc/docfile.cxx
@@ -3398,6 +3398,27 @@ bool SfxMedium::SetWritableForUserOnly( const OUString& aURL )
     return bResult;
 }
 
+namespace
+{
+/// Get the parent directory of a temporary file for output purposes.
+OUString GetLogicBase(std::unique_ptr<SfxMedium_Impl>& pImpl)
+{
+    OUString aLogicBase;
+
+    if (comphelper::isFileUrl(pImpl->m_aLogicName) && !pImpl->m_pInStream)
+    {
+        // Try to create the temp file in the same directory when storing.
+        sal_Int32 nOffset = pImpl->m_aLogicName.lastIndexOf("/");
+        if (nOffset != -1)
+            aLogicBase = pImpl->m_aLogicName.copy(0, nOffset);
+        if (aLogicBase == "file://")
+            // Doesn't make sense.
+            aLogicBase.clear();
+    }
+
+    return aLogicBase;
+}
+}
 
 void SfxMedium::CreateTempFile( bool bReplace )
 {
@@ -3410,17 +3431,7 @@ void SfxMedium::CreateTempFile( bool bReplace )
         pImpl->m_aName.clear();
     }
 
-    OUString aLogicBase;
-    if (comphelper::isFileUrl(pImpl->m_aLogicName) && !pImpl->m_pInStream)
-    {
-        // Try to create the temp file in the same directory when storing.
-        sal_Int32 nOffset = pImpl->m_aLogicName.lastIndexOf("/");
-        if (nOffset != -1)
-            aLogicBase = pImpl->m_aLogicName.copy(0, nOffset);
-        if (aLogicBase == "file://")
-            // Doesn't make sense.
-            aLogicBase.clear();
-    }
+    OUString aLogicBase = GetLogicBase(pImpl);
     pImpl->pTempFile = new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase);
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();
@@ -3519,7 +3530,8 @@ void SfxMedium::CreateTempFileNoCopy()
     // this call always replaces the existing temporary file
     delete pImpl->pTempFile;
 
-    pImpl->pTempFile = new ::utl::TempFile();
+    OUString aLogicBase = GetLogicBase(pImpl);
+    pImpl->pTempFile = new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase);
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();
     if ( pImpl->m_aName.isEmpty() )

commit f586816c3029aca7cb1239a2e57065cea346bcd5

tdf#117225 sfx2: fix leftover temp file when saving doc with embedded objects
Regression from fd3a99763a11cda79332d5a801826d358ebfde45 (sfx2 store:
create temp files next to local files (storage case), 2018-01-17), the
optimization to store temp files during save next to the destination
document (so that it can be renamed and not copied after writing the
data successfully) causes problems when we have embedded objects.

Avoid the problem by disabling this new optimization when the document
has embedded objects.

How to fix the actual root cause is not clear to me, I see that:

- the SfxMedium::GetOutputStorage() call in
  SfxObjectShell::SaveTo_Impl() create a temp file
- the SfxMedium::Commit() call in SfxObjectShell::SaveTo_Impl() tries to
  remove the file, which fails on Windows as there is an open file
  handle to that file
- SfxObjectShell::SwitchChildrenPersistance() would close the storage,
  owning that open file handle, but it's too late

So just go back to the previous behavior for now.

Change-Id: I37259100d1ddf963c1f2d3b7bd9f460bc995815c
Reviewed-on: https://gerrit.libreoffice.org/54340
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

include/sfx2/docfile.hxx
@@ -270,6 +270,8 @@ public:
     SAL_DLLPRIVATE SignatureState GetCachedSignatureState_Impl();
     SAL_DLLPRIVATE void       SetCachedSignatureState_Impl( SignatureState nState );
 
+    void SetHasEmbeddedObjects(bool bHasEmbeddedObjects);
+
     static css::uno::Sequence < css::util::RevisionTag > GetVersionList(
                     const css::uno::Reference< css::embed::XStorage >& xStorage );
     static OUString CreateTempCopyWithExt( const OUString& aURL );

sfx2/source/doc/docfile.cxx
@@ -288,6 +288,8 @@ public:
     //             in this case the member will hold this information
     SignatureState             m_nSignatureState;
 
+    bool m_bHasEmbeddedObjects = false;
+
     util::DateTime m_aDateTime;
 
     explicit SfxMedium_Impl();
@@ -3525,6 +3527,10 @@ OUString GetLogicBase(std::unique_ptr<SfxMedium_Impl> const & pImpl)
             aLogicBase.clear();
     }
 
+    if (pImpl->m_bHasEmbeddedObjects)
+        // Embedded objects would mean a special base, ignore that.
+        aLogicBase.clear();
+
     return aLogicBase;
 }
 }
@@ -3826,6 +3832,11 @@ SignatureState SfxMedium::GetCachedSignatureState_Impl()
 void SfxMedium::SetCachedSignatureState_Impl( SignatureState nState )
 {
     pImpl->m_nSignatureState = nState;
+}
+
+void SfxMedium::SetHasEmbeddedObjects(bool bHasEmbeddedObjects)
+{
+    pImpl->m_bHasEmbeddedObjects = bHasEmbeddedObjects;
 }
 
 bool SfxMedium::HasStorage_Impl() const

sfx2/source/doc/objstor.cxx
@@ -1239,7 +1239,9 @@ bool SfxObjectShell::SaveTo_Impl
                     //             if the url is not provided ( means the document is based on a stream ) this code is not
                     //             reachable.
                     rMedium.CloseAndRelease();
+                    rMedium.SetHasEmbeddedObjects(GetEmbeddedObjectContainer().HasEmbeddedObjects());
                     rMedium.GetOutputStorage();
+                    rMedium.SetHasEmbeddedObjects(false);
                 }
             }
             else if ( !bStorageBasedSource && !bStorageBasedTarget )

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -120,7 +120,29 @@
 #include <iodetect.hxx>
 #include <wrthtml.hxx>
 
-static char const DATA_DIRECTORY[] = "/sw/qa/extras/uiwriter/data/";
+namespace
+{
+char const DATA_DIRECTORY[] = "/sw/qa/extras/uiwriter/data/";
+
+int CountFilesInDirectory(const OUString &rURL)
+{
+    int nRet = 0;
+
+    osl::Directory aDir(rURL);
+    CPPUNIT_ASSERT_EQUAL(osl::FileBase::E_None, aDir.open());
+
+    osl::DirectoryItem aItem;
+    osl::FileStatus aFileStatus(osl_FileStatus_Mask_FileURL|osl_FileStatus_Mask_Type);
+    while (aDir.getNextItem(aItem) == osl::FileBase::E_None)
+    {
+        aItem.getFileStatus(aFileStatus);
+        if (aFileStatus.getFileType() != osl::FileStatus::Directory)
+            ++nRet;
+    }
+
+    return nRet;
+}
+}
 
 class SwUiWriterTest : public SwModelTestBase, public HtmlTestTools
 {
@@ -319,6 +341,7 @@ public:
     void testTdf116403();
     void testHtmlCopyImages();
     void testTdf116789();
+    void testTdf117225();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -511,6 +534,7 @@ public:
     CPPUNIT_TEST(testTdf116403);
     CPPUNIT_TEST(testHtmlCopyImages);
     CPPUNIT_TEST(testTdf116789);
+    CPPUNIT_TEST(testTdf117225);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -6189,6 +6213,23 @@ void SwUiWriterTest::testTdf116789()
     }
     // This failed, we got two different SwXCell for the same bookmark anchor text.
     CPPUNIT_ASSERT_EQUAL(xText1, xText2);
+}
+
+void SwUiWriterTest::testTdf117225()
+{
+    // Test that saving a document with an embedded object does not leak
+    // tempfiles in the directory of the target file.
+    OUString aTargetDirectory = m_directories.getURLFromWorkdir("/CppunitTest/sw_uiwriter.test.user/");
+    OUString aTargetFile = aTargetDirectory + "tdf117225.odt";
+    OUString aSourceFile = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf117225.odt";
+    osl::File::copy(aSourceFile, aTargetFile);
+    mxComponent = loadFromDesktop(aTargetFile);
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
+    int nExpected = CountFilesInDirectory(aTargetDirectory);
+    xStorable->store();
+    int nActual = CountFilesInDirectory(aTargetDirectory);
+    // nActual was nExpected + 1, i.e. we leaked a tempfile.
+    CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);