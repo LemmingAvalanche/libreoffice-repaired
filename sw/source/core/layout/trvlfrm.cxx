LemmingAvalanche/libreoffice-repaired

commit f2bcec06e4d1e3d3bae16b9181fa9331231e21ff

convert MT_ constants to scoped enum
Change-Id: I21e253ed9dccdab84be4838b5beee9385467ed37
Reviewed-on: https://gerrit.libreoffice.org/16068
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

sw/source/core/layout/trvlfrm.cxx
@@ -2145,11 +2145,11 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
             SwRect aTmp( aStRect );
 
             // BiDi-Portions are swimming against the current.
-            const bool bPorR2L = ( MT_BIDI == pSt2Pos->nMultiType ) ?
+            const bool bPorR2L = ( MultiPortionType::BIDI == pSt2Pos->nMultiType ) ?
                 ! bR2L :
             bR2L;
 
-            if( MT_BIDI == pSt2Pos->nMultiType &&
+            if( MultiPortionType::BIDI == pSt2Pos->nMultiType &&
                 (pSt2Pos->aPortion2.*fnRect->fnGetWidth)() )
             {
                 // nested bidi portion
@@ -2177,7 +2177,7 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
                         (pSt2Pos->aPortion.*fnRect->fnGetRight)() );
             }
 
-            if( MT_ROT_90 == pSt2Pos->nMultiType ||
+            if( MultiPortionType::ROT_90 == pSt2Pos->nMultiType ||
                 (pSt2Pos->aPortion.*fnRect->fnGetTop)() ==
                 (aTmp.*fnRect->fnGetTop)() )
             {
@@ -2189,7 +2189,7 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
             Sub( aRegion, aTmp );
 
             SwTwips nTmp = (pSt2Pos->aLine.*fnRect->fnGetBottom)();
-            if( MT_ROT_90 != pSt2Pos->nMultiType &&
+            if( MultiPortionType::ROT_90 != pSt2Pos->nMultiType &&
                 (aStRect.*fnRect->fnBottomDist)( nTmp ) > 0 )
             {
                 (aTmp.*fnRect->fnSetTop)( (aTmp.*fnRect->fnGetBottom)() );
@@ -2221,11 +2221,11 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
             SwRect aTmp( aEndRect );
 
             // BiDi-Portions are swimming against the current.
-            const bool bPorR2L = ( MT_BIDI == pEnd2Pos->nMultiType ) ?
+            const bool bPorR2L = ( MultiPortionType::BIDI == pEnd2Pos->nMultiType ) ?
                                        ! bEndR2L :
                                          bEndR2L;
 
-            if( MT_BIDI == pEnd2Pos->nMultiType &&
+            if( MultiPortionType::BIDI == pEnd2Pos->nMultiType &&
                 (pEnd2Pos->aPortion2.*fnRectX->fnGetWidth)() )
             {
                 // nested bidi portion
@@ -2253,7 +2253,7 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
                         (pEnd2Pos->aPortion.*fnRectX->fnGetLeft)() );
             }
 
-            if( MT_ROT_90 == pEnd2Pos->nMultiType ||
+            if( MultiPortionType::ROT_90 == pEnd2Pos->nMultiType ||
                 (pEnd2Pos->aPortion.*fnRectX->fnGetBottom)() ==
                 (aEndRect.*fnRectX->fnGetBottom)() )
             {
@@ -2265,7 +2265,7 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
             Sub( aRegion, aTmp );
 
             // The next statement means neither ruby nor rotate(90):
-            if( !( MT_RUBY & pEnd2Pos->nMultiType ) )
+            if( !( MultiPortionType::RUBY == pEnd2Pos->nMultiType ) )
             {
                 SwTwips nTmp = (pEnd2Pos->aLine.*fnRectX->fnGetTop)();
                 if( (aEndRect.*fnRectX->fnGetTop)() != nTmp )
@@ -2296,8 +2296,8 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
         }
     }
     else if( pSt2Pos && pEnd2Pos &&
-             MT_BIDI == pSt2Pos->nMultiType &&
-             MT_BIDI == pEnd2Pos->nMultiType &&
+             MultiPortionType::BIDI == pSt2Pos->nMultiType &&
+             MultiPortionType::BIDI == pEnd2Pos->nMultiType &&
              pSt2Pos->aPortion == pEnd2Pos->aPortion &&
              pSt2Pos->aPortion2 != pEnd2Pos->aPortion2 )
     {
@@ -2363,7 +2363,7 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
     if( pStartFrm == pEndFrm )
     {
         bool bSameRotatedOrBidi = pSt2Pos && pEnd2Pos &&
-            ( MT_BIDI & pSt2Pos->nMultiType ) &&
+            ( MultiPortionType::BIDI == pSt2Pos->nMultiType ) &&
             pSt2Pos->aPortion == pEnd2Pos->aPortion;
         //case 1: (Same frame and same row)
         if( bSameRotatedOrBidi ||

sw/inc/crstate.hxx
@@ -49,18 +49,21 @@ struct SwFillCrsrPos
 
 // Multiportion types: two lines, bidirectional, 270 degrees rotation,
 //                     ruby portion and 90 degrees rotation
-#define MT_TWOLINE  0
-#define MT_BIDI     1
-#define MT_ROT_270  3
-#define MT_RUBY     4
-#define MT_ROT_90   7
+enum class MultiPortionType : sal_uInt8
+{
+        TWOLINE  = 0,
+        BIDI     = 1,
+        ROT_270  = 3,
+        RUBY     = 4,
+        ROT_90   = 7,
+};
 
 struct Sw2LinesPos
 {
     SwRect aLine;           ///< Position and size of the line
     SwRect aPortion;        ///< Position and size of the multi portion
     SwRect aPortion2;       ///< needed for nested multi portions
-    sal_uInt8 nMultiType;        ///< Multiportion type
+    MultiPortionType nMultiType;  ///< Multiportion type
 };
 
 /**

sw/source/core/text/itrcrsr.cxx
@@ -677,16 +677,16 @@ void SwTextCursor::_GetCharRect( SwRect* pOrig, const sal_Int32 nOfst,
                             if( static_cast<SwMultiPortion*>(pPor)->HasRotation() )
                             {
                                 if( static_cast<SwMultiPortion*>(pPor)->IsRevers() )
-                                    pCMS->p2Lines->nMultiType = MT_ROT_270;
+                                    pCMS->p2Lines->nMultiType = MultiPortionType::ROT_270;
                                 else
-                                    pCMS->p2Lines->nMultiType = MT_ROT_90;
+                                    pCMS->p2Lines->nMultiType = MultiPortionType::ROT_90;
                             }
                             else if( static_cast<SwMultiPortion*>(pPor)->IsDouble() )
-                                pCMS->p2Lines->nMultiType = MT_TWOLINE;
+                                pCMS->p2Lines->nMultiType = MultiPortionType::TWOLINE;
                             else if( static_cast<SwMultiPortion*>(pPor)->IsBidi() )
-                                pCMS->p2Lines->nMultiType = MT_BIDI;
+                                pCMS->p2Lines->nMultiType = MultiPortionType::BIDI;
                             else
-                                pCMS->p2Lines->nMultiType = MT_RUBY;
+                                pCMS->p2Lines->nMultiType = MultiPortionType::RUBY;
 
                             SwTwips nTmpWidth = pPor->Width();
                             if( nSpaceAdd )

commit 094fe756801349c2d50374cc0b7dac9408c62d44

tdf#102907 selection indicator missing on vertical text
regression from

    commit f2bcec06e4d1e3d3bae16b9181fa9331231e21ff
    convert MT_ constants to scoped enum

The original code used bit-wise operations and some clever constant
values, which I did not convert correctly

Change-Id: I2fcd9e2584638d4a690773ef3f178415153b8b19
Reviewed-on: https://gerrit.libreoffice.org/55596
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/trvlfrm.cxx
@@ -2271,7 +2271,7 @@ void SwRootFrame::CalcFrameRects(SwShellCursor &rCursor)
             Sub( aRegion, aTmp );
 
             // The next statement means neither ruby nor rotate(90):
-            if( MultiPortionType::RUBY != pEnd2Pos->nMultiType )
+            if( MultiPortionType::RUBY != pEnd2Pos->nMultiType && MultiPortionType::ROT_90 != pEnd2Pos->nMultiType )
             {
                 SwTwips nTmp = fnRectX.GetTop(pEnd2Pos->aLine);
                 if( fnRectX.GetTop(aEndRect) != nTmp )
@@ -2366,7 +2366,9 @@ void SwRootFrame::CalcFrameRects(SwShellCursor &rCursor)
     if( pStartFrame == pEndFrame )
     {
         bool bSameRotatedOrBidi = pSt2Pos && pEnd2Pos &&
-            ( MultiPortionType::BIDI == pSt2Pos->nMultiType ) &&
+            ( MultiPortionType::BIDI == pSt2Pos->nMultiType  ||
+              MultiPortionType::ROT_270 == pSt2Pos->nMultiType ||
+              MultiPortionType::ROT_90  == pSt2Pos->nMultiType ) &&
             pSt2Pos->aPortion == pEnd2Pos->aPortion;
         //case 1: (Same frame and same row)
         if( bSameRotatedOrBidi ||