LemmingAvalanche/libreoffice-repaired

commit 3307224e9fed1af352344593ea7a91f4add35c57

loplugin:useuniqueptr in OTableSubscriptionDialog
Change-Id: If8fa9edc62f75f41de1392688a169458ce003bda
Reviewed-on: https://gerrit.libreoffice.org/54852
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/ui/dlg/TablesSingleDlg.cxx
@@ -47,10 +47,10 @@ OTableSubscriptionDialog::OTableSubscriptionDialog(vcl::Window* pParent
 {
     m_pImpl->setDataSourceOrName(_aDataSourceName);
     Reference< XPropertySet > xDatasource = m_pImpl->getCurrentDataSource();
-    m_pOutSet = new SfxItemSet( *_pItems );
+    m_pOutSet.reset(new SfxItemSet( *_pItems ));
 
     m_pImpl->translateProperties(xDatasource, *m_pOutSet);
-    SetInputSet(m_pOutSet);
+    SetInputSet(m_pOutSet.get());
 
     VclPtrInstance<OTableSubscriptionPage> pTabPage(get_content_area(), *m_pOutSet, this);
     pTabPage->SetServiceFactory(_rxORB);
@@ -64,7 +64,7 @@ OTableSubscriptionDialog::~OTableSubscriptionDialog()
 
 void OTableSubscriptionDialog::dispose()
 {
-    delete m_pOutSet;
+    m_pOutSet.reset();
     SfxSingleTabDialog::dispose();
 }
 
@@ -110,12 +110,12 @@ Reference< XPropertySet > const & OTableSubscriptionDialog::getCurrentDataSource
 
 const SfxItemSet* OTableSubscriptionDialog::getOutputSet() const
 {
-    return m_pOutSet;
+    return m_pOutSet.get();
 }
 
 SfxItemSet* OTableSubscriptionDialog::getWriteOutputSet()
 {
-    return m_pOutSet;
+    return m_pOutSet.get();
 }
 
 }   // namespace dbaui

dbaccess/source/ui/inc/TablesSingleDlg.hxx
@@ -44,7 +44,7 @@ class ODbDataSourceAdministrationHelper;
         std::unique_ptr<ODbDataSourceAdministrationHelper>  m_pImpl;
         bool m_bStopExecution; // set when the dialog should not be executed
 
-        SfxItemSet*             m_pOutSet;
+        std::unique_ptr<SfxItemSet> m_pOutSet;
     public:
 
         OTableSubscriptionDialog(vcl::Window* pParent

commit 1907e88056b5108948ce85aaac9e03dee17d9df2

tdf#118220 Base : Tools -> Table Filter crashes
regression from
    commit	3307224e9fed1af352344593ea7a91f4add35c57
    loplugin:useuniqueptr in OTableSubscriptionDialog

Change-Id: I9f29452736554b18dbc207c870ed1799fe38b6e1
Reviewed-on: https://gerrit.libreoffice.org/56110
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/ui/dlg/TablesSingleDlg.cxx
@@ -43,7 +43,6 @@ OTableSubscriptionDialog::OTableSubscriptionDialog(vcl::Window* pParent
         "dbaccess/ui/tablesfilterdialog.ui")
     , m_pImpl( new ODbDataSourceAdministrationHelper( _rxORB, pParent, this ) )
     , m_bStopExecution(false)
-    , m_pOutSet(_pItems)
 {
     m_pImpl->setDataSourceOrName(_aDataSourceName);
     Reference< XPropertySet > xDatasource = m_pImpl->getCurrentDataSource();