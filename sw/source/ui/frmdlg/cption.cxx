LemmingAvalanche/libreoffice-repaired

commit 6f361a4c185257aef185d6466ac884b87031d626

use std::vector::insert when inserting multiple of the same thing
instead of looping

Change-Id: I7b8c1663e303449302e0b96dc5b483367c2a52ba

sw/source/ui/frmdlg/cption.cxx
@@ -366,10 +366,8 @@ void SwCaptionDialog::DrawSample()
                                             RES_SETEXPFLD, sFieldTypeName ));
             if( pFieldType && pFieldType->GetOutlineLvl() < MAXLEVEL )
             {
-                sal_Int8 nLvl = pFieldType->GetOutlineLvl();
                 SwNumberTree::tNumberVector aNumVector;
-                for( sal_Int8 i = 0; i <= nLvl; ++i )
-                    aNumVector.push_back(1);
+                aNumVector.insert(aNumVector.end(), pFieldType->GetOutlineLvl(), 1);
 
                 OUString sNumber( rSh.GetOutlineNumRule()->
                                 MakeNumString(aNumVector, false ));

dbaccess/source/ui/querydesign/SelectionBrowseBox.cxx
@@ -140,8 +140,7 @@ OSelectionBrowseBox::OSelectionBrowseBox( vcl::Window* pParent )
     for (sal_Int32 nIdx = 0; nIdx < nCount; nIdx++)
         m_pOrderCell->InsertEntry(aTxt.getToken(nIdx, ';'));
 
-    for(long i=0;i < BROW_ROW_CNT;i++)
-        m_bVisibleRow.push_back(true);
+    m_bVisibleRow.insert(m_bVisibleRow.end(), BROW_ROW_CNT, true);
 
     m_bVisibleRow[BROW_FUNCTION_ROW] = false;   // first hide
 

dbaccess/source/ui/querydesign/TableFieldDescription.cxx
@@ -104,8 +104,7 @@ void OTableFieldDesc::SetCriteria( sal_uInt16 nIdx, const OUString& rCrit)
         m_aCriteria[nIdx] = rCrit;
     else
     {
-        for(sal_Int32 i=m_aCriteria.size();i<nIdx;++i)
-            m_aCriteria.push_back( OUString());
+        m_aCriteria.insert(m_aCriteria.end(), nIdx - m_aCriteria.size(), OUString());
         m_aCriteria.push_back(rCrit);
     }
 }

sc/source/filter/oox/worksheethelper.cxx
@@ -1248,8 +1248,7 @@ void WorksheetGlobals::convertOutlines( OutlineLevelVec& orLevels,
     if( nSize < nLevel )
     {
         // Outline level increased. Push the begin column position.
-        for( sal_Int32 nIndex = nSize; nIndex < nLevel; ++nIndex )
-            orLevels.push_back( nColRow );
+        orLevels.insert(orLevels.end(), nLevel - nSize, nColRow);
     }
     else if( nLevel < nSize )
     {

xmloff/source/chart/SchXMLExport.cxx
@@ -739,12 +739,13 @@ void lcl_ReorderInternalSequencesAccordingToTheirRangeName(
         if( aIt->first < 0 )
             continue;
         // fill empty columns
-        for( ; nIndex < aIt->first; ++nIndex )
-            rInOutSequences.push_back(
+        rInOutSequences.insert(
+                rInOutSequences.end(),
+                aIt->first - nIndex,
                 SchXMLExportHelper_Impl::tDataSequenceCont::value_type(
                     uno::Reference< chart2::data::XDataSequence >(),
                     uno::Reference< chart2::data::XDataSequence >() ));
-        OSL_ASSERT( nIndex == aIt->first );
+        nIndex = aIt->first;
         rInOutSequences.push_back( aIt->second );
     }
 }

commit 587ba5860073a318bee69a4095c69078f1f884db

tdf#118316: fix off-by-1 error (outline level is 0-based)
regression since commit 6f361a4c185257aef185d6466ac884b87031d626

Change-Id: I50627cde3a9a91189db61e19850768412b058064
Reviewed-on: https://gerrit.libreoffice.org/56295
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/ui/frmdlg/cption.cxx
@@ -376,7 +376,7 @@ void SwCaptionDialog::DrawSample()
             if( pFieldType && pFieldType->GetOutlineLvl() < MAXLEVEL )
             {
                 SwNumberTree::tNumberVector aNumVector;
-                aNumVector.insert(aNumVector.end(), pFieldType->GetOutlineLvl(), 1);
+                aNumVector.insert(aNumVector.end(), pFieldType->GetOutlineLvl() + 1, 1);
 
                 OUString sNumber( rSh.GetOutlineNumRule()->
                                 MakeNumString(aNumVector, false ));