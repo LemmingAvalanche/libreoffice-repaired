LemmingAvalanche/libreoffice-repaired

commit 62ea65c9ac7b7109925b45700629f3836440fdac

sw_redlinehide: fix SwCursorShell::GetContentAtPos() redline code
This calls SwTextFrame::GetCharRect() on the frame where the mouse
cursor is, with SwPositions that may be in a unrelated SwTextNode.

Bogus positions now triggers asserts, so let's fix it.

Change-Id: Idedb335ffd79c299976ad91b683dfb30cd0ceac2

sw/source/core/crsr/crstrvl.cxx
@@ -1518,11 +1518,17 @@ bool SwCursorShell::GetContentAtPos( const Point& rPt,
 
                         if( pFieldRect && nullptr != ( pFrame = pTextNd->getLayoutFrame( GetLayout(), &aPt ) ) )
                         {
+                            // not sure if this should be limited to one
+                            // paragraph, or mark the entire redline; let's
+                            // leave it limited to one for now...
+                            sal_Int32 nStart;
+                            sal_Int32 nEnd;
+                            pRedl->CalcStartEnd(pTextNd->GetIndex(), nStart, nEnd);
                             //get bounding box of range
                             SwRect aStart;
-                            pFrame->GetCharRect(aStart, *pRedl->Start(), &aTmpState);
+                            pFrame->GetCharRect(aStart, SwPosition(*pTextNd, nStart), &aTmpState);
                             SwRect aEnd;
-                            pFrame->GetCharRect(aEnd, *pRedl->End(), &aTmpState);
+                            pFrame->GetCharRect(aEnd, SwPosition(*pTextNd, nEnd), &aTmpState);
                             if (aStart.Top() != aEnd.Top() || aStart.Bottom() != aEnd.Bottom())
                             {
                                 aStart.Left(pFrame->getFrameArea().Left());

commit 9b8b19557efde0bd0100c2ccc1e658b7910e8da1

tdf#118219 sw: fix SwCursorShell::GetContentAtPos() redline crash
The problem is that CalcStartEnd() will not return the length of the
node, but COMPLETE_STRING if the redline ends in a subsequent node.

(regression from 62ea65c9ac7b7109925b45700629f3836440fdac)

Change-Id: Ic0a27f87b79f68417777842c611c2129fdc9b1f9
Reviewed-on: https://gerrit.libreoffice.org/56402
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

sw/source/core/crsr/crstrvl.cxx
@@ -1556,6 +1556,17 @@ bool SwCursorShell::GetContentAtPos( const Point& rPt,
                             sal_Int32 nStart;
                             sal_Int32 nEnd;
                             pRedl->CalcStartEnd(pTextNd->GetIndex(), nStart, nEnd);
+                            if (nStart == COMPLETE_STRING)
+                            {
+                                // consistency: found pRedl, so there must be
+                                // something in pTextNd
+                                assert(nEnd != COMPLETE_STRING);
+                                nStart = 0;
+                            }
+                            if (nEnd == COMPLETE_STRING)
+                            {
+                                nEnd = pTextNd->Len();
+                            }
                             //get bounding box of range
                             SwRect aStart;
                             pFrame->GetCharRect(aStart, SwPosition(*pTextNd, nStart), &aTmpState);