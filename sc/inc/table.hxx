LemmingAvalanche/libreoffice-repaired

commit 802c0f52662c07b4d820c0808929e80303b3aca3

Circular movement of cursor in a table with  hidden columns/rows, tdf#68290 related
If there is a table with range on the edges of the hidden columns/rows,
it allows you to quickly jump to the beginning/end of a neighboring
column using Enter/Shift+Enter

Change-Id: I5b18789b70089a6987ec7718998f5861261b22f8
Reviewed-on: https://gerrit.libreoffice.org/48691
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sc/source/ui/view/tabview3.cxx
@@ -1369,21 +1369,21 @@ void ScTabView::MoveCursorEnter( bool bShift )          // bShift -> up/down
             break;
     }
 
+    SCCOL nCurX;
+    SCROW nCurY;
+    aViewData.GetMoveCursor( nCurX,nCurY );
+    SCCOL nNewX = nCurX;
+    SCROW nNewY = nCurY;
+    SCTAB nTab  = aViewData.GetTabNo();
+
     ScMarkData& rMark = aViewData.GetMarkData();
+    ScDocument* pDoc  = aViewData.GetDocument();
+
     if (rMark.IsMarked() || rMark.IsMultiMarked())
     {
-        SCCOL nCurX;
-        SCROW nCurY;
-        aViewData.GetMoveCursor( nCurX,nCurY );
-        SCCOL nNewX = nCurX;
-        SCROW nNewY = nCurY;
-        SCTAB nTab = aViewData.GetTabNo();
+        pDoc->GetNextPos( nNewX, nNewY, nTab, nMoveX, nMoveY, true, false, rMark );
 
-        ScDocument* pDoc = aViewData.GetDocument();
-        pDoc->GetNextPos( nNewX,nNewY, nTab, nMoveX,nMoveY, true, false, rMark );
-
-        MoveCursorRel( nNewX-nCurX, nNewY-nCurY,
-                            SC_FOLLOW_LINE, false, true );
+        MoveCursorRel( nNewX - nCurX, nNewY - nCurY, SC_FOLLOW_LINE, false, true );
 
         //  update input line even if cursor was not moved
         if ( nNewX == nCurX && nNewY == nCurY )
@@ -1393,18 +1393,17 @@ void ScTabView::MoveCursorEnter( bool bShift )          // bShift -> up/down
     {
         if ( nMoveY != 0 && !nMoveX )
         {
+            pDoc->GetNextPos( nNewX, nNewY, nTab, nMoveX, nMoveY, true, false, rMark );
+
             // after Tab and Enter back to the starting column again
             SCCOL nTabCol = aViewData.GetTabStartCol();
             if (nTabCol != SC_TABSTART_NONE)
             {
-                SCCOL nCurX;
-                SCROW nCurY;
-                aViewData.GetMoveCursor( nCurX,nCurY );
-                nMoveX = nTabCol-nCurX;
+                nNewX = nTabCol;
             }
         }
 
-        MoveCursorRel( nMoveX,nMoveY, SC_FOLLOW_LINE, false );
+        MoveCursorRel( nNewX - nCurX, nNewY - nCurY, SC_FOLLOW_LINE, false, true );
     }
 }
 

commit 795e12d72a1ad4a8b828a9285f0d08a0bab577a7

Resolves: tdf#117458 let Enter move left/right again, tdf#68290 follow-up
Regression from

    commit 802c0f52662c07b4d820c0808929e80303b3aca3
    CommitDate: Tue Jan 30 23:12:04 2018 +0100

        Circular movement of cursor in a table with  hidden columns/rows, tdf#68290 related

where for nMoveX case nNewX - nCurX and nNewY - nCurY were both 0.

Also don't abuse the bMarked flag for GetNextPos() as bUnprotected
if nothing is marked, in which case also bKeepSel doesn't make
sense.

Change-Id: I59af09f1cfa397e68ce16d151ba7eee6e6598a75
Reviewed-on: https://gerrit.libreoffice.org/56935
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/inc/table.hxx
@@ -589,7 +589,7 @@ public:
                                 bool bMarked, bool bUnprotected, const ScMarkData& rMark ) const;
 
     bool        SkipRow( const SCCOL rCol, SCROW& rRow, const SCROW nMovY, const ScMarkData& rMark,
-                         const bool bUp, const SCROW nUsedY, const bool bSheetProtected ) const;
+                         const bool bUp, const SCROW nUsedY, const bool bMarked, const bool bSheetProtected ) const;
     void        LimitChartArea( SCCOL& rStartCol, SCROW& rStartRow, SCCOL& rEndCol, SCROW& rEndRow ) const;
 
     bool        HasData( SCCOL nCol, SCROW nRow ) const;

sc/source/core/data/table1.cxx
@@ -1325,7 +1325,8 @@ bool ScTable::ValidNextPos( SCCOL nCol, SCROW nRow, const ScMarkData& rMark,
 
 // Skips the current cell if it is Hidden, Overlapped or Protected and Sheet is Protected
 bool ScTable::SkipRow( const SCCOL nCol, SCROW& rRow, const SCROW nMovY,
-        const ScMarkData& rMark, const bool bUp, const SCROW nUsedY, const bool bSheetProtected ) const
+        const ScMarkData& rMark, const bool bUp, const SCROW nUsedY,
+        const bool bMarked, const bool bSheetProtected ) const
 {
     if ( !ValidRow( rRow ))
         return false;
@@ -1337,7 +1338,8 @@ bool ScTable::SkipRow( const SCCOL nCol, SCROW& rRow, const SCROW nMovY,
         else
             rRow += nMovY;
 
-        rRow  = rMark.GetNextMarked( nCol, rRow, bUp );
+        if (bMarked)
+            rRow  = rMark.GetNextMarked( nCol, rRow, bUp );
 
         return true;
     }
@@ -1349,7 +1351,8 @@ bool ScTable::SkipRow( const SCCOL nCol, SCROW& rRow, const SCROW nMovY,
         if ( bRowHidden || bOverlapped )
         {
             rRow += nMovY;
-            rRow  = rMark.GetNextMarked( nCol, rRow, bUp );
+            if (bMarked)
+                rRow = rMark.GetNextMarked( nCol, rRow, bUp );
 
             return true;
         }
@@ -1376,16 +1379,17 @@ void ScTable::GetNextPos( SCCOL& rCol, SCROW& rRow, SCCOL nMovX, SCROW nMovY,
     OSL_ENSURE( !nMovY || !bUnprotected,
                 "GetNextPos with bUnprotected horizontal not implemented" );
 
-    if ( nMovY && bMarked )
+    if ( nMovY && (bMarked || bUnprotected))
     {
         bool  bUp    = ( nMovY < 0 );
         SCROW nUsedY = nRow;
         SCCOL nUsedX = nCol;
 
-        nRow = rMark.GetNextMarked( nCol, nRow, bUp );
+        if (bMarked)
+            nRow = rMark.GetNextMarked( nCol, nRow, bUp );
         pDocument->GetPrintArea( nTab, nUsedX, nUsedY );
 
-        while ( SkipRow( nCol, nRow, nMovY, rMark, bUp, nUsedY, bSheetProtected ))
+        while ( SkipRow( nCol, nRow, nMovY, rMark, bUp, nUsedY, bMarked, bSheetProtected ))
             ;
 
         while ( nRow < 0 || nRow > MAXROW )
@@ -1413,9 +1417,10 @@ void ScTable::GetNextPos( SCCOL& rCol, SCROW& rRow, SCCOL nMovX, SCROW nMovY,
             else if (nRow > MAXROW)
                 nRow = 0;
 
-            nRow = rMark.GetNextMarked( nCol, nRow, bUp );
+            if (bMarked)
+                nRow = rMark.GetNextMarked( nCol, nRow, bUp );
 
-            while ( SkipRow( nCol, nRow, nMovY, rMark, bUp, nUsedY, bSheetProtected ))
+            while ( SkipRow( nCol, nRow, nMovY, rMark, bUp, nUsedY, bMarked, bSheetProtected ))
                 ;
         }
     }

sc/source/ui/view/tabview3.cxx
@@ -1388,10 +1388,10 @@ void ScTabView::MoveCursorEnter( bool bShift )          // bShift -> up/down
     }
     else
     {
+        pDoc->GetNextPos( nNewX, nNewY, nTab, nMoveX, nMoveY, false, true, rMark );
+
         if ( nMoveY != 0 && !nMoveX )
         {
-            pDoc->GetNextPos( nNewX, nNewY, nTab, nMoveX, nMoveY, true, false, rMark );
-
             // after Tab and Enter back to the starting column again
             SCCOL nTabCol = aViewData.GetTabStartCol();
             if (nTabCol != SC_TABSTART_NONE)
@@ -1400,7 +1400,7 @@ void ScTabView::MoveCursorEnter( bool bShift )          // bShift -> up/down
             }
         }
 
-        MoveCursorRel( nNewX - nCurX, nNewY - nCurY, SC_FOLLOW_LINE, false, true );
+        MoveCursorRel( nNewX - nCurX, nNewY - nCurY, SC_FOLLOW_LINE, false);
     }
 }
 