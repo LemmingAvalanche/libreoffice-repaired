LibreOffice/core

tdf#101856 Backport parsing of bookmark properties
Change-Id: I7654aa93d4d86a5d36201832ac3609b9f4c30e03

include/xmloff/txtparae.hxx
@@ -201,6 +201,8 @@ protected:
     const OUString sTextFieldStart;
     const OUString sTextFieldEnd;
     const OUString sTextFieldStartEnd;
+    const OUString sBookmarkHidden;
+    const OUString sBookmarkCondition;
 
     SinglePropertySetInfoCache aCharStyleNamesPropInfoCache;
 

xmloff/source/text/txtparae.cxx
@@ -1250,6 +1250,8 @@ XMLTextParagraphExport::XMLTextParagraphExport(
     sTextFieldStart( "TextFieldStart"  ),
     sTextFieldEnd( "TextFieldEnd"  ),
     sTextFieldStartEnd( "TextFieldStartEnd"  ),
+    sBookmarkHidden("BookmarkHidden"),
+    sBookmarkCondition("BookmarkCondition"),
     aCharStyleNamesPropInfoCache( sCharStyleNames )
 {
     rtl::Reference < XMLPropertySetMapper > xPropMapper(new XMLTextPropertySetMapper( TextPropMap::PARA, true ));

LemmingAvalanche/libreoffice-repaired

Reviewed-on: https://gerrit.libreoffice.org/50565
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
(cherry picked from commit 88b6b1a8c149163d226eb3295930e3407bcb8b5b)
Reviewed-on: https://gerrit.libreoffice.org/56958
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

include/xmloff/txtparae.hxx
@@ -204,6 +204,8 @@ private:
     const OUString sTextFieldStart;
     const OUString sTextFieldEnd;
     const OUString sTextFieldStartEnd;
+    const OUString m_sBookmarkHidden;
+    const OUString m_sBookmarkCondition;
 
 protected:
     const OUString sFrameStyleName;

xmloff/source/text/txtparae.cxx
@@ -1248,6 +1248,8 @@ XMLTextParagraphExport::XMLTextParagraphExport(
     sTextFieldStart( "TextFieldStart"  ),
     sTextFieldEnd( "TextFieldEnd"  ),
     sTextFieldStartEnd( "TextFieldStartEnd"  ),
+    m_sBookmarkHidden("BookmarkHidden"),
+    m_sBookmarkCondition("BookmarkCondition"),
     sFrameStyleName("FrameStyleName"),
     aCharStyleNamesPropInfoCache( sCharStyleNames )
 {