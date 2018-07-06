LemmingAvalanche/libreoffice-repaired

commit 15cb18dbdfeaff761bea719af167204f6b25623c

sw_redlinehide: fix the fieldmark toxic waste in GetNextAttr()
Out of bounds indexes returned here trigger assertions in new
mapping functions.

Why would you set it to p+1 anyway?

Change-Id: I024e1ab6f40b5545c2e9f71f63620be57fba31d2

sw/source/core/text/itratr.cxx
@@ -629,7 +629,9 @@ static sal_Int32 GetNextAttrImpl(SwTextNode const*const pTextNode,
         size_t const nStartIndex, size_t const nEndIndex,
         sal_Int32 const nPosition)
 {
-    sal_Int32 nNext = COMPLETE_STRING;
+    // note: this used to be COMPLETE_STRING, but was set to Len() + 1 below,
+    // which is rather silly, so set it to Len() instead
+    sal_Int32 nNext = pTextNode->Len();
     if (SwpHints const*const pHints = pTextNode->GetpSwpHints())
     {
         // are there attribute starts left?
@@ -654,29 +656,27 @@ static sal_Int32 GetNextAttrImpl(SwTextNode const*const pTextNode,
             }
         }
     }
-    if (pTextNode != nullptr)
+    // TODO: maybe use hints like FieldHints for this instead of looking at the text...
+    const sal_Int32 l = std::min(nNext, pTextNode->Len());
+    sal_Int32 p = nPosition;
+    const sal_Unicode* pStr = pTextNode->GetText().getStr();
+    while (p < l)
     {
-        // TODO: maybe use hints like FieldHints for this instead of looking at the text...
-        const sal_Int32 l = std::min(nNext, pTextNode->Len());
-        sal_Int32 p = nPosition;
-        const sal_Unicode* aStr = pTextNode->GetText().getStr();
-        while (p<l)
+        sal_Unicode aChar = pStr[p];
+        if (aChar < CH_TXT_ATR_FORMELEMENT
+            || aChar > CH_TXT_ATR_FIELDEND)
         {
-            sal_Unicode aChar = aStr[p];
-            if (aChar < CH_TXT_ATR_FORMELEMENT
-                || aChar > CH_TXT_ATR_FIELDEND)
-            {
-                ++p;
-            }
-            else
-            {
-                break;
-            }
+            ++p;
         }
-        if ((p < l && nPosition < p) || nNext <= p)
-        nNext=p;
         else
-        nNext=p+1;
+        {
+            break;
+        }
+    }
+    assert(p <= nNext);
+    if (p < l && nPosition < p)
+    {
+        nNext=p;
     }
     return nNext;
 }

commit 8f8b5a672f518c83c2f6dd7f4d0ef9eb75697720

tdf#118414 sw: fix fieldmarks in GetNextAttr()
Apparently this needs to stop both before and after the CH_TXT_ATR_*.

(regression from 15cb18dbdfeaff761bea719af167204f6b25623c)

Change-Id: Iff3d594d2eec59436ad683b9cc7aca04f7527bd5
Reviewed-on: https://gerrit.libreoffice.org/57017
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

sw/source/core/text/itratr.cxx
@@ -674,9 +674,11 @@ static sal_Int32 GetNextAttrImpl(SwTextNode const*const pTextNode,
         }
     }
     assert(p <= nNext);
-    if (p < l && nPosition < p)
+    if (p < l)
     {
-        nNext=p;
+        // found a CH_TXT_ATR_FIELD*: if it's same as current position,
+        // skip behind it so that both before- and after-positions are returned
+        nNext = (nPosition < p) ? p : p + 1;
     }
     return nNext;
 }