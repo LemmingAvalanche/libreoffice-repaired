LemmingAvalanche/libreoffice-repaired

commit 3a648a2d69da330f436d24abbea5599a4ad8ba99

Clean up uses of Any::getValue() in sw
Change-Id: Ifaa239ab3e285e8cf998339456ece5ce99008af9

sw/source/uibase/uno/unotxdoc.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <o3tl/any.hxx>
 #include <osl/mutex.hxx>
 #include <vcl/image.hxx>
 #include <vcl/virdev.hxx>
@@ -1140,8 +1143,12 @@ void SwXTextDocument::setPagePrintSettings(const Sequence< beans::PropertyValue 
             }
             else if(sName == "IsLandscape")
             {
-                bException =  (cppu::UnoType<bool>::get() != rVal.getValueType());
-                aData.SetLandscape(*static_cast<sal_Bool const *>(rVal.getValue()));
+                auto b = o3tl::tryAccess<bool>(rVal);
+                bException = bool(b);
+                if (b)
+                {
+                    aData.SetLandscape(*b);
+                }
             }
             else
                 bException = true;
@@ -1197,9 +1204,9 @@ void SwXTextDocument::printPages(const Sequence< beans::PropertyValue >& xOption
             // Collate-Property
             else if ( rProp.Name == UNO_NAME_COLLATE )
             {
-                if ( rProp.Value.getValueType() == cppu::UnoType<bool>::get())
+                if ( auto b = o3tl::tryAccess<bool>(rProp.Value) )
 
-                    aReq.AppendItem(SfxBoolItem( SID_PRINT_COLLATE, *static_cast<sal_Bool const *>(rProp.Value.getValue()) ) );
+                    aReq.AppendItem(SfxBoolItem( SID_PRINT_COLLATE, *b ) );
                 else
                     throw IllegalArgumentException();
             }
@@ -1207,8 +1214,8 @@ void SwXTextDocument::printPages(const Sequence< beans::PropertyValue >& xOption
             // Sort-Property
             else if ( rProp.Name == UNO_NAME_SORT )
             {
-                if ( rProp.Value.getValueType() == cppu::UnoType<bool>::get() )
-                    aReq.AppendItem(SfxBoolItem( SID_PRINT_SORT, *static_cast<sal_Bool const *>(rProp.Value.getValue()) ) );
+                if ( auto b = o3tl::tryAccess<bool>(rProp.Value) )
+                    aReq.AppendItem(SfxBoolItem( SID_PRINT_SORT, *b ) );
                 else
                     throw IllegalArgumentException();
             }
@@ -1835,7 +1842,7 @@ void SwXTextDocument::setPropertyValue(const OUString& rPropertyName, const Any&
         case WID_DOC_CHANGES_RECORD:
         case WID_DOC_CHANGES_SHOW:
         {
-            bool bSet = *static_cast<sal_Bool const *>(aValue.getValue());
+            bool bSet = *o3tl::doAccess<bool>(aValue);
             sal_uInt16 eMode = pDocShell->GetDoc()->getIDocumentRedlineAccess().GetRedlineMode();
             if(WID_DOC_CHANGES_SHOW == pEntry->nWID)
             {
@@ -1875,7 +1882,7 @@ void SwXTextDocument::setPropertyValue(const OUString& rPropertyName, const Any&
         }
         break;
         case WID_DOC_HIDE_TIPS :
-            SW_MOD()->GetModuleConfig()->SetHideFieldTips(*static_cast<sal_Bool const *>(aValue.getValue()));
+            SW_MOD()->GetModuleConfig()->SetHideFieldTips(*o3tl::doAccess<bool>(aValue));
         break;
         case WID_DOC_REDLINE_DISPLAY:
         {
@@ -1908,7 +1915,7 @@ void SwXTextDocument::setPropertyValue(const OUString& rPropertyName, const Any&
         case WID_DOC_AUTOMATIC_CONTROL_FOCUS:
         {
             SwDrawModel * pDrawDoc;
-            bool bAuto = *static_cast<sal_Bool const *>(aValue.getValue());
+            bool bAuto = *o3tl::doAccess<bool>(aValue);
 
             if ( nullptr != ( pDrawDoc = pDocShell->GetDoc()->getIDocumentDrawModelAccess().GetDrawModel() ) )
                 pDrawDoc->SetAutoControlFocus( bAuto );
@@ -1928,7 +1935,7 @@ void SwXTextDocument::setPropertyValue(const OUString& rPropertyName, const Any&
         case WID_DOC_APPLY_FORM_DESIGN_MODE:
         {
             SwDrawModel * pDrawDoc;
-            bool bMode = *static_cast<sal_Bool const *>(aValue.getValue());
+            bool bMode = *o3tl::doAccess<bool>(aValue);
 
             if ( nullptr != ( pDrawDoc = pDocShell->GetDoc()->getIDocumentDrawModelAccess().GetDrawModel() ) )
                 pDrawDoc->SetOpenInDesignMode( bMode );

sw/source/core/edit/edlingu.cxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/text/XFlatParagraph.hpp>
 #include <com/sun/star/i18n/ScriptType.hpp>
 #include <comphelper/string.hxx>
+#include <o3tl/any.hxx>
 
 #include <unoflatpara.hxx>
 
@@ -412,8 +413,8 @@ uno::Any SwConvIter::Continue( sal_uInt16* pPageCnt, sal_uInt16* pPageSt )
 bool SwHyphIter::IsAuto()
 {
     uno::Reference< beans::XPropertySet >  xProp( ::GetLinguPropertySet() );
-    return xProp.is() && *static_cast<sal_Bool const *>(xProp->getPropertyValue(
-                                UPN_IS_HYPH_AUTO ).getValue());
+    return xProp.is() && *o3tl::doAccess<bool>(xProp->getPropertyValue(
+                                UPN_IS_HYPH_AUTO ));
 }
 
 void SwHyphIter::ShowSelection()

sw/source/core/fields/authfld.cxx
@@ -21,6 +21,7 @@
 #include <comphelper/string.hxx>
 #include <editeng/unolingu.hxx>
 #include <editeng/langitem.hxx>
+#include <o3tl/any.hxx>
 #include <o3tl/make_unique.hxx>
 #include <swtypes.hxx>
 #include <tools/resid.hxx>
@@ -413,10 +414,10 @@ bool    SwAuthorityFieldType::PutValue( const Any& rAny, sal_uInt16 nWhichId )
         break;
     }
     case FIELD_PROP_BOOL1:
-        m_bIsSequence = *static_cast<sal_Bool const *>(rAny.getValue());
+        m_bIsSequence = *o3tl::doAccess<bool>(rAny);
         break;
     case FIELD_PROP_BOOL2:
-        m_bSortByDocument = *static_cast<sal_Bool const *>(rAny.getValue());
+        m_bSortByDocument = *o3tl::doAccess<bool>(rAny);
         break;
 
     case FIELD_PROP_LOCALE:
@@ -450,7 +451,7 @@ bool    SwAuthorityFieldType::PutValue( const Any& rAny, sal_uInt16 nWhichId )
                         }
                         else if(pValue[j].Name == UNO_NAME_IS_SORT_ASCENDING)
                         {
-                            aSortKey.bSortAscending = *static_cast<sal_Bool const *>(pValue[j].Value.getValue());
+                            aSortKey.bSortAscending = *o3tl::doAccess<bool>(pValue[j].Value);
                         }
                     }
                     m_SortKeyArr.push_back(aSortKey);

sw/source/core/fields/dbfld.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <float.h>
+#include <o3tl/any.hxx>
 #include <sfx2/app.hxx>
 #include <svl/zforlist.hxx>
 #include <svx/pageitem.hxx>
@@ -391,7 +392,7 @@ bool SwDBField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
     switch( nWhichId )
     {
     case FIELD_PROP_BOOL1:
-        if( *static_cast<sal_Bool const *>(rAny.getValue()) )
+        if( *o3tl::doAccess<bool>(rAny) )
             SetSubType(GetSubType()&~nsSwExtendedSubType::SUB_OWN_FMT);
         else
             SetSubType(GetSubType()|nsSwExtendedSubType::SUB_OWN_FMT);

sw/source/core/fields/ddefld.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <o3tl/any.hxx>
 #include <osl/thread.h>
 #include <sfx2/linkmgr.hxx>
 #include <doc.hxx>
@@ -341,7 +344,7 @@ bool SwDDEFieldType::PutValue( const uno::Any& rVal, sal_uInt16 nWhichId )
     case FIELD_PROP_PAR4:      nPart = 1; break;
     case FIELD_PROP_SUBTYPE:   nPart = 0; break;
     case FIELD_PROP_BOOL1:
-        SetType( *static_cast<sal_Bool const *>(rVal.getValue()) ?
+        SetType( *o3tl::doAccess<bool>(rVal) ?
                  SfxLinkUpdateMode::ALWAYS :
                  SfxLinkUpdateMode::ONCALL );
         break;

sw/source/core/fields/docufld.cxx
@@ -41,6 +41,7 @@
 #include <com/sun/star/document/XDocumentProperties.hpp>
 #include <com/sun/star/util/Date.hpp>
 #include <com/sun/star/util/Duration.hpp>
+#include <o3tl/any.hxx>
 #include <unotools/localedatawrapper.hxx>
 #include <editeng/unolingu.hxx>
 #include <comphelper/processfactory.hxx>
@@ -385,11 +386,11 @@ bool SwAuthorField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
     switch( nWhichId )
     {
     case FIELD_PROP_BOOL1:
-        SetFormat( *static_cast<sal_Bool const *>(rAny.getValue()) ? AF_NAME : AF_SHORTCUT );
+        SetFormat( *o3tl::doAccess<bool>(rAny) ? AF_NAME : AF_SHORTCUT );
         break;
 
     case FIELD_PROP_BOOL2:
-        if( *static_cast<sal_Bool const *>(rAny.getValue()) )
+        if( *o3tl::doAccess<bool>(rAny) )
             SetFormat( GetFormat() | AF_FIXED);
         else
             SetFormat( GetFormat() & ~AF_FIXED);
@@ -562,7 +563,7 @@ bool SwFileNameField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         break;
 
     case FIELD_PROP_BOOL2:
-        if( *static_cast<sal_Bool const *>(rAny.getValue()) )
+        if( *o3tl::doAccess<bool>(rAny) )
             SetFormat( GetFormat() | FF_FIXED);
         else
             SetFormat( GetFormat() & ~FF_FIXED);
@@ -1233,7 +1234,7 @@ bool SwDocInfoField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         break;
 
     case FIELD_PROP_BOOL1:
-        if(*static_cast<sal_Bool const *>(rAny.getValue()))
+        if(*o3tl::doAccess<bool>(rAny))
             nSubType |= DI_SUB_FIXED;
         else
             nSubType &= ~DI_SUB_FIXED;
@@ -1251,7 +1252,7 @@ bool SwDocInfoField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         break;
     case FIELD_PROP_BOOL2:
         nSubType &= 0xf0ff;
-        if(*static_cast<sal_Bool const *>(rAny.getValue()))
+        if(*o3tl::doAccess<bool>(rAny))
             nSubType |= DI_SUB_DATE;
         else
             nSubType |= DI_SUB_TIME;
@@ -1510,7 +1511,7 @@ bool SwHiddenTextField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         rAny >>= aFALSEText;
         break;
     case FIELD_PROP_BOOL1:
-        bIsHidden = *static_cast<sal_Bool const *>(rAny.getValue());
+        bIsHidden = *o3tl::doAccess<bool>(rAny);
         break;
     case FIELD_PROP_PAR4:
         rAny >>= aContent;
@@ -1609,7 +1610,7 @@ bool SwHiddenParaField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         rAny >>= aCond;
         break;
     case FIELD_PROP_BOOL1:
-        bIsHidden = *static_cast<sal_Bool const *>(rAny.getValue());
+        bIsHidden = *o3tl::doAccess<bool>(rAny);
         break;
 
     default:
@@ -1815,10 +1816,9 @@ bool SwPostItField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         OSL_FAIL("Not implemented!");
         break;
     case FIELD_PROP_DATE:
-        if( rAny.getValueType() == ::cppu::UnoType<util::Date>::get() )
+        if( auto aSetDate = o3tl::tryAccess<util::Date>(rAny) )
         {
-            util::Date aSetDate = *static_cast<util::Date const *>(rAny.getValue());
-            aDateTime = Date(aSetDate.Day, aSetDate.Month, aSetDate.Year);
+            aDateTime = Date(aSetDate->Day, aSetDate->Month, aSetDate->Year);
         }
         break;
     case FIELD_PROP_DATE_TIME:
@@ -1970,7 +1970,7 @@ bool SwExtUserField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         }
         break;
     case FIELD_PROP_BOOL1:
-        if( *static_cast<sal_Bool const *>(rAny.getValue()) )
+        if( *o3tl::doAccess<bool>(rAny) )
             SetFormat(GetFormat() | AF_FIXED);
         else
             SetFormat(GetFormat() & ~AF_FIXED);
@@ -2047,7 +2047,7 @@ bool SwRefPageSetField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
     switch( nWhichId )
     {
     case FIELD_PROP_BOOL1:
-        bOn = *static_cast<sal_Bool const *>(rAny.getValue());
+        bOn = *o3tl::doAccess<bool>(rAny);
         break;
     case FIELD_PROP_USHORT1:
         rAny >>=nOffset;

sw/source/core/fields/expfld.cxx
@@ -21,6 +21,7 @@
 
 #include <UndoTable.hxx>
 #include <hintids.hxx>
+#include <o3tl/any.hxx>
 #include <unotools/collatorwrapper.hxx>
 #include <unotools/charclass.hxx>
 #include <editeng/unolingu.hxx>
@@ -443,7 +444,7 @@ bool SwGetExpField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
     switch( nWhichId )
     {
     case FIELD_PROP_DOUBLE:
-        SwValueField::SetValue(*static_cast<double const *>(rAny.getValue()));
+        SwValueField::SetValue(*o3tl::doAccess<double>(rAny));
         break;
     case FIELD_PROP_FORMAT:
         rAny >>= nTmp;
@@ -466,7 +467,7 @@ bool SwGetExpField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
             SetSubType( static_cast<sal_uInt16>((GetSubType() & 0xff00) | nTmp));
         break;
     case FIELD_PROP_BOOL2:
-        if(*static_cast<sal_Bool const *>(rAny.getValue()))
+        if(*o3tl::doAccess<bool>(rAny))
             nSubType |= nsSwExtendedSubType::SUB_CMD;
         else
             nSubType &= (~nsSwExtendedSubType::SUB_CMD);
@@ -973,7 +974,7 @@ bool SwSetExpField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
     switch( nWhichId )
     {
     case FIELD_PROP_BOOL2:
-        if(*static_cast<sal_Bool const *>(rAny.getValue()))
+        if(*o3tl::doAccess<bool>(rAny))
             nSubType &= ~nsSwExtendedSubType::SUB_INVISIBLE;
         else
             nSubType |= nsSwExtendedSubType::SUB_INVISIBLE;
@@ -1031,13 +1032,13 @@ bool SwSetExpField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         rAny >>= aPText;
         break;
     case FIELD_PROP_BOOL3:
-        if(*static_cast<sal_Bool const *>(rAny.getValue()))
+        if(*o3tl::doAccess<bool>(rAny))
             nSubType |= nsSwExtendedSubType::SUB_CMD;
         else
             nSubType &= (~nsSwExtendedSubType::SUB_CMD);
         break;
     case FIELD_PROP_BOOL1:
-        SetInputFlag(*static_cast<sal_Bool const *>(rAny.getValue()));
+        SetInputFlag(*o3tl::doAccess<bool>(rAny));
         break;
     case FIELD_PROP_PAR4:
         {

sw/source/core/fields/flddat.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <o3tl/any.hxx>
 #include <tools/datetime.hxx>
 #include <svl/zforlist.hxx>
 #include <com/sun/star/util/DateTime.hpp>
@@ -188,14 +191,14 @@ bool SwDateTimeField::PutValue( const uno::Any& rVal, sal_uInt16 nWhichId )
     switch( nWhichId )
     {
     case FIELD_PROP_BOOL1:
-        if(*static_cast<sal_Bool const *>(rVal.getValue()))
+        if(*o3tl::doAccess<bool>(rVal))
             nSubType |= FIXEDFLD;
         else
             nSubType &= ~FIXEDFLD;
         break;
     case FIELD_PROP_BOOL2:
         nSubType &=  ~(DATEFLD|TIMEFLD);
-        nSubType |= *static_cast<sal_Bool const *>(rVal.getValue()) ? DATEFLD : TIMEFLD;
+        nSubType |= *o3tl::doAccess<bool>(rVal) ? DATEFLD : TIMEFLD;
         break;
     case FIELD_PROP_FORMAT:
         rVal >>= nTmp;

commit b84d4873d9bf890a4f1bcb9bd26fcbca39123f22

Make setPagePrintSettings() accept IsLandscape again
Regression from 3a648a2d69da330f436d24abbea5599a4ad8ba99 - invert
the check for boost::optional validity.

Change-Id: If4e041e1fe349c1fcb2c74b2e5780bf57300486f
Reviewed-on: https://gerrit.libreoffice.org/57309
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

sw/source/uibase/uno/unotxdoc.cxx
@@ -1147,7 +1147,7 @@ void SwXTextDocument::setPagePrintSettings(const Sequence< beans::PropertyValue 
         else if(sName == "IsLandscape")
         {
             auto b = o3tl::tryAccess<bool>(rVal);
-            bException = bool(b);
+            bException = !b;
             if (b)
             {
                 aData.SetLandscape(*b);