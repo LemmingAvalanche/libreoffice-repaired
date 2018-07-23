LemmingAvalanche/libreoffice-repaired

commit b0ce6e65ef58f9866a436bb44c0e3b5d261cd471

pass SvStream around by std::unique_ptr
and give utl::OStreamWrapper a new constructor so that it knows it is
taking ownership of  the SvStream, which appears to fix several leaks

Change-Id: Idcbcca9b81a4f0345fd8b8c8a2f4e84213686a6b
Reviewed-on: https://gerrit.libreoffice.org/57187
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/doc/docfile.cxx
@@ -260,8 +260,8 @@ public:
     std::shared_ptr<const SfxFilter> m_pFilter;
     std::shared_ptr<const SfxFilter> m_pCustomFilter;
 
-    SvStream* m_pInStream;
-    SvStream* m_pOutStream;
+    std::unique_ptr<SvStream> m_pInStream;
+    std::unique_ptr<SvStream> m_pOutStream;
 
     std::shared_ptr<const SfxFilter> pOrigFilter;
     OUString    aOrigURL;
@@ -536,11 +536,11 @@ bool SfxMedium::IsSkipImages()
 SvStream* SfxMedium::GetInStream()
 {
     if ( pImpl->m_pInStream )
-        return pImpl->m_pInStream;
+        return pImpl->m_pInStream.get();
 
     if ( pImpl->pTempFile )
     {
-        pImpl->m_pInStream = new SvFileStream(pImpl->m_aName, pImpl->m_nStorOpenMode);
+        pImpl->m_pInStream.reset( new SvFileStream(pImpl->m_aName, pImpl->m_nStorOpenMode) );
 
         pImpl->m_eError = pImpl->m_pInStream->GetError();
 
@@ -548,11 +548,10 @@ SvStream* SfxMedium::GetInStream()
                     && ! pImpl->m_pInStream->IsWritable() )
         {
             pImpl->m_eError = ERRCODE_IO_ACCESSDENIED;
-            delete pImpl->m_pInStream;
-            pImpl->m_pInStream = nullptr;
+            pImpl->m_pInStream.reset();
         }
         else
-            return pImpl->m_pInStream;
+            return pImpl->m_pInStream.get();
     }
 
     GetMedium_Impl();
@@ -560,7 +559,7 @@ SvStream* SfxMedium::GetInStream()
     if ( GetError() )
         return nullptr;
 
-    return pImpl->m_pInStream;
+    return pImpl->m_pInStream.get();
 }
 
 
@@ -586,7 +585,7 @@ void SfxMedium::CloseInStream_Impl()
         return;
     }
 
-    DELETEZ( pImpl->m_pInStream );
+    pImpl->m_pInStream.reset();
     if ( pImpl->m_pSet )
         pImpl->m_pSet->ClearItem( SID_INPUTSTREAM );
 
@@ -628,14 +627,14 @@ SvStream* SfxMedium::GetOutStream()
             {
             // On Unix don't try to re-use XOutStream from xStream if that exists;
             // it causes fdo#59022 (fails opening files via SMB on Linux)
-                pImpl->m_pOutStream = new SvFileStream(
-                            pImpl->m_aName, StreamMode::STD_READWRITE);
+                pImpl->m_pOutStream.reset( new SvFileStream(
+                            pImpl->m_aName, StreamMode::STD_READWRITE) );
             }
             CloseStorage();
         }
     }
 
-    return pImpl->m_pOutStream;
+    return pImpl->m_pOutStream.get();
 }
 
 
@@ -659,8 +658,7 @@ void SfxMedium::CloseOutStream_Impl()
                 CloseStorage();
         }
 
-        delete pImpl->m_pOutStream;
-        pImpl->m_pOutStream = nullptr;
+        pImpl->m_pOutStream.reset();
     }
 
     if ( !pImpl->m_pInStream )
@@ -752,7 +750,7 @@ bool SfxMedium::IsStorage()
     }
     else if ( GetInStream() )
     {
-        pImpl->bIsStorage = SotStorage::IsStorageFile( pImpl->m_pInStream ) && !SotStorage::IsOLEStorage( pImpl->m_pInStream );
+        pImpl->bIsStorage = SotStorage::IsStorageFile( pImpl->m_pInStream.get() ) && !SotStorage::IsOLEStorage( pImpl->m_pInStream.get() );
         if ( !pImpl->m_pInStream->GetError() && !pImpl->bIsStorage )
             pImpl->m_bTriedStorage = true;
     }

commit 088ca6c7bb112814eb009f72c39d95ec8ad91455

crashtesting: ooo72057-1.doc
fallout from
    commit b0ce6e65ef58f986
    pass SvStream around by std::unique_ptr

The backtrace where it was was freeing the SvStream we need is below,
the code seems dodgy because it is re-creating the underlying SvStream. Before
b0ce6e65ef58f986, it just leaked the SvStream.

0  SvStream::~SvStream()
    at tools/source/stream/stream.cxx:369
1  SvFileStream::~SvFileStream()
    at tools/source/stream/strmunx.cxx:300
2  SvFileStream::~SvFileStream()
    at tools/source/stream/strmunx.cxx:298
3  std::default_delete<SvStream>::operator()(SvStream*) const
4  std::unique_ptr<SvStream, std::default_delete<SvStream> >::reset(SvStream*)
5  std::unique_ptr<SvStream, std::default_delete<SvStream> >::operator=(std::unique_ptr<SvStream, std::default_delete<SvStream> >&&)
6  SfxMedium::GetMedium_Impl()
     at sfx2/source/doc/docfile.cxx:2603
7  SfxMedium::GetInputStream()
    at sfx2/source/doc/docfile.cxx:3329
8  ImportExcel8::ReadBasic()
    at sc/source/filter/excel/excimp8.cxx:391
9  ImportExcel8::PostDocLoad()
    at sc/source/filter/excel/excimp8.cxx:425
10 ImportExcel8::Read() (this=0x4727a10)
    at sc/source/filter/excel/read.cxx:1298
11 ScFormatFilterPluginImpl::ScImportExcel(SfxMedium&, ScDocument*, EXCIMPFORMAT)
    at sc/source/filter/excel/excel.cxx:142

Change-Id: I4fb769a7da71ef9a27ddef1581448efb3d303bde
Reviewed-on: https://gerrit.libreoffice.org/57843
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sfx2/source/doc/docfile.cxx
@@ -2597,7 +2597,7 @@ void SfxMedium::GetMedium_Impl()
         if ( !GetError() && !pImpl->xStream.is() && !pImpl->xInputStream.is() )
             SetError(ERRCODE_IO_ACCESSDENIED);
 
-        if ( !GetError() )
+        if ( !GetError() && !pImpl->m_pInStream )
         {
             if ( pImpl->xStream.is() )
                 pImpl->m_pInStream = utl::UcbStreamHelper::CreateStream( pImpl->xStream );