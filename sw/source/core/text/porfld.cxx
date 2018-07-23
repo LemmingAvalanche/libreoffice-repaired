LemmingAvalanche/libreoffice-repaired

commit 4de0dcb5f521782142e466836bf19971072cb387

convert (a>b?a:b) to std::max(a,b)
with something like:
   git grep -nP '(.*)\s*>\s*(.*)\s*\?\s*\g1\s*:\s*\g2'

Change-Id: I60b9a3a2a09162bc0de4c13fdde2c209696e5413
Reviewed-on: https://gerrit.libreoffice.org/47602
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/text/porfld.cxx
@@ -828,7 +828,7 @@ bool SwGrfNumPortion::Format( SwTextFormatInfo &rInf )
     const bool bFull = rInf.Width() < rInf.X() + Width();
     const bool bFly = rInf.GetFly() ||
         ( rInf.GetLast() && rInf.GetLast()->IsFlyPortion() );
-    SetAscent( static_cast<sal_uInt16>(GetRelPos() > 0 ? GetRelPos() : 0) );
+    SetAscent( std::max<sal_uInt16>(GetRelPos(), 0) );
     if( GetAscent() > Height() )
         Height( GetAscent() );
 

sw/source/uibase/uiview/viewtab.cxx
@@ -1703,8 +1703,8 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                 const int nRgt = (bTableVertical ? nPageHeight : nPageWidth) -
                                  (aTabCols.GetLeftMin() + aTabCols.GetRight());
 
-                const sal_uInt16 nL = static_cast< sal_uInt16 >(nLft > 0 ? nLft : 0);
-                const sal_uInt16 nR = static_cast< sal_uInt16 >(nRgt > 0 ? nRgt : 0);
+                const sal_uInt16 nL = std::max< sal_uInt16 >(nLft, 0);
+                const sal_uInt16 nR = std::max< sal_uInt16 >(nRgt, 0);
 
                 SvxColumnItem aColItem(nNum, nL, nR);
 
@@ -1940,8 +1940,8 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                 const int nRgt = (bVerticalWriting ? nPageWidth : nPageHeight) -
                                  (aTabCols.GetLeftMin() + aTabCols.GetRight());
 
-                const sal_uInt16 nL = static_cast< sal_uInt16 >(nLft > 0 ? nLft : 0);
-                const sal_uInt16 nR = static_cast< sal_uInt16 >(nRgt > 0 ? nRgt : 0);
+                const sal_uInt16 nL = std::max< sal_uInt16 >(nLft, 0);
+                const sal_uInt16 nR = std::max< sal_uInt16 >(nRgt, 0);
 
                 SvxColumnItem aColItem(0, nL, nR);
 
@@ -2014,8 +2014,8 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                     const int nLft = aTabCols.GetLeftMin() + aTabCols.GetLeft();
                     const int nRgt = nPageWidth -(aTabCols.GetLeftMin() + aTabCols.GetRight());
 
-                    const sal_uInt16 nL = static_cast< sal_uInt16 >(nLft > 0 ? nLft : 0);
-                    const sal_uInt16 nR = static_cast< sal_uInt16 >(nRgt > 0 ? nRgt : 0);
+                    const sal_uInt16 nL = std::max< sal_uInt16 >(nLft, 0);
+                    const sal_uInt16 nR = std::max< sal_uInt16 >(nRgt, 0);
 
                     aRectangle.Left() = nL;
                     if(nNum > 1)

avmedia/source/vlc/vlcframegrabber.cxx
@@ -81,7 +81,7 @@ VLCFrameGrabber::VLCFrameGrabber( wrapper::EventHandler& eh, const rtl::OUString
             return ::uno::Reference< css::graphic::XGraphic >();
         }
 
-        mPlayer.setTime( ( fMediaTime > 0 ? fMediaTime : 0 ) * MSEC_IN_SEC );
+        mPlayer.setTime( std::max(fMediaTime, 0) * MSEC_IN_SEC );
         mPlayer.pause();
 
         condition.wait(std::chrono::seconds(2));

basctl/source/basicide/baside2.cxx
@@ -817,7 +817,7 @@ sal_Int32 ModulWindow::FormatAndPrint( Printer* pPrinter, sal_Int32 nPrintPage )
     sal_Int32 nLinespPage = aPaperSz.Height()/nLineHeight;
     long nXTextWidth = pPrinter->approximate_char_width();
 
-    sal_Int32 nCharspLine = aPaperSz.Width() / (nXTextWidth > 1 ? nXTextWidth : 1);
+    sal_Int32 nCharspLine = aPaperSz.Width() / std::max<long>(nXTextWidth, 1);
     const sal_uInt32 nParas = GetEditEngine()->GetParagraphCount();
 
     sal_Int32 nPages = nParas/nLinespPage+1;

chart2/source/controller/dialogs/DataBrowser.cxx
@@ -297,7 +297,7 @@ void SeriesHeader::SetSeriesName( const OUString & rName )
 void SeriesHeader::SetRange( sal_Int32 nStartCol, sal_Int32 nEndCol )
 {
     m_nStartCol = nStartCol;
-    m_nEndCol = (nEndCol > nStartCol) ? nEndCol : nStartCol;
+    m_nEndCol = std::max(nEndCol, nStartCol);
     m_spSeriesName->setStartColumn( nStartCol );
 }
 

chart2/source/view/charttypes/GL3DBarChart.cxx
@@ -1479,7 +1479,7 @@ void GL3DBarChart::processAutoFly(sal_uInt32 nId, sal_uInt32 nColor)
         maRenderEvent = EVENT_AUTO_FLY;
         mnSelectBarId = nColorRate > mnColorRate ? nId : mnSelectBarId;
         mnPreSelectBarId = mnSelectBarId;
-        mnColorRate = nColorRate > mnColorRate ? nColorRate : mnColorRate;
+        mnColorRate = std::max(nColorRate, mnColorRate);
     }
 }
 

chart2/source/view/charttypes/Splines.cxx
@@ -420,7 +420,7 @@ bool createParameterT(const tPointVecType& rUniquePoints, double* t)
         dx = rUniquePoints[i].first - rUniquePoints[i-1].first;
         dy = rUniquePoints[i].second - rUniquePoints[i-1].second;
         // scaling to avoid underflow or overflow
-        fDiffMax = (fabs(dx)>fabs(dy)) ? fabs(dx) : fabs(dy);
+        fDiffMax = std::max(fabs(dx), fabs(dy));
         if (fDiffMax == 0.0)
         {
             bIsSuccessful = false;
@@ -446,7 +446,7 @@ bool createParameterT(const tPointVecType& rUniquePoints, double* t)
             {
                 dx = rUniquePoints[i].first - rUniquePoints[i-1].first;
                 dy = rUniquePoints[i].second - rUniquePoints[i-1].second;
-                fDiffMax = (fabs(dx)>fabs(dy)) ? fabs(dx) : fabs(dy);
+                fDiffMax = std::max(fabs(dx), fabs(dy));
                 // same as above, so should not be zero
                 dx /= fDiffMax;
                 dy /= fDiffMax;

connectivity/source/drivers/calc/CTable.cxx
@@ -83,8 +83,8 @@ static void lcl_UpdateArea( const Reference<XCellRange>& xUsedRange, sal_Int32& 
         const CellRangeAddress* pData = aAddresses.getConstArray();
         for ( sal_Int32 i=0; i<nCount; i++ )
         {
-            rEndCol = pData[i].EndColumn > rEndCol ? pData[i].EndColumn : rEndCol;
-            rEndRow = pData[i].EndRow    > rEndRow ? pData[i].EndRow    : rEndRow;
+            rEndCol = std::max(pData[i].EndColumn, rEndCol);
+            rEndRow = std::max(pData[i].EndRow, rEndRow);
         }
     }
 }

commit ab941f3f44a1f190f3693a226c0a414138e04cae

tdf#118716 Inner border of an embedded table can't be dragged around
regression from
    commit 4de0dcb5f521782142e466836bf19971072cb387
    convert (a>b?a:b) to std::max(a,b)

In this code
   int a;
   std::max<sal_uInt16>(a,0)
std::max will first convert a and b to sal_uInt16, therefore potentially
converting a negative number to a positive number due to the conversion
rules. Then it will take the larger number.

While this code
    int a;
    static_cast<sal_uInt16>(a > 0 ? a : 0)
will compare first, and then convert the larger number to sal_uInt16,
which might result in making a "a" that is larger than 2^16 into a
smaller value, but which will never convert a negative "a" value into a
positive value.

Change-Id: Id4f6df0089b1a74ea4ac1c5d05655e7c1d8f0c7f
Reviewed-on: https://gerrit.libreoffice.org/57846
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/text/porfld.cxx
@@ -828,7 +828,7 @@ bool SwGrfNumPortion::Format( SwTextFormatInfo &rInf )
     const bool bFull = rInf.Width() < rInf.X() + Width();
     const bool bFly = rInf.GetFly() ||
         ( rInf.GetLast() && rInf.GetLast()->IsFlyPortion() );
-    SetAscent( std::max<sal_uInt16>(GetRelPos(), 0) );
+    SetAscent( GetRelPos() > 0 ? GetRelPos() : 0 );
     if( GetAscent() > Height() )
         Height( GetAscent() );
 

sw/source/uibase/uiview/viewtab.cxx
@@ -1700,8 +1700,8 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                 const int nRgt = (bTableVertical ? nPageHeight : nPageWidth) -
                                  (aTabCols.GetLeftMin() + aTabCols.GetRight());
 
-                const sal_uInt16 nL = std::max< sal_uInt16 >(nLft, 0);
-                const sal_uInt16 nR = std::max< sal_uInt16 >(nRgt, 0);
+                const sal_uInt16 nL = static_cast< sal_uInt16 >(std::max(nLft, 0));
+                const sal_uInt16 nR = static_cast< sal_uInt16 >(std::max(nRgt, 0));
 
                 SvxColumnItem aColItem(nNum, nL, nR);
 
@@ -1937,8 +1937,8 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                 const int nRgt = (bVerticalWriting ? nPageWidth : nPageHeight) -
                                  (aTabCols.GetLeftMin() + aTabCols.GetRight());
 
-                const sal_uInt16 nL = std::max< sal_uInt16 >(nLft, 0);
-                const sal_uInt16 nR = std::max< sal_uInt16 >(nRgt, 0);
+                const sal_uInt16 nL = static_cast< sal_uInt16 >(std::max(nLft, 0));
+                const sal_uInt16 nR = static_cast< sal_uInt16 >(std::max(nRgt, 0));
 
                 SvxColumnItem aColItem(0, nL, nR);
 
@@ -2011,8 +2011,8 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                     const int nLft = aTabCols.GetLeftMin() + aTabCols.GetLeft();
                     const int nRgt = nPageWidth -(aTabCols.GetLeftMin() + aTabCols.GetRight());
 
-                    const sal_uInt16 nL = std::max< sal_uInt16 >(nLft, 0);
-                    const sal_uInt16 nR = std::max< sal_uInt16 >(nRgt, 0);
+                    const sal_uInt16 nL = static_cast< sal_uInt16 >(std::max(nLft, 0));
+                    const sal_uInt16 nR = static_cast< sal_uInt16 >(std::max(nRgt, 0));
 
                     aRectangle.SetLeft( nL );
                     if(nNum > 1)