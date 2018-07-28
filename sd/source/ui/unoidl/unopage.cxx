LemmingAvalanche/libreoffice-repaired

commit 5a7378dec2506fd789591d54c2fa4868e54a1281

convert SvxDateFormat to scoped enum
And simplify the code in sd/headerfooterdlg, no
need for the complex encoding of date and time when we
can just index into a static array.

Change-Id: I677400bf1f956c81eba665b71dbda9183b63f55c
Reviewed-on: https://gerrit.libreoffice.org/42346
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/unoidl/unopage.cxx
@@ -922,7 +922,8 @@ void SAL_CALL SdGenericDrawPage::setPropertyValue( const OUString& aPropertyName
                 if( ! ( aValue >>= nValue ) )
                     throw lang::IllegalArgumentException();
 
-                aHeaderFooterSettings.meDateTimeFormat = nValue;
+                aHeaderFooterSettings.meDateFormat = static_cast<SvxDateFormat>(nValue & 0x0f);
+                aHeaderFooterSettings.meTimeFormat = static_cast<SvxTimeFormat>((nValue >> 4) & 0x0f);
                 break;
             }
             }
@@ -1289,7 +1290,11 @@ Any SAL_CALL SdGenericDrawPage::getPropertyValue( const OUString& PropertyName )
         }
         break;
     case WID_PAGE_DATETIMEFORMAT:
-        aAny <<= (sal_Int32)GetPage()->getHeaderFooterSettings().meDateTimeFormat;
+        {
+            auto const & rSettings = GetPage()->getHeaderFooterSettings();
+            sal_Int32 x = static_cast<sal_Int32>(rSettings.meDateFormat) & (static_cast<sal_Int32>(rSettings.meTimeFormat) << 4);
+            aAny <<= x;
+        }
         break;
 
     case WID_TRANSITION_TYPE:

editeng/source/items/flditem.cxx
@@ -71,7 +71,8 @@ SvxFieldData* SvxFieldData::Create(const uno::Reference<text::XTextContent>& xTe
                         SvxDateField* pData = new SvxDateField(aDate, bIsFixed ? SvxDateType::Fix : SvxDateType::Var);
                         sal_Int32 nNumFmt = -1;
                         xPropSet->getPropertyValue(UNO_TC_PROP_NUMFORMAT) >>= nNumFmt;
-                        if (nNumFmt >= SVXDATEFORMAT_APPDEFAULT && nNumFmt <= SVXDATEFORMAT_F)
+                        if (static_cast<SvxDateFormat>(nNumFmt) >= SvxDateFormat::AppDefault &&
+                            static_cast<SvxDateFormat>(nNumFmt) <= SvxDateFormat::F)
                             pData->SetFormat(static_cast<SvxDateFormat>(nNumFmt));
 
                         return pData;
@@ -314,7 +315,7 @@ SvxDateField::SvxDateField()
 {
     nFixDate = Date( Date::SYSTEM ).GetDate();
     eType = SvxDateType::Var;
-    eFormat = SVXDATEFORMAT_STDSMALL;
+    eFormat = SvxDateFormat::StdSmall;
 }
 
 
@@ -358,50 +359,50 @@ OUString SvxDateField::GetFormatted( SvNumberFormatter& rFormatter, LanguageType
 
 OUString SvxDateField::GetFormatted( Date const & aDate, SvxDateFormat eFormat, SvNumberFormatter& rFormatter, LanguageType eLang )
 {
-    if ( eFormat == SVXDATEFORMAT_SYSTEM )
+    if ( eFormat == SvxDateFormat::System )
     {
-        OSL_FAIL( "SVXDATEFORMAT_SYSTEM not implemented!" );
-        eFormat = SVXDATEFORMAT_STDSMALL;
+        OSL_FAIL( "SvxDateFormat::System not implemented!" );
+        eFormat = SvxDateFormat::StdSmall;
     }
-    else if ( eFormat == SVXDATEFORMAT_APPDEFAULT )
+    else if ( eFormat == SvxDateFormat::AppDefault )
     {
-        OSL_FAIL( "SVXDATEFORMAT_APPDEFAULT: take them from where? ");
-        eFormat = SVXDATEFORMAT_STDSMALL;
+        OSL_FAIL( "SvxDateFormat::AppDefault: take them from where? ");
+        eFormat = SvxDateFormat::StdSmall;
     }
 
     sal_uInt32 nFormatKey;
 
     switch( eFormat )
     {
-        case SVXDATEFORMAT_STDSMALL:
+        case SvxDateFormat::StdSmall:
             // short
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYSTEM_SHORT, eLang );
         break;
-        case SVXDATEFORMAT_STDBIG:
+        case SvxDateFormat::StdBig:
             // long
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYSTEM_LONG, eLang );
         break;
-        case SVXDATEFORMAT_A:
+        case SvxDateFormat::A:
             // 13.02.96
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYS_DDMMYY, eLang );
         break;
-        case SVXDATEFORMAT_B:
+        case SvxDateFormat::B:
             // 13.02.1996
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYS_DDMMYYYY, eLang );
         break;
-        case SVXDATEFORMAT_C:
+        case SvxDateFormat::C:
             // 13. Feb 1996
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYS_DMMMYYYY, eLang );
         break;
-        case SVXDATEFORMAT_D:
+        case SvxDateFormat::D:
             // 13. February 1996
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYS_DMMMMYYYY, eLang );
         break;
-        case SVXDATEFORMAT_E:
+        case SvxDateFormat::E:
             // The, 13. February 1996
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYS_NNDMMMMYYYY, eLang );
         break;
-        case SVXDATEFORMAT_F:
+        case SvxDateFormat::F:
             // Tuesday, 13. February 1996
             nFormatKey = rFormatter.GetFormatIndex( NF_DATE_SYS_NNNNDMMMMYYYY, eLang );
         break;
@@ -925,18 +926,16 @@ bool SvxDateTimeField::operator==( const SvxFieldData& rCmp ) const
 SvxDateTimeField::SvxDateTimeField() {}
 
 OUString SvxDateTimeField::GetFormatted(
-    Date const & rDate, tools::Time const & rTime, int eFormat, SvNumberFormatter& rFormatter, LanguageType eLanguage )
+    Date const & rDate, tools::Time const & rTime,
+    SvxDateFormat eDateFormat, SvxTimeFormat eTimeFormat,
+    SvNumberFormatter& rFormatter, LanguageType eLanguage )
 {
     OUString aRet;
 
-    SvxDateFormat eDateFormat = (SvxDateFormat)(eFormat & 0x0f);
-
-    if(eDateFormat)
+    if(eDateFormat != SvxDateFormat::AppDefault)
     {
         aRet = SvxDateField::GetFormatted( rDate, eDateFormat, rFormatter, eLanguage );
     }
-
-    SvxTimeFormat eTimeFormat = (SvxTimeFormat)((eFormat >> 4) & 0x0f);
 
     if(eTimeFormat != SvxTimeFormat::AppDefault)
     {

editeng/source/uno/unofield.cxx
@@ -251,7 +251,7 @@ SvxUnoTextField::SvxUnoTextField( sal_Int32 nServiceId ) throw()
     {
     case text::textfield::Type::DATE:
         mpImpl->mbBoolean2 = true;
-        mpImpl->mnInt32 = SVXDATEFORMAT_STDSMALL;
+        mpImpl->mnInt32 = static_cast<sal_Int32>(SvxDateFormat::StdSmall);
         mpImpl->mbBoolean1 = false;
         break;
 
@@ -319,7 +319,7 @@ SvxUnoTextField::SvxUnoTextField( uno::Reference< text::XTextRange > const & xAn
                     mpImpl->maDateTime = getDate( bFixed ?
                                             static_cast<const SvxDateField*>(pData)->GetFixDate() :
                                             Date( Date::SYSTEM ).GetDate() );
-                    mpImpl->mnInt32 = static_cast<const SvxDateField*>(pData)->GetFormat();
+                    mpImpl->mnInt32 = static_cast<sal_Int32>(static_cast<const SvxDateField*>(pData)->GetFormat());
                     mpImpl->mbBoolean1 = bFixed;
                 }
                 break;
@@ -392,7 +392,8 @@ SvxFieldData* SvxUnoTextField::CreateFieldData() const throw()
         {
             Date aDate( setDate( mpImpl->maDateTime ) );
             pData = new SvxDateField( aDate, mpImpl->mbBoolean1?SvxDateType::Fix:SvxDateType::Var );
-            if( mpImpl->mnInt32 >= SVXDATEFORMAT_APPDEFAULT && mpImpl->mnInt32 <= SVXDATEFORMAT_F )
+            if( mpImpl->mnInt32 >= static_cast<sal_Int32>(SvxDateFormat::AppDefault) &&
+                mpImpl->mnInt32 <= static_cast<sal_Int32>(SvxDateFormat::F) )
                 static_cast<SvxDateField*>(pData)->SetFormat( (SvxDateFormat)mpImpl->mnInt32 );
         }
         else

filter/source/msfilter/svdfppt.cxx
@@ -6409,34 +6409,34 @@ PPTFieldEntry::~PPTFieldEntry()
 
 void PPTFieldEntry::GetDateTime( const sal_uInt32 nVal, SvxDateFormat& eDateFormat, SvxTimeFormat& eTimeFormat )
 {
-    eDateFormat = SVXDATEFORMAT_APPDEFAULT;
+    eDateFormat = SvxDateFormat::AppDefault;
     eTimeFormat = SvxTimeFormat::AppDefault;
     // evaluate ID
     switch( nVal )
     {
         case 0:
         case 6:
-            eDateFormat = SVXDATEFORMAT_A;
+            eDateFormat = SvxDateFormat::A;
         break;
         case 1:
-            eDateFormat = SVXDATEFORMAT_F;
+            eDateFormat = SvxDateFormat::F;
         break;
         case 2:
         case 3:
-            eDateFormat = SVXDATEFORMAT_D;
+            eDateFormat = SvxDateFormat::D;
         break;
         case 4:
         case 5:
-            eDateFormat = SVXDATEFORMAT_C;
+            eDateFormat = SvxDateFormat::C;
         break;
         case 7:
-            eDateFormat = SVXDATEFORMAT_A;
+            eDateFormat = SvxDateFormat::A;
             SAL_FALLTHROUGH;
         case 9:
             eTimeFormat = SvxTimeFormat::HH24_MM;
         break;
         case 8:
-            eDateFormat = SVXDATEFORMAT_A;
+            eDateFormat = SvxDateFormat::A;
             SAL_FALLTHROUGH;
         case 11:
             eTimeFormat = SvxTimeFormat::HH12_MM;
@@ -6455,7 +6455,7 @@ void PPTFieldEntry::SetDateTime( sal_uInt32 nVal )
     SvxDateFormat eDateFormat;
     SvxTimeFormat eTimeFormat;
     GetDateTime( nVal, eDateFormat, eTimeFormat );
-    if ( eDateFormat != SVXDATEFORMAT_APPDEFAULT )
+    if ( eDateFormat != SvxDateFormat::AppDefault )
         xField1.reset(new SvxFieldItem(SvxDateField( Date( Date::SYSTEM ), SvxDateType::Var, eDateFormat ), EE_FEATURE_FIELD));
     if ( eTimeFormat != SvxTimeFormat::AppDefault )
     {

filter/source/svg/svgexport.cxx
@@ -221,29 +221,29 @@ public:
         VariableTextField::elementExport( pSVGExport );
         OUString sDateFormat, sTimeFormat;
         SvxDateFormat eDateFormat = (SvxDateFormat)( format & 0x0f );
-        if( eDateFormat )
+        if( eDateFormat != SvxDateFormat::AppDefault )
         {
             switch( eDateFormat )
             {
-                case SVXDATEFORMAT_STDSMALL:
-                case SVXDATEFORMAT_A:       // 13.02.96
+                case SvxDateFormat::StdSmall:
+                case SvxDateFormat::A:       // 13.02.96
                     sDateFormat.clear();
                     break;
-                case SVXDATEFORMAT_C:       // 13.Feb 1996
+                case SvxDateFormat::C:       // 13.Feb 1996
                     sDateFormat.clear();
                     break;
-                case SVXDATEFORMAT_D:       // 13.February 1996
+                case SvxDateFormat::D:       // 13.February 1996
                     sDateFormat.clear();
                     break;
-                case SVXDATEFORMAT_E:       // Tue, 13.February 1996
+                case SvxDateFormat::E:       // Tue, 13.February 1996
                     sDateFormat.clear();
                     break;
-                case SVXDATEFORMAT_STDBIG:
-                case SVXDATEFORMAT_F:       // Tuesday, 13.February 1996
+                case SvxDateFormat::StdBig:
+                case SvxDateFormat::F:       // Tuesday, 13.February 1996
                     sDateFormat.clear();
                     break;
                 // default case
-                case SVXDATEFORMAT_B:      // 13.02.1996
+                case SvxDateFormat::B:      // 13.02.1996
                 default:
                     sDateFormat.clear();
                     break;
@@ -1359,7 +1359,7 @@ void SVGFilter::implGetPagePropSet( const Reference< XDrawPage > & rxPage )
     mVisiblePagePropSet.bIsFooterFieldVisible               = true;
     mVisiblePagePropSet.bIsDateTimeFieldVisible             = true;
     mVisiblePagePropSet.bIsDateTimeFieldFixed               = true;
-    mVisiblePagePropSet.nDateTimeFormat                     = SVXDATEFORMAT_B;
+    mVisiblePagePropSet.nDateTimeFormat                     = SvxDateFormat::B;
     mVisiblePagePropSet.nPageNumberingType                  = css::style::NumberingType::ARABIC;
 
     //  We collect info on master page elements visibility, and placeholder text shape content.
@@ -1377,7 +1377,9 @@ void SVGFilter::implGetPagePropSet( const Reference< XDrawPage > & rxPage )
             implSafeGetPagePropSet( "IsDateTimeVisible", xPropSet, xPropSetInfo )            >>= mVisiblePagePropSet.bIsDateTimeFieldVisible;
 
             implSafeGetPagePropSet( "IsDateTimeFixed", xPropSet, xPropSetInfo )              >>= mVisiblePagePropSet.bIsDateTimeFieldFixed;
-            implSafeGetPagePropSet( "DateTimeFormat", xPropSet, xPropSetInfo )               >>= mVisiblePagePropSet.nDateTimeFormat;
+            sal_Int32 nTmp;
+            if (implSafeGetPagePropSet( "DateTimeFormat", xPropSet, xPropSetInfo ) >>= nTmp)
+                mVisiblePagePropSet.nDateTimeFormat = static_cast<SvxDateFormat>(nTmp);
 
             if( mVisiblePagePropSet.bIsPageNumberFieldVisible )
             {
@@ -2161,7 +2163,7 @@ IMPL_LINK( SVGFilter, CalcFieldHdl, EditFieldInfo*, pInfo, void )
                 }
                 if( bHasCharSetMap && ( pCharSetMap->find( aVariableDateTimeId ) != pCharSetMap->end() ) && !(*pCharSetMap)[ aVariableDateTimeId ].empty() )
                 {
-                    SvxDateFormat eDateFormat = SVXDATEFORMAT_B, eCurDateFormat;
+                    SvxDateFormat eDateFormat = SvxDateFormat::B, eCurDateFormat;
                     const UCharSet & aCharSet = (*pCharSetMap)[ aVariableDateTimeId ];
                     UCharSet::const_iterator aChar = aCharSet.begin();
                     // we look for the most verbose date format
@@ -2170,29 +2172,29 @@ IMPL_LINK( SVGFilter, CalcFieldHdl, EditFieldInfo*, pInfo, void )
                         eCurDateFormat = (SvxDateFormat)( (int)( *aChar ) & 0x0f );
                         switch( eDateFormat )
                         {
-                            case SVXDATEFORMAT_STDSMALL:
-                            case SVXDATEFORMAT_A:       // 13.02.96
-                            case SVXDATEFORMAT_B:       // 13.02.1996
+                            case SvxDateFormat::StdSmall:
+                            case SvxDateFormat::A:       // 13.02.96
+                            case SvxDateFormat::B:       // 13.02.1996
                                 switch( eCurDateFormat )
                                 {
-                                    case SVXDATEFORMAT_C:       // 13.Feb 1996
-                                    case SVXDATEFORMAT_D:       // 13.February 1996
-                                    case SVXDATEFORMAT_E:       // Tue, 13.February 1996
-                                    case SVXDATEFORMAT_STDBIG:
-                                    case SVXDATEFORMAT_F:       // Tuesday, 13.February 1996
+                                    case SvxDateFormat::C:       // 13.Feb 1996
+                                    case SvxDateFormat::D:       // 13.February 1996
+                                    case SvxDateFormat::E:       // Tue, 13.February 1996
+                                    case SvxDateFormat::StdBig:
+                                    case SvxDateFormat::F:       // Tuesday, 13.February 1996
                                         eDateFormat = eCurDateFormat;
                                         break;
                                     default:
                                         break;
                                 }
                                 break;
-                            case SVXDATEFORMAT_C:       // 13.Feb 1996
-                            case SVXDATEFORMAT_D:       // 13.February 1996
+                            case SvxDateFormat::C:       // 13.Feb 1996
+                            case SvxDateFormat::D:       // 13.February 1996
                                 switch( eCurDateFormat )
                                 {
-                                    case SVXDATEFORMAT_E:       // Tue, 13.February 1996
-                                    case SVXDATEFORMAT_STDBIG:
-                                    case SVXDATEFORMAT_F:       // Tuesday, 13.February 1996
+                                    case SvxDateFormat::E:       // Tue, 13.February 1996
+                                    case SvxDateFormat::StdBig:
+                                    case SvxDateFormat::F:       // Tuesday, 13.February 1996
                                         eDateFormat = eCurDateFormat;
                                         break;
                                     default:
@@ -2207,28 +2209,28 @@ IMPL_LINK( SVGFilter, CalcFieldHdl, EditFieldInfo*, pInfo, void )
                     // They should be enough to cover every time format.
                     aRepresentation += "0123456789.:/-APM";
 
-                    if( eDateFormat )
+                    if( eDateFormat != SvxDateFormat::AppDefault )
                     {
                         OUString sDate;
                         LanguageType eLang = pInfo->GetOutliner()->GetLanguage( pInfo->GetPara(), pInfo->GetPos() );
                         SvNumberFormatter * pNumberFormatter = new SvNumberFormatter( ::comphelper::getProcessComponentContext(), LANGUAGE_SYSTEM );
-                        // We always collect the characters obtained by using the SVXDATEFORMAT_B (as: 13.02.1996)
+                        // We always collect the characters obtained by using the SvxDateFormat::B (as: 13.02.1996)
                         // so we are sure to include any unusual day|month|year separator.
                         Date aDate( 1, 1, 1996 );
-                        sDate += SvxDateField::GetFormatted( aDate, SVXDATEFORMAT_B, *pNumberFormatter, eLang );
+                        sDate += SvxDateField::GetFormatted( aDate, SvxDateFormat::B, *pNumberFormatter, eLang );
                         switch( eDateFormat )
                         {
-                            case SVXDATEFORMAT_E:       // Tue, 13.February 1996
-                            case SVXDATEFORMAT_STDBIG:
-                            case SVXDATEFORMAT_F:       // Tuesday, 13.February 1996
+                            case SvxDateFormat::E:       // Tue, 13.February 1996
+                            case SvxDateFormat::StdBig:
+                            case SvxDateFormat::F:       // Tuesday, 13.February 1996
                                 for( sal_uInt16 i = 1; i <= 7; ++i )  // we get all days in a week
                                 {
                                     aDate.SetDay( i );
                                     sDate += SvxDateField::GetFormatted( aDate, eDateFormat, *pNumberFormatter, eLang );
                                 }
                                 SAL_FALLTHROUGH; // We need months too!
-                            case SVXDATEFORMAT_C:       // 13.Feb 1996
-                            case SVXDATEFORMAT_D:       // 13.February 1996
+                            case SvxDateFormat::C:       // 13.Feb 1996
+                            case SvxDateFormat::D:       // 13.February 1996
                                 for( sal_uInt16 i = 1; i <= 12; ++i ) // we get all months in a year
                                 {
                                     aDate.SetMonth( i );
@@ -2236,9 +2238,9 @@ IMPL_LINK( SVGFilter, CalcFieldHdl, EditFieldInfo*, pInfo, void )
                                 }
                                 break;
                             // coverity[dead_error_begin] - following conditions exist to avoid compiler warning
-                            case SVXDATEFORMAT_STDSMALL:
-                            case SVXDATEFORMAT_A:       // 13.02.96
-                            case SVXDATEFORMAT_B:       // 13.02.1996
+                            case SvxDateFormat::StdSmall:
+                            case SvxDateFormat::A:       // 13.02.96
+                            case SvxDateFormat::B:       // 13.02.1996
                             default:
                                 // nothing to do here, we always collect the characters needed for these cases.
                                 break;

filter/source/svg/svgfilter.hxx
@@ -38,6 +38,7 @@
 #include <basegfx/polygon/b2dpolygonclipper.hxx>
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
 #include <comphelper/processfactory.hxx>
+#include <editeng/flditem.hxx>
 #include <unotools/tempfile.hxx>
 #include <unotools/ucbstreamhelper.hxx>
 #include <unotools/streamwrap.hxx>
@@ -132,7 +133,7 @@ struct PagePropertySet
     bool               bIsHeaderFieldVisible;
     sal_Int32          nPageNumberingType;
     bool               bIsDateTimeFieldFixed;
-    sal_Int32          nDateTimeFormat;
+    SvxDateFormat      nDateTimeFormat;
     PagePropertySet()
         : bIsBackgroundVisible(false)
         , bAreBackgroundObjectsVisible(false)
@@ -142,7 +143,7 @@ struct PagePropertySet
         , bIsHeaderFieldVisible(false)
         , nPageNumberingType(0)
         , bIsDateTimeFieldFixed(false)
-        , nDateTimeFormat(0)
+        , nDateTimeFormat(SvxDateFormat::AppDefault)
     {
     }
 };

include/editeng/flditem.hxx
@@ -83,17 +83,18 @@ public:
 
 
 enum class SvxDateType { Fix, Var };
-enum SvxDateFormat {    SVXDATEFORMAT_APPDEFAULT = 0,   // Set as in App
-                        SVXDATEFORMAT_SYSTEM,       // Set as in System
-                        SVXDATEFORMAT_STDSMALL,
-                        SVXDATEFORMAT_STDBIG,
-                        SVXDATEFORMAT_A,    // 13.02.96
-                        SVXDATEFORMAT_B,    // 13.02.1996
-                        SVXDATEFORMAT_C,    // 13.Feb 1996
-                        SVXDATEFORMAT_D,    // 13.February 1996
-                        SVXDATEFORMAT_E,    // Tue, 13.February 1996
-                        SVXDATEFORMAT_F     // Tuesday, 13.February 1996
-                    };
+enum class SvxDateFormat {
+    AppDefault = 0, // Set as in App
+    System,         // Set as in System
+    StdSmall,
+    StdBig,
+    A,              // 13.02.96
+    B,              // 13.02.1996
+    C,              // 13.Feb 1996
+    D,              // 13.February 1996
+    E,              // Tue, 13.February 1996
+    F               // Tuesday, 13.February 1996
+};
 
 class EDITENG_DLLPUBLIC SvxDateField : public SvxFieldData
 {
@@ -107,7 +108,7 @@ public:
                             SvxDateField();
     explicit                SvxDateField( const Date& rDate,
                                 SvxDateType eType,
-                                SvxDateFormat eFormat = SVXDATEFORMAT_STDSMALL );
+                                SvxDateFormat eFormat = SvxDateFormat::StdSmall );
 
     sal_Int32               GetFixDate() const { return nFixDate; }
     void                    SetFixDate( const Date& rDate ) { nFixDate = rDate.GetDate(); }
@@ -407,7 +408,9 @@ public:
     SV_DECL_PERSIST1( SvxDateTimeField, css::text::textfield::Type::PRESENTATION_DATE_TIME )
     SvxDateTimeField();
 
-    static OUString    GetFormatted( Date const & rDate, tools::Time const & rTime, int eFormat, SvNumberFormatter& rFormatter, LanguageType eLanguage );
+    static OUString    GetFormatted( Date const & rDate, tools::Time const & rTime,
+                                     SvxDateFormat eDateFormat, SvxTimeFormat eTimeFormat,
+                                     SvNumberFormatter& rFormatter, LanguageType eLanguage );
 
     virtual SvxFieldData*   Clone() const override;
     virtual bool            operator==( const SvxFieldData& ) const override;

oox/source/export/drawingml.cxx
@@ -1654,16 +1654,17 @@ OUString DrawingML::GetFieldValue( const css::uno::Reference< css::text::XTextRa
                 {
                     sal_Int32 nNumFmt = -1;
                     rXPropSet->getPropertyValue(UNO_TC_PROP_NUMFORMAT) >>= nNumFmt;
-                    switch(nNumFmt)
+                    switch(static_cast<SvxDateFormat>(nNumFmt))
                     {
-                        case SVXDATEFORMAT_STDSMALL:
-                        case SVXDATEFORMAT_A: aFieldValue = "datetime"; // 13/02/96
+                        case SvxDateFormat::StdSmall:
+                        case SvxDateFormat::A: aFieldValue = "datetime"; // 13/02/96
                                               break;
-                        case SVXDATEFORMAT_B: aFieldValue = "datetime1"; // 13/02/1996
+                        case SvxDateFormat::B: aFieldValue = "datetime1"; // 13/02/1996
                                               break;
-                        case SVXDATEFORMAT_STDBIG:
-                        case SVXDATEFORMAT_D: aFieldValue = "datetime3"; // 13 February 1996
+                        case SvxDateFormat::StdBig:
+                        case SvxDateFormat::D: aFieldValue = "datetime3"; // 13 February 1996
                                               break;
+                        default: break;
                     }
                 }
                 else if(aFieldKind == "ExtTime")

commit 75f461e3e5f08757117a9ddfba657774b401f718

tdf#118948 - Date format in Impress header/footer is lost when saving
regression from
    commit 5a7378dec2506fd789591d54c2fa4868e54a1281
    convert SvxDateFormat to scoped enum

Change-Id: I9d365c710a20054e249433f54635068f713afa72
Reviewed-on: https://gerrit.libreoffice.org/58202
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/unoidl/unopage.cxx
@@ -1262,7 +1262,7 @@ Any SAL_CALL SdGenericDrawPage::getPropertyValue( const OUString& PropertyName )
     case WID_PAGE_DATETIMEFORMAT:
         {
             auto const & rSettings = GetPage()->getHeaderFooterSettings();
-            sal_Int32 x = static_cast<sal_Int32>(rSettings.meDateFormat) & (static_cast<sal_Int32>(rSettings.meTimeFormat) << 4);
+            sal_Int32 x = static_cast<sal_Int32>(rSettings.meDateFormat) | (static_cast<sal_Int32>(rSettings.meTimeFormat) << 4);
             aAny <<= x;
         }
         break;