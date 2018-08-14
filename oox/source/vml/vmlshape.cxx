LemmingAvalanche/libreoffice-repaired

commit d14aed2cdbf5aecb17c5544c60f65e2198715a2b

simplify some OUString::copy calls
Change-Id: Ifa228ca02ea79a1309e1875414028aade7e5f12d
Reviewed-on: https://gerrit.libreoffice.org/39801
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

oox/source/vml/vmlshape.cxx
@@ -987,7 +987,7 @@ Reference< XShape > BezierShape::implConvertAndInsert( const Reference< XShapes 
 {
     // If we have an 'x' in the last part of the path it means it is closed...
     sal_Int32 nPos = maShapeModel.maVmlPath.lastIndexOf(',');
-    if ( nPos != -1 && maShapeModel.maVmlPath.copy(nPos).indexOf('x') != -1 )
+    if ( nPos != -1 && maShapeModel.maVmlPath.indexOf(nPos, 'x') != -1 )
     {
         const_cast<BezierShape*>( this )->setService( "com.sun.star.drawing.ClosedBezierShape" );
     }

connectivity/source/drivers/dbase/DIndex.cxx
@@ -463,7 +463,7 @@ bool ODbaseIndex::DropImpl()
     {
         // References the Key to an Index-file?
         aKeyName = aInfFile.GetKeyName( nKey );
-        if (aKeyName.copy(0,3) == "NDX")
+        if (aKeyName.startsWith("NDX"))
         {
             if(sEntry == OStringToOUString(aInfFile.ReadKey(aKeyName),m_pTable->getConnection()->getTextEncoding()))
             {

connectivity/source/drivers/mork/MConnection.cxx
@@ -71,7 +71,7 @@ void OConnection::construct(const OUString& url)
 
     sal_Int32 nLen = url.indexOf(':');
     nLen = url.indexOf(':',nLen+1);
-    OSL_ENSURE( url.copy( 0, nLen ) == "sdbc:address", "OConnection::construct: invalid start of the URI - should never have survived XDriver::acceptsURL!" );
+    OSL_ENSURE( url.startsWith("sdbc:address:"), "OConnection::construct: invalid start of the URI - should never have survived XDriver::acceptsURL!" );
 
     OUString aAddrbookURI(url.copy(nLen+1));
     // Get Scheme

filter/source/svg/svgreader.cxx
@@ -236,7 +236,7 @@ struct AnnotatingVisitor
                 {
                     const OUString sValue(xNode->getNodeValue());
                     ElementRefMapType::iterator aFound=maGradientIdMap.end();
-                    if ( sValue.copy(0,1) == "#" )
+                    if ( sValue.startsWith("#") )
                         aFound = maGradientIdMap.find(sValue.copy(1));
                     else
                         aFound = maGradientIdMap.find(sValue);
@@ -279,7 +279,7 @@ struct AnnotatingVisitor
                 {
                     const OUString sValue(xNode->getNodeValue());
                     ElementRefMapType::iterator aFound=maGradientIdMap.end();
-                    if ( sValue.copy(0,1) == "#" )
+                    if ( sValue.startsWith("#") )
                         aFound = maGradientIdMap.find(sValue.copy(1));
                     else
                         aFound = maGradientIdMap.find(sValue);
@@ -315,7 +315,7 @@ struct AnnotatingVisitor
                 {
                     OUString sValue(xNode->getNodeValue());
                     ElementRefMapType::iterator aFound=maElementIdMap.end();
-                    if ( sValue.copy(0,1) == "#" )
+                    if ( sValue.startsWith("#") )
                         sValue = sValue.copy(1);
                     aFound = maElementIdMap.find(sValue);
                     bool bFound = aFound != maElementIdMap.end();
@@ -988,7 +988,7 @@ struct AnnotatingVisitor
             case XML_HREF:
             {
                 ElementRefMapType::iterator aFound=maStopIdMap.end();
-                if ( sValue.copy(0,1) == "#" )
+                if ( sValue.startsWith("#") )
                     aFound = maStopIdMap.find(sValue.copy(1));
                 else
                     aFound = maStopIdMap.find(sValue);

l10ntools/source/po.cxx
@@ -86,7 +86,7 @@ namespace
         sal_Int32 nIndex = 0;
         while((nIndex=sResult.indexOf("\\n",nIndex))!=-1)
         {
-            if( sResult.copy(nIndex-1,3)!="\\\\n" &&
+            if( !sResult.match("\\\\n", nIndex-1) &&
                 nIndex!=sResult.getLength()-3)
             {
                sResult = sResult.replaceAt(nIndex,2,"\\n\"\n\"");

sc/source/filter/excel/xehelper.cxx
@@ -888,13 +888,13 @@ OUString lclEncodeDosUrl(
         OUString aOldUrl = rUrl;
         aBuf.append(EXC_URLSTART_ENCODED);
 
-        if ( aOldUrl.getLength() > 2 && aOldUrl.copy(0,2) == "\\\\" )
+        if ( aOldUrl.getLength() > 2 && aOldUrl.startsWith("\\\\") )
         {
             // UNC
             aBuf.append(EXC_URL_DOSDRIVE).append('@');
             aOldUrl = aOldUrl.copy(2);
         }
-        else if ( aOldUrl.getLength() > 2 && aOldUrl.copy(1,2) == ":\\" )
+        else if ( aOldUrl.getLength() > 2 && aOldUrl.match(":\\", 1) )
         {
             // drive letter
             sal_Unicode cThisDrive = rBase.isEmpty() ? ' ' : rBase[0];
@@ -916,7 +916,7 @@ OUString lclEncodeDosUrl(
         sal_Int32 nPos = -1;
         while((nPos = aOldUrl.indexOf('\\')) != -1)
         {
-            if ( aOldUrl.copy(0,2) == ".." )
+            if ( aOldUrl.startsWith("..") )
                 // parent dir (NOTE: the MS-XLS spec doesn't mention this, and
                 // Excel seems confused by this token).
                 aBuf.append(EXC_URL_PARENTDIR);

sw/source/filter/xml/xmltexti.cxx
@@ -241,7 +241,7 @@ uno::Reference< XPropertySet > SwXMLTextImportHelper::createAndInsertOLEObject(
 
     SwFrameFormat *pFrameFormat = nullptr;
     SwOLENode *pOLENd = nullptr;
-    if( rHRef.copy( 0, nPos ) == "vnd.sun.star.ServiceName" )
+    if( rHRef.startsWith("vnd.sun.star.ServiceName:") )
     {
         bool bInsert = false;
         SvGlobalName aClassName;

ucb/source/ucp/webdav-neon/webdavprovider.cxx
@@ -142,7 +142,7 @@ ContentProvider::queryContent(
     if ( aURL.getLength() < ( aScheme.getLength() + 3 ) )
         throw ucb::IllegalIdentifierException();
 
-    if ( aURL.copy( aScheme.getLength(), 3 ) != "://" )
+    if ( !aURL.match("://", aScheme.getLength()) )
         throw ucb::IllegalIdentifierException();
 
     uno::Reference< ucb::XContentIdentifier > xCanonicId;

commit a1fa53acf7bf0c006c2ceb2031ad975c58ef9872

tdf#119234 OOXML Shapes from Word fail to render
regression from
	commit d14aed2cdbf5aecb17c5544c60f65e2198715a2b
	simplify some OUString::copy calls
I got the params switched around

Change-Id: I68da0ae681f2b886e35f74bd72de0c482c843ae9
Reviewed-on: https://gerrit.libreoffice.org/58986
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

oox/source/vml/vmlshape.cxx
@@ -1023,7 +1023,7 @@ Reference< XShape > BezierShape::implConvertAndInsert( const Reference< XShapes 
 {
     // If we have an 'x' in the last part of the path it means it is closed...
     sal_Int32 nPos = maShapeModel.maVmlPath.lastIndexOf(',');
-    if ( nPos != -1 && maShapeModel.maVmlPath.indexOf(nPos, 'x') != -1 )
+    if ( nPos != -1 && maShapeModel.maVmlPath.indexOf('x', nPos) != -1 )
     {
         const_cast<BezierShape*>( this )->setService( "com.sun.star.drawing.ClosedBezierShape" );
     }