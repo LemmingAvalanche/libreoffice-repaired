LemmingAvalanche/libreoffice-repaired

commit e944441b7db5754fcc2d1a6481bc403b26064a26

Resolves: tdf#107012 follow date order of the target locale
... when converting format codes between locales, so en-US MM/DD/YYYY
correctly ends up as de-DE DD.MM.YYYY instead of MM.DD.YYYY

Change-Id: Iccfdd4787fc05462f47266c77cc9e95d14dae60d

svl/source/numbers/zforscan.cxx
@@ -1520,6 +1520,17 @@ int ImpSvNumberformatScan::FinalScanGetCalendar( sal_Int32& nPos, sal_uInt16& i,
     return 0;
 }
 
+bool ImpSvNumberformatScan::IsDateFragment( size_t nPos1, size_t nPos2 ) const
+{
+    return nPos2 - nPos1 == 2 && nTypeArray[nPos1+1] == NF_SYMBOLTYPE_DATESEP;
+}
+
+void ImpSvNumberformatScan::SwapArrayElements( size_t nPos1, size_t nPos2 )
+{
+    std::swap( nTypeArray[nPos1], nTypeArray[nPos2]);
+    std::swap( sStrArray[nPos1], sStrArray[nPos2]);
+}
+
 sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
 {
     const LocaleDataWrapper* pLoc = pFormatter->GetLocaleData();
@@ -1535,6 +1546,9 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
     sal_Unicode cOldKeyH    = sKeyword[NF_KEY_H][0];
     sal_Unicode cOldKeyMI   = sKeyword[NF_KEY_MI][0];
     sal_Unicode cOldKeyS    = sKeyword[NF_KEY_S][0];
+    DateOrder eOldDateOrder = pLoc->getDateOrder();
+    sal_uInt16 nDayPos, nMonthPos, nYearPos;
+    nDayPos = nMonthPos = nYearPos = SAL_MAX_UINT16;
 
     // If the group separator is a No-Break Space (French) continue with a
     // normal space instead so queries on space work correctly.
@@ -1546,6 +1560,7 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
     {
         sOldThousandSep = " ";
     }
+    bool bNewDateOrder = false;
     // change locale data et al
     if (bConvertMode)
     {
@@ -1554,6 +1569,7 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
         pLoc = pFormatter->GetLocaleData();
         //! init new keywords
         InitKeywords();
+        bNewDateOrder = (eOldDateOrder != pLoc->getDateOrder());
     }
     const CharClass* pChrCls = pFormatter->GetCharClass();
 
@@ -2318,6 +2334,37 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
             case NF_KEY_RR :                        // RR
                 sStrArray[i] = sKeyword[nTypeArray[i]]; // tTtT -> TTTT
                 nPos = nPos + sStrArray[i].getLength();
+                if (bNewDateOrder)
+                {
+                    // For simple numeric date formats record date order and
+                    // later rearrange.
+                    switch (nTypeArray[i])
+                    {
+                        case NF_KEY_M:
+                        case NF_KEY_MM:
+                            if (nMonthPos == SAL_MAX_UINT16)
+                                nMonthPos = i;
+                            else
+                                bNewDateOrder = false;
+                        break;
+                        case NF_KEY_D:
+                        case NF_KEY_DD:
+                            if (nDayPos == SAL_MAX_UINT16)
+                                nDayPos = i;
+                            else
+                                bNewDateOrder = false;
+                        break;
+                        case NF_KEY_YY:
+                        case NF_KEY_YYYY:
+                            if (nYearPos == SAL_MAX_UINT16)
+                                nYearPos = i;
+                            else
+                                bNewDateOrder = false;
+                        break;
+                        default:
+                            ;   // nothing
+                    }
+                }
                 i++;
                 break;
             default: // Other keywords
@@ -2613,6 +2660,37 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
                 bTimePart = false;
                 sStrArray[i] = sKeyword[nTypeArray[i]]; // tTtT -> TTTT
                 nPos = nPos + sStrArray[i].getLength();
+                if (bNewDateOrder)
+                {
+                    // For simple numeric date formats record date order and
+                    // later rearrange.
+                    switch (nTypeArray[i])
+                    {
+                        case NF_KEY_M:
+                        case NF_KEY_MM:
+                            if (nMonthPos == SAL_MAX_UINT16)
+                                nMonthPos = i;
+                            else
+                                bNewDateOrder = false;
+                        break;
+                        case NF_KEY_D:
+                        case NF_KEY_DD:
+                            if (nDayPos == SAL_MAX_UINT16)
+                                nDayPos = i;
+                            else
+                                bNewDateOrder = false;
+                        break;
+                        case NF_KEY_YY:
+                        case NF_KEY_YYYY:
+                            if (nYearPos == SAL_MAX_UINT16)
+                                nYearPos = i;
+                            else
+                                bNewDateOrder = false;
+                        break;
+                        default:
+                            ;   // nothing
+                    }
+                }
                 i++;
                 break;
             case NF_KEY_THAI_T :
@@ -2652,6 +2730,110 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
     }
     if ( bConvertMode )
     {
+        if (bNewDateOrder && sOldDateSep == "-")
+        {
+            // Keep ISO formats Y-M-D, Y-M and M-D
+            if (IsDateFragment( nYearPos, nMonthPos))
+            {
+                nTypeArray[nYearPos+1] = NF_SYMBOLTYPE_STRING;
+                sStrArray[nYearPos+1] = sOldDateSep;
+                bNewDateOrder = false;
+            }
+            if (IsDateFragment( nMonthPos, nDayPos))
+            {
+                nTypeArray[nMonthPos+1] = NF_SYMBOLTYPE_STRING;
+                sStrArray[nMonthPos+1] = sOldDateSep;
+                bNewDateOrder = false;
+            }
+        }
+        if (bNewDateOrder)
+        {
+            // Rearrange date order to the target locale if the original order
+            // includes date separators and is adjacent.
+            /* TODO: for incomplete dates trailing separators need to be
+             * handled according to the locale's usage, e.g. en-US M/D should
+             * be converted to de-DE D.M. and vice versa. As is, it's
+             * M/D -> D.M and D.M. -> M/D/ where specifically the latter looks
+             * odd. Check accepted date patterns and append/remove? */
+            switch (eOldDateOrder)
+            {
+                case DateOrder::DMY:
+                    switch (pLoc->getDateOrder())
+                    {
+                        case DateOrder::MDY:
+                            if (IsDateFragment( nDayPos, nMonthPos))
+                                SwapArrayElements( nDayPos, nMonthPos);
+                        break;
+                        case DateOrder::YMD:
+                            if (nYearPos != SAL_MAX_UINT16)
+                            {
+                                if (IsDateFragment( nDayPos, nMonthPos) && IsDateFragment( nMonthPos, nYearPos))
+                                    SwapArrayElements( nDayPos, nYearPos);
+                            }
+                            else
+                            {
+                                if (IsDateFragment( nDayPos, nMonthPos))
+                                    SwapArrayElements( nDayPos, nMonthPos);
+                            }
+                        break;
+                        default:
+                            ;   // nothing
+                    }
+                break;
+                case DateOrder::MDY:
+                    switch (pLoc->getDateOrder())
+                    {
+                        case DateOrder::DMY:
+                            if (IsDateFragment( nMonthPos, nDayPos))
+                                SwapArrayElements( nMonthPos, nDayPos);
+                        break;
+                        case DateOrder::YMD:
+                            if (nYearPos != SAL_MAX_UINT16)
+                            {
+                                if (IsDateFragment( nMonthPos, nDayPos) && IsDateFragment( nDayPos, nYearPos))
+                                {
+                                    SwapArrayElements( nYearPos, nMonthPos);    // YDM
+                                    SwapArrayElements( nYearPos, nDayPos);      // YMD
+                                }
+                            }
+                        break;
+                        default:
+                            ;   // nothing
+                    }
+                break;
+                case DateOrder::YMD:
+                    switch (pLoc->getDateOrder())
+                    {
+                        case DateOrder::DMY:
+                            if (nYearPos != SAL_MAX_UINT16)
+                            {
+                                if (IsDateFragment( nYearPos, nMonthPos) && IsDateFragment( nMonthPos, nDayPos))
+                                    SwapArrayElements( nYearPos, nDayPos);
+                            }
+                            else
+                            {
+                                if (IsDateFragment( nMonthPos, nDayPos))
+                                    SwapArrayElements( nMonthPos, nDayPos);
+                            }
+                        break;
+                        case DateOrder::MDY:
+                            if (nYearPos != SAL_MAX_UINT16)
+                            {
+                                if (IsDateFragment( nYearPos, nMonthPos) && IsDateFragment( nMonthPos, nDayPos))
+                                {
+                                    SwapArrayElements( nYearPos, nDayPos);      // DMY
+                                    SwapArrayElements( nYearPos, nMonthPos);    // MDY
+                                }
+                            }
+                        break;
+                        default:
+                            ;   // nothing
+                    }
+                break;
+                default:
+                    ;   // nothing
+            }
+        }
         // strings containing keywords of the target locale must be quoted, so
         // the user sees the difference and is able to edit the format string
         for ( i=0; i < nAnzStrings; i++ )

svl/source/numbers/zforscan.hxx
@@ -241,6 +241,15 @@ private: // Private section
         reused instead of shifting all one up and nPos is decremented! */
     bool InsertSymbol( sal_uInt16 & nPos, svt::NfSymbolType eType, const OUString& rStr );
 
+    /** Whether two key symbols are adjacent separated by date separator.
+        This can only be used at the end of FinalScan() after
+        NF_SYMBOLTYPE_DATESEP has already been set.
+     */
+    bool IsDateFragment( size_t nPos1, size_t nPos2 ) const;
+
+    /** Swap nTypeArray and sStrArray elements at positions. */
+    void SwapArrayElements( size_t nPos1, size_t nPos2 );
+
     static bool StringEqualsChar( const OUString& rStr, sal_Unicode ch )
         { return rStr.getLength() == 1 && rStr[0] == ch; }
 

commit 41203dcd8cf6aae0a55116a2134293321795a554

Resolves: tdf#119013 do not over-aggressively reorder date particles
In particular not when reading documents as we don't know what the
original (default/system) locale was when the date format was
created and stored and whether the format's date order actually
matched the locale's ordering.

Regression from

    commit e944441b7db5754fcc2d1a6481bc403b26064a26
    CommitDate: Tue Apr 18 17:01:27 2017 +0200

        Resolves: tdf#107012 follow date order of the target locale

Change-Id: I9d3bdbd512d95ed81ff6459e368a2d7497ec8a2d
Reviewed-on: https://gerrit.libreoffice.org/59182
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

basic/source/runtime/runtime.cxx
@@ -438,12 +438,12 @@ std::shared_ptr<SvNumberFormatter> SbiInstance::PrepareNumberFormatter( sal_uInt
     }
     OUString aStr( aDateStr );      // PutandConvertEntry() modifies string!
     pNumberFormatter->PutandConvertEntry( aStr, nCheckPos, nType,
-        rnStdDateIdx, LANGUAGE_ENGLISH_US, eLangType );
+        rnStdDateIdx, LANGUAGE_ENGLISH_US, eLangType, true);
     nCheckPos = 0;
     aDateStr += " HH:MM:SS";
     aStr = aDateStr;
     pNumberFormatter->PutandConvertEntry( aStr, nCheckPos, nType,
-        rnStdDateTimeIdx, LANGUAGE_ENGLISH_US, eLangType );
+        rnStdDateTimeIdx, LANGUAGE_ENGLISH_US, eLangType, true);
     return pNumberFormatter;
 }
 

basic/source/sbx/sbxdate.cxx
@@ -142,7 +142,7 @@ double ImpGetDate( const SbxValues* p )
             OUString aStr = aDateStr + " HH:MM:SS";
 
             pFormatter->PutandConvertEntry( aStr, nCheckPos, nType,
-                                            nIndex, LANGUAGE_ENGLISH_US, eLangType );
+                                            nIndex, LANGUAGE_ENGLISH_US, eLangType, true);
             bool bSuccess = pFormatter->IsNumberFormat( *p->pOUString, nIndex, nRes );
             if ( bSuccess )
             {
@@ -337,7 +337,7 @@ start:
                                             nType,
                                             nIndex,
                                             LANGUAGE_ENGLISH_US,
-                                            eLangType );
+                                            eLangType, true);
             pFormatter->GetOutputString( n, nIndex, *p->pOUString, &pColor );
 #endif
             break;

basic/source/sbx/sbxscan.cxx
@@ -720,7 +720,7 @@ void SbxValue::Format( OUString& rRes, const OUString* pFmt ) const
                 else
                 {
                     aFmtStr = OUString::createFromAscii(pInfo->mpOOoFormat);
-                    pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType );
+                    pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType, true);
                 }
                 pFormatter->GetOutputString( nNumber, nIndex, rRes, &pCol );
             }
@@ -737,7 +737,7 @@ void SbxValue::Format( OUString& rRes, const OUString* pFmt ) const
                     if( floor( nNumber ) != nNumber )
                     {
                         aFmtStr = "H:MM:SS AM/PM";
-                        pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType );
+                        pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType, true);
                         OUString aTime;
                         pFormatter->GetOutputString( nNumber, nIndex, aTime, &pCol );
                         rRes += " " + aTime;
@@ -747,7 +747,7 @@ void SbxValue::Format( OUString& rRes, const OUString* pFmt ) const
                 {
                     // long time only
                     aFmtStr = "H:MM:SS AM/PM";
-                    pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType );
+                    pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType, true);
                     pFormatter->GetOutputString( nNumber, nIndex, rRes, &pCol );
                 }
             }
@@ -783,7 +783,7 @@ void SbxValue::Format( OUString& rRes, const OUString* pFmt ) const
             }
             else
             {
-                pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType );
+                pFormatter->PutandConvertEntry( aFmtStr, nCheckPos, nType, nIndex, LANGUAGE_ENGLISH, eLangType, true);
                 pFormatter->GetOutputString( nNumber, nIndex, rRes, &pCol );
             }
 

editeng/source/items/flditem.cxx
@@ -659,7 +659,7 @@ OUString SvxExtTimeField::GetFormatted( tools::Time const & aTime, SvxTimeFormat
             sal_Int32 nCheckPos;
             SvNumFormatType nType;
             rFormatter.PutandConvertEntry( aFormatCode, nCheckPos, nType,
-                                           nFormatKey, LANGUAGE_ENGLISH_US, eLang );
+                                           nFormatKey, LANGUAGE_ENGLISH_US, eLang, true);
             DBG_ASSERT( nCheckPos == 0, "SvxTimeFormat::HH12_MM_SS_00: could not insert format code" );
             if ( nCheckPos )
             {

include/svl/zforlist.hxx
@@ -461,7 +461,7 @@ public:
     bool PutandConvertEntry( OUString& rString, sal_Int32& nCheckPos,
                              SvNumFormatType& nType, sal_uInt32& nKey,
                              LanguageType eLnge, LanguageType eNewLnge,
-                             bool bForExcelExport = false );
+                             bool bConvertDateOrder );
 
     /** Same as <method>PutandConvertEntry</method> but the format code string
          is considered to be of the System language/country eLnge and is

sc/source/filter/excel/xistyle.cxx
@@ -702,7 +702,7 @@ void XclImpNumFmtBuffer::CreateScFormats()
         {
             OUString aFormat( rNumFmt.maFormat );
             rFormatter.PutandConvertEntry( aFormat, nCheckPos,
-                                           nType, nKey, LANGUAGE_ENGLISH_US, rNumFmt.meLanguage );
+                                           nType, nKey, LANGUAGE_ENGLISH_US, rNumFmt.meLanguage, false);
         }
         else
             nKey = rFormatter.GetFormatIndex( rNumFmt.meOffset, rNumFmt.meLanguage );

sc/source/ui/docshell/impex.cxx
@@ -2035,7 +2035,7 @@ bool ScImportExport::Sylk2Doc( SvStream& rStrm )
                     SvNumFormatType nType;
                     sal_uInt32 nKey;
                     pDoc->GetFormatTable()->PutandConvertEntry( aCode, nCheckPos, nType, nKey,
-                                                                LANGUAGE_ENGLISH_US, ScGlobal::eLnge );
+                                                                LANGUAGE_ENGLISH_US, ScGlobal::eLnge, false);
                     if ( nCheckPos )
                         nKey = 0;
                     aFormats.push_back( nKey );

sfx2/source/bastyp/sfxhtml.cxx
@@ -337,7 +337,7 @@ double SfxHTMLParser::GetTableDataOptionsValNum( sal_uInt32& nNumForm,
             rFormatter.PutEntry( aFormat, nCheckPos, nType, nNumForm, eNumLang );
         else
             rFormatter.PutandConvertEntry( aFormat, nCheckPos, nType, nNumForm,
-                                           eParseLang, eNumLang );
+                                           eParseLang, eNumLang, true);
     }
     else
     {

svl/source/numbers/numfmuno.cxx
@@ -458,7 +458,9 @@ sal_Int32 SAL_CALL SvNumberFormatsObj::addNewConverted( const OUString& aFormat,
     sal_uInt32 nKey = 0;
     sal_Int32 nCheckPos = 0;
     SvNumFormatType nType = SvNumFormatType::ALL;
-    bool bOk = pFormatter->PutandConvertEntry( aFormStr, nCheckPos, nType, nKey, eLang, eNewLang );
+    // This is used also when reading OOXML documents, there's no indicator
+    // whether to convert date particle order as well, so don't. See tdf#119013
+    bool bOk = pFormatter->PutandConvertEntry( aFormStr, nCheckPos, nType, nKey, eLang, eNewLang, false);
     if (bOk || nKey > 0)
         nRet = nKey;
     else if (nCheckPos)

svl/source/numbers/zforlist.cxx
@@ -462,7 +462,7 @@ void SvNumberFormatter::ReplaceSystemCL( LanguageType eOldLanguage )
     // convert additional and user defined from old system to new system
     SvNumberformat* pStdFormat = GetFormatEntry( nCLOffset + ZF_STANDARD );
     sal_uInt32 nLastKey = nMaxBuiltin;
-    pFormatScanner->SetConvertMode( eOldLanguage, LANGUAGE_SYSTEM, true );
+    pFormatScanner->SetConvertMode( eOldLanguage, LANGUAGE_SYSTEM, true , true);
     while ( !aOldTable.empty() )
     {
         nKey = aOldTable.begin()->first;
@@ -628,7 +628,7 @@ bool SvNumberFormatter::PutandConvertEntry(OUString& rString,
                                            sal_uInt32& nKey,
                                            LanguageType eLnge,
                                            LanguageType eNewLnge,
-                                           bool bForExcelExport )
+                                           bool bConvertDateOrder )
 {
     ::osl::MutexGuard aGuard( GetInstanceMutex() );
     bool bRes;
@@ -636,7 +636,7 @@ bool SvNumberFormatter::PutandConvertEntry(OUString& rString,
     {
         eNewLnge = IniLnge;
     }
-    pFormatScanner->SetConvertMode(eLnge, eNewLnge, false, bForExcelExport);
+    pFormatScanner->SetConvertMode(eLnge, eNewLnge, false, bConvertDateOrder);
     bRes = PutEntry(rString, nCheckPos, nType, nKey, eLnge);
     pFormatScanner->SetConvertMode(false);
     return bRes;
@@ -655,7 +655,7 @@ bool SvNumberFormatter::PutandConvertEntrySystem(OUString& rString,
     {
         eNewLnge = IniLnge;
     }
-    pFormatScanner->SetConvertMode(eLnge, eNewLnge, true);
+    pFormatScanner->SetConvertMode(eLnge, eNewLnge, true, true);
     bRes = PutEntry(rString, nCheckPos, nType, nKey, eLnge);
     pFormatScanner->SetConvertMode(false);
     return bRes;
@@ -1718,7 +1718,7 @@ bool SvNumberFormatter::GetPreviewStringGuess( const OUString& sFormatString,
 
         // Try English -> other or convert english to other
         LanguageType eFormatLang = LANGUAGE_ENGLISH_US;
-        pFormatScanner->SetConvertMode( LANGUAGE_ENGLISH_US, eLnge );
+        pFormatScanner->SetConvertMode( LANGUAGE_ENGLISH_US, eLnge, false, false);
         sTmpString = sFormatString;
         pEntry.reset(new SvNumberformat( sTmpString, pFormatScanner.get(),
                                      pStringScanner.get(), nCheckPos, eFormatLang ));
@@ -1743,7 +1743,7 @@ bool SvNumberFormatter::GetPreviewStringGuess( const OUString& sFormatString,
                 sal_Int32 nCheckPos2 = -1;
                 // try other --> english
                 eFormatLang = eLnge;
-                pFormatScanner->SetConvertMode( eLnge, LANGUAGE_ENGLISH_US );
+                pFormatScanner->SetConvertMode( eLnge, LANGUAGE_ENGLISH_US, false, false);
                 sTmpString = sFormatString;
                 std::unique_ptr<SvNumberformat> pEntry2(new SvNumberformat( sTmpString, pFormatScanner.get(),
                                                               pStringScanner.get(), nCheckPos2, eFormatLang ));

svl/source/numbers/zformat.cxx
@@ -1883,7 +1883,7 @@ void SvNumberformat::ConvertLanguage( SvNumberFormatter& rConverter,
     SvNumFormatType nType = eType;
     OUString aFormatString( sFormatstring );
     rConverter.PutandConvertEntry( aFormatString, nCheckPos, nType,
-                                   nKey, eConvertFrom, eConvertTo );
+                                   nKey, eConvertFrom, eConvertTo, false);
     const SvNumberformat* pFormat = rConverter.GetEntry( nKey );
     DBG_ASSERT( pFormat, "SvNumberformat::ConvertLanguage: Conversion without format" );
     if ( pFormat )

svl/source/numbers/zforscan.cxx
@@ -121,7 +121,7 @@ ImpSvNumberformatScan::ImpSvNumberformatScan( SvNumberFormatter* pFormatterP )
     pFormatter = pFormatterP;
     xNFC = css::i18n::NumberFormatMapper::create( pFormatter->GetComponentContext() );
     bConvertMode = false;
-    mbConvertForExcelExport = false;
+    mbConvertDateOrder = false;
     bConvertSystemToSystem = false;
     bKeywordsNeedInit = true;            // locale dependent and not locale dependent keywords
     bCompatCurNeedInit = true;           // locale dependent compatibility currency strings
@@ -1784,7 +1784,7 @@ sal_Int32 ImpSvNumberformatScan::FinalScan( OUString& rString )
         // Adapt date order to target locale, but Excel does not handle date
         // particle re-ordering for the target locale when loading documents,
         // though it does exchange separators, tdf#113889
-        bNewDateOrder = (!mbConvertForExcelExport && eOldDateOrder != pLoc->getDateOrder());
+        bNewDateOrder = (mbConvertDateOrder && eOldDateOrder != pLoc->getDateOrder());
     }
     const CharClass* pChrCls = pFormatter->GetCharClass();
 

svl/source/numbers/zforscan.hxx
@@ -148,13 +148,13 @@ public:
     void ReplaceBooleanEquivalent( OUString& rString );
 
     void SetConvertMode(LanguageType eTmpLge, LanguageType eNewLge,
-            bool bSystemToSystem = false, bool bForExcelExport = false)
+            bool bSystemToSystem, bool bConvertDateOrder)
     {
         bConvertMode = true;
         eNewLnge = eNewLge;
         eTmpLnge = eTmpLge;
         bConvertSystemToSystem = bSystemToSystem;
-        mbConvertForExcelExport = bForExcelExport;
+        mbConvertDateOrder = bConvertDateOrder;
     }
     // Only changes the bool variable, in order to temporarily pause the convert mode
     void SetConvertMode(bool bMode) { bConvertMode = bMode; }
@@ -213,7 +213,7 @@ private: // Private section
     static const OUString sErrStr;              // String for error output
 
     bool bConvertMode;                          // Set in the convert mode
-    bool mbConvertForExcelExport;               // Set in the convert mode whether to convert for Excel export
+    bool mbConvertDateOrder;                    // Set in the convert mode whether to convert date particles order
 
     LanguageType eNewLnge;                      // Language/country which the scanned string is converted to (for Excel filter)
     LanguageType eTmpLnge;                      // Language/country which the scanned string is converted from (for Excel filter)

svtools/source/control/fmtfield.cxx
@@ -575,7 +575,7 @@ void FormattedField::SetFormatter(SvNumberFormatter* pFormatter, bool bResetForm
             // convert the old format string into the new language
             sal_Int32 nCheckPos;
             SvNumFormatType nType;
-            pFormatter->PutandConvertEntry(sOldFormat, nCheckPos, nType, nDestKey, aOldLang, aNewLang);
+            pFormatter->PutandConvertEntry(sOldFormat, nCheckPos, nType, nDestKey, aOldLang, aNewLang, true);
             m_nFormatKey = nDestKey;
         }
         m_pFormatter = pFormatter;

sw/source/core/fields/fldbas.cxx
@@ -502,7 +502,7 @@ OUString SwValueFieldType::ExpandValue( const double& rVal,
                 OUString sFormat(pEntry->GetFormatstring());
 
                 pFormatter->PutandConvertEntry(sFormat, nDummy, nType, nFormat,
-                                        pEntry->GetLanguage(), nFormatLng );
+                                        pEntry->GetLanguage(), nFormatLng, false);
             }
             else
                 nFormat = nNewFormat;
@@ -610,7 +610,7 @@ sal_uInt32 SwValueField::GetSystemFormat(SvNumberFormatter* pFormatter, sal_uInt
 
             sal_uInt32 nTempFormat = nFormat;
             pFormatter->PutandConvertEntry(sFormat, nDummy, nType,
-                                           nTempFormat, pEntry->GetLanguage(), nLng);
+                                           nTempFormat, pEntry->GetLanguage(), nLng, true);
             nFormat = nTempFormat;
         }
         else
@@ -660,7 +660,7 @@ void SwValueField::SetLanguage( LanguageType nLng )
                     pFormatter->PutandConvertEntry( sFormat, nDummy, nType,
                                                     nNewFormat,
                                                     pEntry->GetLanguage(),
-                                                    nFormatLng );
+                                                    nFormatLng, false);
                 }
                 SetFormat( nNewFormat );
             }

sw/source/filter/ww8/ww8par5.cxx
@@ -477,7 +477,7 @@ SvNumFormatType SwWW8ImplReader::GetTimeDatePara(OUString const & rStr, sal_uInt
 
         OUString sTemp(sParams);
         pFormatter->PutandConvertEntry(sTemp, nCheckPos, nType, rFormat,
-                                       LANGUAGE_ENGLISH_US, rLang);
+                                       LANGUAGE_ENGLISH_US, rLang, false);
         sParams = sTemp;
 
         return bHasTime ? SvNumFormatType::DATETIME : SvNumFormatType::DATE;

xmloff/source/style/xmlnumfe.cxx
@@ -1975,7 +1975,7 @@ sal_uInt32 SvXMLNumFmtExport::ForceSystemLanguage( sal_uInt32 nKey )
             pFormatter->PutandConvertEntry(
                             aFormatString,
                             nErrorPos, nType, nNewKey,
-                            pFormat->GetLanguage(), LANGUAGE_SYSTEM );
+                            pFormat->GetLanguage(), LANGUAGE_SYSTEM, true);
 
             // success? Then use new key.
             if( nErrorPos == 0 )

svl/qa/unit/svl.cxx
@@ -1656,7 +1656,7 @@ void Test::testColorNamesConversion()
     for (size_t i = NF_KEY_BLACK; i <= NF_KEY_WHITE; ++i)
     {
         aFormatCode = "[" + aGermanKeywords[i] + "]0";
-        aFormatter.PutandConvertEntry( aFormatCode, nCheckPos, nType, nKey, LANGUAGE_GERMAN, LANGUAGE_ENGLISH_US);
+        aFormatter.PutandConvertEntry( aFormatCode, nCheckPos, nType, nKey, LANGUAGE_GERMAN, LANGUAGE_ENGLISH_US, false);
         CPPUNIT_ASSERT_EQUAL_MESSAGE("CheckPos should be 0.", sal_Int32(0), nCheckPos);
         CPPUNIT_ASSERT_EQUAL_MESSAGE("Type should be NUMBER.", SvNumFormatType::NUMBER, nType);
         CPPUNIT_ASSERT_EQUAL( OUString("[" + rEnglishKeywords[i] + "]0"), aFormatCode);