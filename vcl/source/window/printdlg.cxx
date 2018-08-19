LemmingAvalanche/libreoffice-repaired

commit b6570d211f64bc2683f86b8093a9567472e88a37

create appendCopy method in OUStringBuffer
so we can avoid temporary copies when appending a substring of an
OUString to the buffer. I would have preferred to call the method just
"append" but that results in ambiguous method errors when the callsite
is something like
   sal_Int32 n;
   OUStringBuffer s;
   s.append(n, 10);
I'm not sure why

Change-Id: I6b5b6641fcb5b26ce2269f89ef06e03c0b6aa76f
Reviewed-on: https://gerrit.libreoffice.org/58666
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/window/printdlg.cxx
@@ -1343,10 +1343,10 @@ static OUString searchAndReplace( const OUString& i_rOrig, const char* i_pRepl, 
     if( nPos != -1 )
     {
         OUStringBuffer aBuf( i_rOrig.getLength() );
-        aBuf.append( i_rOrig.getStr(), nPos );
+        aBuf.appendCopy( i_rOrig, nPos );
         aBuf.append( i_rRepl );
         if( nPos + i_nReplLen < i_rOrig.getLength() )
-            aBuf.append( i_rOrig.getStr() + nPos + i_nReplLen );
+            aBuf.appendCopy( i_rOrig, nPos + i_nReplLen );
         return aBuf.makeStringAndClear();
     }
     return i_rOrig;

basic/source/comp/codegen.cxx
@@ -212,7 +212,7 @@ void SbiCodeGen::Save()
                         {
                             aIfaceProcName.append(aPropPrefix);
                         }
-                        aIfaceProcName.append(aPureProcName.copy( rIfaceName.getLength() + 1 ));
+                        aIfaceProcName.appendCopy(aPureProcName, rIfaceName.getLength() + 1 );
                         aIfaceName = rIfaceName;
                         nPassCount = 2;
                         break;

basic/source/runtime/runtime.cxx
@@ -2051,7 +2051,7 @@ void SbiRuntime::StepRSET()
         }
         else
         {
-            aNewStr.append(aRefValString.copy(0, nVarStrLen));
+            aNewStr.appendCopy(aRefValString, 0, nVarStrLen);
         }
         refVar->PutString(aNewStr.makeStringAndClear());
 

comphelper/source/misc/string.cxx
@@ -443,7 +443,7 @@ OUString removeAny(OUString const& rIn,
             {
                 if (i > 0)
                 {
-                    buf.append(rIn.copy(0, i));
+                    buf.appendCopy(rIn, 0, i);
                 }
                 isFound = true;
             }

configmgr/qa/unit/test.cxx
@@ -553,7 +553,7 @@ void normalize(
     } else {
         OUStringBuffer buf(path);
         buf.append('/');
-        buf.append(relative.copy(0, i));
+        buf.appendCopy(relative, 0, i);
         *normalizedPath = buf.makeStringAndClear();
         *name = relative.copy(i + 1);
     }

connectivity/source/commontools/dbtools2.cxx
@@ -132,7 +132,7 @@ OUString createStandardTypePart(const Reference< XPropertySet >& xColProp,const 
         }
         else
         {
-            aSql.append(sTypeName.copy(0,++nParenPos));
+            aSql.appendCopy(sTypeName, 0, ++nParenPos);
         }
 
         if ( nPrecision > 0 && nDataType != DataType::TIMESTAMP )
@@ -149,7 +149,7 @@ OUString createStandardTypePart(const Reference< XPropertySet >& xColProp,const 
         else
         {
             nParenPos = sTypeName.indexOf(')',nParenPos);
-            aSql.append(sTypeName.copy(nParenPos));
+            aSql.appendCopy(sTypeName, nParenPos);
         }
     }
     else

connectivity/source/drivers/firebird/Clob.cxx
@@ -99,7 +99,7 @@ OUString SAL_CALL Clob::getSubString(sal_Int64 nPosition,
             if( nCharsToCopy > nLength )
                 nCharsToCopy = nLength;
             // append relevant part of first segment
-            sSegmentBuffer.append( sSegment.copy(0, nCharsToCopy ) );
+            sSegmentBuffer.appendCopy( sSegment, 0, nCharsToCopy );
             nActLen += sSegmentBuffer.getLength();
         }
     }
@@ -115,7 +115,7 @@ OUString SAL_CALL Clob::getSubString(sal_Int64 nPosition,
                             RTL_TEXTENCODING_UTF8 );
         sal_Int32 nStrLen = sSegment.getLength();
         if( nActLen + nStrLen > nLength )
-            sSegmentBuffer.append(sSegment.copy(0, nLength - nActLen) );
+            sSegmentBuffer.appendCopy(sSegment, 0, nLength - nActLen);
         else
             sSegmentBuffer.append(sSegment);
         nActLen += nStrLen;

connectivity/source/drivers/firebird/PreparedStatement.cxx
@@ -333,9 +333,9 @@ sal_Int64 toNumericWithoutDecimalPlace(const OUString& sSource)
         OUStringBuffer sBuffer(15);
         if(nDotIndex > 0)
         {
-            sBuffer.append(sNumber.copy(0, nDotIndex));
+            sBuffer.appendCopy(sNumber, 0, nDotIndex);
         }
-        sBuffer.append(sNumber.copy(nDotIndex + 1));
+        sBuffer.appendCopy(sNumber, nDotIndex + 1);
         return sBuffer.makeStringAndClear().toInt64();
     }
 }

commit 0b26426d484a2f092f56e7015b31d24d568402a9

tdf#119321: fix appendCopy in searchAndReplace (printing)
Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=b6570d211f64bc2683f86b8093a9567472e88a37

Change-Id: Iea4b21647dd01406271f9b3e13fea3cc73e1c801
Reviewed-on: https://gerrit.libreoffice.org/59303
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/window/printdlg.cxx
@@ -1343,7 +1343,7 @@ static OUString searchAndReplace( const OUString& i_rOrig, const char* i_pRepl, 
     if( nPos != -1 )
     {
         OUStringBuffer aBuf( i_rOrig.getLength() );
-        aBuf.appendCopy( i_rOrig, nPos );
+        aBuf.appendCopy( i_rOrig, 0, nPos );
         aBuf.append( i_rRepl );
         if( nPos + i_nReplLen < i_rOrig.getLength() )
             aBuf.appendCopy( i_rOrig, nPos + i_nReplLen );