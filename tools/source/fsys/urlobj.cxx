LemmingAvalanche/libreoffice-repaired

commit eed7d801bb23ece6fa62a8e14b4fc8719eb31103

use consistent #define checks for the Windows platform
stage 1 of replacing usage of various checks for the windows platform
with the compiler-defined '_WIN32' macro

Change-Id: Iece73abdee530937e0737190b1aa97a46cd3075f
Reviewed-on: https://gerrit.libreoffice.org/22390
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Stahl <mstahl@redhat.com>

tools/source/fsys/urlobj.cxx
@@ -1440,7 +1440,7 @@ bool INetURLObject::setAbsURIRef(OUString const & rTheAbsURIRef,
     }
 #endif
 
-#ifdef WIN
+#ifdef _WIN32
     if (m_eScheme==INetProtocol::Smb) {
         // Change "smb://server/path" URIs to "file://server/path"
         // URIs on Windows, since Windows doesn't understand the

tools/qa/cppunit/test_urlobj.cxx
@@ -62,7 +62,7 @@ namespace tools_urlobj
                             == "smb://10.10.1.1/sampledir/sample.file" );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::Smb );
 #endif
-#ifdef WIN
+#ifdef _WIN32
             CPPUNIT_ASSERT( aUrl.GetMainURL( INetURLObject::NO_DECODE ).
                             equalsAscii
                             ( "file://10.10.1.1/sampledir/sample.file" ) );
@@ -89,7 +89,7 @@ namespace tools_urlobj
                             == "smb://137.65.170.24/c$/Img0001.jpg" );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::Smb );
 #endif
-#ifdef WIN
+#ifdef _WIN32
             CPPUNIT_ASSERT( aUrl.GetMainURL( INetURLObject::NO_DECODE ).
                             equalsAscii( "file://137.65.170.24/c$/Img0001.jpg" ) );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::File );
@@ -115,7 +115,7 @@ namespace tools_urlobj
                             == "smb://hive-winxp-x86/pmladek/test2.odt" );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::Smb );
 #endif
-#ifdef WIN
+#ifdef _WIN32
             CPPUNIT_ASSERT( aUrl.GetMainURL( INetURLObject::NO_DECODE ).
                             equalsAscii( "file://hive-winxp-x86/pmladek/test2.odt" ) );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::File );
@@ -134,7 +134,7 @@ namespace tools_urlobj
                             == "smb://10.10.1.1/sampledir/sample.file" );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::Smb );
 #endif
-#ifdef WIN
+#ifdef _WIN32
             CPPUNIT_ASSERT( aUrl.GetMainURL( INetURLObject::NO_DECODE ).
                             equalsAscii( "file://10.10.1.1/sampledir/sample.file" ) );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::File );
@@ -158,7 +158,7 @@ namespace tools_urlobj
                             == "smb://137.65.170.24/c$/Img0001.jpg" );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::Smb );
 #endif
-#ifdef WIN
+#ifdef _WIN32
             CPPUNIT_ASSERT( aUrl.GetMainURL( INetURLObject::NO_DECODE ).
                             equalsAscii( "file://137.65.170.24/c$/Img0001.jpg" ) );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::File );
@@ -183,7 +183,7 @@ namespace tools_urlobj
                             == "smb://hive-winxp-x86/pmladek/test2.odt" );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::Smb );
 #endif
-#ifdef WIN
+#ifdef _WIN32
             CPPUNIT_ASSERT( aUrl.GetMainURL( INetURLObject::NO_DECODE ).
                             equalsAscii( "file://hive-winxp-x86/pmladek/test2.odt" ) );
             CPPUNIT_ASSERT( aUrl.GetProtocol(  ) == INetProtocol::File );

basic/source/sbx/sbxdec.cxx
@@ -34,7 +34,7 @@ SbxDecimal::SbxDecimal()
 
 SbxDecimal::SbxDecimal( const SbxDecimal& rDec )
 {
-#ifdef WIN32
+#ifdef _WIN32
     maDec = rDec.maDec;
 #else
     (void)rDec;
@@ -45,7 +45,7 @@ SbxDecimal::SbxDecimal( const SbxDecimal& rDec )
 SbxDecimal::SbxDecimal
     ( const css::bridge::oleautomation::Decimal& rAutomationDec )
 {
-#ifdef WIN32
+#ifdef _WIN32
     maDec.scale = rAutomationDec.Scale;
     maDec.sign  = rAutomationDec.Sign;
     maDec.Lo32 = rAutomationDec.LowValue;
@@ -60,7 +60,7 @@ SbxDecimal::SbxDecimal
 void SbxDecimal::fillAutomationDecimal
     ( css::bridge::oleautomation::Decimal& rAutomationDec )
 {
-#ifdef WIN32
+#ifdef _WIN32
     rAutomationDec.Scale = maDec.scale;
     rAutomationDec.Sign = maDec.sign;
     rAutomationDec.LowValue = maDec.Lo32;
@@ -88,7 +88,7 @@ void releaseDecimalPtr( SbxDecimal*& rpDecimal )
     }
 }
 
-#ifdef WIN32
+#ifdef _WIN32
 
 bool SbxDecimal::operator -= ( const SbxDecimal &r )
 {
@@ -351,7 +351,7 @@ bool SbxDecimal::getDouble( double& rVal )      { (void)rVal; return false; }
 
 void SbxDecimal::getString( OUString& rString )
 {
-#ifdef WIN32
+#ifdef _WIN32
     static LCID nLANGID = MAKELANGID( LANG_ENGLISH, SUBLANG_ENGLISH_US );
 
     bool bRet = false;

basic/source/sbx/sbxdec.hxx
@@ -45,7 +45,7 @@ class SbxDecimal
 {
     friend void releaseDecimalPtr( SbxDecimal*& rpDecimal );
 
-#ifdef WIN32
+#ifdef _WIN32
     DECIMAL     maDec;
 #endif
     sal_Int32       mnRefCount;
@@ -75,7 +75,7 @@ public:
     bool setString( OUString* pOUString );
     void setDecimal( SbxDecimal* pDecimal )
     {
-#ifdef WIN32
+#ifdef _WIN32
         if( pDecimal )
             maDec = pDecimal->maDec;
 #else

connectivity/source/drivers/mozab/bootstrap/MNSFolders.cxx
@@ -45,7 +45,7 @@ namespace
         ::osl::Security   aSecurity;
         OUString   aConfigPath;
 
-    #if defined(XP_WIN) || defined(MACOSX)
+    #if defined(_WIN32) || defined(MACOSX)
         aSecurity.getConfigDir( aConfigPath );
     #else
         //This is to find the dir under which .mozilla/.thunderbird is created.
@@ -67,7 +67,7 @@ namespace
     // DO NOT CHANGE THE ORDER; ADD ONLY TO THE END
     static const char* DefaultProductDir[NB_PRODUCTS][NB_CANDIDATES] =
     {
-    #if defined(XP_WIN)
+    #if defined(_WIN32)
         { "Mozilla/SeaMonkey/", NULL, NULL, NULL },
         { "Mozilla/Firefox/", NULL, NULL, NULL },
         { "Thunderbird/", "Mozilla/Thunderbird/", NULL, NULL }

cppuhelper/test/bootstrap/bootstrap.test.cxx
@@ -88,7 +88,7 @@ static void s_test__loadSharedLibComponentFactory(rtl::OUString const & clientPu
     uno::Reference<uno::XInterface> xItf(
         cppu::loadSharedLibComponentFactory(
             rtl::OUString(SAL_DLLPREFIX "TestComponent.uno" SAL_DLLEXTENSION),
-#ifdef WIN32
+#ifdef _WIN32
             "",
 #else
             "file://../lib/",

desktop/source/app/cmdlineargs.cxx
@@ -299,7 +299,7 @@ void CommandLineArgs::ParseCommandLine_Impl( Supplier& supplier )
                 exit(0);
             }
 #endif
-#ifdef WIN32
+#ifdef _WIN32
             /* fdo#57203 ignore -Embedding on Windows
                when LibreOffice is launched by COM+
             */

desktop/win32/source/applauncher/launcher.cxx
@@ -19,17 +19,7 @@
 
 #include "launcher.hxx"
 
-#ifndef _WINDOWS_
-#   define WIN32_LEAN_AND_MEAN
-#if defined _MSC_VER
-#pragma warning(push, 1)
-#endif
-#   include <windows.h>
-#   include <shellapi.h>
-#if defined _MSC_VER
-#pragma warning(pop)
-#endif
-#endif
+#include <shellapi.h>
 
 #include <stdlib.h>
 #include <malloc.h>

commit 7db6d348081f4a9a78f3b3b1e31263b6e5642b61

tdf#119326 crash when adding "Windows Share" File resource
this was a regression from

    commit eed7d801bb23ece6fa62a8e14b4fc8719eb31103
    use consistent #define checks for the Windows platform

where I converted
    #ifdef WIN
to
    #ifdef _WIN32

But that was already dead code at that point since we did not define
that preprocessor constant anywhere.

Change-Id: Ieadafd61fada05fc19d04d83992fba7c42969daa
Reviewed-on: https://gerrit.libreoffice.org/59402
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

tools/source/fsys/urlobj.cxx
@@ -1455,15 +1455,6 @@ bool INetURLObject::setAbsURIRef(OUString const & rTheAbsURIRef,
     }
 #endif
 
-#ifdef _WIN32
-    if (m_eScheme==INetProtocol::Smb) {
-        // Change "smb://server/path" URIs to "file://server/path"
-        // URIs on Windows, since Windows doesn't understand the
-        // SMB scheme.
-        changeScheme(INetProtocol::File);
-    }
-#endif
-
     return true;
 }
 

tools/qa/cppunit/test_urlobj.cxx
@@ -54,11 +54,6 @@ namespace tools_urlobj
                                  aUrl.GetMainURL(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(INetProtocol::Smb, aUrl.GetProtocol());
 #endif
-#ifdef _WIN32
-            CPPUNIT_ASSERT_EQUAL( OUString("file://10.10.1.1/sampledir/sample.file"),
-                            aUrl.GetMainURL( INetURLObject::DecodeMechanism::NONE ) );
-            CPPUNIT_ASSERT_EQUAL( INetProtocol::File, aUrl.GetProtocol(  ) );
-#endif
             CPPUNIT_ASSERT_EQUAL(OUString("10.10.1.1"),
                                  aUrl.GetHost(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(OUString("/sampledir/sample.file"),
@@ -79,11 +74,6 @@ namespace tools_urlobj
             CPPUNIT_ASSERT_EQUAL(OUString("smb://137.65.170.24/c$/Img0001.jpg"),
                                  aUrl.GetMainURL(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(INetProtocol::Smb, aUrl.GetProtocol());
-#endif
-#ifdef _WIN32
-            CPPUNIT_ASSERT_EQUAL( OUString("file://137.65.170.24/c$/Img0001.jpg"),
-                            aUrl.GetMainURL( INetURLObject::DecodeMechanism::NONE ) );
-            CPPUNIT_ASSERT_EQUAL( INetProtocol::File, aUrl.GetProtocol(  ) );
 #endif
             CPPUNIT_ASSERT_EQUAL(OUString("137.65.170.24"),
                                  aUrl.GetHost(INetURLObject::DecodeMechanism::NONE));
@@ -106,11 +96,6 @@ namespace tools_urlobj
                                  aUrl.GetMainURL(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(INetProtocol::Smb, aUrl.GetProtocol());
 #endif
-#ifdef _WIN32
-            CPPUNIT_ASSERT_EQUAL( OUString("file://hive-winxp-x86/pmladek/test2.odt"),
-                            aUrl.GetMainURL( INetURLObject::DecodeMechanism::NONE ) );
-            CPPUNIT_ASSERT_EQUAL( INetProtocol::File, aUrl.GetProtocol(  ) );
-#endif
             CPPUNIT_ASSERT_EQUAL(OUString("hive-winxp-x86"),
                                  aUrl.GetHost(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(OUString("/pmladek/test2.odt"),
@@ -124,11 +109,6 @@ namespace tools_urlobj
             CPPUNIT_ASSERT_EQUAL(OUString("smb://10.10.1.1/sampledir/sample.file"),
                                  aUrl.GetMainURL(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL( INetProtocol::Smb, aUrl.GetProtocol(  ) );
-#endif
-#ifdef _WIN32
-            CPPUNIT_ASSERT_EQUAL( OUString("file://10.10.1.1/sampledir/sample.file"),
-                            aUrl.GetMainURL( INetURLObject::DecodeMechanism::NONE ) );
-            CPPUNIT_ASSERT_EQUAL( INetProtocol::File, aUrl.GetProtocol(  ) );
 #endif
             CPPUNIT_ASSERT_EQUAL(OUString("10.10.1.1"),
                                  aUrl.GetHost(INetURLObject::DecodeMechanism::NONE));
@@ -149,11 +129,6 @@ namespace tools_urlobj
                                  aUrl.GetMainURL(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(INetProtocol::Smb, aUrl.GetProtocol());
 #endif
-#ifdef _WIN32
-            CPPUNIT_ASSERT_EQUAL( OUString("file://137.65.170.24/c$/Img0001.jpg"),
-                            aUrl.GetMainURL( INetURLObject::DecodeMechanism::NONE ) );
-            CPPUNIT_ASSERT_EQUAL( INetProtocol::File, aUrl.GetProtocol(  ) );
-#endif
             CPPUNIT_ASSERT_EQUAL(OUString("137.65.170.24"),
                                  aUrl.GetHost(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(OUString("/c$/Img0001.jpg"),
@@ -173,11 +148,6 @@ namespace tools_urlobj
             CPPUNIT_ASSERT_EQUAL(OUString("smb://hive-winxp-x86/pmladek/test2.odt"),
                                  aUrl.GetMainURL(INetURLObject::DecodeMechanism::NONE));
             CPPUNIT_ASSERT_EQUAL(INetProtocol::Smb, aUrl.GetProtocol());
-#endif
-#ifdef _WIN32
-            CPPUNIT_ASSERT_EQUAL( OUString("file://hive-winxp-x86/pmladek/test2.odt"),
-                            aUrl.GetMainURL( INetURLObject::DecodeMechanism::NONE ) );
-            CPPUNIT_ASSERT_EQUAL( INetProtocol::File, aUrl.GetProtocol(  ) );
 #endif
             CPPUNIT_ASSERT_EQUAL(OUString("hive-winxp-x86"),
                                  aUrl.GetHost(INetURLObject::DecodeMechanism::NONE));