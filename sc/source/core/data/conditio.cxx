LemmingAvalanche/libreoffice-repaired

commit 8608381324d95a95de0d36fcd4ee4dc47a071525

Avoid temporary Date and normalization
Rather than possibly calculating Date=((Date=(Date+int))+int) with a
normalization involved for each operator+() force Date=(Date+(int+int))

Change-Id: I6c4b30749d138c0b041ffc9e37796b6f64b6d461

sc/source/core/data/conditio.cxx
@@ -1649,8 +1649,8 @@ bool ScCondDateFormatEntry::IsValid( const ScAddress& rPos ) const
                 const DayOfWeek eDay = rActDate.GetDayOfWeek();
                 if( eDay != SUNDAY )
                 {
-                    Date aBegin(rActDate - 8 - static_cast<sal_Int32>(eDay));
-                    Date aEnd(rActDate - 2 - static_cast<sal_Int32>(eDay));
+                    Date aBegin(rActDate - (8 + static_cast<sal_Int32>(eDay)));
+                    Date aEnd(rActDate - (2 + static_cast<sal_Int32>(eDay)));
                     return aCellDate.IsBetween( aBegin, aEnd );
                 }
                 else
@@ -1666,8 +1666,8 @@ bool ScCondDateFormatEntry::IsValid( const ScAddress& rPos ) const
                 const DayOfWeek eDay = rActDate.GetDayOfWeek();
                 if( eDay != SUNDAY )
                 {
-                    Date aBegin(rActDate - 1 - static_cast<sal_Int32>(eDay));
-                    Date aEnd(rActDate + 5 - static_cast<sal_Int32>(eDay));
+                    Date aBegin(rActDate - (1 + static_cast<sal_Int32>(eDay)));
+                    Date aEnd(rActDate + (5 + static_cast<sal_Int32>(eDay)));
                     return aCellDate.IsBetween( aBegin, aEnd );
                 }
                 else
@@ -1682,8 +1682,8 @@ bool ScCondDateFormatEntry::IsValid( const ScAddress& rPos ) const
                 const DayOfWeek eDay = rActDate.GetDayOfWeek();
                 if( eDay != SUNDAY )
                 {
-                    return aCellDate.IsBetween( rActDate + 6 - static_cast<sal_Int32>(eDay),
-                            rActDate + 12 - static_cast<sal_Int32>(eDay) );
+                    return aCellDate.IsBetween( rActDate + (6 - static_cast<sal_Int32>(eDay)),
+                            rActDate + (12 - static_cast<sal_Int32>(eDay)) );
                 }
                 else
                 {

commit f812aefeee3e85dd3dba553b899c3385aef0ef95

Resolves: tdf#119402 +- typo sign flip
Regression from

    commit 8608381324d95a95de0d36fcd4ee4dc47a071525
    CommitDate: Wed Jul 19 20:13:58 2017 +0200

        Avoid temporary Date and normalization

that introduced a sign flip
from        +  5 - eDay
to          + (5 + eDay)
instead of  + (5 - eDay)

Change-Id: I28194296b20552135e6abd24eb9eefe8634ce7c0
Reviewed-on: https://gerrit.libreoffice.org/59461
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/source/core/data/conditio.cxx
@@ -1619,7 +1619,7 @@ bool ScCondDateFormatEntry::IsValid( const ScAddress& rPos ) const
                 if( eDay != SUNDAY )
                 {
                     Date aBegin(rActDate - (1 + static_cast<sal_Int32>(eDay)));
-                    Date aEnd(rActDate + (5 + static_cast<sal_Int32>(eDay)));
+                    Date aEnd(rActDate + (5 - static_cast<sal_Int32>(eDay)));
                     return aCellDate.IsBetween( aBegin, aEnd );
                 }
                 else