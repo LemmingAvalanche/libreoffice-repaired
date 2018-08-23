LemmingAvalanche/libreoffice-repaired

commit f9720d9559e9bee8b8a9915cb0a0359a83b38762

loplugin:unused-returns in vcl
Change-Id: I507320900a47f604d17ed7d402d531a7cbdf744e
Reviewed-on: https://gerrit.libreoffice.org/48331
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/generic/print/glyphset.cxx
@@ -58,15 +58,15 @@ GlyphSet::~GlyphSet ()
     /* FIXME delete the glyphlist ??? */
 }
 
-bool
+void
 GlyphSet::GetGlyphID (
                       sal_GlyphId nGlyph,
                       unsigned char* nOutGlyphID,
                       sal_Int32* nOutGlyphSetID
                      )
 {
-    return    LookupGlyphID (nGlyph, nOutGlyphID, nOutGlyphSetID)
-           || AddGlyphID    (nGlyph, nOutGlyphID, nOutGlyphSetID);
+    if (LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
+           AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);
 }
 
 bool

commit e67274513518cbd3a12027c61bad948f91a980ee

tdf#119357 add the glyph, if the lookup failed
Regression from commit f9720d9559e9 ("loplugin:unused-returns in
vcl") changing the logic in hunk:

-    return    LookupGlyphID (nGlyph, nOutGlyphID, nOutGlyphSetID)
-           || AddGlyphID    (nGlyph, nOutGlyphID, nOutGlyphSetID);
+    if (LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
+           AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);

Change-Id: Id643120e1cf5e26b3ffda933d07893048493f089
Reviewed-on: https://gerrit.libreoffice.org/59493
Tested-by: Xisco Faulí <xiscofauli@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/unx/generic/print/glyphset.cxx
@@ -65,8 +65,8 @@ GlyphSet::GetGlyphID (
                       sal_Int32* nOutGlyphSetID
                      )
 {
-    if (LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
-           AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);
+    if (!LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
+        AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);
 }
 
 bool