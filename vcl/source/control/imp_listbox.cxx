LemmingAvalanche/libreoffice-repaired

commit 27e31d8821a71d66865ca9d45f450edafd5415f8

Fix list box focused text color
This ignores the rollover state for text colors if the list box
has the focus and just uses the highlight text color.

This is visible in the "gen" VCL plugin and on Windows, e.g.
when the focus is in the Math symbol list box.

Change-Id: I00602e2a2601060f8588e474380dfa9af64a9094
Reviewed-on: https://gerrit.libreoffice.org/59488
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/control/imp_listbox.cxx
@@ -2690,12 +2690,7 @@ void ImplWin::ImplDraw(vcl::RenderContext& rRenderContext, bool bLayout)
                     rRenderContext.SetLineColor();
                     rRenderContext.SetFillColor();
                 }
-                Color aColor;
-                if( bNativeOK && (nState & ControlState::ROLLOVER) )
-                    aColor = rStyleSettings.GetFieldRolloverTextColor();
-                else
-                    aColor = rStyleSettings.GetFieldTextColor();
-                rRenderContext.SetTextColor( aColor );
+                rRenderContext.SetTextColor( rStyleSettings.GetHighlightTextColor() );
                 rRenderContext.DrawRect( maFocusRect );
             }
             else

commit 5ba4a8ae5045a840937dbdf8ab8ccccd67b4a898

Just set highlight color for focus rect list box
Hopefully fixes the regression from commit 27e31d8821a7 ("Fix
list box focused text color"), so it won't re-introduce
tdf#109353.

Change-Id: I7107deb33b959a250da9051b527099d723914223
Reviewed-on: https://gerrit.libreoffice.org/59549
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/control/imp_listbox.cxx
@@ -2684,13 +2684,16 @@ void ImplWin::ImplDraw(vcl::RenderContext& rRenderContext, bool bLayout)
             if (bHasFocus && !ImplGetSVData()->maNWFData.mbDDListBoxNoTextArea)
             {
                 if ( !ImplGetSVData()->maNWFData.mbNoFocusRects )
+                {
                     rRenderContext.SetFillColor( rStyleSettings.GetHighlightColor() );
+                    rRenderContext.SetTextColor( rStyleSettings.GetHighlightTextColor() );
+                }
                 else
                 {
                     rRenderContext.SetLineColor();
                     rRenderContext.SetFillColor();
+                    rRenderContext.SetTextColor( rStyleSettings.GetFieldTextColor() );
                 }
-                rRenderContext.SetTextColor( rStyleSettings.GetHighlightTextColor() );
                 rRenderContext.DrawRect( maFocusRect );
             }
             else