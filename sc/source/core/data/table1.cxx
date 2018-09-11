LemmingAvalanche/libreoffice-repaired

commit dc8ed35a356de890edb9a60857acfa90a664d888

tdf#69977 improve creation of large charts
this patch only tweaks a few things around the edges. The bulk of the
cost is down in the chart2 module where it creates ticks and labels.

Change-Id: If73a16d2f0a7511f07eff379a9b5c1b38ef96786
Reviewed-on: https://gerrit.libreoffice.org/51931
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/data/table1.cxx
@@ -1133,8 +1133,10 @@ void ScTable::LimitChartArea( SCCOL& rStartCol, SCROW& rStartRow, SCCOL& rEndCol
     while ( rStartRow<rEndRow && IsEmptyLine(rStartRow, rStartCol, rEndCol) )
         ++rStartRow;
 
-    while ( rStartRow<rEndRow && IsEmptyLine(rEndRow, rStartCol, rEndCol) )
-        --rEndRow;
+    // Optimised loop for finding the bottom of the area, can be costly in large
+    // spreadsheets.
+    for (SCCOL i=rStartCol; i<=rEndCol; i++)
+        rEndRow = std::min(rEndRow, aCol[i].GetLastDataPos());
 }
 
 SCCOL ScTable::FindNextVisibleCol( SCCOL nCol, bool bRight ) const

svl/source/items/itempool.cxx
@@ -86,14 +86,17 @@ do { \
 
 void SfxItemPool::AddSfxItemPoolUser(SfxItemPoolUser& rNewUser)
 {
-    pImpl->maSfxItemPoolUsers.push_back(&rNewUser);
+    // maintain sorted to reduce cost of remove
+    const auto insertIt = ::std::lower_bound(
+        pImpl->maSfxItemPoolUsers.begin(), pImpl->maSfxItemPoolUsers.end(), &rNewUser);
+    pImpl->maSfxItemPoolUsers.insert(insertIt, &rNewUser);
 }
 
 void SfxItemPool::RemoveSfxItemPoolUser(SfxItemPoolUser& rOldUser)
 {
-    const std::vector<SfxItemPoolUser*>::iterator aFindResult = ::std::find(
+    const auto aFindResult = ::std::lower_bound(
         pImpl->maSfxItemPoolUsers.begin(), pImpl->maSfxItemPoolUsers.end(), &rOldUser);
-    if(aFindResult != pImpl->maSfxItemPoolUsers.end())
+    if(aFindResult != pImpl->maSfxItemPoolUsers.end() && *aFindResult == &rOldUser)
     {
         pImpl->maSfxItemPoolUsers.erase(aFindResult);
     }

commit 9aa32678662c40d7909c6bc51a4053a9c87c666d

tdf#119305 Chart created from fully selected sheet only targets header row
regression from
    commit dc8ed35a356de890edb9a60857acfa90a664d888
    tdf#69977 improve creation of large charts

Change-Id: I09d02447b0e4691e98a44222608a3dc5edc95b0b
Reviewed-on: https://gerrit.libreoffice.org/60318
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/data/table1.cxx
@@ -1123,8 +1123,10 @@ void ScTable::LimitChartArea( SCCOL& rStartCol, SCROW& rStartRow, SCCOL& rEndCol
 
     // Optimised loop for finding the bottom of the area, can be costly in large
     // spreadsheets.
+    SCROW lastDataPos = 0;
     for (SCCOL i=rStartCol; i<=rEndCol; i++)
-        rEndRow = std::min(rEndRow, aCol[i].GetLastDataPos());
+        lastDataPos = std::max(lastDataPos, aCol[i].GetLastDataPos());
+    rEndRow = std::min(rEndRow, lastDataPos);
 }
 
 SCCOL ScTable::FindNextVisibleCol( SCCOL nCol, bool bRight ) const