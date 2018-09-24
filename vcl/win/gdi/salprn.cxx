LemmingAvalanche/libreoffice-repaired

commit 440870573d449a53c8c1abc59c9cc927ad1c47d6

WIN use Reschedule instead of own dispatch loops
Since we're filtering the wakeup timer event in the main dispatch
loop, we should use Application::Reschedule in the Backend.

Change-Id: Ie02c3533e8a6a7905281f129489e4f6f53f74692

vcl/win/gdi/salprn.cxx
@@ -1288,24 +1288,12 @@ BOOL CALLBACK SalPrintAbortProc( HDC hPrnDC, int /* nError */ )
 {
     SalData*    pSalData = GetSalData();
     WinSalPrinter* pPrinter;
-    bool        bWhile = TRUE;
-    int         i = 0;
+    bool        bWhile = true;
 
     do
     {
         // process messages
-        MSG aMsg;
-        if ( PeekMessageW( &aMsg, nullptr, 0, 0, PM_REMOVE ) )
-        {
-            TranslateMessage( &aMsg );
-            DispatchMessageW( &aMsg );
-
-            i++;
-            if ( i > 15 )
-                bWhile = FALSE;
-        }
-        else
-            bWhile = FALSE;
+        bWhile = Application::Reschedule( true );
 
         pPrinter = pSalData->mpFirstPrinter;
         while ( pPrinter )
@@ -1468,25 +1456,7 @@ bool WinSalPrinter::StartJob( const OUString* pFileName,
 
     // As the Telecom Balloon Fax driver tends to send messages repeatedly
     // we try to process first all, and then insert a dummy message
-    bool bWhile = TRUE;
-    int  i = 0;
-    do
-    {
-        // process messages
-        MSG aMsg;
-        if ( PeekMessageW( &aMsg, nullptr, 0, 0, PM_REMOVE ) )
-        {
-            TranslateMessage( &aMsg );
-            DispatchMessageW( &aMsg );
-
-            i++;
-            if ( i > 15 )
-                bWhile = FALSE;
-        }
-        else
-            bWhile = FALSE;
-    }
-    while ( bWhile );
+    while ( Application::Reschedule( true ) );
     BOOL const ret = PostMessageW(GetSalData()->mpFirstInstance->mhComWnd, SAL_MSG_DUMMY, 0, 0);
     SAL_WARN_IF(0 == ret, "vcl", "ERROR: PostMessage() failed!");
 

vcl/win/app/salinst.cxx
@@ -732,6 +732,7 @@ LRESULT CALLBACK SalComWndProc( HWND, UINT nMsg, WPARAM wParam, LPARAM lParam, i
             while ( PeekMessageW(&aMsg, nullptr, SAL_MSG_TIMER_CALLBACK,
                                  SAL_MSG_TIMER_CALLBACK, PM_REMOVE) )
                 assert( "Multiple timer messages in queue" );
+            assert( 0 == wParam );
             if ( 0 == wParam )
                 EmitTimerCallback();
             break;

vcl/win/window/salframe.cxx
@@ -5705,14 +5705,7 @@ LRESULT CALLBACK SalFrameWndProc( HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
                 // messages in the message queue and dispatch them before we return control to the system.
 
                 if ( nRet )
-                {
-                    MSG msg;
-
-                    while( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE ) )
-                    {
-                        DispatchMessage( &msg );
-                    }
-                }
+                    while ( Application::Reschedule( true ) );
             }
             else
             {

commit 5a715ed0eff5088c1cf456d86d9b65b199f15344

tdf#118690 don't try to process all messages
Regression from commit 440870573d44 ("WIN use Reschedule instead
of own dispatch loops"). This limit the maximum reschedule loops
as the original code did. There is still a busy loop when opening
the print dialog, as it was before the patch, but this at least
gets printing started.

Change-Id: If867329b82b13c898bb15b08584244084488a66c
Reviewed-on: https://gerrit.libreoffice.org/60310
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/win/gdi/salprn.cxx
@@ -1287,6 +1287,7 @@ static BOOL CALLBACK SalPrintAbortProc( HDC hPrnDC, int /* nError */ )
 {
     SalData*    pSalData = GetSalData();
     WinSalPrinter* pPrinter;
+    int         i = 0;
     bool        bWhile = true;
 
     // Ensure we handle the mutex which will be released in WinSalInstance::DoYield
@@ -1295,6 +1296,10 @@ static BOOL CALLBACK SalPrintAbortProc( HDC hPrnDC, int /* nError */ )
     {
         // process messages
         bWhile = Application::Reschedule( true );
+        if (i > 15)
+            bWhile = false;
+        else
+            ++i;
 
         pPrinter = pSalData->mpFirstPrinter;
         while ( pPrinter )
@@ -1457,7 +1462,7 @@ bool WinSalPrinter::StartJob( const OUString* pFileName,
 
     // As the Telecom Balloon Fax driver tends to send messages repeatedly
     // we try to process first all, and then insert a dummy message
-    while ( Application::Reschedule( true ) );
+    for (int i = 0; Application::Reschedule( true ) && i <= 15; ++i);
     BOOL const ret = PostMessageW(GetSalData()->mpInstance->mhComWnd, SAL_MSG_DUMMY, 0, 0);
     SAL_WARN_IF(0 == ret, "vcl", "ERROR: PostMessage() failed!");
 