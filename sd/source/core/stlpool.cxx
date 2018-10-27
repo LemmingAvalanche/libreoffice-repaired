LemmingAvalanche/libreoffice-repaired

commit b99b5912394121c20ff00ce44b1c4ea24a512df1

Removing old SfxItemSet::getHash usage
Change-Id: I902da0f6e00253d5daf4bf65784dade2ae02740c
Reviewed-on: https://gerrit.libreoffice.org/41155
Reviewed-by: Michael Stahl <mstahl@redhat.com>
Tested-by: Jenkins <ci@libreoffice.org>

sd/source/core/stlpool.cxx
@@ -651,9 +651,8 @@ void SdStyleSheetPool::CopySheets(SdStyleSheetPool& rSourcePool, SfxStyleFamily 
             // if we have a rename suffix, try to find a new name
             pExistingSheet =
                 GetStyleSheetByPositionInIndex(aSheetsWithName.front()).get();
-            sal_Int32 nHash = xSheet->GetItemSet().getHash();
             if (!rRenameSuffix.isEmpty() &&
-                pExistingSheet->GetItemSet().getHash() != nHash)
+                pExistingSheet->GetItemSet().Equals(xSheet->GetItemSet(), false))
             {
                 // we have found a sheet with the same name, but different contents. Try to find a new name.
                 // If we already have a sheet with the new name, and it is equal to the one in the source pool,
@@ -665,7 +664,7 @@ void SdStyleSheetPool::CopySheets(SdStyleSheetPool& rSourcePool, SfxStyleFamily 
                     aTmpName = aName + rRenameSuffix + OUString::number(nSuffix);
                     pExistingSheet = Find(aTmpName, eFamily);
                     nSuffix++;
-                } while( pExistingSheet && pExistingSheet->GetItemSet().getHash() != nHash );
+                } while( pExistingSheet && pExistingSheet->GetItemSet().Equals(xSheet->GetItemSet(), false) );
                 aName = aTmpName;
                 bAddToList = true;
             }

commit 4f3a2e6f97b3c43c501d7603618970bd1712375c

tdf#120376 sd: fix duplicated styles on copy/paste
Unfortunately the comparison was inverted, so a style is copied
iff it already exists, which is clearly the reviewer's fault...

(regression from b99b5912394121c20ff00ce44b1c4ea24a512df1)

Change-Id: I3425982feb08e980eca9243cc16120897b65a70f
Reviewed-on: https://gerrit.libreoffice.org/62436
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

sd/source/core/stlpool.cxx
@@ -651,7 +651,7 @@ void SdStyleSheetPool::CopySheets(SdStyleSheetPool& rSourcePool, SfxStyleFamily 
             pExistingSheet =
                 GetStyleSheetByPositionInIndex(aSheetsWithName.front());
             if (!rRenameSuffix.isEmpty() &&
-                pExistingSheet->GetItemSet().Equals(pSheet->GetItemSet(), false))
+                !pExistingSheet->GetItemSet().Equals(pSheet->GetItemSet(), false))
             {
                 // we have found a sheet with the same name, but different contents. Try to find a new name.
                 // If we already have a sheet with the new name, and it is equal to the one in the source pool,
@@ -663,7 +663,8 @@ void SdStyleSheetPool::CopySheets(SdStyleSheetPool& rSourcePool, SfxStyleFamily 
                     aTmpName = aName + rRenameSuffix + OUString::number(nSuffix);
                     pExistingSheet = Find(aTmpName, eFamily);
                     nSuffix++;
-                } while( pExistingSheet && pExistingSheet->GetItemSet().Equals(pSheet->GetItemSet(), false) );
+                } while (pExistingSheet &&
+                        !pExistingSheet->GetItemSet().Equals(pSheet->GetItemSet(), false));
                 aName = aTmpName;
                 bAddToList = true;
             }