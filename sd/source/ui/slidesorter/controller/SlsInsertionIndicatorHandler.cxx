LemmingAvalanche/libreoffice-repaired

commit 35d58caa77d2e69328aabd1a499adc0a5428d0f0

clang-tidy readability-simplify-boolean-expr
Change-Id: Iea7ab64683f0b29794d50d774cc482b54a00e70a
Reviewed-on: https://gerrit.libreoffice.org/36450
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/slidesorter/controller/SlsInsertionIndicatorHandler.cxx
@@ -220,10 +220,7 @@ bool InsertionIndicatorHandler::IsInsertionTrivial (
     // to check that the insertion position is not directly in front or
     // directly behind the selection and thus moving the selection there
     // would not change the model.
-    if (nInsertionIndex<nFirstIndex || nInsertionIndex>(nLastIndex+1))
-        return false;
-
-    return true;
+    return nInsertionIndex >= nFirstIndex && nInsertionIndex < nLastIndex;
 }
 
 bool InsertionIndicatorHandler::IsInsertionTrivial (const sal_Int8 nDndAction)

drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -204,12 +204,7 @@ namespace drawinglayer
             aLocalPolygon.transform(maCurrentTransformation);
 
             // try drawing; if it did not work, use standard fallback
-            if(mpOutputDevice->DrawPolyLineDirect( aLocalPolygon, 0.0, fTransparency))
-            {
-                return true;
-            }
-
-            return false;
+            return mpOutputDevice->DrawPolyLineDirect( aLocalPolygon, 0.0, fTransparency);
         }
 
         bool VclPixelProcessor2D::tryDrawPolygonStrokePrimitive2DDirect(const drawinglayer::primitive2d::PolygonStrokePrimitive2D& rSource, double fTransparency)

oox/source/export/chartexport.cxx
@@ -120,9 +120,7 @@ namespace {
 bool isPrimaryAxes(sal_Int32 nIndex)
 {
     assert(nIndex == 0 || nIndex == 1);
-    if (nIndex == 1)
-        return false;
-    return true;
+    return nIndex != 1;
 }
 
 }

scripting/source/provider/MasterScriptProvider.cxx
@@ -55,16 +55,11 @@ using namespace ::sf_misc;
 namespace func_provider
 {
 
-bool endsWith( const OUString& target,
-    const OUString& item )
+bool endsWith( const OUString& target, const OUString& item )
 {
-    sal_Int32 index = 0;
-    if (  ( index = target.indexOf( item ) ) != -1  &&
-       ( index == ( target.getLength() - item.getLength() ) ) )
-    {
-        return true;
-    }
-    return false;
+    sal_Int32 index = target.indexOf( item );
+    return index != -1  &&
+           index == ( target.getLength() - item.getLength() );
 }
 
 /* should be available in some central location. */
@@ -607,30 +602,17 @@ MasterScriptProvider::removeByName( const OUString& Name )
 
 
 void SAL_CALL
-MasterScriptProvider::replaceByName( const OUString& aName, const Any& aElement )
+MasterScriptProvider::replaceByName( const OUString& /*aName*/, const Any& /*aElement*/ )
 {
-    (void)aName;
-    (void)aElement;
-
     // TODO needs implementing
-    if ( true )
-    {
-        throw RuntimeException( "replaceByName not implemented!!!!" );
-    }
+     throw RuntimeException( "replaceByName not implemented!!!!" );
 }
 
 Any SAL_CALL
-MasterScriptProvider::getByName( const OUString& aName )
+MasterScriptProvider::getByName( const OUString& /*aName*/ )
 {
-    (void)aName;
-
     // TODO needs to be implemented
-    Any result;
-    if ( true )
-    {
-        throw RuntimeException( "getByName not implemented!!!!" );
-    }
-    return result;
+    throw RuntimeException( "getByName not implemented!!!!" );
 }
 
 sal_Bool SAL_CALL
@@ -701,12 +683,7 @@ Sequence< OUString > SAL_CALL
 MasterScriptProvider::getElementNames(  )
 {
     // TODO needs implementing
-    Sequence< OUString >  names;
-    if ( true )
-    {
-        throw RuntimeException( "getElementNames not implemented!!!!" );
-    }
-    return names;
+    throw RuntimeException( "getElementNames not implemented!!!!" );
 }
 
 Type SAL_CALL
@@ -720,11 +697,7 @@ MasterScriptProvider::getElementType(  )
 sal_Bool SAL_CALL MasterScriptProvider::hasElements(  )
 {
     // TODO needs implementing
-    if ( true )
-    {
-        throw RuntimeException( "hasElements not implemented!!!!" );
-    }
-    return false;
+    throw RuntimeException( "hasElements not implemented!!!!" );
 }
 
 

scripting/source/vbaevents/eventhelper.cxx
@@ -87,18 +87,12 @@ static const sal_Int32 DELIMLEN = strlen(DELIM);
 
 bool isKeyEventOk( awt::KeyEvent& evt, const Sequence< Any >& params )
 {
-    if ( !( params.getLength() > 0 ) ||
-        !( params[ 0 ] >>= evt ) )
-        return false;
-    return true;
+    return ( params.getLength() > 0 ) && ( params[ 0 ] >>= evt );
 }
 
 bool isMouseEventOk( awt::MouseEvent& evt, const Sequence< Any >& params )
 {
-    if ( !( params.getLength() > 0 ) ||
-        !( params[ 0 ] >>= evt ) )
-        return false;
-    return true;
+    return ( params.getLength() > 0 ) && ( params[ 0 ] >>= evt );
 }
 
 Sequence< Any > ooMouseEvtToVBADblClick( const Sequence< Any >& params )
@@ -793,14 +787,7 @@ bool DenyMouseDrag(const ScriptEvent& evt, void const * )
 {
     awt::MouseEvent aEvent;
     evt.Arguments[ 0 ] >>= aEvent;
-    if (aEvent.Buttons == 0 )
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return aEvent.Buttons == 0;
 }
 
 

sd/source/core/drawdoc2.cxx
@@ -216,10 +216,7 @@ bool SdDrawDocument::IsPageNameUnique( const OUString& rPgName ) const
         nPage++;
     }
 
-    if (nCount == 1)
-        return true;
-    else
-        return false;
+    return nCount == 1;
 }
 
 SdPage* SdDrawDocument::GetSdPage(sal_uInt16 nPgNum, PageKind ePgKind) const

sd/source/filter/html/htmlex.cxx
@@ -2945,10 +2945,7 @@ bool HtmlExport::CreateASPScripts()
             return false;
     }
 
-    if (!CopyScript(maExportPath, "edit.asp", maIndex))
-        return false;
-
-    return true;
+    return CopyScript(maExportPath, "edit.asp", maIndex);
 }
 
 static const char *PERL_Scripts[] = { "webcast.pl", "common.pl", "editpic.pl", "poll.pl", "savepic.pl", "show.pl" };

sd/source/ui/dlg/LayerTabBar.cxx
@@ -287,7 +287,7 @@ void LayerTabBar::EndRenaming()
 
 void LayerTabBar::ActivatePage()
 {
-    if ( /*IsInSwitching*/ true && pDrViewSh!=nullptr)
+    if (pDrViewSh!=nullptr)
     {
 
         SfxDispatcher* pDispatcher = pDrViewSh->GetViewFrame()->GetDispatcher();

commit e93580f13d0fd988d3e366ee5948d766803be82b

tdf#121198: fix crash in SdrPage::GetPageNum()
Regression from 35d58caa77d2e69328aabd1a499adc0a5428d0f0

Change-Id: Ibf9d6b65f0dbcc3a751e3ac5f3dbc28d908d6c25
Reviewed-on: https://gerrit.libreoffice.org/62952
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sd/source/ui/slidesorter/controller/SlsInsertionIndicatorHandler.cxx
@@ -220,7 +220,7 @@ bool InsertionIndicatorHandler::IsInsertionTrivial (
     // to check that the insertion position is not directly in front or
     // directly behind the selection and thus moving the selection there
     // would not change the model.
-    return nInsertionIndex >= nFirstIndex && nInsertionIndex < nLastIndex;
+    return nInsertionIndex >= nFirstIndex && nInsertionIndex <= (nLastIndex+1);
 }
 
 bool InsertionIndicatorHandler::IsInsertionTrivial (const sal_Int8 nDndAction)