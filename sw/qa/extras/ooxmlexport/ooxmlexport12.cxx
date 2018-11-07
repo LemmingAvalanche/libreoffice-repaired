LemmingAvalanche/libreoffice-repaired

commit 2e0121c75f2293358567c40c4d3186414121819c

tdf#117137: DOCX import: don't try to set grab-bag as UNO prop
This modifies commit cf0c0488dd821cd3287fcf304148bbf7c983b9c3
"DOCX import: don't try to set grab-gag props as UNO props"

Perhaps that commit should simply be reverted, but I will trust
that the commit was mostly OK and simply adjust the logic so
that *InteropGrabBag is ignored as before.

Doing this resolves MSO being unable to open a specific document
and LO missing some numbering during LO round-tripping.
Probably these are just side-effects from other locations in the
code that couldn't deal with these unexpected properties. For
example, the numbering.xml file is malformed, since it is
missing the w14: namespace.

Unfortunately, I failed in my attempt to create a minimal
test document.

Change-Id: Idf88cd09d96546b7f03d326afb5f6e58439bcf20
Reviewed-on: https://gerrit.libreoffice.org/53271
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Justin Luth <justin_luth@sil.org>

writerfilter/source/dmapper/NumberingManager.cxx
@@ -218,7 +218,7 @@ uno::Sequence< beans::PropertyValue > ListLevel::GetCharStyleProperties( )
     {
         if (IgnoreForCharStyle(aValIter->Name))
             continue;
-        else
+        else if ( aValIter->Name != "CharInteropGrabBag" && aValIter->Name != "ParaInteropGrabBag" )
             rProperties.emplace_back(aValIter->Name, 0, aValIter->Value, beans::PropertyState_DIRECT_VALUE);
     }
 

commit 411104fa836d228c3b5866ce7a241ce5950ac148

tdf#117137 sw: add testcase for this DOCX import bug
Fails with commit 2e0121c75f2293358567c40c4d3186414121819c (tdf#117137:
DOCX import: don't try to set grab-bag as UNO prop, 2018-04-21)
reverted.

Change-Id: Ice5f9831234ba7ba72a730ea6e33648a339a2a88
Reviewed-on: https://gerrit.libreoffice.org/62998
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins

sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -845,6 +845,22 @@ DECLARE_OOXMLEXPORT_TEST(testTdf105444, "tdf105444.docx")
     assertXPath(pXmlComm, "/w:comments/w:comment/w:p", 1);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf117137, "tdf117137.docx")
+{
+    // Paragraphs were not part of a numbering anymore after roundtrip.
+    uno::Reference<beans::XPropertySet> xPara1(getParagraph(1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xPara1.is());
+    CPPUNIT_ASSERT(xPara1->getPropertyValue("NumberingRules").hasValue());
+
+    uno::Reference<beans::XPropertySet> xPara2(getParagraph(2), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xPara2.is());
+    CPPUNIT_ASSERT(xPara2->getPropertyValue("NumberingRules").hasValue());
+
+    uno::Reference<beans::XPropertySet> xPara3(getParagraph(3), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xPara3.is());
+    CPPUNIT_ASSERT(xPara3->getPropertyValue("NumberingRules").hasValue());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */