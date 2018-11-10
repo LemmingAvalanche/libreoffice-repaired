LemmingAvalanche/libreoffice-repaired

commit 2af49f11a3b3add4f3e94516ba318af33f1bc3c9

clang-tidy performance-unnecessary-copy-init in sc
Change-Id: I93ded61c22bd533a6ffaddd20d6e527f176e7651
Reviewed-on: https://gerrit.libreoffice.org/62218
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/view/viewfun2.cxx
@@ -3047,7 +3047,7 @@ void ScViewFunc::HideTable( const ScMarkData& rMark )
         ScMarkData::MarkedTabsType::const_iterator it;
         std::vector<SCTAB> undoTabs;
 
-        ScMarkData::MarkedTabsType selectedTabs = rMark.GetSelectedTabs();
+        const ScMarkData::MarkedTabsType& selectedTabs = rMark.GetSelectedTabs();
         for (it=selectedTabs.begin(); it!=selectedTabs.end(); ++it)
         {
             nTab = *it;

sc/source/core/data/column4.cxx
@@ -1729,7 +1729,7 @@ public:
                     const ScFormulaCell* pCell = *it;
                     OUString aFormula;
                     pCell->GetFormula(aFormula, formula::FormulaGrammar::GRAM_ENGLISH_XL_R1C1);
-                    auto xCellGroup = pCell->GetCellGroup();
+                    const auto& xCellGroup = pCell->GetCellGroup();
                     sal_uInt64 nGroupLength = 0;
                     if (xCellGroup)
                     {

sc/source/core/data/documen4.cxx
@@ -794,7 +794,7 @@ const SfxPoolItem* ScDocument::GetEffItem(
                     {
                         ScAddress aPos(nCol, nRow, nTab);
                         ScRefCellValue aCell(const_cast<ScDocument&>(*this), aPos);
-                        OUString aStyle = pForm->GetCellStyle(aCell, aPos);
+                        const OUString& aStyle = pForm->GetCellStyle(aCell, aPos);
                         if (!aStyle.isEmpty())
                         {
                             SfxStyleSheetBase* pStyleSheet = mxPoolHelper->GetStylePool()->Find(

sc/source/core/data/dpobject.cxx
@@ -828,8 +828,7 @@ void ScDPObject::ReloadGroupTableData()
         if (pData)
         {
             // Replace the existing group table data with the source data.
-            shared_ptr<ScDPTableData> pSource = pData->GetSourceTableData();
-            mpTableData = pSource;
+            mpTableData = pData->GetSourceTableData();
         }
         return;
     }
@@ -839,7 +838,7 @@ void ScDPObject::ReloadGroupTableData()
     {
         // This is already a group table data. Salvage the source data and
         // re-create a new group data.
-        shared_ptr<ScDPTableData> pSource = pData->GetSourceTableData();
+        const shared_ptr<ScDPTableData>& pSource = pData->GetSourceTableData();
         shared_ptr<ScDPGroupTableData> pGroupData(new ScDPGroupTableData(pSource, pDoc));
         pDimData->WriteToData(*pGroupData);
         mpTableData = pGroupData;

sc/source/core/data/globalx.cxx
@@ -47,7 +47,7 @@ void ScGlobal::InitAddIns()
 
     // multi paths separated by semicolons
     SvtPathOptions aPathOpt;
-    OUString aMultiPath = aPathOpt.GetAddinPath();
+    const OUString& aMultiPath = aPathOpt.GetAddinPath();
     if (aMultiPath.isEmpty())
         return;
 

sc/source/core/data/table3.cxx
@@ -122,7 +122,7 @@ static bool SplitString( const OUString &sWhole,
         return false;
 
     // Get numeral element
-    OUString sUser = ScGlobal::pLocaleData->getNumDecimalSep();
+    const OUString& sUser = ScGlobal::pLocaleData->getNumDecimalSep();
     ParseResult aPRNum = ScGlobal::pCharClass->parsePredefinedToken(
         KParseType::ANY_NUMBER, sWhole, nPos,
         KParseTokens::ANY_NUMBER, "", KParseTokens::ANY_NUMBER, sUser );

sc/source/core/tool/addincol.cxx
@@ -146,7 +146,7 @@ bool ScUnoAddInFuncData::GetExcelName( LanguageType eDestLang, OUString& rRetExc
     if ( !rCompNames.empty() )
     {
         LanguageTag aLanguageTag( eDestLang);
-        const OUString aSearch( aLanguageTag.getBcp47());
+        const OUString& aSearch( aLanguageTag.getBcp47());
 
         // First, check exact match without fallback overhead.
         ::std::vector<LocalizedName>::const_iterator itNames( rCompNames.begin());

sc/source/core/tool/charthelper.cxx
@@ -292,7 +292,7 @@ void ScChartHelper::AddRangesIfProtectedChart( ScRangeListVector& rRangesVector,
         SdrOle2Obj* pSdrOle2Obj = dynamic_cast< SdrOle2Obj* >( pObject );
         if ( pSdrOle2Obj && pSdrOle2Obj->IsChart() )
         {
-            uno::Reference< embed::XEmbeddedObject > xEmbeddedObj = pSdrOle2Obj->GetObjRef();
+            const uno::Reference< embed::XEmbeddedObject >& xEmbeddedObj = pSdrOle2Obj->GetObjRef();
             if ( xEmbeddedObj.is() )
             {
                 bool bDisableDataTableDialog = false;
@@ -306,7 +306,7 @@ void ScChartHelper::AddRangesIfProtectedChart( ScRangeListVector& rRangesVector,
                     ScChartListenerCollection* pCollection = pDocument->GetChartListenerCollection();
                     if (pCollection)
                     {
-                        OUString aChartName = pSdrOle2Obj->GetPersistName();
+                        const OUString& aChartName = pSdrOle2Obj->GetPersistName();
                         const ScChartListener* pListener = pCollection->findByName(aChartName);
                         if (pListener)
                         {
@@ -378,12 +378,12 @@ void ScChartHelper::CreateProtectedChartListenersAndNotify( ScDocument* pDoc, co
                 SdrOle2Obj* pSdrOle2Obj = dynamic_cast< SdrOle2Obj* >( pObject );
                 if ( pSdrOle2Obj && pSdrOle2Obj->IsChart() )
                 {
-                    OUString aChartName = pSdrOle2Obj->GetPersistName();
+                    const OUString& aChartName = pSdrOle2Obj->GetPersistName();
                     ::std::vector< OUString >::const_iterator aEnd = rExcludedChartNames.end();
                     ::std::vector< OUString >::const_iterator aFound = ::std::find( rExcludedChartNames.begin(), aEnd, aChartName );
                     if ( aFound == aEnd )
                     {
-                        uno::Reference< embed::XEmbeddedObject > xEmbeddedObj = pSdrOle2Obj->GetObjRef();
+                        const uno::Reference< embed::XEmbeddedObject >& xEmbeddedObj = pSdrOle2Obj->GetObjRef();
                         if ( xEmbeddedObj.is() && ( nRangeList < nRangeListCount ) )
                         {
                             bool bDisableDataTableDialog = false;

commit e2c4aabfeb7d858b48ed159f8f78795fb970b66d

tdf#121263 Calc crashes when hide multiple sheets
regression from
	commit 2af49f11a3b3add4f3e94516ba318af33f1bc3c9
    clang-tidy performance-unnecessary-copy-init in sc

Change-Id: I49a7099c0a5e9e684008d2e799f9ee94f867d786
Reviewed-on: https://gerrit.libreoffice.org/63239
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/view/viewfun2.cxx
@@ -3045,7 +3045,8 @@ void ScViewFunc::HideTable( const ScMarkData& rMark )
         ScMarkData::MarkedTabsType::const_iterator it;
         std::vector<SCTAB> undoTabs;
 
-        const ScMarkData::MarkedTabsType& selectedTabs = rMark.GetSelectedTabs();
+        // need to take a copy of selectedtabs since it is modified in the loop
+        const ScMarkData::MarkedTabsType selectedTabs = rMark.GetSelectedTabs();
         for (it=selectedTabs.begin(); it!=selectedTabs.end(); ++it)
         {
             nTab = *it;