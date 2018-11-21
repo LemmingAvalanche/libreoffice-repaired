LemmingAvalanche/libreoffice-repaired

commit 842e5cafa7cae7dc97beaf7534b599296bc7a7b3

Replace macro IMPLEMENTATION_ENTRY used once (filter)
Change-Id: I9781487fc7cdfd3dc71f27d51d74c0f4138eec65
Reviewed-on: https://gerrit.libreoffice.org/63524
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

filter/source/msfilter/services.cxx
@@ -21,19 +21,16 @@
 #include <filter/msfilter/msvbahelper.hxx>
 
 using namespace ::com::sun::star::uno;
+using namespace ::ooo::vba;
 
 namespace {
 
-#define IMPLEMENTATION_ENTRY( className ) \
-    { &className##_createInstance, &className##_getImplementationName, &className##_getSupportedServiceNames, ::cppu::createSingleComponentFactory, nullptr, 0 }
-
 static ::cppu::ImplementationEntry const spServices[] =
 {
-    IMPLEMENTATION_ENTRY( ::ooo::vba::VBAMacroResolver ),
-    { nullptr, nullptr, nullptr, nullptr, nullptr, 0 }
+    { VBAMacroResolver_createInstance, VBAMacroResolver_getImplementationName,
+      VBAMacroResolver_getSupportedServiceNames,
+      ::cppu::createSingleComponentFactory, nullptr, 0 }
 };
-
-#undef IMPLEMENTATION_ENTRY
 
 } // namespace
 

commit 22c5945143ba1dec458d2cf6c392370e75c93216

filter: restore null entry at the end of msfilter services list
Regression from 842e5cafa7cae7dc97beaf7534b599296bc7a7b3 (Replace macro
IMPLEMENTATION_ENTRY used once (filter), 2018-11-18).

Change-Id: I6138fc44ddeb33cf2e3f1d37faaf01fdd17c9949
Reviewed-on: https://gerrit.libreoffice.org/63696
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins

filter/source/msfilter/services.cxx
@@ -29,7 +29,8 @@ static ::cppu::ImplementationEntry const spServices[] =
 {
     { VBAMacroResolver_createInstance, VBAMacroResolver_getImplementationName,
       VBAMacroResolver_getSupportedServiceNames,
-      ::cppu::createSingleComponentFactory, nullptr, 0 }
+      ::cppu::createSingleComponentFactory, nullptr, 0 },
+    { nullptr, nullptr, nullptr, nullptr, nullptr, 0 }
 };
 
 } // namespace