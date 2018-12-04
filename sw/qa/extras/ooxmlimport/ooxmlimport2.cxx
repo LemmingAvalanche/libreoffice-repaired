LemmingAvalanche/libreoffice-repaired

commit 8f7b0c987027d1b1a14a156dcf31c3415dd73ab5

Use constants for ST_LineNumberRestart values
Change-Id: I9fe7909bb8f6174ac05edb340a7d5606f077679d

writerfilter/source/dmapper/PropertyMap.cxx
@@ -1047,7 +1047,7 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
     {
         bool bFirst = rDM_Impl.IsLineNumberingSet();
         rDM_Impl.SetLineNumbering( m_nLnnMod, m_nLnc, m_ndxaLnn );
-        if( m_nLnnMin > 0 || (bFirst && m_nLnc == 1))
+        if( m_nLnnMin > 0 || (bFirst && m_nLnc == NS_ooxml::LN_Value_ST_LineNumberRestart_newSection))
         {
             //set the starting value at the beginning of the section
             try

writerfilter/source/dmapper/DomainMapper.cxx
@@ -864,8 +864,7 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
                         pSectionContext->SetdxaLnn( nIntValue );
                 break;
                 case NS_ooxml::LN_CT_LineNumber_restart:
-                    //page:empty, probably 0,section:1,continuous:2;
-                    aSettings.bRestartAtEachPage = nIntValue < 1;
+                    aSettings.bRestartAtEachPage = nIntValue == NS_ooxml::LN_Value_ST_LineNumberRestart_newPage;
                     OSL_ENSURE(pSectionContext, "SectionContext unavailable!");
                     if( pSectionContext )
                         pSectionContext->SetLnc( nIntValue );

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -4529,7 +4529,7 @@ void DomainMapper_Impl::SetLineNumbering( sal_Int32 nLnnMod, sal_Int32 nLnc, sal
             xProperties->setPropertyValue( rPropNameSupplier.GetName( PROP_DISTANCE               ), uno::makeAny( ConversionHelper::convertTwipToMM100(ndxaLnn) ));
             xProperties->setPropertyValue( rPropNameSupplier.GetName( PROP_NUMBER_POSITION        ), uno::makeAny( style::LineNumberPosition::LEFT));
             xProperties->setPropertyValue( rPropNameSupplier.GetName( PROP_NUMBERING_TYPE         ), uno::makeAny( style::NumberingType::ARABIC));
-            xProperties->setPropertyValue( rPropNameSupplier.GetName( PROP_RESTART_AT_EACH_PAGE   ), uno::makeAny( nLnc == 0 ));
+            xProperties->setPropertyValue( rPropNameSupplier.GetName( PROP_RESTART_AT_EACH_PAGE   ), uno::makeAny( nLnc == NS_ooxml::LN_Value_ST_LineNumberRestart_newPage ));
         }
         catch( const uno::Exception& )
         {}

writerfilter/source/ooxml/model.xml
@@ -17686,9 +17686,9 @@
       <value tokenid="ooxml:Value_ST_ChapterSep_enDash">enDash</value>
     </resource>
     <resource name="ST_LineNumberRestart" resource="List">
-      <value tokenid="0">newPage</value>
-      <value tokenid="1">newSection</value>
-      <value tokenid="2">continuous</value>
+      <value tokenid="ooxml:Value_ST_LineNumberRestart_newPage">newPage</value>
+      <value tokenid="ooxml:Value_ST_LineNumberRestart_newSection">newSection</value>
+      <value tokenid="ooxml:Value_ST_LineNumberRestart_continuous">continuous</value>
     </resource>
     <resource name="CT_LineNumber" resource="Properties">
       <attribute name="countBy" tokenid="ooxml:CT_LineNumber_countBy"/>

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -3153,7 +3153,7 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
     case RTF_LINEPPAGE:
     case RTF_LINECONT:
     {
-        RTFValue::Pointer_t pValue(new RTFValue(nKeyword == RTF_LINEPPAGE ? 0 : 2));
+        RTFValue::Pointer_t pValue(new RTFValue(nKeyword == RTF_LINEPPAGE ? NS_ooxml::LN_Value_ST_LineNumberRestart_newPage : NS_ooxml::LN_Value_ST_LineNumberRestart_continuous));
         lcl_putNestedAttribute(m_aStates.top().aSectionSprms,
                                NS_ooxml::LN_EG_SectPrContents_lnNumType, NS_ooxml::LN_CT_LineNumber_restart, pValue);
     }

commit 08edc8534900da1abef7102a96bd068394d14457

tdf#121664 DOCX import: fix lack of line numbering reset on page break
Regression from commit 8f7b0c987027d1b1a14a156dcf31c3415dd73ab5 (Use
constants for ST_LineNumberRestart values, 2014-10-01) where I forgot to
adapt the constructor's initializer list to also use the new constants.

Change-Id: I7113d23cceb3a00f0d466b63c99a18bf3dfd0ee3
Reviewed-on: https://gerrit.libreoffice.org/64489
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/source/dmapper/PropertyMap.cxx
@@ -396,7 +396,7 @@ SectionPropertyMap::SectionPropertyMap( bool bIsFirstSection )
     , m_nDxtCharSpace( 0 )
     , m_bGridSnapToChars( true )
     , m_nLnnMod( 0 )
-    , m_nLnc( 0 )
+    , m_nLnc(NS_ooxml::LN_Value_ST_LineNumberRestart_newPage)
     , m_ndxaLnn( 0 )
     , m_nLnnMin( 0 )
     , m_bDefaultHeaderLinkToPrevious( true )

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -51,6 +51,16 @@ DECLARE_OOXMLIMPORT_TEST(testTdf109053, "tdf109053.docx")
     CPPUNIT_ASSERT_EQUAL(2, getPages());
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf121664, "tdf121664.docx")
+{
+    uno::Reference<text::XLineNumberingProperties> xLineNumbering(mxComponent, uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xLineNumbering.is());
+    // Without the accompanying fix in place, numbering did not restart on the
+    // second page.
+    CPPUNIT_ASSERT(
+        getProperty<bool>(xLineNumbering->getLineNumberingProperties(), "RestartAtEachPage"));
+}
+
 DECLARE_OOXMLIMPORT_TEST(testTdf108849, "tdf108849.docx")
 {
     // sectPr element that is child element of body must be the last child. However, Word accepts it