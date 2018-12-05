LemmingAvalanche/libreoffice-repaired

commit 3895a2b91cfad6463bda90bd2dcc72ccf86cc082

don't replace content of formula field when selecting range, tdf#58635
Change-Id: I85953fdd7c2b7f193b2a815564526d985ae2bba4
Reviewed-on: https://gerrit.libreoffice.org/36309
Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

sc/source/ui/condformat/condformatdlg.cxx
@@ -657,7 +657,10 @@ void ScCondFormatDlg::SetReference(const ScRange& rRef, ScDocument*)
 
         OUString aRefStr(rRef.Format(nFlags, mpViewData->GetDocument(),
             ScAddress::Details(mpViewData->GetDocument()->GetAddressConvention(), 0, 0)));
-        pEdit->SetRefString( aRefStr );
+        if (pEdit != mpEdRange)
+            pEdit->ReplaceSelected(aRefStr);
+        else
+            pEdit->SetRefString( aRefStr );
         updateTitle();
     }
 }

commit a49c1ddffb5f829e3aadbda16c6f402412ece997

tdf#117332: select replaced text to properly replace it next time
... othervise the next drag event will insert the new range immediately
after the just inserted, resulting in invalid reference.

Regression after commit 3895a2b91cfad6463bda90bd2dcc72ccf86cc082

  don't replace content of formula field when selecting range, tdf#58635

Change-Id: I7dfdc1ce041ebadf4406dbb605676d87cd489efa
Reviewed-on: https://gerrit.libreoffice.org/64574
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Mike Kaganski <mike.kaganski@collabora.com>

sc/source/ui/condformat/condformatdlg.cxx
@@ -674,7 +674,13 @@ void ScCondFormatDlg::SetReference(const ScRange& rRef, ScDocument*)
         OUString aRefStr(rRef.Format(nFlags, mpViewData->GetDocument(),
             ScAddress::Details(mpViewData->GetDocument()->GetAddressConvention(), 0, 0)));
         if (pEdit != mpEdRange)
+        {
+            Selection sel = pEdit->GetSelection();
+            sel.Justify();            // in case of RtL selection
+            sel.Max() = sel.Min() + aRefStr.getLength();
             pEdit->ReplaceSelected(aRefStr);
+            pEdit->SetSelection(sel); // to replace it again with next drag event
+        }
         else
             pEdit->SetRefString( aRefStr );
         updateTitle();