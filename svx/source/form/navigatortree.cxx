LemmingAvalanche/libreoffice-repaired

commit 267994c971806ee687c8fe3071d6eb1890b19afb

clang-tidy performance-unnecessary-copy-init in svx
Change-Id: I27f4dd0cb08f9b62496a1c51eed732a678e1c2e5
Reviewed-on: https://gerrit.libreoffice.org/62252
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/form/navigatortree.cxx
@@ -482,7 +482,7 @@ namespace svxform
                         DBG_ASSERT( IsFormEntry(pSelectedForm), "NavigatorTree::Command: This entry must be a FormEntry." );
 
                         FmFormData* pFormData = static_cast<FmFormData*>(pSelectedForm->GetUserData());
-                        Reference< XForm >  xForm(  pFormData->GetFormIface());
+                        const Reference< XForm >&  xForm(  pFormData->GetFormIface());
 
                         Reference< XTabControllerModel >  xTabController(xForm, UNO_QUERY);
                         if( !xTabController.is() )
@@ -1010,7 +1010,7 @@ namespace svxform
         DBG_ASSERT( DND_ACTION_COPY != _nAction, "NavigatorTree::implExecuteDataTransfer: somebody changed the logics!" );
 
         // list of dragged entries
-        ListBoxEntrySet aDropped = _rData.selected();
+        const ListBoxEntrySet& aDropped = _rData.selected();
         DBG_ASSERT(!aDropped.empty(), "NavigatorTree::implExecuteDataTransfer: no entries!");
 
         // shell and model

svx/source/core/graphichelper.cxx
@@ -352,7 +352,7 @@ void GraphicHelper::SaveShapeAsGraphic(weld::Window* pParent,  const Reference< 
         Reference< XPropertySet > xShapeSet( xShape, UNO_QUERY_THROW );
 
         SvtPathOptions aPathOpt;
-        OUString sGraphicPath( aPathOpt.GetGraphicPath() );
+        const OUString& sGraphicPath( aPathOpt.GetGraphicPath() );
 
         FileDialogHelper aDialogHelper(TemplateDescription::FILESAVE_AUTOEXTENSION, FileDialogFlags::NONE, pParent);
         Reference < XFilePicker3 > xFilePicker = aDialogHelper.GetFilePicker();

svx/source/engine3d/helperminimaldepth3d.cxx
@@ -110,7 +110,7 @@ namespace drawinglayer
 
                     for(sal_uInt32 a(0); a < nPolyCount; a++)
                     {
-                           const basegfx::B3DPolygon aPolygon(rPolyPolygon.getB3DPolygon(a));
+                        const basegfx::B3DPolygon& aPolygon(rPolyPolygon.getB3DPolygon(a));
                         const sal_uInt32 nCount(aPolygon.count());
 
                         for(sal_uInt32 b(0); b < nCount; b++)

svx/source/engine3d/obj3d.cxx
@@ -308,7 +308,7 @@ basegfx::B3DRange E3dObject::RecalcBoundVolume() const
     if(pVCOfE3D)
     {
         // BoundVolume is without 3D object transformation, use correct sequence
-        const drawinglayer::primitive3d::Primitive3DContainer xLocalSequence(pVCOfE3D->getVIP3DSWithoutObjectTransform());
+        const drawinglayer::primitive3d::Primitive3DContainer& xLocalSequence(pVCOfE3D->getVIP3DSWithoutObjectTransform());
 
         if(!xLocalSequence.empty())
         {

svx/source/engine3d/view3d.cxx
@@ -168,7 +168,7 @@ void Impl3DMirrorConstructOverlay::SetMirrorAxis(Point aMirrorAxisA, Point aMirr
     for(sal_uInt32 a(0); a < mrView.PaintWindowCount(); a++)
     {
         SdrPaintWindow* pCandidate = mrView.GetPaintWindow(a);
-        rtl::Reference< sdr::overlay::OverlayManager > xTargetOverlay = pCandidate->GetOverlayManager();
+        const rtl::Reference< sdr::overlay::OverlayManager >& xTargetOverlay = pCandidate->GetOverlayManager();
 
         if(xTargetOverlay.is())
         {

svx/source/fmcomp/fmgridcl.cxx
@@ -1497,7 +1497,7 @@ void FmGridControl::ColumnResized(sal_uInt16 nId)
 
     // transfer value to the model
     DbGridColumn* pCol = DbGridControl::GetColumns()[ GetModelColumnPos(nId) ].get();
-    Reference< css::beans::XPropertySet >  xColModel(pCol->getModel());
+    const Reference< css::beans::XPropertySet >&  xColModel(pCol->getModel());
     if (xColModel.is())
     {
         Any aWidth;

svx/source/fmcomp/gridctrl.cxx
@@ -861,7 +861,7 @@ DbGridRow::DbGridRow(CursorWrapper* pCur, bool bPaintCursor)
                 m_eStatus = (pCur->isAfterLast() || pCur->isBeforeFirst()) ? GridRowStatus::Invalid : GridRowStatus::Clean;
             else
             {
-                Reference< XPropertySet > xSet = pCur->getPropertySet();
+                const Reference< XPropertySet >& xSet = pCur->getPropertySet();
                 if (xSet.is())
                 {
                     m_bIsNew = ::comphelper::getBOOL(xSet->getPropertyValue(FM_PROP_ISNEW));
@@ -903,7 +903,7 @@ void DbGridRow::SetState(CursorWrapper* pCur, bool bPaintCursor)
             m_eStatus = GridRowStatus::Clean;
             if (!bPaintCursor)
             {
-                Reference< XPropertySet > xSet = pCur->getPropertySet();
+                const Reference< XPropertySet >& xSet = pCur->getPropertySet();
                 DBG_ASSERT(xSet.is(), "DbGridRow::SetState : invalid cursor !");
 
                 if (::comphelper::getBOOL(xSet->getPropertyValue(FM_PROP_ISMODIFIED)))

svx/source/form/fmundo.cxx
@@ -401,7 +401,7 @@ void FmXUndoEnvironment::Inserted(FmFormObj* pObj)
         {
             try
             {
-                Reference< XIndexContainer > xObjectParent = pObj->GetOriginalParent();
+                const Reference< XIndexContainer >& xObjectParent = pObj->GetOriginalParent();
 
                 FmFormPage& rPage(dynamic_cast< FmFormPage& >( *pObj->getSdrPageFromSdrObject()));
                 Reference< XIndexAccess >  xForms( rPage.GetForms(), UNO_QUERY_THROW );

commit 4058f5f7e72216e94971514d0b207b52bb4c0562

tdf#122127 Crash in FORM EDIT using drag drop to move control in dataform
regression from
    commit 267994c971806ee687c8fe3071d6eb1890b19afb
    Date:   Tue Oct 23 09:47:10 2018 +0200
    clang-tidy performance-unnecessary-copy-init in svx

Change-Id: I4210c328145d062d8b72f09dcff3280e11c6bc36
Reviewed-on: https://gerrit.libreoffice.org/65394
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/form/navigatortree.cxx
@@ -1000,7 +1000,7 @@ namespace svxform
         DBG_ASSERT( DND_ACTION_COPY != _nAction, "NavigatorTree::implExecuteDataTransfer: somebody changed the logics!" );
 
         // list of dragged entries
-        const ListBoxEntrySet& aDropped = _rData.selected();
+        const ListBoxEntrySet aDropped = _rData.selected();
         DBG_ASSERT(!aDropped.empty(), "NavigatorTree::implExecuteDataTransfer: no entries!");
 
         // shell and model