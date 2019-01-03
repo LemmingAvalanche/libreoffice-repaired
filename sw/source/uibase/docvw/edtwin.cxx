LemmingAvalanche/libreoffice-repaired

commit 05e4995536b39064216b3716d0f166ee9a0a987b

changed timers to idles
Change-Id: I223026ce7676a3f8fcda7eb33326cd4ee949c6f0

sw/source/uibase/docvw/edtwin.cxx
@@ -4777,12 +4777,12 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
 
                     m_pApplyTempl->bUndo = true;
                     bCallBase = false;
-                    m_aTemplateTimer.Stop();
+                    m_aTemplateIdle.Stop();
                 }
                 else if(rMEvt.GetClicks() == 1)
                 {
                     // no selection -> so turn off watering can
-                    m_aTemplateTimer.Start();
+                    m_aTemplateIdle.Start();
                 }
             }
         }
@@ -5002,8 +5002,8 @@ SwEditWin::SwEditWin(vcl::Window *pParent, SwView &rMyView):
 
     // TemplatePointer for colors should be resetted without
     // selection after single click
-    m_aTemplateTimer.SetTimeout(400);
-    m_aTemplateTimer.SetTimeoutHdl(LINK(this, SwEditWin, TemplateTimerHdl));
+    m_aTemplateIdle.SetPriority(VCL_IDLE_PRIORITY_LOWEST);
+    m_aTemplateIdle.SetIdleHdl(LINK(this, SwEditWin, TemplateTimerHdl));
 
     // temporary solution!!! Should set the font of the current
     // insert position at every curor movement!

sw/source/uibase/inc/edtwin.hxx
@@ -90,7 +90,7 @@ friend void     PageNumNotify(  SwViewShell* pVwSh,
     Point           m_aStartPos;
     Point           m_aMovePos;
     Point           m_aRszMvHdlPt;
-    Timer           m_aTemplateTimer;
+    Idle            m_aTemplateIdle;
 
     // type/object where the mouse pointer is
     SwCallMouseEvent m_aSaveCallEvent;

cui/source/dialogs/cuigaldlg.cxx
@@ -435,7 +435,7 @@ void TakeProgress::StartExecuteModal( const Link& rEndDialogHdl )
 ActualizeProgress::ActualizeProgress(vcl::Window* pWindow, GalleryTheme* pThm)
     : ModalDialog(pWindow, "GalleryUpdateProgress",
         "cui/ui/galleryupdateprogress.ui")
-    , pTimer(NULL)
+    , pIdle(NULL)
     , pTheme(pThm)
 {
     get(m_pFtActualizeFile, "file");
@@ -447,13 +447,13 @@ short ActualizeProgress::Execute()
 {
     short nRet;
 
-    pTimer = new Timer;
+    pIdle = new Idle;
 
-    if ( pTimer )
+    if ( pIdle )
     {
-        pTimer->SetTimeoutHdl( LINK( this, ActualizeProgress, TimeoutHdl ) );
-        pTimer->SetTimeout( 500 );
-        pTimer->Start();
+        pIdle->SetIdleHdl( LINK( this, ActualizeProgress, TimeoutHdl ) );
+        pIdle->SetPriority( VCL_IDLE_PRIORITY_LOWEST );
+        pIdle->Start();
     }
 
     nRet = ModalDialog::Execute();

cui/source/dialogs/thesdlg.cxx
@@ -60,8 +60,8 @@ LookUpComboBox::LookUpComboBox(vcl::Window *pParent)
 {
     EnableAutoSize(true);
 
-    m_aModifyTimer.SetTimeoutHdl( LINK( this, LookUpComboBox, ModifyTimer_Hdl ) );
-    m_aModifyTimer.SetTimeout( 500 );
+    m_aModifyIdle.SetIdleHdl( LINK( this, LookUpComboBox, ModifyTimer_Hdl ) );
+    m_aModifyIdle.SetPriority( VCL_IDLE_PRIORITY_LOWEST );
 
     EnableAutocomplete( false );
 }
@@ -82,13 +82,13 @@ LookUpComboBox::~LookUpComboBox()
 
 void LookUpComboBox::Modify()
 {
-    m_aModifyTimer.Start();
+    m_aModifyIdle.Start();
 }
 
 IMPL_LINK( LookUpComboBox, ModifyTimer_Hdl, Timer *, EMPTYARG /*pTimer*/ )
 {
     m_pDialog->LookUp( GetText() );
-    m_aModifyTimer.Stop();
+    m_aModifyIdle.Stop();
     return 0;
 }
 

cui/source/inc/cuigaldlg.hxx
@@ -150,7 +150,7 @@ class ActualizeProgress : public ModalDialog
 private:
     FixedText*          m_pFtActualizeFile;
     CancelButton*       m_pBtnCancel;
-    Timer*              pTimer;
+    Idle*               pIdle;
     GalleryTheme*       pTheme;
     GalleryProgress     aStatusProgress;
 

cui/source/inc/thesdlg.hxx
@@ -33,7 +33,7 @@ class SvxThesaurusDialog;
 
 class LookUpComboBox : public ComboBox
 {
-    Timer                       m_aModifyTimer;
+    Idle                        m_aModifyIdle;
     Selection                   m_aSelection;
     SvxThesaurusDialog*         m_pDialog;
 

cui/source/tabpages/backgrnd.cxx
@@ -106,11 +106,11 @@ struct SvxBackgroundPara_Impl
 
 struct SvxBackgroundPage_Impl
 {
-    Timer*          pLoadTimer;
+    Idle*          pLoadIdle;
     bool        bIsImportDlgInExecute;
 
     SvxBackgroundPage_Impl()
-        : pLoadTimer(NULL)
+        : pLoadIdle(NULL)
         , bIsImportDlgInExecute(false)
     {}
 };
@@ -421,7 +421,7 @@ SvxBackgroundTabPage::SvxBackgroundTabPage(vcl::Window* pParent, const SfxItemSe
 
 SvxBackgroundTabPage::~SvxBackgroundTabPage()
 {
-    delete pPageImpl->pLoadTimer;
+    delete pPageImpl->pLoadIdle;
     delete pPageImpl;
     delete pImportDlg;
 
@@ -719,10 +719,10 @@ void SvxBackgroundTabPage::FillUserData()
 
 bool SvxBackgroundTabPage::FillItemSet( SfxItemSet* rCoreSet )
 {
-    if ( pPageImpl->pLoadTimer && pPageImpl->pLoadTimer->IsActive() )
+    if ( pPageImpl->pLoadIdle && pPageImpl->pLoadIdle->IsActive() )
     {
-        pPageImpl->pLoadTimer->Stop();
-        LoadTimerHdl_Impl( pPageImpl->pLoadTimer );
+        pPageImpl->pLoadIdle->Stop();
+        LoadTimerHdl_Impl( pPageImpl->pLoadIdle );
     }
 // os: Such a nonsense! One will always find such an item somewhere,
 //     but it must be existing in the rSet!
@@ -1091,9 +1091,9 @@ void SvxBackgroundTabPage::ShowSelector()
         m_pBtnPosition->SetClickHdl( HDL(RadioClickHdl_Impl) );
 
         // delayed loading via timer (because of UI-Update)
-        pPageImpl->pLoadTimer = new Timer;
-        pPageImpl->pLoadTimer->SetTimeout( 500 );
-        pPageImpl->pLoadTimer->SetTimeoutHdl(
+        pPageImpl->pLoadIdle = new Idle;
+        pPageImpl->pLoadIdle->SetPriority( VCL_IDLE_PRIORITY_LOWEST );
+        pPageImpl->pLoadIdle->SetIdleHdl(
             LINK( this, SvxBackgroundTabPage, LoadTimerHdl_Impl ) );
 
         bAllowShowSelector = false;
@@ -1419,7 +1419,7 @@ IMPL_LINK_NOARG(SvxBackgroundTabPage, BrowseHdl_Impl)
 */
 
 {
-    if ( pPageImpl->pLoadTimer->IsActive() )
+    if ( pPageImpl->pLoadIdle->IsActive() )
         return 0;
     bool bHtml = 0 != ( nHtmlMode & HTMLMODE_ON );
 
@@ -1443,7 +1443,7 @@ IMPL_LINK_NOARG(SvxBackgroundTabPage, BrowseHdl_Impl)
         if ( !m_pBtnLink->IsChecked() && !m_pBtnPreview->IsChecked() )
             m_pBtnPreview->Check( true );
         // timer-delayed loading of the graphic
-        pPageImpl->pLoadTimer->Start();
+        pPageImpl->pLoadIdle->Start();
     }
     else
         DELETEZ( pImportDlg );
@@ -1462,9 +1462,9 @@ IMPL_LINK( SvxBackgroundTabPage, LoadTimerHdl_Impl, Timer* , pTimer )
 */
 
 {
-    if ( pTimer == pPageImpl->pLoadTimer )
+    if ( pTimer == pPageImpl->pLoadIdle )
     {
-        pPageImpl->pLoadTimer->Stop();
+        pPageImpl->pLoadIdle->Stop();
 
         if ( pImportDlg )
         {

cui/source/tabpages/chardlg.cxx
@@ -235,7 +235,7 @@ void SvxCharBasePage::SetPrevFontEscapement( sal_uInt8 nProp, sal_uInt8 nEscProp
 
 struct SvxCharNamePage_Impl
 {
-    Timer           m_aUpdateTimer;
+    Idle            m_aUpdateIdle;
     OUString        m_aNoStyleText;
     const FontList* m_pFontList;
     sal_Int32           m_nExtraEntryPos;
@@ -250,7 +250,7 @@ struct SvxCharNamePage_Impl
         m_bInSearchMode ( false )
 
     {
-        m_aUpdateTimer.SetTimeout( 350 );
+        m_aUpdateIdle.SetPriority( VCL_IDLE_PRIORITY_LOWEST );
     }
 
     ~SvxCharNamePage_Impl()
@@ -398,7 +398,7 @@ void SvxCharNamePage::Initialize()
     m_pCTLFontSizeLB->SetModifyHdl( aLink );
     m_pCTLFontLanguageLB->SetSelectHdl( aLink );
 
-    m_pImpl->m_aUpdateTimer.SetTimeoutHdl( LINK( this, SvxCharNamePage, UpdateHdl_Impl ) );
+    m_pImpl->m_aUpdateIdle.SetIdleHdl( LINK( this, SvxCharNamePage, UpdateHdl_Impl ) );
 }
 
 
@@ -1149,7 +1149,7 @@ IMPL_LINK_NOARG(SvxCharNamePage, UpdateHdl_Impl)
 
 IMPL_LINK( SvxCharNamePage, FontModifyHdl_Impl, void*, pNameBox )
 {
-    m_pImpl->m_aUpdateTimer.Start();
+    m_pImpl->m_aUpdateIdle.Start();
 
     if ( m_pWestFontNameLB == pNameBox || m_pEastFontNameLB == pNameBox || m_pCTLFontNameLB == pNameBox )
     {

commit fd3786f7d65a399a9d465d797a64e65aeece5716

tdf#122442: use Timer to detect single-clicks vs double-clicks
Regression from commit 05e4995536b39064216b3716d0f166ee9a0a987b which
appeared after commits changing scheduler/main loop in the range
d7115151be71eb1b91d2b5b4ba62394afee65b36..2446e845db04bb048a78bbea86baa4836f8bc989

Change-Id: I6cf58c1e83aa9621bdd4b5c14f8565d5c69e20c1
Reviewed-on: https://gerrit.libreoffice.org/65837
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/uibase/docvw/edtwin.cxx
@@ -4756,12 +4756,12 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
                     rSh.EnterStdMode();
                     rSh.SetVisibleCursor(aDocPt);
                     bCallBase = false;
-                    m_aTemplateIdle.Stop();
+                    m_aTemplateTimer.Stop();
                 }
                 else if(rMEvt.GetClicks() == 1)
                 {
                     // no selection -> so turn off watering can
-                    m_aTemplateIdle.Start();
+                    m_aTemplateTimer.Start();
                 }
             }
         }
@@ -4997,9 +4997,9 @@ SwEditWin::SwEditWin(vcl::Window *pParent, SwView &rMyView):
     m_aKeyInputFlushTimer.SetInvokeHandler(LINK(this, SwEditWin, KeyInputFlushHandler));
 
     // TemplatePointer for colors should be resetted without
-    // selection after single click
-    m_aTemplateIdle.SetPriority(TaskPriority::LOWEST);
-    m_aTemplateIdle.SetInvokeHandler(LINK(this, SwEditWin, TemplateTimerHdl));
+    // selection after single click, but not after double-click (tdf#122442)
+    m_aTemplateTimer.SetTimeout(GetSettings().GetMouseSettings().GetDoubleClickTime());
+    m_aTemplateTimer.SetInvokeHandler(LINK(this, SwEditWin, TemplateTimerHdl));
 
     // temporary solution!!! Should set the font of the current
     // insert position at every cursor movement!

sw/source/uibase/inc/edtwin.hxx
@@ -81,7 +81,7 @@ class SwEditWin final : public vcl::Window,
     Point           m_aStartPos;
     Point           m_aMovePos;
     Point           m_aRszMvHdlPt;
-    Idle            m_aTemplateIdle;
+    Timer           m_aTemplateTimer;
 
     // type/object where the mouse pointer is
     SwCallMouseEvent m_aSaveCallEvent;