LemmingAvalanche/libreoffice-repaired

commit 7464a8190d0bd980df7c465b48d1462c792bbfb1

some more ConstAsciiString behind OSL_ENSURE
Change-Id: Ic93ec84ce62234364d233a9188a65439e88a79fe

xmloff/source/forms/elementimport.cxx
@@ -993,8 +993,8 @@ namespace xmloff
         {
             // we have exactly 2 properties where this type class is allowed:
             OSL_ENSURE(
-                    (0 == _rPropValue.Name.compareToAscii(PROPERTY_EFFECTIVE_VALUE))
-                ||  (0 == _rPropValue.Name.compareToAscii(PROPERTY_EFFECTIVE_DEFAULT)),
+                    (0 == _rPropValue.Name.equalsAsciiL(PROPERTY_EFFECTIVE_VALUE.ascii, PROPERTY_EFFECTIVE_VALUE.length))
+                ||  (0 == _rPropValue.Name.equalsAsciiL(PROPERTY_EFFECTIVE_DEFAULT.ascii, PROPERTY_EFFECTIVE_DEFAULT.length)),
                 "OControlImport::implTranslateValueProperty: invalid property type/name combination!");
 
             // Both properties are allowed to have a double or a string value,

commit a5f6552e3770828d0da7619c4d2cccb162baff39

fix incorrect OSL_ENSURE
broken in
    commit 7464a8190d0bd980df7c465b48d1462c792bbfb1
    Date:   Thu May 17 14:51:04 2012 +0100
    some more ConstAsciiString behind OSL_ENSURE

Also convert to SAL_WARN_IF, and make the error message more useful.

Change-Id: I7f1d0c577751e7309d5a0d544f38c5050b903a58
Reviewed-on: https://gerrit.libreoffice.org/67230
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmloff/source/forms/elementimport.cxx
@@ -886,10 +886,10 @@ namespace xmloff
         if (TypeClass_ANY == aProp.Type.getTypeClass())
         {
             // we have exactly 2 properties where this type class is allowed:
-            OSL_ENSURE(
-                    _rPropValue.Name != PROPERTY_EFFECTIVE_VALUE
-                &&  _rPropValue.Name != PROPERTY_EFFECTIVE_DEFAULT,
-                "OControlImport::implTranslateValueProperty: invalid property type/name combination!");
+            SAL_WARN_IF(
+                    _rPropValue.Name == PROPERTY_EFFECTIVE_VALUE
+                ||  _rPropValue.Name == PROPERTY_EFFECTIVE_DEFAULT, "xmloff",
+                "OControlImport::implTranslateValueProperty: invalid property type/name combination, Any and " + _rPropValue.Name);
 
             // Both properties are allowed to have a double or a string value,
             // so first try to convert the string into a number