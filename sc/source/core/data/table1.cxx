LemmingAvalanche/libreoffice-repaired

commit b057ff6366b030c1a365b24a84f59dcf8b369e4a

GetNextPos: deduplicate some nMovX code
Change-Id: I9208e2e0a9eaa27a81ffe1dd7054dffc7ddd75f4
Reviewed-on: https://gerrit.libreoffice.org/57050
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sc/source/core/data/table1.cxx
@@ -1498,63 +1498,28 @@ void ScTable::GetNextPos( SCCOL& rCol, SCROW& rRow, SCCOL nMovX, SCROW nMovY,
             const SCCOL nColCount = nEndCol - nStartCol + 1;
             std::unique_ptr<SCROW[]> pNextRows( new SCROW[nColCount]);
             const SCCOL nLastCol = aCol.size() - 1;
+            const bool bUp = (nMovX < 0);   // Moving left also means moving up in rows.
+            const SCROW nRowAdd = (bUp ? -1 : 1);
             sal_uInt16 nWrap = 0;
 
-            if ( nMovX > 0 )                            //  forward
+            for (SCCOL i = 0; i < nColCount; ++i)
+                pNextRows[i] = (i + nStartCol < nCol) ? (nRow + nRowAdd) : nRow;
+            do
             {
-                for (SCCOL i = 0; i < nColCount; ++i)
-                    pNextRows[i] = (i + nStartCol < nCol) ? (nRow+1) : nRow;
-                do
+                SCROW nNextRow = pNextRows[nCol - nStartCol] + nRowAdd;
+                if ( bMarked )
+                    nNextRow = rMark.GetNextMarked( nCol, nNextRow, bUp );
+                if ( bUnprotected )
+                    nNextRow = ( nCol <= nLastCol ) ? aCol[nCol].GetNextUnprotected( nNextRow, bUp ) :
+                        aDefaultColAttrArray.GetNextUnprotected( nNextRow, bUp );
+                pNextRows[nCol - nStartCol] = nNextRow;
+
+                if (bUp)
                 {
-                    SCROW nNextRow = pNextRows[nCol - nStartCol] + 1;
-                    if ( bMarked )
-                        nNextRow = rMark.GetNextMarked( nCol, nNextRow, false );
-                    if ( bUnprotected )
-                        nNextRow = ( nCol <= nLastCol ) ? aCol[nCol].GetNextUnprotected( nNextRow, false ) :
-                            aDefaultColAttrArray.GetNextUnprotected( nNextRow, false );
-                    pNextRows[nCol - nStartCol] = nNextRow;
-
-                    SCROW nMinRow = nEndRow + 1;
-                    for (SCCOL i = 0; i < nColCount; ++i)
-                    {
-                        if (pNextRows[i] < nMinRow)     // when two equal on the left
-                        {
-                            nMinRow = pNextRows[i];
-                            nCol = i + nStartCol;
-                        }
-                    }
-                    nRow = nMinRow;
-
-                    if ( nRow > nEndRow )
-                    {
-                        if (++nWrap >= 2)
-                            return;
-                        nCol = nStartCol;
-                        nRow = nStartRow;
-                        for (SCCOL i = 0; i < nColCount; ++i)
-                            pNextRows[i] = nStartRow;   // do it all over again
-                    }
-                }
-                while ( !ValidNextPos(nCol, nRow, rMark, bMarked, bUnprotected) );
-            }
-            else                                        //  backwards
-            {
-                for (SCCOL i = 0; i < nColCount; ++i)
-                    pNextRows[i] = (i + nStartCol > nCol) ? (nRow-1) : nRow;
-                do
-                {
-                    SCROW nNextRow = pNextRows[nCol - nStartCol] - 1;
-                    if ( bMarked )
-                        nNextRow = rMark.GetNextMarked( nCol, nNextRow, true );
-                    if ( bUnprotected )
-                        nNextRow = ( nCol <= nLastCol ) ? aCol[nCol].GetNextUnprotected( nNextRow, true ) :
-                            aDefaultColAttrArray.GetNextUnprotected( nNextRow, true );
-                    pNextRows[nCol - nStartCol] = nNextRow;
-
                     SCROW nMaxRow = nStartRow - 1;
                     for (SCCOL i = 0; i < nColCount; ++i)
                     {
-                        if (pNextRows[i] >= nMaxRow)    // when two equal on the right
+                        if (pNextRows[i] >= nMaxRow)    // when two equal the right one
                         {
                             nMaxRow = pNextRows[i];
                             nCol = i + nStartCol;
@@ -1572,8 +1537,31 @@ void ScTable::GetNextPos( SCCOL& rCol, SCROW& rRow, SCCOL nMovX, SCROW nMovY,
                             pNextRows[i] = nEndRow;     // do it all over again
                     }
                 }
-                while ( !ValidNextPos(nCol, nRow, rMark, bMarked, bUnprotected) );
+                else
+                {
+                    SCROW nMinRow = nEndRow + 1;
+                    for (SCCOL i = 0; i < nColCount; ++i)
+                    {
+                        if (pNextRows[i] < nMinRow)     // when two equal the left one
+                        {
+                            nMinRow = pNextRows[i];
+                            nCol = i + nStartCol;
+                        }
+                    }
+                    nRow = nMinRow;
+
+                    if ( nRow > nEndRow )
+                    {
+                        if (++nWrap >= 2)
+                            return;
+                        nCol = nStartCol;
+                        nRow = nStartRow;
+                        for (SCCOL i = 0; i < nColCount; ++i)
+                            pNextRows[i] = nStartRow;   // do it all over again
+                    }
+                }
             }
+            while ( !ValidNextPos(nCol, nRow, rMark, bMarked, bUnprotected) );
         }
     }
 

commit 1f45e1abe0ca11dd8cf5038bccb91d5c8395be1d

Resolves: tdf#123052 correctly init pNextRows[] values for up/back unprotected
Regression from

    commit b057ff6366b030c1a365b24a84f59dcf8b369e4a
    CommitDate: Fri Jul 6 14:25:07 2018 +0200

        GetNextPos: deduplicate some nMovX code

Change-Id: I0352d44eda587d2c340df4c7d8fe35b7b8591fc1
Reviewed-on: https://gerrit.libreoffice.org/67693
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/source/core/data/table1.cxx
@@ -1523,8 +1523,16 @@ void ScTable::GetNextPos( SCCOL& rCol, SCROW& rRow, SCCOL nMovX, SCROW nMovY,
             const SCROW nRowAdd = (bUp ? -1 : 1);
             sal_uInt16 nWrap = 0;
 
-            for (SCCOL i = 0; i < nColCount; ++i)
-                pNextRows[i] = (i + nStartCol < nCol) ? (nRow + nRowAdd) : nRow;
+            if (bUp)
+            {
+                for (SCCOL i = 0; i < nColCount; ++i)
+                    pNextRows[i] = (i + nStartCol > nCol) ? (nRow + nRowAdd) : nRow;
+            }
+            else
+            {
+                for (SCCOL i = 0; i < nColCount; ++i)
+                    pNextRows[i] = (i + nStartCol < nCol) ? (nRow + nRowAdd) : nRow;
+            }
             do
             {
                 SCROW nNextRow = pNextRows[nCol - nStartCol] + nRowAdd;