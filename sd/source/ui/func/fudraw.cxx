LemmingAvalanche/libreoffice-repaired

commit 9be0a088413cbb98970872ac594c0f333c697309

Pointer is pointless
since it is just a wrapper around PointerStyle

Change-Id: I51f065e0d4ad8bd91f5c84c5819048c720a19267
Reviewed-on: https://gerrit.libreoffice.org/67711
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/func/fudraw.cxx
@@ -21,6 +21,7 @@
 
 #include <editeng/eeitem.hxx>
 #include <vcl/waitobj.hxx>
+#include <vcl/ptrstyle.hxx>
 #include <editeng/flditem.hxx>
 #include <svx/svdogrp.hxx>
 #include <svx/svdoole2.hxx>
@@ -478,7 +479,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
         {
             // water can mode
             bDefPointer = false;
-            mpWindow->SetPointer(Pointer(PointerStyle::Fill));
+            mpWindow->SetPointer(PointerStyle::Fill);
         }
     }
     else
@@ -489,7 +490,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
         {
             // water can mode
             bDefPointer = false;
-            mpWindow->SetPointer(Pointer(PointerStyle::Fill));
+            mpWindow->SetPointer(PointerStyle::Fill);
         }
         else if (!pHdl &&
                  mpViewShell->GetViewFrame()->HasChildWindow(SvxBmpMaskChildWindow::GetChildWindowId()))
@@ -500,7 +501,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
             if (pMask && pMask->IsEyedropping())
             {
                 bDefPointer = false;
-                mpWindow->SetPointer(Pointer(PointerStyle::RefHand));
+                mpWindow->SetPointer(PointerStyle::RefHand);
             }
         }
         else if (!mpView->IsAction())
@@ -526,7 +527,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
                 SdrObject* pObject = rMarkList.GetMark(0)->GetMarkedSdrObj();
                 if ((dynamic_cast<const E3dObject* >(pObject) !=  nullptr) && (rMarkList.GetMarkCount() == 1))
                 {
-                    mpWindow->SetPointer(Pointer(PointerStyle::Rotate));
+                    mpWindow->SetPointer(PointerStyle::Rotate);
                     bDefPointer = false;     // Otherwise it'll be called Joe's routine and the mousepointer will reconfigurate again
                 }
             }
@@ -551,7 +552,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
                 {
                     pObj = nullptr;
                     bDefPointer = false;
-                    mpWindow->SetPointer(Pointer(PointerStyle::Arrow));
+                    mpWindow->SetPointer(PointerStyle::Arrow);
                 }
             }
 
@@ -648,7 +649,7 @@ bool FuDraw::SetPointer(SdrObject* pObj, const Point& rPos)
                     {
                         // Animation object
                         bSet = true;
-                        mpWindow->SetPointer(Pointer(PointerStyle::RefHand));
+                        mpWindow->SetPointer(PointerStyle::RefHand);
                     }
             }
             else if (bImageMapInfo &&
@@ -658,7 +659,7 @@ bool FuDraw::SetPointer(SdrObject* pObj, const Point& rPos)
                 * ImageMap
                 ******************************************************/
                 bSet = true;
-                mpWindow->SetPointer(Pointer(PointerStyle::RefHand));
+                mpWindow->SetPointer(PointerStyle::RefHand);
             }
         }
     }

sd/source/ui/func/fudspord.cxx
@@ -20,7 +20,7 @@
 #include <fudspord.hxx>
 
 #include <svx/svxids.hrc>
-#include <vcl/pointr.hxx>
+#include <vcl/ptrstyle.hxx>
 
 #include <app.hrc>
 #include <fupoor.hxx>
@@ -119,7 +119,7 @@ bool FuDisplayOrder::MouseButtonUp(const MouseEvent& rMEvt)
 void FuDisplayOrder::Activate()
 {
     maPtr = mpWindow->GetPointer();
-    mpWindow->SetPointer( Pointer( PointerStyle::RefHand ) );
+    mpWindow->SetPointer( PointerStyle::RefHand );
 }
 
 void FuDisplayOrder::Deactivate()

sd/source/ui/func/fuzoom.cxx
@@ -24,6 +24,7 @@
 #include <sfx2/viewfrm.hxx>
 #include <app.hrc>
 #include <svx/svdpagv.hxx>
+#include <vcl/ptrstyle.hxx>
 
 #include <FrameView.hxx>
 #include <ViewShell.hxx>
@@ -90,9 +91,9 @@ bool FuZoom::MouseButtonDown(const MouseEvent& rMEvt)
 bool FuZoom::MouseMove(const MouseEvent& rMEvt)
 {
     if (rMEvt.IsShift())
-        mpWindow->SetPointer(Pointer(PointerStyle::Hand));
+        mpWindow->SetPointer(PointerStyle::Hand);
     else if (nSlotId != SID_ZOOM_PANNING)
-        mpWindow->SetPointer(Pointer(PointerStyle::Magnify));
+        mpWindow->SetPointer(PointerStyle::Magnify);
 
     if (bStartDrag)
     {
@@ -197,11 +198,11 @@ void FuZoom::Activate()
 
     if (nSlotId == SID_ZOOM_PANNING)
     {
-        mpWindow->SetPointer(Pointer(PointerStyle::Hand));
+        mpWindow->SetPointer(PointerStyle::Hand);
     }
     else
     {
-        mpWindow->SetPointer(Pointer(PointerStyle::Magnify));
+        mpWindow->SetPointer(PointerStyle::Magnify);
     }
 }
 

svtools/source/hatchwindow/ipwin.cxx
@@ -23,6 +23,7 @@
 #include <vcl/event.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/ptrstyle.hxx>
 
 #include "ipwin.hxx"
 #include <hatchwindow.hxx>
@@ -483,10 +484,10 @@ void SvResizeWindow::SelectMouse( const Point & rPos )
         if( m_nMoveGrab == -1 ) // the first time
         {
             m_aOldPointer = GetPointer();
-            SetPointer( Pointer( aStyle ) );
+            SetPointer( aStyle );
         }
         else
-            SetPointer( Pointer( aStyle ) );
+            SetPointer( aStyle );
     }
     m_nMoveGrab = nGrab;
 }

svx/source/svdraw/svdcrtv.cxx
@@ -40,6 +40,7 @@
 #include <svx/sdr/contact/viewcontact.hxx>
 #include <svx/sdr/overlay/overlayprimitive2dsequenceobject.hxx>
 #include <basegfx/matrix/b2dhommatrixtools.hxx>
+#include <vcl/ptrstyle.hxx>
 
 using namespace com::sun::star;
 
@@ -185,7 +186,7 @@ void SdrCreateView::ImpClearVars()
     pCurrentCreate=nullptr;
     pCreatePV=nullptr;
     b1stPointAsCenter=false;
-    aCurrentCreatePointer=Pointer(PointerStyle::Cross);
+    aCurrentCreatePointer=PointerStyle::Cross;
     bUseIncompatiblePathCreateInterface=false;
     nAutoCloseDistPix=5;
     nFreeHandMinDistPix=10;
@@ -367,7 +368,7 @@ void SdrCreateView::SetCurrentObj(sal_uInt16 nIdent, SdrInventor nInvent)
         }
         else
         {
-            aCurrentCreatePointer = Pointer(PointerStyle::Cross);
+            aCurrentCreatePointer = PointerStyle::Cross;
         }
     }
 

toolkit/source/awt/vclxpointer.cxx
@@ -37,14 +37,14 @@ void VCLXPointer::setType( sal_Int32 nType )
 {
     ::osl::Guard< ::osl::Mutex > aGuard( GetMutex() );
 
-    maPointer = Pointer( static_cast<PointerStyle>(nType) );
+    maPointer = static_cast<PointerStyle>(nType);
 }
 
 sal_Int32 VCLXPointer::getType()
 {
     ::osl::Guard< ::osl::Mutex > aGuard( GetMutex() );
 
-    return static_cast<sal_Int32>(maPointer.GetStyle());
+    return static_cast<sal_Int32>(maPointer);
 }
 
 OUString VCLXPointer::getImplementationName()

vcl/source/control/fixedhyper.cxx
@@ -22,6 +22,7 @@
 #include <vcl/settings.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/weld.hxx>
+#include <vcl/ptrstyle.hxx>
 #include <comphelper/anytostring.hxx>
 #include <comphelper/processfactory.hxx>
 #include <cppuhelper/exc_hlp.hxx>

sc/source/ui/drawfunc/fudraw.cxx
@@ -664,7 +664,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
 
         if ( pView->IsTextEdit() )
         {
-            rViewShell.SetActivePointer(Pointer(PointerStyle::Text));        // can't be ?
+            rViewShell.SetActivePointer(PointerStyle::Text);        // can't be ?
         }
         else if ( pHdl )
         {
@@ -673,13 +673,13 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
         }
         else if ( pView->IsMarkedHit(aPnt) )
         {
-            rViewShell.SetActivePointer( Pointer(PointerStyle::Move) );
+            rViewShell.SetActivePointer( PointerStyle::Move );
         }
         else if ( !bAlt && ( !pMEvt || !pMEvt->GetButtons() )
                         && lcl_UrlHit( pView, aPosPixel, pWindow ) )
         {
             //  could be suppressed with ALT
-            pWindow->SetPointer( Pointer( PointerStyle::RefHand ) );          // Text-URL / ImageMap
+            pWindow->SetPointer( PointerStyle::RefHand );          // Text-URL / ImageMap
         }
         else if ( !bAlt && (pObj = pView->PickObj(aPnt, pView->getHitTolLog(), pPV, SdrSearchOptions::PICKMACRO)) )
         {
@@ -688,9 +688,9 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
             rViewShell.SetActivePointer( pObj->GetMacroPointer(aHitRec) );
         }
         else if ( !bAlt && pInfo && (!pInfo->GetMacro().isEmpty() || !pInfo->GetHlink().isEmpty()) )
-            pWindow->SetPointer( Pointer( PointerStyle::RefHand ) );
+            pWindow->SetPointer( PointerStyle::RefHand );
         else if ( IsDetectiveHit( aPnt ) )
-            rViewShell.SetActivePointer( Pointer( PointerStyle::Detective ) );
+            rViewShell.SetActivePointer( PointerStyle::Detective );
         else
             rViewShell.SetActivePointer( aNewPointer );            //! in Gridwin?
     }

commit 7f639e5885901a2d7c7758b44ce8be77dbf605dc

tdf#123439 Assert wrt PointerStyle fails when moving cursor over toolbar
regression from
    commit 9be0a088413cbb98970872ac594c0f333c697309
    Date:   Tue Feb 12 10:12:03 2019 +0200
    Pointer is pointless

Change-Id: I727e68c71f4ebacb9adae83f1f9b43ce999d5374
Reviewed-on: https://gerrit.libreoffice.org/67803
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/func/fudraw.cxx
@@ -75,6 +75,8 @@ namespace sd {
 FuDraw::FuDraw(ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView,
                SdDrawDocument* pDoc, SfxRequest& rReq)
     : FuPoor(pViewSh, pWin, pView, pDoc, rReq)
+    , aNewPointer(PointerStyle::Arrow)
+    , aOldPointer(PointerStyle::Arrow)
     , bMBDown(false)
     , bDragHelpLine(false)
     , nHelpLine(0)

sd/source/ui/func/fudspord.cxx
@@ -34,6 +34,7 @@ namespace sd {
 
 FuDisplayOrder::FuDisplayOrder( ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView, SdDrawDocument* pDoc, SfxRequest& rReq)
 : FuPoor(pViewSh, pWin, pView, pDoc, rReq)
+, maPtr(PointerStyle::Arrow)
 , mpRefObj(nullptr)
 {
 }

sd/source/ui/func/fuzoom.cxx
@@ -50,7 +50,8 @@ FuZoom::FuZoom(
     SfxRequest& rReq)
     : FuPoor(pViewSh, pWin, pView, pDoc, rReq),
       bVisible(false),
-      bStartDrag(false)
+      bStartDrag(false),
+      aPtr(PointerStyle::Arrow)
 {
 }
 

svtools/source/hatchwindow/ipwin.cxx
@@ -434,6 +434,7 @@ SvResizeWindow::SvResizeWindow
     VCLXHatchWindow* pWrapper
 )
     : Window( pParent, WB_CLIPCHILDREN )
+    , m_aOldPointer(PointerStyle::Arrow)
     , m_nMoveGrab( -1 )
     , m_bActive( false )
     , m_pWrapper( pWrapper )

svx/source/svdraw/svdcrtv.cxx
@@ -198,7 +198,8 @@ SdrCreateView::SdrCreateView(
     SdrModel& rSdrModel,
     OutputDevice* pOut)
 :   SdrDragView(rSdrModel, pOut),
-    mpCreateViewExtraData(new ImpSdrCreateViewExtraData())
+    mpCreateViewExtraData(new ImpSdrCreateViewExtraData()),
+    aCurrentCreatePointer(PointerStyle::Arrow)
 {
     ImpClearVars();
 }

toolkit/source/awt/vclxpointer.cxx
@@ -22,7 +22,7 @@
 #include <toolkit/helper/macros.hxx>
 #include <cppuhelper/supportsservice.hxx>
 
-VCLXPointer::VCLXPointer()
+VCLXPointer::VCLXPointer() : maPointer(PointerStyle::Arrow)
 {
 }
 

vcl/source/control/fixedhyper.cxx
@@ -36,6 +36,7 @@ using namespace css;
 FixedHyperlink::FixedHyperlink(vcl::Window* pParent, WinBits nWinStyle)
     : FixedText(pParent, nWinStyle)
     , m_nTextLen(0)
+    , m_aOldPointer(PointerStyle::Arrow)
 {
     Initialize();
 }

vcl/source/window/window.cxx
@@ -44,6 +44,7 @@
 #include <vcl/virdev.hxx>
 #include <vcl/settings.hxx>
 #include <vcl/sysdata.hxx>
+#include <vcl/ptrstyle.hxx>
 #include <vcl/IDialogRenderable.hxx>
 
 #include <vcl/uitest/uiobject.hxx>
@@ -612,6 +613,7 @@ WindowImpl::WindowImpl( WindowType nType )
     mnEventListenersIteratingCount = 0;
     mnChildEventListenersIteratingCount = 0;
     mpCursor                            = nullptr;                      // cursor
+    maPointer                           = PointerStyle::Arrow;
     mpVCLXWindow                        = nullptr;
     mpAccessibleInfos                   = nullptr;
     maControlForeground                 = COL_TRANSPARENT;  // no foreground set