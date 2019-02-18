LemmingAvalanche/libreoffice-repaired

commit 219cf34c729094add2243ec1050ffbbdb1a486fc

when calling std::lower_bound
it's not enough to compare != end(), you also need to compare the key
against the iterator result

Change-Id: Ide5f151ba2297a35e5546f47fbc3c53cbe5ab533
Reviewed-on: https://gerrit.libreoffice.org/62014
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

basic/source/classes/propacc.cxx
@@ -38,13 +38,10 @@ using namespace com::sun::star::lang;
 using namespace com::sun::star::beans;
 using namespace cppu;
 
-struct SbCompare_UString_PropertyValue_Impl
+static bool SbCompare_UString_PropertyValue_Impl(PropertyValue const & lhs, const OUString& rhs)
 {
-   bool operator() (PropertyValue const & lhs, const OUString& rhs)
-   {
-      return lhs.Name.compareTo(rhs) < 0;
-   }
-};
+    return lhs.Name.compareTo(rhs) < 0;
+}
 
 
 SbPropertyValues::SbPropertyValues()
@@ -82,8 +79,8 @@ size_t SbPropertyValues::GetIndex_Impl( const OUString &rPropName ) const
 {
     SbPropertyValueArr_Impl::const_iterator it = std::lower_bound(
           m_aPropVals.begin(), m_aPropVals.end(), rPropName,
-          SbCompare_UString_PropertyValue_Impl() );
-    if (it == m_aPropVals.end())
+          SbCompare_UString_PropertyValue_Impl );
+    if (it == m_aPropVals.end() || !SbCompare_UString_PropertyValue_Impl(*it, rPropName))
     {
         throw beans::UnknownPropertyException(
                 "Property not found: " + rPropName,

comphelper/source/property/propagg.cxx
@@ -239,7 +239,7 @@ sal_Int32 OPropertyArrayAggregationHelper::fillHandles(
     {
         aNameProp.Name = pReqProps[i];
         auto findIter = std::lower_bound(m_aProperties.begin(), m_aProperties.end(), aNameProp, PropertyCompareByName());
-        if ( findIter != m_aProperties.end() )
+        if ( findIter != m_aProperties.end() && !PropertyCompareByName()(*findIter, aNameProp))
         {
             _pHandles[i] = findIter->Handle;
             nHitCount++;

vcl/source/font/font.cxx
@@ -610,9 +610,8 @@ namespace
                     aEnt.string = pOpen+1;
                     aEnt.string_len = (pClose-pOpen)-1;
                     aEnt.weight = WEIGHT_NORMAL;
-                    const int nEnt = SAL_N_ELEMENTS( weight_table );
-                    WeightSearchEntry const * pFound = std::lower_bound( weight_table, weight_table+nEnt, aEnt );
-                    if( pFound != (weight_table+nEnt) )
+                    WeightSearchEntry const * pFound = std::lower_bound( std::begin(weight_table), std::end(weight_table), aEnt );
+                    if( pFound != std::end(weight_table) && !(*pFound < aEnt))
                         o_rResult.SetWeight( pFound->weight );
                 }
             }

svx/source/smarttags/SmartTagMgr.cxx
@@ -180,7 +180,7 @@ OUString SmartTagMgr::GetSmartTagCaption( const OUString& rSmartTagType, const c
 {
     OUString aRet;
 
-    auto aLower = maSmartTagMap.lower_bound( rSmartTagType );
+    auto aLower = maSmartTagMap.find( rSmartTagType );
 
     if ( aLower != maSmartTagMap.end() )
     {

writerfilter/source/rtftok/rtftokenizer.cxx
@@ -247,10 +247,9 @@ bool RTFTokenizer::lookupMathKeyword(RTFMathSymbol& rSymbol)
 {
     auto low
         = std::lower_bound(s_aRTFMathControlWords.begin(), s_aRTFMathControlWords.end(), rSymbol);
-    int i = low - s_aRTFMathControlWords.begin();
     if (low == s_aRTFMathControlWords.end() || rSymbol < *low)
         return false;
-    rSymbol = s_aRTFMathControlWords[i];
+    rSymbol = *low;
     return true;
 }
 

commit 26b85cc1a9c4f496847931bb0a4b13976e5f90c1

fix comparison of std::lower_bound result
turns out using a compare operator that takes something other than the
element type as a parameter is really hard to get right.

The changes in:
    basic/source/classes/propacc.cxx
    comphelper/source/property/propagg.cxx
    vcl/source/font/font.cxx
are a regression from
    commit 219cf34c729094add2243ec1050ffbbdb1a486fc
    Date:   Fri Oct 19 16:01:19 2018 +0200
    when calling std::lower_bound

The change in
    toolkit/source/awt/vclxtoolkit.cxx
is a regression from
    commit bbae9fe1b11bde051a768aa9003b38cf11a2deac
    Date:   Tue Oct 9 16:27:11 2018 +0200
    loplugin:staticvar in various

Change-Id: Icf6db22e1fc091517a53dd3624913c8c2071c106
Reviewed-on: https://gerrit.libreoffice.org/67954
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

basic/source/classes/propacc.cxx
@@ -80,7 +80,7 @@ size_t SbPropertyValues::GetIndex_Impl( const OUString &rPropName ) const
     SbPropertyValueArr_Impl::const_iterator it = std::lower_bound(
           m_aPropVals.begin(), m_aPropVals.end(), rPropName,
           SbCompare_UString_PropertyValue_Impl );
-    if (it == m_aPropVals.end() || !SbCompare_UString_PropertyValue_Impl(*it, rPropName))
+    if (it == m_aPropVals.end() || it->Name != rPropName)
     {
         throw beans::UnknownPropertyException(
                 "Property not found: " + rPropName,

comphelper/source/property/propagg.cxx
@@ -239,7 +239,7 @@ sal_Int32 OPropertyArrayAggregationHelper::fillHandles(
     {
         aNameProp.Name = pReqProps[i];
         auto findIter = std::lower_bound(m_aProperties.begin(), m_aProperties.end(), aNameProp, PropertyCompareByName());
-        if ( findIter != m_aProperties.end() && !PropertyCompareByName()(*findIter, aNameProp))
+        if ( findIter != m_aProperties.end() && findIter->Name == pReqProps[i] )
         {
             _pHandles[i] = findIter->Handle;
             nHitCount++;

toolkit/source/awt/vclxtoolkit.cxx
@@ -791,7 +791,8 @@ WindowType ImplGetComponentType( const OUString& rServiceName )
 
     auto it = std::lower_bound( std::begin(aComponentInfos), std::end(aComponentInfos), sSearch,
                                 ComponentInfoFindCompare );
-    if (it != std::end(aComponentInfos)  && !ComponentInfoFindCompare(*it, sSearch) )
+    if (it != std::end(aComponentInfos) &&
+        rtl_ustr_ascii_compareIgnoreAsciiCase_WithLengths(sSearch.pData->buffer, sSearch.pData->length, it->sName.data, it->sName.size) == 0)
         return it->nWinType;
     return WindowType::NONE;
 }

vcl/source/font/font.cxx
@@ -611,7 +611,8 @@ namespace
                     aEnt.string_len = (pClose-pOpen)-1;
                     aEnt.weight = WEIGHT_NORMAL;
                     WeightSearchEntry const * pFound = std::lower_bound( std::begin(weight_table), std::end(weight_table), aEnt );
-                    if( pFound != std::end(weight_table) && !(*pFound < aEnt))
+                    if( pFound != std::end(weight_table) &&
+                        rtl_str_compareIgnoreAsciiCase_WithLength( pFound->string, pFound->string_len, aEnt.string, aEnt.string_len) == 0 )
                         o_rResult.SetWeight( pFound->weight );
                 }
             }