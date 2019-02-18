LemmingAvalanche/libreoffice-repaired

commit ea88c8906479fa5a6baf0e433d16c92cdbb4955a

tdf#90243 don't append index if shape already has a name.
Change-Id: I518ad83bf3782af7cb3144982ec8844a573f0de4
Reviewed-on: https://gerrit.libreoffice.org/56051
Tested-by: Jenkins
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

sd/source/ui/animations/CustomAnimationList.cxx
@@ -124,16 +124,25 @@ OUString getShapeDescription( const Reference< XShape >& xShape, bool bWithText 
 {
     OUString aDescription;
     Reference< XPropertySet > xSet( xShape, UNO_QUERY );
+    bool bAppendIndex = true;
+
     if( xSet.is() )
     {
-        Reference< XPropertySetInfo > xInfo( xSet->getPropertySetInfo() );
-        const OUString aPropName( "UINameSingular");
-        if( xInfo->hasPropertyByName( aPropName ) )
-            xSet->getPropertyValue( aPropName ) >>= aDescription;
+        Reference<XPropertySetInfo> xInfo(xSet->getPropertySetInfo());
+
+        xSet->getPropertyValue("Name") >>= aDescription;
+        bAppendIndex = aDescription.isEmpty();
+
+        const OUString aPropName("UINameSingular");
+        if(xInfo->hasPropertyByName(aPropName))
+            xSet->getPropertyValue(aPropName) >>= aDescription;
     }
 
-    aDescription += " ";
-    aDescription += OUString::number( getShapeIndex( xShape ) );
+    if (bAppendIndex)
+    {
+        aDescription += " ";
+        aDescription += OUString::number(getShapeIndex(xShape));
+    }
 
     if( bWithText )
     {

commit e2d97ebbcd0f03ac124c0ee89fa01fde9d0ea515

sd: fix crashing when adding new effects to media.
Check if the objct has name roperty with hasPropertyByName() before
accessing in case some objects ( media, table, control, etc. ) do not
have name property.

Regression caused by ea88c8906479f ( related to tdf#90243 ).

Change-Id: I05fc45c631f8acd3b9ba8c8305d4bc1e31651824
Reviewed-on: https://gerrit.libreoffice.org/67901
Tested-by: Jenkins
Reviewed-by: Mark Hung <marklh9@gmail.com>

sd/source/ui/animations/CustomAnimationList.cxx
@@ -129,16 +129,25 @@ OUString getShapeDescription( const Reference< XShape >& xShape, bool bWithText 
     Reference< XPropertySet > xSet( xShape, UNO_QUERY );
     bool bAppendIndex = true;
 
-    if( xSet.is() )
+    if(xSet.is()) try
     {
         Reference<XPropertySetInfo> xInfo(xSet->getPropertySetInfo());
+        if (xInfo.is())
+        {
+            const OUString aPropName1("Name");
+            if(xInfo->hasPropertyByName(aPropName1))
+                xSet->getPropertyValue(aPropName1) >>= aDescription;
 
-        xSet->getPropertyValue("Name") >>= aDescription;
-        bAppendIndex = aDescription.isEmpty();
+            bAppendIndex = aDescription.isEmpty();
 
-        const OUString aPropName("UINameSingular");
-        if(xInfo->hasPropertyByName(aPropName))
-            xSet->getPropertyValue(aPropName) >>= aDescription;
+            const OUString aPropName2("UINameSingular");
+            if(xInfo->hasPropertyByName(aPropName2))
+                xSet->getPropertyValue(aPropName2) >>= aDescription;
+        }
+    }
+    catch( Exception& )
+    {
+        OSL_FAIL("sd::getShapeDescription(), exception caught!" );
     }
 
     if (bAppendIndex)