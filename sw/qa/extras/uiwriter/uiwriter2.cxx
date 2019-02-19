LemmingAvalanche/libreoffice-repaired

commit 79060186c9fff38e5744ce5e1fa007fdecd6b7fe

SwView::SearchAndWrap: fix WrapAround search in fly frames
First, SttDoc() / EndDoc() is the implementation of Ctrl-Home, i.e. it
goes to the start of the current text (like a fly frame), not to the
start of the whole document. When wrapping around, we want the later.

Second, if the normal search have two passes, first searching in the
body text, then searching in the special sections, then the wrap-around
search should do the same.

Change-Id: I0b7466c80476f6fb45174be19215a5d68374d047

sw/source/uibase/uiview/viewsrch.cxx
@@ -554,13 +554,24 @@ bool SwView::SearchAndWrap(bool bApi)
     if (bHasSrchInOther)
     {
         m_pWrtShell->ClearMark();
+        // Select the start or the end of the entire document
         if (bSrchBkwrd)
-            m_pWrtShell->EndDoc();
+            m_pWrtShell->SttEndDoc(false);
         else
-            m_pWrtShell->SttDoc();
+            m_pWrtShell->SttEndDoc(true);
     }
 
     m_bFound = bool(FUNC_Search( aOpts ));
+
+    // If WrapAround found no matches in the body text, search in the special
+    // sections, too.
+    if (!m_bFound && !m_pSrchItem->GetSelection() && !m_bExtra)
+    {
+        m_bExtra = true;
+        if (FUNC_Search(aOpts))
+            m_bFound = true;
+    }
+
     m_pWrtShell->EndAllAction();
     pWait.reset();
 #if HAVE_FEATURE_DESKTOP

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -26,6 +26,7 @@ static const char* DATA_DIRECTORY = "/sw/qa/extras/tiledrendering/data/";
 class SwTiledRenderingTest : public SwModelTestBase
 {
 public:
+    SwTiledRenderingTest();
     void testRegisterCallback();
     void testPostKeyEvent();
     void testPostMouseEvent();
@@ -35,6 +36,7 @@ public:
     void testSearch();
     void testSearchViewArea();
     void testSearchTextFrame();
+    void testSearchTextFrameWrapAround();
     void testDocumentSizeChanged();
 
     CPPUNIT_TEST_SUITE(SwTiledRenderingTest);
@@ -47,6 +49,7 @@ public:
     CPPUNIT_TEST(testSearch);
     CPPUNIT_TEST(testSearchViewArea);
     CPPUNIT_TEST(testSearchTextFrame);
+    CPPUNIT_TEST(testSearchTextFrameWrapAround);
     CPPUNIT_TEST(testDocumentSizeChanged);
     CPPUNIT_TEST_SUITE_END();
 
@@ -57,7 +60,13 @@ private:
     Rectangle m_aInvalidation;
     Size m_aDocumentSize;
     OString m_aTextSelection;
+    bool m_bFound;
 };
+
+SwTiledRenderingTest::SwTiledRenderingTest()
+    : m_bFound(true)
+{
+}
 
 SwXTextDocument* SwTiledRenderingTest::createDoc(const char* pName)
 {
@@ -104,6 +113,11 @@ void SwTiledRenderingTest::callbackImpl(int nType, const char* pPayload)
     case LOK_CALLBACK_TEXT_SELECTION:
     {
         m_aTextSelection = pPayload;
+    }
+    break;
+    case LOK_CALLBACK_SEARCH_NOT_FOUND:
+    {
+        m_bFound = false;
     }
     break;
     }
@@ -339,6 +353,24 @@ void SwTiledRenderingTest::testSearchTextFrame()
     comphelper::dispatchCommand(".uno:ExecuteSearch", aPropertyValues);
     // This was empty: nothing was highlighted after searching for 'TextFrame'.
     CPPUNIT_ASSERT(!m_aTextSelection.isEmpty());
+#endif
+}
+
+void SwTiledRenderingTest::testSearchTextFrameWrapAround()
+{
+#if !defined(WNT) && !defined(MACOSX)
+    SwXTextDocument* pXTextDocument = createDoc("search.odt");
+    pXTextDocument->registerCallback(&SwTiledRenderingTest::callback, this);
+    uno::Sequence<beans::PropertyValue> aPropertyValues(comphelper::InitPropertySequence(
+    {
+        {"SearchItem.SearchString", uno::makeAny(OUString("TextFrame"))},
+        {"SearchItem.Backward", uno::makeAny(false)},
+    }));
+    comphelper::dispatchCommand(".uno:ExecuteSearch", aPropertyValues);
+    CPPUNIT_ASSERT(m_bFound);
+    comphelper::dispatchCommand(".uno:ExecuteSearch", aPropertyValues);
+    // This failed, i.e. the second time 'not found' was reported, instead of wrapping around.
+    CPPUNIT_ASSERT(m_bFound);
 #endif
 }
 

commit 268ac044f09decce0311b9df2c98d53e1c7d2a1b

tdf#101873 sw: fix search for second term after double not found
Regression from commit 79060186c9fff38e5744ce5e1fa007fdecd6b7fe
(SwView::SearchAndWrap: fix WrapAround search in fly frames,
2015-06-03).

The search implementation looks in the body text, then in case searching
in extra content was not enabled already, it attempts to search there.

The problem was that while the wrap-around implementation intended to do
the same, it failed to unset the "search in extra" flag in case nothing
was found, fix that now.

Change-Id: Idf078e464824cad69c9709309435f2db207503b8
Reviewed-on: https://gerrit.libreoffice.org/67986
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/uibase/uiview/viewsrch.cxx
@@ -608,6 +608,8 @@ bool SwView::SearchAndWrap(bool bApi)
         m_bExtra = true;
         if (FUNC_Search(aOpts))
             m_bFound = true;
+        else
+            m_bExtra = false;
     }
 
     m_pWrtShell->EndAllAction();

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -60,6 +60,7 @@ public:
     void testTdf122901();
     void testTdf122942();
     void testTdf52391();
+    void testTdf101873();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest2);
     CPPUNIT_TEST(testRedlineMoveInsertInDelete);
@@ -81,6 +82,7 @@ public:
     CPPUNIT_TEST(testTdf122901);
     CPPUNIT_TEST(testTdf122942);
     CPPUNIT_TEST(testTdf52391);
+    CPPUNIT_TEST(testTdf101873);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -876,6 +878,40 @@ void SwUiWriterTest2::testTdf52391()
     // accepted for "Reject All". Now rejection clears formatting of the text
     // in format-only changes, concatenating the text portions in the first paragraph.
     CPPUNIT_ASSERT_EQUAL(OUString("Portion1Portion2"), xRun->getString());
+}
+
+void SwUiWriterTest2::testTdf101873()
+{
+    SwDoc* pDoc = createDoc();
+    CPPUNIT_ASSERT(pDoc);
+
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    CPPUNIT_ASSERT(pDocShell);
+
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    // Insert some content.
+    pWrtShell->Insert("something");
+
+    // Search for something which does not exist, twice.
+    uno::Sequence<beans::PropertyValue> aFirst(comphelper::InitPropertySequence({
+        { "SearchItem.SearchString", uno::makeAny(OUString("fig")) },
+        { "SearchItem.Backward", uno::makeAny(false) },
+    }));
+    lcl_dispatchCommand(mxComponent, ".uno:ExecuteSearch", aFirst);
+    lcl_dispatchCommand(mxComponent, ".uno:ExecuteSearch", aFirst);
+
+    uno::Sequence<beans::PropertyValue> aSecond(comphelper::InitPropertySequence({
+        { "SearchItem.SearchString", uno::makeAny(OUString("something")) },
+        { "SearchItem.Backward", uno::makeAny(false) },
+    }));
+    lcl_dispatchCommand(mxComponent, ".uno:ExecuteSearch", aSecond);
+
+    // Without the accompanying fix in place, this test would have failed with "Expected: something;
+    // Actual:", i.e. searching for "something" failed, even if it was inserted above.
+    SwShellCursor* pShellCursor = pWrtShell->getShellCursor(false);
+    CPPUNIT_ASSERT_EQUAL(OUString("something"), pShellCursor->GetText());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest2);