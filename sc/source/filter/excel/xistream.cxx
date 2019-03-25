LemmingAvalanche/libreoffice-repaired

commit 952103f66ac57c49ad597a81028091d0bce481de

loplugin:stringloop in sc
Change-Id: I12c020d5dd75d9c8ffcb7a2e8d6ff310628fa04d
Reviewed-on: https://gerrit.libreoffice.org/58432
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/excel/xistream.cxx
@@ -826,7 +826,7 @@ std::size_t XclImpStream::ReadUniStringExtHeader( bool& rb16Bit, sal_uInt8 nFlag
 
 OUString XclImpStream::ReadRawUniString( sal_uInt16 nChars, bool b16Bit )
 {
-    OUString aRet;
+    OUStringBuffer aRet;
     sal_uInt16 nCharsLeft = nChars;
     sal_uInt16 nReadSize;
 
@@ -866,14 +866,14 @@ OUString XclImpStream::ReadRawUniString( sal_uInt16 nChars, bool b16Bit )
         }
 
         *pcEndChar = '\0';
-        aRet += OUString( pcBuffer.get() );
+        aRet.append( pcBuffer.get(), pcUniChar - pcBuffer.get()  );
 
         nCharsLeft = nCharsLeft - nReadSize;
         if( nCharsLeft > 0 )
             JumpToNextStringContinue( b16Bit );
     }
 
-    return aRet;
+    return aRet.makeStringAndClear();
 }
 
 OUString XclImpStream::ReadUniString( sal_uInt16 nChars, sal_uInt8 nFlags )

sc/source/core/data/docpool.cxx
@@ -408,13 +408,12 @@ static bool lcl_HFPresentation
 
     SfxItemIter aIter( rSet );
     pItem = aIter.FirstItem();
-    OUString aText;
 
     while( pItem )
     {
         sal_uInt16 nWhich = pItem->Which();
 
-        aText.clear();
+        OUString aText;
 
         switch( nWhich )
         {

sc/source/core/data/drwlayer.cxx
@@ -1882,8 +1882,7 @@ OUString ScDrawLayer::GetNewGraphicName( long* pnCounter ) const
     while (bThere)
     {
         ++nId;
-        aGraphicName = aBase;
-        aGraphicName += OUString::number( nId );
+        aGraphicName = aBase + OUString::number( nId );
         bThere = ( GetNamedObject( aGraphicName, 0, nDummy ) != nullptr );
     }
 

sc/source/core/data/table3.cxx
@@ -2005,7 +2005,6 @@ bool ScTable::DoSubTotals( ScSubTotalParam& rParam )
     bool bTestPrevSub = ( nLevelCount > 1 );
 
     OUString  aSubString;
-    OUString  aOutString;
 
     bool bIgnoreCase = !rParam.bCaseSens;
 
@@ -2111,7 +2110,7 @@ bool ScTable::DoSubTotals( ScSubTotalParam& rParam )
                         // collect formula positions
                         aRowVector.push_back( aRowEntry );
 
-                        aOutString = aSubString;
+                        OUString aOutString = aSubString;
                         if (aOutString.isEmpty())
                             aOutString = ScResId( STR_EMPTYDATA );
                         aOutString += " ";

sc/source/core/tool/compiler.cxx
@@ -480,7 +480,8 @@ static bool lcl_parseExternalName(
     const sal_Unicode* const pStart = rSymbol.getStr();
     const sal_Unicode* p = pStart;
     sal_Int32 nLen = rSymbol.getLength();
-    OUString aTmpFile, aTmpName;
+    OUString aTmpFile;
+    OUStringBuffer aTmpName;
     sal_Int32 i = 0;
     bool bInName = false;
     if (cSep == '!')
@@ -543,7 +544,7 @@ static bool lcl_parseExternalName(
 
                         i = j;
                         bInName = true;
-                        aTmpName += OUStringLiteral1(c); // Keep the separator as part of the name.
+                        aTmpName.append(c); // Keep the separator as part of the name.
                         break;
                     }
                     aTmpFile += OUStringLiteral1(c);
@@ -573,14 +574,14 @@ static bool lcl_parseExternalName(
                 // A second separator ?  Not a valid external name.
                 return false;
             }
-            aTmpName += OUStringLiteral1(c);
+            aTmpName.append(c);
         }
         else
         {
             if (c == cSep)
             {
                 bInName = true;
-                aTmpName += OUStringLiteral1(c); // Keep the separator as part of the name.
+                aTmpName.append(c); // Keep the separator as part of the name.
             }
             else
             {
@@ -637,12 +638,12 @@ static bool lcl_parseExternalName(
     if (aTmpName[nNameLen-1] == '!')
     {
         // Check against #REF!.
-        if (aTmpName.equalsIgnoreAsciiCase("#REF!"))
+        if (aTmpName.toString().equalsIgnoreAsciiCase("#REF!"))
             return false;
     }
 
     rFile = aTmpFile;
-    rName = aTmpName.copy(1); // Skip the first char as it is always the separator.
+    rName = aTmpName.makeStringAndClear().copy(1); // Skip the first char as it is always the separator.
     return true;
 }
 
@@ -3981,7 +3982,8 @@ void ScCompiler::AutoCorrectParsedSymbol()
                 sal_Int32 nIndex = 0;
                 OUString aTmp1( aSymbol.getToken( 0, ':', nIndex ) );
                 sal_Int32 nLen1 = aTmp1.getLength();
-                OUString aSym, aTmp2;
+                OUStringBuffer aSym;
+                OUString aTmp2;
                 bool bLastAlp, bNextNum;
                 bLastAlp = bNextNum = true;
                 sal_Int32 nStrip = 0;
@@ -3994,7 +3996,7 @@ void ScCompiler::AutoCorrectParsedSymbol()
                     {
                         if ( nLen1 )
                         {
-                            aSym += aTmp1;
+                            aSym.append(aTmp1);
                             bLastAlp = CharClass::isAsciiAlpha( aTmp1 );
                         }
                         if ( nLen2 )
@@ -4009,8 +4011,8 @@ void ScCompiler::AutoCorrectParsedSymbol()
                             }
                             else
                             {
-                                if ( !aSym.isEmpty() && !aSym.endsWith(":"))
-                                    aSym += ":";
+                                if ( !aSym.isEmpty() && aSym[aSym.getLength()-1] != ':')
+                                    aSym.append(":");
                                 nStrip = 0;
                             }
                             bLastAlp = !bNextNum;
@@ -4030,7 +4032,7 @@ void ScCompiler::AutoCorrectParsedSymbol()
                     else
                         nRefs--;
                 }
-                aSymbol = aSym;
+                aSymbol = aSym.makeStringAndClear();
                 aSymbol += aTmp1;
             }
             else
@@ -4062,12 +4064,12 @@ void ScCompiler::AutoCorrectParsedSymbol()
                         aRef[j] = aRef[j].copy( nDotPos + 1 );
                     }
                     OUString aOld( aRef[j] );
-                    OUString aStr2;
+                    OUStringBuffer aStr2;
                     const sal_Unicode* p = aRef[j].getStr();
                     while ( *p && rtl::isAsciiDigit( *p ) )
-                        aStr2 += OUStringLiteral1(*p++);
+                        aStr2.append(*p++);
                     aRef[j] = OUString( p );
-                    aRef[j] += aStr2;
+                    aRef[j] += aStr2.makeStringAndClear();
                     if ( bColons || aRef[j] != aOld )
                     {
                         bChanged = true;

sc/source/core/tool/interpr2.cxx
@@ -3042,7 +3042,7 @@ void ScInterpreter::ScRoman()
             static const sal_uInt16 pValues[] = { 1000, 500, 100, 50, 10, 5, 1 };
             static const sal_uInt16 nMaxIndex = sal_uInt16(SAL_N_ELEMENTS(pValues) - 1);
 
-            OUString aRoman;
+            OUStringBuffer aRoman;
             sal_uInt16 nVal = static_cast<sal_uInt16>(fVal);
             sal_uInt16 nMode = static_cast<sal_uInt16>(fMode);
 
@@ -3066,8 +3066,7 @@ void ScInterpreter::ScRoman()
                         else
                             nSteps = nMode;
                     }
-                    aRoman += OUStringLiteral1( pChars[ nIndex ] )
-                        + OUStringLiteral1( pChars[ nIndex2 ] );
+                    aRoman.append( pChars[ nIndex ] ).append( pChars[ nIndex2 ] );
                     nVal = sal::static_int_cast<sal_uInt16>( nVal + pValues[ nIndex ] );
                     nVal = sal::static_int_cast<sal_uInt16>( nVal - pValues[ nIndex2 ] );
                 }
@@ -3077,7 +3076,7 @@ void ScInterpreter::ScRoman()
                     {
                         // assert can't happen with nVal<4000 precondition
                         assert( nIndex >= 1 );
-                        aRoman += OUStringLiteral1( pChars[ nIndex - 1 ] );
+                        aRoman.append( pChars[ nIndex - 1 ] );
                     }
                     sal_Int32 nPad = nDigit % 5;
                     if (nPad)
@@ -3091,7 +3090,7 @@ void ScInterpreter::ScRoman()
                 }
             }
 
-            PushString( aRoman );
+            PushString( aRoman.makeStringAndClear() );
         }
         else
             PushIllegalArgument();

sc/source/core/tool/reffind.cxx
@@ -241,7 +241,7 @@ void ScRefFinder::ToggleRel( sal_Int32 nStartPos, sal_Int32 nEndPos )
 
     ExpandToText(pSource, nLen, nStartPos, nEndPos, meConv);
 
-    OUString aResult;
+    OUStringBuffer aResult;
     OUString aExpr;
     OUString aSep;
     ScAddress aAddr;
@@ -314,14 +314,14 @@ void ScRefFinder::ToggleRel( sal_Int32 nStartPos, sal_Int32 nEndPos )
 
         // assemble
 
-        aResult += aSep;
-        aResult += aExpr;
+        aResult.append(aSep);
+        aResult.append(aExpr);
 
         nLoopStart = nEEnd;
     }
 
     OUString aTotal = maFormula.copy(0, nStartPos);
-    aTotal += aResult;
+    aTotal += aResult.makeStringAndClear();
     if (nEndPos < maFormula.getLength()-1)
         aTotal += maFormula.copy(nEndPos+1);
 

sc/source/filter/excel/xehelper.cxx
@@ -707,7 +707,7 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
     for( sal_Int32 nPara = 0; nPara < nParaCount; ++nPara )
     {
         ESelection aSel( nPara, 0 );
-        OUString aParaText;
+        OUStringBuffer aParaText;
         sal_Int32 nParaHeight = 0;
         std::vector<sal_Int32> aPosList;
         mrEE.GetPortions( nPara, aPosList );
@@ -744,9 +744,9 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
                             aNewData.mbItalic ? ITALIC_NORMAL : ITALIC_NONE ) );
                         aNewData.maStyle = pFontList->GetStyleName( aFontMetric );
                         if( !aNewData.maStyle.isEmpty() )
-                            aParaText += "," + aNewData.maStyle;
+                            aParaText.append(",").append(aNewData.maStyle);
                     }
-                    aParaText += "\"";
+                    aParaText.append("\"");
                 }
 
                 // height
@@ -756,7 +756,7 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
                 (aNewData.mnHeight += 10) /= 20;
                 bool bFontHtChanged = (aFontData.mnHeight != aNewData.mnHeight);
                 if( bFontHtChanged )
-                    aParaText += "&" + OUString::number( aNewData.mnHeight );
+                    aParaText.append("&").append(OUString::number( aNewData.mnHeight ));
                 // update maximum paragraph height, convert to twips
                 nParaHeight = ::std::max< sal_Int32 >( nParaHeight, aNewData.mnHeight * 20 );
 
@@ -773,13 +773,13 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
                 {
                     sal_uInt8 nTmpUnderl = (aNewData.mnUnderline == EXC_FONTUNDERL_NONE) ?
                         aFontData.mnUnderline : aNewData.mnUnderline;
-                    (nTmpUnderl == EXC_FONTUNDERL_SINGLE)? aParaText += "&U" : aParaText += "&E";
+                    (nTmpUnderl == EXC_FONTUNDERL_SINGLE)? aParaText.append("&U") : aParaText.append("&E");
                 }
 
                 // strikeout
                 aNewData.mbStrikeout = (aFont.GetStrikeout() != STRIKEOUT_NONE);
                 if( aFontData.mbStrikeout != aNewData.mbStrikeout )
-                    aParaText += "&S";
+                    aParaText.append("&S");
 
                 // super/sub script
                 const SvxEscapementItem& rEscapeItem = aEditSet.Get( EE_CHAR_ESCAPEMENT );
@@ -789,9 +789,9 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
                     switch(aNewData.mnEscapem)
                     {
                         // close the previous super/sub script.
-                        case EXC_FONTESC_NONE:  (aFontData.mnEscapem == EXC_FONTESC_SUPER) ? aParaText += "&X" : aParaText += "&Y"; break;
-                        case EXC_FONTESC_SUPER: aParaText += "&X";  break;
-                        case EXC_FONTESC_SUB:   aParaText += "&Y";  break;
+                        case EXC_FONTESC_NONE:  (aFontData.mnEscapem == EXC_FONTESC_SUPER) ? aParaText.append("&X") : aParaText.append("&Y"); break;
+                        case EXC_FONTESC_SUPER: aParaText.append("&X");  break;
+                        case EXC_FONTESC_SUB:   aParaText.append("&Y");  break;
                         default: break;
                     }
                 }
@@ -807,30 +807,30 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
                     if( const SvxFieldData* pFieldData = static_cast< const SvxFieldItem* >( pItem )->GetField() )
                     {
                         if( dynamic_cast<const SvxPageField*>( pFieldData) !=  nullptr )
-                            aParaText += "&P";
+                            aParaText.append("&P");
                         else if( dynamic_cast<const SvxPagesField*>( pFieldData) !=  nullptr )
-                            aParaText += "&N";
+                            aParaText.append("&N");
                         else if( dynamic_cast<const SvxDateField*>( pFieldData) !=  nullptr )
-                            aParaText += "&D";
+                            aParaText.append("&D");
                         else if( dynamic_cast<const SvxTimeField*>( pFieldData) != nullptr || dynamic_cast<const SvxExtTimeField*>( pFieldData) !=  nullptr )
-                            aParaText += "&T";
+                            aParaText.append("&T");
                         else if( dynamic_cast<const SvxTableField*>( pFieldData) !=  nullptr )
-                            aParaText += "&A";
+                            aParaText.append("&A");
                         else if( dynamic_cast<const SvxFileField*>( pFieldData) !=  nullptr )  // title -> file name
-                            aParaText += "&F";
+                            aParaText.append("&F");
                         else if( const SvxExtFileField* pFileField = dynamic_cast<const SvxExtFileField*>( pFieldData )  )
                         {
                             switch( pFileField->GetFormat() )
                             {
                                 case SvxFileFormat::NameAndExt:
                                 case SvxFileFormat::NameOnly:
-                                    aParaText += "&F";
+                                    aParaText.append("&F");
                                 break;
                                 case SvxFileFormat::PathOnly:
-                                    aParaText += "&Z";
+                                    aParaText.append("&Z");
                                 break;
                                 case SvxFileFormat::PathFull:
-                                    aParaText += "&Z&F";
+                                    aParaText.append("&Z&F");
                                 break;
                                 default:
                                     OSL_FAIL( "XclExpHFConverter::AppendPortion - unknown file field" );
@@ -848,16 +848,16 @@ void XclExpHFConverter::AppendPortion( const EditTextObject* pTextObj, sal_Unico
                         sal_Unicode cLast = aParaText[ aParaText.getLength() - 1 ];
                         sal_Unicode cFirst = aPortionText[0];
                         if( ('0' <= cLast) && (cLast <= '9') && ('0' <= cFirst) && (cFirst <= '9') )
-                            aParaText += " ";
+                            aParaText.append(" ");
                     }
-                    aParaText += aPortionText;
+                    aParaText.append(aPortionText);
                 }
             }
 
             aSel.nStartPos = aSel.nEndPos;
         }
 
-        aText = ScGlobal::addToken( aText, aParaText, '\n' );
+        aText = ScGlobal::addToken( aText, aParaText.makeStringAndClear(), '\n' );
         if( nParaHeight == 0 )
             nParaHeight = aFontData.mnHeight * 20;  // points -> twips
         nHeight += nParaHeight;

commit b053389c6704a9a30c7462005bef0515bc6c974e

tdf#124318 FILEOPEN: XLS Hyperlink URL incorrect
regression from
    commit 952103f66ac57c49ad597a81028091d0bce481de
    Date:   Wed Aug 1 17:03:18 2018 +0200
    loplugin:stringloop in sc

Change-Id: I7d481ea27866fc146c5de9d027aa7bb308f14e52
Reviewed-on: https://gerrit.libreoffice.org/69652
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/excel/xistream.cxx
@@ -871,7 +871,9 @@ OUString XclImpStream::ReadRawUniString( sal_uInt16 nChars, bool b16Bit )
         }
 
         *pcEndChar = '\0';
-        aRet.append( pcBuffer.get(), pcUniChar - pcBuffer.get()  );
+        // this has the side-effect of only copying as far as the first null, which appears to be intentional. e.g.
+        // see tdf#124318
+        aRet.append( pcBuffer.get() );
 
         nCharsLeft = nCharsLeft - nReadSize;
         if( nCharsLeft > 0 )