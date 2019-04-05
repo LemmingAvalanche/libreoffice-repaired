LemmingAvalanche/libreoffice-repaired

commit fe71af54c013f5084b8711129c9b60462c682610

tdf#122927 prevent SolarMutex deadlock on join()
The AsyncRequests thread can be blocked in a SolarMutexGuard, so
we have to release the mutex before calling join().

But there is also a static AsyncRequests object inside the
VistaFilePickerEventHandler::impl_sendEvent function, which will
just be cleaned up at DLL atexit.

"Luckily" it won't ever run, so it doesn't need to be joined and
we can use isRunning to detect it, prevent releasing a non-
existing SolarMutex at this point and so won't crash LO on exit.

Change-Id: I0c56b89a11f96be54e82b756c5e18a058b04a41d
Reviewed-on: https://gerrit.libreoffice.org/66901
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

fpicker/source/win32/asyncrequests.cxx
@@ -75,7 +75,14 @@ AsyncRequests::~AsyncRequests()
     aLock.clear();
     // <- SYNCHRONIZED
 
-    join();
+    // The static AsyncRequests aNotify in VistaFilePickerEventHandler::impl_sendEvent
+    // is destructed at DLL atexit. But it won't run, so needs no join and release of
+    // the already destructed SolarMutex, which would crash LO on exit.
+    if (isRunning())
+    {
+        SolarMutexReleaser aReleaser;
+        join();
+    }
 }
 
 void AsyncRequests::triggerJobExecution()

fpicker/source/win32/VistaFilePickerEventHandler.cxx
@@ -289,6 +289,7 @@ public:
 void VistaFilePickerEventHandler::impl_sendEvent(  EEventType eEventType,
                                                  ::sal_Int16  nControlID)
 {
+    // See special handling in ~AsyncRequests for this static
     static AsyncRequests aNotify(RequestHandlerRef(new AsyncPickerEvents()));
 
     ::cppu::OInterfaceContainerHelper* pContainer = m_lListener.getContainer( cppu::UnoType<css::ui::dialogs::XFilePickerListener>::get());

commit 733b910c8e3f684144df655758ed2afcdc7fa80b

tdf#123502: Make sure to hold the mutex before releasing it
Fixes a regression after commit fe71af54c013f5084b8711129c9b60462c682610

The destruction of the dialog is initiated by a callback executed from
VCLXButton::ProcessWindowEvent, which notifies listeners, using a call
to VCLXWindow::ImplExecuteAsyncWithoutSolarLock. The stack looks like
this:

ucrtbased.dll!issue_debug_notification(const wchar_t * const message) Line 28
	at minkernel\crts\ucrt\src\appcrt\internal\report_runtime_error.cpp(28)
ucrtbased.dll!__acrt_report_runtime_error(const wchar_t * message) Line 154
	at minkernel\crts\ucrt\src\appcrt\internal\report_runtime_error.cpp(154)
ucrtbased.dll!abort() Line 51
	at minkernel\crts\ucrt\src\appcrt\startup\abort.cpp(51)
comphelper.dll!comphelper::SolarMutex::doRelease(bool bUnlockAll) Line 65
	at c:\lo\src\core2\comphelper\source\misc\solarmutex.cxx(65)
vclplug_winlo.dll!SalYieldMutex::doRelease(bool bUnlockAll) Line 169
	at c:\lo\src\core2\vcl\win\app\salinst.cxx(169)
comphelper.dll!comphelper::SolarMutex::release(bool bUnlockAll) Line 89
	at c:\lo\src\core2\include\comphelper\solarmutex.hxx(89)
vcllo.dll!SalInstance::ReleaseYieldMutexAll() Line 114
	at c:\lo\src\core2\vcl\source\app\salvtables.cxx(114)
vcllo.dll!Application::ReleaseSolarMutex() Line 548
	at c:\lo\src\core2\vcl\source\app\svapp.cxx(548)
fps.dll!SolarMutexReleaser::SolarMutexReleaser() Line 1479
	at c:\lo\src\core2\include\vcl\svapp.hxx(1479)
fps.dll!fpicker::win32::vista::AsyncRequests::~AsyncRequests() Line 84
	at c:\lo\src\core2\fpicker\source\win32\asyncrequests.cxx(84)
fps.dll!fpicker::win32::vista::VistaFilePicker::~VistaFilePicker() Line 72
	at c:\lo\src\core2\fpicker\source\win32\vistafilepicker.cxx(72)
fps.dll!fpicker::win32::vista::VistaFilePicker::`scalar deleting destructor'(unsigned int)
cppuhelper3MSC.dll!cppu::OWeakObject::release() Line 233
	at c:\lo\src\core2\cppuhelper\source\weak.cxx(233)
cppuhelper3MSC.dll!cppu::WeakComponentImplHelperBase::release() Line 84

fpicker/source/win32/asyncrequests.cxx
@@ -80,6 +80,11 @@ AsyncRequests::~AsyncRequests()
     // the already destructed SolarMutex, which would crash LO on exit.
     if (isRunning())
     {
+        // tdf#123502: make sure we actually hold the mutex before releasing it
+        // UNO directly destroys the VistaFilePicker object, so we need GUI protection in there.
+        // But since we redirect GUI stuff to the async thread we also have to release it, so we
+        // can join it, if the thread currently blocks on the SolarMutex.
+        SolarMutexGuard aGuard;
         SolarMutexReleaser aReleaser;
         join();
     }