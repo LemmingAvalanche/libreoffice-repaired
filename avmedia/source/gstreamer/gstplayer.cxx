LemmingAvalanche/libreoffice-repaired

commit 988acfdb7a0ab7ac4effc487f3251ea9e623b842

avmedia: set gstreamer gst_video_overlay size and position.
Have video fit to the media object size on the slide,
allow more complicated interactions.

Change-Id: Ice1fa4b521176ad7ed7f7d1d2b13e617e8282390
Reviewed-on: https://gerrit.libreoffice.org/67978
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

avmedia/source/gstreamer/gstplayer.cxx
@@ -487,7 +487,13 @@ GstBusSyncReply Player::processSyncMessage( GstMessage *message )
             mpXOverlay = GST_VIDEO_OVERLAY( GST_MESSAGE_SRC( message ) );
             g_object_ref( G_OBJECT ( mpXOverlay ) );
             if ( mnWindowID != 0 )
+            {
                 gst_video_overlay_set_window_handle( mpXOverlay, mnWindowID );
+
+                if (maArea.Width > 0 && maArea.Height > 0)
+                    gst_video_overlay_set_render_rectangle(mpXOverlay, maArea.X, maArea.Y, maArea.Width, maArea.Height);
+            }
+
             return GST_BUS_DROP;
         }
     }
@@ -894,7 +900,12 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
     ::osl::MutexGuard aGuard(m_aMutex);
 
     uno::Reference< ::media::XPlayerWindow >    xRet;
-    awt::Size                                   aSize( getPreferredPlayerWindowSize() );
+    awt::Size                                   aSize;
+
+    if (rArguments.getLength() > 1 && (rArguments[1] >>= maArea))
+        aSize = awt::Size(maArea.Width, maArea.Height);
+    else
+        aSize = getPreferredPlayerWindowSize();
 
     if( mbFakeVideo )
         preparePlaybin( maURL, nullptr );
@@ -912,6 +923,14 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
             sal_IntPtr pIntPtr = 0;
             rArguments[ 2 ] >>= pIntPtr;
             SystemChildWindow *pParentWindow = reinterpret_cast< SystemChildWindow* >( pIntPtr );
+
+            if (pParentWindow)
+            {
+                Point aPoint = pParentWindow->GetPosPixel();
+                maArea.X = aPoint.getX();
+                maArea.Y = aPoint.getY();
+            }
+
             const SystemEnvData* pEnvData = pParentWindow ? pParentWindow->GetSystemData() : nullptr;
             OSL_ASSERT(pEnvData);
             if (pEnvData)

avmedia/source/gstreamer/gstplayer.hxx
@@ -101,6 +101,8 @@ private:
     int                     mnWidth;
     int                     mnHeight;
 
+    css::awt::Rectangle     maArea;     // Area of the player window.
+
     guint                   mnWatchID;
     bool                    mbWatchID;
 

commit d6bbe56b7221283cc35743a6a20c10f854f4e3a0

tdf#124027: use ID of the embedded window and fix position of overlay
1) after many trials and errors what LibO apparently expects here is
the ID of QWindow parent of [embedded] system child window

2) the position of the embedded window for video overlay has already
been translated relative to the top-left corner of the slide (see
bugfix of tdf#42873 how) in gen, gtk and kde5 vclplugs. So let's limit
translating it 2nd time only to gtk3 vclplug which for some reason
behaves differently 
(regression from 988acfdb7a0ab7ac4effc487f3251ea9e623b842)

Change-Id: Ie4c6d14a50959c0fdd04e745918d4889c9da45ec
Reviewed-on: https://gerrit.libreoffice.org/72458
Tested-by: Jenkins
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

avmedia/source/gstreamer/gstplayer.cxx
@@ -926,19 +926,27 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
             rArguments[ 2 ] >>= pIntPtr;
             SystemChildWindow *pParentWindow = reinterpret_cast< SystemChildWindow* >( pIntPtr );
 
-            if (pParentWindow)
-            {
-                Point aPoint = pParentWindow->GetPosPixel();
-                maArea.X = aPoint.getX();
-                maArea.Y = aPoint.getY();
-            }
-
             const SystemEnvData* pEnvData = pParentWindow ? pParentWindow->GetSystemData() : nullptr;
             OSL_ASSERT(pEnvData);
             if (pEnvData)
             {
+                OUString aToolkit = OUString::createFromAscii(pEnvData->pToolkit);
+
+                // tdf#124027: the position of embedded window is identical w/ the position
+                // of media object in all other vclplugs (gtk, kde5, gen), in gtk3 w/o gtksink it
+                // needs to be translated
+                if (aToolkit == "gtk3")
+                {
+                    if (pParentWindow)
+                    {
+                        Point aPoint = pParentWindow->GetPosPixel();
+                        maArea.X = aPoint.getX();
+                        maArea.Y = aPoint.getY();
+                    }
+                }
+
 #if defined(ENABLE_GTKSINK)
-                GstElement *pVideosink = g_strcmp0(pEnvData->pToolkit, "gtk3") == 0 ?
+                GstElement *pVideosink = (aToolkit == "gtk3") ?
                                            gst_element_factory_make("gtksink", "gtksink") : nullptr;
                 if (pVideosink)
                 {

vcl/qt5/Qt5Object.cxx
@@ -40,7 +40,7 @@ Qt5Object::Qt5Object(Qt5Frame* pParent, bool bShow)
         m_pQWidget->show();
 
     m_aSystemData.nSize = sizeof(SystemEnvData);
-    m_aSystemData.aWindow = pParent->GetQWidget()->winId();
+    m_aSystemData.aWindow = m_pQWindow->winId(); // ID of the embedded window
     m_aSystemData.aShellWindow = reinterpret_cast<sal_IntPtr>(this);
     //m_aSystemData.pSalFrame = this;
     //m_aSystemData.pWidget = m_pQWidget;