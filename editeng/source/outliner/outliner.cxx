LemmingAvalanche/libreoffice-repaired

commit 36069dee1b64105c69c7d1bb27e7e3715a9124f7

tdf#119650 slow saving spreadsheet with comments, part 1
Reducing the number of times the editeng needs to reformat text takes
saving from from 278s to 151s

Change-Id: I32ba0ab65b4dc0f268cc2cda802570673a316ea0
Reviewed-on: https://gerrit.libreoffice.org/71358
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

editeng/source/outliner/outliner.cxx
@@ -407,11 +407,14 @@ void Outliner::SetText( const OUString& rText, Paragraph* pPara )
 {
     DBG_ASSERT(pPara,"SetText:No Para");
 
+    sal_Int32 nPara = pParaList->GetAbsPos( pPara );
+
+    if (pEditEngine->GetText( nPara ) == rText)
+        return; // do nothing
+
     bool bUpdate = pEditEngine->GetUpdateMode();
     pEditEngine->SetUpdateMode( false );
     ImplBlockInsertionCallbacks( true );
-
-    sal_Int32 nPara = pParaList->GetAbsPos( pPara );
 
     if (rText.isEmpty())
     {

commit f042d8b08a69f8200cf306bb85d0f8cdeb25d0c0

tdf#125297 List Format wrongly applied to text pasted
regression from
    commit 36069dee1b64105c69c7d1bb27e7e3715a9124f7
    Date:   Thu Apr 25 16:35:14 2019 +0200
    tdf#119650 slow saving spreadsheet with comments, part 1

Change-Id: I91b3c009fb8b6739e98537de227ab563828b9c80
Reviewed-on: https://gerrit.libreoffice.org/72842
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

editeng/source/outliner/outliner.cxx
@@ -410,7 +410,11 @@ void Outliner::SetText( const OUString& rText, Paragraph* pPara )
     sal_Int32 nPara = pParaList->GetAbsPos( pPara );
 
     if (pEditEngine->GetText( nPara ) == rText)
-        return; // do nothing
+    {
+        // short-circuit logic to improve performance
+        bFirstParaIsEmpty = false;
+        return;
+    }
 
     bool bUpdate = pEditEngine->GetUpdateMode();
     pEditEngine->SetUpdateMode( false );