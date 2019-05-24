LemmingAvalanche/libreoffice-repaired

commit 19cac8b516f8ddbb5452ff6f3e8df6a633e873e6

Replace list by vector for ConnectedComponentsList
Change-Id: I2e64d4b61605267cca7709c1cfe50d35069f4459
Reviewed-on: https://gerrit.libreoffice.org/71790
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

vcl/source/gdi/print2.cxx
@@ -62,7 +62,7 @@ struct ConnectedComponents
     bool            bIsFullyTransparent;
 };
 
-typedef ::std::list< ConnectedComponents > ConnectedComponentsList;
+typedef ::std::vector< ConnectedComponents > ConnectedComponentsList;
 
 namespace {
 

commit e28839ec5be427ffb21c5d0b35cfcfa945cbc61b

tdf#125472: fix RemoveTransparenciesFromMetaFile(vcl/gdi)
Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=19cac8b516f8ddbb5452ff6f3e8df6a633e873e6

+ some slight simplification

Change-Id: Ifa9d4d7d27e80217f778f3625ef4437828600dd3
Reviewed-on: https://gerrit.libreoffice.org/72922
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

vcl/source/gdi/print2.cxx
@@ -917,8 +917,6 @@ bool OutputDevice::RemoveTransparenciesFromMetaFile( const GDIMetaFile& rInMtf, 
                         bTreatSpecial = true;
                 }
 
-                ConnectedComponentsList::iterator       aCurrCC;
-                const ConnectedComponentsList::iterator aLastCC( aCCList.end() );
                 bool                                    bSomeComponentsChanged;
 
                 // now, this is unfortunate: since changing anyone of
@@ -934,7 +932,7 @@ bool OutputDevice::RemoveTransparenciesFromMetaFile( const GDIMetaFile& rInMtf, 
                     bSomeComponentsChanged = false;
 
                     // iterate over all current members of aCCList
-                    for( aCurrCC=aCCList.begin(); aCurrCC != aLastCC; )
+                    for( auto aCurrCC=aCCList.begin(); aCurrCC != aCCList.end(); )
                     {
                         // first check if current element's bounds are
                         // empty. This ensures that empty actions are not