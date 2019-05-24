LemmingAvalanche/libreoffice-repaired

commit e94565a1ec8d74c47658c540f8e3f0ed61319eb1

Fix documented 'soffice --accept' parameter syntax
The parameter value for '--accept' is no UNO-Url, but uses a
similar syntax. This was wrongly changed in commit 88f19155e336
("tdf#100836 "Starting the LibreOffice Software With Parameters"
help update").

So this changes the parameter value back to {accept-string},
documents its syntax and adds some common examples.

Change-Id: If8159b1d982c54e3dca6d27a1c400d2450ff2d1e
Reviewed-on: https://gerrit.libreoffice.org/72859
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
Tested-by: Jenkins

desktop/source/app/cmdlinehelp.cxx
@@ -96,14 +96,16 @@ namespace desktop
         "   --safe-mode         Starts in a safe mode, i.e. starts temporarily with a   \n"
         "                       fresh user profile and helps to restore a broken        \n"
         "                       configuration.                                          \n"
-        "   --accept={UNO-URL}  Specifies a UNO-URL connect-string to create a UNO      \n"
+        "   --accept={connect-string}  Specifies a UNO connect-string to create a UNO   \n"
         "                       acceptor through which other programs can connect to    \n"
         "                       access the API. Note that API access allows execution   \n"
         "                       of arbitrary commands.                                  \n"
-        "                       The syntax of a UNO-URL connect-string is:              \n"
-        "                   uno:connection-type,params;protocol-name,params;ObjectName  \n"
-        "   --unaccept={UNO-URL} Closes an acceptor that was created with --accept. Use \n"
-        "                       --unaccept=all to close all open acceptors.             \n"
+        "                       The syntax of the {connect-string} is:                  \n"
+        "                         connection-type,params;protocol-name,params           \n"
+        "                       e.g.  pipe,name={some name};urb                         \n"
+        "                         or  socket,host=localhost,port=54321;urb              \n"
+        "   --unaccept={connect-string}  Closes an acceptor that was created with       \n"
+        "                       --accept. Use --unaccept=all to close all acceptors.    \n"
         "   --language={lang}   Uses specified language, if language is not selected    \n"
         "                       yet for UI. The lang is a tag of the language in IETF   \n"
         "                       language tag.                                           \n\n"

commit 936d14cc3ba78402989d9f71e46f90b7ee42e8d7

Fix typo in help: 'urb' => 'urp'
Introduced in commit e94565a1ec8d ("Fix documented 'soffice
--accept' parameter syntax").

Change-Id: Iaa8e821abeb8bdbce36091c4d0264f658c677a76
Reviewed-on: https://gerrit.libreoffice.org/72905
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
Tested-by: Michael Stahl <Michael.Stahl@cib.de>

desktop/source/app/cmdlinehelp.cxx
@@ -102,8 +102,8 @@ namespace desktop
         "                       of arbitrary commands.                                  \n"
         "                       The syntax of the {connect-string} is:                  \n"
         "                         connection-type,params;protocol-name,params           \n"
-        "                       e.g.  pipe,name={some name};urb                         \n"
-        "                         or  socket,host=localhost,port=54321;urb              \n"
+        "                       e.g.  pipe,name={some name};urp                         \n"
+        "                         or  socket,host=localhost,port=54321;urp              \n"
         "   --unaccept={connect-string}  Closes an acceptor that was created with       \n"
         "                       --accept. Use --unaccept=all to close all acceptors.    \n"
         "   --language={lang}   Uses specified language, if language is not selected    \n"