LemmingAvalanche/libreoffice-repaired

commit 482546867933025f99ce17b65168d737b5b9c8e0

tdf#42949 Fix IWYU warnings in i18npool/
Found with bin/find-unneeded-includes
Only removal proposals are dealt with here.

Change-Id: Ic331c845f0a8f06c4a8f8f79b6f87e26ca7c3a7d
Reviewed-on: https://gerrit.libreoffice.org/72972
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

i18npool/IwyuFilter_i18npool.yaml
@@ -1,0 +1,94 @@
+---
+assumeFilename: i18npool/source/localedata/localedata.cxx
+blacklist:
+    i18npool/inc/breakiteratorImpl.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XBreakIterator.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/calendar_gregorian.hxx:
+    # Needed for unique_ptr
+    - unicode/calendar.h
+    i18npool/inc/cclass_unicode.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XCharacterClassification.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/characterclassificationImpl.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XCharacterClassification.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/collatorImpl.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XCollator.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/collator_unicode.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XCollator.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    # Needed for unique_ptr
+    - unicode/tblcoll.h
+    i18npool/inc/defaultnumberingprovider.hxx:
+    # Base class needs complete type
+    - com/sun/star/text/XDefaultNumberingProvider.hpp
+    - com/sun/star/text/XNumberingFormatter.hpp
+    - com/sun/star/text/XNumberingTypeInfo.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/indexentrysupplier.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XExtendedIndexEntrySupplier.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/indexentrysupplier_common.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XExtendedIndexEntrySupplier.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/inputsequencechecker.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XExtendedInputSequenceChecker.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/localedata.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XLocaleData5.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    # Don't propose hxx -> h change in URE libs
+    - osl/module.hxx
+    i18npool/inc/numberformatcode.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XNumberFormatCode.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/ordinalsuffix.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XOrdinalSuffix.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/textconversion.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XExtendedTextConversion.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/textToPronounce_zh.hxx:
+    # Don't propose hxx -> h change in URE libs
+    - osl/module.hxx
+    i18npool/inc/transliterationImpl.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XExtendedTransliteration.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/transliteration_Ignore.hxx:
+    # Needed for variable type
+    - unicode/translit.h
+    i18npool/inc/transliteration_commonclass.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XExtendedTransliteration.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/inc/unoscripttypedetector.hxx:
+    # Base class needs complete type
+    - com/sun/star/i18n/XScriptTypeDetector.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/source/search/textsearch.hxx:
+    # Base class needs complete type
+    - com/sun/star/util/XTextSearch2.hpp
+    - com/sun/star/lang/XServiceInfo.hpp
+    i18npool/source/breakiterator/xdictionary.cxx:
+    # Used after #ifdef DICT_JA_ZH_IN_DATAFILE
+    - config_folders.h
+    - osl/file.h
+    - rtl/bootstrap.hxx
+    i18npool/source/collator/collator_unicode.cxx:
+    # Config options are used in #ifdef
+    - config_locales.h

i18npool/inc/breakiterator_unicode.hxx
@@ -21,9 +21,11 @@
 
 #include "breakiteratorImpl.hxx"
 
-#include <unicode/brkiter.h>
+#include <unicode/utext.h>
 #include <memory>
 #include <unordered_map>
+
+namespace icu_63 { class BreakIterator; }
 
 namespace i18npool {
 

i18npool/source/search/textsearch.hxx
@@ -22,16 +22,21 @@
 
 #include <cppuhelper/implbase.hxx>
 #include <com/sun/star/util/XTextSearch2.hpp>
-#include <com/sun/star/i18n/XBreakIterator.hpp>
-#include <com/sun/star/i18n/XExtendedTransliteration.hpp>
-#include <com/sun/star/i18n/XCharacterClassification.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
-#include <com/sun/star/uno/XComponentContext.hpp>
 
 #include <map>
 #include <memory>
 
-#include <unicode/regex.h>
+#include <unicode/unistr.h>
+#include <unicode/uversion.h>
+
+namespace com::sun::star::i18n { class XBreakIterator; }
+namespace com::sun::star::i18n { class XCharacterClassification; }
+namespace com::sun::star::i18n { class XExtendedTransliteration; }
+namespace com::sun::star::uno { class XComponentContext; }
+namespace icu_63 { class RegexMatcher; }
+
+
 using namespace U_ICU_NAMESPACE;
 typedef U_ICU_NAMESPACE::UnicodeString IcuUniString;
 

commit 7f638ac94b12e94aee6a56868a260999eb65023e

i18npool: remove versioned ICU forward declarations
(regression from 482546867933025f99ce17b65168d737b5b9c8e0)

i18npool/IwyuFilter_i18npool.yaml
@@ -1,6 +1,9 @@
 ---
 assumeFilename: i18npool/source/localedata/localedata.cxx
 blacklist:
+    i18npool/inc/breakiterator_unicode.hxx:
+    # contains versioned namespace so cannot forward declare
+    - unicode/brkiter.h
     i18npool/inc/breakiteratorImpl.hxx:
     # Base class needs complete type
     - com/sun/star/i18n/XBreakIterator.hpp
@@ -84,6 +87,8 @@ blacklist:
     # Base class needs complete type
     - com/sun/star/util/XTextSearch2.hpp
     - com/sun/star/lang/XServiceInfo.hpp
+    # contains versioned namespace so cannot forward declare
+    - unicode/regex.h
     i18npool/source/breakiterator/xdictionary.cxx:
     # Used after #ifdef DICT_JA_ZH_IN_DATAFILE
     - config_folders.h

i18npool/inc/breakiterator_unicode.hxx
@@ -21,11 +21,10 @@
 
 #include "breakiteratorImpl.hxx"
 
+#include <unicode/brkiter.h>
 #include <unicode/utext.h>
 #include <memory>
 #include <unordered_map>
-
-namespace icu_63 { class BreakIterator; }
 
 namespace i18npool {
 

i18npool/source/search/textsearch.hxx
@@ -27,6 +27,7 @@
 #include <map>
 #include <memory>
 
+#include <unicode/regex.h>
 #include <unicode/unistr.h>
 #include <unicode/uversion.h>
 
@@ -34,7 +35,6 @@ namespace com::sun::star::i18n { class XBreakIterator; }
 namespace com::sun::star::i18n { class XCharacterClassification; }
 namespace com::sun::star::i18n { class XExtendedTransliteration; }
 namespace com::sun::star::uno { class XComponentContext; }
-namespace icu_63 { class RegexMatcher; }
 
 
 using namespace U_ICU_NAMESPACE;