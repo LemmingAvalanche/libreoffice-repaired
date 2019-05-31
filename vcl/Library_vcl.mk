LemmingAvalanche/libreoffice-repaired

commit 0d8e9d1ff0ce5c468c0f910d61a9cd5b047ddc74

tdf#113714 vcl menu bar window: avoid flicker
Regression from commit d3ad138e1b058974353892f4c859d5937e462b29 (further
refactor Menu to use RenderContext, 2015-05-15), if we do full paint
instead of incremental paint, then need to ensure that an intermediate
state is not painted.

Paint happens at idle time by default on Windows (OpenGL) and also on
Linux (gtk3), but some other backends like Windows GDI had flicker, this
fixes the problem.

Change-Id: I530166cea93513aec4648dd0a385359f4b998b6f
Reviewed-on: https://gerrit.libreoffice.org/72610
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/window/menubarwindow.cxx
@@ -24,6 +24,7 @@
 #include <vcl/dockingarea.hxx>
 #include <vcl/settings.hxx>
 #include <vcl/taskpanelist.hxx>
+#include <vcl/virdev.hxx>
 #include <sal/log.hxx>
 
 #include <salframe.hxx>
@@ -912,47 +913,54 @@ void MenuBarWindow::Paint(vcl::RenderContext& rRenderContext, const tools::Recta
         return;
     }
 
+    // Make sure that all actual rendering happens in one go to avoid flicker.
+    ScopedVclPtrInstance<VirtualDevice> pBuffer;
+    pBuffer->SetOutputSizePixel(aOutputSize, false);
+
     if (rRenderContext.IsNativeControlSupported(ControlType::Menubar, ControlPart::Entire))
     {
         MenubarValue aMenubarValue;
         aMenubarValue.maTopDockingAreaHeight = ImplGetTopDockingAreaHeight(this);
 
         if (!rStyleSettings.GetPersonaHeader().IsEmpty())
-            Erase(rRenderContext);
+            Erase(*pBuffer);
         else
         {
             tools::Rectangle aCtrlRegion( Point(), aOutputSize );
 
-            rRenderContext.DrawNativeControl(ControlType::Menubar, ControlPart::Entire, aCtrlRegion,
-                                             ControlState::ENABLED, aMenubarValue, OUString());
+            pBuffer->DrawNativeControl(ControlType::Menubar, ControlPart::Entire, aCtrlRegion,
+                                       ControlState::ENABLED, aMenubarValue, OUString());
         }
 
-        ImplAddNWFSeparator(rRenderContext, aOutputSize, aMenubarValue);
+        ImplAddNWFSeparator(*pBuffer, aOutputSize, aMenubarValue);
     }
 
     // shrink the area of the buttons
     aOutputSize.AdjustWidth( -(aCloseBtn->GetSizePixel().Width()) );
 
-    rRenderContext.SetFillColor(rStyleSettings.GetMenuColor());
-    pMenu->ImplPaint(rRenderContext, aOutputSize, 0);
+    pBuffer->SetFillColor(rStyleSettings.GetMenuColor());
+    pMenu->ImplPaint(*pBuffer, aOutputSize, 0);
 
     if (nHighlightedItem != ITEMPOS_INVALID && pMenu && !pMenu->GetItemList()->GetDataFromPos(nHighlightedItem)->bHiddenOnGUI)
-        HighlightItem(rRenderContext, nHighlightedItem);
+        HighlightItem(*pBuffer, nHighlightedItem);
     else if (ImplGetSVData()->maNWFData.mbRolloverMenubar && nRolloveredItem != ITEMPOS_INVALID)
-        HighlightItem(rRenderContext, nRolloveredItem);
+        HighlightItem(*pBuffer, nRolloveredItem);
 
     // in high contrast mode draw a separating line on the lower edge
     if (!rRenderContext.IsNativeControlSupported( ControlType::Menubar, ControlPart::Entire) &&
         rStyleSettings.GetHighContrastMode())
     {
-        rRenderContext.Push(PushFlags::LINECOLOR | PushFlags::MAPMODE);
-        rRenderContext.SetLineColor(COL_WHITE);
-        rRenderContext.SetMapMode(MapMode(MapUnit::MapPixel));
+        pBuffer->Push(PushFlags::LINECOLOR | PushFlags::MAPMODE);
+        pBuffer->SetLineColor(COL_WHITE);
+        pBuffer->SetMapMode(MapMode(MapUnit::MapPixel));
         Size aSize = GetSizePixel();
-        rRenderContext.DrawLine(Point(0, aSize.Height() - 1),
-                                Point(aSize.Width() - 1, aSize.Height() - 1));
-        rRenderContext.Pop();
+        pBuffer->DrawLine(Point(0, aSize.Height() - 1),
+                          Point(aSize.Width() - 1, aSize.Height() - 1));
+        pBuffer->Pop();
     }
+
+    rRenderContext.DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
+                              *pBuffer);
 }
 
 void MenuBarWindow::Resize()

commit a044ce4e11e69b9795ea0b793da1c43b2ea8d9ae

tdf#125550 vcl menu bar / floating window: fix text color
Regression from commit 0d8e9d1ff0ce5c468c0f910d61a9cd5b047ddc74
(tdf#113714 vcl menu bar window: avoid flicker, 2019-05-20), the problem
was that a freshly created VirtualDevice doesn't have the default text
color, so we need to initialize that explicitly based on the render
context text color.

Also introduce a BufferDevice to do this initialization, instead of
fixing this in MenuBarWindow::Paint(), then copy&pasting the fix to
MenuFloatingWindow::Paint().

Change-Id: Ib171cd52e7cabe0bc3c639821f558d8303039fe6
Reviewed-on: https://gerrit.libreoffice.org/73269
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/Library_vcl.mk
@@ -124,6 +124,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/window/accel \
     vcl/source/window/accmgr \
     vcl/source/window/brdwin \
+    vcl/source/window/bufferdevice \
     vcl/source/window/accessibility \
     vcl/source/window/legacyaccessibility \
     vcl/source/window/clipping \

vcl/source/window/bufferdevice.cxx
@@ -1,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "bufferdevice.hxx"
+
+namespace vcl
+{
+BufferDevice::BufferDevice(const VclPtr<vcl::Window>& pWindow, vcl::RenderContext& rRenderContext)
+    : m_pBuffer(VclPtr<VirtualDevice>::Create())
+    , m_pWindow(pWindow)
+    , m_rRenderContext(rRenderContext)
+{
+    m_pBuffer->SetOutputSizePixel(pWindow->GetOutputSizePixel(), false);
+    m_pBuffer->SetTextColor(rRenderContext.GetTextColor());
+    m_pBuffer->DrawOutDev(Point(0, 0), pWindow->GetOutputSizePixel(), Point(0, 0),
+                          pWindow->GetOutputSizePixel(), rRenderContext);
+}
+
+BufferDevice::~BufferDevice()
+{
+    m_rRenderContext.DrawOutDev(Point(0, 0), m_pWindow->GetOutputSizePixel(), Point(0, 0),
+                                m_pWindow->GetOutputSizePixel(), *m_pBuffer);
+}
+
+vcl::RenderContext* BufferDevice::operator->() { return m_pBuffer.get(); }
+
+vcl::RenderContext& BufferDevice::operator*() { return *m_pBuffer; }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/window/bufferdevice.hxx
@@ -1,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_SOURCE_WINDOW_BUFFERDEVICE_HXX
+#define INCLUDED_VCL_SOURCE_WINDOW_BUFFERDEVICE_HXX
+
+#include <vcl/virdev.hxx>
+#include <vcl/window.hxx>
+
+namespace vcl
+{
+/// Buffers drawing on a vcl::RenderContext using a VirtualDevice.
+class BufferDevice
+{
+    ScopedVclPtr<VirtualDevice> m_pBuffer;
+    VclPtr<vcl::Window> m_pWindow;
+    vcl::RenderContext& m_rRenderContext;
+
+public:
+    BufferDevice(const VclPtr<vcl::Window>& pWindow, vcl::RenderContext& rRenderContext);
+    ~BufferDevice();
+
+    vcl::RenderContext* operator->();
+    vcl::RenderContext& operator*();
+};
+}
+
+#endif // INCLUDED_VCL_SOURCE_WINDOW_BUFFERDEVICE_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/window/menubarwindow.cxx
@@ -33,6 +33,7 @@
 #include <strings.hrc>
 #include <bitmaps.hlst>
 #include <window.h>
+#include "bufferdevice.hxx"
 
 // document closing button
 #define IID_DOCUMENTCLOSE 1
@@ -914,11 +915,7 @@ void MenuBarWindow::Paint(vcl::RenderContext& rRenderContext, const tools::Recta
     }
 
     // Make sure that all actual rendering happens in one go to avoid flicker.
-    ScopedVclPtrInstance<VirtualDevice> pBuffer;
-    pBuffer->SetOutputSizePixel(aOutputSize, false);
-    // Copy the current state to the buffer.
-    pBuffer->DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
-                        rRenderContext);
+    vcl::BufferDevice pBuffer(this, rRenderContext);
 
     if (rRenderContext.IsNativeControlSupported(ControlType::Menubar, ControlPart::Entire))
     {
@@ -961,10 +958,6 @@ void MenuBarWindow::Paint(vcl::RenderContext& rRenderContext, const tools::Recta
                           Point(aSize.Width() - 1, aSize.Height() - 1));
         pBuffer->Pop();
     }
-
-    // Copy the current state from the buffer.
-    rRenderContext.DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
-                              *pBuffer);
 }
 
 void MenuBarWindow::Resize()

vcl/source/window/menufloatingwindow.cxx
@@ -20,6 +20,7 @@
 #include "menufloatingwindow.hxx"
 #include "menuitemlist.hxx"
 #include "menubarwindow.hxx"
+#include "bufferdevice.hxx"
 
 #include <sal/log.hxx>
 #include <salmenu.hxx>
@@ -1208,10 +1209,7 @@ void MenuFloatingWindow::Paint(vcl::RenderContext& rRenderContext, const tools::
         return;
 
     // Make sure that all actual rendering happens in one go to avoid flicker.
-    ScopedVclPtrInstance<VirtualDevice> pBuffer;
-    pBuffer->SetOutputSizePixel(GetOutputSizePixel(), false);
-    pBuffer->DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
-                        rRenderContext);
+    vcl::BufferDevice pBuffer(this, rRenderContext);
 
     pBuffer->Push(PushFlags::CLIPREGION);
     pBuffer->SetClipRegion(vcl::Region(rPaintRect));
@@ -1239,9 +1237,6 @@ void MenuFloatingWindow::Paint(vcl::RenderContext& rRenderContext, const tools::
         RenderHighlightItem(*pBuffer, nHighlightedItem);
 
     pBuffer->Pop();
-
-    rRenderContext.DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
-                              *pBuffer);
 }
 
 void MenuFloatingWindow::ImplDrawScroller(vcl::RenderContext& rRenderContext, bool bUp)