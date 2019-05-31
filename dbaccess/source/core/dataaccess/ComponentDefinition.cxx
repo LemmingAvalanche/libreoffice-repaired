LemmingAvalanche/libreoffice-repaired

commit b25f62e910bb5e9ab8d3fc4cbd307eeaea96e7af

new loplugin: refcounting
This was a feature requested by mmeeks, as a result of
tdf#92611.

It validates that things that extend XInterface are not
directly heap/stack-allocated, but have their lifecycle managed
via css::uno::Reference or rtl::Reference.

Change-Id: I28e3b8b236f6a4a56d0a6d6f26ad54e44b36e692
Reviewed-on: https://gerrit.libreoffice.org/16924
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

dbaccess/source/core/dataaccess/ComponentDefinition.cxx
@@ -191,8 +191,9 @@ Reference< XInterface > OComponentDefinition::Create( const Reference< XComponen
 void SAL_CALL OComponentDefinition::disposing()
 {
     OContentHelper::disposing();
-    if ( m_pColumns.get() )
-        m_pColumns->disposing();
+    if ( m_xColumns.is() )
+        m_xColumns->disposing();
+    m_xColumns.clear();
     m_xColumnPropertyListener->clear();
     m_xColumnPropertyListener.clear();
 }
@@ -227,7 +228,7 @@ Reference< XNameAccess> OComponentDefinition::getColumns() throw (RuntimeExcepti
     ::osl::MutexGuard aGuard(m_aMutex);
     ::connectivity::checkDisposed(OContentHelper::rBHelper.bDisposed);
 
-    if ( !m_pColumns.get() )
+    if ( !m_xColumns.is() )
     {
         ::std::vector< OUString> aNames;
 
@@ -239,10 +240,10 @@ Reference< XNameAccess> OComponentDefinition::getColumns() throw (RuntimeExcepti
         for ( ; aIter != aEnd; ++aIter )
             aNames.push_back( aIter->first );
 
-        m_pColumns.reset( new OColumns( *this, m_aMutex, true, aNames, this, NULL, true, false, false ) );
-        m_pColumns->setParent( *this );
+        m_xColumns = new OColumns( *this, m_aMutex, true, aNames, this, NULL, true, false, false );
+        m_xColumns->setParent( *this );
     }
-    return m_pColumns.get();
+    return m_xColumns.get();
 }
 
 OColumn* OComponentDefinition::createColumn(const OUString& _rName) const

dbaccess/source/core/dataaccess/ComponentDefinition.hxx
@@ -88,7 +88,7 @@ class OComponentDefinition  :public OContentHelper
     OComponentDefinition();
 
 protected:
-    ::std::unique_ptr< OColumns >     m_pColumns;
+    css::uno::Reference< OColumns >     m_xColumns;
     rtl::Reference<OColumnPropertyListener> m_xColumnPropertyListener;
     bool                        m_bTable;
 

dbaccess/source/ui/misc/singledoccontroller.cxx
@@ -54,10 +54,10 @@ namespace dbaui
     // OSingleDocumentController_Data
     struct OSingleDocumentController_Data
     {
-        ::boost::scoped_ptr< UndoManager >  m_pUndoManager;
+        Reference< UndoManager >  m_xUndoManager;
 
         OSingleDocumentController_Data( ::cppu::OWeakObject& i_parent, ::osl::Mutex& i_mutex )
-            :m_pUndoManager( new UndoManager( i_parent, i_mutex ) )
+            :m_xUndoManager( new UndoManager( i_parent, i_mutex ) )
         {
         }
     };
@@ -77,7 +77,7 @@ namespace dbaui
     {
         OSingleDocumentController_Base::disposing();
         ClearUndoManager();
-        m_pData->m_pUndoManager->disposing();
+        m_pData->m_xUndoManager->disposing();
     }
 
     void SAL_CALL OSingleDocumentController::disposing( const EventObject& i_event ) throw( RuntimeException, std::exception )
@@ -93,7 +93,7 @@ namespace dbaui
 
     SfxUndoManager& OSingleDocumentController::GetUndoManager() const
     {
-        return m_pData->m_pUndoManager->GetSfxUndoManager();
+        return m_pData->m_xUndoManager->GetSfxUndoManager();
     }
 
     void OSingleDocumentController::addUndoActionAndInvalidate(SfxUndoAction *_pAction)
@@ -111,7 +111,7 @@ namespace dbaui
 
     Reference< XUndoManager > SAL_CALL OSingleDocumentController::getUndoManager(  ) throw (RuntimeException, std::exception)
     {
-        return m_pData->m_pUndoManager.get();
+        return m_pData->m_xUndoManager.get();
     }
 
     FeatureState OSingleDocumentController::GetState(sal_uInt16 _nId) const

commit e2440d8abf5916940d418ffa12e8bab6acbaf555

dbaccess: fix more memory leaks caused by circular Reference
Followup to 6e753c4ca0abe4aa631b547c27fd5378600423bb; more overridden
acquire() creating cycles in dbaccess.

(regression from b25f62e910bb5e9ab8d3fc4cbd307eeaea96e7af)

Change-Id: I134343b3afbcd5ee3f71212ec18e551455eaee5b
Reviewed-on: https://gerrit.libreoffice.org/73158
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
Tested-by: Michael Stahl <Michael.Stahl@cib.de>

dbaccess/source/core/dataaccess/ComponentDefinition.cxx
@@ -157,9 +157,10 @@ Sequence< OUString > SAL_CALL OComponentDefinition::getSupportedServiceNames()
 void SAL_CALL OComponentDefinition::disposing()
 {
     OContentHelper::disposing();
-    if ( m_xColumns.is() )
-        m_xColumns->disposing();
-    m_xColumns.clear();
+    if (m_pColumns)
+    {
+        m_pColumns->disposing();
+    }
     m_xColumnPropertyListener->clear();
     m_xColumnPropertyListener.clear();
 }
@@ -194,7 +195,7 @@ Reference< XNameAccess> OComponentDefinition::getColumns()
     ::osl::MutexGuard aGuard(m_aMutex);
     ::connectivity::checkDisposed(OContentHelper::rBHelper.bDisposed);
 
-    if ( !m_xColumns.is() )
+    if (!m_pColumns)
     {
         std::vector< OUString> aNames;
 
@@ -204,10 +205,11 @@ Reference< XNameAccess> OComponentDefinition::getColumns()
         for (auto const& definition : rDefinition)
             aNames.push_back(definition.first);
 
-        m_xColumns = new OColumns( *this, m_aMutex, true, aNames, this, nullptr, true, false, false );
-        m_xColumns->setParent( *this );
+        m_pColumns.reset(new OColumns(*this, m_aMutex, true, aNames, this, nullptr, true, false, false));
+        m_pColumns->setParent(*this);
     }
-    return m_xColumns.get();
+    // see OCollection::acquire
+    return m_pColumns.get();
 }
 
 OColumn* OComponentDefinition::createColumn(const OUString& _rName) const

dbaccess/source/core/dataaccess/ComponentDefinition.hxx
@@ -84,7 +84,8 @@ class OComponentDefinition  :public OContentHelper
                             ,public OComponentDefinition_BASE
                             ,public ::comphelper::OPropertyArrayUsageHelper< OComponentDefinition >
 {
-    rtl::Reference< OColumns >     m_xColumns;
+    // no Reference! see OCollection::acquire
+    std::unique_ptr<OColumns> m_pColumns;
     rtl::Reference<OColumnPropertyListener> m_xColumnPropertyListener;
     bool                        m_bTable;
 

dbaccess/source/ui/misc/singledoccontroller.cxx
@@ -39,10 +39,11 @@ namespace dbaui
     // OSingleDocumentController_Data
     struct OSingleDocumentController_Data
     {
-        rtl::Reference< UndoManager >  m_xUndoManager;
+        // no Reference! see UndoManager::acquire
+        std::unique_ptr<UndoManager> m_pUndoManager;
 
         OSingleDocumentController_Data( ::cppu::OWeakObject& i_parent, ::osl::Mutex& i_mutex )
-            :m_xUndoManager( new UndoManager( i_parent, i_mutex ) )
+            : m_pUndoManager(new UndoManager(i_parent, i_mutex))
         {
         }
     };
@@ -62,7 +63,7 @@ namespace dbaui
     {
         OSingleDocumentController_Base::disposing();
         ClearUndoManager();
-        m_pData->m_xUndoManager->disposing();
+        m_pData->m_pUndoManager->disposing();
     }
 
     void OSingleDocumentController::ClearUndoManager()
@@ -72,7 +73,7 @@ namespace dbaui
 
     SfxUndoManager& OSingleDocumentController::GetUndoManager() const
     {
-        return m_pData->m_xUndoManager->GetSfxUndoManager();
+        return m_pData->m_pUndoManager->GetSfxUndoManager();
     }
 
     void OSingleDocumentController::addUndoActionAndInvalidate(std::unique_ptr<SfxUndoAction> _pAction)
@@ -90,7 +91,8 @@ namespace dbaui
 
     Reference< XUndoManager > SAL_CALL OSingleDocumentController::getUndoManager(  )
     {
-        return m_pData->m_xUndoManager.get();
+        // see UndoManager::acquire
+        return m_pData->m_pUndoManager.get();
     }
 
     FeatureState OSingleDocumentController::GetState(sal_uInt16 _nId) const