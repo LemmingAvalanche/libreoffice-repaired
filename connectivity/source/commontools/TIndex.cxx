LemmingAvalanche/libreoffice-repaired

commit 9577196e5d1774e333a920cec7c959190db01b08

improve refcounting loplugin
to find ref-counted classes being managed via other smart pointer
classes.
Hopefully prevent needing fixes like
36399a1ef5fb6f1e2541b768b319ad287be59b55
"ChangedUIEventListener is refcounted, mustn't be helt by unique_ptr"

Change-Id: I6b0c5f8f87ce3546a8a1104ce1000470c09459bd
Reviewed-on: https://gerrit.libreoffice.org/39378
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

connectivity/source/commontools/TIndex.cxx
@@ -37,7 +37,7 @@ OIndexHelper::OIndexHelper( OTableHelper* _pTable) : connectivity::sdbcx::OIndex
 {
     construct();
     std::vector< OUString> aVector;
-    m_pColumns.reset( new OIndexColumns(this,m_aMutex,aVector) );
+    m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
 }
 
 OIndexHelper::OIndexHelper( OTableHelper* _pTable,
@@ -93,7 +93,7 @@ void OIndexHelper::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OIndexColumns(this,m_aMutex,aVector) );
+        m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
 }
 
 

connectivity/source/commontools/TKey.cxx
@@ -100,7 +100,7 @@ void OTableKeyHelper::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OKeyColumnsHelper(this,m_aMutex,aVector) );
+        m_pColumns = new OKeyColumnsHelper(this,m_aMutex,aVector);
 }
 
 

connectivity/source/drivers/ado/AGroup.cxx
@@ -79,7 +79,7 @@ void OAdoGroup::refreshUsers()
     if(m_pUsers)
         m_pUsers->reFill(aVector);
     else
-        m_pUsers.reset( new OUsers(m_pCatalog,m_aMutex,aVector,aUsers,isCaseSensitive()) );
+        m_pUsers = new OUsers(m_pCatalog,m_aMutex,aVector,aUsers,isCaseSensitive());
 }
 
 Sequence< sal_Int8 > OAdoGroup::getUnoTunnelImplementationId()

connectivity/source/drivers/ado/AIndex.cxx
@@ -68,7 +68,7 @@ void OAdoIndex::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection) );
+        m_pColumns = new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection);
 }
 
 

connectivity/source/drivers/ado/AKey.cxx
@@ -64,7 +64,7 @@ void OAdoKey::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection) );
+        m_pColumns = new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection);
 }
 
 Sequence< sal_Int8 > OAdoKey::getUnoTunnelImplementationId()

connectivity/source/drivers/ado/AUser.cxx
@@ -63,7 +63,7 @@ void OAdoUser::refreshGroups()
     if(m_pGroups)
         m_pGroups->reFill(aVector);
     else
-        m_pGroups.reset( new OGroups(m_pCatalog,m_aMutex,aVector,aGroups,isCaseSensitive()) );
+        m_pGroups = new OGroups(m_pCatalog,m_aMutex,aVector,aGroups,isCaseSensitive());
 }
 
 Sequence< sal_Int8 > OAdoUser::getUnoTunnelImplementationId()

connectivity/source/drivers/dbase/DIndex.cxx
@@ -103,7 +103,7 @@ void ODbaseIndex::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new ODbaseIndexColumns(this,m_aMutex,aVector) );
+        m_pColumns = new ODbaseIndexColumns(this,m_aMutex,aVector);
 }
 
 Sequence< sal_Int8 > ODbaseIndex::getUnoTunnelImplementationId()

include/connectivity/sdbcx/VGroup.hxx
@@ -56,7 +56,7 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            std::unique_ptr<OUsers>    m_pUsers;
+            rtl::Reference<OUsers>    m_pUsers;
 
             using OGroup_BASE::rBHelper;
 

commit 012235924f9ac14fd4f0ee64850db1b99189142b

connectivity: fix memory leaks caused by OConnection::acquire()
Followup to 093b78f61d7f4639c1ec84d8e769ddc0360eb1be;
OConnection must not be held by rtl::Reference as that creates a cycle.

(regression from 9577196e5d1774e333a920cec7c959190db01b08)

Change-Id: Ibd56d335e3e2631c5a57ea435f1035e89868a5a6
Reviewed-on: https://gerrit.libreoffice.org/73155
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

connectivity/source/commontools/TIndex.cxx
@@ -37,7 +37,7 @@ OIndexHelper::OIndexHelper( OTableHelper* _pTable) : connectivity::sdbcx::OIndex
 {
     construct();
     std::vector< OUString> aVector;
-    m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
+    m_pColumns.reset(new OIndexColumns(this,m_aMutex,aVector));
 }
 
 OIndexHelper::OIndexHelper( OTableHelper* _pTable,
@@ -93,7 +93,7 @@ void OIndexHelper::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
+        m_pColumns.reset(new OIndexColumns(this,m_aMutex,aVector));
 }
 
 

connectivity/source/commontools/TKey.cxx
@@ -100,7 +100,7 @@ void OTableKeyHelper::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new OKeyColumnsHelper(this,m_aMutex,aVector);
+        m_pColumns.reset(new OKeyColumnsHelper(this,m_aMutex,aVector));
 }
 
 

connectivity/source/drivers/ado/AGroup.cxx
@@ -78,7 +78,7 @@ void OAdoGroup::refreshUsers()
     if(m_pUsers)
         m_pUsers->reFill(aVector);
     else
-        m_pUsers = new OUsers(m_pCatalog,m_aMutex,aVector,aUsers,isCaseSensitive());
+        m_pUsers.reset(new OUsers(m_pCatalog, m_aMutex, aVector, aUsers, isCaseSensitive()));
 }
 
 Sequence< sal_Int8 > OAdoGroup::getUnoTunnelImplementationId()

connectivity/source/drivers/ado/AIndex.cxx
@@ -66,7 +66,7 @@ void OAdoIndex::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection);
+        m_pColumns.reset(new OColumns(*this, m_aMutex, aVector, aColumns, isCaseSensitive(), m_pConnection));
 }
 
 

connectivity/source/drivers/ado/AKey.cxx
@@ -63,7 +63,7 @@ void OAdoKey::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection);
+        m_pColumns.reset(new OColumns(*this, m_aMutex, aVector, aColumns, isCaseSensitive(), m_pConnection));
 }
 
 Sequence< sal_Int8 > OAdoKey::getUnoTunnelImplementationId()

connectivity/source/drivers/ado/AUser.cxx
@@ -62,7 +62,7 @@ void OAdoUser::refreshGroups()
     if(m_pGroups)
         m_pGroups->reFill(aVector);
     else
-        m_pGroups = new OGroups(m_pCatalog,m_aMutex,aVector,aGroups,isCaseSensitive());
+        m_pGroups.reset(new OGroups(m_pCatalog, m_aMutex, aVector, aGroups, isCaseSensitive()));
 }
 
 Sequence< sal_Int8 > OAdoUser::getUnoTunnelImplementationId()

connectivity/source/drivers/dbase/DIndex.cxx
@@ -101,7 +101,7 @@ void ODbaseIndex::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new ODbaseIndexColumns(this,m_aMutex,aVector);
+        m_pColumns.reset(new ODbaseIndexColumns(this,m_aMutex,aVector));
 }
 
 Sequence< sal_Int8 > ODbaseIndex::getUnoTunnelImplementationId()

include/connectivity/sdbcx/VGroup.hxx
@@ -51,7 +51,8 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            rtl::Reference<OUsers>    m_pUsers;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OUsers> m_pUsers;
 
             using OGroup_BASE::rBHelper;
 

include/connectivity/sdbcx/VIndex.hxx
@@ -53,7 +53,8 @@ namespace connectivity
             bool            m_IsPrimaryKeyIndex;
             bool            m_IsClustered;
 
-            rtl::Reference<OCollection>    m_pColumns;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OCollection> m_pColumns;
 
             using ODescriptor_BASE::rBHelper;
             virtual void refreshColumns() override;

include/connectivity/sdbcx/VKey.hxx
@@ -68,7 +68,8 @@ namespace connectivity
         {
         protected:
             std::shared_ptr<KeyProperties>   m_aProps;
-            rtl::Reference<OCollection>      m_pColumns;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OCollection> m_pColumns;
 
             using ODescriptor_BASE::rBHelper;
             // OPropertyArrayUsageHelper

include/connectivity/sdbcx/VUser.hxx
@@ -51,7 +51,8 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            rtl::Reference<OGroups>  m_pGroups;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OGroups> m_pGroups;
 
             using OUser_BASE::rBHelper;
 