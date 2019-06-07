LemmingAvalanche/libreoffice-repaired

commit d59f9b494093e4fb53c2902f0dac83a1b76aaf8b

SwViewShell::ImplEndAction: still paint directly when non-double-buffering
It turns out that in this case it hurts performance if we go via
invaliation-then-paint instead of direct painting when double-buffering
is not enabled.

Related commits:

- e1696d14108f26fd4665f1d9020753c244a9fa8c (SwViewShell::ImplEndAction:
  avoid direct paint, 2015-06-29) and
  f48041921da27d01c942dcdbf7bc6c5f7f214b43 (SwViewShell::ImplEndAction:
  avoid direct PaintDesktop(), 2015-07-03) globally avoided direct paints
- d680343d3d2fe4d6fab33e63de16598e802066ac (tdf#93096 sw: fix selection
  with keyboard outside current view, 2015-08-18) restored direct paint
  when non-double-buffering for the second commit, this one does the
  same for the first

Change-Id: Ida0c6917a8cdec74209bd64813c7876d4fa61b8a

sw/source/core/view/viewsh.cxx
@@ -419,7 +419,10 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
 
                             if ( bPaintsFromSystem )
                                 PaintDesktop(*GetOut(), aRect);
-                            pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
+                            if (!isTiledRendering())
+                                pCurrentLayout->Paint( *mpOut, aRect );
+                            else
+                                pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
 
                             // #i75172# end DrawingLayer paint
                             DLPostPaint2(true);

commit f0c3bf35ff0075ec691f227a08d889f30d144cfe

tdf#117347 sw form controls: fix disappearing FixedText when editing TextField
This is nominally a regression from commit
d59f9b494093e4fb53c2902f0dac83a1b76aaf8b (SwViewShell::ImplEndAction:
still paint directly when non-double-buffering, 2015-09-01), but that
just uncovered an already existing problem introduced earlier in commit
fbde824ea6ff02f6e920d80c1ff1e2a4cf2bf0f9 (tdf#92577 sw rendercontext:
paint transparent from controls conditionally, 2015-07-06).

Given that the first commit (as a side-effect) solved the problem the
second commit tried to solve, it's safe to just revert this.

(Sadly we have no way to test incremental paints with a reasonable
amount of effort, so no testcase.)

Change-Id: Ia59efbf45a0cd5af3580914e1dcee74d89068f03
Reviewed-on: https://gerrit.libreoffice.org/73647
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/view/viewsh.cxx
@@ -430,8 +430,8 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
                             DLPostPaint2(true);
                         }
                     }
-                    else
-                        lcl_PaintTransparentFormControls(*this, aRect); // i#107365
+
+                    lcl_PaintTransparentFormControls(*this, aRect); // i#107365
                 }
             }
             if( bShowCursor )