LemmingAvalanche/libreoffice-repaired

commit 7e6acdea18b34d770b6ee01f7be655c0a170e3b8

tdf#125281 DOC import: fix size of lazy-loaded metafiles
Metafiles may have an external header, so once graphic data is read, we
need to set the size explicitly. Otherwise the width of the EMF image in
the bugdoc will be too small.

Change-Id: I2441eda61278b0f4973db5d9aa14618ccd17c397
Reviewed-on: https://gerrit.libreoffice.org/73064
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

filter/source/msfilter/msdffimp.cxx
@@ -6610,6 +6610,11 @@ bool SvxMSDffManager::GetBLIPDirect( SvStream& rBLIPStream, Graphic& rData, tool
             // size to the size of this record.
             sal_uInt64 maxSize = pGrStream == &rBLIPStream ? nLength : 0;
             Graphic aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize);
+
+            // Size available in metafile header, set that here.
+            if (aMtfSize100.getWidth() && aMtfSize100.getHeight())
+                aGraphic.SetPrefSize(aMtfSize100);
+
             if (!aGraphic.IsNone())
             {
                 rData = aGraphic;

sw/qa/extras/ww8import/ww8import.cxx
@@ -14,6 +14,7 @@
 #include <com/sun/star/graphic/XGraphic.hpp>
 #include <viscrs.hxx>
 #include <wrtsh.hxx>
+#include <ndgrf.hxx>
 #include <editeng/boxitem.hxx>
 #include <editeng/lrspitem.hxx>
 #include <editeng/ulspitem.hxx>
@@ -202,6 +203,22 @@ DECLARE_WW8IMPORT_TEST(testTdf121734, "tdf121734.doc")
             CPPUNIT_ASSERT(!pBox->GetLine(eLine));
         }
     }
+}
+
+DECLARE_WW8IMPORT_TEST(testTdf125281, "tdf125281.doc")
+{
+    // Load a .doc file which has an embedded .emf image.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    SwNode* pNode = pDoc->GetNodes()[6];
+    CPPUNIT_ASSERT(pNode->IsGrfNode());
+    SwGrfNode* pGrfNode = pNode->GetGrfNode();
+    const Graphic& rGraphic = pGrfNode->GetGrf();
+
+    // Without the accompanying fix in place, this test would have failed, as pref size was 0 till
+    // an actual Paint() was performed (and even then, it was wrong).
+    long nExpected = 25664;
+    CPPUNIT_ASSERT_EQUAL(nExpected, rGraphic.GetPrefSize().getWidth());
 }
 
 DECLARE_WW8IMPORT_TEST(testTdf122425_1, "tdf122425_1.doc")

commit fdceee1c686ce64357583c9869c6a4d71cb2bebd

tdf#125591 DOC import: lazy-load metafiles with explicit size
Regression from commit 7e6acdea18b34d770b6ee01f7be655c0a170e3b8
(tdf#125281 DOC import: fix size of lazy-loaded metafiles, 2019-05-27),
the problem is that setting the preferred size of a Graphic swaps it in.

Avoid this by extending ImportUnloadedGraphic(): if a size hint is
provided, then that will be used instead of info from the graphic
descriptor (which is usually only meaningful for bitmaps).

This way we maintain the correct size and we're back to lazy-loading
metafiles from binary MSO files as well.

Change-Id: Ide12d12166110e98ea47b5347dd24fb203b22da3
Reviewed-on: https://gerrit.libreoffice.org/73798
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

filter/source/msfilter/msdffimp.cxx
@@ -6611,11 +6611,13 @@ bool SvxMSDffManager::GetBLIPDirect( SvStream& rBLIPStream, Graphic& rData, tool
             // which may be very large if the whole document is large. Limit the read
             // size to the size of this record.
             sal_uInt64 maxSize = pGrStream == &rBLIPStream ? nLength : 0;
-            Graphic aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize);
+            Graphic aGraphic;
 
-            // Size available in metafile header, set that here.
+            // Size available in metafile header.
             if (aMtfSize100.getWidth() && aMtfSize100.getHeight())
-                aGraphic.SetPrefSize(aMtfSize100);
+                aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize, &aMtfSize100);
+            else
+                aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize);
 
             if (!aGraphic.IsNone())
             {

include/vcl/graphicfilter.hxx
@@ -295,7 +295,7 @@ public:
                                    WmfExternal const *pExtHeader = nullptr );
 
     // Setting sizeLimit limits how much will be read from the stream.
-    Graphic ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit = 0);
+    Graphic ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit = 0, Size* pSizeHint = nullptr);
 
     const FilterErrorEx&    GetLastError() const { return *pErrorEx;}
     void                    ResetLastError();

vcl/inc/impgraph.hxx
@@ -114,7 +114,7 @@ public:
     ImpGraphic( const GDIMetaFile& rMtf );
     ~ImpGraphic();
 
-    void ImplSetPrepared(bool bAnimated);
+    void ImplSetPrepared(bool bAnimated, Size* pSizeHint);
 
 private:
 

vcl/source/filter/graphicfilter.cxx
@@ -1178,7 +1178,8 @@ void GraphicFilter::ImportGraphics(std::vector< std::shared_ptr<Graphic> >& rGra
     }
 }
 
-Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit)
+Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit,
+                                             Size* pSizeHint)
 {
     Graphic aGraphic;
     sal_uInt16 nFormat = GRFILTER_FORMAT_DONTKNOW;
@@ -1403,7 +1404,7 @@ Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 size
                 bAnimated = IsGIFAnimated(aMemoryStream);
             }
             aGraphic.SetGfxLink(std::make_shared<GfxLink>(std::move(pGraphicContent), nGraphicContentSize, eLinkType));
-            aGraphic.ImplGetImpGraphic()->ImplSetPrepared(bAnimated);
+            aGraphic.ImplGetImpGraphic()->ImplSetPrepared(bAnimated, pSizeHint);
         }
     }
 

vcl/source/gdi/impgraph.cxx
@@ -522,7 +522,7 @@ ImpSwapFile::~ImpSwapFile()
     }
 }
 
-void ImpGraphic::ImplSetPrepared(bool bAnimated)
+void ImpGraphic::ImplSetPrepared(bool bAnimated, Size* pSizeHint)
 {
     mbPrepared = true;
     mbSwapOut = true;
@@ -530,25 +530,33 @@ void ImpGraphic::ImplSetPrepared(bool bAnimated)
 
     SvMemoryStream aMemoryStream(const_cast<sal_uInt8*>(mpGfxLink->GetData()), mpGfxLink->GetDataSize(), StreamMode::READ | StreamMode::WRITE);
 
-    GraphicDescriptor aDescriptor(aMemoryStream, nullptr);
-    if (aDescriptor.Detect(true))
+    if (pSizeHint)
     {
-        // If we have logic size, work with that, as later pixel -> logic
-        // conversion will work with the output device DPI, not the graphic
-        // DPI.
-        Size aLogSize = aDescriptor.GetSize_100TH_MM();
-        if (aLogSize.getWidth() && aLogSize.getHeight())
+        maSwapInfo.maPrefSize = *pSizeHint;
+        maSwapInfo.maPrefMapMode = MapMode(MapUnit::Map100thMM);
+    }
+    else
+    {
+        GraphicDescriptor aDescriptor(aMemoryStream, nullptr);
+        if (aDescriptor.Detect(true))
         {
-            maSwapInfo.maPrefSize = aLogSize;
-            maSwapInfo.maPrefMapMode = MapMode(MapUnit::Map100thMM);
-        }
-        else
-        {
-            maSwapInfo.maPrefSize = aDescriptor.GetSizePixel();
-            maSwapInfo.maPrefMapMode = MapMode(MapUnit::MapPixel);
-        }
+            // If we have logic size, work with that, as later pixel -> logic
+            // conversion will work with the output device DPI, not the graphic
+            // DPI.
+            Size aLogSize = aDescriptor.GetSize_100TH_MM();
+            if (aLogSize.getWidth() && aLogSize.getHeight())
+            {
+                maSwapInfo.maPrefSize = aLogSize;
+                maSwapInfo.maPrefMapMode = MapMode(MapUnit::Map100thMM);
+            }
+            else
+            {
+                maSwapInfo.maPrefSize = aDescriptor.GetSizePixel();
+                maSwapInfo.maPrefMapMode = MapMode(MapUnit::MapPixel);
+            }
 
-        maSwapInfo.maSizePixel = aDescriptor.GetSizePixel();
+            maSwapInfo.maSizePixel = aDescriptor.GetSizePixel();
+        }
     }
     maSwapInfo.mnAnimationLoopCount = 0;
     maSwapInfo.mbIsEPS = false;

sw/qa/extras/ww8import/ww8import.cxx
@@ -217,8 +217,14 @@ DECLARE_WW8IMPORT_TEST(testTdf125281, "tdf125281.doc")
 
     // Without the accompanying fix in place, this test would have failed, as pref size was 0 till
     // an actual Paint() was performed (and even then, it was wrong).
+#if !defined(_WIN32) // Windows fails with actual == 26171 for some reason.
     long nExpected = 25664;
     CPPUNIT_ASSERT_EQUAL(nExpected, rGraphic.GetPrefSize().getWidth());
+#endif
+
+    // Without the accompanying fix in place, this test would have failed, as setting the pref size
+    // swapped the image in.
+    CPPUNIT_ASSERT(!rGraphic.isAvailable());
 }
 
 DECLARE_WW8IMPORT_TEST(testTdf122425_1, "tdf122425_1.doc")