LemmingAvalanche/libreoffice-repaired

commit b64b38dea713ec576470dfc5cbc9826cf6f6b4b6

tdf#125670 just check gradient clipping on drawing
Otherwise the gradient won't be commited to the Metafile, as
mbOutputClipped will be true, because the output device will
have just one pixel size.

Change-Id: I73084eb715ee6313f6478eded24feb9abfc411f3
Reviewed-on: https://gerrit.libreoffice.org/73423
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/outdev/gradient.cxx
@@ -50,12 +50,6 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
 {
     assert(!is_double_buffered_window());
 
-    if ( mbInitClipRegion )
-        InitClipRegion();
-
-    if ( mbOutputClipped )
-        return;
-
     if ( rPolyPoly.Count() && rPolyPoly[ 0 ].GetSize() )
     {
         if ( mnDrawMode & ( DrawModeFlags::BlackGradient | DrawModeFlags::WhiteGradient | DrawModeFlags::SettingsGradient) )
@@ -106,6 +100,9 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
 
                 if( mbInitClipRegion )
                     InitClipRegion();
+
+                if ( mbOutputClipped )
+                    return;
 
                 // try to draw gradient natively
                 bDrawn = mpGraphics->DrawGradient( aClixPolyPoly, aGradient );

commit ac0ea4cfe68b7ffb0d24391688301f2e3a411b5c

tdf#125777 restore old RenderContext before return
Regression from commit b64b38dea713 ("tdf#125670 just check gradient
clipping on drawing")

Change-Id: I6acf23675414983bd07639703f43aa934c0a48ed
Reviewed-on: https://gerrit.libreoffice.org/74502
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/outdev/gradient.cxx
@@ -101,8 +101,11 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
                 if( mbInitClipRegion )
                     InitClipRegion();
 
-                if ( mbOutputClipped )
+                if (mbOutputClipped)
+                {
+                    Pop();
                     return;
+                }
 
                 // try to draw gradient natively
                 bDrawn = mpGraphics->DrawGradient( aClixPolyPoly, aGradient );