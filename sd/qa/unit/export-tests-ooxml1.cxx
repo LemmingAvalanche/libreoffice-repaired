LemmingAvalanche/libreoffice-repaired

commit d2aad147152971ef84d1cf7bb67f7360a50cb6ae

oox: disable gradient fill grab-bag for PPTX
This was added in commit 7f3762a70b15ea2ecebada02029d994580372f23
(ooxml: preserve gradient shape fill, 2014-01-31), and assumes that the
theme colors can be preserved, as the theme definition is grab-bagged as
well.

But the theme is grab-bagged only for DOCX, not for PPTX, so skip
gradient grab-bag for PPTX, otherwise the gradient would refer to
incorrect colors in the theme.

Change-Id: I98e1c67d4b10e68916f81dd7fc508eb4146d506b
Reviewed-on: https://gerrit.libreoffice.org/67386
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

oox/source/export/drawingml.cxx
@@ -417,7 +417,9 @@ void DrawingML::WriteGradientFill( const Reference< XPropertySet >& rXPropSet )
         }
 
         // check if an ooxml gradient had been imported and if the user has modified it
-        if( EqualGradients( aOriginalGradient, aGradient ) )
+        // Gradient grab-bag depends on theme grab-bag, which is implemented
+        // only for DOCX.
+        if( EqualGradients( aOriginalGradient, aGradient ) && GetDocumentType() == DOCUMENT_DOCX)
         {
             // If we have no gradient stops that means original gradient were defined by a theme.
             if( aGradientStops.hasElements() )

sd/qa/unit/export-tests-ooxml1.cxx
@@ -877,6 +877,12 @@ void SdOOXMLExportTest1::testTdf94238()
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(100), aGradient.YOffset);
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(39), aGradient.Border);
 
+    // Without the accompanying fix in place, this test would have failed with
+    // 'Expected: 0, Actual  : 10592673', i.e. the start color of the gradient
+    // was incorrect.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), aGradient.StartColor);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0x8B8B8B), aGradient.EndColor);
+
     xDocShRef->DoClose();
 }
 

commit 338da09e37220e01715e51ef9b60c12121ead618

tdf#125554 PPTX export: handle gradient transparency for gradient fill
Regression from commit d2aad147152971ef84d1cf7bb67f7360a50cb6ae (oox:
disable gradient fill grab-bag for PPTX, 2019-02-05), the problem was
that in the past grab-bag roundtrip worked (in some cases) for this
shape fill case, but true roundtrip did not.

So when the commit disabled grab-bags (since their color pointers in the
theme don't work in the PPTX case), a previously not implemented feature
now started causing a real problem.

Fix the bug by adding support for transparent linear gradients on the
exports side. This means that in case the import creates both a fill
gradient and a transparency gradient, then now the export creates markup
based on both, not only based on the fill gradient.

Change-Id: I99fa3caba2b2884c2acb7e0704bbeb0b6cffd4a4
Reviewed-on: https://gerrit.libreoffice.org/74968
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

include/oox/export/drawingml.hxx
@@ -183,7 +183,7 @@ public:
     void WriteColor( ::Color nColor, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteColor( const OUString& sColorSchemeName, const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteColorTransformations( const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
-    void WriteGradientStop( sal_uInt16 nStop, ::Color nColor );
+    void WriteGradientStop(sal_uInt16 nStop, ::Color nColor, sal_Int32 nAlpha = MAX_PERCENT);
     void WriteLineArrow( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet, bool bLineStart );
     void WriteConnectorConnections( EscherConnectorListEntry& rConnectorEntry, sal_Int32 nStartID, sal_Int32 nEndID );
 
@@ -191,7 +191,12 @@ public:
     void WriteSolidFill( const OUString& sSchemeName, const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteSolidFill( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet );
     void WriteGradientFill( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet );
-    void WriteGradientFill( css::awt::Gradient rGradient );
+
+    /// In case rXPropSet is set, it may serve as a source of gradient transparency information.
+    void WriteGradientFill(css::awt::Gradient rGradient,
+                           const css::uno::Reference<css::beans::XPropertySet>& rXPropSet
+                           = css::uno::Reference<css::beans::XPropertySet>());
+
     void WriteGrabBagGradientFill( const css::uno::Sequence< css::beans::PropertyValue >& aGradientStops, css::awt::Gradient rGradient);
 
     void WriteBlipOrNormalFill( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet,

oox/source/export/drawingml.cxx
@@ -132,6 +132,17 @@ using ::css::io::XOutputStream;
 using ::sax_fastparser::FSHelperPtr;
 using ::sax_fastparser::FastSerializerHelper;
 
+namespace
+{
+/// Extracts start or end alpha information from a transparency gradient.
+sal_Int32 GetAlphaFromTransparenceGradient(const awt::Gradient& rGradient, bool bStart)
+{
+    // Our alpha is a gray color value.
+    sal_uInt8 nRed = ::Color(bStart ? rGradient.StartColor : rGradient.EndColor).GetRed();
+    // drawingML alpha is a percentage on a 0..100000 scale.
+    return (255 - nRed) * oox::drawingml::MAX_PERCENT / 255;
+}
+}
 
 namespace oox {
 namespace drawingml {
@@ -415,10 +426,10 @@ void DrawingML::WriteSolidFill( const Reference< XPropertySet >& rXPropSet )
     }
 }
 
-void DrawingML::WriteGradientStop( sal_uInt16 nStop, ::Color nColor )
+void DrawingML::WriteGradientStop(sal_uInt16 nStop, ::Color nColor, sal_Int32 nAlpha)
 {
     mpFS->startElementNS(XML_a, XML_gs, XML_pos, OString::number(nStop * 1000));
-    WriteColor( nColor );
+    WriteColor(nColor, nAlpha);
     mpFS->endElementNS( XML_a, XML_gs );
 }
 
@@ -480,7 +491,7 @@ void DrawingML::WriteGradientFill( const Reference< XPropertySet >& rXPropSet )
         else
         {
             mpFS->startElementNS(XML_a, XML_gradFill, XML_rotWithShape, "0");
-            WriteGradientFill(aGradient);
+            WriteGradientFill(aGradient, rXPropSet);
             mpFS->endElementNS( XML_a, XML_gradFill );
         }
     }
@@ -545,20 +556,40 @@ void DrawingML::WriteGrabBagGradientFill( const Sequence< PropertyValue >& aGrad
     }
 }
 
-void DrawingML::WriteGradientFill( awt::Gradient rGradient )
+void DrawingML::WriteGradientFill(awt::Gradient rGradient,
+                                  const uno::Reference<beans::XPropertySet>& rXPropSet)
 {
     switch( rGradient.Style )
     {
         default:
         case awt::GradientStyle_LINEAR:
+        {
+            awt::Gradient aTransparenceGradient;
+            bool bTransparent = false;
+            if (rXPropSet.is() && GetProperty(rXPropSet, "FillTransparenceGradient"))
+            {
+                aTransparenceGradient = *o3tl::doAccess<awt::Gradient>(mAny);
+                bTransparent = true;
+            }
+
             mpFS->startElementNS(XML_a, XML_gsLst);
-            WriteGradientStop( 0, ColorWithIntensity( rGradient.StartColor, rGradient.StartIntensity ) );
-            WriteGradientStop( 100, ColorWithIntensity( rGradient.EndColor, rGradient.EndIntensity ) );
+            sal_Int32 nStartAlpha = MAX_PERCENT;
+            sal_Int32 nEndAlpha = MAX_PERCENT;
+            if (bTransparent)
+            {
+                nStartAlpha = GetAlphaFromTransparenceGradient(aTransparenceGradient, true);
+                nEndAlpha = GetAlphaFromTransparenceGradient(aTransparenceGradient, false);
+            }
+            WriteGradientStop(0, ColorWithIntensity(rGradient.StartColor, rGradient.StartIntensity),
+                              nStartAlpha);
+            WriteGradientStop(100, ColorWithIntensity(rGradient.EndColor, rGradient.EndIntensity),
+                              nEndAlpha);
             mpFS->endElementNS( XML_a, XML_gsLst );
             mpFS->singleElementNS(
                 XML_a, XML_lin, XML_ang,
                 OString::number((((3600 - rGradient.Angle + 900) * 6000) % 21600000)));
             break;
+        }
 
         case awt::GradientStyle_AXIAL:
             mpFS->startElementNS(XML_a, XML_gsLst);

sd/qa/unit/export-tests-ooxml1.cxx
@@ -109,6 +109,7 @@ public:
     void testCustomXml();
     void testTdf94238();
     void testPictureTransparency();
+    void testTdf125554();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest1);
 
@@ -140,6 +141,7 @@ public:
     CPPUNIT_TEST(testTdf112633);
     CPPUNIT_TEST(testCustomXml);
     CPPUNIT_TEST(testTdf94238);
+    CPPUNIT_TEST(testTdf125554);
     CPPUNIT_TEST(testPictureTransparency);
 
     CPPUNIT_TEST_SUITE_END();
@@ -909,6 +911,23 @@ void SdOOXMLExportTest1::testPictureTransparency()
     sal_Int16 nTransparency = 0;
     CPPUNIT_ASSERT(xGraphicShape->getPropertyValue("Transparency") >>= nTransparency);
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(51), nTransparency);
+
+    xDocShRef->DoClose();
+}
+
+void SdOOXMLExportTest1::testTdf125554()
+{
+    ::sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/pptx/tdf125554.pptx"), PPTX);
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX);
+
+    uno::Reference<beans::XPropertySet> xShape = getShapeFromPage(0, 0, xDocShRef);
+    uno::Any aFillTransparenceGradientName
+        = xShape->getPropertyValue("FillTransparenceGradientName");
+    CPPUNIT_ASSERT(aFillTransparenceGradientName.has<OUString>());
+    // Without the accompanying fix in place, this test would have failed, i.e. the transparency of
+    // the shape has no gradient, so it looked like a solid fill instead of a gradient fill.
+    CPPUNIT_ASSERT(!aFillTransparenceGradientName.get<OUString>().isEmpty());
 
     xDocShRef->DoClose();
 }