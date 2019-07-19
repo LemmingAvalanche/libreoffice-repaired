LemmingAvalanche/libreoffice-repaired

commit 1b0fd941cb947d77dbfe9d4ae26f6177b1048ac8

Make font-based unit test depend on instdir fonts
The current dependency is already a hack, because there is no way
I know of to depend on delivered top-level modules like more_fonts.
The original patch parses the gb_Package_MODULE_ooo_fonts list of
registered packages to add them as build dependencies.

But this is not sufficient, as it just adds the dependencies on the
installed / unpacked fonts in the workdir (actually it's just the
installer filelist), where they can't be found by the unit test
running in the instdir environment.

So this converts the depndency into a make error, if either the
filelist is missing or the included font files. But if we are in
a full run and know the more_fonts module, we simply depend on its
delivered files.

This needs some minimal changes to gbuild, as neither the delivered
file list nor the modules class names are yet available. And this
moves the fontconfig handling to extras, where the opensymbol font
is already handled.

Change-Id: I1b70a4c45ff189266ce56c57e534ddc45e7c5c19
Reviewed-on: https://gerrit.libreoffice.org/74624
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

solenv/gbuild/TestHelpers.mk
@@ -1,0 +1,45 @@
+# -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+# This makes sure that more_fonts, opensymbol and fontconfig is installed
+define gb_TestHelpers_use_more_fonts
+
+ifneq (,$$(filter MORE_FONTS,$$(BUILD_TYPE)))
+ifeq (,$$(filter more_fonts,$$(gb_Module_ALLMODULES)))
+
+# the gbuildtojson / gbuild self-test runs tests "out of scope", but itself depends
+# on more fonts, so we must ignore the font tests for these tests.
+ifeq (,$$(gb_IGNORE_MORE_FONTS))
+
+# we are in single module mode -> fail test on missing fonts from filelists!
+# include Module_more_font.mk fails with strange error messages,
+# so we can't check if the more_fonts filelists are up-to-date, so just assume it.
+
+$(1): \
+    $$(foreach font,$$(gb_Package_MODULE_ooo_fonts), \
+        $$(if $$(wildcard $$(call gb_Package_get_target,$$(font))), \
+            $$(foreach file,$$(shell cat $$(call gb_Package_get_target,$$(font))), \
+                $$(if $$(wildcard $$(file)),,$$(error Missing font -> run make more_fonts extras))), \
+            $$(error Missing font filelist -> run make more_fonts extras)))
+endif
+else
+# we're in global run mode (full knowledge) -> depend directly on the delivered fonts!
+
+$(1): \
+    $$(foreach package,$$(subst ExternalPackage_,,$$(filter ExternalPackage_%,$$(call gb_Module_get_classnames,more_fonts))), \
+        $$(call gb_Package_get_files,$$(package))) \
+    $$(call gb_Package_get_files,extras_font) \
+    $$(if $$(filter Package_extras_fontconfig,$$(call gb_Module_get_classnames,extras)), \
+        $$(call gb_Package_get_files,extras_fontconfig))
+endif
+endif
+
+endef
+
+# vim: set noet sw=4:

Repository.mk
@@ -983,7 +983,7 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 $(eval $(call gb_Helper_register_packages_for_install,ooo_fonts,\
 	extras_fonts \
 	$(if $(USING_X11)$(DISABLE_GUI)$(filter ANDROID,$(OS)), \
-		postprocess_fontconfig) \
+		extras_fontconfig) \
 	$(call gb_Helper_optional,MORE_FONTS,\
 		fonts_alef \
 		fonts_amiri \

RepositoryModule_host.mk
@@ -11,6 +11,13 @@
 
 $(eval $(call gb_Module_Module,libreoffice))
 
+# These have to be included before the others for more_fonts dependencies
+$(eval $(call gb_Module_add_moduledirs,libreoffice,\
+	$(call gb_Helper_optional,MORE_FONTS,external/more_fonts) \
+	extras \
+))
+
+
 # This makefile needs to be read first because some variables like T_LIBS
 # for libmerged are cleared there and then later we append stuff to it
 # through e.g. gb_Library_use_external for various libraries in libmerged.
@@ -55,7 +62,6 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	eventattacher \
 	extensions \
 	external \
-	extras \
 	filter \
 	$(call gb_Helper_optional,DBCONNECTIVITY,forms) \
 	formula \

extras/CustomTarget_fontconfig.mk
@@ -7,19 +7,19 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_CustomTarget_CustomTarget,postprocess/fontconfig))
+$(eval $(call gb_CustomTarget_CustomTarget,extras/fontconfig))
 
-$(call gb_CustomTarget_get_workdir,postprocess/fontconfig)/fc_local.conf: \
+$(call gb_CustomTarget_get_workdir,extras/fontconfig)/fc_local.conf: \
     $(SRCDIR)/extras/source/truetype/symbol/fc_local.snippet \
-    $(SRCDIR)/postprocess/CustomTarget_fontconfig.mk \
-    | $(call gb_CustomTarget_get_workdir,postprocess/fontconfig)/.dir
+    $(SRCDIR)/extras/CustomTarget_fontconfig.mk \
+    | $(call gb_CustomTarget_get_workdir,extras/fontconfig)/.dir
 
 ifneq ($(filter MORE_FONTS,$(BUILD_TYPE)),)
-$(call gb_CustomTarget_get_workdir,postprocess/fontconfig)/fc_local.conf: \
+$(call gb_CustomTarget_get_workdir,extras/fontconfig)/fc_local.conf: \
     $(SRCDIR)/external/more_fonts/fc_local.snippet
 endif
 
-$(call gb_CustomTarget_get_workdir,postprocess/fontconfig)/fc_local.conf:
+$(call gb_CustomTarget_get_workdir,extras/fontconfig)/fc_local.conf:
 	printf '<?xml version="1.0"?>\n<!DOCTYPE fontconfig SYSTEM "/etc/fonts/conf.d/fonts.dtd">\n<fontconfig>\n' >$@
 	cat $(SRCDIR)/extras/source/truetype/symbol/fc_local.snippet >>$@
 ifneq ($(filter MORE_FONTS,$(BUILD_TYPE)),)

extras/Module_extras.mk
@@ -80,4 +80,17 @@ $(eval $(call gb_Module_add_targets,extras,\
 	CustomTarget_opensymbol \
 ))
 
+# For configurations that use fontconfig (cf. inclusion of
+# vcl/unx/generic/fontmanager/fontconfig.cxx in Library_vcl), add
+# instdir/share/fonts/truetype/fc_local.conf when it shall
+# contain content from at least one of external/more_fonts/fc_local.snippet
+# (conditional on MORE_FONTS in BUILD_TYPE) and
+# extras/source/truetype/symbol/fc_local.snippet (unconditional):
+ifneq ($(USING_X11)$(DISABLE_GUI)$(filter ANDROID,$(OS)),)
+$(eval $(call gb_Module_add_targets,extras, \
+    CustomTarget_fontconfig \
+    Package_fontconfig \
+))
+endif
+
 # vim: set noet sw=4 ts=4:

extras/Package_fontconfig.mk
@@ -7,12 +7,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,postprocess_fontconfig,$(call gb_CustomTarget_get_workdir,postprocess/fontconfig)))
+$(eval $(call gb_Package_Package,extras_fontconfig,$(call gb_CustomTarget_get_workdir,extras/fontconfig)))
 
-$(eval $(call gb_Package_add_files,postprocess_fontconfig,$(LIBO_SHARE_FOLDER)/fonts/truetype, \
+$(eval $(call gb_Package_add_files,extras_fontconfig,$(LIBO_SHARE_FOLDER)/fonts/truetype, \
     fc_local.conf \
 ))
 
-$(eval $(call gb_Package_use_custom_target,postprocess_fontconfig,postprocess/fontconfig))
+$(eval $(call gb_Package_use_custom_target,extras_fontconfig,extras/fontconfig))
 
 # vim: set noet sw=4 ts=4:

postprocess/Module_postprocess.mk
@@ -28,19 +28,6 @@ $(eval $(call gb_Module_add_targets,postprocess,\
 ))
 endif
 
-# For configurations that use fontconfig (cf. inclusion of
-# vcl/unx/generic/fontmanager/fontconfig.cxx in Library_vcl), add
-# instdir/share/fonts/truetype/fc_local.conf when it shall
-# contain content from at least one of external/more_fonts/fc_local.snippet
-# (conditional on MORE_FONTS in BUILD_TYPE) and
-# extras/source/truetype/symbol/fc_local.snippet (unconditional):
-ifneq ($(USING_X11)$(DISABLE_GUI)$(filter ANDROID,$(OS)),)
-$(eval $(call gb_Module_add_targets,postprocess, \
-    CustomTarget_fontconfig \
-    Package_fontconfig \
-))
-endif
-
 ifeq ($(OS),LINUX)
 ifneq ($(PKGFORMAT),)
 $(eval $(call gb_Module_add_check_targets,postprocess,\

solenv/PythonTest_solenv_python.mk
@@ -15,4 +15,6 @@ $(eval $(call gb_PythonTest_add_modules,solenv_python,$(SRCDIR)/solenv/qa/python
 
 $(call gb_PythonTest_get_target,solenv_python): $(call gb_CustomTarget_get_target,solenv/gbuildtesttools)
 
+$(eval $(call gb_PythonTest_use_more_fonts,solenv_python))
+
 # vim: set noet sw=4 ts=4:

commit 3859bcc2deb172d5a2ce23a1d1ab3b864375cb02

Don't error more_fonts on the clean target
Regression from commit 1b0fd941cb94 ("Make font-based unit test
depend on instdir fonts")

Change-Id: Ie106894803b84430f9b7d9bfd97c420d6eb29506
Reviewed-on: https://gerrit.libreoffice.org/75747
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

solenv/gbuild/TestHelpers.mk
@@ -10,6 +10,7 @@
 # This makes sure that more_fonts, opensymbol and fontconfig is installed
 define gb_TestHelpers_use_more_fonts
 
+ifneq ($(MAKECMDGOALS),clean)
 ifneq (,$$(filter MORE_FONTS,$$(BUILD_TYPE)))
 ifeq (,$$(filter more_fonts,$$(gb_Module_ALLMODULES)))
 
@@ -37,6 +38,7 @@ $(1): \
     $$(call gb_Package_get_files,extras_font) \
     $$(if $$(filter Package_extras_fontconfig,$$(call gb_Module_get_classnames,extras)), \
         $$(call gb_Package_get_files,extras_fontconfig))
+endif
 endif
 endif
 