LemmingAvalanche/libreoffice-repaired

commit 59eef6c1f825770351dcddcb00318dff010ad917

tdf#122590 Import and export xlsx x14:cfRule element for type = cellis.
Change-Id: I3d940269ec25657b64c714400a2828781dd9dd3e
Reviewed-on: https://gerrit.libreoffice.org/69713
Tested-by: Jenkins
Reviewed-by: Andras Timar <andras.timar@collabora.com>

sc/source/core/data/table2.cxx
@@ -640,7 +640,8 @@ void ScTable::CopyConditionalFormat( SCCOL nCol1, SCROW nRow1, SCCOL nCol2, SCRO
             {
                 OUString aStyleName;
                 const ScFormatEntry* pEntry = pNewFormatTmp->GetEntry(i);
-                if(pEntry->GetType() == ScFormatEntry::Type::Condition)
+                if(pEntry->GetType() == ScFormatEntry::Type::Condition ||
+                   pEntry->GetType() == ScFormatEntry::Type::ExtCondition)
                     aStyleName = static_cast<const ScCondFormatEntry*>(pEntry)->GetStyle();
                 else if(pEntry->GetType() == ScFormatEntry::Type::Date)
                     aStyleName = static_cast<const ScCondDateFormatEntry*>(pEntry)->GetStyleName();
@@ -2194,7 +2195,8 @@ void ScTable::FindMaxRotCol( RowInfo* pRowInfo, SCSIZE nArrCount, SCCOL nX1, SCC
                                 for (size_t nEntry=0; nEntry<nEntryCount; nEntry++)
                                 {
                                     const ScFormatEntry* pEntry = pFormat->GetEntry(nEntry);
-                                    if(pEntry->GetType() != ScFormatEntry::Type::Condition)
+                                    if(pEntry->GetType() != ScFormatEntry::Type::Condition ||
+                                       pEntry->GetType() != ScFormatEntry::Type::ExtCondition)
                                         continue;
 
                                     OUString  aStyleName = static_cast<const ScCondFormatEntry*>(pEntry)->GetStyle();

sc/source/ui/unoobj/fmtuno.cxx
@@ -162,7 +162,8 @@ ScTableConditionalFormat::ScTableConditionalFormat(
                 {
                     ScCondFormatEntryItem aItem;
                     const ScFormatEntry* pFrmtEntry = pFormat->GetEntry(i);
-                    if(pFrmtEntry->GetType() != ScFormatEntry::Type::Condition)
+                    if(pFrmtEntry->GetType() != ScFormatEntry::Type::Condition ||
+                       pFrmtEntry->GetType() != ScFormatEntry::Type::ExtCondition)
                         continue;
 
                     const ScCondFormatEntry* pFormatEntry = static_cast<const ScCondFormatEntry*>(pFrmtEntry);

sc/inc/conditio.hxx
@@ -27,6 +27,7 @@
 #include <formula/grammar.hxx>
 #include "scdllapi.h"
 #include "rangelst.hxx"
+#include "tokenarray.hxx"
 
 #include <svl/listener.hxx>
 
@@ -231,6 +232,7 @@ public:
     enum class Type
     {
         Condition,
+        ExtCondition,
         Colorscale,
         Databar,
         Iconset,
@@ -264,6 +266,9 @@ inline std::basic_ostream<charT, traits> & operator <<(std::basic_ostream<charT,
     {
     case ScFormatEntry::Type::Condition:
         stream << "Condition";
+        break;
+    case ScFormatEntry::Type::ExtCondition:
+        stream << "ExtCondition";
         break;
     case ScFormatEntry::Type::Colorscale:
         stream << "Colorscale";
@@ -322,6 +327,7 @@ class SC_DLLPUBLIC ScConditionEntry : public ScFormatEntry
     bool                bRelRef2;
     bool                bFirstRun;
     std::unique_ptr<ScFormulaListener> mpListener;
+    Type eConditionType;                //It can be Condition or ExtCondition
 
     static void SimplifyCompiledFormula( std::unique_ptr<ScTokenArray>& rFormula,
                                      double& rVal,
@@ -346,7 +352,8 @@ public:
                                 ScDocument* pDocument, const ScAddress& rPos,
                                 const OUString& rExprNmsp1, const OUString& rExprNmsp2,
                                 formula::FormulaGrammar::Grammar eGrammar1,
-                                formula::FormulaGrammar::Grammar eGrammar2 );
+                                formula::FormulaGrammar::Grammar eGrammar2,
+                                Type eType = Type::Condition );
             ScConditionEntry( ScConditionMode eOper,
                                 const ScTokenArray* pArr1, const ScTokenArray* pArr2,
                                 ScDocument* pDocument, const ScAddress& rPos );
@@ -391,7 +398,7 @@ public:
 
     bool            MarkUsedExternalReferences() const;
 
-    virtual Type GetType() const override { return Type::Condition; }
+    virtual Type GetType() const override { return eConditionType; }
 
     virtual ScFormatEntry* Clone(ScDocument* pDoc) const override;
 
@@ -442,7 +449,8 @@ private:
 //  single condition entry for conditional formatting
 class SC_DLLPUBLIC ScCondFormatEntry : public ScConditionEntry
 {
-    OUString                  aStyleName;
+    OUString aStyleName;
+    Type eCondFormatType = Type::Condition;
 
 public:
             ScCondFormatEntry( ScConditionMode eOper,
@@ -452,7 +460,8 @@ public:
                                 const OUString& rExprNmsp1 = EMPTY_OUSTRING,
                                 const OUString& rExprNmsp2 = EMPTY_OUSTRING,
                                 formula::FormulaGrammar::Grammar eGrammar1 = formula::FormulaGrammar::GRAM_DEFAULT,
-                                formula::FormulaGrammar::Grammar eGrammar2 = formula::FormulaGrammar::GRAM_DEFAULT );
+                                formula::FormulaGrammar::Grammar eGrammar2 = formula::FormulaGrammar::GRAM_DEFAULT,
+                                Type eType = Type::Condition);
             ScCondFormatEntry( ScConditionMode eOper,
                                 const ScTokenArray* pArr1, const ScTokenArray* pArr2,
                                 ScDocument* pDocument, const ScAddress& rPos,
@@ -466,6 +475,7 @@ public:
     const OUString&   GetStyle() const        { return aStyleName; }
     void            UpdateStyleName(const OUString& rNew)  { aStyleName=rNew; }
     virtual ScFormatEntry* Clone(ScDocument* pDoc) const override;
+    virtual Type GetType() const override { return eCondFormatType; }
 
 protected:
     virtual void    DataChanged() const override;

sc/qa/unit/subsequent_export-test.cxx
@@ -91,6 +91,7 @@ public:
     void testPasswordExportODS();
     void testConditionalFormatExportODS();
     void testConditionalFormatExportXLSX();
+    void testCondFormatExportCellIs();
     void testTdf99856_dataValidationTest();
     void testProtectionKeyODS_UTF16LErtlSHA1();
     void testProtectionKeyODS_UTF8SHA1();
@@ -226,6 +227,7 @@ public:
     CPPUNIT_TEST(testTdf111876);
     CPPUNIT_TEST(testPasswordExportODS);
     CPPUNIT_TEST(testConditionalFormatExportODS);
+    CPPUNIT_TEST(testCondFormatExportCellIs);
     CPPUNIT_TEST(testConditionalFormatExportXLSX);
     CPPUNIT_TEST(testTdf99856_dataValidationTest);
     CPPUNIT_TEST(testProtectionKeyODS_UTF16LErtlSHA1);
@@ -497,6 +499,42 @@ void ScExportTest::testConditionalFormatExportODS()
     OUString aCSVPath;
     createCSVPath( "new_cond_format_test_export.", aCSVPath );
     testCondFile(aCSVPath, &rDoc, 0);
+
+    xDocSh->DoClose();
+}
+
+void ScExportTest::testCondFormatExportCellIs()
+{
+    ScDocShellRef xShell = loadDoc("condFormat_cellis.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xShell.is());
+    ScDocShellRef xDocSh = saveAndReload(&(*xShell), FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    ScDocument& rDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT_EQUAL(size_t(1), rDoc.GetCondFormList(0)->size());
+
+    ScConditionalFormat* pFormat = rDoc.GetCondFormat(0, 0, 0);
+    CPPUNIT_ASSERT(pFormat);
+
+    const ScFormatEntry* pEntry = pFormat->GetEntry(0);
+    CPPUNIT_ASSERT(pEntry);
+    CPPUNIT_ASSERT_EQUAL(ScFormatEntry::Type::ExtCondition, pEntry->GetType());
+
+    const ScCondFormatEntry* pCondition = static_cast<const ScCondFormatEntry*>(pEntry);
+    CPPUNIT_ASSERT_EQUAL( ScConditionMode::Equal,  pCondition->GetOperation());
+
+    OUString aStr = pCondition->GetExpression(ScAddress(0, 0, 0), 0);
+    CPPUNIT_ASSERT_EQUAL( OUString("$Sheet2.$A$1"), aStr );
+
+    pEntry = pFormat->GetEntry(1);
+    CPPUNIT_ASSERT(pEntry);
+    CPPUNIT_ASSERT_EQUAL(ScFormatEntry::Type::ExtCondition, pEntry->GetType());
+
+    pCondition = static_cast<const ScCondFormatEntry*>(pEntry);
+    CPPUNIT_ASSERT_EQUAL( ScConditionMode::Equal,  pCondition->GetOperation());
+
+    aStr = pCondition->GetExpression(ScAddress(0, 0, 0), 0);
+    CPPUNIT_ASSERT_EQUAL( OUString("$Sheet2.$A$2"), aStr );
 
     xDocSh->DoClose();
 }

sc/qa/unit/subsequent_filters-test.cxx
@@ -144,6 +144,7 @@ public:
     void testNewCondFormatODS();
     void testNewCondFormatXLSX();
     void testCondFormatThemeColorXLSX();
+    void testCondFormatImportCellIs();
     void testCondFormatThemeColor2XLSX(); // negative bar color and axis color
     void testCondFormatThemeColor3XLSX(); // theme index 2 and 3 are switched
     void testComplexIconSetsXLSX();
@@ -303,6 +304,7 @@ public:
     CPPUNIT_TEST(testNewCondFormatODS);
     CPPUNIT_TEST(testNewCondFormatXLSX);
     CPPUNIT_TEST(testCondFormatThemeColorXLSX);
+    CPPUNIT_TEST(testCondFormatImportCellIs);
     CPPUNIT_TEST(testCondFormatThemeColor2XLSX);
     CPPUNIT_TEST(testCondFormatThemeColor3XLSX);
     CPPUNIT_TEST(testComplexIconSetsXLSX);
@@ -2256,6 +2258,40 @@ void ScFiltersTest::testNewCondFormatXLSX()
     OUString aCSVPath;
     createCSVPath( "new_cond_format_test.", aCSVPath );
     testCondFile(aCSVPath, &rDoc, 0);
+
+    xDocSh->DoClose();
+}
+
+void ScFiltersTest::testCondFormatImportCellIs()
+{
+    ScDocShellRef xDocSh = ScBootstrapFixture::loadDoc("condFormat_cellis.", FORMAT_XLSX);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load condFormat_cellis.xlsx", xDocSh.is());
+
+    ScDocument& rDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT_EQUAL(size_t(1), rDoc.GetCondFormList(0)->size());
+
+    ScConditionalFormat* pFormat = rDoc.GetCondFormat(0, 0, 0);
+    CPPUNIT_ASSERT(pFormat);
+
+    const ScFormatEntry* pEntry = pFormat->GetEntry(0);
+    CPPUNIT_ASSERT(pEntry);
+    CPPUNIT_ASSERT_EQUAL(ScFormatEntry::Type::ExtCondition, pEntry->GetType());
+
+    const ScCondFormatEntry* pCondition = static_cast<const ScCondFormatEntry*>(pEntry);
+    CPPUNIT_ASSERT_EQUAL( ScConditionMode::Equal,  pCondition->GetOperation());
+
+    OUString aStr = pCondition->GetExpression(ScAddress(0, 0, 0), 0);
+    CPPUNIT_ASSERT_EQUAL( OUString("$Sheet2.$A$1"), aStr );
+
+    pEntry = pFormat->GetEntry(1);
+    CPPUNIT_ASSERT(pEntry);
+    CPPUNIT_ASSERT_EQUAL(ScFormatEntry::Type::ExtCondition, pEntry->GetType());
+
+    pCondition = static_cast<const ScCondFormatEntry*>(pEntry);
+    CPPUNIT_ASSERT_EQUAL( ScConditionMode::Equal,  pCondition->GetOperation());
+
+    aStr = pCondition->GetExpression(ScAddress(0, 0, 0), 0);
+    CPPUNIT_ASSERT_EQUAL( OUString("$Sheet2.$A$2"), aStr );
 
     xDocSh->DoClose();
 }

sc/source/core/data/conditio.cxx
@@ -189,6 +189,7 @@ ScConditionEntry::ScConditionEntry( const ScConditionEntry& r ) :
     bRelRef2(r.bRelRef2),
     bFirstRun(true),
     mpListener(new ScFormulaListener(r.mpDoc)),
+    eConditionType( r.eConditionType ),
     pCondFormat(r.pCondFormat)
 {
     // ScTokenArray copy ctor creates a flat copy
@@ -221,6 +222,7 @@ ScConditionEntry::ScConditionEntry( ScDocument* pDocument, const ScConditionEntr
     bRelRef2(r.bRelRef2),
     bFirstRun(true),
     mpListener(new ScFormulaListener(pDocument)),
+    eConditionType( r.eConditionType),
     pCondFormat(r.pCondFormat)
 {
     // Real copy of the formulas (for Ref Undo)
@@ -236,7 +238,8 @@ ScConditionEntry::ScConditionEntry( ScDocument* pDocument, const ScConditionEntr
 ScConditionEntry::ScConditionEntry( ScConditionMode eOper,
         const OUString& rExpr1, const OUString& rExpr2, ScDocument* pDocument, const ScAddress& rPos,
         const OUString& rExprNmsp1, const OUString& rExprNmsp2,
-        FormulaGrammar::Grammar eGrammar1, FormulaGrammar::Grammar eGrammar2 ) :
+        FormulaGrammar::Grammar eGrammar1, FormulaGrammar::Grammar eGrammar2,
+        Type eType ) :
     ScFormatEntry(pDocument),
     eOp(eOper),
     nOptions(0),
@@ -253,6 +256,7 @@ ScConditionEntry::ScConditionEntry( ScConditionMode eOper,
     bRelRef2(false),
     bFirstRun(true),
     mpListener(new ScFormulaListener(pDocument)),
+    eConditionType(eType),
     pCondFormat(nullptr)
 {
     Compile( rExpr1, rExpr2, rExprNmsp1, rExprNmsp2, eGrammar1, eGrammar2, false );
@@ -1457,9 +1461,11 @@ ScCondFormatEntry::ScCondFormatEntry( ScConditionMode eOper,
                                         const OUString& rStyle,
                                         const OUString& rExprNmsp1, const OUString& rExprNmsp2,
                                         FormulaGrammar::Grammar eGrammar1,
-                                        FormulaGrammar::Grammar eGrammar2 ) :
-    ScConditionEntry( eOper, rExpr1, rExpr2, pDocument, rPos, rExprNmsp1, rExprNmsp2, eGrammar1, eGrammar2 ),
-    aStyleName( rStyle )
+                                        FormulaGrammar::Grammar eGrammar2,
+                                        ScFormatEntry::Type eType ) :
+    ScConditionEntry( eOper, rExpr1, rExpr2, pDocument, rPos, rExprNmsp1, rExprNmsp2, eGrammar1, eGrammar2, eType ),
+    aStyleName( rStyle ),
+    eCondFormatType( eType )
 {
 }
 
@@ -1474,13 +1480,15 @@ ScCondFormatEntry::ScCondFormatEntry( ScConditionMode eOper,
 
 ScCondFormatEntry::ScCondFormatEntry( const ScCondFormatEntry& r ) :
     ScConditionEntry( r ),
-    aStyleName( r.aStyleName )
+    aStyleName( r.aStyleName ),
+    eCondFormatType( r.eCondFormatType)
 {
 }
 
 ScCondFormatEntry::ScCondFormatEntry( ScDocument* pDocument, const ScCondFormatEntry& r ) :
     ScConditionEntry( pDocument, r ),
-    aStyleName( r.aStyleName )
+    aStyleName( r.aStyleName ),
+    eCondFormatType( r.eCondFormatType)
 {
 }
 
@@ -1785,7 +1793,8 @@ const OUString& ScConditionalFormat::GetCellStyle( ScRefCellValue& rCell, const 
 {
     for (const auto& rxEntry : maEntries)
     {
-        if(rxEntry->GetType() == ScFormatEntry::Type::Condition)
+        if(rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+           rxEntry->GetType() == ScFormatEntry::Type::ExtCondition)
         {
             const ScCondFormatEntry& rEntry = static_cast<const ScCondFormatEntry&>(*rxEntry);
             if (rEntry.IsCellValid(rCell, rPos))
@@ -1807,7 +1816,9 @@ ScCondFormatData ScConditionalFormat::GetData( ScRefCellValue& rCell, const ScAd
     ScCondFormatData aData;
     for(const auto& rxEntry : maEntries)
     {
-        if(rxEntry->GetType() == ScFormatEntry::Type::Condition && aData.aStyleName.isEmpty())
+        if( (rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+             rxEntry->GetType() == ScFormatEntry::Type::ExtCondition) &&
+             aData.aStyleName.isEmpty())
         {
             const ScCondFormatEntry& rEntry = static_cast<const ScCondFormatEntry&>(*rxEntry);
             if (rEntry.IsCellValid(rCell, rPos))
@@ -1847,14 +1858,16 @@ void ScConditionalFormat::DoRepaint()
 void ScConditionalFormat::CompileAll()
 {
     for(auto& rxEntry : maEntries)
-        if(rxEntry->GetType() == ScFormatEntry::Type::Condition)
+        if(rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+           rxEntry->GetType() == ScFormatEntry::Type::ExtCondition)
             static_cast<ScCondFormatEntry&>(*rxEntry).CompileAll();
 }
 
 void ScConditionalFormat::CompileXML()
 {
     for(auto& rxEntry : maEntries)
-        if(rxEntry->GetType() == ScFormatEntry::Type::Condition)
+        if(rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+           rxEntry->GetType() == ScFormatEntry::Type::ExtCondition)
             static_cast<ScCondFormatEntry&>(*rxEntry).CompileXML();
 }
 
@@ -1977,7 +1990,8 @@ void ScConditionalFormat::DeleteArea( SCCOL nCol1, SCROW nRow1, SCCOL nCol2, SCR
 void ScConditionalFormat::RenameCellStyle(const OUString& rOld, const OUString& rNew)
 {
     for(const auto& rxEntry : maEntries)
-        if(rxEntry->GetType() == ScFormatEntry::Type::Condition)
+        if(rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+           rxEntry->GetType() == ScFormatEntry::Type::ExtCondition)
         {
             ScCondFormatEntry& rFormat = static_cast<ScCondFormatEntry&>(*rxEntry);
             if(rFormat.GetStyle() == rOld)
@@ -1989,7 +2003,8 @@ bool ScConditionalFormat::MarkUsedExternalReferences() const
 {
     bool bAllMarked = false;
     for(const auto& rxEntry : maEntries)
-        if(rxEntry->GetType() == ScFormatEntry::Type::Condition)
+        if(rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+           rxEntry->GetType() == ScFormatEntry::Type::ExtCondition)
         {
             const ScCondFormatEntry& rFormat = static_cast<const ScCondFormatEntry&>(*rxEntry);
             bAllMarked = rFormat.MarkUsedExternalReferences();
@@ -2020,7 +2035,8 @@ void ScConditionalFormat::CalcAll()
 {
     for(const auto& rxEntry : maEntries)
     {
-        if (rxEntry->GetType() == ScFormatEntry::Type::Condition)
+        if (rxEntry->GetType() == ScFormatEntry::Type::Condition ||
+            rxEntry->GetType() == ScFormatEntry::Type::ExtCondition)
         {
             ScCondFormatEntry& rFormat = static_cast<ScCondFormatEntry&>(*rxEntry);
             rFormat.CalcAll();

sc/source/filter/excel/xecontent.cxx
@@ -1287,6 +1287,15 @@ XclExpCondfmt::XclExpCondfmt( const XclExpRoot& rRoot, const ScConditionalFormat
             {
                 if(pFormatEntry->GetType() == ScFormatEntry::Type::Condition)
                     maCFList.AppendNewRecord( new XclExpCF( GetRoot(), static_cast<const ScCondFormatEntry&>(*pFormatEntry), ++rIndex ) );
+                else if(pFormatEntry->GetType() == ScFormatEntry::Type::ExtCondition)
+                {
+                    const ScCondFormatEntry& rFormat = static_cast<const ScCondFormatEntry&>(*pFormatEntry);
+                    XclExpExtCondFormatData aExtEntry;
+                    aExtEntry.nPriority = ++rIndex;
+                    aExtEntry.aGUID = generateGUIDString();
+                    aExtEntry.pEntry = &rFormat;
+                    aExtEntries.push_back(aExtEntry);
+                }
                 else if(pFormatEntry->GetType() == ScFormatEntry::Type::Colorscale)
                     maCFList.AppendNewRecord( new XclExpColorScale( GetRoot(), static_cast<const ScColorScaleFormat&>(*pFormatEntry), ++rIndex ) );
                 else if(pFormatEntry->GetType() == ScFormatEntry::Type::Databar)

commit dea89bb05199d7a616d5eb77f5ced62b4f2932fc

tdf#125800: accessing cond format props in UNO throws error
These conditions were always evaluated to true: OR should
become AND when negated.

Regression from 59eef6c1f825770351dcddcb00318dff010ad917

Change-Id: I87038319ed27eff2bed2412da49e01f73ec0a79c
Reviewed-on: https://gerrit.libreoffice.org/76493
Reviewed-by: Gülşah Köse <gulsah.kose@collabora.com>
Reviewed-by: Aron Budea <aron.budea@collabora.com>
Tested-by: Aron Budea <aron.budea@collabora.com>

sc/source/core/data/table2.cxx
@@ -2191,7 +2191,7 @@ void ScTable::FindMaxRotCol( RowInfo* pRowInfo, SCSIZE nArrCount, SCCOL nX1, SCC
                                 for (size_t nEntry=0; nEntry<nEntryCount; nEntry++)
                                 {
                                     const ScFormatEntry* pEntry = pFormat->GetEntry(nEntry);
-                                    if(pEntry->GetType() != ScFormatEntry::Type::Condition ||
+                                    if(pEntry->GetType() != ScFormatEntry::Type::Condition &&
                                        pEntry->GetType() != ScFormatEntry::Type::ExtCondition)
                                         continue;
 

sc/source/ui/unoobj/fmtuno.cxx
@@ -161,7 +161,7 @@ ScTableConditionalFormat::ScTableConditionalFormat(
                 {
                     ScCondFormatEntryItem aItem;
                     const ScFormatEntry* pFrmtEntry = pFormat->GetEntry(i);
-                    if(pFrmtEntry->GetType() != ScFormatEntry::Type::Condition ||
+                    if(pFrmtEntry->GetType() != ScFormatEntry::Type::Condition &&
                        pFrmtEntry->GetType() != ScFormatEntry::Type::ExtCondition)
                         continue;
 