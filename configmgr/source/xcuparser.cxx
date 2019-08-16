LemmingAvalanche/libreoffice-repaired

commit 223646bafa109169ce0481731c216afe60c45c84

jl153: #i110720# ignore spurious <prop> elements in .xcu set nodes

configmgr/source/xcuparser.cxx
@@ -196,6 +196,14 @@ bool XcuParser::startElement(
             {
                 handleSetNode(
                     reader, dynamic_cast< SetNode * >(state_.top().node.get()));
+            } else if (ns == XmlReader::NAMESPACE_NONE &&
+                       name.equals(RTL_CONSTASCII_STRINGPARAM("prop")))
+            {
+                OSL_TRACE(
+                    "configmgr bad set node <prop> member in %s",
+                    rtl::OUStringToOString(
+                        reader.getUrl(), RTL_TEXTENCODING_UTF8).getStr());
+                state_.push(State(true)); // ignored
             } else {
                 throw css::uno::RuntimeException(
                     (rtl::OUString(

commit 7723ad0e0bff23f8bf379941fff61ad35ec1718d

Related tdf#90429: Don't erroneously pop unrelated path segments
...when coming across a bad set node <prop> member.  The ooo2gd_3.0.0.oxt (see
referenced bug for links) Addons.xcu contains two such bogus props

> <node oor:name="AddonUI">
>   <node oor:name="OfficeMenuBarMerging">
>     <node oor:name="org.openoffice.gdocs.gdocs" oor:op="replace">
>       <prop oor:name="ImageIdentifier" oor:type="xs:string">
>         <value/>
>       </prop>
>     </node>
>   </node>
>   <node oor:name="OfficeToolBar">
>     <node oor:name="org.openoffice.gdocs.gdocs" oor:op="replace">
>       <prop oor:name="UIName" oor:type="xs:string">
>         <value>Google Docs</value>
>       </prop>
>     </node>
>   </node>
> </node>

so that a later

> <node oor:name="AddonUI">
>   <node oor:name="Images">

was processed with path_ erroneously stripped down to the root path.

It appears that this has been broken ever since
223646bafa109169ce0481731c216afe60c45c84 "jl153: #i110720# ignore spurious
<prop> elements in .xcu set nodes".

Change-Id: I1d069b7226c8202b6eb93f59d294ce7f25681f80
Reviewed-on: https://gerrit.libreoffice.org/77537
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

configmgr/source/xcuparser.cxx
@@ -157,7 +157,7 @@ bool XcuParser::startElement(
                     "configmgr",
                     "bad set node <prop> member in \"" << reader.getUrl()
                         << '"');
-                state_.push(State::Ignore(true));
+                state_.push(State::Ignore(false));
             } else {
                 throw css::uno::RuntimeException(
                     "bad set node member <" + name.convertFromUtf8() +