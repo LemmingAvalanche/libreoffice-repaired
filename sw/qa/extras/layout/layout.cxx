LemmingAvalanche/libreoffice-repaired

commit 1cbeeb4cf26ddfb4db6e7585ae249a84749a8519

tdf#127304 Chart: fix rotation of multilevel axis labels
If the first level of horizontal category axis labels
is rotated vertically, then the other levels should not be
rotated.

Regression from commit: 37e4502c99f74978183ba46a80a64190f4dffb04
(tdf#107074 uncommenting createLabels for the axis fixes the issue)

Change-Id: Ia1978fdc345fa154cff2ea03d2a94475f73398a6
Reviewed-on: https://gerrit.libreoffice.org/78557
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

chart2/source/view/axes/VCartesianAxis.cxx
@@ -757,8 +757,8 @@ bool VCartesianAxis::createTextShapes(
         }
     }
 
-     // Stores an array of text label strings in case of a normal
-     // (non-complex) category axis.
+    // Stores an array of text label strings in case of a normal
+    // (non-complex) category axis.
     const uno::Sequence<OUString>* pCategories = nullptr;
     if( m_bUseTextLabels && !m_aAxisProperties.m_bComplexCategories )
         pCategories = &m_aTextLabels;
@@ -1680,10 +1680,12 @@ void VCartesianAxis::createLabels()
             {
                 aComplexProps.bLineBreakAllowed = true;
                 aComplexProps.bOverlapAllowed = aComplexProps.fRotationAngleDegree != 0.0;
-                //Only the first level of complex vertical category axis labels orientation should be horizontal
-                if( nTextLevel > 0 && m_aAxisProperties.m_bSwapXAndY )
+                if( nTextLevel > 0 )
                 {
-                    aComplexProps.fRotationAngleDegree = 90.0;
+                    if( m_aAxisProperties.m_bSwapXAndY )
+                        aComplexProps.fRotationAngleDegree = 90.0;
+                    else
+                        aComplexProps.fRotationAngleDegree = 0.0;
                 }
             }
             AxisLabelProperties& rAxisLabelProperties =  m_aAxisProperties.m_bComplexCategories ? aComplexProps : m_aAxisLabelProperties;

sw/qa/extras/layout/layout.cxx
@@ -2320,6 +2320,27 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf126244)
                 "900");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf127304)
+{
+    SwDoc* pDoc = createDoc("tdf127304.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Test the first level of horizontal category axis labels orientation. The first level orientation should be vertical.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/font[1]", "orientation",
+                "900");
+    // Test the second level of horizontal category axis labels orientation. The second level orientation should be horizontal.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/font[5]", "orientation",
+                "0");
+    // Test the third level of horizontal category axis labels orientation. The third level orientation should be horizontal.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/font[7]", "orientation",
+                "0");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf124796)
 {
     SwDoc* pDoc = createDoc("tdf124796.odt");

commit 244333577efcdc6cd4df4e919312ab222ee06111

tdf127448 Chart: Avoid distortion of charts with multilevel axis labels
and too long tickmark lines.

Note: larger chart distortion is a regression
from commit: 1cbeeb4cf26ddfb4db6e7585ae249a84749a8519
(tdf#127304 Chart: fix rotation of multilevel axis labels)

Change-Id: Idbcd40d93c21a685e9ad1e49eb8c088a20ecae15
Reviewed-on: https://gerrit.libreoffice.org/78662
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

chart2/source/view/axes/VCartesianAxis.cxx
@@ -1619,7 +1619,11 @@ void VCartesianAxis::doStaggeringOfLabels( const AxisLabelProperties& rAxisLabel
                 if( nTextLevel>0 )
                 {
                     lcl_shiftLabels(*apTickIter, aCummulatedLabelsDistance);
-                    fRotationAngleDegree = 0.0;
+                    //multilevel labels: 0 or 90 by default
+                    if( m_aAxisProperties.m_bSwapXAndY )
+                        fRotationAngleDegree = 90.0;
+                    else
+                        fRotationAngleDegree = 0.0;
                 }
                 aCummulatedLabelsDistance += lcl_getLabelsDistance(
                     *apTickIter, pTickFactory2D->getDistanceAxisTickToText(m_aAxisProperties),
@@ -1682,6 +1686,7 @@ void VCartesianAxis::createLabels()
                 aComplexProps.bOverlapAllowed = aComplexProps.fRotationAngleDegree != 0.0;
                 if( nTextLevel > 0 )
                 {
+                    //multilevel labels: 0 or 90 by default
                     if( m_aAxisProperties.m_bSwapXAndY )
                         aComplexProps.fRotationAngleDegree = 90.0;
                     else
@@ -1769,7 +1774,7 @@ void VCartesianAxis::updatePositions()
                 double fRotationAngleDegree = m_aAxisLabelProperties.fRotationAngleDegree;
                 if( nDepth > 0 )
                 {
-                    /* Multi-level Labels: default to 0 or 90 */
+                    //multilevel labels: 0 or 90 by default
                     if( pTickFactory2D->isHorizontalAxis() )
                         fRotationAngleDegree = 0.0;
                     else
@@ -1863,6 +1868,14 @@ void VCartesianAxis::createShapes()
                 if( apTickIter )
                 {
                     double fRotationAngleDegree = m_aAxisLabelProperties.fRotationAngleDegree;
+                    if( nTextLevel > 0 )
+                    {
+                        //Multi-level Labels: default to 0 or 90
+                        if( m_aAxisProperties.m_bSwapXAndY )
+                            fRotationAngleDegree = 90.0;
+                        else
+                            fRotationAngleDegree = 0.0;
+                    }
                     B2DVector aLabelsDistance(lcl_getLabelsDistance(
                         *apTickIter, pTickFactory2D->getDistanceAxisTickToText(m_aAxisProperties),
                         fRotationAngleDegree));

sw/qa/extras/layout/layout.cxx
@@ -2341,6 +2341,21 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf127304)
                 "0");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testHorizontal_multilevel)
+{
+    SwDoc* pDoc = createDoc("horizontal_multilevel.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Test the Y position of horizontal category axis label.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/textarray[7]", "y",
+                "7945");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf124796)
 {
     SwDoc* pDoc = createDoc("tdf124796.odt");