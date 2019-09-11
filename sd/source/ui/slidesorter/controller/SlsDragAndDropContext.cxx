LemmingAvalanche/libreoffice-repaired

commit 2cd68e690bfbf6ab6e324ed5d9f9b3078b245743

sd: replace boost::bind with C++11 lambdas and for loops
Change-Id: I4de92df1848a1c00e71d4cdb5638b73c3b76f282

sd/source/ui/slidesorter/controller/SlsDragAndDropContext.cxx
@@ -36,7 +36,6 @@
 #include "app.hrc"
 #include "sdtreelb.hxx"
 #include <sfx2/bindings.hxx>
-#include <boost/bind.hpp>
 
 namespace sd { namespace slidesorter { namespace controller {
 
@@ -95,8 +94,9 @@ void DragAndDropContext::UpdatePosition (
     bool bDoAutoScroll = bAllowAutoScroll
             && mpTargetSlideSorter->GetController().GetScrollBarManager().AutoScroll(
                 rMousePosition,
-                ::boost::bind(
-                    &DragAndDropContext::UpdatePosition, this, rMousePosition, eMode, false));
+                [this, eMode, &rMousePosition] () {
+                    return this->UpdatePosition(rMousePosition, eMode, false);
+                });
 
     if (!bDoAutoScroll)
     {

sd/source/ui/slidesorter/controller/SlsAnimator.cxx
@@ -20,7 +20,6 @@
 #include "controller/SlsAnimator.hxx"
 #include "view/SlideSorterView.hxx"
 #include "View.hxx"
-#include <boost/bind.hpp>
 
 namespace sd { namespace slidesorter { namespace controller {
 
@@ -132,10 +131,8 @@ void Animator::RemoveAnimation (const Animator::AnimationId nId)
     const AnimationList::iterator iAnimation (::std::find_if(
         maAnimations.begin(),
         maAnimations.end(),
-        ::boost::bind(
-            ::std::equal_to<Animator::AnimationId>(),
-            nId,
-            ::boost::bind(&Animation::mnAnimationId, _1))));
+        [nId] (std::shared_ptr<Animation> const& pAnim)
+            { return nId == pAnim->mnAnimationId; }));
     if (iAnimation != maAnimations.end())
     {
         OSL_ASSERT((*iAnimation)->mnAnimationId == nId);
@@ -156,12 +153,10 @@ void Animator::RemoveAnimation (const Animator::AnimationId nId)
 
 void Animator::RemoveAllAnimations()
 {
-    ::std::for_each(
-        maAnimations.begin(),
-        maAnimations.end(),
-        ::boost::bind(
-            &Animation::Expire,
-            _1));
+    for (auto const& it : maAnimations)
+    {
+        it->Expire();
+    }
     maAnimations.clear();
     mnNextAnimationId = 0;
 

sd/source/ui/slidesorter/controller/SlsSelectionFunction.cxx
@@ -62,7 +62,6 @@
 #include <svx/svdpagv.hxx>
 #include <vcl/msgbox.hxx>
 #include <svx/svxids.hrc>
-#include <boost/bind.hpp>
 #include <boost/optional.hpp>
 
 namespace {
@@ -1386,11 +1385,7 @@ void MultiSelectionModeHandler::UpdatePosition (
 
     bool bDoAutoScroll = bAllowAutoScroll && mrSlideSorter.GetController().GetScrollBarManager().AutoScroll(
         rMousePosition,
-        ::boost::bind(
-            &MultiSelectionModeHandler::UpdatePosition,
-            this,
-            rMousePosition,
-            false));
+        [this, &rMousePosition] () { return this->UpdatePosition(rMousePosition, false); });
 
     if (!bDoAutoScroll)
         UpdateModelPosition(aMouseModelPosition);

sd/source/ui/slidesorter/controller/SlsSlotManager.cxx
@@ -87,7 +87,6 @@
 #include <com/sun/star/drawing/XDrawPages.hpp>
 #include <vcl/svapp.hxx>
 
-#include <boost/bind.hpp>
 #include <memory>
 
 using namespace ::com::sun::star;
@@ -1137,13 +1136,10 @@ void SlotManager::DuplicateSelectedSlides (SfxRequest& rRequest)
     // Set the selection to the pages in aPagesToSelect.
     PageSelector& rSelector (mrSlideSorter.GetController().GetPageSelector());
     rSelector.DeselectAllPages();
-    ::std::for_each (
-        aPagesToSelect.begin(),
-        aPagesToSelect.end(),
-        ::boost::bind(
-            static_cast<void (PageSelector::*)(const SdPage*)>(&PageSelector::SelectPage),
-            ::boost::ref(rSelector),
-            _1));
+    for (auto const& it: aPagesToSelect)
+    {
+        rSelector.SelectPage(it);
+    }
 }
 
 void SlotManager::ChangeSlideExclusionState (

commit ab07423eb13a3dc841e0d32d153976f311c55904

Fix stack-use-after-return when moving a slide in the list
...very far, so that the list needs to scroll:

> ==1676==ERROR: AddressSanitizer: stack-use-after-return on address 0x7f039a1e6420 at pc 0x7f03c34e0658 bp 0x7ffede26b140 sp 0x7ffede26b138
> READ of size 8 at 0x7f039a1e6420 thread T0
>  #0 in Point::X() const at include/tools/gen.hxx:78:44 (instdir/program/libvcllo.so +0x5cf6657)
>  #1 in OutputDevice::PixelToLogic(Point const&) const at vcl/source/outdev/map.cxx:1191:47
>  #2 in sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool) at sd/source/ui/slidesorter/controller/SlsDragAndDropContext.cxx:84:47
>  #3 in sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0::operator()() const at sd/source/ui/slidesorter/controller/SlsDragAndDropContext.cxx:92:34
>  #4 in void std::__invoke_impl<void, sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0&>(std::__invoke_other, sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0&) at gcc/x86_64-pc-linux-gnu/10.0.0/../../../../include/c++/10.0.0/bits/invoke.h:60:14
>  #5 in std::enable_if<is_invocable_r_v<void, sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0&>, void>::type std::__invoke_r<void, sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0&>(sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0&) at gcc/x86_64-pc-linux-gnu/10.0.0/../../../../include/c++/10.0.0/bits/invoke.h:110:2
>  #6 in std::_Function_handler<void (), sd::slidesorter::controller::DragAndDropContext::UpdatePosition(Point const&, sd::slidesorter::controller::InsertionIndicatorHandler::Mode, bool)::$_0>::_M_invoke(std::_Any_data const&) at gcc/x86_64-pc-linux-gnu/10.0.0/../../../../include/c++/10.0.0/bits/std_function.h:291:9
>  #7 in std::function<void ()>::operator()() const at gcc/x86_64-pc-linux-gnu/10.0.0/../../../../include/c++/10.0.0/bits/std_function.h:622:14
>  #8 in sd::slidesorter::controller::ScrollBarManager::RepeatAutoScroll() at sd/source/ui/slidesorter/controller/SlsScrollBarManager.cxx:518:17
>  #9 in sd::slidesorter::controller::ScrollBarManager::AutoScrollTimeoutHandler(Timer*) at sd/source/ui/slidesorter/controller/SlsScrollBarManager.cxx:539:5
>  #10 in sd::slidesorter::controller::ScrollBarManager::LinkStubAutoScrollTimeoutHandler(void*, Timer*) at sd/source/ui/slidesorter/controller/SlsScrollBarManager.cxx:537:1
>  #11 in Link<Timer*, void>::Call(Timer*) const at include/tools/link.hxx:112:45
>  #12 in Timer::Invoke() at vcl/source/app/timer.cxx:75:21
>  #13 in Scheduler::ProcessTaskScheduling() at vcl/source/app/scheduler.cxx:479:20
>  #14 in Scheduler::CallbackTaskScheduling() at vcl/source/app/scheduler.cxx:287:5
>  #15 in SalTimer::CallCallback() at vcl/inc/saltimer.hxx:54:13
>  #16 in sal_gtk_timeout_dispatch(_GSource*, int (*)(void*), void*) at vcl/unx/gtk3/gtk3gtkdata.cxx:761:45
>  #17 in g_main_dispatch at ../glib/gmain.c:3189:28
> [...]
> Address 0x7f039a1e6420 is located in stack of thread T0 at offset 32 in frame
>  #0 in sd::slidesorter::controller::SelectionFunction::MouseDragged(AcceptDropEvent const&, signed char) at sd/source/ui/slidesorter/controller/SlsSelectionFunction.cxx:630 (instdir/program/../program/libsdlo.so +0x5c22e1f)
>   This frame has 1 object(s):
>     [32, 104) 'aEventDescriptor' (line 631) <== Memory access at offset 32 is inside this variable
> HINT: this may be a false positive if your program uses some custom stack unwind mechanism, swapcontext or vfork
>       (longjmp and C++ exceptions *are* supported)

Presumably a regression introduced with 2cd68e690bfbf6ab6e324ed5d9f9b3078b245743

sd/source/ui/slidesorter/controller/SlsDragAndDropContext.cxx
@@ -88,7 +88,7 @@ void DragAndDropContext::UpdatePosition (
     bool bDoAutoScroll = bAllowAutoScroll
             && mpTargetSlideSorter->GetController().GetScrollBarManager().AutoScroll(
                 rMousePosition,
-                [this, eMode, &rMousePosition] () {
+                [this, eMode, rMousePosition] () {
                     return this->UpdatePosition(rMousePosition, eMode, false);
                 });
 