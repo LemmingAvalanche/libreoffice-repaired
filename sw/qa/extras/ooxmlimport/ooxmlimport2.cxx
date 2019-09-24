LemmingAvalanche/libreoffice-repaired

commit ba9237c3b87b5d5e503a6c3938c5515152801939

writerfilter: reset properties of reused styles
When we have a style to import and the name is already taken, we reuse
the style (we can't use replaceByName(), it only works with user-defined
styles). If that happens, reset non-default properties of it, so the
result will be what was in the file, not a merge of Writer defaults and
what was in the file.

Change-Id: Ifb1098f78254a061b11fd6c6d2a2fa8d85a00d60

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -41,6 +41,7 @@
 #include <stdio.h>
 #include <rtl/ustrbuf.hxx>
 #include <comphelper/string.hxx>
+#include <comphelper/sequenceasvector.hxx>
 
 #include <dmapperLoggers.hxx>
 
@@ -938,6 +939,33 @@ void StyleSheetTable::ApplyStyleSheets( FontTablePtr rFontTable )
                             continue;
                         }
                         xStyles->getByName( sConvertedStyleName ) >>= xStyle;
+
+                        // See if the existing style has any non-default properties. If so, reset them back to default.
+                        uno::Reference<beans::XPropertySet> xPropertySet(xStyle, uno::UNO_QUERY);
+                        uno::Reference<beans::XPropertySetInfo> xPropertySetInfo = xPropertySet->getPropertySetInfo();
+                        uno::Sequence<beans::Property> aProperties = xPropertySetInfo->getProperties();
+                        comphelper::SequenceAsVector<OUString> aPropertyNames;
+                        for (sal_Int32 i = 0; i < aProperties.getLength(); ++i)
+                        {
+                            aPropertyNames.push_back(aProperties[i].Name);
+                        }
+
+                        uno::Reference<beans::XPropertyState> xPropertyState(xStyle, uno::UNO_QUERY);
+                        uno::Sequence<beans::PropertyState> aStates = xPropertyState->getPropertyStates(aPropertyNames.getAsConstList());
+                        for (sal_Int32 i = 0; i < aStates.getLength(); ++i)
+                        {
+                            if (aStates[i] == beans::PropertyState_DIRECT_VALUE)
+                            {
+                                try
+                                {
+                                    xPropertyState->setPropertyToDefault(aPropertyNames[i]);
+                                }
+                                catch(const uno::Exception& rException)
+                                {
+                                    SAL_INFO("writerfilter", "setPropertyToDefault(" << aPropertyNames[i] << ") failed: " << rException.Message);
+                                }
+                            }
+                        }
                     }
                     else
                     {

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2164,6 +2164,13 @@ DECLARE_OOXMLIMPORT_TEST(testBnc875718, "bnc875718.docx")
     CPPUNIT_ASSERT_EQUAL( OUString( "Text\n" ), text->getString());
 }
 
+DECLARE_OOXMLIMPORT_TEST(testCaption, "caption.docx")
+{
+    uno::Reference<beans::XPropertySet> xStyle(getStyles("ParagraphStyles")->getByName("Caption"), uno::UNO_QUERY);
+    // This was awt::FontSlant_ITALIC: Writer default was used instead of what is in the document.
+    CPPUNIT_ASSERT_EQUAL(awt::FontSlant_NONE, getProperty<awt::FontSlant>(xStyle, "CharPosture"));
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 96b614834354ca9ec7f7464edac336dfb4f37a9f

tdf#104167 DOCX import: don't map Heading1 to Heading 1 para style
This is nominally a regression from commit
ba9237c3b87b5d5e503a6c3938c5515152801939 (writerfilter: reset properties
of reused styles, 2014-06-04), which assumed that style names are
unique.

They are not unique for the bugdoc, as it has paragraph styles named
'heading 1' and 'Heading1', both mapped to 'Heading 1'.

The mapping for the style name without a space was added in commit
1d2af74e226bf44e1a0f1e1981ca965cffeaa0b2 (INTEGRATION: CWS xmlfilter02
(1.27.12); FILE MERGED, 2008-01-10), in the "2007/11/22 14:53:43 os
1.27.12.9: Tab stops almost perfect now" part, sadly without any
indication on why that would be a good idea.

The binary DOC import never mapped Heading1 to Heading 1, so it doesn't
have this problem.

Fix the problem by removing this mapping, it leads to formatting loss
when a document has both HeadingN and Heading N styles.

Change-Id: I26dd999f5a536e079ac351ba5d40d20c83b99310
Reviewed-on: https://gerrit.libreoffice.org/79422
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -1278,15 +1278,6 @@ static const sal_Char* const aStyleNamePairs[] =
     "heading 7",                  "Heading 7",
     "heading 8",                  "Heading 8",
     "heading 9",                  "Heading 9",
-    "Heading1",                   "Heading 1",
-    "Heading2",                   "Heading 2",
-    "Heading3",                   "Heading 3",
-    "Heading4",                   "Heading 4",
-    "Heading5",                   "Heading 5",
-    "Heading6",                   "Heading 6",
-    "Heading7",                   "Heading 7",
-    "Heading8",                   "Heading 8",
-    "Heading9",                   "Heading 9",
     "Heading 1",                  "Heading 1",
     "Heading 2",                  "Heading 2",
     "Heading 3",                  "Heading 3",

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -19,6 +19,7 @@
 #include <com/sun/star/document/XEmbeddedObjectSupplier2.hpp>
 #include <com/sun/star/embed/Aspects.hpp>
 #include <com/sun/star/text/WritingMode2.hpp>
+#include <com/sun/star/style/BreakType.hpp>
 #include <xmloff/odffields.hxx>
 #include <IDocumentMarkAccess.hxx>
 #include <IMark.hxx>
@@ -307,6 +308,18 @@ DECLARE_OOXMLIMPORT_TEST(testTdf124398, "tdf124398.docx")
 
     uno::Reference<drawing::XShapeDescriptor> xShape(xGroup->getByIndex(1), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.OLE2Shape"), xShape->getShapeType());
+}
+
+DECLARE_OOXMLIMPORT_TEST(testTdf104167, "tdf104167.docx")
+{
+    // Make sure that heading 1 paragraphs start on a new page.
+    uno::Any xStyle = getStyles("ParagraphStyles")->getByName("Heading 1");
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 4
+    // - Actual  : 0
+    // i.e. the <w:pageBreakBefore/> was lost on import.
+    CPPUNIT_ASSERT_EQUAL(style::BreakType_PAGE_BEFORE,
+                         getProperty<style::BreakType>(xStyle, "BreakType"));
 }
 
 DECLARE_OOXMLIMPORT_TEST(testTdf113946, "tdf113946.docx")