LemmingAvalanche/libreoffice-repaired

commit 7e9842d66b537de03690d9e2efa6c7d5d886d1ae

tdf#126819 Can't retrieve the mouse position written in the statusbar
regression from
    commit ce82768dfa6eebe13af51559840a8341a6bbf7f9
    Date:   Mon Jul 8 11:22:12 2019 +0200
    tdf#121793 speedup VCLXAccessibleStatusBarItem::GetItemText

Make the custom StatusBarControls set better text on their parent
StatusBar. Also remove some unnecessary work being done in the Paint
methods, to reduce confusion.

Change-Id: I3b0a0af3b01d3b01f85ab335a3eb5993c07a50f4
Reviewed-on: https://gerrit.libreoffice.org/77586
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/stbctrls/zoomctrl.cxx
@@ -111,6 +111,7 @@ SvxZoomStatusBarControl::SvxZoomStatusBarControl( sal_uInt16 _nSlotId,
     nValueSet( SvxZoomEnableFlags::ALL )
 {
     GetStatusBar().SetQuickHelpText(GetId(), SvxResId(RID_SVXSTR_ZOOMTOOL_HINT));
+    ImplUpdateItemText();
 }
 
 void SvxZoomStatusBarControl::StateChanged( sal_uInt16, SfxItemState eState,
@@ -124,9 +125,7 @@ void SvxZoomStatusBarControl::StateChanged( sal_uInt16, SfxItemState eState,
     else if ( auto pItem = dynamic_cast< const SfxUInt16Item* >(pState) )
     {
         nZoom = pItem->GetValue();
-
-        OUString aStr(unicode::formatPercent(nZoom, Application::GetSettings().GetUILanguageTag()));
-        GetStatusBar().SetItemText( GetId(), aStr );
+        ImplUpdateItemText();
 
         if ( auto pZoomItem = dynamic_cast<const SvxZoomItem*>(pState) )
         {
@@ -140,10 +139,14 @@ void SvxZoomStatusBarControl::StateChanged( sal_uInt16, SfxItemState eState,
     }
 }
 
-void SvxZoomStatusBarControl::Paint( const UserDrawEvent& )
+void SvxZoomStatusBarControl::ImplUpdateItemText()
 {
     OUString aStr(unicode::formatPercent(nZoom, Application::GetSettings().GetUILanguageTag()));
     GetStatusBar().SetItemText( GetId(), aStr );
+}
+
+void SvxZoomStatusBarControl::Paint( const UserDrawEvent& )
+{
 }
 
 void SvxZoomStatusBarControl::Command( const CommandEvent& rCEvt )
@@ -156,6 +159,7 @@ void SvxZoomStatusBarControl::Command( const CommandEvent& rCEvt )
         if (aPop.Execute(&rStatusbar, rCEvt.GetMousePosPixel()) && (nZoom != aPop.GetZoom() || !nZoom))
         {
             nZoom = aPop.GetZoom();
+            ImplUpdateItemText();
             SvxZoomItem aZoom(SvxZoomType::PERCENT, nZoom, GetId());
 
             OString sIdent = aPop.GetCurItemIdent();

sw/source/uibase/utlui/zoomctrl.cxx
@@ -56,12 +56,8 @@ void SwZoomControl::StateChanged( sal_uInt16 nSID, SfxItemState eState,
     }
 }
 
-void SwZoomControl::Paint( const UserDrawEvent& rUsrEvt )
+void SwZoomControl::Paint( const UserDrawEvent& )
 {
-    if (sPreviewZoom.isEmpty())
-        SvxZoomStatusBarControl::Paint(rUsrEvt);
-    else
-        GetStatusBar().SetItemText(GetId(), sPreviewZoom);
 }
 
 void SwZoomControl::Command( const CommandEvent& rCEvt )

include/svx/pszctrl.hxx
@@ -39,6 +39,8 @@ public:
     virtual void    StateChanged( sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState ) override;
     virtual void    Paint( const UserDrawEvent& rEvt ) override;
     virtual void    Command( const CommandEvent& rCEvt ) override;
+private:
+    void ImplUpdateItemText();
 };
 
 #endif

include/svx/zoomctrl.hxx
@@ -40,6 +40,8 @@ public:
 
     SvxZoomStatusBarControl( sal_uInt16 nSlotId, sal_uInt16 nId, StatusBar& rStb );
 
+private:
+    void ImplUpdateItemText();
 };
 
 class SVX_DLLPUBLIC SvxZoomPageStatusBarControl : public SfxStatusBarControl

sd/source/ui/app/tmplctrl.cxx
@@ -89,7 +89,6 @@ void SdTemplateControl::StateChanged(
 
 void SdTemplateControl::Paint( const UserDrawEvent&  )
 {
-    GetStatusBar().SetItemText( GetId(), msTemplate );
 }
 
 void SdTemplateControl::Command( const CommandEvent& rCEvt )

svx/source/stbctrls/insctrl.cxx
@@ -65,7 +65,6 @@ void SvxInsertStatusBarControl::StateChanged( sal_uInt16 , SfxItemState eState,
 
 void SvxInsertStatusBarControl::Paint( const UserDrawEvent& )
 {
-    DrawItemText_Impl();
 }
 
 void SvxInsertStatusBarControl::DrawItemText_Impl()

svx/source/stbctrls/pszctrl.cxx
@@ -231,6 +231,7 @@ SvxPosSizeStatusBarControl::SvxPosSizeStatusBarControl( sal_uInt16 _nSlotId,
     addStatusListener( STR_POSITION);         // SID_ATTR_POSITION
     addStatusListener( STR_TABLECELL);   // SID_TABLE_CELL
     addStatusListener( STR_FUNC);    // SID_PSZ_FUNCTION
+    ImplUpdateItemText();
 }
 
 /*  [Description]
@@ -334,12 +335,7 @@ void SvxPosSizeStatusBarControl::StateChanged( sal_uInt16 nSID, SfxItemState eSt
 
     GetStatusBar().SetItemData( GetId(), nullptr );
 
-    //  set only strings as text at the statusBar, so that the Help-Tips
-    //  can work with the text, when it is too long for the statusBar
-    OUString aText;
-    if ( pImpl->bTable )
-        aText = pImpl->aStr;
-    GetStatusBar().SetItemText( GetId(), aText );
+    ImplUpdateItemText();
 }
 
 
@@ -461,4 +457,27 @@ void SvxPosSizeStatusBarControl::Paint( const UserDrawEvent& rUsrEvt )
     pDev->SetFillColor( aOldFillColor );
 }
 
+void SvxPosSizeStatusBarControl::ImplUpdateItemText()
+{
+    //  set only strings as text at the statusBar, so that the Help-Tips
+    //  can work with the text, when it is too long for the statusBar
+    OUString aText;
+    if ( pImpl->bPos || pImpl->bSize )
+    {
+        aText = GetMetricStr_Impl( pImpl->aPos.X());
+        aText += " / ";
+        aText += GetMetricStr_Impl( pImpl->aPos.Y());
+        if ( pImpl->bSize )
+        {
+            aText += " ";
+            aText += GetMetricStr_Impl( pImpl->aSize.Width() );
+            aText += " x ";
+            aText += GetMetricStr_Impl( pImpl->aSize.Height() );
+        }
+    }
+    else if ( pImpl->bTable )
+       aText = pImpl->aStr;
+
+    GetStatusBar().SetItemText( GetId(), aText );
+}
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/uibase/utlui/bookctrl.cxx
@@ -96,7 +96,6 @@ void SwBookmarkControl::StateChanged(
 
 void SwBookmarkControl::Paint( const UserDrawEvent&  )
 {
-    GetStatusBar().SetItemText( GetId(), sPageNumber );
 }
 
 void SwBookmarkControl::Command( const CommandEvent& rCEvt )

commit a4c4eed5bb4257536434c127fa8aa89da32f092d

tdf#127701 Zoom factor percentage calculating fault
regression from
    commit 7e9842d66b537de03690d9e2efa6c7d5d886d1ae
tdf#126819 Can't retrieve the mouse position written in the
statusbar

note that this is a workaround - we fire an sfx2 event when we choose
one of the "smart" zoom options, and normally we then receive another
sfx2 event to notify us of the real zoom value.
But when we choose the same smart option twice, we don't receive the
second sfx2 event.

Change-Id: I31c2e34ff31a4f4e03a447ffbd783589982edc57
Reviewed-on: https://gerrit.libreoffice.org/79516
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/stbctrls/zoomctrl.cxx
@@ -141,8 +141,12 @@ void SvxZoomStatusBarControl::StateChanged( sal_uInt16, SfxItemState eState,
 
 void SvxZoomStatusBarControl::ImplUpdateItemText()
 {
-    OUString aStr(unicode::formatPercent(nZoom, Application::GetSettings().GetUILanguageTag()));
-    GetStatusBar().SetItemText( GetId(), aStr );
+    // workaround - don't bother updating when we don't have a real zoom value
+    if (nZoom)
+    {
+        OUString aStr(unicode::formatPercent(nZoom, Application::GetSettings().GetUILanguageTag()));
+        GetStatusBar().SetItemText( GetId(), aStr );
+    }
 }
 
 void SvxZoomStatusBarControl::Paint( const UserDrawEvent& )