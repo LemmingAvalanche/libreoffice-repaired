LemmingAvalanche/libreoffice-repaired

commit 2af93f8690f47af41901e9543a4d3aa115bb201a

tdf#126424 Don't dispose decks before saving their state
Only dispose them in SidebarController::disposing after the
state has been saved.

Change-Id: I9e42383e20e2bc0100557b8cb8fbd26b3505f236
Reviewed-on: https://gerrit.libreoffice.org/75719
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

sfx2/source/sidebar/SidebarController.cxx
@@ -192,7 +192,6 @@ void SidebarController::registerSidebarForFrame(SidebarController* pController, 
 
 void SidebarController::unregisterSidebarForFrame(SidebarController* pController, const css::uno::Reference<css::frame::XController>& xController)
 {
-    pController->disposeDecks();
     css::uno::Reference<css::ui::XContextChangeEventMultiplexer> xMultiplexer (
         css::ui::ContextChangeEventMultiplexer::get(
             ::comphelper::getProcessComponentContext()));
@@ -228,22 +227,7 @@ void SAL_CALL SidebarController::disposing()
     }
 
     // clear decks
-    ResourceManager::DeckContextDescriptorContainer aDecks;
-
-    mpResourceManager->GetMatchingDecks (
-            aDecks,
-            GetCurrentContext(),
-            IsDocumentReadOnly(),
-            mxFrame->getController());
-
-    for (const auto& rDeck : aDecks)
-    {
-        std::shared_ptr<DeckDescriptor> deckDesc = mpResourceManager->GetDeckDescriptor(rDeck.msId);
-
-        VclPtr<Deck> aDeck = deckDesc->mpDeck;
-        if (aDeck)
-            aDeck.disposeAndClear();
-    }
+    disposeDecks();
 
     uno::Reference<css::frame::XController> xController = mxFrame->getController();
     if (!xController.is())
@@ -698,9 +682,6 @@ void SidebarController::SwitchToDeck (
     const DeckDescriptor& rDeckDescriptor,
     const Context& rContext)
 {
-
-    maFocusManager.Clear();
-
     const bool bForceNewDeck ((mnRequestedForceFlags&SwitchFlag_ForceNewDeck)!=0);
     const bool bForceNewPanels ((mnRequestedForceFlags&SwitchFlag_ForceNewPanels)!=0);
     mnRequestedForceFlags = SwitchFlag_NoForce;

commit 89c35013d06a1eeeb367b3a985cdde8c7bf70a55

tdf#127306 Fix crash when closing LO after deleting a chart
This crash was introduced with 2af93f8690f47af41901e9543a4d3aa115bb201a

This patch basically reverts that patch and implements a different fix.

Change-Id: I394a2f6490d3ee0769c0f78bb8a3980167719893
Reviewed-on: https://gerrit.libreoffice.org/79865
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

include/sfx2/sidebar/SidebarController.hxx
@@ -167,6 +167,8 @@ public:
     sal_Int32 getMaximumWidth() const { return mnMaximumSidebarWidth; }
     void setMaximumWidth(sal_Int32 nMaximumWidth) { mnMaximumSidebarWidth = nMaximumWidth; }
 
+    void saveDeckState();
+
 private:
     SidebarController(SidebarDockingWindow* pParentWindow, const SfxViewFrame* pViewFrame);
 

sfx2/source/sidebar/SidebarController.cxx
@@ -221,6 +221,9 @@ void SidebarController::registerSidebarForFrame(SidebarController* pController, 
 
 void SidebarController::unregisterSidebarForFrame(SidebarController* pController, const css::uno::Reference<css::frame::XController>& xController)
 {
+    pController->saveDeckState();
+    pController->disposeDecks();
+
     css::uno::Reference<css::ui::XContextChangeEventMultiplexer> xMultiplexer (
         css::ui::ContextChangeEventMultiplexer::get(
             ::comphelper::getProcessComponentContext()));
@@ -258,19 +261,25 @@ void SAL_CALL SidebarController::disposing()
     maFocusManager.Clear();
     mpTabBar.disposeAndClear();
 
-    // save decks settings
-    // Impress shutdown : context (frame) is disposed before sidebar disposing
-    // calc writer : context (frame) is disposed after sidebar disposing
-    // so need to test if GetCurrentContext is still valid regarding msApplication
-
-    if (GetCurrentContext().msApplication != "none")
-    {
-        mpResourceManager->SaveDecksSettings(GetCurrentContext());
-        mpResourceManager->SaveLastActiveDeck(GetCurrentContext(), msCurrentDeckId);
-    }
+    saveDeckState();
 
     // clear decks
-    disposeDecks();
+    ResourceManager::DeckContextDescriptorContainer aDecks;
+
+    mpResourceManager->GetMatchingDecks (
+            aDecks,
+            GetCurrentContext(),
+            IsDocumentReadOnly(),
+            mxFrame->getController());
+
+    for (const auto& rDeck : aDecks)
+    {
+        std::shared_ptr<DeckDescriptor> deckDesc = mpResourceManager->GetDeckDescriptor(rDeck.msId);
+
+        VclPtr<Deck> aDeck = deckDesc->mpDeck;
+        if (aDeck)
+            aDeck.disposeAndClear();
+    }
 
     uno::Reference<css::frame::XController> xController = mxFrame->getController();
     if (!xController.is())
@@ -1560,6 +1569,18 @@ void SidebarController::frameAction(const css::frame::FrameActionEvent& rEvent)
             unregisterSidebarForFrame(this, mxFrame->getController());
         else if (rEvent.Action == css::frame::FrameAction_COMPONENT_REATTACHED)
             registerSidebarForFrame(this, mxFrame->getController());
+    }
+}
+
+void SidebarController::saveDeckState()
+{
+    // Impress shutdown : context (frame) is disposed before sidebar disposing
+    // calc writer : context (frame) is disposed after sidebar disposing
+    // so need to test if GetCurrentContext is still valid regarding msApplication
+    if (GetCurrentContext().msApplication != "none")
+    {
+        mpResourceManager->SaveDecksSettings(GetCurrentContext());
+        mpResourceManager->SaveLastActiveDeck(GetCurrentContext(), msCurrentDeckId);
     }
 }
 