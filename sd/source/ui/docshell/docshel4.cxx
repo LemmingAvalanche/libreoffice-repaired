LemmingAvalanche/libreoffice-repaired

commit d082a48926e1446390648fb1ea1933fd788a6ce9

tdf#118696 impress navigator: zoom slide/slide object on select
Change-Id: I5656c99f72fb94c791282bbf2fcf5044bcdd8a7a
Reviewed-on: https://gerrit.libreoffice.org/57379
Tested-by: Jenkins
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

sd/source/ui/docshell/docshel4.cxx
@@ -87,6 +87,8 @@
 
 #include <SdUnoDrawView.hxx>
 
+#include <sfx2/zoomitem.hxx>
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using ::sd::framework::FrameworkHelper;
@@ -858,11 +860,14 @@ void DrawDocShell::GotoBookmark(const OUString& rBookmark)
                     pDrawViewShell->SwitchPage(nSdPgNum);
                 }
 
+                // show page
+                SvxZoomItem aZoom;
+                aZoom.SetType( SvxZoomType::WHOLEPAGE );
+                pDrawViewShell->GetDispatcher()->ExecuteList(SID_ATTR_ZOOM, SfxCallMode::ASYNCHRON, { &aZoom });
+
                 if (pObj != nullptr)
                 {
-                    // show and select object
-                    pDrawViewShell->MakeVisible(pObj->GetLogicRect(),
-                        *pDrawViewShell->GetActiveWindow());
+                    // select object
                     pDrawViewShell->GetView()->UnmarkAll();
                     pDrawViewShell->GetView()->MarkObj(
                         pObj,

commit 67f4e956a39b532c3b39999f77145548314c6d4f

tdf#128013: fix Crash when linking an odp file
See bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=154847

Regression introduced by:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=d082a48926e1446390648fb1ea1933fd788a6ce9

Change-Id: Id9355958b0c4a56215ff98f0e5be13a3074ce45f
Reviewed-on: https://gerrit.libreoffice.org/80500
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sd/source/ui/docshell/docshel4.cxx
@@ -819,10 +819,13 @@ void DrawDocShell::GotoBookmark(const OUString& rBookmark)
                 pDrawViewShell->SwitchPage(nSdPgNum);
             }
 
-            // show page
-            SvxZoomItem aZoom;
-            aZoom.SetType( SvxZoomType::WHOLEPAGE );
-            pDrawViewShell->GetDispatcher()->ExecuteList(SID_ATTR_ZOOM, SfxCallMode::ASYNCHRON, { &aZoom });
+            if (pDrawViewShell->GetDispatcher())
+            {
+                // show page
+                SvxZoomItem aZoom;
+                aZoom.SetType( SvxZoomType::WHOLEPAGE );
+                pDrawViewShell->GetDispatcher()->ExecuteList(SID_ATTR_ZOOM, SfxCallMode::ASYNCHRON, { &aZoom });
+            }
 
             if (pObj != nullptr)
             {