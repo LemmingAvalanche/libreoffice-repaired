LemmingAvalanche/libreoffice-repaired

commit ccadb3eb11d2ba380e47dcad6705de3ccd423a37

tdf120224 writerfilter: consume excess bookmark from fields
Although this follows a very different code path, copy the ww8
import idea of "consuming" the bookmark of a BOOK_FIELD.

This is particularly important for textcontrols, especially
since LO keeps duplicating bookmarks as it adds another
bookmark for the field name at each save.

Existing unit tests that this matches are fdo53985.docx and
tdf111964.docx. I expected more, but apparently most fields
don't contain or represent any bookmarks.

This patch is for import only. A followup patch stops
the creation of duplicate bookmarks during export.

Change-Id: I1e11980e52dc523393fd6d621191228d676e9a17
Reviewed-on: https://gerrit.libreoffice.org/61615
Tested-by: Jenkins
Reviewed-by: Justin Luth <justin_luth@sil.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -5101,7 +5101,22 @@ void DomainMapper_Impl::SetBookmarkName( const OUString& rBookmarkName )
 {
     BookmarkMap_t::iterator aBookmarkIter = m_aBookmarkMap.find( m_sCurrentBkmkId );
     if( aBookmarkIter != m_aBookmarkMap.end() )
+    {
+        // fields are internal bookmarks: consume redundant "normal" bookmark
+        if ( IsOpenField() )
+        {
+            FFDataHandler::Pointer_t  pFFDataHandler(GetTopFieldContext()->getFFDataHandler());
+            if ( IsOpenFieldCommand() || (pFFDataHandler && pFFDataHandler->getName() == rBookmarkName) )
+            {
+                // HACK: At the END marker, StartOrEndBookmark will START
+                // a bookmark which will eventually be abandoned, not created.
+                m_aBookmarkMap.erase(aBookmarkIter);
+                return;
+            }
+        }
+
         aBookmarkIter->second.m_sBookmarkName = rBookmarkName;
+    }
     else
         m_sCurrentBkmkName = rBookmarkName;
 }

sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -723,6 +723,45 @@ DECLARE_OOXMLEXPORT_TEST(testObjectCrossReference, "object_cross_reference.odt")
     CPPUNIT_ASSERT_EQUAL(sal_uInt16(21), nIndex);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf120224_textControlCrossRef, "tdf120224_textControlCrossRef.docx")
+{
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(xTextDocument->getText(),
+                                                                  uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+    uno::Reference<container::XEnumerationAccess> xRunEnumAccess(xParaEnum->nextElement(),
+                                                                 uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xRunEnum = xRunEnumAccess->createEnumeration();
+    xRunEnum->nextElement(); //Text
+    uno::Reference<beans::XPropertySet> xPropertySet(xRunEnum->nextElement(), uno::UNO_QUERY);
+
+    CPPUNIT_ASSERT_EQUAL(OUString("TextFieldStart"),
+                         getProperty<OUString>(xPropertySet, "TextPortionType"));
+    uno::Reference<container::XNamed> xBookmark(
+        getProperty<uno::Reference<beans::XPropertySet>>(xPropertySet, "Bookmark"), uno::UNO_QUERY);
+
+    // Critical test: does TextField's bookmark name match cross-reference?
+    const OUString& sTextFieldName(xBookmark->getName());
+    uno::Reference<text::XTextFieldsSupplier> xTextFieldsSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xFieldsAccess(
+        xTextFieldsSupplier->getTextFields());
+    uno::Reference<container::XEnumeration> xFields(xFieldsAccess->createEnumeration());
+    CPPUNIT_ASSERT(xFields->hasMoreElements());
+    xPropertySet.set(xFields->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sTextFieldName, getProperty<OUString>(xPropertySet, "SourceName"));
+
+    uno::Reference<text::XBookmarksSupplier> xBookmarksSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xBookmarksByIdx(xBookmarksSupplier->getBookmarks(),
+                                                            uno::UNO_QUERY);
+    uno::Reference<container::XNameAccess> xBookmarksByName(xBookmarksSupplier->getBookmarks(),
+                                                            uno::UNO_QUERY);
+    // TextFields should not be turned into real bookmarks.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), xBookmarksByIdx->getCount());
+
+    // The actual name isn't critical, but if it fails, it is worth asking why.
+    CPPUNIT_ASSERT_EQUAL(OUString("Text1"), sTextFieldName);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf117504_numberingIndent, "tdf117504_numberingIndent.docx")
 {
     OUString sName = getProperty<OUString>(getParagraph(1), "NumberingStyleName");

commit 443a8d359c1553732bc4da1b69ba1ac3692feba4

tdf#120224 writerfilter: don't suppress all bookmarks inside fields
MSWordExportBase used to erroneously export all sorts of marks as
bookmarks, but we fixed that.  Apparently this code in
DomainMapper_Impl::SetBookmarkName() should prevent import of the
bookmarks that were exported as duplicates for fieldmarks; suppress
only the marks with the same name as the fieldmark, but not unrelated
marks that happen to start inside the field, like the "_GoBack" one in
tdf120224_textControlCrossRef.docx.

The reason why this was necessary previously is some dubious
nonsense in model.xml that caused the ffData stuff to be delayed until
the next run, hence the bookmark start is sent to domain mapper before
the ffData; this was already helpfully marked with a TODO
comment, so just implement the suggestion.

(regression from ccadb3eb11d2ba380e47dcad6705de3ccd423a37)

Change-Id: I15a1865ac34b0b9b3f11849d612e95115582b90b
Reviewed-on: https://gerrit.libreoffice.org/80917
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -5455,7 +5455,7 @@ void DomainMapper_Impl::SetBookmarkName( const OUString& rBookmarkName )
         if ( IsOpenField() )
         {
             FFDataHandler::Pointer_t  pFFDataHandler(GetTopFieldContext()->getFFDataHandler());
-            if ( IsOpenFieldCommand() || (pFFDataHandler && pFFDataHandler->getName() == rBookmarkName) )
+            if (pFFDataHandler && pFFDataHandler->getName() == rBookmarkName)
             {
                 // HACK: At the END marker, StartOrEndBookmark will START
                 // a bookmark which will eventually be abandoned, not created.

writerfilter/source/ooxml/model.xml
@@ -17650,8 +17650,7 @@
       <element name="checkBox" tokenid="ooxml:CT_FFData_checkBox"/>
       <element name="ddList" tokenid="ooxml:CT_FFData_ddList"/>
       <element name="textInput" tokenid="ooxml:CT_FFData_textInput"/>
-      <!-- TODO: This is possibly wrong and should be sendPropertiesWithId -->
-      <action name="end" action="propagateCharacterPropertiesAsSet" sendtokenid="ooxml:ffdata"/>
+      <action name="end" action="sendPropertiesWithId" sendtokenid="ooxml:ffdata"/>
       <action name="end" action="clearProps"/>
     </resource>
     <resource name="CT_FFHelpText" resource="Properties">