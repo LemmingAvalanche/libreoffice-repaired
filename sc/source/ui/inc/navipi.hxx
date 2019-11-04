LemmingAvalanche/libreoffice-repaired

commit c6f20f28563ff8597f7bbab97d4867e83c9affda

sc: rowcol: tdf#50916 convert core/tool
Change-Id: I0fe5a7ef4a79d0832802945ce1b9da4752f1d5fc
Reviewed-on: https://gerrit.libreoffice.org/81598
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/inc/navipi.hxx
@@ -112,9 +112,10 @@ class ColumnEdit : public SpinField
 public:
     ColumnEdit(Window* pParent, WinBits nWinBits);
     ~ColumnEdit() override;
-    void SetNavigatorDlg(ScNavigatorDlg *pNaviDlg)
+    void SetNavigatorDlg(ScNavigatorDlg *pNaviDlg, const ScDocument* pDoc)
     {
         xDlg = pNaviDlg;
+        mpDoc = pDoc;
     }
     SCCOL   GetCol() const { return nCol; }
     void    SetCol( SCCOL nColNo );
@@ -129,12 +130,13 @@ protected:
     virtual void    dispose() override;
 
 private:
+    const ScDocument* mpDoc;
     VclPtr<ScNavigatorDlg> xDlg;
     SCCOL           nCol;
 
     void EvalText();
     void ExecuteCol();
-    static SCCOL AlphaToNum    ( OUString& rStr );
+    static SCCOL AlphaToNum    ( const ScDocument* pDoc, OUString& rStr );
     static SCCOL NumStrToAlpha ( OUString& rStr );
     static SCCOL NumToAlpha    ( SCCOL nColNo, OUString& rStr );
 };

sc/source/ui/navipi/navipi.cxx
@@ -68,8 +68,8 @@ void ScNavigatorDlg::ReleaseFocus()
 }
 
 ColumnEdit::ColumnEdit(Window* pParent, WinBits nWinBits)
-    : SpinField(pParent, nWinBits)
-    , nCol(0)
+    : SpinField(pParent, nWinBits),
+    nCol(0)
 {
     SetMaxTextLen(SCNAV_COLDIGITS);   // 1...256...18278 or A...IV...ZZZ
 }
@@ -162,7 +162,7 @@ void ColumnEdit::EvalText()
         if ( CharClass::isAsciiNumeric(aStrCol) )
             nCol = NumStrToAlpha( aStrCol );
         else
-            nCol = AlphaToNum( aStrCol );
+            nCol = AlphaToNum( mpDoc, aStrCol );
     }
     else
         nCol = 0;
@@ -197,7 +197,7 @@ void ColumnEdit::SetCol( SCCOL nColNo )
     }
 }
 
-SCCOL ColumnEdit::AlphaToNum( OUString& rStr )
+SCCOL ColumnEdit::AlphaToNum( const ScDocument* pDoc, OUString& rStr )
 {
     SCCOL  nColumn = 0;
 
@@ -205,7 +205,7 @@ SCCOL ColumnEdit::AlphaToNum( OUString& rStr )
     {
         rStr = rStr.toAsciiUpperCase();
 
-        if (::AlphaToCol( nColumn, rStr))
+        if (::AlphaToCol( pDoc, nColumn, rStr))
             ++nColumn;
 
         if ( (rStr.getLength() > SCNAV_COLLETTERS) || (nColumn > SCNAV_MAXCOL) )
@@ -459,7 +459,8 @@ ScNavigatorDlg::ScNavigatorDlg(SfxBindings* pB, vcl::Window* pParent)
 {
     get(aLbDocuments, "documents");
     get(aEdCol, "column");
-    aEdCol->SetNavigatorDlg(this);
+    ScTabViewShell* pViewSh = GetTabViewShell();
+    aEdCol->SetNavigatorDlg(this, pViewSh->GetViewData().GetDocument());
     get(aEdRow, "row");
     aEdRow->SetNavigatorDlg(this);
     get(aTbxCmd, "toolbox");

commit eb9224540cdf057659a05a8a5a5737182a6fda8d

tdf#128562 crash when opening navigator in calc
regression from
    commit c6f20f28563ff8597f7bbab97d4867e83c9affda
    Date:   Mon Oct 28 13:08:29 2019 +0200
    sc: rowcol: tdf#50916 convert core/tool

Change-Id: If2784e3c719de39071b699a5573b65503d3cf10e
Reviewed-on: https://gerrit.libreoffice.org/81985
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/inc/navipi.hxx
@@ -231,7 +231,8 @@ private:
 
     static ScTabViewShell*  GetTabViewShell();
     static ScNavigatorSettings* GetNavigatorSettings();
-    bool                    GetViewData();
+    ScViewData*             GetViewData();
+
 
     void    UpdateColumn    ( const SCCOL* pCol = nullptr );
     void    UpdateRow       ( const SCROW* pRow = nullptr );

sc/source/ui/navipi/navcitem.cxx
@@ -23,6 +23,7 @@
 
 #include <navcitem.hxx>
 #include <navipi.hxx>
+#include <viewdata.hxx>
 #include <sc.hrc>
 
 ScNavigatorControllerItem::ScNavigatorControllerItem( sal_uInt16          nIdP,
@@ -49,7 +50,7 @@ void ScNavigatorControllerItem::StateChanged( sal_uInt16 /* nSID */, SfxItemStat
                 {
                     const OUString&  aAddress( pCellPosItem->GetValue() );
                     ScAddress aScAddress;
-                    aScAddress.Parse( aAddress );
+                    aScAddress.Parse( aAddress, rNavigatorDlg.GetViewData()->GetDocument() );
 
                     SCCOL nCol = aScAddress.Col()+1;
                     SCROW nRow = aScAddress.Row()+1;

sc/source/ui/navipi/navipi.cxx
@@ -818,12 +818,11 @@ ScNavigatorSettings* ScNavigatorDlg::GetNavigatorSettings()
     return pViewSh ? pViewSh->GetNavigatorSettings() : nullptr;
 }
 
-bool ScNavigatorDlg::GetViewData()
+ScViewData* ScNavigatorDlg::GetViewData()
 {
     ScTabViewShell* pViewSh = GetTabViewShell();
     pViewData = pViewSh ? &pViewSh->GetViewData() : nullptr;
-
-    return ( pViewData != nullptr );
+    return pViewData;
 }
 
 void ScNavigatorDlg::UpdateColumn( const SCCOL* pCol )