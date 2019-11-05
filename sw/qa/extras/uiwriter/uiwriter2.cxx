LemmingAvalanche/libreoffice-repaired

commit c78b3f5d8b50668054683ca21439d5283b80c33c

SwDrawView::DeleteMarked: delete textbox of shape as well
If we delete a shape that had a textbox, then delete that textbox as
well. Without that, the doc model would be still consistent, but most
probably would not be what the user expects.

Change-Id: Id5075233ce66d7a398c88ff3e63b05a6b2133571

sw/source/core/draw/dview.cxx
@@ -34,6 +34,7 @@
 #include "frmfmt.hxx"
 #include "dflyobj.hxx"
 #include "dcontact.hxx"
+#include "textboxhelper.hxx"
 #include "frmatr.hxx"
 #include "viewsh.hxx"
 #include "viewimp.hxx"
@@ -939,10 +940,27 @@ void SwDrawView::DeleteMarked()
             }
         }
     }
+
+    // Check what textboxes have to be deleted afterwards.
+    const SdrMarkList& rMarkList = GetMarkedObjectList();
+    std::vector<SwFrmFmt*> aTextBoxesToDelete;
+    for (sal_uInt16 i = 0; i < rMarkList.GetMarkCount(); ++i)
+    {
+        SdrObject *pObject = rMarkList.GetMark(i)->GetMarkedSdrObj();
+        SwDrawContact* pDrawContact = static_cast<SwDrawContact*>(GetUserCall(pObject));
+        SwFrmFmt* pFmt = pDrawContact->GetFmt();
+        if (SwFrmFmt* pTextBox = SwTextBoxHelper::findTextBox(pFmt))
+            aTextBoxesToDelete.push_back(pTextBox);
+    }
+
     if ( pDoc->DeleteSelection( *this ) )
     {
         FmFormView::DeleteMarked();
         ::FrameNotify( Imp().GetShell(), FLY_DRAG_END );
+
+        // Only delete these now: earlier deletion would clear the mark list as well.
+        for (std::vector<SwFrmFmt*>::iterator i = aTextBoxesToDelete.begin(); i != aTextBoxesToDelete.end(); ++i)
+            pDoc->DelLayoutFmt(*i);
     }
     pDoc->GetIDocumentUndoRedo().EndUndo(UNDO_EMPTY, NULL);
     if( pTmpRoot )

sw/CppunitTest_sw_uiwriter.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sw_uiwriter, \
     cppuhelper \
     sal \
     svt \
+	svxcore \
     sw \
     test \
     unotest \

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -12,6 +12,9 @@
 #include <crsskip.hxx>
 #include <shellio.hxx>
 #include <expfld.hxx>
+#include <drawdoc.hxx>
+
+#include <svx/svdpage.hxx>
 
 #include "UndoManager.hxx"
 
@@ -32,6 +35,7 @@ public:
     void testFdo75110();
     void testFdo75898();
     void testFdo74981();
+    void testShapeTextboxDelete();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -43,6 +47,7 @@ public:
     CPPUNIT_TEST(testFdo75110);
     CPPUNIT_TEST(testFdo75898);
     CPPUNIT_TEST(testFdo74981);
+    CPPUNIT_TEST(testShapeTextboxDelete);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -269,6 +274,23 @@ void SwUiWriterTest::testFdo74981()
     aIdx--;
     pTxtNode = aIdx.GetNode().GetTxtNode();
     CPPUNIT_ASSERT(!pTxtNode->HasHints());
+}
+
+void SwUiWriterTest::testShapeTextboxDelete()
+{
+    SwDoc* pDoc = createDoc("shape-textbox-delete.odt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SdrPage* pPage = pDoc->getIDocumentDrawModelAccess().GetDrawModel()->GetPage(0);
+    SdrObject* pObject = pPage->GetObj(0);
+    pWrtShell->SelectObj(Point(), 0, pObject);
+    sal_Int32 nActual = pPage->GetObjCount();
+    // Two objects on the draw page: the shape and its textbox.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), nActual);
+
+    pWrtShell->DelSelectedObj();
+    nActual = pPage->GetObjCount();
+    // Both (not only the shape) should be removed by now (the textbox wasn't removed, so this was 1).
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), nActual);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);

commit feeff3fe677008295038118cf0f59d4d1643f3be

tdf#128335 sw textbox: fix crash on deleting multiple shapes with textboxes
This was introduced in commit c78b3f5d8b50668054683ca21439d5283b80c33c
(SwDrawView::DeleteMarked: delete textbox of shape as well, 2014-06-23),
the problem is that in case one shape+textframe pair is anchored in an
other textframe, which is also part of a shape+textframe pair, it
matters what order we use for deleting these objects.

What happened is that the anchor position of a 2nd textbox was updated
on the deletion (cut) of the 1st textbox, and this was not properly
restored on paste.

Fix the problem by deleting in reverse order, assuming that the
container is ordered by anchor positions.

Change-Id: I62bc12a5064ce397234999e8fd7b108f79de3c24
Reviewed-on: https://gerrit.libreoffice.org/82042
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/draw/dview.cxx
@@ -975,8 +975,12 @@ void SwDrawView::DeleteMarked()
         ::FrameNotify( Imp().GetShell(), FLY_DRAG_END );
 
         // Only delete these now: earlier deletion would clear the mark list as well.
-        for (auto& rpTextBox : aTextBoxesToDelete)
+        // Delete in reverse order, assuming that the container is sorted by anchor positions.
+        for (int i = aTextBoxesToDelete.size() - 1; i >= 0; --i)
+        {
+            SwFrameFormat*& rpTextBox = aTextBoxesToDelete[i];
             pDoc->getIDocumentLayoutAccess().DelLayoutFormat(rpTextBox);
+        }
     }
     pDoc->GetIDocumentUndoRedo().EndUndo(SwUndoId::EMPTY, nullptr);
     if( pTmpRoot )

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -17,6 +17,7 @@
 #include <com/sun/star/frame/DispatchHelper.hpp>
 #include <com/sun/star/style/LineSpacing.hpp>
 #include <com/sun/star/text/TableColumnSeparator.hpp>
+#include <com/sun/star/view/XSelectionSupplier.hpp>
 #include <comphelper/propertysequence.hxx>
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
 #include <i18nlangtag/languagetag.hxx>
@@ -2327,6 +2328,41 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf118311)
     discardDumpedLayout();
     pXmlDoc = parseLayoutDump();
     assertXPath(pXmlDoc, "//page[1]//body/tab", 0);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf128335)
+{
+    // Load the bugdoc, which has 3 textboxes.
+    SwDoc* pDoc = createDoc("tdf128335.odt");
+
+    // Select the 3rd textbox.
+    SwView* pView = pDoc->GetDocShell()->GetView();
+    pView->GetViewFrame()->GetDispatcher()->Execute(FN_CNTNT_TO_NEXT_FRAME, SfxCallMode::SYNCHRON);
+    // Make sure SwTextShell is replaced with SwDrawShell right now, not after 120 ms, as set in the
+    // SwView ctor.
+    pView->StopShellTimer();
+    SwXTextDocument* pXTextDocument = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    pXTextDocument->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, KEY_TAB);
+    pXTextDocument->postKeyEvent(LOK_KEYEVENT_KEYUP, 0, KEY_TAB);
+    pXTextDocument->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, KEY_TAB);
+    pXTextDocument->postKeyEvent(LOK_KEYEVENT_KEYUP, 0, KEY_TAB);
+    Scheduler::ProcessEventsToIdle();
+
+    // Cut it.
+    pView->GetViewFrame()->GetDispatcher()->Execute(SID_CUT, SfxCallMode::SYNCHRON);
+
+    // Paste it: this makes the 3rd textbox anchored in the 2nd one.
+    pView->GetViewFrame()->GetDispatcher()->Execute(SID_PASTE, SfxCallMode::SYNCHRON);
+
+    // Select all shapes.
+    uno::Reference<view::XSelectionSupplier> xSelectionSupplier(
+        pXTextDocument->getCurrentController(), uno::UNO_QUERY);
+    xSelectionSupplier->select(pXTextDocument->getDrawPages()->getByIndex(0));
+
+    // Cut them.
+    // Without the accompanying fix in place, this test would have crashed as the textboxes were
+    // deleted in an incorrect order.
+    pView->GetViewFrame()->GetDispatcher()->Execute(SID_CUT, SfxCallMode::SYNCHRON);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */