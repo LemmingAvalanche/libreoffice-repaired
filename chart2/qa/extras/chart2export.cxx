LemmingAvalanche/libreoffice-repaired

commit bbd289bb245a0176e2e0bf217366f988d01e78a1

tdf#101322 Chart OOXML Export: fix missing subtitle
Concatenate subtitle and main title texts, if both exist, just like
at the XLS/DOC export, because OOXML does not support subtitles.
If we only have a subtitle, export the whole subtitle shape instead of
the title shape, keeping its properties.

Change-Id: If6a27c023fcce58f4549a0edad44027bb0348b93
Reviewed-on: https://gerrit.libreoffice.org/77499
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

oox/source/export/chartexport.cxx
@@ -874,13 +874,13 @@ void ChartExport::exportChart( const Reference< css::chart::XChartDocument >& xC
 
     // get Properties of ChartDocument
     bool bHasMainTitle = false;
+    bool bHasSubTitle = false;
     bool bHasLegend = false;
     Reference< beans::XPropertySet > xDocPropSet( xChartDoc, uno::UNO_QUERY );
     if( xDocPropSet.is())
     {
         try
         {
-            bool bHasSubTitle = false;
             Any aAny( xDocPropSet->getPropertyValue("HasMainTitle"));
             aAny >>= bHasMainTitle;
             aAny = xDocPropSet->getPropertyValue("HasSubTitle");
@@ -899,15 +899,27 @@ void ChartExport::exportChart( const Reference< css::chart::XChartDocument >& xC
     FSHelperPtr pFS = GetFS();
     pFS->startElement(FSNS(XML_c, XML_chart));
 
-    // title
-    if( bHasMainTitle )
+    // titles
+    if( bHasMainTitle || bHasSubTitle )
     {
-        Reference< drawing::XShape > xShape = xChartDoc->getTitle();
-        if( xShape.is() )
+        OUString aSubText;
+        Reference< drawing::XShape > xShape;
+        if( bHasSubTitle )
         {
-            exportTitle( xShape );
-            pFS->singleElement(FSNS(XML_c, XML_autoTitleDeleted), XML_val, "0");
+            xShape = xChartDoc->getSubTitle();
+            if( bHasMainTitle )
+            {
+                // if we have a title and a subtitle too, we need only the subtitle text
+                Reference< beans::XPropertySet > xPropSet(xShape, uno::UNO_QUERY);
+                if( xPropSet.is() )
+                    xPropSet->getPropertyValue("String") >>= aSubText;
+            }
         }
+        if( bHasMainTitle )
+            xShape = xChartDoc->getTitle();
+
+        exportTitle( xShape, !aSubText.isEmpty() ? &aSubText : nullptr );
+        pFS->singleElement(FSNS(XML_c, XML_autoTitleDeleted), XML_val, "0");
     }
     InitPlotArea( );
     if( mbIs3DChart )
@@ -1086,7 +1098,7 @@ void ChartExport::exportLegend( const Reference< css::chart::XChartDocument >& x
     pFS->endElement( FSNS( XML_c, XML_legend ) );
 }
 
-void ChartExport::exportTitle( const Reference< XShape >& xShape )
+void ChartExport::exportTitle( const Reference< XShape >& xShape, const OUString* pSubText)
 {
     OUString sText;
     Reference< beans::XPropertySet > xPropSet( xShape, uno::UNO_QUERY );
@@ -1094,6 +1106,11 @@ void ChartExport::exportTitle( const Reference< XShape >& xShape )
     {
         xPropSet->getPropertyValue("String") >>= sText;
     }
+
+    // tdf#101322: add subtitle to title
+    if( pSubText )
+        sText = sText.isEmpty() ? *pSubText : sText + "\n" + *pSubText;
+
     if( sText.isEmpty() )
         return;
 

chart2/qa/extras/chart2export.cxx
@@ -103,6 +103,8 @@ public:
     void testTitleManualLayoutXLSX();
     void testPlotAreaManualLayoutXLSX();
     void testLegendManualLayoutXLSX();
+    void testChartSubTitle();
+    void testChartMainWithSubTitle();
     void testChartTitlePropertiesColorFillXLSX();
     void testChartTitlePropertiesGradientFillXLSX();
     void testChartTitlePropertiesBitmapFillXLSX();
@@ -203,6 +205,8 @@ public:
     CPPUNIT_TEST(testTitleManualLayoutXLSX);
     CPPUNIT_TEST(testPlotAreaManualLayoutXLSX);
     CPPUNIT_TEST(testLegendManualLayoutXLSX);
+    CPPUNIT_TEST(testChartSubTitle);
+    CPPUNIT_TEST(testChartMainWithSubTitle);
     CPPUNIT_TEST(testChartTitlePropertiesColorFillXLSX);
     CPPUNIT_TEST(testChartTitlePropertiesGradientFillXLSX);
     CPPUNIT_TEST(testChartTitlePropertiesBitmapFillXLSX);
@@ -1643,6 +1647,35 @@ void Chart2ExportTest::testLegendManualLayoutXLSX()
 
     // Make sure that default text font size is preserved after export
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:legend/c:txPr/a:p/a:pPr/a:defRPr", "sz", "900");
+}
+
+void Chart2ExportTest::testChartSubTitle()
+{
+    load("/chart2/qa/extras/data/ods/", "testChartSubTitle.ods");
+    xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+    CPPUNIT_ASSERT(pXmlDoc);
+    // test properties of subtitle
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr", "sz", "1100");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr", "b", "1");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr/a:solidFill/a:srgbClr", "val", "00a933");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:r/a:rPr/a:latin", "typeface", "Times New Roman");
+    assertXPathContent(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:r/a:t", "It is a Subtitle");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:spPr/a:solidFill/a:srgbClr", "val", "b2b2b2");
+}
+
+void Chart2ExportTest::testChartMainWithSubTitle()
+{
+    load("/chart2/qa/extras/data/ods/", "testChartMainWithSubTitle.ods");
+    xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+    CPPUNIT_ASSERT(pXmlDoc);
+    // test properties of title
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr", "sz", "1300");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr", "b", "0");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr", "i", "1");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:pPr/a:defRPr/a:solidFill/a:srgbClr", "val", "f10d0c");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:r/a:rPr/a:latin", "typeface", "Arial");
+    assertXPathContent(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:r/a:t", "It is a Maintitle\nIt is a Subtitle");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:spPr/a:solidFill/a:srgbClr", "val", "81d41a");
 }
 
 void Chart2ExportTest::testChartTitlePropertiesColorFillXLSX()

include/oox/export/chartexport.hxx
@@ -136,8 +136,8 @@ private:
                               css::chart::XChartDocument >& rChartDoc );
     void exportLegend( const css::uno::Reference<
                           css::chart::XChartDocument >& rChartDoc );
-    void exportTitle( const css::uno::Reference<
-                          css::drawing::XShape >& xShape );
+    void exportTitle( const css::uno::Reference< css::drawing::XShape >& xShape,
+                          const OUString* pSubText = nullptr );
     void exportPlotArea( const css::uno::Reference<
                              css::chart::XChartDocument >& rChartDoc );
     void exportFill( const css::uno::Reference< css::beans::XPropertySet >& xPropSet );

commit 2ee415d109c8fd0ba16f4ab13b2a6d5748096e4c

tdf#128618 OOXML chart export: deleted automatic title returns on save
If there is no main title and/or subtitle, write out autoTitleDeleted
with a true value.

Regression from the commit bbd289bb245a0176e2e0bf217366f988d01e78a1
(tdf#101322 Chart OOXML Export: fix missing subtitle)

Change-Id: I0094014fc4da4cb66d31e4249f916452d00758c7
Reviewed-on: https://gerrit.libreoffice.org/82142
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

oox/source/export/chartexport.cxx
@@ -917,7 +917,7 @@ void ChartExport::exportChart( const Reference< css::chart::XChartDocument >& xC
 
     // get Properties of ChartDocument
     bool bHasMainTitle = false;
-    bool bHasSubTitle = false;
+    OUString aSubTitle;
     bool bHasLegend = false;
     Reference< beans::XPropertySet > xDocPropSet( xChartDoc, uno::UNO_QUERY );
     if( xDocPropSet.is())
@@ -926,8 +926,6 @@ void ChartExport::exportChart( const Reference< css::chart::XChartDocument >& xC
         {
             Any aAny( xDocPropSet->getPropertyValue("HasMainTitle"));
             aAny >>= bHasMainTitle;
-            aAny = xDocPropSet->getPropertyValue("HasSubTitle");
-            aAny >>= bHasSubTitle;
             aAny = xDocPropSet->getPropertyValue("HasLegend");
             aAny >>= bHasLegend;
         }
@@ -937,33 +935,38 @@ void ChartExport::exportChart( const Reference< css::chart::XChartDocument >& xC
         }
     } // if( xDocPropSet.is())
 
-    // chart element
+    Reference< beans::XPropertySet > xPropSubTitle( xChartDoc->getSubTitle(), UNO_QUERY );
+    if( xPropSubTitle.is())
+    {
+        try
+        {
+            xPropSubTitle->getPropertyValue("String") >>= aSubTitle;
+        }
+        catch( beans::UnknownPropertyException & )
+        {
+        }
+    }
 
+    // chart element
     FSHelperPtr pFS = GetFS();
     pFS->startElement(FSNS(XML_c, XML_chart));
 
     // titles
-    if( bHasMainTitle || bHasSubTitle )
+    if( bHasMainTitle )
     {
-        OUString aSubText;
-        Reference< drawing::XShape > xShape;
-        if( bHasSubTitle )
-        {
-            xShape = xChartDoc->getSubTitle();
-            if( bHasMainTitle )
-            {
-                // if we have a title and a subtitle too, we need only the subtitle text
-                Reference< beans::XPropertySet > xPropSet(xShape, uno::UNO_QUERY);
-                if( xPropSet.is() )
-                    xPropSet->getPropertyValue("String") >>= aSubText;
-            }
-        }
-        if( bHasMainTitle )
-            xShape = xChartDoc->getTitle();
-
-        exportTitle( xShape, !aSubText.isEmpty() ? &aSubText : nullptr );
+        exportTitle( xChartDoc->getTitle(), !aSubTitle.isEmpty() ? &aSubTitle : nullptr );
         pFS->singleElement(FSNS(XML_c, XML_autoTitleDeleted), XML_val, "0");
     }
+    else if( !aSubTitle.isEmpty() )
+    {
+        exportTitle( xChartDoc->getSubTitle(), nullptr );
+        pFS->singleElement(FSNS(XML_c, XML_autoTitleDeleted), XML_val, "0");
+    }
+    else
+    {
+        pFS->singleElement(FSNS(XML_c, XML_autoTitleDeleted), XML_val, "1");
+    }
+
     InitPlotArea( );
     if( mbIs3DChart )
     {

chart2/qa/extras/chart2export.cxx
@@ -108,6 +108,7 @@ public:
     void testLegendManualLayoutXLSX();
     void testChartSubTitle();
     void testChartMainWithSubTitle();
+    void testAutoTitleDeleted();
     void testChartTitlePropertiesColorFillXLSX();
     void testChartTitlePropertiesGradientFillXLSX();
     void testChartTitlePropertiesBitmapFillXLSX();
@@ -217,6 +218,7 @@ public:
     CPPUNIT_TEST(testLegendManualLayoutXLSX);
     CPPUNIT_TEST(testChartSubTitle);
     CPPUNIT_TEST(testChartMainWithSubTitle);
+    CPPUNIT_TEST(testAutoTitleDeleted);
     CPPUNIT_TEST(testChartTitlePropertiesColorFillXLSX);
     CPPUNIT_TEST(testChartTitlePropertiesGradientFillXLSX);
     CPPUNIT_TEST(testChartTitlePropertiesBitmapFillXLSX);
@@ -1730,6 +1732,14 @@ void Chart2ExportTest::testChartMainWithSubTitle()
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:r/a:rPr/a:latin", "typeface", "Arial");
     assertXPathContent(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:tx/c:rich/a:p/a:r/a:t", "It is a Maintitle\nIt is a Subtitle");
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:title/c:spPr/a:solidFill/a:srgbClr", "val", "81d41a");
+}
+
+void Chart2ExportTest::testAutoTitleDeleted()
+{
+    load("/chart2/qa/extras/data/xlsx/", "testAutoTitleDeleted.xlsx");
+    xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+    CPPUNIT_ASSERT(pXmlDoc);
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:autoTitleDeleted", "val", "1");
 }
 
 void Chart2ExportTest::testChartTitlePropertiesColorFillXLSX()