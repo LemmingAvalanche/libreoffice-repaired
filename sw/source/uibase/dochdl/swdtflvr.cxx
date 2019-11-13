LemmingAvalanche/libreoffice-repaired

commit 6356bbb6ebe1155cbe24f11bd7a65d82629df80c

tdf#84806 Writer: drag and drop selected tables, don't empty
Drag and drop operation on a wholly selected table
resulted a copy of the original table, keeping also the
original one without its text content, ie. emptying it.

Now drag and drop works as intended: moving the table
instead of copying and emptying (like also MSO does).

See also commit a09994553425a31465e9d1d463e15d09b72e9896
"tdf#118311 Writer: cut selected table, not only its text content".

Change-Id: I7156420d35bd8de320c2559bc9453dbad846517d
Reviewed-on: https://gerrit.libreoffice.org/79517
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -3611,6 +3611,7 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
     bool bSttWrd = false;
     bool bSttPara = false;
     bool bTableSel = false;
+    bool bTableWholeSel = false;
     bool bFrameSel = false;
 
     SwWrtShell& rSrcSh = *GetShell();
@@ -3674,7 +3675,11 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
         return false;
 
     if( rSrcSh.IsTableMode() )
+    {
         bTableSel = true;
+        if ( rSrcSh.HasWholeTabSelection() )
+            bTableWholeSel = true;
+    }
     else if( rSrcSh.IsSelFrameMode() || rSrcSh.IsObjSelected() )
     {
         // don't move position-protected objects!
@@ -3846,6 +3851,11 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                 rSh.EndSelect();
             }
         }
+    }
+    else if ( bRet && bTableWholeSel )
+    {
+        SfxDispatcher* pDispatch = rSrcSh.GetView().GetViewFrame()->GetDispatcher();
+        pDispatch->Execute(FN_TABLE_DELETE_TABLE, SfxCallMode::SYNCHRON);
     }
 
     if( bRet && bMove && bFrameSel )

commit c8b6d92860b339b13247752f586802a3cec17a9a

tdf#128776 Writer: fix Ctrl + drag and drop table copy
Regression from the commit 6356bbb6ebe1155cbe24f11bd7a65d82629df80c
(tdf#84806 Writer: drag and drop selected tables, don't empty).

Change-Id: I9061b80dd8fab29aa5ec74655dc6c3f7686a91ee
Reviewed-on: https://gerrit.libreoffice.org/82579
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -3696,7 +3696,7 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
     bool bSttWrd = false;
     bool bSttPara = false;
     bool bTableSel = false;
-    bool bTableWholeSel = false;
+    bool bTableMove = false;
     bool bFrameSel = false;
 
     SwWrtShell& rSrcSh = *GetShell();
@@ -3762,8 +3762,8 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
     if( rSrcSh.IsTableMode() )
     {
         bTableSel = true;
-        if ( rSrcSh.HasWholeTabSelection() )
-            bTableWholeSel = true;
+        if ( bMove && rSrcSh.HasWholeTabSelection() )
+            bTableMove = true;
     }
     else if( rSrcSh.IsSelFrameMode() || rSrcSh.IsObjSelected() )
     {
@@ -3937,7 +3937,7 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
             }
         }
     }
-    else if ( bRet && bTableWholeSel )
+    else if ( bRet && bTableMove )
     {
         SfxDispatcher* pDispatch = rSrcSh.GetView().GetViewFrame()->GetDispatcher();
         pDispatch->Execute(FN_TABLE_DELETE_TABLE, SfxCallMode::SYNCHRON);