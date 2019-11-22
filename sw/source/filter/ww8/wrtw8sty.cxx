LemmingAvalanche/libreoffice-repaired

commit 91e4d3db4c89e95f9d65e3ac18667a5e334eac0e

tdf#92335 DOCX: fix multiplying of "ListLabel" styles
by removing the redundant export of the temporary
DOCX import character styles for bullets of bulleted lists.

Note: this commit will fix the older documents – sometimes
with thousands of unused styles – by the next export.

The problem was escalated from the commit
036a5d56f9f92f1d3efd16c43dc14d103e9a2623
"tdf#95213 DOCX import: don't reuse list label styles"

Change-Id: I90590352c0c85a92dbad19a45e82339e1201f1e9
Reviewed-on: https://gerrit.libreoffice.org/73098
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/filter/ww8/wrtw8sty.cxx
@@ -618,6 +618,11 @@ void MSWordStyles::OutputStyle( SwFormat* pFormat, sal_uInt16 nPos )
                 aName = aBaseName + OUString::number(++nSuffix);
             }
         }
+        else if (!bFormatColl && m_rExport.m_pStyles->GetStyleId(nPos).startsWith("ListLabel"))
+        {
+            // tdf#92335 don't export redundant DOCX import style "ListLabel"
+            return;
+        }
 
         m_rExport.AttrOutput().StartStyle( aName, (bFormatColl ? STYLE_TYPE_PARA : STYLE_TYPE_CHAR),
                 nBase, nWwNext, GetWWId( *pFormat ), nPos,

sw/qa/extras/ooxmlexport/ooxmlexport7.cxx
@@ -1122,6 +1122,15 @@ DECLARE_OOXMLEXPORT_TEST(testFlipAndRotateCustomShape, "flip_and_rotate.odt")
 #endif
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf92335, "tdf92335.docx")
+{
+    // Don't export redundant ListLabel character styles
+    xmlDocPtr pXmlStyles = parseExport("word/styles.xml");
+    if (!pXmlStyles)
+        return;
+
+    assertXPath(pXmlStyles, "//w:style[@w:styleId='ListLabel1']", 0);
+}
 
 CPPUNIT_PLUGIN_IMPLEMENT();
 

commit 24fa491c1df35721b47d700e6383f3239c0cfca7

tdf#128499 DOC export: fix lost ToC formatting regression
caused by commit 91e4d3db4c89e95f9d65e3ac18667a5e334eac0e
(tdf#92335 DOCX: fix multiplying of "ListLabel" styles).

Change-Id: I7d6fc2be3ad7556b988cb00936b9b49deae23f19
Reviewed-on: https://gerrit.libreoffice.org/83443
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/filter/ww8/wrtw8sty.cxx
@@ -628,7 +628,8 @@ void MSWordStyles::OutputStyle( SwFormat* pFormat, sal_uInt16 nPos )
                 aName = aBaseName + OUString::number(++nSuffix);
             }
         }
-        else if (!bFormatColl && m_rExport.m_pStyles->GetStyleId(nPos).startsWith("ListLabel"))
+        else if (!bFormatColl && m_rExport.GetExportFormat() == MSWordExportBase::DOCX &&
+                        m_rExport.m_pStyles->GetStyleId(nPos).startsWith("ListLabel"))
         {
             // tdf#92335 don't export redundant DOCX import style "ListLabel"
             return;