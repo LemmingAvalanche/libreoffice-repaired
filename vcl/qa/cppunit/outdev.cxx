LemmingAvalanche/libreoffice-repaired

commit b377f8e0979b9c1d186081482da82d60f6584f62

vcl: avoid downscale && upscale in DrawTransformedBitmapEx()
If we rotate a bitmap and put it to a metafile, we'll create a
MetaBmpExScaleAction. But just because we need to store a transformed
bitmap for rotation purposes, it doesn't mean we also need to scale it.

This helps in case later the metafile is upscaled and the downscaled
bitmap would look blurry.

Change-Id: I7d64a88af460e80dffde8052186888eddbb440fe
Reviewed-on: https://gerrit.libreoffice.org/80426
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/outdev/bitmap.cxx
@@ -1208,7 +1208,7 @@ void OutputDevice::DrawTransformedBitmapEx(
     const bool bInvert(RasterOp::Invert == meRasterOp);
     const bool bBitmapChangedColor(mnDrawMode & (DrawModeFlags::BlackBitmap | DrawModeFlags::WhiteBitmap | DrawModeFlags::GrayBitmap ));
     bool bDone(false);
-    const basegfx::B2DHomMatrix aFullTransform(GetViewTransformation() * rTransformation);
+    basegfx::B2DHomMatrix aFullTransform(GetViewTransformation() * rTransformation);
     const bool bTryDirectPaint(!bInvert && !bBitmapChangedColor && !bMetafile );
 
     if(bTryDirectPaint)
@@ -1270,6 +1270,19 @@ void OutputDevice::DrawTransformedBitmapEx(
                 aMaskBmp.Erase(0);
 
                 aTransformed = BitmapEx(aContent, aMaskBmp);
+            }
+
+            // Remove scaling from aFulltransform: we transform due to shearing or rotation, scaling
+            // will happen according to aDestSize.
+            basegfx::B2DVector aFullScale, aFullTranslate;
+            double fFullRotate, fFullShearX;
+            aFullTransform.decompose(aFullScale, aFullTranslate, fFullRotate, fFullShearX);
+            if (aFullScale.getX() != 0 && aFullScale.getY() != 0)
+            {
+                basegfx::B2DHomMatrix aTransform = basegfx::utils::createScaleB2DHomMatrix(
+                    rOriginalSizePixel.getWidth() / aFullScale.getX(),
+                    rOriginalSizePixel.getHeight() / aFullScale.getY());
+                aFullTransform *= aTransform;
             }
 
             aTransformed = aTransformed.getTransformed(

vcl/qa/cppunit/outdev.cxx
@@ -13,6 +13,8 @@
 #include <vcl/virdev.hxx>
 #include <vcl/window.hxx>
 #include <vcl/bitmapaccess.hxx>
+#include <vcl/gdimtf.hxx>
+#include <vcl/metaact.hxx>
 
 #include <basegfx/matrix/b2dhommatrix.hxx>
 
@@ -27,6 +29,7 @@ public:
     void testWindowBackgroundColor();
     void testGetReadableFontColorPrinter();
     void testGetReadableFontColorWindow();
+    void testDrawTransformedBitmapEx();
 
     CPPUNIT_TEST_SUITE(VclOutdevTest);
     CPPUNIT_TEST(testVirtualDevice);
@@ -35,6 +38,7 @@ public:
     CPPUNIT_TEST(testWindowBackgroundColor);
     CPPUNIT_TEST(testGetReadableFontColorPrinter);
     CPPUNIT_TEST(testGetReadableFontColorWindow);
+    CPPUNIT_TEST(testDrawTransformedBitmapEx);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -130,6 +134,34 @@ void VclOutdevTest::testUseAfterDispose()
     pVDev->GetInverseViewTransformation();
 
     pVDev->GetViewTransformation();
+}
+
+void VclOutdevTest::testDrawTransformedBitmapEx()
+{
+    // Create a virtual device, and connect a metafile to it.
+    // Also create a 16x16 bitmap.
+    ScopedVclPtrInstance<VirtualDevice> pVDev;
+    Bitmap aBitmap(Size(16, 16), 24);
+    BitmapEx aBitmapEx(aBitmap);
+    basegfx::B2DHomMatrix aMatrix;
+    aMatrix.scale(8, 8);
+    aMatrix.rotate(M_PI / 2);
+    GDIMetaFile aMtf;
+    aMtf.Record(pVDev.get());
+
+    // Draw the rotated bitmap on the vdev.
+    pVDev->DrawTransformedBitmapEx(aMatrix, aBitmapEx);
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aMtf.GetActionSize());
+    MetaAction* pAction = aMtf.GetAction(0);
+    CPPUNIT_ASSERT_EQUAL(MetaActionType::BMPEXSCALE, pAction->GetType());
+    auto pBitmapAction = static_cast<MetaBmpExScaleAction*>(pAction);
+    const BitmapEx& rBitmapEx = pBitmapAction->GetBitmapEx();
+    Size aTransformedSize = rBitmapEx.GetSizePixel();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 16x16
+    // - Actual  : 8x8
+    // I.e. the bitmap before scaling was already scaled down, just because it was rotated.
+    CPPUNIT_ASSERT_EQUAL(Size(16, 16), aTransformedSize);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(VclOutdevTest);

commit 77bf78e9e8124868628e9fa5fe495cd6aa4679b9

tdf#128629 vcl DrawTransformedBitmapEx: do scaling for flipped bitmaps
Regression from commit b377f8e0979b9c1d186081482da82d60f6584f62 (vcl:
avoid downscale && upscale in DrawTransformedBitmapEx(), 2019-10-08),
the original problem to be solved was that in case you downscale a
bitmap and upscale it later, then you get blurry result, so we try to
avoid touching the pixels and just scale during rendering of the bitmap.

However, here the problem is that scaling is also (mis)used for flip
purposes, so go back to the original behavior for negative scaling.

This keeps the original problem fixed and solves the loss of flip as
well.

Change-Id: Ic9a6eb49d55f2fb8ccf18d982e574398f010cabd
Reviewed-on: https://gerrit.libreoffice.org/83711
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

vcl/source/outdev/bitmap.cxx
@@ -1277,7 +1277,8 @@ void OutputDevice::DrawTransformedBitmapEx(
             basegfx::B2DVector aFullScale, aFullTranslate;
             double fFullRotate, fFullShearX;
             aFullTransform.decompose(aFullScale, aFullTranslate, fFullRotate, fFullShearX);
-            if (aFullScale.getX() != 0 && aFullScale.getY() != 0)
+            // Require positive scaling, negative scaling would loose horizontal or vertical flip.
+            if (aFullScale.getX() > 0 && aFullScale.getY() > 0)
             {
                 basegfx::B2DHomMatrix aTransform = basegfx::utils::createScaleB2DHomMatrix(
                     rOriginalSizePixel.getWidth() / aFullScale.getX(),

vcl/qa/cppunit/outdev.cxx
@@ -31,6 +31,7 @@ public:
     void testGetReadableFontColorPrinter();
     void testGetReadableFontColorWindow();
     void testDrawTransformedBitmapEx();
+    void testDrawTransformedBitmapExFlip();
 
     CPPUNIT_TEST_SUITE(VclOutdevTest);
     CPPUNIT_TEST(testVirtualDevice);
@@ -40,6 +41,7 @@ public:
     CPPUNIT_TEST(testGetReadableFontColorPrinter);
     CPPUNIT_TEST(testGetReadableFontColorWindow);
     CPPUNIT_TEST(testDrawTransformedBitmapEx);
+    CPPUNIT_TEST(testDrawTransformedBitmapExFlip);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -199,6 +201,57 @@ void VclOutdevTest::testDrawTransformedBitmapEx()
             CPPUNIT_ASSERT_EQUAL_MESSAGE(ss.str(), aExpected, Color(aColor));
         }
     }
+}
+
+void VclOutdevTest::testDrawTransformedBitmapExFlip()
+{
+    // Create a virtual device, and connect a metafile to it.
+    // Also create a 16x16 bitmap.
+    ScopedVclPtrInstance<VirtualDevice> pVDev;
+    Bitmap aBitmap(Size(16, 16), 24);
+    {
+        // Fill the top left quarter with black.
+        BitmapScopedWriteAccess pWriteAccess(aBitmap);
+        pWriteAccess->Erase(COL_WHITE);
+        for (int i = 0; i < 8; ++i)
+        {
+            for (int j = 0; j < 8; ++j)
+            {
+                pWriteAccess->SetPixel(j, i, COL_BLACK);
+            }
+        }
+    }
+    BitmapEx aBitmapEx(aBitmap);
+    basegfx::B2DHomMatrix aMatrix;
+    // Negative y scale: bitmap should be upside down, so the black part goes to the bottom left.
+    aMatrix.scale(8, -8);
+    // Rotate 90 degrees clockwise, so the black part goes back to the top left.
+    aMatrix.rotate(M_PI / 2);
+    GDIMetaFile aMtf;
+    aMtf.Record(pVDev.get());
+
+    // Draw the scaled and rotated bitmap on the vdev.
+    pVDev->DrawTransformedBitmapEx(aMatrix, aBitmapEx);
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aMtf.GetActionSize());
+    MetaAction* pAction = aMtf.GetAction(0);
+    CPPUNIT_ASSERT_EQUAL(MetaActionType::BMPEXSCALE, pAction->GetType());
+    auto pBitmapAction = static_cast<MetaBmpExScaleAction*>(pAction);
+    const BitmapEx& rBitmapEx = pBitmapAction->GetBitmapEx();
+
+    aBitmap = rBitmapEx.GetBitmap();
+    Bitmap::ScopedReadAccess pAccess(aBitmap);
+    int nX = 8 * 0.25;
+    int nY = 8 * 0.25;
+    BitmapColor aColor = pAccess->GetPixel(nY, nX);
+    std::stringstream ss;
+    ss << "Color is expected to be black, is " << aColor.AsRGBHexString();
+    ss << " (row " << nY << ", col " << nX << ")";
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: c[00000000]
+    // - Actual  : c[ffffff00]
+    // - Color is expected to be black, is ffffff (row 2, col 2)
+    // i.e. the top left quarter of the image was not black, due to a missing flip.
+    CPPUNIT_ASSERT_EQUAL_MESSAGE(ss.str(), COL_BLACK, Color(aColor));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(VclOutdevTest);