LemmingAvalanche/libreoffice-repaired

commit 40c804dfba67282ea998e41b6311d7bebcd88144

loplugin:constantparam in accessibility to basic
Change-Id: Id77cbcaefebf310a5d99c6ac12d154e182fd0a1c
Reviewed-on: https://gerrit.libreoffice.org/28860
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

basic/source/sbx/sbxbase.cxx
@@ -119,15 +119,7 @@ void SbxBase::AddFactory( SbxFactory* pFac )
 {
     SbxAppData& r = GetSbxData_Impl();
 
-    // From 1996-03-06: take the HandleLast-Flag into account
-    sal_uInt16 nPos = r.m_Factories.size(); // Insert position
-    if( !pFac->IsHandleLast() )         // Only if not self HandleLast
-    {
-        // Rank new factory in front of factories with HandleLast
-        while (nPos > 0 && r.m_Factories[ nPos-1 ]->IsHandleLast())
-            nPos--;
-    }
-    r.m_Factories.insert(r.m_Factories.begin() + nPos, std::unique_ptr<SbxFactory>(pFac));
+    r.m_Factories.insert(r.m_Factories.begin(), std::unique_ptr<SbxFactory>(pFac));
 }
 
 void SbxBase::RemoveFactory( SbxFactory* pFac )

accessibility/inc/extended/AccessibleGridControlTableCell.hxx
@@ -76,8 +76,7 @@ namespace accessibility
         AccessibleGridControlTableCell( const css::uno::Reference< css::accessibility::XAccessible >& _rxParent,
                                     ::svt::table::IAccessibleTable& _rTable,
                                     sal_Int32 _nRowId,
-                                    sal_uInt16 _nColId,
-                                    svt::table::AccessibleTableControlObjType  eObjType);
+                                    sal_uInt16 _nColId);
 
         // XInterface -------------------------------------------------------------
 

accessibility/inc/standard/vclxaccessiblemenuseparator.hxx
@@ -29,7 +29,7 @@
 class VCLXAccessibleMenuSeparator : public OAccessibleMenuItemComponent
 {
 public:
-    VCLXAccessibleMenuSeparator( Menu* pParent, sal_uInt16 nItemPos, Menu* pMenu = nullptr );
+    VCLXAccessibleMenuSeparator( Menu* pParent, sal_uInt16 nItemPos );
     virtual ~VCLXAccessibleMenuSeparator() override;
 
     // XServiceInfo

accessibility/source/extended/AccessibleGridControlTable.cxx
@@ -66,7 +66,7 @@ AccessibleGridControlTable::getAccessibleChild( sal_Int32 nChildIndex )
     }
     if(!m_pAccessCellVector[nChildIndex].is())
     {
-        AccessibleGridControlTableCell* pCell = new AccessibleGridControlTableCell(this, m_aTable, nChildIndex/m_aTable.GetColumnCount(), nChildIndex%m_aTable.GetColumnCount(), TCTYPE_TABLECELL);
+        AccessibleGridControlTableCell* pCell = new AccessibleGridControlTableCell(this, m_aTable, nChildIndex/m_aTable.GetColumnCount(), nChildIndex%m_aTable.GetColumnCount());
         m_pCellVector[nChildIndex] = pCell;
         m_pAccessCellVector[nChildIndex] = pCell;
     }
@@ -99,7 +99,7 @@ AccessibleGridControlTable::getAccessibleAtPoint( const awt::Point& rPoint )
     sal_Int32 nRow = 0;
     sal_Int32 nColumnPos = 0;
     if( m_aTable.ConvertPointToCellAddress( nRow, nColumnPos, VCLPoint( rPoint ) ) )
-        xChild = new AccessibleGridControlTableCell(this, m_aTable, nRow, nColumnPos, TCTYPE_TABLECELL);
+        xChild = new AccessibleGridControlTableCell(this, m_aTable, nRow, nColumnPos);
     return xChild;
 }
 
@@ -219,7 +219,7 @@ Reference< XAccessible > SAL_CALL AccessibleGridControlTable::getAccessibleCellA
     }
     if(!m_pAccessCellVector[nChildIndex].is())
     {
-        AccessibleGridControlTableCell* pCell = new AccessibleGridControlTableCell(this, m_aTable, nRow, nColumn, TCTYPE_TABLECELL);
+        AccessibleGridControlTableCell* pCell = new AccessibleGridControlTableCell(this, m_aTable, nRow, nColumn);
         m_pCellVector[nChildIndex] = pCell;
         m_pAccessCellVector[nChildIndex] = pCell;
     }

accessibility/source/extended/AccessibleGridControlTableCell.cxx
@@ -103,9 +103,8 @@ namespace accessibility
     AccessibleGridControlTableCell::AccessibleGridControlTableCell(const css::uno::Reference<XAccessible >& _rxParent,
                                 ::svt::table::IAccessibleTable& _rTable,
                                 sal_Int32 _nRowPos,
-                                sal_uInt16 _nColPos,
-                                ::svt::table::AccessibleTableControlObjType  eObjType)
-        :AccessibleGridControlCell( _rxParent, _rTable, _nRowPos, _nColPos, eObjType )
+                                sal_uInt16 _nColPos)
+        :AccessibleGridControlCell( _rxParent, _rTable, _nRowPos, _nColPos, TCTYPE_TABLECELL )
     {
     }
 

accessibility/source/standard/vclxaccessiblemenuseparator.cxx
@@ -31,8 +31,8 @@ using namespace ::comphelper;
 // class VCLXAccessibleMenuSeparator
 
 
-VCLXAccessibleMenuSeparator::VCLXAccessibleMenuSeparator( Menu* pParent, sal_uInt16 nItemPos, Menu* pMenu )
-    :OAccessibleMenuItemComponent( pParent, nItemPos, pMenu )
+VCLXAccessibleMenuSeparator::VCLXAccessibleMenuSeparator( Menu* pParent, sal_uInt16 nItemPos )
+    :OAccessibleMenuItemComponent( pParent, nItemPos, nullptr )
 {
 }
 

include/basic/sbxfac.hxx
@@ -29,11 +29,9 @@ class SbxObject;
 
 class BASIC_DLLPUBLIC SbxFactory
 {
-    bool bHandleLast;   // true: Factory is asked at last because of its expensiveness
 public:
     virtual ~SbxFactory();
-    SbxFactory( bool bLast=false )  { bHandleLast = bLast; }
-    bool IsHandleLast()       { return bHandleLast; }
+    SbxFactory()  {}
     virtual SbxBase* Create( sal_uInt16 nSbxId, sal_uInt32 );
     virtual SbxObject* CreateObject( const OUString& );
 };

commit dc866dbb206919f2b0685ce29d6dc600850c6f56

tdf#129227: this was always appending, not prepending
Regression after 40c804dfba67282ea998e41b6311d7bebcd88144
The change replaced

    // From 1996-03-06: take the HandleLast-Flag into account
    sal_uInt16 nPos = r.m_Factories.size(); // Insert position
    if( !pFac->IsHandleLast() )         // Only if not self HandleLast
    {
        // Rank new factory in front of factories with HandleLast
        while (nPos > 0 && r.m_Factories[ nPos-1 ]->IsHandleLast())
            nPos--;
    }
    r.m_Factories.insert(r.m_Factories.begin() + nPos, std::unique_ptr<SbxFactory>(pFac));

with

    r.m_Factories.insert(r.m_Factories.begin(), std::unique_ptr<SbxFactory>(pFac));

Before that commit condition in while was always false, so decrementing
nPos had never happened, and insertion to the end was always performed.
This change restores that.

Change-Id: I778d6fdc93e9078a541a9b98c9432b5cf88d9791
Reviewed-on: https://gerrit.libreoffice.org/84609
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Mike Kaganski <mike.kaganski@collabora.com>

basic/source/sbx/sbxbase.cxx
@@ -114,9 +114,7 @@ void SbxBase::ResetError()
 
 void SbxBase::AddFactory( SbxFactory* pFac )
 {
-    SbxAppData& r = GetSbxData_Impl();
-
-    r.m_Factories.insert(r.m_Factories.begin(), std::unique_ptr<SbxFactory>(pFac));
+    GetSbxData_Impl().m_Factories.emplace_back(pFac);
 }
 
 void SbxBase::RemoveFactory( SbxFactory const * pFac )