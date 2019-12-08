LemmingAvalanche/libreoffice-repaired

commit 5890827798e88ece17eae62af8767c117fa19739

handle w:gridBefore by faking cells (fdo#38414)
Docx's w:gridBefore means that there should be this given space in the table
grid before any cells come. But writer requires tables to be rectangular, so
the space needs to be faked using cells without border. So far so good, but
now reality in the form of the retarded overdesigned writerfilter comes.
The internal representation of table data (and not just one actually) is
pretty non-obvious and hard to modify, seems to be modelled just to follow
the parser data the way it comes. Moreover dmapper gets notified of w:gridBefore
only after cells in the row have been already processed. So after futile attempts
to add the fake cells somehow in dmapper I've eventually given up and hacked up
input handling to fake input as if the fake cells were actually there (which
was tedious to find out as well, but at least it's reasonably doable).

Change-Id: I7107e13f28dd3f7093688782f64238167cead76f

writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -1966,6 +1966,85 @@ void OOXMLFastContextHandlerTextTableRow::endRow()
     endParagraphGroup();
 }
 
+// Handle w:gridBefore here by faking necessary input that'll fake cells. I'm apparently
+// not insane enough to find out how to add cells in dmapper.
+void OOXMLFastContextHandlerTextTableRow::handleGridBefore( OOXMLValue::Pointer_t val )
+{
+    int count = val->getInt();
+    for( int i = 0;
+         i < count;
+         ++i )
+    {
+        endOfParagraph();
+
+        if (isForwardEvents())
+        {
+            // This whole part is OOXMLFastContextHandlerTextTableCell::endCell() .
+            OOXMLPropertySet * pProps = new OOXMLPropertySetImpl();
+            {
+                OOXMLValue::Pointer_t pVal
+                    (new OOXMLIntegerValue(mnTableDepth));
+                OOXMLProperty::Pointer_t pProp
+                    (new OOXMLPropertyImpl(NS_ooxml::LN_tblDepth, pVal, OOXMLPropertyImpl::SPRM));
+                pProps->add(pProp);
+            }
+            {
+                OOXMLValue::Pointer_t pVal
+                    (new OOXMLIntegerValue(1));
+                OOXMLProperty::Pointer_t pProp
+                    (new OOXMLPropertyImpl(NS_ooxml::LN_inTbl, pVal, OOXMLPropertyImpl::SPRM));
+                pProps->add(pProp);
+            }
+            {
+                OOXMLValue::Pointer_t pVal
+                    (new OOXMLBooleanValue(mnTableDepth > 0));
+                OOXMLProperty::Pointer_t pProp
+                    (new OOXMLPropertyImpl(NS_ooxml::LN_tblCell, pVal, OOXMLPropertyImpl::SPRM));
+                pProps->add(pProp);
+            }
+
+    #ifdef DEBUG_PROPERTIES
+            debug_logger->startElement("handlegridbefore");
+            debug_logger->propertySet(OOXMLPropertySet::Pointer_t(pProps->clone()),
+                    IdToString::Pointer_t(new OOXMLIdToString()));
+            debug_logger->endElement();
+    #endif
+            mpStream->props(writerfilter::Reference<Properties>::Pointer_t(pProps));
+
+            // fake <w:tcBorders> with no border
+            OOXMLPropertySet::Pointer_t pCellProps( new OOXMLPropertySetImpl());
+            {
+                OOXMLPropertySet::Pointer_t pBorderProps( new OOXMLPropertySetImpl());
+                static Id borders[] = { NS_ooxml::LN_CT_TcBorders_top, NS_ooxml::LN_CT_TcBorders_bottom,
+                    NS_ooxml::LN_CT_TcBorders_start, NS_ooxml::LN_CT_TcBorders_end };
+                for( size_t j = 0; j < SAL_N_ELEMENTS( borders ); ++j )
+                    pBorderProps->add( fakeNoBorder( borders[ j ] ));
+                OOXMLValue::Pointer_t pValue( new OOXMLPropertySetValue( pBorderProps ));
+                OOXMLProperty::Pointer_t pProp
+                    (new OOXMLPropertyImpl(NS_ooxml::LN_CT_TcPrBase_tcBorders, pValue, OOXMLPropertyImpl::SPRM));
+                pCellProps->add(pProp);
+                mpParserState->setCellProperties(pCellProps);
+            }
+        }
+
+        sendCellProperties();
+        endParagraphGroup();
+    }
+}
+
+OOXMLProperty::Pointer_t OOXMLFastContextHandlerTextTableRow::fakeNoBorder( Id id )
+{
+    OOXMLPropertySet::Pointer_t pProps( new OOXMLPropertySetImpl());
+    OOXMLValue::Pointer_t pVal(new OOXMLIntegerValue(0));
+    OOXMLProperty::Pointer_t pPropVal
+        (new OOXMLPropertyImpl(NS_ooxml::LN_CT_Border_val, pVal, OOXMLPropertyImpl::ATTRIBUTE));
+    pProps->add(pPropVal);
+    OOXMLValue::Pointer_t pValue( new OOXMLPropertySetValue( pProps ));
+    OOXMLProperty::Pointer_t pProp
+        (new OOXMLPropertyImpl(id, pValue, OOXMLPropertyImpl::SPRM));
+    return pProp;
+}
+
 /*
   class OOXMLFastContextHandlerTextTable
  */

writerfilter/source/ooxml/model.xml
@@ -19563,7 +19563,7 @@
             </optional>
             <optional>
               <element name="gridBefore">
-                <ref name="CT_DecimalNumber"/>
+                <ref name="CT_TrPrBaseGridBefore"/>
               </element>
             </optional>
             <optional>
@@ -19613,6 +19613,11 @@
             </optional>
           </choice>
         </oneOrMore>
+      </define>
+      <define name="CT_TrPrBaseGridBefore">
+        <attribute name="val">
+          <ref name="ST_DecimalNumber"/>
+        </attribute>
       </define>
       <define name="CT_TrPr">
         <ref name="CT_TrPrBase"/>
@@ -24766,7 +24771,7 @@
       <kind name="table"/>
       <element name="cnfStyle" tokenid="ooxml:CT_TrPrBase_cnfStyle"/>
       <element name="divId" tokenid="ooxml:CT_TrPrBase_divId"/>
-      <element name="gridBefore" tokenid="ooxml:CT_TrPrBase_gridBefore"/>
+<!--      <element name="gridBefore" tokenid="ooxml:CT_TrPrBase_gridBefore"/> -->
       <element name="gridAfter" tokenid="ooxml:CT_TrPrBase_gridAfter"/>
       <element name="wBefore" tokenid="ooxml:CT_TrPrBase_wBefore"/>
       <element name="wAfter" tokenid="ooxml:CT_TrPrBase_wAfter"/>
@@ -24776,6 +24781,9 @@
       <element name="tblCellSpacing" tokenid="ooxml:CT_TrPrBase_tblCellSpacing"/>
       <element name="jc" tokenid="ooxml:CT_TrPrBase_jc"/>
       <element name="hidden" tokenid="ooxml:CT_TrPrBase_hidden"/>
+    </resource>
+    <resource name="CT_TrPrBaseGridBefore" resource="TextTableRow">
+      <attribute name="val" tokenid="ooxml:CT_TrPrBase_gridBefore" action="handleGridBefore"/>
     </resource>
     <resource name="CT_TrPr" resource="Properties" tag="table">
       <kind name="table"/>

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2065,6 +2065,18 @@ DECLARE_OOXMLIMPORT_TEST(testFdo55381, "fdo55381.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int16(4), xCursor->getPage());
 }
 
+DECLARE_OOXMLIMPORT_TEST(testGridBefore, "gridbefore.docx")
+{
+    // w:gridBefore is faked by inserting two cells without border (because Writer can't do non-rectangular tables).
+    // So check the first cell in the first row is in fact 3rd and that it's more to the right than the second
+    // cell on the second row.
+    OUString textA3 = parseDump("/root/page/body/tab/row[1]/cell[1]/txt/text()" );
+    OUString leftA3 = parseDump("/root/page/body/tab/row[1]/cell[1]/infos/bounds", "left" );
+    OUString leftB2 = parseDump("/root/page/body/tab/row[2]/cell[2]/infos/bounds", "left" );
+    CPPUNIT_ASSERT_EQUAL( OUString( "A3" ), textA3 );
+    CPPUNIT_ASSERT( leftA3.toInt32() > leftB2.toInt32());
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

writerfilter/source/ooxml/OOXMLFastContextHandler.hxx
@@ -491,6 +491,9 @@ public:
 
     void startRow();
     void endRow();
+    void handleGridBefore( OOXMLValue::Pointer_t val );
+private:
+    OOXMLProperty::Pointer_t fakeNoBorder( Id id );
 };
 
 class OOXMLFastContextHandlerTextTable : public OOXMLFastContextHandler

writerfilter/source/ooxml/factoryimpl_ns.xsl
@@ -476,6 +476,10 @@ CreateElementMapPointer </xsl:text>
             <xsl:value-of select="@action"/>
             <xsl:text>();</xsl:text>
         </xsl:when>
+        <xsl:when test="@action='handleGridBefore'">
+            <xsl:text>
+    dynamic_cast&lt;OOXMLFastContextHandlerTextTableRow*&gt;(pHandler)-&gt;handleGridBefore();</xsl:text>
+        </xsl:when>
         <xsl:when test="@action='sendProperty' or @action='handleHyperlink'">
             <xsl:text>
     dynamic_cast&lt;OOXMLFastContextHandlerStream*&gt;(pHandler)-&gt;</xsl:text>

commit 32f3e31bf5e9cbae85cfd9e46cb818c73dc63e33

tdf#116194 DOCX import: fix missing tables with w:gridBefore
Regression from the commit 5890827798e88ece17eae62af8767c117fa19739
"handle w:gridBefore by faking cells (fdo#38414)"

This patch replaces the previous fix with a better solution,
fixing tdf#38414 on the proposed DomainMapper level. (Note:
to reject the old fix completely, its follow-up commit w:gridAfter
will be handled in a similar way.)

Now the related regressions, tdf#111679, tdf#120512 and the complex
forms of tdf#116194, tdf120256 and tdf#122608 are fixed, too.

Change-Id: Id25f5fb4d9021c87ee8c82782b2038e6fb255673
Reviewed-on: https://gerrit.libreoffice.org/84263
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapperTableManager.cxx
@@ -47,7 +47,7 @@ DomainMapperTableManager::DomainMapperTableManager() :
     m_nRow(0),
     m_nCell(),
     m_nGridSpan(1),
-    m_nGridBefore(0),
+    m_aGridBefore(),
     m_nGridAfter(0),
     m_nHeaderRepeat(0),
     m_nTableWidth(0),
@@ -354,7 +354,7 @@ bool DomainMapperTableManager::sprm(Sprm & rSprm)
                 }
                 break;
             case NS_ooxml::LN_CT_TrPrBase_gridBefore:
-                m_nGridBefore = nIntValue;
+                m_aGridBefore.back( ) = nIntValue;
                 break;
             case NS_ooxml::LN_CT_TrPrBase_gridAfter:
                 m_nGridAfter = nIntValue;
@@ -392,6 +392,11 @@ bool DomainMapperTableManager::sprm(Sprm & rSprm)
 DomainMapperTableManager::IntVectorPtr const & DomainMapperTableManager::getCurrentGrid( )
 {
     return m_aTableGrid.back( );
+}
+
+sal_uInt32 DomainMapperTableManager::getCurrentGridBefore( )
+{
+    return m_aGridBefore.back( );
 }
 
 bool DomainMapperTableManager::hasCurrentSpans() const
@@ -456,6 +461,7 @@ void DomainMapperTableManager::startLevel( )
     m_aTmpPosition.push_back( pTmpPosition );
     m_aTmpTableProperties.push_back( pTmpProperties );
     m_nCell.push_back( 0 );
+    m_aGridBefore.push_back( 0 );
     m_nTableWidth = 0;
     m_nLayoutType = 0;
 
@@ -485,6 +491,7 @@ void DomainMapperTableManager::endLevel( )
         m_aCellWidths.back()->push_back(*oCurrentWidth);
 
     m_nCell.pop_back( );
+    m_aGridBefore.pop_back( );
     m_nTableWidth = 0;
     m_nLayoutType = 0;
 
@@ -540,6 +547,7 @@ void DomainMapperTableManager::endOfRowAction()
         IntVectorPtr pTmpGridSpans = m_aGridSpans.back();
         IntVectorPtr pTmpCellWidths = m_aCellWidths.back();
         sal_uInt32 nTmpCell = m_nCell.back();
+        sal_uInt32 nTmpGridBefore = m_aGridBefore.back();
 
         // endLevel and startLevel are taking care of the non finished row
         // to carry it over to the next table
@@ -552,10 +560,12 @@ void DomainMapperTableManager::endOfRowAction()
         m_aGridSpans.pop_back();
         m_aCellWidths.pop_back();
         m_nCell.pop_back();
+        m_aGridBefore.pop_back();
         m_aTableGrid.push_back(pTmpTableGrid);
         m_aGridSpans.push_back(pTmpGridSpans);
         m_aCellWidths.push_back(pTmpCellWidths);
         m_nCell.push_back(nTmpCell);
+        m_aGridBefore.push_back(nTmpGridBefore);
     }
 
     // Push the tmp position now that we compared it
@@ -596,10 +606,16 @@ void DomainMapperTableManager::endOfRowAction()
     }
 
     IntVectorPtr pCurrentSpans = getCurrentSpans( );
-    if( pCurrentSpans->size() < m_nCell.back( ) )
+
+    if( m_aGridBefore.back() > 0 )
+    {
+        //fill missing gridBefore elements with '1'
+        pCurrentSpans->insert( pCurrentSpans->begin( ), m_aGridBefore.back(), 1 );
+    }
+    if( pCurrentSpans->size() < m_aGridBefore.back() + m_nCell.back( ))
     {
         //fill missing elements with '1'
-        pCurrentSpans->insert( pCurrentSpans->end( ), m_nCell.back( ) - pCurrentSpans->size(), 1 );
+        pCurrentSpans->insert( pCurrentSpans->end( ), m_aGridBefore.back() + m_nCell.back( ) - pCurrentSpans->size(), 1 );
     }
 
 #ifdef DBG_UTIL
@@ -626,7 +642,7 @@ void DomainMapperTableManager::endOfRowAction()
     for (int i : (*pTableGrid))
         nFullWidthRelative = o3tl::saturating_add(nFullWidthRelative, i);
 
-    if( pTableGrid->size() == ( m_nGridBefore + nGrids + m_nGridAfter ) && m_nCell.back( ) > 0 )
+    if( pTableGrid->size() == ( nGrids + m_nGridAfter ) && m_nCell.back( ) > 0 )
     {
         /*
          * If table width property set earlier is smaller than the current table width,
@@ -656,12 +672,12 @@ void DomainMapperTableManager::endOfRowAction()
                 }
             }
         }
-        uno::Sequence< text::TableColumnSeparator > aSeparators( m_nCell.back( ) - 1 );
+        uno::Sequence< text::TableColumnSeparator > aSeparators( m_aGridBefore.back() + m_nCell.back( ) - 1 );
         text::TableColumnSeparator* pSeparators = aSeparators.getArray();
         double nLastRelPos = 0.0;
-        sal_uInt32 nBorderGridIndex = m_nGridBefore;
+        sal_uInt32 nBorderGridIndex = 0;
 
-        size_t nWidthsBound =  m_nCell.back( ) - 1;
+        size_t nWidthsBound = m_aGridBefore.back() + m_nCell.back() - 1;
         if (nWidthsBound)
         {
             if (nFullWidthRelative == 0)
@@ -694,7 +710,7 @@ void DomainMapperTableManager::endOfRowAction()
     }
     else if ( !pCellWidths->empty() &&
                ( m_nLayoutType == NS_ooxml::LN_Value_doc_ST_TblLayout_fixed
-                 || pCellWidths->size() == ( m_nGridBefore + nGrids + m_nGridAfter ) )
+                 || pCellWidths->size() == ( nGrids + m_nGridAfter ) )
              )
     {
         // If we're here, then the number of cells does not equal to the amount
@@ -748,11 +764,12 @@ void DomainMapperTableManager::endOfRowAction()
 
     ++m_nRow;
     m_nCell.back( ) = 0;
+    m_aGridBefore.back( ) = 0;
     getCurrentGrid()->clear();
     pCurrentSpans->clear();
     pCellWidths->clear();
 
-    m_nGridBefore = m_nGridAfter = 0;
+    m_nGridAfter = 0;
     m_bTableSizeTypeInserted = false;
 
 #ifdef DBG_UTIL

writerfilter/source/dmapper/DomainMapperTableManager.hxx
@@ -42,7 +42,7 @@ class DomainMapperTableManager : public TableManager
     sal_uInt32      m_nRow;
     ::std::vector< sal_uInt32 > m_nCell;
     sal_uInt32      m_nGridSpan;
-    sal_uInt32      m_nGridBefore; ///< number of grid columns in the parent table's table grid which must be skipped before the contents of this table row are added to the parent table
+    ::std::vector< sal_uInt32 > m_aGridBefore; ///< number of grid columns in the parent table's table grid which must be skipped before the contents of this table row are added to the parent table
     sal_uInt32      m_nGridAfter; ///< number of grid columns in the parent table's table grid which shall be left after the last cell in the table row
     sal_Int32       m_nHeaderRepeat; //counter of repeated headers - if == -1 then the repeating stops
     sal_Int32       m_nTableWidth; //might be set directly or has to be calculated from the column positions
@@ -93,6 +93,7 @@ public:
     bool hasCurrentSpans() const;
     IntVectorPtr const & getCurrentSpans( );
     IntVectorPtr const & getCurrentCellWidths( );
+    sal_uInt32 getCurrentGridBefore( );
 
     /// Turn the attributes collected so far in m_aTableLook into a property and clear the container.
     void finishTableLook();

writerfilter/source/dmapper/TableData.hxx
@@ -131,11 +131,18 @@ public:
        @param start     the start handle of the cell
        @param end       the end handle of the cell
        @param pProps    the properties of the cell
+       @param bAddBefore true: add an empty cell at beginning of the row for gridBefore
      */
-    void addCell(const css::uno::Reference<css::text::XTextRange>& start, TablePropertyMapPtr pProps)
+    void addCell(const css::uno::Reference<css::text::XTextRange>& start, TablePropertyMapPtr pProps, bool bAddBefore = false)
     {
         CellData::Pointer_t pCellData(new CellData(start, pProps));
-        mCells.push_back(pCellData);
+        if (bAddBefore)
+        {
+            mCells.insert(mCells.begin(), pCellData);
+            mCells[0]->setEnd(start);
+        }
+        else
+            mCells.push_back(pCellData);
     }
 
     void endCell(const css::uno::Reference<css::text::XTextRange>& end)

writerfilter/source/dmapper/TableManager.cxx
@@ -407,6 +407,26 @@ void TableManager::endRow()
 #ifdef DBG_UTIL
     TagLogger::getInstance().element("tablemanager.endRow");
 #endif
+    TableData::Pointer_t pTableData = mTableDataStack.top();
+
+    // Add borderless w:gridBefore cell(s) to the row
+    if (pTableData)
+    {
+        sal_uInt32 nGridBefore = mpTableDataHandler->getDomainMapperImpl().getTableManager().getCurrentGridBefore();
+        for (unsigned int i = 0; i < nGridBefore; ++i)
+        {
+            css::table::BorderLine2 aBorderLine;
+            aBorderLine.Color = 0;
+            aBorderLine.InnerLineWidth = 0;
+            aBorderLine.OuterLineWidth = 0;
+            TablePropertyMapPtr pCellProperties(new TablePropertyMap);
+            pCellProperties->Insert(PROP_TOP_BORDER, css::uno::makeAny(aBorderLine));
+            pCellProperties->Insert(PROP_LEFT_BORDER, css::uno::makeAny(aBorderLine));
+            pCellProperties->Insert(PROP_BOTTOM_BORDER, css::uno::makeAny(aBorderLine));
+            pCellProperties->Insert(PROP_RIGHT_BORDER, css::uno::makeAny(aBorderLine));
+            pTableData->getCurrentRow()->addCell(pTableData->getCurrentRow()->getCellStart(0), pCellProperties, /*bAddBefore=*/true);
+        }
+    }
 
     setRowEnd(true);
 }

writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -1461,6 +1461,10 @@ OOXMLValue::Pointer_t fakeNoBorder()
 // not insane enough to find out how to add cells in dmapper.
 void OOXMLFastContextHandlerTextTableRow::handleGridBefore( const OOXMLValue::Pointer_t& val )
 {
+    // start removing: disable for w:gridBefore
+    if (!mpGridAfter)
+        return;
+
     int count = val->getInt();
     for( int i = 0;
          i < count;

writerfilter/source/ooxml/model.xml
@@ -14442,7 +14442,7 @@
             <ref name="CT_DecimalNumber"/>
           </element>
           <element name="gridBefore">
-            <ref name="CT_TrPrBaseGridBefore"/>
+            <ref name="CT_DecimalNumber"/>
           </element>
           <element name="gridAfter">
             <ref name="CT_TrPrBaseGridAfter"/>
@@ -18404,6 +18404,7 @@
     <resource name="CT_TrPrBase" resource="Properties">
       <element name="cnfStyle" tokenid="ooxml:CT_TrPrBase_cnfStyle"/>
       <element name="divId" tokenid="ooxml:CT_TrPrBase_divId"/>
+      <element name="gridBefore" tokenid="ooxml:CT_TrPrBase_gridBefore"/>
       <element name="wBefore" tokenid="ooxml:CT_TrPrBase_wBefore"/>
       <element name="wAfter" tokenid="ooxml:CT_TrPrBase_wAfter"/>
       <element name="cantSplit" tokenid="ooxml:CT_TrPrBase_cantSplit"/>
@@ -18414,7 +18415,7 @@
       <element name="hidden" tokenid="ooxml:CT_TrPrBase_hidden"/>
     </resource>
     <resource name="CT_TrPrBaseGridBefore" resource="TextTableRow">
-      <attribute name="val" tokenid="ooxml:CT_TrPrBase_gridBefore" action="handleGridBefore"/>
+      <attribute name="val" tokenid="ooxml:CT_TrPrBase_gridBefore"/>
     </resource>
     <resource name="CT_TrPrBaseGridAfter" resource="TextTableRow">
       <attribute name="val" tokenid="ooxml:CT_TrPrBase_gridAfter" action="handleGridAfter"/>

sw/qa/extras/ooxmlexport/ooxmlexport10.cxx
@@ -595,6 +595,15 @@ DECLARE_OOXMLEXPORT_TEST(testGridBefore, "gridbefore.docx")
     CPPUNIT_ASSERT( leftA3.toInt32() > leftB2.toInt32());
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf116194, "tdf116194.docx")
+{
+    // The problem was that the importer lost consecutive tables with w:gridBefore
+    xmlDocPtr pXmlDoc = parseExport();
+    if (!pXmlDoc)
+        return;
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl", 2);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testMsoBrightnessContrast, "msobrightnesscontrast.docx")
 {
     uno::Reference<text::XTextDocument> textDocument(mxComponent, uno::UNO_QUERY);