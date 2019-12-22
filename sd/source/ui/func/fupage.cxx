LemmingAvalanche/libreoffice-repaired

commit 250ceb8e3575d3f1d1dc81f34b80b2d486b5225b

sd: support inserting background image from file via .uno:SelectBackground
The UNO command always prompted the user via file open dialog.
This change allows for passing the filename as an argument
to allow for inserting slide background programatically.

Also, hide the Insert Image button in the sidebar
since we can't use that (just yet), because
it invokes .uno:SelectBackground directly. We would
need to send LOOL a notification to invoke this on
its own end to first prompt the user for a file.

(cherry picked from commit 2118143bdd246921439ba9e835207585203dd45f)

Change-Id: I20c0e33d66f8bcd72a6388e39c4ac92e64978f45
Reviewed-on: https://gerrit.libreoffice.org/73481
Tested-by: Jenkins
Reviewed-by: Jan Holesovsky <kendy@collabora.com>

sd/source/ui/func/fupage.cxx
@@ -26,6 +26,7 @@
 #include <svl/itempool.hxx>
 #include <sfx2/request.hxx>
 #include <vcl/prntypes.hxx>
+#include <vcl/graphicfilter.hxx>
 #include <stlsheet.hxx>
 #include <editeng/eeitem.hxx>
 #include <editeng/frmdiritem.hxx>
@@ -111,7 +112,7 @@ rtl::Reference<FuPoor> FuPage::Create( ViewShell* pViewSh, ::sd::Window* pWin, :
     return xFunc;
 }
 
-void FuPage::DoExecute( SfxRequest& )
+void FuPage::DoExecute(SfxRequest& rReq)
 {
     mpDrawViewShell = dynamic_cast<DrawViewShell*>(mpViewShell);
     DBG_ASSERT( mpDrawViewShell, "sd::FuPage::FuPage(), called without a current DrawViewShell!" );
@@ -129,10 +130,11 @@ void FuPage::DoExecute( SfxRequest& )
         return;
 
     // if there are no arguments given, open the dialog
-    if( !mpArgs )
+    const SfxPoolItem* pItem;
+    if (!mpArgs || mpArgs->GetItemState(SID_SELECT_BACKGROUND, true, &pItem) == SfxItemState::SET)
     {
         mpView->SdrEndTextEdit();
-        mpArgs = ExecuteDialog(mpWindow ? mpWindow->GetFrameWeld() : nullptr);
+        mpArgs = ExecuteDialog(mpWindow ? mpWindow->GetFrameWeld() : nullptr, rReq);
     }
 
     // if we now have arguments, apply them to current page
@@ -189,7 +191,7 @@ void MergePageBackgroundFilling(SdPage *pPage, SdStyleSheet *pStyleSheet, bool b
     }
 }
 
-const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent)
+const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent, SfxRequest& rReq)
 {
     if (!mpDrawViewShell)
         return nullptr;
@@ -288,26 +290,50 @@ const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent)
     }
     else if (nId == SID_SELECT_BACKGROUND)
     {
-        SvxOpenGraphicDialog aDlg(SdResId(STR_SET_BACKGROUND_PICTURE), pParent);
+        OUString aFileName;
+        OUString aFilterName;
+        Graphic aGraphic;
+        ErrCode nError = ERRCODE_GRFILTER_OPENERROR;
 
-        if( aDlg.Execute() == ERRCODE_NONE )
+        const SfxItemSet* pArgs = rReq.GetArgs();
+        const SfxPoolItem* pItem;
+
+        if (pArgs && pArgs->GetItemState(SID_SELECT_BACKGROUND, true, &pItem) == SfxItemState::SET)
         {
-            Graphic     aGraphic;
-            ErrCode nError = aDlg.GetGraphic(aGraphic);
-            if( nError == ERRCODE_NONE )
+            aFileName = static_cast<const SfxStringItem*>(pItem)->GetValue();
+
+            if (pArgs->GetItemState(FN_PARAM_FILTER, true, &pItem) == SfxItemState::SET)
+                aFilterName = static_cast<const SfxStringItem*>(pItem)->GetValue();
+
+            nError = GraphicFilter::LoadGraphic(aFileName, aFilterName, aGraphic,
+                                                &GraphicFilter::GetGraphicFilter());
+        }
+        else
+        {
+            SvxOpenGraphicDialog aDlg(SdResId(STR_SET_BACKGROUND_PICTURE), pParent);
+
+            nError = aDlg.Execute();
+            if (nError != ERRCODE_NONE)
             {
-                pTempSet.reset( new SfxItemSet( mpDoc->GetPool(), svl::Items<XATTR_FILL_FIRST, XATTR_FILL_LAST>{}) );
-
-                pTempSet->Put( XFillStyleItem( drawing::FillStyle_BITMAP ) );
-
-                // MigrateItemSet makes sure the XFillBitmapItem will have a unique name
-                SfxItemSet aMigrateSet( mpDoc->GetPool(), svl::Items<XATTR_FILLBITMAP, XATTR_FILLBITMAP>{} );
-                aMigrateSet.Put(XFillBitmapItem("background", aGraphic));
-                SdrModel::MigrateItemSet( &aMigrateSet, pTempSet.get(), mpDoc );
-
-                pTempSet->Put( XFillBmpStretchItem( true ));
-                pTempSet->Put( XFillBmpTileItem( false ));
+                nError = aDlg.GetGraphic(aGraphic);
+                aFileName = aDlg.GetPath();
+                aFilterName = aDlg.GetDetectedFilter();
             }
+        }
+
+        if (nError == ERRCODE_NONE)
+        {
+            pTempSet.reset( new SfxItemSet( mpDoc->GetPool(), svl::Items<XATTR_FILL_FIRST, XATTR_FILL_LAST>{}) );
+
+            pTempSet->Put( XFillStyleItem( drawing::FillStyle_BITMAP ) );
+
+            // MigrateItemSet makes sure the XFillBitmapItem will have a unique name
+            SfxItemSet aMigrateSet( mpDoc->GetPool(), svl::Items<XATTR_FILLBITMAP, XATTR_FILLBITMAP>{} );
+            aMigrateSet.Put(XFillBitmapItem("background", aGraphic));
+            SdrModel::MigrateItemSet( &aMigrateSet, pTempSet.get(), mpDoc );
+
+            pTempSet->Put( XFillBmpStretchItem( true ));
+            pTempSet->Put( XFillBmpTileItem( false ));
         }
     }
 

sd/sdi/sdraw.sdi
@@ -4025,7 +4025,7 @@ SfxVoidItem CloseMasterView SID_CLOSE_MASTER_VIEW()
 ]
 
 SfxVoidItem SelectBackground SID_SELECT_BACKGROUND
-()
+(SfxStringItem FileName SID_SELECT_BACKGROUND,SfxStringItem FilterName FN_PARAM_FILTER,SfxBoolItem AsLink FN_PARAM_1,SfxStringItem Style FN_PARAM_2)
 [
     AutoUpdate = FALSE,
     FastCall = FALSE,

sd/source/ui/inc/fupage.hxx
@@ -42,7 +42,7 @@ class FuPage
     virtual void Activate() override;
     virtual void Deactivate() override;
 
-    const SfxItemSet* ExecuteDialog(weld::Window* pParent);
+    const SfxItemSet* ExecuteDialog(weld::Window* pParent, SfxRequest& rReq);
 
 protected:
     virtual ~FuPage() override;

sd/source/ui/sidebar/MasterPageDescriptor.cxx
@@ -207,6 +207,7 @@ bool MasterPageDescriptor::UpdatePreview (
         {
             pPage = mpMasterPage;
         }
+        //TODO: Notify LOOL of preview updates.
         maLargePreview = (*mpPreviewProvider)(
             rLargeSize.Width(),
             pPage,

sd/source/ui/sidebar/SlideBackground.cxx
@@ -298,6 +298,14 @@ void SlideBackground::HandleContextChange(
             mpInsertImage->Show();
         }
 
+        // The Insert Image button in the sidebar issues .uno:SelectBackground,
+        // which when invoked without arguments will open the file-open-dialog
+        // to prompt the user to select a file. This is useless in LOOL.
+        // Hide for now so the user will only be able to use the menu to insert
+        // background image, which prompts the user for file selection in the browser.
+        if (comphelper::LibreOfficeKit::isActive())
+            mpInsertImage->Hide();
+
         // Need to do a relayouting, otherwise the panel size is not updated after show / hide controls
         sfx2::sidebar::Panel* pPanel = dynamic_cast<sfx2::sidebar::Panel*>(GetParent());
         if(pPanel)
@@ -864,7 +872,7 @@ void SlideBackground::NotifyItemUpdate(
             if (pSizeItem)
             {
                 Size aPaperSize = pSizeItem->GetSize();
-                if(mpPaperOrientation->GetSelectedEntryPos() == 0)
+                if (mpPaperOrientation->GetSelectedEntryPos() == 0)
                    Swap(aPaperSize);
 
                 Paper ePaper = SvxPaperInfo::GetSvxPaper(aPaperSize, meUnit);

commit 8de73f2d1f859ba6bfb3b0549792802bf41a8069

tdf#128449 Set graphic when setting background in Impress
Regression from 250ceb8e3575d3f1d1dc81f34b80b2d486b5225b

Change-Id: If0510967a339b6b46da2b7f798f478a46bd10b48
Reviewed-on: https://gerrit.libreoffice.org/85688
Tested-by: Jenkins
Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>

sd/source/ui/func/fupage.cxx
@@ -293,8 +293,6 @@ const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent, const SfxRequest&
     }
     else if (nId == SID_SELECT_BACKGROUND)
     {
-        OUString aFileName;
-        OUString aFilterName;
         Graphic aGraphic;
         ErrCode nError = ERRCODE_GRFILTER_OPENERROR;
 
@@ -303,7 +301,8 @@ const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent, const SfxRequest&
 
         if (pArgs && pArgs->GetItemState(SID_SELECT_BACKGROUND, true, &pItem) == SfxItemState::SET)
         {
-            aFileName = static_cast<const SfxStringItem*>(pItem)->GetValue();
+            OUString aFileName(static_cast<const SfxStringItem*>(pItem)->GetValue());
+            OUString aFilterName;
 
             if (pArgs->GetItemState(FN_PARAM_FILTER, true, &pItem) == SfxItemState::SET)
                 aFilterName = static_cast<const SfxStringItem*>(pItem)->GetValue();
@@ -316,11 +315,9 @@ const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent, const SfxRequest&
             SvxOpenGraphicDialog aDlg(SdResId(STR_SET_BACKGROUND_PICTURE), pParent);
 
             nError = aDlg.Execute();
-            if (nError != ERRCODE_NONE)
+            if (nError == ERRCODE_NONE)
             {
                 nError = aDlg.GetGraphic(aGraphic);
-                aFileName = aDlg.GetPath();
-                aFilterName = aDlg.GetDetectedFilter();
             }
         }
 