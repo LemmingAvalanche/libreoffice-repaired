LemmingAvalanche/libreoffice-repaired

commit 1b5ed6e40a58fbcbba82a7e0c84990fc638e0729

tdf#104615: there's no mpView when opening odc directly
2 parts changed for these bts:
0  SdrEditView::GetGeoAttrFromMarked (this=0x0) at /home/julien/lo/libreoffice/svx/source/svdraw/svdedtv1.cxx:1278
1  0x00002aaadf70ae97 in SvxBitmapTabPage::Reset (this=0x55555b2eb8b0, rAttrs=0x55555afa0310) at /home/julien/lo/libreoffice/cui/source/tabpages/tpbitmap.cxx:278
See https://bugs.documentfoundation.org//show_bug.cgi?id=104615#c1

0  SdrPaintView::GetModel (this=0x0) at /home/julien/lo/libreoffice/include/svx/svdpntv.hxx:256
1  0x00002aaadf90ba0f in SvxBitmapTabPage::ModifyBitmapHdl (this=this@entry=0x55555b2e7950) at /home/julien/lo/libreoffice/cui/source/tabpages/tpbitmap.cxx:526
2  0x00002aaadf90bcc7 in SvxBitmapTabPage::ClickBitmapHdl_Impl (this=this@entry=0x55555b2e7950) at /home/julien/lo/libreoffice/cui/source/tabpages/tpbitmap.cxx:457
3  0x00002aaadf90c9d8 in SvxBitmapTabPage::Reset (this=0x55555b2e7950, rAttrs=<optimized out>) at /home/julien/lo/libreoffice/cui/source/tabpages/tpbitmap.cxx:441
See https://bugs.documentfoundation.org//show_bug.cgi?id=104615#c2

Thank you to Katarina Behrens for her help on this.
See http://nabble.documentfoundation.org/About-opening-odc-file-and-mpView-null-td4202317.html

Change-Id: Ibbca868f33ca3292e0d3f787df87876bc9464e0b
Reviewed-on: https://gerrit.libreoffice.org/31924
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
Tested-by: Katarina Behrens <Katarina.Behrens@cib.de>

cui/source/tabpages/tpbitmap.cxx
@@ -275,17 +275,26 @@ bool SvxBitmapTabPage::FillItemSet( SfxItemSet* rAttrs )
 
 void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
 {
-    SfxItemSet rGeoAttr(mpView->GetGeoAttrFromMarked());
-    const SfxPoolItem* pItem = nullptr;
-    const double fUIScale(double(mpView->GetModel()->GetUIScale()));
+    const SfxPoolItem* pItemTransfWidth = nullptr;
+    const SfxPoolItem* pItemTransfHeight = nullptr;
+    double fUIScale  = 1.0;
+    if (mpView)
+    {
+        fUIScale  = ( mpView->GetModel() ? double(mpView->GetModel()->GetUIScale()) : 1.0);
 
-    pItem = GetItem( rGeoAttr, SID_ATTR_TRANSFORM_WIDTH );
-    m_fObjectWidth = std::max( pItem ? (double)static_cast<const SfxUInt32Item*>(pItem)->GetValue() : 0.0, 1.0 );
+
+        if (mpView->AreObjectsMarked())
+        {
+            SfxItemSet rGeoAttr(mpView->GetGeoAttrFromMarked());
+            pItemTransfWidth = GetItem( rGeoAttr, SID_ATTR_TRANSFORM_WIDTH );
+            pItemTransfHeight= GetItem( rGeoAttr, SID_ATTR_TRANSFORM_HEIGHT );
+        }
+    }
+    m_fObjectWidth = std::max( pItemTransfWidth ? (double)static_cast<const SfxUInt32Item*>(pItemTransfWidth)->GetValue() : 0.0, 1.0 );
+    m_fObjectHeight = std::max( pItemTransfHeight ? (double)static_cast<const SfxUInt32Item*>(pItemTransfHeight)->GetValue() : 0.0, 1.0 );
     double fTmpWidth((OutputDevice::LogicToLogic(static_cast<sal_Int32>(m_fObjectWidth), mePoolUnit, MapUnit::Map100thMM )) / fUIScale);
     m_fObjectWidth = fTmpWidth;
 
-    pItem = GetItem( rGeoAttr, SID_ATTR_TRANSFORM_HEIGHT );
-    m_fObjectHeight = std::max( pItem ? (double)static_cast<const SfxUInt32Item*>(pItem)->GetValue() : 0.0, 1.0 );
     double fTmpHeight((OutputDevice::LogicToLogic(static_cast<sal_Int32>(m_fObjectHeight), mePoolUnit, MapUnit::Map100thMM )) / fUIScale);
     m_fObjectHeight = fTmpHeight;
 
@@ -518,7 +527,7 @@ IMPL_LINK_NOARG(SvxBitmapTabPage, ModifyBitmapHdl, ValueSet*, void)
 
     BitmapEx aBmpEx(pGraphicObject->GetGraphic().GetBitmapEx());
     Size aTempBitmapSize = aBmpEx.GetSizePixel();
-    const double fUIScale(double(mpView->GetModel()->GetUIScale()));
+    const double fUIScale = ( (mpView && mpView->GetModel()) ? double(mpView->GetModel()->GetUIScale()) : 1.0);
 
     rBitmapSize.Width() = ((OutputDevice::LogicToLogic(static_cast<sal_Int32>(aTempBitmapSize.Width()),MapUnit::MapPixel, MapUnit::Map100thMM )) / fUIScale);
     rBitmapSize.Height() = ((OutputDevice::LogicToLogic(static_cast<sal_Int32>(aTempBitmapSize.Height()),MapUnit::MapPixel, MapUnit::Map100thMM )) / fUIScale);

commit c6d22d6137cc3acde3c9bf1622c3a6778dcaf551

Fix SfxPoolItem use-after-free
...as observed with -fsanitize=address in Draw, after drawing some rectangle (so
that there is at least one marked object) doing "Format - Area... - Area -
Bitmap":

> ERROR: AddressSanitizer: heap-use-after-free on address 0x6030004aca50 at pc 0x7f14d0ef5fe1 bp 0x7ffd966c6cb0 sp 0x7ffd966c6ca8
> READ of size 4 at 0x6030004aca50 thread T0
>  #0 in CntUInt32Item::GetValue() const at include/svl/cintitem.hxx:163:42
>  #1 in SvxBitmapTabPage::Reset(SfxItemSet const*) at cui/source/tabpages/tpbitmap.cxx:278:124
>  #2 in SvxAreaTabPage::CreatePage(int, SfxTabPage*) at cui/source/tabpages/tparea.cxx:448:21
>  #3 in SvxAreaTabPage::SelectFillType(weld::ToggleButton&, SfxItemSet const*) at cui/source/tabpages/tparea.cxx:381:9
>  #4 in SvxAreaTabPage::SelectFillTypeHdl_Impl(weld::ToggleButton&) at cui/source/tabpages/tparea.cxx:364:5
>  #5 in SvxAreaTabPage::LinkStubSelectFillTypeHdl_Impl(void*, weld::ToggleButton&) at cui/source/tabpages/tparea.cxx:358:1
>  #6 in Link<weld::ToggleButton&, void>::Call(weld::ToggleButton&) const at include/tools/link.hxx:111:45
>  #7 in weld::ToggleButton::signal_toggled() at include/vcl/weld.hxx:1130:42
[...]
> 0x6030004aca50 is located 16 bytes inside of 24-byte region [0x6030004aca40,0x6030004aca58)
> freed by thread T0 here:
>  #0 in operator delete(void*, unsigned long) at ~/github.com/llvm/llvm-project/compiler-rt/lib/asan/asan_new_delete.cpp:172:3
>  #1 in SfxUInt32Item::~SfxUInt32Item() at include/svl/intitem.hxx:113:21
>  #2 in SfxItemPool::Remove(SfxPoolItem const&) at svl/source/items/itempool.cxx:710:13
>  #3 in SfxItemSet::~SfxItemSet() at svl/source/items/itemset.cxx:252:42
>  #4 in SvxBitmapTabPage::Reset(SfxItemSet const*) at cui/source/tabpages/tpbitmap.cxx:276:9
>  #5 in SvxAreaTabPage::CreatePage(int, SfxTabPage*) at cui/source/tabpages/tparea.cxx:448:21
>  #6 in SvxAreaTabPage::SelectFillType(weld::ToggleButton&, SfxItemSet const*) at cui/source/tabpages/tparea.cxx:381:9
>  #7 in SvxAreaTabPage::SelectFillTypeHdl_Impl(weld::ToggleButton&) at cui/source/tabpages/tparea.cxx:364:5
>  #8 in SvxAreaTabPage::LinkStubSelectFillTypeHdl_Impl(void*, weld::ToggleButton&) at cui/source/tabpages/tparea.cxx:358:1
>  #9 in Link<weld::ToggleButton&, void>::Call(weld::ToggleButton&) const at include/tools/link.hxx:111:45

This appears to be broken ever since 1b5ed6e40a58fbcbba82a7e0c84990fc638e0729

cui/source/tabpages/tpbitmap.cxx
@@ -260,8 +260,8 @@ bool SvxBitmapTabPage::FillItemSet( SfxItemSet* rAttrs )
 
 void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
 {
-    const SfxPoolItem* pItemTransfWidth = nullptr;
-    const SfxPoolItem* pItemTransfHeight = nullptr;
+    double transfWidth = 0.0;
+    double transfHeight = 0.0;
     double fUIScale  = 1.0;
     if (mpView)
     {
@@ -271,12 +271,12 @@ void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
         if (mpView->AreObjectsMarked())
         {
             SfxItemSet rGeoAttr(mpView->GetGeoAttrFromMarked());
-            pItemTransfWidth = GetItem( rGeoAttr, SID_ATTR_TRANSFORM_WIDTH );
-            pItemTransfHeight= GetItem( rGeoAttr, SID_ATTR_TRANSFORM_HEIGHT );
+            transfWidth = static_cast<double>(GetItem( rGeoAttr, SID_ATTR_TRANSFORM_WIDTH )->GetValue());
+            transfHeight= static_cast<double>(GetItem( rGeoAttr, SID_ATTR_TRANSFORM_HEIGHT )->GetValue());
         }
     }
-    m_fObjectWidth = std::max( pItemTransfWidth ? static_cast<double>(static_cast<const SfxUInt32Item*>(pItemTransfWidth)->GetValue()) : 0.0, 1.0 );
-    m_fObjectHeight = std::max( pItemTransfHeight ? static_cast<double>(static_cast<const SfxUInt32Item*>(pItemTransfHeight)->GetValue()) : 0.0, 1.0 );
+    m_fObjectWidth = std::max( transfWidth, 1.0 );
+    m_fObjectHeight = std::max( transfHeight, 1.0 );
     double fTmpWidth((OutputDevice::LogicToLogic(static_cast<sal_Int32>(m_fObjectWidth), mePoolUnit, MapUnit::Map100thMM )) / fUIScale);
     m_fObjectWidth = fTmpWidth;
 