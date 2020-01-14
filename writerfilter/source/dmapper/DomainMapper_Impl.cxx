LemmingAvalanche/libreoffice-repaired

commit 922da2f90133c38b76563aac182fd987bac61d87

writerfilter: import nested generic fields from RTF
* for m_bStartGenericField, push new insert position onto the stack in
  DomainMapper_Impl::CloseFieldCommand(), not in
  DomainMapper_Impl::appendTextPortion()
* how is the field result inserted into the field, instead of after it?
  - it doesn't seem possible to move the insertion of the field mark to
    PopFieldContext(), as there's no way to pass 3 positions to
    attach(), and exposing a writable property for the position(s)
    is a bad idea
  - finishParagraph() calls AppendTextNode(), which happily ignores a
    content index on the given XTextRange
  - so temporarily insert a paragraph break, override the insert
    position, and join the paragraphs in PopFieldContext()

Change-Id: Ie49084f6ea6451e7d7f9b613cedce326de9a54f1
Reviewed-on: https://gerrit.libreoffice.org/80075
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1779,7 +1779,10 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
                         m_bTextInserted = true;
                         xTOCTextCursor->gotoRange(xTextRange->getEnd(), true);
                         mxTOCTextCursor = xTOCTextCursor;
-                        m_aTextAppendStack.push(TextAppendContext(xTextAppend, xTOCTextCursor));
+                        if (!m_bStartGenericField)
+                        {
+                            m_aTextAppendStack.push(TextAppendContext(xTextAppend, xTOCTextCursor));
+                        }
                     }
                 }
                 else
@@ -5029,12 +5032,27 @@ void DomainMapper_Impl::CloseFieldCommand()
                     const uno::Reference<text::XTextContent> xTextContent(xFieldInterface, uno::UNO_QUERY_THROW);
                     uno::Reference< text::XTextAppend > xTextAppend = m_aTextAppendStack.top().xTextAppend;
                     uno::Reference< text::XTextCursor > xCrsr = xTextAppend->createTextCursorByRange(pContext->GetStartRange());
-                    if (xTextContent.is())
+                    if (m_aTextAppendStack.top().xInsertPosition.is())
                     {
-                        xTextAppend->insertTextContent(xCrsr,xTextContent, true);
+                        xCrsr->gotoRange(m_aTextAppendStack.top().xInsertPosition, true);
                     }
-                    uno::Reference<uno::XInterface> xContent(xTextContent);
-                    uno::Reference< text::XFormField> xFormField(xContent, uno::UNO_QUERY);
+                    else
+                    {
+                        xCrsr->gotoEnd(true);
+                    }
+                    xTextAppend->insertTextContent(xCrsr, xTextContent, true);
+                    // problem: the fieldmark must be inserted here, because
+                    //          attach() takes 2 positions, not 3!
+                    // FAIL: AppendTextNode() ignores the content index!
+                    // plan B: insert a spurious paragraph break now and join
+                    //         it in PopFieldContext()!
+                    xCrsr->gotoRange(xTextContent->getAnchor()->getEnd(), false);
+                    xCrsr->goLeft(1, false); // skip CH_TXT_ATR_FIELDEND
+                    xTextAppend->insertControlCharacter(xCrsr, text::ControlCharacter::PARAGRAPH_BREAK, false);
+                    xCrsr->goLeft(1, false); // back to previous paragraph
+                    m_aTextAppendStack.push(TextAppendContext(xTextAppend, xCrsr));
+
+                    uno::Reference<text::XFormField> xFormField(xTextContent, uno::UNO_QUERY);
                     xFormField->setFieldType(aCode);
                     m_bStartGenericField = true;
                     pContext->SetFormField( xFormField );
@@ -5396,9 +5414,14 @@ void DomainMapper_Impl::PopFieldContext()
                         else if(m_bStartGenericField)
                         {
                             m_bStartGenericField = false;
+                            xCrsr->gotoRange(m_aTextAppendStack.top().xInsertPosition, false);
+                            xCrsr->goRight(1, true);
+                            xCrsr->setString(OUString()); // undo SplitNode from CloseFieldCommand()
+                            // note: paragraph properties will be overwritten
+                            // by finishParagraph() anyway so ignore here
+                            m_aTextAppendStack.pop();
                             if(m_bTextInserted)
                             {
-                                m_aTextAppendStack.pop();
                                 m_bTextInserted = false;
                             }
                         }

sw/qa/extras/ooxmlexport/ooxmlfieldexport.cxx
@@ -537,7 +537,7 @@ DECLARE_OOXMLEXPORT_TEST(testSdtDateDuplicate, "sdt-date-duplicate.docx")
 DECLARE_OOXMLEXPORT_TEST(testFdo81492, "fdo81492.docx")
 {
     if (xmlDocPtr pXmlDoc = parseExport())
-        assertXPathContent(pXmlDoc, "/w:document/w:body/w:p[1]/w:r[9]/w:instrText", "ADDIN EN.CITE.DATA");
+        assertXPathContent(pXmlDoc, "/w:document/w:body/w:p[1]/w:r[7]/w:instrText", "ADDIN EN.CITE.DATA");
 }
 
 DECLARE_OOXMLEXPORT_TEST(testEditTime, "fdo81341.docx")

commit 9623a8ab9c0174f91fcba4ba8667ba6621709bda

tdf#129805 writerfilter: fix import of nested generic field
The problem was that the end of the outer nested generic field did not
call PopFieldmark(), so the end of the field was at the end of the
document.

(regression from 922da2f90133c38b76563aac182fd987bac61d87)

Change-Id: If5928b14dd35f7dd509370c2b8eef4c31bd149dc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86785
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -261,7 +261,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bStartedTOC(false),
         m_bStartIndex(false),
         m_bStartBibliography(false),
-        m_bStartGenericField(false),
+        m_nStartGenericField(0),
         m_bTextInserted(false),
         m_sCurrentPermId(0),
         m_pLastSectionContext( ),
@@ -1878,7 +1878,7 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
             }
             else
             {
-                if (m_bStartTOC || m_bStartIndex || m_bStartBibliography || m_bStartGenericField)
+                if (m_bStartTOC || m_bStartIndex || m_bStartBibliography || m_nStartGenericField != 0)
                 {
                     if (IsInHeaderFooter() && !m_bStartTOCHeaderFooter)
                     {
@@ -1890,15 +1890,17 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
                         uno::Reference< text::XTextCursor > xTOCTextCursor = xTextAppend->getEnd()->getText( )->createTextCursor( );
                         assert(xTOCTextCursor.is());
                         xTOCTextCursor->gotoEnd(false);
-                        if (m_bStartGenericField)
+                        if (m_nStartGenericField != 0)
+                        {
                             xTOCTextCursor->goLeft(1, false);
+                        }
                         xTextRange = xTextAppend->insertTextPortion(rString, aValues, xTOCTextCursor);
                         SAL_WARN_IF(!xTextRange.is(), "writerfilter.dmapper", "insertTextPortion failed");
                         if (!xTextRange.is())
                             throw uno::Exception("insertTextPortion failed", nullptr);
                         m_bTextInserted = true;
                         xTOCTextCursor->gotoRange(xTextRange->getEnd(), true);
-                        if (!m_bStartGenericField)
+                        if (m_nStartGenericField == 0)
                         {
                             m_aTextAppendStack.push(TextAppendContext(xTextAppend, xTOCTextCursor));
                         }
@@ -5390,7 +5392,7 @@ void DomainMapper_Impl::CloseFieldCommand()
                     InsertFieldmark(m_aTextAppendStack, xFormField, pContext->GetStartRange(),
                             pContext->GetFieldId());
                     xFormField->setFieldType(ODF_UNHANDLED);
-                    m_bStartGenericField = true;
+                    ++m_nStartGenericField;
                     pContext->SetFormField( xFormField );
                     uno::Reference<container::XNameContainer> const xNameCont(xFormField->getParameters());
                     // note: setting the code to empty string is *required* in
@@ -5806,9 +5808,9 @@ void DomainMapper_Impl::PopFieldContext()
                                 }
                             }
                         }
-                        else if(m_bStartGenericField)
+                        else if (m_nStartGenericField != 0)
                         {
-                            m_bStartGenericField = false;
+                            --m_nStartGenericField;
                             PopFieldmark(m_aTextAppendStack, xCrsr, pContext->GetFieldId());
                             if(m_bTextInserted)
                             {

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -472,7 +472,7 @@ private:
     bool                                                                            m_bStartedTOC;
     bool                                                                            m_bStartIndex;
     bool                                                                            m_bStartBibliography;
-    bool                                                                            m_bStartGenericField;
+    unsigned int                                                                    m_nStartGenericField;
     bool                                                                            m_bTextInserted;
     LineNumberSettings                                                              m_aLineNumberSettings;
 