LemmingAvalanche/libreoffice-repaired

commit efa23f164e918ec0867b5d9aab15271628427bfd

tdf#87569 tdf#109411 DOCX import: fix shape anchor in tables
Import "relative from page" horizontal setting of
VML and DrawingML shapes as "relative from column"
in tables, just as MSO handles it.

Change-Id: If71f2e52bbba324a98651e701feaeb99acfefc48
Reviewed-on: https://gerrit.libreoffice.org/85141
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper.cxx
@@ -3023,6 +3023,21 @@ void DomainMapper::lcl_startShape(uno::Reference<drawing::XShape> const& xShape)
         m_pImpl->PushPendingShape(xShape);
 
     m_pImpl->SetIsFirstParagraphInShape(true);
+
+    //tdf#87569: Fix table layout with correcting anchoring
+    //If anchored object is in table, Word calculates its position from cell border
+    //instead of page (what is set in the sample document)
+    if (m_pImpl->m_nTableDepth > 0) //if we had a table
+    {
+        uno::Reference<beans::XPropertySet> xShapePropSet(xShape, uno::UNO_QUERY);
+        sal_Int16 nCurrentHorOriRel; //A temp variable for storaging the current setting
+        xShapePropSet->getPropertyValue("HoriOrientRelation") >>= nCurrentHorOriRel;
+        //and the correction:
+        if (nCurrentHorOriRel == text::RelOrientation::PAGE_FRAME)
+            xShapePropSet->setPropertyValue("HoriOrientRelation",
+                                            uno::makeAny(text::RelOrientation::FRAME));
+    }
+
 }
 
 void DomainMapper::lcl_endShape( )

writerfilter/source/dmapper/GraphicImport.cxx
@@ -867,6 +867,15 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                             if (nRotation)
                                 xShapeProps->setPropertyValue("RotateAngle", uno::makeAny(nRotation));
                         }
+
+                        //tdf#109411 If anchored object is in table, Word calculates its position from cell border
+                        //instead of page (what is set in the sample document)
+                        if (m_pImpl->rDomainMapper.IsInTable() &&
+                            m_pImpl->nHoriRelation == text::RelOrientation::PAGE_FRAME && IsGraphic())
+                        {
+                            m_pImpl->nHoriRelation = text::RelOrientation::FRAME;
+                        }
+
                         m_pImpl->applyRelativePosition(xShapeProps, /*bRelativeOnly=*/true);
 
                         xShapeProps->setPropertyValue("SurroundContour", uno::makeAny(m_pImpl->bContour));

sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -17,6 +17,7 @@
 #include <tools/lineend.hxx>
 #include <com/sun/star/text/TableColumnSeparator.hpp>
 #include <com/sun/star/text/XDocumentIndex.hpp>
+#include <com/sun/star/text/RelOrientation.hpp>
 
 class Test : public SwModelTestBase
 {
@@ -33,6 +34,25 @@ protected:
     }
 };
 
+DECLARE_OOXMLEXPORT_TEST(testTdf87569v, "tdf87569_vml.docx")
+{
+    //the original tdf87569 sample has vml shapes...
+    uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
+    sal_Int16 nValue;
+    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf87569_vml: The Shape is not in the table!",
+                                 text::RelOrientation::FRAME, nValue);
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf87569d, "tdf87569_drawingml.docx")
+{
+    //if the original tdf87569 sample is upgraded it will have drawingml shapes...
+    uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
+    sal_Int16 nValue;
+    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf87569_drawingml: The Shape is not in the table!",
+                                 text::RelOrientation::FRAME, nValue);
+}
 
 DECLARE_OOXMLEXPORT_TEST(testTdf120315, "tdf120315.docx")
 {

commit 4a14e2122db3e850eb0129b89426f2a4f5e91fc6

tdf#129888 DOCX shape import: handle o:allowincell
(VML) and layoutInCell (DrawingML) attributes to fix
regressions caused by commit efa23f164e918ec0867b5d9aab15271628427bfd
(tdf#87569 tdf#109411 DOCX import: fix shape anchor in tables).

Position of shapes anchored to tables is calculated from
the cell margin only if the previous attributes allow that.

Change-Id: Ifcfcb7f4959aea522dd45dff00cefd1bb9f4edda
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86922
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper.cxx
@@ -3043,20 +3043,6 @@ void DomainMapper::lcl_startShape(uno::Reference<drawing::XShape> const& xShape)
 
     m_pImpl->SetIsFirstParagraphInShape(true);
 
-    //tdf#87569: Fix table layout with correcting anchoring
-    //If anchored object is in table, Word calculates its position from cell border
-    //instead of page (what is set in the sample document)
-    if (m_pImpl->m_nTableDepth > 0) //if we had a table
-    {
-        uno::Reference<beans::XPropertySet> xShapePropSet(xShape, uno::UNO_QUERY);
-        sal_Int16 nCurrentHorOriRel; //A temp variable to store the current setting
-        xShapePropSet->getPropertyValue("HoriOrientRelation") >>= nCurrentHorOriRel;
-        //and the correction:
-        if (nCurrentHorOriRel == text::RelOrientation::PAGE_FRAME)
-            xShapePropSet->setPropertyValue("HoriOrientRelation",
-                                            uno::makeAny(text::RelOrientation::FRAME));
-    }
-
 }
 
 void DomainMapper::lcl_endShape( )

writerfilter/source/dmapper/GraphicImport.cxx
@@ -875,7 +875,7 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
 
                         //tdf#109411 If anchored object is in table, Word calculates its position from cell border
                         //instead of page (what is set in the sample document)
-                        if (m_pImpl->rDomainMapper.IsInTable() &&
+                        if (m_pImpl->rDomainMapper.IsInTable() && m_pImpl->bLayoutInCell &&
                             m_pImpl->nHoriRelation == text::RelOrientation::PAGE_FRAME && IsGraphic())
                         {
                             m_pImpl->nHoriRelation = text::RelOrientation::FRAME;

writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/text/RelOrientation.hpp>
 #include <com/sun/star/lang/WrappedTargetRuntimeException.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/xml/sax/FastShapeContextHandler.hpp>
@@ -65,6 +67,7 @@ OOXMLFastContextHandler::OOXMLFastContextHandler
   mpStream(nullptr),
   mnTableDepth(0),
   inPositionV(false),
+  mbLayoutInCell(true),
   m_xContext(context),
   m_bDiscardChildren(false),
   m_bTookChoice(false)
@@ -85,6 +88,7 @@ OOXMLFastContextHandler::OOXMLFastContextHandler(OOXMLFastContextHandler * pCont
   mpParserState(pContext->mpParserState),
   mnTableDepth(pContext->mnTableDepth),
   inPositionV(pContext->inPositionV),
+  mbLayoutInCell(pContext->mbLayoutInCell),
   m_xContext(pContext->m_xContext),
   m_bDiscardChildren(pContext->m_bDiscardChildren),
   m_bTookChoice(pContext->m_bTookChoice)
@@ -1663,6 +1667,21 @@ void OOXMLFastContextHandlerShape::sendShape( Token_t Element )
 
             bool bIsPicture = Element == ( NMSP_dmlPicture | XML_pic );
 
+
+            //tdf#87569: Fix table layout with correcting anchoring
+            //If anchored object is in table, Word calculates its position from cell border
+            //instead of page (what is set in the sample document)
+            if (mnTableDepth > 0 && mbLayoutInCell) //if we had a table
+            {
+                uno::Reference<beans::XPropertySet> xShapePropSet(xShape, uno::UNO_QUERY);
+                sal_Int16 nCurrentHorOriRel; //A temp variable for storaging the current setting
+                xShapePropSet->getPropertyValue("HoriOrientRelation") >>= nCurrentHorOriRel;
+                //and the correction:
+                if (nCurrentHorOriRel == com::sun::star::text::RelOrientation::PAGE_FRAME)
+                    xShapePropSet->setPropertyValue("HoriOrientRelation",
+                                                    uno::makeAny(text::RelOrientation::FRAME));
+            }
+
             // Notify the dmapper that the shape is ready to use
             if ( !bIsPicture )
             {
@@ -1734,6 +1753,11 @@ OOXMLFastContextHandlerShape::lcl_createFastChildContext
                         new OOXMLFastContextHandlerWrapper(this,
                                                            pChildContext,
                                                            this);
+
+                    //tdf129888 store allowincell attribute of the VML shape
+                    if (Attribs->hasAttribute(NMSP_vmlOffice | XML_allowincell))
+                        mbLayoutInCell
+                            = !(Attribs->getValue(NMSP_vmlOffice | XML_allowincell) == "f");
 
                     if (!bGroupShape)
                     {

writerfilter/source/ooxml/OOXMLFastContextHandler.hxx
@@ -226,6 +226,7 @@ protected:
     const css::uno::Reference< css::uno::XComponentContext >& getComponentContext() const { return m_xContext;}
 
     bool inPositionV;
+    bool mbLayoutInCell; // o:allowincell
     OOXMLValue::Pointer_t mpGridAfter;
 
 private:

sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -43,6 +43,34 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128207, "tdf128207.docx")
     assertXPathContent(p_XmlDoc, "/w:document/w:body/w:p[1]/w:r[1]/w:drawing/wp:anchor/wp:positionH/wp:posOffset", "4445");
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf129888vml, "tdf129888vml.docx")
+{
+    //the line shape has anchor in the first cell however it has to
+    //be positioned to an another cell. To reach this we must handle
+    //the o:allowincell attribute of the shape, and its position has
+    //to be calculated from the page frame instead of the table:
+
+    uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
+    sal_Int16 nValue;
+    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf129888vml The line shape has bad place!",
+                                 text::RelOrientation::PAGE_FRAME, nValue);
+}
+
+DECLARE_OOXMLIMPORT_TEST(testTdf129888dml, "tdf129888dml.docx")
+{
+    //the shape has anchor in the first cell however it has to
+    //be positioned to the right side of the page. To reach this we must handle
+    //the layoutInCell attribute of the shape, and its position has
+    //to be calculated from the page frame instead of the table:
+
+    uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
+    sal_Int16 nValue;
+    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf129888dml The shape has bad place!",
+                                 text::RelOrientation::PAGE_FRAME, nValue);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf87569v, "tdf87569_vml.docx")
 {
     //the original tdf87569 sample has vml shapes...