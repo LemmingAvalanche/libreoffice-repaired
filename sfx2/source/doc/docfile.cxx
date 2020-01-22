LemmingAvalanche/libreoffice-repaired

commit 366fbe7fc299a7ffb42cee3a23570aff15d965e7

sw reqif-xhtml import: add a new AllowedRTFOLEMimeTypes parameter
The HTML import is an old-style filter, so it has no XFilter
implementation where filter() would get custom parameters out of the
box. One way would be to fix by adding one more entry to the aFormalArgs
table under sfx2/, but doing that with a random parameter of a random
import filter feels dirty.

So instead make SfxMedium store all arguments as-is, this way accessing
other keys is as easy to accessing the already available FilterOptions
(string) key.

Regarding the actual filter change, don't require "text/rtf" as a mime
type for embedded objects in the reqif XHTML import, so that in case the
file has e.g. application/rtf, then that works as well.

In case an (UNO) client wants to still limit the accepted set of MIME
types, that's possible via the new parameter.

Change-Id: Ia60da44d692f550d8ad5bbf374171cac67d9e04f
Reviewed-on: https://gerrit.libreoffice.org/85229
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sfx2/source/doc/docfile.cxx
@@ -328,6 +328,8 @@ public:
 
     util::DateTime m_aDateTime;
 
+    uno::Sequence<beans::PropertyValue> m_aArgs;
+
     explicit SfxMedium_Impl();
     ~SfxMedium_Impl();
     SfxMedium_Impl(const SfxMedium_Impl&) = delete;
@@ -3240,6 +3242,7 @@ SfxMedium::SfxMedium( const uno::Sequence<beans::PropertyValue>& aArgs ) :
     SfxAllItemSet *pParams = new SfxAllItemSet( SfxGetpApp()->GetPool() );
     pImpl->m_pSet.reset( pParams );
     TransformParameters( SID_OPENDOC, aArgs, *pParams );
+    pImpl->m_aArgs = aArgs;
 
     OUString aFilterProvider, aFilterName;
     {
@@ -3301,6 +3304,7 @@ SfxMedium::SfxMedium( const uno::Sequence<beans::PropertyValue>& aArgs ) :
     Init_Impl();
 }
 
+uno::Sequence<beans::PropertyValue> SfxMedium::GetArgs() const { return pImpl->m_aArgs; }
 
 SfxMedium::SfxMedium( const uno::Reference < embed::XStorage >& rStor, const OUString& rBaseURL, const std::shared_ptr<SfxItemSet>& p ) :
     pImpl(new SfxMedium_Impl)

include/sfx2/docfile.hxx
@@ -108,6 +108,7 @@ public:
     const OUString&     GetOrigURL() const;
 
     SfxItemSet  *       GetItemSet() const;
+    css::uno::Sequence<css::beans::PropertyValue> GetArgs() const;
     void                Close(bool bInDestruction = false);
     void                CloseAndRelease();
     void                ReOpen();

sw/qa/extras/htmlimport/data/allowed-rtf-ole-mime-types.xhtml
@@ -1,0 +1,5 @@
+<reqif-xhtml:div>
+	<reqif-xhtml:object data="ole2.ole" type="test/rtf">
+		<reqif-xhtml:object data="ole2.png" type="image/png">OLE Object</reqif-xhtml:object>
+	</reqif-xhtml:object>
+</reqif-xhtml:div>

sw/qa/extras/htmlimport/htmlimport.cxx
@@ -13,8 +13,11 @@
 #include <com/sun/star/graphic/GraphicType.hpp>
 #include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/drawing/BitmapMode.hpp>
+#include <com/sun/star/document/XEmbeddedObjectSupplier2.hpp>
+#include <com/sun/star/embed/XInplaceObject.hpp>
 #include <tools/datetime.hxx>
 #include <sfx2/linkmgr.hxx>
+#include <comphelper/propertyvalue.hxx>
 
 #include <docsh.hxx>
 #include <editsh.hxx>
@@ -367,6 +370,38 @@ DECLARE_HTMLIMPORT_TEST(testReqIfPageStyle, "reqif-page-style.xhtml")
     // 'Expected: Standard, Actual  : HTML'.
     CPPUNIT_ASSERT_EQUAL(OUString("Standard"),
                          getProperty<OUString>(getParagraph(1), "PageStyleName"));
+}
+
+/// HTML import to the sw doc model tests.
+class SwHtmlOptionsImportTest : public SwModelTestBase
+{
+};
+
+char const DATA_DIRECTORY[] = "/sw/qa/extras/htmlimport/data/";
+
+CPPUNIT_TEST_FIXTURE(SwHtmlOptionsImportTest, testAllowedRTFOLEMimeTypes)
+{
+    uno::Sequence<OUString> aTypes = { OUString("test/rtf") };
+    uno::Sequence<beans::PropertyValue> aLoadProperties = {
+        comphelper::makePropertyValue("FilterName", OUString("HTML (StarWriter)")),
+        comphelper::makePropertyValue("FilterOptions", OUString("xhtmlns=reqif-xhtml")),
+        comphelper::makePropertyValue("AllowedRTFOLEMimeTypes", aTypes),
+    };
+    OUString aURL
+        = m_directories.getURLFromSrc(DATA_DIRECTORY) + "allowed-rtf-ole-mime-types.xhtml";
+    mxComponent = loadFromDesktop(aURL, "com.sun.star.text.TextDocument", aLoadProperties);
+    uno::Reference<text::XTextEmbeddedObjectsSupplier> xSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xObjects(xSupplier->getEmbeddedObjects(),
+                                                     uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), xObjects->getCount());
+    uno::Reference<document::XEmbeddedObjectSupplier2> xObject(xObjects->getByIndex(0),
+                                                               uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xObject.is());
+    uno::Reference<embed::XInplaceObject> xEmbeddedObject(
+        xObject->getExtendedControlOverEmbeddedObject(), uno::UNO_QUERY);
+    // Without the accompanying fix in place, this test would have failed, because the returned
+    // embedded object was a dummy one, which does not support in-place editing.
+    CPPUNIT_ASSERT(xEmbeddedObject.is());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/filter/html/htmlplug.cxx
@@ -554,7 +554,11 @@ bool SwHTMLParser::InsertEmbed()
                                      StreamMode::READ);
             uno::Reference<io::XInputStream> xInStream;
             SvMemoryStream aMemoryStream;
-            if (aType == "text/rtf")
+
+            // Allow any MIME type that starts with magic, unless a set of allowed types are
+            // specified.
+            auto it = m_aAllowedRTFOLEMimeTypes.find(aType);
+            if (m_aAllowedRTFOLEMimeTypes.empty() || it != m_aAllowedRTFOLEMimeTypes.end())
             {
                 OString aMagic("{\\object");
                 OString aHeader(read_uInt8s_ToOString(aFileStream, aMagic.getLength()));

sw/source/filter/html/swhtml.cxx
@@ -118,6 +118,8 @@
 #include <sfx2/viewfrm.hxx>
 #include <svx/svdobj.hxx>
 #include <officecfg/Office/Writer.hxx>
+#include <comphelper/sequenceashashmap.hxx>
+#include <comphelper/sequence.hxx>
 
 #include <swerror.h>
 #include <hints.hxx>
@@ -433,6 +435,23 @@ SwHTMLParser::SwHTMLParser( SwDoc* pD, SwPaM& rCursor, SvStream& rIn,
         if (rNamespace == "reqif-xhtml")
             m_bReqIF = true;
     }
+
+    // Extract load parameters which are specific to this filter.
+    if (!pMed)
+    {
+        return;
+    }
+
+    comphelper::SequenceAsHashMap aLoadMap(pMed->GetArgs());
+    auto it = aLoadMap.find("AllowedRTFOLEMimeTypes");
+    if (it == aLoadMap.end())
+    {
+        return;
+    }
+
+    uno::Sequence<OUString> aTypes;
+    it->second >>= aTypes;
+    m_aAllowedRTFOLEMimeTypes = comphelper::sequenceToContainer<std::set<OUString>>(aTypes);
 }
 
 SwHTMLParser::~SwHTMLParser()

commit 141c0ea9d27ab70eb538a7bb2ef0f76b15dc01f0

tdf#129829 sfx2: fix handling of password to open vs modify
Regression from commit 366fbe7fc299a7ffb42cee3a23570aff15d965e7 (sw
reqif-xhtml import: add a new AllowedRTFOLEMimeTypes parameter,
2019-12-16), the problem was that in case the (input) stream for an
opened document is kept alive, then we don't try to re-create it, which
means providing a different password behaves the same as providing the
same (wrong) password.

It seems CheckPasswd_Impl() in sfx2 only calls
comphelper::DocPasswordHelper::requestAndVerifyDocPassword() in case we
don't have a decrypted stream already.

Fix the problem by deleting those streams from the remembered input
parameters, the interesting ones have custom key names anyway.

[ No testcase, the problem is Windows-only and it's not clear to me why
the behavior is platform-specific. ]

Change-Id: I2fc1f0567244897caa18548161fe426ac7d0dfd4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87182
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sfx2/source/doc/docfile.cxx
@@ -3220,7 +3220,7 @@ SfxMedium::SfxMedium( const uno::Sequence<beans::PropertyValue>& aArgs ) :
     SfxAllItemSet *pParams = new SfxAllItemSet( SfxGetpApp()->GetPool() );
     pImpl->m_pSet.reset( pParams );
     TransformParameters( SID_OPENDOC, aArgs, *pParams );
-    pImpl->m_aArgs = aArgs;
+    SetArgs(aArgs);
 
     OUString aFilterProvider, aFilterName;
     {
@@ -3284,7 +3284,10 @@ SfxMedium::SfxMedium( const uno::Sequence<beans::PropertyValue>& aArgs ) :
 
 void SfxMedium::SetArgs(const uno::Sequence<beans::PropertyValue>& rArgs)
 {
-    pImpl->m_aArgs = rArgs;
+    comphelper::SequenceAsHashMap aArgsMap(rArgs);
+    aArgsMap.erase("Stream");
+    aArgsMap.erase("InputStream");
+    pImpl->m_aArgs = aArgsMap.getAsConstPropertyValueList();
 }
 
 uno::Sequence<beans::PropertyValue> SfxMedium::GetArgs() const { return pImpl->m_aArgs; }