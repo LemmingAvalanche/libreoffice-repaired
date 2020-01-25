LemmingAvalanche/libreoffice-repaired

commit 1fc53344d4c2fa5da6ebd6921dbadb9637f367a2

Simplify containers iterations in vcl
Use range-based loop or replace with STL functions.

Change-Id: Ide2f89194238ae6a1f21e8132e2297710d9e6dcd
Reviewed-on: https://gerrit.libreoffice.org/61756
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/generic/printer/ppdparser.cxx
@@ -1215,9 +1215,8 @@ void PPDParser::parse( ::std::vector< OString >& rLines )
     }
 
     // second pass: fill in defaults
-    for( line = rLines.begin(); line != rLines.end(); ++line )
+    for( const auto& aLine : rLines )
     {
-        OString aLine(*line);
         if (aLine.startsWith("*Default"))
         {
             SAL_INFO("vcl.unx.print", "Found a default: '" << aLine << "'");
@@ -1598,14 +1597,10 @@ void PPDKey::eraseValue( const OUString& rOption )
     if( it == m_aValues.end() )
         return;
 
-    for( PPDKey::value_type::iterator vit = m_aOrderedValues.begin(); vit != m_aOrderedValues.end(); ++vit )
-    {
-        if( *vit == &(it->second ) )
-        {
-            m_aOrderedValues.erase( vit );
-            break;
-        }
-    }
+    auto vit = std::find(m_aOrderedValues.begin(), m_aOrderedValues.end(), &(it->second ));
+    if( vit != m_aOrderedValues.end() )
+        m_aOrderedValues.erase( vit );
+
     m_aValues.erase( it );
 }
 
@@ -1649,10 +1644,12 @@ PPDContext& PPDContext::operator=( PPDContext&& rCopy )
 
 const PPDKey* PPDContext::getModifiedKey( int n ) const
 {
-    hash_type::const_iterator it;
-    for( it = m_aCurrentValues.begin(); it != m_aCurrentValues.end() && n--; ++it )
-        ;
-    return it != m_aCurrentValues.end() ? it->first : nullptr;
+    if( m_aCurrentValues.size() < static_cast<hash_type::size_type>(n) )
+        return nullptr;
+
+    hash_type::const_iterator it = m_aCurrentValues.begin();
+    std::advance(it, n);
+    return it->first;
 }
 
 void PPDContext::setParser( const PPDParser* pParser )

vcl/headless/svpframe.cxx
@@ -81,9 +81,8 @@ SvpSalFrame::~SvpSalFrame()
         m_pInstance->deregisterFrame( this );
 
     std::list<SvpSalFrame*> Children = m_aChildren;
-    for( std::list<SvpSalFrame*>::iterator it = Children.begin();
-         it != Children.end(); ++it )
-         (*it)->SetParent( m_pParent );
+    for( auto& rChild : Children )
+        rChild->SetParent( m_pParent );
     if( m_pParent )
         m_pParent->m_aChildren.remove( this );
 

vcl/inc/impdel.hxx
@@ -67,9 +67,8 @@ class DeletionListener
 
 inline void DeletionNotifier::notifyDelete()
 {
-    for( std::list< DeletionListener* >::const_iterator it =
-            m_aListeners.begin(); it != m_aListeners.end(); ++it )
-       (*it)->deleted();
+    for( auto& rListener : m_aListeners )
+       rListener->deleted();
 
     m_aListeners.clear();
 }

vcl/osx/salnativewidgets.cxx
@@ -74,12 +74,10 @@ public:
 void AquaBlinker::Blink( AquaSalFrame* pFrame, const tools::Rectangle& rRect, int nTimeout )
 {
     // prevent repeated paints from triggering themselves all the time
-    for( std::list< AquaBlinker* >::const_iterator it = pFrame->maBlinkers.begin();
-         it != pFrame->maBlinkers.end(); ++it )
-    {
-        if( (*it)->maInvalidateRect == rRect )
-            return;
-    }
+    auto isRepeated = std::any_of(pFrame->maBlinkers.begin(), pFrame->maBlinkers.end(),
+        [&rRect](AquaBlinker* pBlinker) { return pBlinker->maInvalidateRect == rRect; });
+    if( isRepeated )
+        return;
     AquaBlinker* pNew = new AquaBlinker( pFrame, rRect );
     pNew->SetTimeout( nTimeout );
     pNew->Start();

vcl/qt5/Qt5Instance_Print.cxx
@@ -89,12 +89,12 @@ void Qt5Instance::GetPrinterQueueInfo(ImplPrnQueueList* pList)
     ::std::vector<OUString> aPrinters;
     rManager.listPrinters(aPrinters);
 
-    for (::std::vector<OUString>::iterator it = aPrinters.begin(); it != aPrinters.end(); ++it)
+    for (const auto& rPrinter : aPrinters)
     {
-        const PrinterInfo& rInfo(rManager.getPrinterInfo(*it));
+        const PrinterInfo& rInfo(rManager.getPrinterInfo(rPrinter));
         // create new entry
         SalPrinterQueueInfo* pInfo = new SalPrinterQueueInfo;
-        pInfo->maPrinterName = *it;
+        pInfo->maPrinterName = rPrinter;
         pInfo->maDriver = rInfo.m_aDriverName;
         pInfo->maLocation = rInfo.m_aLocation;
         pInfo->maComment = rInfo.m_aComment;

vcl/quartz/ctfonts.cxx
@@ -537,10 +537,9 @@ void SystemFontList::AddFont( CoreTextFontFace* pFontData )
 
 void SystemFontList::AnnounceFonts( PhysicalFontCollection& rFontCollection ) const
 {
-    auto it = maFontContainer.cbegin();
-    for(; it != maFontContainer.cend(); ++it )
+    for(const auto& rEntry : maFontContainer )
     {
-        rFontCollection.Add( (*it).second.get() );
+        rFontCollection.Add( rEntry.second.get() );
     }
 }
 

vcl/quartz/salgdicommon.cxx
@@ -1769,18 +1769,17 @@ bool AquaSalGraphics::setClipRegion( const vcl::Region& i_rClip )
         RectangleVector aRectangles;
         i_rClip.GetRegionRectangles(aRectangles);
 
-        for(RectangleVector::const_iterator aRectIter(aRectangles.begin());
-            aRectIter != aRectangles.end(); ++aRectIter)
+        for(const auto& rRect : aRectangles)
         {
-            const long nW(aRectIter->Right() - aRectIter->Left() + 1); // uses +1 logic in original
+            const long nW(rRect.Right() - rRect.Left() + 1); // uses +1 logic in original
 
             if(nW)
             {
-                const long nH(aRectIter->Bottom() - aRectIter->Top() + 1); // uses +1 logic in original
+                const long nH(rRect.Bottom() - rRect.Top() + 1); // uses +1 logic in original
 
                 if(nH)
                 {
-                    const CGRect aRect = CGRectMake( aRectIter->Left(), aRectIter->Top(), nW, nH);
+                    const CGRect aRect = CGRectMake( rRect.Left(), rRect.Top(), nW, nH);
                     SAL_INFO( "vcl.cg", "CGPathAddRect(" << mxClipPath << ",NULL," << aRect << ")" );
                     CGPathAddRect( mxClipPath, nullptr, aRect );
                 }

vcl/source/control/throbber.cxx
@@ -78,13 +78,10 @@ namespace
         aImages.reserve( aImageURLs.size() );
 
         ::comphelper::NamedValueCollection aMediaProperties;
-        for (   ::std::vector< OUString >::const_iterator imageURL = aImageURLs.begin();
-                imageURL != aImageURLs.end();
-                ++imageURL
-            )
+        for ( const auto& rImageURL : aImageURLs )
         {
             Reference< XGraphic > xGraphic;
-            aMediaProperties.put( "URL", *imageURL );
+            aMediaProperties.put( "URL", rImageURL );
             xGraphic.set( xGraphicProvider->queryGraphic( aMediaProperties.getPropertyValues() ), UNO_QUERY );
             aImages.emplace_back( xGraphic );
         }

commit 5df6c990b2aaeec11c611cdb9b11770897db010d

Fix off-by-one error in PPDContext::getModifiedKey
...introduced with 1fc53344d4c2fa5da6ebd6921dbadb9637f367a2 "Simplify containers
iterations in vcl"

Change-Id: I95bd303b744796a2b585575c4cdf548e4a0a729f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87379
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Arkadiy Illarionov <qarkai@gmail.com>
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

vcl/unx/generic/printer/ppdparser.cxx
@@ -1646,7 +1646,7 @@ PPDContext& PPDContext::operator=( PPDContext&& rCopy )
 
 const PPDKey* PPDContext::getModifiedKey( int n ) const
 {
-    if( m_aCurrentValues.size() < static_cast<hash_type::size_type>(n) )
+    if( m_aCurrentValues.size() <= static_cast<hash_type::size_type>(n) )
         return nullptr;
 
     hash_type::const_iterator it = m_aCurrentValues.begin();