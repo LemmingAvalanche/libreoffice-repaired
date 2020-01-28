LemmingAvalanche/libreoffice-repaired

commit 3e5ec561a36e08c7b6f0ac1e097330539ab6cc1c

DOCX import: make sure rotation does not affect shape position
Change-Id: I7916a24d63bb7c995267059e9bedbf1f08adc152

writerfilter/source/dmapper/GraphicImport.cxx
@@ -779,9 +779,23 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                         if (xServiceInfo->supportsService("com.sun.star.drawing.GroupShape") ||
                                 xServiceInfo->supportsService("com.sun.star.drawing.GraphicObjectShape"))
                         {
+                            // You would expect that position and rotation are
+                            // independent, but they are not. Till we are not
+                            // there yet to handle all scaling, translation and
+                            // rotation with a single transformation matrix,
+                            // make sure there is no rotation set when we set
+                            // the position.
+                            sal_Int32 nRotation = 0;
+                            xShapeProps->getPropertyValue("RotateAngle") >>= nRotation;
+                            if (nRotation)
+                                xShapeProps->setPropertyValue("RotateAngle", uno::makeAny(sal_Int32(0)));
+
                             // Position of the groupshape should be set after children have been added.
                             // fdo#80555: also set position for graphic shapes here
                             m_xShape->setPosition(awt::Point(m_pImpl->nLeftPosition, m_pImpl->nTopPosition));
+
+                            if (nRotation)
+                                xShapeProps->setPropertyValue("RotateAngle", uno::makeAny(nRotation));
                         }
                         m_pImpl->applyRelativePosition(xShapeProps);
 

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -272,6 +272,17 @@ DECLARE_OOXMLEXPORT_TEST(testChartDupe, "chart-dupe.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xEmbeddedObjects->getCount());
 }
 
+DECLARE_OOXMLEXPORT_TEST(testPositionAndRotation, "position-and-rotation.docx")
+{
+    // The document should look like: "This line is tricky, <image> because only 'This line is tricky,' is on the left."
+    // But the image was pushed down, so it did not break the line into two text portions.
+    uno::Reference<drawing::XShape> xShape = getShape(1);
+    // Should be 1559, was -5639
+    CPPUNIT_ASSERT(xShape->getPosition().X > 1500);
+    // Should be 88, was 473
+    CPPUNIT_ASSERT(xShape->getPosition().Y < 100);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 1c2f1b589958cd738e8fec4e509a4814038ecbb5

tdf#103964 DOCX import: ignore rotation when setting position of group shapes
Regression from commit 3e5ec561a36e08c7b6f0ac1e097330539ab6cc1c (DOCX
import: make sure rotation does not affect shape position, 2014-09-24),
the motivation for tweaking the rotation when setting the position of a
shape was for images. By accident, this also happened for group shapes.

But the bugdoc shows it's not a good idea to read the rotation of group
shapes: the group shape is just a container, it does not have rotation
in itself.

(The test120551 intention was probably just to verify that the position
is not entirely off, so the small required change to the expected value
should be OK.)

Change-Id: I8e12c28e65c5f64168c3f802546fddf472fcc6eb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87551
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/CppunitTest_writerfilter_dmapper.mk
@@ -19,6 +19,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,writerfilter_dmapper, \
     writerfilter/qa/cppunittests/dmapper/CellColorHandler \
     writerfilter/qa/cppunittests/dmapper/DomainMapperTableHandler \
     writerfilter/qa/cppunittests/dmapper/DomainMapper_Impl \
+    writerfilter/qa/cppunittests/dmapper/GraphicImport \
     writerfilter/qa/cppunittests/dmapper/TextEffectsHandler \
 ))
 

writerfilter/source/dmapper/GraphicImport.cxx
@@ -851,10 +851,13 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                             // independent, but they are not. Till we are not
                             // there yet to handle all scaling, translation and
                             // rotation with a single transformation matrix,
-                            // make sure there is no rotation set when we set
+                            // make sure there is no graphic rotation set when we set
                             // the position.
                             sal_Int32 nRotation = 0;
-                            xShapeProps->getPropertyValue("RotateAngle") >>= nRotation;
+                            if (xServiceInfo->supportsService("com.sun.star.drawing.GraphicObjectShape"))
+                            {
+                                xShapeProps->getPropertyValue("RotateAngle") >>= nRotation;
+                            }
                             if (nRotation)
                                 xShapeProps->setPropertyValue("RotateAngle", uno::makeAny(sal_Int32(0)));
 

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -176,8 +176,8 @@ DECLARE_OOXMLIMPORT_TEST(test120551, "tdf120551.docx")
 {
     auto nHoriOrientPosition = getProperty<sal_Int32>(getShape(1), "HoriOrientPosition");
     // Without the accompanying fix in place, this test would have failed with
-    // 'Expected: 436, Actual  : -2542'.
-    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(436), nHoriOrientPosition);
+    // 'Expected: 430, Actual  : -2542'.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(430), nHoriOrientPosition);
 }
 
 DECLARE_OOXMLIMPORT_TEST(testTdf111550, "tdf111550.docx")

writerfilter/qa/cppunittests/dmapper/GraphicImport.cxx
@@ -1,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/drawing/XDrawPageSupplier.hpp>
+#include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/table/BorderLine2.hpp>
+#include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/text/XTextTablesSupplier.hpp>
+
+#include <comphelper/processfactory.hxx>
+
+using namespace ::com::sun::star;
+
+namespace
+{
+/// Tests for writerfilter/source/dmapper/GraphicImport.cxx.
+class Test : public test::BootstrapFixture, public unotest::MacrosTest
+{
+private:
+    uno::Reference<uno::XComponentContext> mxComponentContext;
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    void setUp() override;
+    void tearDown() override;
+    uno::Reference<lang::XComponent>& getComponent() { return mxComponent; }
+};
+
+void Test::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    mxComponentContext.set(comphelper::getComponentContext(getMultiServiceFactory()));
+    mxDesktop.set(frame::Desktop::create(mxComponentContext));
+}
+
+void Test::tearDown()
+{
+    if (mxComponent.is())
+        mxComponent->dispose();
+
+    test::BootstrapFixture::tearDown();
+}
+
+char const DATA_DIRECTORY[] = "/writerfilter/qa/cppunittests/dmapper/data/";
+
+CPPUNIT_TEST_FIXTURE(Test, testGroupShapeRotation)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "group-shape-rotation.docx";
+    getComponent() = loadFromDesktop(aURL);
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    sal_Int32 nVertPosition = 0;
+    xShape->getPropertyValue("VertOrientPosition") >>= nVertPosition;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1221
+    // - Actual  : -2048
+    // i.e. the group shape had a so low vertical position that the line shape did not point into
+    // it.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1221), nVertPosition);
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */