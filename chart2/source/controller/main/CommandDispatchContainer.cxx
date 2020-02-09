LemmingAvalanche/libreoffice-repaired

commit 8d6e092ef852a3661ac8592a43ddb5a955ed6819

rework SvxUndoRedoControl to be a PopupWindowController
Change-Id: I78b4e03c76bc3aa8479013ae157a9e1316dcfecd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86841
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

svx/source/tbxctrls/lboxctrl.cxx
@@ -25,7 +25,6 @@
 #include <vcl/toolbox.hxx>
 #include <vcl/event.hxx>
 #include <sfx2/app.hxx>
-#include <sfx2/tbxctrl.hxx>
 #include <sfx2/bindings.hxx>
 #include <sfx2/dispatch.hxx>
 #include <sfx2/viewsh.hxx>
@@ -33,6 +32,7 @@
 #include <svl/eitem.hxx>
 #include <svl/slstitm.hxx>
 #include <svl/stritem.hxx>
+#include <svtools/toolbarmenu.hxx>
 #include <svx/dialmgr.hxx>
 #include <svx/lboxctrl.hxx>
 #include <vcl/mnemonic.hxx>
@@ -42,41 +42,38 @@
 #include <svx/svxids.hrc>
 #include <svx/strings.hrc>
 
+#include <comphelper/processfactory.hxx>
+#include <com/sun/star/util/URLTransformer.hpp>
+#include <com/sun/star/frame/XDispatchProvider.hpp>
+#include <com/sun/star/frame/XFrame.hpp>
+
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::util;
 using namespace ::com::sun::star::frame;
 
-class SvxPopupWindowListBox;
-
-class SvxPopupWindowListBox: public SfxPopupWindow
+class SvxPopupWindowListBox final : public svtools::ToolbarPopup
 {
     VclPtr<ListBox> m_pListBox;
-    ToolBox &       rToolBox;
-    bool            bUserSel;
-    sal_uInt16 const nTbxId;
+    rtl::Reference<SvxUndoRedoControl> m_xControl;
+
+    DECL_LINK( SelectHdl, ListBox&, void );
 
 public:
-    SvxPopupWindowListBox( sal_uInt16 nSlotId, const OUString& rCommandURL, sal_uInt16 nTbxId, ToolBox& rTbx );
+    SvxPopupWindowListBox(SvxUndoRedoControl* pControl, vcl::Window* pParent);
     virtual ~SvxPopupWindowListBox() override;
     virtual void dispose() override;
 
-    // SfxPopupWindow
-    virtual void                PopupModeEnd() override;
-    virtual void                statusChanged( const css::frame::FeatureStateEvent& rEvent ) override;
-
     ListBox &            GetListBox()    { return *m_pListBox; }
 
-    bool                        IsUserSelected() const          { return bUserSel; }
-    void                        SetUserSelected( bool bVal )    { bUserSel = bVal; }
+    void                 SetInfo(sal_Int32 nCount);
 };
 
-SvxPopupWindowListBox::SvxPopupWindowListBox(sal_uInt16 nSlotId, const OUString& rCommandURL, sal_uInt16 nId, ToolBox& rTbx)
-    : SfxPopupWindow(nSlotId, &rTbx, "FloatingUndoRedo", "svx/ui/floatingundoredo.ui")
-    , rToolBox(rTbx)
-    , bUserSel(false)
-    , nTbxId(nId)
+SvxPopupWindowListBox::SvxPopupWindowListBox(SvxUndoRedoControl* pControl, vcl::Window* pParent)
+    : ToolbarPopup(pControl->getFrameInterface(), pParent, "FloatingUndoRedo", "svx/ui/floatingundoredo.ui")
+    , m_xControl(pControl)
 {
-    DBG_ASSERT( nSlotId == GetId(), "id mismatch" );
     get(m_pListBox, "treeview");
     WinBits nBits(m_pListBox->GetStyle());
     nBits &= ~WB_SIMPLEMODE;
@@ -86,7 +83,8 @@ SvxPopupWindowListBox::SvxPopupWindowListBox(sal_uInt16 nSlotId, const OUString&
     m_pListBox->set_height_request(aSize.Height());
     m_pListBox->EnableMultiSelection( true, true );
     SetBackground( GetSettings().GetStyleSettings().GetDialogColor() );
-    AddStatusListener( rCommandURL );
+
+    m_pListBox->SetSelectHdl( LINK( this, SvxPopupWindowListBox, SelectHdl ) );
 }
 
 SvxPopupWindowListBox::~SvxPopupWindowListBox()
@@ -97,156 +95,142 @@ SvxPopupWindowListBox::~SvxPopupWindowListBox()
 void SvxPopupWindowListBox::dispose()
 {
     m_pListBox.clear();
-    SfxPopupWindow::dispose();
+    ToolbarPopup::dispose();
 }
 
-void SvxPopupWindowListBox::PopupModeEnd()
+void SvxPopupWindowListBox::SetInfo( sal_Int32 nCount )
 {
-    rToolBox.EndSelection();
-    SfxPopupWindow::PopupModeEnd();
-    //FloatingWindow::PopupModeEnd();
-
-    if( SfxViewShell::Current() )
-    {
-        vcl::Window* pShellWnd = SfxViewShell::Current()->GetWindow();
-        if (pShellWnd)
-            pShellWnd->GrabFocus();
-    }
-}
-
-
-void SvxPopupWindowListBox::statusChanged( const css::frame::FeatureStateEvent& rEvent )
-{
-    rToolBox.EnableItem( nTbxId, rEvent.IsEnabled );
-    SfxPopupWindow::statusChanged( rEvent );
-}
-
-IMPL_LINK_NOARG(SvxUndoRedoControl, PopupModeEndHdl, FloatingWindow*, void)
-{
-    if( pPopupWin && FloatWinPopupFlags::NONE == pPopupWin->GetPopupModeFlags()  &&
-        pPopupWin->IsUserSelected() )
-    {
-        const sal_Int32 nCount = pPopupWin->GetListBox().GetSelectedEntryCount();
-
-        INetURLObject aObj( m_aCommandURL );
-
-        Sequence< PropertyValue > aArgs( 1 );
-        aArgs[0].Name   = aObj.GetURLPath();
-        aArgs[0].Value  <<= sal_Int16( nCount );
-        SfxToolBoxControl::Dispatch( m_aCommandURL, aArgs );
-    }
-}
-
-
-void SvxUndoRedoControl::Impl_SetInfo( sal_Int32 nCount )
-{
-    DBG_ASSERT( pPopupWin, "NULL pointer, PopupWindow missing" );
-
     const char* pId;
     if (nCount == 1)
-        pId = SID_UNDO == GetSlotId() ? RID_SVXSTR_NUM_UNDO_ACTION : RID_SVXSTR_NUM_REDO_ACTION;
+        pId = m_xControl->getCommandURL() == ".uno:Undo" ? RID_SVXSTR_NUM_UNDO_ACTION : RID_SVXSTR_NUM_REDO_ACTION;
     else
-        pId = SID_UNDO == GetSlotId() ? RID_SVXSTR_NUM_UNDO_ACTIONS : RID_SVXSTR_NUM_REDO_ACTIONS;
-    aActionStr = SvxResId(pId);
-
+        pId = m_xControl->getCommandURL() == ".uno:Undo" ? RID_SVXSTR_NUM_UNDO_ACTIONS : RID_SVXSTR_NUM_REDO_ACTIONS;
+    OUString aActionStr = SvxResId(pId);
     OUString aText = aActionStr.replaceAll("$(ARG1)", OUString::number(nCount));
-    pPopupWin->SetText(aText);
+    SetText(aText);
 }
 
-
-IMPL_LINK_NOARG(SvxUndoRedoControl, SelectHdl, ListBox&, void)
+IMPL_LINK(SvxPopupWindowListBox, SelectHdl, ListBox&, rListBox, void)
 {
-    if (pPopupWin)
+    if (rListBox.IsTravelSelect())
+        SetInfo(rListBox.GetSelectedEntryCount());
+    else
     {
-        //pPopupWin->SetUserSelected( false );
+        fprintf(stderr, "popdown\n");
+        m_xControl->Do(GetListBox().GetSelectedEntryCount());
+        EndPopupMode();
+    }
+}
 
-        ListBox &rListBox = pPopupWin->GetListBox();
-        if (rListBox.IsTravelSelect())
-            Impl_SetInfo( rListBox.GetSelectedEntryCount() );
-        else
+void SvxUndoRedoControl::Do(sal_Int16 nCount)
+{
+    Reference< XDispatchProvider > xDispatchProvider( m_xFrame, UNO_QUERY );
+    if ( xDispatchProvider.is() )
+    {
+        css::util::URL aTargetURL;
+        Reference < XURLTransformer > xTrans( URLTransformer::create(::comphelper::getProcessComponentContext()) );
+        aTargetURL.Complete = m_aCommandURL;
+        xTrans->parseStrict( aTargetURL );
+
+        Reference< XDispatch > xDispatch = xDispatchProvider->queryDispatch( aTargetURL, OUString(), 0 );
+        if ( xDispatch.is() )
         {
-            pPopupWin->SetUserSelected( true );
-            pPopupWin->EndPopupMode();
+            INetURLObject aObj( m_aCommandURL );
+            Sequence< PropertyValue > aArgs( 1 );
+            aArgs[0].Name = aObj.GetURLPath();
+            aArgs[0].Value <<= nCount;
+            xDispatch->dispatch(aTargetURL, aArgs);
         }
     }
 }
 
-
-SFX_IMPL_TOOLBOX_CONTROL( SvxUndoRedoControl, SfxStringItem );
-
-SvxUndoRedoControl::SvxUndoRedoControl( sal_uInt16 nSlotId, sal_uInt16 nId, ToolBox& rTbx )
-    :SfxToolBoxControl( nSlotId, nId, rTbx ),
-    pPopupWin   ( nullptr )
+SvxUndoRedoControl::SvxUndoRedoControl(const css::uno::Reference<css::uno::XComponentContext>& rContext)
+    : PopupWindowController(rContext, nullptr, OUString())
 {
-    rTbx.SetItemBits( nId, ToolBoxItemBits::DROPDOWN | rTbx.GetItemBits( nId ) );
-    rTbx.Invalidate();
-    aDefaultTooltip = rTbx.GetQuickHelpText( nId );
+}
+
+void SvxUndoRedoControl::initialize( const css::uno::Sequence< css::uno::Any >& rArguments )
+{
+    PopupWindowController::initialize(rArguments);
+
+    ToolBox* pToolBox = nullptr;
+    sal_uInt16 nId = 0;
+    if (getToolboxId(nId, &pToolBox) && pToolBox->GetItemCommand(nId) == m_aCommandURL)
+    {
+        pToolBox->SetItemBits(nId, ToolBoxItemBits::DROPDOWN | pToolBox->GetItemBits(nId));
+        aDefaultTooltip = pToolBox->GetQuickHelpText(nId);
+    }
 }
 
 SvxUndoRedoControl::~SvxUndoRedoControl()
 {
 }
 
-void SvxUndoRedoControl::StateChanged(
-    sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState )
+// XStatusListener
+void SAL_CALL SvxUndoRedoControl::statusChanged(const css::frame::FeatureStateEvent& rEvent)
 {
-    if ( nSID == SID_UNDO || nSID == SID_REDO )
+    if (rEvent.FeatureURL.Main == ".uno:GetUndoStrings" || rEvent.FeatureURL.Main == ".uno:GetRedoStrings")
     {
-        if ( eState == SfxItemState::DISABLED )
-        {
-            ToolBox& rBox = GetToolBox();
-            rBox.SetQuickHelpText( GetId(), aDefaultTooltip );
-        }
-        else if ( auto pStringItem = dynamic_cast<const SfxStringItem*>( pState) )
-        {
-            ToolBox& rBox = GetToolBox();
-            const OUString& aQuickHelpText = pStringItem->GetValue();
-            rBox.SetQuickHelpText( GetId(), aQuickHelpText );
-        }
-        GetToolBox().EnableItem( GetId(),
-                                SfxItemState::DISABLED != GetItemState(pState) );
+        css::uno::Sequence<OUString> aStrings;
+        rEvent.State >>= aStrings;
+        aUndoRedoList = comphelper::sequenceToContainer<std::vector<OUString>>(aStrings);
+        return;
     }
-    else
-    {
-        aUndoRedoList.clear();
 
-        if ( auto pStringListItem = dynamic_cast<const SfxStringListItem*>( pState) )
-        {
-            aUndoRedoList = pStringListItem->GetList();
-        }
+    PopupWindowController::statusChanged(rEvent);
+
+    ToolBox* pToolBox = nullptr;
+    sal_uInt16 nId = 0;
+    if (!getToolboxId(nId, &pToolBox))
+        return;
+
+    if (!rEvent.IsEnabled)
+    {
+        pToolBox->SetQuickHelpText(nId, aDefaultTooltip);
+        return;
     }
+
+    OUString aQuickHelpText;
+    if (rEvent.State >>= aQuickHelpText)
+        pToolBox->SetQuickHelpText(nId, aQuickHelpText);
 }
 
-VclPtr<SfxPopupWindow> SvxUndoRedoControl::CreatePopupWindow()
+VclPtr<vcl::Window> SvxUndoRedoControl::createPopupWindow(vcl::Window* pParent)
 {
-    DBG_ASSERT(( SID_UNDO == GetSlotId() || SID_REDO == GetSlotId() ), "mismatching ids" );
-
     if ( m_aCommandURL == ".uno:Undo" )
         updateStatus( ".uno:GetUndoStrings");
     else
         updateStatus( ".uno:GetRedoStrings");
 
-    ToolBox& rBox = GetToolBox();
+    auto xPopupWin = VclPtr<SvxPopupWindowListBox>::Create(this, pParent);
 
-    pPopupWin = VclPtr<SvxPopupWindowListBox>::Create( GetSlotId(), m_aCommandURL, GetId(), rBox );
-    pPopupWin->SetPopupModeEndHdl( LINK( this, SvxUndoRedoControl, PopupModeEndHdl ) );
-    ListBox &rListBox = pPopupWin->GetListBox();
-    rListBox.SetSelectHdl( LINK( this, SvxUndoRedoControl, SelectHdl ) );
+    ListBox &rListBox = xPopupWin->GetListBox();
 
     for(const OUString & s : aUndoRedoList)
         rListBox.InsertEntry( s );
 
-    rListBox.SelectEntryPos( 0 );
-    aActionStr = SvxResId(SID_UNDO == GetSlotId() ?
-                                  RID_SVXSTR_NUM_UNDO_ACTIONS : RID_SVXSTR_NUM_REDO_ACTIONS);
-    Impl_SetInfo( rListBox.GetSelectedEntryCount() );
+    rListBox.SelectEntryPos(0);
+    xPopupWin->SetInfo(rListBox.GetSelectedEntryCount());
 
-    // move focus in floating window without
-    // closing it (GrabFocus() would close it!)
-    pPopupWin->StartPopupMode( &rBox, FloatWinPopupFlags::GrabFocus );
-    //pPopupWin->GetListBox().GrabFocus();
+    return xPopupWin;
+}
 
-    return pPopupWin;
+OUString SvxUndoRedoControl::getImplementationName()
+{
+    return "com.sun.star.comp.svx.UndoRedoToolBoxControl";
+}
+
+css::uno::Sequence<OUString> SvxUndoRedoControl::getSupportedServiceNames()
+{
+    return { "com.sun.star.frame.ToolbarController" };
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
+com_sun_star_comp_svx_UndoRedoToolBoxControl_get_implementation(
+    css::uno::XComponentContext* rContext,
+    css::uno::Sequence<css::uno::Any> const & )
+{
+    return cppu::acquire(new SvxUndoRedoControl(rContext));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/svx/lboxctrl.hxx
@@ -20,38 +20,36 @@
 #ifndef INCLUDED_SVX_LBOXCTRL_HXX
 #define INCLUDED_SVX_LBOXCTRL_HXX
 
-#include <sfx2/tbxctrl.hxx>
 #include <rtl/ustring.hxx>
 #include <vector>
+#include <svtools/popupwindowcontroller.hxx>
 #include <svx/svxdllapi.h>
 
 class ToolBox;
 class SvxPopupWindowListBox;
 
-
-class SVX_DLLPUBLIC SvxUndoRedoControl final : public SfxToolBoxControl
+class SVX_DLLPUBLIC SvxUndoRedoControl final : public svt::PopupWindowController
 {
-    OUString                aActionStr;
-    VclPtr<SvxPopupWindowListBox> pPopupWin;
     std::vector< OUString > aUndoRedoList;
     OUString                aDefaultTooltip;
 
-    void    Impl_SetInfo( sal_Int32 nCount );
-
-    DECL_LINK( PopupModeEndHdl, FloatingWindow*, void );
-    DECL_LINK( SelectHdl, ListBox&, void );
-
 public:
-    SFX_DECL_TOOLBOX_CONTROL();
-
-    SvxUndoRedoControl( sal_uInt16 nSlotId, sal_uInt16 nId, ToolBox& rTbx );
+    SvxUndoRedoControl(const css::uno::Reference<css::uno::XComponentContext>& rContext);
     virtual ~SvxUndoRedoControl() override;
 
-    virtual void StateChanged( sal_uInt16 nSID,
-                               SfxItemState eState,
-                               const SfxPoolItem* pState ) override;
+    using svt::ToolboxController::createPopupWindow;
+    virtual VclPtr<vcl::Window> createPopupWindow( vcl::Window* pParent ) override;
 
-    virtual VclPtr<SfxPopupWindow> CreatePopupWindow() override;
+    // XServiceInfo
+    virtual OUString SAL_CALL getImplementationName() override;
+    virtual css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
+
+    // XInitialization
+    virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& rArguments ) override;
+
+    virtual void SAL_CALL statusChanged(const css::frame::FeatureStateEvent& rEvent) override;
+
+    void Do(sal_Int16 nCount);
 };
 
 #endif

officecfg/registry/data/org/openoffice/Office/UI/Controller.xcu
@@ -1536,6 +1536,28 @@
           <value>com.sun.star.comp.svx.UnderlineToolBoxControl</value>
         </prop>
       </node>
+      <node oor:name="UndoToolBoxControl" oor:op="replace">
+        <prop oor:name="Command">
+          <value>.uno:Undo</value>
+        </prop>
+        <prop oor:name="Module">
+          <value/>
+        </prop>
+        <prop oor:name="Controller">
+          <value>com.sun.star.comp.svx.UndoRedoToolBoxControl</value>
+        </prop>
+      </node>
+      <node oor:name="RedoToolBoxControl" oor:op="replace">
+        <prop oor:name="Command">
+          <value>.uno:Redo</value>
+        </prop>
+        <prop oor:name="Module">
+          <value/>
+        </prop>
+        <prop oor:name="Controller">
+          <value>com.sun.star.comp.svx.UndoRedoToolBoxControl</value>
+        </prop>
+      </node>
       <node oor:name="XLineColorToolBox" oor:op="replace">
         <prop oor:name="Command">
           <value>.uno:XLineColor</value>

sc/source/ui/app/scdll.cxx
@@ -151,8 +151,6 @@ void ScDLL::Init()
     SvxStyleToolBoxControl          ::RegisterControl(SID_STYLE_APPLY,          pMod);
     SvxClipBoardControl             ::RegisterControl(SID_PASTE,                pMod );
     SvxClipBoardControl             ::RegisterControl(SID_PASTE_UNFORMATTED,    pMod );
-    SvxUndoRedoControl              ::RegisterControl(SID_UNDO,                 pMod );
-    SvxUndoRedoControl              ::RegisterControl(SID_REDO,                 pMod );
     svx::FormatPaintBrushToolBoxControl::RegisterControl(SID_FORMATPAINTBRUSH,  pMod );
     sc::ScNumberFormatControl       ::RegisterControl(SID_NUMBER_TYPE_FORMAT,   pMod );
 

sd/source/ui/app/sddll.cxx
@@ -206,10 +206,6 @@ void SdDLL::RegisterControllers(SdModule* pMod)
     SvxModifyControl::RegisterControl( SID_DOC_MODIFIED, pMod );
     SvxZoomSliderControl::RegisterControl( SID_ATTR_ZOOMSLIDER, pMod );
 
-    // #UndoRedo#
-    SvxUndoRedoControl::RegisterControl( SID_UNDO , pMod );
-    SvxUndoRedoControl::RegisterControl( SID_REDO , pMod );
-
     svx::FormatPaintBrushToolBoxControl::RegisterControl(SID_FORMATPAINTBRUSH, pMod );
 
     SvxClipBoardControl::RegisterControl( SID_PASTE, pMod );

starmath/source/smdll.cxx
@@ -68,8 +68,6 @@ namespace
         SvxZoomStatusBarControl::RegisterControl(SID_ATTR_ZOOM, pModule);
         SvxZoomSliderControl::RegisterControl(SID_ATTR_ZOOMSLIDER, pModule);
         SvxModifyControl::RegisterControl(SID_TEXTSTATUS, pModule);
-        SvxUndoRedoControl::RegisterControl(SID_UNDO, pModule);
-        SvxUndoRedoControl::RegisterControl(SID_REDO, pModule);
         XmlSecStatusBarControl::RegisterControl(SID_SIGNATURE, pModule);
 
         SmCmdBoxWrapper::RegisterChildWindow(true);

svx/util/svx.component
@@ -124,4 +124,8 @@
     constructor="com_sun_star_comp_svx_UnderlineToolBoxControl_get_implementation">
     <service name="com.sun.star.frame.ToolbarController"/>
   </implementation>
+  <implementation name="com.sun.star.comp.svx.UndoRedoToolBoxControl"
+    constructor="com_sun_star_comp_svx_UndoRedoToolBoxControl_get_implementation">
+    <service name="com.sun.star.frame.ToolbarController"/>
+  </implementation>
 </component>

sw/source/uibase/app/swmodule.cxx
@@ -281,8 +281,6 @@ void SwDLL::RegisterControls()
     svx::ParaFirstLineSpacingControl::RegisterControl(SID_ATTR_PARA_FIRSTLINESPACE, pMod);
 
     SvxClipBoardControl::RegisterControl(SID_PASTE, pMod );
-    SvxUndoRedoControl::RegisterControl(SID_UNDO, pMod );
-    SvxUndoRedoControl::RegisterControl(SID_REDO, pMod );
     svx::FormatPaintBrushToolBoxControl::RegisterControl(SID_FORMATPAINTBRUSH, pMod );
 
     SvxFillToolBoxControl::RegisterControl(SID_ATTR_FILL_STYLE, pMod );

commit 3c451d4c41014799b0c0585a66b464e7b57e8cc6

Fix undo and redo dropdowns in non-sfx2 modules
after commit 8d6e092ef852a3661ac8592a43ddb5a955ed6819
("rework SvxUndoRedoControl to be a PopupWindowController")
accidentally enabled them there, instead of the simple
buttons those modules used to have. Just implement the
necessary stuff, instead of hiding the dropdown again.

Change-Id: Ic93114f7f3cec8e96f3389ceb0d52552cde02a83
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88333
Tested-by: Jenkins
Reviewed-by: Maxim Monastirsky <momonasmon@gmail.com>

chart2/source/controller/main/CommandDispatchContainer.cxx
@@ -84,13 +84,16 @@ Reference< frame::XDispatch > CommandDispatchContainer::getDispatchForURL(
     {
         uno::Reference< frame::XModel > xModel( m_xModel );
 
-        if( xModel.is() && ( rURL.Path == "Undo" || rURL.Path == "Redo" ) )
+        if( xModel.is() && ( rURL.Path == "Undo" || rURL.Path == "Redo" ||
+                             rURL.Path == "GetUndoStrings" || rURL.Path == "GetRedoStrings" ) )
         {
             CommandDispatch * pDispatch = new UndoCommandDispatch( m_xContext, xModel );
             xResult.set( pDispatch );
             pDispatch->initialize();
             m_aCachedDispatches[ ".uno:Undo" ].set( xResult );
             m_aCachedDispatches[ ".uno:Redo" ].set( xResult );
+            m_aCachedDispatches[ ".uno:GetUndoStrings" ].set( xResult );
+            m_aCachedDispatches[ ".uno:GetRedoStrings" ].set( xResult );
             m_aToBeDisposedDispatches.push_back( xResult );
         }
         else if( xModel.is() && ( rURL.Path == "Context" || rURL.Path == "ModifiedStatus" ) )

chart2/source/controller/main/UndoCommandDispatch.cxx
@@ -64,23 +64,30 @@ void UndoCommandDispatch::fireStatusEvent(
     if( m_xUndoManager.is() )
     {
         const bool bFireAll = rURL.isEmpty();
-        uno::Any aUndoState, aRedoState;
+        uno::Any aUndoState, aRedoState, aUndoStrings, aRedoStrings;
         if( m_xUndoManager->isUndoPossible())
             aUndoState <<= SvtResId( STR_UNDO ) + m_xUndoManager->getCurrentUndoActionTitle();
         if( m_xUndoManager->isRedoPossible())
             aRedoState <<= SvtResId( STR_REDO ) + m_xUndoManager->getCurrentRedoActionTitle();
 
+        aUndoStrings <<= m_xUndoManager->getAllUndoActionTitles();
+        aRedoStrings <<= m_xUndoManager->getAllRedoActionTitles();
+
         if( bFireAll || rURL == ".uno:Undo" )
             fireStatusEventForURL( ".uno:Undo", aUndoState, m_xUndoManager->isUndoPossible(), xSingleListener );
         if( bFireAll || rURL == ".uno:Redo" )
             fireStatusEventForURL( ".uno:Redo", aRedoState, m_xUndoManager->isRedoPossible(), xSingleListener );
+        if( bFireAll || rURL == ".uno:GetUndoStrings" )
+            fireStatusEventForURL( ".uno:GetUndoStrings", aUndoStrings, true, xSingleListener );
+        if( bFireAll || rURL == ".uno:GetRedoStrings" )
+            fireStatusEventForURL( ".uno:GetRedoStrings", aRedoStrings, true, xSingleListener );
     }
 }
 
 // ____ XDispatch ____
 void SAL_CALL UndoCommandDispatch::dispatch(
     const util::URL& URL,
-    const Sequence< beans::PropertyValue >& /* Arguments */ )
+    const Sequence< beans::PropertyValue >& Arguments )
 {
     if( m_xUndoManager.is() )
     {
@@ -88,10 +95,17 @@ void SAL_CALL UndoCommandDispatch::dispatch(
         SolarMutexGuard aSolarGuard;
         try
         {
-            if ( URL.Path == "Undo" )
-                m_xUndoManager->undo();
-            else
-                m_xUndoManager->redo();
+            sal_Int16 nCount( 1 );
+            if ( Arguments.hasElements() && Arguments[0].Name == URL.Path )
+                Arguments[0].Value >>= nCount;
+
+            while ( nCount-- )
+            {
+                if ( URL.Path == "Undo" )
+                    m_xUndoManager->undo();
+                else
+                    m_xUndoManager->redo();
+            }
         }
         catch( const document::UndoFailedException& )
         {

dbaccess/source/ui/misc/singledoccontroller.cxx
@@ -120,6 +120,28 @@ namespace dbaui
                 }
                 break;
 
+            case SID_GETUNDOSTRINGS:
+            {
+                size_t nCount(GetUndoManager().GetUndoActionCount());
+                Sequence<OUString> aSeq(nCount);
+                for (size_t n = 0; n < nCount; ++n)
+                    aSeq[n] = GetUndoManager().GetUndoActionComment(n);
+                aReturn.aValue <<= aSeq;
+                aReturn.bEnabled = true;
+                break;
+            }
+
+            case SID_GETREDOSTRINGS:
+            {
+                size_t nCount(GetUndoManager().GetRedoActionCount());
+                Sequence<OUString> aSeq(nCount);
+                for (size_t n = 0; n < nCount; ++n)
+                    aSeq[n] = GetUndoManager().GetRedoActionComment(n);
+                aReturn.aValue <<= aSeq;
+                aReturn.bEnabled = true;
+                break;
+            }
+
             default:
                 aReturn = OSingleDocumentController_Base::GetState(_nId);
         }
@@ -130,16 +152,24 @@ namespace dbaui
         switch ( _nId )
         {
             case ID_BROWSER_UNDO:
-                GetUndoManager().Undo();
-                InvalidateFeature( ID_BROWSER_UNDO );
-                InvalidateFeature( ID_BROWSER_REDO );
-                break;
-
             case ID_BROWSER_REDO:
-                GetUndoManager().Redo();
+            {
+                sal_Int16 nCount(1);
+                if (_rArgs.hasElements() && _rArgs[0].Name != "KeyModifier")
+                    _rArgs[0].Value >>= nCount;
+
+                while (nCount--)
+                {
+                    if (_nId == ID_BROWSER_UNDO)
+                        GetUndoManager().Undo();
+                    else
+                        GetUndoManager().Redo();
+                }
+
                 InvalidateFeature( ID_BROWSER_UNDO );
                 InvalidateFeature( ID_BROWSER_REDO );
                 break;
+            }
 
             default:
                 OSingleDocumentController_Base::Execute( _nId, _rArgs );

dbaccess/source/ui/querydesign/JoinController.cxx
@@ -307,6 +307,8 @@ void OJoinController::describeSupportedFeatures()
     implDescribeSupportedFeature( ".uno:Undo",      ID_BROWSER_UNDO,    CommandGroup::EDIT );
     implDescribeSupportedFeature( ".uno:AddTable",  ID_BROWSER_ADDTABLE,CommandGroup::EDIT );
     implDescribeSupportedFeature( ".uno:EditDoc",   ID_BROWSER_EDITDOC, CommandGroup::EDIT );
+    implDescribeSupportedFeature( ".uno:GetUndoStrings", SID_GETUNDOSTRINGS );
+    implDescribeSupportedFeature( ".uno:GetRedoStrings", SID_GETREDOSTRINGS );
 }
 
 sal_Bool SAL_CALL OJoinController::suspend(sal_Bool _bSuspend)

dbaccess/source/ui/tabledesign/TableController.cxx
@@ -606,6 +606,8 @@ void OTableController::describeSupportedFeatures()
     implDescribeSupportedFeature( ".uno:SaveAs",        ID_BROWSER_SAVEASDOC,   CommandGroup::DOCUMENT );
     implDescribeSupportedFeature( ".uno:DBIndexDesign", SID_INDEXDESIGN,        CommandGroup::APPLICATION );
     implDescribeSupportedFeature( ".uno:EditDoc",       ID_BROWSER_EDITDOC,     CommandGroup::EDIT );
+    implDescribeSupportedFeature( ".uno:GetUndoStrings", SID_GETUNDOSTRINGS );
+    implDescribeSupportedFeature( ".uno:GetRedoStrings", SID_GETREDOSTRINGS );
 }
 
 void OTableController::impl_onModifyChanged()

reportdesign/source/ui/report/ReportController.cxx
@@ -453,6 +453,24 @@ FeatureState OReportController::GetState(sal_uInt16 _nId) const
                 }
             }
             break;
+        case SID_GETUNDOSTRINGS:
+        case SID_GETREDOSTRINGS:
+            {
+                size_t ( SfxUndoManager::*retrieveCount )( bool const ) const =
+                    ( _nId == SID_GETUNDOSTRINGS ) ? &SfxUndoManager::GetUndoActionCount : &SfxUndoManager::GetRedoActionCount;
+
+                OUString ( SfxUndoManager::*retrieveComment )( size_t, bool const ) const =
+                    ( _nId == SID_GETUNDOSTRINGS ) ? &SfxUndoManager::GetUndoActionComment : &SfxUndoManager::GetRedoActionComment;
+
+                SfxUndoManager& rUndoManager( getUndoManager() );
+                size_t nCount(( rUndoManager.*retrieveCount )( SfxUndoManager::TopLevel ));
+                Sequence<OUString> aSeq(nCount);
+                for (size_t n = 0; n < nCount; ++n)
+                    aSeq[n] = (rUndoManager.*retrieveComment)( n, SfxUndoManager::TopLevel );
+                aReturn.aValue <<= aSeq;
+                aReturn.bEnabled = true;
+            }
+            break;
         case SID_OBJECT_RESIZING:
         case SID_OBJECT_SMALLESTWIDTH:
         case SID_OBJECT_SMALLESTHEIGHT:
@@ -1016,9 +1034,13 @@ void OReportController::Execute(sal_uInt16 _nId, const Sequence< PropertyValue >
             const OXUndoEnvironment::OUndoMode aLock( m_aReportModel->GetUndoEnv() );
             bool ( SfxUndoManager::*doXDo )() =
                 ( _nId == SID_UNDO ) ? &SfxUndoManager::Undo : &SfxUndoManager::Redo;
-
             SfxUndoManager& rUndoManager( getUndoManager() );
-            (rUndoManager.*doXDo)();
+
+            sal_Int16 nCount(1);
+            if (aArgs.hasElements() && aArgs[0].Name != "KeyModifier")
+                aArgs[0].Value >>= nCount;
+            while (nCount--)
+                (rUndoManager.*doXDo)();
             InvalidateAll();
             if (m_xGroupsFloater && m_xGroupsFloater->getDialog()->get_visible())
                 m_xGroupsFloater->UpdateData();
@@ -2022,6 +2044,8 @@ void OReportController::describeSupportedFeatures()
     implDescribeSupportedFeature( ".uno:SelectAllEdits",                SID_SELECT_ALL_EDITS);
     implDescribeSupportedFeature( ".uno:CollapseSection",           SID_COLLAPSE_SECTION);
     implDescribeSupportedFeature( ".uno:ExpandSection",             SID_EXPAND_SECTION);
+    implDescribeSupportedFeature( ".uno:GetUndoStrings",            SID_GETUNDOSTRINGS);
+    implDescribeSupportedFeature( ".uno:GetRedoStrings",            SID_GETREDOSTRINGS);
 }
 
 void OReportController::impl_onModifyChanged()

svx/source/tbxctrls/lboxctrl.cxx
@@ -154,7 +154,7 @@ void SvxUndoRedoControl::initialize( const css::uno::Sequence< css::uno::Any >& 
 
     ToolBox* pToolBox = nullptr;
     sal_uInt16 nId = 0;
-    if (getToolboxId(nId, &pToolBox) && pToolBox->GetItemCommand(nId) == m_aCommandURL)
+    if (getToolboxId(nId, &pToolBox))
     {
         pToolBox->SetItemBits(nId, ToolBoxItemBits::DROPDOWN | pToolBox->GetItemBits(nId));
         aDefaultTooltip = pToolBox->GetQuickHelpText(nId);