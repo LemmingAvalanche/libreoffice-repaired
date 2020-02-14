LemmingAvalanche/libreoffice-repaired

commit ba21fdf3bd564bdcedfc4d445222d2c2e0801062

convert BmpConversion to scoped enum
and drop unused 4BIT_TRANS and 1BIT_MATRIX enumerators

Also fix a bug in x11::convertBitmapDepth where we were incorrectly
passing BmpConversion enumeratirs to ReduceColors

Change-Id: I903c6866750e46ee752e10a17c05fcaaf6b11242
Reviewed-on: https://gerrit.libreoffice.org/34062
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/bitmap.hxx
@@ -70,19 +70,17 @@ namespace o3tl
 
 #define BMP_COL_TRANS               Color( 252, 3, 251 )
 
-enum BmpConversion
+enum class BmpConversion
 {
-    BMP_CONVERSION_NONE = 0,
-    BMP_CONVERSION_1BIT_THRESHOLD = 1,
-    BMP_CONVERSION_1BIT_MATRIX = 2,
-    BMP_CONVERSION_4BIT_GREYS = 3,
-    BMP_CONVERSION_4BIT_COLORS = 4,
-    BMP_CONVERSION_8BIT_GREYS = 5,
-    BMP_CONVERSION_8BIT_COLORS = 6,
-    BMP_CONVERSION_24BIT = 7,
-    BMP_CONVERSION_4BIT_TRANS = 8,
-    BMP_CONVERSION_8BIT_TRANS = 9,
-    BMP_CONVERSION_GHOSTED = 10
+    NNONE,
+    N1BitThreshold,
+    N4BitGreys,
+    N4BitColors,
+    N8BitGreys,
+    N8BitColors,
+    N24Bit,
+    N8BitTrans,
+    Ghosted
 };
 
 enum class BmpCombine

vcl/source/gdi/bitmap3.cxx
@@ -263,19 +263,15 @@ bool Bitmap::Convert( BmpConversion eConversion )
 
     switch( eConversion )
     {
-        case BMP_CONVERSION_1BIT_THRESHOLD:
+        case BmpConversion::N1BitThreshold:
             bRet = ImplMakeMono( 128 );
         break;
 
-        case BMP_CONVERSION_1BIT_MATRIX:
-            bRet = ImplMakeMonoDither();
-        break;
-
-        case BMP_CONVERSION_4BIT_GREYS:
+        case BmpConversion::N4BitGreys:
             bRet = ImplMakeGreyscales( 16 );
         break;
 
-        case BMP_CONVERSION_4BIT_COLORS:
+        case BmpConversion::N4BitColors:
         {
             if( nBitCount < 4 )
                 bRet = ImplConvertUp( 4 );
@@ -286,22 +282,11 @@ bool Bitmap::Convert( BmpConversion eConversion )
         }
         break;
 
-        case BMP_CONVERSION_4BIT_TRANS:
-        {
-            Color aTrans( BMP_COL_TRANS );
-
-            if( nBitCount < 4 )
-                bRet = ImplConvertUp( 4, &aTrans );
-            else
-                bRet = ImplConvertDown( 4, &aTrans );
-        }
-        break;
-
-        case BMP_CONVERSION_8BIT_GREYS:
+        case BmpConversion::N8BitGreys:
             bRet = ImplMakeGreyscales( 256 );
         break;
 
-        case BMP_CONVERSION_8BIT_COLORS:
+        case BmpConversion::N8BitColors:
         {
             if( nBitCount < 8 )
                 bRet = ImplConvertUp( 8 );
@@ -312,7 +297,7 @@ bool Bitmap::Convert( BmpConversion eConversion )
         }
         break;
 
-        case BMP_CONVERSION_8BIT_TRANS:
+        case BmpConversion::N8BitTrans:
         {
             Color aTrans( BMP_COL_TRANS );
 
@@ -323,7 +308,7 @@ bool Bitmap::Convert( BmpConversion eConversion )
         }
         break;
 
-        case BMP_CONVERSION_24BIT:
+        case BmpConversion::N24Bit:
         {
             if( nBitCount < 24 )
                 bRet = ImplConvertUp( 24 );
@@ -332,7 +317,7 @@ bool Bitmap::Convert( BmpConversion eConversion )
         }
         break;
 
-        case BMP_CONVERSION_GHOSTED:
+        case BmpConversion::Ghosted:
             bRet = ImplConvertGhosted();
         break;
 
@@ -1018,18 +1003,18 @@ void Bitmap::ImplAdaptBitCount(Bitmap& rNew) const
         {
             case 1:
             {
-                rNew.Convert(BMP_CONVERSION_1BIT_THRESHOLD);
+                rNew.Convert(BmpConversion::N1BitThreshold);
                 break;
             }
             case 4:
             {
                 if(HasGreyPalette())
                 {
-                    rNew.Convert(BMP_CONVERSION_4BIT_GREYS);
+                    rNew.Convert(BmpConversion::N4BitGreys);
                 }
                 else
                 {
-                    rNew.Convert(BMP_CONVERSION_4BIT_COLORS);
+                    rNew.Convert(BmpConversion::N4BitColors);
                 }
                 break;
             }
@@ -1037,17 +1022,17 @@ void Bitmap::ImplAdaptBitCount(Bitmap& rNew) const
             {
                 if(HasGreyPalette())
                 {
-                    rNew.Convert(BMP_CONVERSION_8BIT_GREYS);
+                    rNew.Convert(BmpConversion::N8BitGreys);
                 }
                 else
                 {
-                    rNew.Convert(BMP_CONVERSION_8BIT_COLORS);
+                    rNew.Convert(BmpConversion::N8BitColors);
                 }
                 break;
             }
             case 24:
             {
-                rNew.Convert(BMP_CONVERSION_24BIT);
+                rNew.Convert(BmpConversion::N24Bit);
                 break;
             }
             default:

drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -457,7 +457,7 @@ namespace drawinglayer
                                     // a bitmap in gray or Black/White (!)
                                     if(aBitmapEx.GetBitCount() < 24)
                                     {
-                                        aBitmapEx.Convert(BMP_CONVERSION_24BIT);
+                                        aBitmapEx.Convert(BmpConversion::N24Bit);
                                     }
 
                                     aBitmapEx.Scale(aNeededBitmapSizePixel, BmpScaleFlag::Interpolate);

filter/source/graphicfilter/egif/egif.cxx
@@ -252,17 +252,17 @@ bool GIFWriter::CreateAccess( const BitmapEx& rBmpEx )
 
         if( !!aMask )
         {
-            if( aAccBmp.Convert( BMP_CONVERSION_8BIT_TRANS ) )
+            if( aAccBmp.Convert( BmpConversion::N8BitTrans ) )
             {
-                aMask.Convert( BMP_CONVERSION_1BIT_THRESHOLD );
+                aMask.Convert( BmpConversion::N1BitThreshold );
                 aAccBmp.Replace( aMask, BMP_COL_TRANS );
                 bTransparent = true;
             }
             else
-                aAccBmp.Convert( BMP_CONVERSION_8BIT_COLORS );
+                aAccBmp.Convert( BmpConversion::N8BitColors );
         }
         else
-            aAccBmp.Convert( BMP_CONVERSION_8BIT_COLORS );
+            aAccBmp.Convert( BmpConversion::N8BitColors );
 
         m_pAcc = aAccBmp.AcquireReadAccess();
 

filter/source/graphicfilter/eps/eps.cxx
@@ -353,7 +353,7 @@ bool PSWriter::WritePS( const Graphic& rGraphic, SvStream& rTargetStream, Filter
         if ( mbGrayScale )
         {
             BitmapEx aTempBitmapEx( rGraphic.GetBitmapEx() );
-            aTempBitmapEx.Convert( BMP_CONVERSION_8BIT_GREYS );
+            aTempBitmapEx.Convert( BmpConversion::N8BitGreys );
             nErrCode = GraphicConverter::Export( rTargetStream, aTempBitmapEx, ConvertDataFormat::TIF ) ;
         }
         else
@@ -487,7 +487,7 @@ void PSWriter::ImplWriteProlog( const Graphic* pPreview )
         Size aSizeBitmap( ( aSizePoint.Width() + 7 ) & ~7, aSizePoint.Height() );
         Bitmap aTmpBitmap( pPreview->GetBitmap() );
         aTmpBitmap.Scale( aSizeBitmap, BmpScaleFlag::BestQuality );
-        aTmpBitmap.Convert( BMP_CONVERSION_1BIT_THRESHOLD );
+        aTmpBitmap.Convert( BmpConversion::N1BitThreshold );
         BitmapReadAccess* pAcc = aTmpBitmap.AcquireReadAccess();
         if ( pAcc )
         {
@@ -795,7 +795,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
             {
                 Bitmap aBitmap = static_cast<const MetaBmpAction*>(pMA)->GetBitmap();
                 if ( mbGrayScale )
-                    aBitmap.Convert( BMP_CONVERSION_8BIT_GREYS );
+                    aBitmap.Convert( BmpConversion::N8BitGreys );
                 Point aPoint = static_cast<const MetaBmpAction*>(pMA)->GetPoint();
                 Size aSize( rVDev.PixelToLogic( aBitmap.GetSizePixel() ) );
                 ImplBmp( &aBitmap, nullptr, aPoint, aSize.Width(), aSize.Height() );
@@ -806,7 +806,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
             {
                 Bitmap aBitmap = static_cast<const MetaBmpScaleAction*>(pMA)->GetBitmap();
                 if ( mbGrayScale )
-                    aBitmap.Convert( BMP_CONVERSION_8BIT_GREYS );
+                    aBitmap.Convert( BmpConversion::N8BitGreys );
                 Point aPoint = static_cast<const MetaBmpScaleAction*>(pMA)->GetPoint();
                 Size aSize = static_cast<const MetaBmpScaleAction*>(pMA)->GetSize();
                 ImplBmp( &aBitmap, nullptr, aPoint, aSize.Width(), aSize.Height() );
@@ -819,7 +819,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                 aBitmap.Crop( Rectangle( static_cast<const MetaBmpScalePartAction*>(pMA)->GetSrcPoint(),
                     static_cast<const MetaBmpScalePartAction*>(pMA)->GetSrcSize() ) );
                 if ( mbGrayScale )
-                    aBitmap.Convert( BMP_CONVERSION_8BIT_GREYS );
+                    aBitmap.Convert( BmpConversion::N8BitGreys );
                 Point aPoint = static_cast<const MetaBmpScalePartAction*>(pMA)->GetDestPoint();
                 Size aSize = static_cast<const MetaBmpScalePartAction*>(pMA)->GetDestSize();
                 ImplBmp( &aBitmap, nullptr, aPoint, aSize.Width(), aSize.Height() );
@@ -831,7 +831,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                 BitmapEx aBitmapEx( static_cast<MetaBmpExAction*>(pMA)->GetBitmapEx() );
                 Bitmap aBitmap( aBitmapEx.GetBitmap() );
                 if ( mbGrayScale )
-                    aBitmap.Convert( BMP_CONVERSION_8BIT_GREYS );
+                    aBitmap.Convert( BmpConversion::N8BitGreys );
                 Bitmap aMask( aBitmapEx.GetMask() );
                 Point aPoint( static_cast<const MetaBmpExAction*>(pMA)->GetPoint() );
                 Size aSize( rVDev.PixelToLogic( aBitmap.GetSizePixel() ) );
@@ -844,7 +844,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                 BitmapEx aBitmapEx( static_cast<MetaBmpExScaleAction*>(pMA)->GetBitmapEx() );
                 Bitmap aBitmap( aBitmapEx.GetBitmap() );
                 if ( mbGrayScale )
-                    aBitmap.Convert( BMP_CONVERSION_8BIT_GREYS );
+                    aBitmap.Convert( BmpConversion::N8BitGreys );
                 Bitmap aMask( aBitmapEx.GetMask() );
                 Point aPoint = static_cast<const MetaBmpExScaleAction*>(pMA)->GetPoint();
                 Size aSize( static_cast<const MetaBmpExScaleAction*>(pMA)->GetSize() );
@@ -859,7 +859,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                     static_cast<const MetaBmpExScalePartAction*>(pMA)->GetSrcSize() ) );
                 Bitmap      aBitmap( aBitmapEx.GetBitmap() );
                 if ( mbGrayScale )
-                    aBitmap.Convert( BMP_CONVERSION_8BIT_GREYS );
+                    aBitmap.Convert( BmpConversion::N8BitGreys );
                 Bitmap      aMask( aBitmapEx.GetMask() );
                 Point aPoint = static_cast<const MetaBmpExScalePartAction*>(pMA)->GetDestPoint();
                 Size aSize = static_cast<const MetaBmpExScalePartAction*>(pMA)->GetDestSize();

filter/source/msfilter/msdffimp.cxx
@@ -3916,9 +3916,9 @@ SdrObject* SvxMSDffManager::ImportGraphic( SvStream& rSt, SfxItemSet& rSet, cons
                             if ( nBrightness || nContrast || ( nGamma != 0x10000 ) )
                                 aBitmapEx.Adjust( nBrightness, (sal_Int16)nContrast, 0, 0, 0, (double)nGamma / 0x10000, false, true );
                             if ( eDrawMode == GRAPHICDRAWMODE_GREYS )
-                                aBitmapEx.Convert( BMP_CONVERSION_8BIT_GREYS );
+                                aBitmapEx.Convert( BmpConversion::N8BitGreys );
                             else if ( eDrawMode == GRAPHICDRAWMODE_MONO )
-                                aBitmapEx.Convert( BMP_CONVERSION_1BIT_THRESHOLD );
+                                aBitmapEx.Convert( BmpConversion::N1BitThreshold );
                             aGraf = aBitmapEx;
 
                         }

include/vcl/gdimtf.hxx
@@ -193,7 +193,7 @@ public:
     /// Creates an antialiased thumbnail, with maximum width or height of nMaximumExtent.
     bool            CreateThumbnail(BitmapEx& rBitmapEx,
                                     sal_uInt32 nMaximumExtent = 256,
-                                    BmpConversion nColorConversion = BMP_CONVERSION_24BIT,
+                                    BmpConversion nColorConversion = BmpConversion::N24Bit,
                                     BmpScaleFlag nScaleFlag = BmpScaleFlag::BestQuality) const;
 
     void            UseCanvas( bool _bUseCanvas );

sc/source/filter/excel/xeescher.cxx
@@ -401,7 +401,7 @@ void XclExpImgData::Save( XclExpStream& rStrm )
 {
     Bitmap aBmp = maGraphic.GetBitmap();
     if( aBmp.GetBitCount() != 24 )
-        aBmp.Convert( BMP_CONVERSION_24BIT );
+        aBmp.Convert( BmpConversion::N24Bit );
 
     Bitmap::ScopedReadAccess pAccess(aBmp);
     if( pAccess )

commit 1f593b8edc2be81efe3111954ae07154680fe58c

tdf#130573 labels exchanged in export to BMP
In the commit below, I removed the 1-bit dithered output,
so restore it.

regression from
    commit ba21fdf3bd564bdcedfc4d445222d2c2e0801062
    Date:   Wed Feb 8 16:18:32 2017 +0200
    convert BmpConversion to scoped enum

Note that this bug has been around since LO5.4
which means that anyone who has adjusted their
setting in
   officecfg/registry/schema/org/openoffice/Office/Common.xcs
   with key BMP
runs the risk of having that setting now revert to its
prior (documented) meaning.

Change-Id: Ibbda8aefbac261ff37ffab7223714f5d0343c692
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88528
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/BitmapMonochromeMatrixFilter.hxx
@@ -1,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_INCLUDE_VCL_BITMAPMONOCHROMEMATRIXFILTER_HXX
+#define INCLUDED_INCLUDE_VCL_BITMAPMONOCHROMEMATRIXFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+class VCL_DLLPUBLIC BitmapMonochromeMatrixFilter final : public BitmapFilter
+{
+public:
+    /** Convert to monochrome (1-bit) bitmap using dithering
+     */
+    BitmapMonochromeMatrixFilter() {}
+
+    virtual BitmapEx execute(BitmapEx const& rBitmapEx) const override;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/vcl/bitmap.hxx
@@ -66,6 +66,7 @@ enum class BmpConversion
 {
     NNONE,
     N1BitThreshold,
+    N1BitMatrix, // aka Dithered, used in export to bitmap
     N4BitGreys,
     N4BitColors,
     N8BitGreys,

vcl/Library_vcl.mk
@@ -333,6 +333,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/bitmapfilter \
     vcl/source/bitmap/BitmapAlphaClampFilter \
     vcl/source/bitmap/BitmapMonochromeFilter \
+    vcl/source/bitmap/BitmapMonochromeMatrixFilter \
     vcl/source/bitmap/BitmapSmoothenFilter \
     vcl/source/bitmap/BitmapLightenFilter \
     vcl/source/bitmap/BitmapDisabledImageFilter \

vcl/source/bitmap/BitmapMonochromeMatrixFilter.cxx
@@ -1,0 +1,142 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <vcl/bitmap.hxx>
+#include <vcl/bitmapex.hxx>
+#include <vcl/BitmapMonochromeMatrixFilter.hxx>
+#include <vcl/bitmapaccess.hxx>
+
+#include <bitmapwriteaccess.hxx>
+
+static void ImplCreateDitherMatrix(sal_uInt8 (*pDitherMatrix)[16][16])
+{
+    const double fVal = 3.125;
+    const double fVal16 = fVal / 16.;
+    const double fValScale = 254.;
+    sal_uInt16 pMtx[16][16];
+    sal_uInt16 nMax = 0;
+    static const sal_uInt8 pMagic[4][4] = { {
+                                                0,
+                                                14,
+                                                3,
+                                                13,
+                                            },
+                                            {
+                                                11,
+                                                5,
+                                                8,
+                                                6,
+                                            },
+                                            {
+                                                12,
+                                                2,
+                                                15,
+                                                1,
+                                            },
+                                            { 7, 9, 4, 10 } };
+
+    // Build MagicSquare
+    for (long i = 0; i < 4; i++)
+        for (long j = 0; j < 4; j++)
+            for (long k = 0; k < 4; k++)
+                for (long l = 0; l < 4; l++)
+                {
+                    pMtx[(k << 2) + i][(l << 2) + j] = static_cast<sal_uInt16>(
+                        0.5 + pMagic[i][j] * fVal + pMagic[k][l] * fVal16);
+                    nMax = std::max(pMtx[(k << 2) + i][(l << 2) + j], nMax);
+                }
+
+    // Scale to interval [0;254]
+    double tmp = fValScale / nMax;
+    for (long i = 0; i < 16; i++)
+        for (long j = 0; j < 16; j++)
+            (*pDitherMatrix)[i][j] = static_cast<sal_uInt8>(tmp * pMtx[i][j]);
+}
+
+BitmapEx BitmapMonochromeMatrixFilter::execute(BitmapEx const& aBitmapEx) const
+{
+    Bitmap aBitmap = aBitmapEx.GetBitmap();
+    Bitmap::ScopedReadAccess pReadAcc(aBitmap);
+    bool bRet = false;
+
+    if (pReadAcc)
+    {
+        Bitmap aNewBmp(aBitmap.GetSizePixel(), 1);
+        BitmapScopedWriteAccess pWriteAcc(aNewBmp);
+
+        if (pWriteAcc)
+        {
+            const BitmapColor aBlack(pWriteAcc->GetBestMatchingColor(COL_BLACK));
+            const BitmapColor aWhite(pWriteAcc->GetBestMatchingColor(COL_WHITE));
+            const long nWidth = pWriteAcc->Width();
+            const long nHeight = pWriteAcc->Height();
+            sal_uInt8 pDitherMatrix[16][16];
+
+            ImplCreateDitherMatrix(&pDitherMatrix);
+
+            if (pReadAcc->HasPalette())
+            {
+                for (long nY = 0; nY < nHeight; nY++)
+                {
+                    for (long nX = 0, nModY = nY % 16; nX < nWidth; nX++)
+                    {
+                        const sal_uInt8 cIndex = pReadAcc->GetPixelIndex(nY, nX);
+                        if (pReadAcc->GetPaletteColor(cIndex).GetLuminance()
+                            > pDitherMatrix[nModY][nX % 16])
+                        {
+                            pWriteAcc->SetPixel(nY, nX, aWhite);
+                        }
+                        else
+                            pWriteAcc->SetPixel(nY, nX, aBlack);
+                    }
+                }
+            }
+            else
+            {
+                for (long nY = 0; nY < nHeight; nY++)
+                {
+                    for (long nX = 0, nModY = nY % 16; nX < nWidth; nX++)
+                    {
+                        if (pReadAcc->GetPixel(nY, nX).GetLuminance()
+                            > pDitherMatrix[nModY][nX % 16])
+                        {
+                            pWriteAcc->SetPixel(nY, nX, aWhite);
+                        }
+                        else
+                            pWriteAcc->SetPixel(nY, nX, aBlack);
+                    }
+                }
+            }
+
+            pWriteAcc.reset();
+            bRet = true;
+        }
+
+        pReadAcc.reset();
+
+        if (bRet)
+        {
+            const MapMode aMap(aBitmap.GetPrefMapMode());
+            const Size aSize(aBitmap.GetPrefSize());
+
+            aBitmap = aNewBmp;
+
+            aBitmap.SetPrefMapMode(aMap);
+            aBitmap.SetPrefSize(aSize);
+        }
+    }
+
+    if (bRet)
+        return BitmapEx(aBitmap);
+
+    return BitmapEx();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/gdi/bitmap3.cxx
@@ -30,6 +30,7 @@
 #include <vcl/opengl/OpenGLHelper.hxx>
 #endif
 #include <vcl/BitmapMonochromeFilter.hxx>
+#include <vcl/BitmapMonochromeMatrixFilter.hxx>
 
 #include <BitmapScaleSuperFilter.hxx>
 #include <BitmapScaleConvolutionFilter.hxx>
@@ -251,6 +252,14 @@ bool Bitmap::Convert( BmpConversion eConversion )
         {
             BitmapEx aBmpEx(*this);
             bRet = BitmapFilter::Filter(aBmpEx, BitmapMonochromeFilter(128));
+            *this = aBmpEx.GetBitmap();
+        }
+        break;
+
+        case BmpConversion::N1BitMatrix:
+        {
+            BitmapEx aBmpEx(*this);
+            bRet = BitmapFilter::Filter(aBmpEx, BitmapMonochromeMatrixFilter());
             *this = aBmpEx.GetBitmap();
         }
         break;