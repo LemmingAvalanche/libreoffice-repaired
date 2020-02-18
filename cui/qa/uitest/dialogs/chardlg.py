LemmingAvalanche/libreoffice-repaired

commit c69f8f709f37aed7596b8307696a80572beff71e

cui: add UI for semi-transparent shape text
- Make font color only work with the RGB color, otherwise the preview
  would be white for e.g. half-transparent red.

- Add label and widget to see already set transparency.

- Add a flag to show these only for Draw/Impress and leave Writer/Calc
  unchanged.

- Update returned item set to contain transparency in case the widget
  changes.

Change-Id: If77771076ff4b10a4a5d468a6583809a94deb57e
Reviewed-on: https://gerrit.libreoffice.org/83520
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

cui/source/tabpages/chardlg.cxx
@@ -1314,6 +1314,9 @@ SvxCharEffectsPage::SvxCharEffectsPage(weld::Container* pPage, weld::DialogContr
     , m_bUnderlineColorDisabled(false)
     , m_xFontColorFT(m_xBuilder->weld_label("fontcolorft"))
     , m_xFontColorLB(new ColorListBox(m_xBuilder->weld_menu_button("fontcolorlb"), pController->getDialog()))
+    , m_xFontTransparencyFT(m_xBuilder->weld_label("fonttransparencyft"))
+    , m_xFontTransparencyMtr(
+          m_xBuilder->weld_metric_spin_button("fonttransparencymtr", FieldUnit::PERCENT))
     , m_xEffectsFT(m_xBuilder->weld_label("effectsft"))
     , m_xEffectsLB(m_xBuilder->weld_combo_box("effectslb"))
     , m_xReliefFT(m_xBuilder->weld_label("reliefft"))
@@ -1365,6 +1368,7 @@ SvxCharEffectsPage::~SvxCharEffectsPage()
 {
     m_xUnderlineColorLB.reset();
     m_xOverlineColorLB.reset();
+    m_xFontTransparencyMtr.reset();
     m_xFontColorLB.reset();
 }
 
@@ -1388,6 +1392,8 @@ void SvxCharEffectsPage::Initialize()
     }
 
     m_xFontColorLB->SetSelectHdl(LINK(this, SvxCharEffectsPage, ColorBoxSelectHdl_Impl));
+    m_xFontTransparencyMtr->connect_value_changed(
+        LINK(this, SvxCharEffectsPage, ModifyFontTransparencyHdl_Impl));
 
     // handler
     Link<weld::ComboBox&,void> aLink = LINK( this, SvxCharEffectsPage, SelectListBoxHdl_Impl );
@@ -1553,7 +1559,19 @@ void SvxCharEffectsPage::ResetColor_Impl( const SfxItemSet& rSet )
 
             m_aPreviewWin.Invalidate();
 
-            m_xFontColorLB->SelectEntry(aColor);
+            Color aRGBColor = aColor;
+            if (aRGBColor.GetTransparency())
+            {
+                aRGBColor.SetTransparency(0);
+            }
+            m_xFontColorLB->SelectEntry(aRGBColor);
+
+            if (m_xFontTransparencyMtr->get_visible() && aColor != COL_AUTO)
+            {
+                double fTransparency = aColor.GetTransparency() * 100.0 / 255;
+                m_xFontTransparencyMtr->set_value(basegfx::fround(fTransparency),
+                                                  FieldUnit::PERCENT);
+            }
 
             m_aOrigFontColor = aColor;
             m_bOrigFontColor = true;
@@ -1574,6 +1592,14 @@ bool SvxCharEffectsPage::FillItemSetColor_Impl( SfxItemSet& rSet )
     if (bChanged)
     {
         aSelectedColor = m_xFontColorLB->GetSelectEntryColor();
+
+        if (m_xFontTransparencyMtr->get_value_changed_from_saved())
+        {
+            double fTransparency
+                = m_xFontTransparencyMtr->get_value(FieldUnit::PERCENT) * 255.0 / 100;
+            aSelectedColor.SetTransparency(static_cast<sal_uInt8>(basegfx::fround(fTransparency)));
+        }
+
         if (m_bOrigFontColor)
             bChanged = aSelectedColor != m_aOrigFontColor;
         if (m_bEnableNoneFontColor && bChanged && aSelectedColor == COL_NONE_COLOR)
@@ -1645,6 +1671,11 @@ IMPL_LINK(SvxCharEffectsPage, ColorBoxSelectHdl_Impl, ColorListBox&, rBox, void)
     if (m_xFontColorLB.get() == &rBox)
         m_bNewFontColor = true;
     UpdatePreview_Impl();
+}
+
+IMPL_LINK_NOARG(SvxCharEffectsPage, ModifyFontTransparencyHdl_Impl, weld::MetricSpinButton&, void)
+{
+    m_bNewFontColor = true;
 }
 
 DeactivateRC SvxCharEffectsPage::DeactivatePage( SfxItemSet* _pSet )
@@ -2049,6 +2080,7 @@ void SvxCharEffectsPage::ChangesApplied()
     m_xShadowBtn->save_state();
     m_xBlinkingBtn->save_state();
     m_xHiddenBtn->save_state();
+    m_xFontTransparencyMtr->save_value();
 }
 
 bool SvxCharEffectsPage::FillItemSet( SfxItemSet* rSet )
@@ -2412,6 +2444,12 @@ void SvxCharEffectsPage::PageCreated(const SfxAllItemSet& aSet)
         if ( ( nFlags & SVX_PREVIEW_CHARACTER ) == SVX_PREVIEW_CHARACTER )
             // the writer uses SID_ATTR_BRUSH as font background
             m_bPreviewBackgroundToCharacter = true;
+        if ((nFlags & SVX_ENABLE_CHAR_TRANSPARENCY) != SVX_ENABLE_CHAR_TRANSPARENCY)
+        {
+            // Only show these in case client code explicitly wants this.
+            m_xFontTransparencyFT->hide();
+            m_xFontTransparencyMtr->hide();
+        }
     }
 }
 

cui/qa/uitest/dialogs/chardlg.py
@@ -1,0 +1,49 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.framework import UITestCase
+from uitest.uihelper.common import select_pos
+
+# Test for cui/source/tabpages/chardlg.cxx.
+class Test(UITestCase):
+
+    def testSvxCharEffectsPage(self):
+        # Start Impress.
+        self.ui_test.create_doc_in_start_center("impress")
+        template = self.xUITest.getTopFocusWindow()
+        self.ui_test.close_dialog_through_button(template.getChild("cancel"))
+        doc = self.xUITest.getTopFocusWindow()
+        editWin = doc.getChild("impress_win")
+        # Select the title shape.
+        editWin.executeAction("TYPE", mkPropertyValues({"KEYCODE": "TAB"}))
+        editWin.executeAction("TYPE", mkPropertyValues({"TEXT": "t"}))
+        self.xUITest.executeCommand(".uno:SelectAll")
+
+        # Now use Format -> Character.
+        self.ui_test.execute_dialog_through_command(".uno:FontDialog")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xTabs = xDialog.getChild("tabcontrol")
+        # Select RID_SVXPAGE_CHAR_EFFECTS.
+        select_pos(xTabs, "1")
+        xFontTransparency = xDialog.getChild("fonttransparencymtr")
+        for _ in range(5):
+            xFontTransparency.executeAction("UP", tuple())
+        self.ui_test.close_dialog_through_button(xDialog.getChild("ok"))
+
+        # Verify the result.
+        component = self.ui_test.get_component()
+        drawPage = component.getDrawPages().getByIndex(0)
+        shape = drawPage.getByIndex(0)
+
+        # Without the accompanying fix in place, this test would have failed with:
+        # AssertionError: 100 != 5
+        # i.e. the dialog did not set transparency to 5%, instead it left the character color at
+        # COL_AUTO.
+        self.assertEqual(shape.CharTransparence, 5)
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:

cui/source/inc/chardlg.hxx
@@ -170,6 +170,8 @@ private:
 
     std::unique_ptr<weld::Label> m_xFontColorFT;
     std::unique_ptr<ColorListBox> m_xFontColorLB;
+    std::unique_ptr<weld::Label> m_xFontTransparencyFT;
+    std::unique_ptr<weld::MetricSpinButton> m_xFontTransparencyMtr;
     std::unique_ptr<weld::Label> m_xEffectsFT;
     std::unique_ptr<weld::ComboBox> m_xEffectsLB;
     std::unique_ptr<weld::Label> m_xReliefFT;
@@ -205,6 +207,7 @@ private:
     DECL_LINK(CbClickHdl_Impl, weld::ToggleButton&, void);
     DECL_LINK(TristClickHdl_Impl, weld::ToggleButton&, void);
     DECL_LINK(ColorBoxSelectHdl_Impl, ColorListBox&, void);
+    DECL_LINK(ModifyFontTransparencyHdl_Impl, weld::MetricSpinButton&, void);
 
 public:
     SvxCharEffectsPage(weld::Container* pPage, weld::DialogController* pController, const SfxItemSet& rSet);

cui/uiconfig/ui/effectspage.ui
@@ -585,6 +585,31 @@
                   </packing>
                 </child>
                 <child>
+                  <object class="GtkLabel" id="fonttransparencyft">
+                    <property name="visible">True</property>
+                    <property name="can_focus">False</property>
+                    <property name="label" translatable="yes" context="effectspage|fonttransparencyft">_Transparency:</property>
+                    <property name="use_underline">True</property>
+                    <property name="mnemonic_widget">fonttransparencymtr</property>
+                  </object>
+                  <packing>
+                    <property name="left_attach">0</property>
+                    <property name="top_attach">2</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkSpinButton" id="fonttransparencymtr">
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="activates_default">True</property>
+                    <property name="adjustment">adjustmentPercent</property>
+                  </object>
+                  <packing>
+                    <property name="left_attach">1</property>
+                    <property name="top_attach">2</property>
+                  </packing>
+                </child>
+                <child>
                   <placeholder/>
                 </child>
               </object>
@@ -635,5 +660,10 @@
       <widget name="strikeoutlb"/>
       <widget name="fontcolorlb"/>
     </widgets>
+  </object>
+  <object class="GtkAdjustment" id="adjustmentPercent">
+    <property name="upper">100</property>
+    <property name="step_increment">1</property>
+    <property name="page_increment">10</property>
   </object>
 </interface>

include/svx/flagsdef.hxx
@@ -67,6 +67,7 @@ namespace o3tl
 #define SVX_RELATIVE_MODE       0x02
 // flags for SvxCharEffectsPage
 #define SVX_ENABLE_FLASH    0x04
+#define SVX_ENABLE_CHAR_TRANSPARENCY 0x08
 
 
 // Default values for Number Format Category List and Preview

sd/source/ui/dlg/dlgchar.cxx
@@ -56,6 +56,8 @@ void SdCharDlg::PageCreated(const OString& rId, SfxTabPage &rPage)
     }
     else if (rId == "RID_SVXPAGE_CHAR_EFFECTS")
     {
+        // Opt in for character transparency.
+        aSet.Put(SfxUInt32Item(SID_FLAG_TYPE, SVX_ENABLE_CHAR_TRANSPARENCY));
         rPage.PageCreated(aSet);
     }
     else if (rId == "RID_SVXPAGE_BACKGROUND")

commit eeee770c173cc1486c6d5dee185b3eac7d84d63f

tdf#130384 cui: fix automatic color showing up as white in the char dlg
Regression from commit c69f8f709f37aed7596b8307696a80572beff71e (cui:
add UI for semi-transparent shape text, 2019-11-22), the problem was
that COL_AUTO has the alpha channel set to 0xff, and we should not clear
that, otherwise the color list box will present the automatic color as
white.

Change-Id: I6eae788606b003eec014c0c4a22b40053ff534d5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88884
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

cui/source/tabpages/chardlg.cxx
@@ -1563,7 +1563,7 @@ void SvxCharEffectsPage::ResetColor_Impl( const SfxItemSet& rSet )
             m_aPreviewWin.Invalidate();
 
             Color aRGBColor = aColor;
-            if (aRGBColor.GetTransparency())
+            if (aRGBColor.GetTransparency() && aColor != COL_AUTO)
             {
                 aRGBColor.SetTransparency(0);
             }

cui/qa/uitest/dialogs/chardlg.py
@@ -79,4 +79,25 @@ class Test(UITestCase):
         self.assertEqual(paragraph.CharTransparence, 5)
         self.ui_test.close_doc()
 
+    def testSvxCharEffectsPageWriterAutomatic(self):
+        # Start Writer.
+        self.ui_test.create_doc_in_start_center("writer")
+        doc = self.xUITest.getTopFocusWindow()
+        editWin = doc.getChild("writer_edit")
+
+        # Use Format -> Character.
+        self.ui_test.execute_dialog_through_command(".uno:FontDialog")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xTabs = xDialog.getChild("tabcontrol")
+        # Select RID_SVXPAGE_CHAR_EFFECTS.
+        select_pos(xTabs, "1")
+        xFontColorLB = xDialog.getChild("fontcolorlb")
+        # Without the accompanying fix in place, this test would have failed with:
+        # AssertionError: 'White' != 'Automatic'
+        # i.e. the auto color lost its alpha component and appeared as white.
+        self.assertEqual(get_state_as_dict(xFontColorLB)["Text"], "Automatic")
+
+        self.ui_test.close_dialog_through_button(xDialog.getChild("ok"))
+        self.ui_test.close_doc()
+
 # vim: set shiftwidth=4 softtabstop=4 expandtab: