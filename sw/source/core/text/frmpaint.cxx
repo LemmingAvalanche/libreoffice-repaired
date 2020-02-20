LemmingAvalanche/libreoffice-repaired

commit 548590426790b6e5318ee0f9f4a5d648ca272449

sw: replace confusing nVirtPageNum that is actually physical page
... in SwExtraPainter::SwExtraPainter().

Change-Id: Iac8a849b5fab25c80e81c61b267100a3fc3e2d26
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88979
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/text/frmpaint.cxx
@@ -112,7 +112,7 @@ SwExtraPainter::SwExtraPainter( const SwTextFrame *pFrame, SwViewShell *pVwSh,
         if( m_aRect.Bottom() > nBottom )
             m_aRect.Bottom( nBottom );
     }
-    int nVirtPageNum = 0;
+    bool isRightPage(false);
     if( bLineNum )
     {
         /* Initializes the Members necessary for line numbering:
@@ -138,13 +138,13 @@ SwExtraPainter::SwExtraPainter( const SwTextFrame *pFrame, SwViewShell *pVwSh,
         {
             if( pFrame->FindPageFrame()->OnRightPage() )
             {
-                nVirtPageNum = 1;
+                isRightPage = true;
                 ePos = ePos == LINENUMBER_POS_INSIDE ?
                         LINENUMBER_POS_LEFT : LINENUMBER_POS_RIGHT;
             }
             else
             {
-                nVirtPageNum = 2;
+                isRightPage = false;
                 ePos = ePos == LINENUMBER_POS_OUTSIDE ?
                         LINENUMBER_POS_LEFT : LINENUMBER_POS_RIGHT;
             }
@@ -164,9 +164,9 @@ SwExtraPainter::SwExtraPainter( const SwTextFrame *pFrame, SwViewShell *pVwSh,
     {
         if( text::HoriOrientation::INSIDE == eHor || text::HoriOrientation::OUTSIDE == eHor )
         {
-            if( !nVirtPageNum )
-                nVirtPageNum = pFrame->FindPageFrame()->OnRightPage() ? 1 : 2;
-            if( nVirtPageNum % 2 )
+            if (!isRightPage)
+                isRightPage = pFrame->FindPageFrame()->OnRightPage();
+            if (isRightPage)
                 eHor = eHor == text::HoriOrientation::INSIDE ? text::HoriOrientation::LEFT : text::HoriOrientation::RIGHT;
             else
                 eHor = eHor == text::HoriOrientation::OUTSIDE ? text::HoriOrientation::LEFT : text::HoriOrientation::RIGHT;

commit 289052111e366a3cd9cbdde8f41981f31ad8559a

sw: fix unintended change in SwExtraPainter::SwExtraPainter()
nVirtPageNum was an obfuscated optional, by assigning 1 and 2 instead of
1 and 0... probably the only difference is that this saves a function
call?

(regression from 548590426790b6e5318ee0f9f4a5d648ca272449)

Change-Id: Ie8bccebd331fade9dd006da7c1b5a758946b8549
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89049
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/text/frmpaint.cxx
@@ -112,7 +112,7 @@ SwExtraPainter::SwExtraPainter( const SwTextFrame *pFrame, SwViewShell *pVwSh,
         if( m_aRect.Bottom() > nBottom )
             m_aRect.Bottom( nBottom );
     }
-    bool isRightPage(false);
+    o3tl::optional<bool> oIsRightPage;
     if( bLineNum )
     {
         /* Initializes the Members necessary for line numbering:
@@ -138,13 +138,13 @@ SwExtraPainter::SwExtraPainter( const SwTextFrame *pFrame, SwViewShell *pVwSh,
         {
             if( pFrame->FindPageFrame()->OnRightPage() )
             {
-                isRightPage = true;
+                oIsRightPage = true;
                 ePos = ePos == LINENUMBER_POS_INSIDE ?
                         LINENUMBER_POS_LEFT : LINENUMBER_POS_RIGHT;
             }
             else
             {
-                isRightPage = false;
+                oIsRightPage = false;
                 ePos = ePos == LINENUMBER_POS_OUTSIDE ?
                         LINENUMBER_POS_LEFT : LINENUMBER_POS_RIGHT;
             }
@@ -164,9 +164,9 @@ SwExtraPainter::SwExtraPainter( const SwTextFrame *pFrame, SwViewShell *pVwSh,
     {
         if( text::HoriOrientation::INSIDE == eHor || text::HoriOrientation::OUTSIDE == eHor )
         {
-            if (!isRightPage)
-                isRightPage = pFrame->FindPageFrame()->OnRightPage();
-            if (isRightPage)
+            if (!oIsRightPage)
+                oIsRightPage = pFrame->FindPageFrame()->OnRightPage();
+            if (*oIsRightPage)
                 eHor = eHor == text::HoriOrientation::INSIDE ? text::HoriOrientation::LEFT : text::HoriOrientation::RIGHT;
             else
                 eHor = eHor == text::HoriOrientation::OUTSIDE ? text::HoriOrientation::LEFT : text::HoriOrientation::RIGHT;