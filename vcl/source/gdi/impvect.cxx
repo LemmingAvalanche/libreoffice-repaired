LemmingAvalanche/libreoffice-repaired

commit 3efa808b58cff1c00e2465ee0ffc2aebf547937b

loplugin:useuniqueptr in vcl
Change-Id: I24eca813321fd3919bba9d37c285484f865ea2ea
Reviewed-on: https://gerrit.libreoffice.org/64877
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/impvect.cxx
@@ -30,6 +30,7 @@
 #include <vcl/wrkwin.hxx>
 #include <vcl/virdev.hxx>
 #include "impvect.hxx"
+#include <array>
 #include <memory>
 
 #define VECT_POLY_MAX 8192
@@ -112,30 +113,24 @@ struct ImplColorSet
     bool        mbSet = false;
 };
 
-extern "C" {
-
-static int ImplColorSetCmpFnc( const void* p1, const void* p2 )
+static int ImplColorSetCmpFnc( const ImplColorSet& lhs, const ImplColorSet& rhs)
 {
-    ImplColorSet const * pSet1 = static_cast<ImplColorSet const *>(p1);
-    ImplColorSet const * pSet2 = static_cast<ImplColorSet const *>(p2);
     int             nRet;
 
-    if( pSet1->mbSet && pSet2->mbSet )
+    if( lhs.mbSet && rhs.mbSet )
     {
-        const sal_uInt8 cLum1 = pSet1->maColor.GetLuminance();
-        const sal_uInt8 cLum2 = pSet2->maColor.GetLuminance();
-        nRet = ( ( cLum1 > cLum2 ) ? -1 : ( ( cLum1 == cLum2 ) ? 0 : 1 ) );
+        const sal_uInt8 cLum1 = lhs.maColor.GetLuminance();
+        const sal_uInt8 cLum2 = rhs.maColor.GetLuminance();
+        nRet = ( cLum1 > cLum2 ) ? -1 : ( ( cLum1 == cLum2 ) ? 0 : 1 );
     }
-    else if( pSet1->mbSet )
+    else if( lhs.mbSet )
         nRet = -1;
-    else if( pSet2->mbSet )
+    else if( rhs.mbSet )
         nRet = 1;
     else
         nRet = 0;
 
     return nRet;
-}
-
 }
 
 class ImplPointArray
@@ -652,28 +647,28 @@ bool ImplVectorize( const Bitmap& rColorBmp, GDIMetaFile& rMtf,
         const long          nHeight = pRAcc->Height();
         const sal_uInt16        nColorCount = pRAcc->GetPaletteEntryCount();
         sal_uInt16              n;
-        ImplColorSet*       pColorSet = new ImplColorSet[ 256 ];
+        std::array<ImplColorSet, 256> aColorSet;
 
         rMtf.Clear();
 
         // get used palette colors and sort them from light to dark colors
         for( n = 0; n < nColorCount; n++ )
         {
-            pColorSet[ n ].mnIndex = n;
-            pColorSet[ n ].maColor = pRAcc->GetPaletteColor( n );
+            aColorSet[ n ].mnIndex = n;
+            aColorSet[ n ].maColor = pRAcc->GetPaletteColor( n );
         }
 
         for( long nY = 0; nY < nHeight; nY++ )
         {
             Scanline pScanlineRead = pRAcc->GetScanline( nY );
             for( long nX = 0; nX < nWidth; nX++ )
-                pColorSet[ pRAcc->GetIndexFromData( pScanlineRead, nX ) ].mbSet = true;
+                aColorSet[ pRAcc->GetIndexFromData( pScanlineRead, nX ) ].mbSet = true;
         }
 
-        qsort( pColorSet, 256, sizeof( ImplColorSet ), ImplColorSetCmpFnc );
+        std::sort( aColorSet.begin(), aColorSet.end(), ImplColorSetCmpFnc );
 
         for( n = 0; n < 256; n++ )
-            if( !pColorSet[ n ].mbSet )
+            if( !aColorSet[ n ].mbSet )
                 break;
 
         if( n )
@@ -684,7 +679,7 @@ bool ImplVectorize( const Bitmap& rColorBmp, GDIMetaFile& rMtf,
 
         for( sal_uInt16 i = 0; i < n; i++ )
         {
-            const BitmapColor   aBmpCol( pRAcc->GetPaletteColor( pColorSet[ i ].mnIndex ) );
+            const BitmapColor   aBmpCol( pRAcc->GetPaletteColor( aColorSet[ i ].mnIndex ) );
             const Color         aFindColor( aBmpCol.GetRed(), aBmpCol.GetGreen(), aBmpCol.GetBlue() );
             std::unique_ptr<ImplVectMap> xMap(ImplExpand( pRAcc.get(), aFindColor ));
 
@@ -715,8 +710,6 @@ bool ImplVectorize( const Bitmap& rColorBmp, GDIMetaFile& rMtf,
             fPercent += fPercentStep_2;
             VECT_PROGRESS( pProgress, FRound( fPercent ) );
         }
-
-        delete[] pColorSet;
 
         if( rMtf.GetActionSize() )
         {

compilerplugins/clang/useuniqueptr.cxx
@@ -477,14 +477,15 @@ void UseUniquePtr::CheckDeleteLocalVar(const FunctionDecl* functionDecl, const C
     // linked list
     if (parentName == "ScFunctionList" || parentName == "SwNodes"
         || parentName == "SwUnoCursor" || parentName == "SortedResultSet"
-        || parentName == "Atom")
+        || parentName == "Atom" || parentName == "RegionBand" || parentName == "WMFWriter"
+        || parentName == "Scheduler" || parentName == "OpenGLContext")
         return;
     // manual ref counting
     if (parentName == "ScBroadcastAreaSlot")
         return;
     // complicated
     if (parentName == "SwFormatField" || parentName == "FontPropertyBox" || parentName == "SdFontPropertyBox"
-        || parentName == "SwHTMLParser")
+        || parentName == "SwHTMLParser" || parentName == "PDFWriterImpl")
         return;
 
     if (functionDecl->getIdentifier())
@@ -515,6 +516,12 @@ void UseUniquePtr::CheckDeleteLocalVar(const FunctionDecl* functionDecl, const C
             return;
         // very dodgy
         if (name == "UCBStorage::OpenStorage_Impl")
+            return;
+        // complicated ownership
+        if (name == "ParseCMAP" || name == "OpenGLSalBitmap::CreateTexture" || name == "X11SalGraphicsImpl::drawAlphaBitmap")
+            return;
+        // complicated delete
+        if (name == "X11SalObject::CreateObject")
             return;
     }
 

vcl/inc/svdata.hxx
@@ -191,7 +191,7 @@ struct ImplSVGDIData
     OpenGLContext*          mpLastContext = nullptr;        // Last OpenGLContext
     VclPtr<Printer>         mpFirstPrinter;                 // First Printer
     VclPtr<Printer>         mpLastPrinter;                  // Last Printer
-    ImplPrnQueueList*       mpPrinterQueueList = nullptr;   // List of all printer queue
+    std::unique_ptr<ImplPrnQueueList> mpPrinterQueueList;   // List of all printer queue
     std::shared_ptr<PhysicalFontCollection> mxScreenFontList; // Screen-Font-List
     std::shared_ptr<ImplFontCache> mxScreenFontCache;       // Screen-Font-Cache
     ImplDirectFontSubstitution* mpDirectFontSubst = nullptr; // Font-Substitutions defined in Tools->Options->Fonts

vcl/source/app/svapp.cxx
@@ -813,19 +813,17 @@ ImplSVEvent * Application::PostKeyEvent( VclEventId nEvent, vcl::Window *pWin, K
 
     if( pWin && pKeyEvent )
     {
-        ImplPostEventData* pPostEventData = new ImplPostEventData( nEvent, pWin, *pKeyEvent );
+        std::unique_ptr<ImplPostEventData> pPostEventData(new ImplPostEventData( nEvent, pWin, *pKeyEvent ));
 
         nEventId = PostUserEvent(
                        LINK( nullptr, Application, PostEventHandler ),
-                       pPostEventData );
+                       pPostEventData.get() );
 
         if( nEventId )
         {
             pPostEventData->mnEventId = nEventId;
-            ImplGetSVData()->maAppData.maPostedEventList.emplace_back( pWin, pPostEventData );
+            ImplGetSVData()->maAppData.maPostedEventList.emplace_back( pWin, pPostEventData.release() );
         }
-        else
-            delete pPostEventData;
     }
 
     return nEventId;
@@ -846,19 +844,17 @@ ImplSVEvent * Application::PostMouseEvent( VclEventId nEvent, vcl::Window *pWin,
         const MouseEvent aTransformedEvent( aTransformedPos, pMouseEvent->GetClicks(), pMouseEvent->GetMode(),
                                             pMouseEvent->GetButtons(), pMouseEvent->GetModifier() );
 
-        ImplPostEventData* pPostEventData = new ImplPostEventData( nEvent, pWin, aTransformedEvent );
+        std::unique_ptr<ImplPostEventData> pPostEventData(new ImplPostEventData( nEvent, pWin, aTransformedEvent ));
 
         nEventId = PostUserEvent(
                        LINK( nullptr, Application, PostEventHandler ),
-                       pPostEventData );
+                       pPostEventData.get() );
 
         if( nEventId )
         {
             pPostEventData->mnEventId = nEventId;
-            ImplGetSVData()->maAppData.maPostedEventList.emplace_back( pWin, pPostEventData );
+            ImplGetSVData()->maAppData.maPostedEventList.emplace_back( pWin, pPostEventData.release() );
         }
-        else
-            delete pPostEventData;
     }
 
     return nEventId;

vcl/source/app/svdata.cxx
@@ -50,6 +50,7 @@
 #include <salsys.hxx>
 #include <strings.hrc>
 #include <units.hrc>
+#include <print.h>
 
 #include <com/sun/star/accessibility/MSAAService.hpp>
 

vcl/source/bitmap/BitmapConvolutionMatrixFilter.cxx
@@ -18,6 +18,7 @@
 #include <vcl/BitmapSharpenFilter.hxx>
 
 #include <bitmapwriteaccess.hxx>
+#include <array>
 
 BitmapEx BitmapConvolutionMatrixFilter::execute(BitmapEx const& rBitmapEx) const
 {
@@ -36,20 +37,17 @@ BitmapEx BitmapConvolutionMatrixFilter::execute(BitmapEx const& rBitmapEx) const
         {
             const long nWidth = pWriteAcc->Width(), nWidth2 = nWidth + 2;
             const long nHeight = pWriteAcc->Height(), nHeight2 = nHeight + 2;
-            long* pColm = new long[nWidth2];
-            long* pRows = new long[nHeight2];
-            BitmapColor* pColRow1
-                = reinterpret_cast<BitmapColor*>(new sal_uInt8[sizeof(BitmapColor) * nWidth2]);
-            BitmapColor* pColRow2
-                = reinterpret_cast<BitmapColor*>(new sal_uInt8[sizeof(BitmapColor) * nWidth2]);
-            BitmapColor* pColRow3
-                = reinterpret_cast<BitmapColor*>(new sal_uInt8[sizeof(BitmapColor) * nWidth2]);
-            BitmapColor* pRowTmp1 = pColRow1;
-            BitmapColor* pRowTmp2 = pColRow2;
-            BitmapColor* pRowTmp3 = pColRow3;
+            std::unique_ptr<long[]> pColm(new long[nWidth2]);
+            std::unique_ptr<long[]> pRows(new long[nHeight2]);
+            std::unique_ptr<BitmapColor[]> pColRow1(new BitmapColor[nWidth2]);
+            std::unique_ptr<BitmapColor[]> pColRow2(new BitmapColor[nWidth2]);
+            std::unique_ptr<BitmapColor[]> pColRow3(new BitmapColor[nWidth2]);
+            BitmapColor* pRowTmp1 = pColRow1.get();
+            BitmapColor* pRowTmp2 = pColRow2.get();
+            BitmapColor* pRowTmp3 = pColRow3.get();
             BitmapColor* pColor;
             long nY, nX, i, nSumR, nSumG, nSumB, nMatrixVal, nTmp;
-            long(*pKoeff)[256] = new long[9][256];
+            std::array<std::array<long, 256>, 9> aKoeff;
             long* pTmp;
 
             // create LUT of products of matrix value and possible color component values
@@ -57,7 +55,7 @@ BitmapEx BitmapConvolutionMatrixFilter::execute(BitmapEx const& rBitmapEx) const
             {
                 for (nX = nTmp = 0, nMatrixVal = mrMatrix[nY]; nX < 256; nX++, nTmp += nMatrixVal)
                 {
-                    pKoeff[nY][nX] = nTmp;
+                    aKoeff[nY][nX] = nTmp;
                 }
             }
 
@@ -92,52 +90,52 @@ BitmapEx BitmapConvolutionMatrixFilter::execute(BitmapEx const& rBitmapEx) const
                 for (nX = 0; nX < nWidth; nX++)
                 {
                     // first row
-                    pTmp = pKoeff[0];
+                    pTmp = aKoeff[0].data();
                     pColor = pRowTmp1 + nX;
                     nSumR = pTmp[pColor->GetRed()];
                     nSumG = pTmp[pColor->GetGreen()];
                     nSumB = pTmp[pColor->GetBlue()];
 
-                    pTmp = pKoeff[1];
+                    pTmp = aKoeff[1].data();
                     nSumR += pTmp[(++pColor)->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
-                    pTmp = pKoeff[2];
+                    pTmp = aKoeff[2].data();
                     nSumR += pTmp[(++pColor)->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
                     // second row
-                    pTmp = pKoeff[3];
+                    pTmp = aKoeff[3].data();
                     pColor = pRowTmp2 + nX;
                     nSumR += pTmp[pColor->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
-                    pTmp = pKoeff[4];
+                    pTmp = aKoeff[4].data();
                     nSumR += pTmp[(++pColor)->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
-                    pTmp = pKoeff[5];
+                    pTmp = aKoeff[5].data();
                     nSumR += pTmp[(++pColor)->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
                     // third row
-                    pTmp = pKoeff[6];
+                    pTmp = aKoeff[6].data();
                     pColor = pRowTmp3 + nX;
                     nSumR += pTmp[pColor->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
-                    pTmp = pKoeff[7];
+                    pTmp = aKoeff[7].data();
                     nSumR += pTmp[(++pColor)->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
 
-                    pTmp = pKoeff[8];
+                    pTmp = aKoeff[8].data();
                     nSumR += pTmp[(++pColor)->GetRed()];
                     nSumG += pTmp[pColor->GetGreen()];
                     nSumB += pTmp[pColor->GetBlue()];
@@ -152,23 +150,23 @@ BitmapEx BitmapConvolutionMatrixFilter::execute(BitmapEx const& rBitmapEx) const
 
                 if (++nY < nHeight)
                 {
-                    if (pRowTmp1 == pColRow1)
+                    if (pRowTmp1 == pColRow1.get())
                     {
-                        pRowTmp1 = pColRow2;
-                        pRowTmp2 = pColRow3;
-                        pRowTmp3 = pColRow1;
+                        pRowTmp1 = pColRow2.get();
+                        pRowTmp2 = pColRow3.get();
+                        pRowTmp3 = pColRow1.get();
                     }
-                    else if (pRowTmp1 == pColRow2)
+                    else if (pRowTmp1 == pColRow2.get())
                     {
-                        pRowTmp1 = pColRow3;
-                        pRowTmp2 = pColRow1;
-                        pRowTmp3 = pColRow2;
+                        pRowTmp1 = pColRow3.get();
+                        pRowTmp2 = pColRow1.get();
+                        pRowTmp3 = pColRow2.get();
                     }
                     else
                     {
-                        pRowTmp1 = pColRow1;
-                        pRowTmp2 = pColRow2;
-                        pRowTmp3 = pColRow3;
+                        pRowTmp1 = pColRow1.get();
+                        pRowTmp2 = pColRow2.get();
+                        pRowTmp3 = pColRow3.get();
                     }
 
                     for (i = 0; i < nWidth2; i++)
@@ -177,13 +175,6 @@ BitmapEx BitmapConvolutionMatrixFilter::execute(BitmapEx const& rBitmapEx) const
                     }
                 }
             }
-
-            delete[] pKoeff;
-            delete[] reinterpret_cast<sal_uInt8*>(pColRow1);
-            delete[] reinterpret_cast<sal_uInt8*>(pColRow2);
-            delete[] reinterpret_cast<sal_uInt8*>(pColRow3);
-            delete[] pColm;
-            delete[] pRows;
 
             pWriteAcc.reset();
 

vcl/source/bitmap/BitmapEmbossGreyFilter.cxx
@@ -46,8 +46,8 @@ BitmapEx BitmapEmbossGreyFilter::execute(BitmapEx const& rBitmapEx) const
                 long nGrey31, nGrey32, nGrey33;
                 double fAzim = basegfx::deg2rad(mnAzimuthAngle100 * 0.01);
                 double fElev = basegfx::deg2rad(mnElevationAngle100 * 0.01);
-                long* pHMap = new long[nWidth + 2];
-                long* pVMap = new long[nHeight + 2];
+                std::unique_ptr<long[]> pHMap(new long[nWidth + 2]);
+                std::unique_ptr<long[]> pVMap(new long[nHeight + 2]);
                 long nX, nY, nNx, nNy, nDotL;
                 const long nLx = FRound(cos(fAzim) * cos(fElev) * 255.0);
                 const long nLy = FRound(sin(fAzim) * cos(fElev) * 255.0);
@@ -127,8 +127,8 @@ BitmapEx BitmapEmbossGreyFilter::execute(BitmapEx const& rBitmapEx) const
                     }
                 }
 
-                delete[] pHMap;
-                delete[] pVMap;
+                pHMap.reset();
+                pVMap.reset();
                 pWriteAcc.reset();
                 bRet = true;
             }

vcl/source/bitmap/BitmapMedianFilter.cxx
@@ -68,17 +68,14 @@ BitmapEx BitmapMedianFilter::execute(BitmapEx const& rBitmapEx) const
         {
             const long nWidth = pWriteAcc->Width(), nWidth2 = nWidth + 2;
             const long nHeight = pWriteAcc->Height(), nHeight2 = nHeight + 2;
-            long* pColm = new long[nWidth2];
-            long* pRows = new long[nHeight2];
-            BitmapColor* pColRow1
-                = reinterpret_cast<BitmapColor*>(new sal_uInt8[sizeof(BitmapColor) * nWidth2]);
-            BitmapColor* pColRow2
-                = reinterpret_cast<BitmapColor*>(new sal_uInt8[sizeof(BitmapColor) * nWidth2]);
-            BitmapColor* pColRow3
-                = reinterpret_cast<BitmapColor*>(new sal_uInt8[sizeof(BitmapColor) * nWidth2]);
-            BitmapColor* pRowTmp1 = pColRow1;
-            BitmapColor* pRowTmp2 = pColRow2;
-            BitmapColor* pRowTmp3 = pColRow3;
+            std::unique_ptr<long[]> pColm(new long[nWidth2]);
+            std::unique_ptr<long[]> pRows(new long[nHeight2]);
+            std::unique_ptr<BitmapColor[]> pColRow1(new BitmapColor[nWidth2]);
+            std::unique_ptr<BitmapColor[]> pColRow2(new BitmapColor[nWidth2]);
+            std::unique_ptr<BitmapColor[]> pColRow3(new BitmapColor[nWidth2]);
+            BitmapColor* pRowTmp1 = pColRow1.get();
+            BitmapColor* pRowTmp2 = pColRow2.get();
+            BitmapColor* pRowTmp3 = pColRow3.get();
             BitmapColor* pColor;
             long nY, nX, i;
             long nR1, nR2, nR3, nR4, nR5, nR6, nR7, nR8, nR9;
@@ -171,35 +168,29 @@ BitmapEx BitmapMedianFilter::execute(BitmapEx const& rBitmapEx) const
 
                 if (++nY < nHeight)
                 {
-                    if (pRowTmp1 == pColRow1)
+                    if (pRowTmp1 == pColRow1.get())
                     {
-                        pRowTmp1 = pColRow2;
-                        pRowTmp2 = pColRow3;
-                        pRowTmp3 = pColRow1;
+                        pRowTmp1 = pColRow2.get();
+                        pRowTmp2 = pColRow3.get();
+                        pRowTmp3 = pColRow1.get();
                     }
-                    else if (pRowTmp1 == pColRow2)
+                    else if (pRowTmp1 == pColRow2.get())
                     {
-                        pRowTmp1 = pColRow3;
-                        pRowTmp2 = pColRow1;
-                        pRowTmp3 = pColRow2;
+                        pRowTmp1 = pColRow3.get();
+                        pRowTmp2 = pColRow1.get();
+                        pRowTmp3 = pColRow2.get();
                     }
                     else
                     {
-                        pRowTmp1 = pColRow1;
-                        pRowTmp2 = pColRow2;
-                        pRowTmp3 = pColRow3;
+                        pRowTmp1 = pColRow1.get();
+                        pRowTmp2 = pColRow2.get();
+                        pRowTmp3 = pColRow3.get();
                     }
 
                     for (i = 0; i < nWidth2; i++)
                         pRowTmp3[i] = pReadAcc->GetColor(pRows[nY + 2], pColm[i]);
                 }
             }
-
-            delete[] reinterpret_cast<sal_uInt8*>(pColRow1);
-            delete[] reinterpret_cast<sal_uInt8*>(pColRow2);
-            delete[] reinterpret_cast<sal_uInt8*>(pColRow3);
-            delete[] pColm;
-            delete[] pRows;
 
             pWriteAcc.reset();
 

commit 369db13dbc8f29894ac54d97799bf28a0c395bd9

tdf#130679 Draw: Convert to polygon doesn't work
regression from
    commit 3efa808b58cff1c00e2465ee0ffc2aebf547937b
    Date:   Mon Dec 10 11:28:59 2018 +0200
    loplugin:useuniqueptr in vcl

Change-Id: I7753f54822d0249d1fcda97581051d023969fc2f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89636
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/impvect.cxx
@@ -129,7 +129,7 @@ static bool ImplColorSetCmpFnc( const ImplColorSet& lhs, const ImplColorSet& rhs
         const sal_uInt8 cLum2 = rhs.maColor.GetLuminance();
         return cLum1 < cLum2;
     }
-    return lhs.mbSet < rhs.mbSet;
+    return lhs.mbSet > rhs.mbSet;
 }
 
 namespace {