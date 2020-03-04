LemmingAvalanche/libreoffice-repaired

commit c6a931f0a054062c3eee4849b736aefb10a58956

tdf#118699 sw_redlinehide: need some more changes to use the last node
... as property node for the merged frame.

This would assert in SwContentNode::DelFrames() during tdf#126627
and also in crashtesting, e.g. fdo66302-1.odt.

(regression from adfbd4fe799f0e87201d1d8e346518370c15d8a5)

Change-Id: Ia6d52287ed42f41d16ebf12bc61d7f4a6d7c5b03
Reviewed-on: https://gerrit.libreoffice.org/77212
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

sw/source/core/docnode/node.cxx
@@ -1371,8 +1371,24 @@ void SwContentNode::DelFrames(SwRootFrame const*const pLayout)
                     // because that would access deleted wrong-lists
                     sw::UpdateMergedParaForDelete(*pMerged, true,
                             *static_cast<SwTextNode*>(this), 0, Len());
-                    // pointer should have been updated to a different node
-                    assert(this != pMerged->pParaPropsNode);
+                    if (this == pMerged->pParaPropsNode)
+                    {
+                        // otherwise pointer should have been updated to a different node
+                        assert(this == pMerged->pLastNode);
+                        assert(pMerged->extents.empty());
+                        for (sal_uLong i = pMerged->pLastNode->GetIndex() - 1;;
+                                --i)
+                        {
+                            assert(pMerged->pFirstNode->GetIndex() <= i);
+                            SwNode *const pNode(GetNodes()[i]);
+                            if (pNode->IsTextNode()
+                                && pNode->GetRedlineMergeFlag() != Merge::Hidden)
+                            {
+                                pMerged->pParaPropsNode = pNode->GetTextNode();
+                                break;
+                            }
+                        }
+                    }
                     if (this == pMerged->pLastNode)
                     {
                         pMerged->pLastNode = GetNodes()[GetIndex()-1]->GetTextNode();

sw/source/core/text/txtfrm.cxx
@@ -1014,8 +1014,9 @@ static TextFrameIndex UpdateMergedParaForInsert(MergedPara & rMerged,
         rMerged.extents.emplace(itInsert, const_cast<SwTextNode*>(&rNode), nIndex, nIndex + nLen);
         text.insert(nTFIndex, rNode.GetText().copy(nIndex, nLen));
         nInserted = nLen;
-        if (rNode.GetIndex() < rMerged.pParaPropsNode->GetIndex())
-        {   // text inserted before current para-props node
+        if (rMerged.extents.size() == 1 // also if it was empty!
+            || rMerged.pParaPropsNode->GetIndex() < rNode.GetIndex())
+        {   // text inserted after current para-props node
             rMerged.pParaPropsNode->RemoveFromListRLHidden();
             rMerged.pParaPropsNode = &const_cast<SwTextNode&>(rNode);
             rMerged.pParaPropsNode->AddToListRLHidden();
@@ -1147,7 +1148,7 @@ TextFrameIndex UpdateMergedParaForDelete(MergedPara & rMerged,
         {
             rMerged.pParaPropsNode->RemoveFromListRLHidden();
             rMerged.pParaPropsNode = rMerged.extents.empty()
-                ? rMerged.pFirstNode
+                ? const_cast<SwTextNode*>(rMerged.pLastNode)
                 : rMerged.extents.front().pNode;
             rMerged.pParaPropsNode->AddToListRLHidden();
         }

commit dcc743b8cf853f05316f476452fa1ada4e00bdce

tdf#130680 sw_redlinehide: fix crash in SwUndoDelete
The problem is that the code that adjusts pLastNode in
SwContentNode::DelFrames() assumes that the nodes are deleted forward
but MoveNode() runs backward so the only solution is to put pLastNode
temporarily onto some node without checking if it has been fully
deleted, while skipping over tables and sections.

At the end of MoveNode() it must be on a valid (not fully deleted) node
again.

(regression from c6a931f0a054062c3eee4849b736aefb10a58956)

Change-Id: If54c01f885e0c040aed9068823deae7dd167fd50
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89896
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/docnode/node.cxx
@@ -1387,15 +1387,26 @@ void SwContentNode::DelFrames(SwRootFrame const*const pLayout)
                             }
                         }
                     }
+                    assert(GetIndex() <= pMerged->pLastNode->GetIndex());
                     if (this == pMerged->pLastNode)
                     {
-                        pMerged->pLastNode = GetNodes()[GetIndex()-1]->GetTextNode();
-                        // at first glance nothing guarantees this...
-                        // but the redline must end on a text-node...
-                        // so everything before this node that isn't a text
-                        // node should have been deleted already so that
-                        // there's a text node before.
-                        assert(pMerged->pLastNode->IsTextNode());
+                        // tdf#130680 find the previous node that is a
+                        // listener of pMerged; see CheckParaRedlineMerge()
+                        for (sal_uLong i = GetIndex() - 1;
+                             this == pMerged->pLastNode; --i)
+                        {
+                            SwNode *const pNode = GetNodes()[i];
+                            if (pNode->IsTextNode())
+                            {
+                                pMerged->pLastNode = pNode->GetTextNode();
+                            }
+                            else if (SwEndNode const*const pEnd = pNode->GetEndNode())
+                            {
+                                SwStartNode const*const pStart(pEnd->StartOfSectionNode());
+                                i = pStart->GetIndex(); // skip table or section
+                            }
+                        }
+                        assert(pMerged->pFirstNode->GetIndex() <= pMerged->pLastNode->GetIndex());
                     }
                     // avoid re-parenting mess (ModifyChangedHint)
                     pMerged->listener.EndListening(this);