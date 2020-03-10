LemmingAvalanche/libreoffice-repaired

commit f7de5c8370528253597b84a83a7adedb50d78e52

fix wrong SET/QUERY flags passed to uno::Reference
By creating deleted methods for the wrong calls.

Avoids the compiler needing to construct a temporary

Change-Id: I3b8c648d6bb22d22827bf74f21ea5a2a17fc0f6a
Reviewed-on: https://gerrit.libreoffice.org/72103
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/form/fmvwimp.cxx
@@ -197,7 +197,7 @@ void FormViewPageWindowAdapter::dispose()
     {
         try
         {
-            Reference< XFormController > xController( *i, UNO_QUERY_THROW );
+            Reference< XFormController > xController( *i, UNO_SET_THROW );
 
             // detaching the events
             Reference< XChild > xControllerModel( xController->getModel(), UNO_QUERY );
@@ -868,7 +868,7 @@ namespace
                     continue;
 
                 Reference< XChild > xModel( pFormObject->GetUnoControlModel(), UNO_QUERY_THROW );
-                Reference< XInterface > xModelParent( xModel->getParent(), UNO_QUERY_THROW );
+                Reference< XInterface > xModelParent( xModel->getParent(), UNO_SET_THROW );
 
                 if ( xNormalizedForm.get() != xModelParent.get() )
                     continue;

commit 06a01115ae3b86c43d040e0e0d4d98126a676d9b

tdf#131088 Automatic Control Focus not working
regression from
    commit f7de5c8370528253597b84a83a7adedb50d78e52
    Date:   Fri May 10 10:13:12 2019 +0200
    fix wrong SET/QUERY flags passed to uno::Reference

Change-Id: Id0dd46c3d40f8acab980147c4c7f64911bc30d22
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90293
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/form/fmvwimp.cxx
@@ -862,7 +862,7 @@ namespace
                     continue;
 
                 Reference< XChild > xModel( pFormObject->GetUnoControlModel(), UNO_QUERY_THROW );
-                Reference< XInterface > xModelParent( xModel->getParent(), UNO_SET_THROW );
+                Reference< XInterface > xModelParent( xModel->getParent(), UNO_QUERY );
 
                 if ( xNormalizedForm.get() != xModelParent.get() )
                     continue;