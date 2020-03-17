LemmingAvalanche/libreoffice-repaired

commit c34d2b7fcf1d7c6c7e0fe3a567289e39dd999830

tdf#103983 VML import: handle <v:textbox style="mso-fit-shape-to-text:t">
We used to always always set TextAutoGrowHeight to true, only do that
when the matching VML attribute is detected, default to false.

This helps the exporter, so it writes the correct markup on save of the
bugdoc.

Also adapt testGroupshapeChildRotation, which in practice tested the
automatic height of the shape. The point of "auto" is that it changes as
needed, hardcoding that value in a test is not a great idea. Rather test
that the height is no longer automatic (there is no explicit markup in
the file, and the default is false).

Change-Id: Ie39408b7da53f4923a2ade503e520c704a86bcf4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87104
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

oox/source/vml/vmlshape.cxx
@@ -792,6 +792,9 @@ Reference< XShape > SimpleShape::implConvertAndInsert( const Reference< XShapes 
                 xShape->setSize(aSize);
             }
         }
+
+        PropertySet(xShape).setAnyProperty(PROP_TextAutoGrowHeight,
+                                           makeAny(maTypeModel.mbAutoHeight));
     }
 
     // Import Legacy Fragments (if any)

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -569,8 +569,9 @@ DECLARE_OOXMLIMPORT_TEST(testGroupshapeChildRotation, "groupshape-child-rotation
 
 #if HAVE_MORE_FONTS
     xShape.set(xGroupShape->getByIndex(4), uno::UNO_QUERY);
-    // This was 887, i.e. border distances were included in the height.
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(661), xShape->getSize().Height);
+    // This was true, a VML textbox without <v:textbox style="mso-fit-shape-to-text:t"> had
+    // auto-grow on.
+    CPPUNIT_ASSERT(!getProperty<bool>(xShape, "TextAutoGrowHeight"));
     // Paragraph Style Normal should provide the font name - which slightly affects the shape's height (was 686)
     uno::Reference<text::XText> xText = uno::Reference<text::XTextRange>(xShape, uno::UNO_QUERY_THROW)->getText();
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Font", OUString("Times New Roman"), getProperty<OUString>(getRun(xText, 1), "CharFontName"));

commit b03c34642f8136d66b6f5bfb6043ab1d6b4ae36c

tdf#131118 VML import: fix size of non-autosize shapes containing text
Regression from commit c34d2b7fcf1d7c6c7e0fe3a567289e39dd999830
(tdf#103983 VML import: handle <v:textbox
style="mso-fit-shape-to-text:t">, 2020-01-20), the problem was that in
case we disable autosize too late, then the size will be already set
during adding text to the shape.

Do it before adding text, this way adding text won't change the shape
size, so it'll be correct at the end of the import.

Change-Id: I9410fc695c3edfa5089d845864bf237e71c533c4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90592
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

oox/source/vml/vmlshape.cxx
@@ -783,6 +783,9 @@ Reference< XShape > SimpleShape::implConvertAndInsert( const Reference< XShapes 
             eTextVerticalAdjust = drawing::TextVerticalAdjust_BOTTOM;
         PropertySet(xShape).setAnyProperty(PROP_TextVerticalAdjust, makeAny(eTextVerticalAdjust));
 
+        PropertySet(xShape).setAnyProperty(PROP_TextAutoGrowHeight,
+                                           makeAny(maTypeModel.mbAutoHeight));
+
         if (getTextBox())
         {
             getTextBox()->convert(xShape);
@@ -796,9 +799,6 @@ Reference< XShape > SimpleShape::implConvertAndInsert( const Reference< XShapes 
                 xShape->setSize(aSize);
             }
         }
-
-        PropertySet(xShape).setAnyProperty(PROP_TextAutoGrowHeight,
-                                           makeAny(maTypeModel.mbAutoHeight));
     }
 
     // Import Legacy Fragments (if any)

oox/qa/unit/vml.cxx
@@ -91,6 +91,24 @@ CPPUNIT_TEST_FIXTURE(OoxVmlTest, testSpt202ShapeType)
     CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.TextShape"), xShape->getShapeType());
 }
 
+CPPUNIT_TEST_FIXTURE(OoxVmlTest, testShapeNonAutosizeWithText)
+{
+    // Load a document which has a group shape, containing a single child.
+    // 17.78 cm is the full group shape width, 19431/64008 is the child shape's relative width inside
+    // that, so 5.3975 cm should be the shape width.
+    load("shape-non-autosize-with-text.docx");
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xGroup(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<drawing::XShape> xShape(xGroup->getByIndex(0), uno::UNO_QUERY);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Actual  : 1115
+    // - Expected: 5398
+    // because the width was determined using its text size, not using the explicit size.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(5398), xShape->getSize().Width);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */