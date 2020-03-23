LemmingAvalanche/libreoffice-repaired

commit 2ea0cb40b5aa3afc97788544b9010806321b8da1

Build external lxml if not provided by system
except on windows, where gla11y will resort to python's internal xml parser,
which does not provide line numbers.

This allows gla11y to be runnable on all systems.

Change-Id: Ica4eb90f59bddfcefd783fc2ed9c8c27357e7572
Reviewed-on: https://gerrit.libreoffice.org/50115
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

configure.ac
@@ -8147,19 +8147,36 @@ if test $enable_python = system; then
     PYTHON_FOR_BUILD=$PYTHON
 fi
 
+if -z "$PYTHON_FOR_BUILD"; then
+    case $build_os in
+        cygwin)
+            AC_MSG_WARN([No system-provided python lxml, gla11y will only report widget classes and ids])
+            ;;
+        *)
+            BUILD_TYPE="$BUILD_TYPE LXML"
+            ;;
+    esac
+else
+    AC_MSG_CHECKING([for python lxml])
+    if $PYTHON_FOR_BUILD -c "import lxml.etree as ET" 2> /dev/null ; then
+        AC_MSG_RESULT([yes])
+    else
+        case $build_os in
+            cygwin)
+                AC_MSG_RESULT([no, gla11y will only report widget classes and ids])
+                ;;
+            *)
+                BUILD_TYPE="$BUILD_TYPE LXML"
+                AC_MSG_RESULT([no, using internal lxml])
+                ;;
+        esac
+    fi
+fi
+
 dnl By now enable_python should be "system", "internal" or "no"
-PYTHON_LXML=
 case $enable_python in
 system)
     SYSTEM_PYTHON=TRUE
-
-    AC_MSG_CHECKING([for python lxml])
-    if $PYTHON_FOR_BUILD -c "import lxml.etree as ET" ; then
-        PYTHON_LXML=TRUE
-        AC_MSG_RESULT([yes])
-    else
-        AC_MSG_RESULT([no, will not be able to check UI accessibility])
-    fi
 
     dnl Check if the headers really work
     save_CPPFLAGS="$CPPFLAGS"
@@ -8222,7 +8239,6 @@ AC_SUBST(DISABLE_PYTHON)
 AC_SUBST(SYSTEM_PYTHON)
 AC_SUBST(PYTHON_CFLAGS)
 AC_SUBST(PYTHON_LIBS)
-AC_SUBST(PYTHON_LXML)
 AC_SUBST(PYTHON_VERSION)
 AC_SUBST(PYTHON_VERSION_MAJOR)
 AC_SUBST(PYTHON_VERSION_MINOR)

solenv/gbuild/platform/com_GCC_class.mk
@@ -155,4 +155,13 @@ $(call gb_Helper_abbreviate_dirs,\
 
 endef
 
+define gb_UIConfig__gla11y_command
+$(call gb_Helper_abbreviate_dirs,\
+	$(gb_UIConfig_LXML_PATH) $(gb_Helper_set_ld_path) \
+	$(call gb_ExternalExecutable_get_command,python) \
+	$(gb_UIConfig_gla11y_SCRIPT) -o $@ $(UIFILES)
+)
+
+endef
+
 # vim: set noet sw=4 ts=4:

solenv/gbuild/platform/com_MSC_class.mk
@@ -576,6 +576,18 @@ $(call gb_Helper_abbreviate_dirs,\
 
 endef
 
+# use file list file because swriter has too many files for command line
+define gb_UIConfig__gla11y_command
+$(call gb_ExternalExecutale__check_registration,python)
+$(call gb_Helper_abbreviate_dirs,\
+	FILES=$(call var2file,$(shell $(gb_MKTEMP)),100,$(UIFILES)) && \
+	$(gb_UIConfig_LXML_PATH) $(gb_Helper_set_ld_path) \
+	$(call gb_ExternalExecutable_get_command,python) \
+	$(gb_UIConfig_gla11y_SCRIPT) -o $@ -L $$FILES
+)
+
+endef
+
 # UIMenubarTarget class
 
 define gb_UIMenubarTarget__command
@@ -587,7 +599,7 @@ endef
 gb_UIMenubarTarget_UIMenubarTarget_platform :=
 
 # Python
-gb_Python_PRECOMMAND := PATH="$(shell cygpath -w $(INSTDIR)/program)" PYTHONHOME="$(INSTDIR)/program/python-core-$(PYTHON_VERSION)" PYTHONPATH="$(INSTDIR)/program/python-core-$(PYTHON_VERSION)/lib;$(INSTDIR)/program/python-core-$(PYTHON_VERSION)/lib/lib-dynload:$(INSTDIR)/program"
+gb_Python_PRECOMMAND := PATH="$(shell cygpath -w $(INSTDIR)/program)" PYTHONHOME="$(INSTDIR)/program/python-core-$(PYTHON_VERSION)" PYTHONPATH="$${PYPATH:+$$PYPATH:}$(INSTDIR)/program/python-core-$(PYTHON_VERSION)/lib;$(INSTDIR)/program/python-core-$(PYTHON_VERSION)/lib/lib-dynload:$(INSTDIR)/program"
 gb_Python_INSTALLED_EXECUTABLE := $(INSTROOT)/$(LIBO_BIN_FOLDER)/python.exe
 
 gb_ICU_PRECOMMAND := PATH="$(shell cygpath -w $(WORKDIR_FOR_BUILD)/UnpackedTarball/icu/source/lib)"

Makefile.fetch
@@ -163,6 +163,7 @@ $(WORKDIR)/download: $(BUILDDIR)/config_$(gb_Side).mk $(SRCDIR)/download.lst $(S
 		XMLSEC_TARBALL \
 		$(call fetch_Optional,LIBXSLT,LIBXSLT_TARBALL) \
 		$(call fetch_Optional,LPSOLVE,LPSOLVE_TARBALL) \
+		$(call fetch_Optional,LXML,LXML_TARBALL) \
 		$(call fetch_Optional,MARIADB_CONNECTOR_C,MARIADB_CONNECTOR_C_TARBALL) \
 		$(call fetch_Optional,MDDS,MDDS_TARBALL) \
 		$(call fetch_Optional,MDNSRESPONDER,MDNSRESPONDER_TARBALL) \

RepositoryExternal.mk
@@ -4017,6 +4017,7 @@ else
 
 define gb_ExternalExecutable__register_python
 $(call gb_ExternalExecutable_set_external,python,$(PYTHON_FOR_BUILD))
+$(call gb_ExternalExecutable_set_precommand,python,PYTHONPATH=$$$$PYTHONPATH$$$${PYTHONPATH:+$$$${PYPATH:+:}}$$$$PYPATH)
 
 endef
 

bin/gla11y
@@ -31,33 +31,84 @@ from __future__ import print_function
 import os
 import sys
 import getopt
-import lxml.etree as ET
+try:
+    import lxml.etree as ET
+    lxml = True
+except ImportError:
+    import xml.etree.ElementTree as ET
+    lxml = False
 
 progname = os.path.basename(sys.argv[0])
+outfile = None
 Werror = False
 Wnone = False
 errors = 0
 warnings = 0
 
-
-def errstr(elm):
+def step_elm(elm):
     """
-    Print the line number of the element
+    Return the XML class path step corresponding to elm.
+    This can be empty if the elm does not have any class or id.
     """
+    step = elm.attrib.get('class')
+    if step is None:
+        step = ""
+    oid = elm.attrib.get('id')
+    if oid is not None:
+        oid = oid.encode('ascii','ignore').decode('ascii')
+        step += "[@id='%s']" % oid
+    if len(step) > 0:
+        step += '/'
+    return step
 
-    return str(elm.sourceline)
+def find_elm(root, elm):
+    """
+    Return the XML class path of the element from the given root.
+    This is the slow version used when getparent is not available.
+    """
+    if root == elm:
+        return ""
+    for o in root:
+        path = find_elm(o, elm)
+        if path is not None:
+            step = step_elm(o)
+            return step + path
+    return None
 
-def err(filename, elm, msg):
+
+def elm_prefix(filename, elm):
+    """
+    Return the display prefix of the element
+    """
+    if elm == None or not lxml:
+        return "%s:" % filename
+    else:
+        return "%s:%u" % (filename, elm.sourceline)
+
+def elm_name(elm):
+    """
+    Return a display name of the element
+    """
+    if elm is not None:
+        name = ""
+        if 'class' in elm.attrib:
+            name = "'%s' " % elm.attrib['class']
+        if 'id' in elm.attrib:
+            id = elm.attrib['id'].encode('ascii','ignore').decode('ascii')
+            name += "'%s' " % id
+        return name
+    return ""
+
+def err(filename, tree, elm, msg):
     global errors
 
-    if elm == None:
-        prefix = "%s:" % filename
-    else:
-        prefix = "%s:%s" % (filename, errstr(elm))
+    prefix = elm_prefix(filename, elm)
 
     errors += 1
-    msg = "%s ERROR: %s" % (prefix, msg)
-    print(msg.encode('ascii', 'ignore'))
+    msg = "%s ERROR: %s%s" % (prefix, elm_name(elm), msg)
+    print(msg)
+    if outfile is not None:
+        print(msg, file=outfile)
 
 
 def warn(filename, elm, msg):
@@ -66,61 +117,73 @@ def warn(filename, elm, msg):
     if Wnone:
         return
 
-    prefix = "%s:%s" % (filename, errstr(elm))
+    prefix = elm_prefix(filename, elm)
 
     if Werror:
         errors += 1
     else:
         warnings += 1
 
-    msg = "%s WARNING: %s" % (prefix,  msg)
-    print(msg.encode('ascii', 'ignore'))
+    msg = "%s WARNING: %s%s" % (prefix, elm_name(elm), msg)
+    print(msg)
+    if outfile is not None:
+        print(msg, file=outfile)
 
 
-def check_objects(filename, elm, objects, target):
+def check_objects(filename, tree, elm, objects, target):
     """
     Check that objects contains exactly one object
     """
     length = len(list(objects))
     if length == 0:
-        err(filename, elm, "use of undeclared target '%s'" % target)
+        err(filename, tree, elm, "uses undeclared target '%s'" % target)
     elif length > 1:
-        err(filename, elm, "several targets are named '%s'" % target)
+        err(filename, tree, elm, "several targets are named '%s'" % target)
 
-def check_props(filename, root, props):
+def check_props(filename, tree, root, elm, props):
     """
     Check the given list of relation properties
     """
     for prop in props:
         objects = root.iterfind(".//object[@id='%s']" % prop.text)
-        check_objects(filename, prop, objects, prop.text)
+        check_objects(filename, tree, elm, objects, prop.text)
 
-def check_rels(filename, root, rels):
+def check_rels(filename, tree, root, elm, rels):
     """
     Check the given list of relations
     """
     for rel in rels:
         target = rel.attrib['target']
         targets = root.iterfind(".//object[@id='%s']" % target)
-        check_objects(filename, rel, targets, target)
+        check_objects(filename, tree, elm, targets, target)
 
-def check_a11y_relation(filename, root):
+def elms_lines(elms):
+    """
+    Return the list of lines for the given elements.
+    """
+    if lxml:
+        return ": lines " + ', '.join([str(l.sourceline) for l in elms])
+    else:
+        return ""
+
+def check_a11y_relation(filename, tree):
     """
     Emit an error message if any of the 'object' elements of the XML
     document represented by `root' doesn't comply with Accessibility
     rules.
     """
+    root = tree.getroot()
 
     for obj in root.iter('object'):
 
         label_for = obj.findall("accessibility/relation[@type='label-for']")
-        check_rels(filename, root, label_for)
+        check_rels(filename, tree, root, obj, label_for)
 
         labelled_by = obj.findall("accessibility/relation[@type='labelled-by']")
-        check_rels(filename, root, labelled_by)
+        check_rels(filename, tree, root, obj, labelled_by)
 
         member_of = obj.findall("accessibility/relation[@type='member-of']")
-        check_rels(filename, root, member_of)
+        check_rels(filename, tree, root, obj, member_of)
 
         if obj.attrib['class'] == 'GtkLabel':
             # Case 0: A 'GtkLabel' must contain one or more "label-for"
@@ -130,14 +193,13 @@ def check_a11y_relation(filename, root):
 
             # ...a single "mnemonic_widget"
             properties = obj.findall("property[@name='mnemonic_widget']")
-            check_props(filename, root, properties)
+            check_props(filename, tree, root, obj, properties)
             if len(properties) > 1:
                 # It does not make sense for a label to be a mnemonic for
                 # several actions.
-                lines = ', '.join([str(p.sourceline) for p in properties])
-                err(filename, obj, "too many sub-elements"
+                err(filename, tree, obj, "multiple-mnemonic", "has too many sub-elements"
                     ", expected single <property name='mnemonic_widgets'>"
-                    ": lines %s" % lines)
+                    "%s" % elms_lines(properties))
                 continue
             if len(properties) == 1:
                 continue
@@ -148,10 +210,9 @@ def check_a11y_relation(filename, root):
         children = obj.findall("child[@internal-child='accessible']")
         if children:
             if len(children) > 1:
-                lines = ', '.join([str(c.sourceline) for c in children])
-                err(filename, obj, "too many sub-elements"
+                err(filename, tree, obj, "multiple-accessible", "has too many sub-elements"
                     ", expected single <child internal-child='accessible'>"
-                    ": lines %s" % lines)
+                    "%s" % elms_lines(children))
             continue
 
         # Case 2: has an <accessibility> sub-element with a "labelled-by"
@@ -164,42 +225,66 @@ def check_a11y_relation(filename, root):
 
 
 def usage():
-    print("%s [-W error|none] [file ...]" % progname,
+    print("%s [-W error|none] [-o LOG_FILE] [file ... | -L filelist]" % progname,
           file=sys.stderr)
+    print("  -o Also prints errors and warnings to given file");
     sys.exit(2)
 
 
 def main():
-    global Werror, Wnone, errors
+    global Werror, Wnone, errors, outfile
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "pW:")
+        opts, args = getopt.getopt(sys.argv[1:], "W:o:L:")
     except getopt.GetoptError:
         usage()
 
+    out = None
+    filelist = None
     for o, a in opts:
         if o == "-W":
             if a == "error":
                 Werror = True
             elif a == "none":
                 Wnone = True
+        elif o == "-o":
+            out = a
+        elif o == "-L":
+            filelist = a
+
+    if out is not None:
+        outfile = open(out, 'w')
+
+    if filelist is not None:
+        try:
+            filelistfile = open(filelist, 'r')
+            for line in filelistfile.readlines():
+                line = line.strip()
+                if line:
+                    args += line.split(' ')
+            filelistfile.close()
+        except IOError:
+            err(filelist, None, None, "unable to read file list file")
 
     for filename in args:
         try:
             tree = ET.parse(filename)
         except ET.ParseError:
-            err(filename, None, "malformatted xml file")
+            err(filename, None, None, "malformatted xml file")
+            continue
         except IOError:
-            err(filename, None, "unable to read file")
+            err(filename, None, None, "unable to read file")
+            continue
 
         try:
-            check_a11y_relation(filename, tree.getroot())
+            check_a11y_relation(filename, tree)
         except Exception as error:
             import traceback
             traceback.print_exc()
-            err(filename, None, "error parsing file")
+            err(filename, None, None, "error parsing file")
 
-
+    if outfile is not None:
+        outfile.close()
     if errors > 0:
         sys.exit(1)
 

config_host.mk.in
@@ -460,7 +460,6 @@ export PTHREAD_LIBS=@PTHREAD_LIBS@
 export PYTHON_CFLAGS=$(gb_SPACE)@PYTHON_CFLAGS@
 export PYTHON_FOR_BUILD=@PYTHON_FOR_BUILD@
 export PYTHON_LIBS=$(gb_SPACE)@PYTHON_LIBS@
-export PYTHON_LXML=@PYTHON_LXML@
 export PYTHON_VERSION=@PYTHON_VERSION@
 export PYTHON_VERSION_MAJOR=@PYTHON_VERSION_MAJOR@
 export PYTHON_VERSION_MINOR=@PYTHON_VERSION_MINOR@

download.lst
@@ -160,6 +160,8 @@ export LIBXSLT_VERSION_MICRO := 32
 export LIBXSLT_TARBALL := libxslt-1.1.$(LIBXSLT_VERSION_MICRO).tar.gz
 export LPSOLVE_SHA256SUM := 171816288f14215c69e730f7a4f1c325739873e21f946ff83884b350574e6695
 export LPSOLVE_TARBALL := 26b3e95ddf3d9c077c480ea45874b3b8-lp_solve_5.5.tar.gz
+export LXML_SHA256SUM := 940caef1ec7c78e0c34b0f6b94fe42d0f2022915ffc78643d28538a5cfd0f40e
+export LXML_TARBALL := lxml-4.1.1.tgz
 export MARIADB_CONNECTOR_C_SHA256SUM := fd2f751dea049c1907735eb236aeace1d811d6a8218118b00bbaa9b84dc5cd60
 export MARIADB_CONNECTOR_C_TARBALL := a233181e03d3c307668b4c722d881661-mariadb_client-2.0.0-src.tar.gz
 export MDDS_SHA256SUM := dcb8cd2425567a5a5ec164afea475bce57784bca3e352ad4cbdd3d1a7e08e5a1

commit df9f5f4815a54694048ef6257421d90bb9928d10

configure,gbuild: gla11y fails on Fedora 31
The problem is that the LD_LIBRARY_PATH on the command line causes
/usr/bin/python to find LO's libpython*.so*:

     18269:	find library=libpython3.7m.so.1.0 [0]; searching
     18269:	 search path=.../instdir/program		(LD_LIBRARY_PATH)
     18269:	  trying file=.../instdir/program/libpython3.7m.so.1.0

Presumably LD_LIBRARY_PATH is used to find bundled libxml/libxslt.

So let's try to disable the broken case where a bundled lxml is used
with system python and bundled libxml/libxslt; this cannot work.

(regression from 2ea0cb40b5aa3afc97788544b9010806321b8da1)

Change-Id: I67aa8250691cae8f899d65f674aa9da23a9d1d7a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90906
Reviewed-by: Samuel Thibault <sthibault@hypra.fr>
Reviewed-by: Michael Stahl <michael.stahl@cib.de>
Tested-by: Jenkins

configure.ac
@@ -8951,8 +8951,12 @@ if test "$with_lxml" != no; then
                     ;;
                 *)
                     if test "$cross_compiling" != yes -a "x$ac_cv_header_Python_h" = "xyes"; then
-                        BUILD_TYPE="$BUILD_TYPE LXML"
-                        AC_MSG_RESULT([no, using internal lxml])
+                        if test -n ${SYSTEM_LIBXSLT} -o -n ${SYSTEM_LIBXML}; then
+                            AC_MSG_RESULT([no, and no system libxml/libxslt, gla11y will only report widget classes and ids])
+                        else
+                            BUILD_TYPE="$BUILD_TYPE LXML"
+                            AC_MSG_RESULT([no, using internal lxml])
+                        fi
                     else
                         AC_MSG_RESULT([no, and system does not provide python development headers, gla11y will only report widget classes and ids])
                     fi

solenv/gbuild/platform/com_GCC_class.mk
@@ -215,7 +215,7 @@ endef
 
 define gb_UIConfig__gla11y_command
 $(call gb_Helper_abbreviate_dirs,\
-	$(gb_UIConfig_LXML_PATH) $(gb_Helper_set_ld_path) \
+	$(gb_UIConfig_LXML_PATH) $(if $(SYSTEM_LIBXML)$(SYSTEM_LIBXSLT),,$(gb_Helper_set_ld_path)) \
 	$(call gb_ExternalExecutable_get_command,python) \
 	$(gb_UIConfig_gla11y_SCRIPT) $(gb_UIConfig_gla11y_PARAMETERS) -o $@ $(UIFILES)
 )

solenv/gbuild/platform/com_MSC_class.mk
@@ -654,7 +654,7 @@ define gb_UIConfig__gla11y_command
 $(call gb_ExternalExecutale__check_registration,python)
 $(call gb_Helper_abbreviate_dirs,\
 	FILES=$(call var2file,$(shell $(gb_MKTEMP)),100,$(UIFILES)) && \
-	$(gb_UIConfig_LXML_PATH) $(gb_Helper_set_ld_path) \
+	$(gb_UIConfig_LXML_PATH) $(if $(SYSTEM_LIBXML)$(SYSTEM_LIBXSLT),,$(gb_Helper_set_ld_path)) \
 	$(call gb_ExternalExecutable_get_command,python) \
 	$(gb_UIConfig_gla11y_SCRIPT) $(gb_UIConfig_gla11y_PARAMETERS) -o $@ -L $$FILES
 )