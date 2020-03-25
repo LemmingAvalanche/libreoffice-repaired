LemmingAvalanche/libreoffice-repaired

commit 3575d9df284050e29e2ff377a944cdbe6840a2b7

build nss using their new build system (gyp/ninja-based)
This requires installed ninja, gyp is included as source. This
allows nss be built as a parallel build, unlike the old Makefile
build system.
Since gyp internally uses python, even recursively, this requires
more complicated setup in case our internal python is used. Moreover
gyp itself seems to be kind of deprecated itself and hasn't
been ported to python3 yet, so that needs patching too.
So far only easy Unix-like systems are converted, Windows I'll
do later, the more complicated systems I'll leave to whoever
has access to them.

Change-Id: I358baad7690d2aa6df44bafa9244dc7cc828fc3f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90115
Tested-by: Jenkins
Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

external/nss/UnpackedTarball_nss.mk
@@ -34,6 +34,8 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
 		external/nss/nss-android.patch.1) \
 	external/nss/nss.bzmozilla1238154.patch \
     external/nss/macos-dlopen.patch.0 \
+	external/nss/buildsystem.patch.0 \
+	external/nss/fix-cc-detect-with-ccache.patch.0 \
 ))
 
 ifeq ($(COM_IS_CLANG),TRUE)

Makefile.fetch
@@ -131,6 +131,7 @@ $(WORKDIR)/download: $(BUILDDIR)/config_$(gb_Side).mk $(SRCDIR)/download.lst $(S
 		$(call fetch_Optional,GLM,GLM_TARBALL) \
 		$(call fetch_Optional,GPGMEPP,GPGME_TARBALL) \
 		$(call fetch_Optional,GRAPHITE,GRAPHITE_TARBALL) \
+		$(call fetch_Optional,GYP,GYP_TARBALL) \
 		$(call fetch_Optional,HARFBUZZ,HARFBUZZ_TARBALL) \
 		$(call fetch_Optional,HSQLDB,HSQLDB_TARBALL) \
 		$(call fetch_Optional,HUNSPELL,HUNSPELL_TARBALL) \

configure.ac
@@ -9667,6 +9667,10 @@ if test $_os != iOS -a "$enable_fuzzers" != "yes"; then
     AC_DEFINE(HAVE_FEATURE_NSS)
     ENABLE_NSS="TRUE"
     AC_DEFINE(ENABLE_NSS)
+    # internal nss uses gyp
+    if test -z "$SYSTEM_NSS"; then
+       BUILD_TYPE="$BUILD_TYPE GYP"
+    fi
 elif test $_os != iOS ; then
     with_tls=openssl
 fi

download.lst
@@ -99,6 +99,8 @@ export GPGME_SHA256SUM := 1b29fedb8bfad775e70eafac5b0590621683b2d9869db994568e64
 export GPGME_TARBALL := gpgme-1.9.0.tar.bz2
 export GRAPHITE_SHA256SUM := d47d387161db7f7ebade1920aa7cbdc797e79772597d8b55e80b58d1071bcc36
 export GRAPHITE_TARBALL := graphite2-minimal-1.3.13.tgz
+export GYP_SHA256SUM := 53cb1f2b19048544950422cd86c6093ba2c4ff9748e27cc560dfff9791700c9d
+export GYP_TARBALL := gyp-e87d37d6bce611abed35e854d5ae1a401e9ce04c.tar.xz
 export HARFBUZZ_SHA256SUM := 9cf7d117548265f95ca884e2f4c9fafaf4e17d45a67b11107147b79eed76c966
 export HARFBUZZ_TARBALL := harfbuzz-2.6.0.tar.xz
 export HSQLDB_SHA256SUM := d30b13f4ba2e3b6a2d4f020c0dee0a9fb9fc6fbcc2d561f36b78da4bf3802370

external/nss/ExternalProject_nss.mk
@@ -34,7 +34,31 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecuta
 	,nss)
 	$(call gb_Trace_EndRange,nss,EXTERNAL)
 
-else # OS!=WNT
+else ifneq (,$(filter FREEBSD LINUX NETBSD OPENBSD SOLARIS,$(OS))) # non-WNT gyp-based
+
+# The nss build system uses 'python', so make it find our internal python if necessary.
+nss_PYTHON := $(call gb_UnpackedTarball_get_dir,nss)/python
+nss_SETUP_PYTHON := $(call gb_UnpackedTarball_get_dir,nss)/setup-python
+
+$(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecutable_get_dependencies,python)
+	$(call gb_Trace_StartRange,nss,EXTERNAL)
+	echo "#! /bin/sh" > $(nss_PYTHON)
+	pythondir=$$($(call gb_ExternalExecutable_get_command,python) -c 'import sys; import os; sys.stdout.write(os.path.dirname(sys.executable))') \
+		&& echo PATH=\"$$pythondir:\$$PATH\" >>$(nss_PYTHON)
+	echo '$(call gb_ExternalExecutable_get_command,python)' \"$$\@\" $(if $(ICECREAM_RUN), | sed 's/$(ICECREAM_RUN)//') >> $(nss_PYTHON)
+	chmod +x $(nss_PYTHON)
+	cp $(SRCDIR)/external/nss/setup-python $(nss_SETUP_PYTHON)
+	chmod +x $(nss_SETUP_PYTHON)
+	$(call gb_ExternalProject_run,build,\
+			COMMA=$(COMMA) \
+			PATH=$(call gb_UnpackedTarball_get_dir,nss):$(call gb_UnpackedTarball_get_dir,gyp):$$PATH \
+			./build.sh -v --disable-tests --enable-libpkix \
+				$(if $(ENABLE_DBGUTIL),,--opt) \
+		&& rm -f $(call gb_UnpackedTarball_get_dir,nss)/dist/out/lib/*.a \
+	,nss)
+	$(call gb_Trace_EndRange,nss,EXTERNAL)
+
+else # platforms which need(?) the old build system (feel free to port to the new system)
 # make sure to specify NSPR_CONFIGURE_OPTS as env (before make command), so nss can append it's own defaults
 # OTOH specify e.g. CC and NSINSTALL as arguments (after make command), so they will overrule nss makefile values
 $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecutable_get_dependencies,python)

external/nss/UnpackedTarball_gyp.mk
@@ -7,13 +7,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Module_Module,nss))
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,gyp))
 
-$(eval $(call gb_Module_add_targets,nss,\
-	UnpackedTarball_nss \
-	UnpackedTarball_gyp \
-	ExternalPackage_nss \
-	ExternalProject_nss \
+$(eval $(call gb_UnpackedTarball_set_tarball,gyp,$(GYP_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_add_patches,gyp,\
+	external/nss/gyp-buildsystem.patch.0 \
 ))
 
 # vim: set noet sw=4 ts=4:

external/nss/buildsystem.patch.0
@@ -1,0 +1,32 @@
+--- ./nss/coreconf/nspr.sh.sav	2019-11-19 20:55:30.000000000 +0100
++++ ./nss/coreconf/nspr.sh	2020-02-19 22:25:05.863542364 +0100
+@@ -28,7 +28,7 @@
+ 
+     # These NSPR options are directory-specific, so they don't need to be
+     # included in nspr_opt and changing them doesn't force a rebuild of NSPR.
+-    extra_params=(--prefix="$dist_dir"/$target)
++    extra_params=(--prefix="$dist_dir"/out --libdir="$dist_dir"/out/lib --includedir="$dist_dir"/out/include)
+     if [ "$opt_build" = 1 ]; then
+         extra_params+=(--disable-debug --enable-optimize)
+     fi
+--- ./nss/build.sh.sav	2019-11-19 20:55:30.000000000 +0100
++++ ./nss/build.sh	2020-02-19 22:28:30.976068194 +0100
+@@ -243,10 +243,10 @@
+         exit 3
+     fi
+     # These extra arguments aren't used in determining whether to rebuild.
+-    obj_dir="$dist_dir/$target"
++    obj_dir="$dist_dir/out"
+     gyp_params+=(-Dnss_dist_obj_dir="$obj_dir")
+     if [ "$no_local_nspr" = 0 ]; then
+-        set_nspr_path "$obj_dir/include/nspr:$obj_dir/lib"
++        set_nspr_path "$obj_dir/include:$obj_dir/lib"
+     fi
+ 
+     run_verbose run_scanbuild ${GYP} -f ninja "${gyp_params[@]}" "$cwd/nss.gyp"
+@@ -263,4 +263,4 @@
+     echo "Building NSS requires an installation of ninja: https://ninja-build.org/" 1>&2
+     exit 3
+ fi
+-run_scanbuild "$ninja" -C "$target_dir" "${ninja_params[@]}"
++run_scanbuild setup-python "$ninja" -C "$target_dir" "${ninja_params[@]}"

external/nss/fix-cc-detect-with-ccache.patch.0
@@ -1,0 +1,11 @@
+--- ./nss/coreconf/check_cc.py.sav	2020-03-20 17:58:57.275605539 +0100
++++ ./nss/coreconf/check_cc.py	2020-03-20 18:10:56.053688945 +0100
+@@ -11,7 +11,7 @@ def main():
+         cc = os.environ.get('CC', 'cc')
+         try:
+             cc_is_arg = sys.argv[1] in subprocess.check_output(
+-              [cc, '--version'], universal_newlines=True)
++              cc.split(' ') + ['--version'], universal_newlines=True)
+         except OSError:
+             # We probably just don't have CC/cc.
+             cc_is_arg = False

commit 373c7ab43f8b81eb17b822b68566e21a3d952a36

nss: fix this up to work with Online again
Regression from commit 3575d9df284050e29e2ff377a944cdbe6840a2b7 (build
nss using their new build system (gyp/ninja-based), 2020-02-19), the
problem was that for some reason nss was pre-loaded in the past in some
magic way, but after that commit we need to do this explicitly.

Additionally, if the core.git build is a debug one, we now get an nss
assert that checks if there was a fork between loading and using nss. I
don't think that was enabled before, so just patch that out to fix the
build breakage on the online.git side.

online.git's unit-password-protected now passes again with this.

Change-Id: I56c0019c446b4cabbb73141bbff80e8d8ffdb54c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91068
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

desktop/source/lib/init.cxx
@@ -164,6 +164,8 @@
 #include "lokinteractionhandler.hxx"
 #include "lokclipboard.hxx"
 #include <officecfg/Office/Impress.hxx>
+#include <comphelper/hash.hxx>
+#include <comphelper/storagehelper.hxx>
 
 using namespace css;
 using namespace vcl;
@@ -5686,6 +5688,12 @@ static void preloadData()
     // force load language singleton
     SvtLanguageTable::HasLanguageType(LANGUAGE_SYSTEM);
     (void)LanguageTag::isValidBcp47("foo", nullptr);
+
+    std::cerr << "Preload nss\n";
+    {
+        comphelper::Hash aHash(comphelper::HashType::SHA256);
+        comphelper::OStorageHelper::CreatePackageEncryptionData("x");
+    }
 
     std::cerr << "Preload fonts\n";
 

external/nss/UnpackedTarball_nss.mk
@@ -36,6 +36,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
     external/nss/macos-dlopen.patch.0 \
 	external/nss/buildsystem.patch.0 \
 	external/nss/fix-cc-detect-with-ccache.patch.0 \
+	external/nss/nss-online.patch.0 \
 ))
 
 ifeq ($(OS),WNT)

external/nss/nss-online.patch.0
@@ -1,0 +1,11 @@
+--- nss/lib/softoken/softoken.h.orig	2020-03-25 18:51:38.786887278 +0100
++++ nss/lib/softoken/softoken.h	2020-03-25 18:51:47.450997085 +0100
+@@ -145,7 +145,7 @@
+ /*
+ ** macros to check for forked child process after C_Initialize
+ */
+-#if defined(XP_UNIX) && !defined(NO_FORK_CHECK)
++#if 0
+ 
+ #ifdef DEBUG
+ 