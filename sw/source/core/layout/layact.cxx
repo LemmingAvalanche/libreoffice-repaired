LemmingAvalanche/libreoffice-repaired

commit 5edacef4837375cddbb6b069204dfc72508bb9bf

sw: fix moving more than 20 table frames to a previous page
Steps to reproduce the problem:

- have some content on page1
- have more than 20 tables on page 2
- delete all content on page 1

The first 20 tables are moved to page 1 then the layout process stops as
the layout loop control aborts it:

warn:legacy.osl:8282:8282:sw/source/core/layout/layact.cxx:544: LoopControl_1 in SwLayAction::InternalAction

and the remaining tables stay on page 2, even if page 1 would have space
for them.

There are various other ways to trigger the same problem, e.g. have a
ToC, add lots of headings, update the ToC, undo.

Fix the problem by doing more work in SwLayAction::FormatLayout in a
single iteration: if a table frame is moved to a different parent we can
still format the table's next frame in the same iteration with a bit of
effort.

Change-Id: I25912a69c19e042f0e0375898f4e0a5fa13321fc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91377
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/layact.cxx
@@ -1354,6 +1354,7 @@ bool SwLayAction::FormatLayout( OutputDevice *pRenderContext, SwLayoutFrame *pLa
     bool bTabChanged = false;
     while ( pLow && pLow->GetUpper() == pLay )
     {
+        SwFrame* pNext = nullptr;
         if ( pLow->IsLayoutFrame() )
         {
             if ( pLow->IsTabFrame() )
@@ -1364,6 +1365,11 @@ bool SwLayAction::FormatLayout( OutputDevice *pRenderContext, SwLayoutFrame *pLa
                 }
 
                 ++m_nTabLevel;
+
+                // Remember what was the next of the lower. Formatting may move it to the previous
+                // page, in which case it looses its next.
+                pNext = pLow->GetNext();
+
                 bTabChanged |= FormatLayoutTab( static_cast<SwTabFrame*>(pLow), bAddRect );
                 --m_nTabLevel;
             }
@@ -1378,7 +1384,11 @@ bool SwLayAction::FormatLayout( OutputDevice *pRenderContext, SwLayoutFrame *pLa
 
         if ( IsAgain() )
             return false;
-        pLow = pLow->GetNext();
+        if (!pNext)
+        {
+            pNext = pLow->GetNext();
+        }
+        pLow = pNext;
     }
     // add complete frame area as paint area, if frame
     // area has been already added and after formatting its lowers the frame area

sw/qa/core/layout/layout.cxx
@@ -11,6 +11,8 @@
 
 #include <vcl/gdimtf.hxx>
 
+#include <wrtsh.hxx>
+
 static char const DATA_DIRECTORY[] = "/sw/qa/core/layout/data/";
 
 /// Covers sw/source/core/layout/ fixes.
@@ -90,6 +92,30 @@ CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTableFlyOverlapSpacing)
     // - Actual  : 3993
     // i.e. the table was below the image, not on the left of the image.
     CPPUNIT_ASSERT_LESS(nFlyBottom, nTableTop);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTablesMoveBackwards)
+{
+    // Load a document with 1 pages: empty content on first page, then 21 tables on the second page.
+    load(DATA_DIRECTORY, "tables-move-backwards.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+
+    // Delete the content on the first page.
+    pWrtShell->SttEndDoc(/*bStart=*/true);
+    pWrtShell->EndPg(/*bSelect=*/true);
+    pWrtShell->DelLeft();
+
+    // Calc the layout and check the number of pages.
+    pWrtShell->CalcLayout();
+    xmlDocPtr pLayout = parseLayoutDump();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 2
+    // i.e. there was an unexpected 2nd page, as only 20 out of 21 tables were moved to the first
+    // page.
+    assertXPath(pLayout, "//page", 1);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit fad4bc08acf92210bc6bd2a9aad08349a28e2bc5

sw: fix use-after-free when moving multiple tables to a previous page
Regression from commit 5edacef4837375cddbb6b069204dfc72508bb9bf (sw: fix
moving more than 20 table frames to a previous page, 2020-03-30), asan
found a heap-use-after-free during CppunitTest_sw_ooxmlexport5
CPPUNIT_TEST_NAME=testOldComplexMergeTableInTable, the follow frame is
deleted like this:

 #1 in SwTabFrame::~SwTabFrame() at sw/source/core/layout/tabfrm.cxx:145:1 (instdir/program/libswlo.so +0xec98ba5)
 #2 in SwFrame::DestroyFrame(SwFrame*) at sw/source/core/layout/ssfrm.cxx:389:9 (instdir/program/libswlo.so +0xec8495f)
 #3 in SwTabFrame::Join() at sw/source/core/layout/tabfrm.cxx:1390:9 (instdir/program/libswlo.so +0xecb6088)
 #4 in SwTabFrame::MakeAll(OutputDevice*) at sw/source/core/layout/tabfrm.cxx:1865:9 (instdir/program/libswlo.so +0xecbc1f6)
 #5 in SwFrame::PrepareMake(OutputDevice*) at sw/source/core/layout/calcmove.cxx:370:5 (instdir/program/libswlo.so +0xe519919)
 #6 in SwFrame::Calc(OutputDevice*) const at sw/source/core/layout/trvlfrm.cxx:1789:37 (instdir/program/libswlo.so +0xed8424e)
 #7 in SwLayAction::FormatLayoutTab(SwTabFrame*, bool) at sw/source/core/layout/layact.cxx:1485:15 (instdir/program/libswlo.so +0xe897ea9)

Fix the problem by not moving multiple tables to a previous page in one
iteration when the table is a follow one.

Change-Id: I443240b6153b74d6def97140c516d7cf7a2d35e4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91425
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/layact.cxx
@@ -1370,6 +1370,17 @@ bool SwLayAction::FormatLayout( OutputDevice *pRenderContext, SwLayoutFrame *pLa
                 // page, in which case it looses its next.
                 pNext = pLow->GetNext();
 
+                if (pNext && pNext->IsTabFrame())
+                {
+                    auto pTab = static_cast<SwTabFrame*>(pNext);
+                    if (pTab->IsFollow())
+                    {
+                        // The next frame is a follow of the previous frame, SwTabFrame::Join() will
+                        // delete this one as part of formatting, so forget about it.
+                        pNext = nullptr;
+                    }
+                }
+
                 bTabChanged |= FormatLayoutTab( static_cast<SwTabFrame*>(pLow), bAddRect );
                 --m_nTabLevel;
             }