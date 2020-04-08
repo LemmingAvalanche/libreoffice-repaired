LemmingAvalanche/libreoffice-repaired

commit 20ce9f46046e716617cf62794bf76eb132397839

Avoid -fsanitize=misaligned-pointer-use
...like

> vcl/source/filter/jpeg/Exif.cxx:158:31: runtime error: member access within misaligned address 0x624000a5610a for type 'Exif::ExifIFD', which requires 4 byte alignment
> 0x624000a5610a: note: pointer points here
>  00 00  00 07 01 12 00 03 00 00  00 01 00 01 00 00 01 1a  00 05 00 00 00 01 00 00  00 62 01 1b 00 05
>               ^
>  #0 in Exif::processIFD(unsigned char*, unsigned short, unsigned short, unsigned short, bool, bool) at vcl/source/filter/jpeg/Exif.cxx:158:31
>  #1 in Exif::processExif(SvStream&, unsigned short, bool) at vcl/source/filter/jpeg/Exif.cxx:255:5
>  #2 in Exif::processJpeg(SvStream&, bool) at vcl/source/filter/jpeg/Exif.cxx:132:20
[...]

when inserting some .jpg into Draw.

The swapping in Exif::processIFD had been introduced with
b2ee7cf2a4787f9af243f5336828ebfb9af8e645 "fdo#57659: fix exif processing", and
it had probably been an oversight that it used OSL_SWAPWORD instead of
OSL_SWAPDWORD to swap sal_uInt32 ifd->offset (in two places), and that it failed
to swap ifd->type and ifd->count in the bSetValue branch (see the discussion at
<https://gerrit.libreoffice.org/#/c/6245/> "fdo#57659: fix exif processing").

Change-Id: If40ddb9b1ef4e2ffc08a0899920919989e8cdfdc
Reviewed-on: https://gerrit.libreoffice.org/74236
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

vcl/source/filter/jpeg/Exif.cxx
@@ -148,38 +148,73 @@ bool Exif::processJpeg(SvStream& rStream, bool bSetValue)
     return false;
 }
 
-void Exif::processIFD(sal_uInt8* pExifData, sal_uInt16 aLength, sal_uInt16 aOffset, sal_uInt16 aNumberOfTags, bool bSetValue, bool bSwap)
+namespace {
+
+sal_uInt16 read16(sal_uInt8 const * data, bool littleEndian) {
+    if (littleEndian) {
+        return data[0] | (sal_uInt16(data[1]) << 8);
+    } else {
+        return data[1] | (sal_uInt16(data[0]) << 8);
+    }
+}
+
+void write16(sal_uInt16 value, sal_uInt8 * data, bool littleEndian) {
+    if (littleEndian) {
+        data[0] = value & 0xFF;
+        data[1] = value >> 8;
+    } else {
+        data[1] = value & 0xFF;
+        data[0] = value >> 8;
+    }
+}
+
+sal_uInt32 read32(sal_uInt8 const * data, bool littleEndian) {
+    if (littleEndian) {
+        return data[0] | (sal_uInt32(data[1]) << 8)
+            | (sal_uInt32(data[2]) << 16) | (sal_uInt32(data[3]) << 24);
+    } else {
+        return data[3] | (sal_uInt32(data[2]) << 8)
+            | (sal_uInt32(data[1]) << 16) | (sal_uInt32(data[0]) << 24);
+    }
+}
+
+void write32(sal_uInt32 value, sal_uInt8 * data, bool littleEndian) {
+    if (littleEndian) {
+        data[0] = value & 0xFF;
+        data[1] = (value >> 8) & 0xFF;
+        data[2] = (value >> 16) & 0xFF;
+        data[3] = value >> 24;
+    } else {
+        data[3] = value & 0xFF;
+        data[2] = (value >> 8) & 0xFF;
+        data[1] = (value >> 16) & 0xFF;
+        data[0] = value >> 24;
+    }
+}
+
+}
+
+void Exif::processIFD(sal_uInt8* pExifData, sal_uInt16 aLength, sal_uInt16 aOffset, sal_uInt16 aNumberOfTags, bool bSetValue, bool littleEndian)
 {
     ExifIFD* ifd = nullptr;
 
     while (aOffset <= aLength - 12 && aNumberOfTags > 0)
     {
         ifd = reinterpret_cast<ExifIFD*>(&pExifData[aOffset]);
-        sal_uInt16 tag = ifd->tag;
-        if (bSwap)
-        {
-            tag = OSL_SWAPWORD(ifd->tag);
-        }
+        sal_uInt16 tag = read16(ifd->tag, littleEndian);
 
         if (tag == ORIENTATION)
         {
             if(bSetValue)
             {
-                ifd->tag = ORIENTATION;
-                ifd->type = 3;
-                ifd->count = 1;
-                ifd->offset = maOrientation;
-                if (bSwap)
-                {
-                    ifd->tag = OSL_SWAPWORD(ifd->tag);
-                    ifd->offset = OSL_SWAPWORD(ifd->offset);
-                }
+                write16(ORIENTATION, ifd->tag, littleEndian);
+                write16(3, ifd->type, littleEndian);
+                write32(1, ifd->count, littleEndian);
+                write32(maOrientation, ifd->offset, littleEndian);
             }
             else
             {
-                sal_uInt32 nIfdOffset = ifd->offset;
-                if (bSwap)
-                    nIfdOffset = OSL_SWAPWORD(ifd->offset);
+                sal_uInt32 nIfdOffset = read32(ifd->offset, littleEndian);
                 maOrientation = convertToOrientation(nIfdOffset);
             }
         }
@@ -252,7 +287,7 @@ bool Exif::processExif(SvStream& rStream, sal_uInt16 aSectionLength, bool bSetVa
         aNumberOfTags = ((aExifData[aOffset] << 8) | aExifData[aOffset+1]);
     }
 
-    processIFD(aExifData.get(), aLength, aOffset+2, aNumberOfTags, bSetValue, bSwap);
+    processIFD(aExifData.get(), aLength, aOffset+2, aNumberOfTags, bSetValue, bIntel);
 
     if (bSetValue)
     {

vcl/source/filter/jpeg/Exif.hxx
@@ -49,13 +49,13 @@ private:
 
     bool processJpeg(SvStream& rStream, bool bSetValue);
     bool processExif(SvStream& rStream, sal_uInt16 aLength, bool bSetValue);
-    void processIFD(sal_uInt8* pExifData, sal_uInt16 aLength, sal_uInt16 aOffset, sal_uInt16 aNumberOfTags, bool bSetValue, bool bMoto);
+    void processIFD(sal_uInt8* pExifData, sal_uInt16 aLength, sal_uInt16 aOffset, sal_uInt16 aNumberOfTags, bool bSetValue, bool bLittleEndian);
 
     struct ExifIFD {
-        sal_uInt16 tag;
-        sal_uInt16 type;
-        sal_uInt32 count;
-        sal_uInt32 offset;
+        sal_uInt8 tag[2];
+        sal_uInt8 type[2];
+        sal_uInt8 count[4];
+        sal_uInt8 offset[4];
     };
 
     struct TiffHeader {

commit 1872242d4f1d0e4792495a4a0e241b9ca902e10d

tdf#131969: Fix reading SHORT Orientation value
<http://www.cipa.jp/std/documents/e/DC-008-2012_E.pdf> "CIPA DC- 008-Translation-
2012: Exchangeable image file format for digital still cameras: Exif Version 2.3"
documents that the Orientation tag 0x0112 expects a count of 1 values of type SHORT
(16 bit), and details that values <= 4 bytes are stored in the Value Offset field
always using bytes starting from the left of the field.

This is a regression introduced with 20ce9f46046e716617cf62794bf76eb132397839 "Avoid
-fsanitize=misaligned-pointer-use".  That commit had wondered why the original code
had used OSL_SWAPWORD instead of OSL_SWAPDWORD when reading and writing such
orientation values.  It turns out that that original code had happened to work
correctly when processing either little or big endian data on a little endian
machine.  (Though it would have worked incorrectly when processing either little or
big endian data on a big endian machine.)  And with
20ce9f46046e716617cf62794bf76eb132397839, the code worked when processing little
endian data on a little endian machine, but failed when processing big endian data
on a little endian machine, as is the case for tdf#131669 on e.g. x86_64.

(read32 has become unused and is thus removed.)

Change-Id: I7992629048ac44c00ee703c75164f3d094773244
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91881
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/filter/jpeg/Exif.cxx
@@ -171,16 +171,6 @@ void write16(sal_uInt16 value, sal_uInt8 (& data)[2], bool littleEndian) {
     }
 }
 
-sal_uInt32 read32(sal_uInt8 const (& data)[4], bool littleEndian) {
-    if (littleEndian) {
-        return data[0] | (sal_uInt32(data[1]) << 8)
-            | (sal_uInt32(data[2]) << 16) | (sal_uInt32(data[3]) << 24);
-    } else {
-        return data[3] | (sal_uInt32(data[2]) << 8)
-            | (sal_uInt32(data[1]) << 16) | (sal_uInt32(data[0]) << 24);
-    }
-}
-
 void write32(sal_uInt32 value, sal_uInt8 (& data)[4], bool littleEndian) {
     if (littleEndian) {
         data[0] = value & 0xFF;
@@ -212,11 +202,13 @@ void Exif::processIFD(sal_uInt8* pExifData, sal_uInt16 aLength, sal_uInt16 aOffs
             {
                 write16(3, ifd->type, littleEndian);
                 write32(1, ifd->count, littleEndian);
-                write32(maOrientation, ifd->offset, littleEndian);
+                write16(
+                    maOrientation, reinterpret_cast<sal_uInt8 (&)[2]>(ifd->offset), littleEndian);
             }
             else
             {
-                sal_uInt32 nIfdOffset = read32(ifd->offset, littleEndian);
+                sal_uInt16 nIfdOffset = read16(
+                    reinterpret_cast<sal_uInt8 (&)[2]>(ifd->offset), littleEndian);
                 maOrientation = convertToOrientation(nIfdOffset);
             }
         }