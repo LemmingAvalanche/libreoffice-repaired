LemmingAvalanche/libreoffice-repaired

commit 51c97e5e3ae88c43e611a4e6a61e375c66d5d007

tdf#121300 sw: consistent fly at-pargraph selection
As a follow-up to commit 245b52ee39d03c62998acba87405ead7afc51d71, add
IsSelectFrameAnchoredAtPara() function and use it to harmonize at-para
fly selection across all relevant operations:

* CopyImpl:
  - it had a pre-existing bugs that would lose a fly anchored to the
    2nd (1st fully selected) node of a redline
  - remove a bunch of code for finding the last node of the body content,
    which doesn't matter for the remaining at-fly checks
  - flys that already existed at the insert position need to have their
    anchors corrected

* DeleteRange:
  - get rid of the bDelFwrd checks

* MoveRange:
  - the ALLFLYS flag would be obsoleted by the new selection, were it
    not for the writerfiler "special hack", see below

* also adapt A11y and UI selection, SwRootFrame::CalcFrameRects()

The selection behavior is changed:

* the bDelFwrd case is quite odd, some code was checking whether a
  deletion was "forward" or "backward" and removing only the flys at the
  point node while retaining the flys at the mark node; this worked in a
  very non-obvious way relying on sw_GetJoinFlags actually calling
  Exchange() on the cursor, and then SwUndoDelete stored it in
  m_bJoinNext, but it turns out that only SwUndoMove also has a
  m_bJoinNext and it's dead, and no other Undo has such a flag, so this
  only worked for "delete". It's not obvious what the value of this is,
  let's just ignore the "direction".

* Selections exclude the start and end position except if it's a fully
  selected node or at the start or end of a section (i.e. Ctrl+A should
  also select every at-para fly).

* An exception is made in case the selection looks like it's a
  backspace/delete joining 2 paragraphs; flys are not deleted in that
  case because it seemed annoying (and as it happens, Word does not
  appear to delete flys in that case), particularly if both of the nodes
  are already empty. This is done with a heuristic, it's conceivable to

sw/source/core/doc/docedt.cxx
@@ -138,14 +138,14 @@ void SaveFlyInRange( const SwPaM& rPam, const SwPosition& rInsPos,
 
     const SwPosition* pPos = rPam.Start();
     const SwNodeIndex& rSttNdIdx = pPos->nNode;
-    short nSttOff = (!bMoveAllFlys && rSttNdIdx.GetNode().IsContentNode() &&
-                    pPos->nContent.GetIndex()) ? 1 : 0;
 
-    pPos = rPam.GetPoint() == pPos ? rPam.GetMark() : rPam.GetPoint();
-    const SwNodeIndex& rEndNdIdx = pPos->nNode;
-    short nOff = ( bMoveAllFlys || ( rEndNdIdx.GetNode().IsContentNode() &&
-                pPos->nContent == rEndNdIdx.GetNode().GetContentNode()->Len() ))
-                    ? 0 : 1;
+    SwPosition atParaEnd(*rPam.End());
+    if (bMoveAllFlys)
+    {
+        assert(rPam.End()->nContent.GetIndex() == rPam.End()->nNode.GetNode().GetTextNode()->Len());
+        ++atParaEnd.nNode;
+        atParaEnd.nContent.Assign(atParaEnd.nNode.GetNode().GetContentNode(), 0);
+    }
 
     for( SwFrameFormats::size_type n = 0; n < rFormats.size(); ++n )
     {
@@ -163,27 +163,13 @@ void SaveFlyInRange( const SwPaM& rPam, const SwPosition& rInsPos,
         {
             bool bInsPos = false;
 
-            if (!bMoveAllFlys
-                && RndStdIds::FLY_AT_CHAR != pAnchor->GetAnchorId()
-                && rEndNdIdx == pAPos->nNode)
-            {
-                // Do not touch Anchor, if only a part of the EndNode
-                // or the whole EndNode is identical with the SttNode
-                if( rSttNdIdx != pAPos->nNode )
-                {
-                    // Only attach an anchor to the beginning or end
-                    SwPosition aPos( rSttNdIdx );
-                    SwFormatAnchor aAnchor( *pAnchor );
-                    aAnchor.SetAnchor( &aPos );
-                    pFormat->SetFormatAttr( aAnchor );
-                }
-            }
-            else if (  (//bMoveAllFlys ... no do not check - all callers are actually from redline code, from the MoveToSection case; so check bMoveAllFlys only for AT_PARA!
-                           (RndStdIds::FLY_AT_CHAR == pAnchor->GetAnchorId())
+            if (       (RndStdIds::FLY_AT_CHAR == pAnchor->GetAnchorId()
                         && IsDestroyFrameAnchoredAtChar(*pAPos, *rPam.Start(), *rPam.End()))
                     || (RndStdIds::FLY_AT_PARA == pAnchor->GetAnchorId()
-                        && rSttNdIdx.GetIndex() + nSttOff <= pAPos->nNode.GetIndex()
-                        && pAPos->nNode.GetIndex() <= rEndNdIdx.GetIndex() - nOff)
+                        && IsSelectFrameAnchoredAtPara(*pAPos, *rPam.Start(), atParaEnd,
+                            bMoveAllFlys
+                                ? DelContentType::CheckNoCntnt|DelContentType::AllMask
+                                : DelContentType::AllMask))
                     || (RndStdIds::FLY_AT_PARA == pAnchor->GetAnchorId()
                             && (bInsPos = (rInsPos.nNode == pAPos->nNode)))
                     || (RndStdIds::FLY_AT_CHAR == pAnchor->GetAnchorId()
@@ -224,7 +210,6 @@ void DelFlyInRange( const SwNodeIndex& rMkNdIdx,
                             : SwPosition(rMkNdIdx));
     SwPosition const& rStart = mark <= point ? mark : point;
     SwPosition const& rEnd   = mark <= point ? point : mark;
-    const bool bDelFwrd = rMkNdIdx.GetIndex() <= rPtNdIdx.GetIndex();
 
     SwDoc* pDoc = rMkNdIdx.GetNode().GetDoc();
     SwFrameFormats& rTable = *pDoc->GetSpzFrameFormats();
@@ -235,25 +220,14 @@ void DelFlyInRange( const SwNodeIndex& rMkNdIdx,
         SwPosition const*const pAPos = rAnch.GetContentAnchor();
         if (pAPos &&
             (((rAnch.GetAnchorId() == RndStdIds::FLY_AT_PARA)
-                && (bDelFwrd
-                    ? rMkNdIdx < pAPos->nNode && pAPos->nNode <= rPtNdIdx
-                    : rPtNdIdx <= pAPos->nNode && pAPos->nNode < rMkNdIdx))
+                && IsSelectFrameAnchoredAtPara(*pAPos, rStart, rEnd, pPtIdx
+                    ? DelContentType::AllMask
+                    : DelContentType::AllMask|DelContentType::CheckNoCntnt))
             || ((rAnch.GetAnchorId() == RndStdIds::FLY_AT_CHAR)
                 && IsDestroyFrameAnchoredAtChar(*pAPos, rStart, rEnd, pPtIdx
                     ? DelContentType::AllMask
                     : DelContentType::AllMask|DelContentType::CheckNoCntnt))))
         {
-            // Only move the Anchor??
-            if ((rAnch.GetAnchorId() == RndStdIds::FLY_AT_PARA)
-                && rPtNdIdx == pAPos->nNode )
-            {
-                SwFormatAnchor aAnch( pFormat->GetAnchor() );
-                SwPosition aPos( rMkNdIdx );
-                aAnch.SetAnchor( &aPos );
-                pFormat->SetFormatAttr( aAnch );
-            }
-            else
-            {
                 // If the Fly is deleted, all Flys in its content have to be deleted too.
                 const SwFormatContent &rContent = pFormat->GetContent();
                 // But only fly formats own their content, not draw formats.
@@ -274,7 +248,6 @@ void DelFlyInRange( const SwNodeIndex& rMkNdIdx,
                 // DelLayoutFormat can also trigger the deletion of objects.
                 if( i > rTable.size() )
                     i = rTable.size();
-            }
         }
     }
 }

sw/inc/undobj.hxx
@@ -135,7 +135,7 @@ enum class DelContentType : sal_uInt16
     Fly          = 0x02,
     Bkm          = 0x08,
     AllMask      = 0x0b,
-    ExcludeAtCharFlyAtStartEnd = 0x40,
+    ExcludeFlyAtStartEnd = 0x40,
     CheckNoCntnt = 0x80,
 };
 namespace o3tl {
@@ -144,6 +144,10 @@ namespace o3tl {
 
 /// will DelContentIndex destroy a frame anchored at character at rAnchorPos?
 bool IsDestroyFrameAnchoredAtChar(SwPosition const & rAnchorPos,
+        SwPosition const & rStart, SwPosition const & rEnd,
+        DelContentType const nDelContentType = DelContentType::AllMask);
+/// is a fly anchored at paragraph at rAnchorPos selected?
+bool IsSelectFrameAnchoredAtPara(SwPosition const & rAnchorPos,
         SwPosition const & rStart, SwPosition const & rEnd,
         DelContentType const nDelContentType = DelContentType::AllMask);
 

sw/qa/core/uwriter.cxx
@@ -1165,8 +1165,7 @@ void SwDocTest::randomTest()
                 SwMoveFlags nFlags =
                          getRand(1) // FIXME: puterb this more ?
                          ? SwMoveFlags::DEFAULT
-                         : SwMoveFlags::ALLFLYS |
-                           SwMoveFlags::CREATEUNDOOBJ |
+                         : SwMoveFlags::CREATEUNDOOBJ |
                            SwMoveFlags::REDLINES |
                            SwMoveFlags::NO_DELFRMS;
                 SwPosition aTo(getRandomPosition(m_pDoc, i/10));

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -168,6 +168,8 @@ public:
     void testRedlineFrameAtCharStartInside();
     void testRedlineFrameAtParaStartOutside();
     void testRedlineFrameAtParaEndInside();
+    void testRedlineFrameAtParaOneParagraph();
+    void testRedlineFrameAtPara2ndParagraph();
     void testThreadedException();
     void testBookmarkCopy();
     void testFdo69893();
@@ -368,6 +370,8 @@ public:
     CPPUNIT_TEST(testRedlineFrameAtCharStartInside);
     CPPUNIT_TEST(testRedlineFrameAtParaStartOutside);
     CPPUNIT_TEST(testRedlineFrameAtParaEndInside);
+    CPPUNIT_TEST(testRedlineFrameAtParaOneParagraph);
+    CPPUNIT_TEST(testRedlineFrameAtPara2ndParagraph);
     CPPUNIT_TEST(testThreadedException);
     CPPUNIT_TEST(testBookmarkCopy);
     CPPUNIT_TEST(testFdo69893);
@@ -682,6 +686,18 @@ void SwUiWriterTest::testRedlineFrameAtParaStartOutside()
 void SwUiWriterTest::testRedlineFrameAtParaEndInside()
 {
     testRedlineFrame("redline_fly_duplication_at_para_end_inside.fodt");
+}
+
+void SwUiWriterTest::testRedlineFrameAtParaOneParagraph()
+{
+    // test ALLFLYS flag: oddly enough it didn't fail as fodt but failed as odt...
+    testRedlineFrame("redline_fly_at_para_one_paragraph.odt");
+}
+
+void SwUiWriterTest::testRedlineFrameAtPara2ndParagraph()
+{
+    // lost via the buggy increment in Copy
+    testRedlineFrame("redline_fly_duplication_at_para_2nd_paragraph.fodt");
 }
 
 void SwUiWriterTest::testThreadedException()

sw/qa/uitest/writer_tests4/tdf108124.py
@@ -28,11 +28,22 @@ class tdf108124(UITestCase):
     xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+a"})) # select all
     xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+c"})) # copy
     xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+v"})) # paste
-    self.assertEqual(document.GraphicObjects.getCount(), 4)
+    self.assertEqual(document.GraphicObjects.getCount(), 2)
+    xObj1Old = document.GraphicObjects[0]
+    xObj2Old = document.GraphicObjects[1]
     self.xUITest.executeCommand(".uno:Undo")  #Undo
     self.assertEqual(document.GraphicObjects.getCount(), 2)
+    xObj1New = document.GraphicObjects[0]
+    xObj2New = document.GraphicObjects[1]
+    # there should be 2 different objects now but they have the same names,
+    # so rely on the object identity for testing...
+    self.assertNotEqual(xObj1Old, xObj1New)
+    self.assertNotEqual(xObj1Old, xObj2New)
+    self.assertNotEqual(xObj2Old, xObj1New)
+    self.assertNotEqual(xObj2Old, xObj2New)
     self.xUITest.executeCommand(".uno:Redo")  #Redo
-    self.assertEqual(document.GraphicObjects.getCount(), 4)
+    self.assertEqual(document.GraphicObjects.getCount(), 2)
 
     self.ui_test.close_doc()
+
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

sw/source/core/access/accframebase.cxx
@@ -319,8 +319,7 @@ bool SwAccessibleFrameBase::GetSelectedState( )
                         }
                         else if( rAnchor.GetAnchorId() == RndStdIds::FLY_AT_PARA )
                         {
-                            if( ((nHere > nStartIndex) || pStart->nContent.GetIndex() ==0 )
-                                && (nHere < nEndIndex ) )
+                            if (IsSelectFrameAnchoredAtPara(*pPos, *pStart, *pEnd))
                                 return true;
                         }
                         else if (rAnchor.GetAnchorId() == RndStdIds::FLY_AT_CHAR)

commit 9a9570f6ec3517064c66f2571c0841e9a74c916e

sw: crashtesting: fix assert on ooo95711-1.odt
Document is special and contains deletion that contains only a table, no
paragraphs before or after it.  The assertion is wrong in this case.
It doesn't matter if the atParaEndNode is incremented because it's going
to be on an end node either way.

(regression from 51c97e5e3ae88c43e611a4e6a61e375c66d5d007)

Change-Id: I3fe82f0ed0b7b213df036bb8026dc209c00123de
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91873
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/doc/docedt.cxx
@@ -144,7 +144,8 @@ void SaveFlyInRange( const SwPaM& rPam, const SwPosition& rInsPos,
     SwPosition atParaEnd(*rPam.End());
     if (bMoveAllFlys)
     {
-        assert(rPam.End()->nContent.GetIndex() == rPam.End()->nNode.GetNode().GetTextNode()->Len());
+        assert(!rPam.End()->nNode.GetNode().IsTextNode() // can be table end-node
+            || rPam.End()->nContent.GetIndex() == rPam.End()->nNode.GetNode().GetTextNode()->Len());
         ++atParaEnd.nNode;
         atParaEnd.nContent.Assign(atParaEnd.nNode.GetNode().GetContentNode(), 0);
     }