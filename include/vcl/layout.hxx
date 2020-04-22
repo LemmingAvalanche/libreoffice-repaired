LemmingAvalanche/libreoffice-repaired

commit b27e9a137de9774af3cfb1ed1241e2aae92c4269

make more classes private in mergedlibs mode
Change-Id: I486922d0652f26fa7ee56f5fe308e19fe5ff137e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90856
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/layout.hxx
@@ -10,6 +10,7 @@
 #ifndef INCLUDED_VCL_LAYOUT_HXX
 #define INCLUDED_VCL_LAYOUT_HXX
 
+#include <config_options.h>
 #include <vcl/dllapi.h>
 #include <vcl/ctrl.hxx>
 #include <vcl/help.hxx>
@@ -306,7 +307,7 @@ private:
     }
 };
 
-class VCL_DLLPUBLIC VclGrid final : public VclContainer
+class UNLESS_MERGELIBS(VCL_DLLPUBLIC) VclGrid final : public VclContainer
 {
 private:
     bool m_bRowHomogeneous;
@@ -361,7 +362,7 @@ public:
     virtual bool set_property(const OString &rKey, const OUString &rValue) override;
 };
 
-class VCL_DLLPUBLIC VclBin : public VclContainer
+class UNLESS_MERGELIBS(VCL_DLLPUBLIC) VclBin : public VclContainer
 {
 public:
     VclBin(vcl::Window *pParent, WinBits nStyle = WB_HIDE | WB_CLIPCHILDREN)
@@ -443,7 +444,7 @@ private:
     virtual OUString getDefaultAccessibleName() const override;
 };
 
-class VCL_DLLPUBLIC VclAlignment final : public VclBin
+class UNLESS_MERGELIBS(VCL_DLLPUBLIC) VclAlignment final : public VclBin
 {
 public:
     VclAlignment(vcl::Window *pParent)

commit 8b9d2386dd7286a4a17ec3d3cddb909c89fe4b04

typeinfo for VclGrid still needed with --enable-mergelibs
...for the dynamic_casts in SAL_WARN_IF and assertin VclBuilder::get,

> LNK] Library/libbiblo.so
> include/vcl/builder.hxx:430: error: undefined reference to 'typeinfo for VclGrid'
> include/vcl/builder.hxx:430: error: undefined reference to 'typeinfo for VclGrid'
> include/vcl/builder.hxx:433: error: undefined reference to 'typeinfo for VclGrid'
> collect2: error: ld returned 1 exit status
> make[1]: *** [extensions/Library_bib.mk:12: instdir/program/libbiblo.so] Error 1

(see e.g., <https://ci.libreoffice.org/job/lo_tb_random_config_linux/2358/>).
Broken with b27e9a137de9774af3cfb1ed1241e2aae92c4269 "make more classes private
in mergedlibs mode".

Change-Id: Ic20abe22b52026907a5624993008e497b173d14c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92613
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/layout.hxx
@@ -307,7 +307,7 @@ private:
     }
 };
 
-class UNLESS_MERGELIBS(VCL_DLLPUBLIC) VclGrid final : public VclContainer
+class VCL_DLLPUBLIC VclGrid final : public VclContainer
 {
 private:
     bool m_bRowHomogeneous;