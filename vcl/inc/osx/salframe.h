LemmingAvalanche/libreoffice-repaired

commit cc30e7230b5d5b6808883582e413877544449561

simplify ORefVector code
by making it extend std::vector - it wants to be a ref-counted vector,
so let it be, and we can simplify the usage sites

Change-Id: I93ff6ee1522da965e16223dca171401d36fd67b7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90664
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/osx/salframe.h
@@ -161,6 +161,8 @@ public:
 
     void UpdateFrameGeometry();
 
+    virtual void BeginSheet(SalFrame* pSheetFrame) const override;
+
     // trigger painting of the window
     void SendPaintEvent( const tools::Rectangle* pRect = nullptr );
 

vcl/inc/salframe.hxx
@@ -259,6 +259,10 @@ public:
         return false;
     }
 
+    /** only does anything on MACOSX where it indicates to the OS that this window is a modal-dialog
+     * and  should be displayed using the special sheet mode */
+    virtual void BeginSheet(SalFrame* /*pSheetWindow*/) const {}
+
     // return true to indicate tooltips are shown natively, false otherwise
     virtual bool            ShowTooltip(const OUString& /*rHelpText*/, const tools::Rectangle& /*rHelpArea*/)
     {

vcl/osx/salframe.cxx
@@ -1808,4 +1808,13 @@ void AquaSalFrame::EndSetClipRegion()
     // shadow is invalidated when view gets drawn again
 }
 
+void AquaSalFrame::BeginSheet(SalFrame* pSheetFrame) const
+{
+    NSWindow* pSheetNSWindow = static_cast<AquaSalFrame*>(pSheetFrame)->mpNSWindow;
+//    [mpNSWindow beginSheet:pSheetNSWindow];
+//    beginSheet(mpNSWindow];
+
+    [mpNSWindow beginSheet:pSheetNSWindow completionHandler:nil ];
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/window/dialog.cxx
@@ -965,6 +965,9 @@ bool Dialog::ImplStartExecute()
     // FIXME: no layouting, workaround some clipping issues
     ImplAdjustNWFSizes();
 
+    if (mpDialogParent)
+        mpDialogParent->ImplGetFrame()->BeginSheet(ImplGetFrame()); //  only does anything on macOS
+
     css::uno::Reference< css::uno::XComponentContext > xContext(
         comphelper::getProcessComponentContext());
     bool bForceFocusAndToFront(officecfg::Office::Common::View::NewDocumentHandling::ForceFocusAndToFront::get(xContext));

connectivity/source/commontools/TSortIndex.cxx
@@ -85,10 +85,10 @@ struct TKeyValueFunc
     Freeze();
 
     ::rtl::Reference<OKeySet> pKeySet = new OKeySet();
-    pKeySet->get().reserve(m_aKeyValues.size());
+    pKeySet->reserve(m_aKeyValues.size());
     std::transform(m_aKeyValues.begin()
                     ,m_aKeyValues.end()
-                    ,std::back_inserter(pKeySet->get())
+                    ,std::back_inserter(*pKeySet)
                     ,::o3tl::select1st<TIntValuePairVector::value_type>());
     pKeySet->setFrozen();
     return pKeySet;

connectivity/source/commontools/dbtools.cxx
@@ -1965,8 +1965,8 @@ void checkDisposed(bool _bThrow)
 
 }
 
-OSQLColumns::Vector::const_iterator find(const OSQLColumns::Vector::const_iterator& first,
-                                        const OSQLColumns::Vector::const_iterator& last,
+OSQLColumns::const_iterator find(const OSQLColumns::const_iterator& first,
+                                        const OSQLColumns::const_iterator& last,
                                         const OUString& _rVal,
                                         const ::comphelper::UStringMixEqual& _rCase)
 {
@@ -1974,8 +1974,8 @@ OSQLColumns::Vector::const_iterator find(const OSQLColumns::Vector::const_iterat
     return find(first,last,sName,_rVal,_rCase);
 }
 
-OSQLColumns::Vector::const_iterator findRealName(const OSQLColumns::Vector::const_iterator& first,
-                                        const OSQLColumns::Vector::const_iterator& last,
+OSQLColumns::const_iterator findRealName(const OSQLColumns::const_iterator& first,
+                                        const OSQLColumns::const_iterator& last,
                                         const OUString& _rVal,
                                         const ::comphelper::UStringMixEqual& _rCase)
 {
@@ -1983,8 +1983,8 @@ OSQLColumns::Vector::const_iterator findRealName(const OSQLColumns::Vector::cons
     return find(first,last,sRealName,_rVal,_rCase);
 }
 
-OSQLColumns::Vector::const_iterator find(OSQLColumns::Vector::const_iterator first,
-                                        const OSQLColumns::Vector::const_iterator& last,
+OSQLColumns::const_iterator find(OSQLColumns::const_iterator first,
+                                        const OSQLColumns::const_iterator& last,
                                         const OUString& _rProp,
                                         const OUString& _rVal,
                                         const ::comphelper::UStringMixEqual& _rCase)

connectivity/source/drivers/calc/CTable.cxx
@@ -478,12 +478,12 @@ void OCalcTable::fillColumns()
 
         // check if the column name already exists
         OUString aAlias = aColumnName;
-        OSQLColumns::Vector::const_iterator aFind = connectivity::find(m_aColumns->get().begin(),m_aColumns->get().end(),aAlias,aCase);
+        OSQLColumns::const_iterator aFind = connectivity::find(m_aColumns->begin(),m_aColumns->end(),aAlias,aCase);
         sal_Int32 nExprCnt = 0;
-        while(aFind != m_aColumns->get().end())
+        while(aFind != m_aColumns->end())
         {
             aAlias = aColumnName + OUString::number(++nExprCnt);
-            aFind = connectivity::find(m_aColumns->get().begin(),m_aColumns->get().end(),aAlias,aCase);
+            aFind = connectivity::find(m_aColumns->begin(),m_aColumns->end(),aAlias,aCase);
         }
 
         sdbcx::OColumn* pColumn = new sdbcx::OColumn( aAlias, aTypeName, OUString(),OUString(),
@@ -492,7 +492,7 @@ void OCalcTable::fillColumns()
                                                 bStoresMixedCaseQuotedIdentifiers,
                                                 m_CatalogName, getSchema(), getName());
         Reference< XPropertySet> xCol = pColumn;
-        m_aColumns->get().push_back(xCol);
+        m_aColumns->push_back(xCol);
         m_aTypes.push_back(eType);
     }
 }
@@ -633,21 +633,21 @@ bool OCalcTable::fetchRow( OValueRefRow& _rRow, const OSQLColumns & _rCols,
     // read the bookmark
 
     _rRow->setDeleted(false);
-    *(_rRow->get())[0] = m_nFilePos;
+    *(*_rRow)[0] = m_nFilePos;
 
     if (!bRetrieveData)
         return true;
 
     // fields
 
-    const OValueRefVector::Vector::size_type nCount = std::min(_rRow->get().size(), _rCols.get().size() + 1);
-    for (OValueRefVector::Vector::size_type i = 1; i < nCount; i++)
+    const OValueRefVector::size_type nCount = std::min(_rRow->size(), _rCols.size() + 1);
+    for (OValueRefVector::size_type i = 1; i < nCount; i++)
     {
-        if ( (_rRow->get())[i]->isBound() )
+        if ( (*_rRow)[i]->isBound() )
         {
             sal_Int32 nType = m_aTypes[i-1];
 
-            lcl_SetValue( (_rRow->get())[i]->get(), m_xSheet, m_nStartCol, m_nStartRow, m_bHasHeaders,
+            lcl_SetValue( (*_rRow)[i]->get(), m_xSheet, m_nStartCol, m_nStartRow, m_bHasHeaders,
                                 m_aNullDate, m_nFilePos, i, nType );
         }
     }

connectivity/source/drivers/component/CColumns.cxx
@@ -33,9 +33,9 @@ sdbcx::ObjectType OComponentColumns::createObject(const OUString& _rName)
 {
     ::rtl::Reference<OSQLColumns> aCols = m_pTable->getTableColumns();
 
-    OSQLColumns::Vector::const_iterator aIter = find(aCols->get().begin(),aCols->get().end(),_rName,::comphelper::UStringMixEqual(isCaseSensitive()));
+    OSQLColumns::const_iterator aIter = find(aCols->begin(),aCols->end(),_rName,::comphelper::UStringMixEqual(isCaseSensitive()));
     sdbcx::ObjectType xRet;
-    if(aIter != aCols->get().end())
+    if(aIter != aCols->end())
         xRet = sdbcx::ObjectType(*aIter,UNO_QUERY);
     return xRet;
 }

commit f2dc705e9751327024d0645a001de805f35e2243

tf#131549 No titlebar on modal dialogs on macOS
regression from
    commit cc30e7230b5d5b6808883582e413877544449561
    Date:   Tue Mar 17 21:34:21 2020 +0200
    simplify ORefVector code
where a half-finished patch of mine snuck into another patch.

Change-Id: I01f59a9d451f4535197d3abd7b37bfdbc8461c15
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92901
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/osx/salframe.h
@@ -163,8 +163,6 @@ public:
 
     void UpdateFrameGeometry();
 
-    virtual void BeginSheet(SalFrame* pSheetFrame) const override;
-
     // trigger painting of the window
     void SendPaintEvent( const tools::Rectangle* pRect = nullptr );
 

vcl/inc/salframe.hxx
@@ -259,10 +259,6 @@ public:
         return false;
     }
 
-    /** only does anything on MACOSX where it indicates to the OS that this window is a modal-dialog
-     * and  should be displayed using the special sheet mode */
-    virtual void BeginSheet(SalFrame* /*pSheetWindow*/) const {}
-
     // return true to indicate tooltips are shown natively, false otherwise
     virtual bool            ShowTooltip(const OUString& /*rHelpText*/, const tools::Rectangle& /*rHelpArea*/)
     {

vcl/osx/salframe.cxx
@@ -1822,13 +1822,4 @@ void AquaSalFrame::EndSetClipRegion()
     // shadow is invalidated when view gets drawn again
 }
 
-void AquaSalFrame::BeginSheet(SalFrame* pSheetFrame) const
-{
-    NSWindow* pSheetNSWindow = static_cast<AquaSalFrame*>(pSheetFrame)->mpNSWindow;
-//    [mpNSWindow beginSheet:pSheetNSWindow];
-//    beginSheet(mpNSWindow];
-
-    [mpNSWindow beginSheet:pSheetNSWindow completionHandler:nil ];
-}
-
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/window/dialog.cxx
@@ -966,9 +966,6 @@ bool Dialog::ImplStartExecute()
     // FIXME: no layouting, workaround some clipping issues
     ImplAdjustNWFSizes();
 
-    if (mpDialogParent)
-        mpDialogParent->ImplGetFrame()->BeginSheet(ImplGetFrame()); //  only does anything on macOS
-
     css::uno::Reference< css::uno::XComponentContext > xContext(
         comphelper::getProcessComponentContext());
     bool bForceFocusAndToFront(officecfg::Office::Common::View::NewDocumentHandling::ForceFocusAndToFront::get(xContext));