LemmingAvalanche/libreoffice-repaired

commit eabcd10e4467d52ec03d0369e3f88334e51a698d

remove unnecessary use of OString::getStr
Change-Id: I0490efedf459190521f4339854b3394d57765fdb
Reviewed-on: https://gerrit.libreoffice.org/38058
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

cppu/source/typelib/typelib.cxx
@@ -530,9 +530,7 @@ bool complete(typelib_TypeDescription ** ppTypeDescr, bool initTables) {
         }
         else
         {
-            OString aStr(
-                rtl::OUStringToOString( (*ppTypeDescr)->pTypeName, RTL_TEXTENCODING_ASCII_US ) );
-            SAL_INFO( "cppu.typelib", "type cannot be completed: " <<  aStr.getStr() );
+            SAL_INFO( "cppu.typelib", "type cannot be completed: " <<  (*ppTypeDescr)->pTypeName );
             return false;
         }
     }

commit 87b96cb6fed8471952b592f0fbdbf83f6549a0b0

Fix printing rtl_uString*
...broken with eabcd10e4467d52ec03d0369e3f88334e51a698d "remove unnecessary use
of OString::getStr"

Change-Id: I85f5ccb6c5114ea5e3eab43a3c1821292cf4e994
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92993
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

cppu/source/typelib/typelib.cxx
@@ -497,7 +497,9 @@ bool complete(typelib_TypeDescription ** ppTypeDescr, bool initTables) {
         }
         else
         {
-            SAL_INFO( "cppu.typelib", "type cannot be completed: " <<  (*ppTypeDescr)->pTypeName );
+            SAL_INFO(
+                "cppu.typelib",
+                "type cannot be completed: " << OUString::unacquired(&(*ppTypeDescr)->pTypeName));
             return false;
         }
     }