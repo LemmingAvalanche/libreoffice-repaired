LemmingAvalanche/libreoffice-repaired

commit 1b8b65ba68957341a97432e339cb9bd408810d91

tdf#132076 Chart OOXML: fix lost date format of X axis
labels. Set the number format of category or date axes
too and use date axis type for date axes at import.
Export the real axis type of the X axis.

Change-Id: I45f5c69f0aadc4ec1db0b8873b5e374e2728d39c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92396
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/chartexport.cxx
@@ -67,6 +67,7 @@
 #include <com/sun/star/chart2/data/XNumericalDataSequence.hpp>
 #include <com/sun/star/chart2/data/XLabeledDataSequence.hpp>
 #include <com/sun/star/chart2/XAnyDescriptionAccess.hpp>
+#include <com/sun/star/chart2/AxisType.hpp>
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/container/XNameAccess.hpp>
@@ -258,6 +259,39 @@ static bool lcl_isCategoryAxisShifted(const Reference< chart2::XChartDocument >&
     }
 
     return isCategoryPositionShifted;
+}
+
+static sal_Int32 lcl_getCategoryAxisType( const Reference< chart2::XDiagram >& xDiagram, sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex )
+{
+    sal_Int32 nAxisType = -1;
+    try
+    {
+        Reference< chart2::XCoordinateSystemContainer > xCooSysCnt(
+            xDiagram, uno::UNO_QUERY_THROW);
+        const Sequence< Reference< chart2::XCoordinateSystem > > aCooSysSeq(
+            xCooSysCnt->getCoordinateSystems());
+        for( const auto& xCooSys : aCooSysSeq )
+        {
+            OSL_ASSERT(xCooSys.is());
+            if( nDimensionIndex < xCooSys->getDimension() && nAxisIndex <= xCooSys->getMaximumAxisIndexByDimension(nDimensionIndex) )
+            {
+                Reference< chart2::XAxis > xAxis = xCooSys->getAxisByDimension(nDimensionIndex, nAxisIndex);
+                OSL_ASSERT(xAxis.is());
+                if( xAxis.is() )
+                {
+                    chart2::ScaleData aScaleData = xAxis->getScaleData();
+                    nAxisType = aScaleData.AxisType;
+                    break;
+                }
+            }
+        }
+    }
+    catch (const uno::Exception&)
+    {
+        DBG_UNHANDLED_EXCEPTION("oox");
+    }
+
+    return nAxisType;
 }
 
 static bool lcl_isSeriesAttachedToFirstAxis(
@@ -2644,7 +2678,7 @@ void ChartExport::exportAxes( )
 
 namespace {
 
-sal_Int32 getXAxisType(sal_Int32 eChartType)
+sal_Int32 getXAxisTypeByChartType(sal_Int32 eChartType)
 {
     if( (eChartType == chart::TYPEID_SCATTER)
             || (eChartType == chart::TYPEID_BUBBLE) )
@@ -2653,6 +2687,18 @@ sal_Int32 getXAxisType(sal_Int32 eChartType)
         return  XML_dateAx;
 
     return XML_catAx;
+}
+
+sal_Int32 getRealXAxisType(sal_Int32 nAxisType)
+{
+    if( nAxisType == chart2::AxisType::CATEGORY )
+        return XML_catAx;
+    else if( nAxisType == chart2::AxisType::DATE )
+        return XML_dateAx;
+    else if( nAxisType == chart2::AxisType::SERIES )
+        return XML_serAx;
+
+    return XML_valAx;
 }
 
 }
@@ -2709,8 +2755,11 @@ void ChartExport::exportAxis(const AxisIdPair& rAxisIdPair)
             if( bHasXAxisMinorGrid )
                 xMinorGrid = xAxisXSupp->getXHelpGrid();
 
-            sal_Int32 eChartType = getChartType();
-            nAxisType = getXAxisType(eChartType);
+            nAxisType = lcl_getCategoryAxisType(mxNewDiagram, 0, 0);
+            if( nAxisType != -1 )
+                nAxisType = getRealXAxisType(nAxisType);
+            else
+                nAxisType = getXAxisTypeByChartType( getChartType() );
             // FIXME: axPos, need to check axis direction
             sAxPos = "b";
             break;
@@ -2767,8 +2816,11 @@ void ChartExport::exportAxis(const AxisIdPair& rAxisIdPair)
                 xAxisTitle = xAxisSupp->getSecondXAxisTitle();
             }
 
-            sal_Int32 eChartType = getChartType();
-            nAxisType = getXAxisType(eChartType);
+            nAxisType = lcl_getCategoryAxisType(mxNewDiagram, 0, 1);
+            if( nAxisType != -1 )
+                nAxisType = getRealXAxisType(nAxisType);
+            else
+                nAxisType = getXAxisTypeByChartType( getChartType() );
             // FIXME: axPos, need to check axis direction
             sAxPos = "t";
             break;

chart2/qa/extras/chart2export.cxx
@@ -165,6 +165,7 @@ public:
     void testTdf75330();
     void testTdf127792();
     void testTdf131979();
+    void testTdf132076();
 
     CPPUNIT_TEST_SUITE(Chart2ExportTest);
     CPPUNIT_TEST(testErrorBarXLSX);
@@ -293,6 +294,7 @@ public:
     CPPUNIT_TEST(testTdf75330);
     CPPUNIT_TEST(testTdf127792);
     CPPUNIT_TEST(testTdf131979);
+    CPPUNIT_TEST(testTdf132076);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2708,6 +2710,24 @@ void Chart2ExportTest::testTdf131979()
         bool blinknumberformattosource = true;
         CPPUNIT_ASSERT(xPropertySet->getPropertyValue(CHART_UNONAME_LINK_TO_SRC_NUMFMT) >>= blinknumberformattosource);
         CPPUNIT_ASSERT_MESSAGE("\"LinkNumberFormatToSource\" should be set to true.", blinknumberformattosource);
+    }
+}
+
+void Chart2ExportTest::testTdf132076()
+{
+    {
+        load("/chart2/qa/extras/data/ods/", "tdf132076.ods");
+        xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+        CPPUNIT_ASSERT(pXmlDoc);
+        assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:catAx/c:numFmt", "formatCode", "dd");
+        assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:catAx/c:numFmt", "sourceLinked", "0");
+    }
+    {
+        load("/chart2/qa/extras/data/xlsx/", "tdf132076.xlsx");
+        xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+        CPPUNIT_ASSERT(pXmlDoc);
+        assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:dateAx/c:numFmt", "formatCode", "dd");
+        assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:dateAx/c:numFmt", "sourceLinked", "0");
     }
 }
 

oox/source/drawingml/chart/axisconverter.cxx
@@ -205,10 +205,8 @@ void AxisConverter::convertFromModel(
                     OSL_ENSURE( (mrModel.mnTypeId == C_TOKEN( catAx )) || (mrModel.mnTypeId == C_TOKEN( dateAx )),
                         "AxisConverter::convertFromModel - unexpected axis model type (must: c:catAx or c:dateAx)" );
                     bool bDateAxis = mrModel.mnTypeId == C_TOKEN( dateAx );
-                    /*  Chart2 requires axis type CATEGORY for automatic
-                        category/date axis (even if it is a date axis
-                        currently). */
-                    aScaleData.AxisType = (bDateAxis && !mrModel.mbAuto) ? cssc2::AxisType::DATE : cssc2::AxisType::CATEGORY;
+                    // tdf#132076: set axis type to date, if it is a date axis!
+                    aScaleData.AxisType = bDateAxis ? cssc2::AxisType::DATE : cssc2::AxisType::CATEGORY;
                     aScaleData.AutoDateAxis = mrModel.mbAuto;
                     aScaleData.Categories = rTypeGroups.front()->createCategorySequence();
                     /* set default ShiftedCategoryPosition values for some charttype,
@@ -341,8 +339,10 @@ void AxisConverter::convertFromModel(
         xAxis->setScaleData( aScaleData );
 
         // number format ------------------------------------------------------
-        if( !mrModel.mbDeleted && ((aScaleData.AxisType == cssc2::AxisType::REALNUMBER) || (aScaleData.AxisType == cssc2::AxisType::PERCENT)) )
+        if( !mrModel.mbDeleted && aScaleData.AxisType != cssc2::AxisType::SERIES )
+        {
             getFormatter().convertNumberFormat(aAxisProp, mrModel.maNumberFormat, true);
+        }
 
         // position of crossing axis ------------------------------------------
 

commit 4f193a3699a1cdbd1339e9d28a98290fdddaab4a

tdf#132486 Chart: fix OOXML export of ShiftedCategoryPosition
Regression from commit 1b8b65ba68957341a97432e339cb9bd408810d91
(tdf#132076 Chart OOXML: fix lost date format of X axis)

Change-Id: I4bb62959775b0b6ed11e1f7e5473c3b9805f4e29
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92420
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

include/oox/export/chartexport.hxx
@@ -106,7 +106,6 @@ private:
 
     // members filled by InitRangeSegmentationProperties (retrieved from DataProvider)
     bool mbHasCategoryLabels; //if the categories are only automatically generated this will be false
-    bool mbIsCategoryPositionShifted; //if the value axis crosses the category axis between tickmarks this will be true
 
     //css::uno::Reference< css::drawing::XShapes > mxAdditionalShapes;
     css::uno::Reference< css::chart2::data::XDataSequence > mxCategoriesValues;

oox/source/export/chartexport.cxx
@@ -219,46 +219,37 @@ static bool lcl_hasCategoryLabels( const Reference< chart2::XChartDocument >& xC
     return xCategories.is();
 }
 
-static bool lcl_isCategoryAxisShifted(const Reference< chart2::XChartDocument >& xChartDoc)
+static bool lcl_isCategoryAxisShifted( const Reference< chart2::XDiagram >& xDiagram )
 {
-    Reference< chart2::XDiagram > xDiagram(xChartDoc->getFirstDiagram());
-    bool isCategoryPositionShifted = false;
-
+    bool bCategoryPositionShifted = false;
     try
     {
         Reference< chart2::XCoordinateSystemContainer > xCooSysCnt(
             xDiagram, uno::UNO_QUERY_THROW);
         const Sequence< Reference< chart2::XCoordinateSystem > > aCooSysSeq(
             xCooSysCnt->getCoordinateSystems());
-        for( const auto& xCooSys : aCooSysSeq )
+        for (const auto& xCooSys : aCooSysSeq)
         {
             OSL_ASSERT(xCooSys.is());
-            for( sal_Int32 nN = xCooSys->getDimension(); nN--; )
+            if( 0 < xCooSys->getDimension() && 0 <= xCooSys->getMaximumAxisIndexByDimension(0) )
             {
-                const sal_Int32 nMaxAxisIndex = xCooSys->getMaximumAxisIndexByDimension(nN);
-                for( sal_Int32 nI = 0; nI <= nMaxAxisIndex; ++nI )
+                Reference< chart2::XAxis > xAxis = xCooSys->getAxisByDimension(0, 0);
+                OSL_ASSERT(xAxis.is());
+                if (xAxis.is())
                 {
-                    Reference< chart2::XAxis > xAxis = xCooSys->getAxisByDimension(nN, nI);
-                    OSL_ASSERT(xAxis.is());
-                    if( xAxis.is())
-                    {
-                        chart2::ScaleData aScaleData = xAxis->getScaleData();
-                        if( aScaleData.AxisType == AXIS_PRIMARY_Y )
-                        {
-                            isCategoryPositionShifted = aScaleData.ShiftedCategoryPosition;
-                            break;
-                        }
-                    }
+                    chart2::ScaleData aScaleData = xAxis->getScaleData();
+                    bCategoryPositionShifted = aScaleData.ShiftedCategoryPosition;
+                    break;
                 }
             }
         }
     }
-    catch (const uno::Exception &)
+    catch (const uno::Exception&)
     {
         DBG_UNHANDLED_EXCEPTION("oox");
     }
 
-    return isCategoryPositionShifted;
+    return bCategoryPositionShifted;
 }
 
 static sal_Int32 lcl_getCategoryAxisType( const Reference< chart2::XDiagram >& xDiagram, sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex )
@@ -447,7 +438,6 @@ ChartExport::ChartExport( sal_Int32 nXmlNamespace, FSHelperPtr pFS, Reference< f
     , mxChartModel( xModel )
     , mpURLTransformer(std::make_shared<URLTransformer>())
     , mbHasCategoryLabels( false )
-    , mbIsCategoryPositionShifted( false )
     , mbHasZAxis( false )
     , mbIs3DChart( false )
     , mbStacked(false)
@@ -805,7 +795,6 @@ void ChartExport::InitRangeSegmentationProperties( const Reference< chart2::XCha
         if( xDataProvider.is())
         {
             mbHasCategoryLabels = lcl_hasCategoryLabels( xChartDoc );
-            mbIsCategoryPositionShifted = lcl_isCategoryAxisShifted( xChartDoc );
         }
     }
     catch( const uno::Exception & )
@@ -3095,7 +3084,7 @@ void ChartExport::_exportAxis(
     // crossBetween
     if( nAxisType == XML_valAx )
     {
-        if( mbIsCategoryPositionShifted )
+        if( lcl_isCategoryAxisShifted( mxNewDiagram ))
             pFS->singleElement(FSNS(XML_c, XML_crossBetween), XML_val, "between");
         else
             pFS->singleElement(FSNS(XML_c, XML_crossBetween), XML_val, "midCat");

chart2/qa/extras/chart2export.cxx
@@ -2068,11 +2068,19 @@ void Chart2ExportTest::testCombinedChartSecondaryAxisODS()
 
 void Chart2ExportTest::testCrossBetweenXLSX()
 {
-    // Original file was created with MS Office
-    load("/chart2/qa/extras/data/xlsx/", "tdf127777.xlsx");
-    xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
-    CPPUNIT_ASSERT(pXmlDoc);
-    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:valAx/c:crossBetween", "val", "between");
+    // Original files were created with MS Office
+    {
+        load("/chart2/qa/extras/data/xlsx/", "tdf127777.xlsx");
+        xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+        CPPUNIT_ASSERT(pXmlDoc);
+        assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:valAx/c:crossBetween", "val", "between");
+    }
+    {
+        load("/chart2/qa/extras/data/xlsx/", "tdf132076.xlsx");
+        xmlDocPtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+        CPPUNIT_ASSERT(pXmlDoc);
+        assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:valAx/c:crossBetween", "val", "between");
+    }
 }
 
 void Chart2ExportTest::testCrossBetweenWithDeletedAxis()