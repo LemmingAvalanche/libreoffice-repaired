LemmingAvalanche/libreoffice-repaired

commit 409c4318a97edddaa8d5c7119a959e8f1cca8307

compact namespace in ucb..uui
Change-Id: I644d5e418028b4b4e66cf67b20a1155a689acab0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93906
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/webdav/ContentProperties.hxx
@@ -29,7 +29,7 @@
 #include <com/sun/star/uno/Sequence.hxx>
 #include "DAVResource.hxx"
 
-namespace com { namespace sun { namespace star { namespace beans {
+namespace com::sun::star::beans {
     struct Property;
 } } } }
 

ucb/source/ucp/webdav/DAVSession.hxx
@@ -30,7 +30,7 @@
 #include "DAVTypes.hxx"
 #include "DAVRequestEnvironment.hxx"
 
-namespace com { namespace sun { namespace star { namespace ucb {
+namespace com::sun::star::ucb {
     struct Lock;
 } } } }
 

ucb/source/ucp/webdav/DAVSessionFactory.hxx
@@ -34,7 +34,7 @@
 
 using namespace com::sun::star;
 
-namespace com { namespace sun { namespace star { namespace lang {
+namespace com::sun::star::lang {
     class XMultiServiceFactory;
 } } } }
 

ucb/source/ucp/webdav/DateTimeHelper.hxx
@@ -24,7 +24,7 @@
 #include <sal/types.h>
 #include <rtl/ustring.hxx>
 
-namespace com { namespace sun { namespace star { namespace util {
+namespace com::sun::star::util {
     struct DateTime;
 } } } }
 

ucb/source/ucp/webdav/webdavcontent.hxx
@@ -30,20 +30,20 @@
 #include "DAVResourceAccess.hxx"
 #include "PropertyMap.hxx"
 
-namespace com { namespace sun { namespace star { namespace beans {
+namespace com::sun::star::beans {
     struct Property;
     struct PropertyValue;
 } } } }
 
-namespace com { namespace sun { namespace star { namespace io {
+namespace com::sun::star::io {
     class XInputStream;
 } } } }
 
-namespace com { namespace sun { namespace star { namespace sdbc {
+namespace com::sun::star::sdbc {
     class XRow;
 } } } }
 
-namespace com { namespace sun { namespace star { namespace ucb {
+namespace com::sun::star::ucb {
     struct OpenCommandArgument2;
     struct PropertyCommandArgument;
     struct PostCommandArgument2;

ucb/source/ucp/webdav/webdavprovider.hxx
@@ -31,7 +31,7 @@
 #include <ucbhelper/providerhelper.hxx>
 #include "PropertyMap.hxx"
 
-namespace com { namespace sun { namespace star { namespace lang {
+namespace com::sun::star::lang {
 class XSingleServiceFactory;
 } } } }
 

ucb/source/ucp/webdav-neon/ContentProperties.hxx
@@ -37,9 +37,9 @@
 #include <com/sun/star/uno/Sequence.hxx>
 #include "DAVResource.hxx"
 
-namespace com { namespace sun { namespace star { namespace beans {
+namespace com::sun::star::beans {
     struct Property;
-} } } }
+}
 
 namespace webdav_ucp
 {

ucb/source/ucp/webdav-neon/DAVSession.hxx
@@ -40,13 +40,13 @@
 #include "DAVTypes.hxx"
 #include "DAVRequestEnvironment.hxx"
 
-namespace com { namespace sun { namespace star { namespace beans {
+namespace com::sun::star::beans {
     struct NamedValue;
-} } } }
+}
 
-namespace com { namespace sun { namespace star { namespace ucb {
+namespace com::sun::star::ucb {
     struct Lock;
-} } } }
+}
 
 namespace webdav_ucp
 {

commit 1d727dd357d562acd3a97ef405195300d98a4f34

Fix syntax errors in --with-webdav=serf code
...introduced with 409c4318a97edddaa8d5c7119a959e8f1cca8307 "compact namespace
in ucb..uui"

Change-Id: I58e24303e1e12db0a53d46acc2daef1faa2e4cf6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94128
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

ucb/source/ucp/webdav/ContentProperties.hxx
@@ -31,7 +31,7 @@
 
 namespace com::sun::star::beans {
     struct Property;
-} } } }
+}
 
 namespace http_dav_ucp
 {

ucb/source/ucp/webdav/DAVSession.hxx
@@ -32,7 +32,7 @@
 
 namespace com::sun::star::ucb {
     struct Lock;
-} } } }
+}
 
 namespace http_dav_ucp
 {

ucb/source/ucp/webdav/DAVSessionFactory.hxx
@@ -36,7 +36,7 @@ using namespace com::sun::star;
 
 namespace com::sun::star::lang {
     class XMultiServiceFactory;
-} } } }
+}
 
 namespace http_dav_ucp
 {

ucb/source/ucp/webdav/DateTimeHelper.hxx
@@ -26,7 +26,7 @@
 
 namespace com::sun::star::util {
     struct DateTime;
-} } } }
+}
 
 namespace rtl {
     class OUString;

ucb/source/ucp/webdav/webdavcontent.hxx
@@ -33,22 +33,22 @@
 namespace com::sun::star::beans {
     struct Property;
     struct PropertyValue;
-} } } }
+}
 
 namespace com::sun::star::io {
     class XInputStream;
-} } } }
+}
 
 namespace com::sun::star::sdbc {
     class XRow;
-} } } }
+}
 
 namespace com::sun::star::ucb {
     struct OpenCommandArgument2;
     struct PropertyCommandArgument;
     struct PostCommandArgument2;
     struct TransferInfo;
-} } } }
+}
 
 namespace http_dav_ucp
 {

ucb/source/ucp/webdav/webdavprovider.hxx
@@ -33,7 +33,7 @@
 
 namespace com::sun::star::lang {
 class XSingleServiceFactory;
-} } } }
+}
 
 namespace http_dav_ucp {
 