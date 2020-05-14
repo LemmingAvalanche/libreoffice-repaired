LemmingAvalanche/libreoffice-repaired

commit 33b47bfc78587a6590a382e89623365775daf73c

tdf#92079 vcl: fix missing image background on dialog from basic
Regression from commit 2c30a5aba4a192842263f17b4ebf3faffa27910d (add
RenderContext to Window::PushPaintHelper, use Erase correctly,
2015-05-18), the problem was the unconditional ApplySettings() call in
the direct painting case.

It's not clear what was the intent of that addition, since
vcl::Window::ApplySettings() is virtual, and the default implementation
is empty. But at least the Dialog subclass sets the background there,
leading to the loss of the original bitmap background.

Fix the problem by explicitly restoring the old background after
ApplySettings() at least in the bitmap case. (Wallpaper is
copy-on-write, so no problem to make a copy before ApplySettings()
unconditionally.)

Change-Id: I89768a1331f9dc1ea4508336723484d112156338
Reviewed-on: https://gerrit.libreoffice.org/73965
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/window/paint.cxx
@@ -296,7 +296,10 @@ void PaintHelper::DoPaint(const vcl::Region* pRegion)
         else
         {
             // direct painting
+            Wallpaper aBackground = m_pWindow->GetBackground();
             m_pWindow->ApplySettings(*m_pWindow);
+            if (aBackground.IsBitmap())
+                m_pWindow->SetBackground(aBackground);
             m_pWindow->PushPaintHelper(this, *m_pWindow);
             m_pWindow->Paint(*m_pWindow, m_aPaintRect);
         }

commit 41aeca6bc79795cdf1ebb32138298270d3aba4f1

tdf#132788 vcl: fix redraw on theme change
Regression from commit 33b47bfc78587a6590a382e89623365775daf73c
(tdf#92079 vcl: fix missing image background on dialog from basic,
2019-06-13), the original scenario was about an unexpected change from
bitmap wallpaper to a non-bitmap one.

That means the condition for the above change can be more strict: just
restore the old wallpaper if it's now a non-bitmap one, otherwise leave
it alone. This way the above scenario keeps working and changing themes
again doesn't require a restart of the process.

Change-Id: I256372ad30184cc150d6819dd61cdd38af7d83ba
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94189
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/window/paint.cxx
@@ -302,9 +302,11 @@ void PaintHelper::DoPaint(const vcl::Region* pRegion)
             // direct painting
             Wallpaper aBackground = m_pWindow->GetBackground();
             m_pWindow->ApplySettings(*m_pWindow);
-            // Restore lost bitmap background.
-            if (aBackground.IsBitmap())
+            // Restore bitmap background if it was lost.
+            if (aBackground.IsBitmap() && !m_pWindow->GetBackground().IsBitmap())
+            {
                 m_pWindow->SetBackground(aBackground);
+            }
             m_pWindow->PushPaintHelper(this, *m_pWindow);
             m_pWindow->Paint(*m_pWindow, m_aPaintRect);
         }