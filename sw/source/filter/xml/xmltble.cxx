LemmingAvalanche/libreoffice-repaired

commit 9062a46752efeae97b4320e59a4d83c7b18f5f68

tdf#102256 sw: ODF export: don't write table:table in shapes
... for strict ODF versions, only write loext:table in extended.

Since commit f9c25980ba41ec1c82ac1269b20e9538b5da3747 LO can read the
loext:table elements, LO 4.4 should be sufficiently long ago.

Actually with ODF 1.2, the export asserts because loext namespace was
exported but isn't declared.

Change-Id: I92318f78b3282989de1d9c2764c2ed7663cb50b7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94438
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/filter/xml/xmltble.cxx
@@ -1094,15 +1094,26 @@ void SwXMLExport::ExportTable( const SwTableNode& rTableNd )
         AddAttribute(XML_NAMESPACE_TABLE, XML_TEMPLATE_NAME, sStyleName);
     }
 
-    sal_uInt16 nPrefix = XML_NAMESPACE_TABLE;
+    ::std::optional<sal_uInt16> oPrefix = XML_NAMESPACE_TABLE;
     if (const SwFrameFormat* pFlyFormat = rTableNd.GetFlyFormat())
     {
         if (SwTextBoxHelper::isTextBox(pFlyFormat, RES_FLYFRMFMT))
-            nPrefix = XML_NAMESPACE_LO_EXT;
+        {
+            // TODO ODF 1.4 OFFICE-3761
+            if (getSaneDefaultVersion() & SvtSaveOptions::ODFSVER_EXTENDED)
+            {
+                oPrefix = XML_NAMESPACE_LO_EXT;
+            }
+            else
+            {
+                oPrefix.reset(); // no export to OASIS namespace yet
+            }
+        }
     }
 
+    if (oPrefix)
     {
-        SvXMLElementExport aElem( *this, nPrefix, XML_TABLE, true, true );
+        SvXMLElementExport aElem(*this, *oPrefix, XML_TABLE, true, true);
 
         // export DDE source (if this is a DDE table)
         if ( dynamic_cast<const SwDDETable*>( &rTable) !=  nullptr )
@@ -1137,7 +1148,7 @@ void SwXMLExport::ExportTable( const SwTableNode& rTableNd )
                                        XML_DDE_SOURCE, true, false);
         }
 
-        SwXMLTableInfo_Impl aTableInfo( &rTable, nPrefix );
+        SwXMLTableInfo_Impl aTableInfo(&rTable, *oPrefix);
         ExportTableLines( rTable.GetTabLines(), aTableInfo, rTable.GetRowsToRepeat() );
 
         for( SwTableLine *pLine : const_cast<SwTable &>(rTable).GetTabLines() )

commit b8934da4972194f90ada281b9587437a53ea1935

tdf#102256 sw: ODF export: oops, don't write table attributes ...
... when not writing table itself.

(regression from commit 9062a46752efeae97b4320e59a4d83c7b18f5f68)

Change-Id: I73cbb126a5c6d12399806a20cd7f4307111a6c8e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95057
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/filter/xml/xmltble.cxx
@@ -1077,23 +1077,6 @@ void lcl_xmltble_ClearName_Line( SwTableLine* pLine )
 
 void SwXMLExport::ExportTable( const SwTableNode& rTableNd )
 {
-    const SwTable& rTable = rTableNd.GetTable();
-    const SwFrameFormat *pTableFormat = rTable.GetFrameFormat();
-    if( pTableFormat && !pTableFormat->GetName().isEmpty() )
-    {
-        AddAttribute( XML_NAMESPACE_TABLE, XML_NAME, pTableFormat->GetName() );
-        AddAttribute( XML_NAMESPACE_TABLE, XML_STYLE_NAME,
-                      EncodeStyleName( pTableFormat->GetName() ) );
-    }
-
-    // table:template-name=
-    if (!rTable.GetTableStyleName().isEmpty())
-    {
-        OUString sStyleName;
-        SwStyleNameMapper::FillProgName(rTable.GetTableStyleName(), sStyleName, SwGetPoolIdFromName::TabStyle);
-        AddAttribute(XML_NAMESPACE_TABLE, XML_TEMPLATE_NAME, sStyleName);
-    }
-
     ::std::optional<sal_uInt16> oPrefix = XML_NAMESPACE_TABLE;
     if (const SwFrameFormat* pFlyFormat = rTableNd.GetFlyFormat())
     {
@@ -1113,6 +1096,23 @@ void SwXMLExport::ExportTable( const SwTableNode& rTableNd )
 
     if (oPrefix)
     {
+        const SwTable& rTable = rTableNd.GetTable();
+        const SwFrameFormat *pTableFormat = rTable.GetFrameFormat();
+        if (pTableFormat && !pTableFormat->GetName().isEmpty())
+        {
+            AddAttribute(XML_NAMESPACE_TABLE, XML_NAME, pTableFormat->GetName());
+            AddAttribute(XML_NAMESPACE_TABLE, XML_STYLE_NAME,
+                         EncodeStyleName(pTableFormat->GetName()));
+        }
+
+        // table:template-name=
+        if (!rTable.GetTableStyleName().isEmpty())
+        {
+            OUString sStyleName;
+            SwStyleNameMapper::FillProgName(rTable.GetTableStyleName(), sStyleName, SwGetPoolIdFromName::TabStyle);
+            AddAttribute(XML_NAMESPACE_TABLE, XML_TEMPLATE_NAME, sStyleName);
+        }
+
         SvXMLElementExport aElem(*this, *oPrefix, XML_TABLE, true, true);
 
         // export DDE source (if this is a DDE table)