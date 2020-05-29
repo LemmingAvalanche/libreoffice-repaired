LemmingAvalanche/libreoffice-repaired

commit b8a0abc81503ff6e9850b8d681ddd7562a8acc80

tdf#76481 speed up searching in autofilter pulldown
turning setUpdateMode on/off fixes the common case, but we need to
special case the "return to show all items" situation

On my machine this takes the searching time from "more than 30s" to
"just under 1s"

Change-Id: I02d11c428e82dba1e840e981507337a1012dd09f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94633
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/cctrl/checklistmenu.cxx
@@ -1223,20 +1223,27 @@ IMPL_LINK_NOARG(ScCheckListMenuWindow, EdModifyHdl, Edit&, void)
     bool bSearchTextEmpty = aSearchText.isEmpty();
     size_t n = maMembers.size();
     size_t nSelCount = 0;
-    OUString aLabelDisp;
     bool bSomeDateDeletes = false;
 
-    for (size_t i = 0; i < n; ++i)
+    maChecks->SetUpdateMode(false);
+
+    if (bSearchTextEmpty)
     {
-        bool bIsDate = maMembers[i].mbDate;
-        bool bPartialMatch = false;
-
-        aLabelDisp = maMembers[i].maName;
-        if ( aLabelDisp.isEmpty() )
-            aLabelDisp = ScResId( STR_EMPTYDATA );
-
-        if ( !bSearchTextEmpty )
+        // when there are a lot of rows, it is cheaper to simply clear the tree and re-initialise
+        maChecks->Clear();
+        initMembers();
+    }
+    else
+    {
+        for (size_t i = 0; i < n; ++i)
         {
+            bool bIsDate = maMembers[i].mbDate;
+            bool bPartialMatch = false;
+
+            OUString aLabelDisp = maMembers[i].maName;
+            if ( aLabelDisp.isEmpty() )
+                aLabelDisp = ScResId( STR_EMPTYDATA );
+
             if ( !bIsDate )
                 bPartialMatch = ( ScGlobal::getCharClassPtr()->lowercase( aLabelDisp ).indexOf( aSearchText ) != -1 );
             else if ( maMembers[i].meDatePartType == ScCheckListMember::DAY ) // Match with both numerical and text version of month
@@ -1244,30 +1251,19 @@ IMPL_LINK_NOARG(ScCheckListMenuWindow, EdModifyHdl, Edit&, void)
                                 maMembers[i].maRealName + maMembers[i].maDateParts[1] )).indexOf( aSearchText ) != -1);
             else
                 continue;
-        }
-        else if ( bIsDate && maMembers[i].meDatePartType != ScCheckListMember::DAY )
-            continue;
 
-        if ( bSearchTextEmpty )
-        {
-            SvTreeListEntry* pLeaf = maChecks->ShowCheckEntry( aLabelDisp, maMembers[i], true, maMembers[i].mbVisible );
-            updateMemberParents( pLeaf, i );
-            if ( maMembers[i].mbVisible )
+            if ( bPartialMatch )
+            {
+                SvTreeListEntry* pLeaf = maChecks->ShowCheckEntry( aLabelDisp, maMembers[i] );
+                updateMemberParents( pLeaf, i );
                 ++nSelCount;
-            continue;
-        }
-
-        if ( bPartialMatch )
-        {
-            SvTreeListEntry* pLeaf = maChecks->ShowCheckEntry( aLabelDisp, maMembers[i] );
-            updateMemberParents( pLeaf, i );
-            ++nSelCount;
-        }
-        else
-        {
-            maChecks->ShowCheckEntry( aLabelDisp, maMembers[i], false, false );
-            if( bIsDate )
-                bSomeDateDeletes = true;
+            }
+            else
+            {
+                maChecks->ShowCheckEntry( aLabelDisp, maMembers[i], false, false );
+                if( bIsDate )
+                    bSomeDateDeletes = true;
+            }
         }
     }
 
@@ -1280,6 +1276,8 @@ IMPL_LINK_NOARG(ScCheckListMenuWindow, EdModifyHdl, Edit&, void)
             updateMemberParents( nullptr, i );
         }
     }
+
+    maChecks->SetUpdateMode(true);
 
     if ( nSelCount == n )
         maChkToggleAll->SetState( TRISTATE_TRUE );

commit 5267c188e613ac5fb7c565c70c33454d3dd81ee6

fix tree disabled in autofilter pulldown, tdf#76481 related
regression from
    commit b8a0abc81503ff6e9850b8d681ddd7562a8acc80
    Date:   Thu May 21 15:05:08 2020 +0200
    tdf#76481 speed up searching in autofilter pulldown

Change-Id: Iac7fba87e12ae68a040706694ef94655113a6491
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95142
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/cctrl/checklistmenu.cxx
@@ -1231,7 +1231,7 @@ IMPL_LINK_NOARG(ScCheckListMenuWindow, EdModifyHdl, Edit&, void)
     {
         // when there are a lot of rows, it is cheaper to simply clear the tree and re-initialise
         maChecks->Clear();
-        initMembers();
+        nSelCount = initMembers();
     }
     else
     {
@@ -1889,10 +1889,11 @@ void ScCheckListMenuWindow::setHasDates(bool bHasDates)
         maChecks->SetStyle(WB_HASBUTTONS);
 }
 
-void ScCheckListMenuWindow::initMembers()
+size_t ScCheckListMenuWindow::initMembers()
 {
     size_t n = maMembers.size();
     size_t nVisMemCount = 0;
+
 
     maChecks->SetUpdateMode(false);
     maChecks->GetModel()->EnableInvalidate(false);
@@ -1945,6 +1946,7 @@ void ScCheckListMenuWindow::initMembers()
 
     maChecks->GetModel()->EnableInvalidate(true);
     maChecks->SetUpdateMode(true);
+    return nVisMemCount;
 }
 
 void ScCheckListMenuWindow::setConfig(const Config& rConfig)

sc/source/ui/inc/checklistmenu.hxx
@@ -346,7 +346,7 @@ public:
     void setHasDates(bool bHasDates);
     void addDateMember(const OUString& rName, double nVal, bool bVisible);
     void addMember(const OUString& rName, bool bVisible);
-    void initMembers();
+    size_t initMembers();
     void setConfig(const Config& rConfig);
 
     bool isAllSelected() const;