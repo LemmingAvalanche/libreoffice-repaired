LemmingAvalanche/libreoffice-repaired

commit afe7eeaee8f168d2ccb0a7fccb4bc9855aa0b07b

Prevent deadlock accessing clipboard
Main thread call stack:

	win32u.dll!NtUserMsgWaitForMultipleObjectsEx()
	user32.dll!RealMsgWaitForMultipleObjectsEx()
	combase.dll!CCliModalLoop::BlockFn(void * * ahEvent, unsigned long cEvents, unsigned long * lpdwSignaled) Line 2233
	combase.dll!ModalLoop(CSyncClientCall * pClientCall) Line 166
	combase.dll!ClassicSTAThreadDispatchCrossApartmentCall(tagRPCOLEMESSAGE * pMessage, OXIDEntry * pOXIDEntry, CSyncClientCall * pClientCall) Line 321
	[Inline Frame] combase.dll!CSyncClientCall::SwitchAptAndDispatchCall(tagRPCOLEMESSAGE * pMessage) Line 5696
	combase.dll!CSyncClientCall::SendReceive2(tagRPCOLEMESSAGE * pMessage, unsigned long * pstatus) Line 5377
	[Inline Frame] combase.dll!SyncClientCallRetryContext::SendReceiveWithRetry(tagRPCOLEMESSAGE *) Line 1617
	[Inline Frame] combase.dll!CSyncClientCall::SendReceiveInRetryContext(SyncClientCallRetryContext *) Line 567
	combase.dll!ClassicSTAThreadSendReceive(CSyncClientCall * pClientCall, tagRPCOLEMESSAGE * pMsg, unsigned long * pulStatus) Line 549
	combase.dll!CSyncClientCall::SendReceive(tagRPCOLEMESSAGE * pMessage, unsigned long * pulStatus) Line 783
	combase.dll!CClientChannel::SendReceive(tagRPCOLEMESSAGE * pMessage, unsigned long * pulStatus) Line 653
	combase.dll!NdrExtpProxySendReceive(void * pThis, _MIDL_STUB_MESSAGE * pStubMsg) Line 2002
	rpcrt4.dll!NdrpClientCall3()
	rpcrt4.dll!NdrClientCall3()
	[Inline Frame] combase.dll!IDataObject_RemoteGetData_Proxy(IDataObject *) Line 2082
	combase.dll!IDataObject_GetData_Proxy(IDataObject * This, tagFORMATETC * pformatetcIn, tagSTGMEDIUM * pMedium) Line 1270
	sysdtrans.dll!CAPNDataObject::GetData(tagFORMATETC * pFormatetc, tagSTGMEDIUM * pmedium) Line 146
	sysdtrans.dll!CDOTransferable::getClipboardData(CFormatEtc & aFormatEtc) Line 421
	sysdtrans.dll!CDOTransferable::getTransferData(const com::sun::star::datatransfer::DataFlavor & aFlavor) Line 251

vcl/source/treelist/transfer.cxx
@@ -1442,6 +1442,10 @@ Any TransferableDataHelper::GetAny( const DataFlavor& rFlavor, const OUString& r
                 {
                     if( ( nRequestFormat == format.mnSotId ) && !rFlavor.MimeType.equalsIgnoreAsciiCase( format.MimeType ) )
                     {
+                        // Our own thread may handle the nested IDataObject::GetData call,
+                        // and try to acquire solar mutex
+                        SolarMutexReleaser r;
+
                         if (xTransfer2.is())
                             aRet = xTransfer2->getTransferData2(format, rDestDoc);
                         else
@@ -1455,6 +1459,10 @@ Any TransferableDataHelper::GetAny( const DataFlavor& rFlavor, const OUString& r
 
             if( !aRet.hasValue() )
             {
+                // Our own thread may handle the nested IDataObject::GetData call,
+                // and try to acquire solar mutex
+                SolarMutexReleaser r;
+
                 if (xTransfer2.is())
                     aRet = xTransfer2->getTransferData2(rFlavor, rDestDoc);
                 else

commit 8ce1fb2b49e8385bb53ab997cc5343189a69e25e

tdf#133365: only release solar mutex on Windows
Regression after afe7eeaee8f168d2ccb0a7fccb4bc9855aa0b07b

Change-Id: Id12a81b2ca79edb7ae056b357dcfd1d8f4bb56ef
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95088
Tested-by: Jenkins
Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-by: Jean-Baptiste Faure <jbfaure@libreoffice.org>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

vcl/source/treelist/transfer.cxx
@@ -1442,9 +1442,12 @@ Any TransferableDataHelper::GetAny( const DataFlavor& rFlavor, const OUString& r
                 {
                     if( ( nRequestFormat == format.mnSotId ) && !rFlavor.MimeType.equalsIgnoreAsciiCase( format.MimeType ) )
                     {
+// tdf#133365: only release solar mutex on Windows
+#ifdef _WIN32
                         // Our own thread may handle the nested IDataObject::GetData call,
                         // and try to acquire solar mutex
                         SolarMutexReleaser r;
+#endif // _WIN32
 
                         if (xTransfer2.is())
                             aRet = xTransfer2->getTransferData2(format, rDestDoc);
@@ -1459,9 +1462,12 @@ Any TransferableDataHelper::GetAny( const DataFlavor& rFlavor, const OUString& r
 
             if( !aRet.hasValue() )
             {
+// tdf#133365: only release solar mutex on Windows
+#ifdef _WIN32
                 // Our own thread may handle the nested IDataObject::GetData call,
                 // and try to acquire solar mutex
                 SolarMutexReleaser r;
+#endif // _WIN32
 
                 if (xTransfer2.is())
                     aRet = xTransfer2->getTransferData2(rFlavor, rDestDoc);