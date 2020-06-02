LemmingAvalanche/libreoffice-repaired

commit 1dcf7b57b222368f7b6d29bf1add26f0c14c4bcb

Don't create unnecessary tempfiles for opened doc
A copy of the doc was created for each opened file as a temp file.
Try to avoid that.

Change-Id: I11db2a9c8d77b954de56a6e977b1555c8f876416
Reviewed-on: https://gerrit.libreoffice.org/81031
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

sfx2/source/doc/objserv.cxx
@@ -1608,7 +1608,7 @@ uno::Sequence< security::DocumentSignatureInformation > SfxObjectShell::GetDocum
                                                                 uno::Reference< io::XInputStream >() );
             else
             {
-                if (GetMedium()->GetStorage().is())
+                if (GetMedium()->GetStorage(false).is())
                 {
                     // Something ZIP-based.
                     // Only call into xmlsecurity if we see a signature stream,

sfx2/source/doc/objcont.cxx
@@ -404,7 +404,7 @@ void SfxObjectShell::UpdateFromTemplate_Impl(  )
     // tdf#113935 - do not remove this line - somehow, it makes the process
     // of switching from viewing a read-only document to opening it in writable
     // mode much faster.
-    uno::Reference< embed::XStorage > xDocStor = pFile->GetStorage();
+    uno::Reference< embed::XStorage > xDocStor = pFile->GetStorage(false);
 
     // only for own storage formats
     if ( !pFile->GetFilter() || !pFile->GetFilter()->IsOwnFormat() )

commit 60be045764a260b1fd89e39ff21aa619ba31217f

tdf#130354 sfx2: make PDF signing UI work again
Regression from commit 1dcf7b57b222368f7b6d29bf1add26f0c14c4bcb (Don't
create unnecessary tempfiles for opened doc, 2019-10-18), restore the
temporary file creation for the PDF case, as it's needed for PDF signing
to work.

Change-Id: I945f04f6ab90d0e04fb14a7634ec877fb0af1a0f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95367
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sfx2/source/doc/objserv.cxx
@@ -1568,6 +1568,10 @@ uno::Sequence< security::DocumentSignatureInformation > SfxObjectShell::GetDocum
                 else
                 {
                     // Not ZIP-based, e.g. PDF.
+
+                    // Create temp file if needed.
+                    GetMedium()->CreateTempFile(/*bReplace=*/false);
+
                     std::unique_ptr<SvStream> pStream(utl::UcbStreamHelper::CreateStream(GetMedium()->GetName(), StreamMode::READ));
                     uno::Reference<io::XStream> xStream(new utl::OStreamWrapper(*pStream));
                     uno::Reference<io::XInputStream> xInputStream(xStream, uno::UNO_QUERY);