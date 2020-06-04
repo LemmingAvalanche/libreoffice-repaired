LemmingAvalanche/libreoffice-repaired

commit 7af18ebf2a5c6306fdd0e11432ebce9bc425dcf2

tdf#95033 DOCX import: apply tblPrEx table border
exceptions of table rows, fixing for example
missing cell borders.

Change-Id: Id66af2706f564f46a1afd04bad65d1bf086a3232
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87514
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -738,6 +738,12 @@ CellPropertyValuesSeq_t DomainMapperTableHandler::endTableGetCellProperties(Tabl
         sal_Int32 nCell = 0;
         pCellProperties[nRow].realloc( aRowOfCellsIterator->size() );
         beans::PropertyValues* pSingleCellProperties = pCellProperties[nRow].getArray();
+
+        static const PropertyIds pBorders[] =
+        {
+            PROP_TOP_BORDER, PROP_LEFT_BORDER, PROP_BOTTOM_BORDER, PROP_RIGHT_BORDER
+        };
+
         while( aCellIterator != aCellIteratorEnd )
         {
             PropertyMapPtr pAllCellProps( new PropertyMap );
@@ -794,14 +800,33 @@ CellPropertyValuesSeq_t DomainMapperTableHandler::endTableGetCellProperties(Tabl
                     PropertyMapPtr pStyleProps = rInfo.pTableStyle->GetProperties( nCnfStyleMask );
 
                     // Check if we need to clean up some empty border definitions to match what Word does.
-                    static const PropertyIds pBorders[] =
-                    {
-                        PROP_TOP_BORDER, PROP_LEFT_BORDER, PROP_BOTTOM_BORDER, PROP_RIGHT_BORDER
-                    };
+                    // Apply also possible tblPrEx borders on cells
                     for (const PropertyIds& rBorder : pBorders)
                     {
                         o3tl::optional<PropertyMap::Property> oStyleCellBorder = pStyleProps->getProperty(rBorder);
+                        o3tl::optional<PropertyMap::Property> oRowCellBorder;
+                        // we can have table border exception in row properties
+                        if (*aRowIter && (*aRowIter)->isSet(rBorder))
+                            oRowCellBorder = (*aRowIter)->getProperty(rBorder);
                         o3tl::optional<PropertyMap::Property> oDirectCellBorder = (*aCellIterator)->getProperty(rBorder);
+                        if (oRowCellBorder && oDirectCellBorder)
+                        {
+                            table::BorderLine2 aRowCellBorder = oRowCellBorder->second.get<table::BorderLine2>();
+                            table::BorderLine2 aDirectCellBorder = oDirectCellBorder->second.get<table::BorderLine2>();
+                            if (aRowCellBorder.LineStyle != table::BorderLineStyle::NONE && aDirectCellBorder.LineStyle == table::BorderLineStyle::NONE)
+                                oDirectCellBorder = o3tl::optional<PropertyMap::Property>();
+                        }
+                        if (oRowCellBorder && !oDirectCellBorder)
+                        {
+                            table::BorderLine2 aRowCellBorder = oRowCellBorder->second.get<table::BorderLine2>();
+                            if ( (rBorder == PROP_LEFT_BORDER && nCnfStyleMask&CNF_FIRST_COLUMN)
+                                    || (rBorder == PROP_RIGHT_BORDER && bIsEndCol)
+                                    || (rBorder != PROP_LEFT_BORDER && rBorder != PROP_RIGHT_BORDER) )
+                            {
+                                (*aCellIterator)->Insert(rBorder, uno::makeAny(aRowCellBorder));
+                                oDirectCellBorder = (*aCellIterator)->getProperty(rBorder);
+                            }
+                        }
                         if (oStyleCellBorder && oDirectCellBorder)
                         {
                             // We have a cell border from the table style and as direct formatting as well.
@@ -914,6 +939,14 @@ CellPropertyValuesSeq_t DomainMapperTableHandler::endTableGetCellProperties(Tabl
             ++nCell;
             ++aCellIterator;
         }
+
+        // remove tblPrEx table border properties stored in row properties temporarily
+        for (const PropertyIds& rBorder : pBorders)
+        {
+            if (*aRowIter && (*aRowIter)->isSet(rBorder))
+                (*aRowIter)->Erase(rBorder);
+        }
+
         ++nRow;
         ++aRowOfCellsIterator;
         ++aRowIter;

writerfilter/source/dmapper/TablePropertiesHandler.cxx
@@ -213,6 +213,25 @@ namespace dmapper {
                     pManager->SetLayoutType(static_cast<sal_uInt32>(nIntValue));
             }
             break;
+            case NS_ooxml::LN_CT_TblPrEx_tblBorders:
+            {
+                writerfilter::Reference<Properties>::Pointer_t pProperties = rSprm.getProps();
+                if( pProperties.get())
+                {
+                    auto pBorderHandler = std::make_shared<BorderHandler>(true);
+                    pProperties->resolve(*pBorderHandler);
+                    TablePropertyMapPtr pTablePropMap( new TablePropertyMap );
+                    pTablePropMap->InsertProps(pBorderHandler->getProperties());
+
+#ifdef DBG_UTIL
+                    pTablePropMap->dumpXml();
+#endif
+                    // store row-level table border exceptions in row properties temporarily
+                    // and apply them on cells later
+                    insertRowProps( pTablePropMap );
+                }
+            }
+            break;
             case NS_ooxml::LN_CT_TcPrBase_tcBorders ://cell borders
             //contains CT_TcBorders_left, right, top, bottom
             {

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -593,6 +593,23 @@ DECLARE_OOXMLEXPORT_TEST(testfdo80097, "fdo80097.docx")
     assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[1]/w:tc[1]/w:tbl/w:tr[1]/w:tc[1]/w:tcPr/w:tcBorders/w:insideV",0);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf95033, "tdf95033.docx")
+{
+    //tdf#95033 : Table borders defined by row-level tblPrEx are not getting preserved.
+
+    xmlDocPtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    //Not disabled table cell borders
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[5]/w:tc[1]/w:tcPr/w:tcBorders/w:start[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[5]/w:tc[2]/w:tcPr/w:tcBorders/w:end[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[9]/w:tc[1]/w:tcPr/w:tcBorders/w:start[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[9]/w:tc[2]/w:tcPr/w:tcBorders/w:end[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[9]/w:tc[1]/w:tcPr/w:tcBorders/w:bottom[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[9]/w:tc[2]/w:tcPr/w:tcBorders/w:bottom[@w:val = 'nil']", 0);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testFdo77129, "fdo77129.docx")
 {
     // The problem was that text after TOC field was missing if footer reference  comes in field.

writerfilter/source/ooxml/model.xml
@@ -18491,6 +18491,7 @@
     </resource>
     <resource name="CT_TblPrEx" resource="Properties">
         <element name="tblPrExChange" tokenid="ooxml:CT_TblPrEx_tblPrExChange"/>
+        <element name="tblBorders" tokenid="ooxml:CT_TblPrEx_tblBorders"/>
     </resource>
     <resource name="CT_Tbl" resource="TextTable"/>
     <!-- tdf#111550 : allow <w:tbl> at paragraph level (despite this is illegal according to ECMA-376-1:2016) - bug-to-bug compatibility with Word -->

commit 4dd6995b077f5247c6de265a44ee4b962fc635e6

tdf#133455 DOCX import: fix table border regression
caused by commit 7af18ebf2a5c6306fdd0e11432ebce9bc425dcf2
(tdf#95033 DOCX import: apply tblPrEx table border).

Change-Id: Ief4da3bce3282b5dab1ce4dabb4e075efcc1abaf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95504
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -762,11 +762,6 @@ CellPropertyValuesSeq_t DomainMapperTableHandler::endTableGetCellProperties(Tabl
         pCellProperties[nRow].realloc( aRowOfCellsIterator->size() );
         beans::PropertyValues* pSingleCellProperties = pCellProperties[nRow].getArray();
 
-        static const PropertyIds pBorders[] =
-        {
-            PROP_TOP_BORDER, PROP_LEFT_BORDER, PROP_BOTTOM_BORDER, PROP_RIGHT_BORDER
-        };
-
         while( aCellIterator != aCellIteratorEnd )
         {
             PropertyMapPtr pAllCellProps( new PropertyMap );
@@ -823,33 +818,15 @@ CellPropertyValuesSeq_t DomainMapperTableHandler::endTableGetCellProperties(Tabl
                     PropertyMapPtr pStyleProps = rInfo.pTableStyle->GetProperties( nCnfStyleMask );
 
                     // Check if we need to clean up some empty border definitions to match what Word does.
-                    // Apply also possible tblPrEx borders on cells
+                    static const PropertyIds pBorders[] =
+                    {
+                        PROP_TOP_BORDER, PROP_LEFT_BORDER, PROP_BOTTOM_BORDER, PROP_RIGHT_BORDER
+                    };
                     for (const PropertyIds& rBorder : pBorders)
                     {
                         std::optional<PropertyMap::Property> oStyleCellBorder = pStyleProps->getProperty(rBorder);
                         std::optional<PropertyMap::Property> oRowCellBorder;
-                        // we can have table border exception in row properties
-                        if (*aRowIter && (*aRowIter)->isSet(rBorder))
-                            oRowCellBorder = (*aRowIter)->getProperty(rBorder);
                         std::optional<PropertyMap::Property> oDirectCellBorder = (*aCellIterator)->getProperty(rBorder);
-                        if (oRowCellBorder && oDirectCellBorder)
-                        {
-                            table::BorderLine2 aRowCellBorder = oRowCellBorder->second.get<table::BorderLine2>();
-                            table::BorderLine2 aDirectCellBorder = oDirectCellBorder->second.get<table::BorderLine2>();
-                            if (aRowCellBorder.LineStyle != table::BorderLineStyle::NONE && aDirectCellBorder.LineStyle == table::BorderLineStyle::NONE)
-                                oDirectCellBorder = std::optional<PropertyMap::Property>();
-                        }
-                        if (oRowCellBorder && !oDirectCellBorder)
-                        {
-                            table::BorderLine2 aRowCellBorder = oRowCellBorder->second.get<table::BorderLine2>();
-                            if ( (rBorder == PROP_LEFT_BORDER && nCnfStyleMask&CNF_FIRST_COLUMN)
-                                    || (rBorder == PROP_RIGHT_BORDER && bIsEndCol)
-                                    || (rBorder != PROP_LEFT_BORDER && rBorder != PROP_RIGHT_BORDER) )
-                            {
-                                (*aCellIterator)->Insert(rBorder, uno::makeAny(aRowCellBorder));
-                                oDirectCellBorder = (*aCellIterator)->getProperty(rBorder);
-                            }
-                        }
                         if (oStyleCellBorder && oDirectCellBorder)
                         {
                             // We have a cell border from the table style and as direct formatting as well.
@@ -972,14 +949,6 @@ CellPropertyValuesSeq_t DomainMapperTableHandler::endTableGetCellProperties(Tabl
             ++nCell;
             ++aCellIterator;
         }
-
-        // remove tblPrEx table border properties stored in row properties temporarily
-        for (const PropertyIds& rBorder : pBorders)
-        {
-            if (*aRowIter && (*aRowIter)->isSet(rBorder))
-                (*aRowIter)->Erase(rBorder);
-        }
-
         ++nRow;
         ++aRowOfCellsIterator;
         ++aRowIter;

writerfilter/source/dmapper/TableManager.cxx
@@ -101,6 +101,22 @@ void TableManager::cellProps(const TablePropertyMapPtr& pProps)
 #endif
 }
 
+void TableManager::tableExceptionProps(const TablePropertyMapPtr& pProps)
+{
+#ifdef DBG_UTIL
+    TagLogger::getInstance().startElement("tablemanager.tableExceptionProps");
+#endif
+
+    if (getTableExceptionProps())
+        getTableExceptionProps()->InsertProps(pProps.get());
+    else
+        mState.setTableExceptionProps(pProps);
+
+#ifdef DBG_UTIL
+    TagLogger::getInstance().endElement();
+#endif
+}
+
 void TableManager::utext(const sal_uInt8* data, std::size_t len)
 {
     // optimization: cell/row end characters are the last characters in a run

writerfilter/source/dmapper/TableManager.hxx
@@ -58,6 +58,11 @@ class TableManager : public virtual SvRefBase
         TablePropertyMapPtr mpRowProps;
 
         /**
+         table exception properties of the current row
+         */
+        TablePropertyMapPtr mpTableExceptionProps;
+
+        /**
          properties of the current table
          */
         std::stack<TablePropertyMapPtr> mTableProps;
@@ -109,7 +114,7 @@ class TableManager : public virtual SvRefBase
 
         void resetCellProps()
         {
-            mpCellProps.clear();
+            mpCellProps = getTableExceptionProps();
         }
 
         void setCellProps(TablePropertyMapPtr pProps)
@@ -124,6 +129,10 @@ class TableManager : public virtual SvRefBase
 
         void resetRowProps()
         {
+            // reset also table exception and
+            // its copy set by the previous resetCellProps()
+            mpTableExceptionProps.clear();
+            resetCellProps();
             mpRowProps.clear();
         }
 
@@ -135,6 +144,16 @@ class TableManager : public virtual SvRefBase
         const TablePropertyMapPtr& getRowProps() const
         {
             return mpRowProps;
+        }
+
+        void setTableExceptionProps(TablePropertyMapPtr pProps)
+        {
+            mpTableExceptionProps = pProps;
+        }
+
+        const TablePropertyMapPtr& getTableExceptionProps() const
+        {
+            return mpTableExceptionProps;
         }
 
         void resetTableProps()
@@ -207,6 +226,11 @@ public:
     TablePropertyMapPtr const & getRowProps() const
     {
         return mState.getRowProps();
+    }
+
+    TablePropertyMapPtr const & getTableExceptionProps() const
+    {
+        return mState.getTableExceptionProps();
     }
 
 protected:
@@ -453,6 +477,13 @@ public:
        @param pProps   the properties
      */
     virtual void insertRowProps(const TablePropertyMapPtr& pProps);
+
+    /**
+       Handle table exception properties of the current row.
+
+       @param pProps   the properties
+     */
+    virtual void tableExceptionProps(const TablePropertyMapPtr& pProps);
 
     /**
        Handle properties of the current table.

writerfilter/source/dmapper/TablePropertiesHandler.cxx
@@ -224,9 +224,7 @@ namespace writerfilter::dmapper {
 #ifdef DBG_UTIL
                     pTablePropMap->dumpXml();
 #endif
-                    // store row-level table border exceptions in row properties temporarily
-                    // and apply them on cells later
-                    insertRowProps( pTablePropMap );
+                    tableExceptionProps( pTablePropMap );
                 }
             }
             break;

writerfilter/source/dmapper/TablePropertiesHandler.hxx
@@ -74,6 +74,17 @@ private:
             m_pCurrentProperties->InsertProps(pProps.get());
     };
 
+    void tableExceptionProps( TablePropertyMapPtr pProps )
+    {
+        if ( m_pTableManager )
+        {
+            m_pTableManager->tableExceptionProps( pProps );
+            cellProps( pProps );
+        }
+        else
+            m_pCurrentProperties->InsertProps(pProps.get());
+    };
+
     void insertTableProps( TablePropertyMapPtr pProps )
     {
         if ( m_pTableManager )

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -726,6 +726,23 @@ DECLARE_OOXMLEXPORT_TEST(testTdf95033, "tdf95033.docx")
     assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[9]/w:tc[2]/w:tcPr/w:tcBorders/w:bottom[@w:val = 'nil']", 0);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf133455, "tdf133455.docx")
+{
+    xmlDocUniquePtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    //Not disabled table cell borders
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[1]/w:tc[1]/w:tcPr/w:tcBorders/w:top[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[1]/w:tc[1]/w:tcPr/w:tcBorders/w:start[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[1]/w:tc[1]/w:tcPr/w:tcBorders/w:end[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:tcPr/w:tcBorders/w:start[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:tcPr/w:tcBorders/w:end[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[11]/w:tc[1]/w:tcPr/w:tcBorders/w:start[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[11]/w:tc[1]/w:tcPr/w:tcBorders/w:end[@w:val = 'nil']", 0);
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[11]/w:tc[1]/w:tcPr/w:tcBorders/w:bottom[@w:val = 'nil']", 0);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128646, "tdf128646.docx")
 {
     // The problem was that not hidden shapes anchored to empty hidden paragraphs were imported as hidden.