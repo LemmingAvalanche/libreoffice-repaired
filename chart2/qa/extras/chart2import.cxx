LemmingAvalanche/libreoffice-repaired

commit 28b56c35f60424035ed9adaf2c27423ecf864134

tdf#132174 Chart DOCX import: fix label number format
The inner data table of charts has no own cell number format,
unlike the MSO embedded XLSX, so we always have to set the link
number format to false, if we have an inner data table.

Regression from commit: fa1d250824262315fc8992f58d2d9b9c9879ac4b
(tdf#129173 tdf#129175 Fix number format of data labels)

Change-Id: I5e7865cb8c19fc6fae5b68305c7dc302516c0ad4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92695
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: Tünde Tóth <tundeth@gmail.com>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/seriesconverter.cxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/chart2/RelativePosition.hpp>
 #include <com/sun/star/chart/ErrorBarStyle.hpp>
 #include <com/sun/star/chart2/DataPointLabel.hpp>
+#include <com/sun/star/chart2/XChartDocument.hpp>
 #include <com/sun/star/chart2/XDataPointCustomLabelField.hpp>
 #include <com/sun/star/chart2/DataPointCustomLabelField.hpp>
 #include <com/sun/star/chart2/DataPointCustomLabelFieldType.hpp>
@@ -323,6 +324,9 @@ void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDa
     if( !mrModel.mbDeleted )
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
+        // tdf#132174: the inner data table has no own cell number format.
+        if( getChartDocument()->hasInternalDataProvider() )
+            mrModel.maNumberFormat.mbSourceLinked = false;
         lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc );
 
         if (mrModel.mxShapeProp)

chart2/qa/extras/chart2import.cxx
@@ -90,6 +90,7 @@ public:
     // void testTextCanOverlapXLSX(); // TODO : temporarily disabled.
     void testTextBreakXLSX();
     void testNumberFormatsXLSX();
+    void testNumberFormatsDOCX();
 
     void testTransparentBackground(OUString const & filename);
 
@@ -204,6 +205,7 @@ public:
     // CPPUNIT_TEST(testTextCanOverlapXLSX); // TODO : temporarily disabled.
     CPPUNIT_TEST(testTextBreakXLSX);
     CPPUNIT_TEST(testNumberFormatsXLSX);
+    CPPUNIT_TEST(testNumberFormatsDOCX);
     CPPUNIT_TEST(testAutoTitleDelDefaultValue2007XLSX);
     CPPUNIT_TEST(testAutoTitleDelDefaultValue2013XLSX);
     CPPUNIT_TEST(testDispBlanksAsDefaultValue2007XLSX);
@@ -1282,6 +1284,28 @@ void Chart2ImportTest::testNumberFormatsXLSX()
     CPPUNIT_ASSERT_EQUAL(false, bSuccess);
     bSuccess = xPropertySet->getPropertyValue(CHART_UNONAME_LINK_TO_SRC_NUMFMT) >>= bLinkNumberFormatToSource;
     CPPUNIT_ASSERT_MESSAGE("\"LinkNumberFormatToSource\" should be set to true.", bSuccess && bLinkNumberFormatToSource);
+}
+
+void Chart2ImportTest::testNumberFormatsDOCX()
+{
+    load("/chart2/qa/extras/data/docx/", "tdf132174.docx");
+    uno::Reference< chart2::XChartDocument > xChartDoc(getChartDocFromWriter(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    css::uno::Reference<chart2::XDiagram> xDiagram(xChartDoc->getFirstDiagram(), UNO_SET_THROW);
+    Reference<chart2::XDataSeries> xDataSeries = getDataSeriesFromDoc(xChartDoc, 0);
+    uno::Reference<beans::XPropertySet> xPropertySet(xDataSeries, uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT(xPropertySet.is());
+
+    sal_Int32 nNumberFormat;
+    bool bLinkNumberFormatToSource = true;
+    const sal_Int32 nChartDataNumberFormat = getNumberFormat(xChartDoc, "0%");
+    xPropertySet->getPropertyValue(CHART_UNONAME_NUMFMT) >>= nNumberFormat;
+    CPPUNIT_ASSERT_EQUAL(nChartDataNumberFormat, nNumberFormat);
+    xPropertySet->getPropertyValue(CHART_UNONAME_LINK_TO_SRC_NUMFMT) >>= bLinkNumberFormatToSource;
+    // LinkNumberFormatToSource should be set to false even if the OOXML contain a true value,
+    // because the inner data table of charts have no own number format!
+    CPPUNIT_ASSERT_MESSAGE("\"LinkNumberFormatToSource\" should be set to false.", !bLinkNumberFormatToSource);
 }
 
 void Chart2ImportTest::testAutoTitleDelDefaultValue2007XLSX()

commit f276ef67792dea33fd984376fe2216dd36ec41ca

tdf#133632 Chart DOCX Import: fix percentage number format
Set the LinkNumberFormatToSource to false only if we have
an inner data table and the labels are shown as values.

Regression from commit: 28b56c35f60424035ed9adaf2c27423ecf864134
(tdf#132174 Chart DOCX import: fix label number format)

Change-Id: I879c5d81709995bfa49c18e0c84aaf6dc3dea41c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95493
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/seriesconverter.cxx
@@ -343,7 +343,7 @@ void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDa
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
         // tdf#132174: the inner data table has no own cell number format.
-        if( getChartDocument()->hasInternalDataProvider() )
+        if( getChartDocument()->hasInternalDataProvider() && mrModel.mobShowVal.get(!bMSO2007Doc) )
             mrModel.maNumberFormat.mbSourceLinked = false;
         lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc );
 

chart2/qa/extras/chart2import.cxx
@@ -91,6 +91,7 @@ public:
     void testTextBreakXLSX();
     void testNumberFormatsXLSX();
     void testNumberFormatsDOCX();
+    void testPercentageNumberFormatsDOCX();
 
     void testTransparentBackground(OUString const & filename);
 
@@ -208,6 +209,7 @@ public:
     CPPUNIT_TEST(testTextBreakXLSX);
     CPPUNIT_TEST(testNumberFormatsXLSX);
     CPPUNIT_TEST(testNumberFormatsDOCX);
+    CPPUNIT_TEST(testPercentageNumberFormatsDOCX);
     CPPUNIT_TEST(testAutoTitleDelDefaultValue2007XLSX);
     CPPUNIT_TEST(testAutoTitleDelDefaultValue2013XLSX);
     CPPUNIT_TEST(testDispBlanksAsDefaultValue2007XLSX);
@@ -1310,6 +1312,26 @@ void Chart2ImportTest::testNumberFormatsDOCX()
     // LinkNumberFormatToSource should be set to false even if the OOXML contain a true value,
     // because the inner data table of charts have no own number format!
     CPPUNIT_ASSERT_MESSAGE("\"LinkNumberFormatToSource\" should be set to false.", !bLinkNumberFormatToSource);
+}
+
+void Chart2ImportTest::testPercentageNumberFormatsDOCX()
+{
+    load("/chart2/qa/extras/data/docx/", "tdf133632.docx");
+    uno::Reference< chart2::XChartDocument > xChartDoc(getChartDocFromWriter(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    css::uno::Reference<chart2::XDiagram> xDiagram(xChartDoc->getFirstDiagram(), UNO_SET_THROW);
+    Reference<chart2::XDataSeries> xDataSeries = getDataSeriesFromDoc(xChartDoc, 0);
+    uno::Reference<beans::XPropertySet> xPropertySet(xDataSeries, uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT(xPropertySet.is());
+
+    bool bLinkNumberFormatToSource = false;
+    chart2::DataPointLabel aLabel;
+    xPropertySet->getPropertyValue("Label") >>= aLabel;
+    CPPUNIT_ASSERT_EQUAL(sal_False, aLabel.ShowNumber);
+    CPPUNIT_ASSERT_EQUAL(sal_True, aLabel.ShowNumberInPercent);
+    bool bSuccess = xPropertySet->getPropertyValue(CHART_UNONAME_LINK_TO_SRC_NUMFMT) >>= bLinkNumberFormatToSource;
+    CPPUNIT_ASSERT_MESSAGE("\"LinkNumberFormatToSource\" should be set to true.", bSuccess && bLinkNumberFormatToSource);
 }
 
 void Chart2ImportTest::testAutoTitleDelDefaultValue2007XLSX()