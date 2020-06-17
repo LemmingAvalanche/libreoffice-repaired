LemmingAvalanche/libreoffice-repaired

commit 96e66941e2e844a08c09a9c5df1deb593f58793a

mariadb: upgrade to release 3.1.8
Fixes CVE-2018-3081 CVE-2020-2574 CVE-2020-2752 CVE-2020-2922 CVE-2020-13249

Remove obsolete patches:
* mariadb-msvc.patch.1
* mariadb-swap.patch
* mariadb-inline.patch.1
* mariadb-CONC-104.patch.1

Don't build anything from plugins/ in the hope that it's not needed.

Change-Id: I1c8633866b7108a8bb22dae0e0dd5f4a44bf5150
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96466
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

external/mariadb-connector-c/StaticLibrary_mariadb-connector-c.mk
@@ -34,89 +34,39 @@ $(eval $(call gb_StaticLibrary_set_include,mariadb-connector-c,\
 endif
 
 $(eval $(call gb_StaticLibrary_add_generated_cobjects,mariadb-connector-c,\
-	UnpackedTarball/mariadb-connector-c/libmariadb/array \
-	UnpackedTarball/mariadb-connector-c/libmariadb/ma_dyncol \
-	UnpackedTarball/mariadb-connector-c/libmariadb/bchange \
-	UnpackedTarball/mariadb-connector-c/libmariadb/bmove \
 	UnpackedTarball/mariadb-connector-c/libmariadb/bmove_upp \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_charset \
-	UnpackedTarball/mariadb-connector-c/libmariadb/hash \
-	UnpackedTarball/mariadb-connector-c/libmariadb/violite \
-	UnpackedTarball/mariadb-connector-c/libmariadb/net \
-	UnpackedTarball/mariadb-connector-c/libmariadb/charset \
-	UnpackedTarball/mariadb-connector-c/libmariadb/ma_time \
-	UnpackedTarball/mariadb-connector-c/libmariadb/dbug \
-	UnpackedTarball/mariadb-connector-c/libmariadb/default \
-	UnpackedTarball/mariadb-connector-c/libmariadb/errmsg \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_vsnprintf \
-	UnpackedTarball/mariadb-connector-c/libmariadb/errors \
-	UnpackedTarball/mariadb-connector-c/libmariadb/getopt1 \
-	UnpackedTarball/mariadb-connector-c/libmariadb/getopt \
 	UnpackedTarball/mariadb-connector-c/libmariadb/get_password \
-	UnpackedTarball/mariadb-connector-c/libmariadb/int2str \
-	UnpackedTarball/mariadb-connector-c/libmariadb/is_prefix \
-	UnpackedTarball/mariadb-connector-c/libmariadb/libmariadb \
-	UnpackedTarball/mariadb-connector-c/libmariadb/list \
-	UnpackedTarball/mariadb-connector-c/libmariadb/llstr \
-	UnpackedTarball/mariadb-connector-c/libmariadb/longlong2str \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_dirname \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_fn_ext \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_format \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_loadpath \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_pack \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_path \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_unixpath \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mf_wcomp \
-	UnpackedTarball/mariadb-connector-c/libmariadb/mulalloc \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_alloc \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_compress \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_div \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_error \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_fopen \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_fstream \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_getwd \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_init \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_lib \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_malloc \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_messnc \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_net \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_once \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_open \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_port \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_pthread \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_read \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_realloc \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_seek \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_static \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_symlink \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_thr_init \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_write \
-	UnpackedTarball/mariadb-connector-c/libmariadb/password \
-	UnpackedTarball/mariadb-connector-c/libmariadb/str2int \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strcend \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strcont \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strend \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strfill \
-	UnpackedTarball/mariadb-connector-c/libmariadb/string \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strinstr \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strmake \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strmov \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strnmov \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strtoll \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strtoull \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strxmov \
-	UnpackedTarball/mariadb-connector-c/libmariadb/strxnmov \
-	UnpackedTarball/mariadb-connector-c/libmariadb/thr_mutex \
-	UnpackedTarball/mariadb-connector-c/libmariadb/typelib \
-	UnpackedTarball/mariadb-connector-c/libmariadb/sha1 \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_stmt \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_loaddata \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_stmt_codec \
-	UnpackedTarball/mariadb-connector-c/libmariadb/client_plugin \
-	UnpackedTarball/mariadb-connector-c/libmariadb/my_auth \
-	UnpackedTarball/mariadb-connector-c/libmariadb/ma_secure \
-$(if $(filter $(OS),WNT), \
-	UnpackedTarball/mariadb-connector-c/win-iconv/win_iconv,) \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_alloc \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_array \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_charset \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_compress \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_context \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_default \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_dtoa \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_errmsg \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_hash \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_init \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_io \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_list \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_ll2str \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_loaddata \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_net \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_password \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_pvio \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_sha1 \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_stmt_codec \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_string \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_time \
+	UnpackedTarball/mariadb-connector-c/libmariadb/mariadb_async \
+	UnpackedTarball/mariadb-connector-c/libmariadb/mariadb_charset \
+	UnpackedTarball/mariadb-connector-c/libmariadb/mariadb_dyncol \
+	UnpackedTarball/mariadb-connector-c/libmariadb/mariadb_lib \
+	UnpackedTarball/mariadb-connector-c/libmariadb/mariadb_stmt \
+	UnpackedTarball/mariadb-connector-c/libmariadb/ma_client_plugin \
+	UnpackedTarball/mariadb-connector-c/plugins/auth/my_auth \
+	$(if $(filter $(OS),WNT), \
+		UnpackedTarball/mariadb-connector-c/libmariadb/win32_errmsg \
+		UnpackedTarball/mariadb-connector-c/win-iconv/win_iconv) \
 ))
 
 # vim: set noet sw=4 ts=4:

external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
@@ -11,27 +11,28 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,mariadb-connector-c))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,mariadb-connector-c,$(MARIADB_CONNECTOR_C_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/mysql_version.h,external/mariadb-connector-c/configs/mysql_version.h))
+$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/mariadb_version.h,external/mariadb-connector-c/configs/mariadb_version.h))
 
-ifneq ($(OS),WNT)
-ifeq ($(OS),MACOSX)
-$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/my_config.h,external/mariadb-connector-c/configs/mac_my_config.h))
+ifeq ($(OS),WNT)
+$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/ma_config.h,external/mariadb-connector-c/configs/wnt_ma_config.h))
 else
-$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/my_config.h,external/mariadb-connector-c/configs/linux_my_config.h))
+ifeq ($(OS),MACOSX)
+$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/ma_config.h,external/mariadb-connector-c/configs/mac_my_config.h))
+else
+$(eval $(call gb_UnpackedTarball_add_file,mariadb-connector-c,include/ma_config.h,external/mariadb-connector-c/configs/linux_my_config.h))
 endif
 endif # $(OS),WNT
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,mariadb-connector-c,1))
 
-# mariadb-connector-c/mariadb-static-inline.patch upstream at
-# <https://mariadb.atlassian.net/browse/CONC-18> "no external definition of
-# non-static inline local_thr_alarm in libmariadb/net.c":
 $(eval $(call gb_UnpackedTarball_add_patches,mariadb-connector-c,\
-    external/mariadb-connector-c/mariadb-swap.patch \
-    $(if $(filter MSC,$(COM)),external/mariadb-connector-c/mariadb-msvc.patch.1) \
-    external/mariadb-connector-c/mariadb-inline.patch.1 \
-    external/mariadb-connector-c/mariadb-CONC-104.patch.1 \
     external/mariadb-connector-c/clang-cl.patch.0 \
 ))
+
+# TODO are any "plugins" needed?
+$(eval $(call gb_UnpackedTarball_set_post_action,mariadb-connector-c, \
+	< libmariadb/ma_client_plugin.c.in sed -e 's/@EXTERNAL_PLUGINS@//' -e 's/@BUILTIN_PLUGINS@//' > libmariadb/ma_client_plugin.c  \
+))
+
 
 # vim: set noet sw=4 ts=4:

RepositoryExternal.mk
@@ -295,6 +295,13 @@ $(call gb_LinkTarget_add_libs,$(1),\
 	-liconv \
 )
 endif
+$(call gb_LinkTarget_use_system_win32_libs,$(1),\
+	ws2_32 \
+	advapi32 \
+	kernel32 \
+	shlwapi \
+	crypt32 \
+)
 
 endef
 define gb_ExternalProject__use_mariadb-connector-c

download.lst
@@ -178,8 +178,8 @@ export LPSOLVE_SHA256SUM := 171816288f14215c69e730f7a4f1c325739873e21f946ff83884
 export LPSOLVE_TARBALL := 26b3e95ddf3d9c077c480ea45874b3b8-lp_solve_5.5.tar.gz
 export LXML_SHA256SUM := 940caef1ec7c78e0c34b0f6b94fe42d0f2022915ffc78643d28538a5cfd0f40e
 export LXML_TARBALL := lxml-4.1.1.tgz
-export MARIADB_CONNECTOR_C_SHA256SUM := fd2f751dea049c1907735eb236aeace1d811d6a8218118b00bbaa9b84dc5cd60
-export MARIADB_CONNECTOR_C_TARBALL := a233181e03d3c307668b4c722d881661-mariadb_client-2.0.0-src.tar.gz
+export MARIADB_CONNECTOR_C_SHA256SUM := 431434d3926f4bcce2e5c97240609983f60d7ff50df5a72083934759bb863f7b
+export MARIADB_CONNECTOR_C_TARBALL := mariadb-connector-c-3.1.8-src.tar.gz
 export MDDS_SHA256SUM := f1585c9cbd12f83a6d43d395ac1ab6a9d9d5d77f062c7b5f704e24ed72dae07d
 export MDDS_TARBALL := mdds-1.6.0.tar.bz2
 export MDNSRESPONDER_SHA256SUM := e777b4d7dbf5eb1552cb80090ad1ede319067ab6e45e3990d68aabf6e8b3f5a0

external/mariadb-connector-c/README
@@ -1,16 +1,8 @@
-Update to new upstream bzr snapshot:
+MariaDB Connector/C
 
-Don't use 'bzr diff', it will not put renames in the diff in a way
-that patch understands.
+https://mariadb.com/kb/en/mariadb-connector-c-release-notes/
+https://downloads.mariadb.com/Connectors/c/
 
-bzr -Ossl.cert_reqs=none branch lp:mariadb-native-client
-mv mariadb-native-client mariadb-native-client.trunk
-cp -R mariadb-native-client.trunk mariadb-native-client.release
-cd mariadb-native-client.release
-bzr revert -r mariadb-native-client-1.0.0
-cd ..
-diff -x .bzr -u --recursive -N mariadb-native-client.release/ mariadb-native-client.trunk/  > /path/to/libreoffice_tree/libmariadb/mariadb-trunk-NNN.patch
-sed -i -e 's@^\([+-]\{3\} \)mariadb-native-client.\(trunk\|release\)/@\1mariadb/@' /path/to/libreoffice_tree/libmariadb/mariadb-trunk-NNN.patch
-dos2unix -f /path/to/libreoffice_tree/libmariadb/mariadb-trunk-NNN.patch
+configs/ generated like this:
 
-regenerate configs
+cmake -DWITH_CURL=OFF -DWITH_SSL=OFF -DWITH_UNIT_TESTS=OFF -DWITH_DYNCOL=OFF .../workdir/UnpackedTarball/mariadb-connector-c/

external/mariadb-connector-c/clang-cl.patch.0
@@ -1,5 +1,5 @@
---- libmariadb/getopt.c
-+++ libmariadb/getopt.c
+--- unittest/libmariadb/getopt.c
++++ unittest/libmariadb/getopt.c
 @@ -38,7 +38,7 @@
  #include <config.h>
  #endif

external/mariadb-connector-c/configs/mac_my_config.h
@@ -1,277 +1,142 @@
 
-#define HAVE_GETHOSTBYNAME_R_GLIBC2_STYLE 1
-
 /*
  * Include file constants (processed in LibmysqlIncludeFiles.txt 1
  */
+/* #undef HAVE_OPENSSL_APPLINK_C */
 #define HAVE_ALLOCA_H 1
-#define HAVE_ARPA_INET_H 1
-/* #undef HAVE_CRYPT_H */
-#define HAVE_DIRENT_H 1
+/* #undef HAVE_BIGENDIAN */
+#define HAVE_SETLOCALE 1
+#define HAVE_NL_LANGINFO 1
 #define HAVE_DLFCN_H 1
-#define HAVE_EXECINFO_H 1
 #define HAVE_FCNTL_H 1
-#define HAVE_FENV_H 1
 #define HAVE_FLOAT_H 1
-/* #undef HAVE_FPU_CONTROL_H */
-#define HAVE_GRP_H 1
-/* #undef HAVE_IEEEFP_H */
 #define HAVE_LIMITS_H 1
-/* #undef HAVE_MALLOC_H */
-#define HAVE_MEMORY_H 1
-#define HAVE_NETINET_IN_H 1
-#define HAVE_PATHS_H 1
 #define HAVE_PWD_H 1
-#define HAVE_SCHED_H 1
 /* #undef HAVE_SELECT_H */
 #define HAVE_STDDEF_H 1
 #define HAVE_STDINT_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRING_H 1
-#define HAVE_STRINGS_H 1
-/* #undef HAVE_SYNCH_H */
-/* #undef HAVE_SYS_FPU_H */
 #define HAVE_SYS_IOCTL_H 1
-#define HAVE_SYS_IPC_H 1
-#define HAVE_SYS_MMAN_H 1
-/* #undef HAVE_SYS_PRCTL_H */
 #define HAVE_SYS_SELECT_H 1
-#define HAVE_SYS_SHM_H 1
 #define HAVE_SYS_SOCKET_H 1
-#define HAVE_SYS_STAT_H 1
 /* #undef HAVE_SYS_STREAM_H */
-#define HAVE_SYS_TIMEB_H 1
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_UN_H 1
-/* #undef HAVE_SYSENT_H */
-/* #undef HAVE_TERMIO_H */
-#define HAVE_TERMIOS_H 1
 #define HAVE_UNISTD_H 1
-#define HAVE_UTIME_H 1
+/* #undef HAVE_UCONTEXT_H */
 
 /*
- * function definitions - processed in LibmysqlFunctions.txt
+ * function definitions - processed in LibmysqlFunctions.txt 
  */
-#define HAVE_ACCESS 1
-/* #undef HAVE_AIOWAIT */
-#define HAVE_ALARM 1
-/* #undef HAVE_ALLOCA */
-#define HAVE_BCMP 1
-/* #undef HAVE_BFILL */
-/* #undef HAVE_BMOVE */
-#define HAVE_BZERO 1
-/* #undef HAVE_CLOCK_GETTIME */
-/* #undef HAVE_COMPRESS */
-#define HAVE_CRYPT 1
+
 #define HAVE_DLERROR 1
 #define HAVE_DLOPEN 1
-#define HAVE_FCHMOD 1
-#define HAVE_FCNTL 1
-/* #undef HAVE_FCONVERT */
-/* #undef HAVE_FDATASYNC */
-#define HAVE_FESETROUND 1
-#define HAVE_FINITE 1
-#define HAVE_FSEEKO 1
-#define HAVE_FSYNC 1
-#define HAVE_GETADDRINFO 1
-#define HAVE_GETCWD 1
-/* #undef HAVE_GETHOSTBYADDR_R */
-/* #undef HAVE_GETHOSTBYNAME_R */
-/* #undef HAVE_GETHRTIME */
-#define HAVE_GETNAMEINFO 1
-#define HAVE_GETPAGESIZE 1
-#define HAVE_GETPASS 1
-/* #undef HAVE_GETPASSPHRASE */
-#define HAVE_GETPWNAM 1
 #define HAVE_GETPWUID 1
-#define HAVE_GETRLIMIT 1
-#define HAVE_GETRUSAGE 1
-#define HAVE_GETWD 1
-#define HAVE_GMTIME_R 1
-#define HAVE_INITGROUPS 1
-#define HAVE_LDIV 1
-#define HAVE_LOCALTIME_R 1
-#define HAVE_LOG2 1
-#define HAVE_LONGJMP 1
-#define HAVE_LSTAT 1
-#define HAVE_MADVISE 1
-/* #undef HAVE_MALLINFO */
-/* #undef HAVE_MEMALIGN */
 #define HAVE_MEMCPY 1
-#define HAVE_MEMMOVE 1
-#define HAVE_MKSTEMP 1
-#define HAVE_MLOCK 1
-#define HAVE_MLOCKALL 1
-#define HAVE_MMAP 1
-/* #undef HAVE_MMAP64 */
-#define HAVE_PERROR 1
 #define HAVE_POLL 1
-#define HAVE_PREAD 1
-/* #undef HAVE_PTHREAD_ATTR_CREATE */
-#define HAVE_PTHREAD_ATTR_GETSTACKSIZE 1
-/* #undef HAVE_PTHREAD_ATTR_SETPRIO */
-#define HAVE_PTHREAD_ATTR_SETSCHEDPARAM 1
-#define HAVE_PTHREAD_ATTR_SETSCOPE 1
-#define HAVE_PTHREAD_ATTR_SETSTACKSIZE 1
-/* #undef HAVE_PTHREAD_CONDATTR_CREATE */
-/* #undef HAVE_PTHREAD_INIT */
-#define HAVE_PTHREAD_KEY_DELETE 1
-#define HAVE_PTHREAD_KILL 1
-#define HAVE_PTHREAD_RWLOCK_RDLOCK 1
-/* #undef HAVE_PTHREAD_SETPRIO_NP */
-#define HAVE_PTHREAD_SETSCHEDPARAM 1
-#define HAVE_PTHREAD_SIGMASK 1
-/* #undef HAVE_PTHREAD_THREADMASK */
-#define HAVE_PTHREAD_YIELD_NP 1
-#define HAVE_READDIR_R 1
-#define HAVE_READLINK 1
-#define HAVE_REALPATH 1
-#define HAVE_RENAME 1
-#define HAVE_SCHED_YIELD 1
-#define HAVE_SELECT 1
-/* #undef HAVE_SETFD */
-/* #undef HAVE_SETFILEPOINTER */
-#define HAVE_SIGNAL 1
-#define HAVE_SIGACTION 1
-/* #undef HAVE_SIGTHREADMASK */
-#define HAVE_SIGWAIT 1
-#define HAVE_SLEEP 1
-#define HAVE_SNPRINTF 1
-#define HAVE_STPCPY 1
-#define HAVE_STRERROR 1
-#define HAVE_STRLCPY 1
-#define HAVE_STRNLEN 1
-#define HAVE_STRPBRK 1
-#define HAVE_STRSEP 1
-#define HAVE_STRSTR 1
-#define HAVE_STRTOK_R 1
-#define HAVE_STRTOL 1
-#define HAVE_STRTOLL 1
-#define HAVE_STRTOUL 1
-#define HAVE_STRTOULL 1
+/* #undef HAVE_STRTOK_R */
+/* #undef HAVE_STRTOL */
+/* #undef HAVE_STRTOLL */
+/* #undef HAVE_STRTOUL */
+/* #undef HAVE_STRTOULL */
 /* #undef HAVE_TELL */
 /* #undef HAVE_THR_SETCONCURRENCY */
 /* #undef HAVE_THR_YIELD */
-#define HAVE_VASPRINTF 1
-#define HAVE_VSNPRINTF 1
+/* #undef HAVE_VASPRINTF */
+/* #undef HAVE_VSNPRINTF */
+/* #undef HAVE_CUSERID */
 
 /*
  * types and sizes
  */
-/* Types we may use */
-#define SIZEOF_CHAR 1
-#ifdef SIZEOF_CHAR
-# define HAVE_CHAR 1
-#endif
 
-#define SIZEOF_CHARP 4
-#ifdef SIZEOF_CHARP
+
+#define SIZEOF_CHARP 8
+#if defined(SIZEOF_CHARP)
 # define HAVE_CHARP 1
 #endif
 
-#define SIZEOF_SHORT 2
-#ifdef SIZEOF_SHORT
-# define HAVE_SHORT 1
-#endif
 
 #define SIZEOF_INT 4
-#ifdef SIZEOF_INT
+#if defined(SIZEOF_INT)
 # define HAVE_INT 1
 #endif
 
-#define SIZEOF_LONG 4
-#ifdef SIZEOF_LONG
+#define SIZEOF_LONG 8
+#if defined(SIZEOF_LONG)
 # define HAVE_LONG 1
 #endif
 
 #define SIZEOF_LONG_LONG 8
-#ifdef SIZEOF_LONG_LONG
+#if defined(SIZEOF_LONG_LONG)
 # define HAVE_LONG_LONG 1
 #endif
 
-#define SIZEOF_OFF_T 8
-#ifdef SIZEOF_OFF_T
-# define HAVE_OFF_T 1
-#endif
 
-#define SIZEOF_SIGSET_T 4
-#ifdef SIZEOF_SIGSET_T
-# define HAVE_SIGSET_T 1
-#endif
-
-#define SIZEOF_SIZE_T 4
-#ifdef SIZEOF_SIZE_T
+#define SIZEOF_SIZE_T 8
+#if defined(SIZEOF_SIZE_T)
 # define HAVE_SIZE_T 1
 #endif
 
-/* #undef SIZEOF_UCHAR */
-#ifdef SIZEOF_UCHAR
-# define HAVE_UCHAR 1
-#endif
 
 #define SIZEOF_UINT 4
-#ifdef SIZEOF_UINT
+#if defined(SIZEOF_UINT)
 # define HAVE_UINT 1
 #endif
 
 /* #undef SIZEOF_ULONG */
-#ifdef SIZEOF_ULONG
+#if defined(SIZEOF_ULONG)
 # define HAVE_ULONG 1
 #endif
 
 /* #undef SIZEOF_INT8 */
-#ifdef SIZEOF_INT8
+#if defined(SIZEOF_INT8)
 # define HAVE_INT8 1
 #endif
 /* #undef SIZEOF_UINT8 */
-#ifdef SIZEOF_UINT8
+#if defined(SIZEOF_UINT8)
 # define HAVE_UINT8 1
 #endif
 
 /* #undef SIZEOF_INT16 */
-#ifdef SIZEOF_INT16
+#if defined(SIZEOF_INT16)
 # define HAVE_INT16 1
 #endif
 /* #undef SIZEOF_UINT16 */
-#ifdef SIZEOF_UINT16
+#if defined(SIZEOF_UINT16)
 # define HAVE_UINT16 1
 #endif
 
 /* #undef SIZEOF_INT32 */
-#ifdef SIZEOF_INT32
+#if defined(SIZEOF_INT32)
 # define HAVE_INT32 1
 #endif
 /* #undef SIZEOF_UINT32 */
-#ifdef SIZEOF_UINT32
+#if defined(SIZEOF_UINT32)
 # define HAVE_UINT32 1
-#endif
-/* #undef SIZEOF_U_INT32_T */
-#ifdef SIZEOF_U_INT32_T
-# define HAVE_U_INT32_T 1
 #endif
 
 /* #undef SIZEOF_INT64 */
-#ifdef SIZEOF_INT64
+#if defined(SIZEOF_INT64)
 # define HAVE_INT64 1
 #endif
 /* #undef SIZEOF_UINT64 */
-#ifdef SIZEOF_UINT64
+#if defined(SIZEOF_UINT64)
 # define HAVE_UINT64 1
 #endif
 
 /* #undef SIZEOF_SOCKLEN_T */
-#ifdef SIZEOF_SOCKLEN_T
+#if defined(SIZEOF_SOCKLEN_T)
 # define HAVE_SOCKLEN_T 1
 #endif
 
 #define SOCKET_SIZE_TYPE socklen_t
 
-#define RETSIGTYPE void
-#define RETQSORTTYPE void
+#define LOCAL_INFILE_MODE_OFF  0
+#define LOCAL_INFILE_MODE_ON   1
+#define LOCAL_INFILE_MODE_AUTO 2
+#define ENABLED_LOCAL_INFILE LOCAL_INFILE_MODE_AUTO
 
-/*
- * various other defines
- */
-/* #undef HAVE_THREADS */
-#define SHAREDIR "share"
-#define DEFAULT_CHARSET_HOME "/usr/local"
+#define MARIADB_DEFAULT_CHARSET "latin1"
+

external/mariadb-connector-c/configs/mariadb_version.h
@@ -1,0 +1,38 @@
+/* Copyright Abandoned 1996, 1999, 2001 MySQL AB
+   This file is public domain and comes with NO WARRANTY of any kind */
+
+/* Version numbers for protocol & mysqld */
+
+#ifndef _mariadb_version_h_
+#define _mariadb_version_h_
+
+#ifdef _CUSTOMCONFIG_
+#include <custom_conf.h>
+#else
+#define PROTOCOL_VERSION		10
+#define MARIADB_CLIENT_VERSION_STR	"10.4.3"
+#define MARIADB_BASE_VERSION		"mariadb-10.4"
+#define MARIADB_VERSION_ID		100403
+#define MARIADB_PORT	        	3306
+#define MARIADB_UNIX_ADDR               "/tmp/mysql.sock"
+
+#define MYSQL_CONFIG_NAME               "my"
+#define MYSQL_VERSION_ID                100403
+#define MYSQL_SERVER_VERSION            "10.4.3-MariaDB"
+
+#define MARIADB_PACKAGE_VERSION "3.1.8"
+#define MARIADB_PACKAGE_VERSION_ID 30108
+#define MARIADB_SYSTEM_TYPE "Linux"
+#define MARIADB_MACHINE_TYPE "x86_64"
+#define MARIADB_PLUGINDIR "/usr/local/lib/mariadb/plugin"
+
+/* mysqld compile time options */
+#ifndef MYSQL_CHARSET
+#define MYSQL_CHARSET			""
+#endif
+#endif
+
+/* Source information */
+#define CC_SOURCE_REVISION ""
+
+#endif /* _mariadb_version_h_ */

commit 2034e832843aa84aa86989ab65c23071f613c1fe

mariadb: the "pvio_socket" plugin turns out to be important
... otherwise can't connect to a TCP socket.

(regression from 96e66941e2e844a08c09a9c5df1deb593f58793a)

Change-Id: I2a1f2968321aae108bfef67f602f06efcf3affd8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96536
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

external/mariadb-connector-c/StaticLibrary_mariadb-connector-c.mk
@@ -64,6 +64,7 @@ $(eval $(call gb_StaticLibrary_add_generated_cobjects,mariadb-connector-c,\
 	UnpackedTarball/mariadb-connector-c/libmariadb/mariadb_stmt \
 	UnpackedTarball/mariadb-connector-c/libmariadb/ma_client_plugin \
 	UnpackedTarball/mariadb-connector-c/plugins/auth/my_auth \
+	UnpackedTarball/mariadb-connector-c/plugins/pvio/pvio_socket \
 	$(if $(filter $(OS),WNT), \
 		UnpackedTarball/mariadb-connector-c/libmariadb/win32_errmsg \
 		UnpackedTarball/mariadb-connector-c/win-iconv/win_iconv) \

external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
@@ -31,7 +31,10 @@ $(eval $(call gb_UnpackedTarball_add_patches,mariadb-connector-c,\
 
 # TODO are any "plugins" needed?
 $(eval $(call gb_UnpackedTarball_set_post_action,mariadb-connector-c, \
-	< libmariadb/ma_client_plugin.c.in sed -e 's/@EXTERNAL_PLUGINS@//' -e 's/@BUILTIN_PLUGINS@//' > libmariadb/ma_client_plugin.c  \
+	< libmariadb/ma_client_plugin.c.in sed \
+		-e 's/@EXTERNAL_PLUGINS@/extern struct st_mysql_client_plugin pvio_socket_client_plugin;/' \
+		-e 's/@BUILTIN_PLUGINS@/(struct st_mysql_client_plugin *)\&pvio_socket_client_plugin$(COMMA)/' \
+		> libmariadb/ma_client_plugin.c  \
 ))
 
 