LemmingAvalanche/libreoffice-repaired

commit 0e5d0221beab66b4786dc331ce3a7f08658d9ce8

tdf#132744 sw: fix subtle difference when checking end of section
In a few places, such as CopyFlyInFlyImpl() and DelFlyInRange(),
the passed start/end positions aren't necessarily from a cursor but can
be section start/end nodes instead.

(regression from aecb5fabbfd505fd291e8e90e7c68f26ea7d21bb)

Change-Id: I1fb24f1f9d027aa3685ac5a7459891cb8c2b9a41
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96124
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/undo/undobj.cxx
@@ -1536,6 +1536,19 @@ static bool IsAtStartOfSection(SwPosition const& rAnchorPos)
     return node == rAnchorPos.nNode && rAnchorPos.nContent == 0;
 }
 
+/// passed start / end position could be on section start / end node
+static bool IsAtEndOfSection2(SwPosition const& rPos)
+{
+    return rPos.nNode.GetNode().IsEndNode()
+        || IsAtEndOfSection(rPos);
+}
+
+static bool IsAtStartOfSection2(SwPosition const& rPos)
+{
+    return rPos.nNode.GetNode().IsStartNode()
+        || IsAtStartOfSection(rPos);
+}
+
 static bool IsNotBackspaceHeuristic(
         SwPosition const& rStart, SwPosition const& rEnd)
 {
@@ -1577,13 +1590,13 @@ bool IsDestroyFrameAnchoredAtChar(SwPosition const & rAnchorPos,
                 && ((rStart.nNode != rEnd.nNode && rStart.nContent == 0
                         // but not if the selection is backspace/delete!
                         && IsNotBackspaceHeuristic(rStart, rEnd))
-                    || (IsAtStartOfSection(rAnchorPos) && IsAtEndOfSection(rEnd)))))
+                    || (IsAtStartOfSection(rAnchorPos) && IsAtEndOfSection2(rEnd)))))
         && ((rAnchorPos < rEnd)
             || (rAnchorPos == rEnd
                 // special case: fully deleted node
                 && ((rEnd.nNode != rStart.nNode && rEnd.nContent == rEnd.nNode.GetNode().GetTextNode()->Len()
                         && IsNotBackspaceHeuristic(rStart, rEnd))
-                    || (IsAtEndOfSection(rAnchorPos) && IsAtStartOfSection(rStart)))));
+                    || (IsAtEndOfSection(rAnchorPos) && IsAtStartOfSection2(rStart)))));
 }
 
 bool IsSelectFrameAnchoredAtPara(SwPosition const & rAnchorPos,

commit 57b45daab206fbc3f04fad22b3587e05567b5473

tdf#134404 sw: another subtlety with start/end of section (at-para flys)
fix this problem by comparing the start/end of the selection instead of
the anchor position, which always has an index of 0:

1. new document with image anchored to paragraph
2. insert one letter
3. press Enter
4. Backspace -> Image gone

(regression from 0e5d0221beab66b4786dc331ce3a7f08658d9ce8)

Change-Id: I1219a9f58ce19ea88d697c90c862fe9005318177
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97511
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/undo/undobj.cxx
@@ -1650,14 +1650,14 @@ bool IsSelectFrameAnchoredAtPara(SwPosition const & rAnchorPos,
                 && ((rStart.nNode != rEnd.nNode && rStart.nContent == 0
                         // but not if the selection is backspace/delete!
                         && IsNotBackspaceHeuristic(rStart, rEnd))
-                    || (IsAtStartOfSection(rAnchorPos) && IsAtEndOfSection2(rEnd)))))
+                    || (IsAtStartOfSection2(rStart) && IsAtEndOfSection2(rEnd)))))
         && ((rAnchorPos.nNode < rEnd.nNode)
             || (rAnchorPos.nNode == rEnd.nNode
                 && !(nDelContentType & DelContentType::ExcludeFlyAtStartEnd)
                 // special case: fully deleted node
                 && ((rEnd.nNode != rStart.nNode && rEnd.nContent == rEnd.nNode.GetNode().GetTextNode()->Len()
                         && IsNotBackspaceHeuristic(rStart, rEnd))
-                    || (IsAtEndOfSection(rAnchorPos) && IsAtStartOfSection2(rStart)))));
+                    || (IsAtEndOfSection2(rEnd) && IsAtStartOfSection2(rStart)))));
 }
 
 bool IsFlySelectedByCursor(SwDoc const & rDoc,