LemmingAvalanche/libreoffice-repaired

commit 996e34f510fa975b80e869486a21d58dbce88d36

RTF import: implement support for tables inside text frames
Change-Id: I6088adad20212cdbcc03b193cc079c51a305602a

writerfilter/source/rtftok/rtfsdrimport.cxx
@@ -166,7 +166,7 @@ void RTFSdrImport::applyProperty(uno::Reference<drawing::XShape> xShape, OUStrin
         xPropertySet->setPropertyValue("VertOrient", uno::makeAny(nVertOrient));
 }
 
-void RTFSdrImport::resolve(RTFShape& rShape)
+void RTFSdrImport::resolve(RTFShape& rShape, bool bClose)
 {
     int nType = -1;
     bool bPib = false;
@@ -204,7 +204,7 @@ void RTFSdrImport::resolve(RTFShape& rShape)
                     break;
                 case ESCHER_ShpInst_Rectangle:
                 case ESCHER_ShpInst_TextBox:
-                    if (!m_rImport.getShapetextBuffer().empty())
+                    if (!bClose)
                     {
                         createShape("com.sun.star.text.TextFrame", xShape, xPropertySet);
                         bTextFrame = true;
@@ -594,8 +594,22 @@ void RTFSdrImport::resolve(RTFShape& rShape)
 
     // Send it to dmapper
     m_rImport.Mapper().startShape(xShape);
-    m_rImport.replayShapetext();
+    if (bClose)
+    {
+        m_rImport.replayShapetext();
+        m_rImport.Mapper().endShape();
+    }
+    m_xShape = xShape;
+}
+
+void RTFSdrImport::close()
+{
     m_rImport.Mapper().endShape();
+}
+
+void RTFSdrImport::append(OUString aKey, OUString aValue)
+{
+    applyProperty(m_xShape, aKey, aValue);
 }
 
 } // namespace rtftok

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1450,10 +1450,11 @@ int RTFDocumentImpl::dispatchDestination(RTFKeyword nKeyword)
         case RTF_SHPTXT:
         case RTF_DPTXBXTEXT:
             m_aStates.top().nDestinationState = DESTINATION_SHAPETEXT;
+            checkFirstRun();
             dispatchFlag(RTF_PARD);
             m_bNeedPap = true;
-            OSL_ENSURE(!m_aShapetextBuffer.size(), "shapetext buffer is not empty");
-            m_aStates.top().pCurrentBuffer = &m_aShapetextBuffer;
+            if (nKeyword == RTF_SHPTXT)
+                m_pSdrImport->resolve(m_aStates.top().aShape, false);
             break;
         case RTF_FORMFIELD:
             if (m_aStates.top().nDestinationState == DESTINATION_FIELDINSTRUCTION)
@@ -2236,7 +2237,7 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
                 // Reset everything.
                 m_aStates.top().aParagraphSprms = m_aDefaultState.aParagraphSprms;
                 m_aStates.top().aParagraphAttributes = m_aDefaultState.aParagraphAttributes;
-                if (m_aStates.top().nDestinationState != DESTINATION_SHAPETEXT)
+                if (m_aStates.top().pCurrentBuffer != &m_aShapetextBuffer)
                     m_aStates.top().pCurrentBuffer = 0;
             }
             else
@@ -2557,6 +2558,9 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
                                 std::vector<beans::PropertyValue> aDefaults = m_pSdrImport->getTextFrameDefaults(false);
                                 for (size_t i = 0; i < aDefaults.size(); ++i)
                                     m_aStates.top().aDrawingObject.aPendingProperties.push_back(aDefaults[i]);
+                                checkFirstRun();
+                                Mapper().startShape(m_aStates.top().aDrawingObject.xShape);
+                                m_aStates.top().aDrawingObject.bHadShapeText = true;
                             }
                             break;
                         default:
@@ -3872,12 +3876,16 @@ int RTFDocumentImpl::popState()
             break;
         case DESTINATION_SHAPEPROPERTYVALUE:
             if (aState.aShape.aProperties.size())
+            {
                 aState.aShape.aProperties.back().second = m_aStates.top().aDestinationText.makeStringAndClear();
+                if (m_aStates.top().bHadShapeText)
+                    m_pSdrImport->append(aState.aShape.aProperties.back().first, aState.aShape.aProperties.back().second);
+            }
             break;
         case DESTINATION_PICPROP:
         case DESTINATION_SHAPEINSTRUCTION:
-            if (!m_bObject && !aState.bInListpicture)
-                m_pSdrImport->resolve(m_aStates.top().aShape);
+            if (!m_bObject && !aState.bInListpicture && !m_aStates.top().bHadShapeText)
+                m_pSdrImport->resolve(m_aStates.top().aShape, true);
             break;
         case DESTINATION_BOOKMARKSTART:
             {
@@ -4136,8 +4144,11 @@ int RTFDocumentImpl::popState()
 
                 m_pSdrImport->resolveFLine(xPropertySet, rDrawing.nFLine);
 
-                Mapper().startShape(xShape);
-                replayShapetext();
+                if (!m_aStates.top().aDrawingObject.bHadShapeText)
+                {
+                    Mapper().startShape(xShape);
+                    replayShapetext();
+                }
                 Mapper().endShape();
             }
             break;
@@ -4581,6 +4592,14 @@ int RTFDocumentImpl::popState()
                     m_xDocumentProperties->setTitle(aState.aDestinationText.makeStringAndClear());
             }
             break;
+        case DESTINATION_SHAPETEXT:
+            // If we're leaving the shapetext group (it may have nested ones) and this is a shape, not an old drawingobject.
+            if (m_aStates.top().nDestinationState != DESTINATION_SHAPETEXT && !m_aStates.top().aDrawingObject.bHadShapeText)
+            {
+                m_aStates.top().bHadShapeText = true;
+                m_pSdrImport->close();
+            }
+            break;
         default:
             {
                 if (m_aStates.size() && m_aStates.top().nDestinationState == DESTINATION_PICT)
@@ -4713,7 +4732,8 @@ RTFParserState::RTFParserState(RTFDocumentImpl *pDocumentImpl)
     pCurrentBuffer(0),
     bHasTableStyle(false),
     bInListpicture(false),
-    bInBackground(false)
+    bInBackground(false),
+    bHadShapeText(false)
 {
 }
 
@@ -4767,7 +4787,8 @@ RTFDrawingObject::RTFDrawingObject()
     bHasFillColor(false),
     nDhgt(0),
     nFLine(-1),
-    nPolyLineCount(0)
+    nPolyLineCount(0),
+    bHadShapeText(false)
 {
 }
 

writerfilter/source/rtftok/rtfdocumentimpl.hxx
@@ -265,6 +265,7 @@ namespace writerfilter {
                 sal_Int32 nFLine;
                 sal_Int32 nPolyLineCount;
                 uno::Sequence<awt::Point> aPolyLinePoints;
+                bool bHadShapeText;
         };
 
         /// Stores the properties of a picture.
@@ -403,6 +404,8 @@ namespace writerfilter {
 
                 /// If we're inside a \background group.
                 bool bInBackground;
+
+                bool bHadShapeText;
         };
 
         class RTFTokenizer;

writerfilter/source/rtftok/rtfsdrimport.hxx
@@ -21,7 +21,9 @@ namespace writerfilter {
                 RTFSdrImport(RTFDocumentImpl& rImport, uno::Reference<lang::XComponent> const& xDstDoc);
                 virtual ~RTFSdrImport();
 
-                void resolve(RTFShape& rShape);
+                void resolve(RTFShape& rShape, bool bClose);
+                void close();
+                void append(OUString aKey, OUString aValue);
                 void resolveDhgt(uno::Reference<beans::XPropertySet> xPropertySet, sal_Int32 nZOrder);
                 void resolveFLine(uno::Reference<beans::XPropertySet> xPropertySet, sal_Int32 nFLine);
                 /**
@@ -36,6 +38,7 @@ namespace writerfilter {
 
                 RTFDocumentImpl& m_rImport;
                 uno::Reference<drawing::XDrawPage> m_xDrawPage;
+                uno::Reference<drawing::XShape> m_xShape;
         };
     } // namespace rtftok
 } // namespace writerfilter

commit c767ccb39d93619a0074ae12dcffdea480e7b528

tdf#134400 RTF import: fix unexpected inner properties for picture-in-textframe
Regression from commit 996e34f510fa975b80e869486a21d58dbce88d36 (RTF
import: implement support for tables inside text frames, 2013-06-16),
the problem was that both the outer "textbox" and the inner "picture
frame" object had a shapeType property, and the properties were stored
in a vector. So by the time RTFSdrImport::initShape() looked up the
shape type for the inner shape, it thought it's not a picture frame,
leading to data loss.

Change-Id: I4a536789371619d1d54afa8c8d41c7d273b0d21b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99111
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/CppunitTest_writerfilter_rtftok.mk
@@ -1,0 +1,49 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,writerfilter_rtftok))
+
+$(eval $(call gb_CppunitTest_use_externals,writerfilter_rtftok,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,writerfilter_rtftok, \
+    writerfilter/qa/cppunittests/rtftok/rtfsdrimport \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,writerfilter_rtftok, \
+    basegfx \
+    comphelper \
+    cppu \
+    oox \
+    sal \
+    test \
+    unotest \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,writerfilter_rtftok))
+
+$(eval $(call gb_CppunitTest_use_ure,writerfilter_rtftok))
+$(eval $(call gb_CppunitTest_use_vcl,writerfilter_rtftok))
+
+$(eval $(call gb_CppunitTest_use_rdb,writerfilter_rtftok,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,writerfilter_rtftok,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,writerfilter_rtftok))
+
+# we need to explicitly depend on library writerfilter because it is not implied
+# by a link relation
+$(call gb_CppunitTest_get_target,writerfilter_rtftok) : $(call gb_Library_get_target,writerfilter)
+
+# vim: set noet sw=4 ts=4:

writerfilter/Module_writerfilter.mk
@@ -18,6 +18,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,writerfilter,\
     CppunitTest_writerfilter_filters_test \
     CppunitTest_writerfilter_misc \
     CppunitTest_writerfilter_dmapper \
+    CppunitTest_writerfilter_rtftok \
 ))
 
 # vim: set noet sw=4 ts=4:

writerfilter/source/rtftok/rtfsdrimport.cxx
@@ -1126,6 +1126,11 @@ void RTFSdrImport::resolve(RTFShape& rShape, bool bClose, ShapeOrPict const shap
             m_rImport.Mapper().endShape();
         }
     }
+
+    // If the shape has an inner shape, the inner object's properties should not be influenced by
+    // the outer one.
+    rShape.getProperties().clear();
+
     m_xShape = xShape;
 }
 

writerfilter/qa/cppunittests/rtftok/data/picture-in-textframe.rtf
@@ -1,0 +1,29 @@
+{\rtf1
+\paperw10080\paperh12960\margl1066\margr1066\margt1642\margb1066
+\pard\plain
+{\shp
+{\*\shpinst\shpleft31\shptop105\shpright4108\shpbottom3887\shpfhdr0\shpwr2\shpwrk3\shpfblwtxt0
+{\sp
+{\sn shapeType}
+{\sv 202}
+}
+{\shptxt \pard\plain
+before
+{\*\shppict
+{\pict
+{\*\picprop\shplid1025
+{\sp
+{\sn shapeType}
+{\sv 75}
+}
+}
+\picscalex23\picscaley24\piccropl0\piccropr0\piccropt0\piccropb0\picw30372\pich22437\picwgoal17219\pichgoal12720\pngblip
+47494638396110001000d5ff00000000ffffffc0c0c0555f00ffffaafcfcfcf6f6f6eaeaeae6e6e6e4e4e4e3e3e3c2c2c2c1c1c1bcbcbcb5b5b5b3b3b3b0b0b0adadada5a5a5a2a2a2a1a1a19f9f9f9494948a8a8a8888888686867b7b7b6c6c6c5c5c5c4e4e4e4b4b4b4747474646463d3d3d3c3c3c2e2e2e2525251b1b1b18181810101009090906060603030300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000021f90401000002002c0000000010001000000684408170482c0a06c8a4728924389f506833b281302a8e6b164b18103024c52111504cca67332102e0042e9a40d9319f8300a343c1200f54e47f7e2a00001e0b0a7d0d728a010d838400261a7c0d94947784252700127e9d159f6c8411140019080ea7a9a85f842122281612b1b3b25d6b1f29291d0fbbbdbc5d5e51c34e4cc64a46c94341003b
+}
+}
+after\par
+}
+}
+}
+\par
+}

writerfilter/qa/cppunittests/rtftok/rtfsdrimport.cxx
@@ -1,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+
+#include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/drawing/XDrawPageSupplier.hpp>
+#include <com/sun/star/text/TextContentAnchorType.hpp>
+
+using namespace ::com::sun::star;
+
+namespace
+{
+/// Tests for writerfilter/source/rtftok/rtfsdrimport.cxx.
+class Test : public test::BootstrapFixture, public unotest::MacrosTest
+{
+private:
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    void setUp() override;
+    void tearDown() override;
+    uno::Reference<lang::XComponent>& getComponent() { return mxComponent; }
+};
+
+void Test::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    mxDesktop.set(frame::Desktop::create(mxComponentContext));
+}
+
+void Test::tearDown()
+{
+    if (mxComponent.is())
+        mxComponent->dispose();
+
+    test::BootstrapFixture::tearDown();
+}
+
+char const DATA_DIRECTORY[] = "/writerfilter/qa/cppunittests/rtftok/data/";
+
+CPPUNIT_TEST_FIXTURE(Test, testPictureInTextframe)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "picture-in-textframe.rtf";
+    getComponent() = loadFromDesktop(aURL);
+    uno::Reference<drawing::XDrawPageSupplier> xTextDocument(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xTextDocument->getDrawPage();
+    uno::Reference<beans::XPropertySet> xInnerShape(xDrawPage->getByIndex(1), uno::UNO_QUERY);
+    text::TextContentAnchorType eAnchorType = text::TextContentAnchorType_AT_PARAGRAPH;
+    xInnerShape->getPropertyValue("AnchorType") >>= eAnchorType;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 4
+    // i.e. the properties of the inner shape (including its anchor type and bitmap fill) were lost
+    // on import.
+    CPPUNIT_ASSERT_EQUAL(text::TextContentAnchorType_AS_CHARACTER, eAnchorType);
+}
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */