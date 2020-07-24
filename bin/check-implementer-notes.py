LemmingAvalanche/libreoffice-repaired

commit 9ed57b7af966ee017a1dff0615653d13820a343b

Don't rely on Python's treatment of unrecognized escape sequences
According to [1]:

> Changed in version 3.6: Unrecognized escape sequences produce a DeprecationWarning.
> In a future Python version they will be a SyntaxWarning and eventually a SyntaxError.

[1] https://docs.python.org/3/reference/lexical_analysis.html#string-and-bytes-literals

Change-Id: Ia4f79f17ccb121f423f35b1e1306d5ae285e8762
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98321
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

bin/check-implementer-notes.py
@@ -11,7 +11,7 @@ wiki_pages = [
 
 # get all commit hashes mentioned in implementer notes
 wiki_commit_hashes = {}
-query = re.compile('\{\{commit\|(\\w+)\|\\w*\|\\w*\}\}', re.IGNORECASE)
+query = re.compile(r'\{\{commit\|(\\w+)\|\\w*\|\\w*\}\}', re.IGNORECASE)
 for page in wiki_pages:
     r = http.request('GET', page)
     data = json.loads(r.data.decode('utf-8'))

bin/find-unused-defines.py
@@ -99,9 +99,9 @@ def in_exclusion_set( a ):
     return False;
 
 # find defines, excluding the externals folder
-a = subprocess.Popen("git grep -hP '^#define\s+\w\w\w\w+\s*' -- \"[!e][!x][!t]*\" | sort -u", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen(r"git grep -hP '^#define\s+\w\w\w\w+\s*' -- \"[!e][!x][!t]*\" | sort -u", stdout=subprocess.PIPE, shell=True)
 
-name_re = re.compile("#define\s+(\w+)")
+name_re = re.compile(r"#define\s+(\w+)")
 with a.stdout as txt:
     for line in txt:
         idName = name_re.match(line).group(1)

bin/find-unused-typedefs.py
@@ -3,7 +3,7 @@
 import subprocess
 
 # find typedefs, excluding the externals folder
-a = subprocess.Popen("git grep -P 'typedef\s+.+\s+\w+;' -- \"[!e][!x][!t]*\"", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen(r"git grep -P 'typedef\s+.+\s+\w+;' -- \"[!e][!x][!t]*\"", stdout=subprocess.PIPE, shell=True)
 
 # parse out the typedef names
 typedefSet = set()

bin/extract-tooltip.py
@@ -16,7 +16,7 @@ def parseFile(filename):
     data = [line.rstrip('\n') for line in data]
 
     pairs = {}
-    regEx = re.compile("^(\S+)\s(\S+)\s(\S+)\s((?:\s*\S*)+)$")
+    regEx = re.compile(r"^(\S+)\s(\S+)\s(\S+)\s((?:\s*\S*)+)$")
     old_line = None
     for line in data:
         if len(line) > 0:

bin/find-duplicated-sids.py
@@ -30,8 +30,8 @@ def extractSidValue(sidValue):
     return rv
 
 
-#a = subprocess.Popen("git grep -P '#define\s+(SID_|SC_|DETECTIVE_|DRAWTEXTBAR_|DRAW_BAR_|RID_|OBJBAR_FORMAT_|TAB_POPUP_|DATA_MENU_|EXTRA_MENU_|FORMAT_MENU_|INSERT_MENU_|VIEW_MENU_|EDIT_MENU_|FILE_MENU_|SC_FUNCTION_|RC_)'", stdout=subprocess.PIPE, shell=True)
-a = subprocess.Popen("git grep -Pn '#define\s+(\S+)' -- *.hrc", stdout=subprocess.PIPE, shell=True)
+#a = subprocess.Popen(r"git grep -P '#define\s+(SID_|SC_|DETECTIVE_|DRAWTEXTBAR_|DRAW_BAR_|RID_|OBJBAR_FORMAT_|TAB_POPUP_|DATA_MENU_|EXTRA_MENU_|FORMAT_MENU_|INSERT_MENU_|VIEW_MENU_|EDIT_MENU_|FILE_MENU_|SC_FUNCTION_|RC_)'", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen(r"git grep -Pn '#define\s+(\S+)' -- *.hrc", stdout=subprocess.PIPE, shell=True)
 
 with a.stdout as txt:
     for line in txt:

bin/find-german-comments
@@ -96,12 +96,12 @@ class Parser:
                 # start of a real multiline comment
                 in_comment = True
                 linenum = count
-                s = re.sub(".*/\*+", "", i.strip(self.strip))
+                s = re.sub(r".*/\*+", "", i.strip(self.strip))
                 if len(s):
                     buf.append(s.strip(self.strip))
             elif in_comment and not "*/" in i:
                 # in multiline comment
-                s = re.sub("^( |\|)*\*?", "", i)
+                s = re.sub(r"^( |\|)*\*?", "", i)
                 if len(s.strip(self.strip)):
                     buf.append(s.strip(self.strip))
             elif "*/" in i and in_comment:
@@ -114,7 +114,7 @@ class Parser:
                 buf = []
             elif "/*" in i and "*/" in i:
                 # c-style oneliner comment
-                yield (count, re.sub(".*/\*(.*)\*/.*", r"\1", i).strip(self.strip))
+                yield (count, re.sub(r".*/\*(.*)\*/.*", r"\1", i).strip(self.strip))
             count += 1
 
     def start_text_cat(self):

bin/find-unused-sid-commands.py
@@ -9,7 +9,7 @@
 import subprocess
 
 # search for entries in .sdi files that declare UNO/SID commands
-a = subprocess.Popen("git grep -P '^\s*\w+Item\s+\w+\s+SID_\w+$' -- *.sdi", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen(r"git grep -P '^\s*\w+Item\s+\w+\s+SID_\w+$' -- *.sdi", stdout=subprocess.PIPE, shell=True)
 
 # parse out the UNO command names
 commandSet = list()

bin/gbuild-to-ide
@@ -94,12 +94,12 @@ class GbuildParser:
         (self.libs, self.exes, self.tests, self.modulenamelist) = ([], [], [], [])
         (self.target_by_path, self.target_by_location) = ({}, {})
 
-    includepattern = re.compile('-I(\S+)')
-    isystempattern = re.compile('-isystem\s*(\S+)')
-    warningpattern = re.compile('-W\S+')
-    libpattern = re.compile('Library_(.*)\.mk')
-    exepattern = re.compile('Executable_(.*)\.mk')
-    testpattern = re.compile('CppunitTest_(.*)\.mk')
+    includepattern = re.compile(r'-I(\S+)')
+    isystempattern = re.compile(r'-isystem\s*(\S+)')
+    warningpattern = re.compile(r'-W\S+')
+    libpattern = re.compile(r'Library_(.*)\.mk')
+    exepattern = re.compile(r'Executable_(.*)\.mk')
+    testpattern = re.compile(r'CppunitTest_(.*)\.mk')
 
     @staticmethod
     def __split_includes(includes):
@@ -1071,7 +1071,7 @@ class VisualStudioIntegrationGenerator(IdeIntegrationGenerator):
         proj_name_node = ET.SubElement(globals_node, '{%s}ProjectName' % ns)
         proj_name_node.text = target.short_name()
 
-        ET.SubElement(proj_node, '{%s}Import' % ns, Project='$(VCTargetsPath)\Microsoft.Cpp.Default.props')
+        ET.SubElement(proj_node, '{%s}Import' % ns, Project='$(VCTargetsPath)\\Microsoft.Cpp.Default.props')
         for configuration in self.configurations:
             conf_node = ET.SubElement(proj_node, '{%s}PropertyGroup' % ns, Label="Configuration",
                                       Condition="'$(Configuration)|$(Platform)'=='%s|%s'" % (configuration, platform))
@@ -1082,14 +1082,14 @@ class VisualStudioIntegrationGenerator(IdeIntegrationGenerator):
             platform_toolset_node = ET.SubElement(conf_node, '{%s}PlatformToolset' % ns)
             platform_toolset_node.text = self.toolset
 
-        ET.SubElement(proj_node, '{%s}Import' % ns, Project='$(VCTargetsPath)\Microsoft.Cpp.props')
+        ET.SubElement(proj_node, '{%s}Import' % ns, Project='$(VCTargetsPath)\\Microsoft.Cpp.props')
         ET.SubElement(proj_node, '{%s}ImportGroup' % ns, Label='ExtensionSettings')
         for configuration in self.configurations:
             prop_sheets_node = ET.SubElement(proj_node, '{%s}ImportGroup' % ns, Label='Configuration',
                                              Condition="'$(Configuration)|$(Platform)'=='%s|%s'" % (configuration, platform))
             ET.SubElement(prop_sheets_node, '{%s}Import' % ns,
-                          Project='$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props',
-                          Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')",
+                          Project='$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props',
+                          Condition="exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')",
                           Label='LocalAppDataPlatform')
 
         ET.SubElement(proj_node, '{%s}PropertyGroup' % ns, Label='UserMacros')
@@ -1156,7 +1156,7 @@ class VisualStudioIntegrationGenerator(IdeIntegrationGenerator):
             hfile = include_abs_path + '.h'
             if os.path.isfile(hfile):
                 ET.SubElement(includes_node, '{%s}ClInclude' % ns, Include=hfile)
-        ET.SubElement(proj_node, '{%s}Import' % ns, Project='$(VCTargetsPath)\Microsoft.Cpp.targets')
+        ET.SubElement(proj_node, '{%s}Import' % ns, Project='$(VCTargetsPath)\\Microsoft.Cpp.targets')
         ET.SubElement(proj_node, '{%s}ImportGroup' % ns, Label='ExtensionTargets')
         self.write_pretty_xml(proj_node, project_path)
         self.write_filters(project_path + '.filters',

commit aa58f98d4f9f78b1443370376e791eb74911b697

Fix escaped sequences in regular expressions
Regression after commit 9ed57b7af966ee017a1dff0615653d13820a343b,
which changed meaning of these.

Thanks Stephan!

Change-Id: I43cb871de32bd01d98e9adbb6c20b4e3c88ce715
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99348
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

bin/check-implementer-notes.py
@@ -11,7 +11,7 @@ wiki_pages = [
 
 # get all commit hashes mentioned in implementer notes
 wiki_commit_hashes = {}
-query = re.compile(r'\{\{commit\|(\\w+)\|\\w*\|\\w*\}\}', re.IGNORECASE)
+query = re.compile(r'\{\{commit\|(\w+)\|\w*\|\w*\}\}', re.IGNORECASE)
 for page in wiki_pages:
     r = http.request('GET', page)
     data = json.loads(r.data.decode('utf-8'))

bin/find-unused-defines.py
@@ -99,7 +99,7 @@ def in_exclusion_set( a ):
     return False;
 
 # find defines, excluding the externals folder
-a = subprocess.Popen(r"git grep -hP '^#define\s+\w\w\w\w+\s*' -- \"[!e][!x][!t]*\" | sort -u", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen("git grep -hP '^#define\\s+\\w\\w\\w\\w+\\s*' -- \"[!e][!x][!t]*\" | sort -u", stdout=subprocess.PIPE, shell=True)
 
 name_re = re.compile(r"#define\s+(\w+)")
 with a.stdout as txt:

bin/find-unused-typedefs.py
@@ -3,7 +3,7 @@
 import subprocess
 
 # find typedefs, excluding the externals folder
-a = subprocess.Popen(r"git grep -P 'typedef\s+.+\s+\w+;' -- \"[!e][!x][!t]*\"", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen("git grep -P 'typedef\\s+.+\\s+\\w+;' -- \"[!e][!x][!t]*\"", stdout=subprocess.PIPE, shell=True)
 
 # parse out the typedef names
 typedefSet = set()