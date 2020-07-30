LemmingAvalanche/libreoffice-repaired

commit 36181bc30669ce576e5bc91d3f27e042de033d81

fdo#66215: sw: fix clicking on text above background fly
SwPageFrm::GetCrsrOfst() tries to compare the distance to the closest
text vs. fly but does not do it right because GetCharRect()
returns just a line of width 1 on the left edge of the character;
try to figure out the entire area covered by the character via 2 calls
to GetCrsrOfst(), which gives much better clickability.

(regression from b2eed0eea05a926d3ab9355b4d1f34541c9ec72d)

Change-Id: I825e86daf65692dfb962ad576772c5f543d02d19

sw/source/core/layout/trvlfrm.cxx
@@ -19,6 +19,7 @@
 
 #include <hintids.hxx>
 #include <hints.hxx>
+#include <comphelper/flagguard.hxx>
 #include <tools/bigint.hxx>
 #include <tools/line.hxx>
 #include <editeng/opaqitem.hxx>
@@ -283,10 +284,48 @@ sal_Bool SwPageFrm::GetCrsrOfst( SwPosition *pPos, Point &rPoint,
             if ( pTextNd )
             {
                 SwCntntFrm* pTextFrm = pTextNd->getLayoutFrm( getRootFrm( ) );
-                SwRect rTextRect;
-                pTextFrm->GetCharRect( rTextRect, aTextPos );
 
-                nTextDistance = lcl_getDistance( rTextRect, rPoint );
+                // try this again but prefer the "previous" position
+                SwCrsrMoveState aMoveState;
+                SwCrsrMoveState *const pState((pCMS) ? pCMS : &aMoveState);
+                comphelper::FlagRestorationGuard g(
+                        pState->bPosMatchesBounds, true);
+                SwPosition prevTextPos(*pPos);
+                SwLayoutFrm::GetCrsrOfst(&prevTextPos, aPoint, pState);
+
+                SwRect aTextRect;
+                pTextFrm->GetCharRect(aTextRect, prevTextPos);
+
+                if (prevTextPos.nContent < pTextNd->Len())
+                {
+                    // aRextRect is just a line on the left edge of the
+                    // previous character; to get a better measure from
+                    // lcl_getDistance, extend that to a rectangle over
+                    // the entire character.
+                    SwPosition const nextTextPos(prevTextPos.nNode,
+                            SwIndex(prevTextPos.nContent, +1));
+                    SwRect nextTextRect;
+                    pTextFrm->GetCharRect(nextTextRect, nextTextPos);
+                    SWRECTFN(pTextFrm);
+                    if ((aTextRect.*fnRect->fnGetTop)() ==
+                        (nextTextRect.*fnRect->fnGetTop)()) // same line?
+                    {
+                        // need to handle mixed RTL/LTR portions somehow
+                        if ((aTextRect.*fnRect->fnGetLeft)() <
+                            (nextTextRect.*fnRect->fnGetLeft)())
+                        {
+                            (aTextRect.*fnRect->fnSetRight)(
+                                    (nextTextRect.*fnRect->fnGetLeft)());
+                        }
+                        else // RTL
+                        {
+                            (aTextRect.*fnRect->fnSetLeft)(
+                                    (nextTextRect.*fnRect->fnGetLeft)());
+                        }
+                    }
+                }
+
+                nTextDistance = lcl_getDistance(aTextRect, rPoint);
                 bValidTextDistance = true;
             }
 

sw/inc/crstate.hxx
@@ -143,7 +143,7 @@ struct SwCrsrMoveState
     sal_Bool bRealWidth;            ///< Calculation of the width required
     sal_Bool b2Lines;               ///< Check 2line portions and fill p2Lines
     sal_Bool bNoScroll;             ///< No scrolling of undersized textframes
-    sal_Bool bPosMatchesBounds;     /**< GetCrsrOfst should not return the next
+    bool bPosMatchesBounds;         /**< GetCrsrOfst should not return the next
                                        position if screen position is inside second
                                        have of bound rect */
 

commit 8cbfb1f7431e7acfd22b6d2f4113b75970e56303

(related: tdf#134252) sw: fix assert when moving mouse pointer
SwPageFrame::GetModelPositionForViewPoint() was calling GetCharRect() on
a frame that doesn't match the passed prevTextPos; it was supposed to be
initialised by GetModelPositionForViewPoint() call but that didn't work
because the point was outside the frame so nothing was inited.

(regression from 36181bc30669ce576e5bc91d3f27e042de033d81)

Change-Id: Ic064c3efc1b7f29d18d713206e5ea5ac0b67fbc8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99692
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/layout/trvlfrm.cxx
@@ -292,42 +292,43 @@ bool SwPageFrame::GetModelPositionForViewPoint( SwPosition *pPos, Point &rPoint,
                 comphelper::FlagRestorationGuard g(
                         pState->m_bPosMatchesBounds, true);
                 SwPosition prevTextPos(*pPos);
-                SwLayoutFrame::GetModelPositionForViewPoint(&prevTextPos, aPoint, pState);
-
-                SwRect aTextRect;
-                pTextFrame->GetCharRect(aTextRect, prevTextPos);
-
-                if (prevTextPos.nContent < pContentNode->Len())
+                if (SwLayoutFrame::GetModelPositionForViewPoint(&prevTextPos, aPoint, pState))
                 {
-                    // aRextRect is just a line on the left edge of the
-                    // previous character; to get a better measure from
-                    // lcl_getDistance, extend that to a rectangle over
-                    // the entire character.
-                    SwPosition const nextTextPos(prevTextPos.nNode,
-                            SwIndex(prevTextPos.nContent, +1));
-                    SwRect nextTextRect;
-                    pTextFrame->GetCharRect(nextTextRect, nextTextPos);
-                    SwRectFnSet aRectFnSet(pTextFrame);
-                    if (aRectFnSet.GetTop(aTextRect) ==
-                        aRectFnSet.GetTop(nextTextRect)) // same line?
+                    SwRect aTextRect;
+                    pTextFrame->GetCharRect(aTextRect, prevTextPos);
+
+                    if (prevTextPos.nContent < pContentNode->Len())
                     {
-                        // need to handle mixed RTL/LTR portions somehow
-                        if (aRectFnSet.GetLeft(aTextRect) <
-                            aRectFnSet.GetLeft(nextTextRect))
+                        // aRextRect is just a line on the left edge of the
+                        // previous character; to get a better measure from
+                        // lcl_getDistance, extend that to a rectangle over
+                        // the entire character.
+                        SwPosition const nextTextPos(prevTextPos.nNode,
+                                SwIndex(prevTextPos.nContent, +1));
+                        SwRect nextTextRect;
+                        pTextFrame->GetCharRect(nextTextRect, nextTextPos);
+                        SwRectFnSet aRectFnSet(pTextFrame);
+                        if (aRectFnSet.GetTop(aTextRect) ==
+                            aRectFnSet.GetTop(nextTextRect)) // same line?
                         {
-                            aRectFnSet.SetRight( aTextRect,
-                                aRectFnSet.GetLeft(nextTextRect));
-                        }
-                        else // RTL
-                        {
-                            aRectFnSet.SetLeft( aTextRect,
-                                aRectFnSet.GetLeft(nextTextRect));
+                            // need to handle mixed RTL/LTR portions somehow
+                            if (aRectFnSet.GetLeft(aTextRect) <
+                                aRectFnSet.GetLeft(nextTextRect))
+                            {
+                                aRectFnSet.SetRight( aTextRect,
+                                    aRectFnSet.GetLeft(nextTextRect));
+                            }
+                            else // RTL
+                            {
+                                aRectFnSet.SetLeft( aTextRect,
+                                    aRectFnSet.GetLeft(nextTextRect));
+                            }
                         }
                     }
-                }
 
-                nTextDistance = lcl_getDistance(aTextRect, rPoint);
-                bValidTextDistance = true;
+                    nTextDistance = lcl_getDistance(aTextRect, rPoint);
+                    bValidTextDistance = true;
+                }
             }
 
             double nBackDistance = 0;