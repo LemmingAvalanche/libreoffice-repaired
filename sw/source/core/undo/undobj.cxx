LemmingAvalanche/libreoffice-repaired

commit b35891b15c4bce22b737b7e18af5cab757f6a0dd

tdf#128739 sw: delete fieldmarks if exactly selected ...
... in SwUndoSaveContent::DelContentIndex().

The problem is that the fieldmark survives but its dummy characters are
all deleted; this will eventually lead to unhappiness in Undo.

Another issue is that SwHistoryBookmark doesn't know about dummy
characters, so use the SwHistory*Fieldmark instead.

This still doesn't work completely because SwUndoDelete::RedoImpl() is
quite borked.

(regression from 082f49573476c04cc567ef0e297a303022d6992a..16de70edc100e6cdad89fe7d38e8707bc4868df5)

Change-Id: Ia98d143fa46e79348fde200be5462cc461455b58
Reviewed-on: https://gerrit.libreoffice.org/82815
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/undo/undobj.cxx
@@ -1063,13 +1063,13 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
         IDocumentMarkAccess* const pMarkAccess = pDoc->getIDocumentMarkAccess();
         if( pMarkAccess->getAllMarksCount() )
         {
-
             for( sal_Int32 n = 0; n < pMarkAccess->getAllMarksCount(); ++n )
             {
                 // #i81002#
                 bool bSavePos = false;
                 bool bSaveOtherPos = false;
-                const ::sw::mark::IMark* pBkmk = pMarkAccess->getAllMarksBegin()[n];
+                const ::sw::mark::IMark *const pBkmk = pMarkAccess->getAllMarksBegin()[n];
+                auto const type(IDocumentMarkAccess::GetType(*pBkmk));
 
                 if( DelContentType::CheckNoCntnt & nDelContentType )
                 {
@@ -1090,8 +1090,8 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
                     // #i92125#
                     // keep cross-reference bookmarks, if content inside one paragraph is deleted.
                     if ( rMark.nNode == rPoint.nNode
-                         && ( IDocumentMarkAccess::GetType(*pBkmk) == IDocumentMarkAccess::MarkType::CROSSREF_HEADING_BOOKMARK
-                              || IDocumentMarkAccess::GetType(*pBkmk) == IDocumentMarkAccess::MarkType::CROSSREF_NUMITEM_BOOKMARK ) )
+                        && (   type == IDocumentMarkAccess::MarkType::CROSSREF_HEADING_BOOKMARK
+                            || type == IDocumentMarkAccess::MarkType::CROSSREF_NUMITEM_BOOKMARK))
                     {
                         continue;
                     }
@@ -1109,7 +1109,11 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
                         *pStt <= pBkmk->GetOtherMarkPos() && pBkmk->GetOtherMarkPos() <= *pEnd )
                     {
                         if ( bSavePos || bSaveOtherPos
-                             || ( pBkmk->GetOtherMarkPos() < *pEnd && pBkmk->GetOtherMarkPos() > *pStt ) )
+                            || (*pStt < pBkmk->GetOtherMarkPos() && pBkmk->GetOtherMarkPos() < *pEnd)
+                            || type == IDocumentMarkAccess::MarkType::TEXT_FIELDMARK
+                            || type == IDocumentMarkAccess::MarkType::CHECKBOX_FIELDMARK
+                            || type == IDocumentMarkAccess::MarkType::DROPDOWN_FIELDMARK
+                            || type == IDocumentMarkAccess::MarkType::DATE_FIELDMARK)
                         {
                             if( bMaybe )
                                 bSavePos = true;
@@ -1145,7 +1149,7 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
                             }
                         }
                     }
-                    else if ( IDocumentMarkAccess::GetType(*pBkmk) == IDocumentMarkAccess::MarkType::ANNOTATIONMARK )
+                    else if (type == IDocumentMarkAccess::MarkType::ANNOTATIONMARK)
                     {
                         // delete annotation marks, if its end position is covered by the deletion
                         const SwPosition& rAnnotationEndPos = pBkmk->GetMarkEnd();
@@ -1159,7 +1163,7 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
 
                 if ( bSavePos || bSaveOtherPos )
                 {
-                    if( IDocumentMarkAccess::GetType(*pBkmk) != IDocumentMarkAccess::MarkType::UNO_BOOKMARK )
+                    if (type != IDocumentMarkAccess::MarkType::UNO_BOOKMARK)
                     {
                         if( !m_pHistory )
                             m_pHistory.reset( new SwHistory );

sw/source/core/undo/rolbck.cxx
@@ -1070,7 +1070,26 @@ void SwHistory::Add(const ::sw::mark::IMark& rBkmk, bool bSavePos, bool bSaveOth
 {
     OSL_ENSURE( !m_nEndDiff, "History was not deleted after REDO" );
 
-    std::unique_ptr<SwHistoryHint> pHt(new SwHistoryBookmark(rBkmk, bSavePos, bSaveOtherPos));
+    std::unique_ptr<SwHistoryHint> pHt;
+
+    switch (IDocumentMarkAccess::GetType(rBkmk))
+    {
+        case IDocumentMarkAccess::MarkType::TEXT_FIELDMARK:
+        case IDocumentMarkAccess::MarkType::DATE_FIELDMARK:
+            assert(bSavePos && bSaveOtherPos); // must be deleted completely!
+            pHt.reset(new SwHistoryTextFieldmark(dynamic_cast<sw::mark::IFieldmark const&>(rBkmk)));
+            break;
+        case IDocumentMarkAccess::MarkType::CHECKBOX_FIELDMARK:
+        case IDocumentMarkAccess::MarkType::DROPDOWN_FIELDMARK:
+            assert(bSavePos && bSaveOtherPos); // must be deleted completely!
+            pHt.reset(new SwHistoryNoTextFieldmark(dynamic_cast<sw::mark::IFieldmark const&>(rBkmk)));
+            break;
+        default:
+            pHt.reset(new SwHistoryBookmark(rBkmk, bSavePos, bSaveOtherPos));
+            break;
+    }
+
+    assert(pHt);
     m_SwpHstry.push_back( std::move(pHt) );
 }
 

commit c8740214e4af473f6c6727d224138d097231c589

sw: avoid assert when deletion starts at fieldmark end
This hit assert(bSavePos && bSaveOtherPos);

Tweak SwUndoSaveContent::DelContentIndex() for fieldmarks to only accept
an equal end position if the start position was in range as well.

(regression from b35891b15c4bce22b737b7e18af5cab757f6a0dd)

Change-Id: If6c9b049193bb7f1bc39ec66d1c965512f9d6ec1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100660
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/undo/undobj.cxx
@@ -1118,12 +1118,14 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
             if( pBkmk->IsExpanded() &&
                 *pStt <= pBkmk->GetOtherMarkPos() && pBkmk->GetOtherMarkPos() <= *pEnd )
             {
-                if ( bSavePos || bSaveOtherPos
+                assert(!bSaveOtherPos);
+                if (   bSavePos
                     || (*pStt < pBkmk->GetOtherMarkPos() && pBkmk->GetOtherMarkPos() < *pEnd)
-                    || type == IDocumentMarkAccess::MarkType::TEXT_FIELDMARK
-                    || type == IDocumentMarkAccess::MarkType::CHECKBOX_FIELDMARK
-                    || type == IDocumentMarkAccess::MarkType::DROPDOWN_FIELDMARK
-                    || type == IDocumentMarkAccess::MarkType::DATE_FIELDMARK)
+                    || (bMaybe
+                        && (   type == IDocumentMarkAccess::MarkType::TEXT_FIELDMARK
+                            || type == IDocumentMarkAccess::MarkType::CHECKBOX_FIELDMARK
+                            || type == IDocumentMarkAccess::MarkType::DROPDOWN_FIELDMARK
+                            || type == IDocumentMarkAccess::MarkType::DATE_FIELDMARK)))
                 {
                     if( bMaybe )
                         bSavePos = true;