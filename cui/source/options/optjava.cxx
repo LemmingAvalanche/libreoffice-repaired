LemmingAvalanche/libreoffice-repaired

commit 87f2469931c0ad49c06f342ce8e3c83f6b61d0d4

weld SvxJavaOptionsPage
Change-Id: Ib37f6f554a890bd6397ae7b6c56f1615cd12b6b1
Reviewed-on: https://gerrit.libreoffice.org/76604
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

cui/source/options/optjava.hxx
@@ -53,19 +53,6 @@ class   OfaTreeOptionsDialog;
 class SvxJavaOptionsPage : public SfxTabPage
 {
 private:
-    VclPtr<CheckBox>                   m_pJavaEnableCB;
-    VclPtr<VclContainer>               m_pJavaBox;
-    VclPtr<SvxJavaListBox>             m_pJavaList;
-    VclPtr<FixedText>                  m_pJavaPathText;
-    VclPtr<PushButton>                 m_pAddBtn;
-    VclPtr<PushButton>                 m_pParameterBtn;
-    VclPtr<PushButton>                 m_pClassPathBtn;
-    VclPtr<PushButton>                 m_pExpertConfigBtn;
-
-    std::unique_ptr<SvxJavaParameterDlg> m_xParamDlg;
-    std::unique_ptr<SvxJavaClassPathDlg> m_xPathDlg;
-    VclPtr<OfaTreeOptionsDialog>       m_pParentDlg;
-
 #if HAVE_FEATURE_JAVA
     std::vector<std::unique_ptr<JavaInfo>> m_parJavaInfo;
     std::vector<OUString>   m_parParameters;
@@ -76,38 +63,54 @@ private:
     OUString                m_sAddDialogText;
     Idle                    m_aResetIdle;
 
-    VclPtr<CheckBox>               m_pExperimentalCB;
-    VclPtr<CheckBox>               m_pMacroCB;
-
-    std::vector<std::unique_ptr<JavaInfo>>
-                            m_aAddedInfos;
+    std::vector<std::unique_ptr<JavaInfo>> m_aAddedInfos;
 
     rtl::Reference< ::svt::DialogClosedListener >           xDialogListener;
     css::uno::Reference< css::ui::dialogs::XFolderPicker2 > xFolderPicker;
 
-    DECL_LINK(        EnableHdl_Impl, Button*, void);
-    DECL_LINK(        CheckHdl_Impl, SvTreeListBox*, void );
-    DECL_LINK(        SelectHdl_Impl, SvTreeListBox*, void);
-    DECL_LINK(        AddHdl_Impl, Button*, void);
-    DECL_LINK(        ParameterHdl_Impl, Button*, void);
-    DECL_LINK(        ClassPathHdl_Impl, Button*, void);
-    DECL_LINK(        ResetHdl_Impl, Timer *, void);
+    std::unique_ptr<weld::CheckButton> m_xJavaEnableCB;
+    std::unique_ptr<weld::TreeView> m_xJavaList;
+    std::unique_ptr<weld::Label> m_xJavaPathText;
+    std::unique_ptr<weld::Button> m_xAddBtn;
+    std::unique_ptr<weld::Button> m_xParameterBtn;
+    std::unique_ptr<weld::Button> m_xClassPathBtn;
+    std::unique_ptr<weld::Button> m_xExpertConfigBtn;
 
-    DECL_LINK(        StartFolderPickerHdl, void *, void );
-    DECL_LINK(        DialogClosedHdl, css::ui::dialogs::DialogClosedEvent*, void );
+    std::unique_ptr<SvxJavaParameterDlg> m_xParamDlg;
+    std::unique_ptr<SvxJavaClassPathDlg> m_xPathDlg;
 
-    DECL_STATIC_LINK(SvxJavaOptionsPage, ExpertConfigHdl_Impl, Button*, void);
+    std::unique_ptr<weld::CheckButton> m_xExperimentalCB;
+    std::unique_ptr<weld::CheckButton> m_xMacroCB;
+
+    std::unique_ptr<weld::Label> m_xAccessibilityText;
+    std::unique_ptr<weld::Label> m_xAddDialogText;
+
+    std::unique_ptr<weld::Widget> m_xJavaFrame;
+
+    DECL_LINK(EnableHdl_Impl, weld::Button&, void);
+    typedef std::pair<int, int> row_col;
+    DECL_LINK(CheckHdl_Impl, const row_col&, void);
+    DECL_LINK(SelectHdl_Impl, weld::TreeView&, void);
+    DECL_LINK(AddHdl_Impl, weld::Button&, void);
+    DECL_LINK(ParameterHdl_Impl, weld::Button&, void);
+    DECL_LINK(ClassPathHdl_Impl, weld::Button&, void);
+    DECL_LINK(ResetHdl_Impl, Timer *, void);
+
+    DECL_LINK(StartFolderPickerHdl, void *, void);
+    DECL_LINK(DialogClosedHdl, css::ui::dialogs::DialogClosedEvent*, void);
+
+    DECL_LINK(ExpertConfigHdl_Impl, weld::Button&, void);
 
     void                    ClearJavaInfo();
     void                    ClearJavaList();
     void                    LoadJREs();
     void                    AddJRE( JavaInfo const * _pInfo );
-    void                    HandleCheckEntry( SvTreeListEntry* _pEntry );
+    void                    HandleCheckEntry(int nCheckedRow);
     void                    AddFolder( const OUString& _rFolder );
     void                    RequestRestart( svtools::RestartReason eReason );
 
 public:
-    SvxJavaOptionsPage( vcl::Window* pParent, const SfxItemSet& rSet );
+    SvxJavaOptionsPage(TabPageParent pParent, const SfxItemSet& rSet);
     virtual ~SvxJavaOptionsPage() override;
     virtual void            dispose() override;
 

cui/uiconfig/ui/optadvancedpage.ui
@@ -1,11 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- Generated with glade 3.19.0 -->
+<!-- Generated with glade 3.22.1 -->
 <interface domain="cui">
   <requires lib="gtk+" version="3.18"/>
-  <requires lib="LibreOffice" version="1.0"/>
+  <object class="GtkTreeStore" id="liststore1">
+    <columns>
+      <!-- column-name check1 -->
+      <column type="gboolean"/>
+      <!-- column-name text -->
+      <column type="gchararray"/>
+      <!-- column-name text2 -->
+      <column type="gchararray"/>
+      <!-- column-name text3 -->
+      <column type="gchararray"/>
+      <!-- column-name id -->
+      <column type="gchararray"/>
+      <!-- column-name checkvis1 -->
+      <column type="gboolean"/>
+      <!-- column-name checktri1 -->
+      <column type="gboolean"/>
+    </columns>
+  </object>
   <object class="GtkBox" id="OptAdvancedPage">
     <property name="visible">True</property>
     <property name="can_focus">False</property>
+    <property name="hexpand">True</property>
+    <property name="vexpand">True</property>
     <property name="border_width">6</property>
     <property name="orientation">vertical</property>
     <property name="spacing">18</property>
@@ -57,13 +76,13 @@
                     <property name="vexpand">True</property>
                     <property name="left_padding">12</property>
                     <child>
-                      <object class="GtkBox" id="javabox">
+                      <object class="GtkGrid" id="javabox">
                         <property name="visible">True</property>
                         <property name="can_focus">False</property>
                         <property name="hexpand">True</property>
                         <property name="vexpand">True</property>
                         <property name="orientation">vertical</property>
-                        <property name="spacing">12</property>
+                        <property name="row_spacing">12</property>
                         <child>
                           <object class="GtkLabel" id="label2">
                             <property name="visible">True</property>
@@ -74,9 +93,8 @@
                             <property name="mnemonic_widget">javas</property>
                           </object>
                           <packing>
-                            <property name="expand">False</property>
-                            <property name="fill">True</property>
-                            <property name="position">0</property>
+                            <property name="left_attach">0</property>
+                            <property name="top_attach">0</property>
                           </packing>
                         </child>
                         <child>
@@ -142,96 +160,94 @@
                               </packing>
                             </child>
                             <child>
-                              <object class="GtkGrid" id="grid2">
+                              <object class="GtkBox" id="box">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                                 <property name="hexpand">True</property>
                                 <property name="vexpand">True</property>
+                                <property name="orientation">vertical</property>
                                 <child>
-                                  <object class="GtkGrid" id="grid3">
-                                    <property name="can_focus">False</property>
-                                    <property name="no_show_all">True</property>
-                                    <property name="column_spacing">6</property>
-                                    <child>
-                                      <object class="GtkLabel" id="vendor">
-                                        <property name="visible">True</property>
-                                        <property name="can_focus">False</property>
-                                        <property name="label" translatable="yes" context="optadvancedpage|vendor">Vendor</property>
-                                        <property name="xalign">0</property>
-                                      </object>
-                                      <packing>
-                                        <property name="left_attach">0</property>
-                                        <property name="top_attach">1</property>
-                                      </packing>
-                                    </child>
-                                    <child>
-                                      <object class="GtkLabel" id="version">
-                                        <property name="visible">True</property>
-                                        <property name="can_focus">False</property>
-                                        <property name="label" translatable="yes" context="optadvancedpage|version">Version</property>
-                                        <property name="xalign">0</property>
-                                      </object>
-                                      <packing>
-                                        <property name="left_attach">1</property>
-                                        <property name="top_attach">1</property>
-                                      </packing>
-                                    </child>
-                                    <child>
-                                      <object class="GtkLabel" id="features">
-                                        <property name="visible">True</property>
-                                        <property name="can_focus">False</property>
-                                        <property name="label" translatable="yes" context="optadvancedpage|features">Features</property>
-                                        <property name="xalign">0</property>
-                                      </object>
-                                      <packing>
-                                        <property name="left_attach">2</property>
-                                        <property name="top_attach">1</property>
-                                      </packing>
-                                    </child>
-                                    <child>
-                                      <object class="GtkLabel" id="a11y">
-                                        <property name="visible">True</property>
-                                        <property name="can_focus">False</property>
-                                        <property name="label" translatable="yes" context="optadvancedpage|a11y">with accessibility support</property>
-                                      </object>
-                                      <packing>
-                                        <property name="left_attach">1</property>
-                                        <property name="top_attach">0</property>
-                                      </packing>
-                                    </child>
-                                    <child>
-                                      <object class="GtkLabel" id="selectruntime">
-                                        <property name="visible">True</property>
-                                        <property name="can_focus">False</property>
-                                        <property name="label" translatable="yes" context="optadvancedpage|selectruntime">Select a Java Runtime Environment</property>
-                                      </object>
-                                      <packing>
-                                        <property name="left_attach">2</property>
-                                        <property name="top_attach">0</property>
-                                      </packing>
-                                    </child>
-                                    <child>
-                                      <placeholder/>
-                                    </child>
-                                  </object>
-                                  <packing>
-                                    <property name="left_attach">0</property>
-                                    <property name="top_attach">0</property>
-                                  </packing>
-                                </child>
-                                <child>
-                                  <object class="svtlo-SvSimpleTableContainer" id="javas">
+                                  <object class="GtkScrolledWindow" id="javasscroll">
                                     <property name="visible">True</property>
-                                    <property name="can_focus">False</property>
+                                    <property name="can_focus">True</property>
                                     <property name="hexpand">True</property>
                                     <property name="vexpand">True</property>
-                                    <child internal-child="selection">
-                                      <object class="GtkTreeSelection" id="Simple Table Container-selection1"/>
+                                    <property name="shadow_type">in</property>
+                                    <child>
+                                      <object class="GtkTreeView" id="javas">
+                                        <property name="visible">True</property>
+                                        <property name="can_focus">True</property>
+                                        <property name="receives_default">True</property>
+                                        <property name="hexpand">True</property>
+                                        <property name="vexpand">True</property>
+                                        <property name="model">liststore1</property>
+                                        <property name="search_column">0</property>
+                                        <property name="show_expanders">False</property>
+                                        <child internal-child="selection">
+                                          <object class="GtkTreeSelection" id="Macro Library List-selection2"/>
+                                        </child>
+                                        <child>
+                                          <object class="GtkTreeViewColumn" id="treeviewcolumn4">
+                                            <property name="resizable">True</property>
+                                            <property name="spacing">6</property>
+                                            <property name="alignment">0.5</property>
+                                            <child>
+                                              <object class="GtkCellRendererToggle" id="cellrenderer5">
+                                                <property name="radio">True</property>
+                                              </object>
+                                              <attributes>
+                                                <attribute name="visible">4</attribute>
+                                                <attribute name="active">0</attribute>
+                                              </attributes>
+                                            </child>
+                                          </object>
+                                        </child>
+                                        <child>
+                                          <object class="GtkTreeViewColumn" id="treeviewcolumn5">
+                                            <property name="resizable">True</property>
+                                            <property name="spacing">6</property>
+                                            <property name="title" translatable="yes" context="optadvancedpage|vendor">Vendor</property>
+                                            <child>
+                                              <object class="GtkCellRendererText" id="cellrenderer4"/>
+                                              <attributes>
+                                                <attribute name="text">1</attribute>
+                                              </attributes>
+                                            </child>
+                                          </object>
+                                        </child>
+                                        <child>
+                                          <object class="GtkTreeViewColumn" id="treeviewcolumn1">
+                                            <property name="resizable">True</property>
+                                            <property name="spacing">6</property>
+                                            <property name="title" translatable="yes" context="optadvancedpage|version">Version</property>
+                                            <child>
+                                              <object class="GtkCellRendererText" id="cellrenderer1"/>
+                                              <attributes>
+                                                <attribute name="text">2</attribute>
+                                              </attributes>
+                                            </child>
+                                          </object>
+                                        </child>
+                                        <child>
+                                          <object class="GtkTreeViewColumn" id="treeviewcolumn2">
+                                            <property name="resizable">True</property>
+                                            <property name="spacing">6</property>
+                                            <property name="title" translatable="yes" context="optadvancedpage|features">Features</property>
+                                            <child>
+                                              <object class="GtkCellRendererText" id="cellrenderer2"/>
+                                              <attributes>
+                                                <attribute name="text">3</attribute>
+                                              </attributes>
+                                            </child>
+                                          </object>
+                                        </child>
+                                      </object>
                                     </child>
                                   </object>
                                   <packing>
-                                    <property name="left_attach">0</property>
-                                    <property name="top_attach">1</property>
+                                    <property name="expand">False</property>
+                                    <property name="fill">True</property>
+                                    <property name="position">0</property>
                                   </packing>
                                 </child>
                               </object>
@@ -242,9 +258,8 @@
                             </child>
                           </object>
                           <packing>
-                            <property name="expand">False</property>
-                            <property name="fill">True</property>
-                            <property name="position">1</property>
+                            <property name="left_attach">0</property>
+                            <property name="top_attach">1</property>
                           </packing>
                         </child>
                         <child>
@@ -255,16 +270,48 @@
                             <property name="xalign">0</property>
                           </object>
                           <packing>
-                            <property name="expand">False</property>
-                            <property name="fill">True</property>
-                            <property name="position">2</property>
+                            <property name="left_attach">0</property>
+                            <property name="top_attach">2</property>
+                          </packing>
+                        </child>
+                        <child>
+                          <object class="GtkGrid" id="grid3">
+                            <property name="can_focus">False</property>
+                            <property name="no_show_all">True</property>
+                            <property name="column_spacing">6</property>
+                            <child>
+                              <object class="GtkLabel" id="a11y">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="label" translatable="yes" context="optadvancedpage|a11y">with accessibility support</property>
+                              </object>
+                              <packing>
+                                <property name="left_attach">0</property>
+                                <property name="top_attach">0</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkLabel" id="selectruntime">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="label" translatable="yes" context="optadvancedpage|selectruntime">Select a Java Runtime Environment</property>
+                              </object>
+                              <packing>
+                                <property name="left_attach">1</property>
+                                <property name="top_attach">0</property>
+                              </packing>
+                            </child>
+                          </object>
+                          <packing>
+                            <property name="left_attach">0</property>
+                            <property name="top_attach">3</property>
                           </packing>
                         </child>
                       </object>
                     </child>
                   </object>
                   <packing>
-                    <property name="expand">False</property>
+                    <property name="expand">True</property>
                     <property name="fill">True</property>
                     <property name="position">1</property>
                   </packing>
@@ -331,8 +378,8 @@
                     <property name="visible">True</property>
                     <property name="can_focus">True</property>
                     <property name="receives_default">False</property>
-                    <property name="use_underline">True</property>
                     <property name="halign">start</property>
+                    <property name="use_underline">True</property>
                     <property name="draw_indicator">True</property>
                   </object>
                   <packing>

commit adea495d0c2444808f48e93cbedfec4a64bd0d66

Always display highlighted JRE's location
...when opening the Advanced options page and after adding a new JRE via the
"Add..." button, not only after highlighting another JRE line.  (I suspect this
broke with 87f2469931c0ad49c06f342ce8e3c83f6b61d0d4 "weld SvxJavaOptionsPage"
towards LO 6.4.)

Change-Id: I5f9b63e2d33a351eeef09712969b703f1e99ef7e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100860
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

cui/source/options/optjava.cxx
@@ -20,6 +20,7 @@
 #include <sal/config.h>
 #include <sal/log.hxx>
 
+#include <cassert>
 #include <memory>
 
 #include <config_java.h>
@@ -145,11 +146,7 @@ IMPL_LINK(SvxJavaOptionsPage, CheckHdl_Impl, const weld::TreeView::iter_col&, rR
 
 IMPL_LINK_NOARG(SvxJavaOptionsPage, SelectHdl_Impl, weld::TreeView&, void)
 {
-    // set installation directory info
-    OUString sLocation = m_xJavaList->get_selected_id();
-    // tdf#80646 insert LTR mark after label
-    OUString sInfo = m_sInstallText + u"\u200E" + sLocation;
-    m_xJavaPathText->set_label(sInfo);
+    UpdateJavaPathText();
 }
 
 IMPL_LINK_NOARG(SvxJavaOptionsPage, AddHdl_Impl, weld::Button&, void)
@@ -337,6 +334,7 @@ void SvxJavaOptionsPage::LoadJREs()
         if ( jfw_areEqualJavaInfo( pCmpInfo.get(), pSelectedJava.get() ) )
         {
             HandleCheckEntry(i);
+            UpdateJavaPathText();
             break;
         }
         ++i;
@@ -373,6 +371,16 @@ void SvxJavaOptionsPage::HandleCheckEntry(int nCheckedRow)
         // we have radio button behavior -> so uncheck the other entries
         m_xJavaList->set_toggle(i, i == nCheckedRow ? TRISTATE_TRUE : TRISTATE_FALSE);
     }
+}
+
+void SvxJavaOptionsPage::UpdateJavaPathText()
+{
+    assert(m_xJavaList->get_selected_index() != -1);
+    // set installation directory info
+    OUString sLocation = m_xJavaList->get_selected_id();
+    // tdf#80646 insert LTR mark after label
+    OUString sInfo = m_sInstallText + u"\u200E" + sLocation;
+    m_xJavaPathText->set_label(sInfo);
 }
 
 void SvxJavaOptionsPage::AddFolder( const OUString& _rFolder )
@@ -417,6 +425,7 @@ void SvxJavaOptionsPage::AddFolder( const OUString& _rFolder )
         }
 
         HandleCheckEntry(nPos);
+        UpdateJavaPathText();
         bStartAgain = false;
     }
     else if ( JFW_E_NOT_RECOGNIZED == eErr )

cui/source/options/optjava.hxx
@@ -98,6 +98,7 @@ private:
     void                    LoadJREs();
     void                    AddJRE( JavaInfo const * _pInfo );
     void                    HandleCheckEntry(int nCheckedRow);
+    void                    UpdateJavaPathText();
     void                    AddFolder( const OUString& _rFolder );
     void                    RequestRestart( svtools::RestartReason eReason );
 