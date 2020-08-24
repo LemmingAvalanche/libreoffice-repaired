LemmingAvalanche/libreoffice-repaired

commit 3f8d229e7418415efcd513de6a9104f0ba42512c

Add missing mpAlphaVDev handling
... a pre-requisite for re-implementing soft edge to use mpAlphaVDev
necessary to keep objects' transparency when applying the effect

Change-Id: I56f5c0f2468c32843abbc1f14e06861a22f6919d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94662
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

vcl/source/outdev/bitmap.cxx
@@ -1051,17 +1051,25 @@ bool OutputDevice::DrawTransformBitmapExDirect(
     const basegfx::B2DPoint aTopY(aFullTransform * basegfx::B2DPoint(0.0, 1.0));
     SalBitmap* pSalSrcBmp = rBitmapEx.GetBitmap().ImplGetSalBitmap().get();
     SalBitmap* pSalAlphaBmp = nullptr;
+    Bitmap aAlphaBitmap;
 
     if(rBitmapEx.IsTransparent())
     {
         if(rBitmapEx.IsAlpha())
         {
-            pSalAlphaBmp = rBitmapEx.GetAlpha().ImplGetSalBitmap().get();
+            aAlphaBitmap = rBitmapEx.GetAlpha();
         }
         else
         {
-            pSalAlphaBmp = rBitmapEx.GetMask().ImplGetSalBitmap().get();
+            aAlphaBitmap = rBitmapEx.GetMask();
         }
+        if (!mpAlphaVDev)
+            pSalAlphaBmp = aAlphaBitmap.ImplGetSalBitmap().get();
+    }
+    else if (mpAlphaVDev)
+    {
+        aAlphaBitmap = Bitmap(rBitmapEx.GetSizePixel(), 1);
+        aAlphaBitmap.Erase(COL_BLACK);
     }
 
     bDone = mpGraphics->DrawTransformedBitmap(
@@ -1071,6 +1079,9 @@ bool OutputDevice::DrawTransformBitmapExDirect(
         *pSalSrcBmp,
         pSalAlphaBmp,
         this);
+
+    if (mpAlphaVDev)
+        mpAlphaVDev->DrawTransformBitmapExDirect(aFullTransform, BitmapEx(aAlphaBitmap));
 
     return bDone;
 };

vcl/source/outdev/polygon.cxx
@@ -338,6 +338,9 @@ void OutputDevice::ImplDrawPolyPolygonWithB2DPolyPolygon(const basegfx::B2DPolyP
 
         if(bSuccess)
         {
+            if (mpAlphaVDev)
+                mpAlphaVDev->ImplDrawPolyPolygonWithB2DPolyPolygon(rB2DPolyPoly);
+
             return;
         }
     }

vcl/source/outdev/polyline.cxx
@@ -342,11 +342,12 @@ bool OutputDevice::DrawPolyLineDirect(
         const basegfx::B2DHomMatrix aTransform(ImplGetDeviceTransformation() * rObjectTransform);
         const bool bPixelSnapHairline((mnAntialiasing & AntialiasingFlags::PixelSnapHairline) && rB2DPolygon.count() < 1000);
 
+        const double fAdjustedTransparency = mpAlphaVDev ? 0 : fTransparency;
         // draw the polyline
         bool bDrawSuccess = mpGraphics->DrawPolyLine(
             aTransform,
             rB2DPolygon,
-            fTransparency,
+            fAdjustedTransparency,
             fLineWidth, // tdf#124848 use LineWidth direct, do not try to solve for zero-case (aka hairline)
             pStroke, // MM01
             eLineJoin,
@@ -370,6 +371,12 @@ bool OutputDevice::DrawPolyLineDirect(
                 const tools::Polygon aToolsPolygon( rB2DPolygon );
                 mpMetaFile->AddAction( new MetaPolyLineAction( aToolsPolygon, aLineInfo ) );
             }
+
+            if (mpAlphaVDev)
+                mpAlphaVDev->DrawPolyLineDirect(rObjectTransform, rB2DPolygon, fLineWidth,
+                                                fTransparency, pStroke, eLineJoin, eLineCap,
+                                                fMiterMinimumAngle, bBypassAACheck);
+
             return true;
         }
     }

vcl/source/outdev/transparent.cxx
@@ -248,6 +248,7 @@ void OutputDevice::DrawTransparent(
 
         // create ObjectToDevice transformation
         const basegfx::B2DHomMatrix aFullTransform(ImplGetDeviceTransformation() * rObjectTransform);
+        const double fAdjustedTransparency = mpAlphaVDev ? 0 : fTransparency;
         bool bDrawnOk(true);
 
         if( IsFillColor() )
@@ -255,7 +256,7 @@ void OutputDevice::DrawTransparent(
             bDrawnOk = mpGraphics->DrawPolyPolygon(
                 aFullTransform,
                 aB2DPolyPolygon,
-                fTransparency,
+                fAdjustedTransparency,
                 this);
         }
 
@@ -268,7 +269,7 @@ void OutputDevice::DrawTransparent(
                 mpGraphics->DrawPolyLine(
                     aFullTransform,
                     rPolygon,
-                    fTransparency,
+                    fAdjustedTransparency,
                     0.0, // tdf#124848 hairline
                     nullptr, // MM01
                     basegfx::B2DLineJoin::NONE,
@@ -290,6 +291,21 @@ void OutputDevice::DrawTransparent(
                     new MetaTransparentAction(
                         tools::PolyPolygon(aB2DPolyPoly),
                         static_cast< sal_uInt16 >(fTransparency * 100.0)));
+            }
+
+            if (mpAlphaVDev)
+            {
+                const Color aFillCol(mpAlphaVDev->GetFillColor());
+                const Color aLineColor(mpAlphaVDev->GetLineColor());
+                const auto nGreyScale = static_cast<sal_uInt8>(std::round(255 * fTransparency));
+                const Color aNewColor(nGreyScale, nGreyScale, nGreyScale);
+                mpAlphaVDev->SetFillColor(aNewColor);
+                mpAlphaVDev->SetLineColor(aNewColor);
+
+                mpAlphaVDev->DrawTransparent(rObjectTransform, rB2DPolyPoly, fTransparency);
+
+                mpAlphaVDev->SetFillColor(aFillCol);
+                mpAlphaVDev->SetLineColor(aLineColor);
             }
 
             return;

commit bc350954a8e8c0cb55ad00e866358144ca30d516

tdf#136047: fix OutputDevice::DrawTransformBitmapExDirect for ...
transparent bitmap case
This has been broken in commit 3f8d229e7418415efcd513de6a9104f0ba42512c,
where transparent bitmap started to override any transparency of vdev
with transparency of its pixels, and not use alpha when drawing RGB when
mpAlphaVDev was present.

Instead, this restores using alpha in RGB output, and blends alpha to
what is already in mpAlphaVDev.

Change-Id: Id82717def4535336450a7118f8bd29a1660c9520
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101254
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

vcl/source/outdev/bitmap.cxx
@@ -1050,7 +1050,6 @@ bool OutputDevice::DrawTransformBitmapExDirect(
     const basegfx::B2DPoint aTopX(aFullTransform * basegfx::B2DPoint(1.0, 0.0));
     const basegfx::B2DPoint aTopY(aFullTransform * basegfx::B2DPoint(0.0, 1.0));
     SalBitmap* pSalSrcBmp = rBitmapEx.GetBitmap().ImplGetSalBitmap().get();
-    SalBitmap* pSalAlphaBmp = nullptr;
     Bitmap aAlphaBitmap;
 
     if(rBitmapEx.IsTransparent())
@@ -1063,14 +1062,14 @@ bool OutputDevice::DrawTransformBitmapExDirect(
         {
             aAlphaBitmap = rBitmapEx.GetMask();
         }
-        if (!mpAlphaVDev)
-            pSalAlphaBmp = aAlphaBitmap.ImplGetSalBitmap().get();
     }
     else if (mpAlphaVDev)
     {
         aAlphaBitmap = Bitmap(rBitmapEx.GetSizePixel(), 1);
         aAlphaBitmap.Erase(COL_BLACK);
     }
+
+    SalBitmap* pSalAlphaBmp = aAlphaBitmap.ImplGetSalBitmap().get();
 
     bDone = mpGraphics->DrawTransformedBitmap(
         aNull,
@@ -1081,7 +1080,12 @@ bool OutputDevice::DrawTransformBitmapExDirect(
         this);
 
     if (mpAlphaVDev)
-        mpAlphaVDev->DrawTransformBitmapExDirect(aFullTransform, BitmapEx(aAlphaBitmap));
+    {
+        // Merge bitmap alpha to alpha device
+        Bitmap aBlack(rBitmapEx.GetSizePixel(), 1);
+        aBlack.Erase(COL_BLACK);
+        mpAlphaVDev->DrawTransformBitmapExDirect(aFullTransform, BitmapEx(aBlack, aAlphaBitmap));
+    }
 
     return bDone;
 };