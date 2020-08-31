LemmingAvalanche/libreoffice-repaired

commit 40e94ca8fcf8a64f5929d894e100ba2dfb2316e2

tdf#96059 Replaced imageproducer with CommandInfoProvider
Replaced all instances of framework::GetImageFromURL with vcl::CommandInfoProvider::Instance().GetImageForCommand

Change-Id: I895a4a80d0219a6523b62d90ca88ec67a433b01c
Reviewed-on: https://gerrit.libreoffice.org/21101
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: jan iversen <jani@documentfoundation.org>

framework/source/uielement/menubarmanager.cxx
@@ -1297,7 +1297,7 @@ void MenuBarManager::FillMenuManager( Menu* pMenu, const Reference< XFrame >& rF
                         if ( bItemShowMenuImages && !pPopup->GetItemImage( ITEMID_ADDONLIST ))
                         {
                             Reference< XFrame > xTemp( rFrame );
-                            Image aImage = GetImageFromURL( xTemp, aItemCommand, false );
+                            Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aItemCommand, false, xTemp );
                             if ( !!aImage )
                                    pPopup->SetItemImage( ITEMID_ADDONLIST, aImage );
                         }
@@ -1326,12 +1326,12 @@ void MenuBarManager::FillMenuManager( Menu* pMenu, const Reference< XFrame >& rF
                     if ( pMenuAttributes && !pMenuAttributes->aImageId.isEmpty() )
                     {
                         // Retrieve image id from menu attributes
-                        aImage = GetImageFromURL( m_xFrame, aImageId, false );
+                        aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aImageId, false, m_xFrame );
                     }
 
                     if ( !aImage )
                     {
-                        aImage = GetImageFromURL( m_xFrame, aItemCommand, false );
+                        aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aItemCommand, false, m_xFrame );
                         if ( !aImage )
                             aImage = AddonsOptions().GetImageFromURL( aItemCommand, false );
                     }

framework/source/classes/menumanager.cxx
@@ -51,6 +51,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/window.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/commandinfoprovider.hxx>
 
 #include <osl/mutex.hxx>
 #include <osl/file.hxx>
@@ -165,7 +166,7 @@ MenuManager::MenuManager(
                         // Set image for the addon popup menu item
                         if ( bShowMenuImages && !pPopupMenu->GetItemImage( ITEMID_ADDONLIST ))
                         {
-                            Image aImage = GetImageFromURL( rFrame, aItemCommand, false );
+                            Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aItemCommand, false, rFrame );
                             if ( !!aImage )
                                    pPopupMenu->SetItemImage( ITEMID_ADDONLIST, aImage );
                         }
@@ -186,7 +187,7 @@ MenuManager::MenuManager(
                 AddMenu(pSubMenu,OUString(),nItemId,true,false);
                 if ( bShowMenuImages && !pMenu->GetItemImage( nItemId ))
                 {
-                    Image aImage = GetImageFromURL( rFrame, aItemCommand, false );
+                    Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aItemCommand, false, rFrame );
                     if ( !!aImage )
                            pMenu->SetItemImage( nItemId, aImage );
                 }
@@ -201,7 +202,7 @@ MenuManager::MenuManager(
 
                 if ( bShowMenuImages && !pMenu->GetItemImage( nItemId ))
                 {
-                    Image aImage = GetImageFromURL( rFrame, aItemCommand, false );
+                    Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aItemCommand, false, rFrame );
                     if ( !!aImage )
                            pMenu->SetItemImage( nItemId, aImage );
                 }
@@ -222,12 +223,12 @@ MenuManager::MenuManager(
                         if ( pMenuAttributes && !pMenuAttributes->aImageId.isEmpty() )
                         {
                             // Retrieve image id from menu attributes
-                            aImage = GetImageFromURL( rFrame, aImageId, false );
+                            aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aImageId, false, rFrame );
                         }
 
                         if ( !aImage )
                         {
-                            aImage = GetImageFromURL( rFrame, aItemCommand, false );
+                            aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aItemCommand, false, rFrame );
                             if ( !aImage )
                                 aImage = AddonsOptions().GetImageFromURL( aItemCommand, false );
                         }
@@ -237,7 +238,7 @@ MenuManager::MenuManager(
                     }
                     else if ( !pMenu->GetItemImage( nItemId ))
                     {
-                        Image aImage = GetImageFromURL( rFrame, aItemCommand, false );
+                        Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aItemCommand, false, rFrame );
                         if ( !!aImage )
                                pMenu->SetItemImage( nItemId, aImage );
                     }
@@ -948,7 +949,7 @@ void MenuManager::FillMenuImages(Reference< XFrame >& _xFrame, Menu* _pMenu,bool
 
                 if ( !aImageId.isEmpty() )
                 {
-                    Image aImage = GetImageFromURL( _xFrame, aImageId, false );
+                    Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aImageId, false, _xFrame );
                     if ( !!aImage )
                     {
                         bImageSet = true;
@@ -959,7 +960,7 @@ void MenuManager::FillMenuImages(Reference< XFrame >& _xFrame, Menu* _pMenu,bool
                 if ( !bImageSet )
                 {
                     OUString aMenuItemCommand = _pMenu->GetItemCommand( nId );
-                    Image aImage = GetImageFromURL( _xFrame, aMenuItemCommand, false );
+                    Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aMenuItemCommand, false, _xFrame );
                     if ( !aImage )
                         aImage = aAddonOptions.GetImageFromURL( aMenuItemCommand, false );
 

framework/source/fwe/classes/bmkmenu.cxx
@@ -34,6 +34,7 @@
 
 #include <vcl/svapp.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/commandinfoprovider.hxx>
 #include <unotools/dynamicmenuoptions.hxx>
 #include <svtools/menuoptions.hxx>
 
@@ -144,7 +145,7 @@ void BmkMenu::Initialize()
 
                 if ( !aImageId.isEmpty() )
                 {
-                    Image aImage = GetImageFromURL( m_xFrame, aImageId, false );
+                    Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aImageId, false, m_xFrame );
                     if ( !!aImage )
                     {
                         bImageSet = true;
@@ -154,7 +155,7 @@ void BmkMenu::Initialize()
 
                 if ( !bImageSet )
                 {
-                    Image aImage = GetImageFromURL( m_xFrame, aURL, false );
+                    Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aURL, false, m_xFrame );
                     if ( !aImage )
                         InsertItem( nId, aTitle );
                     else

framework/source/uielement/addonstoolbarmanager.cxx
@@ -56,6 +56,7 @@
 #include <vcl/taskpanelist.hxx>
 #include <vcl/toolbox.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/commandinfoprovider.hxx>
 
 //  namespaces
 
@@ -121,14 +122,14 @@ static Image RetrieveImage( Reference< css::frame::XFrame >& rFrame,
         if ( !!aImage )
             return aImage;
         else
-            aImage = GetImageFromURL( rFrame, aImageId, bBigImage );
+            aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aImageId, bBigImage, rFrame );
         if ( !!aImage )
             return aImage;
     }
 
     aImage = framework::AddonsOptions().GetImageFromURL( aURL, bBigImage );
     if ( !aImage )
-        aImage = GetImageFromURL( rFrame, aImageId, bBigImage );
+        aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand(aImageId, bBigImage, rFrame );
 
     return aImage;
 }

framework/source/uielement/newmenucontroller.cxx
@@ -37,6 +37,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/i18nhelp.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/commandinfoprovider.hxx>
 #include <rtl/ustrbuf.hxx>
 #include <osl/file.hxx>
 #include <svtools/menuoptions.hxx>
@@ -89,7 +90,7 @@ void NewMenuController::setMenuImages( PopupMenu* pPopupMenu, bool bSetImages )
 
                 if ( !aImageId.isEmpty() )
                 {
-                    aImage = GetImageFromURL( xFrame, aImageId, false );
+                    aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aImageId, false, xFrame );
                     if ( !!aImage )
                     {
                         bImageSet = true;
@@ -101,7 +102,7 @@ void NewMenuController::setMenuImages( PopupMenu* pPopupMenu, bool bSetImages )
                 {
                     OUString aCmd( pPopupMenu->GetItemCommand( nItemId ) );
                     if ( !aCmd.isEmpty() )
-                        aImage = GetImageFromURL( xFrame, aCmd, false );
+                        aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( aCmd, false, xFrame );
 
                     if ( !!aImage )
                         pPopupMenu->SetItemImage( nItemId, aImage );

framework/source/uielement/subtoolbarcontroller.cxx
@@ -27,6 +27,7 @@
 #include <tools/gen.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/toolbox.hxx>
+#include <vcl/commandinfoprovider.hxx>
 
 #include <com/sun/star/awt/XDockableWindow.hpp>
 #include <com/sun/star/frame/XSubToolbarController.hpp>
@@ -291,7 +292,7 @@ void SubToolBarController::updateImage()
         sal_uInt16 nId = 0;
         if ( getToolboxId( nId, &pToolBox ) )
         {
-            Image aImage = framework::GetImageFromURL( getFrameInterface(), m_aLastCommand, pToolBox->GetToolboxButtonSize() == TOOLBOX_BUTTONSIZE_LARGE );
+            Image aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( m_aLastCommand, pToolBox->GetToolboxButtonSize() == TOOLBOX_BUTTONSIZE_LARGE, getFrameInterface() );
             if ( !!aImage )
                 pToolBox->SetItemImage( nId, aImage );
         }

framework/source/uielement/toolbarsmenucontroller.cxx
@@ -48,6 +48,7 @@
 #include <vcl/i18nhelp.hxx>
 #include <vcl/image.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/commandinfoprovider.hxx>
 #include <rtl/ustrbuf.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
 #include <vcl/window.hxx>
@@ -175,7 +176,7 @@ void ToolbarsMenuController::addCommand(
     const StyleSettings& rSettings = Application::GetSettings().GetStyleSettings();
 
     if ( rSettings.GetUseImagesInMenus() )
-        aImage = GetImageFromURL( m_xFrame, rCommandURL, false );
+        aImage = vcl::CommandInfoProvider::Instance().GetImageForCommand( rCommandURL, false, m_xFrame );
 
     VCLXPopupMenu* pPopupMenu = static_cast<VCLXPopupMenu *>(VCLXPopupMenu::GetImplementation( rPopupMenu ));
     if ( pPopupMenu )

commit a23e68c91d63209747e759a3edf4f53a95e31f03

Remove remains of private:image/ via ImageIdentifier addon property
This is broken since commit 40e94ca8fcf8a64f5929d894e100ba2dfb2316e2
("tdf#96059 Replaced imageproducer with CommandInfoProvider") but so
far no one complained (maybe because the usefulness of such internal
images from extensions is questionable at least). Given also that
the whole ImageIdentifier feature (even its still working part) is
obsolete since OOo 2.0.3 (according to the OOo dev guide), and that
the availability of a particular image from an internal hardcoded
image list by a particular numerical id is more an implementation
detail, let's just remove the broken code instead of fixing it.

In the meantime, the code was also copied into the newly introduced
notebookbar addon code, so I handled it there too.

There are also the registry schema and a sdk example that mention this
feature, and need to be adjusted. Interesting that the particular
example used there - private:image/3216 is actually broken since 2011
with commit 65ea6a256c94b80f47ae413d0ed8949b3e9a5ef8
("FDO#42454 - EasyHack: remove code associated with unused icons").

Change-Id: I968b4fb8c5b207654476dd92c57d8db0815520ab
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101529
Tested-by: Jenkins
Reviewed-by: Maxim Monastirsky <momonasmon@gmail.com>

framework/inc/addonmenu.hxx
@@ -74,7 +74,6 @@ class AddonMenuManager
                                         OUString& rTitle,
                                         OUString& rURL,
                                         OUString& rTarget,
-                                        OUString& rImageId,
                                         OUString& rContext,
                                         css::uno::Sequence< css::uno::Sequence< css::beans::PropertyValue > >& rAddonSubMenu );
 };

framework/inc/uielement/toolbarmerger.hxx
@@ -42,7 +42,6 @@ struct AddonToolbarItem
 {
     OUString aCommandURL;
     OUString aLabel;
-    OUString aImageIdentifier;
     OUString aTarget;
     OUString aContext;
     OUString aControlType;
@@ -68,7 +67,6 @@ class ToolBarMerger
         static void       ConvertSequenceToValues( const css::uno::Sequence< css::beans::PropertyValue >& rSequence,
                                                    OUString& rCommandURL,
                                                    OUString& rLabel,
-                                                   OUString& rImageIdentifier,
                                                    OUString& rTarget,
                                                    OUString& rContext,
                                                    OUString& rControlType,

framework/source/fwe/classes/addonmenu.cxx
@@ -144,7 +144,6 @@ void AddonMenuManager::MergeAddonPopupMenus( const Reference< XFrame >& rFrame,
     OUString                              aTitle;
     OUString                              aURL;
     OUString                              aTarget;
-    OUString                              aImageId;
     OUString                              aContext;
     Sequence< Sequence< PropertyValue > > aAddonSubMenu;
     sal_uInt16                            nUniqueMenuId = ADDONMENU_ITEMID_START;
@@ -158,7 +157,6 @@ void AddonMenuManager::MergeAddonPopupMenus( const Reference< XFrame >& rFrame,
                                         aTitle,
                                         aURL,
                                         aTarget,
-                                        aImageId,
                                         aContext,
                                         aAddonSubMenu );
         if ( !aTitle.isEmpty() &&
@@ -202,12 +200,11 @@ void AddonMenuManager::BuildMenu( PopupMenu*                            pCurrent
     OUString aTitle;
     OUString aURL;
     OUString aTarget;
-    OUString aImageId;
     OUString aContext;
 
     for ( i = 0; i < nCount; ++i )
     {
-        GetMenuEntry( aAddonMenuDefinition[i], aTitle, aURL, aTarget, aImageId, aContext, aAddonSubMenu );
+        GetMenuEntry( aAddonMenuDefinition[i], aTitle, aURL, aTarget, aContext, aAddonSubMenu );
 
         if ( !IsCorrectContext( rModuleIdentifier, aContext ) || ( aTitle.isEmpty() && aURL.isEmpty() ))
             continue;
@@ -248,7 +245,7 @@ void AddonMenuManager::BuildMenu( PopupMenu*                            pCurrent
 
             // Store values from configuration to the New and Wizard menu entries to enable
             // sfx2 based code to support high contrast mode correctly!
-            void* nAttributePtr = MenuAttributes::CreateAttribute(aTarget, aImageId);
+            void* nAttributePtr = MenuAttributes::CreateAttribute(aTarget, OUString());
             pCurrentMenu->SetUserValue(nId, nAttributePtr, MenuAttributes::ReleaseAttribute);
             pCurrentMenu->SetItemCommand( nId, aURL );
 
@@ -263,7 +260,6 @@ void AddonMenuManager::GetMenuEntry( const Sequence< PropertyValue >& rAddonMenu
                                      OUString& rTitle,
                                      OUString& rURL,
                                      OUString& rTarget,
-                                     OUString& rImageId,
                                      OUString& rContext,
                                      Sequence< Sequence< PropertyValue > >& rAddonSubMenu )
 {
@@ -279,8 +275,6 @@ void AddonMenuManager::GetMenuEntry( const Sequence< PropertyValue >& rAddonMenu
             rEntry.Value >>= rTitle;
         else if ( aMenuEntryPropName == ADDONSMENUITEM_STRING_TARGET )
             rEntry.Value >>= rTarget;
-        else if ( aMenuEntryPropName == ADDONSMENUITEM_STRING_IMAGEIDENTIFIER )
-            rEntry.Value >>= rImageId;
         else if ( aMenuEntryPropName == ADDONSMENUITEM_STRING_SUBMENU )
             rEntry.Value >>= rAddonSubMenu;
         else if ( aMenuEntryPropName == ADDONSMENUITEM_STRING_CONTEXT )

framework/source/uielement/menubarmanager.cxx
@@ -1726,34 +1726,12 @@ void MenuBarManager::FillMenuImages(Reference< XFrame > const & _xFrame, Menu* _
 
             if ( bTmpShowMenuImages )
             {
-                bool        bImageSet = false;
-                OUString aImageId;
+                OUString aMenuItemCommand = _pMenu->GetItemCommand( nId );
+                Image aImage = vcl::CommandInfoProvider::GetImageForCommand(aMenuItemCommand, _xFrame);
+                if ( !aImage )
+                    aImage = Image(aAddonOptions.GetImageFromURL(aMenuItemCommand, false));
 
-                ::framework::MenuAttributes* pMenuAttributes =
-                    static_cast< ::framework::MenuAttributes*>(_pMenu->GetUserValue( nId ));
-
-                if ( pMenuAttributes )
-                    aImageId = pMenuAttributes->aImageId; // Retrieve image id from menu attributes
-
-                if ( !aImageId.isEmpty() )
-                {
-                    Image aImage = vcl::CommandInfoProvider::GetImageForCommand(aImageId, _xFrame);
-                    if ( !!aImage )
-                    {
-                        bImageSet = true;
-                        _pMenu->SetItemImage( nId, aImage );
-                    }
-                }
-
-                if ( !bImageSet )
-                {
-                    OUString aMenuItemCommand = _pMenu->GetItemCommand( nId );
-                    Image aImage = vcl::CommandInfoProvider::GetImageForCommand(aMenuItemCommand, _xFrame);
-                    if ( !aImage )
-                        aImage = Image(aAddonOptions.GetImageFromURL(aMenuItemCommand, false));
-
-                    _pMenu->SetItemImage( nId, aImage );
-                }
+                _pMenu->SetItemImage( nId, aImage );
             }
             else
                 _pMenu->SetItemImage( nId, Image() );

framework/source/uielement/toolbarmanager.cxx
@@ -1185,13 +1185,12 @@ void ToolBarManager::FillAddonToolbar( const Sequence< Sequence< PropertyValue >
     {
         OUString   aURL;
         OUString   aTitle;
-        OUString   aImageId;
         OUString   aContext;
         OUString   aTarget;
         OUString   aControlType;
         sal_uInt16 nWidth( 0 );
 
-        ToolBarMerger::ConvertSequenceToValues( rSeq, aURL, aTitle, aImageId, aTarget, aContext, aControlType, nWidth );
+        ToolBarMerger::ConvertSequenceToValues( rSeq, aURL, aTitle, aTarget, aContext, aControlType, nWidth );
 
         if ( ToolBarMerger::IsCorrectContext( aContext, m_aModuleIdentifier ) )
         {

framework/source/uielement/toolbarmerger.cxx
@@ -35,7 +35,6 @@ namespace framework
 
 const char MERGE_TOOLBAR_URL[]             = "URL";
 const char MERGE_TOOLBAR_TITLE[]           = "Title";
-const char MERGE_TOOLBAR_IMAGEID[]         = "ImageIdentifier";
 const char MERGE_TOOLBAR_CONTEXT[]         = "Context";
 const char MERGE_TOOLBAR_TARGET[]          = "Target";
 const char MERGE_TOOLBAR_CONTROLTYPE[]     = "ControlType";
@@ -121,7 +120,6 @@ void ToolBarMerger::ConvertSeqSeqToVector(
         ConvertSequenceToValues( rSequence[i],
                                  aAddonToolbarItem.aCommandURL,
                                  aAddonToolbarItem.aLabel,
-                                 aAddonToolbarItem.aImageIdentifier,
                                  aAddonToolbarItem.aTarget,
                                  aAddonToolbarItem.aContext,
                                  aAddonToolbarItem.aControlType,
@@ -152,12 +150,6 @@ void ToolBarMerger::ConvertSeqSeqToVector(
      Name="Title"
 
  @param
-     rImageIdentifier
-
-     Contains the value of the property with
-     Name="ImageIdentifier"
-
- @param
      rTarget
 
      Contains the value of the property with
@@ -184,7 +176,6 @@ void ToolBarMerger::ConvertSequenceToValues(
     const uno::Sequence< beans::PropertyValue >& rSequence,
     OUString& rCommandURL,
     OUString& rLabel,
-    OUString& rImageIdentifier,
     OUString& rTarget,
     OUString& rContext,
     OUString& rControlType,
@@ -196,8 +187,6 @@ void ToolBarMerger::ConvertSequenceToValues(
             prop.Value >>= rCommandURL;
         else if ( prop.Name == MERGE_TOOLBAR_TITLE )
             prop.Value >>= rLabel;
-        else if ( prop.Name == MERGE_TOOLBAR_IMAGEID )
-            prop.Value >>= rImageIdentifier;
         else if ( prop.Name == MERGE_TOOLBAR_CONTEXT )
             prop.Value >>= rContext;
         else if ( prop.Name == MERGE_TOOLBAR_TARGET )

include/vcl/NotebookBarAddonsMerger.hxx
@@ -38,18 +38,10 @@ struct NotebookBarAddonsItem
     std::vector<css::uno::Sequence<css::uno::Sequence<css::beans::PropertyValue>>> aAddonValues;
 };
 
-struct AddonsParams
-{
-    OUString sImageId;
-    OUString sControlType;
-    sal_uInt16 nWidth;
-};
-
 struct AddonNotebookBarItem
 {
     OUString sCommandURL;
     OUString sLabel;
-    OUString sImageIdentifier;
     OUString sTarget;
     OUString sContext;
     OUString sControlType;

odk/examples/DevelopersGuide/Components/Addons/JobsAddon/Addons.xcu
@@ -29,11 +29,6 @@
 
                 </prop>
 
-                <prop oor:name="ImageIdentifier" oor:type="xs:string">
-                         <value>private:image/3216</value>
-
-                </prop>
-
                 <prop oor:name="Title" oor:type="xs:string">
                     <!-- The x-no-translate entry is a marker to prevent l10n tooling from translation. If it is not necessary. -->
                         <value xml:lang="x-no-translate"></value>
@@ -53,11 +48,6 @@
 
                 </prop>
 
-                <prop oor:name="ImageIdentifier" oor:type="xs:string">
-                         <value>private:image/3216</value>
-
-                </prop>
-
                 <prop oor:name="Title" oor:type="xs:string">
                         <!-- The x-no-translate entry is a marker to prevent l10n tooling from translation. If it is not necessary. -->
                         <value xml:lang="x-no-translate"></value>
@@ -74,11 +64,6 @@
                 <prop oor:name="URL" oor:type="xs:string">
 
                    <value>vnd.sun.star.job:service=com.sun.star.comp.framework.java.services.AsyncJob</value>
-
-                </prop>
-
-                <prop oor:name="ImageIdentifier" oor:type="xs:string">
-                         <value>private:image/3216</value>
 
                 </prop>
 

officecfg/registry/schema/org/openoffice/Office/Addons.xcs
@@ -38,7 +38,9 @@
       </prop>
       <prop oor:name="ImageIdentifier" oor:type="xs:string">
         <info>
-          <desc>A private URL used to reference an optional internal menu image. Example: private:image/3216 addresses the internal Office image with id=3216.</desc>
+          <desc>A base name for an image to be used for the given URL. _26.bmp and _16.bmp are
+                appended to that base name for large or small size icons respectively. Has higher
+                priority than images defined in the Images set.</desc>
         </info>
       </prop>
       <prop oor:name="Target" oor:type="xs:string">
@@ -168,7 +170,9 @@
       </prop>
       <prop oor:name="ImageIdentifier" oor:type="xs:string">
         <info>
-          <desc>A private URL used to reference an optional internal menu image. Example: private:image/3216 addresses the internal Office image with id=3216.</desc>
+          <desc>A base name for an image to be used for the given URL. _26.bmp and _16.bmp are
+                appended to that base name for large or small size icons respectively. Has higher
+                priority than images defined in the Images set.</desc>
         </info>
       </prop>
       <prop oor:name="Target" oor:type="xs:string">
@@ -351,7 +355,9 @@
       </prop>
       <prop oor:name="ImageIdentifier" oor:type="xs:string">
         <info>
-          <desc>A private URL used to reference an optional internal menu image. Example: private:image/3216 addresses the internal Office image with id=3216.</desc>
+          <desc>A base name for an image to be used for the given URL. _26.bmp and _16.bmp are
+                appended to that base name for large or small size icons respectively. Has higher
+                priority than images defined in the Images set.</desc>
         </info>
       </prop>
       <prop oor:name="Target" oor:type="xs:string">

pyuno/demo/Addons.xcu
@@ -26,9 +26,6 @@
    <prop oor:name="URL" oor:type="xs:string">
      <value>service:org.openoffice.comp.pyuno.demo.HelloWorld?insert</value>
    </prop>
-   <prop oor:name="ImageIdentifier" oor:type="xs:string">
-    <value>private:image/3216</value>
-   </prop>
    <prop oor:name="Title" oor:type="xs:string">
    <value xml:lang="x-no-translate">Insert Hello World</value>
    <value xml:lang="en-US">Insert Hello World</value>

sfx2/source/notebookbar/SfxNotebookBar.cxx
@@ -41,7 +41,6 @@ using namespace css;
 #define MENUBAR_STR "private:resource/menubar/menubar"
 
 const char MERGE_NOTEBOOKBAR_URL[] = "URL";
-const char MERGE_NOTEBOOKBAR_IMAGEID[] = "ImageIdentifier";
 
 bool SfxNotebookBar::m_bLock = false;
 bool SfxNotebookBar::m_bHide = false;
@@ -65,22 +64,10 @@ static void NotebookbarAddonValues(
             for (const auto& rProp : rExtensionVal)
             {
                 OUString sImage;
-                if (rProp.Name == MERGE_NOTEBOOKBAR_IMAGEID)
+                if (rProp.Name == MERGE_NOTEBOOKBAR_URL)
                 {
                     rProp.Value >>= sImage;
                     aImage = Image(framework::AddonsOptions().GetImageFromURL(sImage, isBigImage));
-                }
-            }
-            if(!aImage)
-            {
-                for (const auto& rProp : rExtensionVal)
-                {
-                    OUString sImage;
-                    if (rProp.Name == MERGE_NOTEBOOKBAR_URL)
-                    {
-                        rProp.Value >>= sImage;
-                        aImage = Image(framework::AddonsOptions().GetImageFromURL(sImage, isBigImage));
-                    }
                 }
             }
             aImageValues.push_back(aImage);

vcl/source/window/NotebookBarAddonsMerger.cxx
@@ -33,7 +33,6 @@ const char STYLE_ICON[] = "Icon";
 
 const char MERGE_NOTEBOOKBAR_URL[] = "URL";
 const char MERGE_NOTEBOOKBAR_TITLE[] = "Title";
-const char MERGE_NOTEBOOKBAR_IMAGEID[] = "ImageIdentifier";
 const char MERGE_NOTEBOOKBAR_CONTEXT[] = "Context";
 const char MERGE_NOTEBOOKBAR_TARGET[] = "Target";
 const char MERGE_NOTEBOOKBAR_CONTROLTYPE[] = "ControlType";
@@ -49,8 +48,6 @@ static void GetAddonNotebookBarItem(const css::uno::Sequence<css::beans::Propert
             i.Value >>= aAddonNotebookBarItem.sCommandURL;
         else if (i.Name == MERGE_NOTEBOOKBAR_TITLE)
             i.Value >>= aAddonNotebookBarItem.sLabel;
-        else if (i.Name == MERGE_NOTEBOOKBAR_IMAGEID)
-            i.Value >>= aAddonNotebookBarItem.sImageIdentifier;
         else if (i.Name == MERGE_NOTEBOOKBAR_CONTEXT)
             i.Value >>= aAddonNotebookBarItem.sContext;
         else if (i.Name == MERGE_NOTEBOOKBAR_TARGET)
@@ -89,8 +86,8 @@ static void CreateNotebookBarToolBox(vcl::Window* pNotebookbarToolBox,
         sImage = aImageVec[nIter];
         if (!sImage)
         {
-            sImage = vcl::CommandInfoProvider::GetImageForCommand(
-                aAddonNotebookBarItem.sImageIdentifier, m_xFrame);
+            sImage = vcl::CommandInfoProvider::GetImageForCommand(aAddonNotebookBarItem.sCommandURL,
+                                                                  m_xFrame);
         }
     }
 