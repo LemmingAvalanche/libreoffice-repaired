LemmingAvalanche/libreoffice-repaired

commit 1ff602d59fb4e4961bfbb89d887c60fb74516858

tdf#132326 sw_redlinehide: no frames allowed in Undo SwNodesArray
This crashes in SwAccessibleParagraph::GetRealHeadingLevel(); the a11y
should have been disposed earlier because the node it's connected to is
in the Undo SwNodesArray.

When SwUndoDelete::SwUndoDelete() calls SwNodes::MoveNodes(), all
existing frames must be destroyed; the MoveNodes() would reset its flag
temporarily inside a section, to delay creating the frames inside the
section when moving to the live SwNodesArray, but this also prevents
the deletion when moving in the other direction.

(crash is regression from c92941613e61624010befde4f8f772d323848a24)

Change-Id: I13719bf98ea96b8d68db3ee78cf7f4b61a99e7e5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96734
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/docnode/nodes.cxx
@@ -661,7 +661,11 @@ bool SwNodes::MoveNodes( const SwNodeRange& aRange, SwNodes & rNodes,
                     {
                         pSctNd->NodesArrChgd();
                         ++nSectNdCnt;
-                        bNewFrames = false;
+                        // tdf#132326 do not let frames survive in undo nodes
+                        if (!GetDoc()->GetIDocumentUndoRedo().IsUndoNodes(rNodes))
+                        {
+                            bNewFrames = false;
+                        }
                     }
                 }
             }

commit f545ffae3af1ccb08507579df1371b046745a05e

tdf#135056 sw_redlinehide: when moving sections to undo nodes-array,
... delete frames with utmost prejudice - the code that was checking
for empty frames or cells was going into infinite loop because
first the *outer* section is moved to the undo nodes array, then when
the *inner* (index header) section is moved, its section node's
m_pStartOfSection points to the section node that is already in
undo nodes array and so SwNodes::GoPrevSection() goes very wrong
and CheckNodesRange() is called with positions in 2 different arrays.

(regression from 1ff602d59fb4e4961bfbb89d887c60fb74516858)

Change-Id: I5682ebc2081ffc7dc49a9c43e613f72c26e93673
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101755
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/inc/node.hxx
@@ -551,7 +551,7 @@ public:
 
     /** Method deletes all views of document for the node. The
      content frames are removed from the respective layout. */
-    void DelFrames(SwRootFrame const* pLayout = nullptr);
+    void DelFrames(SwRootFrame const* pLayout = nullptr, bool bForce = false);
 
     /** Method creates all views of document for the previous node.
        The content frames created are put into the respective layout. */

sw/source/core/docnode/ndsect.cxx
@@ -1159,7 +1159,7 @@ void SwSectionNode::MakeOwnFrames(SwNodeIndex* pIdxBehind, SwNodeIndex* pEndIdx)
     }
 }
 
-void SwSectionNode::DelFrames(SwRootFrame const*const /*FIXME TODO*/)
+void SwSectionNode::DelFrames(SwRootFrame const*const /*FIXME TODO*/, bool const bForce)
 {
     sal_uLong nStt = GetIndex()+1, nEnd = EndOfSectionIndex();
     if( nStt >= nEnd )
@@ -1176,6 +1176,7 @@ void SwSectionNode::DelFrames(SwRootFrame const*const /*FIXME TODO*/)
     // If the Area is within a Fly or TableBox, we can only hide it if
     // there is more Content which has Frames.
     // Or else the Fly/TableBox Frame does not have a Lower!
+    if (!bForce)
     {
         SwNodeIndex aIdx( *this );
         if( !SwNodes::GoPrevSection( &aIdx, true, false ) ||

sw/source/core/docnode/nodes.cxx
@@ -636,7 +636,12 @@ bool SwNodes::MoveNodes( const SwNodeRange& aRange, SwNodes & rNodes,
 
                     SwSectionNode* pSctNd = pSttNd->GetSectionNode();
                     if( bNewFrames && pSctNd )
-                        pSctNd->DelFrames();
+                    {   // tdf#135056 skip over code in DelFrames() that moves
+                        // SwNodeIndex around because in case of nested
+                        // sections, m_pStartOfSection will point between
+                        // undo nodes-array and doc nodes-array
+                        pSctNd->DelFrames(nullptr, true);
+                    }
 
                     RemoveNode( aRg.aEnd.GetIndex(), 1, false ); // delete EndNode
                     sal_uLong nSttPos = pSttNd->GetIndex();