LemmingAvalanche/libreoffice-repaired

commit ef588eb3b8d11a1fec50bfb2689a603010c78c7e

rework treeview initial toggle button col to be like expander col
cause this assumption is baked into the vcl one making it hard
to adapt remaining cases

Change-Id: I75dd5264c65b1ffbf4d26c9a86f6d4d08b400d90
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95622
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

cui/source/options/fontsubs.cxx
@@ -54,14 +54,15 @@ SvxFontSubstTabPage::SvxFontSubstTabPage(weld::Container* pPage, weld::DialogCon
 
     m_xCheckLB->set_size_request(m_xCheckLB->get_approximate_digit_width() * 60,
                                  m_xCheckLB->get_height_rows(8));
+    m_xCheckLB->enable_toggle_buttons(weld::ColumnToggleType::Check);
     m_xCheckLB->set_help_id(HID_OFA_FONT_SUBST_CLB);
     m_xCheckLB->set_selection_mode(SelectionMode::Multiple);
 
     setColSizes(m_xCheckLB->get_size_request());
     m_xCheckLB->connect_size_allocate(LINK(this, SvxFontSubstTabPage, ResizeHdl));
 
+    m_xCheckLB->set_centered_column(0);
     m_xCheckLB->set_centered_column(1);
-    m_xCheckLB->set_centered_column(2);
 
     Link<weld::ComboBox&,void> aLink2(LINK(this, SvxFontSubstTabPage, SelectComboBoxHdl));
     Link<weld::Button&,void> aClickLink(LINK(this, SvxFontSubstTabPage, ClickHdl));
@@ -114,15 +115,14 @@ IMPL_LINK(SvxFontSubstTabPage, HeaderBarClick, int, nColumn, void)
 
 void SvxFontSubstTabPage::setColSizes(const Size& rSize)
 {
-    int nW1 = m_xCheckLB->get_pixel_size(m_xCheckLB->get_column_title(3)).Width();
-    int nW2 = m_xCheckLB->get_pixel_size(m_xCheckLB->get_column_title(4)).Width();
+    int nW1 = m_xCheckLB->get_pixel_size(m_xCheckLB->get_column_title(2)).Width();
+    int nW2 = m_xCheckLB->get_pixel_size(m_xCheckLB->get_column_title(3)).Width();
     int nMax = std::max( nW1, nW2 ) + 6; // width of the longest header + a little offset
     int nMin = m_xCheckLB->get_checkbox_column_width();
     nMax = std::max(nMax, nMin);
     const int nDoubleMax = 2*nMax;
     const int nRest = rSize.Width() - nDoubleMax;
     std::vector<int> aWidths;
-    aWidths.push_back(1);   // just abandon the built-in column for checkbuttons and use another
     aWidths.push_back(nMax);
     aWidths.push_back(nMax);
     aWidths.push_back(nRest/2);
@@ -152,10 +152,10 @@ bool  SvxFontSubstTabPage::FillItemSet( SfxItemSet* )
 
     m_xCheckLB->all_foreach([this](weld::TreeIter& rIter) {
         SubstitutionStruct aAdd;
-        aAdd.sFont = m_xCheckLB->get_text(rIter, 3);
-        aAdd.sReplaceBy = m_xCheckLB->get_text(rIter, 4);
-        aAdd.bReplaceAlways = m_xCheckLB->get_toggle(rIter, 1);
-        aAdd.bReplaceOnScreenOnly = m_xCheckLB->get_toggle(rIter, 2);
+        aAdd.sFont = m_xCheckLB->get_text(rIter, 2);
+        aAdd.sReplaceBy = m_xCheckLB->get_text(rIter, 3);
+        aAdd.bReplaceAlways = m_xCheckLB->get_toggle(rIter, 0);
+        aAdd.bReplaceOnScreenOnly = m_xCheckLB->get_toggle(rIter, 1);
         m_xConfig->AddSubstitution(aAdd);
         return false;
     });
@@ -215,17 +215,17 @@ void  SvxFontSubstTabPage::Reset( const SfxItemSet* )
     {
         m_xCheckLB->append(xIter.get());
         const SubstitutionStruct* pSubs = m_xConfig->GetSubstitution(i);
-        m_xCheckLB->set_toggle(*xIter, pSubs->bReplaceAlways ? TRISTATE_TRUE : TRISTATE_FALSE, 1);
-        m_xCheckLB->set_toggle(*xIter, pSubs->bReplaceOnScreenOnly ? TRISTATE_TRUE : TRISTATE_FALSE, 2);
-        m_xCheckLB->set_text(*xIter, pSubs->sFont, 3);
-        m_xCheckLB->set_text(*xIter, pSubs->sReplaceBy, 4);
+        m_xCheckLB->set_toggle(*xIter, pSubs->bReplaceAlways ? TRISTATE_TRUE : TRISTATE_FALSE, 0);
+        m_xCheckLB->set_toggle(*xIter, pSubs->bReplaceOnScreenOnly ? TRISTATE_TRUE : TRISTATE_FALSE, 1);
+        m_xCheckLB->set_text(*xIter, pSubs->sFont, 2);
+        m_xCheckLB->set_text(*xIter, pSubs->sReplaceBy, 3);
     }
 
     m_xCheckLB->thaw();
 
     m_xCheckLB->make_sorted();
-    m_xCheckLB->set_sort_column(3);
-    m_xCheckLB->set_sort_indicator(TRISTATE_TRUE, 3);
+    m_xCheckLB->set_sort_column(2);
+    m_xCheckLB->set_sort_indicator(TRISTATE_TRUE, 2);
 
     CheckEnable();
 
@@ -297,7 +297,7 @@ void SvxFontSubstTabPage::SelectHdl(const weld::Widget* pWin)
             if (nPos != -1)
             {
                 // change entry
-                m_xCheckLB->set_text(nPos, m_xFont2CB->get_active_text(), 4);
+                m_xCheckLB->set_text(nPos, m_xFont2CB->get_active_text(), 3);
                 m_xCheckLB->select(nPos);
             }
             else
@@ -308,10 +308,10 @@ void SvxFontSubstTabPage::SelectHdl(const weld::Widget* pWin)
 
                 std::unique_ptr<weld::TreeIter> xIter(m_xCheckLB->make_iterator());
                 m_xCheckLB->append(xIter.get());
+                m_xCheckLB->set_toggle(*xIter, TRISTATE_FALSE, 0);
                 m_xCheckLB->set_toggle(*xIter, TRISTATE_FALSE, 1);
-                m_xCheckLB->set_toggle(*xIter, TRISTATE_FALSE, 2);
-                m_xCheckLB->set_text(*xIter, sFont1, 3);
-                m_xCheckLB->set_text(*xIter, sFont2, 4);
+                m_xCheckLB->set_text(*xIter, sFont1, 2);
+                m_xCheckLB->set_text(*xIter, sFont2, 3);
                 m_xCheckLB->select(*xIter);
             }
         }

basctl/source/basicide/moduldl2.cxx
@@ -648,8 +648,8 @@ void LibPage::InsertLib()
             weld::TreeView& rView = xLibDlg->GetLibBox();
             rView.append();
             const int nRow = rView.n_children() - 1;
-            rView.set_toggle(nRow, TRISTATE_TRUE, 0);
-            rView.set_text(nRow, aLibName, 1);
+            rView.set_toggle(nRow, TRISTATE_TRUE);
+            rView.set_text(nRow, aLibName, 0);
             rView.set_cursor(rView.find_text(aLibName));
         }
     }
@@ -682,9 +682,9 @@ void LibPage::InsertLib()
             weld::TreeView& rView = xLibDlg->GetLibBox();
             for (int nLib = 0, nChildren = rView.n_children(); nLib < nChildren; ++nLib)
             {
-                if (rView.get_toggle(nLib, 0) == TRISTATE_TRUE)
+                if (rView.get_toggle(nLib) == TRISTATE_TRUE)
                 {
-                    OUString aLibName(rView.get_text(nLib, 1));
+                    OUString aLibName(rView.get_text(nLib));
                     Reference< script::XLibraryContainer2 > xModLibContainer( m_aCurDocument.getLibraryContainer( E_SCRIPTS ), UNO_QUERY );
                     Reference< script::XLibraryContainer2 > xDlgLibContainer( m_aCurDocument.getLibraryContainer( E_DIALOGS ), UNO_QUERY );
 

basctl/source/basicide/moduldlg.cxx
@@ -897,6 +897,7 @@ LibDialog::LibDialog(weld::Window* pParent)
 {
     m_xLibBox->set_size_request(m_xLibBox->get_approximate_digit_width() * 28,
                                 m_xLibBox->get_height_rows(8));
+    m_xLibBox->enable_toggle_buttons(weld::ColumnToggleType::Check);
     // tdf#93476 The libraries should be listed alphabetically
     m_xLibBox->make_sorted();
 }

basctl/source/dlged/managelang.cxx
@@ -226,9 +226,7 @@ SetDefaultLanguageDialog::SetDefaultLanguageDialog(weld::Window* pParent, std::s
 {
     m_xLanguageLB->set_size_request(-1, m_xLanguageLB->get_height_rows(10));
     m_xCheckLangLB->set_size_request(-1, m_xCheckLangLB->get_height_rows(10));
-    std::vector<int> aWidths;
-    aWidths.push_back(m_xCheckLangLB->get_checkbox_column_width());
-    m_xCheckLangLB->set_column_fixed_widths(aWidths);
+    m_xCheckLangLB->enable_toggle_buttons(weld::ColumnToggleType::Check);
 
     if (m_xLocalizationMgr->isLibraryLocalized())
     {
@@ -270,8 +268,8 @@ void SetDefaultLanguageDialog::FillLanguageBox()
             LanguageType eLang = m_xLanguageCB->get_id(j);
             m_xCheckLangLB->append();
             const int nRow = m_xCheckLangLB->n_children() - 1;
-            m_xCheckLangLB->set_toggle(nRow, TRISTATE_FALSE, 0);
-            m_xCheckLangLB->set_text(nRow, m_xLanguageCB->get_text(j), 1);
+            m_xCheckLangLB->set_toggle(nRow, TRISTATE_FALSE);
+            m_xCheckLangLB->set_text(nRow, m_xLanguageCB->get_text(j), 0);
             m_xCheckLangLB->set_id(nRow, OUString::number(eLang.get()));
         }
         m_xLanguageCB.reset();
@@ -304,7 +302,7 @@ Sequence< Locale > SetDefaultLanguageDialog::GetLocales() const
     const sal_Int32 nCount = m_xCheckLangLB->n_children();
     for (sal_Int32 i = 0; i < nCount; ++i)
     {
-        if (m_xCheckLangLB->get_toggle(i, 0) == TRISTATE_TRUE)
+        if (m_xCheckLangLB->get_toggle(i) == TRISTATE_TRUE)
         {
             LanguageType eType(m_xCheckLangLB->get_id(i).toUInt32());
             aLocaleSeq.push_back(LanguageTag::convertToLocale(eType));

cui/source/customize/SvxMenuConfigPage.cxx
@@ -115,22 +115,10 @@ IMPL_LINK(SvxMenuConfigPage, MenuEntriesSizeAllocHdl, const Size&, rSize, void)
     weld::TreeView& rTreeView = m_xContentsListBox->get_widget();
     std::vector<int> aWidths;
 
-    int nExpectedSize = 16;
-
     int nStandardImageColWidth = rTreeView.get_checkbox_column_width();
-    int nMargin = nStandardImageColWidth - nExpectedSize;
-    if (nMargin < 16)
-        nMargin = 16;
+    int nMargin = 16;
 
-    if (SvxConfigPageHelper::GetImageType() & css::ui::ImageType::SIZE_LARGE)
-        nExpectedSize = 24;
-    else if (SvxConfigPageHelper::GetImageType() & css::ui::ImageType::SIZE_32)
-        nExpectedSize = 32;
-
-    int nImageColWidth = nExpectedSize + nMargin;
-
-    aWidths.push_back(nImageColWidth);
-    aWidths.push_back(rSize.Width() - (nImageColWidth + nStandardImageColWidth));
+    aWidths.push_back(rSize.Width() - (nMargin + nStandardImageColWidth));
     rTreeView.set_column_fixed_widths(aWidths);
 }
 
@@ -279,7 +267,7 @@ void SvxMenuConfigPage::SelectElement()
             auto const& entry = (*pEntries)[nIdx];
             OUString sId(OUString::number(reinterpret_cast<sal_Int64>(entry)));
             rTreeView.set_id(rIter, sId);
-            InsertEntryIntoUI(entry, rTreeView, rIter, 0);
+            InsertEntryIntoUI(entry, rTreeView, rIter, true);
         });
     }
 
@@ -369,7 +357,7 @@ IMPL_LINK_NOARG( SvxMenuConfigPage, AddCommandHdl, weld::Button&, void )
     weld::TreeView& rTreeView = m_xContentsListBox->get_widget();
     SvxConfigEntry* pEntry =
         reinterpret_cast<SvxConfigEntry*>(rTreeView.get_id(nPos).toInt64());
-    InsertEntryIntoUI(pEntry, rTreeView, nPos, 0);
+    InsertEntryIntoUI(pEntry, rTreeView, nPos, true);
 }
 
 IMPL_LINK_NOARG( SvxMenuConfigPage, RemoveCommandHdl, weld::Button&, void )
@@ -389,7 +377,7 @@ IMPL_LINK(SvxMenuConfigPage, InsertHdl, const OString&, rIdent, void)
         SvxConfigEntry* pNewEntryData = new SvxConfigEntry;
         pNewEntryData->SetUserDefined();
         int nPos = AppendEntry(pNewEntryData, -1);
-        InsertEntryIntoUI(pNewEntryData, rTreeView, nPos, 0);
+        InsertEntryIntoUI(pNewEntryData, rTreeView, nPos, true);
     }
     else if (rIdent == "insertsubmenu")
     {
@@ -410,7 +398,7 @@ IMPL_LINK(SvxMenuConfigPage, InsertHdl, const OString&, rIdent, void)
             pNewEntryData->SetUserDefined();
 
             int nPos = AppendEntry(pNewEntryData, -1);
-            InsertEntryIntoUI(pNewEntryData, rTreeView, nPos, 0);
+            InsertEntryIntoUI(pNewEntryData, rTreeView, nPos, true);
 
             ReloadTopLevelListBox();
 

cui/source/customize/SvxNotebookbarConfigPage.cxx
@@ -124,28 +124,9 @@ SvxNotebookbarConfigPage::SvxNotebookbarConfigPage(weld::Container* pPage,
         new SvxNotebookbarEntriesListBox(m_xBuilder->weld_tree_view("toolcontents"), this));
     m_xDropTargetHelper.reset(
         new SvxConfigPageFunctionDropTarget(*this, m_xContentsListBox->get_widget()));
-    std::vector<int> aWidths;
     weld::TreeView& rTreeView = m_xContentsListBox->get_widget();
     Size aSize(m_xFunctions->get_size_request());
     rTreeView.set_size_request(aSize.Width(), aSize.Height());
-
-    int nExpectedSize = 16;
-
-    int nStandardImageColWidth = rTreeView.get_checkbox_column_width();
-    int nMargin = nStandardImageColWidth - nExpectedSize;
-    if (nMargin < 16)
-        nMargin = 16;
-
-    if (SvxConfigPageHelper::GetImageType() & css::ui::ImageType::SIZE_LARGE)
-        nExpectedSize = 24;
-    else if (SvxConfigPageHelper::GetImageType() & css::ui::ImageType::SIZE_32)
-        nExpectedSize = 32;
-
-    int nImageColWidth = nExpectedSize + nMargin;
-
-    aWidths.push_back(nStandardImageColWidth);
-    aWidths.push_back(nImageColWidth);
-    rTreeView.set_column_fixed_widths(aWidths);
 
     rTreeView.set_hexpand(true);
     rTreeView.set_vexpand(true);
@@ -219,7 +200,7 @@ void SvxConfigPage::InsertEntryIntoNotebookbarTabUI(const OUString& sClassId,
                                                     const OUString& sUIItemId,
                                                     const OUString& sUIItemCommand,
                                                     weld::TreeView& rTreeView,
-                                                    weld::TreeIter& rIter, int nStartCol)
+                                                    weld::TreeIter& rIter)
 {
     css::uno::Reference<css::container::XNameAccess> m_xCommandToLabelMap;
     uno::Reference<uno::XComponentContext> xContext = ::comphelper::getProcessComponentContext();
@@ -249,7 +230,7 @@ void SvxConfigPage::InsertEntryIntoNotebookbarTabUI(const OUString& sClassId,
 
     if (sClassId == "GtkSeparatorMenuItem" || sClassId == "GtkSeparator")
     {
-        rTreeView.set_text(rIter, "--------------------------------------------", nStartCol + 1);
+        rTreeView.set_text(rIter, "--------------------------------------------", 0);
     }
     else
     {
@@ -257,8 +238,8 @@ void SvxConfigPage::InsertEntryIntoNotebookbarTabUI(const OUString& sClassId,
             aName = sUIItemId;
         auto xImage = GetSaveInData()->GetImage(sUIItemCommand);
         if (xImage.is())
-            rTreeView.set_image(rIter, xImage, nStartCol);
-        rTreeView.set_text(rIter, aName, nStartCol + 1);
+            rTreeView.set_image(rIter, xImage, -1);
+        rTreeView.set_text(rIter, aName, 0);
         rTreeView.set_id(rIter, sUIItemId);
     }
 }
@@ -465,15 +446,15 @@ void SvxNotebookbarConfigPage::SelectElement()
             {
                 if (aEntries[nIdx].sVisibleValue == "True")
                 {
-                    rTreeView.set_toggle(rIter, TRISTATE_TRUE, 0);
+                    rTreeView.set_toggle(rIter, TRISTATE_TRUE);
                 }
                 else
                 {
-                    rTreeView.set_toggle(rIter, TRISTATE_FALSE, 0);
+                    rTreeView.set_toggle(rIter, TRISTATE_FALSE);
                 }
             }
             InsertEntryIntoNotebookbarTabUI(aEntries[nIdx].sClassId, aEntries[nIdx].sDisplayName,
-                                            aEntries[nIdx].sActionName, rTreeView, rIter, 1);
+                                            aEntries[nIdx].sActionName, rTreeView, rIter);
         });
 
     aEntries.clear();
@@ -532,7 +513,7 @@ void SvxNotebookbarEntriesListBox::ChangedVisibility(int nRow)
     OUString sNotebookbarInterface = getFileName(m_pPage->GetFileName());
 
     OUString sVisible;
-    if (m_xControl->get_toggle(nRow, 0) == TRISTATE_TRUE)
+    if (m_xControl->get_toggle(nRow) == TRISTATE_TRUE)
         sVisible = "True";
     else
         sVisible = "False";
@@ -556,9 +537,8 @@ IMPL_LINK(SvxNotebookbarEntriesListBox, KeyInputHdl, const KeyEvent&, rKeyEvent,
     if (rKeyEvent.GetKeyCode() == KEY_SPACE)
     {
         int nRow = m_xControl->get_selected_index();
-        m_xControl->set_toggle(
-            nRow, m_xControl->get_toggle(nRow, 0) == TRISTATE_TRUE ? TRISTATE_FALSE : TRISTATE_TRUE,
-            0);
+        m_xControl->set_toggle(nRow, m_xControl->get_toggle(nRow) == TRISTATE_TRUE ? TRISTATE_FALSE
+                                                                                   : TRISTATE_TRUE);
         ChangedVisibility(nRow);
         return true;
     }

cui/source/customize/SvxToolbarConfigPage.cxx
@@ -53,28 +53,9 @@ SvxToolbarConfigPage::SvxToolbarConfigPage(weld::Container* pPage, weld::DialogC
     m_xContentsListBox.reset(new SvxToolbarEntriesListBox(m_xBuilder->weld_tree_view("toolcontents"), this));
     m_xDropTargetHelper.reset(new SvxConfigPageFunctionDropTarget(*this, m_xContentsListBox->get_widget()));
 
-    std::vector<int> aWidths;
     weld::TreeView& rTreeView = m_xContentsListBox->get_widget();
     Size aSize(m_xFunctions->get_size_request());
     rTreeView.set_size_request(aSize.Width(), aSize.Height());
-
-    int nExpectedSize = 16;
-
-    int nStandardImageColWidth = rTreeView.get_checkbox_column_width();
-    int nMargin = nStandardImageColWidth - nExpectedSize;
-    if (nMargin < 16)
-        nMargin = 16;
-
-    if (SvxConfigPageHelper::GetImageType() & css::ui::ImageType::SIZE_LARGE)
-        nExpectedSize = 24;
-    else if (SvxConfigPageHelper::GetImageType() & css::ui::ImageType::SIZE_32)
-        nExpectedSize = 32;
-
-    int nImageColWidth = nExpectedSize + nMargin;
-
-    aWidths.push_back(nStandardImageColWidth);
-    aWidths.push_back(nImageColWidth);
-    rTreeView.set_column_fixed_widths(aWidths);
 
     rTreeView.set_hexpand(true);
     rTreeView.set_vexpand(true);
@@ -467,7 +448,7 @@ IMPL_LINK(SvxToolbarConfigPage, InsertHdl, const OString&, rIdent, void)
         pNewEntryData->SetUserDefined();
 
         int nPos = AppendEntry(pNewEntryData, -1);
-        InsertEntryIntoUI(pNewEntryData, m_xContentsListBox->get_widget(), nPos, 1);
+        InsertEntryIntoUI(pNewEntryData, m_xContentsListBox->get_widget(), nPos);
 
         static_cast<ToolbarSaveInData*>( GetSaveInData())->ApplyToolbar( pToolbar );
 
@@ -516,7 +497,7 @@ IMPL_LINK(SvxToolbarConfigPage, ModifyItemHdl, const OString&, rIdent, void)
             else
                 pEntry->SetName( aNewName );
 
-            m_xContentsListBox->set_text(nActEntry, aNewName, 2);
+            m_xContentsListBox->set_text(nActEntry, aNewName, 0);
             bNeedsApply = true;
         }
     }
@@ -564,8 +545,8 @@ IMPL_LINK(SvxToolbarConfigPage, ModifyItemHdl, const OString&, rIdent, void)
 
                     OUString sId(OUString::number(reinterpret_cast<sal_Int64>(pEntry)));
                     m_xContentsListBox->insert(nActEntry, sId);
-                    m_xContentsListBox->set_toggle(nActEntry, pEntry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE, 0);
-                    InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nActEntry, 1);
+                    m_xContentsListBox->set_toggle(nActEntry, pEntry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE);
+                    InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nActEntry);
 
                     m_xContentsListBox->select(nActEntry);
                     m_xContentsListBox->scroll_to_row(nActEntry);
@@ -604,8 +585,8 @@ IMPL_LINK(SvxToolbarConfigPage, ModifyItemHdl, const OString&, rIdent, void)
 
             OUString sId(OUString::number(reinterpret_cast<sal_Int64>(pEntry)));
             m_xContentsListBox->insert(nActEntry, sId);
-            m_xContentsListBox->set_toggle(nActEntry, pEntry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE, 0);
-            InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nActEntry, 1);
+            m_xContentsListBox->set_toggle(nActEntry, pEntry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE);
+            InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nActEntry);
 
             m_xContentsListBox->select(nActEntry);
             m_xContentsListBox->scroll_to_row(nActEntry);
@@ -638,7 +619,7 @@ IMPL_LINK(SvxToolbarConfigPage, ModifyItemHdl, const OString&, rIdent, void)
         {
             pEntry->SetName( aSystemName );
             m_xContentsListBox->set_text(
-                nActEntry, SvxConfigPageHelper::stripHotKey(aSystemName), 2);
+                nActEntry, SvxConfigPageHelper::stripHotKey(aSystemName), 0);
             bNeedsApply = true;
         }
 
@@ -661,8 +642,8 @@ IMPL_LINK(SvxToolbarConfigPage, ModifyItemHdl, const OString&, rIdent, void)
             OUString sId(OUString::number(reinterpret_cast<sal_Int64>(pEntry)));
             m_xContentsListBox->insert(nActEntry, sId);
             m_xContentsListBox->set_toggle(nActEntry,
-                pEntry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE, 0);
-            InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nActEntry, 1);
+                pEntry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE);
+            InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nActEntry);
 
             m_xContentsListBox->select(nActEntry);
             m_xContentsListBox->scroll_to_row(nActEntry);
@@ -793,8 +774,8 @@ void SvxToolbarConfigPage::SelectElement()
         OUString sId(OUString::number(reinterpret_cast<sal_Int64>(entry)));
         m_xContentsListBox->insert(i, sId);
         if (entry->IsBinding() && !entry->IsSeparator())
-            m_xContentsListBox->set_toggle(i,  entry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE, 0);
-        InsertEntryIntoUI(entry, m_xContentsListBox->get_widget(), i, 1);
+            m_xContentsListBox->set_toggle(i,  entry->IsVisible() ? TRISTATE_TRUE : TRISTATE_FALSE);
+        InsertEntryIntoUI(entry, m_xContentsListBox->get_widget(), i);
         ++i;
     }
 
@@ -820,10 +801,10 @@ void SvxToolbarConfigPage::AddFunction(int nTarget)
     if ( pEntry->IsBinding() ) //TODO sep ?
     {
         pEntry->SetVisible(true);
-        m_xContentsListBox->set_toggle(nNewLBEntry, TRISTATE_TRUE, 0);
+        m_xContentsListBox->set_toggle(nNewLBEntry, TRISTATE_TRUE);
     }
 
-    InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nNewLBEntry, 1);
+    InsertEntryIntoUI(pEntry, m_xContentsListBox->get_widget(), nNewLBEntry);
 
     // Changes are not visible on the toolbar until this point
     // TODO: Figure out a way to show the changes on the toolbar, but revert if
@@ -854,7 +835,7 @@ void SvxToolbarEntriesListBox::ChangedVisibility(int nRow)
 
     if (pEntryData->IsBinding())
     {
-        pEntryData->SetVisible(m_xControl->get_toggle(nRow, 0) == TRISTATE_TRUE);
+        pEntryData->SetVisible(m_xControl->get_toggle(nRow) == TRISTATE_TRUE);
 
         SvxConfigEntry* pToolbar = m_pPage->GetTopLevelSelection();
 
@@ -879,7 +860,7 @@ IMPL_LINK(SvxToolbarEntriesListBox, KeyInputHdl, const KeyEvent&, rKeyEvent, boo
         SvxConfigEntry* pEntryData = reinterpret_cast<SvxConfigEntry*>(m_xControl->get_id(nRow).toInt64());
         if (pEntryData->IsBinding() && !pEntryData->IsSeparator())
         {
-            m_xControl->set_toggle(nRow, m_xControl->get_toggle(nRow, 0) == TRISTATE_TRUE ? TRISTATE_FALSE : TRISTATE_TRUE, 0);
+            m_xControl->set_toggle(nRow, m_xControl->get_toggle(nRow) == TRISTATE_TRUE ? TRISTATE_FALSE : TRISTATE_TRUE);
             ChangedVisibility(nRow);
         }
         return true;

cui/source/customize/cfg.cxx
@@ -926,6 +926,7 @@ SvxMenuEntriesListBox::SvxMenuEntriesListBox(std::unique_ptr<weld::TreeView> xCo
     , m_xDropDown(m_xControl->create_virtual_device())
     , m_pPage(pPg)
 {
+    m_xControl->enable_toggle_buttons(weld::ColumnToggleType::Check);
     CreateDropDown();
     m_xControl->connect_key_press(LINK(this, SvxMenuEntriesListBox, KeyInputHdl));
 }
@@ -1507,7 +1508,8 @@ int SvxConfigPage::AppendEntry(
 
 namespace
 {
-    template<typename itertype> void TmplInsertEntryIntoUI(SvxConfigEntry* pNewEntryData, weld::TreeView& rTreeView, itertype& rIter, int nStartCol, SaveInData* pSaveInData, VirtualDevice& rDropDown)
+    template<typename itertype> void TmplInsertEntryIntoUI(SvxConfigEntry* pNewEntryData, weld::TreeView& rTreeView, itertype& rIter, SaveInData* pSaveInData,
+                                                           VirtualDevice& rDropDown, bool bMenu)
     {
         OUString sId(OUString::number(reinterpret_cast<sal_Int64>(pNewEntryData)));
 
@@ -1515,37 +1517,37 @@ namespace
 
         if (pNewEntryData->IsSeparator())
         {
-            rTreeView.set_text(rIter, "----------------------------------", nStartCol + 1);
+            rTreeView.set_text(rIter, "----------------------------------", 0);
         }
         else
         {
             auto xImage = pSaveInData->GetImage(pNewEntryData->GetCommand());
             if (xImage.is())
-                rTreeView.set_image(rIter, xImage, nStartCol);
+                rTreeView.set_image(rIter, xImage, -1);
             OUString aName = SvxConfigPageHelper::stripHotKey( pNewEntryData->GetName() );
-            rTreeView.set_text(rIter, aName, nStartCol + 1);
+            rTreeView.set_text(rIter, aName, 0);
         }
 
-        if (nStartCol == 0)  // menus
+        if (bMenu)  // menus
         {
             if (pNewEntryData->IsPopup() || pNewEntryData->GetStyle() & css::ui::ItemStyle::DROP_DOWN)
-                rTreeView.set_image(rIter, rDropDown, nStartCol + 2);
+                rTreeView.set_image(rIter, rDropDown, 1);
             else
-                rTreeView.set_image(rIter, css::uno::Reference<css::graphic::XGraphic>(), nStartCol + 2);
+                rTreeView.set_image(rIter, css::uno::Reference<css::graphic::XGraphic>(), 1);
         }
     }
 }
 
-void SvxConfigPage::InsertEntryIntoUI(SvxConfigEntry* pNewEntryData, weld::TreeView& rTreeView, int nPos, int nStartCol)
+void SvxConfigPage::InsertEntryIntoUI(SvxConfigEntry* pNewEntryData, weld::TreeView& rTreeView, int nPos, bool bMenu)
 {
-    TmplInsertEntryIntoUI<int>(pNewEntryData, rTreeView, nPos, nStartCol,
-            GetSaveInData(), m_xContentsListBox->get_dropdown_image());
+    TmplInsertEntryIntoUI<int>(pNewEntryData, rTreeView, nPos, GetSaveInData(),
+                               m_xContentsListBox->get_dropdown_image(), bMenu);
 }
 
-void SvxConfigPage::InsertEntryIntoUI(SvxConfigEntry* pNewEntryData, weld::TreeView& rTreeView, weld::TreeIter& rIter, int nStartCol)
+void SvxConfigPage::InsertEntryIntoUI(SvxConfigEntry* pNewEntryData, weld::TreeView& rTreeView, weld::TreeIter& rIter, bool bMenu)
 {
-    TmplInsertEntryIntoUI<weld::TreeIter>(pNewEntryData, rTreeView, rIter, nStartCol,
-            GetSaveInData(), m_xContentsListBox->get_dropdown_image());
+    TmplInsertEntryIntoUI<weld::TreeIter>(pNewEntryData, rTreeView, rIter, GetSaveInData(),
+                                          m_xContentsListBox->get_dropdown_image(), bMenu);
 }
 
 IMPL_LINK(SvxConfigPage, MoveHdl, weld::Button&, rButton, void)

commit 046d98cda9b94c676af41462767ab69a31934a9b

tdf#136534: Font replacement table loads existing rule incorrectly
Regression from ef588eb3b8d11a1fec50bfb2689a603010c78c7e
author	Caolán McNamara <caolanm@redhat.com>	2020-06-05 16:11:39 +0100
committer	Caolán McNamara <caolanm@redhat.com>	2020-06-08 20:21:35 +0200
commit	ef588eb3b8d11a1fec50bfb2689a603010c78c7e (patch)
tree	49ae5191c2bd4b13c3cd547951933fbc37cda0fa /cui/source/options/fontsubs.cxx
parent	23c73ead4186671b29ea310996ef3f2e8913458d (diff)
rework treeview initial toggle button col to be like expander col
cause this assumption is baked into the vcl one making it hard
to adapt remaining cases

Change-Id: I31854211be5f558b51f345af6a11c471a9bd120d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102148
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

cui/source/options/fontsubs.cxx
@@ -326,8 +326,8 @@ void SvxFontSubstTabPage::SelectHdl(const weld::Widget* pWin)
         if (m_xCheckLB->count_selected_rows() == 1)
         {
             int nRow = m_xCheckLB->get_selected_index();
-            m_xFont1CB->set_entry_text(m_xCheckLB->get_text(nRow, 3));
-            m_xFont2CB->set_entry_text(m_xCheckLB->get_text(nRow, 4));
+            m_xFont1CB->set_entry_text(m_xCheckLB->get_text(nRow, 2));
+            m_xFont2CB->set_entry_text(m_xCheckLB->get_text(nRow, 3));
         }
     }
 