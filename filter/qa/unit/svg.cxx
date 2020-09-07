LemmingAvalanche/libreoffice-repaired

commit 6b4cd5d4e4be6adcce04328c4362d8559e85bafc

sw SVG export: try to reuse original bitmap data for JPG and PNG bitmaps
Writer shapes are implemented using SwXShape, Impress shapes use
SdrGrafObj. So switch to working with the XShape interface, which is
supported by both.

Also, don't work with the transformed graphic if it has the same
checksum as the original graphic: the transformed graphic is not linked
to the original JPG/PNG data.

Now selecting an image in Writer Online has the same speedup that
Impress Online already had.

Change-Id: Iab2791c5f5c7a2754e3de0ebb2d6ea664f6c77e4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89540
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

filter/source/svg/svgexport.cxx
@@ -693,13 +693,20 @@ bool SVGFilter::implExportWriterTextGraphic( const Reference< view::XSelectionSu
     if (xSelection.is() && xSelection->supportsService("com.sun.star.text.TextGraphicObject"))
     {
         uno::Reference<beans::XPropertySet> xPropertySet(xSelection, uno::UNO_QUERY);
-        uno::Reference<graphic::XGraphic> xGraphic;
-        xPropertySet->getPropertyValue("TransformedGraphic") >>= xGraphic;
 
-        if (!xGraphic.is())
+        uno::Reference<graphic::XGraphic> xOriginalGraphic;
+        xPropertySet->getPropertyValue("Graphic") >>= xOriginalGraphic;
+        const Graphic aOriginalGraphic(xOriginalGraphic);
+
+        uno::Reference<graphic::XGraphic> xTransformedGraphic;
+        xPropertySet->getPropertyValue("TransformedGraphic") >>= xTransformedGraphic;
+
+        if (!xTransformedGraphic.is())
             return false;
-
-        const Graphic aGraphic(xGraphic);
+        const Graphic aTransformedGraphic(xTransformedGraphic);
+        bool bChecksumMatches = aOriginalGraphic.GetChecksum() == aTransformedGraphic.GetChecksum();
+        const Graphic aGraphic = bChecksumMatches ? aOriginalGraphic : aTransformedGraphic;
+        uno::Reference<graphic::XGraphic> xGraphic = bChecksumMatches ? xOriginalGraphic : xTransformedGraphic;
 
         // Calculate size from Graphic
         Point aPos( OutputDevice::LogicToLogic(aGraphic.GetPrefMapMode().GetOrigin(), aGraphic.GetPrefMapMode(), MapMode(MapUnit::Map100thMM)) );

filter/source/svg/svgwriter.cxx
@@ -2688,20 +2688,22 @@ void SVGActionWriter::ImplWriteText( const Point& rPos, const OUString& rText,
 
 namespace
 {
-SdrGrafObj* GetSdrGrafObjFromXShape(const css::uno::Reference<css::drawing::XShape>* pShape)
+void GetGraphicFromXShape(const css::uno::Reference<css::drawing::XShape>* pShape, Graphic& rGraphic)
 {
     if (!pShape)
     {
-        return nullptr;
+        return;
     }
 
-    auto pObject = dynamic_cast<SvxGraphicObject*>(pShape->get());
-    if (!pObject)
+    uno::Reference<beans::XPropertySet> xPropertySet(*pShape, uno::UNO_QUERY);
+    if (!xPropertySet.is())
     {
-        return nullptr;
+        return;
     }
 
-    return dynamic_cast<SdrGrafObj*>(pObject->GetSdrObject());
+    uno::Reference<graphic::XGraphic> xGraphic;
+    xPropertySet->getPropertyValue("Graphic") >>= xGraphic;
+    rGraphic= Graphic(xGraphic);
 }
 }
 
@@ -2724,34 +2726,30 @@ void SVGActionWriter::ImplWriteBmp( const BitmapEx& rBmpEx,
             SvMemoryStream aOStm( 65535, 65535 );
 
             bool bCached = false;
-            SdrGrafObj* pGrafObj = nullptr;
+            Graphic aGraphic;
             bool bPNG = false;
             bool bJPG = false;
             if (pShape)
             {
-                pGrafObj = GetSdrGrafObjFromXShape(pShape);
-                if (pGrafObj)
+                GetGraphicFromXShape(pShape, aGraphic);
+                if (aGraphic.GetType() == GraphicType::Bitmap)
                 {
-                    const Graphic& rGraphic = pGrafObj->GetGraphic();
-                    if (rGraphic.GetType() == GraphicType::Bitmap)
+                    const BitmapEx& rGraphicBitmap = aGraphic.GetBitmapExRef();
+                    if (rGraphicBitmap.GetChecksum() == rBmpEx.GetChecksum())
                     {
-                        const BitmapEx& rGraphicBitmap = rGraphic.GetBitmapExRef();
-                        if (rGraphicBitmap.GetChecksum() == rBmpEx.GetChecksum())
+                        GfxLink aGfxLink = aGraphic.GetGfxLink();
+                        if (aGfxLink.GetType() == GfxLinkType::NativePng)
                         {
-                            GfxLink aGfxLink = rGraphic.GetGfxLink();
-                            if (aGfxLink.GetType() == GfxLinkType::NativePng)
-                            {
-                                bPNG = true;
-                            }
-                            else if (aGfxLink.GetType() == GfxLinkType::NativeJpg)
-                            {
-                                bJPG = true;
-                            }
-                            if (bPNG || bJPG)
-                            {
-                                aOStm.WriteBytes(aGfxLink.GetData(), aGfxLink.GetDataSize());
-                                bCached = true;
-                            }
+                            bPNG = true;
+                        }
+                        else if (aGfxLink.GetType() == GfxLinkType::NativeJpg)
+                        {
+                            bJPG = true;
+                        }
+                        if (bPNG || bJPG)
+                        {
+                            aOStm.WriteBytes(aGfxLink.GetData(), aGfxLink.GetDataSize());
+                            bCached = true;
                         }
                     }
                 }

filter/qa/unit/svg.cxx
@@ -1,0 +1,100 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+#include <test/xmltesttools.hxx>
+
+#include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
+#include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/frame/XStorable.hpp>
+#include <com/sun/star/io/XOutputStream.hpp>
+#include <unotools/streamwrap.hxx>
+#include <unotools/mediadescriptor.hxx>
+#include <tools/stream.hxx>
+
+using namespace ::com::sun::star;
+
+char const DATA_DIRECTORY[] = "/filter/qa/unit/data/";
+
+/// SVG filter tests.
+class SvgFilterTest : public test::BootstrapFixture, public unotest::MacrosTest, public XmlTestTools
+{
+private:
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    void setUp() override;
+    void tearDown() override;
+    void registerNamespaces(xmlXPathContextPtr& pXmlXpathCtx) override;
+    uno::Reference<lang::XComponent>& getComponent() { return mxComponent; }
+    void load(const OUString& rURL);
+};
+
+void SvgFilterTest::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    mxDesktop.set(frame::Desktop::create(mxComponentContext));
+}
+
+void SvgFilterTest::tearDown()
+{
+    if (mxComponent.is())
+        mxComponent->dispose();
+
+    test::BootstrapFixture::tearDown();
+}
+
+void SvgFilterTest::load(const OUString& rFileName)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + rFileName;
+    mxComponent = loadFromDesktop(aURL);
+}
+
+void SvgFilterTest::registerNamespaces(xmlXPathContextPtr& pXmlXpathCtx)
+{
+    xmlXPathRegisterNs(pXmlXpathCtx, BAD_CAST("svg"), BAD_CAST("http://www.w3.org/2000/svg"));
+}
+
+CPPUNIT_TEST_FIXTURE(SvgFilterTest, testPreserveJpg)
+{
+#if !defined(MACOSX)
+    // Load a document with a jpeg image in it.
+    load("preserve-jpg.odt");
+
+    // Select the image.
+    dispatchCommand(getComponent(), ".uno:JumpToNextFrame", {});
+
+    // Export the selection to SVG.
+    uno::Reference<frame::XStorable> xStorable(getComponent(), uno::UNO_QUERY_THROW);
+    SvMemoryStream aStream;
+    uno::Reference<io::XOutputStream> xOut = new utl::OOutputStreamWrapper(aStream);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("writer_svg_Export");
+    aMediaDescriptor["SelectionOnly"] <<= true;
+    aMediaDescriptor["OutputStream"] <<= xOut;
+    xStorable->storeToURL("private:stream", aMediaDescriptor.getAsConstPropertyValueList());
+    aStream.Seek(STREAM_SEEK_TO_BEGIN);
+
+    // Make sure the the original JPG data is reused and we don't perform a PNG re-compress.
+    xmlDocPtr pXmlDoc = parseXmlStream(&aStream);
+    OUString aAttributeValue = getXPath(pXmlDoc, "//svg:image", "href");
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expression: aAttributeValue.startsWith("data:image/jpeg")
+    // i.e. the SVG export result re-compressed the image as PNG, even if the original and the
+    // transformed image is the same, so there is no need for that.
+    CPPUNIT_ASSERT(aAttributeValue.startsWith("data:image/jpeg"));
+#endif
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

filter/CppunitTest_filter_svg.mk
@@ -1,0 +1,47 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,filter_svg))
+
+$(eval $(call gb_CppunitTest_use_externals,filter_svg,\
+	boost_headers \
+	libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,filter_svg, \
+    filter/qa/unit/svg \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,filter_svg, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    sal \
+    test \
+    unotest \
+    utl \
+    tl \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,filter_svg))
+
+$(eval $(call gb_CppunitTest_use_ure,filter_svg))
+$(eval $(call gb_CppunitTest_use_vcl,filter_svg))
+
+$(eval $(call gb_CppunitTest_use_rdb,filter_svg,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,filter_svg,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,filter_svg))
+
+# vim: set noet sw=4 ts=4:

filter/Module_filter.mk
@@ -67,6 +67,7 @@ $(eval $(call gb_Module_add_check_targets,filter,\
     CppunitTest_filter_ras_test \
     CppunitTest_filter_tiff_test \
     CppunitTest_filter_tga_test \
+    CppunitTest_filter_svg \
 ))
 endif
 

commit b7f5db5e1b99bf73d6eb48344b42a8406263622f

SVG export: fix failure on trying to read graphic of a 3D shape
Regression from commit 6b4cd5d4e4be6adcce04328c4362d8559e85bafc (sw SVG
export: try to reuse original bitmap data for JPG and PNG bitmaps,
2020-02-27), which generalized the code that tries to get the original
bitmap of shapes for performance reasons. Originally this worked with
SdrGrafObj, but now we try every XShape which has a Graphic property.

Add the missing check, so shapes without a graphic property just miss
the fast path, but doesn't fail the entire SVG export.

Change-Id: I6b20083110f3269337b9df6a23bd193cc6f7d13b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102187
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

filter/source/svg/svgexport.cxx
@@ -59,6 +59,7 @@
 #include <svx/svdograf.hxx>
 #include <svx/svdmodel.hxx>
 #include <svx/svdxcgv.hxx>
+#include <sal/log.hxx>
 
 #include <memory>
 
@@ -608,7 +609,7 @@ bool SVGFilter::implExportImpressOrDraw( const Reference< XOutputStream >& rxOSt
                 {
                     delete mpSVGDoc;
                     mpSVGDoc = nullptr;
-                    OSL_FAIL( "Exception caught" );
+                    SAL_WARN("filter.svg", "Exception caught");
                 }
 
                 if( nullptr != pSdrModel )

filter/source/svg/svgwriter.cxx
@@ -2758,7 +2758,10 @@ void GetGraphicFromXShape(const css::uno::Reference<css::drawing::XShape>* pShap
     }
 
     uno::Reference<graphic::XGraphic> xGraphic;
-    xPropertySet->getPropertyValue("Graphic") >>= xGraphic;
+    if (xPropertySet->getPropertySetInfo()->hasPropertyByName("Graphic"))
+    {
+        xPropertySet->getPropertyValue("Graphic") >>= xGraphic;
+    }
     rGraphic= Graphic(xGraphic);
 }
 }

filter/qa/unit/svg.cxx
@@ -160,6 +160,25 @@ CPPUNIT_TEST_FIXTURE(SvgFilterTest, testSemiTransparentText)
     assertXPath(pXmlDoc, "//svg:text[2]/svg:tspan/svg:tspan/svg:tspan[@fill-opacity]", 0);
 }
 
+CPPUNIT_TEST_FIXTURE(SvgFilterTest, testShapeNographic)
+{
+    // Load a document containing a 3D shape.
+    load("shape-nographic.odp");
+
+    // Export to SVG.
+    uno::Reference<frame::XStorable> xStorable(getComponent(), uno::UNO_QUERY_THROW);
+    SvMemoryStream aStream;
+    uno::Reference<io::XOutputStream> xOut = new utl::OOutputStreamWrapper(aStream);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("impress_svg_Export");
+    aMediaDescriptor["OutputStream"] <<= xOut;
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // An uncaught exception of type com.sun.star.io.IOException
+    // - SfxBaseModel::impl_store <private:stream> failed: 0xc10(Error Area:Io Class:Write Code:16)
+    xStorable->storeToURL("private:stream", aMediaDescriptor.getAsConstPropertyValueList());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */