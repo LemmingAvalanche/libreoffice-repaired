LemmingAvalanche/libreoffice-repaired

commit 86bd98d2d65e16ed9903af37db12d63352772bf2

extract PDF import for annotations to a separate function
Change-Id: Ibaabaee5e1e6416f1ac518ff846b13e6c3f29cad
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102091
Tested-by: Tomaž Vajngerl <quikee@gmail.com>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/filter/ipdf/pdfread.cxx
@@ -226,6 +226,58 @@ bool ImportPDF(SvStream& rStream, Graphic& rGraphic)
     return true;
 }
 
+namespace
+{
+std::vector<PDFGraphicAnnotation> findAnnotations(std::unique_ptr<vcl::pdf::PDFiumPage>& pPage,
+                                                  basegfx::B2DSize aPageSize)
+{
+    std::vector<PDFGraphicAnnotation> aPDFGraphicAnnotations;
+    for (int nAnnotation = 0; nAnnotation < pPage->getAnnotationCount(); nAnnotation++)
+    {
+        auto pAnnotation = pPage->getAnnotation(nAnnotation);
+        if (pAnnotation)
+        {
+            auto eSubtype = pAnnotation->getSubType();
+
+            if (eSubtype == vcl::pdf::PDFAnnotationSubType::Text
+                && pAnnotation->hasKey(vcl::pdf::constDictionaryKeyPopup))
+            {
+                OUString sAuthor = pAnnotation->getString(vcl::pdf::constDictionaryKeyTitle);
+                OUString sText = pAnnotation->getString(vcl::pdf::constDictionaryKeyContents);
+                auto pPopupAnnotation = pAnnotation->getLinked(vcl::pdf::constDictionaryKeyPopup);
+
+                basegfx::B2DRectangle rRectangle = pAnnotation->getRectangle();
+                basegfx::B2DRectangle rRectangleHMM(
+                    convertPointToMm100(rRectangle.getMinX()),
+                    convertPointToMm100(aPageSize.getY() - rRectangle.getMinY()),
+                    convertPointToMm100(rRectangle.getMaxX()),
+                    convertPointToMm100(aPageSize.getY() - rRectangle.getMaxY()));
+
+                OUString sDateTimeString
+                    = pAnnotation->getString(vcl::pdf::constDictionaryKeyModificationDate);
+                OUString sISO8601String = vcl::pdf::convertPdfDateToISO8601(sDateTimeString);
+
+                css::util::DateTime aDateTime;
+                if (!sISO8601String.isEmpty())
+                {
+                    utl::ISO8601parseDateTime(sISO8601String, aDateTime);
+                }
+
+                PDFGraphicAnnotation aPDFGraphicAnnotation;
+                aPDFGraphicAnnotation.maRectangle = rRectangleHMM;
+                aPDFGraphicAnnotation.maAuthor = sAuthor;
+                aPDFGraphicAnnotation.maText = sText;
+                aPDFGraphicAnnotation.maDateTime = aDateTime;
+                aPDFGraphicAnnotation.meSubType = eSubtype;
+                aPDFGraphicAnnotations.push_back(aPDFGraphicAnnotation);
+            }
+        }
+    }
+    return aPDFGraphicAnnotations;
+}
+
+} // end anonymous namespace
+
 size_t ImportPDFUnloaded(const OUString& rURL, std::vector<PDFGraphicResult>& rGraphics)
 {
 #if HAVE_FEATURE_PDFIUM
@@ -282,49 +334,8 @@ size_t ImportPDFUnloaded(const OUString& rURL, std::vector<PDFGraphicResult>& rG
 
         auto pPage = pPdfDocument->openPage(nPageIndex);
 
-        std::vector<PDFGraphicAnnotation> aPDFGraphicAnnotations;
-        for (int nAnnotation = 0; nAnnotation < pPage->getAnnotationCount(); nAnnotation++)
-        {
-            auto pAnnotation = pPage->getAnnotation(nAnnotation);
-            if (pAnnotation)
-            {
-                auto eSubtype = pAnnotation->getSubType();
-
-                if (eSubtype == vcl::pdf::PDFAnnotationSubType::Text
-                    && pAnnotation->hasKey(vcl::pdf::constDictionaryKeyPopup))
-                {
-                    OUString sAuthor = pAnnotation->getString(vcl::pdf::constDictionaryKeyTitle);
-                    OUString sText = pAnnotation->getString(vcl::pdf::constDictionaryKeyContents);
-                    auto pPopupAnnotation
-                        = pAnnotation->getLinked(vcl::pdf::constDictionaryKeyPopup);
-
-                    basegfx::B2DRectangle rRectangle = pAnnotation->getRectangle();
-                    basegfx::B2DRectangle rRectangleHMM(
-                        convertPointToMm100(rRectangle.getMinX()),
-                        convertPointToMm100(aPageSize.getY() - rRectangle.getMinY()),
-                        convertPointToMm100(rRectangle.getMaxX()),
-                        convertPointToMm100(aPageSize.getY() - rRectangle.getMaxY()));
-
-                    OUString sDateTimeString
-                        = pAnnotation->getString(vcl::pdf::constDictionaryKeyModificationDate);
-                    OUString sISO8601String = vcl::pdf::convertPdfDateToISO8601(sDateTimeString);
-
-                    css::util::DateTime aDateTime;
-                    if (!sISO8601String.isEmpty())
-                    {
-                        utl::ISO8601parseDateTime(sISO8601String, aDateTime);
-                    }
-
-                    PDFGraphicAnnotation aPDFGraphicAnnotation;
-                    aPDFGraphicAnnotation.maRectangle = rRectangleHMM;
-                    aPDFGraphicAnnotation.maAuthor = sAuthor;
-                    aPDFGraphicAnnotation.maText = sText;
-                    aPDFGraphicAnnotation.maDateTime = aDateTime;
-                    aPDFGraphicAnnotation.meSubType = eSubtype;
-                    aPDFGraphicAnnotations.push_back(aPDFGraphicAnnotation);
-                }
-            }
-        }
+        std::vector<PDFGraphicAnnotation> aPDFGraphicAnnotations
+            = findAnnotations(pPage, aPageSize);
 
         rGraphics.emplace_back(std::move(aGraphic), Size(nPageWidth, nPageHeight),
                                aPDFGraphicAnnotations);

commit 00545c7f3e4e6fd241ce5bc4597e00b2d97a6fb2

Confine findAnnotations to HAVE_FEATURE_PDFIUM
Otherwise e.g. Android builds would fail with

> vcl/source/filter/ipdf/pdfread.cxx:231:77: error: no member named 'PDFiumPage' in namespace 'vcl::pdf'
> std::vector<PDFGraphicAnnotation> findAnnotations(std::unique_ptr<vcl::pdf::PDFiumPage>& pPage,
>                                                                   ~~~~~~~~~~^

etc.

Regression apparently introduced with 86bd98d2d65e16ed9903af37db12d63352772bf2
"extract PDF import for annotations to a separate function".

Change-Id: I364d8d3f30c7eeaa933bfe6aebaf4cad35c20d2b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102153
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

vcl/source/filter/ipdf/pdfread.cxx
@@ -226,6 +226,7 @@ bool ImportPDF(SvStream& rStream, Graphic& rGraphic)
     return true;
 }
 
+#if HAVE_FEATURE_PDFIUM
 namespace
 {
 std::vector<PDFGraphicAnnotation> findAnnotations(std::unique_ptr<vcl::pdf::PDFiumPage>& pPage,
@@ -275,6 +276,7 @@ std::vector<PDFGraphicAnnotation> findAnnotations(std::unique_ptr<vcl::pdf::PDFi
 }
 
 } // end anonymous namespace
+#endif
 
 size_t ImportPDFUnloaded(const OUString& rURL, std::vector<PDFGraphicResult>& rGraphics)
 {