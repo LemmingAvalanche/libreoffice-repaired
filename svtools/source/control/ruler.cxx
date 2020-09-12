LemmingAvalanche/libreoffice-repaired

commit 7887651d024df504a441b81fd8126fc5d49c4299

loplugin:simplifybool more
look for expressions like
    !(a && !b)
which can be expanded out

Change-Id: I72515a9638762b050f9a258c08da39ebfa2ef8e7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100579
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svtools/source/control/ruler.cxx
@@ -363,7 +363,7 @@ void Ruler::ImplVDrawText(vcl::RenderContext& rRenderContext, long nX, long nY, 
 void Ruler::ImplInvertLines(vcl::RenderContext& rRenderContext)
 {
     // Position lines
-    if (!(!mpData->pLines.empty() && mbActive && !mbDrag && !mbFormat && !(mnUpdateFlags & RULER_UPDATE_LINES)) )
+    if (mpData->pLines.empty() || !mbActive || mbDrag || mbFormat || (mnUpdateFlags & RULER_UPDATE_LINES) )
         return;
 
     long nNullWinOff = mpData->nNullVirOff + mnVirOff;
@@ -1915,7 +1915,7 @@ void Ruler::ImplEndDrag()
 
 void Ruler::MouseButtonDown( const MouseEvent& rMEvt )
 {
-    if ( !(rMEvt.IsLeft() && !IsTracking()) )
+    if ( rMEvt.IsLeft() || IsTracking() )
         return;
 
     Point   aMousePos = rMEvt.GetPosPixel();

commit e4396317033cccfbc44efe23bd48b9dc702f1bce

tdf#136217 fix moving tag in writer
regression from
    commit 7887651d024df504a441b81fd8126fc5d49c4299
    loplugin:simplifybool more

Change-Id: I4c62a7922734ea76ecec580491bfe8b0f62b781b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102514
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svtools/source/control/ruler.cxx
@@ -1915,7 +1915,7 @@ void Ruler::ImplEndDrag()
 
 void Ruler::MouseButtonDown( const MouseEvent& rMEvt )
 {
-    if ( rMEvt.IsLeft() || IsTracking() )
+    if ( !rMEvt.IsLeft() || IsTracking() )
         return;
 
     Point   aMousePos = rMEvt.GetPosPixel();