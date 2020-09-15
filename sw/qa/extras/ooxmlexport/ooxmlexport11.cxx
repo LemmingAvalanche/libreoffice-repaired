LemmingAvalanche/libreoffice-repaired

commit 8a7549bd4359e12a7ea277452ba3056f3f7ff09b

tdf#132271 DOCX: import change tracking in floating tables
Change-Id: If892a16875ef16015639aacf8359d15c953fb1d2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102149
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/PropertyMap.cxx
@@ -45,8 +45,11 @@
 #include <com/sun/star/text/VertOrientation.hpp>
 #include <com/sun/star/text/WritingMode.hpp>
 #include <com/sun/star/text/WritingMode2.hpp>
+#include <com/sun/star/text/XRedline.hpp>
 #include <com/sun/star/text/XTextColumns.hpp>
 #include <com/sun/star/text/XText.hpp>
+#include <com/sun/star/text/XTextFrame.hpp>
+#include <com/sun/star/text/XTextTablesSupplier.hpp>
 #include <com/sun/star/text/TextGridMode.hpp>
 #include <com/sun/star/text/XTextCopy.hpp>
 #include <com/sun/star/style/VerticalAlignment.hpp>
@@ -1371,15 +1374,93 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
         rInfo.m_nBreakType = m_nBreakType;
         if ( FloatingTableConversion( rDM_Impl, rInfo ) )
         {
+            std::vector<css::uno::Any> aFramedRedlines = rDM_Impl.aFramedRedlines;
             try
             {
+                // convert redline ranges to cursor movement and character length
+                std::vector<sal_Int32> redPos, redLen;
+                std::vector<OUString> redCell;
+                OUString sTableName;
+                for( size_t i = 0; i < aFramedRedlines.size(); i+=3)
+                {
+                    uno::Reference<text::XText> xCell;
+                    uno::Reference< text::XTextRange > xRange;
+                    aFramedRedlines[i] >>= xRange;
+                    uno::Reference< beans::XPropertySet > xRangeProperties;
+                    if ( xRange.is() )
+                    {
+                        xRangeProperties.set( xRange, uno::UNO_QUERY_THROW );
+
+                        const uno::Sequence<beans::Property> aRangeProperties
+                            = xRangeProperties->getPropertySetInfo()->getProperties();
+
+                        for (const beans::Property& rProperty : aRangeProperties)
+                        {
+                            const OUString& rKey = rProperty.Name;
+                            uno::Any aValue = xRangeProperties->getPropertyValue(rKey);
+                            if ( rKey == "TextTable" )
+                            {
+                                uno::Reference<text::XTextTable> xTable;
+                                aValue >>= xTable;
+                                uno::Reference<beans::XPropertySet> xTableProperties(xTable, uno::UNO_QUERY);
+                                xTableProperties->getPropertyValue("TableName") >>= sTableName;
+                            }
+                            else if ( rKey == "Cell" )
+                            {
+                                 OUString sCellName;
+                                 aValue >>= xCell;
+                                 uno::Reference<beans::XPropertySet> xCellProperties(xCell, uno::UNO_QUERY);
+                                 xCellProperties->getPropertyValue("CellName") >>= sCellName;
+                                 redCell.push_back(sCellName);
+                            }
+                        }
+
+                        uno::Reference<text::XTextCursor> xRangeCursor = xCell->createTextCursorByRange( xRange );
+                        if ( xRangeCursor.is() )
+                        {
+                            sal_Int32 nLen = xRange->getString().getLength();
+                            redLen.push_back(nLen);
+                            xRangeCursor->gotoStart(true);
+                            redPos.push_back(xRangeCursor->getString().getLength() - nLen);
+                        }
+                        else
+                        {
+                            // failed createTextCursorByRange()
+                            redLen.push_back(-1);
+                            redPos.push_back(-1);
+                        }
+                    }
+                }
+
                 xBodyText->convertToTextFrame(rInfo.m_xStart, rInfo.m_xEnd,
                                               rInfo.m_aFrameProperties);
+
+                uno::Reference<text::XTextTablesSupplier> xTextDocument(rDM_Impl.GetTextDocument(), uno::UNO_QUERY);
+                uno::Reference<container::XNameAccess> xTables = xTextDocument->getTextTables();
+                uno::Reference<text::XTextTable> xTable(xTables->getByName(sTableName), uno::UNO_QUERY);
+                for( size_t i = 0; i < aFramedRedlines.size(); i+=3)
+                {
+                    OUString sType;
+                    beans::PropertyValues aRedlineProperties( 3 );
+                    // skip failed createTextCursorByRange()
+                    if (redPos[i/3] == -1)
+                        continue;
+                    aFramedRedlines[i+1] >>= sType;
+                    aFramedRedlines[i+2] >>= aRedlineProperties;
+                    uno::Reference<text::XText> xCell(xTable->getCellByName(redCell[i/3]), uno::UNO_QUERY);
+                    uno::Reference<text::XTextCursor> xCrsr = xCell->createTextCursor();
+                    xCrsr->goRight(redPos[i/3], false);
+                    xCrsr->goRight(redLen[i/3], true);
+                    uno::Reference < text::XRedline > xRedline( xCrsr, uno::UNO_QUERY_THROW );
+                    xRedline->makeRedline( sType, aRedlineProperties );
+                }
             }
             catch (const uno::Exception&)
             {
                 DBG_UNHANDLED_EXCEPTION("writerfilter", "convertToTextFrame() failed");
             }
+
+            aFramedRedlines.clear();
         }
     }
     rPendingFloatingTables.clear();

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -971,6 +971,14 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf125894, "tdf125894.docx")
     assertXPath(pXmlDoc, "//w:ins");
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf132271, "tdf132271.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    // import change tracking in floating tables
+    assertXPath(pXmlDoc, "//w:del", 2);
+    assertXPath(pXmlDoc, "//w:ins", 2);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128156, "tdf128156.docx")
 {
     xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2633,7 +2633,8 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
             uno::Reference < text::XRedline > xRedline( xRange, uno::UNO_QUERY_THROW );
             xRedline->makeRedline( sType, aRedlineProperties );
         }
-        else
+        // store frame and (possible floating) table redline data for restoring them after frame conversion
+        if (m_bIsActualParagraphFramed || (hasTableManager() && getTableManager().isInTable()))
         {
             aFramedRedlines.push_back( uno::makeAny(xRange) );
             aFramedRedlines.push_back( uno::makeAny(sType) );

commit abd6c3bbb5bee81955d068071bdc02a97a9a5903

tdf#136667 DOCX import: fix crash of floating tables
with tracked changes of table structure (not only cell
text content), where text ranges of redlines aren't
connected to a cell. Support also different table names
of redline text ranges for sure.

Regression from commit 8a7549bd4359e12a7ea277452ba3056f3f7ff09b
(tdf#132271 DOCX: import change tracking in floating tables).

Change-Id: I58b1b21c8016d682a292409165991dec2f8cfa3d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102655
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/PropertyMap.cxx
@@ -1380,52 +1380,57 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
                 // convert redline ranges to cursor movement and character length
                 std::vector<sal_Int32> redPos, redLen;
                 std::vector<OUString> redCell;
-                OUString sTableName;
+                std::vector<OUString> redTable;
                 for( size_t i = 0; i < aFramedRedlines.size(); i+=3)
                 {
                     uno::Reference<text::XText> xCell;
                     uno::Reference< text::XTextRange > xRange;
                     aFramedRedlines[i] >>= xRange;
                     uno::Reference< beans::XPropertySet > xRangeProperties;
+                    OUString sTableName;
+                    OUString sCellName;
                     if ( xRange.is() )
                     {
                         xRangeProperties.set( xRange, uno::UNO_QUERY_THROW );
-
-                        const uno::Sequence<beans::Property> aRangeProperties
-                            = xRangeProperties->getPropertySetInfo()->getProperties();
-
-                        for (const beans::Property& rProperty : aRangeProperties)
+                        if (xRangeProperties->getPropertySetInfo()->hasPropertyByName("TextTable"))
                         {
-                            const OUString& rKey = rProperty.Name;
-                            uno::Any aValue = xRangeProperties->getPropertyValue(rKey);
-                            if ( rKey == "TextTable" )
+                            uno::Any aTable = xRangeProperties->getPropertyValue("TextTable");
+                            if ( aTable != uno::Any() )
                             {
                                 uno::Reference<text::XTextTable> xTable;
-                                aValue >>= xTable;
+                                aTable >>= xTable;
                                 uno::Reference<beans::XPropertySet> xTableProperties(xTable, uno::UNO_QUERY);
                                 xTableProperties->getPropertyValue("TableName") >>= sTableName;
                             }
-                            else if ( rKey == "Cell" )
+                            if (xRangeProperties->getPropertySetInfo()->hasPropertyByName("Cell"))
                             {
-                                 OUString sCellName;
-                                 aValue >>= xCell;
-                                 uno::Reference<beans::XPropertySet> xCellProperties(xCell, uno::UNO_QUERY);
-                                 xCellProperties->getPropertyValue("CellName") >>= sCellName;
-                                 redCell.push_back(sCellName);
+                                uno::Any aCell = xRangeProperties->getPropertyValue("Cell");
+                                if ( aCell != uno::Any() )
+                                {
+                                    aCell >>= xCell;
+                                    uno::Reference<beans::XPropertySet> xCellProperties(xCell, uno::UNO_QUERY);
+                                    xCellProperties->getPropertyValue("CellName") >>= sCellName;
+                                }
                             }
                         }
-
-                        uno::Reference<text::XTextCursor> xRangeCursor = xCell->createTextCursorByRange( xRange );
-                        if ( xRangeCursor.is() )
+                        redTable.push_back(sTableName);
+                        redCell.push_back(sCellName);
+                        bool bOk = false;
+                        if (!sTableName.isEmpty() && !sCellName.isEmpty())
                         {
-                            sal_Int32 nLen = xRange->getString().getLength();
-                            redLen.push_back(nLen);
-                            xRangeCursor->gotoStart(true);
-                            redPos.push_back(xRangeCursor->getString().getLength() - nLen);
+                            uno::Reference<text::XTextCursor> xRangeCursor = xCell->createTextCursorByRange( xRange );
+                            if ( xRangeCursor.is() )
+                            {
+                                bOk = true;
+                                sal_Int32 nLen = xRange->getString().getLength();
+                                redLen.push_back(nLen);
+                                xRangeCursor->gotoStart(true);
+                                redPos.push_back(xRangeCursor->getString().getLength() - nLen);
+                            }
                         }
-                        else
+                        if (!bOk)
                         {
-                            // failed createTextCursorByRange()
+                            // missing cell or failed createTextCursorByRange()
                             redLen.push_back(-1);
                             redPos.push_back(-1);
                         }
@@ -1437,7 +1442,6 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
 
                 uno::Reference<text::XTextTablesSupplier> xTextDocument(rDM_Impl.GetTextDocument(), uno::UNO_QUERY);
                 uno::Reference<container::XNameAccess> xTables = xTextDocument->getTextTables();
-                uno::Reference<text::XTextTable> xTable(xTables->getByName(sTableName), uno::UNO_QUERY);
                 for( size_t i = 0; i < aFramedRedlines.size(); i+=3)
                 {
                     OUString sType;
@@ -1447,6 +1451,7 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
                         continue;
                     aFramedRedlines[i+1] >>= sType;
                     aFramedRedlines[i+2] >>= aRedlineProperties;
+                    uno::Reference<text::XTextTable> xTable(xTables->getByName(redTable[i/3]), uno::UNO_QUERY);
                     uno::Reference<text::XText> xCell(xTable->getCellByName(redCell[i/3]), uno::UNO_QUERY);
                     uno::Reference<text::XTextCursor> xCrsr = xCell->createTextCursor();
                     xCrsr->goRight(redPos[i/3], false);

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -979,6 +979,14 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf132271, "tdf132271.docx")
     assertXPath(pXmlDoc, "//w:ins", 2);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf136667, "tdf136667.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    // import change tracking in floating tables
+    assertXPath(pXmlDoc, "//w:del", 2);
+    assertXPath(pXmlDoc, "//w:ins", 4);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128156, "tdf128156.docx")
 {
     xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");