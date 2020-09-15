LemmingAvalanche/libreoffice-repaired

commit cbca89511546fb2c32f07166f8f1f9ec78e30940

tdf#105330 sw: fix lost cursor on undoing nested table insert
This is a regression from commit
232a3ce5eba22c40469bae4e3ee62f969fd20aee (n#751313 SwCallLink: avoid
redrawing complete rows without nested tables, 2012-04-20), though
manual testing shows that the underlying problem has been addressed in
the meantime, so this can be reverted.

Over time, some poor tests started to depend on the new behavior so
adapt them as necessary:

1) Change back test added in commit
1981309bc42836c21652215b4b0fc0d984369cfc (testcase for fdo#38414,
2014-03-18) to its original form, that was changed to an export test in
commit 916e9a24d31d1888a40b96cd6a92c0f26c0a48d7 (move round-tripables to
ooxmlexport, 2016-10-07), as the export of that document still results
in data loss of cell content, just happened to pass so far.

2) Explicitly calculate content of text frames in two more tests, which
just hoped that by the time they assert, the layout is ready already
(but now that the missing notification is restored, it happens that the
first pass of the layout doesn't create them; only a later pass, invoked
by Idle, which doesn't run during cppunit tests).

Change-Id: Ie13d17266cd552f3850848ae980bcfe8776997ac
Reviewed-on: https://gerrit.libreoffice.org/79026
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/crsr/callnk.cxx
@@ -75,19 +75,8 @@ static void lcl_notifyRow(const SwContentNode* pNode, SwCursorShell const & rShe
         return;
 
     const SwTableLine* pLine = pRow->GetTabLine( );
-    // Avoid redrawing the complete row if there are no nested tables
-    for (SwFrame *pCell = pRow->GetLower(); pCell; pCell = pCell->GetNext())
-    {
-        for (SwFrame *pContent = pCell->GetLower(); pContent; pContent = pContent->GetNext())
-        {
-            if (pContent->GetType() == SwFrameType::Tab)
-            {
-                SwFormatFrameSize aSize = pLine->GetFrameFormat()->GetFrameSize();
-                pRow->ModifyNotification(nullptr, &aSize);
-                return;
-            }
-        }
-    }
+    SwFormatFrameSize aSize = pLine->GetFrameFormat()->GetFrameSize();
+    pRow->ModifyNotification(nullptr, &aSize);
 }
 
 SwCallLink::~SwCallLink() COVERITY_NOEXCEPT_FALSE

sw/inc/crsrsh.hxx
@@ -691,6 +691,7 @@ public:
     // (This is needed for displaying the Drag&Drop/Copy-Cursor.)
     bool SetVisibleCursor( const Point &rPt );
     inline void UnSetVisibleCursor();
+    SwVisibleCursor* GetVisibleCursor() const;
 
     // jump to the next or previous field of the corresponding type
     bool MoveFieldType(

sw/inc/viscrs.hxx
@@ -25,6 +25,7 @@
 #include "swcrsr.hxx"
 #include "swrect.hxx"
 #include "swregion.hxx"
+#include "swdllapi.h"
 
 namespace sdr { namespace overlay { class OverlayObject; } }
 
@@ -33,7 +34,7 @@ class SfxViewShell;
 
 // From here classes/methods for non-text cursor.
 
-class SwVisibleCursor
+class SW_DLLPUBLIC SwVisibleCursor
 {
     friend void InitCore();
     friend void FinitCore();
@@ -57,6 +58,7 @@ public:
     bool IsVisible() const { return m_bIsVisible; }
     void SetDragCursor( bool bFlag = true ) { m_bIsDragCursor = bFlag; }
     void SetPosAndShow(SfxViewShell const * pViewShell);
+    const vcl::Cursor& GetTextCursor() const;
 };
 
 // From here classes/methods for selections.

sw/qa/extras/layout/layout.cxx
@@ -2525,6 +2525,14 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf118672)
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf117923)
 {
     createDoc("tdf117923.doc");
+    // Ensure that all text portions are calculated before testing.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwViewShell* pViewShell
+        = pTextDoc->GetDocShell()->GetDoc()->getIDocumentLayoutAccess().GetCurrentViewShell();
+    CPPUNIT_ASSERT(pViewShell);
+    pViewShell->Reformat();
+
     xmlDocPtr pXmlDoc = parseLayoutDump();
 
     // Check that we actually test the line we need

sw/qa/extras/ooxmlexport/ooxmlexport10.cxx
@@ -525,7 +525,7 @@ DECLARE_OOXMLEXPORT_TEST(testNegativeCellMarginTwips, "negative-cell-margin-twip
     CPPUNIT_ASSERT( width.toInt32() > 0 );
 }
 
-DECLARE_OOXMLEXPORT_TEST(testFdo38414, "fdo38414.docx" )
+DECLARE_OOXMLIMPORT_TEST(testFdo38414, "fdo38414.docx" )
 {
     // The cells in the last (4th) column were merged properly and so the result didn't have the same height.
     // (Since w:gridBefore is worked around by faking another cell in the row, so column count is thus 5

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -39,6 +39,7 @@
 #include <svl/stritem.hxx>
 #include <svx/svxids.hrc>
 #include <comphelper/lok.hxx>
+#include <comphelper/propertyvalue.hxx>
 #include <txtfrm.hxx>
 #include <redline.hxx>
 #include <view.hxx>
@@ -2226,6 +2227,30 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testOleSaveWhileEdit)
     mxComponent->dispose();
     mxComponent.clear();
     comphelper::LibreOfficeKit::setActive(false);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf105330)
+{
+    load(DATA_DIRECTORY, "tdf105330.odt");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    SwWrtShell* pWrtShell = pTextDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->Down(/*bSelect=*/false);
+    uno::Sequence<beans::PropertyValue> aTableArgs = {
+        comphelper::makePropertyValue("Rows", static_cast<sal_uInt16>(1)),
+        comphelper::makePropertyValue("Columns", static_cast<sal_uInt16>(1)),
+    };
+    lcl_dispatchCommand(mxComponent, ".uno:InsertTable", aTableArgs);
+
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    sw::UndoManager& rUndoManager = pDoc->GetUndoManager();
+    rUndoManager.Undo();
+
+    //  Without the accompanying fix in place, height was only 1 twips (practically invisible).
+    CPPUNIT_ASSERT_EQUAL(static_cast<long>(276),
+                         pWrtShell->GetVisibleCursor()->GetTextCursor().GetSize().getHeight());
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/extras/ww8import/ww8import.cxx
@@ -164,6 +164,14 @@ DECLARE_OOXMLIMPORT_TEST(testImageLazyRead, "image-lazy-read.doc")
 
 DECLARE_WW8IMPORT_TEST(testTdf106799, "tdf106799.doc")
 {
+    // Ensure that all text portions are calculated before testing.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwViewShell* pViewShell
+        = pTextDoc->GetDocShell()->GetDoc()->getIDocumentLayoutAccess().GetCurrentViewShell();
+    CPPUNIT_ASSERT(pViewShell);
+    pViewShell->Reformat();
+
     sal_Int32 const nCellWidths[3][4] = { { 9530, 0, 0, 0 },{ 2382, 2382, 2382, 2384 },{ 2382, 2382, 2382, 2384 } };
     sal_Int32 const nCellTxtLns[3][4] = { { 1, 0, 0, 0 },{ 1, 0, 0, 0},{ 1, 1, 1, 1 } };
     // Table was distorted because of missing sprmPFInnerTableCell at paragraph marks (0x0D) with sprmPFInnerTtp

commit 7d74d6ebd2d742cf9e73249830360ce839a8e241

tdf#135682 sw: fix lost selection-all when doc starts with table
Regression from commit cbca89511546fb2c32f07166f8f1f9ec78e30940
(tdf#105330 sw: fix lost cursor on undoing nested table insert,
2019-09-16), the problem was that the change reverted lcl_notifyRow()
back to its original state, because it seemed the conditional
notification is no longer needed. However, this broke the fix for
tdf#37606 (ability to select-all when the doc starts with a table).

Fix the problem by handling the starts-with-table case similar to a
normal table selection, so there is still no need to restore the nested
table visitor code but select-all works nicely with starts-with-table
documents again.

Change-Id: Icb823a39432d1774a63a0c633c172bba827ac76d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102694
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/crsr/callnk.cxx
@@ -59,7 +59,7 @@ SwCallLink::SwCallLink( SwCursorShell & rSh )
     }
 }
 
-static void lcl_notifyRow(const SwContentNode* pNode, SwCursorShell const & rShell)
+static void lcl_notifyRow(const SwContentNode* pNode, SwCursorShell & rShell)
 {
     if ( !pNode )
         return;
@@ -76,11 +76,12 @@ static void lcl_notifyRow(const SwContentNode* pNode, SwCursorShell const & rShe
 
     const SwTableLine* pLine = pRow->GetTabLine( );
 
-    if (rShell.IsTableMode())
+    if (rShell.IsTableMode() || (rShell.StartsWithTable() && rShell.ExtendedSelectedAll()))
     {
         // If we have a table selection, then avoid the notification: it's not necessary (the text
         // cursor needs no updating) and the notification may kill the selection overlay, leading to
         // flicker.
+        // Same for whole-document selection when it starts with a table.
         return;
     }
 

sw/source/core/doc/docfmt.cxx
@@ -1955,6 +1955,7 @@ void SwDoc::dumpAsXml(xmlTextWriterPtr pWriter) const
     mpFrameFormatTable->dumpAsXml(pWriter, "frmFormatTable");
     mpSpzFrameFormatTable->dumpAsXml(pWriter, "spzFrameFormatTable");
     mpSectionFormatTable->dumpAsXml(pWriter);
+    mpTableFrameFormatTable->dumpAsXml(pWriter, "tableFrameFormatTable");
     mpNumRuleTable->dumpAsXml(pWriter);
     getIDocumentRedlineAccess().GetRedlineTable().dumpAsXml(pWriter);
     getIDocumentRedlineAccess().GetExtraRedlineTable().dumpAsXml(pWriter);

sw/qa/core/crsr/crsr.cxx
@@ -18,6 +18,13 @@
 
 #include <comphelper/propertysequence.hxx>
 #include <svl/srchitem.hxx>
+#include <vcl/scheduler.hxx>
+
+#include <docsh.hxx>
+#include <unotxdoc.hxx>
+#include <wrtsh.hxx>
+
+char const DATA_DIRECTORY[] = "/sw/qa/core/crsr/data/";
 
 /// Covers sw/source/core/crsr/ fixes.
 class SwCoreCrsrTest : public SwModelTestBase
@@ -73,6 +80,28 @@ CPPUNIT_TEST_FIXTURE(SwCoreCrsrTest, testFindReplace)
     // - Actual  : foo
     // i.e. the foo on the second line was not replaced.
     CPPUNIT_ASSERT_EQUAL(OUString("bar"), aActualStart);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreCrsrTest, testSelAllStartsWithTable)
+{
+    load(DATA_DIRECTORY, "sel-all-starts-with-table.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    SwDoc* pDoc = pDocShell->GetDoc();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDoc->GetTableFrameFormatCount(/*bUsed=*/true));
+
+    pWrtShell->SelAll();
+    pWrtShell->SelAll();
+    Scheduler::ProcessEventsToIdle();
+    pWrtShell->DelLeft();
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 0
+    // - Actual  : 1
+    // i.e. the table selection was lost and the table was not deleted.
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), pDoc->GetTableFrameFormatCount(/*bUsed=*/true));
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();