LemmingAvalanche/libreoffice-repaired

commit 3a30a837b73fb18175d3846f2617106bdba179e6

tdf#135094 cairo canvas: fix black slide containing a very small image
Don't paint when the area would be 0, that would not be visible anyway,
and the _cairo_matrix_to_pixman_matrix_offset() call would fail with
CAIRO_INT_STATUS_INVALID_MATRIX in _pixman_image_set_properties(),
failing the render of the whole slide.

Also, warn in case the painting fails, so the next time something
breaks, it's easier to find the problematic place.

[ No testcase, our tests are typically headless and currently
SvpSalGraphics::SupportsCairo() reports false, so this would be tricky
to test. ]

Change-Id: I7cdb9462ff8155232ea51abf321b365c2219575b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100743
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

canvas/source/cairo/cairo_canvashelper.cxx
@@ -1177,10 +1177,23 @@ namespace cairocanvas
             cairo_rectangle( mpCairo.get(), 0, 0, aBitmapSize.Width, aBitmapSize.Height );
             cairo_clip( mpCairo.get() );
 
-            if( bModulateColors )
-                cairo_paint_with_alpha( mpCairo.get(), renderState.DeviceColor[3] );
-            else
-                cairo_paint( mpCairo.get() );
+            int nPixelWidth = std::round(rSize.Width * aMatrix.xx);
+            int nPixelHeight = std::round(rSize.Height * aMatrix.yy);
+            if (nPixelWidth > 0 && nPixelHeight > 0)
+            {
+                // Only render the image if it's at least 1x1 px sized.
+                if (bModulateColors)
+                    cairo_paint_with_alpha(mpCairo.get(), renderState.DeviceColor[3]);
+                else
+                {
+                    cairo_paint(mpCairo.get());
+                    if (cairo_status(mpCairo.get()) != CAIRO_STATUS_SUCCESS)
+                    {
+                        SAL_WARN("canvas.cairo", "cairo_paint() failed: " << cairo_status_to_string(
+                                                     cairo_status(mpCairo.get())));
+                    }
+                }
+            }
             cairo_restore( mpCairo.get() );
         }
         else

commit 35d68f83b1736ebb9b86496b5c0a788009d4ffcd

tdf#136337 cairo canvas: fix missing image with negative height
Regression from commit 3a30a837b73fb18175d3846f2617106bdba179e6
(tdf#135094 cairo canvas: fix black slide containing a very small image,
2020-08-14), we try to predict when cairo would end up in an invalid
state due to an invalid transformation matrix, but in this case we were
too aggressive, so the image was missing while presenting.

Fix the problem by allowing negative sizes, just require that neither of
width/height is zero, because negative values are poor, but valid way of
vertical/horizontal flip.

[ No testcase, our tests are typically headless and currently
SvpSalGraphics::SupportsCairo() reports false, so this would be tricky
to test. ]

Change-Id: Iaf843c0d40c108d5221c9b94b39d617e4d50f65c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103127
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

canvas/source/cairo/cairo_canvashelper.cxx
@@ -1179,7 +1179,7 @@ namespace cairocanvas
 
             int nPixelWidth = std::round(rSize.Width * aMatrix.xx);
             int nPixelHeight = std::round(rSize.Height * aMatrix.yy);
-            if (nPixelWidth > 0 && nPixelHeight > 0)
+            if (std::abs(nPixelWidth) > 0 && std::abs(nPixelHeight) > 0)
             {
                 // Only render the image if it's at least 1x1 px sized.
                 if (bModulateColors)