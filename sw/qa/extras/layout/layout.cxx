LemmingAvalanche/libreoffice-repaired

commit 712b0cd7f7425e779c58af9d736a752961e81010

tdf#112290 sw: fix missing line break in small capital
Calculated hyphenation positions are skipped in the case
small capital text, resulting long lines exceeding paragraph
width, for example after wide images.

Likely the reason of the problem is tdf#78527 "FORMATTING:
automatic hyphenation doesn't work on text in small caps or
all caps".

Change-Id: Ia0121728bcc099f3ec19e01afd64e1594d6274be
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99992
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/portxt.cxx
@@ -344,7 +344,8 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
         Insert( aGuess.ReleaseHangingPortion() );
     }
     // breakPos >= index
-    else if (aGuess.BreakPos() >= rInf.GetIdx() && aGuess.BreakPos() != TextFrameIndex(COMPLETE_STRING))
+    else if (aGuess.BreakPos() >= rInf.GetIdx() && aGuess.BreakPos() != TextFrameIndex(COMPLETE_STRING)
+        && rInf.GetFont()->GetCaseMap() != SvxCaseMap::SmallCaps )
     {
         // case B1
         if( aGuess.HyphWord().is() && aGuess.BreakPos() > rInf.GetLineStart()

sw/qa/extras/layout/layout.cxx
@@ -3474,6 +3474,14 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf115094)
     CPPUNIT_ASSERT_LESS(nTopOfB2Anchored, nTopOfB2);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf112290)
+{
+    SwDoc* pDoc = createDoc("tdf112290.docx");
+    CPPUNIT_ASSERT(pDoc);
+    auto pXml = parseLayoutDump();
+    assertXPath(pXml, "/root/page/body/txt/LineBreak[2]", "Line", "Xxxx Xxxx");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf122607)
 {
     createDoc("tdf122607.odt");

commit dd857a9d7a3190c79f2fbe1decac64f476479c95

tdf#136588 sw: fix line break regression with small caps
Regression from commit 712b0cd7f7425e779c58af9d736a752961e81010
(tdf#112290 sw: fix missing line break in small capital).

This replaces the previous commit with a better solution.

Change-Id: I7510b78cd6520f260e6a3804ec69e0364b93440f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103178
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/portxt.cxx
@@ -344,8 +344,7 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
         Insert( aGuess.ReleaseHangingPortion() );
     }
     // breakPos >= index
-    else if (aGuess.BreakPos() >= rInf.GetIdx() && aGuess.BreakPos() != TextFrameIndex(COMPLETE_STRING)
-        && rInf.GetFont()->GetCaseMap() != SvxCaseMap::SmallCaps )
+    else if (aGuess.BreakPos() >= rInf.GetIdx() && aGuess.BreakPos() != TextFrameIndex(COMPLETE_STRING))
     {
         // case B1
         if( aGuess.HyphWord().is() && aGuess.BreakPos() > rInf.GetLineStart()

sw/source/core/txtnode/fntcache.cxx
@@ -2536,6 +2536,13 @@ TextFrameIndex SwFont::GetTextBreak(SwDrawTextInfo const & rInf, long nTextWidth
             ? TextFrameIndex(COMPLETE_STRING)
             : nTextBreak;
 
+    // tdf112290 tdf136588 Break the line correctly only if there is an image inline,
+    // and the image wider than the line...
+    if (GetCaseMap() == SvxCaseMap::SmallCaps && TextFrameIndex(COMPLETE_STRING) == nTextBreak2 &&
+        ! bCompress && nTextWidth == 0)
+        // If nTextWidth == 0 means the line is full, we have to break it
+        nTextBreak2 = TextFrameIndex(1);
+
     if ( ! bCompress )
         return nTextBreak2;
 

sw/qa/extras/layout/layout.cxx
@@ -192,6 +192,17 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testRedlineFootnotes)
     CheckRedlineFootnotesHidden();
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TestTdf136588)
+{
+    load(DATA_DIRECTORY, "tdf136588.docx");
+    auto pXMLLayoutDump = parseLayoutDump();
+    CPPUNIT_ASSERT(pXMLLayoutDump);
+
+    //there was a bad line break before, the correct break layout is this:
+    assertXPath(pXMLLayoutDump, "/root/page/body/txt[2]/LineBreak[2]", "Line",
+                "effectively by modern-day small to ");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testRedlineFlysInBody)
 {
     loadURL("private:factory/swriter", nullptr);