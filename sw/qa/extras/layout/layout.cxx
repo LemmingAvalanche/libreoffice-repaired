LemmingAvalanche/libreoffice-repaired

commit 5f120cf4af5208b2be8b1e418a8f0105956fb968

tdf#136752 pie chart: improve data label position
to avoid chart distortion.

Set the maximum text width of data point label shape based
to the 80% of the remaining space, when text wrapping is enabled
and the label placement is AVOID_OVERLAP.

Change-Id: I174089ec62df3aa21b5d1f26f74d475945d83684
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102744
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

chart2/source/view/charttypes/PieChart.cxx
@@ -32,6 +32,7 @@
 
 #include <com/sun/star/container/XChild.hpp>
 #include <com/sun/star/drawing/XShape.hpp>
+#include <com/sun/star/drawing/XShapes.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <rtl/math.hxx>
 #include <sal/log.hxx>
@@ -382,15 +383,51 @@ void PieChart::createTextLabelShape(
 
     aPieLabelInfo.xLabelGroupShape.set( xChild->getParent(), uno::UNO_QUERY );
 
+    if (bMovementAllowed && !m_bUseRings)
+    {
+        /** Handle the placement of the label in the best fit case.
+         *  First off the routine try to place the label inside the related pie slice,
+         *  if this is not possible the label is placed outside.
+         */
+        if (!performLabelBestFitInnerPlacement(rParam, aPieLabelInfo)
+            && m_aAvailableOuterRect.getWidth())
+        {
+            double fAngleDegree
+                = rParam.mfUnitCircleStartAngleDegree + rParam.mfUnitCircleWidthAngleDegree / 2.0;
+            while (fAngleDegree > 360.0)
+                fAngleDegree -= 360.0;
+            while (fAngleDegree < 0.0)
+                fAngleDegree += 360.0;
+
+            if (fAngleDegree < 67.5 || fAngleDegree >= 292.5)
+                fTextMaximumFrameWidth
+                    = 0.8 * (m_aAvailableOuterRect.getMaxX() - aPieLabelInfo.aFirstPosition.getX());
+            else if (fAngleDegree < 112.5 || fAngleDegree >= 247.5)
+                fTextMaximumFrameWidth = 0.8 * m_aAvailableOuterRect.getWidth();
+            else
+                fTextMaximumFrameWidth
+                    = 0.8 * (aPieLabelInfo.aFirstPosition.getX() - m_aAvailableOuterRect.getMinX());
+
+            nTextMaximumFrameWidth = ceil(fTextMaximumFrameWidth);
+            uno::Reference<drawing::XShapes> xShapes(xChild->getParent(), uno::UNO_QUERY);
+            xShapes->remove(aPieLabelInfo.xTextShape);
+            aPieLabelInfo.xTextShape
+                = createDataLabel(xTextTarget, rSeries, nPointIndex, nVal, rParam.mfLogicYSum,
+                                  aScreenPosition2D, eAlignment, 0, nTextMaximumFrameWidth);
+            xChild.clear();
+            xChild.set(uno::Reference<container::XChild>(aPieLabelInfo.xTextShape, uno::UNO_QUERY));
+            if (!xChild.is())
+                return;
+
+            aPieLabelInfo.xLabelGroupShape.set(xChild->getParent(), uno::UNO_QUERY);
+            performLabelBestFitOuterPlacement(rParam, aPieLabelInfo);
+        }
+    }
+
     aPieLabelInfo.fValue = nVal;
     aPieLabelInfo.bMovementAllowed = bMovementAllowed;
-    aPieLabelInfo.bMoved= false;
+    aPieLabelInfo.bMoved = false;
     aPieLabelInfo.xTextTarget = xTextTarget;
-
-    if (bMovementAllowed)
-    {
-        performLabelBestFit(rParam, aPieLabelInfo);
-    }
 
     m_aLabelInfoList.push_back(aPieLabelInfo);
 }
@@ -1571,25 +1608,16 @@ bool PieChart::performLabelBestFitInnerPlacement(ShapeParam& rShapeParam, PieLab
     return true;
 }
 
-/** Handle the placement of the label in the best fit case.
- *  First off the routine try to place the label inside the related pie slice,
- *  if this is not possible the label is placed outside.
- */
-void PieChart::performLabelBestFit(ShapeParam& rShapeParam, PieLabelInfo const & rPieLabelInfo)
+void PieChart::performLabelBestFitOuterPlacement(ShapeParam& rShapeParam,
+                                                 PieLabelInfo const& rPieLabelInfo)
 {
-    if( m_bUseRings )
-        return;
-
-    if( performLabelBestFitInnerPlacement(rShapeParam, rPieLabelInfo) )
-        return;
-
-    // If it does not fit inside, let's put it outside
     awt::Point aOldPos(rPieLabelInfo.xLabelGroupShape->getPosition());
     basegfx::B2IVector aTranslationVector = rPieLabelInfo.aFirstPosition - rPieLabelInfo.aOrigin;
     awt::Point aScreenPosition2D(aOldPos.X + aTranslationVector.getX(),
-        aOldPos.Y + aTranslationVector.getY());
+                                 aOldPos.Y + aTranslationVector.getY());
 
-    double fAngleDegree = rShapeParam.mfUnitCircleStartAngleDegree + rShapeParam.mfUnitCircleWidthAngleDegree / 2.0;
+    double fAngleDegree
+        = rShapeParam.mfUnitCircleStartAngleDegree + rShapeParam.mfUnitCircleWidthAngleDegree / 2.0;
     ::basegfx::B2IRectangle aBb(lcl_getRect(rPieLabelInfo.xLabelGroupShape));
     double fLabelWidth = aBb.getWidth();
     double fLabelHeight = aBb.getHeight();

chart2/source/view/charttypes/PieChart.hxx
@@ -105,8 +105,10 @@ struct PieLabelInfo;
                                 , PieLabelInfo* pCenter, bool bSingleCenter, bool& rbAlternativeMoveDirection
                                 , const css::awt::Size& rPageSize );
 
-    bool                performLabelBestFitInnerPlacement(ShapeParam& rShapeParam, PieLabelInfo const & rPieLabelInfo);
-    void                performLabelBestFit(ShapeParam& rShapeParam, PieLabelInfo const & rPieLabelInfo);
+    bool                performLabelBestFitInnerPlacement( ShapeParam& rShapeParam
+                                , PieLabelInfo const & rPieLabelInfo );
+    static void         performLabelBestFitOuterPlacement( ShapeParam& rShapeParam
+                                , PieLabelInfo const & rPieLabelInfo );
 
 private: //member
     std::unique_ptr<PiePositionHelper>

chart2/qa/extras/chart2import.cxx
@@ -165,6 +165,7 @@ public:
     void testTdf136105();
     void testTdf91250();
     void testTdf134111();
+    void testTdf136752();
 
     CPPUNIT_TEST_SUITE(Chart2ImportTest);
     CPPUNIT_TEST(Fdo60083);
@@ -278,6 +279,7 @@ public:
     CPPUNIT_TEST(testTdf136105);
     CPPUNIT_TEST(testTdf91250);
     CPPUNIT_TEST(testTdf134111);
+    CPPUNIT_TEST(testTdf136752);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2550,8 +2552,8 @@ void Chart2ImportTest::testTdf133376()
     CPPUNIT_ASSERT(xDataPointLabel.is());
     // Check the position of the 3rd data point label, which is out from the pie slice
     awt::Point aLabelPosition = xDataPointLabel->getPosition();
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(482, aLabelPosition.X, 30);
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(5427, aLabelPosition.Y, 30);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(349, aLabelPosition.X, 30);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(5480, aLabelPosition.Y, 30);
 }
 
 void Chart2ImportTest::testTdf134225()
@@ -2656,6 +2658,25 @@ void Chart2ImportTest::testTdf134111()
     xAxisProp->getPropertyValue("TextBreak") >>= bTextBreak;
     // Expected value of 'TextBreak' is true
     CPPUNIT_ASSERT(bTextBreak);
+}
+
+void Chart2ImportTest::testTdf136752()
+{
+    load("/chart2/qa/extras/data/xlsx/", "tdf136752.xlsx");
+    Reference<chart::XChartDocument> xChartDoc(getChartDocFromSheet(0, mxComponent),
+                                               UNO_QUERY_THROW);
+
+    Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(xChartDoc, UNO_QUERY_THROW);
+    Reference<drawing::XDrawPage> xDrawPage(xDrawPageSupplier->getDrawPage(), UNO_SET_THROW);
+    Reference<drawing::XShapes> xShapes(xDrawPage->getByIndex(0), UNO_QUERY_THROW);
+    Reference<drawing::XShape> xDataPointLabel(getShapeByName(xShapes,
+        "CID/MultiClick/CID/D=0:CS=0:CT=0:Series=0:DataLabels=:DataLabel=0"), UNO_SET_THROW);
+
+    CPPUNIT_ASSERT(xDataPointLabel.is());
+    // Check the position of the 1st data point label, which is out from the pie slice
+    awt::Point aLabelPosition = xDataPointLabel->getPosition();
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(8675, aLabelPosition.X, 500);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(1458, aLabelPosition.Y, 500);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Chart2ImportTest);

commit d90c754160bfc5e6d6a9fcfe1e6bcf2c770101f9

tdf#137116 pie chart: improve BestFit position of data labels
Regression from commit: 5f120cf4af5208b2be8b1e418a8f0105956fb968
(tdf#136752 pie chart: improve data label position)

Change-Id: I66943684957a220c81db7f928fbb47700b78d012
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103610
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

chart2/source/view/charttypes/PieChart.cxx
@@ -389,26 +389,51 @@ void PieChart::createTextLabelShape(
          *  First off the routine try to place the label inside the related pie slice,
          *  if this is not possible the label is placed outside.
          */
-        if (!performLabelBestFitInnerPlacement(rParam, aPieLabelInfo)
-            && m_aAvailableOuterRect.getWidth())
+        if (!performLabelBestFitInnerPlacement(rParam, aPieLabelInfo))
         {
-            double fAngleDegree
-                = rParam.mfUnitCircleStartAngleDegree + rParam.mfUnitCircleWidthAngleDegree / 2.0;
-            while (fAngleDegree > 360.0)
-                fAngleDegree -= 360.0;
-            while (fAngleDegree < 0.0)
-                fAngleDegree += 360.0;
+            if (m_aAvailableOuterRect.getWidth())
+            {
+                double fAngleDegree = rParam.mfUnitCircleStartAngleDegree
+                                      + rParam.mfUnitCircleWidthAngleDegree / 2.0;
+                while (fAngleDegree > 360.0)
+                    fAngleDegree -= 360.0;
+                while (fAngleDegree < 0.0)
+                    fAngleDegree += 360.0;
 
-            if (fAngleDegree < 67.5 || fAngleDegree >= 292.5)
-                fTextMaximumFrameWidth
-                    = 0.8 * (m_aAvailableOuterRect.getMaxX() - aPieLabelInfo.aFirstPosition.getX());
-            else if (fAngleDegree < 112.5 || fAngleDegree >= 247.5)
-                fTextMaximumFrameWidth = 0.8 * m_aAvailableOuterRect.getWidth();
-            else
-                fTextMaximumFrameWidth
-                    = 0.8 * (aPieLabelInfo.aFirstPosition.getX() - m_aAvailableOuterRect.getMinX());
+                if (fAngleDegree < 67.5 || fAngleDegree >= 292.5)
+                    fTextMaximumFrameWidth
+                        = 0.8
+                          * (m_aAvailableOuterRect.getMaxX() - aPieLabelInfo.aFirstPosition.getX());
+                else if (fAngleDegree < 112.5 || fAngleDegree >= 247.5)
+                    fTextMaximumFrameWidth = 0.8 * m_aAvailableOuterRect.getWidth();
+                else
+                    fTextMaximumFrameWidth
+                        = 0.8
+                          * (aPieLabelInfo.aFirstPosition.getX() - m_aAvailableOuterRect.getMinX());
 
-            nTextMaximumFrameWidth = ceil(fTextMaximumFrameWidth);
+                nTextMaximumFrameWidth = ceil(fTextMaximumFrameWidth);
+            }
+
+            nScreenValueOffsetInRadiusDirection = (m_nDimension != 3) ? 150 : 0;
+            aScreenPosition2D
+                = aPolarPosHelper.getLabelScreenPositionAndAlignmentForUnitCircleValues(
+                    eAlignment, css::chart::DataLabelPlacement::OUTSIDE,
+                    rParam.mfUnitCircleStartAngleDegree,
+                    rParam.mfUnitCircleWidthAngleDegree, rParam.mfUnitCircleInnerRadius,
+                    rParam.mfUnitCircleOuterRadius, rParam.mfLogicZ + 0.5, 0);
+            aPieLabelInfo.aFirstPosition
+                = basegfx::B2IVector(aScreenPosition2D.X, aScreenPosition2D.Y);
+
+            //add a scaling independent Offset if requested
+            if (nScreenValueOffsetInRadiusDirection != 0)
+            {
+                basegfx::B2IVector aDirection(aScreenPosition2D.X - aOrigin.X,
+                                              aScreenPosition2D.Y - aOrigin.Y);
+                aDirection.setLength(nScreenValueOffsetInRadiusDirection);
+                aScreenPosition2D.X += aDirection.getX();
+                aScreenPosition2D.Y += aDirection.getY();
+            }
+
             uno::Reference<drawing::XShapes> xShapes(xChild->getParent(), uno::UNO_QUERY);
             xShapes->remove(aPieLabelInfo.xTextShape);
             aPieLabelInfo.xTextShape
@@ -420,7 +445,6 @@ void PieChart::createTextLabelShape(
                 return;
 
             aPieLabelInfo.xLabelGroupShape.set(xChild->getParent(), uno::UNO_QUERY);
-            performLabelBestFitOuterPlacement(rParam, aPieLabelInfo);
         }
     }
 
@@ -1606,57 +1630,6 @@ bool PieChart::performLabelBestFitInnerPlacement(ShapeParam& rShapeParam, PieLab
               "      new position = (" << aNewPos.X << "," << aNewPos.Y << ")" );
 
     return true;
-}
-
-void PieChart::performLabelBestFitOuterPlacement(ShapeParam& rShapeParam,
-                                                 PieLabelInfo const& rPieLabelInfo)
-{
-    awt::Point aOldPos(rPieLabelInfo.xLabelGroupShape->getPosition());
-    basegfx::B2IVector aTranslationVector = rPieLabelInfo.aFirstPosition - rPieLabelInfo.aOrigin;
-    awt::Point aScreenPosition2D(aOldPos.X + aTranslationVector.getX(),
-                                 aOldPos.Y + aTranslationVector.getY());
-
-    double fAngleDegree
-        = rShapeParam.mfUnitCircleStartAngleDegree + rShapeParam.mfUnitCircleWidthAngleDegree / 2.0;
-    ::basegfx::B2IRectangle aBb(lcl_getRect(rPieLabelInfo.xLabelGroupShape));
-    double fLabelWidth = aBb.getWidth();
-    double fLabelHeight = aBb.getHeight();
-
-    while (fAngleDegree > 360.0)
-        fAngleDegree -= 360.0;
-    while (fAngleDegree < 0.0)
-        fAngleDegree += 360.0;
-
-    if (fAngleDegree <= 22.5 || fAngleDegree >= 337.5)
-        aScreenPosition2D.X += fLabelWidth / 2;
-    else if (fAngleDegree < 67.5)
-    {
-        aScreenPosition2D.X += fLabelWidth / 2;
-        aScreenPosition2D.Y -= fLabelHeight / 2;
-    }
-    else if (fAngleDegree < 112.5)
-        aScreenPosition2D.Y -= fLabelHeight / 2;
-    else if (fAngleDegree <= 157.5)
-    {
-        aScreenPosition2D.X -= fLabelWidth / 2;
-        aScreenPosition2D.Y -= fLabelHeight / 2;
-    }
-    else if (fAngleDegree <= 202.5)
-        aScreenPosition2D.X -= fLabelWidth / 2;
-    else if (fAngleDegree < 247.5)
-    {
-        aScreenPosition2D.X -= fLabelWidth / 2;
-        aScreenPosition2D.Y += fLabelHeight / 2;
-    }
-    else if (fAngleDegree < 292.5)
-        aScreenPosition2D.Y += fLabelHeight / 2;
-    else
-    {
-        aScreenPosition2D.X += fLabelWidth / 2;
-        aScreenPosition2D.Y += fLabelHeight / 2;
-    }
-
-    rPieLabelInfo.xLabelGroupShape->setPosition(aScreenPosition2D);
 }
 
 } //namespace chart

chart2/source/view/charttypes/PieChart.hxx
@@ -107,8 +107,6 @@ struct PieLabelInfo;
 
     bool                performLabelBestFitInnerPlacement( ShapeParam& rShapeParam
                                 , PieLabelInfo const & rPieLabelInfo );
-    static void         performLabelBestFitOuterPlacement( ShapeParam& rShapeParam
-                                , PieLabelInfo const & rPieLabelInfo );
 
 private: //member
     std::unique_ptr<PiePositionHelper>

chart2/qa/extras/chart2import.cxx
@@ -2579,8 +2579,8 @@ void Chart2ImportTest::testTdf134225()
     awt::Point aLabelPosition2 = xDataPointLabel2->getPosition();
 
     // Check the distance between the position of the 1st data point label and the second one
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(1800, sal_Int32(aLabelPosition2.X - aLabelPosition1.X), 30);
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(2123, sal_Int32(aLabelPosition2.Y - aLabelPosition1.Y), 30);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(1669, sal_Int32(aLabelPosition2.X - aLabelPosition1.X), 30);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2166, sal_Int32(aLabelPosition2.Y - aLabelPosition1.Y), 30);
 #endif
 }
 

sw/qa/extras/layout/layout.cxx
@@ -2923,6 +2923,26 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf134866)
         pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/textarray[2]/text", "100%");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf137116)
+{
+    SwDoc* pDoc = createDoc("tdf137116.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    sal_Int32 nX2 = getXPath(pXmlDoc, "//textarray[2]", "x").toInt32(); // second data label
+    sal_Int32 nX4 = getXPath(pXmlDoc, "//textarray[4]", "x").toInt32(); // fourth data label
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 694
+    // - Actual  : -225
+    // - Delta   : 100
+    // i.e. the second data label appeared inside the pie slice.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(694, nX2 - nX4, 100);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf130031)
 {
     SwDoc* pDoc = createDoc("tdf130031.docx");