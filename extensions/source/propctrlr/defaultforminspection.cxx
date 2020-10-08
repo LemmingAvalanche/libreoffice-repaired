LemmingAvalanche/libreoffice-repaired

commit 301d23df155f7d6b452a5fccad1d07d85825c5e2

loplugin:const* make some params and methods const
Change-Id: I97c5bbb929a2a4a029af4e6cb0fd571bbc2b698b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104030
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

extensions/source/propctrlr/defaultforminspection.cxx
@@ -207,7 +207,7 @@ namespace pcr
 
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
 extensions_propctrlr_DefaultFormComponentInspectorModel_get_implementation(
-    css::uno::XComponentContext* context , css::uno::Sequence<css::uno::Any> const&)
+    const css::uno::XComponentContext* context , css::uno::Sequence<css::uno::Any> const&)
 {
     return cppu::acquire(new pcr::DefaultFormComponentInspectorModel(context));
 }

desktop/source/lib/init.cxx
@@ -715,7 +715,7 @@ std::string extractPrivateKey(const std::string & privateKey)
     return privateKey.substr(pos1, pos2);
 }
 
-OUString lcl_getCurrentDocumentMimeType(LibLODocument_Impl* pDocument)
+OUString lcl_getCurrentDocumentMimeType(const LibLODocument_Impl* pDocument)
 {
     SfxBaseModel* pBaseModel = dynamic_cast<SfxBaseModel*>(pDocument->mxComponent.get());
     if (!pBaseModel)

extensions/source/propctrlr/browserview.cxx
@@ -27,7 +27,7 @@ namespace pcr
     using namespace ::com::sun::star::uno;
     using namespace ::com::sun::star::lang;
 
-    OPropertyBrowserView::OPropertyBrowserView(css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder)
+    OPropertyBrowserView::OPropertyBrowserView(const css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder)
         : m_xPropBox(new OPropertyEditor(rContext, rBuilder))
         , m_nActivePage(0)
     {

extensions/source/propctrlr/browserview.hxx
@@ -35,7 +35,7 @@ namespace pcr
         Link<LinkParamNone*,void>   m_aPageActivationHandler;
 
     public:
-        explicit OPropertyBrowserView(css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder);
+        explicit OPropertyBrowserView(const css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder);
         ~OPropertyBrowserView();
 
         OPropertyEditor&    getPropertyBox() { return *m_xPropBox; }

extensions/source/propctrlr/handlerhelper.cxx
@@ -296,14 +296,14 @@ namespace pcr
         return std::unique_ptr<weld::Builder>(Application::CreateBuilder(rTunnel.getWidget(), rUIFile));
     }
 
-    void PropertyHandlerHelper::setBuilderParent(css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Widget* pParent)
+    void PropertyHandlerHelper::setBuilderParent(const css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Widget* pParent)
     {
         Reference<css::container::XNameContainer> xName(rContext, UNO_QUERY_THROW);
         Reference<XWindow> xWindow(new weld::TransportAsXWindow(pParent));
         xName->insertByName("BuilderParent", makeAny(xWindow));
     }
 
-    void PropertyHandlerHelper::clearBuilderParent(css::uno::Reference<css::uno::XComponentContext>& rContext)
+    void PropertyHandlerHelper::clearBuilderParent(const css::uno::Reference<css::uno::XComponentContext>& rContext)
     {
         Reference<css::container::XNameContainer> xName(rContext, UNO_QUERY_THROW);
         xName->removeByName("BuilderParent");

extensions/source/propctrlr/handlerhelper.hxx
@@ -214,9 +214,9 @@ namespace pcr
 
         static std::unique_ptr<weld::Builder> makeBuilder(const OUString& rUIFile, const css::uno::Reference<css::uno::XComponentContext>& rContext);
 
-        static void setBuilderParent(css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Widget* pParent);
+        static void setBuilderParent(const css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Widget* pParent);
 
-        static void clearBuilderParent(css::uno::Reference<css::uno::XComponentContext>& rContext);
+        static void clearBuilderParent(const css::uno::Reference<css::uno::XComponentContext>& rContext);
 
     private:
         PropertyHandlerHelper( const PropertyHandlerHelper& ) = delete;

extensions/source/propctrlr/propertyeditor.cxx
@@ -31,7 +31,7 @@ namespace pcr
     using ::com::sun::star::inspection::XPropertyControl;
     using ::com::sun::star::uno::Reference;
 
-    OPropertyEditor::OPropertyEditor(css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder)
+    OPropertyEditor::OPropertyEditor(const css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder)
         : m_xContainer(rBuilder.weld_container("box"))
         , m_xTabControl(rBuilder.weld_notebook("tabcontrol"))
         , m_xControlHoldingParent(rBuilder.weld_container("controlparent")) // controls initially have this parent before they are moved

extensions/source/propctrlr/propertyeditor.hxx
@@ -66,7 +66,7 @@ namespace pcr
         std::map<sal_uInt16, PropertyPage> m_aHiddenPages;
 
     public:
-        explicit OPropertyEditor(css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder);
+        explicit OPropertyEditor(const css::uno::Reference<css::uno::XComponentContext>& rContext, weld::Builder& rBuilder);
         ~OPropertyEditor();
 
         void                        SetLineListener( IPropertyLineListener* );

commit b69efbfd8eb8083abadb3e943337e94cd4d0d2ba

Fix C++ UNO component constructor function signature
...broken by 301d23df155f7d6b452a5fccad1d07d85825c5e2 "loplugin:const* make some
params and methods const", and reported by UBSan during CppunitTest_services:

> ~/gcc/trunk/inst/lib/gcc/x86_64-pc-linux-gnu/11.0.0/../../../../include/c++/11.0.0/bits/invoke.h:60:14: runtime error: call to function extensions_propctrlr_DefaultFormComponentInspectorModel_get_implementation through pointer to incorrect function type 'com::sun::star::uno::XInterface *(*)(com::sun::star::uno::XComponentContext *, const com::sun::star::uno::Sequence<com::sun::star::uno::Any> &)'
> extensions/source/propctrlr/defaultforminspection.cxx:211: note: extensions_propctrlr_DefaultFormComponentInspectorModel_get_implementation defined here

Change-Id: Ic9b8df736169c478fa1184fc1bb30ae992ce1cff
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104086
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

extensions/source/propctrlr/defaultforminspection.cxx
@@ -207,7 +207,7 @@ namespace pcr
 
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
 extensions_propctrlr_DefaultFormComponentInspectorModel_get_implementation(
-    const css::uno::XComponentContext* context , css::uno::Sequence<css::uno::Any> const&)
+    css::uno::XComponentContext* context , css::uno::Sequence<css::uno::Any> const&)
 {
     return cppu::acquire(new pcr::DefaultFormComponentInspectorModel(context));
 }