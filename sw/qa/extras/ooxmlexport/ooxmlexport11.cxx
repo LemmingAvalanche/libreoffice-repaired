LemmingAvalanche/libreoffice-repaired

commit 2a39664367e0a33a797d2f7e4a8505ba229ac2fd

tdf#118521 DOCX import: fix paragraph margin from paragraph style
when the other (top or bottom) margin was set by direct formatting.

Change-Id: Ic4038397329374fb2d6cf974751310f7dd8e8b31
Reviewed-on: https://gerrit.libreoffice.org/57585
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1415,6 +1415,31 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap )
                                  uno::makeAny( ConversionHelper::convertTwipToMM100(nSize)));
                     }
                 }
+
+                // tdf#118521 set paragraph top or bottom margin based on the paragraph style
+                // if we already set the other margin with direct formatting
+                if (pParaContext && m_xPreviousParagraph.is() &&
+                        pParaContext->isSet(PROP_PARA_TOP_MARGIN) != pParaContext->isSet(PROP_PARA_BOTTOM_MARGIN))
+                {
+                    const StyleSheetEntryPtr pEntry = GetStyleSheetTable()->FindStyleSheetByConvertedStyleName( GetCurrentParaStyleName() );
+                    OSL_ENSURE( pEntry.get(), "no style sheet found" );
+                    const StyleSheetPropertyMap* pStyleSheetProperties =
+                            dynamic_cast<const StyleSheetPropertyMap*>(pEntry ? pEntry->pProperties.get() : nullptr);
+                    if (pStyleSheetProperties) {
+                        boost::optional<PropertyMap::Property> oProperty;
+                        if (pParaContext->isSet(PROP_PARA_TOP_MARGIN))
+                        {
+                            if ( (oProperty = pStyleSheetProperties->getProperty(PROP_PARA_BOTTOM_MARGIN)) )
+                                m_xPreviousParagraph->setPropertyValue("ParaBottomMargin", oProperty->second);
+                        }
+                        else
+                        {
+                            if ( (oProperty = pStyleSheetProperties->getProperty(PROP_PARA_TOP_MARGIN)) )
+                                m_xPreviousParagraph->setPropertyValue("ParaTopMargin", oProperty->second);
+                        }
+                    }
+                }
+
             }
             if( !bKeepLastParagraphProperties )
                 rAppendContext.pLastParagraphProperties = pToBeSavedProperties;

commit 922242a3d6bf11f426ff319e9e29b2623f9102f0

tdf#136955 DOCX import: fix shape paragraph margins
based on bad style inheritance, in the case of not
first paragraphs of shapes.

Regression from commit 2a39664367e0a33a797d2f7e4a8505ba229ac2fd
(tdf#118521 DOCX import: fix paragraph margin from paragraph style),
revealing the problematic m_sCurrentParaStyleName, see also
commit 7aae3e2fc692620d1a6a22660db9e647d5450dda for more information.

Follow-up of commit 91983c0bdffc1c44d8686dd184fc5b7eef1e3a7c
(tdf#134784 DOCX import: fix shape paragraph margins).

Change-Id: I4feab19fc58653d829d7a1ae8da3e3ce4fb3e2c3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104116
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -785,7 +785,7 @@ OUString DomainMapper_Impl::GetCurrentParaStyleName()
     // use saved currParaStyleName as a fallback, in case no particular para style name applied.
     // tdf#134784 except in the case of first paragraph of shapes to avoid bad fallback.
     // TODO fix this "highly inaccurate" m_sCurrentParaStyleName
-    if ( !m_bIsFirstParaInShape )
+    if ( !IsInShape() )
         sName = m_sCurrentParaStyleName;
 
     PropertyMapPtr pParaContext = GetTopContextOfType(CONTEXT_PARAGRAPH);

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -813,6 +813,17 @@ DECLARE_OOXMLEXPORT_TEST(testTdf134784, "tdf134784.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xParagraph, "ParaTopMargin"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf136955, "tdf134784.docx")
+{
+    uno::Reference<text::XText> textbox(getShape(1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(3, getParagraphs(textbox));
+    uno::Reference<text::XTextRange> xParagraph = getParagraphOfText(2, textbox);
+
+    // These weren't zero (values inherited from style of the previous paragraph in the main text)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xParagraph, "ParaBottomMargin"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xParagraph, "ParaTopMargin"));
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf104348_contextMargin, "tdf104348_contextMargin.docx")
 {
     // tdf#104348 shows that ContextMargin belongs with Top/Bottom handling