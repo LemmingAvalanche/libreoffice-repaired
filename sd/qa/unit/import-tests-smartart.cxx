LemmingAvalanche/libreoffice-repaired

commit 2779646a39f919b7cf76de7d06e60a502dfa2fe1

oox smartart: consider rules when scaling in linear layout
The bugdoc has an arrow shape which is 100% wide, and there are multiple
shapes before it, which also have a 100% wide constraint. The reason
PowerPoint scales down the shapes (but not the arrow) is because rules
declare it should happen this way.

So start taking rules into account in linear layouts.

Change-Id: I352443277e88be0eb711659489587127727a258f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99855
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

oox/source/drawingml/diagram/diagramlayoutatoms.cxx
@@ -19,6 +19,8 @@
 
 #include "diagramlayoutatoms.hxx"
 
+#include <set>
+
 #include "layoutatomvisitorbase.hxx"
 
 #include <basegfx/numeric/ftools.hxx>
@@ -478,10 +480,21 @@ void ApplyConstraintToLayout(const Constraint& rConstraint, LayoutPropertyMap& r
 }
 }
 
-void AlgAtom::layoutShape( const ShapePtr& rShape,
-                           const std::vector<Constraint>& rConstraints,
-                           const std::vector<Rule>& /*rRules*/ )
+void AlgAtom::layoutShape(const ShapePtr& rShape, const std::vector<Constraint>& rConstraints,
+                          const std::vector<Rule>& rRules)
 {
+    if (mnType != XML_lin)
+    {
+        // TODO Handle spacing from constraints for non-lin algorithms as well.
+        rShape->getChildren().erase(
+            std::remove_if(rShape->getChildren().begin(), rShape->getChildren().end(),
+                           [](const ShapePtr& aChild) {
+                               return aChild->getServiceName() == "com.sun.star.drawing.GroupShape"
+                                      && aChild->getChildren().empty();
+                           }),
+            rShape->getChildren().end());
+    }
+
     switch(mnType)
     {
         case XML_composite:
@@ -928,6 +941,45 @@ void AlgAtom::layoutShape( const ShapePtr& rShape,
             }
 
             // first approximation of children size
+            std::set<OUString> aChildrenToShrink;
+            for (const auto& rRule : rRules)
+            {
+                // Consider rules: when scaling down, only change children where the rule allows
+                // doing so.
+                aChildrenToShrink.insert(rRule.msForName);
+            }
+
+            if (!aChildrenToShrink.empty())
+            {
+                // Have scaling info from rules: then only count scaled children.
+                for (auto& aCurrShape : rShape->getChildren())
+                {
+                    if (aChildrenToShrink.find(aCurrShape->getInternalName())
+                        == aChildrenToShrink.end())
+                    {
+                        if (nCount > 1)
+                        {
+                            --nCount;
+                        }
+                    }
+                }
+
+                // No manual spacing: spacings are children as well.
+                aSpaceSize = awt::Size();
+            }
+            else
+            {
+                // TODO Handle spacing from constraints without rules as well.
+                rShape->getChildren().erase(
+                    std::remove_if(rShape->getChildren().begin(), rShape->getChildren().end(),
+                                   [](const ShapePtr& aChild) {
+                                       return aChild->getServiceName()
+                                                  == "com.sun.star.drawing.GroupShape"
+                                              && aChild->getChildren().empty();
+                                   }),
+                    rShape->getChildren().end());
+                nCount = rShape->getChildren().size();
+            }
             awt::Size aChildSize = rShape->getSize();
             if (nDir == XML_fromL || nDir == XML_fromR)
                 aChildSize.Width /= nCount;
@@ -979,8 +1031,18 @@ void AlgAtom::layoutShape( const ShapePtr& rShape,
                     aSize.Width = oWidth.get();
                 if (oHeight.has())
                     aSize.Height = oHeight.get();
-                aSize.Width *= fWidthScale;
-                aSize.Height *= fHeightScale;
+                if (aChildrenToShrink.empty()
+                    || aChildrenToShrink.find(aCurrShape->getInternalName())
+                           != aChildrenToShrink.end())
+                {
+                    aSize.Width *= fWidthScale;
+                }
+                if (aChildrenToShrink.empty()
+                    || aChildrenToShrink.find(aCurrShape->getInternalName())
+                           != aChildrenToShrink.end())
+                {
+                    aSize.Height *= fHeightScale;
+                }
                 aCurrShape->setSize(aSize);
                 aCurrShape->setChildSize(aSize);
 

sd/qa/unit/import-tests-smartart.cxx
@@ -110,6 +110,7 @@ public:
     void testTdf131553();
     void testFillColorList();
     void testTdf134221();
+    void testLinearRule();
 
     CPPUNIT_TEST_SUITE(SdImportTestSmartArt);
 
@@ -157,6 +158,7 @@ public:
     CPPUNIT_TEST(testTdf131553);
     CPPUNIT_TEST(testFillColorList);
     CPPUNIT_TEST(testTdf134221);
+    CPPUNIT_TEST(testLinearRule);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -1521,6 +1523,23 @@ void SdImportTestSmartArt::testTdf134221()
 
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(736),
         xTxtProps->getPropertyValue("TextUpperDistance").get<sal_Int32>());
+
+    xDocShRef->DoClose();
+}
+
+void SdImportTestSmartArt::testLinearRule()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/pptx/smartart-linear-rule.pptx"), PPTX);
+
+    uno::Reference<drawing::XShape> xGroup(getShapeFromPage(0, 0, xDocShRef), uno::UNO_QUERY);
+    // Last child, then again last child.
+    uno::Reference<drawing::XShape> xShape = getChildShape(getChildShape(xGroup, 1), 3);
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected greater than: 17500 (19867)
+    // - Actual  : 4966
+    // i.e. the width of the background arrow was too small.
+    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(17500), xShape->getSize().Width);
 
     xDocShRef->DoClose();
 }

oox/source/drawingml/diagram/diagram.cxx
@@ -82,6 +82,25 @@ static void sortChildrenByZOrder(const ShapePtr& pShape)
         sortChildrenByZOrder(rChild);
 }
 
+/// Removes empty group shapes, now that their spacing influenced the layout.
+static void removeUnneededGroupShapes(const ShapePtr& pShape)
+{
+    std::vector<ShapePtr>& rChildren = pShape->getChildren();
+
+    rChildren.erase(std::remove_if(rChildren.begin(), rChildren.end(),
+                                   [](const ShapePtr& aChild) {
+                                       return aChild->getServiceName()
+                                                  == "com.sun.star.drawing.GroupShape"
+                                              && aChild->getChildren().empty();
+                                   }),
+                    rChildren.end());
+
+    for (const auto& pChild : rChildren)
+    {
+        removeUnneededGroupShapes(pChild);
+    }
+}
+
 void Diagram::addTo( const ShapePtr & pParentShape )
 {
     if (pParentShape->getSize().Width == 0 || pParentShape->getSize().Height == 0)
@@ -103,6 +122,7 @@ void Diagram::addTo( const ShapePtr & pParentShape )
         mpLayout->getNode()->accept(aLayoutingVisitor);
 
         sortChildrenByZOrder(pParentShape);
+        removeUnneededGroupShapes(pParentShape);
     }
 
     ShapePtr pBackground = std::make_shared<Shape>("com.sun.star.drawing.CustomShape");

oox/source/drawingml/diagram/layoutatomvisitors.cxx
@@ -123,12 +123,6 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
     meLookFor = LAYOUT_NODE;
     defaultVisit(rAtom);
 
-    // remove unneeded empty group shapes
-    pCurrParent->getChildren().erase(
-        std::remove_if(pCurrParent->getChildren().begin(), pCurrParent->getChildren().end(),
-            [] (const ShapePtr & aChild) { return aChild->getServiceName() == "com.sun.star.drawing.GroupShape" && aChild->getChildren().empty(); }),
-        pCurrParent->getChildren().end());
-
     meLookFor = ALGORITHM;
     defaultVisit(rAtom);
     meLookFor = LAYOUT_NODE;

commit bc89ffddcc23047572230b646fc165c762787f29

tdf#137417 oox smartart: avoid considering rules in vertical linear layouts
Regression from commit 2779646a39f919b7cf76de7d06e60a502dfa2fe1 (oox
smartart: consider rules when scaling in linear layout, 2020-07-31), the
problem is that I only tested horizontal layouts and this is not working
for vertical layouts.

Just disable the vertical case for now, to avoid unwanted side effects.

Change-Id: I31a894157996a2371b8d0ec482ee91dc4d5b053e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104541
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

oox/source/drawingml/diagram/diagramlayoutatoms.cxx
@@ -1376,6 +1376,12 @@ void AlgAtom::layoutShape(const ShapePtr& rShape, const std::vector<Constraint>&
                 aChildrenToShrink.insert(rRule.msForName);
             }
 
+            if (nDir == XML_fromT || nDir == XML_fromB)
+            {
+                // TODO consider rules for vertical linear layout as well.
+                aChildrenToShrink.clear();
+            }
+
             if (!aChildrenToShrink.empty())
             {
                 // Have scaling info from rules: then only count scaled children.

sd/qa/unit/import-tests-smartart.cxx
@@ -111,6 +111,7 @@ public:
     void testFillColorList();
     void testTdf134221();
     void testLinearRule();
+    void testLinearRuleVert();
     void testAutofitSync();
     void testSnakeRows();
 
@@ -161,6 +162,7 @@ public:
     CPPUNIT_TEST(testFillColorList);
     CPPUNIT_TEST(testTdf134221);
     CPPUNIT_TEST(testLinearRule);
+    CPPUNIT_TEST(testLinearRuleVert);
     CPPUNIT_TEST(testAutofitSync);
     CPPUNIT_TEST(testSnakeRows);
 
@@ -1564,6 +1566,26 @@ void SdImportTestSmartArt::testLinearRule()
     // - Actual  : 20183
     // i.e. the arrow height was larger than the canvas given to the smartart on slide 1.
     CPPUNIT_ASSERT_LESSEQUAL(static_cast<sal_Int32>(10092), xShape->getSize().Height);
+
+    xDocShRef->DoClose();
+}
+
+void SdImportTestSmartArt::testLinearRuleVert()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/pptx/smartart-linear-rule-vert.pptx"), PPTX);
+
+    uno::Reference<drawing::XShape> xGroup(getShapeFromPage(0, 0, xDocShRef), uno::UNO_QUERY);
+    // Get the P1 shape.
+    uno::Reference<drawing::XShape> xShape = getChildShape(getChildShape(xGroup, 1), 1);
+    uno::Reference<text::XTextRange> xShapeText(xShape, uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("P1"), xShapeText->getString());
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 2020
+    // - Actual  : 10308
+    // i.e. the first item on the vertical linear layout used ~all space, the other items were not
+    // visible.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2020), xShape->getSize().Height);
 
     xDocShRef->DoClose();
 }