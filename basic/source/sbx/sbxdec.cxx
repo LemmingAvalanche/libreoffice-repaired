LemmingAvalanche/libreoffice-repaired

commit 1429201e5ae8d02564b20285677b66891530dedf

convert some more long -> tools::Long
grepping for stuff in template params this time

Change-Id: Ia37bfd85480b3a72c3c465489581d56ad8dde851
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104855
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

basic/source/sbx/sbxdec.cxx
@@ -156,7 +156,7 @@ void SbxDecimal::setShort( sal_Int16 val )
 
 void SbxDecimal::setLong( sal_Int32 val )
 {
-    VarDecFromI4( static_cast<long>(val), &maDec );
+    VarDecFromI4( static_cast<tools::Long>(val), &maDec );
 }
 
 void SbxDecimal::setUShort( sal_uInt16 val )

commit b3424bbe4347ddad8dab743a72751332fec0c84e

basic: fix build after long->tools::Long
Seems 1429201e5ae8d02564b20285677b66891530dedf broke Windows ARM

Change-Id: I5c453ce39c81ef34e3cd55e519a7752881fd72a7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104991
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

basic/source/sbx/sbxdec.cxx
@@ -19,6 +19,7 @@
 
 #include <o3tl/char16_t2wchar_t.hxx>
 
+#include <tools/long.hxx>
 #include <basic/sberrors.hxx>
 #include "sbxconv.hxx"
 