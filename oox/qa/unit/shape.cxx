LemmingAvalanche/libreoffice-repaired

commit da9dedc13d197fd0951fe9151a340a5d570c04c6

ooxml import: supprt cropping to shape
Change-Id: I7bdc959921ecb0cbf19037a78b63eaeb8fc52814
Reviewed-on: https://gerrit.libreoffice.org/72206
Tested-by: Jenkins
Reviewed-by: Tamás Bunth <btomi96@gmail.com>

oox/source/drawingml/shape.cxx
@@ -658,8 +658,18 @@ Reference< XShape > const & Shape::createAndInsert(
     {
         aServiceName = finalizeServiceName( rFilterBase, rServiceName, aShapeRectHmm );
     }
+    // Use custom shape instead of GraphicObjectShape if the image is cropped to
+    // shape. Except rectangle, which does not require further cropping
+    bool bIsCroppedGraphic = (aServiceName == "com.sun.star.drawing.GraphicObjectShape" && mpCustomShapePropertiesPtr->getShapePresetType() >= 0
+            && mpCustomShapePropertiesPtr->getShapePresetType() != XML_Rect && mpCustomShapePropertiesPtr->getShapePresetType() != XML_rect);
     bool bIsCustomShape = ( aServiceName == "com.sun.star.drawing.CustomShape" ||
-                            aServiceName == "com.sun.star.drawing.ConnectorShape" );
+                            aServiceName == "com.sun.star.drawing.ConnectorShape" ||
+                            bIsCroppedGraphic);
+    if(bIsCroppedGraphic)
+    {
+        aServiceName = "com.sun.star.drawing.CustomShape";
+        mpGraphicPropertiesPtr->mbIsCustomShape = true;
+    }
     bool bUseRotationTransform = ( !mbWps ||
             aServiceName == "com.sun.star.drawing.LineShape" ||
             aServiceName == "com.sun.star.drawing.GroupShape" ||
@@ -971,13 +981,14 @@ Reference< XShape > const & Shape::createAndInsert(
         // applying properties
         aShapeProps.assignUsed( getShapeProperties() );
         aShapeProps.assignUsed( maDefaultShapeProperties );
-        if ( bIsEmbMedia || aServiceName == "com.sun.star.drawing.GraphicObjectShape" || aServiceName == "com.sun.star.drawing.OLE2Shape" )
+        if ( bIsEmbMedia || aServiceName == "com.sun.star.drawing.GraphicObjectShape" || aServiceName == "com.sun.star.drawing.OLE2Shape" || bIsCustomShape )
             mpGraphicPropertiesPtr->pushToPropMap( aShapeProps, rGraphicHelper );
         if ( mpTablePropertiesPtr.get() && aServiceName == "com.sun.star.drawing.TableShape" )
             mpTablePropertiesPtr->pushToPropSet( rFilterBase, xSet, mpMasterTextListStyle );
 
         FillProperties aFillProperties = getActualFillProperties(pTheme, &rShapeOrParentShapeFillProps);
-        aFillProperties.pushToPropMap( aShapeProps, rGraphicHelper, mnRotation, nFillPhClr, mbFlipH, mbFlipV );
+        if(!bIsCroppedGraphic)
+            aFillProperties.pushToPropMap( aShapeProps, rGraphicHelper, mnRotation, nFillPhClr, mbFlipH, mbFlipV );
         LineProperties aLineProperties = getActualLineProperties(pTheme);
         aLineProperties.pushToPropMap( aShapeProps, rGraphicHelper, nLinePhClr );
         EffectProperties aEffectProperties = getActualEffectProperties(pTheme);

oox/inc/drawingml/graphicproperties.hxx
@@ -40,12 +40,13 @@ struct GraphicProperties
 {
     BlipFillProperties      maBlipProps;            ///< Properties for the graphic.
     OUString                m_sMediaPackageURL;     ///< Audio/Video URL.
+    bool                    mbIsCustomShape = false;
     css::uno::Reference<css::io::XInputStream> m_xMediaStream; ///< Audio/Video input stream.
 
     /** Writes the properties to the passed property map. */
     void                pushToPropMap(
                             PropertyMap& rPropMap,
-                            const GraphicHelper& rGraphicHelper ) const;
+                            const GraphicHelper& rGraphicHelper) const;
 };
 
 } // namespace drawingml

oox/source/drawingml/fillproperties.cxx
@@ -739,7 +739,7 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
     }
 }
 
-void GraphicProperties::pushToPropMap( PropertyMap& rPropMap, const GraphicHelper& rGraphicHelper ) const
+void GraphicProperties::pushToPropMap( PropertyMap& rPropMap, const GraphicHelper& rGraphicHelper) const
 {
     sal_Int16 nBrightness = getLimitedValue< sal_Int16, sal_Int32 >( maBlipProps.moBrightness.get( 0 ) / PER_PERCENT, -100, 100 );
     sal_Int16 nContrast = getLimitedValue< sal_Int16, sal_Int32 >( maBlipProps.moContrast.get( 0 ) / PER_PERCENT, -100, 100 );
@@ -770,7 +770,15 @@ void GraphicProperties::pushToPropMap( PropertyMap& rPropMap, const GraphicHelpe
             nBrightness = 0;
             nContrast = 0;
         }
-        rPropMap.setProperty(PROP_Graphic, xGraphic);
+        if(mbIsCustomShape)
+        {
+            // it is a cropped graphic.
+            rPropMap.setProperty(PROP_FillStyle, FillStyle_BITMAP);
+            rPropMap.setProperty(PROP_FillBitmapMode, BitmapMode_STRETCH);
+            rPropMap.setProperty(PROP_FillBitmap, xGraphic);
+        }
+        else
+            rPropMap.setProperty(PROP_Graphic, xGraphic);
 
         // cropping
         if ( maBlipProps.moClipRect.has() )

commit b2cd4137562a60908745143dab0f173b46335fc6

tdf#128877 DOCX import: fix lost position of image cropped to shape
Regression from commit da9dedc13d197fd0951fe9151a340a5d570c04c6 (ooxml
import: supprt cropping to shape, 2019-05-13), which changed the type of
a cropped-to-shape image from drawing.GraphicObjectShape to
drawing.CustomShape.

drawing.GraphicObjectShape worked because GraphicImport::lcl_attribute()
in writerfilter/ had a check for drawing.GraphicObjectShape and did an
explicit setPosition().

Doing the same for bitmap-filled custom shapes would be an option, but
it would be ugly: scaling/translation/rotation/mirroring can only work
together if they are only applied once, and that should happen in oox/,
that's why we already have a mechanism to send the position from
writerfilter/ to oox/ for WPS shapes. (<a:xfrm> contains the size, but
not the position of the shape, so oox/ in itself could not know the
position.)

Fix the problem by improving ShapeContextHandler instead the pass the
position from writerfilter/ to oox/ for <pic:pic> as well, the same is
done for <wps:wsp> already since commit
6f8c8c6c940ce378089058731f6b94b1f88fc043 (DOCX drawingML shape import:
fix position when CustomShapeGeometry is set, 2013-11-21).

Change-Id: I74a60136d0ca8383e58948711b47858823f42437
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105263
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

oox/source/shape/ShapeContextHandler.cxx
@@ -504,6 +504,15 @@ ShapeContextHandler::getShape()
         else if (mpShape)
         {
             basegfx::B2DHomMatrix aTransformation;
+
+            if (maPosition.X != 0 || maPosition.Y != 0)
+            {
+                // We got a position from writerfilter/, store that in the shape, otherwise the
+                // position won't be set.
+                mpShape->setWps(true);
+                mpShape->setPosition(maPosition);
+            }
+
             mpShape->addShape(*mxFilterBase, mpThemePtr.get(), xShapes, aTransformation, mpShape->getFillProperties() );
             xResult.set(mpShape->getXShape());
             mxGraphicShapeContext.clear( );

oox/qa/unit/shape.cxx
@@ -12,6 +12,9 @@
 
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+
+#include <rtl/math.hxx>
 
 using namespace ::com::sun::star;
 
@@ -63,6 +66,26 @@ CPPUNIT_TEST_FIXTURE(OoxShapeTest, testMultipleGroupShapes)
     // - Actual  : 1
     // i.e. the 2 group shapes from the document were imported as a single one.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2), xDrawPage->getCount());
+}
+
+CPPUNIT_TEST_FIXTURE(OoxShapeTest, testCustomshapePosition)
+{
+    load("customshape-position.docx");
+
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+
+    sal_Int32 nY{};
+    xShape->getPropertyValue("VertOrientPosition") >>= nY;
+    // <wp:posOffset>581025</wp:posOffset> in the document.
+    sal_Int32 nExpected = rtl::math::round(581025.0 / 360);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1614
+    // - Actual  : 0
+    // i.e. the position of the shape was lost on import due to the rounded corners.
+    CPPUNIT_ASSERT_EQUAL(nExpected, nY);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();