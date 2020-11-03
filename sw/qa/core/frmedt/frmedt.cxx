LemmingAvalanche/libreoffice-repaired

commit 0df7a9f7c2fdaad35b8f35ca808edfcfb3ac5dc9

fdo#82191 sw::DocumentLayoutManager: copy textbox content of draw formats
The SwFmtCntnt, i.e. the content of the draw format was already copied,
but that's only a pointer to the real content: instead duplicate the
real contents on copy&paste, that's how we copy fly frames as well.

Change-Id: I42475e356aaa1c54c08fb23a6a395d1726e5f33e

sw/source/core/frmedt/fecopy.cxx
@@ -75,6 +75,7 @@
 #include <docsh.hxx>
 #include <pagedesc.hxx>
 #include <mvsave.hxx>
+#include <textboxhelper.hxx>
 #include <vcl/virdev.hxx>
 #include <svx/svdundo.hxx>
 
@@ -884,6 +885,7 @@ bool SwFEShell::Paste( SwDoc* pClpDoc, bool bIncludingPageFrames )
             if( !Imp()->GetDrawView() )
                 MakeDrawView();
 
+            std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pClpDoc);
             for ( sal_uInt16 i = 0; i < pClpDoc->GetSpzFrmFmts()->size(); ++i )
             {
                 bool bInsWithFmt = true;
@@ -952,6 +954,10 @@ bool SwFEShell::Paste( SwDoc* pClpDoc, bool bIncludingPageFrames )
                             if (pCpyObj && CheckControlLayer(pCpyObj))
                                 continue;
                         }
+
+                        // Ignore TextBoxes, they are already handled in sw::DocumentLayoutManager::CopyLayoutFmt().
+                        if (aTextBoxes.find(&rCpyFmt) != aTextBoxes.end())
+                            continue;
 
                         aAnchor.SetAnchor( pPos );
                     }

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -18,6 +18,7 @@
 #include <section.hxx>
 #include <fmtclds.hxx>
 #include <dcontact.hxx>
+#include <textboxhelper.hxx>
 
 #include <svx/svdpage.hxx>
 #include <svx/svdview.hxx>
@@ -46,6 +47,7 @@ public:
     void testCp1000071();
     void testShapeTextboxVertadjust();
     void testShapeTextboxAutosize();
+    void testFdo82191();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -62,6 +64,7 @@ public:
     CPPUNIT_TEST(testCp1000071);
     CPPUNIT_TEST(testShapeTextboxVertadjust);
     CPPUNIT_TEST(testShapeTextboxAutosize);
+    CPPUNIT_TEST(testFdo82191);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -388,6 +391,27 @@ void SwUiWriterTest::testShapeTextboxAutosize()
     // same height as the first, even though the first contained 1 paragraph
     // and the other 2 ones.
     CPPUNIT_ASSERT(pFirst->GetSnapRect().getHeight() < pSecond->GetSnapRect().getHeight());
+}
+
+void SwUiWriterTest::testFdo82191()
+{
+    SwDoc* pDoc = createDoc("fdo82191.odt");
+    SdrPage* pPage = pDoc->getIDocumentDrawModelAccess().GetDrawModel()->GetPage(0);
+    std::set<const SwFrmFmt*> aTextBoxes = SwTextBoxHelper::findTextBoxes(pDoc);
+    // Make sure we have a single draw shape.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), SwTextBoxHelper::getCount(pPage, aTextBoxes));
+
+    SwDoc aClipboard;
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SdrObject* pObject = pPage->GetObj(0);
+    // Select it, then copy and paste.
+    pWrtShell->SelectObj(Point(), 0, pObject);
+    pWrtShell->Copy(&aClipboard);
+    pWrtShell->Paste(&aClipboard);
+
+    aTextBoxes = SwTextBoxHelper::findTextBoxes(pDoc);
+    // This was one: the textbox of the shape wasn't copied.
+    CPPUNIT_ASSERT_EQUAL(size_t(2), aTextBoxes.size());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);

sw/source/core/doc/DocumentLayoutManager.cxx
@@ -39,6 +39,7 @@
 #include <unoframe.hxx>
 #include <docary.hxx>
 #include <dcontact.hxx>
+#include <textboxhelper.hxx>
 
 using namespace ::com::sun::star;
 
@@ -492,6 +493,16 @@ SwFrmFmt *DocumentLayoutManager::CopyLayoutFmt(
 
     if( bMakeFrms )
         pDest->MakeFrms();
+
+    // If the draw format has a TextBox, then copy its fly format as well.
+    if (SwFrmFmt* pSourceTextBox = SwTextBoxHelper::findTextBox(&rSource))
+    {
+        SwFrmFmt* pDestTextBox = CopyLayoutFmt(*pSourceTextBox, rNewAnchor, bSetTxtFlyAtt, bMakeFrms);
+        SwAttrSet aSet(pDest->GetAttrSet());
+        SwFmtCntnt aCntnt(pDestTextBox->GetCntnt().GetCntntIdx()->GetNode().GetStartNode());
+        aSet.Put(aCntnt);
+        pDest->SetFmtAttr(aSet);
+    }
 
     return pDest;
 }

commit ef7a45bbc622bcad55416bd028c82282400f63da

tdf#135893 sw paste: fix copying fly frame in textbox twice
Regression from commit 0df7a9f7c2fdaad35b8f35ca808edfcfb3ac5dc9
(fdo#82191 sw::DocumentLayoutManager: copy textbox content of draw
formats, 2014-08-15), the problem is that the fly+draw format copies the
fly content recursively already, so when we would copy all special
formats of the document, the inner fly frame is copied twice.

This is normally not a problem for fly frames (where you can't select
multiple fly frames at the same time), nor a problem for draw frames
(which have no sw content), but it's a problem for the combination of
these, TextBoxes.

Fix the problem by ignoring fly frames which are anchored in such
TextBoxes: we do the same for fly frames which have an associated draw
format already.

Change-Id: I3376beb414f91abfa6f3f5640f825ccae34911c2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105204
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/frmedt/fecopy.cxx
@@ -666,6 +666,41 @@ namespace {
     {
         return rPaM.HasMark() && *rPaM.GetPoint() != *rPaM.GetMark();
     }
+
+    /// Is pFormat anchored in a fly frame which has an associated draw format?
+    bool IsInTextBox(const SwFrameFormat* pFormat)
+    {
+        const SwFormatAnchor& rAnchor = pFormat->GetAnchor();
+        const SwPosition* pPosition = rAnchor.GetContentAnchor();
+        if (!pPosition)
+        {
+            return false;
+        }
+
+        const SwStartNode* pFlyNode = pPosition->nNode.GetNode().FindFlyStartNode();
+        if (!pFlyNode)
+        {
+            return false;
+        }
+
+        for ( const auto& pSpzFormat : *pFormat->GetDoc()->GetSpzFrameFormats() )
+        {
+            if (pSpzFormat->Which() != RES_FLYFRMFMT)
+            {
+                continue;
+            }
+
+            const SwNodeIndex* pIdx = pSpzFormat->GetContent().GetContentIdx();
+            if (!pIdx || pFlyNode != &pIdx->GetNode())
+            {
+                continue;
+            }
+
+            return SwTextBoxHelper::isTextBox(pSpzFormat, RES_FLYFRMFMT);
+        }
+
+        return false;
+    }
 }
 
 bool SwFEShell::Paste(SwDoc& rClpDoc, bool bNestedTable)
@@ -951,6 +986,13 @@ bool SwFEShell::Paste(SwDoc& rClpDoc, bool bNestedTable)
                                 const SdrObject *pCpyObj = pCpyFormat->FindSdrObject();
                                 if (pCpyObj && CheckControlLayer(pCpyObj))
                                     continue;
+                            }
+                            else if (pCpyFormat->Which() == RES_FLYFRMFMT && IsInTextBox(pCpyFormat))
+                            {
+                                // This is a fly frame which is anchored in a TextBox, ignore it as
+                                // it's already copied as part of copying the content of the
+                                // TextBox.
+                                continue;
                             }
 
                             // Ignore TextBoxes, they are already handled in sw::DocumentLayoutManager::CopyLayoutFormat().

sw/qa/core/frmedt/frmedt.cxx
@@ -22,6 +22,7 @@
 #include <frameformats.hxx>
 #include <unotxdoc.hxx>
 #include <docsh.hxx>
+#include <swdtflvr.hxx>
 
 char const DATA_DIRECTORY[] = "/sw/qa/core/frmedt/data/";
 
@@ -107,6 +108,35 @@ CPPUNIT_TEST_FIXTURE(SwCoreFrmedtTest, testVertPosFromBottomBoundingBox)
     // i.e. UI did not allow anchoring a shape 10cm above the bottom of the page due to wrong
     // bounding box.
     CPPUNIT_ASSERT_EQUAL(-1 * nPagePrintAreaBottom, aBoundRect.Pos().getY());
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreFrmedtTest, testPasteFlyInTextBox)
+{
+    // Given a document that contains a textbox, which contains an sw image (fly frame)
+    load(DATA_DIRECTORY, "paste-fly-in-textbox.docx");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    SwDoc* pDoc = pDocShell->GetDoc();
+    SdrPage* pPage = pDoc->getIDocumentDrawModelAccess().GetDrawModel()->GetPage(0);
+    SdrObject* pObject = pPage->GetObj(0);
+    pWrtShell->SelectObj(Point(), 0, pObject);
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(3), pDoc->GetSpzFrameFormats()->GetFormatCount());
+    rtl::Reference<SwTransferable> pTransfer = new SwTransferable(*pWrtShell);
+    pTransfer->Cut();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), pDoc->GetSpzFrameFormats()->GetFormatCount());
+    TransferableDataHelper aHelper(pTransfer.get());
+
+    // When pasting that to an empty document.
+    SwTransferable::Paste(*pWrtShell, aHelper);
+
+    // Then we should have the image only once: 3 formats (draw+fly formats for the textbox and a
+    // fly format for the image).
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 3
+    // - Actual  : 4
+    // i.e. the image was pasted twice.
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(3), pDoc->GetSpzFrameFormats()->GetFormatCount());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();