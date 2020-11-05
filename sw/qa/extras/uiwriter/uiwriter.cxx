LemmingAvalanche/libreoffice-repaired

commit 5bfc76543bc6a91e3ed306bc386653b44b664ebe

tdf#79717 save/restore character format on selection overwrite
Change-Id: Ic82c50d07cf4a2208a6e2956c8b9ebc4f8630b31
Reviewed-on: https://gerrit.libreoffice.org/67597
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/uibase/wrtsh/wrtsh1.cxx
@@ -41,6 +41,7 @@
 #include <svtools/ehdl.hxx>
 #include <svtools/soerr.hxx>
 #include <unotools/moduleoptions.hxx>
+#include <editeng/rsiditem.hxx>
 #include <editeng/sizeitem.hxx>
 #include <editeng/formatbreakitem.hxx>
 #include <editeng/svxacorr.hxx>
@@ -185,6 +186,10 @@ void SwWrtShell::Insert( const OUString &rStr )
          bCallIns = m_bIns /*|| bHasSel*/;
     bool bDeleted = false;
 
+    SfxItemSet aCharAttrSet(
+        GetAttrPool(),
+        svl::Items<RES_CHRATR_BEGIN, RES_CHRATR_END - 1>{});
+
     if( bHasSel || ( !m_bIns && SelectHiddenRange() ) )
     {
             // Only here parenthesizing, because the normal
@@ -204,6 +209,13 @@ void SwWrtShell::Insert( const OUString &rStr )
             aRewriter.AddRule(UndoArg3, aTmpStr);
         }
 
+        // tdf#79717 Save character formatting of the start of the selection
+        const SwPosition *pStart = GetCursor()->Start();
+        SwPaM aPaM(pStart->nNode.GetNode(), pStart->nContent.GetIndex(),
+                   pStart->nNode.GetNode(), pStart->nContent.GetIndex() + 1);
+        aCharAttrSet.ClearItem(RES_CHRATR_RSID);
+        GetPaMAttr(&aPaM, aCharAttrSet);
+
         StartUndo(SwUndoId::REPLACE, &aRewriter);
         bStarted = true;
         bDeleted = DelRight();
@@ -211,6 +223,22 @@ void SwWrtShell::Insert( const OUString &rStr )
 
     bCallIns ?
         SwEditShell::Insert2( rStr, bDeleted ) : SwEditShell::Overwrite( rStr );
+
+    if( bDeleted )
+    {
+        // tdf#79717 Restore formatting of the deleted selection
+        SwPosition* pEnd = GetCursor()->Start();
+        SwPaM aPaM(pEnd->nNode.GetNode(), pEnd->nContent.GetIndex() - rStr.getLength(),
+                   pEnd->nNode.GetNode(), pEnd->nContent.GetIndex());
+
+        std::set<sal_uInt16> aAttribs;
+        for (sal_uInt16 i = RES_CHRATR_BEGIN; i < RES_CHRATR_END; ++i)
+            if (i != sal_uInt16(RES_CHRATR_RSID))
+                aAttribs.insert(aAttribs.end(), i);
+        ResetAttr(aAttribs, &aPaM);
+
+        SetAttrSet(aCharAttrSet, SetAttrMode::DEFAULT, &aPaM);
+    }
 
     if( bStarted )
     {

commit 16c4ab89ba18b8dc0edc0c938084c7f1449d4ba1

tdf#137532: do not reset attributes deleting selection
Regression from 5bfc76543bc6a91e3ed306bc386653b44b664ebe

While at it, extend unittest for tdf#79717 to test another
regression from the same commit I found while testing the patch:
type 'a', type bold 'b', select both characters, type 'c'
-> it should be bold

Change-Id: I5b0b8ce4bfdfb4d52051e25c366827d7b594bb1a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105316
Tested-by: Mike Kaganski <mike.kaganski@collabora.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/uibase/wrtsh/wrtsh1.cxx
@@ -245,13 +245,6 @@ void SwWrtShell::Insert( const OUString &rStr )
         SwPaM aPaM(pEnd->nNode.GetNode(), pEnd->nContent.GetIndex() - rStr.getLength(),
                    pEnd->nNode.GetNode(), pEnd->nContent.GetIndex());
 
-        o3tl::sorted_vector<sal_uInt16> aAttribs;
-        for (sal_uInt16 i = RES_CHRATR_BEGIN; i < RES_CHRATR_END; ++i)
-            if (i != sal_uInt16(RES_CHRATR_RSID))
-                aAttribs.insert(i);
-        aAttribs.insert(RES_TXTATR_CHARFMT);
-        ResetAttr(aAttribs, &aPaM);
-
         SetAttrSet(aCharAttrSet, SetAttrMode::DEFAULT, &aPaM);
     }
 

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -214,6 +214,7 @@ public:
     void testBookmarkUndo();
     void testFdo85876();
     void testTdf79717();
+    void testTdf137532();
     void testFdo87448();
     void testTextCursorInvalidation();
     void testTdf68183();
@@ -442,6 +443,7 @@ public:
     CPPUNIT_TEST(testBookmarkUndo);
     CPPUNIT_TEST(testFdo85876);
     CPPUNIT_TEST(testTdf79717);
+    CPPUNIT_TEST(testTdf137532);
     CPPUNIT_TEST(testFdo87448);
     CPPUNIT_TEST(testTextCursorInvalidation);
     CPPUNIT_TEST(testTdf68183);
@@ -2020,11 +2022,11 @@ void SwUiWriterTest::testTdf79717()
     lcl_setWeight(pWrtShell, WEIGHT_BOLD);
     pWrtShell->Insert("bold");
     pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/false, 1, /*bBasicCall=*/false);
-    // Select 'bol' and rewrite it
+    // Select 'bol' and replace it
     pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/true, 3, /*bBasicCall=*/false);
     pWrtShell->Insert("bol");
 
-    // Without the fix in place, 'bol' would have been rewritten with normal font weight
+    // Without the fix in place, 'bol' would have been replaced with normal font weight
 
     auto xText = getParagraph(1)->getText();
     CPPUNIT_ASSERT(xText.is());
@@ -2040,6 +2042,67 @@ void SwUiWriterTest::testTdf79717()
         CPPUNIT_ASSERT_EQUAL(OUString("bold"), xCursor->getString());
         CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, getProperty<float>(xCursor, "CharWeight"));
     }
+
+    // Now select characters from both runs and replace them
+    pWrtShell->EndPara();
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/true, 5, /*bBasicCall=*/false);
+    pWrtShell->Insert("new");
+    {
+        auto xCursor(xText->createTextCursorByRange(getRun(getParagraph(1), 1)));
+        CPPUNIT_ASSERT(xCursor.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("norma"), xCursor->getString());
+        CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL, getProperty<float>(xCursor, "CharWeight"));
+    }
+    {
+        auto xCursor(xText->createTextCursorByRange(getRun(getParagraph(1), 2)));
+        CPPUNIT_ASSERT(xCursor.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("new"), xCursor->getString());
+        CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, getProperty<float>(xCursor, "CharWeight"));
+    }
+}
+
+void SwUiWriterTest::testTdf137532()
+{
+    SwDoc* const pDoc = createDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->Insert("test");
+
+    //Select the word and change it to bold
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/true, 4, /*bBasicCall=*/false);
+    lcl_setWeight(pWrtShell, WEIGHT_BOLD);
+
+    // Select first character and replace it
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/false, 1, /*bBasicCall=*/false);
+    pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/true, 1, /*bBasicCall=*/false);
+    pWrtShell->Insert("x");
+
+    auto xText = getParagraph(1)->getText();
+    CPPUNIT_ASSERT(xText.is());
+    auto xCursor(xText->createTextCursorByRange(getRun(getParagraph(1), 1)));
+
+    CPPUNIT_ASSERT(xCursor.is());
+    CPPUNIT_ASSERT_EQUAL(OUString("xest"), xCursor->getString());
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, getProperty<float>(xCursor, "CharWeight"));
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    Scheduler::ProcessEventsToIdle();
+
+    xCursor.set(xText->createTextCursorByRange(getRun(getParagraph(1), 1)));
+    CPPUNIT_ASSERT(xCursor.is());
+    CPPUNIT_ASSERT_EQUAL(OUString("test"), xCursor->getString());
+
+    // Without the fix in place, this test would have failed in
+    // - Expected: 150
+    // - Actual  : 100
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, getProperty<float>(xCursor, "CharWeight"));
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    Scheduler::ProcessEventsToIdle();
+
+    xCursor.set(xText->createTextCursorByRange(getRun(getParagraph(1), 1)));
+    CPPUNIT_ASSERT(xCursor.is());
+    CPPUNIT_ASSERT_EQUAL(OUString("test"), xCursor->getString());
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL, getProperty<float>(xCursor, "CharWeight"));
 }
 
 void SwUiWriterTest::testFdo87448()