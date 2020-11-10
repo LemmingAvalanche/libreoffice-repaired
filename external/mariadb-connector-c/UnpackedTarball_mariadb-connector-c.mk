LemmingAvalanche/libreoffice-repaired

commit b75878a9175228d9e82661ba9cf9cd9e701e1879

tdf#135202: Mysql use openssl libs to be able to use caching_sha2_pw
Change-Id: I7552b65022b725c6e87fef61478dc6e9c4322559
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105376
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
@@ -32,8 +32,14 @@ $(eval $(call gb_UnpackedTarball_add_patches,mariadb-connector-c,\
 # TODO are any "plugins" needed?
 $(eval $(call gb_UnpackedTarball_set_post_action,mariadb-connector-c, \
 	< libmariadb/ma_client_plugin.c.in sed \
-		-e 's/@EXTERNAL_PLUGINS@/extern struct st_mysql_client_plugin pvio_socket_client_plugin;/' \
-		-e 's/@BUILTIN_PLUGINS@/(struct st_mysql_client_plugin *)\&pvio_socket_client_plugin$(COMMA)/' \
+		-e 's/@EXTERNAL_PLUGINS@/\
+			extern struct st_mysql_client_plugin pvio_socket_client_plugin;\
+			extern struct st_mysql_client_plugin caching_sha2_password_client_plugin;\
+			/' \
+		-e 's/@BUILTIN_PLUGINS@/\
+			(struct st_mysql_client_plugin *)\&pvio_socket_client_plugin$(COMMA)\
+			(struct st_mysql_client_plugin *)\&caching_sha2_password_client_plugin$(COMMA)\
+			/' \
 		> libmariadb/ma_client_plugin.c  \
 ))
 

connectivity/Library_mysqlc.mk
@@ -12,6 +12,7 @@ $(eval $(call gb_Library_Library,mysqlc))
 $(eval $(call gb_Library_use_externals,mysqlc,\
 	boost_headers \
 	mariadb-connector-c \
+	openssl \
 ))
 
 $(eval $(call gb_Library_set_include,mysqlc,\

external/mariadb-connector-c/StaticLibrary_mariadb-connector-c.mk
@@ -21,8 +21,8 @@ $(eval $(call gb_StaticLibrary_set_include,mariadb-connector-c,\
 # This is needed for MSVC 2008: it somehow finds a dlopen somewhere
 # but the static library then contains unreferenced symbols.
 # This macro enables a re-definition to native Win32 APIs in my_global.h.
-## TODO missing enable: -DHAVE_OPENSSL -D HAVE_COMPRESS
-## (but then need to add "-lssl -lcrypto -lz" to mysqlcppconn linking)
+## TODO missing enable: -D HAVE_COMPRESS
+## (but then need to add "-lz" to mysqlcppconn linking)
 $(eval $(call gb_StaticLibrary_add_cflags,mariadb-connector-c,-DHAVE_DLOPEN -D ENABLED_LOCAL_INFILE -D LIBMARIADB -D THREAD -DSQLITE_ENABLE_COLUMN_METADATA=1))
 
 ifeq ($(OS),WNT)
@@ -31,6 +31,9 @@ $(eval $(call gb_StaticLibrary_set_include,mariadb-connector-c,\
 	$$(INCLUDE) \
 	-I$(call gb_UnpackedTarball_get_dir,mariadb-connector-c)/win-iconv \
 ))
+else
+$(eval $(call gb_StaticLibrary_use_external,mariadb-connector-c,openssl_headers))
+$(eval $(call gb_StaticLibrary_add_cflags,mariadb-connector-c,-DHAVE_OPENSSL))
 endif
 
 $(eval $(call gb_StaticLibrary_add_generated_cobjects,mariadb-connector-c,\
@@ -68,7 +71,10 @@ $(eval $(call gb_StaticLibrary_add_generated_cobjects,mariadb-connector-c,\
 	$(if $(filter $(OS),WNT), \
 		UnpackedTarball/mariadb-connector-c/libmariadb/win32_errmsg \
 		UnpackedTarball/mariadb-connector-c/libmariadb/secure/win_crypt \
-		UnpackedTarball/mariadb-connector-c/win-iconv/win_iconv) \
+		UnpackedTarball/mariadb-connector-c/win-iconv/win_iconv \
+		, \
+		UnpackedTarball/mariadb-connector-c/libmariadb/secure/openssl_crypt \
+	) \
 ))
 
 # vim: set noet sw=4 ts=4:

commit 2bade7f5cbf6985c13183d4303461167158eeec2

mariadb-connector-c: fix makefile for GNU make 3.82
Old make thinks that ; terminates the recipe, have to escape it.
(other changes are just cosmetic)

(regression from b75878a9175228d9e82661ba9cf9cd9e701e1879)

Change-Id: Ib0119511977bbff077f037fbb5975df9f860ae58
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105516
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
@@ -32,15 +32,15 @@ $(eval $(call gb_UnpackedTarball_add_patches,mariadb-connector-c,\
 # TODO are any "plugins" needed?
 $(eval $(call gb_UnpackedTarball_set_post_action,mariadb-connector-c, \
 	< libmariadb/ma_client_plugin.c.in sed \
-		-e 's/@EXTERNAL_PLUGINS@/\
-			extern struct st_mysql_client_plugin pvio_socket_client_plugin;\
-			extern struct st_mysql_client_plugin caching_sha2_password_client_plugin;\
+		-e 's/@EXTERNAL_PLUGINS@/ \
+			extern struct st_mysql_client_plugin pvio_socket_client_plugin\; \
+			extern struct st_mysql_client_plugin caching_sha2_password_client_plugin\; \
 			/' \
-		-e 's/@BUILTIN_PLUGINS@/\
-			(struct st_mysql_client_plugin *)\&pvio_socket_client_plugin$(COMMA)\
-			(struct st_mysql_client_plugin *)\&caching_sha2_password_client_plugin$(COMMA)\
+		-e 's/@BUILTIN_PLUGINS@/ \
+			(struct st_mysql_client_plugin *)\&pvio_socket_client_plugin$(COMMA) \
+			(struct st_mysql_client_plugin *)\&caching_sha2_password_client_plugin$(COMMA) \
 			/' \
-		> libmariadb/ma_client_plugin.c  \
+		> libmariadb/ma_client_plugin.c \
 ))
 
 