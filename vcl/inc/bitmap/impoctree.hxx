LemmingAvalanche/libreoffice-repaired

commit 0e115b627439967be551e12d49cd56b5b2b0416c

tools::Long->sal_uInt16 in ImpErrorQuad
This class only wants about 12 bits of data per color component.

Change-Id: I363ef085a302b3f1599b626e5cec92efe1c65026
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105909
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/bitmap/impoctree.hxx
@@ -24,9 +24,9 @@
 
 class ImpErrorQuad
 {
-    tools::Long nRed;
-    tools::Long nGreen;
-    tools::Long nBlue;
+    sal_uInt16 nRed;
+    sal_uInt16 nGreen;
+    sal_uInt16 nBlue;
 
 public:
     ImpErrorQuad()
@@ -37,9 +37,9 @@ public:
     }
 
     ImpErrorQuad(const BitmapColor& rColor)
-        : nRed(tools::Long(rColor.GetRed()) << 5)
-        , nGreen(tools::Long(rColor.GetGreen()) << 5)
-        , nBlue(tools::Long(rColor.GetBlue()) << 5)
+        : nRed(rColor.GetRed() << 5)
+        , nGreen(rColor.GetGreen() << 5)
+        , nBlue(rColor.GetBlue() << 5)
     {
     }
 
@@ -56,16 +56,16 @@ public:
 
 inline void ImpErrorQuad::operator=(const BitmapColor& rColor)
 {
-    nRed = tools::Long(rColor.GetRed()) << 5;
-    nGreen = tools::Long(rColor.GetGreen()) << 5;
-    nBlue = tools::Long(rColor.GetBlue()) << 5;
+    nRed = rColor.GetRed() << 5;
+    nGreen = rColor.GetGreen() << 5;
+    nBlue = rColor.GetBlue() << 5;
 }
 
 inline ImpErrorQuad& ImpErrorQuad::operator-=(const BitmapColor& rColor)
 {
-    nRed -= tools::Long(rColor.GetRed()) << 5;
-    nGreen -= tools::Long(rColor.GetGreen()) << 5;
-    nBlue -= tools::Long(rColor.GetBlue()) << 5;
+    nRed -= rColor.GetRed() << 5;
+    nGreen -= rColor.GetGreen() << 5;
+    nBlue -= rColor.GetBlue() << 5;
 
     return *this;
 }
@@ -100,9 +100,9 @@ inline void ImpErrorQuad::ImplAddColorError7(const ImpErrorQuad& rErrQuad)
 
 inline BitmapColor ImpErrorQuad::ImplGetColor()
 {
-    return BitmapColor(std::clamp(nRed, tools::Long(0), tools::Long(8160)) >> 5,
-                       std::clamp(nGreen, tools::Long(0), tools::Long(8160)) >> 5,
-                       std::clamp(nBlue, tools::Long(0), tools::Long(8160)) >> 5);
+    return BitmapColor(std::clamp<sal_uInt16>(nRed, 0, 8160) >> 5,
+                       std::clamp<sal_uInt16>(nGreen, 0, 8160) >> 5,
+                       std::clamp<sal_uInt16>(nBlue, 0, 8160) >> 5);
 }
 
 #endif // INCLUDED_VCL_INC_IMPOCTREE_HXX

commit 3a02d7c3c70f88e2512cdf2d740c4a1eeaa91b5e

tdf#138326 impress template thumbnails are blurred
regression from
    commit 0e115b627439967be551e12d49cd56b5b2b0416c
    Author: Noel <noelgrandin@gmail.com>
    Date:   Fri Nov 13 10:23:36 2020 +0200
    tools::Long->sal_uInt16 in ImpErrorQuad

Change-Id: I29403afcb7d88dee27f1565793ffbd7ad4c53a17
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106545
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/bitmap/impoctree.hxx
@@ -24,9 +24,9 @@
 
 class ImpErrorQuad
 {
-    sal_uInt16 nRed;
-    sal_uInt16 nGreen;
-    sal_uInt16 nBlue;
+    sal_uInt32 nRed;
+    sal_uInt32 nGreen;
+    sal_uInt32 nBlue;
 
 public:
     ImpErrorQuad()