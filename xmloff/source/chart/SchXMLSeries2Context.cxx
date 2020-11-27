LemmingAvalanche/libreoffice-repaired

commit 81e9dbc8d3347834067c6740a3a938892b654663

Fix typo
Change-Id: I9bc856d4d7370f2a5fa09001d5b8b933bc72e96f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102095
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

xmloff/source/chart/SchXMLSeries2Context.cxx
@@ -828,7 +828,7 @@ const std::pair<OUString, OUString> aApiToLabelFooPairs[]
     = { { "LineStyle", "LabelBorderStyle" },
         { "LineWidth", "LabelBorderWidth" },
         { "LineColor", "LabelBorderColor" },
-        // The name "LaberBorderDash" ist defined, but the associated API name "LineDash" belongs to
+        // TThe name "LabelBorderDash" is defined, but the associated API name "LineDash" belongs to
         // the <draw:stroke-dash> element and is not used directly as line property.
         //{"LineDash", "LabelBorderDash"},
         { "LineDashName", "LabelBorderDashName" },

commit 9a447345c61e2c71bdf1633a5828feec4ebf9405

Fix typo in comment
...introduced with 81e9dbc8d3347834067c6740a3a938892b654663 "Fix typo"

Change-Id: Id3888f0216a7702d7f09d25d069d50f64859eff9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106714
Tested-by: Jenkins
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

xmloff/source/chart/SchXMLSeries2Context.cxx
@@ -828,7 +828,7 @@ const std::pair<OUString, OUString> aApiToLabelFooPairs[]
     = { { "LineStyle", "LabelBorderStyle" },
         { "LineWidth", "LabelBorderWidth" },
         { "LineColor", "LabelBorderColor" },
-        // TThe name "LabelBorderDash" is defined, but the associated API name "LineDash" belongs to
+        // The name "LabelBorderDash" is defined, but the associated API name "LineDash" belongs to
         // the <draw:stroke-dash> element and is not used directly as line property.
         //{"LineDash", "LabelBorderDash"},
         { "LineDashName", "LabelBorderDashName" },