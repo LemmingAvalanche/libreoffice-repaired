LemmingAvalanche/libreoffice-repaired

commit 59d2d961ced0d05e766f1de85d574bdfcb7d05be

it does not work to clear data of other thread's thread_local
The thread-specific RecursionHelper object is reused between runs,
so it should not be freed after a threaded calculation finishes.
But since threads may be left around longer by the thread pool,
the object may be destroyed at an unknown later time. Which could
possibly cause problems if it refers to data that's been destroyed
meanwhile (I think all the pointers in RecursionHelper are not
owning, so this should not be a problem in practice, but still).
So at least clear the contents, they shouldn't be reused anyway.

Change-Id: I8934441c754d20bd20d7e19a8510d9323c0db894
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106758
Tested-by: Jenkins
Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

sc/source/core/data/documen8.cxx
@@ -80,6 +80,7 @@
 #include <stringutil.hxx>
 #include <documentlinkmgr.hxx>
 #include <tokenarray.hxx>
+#include <recursionhelper.hxx>
 
 #include <memory>
 #include <utility>
@@ -420,6 +421,10 @@ const ScDocumentThreadSpecific& ScDocument::CalculateInColumnInThread( ScInterpr
 
     assert(IsThreadedGroupCalcInProgress());
     maThreadSpecific.pContext = nullptr;
+    // If any of the thread_local data would cause problems if they stay around for too long
+    // (and e.g. outlive the ScDocument), clean them up here, they cannot be cleaned up
+    // later from the main thread.
+    maThreadSpecific.xRecursionHelper->Clear();
 
     return maThreadSpecific;
 }

sc/source/core/data/documen2.cxx
@@ -387,7 +387,7 @@ ScDocument::~ScDocument()
 
     pScriptTypeData.reset();
     maNonThreaded.xRecursionHelper.reset();
-    maThreadSpecific.xRecursionHelper.reset();
+    assert(!maThreadSpecific.xRecursionHelper);
 
     pPreviewFont.reset();
     SAL_WARN_IF( pAutoNameCache, "sc.core", "AutoNameCache still set in dtor" );

commit f342127168c16400ac7f466fa46afa42a58c5069

avoid possible nullptr dereference
Regression from 59d2d961ced0d05e766f1de85d574bdfcb7d05be.

Change-Id: I6e00f43438882b9c96005ce921fbfd419ca634c7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107358
Tested-by: Jenkins
Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

sc/source/core/data/documen8.cxx
@@ -423,7 +423,8 @@ void ScDocument::CalculateInColumnInThread( ScInterpreterContext& rContext, cons
     // If any of the thread_local data would cause problems if they stay around for too long
     // (and e.g. outlive the ScDocument), clean them up here, they cannot be cleaned up
     // later from the main thread.
-    maThreadSpecific.xRecursionHelper->Clear();
+    if(maThreadSpecific.xRecursionHelper)
+        maThreadSpecific.xRecursionHelper->Clear();
 }
 
 void ScDocument::HandleStuffAfterParallelCalculation( SCCOL nColStart, SCCOL nColEnd, SCROW nRow, size_t nLen, SCTAB nTab, ScInterpreter* pInterpreter )