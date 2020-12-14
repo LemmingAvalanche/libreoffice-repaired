LemmingAvalanche/libreoffice-repaired

commit f9210f6cf45b2a6149d602f48c0f1ec1fc0e5f08

tdf#138715 remove mork driver
since TB moved to sqlite

Change-Id: Ifddf1a6c9783a294ee9ccfac667c7e798674d7a0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107574
Tested-by: Jenkins
Reviewed-by: Tor Lillqvist <tml@collabora.com>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

compilerplugins/clang/unusedmethods.results
@@ -56,8 +56,6 @@ connectivity/source/drivers/firebird/Util.hxx:72
     short connectivity::firebird::ColumnTypeInfo::getSubType() const
 connectivity/source/drivers/firebird/Util.hxx:74
     const class rtl::OUString & connectivity::firebird::ColumnTypeInfo::getCharacterSet() const
-connectivity/source/drivers/mork/mork_helper.cxx:37
-    int main(int,char **)
 connectivity/source/drivers/mysqlc/mysqlc_connection.hxx:176
     class rtl::OUString connectivity::mysqlc::OConnection::transFormPreparedStatement(const class rtl::OUString &)
 connectivity/source/drivers/mysqlc/mysqlc_prepared_resultset.hxx:94

commit dfd076170a045543e467cb1778e9003c11f69231

tdf#138715 remove mork strings and install targets
Hopefully fixes the package build. Regression from commit
f9210f6cf45b2a6149d602f48c0f1ec1fc0e5f08 ("tdf#138715 remove mork
driver").

Change-Id: I0faad65af834e3668c9845eadfe3439ae57b3699
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107692
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

Repository.mk
@@ -43,7 +43,6 @@ $(eval $(call gb_Helper_register_executables,NONE, \
 	localize \
 	makedepend \
 	mbsdiff \
-	mork_helper \
 	osl_process_child \
 	pdf2xml \
 	pdfunzip \
@@ -384,8 +383,6 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	$(if $(MERGELIBS),merged) \
 	migrationoo2 \
 	migrationoo3 \
-	mork \
-	mozbootstrap \
 	msfilter \
 	$(call gb_Helper_optional,SCRIPTING,msforms) \
 	mtfrenderer \

compilerplugins/clang/staticmethods.cxx
@@ -150,8 +150,6 @@ bool StaticMethods::TraverseCXXMethodDecl(const CXXMethodDecl * pCXXMethodDecl) 
          .Class("OleEmbeddedObject").GlobalNamespace())
         || (fdc.Function("GetRidOfComponent").Class("OleEmbeddedObject")
             .GlobalNamespace())
-        || (fdc.Function("isProfileLocked").Class("ProfileAccess")
-            .Namespace("mozab").Namespace("connectivity").GlobalNamespace())
         || cdc.Class("SbxDecimal").GlobalNamespace()
         || fdc.Function("Call").Class("SbiDllMgr").GlobalNamespace()
         || fdc.Function("FreeDll").Class("SbiDllMgr").GlobalNamespace()

compilerplugins/clang/unusedmethods.results
@@ -54,8 +54,6 @@ connectivity/source/drivers/firebird/Util.hxx:72
     short connectivity::firebird::ColumnTypeInfo::getSubType() const
 connectivity/source/drivers/firebird/Util.hxx:74
     const class rtl::OUString & connectivity::firebird::ColumnTypeInfo::getCharacterSet() const
-connectivity/source/drivers/mork/mork_helper.cxx:37
-    int main(int,char **)
 connectivity/source/drivers/mysqlc/mysqlc_connection.hxx:176
     class rtl::OUString connectivity::mysqlc::OConnection::transFormPreparedStatement(const class rtl::OUString &)
 connectivity/source/drivers/mysqlc/mysqlc_prepared_resultset.hxx:94

connectivity/inc/strings.hrc
@@ -17,22 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef CONNECTIVITY_STRINGS_HRC
-#define CONNECTIVITY_STRINGS_HRC
+#pragma once
 
 #define NC_(Context, String) reinterpret_cast<char const *>(Context "\004" u8##String)
 
-// = the mozab driver's resource strings
-#define STR_ERR_EXECUTING_QUERY                 NC_("STR_ERR_EXECUTING_QUERY", "An error occurred while executing the query.")
-#define STR_QUERY_AT_LEAST_ONE_TABLES           NC_("STR_QUERY_AT_LEAST_ONE_TABLES", "The query can not be executed. It needs at least one table.")
-#define STR_NO_COUNT_SUPPORT                    NC_("STR_NO_COUNT_SUPPORT", "The driver does not support the 'COUNT' function.")
-#define STR_STMT_TYPE_NOT_SUPPORTED             NC_("STR_STMT_TYPE_NOT_SUPPORTED", "This statement type not supported by this database driver.")
-#define STR_UNSPECIFIED_ERROR                   NC_("STR_UNSPECIFIED_ERROR", "An unknown error occurred.")
-#define STR_ERROR_REFRESH_ROW                   NC_("STR_ERROR_REFRESH_ROW", "An error occurred while refreshing the current row.")
-#define STR_ERROR_GET_ROW                       NC_("STR_ERROR_GET_ROW", "An error occurred while getting the current row.")
-#define STR_QUERY_INVALID_IS_NULL_COLUMN        NC_("STR_QUERY_INVALID_IS_NULL_COLUMN", "The query can not be executed. The 'IS NULL' can only be used with a column name.")
-#define STR_ILLEGAL_MOVEMENT                    NC_("STR_ILLEGAL_MOVEMENT", "Illegal cursor movement occurred.")
-#define STR_COMMIT_ROW                          NC_("STR_COMMIT_ROW", "Please commit row '$position$' before update rows or insert new rows.")
 // = common strings
 #define STR_NO_CONNECTION_GIVEN                 NC_("STR_NO_CONNECTION_GIVEN", "No connection to the database exists.")
 #define STR_WRONG_PARAM_INDEX                   NC_("STR_WRONG_PARAM_INDEX", "You tried to set a parameter at position '$pos$' but there is/are only '$count$' parameter(s) allowed. One reason may be that the property \"ParameterNameSubstitution\" is not set to TRUE in the data source.")
@@ -138,7 +126,5 @@
 #define STR_DB_NOT_CONNECTED                    NC_("STR_DB_NOT_CONNECTED", "No connection to the database exists.")
 #define STR_AB_ADDRESSBOOK_NOT_FOUND            NC_("STR_AB_ADDRESSBOOK_NOT_FOUND", "No $1$ exists.")
 #define STR_DATA_CANNOT_SELECT_UNFILTERED       NC_("STR_DATA_CANNOT_SELECT_UNFILTERED", "Unable to display the complete table content. Please apply a filter.")
-
-#endif // CONNECTIVITY_RESOURCE_MOZAB_HRC
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/sal/log-areas.dox
@@ -82,8 +82,6 @@ certain functionality.
 @li @c connectivity.kab
 @li @c connectivity.macab
 @li @c connectivity.manager
-@li @c connectivity.mork
-@li @c connectivity.mozab
 @li @c connectivity.mysqlc
 @li @c connectivity.odbc
 @li @c connectivity.parse