LemmingAvalanche/libreoffice-repaired

commit edc91b544229bbcbdc71a722827a510584ef9ad2

tdf#128399 sw btlr: fix clicking to lower rotated cell
In case a table has more than 1 btlr cells in multiple rows, the first
captured the mouse.

This was because SwFrame::GetPaintArea() assumed that left and right
edges can be compared directly, without going via the SwRectFnSet
abstraction. This works for hozizontal and plain vertical directions,
but not for btlr.

Change-Id: Ia3bd7623a5d8d9ad3bdfabceb10adc0dbf105059
Reviewed-on: https://gerrit.libreoffice.org/81688
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/ssfrm.cxx
@@ -585,6 +585,7 @@ SwRect SwFrame::GetPaintArea() const
     SwRect aRect = IsRowFrame() ? GetUpper()->getFrameArea() : getFrameArea();
     const bool bVert = IsVertical();
     SwRectFn fnRect = bVert ? ( IsVertLR() ? (IsVertLRBT() ? fnRectVertL2RB2T : fnRectVertL2R) : fnRectVert ) : fnRectHori;
+    SwRectFnSet aRectFnSet(this);
     long nRight = (aRect.*fnRect->fnGetRight)();
     long nLeft  = (aRect.*fnRect->fnGetLeft)();
     const SwFrame* pTmp = this;
@@ -613,9 +614,9 @@ SwRect SwFrame::GetPaintArea() const
             pTmp->IsCellFrame() || pTmp->IsRowFrame() || //nobody leaves a table!
             pTmp->IsRootFrame() )
         {
-            if( bLeft || nLeft < nTmpLeft )
+            if( bLeft || aRectFnSet.XDiff(nTmpLeft, nLeft) > 0 )
                 nLeft = nTmpLeft;
-            if( bRight || nTmpRight < nRight )
+            if( bRight || aRectFnSet.XDiff(nRight, nTmpRight) > 0 )
                 nRight = nTmpRight;
             if( pTmp->IsPageFrame() || pTmp->IsFlyFrame() || pTmp->IsRootFrame() )
                 break;
@@ -628,14 +629,14 @@ SwRect SwFrame::GetPaintArea() const
             // the first column has _no_ influence to the left range
             if( bR2L ? pTmp->GetNext() : pTmp->GetPrev() )
             {
-                if( bLeft || nLeft < nTmpLeft )
+                if( bLeft || aRectFnSet.XDiff(nTmpLeft, nLeft) > 0 )
                     nLeft = nTmpLeft;
                 bLeft = false;
             }
              // the last column has _no_ influence to the right range
             if( bR2L ? pTmp->GetPrev() : pTmp->GetNext() )
             {
-                if( bRight || nTmpRight < nRight )
+                if( bRight || aRectFnSet.XDiff(nRight, nTmpRight) > 0 )
                     nRight = nTmpRight;
                 bRight = false;
             }
@@ -648,14 +649,14 @@ SwRect SwFrame::GetPaintArea() const
             // the next frame of a body frame may be a footnotecontainer or
             // a footer. The footnotecontainer has the same direction like
             // the body frame.
-            if( pTmp->GetPrev() && ( bLeft || nLeft < nTmpLeft ) )
+            if( pTmp->GetPrev() && ( bLeft || aRectFnSet.XDiff(nTmpLeft, nLeft) > 0 ) )
             {
                 nLeft = nTmpLeft;
                 bLeft = false;
             }
             if( pTmp->GetNext() &&
                 ( pTmp->GetNext()->IsFooterFrame() || pTmp->GetNext()->GetNext() )
-                && ( bRight || nTmpRight < nRight ) )
+                && ( bRight || aRectFnSet.XDiff(nRight, nTmpRight) > 0 ) )
             {
                 nRight = nTmpRight;
                 bRight = false;

sw/qa/extras/layout/layout.cxx
@@ -3386,6 +3386,33 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testContinuousEndnotesDeletePageAtStart)
     assertXPath(pXmlDoc, "/root/page[1]/ftncont", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf128399)
+{
+    SwDoc* pDoc = createDoc("tdf128399.docx");
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    SwFrame* pPage = pLayout->GetLower();
+    SwFrame* pBody = pPage->GetLower();
+    SwFrame* pTable = pBody->GetLower();
+    SwFrame* pRow1 = pTable->GetLower();
+    SwFrame* pRow2 = pRow1->GetNext();
+    const SwRect& rRow2Rect = pRow2->getFrameArea();
+    Point aPoint = rRow2Rect.Center();
+
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SwPosition aPosition = *pWrtShell->GetCursor()->Start();
+    SwPosition aFirstRow(aPosition);
+    SwCursorMoveState aState(MV_NONE);
+    pLayout->GetCursorOfst(&aPosition, aPoint, &aState);
+    // Second row is +3: end node, start node and the first text node in the 2nd row.
+    sal_uLong nExpected = aFirstRow.nNode.GetIndex() + 3;
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 14
+    // - Actual  : 11
+    // i.e. clicking on the center of the 2nd row placed the cursor in the 1st row.
+    CPPUNIT_ASSERT_EQUAL(nExpected, aPosition.nNode.GetIndex());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 512d52b301e936d8cc35330ee5759d2e8ff43b4a

tdf#138600 sw: fix too small print area for btlr text in nested table
Regression from commit edc91b544229bbcbdc71a722827a510584ef9ad2
(tdf#128399 sw btlr: fix clicking to lower rotated cell, 2019-10-29),
the bugdoc has a btlr table cell and the row frame of the outer table
has a bottom value which is very small at the point when
SwFrame::GetPaintArea() is invoked for the inner btlr cell.

This means the "cell should not leave its parent" mechanism kicks in and
reduces the bottom of the paint area to a small value, so the text is
not visible at all.

Fix the problem by teaching SwFrame::GetPaintArea() that btlr cell
frames are OK to leave their parent towards the bottom of the page; that
parent will grow at a later phase of the layout process anyway.

Change-Id: I99334bbf0116df8d8ed27f192c81c0441b6c797d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107730
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/layout/ssfrm.cxx
@@ -633,7 +633,8 @@ SwRect SwFrame::GetPaintArea() const
             pTmp->IsCellFrame() || pTmp->IsRowFrame() || //nobody leaves a table!
             pTmp->IsRootFrame() )
         {
-            if( bLeft || aRectFnSet.XDiff(nTmpLeft, nLeft) > 0 )
+            // BTLR is OK to expand towards the physical down direction. Physical down is left.
+            if( bLeft || (aRectFnSet.XDiff(nTmpLeft, nLeft) > 0 && !IsVertLRBT()) )
                 nLeft = nTmpLeft;
             if( bRight || aRectFnSet.XDiff(nRight, nTmpRight) > 0 )
                 nRight = nTmpRight;

sw/qa/core/layout/layout.cxx
@@ -10,6 +10,7 @@
 #include <swmodeltestbase.hxx>
 
 #include <vcl/gdimtf.hxx>
+#include <svx/svdpage.hxx>
 
 #include <wrtsh.hxx>
 #include <docsh.hxx>
@@ -17,7 +18,8 @@
 #include <drawdoc.hxx>
 #include <IDocumentDrawModelAccess.hxx>
 #include <IDocumentState.hxx>
-#include <svx/svdpage.hxx>
+#include <IDocumentLayoutAccess.hxx>
+#include <rootfrm.hxx>
 
 char const DATA_DIRECTORY[] = "/sw/qa/core/layout/data/";
 
@@ -226,6 +228,30 @@ CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTextboxModification)
     // Without the accompanying fix in place, this test would have failed, as the document was
     // marked as modified right after the import.
     CPPUNIT_ASSERT(!pDocShell->IsModified());
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testBtlrNestedCell)
+{
+    // Load a document with a nested table, the inner A1 cell has a btlr text direction.
+    load(DATA_DIRECTORY, "btlr-nested-cell.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    SwFrame* pPage = pLayout->GetLower();
+    SwFrame* pBody = pPage->GetLower();
+    SwFrame* pOuterTable = pBody->GetLower()->GetNext();
+    SwFrame* pInnerTable = pOuterTable->GetLower()->GetLower()->GetLower();
+
+    // Check the paint area of the only text frame in the cell.
+    SwFrame* pTextFrame = pInnerTable->GetLower()->GetLower()->GetLower();
+    tools::Long nFrameBottom = pTextFrame->getFrameArea().Bottom();
+    SwRect aPaintArea = pTextFrame->GetPaintArea();
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected greater or equal than: 2829
+    // - Actual  : 2080
+    // i.e. part of the text frame area was not painted, hiding the actual text.
+    CPPUNIT_ASSERT_GREATEREQUAL(nFrameBottom, aPaintArea.Bottom());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();