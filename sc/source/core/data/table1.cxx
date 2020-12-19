LemmingAvalanche/libreoffice-repaired

commit 992f19f15ad632d76b257edbe730a824f9e0c6e4

replace std::max(std::min()) with std::clamp
Change-Id: I890d19f5e2177294dc1175c90c98b964347f9e85
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105751
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/data/table1.cxx
@@ -1164,7 +1164,7 @@ void ScTable::LimitChartArea( SCCOL& rStartCol, SCROW& rStartRow, SCCOL& rEndCol
     SCROW lastDataPos = 0;
     for (SCCOL i=rStartCol; i<=rEndCol; i++)
         lastDataPos = std::max(lastDataPos, aCol[i].GetLastDataPos());
-    rEndRow = std::max( rStartRow, std::min(rEndRow, lastDataPos));
+    rEndRow = std::clamp( rEndRow, rStartRow, lastDataPos );
 }
 
 SCCOL ScTable::FindNextVisibleCol( SCCOL nCol, bool bRight ) const

cui/source/dialogs/colorpicker.cxx
@@ -110,9 +110,9 @@ static void CMYKtoRGB( double fCyan, double fMagenta, double fYellow, double fKe
     fMagenta = (fMagenta * ( 1.0 - fKey )) + fKey;
     fYellow = (fYellow * ( 1.0 - fKey )) + fKey;
 
-    dR = std::max( std::min( ( 1.0 - fCyan ), 1.0), 0.0 );
-    dG = std::max( std::min( ( 1.0 - fMagenta ), 1.0), 0.0 );
-    dB = std::max( std::min( ( 1.0 - fYellow ), 1.0), 0.0 );
+    dR = std::clamp( 1.0 - fCyan, 0.0, 1.0 );
+    dG = std::clamp( 1.0 - fMagenta, 0.0, 1.0 );
+    dB = std::clamp( 1.0 - fYellow, 0.0, 1.0 );
 }
 
 // CMY results from 0 to 1

drawinglayer/source/processor3d/zbufferprocessor3d.cxx
@@ -298,7 +298,7 @@ void ZBufferRasterConverter3D::processLineSpan(const basegfx::RasterConversionLi
     while(nXA < nXB)
     {
         // early-test Z values if we need to do anything at all
-        const double fNewZ(std::max(0.0, std::min(double(0xffff), maIntZ.getVal())));
+        const double fNewZ(std::clamp(maIntZ.getVal(), 0.0, 65535.0));
         const sal_uInt16 nNewZ(static_cast< sal_uInt16 >(fNewZ));
         sal_uInt16& rOldZ(mrBuffer.getZ(nScanlineIndex));
 

drawinglayer/source/tools/wmfemfhelper.cxx
@@ -1652,8 +1652,8 @@ namespace wmfemfhelper
                                 {
                                     double fRadiusX((nHor * 2.0) / (aRange.getWidth() > 0.0 ? aRange.getWidth() : 1.0));
                                     double fRadiusY((nVer * 2.0) / (aRange.getHeight() > 0.0 ? aRange.getHeight() : 1.0));
-                                    fRadiusX = std::max(0.0, std::min(1.0, fRadiusX));
-                                    fRadiusY = std::max(0.0, std::min(1.0, fRadiusY));
+                                    fRadiusX = std::clamp(fRadiusX, 0.0, 1.0);
+                                    fRadiusY = std::clamp(fRadiusY, 0.0, 1.0);
 
                                     aOutline = basegfx::utils::createPolygonFromRect(aRange, fRadiusX, fRadiusY);
                                 }

framework/source/layoutmanager/toolbarlayoutmanager.cxx
@@ -2714,8 +2714,8 @@ void ToolbarLayoutManager::implts_calcDockingPosSize(
                     {
                         if ( bHorizontalDockArea )
                         {
-                            sal_Int32 nSize = ::std::max( sal_Int32( 0 ), std::min( sal_Int32( aContainerWinSize.Width() -  aWindowRect.Left() ),
-                                                                                    sal_Int32( aTrackingRect.getWidth() )));
+                            sal_Int32 nSize = std::clamp( sal_Int32(aContainerWinSize.Width() -  aWindowRect.Left()),
+                                                          sal_Int32(0), sal_Int32(aTrackingRect.getWidth()) );
                             if ( nSize == 0 )
                                 nSize = aWindowRect.getWidth();
 
@@ -2728,9 +2728,8 @@ void ToolbarLayoutManager::implts_calcDockingPosSize(
                         }
                         else
                         {
-                            sal_Int32 nSize = ::std::max( sal_Int32( 0 ), std::min( sal_Int32(
-                                                    nTopDockingAreaSize + nMaxLeftRightDockAreaSize - aWindowRect.Top() ),
-                                                    sal_Int32( aTrackingRect.getHeight() )));
+                            sal_Int32 nSize = std::clamp( sal_Int32(nTopDockingAreaSize + nMaxLeftRightDockAreaSize - aWindowRect.Top()),
+                                                          sal_Int32(0), sal_Int32(aTrackingRect.getHeight()) );
                             if ( nSize == 0 )
                                 nSize = aWindowRect.getHeight();
 
@@ -2751,8 +2750,8 @@ void ToolbarLayoutManager::implts_calcDockingPosSize(
                     {
                         if ( bHorizontalDockArea )
                         {
-                            sal_Int32 nSize = ::std::max( sal_Int32( 0 ), std::min( sal_Int32(( aContainerWinSize.Width() ) - aWindowRect.Right() ),
-                                                                                    sal_Int32( aTrackingRect.getWidth() )));
+                            sal_Int32 nSize = ::std::clamp( sal_Int32(aContainerWinSize.Width() - aWindowRect.Right()),
+                                                            sal_Int32(0), sal_Int32(aTrackingRect.getWidth()) );
                             if ( nSize == 0 )
                             {
                                 aUIElementRect.SetPos( ::Point( aContainerWinSize.Width() - aTrackingRect.getWidth(), aWindowRect.Top() ));
@@ -2772,8 +2771,8 @@ void ToolbarLayoutManager::implts_calcDockingPosSize(
                         }
                         else
                         {
-                            sal_Int32 nSize = ::std::max( sal_Int32( 0 ), std::min( sal_Int32( nTopDockingAreaSize + nMaxLeftRightDockAreaSize - aWindowRect.Bottom() ),
-                                                                                    sal_Int32( aTrackingRect.getHeight() )));
+                            sal_Int32 nSize = std::clamp( sal_Int32(nTopDockingAreaSize + nMaxLeftRightDockAreaSize - aWindowRect.Bottom()),
+                                                            sal_Int32(0), sal_Int32(aTrackingRect.getHeight()) );
                             aUIElementRect.SetPos( ::Point( aWindowRect.Left(), aWindowRect.Bottom() ));
                             aUIElementRect.SetSize( ::Size( aWindowRect.getWidth(), nSize ));
 

framework/source/uielement/progressbarwrapper.cxx
@@ -203,7 +203,7 @@ void ProgressBarWrapper::setValue( ::sal_Int32 nValue )
         if ( m_nRange > 0 )
         {
             fVal = ( double( nValue ) / double( m_nRange )) * 100;
-            fVal = std::max( double( 0 ), std::min( fVal, double( 100 )));
+            fVal = std::clamp( fVal, 0.0, 100.0 );
         }
 
         if ( m_nValue != sal_Int32( fVal ))

oox/source/vml/vmlshapecontext.cxx
@@ -78,7 +78,7 @@ OptValue< double > lclDecodeOpacity( const AttributeList& rAttribs, sal_Int32 nT
         {
             if(aString.endsWith("f"))
             {
-                fRetval = std::max(0.0, std::min(1.0, aString.toDouble() / 65536.0));
+                fRetval = std::clamp(aString.toDouble() / 65536.0, 0.0, 1.0);
             }
             else
             {

sal/rtl/math.cxx
@@ -288,7 +288,7 @@ void doubleToString(typename T::String ** pResult,
             if (nDecPlaces == rtl_math_DecimalPlaces_Max)
                 nDecPlaces = 0;
             else
-                nDecPlaces = ::std::max< sal_Int32 >(::std::min<sal_Int32>(nDecPlaces, 15), -15);
+                nDecPlaces = ::std::clamp< sal_Int32 >(nDecPlaces, -15, 15);
 
             if (bEraseTrailingDecZeros && nDecPlaces > 0)
                 nDecPlaces = 0;
@@ -437,7 +437,7 @@ void doubleToString(typename T::String ** pResult,
     // rtl_math_DecimalPlaces_Max was passed with rtl_math_StringFormat_F or
     // others, but we don't want to allocate/deallocate 2GB just to fill it
     // with trailing '0' characters..
-    nDecPlaces = std::max<sal_Int32>(std::min<sal_Int32>(nDecPlaces, 20), -20);
+    nDecPlaces = std::clamp<sal_Int32>(nDecPlaces, -20, 20);
 
     sal_Int32 nDigits = nDecPlaces + 1;
 

commit 510411a3e49976bc79b235989de11be55ccf068a

fix crash inserting chart
regression from
    commit 992f19f15ad632d76b257edbe730a824f9e0c6e4
    Author: Noel <noelgrandin@gmail.com>
    Date:   Thu Nov 12 15:38:13 2020 +0200
    replace std::max(std::min()) with std::clamp

Thanks to Leyan Ouyang for debugging this.

Change-Id: Iafd6d47c1974be2cd55f76aa1fc5f4f614a8d093
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108020
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/data/table1.cxx
@@ -1164,7 +1164,10 @@ void ScTable::LimitChartArea( SCCOL& rStartCol, SCROW& rStartRow, SCCOL& rEndCol
     SCROW lastDataPos = 0;
     for (SCCOL i=rStartCol; i<=rEndCol; i++)
         lastDataPos = std::max(lastDataPos, aCol[i].GetLastDataPos());
-    rEndRow = std::clamp( rEndRow, rStartRow, lastDataPos );
+    // reduce EndRow to the last row with data
+    rEndRow = std::min(rEndRow, lastDataPos);
+    // but make sure EndRow is >= StartRow
+    rEndRow = std::max(rStartRow, rEndRow);
 }
 
 SCCOL ScTable::FindNextVisibleCol( SCCOL nCol, bool bRight ) const