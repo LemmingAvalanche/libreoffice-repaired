LemmingAvalanche/libreoffice-repaired

commit 6f22885b5e9ad6a0ee7a619849ce133d5c302963

loplugin:unusedmethods
Change-Id: I2dd10873be73256a3689233c7b1e37bde8f685ee
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100820
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/webdav/webdavprovider.cxx
@@ -18,7 +18,6 @@
  */
 
 #include <ucbhelper/contentidentifier.hxx>
-#include <ucbhelper/getcomponentcontext.hxx>
 #include <ucbhelper/macros.hxx>
 #include "webdavprovider.hxx"
 #include "webdavcontent.hxx"

ucb/source/ucp/webdav-neon/webdavprovider.cxx
@@ -31,7 +31,6 @@
 #include <com/sun/star/ucb/IllegalIdentifierException.hpp>
 #include <comphelper/processfactory.hxx>
 #include <ucbhelper/contentidentifier.hxx>
-#include <ucbhelper/getcomponentcontext.hxx>
 #include <ucbhelper/macros.hxx>
 #include <cppuhelper/queryinterface.hxx>
 #include "webdavprovider.hxx"

dbaccess/Library_dbu.mk
@@ -102,7 +102,6 @@ $(eval $(call gb_Library_add_exception_objects,dbu,\
     dbaccess/source/ui/control/FieldDescControl \
     dbaccess/source/ui/control/opendoccontrols \
     dbaccess/source/ui/control/RelationControl \
-    dbaccess/source/ui/control/ScrollHelper \
     dbaccess/source/ui/control/sqledit \
     dbaccess/source/ui/control/SqlNameEdit \
     dbaccess/source/ui/control/TableGrantCtrl \

dbaccess/source/ui/control/ScrollHelper.cxx
@@ -1,52 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-
-#include <ScrollHelper.hxx>
-
-#define LISTBOX_SCROLLING_AREA  12
-namespace dbaui
-{
-
-    OScrollHelper::OScrollHelper()
-    {
-    }
-    OScrollHelper::~OScrollHelper()
-    {
-
-    }
-    void OScrollHelper::scroll(const Point& _rPoint, const Size& _rOutputSize)
-    {
-        // Scrolling Areas
-        tools::Rectangle aScrollArea( Point(0, _rOutputSize.Height() - LISTBOX_SCROLLING_AREA),
-                                     Size(_rOutputSize.Width(), LISTBOX_SCROLLING_AREA) );
-
-        // if pointer in bottom area begin scroll
-        if( aScrollArea.IsInside(_rPoint) )
-            m_aUpScroll.Call(nullptr);
-        else
-        {
-            aScrollArea.SetPos(Point(0,0));
-            // if pointer in top area begin scroll
-            if( aScrollArea.IsInside(_rPoint) )
-                m_aDownScroll.Call(nullptr);
-        }
-    }
-}
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

dbaccess/source/ui/control/tabletree.cxx
@@ -447,22 +447,9 @@ void TableTreeListBox::UpdateTableList( const Reference< XConnection >& _rxConne
     m_xTreeView->make_sorted();
 }
 
-bool OTableTreeListBox::isWildcardChecked(const weld::TreeIter& rEntry)
-{
-    return m_xTreeView->get_text_emphasis(rEntry, 0);
-}
-
 bool TableTreeListBox::isWildcardChecked(const weld::TreeIter& rEntry)
 {
     return m_xTreeView->get_text_emphasis(rEntry, 0);
-}
-
-void OTableTreeListBox::checkWildcard(weld::TreeIter& rEntry)
-{
-    if (!m_bShowToggles)
-        return;
-    m_xTreeView->set_toggle(rEntry, TRISTATE_TRUE);
-    checkedButton_noBroadcast(rEntry);
 }
 
 void TableTreeListBox::checkWildcard(weld::TreeIter& rEntry)

dbaccess/source/ui/inc/ScrollHelper.hxx
@@ -1,67 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-#ifndef INCLUDED_DBACCESS_SOURCE_UI_INC_SCROLLHELPER_HXX
-#define INCLUDED_DBACCESS_SOURCE_UI_INC_SCROLLHELPER_HXX
-
-#include <tools/link.hxx>
-#include <tools/gen.hxx>
-
-namespace dbaui
-{
-    class OScrollHelper
-    {
-        Link<LinkParamNone*,void>  m_aUpScroll;
-        Link<LinkParamNone*,void>  m_aDownScroll;
-    public:
-        /** default constructor
-        */
-        OScrollHelper();
-
-        ~OScrollHelper();
-
-        /** set the memthod which should be called when scrolling up
-            @param  _rUpScroll
-                the method to set
-        */
-        void setUpScrollMethod( const Link<LinkParamNone*,void>& _rUpScroll )
-        {
-            m_aUpScroll = _rUpScroll;
-        }
-
-        /** set the memthod which should be called when scrolling down
-            @param  _rDownScroll
-                the method to set
-        */
-        void setDownScrollMethod( const Link<LinkParamNone*,void>& _rDownScroll )
-        {
-            m_aDownScroll = _rDownScroll;
-        }
-
-        /** check if a scroll method has to be called
-            @param  _rPoint
-                the current selection point
-            @param  _rOutputSize
-                the output size of the window
-        */
-        void scroll(const Point& _rPoint, const Size& _rOutputSize);
-    };
-}
-#endif // INCLUDED_DBACCESS_SOURCE_UI_INC_SCROLLHELPER_HXX
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

dbaccess/source/ui/inc/dbtreelistbox.hxx
@@ -19,8 +19,6 @@
 #ifndef INCLUDED_DBACCESS_SOURCE_UI_INC_DBTREELISTBOX_HXX
 #define INCLUDED_DBACCESS_SOURCE_UI_INC_DBTREELISTBOX_HXX
 
-#include "ScrollHelper.hxx"
-
 #include <com/sun/star/frame/XPopupMenuController.hpp>
 
 #include <vcl/InterimItemWindow.hxx>

dbaccess/source/ui/inc/imageprovider.hxx
@@ -116,18 +116,6 @@ namespace dbaui
         */
         static OUString getDefaultImageResourceID(sal_Int32 _nDatabaseObjectType);
 
-        /** retrieves the image to be used for folders of database objects
-            @param  _nDatabaseObjectType
-                the type of the object. Must be one of the css.sdb.application.DatabaseObject
-                constants.
-            @param  _rName
-                the name of the object
-            @return
-                the image to be used for folders of the given type
-        */
-        static Image getFolderImage(
-            sal_Int32 _nDatabaseObjectType
-        );
         static OUString getFolderImageId(
             sal_Int32 _nDatabaseObjectType
         );

commit 7a7f5a214d36aa02a616f2e7c72fef758fc4726f

ucp: fix call to getComponentContext
Commit 6f22885b5e9ad6a0ee7a619849ce133d5c302963 mistakenly removed
include/ucbhelper/getcomponentcontext.hxx seeing the getComponentContext method
as unused.
According to that file, ucbhelper::getComponentContext was just a duplicate of
comphelper::getComponentContext so this commit changes to calling that method
instead.

Change-Id: Ibd66b56a72afce437a26417d7944ec1ace34a4eb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108262
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/webdav/webdavprovider.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <comphelper/processfactory.hxx>
 #include <ucbhelper/contentidentifier.hxx>
 #include <ucbhelper/macros.hxx>
 #include "webdavprovider.hxx"
@@ -106,7 +107,7 @@ static css::uno::Reference< css::uno::XInterface >
 ContentProvider_CreateInstance( const css::uno::Reference< css::lang::XMultiServiceFactory> & rSMgr )
 {
     css::lang::XServiceInfo* pX =
-                static_cast<css::lang::XServiceInfo*>(new ContentProvider( ucbhelper::getComponentContext(rSMgr) ));
+                static_cast<css::lang::XServiceInfo*>(new ContentProvider( comphelper::getComponentContext(rSMgr) ));
     return css::uno::Reference< css::uno::XInterface >::query( pX );
 }
 