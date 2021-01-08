LemmingAvalanche/libreoffice-repaired

commit 41f1533cb5482bac78dce16953c423e15f3043ba

tdf#138194 Chart OOXML import: set text break to true
of rotated category axis label, if the rotation is 90
or 270 degree and the inner size of the chart is not fixed.

Change-Id: I02e1fd940af8a277435aa46d4ad93a42b6723710
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106422
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/axisconverter.cxx
@@ -137,9 +137,10 @@ AxisConverter::~AxisConverter()
 {
 }
 
-void AxisConverter::convertFromModel(
-    const Reference< XCoordinateSystem >& rxCoordSystem,
-    RefVector<TypeGroupConverter>& rTypeGroups, const AxisModel* pCrossingAxis, sal_Int32 nAxesSetIdx, sal_Int32 nAxisIdx )
+void AxisConverter::convertFromModel(const Reference<XCoordinateSystem>& rxCoordSystem,
+                                     RefVector<TypeGroupConverter>& rTypeGroups,
+                                     const AxisModel* pCrossingAxis, sal_Int32 nAxesSetIdx,
+                                     sal_Int32 nAxisIdx, bool bUseFixedInnerSize)
 {
     if (rTypeGroups.empty())
         return;
@@ -269,10 +270,17 @@ void AxisConverter::convertFromModel(
                 }
                 else
                 {
-                    // do not overlap text unless all labels are visible
-                    aAxisProp.setProperty( PROP_TextOverlap, mrModel.mnTickLabelSkip == 1 );
-                    // do not break text into several lines unless the rotation is 0 degree
-                    aAxisProp.setProperty( PROP_TextBreak, ObjectFormatter::getTextRotation( mrModel.mxTextProp ) );
+                    aAxisProp.setProperty(PROP_TextOverlap, true);
+                    /* do not break text into several lines unless the rotation is 0 degree,
+                       or the rotation is 90 degree and the inner size of the chart is not fixed,
+                       or the rotation is 270 degree and the inner size of the chart is not fixed */
+                    bool bTextBreak = true;
+                    double fRotationAngle = 0.0;
+                    if (aAxisProp.getProperty(fRotationAngle, PROP_TextRotation)
+                        && fRotationAngle != 0.0)
+                        bTextBreak = !bUseFixedInnerSize
+                                     && (fRotationAngle == 90.0 || fRotationAngle == 270.0);
+                    aAxisProp.setProperty(PROP_TextBreak, bTextBreak);
                     // do not stagger labels in two lines
                     aAxisProp.setProperty( PROP_ArrangeOrder, cssc::ChartAxisArrangeOrderType_SIDE_BY_SIDE );
                     //! TODO #i58731# show n-th category

sw/qa/extras/layout/layout2.cxx
@@ -700,6 +700,23 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testHorizontal_multilevel)
     CPPUNIT_ASSERT_DOUBLES_EQUAL(7945, nYposition, 20);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf138194)
+{
+    SwDoc* pDoc = createDoc("xaxis-labelbreak.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 8
+    // - Actual  : 7
+    // i.e. the X axis label flowed out of chart area.
+    assertXPath(pXmlDoc, "//textarray", 8);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf124796)
 {
     SwDoc* pDoc = createDoc("tdf124796.odt");

compilerplugins/clang/constantparam.booleans.results
@@ -1930,10 +1930,6 @@ o3tl/qa/cow_wrapper_clients.hxx:41
     void o3tltests::cow_wrapper_client1::cow_wrapper_client1(int)
     int nVal
     1
-oox/inc/drawingml/chart/objectformatter.hxx:146
-    _Bool oox::drawingml::chart::ObjectFormatter::getTextRotation(const class oox::drawingml::chart::ModelRef<class oox::drawingml::TextBody> &,int)
-    int nDefaultRotation
-    0
 oox/inc/drawingml/chart/typegroupconverter.hxx:153
     void oox::drawingml::chart::TypeGroupConverter::convertLineSmooth(class oox::PropertySet &,_Bool) const
     _Bool bOoxSmooth

oox/inc/drawingml/chart/axisconverter.hxx
@@ -60,7 +60,7 @@ public:
     void convertFromModel(
         const css::uno::Reference<css::chart2::XCoordinateSystem>& rxCoordSystem,
         RefVector<TypeGroupConverter>& rTypeGroups, const AxisModel* pCrossingAxis,
-        sal_Int32 nAxesSetIdx, sal_Int32 nAxisIdx );
+        sal_Int32 nAxesSetIdx, sal_Int32 nAxisIdx, bool bUseFixedInnerSize );
 };
 
 

oox/inc/drawingml/chart/objectformatter.hxx
@@ -142,11 +142,6 @@ public:
     /** Returns true, if the passed shape properties have automatic fill mode. */
     static bool         isAutomaticFill( const ModelRef< Shape >& rxShapeProp );
 
-    /** Returns true, if the X Axis label rotation is 0 degree. */
-    static bool         getTextRotation(
-                            const ModelRef< TextBody >& rxTextProp,
-                            sal_Int32 nDefaultRotation = 0 );
-
 private:
     std::shared_ptr< ObjectFormatterData > mxData;
 };

oox/source/drawingml/chart/objectformatter.cxx
@@ -1139,26 +1139,6 @@ bool ObjectFormatter::isAutomaticFill( const ModelRef< Shape >& rxShapeProp )
     return !rxShapeProp || !rxShapeProp->getFillProperties().moFillType.has();
 }
 
-bool ObjectFormatter::getTextRotation( const ModelRef< TextBody >& rxTextProp, sal_Int32 nDefaultRotation )
-{
-    if( rxTextProp.is() )
-    {
-        double fAnglevalue = static_cast< double >( rxTextProp->getTextProperties().moRotation.get( nDefaultRotation ) );
-        if( fAnglevalue < -5400000.0 || fAnglevalue > 5400000.0 || fAnglevalue == 0.0 )
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return true;
-    }
-}
-
 } // namespace oox
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

oox/source/drawingml/chart/plotareaconverter.cxx
@@ -70,7 +70,8 @@ public:
                             const Reference< XDiagram >& rxDiagram,
                             View3DModel& rView3DModel,
                             sal_Int32 nAxesSetIdx,
-                            bool bSupportsVaryColorsByPoint );
+                            bool bSupportsVaryColorsByPoint,
+                            bool bUseFixedInnerSize );
 
     /** Returns the automatic chart title if the axes set contains only one series. */
     const OUString& getAutomaticTitle() const { return maAutoTitle; }
@@ -105,7 +106,8 @@ ModelRef< AxisModel > lclGetOrCreateAxis( const AxesSetModel::AxisMap& rFromAxes
 }
 
 void AxesSetConverter::convertFromModel( const Reference< XDiagram >& rxDiagram,
-        View3DModel& rView3DModel, sal_Int32 nAxesSetIdx, bool bSupportsVaryColorsByPoint )
+                                        View3DModel& rView3DModel, sal_Int32 nAxesSetIdx,
+                                        bool bSupportsVaryColorsByPoint, bool bUseFixedInnerSize)
 {
     // create type group converter objects for all type groups
     typedef RefVector< TypeGroupConverter > TypeGroupConvVector;
@@ -165,15 +167,18 @@ void AxesSetConverter::convertFromModel( const Reference< XDiagram >& rxDiagram,
             ModelRef< AxisModel > xYAxis = lclGetOrCreateAxis( mrModel.maAxes, API_Y_AXIS, C_TOKEN( valAx ), bMSO2007Doc );
 
             AxisConverter aXAxisConv( *this, *xXAxis );
-            aXAxisConv.convertFromModel( xCoordSystem, aTypeGroups, xYAxis.get(), nAxesSetIdx, API_X_AXIS );
+            aXAxisConv.convertFromModel(xCoordSystem, aTypeGroups, xYAxis.get(), nAxesSetIdx,
+                                        API_X_AXIS, bUseFixedInnerSize);
             AxisConverter aYAxisConv( *this, *xYAxis );
-            aYAxisConv.convertFromModel( xCoordSystem, aTypeGroups, xXAxis.get(), nAxesSetIdx, API_Y_AXIS );
+            aYAxisConv.convertFromModel(xCoordSystem, aTypeGroups, xXAxis.get(), nAxesSetIdx,
+                                        API_Y_AXIS, bUseFixedInnerSize);
 
             if( rFirstTypeGroup.isDeep3dChart() )
             {
                 ModelRef< AxisModel > xZAxis = lclGetOrCreateAxis( mrModel.maAxes, API_Z_AXIS, C_TOKEN( serAx ), bMSO2007Doc );
                 AxisConverter aZAxisConv( *this, *xZAxis );
-                aZAxisConv.convertFromModel( xCoordSystem, aTypeGroups, nullptr, nAxesSetIdx, API_Z_AXIS );
+                aZAxisConv.convertFromModel(xCoordSystem, aTypeGroups, nullptr, nAxesSetIdx,
+                                            API_Z_AXIS, bUseFixedInnerSize);
             }
 
             // convert all chart type groups, this converts all series data and formatting
@@ -420,10 +425,15 @@ void PlotAreaConverter::convertFromModel( View3DModel& rView3DModel )
             && aAxesSets[0]->maAxes[ API_Y_AXIS ]->mnAxisId != rValAxisIds[0] ) ? 1 : 0;
     sal_Int32 nAxesSetIdx = nStartAxesSetIdx;
 
+    bool bUseFixedInnerSize = false;
+    if (mrModel.mxLayout && !mrModel.mxLayout->mbAutoLayout)
+        bUseFixedInnerSize = mrModel.mxLayout->mnTarget == XML_inner;
+
     for (auto const& axesSet : aAxesSets)
     {
         AxesSetConverter aAxesSetConv(*this, *axesSet);
-        aAxesSetConv.convertFromModel( xDiagram, rView3DModel, nAxesSetIdx, bSupportsVaryColorsByPoint );
+        aAxesSetConv.convertFromModel(xDiagram, rView3DModel, nAxesSetIdx,
+                                      bSupportsVaryColorsByPoint, bUseFixedInnerSize);
         if(nAxesSetIdx == nStartAxesSetIdx)
         {
             maAutoTitle = aAxesSetConv.getAutomaticTitle();

commit 94a865c185963fe37e49f8dd9ed0172e25460382

tdf138773 Chart OOXML import: set text overlap to false
of category axis label, if the rotation is undefined in xml,
to avoid e.g. missing rotation of them.

Regression from commit: 41f1533cb5482bac78dce16953c423e15f3043ba
(tdf#138194 Chart OOXML import: set text break to true)

Change-Id: I5b03614eca547a206afc0d8118e3f6c055ed5348
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108792
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/axisconverter.cxx
@@ -38,6 +38,7 @@
 #include <drawingml/chart/titleconverter.hxx>
 #include <drawingml/chart/typegroupconverter.hxx>
 #include <drawingml/lineproperties.hxx>
+#include <drawingml/textbody.hxx>
 #include <oox/token/namespaces.hxx>
 #include <oox/token/properties.hxx>
 #include <oox/token/tokens.hxx>
@@ -270,7 +271,11 @@ void AxisConverter::convertFromModel(const Reference<XCoordinateSystem>& rxCoord
                 }
                 else
                 {
-                    aAxisProp.setProperty(PROP_TextOverlap, true);
+                    // do not overlap text when the rotation is undefined in xml
+                    bool bTextOverlap
+                        = mrModel.mxTextProp.is()
+                          && mrModel.mxTextProp->getTextProperties().moRotation.has();
+                    aAxisProp.setProperty(PROP_TextOverlap, bTextOverlap);
                     /* do not break text into several lines unless the rotation is 0 degree,
                        or the rotation is 90 degree and the inner size of the chart is not fixed,
                        or the rotation is 270 degree and the inner size of the chart is not fixed */

sw/qa/extras/layout/layout2.cxx
@@ -733,6 +733,24 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf138194)
     assertXPath(pXmlDoc, "//textarray", 8);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf138773)
+{
+    SwDoc* pDoc = createDoc("tdf138773.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    const sal_Int32 nFirstLabelLines
+        = getXPathContent(pXmlDoc, "count(//text[contains(text(),\"2000-01\")])").toInt32();
+
+    // This failed, if the first X axis label broke to multiple lines.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), nFirstLabelLines);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf124796)
 {
     SwDoc* pDoc = createDoc("tdf124796.odt");