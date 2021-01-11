LemmingAvalanche/libreoffice-repaired

commit d7c7e693c280465964d6d9655d40d64529376c51

use fastparser in forms
Change-Id: I7d09d64857e24267b4b4baddb563e28ceea92f2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105560
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/xmloff/xmltoken.hxx
@@ -217,6 +217,9 @@ namespace xmloff::token {
         XML_ALIGN,
         XML_ALL,
         XML_ALLOW_EMPTY_CELL,
+        XML_ALLOW_DELETES,
+        XML_ALLOW_INSERTS,
+        XML_ALLOW_UPDATES,
         XML_ALPHABETICAL_INDEX,
         XML_ALPHABETICAL_INDEX_AUTO_MARK_FILE,
         XML_ALPHABETICAL_INDEX_ENTRY_TEMPLATE,
@@ -365,6 +368,7 @@ namespace xmloff::token {
         XML_BUTTON3,
         XML_BUTTON4,
         XML_BUTTONS,
+        XML_BUTTON_TYPE,
         XML_BVAR,
         XML_C,
         XML_CALCULATION_SETTINGS,
@@ -488,6 +492,7 @@ namespace xmloff::token {
         XML_CONTROL,
         XML_CONVERSION_MODE,
         XML_CONVERSION_TYPE,
+        XML_CONVERT_EMPTY_TO_NULL,
         XML_COPY_BACK,
         XML_COPY_FORMULAS,
         XML_COPY_OUTLINE_LEVELS,
@@ -612,6 +617,7 @@ namespace xmloff::token {
         XML_DECORATIVE,
         XML_DEEP,
         XML_DEFAULT,
+        XML_DEFAULT_BUTTON,
         XML_DEFAULT_CELL_STYLE_NAME,
         XML_DEFAULT_STYLE,
         XML_DEFAULT_STYLE_NAME,
@@ -684,6 +690,7 @@ namespace xmloff::token {
         XML_DROPDOWN,
         XML_DROP_CAP,
         XML_DYNAMIC,
+        XML_ECHO_CHAR,
         XML_EDGE_ROUNDING,
         XML_EDITABLE,
         XML_EDITING_CYCLES,
@@ -702,6 +709,7 @@ namespace xmloff::token {
         XML_ENABLE_NUMBERING,
         XML_ENABLED,
         XML_ENCODING,
+        XML_ENCTYPE,
         XML_END,
         XML_END_ANGLE,
         XML_END_CELL_ADDRESS,
@@ -809,6 +817,7 @@ namespace xmloff::token {
         XML_FLOOR,
         XML_FN,
         XML_FOCAL_LENGTH,
+        XML_FOCUS_ON_CLICK,
         XML_FONT_CHAR_WIDTH,
         XML_FONT_CHARSET,
         XML_FONT_CHARSET_ASIAN,
@@ -880,6 +889,7 @@ namespace xmloff::token {
         XML_FOOTNOTE_SEP,
         XML_FOOTNOTES_CONFIGURATION,
         XML_FOOTNOTES_POSITION,
+        XML_FOR,
         XML_FORALL,
         XML_FORCE_MANUAL,
         XML_FOREGROUND,
@@ -1021,6 +1031,7 @@ namespace xmloff::token {
         XML_IDEOGRAPH_ALPHA,
         XML_IGNORE_CASE,
         XML_IGNORE_EMPTY_ROWS,
+        XML_IGNORE_RESULT,
         XML_IGNORE_SELECTED_PAGE, // used for ODF compatibility
         XML_ILLUSTRATION_INDEX,
         XML_ILLUSTRATION_INDEX_ENTRY_TEMPLATE,
@@ -1055,6 +1066,7 @@ namespace xmloff::token {
         XML_INFORMATION,
         XML_INITIAL_CREATOR,
         XML_INPROCEEDINGS,
+        XML_INPUT_REQUIRED,
         XML_INSERT_COLUMNS,
         XML_INSERT_ROWS,
         XML_INSERTION,
@@ -1078,6 +1090,7 @@ namespace xmloff::token {
         XML_IS_SELECTION,
         XML_ISBN,
         XML_ITALIC,
+        XML_ITEM,
         XML_ITERATION,
         XML_JOURNAL,
         XML_JUSTIFIED,
@@ -1302,6 +1315,8 @@ namespace xmloff::token {
         XML_MTEXT,
         XML_MTR,
         XML_MULTI_DELETION_SPANNED,
+        XML_MULTI_LINE,
+        XML_MULTIPLE,
         XML_MUNDER,
         XML_MUNDEROVER,
         XML_NAME,
@@ -1309,6 +1324,7 @@ namespace xmloff::token {
         XML_NAMED_EXPRESSION,
         XML_NAMED_EXPRESSIONS,
         XML_NAMED_RANGE,
+        XML_NAVIGATION_MODE,
         XML_NAVY,
         XML_NEGATIVE_COLOR,
         XML_NEQ,
@@ -1416,6 +1432,7 @@ namespace xmloff::token {
         XML_PAGE_NUMBER,
         XML_PAGE_NUMBER_VISIBLE,
         XML_PAGE_START_MARGIN,
+        XML_PAGE_STEP_SIZE,
         XML_PAGE_STYLE_NAME,
         XML_PAGE_THUMBNAIL,
         XML_PAGE_USAGE,
@@ -1769,6 +1786,7 @@ namespace xmloff::token {
         XML_START_TABLE,
         XML_START_VALUE,
         XML_START_WITH_NAVIGATOR,
+        XML_STATE,
         XML_STATISTICS,
         XML_STATUS,
         XML_STAY_ON_TOP,
@@ -1823,6 +1841,8 @@ namespace xmloff::token {
         XML_SYMBOL_WIDTH,
         XML_SYSTEM,
         XML_TAB_COLOR,
+        XML_TAB_CYCLE,
+        XML_TAB_INDEX,
         XML_TAB_STOP,
         XML_TAB_STOP_DISTANCE,
         XML_TAB_STOPS,
@@ -1930,6 +1950,7 @@ namespace xmloff::token {
         XML_TOC_MARK,
         XML_TOC_MARK_END,
         XML_TOC_MARK_START,
+        XML_TOGGLE,
         XML_TOP,
         XML_TOP_LEFT,
         XML_TOP_PERCENT,
@@ -2012,6 +2033,7 @@ namespace xmloff::token {
         XML_USER_INDEX_SOURCE,
         XML_USER_TRANSFORMED,
         XML_USERNAME,
+        XML_VALIDATION,
         XML_VALUE,
         XML_VALUE_TYPE,
         XML_VALUES_CELL_RANGE_ADDRESS,
@@ -2051,6 +2073,7 @@ namespace xmloff::token {
         XML_VISIBLE_AREA_TOP,
         XML_VISIBLE_AREA_WIDTH,
         XML_VISITED_STYLE_NAME,
+        XML_VISUAL_EFFECT,
         XML_VOLATILE,
         XML_VOLUME,
         XML_VPN,

xmloff/source/core/xmltoken.cxx
@@ -222,6 +222,9 @@ namespace xmloff::token {
         TOKEN( "align",                           XML_ALIGN ),
         TOKEN( "all",                             XML_ALL ),
         TOKEN( "allow-empty-cell",                XML_ALLOW_EMPTY_CELL ),
+        TOKEN( "allow-deletes",                   XML_ALLOW_DELETES ),
+        TOKEN( "allow-inserts",                   XML_ALLOW_INSERTS ),
+        TOKEN( "allow-updates",                   XML_ALLOW_UPDATES ),
         TOKEN( "alphabetical-index",              XML_ALPHABETICAL_INDEX ),
         TOKEN( "alphabetical-index-auto-mark-file",       XML_ALPHABETICAL_INDEX_AUTO_MARK_FILE ),
         TOKEN( "alphabetical-index-entry-template",       XML_ALPHABETICAL_INDEX_ENTRY_TEMPLATE ),
@@ -370,6 +373,7 @@ namespace xmloff::token {
         TOKEN( "button3",                         XML_BUTTON3 ),
         TOKEN( "button4",                         XML_BUTTON4 ),
         TOKEN( "buttons",                         XML_BUTTONS ),
+        TOKEN( "button-type",                     XML_BUTTON_TYPE ),
         TOKEN( "bvar",                            XML_BVAR ),
         TOKEN( "c",                               XML_C ),
         TOKEN( "calculation-settings",            XML_CALCULATION_SETTINGS ),
@@ -493,6 +497,7 @@ namespace xmloff::token {
         TOKEN( "control",                         XML_CONTROL ),
         TOKEN( "conversion-mode",                 XML_CONVERSION_MODE ),
         TOKEN( "conversion-type",                 XML_CONVERSION_TYPE ),
+        TOKEN( "convert-empty-to-null",           XML_CONVERT_EMPTY_TO_NULL ),
         TOKEN( "copy-back",                       XML_COPY_BACK ),
         TOKEN( "copy-formulas",                   XML_COPY_FORMULAS ),
         TOKEN( "copy-outline-levels",             XML_COPY_OUTLINE_LEVELS ),
@@ -617,6 +622,7 @@ namespace xmloff::token {
         TOKEN( "decorative",                      XML_DECORATIVE ),
         TOKEN( "deep",                            XML_DEEP ),
         TOKEN( "default",                         XML_DEFAULT ),
+        TOKEN( "default-button",                  XML_DEFAULT_BUTTON ),
         TOKEN( "default-cell-style-name",         XML_DEFAULT_CELL_STYLE_NAME ),
         TOKEN( "default-style",                   XML_DEFAULT_STYLE ),
         TOKEN( "default-style-name",              XML_DEFAULT_STYLE_NAME ),
@@ -689,6 +695,7 @@ namespace xmloff::token {
         TOKEN( "dropdown",                        XML_DROPDOWN ),
         TOKEN( "drop-cap",                        XML_DROP_CAP ),
         TOKEN( "dynamic",                         XML_DYNAMIC ),
+        TOKEN( "echo-char",                       XML_ECHO_CHAR ),
         TOKEN( "edge-rounding",                   XML_EDGE_ROUNDING ),
         TOKEN( "editable",                        XML_EDITABLE ),
         TOKEN( "editing-cycles",                  XML_EDITING_CYCLES ),
@@ -707,6 +714,7 @@ namespace xmloff::token {
         TOKEN( "enable-numbering",                XML_ENABLE_NUMBERING ),
         TOKEN( "enabled",                         XML_ENABLED ),
         TOKEN( "encoding",                        XML_ENCODING ),
+        TOKEN( "enctype",                         XML_ENCTYPE ),
         TOKEN( "end",                             XML_END ),
         TOKEN( "end-angle",                       XML_END_ANGLE ),
         TOKEN( "end-cell-address",                XML_END_CELL_ADDRESS ),
@@ -814,6 +822,7 @@ namespace xmloff::token {
         TOKEN( "floor",                           XML_FLOOR ),
         TOKEN( "fn",                              XML_FN ),
         TOKEN( "focal-length",                    XML_FOCAL_LENGTH ),
+        TOKEN( "focus-on-click",                  XML_FOCUS_ON_CLICK ),
         TOKEN( "font-char-width",                 XML_FONT_CHAR_WIDTH ),
         TOKEN( "font-charset",                    XML_FONT_CHARSET ),
         TOKEN( "font-charset-asian",              XML_FONT_CHARSET_ASIAN ),
@@ -885,6 +894,7 @@ namespace xmloff::token {
         TOKEN( "footnote-sep",                    XML_FOOTNOTE_SEP ),
         TOKEN( "footnotes-configuration",         XML_FOOTNOTES_CONFIGURATION ),
         TOKEN( "footnotes-position",              XML_FOOTNOTES_POSITION ),
+        TOKEN( "for",                             XML_FOR ),
         TOKEN( "forall",                          XML_FORALL ),
         TOKEN( "force-manual",                    XML_FORCE_MANUAL ),
         TOKEN( "foreground",                      XML_FOREGROUND ),
@@ -1027,6 +1037,7 @@ namespace xmloff::token {
         TOKEN( "ideograph-alpha",                 XML_IDEOGRAPH_ALPHA ),
         TOKEN( "ignore-case",                     XML_IGNORE_CASE ),
         TOKEN( "ignore-empty-rows",               XML_IGNORE_EMPTY_ROWS ),
+        TOKEN( "ignore-result",                   XML_IGNORE_RESULT ),
         TOKEN( "ignore-selected-page",            XML_IGNORE_SELECTED_PAGE ),
         TOKEN( "illustration-index",              XML_ILLUSTRATION_INDEX ),
         TOKEN( "illustration-index-entry-template",       XML_ILLUSTRATION_INDEX_ENTRY_TEMPLATE ),
@@ -1061,6 +1072,7 @@ namespace xmloff::token {
         TOKEN( "information",                     XML_INFORMATION ),
         TOKEN( "initial-creator",                 XML_INITIAL_CREATOR ),
         TOKEN( "inproceedings",                   XML_INPROCEEDINGS ),
+        TOKEN( "input-required",                  XML_INPUT_REQUIRED ),
         TOKEN( "insert-columns",                  XML_INSERT_COLUMNS ),
         TOKEN( "insert-rows",                     XML_INSERT_ROWS ),
         TOKEN( "insertion",                       XML_INSERTION ),
@@ -1084,6 +1096,7 @@ namespace xmloff::token {
         TOKEN( "is-selection",                    XML_IS_SELECTION ),
         TOKEN( "isbn",                            XML_ISBN ),
         TOKEN( "italic",                          XML_ITALIC ),
+        TOKEN( "item",                            XML_ITEM ),
         TOKEN( "iteration",                       XML_ITERATION ),
         TOKEN( "journal",                         XML_JOURNAL ),
         TOKEN( "justified",                       XML_JUSTIFIED ),
@@ -1308,6 +1321,8 @@ namespace xmloff::token {
         TOKEN( "mtext",                           XML_MTEXT ),
         TOKEN( "mtr",                             XML_MTR ),
         TOKEN( "multi-deletion-spanned",          XML_MULTI_DELETION_SPANNED ),
+        TOKEN( "multi-line",                      XML_MULTI_LINE ),
+        TOKEN( "multiple",                        XML_MULTIPLE ),
         TOKEN( "munder",                          XML_MUNDER ),
         TOKEN( "munderover",                      XML_MUNDEROVER ),
         TOKEN( "name",                            XML_NAME ),
@@ -1315,6 +1330,7 @@ namespace xmloff::token {
         TOKEN( "named-expression",                XML_NAMED_EXPRESSION ),
         TOKEN( "named-expressions",               XML_NAMED_EXPRESSIONS ),
         TOKEN( "named-range",                     XML_NAMED_RANGE ),
+        TOKEN( "navigation-mode",                 XML_NAVIGATION_MODE ),
         TOKEN( "navy",                            XML_NAVY ),
         TOKEN( "negative-color",                   XML_NEGATIVE_COLOR ),
         TOKEN( "neq",                             XML_NEQ ),
@@ -1422,6 +1438,7 @@ namespace xmloff::token {
         TOKEN( "page-number",                     XML_PAGE_NUMBER ),
         TOKEN( "page-number-visible",             XML_PAGE_NUMBER_VISIBLE ),
         TOKEN( "page-start-margin",               XML_PAGE_START_MARGIN ),
+        TOKEN( "page-step-size",                  XML_PAGE_STEP_SIZE ),
         TOKEN( "page-style-name",                 XML_PAGE_STYLE_NAME ),
         TOKEN( "page-thumbnail",                  XML_PAGE_THUMBNAIL ),
         TOKEN( "page-usage",                      XML_PAGE_USAGE ),
@@ -1775,6 +1792,7 @@ namespace xmloff::token {
         TOKEN( "start-table",                     XML_START_TABLE ),
         TOKEN( "start-value",                     XML_START_VALUE ),
         TOKEN( "start-with-navigator",            XML_START_WITH_NAVIGATOR ),
+        TOKEN( "state",                           XML_STATE ),
         TOKEN( "statistics",                      XML_STATISTICS ),
         TOKEN( "status",                          XML_STATUS ),
         TOKEN( "stay-on-top",                     XML_STAY_ON_TOP ),
@@ -1829,6 +1847,8 @@ namespace xmloff::token {
         TOKEN( "symbol-width",                    XML_SYMBOL_WIDTH ),
         TOKEN( "system",                          XML_SYSTEM ),
         TOKEN( "tab-color",                       XML_TAB_COLOR ),
+        TOKEN( "tab-cycle",                       XML_TAB_CYCLE ),
+        TOKEN( "tab-index",                       XML_TAB_INDEX ),
         TOKEN( "tab-stop",                        XML_TAB_STOP ),
         TOKEN( "tab-stop-distance",               XML_TAB_STOP_DISTANCE ),
         TOKEN( "tab-stops",                       XML_TAB_STOPS ),
@@ -1936,6 +1956,7 @@ namespace xmloff::token {
         TOKEN( "toc-mark",                        XML_TOC_MARK ),
         TOKEN( "toc-mark-end",                    XML_TOC_MARK_END ),
         TOKEN( "toc-mark-start",                  XML_TOC_MARK_START ),
+        TOKEN( "toggle",                          XML_TOGGLE ),
         TOKEN( "top",                             XML_TOP ),
         TOKEN( "top-left",                        XML_TOP_LEFT ),
         TOKEN( "top percent",                     XML_TOP_PERCENT ),
@@ -2018,6 +2039,7 @@ namespace xmloff::token {
         TOKEN( "user-index-source",               XML_USER_INDEX_SOURCE ),
         TOKEN( "user-transformed",                XML_USER_TRANSFORMED ),
         TOKEN( "username",                        XML_USERNAME ),
+        TOKEN( "validation",                      XML_VALIDATION ),
         TOKEN( "value",                           XML_VALUE ),
         TOKEN( "value-type",                      XML_VALUE_TYPE ),
         TOKEN( "values-cell-range-address",       XML_VALUES_CELL_RANGE_ADDRESS ),
@@ -2057,6 +2079,7 @@ namespace xmloff::token {
         TOKEN( "visible-area-top",                XML_VISIBLE_AREA_TOP ),
         TOKEN( "visible-area-width",              XML_VISIBLE_AREA_WIDTH ),
         TOKEN( "visited-style-name",              XML_VISITED_STYLE_NAME ),
+        TOKEN( "visual-effect",              XML_VISUAL_EFFECT ),
         TOKEN( "volatile",                        XML_VOLATILE ),
         TOKEN( "volume",                          XML_VOLUME ),
         TOKEN( "vpn",                             XML_VPN ),

include/sax/fastattribs.hxx
@@ -73,6 +73,7 @@ class SAX_DLLPUBLIC FastAttributeList final : public cppu::WeakImplHelper< css::
 {
 public:
     FastAttributeList( FastTokenHandlerBase *pTokenHandler );
+    FastAttributeList( const css::uno::Reference< css::xml::sax::XFastAttributeList > & xAttrList );
     virtual ~FastAttributeList() override;
 
     void clear();
@@ -81,6 +82,8 @@ public:
         maAttributeValues.reserve(nNumTokens+1);
         maAttributeTokens.reserve(nNumTokens);
     }
+    void add( const FastAttributeList& );
+    void add( const css::uno::Reference<css::xml::sax::XFastAttributeList>& );
     void add( sal_Int32 nToken, const char* pValue );
     void add( sal_Int32 nToken, const char* pValue, size_t nValueLength );
     void add( sal_Int32 nToken, const OString& rValue );
@@ -91,6 +94,7 @@ public:
     const std::vector< sal_Int32 >&  getFastAttributeTokens() const { return maAttributeTokens; }
     const char* getFastAttributeValue(size_t nIndex) const { return mpChunk + maAttributeValues[nIndex]; }
     sal_Int32 AttributeValueLength(size_t i) const { return maAttributeValues[i + 1] - maAttributeValues[i] - 1; }
+    size_t size() const { return maAttributeValues.size(); }
 
     // performance sensitive shortcuts to avoid allocation ...
     bool getAsInteger( sal_Int32 nToken, sal_Int32 &rInt) const;

include/xmloff/formlayerimport.hxx
@@ -29,6 +29,7 @@
 namespace com::sun::star::beans { class XPropertySet; }
 namespace com::sun::star::drawing { class XDrawPage; }
 namespace com::sun::star::xml::sax { class XAttributeList; }
+namespace com::sun::star::xml::sax { class XFastAttributeList; }
 
 class SvXMLImport;
 class SvXMLImportContext;
@@ -68,6 +69,8 @@ namespace xmloff
             SvXMLImport& _rImport,
             sal_uInt16 _nPrefix,
             const OUString& _rLocalName);
+        static SvXMLImportContext* createOfficeFormsContext(
+            SvXMLImport& _rImport);
 
         /** create an SvXMLImportContext instance which is able to import the &lt;form:form&gt;
             element.
@@ -78,9 +81,8 @@ namespace xmloff
             @see endPage
         */
         SvXMLImportContext* createContext(
-            const sal_uInt16 _nPrefix,
-            const OUString& _rLocalName,
-            const css::uno::Reference< css::xml::sax::XAttributeList >& _rxAttribs);
+            const sal_Int32 _nElement,
+            const css::uno::Reference< css::xml::sax::XFastAttributeList >& _rxAttribs);
 
         /** lookup a control given by id.
 

include/xmloff/xformsimport.hxx
@@ -39,6 +39,8 @@ XMLOFF_DLLPUBLIC SvXMLImportContext* createXFormsModelContext(
     SvXMLImport& rImport,
     sal_uInt16 nPrefix,
     const OUString& rLocalName );
+XMLOFF_DLLPUBLIC SvXMLImportContext* createXFormsModelContext(
+    SvXMLImport& rImport );
 
 /** perform the actual binding of an XForms-binding with the suitable control
  *  @param document which contains the XForms-model(s)

sax/source/tools/fastattribs.cxx
@@ -63,6 +63,19 @@ FastAttributeList::FastAttributeList( sax_fastparser::FastTokenHandlerBase *pTok
     maAttributeValues.push_back( 0 );
 }
 
+FastAttributeList::FastAttributeList( const css::uno::Reference< css::xml::sax::XFastAttributeList > & xAttrList )
+{
+    const auto& rOther = castToFastAttributeList(xAttrList);
+    mpTokenHandler = rOther.mpTokenHandler;
+    mpChunk = static_cast<char *>(malloc( rOther.mnChunkLength ));
+    mnChunkLength = rOther.mnChunkLength;
+    memcpy(mpChunk, rOther.mpChunk, rOther.mnChunkLength);
+    maAttributeValues = rOther.maAttributeValues;
+    maAttributeTokens = rOther.maAttributeTokens;
+    maUnknownAttributes = rOther.maUnknownAttributes;
+}
+
+
 FastAttributeList::~FastAttributeList()
 {
     free( mpChunk );
@@ -79,6 +92,8 @@ void FastAttributeList::clear()
 void FastAttributeList::add( sal_Int32 nToken, const char* pValue, size_t nValueLength )
 {
     assert(nToken != -1);
+    assert(nToken != 0);
+    assert(nValueLength < SAL_MAX_INT32); // protect against absurd values
     maAttributeTokens.push_back( nToken );
     sal_Int32 nWritePosition = maAttributeValues.back();
     maAttributeValues.push_back( maAttributeValues.back() + nValueLength + 1 );
@@ -121,6 +136,20 @@ void FastAttributeList::addUnknown( const OUString& rNamespaceURL, const OString
 void FastAttributeList::addUnknown( const OString& rName, const OString& value )
 {
     maUnknownAttributes.emplace_back( rName, value );
+}
+
+void FastAttributeList::add( const css::uno::Reference<css::xml::sax::XFastAttributeList>& xAttrList )
+{
+    const auto& rOther = castToFastAttributeList(xAttrList);
+    add(rOther);
+}
+
+void FastAttributeList::add( const FastAttributeList& rOther )
+{
+    for (size_t i=0; i < rOther.maAttributeTokens.size(); ++i)
+        add(rOther.maAttributeTokens[i], rOther.getFastAttributeValue(i), rOther.AttributeValueLength(i));
+    for (const auto & i : rOther.maUnknownAttributes)
+        addUnknown(i.maNamespaceURL, i.maName, i.maValue);
 }
 
 // XFastAttributeList

xmloff/source/forms/controlelement.cxx
@@ -18,6 +18,9 @@
  */
 
 #include "controlelement.hxx"
+#include <xmloff/xmltoken.hxx>
+
+using namespace ::xmloff::token;
 
 namespace xmloff
 {
@@ -48,6 +51,34 @@ namespace xmloff
             case DATE: return "date";
 
             default: return "generic-control";
+        }
+    }
+
+    sal_Int32 OControlElement::getElementToken(ElementType _eType)
+    {
+        switch (_eType)
+        {
+            case TEXT: return XML_TEXT;
+            case TEXT_AREA: return XML_TEXTAREA;
+            case PASSWORD: return XML_PASSWORD;
+            case FILE: return XML_FILE;
+            case FORMATTED_TEXT: return XML_FORMATTED_TEXT;
+            case FIXED_TEXT: return XML_FIXED_TEXT;
+            case COMBOBOX: return XML_COMBOBOX;
+            case LISTBOX: return XML_LISTBOX;
+            case BUTTON: return XML_BUTTON;
+            case IMAGE: return XML_IMAGE;
+            case CHECKBOX: return XML_CHECKBOX;
+            case RADIO: return XML_RADIO;
+            case FRAME: return XML_FRAME;
+            case IMAGE_FRAME: return XML_IMAGE_FRAME;
+            case HIDDEN: return XML_HIDDEN;
+            case GRID: return XML_GRID;
+            case VALUERANGE: return XML_VALUE_RANGE;
+            case TIME: return XML_TIME;
+            case DATE: return XML_DATE;
+
+            default: return XML_GENERIC_CONTROL;
         }
     }
 

xmloff/source/forms/controlelement.hxx
@@ -73,6 +73,15 @@ namespace xmloff
                 the element type
         */
         static const char* getElementName(ElementType _eType);
+
+        /** retrieves the tag name to be used to describe a control of the given type
+
+            <p>The returned string is the pure token, without any namespace.</p>
+
+            @param  _eType
+                the element type
+        */
+        static sal_Int32 getElementToken(ElementType _eType);
     };
 
 }   // namespace xmloff

commit cbc1a631f153c64ca13ad5328620f260e08c063c

xmloff: "form:datasource" vs. "db:data-source"
Recently xmloff started to use XML_DATASOURCE for "form:datasource"
but it actually means "data-source".

Add XML_DATA_SOURCE and use it for "db:data-source".

(regression from d7c7e693c280465964d6d9655d40d64529376c51)

Change-Id: I893efd3fb0fb7c19ae998442459bf36cb1da0b20
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109019
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

dbaccess/source/filter/xml/xmlDatabase.cxx
@@ -57,7 +57,7 @@ css::uno::Reference< css::xml::sax::XFastContextHandler > OXMLDatabase::createFa
 
     switch( nElement & TOKEN_MASK )
     {
-        case XML_DATASOURCE:
+        case XML_DATA_SOURCE:
             GetOwnImport().GetProgressBarHelper()->Increment( PROGRESS_BAR_STEP );
             pContext = new OXMLDataSource( GetOwnImport(), xAttrList, OXMLDataSource::eDataSource );
             break;

dbaccess/source/filter/xml/xmlExport.cxx
@@ -420,7 +420,7 @@ void ODBExport::exportDataSource()
         if ( aDelimiter.bUsed )
             m_aDelimiter.reset( new TDelimiter( aDelimiter ) );
 
-        SvXMLElementExport aElem(*this,XML_NAMESPACE_DB, XML_DATASOURCE, true, true);
+        SvXMLElementExport aElem(*this, XML_NAMESPACE_DB, XML_DATA_SOURCE, true, true);
 
         exportConnectionData();
         exportDriverSettings(aSettingsMap);

include/xmloff/xmltoken.hxx
@@ -2304,7 +2304,8 @@ namespace xmloff::token {
         XML_SORTED_ASCENDING,
 
         XML_DATABASE,
-        XML_DATASOURCE,
+        XML_DATASOURCE, // forms "datasource"
+        XML_DATA_SOURCE, // db "data-source"
         XML_QUERIES,
         XML_REPORTS,
         XML_REPORT,

xmloff/source/core/xmltoken.cxx
@@ -2316,7 +2316,8 @@ namespace xmloff::token {
         TOKEN( "sorted-ascending",                 XML_SORTED_ASCENDING ),
 
         TOKEN( "database",                         XML_DATABASE ),
-        TOKEN( "data-source",                      XML_DATASOURCE ),
+        TOKEN( "datasource",                       XML_DATASOURCE ),
+        TOKEN( "data-source",                      XML_DATA_SOURCE ),
         TOKEN( "queries",                          XML_QUERIES ),
         TOKEN( "reports",                          XML_REPORTS ),
         TOKEN( "report",                           XML_REPORT ),