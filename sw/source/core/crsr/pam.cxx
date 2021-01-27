LemmingAvalanche/libreoffice-repaired

commit 18b3312294864031a4e4e51ff9fb191f2bcf8b63

sw_fieldmarkhide: allow editing all fieldmarks if ...
ForceImportWWFieldsAsGenericFields is set. Not sure what restrictions it
needs.

Change-Id: I8b8e61fb320d5c2b66d5ff332517759b53e68817
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109378
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/crsr/pam.cxx
@@ -23,6 +23,8 @@
 
 #include <tools/gen.hxx>
 #include <editeng/protitem.hxx>
+#include <officecfg/Office/Common.hxx>
+
 #include <cntfrm.hxx>
 #include <pagefrm.hxx>
 #include <doc.hxx>
@@ -729,7 +731,9 @@ bool SwPaM::HasReadonlySel( bool bFormView ) const
     const bool bAtStartA = (pA != nullptr) && (pA->GetMarkStart() == *GetPoint());
     const bool bAtStartB = (pB != nullptr) && (pB->GetMarkStart() == *GetMark());
 
-    if (!bRet)
+    if (!bRet &&
+        // allow editing all fields in generic mode
+        !officecfg::Office::Common::Filter::Microsoft::Import::ForceImportWWFieldsAsGenericFields::get(comphelper::getProcessComponentContext()))
     {
         bool bUnhandledMark = pA && pA->GetFieldname( ) == ODF_UNHANDLED;
         // Unhandled fieldmarks case shouldn't be edited manually to avoid breaking anything

commit eb3f1a6111ccf012c7bd1701459914156439b5a0

sw_fieldmarkhide: adjust SwPaM::HasReadonlySel() to the fact that...
... there is, in fact, an else branch. D'oh.

(regression from 18b3312294864031a4e4e51ff9fb191f2bcf8b63)

Change-Id: I378b0a78a4ccc9a0e7fa9a6bf2e1ecb456133bed
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110012
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/crsr/pam.cxx
@@ -731,9 +731,11 @@ bool SwPaM::HasReadonlySel( bool bFormView ) const
     const bool bAtStartA = (pA != nullptr) && (pA->GetMarkStart() == *GetPoint());
     const bool bAtStartB = (pB != nullptr) && (pB->GetMarkStart() == *GetMark());
 
-    if (!bRet &&
-        // allow editing all fields in generic mode
-        !officecfg::Office::Common::Filter::Microsoft::Import::ForceImportWWFieldsAsGenericFields::get(comphelper::getProcessComponentContext()))
+    if (officecfg::Office::Common::Filter::Microsoft::Import::ForceImportWWFieldsAsGenericFields::get(comphelper::getProcessComponentContext()))
+    {
+        ; // allow editing all fields in generic mode
+    }
+    else if (!bRet)
     {
         bool bUnhandledMark = pA && pA->GetFieldname( ) == ODF_UNHANDLED;
         // Unhandled fieldmarks case shouldn't be edited manually to avoid breaking anything