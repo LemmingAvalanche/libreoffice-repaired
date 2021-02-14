LemmingAvalanche/libreoffice-repaired

commit 59c63882cb6633a6eb6d5f2e49057d5301da087d

sfx2: use the fastparser API when possible
part of the process of making SvXMLImport fastparser-only

Change-Id: Ib5acae303483eb86338cb39dd97a357ff90b1870
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104453
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/doc/SfxDocumentMetaData.cxx
@@ -1737,7 +1737,6 @@ SfxDocumentMetaData::loadFromStorage(
     // create DOM parser service
     css::uno::Reference<css::lang::XMultiComponentFactory> xMsf (
         m_xContext->getServiceManager());
-    css::uno::Reference<css::xml::sax::XParser> xParser = css::xml::sax::Parser::create(m_xContext);
     css::xml::sax::InputSource input;
     input.aInputStream = xInStream;
 
@@ -1761,21 +1760,18 @@ SfxDocumentMetaData::loadFromStorage(
     css::uno::Sequence< css::uno::Any > args(1);
     args[0] <<= xPropArg;
 
-    css::uno::Reference<css::xml::sax::XDocumentHandler> xDocHandler (
+    // the underlying SvXMLImport implements XFastParser, XImporter, XFastDocumentHandler
+    css::uno::Reference<XInterface> xFilter =
         xMsf->createInstanceWithArgumentsAndContext(
-            OUString::createFromAscii(pServiceName), args, m_xContext),
-        css::uno::UNO_QUERY_THROW);
-    css::uno::Reference<css::document::XImporter> xImp (xDocHandler,
-        css::uno::UNO_QUERY_THROW);
+            OUString::createFromAscii(pServiceName), args, m_xContext);
+    assert(xFilter);
+    css::uno::Reference<css::xml::sax::XFastParser> xDocHandler(xFilter, css::uno::UNO_QUERY);
+    assert(xDocHandler);
+    css::uno::Reference<css::document::XImporter> xImp(xDocHandler, css::uno::UNO_QUERY);
+    assert(xImp);
     xImp->setTargetDocument(css::uno::Reference<css::lang::XComponent>(this));
-    xParser->setDocumentHandler(xDocHandler);
-    css::uno::Reference< css::xml::sax::XFastParser > xFastParser = dynamic_cast<
-                            css::xml::sax::XFastParser* >( xDocHandler.get() );
     try {
-        if( xFastParser.is() )
-            xFastParser->parseStream(input);
-        else
-            xParser->parseStream(input);
+        xDocHandler->parseStream(input);
     } catch (const css::xml::sax::SAXException &) {
         throw css::io::WrongFormatException(
                 "SfxDocumentMetaData::loadFromStorage:"

commit 0d2e3932db64b00d36d9ae2a71cec827c39e902e

tdf#140308 Crash import old OpenOffice template
regression from
    commit 59c63882cb6633a6eb6d5f2e49057d5301da087d
    sfx2: use the fastparser API when possible

We end up using the OOo2OasisTransformer filter,
which is not fastparser compatible

Bisected with: bibisect-linux64-7.1

Change-Id: I7bd4191f098096034dc8ff368aba4b1ff313ce18
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110883
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/doc/SfxDocumentMetaData.cxx
@@ -47,6 +47,7 @@
 #include <com/sun/star/document/XExporter.hpp>
 #include <com/sun/star/document/XFilter.hpp>
 #include <com/sun/star/xml/sax/Writer.hpp>
+#include <com/sun/star/xml/sax/Parser.hpp>
 #include <com/sun/star/xml/sax/XFastParser.hpp>
 #include <com/sun/star/xml/dom/DOMException.hpp>
 #include <com/sun/star/xml/dom/XDocument.hpp>
@@ -1764,13 +1765,19 @@ SfxDocumentMetaData::loadFromStorage(
         xMsf->createInstanceWithArgumentsAndContext(
             OUString::createFromAscii(pServiceName), args, m_xContext);
     assert(xFilter);
-    css::uno::Reference<css::xml::sax::XFastParser> xDocHandler(xFilter, css::uno::UNO_QUERY);
-    assert(xDocHandler);
-    css::uno::Reference<css::document::XImporter> xImp(xDocHandler, css::uno::UNO_QUERY);
-    assert(xImp);
+    css::uno::Reference<css::xml::sax::XFastParser> xFastParser(xFilter, css::uno::UNO_QUERY);
+    css::uno::Reference<css::document::XImporter> xImp(xFilter, css::uno::UNO_QUERY_THROW);
     xImp->setTargetDocument(css::uno::Reference<css::lang::XComponent>(this));
     try {
-        xDocHandler->parseStream(input);
+        if (xFastParser)
+            xFastParser->parseStream(input);
+        else
+        {
+            css::uno::Reference<css::xml::sax::XDocumentHandler> xDocHandler(xFilter, css::uno::UNO_QUERY_THROW);
+            css::uno::Reference<css::xml::sax::XParser> xParser = css::xml::sax::Parser::create(m_xContext);
+            xParser->setDocumentHandler(xDocHandler);
+            xParser->parseStream(input);
+        }
     } catch (const css::xml::sax::SAXException &) {
         throw css::io::WrongFormatException(
                 "SfxDocumentMetaData::loadFromStorage:"