LemmingAvalanche/libreoffice-repaired

commit c21648a8e433feb70cef600bfe58ebd4e3fd04e2

tdf#98584 Correct import draw:transform values skewY and matrix
Covers tdf#98583 and tdf#98565 too.
TRBaseGeomety (API) uses for skewX the same angle orientation as
written to file. But that results in mathematically wrong matrices.
Change sign where needed.
Vertical shearing is converted and written to file by LO as sequence
rotation * shear horizontal * scale. Same should happen on reading.
Because LO does not write skewY itself, I have used the angle
orientation, that was used in OOo1.1.5 and that is used in
Scribus 1.5.4.
Import generates a transformation matrix from the draw:transform
attribute. That is a mathematically correct matrix. It is applied
to the shape via TRSetBaseGeometry. But that uses a wrong sign in
the shear angle. Therefore conversion of mathematical matrix to
TRBaseGeometry matrix is needed.
The draw:transform attribute can generate a scaling, which needs to
be applied on top of the scaling made from svg:width and svg:height.
Such happens on import of skewY() and might happen with matrix().
SdrPathObject puts scaling form svg:width and svg:height directly
into the coordinates of its points. It had ignored any additional
scaling. I have add a part to detect and apply it.

Change-Id: I7636b9feec432cf403e7c6ef8dbd6a769793d144
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86244
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
Reviewed-by: Regina Henschel <rb.henschel@t-online.de>

svx/source/svdraw/svdopath.cxx
@@ -2929,12 +2929,30 @@ void SdrPathObj::TRSetBaseGeometry(const basegfx::B2DHomMatrix& rMatrix, const b
     // #i75086#
     // Given polygon is already scaled (for historical reasons), but not mirrored yet.
     // Thus, when scale is negative in X or Y, apply the needed mirroring accordingly.
-    if(basegfx::fTools::less(aScale.getX(), 0.0) || basegfx::fTools::less(aScale.getY(), 0.0))
+    double fScaleX(basegfx::fTools::less(aScale.getX(), 0.0) ? -1.0 : 1.0);
+    double fScaleY(basegfx::fTools::less(aScale.getY(), 0.0) ? -1.0 : 1.0);
+
+    // tdf#98565, tdf#98584. While loading a shape, svg:width and svg:height is used to scale
+    // the polygon. But draw:transform might introduce additional scaling factors, which need to
+    // be applied to the polygon too, so aScale cannot be ignored while loading.
+    // I use "maSnapRect.IsEmpty() && GetPathPoly().count()" to detect this case. Any better
+    // idea? The behavior in other cases is the same as it was before this fix.
+    if (maSnapRect.IsEmpty() && GetPathPoly().count())
     {
-        aTransform.scale(
-            basegfx::fTools::less(aScale.getX(), 0.0) ? -1.0 : 1.0,
-            basegfx::fTools::less(aScale.getY(), 0.0) ? -1.0 : 1.0);
+        // In case of a Writer document, the scaling factors were converted to twips. That is not
+        // correct here, because width and height are already in the points coordinates and aScale
+        // is no length but only a factor here. Convert back.
+        if (getSdrModelFromSdrObject().IsWriter())
+        {
+            aScale.setX(aScale.getX() * 127.0 / 72.0);
+            aScale.setY(aScale.getY() * 127.0 / 72.0);
+        }
+        fScaleX *= fabs(aScale.getX());
+        fScaleY *= fabs(aScale.getY());
     }
+
+    if (fScaleX != 1.0 || fScaleY != 1.0)
+        aTransform.scale(fScaleX, fScaleY);
 
     if(!basegfx::fTools::equalZero(fShearX))
     {

svx/CppunitTest_svx_unit.mk
@@ -25,6 +25,7 @@ $(eval $(call gb_CppunitTest_set_include,svx_unit,\
 $(eval $(call gb_CppunitTest_add_exception_objects,svx_unit, \
 	svx/qa/unit/svdraw/test_SdrTextObject \
 	svx/qa/unit/customshapes \
+    svx/qa/unit/classicshapes \
 	svx/qa/unit/svdraw \
 	svx/qa/unit/unodraw \
 	svx/qa/unit/xoutdev \

svx/qa/unit/classicshapes.cxx
@@ -1,0 +1,189 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+#include <rtl/ustring.hxx>
+#include <editeng/unoprnms.hxx>
+
+#include <cppunit/TestAssert.h>
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
+#include <com/sun/star/drawing/XDrawPage.hpp>
+#include <com/sun/star/awt/Rectangle.hpp>
+#include <com/sun/star/frame/Desktop.hpp>
+
+using namespace ::com::sun::star;
+
+namespace
+{
+const OUString sDataDirectory("svx/qa/unit/data/");
+
+/// Tests not about special features of custom shapes, but about shapes in general.
+class ClassicshapesTest : public test::BootstrapFixture, public unotest::MacrosTest
+{
+protected:
+    uno::Reference<lang::XComponent> mxComponent;
+    uno::Reference<drawing::XShape> getShape(sal_uInt8 nShapeIndex, sal_uInt8 nPageIndex);
+
+public:
+    virtual void setUp() override
+    {
+        test::BootstrapFixture::setUp();
+        mxDesktop.set(frame::Desktop::create(m_xContext));
+    }
+
+    virtual void tearDown() override
+    {
+        if (mxComponent.is())
+        {
+            mxComponent->dispose();
+        }
+        test::BootstrapFixture::tearDown();
+    }
+};
+
+uno::Reference<drawing::XShape> ClassicshapesTest::getShape(sal_uInt8 nShapeIndex,
+                                                            sal_uInt8 nPageIndex)
+{
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(mxComponent,
+                                                                   uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_MESSAGE("Could not get XDrawPagesSupplier", xDrawPagesSupplier.is());
+    uno::Reference<drawing::XDrawPages> xDrawPages(xDrawPagesSupplier->getDrawPages());
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPages->getByIndex(nPageIndex),
+                                                 uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_MESSAGE("Could not get xDrawPage", xDrawPage.is());
+    uno::Reference<drawing::XShape> xShape(xDrawPage->getByIndex(nShapeIndex), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_MESSAGE("Could not get xShape", xShape.is());
+    return xShape;
+}
+
+CPPUNIT_TEST_FIXTURE(ClassicshapesTest, testTdf98584ShearVertical)
+{
+    // The document contains draw:rect, draw:polygon and draw:path objects.
+    // They are vertical sheared by skewY(-0.927295218002) or by matrix(1 2 0 1 1cm 1cm).
+    // Notice, skewY and matrix are interpreted on file open, but not written on file save.
+    // They are converted to rotate * shear horizontal * scale.
+    // Besides using a wrong sign in shear angle, error was, that TRSetGeometry of SdrPathObj did
+    // not consider the additional scaling (tdf#98565).
+    const OUString sFileName("tdf98584_ShearVertical.odg");
+    const OUString sURL(m_directories.getURLFromSrc(sDataDirectory) + sFileName);
+    mxComponent = loadFromDesktop(sURL, "com.sun.star.comp.drawing.DrawingDocument");
+    CPPUNIT_ASSERT_MESSAGE("Could not load document", mxComponent.is());
+
+    OUString sErrors; // sErrors collects the errors and should be empty in case all is OK.
+    // All tests have a small tolerance for to avoid failing because of rounding errors.
+
+    // Tests skewY
+    sal_Int32 nShearE = -5313; // expected angle for horizontal shear
+    sal_Int32 nRotE = 30687; // = -5313 expected angle for generated rotation
+    // expected width of shape, should not change on vertical shearing
+    sal_Int32 nWidthE = 5001;
+
+    for (sal_uInt8 nPageIndex = 0; nPageIndex < 3; ++nPageIndex)
+    {
+        awt::Rectangle aFrameRect;
+        uno::Reference<drawing::XShape> xShape(getShape(0, nPageIndex));
+        uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_MESSAGE("Could not get the shape properties", xShapeProps.is());
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_FRAMERECT) >>= aFrameRect;
+        const sal_Int32 nWidthA(aFrameRect.Width);
+        if (abs(nWidthE - nWidthA) > 2)
+            sErrors += "skewY page " + OUString::number(nPageIndex) + " width expected "
+                       + OUString::number(nWidthE) + ", actual " + OUString::number(nWidthA) + "\n";
+        sal_Int32 nShearA(0);
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_SHEARANGLE) >>= nShearA;
+        if (abs(nShearE - nShearA) > 2)
+            sErrors += "skewY page" + OUString::number(nPageIndex) + " shear angle expected "
+                       + OUString::number(nShearE) + ", actual " + OUString::number(nShearA) + "\n";
+        sal_Int32 nRotA(0);
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_ROTATEANGLE) >>= nRotA;
+        if (abs(nRotE - nRotA) > 2)
+            sErrors += "skewY page" + OUString::number(nPageIndex) + " rotate angle expected "
+                       + OUString::number(nRotE) + ", actual " + OUString::number(nRotA) + "\n";
+    }
+
+    // Tests matrix
+    nShearE = -6343;
+    nRotE = 29657;
+    nWidthE = 5001;
+
+    for (sal_uInt8 nPageIndex = 3; nPageIndex < 6; ++nPageIndex)
+    {
+        awt::Rectangle aFrameRect;
+        uno::Reference<drawing::XShape> xShape(getShape(0, nPageIndex));
+        uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_MESSAGE("Could not get the shape properties", xShapeProps.is());
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_FRAMERECT) >>= aFrameRect;
+        const sal_Int32 nWidthA(aFrameRect.Width);
+        if (abs(nWidthE - nWidthA) > 2)
+            sErrors += "matrix page " + OUString::number(nPageIndex) + " width expected "
+                       + OUString::number(nWidthE) + ", actual " + OUString::number(nWidthA) + "\n";
+        sal_Int32 nShearA(0);
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_SHEARANGLE) >>= nShearA;
+        if (abs(nShearE - nShearA) > 2)
+            sErrors += "matrix page" + OUString::number(nPageIndex) + " shear angle expected "
+                       + OUString::number(nShearE) + ", actual " + OUString::number(nShearA) + "\n";
+        sal_Int32 nRotA(0);
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_ROTATEANGLE) >>= nRotA;
+        if (abs(nRotE - nRotA) > 2)
+            sErrors += "matrix page" + OUString::number(nPageIndex) + " rotate angle expected "
+                       + OUString::number(nRotE) + ", actual " + OUString::number(nRotA) + "\n";
+    }
+
+    CPPUNIT_ASSERT_EQUAL(OUString(), sErrors);
+}
+
+CPPUNIT_TEST_FIXTURE(ClassicshapesTest, testTdf98583ShearHorizontal)
+{
+    // The document contains rectangles with LT 3000,5000 and RB 5000,9000.
+    // skewX (-0.78539816339744830961) = skewX(-45deg) is applied on the first page
+    // matrix(1 0 1 1 0cm 0cm) on the second page. Both should result in a parallelogram with
+    // LT 8000,5000 and RB 14000, 9000, which means width 6001, height 4001.
+    // Error was, that not the mathematical matrix was used, but the API matrix, which has
+    // wrong sign in shear angle.
+    const OUString sFileName("tdf98583_ShearHorizontal.odp");
+    const OUString sURL(m_directories.getURLFromSrc(sDataDirectory) + sFileName);
+    mxComponent = loadFromDesktop(sURL, "com.sun.star.comp.presentation.PresentationDocument");
+    CPPUNIT_ASSERT_MESSAGE("Could not load document", mxComponent.is());
+
+    OUString sErrors; // sErrors collects the errors and should be empty in case all is OK.
+    // All tests have a small tolerance for to avoid failing because of rounding errors.
+    const sal_Int32 nLeftE(8000); // expected values
+    const sal_Int32 nTopE(5000);
+    const sal_Int32 nWidthE(6001);
+    const sal_Int32 nHeightE(4001);
+    for (sal_uInt8 nPageIndex = 0; nPageIndex < 2; ++nPageIndex)
+    {
+        awt::Rectangle aFrameRect;
+        uno::Reference<drawing::XShape> xShape(getShape(0, nPageIndex));
+        uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_MESSAGE("Could not get the shape properties", xShapeProps.is());
+        xShapeProps->getPropertyValue(UNO_NAME_MISC_OBJ_FRAMERECT) >>= aFrameRect;
+        const sal_Int32 nLeftA(aFrameRect.X);
+        const sal_Int32 nTopA(aFrameRect.Y);
+        const sal_Int32 nWidthA(aFrameRect.Width);
+        const sal_Int32 nHeightA(aFrameRect.Height);
+        if (abs(nLeftE - nLeftA) > 2 || abs(nTopE - nTopA) > 2)
+            sErrors += "page " + OUString::number(nPageIndex) + " LT expected "
+                       + OUString::number(nLeftE) + " | " + OUString::number(nTopE) + ", actual "
+                       + OUString::number(nLeftA) + " | " + OUString::number(nTopA) + "\n";
+        if (abs(nWidthE - nWidthA) > 2 || abs(nHeightE - nHeightA) > 2)
+            sErrors += "page " + OUString::number(nPageIndex) + " width x height expected "
+                       + OUString::number(nWidthE) + " x " + OUString::number(nHeightE)
+                       + ", actual " + OUString::number(nWidthA) + " x "
+                       + OUString::number(nHeightA) + "\n";
+    }
+
+    CPPUNIT_ASSERT_EQUAL(OUString(), sErrors);
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

xmloff/source/draw/xexptran.cxx
@@ -510,12 +510,18 @@ void SdXMLImExTransform2D::GetFullTransform(::basegfx::B2DHomMatrix& rFullTrans)
             }
             case IMP_SDXMLEXP_TRANSOBJ2D_SKEWX      :
             {
-                rFullTrans.shearX(tan(static_cast<ImpSdXMLExpTransObj2DSkewX*>(pObj)->mfSkewX));
+                // For to get a mathematical correct matrix from already existing documents,
+                // mirror the value here. ODF spec is unclear about direction.
+                rFullTrans.shearX(-tan(static_cast<ImpSdXMLExpTransObj2DSkewX*>(pObj)->mfSkewX));
                 break;
             }
             case IMP_SDXMLEXP_TRANSOBJ2D_SKEWY      :
             {
-                rFullTrans.shearY(tan(static_cast<ImpSdXMLExpTransObj2DSkewY*>(pObj)->mfSkewY));
+                // LibreOffice does not write skewY, OOo neither. Such files are foreign documents
+                // or manually set transformations. OOo had used the value as -tan(value) before
+                // errors were introduced, Scribus 1.5.4 uses it as -tan(value) too, MS Office does
+                // not shear at all. ODF spec is unclear about direction.
+                rFullTrans.shearY(-tan(static_cast<ImpSdXMLExpTransObj2DSkewY*>(pObj)->mfSkewY));
                 break;
             }
             case IMP_SDXMLEXP_TRANSOBJ2D_MATRIX     :

xmloff/source/draw/ximpshap.cxx
@@ -575,21 +575,38 @@ void SdXMLShapeContext::SetTransformation()
             }
 
             // now set transformation for this object
-            drawing::HomogenMatrix3 aMatrix;
 
-            aMatrix.Line1.Column1 = maUsedTransformation.get(0, 0);
-            aMatrix.Line1.Column2 = maUsedTransformation.get(0, 1);
-            aMatrix.Line1.Column3 = maUsedTransformation.get(0, 2);
+            // maUsedTransformtion contains the mathematical correct matrix, which if
+            // applied to a unit square would generate the transformed shape. But the property
+            // "Transformation" contains a matrix, which can be used in TRSetBaseGeometry
+            // and would be created by TRGetBaseGeometry. And those use a mathematically wrong
+            // sign for the shearing angle. So we need to adapt the matrix here.
+            basegfx::B2DTuple aScale;
+            basegfx::B2DTuple aTranslate;
+            double fRotate;
+            double fShearX;
+            maUsedTransformation.decompose(aScale, aTranslate, fRotate, fShearX);
+            basegfx::B2DHomMatrix aB2DHomMatrix;
+            aB2DHomMatrix = basegfx::utils::createScaleShearXRotateTranslateB2DHomMatrix(
+                                        aScale,
+                                        basegfx::fTools::equalZero(fShearX) ? 0.0 : -fShearX,
+                                        basegfx::fTools::equalZero(fRotate) ? 0.0 : fRotate,
+                                    aTranslate);
+            drawing::HomogenMatrix3 aUnoMatrix;
 
-            aMatrix.Line2.Column1 = maUsedTransformation.get(1, 0);
-            aMatrix.Line2.Column2 = maUsedTransformation.get(1, 1);
-            aMatrix.Line2.Column3 = maUsedTransformation.get(1, 2);
+            aUnoMatrix.Line1.Column1 = aB2DHomMatrix.get(0, 0);
+            aUnoMatrix.Line1.Column2 = aB2DHomMatrix.get(0, 1);
+            aUnoMatrix.Line1.Column3 = aB2DHomMatrix.get(0, 2);
 
-            aMatrix.Line3.Column1 = maUsedTransformation.get(2, 0);
-            aMatrix.Line3.Column2 = maUsedTransformation.get(2, 1);
-            aMatrix.Line3.Column3 = maUsedTransformation.get(2, 2);
+            aUnoMatrix.Line2.Column1 = aB2DHomMatrix.get(1, 0);
+            aUnoMatrix.Line2.Column2 = aB2DHomMatrix.get(1, 1);
+            aUnoMatrix.Line2.Column3 = aB2DHomMatrix.get(1, 2);
 
-            xPropSet->setPropertyValue("Transformation", Any(aMatrix));
+            aUnoMatrix.Line3.Column1 = aB2DHomMatrix.get(2, 0);
+            aUnoMatrix.Line3.Column2 = aB2DHomMatrix.get(2, 1);
+            aUnoMatrix.Line3.Column3 = aB2DHomMatrix.get(2, 2);
+
+            xPropSet->setPropertyValue("Transformation", Any(aUnoMatrix));
         }
     }
 }
@@ -1093,9 +1110,9 @@ void SdXMLLineShapeContext::StartElement(const uno::Reference< xml::sax::XAttrib
         xPropSet->setPropertyValue("Geometry", Any(aPolyPoly));
     }
 
-    // set sizes for transformation
-    maSize.Width = o3tl::saturating_sub(aBottomRight.X, aTopLeft.X);
-    maSize.Height = o3tl::saturating_sub(aBottomRight.Y, aTopLeft.Y);
+    // Size is included in point coordinates
+    maSize.Width = 1;
+    maSize.Height = 1;
     maPosition.X = aTopLeft.X;
     maPosition.Y = aTopLeft.Y;
 
@@ -1322,6 +1339,10 @@ void SdXMLPolygonShapeContext::StartElement(const uno::Reference< xml::sax::XAtt
                         css::drawing::PointSequenceSequence aPointSequenceSequence;
                         basegfx::utils::B2DPolyPolygonToUnoPointSequenceSequence(basegfx::B2DPolyPolygon(aPolygon), aPointSequenceSequence);
                         xPropSet->setPropertyValue("Geometry", Any(aPointSequenceSequence));
+                        // Size is now contained in the point coordinates, adapt maSize for
+                        // to use the correct transformation matrix in SetTransformation()
+                        maSize.Width = 1;
+                        maSize.Height = 1;
                     }
                 }
             }
@@ -1471,6 +1492,10 @@ void SdXMLPathShapeContext::StartElement(const uno::Reference< xml::sax::XAttrib
                         }
 
                         xPropSet->setPropertyValue("Geometry", aAny);
+                        // Size is now contained in the point coordinates, adapt maSize for
+                        // to use the correct transformation matrix in SetTransformation()
+                        maSize.Width = 1;
+                        maSize.Height = 1;
                     }
 
                     // set pos, size, shear and rotate

commit 17839c33c5248df744a56d4eda52629c677b353b

svx: fix unexpected large bezier shapes when created via the UNO API
Regression from commit c21648a8e433feb70cef600bfe58ebd4e3fd04e2
(tdf#98584 Correct import draw:transform values skewY and matrix,
2020-01-02), then problem is was that now we handle the transform matrix
for path objects the way ODF likes it, but this breaks UNO API clients
with existing code.

In an attempt to make everyone happy, revert the default to ignore
scaling from the transform matrix, but add a way so that xmloff/ can opt
in to handle it. This way the ODF import result doesn't regression and
UNO API compatibility is not broken, either.

Change-Id: I8d525e56954bf68c42d3629eca87fe9b513de507
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111340
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

include/svx/svdopath.hxx
@@ -49,6 +49,7 @@ private:
 
     basegfx::B2DPolyPolygon maPathPolygon;
     SdrObjKind                  meKind;
+    bool mbHandleScale = false;
 
     // for isolation of old Drag/Create code
     std::unique_ptr<ImpPathForDragAndCreate> mpDAC;
@@ -163,6 +164,12 @@ public:
     // If it's an SdrPathObj it will use the provided geometry information. The Polygon has
     // to use (0,0) as upper left and will be scaled to the given size in the matrix.
     virtual void TRSetBaseGeometry(const basegfx::B2DHomMatrix& rMatrix, const basegfx::B2DPolyPolygon& rPolyPolygon) override;
+
+    /**
+     * Scaling is ignored from the transform matrix by default, to not break compatibility. One can
+     * opt in to handle scaling if matching ODF behavior is more important.
+     */
+    void SetHandleScale(bool bHandleScale);
 };
 
 #endif // INCLUDED_SVX_SVDOPATH_HXX

svx/source/svdraw/svdopath.cxx
@@ -2877,6 +2877,11 @@ bool SdrPathObj::TRGetBaseGeometry(basegfx::B2DHomMatrix& rMatrix, basegfx::B2DP
     return true;
 }
 
+void SdrPathObj::SetHandleScale(bool bHandleScale)
+{
+    mbHandleScale = bHandleScale;
+}
+
 // Sets the base geometry of the object using infos contained in the homogeneous 3x3 matrix.
 // If it's an SdrPathObj it will use the provided geometry information. The Polygon has
 // to use (0,0) as upper left and will be scaled to the given size in the matrix.
@@ -2929,7 +2934,7 @@ void SdrPathObj::TRSetBaseGeometry(const basegfx::B2DHomMatrix& rMatrix, const b
     // be applied to the polygon too, so aScale cannot be ignored while loading.
     // I use "maSnapRect.IsEmpty() && GetPathPoly().count()" to detect this case. Any better
     // idea? The behavior in other cases is the same as it was before this fix.
-    if (maSnapRect.IsEmpty() && GetPathPoly().count())
+    if (maSnapRect.IsEmpty() && GetPathPoly().count() && mbHandleScale)
     {
         // In case of a Writer document, the scaling factors were converted to twips. That is not
         // correct here, because width and height are already in the points coordinates and aScale

svx/source/unodraw/unoshape.cxx
@@ -85,6 +85,7 @@
 #include <vcl/wmf.hxx>
 #include <svx/sdtfsitm.hxx>
 #include <svx/svdoutl.hxx>
+#include <svx/svdopath.hxx>
 
 #include <memory>
 #include <vector>
@@ -1608,6 +1609,20 @@ void SvxShape::_setPropertyValue( const OUString& rPropertyName, const uno::Any&
             mpPropSet->setPropertyValue( pMap, rVal );
         }
 
+        return;
+    }
+
+    if (rPropertyName == "HandlePathObjScale")
+    {
+        auto pPathObj = dynamic_cast<SdrPathObj*>(GetSdrObject());
+        if (pPathObj)
+        {
+            bool bHandleScale{};
+            if (rVal >>= bHandleScale)
+            {
+                pPathObj->SetHandleScale(bHandleScale);
+            }
+        }
         return;
     }
 

xmloff/source/draw/shapeimport.cxx
@@ -471,6 +471,12 @@ void XMLShapeImportHelper::addShape( uno::Reference< drawing::XShape >& rShape,
     {
         // add new shape to parent
         rShapes->add( rShape );
+
+        uno::Reference<beans::XPropertySet> xPropertySet(rShape, uno::UNO_QUERY);
+        if (xPropertySet.is())
+        {
+            xPropertySet->setPropertyValue("HandlePathObjScale", uno::makeAny(true));
+        }
     }
 }
 

svx/qa/unit/svdraw.cxx
@@ -16,6 +16,10 @@
 #include <com/sun/star/drawing/XDrawPage.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/text/XTextRange.hpp>
+#include <com/sun/star/drawing/FillStyle.hpp>
+#include <com/sun/star/drawing/LineStyle.hpp>
+#include <com/sun/star/drawing/PolyPolygonBezierCoords.hpp>
+#include <com/sun/star/drawing/HomogenMatrix3.hpp>
 
 #include <drawinglayer/tools/primitive2dxmldump.hxx>
 #include <rtl/ustring.hxx>
@@ -104,6 +108,84 @@ CPPUNIT_TEST_FIXTURE(SvdrawTest, testSemiTransparentText)
     double fTransparence = getXPath(pDocument, "//unifiedtransparence", "transparence").toDouble();
     CPPUNIT_ASSERT_EQUAL(nTransparence,
                          static_cast<sal_Int16>(basegfx::fround(fTransparence * 100)));
+}
+
+CPPUNIT_TEST_FIXTURE(SvdrawTest, testHandlePathObjScale)
+{
+    // Given a path object:
+    getComponent() = loadFromDesktop("private:factory/sdraw");
+    uno::Reference<lang::XMultiServiceFactory> xFactory(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XShape> xShape(
+        xFactory->createInstance("com.sun.star.drawing.ClosedBezierShape"), uno::UNO_QUERY);
+
+    // When setting its scale by both using setSize() and scaling in a transform matrix:
+    // Set size and basic properties.
+    xShape->setPosition(awt::Point(2512, 6062));
+    xShape->setSize(awt::Size(112, 112));
+    uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+    xShapeProps->setPropertyValue("FillStyle", uno::makeAny(drawing::FillStyle_SOLID));
+    xShapeProps->setPropertyValue("LineStyle", uno::makeAny(drawing::LineStyle_SOLID));
+    xShapeProps->setPropertyValue("FillColor", uno::makeAny(static_cast<sal_Int32>(0)));
+    // Add it to the draw page.
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+    xDrawPage->add(xShape);
+    // Set polygon coordinates.
+    drawing::PolyPolygonBezierCoords aPolyPolygonBezierCoords;
+    aPolyPolygonBezierCoords.Coordinates = {
+        {
+            awt::Point(2624, 6118),
+            awt::Point(2624, 6087),
+            awt::Point(2599, 6062),
+            awt::Point(2568, 6062),
+            awt::Point(2537, 6062),
+            awt::Point(2512, 6087),
+            awt::Point(2512, 6118),
+            awt::Point(2512, 6149),
+            awt::Point(2537, 6175),
+            awt::Point(2568, 6174),
+            awt::Point(2599, 6174),
+            awt::Point(2625, 6149),
+            awt::Point(2624, 6118),
+        },
+    };
+    aPolyPolygonBezierCoords.Flags = {
+        {
+            drawing::PolygonFlags_NORMAL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_NORMAL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_NORMAL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_NORMAL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_CONTROL,
+            drawing::PolygonFlags_NORMAL,
+        },
+    };
+    xShapeProps->setPropertyValue("PolyPolygonBezier", uno::makeAny(aPolyPolygonBezierCoords));
+    drawing::HomogenMatrix3 aMatrix;
+    aMatrix.Line1.Column1 = 56;
+    aMatrix.Line2.Column1 = -97;
+    aMatrix.Line3.Column1 = 0;
+    aMatrix.Line1.Column2 = 97;
+    aMatrix.Line2.Column2 = 56;
+    aMatrix.Line3.Column2 = 0;
+    aMatrix.Line1.Column3 = 3317;
+    aMatrix.Line2.Column3 = 5583;
+    aMatrix.Line3.Column3 = 1;
+    xShapeProps->setPropertyValue("Transformation", uno::makeAny(aMatrix));
+
+    // Then make sure the scaling is only applied once:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 113
+    // - Actual  : 12566
+    // i.e. the scaling was applied twice.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(113), xShape->getSize().Width);
 }
 }
 