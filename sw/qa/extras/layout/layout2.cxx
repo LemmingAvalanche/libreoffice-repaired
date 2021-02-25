LemmingAvalanche/libreoffice-repaired

commit b13860034a20cc32c27a38a285fa241f1b2a3a5a

tdf#135653 OOXML import: fix OLE background color
When importing a .docx file Writer used to ignore the 'filled' and
'fillcolor' attributes in the 'shape' tag belonging to an OLE object.
Now both these are imported and displayed correctly.

Co-authored-by: Daniel Arato (NISZ)

Change-Id: I2e6b880d88e4c46af6f3f2316ee966bac1a1f2e0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100922
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/vml/vmlshape.cxx
@@ -293,13 +293,18 @@ void ShapeBase::finalizeFragmentImport()
         aType = aType.copy(1);
     if( const ShapeType* pShapeType = mrDrawing.getShapes().getShapeTypeById( aType ) )
     {
-        // Make sure that the stroke props from maTypeModel have priority over the stroke props from
+        // Make sure that the props from maTypeModel have priority over the props from
         // the shape type.
         StrokeModel aMergedStrokeModel;
         aMergedStrokeModel.assignUsed(pShapeType->getTypeModel().maStrokeModel);
         aMergedStrokeModel.assignUsed(maTypeModel.maStrokeModel);
+        FillModel aMergedFillModel;
+        aMergedFillModel.assignUsed(pShapeType->getTypeModel().maFillModel);
+        aMergedFillModel.assignUsed(maTypeModel.maFillModel);
+
         maTypeModel.assignUsed( pShapeType->getTypeModel() );
         maTypeModel.maStrokeModel = aMergedStrokeModel;
+        maTypeModel.maFillModel = aMergedFillModel;
     }
     else {
         // Temporary fix, shapetype not found if referenced from different substream
@@ -1359,6 +1364,8 @@ Reference< XShape > ComplexShape::implConvertAndInsert( const Reference< XShapes
         oox::drawingml::ShapePropertyMap aPropMap(mrDrawing.getFilter().getModelObjectHelper());
         const GraphicHelper& rGraphicHelper = mrDrawing.getFilter().getGraphicHelper();
         maTypeModel.maStrokeModel.pushToPropMap(aPropMap, rGraphicHelper);
+        //And, fill-color properties as well...
+        maTypeModel.maFillModel.pushToPropMap(aPropMap, rGraphicHelper);
         PropertySet(xShape).setProperties(aPropMap);
 
         return xShape;

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -101,6 +101,7 @@
 #include <unotools/mediadescriptor.hxx>
 #include <tools/diagnose_ex.h>
 #include <sal/log.hxx>
+#include <com/sun/star/drawing/FillStyle.hpp>
 
 #include <unicode/errorcode.h>
 #include <unicode/regex.h>
@@ -2246,18 +2247,35 @@ void DomainMapper_Impl::appendOLE( const OUString& rStreamName, const std::share
         uno::Reference<beans::XPropertySet> xReplacementProperties(pOLEHandler->getShape(), uno::UNO_QUERY);
         if (xReplacementProperties.is())
         {
+            table::BorderLine2 aBorderProps;
+            xReplacementProperties->getPropertyValue("LineColor") >>= aBorderProps.Color;
+            xReplacementProperties->getPropertyValue("LineWidth") >>= aBorderProps.LineWidth;
+            xReplacementProperties->getPropertyValue("LineStyle") >>= aBorderProps.LineStyle;
+
+            xOLEProperties->setPropertyValue("RightBorder", uno::Any(aBorderProps));
+            xOLEProperties->setPropertyValue("TopBorder", uno::Any(aBorderProps));
+            xOLEProperties->setPropertyValue("LeftBorder", uno::Any(aBorderProps));
+            xOLEProperties->setPropertyValue("BottomBorder", uno::Any(aBorderProps));
+
             OUString pProperties[] = {
-                OUString("AnchorType"),
-                OUString("Surround"),
-                OUString("HoriOrient"),
-                OUString("HoriOrientPosition"),
-                OUString("VertOrient"),
-                OUString("VertOrientPosition"),
-                OUString("VertOrientRelation"),
-                OUString("HoriOrientRelation")
+                "AnchorType",
+                "Surround",
+                "HoriOrient",
+                "HoriOrientPosition",
+                "VertOrient",
+                "VertOrientPosition",
+                "VertOrientRelation",
+                "HoriOrientRelation",
+                "FillStyle",
+                "FillColor",
+                "FillColor2",
+                "LineStyle",
             };
-            for (const OUString & s : pProperties)
-                xOLEProperties->setPropertyValue(s, xReplacementProperties->getPropertyValue(s));
+            for (const OUString& s : pProperties)
+            {
+                const uno::Any aVal = xReplacementProperties->getPropertyValue(s);
+                xOLEProperties->setPropertyValue(s, aVal);
+            }
         }
         else
             // mimic the treatment of graphics here... it seems anchoring as character

sw/qa/extras/ooxmlexport/ooxmlexport15.cxx
@@ -9,6 +9,8 @@
 
 #include <swmodeltestbase.hxx>
 
+#include <com/sun/star/drawing/FillStyle.hpp>
+#include <tools/color.hxx>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/text/RelOrientation.hpp>
 #include <com/sun/star/text/XTextViewCursorSupplier.hpp>
@@ -367,6 +369,23 @@ DECLARE_OOXMLEXPORT_TEST(testTdf134063, "tdf134063.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(720), getXPath(pDump, "//page[1]/body/txt[1]/Text[1]", "nWidth").toInt32());
     CPPUNIT_ASSERT_EQUAL(sal_Int32(720), getXPath(pDump, "//page[1]/body/txt[1]/Text[2]", "nWidth").toInt32());
     CPPUNIT_ASSERT_EQUAL(sal_Int32(720), getXPath(pDump, "//page[1]/body/txt[1]/Text[3]", "nWidth").toInt32());
+}
+
+DECLARE_OOXMLIMPORT_TEST(TestTdf135653, "tdf135653.docx")
+{
+    uno::Reference<beans::XPropertySet> xOLEProps(getShape(1), uno::UNO_QUERY_THROW);
+    drawing::FillStyle nFillStyle = static_cast<drawing::FillStyle>(-1);
+    xOLEProps->getPropertyValue("FillStyle") >>= nFillStyle;
+    Color aFillColor(COL_AUTO);
+    xOLEProps->getPropertyValue("FillColor") >>= aFillColor;
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Fill style setting does not match!",
+                                 drawing::FillStyle::FillStyle_SOLID, nFillStyle);
+    Color aExpectedColor;
+    aExpectedColor.SetRed(255);
+    aExpectedColor.SetGreen(0);
+    aExpectedColor.SetBlue(0);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("OLE bg color does not match!", aExpectedColor, aFillColor);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testAtPageShapeRelOrientation, "rotated_shape.fodt")

commit ecb4625ee178f960cffa39609f063ad61a42083f

tdf#136570 OOXML import: fix height of OLE objects
e.g. OLE icons or math formulas by skipping unset
border properties.

Regression from b13860034a20cc32c27a38a285fa241f1b2a3a5a
(tdf#135653 OOXML import: fix OLE background color).

Change-Id: I64bd68037d063de81fbb302b90d65b77af50a622
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111119
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/vml/vmlshape.cxx
@@ -1457,7 +1457,7 @@ Reference< XShape > ComplexShape::implConvertAndInsert( const Reference< XShapes
         oox::drawingml::ShapePropertyMap aPropMap(mrDrawing.getFilter().getModelObjectHelper());
         const GraphicHelper& rGraphicHelper = mrDrawing.getFilter().getGraphicHelper();
         maTypeModel.maStrokeModel.pushToPropMap(aPropMap, rGraphicHelper);
-        //And, fill-color properties as well...
+        // And, fill-color properties as well...
         maTypeModel.maFillModel.pushToPropMap(aPropMap, rGraphicHelper);
         PropertySet(xShape).setProperties(aPropMap);
 

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2321,11 +2321,13 @@ void DomainMapper_Impl::appendOLE( const OUString& rStreamName, const std::share
             xReplacementProperties->getPropertyValue("LineWidth") >>= aBorderProps.LineWidth;
             xReplacementProperties->getPropertyValue("LineStyle") >>= aBorderProps.LineStyle;
 
-            xOLEProperties->setPropertyValue("RightBorder", uno::Any(aBorderProps));
-            xOLEProperties->setPropertyValue("TopBorder", uno::Any(aBorderProps));
-            xOLEProperties->setPropertyValue("LeftBorder", uno::Any(aBorderProps));
-            xOLEProperties->setPropertyValue("BottomBorder", uno::Any(aBorderProps));
-
+            if (aBorderProps.LineStyle) // Set line props only if LineStyle is set
+            {
+                xOLEProperties->setPropertyValue("RightBorder", uno::Any(aBorderProps));
+                xOLEProperties->setPropertyValue("TopBorder", uno::Any(aBorderProps));
+                xOLEProperties->setPropertyValue("LeftBorder", uno::Any(aBorderProps));
+                xOLEProperties->setPropertyValue("BottomBorder", uno::Any(aBorderProps));
+            }
             OUString pProperties[] = {
                 "AnchorType",
                 "Surround",
@@ -2339,16 +2341,23 @@ void DomainMapper_Impl::appendOLE( const OUString& rStreamName, const std::share
                 "LeftMargin",
                 "RightMargin",
                 "TopMargin",
-                "BottomMargin",
-                "FillStyle",
-                "FillColor",
-                "FillColor2",
-                "LineStyle",
+                "BottomMargin"
             };
             for (const OUString& s : pProperties)
             {
                 const uno::Any aVal = xReplacementProperties->getPropertyValue(s);
                 xOLEProperties->setPropertyValue(s, aVal);
+            }
+
+            if (xReplacementProperties->getPropertyValue("FillStyle").get<css::drawing::FillStyle>()
+                != css::drawing::FillStyle::FillStyle_NONE) // Apply fill props if style is set
+            {
+                xOLEProperties->setPropertyValue(
+                    "FillStyle", xReplacementProperties->getPropertyValue("FillStyle"));
+                xOLEProperties->setPropertyValue(
+                    "FillColor", xReplacementProperties->getPropertyValue("FillColor"));
+                xOLEProperties->setPropertyValue(
+                    "FillColor2", xReplacementProperties->getPropertyValue("FillColor2"));
             }
         }
         else

sw/qa/extras/layout/layout2.cxx
@@ -518,6 +518,22 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf126425)
     assertXPath(pXmlDoc, "//textarray", 14);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testUnusedOLEprops)
+{
+    CPPUNIT_ASSERT(createDoc("tdf138465min.docx"));
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: >300
+    // - Actual  : 142
+    // i.e. the formula squashed
+    CPPUNIT_ASSERT_GREATEREQUAL(
+        double(300),
+        getXPath(pXmlDoc, "/root/page/body/txt[2]/anchored/fly/notxt/infos/bounds", "height")
+            .toDouble());
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf123268)
 {
     SwDoc* pDoc = createDoc("tdf123268.odt");