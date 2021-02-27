LemmingAvalanche/libreoffice-repaired

commit e0fbf46de25c696358c3ae7cb90b9920ae53c072

avoid coverity#1371269 Missing move assignment operator
Change-Id: I0ba8991ecc0934d839ac4e1bfe80f0568057fcc7

sw/source/ui/misc/outline.cxx
@@ -305,7 +305,7 @@ IMPL_LINK( SwOutlineTabDialog, MenuSelectHdl, Menu *, pMenu, bool )
         const SwNumRulesWithName *pRules = pChapterNumRules->GetRules( nLevelNo );
         if( pRules )
         {
-            *xNumRule = pRules->MakeNumRule(rWrtSh);
+            xNumRule.reset(pRules->MakeNumRule(rWrtSh));
             xNumRule->SetRuleType( OUTLINE_RULE );
         }
         else

sw/source/uibase/config/uinums.cxx
@@ -159,19 +159,19 @@ SwNumRulesWithName& SwNumRulesWithName::operator=(const SwNumRulesWithName &rCop
     return *this;
 }
 
-SwNumRule SwNumRulesWithName::MakeNumRule(SwWrtShell& rSh) const
+SwNumRule* SwNumRulesWithName::MakeNumRule(SwWrtShell& rSh) const
 {
     // #i89178#
-    SwNumRule aChg(maName, numfunc::GetDefaultPositionAndSpaceMode());
-    aChg.SetAutoRule( false );
+    SwNumRule* pChg = new SwNumRule(maName, numfunc::GetDefaultPositionAndSpaceMode());
+    pChg->SetAutoRule( false );
     for (sal_uInt16 n = 0; n < MAXLEVEL; ++n)
     {
         SwNumFormatGlobal* pFormat = aFormats[ n ];
         if (!pFormat)
             continue;
-        aChg.Set(n, pFormat->MakeNumFormat(rSh));
+        pChg->Set(n, pFormat->MakeNumFormat(rSh));
     }
-    return aChg;
+    return pChg;
 }
 
 void SwNumRulesWithName::GetNumFormat(

sw/source/uibase/inc/uinums.hxx
@@ -73,7 +73,7 @@ public:
     SwNumRulesWithName &operator=(const SwNumRulesWithName &);
 
     const OUString& GetName() const               { return maName; }
-    SwNumRule MakeNumRule(SwWrtShell& rSh) const;
+    SwNumRule* MakeNumRule(SwWrtShell& rSh) const;
 
     void GetNumFormat(size_t, SwNumFormat const*&, OUString const*&) const;
 };

commit 817db293aeccfe955cb8b813d1bff29a8ac9581d

tdf#140528 Crash in writer when switching from Numbering to Position
regression from
    commit e0fbf46de25c696358c3ae7cb90b9920ae53c072
    Date:   Mon Dec 5 09:52:43 2016 +0000
    avoid coverity#1371269 Missing move assignment operator

rather write this code in a more obvious fashion, and bypass
the use of the very weird operator= method, which barely
copies anything at all.

Change-Id: I548d8f73224c2625ed6389861551038b18396b0f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111677
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/inc/numrule.hxx
@@ -136,7 +136,7 @@ private:
     bool mbCountPhantoms;
     bool mbUsedByRedline;    /// it needs to export as part of tracked numbering change
 
-    const SvxNumberFormat::SvxNumPositionAndSpaceMode meDefaultNumberFormatPositionAndSpaceMode;
+    SvxNumberFormat::SvxNumPositionAndSpaceMode meDefaultNumberFormatPositionAndSpaceMode;
     OUString msDefaultListId;
     std::shared_ptr<SfxGrabBagItem> mpGrabBagItem; ///< Style InteropGrabBag.
 
@@ -152,6 +152,8 @@ public:
     SwNumRule& operator=( const SwNumRule& );
     bool operator==( const SwNumRule& ) const;
     bool operator!=( const SwNumRule& r ) const { return !(*this == r); }
+
+    void Reset( const OUString& rName );
 
     const SwNumFormat* GetNumFormat( sal_uInt16 i ) const;
     const SwNumFormat& Get( sal_uInt16 i ) const;

sw/source/core/doc/number.cxx
@@ -553,6 +553,23 @@ SwNumRule& SwNumRule::operator=( const SwNumRule& rNumRule )
     return *this;
 }
 
+void SwNumRule::Reset( const OUString& rName )
+{
+    for( sal_uInt16 n = 0; n < MAXLEVEL; ++n )
+        Set( n, nullptr);
+
+    meRuleType = NUM_RULE;
+    msName = rName;
+    mbAutoRuleFlag = true;
+    mbInvalidRuleFlag = true;
+    mbContinusNum = false;
+    mbAbsSpaces = false;
+    mbHidden = false;
+    mnPoolFormatId = USHRT_MAX;
+    mnPoolHelpId = USHRT_MAX;
+    mnPoolHlpFileId = UCHAR_MAX;
+}
+
 bool SwNumRule::operator==( const SwNumRule& rRule ) const
 {
     bool bRet = meRuleType == rRule.meRuleType &&

sw/source/ui/misc/outline.cxx
@@ -277,7 +277,7 @@ IMPL_LINK(SwOutlineTabDialog, MenuSelectHdl, const OString&, rIdent, void)
         const SwNumRulesWithName *pRules = pChapterNumRules->GetRules( nLevelNo );
         if( pRules )
         {
-            xNumRule = pRules->MakeNumRule(rWrtSh);
+            pRules->ResetNumRule(rWrtSh, *xNumRule);
             xNumRule->SetRuleType( OUTLINE_RULE );
             SfxTabPage* pOutlinePage = GetTabPage("numbering");
             assert(pOutlinePage);

sw/source/uibase/config/uinums.cxx
@@ -143,19 +143,18 @@ SwNumRulesWithName& SwNumRulesWithName::operator=(const SwNumRulesWithName &rCop
     return *this;
 }
 
-std::unique_ptr<SwNumRule> SwNumRulesWithName::MakeNumRule(SwWrtShell& rSh) const
+void SwNumRulesWithName::ResetNumRule(SwWrtShell& rSh, SwNumRule& rNumRule) const
 {
     // #i89178#
-    std::unique_ptr<SwNumRule> pChg(new SwNumRule(maName, numfunc::GetDefaultPositionAndSpaceMode()));
-    pChg->SetAutoRule( false );
+    rNumRule.Reset(maName);
+    rNumRule.SetAutoRule( false );
     for (sal_uInt16 n = 0; n < MAXLEVEL; ++n)
     {
         SwNumFormatGlobal* pFormat = aFormats[ n ].get();
         if (!pFormat)
             continue;
-        pChg->Set(n, pFormat->MakeNumFormat(rSh));
+        rNumRule.Set(n, pFormat->MakeNumFormat(rSh));
     }
-    return pChg;
 }
 
 void SwNumRulesWithName::GetNumFormat(

sw/source/uibase/inc/uinums.hxx
@@ -72,7 +72,7 @@ public:
     SwNumRulesWithName &operator=(const SwNumRulesWithName &);
 
     const OUString& GetName() const               { return maName; }
-    std::unique_ptr<SwNumRule> MakeNumRule(SwWrtShell& rSh) const;
+    void ResetNumRule(SwWrtShell& rSh, SwNumRule& ) const;
 
     void GetNumFormat(size_t, SwNumFormat const*&, OUString const*&) const;
 };