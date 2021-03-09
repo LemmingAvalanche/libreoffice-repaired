LemmingAvalanche/libreoffice-repaired

commit 0ac45b0739245ed5ad953371b7ac62eacc3374e7

tdf#83419 sw change tracking: clean-up autocorrect
fix of commit 033e7542fc469c6175e7c942dc8122b4dc3cf49a
(tdf#83419 sw change tracking: fix bad autocorrect).

Now automatic sentence capitalization and
correction of two initial capitals are allowed
again, if there is a tracked deletion in the same
paragraph before the modified word.

Change-Id: I8c583df2f14468b47079019009e7937f559b652b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105477
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -2523,6 +2523,35 @@ SwRedlineTable::size_type DocumentRedlineManager::GetRedlinePos( const SwNode& r
     // #TODO - add 'SwExtraRedlineTable' also ?
 }
 
+bool DocumentRedlineManager::HasRedline( const SwPaM& rPam, RedlineType nType, bool bStartOrEndInRange ) const // xxx
+{
+    SwPosition currentStart(*rPam.Start());
+    SwPosition currentEnd(*rPam.End());
+    SwNodeIndex pEndNodeIndex(currentEnd.nNode.GetNode());
+
+    for( SwRedlineTable::size_type n = GetRedlinePos( rPam.Start()->nNode.GetNode(), nType );
+                    n < mpRedlineTable->size(); ++n )
+    {
+        const SwRangeRedline* pTmp = (*mpRedlineTable)[ n ];
+
+        if ( pTmp->Start()->nNode > pEndNodeIndex )
+            break;
+
+        if( RedlineType::Any != nType && nType != pTmp->GetType() )
+            continue;
+
+        // redline over the range
+        if ( currentStart < *pTmp->End() && *pTmp->Start() <= currentEnd &&
+             // starting or ending within the range
+             ( !bStartOrEndInRange ||
+                 ( currentStart <= *pTmp->Start() || *pTmp->End() <= currentEnd ) ) )
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 const SwRangeRedline* DocumentRedlineManager::GetRedline( const SwPosition& rPos,
                                     SwRedlineTable::size_type* pFndPos ) const
 {

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -7652,11 +7652,18 @@ void SwUiWriterTest::testRedlineAutoCorrect()
     nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
     CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
 
+    // Including capitalization
+    pWrtShell->Insert("end. word");
+    pWrtShell->AutoCorrect(corr, ' ');
+    sReplaced = "tset test end. Word ";
+    nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+
     // tracked deletions after the correction point doesn't affect autocorrect
     dispatchCommand(mxComponent, ".uno:GoToStartOfDoc", {});
     pWrtShell->Insert("a");
     pWrtShell->AutoCorrect(corr, ' ');
-    sReplaced = "A tset test ";
+    sReplaced = "A tset test end. Word ";
     nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
     CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
 }

sw/inc/IDocumentRedlineAccess.hxx
@@ -170,6 +170,11 @@ public:
         /*[in]*/const SwNode& rNode,
         /*[in]*/RedlineType nType) const = 0;
 
+    virtual bool HasRedline(
+        /*[in]*/const SwPaM& rPam,
+        /*[in]*/RedlineType nType,
+        /*[in]*/bool bStartOrEndInRange) const = 0;
+
     virtual void CompressRedlines() = 0;
 
     virtual const SwRangeRedline* GetRedline(

sw/source/core/edit/acorrect.cxx
@@ -42,6 +42,7 @@
 #include <editeng/acorrcfg.hxx>
 #include <redline.hxx>
 #include <IDocumentRedlineAccess.hxx>
+#include <rootfrm.hxx>
 
 using namespace ::com::sun::star;
 
@@ -231,31 +232,10 @@ bool SwAutoCorrDoc::ReplaceRange( sal_Int32 nPos, sal_Int32 nSourceLength, const
     // tdf#83419 avoid bad autocorrect with visible redlines
     // e.g. replacing the first letter of the tracked deletion
     // with its capitalized (and not deleted) version.
-    if ( bDoReplace )
+    if ( bDoReplace && !pFrame->getRootFrame()->IsHideRedlines() &&
+         m_rEditSh.GetDoc()->getIDocumentRedlineAccess().HasRedline( *pPam, RedlineType::Delete, /*bStartOrEndInRange=*/false ) )
     {
-        const OUString& rOrigText = pos.first->GetText();
-        // GetRedlineText() doesn't contain dummy characters, so handle them
-        sal_Int32 nLengthCorrection = 0;
-        for (sal_Int32 n = 0; n < rOrigText.getLength(); ++n)
-        {
-            sal_Unicode const Char = rOrigText[n];
-            if ( CH_TXTATR_BREAKWORD == Char || CH_TXTATR_INWORD == Char )
-                ++nLengthCorrection;
-        }
-        sal_Int32 nDelChars = rOrigText.getLength() - nLengthCorrection -
-                              pos.first->GetRedlineText().getLength();
-        // Are there tracked deletions before the correction point?
-        if ( nDelChars > 0 && pos.first->GetRedlineText().compareTo( nLengthCorrection == 0
-                                  ? rOrigText
-                                  : rOrigText.replaceAll(OUString(CH_TXTATR_INWORD), "")
-                                         .replaceAll(OUString(CH_TXTATR_BREAKWORD), ""),
-                              pos.second + nSourceLength + nDelChars ) != 0 &&
-            // and are they visible?
-            pFrame->GetText().compareTo(
-                    rOrigText, pos.second + nSourceLength + nDelChars + nLengthCorrection) == 0 )
-        {
-            bDoReplace = false;
-        }
+        bDoReplace = false;
     }
 
     if ( bDoReplace )
@@ -434,22 +414,9 @@ bool SwAutoCorrDoc::ChgAutoCorrWord( sal_Int32& rSttPos, sal_Int32 nEndPos,
         SwPaM aPam(aStartPos, aEndPos);
 
         // don't replace, if a redline starts or ends within the original text
-        for ( SwRedlineTable::size_type nAct =
-                  pDoc->getIDocumentRedlineAccess().GetRedlinePos( m_rCursor.GetNode(), RedlineType::Any );
-                  nAct < pDoc->getIDocumentRedlineAccess().GetRedlineTable().size(); ++nAct )
+        if ( pDoc->getIDocumentRedlineAccess().HasRedline( aPam, RedlineType::Any, /*bStartOrEndInRange=*/true ) )
         {
-            const SwRangeRedline* pRed = pDoc->getIDocumentRedlineAccess().GetRedlineTable()[ nAct ];
-
-            if ( pRed->Start()->nNode > pTextNd->GetIndex() )
-                break;
-
-            // redline over the original text
-            if ( aStartPos < *pRed->End() && *pRed->Start() < aEndPos &&
-                 // starting or ending within the original text
-                 ( aStartPos < *pRed->Start() || *pRed->End() < aEndPos ) )
-            {
-                return bRet;
-            }
+            return bRet;
         }
 
         if( pFnd->IsTextOnly() )

sw/source/core/inc/DocumentRedlineManager.hxx
@@ -76,6 +76,11 @@ public:
         /*[in]*/const SwNode& rNode,
         /*[in]*/RedlineType nType) const override;
 
+    virtual bool HasRedline(
+        /*[in]*/const SwPaM& rPam,
+        /*[in]*/RedlineType nType,
+        /*[in]*/bool bStartOrEndInRange) const override;
+
     virtual void CompressRedlines() override;
 
     virtual const SwRangeRedline* GetRedline(

commit 76c8bf006d2406b7d935e76d9a50893eb67ee12c

tdf#140674 sw change tracking: fix :emoji: replacement
Regression from commit 0ac45b0739245ed5ad953371b7ac62eacc3374e7
(tdf#83419 sw change tracking: clean-up autocorrect).

Note: only :snowman: -> ☃ was added to the test autocorrect
file acor_en-GB.dat.

Change-Id: Ic3013e969ab937a5f9e5b6d1a6fac2f533b541f8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112166
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -2555,7 +2555,7 @@ SwRedlineTable::size_type DocumentRedlineManager::GetRedlinePos( const SwNode& r
     // #TODO - add 'SwExtraRedlineTable' also ?
 }
 
-bool DocumentRedlineManager::HasRedline( const SwPaM& rPam, RedlineType nType, bool bStartOrEndInRange ) const // xxx
+bool DocumentRedlineManager::HasRedline( const SwPaM& rPam, RedlineType nType, bool bStartOrEndInRange ) const
 {
     SwPosition currentStart(*rPam.Start());
     SwPosition currentEnd(*rPam.End());
@@ -2576,7 +2576,7 @@ bool DocumentRedlineManager::HasRedline( const SwPaM& rPam, RedlineType nType, b
         if ( currentStart < *pTmp->End() && *pTmp->Start() <= currentEnd &&
              // starting or ending within the range
              ( !bStartOrEndInRange ||
-                 ( currentStart <= *pTmp->Start() || *pTmp->End() <= currentEnd ) ) )
+                 ( currentStart < *pTmp->Start() || *pTmp->End() < currentEnd ) ) )
         {
             return true;
         }

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -399,6 +399,7 @@ public:
     void testSpellOnlineParameter();
     void testRedlineAutoCorrect();
     void testRedlineAutoCorrect2();
+    void testEmojiAutoCorrect();
 #if HAVE_FEATURE_PDFIUM
     void testInsertPdf();
 #endif
@@ -631,6 +632,7 @@ public:
     CPPUNIT_TEST(testSpellOnlineParameter);
     CPPUNIT_TEST(testRedlineAutoCorrect);
     CPPUNIT_TEST(testRedlineAutoCorrect2);
+    CPPUNIT_TEST(testEmojiAutoCorrect);
 #if HAVE_FEATURE_PDFIUM
     CPPUNIT_TEST(testInsertPdf);
 #endif
@@ -7768,6 +7770,39 @@ void SwUiWriterTest::testRedlineAutoCorrect2()
     pWrtShell->AutoCorrect(corr, ' ');
     nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
     sReplaced = u"Lorem,... Lorem,… ";
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+}
+
+void SwUiWriterTest::testEmojiAutoCorrect()
+{
+    SwDoc* pDoc = createDoc("redline-autocorrect2.fodt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    // Emoji replacement (:snowman: -> ☃)
+
+    // without change tracking
+    CPPUNIT_ASSERT(!(pWrtShell->GetRedlineFlags() & RedlineFlags::On));
+    SwAutoCorrect corr(*SvxAutoCorrCfg::Get().GetAutoCorrect());
+    pWrtShell->Insert(":snowman");
+    pWrtShell->AutoCorrect(corr, ':');
+    sal_uLong nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    OUString sReplaced = u"☃Lorem,";
+    nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+
+    // with change tracking (showing redlines)
+    RedlineFlags const nMode(pWrtShell->GetRedlineFlags() | RedlineFlags::On);
+    CPPUNIT_ASSERT(nMode & (RedlineFlags::ShowDelete | RedlineFlags::ShowInsert));
+    pWrtShell->SetRedlineFlags(nMode);
+    CPPUNIT_ASSERT(nMode & RedlineFlags::On);
+    CPPUNIT_ASSERT(nMode & RedlineFlags::ShowDelete);
+
+    pWrtShell->Insert(":snowman");
+    pWrtShell->AutoCorrect(corr, ':');
+    sReplaced = u"☃☃Lorem,";
+    nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+
+    // tdf#140674 This was ":snowman:" instead of autocorrect
     CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
 }
 