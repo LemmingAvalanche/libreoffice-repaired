LemmingAvalanche/libreoffice-repaired

commit 7e0b38e7b86946b426c68e543a10488cb057c67f

tdf#138995 DOCX import: fix handling of textbox zorders
Regression from commit c6a2de20fc98b8559aa44e0383788eb9fbf50338 (oox:
import WPS shape with text as shape with textbox, 2014-06-18), the
problem was that a textbox's shape + textframe are internally 2 sdr
objects, so once GraphicZOrderHelper knows the current shape should be
on top of a shape+frame pair, it should suggest a larger ZOrder.

This is necessary till there is no setter version of
SwTextBoxHelper::getOrdNum(), which would allow import filters to ignore
this complexity, but that would be a larger change.

Change-Id: Ibbb1bcd9301eb369f25f211120f62be7c59b0fd2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109111
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/CppunitTest_sw_core_doc.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sw_core_doc, \
     editeng \
     sal \
     sfx \
+    svxcore \
     sw \
 	swqahelper \
     test \

sw/qa/core/doc/doc.cxx
@@ -113,6 +113,22 @@ CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testLocaleIndependentTemplate)
     ErrorRegistry::Reset();
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testTextBoxZOrder)
+{
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "textbox-zorder.docx");
+    SwFrameFormats& rFormats = *pDoc->GetSpzFrameFormats();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(3), rFormats.size());
+    const SwFrameFormat* pEllipse = rFormats[2];
+    const SdrObject* pEllipseShape = pEllipse->FindRealSdrObject();
+    // Make sure we test the right shape.
+    CPPUNIT_ASSERT_EQUAL(OUString("Shape3"), pEllipseShape->GetName());
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 2
+    // - Actual  : 1
+    // i.e. the ellipse was under the frame of the shape-frame pair, not on top of it.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt32>(2), pEllipseShape->GetOrdNum());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

writerfilter/source/dmapper/GraphicHelpers.cxx
@@ -295,6 +295,20 @@ sal_Int32 GraphicZOrderHelper::findZOrder( sal_Int32 relativeHeight, bool bOldSt
             return 0;
         --it;
         itemZOrderOffset = 1; // after the topmost
+
+        // Check if this shape has a textbox. If so, the textbox will have its own ZOrder, so
+        // suggest a larger offset.
+        bool bTextBox = false;
+        uno::Reference<beans::XPropertySet> xShape = it->second;
+        uno::Reference<beans::XPropertySetInfo> xInfo = xShape->getPropertySetInfo();
+        if (xInfo->hasPropertyByName("TextBox"))
+        {
+            xShape->getPropertyValue("TextBox") >>= bTextBox;
+        }
+        if (bTextBox)
+        {
+            ++itemZOrderOffset;
+        }
     }
     // SwXFrame::getPropertyValue throws uno::RuntimeException
     // when its GetFrameFormat() returns nullptr

commit 33ab9ce6696a58fc6fab550d268625e8a72f0604

tdf#140292 sw: fix z-order of textbox on undo
Regression from commit 7e0b38e7b86946b426c68e543a10488cb057c67f
(tdf#138995 DOCX import: fix handling of textbox zorders, 2021-01-11),
the problem was already fixed by Michael Stahl, this just adds a test
for it.

Change-Id: Id613224d5fab0c2d2a3bdfd58bff6d77a92dd374
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112181
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/qa/core/draw/draw.cxx
@@ -10,11 +10,14 @@
 #include <swmodeltestbase.hxx>
 
 #include <svx/svdpage.hxx>
+#include <vcl/scheduler.hxx>
 
 #include <IDocumentDrawModelAccess.hxx>
 #include <docsh.hxx>
 #include <drawdoc.hxx>
 #include <wrtsh.hxx>
+#include <frameformats.hxx>
+#include <textboxhelper.hxx>
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/core/draw/data/";
 
@@ -44,6 +47,56 @@ CPPUNIT_TEST_FIXTURE(SwCoreDrawTest, testTextboxDeleteAsChar)
     // - Actual  : 2
     // i.e. the fly frame of the shape and the inner Writer image was not deleted.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), nActual);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreDrawTest, testTextboxUndoOrdNum)
+{
+    // Given a document with 5 frame formats:
+    // - picture
+    // - draw format + fly format and a picture in it
+    // - picture
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "textbox-undo-ordnum.docx");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    const SwFrameFormats& rFormats = *pDoc->GetSpzFrameFormats();
+    // Test the state before del + undo.
+    for (const auto& pFormat : rFormats)
+    {
+        const SwFrameFormat* pFlyFormat
+            = SwTextBoxHelper::getOtherTextBoxFormat(pFormat, RES_DRAWFRMFMT);
+        if (!pFlyFormat)
+        {
+            continue;
+        }
+
+        sal_Int32 nDrawOrdNum = pFormat->FindRealSdrObject()->GetOrdNum();
+        sal_Int32 nFlyOrdNum = pFlyFormat->FindRealSdrObject()->GetOrdNum();
+        CPPUNIT_ASSERT_EQUAL(nDrawOrdNum + 1, nFlyOrdNum);
+    }
+
+    // When selecting the first page, deleting the selection and undoing:
+    pWrtShell->Down(true, 3);
+    pWrtShell->DelLeft();
+    pWrtShell->Undo();
+
+    // Then the z-order of the fly format should be still the z-order of the draw format + 1, when
+    // the fly and draw formats form a textbox pair.
+    for (const auto& pFormat : rFormats)
+    {
+        const SwFrameFormat* pFlyFormat
+            = SwTextBoxHelper::getOtherTextBoxFormat(pFormat, RES_DRAWFRMFMT);
+        if (!pFlyFormat)
+        {
+            continue;
+        }
+
+        sal_Int32 nDrawOrdNum = pFormat->FindRealSdrObject()->GetOrdNum();
+        sal_Int32 nFlyOrdNum = pFlyFormat->FindRealSdrObject()->GetOrdNum();
+        // Without the accompanying fix in place, this test would have failed with:
+        // - Expected: 4
+        // - Actual  : 2
+        // i.e. the fly format was behind the draw format, not visible.
+        CPPUNIT_ASSERT_EQUAL(nDrawOrdNum + 1, nFlyOrdNum);
+    }
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();