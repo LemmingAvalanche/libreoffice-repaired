LemmingAvalanche/libreoffice-repaired

commit 77e483362277820a369a062f7b2d74a77bfd427e

tdf#138612 DOCX import: fix lost part of split table cell
by removing unnecessary rewriting of cell properties
during import of w:tblPrEx.

Regression from commit 4dd6995b077f5247c6de265a44ee4b962fc635e6
(tdf#133455 DOCX import: fix table border regression)

Change-Id: I93ce36991437644db439c8cf02e1a8503fbdfba7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107239
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -732,6 +732,23 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf133455, "tdf133455.docx")
     assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[11]/w:tc[1]/w:tcPr/w:tcBorders/w:bottom[@w:val = 'nil']", 0);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf138612, "tdf138612.docx")
+{
+    xmlDocUniquePtr pXmlDocument = parseExport("word/document.xml");
+
+    // Row 5 Col 1 - vertically merged cell
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[5]/w:tc[1]/w:tcPr/w:vMerge", "val", "restart");
+    // Row 5 Col 2 - split cell
+    // This was w:vMerge="restart"
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[5]/w:tc[2]/w:tcPr/w:vMerge", 0);
+
+    // Row 6 Col 1 - merged with cell in Row 5 Col 1
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[6]/w:tc[1]/w:tcPr/w:vMerge", "val", "continue");
+    // Row 6 Col 2 - split cell
+    // This was w:vMerge="continue" (merged with cell in Row 5 Col 2)
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[6]/w:tc[2]/w:tcPr/w:vMerge", 0);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128646, "tdf128646.docx")
 {
     // The problem was that not hidden shapes anchored to empty hidden paragraphs were imported as hidden.

writerfilter/source/dmapper/TablePropertiesHandler.hxx
@@ -76,10 +76,7 @@ private:
     void tableExceptionProps( TablePropertyMapPtr pProps )
     {
         if ( m_pTableManager )
-        {
             m_pTableManager->tableExceptionProps( pProps );
-            cellProps( pProps );
-        }
         else
             m_pCurrentProperties->InsertProps(pProps.get());
     };

commit 0d4fff1a601cd7acaa90fc630ec77b9f789178a5

tdf#140597 DOCX import: fix missing tblPrEx border
of first table cells, caused by the workaround for tdf#138612.

Now property set of a new cell is a copy of the table exception
property set of the table row, as needed for the import of the
table style inheritance.

Regression from commit 77e483362277820a369a062f7b2d74a77bfd427e
(tdf#138612 DOCX import: fix lost part of split table cell).

Change-Id: Iaf6637e757fbfeef7651a4300a7f65a23615f5c5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112247
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/TableManager.hxx
@@ -112,7 +112,14 @@ class TableManager : public virtual SvRefBase
 
         void resetCellProps()
         {
-            mpCellProps = getTableExceptionProps();
+            // copy tblPrEx table exception properties, if they exist
+            if (getTableExceptionProps().is())
+            {
+                mpCellProps = new TablePropertyMap;
+                mpCellProps->InsertProps(getTableExceptionProps().get());
+            }
+            else
+                mpCellProps.clear();
         }
 
         void setCellProps(TablePropertyMapPtr pProps)
@@ -147,6 +154,8 @@ class TableManager : public virtual SvRefBase
         void setTableExceptionProps(TablePropertyMapPtr pProps)
         {
             mpTableExceptionProps = pProps;
+            // set table exception properties of the first cell
+            resetCellProps();
         }
 
         const TablePropertyMapPtr& getTableExceptionProps() const

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -749,6 +749,19 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf138612, "tdf138612.docx")
     assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[6]/w:tc[2]/w:tcPr/w:vMerge", 0);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf140597, "tdf140597.docx")
+{
+    xmlDocUniquePtr pXmlDocument = parseExport("word/document.xml");
+
+    // There were missing tblPrEx table exception borders
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:tcPr/w:tcBorders/w:top");
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:tcPr/w:tcBorders/w:start");
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[3]/w:tc[1]/w:tcPr/w:tcBorders/w:top");
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[3]/w:tc[1]/w:tcPr/w:tcBorders/w:start");
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[4]/w:tc[1]/w:tcPr/w:tcBorders/w:top");
+    assertXPath(pXmlDocument, "/w:document/w:body/w:tbl/w:tr[4]/w:tc[1]/w:tcPr/w:tcBorders/w:start");
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128646, "tdf128646.docx")
 {
     // The problem was that not hidden shapes anchored to empty hidden paragraphs were imported as hidden.