LemmingAvalanche/libreoffice-repaired

commit b52ea28ee2f5945c3fc67521de8bb60ad1f15a8e

sw: store Outline Numbering as ODF fragment instead of SfxPoolItems
This should be less fragile and re-uses some xmloff and unocore code.

Storing from ~SwBaseNumRules() does not work because other gobals that
the export code relies on are already dead at the time it is called from
a terminate() listener, so store when the dialog updates a format.

Change-Id: I4e148b82d0a338ec7ffa6429c6e162db79f8c44e

sw/source/uibase/config/StoredChapterNumbering.cxx
@@ -1,0 +1,496 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <uinums.hxx>
+
+#include <cppuhelper/implbase2.hxx>
+
+#include <com/sun/star/container/XIndexReplace.hpp>
+#include <com/sun/star/container/XNamed.hpp>
+#include <com/sun/star/io/XActiveDataSource.hpp>
+#include <com/sun/star/util/MeasureUnit.hpp>
+#include <com/sun/star/xml/sax/Parser.hpp>
+#include <com/sun/star/xml/sax/Writer.hpp>
+
+#include <comphelper/processfactory.hxx>
+
+#include <unotools/streamwrap.hxx>
+
+#include <xmloff/xmlnmspe.hxx>
+#include <xmloff/xmltoken.hxx>
+#include <xmloff/nmspmap.hxx>
+#include <xmloff/xmlexp.hxx>
+#include <xmloff/xmlnume.hxx>
+#include <xmloff/xmlimp.hxx>
+#include <xmloff/xmlictxt.hxx>
+#include <xmloff/xmlnumi.hxx>
+
+#include <vcl/svapp.hxx>
+
+#include <unosett.hxx>
+
+
+using namespace ::com::sun::star;
+using namespace ::xmloff::token;
+
+namespace sw {
+
+class StoredChapterNumberingRules
+    : public ::cppu::WeakImplHelper2<container::XNamed,container::XIndexReplace>
+{
+private:
+    // TODO in case this ever becomes accessible via api need a invalidate
+    SwBaseNumRules & m_rNumRules;
+    sal_uInt16 const m_nIndex;
+
+    SwNumRulesWithName * GetOrCreateRules()
+    {
+        SwNumRulesWithName const* pRules(m_rNumRules.GetRules(m_nIndex));
+        if (!pRules)
+        {
+            m_rNumRules.CreateEmptyNumRule(m_nIndex);
+            pRules = m_rNumRules.GetRules(m_nIndex);
+            assert(pRules);
+        }
+        return const_cast<SwNumRulesWithName*>(pRules);
+    }
+
+public:
+    StoredChapterNumberingRules(
+            SwBaseNumRules & rNumRules, sal_uInt16 const nIndex)
+        : m_rNumRules(rNumRules)
+        , m_nIndex(nIndex)
+    {
+        assert(m_nIndex < SwBaseNumRules::nMaxRules);
+    }
+
+    // XNamed
+    virtual OUString SAL_CALL getName()
+        throw (uno::RuntimeException, std::exception) SAL_OVERRIDE
+    {
+        SolarMutexGuard g;
+        SwNumRulesWithName const* pRules(m_rNumRules.GetRules(m_nIndex));
+        if (!pRules)
+        {
+            return OUString();
+        }
+        return pRules->GetName();
+    }
+
+    virtual void SAL_CALL setName(OUString const& rName)
+        throw (uno::RuntimeException, std::exception) SAL_OVERRIDE
+    {
+        SolarMutexGuard g;
+        SwNumRulesWithName *const pRules(GetOrCreateRules());
+        pRules->SetName(rName);
+    }
+
+    // XElementAccess
+    virtual uno::Type SAL_CALL getElementType()
+        throw (uno::RuntimeException, std::exception) SAL_OVERRIDE
+    {
+        return ::cppu::UnoType<uno::Sequence<beans::PropertyValue>>::get();
+    }
+
+    virtual ::sal_Bool SAL_CALL hasElements()
+        throw (uno::RuntimeException, std::exception) SAL_OVERRIDE
+    {
+        return sal_True;
+    }
+
+    // XIndexAccess
+    virtual sal_Int32 SAL_CALL getCount()
+        throw (uno::RuntimeException, std::exception) SAL_OVERRIDE
+    {
+        return MAXLEVEL;
+    }
+
+    virtual uno::Any SAL_CALL getByIndex(sal_Int32 nIndex)
+        throw (lang::IndexOutOfBoundsException, lang::WrappedTargetException,
+               uno::RuntimeException, std::exception) SAL_OVERRIDE
+    {
+        if (nIndex < 0 || MAXLEVEL <= nIndex)
+            throw lang::IndexOutOfBoundsException();
+
+        SolarMutexGuard g;
+        SwNumRulesWithName const* pRules(m_rNumRules.GetRules(m_nIndex));
+        if (!pRules)
+        {
+            return uno::Any();
+        }
+        SwNumFmt const* pNumFmt(0);
+        OUString const* pCharStyleName(0);
+        pRules->GetNumFmt(nIndex, pNumFmt, pCharStyleName);
+        if (!pNumFmt)
+        {   // the dialog only fills in those levels that are non-default
+            return uno::Any(); // the export will ignore this level, yay
+        }
+        assert(pCharStyleName);
+        OUString dummy; // pass in empty HeadingStyleName - can't import anyway
+        uno::Sequence<beans::PropertyValue> const ret(
+            SwXNumberingRules::GetPropertiesForNumFmt(
+                *pNumFmt, *pCharStyleName, &dummy));
+        return uno::makeAny(ret);
+    }
+
+    // XIndexReplace
+    virtual void SAL_CALL replaceByIndex(
+            sal_Int32 nIndex, uno::Any const& rElement)
+        throw (lang::IllegalArgumentException, lang::IndexOutOfBoundsException,
+               lang::WrappedTargetException, uno::RuntimeException,
+               std::exception) SAL_OVERRIDE
+    {
+        if (nIndex < 0 || MAXLEVEL <= nIndex)
+            throw lang::IndexOutOfBoundsException();
+        uno::Sequence<beans::PropertyValue> props;
+        if (!(rElement >>= props))
+            throw lang::IllegalArgumentException("invalid type",
+                    static_cast< ::cppu::OWeakObject*>(this), 1);
+
+        SolarMutexGuard g;
+        SwNumFmt numFmt;
+        OUString charStyleName;
+        SwXNumberingRules::SetPropertiesToNumFmt(
+            numFmt,
+            charStyleName,
+            0, 0, 0, 0, 0,
+            props);
+        SwNumRulesWithName *const pRules(GetOrCreateRules());
+        pRules->SetNumFmt(nIndex, numFmt, charStyleName);
+    }
+};
+
+class StoredChapterNumberingExport
+    : public SvXMLExport
+{
+public:
+    StoredChapterNumberingExport(
+            uno::Reference<uno::XComponentContext> const& xContext,
+            OUString const& rFileName,
+            uno::Reference<xml::sax::XDocumentHandler> const& xHandler)
+        : SvXMLExport(xContext, "sw::StoredChapterNumberingExport", rFileName,
+            util::MeasureUnit::CM, xHandler)
+    {
+        _GetNamespaceMap().Add(GetXMLToken(XML_NP_OFFICE),
+                               GetXMLToken(XML_N_OFFICE), XML_NAMESPACE_OFFICE);
+        _GetNamespaceMap().Add(GetXMLToken(XML_NP_TEXT),
+                               GetXMLToken(XML_N_TEXT), XML_NAMESPACE_TEXT);
+        _GetNamespaceMap().Add(GetXMLToken(XML_NP_STYLE),
+                               GetXMLToken(XML_N_STYLE), XML_NAMESPACE_STYLE);
+        _GetNamespaceMap().Add(GetXMLToken(XML_NP_FO),
+                               GetXMLToken(XML_N_FO), XML_NAMESPACE_FO);
+        _GetNamespaceMap().Add(GetXMLToken(XML_NP_SVG),
+                               GetXMLToken(XML_N_SVG), XML_NAMESPACE_SVG);
+    }
+
+    virtual void _ExportAutoStyles() SAL_OVERRIDE {}
+    virtual void _ExportMasterStyles() SAL_OVERRIDE {}
+    virtual void _ExportContent() SAL_OVERRIDE {}
+
+    void ExportRule(SvxXMLNumRuleExport & rExport,
+            uno::Reference<container::XIndexReplace> const& xRule)
+    {
+        uno::Reference<container::XNamed> const xNamed(xRule, uno::UNO_QUERY);
+        OUString const name(xNamed->getName());
+        bool bEncoded(false);
+        AddAttribute( XML_NAMESPACE_STYLE, XML_NAME,
+                      EncodeStyleName(name, &bEncoded) );
+        if (bEncoded)
+        {
+            AddAttribute(XML_NAMESPACE_STYLE, XML_DISPLAY_NAME, name);
+        }
+
+        SvXMLElementExport aElem( *this, XML_NAMESPACE_TEXT,
+                                  XML_OUTLINE_STYLE, true, true );
+        rExport.exportLevelStyles(xRule, true);
+    }
+
+    void ExportRules(
+            std::set<OUString> const& rCharStyles,
+            std::vector<uno::Reference<container::XIndexReplace>> const& rRules)
+    {
+        GetDocHandler()->startDocument();
+
+        AddAttribute(XML_NAMESPACE_NONE,
+                      _GetNamespaceMap().GetAttrNameByKey(XML_NAMESPACE_OFFICE),
+                      _GetNamespaceMap().GetNameByKey(XML_NAMESPACE_OFFICE));
+        AddAttribute(XML_NAMESPACE_NONE,
+                      _GetNamespaceMap().GetAttrNameByKey (XML_NAMESPACE_TEXT),
+                      _GetNamespaceMap().GetNameByKey(XML_NAMESPACE_TEXT));
+        AddAttribute(XML_NAMESPACE_NONE,
+                      _GetNamespaceMap().GetAttrNameByKey(XML_NAMESPACE_STYLE),
+                      _GetNamespaceMap().GetNameByKey(XML_NAMESPACE_STYLE));
+        AddAttribute(XML_NAMESPACE_NONE,
+                      _GetNamespaceMap().GetAttrNameByKey(XML_NAMESPACE_FO),
+                      _GetNamespaceMap().GetNameByKey(XML_NAMESPACE_FO));
+        AddAttribute(XML_NAMESPACE_NONE,
+                      _GetNamespaceMap().GetAttrNameByKey(XML_NAMESPACE_SVG),
+                      _GetNamespaceMap().GetNameByKey(XML_NAMESPACE_SVG));
+
+        {
+            // let's just have a office:styles as a dummy root
+            SvXMLElementExport styles(*this,
+                    XML_NAMESPACE_OFFICE, XML_STYLES, true, true);
+
+            // horrible hack for char styles to get display-name mapping
+            for (auto it = rCharStyles.begin(); it != rCharStyles.end(); ++it)
+            {
+                AddAttribute( XML_NAMESPACE_STYLE, XML_FAMILY, XML_TEXT );
+                bool bEncoded(false);
+                AddAttribute( XML_NAMESPACE_STYLE, XML_NAME,
+                              EncodeStyleName(*it, &bEncoded) );
+                if (bEncoded)
+                {
+                    AddAttribute(XML_NAMESPACE_STYLE, XML_DISPLAY_NAME, *it);
+                }
+
+                SvXMLElementExport style(*this,
+                        XML_NAMESPACE_STYLE, XML_STYLE, true, true);
+            }
+
+            SvxXMLNumRuleExport numRuleExport(*this);
+
+            for (auto it = rRules.begin(); it != rRules.end(); ++it)
+            {
+                ExportRule(numRuleExport, *it);
+            }
+        }
+
+        GetDocHandler()->endDocument();
+    }
+};
+
+/** Dummy import context for style:style element that can just read the
+    attributes needed to map name to display-name.
+    Unfortunately the "real" context for this depends on some other things.
+    The mapping is necessary to import the text:style-name attribute
+    of the text:outline-level-style element.
+ */
+class StoredChapterNumberingDummyStyleContext
+    : public SvXMLImportContext
+{
+public:
+    StoredChapterNumberingDummyStyleContext(
+            SvXMLImport & rImport,
+            sal_uInt16 const nPrefix, OUString const& rLocalName,
+            uno::Reference<xml::sax::XAttributeList> const& xAttrList)
+        : SvXMLImportContext(rImport, nPrefix, rLocalName)
+    {
+        OUString name;
+        OUString displayName;
+        sal_uInt16 nFamily(0);
+        for (sal_Int32 i = 0; i < xAttrList->getLength(); ++i)
+        {
+            OUString localName;
+            sal_uInt16 const prefix(rImport.GetNamespaceMap().GetKeyByAttrName(
+                xAttrList->getNameByIndex(i), &localName));
+            OUString const& rValue = xAttrList->getValueByIndex(i);
+
+            if (XML_NAMESPACE_STYLE == prefix)
+            {
+                if (IsXMLToken(localName, XML_FAMILY))
+                {
+                    if (IsXMLToken(rValue, XML_TEXT))
+                    {
+                        nFamily = XML_STYLE_FAMILY_TEXT_TEXT;
+                    }
+                }
+                else if (IsXMLToken(localName, XML_NAME))
+                {
+                    name = rValue;
+                }
+                else if (IsXMLToken(localName, XML_DISPLAY_NAME))
+                {
+                    displayName = rValue;
+                }
+            }
+        }
+        if (nFamily && !name.isEmpty() && !displayName.isEmpty())
+        {
+            rImport.AddStyleDisplayName(nFamily, name, displayName);
+        }
+    }
+};
+
+class StoredChapterNumberingImport;
+
+class StoredChapterNumberingRootContext
+    : public SvXMLImportContext
+{
+private:
+    SwBaseNumRules & m_rNumRules;
+    size_t m_nCounter;
+    ::std::vector<tools::SvRef<SvxXMLListStyleContext>> m_Contexts;
+
+public:
+    StoredChapterNumberingRootContext(
+            SwBaseNumRules & rNumRules, SvXMLImport & rImport,
+            sal_uInt16 const nPrefix, OUString const& rLocalName)
+        : SvXMLImportContext(rImport, nPrefix, rLocalName)
+        , m_rNumRules(rNumRules)
+        , m_nCounter(0)
+    {
+    }
+
+    virtual void EndElement() SAL_OVERRIDE
+    {
+        assert(m_Contexts.size() < SwBaseNumRules::nMaxRules);
+        for (auto iter = m_Contexts.begin(); iter != m_Contexts.end(); ++iter)
+        {
+            uno::Reference<container::XIndexReplace> const xRule(
+                new sw::StoredChapterNumberingRules(m_rNumRules,
+                    iter - m_Contexts.begin()));
+            (*iter)->FillUnoNumRule(xRule, 0);
+            // TODO: xmloff's outline-style import seems to ignore this???
+            uno::Reference<container::XNamed> const xNamed(xRule, uno::UNO_QUERY);
+            xNamed->setName((*iter)->GetDisplayName());
+        }
+    }
+
+    virtual SvXMLImportContext * CreateChildContext(
+        sal_uInt16 const nPrefix, OUString const& rLocalName,
+        uno::Reference<xml::sax::XAttributeList> const& xAttrList) SAL_OVERRIDE
+    {
+        if (XML_NAMESPACE_TEXT == nPrefix && IsXMLToken(rLocalName, XML_OUTLINE_STYLE))
+        {
+            ++m_nCounter;
+            if (m_nCounter <= SwBaseNumRules::nMaxRules)
+            {
+                SvxXMLListStyleContext *const pContext(
+                    new SvxXMLListStyleContext(GetImport(),
+                                nPrefix, rLocalName, xAttrList, true));
+                m_Contexts.push_back(pContext);
+                return pContext;
+            }
+        }
+        else if (XML_NAMESPACE_STYLE == nPrefix && IsXMLToken(rLocalName, XML_STYLE))
+        {
+            new StoredChapterNumberingDummyStyleContext(
+                    GetImport(), nPrefix, rLocalName, xAttrList);
+        }
+
+        return SvXMLImportContext::CreateChildContext(
+                    nPrefix, rLocalName, xAttrList);
+    }
+};
+
+class StoredChapterNumberingImport
+    : public SvXMLImport
+{
+private:
+    SwBaseNumRules & m_rNumRules;
+
+public:
+    StoredChapterNumberingImport(
+            uno::Reference<uno::XComponentContext> const& xContext,
+            SwBaseNumRules & rNumRules)
+        : SvXMLImport(xContext, "sw::StoredChapterNumberingImport", IMPORT_ALL)
+        , m_rNumRules(rNumRules)
+    {
+    }
+
+    virtual SvXMLImportContext * CreateContext(
+        sal_uInt16 const nPrefix, OUString const& rLocalName,
+        uno::Reference<xml::sax::XAttributeList> const& xAttrList) SAL_OVERRIDE
+    {
+        if (XML_NAMESPACE_OFFICE == nPrefix && IsXMLToken(rLocalName, XML_STYLES))
+        {
+            return new StoredChapterNumberingRootContext(m_rNumRules,
+                    *this, nPrefix, rLocalName);
+        }
+        return SvXMLImport::CreateContext(nPrefix, rLocalName, xAttrList);
+    }
+};
+
+void ExportStoredChapterNumberingRules(SwBaseNumRules & rRules,
+        SvStream & rStream, OUString const& rFileName)
+{
+    uno::Reference<uno::XComponentContext> const xContext(
+            ::comphelper::getProcessComponentContext());
+
+    uno::Reference<io::XOutputStream> const xOutStream(
+            new ::utl::OOutputStreamWrapper(rStream));
+
+    uno::Reference<xml::sax::XWriter> const xWriter(
+            xml::sax::Writer::create(xContext));
+
+    uno::Reference<io::XActiveDataSource> const xADS(xWriter, uno::UNO_QUERY);
+    xADS->setOutputStream(xOutStream);
+
+    uno::Reference<xml::sax::XDocumentHandler> const xHandler(
+            xWriter, uno::UNO_QUERY);
+
+    StoredChapterNumberingExport exp(xContext, rFileName, xWriter);
+
+    // if style name contains a space then name != display-name
+    // ... and the import needs to map from name to display-name then!
+    std::set<OUString> charStyles;
+    std::vector<uno::Reference<container::XIndexReplace>> numRules;
+    for (size_t i = 0; i < SwBaseNumRules::nMaxRules; ++i)
+    {
+        if (SwNumRulesWithName const* pRule = rRules.GetRules(i))
+        {
+            for (size_t j = 0; j < MAXLEVEL; ++j)
+            {
+                SwNumFmt const* pDummy(0);
+                OUString const* pCharStyleName(0);
+                pRule->GetNumFmt(j, pDummy, pCharStyleName);
+                if (pCharStyleName && !pCharStyleName->isEmpty())
+                {
+                    charStyles.insert(*pCharStyleName);
+                }
+            }
+            numRules.push_back(new StoredChapterNumberingRules(rRules, i));
+        }
+    }
+
+    try
+    {
+        exp.ExportRules(charStyles, numRules);
+    }
+    catch (uno::Exception const& e)
+    {
+        SAL_WARN("sw.ui",
+            "ExportStoredChapterNumberingRules: exception: " << e.Message);
+    }
+}
+
+void ImportStoredChapterNumberingRules(SwBaseNumRules & rRules,
+        SvStream & rStream, OUString const& rFileName)
+{
+    uno::Reference<uno::XComponentContext> const xContext(
+            ::comphelper::getProcessComponentContext());
+
+    uno::Reference<io::XInputStream> const xInStream(
+            new ::utl::OInputStreamWrapper(rStream));
+
+    uno::Reference<xml::sax::XParser> const xParser(
+            xml::sax::Parser::create(xContext));
+
+    uno::Reference<xml::sax::XDocumentHandler> const xHandler(
+            new StoredChapterNumberingImport(xContext, rRules));
+
+    xParser->setDocumentHandler(xHandler);
+
+    xml::sax::InputSource const source(xInStream, "", "", rFileName);
+
+    try
+    {
+        xParser->parseStream(source);
+    }
+    catch (uno::Exception const& e)
+    {
+        SAL_WARN("sw.ui",
+            "ImportStoredChapterNumberingRules: exception: " << e.Message);
+    }
+}
+
+} // namespace sw
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/xmloff/xmlnume.hxx
@@ -37,7 +37,7 @@ class SvXMLExportItemMapper;
 class SvXMLExport;
 class XMLTextListAutoStylePool;
 
-class SvxXMLNumRuleExport
+class XMLOFF_DLLPUBLIC SvxXMLNumRuleExport
 {
     SvXMLExport& rExport;
     const OUString sNumberingRules;
@@ -49,12 +49,7 @@ class SvxXMLNumRuleExport
     // to be suppressed on writing ODF 1.0 respectively ODF 1.1
     bool mbExportPositionAndSpaceModeLabelAlignment;
 
-    void exportLevelStyles(
-            const ::com::sun::star::uno::Reference<
-                ::com::sun::star::container::XIndexReplace > & xNumRule,
-            bool bOutline=false );
-
-    void exportLevelStyle(
+    SAL_DLLPRIVATE void exportLevelStyle(
             sal_Int32 nLevel,
             const ::com::sun::star::uno::Sequence<
                 ::com::sun::star::beans::PropertyValue>& rProps,
@@ -64,11 +59,11 @@ protected:
 
     // This method may be overloaded to add attributes to the <list-style>
     // element.
-    virtual void AddListStyleAttributes();
+    SAL_DLLPRIVATE virtual void AddListStyleAttributes();
 
-    bool exportStyle( const ::com::sun::star::uno::Reference<
+    SAL_DLLPRIVATE bool exportStyle( const ::com::sun::star::uno::Reference<
                             ::com::sun::star::style::XStyle >& rStyle );
-    void exportOutline();
+    SAL_DLLPRIVATE void exportOutline();
 
     SvXMLExport& GetExport() { return rExport; }
 
@@ -76,6 +71,12 @@ public:
 
     SvxXMLNumRuleExport( SvXMLExport& rExport );
     virtual ~SvxXMLNumRuleExport();
+
+    // should be private but sw::StoredChapterNumberingExport needs it
+    void exportLevelStyles(
+            const ::com::sun::star::uno::Reference<
+                ::com::sun::star::container::XIndexReplace > & xNumRule,
+            bool bOutline=false );
 
     void exportStyles( bool bUsed,
                        XMLTextListAutoStylePool *pPool,

include/xmloff/xmlnumi.hxx
@@ -30,7 +30,8 @@ class SvI18NMap;
 class SvxXMLListLevelStyleContext_Impl;
 typedef std::vector<SvxXMLListLevelStyleContext_Impl *> SvxXMLListStyle_Impl;
 
-class SvxXMLListStyleContext : public SvXMLStyleContext
+class XMLOFF_DLLPUBLIC SvxXMLListStyleContext
+    : public SvXMLStyleContext
 {
     const OUString       sIsPhysical;
     const OUString       sNumberingRules;
@@ -47,7 +48,7 @@ class SvxXMLListStyleContext : public SvXMLStyleContext
 
 protected:
 
-    virtual void SetAttribute( sal_uInt16 nPrefixKey,
+    SAL_DLLPRIVATE virtual void SetAttribute( sal_uInt16 nPrefixKey,
                                const OUString& rLocalName,
                                const OUString& rValue ) SAL_OVERRIDE;
 

sw/Library_sw.mk
@@ -574,6 +574,7 @@ $(eval $(call gb_Library_add_exception_objects,sw,\
     sw/source/uibase/cctrl/popbox \
     sw/source/uibase/cctrl/swlbox \
     sw/source/uibase/chrdlg/ccoll \
+    sw/source/uibase/config/StoredChapterNumbering \
     sw/source/uibase/config/barcfg \
     sw/source/uibase/config/caption \
     sw/source/uibase/config/cfgitems \

sw/source/uibase/config/uinums.cxx
@@ -63,7 +63,7 @@ SwBaseNumRules::SwBaseNumRules( const OUString& rFileName )
     Init();
 }
 
-SwBaseNumRules::~SwBaseNumRules()
+void SwBaseNumRules::Save()
 {
     if( bModified )
     {
@@ -78,14 +78,17 @@ SwBaseNumRules::~SwBaseNumRules()
         bool bRet = (pStream && pStream->GetError() == 0);
         if (bRet)
         {
-            Store( *pStream );
+            sw::ExportStoredChapterNumberingRules(*this, *pStream, sFileName);
 
             pStream->Flush();
 
             aMedium.Commit();
         }
     }
+}
 
+SwBaseNumRules::~SwBaseNumRules()
+{
     for( sal_uInt16 i = 0; i < nMaxRules; ++i )
         delete pNumRules[i];
 }
@@ -100,8 +103,15 @@ void  SwBaseNumRules::Init()
     if( aOpt.SearchFile( sNm, SvtPathOptions::PATH_USERCONFIG ))
     {
         SfxMedium aStrm( sNm, STREAM_STD_READ );
-        Load( *aStrm.GetInStream() );
+        sw::ImportStoredChapterNumberingRules(*this, *aStrm.GetInStream(), sFileName);
     }
+}
+
+void SwBaseNumRules::CreateEmptyNumRule(sal_uInt16 const nIndex)
+{
+    assert(nIndex < nMaxRules);
+    assert(!pNumRules[nIndex]);
+    pNumRules[nIndex] = new SwNumRulesWithName;
 }
 
 void SwBaseNumRules::ApplyNumRules(const SwNumRulesWithName &rCopy, sal_uInt16 nIdx)
@@ -111,6 +121,7 @@ void SwBaseNumRules::ApplyNumRules(const SwNumRulesWithName &rCopy, sal_uInt16 n
         pNumRules[nIdx] = new SwNumRulesWithName( rCopy );
     else
         *pNumRules[nIdx] = rCopy;
+    Save(); // store it immediately
 }
 
 bool SwBaseNumRules::Store(SvStream &rStream)
@@ -189,6 +200,11 @@ SwNumRulesWithName::SwNumRulesWithName( const SwNumRule &rCopy,
         else
             aFmts[ n ] = 0;
     }
+}
+
+SwNumRulesWithName::SwNumRulesWithName()
+{
+    memset(aFmts, 0, sizeof(aFmts));
 }
 
 SwNumRulesWithName::SwNumRulesWithName( const SwNumRulesWithName& rCopy )
@@ -277,6 +293,23 @@ void SwNumRulesWithName::Store( SvStream &rStream )
         else
             rStream.WriteChar( (char)0 );
     }
+}
+
+void SwNumRulesWithName::GetNumFmt(
+    size_t const nIndex, SwNumFmt const*& rpNumFmt, OUString const*& rpName) const
+{
+    rpNumFmt = (aFmts[nIndex]) ? &aFmts[nIndex]->aFmt : 0;
+    rpName = (aFmts[nIndex]) ? &aFmts[nIndex]->sCharFmtName : 0;
+}
+
+void SwNumRulesWithName::SetNumFmt(
+        size_t const nIndex, SwNumFmt const& rNumFmt, OUString const& rName)
+{
+    delete aFmts[nIndex];
+    aFmts[nIndex] = new _SwNumFmtGlobal(rNumFmt);
+    aFmts[nIndex]->sCharFmtName = rName;
+    aFmts[nIndex]->nCharPoolId = USHRT_MAX;
+    aFmts[nIndex]->aItems.clear();
 }
 
 SwNumRulesWithName::_SwNumFmtGlobal::_SwNumFmtGlobal( const SwNumFmt& rFmt )

sw/source/uibase/inc/uinums.hxx
@@ -27,6 +27,8 @@ class SfxPoolItem;
 class SwWrtShell;
 class SvStream;
 
+namespace sw { class StoredChapterNumberingRules; }
+
 #define MAX_NUM_RULES 9
 
 typedef boost::ptr_vector<SfxPoolItem> _SwNumFmtsAttrs;
@@ -38,6 +40,7 @@ class SW_DLLPUBLIC SwNumRulesWithName
     // (They should always be there!)
     class SAL_DLLPRIVATE _SwNumFmtGlobal
     {
+        friend class SwNumRulesWithName;
         SwNumFmt aFmt;
         OUString sCharFmtName;
         sal_uInt16 nCharPoolId;
@@ -58,7 +61,11 @@ class SW_DLLPUBLIC SwNumRulesWithName
     _SwNumFmtGlobal* aFmts[ MAXLEVEL ];
 
 protected:
+    friend class sw::StoredChapterNumberingRules;
+    friend class SwBaseNumRules;
     void SetName(const OUString& rSet) {maName = rSet;}
+    void SetNumFmt(size_t, SwNumFmt const&, OUString const&);
+    SwNumRulesWithName();
 
 public:
     SwNumRulesWithName(const SwNumRule &, const OUString &);
@@ -72,6 +79,8 @@ public:
     void MakeNumRule( SwWrtShell& rSh, SwNumRule& rChg ) const;
 
     void Store( SvStream& );
+
+    void GetNumFmt(size_t, SwNumFmt const*&, OUString const*&) const;
 };
 
 class SwBaseNumRules
@@ -88,12 +97,14 @@ protected:
     virtual bool        Store(SvStream&);
 
     void                Init();
+    void Save();
 
 public:
     SwBaseNumRules(const OUString& rFileName);
     virtual ~SwBaseNumRules();
 
     inline const SwNumRulesWithName*    GetRules(sal_uInt16 nIdx) const;
+    void CreateEmptyNumRule(sal_uInt16 nIdx); // for import
     virtual void                        ApplyNumRules(
                                                 const SwNumRulesWithName &rCopy,
                                                 sal_uInt16 nIdx);
@@ -116,6 +127,17 @@ inline const SwNumRulesWithName *SwBaseNumRules::GetRules(sal_uInt16 nIdx) const
     OSL_ENSURE(nIdx < nMaxRules, "Array der NumRules ueberindiziert.");
     return pNumRules[nIdx];
 }
+
+
+namespace sw
+{
+
+void ExportStoredChapterNumberingRules(
+        SwBaseNumRules & rRules, SvStream & rStream, OUString const&);
+void ImportStoredChapterNumberingRules(
+        SwBaseNumRules & rRules, SvStream & rStream, OUString const&);
+
+} // namespace sw
 
 #endif
 

commit 30dc5ac7ae065d9cd940cdac7988e7be3dec8b8f

tdf#140590 Crash in Save-As dialog in Tools>Chapter Numbering
This would only affect assert builds. The assert condition
here did not match the condition in createFastChildContext
several lines below.

Bug since this code was introduced in
    commit b52ea28ee2f5945c3fc67521de8bb60ad1f15a8e
    Date:   Fri Nov 7 19:11:11 2014 +0100
    sw: store Outline Numbering as ODF fragment instead of SfxPoolItems

Change-Id: I3f6a0b91e3c17f4ca358a75116481fc38c01e94e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112233
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/uibase/config/StoredChapterNumbering.cxx
@@ -320,7 +320,7 @@ public:
 
     virtual void SAL_CALL endFastElement(sal_Int32 /*Element*/) override
     {
-        assert(m_Contexts.size() < SwChapterNumRules::nMaxRules);
+        assert(m_Contexts.size() <= SwChapterNumRules::nMaxRules);
         for (auto iter = m_Contexts.begin(); iter != m_Contexts.end(); ++iter)
         {
             uno::Reference<container::XIndexReplace> const xRule(