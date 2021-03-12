LemmingAvalanche/libreoffice-repaired

commit a5c15405be3bfbf9875e02d55ada29a7e5405f92

tdf#116675 vcl tree list: toggle by label click (e.g. in AutoFilter)
Checkboxes in a tree list could be toggled only by clicking
on them, but not on the associated labels, despite UX guidelines
and interoperability requirements especially for AutoFilter usage
in the spreadsheet.

Note: Implemented only for generic VCL plugin. Testing on Linux:

SAL_USE_VCLPLUGIN=gen instdir/program/soffice

Co-authored-by: Tibor Nagy (NISZ)

Change-Id: Ibc3d42271a1be53a54e11dedf4ab3ab506d680cf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104414
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

vcl/source/treelist/treelistbox.cxx
@@ -2288,6 +2288,29 @@ void SvTreeListBox::MouseButtonDown( const MouseEvent& rMEvt )
 
 void SvTreeListBox::MouseButtonUp( const MouseEvent& rMEvt )
 {
+    // tdf#116675 clicking on an entry should toggle its checkbox
+    if (rMEvt.IsLeft() && (nTreeFlags & SvTreeFlags::CHKBTN))
+    {
+        const Point aPnt = rMEvt.GetPosPixel();
+        SvTreeListEntry* pEntry = GetEntry(aPnt);
+        if (pEntry && pEntry->m_Items.size() > 0)
+        {
+            SvLBoxItem* pItem = GetItem(pEntry, aPnt.X());
+            // if the checkbox button was clicked, that will be toggled later, do not toggle here
+            // anyway users probably don't want to toggle the checkbox by clickink on another button
+            if (!pItem || pItem->GetType() != SvLBoxItemType::Button)
+            {
+                SvLBoxButton* pItemCheckBox
+                    = static_cast<SvLBoxButton*>(pEntry->GetFirstItem(SvLBoxItemType::Button));
+                if (pItemCheckBox)
+                {
+                    pItemCheckBox->ClickHdl(pEntry);
+                    InvalidateEntry(pEntry);
+                }
+            }
+        }
+    }
+
     pImpl->MouseButtonUp( rMEvt );
 }
 

commit 60c24b097ff10ee0c0b6b3fd20449502aadfa3dc

tdf#140136 sc: fix tree list expansion in AutoFilter
Now clicking on +/- buttons (i.e. before the checkbox)
only expands/collapses the tree without toggling the
associated checkboxes, using the new GetItemPos() to get
the position (and width) of the checkbox in the actual
list item.

Regression from commit a5c15405be3bfbf9875e02d55ada29a7e5405f92
"tdf#116675 vcl tree list: toggle by label click (e.g. in AutoFilter)".

Note: Use generic VCL plugin to test it on Linux:

SAL_USE_VCLPLUGIN=gen instdir/program/soffice

Co-authored-by: Tibor Nagy (NISZ)

Change-Id: Iceb17bc9b235d297c313361429ee89f04d809e96
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111668
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

include/vcl/toolkit/treelistbox.hxx
@@ -637,6 +637,7 @@ public:
     void            InvalidateEntry( SvTreeListEntry* );
     SvLBoxItem*     GetItem( SvTreeListEntry*, tools::Long nX, SvLBoxTab** ppTab);
     SvLBoxItem*     GetItem( SvTreeListEntry*, tools::Long nX );
+    std::pair<tools::Long, tools::Long> GetItemPos(SvTreeListEntry* pEntry, sal_uInt16 nTabIdx);
 
     void            SetDragDropMode( DragDropMode );
     void            SetSelectionMode( SelectionMode );

vcl/source/treelist/treelistbox.cxx
@@ -2304,7 +2304,7 @@ void SvTreeListBox::MouseButtonUp( const MouseEvent& rMEvt )
             {
                 SvLBoxButton* pItemCheckBox
                     = static_cast<SvLBoxButton*>(pEntry->GetFirstItem(SvLBoxItemType::Button));
-                if (pItemCheckBox)
+                if (pItemCheckBox && GetItemPos(pEntry, 0).first < aPnt.X() - GetMapMode().GetOrigin().X())
                 {
                     pItemCheckBox->ClickHdl(pEntry);
                     InvalidateEntry(pEntry);
@@ -3033,6 +3033,45 @@ SvLBoxItem* SvTreeListBox::GetItem_Impl( SvTreeListEntry* pEntry, tools::Long nX
         nNextItem++;
     }
     return pItemClicked;
+}
+
+std::pair<tools::Long, tools::Long> SvTreeListBox::GetItemPos(SvTreeListEntry* pEntry, sal_uInt16 nTabIdx)
+{
+    sal_uInt16 nTabCount = aTabs.size();
+    sal_uInt16 nItemCount = pEntry->ItemCount();
+    if (nTabIdx >= nItemCount || nTabIdx >= nTabCount)
+        return std::make_pair(-1, -1);
+
+    SvLBoxTab* pTab = aTabs.front().get();
+    SvLBoxItem* pItem = &pEntry->GetItem(nTabIdx);
+    sal_uInt16 nNextItem = nTabIdx + 1;
+
+    tools::Long nRealWidth = pImpl->GetOutputSize().Width();
+    nRealWidth -= GetMapMode().GetOrigin().X();
+
+    SvLBoxTab* pNextTab = nNextItem < nTabCount ? aTabs[nNextItem].get() : nullptr;
+    tools::Long nStart = GetTabPos(pEntry, pTab);
+
+    tools::Long nNextTabPos;
+    if (pNextTab)
+        nNextTabPos = GetTabPos(pEntry, pNextTab);
+    else
+    {
+        nNextTabPos = nRealWidth;
+        if (nStart > nRealWidth)
+            nNextTabPos += 50;
+    }
+
+    auto nItemWidth(pItem->GetWidth(this, pEntry));
+    nStart += pTab->CalcOffset(nItemWidth, nNextTabPos - nStart);
+    auto nLen = nItemWidth;
+    if (pNextTab)
+    {
+        tools::Long nTabWidth = GetTabPos(pEntry, pNextTab) - nStart;
+        if (nTabWidth < nLen)
+            nLen = nTabWidth;
+    }
+    return std::make_pair(nStart, nLen);
 }
 
 tools::Long SvTreeListBox::getPreferredDimensions(std::vector<tools::Long> &rWidths) const