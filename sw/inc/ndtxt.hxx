LemmingAvalanche/libreoffice-repaired

commit 1352b20c0dead3736964bcca8485e9f0d403cadf

tdf#109266 sw change tracking: track transliteration
Format->Text->UPPERCASE, tOGGLE cASE etc. weren't
supported by change tracking.

Change-Id: Ic2c94375522a5469056837ab23fdbb9557212b53
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105345
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/txtnode/txtedt.cxx
@@ -1898,6 +1898,7 @@ void SwTextNode::TransliterateText(
     // now apply the changes from end to start to leave the offsets of the
     // yet unchanged text parts remain the same.
     size_t nSum(0);
+    bool bIsRedlineOn(GetDoc().getIDocumentRedlineAccess().IsRedlineOn());
     for (size_t i = 0; i < aChanges.size(); ++i)
     {   // check this here since AddChanges cannot be moved below
         // call to ReplaceTextOnly
@@ -1910,9 +1911,24 @@ void SwTextNode::TransliterateText(
                     "node text with insertion > node capacity.");
             return;
         }
-        if (pUndo)
-            pUndo->AddChanges( *this, rData.nStart, rData.nLen, rData.aOffsets );
-        ReplaceTextOnly( rData.nStart, rData.nLen, rData.sChanged, rData.aOffsets );
+
+        if ( bIsRedlineOn )
+        {
+            // create SwPaM with mark & point spanning the attributed text
+            //SwPaM aCurPaM( *this, *this, nBegin, nBegin + nLen ); <-- wrong c-tor, does sth different
+            SwPaM aCurPaM( *this, rData.nStart );
+            aCurPaM.SetMark();
+            aCurPaM.GetPoint()->nContent = rData.nStart + rData.nLen;
+            // replace the changed words
+            if ( aCurPaM.GetText() != rData.sChanged )
+                GetDoc().getIDocumentContentOperations().ReplaceRange( aCurPaM, rData.sChanged, false );
+        }
+        else
+        {
+            if (pUndo)
+                pUndo->AddChanges( *this, rData.nStart, rData.nLen, rData.aOffsets );
+            ReplaceTextOnly( rData.nStart, rData.nLen, rData.sChanged, rData.aOffsets );
+        }
     }
 }
 

sw/qa/extras/uiwriter/data2/lorem.fodt
@@ -1,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:officeooo="http://openoffice.org/2009/office" office:version="1.2" office:mimetype="application/vnd.oasis.opendocument.text" xmlns:dc="http://purl.org/dc/elements/1.1/">
+ <office:styles>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+  <style:default-style style:family="paragraph">
+   <style:text-properties fo:language="en" fo:country="US"/>
+  </style:default-style>
+ </office:styles>
+ <office:body>
+  <office:text>
+   <text:p>Lorem ipsum...</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -3093,6 +3093,75 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf90069)
     CPPUNIT_ASSERT_EQUAL(OUString("Lohit Devanagari"), sFontName);
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf109266)
+{
+    // transliteration with redlining
+    SwDoc* pDoc = createDoc("lorem.fodt");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    CPPUNIT_ASSERT(pDocShell);
+
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    sal_uLong nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    SwTextNode* pTextNode = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem ipsum..."), pTextNode->GetText());
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem ipsum..."), pTextNode->GetRedlineText());
+
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:ChangeCaseToTitleCase", {});
+
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem Ipsum..."), pTextNode->GetText());
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem Ipsum..."), pTextNode->GetRedlineText());
+
+    //turn on red-lining and show changes
+    RedlineFlags const mode(pWrtShell->GetRedlineFlags() | RedlineFlags::On);
+    CPPUNIT_ASSERT(mode & (RedlineFlags::ShowDelete | RedlineFlags::ShowInsert));
+    pWrtShell->SetRedlineFlags(mode);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE(
+        "redlines should be visible",
+        IDocumentRedlineAccess::IsShowChanges(pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:ChangeCaseToTitleCase", {});
+
+    // This was "Lorem Ipsum..." (missing redlining)
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem ipsumIpsum..."), pTextNode->GetText());
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem Ipsum..."), pTextNode->GetRedlineText());
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:ChangeCaseToUpper", {});
+
+    // This was "LOREM IPSUM..." (missing redlining)
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem ipsum...LOREM IPSUM..."), pTextNode->GetText());
+    CPPUNIT_ASSERT_EQUAL(OUString("LOREM IPSUM..."), pTextNode->GetRedlineText());
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:ChangeCaseToLower", {});
+
+    // This was "lorem ipsum..." (missing redlining)
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem ipsum...lorem ipsum..."), pTextNode->GetText());
+    CPPUNIT_ASSERT_EQUAL(OUString("lorem ipsum..."), pTextNode->GetRedlineText());
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:ChangeCaseToToggleCase", {});
+
+    // This was "lOREM IPSUM..." (missing redlining)
+    CPPUNIT_ASSERT_EQUAL(OUString("Lorem ipsum...lOREM IPSUM..."), pTextNode->GetText());
+    CPPUNIT_ASSERT_EQUAL(OUString("lOREM IPSUM..."), pTextNode->GetRedlineText());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf129655)
 {
     createDoc("tdf129655-vtextbox.odt");

commit 6f44bb53b0381a1665feb9dbe9cbf3d777603586

tdf#140731: sw transliteration: avoid too many redlines
As a workaround for the performance regression
from commit 1352b20c0dead3736964bcca8485e9f0d403cadf
(tdf#109266 sw change tracking: track transliteration),
switch off redlining to avoid ~freezing, if a single
transliteration could result too many (>~500) redlines.

A single transliteration creates n redlines
for n paragraphs of the selected text, except in
the case of transliterating to title case, where it
creates n redlines for n words. It's very easy
to freeze Writer, because Writer's slowing down with
n redlines is described by an O(n²) (quadratic) time
complexity. Eg. in an experiment, title casing
~660 words was 6 sec, but ~3000 words was 85 sec,
regarding to creating 660 vs 3000 redlines.

Note: this is a partial revert of commit
1352b20c0dead3736964bcca8485e9f0d403cadf, if the
selection contains more than 500 paragraphs (or in the
case transliterating to title case, ~500 words).

Change-Id: Iad98943cc9e1ed64aa9779e49ee3e941abad02ac
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111637
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/inc/ndtxt.hxx
@@ -746,7 +746,7 @@ public:
     /// change text to Upper/Lower/Hiragana/Katakana/...
     void TransliterateText( utl::TransliterationWrapper& rTrans,
                             sal_Int32 nStart, sal_Int32 nEnd,
-                            SwUndoTransliterate* pUndo );
+                            SwUndoTransliterate* pUndo, bool bUseRedlining = false );
 
     /// count words in given range - returns true if we refreshed out count
     bool CountWords( SwDocStat& rStat, sal_Int32 nStart, sal_Int32 nEnd ) const;

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -74,6 +74,8 @@
 #include <sal/log.hxx>
 #include <unotools/charclass.hxx>
 #include <unotools/configmgr.hxx>
+#include <unotools/transliterationwrapper.hxx>
+#include <i18nutil/transliteration.hxx>
 #include <sfx2/Metadatable.hxx>
 #include <sot/exchange.hxx>
 #include <svl/stritem.hxx>
@@ -2821,6 +2823,44 @@ void DocumentContentOperationsManager::TransliterateText(
         }
     }
 
+    bool bUseRedlining = m_rDoc.getIDocumentRedlineAccess().IsRedlineOn();
+    // as a workaround for a known performance problem, switch off redlining
+    // to avoid freezing, if transliteration could result too many redlines
+    if ( bUseRedlining )
+    {
+        const sal_uLong nMaxRedlines = 500;
+        const bool bIsTitleCase = rTrans.getType() == TransliterationFlags::TITLE_CASE;
+        sal_uLong nAffectedNodes = 0;
+        sal_uLong nAffectedChars = nEndCnt;
+        SwNodeIndex aIdx( pStt->nNode );
+        for( ; aIdx.GetIndex() <= nEndNd; ++aIdx )
+        {
+            SwTextNode* pAffectedNode = aIdx.GetNode().GetTextNode();
+
+            // don't count not text nodes or empty text nodes
+            if( !pAffectedNode || pAffectedNode->GetText().isEmpty() )
+                continue;
+
+            nAffectedNodes++;
+
+            // count characters of the node (the last - maybe partially
+            // selected - node was counted at initialization of nAffectedChars)
+            if( aIdx.GetIndex() < nEndNd )
+                nAffectedChars += pAffectedNode->GetText().getLength();
+
+            // transliteration creates n redlines for n nodes, except in the
+            // case of title case, where it creates n redlines for n words
+            if( nAffectedNodes > nMaxRedlines ||
+                    // estimate word count based on the character count, where
+                    // 6 = average English word length is ~5 letters + space
+                    ( bIsTitleCase && (nAffectedChars - nSttCnt)/6 > nMaxRedlines ) )
+            {
+                bUseRedlining = false;
+                break;
+            }
+        }
+    }
+
     if( nSttNd != nEndNd )  // is more than one text node involved?
     {
         // iterate over all effected text nodes, the first and the last one
@@ -2831,8 +2871,10 @@ void DocumentContentOperationsManager::TransliterateText(
         {
             ++aIdx;
             if( pTNd )
+            {
                 pTNd->TransliterateText(
-                        rTrans, nSttCnt, pTNd->GetText().getLength(), pUndo.get());
+                        rTrans, nSttCnt, pTNd->GetText().getLength(), pUndo.get(), bUseRedlining);
+            }
         }
 
         for( ; aIdx.GetIndex() < nEndNd; ++aIdx )
@@ -2841,16 +2883,19 @@ void DocumentContentOperationsManager::TransliterateText(
             if (pTNd)
             {
                 pTNd->TransliterateText(
-                        rTrans, 0, pTNd->GetText().getLength(), pUndo.get());
+                        rTrans, 0, pTNd->GetText().getLength(), pUndo.get(), bUseRedlining);
             }
         }
 
         if( nEndCnt && nullptr != ( pTNd = pEnd->nNode.GetNode().GetTextNode() ))
-            pTNd->TransliterateText( rTrans, 0, nEndCnt, pUndo.get() );
+        {
+            pTNd->TransliterateText( rTrans, 0, nEndCnt, pUndo.get(), bUseRedlining );
+        }
     }
     else if( pTNd && nSttCnt < nEndCnt )
-        pTNd->TransliterateText( rTrans, nSttCnt, nEndCnt, pUndo.get() );
-
+    {
+        pTNd->TransliterateText( rTrans, nSttCnt, nEndCnt, pUndo.get(), bUseRedlining );
+    }
     if( pUndo && pUndo->HasData() )
     {
         m_rDoc.GetIDocumentUndoRedo().AppendUndo(std::move(pUndo));

sw/source/core/txtnode/txtedt.cxx
@@ -1684,7 +1684,7 @@ namespace
 void SwTextNode::TransliterateText(
     utl::TransliterationWrapper& rTrans,
     sal_Int32 nStt, sal_Int32 nEnd,
-    SwUndoTransliterate* pUndo )
+    SwUndoTransliterate* pUndo, bool bUseRedlining )
 {
     if (nStt >= nEnd)
         return;
@@ -1908,7 +1908,7 @@ void SwTextNode::TransliterateText(
     // now apply the changes from end to start to leave the offsets of the
     // yet unchanged text parts remain the same.
     size_t nSum(0);
-    bool bIsRedlineOn(GetDoc().getIDocumentRedlineAccess().IsRedlineOn());
+
     for (size_t i = 0; i < aChanges.size(); ++i)
     {   // check this here since AddChanges cannot be moved below
         // call to ReplaceTextOnly
@@ -1922,7 +1922,7 @@ void SwTextNode::TransliterateText(
             return;
         }
 
-        if ( bIsRedlineOn )
+        if ( bUseRedlining )
         {
             // create SwPaM with mark & point spanning the attributed text
             //SwPaM aCurPaM( *this, *this, nBegin, nBegin + nLen ); <-- wrong c-tor, does sth different