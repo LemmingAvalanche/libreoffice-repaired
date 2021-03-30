LemmingAvalanche/libreoffice-repaired

commit 22f4e0de29699d0b0a038a710f6f803ac2fa2da0

tdf#121723 vcl: leave popup mode on focus loss of toplevel windows
This way it can't happen that we open a menu or context menu, the user
switches away (loosing focus) and a leftover floating window is still
there.

Handle this at the same place where we stop blinking the cursor on focus
loss.

Change-Id: I4321e8e3fa1d3f8976c0a1fa29c6a182972023bf
Reviewed-on: https://gerrit.libreoffice.org/64053
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/window/winproc.cxx
@@ -1842,6 +1842,11 @@ static void ImplHandleLoseFocus( vcl::Window* pWindow )
     vcl::Window* pFocusWin = pWindow->ImplGetWindowImpl()->mpFrameData->mpFocusWin;
     if ( pFocusWin && pFocusWin->ImplGetWindowImpl()->mpCursor )
         pFocusWin->ImplGetWindowImpl()->mpCursor->ImplHide();
+
+    // Make sure that no menu is visible when a toplevel window loses focus.
+    VclPtr<FloatingWindow> pFirstFloat = pSVData->maWinData.mpFirstFloat;
+    if (pFirstFloat && !pWindow->GetParent())
+        pFirstFloat->EndPopupMode(FloatWinPopupEndFlags::Cancel | FloatWinPopupEndFlags::CloseAll);
 }
 
 struct DelayedCloseEvent

commit 330fd0b1415270ab85c627a75126711c786d0e15

tdf#133350 vcl focus loss: fix unwanted cancel of non-menu popup
Regression from commit 22f4e0de29699d0b0a038a710f6f803ac2fa2da0
(tdf#121723 vcl: leave popup mode on focus loss of toplevel windows,
2018-11-26), the original motivation was to cancel menu popus (toplevel
window menus, context menus).

Fix the unwanted cancel of ImplPopupFloatWin/ImplListBoxFloatingWindow
by restricting the cancel to menu floating windows. This keeps the
original bug fixed and allows setting e.g. the font size in the
toolbar's popup part.

Change-Id: Ida503d0c13dc5a96287bfa31b6c2ce9d4a53e940
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113326
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

vcl/source/window/winproc.cxx
@@ -1898,8 +1898,10 @@ static void ImplHandleLoseFocus( vcl::Window* pWindow )
 
     // Make sure that no menu is visible when a toplevel window loses focus.
     VclPtr<FloatingWindow> pFirstFloat = pSVData->mpWinData->mpFirstFloat;
-    if (pFirstFloat && !pWindow->GetParent())
+    if (pFirstFloat && pFirstFloat->IsMenuFloatingWindow() && !pWindow->GetParent())
+    {
         pFirstFloat->EndPopupMode(FloatWinPopupEndFlags::Cancel | FloatWinPopupEndFlags::CloseAll);
+    }
 }
 
 namespace {