LemmingAvalanche/libreoffice-repaired

commit 85350dcc23f4b12fd66c861017192af8974f2d0c

PPTX export: fix missing audio anim node for slide narrations
Once the audio node is there, it'll be possible to specify various
custom properties on it in follow-up commits.

Change-Id: I8c00de27de483687eaf76d1661baf59c52711246
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109920
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sd/source/filter/eppt/pptx-animations.cxx
@@ -54,6 +54,7 @@
 #include <com/sun/star/presentation/TextAnimationType.hpp>
 #include <com/sun/star/text/XSimpleText.hpp>
 #include <com/sun/star/drawing/XShape.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
 #include <oox/export/utils.hxx>
 #include <oox/ppt/pptfilterhelpers.hxx>
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
@@ -652,6 +653,12 @@ public:
     PPTXAnimationExport(PowerPointExport& rExport, const FSHelperPtr& pFS);
     void WriteAnimations(const Reference<XDrawPage>& rXDrawPage);
 };
+
+/// Returns if rURL has an extension which is an audio format.
+bool IsAudioURL(const OUString& rURL)
+{
+    return rURL.endsWithIgnoreAsciiCase(".wav") || rURL.endsWithIgnoreAsciiCase(".m4a");
+}
 }
 
 namespace oox::core
@@ -1193,14 +1200,40 @@ void PPTXAnimationExport::WriteAnimationNodeAudio()
     Reference<XAudio> xAudio(getCurrentNode(), UNO_QUERY);
 
     OUString sUrl;
+    uno::Reference<drawing::XShape> xShape;
     OUString sRelId;
     OUString sName;
 
-    if (!(xAudio.is() && (xAudio->getSource() >>= sUrl) && !sUrl.isEmpty()
-          && sUrl.endsWithIgnoreAsciiCase(".wav")))
+    if (!xAudio.is())
+    {
+        return;
+    }
+
+    bool bValid = false;
+    if ((xAudio->getSource() >>= sUrl) && !sUrl.isEmpty() && IsAudioURL(sUrl))
+    {
+        bValid = true;
+    }
+
+    if (!bValid)
+    {
+        if (xAudio->getSource() >>= xShape)
+        {
+            uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+            if (xShapeProps->getPropertyValue("MediaURL") >>= sUrl)
+            {
+                bValid = IsAudioURL(sUrl);
+            }
+        }
+    }
+
+    if (!bValid)
         return;
 
-    mrPowerPointExport.embedEffectAudio(mpFS, sUrl, sRelId, sName);
+    if (!xShape.is())
+    {
+        mrPowerPointExport.embedEffectAudio(mpFS, sUrl, sRelId, sName);
+    }
 
     mpFS->startElementNS(XML_p, XML_audio);
     mpFS->startElementNS(XML_p, XML_cMediaNode);
@@ -1211,9 +1244,17 @@ void PPTXAnimationExport::WriteAnimationNodeAudio()
     mpFS->endElementNS(XML_p, XML_cTn);
 
     mpFS->startElementNS(XML_p, XML_tgtEl);
-    mpFS->singleElementNS(XML_p, XML_sndTgt, FSNS(XML_r, XML_embed),
-                          sax_fastparser::UseIf(sRelId, !sRelId.isEmpty()), XML_name,
-                          sax_fastparser::UseIf(sName, !sUrl.isEmpty()));
+    if (xShape.is())
+    {
+        sal_Int32 nShapeID = mrPowerPointExport.GetShapeID(xShape);
+        mpFS->singleElementNS(XML_p, XML_spTgt, XML_spid, OString::number(nShapeID));
+    }
+    else
+    {
+        mpFS->singleElementNS(XML_p, XML_sndTgt, FSNS(XML_r, XML_embed),
+                              sax_fastparser::UseIf(sRelId, !sRelId.isEmpty()), XML_name,
+                              sax_fastparser::UseIf(sName, !sUrl.isEmpty()));
+    }
     mpFS->endElementNS(XML_p, XML_tgtEl);
 
     mpFS->endElementNS(XML_p, XML_cMediaNode);
@@ -1377,8 +1418,23 @@ void NodeContext::initValid(bool bHasValidChild, bool bIsIterateChild)
     {
         Reference<XAudio> xAudio(mxNode, UNO_QUERY);
         OUString sURL;
-        mbValid
-            = xAudio.is() && (xAudio->getSource() >>= sURL) && sURL.endsWithIgnoreAsciiCase(".wav");
+        uno::Reference<drawing::XShape> xShape;
+        mbValid = false;
+        if (xAudio.is())
+        {
+            if (xAudio->getSource() >>= sURL)
+            {
+                mbValid = IsAudioURL(sURL);
+            }
+            else if (xAudio->getSource() >>= xShape)
+            {
+                uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+                if (xShapeProps->getPropertyValue("MediaURL") >>= sURL)
+                {
+                    mbValid = IsAudioURL(sURL);
+                }
+            }
+        }
     }
     else
     {

sd/qa/unit/export-tests-ooxml1.cxx
@@ -1347,6 +1347,12 @@ void SdOOXMLExportTest1::testNarrationMimeType()
     // i.e. '<p:cmd type="call">' was written instead of '<p:cmd type="call" cmd="playFrom(0.0)">'.
     assertXPath(pSlideDoc, "//p:cmd", "cmd", "playFrom(0.0)");
 
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 0
+    // i.e. <p:childTnLst> had no <p:audio> children, the whole audio animation node was lost.
+    assertXPath(pSlideDoc, "//p:childTnLst/p:audio/p:cMediaNode", 1);
+
     xDocShRef->DoClose();
 }
 

commit fe83daa7f5f001d5af6ad687958dfcb895c14c00

tdf#141267 PPTX export: fix handling of audio nodes with non-media source
Regression from commit 85350dcc23f4b12fd66c861017192af8974f2d0c (PPTX
export: fix missing audio anim node for slide narrations, 2021-01-25),
the problem was that the audio source can be any kind of shape, not only
a media shape.

Fix the problem by explicitly checking of the shape has a media URL.

Change-Id: I2bdde0f9ed3576f307aabff5e39d441f295c203f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113715
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sd/source/filter/eppt/pptx-animations.cxx
@@ -1220,7 +1220,8 @@ void PPTXAnimationExport::WriteAnimationNodeAudio()
         if (xAudio->getSource() >>= xShape)
         {
             uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
-            if (xShapeProps->getPropertyValue("MediaURL") >>= sUrl)
+            bool bHasMediaURL = xShapeProps->getPropertySetInfo()->hasPropertyByName("MediaURL");
+            if (bHasMediaURL && (xShapeProps->getPropertyValue("MediaURL") >>= sUrl))
             {
                 bValid = IsAudioURL(sUrl);
             }
@@ -1431,7 +1432,9 @@ void NodeContext::initValid(bool bHasValidChild, bool bIsIterateChild)
             else if (xAudio->getSource() >>= xShape)
             {
                 uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
-                if (xShapeProps->getPropertyValue("MediaURL") >>= sURL)
+                bool bHasMediaURL
+                    = xShapeProps->getPropertySetInfo()->hasPropertyByName("MediaURL");
+                if (bHasMediaURL && (xShapeProps->getPropertyValue("MediaURL") >>= sURL))
                 {
                     mbValid = IsAudioURL(sURL);
                 }

sd/qa/unit/export-tests-ooxml1.cxx
@@ -98,6 +98,7 @@ public:
     void testRoundtripOwnLineStyles();
     void testRoundtripPrstDash();
     void testDashOnHairline();
+    void testNarrationNonMediaShape();
     void testCustomshapeBitmapfillSrcrect();
     void testTdf100348FontworkBitmapFill();
     void testTdf100348FontworkGradientGlow();
@@ -150,6 +151,7 @@ public:
     CPPUNIT_TEST(testRoundtripOwnLineStyles);
     CPPUNIT_TEST(testRoundtripPrstDash);
     CPPUNIT_TEST(testDashOnHairline);
+    CPPUNIT_TEST(testNarrationNonMediaShape);
     CPPUNIT_TEST(testCustomshapeBitmapfillSrcrect);
     CPPUNIT_TEST(testTdf100348FontworkBitmapFill);
     CPPUNIT_TEST(testTdf100348FontworkGradientGlow);
@@ -1142,6 +1144,17 @@ void SdOOXMLExportTest1::testDashOnHairline()
 
     xmlDocUniquePtr pXmlDoc = parseExport(tempFile, "ppt/slides/slide1.xml");
     assertXPath(pXmlDoc, "/p:sld/p:cSld/p:spTree/p:sp/p:spPr/a:ln/a:custDash/a:ds", 11);
+}
+
+void SdOOXMLExportTest1::testNarrationNonMediaShape()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(
+        m_directories.getURLFromSrc(u"sd/qa/unit/data/pptx/narration-non-media-shape.pptx"), PPTX);
+    utl::TempFile aTempFile;
+    // Without the accompanying fix in place, this test would have failed,
+    // beans::UnknownPropertyException was thrown.
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &aTempFile);
+    xDocShRef->DoClose();
 }
 
 void SdOOXMLExportTest1::testCustomshapeBitmapfillSrcrect()