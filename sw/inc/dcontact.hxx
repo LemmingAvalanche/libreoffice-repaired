LemmingAvalanche/libreoffice-repaired

commit 2ebf47e66698930bf637dfe531be8ad42b704f96

(related: tdf#133487) sw: fix ordering of virtual SdrObjects for textboxes
Calling XShapes3::sort() on export of the testTdf130314 fails because of
2 consecutive textboxes; the function requires a textbox to immediately
follow its shape in the list (i.e. textbox has OrdNum of shape + 1).

This is because for shapes in header/footer, one virtual SdrVirtObj is
created per page where the header/footer is shown, and the
SwFlyDrawContact::GetOrdNumForNewRef() does not take textbox ordering
into account.

It's not clear if the assumption that the shape's SdrVirtObj is created
before the textbox's always holds, but let's try this for now.

Change-Id: I860896471211bf6c142ab825f298f4d4c0eec148
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112029
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/inc/dcontact.hxx
@@ -179,13 +179,13 @@ private:
 
     SwFlyDrawObjPtr mpMasterObj;
     void SwClientNotify(const SwModify&, const SfxHint& rHint) override;
-    sal_uInt32 GetOrdNumForNewRef(const SwFlyFrame* pFly);
+    sal_uInt32 GetOrdNumForNewRef(const SwFlyFrame* pFly, SwFrame const& rAnchorFrame);
 
 public:
 
     /// Creates DrawObject and registers it with the Model.
     SwFlyDrawContact(SwFlyFrameFormat* pToRegisterIn, SdrModel& rTargetModel);
-    static SwVirtFlyDrawObj* CreateNewRef(SwFlyFrame* pFly, SwFlyFrameFormat* pFormat);
+    static SwVirtFlyDrawObj* CreateNewRef(SwFlyFrame* pFly, SwFlyFrameFormat* pFormat, SwFrame const& rAnchorFrame);
     virtual ~SwFlyDrawContact() override;
 
     virtual const SwAnchoredObject* GetAnchoredObj( const SdrObject* _pSdrObj ) const override;

sw/source/core/draw/dcontact.cxx
@@ -478,8 +478,28 @@ SwFlyDrawContact::~SwFlyDrawContact()
     }
 }
 
-sal_uInt32 SwFlyDrawContact::GetOrdNumForNewRef(const SwFlyFrame* pFly)
+sal_uInt32 SwFlyDrawContact::GetOrdNumForNewRef(const SwFlyFrame* pFly,
+        SwFrame const& rAnchorFrame)
 {
+    // maintain invariant that a shape's textbox immediately follows the shape
+    // also for the multiple SdrVirtObj created for shapes in header/footer
+    if (SwFrameFormat const*const pDrawFormat =
+            SwTextBoxHelper::getOtherTextBoxFormat(GetFormat(), RES_FLYFRMFMT))
+    {
+        // assume that the draw SdrVirtObj is always created before the flyframe one
+        if (SwSortedObjs const*const pObjs = rAnchorFrame.GetDrawObjs())
+        {
+            for (SwAnchoredObject const*const pAnchoredObj : *pObjs)
+            {
+                if (&pAnchoredObj->GetFrameFormat() == pDrawFormat)
+                {
+                    return pAnchoredObj->GetDrawObj()->GetOrdNum() + 1;
+                }
+            }
+        }
+        // if called from AppendObjs(), this is a problem; if called from lcl_SetFlyFrameAttr() it's not
+        SAL_INFO("sw", "GetOrdNumForNewRef: cannot find SdrObject for text box's shape");
+    }
     // search for another Writer fly frame registered at same frame format
     SwIterator<SwFlyFrame,SwFormat> aIter(*GetFormat());
     const SwFlyFrame* pFlyFrame(nullptr);
@@ -501,7 +521,8 @@ sal_uInt32 SwFlyDrawContact::GetOrdNumForNewRef(const SwFlyFrame* pFly)
     return GetMaster()->GetOrdNumDirect();
 }
 
-SwVirtFlyDrawObj* SwFlyDrawContact::CreateNewRef(SwFlyFrame* pFly, SwFlyFrameFormat* pFormat)
+SwVirtFlyDrawObj* SwFlyDrawContact::CreateNewRef(SwFlyFrame* pFly,
+        SwFlyFrameFormat* pFormat, SwFrame const& rAnchorFrame)
 {
     // Find ContactObject from the Format. If there's already one, we just
     // need to create a new Ref, else we create the Contact now.
@@ -528,7 +549,7 @@ SwVirtFlyDrawObj* SwFlyDrawContact::CreateNewRef(SwFlyFrame* pFly, SwFlyFrameFor
     // #i27030# - insert new <SwVirtFlyDrawObj> instance
     // into drawing page with correct order number
     else
-        rIDDMA.GetDrawModel()->GetPage(0)->InsertObject(pDrawObj, pContact->GetOrdNumForNewRef(pFly));
+        rIDDMA.GetDrawModel()->GetPage(0)->InsertObject(pDrawObj, pContact->GetOrdNumForNewRef(pFly, rAnchorFrame));
     // #i38889# - assure, that new <SwVirtFlyDrawObj> instance
     // is in a visible layer.
     pContact->MoveObjToVisibleLayer(pDrawObj);

sw/qa/extras/mailmerge/mailmerge.cxx
@@ -396,7 +396,7 @@ DECLARE_FILE_MAILMERGE_TEST(testMissingDefaultLineColor, "missing-default-line-c
     executeMailMerge();
     // The document was created by LO version which didn't write out the default value for line color
     // (see XMLGraphicsDefaultStyle::SetDefaults()).
-    uno::Reference<beans::XPropertySet> xPropertySet(getShape(4), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xPropertySet(getShape(5), uno::UNO_QUERY);
     // Lines do not have a line color.
     CPPUNIT_ASSERT( !xPropertySet->getPropertySetInfo()->hasPropertyByName( "LineColor" ));
     SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument *>(mxComponent.get());

sw/source/core/inc/flyfrm.hxx
@@ -64,7 +64,7 @@ class SW_DLLPUBLIC SwFlyFrame : public SwLayoutFrame, public SwAnchoredObject
     friend void Notify( SwFlyFrame *, SwPageFrame *pOld, const SwRect &rOld,
                         const SwRect* pOldPrt );
 
-    void InitDrawObj();     // these to methods are called in the
+    void InitDrawObj(SwFrame const&); // these to methods are called in the
     void FinitDrawObj();    // constructors
 
     void UpdateAttr_( const SfxPoolItem*, const SfxPoolItem*, sal_uInt8 &,

sw/source/core/layout/fly.cxx
@@ -161,7 +161,7 @@ SwFlyFrame::SwFlyFrame( SwFlyFrameFormat *pFormat, SwFrame* pSib, SwFrame *pAnch
     // First the Init, then the Content:
     // This is due to the fact that the Content may have Objects/Frames,
     // which are then registered
-    InitDrawObj();
+    InitDrawObj(*pAnch);
 
     Chain( pAnch );
 
@@ -352,9 +352,9 @@ void SwFlyFrame::DeleteCnt()
     InvalidatePage();
 }
 
-void SwFlyFrame::InitDrawObj()
+void SwFlyFrame::InitDrawObj(SwFrame const& rAnchorFrame)
 {
-    SetDrawObj(*SwFlyDrawContact::CreateNewRef(this, GetFormat()));
+    SetDrawObj(*SwFlyDrawContact::CreateNewRef(this, GetFormat(), rAnchorFrame));
 
     // Set the right Layer
     IDocumentDrawModelAccess& rIDDMA = GetFormat()->getIDocumentDrawModelAccess();

commit 8d5cebc6dca6dda7d892b348b5edd8ab4d1aef9f

tdf#141549 sw: fix ordering of virtual SdrObjects for textboxes
In header/footer it turns out that not only can the text box be inserted
after the shape, it can also be inserted before the shape.

This is because SwDrawFrameFormat::MakeFrames() ends up first destroying
all the SwDrawVirtObj and then creating them from scratch again, while
the SwVirtFlyDrawObj survives.

4  SwDrawContact::DisconnectFromLayout(bool) (this=0x6ed0200, _bMoveMasterToInvisibleLayer=false) at sw/source/core/draw/dcontact.cxx:1664
5  SwDrawContact::ConnectToLayout(SwFormatAnchor const*) (this=0x6ed0200, pAnch=0x0) at sw/source/core/draw/dcontact.cxx:1791
6  SwDrawContact::SwClientNotify(SwModify const&, SfxHint const&) (this=0x6ed0200, rMod=..., rHint=...) at sw/source/core/draw/dcontact.cxx:1521
7  SwModify::CallSwClientNotify(SfxHint const&) const (this=0x6ecfd80, rHint=...) at sw/source/core/attr/calbck.cxx:321
8  sw::BroadcastingModify::CallSwClientNotify(SfxHint const&) const (this=0x6ecfd80, rHint=...) at sw/source/core/attr/calbck.cxx:326
9  SwDrawFrameFormat::MakeFrames() (this=0x6ecfd80) at sw/source/core/layout/atrfrm.cxx:3394
10 AppendAllObjs(SwFrameFormats const*, SwFrame const*) (pTable=0x69c1920, pSib=0x38c6100) at sw/source/core/layout/frmtool.cxx:1349
11 InsertCnt_(SwLayoutFrame*, SwDoc*, unsigned long, bool, unsigned long, SwFrame*, sw::FrameMode) (pLay=0x6be4320, pDoc=0x69af7c0, nIndex=157, bPages=true, nEndIndex=0, pPrv=0x6baedc0, eMode=sw::FrameMode::New) at sw/source/core/layout/frmtool.cxx:1927

Fix this by searching for the existing SwVirtFlyDrawObj in
SwDrawVirtObj::AddToDrawingPage().

(regression from 2ebf47e66698930bf637dfe531be8ad42b704f96)

Change-Id: Ic00878685ab16018c9331190f1e80f874c1a9a4e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113844
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/inc/dcontact.hxx
@@ -251,7 +251,7 @@ class SwDrawVirtObj final : public SdrVirtObj
         void RemoveFromWriterLayout();
 
         /// connection to drawing layer
-        void AddToDrawingPage();
+        void AddToDrawingPage(SwFrame const& rAnchorFrame);
         void RemoveFromDrawingPage();
 
         /** is 'virtual' drawing object connected to writer layout and
@@ -401,7 +401,7 @@ class SAL_DLLPUBLIC_RTTI SwDrawContact final : public SwContact
         /// Moves all SW-connections to new Master)
         void ChangeMasterObject( SdrObject *pNewMaster );
 
-        SwDrawVirtObj* AddVirtObj();
+        SwDrawVirtObj* AddVirtObj(SwFrame const& rAnchorFrame);
 
         void NotifyBackgrdOfAllVirtObjs( const tools::Rectangle* pOldBoundRect );
 

sw/source/core/draw/dcontact.cxx
@@ -827,7 +827,7 @@ SwFrame* SwDrawContact::GetAnchorFrame(SdrObject const *const pDrawObj)
 
 /** add a 'virtual' drawing object to drawing page.
  */
-SwDrawVirtObj* SwDrawContact::AddVirtObj()
+SwDrawVirtObj* SwDrawContact::AddVirtObj(SwFrame const& rAnchorFrame)
 {
     maDrawVirtObjs.push_back(
         SwDrawVirtObjPtr(
@@ -835,7 +835,7 @@ SwDrawVirtObj* SwDrawContact::AddVirtObj()
                 GetMaster()->getSdrModelFromSdrObject(),
                 *GetMaster(),
                 *this)));
-    maDrawVirtObjs.back()->AddToDrawingPage();
+    maDrawVirtObjs.back()->AddToDrawingPage(rAnchorFrame);
     return maDrawVirtObjs.back().get();
 }
 
@@ -1926,7 +1926,7 @@ void SwDrawContact::ConnectToLayout( const SwFormatAnchor* pAnch )
                         else
                         {
                             // append 'virtual' drawing object
-                            SwDrawVirtObj* pDrawVirtObj = AddVirtObj();
+                            SwDrawVirtObj* pDrawVirtObj = AddVirtObj(*pFrame);
                             if ( pAnch->GetAnchorId() == RndStdIds::FLY_AS_CHAR )
                             {
                                 ClrContourCache( pDrawVirtObj );
@@ -2274,30 +2274,56 @@ void SwDrawVirtObj::RemoveFromWriterLayout()
     }
 }
 
-void SwDrawVirtObj::AddToDrawingPage()
+void SwDrawVirtObj::AddToDrawingPage(SwFrame const& rAnchorFrame)
 {
     // determine 'master'
     SdrObject* pOrgMasterSdrObj = mrDrawContact.GetMaster();
 
     // insert 'virtual' drawing object into page, set layer and user call.
     SdrPage* pDrawPg = pOrgMasterSdrObj->getSdrPageFromSdrObject();
+    // default: insert before master object
+    auto NOTM_nOrdNum(GetReferencedObj().GetOrdNum());
+
+    // maintain invariant that a shape's textbox immediately follows the shape
+    // also for the multiple SdrDrawVirtObj created for shapes in header/footer
+    if (SwFrameFormat const*const pFlyFormat =
+            SwTextBoxHelper::getOtherTextBoxFormat(mrDrawContact.GetFormat(), RES_DRAWFRMFMT))
+    {
+        // this is for the case when the flyframe SdrVirtObj is created before the draw one
+        if (SwSortedObjs const*const pObjs = rAnchorFrame.GetDrawObjs())
+        {
+            for (SwAnchoredObject const*const pAnchoredObj : *pObjs)
+            {
+                if (&pAnchoredObj->GetFrameFormat() == pFlyFormat)
+                {
+                    assert(dynamic_cast<SwFlyFrame const*>(pAnchoredObj));
+                    NOTM_nOrdNum = pAnchoredObj->GetDrawObj()->GetOrdNum();
+                    // the master SdrObj should have the highest index
+                    assert(NOTM_nOrdNum < GetReferencedObj().GetOrdNum());
+                    break;
+                }
+            }
+        }
+        // this happens on initial insertion, the draw object is created first
+        SAL_INFO_IF(GetReferencedObj().GetOrdNum() == NOTM_nOrdNum, "sw", "AddToDrawingPage: cannot find SdrObject for text box's shape");
+    }
+
     // #i27030# - apply order number of referenced object
     if ( nullptr != pDrawPg )
     {
         // #i27030# - apply order number of referenced object
-        pDrawPg->InsertObject( this, GetReferencedObj().GetOrdNum() );
+        pDrawPg->InsertObject(this, NOTM_nOrdNum);
     }
     else
     {
         pDrawPg = getSdrPageFromSdrObject();
         if ( pDrawPg )
         {
-            pDrawPg->SetObjectOrdNum( GetOrdNumDirect(),
-                                      GetReferencedObj().GetOrdNum() );
+            pDrawPg->SetObjectOrdNum(GetOrdNumDirect(), NOTM_nOrdNum);
         }
         else
         {
-            SetOrdNum( GetReferencedObj().GetOrdNum() );
+            SetOrdNum(NOTM_nOrdNum);
         }
     }
     SetUserCall( &mrDrawContact );

sw/source/core/layout/frmtool.cxx
@@ -1027,7 +1027,7 @@ void AppendObj(SwFrame *const pFrame, SwPageFrame *const pPage, SwFrameFormat *c
                           pNew->GetAnchorFrame() != pFrame &&
                           !pNew->GetDrawObjectByAnchorFrame( *pFrame ) )
                 {
-                    SwDrawVirtObj* pDrawVirtObj = pNew->AddVirtObj();
+                    SwDrawVirtObj* pDrawVirtObj = pNew->AddVirtObj(*pFrame);
                     pFrame->AppendDrawObj( *(pNew->GetAnchoredObj( pDrawVirtObj )) );
 
                     pDrawVirtObj->ActionChanged();