LemmingAvalanche/libreoffice-repaired

commit 4b6db8e3cde3907055824e280a86d0f2b8058cbc

ref-count SdrPage
which simplifies ownership handling, particularly with regard to
undo/redo

Change-Id: Ie8f300ebfdae6db3c06f78a87e35a07497d6b825
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111991
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/dialog/dlgctl3d.cxx
@@ -45,8 +45,7 @@
 using namespace com::sun::star;
 
 Svx3DPreviewControl::Svx3DPreviewControl()
-    : mpFmPage(nullptr)
-    , mpScene(nullptr)
+    : mpScene(nullptr)
     , mp3DObj(nullptr)
     , mnObjectType(SvxPreviewObjectType::SPHERE)
 {
@@ -81,8 +80,8 @@ void Svx3DPreviewControl::Construct()
     mpModel->GetItemPool().FreezeIdRanges();
 
     // Page
-    mpFmPage = new FmFormPage( *mpModel );
-    mpModel->InsertPage( mpFmPage, 0 );
+    mxFmPage = new FmFormPage( *mpModel );
+    mpModel->InsertPage( mxFmPage.get(), 0 );
 
     // 3D View
     mp3DView.reset(new E3dView(*mpModel, &rDevice));
@@ -112,7 +111,7 @@ void Svx3DPreviewControl::Construct()
     rCamera.SetFocalLength(fDefaultCamFocal);
 
     mpScene->SetCamera( rCamera );
-    mpFmPage->InsertObject( mpScene );
+    mxFmPage->InsertObject( mpScene );
 
     basegfx::B3DHomMatrix aRotation;
     aRotation.rotate(DEG2RAD( 25 ), 0.0, 0.0);
@@ -132,7 +131,7 @@ void Svx3DPreviewControl::Construct()
     mpScene->SetMergedItemSet(aSet);
 
     // PageView
-    SdrPageView* pPageView = mp3DView->ShowSdrPage( mpFmPage );
+    SdrPageView* pPageView = mp3DView->ShowSdrPage( mxFmPage.get() );
     mp3DView->hideMarkHandles();
 
     // mark scene
@@ -144,7 +143,7 @@ void Svx3DPreviewControl::Resize()
     // size of page
     Size aSize(GetOutputSizePixel());
     aSize = GetDrawingArea()->get_ref_device().PixelToLogic(aSize);
-    mpFmPage->SetSize(aSize);
+    mxFmPage->SetSize(aSize);
 
     // set size
     Size aObjSize( aSize.Width()*5/6, aSize.Height()*5/6 );
@@ -685,7 +684,7 @@ void Svx3DLightControl::Resize()
 {
     // set size of page
     const Size aSize(GetDrawingArea()->get_ref_device().PixelToLogic(GetOutputSizePixel()));
-    mpFmPage->SetSize(aSize);
+    mxFmPage->SetSize(aSize);
 
     // set position and size of scene
     mpScene->SetSnapRect(tools::Rectangle(Point(0, 0), aSize));

commit 71e483142179820996e8fce68f94ee6eb58ebef9

tdf#141701 Crash when closing 3D View dialog of 3D chart
regression from
    commit 4b6db8e3cde3907055824e280a86d0f2b8058cbc
    ref-count SdrPage
we need to clear the SdrPage which releases some objects
or some unhelpful DBG_UTIL mode code in ~SdrObject will
kick in and try and release objects, but it ends
up deleting child objects before parent objects,
which cau

Change-Id: Iaad89a05a30c119807beacc54aaa66b6fde634e3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114223
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/dialog/dlgctl3d.cxx
@@ -63,6 +63,7 @@ void Svx3DPreviewControl::SetDrawingArea(weld::DrawingArea* pDrawingArea)
 
 Svx3DPreviewControl::~Svx3DPreviewControl()
 {
+    mxFmPage.clear();
     mp3DView.reset();
     mpModel.reset();
 }