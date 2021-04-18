LemmingAvalanche/libreoffice-repaired

commit 2908149e451766c67aca3ebfc0debf1cc95d2eb2

Revert "Revert "add some libs to libmerged""
This reverts commit c49c8ec3da46b264d0e990ae9a5bde29501f4bd1.

Reason for revert: 

Change-Id: I09b7d548a674fc8b2991be00616a7e95ec7d1444
sberg:You can probably reverts this again, as <https://ci.libreoffice.org/job/lo_daily_tb_win/162/> now succeeded with <https://gerrit.libreoffice.org/plugins/gitiles/core/+/15a316ecf6fef5c02ff7490408023dd8a93b8a6e%5E!/> "Properly use createOneInstanceComponentFactory for javaloader".
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90275
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

solenv/gbuild/extensions/pre_MergedLibsList.mk
@@ -83,13 +83,24 @@ MERGE_LIBRARY_LIST := \
 	svx \
 	svxcore \
 	syssh \
+	textfd \
 	tk \
 	tl \
 	ucb1 \
 	ucbhelper \
 	ucpexpand1 \
+	ucpext \
+	ucphier1 \
+	ucpimage \
 	ucpfile1 \
+	ucptdoc1 \
+	unordf \
 	unoxml \
+	$(if $(ENABLE_ONLINE_UPDATE), \
+		updatecheckui \
+        updchk \
+	) \
+	updatefeed \
 	utl \
 	uui \
 	vcl \

commit 91896dbcbc2f52ac98aa81390dd541ed7be1e7bb

tdf#132901: do not add updatecheck to libmerged
LibreOffice crashes if "online update"
feature is not installed

Regression from 2908149e451766c67aca3ebfc0debf1cc95d2eb2
< Revert "Revert "add some libs to libmerged"" >

Change-Id: Ia18f50bc6baf830b2d3e0814645ef04283bfaa04
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114247
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

solenv/gbuild/extensions/pre_MergedLibsList.mk
@@ -92,10 +92,6 @@ MERGE_LIBRARY_LIST := \
 	ucptdoc1 \
 	unordf \
 	unoxml \
-	$(if $(ENABLE_ONLINE_UPDATE), \
-		updatecheckui \
-        updchk \
-	) \
 	updatefeed \
 	utl \
 	uui \