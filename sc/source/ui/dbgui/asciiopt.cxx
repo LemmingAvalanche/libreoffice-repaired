LemmingAvalanche/libreoffice-repaired

commit 13cdb7f7f1a4538d685a37b4d52ed2900d31e43a

no need to create temporaries when appending number to O[U]StringBuffer
Change-Id: I36d82423b5f75010552696a66cec7e53ee265ce4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114395
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/dbgui/asciiopt.cxx
@@ -192,7 +192,7 @@ OUString ScAsciiOptions::WriteToString() const
         {
             if (i)
                 aOutStr.append("/");
-            aOutStr.append(OUString::number(aFieldSeps[i]));
+            aOutStr.append(aFieldSeps[i]);
         }
         if ( bMergeFieldSeps )
         {
@@ -202,7 +202,7 @@ OUString ScAsciiOptions::WriteToString() const
     }
 
     // Token 1: Text Quote character.
-    aOutStr.append(",").append(OUString::number(cTextSep)).append(",");
+    aOutStr.append(",").append(cTextSep).append(",");
 
     //Token 2: Text encoding.
     if ( bCharSetSystem )           // force "SYSTEM"
@@ -211,16 +211,16 @@ OUString ScAsciiOptions::WriteToString() const
         aOutStr.append(ScGlobal::GetCharsetString( eCharSet ));
 
     //Token 3: Number of start row.
-    aOutStr.append(",").append(OUString::number(nStartRow)).append(",");
+    aOutStr.append(",").append(nStartRow).append(",");
 
     //Token 4: Column info.
     for (size_t nInfo=0; nInfo<mvColStart.size(); nInfo++)
     {
         if (nInfo)
             aOutStr.append("/");
-        aOutStr.append(OUString::number(mvColStart[nInfo]))
+        aOutStr.append(mvColStart[nInfo])
                .append("/")
-               .append(OUString::number(mvColFormat[nInfo]));
+               .append(static_cast<sal_Int32>(mvColFormat[nInfo]));
     }
 
     // #i112025# the options string is used in macros and linked sheets,
@@ -228,7 +228,7 @@ OUString ScAsciiOptions::WriteToString() const
 
     aOutStr.append(",")
                //Token 5: Language
-               .append(OUString::number(static_cast<sal_uInt16>(eLang))).append(",")
+               .append(static_cast<sal_Int32>(static_cast<sal_uInt16>(eLang))).append(",")
                //Token 6: Import quoted field as text.
                .append(OUString::boolean( bQuotedFieldAsText )).append(",")
                //Token 7: Detect special numbers.

commit c7b9eb307eed3311ae5574675fe2cbac7de81d90

tdf#142040 CSV import broken in master
regression from
    commit 13cdb7f7f1a4538d685a37b4d52ed2900d31e43a
no need to create temporaries when appending number to
O[U]StringBuffer

Change-Id: Iacd74475ef7e9a70f60cedf80d3dfe4f1350f40c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115024
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/dbgui/asciiopt.cxx
@@ -192,7 +192,7 @@ OUString ScAsciiOptions::WriteToString() const
         {
             if (i)
                 aOutStr.append("/");
-            aOutStr.append(aFieldSeps[i]);
+            aOutStr.append(OUString::number(aFieldSeps[i]));
         }
         if ( bMergeFieldSeps )
         {
@@ -202,7 +202,7 @@ OUString ScAsciiOptions::WriteToString() const
     }
 
     // Token 1: Text Quote character.
-    aOutStr.append(",").append(cTextSep).append(",");
+    aOutStr.append("," + OUString::number(cTextSep) + ",");
 
     //Token 2: Text encoding.
     if ( bCharSetSystem )           // force "SYSTEM"