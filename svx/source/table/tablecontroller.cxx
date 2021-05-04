LemmingAvalanche/libreoffice-repaired

commit 421afd5264e4981fd3f80adcd512c6a1d2784e60

tdf#129961 svx: finish UI for table shadow as direct format
Normally properties on an SdrObject is set using SetAttributes(), but
that would take the selection controller into account, so we would call
SvxTableController::SetAttributes(), which sets the item set on the
selected cells instead. So use SetAttrToMarked() instead, which works on
the shape's item set, even in the table case. Don't replace all existing
items because we only have shadow properties here and also a disabled
shadow is still a (set) SdrOnOffItem (with value=false), so no old
SdrOnOffItem will be forgotten in the shape's item set.

Also add an outer undo grouping, so once the user presses OK in the
table properties dialog, we only create a single user-visible undo
action, not two.

Change-Id: I77b55ba1f07b8d0eeac5070e0ec07d39573d1f9a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107781
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

svx/source/table/tablecontroller.cxx
@@ -977,7 +977,26 @@ void SvxTableController::onFormatTable(const SfxRequest& rReq)
             if( aNewBoxItem.GetDistance( SvxBoxItemLine::BOTTOM ) != aBoxItem.GetDistance( SvxBoxItemLine::BOTTOM ) )
                 aNewSet.Put(makeSdrTextLowerDistItem( aNewBoxItem.GetDistance( SvxBoxItemLine::BOTTOM ) ) );
 
-            this->SetAttrToSelectedCells(aNewSet, false);
+            if (checkTableObject() && mxTable.is())
+            {
+                // Create a single undo action when applying the result of the dialog.
+                SdrTableObj& rTableObject(*mxTableObj);
+                SdrModel& rSdrModel(rTableObject.getSdrModelFromSdrObject());
+                bool bUndo = rSdrModel.IsUndoEnabled();
+                if (bUndo)
+                {
+                    rSdrModel.BegUndo(SvxResId(STR_TABLE_NUMFORMAT));
+                }
+
+                this->SetAttrToSelectedCells(aNewSet, false);
+
+                this->SetAttrToSelectedShape(aNewSet);
+
+                if (bUndo)
+                {
+                    rSdrModel.EndUndo();
+                }
+            }
         }
 
         xDlg->disposeOnce();
@@ -2705,6 +2724,18 @@ void SvxTableController::SetAttrToSelectedCells(const SfxItemSet& rAttr, bool bR
         rModel.EndUndo();
 }
 
+void SvxTableController::SetAttrToSelectedShape(const SfxItemSet& rAttr)
+{
+    if (!checkTableObject() || !mxTable.is())
+        return;
+
+    // Filter out non-shadow items from rAttr.
+    SfxItemSet aSet(*rAttr.GetPool(), svl::Items<SDRATTR_SHADOW_FIRST, SDRATTR_SHADOW_LAST>{});
+    aSet.Put(rAttr);
+
+    // Set shadow items on the marked shape.
+    mrView.SetAttrToMarked(aSet, /*bReplaceAll=*/false);
+}
 
 bool SvxTableController::GetAttributes(SfxItemSet& rTargetSet, bool bOnlyHardAttr) const
 {

svx/qa/uitest/table/tablecontroller.py
@@ -1,0 +1,45 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import select_pos
+
+
+# Test for SvxTableController.
+class SvxTableControllerTest(UITestCase):
+
+    def testOnFormatTable(self):
+        # Create an Impress document with a single table in it.
+        self.ui_test.create_doc_in_start_center("impress")
+        template = self.xUITest.getTopFocusWindow()
+        self.ui_test.close_dialog_through_button(template.getChild("cancel"))
+        self.xUITest.executeCommand(".uno:SelectAll")
+        self.xUITest.executeCommand(".uno:Delete")
+        self.xUITest.executeCommand(".uno:InsertTable?Columns:short=2&Rows:short=2")
+
+        # Enable shadow.
+        self.ui_test.execute_dialog_through_command(".uno:TableDialog")
+        tableDialog = self.xUITest.getTopFocusWindow()
+        tabs = tableDialog.getChild("tabcontrol")
+        # Select "shadow".
+        select_pos(tabs, "4")
+        shadowCheckbox = tableDialog.getChild("TSB_SHOW_SHADOW")
+        shadowCheckbox.executeAction("CLICK", tuple())
+        self.ui_test.close_dialog_through_button(tableDialog.getChild("ok"))
+
+        # Check if the shadow was enabled.
+        component = self.ui_test.get_component()
+        drawPage = component.getDrawPages().getByIndex(0)
+        shape = drawPage.getByIndex(0)
+        # Without the accompanying fix in place, this test would have failed with:
+        # AssertionError: False != True
+        # i.e. the table still had no shadow.
+        self.assertEqual(shape.Shadow, True)
+
+        # Close the document.
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:

include/svx/sdr/table/tablecontroller.hxx
@@ -85,6 +85,7 @@ public:
 
     SVX_DLLPRIVATE void MergeAttrFromSelectedCells(SfxItemSet& rAttr, bool bOnlyHardAttr) const;
     SVX_DLLPRIVATE void SetAttrToSelectedCells(const SfxItemSet& rAttr, bool bReplaceAll);
+    void SetAttrToSelectedShape(const SfxItemSet& rAttr);
     /** Fill the values that are common for all selected cells.
       *
       * This lets the Borders dialog to display the line arrangement

svx/Module_svx.mk
@@ -57,6 +57,10 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,svx,\
     JunitTest_svx_unoapi \
 ))
 
+$(eval $(call gb_Module_add_uicheck_targets,svx,\
+    UITest_svx_table \
+))
+
 #todo: noopt for EnhanceCustomShapesFunctionParser.cxx on Solaris Sparc and MacOSX
 #todo: -DUNICODE and -D_UNICODE on WNT for source/dialog
 #todo: component file

svx/UITest_svx_table.mk
@@ -1,0 +1,16 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UITest_UITest,svx_table))
+
+$(eval $(call gb_UITest_add_modules,svx_table,$(SRCDIR)/svx/qa/uitest,\
+	table/ \
+))
+
+# vim: set noet sw=4 ts=4:

commit e71556f46cf22ed22bed5aae7b862357bff2a76d

tdf#139500 svx: fix crash on changing table properties during active text edit
Regression from commit 421afd5264e4981fd3f80adcd512c6a1d2784e60
(tdf#129961 svx: finish UI for table shadow as direct format,
2020-12-15), the problem was that the BegUndo() / EndUndo() pair can be
only used if we know that the text edit of a cell of a table shape is
not started or ended in-between.

The bugreport scenario was an active text edit, where setting attributes
on the shape ends the text edit:

    #9 0x7f6dbb417121 in SdrEditView::EndTextEditAllViews() const /svx/source/svdraw/svdedtv.cxx:1079:20
    #10 0x7f6dbb466798 in SdrEditView::SetAttrToMarked(SfxItemSet const&, bool) /svx/source/svdraw/svdedtv1.cxx:1095:9
    #11 0x7f6dbc34b0af in sdr::table::SvxTableController::SetAttrToSelectedShape(SfxItemSet const&) /svx/source/table/tablecontroller.cxx:2738:12

Which also means that the underlying edit engine is deleted. But then
undo/redo would still reference that edit engine:

==31830==ERROR: AddressSanitizer: heap-use-after-free on address 0x60c0001fc300 at pc 0x7f6dd73a9cb9 bp 0x7fff788db4b0 sp 0x7fff788db4a8
READ of size 8 at 0x60c0001fc300 thread T0
    #0 0x7f6dd73a9cb8 in EditUndo::GetComment() const /editeng/source/editeng/editundo.cxx:147:34

Fix the problem by not grouping in case there is an active text edit,
that's not something I considered when I added the original grouping.

Change-Id: I4f3583e21a27f8380c35b3f4563ce496819bcb81
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115049
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

svx/source/table/tablecontroller.cxx
@@ -984,7 +984,7 @@ void SvxTableController::onFormatTable(const SfxRequest& rReq)
                 // Create a single undo action when applying the result of the dialog.
                 SdrTableObj& rTableObject(*mxTableObj);
                 SdrModel& rSdrModel(rTableObject.getSdrModelFromSdrObject());
-                bool bUndo = rSdrModel.IsUndoEnabled();
+                bool bUndo = rSdrModel.IsUndoEnabled() && !mrView.IsTextEdit();
                 if (bUndo)
                 {
                     rSdrModel.BegUndo(SvxResId(STR_TABLE_NUMFORMAT));

svx/qa/uitest/table/tablecontroller.py
@@ -6,6 +6,7 @@
 
 from uitest.framework import UITestCase
 from uitest.uihelper.common import select_pos
+from libreoffice.uno.propertyvalue import mkPropertyValues
 
 
 # Test for SvxTableController.
@@ -38,6 +39,43 @@ class SvxTableControllerTest(UITestCase):
         # AssertionError: False != True
         # i.e. the table still had no shadow.
         self.assertEqual(shape.Shadow, True)
+
+        # Close the document.
+        self.ui_test.close_doc()
+
+    def testUndoCrash(self):
+        # Given an Impress document with a single table in it:
+        self.ui_test.create_doc_in_start_center("impress")
+        template = self.xUITest.getTopFocusWindow()
+        self.ui_test.close_dialog_through_button(template.getChild("close"))
+        self.xUITest.executeCommand(".uno:SelectAll")
+        self.xUITest.executeCommand(".uno:Delete")
+        self.xUITest.executeCommand(".uno:InsertTable?Columns:short=3&Rows:short=3")
+        self.xUITest.executeCommand(".uno:SelectAll")
+
+        # When enabling shadow on the shape while text edit is active:
+        doc = self.xUITest.getTopFocusWindow()
+        impress = doc.getChild("impress_win")
+        impress.executeAction("TYPE", mkPropertyValues({"TEXT": "A1"}))
+        for i in range(6):
+            impress.executeAction("TYPE", mkPropertyValues({"KEYCODE": "CTRL+TAB"}))
+        impress.executeAction("TYPE", mkPropertyValues({"TEXT": "A3"}))
+        self.xUITest.executeCommand(".uno:SelectAll")
+        self.ui_test.execute_dialog_through_command(".uno:TableDialog")
+        tableDialog = self.xUITest.getTopFocusWindow()
+        tabs = tableDialog.getChild("tabcontrol")
+        # Select "shadow".
+        select_pos(tabs, "4")
+        shadowCheckbox = tableDialog.getChild("TSB_SHOW_SHADOW")
+        shadowCheckbox.executeAction("CLICK", tuple())
+        self.ui_test.close_dialog_through_button(tableDialog.getChild("ok"))
+
+        # Then make sure we don't crash:
+        # Without the accompanying fix in place, this test would have failed crashed due to a
+        # use-after-free: text edit ended but an undo action of the text edit remained on the undo
+        # stack.
+        for i in range(2):
+            self.xUITest.executeCommand(".uno:Undo")
 
         # Close the document.
         self.ui_test.close_doc()