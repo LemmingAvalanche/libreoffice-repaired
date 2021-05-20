LemmingAvalanche/libreoffice-repaired

commit 238c17032cfc6c9d03a03b38a032af76ddabc6cb

split OutputDevice from Window
as part of a longer-term goal of doing our
widget rendering only inside a top-level render-
context.

I moved all of the OutputDevice-related code that existed in vcl::Window
into a new subclass of OutputDevice called WindowOutputDevice.

Notes for further work

(*) not sure why we are getting an 1x1 surface in
    SvpSalGraphics::releaseCairoContext, but to fix it I clamp
    the size there

(*) might have to dump VCLXDevice, and move it's code down into VCLXWindow and VCLXVirtualDevice

(*) can we remove use of VCLXDevice in other places, in favour of just talking to the VCL code?

Change-Id: I105946377f5322677d6f7d0c1c23847178a720b6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113204
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdview.cxx
@@ -163,7 +163,7 @@ SdrView::~SdrView()
 
 bool SdrView::KeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
 {
-    SetActualWin(pWin);
+    SetActualWin(pWin->GetOutDev());
     bool bRet = SdrCreateView::KeyInput(rKEvt,pWin);
     if (!bRet && !IsExtendedKeyInputDispatcherEnabled()) {
         bRet = true;
@@ -190,7 +190,7 @@ bool SdrView::KeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
         if (bRet && pWin!=nullptr) {
             pWin->SetPointer(GetPreferredPointer(
                 pWin->PixelToLogic(pWin->ScreenToOutputPixel( pWin->GetPointerPosPixel() ) ),
-                pWin,
+                pWin->GetOutDev(),
                 rKEvt.GetKeyCode().GetModifier()));
         }
     }
@@ -240,7 +240,7 @@ bool SdrView::MouseMove(const MouseEvent& rMEvt, OutputDevice* pWin)
 
 bool SdrView::Command(const CommandEvent& rCEvt, vcl::Window* pWin)
 {
-    SetActualWin(pWin);
+    SetActualWin(pWin->GetOutDev());
     bool bRet = SdrCreateView::Command(rCEvt,pWin);
     return bRet;
 }
@@ -811,7 +811,7 @@ bool SdrView::DoMouseEvent(const SdrViewEvent& rVEvt)
                     if (eHit==SdrHitKind::TextEdit)
                     {
                         bool bRet2(mpActualOutDev && OUTDEV_WINDOW == mpActualOutDev->GetOutDevType() &&
-                            SdrBeginTextEdit(rVEvt.pObj, rVEvt.pPV, static_cast<vcl::Window*>(mpActualOutDev.get())));
+                            SdrBeginTextEdit(rVEvt.pObj, rVEvt.pPV, mpActualOutDev->GetOwnerWindow()));
 
                         if(bRet2)
                         {
@@ -897,7 +897,7 @@ bool SdrView::DoMouseEvent(const SdrViewEvent& rVEvt)
             } else bRet=BegCreateObj(aLogicPos);
         } break;
         case SdrEventKind::BeginMacroObj: {
-            BegMacroObj(aLogicPos,mnHitTolLog,rVEvt.pObj,rVEvt.pPV,static_cast<vcl::Window*>(mpActualOutDev.get()));
+            BegMacroObj(aLogicPos,mnHitTolLog,rVEvt.pObj,rVEvt.pPV,mpActualOutDev->GetOwnerWindow());
             bRet=false;
         } break;
         case SdrEventKind::BeginTextEdit: {
@@ -907,7 +907,7 @@ bool SdrView::DoMouseEvent(const SdrViewEvent& rVEvt)
             }
 
             bRet = mpActualOutDev && OUTDEV_WINDOW == mpActualOutDev->GetOutDevType()&&
-                 SdrBeginTextEdit(rVEvt.pObj, rVEvt.pPV, static_cast<vcl::Window*>(mpActualOutDev.get()));
+                 SdrBeginTextEdit(rVEvt.pObj, rVEvt.pPV, mpActualOutDev->GetOwnerWindow());
 
             if(bRet)
             {
@@ -920,14 +920,14 @@ bool SdrView::DoMouseEvent(const SdrViewEvent& rVEvt)
         default: break;
     } // switch
     if (bRet && mpActualOutDev && mpActualOutDev->GetOutDevType()==OUTDEV_WINDOW) {
-        vcl::Window* pWin=static_cast<vcl::Window*>(mpActualOutDev.get());
+        vcl::Window* pWin=mpActualOutDev->GetOwnerWindow();
         // left mouse button pressed?
         bool bLeftDown=(rVEvt.nMouseCode&MOUSE_LEFT)!=0 && rVEvt.bMouseDown;
         // left mouse button released?
         bool bLeftUp=(rVEvt.nMouseCode&MOUSE_LEFT)!=0 && rVEvt.bMouseUp;
         // left mouse button pressed or held?
         bool bLeftDown1=(rVEvt.nMouseCode&MOUSE_LEFT)!=0 && !rVEvt.bMouseUp;
-        pWin->SetPointer(GetPreferredPointer(rVEvt.aLogicPos,pWin,
+        pWin->SetPointer(GetPreferredPointer(rVEvt.aLogicPos,pWin->GetOutDev(),
                 rVEvt.nMouseCode & (KEY_SHIFT|KEY_MOD1|KEY_MOD2),bLeftDown1));
         bool bAction=IsAction();
         if (bLeftDown && bAction)

commit 97c0b67c0469eecdb78bcf37e4840820f36b7997

fix crash when del in textbox in draw
regression from
    commit 238c17032cfc6c9d03a03b38a032af76ddabc6cb
    split OutputDevice from Window

Change-Id: Ib0df6b691f2281c366126c3a0cf3aae985e706be
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115857
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdview.cxx
@@ -163,7 +163,7 @@ SdrView::~SdrView()
 
 bool SdrView::KeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
 {
-    SetActualWin(pWin->GetOutDev());
+    SetActualWin(pWin ? pWin->GetOutDev() : nullptr);
     bool bRet = SdrCreateView::KeyInput(rKEvt,pWin);
     if (!bRet && !IsExtendedKeyInputDispatcherEnabled()) {
         bRet = true;