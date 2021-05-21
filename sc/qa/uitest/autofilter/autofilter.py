LemmingAvalanche/libreoffice-repaired

commit 19210a8693dc3d1c0a488e93a39a4307b0ea21ad

tdf#140968 tdf#140978 XLSX import: fix lost rounded filters
if the stored filter values are in the visible cell
format (e.g. rounded values) instead of the original
(editing) values.

Now AutoFilter popup window shows the items according
to the visible cell format (e.g. 1.0 instead of 1.01 or
0.99), but still grouping them based on the "editing
format" (e.g. not rounded values which visible during
editing), i.e. there could be repeated values in the
filtering conditions (e.g. two options "1.0" and "1.0"
for 1.01 and 0.99).

Note: Next step will be to group and filter based on the
actual cell format, like MSO does, to simplify filtering
of values rounded by the cell format (e.g. selecting
the single AutoFilter condition "1.0" to filter both
1.01 and 0.99).

Change-Id: I430da5e09794fc4ed8acf79b6485926f46b70277
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112343
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/ui/view/gridwin.cxx
@@ -544,14 +544,18 @@ public:
 
 class AddSelectedItemString
 {
-    std::unordered_set<OUString>& mrSet;
+    std::unordered_set<OUString>& mrSetString;
+    std::unordered_set<double>& mrSetValue;
 public:
-    explicit AddSelectedItemString(std::unordered_set<OUString>& r) :
-        mrSet(r) {}
+    explicit AddSelectedItemString(std::unordered_set<OUString>& rString, std::unordered_set<double>& rValue) :
+        mrSetString(rString), mrSetValue(rValue) {}
 
     void operator() (const ScQueryEntry::Item& rItem)
     {
-        mrSet.insert(rItem.maString.getString());
+        if( rItem.meType == ScQueryEntry::QueryType::ByValue )
+            mrSetValue.insert(rItem.mfVal);
+        else
+            mrSetString.insert(rItem.maString.getString());
     }
 };
 
@@ -659,13 +663,14 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
     ScQueryParam aParam;
     pDBData->GetQueryParam(aParam);
     std::vector<ScQueryEntry*> aEntries = aParam.FindAllEntriesByField(nCol);
-    std::unordered_set<OUString> aSelected;
+    std::unordered_set<OUString> aSelectedString;
+    std::unordered_set<double> aSelectedValue;
     for (ScQueryEntry* pEntry : aEntries)
     {
         if (pEntry && pEntry->bDoQuery && pEntry->eOp == SC_EQUAL)
         {
             ScQueryEntry::QueryItemsType& rItems = pEntry->GetQueryItems();
-            std::for_each(rItems.begin(), rItems.end(), AddSelectedItemString(aSelected));
+            std::for_each(rItems.begin(), rItems.end(), AddSelectedItemString(aSelectedString, aSelectedValue));
         }
     }
 
@@ -673,14 +678,15 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
     rControl.setMemberSize(aFilterEntries.size());
     for (const auto& rEntry : aFilterEntries)
     {
-        const OUString& aVal = rEntry.GetString();
+        const OUString& aStringVal = rEntry.GetString();
+        const double aDoubleVal = rEntry.GetValue();
         bool bSelected = true;
-        if (!aSelected.empty())
-            bSelected = aSelected.count(aVal) > 0;
+        if (!aSelectedValue.empty() || !aSelectedString.empty())
+            bSelected = aSelectedValue.count(aDoubleVal) > 0 || aSelectedString.count(aStringVal) > 0;
         if ( rEntry.IsDate() )
-            rControl.addDateMember( aVal, rEntry.GetValue(), bSelected );
+            rControl.addDateMember( aStringVal, rEntry.GetValue(), bSelected );
         else
-            rControl.addMember(aVal, bSelected);
+            rControl.addMember(aStringVal, bSelected);
     }
 
     // Populate the menu.

sc/qa/uitest/autofilter/autofilter.py
@@ -324,4 +324,19 @@ class AutofilterTest(UITestCase):
         xOkBtn.executeAction("CLICK", tuple())
 
         self.ui_test.close_doc()
+
+    def test_time_value(self):
+        doc = self.ui_test.load_file(get_url_for_data_file("time_value.xlsx"))
+
+        xGridWin = self.xUITest.getTopFocusWindow().getChild("grid_window")
+
+        xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "1", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xTreeList = xCheckListMenu.getChild("check_list_box")
+        self.assertEqual(2, len(xTreeList.getChildren()))
+        xOkBtn = xFloatWindow.getChild("cancel")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

include/svl/zforlist.hxx
@@ -560,9 +560,12 @@ public:
                           OUString& sOutString, const Color** ppColor, bool bUseStarFormat = false );
 
     /** Format a number according to the standard default format matching
-        the given format index */
+        the given format index. rOutString will be the real cell string (e.g.
+        a number rounded by the cell format, which rounded value is used
+        in the filtering condition now), instead of the EditFormat string
+        (e.g a not rounded value, which is visible during editing).*/
     void GetInputLineString( const double& fOutNumber,
-                             sal_uInt32 nFIndex, OUString& rOutString );
+                             sal_uInt32 nFIndex, OUString& rOutString, bool bFiltering = false );
 
     /** Format a number according to a format code string to be scanned.
         @return

sc/inc/cellform.hxx
@@ -44,7 +44,7 @@ public:
 
     static void GetInputString(
         const ScRefCellValue& rCell, sal_uInt32 nFormat, OUString& rString, SvNumberFormatter& rFormatter,
-        const ScDocument& rDoc );
+        const ScDocument& rDoc, bool bFiltering = false );
 
     static OUString GetOutputString(
         ScDocument& rDoc, const ScAddress& rPos, const ScRefCellValue& rCell );

sc/inc/column.hxx
@@ -134,6 +134,8 @@ class ScColumn
     SCCOL           nCol;
     SCTAB           nTab;
 
+    bool mbFiltering; // it is true if there is a filtering in the column
+
 friend class ScDocument;                    // for FillInfo
 friend class ScTable;
 friend class ScValueIterator;
@@ -181,6 +183,7 @@ public:
     ScDocument& GetDoc() const { return pAttrArray->GetDoc(); }
     SCTAB GetTab() const { return nTab; }
     SCCOL GetCol() const { return nCol; }
+    bool HasFiltering() const { return mbFiltering; }
     sc::CellStoreType& GetCellStore() { return maCells; }
     const sc::CellStoreType& GetCellStore() const { return maCells; }
     sc::CellTextAttrStoreType& GetCellAttrStore() { return maCellTextAttrs; }
@@ -534,7 +537,7 @@ public:
 
     void GetFilterEntries(
         sc::ColumnBlockConstPosition& rBlockPos, SCROW nStartRow, SCROW nEndRow,
-        ScFilterEntries& rFilterEntries );
+        ScFilterEntries& rFilterEntries, bool bFiltering );
 
     bool GetDataEntries( SCROW nRow, std::set<ScTypedStrData>& rStrings, bool bLimit ) const;
 

sc/inc/queryentry.hxx
@@ -39,8 +39,9 @@ struct SC_DLLPUBLIC ScQueryEntry
         double        mfVal;
         svl::SharedString maString;
         bool              mbMatchEmpty;
+        bool              mbFormattedValue;
 
-        Item() : meType(ByValue), mfVal(0.0), mbMatchEmpty(false) {}
+        Item() : meType(ByValue), mfVal(0.0), mbMatchEmpty(false), mbFormattedValue(false) {}
 
         bool operator== (const Item& r) const;
     };

sc/inc/table.hxx
@@ -953,7 +953,7 @@ public:
     bool        CreateQueryParam(SCCOL nCol1, SCROW nRow1, SCCOL nCol2, SCROW nRow2, ScQueryParam& rQueryParam);
 
     void GetFilterEntries(SCCOL nCol, SCROW nRow1, SCROW nRow2, ScFilterEntries& rFilterEntries );
-    void GetFilteredFilterEntries(SCCOL nCol, SCROW nRow1, SCROW nRow2, const ScQueryParam& rParam, ScFilterEntries& rFilterEntries );
+    void GetFilteredFilterEntries(SCCOL nCol, SCROW nRow1, SCROW nRow2, const ScQueryParam& rParam, ScFilterEntries& rFilterEntries, bool bFiltering );
     [[nodiscard]]
     bool GetDataEntries(SCCOL nCol, SCROW nRow, std::set<ScTypedStrData>& rStrings, bool bLimit);
 

commit c71d4acf4a523d2ac31e2eda76d72b97b8a00a3a

tdf#142350 sc AutoFilter: fix (empty) entry checking
When a column was filtered for values that included 0,
but not the (empty) entry, the (empty) entry also
was checked in the Autofilter dropdown.

Regression from commit 19210a8693dc3d1c0a488e93a39a4307b0ea21ad
"tdf#140968 tdf#140978 XLSX import: fix lost rounded filters".

Change-Id: Ic2595c707bd43f1a19c86d2ee796f06d9b1af1e3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115755
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/ui/view/gridwin.cxx
@@ -684,7 +684,14 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
         const double aDoubleVal = rEntry.GetValue();
         bool bSelected = true;
         if (!aSelectedValue.empty() || !aSelectedString.empty())
-            bSelected = aSelectedValue.count(aDoubleVal) > 0 || aSelectedString.count(aStringVal) > 0;
+        {
+            if (aStringVal.isEmpty())
+                bSelected = aSelectedString.count(aStringVal) > 0;
+            else
+                bSelected
+                    = aSelectedValue.count(aDoubleVal) > 0 || aSelectedString.count(aStringVal) > 0;
+        }
+
         if ( rEntry.IsDate() )
             rControl.addDateMember( aStringVal, rEntry.GetValue(), bSelected );
         else

sc/qa/uitest/autofilter/autofilter.py
@@ -367,4 +367,47 @@ class AutofilterTest(UITestCase):
         self.assertFalse(is_row_hidden(doc, 6))
 
         self.ui_test.close_doc()
+
+    def test_tdf142350(self):
+        self.ui_test.create_doc_in_start_center("calc")
+        document = self.ui_test.get_component()
+        calcDoc = self.xUITest.getTopFocusWindow()
+        gridwin = calcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+
+        enter_text_to_cell(gridwin, "A1", "A")
+        enter_text_to_cell(gridwin, "A2", "0")
+        enter_text_to_cell(gridwin, "A3", "")
+        enter_text_to_cell(gridwin, "A4", "1")
+
+        gridwin.executeAction("SELECT", mkPropertyValues({"RANGE": "A1:A4"}))
+
+        self.xUITest.executeCommand(".uno:DataFilterAutoFilter")
+
+        gridwin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xList = xCheckListMenu.getChild("check_list_box")
+        xEntry = xList.getChild("2")
+        xEntry.executeAction("CLICK", tuple())
+
+        xOkButton = xFloatWindow.getChild("ok")
+        xOkButton.executeAction("CLICK", tuple())
+
+        self.assertFalse(is_row_hidden(document, 1))
+        self.assertTrue(is_row_hidden(document, 2))
+        self.assertFalse(is_row_hidden(document, 3))
+
+        gridwin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xList = xCheckListMenu.getChild("check_list_box")
+        self.assertEqual(3, len(xList.getChildren()))
+        self.assertEqual('true', get_state_as_dict(xList.getChild('0'))['IsChecked'])
+        self.assertEqual('true', get_state_as_dict(xList.getChild('1'))['IsChecked'])
+        self.assertEqual('false', get_state_as_dict(xList.getChild('2'))['IsChecked'])
+        xCloseButton = xFloatWindow.getChild("cancel")
+        xCloseButton.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
 # vim: set shiftwidth=4 softtabstop=4 expandtab: