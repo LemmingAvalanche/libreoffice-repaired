LemmingAvalanche/libreoffice-repaired

commit a18fed8c5188ee875b449ef35581f4a580c176eb

cleanup mutex and signalling in ExecuteWrapper
found this while examining issues reported by clang-tidy.

Judging from the code, the original intention was to somehow lock access
to the mbSignal field in ExecuteWrapper, but since we were not locking
when writing to that field, and that field was not volatile, it
surprises me that this worked very well at all.

We can accomplish the same end more reliably by just marking the field
as volatile and not doing any locking at all.

Change-Id: I388c7082a809b6aca5a3c8981625f55cfef3cfcd
Reviewed-on: https://gerrit.libreoffice.org/60184
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/uitest/uno/uiobject_uno.cxx
@@ -58,8 +58,7 @@ class ExecuteWrapper
 {
     std::function<void()> mFunc;
     Link<Timer*, void> mHandler;
-    bool mbSignal;
-    std::mutex mMutex;
+    volatile bool mbSignal;
 
 public:
 
@@ -73,11 +72,6 @@ public:
     void setSignal()
     {
         mbSignal = true;
-    }
-
-    std::mutex& getMutex()
-    {
-        return mMutex;
     }
 
     DECL_LINK( ExecuteActionHdl, Timer*, void );
@@ -96,20 +90,8 @@ IMPL_LINK_NOARG(ExecuteWrapper, ExecuteActionHdl, Timer*, void)
             aIdle.Start();
         }
 
-        for (;;) {
-            {
-                std::unique_lock<std::mutex> lock(mMutex);
-                if (mbSignal) {
-                    break;
-                }
-            }
+        while (!mbSignal) {
             Application::Reschedule();
-        }
-        std::unique_lock<std::mutex> lock(mMutex);
-        while (!mbSignal)
-        {
-            // coverity[sleep] - intentional sleep while mutex held
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
         }
     }
     delete this;
@@ -146,7 +128,6 @@ void SAL_CALL UIObjectUnoObj::executeAction(const OUString& rAction, const css::
     };
 
     ExecuteWrapper* pWrapper = new ExecuteWrapper(func, LINK(this, UIObjectUnoObj, NotifyHdl));
-    std::unique_lock<std::mutex>(pWrapper->getMutex());
     aIdle->SetInvokeHandler(LINK(pWrapper, ExecuteWrapper, ExecuteActionHdl));
     {
         SolarMutexGuard aGuard;

commit 3111babf2b4f2c30f9a1add252c47529dadffccb

Fix misuse of volatile to avoid a data race
...introduced with a18fed8c5188ee875b449ef35581f4a580c176eb "cleanup mutex and
signalling in ExecuteWrapper"

Change-Id: Iae1dda078d165355f82986f4105bd4bb53ce6e9a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117885
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

vcl/source/uitest/uno/uiobject_uno.cxx
@@ -7,6 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <sal/config.h>
+
+#include <atomic>
 #include <memory>
 #include "uiobject_uno.hxx"
 #include <utility>
@@ -56,7 +59,7 @@ class ExecuteWrapper
 {
     std::function<void()> mFunc;
     Link<Timer*, void> mHandler;
-    volatile bool mbSignal;
+    std::atomic<bool> mbSignal;
 
 public:
 