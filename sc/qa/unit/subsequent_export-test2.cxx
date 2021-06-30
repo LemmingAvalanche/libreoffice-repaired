LemmingAvalanche/libreoffice-repaired

commit 818412185a82381738de26e5944cf99da01c0edf

tdf#129969 XLSX export: file URLs need IURI encoding
External hyperlinks with # in directory name didn't work
after XLSX export.

Change-Id: Ibdf4fcbb65d0c580841d24c1721454164e341424
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104272
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/filter/excel/xecontent.cxx
@@ -351,9 +351,8 @@ XclExpHyperlink::XclExpHyperlink( const XclExpRoot& rRoot, const SvxURLField& rU
     {
         sal_uInt16 nLevel;
         bool bRel;
-        /* TODO: should we differentiate between BIFF and OOXML and write IURI
-         * encoded for OOXML? */
-        OUString aFileName( BuildFileName( nLevel, bRel, rUrl, rRoot, false ) );
+        OUString aFileName(
+            BuildFileName(nLevel, bRel, rUrl, rRoot, rRoot.GetOutput() == EXC_OUTPUT_XML_2007));
 
         if( eProtocol == INetProtocol::Smb )
         {

sc/qa/unit/subsequent_export-test.cxx
@@ -270,6 +270,7 @@ public:
     void testTdf137000_handle_upright();
     void testTdf126305_DataValidatyErrorAlert();
     void testTdf76047_externalLink();
+    void testTdf129969();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -438,6 +439,7 @@ public:
     CPPUNIT_TEST(testTdf137000_handle_upright);
     CPPUNIT_TEST(testTdf126305_DataValidatyErrorAlert);
     CPPUNIT_TEST(testTdf76047_externalLink);
+    CPPUNIT_TEST(testTdf129969);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -5516,8 +5518,10 @@ void ScExportTest::testTdf76047_externalLink()
     ScDocument& rDoc = pShell->GetDocument();
 
     // compare the loaded data (from external links) to the data copied manually to the testfile
-    for (int nCol = 1; nCol <= 5; nCol++) {
-        for (int nRow = 3; nRow <= 5; nRow++) {
+    for (int nCol = 1; nCol <= 5; nCol++)
+    {
+        for (int nRow = 3; nRow <= 5; nRow++)
+        {
             OUString aStr1 = rDoc.GetString(ScAddress(nCol, nRow, 0));
             OUString aStr2 = rDoc.GetString(ScAddress(nCol, nRow + 5, 0));
             OUString aStr3 = rDoc.GetString(ScAddress(nCol, nRow + 11, 0));
@@ -5526,6 +5530,23 @@ void ScExportTest::testTdf76047_externalLink()
             CPPUNIT_ASSERT_EQUAL(aStr2, aStr3);
         }
     }
+}
+
+void ScExportTest::testTdf129969()
+{
+    ScDocShellRef xShell = loadDoc("external_hyperlink.", FORMAT_ODS);
+    CPPUNIT_ASSERT(xShell.is());
+
+    ScDocShellRef xDocSh = saveAndReload(&(*xShell), FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+    ScDocument& rDoc = xDocSh->GetDocument();
+    ScAddress aPos(0, 0, 0);
+    const EditTextObject* pEditText = rDoc.GetEditText(aPos);
+    const SvxFieldData* pData = pEditText->GetFieldData(0, 0, text::textfield::Type::URL);
+    const SvxURLField* pURLData = static_cast<const SvxURLField*>(pData);
+    CPPUNIT_ASSERT(pURLData->GetURL().endsWith("/%23folder/test.ods#Sheet2.B10"));
+
+    xDocSh->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);

commit abde416f569deaea496f5a6ded15489d12394219

tdf#140431 XLSX export: fix double file:// prefix
Regression from commit: 818412185a82381738de26e5944cf99da01c0edf
(tdf#129969 XLSX export: file URLs need IURI encoding)

Change-Id: I5bfb0ef9ba7fe82cd3f4d0e0b3fdcf8f705cba64
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118090
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/filter/excel/xecontent.cxx
@@ -394,9 +394,9 @@ XclExpHyperlink::XclExpHyperlink( const XclExpRoot& rRoot, const SvxURLField& rU
             for( int i = 0; i < nLevel; ++i )
                 msTarget = "../" + msTarget;
         }
-        else
+        else if (rRoot.GetOutput() != EXC_OUTPUT_XML_2007)
         {
-            // ooxml expects the file:/// part appended ( or at least
+            // xls expects the file:/// part appended ( or at least
             // ms2007 does, ms2010 is more tolerant )
             msTarget = "file:///" + msTarget;
         }

sc/qa/unit/subsequent_export-test2.cxx
@@ -186,6 +186,7 @@ public:
     void testTdf136721_paper_size();
     void testTdf139258_rotated_image();
     void testTdf126541_SheetVisibilityImportXlsx();
+    void testTdf140431();
 
     CPPUNIT_TEST_SUITE(ScExportTest2);
 
@@ -280,6 +281,7 @@ public:
     CPPUNIT_TEST(testTdf136721_paper_size);
     CPPUNIT_TEST(testTdf139258_rotated_image);
     CPPUNIT_TEST(testTdf126541_SheetVisibilityImportXlsx);
+    CPPUNIT_TEST(testTdf140431);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2280,6 +2282,23 @@ void ScExportTest2::testTdf126541_SheetVisibilityImportXlsx()
     // Sheet based grid line visibility setting should not overwrite the global setting.
     xShell = loadDocAndSetupModelViewController(u"tdf126541_GridOff.", FORMAT_XLSX, true);
     CPPUNIT_ASSERT(xShell->GetDocument().GetViewOptions().GetOption(VOPT_GRID));
+}
+
+void ScExportTest2::testTdf140431()
+{
+    ScDocShellRef xShell = loadDoc(u"129969-min.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xShell.is());
+
+    ScDocShellRef xDocSh = saveAndReload(&(*xShell), FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+    ScDocument& rDoc = xDocSh->GetDocument();
+    ScAddress aPos(0, 2, 0);
+    const EditTextObject* pEditText = rDoc.GetEditText(aPos);
+    const SvxFieldData* pData = pEditText->GetFieldData(0, 0, text::textfield::Type::URL);
+    const SvxURLField* pURLData = static_cast<const SvxURLField*>(pData);
+    CPPUNIT_ASSERT(pURLData->GetURL().startsWith("file://ndhlis"));
+
+    xDocSh->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest2);