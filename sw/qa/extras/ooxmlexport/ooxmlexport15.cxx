LemmingAvalanche/libreoffice-repaired

commit 86181f54e25a00e3e126f669e0cb12437e8df8b4

tdf#92525 tdf#142398: fix export of simple custom shapes
Most of the custom shapes were exported with DrawingML custom
geometry instead of DOCX preset shapes, losing their preset type,
adjust handles and text position. Add a preset exporter class to
handle all possible preset shapes later, and fix the export of
the following shapes now: "bevel", "blockArc", "bracePair",
"bracketPair", "hexagon", "octagon", "parallelogram", "plus",
"roundRect" and "triangle".

tdf#92525: keep text position in triangles with different
adjustments, too.

tdf#142398: part 1: export simple shapes as preset shapes.

Change-Id: I6aee74f7670bea8c1fe5909cbf307778ea728669
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115606
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

include/oox/export/DMLPresetShapeExport.hxx
@@ -1,0 +1,138 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+* This file is part of the LibreOffice project.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef INCLUDED_OOX_EXPORT_DMLPRESETSHAPEXPORT_HXX
+#define INCLUDED_OOX_EXPORT_DMLPRESETSHAPEXPORT_HXX
+
+#include <com/sun/star/awt/Rectangle.hpp>
+#include <com/sun/star/uno/Any.hxx>
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/uno/Sequence.hxx>
+
+#include <rtl/ustring.hxx>
+#include <sal/types.h>
+
+#include <string_view>
+
+#include <oox/export/drawingml.hxx>
+
+namespace com::sun::star::beans
+{
+struct PropertyValue;
+}
+
+namespace com::sun::star::drawing
+{
+class XShape;
+struct EnhancedCustomShapeAdjustmentValue;
+}
+
+namespace oox::core
+{
+class XmlFilterBase;
+}
+
+namespace oox::drawingml
+{
+/// Class for exporting the custom shapes to OOXML preset ones, if possible.
+/// This functionality needed for keeping the information for the office programs
+/// about the shape type, and geometry data. Before these shapes were exported
+/// with custom geometry, and they kept their geometry but has no information
+/// about the shape itself. This lead to lost textbox size/position/padding for
+/// example.
+class DMLPresetShapeExporter
+{
+private:
+    // the shape to export
+    css::uno::Reference<css::drawing::XShape> m_xShape;
+    // the DMLwriter
+    DrawingML* m_pDMLexporter;
+    // the type of the custom shape (diamond/rectangle/circle/triangle...)
+    OUString m_sPresetShapeType;
+    // True if the shape has points where its geometry can be modified
+    bool m_bHasHandleValues;
+    // The first the x the second the y coordinate, of flipping
+    std::pair<bool, bool> m_bIsFlipped;
+
+    // Custom Shape Geometry information for export:
+
+    // The adjusting values stored in this sequence:
+    css::uno::Sequence<css::drawing::EnhancedCustomShapeAdjustmentValue> m_AdjustmentValues;
+    // Shapes what have adjusting points, the range of these points
+    // and the index of the value stored in this sequence:
+    css::uno::Sequence<css::uno::Sequence<css::beans::PropertyValue>> m_HandleValues;
+
+    //TODO:
+    //css::awt::Rectangle m_ViewBox;
+    //css::uno::Sequence<css::beans::PropertyValue> m_Path;
+    //css::uno::Sequence<OUString> m_Equations;
+
+public:
+    DMLPresetShapeExporter() = delete;
+    ~DMLPresetShapeExporter();
+
+    DMLPresetShapeExporter(DrawingML* pDMLExporter,
+                           css::uno::Reference<css::drawing::XShape> xShape);
+
+    // Writes the preset shape to the xml
+    bool WriteShape();
+
+private:
+    struct AdjustmentPointValueBase
+    {
+        double nMaxVal;
+        double nMinVal;
+        double nCurrVal;
+    };
+
+    typedef AdjustmentPointValueBase RadiusAdjustmentValue;
+    typedef AdjustmentPointValueBase AngleAdjustmentValue;
+    typedef AdjustmentPointValueBase XAdjustmentValue;
+    typedef AdjustmentPointValueBase YAdjustmentValue;
+
+    // Returns true, if the shape has adjusting points
+    bool HasHandleValue();
+
+    // Returns true if the shape flipped.
+    bool IsXFlipped() { return m_bIsFlipped.first; };
+    bool IsYFlipped() { return m_bIsFlipped.second; };
+
+    // Returns with the shape type, like triangle for example
+    OUString GetShapeType();
+    // Returns with the handle points
+    css::uno::Sequence<css::uno::Sequence<css::beans::PropertyValue>> GetHandleValues();
+    // Returns with the adjustment values
+    css::uno::Sequence<css::drawing::EnhancedCustomShapeAdjustmentValue> GetAdjustmentValues();
+    // Returns with the raw value of the given property of the shape geometry.
+    css::uno::Any GetHandleValueOfModificationPoint(sal_Int32 nPoint, std::u16string_view sType);
+    // Returns with the appropriate value of the handle point.
+    RadiusAdjustmentValue GetAdjustmentPointRadiusValue(sal_Int32 nPoint);
+    AngleAdjustmentValue GetAdjustmentPointAngleValue(sal_Int32 nPoint);
+    XAdjustmentValue GetAdjustmentPointXValue(sal_Int32 nPoint);
+    YAdjustmentValue GetAdjustmentPointYValue(sal_Int32 nPoint);
+
+    // Writes one adjustment point.
+    bool WriteAV(const OUString& sValName, const OUString& sVal);
+    // Opens/Closes the AVlist tag.
+    bool StartAVListWriting();
+    bool EndAVListWriting();
+
+    // Finds the given value in the sequence
+    static css::uno::Any FindHandleValue(css::uno::Sequence<css::beans::PropertyValue> aValues,
+                                         std::u16string_view sKey);
+    // Writes and converts the adjustment points from sdr to ooxml ones per shape type.
+    bool WriteShapeWithAVlist();
+
+}; // end of DMLPresetShapeExporter class
+
+} // end of namespace oox::drawingml
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

oox/Library_oox.mk
@@ -217,6 +217,7 @@ $(eval $(call gb_Library_add_exception_objects,oox,\
     oox/source/export/chartexport \
     oox/source/export/ColorPropertySet \
     oox/source/export/drawingml \
+    oox/source/export/DMLPresetShapeExport \
     oox/source/export/shapes \
     oox/source/export/vmlexport \
     oox/source/helper/attributelist \

oox/source/export/shapes.cxx
@@ -73,6 +73,7 @@
 #include <oox/export/chartexport.hxx>
 #include <oox/mathml/export.hxx>
 #include <basegfx/numeric/ftools.hxx>
+#include <oox/export/DMLPresetShapeExport.hxx>
 
 using namespace ::css;
 using namespace ::css::beans;
@@ -832,7 +833,19 @@ ShapeExport& ShapeExport::WriteCustomShape( const Reference< XShape >& xShape )
     else if( bHasHandles )
         bCustGeom = true;
 
-    if (bHasHandles && bCustGeom)
+    bool bPresetWriteSuccessful = false;
+    // Let the custom shapes what has name and preset information in OOXML, to be written
+    // as preset ones with parameters. Try that with this converter class.
+    if (!sShapeType.startsWith("ooxml") && GetDocumentType() == DOCUMENT_DOCX
+        && xShape->getShapeType() == "com.sun.star.drawing.CustomShape")
+    {
+        DMLPresetShapeExporter aCustomShapeConverter(this, xShape);
+        bPresetWriteSuccessful = aCustomShapeConverter.WriteShape();
+    }
+    // If preset writing has problems try to write the shape as it done before
+    if (bPresetWriteSuccessful)
+        ;// Already written do nothing.
+    else if (bHasHandles && bCustGeom)
     {
         WriteShapeTransformation( xShape, XML_a, bFlipH, bFlipV, false, true );// do not flip, polypolygon coordinates are flipped already
         tools::PolyPolygon aPolyPolygon( rSdrObjCustomShape.GetLineGeometry(true) );

sw/qa/extras/ooxmlexport/ooxmlexport16.cxx
@@ -129,6 +129,37 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testGutterTop, "gutter-top.docx")
     assertXPath(pXmlSettings, "/w:settings/w:gutterAtTop", 1);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testCustomShapePresetExport, "testCustomShapePresetExport.odt")
+{
+    // Check if the load failed.
+    CPPUNIT_ASSERT(getPages());
+
+    // Check all shapes of the file
+    int nCount = 0;
+    for (int i = 1; i <= getShapes(); i++)
+    {
+        uno::Reference<beans::XPropertySet> xProperties(getShape(i), uno::UNO_QUERY);
+        if (!xProperties->getPropertySetInfo()->hasPropertyByName("CustomShapeGeometry"))
+            continue;
+        // Get the custom shape property
+        auto aCustomShapeGeometry = xProperties->getPropertyValue("CustomShapeGeometry")
+                                        .get<uno::Sequence<beans::PropertyValue>>();
+        // Find for shape type
+        for (const auto& aCustomGeometryIterator : std::as_const(aCustomShapeGeometry))
+        {
+            if (aCustomGeometryIterator.Name == "Type")
+                CPPUNIT_ASSERT_MESSAGE(
+                    "This is an ooxml preset shape with custom geometry! Shape type lost!",
+                    aCustomGeometryIterator.Value.get<OUString>() != "ooxml-non-primitive");
+            // Without the fix, all shapes have ooxml-non-primitive type, and lost their
+            // real type (like triangle) with the textbox padding.
+        }
+        nCount++;
+    }
+    // Without the fix the count does not match.
+    CPPUNIT_ASSERT_EQUAL(17, nCount);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf69635, "tdf69635.docx")
 {
     xmlDocUniquePtr pXmlHeader1 = parseExport("word/header1.xml");

commit d11f08f6eb2cb18266a847cdd1025b32abec94ad

tdf#143028 DOCX: fix corrupt export of shape "bracePair" etc.
Regression from commit 86181f54e25a00e3e126f669e0cb12437e8df8b4
(tdf#92525 tdf#142398: fix export of simple custom shapes)

Missing property in the shape export property list
caused an exception. The return value false led to writing
the shape transformation again, resulting a corrupt DOCX file.
Optional values have been introduced and if one of the
required value is unset, writing of the transformation
happens only once.

Thanks to Regina Henschel for reporting the problem.

Change-Id: Ieae69bb1d2629fdbb91a84325cb100f0ad9d3e12
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117763
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

include/oox/export/DMLPresetShapeExport.hxx
@@ -86,9 +86,9 @@ public:
 private:
     struct AdjustmentPointValueBase
     {
-        double nMaxVal;
-        double nMinVal;
-        double nCurrVal;
+        std::optional<double> nMaxVal;
+        std::optional<double> nMinVal;
+        std::optional<double> nCurrVal;
     };
 
     typedef AdjustmentPointValueBase RadiusAdjustmentValue;

oox/source/export/DMLPresetShapeExport.cxx
@@ -131,15 +131,22 @@ DMLPresetShapeExporter::RadiusAdjustmentValue
 DMLPresetShapeExporter::GetAdjustmentPointRadiusValue(sal_Int32 nPoint)
 {
     RadiusAdjustmentValue aRet;
-    auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
-                       .get<EnhancedCustomShapeParameterPair>();
-    aRet.nMinVal = GetHandleValueOfModificationPoint(nPoint, u"RadiusRangeMinimum")
-                       .get<EnhancedCustomShapeParameter>()
-                       .Value.get<double>();
-    aRet.nMaxVal = GetHandleValueOfModificationPoint(nPoint, u"RadiusRangeMaximum")
-                       .get<EnhancedCustomShapeParameter>()
-                       .Value.get<double>();
-    aRet.nCurrVal = GetAdjustmentValues()[aValPos.First.Value.get<long>()].Value.get<double>();
+    try
+    {
+        auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
+                           .get<EnhancedCustomShapeParameterPair>();
+        aRet.nMinVal = GetHandleValueOfModificationPoint(nPoint, u"RadiusRangeMinimum")
+                           .get<EnhancedCustomShapeParameter>()
+                           .Value.get<double>();
+        aRet.nMaxVal = GetHandleValueOfModificationPoint(nPoint, u"RadiusRangeMaximum")
+                           .get<EnhancedCustomShapeParameter>()
+                           .Value.get<double>();
+        aRet.nCurrVal = GetAdjustmentValues()[aValPos.First.Value.get<long>()].Value.get<double>();
+    }
+    catch (...)
+    {
+        // Do nothing.
+    }
     return aRet;
 };
 
@@ -147,11 +154,18 @@ DMLPresetShapeExporter::AngleAdjustmentValue
 DMLPresetShapeExporter::GetAdjustmentPointAngleValue(sal_Int32 nPoint)
 {
     AngleAdjustmentValue aRet;
-    auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
-                       .get<EnhancedCustomShapeParameterPair>();
-    aRet.nMinVal = 0;
-    aRet.nMaxVal = 360;
-    aRet.nCurrVal = GetAdjustmentValues()[aValPos.Second.Value.get<long>()].Value.get<double>();
+    try
+    {
+        auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
+                           .get<EnhancedCustomShapeParameterPair>();
+        aRet.nMinVal = 0;
+        aRet.nMaxVal = 360;
+        aRet.nCurrVal = GetAdjustmentValues()[aValPos.Second.Value.get<long>()].Value.get<double>();
+    }
+    catch (...)
+    {
+        // Do nothing.
+    }
     return aRet;
 };
 
@@ -159,15 +173,22 @@ DMLPresetShapeExporter::XAdjustmentValue
 DMLPresetShapeExporter::GetAdjustmentPointXValue(sal_Int32 nPoint)
 {
     XAdjustmentValue aRet;
-    auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
-                       .get<EnhancedCustomShapeParameterPair>();
-    aRet.nMinVal = GetHandleValueOfModificationPoint(nPoint, u"RangeXMinimum")
-                       .get<EnhancedCustomShapeParameter>()
-                       .Value.get<double>();
-    aRet.nMaxVal = GetHandleValueOfModificationPoint(nPoint, u"RangeXMaximum")
-                       .get<EnhancedCustomShapeParameter>()
-                       .Value.get<double>();
-    aRet.nCurrVal = GetAdjustmentValues()[aValPos.First.Value.get<long>()].Value.get<double>();
+    try
+    {
+        auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
+                           .get<EnhancedCustomShapeParameterPair>();
+        aRet.nMinVal = GetHandleValueOfModificationPoint(nPoint, u"RangeXMinimum")
+                           .get<EnhancedCustomShapeParameter>()
+                           .Value.get<double>();
+        aRet.nMaxVal = GetHandleValueOfModificationPoint(nPoint, u"RangeXMaximum")
+                           .get<EnhancedCustomShapeParameter>()
+                           .Value.get<double>();
+        aRet.nCurrVal = GetAdjustmentValues()[aValPos.First.Value.get<long>()].Value.get<double>();
+    }
+    catch (...)
+    {
+        // Do nothing.
+    }
     return aRet;
 };
 
@@ -175,15 +196,22 @@ DMLPresetShapeExporter::YAdjustmentValue
 DMLPresetShapeExporter::GetAdjustmentPointYValue(sal_Int32 nPoint)
 {
     YAdjustmentValue aRet;
-    auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
-                       .get<EnhancedCustomShapeParameterPair>();
-    aRet.nMinVal = GetHandleValueOfModificationPoint(nPoint, u"RangeYMinimum")
-                       .get<EnhancedCustomShapeParameter>()
-                       .Value.get<double>();
-    aRet.nMaxVal = GetHandleValueOfModificationPoint(nPoint, u"RangeYMinimum")
-                       .get<EnhancedCustomShapeParameter>()
-                       .Value.get<double>();
-    aRet.nCurrVal = GetAdjustmentValues()[aValPos.Second.Value.get<long>()].Value.get<double>();
+    try
+    {
+        auto aValPos = GetHandleValueOfModificationPoint(nPoint, u"Position")
+                           .get<EnhancedCustomShapeParameterPair>();
+        aRet.nMinVal = GetHandleValueOfModificationPoint(nPoint, u"RangeYMinimum")
+                           .get<EnhancedCustomShapeParameter>()
+                           .Value.get<double>();
+        aRet.nMaxVal = GetHandleValueOfModificationPoint(nPoint, u"RangeXMaximum")
+                           .get<EnhancedCustomShapeParameter>()
+                           .Value.get<double>();
+        aRet.nCurrVal = GetAdjustmentValues()[aValPos.Second.Value.get<long>()].Value.get<double>();
+    }
+    catch (...)
+    {
+        // Do nothing.
+    }
     return aRet;
 };
 
@@ -393,29 +421,37 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "bevel")
         {
+            auto aPoint1 = GetAdjustmentPointXValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
+
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * 50000);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * 50000);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
         }
         if (sShapeType == "blockArc")
         {
-            m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
-                                                     false, false);
             auto aPointR = GetAdjustmentPointRadiusValue(0);
             auto aPointA = GetAdjustmentPointAngleValue(0);
+            if (!aPointA.nCurrVal.has_value() || !aPointA.nMaxVal.has_value()
+                || !aPointA.nMinVal.has_value() || !aPointR.nCurrVal.has_value()
+                || !aPointR.nMaxVal.has_value() || !aPointR.nMinVal.has_value())
+                return false;
+            m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
+                                                     false, false);
             tools::Long nVal1
-                = std::lround((aPointA.nCurrVal < 0 ? 360 + aPointA.nCurrVal : aPointA.nCurrVal)
-                              / (aPointA.nMaxVal - aPointA.nMinVal) * nConstOfMaxDegreeOf60th);
+                = std::lround((*aPointA.nCurrVal < 0 ? 360 + *aPointA.nCurrVal : *aPointA.nCurrVal)
+                              / (*aPointA.nMaxVal - *aPointA.nMinVal) * nConstOfMaxDegreeOf60th);
             tools::Long nVal2 = std::lround(
-                (aPointA.nCurrVal > 180 ? 360 - aPointA.nCurrVal : 180 - aPointA.nCurrVal)
-                / (aPointA.nMaxVal - aPointA.nMinVal) * nConstOfMaxDegreeOf60th);
+                (*aPointA.nCurrVal > 180 ? 360 - *aPointA.nCurrVal : 180 - *aPointA.nCurrVal)
+                / (*aPointA.nMaxVal - *aPointA.nMinVal) * nConstOfMaxDegreeOf60th);
             tools::Long nVal3 = std::lround(
-                50000 - (aPointR.nCurrVal / (aPointR.nMaxVal - aPointR.nMinVal) * 50000));
+                50000 - (*aPointR.nCurrVal / (*aPointR.nMaxVal - *aPointR.nMinVal) * 50000));
             return StartAVListWriting()
                    && WriteAV(u"adj1", OUString(u"val " + OUString::number(nVal1)))
                    && WriteAV(u"adj2", OUString(u"val " + OUString::number(nVal2)))
@@ -439,22 +475,30 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "bracePair")
         {
+            auto aPoint1 = GetAdjustmentPointYValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * 25000);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * 25000);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
         }
         if (sShapeType == "bracketPair")
         {
+            auto aPoint1 = GetAdjustmentPointYValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointYValue(0);
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * 50000);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * 50000);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
@@ -811,13 +855,17 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "hexagon")
         {
+            auto aPoint1 = GetAdjustmentPointXValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
             tools::Long nMaxVal = 50000 * m_xShape->getSize().Width
                                   / std::min(m_xShape->getSize().Width, m_xShape->getSize().Height);
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * nMaxVal);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * nMaxVal);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && WriteAV(u"vf", OUString(u"val " + OUString::number(115470)))
@@ -960,24 +1008,32 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "octagon")
         {
+            auto aPoint1 = GetAdjustmentPointXValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * 50000);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * 50000);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
         }
         if (sShapeType == "parallelogram")
         {
+            auto aPoint1 = GetAdjustmentPointXValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
             tools::Long nMaxVal = 100000 * m_xShape->getSize().Width
                                   / std::min(m_xShape->getSize().Width, m_xShape->getSize().Height);
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * nMaxVal);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * nMaxVal);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
@@ -1009,11 +1065,15 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "plus")
         {
+            auto aPoint1 = GetAdjustmentPointXValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * 50000);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * 50000);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
@@ -1080,19 +1140,28 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "roundRect")
         {
-            m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
-                                                     false, false);
             tools::Long nVal1 = 0;
             if (m_xShape->getSize().Width >= m_xShape->getSize().Height)
             {
                 auto aPointX = GetAdjustmentPointXValue(0);
-                nVal1 = std::lround(aPointX.nCurrVal / (aPointX.nMaxVal - aPointX.nMinVal) * 50000);
+                if (!aPointX.nCurrVal.has_value() || !aPointX.nMaxVal.has_value()
+                    || !aPointX.nMinVal.has_value())
+                    return false;
+                nVal1 = std::lround(*aPointX.nCurrVal / (*aPointX.nMaxVal - *aPointX.nMinVal)
+                                    * 50000);
             }
             else
             {
                 auto aPointY = GetAdjustmentPointYValue(0);
-                nVal1 = std::lround(aPointY.nCurrVal / (aPointY.nMaxVal - aPointY.nMinVal) * 50000);
+                if (!aPointY.nCurrVal.has_value() || !aPointY.nMaxVal.has_value()
+                    || !aPointY.nMinVal.has_value())
+                    return false;
+                nVal1 = std::lround(*aPointY.nCurrVal / (*aPointY.nMaxVal - *aPointY.nMinVal)
+                                    * 50000);
             }
+
+            m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
+                                                     false, false);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();
@@ -1214,12 +1283,16 @@ bool DMLPresetShapeExporter::WriteShapeWithAVlist()
         }
         if (sShapeType == "triangle")
         {
+            auto aPoint1 = GetAdjustmentPointXValue(0);
+            if (!aPoint1.nCurrVal.has_value() || !aPoint1.nMaxVal.has_value()
+                || !aPoint1.nMinVal.has_value())
+                return false;
+
             m_pDMLexporter->WriteShapeTransformation(m_xShape, XML_a, IsXFlipped(), IsYFlipped(),
                                                      false, false);
-            auto aPoint1 = GetAdjustmentPointXValue(0);
             tools::Long nMaxVal = 100000;
             tools::Long nVal1
-                = std::lround(aPoint1.nCurrVal / (aPoint1.nMaxVal - aPoint1.nMinVal) * nMaxVal);
+                = std::lround(*aPoint1.nCurrVal / (*aPoint1.nMaxVal - *aPoint1.nMinVal) * nMaxVal);
             return StartAVListWriting()
                    && WriteAV(u"adj", OUString(u"val " + OUString::number(nVal1)))
                    && EndAVListWriting();

sw/qa/extras/ooxmlexport/ooxmlexport15.cxx
@@ -919,6 +919,16 @@ DECLARE_OOXMLEXPORT_TEST(TestTdf132483, "tdf132483.docx")
         text::RelOrientation::PAGE_FRAME , nHRelPos);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(TestTdf143028, "fail_bracePair.odt")
+{
+    auto pExportXml = parseExport();
+
+    CPPUNIT_ASSERT_EQUAL(1, getXPathNode(
+        pExportXml, "/w:document/w:body/w:p/w:r/mc:AlternateContent/mc:Choice/w:drawing/wp:anchor/"
+                    "a:graphic/a:graphicData/wps:wsp/wps:spPr/a:xfrm")->nodesetval->nodeNr);
+
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testRelativeAnchorHeightFromBottomMarginNoFooter,
                          "tdf133070_testRelativeAnchorHeightFromBottomMarginNoFooter.docx")
 {