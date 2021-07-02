LemmingAvalanche/libreoffice-repaired

commit 26c2e22962b484ad51f88ccdbdef9124e2bff15c

tdf#111903 tdf#137152 PPTX export: fix placeholders
Empty placeholders were exported as white empty
custom shapes, losing their visibility and usability.

Note: export of properties hasn't been implemented, yet.

Change-Id: Ie8bd6a611f5fb43bcaa55f6b2f5b07daf731b163
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114331
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -1439,13 +1439,22 @@ ShapeExport& PowerPointShapeExport::WritePageShape(const Reference< XShape >& xS
 bool PowerPointShapeExport::WritePlaceholder(const Reference< XShape >& xShape, PlaceholderType ePlaceholder, bool bMaster)
 {
     SAL_INFO("sd.eppt", "WritePlaceholder " << bMaster << " " << ShapeExport::NonEmptyText(xShape));
-    if (bMaster && ShapeExport::NonEmptyText(xShape))
+    if (!xShape)
+        return false;
+    try
     {
-        WritePlaceholderShape(xShape, ePlaceholder);
+        Reference<XPropertySet> xShapeProps(xShape, UNO_QUERY);
+        if (xShapeProps->getPropertyValue("IsPresentationObject").get<bool>())
+        {
+            WritePlaceholderShape(xShape, ePlaceholder);
 
-        return true;
+            return true;
+        }
     }
-
+    catch (Exception&)
+    {
+        return false;
+    }
     return false;
 }
 

sd/qa/unit/export-tests-ooxml1.cxx
@@ -69,6 +69,7 @@ public:
     void testN828390_4();
     void testN828390_5();
     void testFdo71961();
+    void testLostPlaceholders();
     void testN828390();
     void testBnc880763();
     void testBnc862510_5();
@@ -122,6 +123,7 @@ public:
     CPPUNIT_TEST(testN828390_4);
     CPPUNIT_TEST(testN828390_5);
     CPPUNIT_TEST(testFdo71961);
+    CPPUNIT_TEST(testLostPlaceholders);
     CPPUNIT_TEST(testN828390);
     CPPUNIT_TEST(testBnc880763);
     CPPUNIT_TEST(testBnc862510_5);
@@ -370,6 +372,37 @@ void SdOOXMLExportTest1::testN828390_5()
     xDocShRef->DoClose();
 }
 
+void SdOOXMLExportTest1::testLostPlaceholders()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/LostPlaceholder.odp"), ODP);
+    CPPUNIT_ASSERT(xDocShRef.is());
+
+    xDocShRef = saveAndReload( xDocShRef.get(), PPTX );
+    CPPUNIT_ASSERT(xDocShRef.is());
+
+    auto pDoc = xDocShRef->GetDoc();
+    CPPUNIT_ASSERT(pDoc);
+    auto pPage = pDoc->GetPage(1);
+    CPPUNIT_ASSERT(pPage);
+    auto pObj = pPage->GetObj(1);
+    CPPUNIT_ASSERT(pObj);
+    uno::Reference<drawing::XShape> xShp (pObj->getUnoShape(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xShp);
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong ShapeType!", OUString(u"com.sun.star.presentation.OutlinerShape"), xShp->getShapeType());
+    uno::Reference<beans::XPropertySet> xShpProps(xShp, uno::UNO_QUERY);
+    // Without the fix in place there will be the following error:
+    // Expected: com.sun.star.presentation.OutlinerShape
+    // Actual: com.sun.star.drawing.CustomShape
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("It must be a placeholder!", true, xShpProps->getPropertyValue("IsPresentationObject").get<bool>());
+    // Without the fix in place this will the following:
+    // Expected: true
+    // Actual: false
+
+    xDocShRef->DoClose();
+}
+
 void SdOOXMLExportTest1::testFdo71961()
 {
     ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/fdo71961.odp"), ODP);
@@ -379,7 +412,7 @@ void SdOOXMLExportTest1::testFdo71961()
 
     // Export to .pptx changes all text frames to custom shape objects, which obey TextWordWrap property
     // (which is false for text frames otherwise and is ignored). Check that frames that should wrap still do.
-    SdrObjCustomShape *pTxtObj = dynamic_cast<SdrObjCustomShape *>( pPage->GetObj( 1 ));
+    auto  pTxtObj = pPage->GetObj( 1 );
     CPPUNIT_ASSERT_MESSAGE( "no text object", pTxtObj != nullptr);
     CPPUNIT_ASSERT_EQUAL( OUString( "Text to be always wrapped" ), pTxtObj->GetOutlinerParaObject()->GetTextObject().GetText(0));
     CPPUNIT_ASSERT_EQUAL( true, pTxtObj->GetMergedItem(SDRATTR_TEXT_WORDWRAP).GetValue());

commit 1f357a5a6fd8a1b14eaf8466c755e0b4a511c2cd

tdf#142235 PPTX export: fix styles with placeholders
inserting icons and alignment.

Regression from commit 26c2e22962b484ad51f88ccdbdef9124e2bff15c
(tdf#111903 tdf#137152 PPTX export: fix placeholders).

Note: in the unit test document, check fixed vertical position
of the second text boxes of Slide 2–4.

Change-Id: I3c649db69f94a2e9f49ae7aa11d9cd9d9f6d80d7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116828
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/drawingml.cxx
@@ -3081,8 +3081,7 @@ void DrawingML::WriteText(const Reference<XInterface>& rXIface, bool bBodyPr, bo
     const char* sVerticalAlignment = nullptr;
     if (GetProperty(rXPropSet, "TextVerticalAdjust"))
         mAny >>= eVerticalAlignment;
-    if( eVerticalAlignment != TextVerticalAdjust_TOP )
-        sVerticalAlignment = GetTextVerticalAdjust(eVerticalAlignment);
+    sVerticalAlignment = GetTextVerticalAdjust(eVerticalAlignment);
 
     const char* sWritingMode = nullptr;
     bool bVertical = false;

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -1568,14 +1568,25 @@ ShapeExport& PowerPointShapeExport::WritePlaceholderShape(const Reference< XShap
     SAL_INFO("sd.eppt", "write placeholder " << pType);
     if (bUsePlaceholderIndex)
     {
-        mpFS->singleElementNS(
-            XML_p, XML_ph, XML_type, pType, XML_idx,
-            OString::number(
-                static_cast<PowerPointExport*>(GetFB())->CreateNewPlaceholderIndex(xShape)));
+        if ((mePageType == PageType::LAYOUT || mePageType == PageType::NORMAL)
+            && ePlaceholder == Outliner)
+            mpFS->singleElementNS(
+                XML_p, XML_ph, XML_idx,
+                OString::number(
+                    static_cast<PowerPointExport*>(GetFB())->CreateNewPlaceholderIndex(xShape)));
+        else
+            mpFS->singleElementNS(
+                XML_p, XML_ph, XML_type, pType, XML_idx,
+                OString::number(
+                    static_cast<PowerPointExport*>(GetFB())->CreateNewPlaceholderIndex(xShape)));
     }
     else
     {
-        mpFS->singleElementNS(XML_p, XML_ph, XML_type, pType);
+        if ((mePageType == PageType::LAYOUT || mePageType == PageType::NORMAL)
+            && ePlaceholder == Outliner)
+            mpFS->singleElementNS(XML_p, XML_ph);
+        else
+            mpFS->singleElementNS(XML_p, XML_ph, XML_type, pType);
     }
     mpFS->endElementNS(XML_p, XML_nvPr);
     mpFS->endElementNS(XML_p, XML_nvSpPr);

sd/qa/unit/export-tests-ooxml3.cxx
@@ -116,6 +116,7 @@ public:
     void testTdf125560_textDeflate();
     void testTdf125560_textInflateTop();
     void testTdf96061_textHighlight();
+    void testTdf142235_TestPlaceholderTextAlignment();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest3);
 
@@ -184,6 +185,7 @@ public:
     CPPUNIT_TEST(testTdf125560_textDeflate);
     CPPUNIT_TEST(testTdf125560_textInflateTop);
     CPPUNIT_TEST(testTdf96061_textHighlight);
+    CPPUNIT_TEST(testTdf142235_TestPlaceholderTextAlignment);
     CPPUNIT_TEST_SUITE_END();
 
     virtual void registerNamespaces(xmlXPathContextPtr& pXmlXPathCtx) override
@@ -1686,6 +1688,24 @@ void SdOOXMLExportTest3::testTdf125560_textInflateTop()
                 "/office:document-content/office:body/office:presentation/draw:page/"
                 "draw:custom-shape/draw:enhanced-geometry",
                 "type", "mso-spt164");
+}
+
+void SdOOXMLExportTest3::testTdf142235_TestPlaceholderTextAlignment()
+{
+    auto xDocShRef = loadURL(
+        m_directories.getURLFromSrc(u"sd/qa/unit/data/odp/placeholder-box-textalignment.odp"), ODP);
+
+    utl::TempFile tmpfile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tmpfile);
+    xDocShRef->DoClose();
+
+    xmlDocUniquePtr pXml1 = parseExport(tmpfile, "ppt/slides/slide2.xml");
+    xmlDocUniquePtr pXml2 = parseExport(tmpfile, "ppt/slides/slide3.xml");
+
+    // Without the fix in place many of these asserts failed, because alignment was bad.
+
+    assertXPath(pXml1, "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:bodyPr", "anchor", "t");
+    assertXPath(pXml2, "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:bodyPr", "anchor", "t");
 }
 
 void SdOOXMLExportTest3::testTdf96061_textHighlight()