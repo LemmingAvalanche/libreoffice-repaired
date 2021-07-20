LemmingAvalanche/libreoffice-repaired

commit 8ab1b18e8c947e8c994a91fa3a10f9dd385b71ff

Extend ToolBarManager to build weld::Toolbar
- used for SubToolBarController eg. framework shape toolbar popup
present in the sidebar fontwork panel
- add separate implementation for vcl and weld based ToolBarManager
- provide basic functionality for weld based (just inserting standard items with icons
and controllers)
- not implemented: addons items merging etc.

Change-Id: I8e460f34abd512d70e95a1d484d728b7d809ce9d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113387
Tested-by: Jenkins
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

framework/inc/uielement/toolbarmanager.hxx
@@ -33,11 +33,14 @@
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/util/XURLTransformer.hpp>
 
+#include <framework/addonsoptions.hxx>
+
 #include <rtl/ustring.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <cppuhelper/interfacecontainer.hxx>
 
 #include <tools/link.hxx>
+#include <vcl/weld.hxx>
 #include <vcl/window.hxx>
 #include <vcl/timer.hxx>
 #include <vcl/toolbox.hxx>
@@ -50,6 +53,55 @@ class Menu;
 
 namespace framework
 {
+
+class ToolBarManager;
+
+class ToolBarManagerImpl
+{
+public:
+    virtual ~ToolBarManagerImpl() = default;
+    virtual void Init() = 0;
+    virtual void Destroy() = 0;
+    virtual css::uno::Reference<css::awt::XWindow> GetInterface() = 0;
+    virtual css::uno::Reference<css::frame::XStatusListener> CreateToolBoxController(
+                            const css::uno::Reference<css::frame::XFrame>& rFrame,
+                            ToolBoxItemId nId,
+                            const OUString& aCommandURL ) = 0;
+    virtual void InsertItem(ToolBoxItemId nId,
+                            const OUString& rString,
+                            const OUString& rCommandURL,
+                            const OUString& rTooltip,
+                            const OUString& rLabel,
+                            ToolBoxItemBits nItemBits) = 0;
+    virtual void InsertSeparator() = 0;
+    virtual void InsertSpace() = 0;
+    virtual void InsertBreak() = 0;
+    virtual ToolBoxItemId GetItemId(sal_uInt16 nPos) = 0;
+    virtual ToolBoxItemId GetCurItemId() = 0;
+    virtual OUString GetItemCommand(ToolBoxItemId nId) = 0;
+    virtual sal_uInt16 GetItemCount() = 0;
+    virtual void SetItemCheckable(ToolBoxItemId nId) = 0;
+    virtual void HideItem(ToolBoxItemId nId, const OUString& rCommandURL) = 0;
+    virtual bool IsItemVisible(ToolBoxItemId nId, const OUString& rCommandURL) = 0;
+    virtual void Clear() = 0;
+    virtual void SetName(const OUString& rName) = 0;
+    virtual void SetHelpId(const OString& rHelpId) = 0;
+    virtual bool WillUsePopupMode() = 0;
+    virtual bool IsReallyVisible() = 0;
+    virtual void SetIconSize(ToolBoxButtonSize eSize) = 0;
+    virtual vcl::ImageType GetImageSize() = 0;
+    virtual void ConnectCallbacks(ToolBarManager* pManager) = 0;
+    virtual void SetMenuType(ToolBoxMenuType eType) = 0;
+    virtual void MergeToolbar(ToolBoxItemId nItemId,
+                              const OUString& rModuleIdentifier,
+                              CommandToInfoMap& rCommandMap,
+                              MergeToolbarInstruction& rInstruction) = 0;
+    virtual void SetItemImage(ToolBoxItemId nId,
+                              const OUString& rCommandURL,
+                              const Image& rImage) = 0;
+    virtual void UpdateSize() = 0;
+    virtual void SetItemWindow(ToolBoxItemId nItemId, vcl::Window* pNewWindow) = 0;
+};
 
 typedef ::cppu::WeakImplHelper<
            css::frame::XFrameActionListener,
@@ -64,6 +116,11 @@ class ToolBarManager final : public ToolbarManager_Base
                         const css::uno::Reference< css::frame::XFrame >& rFrame,
                         const OUString& rResourceName,
                         ToolBox* pToolBar );
+        ToolBarManager( const css::uno::Reference< css::uno::XComponentContext >& rxContext,
+                        const css::uno::Reference< css::frame::XFrame >& rFrame,
+                        const OUString& rResourceName,
+                        weld::Toolbar* pToolBar,
+                        weld::Builder* pBuilder );
         virtual ~ToolBarManager() override;
 
         ToolBox* GetToolBar() const;
@@ -100,6 +157,13 @@ class ToolBarManager final : public ToolbarManager_Base
             EXEC_CMD_DOCKALLTOOLBARS
         };
 
+        enum ClickAction
+        {
+            Click,
+            DblClick,
+            Execute
+        };
+
         struct ExecuteInfo
         {
             OUString                                            aToolbarResName;
@@ -108,8 +172,10 @@ class ToolBarManager final : public ToolbarManager_Base
             css::uno::Reference< css::awt::XWindow >            xWindow;
         };
 
-    private:
-        DECL_LINK(Click, ToolBox *, void);
+    public:
+        void OnClick(bool bUseExecute = false);
+        void OnDropdownClick(bool bCreatePopupWindow = true);
+
         DECL_LINK(DropdownClick, ToolBox *, void);
         DECL_LINK(DoubleClick, ToolBox *, void);
         DECL_LINK(Select, ToolBox *, void);
@@ -124,6 +190,8 @@ class ToolBarManager final : public ToolbarManager_Base
         DECL_LINK( OverflowEventListener, VclWindowEvent&, void );
         DECL_STATIC_LINK( ToolBarManager, ExecuteHdl_Impl, void*, void );
 
+    private:
+        void Init();
         void AddCustomizeMenuItems(ToolBox const * pToolBar);
         void InitImageManager();
         void RemoveControllers();
@@ -137,7 +205,7 @@ class ToolBarManager final : public ToolbarManager_Base
         ToolBoxItemBits ConvertStyleToToolboxItemBits( sal_Int32 nStyle );
         css::uno::Reference< css::frame::XModel > GetModelFromFrame() const;
         bool IsPluginMode() const;
-        void HandleClick(void ( SAL_CALL css::frame::XToolbarController::*_pClick )(  ));
+        void HandleClick(ClickAction eAction);
         void setToolBarImage(const Image& _aImage,const CommandToInfoMap::const_iterator& _pIter);
         void impl_elementChanged(bool _bRemove,const css::ui::ConfigurationEvent& Event );
 
@@ -146,12 +214,12 @@ class ToolBarManager final : public ToolbarManager_Base
         typedef std::unordered_map<OUString, SubToolBarControllerVector>                                                SubToolBarToSubToolBarControllerMap;
 
         bool m_bDisposed : 1,
-             m_bAddedToTaskPaneList : 1,
              m_bFrameActionRegistered : 1,
              m_bUpdateControllers : 1;
 
         sal_Int16 m_eSymbolSize;
 
+        std::unique_ptr<ToolBarManagerImpl>                          m_pImpl;
         VclPtr<ToolBox>                                              m_pToolBar;
 
         OUString                                                     m_aModuleIdentifier;

framework/UIConfig_startmodule.mk
@@ -14,6 +14,7 @@ $(eval $(call gb_UIConfig_add_menubarfiles,modules/StartModule,\
 ))
 
 $(eval $(call gb_UIConfig_add_uifiles,modules/StartModule,\
+	framework/uiconfig/startmodule/ui/managedtoolbar \
 	framework/uiconfig/startmodule/ui/subtoolbar \
 ))
 

framework/inc/uielement/toolbarwrapper.hxx
@@ -25,6 +25,13 @@
 #include <com/sun/star/ui/XUIFunctionListener.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
 
+namespace weld
+{
+    class Builder;
+    class Container;
+    class Toolbar;
+}
+
 namespace framework
 {
 
@@ -69,6 +76,10 @@ class ToolBarWrapper final : public css::ui::XUIFunctionListener,
 
         css::uno::Reference< css::lang::XComponent >            m_xToolBarManager;
         css::uno::Reference< css::uno::XComponentContext >      m_xContext;
+
+        std::unique_ptr<weld::Builder>                          m_xBuilder;
+        std::unique_ptr<weld::Container>                        m_xTopLevel;
+        std::unique_ptr<weld::Toolbar>                          m_xWeldedToolbar;
 };
 
 }

framework/source/fwe/classes/sfxhelperfunctions.cxx
@@ -24,6 +24,7 @@
 #include <svtools/statusbarcontroller.hxx>
 
 static pfunc_setToolBoxControllerCreator   pToolBoxControllerCreator   = nullptr;
+static pfunc_setWeldToolBoxControllerCreator pWeldToolBoxControllerCreator = nullptr;
 static pfunc_setStatusBarControllerCreator pStatusBarControllerCreator = nullptr;
 static pfunc_getRefreshToolbars            pRefreshToolbars            = nullptr;
 static pfunc_createDockingWindow           pCreateDockingWindow        = nullptr;
@@ -53,6 +54,28 @@ rtl::Reference<svt::ToolboxController> CreateToolBoxController( const Reference<
 
     if ( pFactory )
         return (*pFactory)( rFrame, pToolbox, nID, aCommandURL );
+    else
+        return nullptr;
+}
+
+pfunc_setWeldToolBoxControllerCreator SetWeldToolBoxControllerCreator( pfunc_setWeldToolBoxControllerCreator pSetWeldToolBoxControllerCreator )
+{
+    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+    pfunc_setWeldToolBoxControllerCreator pOldSetToolBoxControllerCreator = pWeldToolBoxControllerCreator;
+    pWeldToolBoxControllerCreator = pSetWeldToolBoxControllerCreator;
+    return pOldSetToolBoxControllerCreator;
+}
+
+css::uno::Reference<css::frame::XToolbarController> CreateWeldToolBoxController( const Reference< XFrame >& rFrame, weld::Toolbar* pToolbar, weld::Builder* pBuilder, const OUString& aCommandURL )
+{
+    pfunc_setWeldToolBoxControllerCreator pFactory = nullptr;
+    {
+        ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+        pFactory = pWeldToolBoxControllerCreator;
+    }
+
+    if ( pFactory )
+        return (*pFactory)( rFrame, pToolbar, pBuilder, aCommandURL );
     else
         return nullptr;
 }

framework/source/uielement/subtoolbarcontroller.cxx
@@ -46,6 +46,7 @@ namespace {
 
 class SubToolBarController : public ToolBarBase
 {
+    DECL_LINK(OnPopoverClose, weld::Popover&, void);
     OUString m_aSubTbName;
     OUString m_aLastCommand;
     css::uno::Reference< css::ui::XUIElement > m_xUIElement;
@@ -242,7 +243,6 @@ std::unique_ptr<WeldToolbarPopup> SubToolBarController::weldPopupWindow()
 
     // create element with factory
     static css::uno::WeakReference< css::ui::XUIElementFactoryManager > xWeakUIElementFactory;
-    css::uno::Reference< css::ui::XUIElement > xUIElement;
     css::uno::Reference< css::ui::XUIElementFactoryManager > xUIElementFactory = xWeakUIElementFactory;
     if ( !xUIElementFactory.is() )
     {
@@ -261,12 +261,16 @@ std::unique_ptr<WeldToolbarPopup> SubToolBarController::weldPopupWindow()
 
     try
     {
-        xUIElement = xUIElementFactory->createUIElement( "private:resource/toolbar/" + m_aSubTbName, aPropSeq );
+        m_xUIElement = xUIElementFactory->createUIElement( "private:resource/toolbar/" + m_aSubTbName, aPropSeq );
     }
     catch ( css::container::NoSuchElementException& )
     {}
     catch ( css::lang::IllegalArgumentException& )
     {}
+
+    weld::Popover* pPopover = dynamic_cast<weld::Popover*>(pPopup->getTopLevel());
+    if (pPopover)
+        pPopover->connect_closed(LINK(this, SubToolBarController, OnPopoverClose));
 
     return pPopup;
 }
@@ -496,6 +500,12 @@ void SubToolBarController::initialize( const css::uno::Sequence< css::uno::Any >
     }
 
     updateImage();
+}
+
+IMPL_LINK_NOARG(SubToolBarController, OnPopoverClose, weld::Popover&, void)
+{
+    disposeUIElement();
+    m_xUIElement = nullptr;
 }
 
 void SubToolBarController::dispose()

framework/source/uielement/toolbarwrapper.cxx
@@ -28,6 +28,7 @@
 
 #include <vcl/svapp.hxx>
 #include <vcl/toolbox.hxx>
+#include <vcl/weldutils.hxx>
 
 using namespace com::sun::star;
 using namespace com::sun::star::uno;
@@ -49,6 +50,9 @@ ToolBarWrapper::ToolBarWrapper( const Reference< XComponentContext >& rxContext 
 
 ToolBarWrapper::~ToolBarWrapper()
 {
+    m_xWeldedToolbar.reset(nullptr);
+    m_xTopLevel.reset(nullptr);
+    m_xBuilder.reset(nullptr);
 }
 
 // XInterface
@@ -148,20 +152,34 @@ void SAL_CALL ToolBarWrapper::initialize( const Sequence< Any >& aArguments )
             m_xToolBarManager = pToolBarManager;
             pToolBar->WillUsePopupMode( bPopupMode );
         }
+        else if (weld::TransportAsXWindow* pTunnel = dynamic_cast<weld::TransportAsXWindow*>(xParentWindow.get()))
+        {
+            m_xBuilder.reset(Application::CreateBuilder(pTunnel->getWidget(), "modules/StartModule/ui/managedtoolbar.ui"));
+            m_xTopLevel = m_xBuilder->weld_container("toolbarcontainer");
+            m_xWeldedToolbar = m_xBuilder->weld_toolbar("managedtoolbar");
+            if ( m_xWeldedToolbar )
+            {
+                pToolBarManager = new ToolBarManager( m_xContext, xFrame, m_aResourceURL, m_xWeldedToolbar.get(), m_xBuilder.get() );
+                m_xToolBarManager = pToolBarManager;
+            }
+        }
     }
 
     try
     {
         m_xConfigData = m_xConfigSource->getSettings( m_aResourceURL, false );
-        if ( m_xConfigData.is() && pToolBar && pToolBarManager )
+        if ( m_xConfigData.is() && (pToolBar || m_xWeldedToolbar) && pToolBarManager )
         {
             // Fill toolbar with container contents
             pToolBarManager->FillToolbar( m_xConfigData );
-            pToolBar->EnableCustomize();
-            ::Size aActSize( pToolBar->GetSizePixel() );
-            ::Size aSize( pToolBar->CalcWindowSizePixel() );
-            aSize.setWidth( aActSize.Width() );
-            pToolBar->SetOutputSizePixel( aSize );
+            if (pToolBar)
+            {
+                pToolBar->EnableCustomize();
+                ::Size aActSize( pToolBar->GetSizePixel() );
+                ::Size aSize( pToolBar->CalcWindowSizePixel() );
+                aSize.setWidth( aActSize.Width() );
+                pToolBar->SetOutputSizePixel( aSize );
+            }
         }
     }
     catch ( const NoSuchElementException& )

framework/uiconfig/startmodule/ui/managedtoolbar.ui
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.20.4 -->
+<interface domain="sfx">
+  <requires lib="gtk+" version="3.20"/>
+  <object class="GtkBox" id="toolbarcontainer">
+    <property name="visible">True</property>
+    <property name="can_focus">False</property>
+    <property name="orientation">vertical</property>
+    <child>
+      <object class="GtkToolbar" id="managedtoolbar">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+      </object>
+      <packing>
+        <property name="expand">True</property>
+        <property name="fill">True</property>
+        <property name="position">0</property>
+      </packing>
+    </child>
+  </object>
+</interface>

commit 084a15dc24689054a8a5ea64f53d0f63c74d573f

framework: fix multiple-free in VclToolBarManager::Destroy()
Multiple ImplToolItem with mnId 1000 are inserted in the same ToolBox
and the problem is that only the first one gets its mpUserData set,
because setting and getting that is based on the assumption that mnId is
unique per container.

So in Destroy() for every item with mnId 1000 the same AddonsParams
object is deleted.

The problem is that in ToolBarManager::FillToolbar() the local nItemId
is copied, so its value never changes when iterating the loop because
only its copy is incremented.

(regression from 8ab1b18e8c947e8c994a91fa3a10f9dd385b71ff)

Change-Id: Ica47051f9447c2b9d1981f8373d754874f0f31b0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119228
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

framework/inc/uielement/toolbarmanager.hxx
@@ -92,7 +92,7 @@ public:
     virtual vcl::ImageType GetImageSize() = 0;
     virtual void ConnectCallbacks(ToolBarManager* pManager) = 0;
     virtual void SetMenuType(ToolBoxMenuType eType) = 0;
-    virtual void MergeToolbar(ToolBoxItemId nItemId,
+    virtual void MergeToolbar(ToolBoxItemId & rItemId,
                               const OUString& rModuleIdentifier,
                               CommandToInfoMap& rCommandMap,
                               MergeToolbarInstruction& rInstruction) = 0;

framework/source/uielement/toolbarmanager.cxx
@@ -333,7 +333,7 @@ public:
         m_pToolBar->SetMenuType( eType );
     }
 
-    virtual void MergeToolbar(ToolBoxItemId nItemId,
+    virtual void MergeToolbar(ToolBoxItemId & rItemId,
                               const OUString& rModuleIdentifier,
                               CommandToInfoMap& rCommandMap,
                               MergeToolbarInstruction& rInstruction) override
@@ -349,7 +349,7 @@ public:
         {
             ToolBarMerger::ProcessMergeOperation( m_pToolBar,
                                                     aRefPoint.nPos,
-                                                    nItemId,
+                                                    rItemId,
                                                     rCommandMap,
                                                     rModuleIdentifier,
                                                     rInstruction.aMergeCommand,
@@ -359,7 +359,7 @@ public:
         else
         {
             ToolBarMerger::ProcessMergeFallback( m_pToolBar,
-                                                    nItemId,
+                                                    rItemId,
                                                     rCommandMap,
                                                     rModuleIdentifier,
                                                     rInstruction.aMergeCommand,
@@ -521,7 +521,7 @@ public:
 
     virtual void SetMenuType(ToolBoxMenuType /*eType*/) override {}
 
-    virtual void MergeToolbar(ToolBoxItemId /*nItemId*/,
+    virtual void MergeToolbar(ToolBoxItemId & /*rItemId*/,
                               const OUString& /*rModuleIdentifier*/,
                               CommandToInfoMap& /*rCommandMap*/,
                               MergeToolbarInstruction& /*rInstruction*/) override {}

framework/source/uielement/toolbarmerger.cxx
@@ -623,6 +623,8 @@ rtl::Reference<::cppu::OWeakObject> ToolBarMerger::CreateController(
 
 void ToolBarMerger::CreateToolbarItem( ToolBox* pToolbar, ToolBox::ImplToolItems::size_type nPos, ToolBoxItemId nItemId, const AddonToolbarItem& rItem )
 {
+    assert(pToolbar->GetItemData(nItemId) == nullptr); // that future would contain a double free
+
     pToolbar->InsertItem( nItemId, rItem.aLabel, ToolBoxItemBits::NONE, nPos );
     pToolbar->SetItemCommand( nItemId, rItem.aCommandURL );
     pToolbar->SetQuickHelpText( nItemId, rItem.aLabel );