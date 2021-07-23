LemmingAvalanche/libreoffice-repaired

commit ba7017a1af4dd562a8830343727a304d44988132

use officecfg to retrieve help system
Change-Id: I09f7a3a91ed63bab28c20984fa80d4d392e9d092
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119413
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svtools/source/config/helpopt.cxx
@@ -44,9 +44,8 @@ enum class HelpProperty
     ExtendedHelp    = 0,
     HelpTips        = 1,
     Locale          = 2,
-    System          = 3,
-    StyleSheet      = 4,
-    OfflineHelpPopUp = 5
+    StyleSheet      = 3,
+    OfflineHelpPopUp = 4
 };
 
 }
@@ -57,7 +56,6 @@ class SvtHelpOptions_Impl : public utl::ConfigItem
     bool            bHelpTips;
     bool            bOfflineHelpPopUp;
     OUString        aLocale;
-    OUString        aSystem;
     OUString        sHelpStyleSheet;
 
     static Sequence< OUString > const & GetPropertyNames();
@@ -77,7 +75,6 @@ public:
     bool            IsHelpTips() const                      { return bHelpTips; }
     void            SetOfflineHelpPopUp(bool b) { bOfflineHelpPopUp = b; SetModified();}
     bool            IsOfflineHelpPopUp() const { return bOfflineHelpPopUp;}
-    const OUString& GetSystem() const                       { return aSystem; }
 
     const OUString& GetHelpStyleSheet()const{return sHelpStyleSheet;}
     void            SetHelpStyleSheet(const OUString& rStyleSheet){sHelpStyleSheet = rStyleSheet; SetModified();}
@@ -168,9 +165,6 @@ void  SvtHelpOptions_Impl::Load(const uno::Sequence< OUString>& rPropertyNames)
                         aLocale = aTmpStr;
                         break;
 
-                    case HelpProperty::System:
-                        aSystem = aTmpStr;
-                        break;
                     case HelpProperty::StyleSheet:
                         sHelpStyleSheet = aTmpStr;
                     break;
@@ -216,9 +210,6 @@ void SvtHelpOptions_Impl::ImplCommit()
                 pValues[nProp] <<= aLocale;
                 break;
 
-            case HelpProperty::System:
-                pValues[nProp] <<= aSystem;
-                break;
             case HelpProperty::StyleSheet:
                 pValues[nProp] <<= sHelpStyleSheet;
             break;
@@ -285,11 +276,6 @@ void SvtHelpOptions::SetHelpTips( bool b )
 bool SvtHelpOptions::IsHelpTips() const
 {
     return pImpl->IsHelpTips();
-}
-
-OUString const & SvtHelpOptions::GetSystem() const
-{
-    return pImpl->GetSystem();
 }
 
 const OUString&   SvtHelpOptions::GetHelpStyleSheet()const

include/svtools/helpopt.hxx
@@ -43,8 +43,6 @@ public:
 
     const OUString& GetHelpStyleSheet()const;
     void            SetHelpStyleSheet(const OUString& rStyleSheet);
-
-    OUString const & GetSystem() const;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/appl/sfxhelp.cxx
@@ -279,7 +279,7 @@ void AppendConfigToken( OUStringBuffer& rURL, bool bQuestionMark )
     rURL.append("Language=");
     rURL.append(aLocaleStr);
     rURL.append("&System=");
-    rURL.append(SvtHelpOptions().GetSystem());
+    rURL.append(officecfg::Office::Common::Help::System::get());
     rURL.append("&Version=");
     rURL.append(utl::ConfigManager::getProductVersion());
 }

commit 8fe0db06ca88ac58d6b7029b1230fff6860728bf

Fix SvtHelpOptions_Impl::GetPropertyNames (svtools)
Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=ba7017a1af4dd562a8830343727a304d44988132
use officecfg to retrieve help system

Change-Id: Id42c5ee06d2231dc690a4f6f81eecddb0b1a2073
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119429
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins

svtools/source/config/helpopt.cxx
@@ -85,7 +85,6 @@ Sequence< OUString > const & SvtHelpOptions_Impl::GetPropertyNames()
         "ExtendedTip",
         "Tip",
         "Locale",
-        "System",
         "BuiltInHelpNotInstalledPopUp"
     };
 