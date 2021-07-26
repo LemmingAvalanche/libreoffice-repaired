LemmingAvalanche/libreoffice-repaired

commit f820141abfbeeb440abce5c9a8a0972f5713e469

Fix ignoring large twips values like MSO does (cp#1000087)
which was introduced in 900170229eb6d0beb31731642a3b59d8593a7082.

Since c8a0241626ce0b45c5bb033fe68a6fbd3429ecb7, only
convertTwipToMM100Unsigned() ignores large values, which presumably
was not the intention. At least commit message suggests so.

So, move the check back to convertTwipToMM100().

Change-Id: I17040f1987e24789b9de39a837d9f7ecaed520fb

writerfilter/source/dmapper/ConversionHelper.cxx
@@ -228,6 +228,10 @@ OUString ConvertMSFormatStringToSO(
 
 sal_Int32 convertTwipToMM100(sal_Int32 _t)
 {
+    // It appears that MSO handles large twip values specially, probably legacy 16bit handling,
+    // anything that's bigger than 32767 appears to be simply ignored.
+    if( _t >= 0x8000 )
+        return 0;
     return ::convertTwipToMm100( _t );
 }
 
@@ -235,11 +239,7 @@ sal_uInt32 convertTwipToMM100Unsigned(sal_Int32 _t)
 {
     if( _t < 0 )
         return 0;
-    // It appears that MSO handles large twip values specially, probably legacy 16bit handling,
-    // anything that's bigger than 32767 appears to be simply ignored.
-    if( _t >= 0x8000 )
-        return 0;
-    return ::convertTwipToMm100( _t );
+    return convertTwipToMM100( _t );
 }
 
 sal_Int32 convertEMUToMM100(sal_Int32 _t)

commit 262a9b9f604a595676dc7bdb0cdbca6a52c32226

tdf#142693 DOCX import: fix zeroing huge page sizes
which bigger than 2^15 twips (> ~57.8 cm).

Regression from commit f820141abfbeeb440abce5c9a8a0972f5713e469
"Fix ignoring large twips values like MSO does (cp#1000087)".

Note: Writer could export page size up to 600 cm × 600 cm,
and now it can import it back as well. It can even import
page sizes in kilometers, and display that, but there will
be other issues about its display. MS Word couldn't save
page size over 2^15, but can import, and display them till
up to 2^16 (that is about 116 cm).

Co-authored-by: Tibor Nagy (NISZ)

Change-Id: Icc1bd708d53036768a7be94cb1ebe18c1c4c8487
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118940
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/ConversionHelper.cxx
@@ -423,6 +423,11 @@ sal_Int32 convertTwipToMM100(sal_Int32 _t)
     return ::convertTwipToMm100( _t );
 }
 
+sal_Int32 convertTwipToMM100WithoutLimit(sal_Int32 _t)
+{
+    return ::convertTwipToMm100(_t);
+}
+
 double convertTwipToMM100Double(sal_Int32 _t)
 {
     // It appears that MSO handles large twip values specially, probably legacy 16bit handling,

writerfilter/source/dmapper/ConversionHelper.hxx
@@ -46,6 +46,7 @@ namespace writerfilter::dmapper::ConversionHelper{
     OUString ConvertMSFormatStringToSO(const OUString& rFormat, css::lang::Locale& rLocale, bool bHijri);
     // export just for test
     SAL_DLLPUBLIC_EXPORT sal_Int32 convertTwipToMM100(sal_Int32 _t);
+    SAL_DLLPUBLIC_EXPORT sal_Int32 convertTwipToMM100WithoutLimit(sal_Int32 _t);
     SAL_DLLPUBLIC_EXPORT double convertTwipToMM100Double(sal_Int32 _t);
     SAL_DLLPUBLIC_EXPORT sal_uInt32 convertTwipToMM100Unsigned(sal_Int32 _t);
     sal_Int16 convertTableJustification( sal_Int32 nIntValue );

writerfilter/source/dmapper/DomainMapper.cxx
@@ -625,7 +625,7 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
             break;
         case NS_ooxml::LN_CT_PageSz_h:
             {
-                sal_Int32 nHeight = ConversionHelper::convertTwipToMM100(nIntValue);
+                sal_Int32 nHeight = ConversionHelper::convertTwipToMM100WithoutLimit(nIntValue);
                 CT_PageSz.h = PaperInfo::sloppyFitPageDimension(nHeight);
             }
             break;
@@ -634,7 +634,7 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
             break;
         case NS_ooxml::LN_CT_PageSz_w:
             {
-                sal_Int32 nWidth = ConversionHelper::convertTwipToMM100(nIntValue);
+                sal_Int32 nWidth = ConversionHelper::convertTwipToMM100WithoutLimit(nIntValue);
                 CT_PageSz.w = PaperInfo::sloppyFitPageDimension(nWidth);
             }
             break;

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -1226,6 +1226,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf127741, "tdf127741.docx")
     CPPUNIT_ASSERT(visitedStyleName.equalsIgnoreAsciiCase("Visited Internet Link"));
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf142693_hugePaperSizeImport, "tdf142693_hugePaperSizeImport.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:sectPr/w:pgSz", "w", "90369");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:sectPr/w:pgSz", "h", "104372");
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf127925, "tdf127925.odt")
 {
     CPPUNIT_ASSERT_EQUAL(1, getPages());