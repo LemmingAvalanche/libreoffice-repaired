LemmingAvalanche/libreoffice-repaired

commit 61a6be3aa3b470e5da3ca7ad4b8d1a6155a6a07f

tdf#142304 a.o. Improve wrap margins in docx filters
LO uses the bounding box of the shape in case of type 'Parallel'. Word
uses in the corresponding wrap 'square' a box based on the full size
of the shape. That will be very different in some cases, e.g. for an
arc. And Word exchanges width and height in case of rotation angle in
[45°;135°[ and [225°; 315°[. To get the same appearance as in Word,
the wrap margins are suitable expanded on import.

Word puts the additional space needed for fat strokes into effectExtent
in case of wrap 'inline', so there is no need to add the half strokes
width in addition. Word determines the area for the shape depending
on rotation angle. Both are now considered. Total same appearance is
not possible because it would need negative vertical wrap margins,
which are currently faulty in LO, see tdf#141880.

Patch solves in addition tdf#142486, tdf#142305

The export to Word would require negative values in effectExtent in
some cases. They are allowed in OOXML but not supported in Word. My
idea is to switch to wrap mode 'Tight' if needed. But export of wrap
has so many bad parts, that it needs separate work and is not
included here.

Handling of border width for export of own frames is missing.

Unittest changes
----------------
testDmlTextshapeB and TestDmlTextshape in ooxmlexport6.cxx are set to
current values. Import and Export still have large errors with these
shapes and correct value from file is unknown. So an exact value is
pointless. Only the original problem needs to be still fixed, which
is the case.

testWpsOnly in ooxmlexport10.cxx. I have removed the test for
LeftMargin equals 0. The test makes no sense, because the original
file has distL=114300.

testTdf124600 in ooxmlimport2.cxx.
I have added a tolerance. It would fail with Expected: 2029,
Actual:2028, likely a rounding problem somewhere.

testTdf124600 in ooxmlimport2.cxx
Word refers to outer edge of the border for align='left', LO aligns

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -1377,7 +1377,7 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf85232)
     CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.LineShape"), xShapeDescriptor->getShapeType());
 
     // This was 2900: horizontal position of the line was incorrect, the 3 children were not connected visually.
-    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2267), xShape->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2265), xShape->getPosition().X);
 }
 
 CPPUNIT_TEST_FIXTURE(Test, testTdf95755)

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -266,13 +266,17 @@ CPPUNIT_TEST_FIXTURE(Test, testGroupShapeFontName)
 CPPUNIT_TEST_FIXTURE(Test, testTdf124600)
 {
     load(mpTestDocumentPath, "tdf124600.docx");
-    uno::Reference<drawing::XShape> xShape = getShape(1);
+    // uno::Reference<drawing::XShape> xShape = getShape(1);
     // Without the accompanying fix in place, this test would have failed with:
     // - Expected: 0
     // - Actual  : 318
     // i.e. the shape had an unexpected left margin, but not in Word.
-    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0),
-                         getProperty<sal_Int32>(xShape, "HoriOrientPosition"));
+    // Regina: LO needs a left margin to get the same rendering as Word, because Word aligns the
+    // shape with the outer edge of the border, but LibreOffice aligns with the snap rectangle.
+    // Expected: 0 is wrong. ToDo: The current margin is wrong and needs to be fixed. Then activate
+    // the test again with the correct margin.
+    // CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0),
+    //                     getProperty<sal_Int32>(xShape, "HoriOrientPosition"));
 
     // Make sure that "Shape 1 text" (anchored in the header) has the same left margin as the body
     // text.
@@ -283,7 +287,7 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf124600)
     // - Actual  : 1815
     // i.e. there was a >0 left margin on the text of the shape, resulting in incorrect horizontal
     // position.
-    CPPUNIT_ASSERT_EQUAL(aBodyTextLeft, aShapeTextLeft);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(aBodyTextLeft.toDouble(), aShapeTextLeft.toDouble(), 1.0);
 }
 
 CPPUNIT_TEST_FIXTURE(Test, testTdf120548)
@@ -303,7 +307,7 @@ CPPUNIT_TEST_FIXTURE(Test, test120551)
     // 'Expected: 430, Actual  : -2542'.
     // CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(430), nHoriOrientPosition);
     // File 140335EMU = 389,8Hmm
-    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(392), nHoriOrientPosition);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(390), nHoriOrientPosition);
 }
 
 CPPUNIT_TEST_FIXTURE(Test, testTdf111550)

writerfilter/qa/cppunittests/dmapper/GraphicImport.cxx
@@ -18,6 +18,8 @@
 #include <com/sun/star/container/XNamed.hpp>
 #include <com/sun/star/text/RelOrientation.hpp>
 #include <com/sun/star/text/VertOrientation.hpp>
+#include <com/sun/star/text/XTextViewCursorSupplier.hpp>
+#include <com/sun/star/view/XViewCursor.hpp>
 #include <com/sun/star/drawing/PointSequenceSequence.hpp>
 
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
@@ -54,6 +56,64 @@ void Test::tearDown()
 }
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/writerfilter/qa/cppunittests/dmapper/data/";
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf142305StrokeGlowMargin)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf142305StrokeGlowMargin.docx";
+    // The document has an arc with fat stroke and glow. Its bounding rectangle differs much
+    // from the snap rectangle. Error was, that the margins were not set in a way, that the shape
+    // would render similar to Word.
+    getComponent() = loadFromDesktop(aURL);
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    sal_Int32 nTopMargin = 0;
+    xShape->getPropertyValue("TopMargin") >>= nTopMargin;
+    // Without fix in place top margin was 0, so that the text comes near to the shape.
+    // The test would have failed with Expected: 838, Actual: 0
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(838), nTopMargin);
+    sal_Int32 nBottomMargin = 0;
+    // Without fix in place bottom margin was >0, so that the text was far from to the shape.
+    // The test would have failed with Expected: 0, Actual: 637
+    xShape->getPropertyValue("BottomMargin") >>= nBottomMargin;
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), nBottomMargin);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf142305SquareWrapMargin)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf142305SquareWrapMargin.docx";
+    getComponent() = loadFromDesktop(aURL);
+    uno::Reference<frame::XModel> xModel(getComponent(), uno::UNO_QUERY);
+    uno::Reference<text::XTextViewCursorSupplier> xTextViewCursorSupplier(
+        xModel->getCurrentController(), uno::UNO_QUERY_THROW);
+    uno::Reference<text::XTextViewCursor> xViewCursor(xTextViewCursorSupplier->getViewCursor());
+    xViewCursor->gotoStart(/*bExpand=*/false);
+    uno::Reference<view::XViewCursor> xCursor(xViewCursor, uno::UNO_QUERY);
+    xCursor->goDown(/*nCount=*/10, /*bExpand=*/false);
+    xViewCursor->goRight(/*nCount=*/1, /*bExpand=*/true);
+    OUString sText = xViewCursor->getString();
+    // Without fix in place, wrap was tight to the bounding box and not around the full shape as in
+    // Word. That results in different text at start of line, here "u" instead of expected "m".
+    CPPUNIT_ASSERT(sText.startsWith("m"));
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf142304GroupPosition)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf142304GroupPosition.docx";
+    getComponent() = loadFromDesktop(aURL);
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    sal_Int32 nVertPosition = 0;
+    xShape->getPropertyValue("VertOrientPosition") >>= nVertPosition;
+    // Without fix in place the group was shifted left and down
+    // The test would have failed with Expected: 2178, Actual: 2521
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2178), nVertPosition);
+    sal_Int32 nHoriPosition = 0;
+    // The test would have failed with Expected: 4304, Actual: 3874
+    xShape->getPropertyValue("HoriOrientPosition") >>= nHoriPosition;
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(4304), nHoriPosition);
+}
 
 CPPUNIT_TEST_FIXTURE(Test, testTdf141540ChildRotation)
 {

oox/source/export/drawingml.cxx
@@ -1736,14 +1736,14 @@ void DrawingML::WriteTransformation(const Reference< XShape >& xShape, const too
                           XML_rot, sax_fastparser::UseIf(OString::number(nRotation), nRotation % 21600000 != 0));
 
     sal_Int32 nLeft = rRect.Left();
-    sal_Int32 nChildLeft = nLeft;
     sal_Int32 nTop = rRect.Top();
-    sal_Int32 nChildTop = nTop;
     if (GetDocumentType() == DOCUMENT_DOCX && !m_xParent.is())
     {
         nLeft = 0;
         nTop = 0;
     }
+    sal_Int32 nChildLeft = nLeft;
+    sal_Int32 nChildTop = nTop;
 
     mpFS->singleElementNS(XML_a, XML_off,
         XML_x, OString::number(oox::drawingml::convertHmmToEmu(nLeft)),

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -578,7 +578,7 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testCropPixel, "crop-pixel.docx")
     // become invisible), now is around 19072.
     CPPUNIT_ASSERT(getXPath(pXmlDoc, "//a:srcRect", "l").toInt32() <= 22452);
 }
-
+/* FixMe: tdf#142805 Test disabled, because the picture is not load at all.
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testEffectExtent, "effect-extent.docx")
 {
     // The problem was that in case there were no shadows on the picture, we
@@ -587,7 +587,7 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testEffectExtent, "effect-extent.docx")
     // E.g. this was 0.
     assertXPath(pXmlDoc, "//wp:effectExtent", "l", "114300");
 }
-
+*/
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testEffectExtentInline, "effect-extent-inline.docx")
 {
     // The problem was that in case there was inline rotated picture, we

commit caca662c8342446645340579ccef27261538ef26

tdf#143476 improve import of lockedCanvas
A lockedCanvas is used in Word 2007 to include dml-shapes. It is
treated similar to a group, only that its content is not editable.

This patch changes the implementation so, that the lockedCanvas is
imported as group. Prior only content was imported in the case the
content was a single custom-shape or a single group.
That has resulted in these errors:
Image or line in a lockedCanvas were not imported at all.
Only one of several shapes in a lockedCanvas was imported.

I have changed mpShape to mpShapePtr to reflect the data type and be
consistent with ShapeContext.

The patch changes GraphicImport so, that lockedCanvas as special
version of a group is detected and handled. That solves regression
from commit 61a6be3aa3b470e5da3ca7ad4b8d1a6155a6a07f

Change-Id: I57005e64ff073bca6ebb46a404486203fb6802ac
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119684
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

oox/source/shape/LockedCanvasContext.cxx
@@ -9,9 +9,13 @@
 
 #include "LockedCanvasContext.hxx"
 #include <sal/log.hxx>
+#include <drawingml/shapepropertiescontext.hxx>
+#include <oox/drawingml/connectorshapecontext.hxx>
+#include <oox/drawingml/graphicshapecontext.hxx>
 #include <oox/drawingml/shape.hxx>
 #include <oox/drawingml/shapecontext.hxx>
 #include <oox/drawingml/shapegroupcontext.hxx>
+#include <oox/helper/attributelist.hxx>
 #include <oox/token/namespaces.hxx>
 #include <oox/token/tokens.hxx>
 
@@ -22,34 +26,64 @@ namespace oox::shape
 LockedCanvasContext::LockedCanvasContext(FragmentHandler2 const& rParent)
     : FragmentHandler2(rParent)
 {
+    mpShapePtr = std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.GroupShape");
+    mpShapePtr->setLockedCanvas(true); // will be "LockedCanvas" in InteropGrabBag
 }
 
 LockedCanvasContext::~LockedCanvasContext() = default;
 
 ::oox::core::ContextHandlerRef
-LockedCanvasContext::onCreateContext(sal_Int32 nElementToken,
-                                     const ::oox::AttributeList& /*rAttribs*/)
+LockedCanvasContext::onCreateContext(sal_Int32 nElementToken, const ::oox::AttributeList& rAttribs)
 {
     switch (getBaseToken(nElementToken))
     {
-        case XML_lockedCanvas:
-        case XML_nvGrpSpPr:
-        case XML_grpSpPr:
+        case XML_nvGrpSpPr: // CT_GvmlGroupShapeNonVisual, child see at end
+            return this;
+        case XML_grpSpPr: // CT_GroupShapeProporties
+            return new oox::drawingml::ShapePropertiesContext(*this, *mpShapePtr);
+        case XML_txSp: // CT_GvmlTextShape
             break;
-        case XML_sp:
+        case XML_sp: // CT_GvmlShape
         {
-            oox::drawingml::ShapePtr pMasterShape;
-            mpShape = std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.CustomShape");
-            mpShape->setLockedCanvas(true);
-            return new oox::drawingml::ShapeContext(*this, pMasterShape, mpShape);
+            return new oox::drawingml::ShapeContext(
+                *this, mpShapePtr,
+                std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.CustomShape", true));
         }
-        case XML_grpSp:
+        case XML_cxnSp: // CT_GvmlConnector
         {
-            oox::drawingml::ShapePtr pMasterShape;
-            mpShape = std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.GroupShape");
-            mpShape->setLockedCanvas(true);
-            return new oox::drawingml::ShapeGroupContext(*this, pMasterShape, mpShape);
+            return new oox::drawingml::ConnectorShapeContext(
+                *this, mpShapePtr,
+                std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.ConnectorShape"));
         }
+        case XML_pic: // CT_GvmlPicture
+        {
+            return new oox::drawingml::GraphicShapeContext(
+                *this, mpShapePtr,
+                std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.GraphicObjectShape"));
+        }
+        case XML_graphicFrame: // CT_GvmlGraphicObjectFrame
+        {
+            return new oox::drawingml::GraphicalObjectFrameContext(
+                *this, mpShapePtr,
+                std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.GraphicObjectShape"),
+                true);
+        }
+        case XML_grpSp: // CT_GvmlGroupShape
+        {
+            return new oox::drawingml::ShapeGroupContext(
+                *this, mpShapePtr,
+                std::make_shared<oox::drawingml::Shape>("com.sun.star.drawing.GroupShape"));
+        }
+        // mandatory child elements of CT_GvmlGroupShapeNonVisual
+        case XML_cNvPr: // CT_NonVisualDrawingProps
+        {
+            mpShapePtr->setHidden(rAttribs.getBool(XML_hidden, false));
+            mpShapePtr->setId(rAttribs.getString(XML_id).get());
+            mpShapePtr->setName(rAttribs.getString(XML_name).get());
+            break;
+        }
+        case XML_cNvGrpSpPr: // CT_NonVisualGroupDrawingShapeProps
+            break;
         default:
             SAL_WARN("oox", "LockedCanvasContext::createFastChildContext: unhandled element:"
                                 << getBaseToken(nElementToken));

oox/source/shape/LockedCanvasContext.hxx
@@ -25,10 +25,10 @@ public:
     oox::core::ContextHandlerRef onCreateContext(sal_Int32 nElementToken,
                                                  const ::oox::AttributeList& rAttribs) override;
 
-    const oox::drawingml::ShapePtr& getShape() const { return mpShape; }
+    const oox::drawingml::ShapePtr& getShape() const { return mpShapePtr; }
 
 private:
-    oox::drawingml::ShapePtr mpShape;
+    oox::drawingml::ShapePtr mpShapePtr;
 };
 }
 

writerfilter/source/dmapper/GraphicImport.cxx
@@ -913,8 +913,12 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                         // and contour wrap is different to Word. As workaround diagrams are excluded
                         // here in various places.
                         const bool bIsDiagram = oox::drawingml::DrawingML::IsDiagram(m_xShape);
+                        // tdf#143476: A lockedCanvas (Word2007) is imported as group, but has not
+                        // got size and position. Values from m_Impl has to be used.
+                        bool bIsLockedCanvas(false);
+                        aInteropGrabBag.getValue("LockedCanvas") >>= bIsLockedCanvas;
                         const bool bIsGroupOrLine = (xServiceInfo->supportsService("com.sun.star.drawing.GroupShape")
-                            && !bIsDiagram)
+                            && !bIsDiagram && !bIsLockedCanvas)
                             || xServiceInfo->supportsService("com.sun.star.drawing.LineShape");
                         SdrObject* pShape = GetSdrObjectFromXShape(m_xShape);
                         if ((bIsGroupOrLine && !lcl_bHasGroupSlantedChild(pShape) && nOOXAngle == 0)

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -720,7 +720,8 @@ CPPUNIT_TEST_FIXTURE(Test, testN820504)
 CPPUNIT_TEST_FIXTURE(Test, testFdo43641)
 {
     load(mpTestDocumentPath, "fdo43641.docx");
-    uno::Reference<container::XIndexAccess> xGroupShape(getShape(1), uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xGroupLockedCanvas(getShape(1), uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xGroupShape(xGroupLockedCanvas->getByIndex(0), uno::UNO_QUERY);
     uno::Reference<drawing::XShape> xLine(xGroupShape->getByIndex(1), uno::UNO_QUERY);
     // This was 2200, not 2579 in mm100, i.e. the size of the line shape was incorrect.
     // File cx=928694EMU = 2579.7Hmm, round up 2580Hmm. Currently off by 1.

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -48,6 +48,44 @@ public:
     }
 };
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf143476LockedCanvas_twoShapes)
+{
+    // Given a lockedCanvas in a docx document with compatibility to Word version 12 (2007).
+    // It contains two shapes. Error was, that the lockedCanvas was not imported as group at all,
+    // and only one shape was imported and that one was scaled to lockedCanvas area.
+    load(mpTestDocumentPath, "tdf143476_lockedCanvas_twoShapes.docx");
+    // The group shape corresponds to the lockedCanvas.
+    uno::Reference<container::XIndexAccess> xGroup(getShape(1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xGroup.is());
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2), xGroup->getCount());
+    uno::Reference<drawing::XShape> xShape(xGroup->getByIndex(1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(14200), xShape->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1120), xShape->getPosition().Y);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1928), xShape->getSize().Width);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1593), xShape->getSize().Height);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf143476LockedCanvas_position)
+{
+    // Given a lockedCanvas in a docx document with compatibility to Word version 12 (2007).
+    // Tests fix for regression introduced by 3262fc5ef3bde5b158909d11ccb008161ea95519
+    // Error was, that the imported shape had wrong position.
+    load(mpTestDocumentPath, "tdf143476_lockedCanvas_position.docx");
+    // The group shape corresponds to the lockedCanvas.
+    uno::Reference<drawing::XShape> xGroupShape(getShape(1), uno::UNO_QUERY);
+    // Without fix in place the test failed with position 185|947.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2351), xGroupShape->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(26), xGroupShape->getPosition().Y);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf143476LockedCanvas_image_line)
+{
+    // Given a lockedCanvas in a docx document with compatibility to Word version 12 (2007).
+    // It contains an image and a line. Error was, that both were not imported.
+    load(mpTestDocumentPath, "tdf143476_lockedCanvas_image_line.docx");
+    CPPUNIT_ASSERT_MESSAGE("No shapes imported", getShapes() > 0);
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testTdf143475rotatedWord2007imageInline)
 {
     // Given a docx document with compatibility to Word version 12 (2007), which has a shape