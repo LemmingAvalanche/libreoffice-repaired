LemmingAvalanche/libreoffice-repaired

commit 9377eb2bd6a7b8cebcb43b68dfed250d58d1b467

sw XHTML import, improved <object> handling for images: support more formats
This was working for only png and gif in the past, handle more formats:

- bmp
- jpg
- emf
- svg
- wmf
- tif

Change-Id: I2af42157428550977f0abab88f2ed728636d2a02
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119287
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

svx/source/xoutdev/_xoutbmp.cxx
@@ -134,7 +134,11 @@ ErrCode XOutBitmap::WriteGraphic( const Graphic& rGraphic, OUString& rFileName,
     {
         const bool bIsSvg(rFilterName.equalsIgnoreAsciiCase("svg") && VectorGraphicDataType::Svg == rVectorGraphicDataPtr->getType());
         const bool bIsWmf(rFilterName.equalsIgnoreAsciiCase("wmf") && VectorGraphicDataType::Wmf == rVectorGraphicDataPtr->getType());
-        const bool bIsEmf(rFilterName.equalsIgnoreAsciiCase("emf") && VectorGraphicDataType::Emf == rVectorGraphicDataPtr->getType());
+        bool bIsEmf(rFilterName.equalsIgnoreAsciiCase("emf") && VectorGraphicDataType::Emf == rVectorGraphicDataPtr->getType());
+        if (!bIsEmf)
+        {
+            bIsEmf = rGraphic.GetGfxLink().IsEMF();
+        }
         const bool bIsPdf(rFilterName.equalsIgnoreAsciiCase("pdf") && VectorGraphicDataType::Pdf == rVectorGraphicDataPtr->getType());
 
         if (bIsSvg || bIsWmf || bIsEmf || bIsPdf)

sw/qa/extras/htmlimport/data/ole-img-svg.xhtml
@@ -1,0 +1,7 @@
+<reqif-xhtml:div>
+<reqif-xhtml:p>
+<reqif-xhtml:object data="ole2.svg" type="image/svg+xml">
+    <reqif-xhtml:object data="ole2.png" type="image/png"></reqif-xhtml:object>
+</reqif-xhtml:object>
+</reqif-xhtml:p>
+</reqif-xhtml:div>

sw/qa/extras/htmlimport/data/ole2.svg
@@ -1,0 +1,3 @@
+<svg width="400" height="110">
+  <rect width="300" height="100" style="fill:none;stroke-width:3;stroke:rgb(0,0,0)" />
+</svg>

sw/qa/extras/htmlimport/htmlimport.cxx
@@ -485,6 +485,30 @@ CPPUNIT_TEST_FIXTURE(SwModelTestBase, testOleImg)
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), xObjects->getCount());
 }
 
+CPPUNIT_TEST_FIXTURE(SwModelTestBase, testOleImgSvg)
+{
+    // Given an XHTML with an <object> (containing SVG) and an inner <object> (containing PNG, to be
+    // ignored):
+    uno::Sequence<beans::PropertyValue> aLoadProperties = {
+        comphelper::makePropertyValue("FilterName", OUString("HTML (StarWriter)")),
+        comphelper::makePropertyValue("FilterOptions", OUString("xhtmlns=reqif-xhtml")),
+    };
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "ole-img-svg.xhtml";
+
+    // When loading the document:
+    mxComponent = loadFromDesktop(aURL, "com.sun.star.text.TextDocument", aLoadProperties);
+
+    // Then make sure the result is a single Writer image:
+    uno::Reference<text::XTextGraphicObjectsSupplier> xSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xObjects(xSupplier->getGraphicObjects(),
+                                                     uno::UNO_QUERY);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 0
+    // - Actual  : 1
+    // i.e. the image was not imported as a Writer image (but as an OLE object).
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), xObjects->getCount());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/filter/html/htmlflywriter.cxx
@@ -1962,6 +1962,30 @@ static Writer& OutHTML_FrameFormatGrfNode( Writer& rWrt, const SwFrameFormat& rF
             if (!rGraphic.isAvailable())
                 const_cast<Graphic&>(rGraphic).makeAvailable();
 
+            if (rHTMLWrt.mbReqIF && bWritePNGFallback)
+            {
+                // ReqIF: force native data if possible.
+                const std::shared_ptr<VectorGraphicData>& pVectorGraphicData = rGraphic.getVectorGraphicData();
+                if (pVectorGraphicData && pVectorGraphicData->getType() == VectorGraphicDataType::Svg)
+                {
+                    aFilterName = "svg";
+                }
+                else if (rGraphic.GetGfxLink().IsEMF())
+                {
+                    aFilterName = "emf";
+                    aMimeType = "image/x-emf"; // avoid image/x-wmf
+                }
+                else if (pVectorGraphicData && pVectorGraphicData->getType() == VectorGraphicDataType::Wmf)
+                {
+                    aFilterName = "wmf";
+                }
+                else if (rGraphic.GetGfxLink().GetType() == GfxLinkType::NativeTif)
+                {
+                    aFilterName = "tif";
+                    aMimeType = "image/tiff"; // avoid image/x-vclgraphic
+                }
+            }
+
             ErrCode nErr = XOutBitmap::WriteGraphic( rGraphic, aGraphicURL,
                     aFilterName, nFlags, &aMM100Size );
             if( nErr )

sw/source/filter/html/htmlplug.cxx
@@ -437,6 +437,12 @@ bool SwHTMLParser::InsertEmbed()
     static const std::set<std::u16string_view> vAllowlist = {
         u"image/png",
         u"image/gif",
+        u"image/x-MS-bmp",
+        u"image/jpeg",
+        u"image/x-wmf",
+        u"image/svg+xml",
+        u"image/tiff",
+        u"image/x-emf",
     };
 
     if (vAllowlist.find(aType) != vAllowlist.end() && m_aEmbeds.empty())

commit 00f5794b1720faf0bbb02cbdcef58469cabe5045

sw HTML export: avoid writing EMF data with JPG extension
Regression from commit 9377eb2bd6a7b8cebcb43b68dfed250d58d1b467 (sw
XHTML import, improved <object> handling for images: support more
formats, 2021-07-20), the problem was that now an EMF image was written
with the JPG extension in the HTML export.

Solve the problem by making the "use emf as-is" check more strict in
XOutBitmap::WriteGraphic(): that will be still true in the XHTML case
when the source image is EMF, but the filter name will be JPG in the
HTML case, so that will avoid the unwanted EMF data in the HTML case.

Change-Id: I7bced482d677cf121be1ed494a3aaeb319e1c5b4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120209
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

svx/source/xoutdev/_xoutbmp.cxx
@@ -132,12 +132,13 @@ ErrCode XOutBitmap::WriteGraphic( const Graphic& rGraphic, OUString& rFileName,
 
     if (rVectorGraphicDataPtr && rVectorGraphicDataPtr->getBinaryDataContainer().getSize())
     {
+        // Does the filter name match the original format?
         const bool bIsSvg(rFilterName.equalsIgnoreAsciiCase("svg") && VectorGraphicDataType::Svg == rVectorGraphicDataPtr->getType());
         const bool bIsWmf(rFilterName.equalsIgnoreAsciiCase("wmf") && VectorGraphicDataType::Wmf == rVectorGraphicDataPtr->getType());
         bool bIsEmf(rFilterName.equalsIgnoreAsciiCase("emf") && VectorGraphicDataType::Emf == rVectorGraphicDataPtr->getType());
         if (!bIsEmf)
         {
-            bIsEmf = rGraphic.GetGfxLink().IsEMF();
+            bIsEmf = rFilterName.equalsIgnoreAsciiCase("emf") && rGraphic.GetGfxLink().IsEMF();
         }
         const bool bIsPdf(rFilterName.equalsIgnoreAsciiCase("pdf") && VectorGraphicDataType::Pdf == rVectorGraphicDataPtr->getType());
 

sw/qa/extras/htmlexport/htmlexport.cxx
@@ -1960,6 +1960,27 @@ CPPUNIT_TEST_FIXTURE(SwHtmlDomExportTest, testListsHeading)
                        "list 1, header 1");
 }
 
+CPPUNIT_TEST_FIXTURE(SwHtmlDomExportTest, testOleEmfPreviewToHtml)
+{
+    // Given a document containing an embedded object, with EMF preview:
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "ole2.odt";
+    mxComponent = loadFromDesktop(aURL, "com.sun.star.text.TextDocument", {});
+
+    // When exporting to HTML:
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
+    uno::Sequence<beans::PropertyValue> aStoreProperties = {
+        comphelper::makePropertyValue("FilterName", OUString("HTML (StarWriter)")),
+    };
+    xStorable->storeToURL(maTempFile.GetURL(), aStoreProperties);
+
+    // Then make sure the <img> tag has matching file extension and data:
+    htmlDocUniquePtr pDoc = parseHtml(maTempFile);
+    OUString aPath = getXPath(pDoc, "/html/body/p/img", "src");
+    // Without the accompanying fix in place, this test would have failed, as aPath was
+    // ole_html_3978e5f373402b43.JPG, with EMF data.
+    CPPUNIT_ASSERT(aPath.endsWith("gif"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */