LemmingAvalanche/libreoffice-repaired

commit b3e89c84779d40947d55e91fdd87fddfcf84be1e

tdf#131546 DOCX import: fix performance regression at tables
Commit ce94fd9185984e311ccd2d1e2c3b9b908acd16c3 "tdf#90069 DOCX:
fix character style of new table rows" caused ~20% slowing down
in loading time of documents with huge tables, related to the
extra processing of the redundant w:rPr of table paragraph runs.
(In DOCX tables, MSO exports the run properties into the run and
paragraph sections too, probably because of compatibility or
usability reasons.)

Theoretically in this case, the run properties which are under the
run section win. On the other hand, because LO copies the props
which are applied on paragraph level, and only them, when copying
a row (e.g. upon inserting a new one), it was needed to apply the
mentioned run props not only as direct character formatting, but
as a direct paragraph formatting too. This way, the support of
copying of rows are solved. Unfortunately, this "double" applying
was done for every single paragraph, which quite slowed down the
opening time. This patch gives a workaround, which completely removes
this double applying functionality in the writerfilter by reverting
commit ce94fd9185984e311ccd2d1e2c3b9b908acd16c3 (except its unit test),
and copy the mentioned run properties into paragraph level, when its
needed: upon inserting a new row before/after. This way we spare a lot
of cycles, as most of the original applies had no real use whatsoever.

Change-Id: Ic24d0d1830e9ec43323e5e213d6481ec28e9abe3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118985
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/table/swtable.cxx
@@ -141,13 +141,28 @@ void InsTableBox( SwDoc& rDoc, SwTableNode* pTableNd,
 
     if( pCNd->IsTextNode() )
     {
-        if( pBox->GetSaveNumFormatColor() && pCNd->GetpSwAttrSet() )
+        if( pCNd->GetpSwAttrSet() )
         {
             SwAttrSet aAttrSet( *pCNd->GetpSwAttrSet() );
-            if( pBox->GetSaveUserColor() )
-                aAttrSet.Put( SvxColorItem( *pBox->GetSaveUserColor(), RES_CHRATR_COLOR ));
-            else
-                aAttrSet.ClearItem( RES_CHRATR_COLOR );
+            SwTextNode* pTNd = static_cast<SwTextNode*>(pCNd);
+            SwpHints * pSwpHints = pTNd->GetpSwpHints();
+            if(pSwpHints && pSwpHints->Count()!=0)
+            {
+                SwTextAttr* textAttr = pSwpHints->Get(pSwpHints->Count()-1);
+                if(textAttr->Which() == RES_TXTATR_AUTOFMT )
+                {
+                    SwFormatAutoFormat& format = static_cast<SwFormatAutoFormat&>(textAttr->GetAttr());
+                    const std::shared_ptr<SfxItemSet>& handle = format.GetStyleHandle();
+                    aAttrSet.Put(*handle);
+                }
+            }
+            if( pBox->GetSaveNumFormatColor() )
+            {
+                if( pBox->GetSaveUserColor() )
+                    aAttrSet.Put( SvxColorItem( *pBox->GetSaveUserColor(), RES_CHRATR_COLOR ));
+                else
+                    aAttrSet.ClearItem( RES_CHRATR_COLOR );
+            }
             rDoc.GetNodes().InsBoxen( pTableNd, pLine, pBoxFrameFormat,
                                     static_cast<SwTextNode*>(pCNd)->GetTextColl(),
                                     &aAttrSet, nInsPos, nCnt );
@@ -155,8 +170,7 @@ void InsTableBox( SwDoc& rDoc, SwTableNode* pTableNd,
         else
             rDoc.GetNodes().InsBoxen( pTableNd, pLine, pBoxFrameFormat,
                                     static_cast<SwTextNode*>(pCNd)->GetTextColl(),
-                                    pCNd->GetpSwAttrSet(),
-                                    nInsPos, nCnt );
+                                    pCNd->GetpSwAttrSet(), nInsPos, nCnt );
     }
     else
         rDoc.GetNodes().InsBoxen( pTableNd, pLine, pBoxFrameFormat,

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2201,24 +2201,6 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                 // fix table paragraph properties
                 if ( xTextRange.is() && xParaProps && m_nTableDepth > 0 )
                 {
-                    uno::Sequence< beans::PropertyValue > aParaProps = pParaContext->GetPropertyValues(false);
-                    uno::Reference<text::XTextCursor> xCur =  xTextRange->getText()->createTextCursorByRange(xTextRange);
-                    uno::Reference< beans::XPropertyState > xRunProperties( xCur, uno::UNO_QUERY_THROW );
-                    // tdf#90069 in tables, apply paragraph level character style also on
-                    // paragraph level to support its copy during insertion of new table rows
-                    for( const auto& rParaProp : std::as_const(aParaProps) )
-                    {
-                        if ( rParaProp.Name.startsWith("Char") && rParaProp.Name != "CharStyleName" && rParaProp.Name != "CharInteropGrabBag" &&
-                            // all text portions contain the same value, so next setPropertyValue() won't overwrite part of them
-                            xRunProperties->getPropertyState(rParaProp.Name) == css::beans::PropertyState_DIRECT_VALUE )
-                        {
-                            uno::Reference<beans::XPropertySet> xRunPropertySet(xCur, uno::UNO_QUERY);
-                            xParaProps->setPropertyValue( rParaProp.Name, xRunPropertySet->getPropertyValue(rParaProp.Name) );
-                            // remember this for table style handling
-                            getTableManager().getCurrentParagraphs()->back().m_aParaOverrideApplied.insert(rParaProp.Name);
-                        }
-                    }
-
                     // tdf#128959 table paragraphs haven't got window and orphan controls
                     uno::Any aAny = uno::makeAny(static_cast<sal_Int8>(0));
                     xParaProps->setPropertyValue("ParaOrphans", aAny);

commit 127dc8e39e517a7641fced1bf58aebf55768fe4c

tdf#143904 fix direct character formatting copied into paragraph level
Currently, Writer copied the direct character formattings, and applied them on paragraph level, when inserting a new row in a table.
This bug is a regression from b3e89c84779d40947d55e91fdd87fddfcf84be1e .

Explanation, why this regression happened:
MSO Word uses a paragraph mark symbol (i.e the control character than can be configured to be shown), which is imported in Writer as a 0-length hint.
To fasten up the import of DOCX tables, the core applies the formattings from this 0-length attribute on paragraph level, directly before inserting a new row.
All in all, to solve this bug, we needed to make sure, that we only copy the formattings, if it is from a 0-length hint.

Change-Id: Iba52e9318cc588feda559339e42897568d009806
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120624
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/table/swtable.cxx
@@ -145,17 +145,11 @@ void InsTableBox( SwDoc& rDoc, SwTableNode* pTableNd,
         if( pCNd->GetpSwAttrSet() )
         {
             SwAttrSet aAttrSet( *pCNd->GetpSwAttrSet() );
-            SwTextNode* pTNd = static_cast<SwTextNode*>(pCNd);
-            SwpHints * pSwpHints = pTNd->GetpSwpHints();
-            if(pSwpHints && pSwpHints->Count()!=0)
+            if(pCNd->GetSwAttrSet().HasItem(RES_PARATR_LIST_AUTOFMT))
             {
-                SwTextAttr* textAttr = pSwpHints->Get(pSwpHints->Count()-1);
-                if(textAttr->Which() == RES_TXTATR_AUTOFMT )
-                {
-                    SwFormatAutoFormat& format = static_cast<SwFormatAutoFormat&>(textAttr->GetAttr());
-                    const std::shared_ptr<SfxItemSet>& handle = format.GetStyleHandle();
-                    aAttrSet.Put(*handle);
-                }
+            SwFormatAutoFormat format = aAttrSet.Get(RES_PARATR_LIST_AUTOFMT);
+            const std::shared_ptr<SfxItemSet>& handle = format.GetStyleHandle();
+            aAttrSet.Put(*handle);
             }
             if( pBox->GetSaveNumFormatColor() )
             {

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -4440,6 +4440,29 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testOfz18563)
     TestImportDOCX(aFileStream);
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf143904)
+{
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf143904.odt");
+
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    sal_uLong nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+
+    dispatchCommand(mxComponent, ".uno:InsertRowsAfter", {});
+    pWrtShell->Down(false);
+    pWrtShell->Insert("foo");
+
+    SwTextNode* pTextNodeA1 = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+    CPPUNIT_ASSERT(pTextNodeA1->GetText().startsWith("Insert"));
+    nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    SwTextNode* pTextNodeA2 = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+    CPPUNIT_ASSERT_EQUAL(OUString("foo"), pTextNodeA2->GetText());
+    CPPUNIT_ASSERT_EQUAL(false, pTextNodeA2->GetSwAttrSet().HasItem(RES_CHRATR_FONT));
+    OUString sFontName = pTextNodeA2->GetSwAttrSet().GetItem(RES_CHRATR_FONT)->GetFamilyName();
+    CPPUNIT_ASSERT_EQUAL(OUString("Liberation Serif"), sFontName);
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf90069)
 {
     SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf90069.docx");