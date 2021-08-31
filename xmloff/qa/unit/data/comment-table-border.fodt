LemmingAvalanche/libreoffice-repaired

commit 06b0fbcb8b2b10fbbbe6a7cb00b2e76ff6e03501

office:annotation-end import
Change-Id: I8d1475b5bc9a36dade5ce28e74139834c69f8e14

xmloff/source/text/txtfldi.cxx
@@ -567,7 +567,9 @@ XMLTextFieldImportContext::CreateTextFieldImportContext(
             break;
 
         case XML_TOK_TEXT_ANNOTATION:
+        case XML_TOK_TEXT_ANNOTATION_END:
             pContext = new XMLAnnotationImportContext( rImport, rHlp,
+                                                       nToken,
                                                        nPrefix, rName);
             break;
 
@@ -3629,6 +3631,7 @@ TYPEINIT1(XMLAnnotationImportContext, XMLTextFieldImportContext);
 XMLAnnotationImportContext::XMLAnnotationImportContext(
     SvXMLImport& rImport,
     XMLTextImportHelper& rHlp,
+    sal_uInt16 nToken,
     sal_uInt16 nPrfx,
     const OUString& sLocalName) :
         XMLTextFieldImportContext(rImport, rHlp, sAPI_annotation,
@@ -3638,7 +3641,9 @@ XMLAnnotationImportContext::XMLAnnotationImportContext(
         sPropertyContent(sAPI_content),
         // why is there no UNO_NAME_DATE_TIME, but only UNO_NAME_DATE_TIME_VALUE?
         sPropertyDate(sAPI_date_time_value),
-        sPropertyTextRange(sAPI_TextRange)
+        sPropertyTextRange(sAPI_TextRange),
+        sPropertyName(sAPI_name),
+        m_nToken(nToken)
 {
     bValid = sal_True;
 
@@ -3649,10 +3654,11 @@ XMLAnnotationImportContext::XMLAnnotationImportContext(
 }
 
 void XMLAnnotationImportContext::ProcessAttribute(
-    sal_uInt16,
-    const OUString& )
+    sal_uInt16 nToken,
+    const OUString& rValue )
 {
-    // ignore
+    if (nToken == XML_TOK_TEXT_NAME)
+        aName = rValue;
 }
 
 SvXMLImportContext* XMLAnnotationImportContext::CreateChildContext(
@@ -3747,7 +3753,21 @@ void XMLAnnotationImportContext::EndElement()
             // workaround for #80606#
             try
             {
-                GetImportHelper().InsertTextContent( xTextContent );
+                if (m_nToken == XML_TOK_TEXT_ANNOTATION_END && m_xStart.is())
+                {
+                    // So we are ending a previous annotation, let's create a
+                    // text range covering the old and the current position.
+                    uno::Reference<text::XText> xText = GetImportHelper().GetText();
+                    uno::Reference<text::XTextCursor> xCursor = xText->createTextCursorByRange(m_xStart->getAnchor());
+                    xCursor->gotoRange(GetImportHelper().GetCursorAsRange(), true);
+                    uno::Reference<text::XTextRange> xTextRange(xCursor, uno::UNO_QUERY);
+                    xText->insertTextContent(xTextRange, xTextContent, !xCursor->isCollapsed());
+
+                    // Now we can delete the old annotation with the incorrect position.
+                    uno::Reference<lang::XComponent>(m_xStart, uno::UNO_QUERY)->dispose();
+                }
+                else
+                    GetImportHelper().InsertTextContent( xTextContent );
             }
             catch (const lang::IllegalArgumentException&)
             {
@@ -3762,6 +3782,39 @@ void XMLAnnotationImportContext::EndElement()
 void XMLAnnotationImportContext::PrepareField(
     const Reference<XPropertySet> & xPropertySet)
 {
+    if (m_nToken == XML_TOK_TEXT_ANNOTATION_END && !aName.isEmpty())
+    {
+        // Search for a previous annotation with the same name.
+        Reference<XTextFieldsSupplier> xTextFieldsSupplier(GetImport().GetModel(), UNO_QUERY);
+        uno::Reference<container::XEnumerationAccess> xFieldsAccess(xTextFieldsSupplier->getTextFields());
+        uno::Reference<container::XEnumeration> xFields(xFieldsAccess->createEnumeration());
+        uno::Reference<beans::XPropertySet> xPrevField;
+        while (xFields->hasMoreElements())
+        {
+            uno::Reference<beans::XPropertySet> xCurrField(xFields->nextElement(), uno::UNO_QUERY);
+            OUString aFieldName;
+            xCurrField->getPropertyValue(sPropertyName) >>= aFieldName;
+            if (aFieldName == aName)
+            {
+                xPrevField = xCurrField;
+                break;
+            }
+        }
+        if (xPrevField.is())
+        {
+            // Found? Then copy over the properties.
+            xPropertySet->setPropertyValue(sPropertyAuthor, xPrevField->getPropertyValue(sPropertyAuthor));
+            xPropertySet->setPropertyValue(sPropertyInitials, xPrevField->getPropertyValue(sPropertyInitials));
+            xPropertySet->setPropertyValue(sPropertyDate, xPrevField->getPropertyValue(sPropertyDate));
+            xPropertySet->setPropertyValue(sPropertyName, xPrevField->getPropertyValue(sPropertyName));
+            xPropertySet->setPropertyValue(sPropertyContent, xPrevField->getPropertyValue(sPropertyContent));
+
+            // And save a reference to it, so we can delete it later.
+            m_xStart.set(xPrevField, uno::UNO_QUERY);
+            return;
+        }
+    }
+
     // import (possibly empty) author
     OUString sAuthor( aAuthorBuffer.makeStringAndClear() );
     xPropertySet->setPropertyValue(sPropertyAuthor, makeAny(sAuthor));
@@ -3792,6 +3845,9 @@ void XMLAnnotationImportContext::PrepareField(
             sBuffer = sBuffer.copy(0, sBuffer.getLength()-1);
         xPropertySet->setPropertyValue(sPropertyContent, makeAny(sBuffer));
     }
+
+    if (!aName.isEmpty())
+        xPropertySet->setPropertyValue(sPropertyName, makeAny(aName));
 }
 
 

xmloff/inc/txtfldi.hxx
@@ -1345,15 +1345,20 @@ class XMLAnnotationImportContext : public XMLTextFieldImportContext
     const ::rtl::OUString sPropertyContent;
     const ::rtl::OUString sPropertyDate;
     const ::rtl::OUString sPropertyTextRange;
+    const ::rtl::OUString sPropertyName;
 
     ::rtl::OUStringBuffer aAuthorBuffer;
     ::rtl::OUStringBuffer aInitialsBuffer;
+    OUString aName;
     ::rtl::OUStringBuffer aTextBuffer;
     ::rtl::OUStringBuffer aDateBuffer;
 
     com::sun::star::uno::Reference < com::sun::star::beans::XPropertySet > mxField;
     com::sun::star::uno::Reference < com::sun::star::text::XTextCursor >  mxCursor;
     com::sun::star::uno::Reference < com::sun::star::text::XTextCursor >  mxOldCursor;
+    /// If this is an annotation end, then position of the start.
+    com::sun::star::uno::Reference < com::sun::star::text::XTextContent >  m_xStart;
+    sal_uInt16 m_nToken;
 
 public:
 
@@ -1361,6 +1366,7 @@ public:
 
     XMLAnnotationImportContext(SvXMLImport& rImport,
                                XMLTextImportHelper& rHlp,
+                               sal_uInt16 nToken,
                                sal_uInt16 nPrfx,
                                const ::rtl::OUString& sLocalName);
 

xmloff/inc/xmloff/txtimp.hxx
@@ -221,6 +221,8 @@ enum XMLTextPElemTokens
     XML_TOK_TEXT_SHEET_NAME,
     XML_TOK_TEXT_BIBLIOGRAPHY_MARK,
     XML_TOK_TEXT_ANNOTATION,
+    XML_TOK_TEXT_ANNOTATION_END,
+    XML_TOK_TEXT_NAME,
     XML_TOK_TEXT_SCRIPT,
     XML_TOK_TEXT_TABLE_FORMULA,
     XML_TOK_TEXT_DROPDOWN,

xmloff/source/text/txtimp.cxx
@@ -271,6 +271,7 @@ static SvXMLTokenMapEntry aTextPElemTokenMap[] =
     { XML_NAMESPACE_TEXT, XML_BIBLIOGRAPHY_MARK,
       XML_TOK_TEXT_BIBLIOGRAPHY_MARK },
     { XML_NAMESPACE_OFFICE, XML_ANNOTATION, XML_TOK_TEXT_ANNOTATION },
+    { XML_NAMESPACE_OFFICE, XML_ANNOTATION_END, XML_TOK_TEXT_ANNOTATION_END },
     { XML_NAMESPACE_TEXT, XML_SCRIPT, XML_TOK_TEXT_SCRIPT },
     { XML_NAMESPACE_TEXT, XML_TABLE_FORMULA, XML_TOK_TEXT_TABLE_FORMULA },
     { XML_NAMESPACE_TEXT, XML_DROPDOWN, XML_TOK_TEXT_DROPDOWN },
@@ -488,6 +489,7 @@ static SvXMLTokenMapEntry aTextFieldAttrTokenMap[] =
     { XML_NAMESPACE_TEXT, XML_CURRENT_VALUE,
                 XML_TOK_TEXTFIELD_CURRENT_VALUE },
     { XML_NAMESPACE_TEXT, XML_TABLE_TYPE, XML_TOK_TEXTFIELD_TABLE_TYPE },
+    { XML_NAMESPACE_OFFICE, XML_NAME, XML_TOK_TEXT_NAME },
 
     XML_TOKEN_MAP_END
 };

commit b3b978122042b1898bc9d8f11a4bb3a2f2eccff9

tdf#126126 ODT import: improve import of cross-table commented text range
Regression from commit 06b0fbcb8b2b10fbbbe6a7cb00b2e76ff6e03501
(office:annotation-end import, 2012-07-20), the problem was that
XMLAnnotationImportContext::EndElement() assumed that we can always call
gotoRange() to go from the annotation start and end points, but this is
not true: an annotation may start inside a table and end outside a
table, which is not a valid selection, so gotoRange() fails.

Fix the regression part by just creating a text range for the anchor of
the comment, so the comment is attached to the end of the range, and
this way the rest of the comment & the document can be at least opened.

[ It seems bookmarks behave similarly: they don't block the whole
import, but don't work cross table boundaries, either. ]

Change-Id: I1b5a2e2e7501ce3054379fc79d2045c3439c52e4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121322
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

xmloff/source/text/txtfldi.cxx
@@ -69,7 +69,7 @@
 #include <rtl/math.hxx>
 #include <tools/debug.hxx>
 #include <osl/diagnose.h>
-
+#include <tools/diagnose_ex.h>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -3257,7 +3257,18 @@ void XMLAnnotationImportContext::endFastElement(sal_Int32 /*nElement*/)
                 uno::Reference<text::XText> xText = GetImportHelper().GetText();
                 uno::Reference<text::XTextCursor> xCursor =
                     xText->createTextCursorByRange(GetImportHelper().GetCursorAsRange());
-                xCursor->gotoRange(xPrevField->getAnchor(), true);
+                try
+                {
+                    xCursor->gotoRange(xPrevField->getAnchor(), true);
+                }
+                catch (const uno::RuntimeException&)
+                {
+                    // Loosing the start of the anchor is better than not opening the document at
+                    // all.
+                    TOOLS_WARN_EXCEPTION(
+                        "xmloff.text",
+                        "XMLAnnotationImportContext::endFastElement: gotoRange() failed: ");
+                }
 
                 xText->insertTextContent(xCursor, xPrevField, !xCursor->isCollapsed());
             }

xmloff/qa/unit/data/comment-table-border.fodt
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+  <office:body>
+    <office:text>
+      <table:table>
+        <table:table-column table:style-name="Table1.A"/>
+        <table:table-row>
+          <table:table-cell table:style-name="Table1.A1" office:value-type="string">
+            <text:p>A<office:annotation office:name="0"><text:p>x</text:p></office:annotation>b</text:p>
+          </table:table-cell>
+        </table:table-row>
+      </table:table>
+      <text:p>b<office:annotation-end office:name="0"/><text:span>Z</text:span></text:p>
+    </office:text>
+  </office:body>
+</office:document>

xmloff/qa/unit/text.cxx
@@ -153,6 +153,14 @@ CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testBibliographyLocalUrl)
     CPPUNIT_ASSERT_EQUAL(OUString("file:///home/me/test.pdf"), aActual);
 }
 
+CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testCommentTableBorder)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "comment-table-border.fodt";
+    // Without the accompanying fix in place, this failed to load, as a comment that started in a
+    // table and ended outside a table aborted the whole importer.
+    getComponent() = loadFromDesktop(aURL);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */