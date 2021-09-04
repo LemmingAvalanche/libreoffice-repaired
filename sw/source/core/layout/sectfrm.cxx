LemmingAvalanche/libreoffice-repaired

commit 7e6c814177317850f2091b6485847f049aa02d72

speedup dynamic_cast to SwTextFrame
which is a hotspot in loading some documents

Change-Id: I7f1c48305cd40bcd9bee4baf219228f10cc297b3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121184
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/sectfrm.cxx
@@ -204,8 +204,8 @@ void SwSectionFrame::DelEmpty( bool bRemove )
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                                dynamic_cast<SwTextFrame*>(FindNextCnt( true )),
-                                dynamic_cast<SwTextFrame*>(FindPrevCnt()) );
+                                FindNextCnt( true )->DynCastTextFrame(),
+                                FindPrevCnt()->DynCastTextFrame() );
             }
         }
         Cut_( bRemove );

sw/source/core/access/accmap.cxx
@@ -3419,8 +3419,7 @@ void SwAccessibleMap::InvalidateTextSelectionOfAllParas()
                                 static_cast<SwAccessibleContext*>( xAcc.get() ) );
                     if ( xAccImpl.is() && xAccImpl->GetFrame() )
                     {
-                        const SwTextFrame* pTextFrame(
-                            dynamic_cast<const SwTextFrame*>(xAccImpl->GetFrame()) );
+                        const SwTextFrame* pTextFrame = xAccImpl->GetFrame()->DynCastTextFrame();
                         OSL_ENSURE( pTextFrame,
                                 "<SwAccessibleMap::_SubmitTextSelectionChangedEvents()> - unexpected type of frame" );
                         if ( pTextFrame )
@@ -3448,8 +3447,7 @@ void SwAccessibleMap::InvalidateTextSelectionOfAllParas()
                         static_cast<SwAccessibleContext*>( xAcc.get() ) );
             if ( xAccImpl.is() && xAccImpl->GetFrame() )
             {
-                const SwTextFrame* pTextFrame(
-                        dynamic_cast<const SwTextFrame*>(xAccImpl->GetFrame()) );
+                const SwTextFrame* pTextFrame = xAccImpl->GetFrame()->DynCastTextFrame();
                 OSL_ENSURE( pTextFrame,
                         "<SwAccessibleMap::_SubmitTextSelectionChangedEvents()> - unexpected type of frame" );
                 if ( pTextFrame )

sw/source/core/access/accpara.cxx
@@ -718,7 +718,7 @@ lang::Locale SAL_CALL SwAccessibleParagraph::getLocale()
 {
     SolarMutexGuard aGuard;
 
-    const SwTextFrame *pTextFrame = dynamic_cast<const SwTextFrame*>( GetFrame()  );
+    const SwTextFrame *pTextFrame = GetFrame()->DynCastTextFrame();
     if( !pTextFrame )
     {
         throw uno::RuntimeException("no SwTextFrame", static_cast<cppu::OWeakObject*>(this));
@@ -738,7 +738,7 @@ uno::Reference<XAccessibleRelationSet> SAL_CALL SwAccessibleParagraph::getAccess
 
     rtl::Reference<utl::AccessibleRelationSetHelper> pHelper = new utl::AccessibleRelationSetHelper();
 
-    const SwTextFrame* pTextFrame = dynamic_cast<const SwTextFrame*>(GetFrame());
+    const SwTextFrame* pTextFrame = GetFrame()->DynCastTextFrame();
     OSL_ENSURE( pTextFrame,
             "<SwAccessibleParagraph::getAccessibleRelationSet()> - missing text frame");
     if ( pTextFrame )

sw/source/core/docnode/ndsect.cxx
@@ -1122,8 +1122,8 @@ void SwSectionNode::MakeFramesForAdjacentContentNode(const SwNodeIndex & rIdx)
                          pViewShell->GetLayout()->IsAnyShellAccessible() )
                     {
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNew->FindPrevCnt()->DynCastTextFrame() );
                     }
                 }
                 pNew = pSct;
@@ -1148,8 +1148,8 @@ void SwSectionNode::MakeFramesForAdjacentContentNode(const SwNodeIndex & rIdx)
                      pViewShell->GetLayout()->IsAnyShellAccessible() )
                 {
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                        dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                        dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                        pNew->FindNextCnt( true )->DynCastTextFrame(),
+                        pNew->FindPrevCnt()->DynCastTextFrame() );
                 }
             }
             if ( bInitNewSect )

sw/source/core/docnode/ndtbl.cxx
@@ -2433,8 +2433,8 @@ void SwTableNode::MakeOwnFrames(SwNodeIndex* pIdxBehind)
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNew->FindPrevCnt()->DynCastTextFrame() );
             }
         }
         pNew->RegistFlys();
@@ -2468,8 +2468,8 @@ void SwTableNode::DelFrames(SwRootFrame const*const pLayout)
                          pViewShell->GetLayout()->IsAnyShellAccessible() )
                     {
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                            pFrame->FindPrevCnt()->DynCastTextFrame() );
                     }
                 }
                 pFrame->Cut();

sw/source/core/docnode/node.cxx
@@ -1393,8 +1393,8 @@ void SwContentNode::MakeFramesForAdjacentContentNode(SwContentNode& rNode)
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNew->FindPrevCnt()->DynCastTextFrame() );
             }
         }
     }
@@ -1486,8 +1486,8 @@ void SwContentNode::DelFrames(SwRootFrame const*const pLayout)
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                            pFrame->FindPrevCnt()->DynCastTextFrame() );
             }
         }
 

sw/source/core/frmedt/feshview.cxx
@@ -1450,8 +1450,7 @@ bool SwFEShell::ShouldObjectBeSelected(const Point& rPt)
                     {
                         if ( pContentFrame->UnionFrame().IsInside( rPt ) )
                         {
-                            const SwTextFrame* pTextFrame =
-                                    dynamic_cast<const SwTextFrame*>(pContentFrame);
+                            const SwTextFrame* pTextFrame = pContentFrame->DynCastTextFrame();
                             if ( pTextFrame )
                             {
                                 SwPosition aPos(GetDoc()->GetNodes());

sw/source/core/frmedt/fews.cxx
@@ -982,7 +982,7 @@ void SwFEShell::CalcBoundRect( SwRect& _orRect,
             // <pFrame>, which is the anchor frame or the proposed anchor frame,
             // doesn't have to be a text frame (e.g. edit a to-page anchored
             // fly frame). Thus, assure this.
-            const SwTextFrame* pTextFrame( dynamic_cast<const SwTextFrame*>(pFrame) );
+            const SwTextFrame* pTextFrame = pFrame->DynCastTextFrame();
             if ( pTextFrame &&
                  (_nAnchorId == RndStdIds::FLY_AT_CHAR) &&
                  ( _eVertRelOrient == text::RelOrientation::CHAR ||

commit 3d1a89be160f13451d82e813445de63e42c524cb

tdf#144193 fix import crash
regression from
    commit 7e6c814177317850f2091b6485847f049aa02d72
    speedup dynamic_cast to SwTextFrame

Change-Id: If602b7166287edfe9a41ac662f1a0b03f283ca83
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121603
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/sectfrm.cxx
@@ -203,9 +203,11 @@ void SwSectionFrame::DelEmpty( bool bRemove )
             if ( pViewShell && pViewShell->GetLayout() &&
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
+                auto pNext = FindNextCnt( true );
+                auto pPrev = FindPrevCnt();
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                                FindNextCnt( true )->DynCastTextFrame(),
-                                FindPrevCnt()->DynCastTextFrame() );
+                                pNext ? pNext->DynCastTextFrame() : nullptr,
+                                pPrev ? pPrev->DynCastTextFrame() : nullptr );
             }
         }
         Cut_( bRemove );