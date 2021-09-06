LemmingAvalanche/libreoffice-repaired

commit b38832decf8f8d63a4e80b2bf07371ec7267e14d

speedup rational_FromDouble
multiplying/dividing by a power of 2 is much cheaper than
the equivalent operation on a factor of 10.

Change-Id: I31a7196a07649336378be867c67eb8a89fe6765f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108019
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

tools/source/generic/fract.cxx
@@ -400,8 +400,8 @@ SvStream& WriteFraction( SvStream& rOStream, const Fraction& rFract )
 }
 
 // If dVal > LONG_MAX or dVal < LONG_MIN, the rational throws a boost::bad_rational.
-// Otherwise, dVal and denominator are multiplied by 10, until one of them
-// is larger than (LONG_MAX / 10).
+// Otherwise, dVal and denominator are multiplied by 8, until one of them
+// is larger than (LONG_MAX / 8).
 //
 // NOTE: here we use 'sal_Int32' due that only values in sal_Int32 range are valid.
 static boost::rational<sal_Int32> rational_FromDouble(double dVal)
@@ -411,11 +411,11 @@ static boost::rational<sal_Int32> rational_FromDouble(double dVal)
          std::isnan(dVal) )
         throw boost::bad_rational();
 
-    const sal_Int32 nMAX = std::numeric_limits<sal_Int32>::max() / 10;
+    const sal_Int32 nMAX = std::numeric_limits<sal_Int32>::max() / 8;
     sal_Int32 nDen = 1;
     while ( std::abs( dVal ) < nMAX && nDen < nMAX ) {
-        dVal *= 10;
-        nDen *= 10;
+        dVal *= 8;
+        nDen *= 8;
     }
     return boost::rational<sal_Int32>( sal_Int32(dVal), nDen );
 }

commit f1488a4b39600bcaadbe00b20a701912d1533fb8

tdf#144319 fix formula load
regression from
    commit b38832decf8f8d63a4e80b2bf07371ec7267e14d
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Sat Dec 19 13:25:53 2020 +0200
    speedup rational_FromDouble

Change-Id: Icb728b63f950e827f1492087114f927c2f85ddb9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121719
Tested-by: Jenkins
Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

tools/source/generic/fract.cxx
@@ -43,6 +43,9 @@ static boost::rational<sal_Int32> toRational(sal_Int32 n, sal_Int32 d)
     // https://github.com/boostorg/boost/issues/335 when these are std::numeric_limits<sal_Int32>::min
     if (n == d)
         return 1;
+    // tdf#144319 avoid boost::bad_rational e.g. if numerator=-476741369, denominator=-2147483648
+    if (d < -std::numeric_limits<sal_Int32>::max())
+        return 0;
     return boost::rational<sal_Int32>(n, d);
 }
 