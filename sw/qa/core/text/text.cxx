LemmingAvalanche/libreoffice-repaired

commit fb5d1f54d3be7c1f12dbe60823a322ee66331c1e

sw: allow the height of a line to be larger than 65536 twips
The line height can be quite large if the line contains an as-char
image.

Found by temporarily changing static_cast<sal_uInt16>() in sw/ to a
template function to make these conversions implicit (excluding cases
where the input and output types can't convert implicitly), then asking
-fsanitize=implicit-unsigned-integer-truncation
-fsanitize=implicit-signed-integer-truncation to flag the problematic
conversions.

The first hit was in SwFlyCntPortion::SetBase() (that's where 67408
turns into 1872), then the same pattern at many other places.

Change-Id: Ie12f490ed8dd5c531f11506c97598ce4f7610e2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115873
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/text/porlay.hxx
@@ -81,8 +81,8 @@ private:
     SwLineLayout *m_pNext;                // The next Line
     std::unique_ptr<std::vector<tools::Long>> m_pLLSpaceAdd;     // Used for justified alignment
     std::unique_ptr<std::deque<sal_uInt16>> m_pKanaComp;  // Used for Kana compression
-    sal_uInt16 m_nRealHeight;             // The height resulting from line spacing and register
-    sal_uInt16 m_nTextHeight;             // The max height of all non-FlyCnt portions in this Line
+    sal_uInt32 m_nRealHeight;             // The height resulting from line spacing and register
+    sal_uInt32 m_nTextHeight;             // The max height of all non-FlyCnt portions in this Line
     bool m_bFormatAdj : 1;
     bool m_bDummy     : 1;
     bool m_bEndHyph   : 1;
@@ -108,7 +108,7 @@ private:
 public:
     // From SwPosSize
     using SwPosSize::Height;
-    virtual void Height(const sal_uInt16 nNew, const bool bText = true) override;
+    virtual void Height(const sal_uInt32 nNew, const bool bText = true) override;
 
     // From SwLinePortion
     virtual SwLinePortion *Insert( SwLinePortion *pPortion ) override;
@@ -165,8 +165,8 @@ public:
     // Collects the data for the line
     void CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf );
 
-    void SetRealHeight( sal_uInt16 nNew ) { m_nRealHeight = nNew; }
-    sal_uInt16 GetRealHeight() const { return m_nRealHeight; }
+    void SetRealHeight( sal_uInt32 nNew ) { m_nRealHeight = nNew; }
+    sal_uInt32 GetRealHeight() const { return m_nRealHeight; }
 
     sal_uInt16 GetTextHeight() const { return m_nTextHeight; }
 

sw/source/core/text/pormulti.cxx
@@ -399,7 +399,7 @@ void SwDoubleLinePortion::FormatBrackets( SwTextFormatInfo &rInf, SwTwips& nMaxW
         m_pBracket->nAscent = rInf.GetAscent();
         m_pBracket->nHeight = aSize.Height();
         pTmpFnt->SetActual( nActualScr );
-        if( nMaxWidth > aSize.Width() )
+        if( nMaxWidth > static_cast<SwTwips>(aSize.Width()) )
         {
             m_pBracket->nPreWidth = aSize.Width();
             nMaxWidth -= aSize.Width();
@@ -428,7 +428,7 @@ void SwDoubleLinePortion::FormatBrackets( SwTextFormatInfo &rInf, SwTwips& nMaxW
         }
         if( aSize.Height() > m_pBracket->nHeight )
             m_pBracket->nHeight = aSize.Height();
-        if( nMaxWidth > aSize.Width() )
+        if( nMaxWidth > static_cast<SwTwips>(aSize.Width()) )
         {
             m_pBracket->nPostWidth = aSize.Width();
             nMaxWidth -= aSize.Width();
@@ -1679,7 +1679,7 @@ void SwTextPainter::PaintMultiPortion( const SwRect &rPaint,
         }
         else if ( rMulti.IsRuby() && rMulti.OnRight() && GetInfo().IsRuby() )
         {
-            SwTwips nLineDiff = std::max(( rMulti.GetRoot().Height() - pPor->Width() ) / 2, 0 );
+            SwTwips nLineDiff = std::max(( rMulti.GetRoot().Height() - pPor->Width() ) / 2, static_cast<sal_uInt32>(0) );
             GetInfo().Y( nOfst + nLineDiff );
             // Draw the ruby text on top of the preserved space.
             GetInfo().X( GetInfo().X() - pPor->Height() );
@@ -2168,7 +2168,7 @@ bool SwTextFormatter::BuildMultiPortion( SwTextFormatInfo &rInf,
             // Setting this to the portion width ( = rMulti.Width() )
             // can make GetTextBreak inside SwTextGuess::Guess return too small
             // values. Therefore we add some extra twips.
-            if( nActWidth > nTmpX + rMulti.Width() + 6 )
+            if( nActWidth > nTmpX + static_cast<SwTwips>(rMulti.Width()) + 6 )
                 nActWidth = nTmpX + rMulti.Width() + 6;
             nMaxWidth = nActWidth;
             nActWidth = ( 3 * nMaxWidth + nMinWidth + 3 ) / 4;
@@ -2519,7 +2519,7 @@ SwTextCursorSave::SwTextCursorSave( SwTextCursor* pCursor,
 {
     pCursor->m_nStart = nCurrStart;
     pCursor->m_pCurr = &pMulti->GetRoot();
-    while( pCursor->Y() + pCursor->GetLineHeight() < nY &&
+    while( pCursor->Y() + static_cast<SwTwips>(pCursor->GetLineHeight()) < nY &&
         pCursor->Next() )
         ; // nothing
     nWidth = pCursor->m_pCurr->Width();

sw/qa/core/text/text.cxx
@@ -135,6 +135,22 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTabOverMarginSection)
     CPPUNIT_ASSERT_LESS(static_cast<sal_Int32>(5000), nWidth);
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testLineHeight)
+{
+    // Given a document with an as-char image, height in twips not fitting into sal_uInt16:
+    createSwDoc(DATA_DIRECTORY, "line-height.fodt");
+
+    // When laying out that document:
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+
+    // Then make sure its top is the top of the page:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 284
+    // - Actual  : -65252
+    // due to various unsigned integer truncations.
+    assertXPath(pXmlDoc, "//fly/infos/bounds", "top", OUString::number(DOCUMENTBORDER));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/core/text/data/line-height.fodt
@@ -1,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" office:mimetype="application/vnd.oasis.opendocument.text">
+  <office:automatic-styles>
+    <style:style style:name="fr1" style:family="graphic" style:parent-style-name="Graphics"/>
+    <style:page-layout style:name="pm1">
+      <style:page-layout-properties fo:page-width="84.1cm" fo:page-height="118.9cm" fo:margin-top="0cm" fo:margin-bottom="0cm" fo:margin-left="0cm" fo:margin-right="0cm"/>
+    </style:page-layout>
+  </office:automatic-styles>
+  <office:master-styles>
+    <style:master-page style:name="Standard" style:page-layout-name="pm1"/>
+  </office:master-styles>
+  <office:body>
+    <office:text>
+      <text:p><draw:frame draw:style-name="fr1" draw:name="Image1" text:anchor-type="as-char" svg:width="84.1cm" svg:height="118.9cm" draw:z-index="0"><draw:image draw:mime-type="image/png"><office:binary-data>iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAQAAAAAYLlVAAAABGdBTUEAALGPC/xhBQAAAAFz
+       UkdCAK7OHOkAAAAgY0hSTQAAeiYAAICEAAD6AAAAgOgAAHUwAADqYAAAOpgAABdwnLpRPAAA
+       AAJiS0dEAACqjSMyAAAACW9GRnMAAAAGAAAAAAAMc1XTAAAACXBIWXMAAA3XAAAN1wFCKJt4
+       AAAACXZwQWcAAABMAAAAQACdMTgbAAABzUlEQVRo3u3ZPU/CQBjA8X+Jxs3ESUDj4iK+LA5+
+       BBfjqBE1cXB2MlFAEqMgxvhNNL4sLsK3UPQL6ObkoAETz+FKW2mxCPRYnucWUu76/OC59C49
+       cGOCKqrD9kHRc6ddPv7oW2WCwMh0nF63Myz7Tm8hPTNu0pgHMER3scepTbgK6enJNND83RLn
+       /878yRaPmgBZFDuMsNLeWB9gmFQHP77MIg9gsYciR50NFKvtjIy10yk84pSZA7DYpwR8scmF
+       QQCMuoQMpzbh0iAARrlnVn90CWHTsZcAiHPPdINQAuqsc2MQAAnKDUKWEhZ10twaBEDSJWQo
+       YlFj7S9CzwEegkXWIbQsRAQASFJhpplwbRAACS+hANRJBxMiAkDcJeQ4sQkBhYgMoJ+Ozlwo
+       2YQ7AJ6CRxyiUGnVy3hVKb0Af9v7hUG2Wy9TEQCUelFTDULB2S+YKYGOMcpM6UIccOQnRA6A
+       cSp6ibfI+wkGADBGpTEd8xz1AaAfTQ7huA8AvUw5hVjuA0D/C5OaMN8XACRZ8F0zCggKAQhA
+       AAIQgAAEIAABCEAAAhCAAAQgAAH4zg3feY4w3Xs44M5+oW0qvCWoGcvaIlM3x/f/ab+O738A
+       hOCNQr34oD4AAAAldEVYdGNyZWF0ZS1kYXRlADIwMTAtMTItMjBUMTc6MDg6MzYrMDE6MDB6
+       5RscAAAAJXRFWHRtb2RpZnktZGF0ZQAyMDEwLTEyLTIwVDE3OjA4OjM3KzAxOjAwgyNmnAAA
+       AABJRU5ErkJggg==
+      </office:binary-data></draw:image></draw:frame></text:p>
+    </office:text>
+  </office:body>
+</office:document>

sw/source/core/inc/txtfrm.hxx
@@ -578,7 +578,7 @@ public:
     virtual void CheckDirection( bool bVert ) override;
 
     /// Returns the sum of line height in pLine
-    sal_uInt16 GetParHeight() const;
+    sal_uInt32 GetParHeight() const;
 
     inline       SwTextFrame *GetFollow();
     inline const SwTextFrame *GetFollow() const;

sw/source/core/text/frmcrsr.cxx
@@ -590,7 +590,7 @@ bool SwTextFrame::GetModelPositionForViewPoint_(SwPosition* pPos, const Point& r
         // See comment in AdjustFrame()
         SwTwips nMaxY = getFrameArea().Top() + getFramePrintArea().Top() + getFramePrintArea().Height();
         aLine.TwipsToLine( rPoint.Y() );
-        while( aLine.Y() + aLine.GetLineHeight() > nMaxY )
+        while( aLine.Y() + static_cast<SwTwips>(aLine.GetLineHeight()) > nMaxY )
         {
             if( !aLine.Prev() )
                 break;

sw/source/core/text/frmform.cxx
@@ -1177,8 +1177,8 @@ bool SwTextFrame::FormatLine( SwTextFormatter &rLine, const bool bPrev )
     SwParaPortion *pPara = rLine.GetInfo().GetParaPortion();
     const SwLineLayout *pOldCur = rLine.GetCurr();
     const TextFrameIndex nOldLen = pOldCur->GetLen();
-    const sal_uInt16 nOldAscent = pOldCur->GetAscent();
-    const sal_uInt16 nOldHeight = pOldCur->Height();
+    const sal_uInt32 nOldAscent = pOldCur->GetAscent();
+    const sal_uInt32 nOldHeight = pOldCur->Height();
     const SwTwips nOldWidth = pOldCur->Width() + pOldCur->GetHangingMargin();
     const bool bOldHyph = pOldCur->IsEndHyph();
     SwTwips nOldTop = 0;
@@ -1200,7 +1200,7 @@ bool SwTextFrame::FormatLine( SwTextFormatter &rLine, const bool bPrev )
                   bOldHyph == pNew->IsEndHyph();
     if ( bUnChg && !bPrev )
     {
-        const tools::Long nWidthDiff = nOldWidth > pNew->Width()
+        const tools::Long nWidthDiff = nOldWidth > static_cast<SwTwips>(pNew->Width())
                                 ? nOldWidth - pNew->Width()
                                 : pNew->Width() - nOldWidth;
 
@@ -1254,7 +1254,7 @@ bool SwTextFrame::FormatLine( SwTextFormatter &rLine, const bool bPrev )
                 rLine.SetUnclipped( false );
             }
         }
-        SwTwips nRght = std::max( nOldWidth, pNew->Width() +
+        SwTwips nRght = std::max( nOldWidth, static_cast<SwTwips>(pNew->Width()) +
                              pNew->GetHangingMargin() );
         SwViewShell *pSh = getRootFrame()->GetCurrShell();
         const SwViewOption *pOpt = pSh ? pSh->GetViewOptions() : nullptr;

sw/source/core/text/frmpaint.cxx
@@ -356,7 +356,7 @@ void SwTextFrame::PaintExtraData( const SwRect &rRect ) const
         SwTextPainter  aLine( const_cast<SwTextFrame*>(this), &aInf );
         bool bNoDummy = !aLine.GetNext(); // Only one empty line!
 
-        while( aLine.Y() + aLine.GetLineHeight() <= rRect.Top() )
+        while( aLine.Y() + static_cast<SwTwips>(aLine.GetLineHeight()) <= rRect.Top() )
         {
             if( !aLine.GetCurr()->IsDummy() &&
                 ( rLineInf.IsCountBlankLines() ||
@@ -398,7 +398,7 @@ void SwTextFrame::PaintExtraData( const SwRect &rRect ) const
                         bool bNum = bLineNum && ( aExtra.HasNumber() || aExtra.HasDivider() );
                         if( bRedInMargin || bNum )
                         {
-                            sal_uInt16 nTmpHeight, nTmpAscent;
+                            sal_uInt32 nTmpHeight, nTmpAscent;
                             aLine.CalcAscentAndHeight( nTmpAscent, nTmpHeight );
                             if ( bRedInMargin )
                             {

commit 5f6fed508e9942ceb7b4ed6dc02d9c8dbe7c9415

tdf#144305 sw: fix rendering of ruby portions with non-default ruby alignment
Regression from fb5d1f54d3be7c1f12dbe60823a322ee66331c1e (sw: allow the
height of a line to be larger than 65536 twips, 2021-05-20), the problem
was that changing from sal_uInt16 to sal_uInt32 broke
SwRubyPortion::Adjust_(), which relied on integer promotion rules to
have a negative diff.

Old storage size was smaller than int, so got promoted to signed int, so
the result could be a small negative number.

New storage size is an unsigned int, so no promotion happens, so the new
result was a large positive number.

Fix this by casting to signed int explicitly.

Change-Id: I8778c1bd0d62e27c99d4ceb1bb7bc6107a179803
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122048
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/CppunitTest_sw_core_text.mk
@@ -40,6 +40,7 @@ $(eval $(call gb_CppunitTest_use_externals,sw_core_text,\
 $(eval $(call gb_CppunitTest_set_include,sw_core_text,\
     -I$(SRCDIR)/sw/inc \
     -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/core/text \
     -I$(SRCDIR)/sw/source/uibase/inc \
     -I$(SRCDIR)/sw/qa/inc \
     $$(INCLUDE) \

sw/source/core/text/porlay.hxx
@@ -77,7 +77,7 @@ public:
 
 /// Collection of SwLinePortion instances, representing one line of text.
 /// Typically owned by an SwParaPortion.
-class SwLineLayout : public SwTextPortion
+class SW_DLLPUBLIC SwLineLayout : public SwTextPortion
 {
 private:
     SwLineLayout *m_pNext;                // The next Line

sw/source/core/text/pormulti.cxx
@@ -630,7 +630,7 @@ SwRubyPortion::SwRubyPortion( const SwMultiCreator& rCreate, const SwFont& rFnt,
 // If there is a tabulator in smaller line, no adjustment is possible.
 void SwRubyPortion::Adjust_( SwTextFormatInfo &rInf )
 {
-    SwTwips nLineDiff = GetRoot().Width() - GetRoot().GetNext()->Width();
+    SwTwips nLineDiff = o3tl::narrowing<SwTwips>(GetRoot().Width()) - GetRoot().GetNext()->Width();
     TextFrameIndex const nOldIdx = rInf.GetIdx();
     if( !nLineDiff )
         return;

sw/qa/core/text/data/ruby.fodt
@@ -1,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+  <office:font-face-decls>
+  </office:font-face-decls>
+  <office:styles>
+  </office:styles>
+  <office:automatic-styles>
+    <style:style style:name="Ru1" style:family="ruby">
+      <style:ruby-properties style:ruby-align="left" style:ruby-position="above" loext:ruby-position="above"/>
+    </style:style>
+    <style:style style:name="Ru2" style:family="ruby">
+      <style:ruby-properties style:ruby-align="center" style:ruby-position="above" loext:ruby-position="above"/>
+    </style:style>
+    <style:style style:name="Ru3" style:family="ruby">
+      <style:ruby-properties style:ruby-align="right" style:ruby-position="above" loext:ruby-position="above"/>
+    </style:style>
+  </office:automatic-styles>
+  <office:master-styles>
+  </office:master-styles>
+  <office:body>
+    <office:text>
+      <text:p text:style-name="P1">Ruby test: <text:ruby text:style-name="Ru1"><text:ruby-base>base text</text:ruby-base><text:ruby-text text:style-name="Rubies">ruby (left)</text:ruby-text></text:ruby> <text:ruby text:style-name="Ru2"><text:ruby-base>base text</text:ruby-base><text:ruby-text>ruby (center)</text:ruby-text></text:ruby> <text:ruby text:style-name="Ru3"><text:ruby-base>base text</text:ruby-base><text:ruby-text>ruby (right)</text:ruby-text></text:ruby></text:p>
+    </office:text>
+  </office:body>
+</office:document>

sw/qa/core/text/text.cxx
@@ -21,6 +21,12 @@
 #include <docsh.hxx>
 #include <unotxdoc.hxx>
 #include <wrtsh.hxx>
+#include <IDocumentLayoutAccess.hxx>
+#include <rootfrm.hxx>
+#include <txtfrm.hxx>
+
+#include <porlay.hxx>
+#include <pormulti.hxx>
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/core/text/data/";
 
@@ -168,6 +174,46 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testLineWidth)
     // - Actual  : 1872
     // i.e. the width (around 67408 twips) was truncated.
     CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(65536), nNewLeft - nOldLeft);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testRuby)
+{
+    // Given a document with multiple ruby portions:
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "ruby.fodt");
+
+    // When laying out that document:
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+
+    // Then make sure that no unwanted margin portions are created, making the actual text
+    // invisible:
+    SwFrame* pPageFrame = pLayout->GetLower();
+    SwFrame* pBodyFrame = pPageFrame->GetLower();
+    SwFrame* pFrame = pBodyFrame->GetLower();
+    CPPUNIT_ASSERT(pFrame->IsTextFrame());
+    auto pTextFrame = static_cast<SwTextFrame*>(pFrame);
+    SwParaPortion* pPara = pTextFrame->GetPara();
+    bool bFirst = true;
+    for (SwLinePortion* pPor = pPara->GetFirstPortion(); pPor; pPor = pPor->GetNextPortion())
+    {
+        // Look for multi-portions in the only paragraph of the document.
+        if (pPor->GetWhichPor() != PortionType::Multi)
+        {
+            continue;
+        }
+
+        if (bFirst)
+        {
+            bFirst = false;
+            continue;
+        }
+
+        // The second multi-portion has two lines, check the start of the second line.
+        auto pMulti = static_cast<SwMultiPortion*>(pPor);
+        // Without the accompanying fix in place, this test would have failed, as the portion was a
+        // margin portion, not a text portion. The margin was so large that the actual text portion was
+        // hidden. No margin is needed here at all.
+        CPPUNIT_ASSERT(pMulti->GetRoot().GetNext()->GetFirstPortion()->IsTextPortion());
+    }
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();