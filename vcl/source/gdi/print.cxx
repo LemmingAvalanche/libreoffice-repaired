LemmingAvalanche/libreoffice-repaired

commit dca4216c68dfea48c34688463cfe63d613cd7ef2

WIN lazy init WinSalInfoPrinter graphics
... and while at it make stuff private and add _ to the newly
private member variables.

The new assert revealed a bug in the SalGraphics refcounting,
because the virtual ReleaseGraphics is called from the
destructor, which was probably also the reason for the HACK
and comment in Printer::ImplReleaseFonts.

Change-Id: I7af0bda19be6810dd8c0ea5b74604381e2047407
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122371
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/gdi/print.cxx
@@ -550,13 +550,7 @@ bool Printer::AcquireGraphics() const
 
 void Printer::ImplReleaseFonts()
 {
-#ifdef UNX
-    // HACK to fix an urgent P1 printing issue fast
-    // WinSalPrinter does not respect GetGraphics/ReleaseGraphics conventions
-    // so Printer::mpGraphics often points to a dead WinSalGraphics
-    // TODO: fix WinSalPrinter's GetGraphics/ReleaseGraphics handling
     mpGraphics->ReleaseFonts();
-#endif
     mbNewFont = true;
     mbInitFont = true;
 
@@ -565,7 +559,7 @@ void Printer::ImplReleaseFonts()
     mpDeviceFontSizeList.reset();
 }
 
-void Printer::ReleaseGraphics( bool bRelease )
+void Printer::ImplReleaseGraphics(bool bRelease)
 {
     DBG_TESTSOLARMUTEX();
 
@@ -616,6 +610,11 @@ void Printer::ReleaseGraphics( bool bRelease )
     mpGraphics      = nullptr;
     mpPrevGraphics  = nullptr;
     mpNextGraphics  = nullptr;
+}
+
+void Printer::ReleaseGraphics(bool bRelease)
+{
+    ImplReleaseGraphics(bRelease);
 }
 
 void Printer::ImplInit( SalPrinterQueueInfo* pInfo )
@@ -912,7 +911,7 @@ void Printer::dispose()
 
     mpPrinterOptions.reset();
 
-    ReleaseGraphics();
+    ImplReleaseGraphics();
     if ( mpInfoPrinter )
         ImplGetSVData()->mpDefInst->DestroyInfoPrinter( mpInfoPrinter );
     if ( mpDisplayDev )

include/vcl/print.hxx
@@ -124,6 +124,7 @@ public:
 protected:
     virtual bool                AcquireGraphics() const override;
     virtual void                ReleaseGraphics( bool bRelease = true ) override;
+    void ImplReleaseGraphics(bool bRelease = true);
     virtual void                ImplReleaseFonts() override;
 
     virtual tools::Long                GetGradientStepCount( tools::Long nMinRect ) override;

vcl/inc/win/salprn.h
@@ -40,18 +40,23 @@ struct SalDriverData
 
 class WinSalGraphics;
 
-class WinSalInfoPrinter : public SalInfoPrinter
+class WinSalInfoPrinter final : public SalInfoPrinter
 {
 public:
-    WinSalGraphics*        mpGraphics;             // current Printer graphics
     OUString               maDriverName;           // printer driver name
     OUString               maDeviceName;           // printer device name
     OUString               maPortName;             // printer port name
-    HDC                    mhDC;                   // printer hdc
-    bool                   mbGraphics;             // is Graphics used
+
+private:
+    HDC m_hDC;                    ///< printer hdc
+    WinSalGraphics* m_pGraphics;  ///< current Printer graphics
+    bool m_bGraphics;             ///< is Graphics used
+
 public:
     WinSalInfoPrinter();
     virtual ~WinSalInfoPrinter() override;
+
+    void setHDC(HDC);
 
     virtual SalGraphics*            AcquireGraphics() override;
     virtual void                    ReleaseGraphics( SalGraphics* pGraphics ) override;

vcl/win/gdi/salprn.cxx
@@ -1041,16 +1041,7 @@ static bool ImplUpdateSalPrnIC( WinSalInfoPrinter* pPrinter, const ImplJobSetup*
     if ( !hNewDC )
         return false;
 
-    if ( pPrinter->mpGraphics )
-    {
-        assert(pPrinter->mpGraphics->getHDC() == pPrinter->mhDC);
-        delete pPrinter->mpGraphics;
-        DeleteDC(pPrinter->mhDC);
-    }
-
-    pPrinter->mpGraphics = ImplCreateSalPrnGraphics( hNewDC );
-    pPrinter->mhDC      = hNewDC;
-
+    pPrinter->setHDC(hNewDC);
     return true;
 }
 
@@ -1075,8 +1066,7 @@ SalInfoPrinter* WinSalInstance::CreateInfoPrinter( SalPrinterQueueInfo* pQueueIn
         return nullptr;
     }
 
-    pPrinter->mpGraphics = ImplCreateSalPrnGraphics( hDC );
-    pPrinter->mhDC      = hDC;
+    pPrinter->setHDC(hDC);
     if ( !pSetupData->GetDriverData() )
         ImplUpdateSalJobSetup( pPrinter, pSetupData, false, nullptr );
     ImplDevModeToJobSetup( pPrinter, pSetupData, JobSetFlags::ALL );
@@ -1092,23 +1082,33 @@ void WinSalInstance::DestroyInfoPrinter( SalInfoPrinter* pPrinter )
 
 
 WinSalInfoPrinter::WinSalInfoPrinter() :
-    mpGraphics( nullptr ),
-    mhDC( nullptr ),
-    mbGraphics( false )
+    m_hDC(nullptr),
+    m_pGraphics(nullptr),
+    m_bGraphics(false)
 {
     m_bPapersInit = false;
 }
 
 WinSalInfoPrinter::~WinSalInfoPrinter()
 {
-    if ( mpGraphics )
+    setHDC(nullptr);
+}
+
+void WinSalInfoPrinter::setHDC(HDC hNewDC)
+{
+    assert(!m_bGraphics);
+
+    if (m_hDC)
     {
+        assert(!m_pGraphics || m_hDC == m_pGraphics->getHDC());
         // we get intermittent crashes on the Windows jenkins box around here, let us see if there is something weird about the DC
-        SAL_WARN("vcl", "Graphics DC " << mpGraphics->getHDC());
-        assert(mpGraphics->getHDC() == mhDC);
-        delete mpGraphics;
-        DeleteDC(mhDC);
+        SAL_WARN_IF(!hNewDC, "vcl", "Graphics DC " << m_hDC);
+        delete m_pGraphics;
+        m_pGraphics = nullptr;
+        DeleteDC(m_hDC);
     }
+
+    m_hDC = hNewDC;
 }
 
 void WinSalInfoPrinter::InitPaperFormats( const ImplJobSetup* pSetupData )
@@ -1149,18 +1149,21 @@ int WinSalInfoPrinter::GetLandscapeAngle( const ImplJobSetup* pSetupData )
 
 SalGraphics* WinSalInfoPrinter::AcquireGraphics()
 {
-    if ( mbGraphics )
+    assert(m_hDC);
+    if (m_bGraphics)
         return nullptr;
 
-    if ( mpGraphics )
-        mbGraphics = true;
+    if (!m_pGraphics)
+        m_pGraphics = ImplCreateSalPrnGraphics(m_hDC);
+    if (m_pGraphics)
+        m_bGraphics = true;
 
-    return mpGraphics;
+    return m_pGraphics;
 }
 
 void WinSalInfoPrinter::ReleaseGraphics( SalGraphics* )
 {
-    mbGraphics = false;
+    m_bGraphics = false;
 }
 
 bool WinSalInfoPrinter::Setup(weld::Window* pFrame, ImplJobSetup* pSetupData)
@@ -1266,7 +1269,7 @@ void WinSalInfoPrinter::GetPageInfo( const ImplJobSetup*,
                                   Point& rPageOffset,
                                   Size& rPaperSize )
 {
-    HDC hDC = mhDC;
+    HDC hDC = m_hDC;
 
     rOutWidth   = GetDeviceCaps( hDC, HORZRES );
     rOutHeight  = GetDeviceCaps( hDC, VERTRES );

commit 3f6fa3f1f892407ef2581de61e13e5470c287b4b

tdf#144952 VCL fix the Printer graphics release
Regression from commit dca4216c68dfea48c34688463cfe63d613cd7ef2
("WIN lazy init WinSalInfoPrinter graphics"), which removed the
// HACK to fix an urgent P1 printing issue fast

SalPrinter::StartPage returns a SalGraphics to the VCL Printer
object. On Windows SalPrinter::EndPage deletes the SalPrinter's
SalGraphics. MacOS keeps a single SalGraphics for the whole
SalPrinter lifetime. PspSalPrinter stores it in a unique_ptr
and always resets it in StartPage.

But in any case the VCL Printer's SalGraphics reference must be
freed before calling a function, which might delete it.

Change-Id: Ice528b2bbc89eaadff576420bf210ea4ec1e58d5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123199
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/gdi/print.cxx
@@ -1564,8 +1564,8 @@ void Printer::ImplEndPage()
 
     if ( mpPrinter )
     {
-        mpPrinter->EndPage();
         ReleaseGraphics();
+        mpPrinter->EndPage();
         mbDevOutput = false;
 
         mpJobGraphics = nullptr;