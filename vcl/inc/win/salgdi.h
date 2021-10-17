LemmingAvalanche/libreoffice-repaired

commit e60e218320875b0c410c456816a5840761b52c52

vcl:use unique_ptr<SalWinGraphics> in WinSalPrinter
to replace plain SalWinGraphics pointer, and get rid
of delete.

Change-Id: If06df98a8ebfdf5797bddfefa37647522b8e711b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123365
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/win/gdi/salprn.cxx
@@ -1350,7 +1350,6 @@ static DEVMODEW const * ImplSalSetCopies( DEVMODEW const * pDevMode, sal_uInt32 
 
 
 WinSalPrinter::WinSalPrinter() :
-    mpGraphics( nullptr ),
     mpInfoPrinter( nullptr ),
     mpNextPrinter( nullptr ),
     mhDC( nullptr ),
@@ -1374,7 +1373,6 @@ WinSalPrinter::~WinSalPrinter()
     HDC hDC = mhDC;
     if ( hDC )
     {
-        delete mpGraphics;
         DeleteDC( hDC );
     }
 
@@ -1531,8 +1529,7 @@ bool WinSalPrinter::EndJob()
     HDC hDC = mhDC;
     if (isValid())
     {
-        delete mpGraphics;
-        mpGraphics = nullptr;
+        mxGraphics.reset();
 
         // #i54419# Windows fax printer brings up a dialog in EndDoc
         // which text previously copied in soffice process can be
@@ -1587,14 +1584,13 @@ SalGraphics* WinSalPrinter::StartPage( ImplJobSetup* pSetupData, bool bNewJobDat
     SelectPen( hDC, hTempPen );
     SelectBrush( hDC, hTempBrush );
 
-    mpGraphics = ImplCreateSalPrnGraphics( hDC );
-    return mpGraphics;
+    mxGraphics.reset(ImplCreateSalPrnGraphics( hDC ));
+    return mxGraphics.get();
 }
 
 void WinSalPrinter::EndPage()
 {
-    delete mpGraphics;
-    mpGraphics = nullptr;
+    mxGraphics.reset();
 
     if (!isValid())
         return;

vcl/inc/win/salprn.h
@@ -78,7 +78,7 @@ public:
 class WinSalPrinter : public SalPrinter
 {
 public:
-    WinSalGraphics*         mpGraphics;             // current Printer graphics
+    std::unique_ptr<WinSalGraphics> mxGraphics;    // current Printer graphics
     WinSalInfoPrinter*      mpInfoPrinter;          // pointer to the compatible InfoPrinter
     WinSalPrinter*          mpNextPrinter;          // next printing printer
     HDC                     mhDC;                   // printer hdc

commit c350b688d9354d805878eabbae9e43a1710ee87e

WIN release SalPrinter's SalGraphics borrowed HDC
Regression from commit e60e218320875b0c410c456816a5840761b52c52
("vcl:use unique_ptr<SalWinGraphics> in WinSalPrinter").

Would need something like Rust's Borrowing semantics to prevent.
Ideas on IRC to make this bug more unlikely were some extra HDC +
SalGraphics struct to pass around or something like unique_ptr
reference passing and moving the value into a local copy.

For now just add some additional comments.

Change-Id: I472ee9acb4a4c02177c27ecd1c1277dfc812cadf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123440
Tested-by: Jenkins
Reviewed-by: Mark Hung <marklh9@gmail.com>

vcl/inc/win/salgdi.h
@@ -141,6 +141,13 @@ public:
     void fill(sal_uInt32 color);
 };
 
+/**
+ * WinSalGraphics never owns the HDC it uses to draw, because the HDC can have
+ * various origins with different ways to correctly free it. And WinSalGraphics
+ * stores all default values (mhDef*) of the HDC, which must be restored when
+ * the HDC changes (setHDC) or the SalGraphics is destructed. So think of the
+ * HDC in terms of Rust's Borrowing semantics.
+ */
 class WinSalGraphics : public SalGraphics
 {
     friend class WinSalGraphicsImpl;
@@ -177,6 +184,7 @@ public:
     HFONT ImplDoSetFont(vcl::font::FontSelectPattern const & i_rFont, const vcl::font::PhysicalFontFace * i_pFontFace, HFONT& o_rOldFont);
 
     HDC getHDC() const { return mhLocalDC; }
+    // NOTE: this doesn't transfer ownership! See class comment.
     void setHDC(HDC aNew);
 
     HPALETTE getDefPal() const;

vcl/win/gdi/salprn.cxx
@@ -1373,6 +1373,9 @@ WinSalPrinter::~WinSalPrinter()
     HDC hDC = mhDC;
     if ( hDC )
     {
+        // explicitly reset(), so the mxGraphics's borrowed HDC defaults are
+        // restored and WinSalGraphics's destructor won't work on a deleted HDC.
+        mxGraphics.reset();
         DeleteDC( hDC );
     }
 