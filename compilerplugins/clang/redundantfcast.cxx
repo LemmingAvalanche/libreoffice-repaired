LemmingAvalanche/libreoffice-repaired

commit 09f146a9dee354b4d7dc2f94c9502bddd8025111

loplugin:moveparam in connectivity
Change-Id: Ie5c2f6ec1573ab2c47218dd4da8c9150b23bcd2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123247
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -801,7 +801,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTableTypes(  )
     aRow[1] = new ORowSetValueDecorator(OUString("SYSTEM TABLE"));
     aResults.push_back(aRow);
 
-    pResultSet->setRows(aResults);
+    pResultSet->setRows(std::move(aResults));
     return pResultSet;
 }
 
@@ -1026,7 +1026,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         tmp.push_back(aRow);
         return tmp;
     }();
-    pResultSet->setRows(aResults);
+    pResultSet->setRows(std::move(aResults));
     return pResultSet;
 }
 
@@ -1099,7 +1099,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getColumnPrivileges(
         aResults.push_back(aCurrentRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1307,7 +1307,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getColumns(
     }
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet = new
             ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::eColumns);
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1424,7 +1424,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTables(
         aResults.push_back(aCurrentRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1552,7 +1552,7 @@ uno::Reference< XResultSet > ODatabaseMetaData::lcl_getKeys(const bool bIsImport
         aResults.push_back(aCurrentRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
     return pResultSet;
 }
 
@@ -1606,7 +1606,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getPrimaryKeys(
     }
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet = new
             ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::ePrimaryKeys);
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1699,7 +1699,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getIndexInfo(
     }
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet = new
             ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::ePrimaryKeys);
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1774,7 +1774,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTablePrivileges(
         aResults.push_back(aRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }

connectivity/source/drivers/jdbc/DatabaseMetaData.cxx
@@ -481,7 +481,7 @@ Reference< XResultSet > SAL_CALL java_sql_DatabaseMetaData::getTablePrivileges(
 
                     aRows.push_back(aRow);
                 }
-                pNewPrivRes->setRows(aRows);
+                pNewPrivRes->setRows(std::move(aRows));
             }
         }
     }

connectivity/source/commontools/FDatabaseMetaDataResultSet.cxx
@@ -143,9 +143,9 @@ Sequence< Type > SAL_CALL ODatabaseMetaDataResultSet::getTypes(  )
     return ::comphelper::concatSequences(aTypes.getTypes(),ODatabaseMetaDataResultSet_BASE::getTypes());
 }
 
-void ODatabaseMetaDataResultSet::setRows(const ORows& _rRows)
+void ODatabaseMetaDataResultSet::setRows(ORows&& _rRows)
 {
-    m_aRows = _rRows;
+    m_aRows = std::move(_rRows);
     m_bBOF = true;
     m_bEOF = m_aRows.empty();
 }
@@ -822,7 +822,7 @@ void SAL_CALL ODatabaseMetaDataResultSet::initialize( const Sequence< Any >& _aA
         }
         aRowsToSet.push_back(aRowToSet);
     } // for (; pRowsIter != pRowsEnd;++pRowsIter
-    setRows(aRowsToSet);
+    setRows(std::move(aRowsToSet));
 }
 // XServiceInfo
 

connectivity/source/commontools/TDatabaseMetaDataBase.cxx
@@ -164,7 +164,7 @@ Reference< XResultSet > SAL_CALL ODatabaseMetaDataBase::getTypeInfo(  )
         }
     }
     rtl::Reference<::connectivity::ODatabaseMetaDataResultSet> pResult = new ::connectivity::ODatabaseMetaDataResultSet(::connectivity::ODatabaseMetaDataResultSet::eTypeInfo);
-    pResult->setRows(m_aTypeInfoRows);
+    pResult->setRows(std::vector(m_aTypeInfoRows));
     return pResult;
 }
 

connectivity/source/commontools/TPrivilegesResultSet.cxx
@@ -74,7 +74,7 @@ OResultSetPrivileges::OResultSetPrivileges( const Reference< XDatabaseMetaData>&
         aRow[6] = new ORowSetValueDecorator(OUString("REFERENCE"));
         aRows.push_back(aRow);
 
-        setRows(aRows);
+        setRows(std::move(aRows));
     }
     osl_atomic_decrement( &m_refCount );
 }

connectivity/source/drivers/ado/ADatabaseMetaData.cxx
@@ -347,7 +347,7 @@ Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTablePrivileges(
         aRows.push_back(aRow);
         aRow[6] = ::connectivity::ODatabaseMetaDataResultSet::getDropValue();
         aRows.push_back(aRow);
-        pResult->setRows(aRows);
+        pResult->setRows(std::move(aRows));
     }
 
     return xRef;

connectivity/source/drivers/calc/CDatabaseMetaData.cxx
@@ -209,7 +209,7 @@ Reference< XResultSet > SAL_CALL OCalcDatabaseMetaData::getTables(
         }
     }
 
-    pResult->setRows(aRows);
+    pResult->setRows(std::move(aRows));
 
     return pResult;
 }

connectivity/source/drivers/component/CDatabaseMetaData.cxx
@@ -115,7 +115,7 @@ Reference< XResultSet > OComponentDatabaseMetaData::impl_getTypeInfo_throw(  )
         aRows.push_back(aRow);
     }
 
-    pResult->setRows(aRows);
+    pResult->setRows(std::move(aRows));
     return pResult;
 }
 
@@ -207,7 +207,7 @@ Reference< XResultSet > SAL_CALL OComponentDatabaseMetaData::getColumns(
     }
 
     rtl::Reference<ODatabaseMetaDataResultSet> pResult = new ODatabaseMetaDataResultSet(::connectivity::ODatabaseMetaDataResultSet::eColumns);
-    pResult->setRows(aRows);
+    pResult->setRows(std::move(aRows));
 
     return pResult;
 }

commit c7f9430293df2a09008148e2e4ff817a16bf8e2b

tdf#145203: FIREBIRD cannot create a table
Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=09f146a9dee354b4d7dc2f94c9502bddd8025111
2021-10-08
loplugin:moveparam in connectivity

Change-Id: Iec0fa1597e63d42ee3a905bf55eff798b3f46177
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123787
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

compilerplugins/clang/redundantfcast.cxx
@@ -331,6 +331,9 @@ public:
         // TODO constructing a temporary to pass to a && param
         if (fn == SRCDIR "/sc/source/ui/view/viewfunc.cxx")
             return false;
+        // tdf#145203: FIREBIRD cannot create a table
+        if (fn == SRCDIR "/connectivity/source/drivers/firebird/DatabaseMetaData.cxx")
+            return false;
         return true;
     }
 

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -1026,7 +1026,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         tmp.push_back(aRow);
         return tmp;
     }();
-    pResultSet->setRows(std::move(aResults));
+    pResultSet->setRows(ODatabaseMetaDataResultSet::ORows(aResults));
     return pResultSet;
 }
 