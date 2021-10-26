LemmingAvalanche/libreoffice-repaired

commit de094f915b352d678a0bb8fb0f2be56cf18520ab

tdf#116305 Add timeout to help Windows bring browsers to the front
Adding a timeout seems to help Windows a lot to actually bring
the browser into the front, especially when it was closed before.

Still no 100% success rate, but much improved now.

Change-Id: I62affee4b837e0a60b1aac2a20be6fe7c3f9d2e0
Reviewed-on: https://gerrit.libreoffice.org/51132
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

sfx2/source/appl/openuriexternally.cxx
@@ -30,27 +30,55 @@
 #include <sfx2/viewsh.hxx>
 #include <sfx2/strings.hrc>
 
-bool sfx2::openUriExternally(
-    OUString const & uri, bool handleSystemShellExecuteException)
+class URITools
+{
+private:
+    Timer aOpenURITimer;
+    OUString msURI;
+    bool mbHandleSystemShellExecuteException;
+    DECL_LINK(onOpenURI, Timer*, void);
+
+public:
+    void openURI(const OUString& sURI, bool bHandleSystemShellExecuteException);
+};
+
+void URITools::openURI(const OUString& sURI, bool bHandleSystemShellExecuteException)
 {
     if (comphelper::LibreOfficeKit::isActive())
     {
-        if(SfxViewShell* pViewShell = SfxViewShell::Current())
+        if (SfxViewShell* pViewShell = SfxViewShell::Current())
         {
             pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_HYPERLINK_CLICKED,
-                                                   uri.toUtf8().getStr());
-            return true;
+                                                   sURI.toUtf8().getStr());
         }
-        return false;
+        delete this;
+        return;
     }
 
+    mbHandleSystemShellExecuteException = bHandleSystemShellExecuteException;
+    msURI = sURI;
+
+    // tdf#116305 Workaround: Use timer to bring browsers to the front
+    aOpenURITimer.SetInvokeHandler(LINK(this, URITools, onOpenURI));
+#ifdef WNT
+    // 200ms seems to be the the best compromise between responsiveness and success rate
+    aOpenURITimer.SetTimeout(200);
+#else
+    aOpenURITimer.SetTimeout(0);
+#endif
+    aOpenURITimer.SetDebugName("sfx2::openUriExternallyTimer");
+    aOpenURITimer.Start();
+}
+
+IMPL_LINK_NOARG(URITools, onOpenURI, Timer*, void)
+{
     css::uno::Reference< css::system::XSystemShellExecute > exec(
         css::system::SystemShellExecute::create(comphelper::getProcessComponentContext()));
     try {
         exec->execute(
-            uri, OUString(),
+            msURI, OUString(),
             css::system::SystemShellExecuteFlags::URIS_ONLY);
-        return true;
+        return;
     } catch (css::lang::IllegalArgumentException & e) {
         if (e.ArgumentPosition != 0) {
             throw css::uno::RuntimeException(
@@ -61,10 +89,10 @@ bool sfx2::openUriExternally(
         std::unique_ptr<weld::MessageDialog> eb(Application::CreateMessageDialog(pWindow ? pWindow->GetFrameWeld() : nullptr,
                                                                  VclMessageType::Warning, VclButtonsType::Ok,
                                                                  SfxResId(STR_NO_ABS_URI_REF)));
-        eb->set_primary_text(eb->get_primary_text().replaceFirst("$(ARG1)", uri));
+        eb->set_primary_text(eb->get_primary_text().replaceFirst("$(ARG1)", msURI));
         eb->run();
     } catch (css::system::SystemShellExecuteException & e) {
-        if (!handleSystemShellExecuteException) {
+        if (!mbHandleSystemShellExecuteException) {
             throw;
         }
         SolarMutexGuard g;
@@ -73,13 +101,19 @@ bool sfx2::openUriExternally(
                                                                  VclMessageType::Warning, VclButtonsType::Ok,
                                                                  SfxResId(STR_NO_WEBBROWSER_FOUND)));
         eb->set_primary_text(
-            eb->get_primary_text().replaceFirst("$(ARG1)", uri)
+            eb->get_primary_text().replaceFirst("$(ARG1)", msURI)
             .replaceFirst("$(ARG2)", OUString::number(e.PosixError))
             .replaceFirst("$(ARG3)", e.Message));
             //TODO: avoid subsequent replaceFirst acting on previous replacement
         eb->run();
     }
-    return false;
+    delete this;
+}
+
+void sfx2::openUriExternally(const OUString& sURI, bool bHandleSystemShellExecuteException)
+{
+    URITools* uriTools = new URITools;
+    uriTools->openURI(sURI, bHandleSystemShellExecuteException);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/inc/openuriexternally.hxx
@@ -12,24 +12,20 @@
 
 #include <sal/config.h>
 
+namespace sfx2
+{
+/** Open a URI via com.sun.star.system.SystemShellExecute
 
-namespace sfx2 {
+    Handles XSystemShellExecute.execute's IllegalArgumentException (throwing a
+    RuntimeException if it is unexpected, i.e., not caused by the given uri not
+    being an absolute URI reference).
 
-/// Open a URI via com.sun.star.system.SystemShellExecute
-///
-/// Handles XSystemShellExecute.execute's IllegalArgumentException (throwing a
-/// RuntimeException if it is unexpected, i.e., not caused by the given uri not
-/// being an absolute URI reference).
-///
-/// Handles XSystemShellExecute.execute's SystemShellExecuteException unless the
-/// given handleSystemShellExecuteException is false (in which case the
-/// exception is re-thrown).
-///
-/// @return true iff execution was successful
-bool openUriExternally(
-    OUString const & uri, bool handleSystemShellExecuteException);
-
-}
+    Handles XSystemShellExecute.execute's SystemShellExecuteException unless the
+    given handleSystemShellExecuteException is false (in which case the
+    exception is re-thrown).
+*/
+void openUriExternally(const OUString& sURI, bool bHandleSystemShellExecuteException);
+};
 
 #endif
 

sfx2/source/view/viewsh.cxx
@@ -641,7 +641,8 @@ void SfxViewShell::ExecMisc_Impl( SfxRequest &rReq )
                     return;
                 }
 
-                rReq.Done(sfx2::openUriExternally(aFileURL, true));
+                sfx2::openUriExternally(aFileURL, true);
+                rReq.Done(true);
                 break;
             }
             else

commit 7ac2f229bd89039b5720fafd548eea99ccca81f4

Fix a memory leak
...introduced with de094f915b352d678a0bb8fb0f2be56cf18520ab "tdf#116305 Add
timeout to help Windows bring browsers to the front".  (And remove a redundant
return statement, which had originally been `return true;`.)

Change-Id: Idf7fe33b25cd243e31eea460e4af325c84dae88b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124174
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

sfx2/source/appl/openuriexternally.cxx
@@ -77,13 +77,13 @@ void URITools::openURI(const OUString& sURI, bool bHandleSystemShellExecuteExcep
 
 IMPL_LINK_NOARG(URITools, onOpenURI, Timer*, void)
 {
+    std::unique_ptr<URITools> guard(this);
     css::uno::Reference< css::system::XSystemShellExecute > exec(
         css::system::SystemShellExecute::create(comphelper::getProcessComponentContext()));
     try {
         exec->execute(
             msURI, OUString(),
             css::system::SystemShellExecuteFlags::URIS_ONLY);
-        return;
     } catch (css::lang::IllegalArgumentException & e) {
         if (e.ArgumentPosition != 0) {
             throw css::uno::RuntimeException(
@@ -112,7 +112,6 @@ IMPL_LINK_NOARG(URITools, onOpenURI, Timer*, void)
             //TODO: avoid subsequent replaceFirst acting on previous replacement
         eb->run();
     }
-    delete this;
 }
 
 void sfx2::openUriExternally(const OUString& sURI, bool bHandleSystemShellExecuteException)