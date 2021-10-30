LemmingAvalanche/libreoffice-repaired

commit bffc3dfb01df47cbc3c59b0abc5c112bbbc0f790

fix xhtml list export in other locales
where other locales will have lists with ',' instead of '.'
which generates an invalid HTML margin-width value

Change-Id: I30ec6f88140ba1187f7c30b697dfe82055b2e25c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111604
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

filter/source/xslt/odf2xhtml/export/common/body.xsl
@@ -105,7 +105,8 @@
         <!-- line breaks in lists need an indent similar to the list label -->
         <xsl:if test="$listIndent">
             <xsl:element namespace="{$namespace}" name="span">
-                <xsl:attribute name="style">margin-left:<xsl:value-of select="$listIndent"/>cm</xsl:attribute>
+                <!-- some locales use , instead of . so replace it -->
+                <xsl:attribute name="style">margin-left:<xsl:value-of select="replace($listIndent, ',', '.')"/>cm</xsl:attribute>
             </xsl:element>
         </xsl:if>
     </xsl:template>

commit 6e4deefcb8e0982538e1d2f003952d5d0a2c63d3

tdf#145361 fix XSLT for older libxslt
regression from
    commit bffc3dfb01df47cbc3c59b0abc5c112bbbc0f790
    Author: Noel <noel.grandin@collabora.co.uk>
    Date:   Fri Feb 26 12:45:19 2021 +0200
    fix xhtml list export in other locales
where I used 'replace' which is not available on older libxslt, so
switch to 'translate' which works just as well here, and is available

Change-Id: Idd1224e23c0d48956d94b6fc2c2a7b1afa05fa0c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124435
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

filter/source/xslt/odf2xhtml/export/common/body.xsl
@@ -106,7 +106,7 @@
         <xsl:if test="$listIndent">
             <xsl:element namespace="{$namespace}" name="span">
                 <!-- some locales use , instead of . so replace it -->
-                <xsl:attribute name="style">margin-left:<xsl:value-of select="replace($listIndent, ',', '.')"/>cm</xsl:attribute>
+                <xsl:attribute name="style">margin-left:<xsl:value-of select="translate($listIndent, ',', '.')"/>cm</xsl:attribute>
             </xsl:element>
         </xsl:if>
     </xsl:template>