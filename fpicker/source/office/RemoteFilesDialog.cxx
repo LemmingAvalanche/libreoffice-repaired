LemmingAvalanche/libreoffice-repaired

commit b6a339017e681c6dc8a4afa90aa5de576a07b47a

tdf#145169 for IsDocument/IsFolder I think we want no interaction
rather than installing something to sometimes disable interaction
I think we just want to not have any interaction here at all

Change-Id: Ic775f0cd88f796db873d2980bb40043caac498bf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123750
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

fpicker/source/office/RemoteFilesDialog.cxx
@@ -22,6 +22,7 @@
 #include <svtools/PlaceEditDialog.hxx>
 #include <tools/debug.hxx>
 #include <ucbhelper/commandenvironment.hxx>
+#include <svl/fstathelper.hxx>
 #include <vcl/errinf.hxx>
 #include <officecfg/Office/Common.hxx>
 
@@ -1142,62 +1143,12 @@ std::vector<OUString> RemoteFilesDialog::GetPathList() const
 
 bool RemoteFilesDialog::ContentIsFolder( const OUString& rURL )
 {
-    try
-    {
-        Reference< XInteractionHandler > xInteractionHandler(
-                        InteractionHandler::createWithParent( m_xContext, nullptr ), UNO_QUERY_THROW );
-        INetURLObject aURLObject(rURL);
-        if (aURLObject.isAnyKnownWebDAVScheme() || aURLObject.GetProtocol() == INetProtocol::Sftp)
-            xInteractionHandler.set(new comphelper::StillReadWriteInteraction(xInteractionHandler, xInteractionHandler));
-        Reference< XCommandEnvironment > xEnv = new ::ucbhelper::CommandEnvironment( xInteractionHandler, Reference< XProgressHandler >() );
-        ::ucbhelper::Content aContent( rURL, xEnv, m_xContext );
-
-        return aContent.isFolder();
-    }
-    catch( const Exception& )
-    {
-        // a content doesn't exist
-    }
-
-    return false;
+    return FStatHelper::IsFolder(rURL);
 }
 
 bool RemoteFilesDialog::ContentIsDocument( const OUString& rURL )
 {
-    try
-    {
-        Reference< XInteractionHandler > xInteractionHandler(
-                        InteractionHandler::createWithParent( m_xContext, nullptr ), UNO_QUERY_THROW );
-        //check if WebDAV or not
-        INetURLObject aURLObject(rURL);
-        if (!aURLObject.isAnyKnownWebDAVScheme() && aURLObject.GetProtocol() != INetProtocol::Sftp)
-        {
-                // no webdav, use the interaction handler as is
-                Reference< XCommandEnvironment > xEnv = new ::ucbhelper::CommandEnvironment( xInteractionHandler, Reference< XProgressHandler >() );
-                ::ucbhelper::Content aContent( rURL, xEnv, m_xContext );
-
-                return aContent.isDocument();
-        }
-        else
-        {
-            // It's a webdav URL, so use the same open sequence as in normal open process.
-            // Let's use a comphelper::StillReadWriteInteraction to trap errors here without showing the user.
-            // This sequence will result in an exception if the target URL resource is not present
-            rtl::Reference<comphelper::StillReadWriteInteraction> pInteraction = new comphelper::StillReadWriteInteraction(xInteractionHandler,xInteractionHandler);
-
-            Reference< XCommandEnvironment > xEnv = new ::ucbhelper::CommandEnvironment( pInteraction, Reference< XProgressHandler >() );
-            ::ucbhelper::Content aContent( rURL, xEnv, m_xContext );
-
-            aContent.openStream();
-            return aContent.isDocument();
-        }
-    }
-    catch( const Exception& )
-    {
-        // a content doesn't exist
-    }
-
-    return false;
+    return FStatHelper::IsDocument(rURL);
 }
 
 sal_Int32 RemoteFilesDialog::getAvailableWidth()

fpicker/source/office/RemoteFilesDialog.hxx
@@ -68,7 +68,7 @@ public:
     virtual const OUString& GetPath() override;
     virtual std::vector<OUString> GetPathList() const override;
     virtual bool ContentIsFolder( const OUString& rURL ) override;
-    bool ContentIsDocument( const OUString& rURL );
+    static bool ContentIsDocument(const OUString& rURL);
 
     virtual OUString getCurrentFileText() const override;
     virtual void setCurrentFileText( const OUString& rText, bool bSelectAll = false ) override;

commit 4cc7f9e28cf58fce27b4e091788810f916c1a3f2

tdf#145769 fpicker: RemoteFilesDialog: don't use FStatHelper
FStatHelper doesn't use an XCommandEnvironment and is thus unsuitable
for remote access as it can't even authenticate with the password
container; it should only be used for local access.

Due to this problem, the ContentIsDocument() would always return a
hard-coded "true" value from the webdav UCP, causing a spurious dialog
that an existing file would be overwritten in Save Remote.

(regression from b6a339017e681c6dc8a4afa90aa5de576a07b47a)

Change-Id: Ibe667f2012e261b84d4c69c84fdd499b7276e64b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125666
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

fpicker/source/office/RemoteFilesDialog.cxx
@@ -22,7 +22,7 @@
 #include <svtools/PlaceEditDialog.hxx>
 #include <tools/debug.hxx>
 #include <ucbhelper/commandenvironment.hxx>
-#include <svl/fstathelper.hxx>
+#include <unotools/ucbhelper.hxx>
 #include <vcl/errinf.hxx>
 #include <officecfg/Office/Common.hxx>
 
@@ -1140,12 +1140,32 @@ std::vector<OUString> RemoteFilesDialog::GetPathList() const
 
 bool RemoteFilesDialog::ContentIsFolder( const OUString& rURL )
 {
-    return FStatHelper::IsFolder(rURL);
+    try
+    {
+        ::ucbhelper::Content content(rURL,
+            ::utl::UCBContentHelper::getDefaultCommandEnvironment(),
+            m_xContext);
+        return content.isFolder();
+    }
+    catch (css::uno::Exception const&)
+    {
+        return false;
+    }
 }
 
 bool RemoteFilesDialog::ContentIsDocument( const OUString& rURL )
 {
-    return FStatHelper::IsDocument(rURL);
+    try
+    {
+        ::ucbhelper::Content content(rURL,
+            ::utl::UCBContentHelper::getDefaultCommandEnvironment(),
+            m_xContext);
+        return content.isDocument();
+    }
+    catch (css::uno::Exception const&)
+    {
+        return false;
+    }
 }
 
 sal_Int32 RemoteFilesDialog::getAvailableWidth()

fpicker/source/office/RemoteFilesDialog.hxx
@@ -68,7 +68,7 @@ public:
     virtual const OUString& GetPath() override;
     virtual std::vector<OUString> GetPathList() const override;
     virtual bool ContentIsFolder( const OUString& rURL ) override;
-    static bool ContentIsDocument(const OUString& rURL);
+    bool ContentIsDocument(const OUString& rURL);
 
     virtual OUString getCurrentFileText() const override;
     virtual void setCurrentFileText( const OUString& rText, bool bSelectAll = false ) override;