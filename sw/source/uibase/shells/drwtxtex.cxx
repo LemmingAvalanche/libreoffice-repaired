LemmingAvalanche/libreoffice-repaired

commit 5f6e172d13c59c2ec184e10f6d96ea7c63766f2a

tdf#112543 Add Edit/Remove hyperlink to text boxes/shapes in Writer
Change-Id: I9052387cf39135c2324b9db03662dc0365323400
Reviewed-on: https://gerrit.libreoffice.org/75479
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

sw/source/uibase/shells/drwtxtex.cxx
@@ -47,6 +47,7 @@
 #include <editeng/contouritem.hxx>
 #include <editeng/postitem.hxx>
 #include <editeng/frmdiritem.hxx>
+#include <editeng/urlfieldhelper.hxx>
 #include <svx/svdoutl.hxx>
 #include <sfx2/viewfrm.hxx>
 #include <svl/stritem.hxx>
@@ -485,6 +486,21 @@ void SwDrawTextShell::Execute( SfxRequest &rReq )
         }
         break;
 
+        case FN_EDIT_HYPERLINK:
+        {
+            // Ensure the field is selected first
+            pOLV->GetFieldAtCursor();
+            GetView().GetViewFrame()->GetDispatcher()->Execute(SID_HYPERLINK_DIALOG);
+        }
+        break;
+
+        case FN_REMOVE_HYPERLINK:
+        {
+            URLFieldHelper::RemoveURLField(pSdrView->GetTextEditOutliner(),
+                                           pOLV);
+        }
+        break;
+
         case SID_TEXTDIRECTION_LEFT_TO_RIGHT:
         case SID_TEXTDIRECTION_TOP_TO_BOTTOM:
             // Shell switch!
@@ -884,12 +900,19 @@ void SwDrawTextShell::GetState(SfxItemSet& rSet)
                     rSet.DisableItem(nWhich);
             }
             break;
+            case FN_REMOVE_HYPERLINK:
+            case FN_EDIT_HYPERLINK:
+            {
+                if (!URLFieldHelper::IsCursorAtURLField(pOLV))
+                    rSet.DisableItem(nWhich);
+            }
+            break;
             default:
                 nSlotId = 0; // don't know this slot
                 break;
         }
 
-        if (nSlotId)
+        if (nSlotId && bFlag)
             rSet.Put(SfxBoolItem(nWhich, bFlag));
 
         nWhich = aIter.NextWhich();

editeng/source/misc/urlfieldhelper.cxx
@@ -24,4 +24,21 @@ void URLFieldHelper::RemoveURLField(Outliner* pOutl, OutlinerView* pOLV)
     }
 }
 
+bool URLFieldHelper::IsCursorAtURLField(OutlinerView* pOLV)
+{
+    if (!pOLV)
+        return false;
+
+    const SvxFieldItem* pFieldItem = pOLV->GetFieldUnderMousePointer();
+    if (!pFieldItem)
+        pFieldItem = pOLV->GetFieldAtSelection();
+    if (pFieldItem)
+    {
+        const SvxFieldData* pField = pFieldItem->GetField();
+        if (dynamic_cast<const SvxURLField*>(pField))
+            return true;
+    }
+    return false;
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

include/editeng/urlfieldhelper.hxx
@@ -17,6 +17,7 @@ class EDITENG_DLLPUBLIC URLFieldHelper
 {
 public:
     static void RemoveURLField(Outliner* pOutl, OutlinerView* pOLV);
+    static bool IsCursorAtURLField(OutlinerView* pOLV);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

sd/source/ui/view/drviewsf.cxx
@@ -475,22 +475,8 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
 
             case SID_REMOVE_HYPERLINK:
             {
-                OutlinerView* pOLV = mpDrawView->GetTextEditOutlinerView();
-                if (pOLV)
-                {
-                    bool bField = false;
-                    const SvxFieldItem* pFieldItem = pOLV->GetFieldUnderMousePointer();
-                    if (!pFieldItem)
-                        pFieldItem = pOLV->GetFieldAtSelection();
-                    if (pFieldItem)
-                    {
-                        const SvxFieldData* pField = pFieldItem->GetField();
-                        if (dynamic_cast<const SvxURLField*>(pField))
-                            bField = true;
-                    }
-                    if (!bField)
-                        rSet.DisableItem(nWhich);
-                }
+                if (!URLFieldHelper::IsCursorAtURLField(mpDrawView->GetTextEditOutlinerView()))
+                    rSet.DisableItem(nWhich);
             }
             break;
 

sw/sdi/drwtxtsh.sdi
@@ -546,6 +546,18 @@ interface TextDrawText
     [
         StateMethod = StateInsert ;
     ]
+    FN_EDIT_HYPERLINK
+    [
+        ExecMethod = Execute ;
+        StateMethod = GetState;
+        DisableFlags="SfxDisableFlags::SwOnProtectedCursor";
+    ]
+    FN_REMOVE_HYPERLINK
+    [
+        ExecMethod = Execute ;
+        StateMethod = GetState;
+        DisableFlags="SfxDisableFlags::SwOnProtectedCursor";
+    ]
     SID_TRANSLITERATE_SENTENCE_CASE
     [
         ExecMethod = ExecTransliteration;

sw/uiconfig/swriter/popupmenu/drawtext.xml
@@ -25,5 +25,8 @@
   <menu:menuseparator/>
   <menu:menuitem menu:id=".uno:ThesaurusFromContext"/>
   <menu:menuseparator/>
+  <menu:menuitem menu:id=".uno:EditHyperlink"/>
+  <menu:menuitem menu:id=".uno:RemoveHyperlink"/>
+  <menu:menuseparator/>
   <menu:menuitem menu:id=".uno:ResetAttributes"/>
 </menu:menupopup>

commit b852d97dd3558ef25b0311679e65c7ec0e53f6fd

tdf#131150 sw menu: use nSlotId=0 to avoid adding to set
This fixes a regression in LO 6.4 caused by
commit 5f6e172d13c59c2ec184e10f6d96ea7c63766f2a

Not surprisingly, adding a new condition that multiple
other items match on caused a regression.

bFlag is always set to false at the top of the loop.
In the above commit, bFlag was not set to true,
and so thus it would always be false.
So just set the SlotId to 0
to avoid rSet.Put - like so many other cases already do.

Change-Id: I02e9ca9d8dd75db519e801ad0e86fc8d541ae20a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125688
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/uibase/shells/drwtxtex.cxx
@@ -957,6 +957,7 @@ void SwDrawTextShell::GetState(SfxItemSet& rSet)
             {
                 if (!URLFieldHelper::IsCursorAtURLField(pOLV))
                     rSet.DisableItem(nWhich);
+                nSlotId = 0;
             }
             break;
             default:
@@ -964,7 +965,7 @@ void SwDrawTextShell::GetState(SfxItemSet& rSet)
                 break;
         }
 
-        if (nSlotId && bFlag)
+        if (nSlotId)
             rSet.Put(SfxBoolItem(nWhich, bFlag));
 
         nWhich = aIter.NextWhich();