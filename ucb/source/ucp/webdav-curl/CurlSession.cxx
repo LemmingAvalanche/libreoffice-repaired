LemmingAvalanche/libreoffice-repaired

commit 08f7a73577f918c46d42fa74eb39e21e4b412a11

ucb: webdav-curl: don't use chunked encoding for PUT
What happens with Nextcloud:

 CURLINFO_HEADER_OUT: 0xa028408: PUT .../testB.odt HTTP/1.1
User-Agent: LibreOffice 7.3.0.0 curl/7.79.1 NSS/3.71
Accept: */*
Accept-Encoding: deflate, gzip
Transfer-Encoding: chunked
Expect: 100-continue
 debug log: 0xa028408: STATE: DO => DID handle 0xa028408; line 2077 (connection #0)
 debug log: 0xa028408: STATE: DID => PERFORMING handle 0xa028408; line 2196 (connection #0)
 debug log: 0xa028408: HTTP 1.1 or later with persistent connection
 CURLINFO_HEADER_IN: 0xa028408: HTTP/1.1 100 Continue
 debug log: 0xa028408: CURLINFO_DATA_OUT 8357
 debug log: 0xa028408: Signaling end of chunked upload via terminating chunk.
 debug log: 0xa028408: CURLINFO_DATA_OUT 5
 debug log: 0xa028408: Mark bundle as not supporting multiuse
 debug log: 0xa028408: HTTP 1.1 or later with persistent connection
 CURLINFO_HEADER_IN: 0xa028408: HTTP/1.1 201 Created
 CURLINFO_HEADER_IN: 0xa028408: Date: Fri, 19 Nov 2021 16:27:47 GMT
 CURLINFO_HEADER_IN: 0xa028408: Server: Apache
 CURLINFO_HEADER_IN: 0xa028408: Content-Length: 0

... yes, "201 Created" a 0 byte file.

https://github.com/nextcloud/server/issues/7995

Apparently this doesn't happen with header Content-Length: 8347

Change-Id: I3f38d50530a8b6430b27de2525370e1d95705da6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125579
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

ucb/source/ucp/webdav-curl/CurlSession.cxx
@@ -1800,13 +1800,15 @@ auto CurlSession::PUT(OUString const& rURIReference,
 
     CurlUri const uri(CurlProcessor::URIReferenceToURI(*this, rURIReference));
 
-    // TODO: either set CURLOPT_INFILESIZE_LARGE or chunked?
-    ::std::unique_ptr<curl_slist, deleter_from_fn<curl_slist, curl_slist_free_all>> pList(
-        curl_slist_append(nullptr, "Transfer-Encoding: chunked"));
-    if (!pList)
+    // NextCloud silently fails with chunked encoding
+    uno::Reference<io::XSeekable> const xSeekable(rxInStream, uno::UNO_QUERY);
+    if (!xSeekable.is())
     {
-        throw uno::RuntimeException("curl_slist_append failed");
+        throw uno::RuntimeException("TODO: not seekable");
     }
+    curl_off_t const len(xSeekable->getLength() - xSeekable->getPosition());
+
+    ::std::unique_ptr<curl_slist, deleter_from_fn<curl_slist, curl_slist_free_all>> pList;
     OUString const* const pToken(g_Init.LockStore.getLockTokenForURI(uri.GetURI(), nullptr));
     if (pToken)
     {
@@ -1828,7 +1830,7 @@ auto CurlSession::PUT(OUString const& rURIReference,
 
     // lock m_Mutex after accessing global LockStore to avoid deadlock
 
-    ::std::vector<CurlOption> const options{};
+    ::std::vector<CurlOption> const options{ { CURLOPT_INFILESIZE_LARGE, len, nullptr } };
 
     CurlProcessor::ProcessRequest(*this, uri, options, &rEnv, ::std::move(pList), nullptr,
                                   &rxInStream, nullptr);

commit ad2a46f9250e5abd4f301929a1a426aff3b7f321

ucb: webdav-curl: properly reset CURLOPT_INFILESIZE_LARGE
If the value is set to 0, then the next PROPFIND after PUT will not
append the "Expect: 100-continue" header, and hang without uploading.
See Curl_http_bodysend() which checks for != 0.

Unfortunately with different default values for "long" and "curl_off_t"
CurlOption becomes a little more complicated.

(regression from 08f7a73577f918c46d42fa74eb39e21e4b412a11)

Change-Id: I55ea02659f7a64f373e89a6d02d6865c158fd0c6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125742
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

ucb/source/ucp/webdav-curl/CurlSession.cxx
@@ -124,17 +124,9 @@ struct CurlOption
     Type const Tag;
     union {
         void const* const pValue;
-        long const lValue;
-        curl_off_t const cValue;
+        long /*const*/ lValue;
+        curl_off_t /*const*/ cValue;
     };
-#if 0
-    ::std::variant<void const*, long
-#if SAL_TYPES_SIZEOFLONG == 4
-                   ,
-                   curl_off_t
-#endif
-                   > const Value;
-#endif
     char const* const pExceptionString;
 
     CurlOption(CURLoption const i_Option, void const* const i_Value,
@@ -145,23 +137,27 @@ struct CurlOption
         , pExceptionString(i_pExceptionString)
     {
     }
-    CurlOption(CURLoption const i_Option, long const i_Value, char const* const i_pExceptionString)
-        : Option(i_Option)
-        , Tag(Type::Long)
-        , lValue(i_Value)
-        , pExceptionString(i_pExceptionString)
-    {
-    }
-#if SAL_TYPES_SIZEOFLONG == 4
+    // Depending on platform, curl_off_t may be "long" or a larger type
+    // so cannot use overloading to distinguish these cases.
     CurlOption(CURLoption const i_Option, curl_off_t const i_Value,
-               char const* const i_pExceptionString)
+               char const* const i_pExceptionString, Type const type = Type::Long)
         : Option(i_Option)
-        , Tag(Type::CurlOffT)
-        , cValue(i_Value)
+        , Tag(type)
         , pExceptionString(i_pExceptionString)
     {
+        static_assert(sizeof(long) <= sizeof(curl_off_t));
+        switch (type)
+        {
+            case Type::Long:
+                lValue = i_Value;
+                break;
+            case Type::CurlOffT:
+                cValue = i_Value;
+                break;
+            default:
+                assert(false);
+        }
     }
-#endif
 };
 
 /// combined guard class to ensure things are released in correct order,
@@ -200,22 +196,6 @@ public:
         for (auto const& it : m_Options)
         {
             CURLcode rc(CURL_LAST); // warning C4701
-#if 0
-            if (void const* const* const pp = ::std::get_if<void const*>(&it.Value))
-            {
-                rc = curl_easy_setopt(m_pCurl, it.Option, *pp);
-            }
-            else if (long const * const pLong = ::std::get_if<long>(&it.Value))
-            {
-                rc = curl_easy_setopt(m_pCurl, it.Option, *pLong);
-            }
-#if SAL_TYPES_SIZEOFLONG == 4
-            else if (curl_off_t const* const pOfft = ::std::get_if<curl_off_t>(&it.Value))
-            {
-                rc = curl_easy_setopt(m_pCurl, it.Option, *pOfft);
-            }
-#endif
-#endif
             if (it.Tag == CurlOption::Type::Pointer)
             {
                 rc = curl_easy_setopt(m_pCurl, it.Option, it.pValue);
@@ -224,12 +204,10 @@ public:
             {
                 rc = curl_easy_setopt(m_pCurl, it.Option, it.lValue);
             }
-#if SAL_TYPES_SIZEOFLONG == 4
             else if (it.Tag == CurlOption::Type::CurlOffT)
             {
                 rc = curl_easy_setopt(m_pCurl, it.Option, it.cValue);
             }
-#endif
             else
             {
                 assert(false);
@@ -258,22 +236,6 @@ public:
         for (auto const& it : m_Options)
         {
             CURLcode rc(CURL_LAST); // warning C4701
-#if 0
-            if (void const* const* const pp = ::std::get_if<void const*>(&it.Value))
-            {
-                rc = curl_easy_setopt(m_pCurl, it.Option, nullptr);
-            }
-            else if (long const * const pLong = ::std::get_if<long>(&it.Value))
-            {
-                rc = curl_easy_setopt(m_pCurl, it.Option, 0L);
-            }
-#if SAL_TYPES_SIZEOFLONG == 4
-            else if (curl_off_t const* const pOfft = ::std::get_if<curl_off_t>(&it.Value))
-            {
-                rc = curl_easy_setopt(m_pCurl, it.Option, curl_off_t(0));
-            }
-#endif
-#endif
             if (it.Tag == CurlOption::Type::Pointer)
             {
                 rc = curl_easy_setopt(m_pCurl, it.Option, nullptr);
@@ -282,12 +244,10 @@ public:
             {
                 rc = curl_easy_setopt(m_pCurl, it.Option, 0L);
             }
-#if SAL_TYPES_SIZEOFLONG == 4
             else if (it.Tag == CurlOption::Type::CurlOffT)
             {
-                rc = curl_easy_setopt(m_pCurl, it.Option, curl_off_t(0));
+                rc = curl_easy_setopt(m_pCurl, it.Option, curl_off_t(-1));
             }
-#endif
             else
             {
                 assert(false);
@@ -1830,7 +1790,8 @@ auto CurlSession::PUT(OUString const& rURIReference,
 
     // lock m_Mutex after accessing global LockStore to avoid deadlock
 
-    ::std::vector<CurlOption> const options{ { CURLOPT_INFILESIZE_LARGE, len, nullptr } };
+    ::std::vector<CurlOption> const options{ { CURLOPT_INFILESIZE_LARGE, len, nullptr,
+                                               CurlOption::Type::CurlOffT } };
 
     CurlProcessor::ProcessRequest(*this, uri, options, &rEnv, ::std::move(pList), nullptr,
                                   &rxInStream, nullptr);