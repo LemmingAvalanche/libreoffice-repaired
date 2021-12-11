LemmingAvalanche/libreoffice-repaired

commit e2b47005a55cb900f58d19aa1c167b2e8f6225ee

Directly initialize vector in connectivity (part 2)
Change-Id: I4dc9862a30fc584e600d2952d1110a1ebbcd160d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115611
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

connectivity/source/drivers/evoab2/NDatabaseMetaData.cxx
@@ -988,15 +988,12 @@ Reference< XResultSet > SAL_CALL OEvoabDatabaseMetaData::getTableTypes(  )
     rtl::Reference<::connectivity::ODatabaseMetaDataResultSet> pResult = new ::connectivity::ODatabaseMetaDataResultSet(::connectivity::ODatabaseMetaDataResultSet::eTableTypes);
 
     // here we fill the rows which should be visible when ask for data from the resultset returned here
-    ODatabaseMetaDataResultSet::ORows aRows;
-    for(std::size_t i=0;i < SAL_N_ELEMENTS(sTableTypes);++i)
+    auto nNbTypes = SAL_N_ELEMENTS(sTableTypes);
+    ODatabaseMetaDataResultSet::ORows aRows(nNbTypes);
+    for(std::size_t i=0;i < nNbTypes;++i)
     {
-        ODatabaseMetaDataResultSet::ORow aRow;
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(OUString(sTableTypes[i])));
-
         // bound row
-        aRows.push_back(aRow);
+        aRows.push_back( { ODatabaseMetaDataResultSet::getEmptyValue(), new ORowSetValueDecorator(OUString(sTableTypes[i])) });
     }
     // here we set the rows at the resultset
     pResult->setRows(aRows);
@@ -1013,29 +1010,29 @@ Reference< XResultSet > OEvoabDatabaseMetaData::impl_getTypeInfo_throw(  )
 
     static ODatabaseMetaDataResultSet::ORows aRows = []()
     {
-        ODatabaseMetaDataResultSet::ORows tmp;
-        ODatabaseMetaDataResultSet::ORow aRow;
-        aRow.reserve(19);
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(OUString("VARCHAR")));
-        aRow.push_back(new ORowSetValueDecorator(DataType::VARCHAR));
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(s_nCHAR_OCTET_LENGTH)));
-        aRow.push_back(ODatabaseMetaDataResultSet::getQuoteValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::getQuoteValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        // aRow.push_back(new ORowSetValueDecorator((sal_Int32)ColumnValue::NULLABLE));
-        aRow.push_back(ODatabaseMetaDataResultSet::get1Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get1Value());
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(ColumnSearch::FULL)));
-        aRow.push_back(ODatabaseMetaDataResultSet::get1Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(10)));
+        ODatabaseMetaDataResultSet::ORows tmp(2);
+        ODatabaseMetaDataResultSet::ORow aRow
+        {
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { new ORowSetValueDecorator(OUString("VARCHAR")) },
+            { new ORowSetValueDecorator(DataType::VARCHAR) },
+            { new ORowSetValueDecorator(sal_Int32(s_nCHAR_OCTET_LENGTH)) },
+            { ODatabaseMetaDataResultSet::getQuoteValue() },
+            { ODatabaseMetaDataResultSet::getQuoteValue() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { ODatabaseMetaDataResultSet::get1Value() },
+            { ODatabaseMetaDataResultSet::get1Value() },
+            { new ORowSetValueDecorator(sal_Int32(ColumnSearch::FULL)) },
+            { ODatabaseMetaDataResultSet::get1Value() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { new ORowSetValueDecorator(sal_Int32(10)) }
+        };
 
         tmp.push_back(aRow);
 
@@ -1190,14 +1187,16 @@ Reference< XResultSet > SAL_CALL OEvoabDatabaseMetaData::getTables(
                 OUString aName = OStringToOUString( e_source_peek_name( pSource ),
                                                               RTL_TEXTENCODING_UTF8 );
 
-                ODatabaseMetaDataResultSet::ORow aRow{
-                    ORowSetValueDecoratorRef(),
-                    ORowSetValueDecoratorRef(),
-                    ORowSetValueDecoratorRef(),
-                    new ORowSetValueDecorator(aName),
-                    new ORowSetValueDecorator(ORowSetValue(aTable)),
-                    ODatabaseMetaDataResultSet::getEmptyValue()};
-                aRows.push_back(aRow);
+                aRows.push_back(
+                                 {
+                                     ORowSetValueDecoratorRef(),
+                                     ORowSetValueDecoratorRef(),
+                                     ORowSetValueDecoratorRef(),
+                                     new ORowSetValueDecorator(aName),
+                                     new ORowSetValueDecorator(ORowSetValue(aTable)),
+                                     ODatabaseMetaDataResultSet::getEmptyValue()
+                                 }
+                               );
             }
         }
     }

connectivity/source/drivers/file/FDatabaseMetaData.cxx
@@ -616,10 +616,7 @@ Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTableTypes(  )
     static ODatabaseMetaDataResultSet::ORows aRows;
     if(aRows.empty())
     {
-        ODatabaseMetaDataResultSet::ORow aRow;
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(OUString("TABLE")));
-        aRows.push_back(aRow);
+        aRows.push_back( { ODatabaseMetaDataResultSet::getEmptyValue(), new ORowSetValueDecorator(OUString("TABLE")) } );
     }
     pResult->setRows(aRows);
     return pResult;

connectivity/source/drivers/file/FDriver.cxx
@@ -97,54 +97,56 @@ Sequence< DriverPropertyInfo > SAL_CALL OFileDriver::getPropertyInfo( const OUSt
 {
     if ( acceptsURL(url) )
     {
-        std::vector< DriverPropertyInfo > aDriverInfo;
 
         Sequence< OUString > aBoolean(2);
         aBoolean[0] = "0";
         aBoolean[1] = "1";
 
-        aDriverInfo.push_back(DriverPropertyInfo(
+        std::vector< DriverPropertyInfo > aDriverInfo
+        {
+             { DriverPropertyInfo(
                 "CharSet"
                 ,"CharSet of the database."
                 ,false
                 ,OUString()
                 ,Sequence< OUString >())
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+             },
+             { DriverPropertyInfo(
                 "Extension"
                 ,"Extension of the file format."
                 ,false
                 ,".*"
                 ,Sequence< OUString >())
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+             },
+             { DriverPropertyInfo(
                 "ShowDeleted"
                 ,"Display inactive records."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+             },
+             { DriverPropertyInfo(
                 "EnableSQL92Check"
                 ,"Use SQL92 naming constraints."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+             },
+             { DriverPropertyInfo(
                 "UseRelativePath"
                 ,"Handle the connection url as relative path."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+             },
+             { DriverPropertyInfo(
                 "URL"
                 ,"The URL of the database document which is used to create an absolute path."
                 ,false
                 ,OUString()
                 ,Sequence< OUString >())
-                );
+             }
+        };
         return Sequence< DriverPropertyInfo >(aDriverInfo.data(),aDriverInfo.size());
     } // if ( acceptsURL(url) )
     {

commit 6c84aa4e79914ce7f6728a07931182dbcb90851a

Fix regression in connectivity/evoab2
Regression from e2b47005a55cb900f58d19aa1c167b2e8f6225ee
Directly initialize vector in connectivity (part 2)

Pinpointed thanks to this log:
warn:dbaccess:612118:612118:dbaccess/source/core/dataaccess/connection.cxx:344: DBG_UNHANDLED_EXCEPTION in OConnection exception: com.sun.star.sdbc.SQLException message: Invalid descriptor index. /home/julien/lo/libreoffice/connectivity/source/commontools/dbexception.cxx:365 SQLState: 07009 ErrorCode: 0
    wrapped: void message: /home/julien/lo/libreoffice/tools/source/debug/debug.cxx:104
when launching Base then connecting to Evolution local.

Change-Id: Id4cb0fc322b0df24ed2b2d89a5595f4841db1845
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126664
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins

connectivity/source/drivers/evoab2/NDatabaseMetaData.cxx
@@ -989,7 +989,7 @@ Reference< XResultSet > SAL_CALL OEvoabDatabaseMetaData::getTableTypes(  )
 
     // here we fill the rows which should be visible when ask for data from the resultset returned here
     auto nNbTypes = SAL_N_ELEMENTS(sTableTypes);
-    ODatabaseMetaDataResultSet::ORows aRows(nNbTypes);
+    ODatabaseMetaDataResultSet::ORows aRows;
     for(std::size_t i=0;i < nNbTypes;++i)
     {
         // bound row
@@ -1010,7 +1010,7 @@ Reference< XResultSet > OEvoabDatabaseMetaData::impl_getTypeInfo_throw(  )
 
     static ODatabaseMetaDataResultSet::ORows aRows = []()
     {
-        ODatabaseMetaDataResultSet::ORows tmp(2);
+        ODatabaseMetaDataResultSet::ORows tmp;
         ODatabaseMetaDataResultSet::ORow aRow
         {
              ODatabaseMetaDataResultSet::getEmptyValue() ,