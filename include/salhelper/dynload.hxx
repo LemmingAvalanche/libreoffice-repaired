LemmingAvalanche/libreoffice-repaired

commit 2369e3cd502f7bb905edb0921f715eaef93d6661

loplugin:flatten in filter..include
Change-Id: I74c1ea8b9b490eaa9508a885758224063e39051b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127235
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/salhelper/dynload.hxx
@@ -150,9 +150,6 @@ public:
     /// Destructor, decrease the reference count and unload the library if it is the last instance.
     ~ODynamicLoader()
     {
-        if( m_pLoader )
-            if (m_pLoader->release()==0)
-                m_pStaticLoader = NULL;
     }
 
     /// Assign operator

hwpfilter/source/hwpfile.cxx
@@ -596,23 +596,20 @@ void HWPFile::AddFBoxStyle(FBoxStyle * fbstyle)
 int HWPFile::compareCharShape(CharShape const *shape)
 {
     int count = cslist.size();
-    if( count > 0 )
+    for(int i = 0; i< count; i++)
     {
-        for(int i = 0; i< count; i++)
-        {
-            CharShape *cshape = getCharShape(i);
+        CharShape *cshape = getCharShape(i);
 
-            if( shape->size == cshape->size &&
-                shape->font[0] == cshape->font[0] &&
-                shape->ratio[0] == cshape->ratio[0] &&
-                shape->space[0] == cshape->space[0] &&
-                shape->color[1] == cshape->color[1] &&
-                shape->color[0] == cshape->color[0] &&
-                shape->shade == cshape->shade &&
-                shape->attr == cshape->attr )
-            {
-                return cshape->index;
-            }
+        if( shape->size == cshape->size &&
+            shape->font[0] == cshape->font[0] &&
+            shape->ratio[0] == cshape->ratio[0] &&
+            shape->space[0] == cshape->space[0] &&
+            shape->color[1] == cshape->color[1] &&
+            shape->color[0] == cshape->color[0] &&
+            shape->shade == cshape->shade &&
+            shape->attr == cshape->attr )
+        {
+            return cshape->index;
         }
     }
     return 0;

i18nlangtag/source/isolang/inunx.cxx
@@ -88,60 +88,60 @@ static void getPlatformSystemLanguageImpl( LanguageType& rSystemLanguage,
 {
     /* get the language from the user environment */
     LanguageType nLang = rSystemLanguage;
+    if ( nLang != LANGUAGE_DONTKNOW )
+        return;
+
+    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex());
+    nLang = rSystemLanguage;
     if ( nLang == LANGUAGE_DONTKNOW )
     {
-        ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex());
-        nLang = rSystemLanguage;
-        if ( nLang == LANGUAGE_DONTKNOW )
-        {
 #ifdef MACOSX
-            rtl_Locale    *procLocale;
-            (void) pGetLangFromEnv; /* unused */
+        rtl_Locale    *procLocale;
+        (void) pGetLangFromEnv; /* unused */
 
-            if ( osl_getProcessLocale(&procLocale) == osl_Process_E_None )
-            {
-                nLang = LanguageTag( *procLocale ).makeFallback().getLanguageType();
-                OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
-                rSystemLanguage = nLang;
-#ifdef DEBUG
-                if ( rSystemLanguage == LANGUAGE_DONTKNOW )
-                    fprintf( stderr, "intnunx.cxx:  failed to convert osl_getProcessLocale() language to system language.\n" );
-#endif
-            }
-#else   /* MACOSX */
-            bool bColonList = false;
-            OString aUnxLang( pGetLangFromEnv( bColonList));
-            if (bColonList)
-            {
-                // Only a very simple "take first". If empty try second or keep empty.
-                sal_Int32 n = aUnxLang.indexOf(':');
-                if (n >= 0)
-                {
-                    sal_Int32 s = 0;
-                    if (n == 0 && aUnxLang.getLength() > 1)
-                    {
-                        n = aUnxLang.indexOf(':', 1);
-                        if (n < 0)
-                            n = aUnxLang.getLength();
-                        if (n < 2)
-                            s = n = 0;
-                        else
-                        {
-                            s = 1;
-                            --n;
-                        }
-                    }
-                    aUnxLang = aUnxLang.copy(s,n);
-                }
-            }
-            nLang = MsLangId::convertUnxByteStringToLanguage( aUnxLang );
+        if ( osl_getProcessLocale(&procLocale) == osl_Process_E_None )
+        {
+            nLang = LanguageTag( *procLocale ).makeFallback().getLanguageType();
             OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
             rSystemLanguage = nLang;
+#ifdef DEBUG
+            if ( rSystemLanguage == LANGUAGE_DONTKNOW )
+                fprintf( stderr, "intnunx.cxx:  failed to convert osl_getProcessLocale() language to system language.\n" );
+#endif
+        }
+#else   /* MACOSX */
+        bool bColonList = false;
+        OString aUnxLang( pGetLangFromEnv( bColonList));
+        if (bColonList)
+        {
+            // Only a very simple "take first". If empty try second or keep empty.
+            sal_Int32 n = aUnxLang.indexOf(':');
+            if (n >= 0)
+            {
+                sal_Int32 s = 0;
+                if (n == 0 && aUnxLang.getLength() > 1)
+                {
+                    n = aUnxLang.indexOf(':', 1);
+                    if (n < 0)
+                        n = aUnxLang.getLength();
+                    if (n < 2)
+                        s = n = 0;
+                    else
+                    {
+                        s = 1;
+                        --n;
+                    }
+                }
+                aUnxLang = aUnxLang.copy(s,n);
+            }
+        }
+        nLang = MsLangId::convertUnxByteStringToLanguage( aUnxLang );
+        OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
+        rSystemLanguage = nLang;
 #endif  /* MACOSX */
-        }
-        else {
-            OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
-        }
+    }
+    else {
+        OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
     }
 }
 

idl/source/objects/slot.cxx
@@ -570,33 +570,32 @@ void SvMetaSlot::WriteSlot( std::string_view rShellName, sal_uInt16 nCount,
 
 sal_uInt16 SvMetaSlot::WriteSlotParamArray( SvIdlDataBase & rBase, SvStream & rOutStm ) const
 {
-    if( IsMethod() )
+    if( !IsMethod() )
+        return 0;
+
+    SvMetaType * pType = GetType();
+
+    if( !SvIdlDataBase::FindType( pType, rBase.aUsedTypes ) )
+        rBase.aUsedTypes.push_back( pType );
+
+    const SvRefMemberList<SvMetaAttribute *>& rList =
+                pType->GetAttrList();
+    for( size_t n = 0; n < rList.size(); n++ )
     {
-        SvMetaType * pType = GetType();
-
-        if( !SvIdlDataBase::FindType( pType, rBase.aUsedTypes ) )
-            rBase.aUsedTypes.push_back( pType );
-
-        const SvRefMemberList<SvMetaAttribute *>& rList =
-                    pType->GetAttrList();
-        for( size_t n = 0; n < rList.size(); n++ )
-        {
-            SvMetaAttribute * pPar  = rList[n];
-            SvMetaType * pPType     = pPar->GetType();
-            WriteTab( rOutStm, 1 );
-            rOutStm.WriteCharPtr("{ (const SfxType*) &a")
-                // item type
-               .WriteOString(pPType->GetName()).WriteCharPtr("_Impl, ")
-                // parameter name
-               .WriteCharPtr("\"").WriteOString(pPar->GetName()).WriteCharPtr("\", ")
-                // slot id
-               .WriteOString(pPar->GetSlotId().getString()).WriteCharPtr(" },") << endl;
-            if( !SvIdlDataBase::FindType( pPType, rBase.aUsedTypes ) )
-                rBase.aUsedTypes.push_back( pPType );
-        }
-        return static_cast<sal_uInt16>(rList.size());
+        SvMetaAttribute * pPar  = rList[n];
+        SvMetaType * pPType     = pPar->GetType();
+        WriteTab( rOutStm, 1 );
+        rOutStm.WriteCharPtr("{ (const SfxType*) &a")
+            // item type
+           .WriteOString(pPType->GetName()).WriteCharPtr("_Impl, ")
+            // parameter name
+           .WriteCharPtr("\"").WriteOString(pPar->GetName()).WriteCharPtr("\", ")
+            // slot id
+           .WriteOString(pPar->GetSlotId().getString()).WriteCharPtr(" },") << endl;
+        if( !SvIdlDataBase::FindType( pPType, rBase.aUsedTypes ) )
+            rBase.aUsedTypes.push_back( pPType );
     }
-    return 0;
+    return static_cast<sal_uInt16>(rList.size());
 }
 
 sal_uInt16 SvMetaSlot::WriteSlotMap( std::string_view rShellName, sal_uInt16 nCount,

idlc/source/astdeclaration.cxx
@@ -125,38 +125,37 @@ bool AstDeclaration::hasAncestor(AstDeclaration* pDecl)
 bool AstDeclaration::dump(RegistryKey& rKey)
 {
     AstScope* pScope = declAsScope(this);
+    if ( !pScope )
+        return true;
+
     bool bRet = true;
-
-    if ( pScope )
+    DeclList::const_iterator iter = pScope->getIteratorBegin();
+    DeclList::const_iterator end = pScope->getIteratorEnd();
+    AstDeclaration* pDecl = nullptr;
+    while ( iter != end && bRet)
     {
-        DeclList::const_iterator iter = pScope->getIteratorBegin();
-        DeclList::const_iterator end = pScope->getIteratorEnd();
-        AstDeclaration* pDecl = nullptr;
-        while ( iter != end && bRet)
+        pDecl = *iter;
+        if ( pDecl->isInMainfile() )
         {
-            pDecl = *iter;
-            if ( pDecl->isInMainfile() )
+            switch ( pDecl->getNodeType() )
             {
-                switch ( pDecl->getNodeType() )
-                {
-                    case NT_module:
-                    case NT_constants:
-                    case NT_interface:
-                    case NT_struct:
-                    case NT_exception:
-                    case NT_enum:
-                    case NT_typedef:
-                    case NT_service:
-                    case NT_singleton:
-                        bRet = pDecl->dump(rKey);
-                        break;
-                    default:
-                        break;
-                }
+                case NT_module:
+                case NT_constants:
+                case NT_interface:
+                case NT_struct:
+                case NT_exception:
+                case NT_enum:
+                case NT_typedef:
+                case NT_service:
+                case NT_singleton:
+                    bRet = pDecl->dump(rKey);
+                    break;
+                default:
+                    break;
             }
-
-            ++iter;
         }
+
+        ++iter;
     }
     return bRet;
 }

idlc/source/astenum.cxx
@@ -66,37 +66,37 @@ bool AstEnum::dump(RegistryKey& rKey)
     }
 
     sal_uInt16 nConst = getNodeCount(NT_enum_val);
-    if ( nConst > 0 )
+    if ( nConst <= 0 )
+        return true;
+
+    typereg::Writer aBlob(
+        m_bPublished ? TYPEREG_VERSION_1 : TYPEREG_VERSION_0,
+        getDocumentation(), "", RT_TYPE_ENUM, m_bPublished,
+        OStringToOUString(getRelativName(), RTL_TEXTENCODING_UTF8), 0,
+        nConst, 0, 0);
+
+    DeclList::const_iterator iter = getIteratorBegin();
+    DeclList::const_iterator end = getIteratorEnd();
+    sal_uInt16 index = 0;
+    while ( iter != end )
     {
-        typereg::Writer aBlob(
-            m_bPublished ? TYPEREG_VERSION_1 : TYPEREG_VERSION_0,
-            getDocumentation(), "", RT_TYPE_ENUM, m_bPublished,
-            OStringToOUString(getRelativName(), RTL_TEXTENCODING_UTF8), 0,
-            nConst, 0, 0);
+        AstDeclaration* pDecl = *iter;
+        if ( pDecl->getNodeType() == NT_enum_val )
+            static_cast<AstConstant*>(pDecl)->dumpBlob(aBlob, index++, false);
 
-        DeclList::const_iterator iter = getIteratorBegin();
-        DeclList::const_iterator end = getIteratorEnd();
-        sal_uInt16 index = 0;
-        while ( iter != end )
-        {
-            AstDeclaration* pDecl = *iter;
-            if ( pDecl->getNodeType() == NT_enum_val )
-                static_cast<AstConstant*>(pDecl)->dumpBlob(aBlob, index++, false);
+        ++iter;
+    }
 
-            ++iter;
-        }
+    sal_uInt32 aBlobSize;
+    void const * pBlob = aBlob.getBlob(&aBlobSize);
 
-        sal_uInt32 aBlobSize;
-        void const * pBlob = aBlob.getBlob(&aBlobSize);
-
-        if (localKey.setValue("", RegValueType::BINARY,
-                                const_cast<RegValue>(pBlob), aBlobSize) != RegError::NO_ERROR)
-        {
-            fprintf(stderr, "%s: warning, could not set value of key \"%s\" in %s\n",
-                    idlc()->getOptions()->getProgramName().getStr(),
-                    getFullName().getStr(), OUStringToOString(localKey.getRegistryName(), RTL_TEXTENCODING_UTF8).getStr());
-            return false;
-        }
+    if (localKey.setValue("", RegValueType::BINARY,
+                            const_cast<RegValue>(pBlob), aBlobSize) != RegError::NO_ERROR)
+    {
+        fprintf(stderr, "%s: warning, could not set value of key \"%s\" in %s\n",
+                idlc()->getOptions()->getProgramName().getStr(),
+                getFullName().getStr(), OUStringToOString(localKey.getRegistryName(), RTL_TEXTENCODING_UTF8).getStr());
+        return false;
     }
 
     return true;

idlc/source/astscope.cxx
@@ -151,37 +151,36 @@ AstDeclaration* AstScope::lookupByName(const OString& scopedName)
         }
     }
 
-    if ( bFindFirstScope && (firstScope != scopedName) )
+    if ( !bFindFirstScope || (firstScope == scopedName) )
+        return pDecl;
+
+    sal_Int32 i = 0;
+    sal_Int32 nOffset = 2;
+    do
     {
-        sal_Int32 i = 0;
-        sal_Int32 nOffset = 2;
-        do
+        pScope = declAsScope(pDecl);
+        if( pScope )
         {
-            pScope = declAsScope(pDecl);
-            if( pScope )
-            {
-                pDecl = pScope->lookupByNameLocal(scopedName.getToken(nOffset, ':', i ));
-                nOffset = 1;
-            }
-            if( !pDecl )
-                break;
-        } while( i != -1 );
-
-        if ( !pDecl )
-        {
-            // last try if is not the global scope and the scopeName isn't specify global too
-            pDecl = scopeAsDecl(this);
-            if ( pDecl && !pDecl->getLocalName().isEmpty() )
-            {
-                pScope = pDecl->getScope();
-                if ( pScope )
-                    pDecl = pScope->lookupByName(scopedName);
-            } else
-            {
-                pDecl = nullptr;
-            }
+            pDecl = pScope->lookupByNameLocal(scopedName.getToken(nOffset, ':', i ));
+            nOffset = 1;
         }
+        if( !pDecl )
+            break;
+    } while( i != -1 );
 
+    if ( !pDecl )
+    {
+        // last try if is not the global scope and the scopeName isn't specify global too
+        pDecl = scopeAsDecl(this);
+        if ( pDecl && !pDecl->getLocalName().isEmpty() )
+        {
+            pScope = pDecl->getScope();
+            if ( pScope )
+                pDecl = pScope->lookupByName(scopedName);
+        } else
+        {
+            pDecl = nullptr;
+        }
     }
 
     return pDecl;

idlc/source/options.cxx
@@ -56,53 +56,53 @@ bool Options::checkArgument (std::vector< std::string > & rArgs, char const * ar
 {
   bool result = ((arg != nullptr) && (len > 0));
   OSL_PRECOND(result, "idlc::Options::checkArgument(): invalid arguments");
-  if (result)
+  if (!result)
+      return false;
+
+  switch(arg[0])
   {
-    switch(arg[0])
+  case '@':
+    result = len > 1;
+    if (result)
     {
-    case '@':
-      result = len > 1;
-      if (result)
+      // "@<cmdfile>"
+      result = Options::checkCommandFile (rArgs, &(arg[1]));
+    }
+    break;
+  case '-':
+    result = len > 1;
+    if (result)
+    {
+      // "-<option>"
+      switch (arg[1])
       {
-        // "@<cmdfile>"
-        result = Options::checkCommandFile (rArgs, &(arg[1]));
-      }
-      break;
-    case '-':
-      result = len > 1;
-      if (result)
-      {
-        // "-<option>"
-        switch (arg[1])
+      case 'O':
+      case 'M':
+      case 'I':
+      case 'D':
         {
-        case 'O':
-        case 'M':
-        case 'I':
-        case 'D':
+          // "-<option>[<param>]
+          std::string option(&(arg[0]), 2);
+          rArgs.push_back(option);
+          if (len > 2)
           {
-            // "-<option>[<param>]
-            std::string option(&(arg[0]), 2);
-            rArgs.push_back(option);
-            if (len > 2)
-            {
-              // "-<option><param>"
-              std::string param(&(arg[2]), len - 2);
-              rArgs.push_back(param);
-            }
-            break;
+            // "-<option><param>"
+            std::string param(&(arg[2]), len - 2);
+            rArgs.push_back(param);
           }
-        default:
-          // "-<option>" ([long] option, w/o param)
-          rArgs.emplace_back(arg, len);
           break;
         }
+      default:
+        // "-<option>" ([long] option, w/o param)
+        rArgs.emplace_back(arg, len);
+        break;
       }
-      break;
-    default:
-      // "<param>"
-      rArgs.emplace_back(arg, len);
-      break;
     }
+    break;
+  default:
+    // "<param>"
+    rArgs.emplace_back(arg, len);
+    break;
   }
   return result;
 }

commit a2c7dd9ca8e808a1ac9c418c95e6e5d8ecbccee6

revert part of "loplugin:flatten in filter..include"
revert part of commit 2369e3cd502f7bb905edb0921f715eaef93d6661
accidentally included (was an early workaround when the plugin
was misbehaving)

Change-Id: I12d06b7c4d6387b674b4d0c69914732094d20f5e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127302
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/salhelper/dynload.hxx
@@ -150,6 +150,9 @@ public:
     /// Destructor, decrease the reference count and unload the library if it is the last instance.
     ~ODynamicLoader()
     {
+        if( m_pLoader )
+            if (m_pLoader->release()==0)
+                m_pStaticLoader = NULL;
     }
 
     /// Assign operator