LemmingAvalanche/libreoffice-repaired

commit 4b9621b588cf9015013e3956da08971cedb29d94

Use libo_CHECK_SYSTEM_MODULE for eot
Change-Id: I019a2047d05019b161c4f08c1d17a084a2224ebc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127639
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

RepositoryExternal.mk
@@ -3250,9 +3250,9 @@ endif # MSC
 
 endif # SYSTEM_LIBORCUS
 
-ifeq ($(ENABLE_EOT),TRUE)
 
-ifneq ($(SYSTEM_LIBEOT),)
+ifeq ($(ENABLE_EOT),TRUE)
+ifeq ($(SYSTEM_LIBEOT),TRUE)
 
 define gb_LinkTarget__use_libeot
 $(call gb_LinkTarget_set_include,$(1),\
@@ -3284,16 +3284,14 @@ $(call gb_ExternalProject_use_external_project,$(1),libeot)
 
 endef
 
-endif # SYSTEM_LIBEOT
-
+endif # !SYSTEM_LIBEOT
 else # !ENABLE_EOT
 
 gb_LinkTarget__use_libeot :=
 gb_ExternalProject__use_libeot :=
 
-endif # ENABLE_EOT
+endif # !ENABLE_EOT
 
-### X11 stuff ###
 
 ifeq ($(USING_X11), TRUE)
 

configure.ac
@@ -911,6 +911,7 @@ cygwin*|wsl*)
     using_freetype_fontconfig=no
     using_x11=no
     test_openldap=no
+    test_eot=no
     build_skia=yes
     _os=WINNT
 
@@ -929,6 +930,7 @@ darwin*|macos*) # macOS
     using_freetype_fontconfig=no
     using_x11=no
     build_skia=yes
+    test_eot=no
     if test -n "$LODE_HOME" ; then
         mac_sanitize_path
         AC_MSG_NOTICE([sanitized the PATH to $PATH])
@@ -1708,11 +1710,6 @@ libo_FUZZ_ARG_ENABLE(gstreamer-1-0,
     AS_HELP_STRING([--disable-gstreamer-1-0],
         [Disable building with the gstreamer 1.0 avmedia backend.]),
 ,test "${enable_gstreamer_1_0+set}" = set || enable_gstreamer_1_0=yes)
-
-libo_FUZZ_ARG_ENABLE([eot],
-    [AS_HELP_STRING([--enable-eot],
-        [Enable support for Embedded OpenType fonts.])],
-,test "${enable_eot+set}" = set || enable_eot=no)
 
 libo_FUZZ_ARG_ENABLE(cve-tests,
     AS_HELP_STRING([--disable-cve-tests],
@@ -9360,18 +9357,7 @@ dnl Check for system xmlsec
 dnl ===================================================================
 libo_CHECK_SYSTEM_MODULE([xmlsec], [XMLSEC], [xmlsec1-nss >= 1.2.28])
 
-AC_MSG_CHECKING([whether to enable Embedded OpenType support])
-if test "$_os" != "WINNT" -a "$_os" != "Darwin" -a "$enable_eot" = "yes"; then
-    ENABLE_EOT="TRUE"
-    AC_DEFINE([ENABLE_EOT])
-    AC_MSG_RESULT([yes])
-
-    libo_CHECK_SYSTEM_MODULE([libeot],[LIBEOT],[libeot >= 0.01])
-else
-    ENABLE_EOT=
-    AC_MSG_RESULT([no])
-fi
-AC_SUBST([ENABLE_EOT])
+libo_CHECK_SYSTEM_MODULE([eot],[EOT],[libeot >= 0.01],disabled)
 
 dnl ===================================================================
 dnl Check for DLP libs

commit a9634306b003ded01c4da4846ae5fc874f417b4d

Fix uppercase EOT => LIBEOT usage
Originally I had these hunks included, but somehow came to think
that using EOT instead of LIBEOT would work... well I'm obviously
wrong, so re-add the uppercase LIBEOT usage.

Still uses the lowercase "eot", because of the autoconf switches.

Regression from commit 4b9621b588cf9015013e3956da08971cedb29d94
("Use libo_CHECK_SYSTEM_MODULE for eot").

Change-Id: Id3e1c97cdb67621f0c0cfd67b78836f62df0e8e9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127737
Tested-by: Julien Nabet <serval2412@yahoo.fr>
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

RepositoryExternal.mk
@@ -3251,7 +3251,7 @@ endif # MSC
 endif # SYSTEM_LIBORCUS
 
 
-ifeq ($(ENABLE_EOT),TRUE)
+ifeq ($(ENABLE_LIBEOT),TRUE)
 ifeq ($(SYSTEM_LIBEOT),TRUE)
 
 define gb_LinkTarget__use_libeot
@@ -3285,12 +3285,12 @@ $(call gb_ExternalProject_use_external_project,$(1),libeot)
 endef
 
 endif # !SYSTEM_LIBEOT
-else # !ENABLE_EOT
+else # !ENABLE_LIBEOT
 
 gb_LinkTarget__use_libeot :=
 gb_ExternalProject__use_libeot :=
 
-endif # !ENABLE_EOT
+endif # !ENABLE_LIBEOT
 
 
 ifeq ($(USING_X11), TRUE)

config_host.mk.in
@@ -154,7 +154,6 @@ export ENABLE_DBGUTIL=@ENABLE_DBGUTIL@
 export ENABLE_DBUS=@ENABLE_DBUS@
 export ENABLE_DCONF=@ENABLE_DCONF@
 export ENABLE_DEBUG=@ENABLE_DEBUG@
-export ENABLE_EOT=@ENABLE_EOT@
 export ENABLE_EVOAB2=@ENABLE_EVOAB2@
 export ENABLE_FIREBIRD_SDBC=@ENABLE_FIREBIRD_SDBC@
 export ENABLE_FORMULA_LOGGER=@ENABLE_FORMULA_LOGGER@
@@ -169,6 +168,7 @@ export ENABLE_HEADLESS=@ENABLE_HEADLESS@
 export ENABLE_HTMLHELP=@ENABLE_HTMLHELP@
 export ENABLE_JAVA=@ENABLE_JAVA@
 export ENABLE_LDAP=@ENABLE_LDAP@
+export ENABLE_LIBEOT=@ENABLE_LIBEOT@
 export ENABLE_LPSOLVE=@ENABLE_LPSOLVE@
 export ENABLE_LTO=@ENABLE_LTO@
 export ENABLE_LWP=@ENABLE_LWP@

config_host/config_eot.h.in
@@ -1,6 +1,6 @@
-#ifndef CONFIG_EOT_H
-#define CONFIG_EOT_H
+#ifndef CONFIG_LIBEOT_H
+#define CONFIG_LIBEOT_H
 
-#define ENABLE_EOT 0
+#define ENABLE_LIBEOT 0
 
 #endif

configure.ac
@@ -9345,7 +9345,7 @@ dnl Check for system xmlsec
 dnl ===================================================================
 libo_CHECK_SYSTEM_MODULE([xmlsec], [XMLSEC], [xmlsec1-nss >= 1.2.28])
 
-libo_CHECK_SYSTEM_MODULE([eot],[EOT],[libeot >= 0.01],disabled)
+libo_CHECK_SYSTEM_MODULE([eot],[LIBEOT],[libeot >= 0.01],disabled)
 
 dnl ===================================================================
 dnl Check for DLP libs

vcl/source/gdi/embeddedfontshelper.cxx
@@ -26,7 +26,7 @@
 #include <sft.hxx>
 
 
-#if ENABLE_EOT
+#if ENABLE_LIBEOT
 extern "C"
 {
 namespace libeot
@@ -108,7 +108,7 @@ bool EmbeddedFontsHelper::addEmbeddedFont( const uno::Reference< io::XInputStrea
             break;
     }
     bool sufficientFontRights(false);
-#if ENABLE_EOT
+#if ENABLE_LIBEOT
     if( eot )
     {
         unsigned uncompressedFontSize = 0;

vcl/source/gdi/pdfwriter_impl.cxx
@@ -85,7 +85,7 @@
 
 #include <config_eot.h>
 
-#if ENABLE_EOT
+#if ENABLE_LIBEOT
 #include <libeot/libeot.h>
 #endif
 