LemmingAvalanche/libreoffice-repaired

commit aeac376fec64f33e1db454f522053c27e52d524a

VCL move platform code from mouse.cxx into plugins
... by moving it into ImplCreate(DragSource|DropTarget).

The existing Create* variant now checks for headless mode and the
IsRunningUnitTest flag, before creating the platform variants.
There are two small helpers to initialize either X11 or Ole based
UNO DnD interace implementations.

Unfortunatly Windows requires to move two dtrans header files, but
at least any other changes are minimal.

Change-Id: Id79459ad71a26243b1c9cb1fe38ab236b0ab8fa6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128049
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/app/dndhelp.cxx
@@ -19,7 +19,13 @@
 
 #include <vcl/dndhelp.hxx>
 
+#include <vcl/svapp.hxx>
+#include <dndhelper.hxx>
+
 #include <cppuhelper/queryinterface.hxx>
+
+#include <com/sun/star/awt/XDisplayConnection.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
 
 using namespace ::com::sun::star;
 
@@ -134,6 +140,28 @@ void vcl::unohelper::DragAndDropWrapper::dragOver( const css::datatransfer::dnd:
 
 void vcl::unohelper::DragAndDropWrapper::dropActionChanged( const css::datatransfer::dnd::DropTargetDragEvent& )
 {
+}
+
+css::uno::Reference<css::uno::XInterface>
+vcl::OleDnDHelper(const css::uno::Reference<css::lang::XInitialization>& pDnD, const sal_IntPtr pWin, DragOrDrop eDoD)
+{
+    if (pWin && pDnD)
+    {
+        if (eDoD == vcl::DragOrDrop::Drag)
+            pDnD->initialize({ uno::Any(), uno::Any(static_cast<sal_uInt64>(pWin)) });
+        else
+            pDnD->initialize({ uno::Any(static_cast<sal_uInt64>(pWin)), uno::Any() });
+    }
+    return css::uno::Reference<css::uno::XInterface>(pDnD);
+}
+
+css::uno::Reference<css::uno::XInterface>
+vcl::X11DnDHelper(const css::uno::Reference<css::lang::XInitialization>& pDnD, const sal_IntPtr pWin)
+{
+    if (pWin && pDnD)
+        pDnD->initialize({ uno::Any(Application::GetDisplayConnection()),
+                           uno::Any(static_cast<sal_uInt64>(pWin)) });
+    return css::uno::Reference<css::uno::XInterface>(pDnD);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/window/mouse.cxx
@@ -39,6 +39,7 @@
 #include <salobj.hxx>
 #include <salgdi.hxx>
 #include <salframe.hxx>
+#include <salinst.hxx>
 
 #include <dndlistenercontainer.hxx>
 #include <dndeventdispatcher.hxx>
@@ -724,70 +725,20 @@ Reference< css::datatransfer::dnd::XDropTarget > Window::GetDropTarget()
 
 Reference< css::datatransfer::dnd::XDragSource > Window::GetDragSource()
 {
-
 #if HAVE_FEATURE_DESKTOP
-
-    if( mpWindowImpl->mpFrameData )
-    {
-        if( ! mpWindowImpl->mpFrameData->mxDragSource.is() )
-        {
-            try
-            {
-                Reference< XComponentContext > xContext( comphelper::getProcessComponentContext() );
-                const SystemEnvData * pEnvData = GetSystemData();
-
-                if( pEnvData )
-                {
-                    Sequence< Any > aDragSourceAL( 2 ), aDropTargetAL( 2 );
-                    auto pDragSourceAL = aDragSourceAL.getArray();
-                    auto pDropTargetAL = aDropTargetAL.getArray();
-                    OUString aDragSourceSN, aDropTargetSN;
-#if defined(_WIN32)
-                    aDragSourceSN = "com.sun.star.datatransfer.dnd.OleDragSource";
-                    aDropTargetSN = "com.sun.star.datatransfer.dnd.OleDropTarget";
-                    pDragSourceAL[ 1 ] <<= static_cast<sal_uInt64>( reinterpret_cast<sal_IntPtr>(pEnvData->hWnd) );
-                    pDropTargetAL[ 0 ] <<= static_cast<sal_uInt64>( reinterpret_cast<sal_IntPtr>(pEnvData->hWnd) );
-#elif defined MACOSX
-            /* FIXME: macOS specific dnd interface does not exist! *
-             * Using Windows based dnd as a temporary solution        */
-                    aDragSourceSN = "com.sun.star.datatransfer.dnd.OleDragSource";
-                    aDropTargetSN = "com.sun.star.datatransfer.dnd.OleDropTarget";
-                    pDragSourceAL[ 1 ] <<= static_cast<sal_uInt64>( reinterpret_cast<sal_IntPtr>(pEnvData->mpNSView) );
-                    pDropTargetAL[ 0 ] <<= static_cast<sal_uInt64>( reinterpret_cast<sal_IntPtr>(pEnvData->mpNSView) );
-#elif USING_X11
-                    aDragSourceSN = "com.sun.star.datatransfer.dnd.X11DragSource";
-                    aDropTargetSN = "com.sun.star.datatransfer.dnd.X11DropTarget";
-
-                    pDragSourceAL[ 0 ] <<= Application::GetDisplayConnection();
-                    pDragSourceAL[ 1 ] <<= pEnvData->aShellWindow;
-                    pDropTargetAL[ 0 ] <<= Application::GetDisplayConnection();
-                    pDropTargetAL[ 1 ] <<= pEnvData->aShellWindow;
-#endif
-                    if( !aDragSourceSN.isEmpty() )
-                        mpWindowImpl->mpFrameData->mxDragSource.set(
-                            xContext->getServiceManager()->createInstanceWithArgumentsAndContext( aDragSourceSN, aDragSourceAL, xContext ),
-                            UNO_QUERY );
-
-                    if( !aDropTargetSN.isEmpty() )
-                        mpWindowImpl->mpFrameData->mxDropTarget.set(
-                           xContext->getServiceManager()->createInstanceWithArgumentsAndContext( aDropTargetSN, aDropTargetAL, xContext ),
-                           UNO_QUERY );
-                }
-            }
-
-            // createInstance can throw any exception
-            catch (const Exception&)
-            {
-                // release all instances
-                mpWindowImpl->mpFrameData->mxDropTarget.clear();
-                mpWindowImpl->mpFrameData->mxDragSource.clear();
-            }
-        }
-
+    const SystemEnvData* pEnvData = GetSystemData();
+    if (!mpWindowImpl->mpFrameData || !pEnvData)
+        return Reference<css::datatransfer::dnd::XDragSource>();
+    if (mpWindowImpl->mpFrameData->mxDragSource.is())
         return mpWindowImpl->mpFrameData->mxDragSource;
-    }
-#endif
+
+    SalInstance* pInst = ImplGetSVData()->mpDefInst;
+    mpWindowImpl->mpFrameData->mxDragSource.set(pInst->CreateDragSource(pEnvData), UNO_QUERY);
+    mpWindowImpl->mpFrameData->mxDropTarget.set(pInst->CreateDropTarget(pEnvData), UNO_QUERY);
+    return mpWindowImpl->mpFrameData->mxDragSource;
+#else
     return Reference< css::datatransfer::dnd::XDragSource > ();
+#endif
 }
 
 Reference< css::datatransfer::dnd::XDragGestureRecognizer > Window::GetDragGestureRecognizer()

solenv/clang-format/excludelist
@@ -14612,6 +14612,7 @@ vcl/inc/unx/x11_cursors/vshear_curs.h
 vcl/inc/unx/x11_cursors/vshear_mask.h
 vcl/inc/vclstatuslistener.hxx
 vcl/inc/wall2.hxx
+vcl/inc/win/dnd_source.hxx
 vcl/inc/win/DWriteTextRenderer.hxx
 vcl/inc/win/salbmp.h
 vcl/inc/win/saldata.hxx
@@ -15194,7 +15195,6 @@ vcl/win/dtrans/MtaOleClipb.cxx
 vcl/win/dtrans/MtaOleClipb.hxx
 vcl/win/dtrans/sourcecontext.cxx
 vcl/win/dtrans/source.cxx
-vcl/win/dtrans/source.hxx
 vcl/win/dtrans/target.cxx
 vcl/win/dtrans/TxtCnvtHlp.cxx
 vcl/win/dtrans/TxtCnvtHlp.hxx

vcl/inc/dndhelper.hxx
@@ -1,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <sal/types.h>
+#include <vcl/dllapi.h>
+#include <com/sun/star/uno/Reference.hxx>
+
+namespace com::sun::star::lang
+{
+class XInitialization;
+}
+namespace com::sun::star::uno
+{
+class XInterface;
+}
+
+namespace vcl
+{
+// Ole and X11 refer to the UNO DnD interface names and their expected XInitialization arguments.
+
+enum class DragOrDrop
+{
+    Drag,
+    Drop
+};
+VCL_DLLPUBLIC css::uno::Reference<css::uno::XInterface>
+OleDnDHelper(const css::uno::Reference<css::lang::XInitialization>&, sal_IntPtr pWin, DragOrDrop);
+
+VCL_DLLPUBLIC css::uno::Reference<css::uno::XInterface>
+X11DnDHelper(const css::uno::Reference<css::lang::XInitialization>&, sal_IntPtr pWin);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/inc/osx/salinst.h
@@ -128,8 +128,8 @@ public:
     // dtrans implementation
     virtual css::uno::Reference< css::uno::XInterface > CreateClipboard(
             const css::uno::Sequence< css::uno::Any >& i_rArguments ) override;
-    virtual css::uno::Reference< css::uno::XInterface > CreateDragSource() override;
-    virtual css::uno::Reference< css::uno::XInterface > CreateDropTarget() override;
+    virtual css::uno::Reference<css::uno::XInterface> ImplCreateDragSource(const SystemEnvData*) override;
+    virtual css::uno::Reference<css::uno::XInterface> ImplCreateDropTarget(const SystemEnvData*) override;
 
     static void handleAppDefinedEvent( NSEvent* pEvent );
 

vcl/inc/qt5/QtInstance.hxx
@@ -158,8 +158,10 @@ public:
 
     virtual css::uno::Reference<css::uno::XInterface>
     CreateClipboard(const css::uno::Sequence<css::uno::Any>& i_rArguments) override;
-    virtual css::uno::Reference<css::uno::XInterface> CreateDragSource() override;
-    virtual css::uno::Reference<css::uno::XInterface> CreateDropTarget() override;
+    virtual css::uno::Reference<css::uno::XInterface>
+    ImplCreateDragSource(const SystemEnvData*) override;
+    virtual css::uno::Reference<css::uno::XInterface>
+    ImplCreateDropTarget(const SystemEnvData*) override;
 
     void UpdateStyle(bool bFontsChanged);
 

vcl/inc/salinst.hxx
@@ -63,6 +63,7 @@ class SalSystem;
 class SalBitmap;
 struct SalItemParams;
 class SalSession;
+struct SystemEnvData;
 struct SystemGraphicsData;
 struct SystemWindowData;
 class Menu;
@@ -179,8 +180,10 @@ public:
 
     // dtrans implementation
     virtual css::uno::Reference< css::uno::XInterface > CreateClipboard( const css::uno::Sequence< css::uno::Any >& i_rArguments );
-    virtual css::uno::Reference< css::uno::XInterface > CreateDragSource();
-    virtual css::uno::Reference< css::uno::XInterface > CreateDropTarget();
+    virtual css::uno::Reference<css::uno::XInterface> ImplCreateDragSource(const SystemEnvData*);
+    virtual css::uno::Reference<css::uno::XInterface> ImplCreateDropTarget(const SystemEnvData*);
+    css::uno::Reference<css::uno::XInterface> CreateDragSource(const SystemEnvData* = nullptr);
+    css::uno::Reference<css::uno::XInterface> CreateDropTarget(const SystemEnvData* = nullptr);
     virtual void            AddToRecentDocumentList(const OUString& rFileUrl, const OUString& rMimeType, const OUString& rDocumentService) = 0;
 
     virtual bool            hasNativeFileSelection() const { return false; }
@@ -208,6 +211,9 @@ public:
     virtual void* CreateGStreamerSink(const SystemChildWindow*) { return nullptr; }
 
     virtual void BeforeAbort(const OUString& /* rErrorText */, bool /* bDumpCore */) {}
+
+    // Note: we cannot make this a global variable, because it might be initialised BEFORE the putenv() call in cppunittester.
+    static bool IsRunningUnitTest() { return getenv("LO_TESTNAME") != nullptr; }
 };
 
 // called from SVMain

vcl/inc/unx/gtk/gtkinst.hxx
@@ -286,8 +286,8 @@ public:
         createFolderPicker( const css::uno::Reference< css::uno::XComponentContext >& ) override;
 
     virtual css::uno::Reference< css::uno::XInterface > CreateClipboard( const css::uno::Sequence< css::uno::Any >& i_rArguments ) override;
-    virtual css::uno::Reference< css::uno::XInterface > CreateDragSource() override;
-    virtual css::uno::Reference< css::uno::XInterface > CreateDropTarget() override;
+    virtual css::uno::Reference<css::uno::XInterface> ImplCreateDragSource(const SystemEnvData*) override;
+    virtual css::uno::Reference<css::uno::XInterface> ImplCreateDropTarget(const SystemEnvData*) override;
     virtual OpenGLContext* CreateOpenGLContext() override;
     virtual std::unique_ptr<weld::Builder> CreateBuilder(weld::Widget* pParent, const OUString& rUIRoot, const OUString& rUIFile) override;
     virtual std::unique_ptr<weld::Builder> CreateInterimBuilder(vcl::Window* pParent, const OUString& rUIRoot, const OUString& rUIFile,

commit b5c2389e8341aa4a5bcad0e16d110045309b4381

Catch exception if failing to set the drag source
... or the drop target.

This is the "don't trust the comment or lose a day debugging WASM
AKA don't switch of your brain" commit.

The comment was "createInstance can throw any exception", so - no
more createInstance, no more exception! Nope, you're WRONG! While
the uno::Reference<...>::set(..., UNO_QUERY) might itself not
throw an exception, something else from the CreateDragSource or
CreateDropTarget call could.

The symptom was a caught exception when loading the initial Writer
document in SynchronousDispatch::dispatch.

While at it, also apply the clean-ups suggested by Mike Kaganski
in a comment to the original patch after the merge.

Regression from commit aeac376fec64f33e1db454f522053c27e52d524a
("VCL move platform code from mouse.cxx into plugins").

Change-Id: If80a90a9a6c070d912584f85b9fd10382b98e29c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128175
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/source/app/dndhelp.cxx
@@ -143,25 +143,25 @@ void vcl::unohelper::DragAndDropWrapper::dropActionChanged( const css::datatrans
 }
 
 css::uno::Reference<css::uno::XInterface>
-vcl::OleDnDHelper(const css::uno::Reference<css::lang::XInitialization>& pDnD, const sal_IntPtr pWin, DragOrDrop eDoD)
+vcl::OleDnDHelper(const css::uno::Reference<css::lang::XInitialization>& xDnD, const sal_IntPtr pWin, DragOrDrop eDoD)
 {
-    if (pWin && pDnD)
+    if (pWin && xDnD)
     {
         if (eDoD == vcl::DragOrDrop::Drag)
-            pDnD->initialize({ uno::Any(), uno::Any(static_cast<sal_uInt64>(pWin)) });
+            xDnD->initialize({ uno::Any(), uno::Any(static_cast<sal_uInt64>(pWin)) });
         else
-            pDnD->initialize({ uno::Any(static_cast<sal_uInt64>(pWin)), uno::Any() });
+            xDnD->initialize({ uno::Any(static_cast<sal_uInt64>(pWin)), uno::Any() });
     }
-    return css::uno::Reference<css::uno::XInterface>(pDnD);
+    return xDnD;
 }
 
 css::uno::Reference<css::uno::XInterface>
-vcl::X11DnDHelper(const css::uno::Reference<css::lang::XInitialization>& pDnD, const sal_IntPtr pWin)
+vcl::X11DnDHelper(const css::uno::Reference<css::lang::XInitialization>& xDnD, const sal_IntPtr pWin)
 {
-    if (pWin && pDnD)
-        pDnD->initialize({ uno::Any(Application::GetDisplayConnection()),
+    if (pWin && xDnD)
+        xDnD->initialize({ uno::Any(Application::GetDisplayConnection()),
                            uno::Any(static_cast<sal_uInt64>(pWin)) });
-    return css::uno::Reference<css::uno::XInterface>(pDnD);
+    return xDnD;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/window/mouse.cxx
@@ -732,9 +732,17 @@ Reference< css::datatransfer::dnd::XDragSource > Window::GetDragSource()
     if (mpWindowImpl->mpFrameData->mxDragSource.is())
         return mpWindowImpl->mpFrameData->mxDragSource;
 
-    SalInstance* pInst = ImplGetSVData()->mpDefInst;
-    mpWindowImpl->mpFrameData->mxDragSource.set(pInst->CreateDragSource(pEnvData), UNO_QUERY);
-    mpWindowImpl->mpFrameData->mxDropTarget.set(pInst->CreateDropTarget(pEnvData), UNO_QUERY);
+    try
+    {
+        SalInstance* pInst = ImplGetSVData()->mpDefInst;
+        mpWindowImpl->mpFrameData->mxDragSource.set(pInst->CreateDragSource(pEnvData), UNO_QUERY);
+        mpWindowImpl->mpFrameData->mxDropTarget.set(pInst->CreateDropTarget(pEnvData), UNO_QUERY);
+    }
+    catch (const Exception&)
+    {
+        mpWindowImpl->mpFrameData->mxDropTarget.clear();
+        mpWindowImpl->mpFrameData->mxDragSource.clear();
+    }
     return mpWindowImpl->mpFrameData->mxDragSource;
 #else
     return Reference< css::datatransfer::dnd::XDragSource > ();