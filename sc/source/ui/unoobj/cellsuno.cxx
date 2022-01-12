LemmingAvalanche/libreoffice-repaired

commit 13824944ff5d04163880317ed8ab1946f8734a00

tdf#142033 - Handle embedded newline set via SetDataArray
Change-Id: I798f9a2a2ce599ba8ca3ef1f5ae91801d8f1b138
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124681
Tested-by: Jenkins
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/ui/unoobj/cellsuno.cxx
@@ -1082,6 +1082,7 @@ static bool lcl_PutDataArray( ScDocShell& rDocShell, const ScRange& rRange,
                         const uno::Sequence< uno::Sequence<uno::Any> >& aData )
 {
     ScDocument& rDoc = rDocShell.GetDocument();
+    ScDocFunc& rDocFunc = rDocShell.GetDocFunc();
     SCTAB nTab = rRange.aStart.Tab();
     SCCOL nStartCol = rRange.aStart.Col();
     SCROW nStartRow = rRange.aStart.Row();
@@ -1158,9 +1159,7 @@ static bool lcl_PutDataArray( ScDocShell& rDocShell, const ScRange& rRange,
                         rElement >>= aUStr;
                         if ( !aUStr.isEmpty() )
                         {
-                            ScSetStringParam aParam;
-                            aParam.setTextInput();
-                            rDoc.SetString(aPos, aUStr, &aParam);
+                            rDocFunc.SetStringOrEditCell(aPos, aUStr, false);
                         }
                     }
                     break;

commit 1155cb67a2f888765226af13dbea4800d1345cff

tdf#146454 - Check for multiline string in setDataArray
Check for a multiline string in the setDataArray function. If that is
the case, set a string or edit cell. Otherwise, just set a plain string in order to prevent performance issues due to the adjustments of the row height.

Regression from 13824944ff5d04163880317ed8ab1946f8734a00

Change-Id: I9216b198b878fda5da29596d25504db853acd756
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127660
Tested-by: Jenkins
Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>

sc/source/ui/unoobj/cellsuno.cxx
@@ -1082,7 +1082,7 @@ static bool lcl_PutDataArray( ScDocShell& rDocShell, const ScRange& rRange,
                         const uno::Sequence< uno::Sequence<uno::Any> >& aData )
 {
     ScDocument& rDoc = rDocShell.GetDocument();
-    ScDocFunc& rDocFunc = rDocShell.GetDocFunc();
+    ScFieldEditEngine& rEngine = rDoc.GetEditEngine();
     SCTAB nTab = rRange.aStart.Tab();
     SCCOL nStartCol = rRange.aStart.Col();
     SCROW nStartRow = rRange.aStart.Row();
@@ -1159,7 +1159,19 @@ static bool lcl_PutDataArray( ScDocShell& rDocShell, const ScRange& rRange,
                         rElement >>= aUStr;
                         if ( !aUStr.isEmpty() )
                         {
-                            rDocFunc.SetStringOrEditCell(aPos, aUStr, false);
+                            // tdf#146454 - check for a multiline string since setting an edit
+                            // or string cell is in magnitudes slower than setting a plain string
+                            if (ScStringUtil::isMultiline(aUStr))
+                            {
+                                rEngine.SetTextCurrentDefaults(aUStr);
+                                rDoc.SetEditText(aPos, rEngine.CreateTextObject());
+                            }
+                            else
+                            {
+                                ScSetStringParam aParam;
+                                aParam.setTextInput();
+                                rDoc.SetString(aPos, aUStr, &aParam);
+                            }
                         }
                     }
                     break;