LemmingAvalanche/libreoffice-repaired

commit 39ece5a11159e1c588ec7eb731a20a5f777dcd2e

use more concrete types in chart2, ChartView
Change-Id: I7316d01d08eb1d9d482937fdd87bd34e2ae7e3c6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128461
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/inc/ChartView.hxx
@@ -114,7 +114,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // ___lang::XInitialization___
-    virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
+    virtual OOO_DLLPUBLIC_CHARTVIEW void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
 
     // ___ExplicitValueProvider___
     virtual bool getExplicitValuesForAxis(

chart2/source/controller/chartapiwrapper/ChartDocumentWrapper.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <ChartDocumentWrapper.hxx>
+#include <ChartView.hxx>
 #include <servicenames.hxx>
 #include <PropertyHelper.hxx>
 #include <TitleHelper.hxx>
@@ -1242,19 +1243,17 @@ uno::Reference< uno::XInterface > SAL_CALL ChartDocumentWrapper::createInstance(
     {
         if( !m_xChartView.is() )
         {
-            Reference< lang::XMultiServiceFactory > xFact(
-                m_spChart2ModelContact->m_xContext->getServiceManager(), uno::UNO_QUERY_THROW );
-            Reference< lang::XInitialization > xViewInit( xFact->createInstance(
-                    CHART_VIEW_SERVICE_NAME ), uno::UNO_QUERY );
-            if(xViewInit.is())
+            ChartModel* pModel = m_spChart2ModelContact->getModel();
+            ChartView* pChartView = pModel->getChartView();
+            if(pChartView)
             {
                 try
                 {
-                    m_xChartView = xViewInit;
+                    m_xChartView = pChartView;
 
                     Sequence< Any > aArguments{ Any(Reference<frame::XModel>(this)),
                                                 Any(true) }; // bRefreshAddIn
-                    xViewInit->initialize(aArguments);
+                    pChartView->initialize(aArguments);
                 }
                 catch (const uno::Exception&)
                 {
@@ -1262,7 +1261,7 @@ uno::Reference< uno::XInterface > SAL_CALL ChartDocumentWrapper::createInstance(
                 }
             }
         }
-        xResult.set( m_xChartView );
+        xResult.set( static_cast<cppu::OWeakObject*>(m_xChartView.get()) );
         bServiceFound = true;
     }
     else
@@ -1272,7 +1271,7 @@ uno::Reference< uno::XInterface > SAL_CALL ChartDocumentWrapper::createInstance(
         {
             if( !m_xShapeFactory.is() && m_xChartView.is() )
             {
-                m_xShapeFactory = getShapeFactory( m_xChartView );
+                m_xShapeFactory = getShapeFactory( static_cast<cppu::OWeakObject*>(m_xChartView.get()) );
             }
             else
             {
@@ -1280,7 +1279,7 @@ uno::Reference< uno::XInterface > SAL_CALL ChartDocumentWrapper::createInstance(
                 if(pModel)
                 {
                     m_xChartView = pModel->getChartView();
-                    m_xShapeFactory = getShapeFactory( m_xChartView );
+                    m_xShapeFactory = getShapeFactory( static_cast<cppu::OWeakObject*>(m_xChartView.get()) );
                 }
             }
 
@@ -1386,7 +1385,7 @@ void ChartDocumentWrapper::_disposing( const lang::EventObject& rSource )
         m_xArea.set( nullptr );
     else if( rSource.Source == m_xAddIn )
         m_xAddIn.set( nullptr );
-    else if( rSource.Source == m_xChartView )
+    else if( rSource.Source == static_cast<cppu::OWeakObject*>(m_xChartView.get()) )
         m_xChartView.set( nullptr );
 }
 

chart2/source/model/main/ChartModel.cxx
@@ -188,9 +188,11 @@ void SAL_CALL ChartModel::initialize( const Sequence< Any >& /*rArguments*/ )
     //support argument "DocumentRecoverySupport"?
 }
 
-css::uno::Reference< css::uno::XInterface > ChartModel::getChartView() const
+ChartView* ChartModel::getChartView() const
 {
-    return static_cast< ::cppu::OWeakObject* >( mxChartView.get() );
+    if(!mxChartView.is())
+        mxChartView = new ChartView( m_xContext, const_cast<ChartModel&>(*this));
+    return mxChartView.get();
 }
 
 // private methods

chart2/inc/ChartModel.hxx
@@ -120,7 +120,7 @@ private:
 
     bool mbTimeBased;
 
-    rtl::Reference<ChartView> mxChartView;
+    mutable rtl::Reference<ChartView> mxChartView;
 
     OUString m_aResource;
     css::uno::Sequence< css::beans::PropertyValue >   m_aMediaDescriptor;
@@ -454,7 +454,7 @@ public:
     css::uno::Reference< css::util::XNumberFormatsSupplier > const &
         getNumberFormatsSupplier();
 
-    css::uno::Reference< css::uno::XInterface > getChartView() const;
+    ChartView* getChartView() const;
 
     bool isTimeBased() const { return mbTimeBased;}
 

chart2/source/controller/inc/ChartDocumentWrapper.hxx
@@ -32,6 +32,7 @@
 
 namespace com::sun::star::uno { class XComponentContext; }
 namespace com::sun::star::util { class XRefreshable; }
+namespace chart { class ChartView; }
 
 namespace chart::wrapper
 {
@@ -157,7 +158,7 @@ private: //member
     OUString   m_aBaseDiagram;
     bool        m_bUpdateAddIn;
 
-    css::uno::Reference< css::uno::XInterface >   m_xChartView;
+    rtl::Reference< ChartView >                   m_xChartView;
     css::uno::Reference< css::lang::XMultiServiceFactory>
                                                   m_xShapeFactory;
 

commit 4e5bc42e162c9e245ec3367c9c7059b6b00d05c9

fix ChartDocumentWrapper::createInstance
which I broke in
    commit 39ece5a11159e1c588ec7eb731a20a5f777dcd2e
    Date:   Sat Jan 15 13:45:44 2022 +0200
    use more concrete types in chart2, ChartView

by copying behaviour from the wrong construction site.

And then in
    commit 80d979ade468f5741b7a4c39e8b01613786cb7da
    Date:   Sun Jan 16 12:25:49 2022 +0200
    use more concrete types in chart2, ChartView

I spread the error further by using the incorrect behaviour I had
introduced in
the first commit in Chart2ModelContact::getChartView()

Change-Id: I4b004b8893fa51b8968328b6a8b6ffde081f1269
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128705
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/inc/ChartView.hxx
@@ -82,7 +82,7 @@ struct TimeBasedInfo
  * The View is not responsible to handle single user events (that is instead
  * done by the ChartWindow).
  */
-class ChartView final : public ::cppu::WeakImplHelper<
+class OOO_DLLPUBLIC_CHARTVIEW ChartView final : public ::cppu::WeakImplHelper<
     css::lang::XInitialization
         ,css::lang::XServiceInfo
         ,css::datatransfer::XTransferable
@@ -114,7 +114,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // ___lang::XInitialization___
-    virtual OOO_DLLPUBLIC_CHARTVIEW void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
+    virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
 
     // ___ExplicitValueProvider___
     virtual bool getExplicitValuesForAxis(

chart2/source/controller/chartapiwrapper/Chart2ModelContact.cxx
@@ -107,7 +107,12 @@ rtl::Reference< ::chart::ChartView > const & Chart2ModelContact::getChartView() 
         // get the chart view
         rtl::Reference<ChartModel> xChartModel( m_xChartModel );
         if( xChartModel )
-            m_xChartView = xChartModel->getChartView(); // will create if necessary
+        {
+            auto xInstance = xChartModel->createInstance( CHART_VIEW_SERVICE_NAME );
+            auto pChartView = dynamic_cast<ChartView*>(xInstance.get());
+            assert(!xInstance || pChartView);
+            m_xChartView = pChartView;
+        }
     }
     return m_xChartView;
 }

chart2/source/controller/chartapiwrapper/ChartDocumentWrapper.cxx
@@ -1231,22 +1231,20 @@ uno::Reference< uno::XInterface > SAL_CALL ChartDocumentWrapper::createInstance(
     {
         if( !m_xChartView.is() )
         {
-            rtl::Reference<ChartModel> pModel = m_spChart2ModelContact->getDocumentModel();
-            ChartView* pChartView = pModel->getChartView();
-            if(pChartView)
-            {
-                try
-                {
-                    m_xChartView = pChartView;
+            rtl::Reference<::chart::ChartModel> pChartModel = new ::chart::ChartModel(m_spChart2ModelContact->m_xContext);
+            rtl::Reference<ChartView> xChartView = new ::chart::ChartView(m_spChart2ModelContact->m_xContext, *pChartModel);
 
-                    Sequence< Any > aArguments{ Any(Reference<frame::XModel>(this)),
-                                                Any(true) }; // bRefreshAddIn
-                    pChartView->initialize(aArguments);
-                }
-                catch (const uno::Exception&)
-                {
-                    DBG_UNHANDLED_EXCEPTION("chart2");
-                }
+            try
+            {
+                m_xChartView = xChartView;
+
+                Sequence< Any > aArguments{ Any(Reference<frame::XModel>(this)),
+                                            Any(true) }; // bRefreshAddIn
+                xChartView->initialize(aArguments);
+            }
+            catch (const uno::Exception&)
+            {
+                DBG_UNHANDLED_EXCEPTION("chart2");
             }
         }
         xResult.set( static_cast<cppu::OWeakObject*>(m_xChartView.get()) );

chart2/source/model/main/ChartModel.cxx
@@ -193,8 +193,6 @@ void SAL_CALL ChartModel::initialize( const Sequence< Any >& /*rArguments*/ )
 
 ChartView* ChartModel::getChartView() const
 {
-    if(!mxChartView.is())
-        mxChartView = new ChartView( m_xContext, const_cast<ChartModel&>(*this));
     return mxChartView.get();
 }
 