LemmingAvalanche/libreoffice-repaired

commit c36bc107cba2a660cf0f5f8204274b1567838193

meRememberedAnimationKind is dead
and removing it actually makes chart loading faster, because we now
avoid always instantiaing the ObjectItemSet

Change-Id: I48420bcc38c61e4f6b211724271bc61db0e1f0d0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126572
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/svx/sdr/contact/viewcontactofsdrobj.hxx
@@ -38,10 +38,6 @@ protected:
     // to be changed in any way.
     SdrObject&                                      mrObject;
 
-    // Remember AnimationKind of object. Used to find out if that kind
-    // has changed in ActionChanged()
-    SdrTextAniKind                                  meRememberedAnimationKind;
-
     // Create an Object-Specific ViewObjectContact, set ViewContact and
     // ObjectContact. Always needs to return something.
     virtual ViewObjectContact& CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact) override;
@@ -61,9 +57,6 @@ public:
     virtual sal_uInt32 GetObjectCount() const override;
     virtual ViewContact& GetViewContact(sal_uInt32 nIndex) const override;
     virtual ViewContact* GetParentContact() const override;
-
-    // React on changes of the object of this ViewContact
-    virtual void ActionChanged() override;
 
     /// override for accessing the SdrObject
     virtual SdrObject* TryToGetSdrObject() const override;

svx/source/sdr/contact/viewcontactofsdrobj.cxx
@@ -41,14 +41,8 @@ ViewObjectContact& ViewContactOfSdrObj::CreateObjectSpecificViewObjectContact(Ob
 }
 
 ViewContactOfSdrObj::ViewContactOfSdrObj(SdrObject& rObj)
-:   mrObject(rObj),
-    meRememberedAnimationKind(SdrTextAniKind::NONE)
+:   mrObject(rObj)
 {
-    // init AnimationKind
-    if(auto pTextObj = dynamic_cast<const SdrTextObj*>( &GetSdrObject() ))
-    {
-        meRememberedAnimationKind = pTextObj->GetTextAniKind();
-    }
 }
 
 ViewContactOfSdrObj::~ViewContactOfSdrObj()
@@ -98,23 +92,6 @@ ViewContact* ViewContactOfSdrObj::GetParentContact() const
     }
 
     return pRetval;
-}
-
-// React on changes of the object of this ViewContact
-void ViewContactOfSdrObj::ActionChanged()
-{
-    // look for own changes
-    if (SdrTextObj* pTextObj = dynamic_cast<SdrTextObj*>(&GetSdrObject()))
-    {
-        if (pTextObj->GetTextAniKind() != meRememberedAnimationKind)
-        {
-            // #i38135# now remember new type
-            meRememberedAnimationKind = pTextObj->GetTextAniKind();
-        }
-    }
-
-    // call parent
-    ViewContact::ActionChanged();
 }
 
 // override for accessing the SdrObject

commit af0968ec8f67e72472b02c13d56e94c848512b5d

tdf#146860 Vertical alignment of text inside grouped shapes textbox incorrect
some stupid side effect at work

regression from
    commit c36bc107cba2a660cf0f5f8204274b1567838193
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Wed Dec 8 12:25:58 2021 +0200
    meRememberedAnimationKind is dead

Change-Id: I7ba245a2096ed011f88e08f9d3a87021948e0b01
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129219
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/svx/sdr/contact/viewcontactofsdrobj.hxx
@@ -55,6 +55,9 @@ public:
     virtual ViewContact& GetViewContact(sal_uInt32 nIndex) const override;
     virtual ViewContact* GetParentContact() const override;
 
+    // React on changes of the object of this ViewContact
+    virtual void ActionChanged() override;
+
     /// override for accessing the SdrObject
     virtual SdrObject* TryToGetSdrObject() const override;
 

svx/source/sdr/contact/viewcontactofsdrobj.cxx
@@ -94,6 +94,20 @@ ViewContact* ViewContactOfSdrObj::GetParentContact() const
     return pRetval;
 }
 
+// React on changes of the object of this ViewContact
+void ViewContactOfSdrObj::ActionChanged()
+{
+    // look for own changes
+    if (SdrTextObj* pTextObj = dynamic_cast<SdrTextObj*>(&GetSdrObject()))
+    {
+        //  tdf#146860 no idea why, but calling this makes the text boxes render properly
+        pTextObj->GetTextAniKind();
+    }
+
+    // call parent
+    ViewContact::ActionChanged();
+}
+
 // override for accessing the SdrObject
 SdrObject* ViewContactOfSdrObj::TryToGetSdrObject() const
 {