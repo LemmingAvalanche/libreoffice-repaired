LibreOffice/core

Fix ambiguous error in VS
Change-Id: If18ef06f5a27a4266b21df4ba07bd5bc01255f55

sfx2/source/view/ipclient.cxx
@@ -93,9 +93,9 @@ public:
 tools::Rectangle lcl_negateRectX(const tools::Rectangle& rRect)
 {
     return tools::Rectangle(
-        std::max(0l, -rRect.Right()),
+        std::max(reinterpret_cast<tools::Long>(0l), -rRect.Right()),
         rRect.Top(),
-        std::max(0l, -rRect.Left()),
+        std::max(reinterpret_cast<tools::Long>(0l), -rRect.Left()),
         rRect.Bottom());
 }
 

LemmingAvalanche/libreoffice-repaired

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128381
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
(cherry picked from commit c2a2ae022132b1dc0c2fae207e45b5c16ecf25f5)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129439
Tested-by: Jenkins
Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

sfx2/source/view/ipclient.cxx
@@ -94,9 +94,9 @@ public:
 tools::Rectangle lcl_negateRectX(const tools::Rectangle& rRect)
 {
     return tools::Rectangle(
-        std::max(0l, -rRect.Right()),
+        std::max(static_cast<tools::Long>(0l), -rRect.Right()),
         rRect.Top(),
-        std::max(0l, -rRect.Left()),
+        std::max(static_cast<tools::Long>(0l), -rRect.Left()),
         rRect.Bottom());
 }
 