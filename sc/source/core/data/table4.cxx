LemmingAvalanche/libreoffice-repaired

commit 293f59e26531f6dccc2c6dd4fe3c39c5ccd4ee1c

Take advantage of segment-based hidden flag storage.
To avoid querying this states for every single column and row.

Change-Id: I3e570ae5309c826e809c170ea4635dd37c9b0d43

sc/source/core/data/table4.cxx
@@ -1270,10 +1270,19 @@ void ScTable::FillAutoSimple(
     CellType eCellType = CELLTYPE_NONE;
     bool bIsOrdinalSuffix = false;
 
+    bool bColHidden = false, bRowHidden = false;
+    SCCOL nColHiddenLast = -1;
+    SCROW nRowHiddenLast = -1;
+
     rInner = nIStart;
     while (true)        // #i53728# with "for (;;)" old solaris/x86 compiler mis-optimizes
     {
-        if(!ColHidden(rCol) && !RowHidden(rRow))
+        if (rCol > nColHiddenLast)
+            bColHidden = ColHidden(rCol, NULL, &nColHiddenLast);
+        if (rRow > nRowHiddenLast)
+            bRowHidden = RowHidden(rRow, NULL, &nRowHiddenLast);
+
+        if (!bColHidden && !bRowHidden)
         {
             if ( bGetCell )
             {

commit df2d9d44c6659e6755f16044ee89150a7b780ef7

tdf#113785 sc FillAutoSimple: also optimize in negative direction
By only coding the optimization for the downward autofill,
it totally broke an upward autofill.
So this patch simply fixes the LO 4.3ish regression
from commit 293f59e26531f6dccc2c6dd4fe3c39c5ccd4ee1c.

This fix means that the non-hidden cells get the auto-value
instead of being erased.

However, the hidden cells are still erased
(which is the topic of tdf#119957).

I'm not going to bother with a unit test for this
portion. If I can solve bug 119957 as well,
then the unit test for that can cover both situations.

Change-Id: If6320ccf87fa8893ca6766c265b7760cc46ed7d8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129723
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sc/source/core/data/table4.cxx
@@ -1782,16 +1782,28 @@ void ScTable::FillAutoSimple(
     bool bIsOrdinalSuffix = false;
 
     bool bColHidden = false, bRowHidden = false;
+    SCCOL nColHiddenFirst = rDocument.MaxCol();
     SCCOL nColHiddenLast = -1;
+    SCROW nRowHiddenFirst = rDocument.MaxRow();
     SCROW nRowHiddenLast = -1;
 
     rInner = nIStart;
     while (true)        // #i53728# with "for (;;)" old solaris/x86 compiler mis-optimizes
     {
-        if (rCol > nColHiddenLast)
-            bColHidden = ColHidden(rCol, nullptr, &nColHiddenLast);
-        if (rRow > nRowHiddenLast)
-            bRowHidden = RowHidden(rRow, nullptr, &nRowHiddenLast);
+        if (bPositive)
+        {
+            if (rCol > nColHiddenLast)
+                bColHidden = ColHidden(rCol, nullptr, &nColHiddenLast);
+            if (rRow > nRowHiddenLast)
+                bRowHidden = RowHidden(rRow, nullptr, &nRowHiddenLast);
+        }
+        else
+        {
+            if (rCol < nColHiddenFirst)
+                bColHidden = ColHidden(rCol, &nColHiddenFirst);
+            if (rRow < nRowHiddenFirst)
+                bRowHidden = RowHidden(rRow, &nRowHiddenFirst);
+        }
 
         if (!bColHidden && !bRowHidden)
         {