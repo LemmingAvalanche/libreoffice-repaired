LemmingAvalanche/libreoffice-repaired

commit 27279d98f0fa0010ec5225e7b9cd17c5d6fc1ad6

tdf#94559: 4th step to remove rtti.hxx
    replaced use of PTR_CAST, IS_TYPE, ISA in
    idl, editeng, sc, sd, sw, sfx2, sot, starmath

Change-Id: I4a5bba4fdc4829099618c09b690c83f876a3d653
Reviewed-on: https://gerrit.libreoffice.org/19132
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Oliver Specht <oliver.specht@cib.de>

sw/source/core/access/accmap.cxx
@@ -165,9 +165,9 @@ void SwDrawModellListener_Impl::Notify( SfxBroadcaster& /*rBC*/,
     const SdrHint *pSdrHint = dynamic_cast<const SdrHint*>( &rHint );
     if ( !pSdrHint ||
          ( pSdrHint->GetObject() &&
-           ( pSdrHint->GetObject()->ISA(SwFlyDrawObj) ||
-             pSdrHint->GetObject()->ISA(SwVirtFlyDrawObj) ||
-             IS_TYPE(SdrObject,pSdrHint->GetObject()) ) ) )
+           ( dynamic_cast< const SwFlyDrawObj* >(pSdrHint->GetObject()) !=  nullptr ||
+              dynamic_cast< const SwVirtFlyDrawObj* >(pSdrHint->GetObject()) !=  nullptr ||
+             typeid(SdrObject) == typeid(pSdrHint->GetObject()) ) ) )
     {
         return;
     }
@@ -1122,7 +1122,7 @@ void SwAccessibleMap::InvalidateShapeInParaSelection()
     size_t nShapes = 0;
 
     const SwViewShell *pVSh = GetShell();
-    const SwFEShell *pFESh = pVSh->ISA( SwFEShell ) ?
+    const SwFEShell *pFESh = dynamic_cast<const SwFEShell*>( pVSh) !=  nullptr ?
                             static_cast< const SwFEShell * >( pVSh ) : nullptr;
     SwPaM* pCrsr = pFESh ? pFESh->GetCrsr( false /* ??? */ ) : nullptr;
 
@@ -1439,7 +1439,7 @@ void SwAccessibleMap::DoInvalidateShapeSelection(bool bInvalidateFocusMode /*=fa
     size_t nShapes = 0;
 
     const SwViewShell *pVSh = GetShell();
-    const SwFEShell *pFESh = pVSh->ISA( SwFEShell ) ?
+    const SwFEShell *pFESh = dynamic_cast<const SwFEShell*>( pVSh) !=  nullptr ?
                             static_cast< const SwFEShell * >( pVSh ) : nullptr;
     const size_t nSelShapes = pFESh ? pFESh->IsObjSelected() : 0;
 
@@ -1607,7 +1607,7 @@ void SwAccessibleMap::DoInvalidateShapeSelection(bool bInvalidateFocusMode /*=fa
 void SwAccessibleMap::DoInvalidateShapeFocus()
 {
     const SwViewShell *pVSh = GetShell();
-    const SwFEShell *pFESh = pVSh->ISA( SwFEShell ) ?
+    const SwFEShell *pFESh = dynamic_cast<const SwFEShell*>( pVSh) !=  nullptr ?
                             static_cast< const SwFEShell * >( pVSh ) : nullptr;
     const size_t nSelShapes = pFESh ? pFESh->IsObjSelected() : 0;
 
@@ -2586,7 +2586,7 @@ void SwAccessibleMap::InvalidateCursorPosition( const SwFrm *pFrm )
     SwAccessibleChild aFrmOrObj( pFrm );
     bool bShapeSelected = false;
     const SwViewShell *pVSh = GetShell();
-    if( pVSh->ISA( SwCrsrShell ) )
+    if( dynamic_cast<const SwCrsrShell*>( pVSh) !=  nullptr )
     {
         const SwCrsrShell *pCSh = static_cast< const SwCrsrShell * >( pVSh );
         if( pCSh->IsTableMode() )
@@ -2594,7 +2594,7 @@ void SwAccessibleMap::InvalidateCursorPosition( const SwFrm *pFrm )
             while( aFrmOrObj.GetSwFrm() && !aFrmOrObj.GetSwFrm()->IsCellFrm() )
                 aFrmOrObj = aFrmOrObj.GetSwFrm()->GetUpper();
         }
-        else if( pVSh->ISA( SwFEShell ) )
+        else if( dynamic_cast<const SwFEShell*>( pVSh) !=  nullptr )
         {
             const SwFEShell *pFESh = static_cast< const SwFEShell * >( pVSh );
             const SwFrm *pFlyFrm = pFESh->GetSelectedFlyFrm();

sw/source/core/doc/doclay.cxx
@@ -147,9 +147,9 @@ SdrObject* SwDoc::CloneSdrObj( const SdrObject& rObj, bool bMoveWithinDoc,
 
     // For drawing objects: set layer of cloned object to invisible layer
     SdrLayerID nLayerIdForClone = rObj.GetLayer();
-    if ( !pObj->ISA(SwFlyDrawObj) &&
-         !pObj->ISA(SwVirtFlyDrawObj) &&
-         !IS_TYPE(SdrObject,pObj) )
+    if ( dynamic_cast<const SwFlyDrawObj*>( pObj) ==  nullptr &&
+         dynamic_cast<const SwVirtFlyDrawObj*>( pObj) ==  nullptr &&
+         typeid(SdrObject) != typeid(pObj) )
     {
         if ( getIDocumentDrawModelAccess().IsVisibleLayerId( nLayerIdForClone ) )
         {
@@ -551,7 +551,7 @@ SwPosFlyFrms SwDoc::GetAllFlyFormats( const SwPaM* pCmpRange, bool bDrawAlso,
             {
                 SwAnchoredObject* pAnchoredObj = rObjs[i];
                 SwFrameFormat *pFly;
-                if ( pAnchoredObj->ISA(SwFlyFrm) )
+                if ( dynamic_cast<const SwFlyFrm*>( pAnchoredObj) !=  nullptr )
                     pFly = &(pAnchoredObj->GetFrameFormat());
                 else if ( bDrawAlso )
                     pFly = &(pAnchoredObj->GetFrameFormat());

sw/source/uibase/app/docsh2.cxx
@@ -145,7 +145,7 @@ VclPtr<SfxDocumentInfoDialog> SwDocShell::CreateDocumentInfoDialog(
     {
         //Not for SourceView.
         SfxViewShell *pVSh = SfxViewShell::Current();
-        if ( pVSh && !pVSh->ISA(SwSrcView) )
+        if ( pVSh && dynamic_cast< const SwSrcView *>( pVSh ) ==  nullptr )
         {
             SwAbstractDialogFactory* pFact = SwAbstractDialogFactory::Create();
             OSL_ENSURE(pFact, "SwAbstractDialogFactory fail!");
@@ -394,10 +394,9 @@ void SwDocShell::Execute(SfxRequest& rReq)
             {
                 // call on all Docs the idle formatter to start
                 // the collection of Words
-                TypeId aType = TYPE(SwDocShell);
-                for( SwDocShell *pDocSh = static_cast<SwDocShell*>(SfxObjectShell::GetFirst(&aType));
+                for( SwDocShell *pDocSh = static_cast<SwDocShell*>(SfxObjectShell::GetFirst(checkSfxObjectShell<SwDocShell>));
                      pDocSh;
-                     pDocSh = static_cast<SwDocShell*>(SfxObjectShell::GetNext( *pDocSh, &aType )) )
+                     pDocSh = static_cast<SwDocShell*>(SfxObjectShell::GetNext( *pDocSh, checkSfxObjectShell<SwDocShell> )) )
                 {
                     SwDoc* pTmp = pDocSh->GetDoc();
                     if ( pTmp->getIDocumentLayoutAccess().GetCurrentViewShell() )
@@ -414,13 +413,13 @@ void SwDocShell::Execute(SfxRequest& rReq)
                 SfxViewFrame *pTmpFrm = SfxViewFrame::GetFirst(this);
                 SfxViewShell* pViewShell = SfxViewShell::Current();
                 SwView* pCurrView = dynamic_cast< SwView *> ( pViewShell );
-                bool bCurrent = IS_TYPE( SwPagePreview, pViewShell );
+                bool bCurrent = typeid(SwPagePreview) == typeid( pViewShell );
 
                 while( pTmpFrm )    // search Preview
                 {
-                    if( IS_TYPE( SwView, pTmpFrm->GetViewShell()) )
+                    if( typeid(SwView) == typeid( pTmpFrm->GetViewShell()) )
                         bOnly = false;
-                    else if( IS_TYPE( SwPagePreview, pTmpFrm->GetViewShell()))
+                    else if( typeid(SwPagePreview) == typeid( pTmpFrm->GetViewShell()))
                     {
                         pTmpFrm->GetFrame().Appear();
                         bFound = true;
@@ -446,7 +445,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                 {
                     // PagePreview in the WebDocShell
                     // is found under Id VIEWSHELL2.
-                    if( ISA(SwWebDocShell) && SID_VIEWSHELL1 == nSlotId )
+                    if( dynamic_cast< const SwWebDocShell *>( this ) !=  nullptr && SID_VIEWSHELL1 == nSlotId )
                         nSlotId = SID_VIEWSHELL2;
 
                     if( pCurrView && pCurrView->GetDocShell() == this )
@@ -525,7 +524,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                             }
                             pFlt = aIter.Next();
                         }
-                        bool bWeb = 0 != dynamic_cast< SwWebDocShell *>( this );
+                        bool bWeb = dynamic_cast< SwWebDocShell *>( this ) !=  nullptr;
                         const SfxFilter *pOwnFlt =
                                 SwDocShell::Factory().GetFilterContainer()->
                                 GetFilter4FilterName(OUString("writer8"));
@@ -1108,7 +1107,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
         case SID_ATTR_YEAR2000:
             if ( pArgs && SfxItemState::SET == pArgs->GetItemState( nWhich , false, &pItem ))
             {
-                OSL_ENSURE(pItem->ISA(SfxUInt16Item), "wrong Item");
+                OSL_ENSURE(dynamic_cast< const SfxUInt16Item *>( pItem ) !=  nullptr, "wrong Item");
                 sal_uInt16 nYear2K = static_cast<const SfxUInt16Item*>(pItem)->GetValue();
                 // iterate over Views and put the State to FormShells
 
@@ -1501,14 +1500,13 @@ int SwFindDocShell( SfxObjectShellRef& xDocSh,
     aTmpObj.SetMark( OUString() );
 
     // Iterate over the DocShell and get the ones with the name
-    TypeId aType( TYPE(SwDocShell) );
 
     SfxObjectShell* pShell = pDestSh;
     bool bFirst = 0 != pShell;
 
     if( !bFirst )
         // No DocShell passed, starting with the first from the DocShell list
-        pShell = SfxObjectShell::GetFirst( &aType );
+        pShell = SfxObjectShell::GetFirst( checkSfxObjectShell<SwDocShell> );
 
     while( pShell )
     {
@@ -1531,10 +1529,10 @@ int SwFindDocShell( SfxObjectShellRef& xDocSh,
         if( bFirst )
         {
             bFirst = false;
-            pShell = SfxObjectShell::GetFirst( &aType );
+            pShell = SfxObjectShell::GetFirst( checkSfxObjectShell<SwDocShell> );
         }
         else
-            pShell = SfxObjectShell::GetNext( *pShell, &aType );
+            pShell = SfxObjectShell::GetNext( *pShell, checkSfxObjectShell<SwDocShell> );
     }
 
     // 2. Open the file ourselves

commit af212aeacf91f4b2940e1bbe8903e91f3b79665e

Fix some tautological std::type_info (in-)equality comparisons
...which could never succeed.

I became aware of this when Clang 15 trunk -std=c++2b, implementing
<http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2021/p1328r1.html> "Making
std::type_info::operator== constexpr", pointed at two of the broken comparisons
with

> sw/source/uibase/app/docsh2.cxx:478:25: error: code will never be executed [-Werror,-Wunreachable-code]
>                         pTmpFrame->GetFrame().Appear();
>                         ^~~~~~~~~
> sw/source/uibase/app/docsh2.cxx:475:33: error: code will never be executed [-Werror,-Wunreachable-code]
>                         bOnly = false;
>                                 ^~~~~

(It didn't emit warnings pointing at any of the other broken comparisons,
though.)

All of these broken comparisons were regressions introduced with
27279d98f0fa0010ec5225e7b9cd17c5d6fc1ad6 "tdf#94559: 4th step to remove
rtti.hxx", replacing uses of the IS_TYPE macro (from include/tools/rtti.hxx,
meanwhile removed with 39bd8231a7b8905a50dea5c788ae936714b66e03 "Remove unused
rtti.hxx").

I now added loplugin:typeidcomparison to also find the other broken comparisons
introduced by that commit.  (The remaining cases where that commit replaced uses
of TYPE_INFO with typeid comparisons were correct and/or have meanwhile been
replaced with code not using typeid, see
9254d9de1c9f804ff256d2372b185b0114a2d1b5 "simplify some use of typeid" and
eeb5dcce463afdd080fd72381982ee9ffcbcbe25 "~SwModify: do not silently tolerate
clients registered past death".)

The original IS_TYPE macro made sure not to dereference null pointers,

> #define IS_TYPE(T,pObj) \
>         ( pObj && (pObj)->Type() == TYPE(T) )

I don't know if any of the pointers now dereferenced in those typeid expressions
can legitimately be null.  But to be on the safe side, I replicated that check
in the newly introduced isType (sw/inc/istype.hxx).

compilerplugins/clang/typeidcomparison.cxx
@@ -1,0 +1,90 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// Find (in-)equality comparisons between typeid expressions that can never succeed.  For now, just
+// detects cases where the two involved types are structurally different, one a pointer type and the
+// other a non-pointer type.
+
+#ifndef LO_CLANG_SHARED_PLUGINS
+
+#include "plugin.hxx"
+
+namespace
+{
+class TypeidComparison final : public loplugin::FilteringPlugin<TypeidComparison>
+{
+public:
+    explicit TypeidComparison(loplugin::InstantiationData const& data)
+        : FilteringPlugin(data)
+    {
+    }
+
+    // For CXXRewrittenBinaryOperator `typeid(...) != typeid(...)`:
+    bool shouldVisitImplicitCode() const { return true; }
+
+    bool preRun() override { return compiler.getLangOpts().CPlusPlus; }
+
+    void run() override
+    {
+        if (preRun())
+        {
+            TraverseDecl(compiler.getASTContext().getTranslationUnitDecl());
+        }
+    }
+
+    bool VisitCXXOperatorCallExpr(CXXOperatorCallExpr const* expr)
+    {
+        if (ignoreLocation(expr))
+        {
+            return true;
+        }
+        auto const op = expr->getOperator();
+        if (op != OO_EqualEqual && op != OO_ExclaimEqual)
+        {
+            return true;
+        }
+        assert(expr->getNumArgs() == 2);
+        auto const e1 = dyn_cast<CXXTypeidExpr>(expr->getArg(0)->IgnoreParenImpCasts());
+        if (e1 == nullptr)
+        {
+            return true;
+        }
+        auto const e2 = dyn_cast<CXXTypeidExpr>(expr->getArg(1)->IgnoreParenImpCasts());
+        if (e2 == nullptr)
+        {
+            return true;
+        }
+        auto const t1 = getOperandType(e1);
+        auto const t2 = getOperandType(e2);
+        if (t1->isPointerType() == t2->isPointerType())
+        {
+            return true;
+        }
+        report(DiagnosticsEngine::Warning,
+               "comparison of type info of mixed pointer and non-pointer types %0 and %1 can never "
+               "succeed",
+               expr->getExprLoc())
+            << t1 << t2 << expr->getSourceRange();
+        return true;
+    }
+
+private:
+    QualType getOperandType(CXXTypeidExpr const* expr)
+    {
+        return expr->isTypeOperand() ? expr->getTypeOperand(compiler.getASTContext())
+                                     : expr->getExprOperand()->getType();
+    }
+};
+
+static loplugin::Plugin::Registration<TypeidComparison> typeidcomparison("typeidcomparison");
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

solenv/CompilerTest_compilerplugins_clang.mk
@@ -95,6 +95,7 @@ $(eval $(call gb_CompilerTest_add_exception_objects,compilerplugins_clang, \
     compilerplugins/clang/test/stringview \
     compilerplugins/clang/test/stringviewparam \
     compilerplugins/clang/test/typedefparam \
+    compilerplugins/clang/test/typeidcomparison \
     compilerplugins/clang/test/unnecessarycatchthrow \
     compilerplugins/clang/test/unnecessaryoverride \
     compilerplugins/clang/test/unnecessaryoverride-dtor \

sw/inc/istype.hxx
@@ -1,0 +1,21 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <sal/config.h>
+
+#include <typeinfo>
+
+template <typename T1, typename T2> bool isType(T2* p)
+{
+    return p != nullptr && typeid(*p) == typeid(T1);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

sw/source/core/access/accmap.cxx
@@ -42,6 +42,7 @@
 #include "acccell.hxx"
 #include "acctable.hxx"
 #include <fesh.hxx>
+#include <istype.hxx>
 #include <rootfrm.hxx>
 #include <txtfrm.hxx>
 #include <hffrm.hxx>
@@ -198,7 +199,7 @@ void SwDrawModellListener_Impl::Notify( SfxBroadcaster& /*rBC*/,
     if (pSdrHint->GetObject() &&
            ( dynamic_cast< const SwFlyDrawObj* >(pSdrHint->GetObject()) !=  nullptr ||
               dynamic_cast< const SwVirtFlyDrawObj* >(pSdrHint->GetObject()) !=  nullptr ||
-             typeid(SdrObject) == typeid(pSdrHint->GetObject()) ) )
+             isType<SdrObject>(pSdrHint->GetObject()) ) )
     {
         return;
     }

sw/source/core/doc/doclay.cxx
@@ -30,6 +30,7 @@
 #include <osl/diagnose.h>
 #include <svx/svdouno.hxx>
 #include <editeng/frmdiritem.hxx>
+#include <istype.hxx>
 #include <swmodule.hxx>
 #include <modcfg.hxx>
 #include <com/sun/star/beans/XPropertySet.hpp>
@@ -139,7 +140,7 @@ SdrObject* SwDoc::CloneSdrObj( const SdrObject& rObj, bool bMoveWithinDoc,
     SdrLayerID nLayerIdForClone = rObj.GetLayer();
     if ( dynamic_cast<const SwFlyDrawObj*>( pObj) ==  nullptr &&
          dynamic_cast<const SwVirtFlyDrawObj*>( pObj) ==  nullptr &&
-         typeid(SdrObject) != typeid(pObj) )
+         !isType<SdrObject>(pObj) )
     {
         if ( getIDocumentDrawModelAccess().IsVisibleLayerId( nLayerIdForClone ) )
         {

sw/source/uibase/app/docsh2.cxx
@@ -66,6 +66,7 @@
 #include <basic/basmgr.hxx>
 #include <comphelper/classids.hxx>
 #include <fmtcol.hxx>
+#include <istype.hxx>
 #include <view.hxx>
 #include <docsh.hxx>
 #include <docary.hxx>
@@ -467,13 +468,13 @@ void SwDocShell::Execute(SfxRequest& rReq)
                 SfxViewFrame *pTmpFrame = SfxViewFrame::GetFirst(this);
                 SfxViewShell* pViewShell = SfxViewShell::Current();
                 SwView* pCurrView = dynamic_cast< SwView *> ( pViewShell );
-                bool bCurrent = typeid(SwPagePreview) == typeid( pViewShell );
+                bool bCurrent = isType<SwPagePreview>( pViewShell );
 
                 while( pTmpFrame )    // search Preview
                 {
-                    if( typeid(SwView) == typeid( pTmpFrame->GetViewShell()) )
+                    if( isType<SwView>( pTmpFrame->GetViewShell()) )
                         bOnly = false;
-                    else if( typeid(SwPagePreview) == typeid( pTmpFrame->GetViewShell()))
+                    else if( isType<SwPagePreview>( pTmpFrame->GetViewShell()))
                     {
                         pTmpFrame->GetFrame().Appear();
                         bFound = true;

compilerplugins/clang/test/typeidcomparison.cxx
@@ -1,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <typeinfo>
+
+struct Base
+{
+    virtual ~Base();
+};
+
+struct Derived : Base
+{
+};
+
+void good(Base* p)
+{
+    (void)(typeid(*p) == typeid(Derived));
+    (void)(typeid(Derived) == typeid(*p));
+    (void)(typeid(*p) != typeid(Derived));
+    (void)(typeid(Derived) != typeid(*p));
+}
+
+void bad(Base* p)
+{
+    // expected-error@+1 {{comparison of type info of mixed pointer and non-pointer types 'Base *' and 'Derived' can never succeed [loplugin:typeidcomparison]}}
+    (void)(typeid(p) == typeid(Derived));
+    // expected-error@+1 {{comparison of type info of mixed pointer and non-pointer types 'Derived' and 'Base *' can never succeed [loplugin:typeidcomparison]}}
+    (void)(typeid(Derived) == typeid(p));
+    // expected-error@+1 {{comparison of type info of mixed pointer and non-pointer types 'Base *' and 'Derived' can never succeed [loplugin:typeidcomparison]}}
+    (void)(typeid(p) != typeid(Derived));
+    // expected-error@+1 {{comparison of type info of mixed pointer and non-pointer types 'Derived' and 'Base *' can never succeed [loplugin:typeidcomparison]}}
+    (void)(typeid(Derived) != typeid(p));
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */