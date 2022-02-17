LemmingAvalanche/libreoffice-repaired

commit 68e98c66d69f06135acd268f3a4cda67ef618776

used cache value in ViewObjectContact::getPrimitive2DSequence (2nd attempt)
Rely on the cached primitives in VOC.

But only enable it for calc and draw right now, by adding a flag
at the SdrModel level.

This way we can leave it disabled for writer, where it definitely doesn't
work.

Change-Id: I11ca4836eb773c0f078cdb82056c6e0309d15a8b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128319
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -29,6 +29,8 @@
 #include <drawinglayer/processor2d/baseprocessor2d.hxx>
 #include <svx/sdr/primitive2d/svx_primitivetypes2d.hxx>
 #include <drawinglayer/primitive2d/transformprimitive2d.hxx>
+#include <svx/svdobj.hxx>
+#include <svx/svdmodel.hxx>
 
 using namespace com::sun::star;
 
@@ -147,6 +149,7 @@ ViewObjectContact::ViewObjectContact(ObjectContact& rObjectContact, ViewContact&
 :   mrObjectContact(rObjectContact),
     mrViewContact(rViewContact),
     maGridOffset(0.0, 0.0),
+    mnActionChangedCount(0),
     mbLazyInvalidate(false)
 {
     // make the ViewContact remember me
@@ -193,7 +196,7 @@ const basegfx::B2DRange& ViewObjectContact::getObjectRange() const
         {
             // if range is not computed (new or LazyInvalidate objects), force it
             const DisplayInfo aDisplayInfo;
-            const drawinglayer::primitive2d::Primitive2DContainer xSequence(getPrimitive2DSequence(aDisplayInfo));
+            const drawinglayer::primitive2d::Primitive2DContainer& xSequence(getPrimitive2DSequence(aDisplayInfo));
 
             if(!xSequence.empty())
             {
@@ -208,6 +211,10 @@ const basegfx::B2DRange& ViewObjectContact::getObjectRange() const
 
 void ViewObjectContact::ActionChanged()
 {
+    // clear cached primitives
+    mxPrimitive2DSequence.clear();
+    ++mnActionChangedCount;
+
     if(mbLazyInvalidate)
         return;
 
@@ -330,6 +337,14 @@ void ViewObjectContact::createPrimitive2DSequence(const DisplayInfo& rDisplayInf
 
 drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const
 {
+    // only some of the top-level apps are any good at reliably invalidating us (e.g. writer is not)
+    if (SdrObject* pSdrObj = mrViewContact.TryToGetSdrObject())
+        if (pSdrObj->getSdrModelFromSdrObject().IsVOCInvalidationIsReliable())
+        {
+            if (!mxPrimitive2DSequence.empty())
+                return mxPrimitive2DSequence;
+        }
+
     /**
     This method is weird because
     (1) we have to re-walk the primitive tree because the flushing is unreliable
@@ -415,21 +430,28 @@ void ViewObjectContact::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInf
     if(!isPrimitiveVisible(rDisplayInfo))
         return;
 
-    drawinglayer::primitive2d::Primitive2DContainer xRetval = getPrimitive2DSequence(rDisplayInfo);
-    if(xRetval.empty())
+    getPrimitive2DSequence(rDisplayInfo);
+    if(mxPrimitive2DSequence.empty())
         return;
 
     // get ranges
     const drawinglayer::geometry::ViewInformation2D& rViewInformation2D(GetObjectContact().getViewInformation2D());
-    const basegfx::B2DRange aObjectRange(xRetval.getB2DRange(rViewInformation2D));
     const basegfx::B2DRange& aViewRange(rViewInformation2D.getViewport());
 
     // check geometrical visibility
-    bool bVisible = aViewRange.isEmpty() || aViewRange.overlaps(aObjectRange);
+    bool bVisible = aViewRange.isEmpty() || aViewRange.overlaps(maObjectRange);
     if(!bVisible)
         return;
 
-    rVisitor.visit(std::move(xRetval));
+    // temporarily take over the mxPrimitive2DSequence, in case it gets invalidated while we want to iterate over it
+    auto tmp = std::move(const_cast<ViewObjectContact*>(this)->mxPrimitive2DSequence);
+    int nPrevCount = mnActionChangedCount;
+
+    rVisitor.visit(tmp);
+
+    // if we received ActionChanged() calls while walking the primitives, then leave it empty, otherwise move it back
+    if (mnActionChangedCount == nPrevCount)
+        const_cast<ViewObjectContact*>(this)->mxPrimitive2DSequence = std::move(tmp);
 }
 
 void ViewObjectContact::getPrimitive2DSequenceSubHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor) const

include/svx/sdr/contact/viewobjectcontact.hxx
@@ -59,6 +59,9 @@ private:
     // possible on-demand calculated GridOffset for non-linear ViewToDevice transformation (calc)
     basegfx::B2DVector                              maGridOffset;
 
+    // used to detect ActionChanged() during primitive construction
+    int                                             mnActionChangedCount;
+
     // This bool gets set when the object gets invalidated by ActionChanged() and
     // can be used from the OC to late-invalidates
     bool                                            mbLazyInvalidate : 1;

include/svx/svdmodel.hxx
@@ -210,6 +210,7 @@ protected:
     bool                m_bPasteResize:1; // Objects are being resized due to Paste with different MapMode
     bool                m_bStarDrawPreviewMode:1;
     bool                mbDisableTextEditUsesCommonUndoManager:1;
+    bool                mbVOCInvalidationIsReliable:1; // does the app reliably invalidate the VOC, or do we need to rebuild the primitives on every render?
     sal_uInt16          m_nDefaultTabulator;
     sal_uInt32          m_nMaxUndoCount;
 
@@ -612,6 +613,9 @@ public:
     virtual sal_Int32 getImagePreferredDPI() const { return 0; }
 
     virtual void dumpAsXml(xmlTextWriterPtr pWriter) const;
+
+    bool IsVOCInvalidationIsReliable() const { return mbVOCInvalidationIsReliable; }
+    void SetVOCInvalidationIsReliable(bool b) { mbVOCInvalidationIsReliable = b; }
 };
 
 /*

sc/source/core/data/drwlayer.cxx
@@ -239,6 +239,8 @@ ScDrawLayer::ScDrawLayer( ScDocument* pDocument, const OUString& rName ) :
     bAdjustEnabled( true ),
     bHyphenatorSet( false )
 {
+    SetVOCInvalidationIsReliable(true);
+
     pGlobalDrawPersist = nullptr;          // Only use once
 
     SfxObjectShell* pObjSh = pDocument ? pDocument->GetDocumentShell() : nullptr;

sd/source/core/drawdoc.cxx
@@ -139,6 +139,8 @@ SdDrawDocument::SdDrawDocument(DocumentType eType, SfxObjectShell* pDrDocSh)
 , mbEmbedFontScriptComplex(true)
 , mnImagePreferredDPI(0)
 {
+    SetVOCInvalidationIsReliable(true);
+
     mpDrawPageListWatcher.reset(new ImpDrawPageListWatcher(*this));
     mpMasterPageListWatcher.reset(new ImpMasterPageListWatcher(*this));
 

svx/source/svdraw/svdmodel.cxx
@@ -121,6 +121,7 @@ SdrModel::SdrModel(SfxItemPool* pPool, comphelper::IEmbeddedHelper* pEmbeddedHel
     , m_bPasteResize(false)
     , m_bStarDrawPreviewMode(false)
     , mbDisableTextEditUsesCommonUndoManager(false)
+    , mbVOCInvalidationIsReliable(false)
     , m_nDefaultTabulator(0)
     , m_nMaxUndoCount(16)
     , m_pTextChain(new TextChain)

commit d73a520935082452a0d680db85b10ab28cc86878

tdf#147164 Drawings are gone converting to PDF from commandline
regression from
    commit 68e98c66d69f06135acd268f3a4cda67ef618776
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Wed Jan 12 10:27:38 2022 +0200
used cache value in ViewObjectContact::getPrimitive2DSequence (2nd
attempt)

for reasons I do not understand, the cached maObjectRange is wrong at this
point in the execution, so switch back to fetching it again, like the code
did before the above commit,

Change-Id: I4a9f0abc38e59ef687b460689a30ee4badf620a0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129994
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -436,10 +436,12 @@ void ViewObjectContact::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInf
 
     // get ranges
     const drawinglayer::geometry::ViewInformation2D& rViewInformation2D(GetObjectContact().getViewInformation2D());
+    // tdf#147164 cannot use maObjectRange here, it is unreliable
+    const basegfx::B2DRange aObjectRange(mxPrimitive2DSequence.getB2DRange(rViewInformation2D));
     const basegfx::B2DRange& aViewRange(rViewInformation2D.getViewport());
 
     // check geometrical visibility
-    bool bVisible = aViewRange.isEmpty() || aViewRange.overlaps(maObjectRange);
+    bool bVisible = aViewRange.isEmpty() || aViewRange.overlaps(aObjectRange);
     if(!bVisible)
         return;
 