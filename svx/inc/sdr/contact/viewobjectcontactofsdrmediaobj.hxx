LemmingAvalanche/libreoffice-repaired

commit 10601d847a1efc34887439584a817ea1c1a2aa19

update video pos and size after change if currently playing
I'd prefer if it changed size during the resizing/repositioning, but
at least make it immediately take the final size after the
resize/reposition has happened.

Change-Id: Ic3b4dd23921ad5cf6092f1514dd6538f9946998a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130178
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

svx/inc/sdr/contact/viewobjectcontactofsdrmediaobj.hxx
@@ -51,7 +51,11 @@ namespace sdr::contact
             void    updateMediaItem( ::avmedia::MediaItem& rItem ) const;
             void    executeMediaItem( const ::avmedia::MediaItem& rItem );
 
+            virtual void ActionChanged() override;
+
         private:
+            void updateMediaWindow() const;
+
 #if HAVE_FEATURE_AVMEDIA
             std::unique_ptr<sdr::contact::SdrMediaWindow> mpMediaWindow;
 #endif

svx/source/sdr/contact/viewobjectcontactofsdrmediaobj.cxx
@@ -84,6 +84,43 @@ Size ViewObjectContactOfSdrMediaObj::getPreferredSize() const
     return aRet;
 }
 
+void ViewObjectContactOfSdrMediaObj::ActionChanged()
+{
+    ViewObjectContactOfSdrObj::ActionChanged();
+    if (mpMediaWindow && mpMediaWindow->isVisible())
+        updateMediaWindow();
+}
+
+void ViewObjectContactOfSdrMediaObj::updateMediaWindow() const
+{
+    basegfx::B2DRange aViewRange(getObjectRange());
+    aViewRange.transform(GetObjectContact().getViewInformation2D().getViewTransformation());
+
+    const tools::Rectangle aViewRectangle(
+        static_cast<sal_Int32>(floor(aViewRange.getMinX())), static_cast<sal_Int32>(floor(aViewRange.getMinY())),
+        static_cast<sal_Int32>(ceil(aViewRange.getMaxX())), static_cast<sal_Int32>(ceil(aViewRange.getMaxY())));
+
+    // mpMediaWindow contains a SalObject window and gtk won't accept
+    // the size until after the SalObject widget is shown but if we
+    // show it before setting a size then vcl will detect that the
+    // vcl::Window has no size and make it invisible instead. If we
+    // call setPosSize twice with the same size before and after show
+    // then the second attempt is a no-op as vcl caches the size.
+
+    // so call it initially with a size arbitrarily 1 pixel wider than
+    // we want so we have an initial size to make vcl happy
+    tools::Rectangle aInitialRect(aViewRectangle);
+    aInitialRect.AdjustRight(1);
+    mpMediaWindow->setPosSize(aInitialRect);
+
+    // then make it visible
+    mpMediaWindow->show();
+
+    // set the final desired size which is different to let vcl send it
+    // through to gtk which will now accept it as the underlying
+    // m_pSocket of GtkSalObject::SetPosSize is now visible
+    mpMediaWindow->setPosSize(aViewRectangle);
+}
 
 void ViewObjectContactOfSdrMediaObj::updateMediaItem( ::avmedia::MediaItem& rItem ) const
 {
@@ -100,33 +137,7 @@ void ViewObjectContactOfSdrMediaObj::updateMediaItem( ::avmedia::MediaItem& rIte
     }
     else
     {
-        basegfx::B2DRange aViewRange(getObjectRange());
-        aViewRange.transform(GetObjectContact().getViewInformation2D().getViewTransformation());
-
-        const tools::Rectangle aViewRectangle(
-            static_cast<sal_Int32>(floor(aViewRange.getMinX())), static_cast<sal_Int32>(floor(aViewRange.getMinY())),
-            static_cast<sal_Int32>(ceil(aViewRange.getMaxX())), static_cast<sal_Int32>(ceil(aViewRange.getMaxY())));
-
-        // mpMediaWindow contains a SalObject window and gtk won't accept
-        // the size until after the SalObject widget is shown but if we
-        // show it before setting a size then vcl will detect that the
-        // vcl::Window has no size and make it invisible instead. If we
-        // call setPosSize twice with the same size before and after show
-        // then the second attempt is a no-op as vcl caches the size.
-
-        // so call it initially with a size arbitrarily 1 pixel wider than
-        // we want so we have an initial size to make vcl happy
-        tools::Rectangle aInitialRect(aViewRectangle);
-        aInitialRect.AdjustRight(1);
-        mpMediaWindow->setPosSize(aInitialRect);
-
-        // then make it visible
-        mpMediaWindow->show();
-
-        // set the final desired size which is different to let vcl send it
-        // through to gtk which will now accept it as the underlying
-        // m_pSocket of GtkSalObject::SetPosSize is now visible
-        mpMediaWindow->setPosSize(aViewRectangle);
+        updateMediaWindow();
     }
 #else
     (void) rItem;

avmedia/source/viewer/mediawindow.cxx
@@ -142,30 +142,30 @@ bool MediaWindow::start()
     return mpImpl->start();
 }
 
-
 void MediaWindow::updateMediaItem( MediaItem& rItem ) const
 {
     mpImpl->updateMediaItem( rItem );
 }
-
 
 void MediaWindow::executeMediaItem( const MediaItem& rItem )
 {
     mpImpl->executeMediaItem( rItem );
 }
 
-
 void MediaWindow::show()
 {
     mpImpl->Show();
 }
-
 
 void MediaWindow::hide()
 {
     mpImpl->Hide();
 }
 
+bool MediaWindow::isVisible() const
+{
+    return mpImpl->IsVisible();
+}
 
 vcl::Window* MediaWindow::getWindow() const
 {

include/avmedia/mediawindow.hxx
@@ -74,6 +74,7 @@ namespace avmedia
 
         void                show();
         void                hide();
+        bool                isVisible() const;
 
     public:
 

commit 0c31cf559f5e6c6e793e6278a9e3c59664766233

Fix --disable-avmedia build
Regression from commit 10601d847a1efc34887439584a817ea1c1a2aa19
("update video pos and size after change if currently playing").

Change-Id: Ib618fcd7347255d0cae352b7fc90aa85a2c14d32
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130215
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

svx/inc/sdr/contact/viewobjectcontactofsdrmediaobj.hxx
@@ -54,7 +54,7 @@ namespace sdr::contact
             virtual void ActionChanged() override;
 
         private:
-            void updateMediaWindow() const;
+            void updateMediaWindow(bool bShow) const;
 
 #if HAVE_FEATURE_AVMEDIA
             std::unique_ptr<sdr::contact::SdrMediaWindow> mpMediaWindow;

svx/source/sdr/contact/viewobjectcontactofsdrmediaobj.cxx
@@ -87,12 +87,15 @@ Size ViewObjectContactOfSdrMediaObj::getPreferredSize() const
 void ViewObjectContactOfSdrMediaObj::ActionChanged()
 {
     ViewObjectContactOfSdrObj::ActionChanged();
-    if (mpMediaWindow && mpMediaWindow->isVisible())
-        updateMediaWindow();
+    updateMediaWindow(false);
 }
 
-void ViewObjectContactOfSdrMediaObj::updateMediaWindow() const
+void ViewObjectContactOfSdrMediaObj::updateMediaWindow(bool bShow) const
 {
+#if HAVE_FEATURE_AVMEDIA
+    if (!mpMediaWindow || (!bShow && !mpMediaWindow->isVisible()))
+        return;
+
     basegfx::B2DRange aViewRange(getObjectRange());
     aViewRange.transform(GetObjectContact().getViewInformation2D().getViewTransformation());
 
@@ -120,6 +123,9 @@ void ViewObjectContactOfSdrMediaObj::updateMediaWindow() const
     // through to gtk which will now accept it as the underlying
     // m_pSocket of GtkSalObject::SetPosSize is now visible
     mpMediaWindow->setPosSize(aViewRectangle);
+#else
+    (void) bShow;
+#endif
 }
 
 void ViewObjectContactOfSdrMediaObj::updateMediaItem( ::avmedia::MediaItem& rItem ) const
@@ -137,7 +143,7 @@ void ViewObjectContactOfSdrMediaObj::updateMediaItem( ::avmedia::MediaItem& rIte
     }
     else
     {
-        updateMediaWindow();
+        updateMediaWindow(true);
     }
 #else
     (void) rItem;