LemmingAvalanche/libreoffice-repaired

commit 0f3d457d2b12d01867f359c79f9408299f781b7d

tdf#112893: Pivot table: filtering of error item is not imported
... from XLSX

Change-Id: Ib155852e5f0718f41e02596d1042513286c6d845
Reviewed-on: https://gerrit.libreoffice.org/43166
Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>

sc/source/filter/inc/pivotcachebuffer.hxx
@@ -57,7 +57,7 @@ public:
     /** Reads the boolean value from a pivot cache item. */
     void                readBool( const AttributeList& rAttribs );
     /** Reads the error code value from a pivot cache item. */
-    void                readError( const AttributeList& rAttribs, const UnitConverter& rUnitConverter );
+    void                readError( const AttributeList& rAttribs );
     /** Reads the index of a shared item. */
     void                readIndex( const AttributeList& rAttribs );
 

sc/source/filter/oox/pivotcachebuffer.cxx
@@ -155,9 +155,9 @@ void PivotCacheItem::readBool( const AttributeList& rAttribs )
     mnType = XML_b;
 }
 
-void PivotCacheItem::readError( const AttributeList& rAttribs, const UnitConverter& rUnitConverter )
+void PivotCacheItem::readError( const AttributeList& rAttribs )
 {
-    maValue <<= static_cast< sal_Int32 >( rUnitConverter.calcBiffErrorCode( rAttribs.getXString( XML_v, OUString() ) ) );
+    maValue <<= rAttribs.getXString( XML_v, OUString() );
     mnType = XML_e;
 }
 
@@ -243,7 +243,7 @@ OUString PivotCacheItem::getFormattedName(const ScDPSaveDimension& rSaveDim, ScD
         case XML_i: return pObj->GetFormattedString(rSaveDim.GetName(), static_cast<double>(maValue.get< sal_Int32 >()));
         case XML_b: return pObj->GetFormattedString(rSaveDim.GetName(), static_cast<double>(maValue.get< bool >()));
         case XML_d: return pObj->GetFormattedString(rSaveDim.GetName(), maValue.get< css::util::DateTime >() - rNullDate);
-        case XML_e: return OUString();                                                              // !TODO
+        case XML_e: return maValue.get< OUString >();
     }
     OSL_FAIL( "PivotCacheItem::getFormattedName - invalid data type" );
     return OUString();
@@ -264,7 +264,7 @@ void PivotCacheItemList::importItem( sal_Int32 nElement, const AttributeList& rA
         case XLS_TOKEN( n ):    rItem.readNumeric( rAttribs );                      break;
         case XLS_TOKEN( d ):    rItem.readDate( rAttribs );                         break;
         case XLS_TOKEN( b ):    rItem.readBool( rAttribs );                         break;
-        case XLS_TOKEN( e ):    rItem.readError( rAttribs, getUnitConverter() );    break;
+        case XLS_TOKEN( e ):    rItem.readError( rAttribs );                        break;
         default:    OSL_FAIL( "PivotCacheItemList::importItem - unknown element type" );
     }
 }

sc/source/filter/oox/pivotcachefragment.cxx
@@ -243,7 +243,7 @@ ContextHandlerRef PivotCacheRecordsFragment::onCreateContext( sal_Int32 nElement
                 case XLS_TOKEN( n ):    aItem.readNumeric( rAttribs );                      break;
                 case XLS_TOKEN( d ):    aItem.readDate( rAttribs );                         break;
                 case XLS_TOKEN( b ):    aItem.readBool( rAttribs );                         break;
-                case XLS_TOKEN( e ):    aItem.readError( rAttribs, getUnitConverter() );    break;
+                case XLS_TOKEN( e ):    aItem.readError( rAttribs );                        break;
                 case XLS_TOKEN( x ):    aItem.readIndex( rAttribs );                        break;
                 default:    OSL_FAIL( "OoxPivotCacheRecordsFragment::onCreateContext - unexpected element" );
             }

sc/qa/unit/subsequent_export-test.cxx
@@ -225,6 +225,7 @@ public:
     void testPivotTableDateFieldFilterXLSX();
     void testPivotTableBoolFieldFilterXLSX();
     void testPivotTableRowColPageFieldFilterXLSX();
+    void testPivotTableErrorItemFilterXLSX();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -344,6 +345,7 @@ public:
     CPPUNIT_TEST(testPivotTableDateFieldFilterXLSX);
     CPPUNIT_TEST(testPivotTableBoolFieldFilterXLSX);
     CPPUNIT_TEST(testPivotTableRowColPageFieldFilterXLSX);
+    CPPUNIT_TEST(testPivotTableErrorItemFilterXLSX);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -5215,6 +5217,36 @@ void ScExportTest::testPivotTableRowColPageFieldFilterXLSX()
         CPPUNIT_ASSERT(pMember);
         CPPUNIT_ASSERT(pMember->HasIsVisible() && pMember->GetIsVisible());
     }*/
+
+    xDocSh->DoClose();
+}
+
+void ScExportTest::testPivotTableErrorItemFilterXLSX()
+{
+    ScDocShellRef xDocSh = loadDoc("pivottable_error_item_filter.", FORMAT_XLSX);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load file", xDocSh.is());
+    ScDocument& rDoc = xDocSh->GetDocument();
+    ScDPCollection* pDPs = rDoc.GetDPCollection();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDPs->GetCount());
+
+    // Reload and check whether filtering is preserved
+    xDocSh = saveAndReload( &(*xDocSh), FORMAT_XLSX);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load file", xDocSh.is());
+    ScDocument& rLoadedDoc = xDocSh->GetDocument();
+    pDPs = rLoadedDoc.GetDPCollection();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDPs->GetCount());
+    const ScDPObject* pDPObj = &(*pDPs)[0];
+    CPPUNIT_ASSERT(pDPObj);
+    ScDPSaveData* pSaveData = pDPObj->GetSaveData();
+    CPPUNIT_ASSERT(pSaveData);
+
+    ScDPSaveDimension* pSaveDim = pSaveData->GetExistingDimensionByName("b");
+    CPPUNIT_ASSERT(pSaveDim);
+    const ScDPSaveDimension::MemberList& rMembers = pSaveDim->GetMembers();
+    CPPUNIT_ASSERT_EQUAL(size_t(4), rMembers.size());
+    ScDPSaveMember* pMember = pSaveDim->GetExistingMemberByName("#DIV/0!");
+    CPPUNIT_ASSERT(pMember);
+    CPPUNIT_ASSERT(pMember->HasIsVisible() && !pMember->GetIsVisible());
 
     xDocSh->DoClose();
 }

sc/source/filter/excel/xepivotxml.cxx
@@ -290,7 +290,8 @@ void XclExpXmlPivotCaches::SavePivotCacheXml( XclExpXmlStream& rStrm, const Entr
         std::set<ScDPItemData::Type> aDPTypesWithoutBlank = aDPTypes;
         aDPTypesWithoutBlank.erase(ScDPItemData::Empty);
 
-        bool isContainsString = aDPTypesWithoutBlank.find(ScDPItemData::String) != aDPTypesWithoutBlank.end();
+        bool isContainsString = aDPTypesWithoutBlank.find(ScDPItemData::String) != aDPTypesWithoutBlank.end() ||
+                                aDPTypesWithoutBlank.find(ScDPItemData::Error) != aDPTypesWithoutBlank.end();
         bool isContainsBlank = aDPTypes.find(ScDPItemData::Empty) != aDPTypeEnd;
         bool isContainsNumber = !isContainsDate && aDPTypesWithoutBlank.find(ScDPItemData::Value) != aDPTypesWithoutBlank.end();
         bool isContainsNonDate = !(isContainsDate && aDPTypesWithoutBlank.size() <= 1);

commit c06433fa2523e586e2eaae039cd6dd2c884d3e5c

tdf#122471 xlsx import: pivottable error OUString, not uInt8
This fixes a LO 6.0 regression from
commit 0f3d457d2b12d01867f359c79f9408299f781b7d
which only half-way changed to OUString from uInt8.

An exception was raised because in XLSX, an INT was written
while the corresponding read function was expecting an OUString.

However, doing this runs into problems with binary files (xlsb),
which were still setting the value to an int.
Unit test shows the need to use OUString for xlsb too,
which now matches what I see in Excel 2003.

make CppunitTest_sc_pivottable_filters_test \
  CPPUNIT_TEST_NAME=testPivotTableErrorItem2FilterXLSX

Change-Id: I399c9e34984bb1ff71695a87aa56f53063d37b3d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130714
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sc/source/filter/inc/pivotcachebuffer.hxx
@@ -71,7 +71,7 @@ public:
     /** Reads the boolean value from a pivot cache item. */
     void                readBool( SequenceInputStream& rStrm );
     /** Reads the error code value from a pivot cache item. */
-    void                readError( SequenceInputStream& rStrm );
+    void                readError(SequenceInputStream& rStrm, const UnitConverter& rUnitConverter);
     /** Reads the index of a shared item. */
     void                readIndex( SequenceInputStream& rStrm );
 

sc/source/filter/oox/pivotcachebuffer.cxx
@@ -202,9 +202,9 @@ void PivotCacheItem::readBool( SequenceInputStream& rStrm )
     mnType = XML_b;
 }
 
-void PivotCacheItem::readError( SequenceInputStream& rStrm )
+void PivotCacheItem::readError(SequenceInputStream& rStrm, const UnitConverter& rUnitConverter)
 {
-    maValue <<= static_cast< sal_Int32 >( rStrm.readuInt8() );
+    maValue <<= rUnitConverter.calcErrorString(rStrm.readuInt8());
     mnType = XML_e;
 }
 
@@ -294,7 +294,7 @@ void PivotCacheItemList::importItem( sal_Int32 nRecId, SequenceInputStream& rStr
         case BIFF12_ID_PCITEM_BOOL:
         case BIFF12_ID_PCITEMA_BOOL:    rItem.readBool( rStrm );    break;
         case BIFF12_ID_PCITEM_ERROR:
-        case BIFF12_ID_PCITEMA_ERROR:   rItem.readError( rStrm );   break;
+        case BIFF12_ID_PCITEMA_ERROR: rItem.readError(rStrm, getUnitConverter()); break;
         default:    OSL_FAIL( "PivotCacheItemList::importItem - unknown record type" );
     }
 }
@@ -339,7 +339,7 @@ void PivotCacheItemList::importArray( SequenceInputStream& rStrm )
         {
             case BIFF12_PCITEM_ARRAY_DOUBLE: createItem().readDouble( rStrm );   break;
             case BIFF12_PCITEM_ARRAY_STRING: createItem().readString( rStrm );   break;
-            case BIFF12_PCITEM_ARRAY_ERROR:  createItem().readError( rStrm );    break;
+            case BIFF12_PCITEM_ARRAY_ERROR: createItem().readError(rStrm, getUnitConverter()); break;
             case BIFF12_PCITEM_ARRAY_DATE:   createItem().readDate( rStrm );     break;
             default:
                 OSL_FAIL( "PivotCacheItemList::importArray - unknown data type" );
@@ -831,7 +831,7 @@ void PivotCacheField::writeItemToSourceDataCell( const WorksheetHelper& rSheetHe
         case XML_i: rSheetData.setValueCell( aModel, rItem.getValue().get< sal_Int16 >() );                             break;
         case XML_d: rSheetData.setDateTimeCell( aModel, rItem.getValue().get< css::util::DateTime >() );                           break;
         case XML_b: rSheetData.setBooleanCell( aModel, rItem.getValue().get< bool >() );                                break;
-        case XML_e: rSheetData.setErrorCell( aModel, static_cast< sal_uInt8 >( rItem.getValue().get< sal_Int32 >() ) ); break;
+        case XML_e: rSheetData.setErrorCell(aModel, rItem.getValue().get<OUString>()); break;
         default:    OSL_FAIL( "PivotCacheField::writeItemToSourceDataCell - unexpected item data type" );
     }
 }

sc/source/filter/oox/pivotcachefragment.cxx
@@ -311,7 +311,7 @@ void PivotCacheRecordsFragment::importPCRecordItem( sal_Int32 nRecId, SequenceIn
         case BIFF12_ID_PCITEM_DOUBLE:   aItem.readDouble( rStrm );  break;
         case BIFF12_ID_PCITEM_DATE:     aItem.readDate( rStrm );    break;
         case BIFF12_ID_PCITEM_BOOL:     aItem.readBool( rStrm );    break;
-        case BIFF12_ID_PCITEM_ERROR:    aItem.readError( rStrm );   break;
+        case BIFF12_ID_PCITEM_ERROR:    aItem.readError( rStrm, getUnitConverter() ); break;
         case BIFF12_ID_PCITEM_INDEX:    aItem.readIndex( rStrm );   break;
         default:    OSL_FAIL( "OoxPivotCacheRecordsFragment::importPCRecordItem - unexpected record" );
     }

sc/qa/unit/pivottable_filters_test.cxx
@@ -79,6 +79,8 @@ public:
     void testPivotTableBoolFieldFilterXLSX();
     void testPivotTableRowColPageFieldFilterXLSX();
     void testPivotTableErrorItemFilterXLSX();
+    void testPivotTableErrorItemFilterXLSB();
+    void testPivotTableErrorItem2FilterXLSX();
     void testPivotTableOutlineModeXLSX();
     void testPivotTableDuplicatedMemberFilterXLSX();
     void testPivotTableTabularModeXLSX();
@@ -130,6 +132,8 @@ public:
     CPPUNIT_TEST(testPivotTableBoolFieldFilterXLSX);
     CPPUNIT_TEST(testPivotTableRowColPageFieldFilterXLSX);
     CPPUNIT_TEST(testPivotTableErrorItemFilterXLSX);
+    CPPUNIT_TEST(testPivotTableErrorItemFilterXLSB);
+    CPPUNIT_TEST(testPivotTableErrorItem2FilterXLSX);
     CPPUNIT_TEST(testPivotTableOutlineModeXLSX);
     CPPUNIT_TEST(testPivotTableDuplicatedMemberFilterXLSX);
     CPPUNIT_TEST(testPivotTableTabularModeXLSX);
@@ -2361,6 +2365,58 @@ void ScPivotTableFiltersTest::testPivotTableErrorItemFilterXLSX()
     CPPUNIT_ASSERT(pMember);
     CPPUNIT_ASSERT(pMember->HasIsVisible());
     CPPUNIT_ASSERT(!pMember->GetIsVisible());
+
+    xDocSh->DoClose();
+}
+
+void ScPivotTableFiltersTest::testPivotTableErrorItemFilterXLSB()
+{
+    ScDocShellRef xDocSh = loadDoc(u"pivottable_error_item_filter.", FORMAT_XLSB);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load file", xDocSh.is());
+    ScDocument& rDoc = xDocSh->GetDocument();
+    ScDPCollection* pDPs = rDoc.GetDPCollection();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDPs->GetCount());
+    const ScDPObject* pDPObj = &(*pDPs)[0];
+    CPPUNIT_ASSERT(pDPObj);
+    ScDPSaveData* pSaveData = pDPObj->GetSaveData();
+    CPPUNIT_ASSERT(pSaveData);
+
+    ScDPSaveDimension* pSaveDim = pSaveData->GetExistingDimensionByName(u"b");
+    CPPUNIT_ASSERT(pSaveDim);
+    const ScDPSaveDimension::MemberList& rMembers = pSaveDim->GetMembers();
+    CPPUNIT_ASSERT_EQUAL(size_t(4), rMembers.size());
+    ScDPSaveMember* pMember = pSaveDim->GetExistingMemberByName("#DIV/0!");
+    CPPUNIT_ASSERT(pMember);
+    CPPUNIT_ASSERT(pMember->HasIsVisible());
+    CPPUNIT_ASSERT(!pMember->GetIsVisible());
+
+    xDocSh->DoClose();
+}
+
+void ScPivotTableFiltersTest::testPivotTableErrorItem2FilterXLSX()
+{
+    ScDocShellRef xDocSh = loadDoc(u"tdf122471.", FORMAT_XLSX);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load file", xDocSh.is());
+    ScDocument& rDoc = xDocSh->GetDocument();
+    ScDPCollection* pDPs = rDoc.GetDPCollection();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDPs->GetCount());
+
+    // Reload and check whether filtering is preserved
+    xDocSh = saveAndReload(*xDocSh, FORMAT_XLSX);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load file", xDocSh.is());
+    ScDocument& rLoadedDoc = xDocSh->GetDocument();
+    pDPs = rLoadedDoc.GetDPCollection();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDPs->GetCount());
+    const ScDPObject* pDPObj = &(*pDPs)[0];
+    CPPUNIT_ASSERT(pDPObj);
+    ScDPSaveData* pSaveData = pDPObj->GetSaveData();
+    CPPUNIT_ASSERT(pSaveData);
+
+    ScDPSaveDimension* pSaveDim = pSaveData->GetExistingDimensionByName(u"PPP");
+    CPPUNIT_ASSERT(pSaveDim);
+    const ScDPSaveDimension::MemberList& rMembers = pSaveDim->GetMembers();
+    // prior to the patch, columns were missing due to an exception dropping the column data
+    CPPUNIT_ASSERT_EQUAL(size_t(21), rMembers.size());
 
     xDocSh->DoClose();
 }