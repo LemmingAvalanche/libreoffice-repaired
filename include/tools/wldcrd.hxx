LemmingAvalanche/libreoffice-repaired

commit f6f35f673a2186891919e7a239bf79d624118b94

simplify and improve Wildcard
it is faster to just process OUString data, rather than perform
expensive conversion to OString and back again.

Change-Id: Ie007b872ee507ac5c6e8b55cc0a49ef3ac1f0139
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117608
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/tools/wldcrd.hxx
@@ -30,10 +30,10 @@
 class SAL_WARN_UNUSED TOOLS_DLLPUBLIC WildCard
 {
 private:
-    OString aWildString;
+    OUString aWildString;
     char cSepSymbol;
 
-    static bool ImpMatch( const char *pWild, const char *pStr );
+    static bool ImpMatch( const sal_Unicode *pWild, const sal_Unicode *pStr );
 
 public:
     WildCard()
@@ -43,19 +43,19 @@ public:
     }
 
     WildCard(std::u16string_view rWildCard, const char cSeparator = '\0')
-        : aWildString(OUStringToOString(rWildCard, osl_getThreadTextEncoding()))
+        : aWildString(rWildCard)
         , cSepSymbol(cSeparator)
     {
     }
 
     OUString getGlob() const
     {
-        return OStringToOUString(aWildString, osl_getThreadTextEncoding());
+        return aWildString;
     }
 
     void setGlob(std::u16string_view rString)
     {
-        aWildString = OUStringToOString(rString, osl_getThreadTextEncoding());
+        aWildString = rString;
     }
 
     bool Matches( std::u16string_view rStr ) const;

tools/source/fsys/wldcrd.cxx
@@ -25,7 +25,7 @@
  * '?' in pWild mean match exactly one character.
  *
  */
-bool WildCard::ImpMatch( const char *pWild, const char *pStr )
+bool WildCard::ImpMatch( const sal_Unicode *pWild, const sal_Unicode *pStr )
 {
     int    pos=0;
     int    flag=0;
@@ -88,8 +88,7 @@ bool WildCard::ImpMatch( const char *pWild, const char *pStr )
 
 bool WildCard::Matches( std::u16string_view rString ) const
 {
-    OString aTmpWild = aWildString;
-    OString aString(OUStringToOString(rString, osl_getThreadTextEncoding()));
+    OUString aTmpWild = aWildString;
 
     sal_Int32 nSepPos;
 
@@ -98,13 +97,13 @@ bool WildCard::Matches( std::u16string_view rString ) const
         while ( (nSepPos = aTmpWild.indexOf(cSepSymbol)) != -1 )
         {
             // Check all split wildcards
-            if ( ImpMatch( aTmpWild.copy( 0, nSepPos ).getStr(), aString.getStr() ) )
+            if ( ImpMatch( aTmpWild.subView( 0, nSepPos ).data(), rString.data() ) )
                 return true;
             aTmpWild = aTmpWild.copy(nSepPos + 1); // remove separator
         }
     }
 
-    return ImpMatch( aTmpWild.getStr(), aString.getStr() );
+    return ImpMatch( aTmpWild.getStr(), rString.data() );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit e43b3d4aaff061c798c3c1fe1a6038ec37266e9a

tdf#147921 Filesave DOC: wrong layout and then all missing from 7.3
regression from
    commit f6f35f673a2186891919e7a239bf79d624118b94
    Author: Noel Grandin <noel@peralex.com>
    Date:   Mon Jun 21 13:00:07 2021 +0200
    simplify and improve Wildcard

Cannot pass a string_view into something expecting a char* because
then it gets the length wrong.

Change-Id: I638660a1e9a8a0d17e4d2f77500b3f01365a43d3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131396
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/tools/wldcrd.hxx
@@ -32,7 +32,7 @@ private:
     OUString aWildString;
     char cSepSymbol;
 
-    static bool ImpMatch( const sal_Unicode *pWild, const sal_Unicode *pStr );
+    static bool ImpMatch( std::u16string_view aWild, std::u16string_view aStr );
 
 public:
     WildCard()

tools/source/fsys/wldcrd.cxx
@@ -25,22 +25,26 @@
  * '?' in pWild mean match exactly one character.
  *
  */
-bool WildCard::ImpMatch( const sal_Unicode *pWild, const sal_Unicode *pStr )
+bool WildCard::ImpMatch( std::u16string_view aWild, std::u16string_view aStr )
 {
     int    pos=0;
     int    flag=0;
+    const sal_Unicode* pWild = aWild.data();
+    const sal_Unicode* pWildEnd = aWild.data() + aWild.size();
+    const sal_Unicode* pStr = aStr.data();
+    const sal_Unicode* pStrEnd = aStr.data() + aStr.size();
 
-    while ( *pWild || flag )
+    while ( pWild != pWildEnd || flag )
     {
         switch (*pWild)
         {
             case '?':
-                if ( *pStr == '\0' )
+                if ( pStr == pStrEnd )
                     return false;
                 break;
 
             default:
-                if ( (*pWild == '\\') && ((*(pWild+1)=='?') || (*(pWild+1) == '*')) )
+                if ( (*pWild == '\\') && (pWild + 1 != pWildEnd) && ((*(pWild+1)=='?') || (*(pWild+1) == '*')) )
                     pWild++;
                 if ( *pWild != *pStr )
                     if ( !pos )
@@ -53,37 +57,37 @@ bool WildCard::ImpMatch( const sal_Unicode *pWild, const sal_Unicode *pStr )
                 // circumstances!
                 [[fallthrough]];
             case '*':
-                while ( *pWild == '*' )
+                while ( pWild != pWildEnd && *pWild == '*' )
                     pWild++;
-                if ( *pWild == '\0' )
+                if ( pWild == pWildEnd )
                     return true;
                 flag = 1;
                 pos  = 0;
-                if ( *pStr == '\0' )
-                    return ( *pWild == '\0' );
-                while ( *pStr && *pStr != *pWild )
+                if ( pStr == pStrEnd )
+                    return false;
+                while ( pStr != pStrEnd && *pStr != *pWild )
                 {
                     if ( *pWild == '?' ) {
                         pWild++;
-                        while ( *pWild == '*' )
+                        while ( pWild != pWildEnd && *pWild == '*' )
                             pWild++;
                     }
                     pStr++;
-                    if ( *pStr == '\0' )
-                        return ( *pWild == '\0' );
+                    if ( pStr == pStrEnd )
+                        return pWild == pWildEnd;
                 }
                 break;
         }
-        if ( *pWild != '\0' )
+        if ( pWild != pWildEnd )
             pWild++;
-        if ( *pStr != '\0' )
+        if ( pStr != pStrEnd )
             pStr++;
         else
             flag = 0;
         if ( flag )
             pos--;
     }
-    return ( *pStr == '\0' ) && ( *pWild == '\0' );
+    return ( pStr == pStrEnd ) && ( pWild == pWildEnd );
 }
 
 bool WildCard::Matches( std::u16string_view rString ) const
@@ -97,13 +101,13 @@ bool WildCard::Matches( std::u16string_view rString ) const
         while ( (nSepPos = aTmpWild.indexOf(cSepSymbol)) != -1 )
         {
             // Check all split wildcards
-            if ( ImpMatch( aTmpWild.subView( 0, nSepPos ).data(), rString.data() ) )
+            if ( ImpMatch( aTmpWild.subView( 0, nSepPos ), rString ) )
                 return true;
             aTmpWild = aTmpWild.copy(nSepPos + 1); // remove separator
         }
     }
 
-    return ImpMatch( aTmpWild.getStr(), rString.data() );
+    return ImpMatch( aTmpWild, rString );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */