LemmingAvalanche/libreoffice-repaired

commit 50768296a576a8cae07e7e4eb14b0e0598982ec1

tdf#128375 sw: fix copying RES_PARATR_LIST_AUTOFMT to different SwDoc
When copying the item to a different SwDoc, the items in the nested
SfxItemSet must be copied into the target SwDoc's item pool, or a
use-after-free is inevitable.

(regression from eb1f7250f2274b13d44da845ee44c52ff479202a)

Change-Id: I7071ff6dccf2285d96f540ac556b9fa2265ddf00
Reviewed-on: https://gerrit.libreoffice.org/81911
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/attr/swatrset.cxx
@@ -26,6 +26,7 @@
 #include <editeng/editeng.hxx>
 #include <fmtanchr.hxx>
 #include <fmtpdsc.hxx>
+#include <fmtautofmt.hxx>
 #include <hintids.hxx>
 #include <list.hxx>
 #include <node.hxx>
@@ -393,6 +394,22 @@ void SwAttrSet::CopyToModify( SwModify& rMod ) const
                 // Anchors at any node position cannot be copied to another document, because the SwPosition
                 // would still point to the old document. It needs to be fixed up explicitly.
                 tmpSet->ClearItem( RES_ANCHOR );
+            }
+
+            if (pSrcDoc != pDstDoc &&
+                SfxItemState::SET == GetItemState(RES_PARATR_LIST_AUTOFMT, false, &pItem))
+            {
+                SfxItemSet const& rAutoStyle(*static_cast<SwFormatAutoFormat const&>(*pItem).GetStyleHandle());
+                std::shared_ptr<SfxItemSet> const pNewSet(
+                    rAutoStyle.SfxItemSet::Clone(true, &pDstDoc->GetAttrPool()));
+                SwFormatAutoFormat item(RES_PARATR_LIST_AUTOFMT);
+                // TODO: for ODF export we'd need to add it to the autostyle pool
+                item.SetStyleHandle(pNewSet);
+                if (!tmpSet)
+                {
+                    tmpSet.reset(new SfxItemSet(*this));
+                }
+                tmpSet->Put(item);
             }
 
             if( tmpSet )

commit 1d2ab6241a3e8cca275ab917352b29d13f279f15

sw: fix crash in SwAttrSet::CopyToModify()
Regression from commit 50768296a576a8cae07e7e4eb14b0e0598982ec1
(tdf#128375 sw: fix copying RES_PARATR_LIST_AUTOFMT to different SwDoc,
2019-11-01), the problem is that we may get an autofmt pool item which
is set, but its style handle is empty. Assume that this is the same case
as having no autofmt at all.

Change-Id: I87494fd04687d31201b4ec712cb0fb1ec7362b46
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132301
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/CppunitTest_sw_core_attr.mk
@@ -1,0 +1,75 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_core_attr))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sw_core_attr))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_core_attr, \
+    sw/qa/core/attr/attr \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_core_attr, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    editeng \
+    sal \
+    sfx \
+    svl \
+    svx \
+    svxcore \
+    sw \
+	swqahelper \
+    test \
+    unotest \
+    utl \
+    vcl \
+    tl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_core_attr,\
+    boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_core_attr,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
+    -I$(SRCDIR)/sw/qa/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_core_attr,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_core_attr))
+$(eval $(call gb_CppunitTest_use_vcl,sw_core_attr))
+
+$(eval $(call gb_CppunitTest_use_rdb,sw_core_attr,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,sw_core_attr,\
+    officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_core_attr))
+
+$(eval $(call gb_CppunitTest_use_uiconfigs,sw_core_attr, \
+    modules/swriter \
+))
+
+$(eval $(call gb_CppunitTest_use_more_fonts,sw_core_attr))
+
+# vim: set noet sw=4 ts=4:

sw/Module_sw.mk
@@ -145,6 +145,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_core_edit \
     CppunitTest_sw_uibase_fldui \
     CppunitTest_sw_core_view \
+    CppunitTest_sw_core_attr \
 ))
 
 ifneq ($(DISABLE_GUI),TRUE)

sw/source/core/attr/swatrset.cxx
@@ -364,7 +364,8 @@ void SwAttrSet::CopyToModify( sw::BroadcastingModify& rMod ) const
 
             const SwFormatAutoFormat* pAutoFormatItem;
             if (pSrcDoc != pDstDoc &&
-                (pAutoFormatItem = GetItemIfSet(RES_PARATR_LIST_AUTOFMT, false)))
+                (pAutoFormatItem = GetItemIfSet(RES_PARATR_LIST_AUTOFMT, false)) &&
+                pAutoFormatItem->GetStyleHandle())
             {
                 SfxItemSet const& rAutoStyle(*pAutoFormatItem->GetStyleHandle());
                 std::shared_ptr<SfxItemSet> const pNewSet(

sw/qa/core/attr/attr.cxx
@@ -1,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <docsh.hxx>
+#include <swdtflvr.hxx>
+#include <swmodule.hxx>
+
+namespace
+{
+constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/core/attr/data/";
+
+/// Covers sw/source/core/attr/ fixes.
+class Test : public SwModelTestBase
+{
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testSwAttrSet)
+{
+    // Given a document with track changes and the whole document is selected:
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "attr-set.docx");
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:Cut", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    rtl::Reference<SwTransferable> xTransferable(new SwTransferable(*pWrtShell));
+    SwModule* pMod = SW_MOD();
+    SwTransferable* pOldTransferable = pMod->m_pXSelection;
+    pMod->m_pXSelection = xTransferable.get();
+
+    // When getting the plain text version of the selection:
+    datatransfer::DataFlavor aFlavor;
+    aFlavor.MimeType = "text/plain;charset=utf-16";
+    aFlavor.DataType = cppu::UnoType<OUString>::get();
+    uno::Any aData = xTransferable->getTransferData(aFlavor);
+
+    // Then make sure we get data without crashing:
+    CPPUNIT_ASSERT(aData.hasValue());
+    pMod->m_pXSelection = pOldTransferable;
+}
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */