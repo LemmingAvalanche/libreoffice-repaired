LemmingAvalanche/libreoffice-repaired

commit 5f1a3f0350c24de1b0c03c6387e51b6aa5a6f229

tdf#136368 bump to libnumbertext 1.0.8
– Soros.py: fix FutureWarning: Possible nested set at position, reported by Rene Engelhard

– tdf#136368 fix Old Hungarian transliteration

– fixes for Chinese, French, Marathi, Polish, Ukrainian

Change-Id: I99f56b8a6089bc94f43056bcdedcae89ff4e68c9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127821
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

download.lst
@@ -156,8 +156,8 @@ export LIBGPGERROR_SHA256SUM := b32d6ff72a73cf79797f7f2d039e95e9c6f92f0c14502154
 export LIBGPGERROR_TARBALL := libgpg-error-1.37.tar.bz2
 export LIBLANGTAG_SHA256SUM := 1f12a20a02ec3a8d22e54dedb8b683a43c9c160bda1ba337bf1060607ae733bd
 export LIBLANGTAG_TARBALL := liblangtag-0.6.3.tar.bz2
-export LIBNUMBERTEXT_SHA256SUM := 17b8249cb89ae11ae15a85612d2665626c0e0e3e56b35654363ba6566d8b61fc
-export LIBNUMBERTEXT_TARBALL := libnumbertext-1.0.7.tar.xz
+export LIBNUMBERTEXT_SHA256SUM := db9060d208501bd7bc06300a55d8489d29dd560ee0fbbd0f41b78af56816680c
+export LIBNUMBERTEXT_TARBALL := libnumbertext-1.0.8.tar.xz
 export LIBTOMMATH_SHA256SUM := 083daa92d8ee6f4af96a6143b12d7fc8fe1a547e14f862304f7281f8f7347483
 export LIBTOMMATH_TARBALL := ltm-1.0.zip
 export XMLSEC_SHA256SUM := 26041d35a20a245ed5a2fb9ee075f10825664d274220cb5190340fa87a4d0931

commit 0e0a556d093b993b7b356865b79d6294472fde39

tdf#147546 bump libnumbertext to 1.0.10
fixing only regression of hu_Hung
transliteration of punctuation marks.
Add unit test for the fix.

Regression from commit 5f1a3f0350c24de1b0c03c6387e51b6aa5a6f229
"tdf#136368 bump to libnumbertext 1.0.8".

Change-Id: I7b49467943c97582dba0e5aca20c02a92c43deff
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132395
Tested-by: Jenkins
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

download.lst
@@ -158,8 +158,8 @@ export LIBGPGERROR_SHA256SUM := a9ab83ca7acc442a5bd846a75b920285ff79bdb4e3d34aa3
 export LIBGPGERROR_TARBALL := libgpg-error-1.43.tar.bz2
 export LIBLANGTAG_SHA256SUM := 1f12a20a02ec3a8d22e54dedb8b683a43c9c160bda1ba337bf1060607ae733bd
 export LIBLANGTAG_TARBALL := liblangtag-0.6.3.tar.bz2
-export LIBNUMBERTEXT_SHA256SUM := db9060d208501bd7bc06300a55d8489d29dd560ee0fbbd0f41b78af56816680c
-export LIBNUMBERTEXT_TARBALL := libnumbertext-1.0.8.tar.xz
+export LIBNUMBERTEXT_SHA256SUM := a285573864eaac8d36a0f66d946e9b1d3cf01c5d93d31fda00264a76f2633beb
+export LIBNUMBERTEXT_TARBALL := libnumbertext-1.0.10.tar.xz
 export LIBTOMMATH_SHA256SUM := 083daa92d8ee6f4af96a6143b12d7fc8fe1a547e14f862304f7281f8f7347483
 export LIBTOMMATH_TARBALL := ltm-1.0.zip
 export LIBWEBP_SHA256SUM := 808b98d2f5b84e9b27fdef6c5372dac769c3bda4502febbfa5031bd3c4d7d018

sw/qa/extras/uiwriter/uiwriter6.cxx
@@ -1732,6 +1732,41 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf133589)
     pWrtShell->AutoCorrect(corr, ' ');
     sReplaced += u"𐳺𐳺𐳿𐳼𐳼 ";
     CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+
+    // tdf#147546 transliterate punctuation marks
+
+    // question mark
+    pWrtShell->Insert(u"Kérdőjel");
+    pWrtShell->AutoCorrect(corr, '?');
+    sReplaced += u"𐲓𐳋𐳢𐳇𐳟𐳒𐳉𐳖";
+    OUString sReplaced2(sReplaced + "?");
+    CPPUNIT_ASSERT_EQUAL(sReplaced2, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+    pWrtShell->AutoCorrect(corr, ' ');
+    sReplaced += u"⸮ ";
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+    // comma
+    pWrtShell->Insert(u"Vessző");
+    pWrtShell->AutoCorrect(corr, ',');
+    sReplaced += u"𐲮𐳉𐳥𐳥𐳟";
+    sReplaced2 = sReplaced + ",";
+    CPPUNIT_ASSERT_EQUAL(sReplaced2, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+    pWrtShell->AutoCorrect(corr, ' ');
+    sReplaced += u"⹁ ";
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+    // semicolon
+    pWrtShell->Insert(u"pontosvessző");
+    pWrtShell->AutoCorrect(corr, ';');
+    sReplaced += u"𐳠𐳛𐳙𐳦𐳛𐳤𐳮𐳉𐳥𐳥𐳟";
+    sReplaced2 = sReplaced + ";";
+    CPPUNIT_ASSERT_EQUAL(sReplaced2, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+    pWrtShell->AutoCorrect(corr, ' ');
+    sReplaced += u"⁏ ";
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
+    // quotation marks
+    pWrtShell->Insert(u"„idézőjel”");
+    pWrtShell->AutoCorrect(corr, ' ');
+    sReplaced += u"⹂𐳐𐳇𐳋𐳯𐳟𐳒𐳉𐳖‟ ";
+    CPPUNIT_ASSERT_EQUAL(sReplaced, static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex])->GetText());
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf143176)