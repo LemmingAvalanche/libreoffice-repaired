LemmingAvalanche/libreoffice-repaired

commit 4805b2d4b321dabb1245956afc4347fb57378232

tdf#145954 Qt unshare QMenubar usage
The Qt code was sharing the menu bar from the top level frame, but
LO expects independent menu bars per SetFrame calls. So instead of
showing the new bar and then hiding the old one, this was always
show and hiding the same menu bar, resulting in a hidden menu bar.

As a result of unsharing, LO now must check that its menu bar
pointer is still valid for usage. The QMainWindows takes ownership
when a QMenuBar is assigned and destroy old ones.

Change-Id: I2c6b12199a1e17a5d9f88686a4b27b1413beda47
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132581
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/qt5/QtMenu.cxx
@@ -59,7 +59,7 @@ void QtMenu::InsertMenuItem(QtMenuItem* pSalMenuItem, unsigned nPos)
     if (mbMenuBar)
     {
         // top-level menu
-        if (mpQMenuBar)
+        if (validateQMenuBar())
         {
             QMenu* pQMenu = new QMenu(toQString(aText), nullptr);
             pSalMenuItem->mpMenu.reset(pQMenu);
@@ -428,20 +428,10 @@ void QtMenu::SetFrame(const SalFrame* pFrame)
     if (!pMainWindow)
         return;
 
-    mpQMenuBar = pMainWindow->menuBar();
-    if (mpQMenuBar)
-    {
-        mpQMenuBar->clear();
-        QPushButton* pButton
-            = static_cast<QPushButton*>(mpQMenuBar->cornerWidget(Qt::TopRightCorner));
-        if (pButton && ((mpCloseButton != pButton) || !maCloseButtonConnection))
-        {
-            maCloseButtonConnection
-                = connect(pButton, &QPushButton::clicked, this, &QtMenu::slotCloseDocument);
-            mpCloseButton = pButton;
-        }
-    }
+    mpQMenuBar = new QMenuBar();
+    pMainWindow->setMenuBar(mpQMenuBar);
 
+    mpCloseButton = nullptr;
     mpQMenu = nullptr;
 
     DoFullMenuUpdate(mpVCLMenu);
@@ -565,9 +555,22 @@ QtMenu* QtMenu::GetTopLevel()
     return pMenu;
 }
 
+bool QtMenu::validateQMenuBar()
+{
+    if (!mpQMenuBar)
+        return false;
+    assert(mpFrame);
+    QtMainWindow* pMainWindow = mpFrame->GetTopLevelWindow();
+    assert(pMainWindow);
+    const bool bValid = mpQMenuBar == pMainWindow->menuBar();
+    if (!bValid)
+        mpQMenuBar = nullptr;
+    return bValid;
+}
+
 void QtMenu::ShowMenuBar(bool bVisible)
 {
-    if (mpQMenuBar)
+    if (validateQMenuBar())
         mpQMenuBar->setVisible(bVisible);
 }
 
@@ -643,7 +646,7 @@ void QtMenu::slotCloseDocument()
 
 void QtMenu::ShowCloseButton(bool bShow)
 {
-    if (!mpQMenuBar)
+    if (!validateQMenuBar())
         return;
 
     QPushButton* pButton = static_cast<QPushButton*>(mpQMenuBar->cornerWidget(Qt::TopRightCorner));
@@ -661,8 +664,7 @@ void QtMenu::ShowCloseButton(bool bShow)
         pButton->setFocusPolicy(Qt::NoFocus);
         pButton->setToolTip(toQString(VclResId(SV_HELPTEXT_CLOSEDOCUMENT)));
         mpQMenuBar->setCornerWidget(pButton, Qt::TopRightCorner);
-        maCloseButtonConnection
-            = connect(pButton, &QPushButton::clicked, this, &QtMenu::slotCloseDocument);
+        connect(pButton, &QPushButton::clicked, this, &QtMenu::slotCloseDocument);
         mpCloseButton = pButton;
     }
 

vcl/inc/qt5/QtMenu.hxx
@@ -50,7 +50,6 @@ private:
     // pointer to QMenu owned by the corresponding QtMenuItem or self (-> mpOwnedQMenu)
     QMenu* mpQMenu;
     QPushButton* mpCloseButton;
-    QMetaObject::Connection maCloseButtonConnection;
 
     void DoFullMenuUpdate(Menu* pMenuBar);
     static void NativeItemText(OUString& rItemText);
@@ -60,6 +59,7 @@ private:
     void ReinitializeActionGroup(unsigned nPos);
     void ResetAllActionGroups();
     void UpdateActionGroupItem(const QtMenuItem* pSalMenuItem);
+    bool validateQMenuBar();
 
 public:
     QtMenu(bool bMenuBar);

commit b97c37006468447190324225e615e5aa113d31aa

tdf#148491 Qt reconnect the QMenuBar close button
When the QMenuBar of a QMainWindow is replaced, an existing
corner widget is preserved / transferred, but its connections
are still severed; a bit unexpected...

The documentation for QMenuBar::setCornerWidget is not really
clear what is happening, but the code has this nice comment:
"// Reparent corner widgets before we delete the old menu".
At least there is no need to explicitly delete the button.

Still we must reconnect an existing button on each SetFrame.

Regression from commit 4805b2d4b321dabb1245956afc4347fb57378232
("tdf#145954 Qt unshare QMenubar usage").

Change-Id: I13c31734e665b78231a08cd76ca6305122e08879
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132836
Tested-by: Jenkins
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

vcl/qt5/QtMenu.cxx
@@ -429,6 +429,10 @@ void QtMenu::SetFrame(const SalFrame* pFrame)
 
     mpQMenuBar = new QMenuBar();
     pMainWindow->setMenuBar(mpQMenuBar);
+
+    QPushButton* pButton = static_cast<QPushButton*>(mpQMenuBar->cornerWidget(Qt::TopRightCorner));
+    if (pButton)
+        connect(pButton, &QPushButton::clicked, this, &QtMenu::slotCloseDocument);
     mpQMenu = nullptr;
 
     DoFullMenuUpdate(mpVCLMenu);
@@ -647,6 +651,8 @@ void QtMenu::ShowCloseButton(bool bShow)
         return;
 
     QPushButton* pButton = static_cast<QPushButton*>(mpQMenuBar->cornerWidget(Qt::TopRightCorner));
+    if (!pButton && !bShow)
+        return;
     if (!pButton)
     {
         QIcon aIcon;