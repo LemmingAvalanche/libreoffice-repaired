LemmingAvalanche/libreoffice-repaired

commit 150bdbd7eb3256654a8be1ee3fd3daaa4d27a67b

v2 tdf#134951 docxexport: don't lose postponed comment
This patch basically reverts and re-writes the original fix.

Prior to mstahl's 7.4 commit 73c40b4093a537d46e5bfeb1445af330df657873
   tdf#143039 tdf#143106 sw: DOCX export: don't export duplicate flys
ooxmlexport5's comment would only last for one or two round-trips,
and then be lost after that. It kept moving around into things
like the fly itself, or the footnote.
With mstahl's commit, it was lost immediately (same as Word 2016),
and so the ooxmlexport5 unit test that broke was commented out.

Change-Id: Ia7e881143f1243a3ab81bb94c02b57189f3730f0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128703
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/filter/ww8/wrtw8nds.cxx
@@ -394,7 +394,7 @@ sal_Int32 SwWW8AttrIter::SearchNext( sal_Int32 nStartPos )
     return nMinPos;
 }
 
-void SwWW8AttrIter::OutAttr(sal_Int32 nSwPos, bool bWriteCombChars, bool bPostponeSingleUse)
+void SwWW8AttrIter::OutAttr(sal_Int32 nSwPos, bool bWriteCombChars)
 {
     m_rExport.AttrOutput().RTLAndCJKState( mbCharIsRTL, GetScript() );
 
@@ -457,13 +457,6 @@ void SwWW8AttrIter::OutAttr(sal_Int32 nSwPos, bool bWriteCombChars, bool bPostpo
                         }
                         nWhichId = aIter.NextWhich();
                     }
-                }
-                else if (bPostponeSingleUse &&
-                         (nWhich == RES_TXTATR_FTN || nWhich == RES_TXTATR_ANNOTATION || nWhich == RES_TXTATR_FIELD))
-                {
-                    // Do not duplicate these multiple times when the character run is split.
-                    // Skip this time - it will be attempted later.
-                    // ?? also RES_TXTATR_REFMARK: RES_TXTATR_TOXMARK: RES_TXTATR_META: RES_TXTATR_METAFIELD: ??
                 }
                 else
                     aRangeItems[nWhich] = (&(pHt->GetAttr()));
@@ -2388,6 +2381,7 @@ void MSWordExportBase::OutputTextNode( SwTextNode& rNode )
 
             const SwRedlineData* pRedlineData = aAttrIter.GetRunLevelRedline( nCurrentPos );
             bool bPostponeWritingText    = false ;
+            bool bStartedPostponedRunProperties = false;
             OUString aSavedSnippet ;
 
             sal_Int32 nNextAttr = GetNextPos( &aAttrIter, rNode, nCurrentPos );
@@ -2655,8 +2649,9 @@ void MSWordExportBase::OutputTextNode( SwTextNode& rNode )
             {
                 // Output the character attributes
                 // #i51277# do this before writing flys at end of paragraph
+                bStartedPostponedRunProperties = true;
                 AttrOutput().StartRunProperties();
-                aAttrIter.OutAttr(nCurrentPos, false, bPostponeWritingText);
+                aAttrIter.OutAttr(nCurrentPos, false);
                 AttrOutput().EndRunProperties( pRedlineData );
             }
 
@@ -2758,26 +2753,25 @@ void MSWordExportBase::OutputTextNode( SwTextNode& rNode )
 
             AttrOutput().WritePostitFieldReference();
 
-            if (bPostponeWritingText
-                && (FLY_PROCESSED == nStateOfFlyFrame || FLY_NONE == nStateOfFlyFrame))
+            if (bPostponeWritingText)
             {
-                AttrOutput().EndRun(&rNode, nCurrentPos, nNextAttr == nEnd);
-                //write the postponed text run
-                AttrOutput().StartRun( pRedlineData, nCurrentPos, bSingleEmptyRun );
-                AttrOutput().SetAnchorIsLinkedToNode( false );
-                AttrOutput().ResetFlyProcessingFlag();
-                if (0 != nEnd)
+                if (FLY_PROCESSED == nStateOfFlyFrame || FLY_NONE == nStateOfFlyFrame)
+                {
+                    AttrOutput().EndRun(&rNode, nCurrentPos, /*bLastRun=*/false);
+
+                    AttrOutput().StartRun( pRedlineData, nCurrentPos, bSingleEmptyRun );
+                    AttrOutput().SetAnchorIsLinkedToNode( false );
+                    AttrOutput().ResetFlyProcessingFlag();
+                }
+                if (0 != nEnd && !bStartedPostponedRunProperties)
                 {
                     AttrOutput().StartRunProperties();
                     aAttrIter.OutAttr( nCurrentPos, false );
                     AttrOutput().EndRunProperties( pRedlineData );
+
+                    // OutAttr may have introduced new comments, so write them out now
+                    AttrOutput().WritePostitFieldReference();
                 }
-                AttrOutput().RunText( aSavedSnippet, eChrSet );
-                AttrOutput().EndRun(&rNode, nCurrentPos, nNextAttr == nEnd);
-            }
-            else if( bPostponeWritingText && !aSavedSnippet.isEmpty() )
-            {
-                //write the postponed text run
                 AttrOutput().RunText( aSavedSnippet, eChrSet );
                 AttrOutput().EndRun(&rNode, nCurrentPos, nNextAttr == nEnd);
             }

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -130,18 +130,15 @@ CPPUNIT_TEST_FIXTURE(Test, testNoDuplicateAttributeExport)
 
 CPPUNIT_TEST_FIXTURE(Test, testfdo79008)
 {
-    loadAndSave("fdo79008.docx");
+    loadAndReload("fdo79008.docx");
     /* File crashing while saving in LO.
      * Check if document.xml file is created after fix
      */
     parseExport("word/document.xml");
 
-#if 0
-    // TODO: jluth says this test case needs more work
     // tdf#134951: there is only one comment
     xmlDocUniquePtr pXmlSettings = parseExport("word/comments.xml");
     assertXPath(pXmlSettings, "/w:comments/w:comment", 1);
-#endif
 
     // Read-only is set, but it is not enforced, so it should be off...
     SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument *>(mxComponent.get());

sw/source/filter/ww8/wrtww8.hxx
@@ -1542,7 +1542,7 @@ public:
 
     void NextPos() { if ( nCurrentSwPos < SAL_MAX_INT32 ) nCurrentSwPos = SearchNext( nCurrentSwPos + 1 ); }
 
-    void OutAttr(sal_Int32 nSwPos, bool bWriteCombinedChars, bool bPostponeSingleUse = false);
+    void OutAttr(sal_Int32 nSwPos, bool bWriteCombinedChars);
     virtual const SfxPoolItem* HasTextItem( sal_uInt16 nWhich ) const override;
     virtual const SfxPoolItem& GetItem( sal_uInt16 nWhich ) const override;
     int OutAttrWithRange(const SwTextNode& rNode, sal_Int32 nPos);

commit 1b20e259a8d8c6d2c1b8067619d6430f342432ad

tdf#148565 sw ms export: a new run can't be postponed-started
This fixes a LO 7.4 regression from
commit 150bdbd7eb3256654a8be1ee3fd3daaa4d27a67b.

A make check with an assert indicated the following
existing unit tests matched - but they had no noticable
char run content to test against.
ooxmlexport6's testShapeThemePreservation
ooxmlexport7's testBnc884615
ooxmlexport9's testTdf90789
ooxmlexport10's testTdf90153
ooxmlexport11's testTdf137655
ooxmlexport14's testTdf131539
ooxmlexport15's testTdf138739
ooxmlexport17's testTdf126287

Change-Id: Iae89ff1f6de06bfe37b886e1e39e8457157ae240
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133187
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/filter/ww8/wrtw8nds.cxx
@@ -2772,6 +2772,8 @@ void MSWordExportBase::OutputTextNode( SwTextNode& rNode )
                 if (FLY_PROCESSED == nStateOfFlyFrame || FLY_NONE == nStateOfFlyFrame)
                 {
                     AttrOutput().EndRun(&rNode, nCurrentPos, /*bLastRun=*/false);
+                    if (!aSavedSnippet.isEmpty())
+                        bStartedPostponedRunProperties = false;
 
                     AttrOutput().StartRun( pRedlineData, nCurrentPos, bSingleEmptyRun );
                     AttrOutput().SetAnchorIsLinkedToNode( false );

sw/qa/extras/ooxmlexport/ooxmlexport15.cxx
@@ -9,6 +9,7 @@
 
 #include <swmodeltestbase.hxx>
 
+#include <com/sun/star/awt/FontWeight.hpp>
 #include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/text/RelOrientation.hpp>
 #include <com/sun/star/text/XTextViewCursorSupplier.hpp>
@@ -268,6 +269,9 @@ DECLARE_OOXMLEXPORT_TEST(testTdf138739, "tdf138739.docx")
     uno::Reference<beans::XPropertySet> xParaProps(getParagraph(1), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Font type name does not match!", OUString("Comic Sans MS"),
                                  xParaProps->getPropertyValue("CharFontName").get<OUString>());
+
+    // tdf#148565: text at anchor point should be bold, Comic Sanc MS font
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, getProperty<float>(getRun(getParagraph(5), 3), "CharWeight"));
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf123390, "tdf123390.fodt")