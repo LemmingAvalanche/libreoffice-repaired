LemmingAvalanche/libreoffice-repaired

commit ef4ac883edb74f9e78cdb4eef9901ae125dba177

try to use also proper debug LDFLAGS for externals libraries
This is basically 8c8622b26c7bc7890cc5c2c6552f6f03a2e0062b but
for LDFLAGS. A number of external libs cannot use this because
their libtool mishandles -fuse-ld.

Change-Id: Idee379eb0a3afb475b536519ee3de064b4e218f4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133639
Tested-by: Jenkins
Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

solenv/gbuild/ExternalProject.mk
@@ -210,6 +210,10 @@ endef
 # gb_ExternalProject_get_build_flags project
 gb_ExternalProject_get_build_flags = $(call gb_LinkTarget__get_debugflags,ExternalProject_$(1))
 
+# Returns flags to include in LDFLAGS to enable optimizations and/or debugging.
+# gb_ExternalProject_get_link_flags project
+gb_ExternalProject_get_link_flags = $(USE_LD) $(call gb_LinkTarget__get_debugldflags,ExternalProject_$(1))
+
 # Run a target command
 #
 # This provides a wrapper that changes to the right directory,

external/cairo/ExternalProject_cairo.mk
@@ -50,7 +50,9 @@ $(call gb_ExternalProject_get_state_target,cairo,build) :
 		$(if $(filter ANDROID iOS,$(OS)),CFLAGS="$(if $(debug),-g) $(ZLIB_CFLAGS) $(gb_VISIBILITY_FLAGS)") \
 		$(if $(filter EMSCRIPTEN,$(OS)),CFLAGS=" $(ZLIB_CFLAGS) -Wno-enum-conversion $(gb_EMSCRIPTEN_CPPFLAGS)" --enable-pthread=yes PTHREAD_LIBS="") \
 		$(if $(filter-out EMSCRIPTEN ANDROID iOS,$(OS)), \
-			CFLAGS="$(CFLAGS) $(call gb_ExternalProject_get_build_flags,cairo) $(ZLIB_CFLAGS)" ) \
+			CFLAGS="$(CFLAGS) $(call gb_ExternalProject_get_build_flags,cairo) $(ZLIB_CFLAGS)" \
+			LDFLAGS="$(call gb_ExternalProject_get_link_flags,cairo)" \
+			) \
 		$(if $(filter ANDROID iOS,$(OS)),PKG_CONFIG=./dummy_pkg_config) \
 		LIBS="$(ZLIB_LIBS)" \
 		$(if $(filter -fsanitize=%,$(LDFLAGS)),LDFLAGS="$(LDFLAGS) -fuse-ld=bfd") \

external/curl/ExternalProject_curl.mk
@@ -65,7 +65,8 @@ $(call gb_ExternalProject_get_state_target,curl,build):
 			$(if $(filter MACOSX,$(OS)),CFLAGS='$(CFLAGS) \
 				-mmacosx-version-min=$(MACOSX_DEPLOYMENT_TARGET)') \
 			CPPFLAGS='$(curl_CPPFLAGS)' \
-			LDFLAGS='$(curl_LDFLAGS)' \
+			CFLAGS="$(gb_CFLAGS) $(call gb_ExternalProject_get_build_flags,curl)" \
+			LDFLAGS='$(call gb_ExternalProject_get_link_flags,curl) $(curl_LDFLAGS)' \
 			ZLIB_CFLAGS='$(ZLIB_CFLAGS)' ZLIB_LIBS='$(ZLIB_LIBS)' \
 		&& cd lib \
 		&& $(MAKE) \

external/firebird/ExternalProject_firebird.mk
@@ -62,6 +62,7 @@ $(call gb_ExternalProject_get_state_target,firebird,build):
 			$(if $(ENABLE_DEBUG),$(if $(filter MSC,$(COM)),-Od -Z7)) \
 		" \
 		&& export LDFLAGS=" \
+			$(call gb_ExternalProject_get_link_flags,firebird) \
 			$(if $(SYSTEM_LIBATOMIC_OPS),$(LIBATOMIC_OPS_LIBS), \
 				-L$(call gb_UnpackedTarball_get_dir,libatomic_ops)/src \
 			) \

external/fontconfig/ExternalProject_fontconfig.mk
@@ -26,6 +26,7 @@ $(call gb_ExternalProject_get_state_target,fontconfig,build) :
 			$(gb_VISIBILITY_FLAGS) \
 			$(if $(filter EMSCRIPTEN,$(OS)),-pthread)" \
 			$(if $(filter ANDROID,$(OS)),LIBS="-lm") \
+		LDFLAGS="$(call gb_ExternalProject_get_link_flags,fontconfig)" \
 		$(gb_RUN_CONFIGURE) ./configure \
 			--disable-shared \
 			--disable-silent-rules \

external/freetype/ExternalProject_freetype.mk
@@ -30,6 +30,7 @@ $(call gb_ExternalProject_get_state_target,freetype,build) :
 				$(call gb_ExternalProject_get_build_flags,freetype) \
 				$(gb_VISIBILITY_FLAGS) \
 				$(gb_EMSCRIPTEN_CPPFLAGS)" \
+			LDFLAGS="$(call gb_ExternalProject_get_link_flags,freetype)" \
 		&& $(MAKE) install \
 		&& touch $@	)
 	$(call gb_Trace_EndRange,freetype,EXTERNAL)

external/harfbuzz/ExternalProject_harfbuzz.mk
@@ -49,6 +49,7 @@ $(call gb_ExternalProject_get_state_target,harfbuzz,build) :
 				$(CXXFLAGS) $(CXXFLAGS_CXX11) \
 				$(ICU_UCHAR_TYPE) \
 				$(if $(filter LINUX,$(OS)),-fvisibility=hidden)' \
+			LDFLAGS="$(call gb_ExternalProject_get_link_flags,harfbuzz)" \
 			MAKE=$(MAKE) \
 		&& (cd $(EXTERNAL_WORKDIR)/src && $(MAKE) lib) \
 	)

external/hunspell/ExternalProject_hunspell.mk
@@ -21,7 +21,7 @@ hunspell_CXXFLAGS:=$(CXXFLAGS) $(gb_LTOFLAGS) \
        $(gb_EMSCRIPTEN_CPPFLAGS) \
        $(call gb_ExternalProject_get_build_flags,hunspell)
 
-hunspell_LDFLAGS:=$(gb_LTOFLAGS)
+hunspell_LDFLAGS:=$(gb_LTOFLAGS) $(call gb_ExternalProject_get_link_flags,hunspell)
 
 $(call gb_ExternalProject_get_state_target,hunspell,build):
 	$(call gb_Trace_StartRange,hunspell,EXTERNAL)

commit b28f5fba36b186f4cb33d5e23e8ce272f0ba6d00

Keep passing LDFLAGS into external proejct builds
...which had been broken with ef4ac883edb74f9e78cdb4eef9901ae125dba177 "try to
use also proper debug LDFLAGS for externals libraries", and e.g. one of my macOS
builds uses a non-standard Clang compiler and libc++ library, and thus needs

  LDFLAGS=-L/Users/stephan/llvm/inst/lib

in autogen.input

Change-Id: Iae67a4a13603b0241e5cd6c0d01a07ac898ffb58
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133746
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

solenv/gbuild/ExternalProject.mk
@@ -212,7 +212,7 @@ gb_ExternalProject_get_build_flags = $(call gb_LinkTarget__get_debugflags,Extern
 
 # Returns flags to include in LDFLAGS to enable optimizations and/or debugging.
 # gb_ExternalProject_get_link_flags project
-gb_ExternalProject_get_link_flags = $(USE_LD) $(call gb_LinkTarget__get_debugldflags,ExternalProject_$(1))
+gb_ExternalProject_get_link_flags = $(LDFLAGS) $(USE_LD) $(call gb_LinkTarget__get_debugldflags,ExternalProject_$(1))
 
 # Run a target command
 #