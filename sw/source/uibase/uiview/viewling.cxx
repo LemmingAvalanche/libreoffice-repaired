LemmingAvalanche/libreoffice-repaired

commit e08ed83f4940841749a22f4ef7991c8e60767b75

sw_redlinehide: get text from shell, not node in ExecSpellPopup()
Change-Id: I0160c4927a2f5e71f5025c2162a103f67ed03723
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131209
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/uibase/uiview/viewling.cxx
@@ -692,7 +692,14 @@ bool SwView::ExecSpellPopup(const Point& rPt)
                 // get paragraph text
                 OUString aParaText;
                 if (pNode)
-                    aParaText = pNode->GetText();    // this may include hidden text but that should be Ok
+                {
+                    pCursorShell->Push();
+                    pCursorShell->MovePara(GoCurrPara, fnParaStart);
+                    pCursorShell->SetMark();
+                    pCursorShell->MovePara(GoCurrPara, fnParaEnd);
+                    aParaText = pCursorShell->GetSelText();
+                    pCursorShell->Pop(SwCursorShell::PopMode::DeleteCurrent);
+                }
                 else
                 {
                     OSL_FAIL("text node expected but not found" );

commit c6b71f71aed2fb71258db5af4c3fbd69e07aca82

tdf#148869 sw_redlinehide: fix SwView::ExecSpellPopup()
How could i forget that MovePara(GoCurrPara, fnParaStart) will move to
a previous paragraph when it's already at the start of the current one,
it's so intuitive.

(regression from commit e08ed83f4940841749a22f4ef7991c8e60767b75)

Change-Id: Ief9bcd42b7ea78feef50ac42bc93580c29c080b2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134468
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/uibase/uiview/viewling.cxx
@@ -694,9 +694,15 @@ bool SwView::ExecSpellPopup(const Point& rPt)
                 if (pNode)
                 {
                     pCursorShell->Push();
-                    pCursorShell->MovePara(GoCurrPara, fnParaStart);
+                    if (!pCursorShell->IsSttPara())
+                    {
+                        pCursorShell->MovePara(GoCurrPara, fnParaStart);
+                    }
                     pCursorShell->SetMark();
-                    pCursorShell->MovePara(GoCurrPara, fnParaEnd);
+                    if (!pCursorShell->IsEndPara())
+                    {
+                        pCursorShell->MovePara(GoCurrPara, fnParaEnd);
+                    }
                     aParaText = pCursorShell->GetSelText();
                     pCursorShell->Pop(SwCursorShell::PopMode::DeleteCurrent);
                 }