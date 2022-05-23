LemmingAvalanche/libreoffice-repaired

commit 8f16d3c820e50c4b79dc639371ea7ca600c11529

sw_redlinehide_4b: visibility checks for bookmarks too
SwCursorShell::GotoMark(), GoNextBookmark(), GoPrevBookmark() check
visibility of mark in layout.

SwWrtShell::GotoMark() returns whether it was successful.

EnhancedPDFExport skips marks that are not visible when creating links.

Change-Id: I36c0706c18a5f861b15eaf451a97daec04104056

sw/source/core/crsr/crbm.cxx
@@ -19,6 +19,8 @@
 
 #include <crsrsh.hxx>
 #include <ndtxt.hxx>
+#include <rootfrm.hxx>
+#include <txtfrm.hxx>
 #include <docary.hxx>
 #include <IMark.hxx>
 #include "callnk.hxx"
@@ -121,10 +123,53 @@ namespace
     EndAction();
     return pMark;
 }
-// set CurrentCursor.SPoint
 
+namespace sw {
+
+bool IsMarkHidden(SwRootFrame const& rLayout, ::sw::mark::IMark const& rMark)
+{
+    if (!rLayout.IsHideRedlines())
+    {
+        return false;
+    }
+    SwTextNode const& rNode(*rMark.GetMarkPos().nNode.GetNode().GetTextNode());
+    SwTextFrame const*const pFrame(static_cast<SwTextFrame const*>(
+        rNode.getLayoutFrame(&rLayout)));
+    if (!pFrame)
+    {
+        return true;
+    }
+    if (rMark.IsExpanded())
+    {
+        SwTextFrame const*const pOtherFrame(static_cast<SwTextFrame const*>(
+            rMark.GetOtherMarkPos().nNode.GetNode().GetTextNode()->getLayoutFrame(&rLayout)));
+        return pFrame == pOtherFrame
+            && pFrame->MapModelToViewPos(rMark.GetMarkPos())
+                == pFrame->MapModelToViewPos(rMark.GetOtherMarkPos());
+    }
+    else
+    {
+        if (rMark.GetMarkPos().nContent.GetIndex() == rNode.Len())
+        {   // at end of node: never deleted (except if node deleted)
+            return rNode.GetRedlineMergeFlag() == SwNode::Merge::Hidden;
+        }
+        else
+        {   // check character following mark pos
+            return pFrame->MapModelToViewPos(rMark.GetMarkPos())
+                == pFrame->MapModelToView(&rNode, rMark.GetMarkPos().nContent.GetIndex() + 1);
+        }
+    }
+}
+
+} // namespace sw
+
+// set CurrentCursor.SPoint
 bool SwCursorShell::GotoMark(const ::sw::mark::IMark* const pMark, bool bAtStart)
 {
+    if (sw::IsMarkHidden(*GetLayout(), *pMark))
+    {
+        return false;
+    }
     // watch Cursor-Moves
     CursorStateHelper aCursorSt(*this);
     if ( bAtStart )
@@ -140,6 +185,10 @@ bool SwCursorShell::GotoMark(const ::sw::mark::IMark* const pMark, bool bAtStart
 
 bool SwCursorShell::GotoMark(const ::sw::mark::IMark* const pMark)
 {
+    if (sw::IsMarkHidden(*GetLayout(), *pMark))
+    {
+        return false;
+    }
     // watch Cursor-Moves
     CursorStateHelper aCursorSt(*this);
     aCursorSt.SetCursorToMark(pMark);
@@ -169,6 +218,10 @@ bool SwCursorShell::GoNextBookmark()
     IDocumentMarkAccess::const_iterator_t ppMark = vCandidates.begin();
     for(; ppMark!=vCandidates.end(); ++ppMark)
     {
+        if (sw::IsMarkHidden(*GetLayout(), **ppMark))
+        {
+            continue;
+        }
         aCursorSt.SetCursorToMark(ppMark->get());
         if(!aCursorSt.RollbackIfIllegal())
             break; // found legal move
@@ -214,6 +267,10 @@ bool SwCursorShell::GoPrevBookmark()
         // above)
         if(!(**ppMark).EndsBefore(*GetCursor()->GetPoint()))
             continue;
+        if (sw::IsMarkHidden(*GetLayout(), **ppMark))
+        {
+            continue;
+        }
         aCursorSt.SetCursorToMark(ppMark->get());
         if(!aCursorSt.RollbackIfIllegal())
             break; // found legal move

sw/source/core/inc/txtfrm.hxx
@@ -26,6 +26,7 @@
 
 namespace com { namespace sun { namespace star { namespace linguistic2 { class XHyphenatedWord; } } } }
 
+namespace sw { namespace mark { class IMark; } }
 class SwCharRange;
 class SwTextNode;
 class SwTextFormatter;
@@ -134,6 +135,8 @@ void AddRemoveFlysAnchoredToFrameStartingAtNode(
 OUString GetExpandTextMerged(SwRootFrame const* pLayout,
         SwTextNode const& rNode, bool bWithNumber,
         bool bWithSpacesForLevel, ExpandMode i_mode);
+
+bool IsMarkHidden(SwRootFrame const& rLayout, ::sw::mark::IMark const& rMark);
 
 } // namespace sw
 

sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -2101,7 +2101,10 @@ void SwEnhancedPDFExportHelper::EnhancedPDFExport()
                 const OUString& sBkName = pBkmk->GetName();
 
                 //jump to it
-                JumpToSwMark( &mrSh, sBkName );
+                if (! JumpToSwMark( &mrSh, sBkName ))
+                {
+                    continue;
+                }
 
                 // Destination Rectangle
                 const SwRect& rDestRect = mrSh.GetCharRect();

sw/source/uibase/inc/wrtsh.hxx
@@ -404,7 +404,7 @@ typedef bool (SwWrtShell:: *FNSimpleMove)();
 
     // jump to bookmark and set the "selections-flags" correctly again
     void GotoMark( const ::sw::mark::IMark* const pMark );
-    void GotoMark( const ::sw::mark::IMark* const pMark, bool bSelect );
+    bool GotoMark( const ::sw::mark::IMark* const pMark, bool bSelect );
     void GotoMark( const OUString& rName );
     bool GoNextBookmark(); // true when there still was one
     bool GoPrevBookmark();

sw/source/uibase/uiview/view2.cxx
@@ -2002,8 +2002,7 @@ bool SwView::JumpToSwMark( const OUString& rMark )
             }
             else if( pMarkAccess->getAllMarksEnd() != (ppMark = pMarkAccess->findMark(sMark)) )
             {
-                m_pWrtShell->GotoMark( ppMark->get(), false );
-                bRet = true;
+                bRet = m_pWrtShell->GotoMark( ppMark->get(), false );
             }
             else if( nullptr != ( pINet = m_pWrtShell->FindINetAttr( sMark ) )) {
                 m_pWrtShell->addCurrentPosition();
@@ -2029,8 +2028,7 @@ bool SwView::JumpToSwMark( const OUString& rMark )
         }
         else if( pMarkAccess->getAllMarksEnd() != (ppMark = pMarkAccess->findMark(sMark)))
         {
-            m_pWrtShell->GotoMark( ppMark->get(), false );
-            bRet = true;
+            bRet = m_pWrtShell->GotoMark( ppMark->get(), false );
         }
         else if( nullptr != ( pINet = m_pWrtShell->FindINetAttr( sMark ) ))
             bRet = m_pWrtShell->GotoINetAttr( *pINet->GetTextINetFormat() );

sw/source/uibase/wrtsh/move.cxx
@@ -576,13 +576,14 @@ bool SwWrtShell::GotoPage(sal_uInt16 nPage, bool bRecord)
     return false;
 }
 
-void SwWrtShell::GotoMark( const ::sw::mark::IMark* const pMark, bool bSelect )
+bool SwWrtShell::GotoMark( const ::sw::mark::IMark* const pMark, bool bSelect )
 {
     ShellMoveCursor aTmp( this, bSelect );
     SwPosition aPos = *GetCursor()->GetPoint();
     bool bRet = SwCursorShell::GotoMark( pMark, true/*bStart*/ );
     if (bRet)
         m_aNavigationMgr.addEntry(aPos);
+    return bRet;
 }
 
 bool SwWrtShell::GotoFly( const OUString& rName, FlyCntType eType, bool bSelFrame )

commit 95cc6ff03498ac6808318bc5abed12803ce357c3

sw_redlinehide: fix crash in IsMarkHidden() if pointing to table node
This is called during mail merge from documentStartPageNumber()
and if the document starts with a table, the passed UNO mark will point
to SwTableNode.

(regression from commit 8f16d3c820e50c4b79dc639371ea7ca600c11529)

Change-Id: Ic69c12ba0d819eda85de5dde95e35a8071466c2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134692
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/crsr/crbm.cxx
@@ -128,9 +128,14 @@ bool IsMarkHidden(SwRootFrame const& rLayout, ::sw::mark::IMark const& rMark)
     {
         return false;
     }
-    SwTextNode const& rNode(*rMark.GetMarkPos().nNode.GetNode().GetTextNode());
+    SwNode const& rNode(rMark.GetMarkPos().nNode.GetNode());
+    SwTextNode const*const pTextNode(rNode.GetTextNode());
+    if (pTextNode == nullptr)
+    {   // UNO_BOOKMARK may point to table node
+        return rNode.GetRedlineMergeFlag() == SwNode::Merge::Hidden;
+    }
     SwTextFrame const*const pFrame(static_cast<SwTextFrame const*>(
-        rNode.getLayoutFrame(&rLayout)));
+        pTextNode->getLayoutFrame(&rLayout)));
     if (!pFrame)
     {
         return true;
@@ -145,14 +150,14 @@ bool IsMarkHidden(SwRootFrame const& rLayout, ::sw::mark::IMark const& rMark)
     }
     else
     {
-        if (rMark.GetMarkPos().nContent.GetIndex() == rNode.Len())
+        if (rMark.GetMarkPos().nContent.GetIndex() == pTextNode->Len())
         {   // at end of node: never deleted (except if node deleted)
-            return rNode.GetRedlineMergeFlag() == SwNode::Merge::Hidden;
+            return pTextNode->GetRedlineMergeFlag() == SwNode::Merge::Hidden;
         }
         else
         {   // check character following mark pos
             return pFrame->MapModelToViewPos(rMark.GetMarkPos())
-                == pFrame->MapModelToView(&rNode, rMark.GetMarkPos().nContent.GetIndex() + 1);
+                == pFrame->MapModelToView(pTextNode, rMark.GetMarkPos().nContent.GetIndex() + 1);
         }
     }
 }