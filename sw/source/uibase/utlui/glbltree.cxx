LemmingAvalanche/libreoffice-repaired

commit 3f151b6ee30b1f253748308ce113e6ee2981ba5e

tdf#144349 Make Navigator global tree track sub-document sections
Master document relies on the Navigator listing all the sub-documents.
However there is no connection between the currently selected section
(containing the sub-document) and the entry in the Navigator making it
very hard to see what file needs to be edited. This patch highlights
the corresponding item in the Navigator at the current cursor position
in the document.

Change-Id: Iee32d1db0e40e302bfff7a879d47a5632ce4ed43
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122103
Tested-by: Jenkins
Reviewed-by: Jim Raykowski <raykowj@gmail.com>

sw/source/uibase/utlui/glbltree.cxx
@@ -773,8 +773,33 @@ void SwGlobalTree::ExecuteContextMenuAction(std::string_view rSelectedPopupEntry
 
 IMPL_LINK_NOARG(SwGlobalTree, Timeout, Timer *, void)
 {
-    if (!m_xTreeView->has_focus() && Update(false))
-        Display();
+    if (m_pActiveShell && m_pActiveShell->GetView().GetEditWin().HasFocus())
+    {
+        if (Update(false))
+            Display();
+        UpdateTracking();
+    }
+}
+
+void SwGlobalTree::UpdateTracking()
+{
+    if (!m_pActiveShell)
+        return;
+
+    // track section at cursor position in document
+    m_xTreeView->unselect_all();
+
+    const SwSection* pActiveShellCurrSection = m_pActiveShell->GetCurrSection();
+    if (pActiveShellCurrSection)
+    {
+        const SwSection* pSection = pActiveShellCurrSection;
+        SwSection* pParent;
+        while ((pParent = pSection->GetParent()) != nullptr)
+            pSection = pParent;
+
+        if (pSection)
+            m_xTreeView->select_text(pSection->GetSectionName());
+    }
 }
 
 void SwGlobalTree::GotoContent(const SwGlblDocContent* pCont)
@@ -803,6 +828,7 @@ void SwGlobalTree::ShowTree()
 {
     m_aUpdateTimer.Start();
     m_xTreeView->show();
+    UpdateTracking();
 }
 
 void SwGlobalTree::HideTree()
@@ -854,10 +880,13 @@ bool SwGlobalTree::Update(bool bHard)
     bool bRet = false;
     if (pActView && pActView->GetWrtShellPtr())
     {
-        const SwWrtShell* pOldShell = m_pActiveShell;
+        SwWrtShell* pOldShell = m_pActiveShell;
         m_pActiveShell = pActView->GetWrtShellPtr();
         if(m_pActiveShell != pOldShell)
         {
+            if (pOldShell)
+                EndListening(*pOldShell->GetView().GetDocShell());
+            StartListening(*m_pActiveShell->GetView().GetDocShell());
             m_pSwGlblDocContents.reset();
         }
         if(!m_pSwGlblDocContents)
@@ -1088,6 +1117,13 @@ IMPL_LINK( SwGlobalTree, DialogClosedHdl, sfx2::FileDialogHelper*, _pFileDlg, vo
     }
     InsertRegion( m_pDocContent.get(), aFileNames );
     m_pDocContent.reset();
+}
+
+void SwGlobalTree::Notify(SfxBroadcaster& rBC, SfxHint const& rHint)
+{
+    SfxListener::Notify(rBC, rHint);
+    if (rHint.GetId() == SfxHintId::SwNavigatorUpdateTracking)
+        UpdateTracking();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/uibase/inc/conttree.hxx
@@ -282,7 +282,7 @@ public:
     SwGlobalTreeDropTarget(SwGlobalTree& rTreeView);
 };
 
-class SwGlobalTree final
+class SwGlobalTree final : public SfxListener
 {
 private:
     std::unique_ptr<weld::TreeView> m_xTreeView;
@@ -368,6 +368,9 @@ public:
     void                ExecuteContextMenuAction(std::string_view rSelectedPopupEntry);
 
     const SwWrtShell*   GetActiveWrtShell() const {return m_pActiveShell;}
+
+    virtual void Notify(SfxBroadcaster& rBC, SfxHint const& rHint) override;
+    void UpdateTracking();
 };
 
 #endif

sw/source/uibase/uiview/viewsrch.cxx
@@ -257,6 +257,8 @@ void SwView::ExecSearch(SfxRequest& rReq)
                 m_pWrtShell->GetSfxViewShell()->setTiledSearching(false);
 
                 GetDocShell()->Broadcast(
+                            SfxHint(SfxHintId::SwNavigatorUpdateTracking));
+                GetDocShell()->Broadcast(
                             SfxHint(SfxHintId::SwNavigatorSelectOutlinesWithSelections));
 
                 if( !bRet )

commit 1925aa0f1d7acba3ed94b9fa5896217b28b82baa

tdf#149231 Fix crash on print preview of master
caused by commit 3f151b6ee30b1f253748308ce113e6ee2981ba5e.

Change-Id: Iecfa019b04b84bb70b5e04af51f9f786d7cd9fdf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134846
Tested-by: Jenkins
Reviewed-by: Jim Raykowski <raykowj@gmail.com>

sw/source/uibase/utlui/glbltree.cxx
@@ -773,7 +773,8 @@ void SwGlobalTree::ExecuteContextMenuAction(std::string_view rSelectedPopupEntry
 
 IMPL_LINK_NOARG(SwGlobalTree, Timeout, Timer *, void)
 {
-    if (m_pActiveShell && m_pActiveShell->GetView().GetEditWin().HasFocus())
+    SwView* pView = GetParentWindow()->GetCreateView();
+    if (pView && pView->GetEditWin().HasFocus())
     {
         if (Update(false))
             Display();
@@ -877,14 +878,13 @@ bool SwGlobalTree::Update(bool bHard)
     bool bRet = false;
     if (pActView && pActView->GetWrtShellPtr())
     {
-        SwWrtShell* pOldShell = m_pActiveShell;
+        const SwWrtShell* pOldShell = m_pActiveShell;
         m_pActiveShell = pActView->GetWrtShellPtr();
         if(m_pActiveShell != pOldShell)
         {
-            if (pOldShell)
-                EndListening(*pOldShell->GetView().GetDocShell());
-            StartListening(*m_pActiveShell->GetView().GetDocShell());
             m_pSwGlblDocContents.reset();
+            if (!IsListening(*m_pActiveShell->GetView().GetDocShell()))
+                StartListening(*m_pActiveShell->GetView().GetDocShell());
         }
         if(!m_pSwGlblDocContents)
         {