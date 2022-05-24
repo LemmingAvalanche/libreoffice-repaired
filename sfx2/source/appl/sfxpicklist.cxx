LemmingAvalanche/libreoffice-repaired

commit 50affcfe2a1b2d19da7b09507fcb517221fe33d1

officecfg,unotools,framework,sfx2: store ReadOnly for recent docs
Add Office::Histories::HistoryItem::ReadOnly flag to configuration so
that a document that was opened read-only isn't opened as editable from
recent documents.

Change-Id: I6985da287d3337a53a7e41e8e500421038eedb91
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133385
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sfx2/source/appl/sfxpicklist.cxx
@@ -123,13 +123,15 @@ void SfxPickListImpl::AddDocumentToPickList( const SfxObjectShell* pDocSh )
             }
         }
     }
+    ::std::optional<bool> const oIsReadOnly(pDocSh->IsReadOnly());
 
     // add to svtool history options
     SvtHistoryOptions::AppendItem( EHistoryType::PickList,
             aURL.GetURLNoPass( INetURLObject::DecodeMechanism::NONE ),
             aFilter,
             aTitle,
-            aThumbnail);
+            aThumbnail,
+            oIsReadOnly);
 
     if ( aURL.GetProtocol() == INetProtocol::File )
         Application::AddToRecentDocumentList( aURL.GetURLNoPass( INetURLObject::DecodeMechanism::NONE ),

dbaccess/source/ui/app/AppController.cxx
@@ -362,7 +362,7 @@ void SAL_CALL OApplicationController::disposing()
                             aURL.GetURLNoPass( INetURLObject::DecodeMechanism::NONE ),
                             aFilter,
                             getStrippedDatabaseName(),
-                            std::nullopt);
+                            std::nullopt, std::nullopt);
 
                     // add to recent document list
                     if ( aURL.GetProtocol() == INetProtocol::File )

dbaccess/source/ui/control/opendoccontrols.cxx
@@ -145,7 +145,7 @@ namespace dbaui
                 OUString sTitle = rHistoryItem.sTitle;
                 OUString sPassword = rHistoryItem.sPassword;
 
-                //  If the entry is an impress file then insert it into the
+                //  If the entry is a Base file then insert it into the
                 //  history list and the list box.
                 Sequence< PropertyValue > aProps;
                 xFilterFactory->getByName( sFilter ) >>= aProps;

framework/source/uielement/recentfilesmenucontroller.cxx
@@ -92,7 +92,7 @@ private:
     void fillPopupMenu( css::uno::Reference< css::awt::XPopupMenu > const & rPopupMenu );
     void executeEntry( sal_Int32 nIndex );
 
-    std::vector< OUString >   m_aRecentFilesItems;
+    std::vector<std::pair<OUString, bool>>   m_aRecentFilesItems;
     bool                      m_bDisabled : 1;
     bool                      m_bShowToolbarEntries;
 };
@@ -157,7 +157,7 @@ void RecentFilesMenuController::fillPopupMenu( Reference< css::awt::XPopupMenu >
         for ( int i = 0; i < nPickListMenuItems; i++ )
         {
             const SvtHistoryOptions::HistoryItem& rPickListEntry = aHistoryList[i];
-            m_aRecentFilesItems.push_back( rPickListEntry.sURL );
+            m_aRecentFilesItems.emplace_back(rPickListEntry.sURL, rPickListEntry.isReadOnly);
         }
     }
 
@@ -191,7 +191,7 @@ void RecentFilesMenuController::fillPopupMenu( Reference< css::awt::XPopupMenu >
 
             // Abbreviate URL
             OUString   aMenuTitle;
-            INetURLObject   aURL( m_aRecentFilesItems[i] );
+            INetURLObject const aURL(m_aRecentFilesItems[i].first);
             OUString aTipHelpText( aURL.getFSysPath( FSysStyle::Detect ) );
 
             if ( aURL.GetProtocol() == INetProtocol::File )
@@ -263,13 +263,15 @@ void RecentFilesMenuController::executeEntry( sal_Int32 nIndex )
     Sequence< PropertyValue > aArgsList{
         comphelper::makePropertyValue("Referer", OUString( "private:user" )),
 
+        comphelper::makePropertyValue("ReadOnly", m_aRecentFilesItems[nIndex].second),
+
         // documents in the picklist will never be opened as templates
         comphelper::makePropertyValue("AsTemplate", false),
 
         // Type detection needs to know which app we are opening it from.
         comphelper::makePropertyValue("DocumentService", m_aModuleName)
     };
-    dispatchCommand( m_aRecentFilesItems[ nIndex ], aArgsList, "_default" );
+    dispatchCommand(m_aRecentFilesItems[nIndex].first, aArgsList, "_default");
 }
 
 // XEventListener

include/unotools/historyoptions.hxx
@@ -58,6 +58,7 @@ namespace SvtHistoryOptions
         OUString sTitle;
         OUString sPassword;
         OUString sThumbnail;
+        bool isReadOnly = false;
     };
     UNOTOOLS_DLLPUBLIC std::vector< HistoryItem > GetList(EHistoryType eHistory);
 
@@ -72,7 +73,7 @@ namespace SvtHistoryOptions
     */
     UNOTOOLS_DLLPUBLIC void AppendItem(EHistoryType eHistory,
             const OUString& sURL, const OUString& sFilter, const OUString& sTitle,
-            const std::optional<OUString>& sThumbnail);
+            const std::optional<OUString>& sThumbnail, std::optional<bool> oIsReadOnly);
 
     /** Delete item from the specified list.
     */

officecfg/registry/schema/org/openoffice/Office/Histories.xcs
@@ -47,6 +47,11 @@
           <desc>Contains a base64 encoded thumbnail of the document.</desc>
         </info>
       </prop>
+      <prop oor:name="ReadOnly" oor:type="xs:boolean">
+        <info>
+          <desc>Stores whether the document was opened editable or read-only.</desc>
+        </info>
+      </prop>
     </group>
     <group oor:name="HistoryOrder">
       <info>

sfx2/inc/recentdocsview.hxx
@@ -63,7 +63,7 @@ public:
     RecentDocsView(std::unique_ptr<weld::ScrolledWindow> xWindow, std::unique_ptr<weld::Menu> xMenu);
     virtual ~RecentDocsView() override;
 
-    void insertItem(const OUString &rURL, const OUString &rTitle, const OUString& rThumbnail, sal_uInt16 nId);
+    void insertItem(const OUString &rURL, const OUString &rTitle, const OUString& rThumbnail, bool isReadOnly, sal_uInt16 nId);
 
     static bool typeMatchesExtension(ApplicationType type, std::u16string_view rExt);
 

sfx2/source/appl/newhelp.cxx
@@ -1186,7 +1186,9 @@ BookmarksTabPage_Impl::~BookmarksTabPage_Impl()
     SvtHistoryOptions::Clear( EHistoryType::HelpBookmarks );
     const sal_Int32 nCount = m_xBookmarksBox->n_children();
     for (sal_Int32 i = 0; i < nCount; ++i)
-        SvtHistoryOptions::AppendItem(EHistoryType::HelpBookmarks, m_xBookmarksBox->get_id(i), "", m_xBookmarksBox->get_text(i), std::nullopt);
+    {
+        SvtHistoryOptions::AppendItem(EHistoryType::HelpBookmarks, m_xBookmarksBox->get_id(i), "", m_xBookmarksBox->get_text(i), std::nullopt, std::nullopt);
+    }
 
     m_xBookmarksBox.reset();
     m_xBookmarksPB.reset();

commit a92db5cbb56c120aa101feaec298eead8754edf6

tdf#149170 sfx2: store IsOriginallyLoadedReadOnly() for recent files
This flag stores the user's intent.

(regression from commit 50affcfe2a1b2d19da7b09507fcb517221fe33d1)

Change-Id: I825eaf1323b613e131d058cb9b9ab86363d5f694
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134880
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sfx2/source/appl/sfxpicklist.cxx
@@ -123,7 +123,7 @@ void SfxPickListImpl::AddDocumentToPickList( const SfxObjectShell* pDocSh )
             }
         }
     }
-    ::std::optional<bool> const oIsReadOnly(pDocSh->IsReadOnly());
+    ::std::optional<bool> const oIsReadOnly(pMed->IsOriginallyLoadedReadOnly());
 
     // add to svtool history options
     SvtHistoryOptions::AppendItem( EHistoryType::PickList,