LemmingAvalanche/libreoffice-repaired

commit af407df46bad3df3f698a532b3e43f1e5084897f

Add sanity check to avoid potential crash in the Collabora Online server
Make sure some coordinates don't become huge negative numbers. This
doesn't fix the unknown root cause of course.

There already was such a sanity check here in this branch, but for
Android only. I now made it cross-platform.

Let's use the same Change-Id as in the corresponding commit in the
co-2021 branch.

Change-Id: I28e343271b3f80ac203b52db1be58d2add89eecd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128932
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130051
Tested-by: Jenkins
Reviewed-by: Tor Lillqvist <tml@collabora.com>

vcl/source/window/layout.cxx
@@ -283,10 +283,11 @@ void VclBox::setAllocation(const Size &rAllocation)
 // Though the dialogs are rendered in javascript for LOK Android some widgets like weld::DrawingArea
 // is sent as bitmap but it is rendered from only the visible part
 // when it gets negative, it shrinks instead of expands and it becomes invisible
-#if HAVE_FEATURE_ANDROID_LOK
         if (nExtraSpace < 0)
+        {
+            SAL_WARN("vcl.layout", "nExtraSpace went negative, setting to zero");
             nExtraSpace = 0;
-#endif
+        }
     }
 
     //Split into those we pack from the start onwards, and those we pack from the end backwards

commit 01a17a266e3d4c09f198be6eed5908ce0d7d199a

tdf#147844: Use sanity check only if LibreOfficeKit is active
Regression from af407df46bad3df3f698a532b3e43f1e5084897f
"Add sanity check to avoid potential crash in the Collabora Online
server"

Change-Id: I2fcfb634b47f9d76f8efbbd64a8e69ea236894c3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135024
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

vcl/source/window/layout.cxx
@@ -14,6 +14,7 @@
 #include <config_features.h>
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
 #include <comphelper/base64.hxx>
+#include <comphelper/lok.hxx>
 #include <o3tl/enumarray.hxx>
 #include <o3tl/enumrange.hxx>
 #include <o3tl/string_view.hxx>
@@ -283,10 +284,14 @@ void VclBox::setAllocation(const Size &rAllocation)
 // Though the dialogs are rendered in javascript for LOK Android some widgets like weld::DrawingArea
 // is sent as bitmap but it is rendered from only the visible part
 // when it gets negative, it shrinks instead of expands and it becomes invisible
-        if (nExtraSpace < 0)
+
+        if( comphelper::LibreOfficeKit::isActive() )
         {
-            SAL_WARN("vcl.layout", "nExtraSpace went negative, setting to zero for VclBox: " << GetHelpId());
-            nExtraSpace = 0;
+            if (nExtraSpace < 0)
+            {
+                SAL_WARN("vcl.layout", "nExtraSpace went negative, setting to zero for VclBox: " << GetHelpId());
+                nExtraSpace = 0;
+            }
         }
     }
 