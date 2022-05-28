LemmingAvalanche/libreoffice-repaired

commit b19b50c302f8e9167b3d8430930892ea0bfefd43

use fastparser in SvXMLPropertySetContext subclasses
Change-Id: I977f1cf198652d3c73e5a0f473794975a5647617
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101564
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmloff/source/text/XMLSectionFootnoteConfigImport.cxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/uno/Reference.h>
 #include <com/sun/star/xml/sax/XAttributeList.hpp>
 #include <com/sun/star/style/NumberingType.hpp>
+#include <sal/log.hxx>
 #include <sax/tools/converter.hxx>
 #include <xmloff/xmlimp.hxx>
 #include <xmloff/xmltoken.hxx>
@@ -47,11 +48,10 @@ using ::com::sun::star::xml::sax::XAttributeList;
 
 XMLSectionFootnoteConfigImport::XMLSectionFootnoteConfigImport(
     SvXMLImport& rImport,
-    sal_uInt16 nPrefix,
-    const OUString& rLocalName,
+    sal_Int32 /*nElement*/,
     vector<XMLPropertyState> & rProps,
     const rtl::Reference<XMLPropertySetMapper> & rMapperRef) :
-        SvXMLImportContext(rImport, nPrefix, rLocalName),
+        SvXMLImportContext(rImport),
         rProperties(rProps),
         rMapper(rMapperRef)
 {
@@ -61,8 +61,9 @@ XMLSectionFootnoteConfigImport::~XMLSectionFootnoteConfigImport()
 {
 }
 
-void XMLSectionFootnoteConfigImport::StartElement(
-    const Reference<XAttributeList> & xAttrList)
+void XMLSectionFootnoteConfigImport::startFastElement(
+    sal_Int32 /*nElement*/,
+    const Reference<css::xml::sax::XFastAttributeList> & xAttrList)
 {
     bool bNumOwn = false;
     bool bNumRestart = false;
@@ -74,18 +75,13 @@ void XMLSectionFootnoteConfigImport::StartElement(
     OUString sNumLetterSync;
 
     // iterate over xattribute list and fill values
-    sal_Int16 nLength = xAttrList->getLength();
-    for(sal_Int16 nAttr = 0; nAttr < nLength; nAttr++)
+    for (auto &aIter : sax_fastparser::castToFastAttributeList(xAttrList))
     {
-        OUString sLocalName;
-        sal_uInt16 nPrefix = GetImport().GetNamespaceMap().
-            GetKeyByAttrName( xAttrList->getNameByIndex(nAttr),
-                              &sLocalName );
-        OUString sAttrValue = xAttrList->getValueByIndex(nAttr);
+        OUString sAttrValue = aIter.toString();
 
-        if (XML_NAMESPACE_TEXT == nPrefix)
+        switch(aIter.getToken())
         {
-            if (IsXMLToken(sLocalName, XML_START_VALUE))
+            case XML_ELEMENT(TEXT, XML_START_VALUE):
             {
                 sal_Int32 nTmp;
                 if (::sax::Converter::convertNumber(nTmp, sAttrValue))
@@ -93,35 +89,40 @@ void XMLSectionFootnoteConfigImport::StartElement(
                     nNumRestartAt = static_cast< sal_Int16 >( nTmp ) - 1;
                     bNumRestart = true;
                 }
+                break;
             }
-            else if( IsXMLToken( sLocalName, XML_NOTE_CLASS ) )
+            case XML_ELEMENT(TEXT, XML_NOTE_CLASS):
             {
                 if( IsXMLToken( sAttrValue, XML_ENDNOTE ) )
                     bEndnote = true;
+                break;
             }
-        }
-        else if (XML_NAMESPACE_STYLE == nPrefix)
-        {
-            if (IsXMLToken(sLocalName, XML_NUM_PREFIX))
+            case XML_ELEMENT(STYLE, XML_NUM_PREFIX):
             {
                 sNumPrefix = sAttrValue;
                 bNumOwn = true;
+                break;
             }
-            else if (IsXMLToken(sLocalName, XML_NUM_SUFFIX))
+            case XML_ELEMENT(TEXT, XML_NUM_SUFFIX):
             {
                 sNumSuffix = sAttrValue;
                 bNumOwn = true;
+                break;
             }
-            else if (IsXMLToken(sLocalName, XML_NUM_FORMAT))
+            case XML_ELEMENT(TEXT, XML_NUM_FORMAT):
             {
                 sNumFormat = sAttrValue;
                 bNumOwn = true;
+                break;
             }
-            else if (IsXMLToken(sLocalName, XML_NUM_LETTER_SYNC))
+            case XML_ELEMENT(TEXT, XML_NUM_LETTER_SYNC):
             {
                 sNumLetterSync = sAttrValue;
                 bNumOwn = true;
+                break;
             }
+            default:
+                SAL_WARN("xmloff", "unknown attribute " << SvXMLImport::getPrefixAndNameFromToken(aIter.getToken()) << "=" << sAttrValue);
         }
     }
 

include/xmloff/xmlprcon.hxx
@@ -39,15 +39,6 @@ protected:
 public:
 
     SvXMLPropertySetContext(
-            SvXMLImport& rImport, sal_uInt16 nPrfx,
-            const OUString& rLName,
-            const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList,
-            sal_uInt32 nFamily,
-            ::std::vector< XMLPropertyState > &rProps,
-            const rtl::Reference < SvXMLImportPropertyMapper > &rMap,
-              sal_Int32 nStartIdx = -1, sal_Int32 nEndIdx = -1 );
-
-    SvXMLPropertySetContext(
             SvXMLImport& rImport, sal_Int32 nElement,
             const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList,
             sal_uInt32 nFamily,
@@ -60,18 +51,18 @@ public:
     virtual void SAL_CALL startFastElement( sal_Int32 /*nElement*/,
         const css::uno::Reference< css::xml::sax::XFastAttributeList >& ) override {}
 
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix,
-                                     const OUString& rLocalName,
-                                     const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList ) override;
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& AttrList ) override;
 
     // This method is called from this instance implementation of
     // CreateChildContext if the element matches an entry in the
     // SvXMLImportItemMapper with the mid flag MID_FLAG_ELEMENT_ITEM_IMPORT
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix,
-                                   const OUString& rLocalName,
-                                   const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList,
-                                   ::std::vector< XMLPropertyState > &rProperties,
-                                   const XMLPropertyState& rProp );
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > createFastChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList,
+        ::std::vector< XMLPropertyState > &rProperties,
+        const XMLPropertyState& rProp );
 
 };
 

include/xmloff/xmlprmap.hxx
@@ -93,6 +93,12 @@ public:
         sal_uInt16 nNamespace, const OUString& rStrName, sal_uInt32 nPropType,
         sal_Int32 nStartAt = -1 ) const;
 
+    /** Returns the index of an entry with the given XML-name and namespace
+        If there is no matching entry the method returns -1 */
+    sal_Int32 GetEntryIndex(
+        sal_Int32 nElement, sal_uInt32 nPropType,
+        sal_Int32 nStartAt = -1 ) const;
+
     /** Retrieves a PropertyHandler for that property which placed at nIndex in the XMLPropertyMapEntry-array */
     const XMLPropertyHandler* GetPropertyHandler( sal_Int32 nIndex ) const;
 

sc/source/filter/xml/xmlstyli.cxx
@@ -273,11 +273,12 @@ class XMLTableCellPropsContext : public SvXMLPropertySetContext
              ::std::vector< XMLPropertyState > &rProps,
              const rtl::Reference < SvXMLImportPropertyMapper > &rMap);
 
-        virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix,
-            const OUString& rLocalName,
-            const uno::Reference< xml::sax::XAttributeList >& xAttrList,
-           ::std::vector< XMLPropertyState > &rProperties,
-           const XMLPropertyState& rProp ) override;
+    using SvXMLPropertySetContext::createFastChildContext;
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > createFastChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList,
+        ::std::vector< XMLPropertyState > &rProperties,
+        const XMLPropertyState& rProp ) override;
 };
 
 }
@@ -293,33 +294,25 @@ XMLTableCellPropsContext::XMLTableCellPropsContext(
 {
 }
 
-SvXMLImportContextRef XMLTableCellPropsContext::CreateChildContext( sal_uInt16 nPrefix,
-            const OUString& rLocalName,
-            const uno::Reference< xml::sax::XAttributeList >& xAttrList,
-           ::std::vector< XMLPropertyState > &rProperties,
-           const XMLPropertyState& rProp )
+css::uno::Reference< css::xml::sax::XFastContextHandler > XMLTableCellPropsContext::createFastChildContext(
+    sal_Int32 nElement,
+    const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList,
+    ::std::vector< XMLPropertyState > &rProperties,
+    const XMLPropertyState& rProp )
 {
     // no need for a custom context or indeed a SvXMLTokenMap to grab just the
     // single attribute ( href ) that we are interested in.
     // still though, we will check namespaces etc.
-    if ((XML_NAMESPACE_STYLE == nPrefix || XML_NAMESPACE_LO_EXT == nPrefix)
-        && IsXMLToken(rLocalName, XML_HYPERLINK))
+    if (nElement == XML_ELEMENT(STYLE, XML_HYPERLINK) ||
+        nElement == XML_ELEMENT(LO_EXT, XML_HYPERLINK) )
     {
         OUString sURL;
-        for ( int i=0; i<xAttrList->getLength(); ++i )
+        for (auto &aIter : sax_fastparser::castToFastAttributeList( xAttrList ))
         {
-            OUString aLocalName;
-            OUString sName = xAttrList->getNameByIndex(i);
-            sal_uInt16 nPrfx = GetImport().GetNamespaceMap().GetKeyByAttrName( sName,
-                                                            &aLocalName );
-            if ( nPrfx == XML_NAMESPACE_XLINK )
-            {
-                if ( IsXMLToken( aLocalName, XML_HREF ) )
-                {
-                    sURL = xAttrList->getValueByIndex(i);
-                    break;
-                }
-            }
+            if ( aIter.getToken() == XML_ELEMENT(XLINK, XML_HREF) )
+                sURL = aIter.toString();
+            else
+                SAL_WARN("sc", "unknown attribute " << SvXMLImport::getPrefixAndNameFromToken(aIter.getToken()) << "=" << aIter.toString());
         }
         if ( !sURL.isEmpty() )
         {
@@ -328,7 +321,7 @@ SvXMLImportContextRef XMLTableCellPropsContext::CreateChildContext( sal_uInt16 n
             rProperties.push_back( aProp );
         }
     }
-    return SvXMLPropertySetContext::CreateChildContext( nPrefix, rLocalName, xAttrList, rProperties, rProp );
+    return SvXMLPropertySetContext::createFastChildContext( nElement, xAttrList, rProperties, rProp );
 }
 
 namespace {

xmloff/inc/XMLBackgroundImageContext.hxx
@@ -43,15 +43,14 @@ class XMLBackgroundImageContext final : public XMLElementPropertyContext
 
 private:
     void ProcessAttrs(
-            const css::uno::Reference<css::xml::sax::XAttributeList > & xAttrList );
+            const css::uno::Reference<css::xml::sax::XFastAttributeList > & xAttrList );
 
 public:
 
     XMLBackgroundImageContext(
         SvXMLImport& rImport,
-        sal_uInt16 nPrfx,
-        const OUString& rLName,
-        const css::uno::Reference<css::xml::sax::XAttributeList > & xAttrList,
+        sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList > & xAttrList,
         const XMLPropertyState& rProp,
         sal_Int32 nPosIdx,
         sal_Int32 nFilterIdx,
@@ -65,7 +64,9 @@ public:
                 const OUString& rLocalName,
                  const css::uno::Reference<css::xml::sax::XAttributeList > & xAttrList ) override;
 
-    virtual void EndElement() override;
+    virtual void SAL_CALL startFastElement( sal_Int32 /*nElement*/,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& ) override {}
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 };
 
 

xmloff/inc/XMLElementPropertyContext.hxx
@@ -39,14 +39,16 @@ protected:
 
 public:
 
-    XMLElementPropertyContext( SvXMLImport& rImport, sal_uInt16 nPrfx,
-                               const OUString& rLName,
+    XMLElementPropertyContext( SvXMLImport& rImport, sal_Int32 nElement,
                                const XMLPropertyState& rProp,
                                 ::std::vector< XMLPropertyState > &rProps );
 
     virtual ~XMLElementPropertyContext() override;
 
-    virtual void EndElement() override;
+    virtual void SAL_CALL startFastElement( sal_Int32 /*nElement*/,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& ) override {}
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
+
 };
 
 

xmloff/inc/XMLShapePropertySetContext.hxx
@@ -30,12 +30,6 @@ class XMLShapePropertySetContext : public SvXMLPropertySetContext
 public:
 
 
-    XMLShapePropertySetContext( SvXMLImport& rImport, sal_uInt16 nPrfx,
-                const OUString& rLName,
-                 const css::uno::Reference<css::xml::sax::XAttributeList >& xAttrList,
-                 sal_uInt32 nFam,
-                 ::std::vector< XMLPropertyState > &rProps,
-                 const rtl::Reference < SvXMLImportPropertyMapper > &rMap );
     XMLShapePropertySetContext( SvXMLImport& rImport, sal_Int32 nElement,
                  const css::uno::Reference<css::xml::sax::XFastAttributeList >& xAttrList,
                  sal_uInt32 nFam,
@@ -44,14 +38,14 @@ public:
 
     virtual ~XMLShapePropertySetContext() override;
 
-    virtual void EndElement() override;
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 
-    using SvXMLPropertySetContext::CreateChildContext;
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix,
-                                   const OUString& rLocalName,
-                                   const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList,
-                                   ::std::vector< XMLPropertyState > &rProperties,
-                                   const XMLPropertyState& rProp) override;
+    using SvXMLPropertySetContext::createFastChildContext;
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > createFastChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList,
+        ::std::vector< XMLPropertyState > &rProperties,
+        const XMLPropertyState& rProp ) override;
 };
 
 #endif

xmloff/inc/XMLTextColumnsContext.hxx
@@ -45,9 +45,8 @@ class XMLTextColumnsContext final :public XMLElementPropertyContext
 public:
 
     XMLTextColumnsContext(
-        SvXMLImport& rImport, sal_uInt16 nPrfx,
-        const OUString& rLName,
-        const css::uno::Reference<css::xml::sax::XAttributeList > & xAttrList,
+        SvXMLImport& rImport, sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList > & xAttrList,
         const XMLPropertyState& rProp,
         ::std::vector< XMLPropertyState > &rProps );
 
@@ -55,7 +54,9 @@ public:
                                    const OUString& rLocalName,
                                    const css::uno::Reference< css::xml::sax::XAttributeList > & xAttrList ) override;
 
-    virtual void EndElement() override;
+    virtual void SAL_CALL startFastElement( sal_Int32 /*nElement*/,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& ) override {}
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 };
 
 

commit ba0439c2475ea63def4a2551230a9fa9d110c8f6

tdf#145178 xmloff: ODF import: fix another bad attribute
...in XMLSectionFootnoteConfigImport.

(regression from commit b19b50c302f8e9167b3d8430930892ea0bfefd43)

Change-Id: I79ab3b74853bd3ec9058fea72c341768e916bbd3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135048
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

xmloff/source/text/XMLSectionFootnoteConfigImport.cxx
@@ -113,7 +113,7 @@ void XMLSectionFootnoteConfigImport::startFastElement(
                 bNumOwn = true;
                 break;
             }
-            case XML_ELEMENT(TEXT, XML_NUM_LETTER_SYNC):
+            case XML_ELEMENT(STYLE, XML_NUM_LETTER_SYNC):
             {
                 sNumLetterSync = aIter.toString();
                 bNumOwn = true;