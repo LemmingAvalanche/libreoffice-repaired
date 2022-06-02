LemmingAvalanche/libreoffice-repaired

commit e613ce28f6468ad0efc41e6a394602edfb56547e

use more FastAttributeIter::toView
Change-Id: I8a8ad5456fea349a45fca0aa468313cb04aa02f5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133198
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmloff/source/style/xmltabi.cxx
@@ -93,7 +93,7 @@ SvxXMLTabStopContext_Impl::SvxXMLTabStopContext_Impl(
             break;
         case XML_ELEMENT(STYLE, XML_CHAR):
             if( !aIter.isEmpty() )
-                aTabStop.DecimalChar = aIter.toString()[0];
+                aTabStop.DecimalChar = aIter.toView()[0];
             break;
         case XML_ELEMENT(STYLE, XML_LEADER_STYLE):
             if( IsXMLToken( aIter, XML_NONE ) )
@@ -105,7 +105,7 @@ SvxXMLTabStopContext_Impl::SvxXMLTabStopContext_Impl(
             break;
         case XML_ELEMENT(STYLE, XML_LEADER_TEXT):
             if( !aIter.isEmpty() )
-                cTextFillChar = aIter.toString()[0];
+                cTextFillChar = aIter.toView()[0];
             break;
         default:
             XMLOFF_WARN_UNKNOWN("xmloff", aIter);

include/sax/tools/converter.hxx
@@ -222,6 +222,10 @@ public:
     static bool convertDuration(css::util::Duration& rDuration,
                         std::u16string_view rString);
 
+    /** convert XMLSchema-2 "duration" string to util::Duration */
+    static bool convertDuration(css::util::Duration& rDuration,
+                        std::string_view rString);
+
     /** convert util::Date to XMLSchema-2 "date" string */
     static void convertDate( OUStringBuffer& rBuffer,
                     const css::util::Date& rDate,

sax/source/tools/converter.cxx
@@ -1216,8 +1216,9 @@ readUnsignedNumberMaxDigits(int maxDigits,
     return bOverflow ? R_OVERFLOW : R_SUCCESS;
 }
 
+template<typename V>
 static bool
-readDurationT(std::u16string_view rString, size_t & io_rnPos)
+readDurationT(V rString, size_t & io_rnPos)
 {
     if ((io_rnPos < rString.size()) &&
         (rString[io_rnPos] == 'T' || rString[io_rnPos] == 't'))
@@ -1228,8 +1229,9 @@ readDurationT(std::u16string_view rString, size_t & io_rnPos)
     return false;
 }
 
+template<typename V>
 static bool
-readDurationComponent(std::u16string_view rString,
+readDurationComponent(V rString,
     size_t & io_rnPos, sal_Int32 & io_rnTemp, bool & io_rbTimePart,
     sal_Int32 & o_rnTarget, const sal_Unicode cLower, const sal_Unicode cUpper)
 {
@@ -1418,6 +1420,165 @@ bool Converter::convertDuration(util::Duration& rDuration,
     return bSuccess;
 }
 
+/** convert ISO8601 "duration" string to util::Duration */
+bool Converter::convertDuration(util::Duration& rDuration,
+                                std::string_view rString)
+{
+    std::string_view string = trim(rString);
+    size_t nPos(0);
+
+    bool bIsNegativeDuration(false);
+    if (!string.empty() && ('-' == string[0]))
+    {
+        bIsNegativeDuration = true;
+        ++nPos;
+    }
+
+    if (nPos < string.size()
+        && string[nPos] != 'P' && string[nPos] != 'p') // duration must start with "P"
+    {
+        return false;
+    }
+
+    ++nPos;
+
+    /// last read number; -1 == no valid number! always reset after using!
+    sal_Int32 nTemp(-1);
+    bool bTimePart(false); // have we read 'T'?
+    bool bSuccess(false);
+    sal_Int32 nYears(0);
+    sal_Int32 nMonths(0);
+    sal_Int32 nDays(0);
+    sal_Int32 nHours(0);
+    sal_Int32 nMinutes(0);
+    sal_Int32 nSeconds(0);
+    sal_Int32 nNanoSeconds(0);
+
+    bTimePart = readDurationT(string, nPos);
+    bSuccess = (R_SUCCESS == readUnsignedNumber(string, nPos, nTemp));
+
+    if (!bTimePart && bSuccess)
+    {
+        bSuccess = readDurationComponent(string, nPos, nTemp, bTimePart,
+                     nYears, 'y', 'Y');
+    }
+
+    if (!bTimePart && bSuccess)
+    {
+        bSuccess = readDurationComponent(string, nPos, nTemp, bTimePart,
+                     nMonths, 'm', 'M');
+    }
+
+    if (!bTimePart && bSuccess)
+    {
+        bSuccess = readDurationComponent(string, nPos, nTemp, bTimePart,
+                     nDays, 'd', 'D');
+    }
+
+    if (bTimePart)
+    {
+        if (-1 == nTemp) // a 'T' must be followed by a component
+        {
+            bSuccess = false;
+        }
+
+        if (bSuccess)
+        {
+            bSuccess = readDurationComponent(string, nPos, nTemp, bTimePart,
+                         nHours, 'h', 'H');
+        }
+
+        if (bSuccess)
+        {
+            bSuccess = readDurationComponent(string, nPos, nTemp, bTimePart,
+                         nMinutes, 'm', 'M');
+        }
+
+        // eeek! seconds are icky.
+        if ((nPos < string.size()) && bSuccess)
+        {
+            if (string[nPos] == '.' ||
+                string[nPos] == ',')
+            {
+                ++nPos;
+                if (-1 != nTemp)
+                {
+                    nSeconds = nTemp;
+                    nTemp = -1;
+                    const sal_Int32 nStart(nPos);
+                    bSuccess = readUnsignedNumberMaxDigits(9, string, nPos, nTemp) == R_SUCCESS;
+                    if ((nPos < string.size()) && bSuccess)
+                    {
+                        if (-1 != nTemp)
+                        {
+                            nNanoSeconds = nTemp;
+                            sal_Int32 nDigits = nPos - nStart;
+                            assert(nDigits >= 0);
+                            for (; nDigits < 9; ++nDigits)
+                            {
+                                nNanoSeconds *= 10;
+                            }
+                            nTemp=-1;
+                            if ('S' == string[nPos] || 's' == string[nPos])
+                            {
+                                ++nPos;
+                            }
+                            else
+                            {
+                                bSuccess = false;
+                            }
+                        }
+                        else
+                        {
+                            bSuccess = false;
+                        }
+                    }
+                }
+                else
+                {
+                    bSuccess = false;
+                }
+            }
+            else if ('S' == string[nPos] || 's' == string[nPos])
+            {
+                ++nPos;
+                if (-1 != nTemp)
+                {
+                    nSeconds = nTemp;
+                    nTemp = -1;
+                }
+                else
+                {
+                    bSuccess = false;
+                }
+            }
+        }
+    }
+
+    if (nPos != string.size()) // string not processed completely?
+    {
+        bSuccess = false;
+    }
+
+    if (nTemp != -1) // unprocessed number?
+    {
+        bSuccess = false;
+    }
+
+    if (bSuccess)
+    {
+        rDuration.Negative      = bIsNegativeDuration;
+        rDuration.Years         = static_cast<sal_Int16>(nYears);
+        rDuration.Months        = static_cast<sal_Int16>(nMonths);
+        rDuration.Days          = static_cast<sal_Int16>(nDays);
+        rDuration.Hours         = static_cast<sal_Int16>(nHours);
+        rDuration.Minutes       = static_cast<sal_Int16>(nMinutes);
+        rDuration.Seconds       = static_cast<sal_Int16>(nSeconds);
+        rDuration.NanoSeconds   = nNanoSeconds;
+    }
+
+    return bSuccess;
+}
 
 static void
 lcl_AppendTimezone(OUStringBuffer & i_rBuffer, int const nOffset)

sc/source/filter/xml/SparklineGroupsImportContext.cxx
@@ -32,29 +32,29 @@ SparklineGroupsImportContext::SparklineGroupsImportContext(ScXMLImport& rImport)
 
 namespace
 {
-sc::SparklineType parseSparklineType(std::u16string_view aString)
+sc::SparklineType parseSparklineType(std::string_view aString)
 {
-    if (aString == u"column")
+    if (aString == "column")
         return sc::SparklineType::Column;
-    else if (aString == u"stacked")
+    else if (aString == "stacked")
         return sc::SparklineType::Stacked;
     return sc::SparklineType::Line;
 }
 
-sc::DisplayEmptyCellsAs parseDisplayEmptyCellsAs(std::u16string_view aString)
+sc::DisplayEmptyCellsAs parseDisplayEmptyCellsAs(std::string_view aString)
 {
-    if (aString == u"span")
+    if (aString == "span")
         return sc::DisplayEmptyCellsAs::Span;
-    else if (aString == u"gap")
+    else if (aString == "gap")
         return sc::DisplayEmptyCellsAs::Gap;
     return sc::DisplayEmptyCellsAs::Zero;
 }
 
-sc::AxisType parseAxisType(std::u16string_view aString)
+sc::AxisType parseAxisType(std::string_view aString)
 {
-    if (aString == u"group")
+    if (aString == "group")
         return sc::AxisType::Group;
-    else if (aString == u"custom")
+    else if (aString == "custom")
         return sc::AxisType::Custom;
     return sc::AxisType::Individual;
 }
@@ -70,8 +70,7 @@ void SparklineGroupsImportContext::fillSparklineGroupID(
         {
             case XML_ELEMENT(CALC_EXT, XML_ID):
             {
-                OString aString = OUStringToOString(rIter.toString(), RTL_TEXTENCODING_ASCII_US);
-                tools::Guid aGuid(aString);
+                tools::Guid aGuid(rIter.toView());
                 m_pCurrentSparklineGroup->setID(aGuid);
                 break;
             }
@@ -90,7 +89,7 @@ void SparklineGroupsImportContext::fillSparklineGroupAttributes(
         {
             case XML_ELEMENT(CALC_EXT, XML_TYPE):
             {
-                rAttributes.setType(parseSparklineType(rIter.toString()));
+                rAttributes.setType(parseSparklineType(rIter.toView()));
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_LINE_WIDTH):
@@ -111,7 +110,7 @@ void SparklineGroupsImportContext::fillSparklineGroupAttributes(
             }
             case XML_ELEMENT(CALC_EXT, XML_DISPLAY_EMPTY_CELLS_AS):
             {
-                auto eDisplayEmptyCellsAs = parseDisplayEmptyCellsAs(rIter.toString());
+                auto eDisplayEmptyCellsAs = parseDisplayEmptyCellsAs(rIter.toView());
                 rAttributes.setDisplayEmptyCellsAs(eDisplayEmptyCellsAs);
                 break;
             }
@@ -157,12 +156,12 @@ void SparklineGroupsImportContext::fillSparklineGroupAttributes(
             }
             case XML_ELEMENT(CALC_EXT, XML_MIN_AXIS_TYPE):
             {
-                rAttributes.setMinAxisType(parseAxisType(rIter.toString()));
+                rAttributes.setMinAxisType(parseAxisType(rIter.toView()));
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_MAX_AXIS_TYPE):
             {
-                rAttributes.setMaxAxisType(parseAxisType(rIter.toString()));
+                rAttributes.setMaxAxisType(parseAxisType(rIter.toView()));
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_RIGHT_TO_LEFT):
@@ -183,56 +182,56 @@ void SparklineGroupsImportContext::fillSparklineGroupAttributes(
             case XML_ELEMENT(CALC_EXT, XML_COLOR_SERIES):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorSeries(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_NEGATIVE):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorNegative(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_AXIS):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorAxis(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_MARKERS):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorMarkers(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_FIRST):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorFirst(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_LAST):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorLast(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_HIGH):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorHigh(aColor);
                 break;
             }
             case XML_ELEMENT(CALC_EXT, XML_COLOR_LOW):
             {
                 Color aColor;
-                sax::Converter::convertColor(aColor, rIter.toString());
+                sax::Converter::convertColor(aColor, rIter.toView());
                 rAttributes.setColorLow(aColor);
                 break;
             }

sc/source/filter/xml/XMLCalculationSettingsContext.cxx
@@ -73,7 +73,7 @@ ScXMLCalculationSettingsContext::ScXMLCalculationSettingsContext( ScXMLImport& r
             break;
         case XML_ELEMENT( TABLE, XML_NULL_YEAR ):
             sal_Int32 nTemp;
-            ::sax::Converter::convertNumber( nTemp, aIter.toString() );
+            ::sax::Converter::convertNumber( nTemp, aIter.toView() );
             nYear2000 = static_cast<sal_uInt16>(nTemp);
             break;
         case XML_ELEMENT( TABLE, XML_USE_REGULAR_EXPRESSIONS ):
@@ -150,7 +150,7 @@ ScXMLNullDateContext::ScXMLNullDateContext( ScXMLImport& rImport,
     if (aIter != rAttrList->end())
     {
         util::DateTime aDateTime;
-        ::sax::Converter::parseDateTime(aDateTime, aIter.toString());
+        ::sax::Converter::parseDateTime(aDateTime, aIter.toView());
         util::Date aDate;
         aDate.Day = aDateTime.Day;
         aDate.Month = aDateTime.Month;

sc/source/filter/xml/XMLCellRangeSourceContext.cxx
@@ -63,7 +63,7 @@ ScXMLCellRangeSourceContext::ScXMLCellRangeSourceContext(
             case XML_ELEMENT( TABLE, XML_LAST_COLUMN_SPANNED ):
             {
                 sal_Int32 nValue;
-                if (::sax::Converter::convertNumber( nValue, aIter.toString(), 1 ))
+                if (::sax::Converter::convertNumber( nValue, aIter.toView(), 1 ))
                     pCellRangeSource->nColumns = nValue;
                 else
                     pCellRangeSource->nColumns = 1;
@@ -72,7 +72,7 @@ ScXMLCellRangeSourceContext::ScXMLCellRangeSourceContext(
             case XML_ELEMENT( TABLE, XML_LAST_ROW_SPANNED ):
             {
                 sal_Int32 nValue;
-                if (::sax::Converter::convertNumber( nValue, aIter.toString(), 1 ))
+                if (::sax::Converter::convertNumber( nValue, aIter.toView(), 1 ))
                     pCellRangeSource->nRows = nValue;
                 else
                     pCellRangeSource->nRows = 1;
@@ -81,7 +81,7 @@ ScXMLCellRangeSourceContext::ScXMLCellRangeSourceContext(
             case XML_ELEMENT( TABLE, XML_REFRESH_DELAY ):
             {
                 double fTime;
-                if (::sax::Converter::convertDuration( fTime, aIter.toString() ))
+                if (::sax::Converter::convertDuration( fTime, aIter.toView() ))
                     pCellRangeSource->nRefresh = std::max( static_cast<sal_Int32>(fTime * 86400.0), sal_Int32(0) );
             }
             break;

sc/source/filter/xml/XMLDetectiveContext.cxx
@@ -167,7 +167,7 @@ ScXMLDetectiveOperationContext::ScXMLDetectiveOperationContext(
                 case XML_ELEMENT( TABLE, XML_INDEX ):
                 {
                     sal_Int32 nValue;
-                    if (::sax::Converter::convertNumber( nValue, aIter.toString(), 0 ))
+                    if (::sax::Converter::convertNumber( nValue, aIter.toView(), 0 ))
                         aDetectiveOp.nIndex = nValue;
                 }
                 break;

sc/source/filter/xml/XMLTableSourceContext.cxx
@@ -62,7 +62,7 @@ ScXMLTableSourceContext::ScXMLTableSourceContext( ScXMLImport& rImport,
             break;
         case XML_ELEMENT( TABLE, XML_REFRESH_DELAY ):
             double fTime;
-            if (::sax::Converter::convertDuration( fTime, aIter.toString() ))
+            if (::sax::Converter::convertDuration( fTime, aIter.toView() ))
                 nRefresh = std::max( static_cast<sal_Int32>(fTime * 86400.0), sal_Int32(0) );
             break;
         }

commit 17d13aeb5a2a4bd85f2ce18e7bcc82d31a82406c

tdf#148846 TOC: Character fill for tabulation is wrong
regression from
    commit e613ce28f6468ad0efc41e6a394602edfb56547e
    use more FastAttributeIter::toView

Change-Id: I510716d2e4c5535faefb3afb658827cca9b6c4fa
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135303
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmloff/source/style/xmltabi.cxx
@@ -93,7 +93,7 @@ SvxXMLTabStopContext_Impl::SvxXMLTabStopContext_Impl(
             break;
         case XML_ELEMENT(STYLE, XML_CHAR):
             if( !aIter.isEmpty() )
-                aTabStop.DecimalChar = aIter.toView()[0];
+                aTabStop.DecimalChar = aIter.toString()[0];
             break;
         case XML_ELEMENT(STYLE, XML_LEADER_STYLE):
             if( IsXMLToken( aIter, XML_NONE ) )
@@ -105,7 +105,7 @@ SvxXMLTabStopContext_Impl::SvxXMLTabStopContext_Impl(
             break;
         case XML_ELEMENT(STYLE, XML_LEADER_TEXT):
             if( !aIter.isEmpty() )
-                cTextFillChar = aIter.toView()[0];
+                cTextFillChar = aIter.toString()[0];
             break;
         default:
             XMLOFF_WARN_UNKNOWN("xmloff", aIter);