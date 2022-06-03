LemmingAvalanche/libreoffice-repaired

commit 68ef41d488bc72ae69ad35defde2a2254109e9b7

tdf#144565 sw_redlinehide: fix mailmerge when flys anchored at last node
The InsertPageBreak() calls SplitNode() which is not ideal as the flys
anchored at the last node of the document may end up anchored to the
newly inserted node and this one will be removed again a bit further on:
  GetNodes().Delete( aDelIdx, iDelNodes );

... which is what crashes, when the SwNodeIndex of the anchor is moved
hard to a different node, which causes inconsistencies such as:

sw/source/core/text/txtfrm.cxx:1263: TextFrameIndex SwTextFrame::MapModelToView(const SwTextNode*, sal_Int32) const: Assertion `static_cast<SwTextNode*>(const_cast<sw::BroadcastingModify*>(SwFrame::GetDep())) == pNode' failed.

Instead, always use AppendTextNode() and then set the break item
directly, which even simplifies the code.

(reportedly a regression from 35fd08e3093ac6a6b1ff1954073d6943b6e7b08f
 although i wasn't able to find an earlier version that didn't crash
 in some way)

Change-Id: I4cac74fc86fc505f62b14cf0d7a7f9689c7402ba
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126921
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/doc/docnew.cxx
@@ -1085,19 +1085,14 @@ SwNodeIndex SwDoc::AppendDoc(const SwDoc& rSource, sal_uInt16 const nStartPageNu
         {
             SwNodeIndex aBreakIdx( GetNodes().GetEndOfContent(), -1 );
             SwPosition aBreakPos( aBreakIdx );
-            // InsertPageBreak just works on SwTextNode nodes, so make
-            // sure the last node is one!
-            bool bIsTextNode = aBreakIdx.GetNode().IsTextNode();
-            if ( !bIsTextNode )
-                getIDocumentContentOperations().AppendTextNode( aBreakPos );
-            const OUString name = pTargetPageDesc->GetName();
-            pTargetShell->InsertPageBreak( &name, nStartPageNumber );
-            if ( !bIsTextNode )
-            {
-                pTargetShell->SttEndDoc( false );
-                --aBreakIdx;
-                GetNodes().Delete( aBreakIdx );
-            }
+            // insert new node - will be removed at the end...
+            // (don't SplitNode() as it may move flys to the wrong node)
+            getIDocumentContentOperations().AppendTextNode(aBreakPos);
+            SwFormatPageDesc pageDesc(pTargetPageDesc);
+            pageDesc.SetNumOffset(nStartPageNumber);
+            // set break on the last paragraph
+            getIDocumentContentOperations().InsertPoolItem(SwPaM(aBreakPos),
+                    pageDesc, SetAttrMode::DEFAULT, pTargetShell->GetLayout());
 
             // There is now a new empty text node on the new page. If it has
             // any marks, those are from the previous page: move them back

commit 5343824750093547493255c8b4f118add00243c9

tdf#148309 sw_redlinehide: fix mail merge performance regression
The problem is that in the call to pTargetShell->EndAllAction(), the
cursor is on the node before the newly appended one that has the page
break, and only this node is formatted (via GetFormatted()), so no new
page frames are inserted in the layout, which then creates massive
performance problems later, particularly since the bugdoc contains
multiple at-page anchored flys.

(regression from commit 68ef41d488bc72ae69ad35defde2a2254109e9b7)

Change-Id: I05cd2a515a7f67132ab1c8c6fa0d675252ea3a15
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135356
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/doc/docnew.cxx
@@ -1099,6 +1099,11 @@ SwNodeIndex SwDoc::AppendDoc(const SwDoc& rSource, sal_uInt16 const nStartPageNu
             // set break on the last paragraph
             getIDocumentContentOperations().InsertPoolItem(SwPaM(aBreakPos),
                     pageDesc, SetAttrMode::DEFAULT, pTargetShell->GetLayout());
+            // tdf#148309 move to the last node - so that the "flush page break"
+            // code below will format the frame of the node with the page break,
+            // which is required for new page frames to be created!  Else layout
+            // performance will be terrible.
+            pTargetShell->SttEndDoc(false);
 
             // There is now a new empty text node on the new page. If it has
             // any marks, those are from the previous page: move them back
@@ -1129,6 +1134,7 @@ SwNodeIndex SwDoc::AppendDoc(const SwDoc& rSource, sal_uInt16 const nStartPageNu
             if ( !bDeletePrevious )
             {
                 SAL_INFO( "sw.pageframe", "(Flush pagebreak AKA EndAllAction" );
+                assert(pTargetShell->GetCursor()->GetPoint()->nNode.GetNode().GetTextNode()->GetSwAttrSet().HasItem(RES_PAGEDESC));
                 pTargetShell->EndAllAction();
                 SAL_INFO( "sw.pageframe",  "Flush changes AKA EndAllAction)" );
                 pTargetShell->StartAllAction();