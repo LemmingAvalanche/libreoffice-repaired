LemmingAvalanche/libreoffice-repaired

commit fd5b3e6e48eb4dc354dfbb9e4bb2517b0854c1ef

weld DataEditor
Change-Id: I8b59140d2e2b0892f17159282a1aeb1a47008ba0
Reviewed-on: https://gerrit.libreoffice.org/79561
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

chart2/source/controller/dialogs/dlg_DataEditor.cxx
@@ -25,6 +25,7 @@
 #include <vcl/taskpanelist.hxx>
 #include <svtools/miscopt.hxx>
 
+#include <com/sun/star/awt/XWindow.hpp>
 #include <com/sun/star/frame/XStorable.hpp>
 
 using namespace ::com::sun::star;
@@ -33,100 +34,70 @@ using ::com::sun::star::uno::Reference;
 namespace chart
 {
 
-DataEditor::DataEditor(vcl::Window* pParent,
+DataEditor::DataEditor(weld::Window* pParent,
     const Reference< chart2::XChartDocument > & xChartDoc,
     const Reference< uno::XComponentContext > & xContext)
-    : ModalDialog(pParent, "ChartDataDialog",
-        "modules/schart/ui/chartdatadialog.ui")
+    : GenericDialogController(pParent, "modules/schart/ui/chartdatadialog.ui", "ChartDataDialog")
     , m_bReadOnly(false)
     , m_xChartDoc(xChartDoc)
     , m_xContext(xContext)
+    , m_xTbxData(m_xBuilder->weld_toolbar("toolbar"))
+    , m_xCloseBtn(m_xBuilder->weld_button("close"))
+    , m_xTable(m_xBuilder->weld_container("datawindow"))
+    , m_xColumns(m_xBuilder->weld_container("columns"))
+    , m_xColors(m_xBuilder->weld_container("colorcolumns"))
+    , m_xTableCtrlParent(m_xTable->CreateChildFrame())
+    , m_xBrwData(VclPtr<DataBrowser>::Create(m_xTableCtrlParent, m_xColumns.get(), m_xColors.get()))
 {
-    m_xBrwData.reset(VclPtr<DataBrowser>::Create(get<vcl::Window>("datawindow"), WB_BORDER | WB_TABSTOP, true /* bLiveUpdate */));
-    m_xBrwData->set_hexpand(true);
-    m_xBrwData->set_vexpand(true);
-    m_xBrwData->set_expand(true);
-    Size aSize(m_xBrwData->LogicToPixel(Size(232, 121), MapMode(MapUnit::MapAppFont)));
-    m_xBrwData->set_width_request(aSize.Width());
-    m_xBrwData->set_height_request(aSize.Height());
+    m_xCloseBtn->connect_clicked(LINK(this, DataEditor, CloseHdl));
+
+    Size aSize(m_xTable->get_approximate_digit_width() * 75, m_xTable->get_text_height() * 15);
+    m_xTable->set_size_request(aSize.Width(), aSize.Height());
+
     m_xBrwData->Show();
 
-    get(m_pTbxData, "toolbar");
-
-    TBI_DATA_INSERT_ROW = m_pTbxData->GetItemId("InsertRow");
-    TBI_DATA_INSERT_COL = m_pTbxData->GetItemId("InsertColumn");
-    TBI_DATA_INSERT_TEXT_COL = m_pTbxData->GetItemId("InsertTextColumn");
-    TBI_DATA_DELETE_ROW = m_pTbxData->GetItemId("RemoveRow");
-    TBI_DATA_DELETE_COL = m_pTbxData->GetItemId("RemoveColumn");
-    TBI_DATA_MOVE_LEFT_COL = m_pTbxData->GetItemId("MoveLeftColumn");
-    TBI_DATA_MOVE_RIGHT_COL = m_pTbxData->GetItemId("MoveRightColumn");
-    TBI_DATA_MOVE_UP_ROW = m_pTbxData->GetItemId("MoveUpRow");
-    TBI_DATA_MOVE_DOWN_ROW = m_pTbxData->GetItemId("MoveDownRow");
-
-    m_pTbxData->SetSelectHdl( LINK( this, DataEditor, ToolboxHdl ));
+    m_xTbxData->connect_clicked(LINK(this, DataEditor, ToolboxHdl));
 
     m_xBrwData->SetCursorMovedHdl( LINK( this, DataEditor, BrowserCursorMovedHdl ));
 
     m_xBrwData->SetDataFromModel( m_xChartDoc, m_xContext );
-    GrabFocus();
-    m_xBrwData->GrabFocus();
+//    m_xDialog->grab_focus();
+//    m_xBrwData->GrabFocus();
 
     bool bReadOnly = true;
     Reference< frame::XStorable > xStor( m_xChartDoc, uno::UNO_QUERY );
     if( xStor.is())
         bReadOnly = xStor->isReadonly();
     SetReadOnly( bReadOnly );
-
-    // change buttons to flat-look if set so by user
-    SvtMiscOptions aMiscOptions;
-    const sal_Int16 nStyle( aMiscOptions.GetToolboxStyle() );
-    // react on changes
-    aMiscOptions.AddListenerLink( LINK( this, DataEditor, MiscHdl ) );
-    m_pTbxData->SetOutStyle( nStyle );
-
-    // allow travelling to toolbar with F6
-    notifySystemWindow( this, m_pTbxData, ::comphelper::mem_fun( & TaskPaneList::AddWindow ));
 }
 
 DataEditor::~DataEditor()
 {
-    disposeOnce();
-}
-
-void DataEditor::dispose()
-{
-    notifySystemWindow( this, m_pTbxData, ::comphelper::mem_fun( & TaskPaneList::RemoveWindow ));
-
-    SvtMiscOptions aMiscOptions;
-    aMiscOptions.RemoveListenerLink( LINK( this, DataEditor, MiscHdl ) );
-
-    m_pTbxData.clear();
     m_xBrwData.disposeAndClear();
-    ModalDialog::dispose();
+    m_xTableCtrlParent->dispose();
+    m_xTableCtrlParent.clear();
 }
 
 // react on click (or keypress) on toolbar icon
-IMPL_LINK_NOARG(DataEditor, ToolboxHdl, ToolBox *, void)
+IMPL_LINK(DataEditor, ToolboxHdl, const OString&, rId, void)
 {
-    sal_uInt16 nId = m_pTbxData->GetCurItemId();
-
-    if (nId == TBI_DATA_INSERT_ROW)
+    if (rId == "InsertRow")
         m_xBrwData->InsertRow();
-    else if (nId == TBI_DATA_INSERT_COL)
+    else if (rId == "InsertColumn")
         m_xBrwData->InsertColumn();
-    else if (nId == TBI_DATA_INSERT_TEXT_COL)
+    else if (rId == "InsertTextColumn")
         m_xBrwData->InsertTextColumn();
-    else if (nId == TBI_DATA_DELETE_ROW)
+    else if (rId == "RemoveRow")
         m_xBrwData->RemoveRow();
-    else if (nId == TBI_DATA_DELETE_COL)
+    else if (rId == "RemoveColumn")
         m_xBrwData->RemoveColumn();
-    else if (nId == TBI_DATA_MOVE_LEFT_COL)
+    else if (rId == "MoveLeftColumn")
         m_xBrwData->MoveLeftColumn();
-    else if (nId == TBI_DATA_MOVE_RIGHT_COL)
+    else if (rId == "MoveRightColumn")
         m_xBrwData->MoveRightColumn();
-    else if (nId == TBI_DATA_MOVE_UP_ROW)
+    else if (rId == "MoveUpRow")
         m_xBrwData->MoveUpRow();
-    else if (nId == TBI_DATA_MOVE_DOWN_ROW)
+    else if (rId == "MoveDownRow")
         m_xBrwData->MoveDownRow();
 }
 
@@ -138,16 +109,16 @@ IMPL_LINK_NOARG(DataEditor, BrowserCursorMovedHdl, DataBrowser*, void)
 
     bool bIsDataValid = m_xBrwData->IsEnableItem();
 
-    m_pTbxData->EnableItem( TBI_DATA_INSERT_ROW, bIsDataValid && m_xBrwData->MayInsertRow() );
-    m_pTbxData->EnableItem( TBI_DATA_INSERT_COL, bIsDataValid && m_xBrwData->MayInsertColumn() );
-    m_pTbxData->EnableItem( TBI_DATA_INSERT_TEXT_COL, bIsDataValid && m_xBrwData->MayInsertColumn() );
-    m_pTbxData->EnableItem( TBI_DATA_DELETE_ROW, m_xBrwData->MayDeleteRow() );
-    m_pTbxData->EnableItem( TBI_DATA_DELETE_COL, m_xBrwData->MayDeleteColumn() );
+    m_xTbxData->set_item_sensitive("InsertRow", bIsDataValid && m_xBrwData->MayInsertRow() );
+    m_xTbxData->set_item_sensitive("InsertColumn", bIsDataValid && m_xBrwData->MayInsertColumn() );
+    m_xTbxData->set_item_sensitive("InsertTextColumn", bIsDataValid && m_xBrwData->MayInsertColumn() );
+    m_xTbxData->set_item_sensitive("RemoveRow", m_xBrwData->MayDeleteRow() );
+    m_xTbxData->set_item_sensitive("RemoveColumn", m_xBrwData->MayDeleteColumn() );
 
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_LEFT_COL,   bIsDataValid && m_xBrwData->MayMoveLeftColumns() );
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_RIGHT_COL,   bIsDataValid && m_xBrwData->MayMoveRightColumns() );
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_DOWN_ROW,   bIsDataValid && m_xBrwData->MayMoveDownRows() );
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_UP_ROW,   bIsDataValid && m_xBrwData->MayMoveUpRows() );
+    m_xTbxData->set_item_sensitive("MoveLeftColumn", bIsDataValid && m_xBrwData->MayMoveLeftColumns() );
+    m_xTbxData->set_item_sensitive("MoveRightColumn", bIsDataValid && m_xBrwData->MayMoveRightColumns() );
+    m_xTbxData->set_item_sensitive("MoveUpRow", bIsDataValid && m_xBrwData->MayMoveDownRows() );
+    m_xTbxData->set_item_sensitive("MoveDownRow", bIsDataValid && m_xBrwData->MayMoveUpRows() );
 }
 
 // disable all modifying controls
@@ -156,61 +127,30 @@ void DataEditor::SetReadOnly( bool bReadOnly )
     m_bReadOnly = bReadOnly;
     if( m_bReadOnly )
     {
-        m_pTbxData->EnableItem( TBI_DATA_INSERT_ROW, false );
-        m_pTbxData->EnableItem( TBI_DATA_INSERT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_INSERT_TEXT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_DELETE_ROW, false );
-        m_pTbxData->EnableItem( TBI_DATA_DELETE_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_LEFT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_RIGHT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_UP_ROW, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_DOWN_ROW, false );
+        m_xTbxData->set_item_sensitive("InsertRow", false);
+        m_xTbxData->set_item_sensitive("InsertColumn", false);
+        m_xTbxData->set_item_sensitive("InsertTextColumn", false);
+        m_xTbxData->set_item_sensitive("RemoveRow", false);
+        m_xTbxData->set_item_sensitive("RemoveColumn", false);
+        m_xTbxData->set_item_sensitive("MoveLeftColumn", false);
+        m_xTbxData->set_item_sensitive("MoveRightColumn", false);
+        m_xTbxData->set_item_sensitive("MoveUpRow", false);
+        m_xTbxData->set_item_sensitive("MoveDownRow", false);
     }
 
     m_xBrwData->SetReadOnly( m_bReadOnly );
 }
 
-IMPL_LINK_NOARG(DataEditor, MiscHdl, LinkParamNone*, void)
+IMPL_LINK_NOARG(DataEditor, CloseHdl, weld::Button&, void)
 {
-    SvtMiscOptions aMiscOptions;
-    sal_Int16 nStyle( aMiscOptions.GetToolboxStyle() );
-
-    m_pTbxData->SetOutStyle( nStyle );
-}
-
-bool DataEditor::Close()
-{
-    if( ApplyChangesToModel() )
-        return ModalDialog::Close();
-    else
-        return true;
+    bool bApplied = ApplyChangesToModel();
+    if (bApplied)
+        m_xDialog->response(RET_CLOSE);
 }
 
 bool DataEditor::ApplyChangesToModel()
 {
     return m_xBrwData->EndEditing();
-}
-
-// add/remove a window (the toolbar) to/from the global list, so that F6
-// travels/no longer travels over this window.  _rMemFunc may be
-// TaskPaneList::AddWindow or TaskPaneList::RemoveWindow
-void DataEditor::notifySystemWindow(
-    vcl::Window const * pWindow, vcl::Window* pToRegister,
-    const ::comphelper::mem_fun1_t<TaskPaneList, vcl::Window*>& rMemFunc )
-{
-    OSL_ENSURE( pWindow, "Window must not be null!" );
-    if( !pWindow )
-        return;
-    vcl::Window* pParent = pWindow->GetParent();
-    while( pParent && ! pParent->IsSystemWindow() )
-    {
-        pParent = pParent->GetParent();
-    }
-    if ( pParent && pParent->IsSystemWindow())
-    {
-        SystemWindow* pSystemWindow = static_cast< SystemWindow* >( pParent );
-        rMemFunc( pSystemWindow->GetTaskPaneList(), pToRegister );
-    }
 }
 
 } // namespace chart

chart2/UIConfig_chart2.mk
@@ -40,9 +40,11 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/schart,\
 	chart2/uiconfig/ui/chardialog \
 	chart2/uiconfig/ui/chartdatadialog \
 	chart2/uiconfig/ui/charttypedialog \
+	chart2/uiconfig/ui/columnfragment \
 	chart2/uiconfig/ui/datarangedialog \
 	chart2/uiconfig/ui/dlg_DataLabel \
 	chart2/uiconfig/ui/dlg_InsertErrorBars \
+	chart2/uiconfig/ui/imagefragment \
 	chart2/uiconfig/ui/insertaxisdlg \
 	chart2/uiconfig/ui/insertgriddlg \
 	chart2/uiconfig/ui/inserttitledlg \

chart2/source/controller/dialogs/DataBrowser.hxx
@@ -22,11 +22,15 @@
 
 #include <svtools/editbrowsebox.hxx>
 #include <vcl/fmtfield.hxx>
+#include <vcl/weld.hxx>
 
 #include <memory>
 #include <vector>
 
 namespace com { namespace sun { namespace star {
+    namespace awt {
+        class XWindow;
+    }
     namespace chart2 {
         class XChartDocument;
     }
@@ -66,7 +70,9 @@ protected:
     virtual void MouseButtonDown( const BrowserMouseEvent& rEvt ) override;
 
 public:
-    DataBrowser( vcl::Window* pParent, WinBits nStyle, bool bLiveUpdate );
+    DataBrowser(const css::uno::Reference<css::awt::XWindow> &rParent,
+                weld::Container* pColumns, weld::Container* pColors);
+
     virtual ~DataBrowser() override;
     virtual void dispose() override;
 
@@ -157,6 +163,8 @@ private:
 
     VclPtr<FormattedField>      m_aNumberEditField;
     VclPtr<Edit>                m_aTextEditField;
+    weld::Container*            m_pColumnsWin;
+    weld::Container*            m_pColorsWin;
 
     /// note: m_aNumberEditField must precede this member!
     ::svt::CellControllerRef    m_rNumberEditController;
@@ -171,8 +179,8 @@ private:
 
     OUString GetColString( sal_Int32 nColumnId ) const;
 
-    DECL_LINK( SeriesHeaderGotFocus, Control&, void );
-    DECL_LINK( SeriesHeaderChanged,  impl::SeriesHeaderEdit*, void );
+    DECL_LINK( SeriesHeaderGotFocus, impl::SeriesHeaderEdit&, void );
+    DECL_LINK( SeriesHeaderChanged,  impl::SeriesHeaderEdit&, void );
 
     DataBrowser( const DataBrowser & ) = delete;
 };

chart2/source/controller/inc/dlg_DataEditor.hxx
@@ -20,8 +20,7 @@
 #ifndef INCLUDED_CHART2_SOURCE_CONTROLLER_INC_DLG_DATAEDITOR_HXX
 #define INCLUDED_CHART2_SOURCE_CONTROLLER_INC_DLG_DATAEDITOR_HXX
 
-#include <vcl/toolbox.hxx>
-#include <vcl/dialog.hxx>
+#include <vcl/weld.hxx>
 
 namespace com { namespace sun { namespace star { namespace uno { class XComponentContext; } } } }
 namespace comphelper { template <class Tp, class Arg> class mem_fun1_t; }
@@ -37,62 +36,37 @@ namespace chart
 
 class DataBrowser;
 
-class DataEditor : public ModalDialog
+class DataEditor : public weld::GenericDialogController
 {
 public:
-    DataEditor( vcl::Window* pParent,
-                const css::uno::Reference< css::chart2::XChartDocument > & xChartDoc,
-                const css::uno::Reference< css::uno::XComponentContext > & xContext );
+    DataEditor(weld::Window* pParent,
+               const css::uno::Reference<css::chart2::XChartDocument> & xChartDoc,
+               const css::uno::Reference<css::uno::XComponentContext> & xContext);
     virtual ~DataEditor() override;
-    virtual void dispose() override;
 
-    // Dialog
-    virtual bool Close() override;
+    DECL_LINK(CloseHdl, weld::Button&, void);
 
     void SetReadOnly( bool bReadOnly );
     bool ApplyChangesToModel();
 
 private:
-    sal_uInt16 TBI_DATA_INSERT_ROW;
-    sal_uInt16 TBI_DATA_INSERT_COL;
-    sal_uInt16 TBI_DATA_INSERT_TEXT_COL;
-    sal_uInt16 TBI_DATA_DELETE_ROW;
-    sal_uInt16 TBI_DATA_DELETE_COL;
-    sal_uInt16 TBI_DATA_MOVE_LEFT_COL;
-    sal_uInt16 TBI_DATA_MOVE_RIGHT_COL;
-    sal_uInt16 TBI_DATA_MOVE_UP_ROW;
-    sal_uInt16 TBI_DATA_MOVE_DOWN_ROW;
-
     bool                           m_bReadOnly;
-    VclPtr<DataBrowser>            m_xBrwData;
-    VclPtr<ToolBox>                m_pTbxData;
-    css::uno::Reference< css::chart2::XChartDocument > m_xChartDoc;
-    css::uno::Reference< css::uno::XComponentContext >
-        m_xContext;
+
+    css::uno::Reference<css::chart2::XChartDocument> m_xChartDoc;
+    css::uno::Reference<css::uno::XComponentContext> m_xContext;
+
+    std::unique_ptr<weld::Toolbar> m_xTbxData;
+    std::unique_ptr<weld::Button> m_xCloseBtn;
+    std::unique_ptr<weld::Container> m_xTable;
+    std::unique_ptr<weld::Container> m_xColumns;
+    std::unique_ptr<weld::Container> m_xColors;
+    css::uno::Reference<css::awt::XWindow> m_xTableCtrlParent;
+    VclPtr<DataBrowser> m_xBrwData;
 
     /// handles actions of the toolbox
-    DECL_LINK( ToolboxHdl, ToolBox*, void );
+    DECL_LINK( ToolboxHdl, const OString&, void );
     /// is called, if the cursor of the table has moved
     DECL_LINK( BrowserCursorMovedHdl, DataBrowser*, void);
-    /// this is called if MiscOptions change, esp. High-Contrast mode
-    DECL_LINK( MiscHdl, LinkParamNone*, void );
-
-    /** notifySystemWindow adds or remove the given window pToRegister at the Systemwindow found when search pWindow.
-        @param  pWindow
-            The window which is used to search for the SystemWindow.
-        @param  pToRegister
-            The window which should be added or removed on the TaskPaneList.
-        @param  rMemFunc
-            The member function which should be called at the SystemWindow when found.
-            Possible values are:
-            ::comphelper::mem_fun(&TaskPaneList::AddWindow)
-            ::comphelper::mem_fun(&TaskPaneList::RemoveWindow)
-
-        @note this code is taken from dbaccess/source/ui/inc/UITools.hxx
-    */
-    static void notifySystemWindow(vcl::Window const * pWindow,
-                            vcl::Window* pToRegister,
-                            const ::comphelper::mem_fun1_t<TaskPaneList, vcl::Window*>& rMemFunc);
 };
 
 } // namespace chart

chart2/source/controller/main/ChartController_EditData.cxx
@@ -38,15 +38,14 @@ namespace chart
 void ChartController::executeDispatch_EditData()
 {
     Reference< chart2::XChartDocument > xChartDoc( getModel(), uno::UNO_QUERY );
-    if( xChartDoc.is())
+    if (xChartDoc.is())
     {
         SolarMutexGuard aSolarGuard;
         UndoLiveUpdateGuardWithData aUndoGuard(
             SchResId( STR_ACTION_EDIT_CHART_DATA ),
             m_xUndoManager );
-        ScopedVclPtrInstance<DataEditor> aDataEditorDialog( GetChartWindow().get(), xChartDoc, m_xCC );
-        if (aDataEditorDialog->Execute() == RET_OK)
-            aDataEditorDialog->ApplyChangesToModel();
+        DataEditor aDataEditorDialog(GetChartFrame(), xChartDoc, m_xCC);
+        aDataEditorDialog.run();
         aUndoGuard.commit();
     }
 }

chart2/uiconfig/ui/chartdatadialog.ui
@@ -1,12 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- Generated with glade 3.18.3 -->
+<!-- Generated with glade 3.22.1 -->
 <interface domain="chart">
   <requires lib="gtk+" version="3.18"/>
   <object class="GtkDialog" id="ChartDataDialog">
     <property name="can_focus">False</property>
     <property name="border_width">6</property>
     <property name="title" translatable="yes" context="chartdatadialog|ChartDataDialog">Data Table</property>
+    <property name="modal">True</property>
     <property name="type_hint">dialog</property>
+    <child>
+      <placeholder/>
+    </child>
     <child internal-child="vbox">
       <object class="GtkBox" id="dialog-vbox1">
         <property name="can_focus">False</property>
@@ -17,24 +21,8 @@
             <property name="can_focus">False</property>
             <property name="layout_style">end</property>
             <child>
-              <object class="GtkButton" id="ok">
-                <property name="label">gtk-ok</property>
-                <property name="visible">True</property>
-                <property name="can_focus">True</property>
-                <property name="can_default">True</property>
-                <property name="has_default">True</property>
-                <property name="receives_default">True</property>
-                <property name="use_stock">True</property>
-              </object>
-              <packing>
-                <property name="expand">False</property>
-                <property name="fill">True</property>
-                <property name="position">0</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkButton" id="cancel">
-                <property name="label">gtk-cancel</property>
+              <object class="GtkButton" id="close">
+                <property name="label">gtk-close</property>
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
                 <property name="receives_default">True</property>
@@ -84,7 +72,6 @@
                   <object class="GtkToolButton" id="InsertRow">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">InsertRow</property>
                     <property name="label" translatable="yes" context="chartdatadialog|InsertRow">Insert Row</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon01.png</property>
@@ -98,7 +85,6 @@
                   <object class="GtkToolButton" id="InsertColumn">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">InsertColumn</property>
                     <property name="label" translatable="yes" context="chartdatadialog|InsertColumn">Insert Series</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon02.png</property>
@@ -112,7 +98,6 @@
                   <object class="GtkToolButton" id="InsertTextColumn">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">InsertTextColumn</property>
                     <property name="label" translatable="yes" context="chartdatadialog|InsertTextColumn">Insert Text Column</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon07.png</property>
@@ -126,7 +111,6 @@
                   <object class="GtkToolButton" id="RemoveRow">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">RemoveRow</property>
                     <property name="label" translatable="yes" context="chartdatadialog|RemoveRow">Delete Row</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon03.png</property>
@@ -140,7 +124,6 @@
                   <object class="GtkToolButton" id="RemoveColumn">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">RemoveColumn</property>
                     <property name="label" translatable="yes" context="chartdatadialog|RemoveColumn">Delete Series</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon04.png</property>
@@ -162,9 +145,8 @@
                 </child>
                 <child>
                   <object class="GtkToolButton" id="MoveLeftColumn">
-                      <property name="visible">True</property>
+                    <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">MoveLeftColumn</property>
                     <property name="label" translatable="yes" context="chartdatadialog|MoveLeftColumn">Move Series Left</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon08.png</property>
@@ -178,7 +160,6 @@
                   <object class="GtkToolButton" id="MoveRightColumn">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">MoveRightColumn</property>
                     <property name="label" translatable="yes" context="chartdatadialog|MoveRightColumn">Move Series Right</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon05.png</property>
@@ -192,7 +173,6 @@
                   <object class="GtkToolButton" id="MoveUpRow">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">MoveUpRow</property>
                     <property name="label" translatable="yes" context="chartdatadialog|MoveUpRow">Move Row Up</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon09.png</property>
@@ -206,20 +186,9 @@
                   <object class="GtkToolButton" id="MoveDownRow">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="action_name">MoveDownRow</property>
                     <property name="label" translatable="yes" context="chartdatadialog|MoveDownRow">Move Row Down</property>
                     <property name="use_underline">True</property>
                     <property name="icon_name">chart2/res/dataeditor_icon06.png</property>
-                  </object>
-                  <packing>
-                    <property name="expand">False</property>
-                    <property name="homogeneous">True</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkSeparatorToolItem" id="toolbutton2">
-                    <property name="visible">True</property>
-                    <property name="can_focus">False</property>
                   </object>
                   <packing>
                     <property name="expand">False</property>
@@ -315,8 +284,7 @@
       </object>
     </child>
     <action-widgets>
-      <action-widget response="-5">ok</action-widget>
-      <action-widget response="-6">cancel</action-widget>
+      <action-widget response="-7">close</action-widget>
       <action-widget response="-11">help</action-widget>
     </action-widgets>
   </object>

chart2/uiconfig/ui/columnfragment.ui
@@ -1,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.22.1 -->
+<interface domain="chart">
+  <requires lib="gtk+" version="3.18"/>
+  <object class="GtkBox" id="container">
+    <property name="visible">True</property>
+    <property name="can_focus">False</property>
+    <child>
+      <object class="GtkImage" id="image">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="stock">gtk-missing-image</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">0</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkEntry" id="entry">
+        <property name="width_request">5</property>
+        <property name="visible">True</property>
+        <property name="can_focus">True</property>
+        <property name="width_chars">1</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">1</property>
+      </packing>
+    </child>
+  </object>
+</interface>

commit d3cce65be41346648b4cce0661d558685317ccdf

tdf#149536 Move Up/Down in Data Table switched enabled statuses
Regression from fd5b3e6e48eb4dc354dfbb9e4bb2517b0854c1ef.

Change-Id: Id2b66b2e87fdb13a55b32a4989cae2c7796fd003
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135801
Tested-by: Jenkins
Reviewed-by: Aron Budea <aron.budea@collabora.com>

chart2/source/controller/dialogs/dlg_DataEditor.cxx
@@ -112,8 +112,8 @@ IMPL_LINK_NOARG(DataEditor, BrowserCursorMovedHdl, DataBrowser*, void)
 
     m_xTbxData->set_item_sensitive("MoveLeftColumn", bIsDataValid && m_xBrwData->MayMoveLeftColumns() );
     m_xTbxData->set_item_sensitive("MoveRightColumn", bIsDataValid && m_xBrwData->MayMoveRightColumns() );
-    m_xTbxData->set_item_sensitive("MoveUpRow", bIsDataValid && m_xBrwData->MayMoveDownRows() );
-    m_xTbxData->set_item_sensitive("MoveDownRow", bIsDataValid && m_xBrwData->MayMoveUpRows() );
+    m_xTbxData->set_item_sensitive("MoveDownRow", bIsDataValid && m_xBrwData->MayMoveDownRows() );
+    m_xTbxData->set_item_sensitive("MoveUpRow", bIsDataValid && m_xBrwData->MayMoveUpRows() );
 }
 
 // disable all modifying controls