LemmingAvalanche/libreoffice-repaired

commit f53565d3403a6f7b0aa5e361189f218018496268

Replace some lists by vectors in unoobj (sc)
Change-Id: I611f1a217ff1d5468c77f04a0c2eddd61ee33b8b
Reviewed-on: https://gerrit.libreoffice.org/42025
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sc/inc/listenercalls.hxx
@@ -20,7 +20,7 @@
 #ifndef INCLUDED_SC_INC_LISTENERCALLS_HXX
 #define INCLUDED_SC_INC_LISTENERCALLS_HXX
 
-#include <list>
+#include <vector>
 #include <com/sun/star/uno/Reference.hxx>
 #include <com/sun/star/lang/EventObject.hpp>
 
@@ -52,7 +52,7 @@ struct ScUnoListenerEntry
 class ScUnoListenerCalls
 {
 private:
-    ::std::list<ScUnoListenerEntry> aEntries;
+    ::std::vector<ScUnoListenerEntry> aEntries;
 
 public:
                 ScUnoListenerCalls();

sc/source/ui/unoobj/listenercalls.cxx
@@ -48,9 +48,8 @@ void ScUnoListenerCalls::ExecuteAndClear()
 
     if (!aEntries.empty())
     {
-        std::list<ScUnoListenerEntry>::iterator aItr(aEntries.begin());
-        std::list<ScUnoListenerEntry>::iterator aEndItr(aEntries.end());
-        while ( aItr != aEndItr )
+        std::vector<ScUnoListenerEntry>::iterator aItr(aEntries.begin());
+        while (aItr != aEntries.end())
         {
             ScUnoListenerEntry aEntry = *aItr;
             try

sc/inc/unoreflist.hxx
@@ -20,7 +20,7 @@
 #ifndef INCLUDED_SC_INC_UNOREFLIST_HXX
 #define INCLUDED_SC_INC_UNOREFLIST_HXX
 
-#include <list>
+#include <vector>
 #include <svl/hint.hxx>
 #include "rangelst.hxx"
 
@@ -42,7 +42,7 @@ struct ScUnoRefEntry
 class ScUnoRefList
 {
 private:
-    ::std::list<ScUnoRefEntry> aEntries;
+    ::std::vector<ScUnoRefEntry> aEntries;
 
 public:
                 ScUnoRefList();

sc/source/ui/unoobj/unoreflist.cxx
@@ -35,11 +35,9 @@ void ScUnoRefList::Add( sal_Int64 nId, const ScRangeList& rOldRanges )
 
 void ScUnoRefList::Undo( ScDocument* pDoc )
 {
-    std::list<ScUnoRefEntry>::const_iterator aEnd( aEntries.end() );
-    for ( std::list<ScUnoRefEntry>::const_iterator aIter( aEntries.begin() );
-          aIter != aEnd; ++aIter )
+    for (auto & entry: aEntries)
     {
-        ScUnoRefUndoHint aHint( *aIter );
+        ScUnoRefUndoHint aHint(entry);
         pDoc->BroadcastUno( aHint );
     }
 }

commit b4124b30ab5d7dea038c62f81725c24a523b6c5f

Resolves: tdf#147822 ScUnoListenerEntry container must be std::list
... instead of std::vector to not get invalidated iterators when
the container is resized.

Regression from

    commit f53565d3403a6f7b0aa5e361189f218018496268
    CommitDate: Wed Sep 6 22:45:10 2017 +0200

        Replace some lists by vectors in unoobj (sc)

which was bad for this case.

Change-Id: I8d3a001242865cadc82b359a3198906d26373a41
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136007
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/inc/listenercalls.hxx
@@ -19,7 +19,7 @@
 
 #pragma once
 
-#include <vector>
+#include <list>
 #include <com/sun/star/uno/Reference.hxx>
 #include <com/sun/star/lang/EventObject.hpp>
 
@@ -48,7 +48,9 @@ struct ScUnoListenerEntry
 class ScUnoListenerCalls
 {
 private:
-    ::std::vector<ScUnoListenerEntry> aEntries;
+    // Must be list, not vector, to not invalidate iterators, see
+    // ExecuteAndClear() implementation.
+    ::std::list<ScUnoListenerEntry> aEntries;
 
 public:
                 ScUnoListenerCalls();

sc/source/ui/unoobj/listenercalls.cxx
@@ -44,7 +44,7 @@ void ScUnoListenerCalls::ExecuteAndClear()
     //  During each modified() call, Add may be called again.
     //  These new calls are executed here, too.
 
-    std::vector<ScUnoListenerEntry>::iterator aItr(aEntries.begin());
+    std::list<ScUnoListenerEntry>::iterator aItr(aEntries.begin());
     while (aItr != aEntries.end())
     {
         ScUnoListenerEntry aEntry = *aItr;