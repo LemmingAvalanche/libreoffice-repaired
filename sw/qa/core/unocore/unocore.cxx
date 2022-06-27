LemmingAvalanche/libreoffice-repaired

commit a743cb055f719e87520652b6efae9f0820d42b22

ODT export: omit unreferenced <text:list xml:id="...">
This attribute is needed when a numbering is built using multiple,
independent <text:list> elements. In that case the markup to connect
these are either:

	<text:list text:style-name="L1">
	</text:list>
	...
	<text:list text:continue-numbering="true" text:style-name="L1">
	</text:list>

In case there is no other list in-between, or:

	<text:list xml:id="..." text:style-name="L1">
	</text:list>
	...
	<text:list text:continue-list="..." text:style-name="L1">
	</text:list>

In case there are other lists in-between.

This means that at least in case all the text nodes of the numbering are
after each other, then the random value in xml:id="..." is never
referenced, so it can be omitted.

This helps deterministic ODF output when the input is HTML, where there
are never text:continue-list="..." attributes that would refer to these
xml:id="..." attributes.

Change-Id: Ice69422a12d4229879f89f3a4a24ed926c6d43af
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131322
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/unocore/unoparagraph.cxx
@@ -921,6 +921,22 @@ static beans::PropertyState lcl_SwXParagraph_getPropertyState(
             bDone = true;
             break;
         }
+        case FN_UNO_LIST_ID:
+        {
+            if (*ppSet)
+            {
+                if ((*ppSet)->GetItemState(RES_PARATR_LIST_ID, false) == SfxItemState::SET)
+                {
+                    SwNumRule* pNumRule = rTextNode.GetNumRule();
+                    if (!pNumRule || pNumRule->HasContinueList())
+                    {
+                        eRet = beans::PropertyState_DIRECT_VALUE;
+                    }
+                }
+                bDone = true;
+            }
+            break;
+        }
         case FN_UNO_ANCHOR_TYPES:
         {
             bDone = true;

sw/inc/numrule.hxx
@@ -273,6 +273,9 @@ public:
     void dumpAsXml(xmlTextWriterPtr w) const;
     void GetGrabBagItem(css::uno::Any& rVal) const;
     void SetGrabBagItem(const css::uno::Any& rVal);
+
+    /// Is it possible that this numbering has multiple lists?
+    bool HasContinueList() const;
 };
 
 /// namespace for static functions and methods for numbering and bullets

sw/source/core/doc/number.cxx
@@ -1154,6 +1154,29 @@ void SwNumRule::SetGrabBagItem(const uno::Any& rVal)
     mpGrabBagItem->PutValue(rVal, 0);
 }
 
+bool SwNumRule::HasContinueList() const
+{
+    // In case all text nodes are after each other, then we won't have a later list that wants to
+    // continue us.
+    SwNodeOffset nIndex(0);
+    for (size_t i = 0; i < maTextNodeList.size(); ++i)
+    {
+        SwTextNode* pNode = maTextNodeList[i];
+        if (i > 0)
+        {
+            if (pNode->GetIndex() != nIndex + 1)
+            {
+                // May have a continue list.
+                return true;
+            }
+        }
+        nIndex = pNode->GetIndex();
+    }
+
+    // Definitely won't have a continue list.
+    return false;
+}
+
 namespace numfunc
 {
     namespace {

xmloff/qa/unit/data/list-id.fodt
@@ -1,0 +1,23 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<office:document xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+  <office:automatic-styles>
+    <text:list-style style:name="L1">
+      <text:list-level-style-number text:level="1" text:style-name="Numbering_20_Symbols" style:num-suffix="." style:num-format="1"/>
+    </text:list-style>
+  </office:automatic-styles>
+  <office:body>
+    <office:text>
+      <text:list text:style-name="L1">
+        <text:list-item>
+          <text:p>First</text:p>
+        </text:list-item>
+        <text:list-item>
+          <text:p>Second</text:p>
+        </text:list-item>
+        <text:list-item>
+          <text:p>Third</text:p>
+        </text:list-item>
+      </text:list>
+    </office:text>
+  </office:body>
+</office:document>

xmloff/qa/unit/text.cxx
@@ -245,6 +245,30 @@ CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testContinueNumberingWord)
     CPPUNIT_ASSERT_EQUAL(OUString("2."), aActual);
 }
 
+CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testListId)
+{
+    // Given a document with a simple list (no continue-list="..." attribute):
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "list-id.fodt";
+    getComponent() = loadFromDesktop(aURL);
+
+    // When storing that document as ODF:
+    uno::Reference<frame::XStorable> xStorable(getComponent(), uno::UNO_QUERY);
+    uno::Sequence<beans::PropertyValue> aStoreProps = comphelper::InitPropertySequence({
+        { "FilterName", uno::makeAny(OUString("writer8")) },
+    });
+    utl::TempFile aTempFile;
+    aTempFile.EnableKillingFile();
+    xStorable->storeToURL(aTempFile.GetURL(), aStoreProps);
+
+    // Then make sure that unreferenced xml:id="..." attributes are not written:
+    std::unique_ptr<SvStream> pStream = parseExportStream(aTempFile, "content.xml");
+    xmlDocUniquePtr pXmlDoc = parseXmlStream(pStream.get());
+    // Without the accompanying fix in place, this failed with:
+    // - XPath '//text:list' unexpected 'id' attribute
+    // i.e. xml:id="..." was written unconditionally, even when no other list needed it.
+    assertXPathNoAttribute(pXmlDoc, "//text:list", "id");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

xmloff/source/text/XMLTextNumRuleInfo.cxx
@@ -21,6 +21,7 @@
 #include <osl/diagnose.h>
 #include <sal/log.hxx>
 #include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/beans/XPropertyState.hpp>
 #include <com/sun/star/beans/PropertyValue.hpp>
 #include <com/sun/star/container/XIndexReplace.hpp>
 #include <com/sun/star/style/NumberingType.hpp>
@@ -37,7 +38,8 @@ using namespace ::com::sun::star::style;
 
 // Complete refactoring of the class and enhancement of the class for lists.
 XMLTextNumRuleInfo::XMLTextNumRuleInfo()
-    : mnListStartValue( -1 )
+    : mbListIdIsDefault(false)
+    , mnListStartValue( -1 )
     , mnListLevel( 0 )
     , mbIsNumbered( false )
     , mbIsRestart( false )
@@ -59,6 +61,7 @@ void XMLTextNumRuleInfo::Set(
     mbOutlineStyleAsNormalListStyle = bOutlineStyleAsNormalListStyle;
 
     Reference< XPropertySet > xPropSet( xTextContent, UNO_QUERY );
+    Reference<XPropertyState> xPropState(xTextContent, UNO_QUERY);
     Reference< XPropertySetInfo > xPropSetInfo = xPropSet->getPropertySetInfo();
 
     // check if this paragraph supports a numbering
@@ -135,6 +138,12 @@ void XMLTextNumRuleInfo::Set(
         if( xPropSetInfo->hasPropertyByName( "ListId" ) )
         {
             xPropSet->getPropertyValue( "ListId" ) >>= msListId;
+
+            if (xPropState.is())
+            {
+                mbListIdIsDefault
+                    = xPropState->getPropertyState("ListId") == PropertyState_DEFAULT_VALUE;
+            }
         }
 
         mbContinueingPreviousSubTree = false;

xmloff/source/text/XMLTextNumRuleInfo.hxx
@@ -43,6 +43,8 @@ class XMLTextNumRuleInfo
 
     // paragraph's list attributes
     OUString            msListId;
+    /// msListId won't be referenced by later lists.
+    bool mbListIdIsDefault;
     sal_Int16           mnListStartValue;
     sal_Int16           mnListLevel;
     bool                mbIsNumbered;
@@ -82,6 +84,8 @@ public:
     {
         return msListId;
     }
+
+    bool IsListIdDefault() const { return mbListIdIsDefault; }
 
     sal_Int16 GetLevel() const
     {

xmloff/source/text/txtparae.cxx
@@ -932,7 +932,7 @@ void XMLTextParagraphExport::exportListChange(
                     {
                         if ( bExportODF &&
                             eODFDefaultVersion >= SvtSaveOptions::ODFSVER_012 &&
-                             !sListId.isEmpty() )
+                             !sListId.isEmpty() && !rNextInfo.IsListIdDefault() )
                         {
                             /* Property text:id at element <text:list> has to be
                                replaced by property xml:id (#i92221#)
@@ -951,7 +951,7 @@ void XMLTextParagraphExport::exportListChange(
                                         mpTextListsHelper->GenerateNewListId() );
                         if ( bExportODF &&
                             eODFDefaultVersion >= SvtSaveOptions::ODFSVER_012 &&
-                             !sListId.isEmpty() )
+                             !sListId.isEmpty() && !rNextInfo.IsListIdDefault() )
                         {
                             /* Property text:id at element <text:list> has to be
                                replaced by property xml:id (#i92221#)

commit 2f2e13b7ad00c2dfaabc5b424a17d904d83257a8

tdf#149668 sw: fix DOCX->ODT: Multilevel lists causing list numbering reset
The bug document had an outer (ordered) numbering and an inner one,
finally the outer numbering was continued. This was fine after DOCX
import, but once we saved to ODT and re-opened, the second outer
numbering was restarted, not continued the first outer one.

This happened because on one hand, lcl_SwXParagraph_getPropertyState()
says that the state of the ListId property is "default" if the
paragraph's item set doesn't have a RES_PARATR_LIST_ID, but on the other
hand xmloff/ will only write the xml:id="..." attribute on a list if the
ListId state is not default. This is a problem, because it can happen
that xmloff/ refers to a list later by its id, even if the list doesn't
have a RES_PARATR_LIST_ID set.

Fix the problem by relaxing the condition where
lcl_SwXParagraph_getPropertyState() reports FN_UNO_LIST_ID as
non-default: just require that that the text node has a numbering rule
+ the numbering rule has at least one continue list. This fixes the
reported DOCX -> ODT case with named list styles and keeps the original
ODT import and HTML import use-cases working to not write un-referenced
xml:id="..." attributes on export.

This was a regression from commit
a743cb055f719e87520652b6efae9f0820d42b22 (ODT export: omit unreferenced
<text:list xml:id="...">, 2022-03-10).

Change-Id: I300ebe7aa7da71a336c03abe7530074b696417f7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136477
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/unocore/unoparagraph.cxx
@@ -922,18 +922,12 @@ static beans::PropertyState lcl_SwXParagraph_getPropertyState(
         }
         case FN_UNO_LIST_ID:
         {
-            if (*ppSet)
+            SwNumRule* pNumRule = rTextNode.GetNumRule();
+            if (pNumRule && pNumRule->HasContinueList())
             {
-                if ((*ppSet)->GetItemState(RES_PARATR_LIST_ID, false) == SfxItemState::SET)
-                {
-                    SwNumRule* pNumRule = rTextNode.GetNumRule();
-                    if (!pNumRule || pNumRule->HasContinueList())
-                    {
-                        eRet = beans::PropertyState_DIRECT_VALUE;
-                    }
-                }
-                bDone = true;
+                eRet = beans::PropertyState_DIRECT_VALUE;
             }
+            bDone = true;
             break;
         }
         case FN_UNO_ANCHOR_TYPES:

sw/qa/core/unocore/unocore.cxx
@@ -639,6 +639,46 @@ CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testContentControlDate)
     CPPUNIT_ASSERT_EQUAL(OUString("008000"), pContentControl->GetColor());
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testListIdState)
+{
+    // Given a document with 3 paragraphs: an outer numbering on para 1 & 3, an inner numbering on
+    // para 2:
+    SwDoc* pDoc = createSwDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    {
+        SfxItemSetFixed<RES_PARATR_NUMRULE, RES_PARATR_NUMRULE> aSet(pWrtShell->GetAttrPool());
+        SwNumRuleItem aItem("Numbering ABC");
+        aSet.Put(aItem);
+        pWrtShell->SetAttrSet(aSet);
+    }
+    pWrtShell->SplitNode();
+    {
+        SfxItemSetFixed<RES_PARATR_NUMRULE, RES_PARATR_NUMRULE> aSet(pWrtShell->GetAttrPool());
+        SwNumRuleItem aItem("Numbering 123");
+        aSet.Put(aItem);
+        pWrtShell->SetAttrSet(aSet);
+    }
+    pWrtShell->SplitNode();
+    {
+        SfxItemSetFixed<RES_PARATR_NUMRULE, RES_PARATR_NUMRULE> aSet(pWrtShell->GetAttrPool());
+        SwNumRuleItem aItem("Numbering ABC");
+        aSet.Put(aItem);
+        pWrtShell->SetAttrSet(aSet);
+    }
+
+    // When checking if xml:id="..." needs writing for the first paragraph during ODT export:
+    uno::Reference<beans::XPropertyState> xPara(getParagraph(1), uno::UNO_QUERY);
+    beans::PropertyState eState = xPara->getPropertyState("ListId");
+
+    // Then make sure that xml:id="..." gets written for para 1, as it'll be continued in para 3.
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 0 (DIRECT_VALUE)
+    // - Actual  : 1 (DEFAULT_VALUE)
+    // i.e. para 1 didn't write an xml:id="..." but para 3 referred to it using continue-list="...",
+    // which is inconsistent.
+    CPPUNIT_ASSERT_EQUAL(beans::PropertyState_DIRECT_VALUE, eState);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */