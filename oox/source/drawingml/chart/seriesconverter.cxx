LemmingAvalanche/libreoffice-repaired

commit 3bd7abe023691a936a1e9eaba49dcb585309d4bd

replace oox::OptValue with std::optional
Change-Id: I16e7179b2851640b4d73665685dcc1e84042ddaf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136270
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

oox/source/drawingml/chart/seriesconverter.cxx
@@ -319,8 +319,8 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
             if( nParagraphs > 1 )
                 nSequenceSize += nParagraphs - 1;
 
-            OptValue< OUString > oaLabelText;
-            OptValue< OUString > oaCellRange;
+            std::optional< OUString > oaLabelText;
+            std::optional< OUString > oaCellRange;
             if (mrModel.mobShowDataLabelsRange.value_or(false))
             {
                 const DataSourceModel* pLabelSource = mrModel.mrParent.mpLabelsSource;

include/oox/drawingml/shape.hxx
@@ -179,7 +179,7 @@ public:
     void                            setSubType( sal_Int32 nSubType ) { mnSubType = nSubType; }
     sal_Int32                       getSubType() const { return mnSubType; }
     void                            setSubTypeIndex( sal_Int32 nSubTypeIndex ) { moSubTypeIndex = nSubTypeIndex; }
-    const OptValue< sal_Int32 >&    getSubTypeIndex() const { return moSubTypeIndex; }
+    const std::optional< sal_Int32 >& getSubTypeIndex() const { return moSubTypeIndex; }
 
     // setDefaults has to be called if styles are imported (OfficeXML is not storing properties having the default value)
     void                            setDefaults(bool bHeight);
@@ -353,7 +353,7 @@ protected:
     OUString                    msId;
     OUString                    msDescription;
     sal_Int32                   mnSubType;      // if this type is not zero, then the shape is a placeholder
-    OptValue< sal_Int32 >       moSubTypeIndex;
+    std::optional< sal_Int32 >  moSubTypeIndex;
 
     ShapeStyleRefMap            maShapeStyleRefs;
 

include/oox/helper/attributelist.hxx
@@ -95,41 +95,41 @@ public:
     // optional return values -------------------------------------------------
 
     /** Returns the token identifier of the value of the specified attribute. */
-    OptValue< sal_Int32 > getToken( sal_Int32 nAttrToken ) const;
+    std::optional< sal_Int32 > getToken( sal_Int32 nAttrToken ) const;
 
     /** Returns the Color object of highlight of the text. */
     oox::drawingml::Color getHighlightColor(sal_Int32 nAttrToken) const;
 
     /** Returns the string value of the specified attribute. */
-    OptValue< OUString > getString( sal_Int32 nAttrToken ) const;
+    std::optional< OUString > getString( sal_Int32 nAttrToken ) const;
 
     /** Returns the string value of the specified attribute, returns an empty string if attribute not present. */
     OUString getStringDefaulted( sal_Int32 nAttrToken ) const;
 
     /** Returns the string value of the specified attribute. All characters in
         the format '_xHHHH_' (H being a hexadecimal digit), will be decoded. */
-    OptValue< OUString > getXString( sal_Int32 nAttrToken ) const;
+    std::optional< OUString > getXString( sal_Int32 nAttrToken ) const;
 
     /** Returns the double value of the specified attribute. */
-    OptValue< double >  getDouble( sal_Int32 nAttrToken ) const;
+    std::optional< double >  getDouble( sal_Int32 nAttrToken ) const;
 
     /** Returns the 32-bit signed integer value of the specified attribute (decimal). */
-    OptValue< sal_Int32 > getInteger( sal_Int32 nAttrToken ) const;
+    std::optional< sal_Int32 > getInteger( sal_Int32 nAttrToken ) const;
 
     /** Returns the 32-bit unsigned integer value of the specified attribute (decimal). */
-    OptValue< sal_uInt32 > getUnsigned( sal_Int32 nAttrToken ) const;
+    std::optional< sal_uInt32 > getUnsigned( sal_Int32 nAttrToken ) const;
 
     /** Returns the 64-bit signed integer value of the specified attribute (decimal). */
-    OptValue< sal_Int64 > getHyper( sal_Int32 nAttrToken ) const;
+    std::optional< sal_Int64 > getHyper( sal_Int32 nAttrToken ) const;
 
     /** Returns the 32-bit signed integer value of the specified attribute (hexadecimal). */
-    OptValue< sal_Int32 > getIntegerHex( sal_Int32 nAttrToken ) const;
+    std::optional< sal_Int32 > getIntegerHex( sal_Int32 nAttrToken ) const;
 
     /** Returns the boolean value of the specified attribute. */
-    OptValue< bool >    getBool( sal_Int32 nAttrToken ) const;
+    std::optional< bool > getBool( sal_Int32 nAttrToken ) const;
 
     /** Returns the date/time value of the specified attribute. */
-    OptValue< css::util::DateTime > getDateTime( sal_Int32 nAttrToken ) const;
+    std::optional< css::util::DateTime > getDateTime( sal_Int32 nAttrToken ) const;
 
     // defaulted return values ------------------------------------------------
 

include/oox/helper/helper.hxx
@@ -24,6 +24,7 @@
 
 #include <cstring>
 #include <limits>
+#include <optional>
 
 #include <o3tl/safeint.hxx>
 #include <osl/endian.h>
@@ -168,37 +169,8 @@ inline void setFlag( Type& ornBitField, Type nMask, bool bSet = true )
 }
 
 
-/** Optional value, similar to ::std::optional<>, with convenience accessors.
- */
 template< typename Type >
-class OptValue
-{
-public:
-                 OptValue() : maValue(), mbHasValue( false ) {}
-    explicit     OptValue( const Type& rValue ) : maValue( rValue ), mbHasValue( true ) {}
-
-    bool         has_value() const { return mbHasValue; }
-    bool         operator!() const { return !mbHasValue; }
-
-    const Type&  value() const { assert(mbHasValue); return maValue; }
-    const Type&  value_or( const Type& rDefValue ) const { return mbHasValue ? maValue : rDefValue; }
-
-    Type&        operator*() { assert(mbHasValue); return maValue; }
-    Type&        emplace() { mbHasValue = true; maValue = Type(); return maValue; }
-
-    OptValue&    operator=( const Type& rValue ) { maValue = rValue; mbHasValue = true; return *this; }
-    bool         operator==( const OptValue& rValue ) const {
-                             return ( ( !mbHasValue && rValue.mbHasValue == false ) ||
-                                 ( mbHasValue == rValue.mbHasValue && maValue == rValue.maValue ) );
-                 }
-
-private:
-    Type                maValue;
-    bool                mbHasValue;
-};
-
-template< typename Type >
-void assignIfUsed( OptValue<Type>& rDestValue, const OptValue<Type>& rSourceValue )
+void assignIfUsed( std::optional<Type>& rDestValue, const std::optional<Type>& rSourceValue )
 {
     if( rSourceValue.has_value() )
         rDestValue = rSourceValue.value();

include/oox/ppt/pptshape.hxx
@@ -39,7 +39,6 @@ namespace com::sun::star {
 namespace oox {
     namespace core { class XmlFilterBase; }
     namespace drawingml { class Theme; }
-    template <typename Type> class OptValue;
 }
 
 namespace oox::ppt {
@@ -83,7 +82,7 @@ public:
     bool hasNonInheritedShapeProperties() const { return mbHasNoninheritedShapeProperties; }
 
     static oox::drawingml::ShapePtr findPlaceholder( const sal_Int32 nFirstSubType,
-            const sal_Int32 nSecondSubType, const OptValue< sal_Int32 >& oSubTypeIndex,
+            const sal_Int32 nSecondSubType, const std::optional< sal_Int32 >& oSubTypeIndex,
             std::vector< oox::drawingml::ShapePtr >& rShapes, bool bMasterOnly = false );
     static oox::drawingml::ShapePtr findPlaceholderByIndex( const sal_Int32 nIdx,
             std::vector< oox::drawingml::ShapePtr >& rShapes, bool bMasterOnly = false );

include/oox/vml/vmlformatting.hxx
@@ -166,8 +166,8 @@ OOX_DLLPUBLIC sal_Int32 decodeMeasureToTwip(const GraphicHelper& rGraphicHelper,
      */
     OOX_DLLPUBLIC ::oox::drawingml::Color decodeColor(
                             const GraphicHelper& rGraphicHelper,
-                            const OptValue< OUString >& roVmlColor,
-                            const OptValue< double >& roVmlOpacity,
+                            const std::optional< OUString >& roVmlColor,
+                            const std::optional< double >& roVmlOpacity,
                             ::Color nDefaultRgb,
                             ::Color nPrimaryRgb = API_RGB_TRANSPARENT );
 
@@ -193,9 +193,9 @@ OOX_DLLPUBLIC sal_Int32 decodeMeasureToTwip(const GraphicHelper& rGraphicHelper,
 /** The stroke arrow model structure contains all properties for a line end arrow. */
 struct StrokeArrowModel
 {
-    OptValue< sal_Int32 > moArrowType;
-    OptValue< sal_Int32 > moArrowWidth;
-    OptValue< sal_Int32 > moArrowLength;
+    std::optional< sal_Int32 > moArrowType;
+    std::optional< sal_Int32 > moArrowWidth;
+    std::optional< sal_Int32 > moArrowLength;
 
     void                assignUsed( const StrokeArrowModel& rSource );
 };
@@ -204,16 +204,16 @@ struct StrokeArrowModel
 /** The stroke model structure contains all shape border properties. */
 struct StrokeModel
 {
-    OptValue< bool >    moStroked;              ///< Shape border line on/off.
+    std::optional< bool >    moStroked;              ///< Shape border line on/off.
     StrokeArrowModel    maStartArrow;           ///< Start line arrow style.
     StrokeArrowModel    maEndArrow;             ///< End line arrow style.
-    OptValue< OUString > moColor;        ///< Solid line color.
-    OptValue< double > moOpacity;               ///< Solid line color opacity.
-    OptValue< OUString > moWeight;       ///< Line width.
-    OptValue< OUString > moDashStyle;    ///< Line dash (predefined or manually).
-    OptValue< sal_Int32 > moLineStyle;          ///< Line style (single, double, ...).
-    OptValue< sal_Int32 > moEndCap;             ///< Type of line end cap.
-    OptValue< sal_Int32 > moJoinStyle;          ///< Type of line join.
+    std::optional< OUString > moColor;        ///< Solid line color.
+    std::optional< double > moOpacity;               ///< Solid line color opacity.
+    std::optional< OUString > moWeight;       ///< Line width.
+    std::optional< OUString > moDashStyle;    ///< Line dash (predefined or manually).
+    std::optional< sal_Int32 > moLineStyle;          ///< Line style (single, double, ...).
+    std::optional< sal_Int32 > moEndCap;             ///< Type of line end cap.
+    std::optional< sal_Int32 > moJoinStyle;          ///< Type of line join.
 
     void                assignUsed( const StrokeModel& rSource );
 
@@ -227,18 +227,18 @@ struct StrokeModel
 /** The fill model structure contains all shape fill properties. */
 struct OOX_DLLPUBLIC FillModel
 {
-    OptValue< bool >    moFilled;               ///< Shape fill on/off.
-    OptValue< OUString > moColor;        ///< Solid fill color.
-    OptValue< double >  moOpacity;              ///< Solid fill color opacity.
-    OptValue< OUString > moColor2;       ///< End color of gradient.
-    OptValue< double >  moOpacity2;             ///< End color opacity of gradient.
-    OptValue< sal_Int32 > moType;               ///< Fill type.
-    OptValue< sal_Int32 > moAngle;              ///< Gradient rotation angle.
-    OptValue< double >  moFocus;                ///< Linear gradient focus of second color.
-    OptValue< DoublePair > moFocusPos;          ///< Rectangular gradient focus position of second color.
-    OptValue< DoublePair > moFocusSize;         ///< Rectangular gradient focus size of second color.
-    OptValue< OUString > moBitmapPath;   ///< Path to fill bitmap fragment.
-    OptValue< bool >    moRotate;               ///< True = rotate gradient/bitmap with shape.
+    std::optional< bool >    moFilled;               ///< Shape fill on/off.
+    std::optional< OUString > moColor;        ///< Solid fill color.
+    std::optional< double >  moOpacity;              ///< Solid fill color opacity.
+    std::optional< OUString > moColor2;       ///< End color of gradient.
+    std::optional< double >  moOpacity2;             ///< End color opacity of gradient.
+    std::optional< sal_Int32 > moType;               ///< Fill type.
+    std::optional< sal_Int32 > moAngle;              ///< Gradient rotation angle.
+    std::optional< double >  moFocus;                ///< Linear gradient focus of second color.
+    std::optional< DoublePair > moFocusPos;          ///< Rectangular gradient focus position of second color.
+    std::optional< DoublePair > moFocusSize;         ///< Rectangular gradient focus size of second color.
+    std::optional< OUString > moBitmapPath;   ///< Path to fill bitmap fragment.
+    std::optional< bool >    moRotate;               ///< True = rotate gradient/bitmap with shape.
 
     void                assignUsed( const FillModel& rSource );
 
@@ -253,10 +253,10 @@ struct OOX_DLLPUBLIC FillModel
 struct OOX_DLLPUBLIC ShadowModel
 {
     bool               mbHasShadow;               ///< Is a v:shadow element seen?
-    OptValue<bool>     moShadowOn;                ///< Is the element turned on?
-    OptValue<OUString> moColor;                   ///< Specifies the color of the shadow.
-    OptValue<OUString> moOffset;                  ///< Specifies the shadow's offset from the shape's location.
-    OptValue<double>   moOpacity;                 ///< Specifies the opacity of the shadow.
+    std::optional<bool>     moShadowOn;                ///< Is the element turned on?
+    std::optional<OUString> moColor;                   ///< Specifies the color of the shadow.
+    std::optional<OUString> moOffset;                  ///< Specifies the shadow's offset from the shape's location.
+    std::optional<double>   moOpacity;                 ///< Specifies the opacity of the shadow.
 
     ShadowModel();
 
@@ -267,9 +267,9 @@ struct OOX_DLLPUBLIC ShadowModel
 /** The shadow model structure contains all shape textpath properties. */
 struct TextpathModel
 {
-    OptValue<OUString> moString;                  ///< Specifies the string of the textpath.
-    OptValue<OUString> moStyle;                   ///< Specifies the style of the textpath.
-    OptValue<bool>     moTrim;                    ///< Specifies whether extra space is removed above and below the text
+    std::optional<OUString> moString;                  ///< Specifies the string of the textpath.
+    std::optional<OUString> moStyle;                   ///< Specifies the style of the textpath.
+    std::optional<bool>     moTrim;                    ///< Specifies whether extra space is removed above and below the text
 
     TextpathModel();
 

include/oox/vml/vmlshape.hxx
@@ -62,10 +62,10 @@ struct ShapeTypeModel
     OUString     maShapeId;              ///< Unique identifier of the shape.
     OUString     maLegacyId;             ///< Plaintext identifier of the shape.
     OUString     maShapeName;            ///< Name of the shape, if present.
-    OptValue< sal_Int32 > moShapeType;          ///< Builtin shape type identifier.
+    std::optional< sal_Int32 > moShapeType;          ///< Builtin shape type identifier.
 
-    OptValue< Int32Pair > moCoordPos;           ///< Top-left position of coordinate system for children scaling.
-    OptValue< Int32Pair > moCoordSize;          ///< Size of coordinate system for children scaling.
+    std::optional< Int32Pair > moCoordPos;           ///< Top-left position of coordinate system for children scaling.
+    std::optional< Int32Pair > moCoordSize;          ///< Size of coordinate system for children scaling.
     OUString     maPosition;             ///< Position type of the shape.
     OUString     maZIndex;                ///< ZIndex of the shape
     OUString     maLeft;                 ///< X position of the shape bounding box (number with unit).
@@ -96,21 +96,21 @@ struct ShapeTypeModel
     ShadowModel         maShadowModel;          ///< Shape shadow formatting.
     TextpathModel       maTextpathModel;        ///< Shape textpath formatting.
 
-    OptValue< OUString > moGraphicPath;  ///< Path to a graphic for this shape.
-    OptValue< OUString > moGraphicTitle; ///< Title of the graphic.
-    OptValue< OUString > moWrapAnchorX;  ///< The base object from which our horizontal positioning should be calculated.
-    OptValue< OUString > moWrapAnchorY;  ///< The base object from which our vertical positioning should be calculated.
-    OptValue< OUString > moWrapType;     ///< How to wrap the text around the object
-    OptValue< OUString > moWrapSide;     ///< On which side to wrap the text around the object
+    std::optional< OUString > moGraphicPath;  ///< Path to a graphic for this shape.
+    std::optional< OUString > moGraphicTitle; ///< Title of the graphic.
+    std::optional< OUString > moWrapAnchorX;  ///< The base object from which our horizontal positioning should be calculated.
+    std::optional< OUString > moWrapAnchorY;  ///< The base object from which our vertical positioning should be calculated.
+    std::optional< OUString > moWrapType;     ///< How to wrap the text around the object
+    std::optional< OUString > moWrapSide;     ///< On which side to wrap the text around the object
     OUString maVTextAnchor; ///< How the text inside the shape is anchored vertically.
     OUString maWrapDistanceLeft;         ///< Distance from the left side of the shape to the text that wraps around it.
     OUString maWrapDistanceRight;        ///< Distance from the right side of the shape to the text that wraps around it.
     OUString maWrapDistanceTop;          ///< Distance from the top of the shape to the text that wraps around it.
     OUString maWrapDistanceBottom;       ///< Distance from the bottom of the shape to the text that wraps around it.
-    OptValue<OUString> moCropBottom; ///< Specifies the how much to crop the image from the bottom up as a fraction of picture size.
-    OptValue<OUString> moCropLeft; ///< Specifies how much to crop the image from the left in as a fraction of picture size.
-    OptValue<OUString> moCropRight; ///< Specifies how much to crop the image from the right in as a fraction of picture size.
-    OptValue<OUString> moCropTop; ///< Specifies how much to crop the image from the top down as a fraction of picture size.
+    std::optional<OUString> moCropBottom; ///< Specifies the how much to crop the image from the bottom up as a fraction of picture size.
+    std::optional<OUString> moCropLeft; ///< Specifies how much to crop the image from the left in as a fraction of picture size.
+    std::optional<OUString> moCropRight; ///< Specifies how much to crop the image from the right in as a fraction of picture size.
+    std::optional<OUString> moCropTop; ///< Specifies how much to crop the image from the top down as a fraction of picture size.
     OUString maLayoutFlowAlt; ///< Specifies the alternate layout flow for text in textboxes.
 
     /// An adjustment for the intensity of all colors, i.e. contrast, on a 0..0x10000 scale.

include/oox/vml/vmlshapecontext.hxx
@@ -109,7 +109,7 @@ private:
     void                setStyle( std::u16string_view rStyle );
 
     /** Resolve a relation identifier to a fragment path. */
-    OptValue< OUString > decodeFragmentPath( const AttributeList& rAttribs, sal_Int32 nToken ) const;
+    std::optional< OUString > decodeFragmentPath( const AttributeList& rAttribs, sal_Int32 nToken ) const;
 
 private:
     std::shared_ptr<ShapeType> m_pShapeType;

commit 67a8aaf1a0f68c3efa6445f4c0e82c89b2ef6fd7

tdf#149985 pptx crash on a one-slide presentation (with a chart)
regression from
    commit 3bd7abe023691a936a1e9eaba49dcb585309d4bd
    Date:   Tue Jun 21 13:17:09 2022 +0200
    replace oox::OptValue with std::optional

Change-Id: I830713495ca04771433018f1091faadf0f6bd71e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137051
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

oox/source/drawingml/chart/seriesconverter.cxx
@@ -361,7 +361,7 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
                         if (eType == DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CELLRANGE && oaCellRange.has_value())
                         {
                             xCustomLabel->setCellRange( oaCellRange.value() );
-                            xCustomLabel->setString( oaLabelText.value() );
+                            xCustomLabel->setString( oaLabelText.value_or("") );
                             xCustomLabel->setDataLabelsRange( true );
                         }
                         else