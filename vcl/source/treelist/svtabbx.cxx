LemmingAvalanche/libreoffice-repaired

commit 2a0ea7b0220ad1ffb27d78187df6f7dd9ec69339

vcl: allow editing a column other than the first one in SvTabListBox
There are 2 members mvTabList and aTabs in SvTabListBox and
SvTreeListBox, and only aTabs is evaluated to determine which column
should be edited when calling start_editing().

This solution is probably not ideal; it would be better to have a
parameter to select the column because one might want multiple editable
columns with multiple buttons...

Also, the native GTK implementation has the same bug (but there
double-clicking the column works).

Change-Id: I727792e635d3b610a2132ee5f4155e3ee610aaf2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137362
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/treelist/svtabbx.cxx
@@ -153,6 +153,16 @@ void SvTabListBox::SetTabs()
     }
     */
 
+    // the 1st column (index 1 or 2 depending on button flags) is always set
+    // editable by SvTreeListBox::SetTabs(),
+    // which prevents setting a different column to editable as the first
+    // one with the flag is picked in SvTreeListBox::ImplEditEntry()
+    assert(aTabs.back()->nFlags & SvLBoxTabFlags::EDITABLE);
+    if (!(mvTabList[0].nFlags & SvLBoxTabFlags::EDITABLE))
+    {
+        aTabs.back()->nFlags &= ~SvLBoxTabFlags::EDITABLE;
+    }
+
     // append all other tabs to the list
     for( sal_uInt16 nCurTab = 1; nCurTab < sal_uInt16(mvTabList.size()); nCurTab++ )
     {

commit 6f36b34f8d9e55e5f11bf14a367747824cabf160

tdf#150181 vcl: first column is editable by default in SvTabListBox
(regression from commit 2a0ea7b0220ad1ffb27d78187df6f7dd9ec69339)

Change-Id: I58e1afb228dfd87b7d642ccf1b58277960f01e14
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137561
Tested-by: Jenkins
Tested-by: Julien Nabet <serval2412@yahoo.fr>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/treelist/svtabbx.cxx
@@ -205,6 +205,7 @@ void SvTabListBox::dispose()
 
 void SvTabListBox::SetTabs(sal_uInt16 nTabs, tools::Long const pTabPositions[], MapUnit eMapUnit)
 {
+    assert(0 < nTabs);
     mvTabList.resize(nTabs);
 
     MapMode aMMSource( eMapUnit );
@@ -218,6 +219,8 @@ void SvTabListBox::SetTabs(sal_uInt16 nTabs, tools::Long const pTabPositions[], 
         mvTabList[nIdx].SetPos( nNewTab );
         mvTabList[nIdx].nFlags &= MYTABMASK;
     }
+    // by default, 1st one is editable, others not; override with set_column_editables
+    mvTabList[0].nFlags |= SvLBoxTabFlags::EDITABLE;
     SvTreeListBox::nTreeFlags |= SvTreeFlags::RECALCTABS;
     if( IsUpdateMode() )
         Invalidate();