LemmingAvalanche/libreoffice-repaired

commit 96f15d326b7ffdb39cd588534bff601e3be33c14

(related: fdo#81033) writerfilter: default tab fill character is space
This avoids an ugly "Character" box in the dialog with a null byte in it.

Change-Id: Iee35a8e1bd6b57fb4eb8ae06722bd79ee1d5d8de

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -215,8 +215,13 @@ typedef std::stack< AnchoredContext >           TextContentStack;
 struct DeletableTabStop : public ::com::sun::star::style::TabStop
 {
     bool bDeleted;
-    DeletableTabStop() :
-        bDeleted( false ){}
+    DeletableTabStop()
+        : bDeleted(false)
+    {
+        // same defaults as SvxXMLTabStopContext_Impl
+        FillChar = ' ';
+        DecimalChar = ',';
+    }
     DeletableTabStop( const ::com::sun::star::style::TabStop& rTabStop ) :
         TabStop( rTabStop ),
             bDeleted( false ){}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1388,7 +1388,7 @@ DECLARE_RTFIMPORT_TEST(testFdo81033, "fdo81033.rtf")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(2), tabs.getLength());
     CPPUNIT_ASSERT_EQUAL(sal_Int32(5808), tabs[0].Position);
     CPPUNIT_ASSERT_EQUAL(style::TabAlign_LEFT, tabs[0].Alignment);
-    CPPUNIT_ASSERT_EQUAL(sal_Unicode(0), tabs[0].FillChar);
+    CPPUNIT_ASSERT_EQUAL(sal_Unicode(' '), tabs[0].FillChar);
     CPPUNIT_ASSERT_EQUAL(sal_Int32(16002), tabs[1].Position);
     CPPUNIT_ASSERT_EQUAL(style::TabAlign_LEFT, tabs[1].Alignment);
     CPPUNIT_ASSERT_EQUAL(sal_Unicode('_'), tabs[1].FillChar);

commit 3d94d273aee0205cd2d2fd998d0e2dc8111cbd48

tdf#120972 DOCX: fix import of decimal tabulators
Use locale specific decimal separator for decimal tabulators,
because OOXML does not support custom decimal separator.

Regression from commit 96f15d326b7ffdb39cd588534bff601e3be33c14
"(related: fdo#81033) writerfilter: default tab fill character is space".

Change-Id: I9d89e5817707f3b39a1b718b732f6e289f7c6a99
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136846
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -290,9 +290,7 @@ struct DeletableTabStop : public css::style::TabStop
     explicit DeletableTabStop()
         : bDeleted(false)
     {
-        // same defaults as SvxXMLTabStopContext_Impl
-        FillChar = ' ';
-        DecimalChar = ',';
+        FillChar = ' ';    // same default as SvxXMLTabStopContext_Impl
     }
     DeletableTabStop(const css::style::TabStop& rTabStop)
         : TabStop(rTabStop),

sw/qa/extras/odfexport/odfexport2.cxx
@@ -11,6 +11,7 @@
 
 #include <com/sun/star/text/XTextTable.hpp>
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
+#include <unotools/localedatawrapper.hxx>
 
 class Test : public SwModelTestBase
 {
@@ -209,6 +210,22 @@ CPPUNIT_TEST_FIXTURE(Test, testStyleLink)
     // - Actual  :
     // i.e. the linked style was lost on import.
     CPPUNIT_ASSERT_EQUAL(OUString("List Paragraph Char"), getProperty<OUString>(aParaStyle, "LinkStyle"));
+}
+
+CPPUNIT_TEST_FIXTURE(Test, tdf120972)
+{
+    loadAndReload("table_number_format_3.docx");
+
+    xmlDocUniquePtr pXmlDoc = parseExport("content.xml");
+    OUString cDecimal(SvtSysLocale().GetLocaleData().getNumDecimalSep()[0]);
+    assertXPath(
+        pXmlDoc,
+        "//style:style[@style:name='P1']/style:paragraph-properties/style:tab-stops/style:tab-stop",
+        "char", cDecimal);
+    assertXPath(
+        pXmlDoc,
+        "//style:style[@style:name='P2']/style:paragraph-properties/style:tab-stops/style:tab-stop",
+        "char", cDecimal);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();