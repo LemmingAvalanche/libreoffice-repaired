LemmingAvalanche/libreoffice-repaired

commit ec07a34b8bbc33e6ba13be86f15f29049274f0a5

clang-tidy modernize-pass-by-value in ucb
Change-Id: I641d05e360c3208845a2fa6721ac8ba1ad0f3309
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137667
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/cmis/auth_provider.hxx
@@ -31,10 +31,10 @@ namespace cmis
         OUString m_sBindingUrl;
 
         public:
-            AuthProvider ( const css::uno::Reference< css::ucb::XCommandEnvironment>& xEnv,
-                           const OUString& sUrl,
-                           const OUString& sBindingUrl ):
-                m_xEnv( xEnv ), m_sUrl( sUrl ), m_sBindingUrl( sBindingUrl ) { }
+            AuthProvider ( css::uno::Reference< css::ucb::XCommandEnvironment> xEnv,
+                           OUString sUrl,
+                           OUString sBindingUrl ):
+                m_xEnv( std::move(xEnv) ), m_sUrl( std::move(sUrl) ), m_sBindingUrl( std::move(sBindingUrl) ) { }
 
             bool authenticationQuery( std::string& username, std::string& password ) override;
 

commit 19c9bf32536d750104989b10fbe89f828022cd1b

cid#1507745 return local
regression from
    commit ec07a34b8bbc33e6ba13be86f15f29049274f0a5
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Mon Aug 1 09:09:05 2022 +0200
    clang-tidy modernize-pass-by-value in ucb

Change-Id: Ie5d7d78ebde6fdee1234a0da496ff9692ee3c2c5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137796
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/cmis/auth_provider.hxx
@@ -31,10 +31,10 @@ namespace cmis
         OUString m_sBindingUrl;
 
         public:
-            AuthProvider ( css::uno::Reference< css::ucb::XCommandEnvironment> xEnv,
+            AuthProvider ( const css::uno::Reference< css::ucb::XCommandEnvironment> & xEnv,
                            OUString sUrl,
                            OUString sBindingUrl ):
-                m_xEnv( std::move(xEnv) ), m_sUrl( std::move(sUrl) ), m_sBindingUrl( std::move(sBindingUrl) ) { }
+                m_xEnv( xEnv ), m_sUrl( std::move(sUrl) ), m_sBindingUrl( std::move(sBindingUrl) ) { }
 
             bool authenticationQuery( std::string& username, std::string& password ) override;
 