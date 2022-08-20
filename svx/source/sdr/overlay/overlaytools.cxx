LemmingAvalanche/libreoffice-repaired

commit 961b7b234939301fd2ceb7bb7fa7c9b9a0398094

loplugin:moveit
Change-Id: Ic593974a44d9e327e0385c7ffaaa6d42576ae01a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135911
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/overlay/overlaytools.cxx
@@ -236,7 +236,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
             rContainer.push_back(
                 new PolygonMarkerPrimitive2D(
-                    aPolygon,
+                    std::move(aPolygon),
                     getRGBColorA(),
                     getRGBColorB(),
                     getDiscreteDashLength()));
@@ -247,7 +247,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
             rContainer.push_back(
                 new PolygonMarkerPrimitive2D(
-                    aPolygon,
+                    std::move(aPolygon),
                     getRGBColorA(),
                     getRGBColorB(),
                     getDiscreteDashLength()));
@@ -339,7 +339,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
                     const basegfx::BColor aEmptyColor(0.0, 0.0, 0.0);
                     const double fHatchRotation(basegfx::deg2rad(45));
                     const double fDiscreteHatchDistance(3.0);
-                    const drawinglayer::attribute::FillHatchAttribute aFillHatchAttribute(
+                    drawinglayer::attribute::FillHatchAttribute aFillHatchAttribute(
                         drawinglayer::attribute::HatchStyle::Single,
                         fDiscreteHatchDistance * getDiscreteUnit(),
                         fHatchRotation - getRotation(),
@@ -350,7 +350,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
                         new PolyPolygonHatchPrimitive2D(
                             aPolyPolygon,
                             aEmptyColor,
-                            aFillHatchAttribute));
+                            std::move(aFillHatchAttribute)));
 
                     aRetval = Primitive2DContainer { aHatch };
                 }
@@ -359,7 +359,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
                     // create fill primitive
                     const Primitive2DReference aFill(
                         new PolyPolygonColorPrimitive2D(
-                            aPolyPolygon,
+                            std::move(aPolyPolygon),
                             getColor()));
 
                     aRetval = Primitive2DContainer { aFill };
@@ -435,7 +435,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
                     rContainer.push_back(
                         new PolygonMarkerPrimitive2D(
-                            aLine,
+                            std::move(aLine),
                             getRGBColorA(),
                             getRGBColorB(),
                             getDiscreteDashLength()));
@@ -451,7 +451,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
                     rContainer.push_back(
                         new PolygonMarkerPrimitive2D(
-                            aLine,
+                            std::move(aLine),
                             getRGBColorA(),
                             getRGBColorB(),
                             getDiscreteDashLength()));
@@ -468,7 +468,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
                     rContainer.push_back(
                         new PolygonMarkerPrimitive2D(
-                            aLineA,
+                            std::move(aLineA),
                             getRGBColorA(),
                             getRGBColorB(),
                             getDiscreteDashLength()));
@@ -478,7 +478,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
                     rContainer.push_back(
                         new PolygonMarkerPrimitive2D(
-                            aLineB,
+                            std::move(aLineB),
                             getRGBColorA(),
                             getRGBColorB(),
                             getDiscreteDashLength()));
@@ -534,45 +534,45 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
             // Left lines
             aLine.append(basegfx::B2DPoint(getViewport().getMinX(), getRollingRectangle().getMinY()));
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMinX(), getRollingRectangle().getMinY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getViewport().getMinX(), getRollingRectangle().getMaxY()));
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMinX(), getRollingRectangle().getMaxY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             // Right lines
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMaxX(), getRollingRectangle().getMinY()));
             aLine.append(basegfx::B2DPoint(getViewport().getMaxX(), getRollingRectangle().getMinY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMaxX(), getRollingRectangle().getMaxY()));
             aLine.append(basegfx::B2DPoint(getViewport().getMaxX(), getRollingRectangle().getMaxY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             // Top lines
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMinX(), getViewport().getMinY()));
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMinX(), getRollingRectangle().getMinY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMaxX(), getViewport().getMinY()));
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMaxX(), getRollingRectangle().getMinY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             // Bottom lines
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMinX(), getRollingRectangle().getMaxY()));
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMinX(), getViewport().getMaxY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
             aLine.clear();
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMaxX(), getRollingRectangle().getMaxY()));
             aLine.append(basegfx::B2DPoint(getRollingRectangle().getMaxX(), getViewport().getMaxY()));
-            rContainer.push_back(new PolygonMarkerPrimitive2D(aLine, getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
+            rContainer.push_back(new PolygonMarkerPrimitive2D(std::move(aLine), getRGBColorA(), getRGBColorB(), getDiscreteDashLength()));
 
         }
 

commit 0ae033e3a2acbca991c88bf70f134d6a7a01b91d

tdf#150502: crash after clicking between the horizontal and vertical rulers
0  0x00007f22e3b294f7 in o3tl::cow_wrapper<ImplB2DPolygon, o3tl::UnsafeRefCountingPolicy>::make_unique() (this=0x7ffc2dcc7640) at include/o3tl/cow_wrapper.hxx:301
1  0x00007f22e3b2a305 in o3tl::cow_wrapper<ImplB2DPolygon, o3tl::UnsafeRefCountingPolicy>::operator*() (this=0x7ffc2dcc7640) at include/o3tl/cow_wrapper.hxx:329
2  0x00007f22e3b18fe5 in basegfx::B2DPolygon::clear() (this=0x7ffc2dcc7640) at basegfx/source/polygon/b2dpolygon.cxx:1433
3  0x00007f22eb42d882 in drawinglayer::primitive2d::OverlayCrosshairPrimitive::create2DDecomposition(drawinglayer::primitive2d::Primitive2DContainer&, drawinglayer::geometry::ViewInformation2D const&) const
   (this=0x55a582f57d40, rContainer=...) at svx/source/sdr/overlay/overlaytools.cxx:244
4  0x00007f22edae9e41 in drawinglayer::primitive2d::BufferedDecompositionPrimitive2D::get2DDecomposition(drawinglayer::primitive2d::Primitive2DDecompositionVisitor&, drawinglayer::geometry::ViewInformation2D const&) const (this=0x55a582f57d40, rVisitor=..., rViewInformation=...) at drawinglayer/source/primitive2d/BufferedDecompositionPrimitive2D.cxx:36
5  0x00007f22e2da32a7 in drawinglayer::primitive2d::ViewportDependentPrimitive2D::get2DDecomposition(drawinglayer::primitive2d::Primitive2DDecompositionVisitor&, drawinglayer::geometry::ViewInformation2D const&) const (this=0x55a582f57d40, rVisitor=..., rViewInformation=...) at drawinglayer/source/primitive2d/primitivetools2d.cxx:70
6  0x00007f22edae80c6 in drawinglayer::primitive2d::BasePrimitive2D::getB2DRange(drawinglayer::geometry::ViewInformation2D const&) const (this=0x55a582f57d40, rViewInformation=...)
    at drawinglayer/source/primitive2d/baseprimitive2d.cxx:73
full bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=181888

Regression from 961b7b234939301fd2ceb7bb7fa7c9b9a0398094

Change-Id: Ide5c03124d11ffe02c0510318d2198f25ea645f0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138564

svx/source/sdr/overlay/overlaytools.cxx
@@ -236,7 +236,7 @@ sal_uInt32 OverlayStaticRectanglePrimitive::getPrimitive2DID() const
 
             rContainer.push_back(
                 new PolygonMarkerPrimitive2D(
-                    std::move(aPolygon),
+                    aPolygon,
                     getRGBColorA(),
                     getRGBColorB(),
                     getDiscreteDashLength()));