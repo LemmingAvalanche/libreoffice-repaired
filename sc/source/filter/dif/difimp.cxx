LemmingAvalanche/libreoffice-repaired

commit e8645ceff094b9edd01e83c087e98ba60f366306

Avoid OUStringBuffer::toString where possible
Change-Id: I99abbe97a48b2077e28d6221fb70036e5e412657
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127479
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sc/source/filter/dif/difimp.cxx
@@ -129,6 +129,7 @@ ErrCode ScFormatFilterPluginImpl::ScImportDif(SvStream& rIn, ScDocument* pDoc, c
 
         ScSetStringParam aStrParam; // used to set string value without number detection.
         aStrParam.setTextInput();
+        const OUString aData = rData.makeStringAndClear();
 
         while( eCurrent != D_EOD )
         {
@@ -136,8 +137,6 @@ ErrCode ScFormatFilterPluginImpl::ScImportDif(SvStream& rIn, ScDocument* pDoc, c
 
             aPrgrsBar.Progress();
             ScAddress aPos(nColCnt, nRowCnt, nBaseTab);
-
-            OUString aData = rData.toString();
 
             switch( eCurrent )
             {
@@ -391,7 +390,7 @@ static void lcl_DeEscapeQuotesDif(OUStringBuffer& rString)
     //  escape quotes in strings (and Excel doesn't when using the clipboard).
     //  The quotes around the string are removed before this function is called.
 
-    rString = rString.toString().replaceAll("\"\"", "\"");
+    rString = rString.makeStringAndClear().replaceAll("\"\"", "\"");
 }
 
 // Determine if passed in string is numeric data and set fVal/nNumFormat if so

basctl/source/basicide/baside2b.cxx
@@ -2591,7 +2591,7 @@ void CodeCompleteWindow::SetMatchingEntries()
     for (sal_Int32 i = 0, nEntryCount = m_xListBox->n_children(); i< nEntryCount; ++i)
     {
         OUString sEntry = m_xListBox->get_text(i);
-        if (sEntry.startsWithIgnoreAsciiCase(aFuncBuffer.toString()))
+        if (sEntry.startsWithIgnoreAsciiCase(aFuncBuffer))
         {
             m_xListBox->select(i);
             break;
@@ -2657,8 +2657,8 @@ bool CodeCompleteWindow::HandleKeyInput( const KeyEvent& rKeyEvt )
                         for (sal_Int32 i = nInd; i != nEntryCount; ++i)
                         {
                             OUString sEntry = m_xListBox->get_text(i);
-                            if( sEntry.startsWithIgnoreAsciiCase( aFuncBuffer.toString() )
-                                && (aFuncBuffer.toString() != sTypedText) && (i != nInd) )
+                            if( sEntry.startsWithIgnoreAsciiCase( aFuncBuffer )
+                                && (std::u16string_view(aFuncBuffer) != sTypedText) && (i != nInd) )
                             {
                                 m_xListBox->select(i);
                                 bFound = true;
@@ -2688,7 +2688,7 @@ bool CodeCompleteWindow::HandleKeyInput( const KeyEvent& rKeyEvt )
                     OUString aTabInsertedStr( GetParentEditView()->GetSelected() );
                     GetParentEditView()->SetSelection( aSel );
 
-                    if( !aTabInsertedStr.isEmpty() && aTabInsertedStr != aFuncBuffer.toString() )
+                    if( !aTabInsertedStr.isEmpty() && aTabInsertedStr != std::u16string_view(aFuncBuffer) )
                     {
                         aFuncBuffer = aTabInsertedStr;
                     }

chart2/source/tools/LogarithmicRegressionCurveCalculator.cxx
@@ -175,7 +175,7 @@ OUString LogarithmicRegressionCurveCalculator::ImplGetRepresentation(
         addStringToEquation( aBuf, nLineLength, aTmpBuf, pFormulaMaxWidth );
     }
 
-    if ( aBuf.toString() == OUStringConcatenation(mYName + " = ") )
+    if ( std::u16string_view(aBuf) == OUStringConcatenation(mYName + " = ") )
         aBuf.append( "0" );
 
     return aBuf.makeStringAndClear();

chart2/source/tools/PolynomialRegressionCurveCalculator.cxx
@@ -381,7 +381,7 @@ OUString PolynomialRegressionCurveCalculator::ImplGetRepresentation(
         }
         addStringToEquation( aBuf, nLineLength, aTmpBuf, pFormulaMaxWidth );
     }
-    if ( aBuf.toString() == OUStringConcatenation( mYName + " = ") )
+    if ( std::u16string_view(aBuf) == OUStringConcatenation( mYName + " = ") )
         aBuf.append( "0" );
 
     return aBuf.makeStringAndClear();

comphelper/source/misc/docpasswordhelper.cxx
@@ -114,7 +114,7 @@ DocPasswordHelper::GenerateNewModifyPasswordInfoOOXML(std::u16string_view aPassw
     uno::Sequence<sal_Int8> aSalt = GenerateRandomByteSequence(16);
     OUStringBuffer aBuffer;
     comphelper::Base64::encode(aBuffer, aSalt);
-    OUString sSalt = aBuffer.toString();
+    OUString sSalt = aBuffer.makeStringAndClear();
 
     sal_Int32 const nIterationCount = 100000;
     OUString sAlgorithm("SHA-512");

dbaccess/source/core/api/OptimisticSet.cxx
@@ -208,7 +208,7 @@ void OptimisticSet::updateRow(const ORowSetRow& _rInsertRow ,const ORowSetRow& _
             OUString sCatalog,sSchema,sTable;
             ::dbtools::qualifiedNameComponents(xMetaData,elem.first,sCatalog,sSchema,sTable,::dbtools::EComposeRule::InDataManipulation);
             OUStringBuffer sSql("UPDATE " + ::dbtools::composeTableNameForSelect( m_xConnection, sCatalog, sSchema, sTable ) +
-                                       " SET " + elem.second.toString());
+                                       " SET " + elem.second);
             OUStringBuffer& rCondition = aKeyConditions[elem.first];
             if ( !rCondition.isEmpty() )
                 sSql.append(" WHERE " + rCondition );
@@ -266,14 +266,14 @@ void OptimisticSet::insertRow( const ORowSetRow& _rInsertRow,const connectivity:
             OUString sCatalog,sSchema,sTable;
             ::dbtools::qualifiedNameComponents(xMetaData,elem.first,sCatalog,sSchema,sTable,::dbtools::EComposeRule::InDataManipulation);
             OUString sComposedTableName = ::dbtools::composeTableNameForSelect( m_xConnection, sCatalog, sSchema, sTable );
-            OUString sSql("INSERT INTO " + sComposedTableName + " ( " + elem.second.toString() +
-                                 ") VALUES ( " + aParameter[elem.first].toString() + " )");
+            OUString sSql("INSERT INTO " + sComposedTableName + " ( " + elem.second +
+                                 ") VALUES ( " + aParameter[elem.first] + " )");
 
             OUStringBuffer& rCondition = aKeyConditions[elem.first];
             if ( !rCondition.isEmpty() )
             {
-                OUString sQuery("SELECT " + elem.second.toString() + " FROM " + sComposedTableName +
-                                       " WHERE " + rCondition.toString());
+                OUString sQuery("SELECT " + elem.second + " FROM " + sComposedTableName +
+                                       " WHERE " + rCondition);
 
                 try
                 {
@@ -330,7 +330,7 @@ void OptimisticSet::deleteRow(const ORowSetRow& _rDeleteRow,const connectivity::
             OUString sCatalog,sSchema,sTable;
             ::dbtools::qualifiedNameComponents(xMetaData,keyCondition.first,sCatalog,sSchema,sTable,::dbtools::EComposeRule::InDataManipulation);
             OUString sSql("DELETE FROM " + ::dbtools::composeTableNameForSelect( m_xConnection, sCatalog, sSchema, sTable ) +
-                                 " WHERE " + rCondition.toString() );
+                                 " WHERE " + rCondition );
             executeDelete(_rDeleteRow, sSql, keyCondition.first);
         }
     }
@@ -544,7 +544,7 @@ void OptimisticSet::fillMissingValues(ORowSetValueVector::Vector& io_aRow) const
                 OUString sCatalog,sSchema,sTable;
                 ::dbtools::qualifiedNameComponents(xMetaData,elem.first,sCatalog,sSchema,sTable,::dbtools::EComposeRule::InDataManipulation);
                 OUString sComposedTableName = ::dbtools::composeTableNameForSelect( m_xConnection, sCatalog, sSchema, sTable );
-                OUString sQuery("SELECT " + elem.second.toString() + " FROM " + sComposedTableName + " WHERE " +
+                OUString sQuery("SELECT " + elem.second + " FROM " + sComposedTableName + " WHERE " +
                                        rCondition.makeStringAndClear());
 
                 try

dbaccess/source/core/recovery/settingsimport.cxx
@@ -139,7 +139,7 @@ namespace dbaccess
         o_rValue.clear();
 
         // the characters building up th evalue
-        const OUString sValue = getAccumulatedCharacters().toString();
+        std::u16string_view sValue = getAccumulatedCharacters();
 
         const OUString& rItemType( getItemType() );
         ENSURE_OR_RETURN_VOID( !rItemType.isEmpty(), "no item type -> no item value" );
@@ -170,7 +170,7 @@ namespace dbaccess
         }
         else if ( ::xmloff::token::IsXMLToken( rItemType, ::xmloff::token::XML_STRING ) )
         {
-            o_rValue <<= sValue;
+            o_rValue <<= OUString(sValue);
         }
         else
         {

desktop/source/app/crashreport.cxx
@@ -202,7 +202,7 @@ OUString CrashReporter::getLoggedUnoCommands()
         aUnoCommandBuffer.append(unocommand);
         aCommandSeperator=",";
     }
-    return aUnoCommandBuffer.toString();
+    return aUnoCommandBuffer.makeStringAndClear();
 }
 
 namespace {

commit 09c1465f9239aa7d2edf81d3cacbd87997701ca6

Resolves: tdf#150599 Move obtaining data into loop again
Regression from

    commit e8645ceff094b9edd01e83c087e98ba60f366306
    CommitDate: Sun Dec 26 20:36:26 2021 +0100

        Avoid OUStringBuffer::toString where possible

that erroneously also moved obtaining the data string outside the
loop, whyever..

Change-Id: I329108d42db43c1ab22ba82650d801e595dd7ece
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138805
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/source/filter/dif/difimp.cxx
@@ -122,7 +122,6 @@ ErrCode ScFormatFilterPluginImpl::ScImportDif(SvStream& rIn, ScDocument* pDoc, c
 
         ScSetStringParam aStrParam; // used to set string value without number detection.
         aStrParam.setTextInput();
-        const OUString aData = rData.makeStringAndClear();
 
         while( eCurrent != D_EOD )
         {
@@ -130,6 +129,7 @@ ErrCode ScFormatFilterPluginImpl::ScImportDif(SvStream& rIn, ScDocument* pDoc, c
 
             aPrgrsBar.Progress();
             ScAddress aPos(nColCnt, nRowCnt, nBaseTab);
+            const OUString aData = rData.makeStringAndClear();
 
             switch( eCurrent )
             {