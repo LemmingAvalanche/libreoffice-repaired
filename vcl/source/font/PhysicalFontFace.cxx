LemmingAvalanche/libreoffice-repaired

commit 836600e40f0963b76f097572d798b73596c96eee

vcl: Consolidate PhysicalFontFace::GetFontCapabilities()
All subclasses are doing the same thing in slightly different ways, so
move it to the base class that we can now access font tables there.

Change-Id: I1f8827dbc345aa852e1f7aaaa4cb4615593289c8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139395
Tested-by: Jenkins
Reviewed-by: خالد حسني <khaled@aliftype.com>

vcl/source/font/PhysicalFontFace.cxx
@@ -25,6 +25,7 @@
 
 #include <fontattributes.hxx>
 #include <impfontcharmap.hxx>
+#include <sft.hxx>
 
 #include <font/FontSelectPattern.hxx>
 #include <font/PhysicalFontFace.hxx>
@@ -244,6 +245,26 @@ FontCharMapRef PhysicalFontFace::GetFontCharMap() const
         mxCharMap = FontCharMap::GetDefaultMap(IsSymbolFont());
 
     return mxCharMap;
+}
+
+bool PhysicalFontFace::GetFontCapabilities(vcl::FontCapabilities& rFontCapabilities) const
+{
+    if (!mbFontCapabilitiesRead)
+    {
+        mbFontCapabilitiesRead = true;
+
+        hb_blob_t* pBlob = GetHbTable(HB_TAG('O', 'S', '/', '2'));
+        if (pBlob)
+        {
+            unsigned int nSize = 0;
+            auto* pData = reinterpret_cast<const unsigned char*>(hb_blob_get_data(pBlob, &nSize));
+            vcl::getTTCoverage(maFontCapabilities.oUnicodeRange, maFontCapabilities.oCodePageRange,
+                               pData, nSize);
+        }
+    }
+
+    rFontCapabilities = maFontCapabilities;
+    return rFontCapabilities.oUnicodeRange || rFontCapabilities.oCodePageRange;
 }
 }
 

vcl/inc/font/PhysicalFontFace.hxx
@@ -25,6 +25,7 @@
 #include <rtl/ref.hxx>
 #include <tools/long.hxx>
 #include <vcl/dllapi.h>
+#include <vcl/fontcapabilities.hxx>
 #include <vcl/fontcharmap.hxx>
 
 #include <fontattributes.hxx>
@@ -40,7 +41,6 @@ class FontSelectPattern;
 
 namespace vcl
 {
-struct FontCapabilities;
 class PhysicalFontFamily;
 }
 
@@ -76,7 +76,7 @@ public:
 
     virtual sal_IntPtr GetFontId() const = 0;
     virtual FontCharMapRef GetFontCharMap() const;
-    virtual bool GetFontCapabilities(vcl::FontCapabilities&) const = 0;
+    virtual bool GetFontCapabilities(vcl::FontCapabilities&) const;
 
     bool IsBetterMatch(const vcl::font::FontSelectPattern&, FontMatchStatus&) const;
     sal_Int32 CompareIgnoreSize(const PhysicalFontFace&) const;
@@ -91,6 +91,8 @@ public:
 protected:
     mutable hb_face_t* mpHbFace;
     mutable FontCharMapRef mxCharMap;
+    mutable vcl::FontCapabilities maFontCapabilities;
+    mutable bool mbFontCapabilitiesRead;
 
     explicit PhysicalFontFace(const FontAttributes&);
 };

vcl/inc/qt5/QtFontFace.hxx
@@ -25,8 +25,6 @@
 #include <font/PhysicalFontFace.hxx>
 
 #include <tools/ref.hxx>
-#include <vcl/fontcapabilities.hxx>
-#include <vcl/fontcharmap.hxx>
 
 #include <QtCore/QString>
 #include <QtGui/QFont>
@@ -52,8 +50,6 @@ public:
 
     QFont CreateFont() const;
 
-    bool GetFontCapabilities(vcl::FontCapabilities&) const override;
-
     rtl::Reference<LogicalFontInstance>
     CreateFontInstance(const vcl::font::FontSelectPattern& rFSD) const override;
 
@@ -67,8 +63,6 @@ private:
 
     const QString m_aFontId;
     const FontIdType m_eFontIdType;
-    mutable vcl::FontCapabilities m_aFontCapabilities;
-    mutable bool m_bFontCapabilitiesRead;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/inc/quartz/salgdi.h
@@ -69,16 +69,12 @@ public:
     int                             GetFontTable( uint32_t nTagCode, unsigned char* ) const;
     int                             GetFontTable( const char pTagName[5], unsigned char* ) const;
 
-    bool GetFontCapabilities(vcl::FontCapabilities&) const override;
-
     rtl::Reference<LogicalFontInstance> CreateFontInstance(const vcl::font::FontSelectPattern&) const override;
 
     virtual hb_blob_t*              GetHbTable(hb_tag_t nTag) const override;
 
 private:
     const sal_IntPtr                mnFontId;
-    mutable vcl::FontCapabilities   maFontCapabilities;
-    mutable bool                    mbFontCapabilitiesRead;
 };
 
 class CoreTextStyle final : public LogicalFontInstance

vcl/inc/unx/freetype_glyphcache.hxx
@@ -72,8 +72,6 @@ public:
 
     void                  AnnounceFont( vcl::font::PhysicalFontCollection* );
 
-    bool GetFontCapabilities(vcl::FontCapabilities&) const;
-
 private:
     friend class FreetypeManager;
     explicit FreetypeFontInfo(FontAttributes , FreetypeFontFile* const pFontFile,
@@ -100,16 +98,9 @@ public:
     virtual rtl::Reference<LogicalFontInstance> CreateFontInstance(const vcl::font::FontSelectPattern&) const override;
     virtual sal_IntPtr      GetFontId() const override { return mpFreetypeFontInfo->GetFontId(); }
 
-    inline bool GetFontCapabilities(vcl::FontCapabilities&) const override;
-
     virtual hb_face_t* GetHbFace() const override;
     virtual hb_blob_t* GetHbTable(hb_tag_t nTag) const override;
 };
-
-bool FreetypeFontFace::GetFontCapabilities(vcl::FontCapabilities& rFontCapabilities) const
-{
-    return mpFreetypeFontInfo->GetFontCapabilities(rFontCapabilities);
-}
 
 class SAL_DLLPUBLIC_RTTI FreetypeFontInstance final : public LogicalFontInstance
 {

vcl/inc/win/salgdi.h
@@ -71,24 +71,16 @@ public:
     BYTE                    GetCharSet() const          { return meWinCharSet; }
     BYTE                    GetPitchAndFamily() const   { return mnPitchAndFamily; }
 
-    bool GetFontCapabilities(vcl::FontCapabilities&) const override;
-
     virtual hb_blob_t*      GetHbTable(hb_tag_t nTag) const override;
 
 private:
     sal_IntPtr              mnId;
-
-    // some members that are initialized lazily when the font gets selected into a HDC
-    mutable bool                    mbFontCapabilitiesRead;
-    mutable vcl::FontCapabilities   maFontCapabilities;
 
     BYTE                    meWinCharSet;
     BYTE                    mnPitchAndFamily;
     bool                    mbAliasSymbolsHigh;
     bool                    mbAliasSymbolsLow;
     HFONT                   mhFont;
-
-    void                    GetFontCapabilities() const;
 };
 
 /** Class that creates (and destroys) a compatible Device Context.

vcl/qt5/QtFontFace.cxx
@@ -25,8 +25,6 @@
 #include <QtFont.hxx>
 #include <QtTools.hxx>
 
-#include <sft.hxx>
-#include <impfontcharmap.hxx>
 #include <fontinstance.hxx>
 #include <font/FontSelectPattern.hxx>
 #include <font/PhysicalFontCollection.hxx>
@@ -163,7 +161,6 @@ QtFontFace::QtFontFace(const FontAttributes& rFA, QString aFontID, const FontIdT
     : PhysicalFontFace(rFA)
     , m_aFontId(std::move(aFontID))
     , m_eFontIdType(eFontIdType)
-    , m_bFontCapabilitiesRead(false)
 {
 }
 
@@ -203,30 +200,6 @@ rtl::Reference<LogicalFontInstance>
 QtFontFace::CreateFontInstance(const vcl::font::FontSelectPattern& rFSD) const
 {
     return new QtFont(*this, rFSD);
-}
-
-bool QtFontFace::GetFontCapabilities(vcl::FontCapabilities& rFontCapabilities) const
-{
-    // read this only once per font
-    if (m_bFontCapabilitiesRead)
-    {
-        rFontCapabilities = m_aFontCapabilities;
-        return rFontCapabilities.oUnicodeRange || rFontCapabilities.oCodePageRange;
-    }
-    m_bFontCapabilitiesRead = true;
-
-    QFont aFont = CreateFont();
-    QRawFont aRawFont(QRawFont::fromFont(aFont));
-    QByteArray aOS2Table = aRawFont.fontTable("OS/2");
-    if (!aOS2Table.isEmpty())
-    {
-        vcl::getTTCoverage(m_aFontCapabilities.oUnicodeRange, m_aFontCapabilities.oCodePageRange,
-                           reinterpret_cast<const unsigned char*>(aOS2Table.data()),
-                           aOS2Table.size());
-    }
-
-    rFontCapabilities = m_aFontCapabilities;
-    return rFontCapabilities.oUnicodeRange || rFontCapabilities.oCodePageRange;
 }
 
 hb_blob_t* QtFontFace::GetHbTable(hb_tag_t nTag) const

vcl/quartz/AquaGraphicsBackend.cxx
@@ -43,7 +43,6 @@
 #ifdef IOS
 #include <svdata.hxx>
 #endif
-#include <sft.hxx>
 
 using namespace vcl;
 

commit d5cac5f7abb0b43fc0d1e34e9798d9098a8bc4f1

Missing initialization of PhysicalFontFace::mbFontCapabilitiesRead
Regression introduced with 836600e40f0963b76f097572d798b73596c96eee "vcl:
Consolidate PhysicalFontFace::GetFontCapabilities()", causing

> vcl/source/font/PhysicalFontFace.cxx:252:10: runtime error: load of value 190, which is not a valid value for type 'bool'
>  #0 in vcl::font::PhysicalFontFace::GetFontCapabilities(vcl::FontCapabilities&) const at vcl/source/font/PhysicalFontFace.cxx:252:10
>  #1 in FreeTypeTextRenderImpl::GetFontCapabilities(vcl::FontCapabilities&) const at vcl/unx/generic/gdi/freetypetextrender.cxx:83:46
>  #2 in SvpSalGraphics::GetFontCapabilities(vcl::FontCapabilities&) const at vcl/headless/svptext.cxx:46:30
>  #3 in OutputDevice::GetFontCapabilities(vcl::FontCapabilities&) const at vcl/source/outdev/font.cxx:262:24
>  #4 in makeShortRepresentativeTextForSelectedFont(OutputDevice const&) at svtools/source/misc/sampletext.cxx:1255:22
>  #5 in DrawPreview(FontMetric const&, Point const&, OutputDevice&, bool) at svtools/source/control/ctrlbox.cxx:634:27
>  #6 in FontNameBox::CachePreview(unsigned long, Point*) at svtools/source/control/ctrlbox.cxx:785:9
>  #7 in FontNameBox::UpdateHdl(Timer*) at svtools/source/control/ctrlbox.cxx:564:5
>  #8 in FontNameBox::LinkStubUpdateHdl(void*, Timer*) at svtools/source/control/ctrlbox.cxx:562:1
>  #9 in Link<Timer*, void>::Call(Timer*) const at include/tools/link.hxx:111:45

during CppunitTest_toolkit_a11y.

Change-Id: I3591c4722410a8c6d1ae539f71a3d8ac37a4d3a3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139464
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

vcl/source/font/PhysicalFontFace.cxx
@@ -38,6 +38,7 @@ namespace vcl::font
 PhysicalFontFace::PhysicalFontFace(const FontAttributes& rDFA)
     : FontAttributes(rDFA)
     , mpHbFace(nullptr)
+    , mbFontCapabilitiesRead(false)
 {
     // StarSymbol is a unicode font, but it still deserves the symbol flag
     if (!IsSymbolFont())