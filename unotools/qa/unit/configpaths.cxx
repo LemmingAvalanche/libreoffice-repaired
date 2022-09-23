LemmingAvalanche/libreoffice-repaired

commit e16acf2c62cdca6da4a4897a80e5f043f1ea746e

use more string_view in unotools
Change-Id: Iaf91f9c63a0a666250e92a5ba7bebdb06dffb258
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140233
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unotools/source/config/configpaths.cxx
@@ -72,72 +72,75 @@ void lcl_resolveCharEntities(OUString & aLocalString)
     aLocalString = aResult.makeStringAndClear();
 }
 
-bool splitLastFromConfigurationPath(OUString const& _sInPath,
+bool splitLastFromConfigurationPath(std::u16string_view _sInPath,
                                         OUString& _rsOutPath,
                                         OUString& _rsLocalName)
 {
-    sal_Int32 nStart,nEnd;
+    size_t nStart,nEnd;
 
-    sal_Int32 nPos = _sInPath.getLength()-1;
+    size_t nPos = _sInPath.size()-1;
 
     // strip trailing slash
-    if (nPos > 0 && _sInPath[ nPos ] == '/')
+    if (nPos != std::u16string_view::npos && _sInPath[ nPos ] == '/')
     {
         OSL_FAIL("Invalid config path: trailing '/' is not allowed");
         --nPos;
     }
 
     // check for predicate ['xxx'] or ["yyy"]
-    if (nPos  > 0 && _sInPath[ nPos ] == ']')
+    if (nPos != std::u16string_view::npos && _sInPath[ nPos ] == ']')
     {
         sal_Unicode chQuote = _sInPath[--nPos];
 
         if (chQuote == '\'' || chQuote == '\"')
         {
             nEnd = nPos;
-            nPos = _sInPath.lastIndexOf(chQuote,nEnd);
+            nPos = _sInPath.find(chQuote,nEnd);
             nStart = nPos + 1;
             --nPos; // nPos = rInPath.lastIndexOf('[',nPos);
         }
         else // allow [xxx]
         {
             nEnd = nPos + 1;
-            nPos = _sInPath.lastIndexOf('[',nEnd);
+            nPos = _sInPath.rfind('[',nEnd);
             nStart = nPos + 1;
         }
 
-        OSL_ENSURE(nPos >= 0 && _sInPath[nPos] == '[', "Invalid config path: unmatched quotes or brackets");
-        if (nPos >= 0 && _sInPath[nPos] == '[')
+        OSL_ENSURE(nPos != std::u16string_view::npos && _sInPath[nPos] == '[', "Invalid config path: unmatched quotes or brackets");
+        if (nPos != std::u16string_view::npos && _sInPath[nPos] == '[')
         {
-            nPos =  _sInPath.lastIndexOf('/',nPos);
+            nPos =  _sInPath.rfind('/',nPos);
         }
         else // defined behavior for invalid paths
         {
             nStart = 0;
-            nEnd = _sInPath.getLength();
-            nPos = -1;
+            nEnd = _sInPath.size();
+            nPos = std::u16string_view::npos;
         }
 
     }
     else
     {
         nEnd = nPos+1;
-        nPos = _sInPath.lastIndexOf('/',nEnd);
+        nPos = _sInPath.rfind('/',nEnd);
         nStart = nPos + 1;
     }
-    OSL_ASSERT( -1 <= nPos &&
+    OSL_ASSERT( nPos != std::u16string_view::npos &&
                 nPos < nStart &&
                 nStart < nEnd &&
-                nEnd <= _sInPath.getLength() );
+                nEnd <= _sInPath.size() );
 
-    OSL_ASSERT(nPos == -1 || _sInPath[nPos] == '/');
+    OSL_ASSERT(nPos == std::u16string_view::npos || _sInPath[nPos] == '/');
     OSL_ENSURE(nPos != 0 , "Invalid config child path: immediate child of root");
 
-    _rsLocalName = _sInPath.copy(nStart, nEnd-nStart);
-    _rsOutPath = (nPos > 0) ? _sInPath.copy(0,nPos) : OUString();
+    _rsLocalName = _sInPath.substr(nStart, nEnd-nStart);
+    if (nPos > 0 && nPos != std::u16string_view::npos)
+        _rsOutPath = _sInPath.substr(0,nPos);
+    else
+        _rsOutPath.clear();
     lcl_resolveCharEntities(_rsLocalName);
 
-    return nPos >= 0;
+    return nPos != std::u16string_view::npos;
 }
 
 OUString extractFirstFromConfigurationPath(OUString const& _sInPath, OUString* _sOutPath)

filter/source/config/cache/filtercache.cxx
@@ -840,7 +840,7 @@ css::uno::Reference< css::uno::XInterface > FilterCache::impl_openConfig(EConfig
     return *pConfig;
 }
 
-css::uno::Any FilterCache::impl_getDirectCFGValue(const OUString& sDirectKey)
+css::uno::Any FilterCache::impl_getDirectCFGValue(std::u16string_view sDirectKey)
 {
     OUString sRoot;
     OUString sKey ;

filter/source/config/cache/filtercache.hxx
@@ -664,7 +664,7 @@ class FilterCache : public cppu::BaseMutex
                         Can be empty if an internal error occurred or if the requested
                         key does not exists!
          */
-        css::uno::Any impl_getDirectCFGValue(const OUString& sDirectKey);
+        css::uno::Any impl_getDirectCFGValue(std::u16string_view sDirectKey);
 
 
         /** @short      load the underlying configuration into this cache.

include/unotools/configpaths.hxx
@@ -51,7 +51,7 @@ namespace utl
             <FALSE/>, if the path was a one-level path or an invalid path
 
     */
-    UNOTOOLS_DLLPUBLIC bool splitLastFromConfigurationPath(OUString const& _sInPath,
+    UNOTOOLS_DLLPUBLIC bool splitLastFromConfigurationPath(std::u16string_view _sInPath,
                                             OUString& _rsOutPath,
                                             OUString& _rsLocalName);
 

commit cc79021b041579dfcd3229588bfb585ed4bfbacb

Add tests for utl::splitLastFromConfigurationPath
...documenting how the implementation is in discordance with the documentation
for some corner cases and how things got recently broken with
e16acf2c62cdca6da4a4897a80e5f043f1ea746e "use more string_view in unotools", in
preparation for an upcoming improvement of that function's implementation

Change-Id: Ia47243c64b724009c5ed5eecb9d890820287e9ea
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140492
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

unotools/CppunitTest_unotools_configpaths.mk
@@ -1,0 +1,21 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t; fill-column: 100 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,unotools_configpaths))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,unotools_configpaths, \
+    unotools/qa/unit/configpaths \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,unotools_configpaths, \
+    sal \
+    utl \
+))
+
+# vim: set noet sw=4 ts=4:

unotools/Module_unotools.mk
@@ -17,6 +17,7 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,unotools,\
     JunitTest_unotools_complex \
 ))
 $(eval $(call gb_Module_add_check_targets,unotools,\
+    CppunitTest_unotools_configpaths \
     CppunitTest_unotools_fontdefs \
 ))
 

unotools/qa/unit/configpaths.cxx
@@ -1,0 +1,128 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/config.h>
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include <unotools/configpaths.hxx>
+
+namespace
+{
+class Test : public CppUnit::TestFixture
+{
+public:
+    void testSplitLastFromConfigurationPath()
+    {
+        {
+            OUString path, last;
+            CPPUNIT_ASSERT(!utl::splitLastFromConfigurationPath(u"", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString(""), path);
+            CPPUNIT_ASSERT_EQUAL(OUString(""), last);
+        }
+        {
+            // Already prior to 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+            // unotools", and in discordance with the documentation, this returned true (but
+            // "@returns <FALSE/>, if the path was a one-level path or an invalid path"):
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(u"/", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString(""), path);
+            CPPUNIT_ASSERT_EQUAL(OUString(""), last);
+        }
+        {
+            // Already prior to 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+            // unotools", and in discordance with the documentation, this returned true (but
+            // "@returns <FALSE/>, if the path was a one-level path or an invalid path"):
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(u"/foo", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString(""), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("foo"), last);
+        }
+        // TODO: Broken since 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+        // unotools" (expected "" vs. actual "/foo"):
+        if ((false))
+        {
+            // Already prior to 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+            // unotools", and in discordance with the documentation, this returned true (but
+            // "@returns <FALSE/>, if the path was a one-level path or an invalid path"):
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(u"/foo/", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString(""), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("foo"), last);
+        }
+        {
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(u"/foo/bar/baz", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString("/foo/bar"), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("baz"), last);
+        }
+        // TODO: Broken since 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+        // unotools" (expected "/foo/bar" vs. actual "/foo/bar/baz"):
+        if ((false))
+        {
+            // Trailing slash accepted for backwards compatibility (cf
+            // . "for backwards compatibility, ignore a final slash" comment in
+            // Data::resolvePathRepresentation, configmgr/source/data.cxx):
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(u"/foo/bar/baz/", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString("/foo/bar"), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("baz"), last);
+        }
+        // TODO: Broken since 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+        // unotools" (expected true vs. actual false return):
+        if ((false))
+        {
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(
+                u"/foo/bar/baz['abc']/baz['de&amp;f']", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString("/foo/bar/baz['abc']"), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("de&f"), last);
+        }
+        {
+            OUString path, last;
+            CPPUNIT_ASSERT(!utl::splitLastFromConfigurationPath(u"foo", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString(""), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("foo"), last);
+        }
+        // TODO: Broken since 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+        // unotools" (expected false vs. actual true return):
+        if ((false))
+        {
+            // Already prior to 5edefc801fb48559c8064003f23d22d838710ee4 "use more string_view in
+            // unotools", and in discordance with the documentation, this set last to "foo" rather
+            // than "foo/" (but "If <var>_sInPath</var> could not be parsed as a valid configuration
+            // path, this is set to <var>_sInPath</var>"):
+            OUString path, last;
+            CPPUNIT_ASSERT(!utl::splitLastFromConfigurationPath(u"foo/", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString(""), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("foo"), last);
+        }
+        {
+            // Some broken input missing a leading slash happens to be considered OK:
+            OUString path, last;
+            CPPUNIT_ASSERT(utl::splitLastFromConfigurationPath(u"foo/bar/baz", path, last));
+            CPPUNIT_ASSERT_EQUAL(OUString("foo/bar"), path);
+            CPPUNIT_ASSERT_EQUAL(OUString("baz"), last);
+        }
+    }
+
+    CPPUNIT_TEST_SUITE(Test);
+    CPPUNIT_TEST(testSplitLastFromConfigurationPath);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Test);
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */