LemmingAvalanche/libreoffice-repaired

commit faf9ac08c5999eb8da8d1aa745f040ce828806e2

tdf#98705 Replace GetCaseCorrectPathName with GetLongPathNameW
Change-Id: I1d03267cd67da4c155248b585ca59dc5191d28f9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130144
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sal/osl/w32/file_url.cxx
@@ -542,134 +542,6 @@ static bool IsPathSpecialPrefix(LPWSTR szPath, LPWSTR szFile)
     return false;
 }
 
-// Expects a proper absolute or relative path. NB: It is different from GetLongPathName WinAPI!
-static DWORD GetCaseCorrectPathNameEx(
-    LPWSTR  lpszPath,   // path buffer to convert
-    sal_uInt32 cchBuffer,      // size of path buffer
-    DWORD   nSkipLevels,
-    bool bCheckExistence )
-{
-        ::osl::LongPathBuffer< WCHAR > szFile( MAX_PATH + 1 );
-        sal_Int32 nRemoved = PathRemoveFileSpec( lpszPath, szFile, MAX_PATH + 1 );
-        sal_Int32 nLastStepRemoved = nRemoved;
-        while ( nLastStepRemoved && szFile[0] == 0 )
-        {
-            // remove separators
-            nLastStepRemoved = PathRemoveFileSpec( lpszPath, szFile, MAX_PATH + 1 );
-            nRemoved += nLastStepRemoved;
-        }
-
-        if ( nRemoved )
-        {
-            bool bSkipThis = false;
-
-            if ( 0 == wcscmp( szFile, L".." ) )
-            {
-                bSkipThis = true;
-                nSkipLevels += 1;
-            }
-            else if ( 0 == wcscmp( szFile, L"." ) )
-            {
-                bSkipThis = true;
-            }
-            else if ( nSkipLevels )
-            {
-                bSkipThis = true;
-                nSkipLevels--;
-            }
-            else
-                bSkipThis = false;
-
-            if ( !GetCaseCorrectPathNameEx( lpszPath, cchBuffer, nSkipLevels, bCheckExistence ) )
-                return 0;
-
-            PathAddBackslash( lpszPath, cchBuffer );
-
-            /* Analyze parent if not only a trailing backslash was cut but a real file spec */
-            if ( !bSkipThis )
-            {
-                if ( bCheckExistence )
-                {
-
-                    if (IsPathSpecialPrefix(lpszPath, szFile))
-                    {
-                        /* add the segment name back */
-                        wcscat(lpszPath, szFile);
-                    }
-                    else
-                    {
-                        osl::LongPathBuffer<WCHAR> aShortPath(MAX_LONG_PATH);
-                        wcscpy(aShortPath, lpszPath);
-                        wcscat(aShortPath, szFile);
-
-                        WIN32_FIND_DATAW aFindFileData;
-                        HANDLE hFind = FindFirstFileW(aShortPath, &aFindFileData);
-
-                        if (IsValidHandle(hFind))
-                        {
-                            wcscat(lpszPath, aFindFileData.cFileName[0]
-                                                 ? aFindFileData.cFileName
-                                                 : aFindFileData.cAlternateFileName);
-
-                            FindClose(hFind);
-                        }
-                        else
-                            lpszPath[0] = 0;
-                    }
-                }
-                else
-                {
-                    /* add the segment name back */
-                    wcscat( lpszPath, szFile );
-                }
-            }
-        }
-        else
-        {
-            /* File specification can't be removed therefore the short path is either a drive
-               or a network share. If still levels to skip are left, the path specification
-               tries to travel below the file system root */
-            if ( nSkipLevels )
-                    lpszPath[0] = 0;
-            else
-                _wcsupr( lpszPath );
-        }
-
-        return wcslen( lpszPath );
-}
-
-DWORD GetCaseCorrectPathName(
-    LPCWSTR lpszShortPath,  // file name
-    LPWSTR  lpszLongPath,   // path buffer
-    sal_uInt32 cchBuffer,      // size of path buffer
-    bool bCheckExistence
-)
-{
-    /* Special handling for "\\.\" as system root */
-    if ( lpszShortPath && 0 == wcscmp( lpszShortPath, o3tl::toW(WSTR_SYSTEM_ROOT_PATH) ) )
-    {
-        if ( cchBuffer >= SAL_N_ELEMENTS(WSTR_SYSTEM_ROOT_PATH) )
-        {
-            wcscpy( lpszLongPath, o3tl::toW(WSTR_SYSTEM_ROOT_PATH) );
-            return SAL_N_ELEMENTS(WSTR_SYSTEM_ROOT_PATH) - 1;
-        }
-        else
-        {
-            return SAL_N_ELEMENTS(WSTR_SYSTEM_ROOT_PATH) - 1;
-        }
-    }
-    else if ( lpszShortPath )
-    {
-        if ( wcslen( lpszShortPath ) <= cchBuffer )
-        {
-            wcscpy( lpszLongPath, lpszShortPath );
-            return GetCaseCorrectPathNameEx( lpszLongPath, cchBuffer, 0, bCheckExistence );
-        }
-    }
-
-    return 0;
-}
-
 static std::optional<OUString> osl_decodeURL_(const OString& sUTF8)
 {
     const char  *pSrcEnd;
@@ -841,10 +713,9 @@ oslFileError osl_getSystemPathFromFileURL_( rtl_uString *strURL, rtl_uString **p
                 else
                 {
                     ::osl::LongPathBuffer< sal_Unicode > aBuf( MAX_LONG_PATH );
-                    sal_uInt32 nNewLen = GetCaseCorrectPathName( o3tl::toW(sDecodedURL->getStr()) + nSkip,
+                    sal_uInt32 nNewLen = GetLongPathNameW( o3tl::toW(sDecodedURL->getStr()) + nSkip,
                                                                  o3tl::toW(aBuf),
-                                                                 aBuf.getBufSizeInSymbols(),
-                                                                 false );
+                                                                 aBuf.getBufSizeInSymbols() );
 
                     if ( nNewLen <= MAX_PATH - 12
                       || sDecodedURL->matchIgnoreAsciiCase(WSTR_SYSTEM_ROOT_PATH, nSkip)

sal/osl/w32/file_dirvol.cxx
@@ -1590,14 +1590,22 @@ oslFileError SAL_CALL osl_getFileStatus(
     {
         if ( !pItemImpl->bFullPathNormalized )
         {
-            ::osl::LongPathBuffer< sal_Unicode > aBuffer( MAX_LONG_PATH );
-            sal_uInt32 nNewLen = GetCaseCorrectPathName( o3tl::toW( sFullPath.getStr() ),
-                                                         o3tl::toW( aBuffer ),
-                                                         aBuffer.getBufSizeInSymbols(),
-                                                         true );
+            ::osl::LongPathBuffer<sal_Unicode> aBuffer(MAX_LONG_PATH);
+            sal_uInt32 nNewLen = GetLongPathNameW(o3tl::toW(sFullPath.getStr()), o3tl::toW(aBuffer),
+                                                 aBuffer.getBufSizeInSymbols());
 
             if ( nNewLen )
             {
+                /* Capitalizes drive name (single letter). Windows file paths are processed
+                case-sensitively. While parsing a path, function osl_DirectoryItem has case
+                PATHTYPE_VOLUME for drives, and capatalizes them. That can be overwritten by
+                function osl_getFileStatus, in it win32 api GetLongPathNameW does no
+                capatilization. Thus it needs to be postprocessed.*/
+                sal_Int32 nIndex = rtl_ustr_indexOfChar(aBuffer, ':');
+                if (nIndex > 0) {
+                    aBuffer[nIndex - 1] = rtl::toAsciiUpperCase(aBuffer[nIndex - 1]);
+                }
+
                 pItemImpl->m_sFullPath = OUString(&*aBuffer, nNewLen);
                 sFullPath = pItemImpl->m_sFullPath;
                 pItemImpl->bFullPathNormalized = true;

sal/osl/w32/file_url.hxx
@@ -52,13 +52,6 @@ DWORD IsValidFilePath (
     rtl_uString ** corrected
 );
 
-DWORD GetCaseCorrectPathName (
-    LPCWSTR lpszShortPath,  // file name
-    LPWSTR  lpszLongPath,   // path buffer
-    sal_uInt32 cchBuffer,   // size of path buffer
-    bool bCheckExistence
-);
-
 oslFileError osl_getSystemPathFromFileURL_ (
     rtl_uString * strURL,
     rtl_uString ** pustrPath,

commit d5a18461203b3a248ba7a6cabf7af9abc9af322b

tdf#151117: Process non-existent long paths correctly
Regression after commit faf9ac08c5999eb8da8d1aa745f040ce828806e2
  Author Kunal Pawar <hellokunalpawar@gmail.com>
  Date   Fri Feb 18 19:15:04 2022 +0530
    tdf#98705 Replace GetCaseCorrectPathName with GetLongPathNameW

The fix tries to keep the performance improvement, and when the path
exists, it will only call GetLongPathNameW once. Anyway, for unclear
reason, this normalization only happens on long paths.

Change-Id: I1cf9a47dfc35046ec1b5eebbbcaca09edb1c471a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140516
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sal/osl/w32/file_url.cxx
@@ -41,14 +41,14 @@
 
 #include "path_helper.hxx"
 
-#define WSTR_SYSTEM_ROOT_PATH               u"\\\\.\\"
-#define WSTR_LONG_PATH_PREFIX               u"\\\\?\\"
-#define WSTR_LONG_PATH_PREFIX_UNC           u"\\\\?\\UNC\\"
-
 // FileURL functions
 
 namespace
 {
+constexpr std::u16string_view WSTR_SYSTEM_ROOT_PATH = u"\\\\.\\";
+constexpr std::u16string_view WSTR_LONG_PATH_PREFIX = u"\\\\?\\";
+constexpr std::u16string_view WSTR_LONG_PATH_PREFIX_UNC = u"\\\\?\\UNC\\";
+
 // Internal functions that expect only backslashes as path separators
 
 bool startsWithDriveColon(std::u16string_view s)
@@ -308,13 +308,13 @@ DWORD IsValidFilePath(const OUString& path, DWORD dwFlags, OUString* corrected)
         if (path.matchIgnoreAsciiCase(WSTR_LONG_PATH_PREFIX_UNC))
         {
             /* This is long path in UNC notation */
-            oComponent = path.subView(SAL_N_ELEMENTS(WSTR_LONG_PATH_PREFIX_UNC) - 1);
+            oComponent = path.subView(WSTR_LONG_PATH_PREFIX_UNC.size());
             dwCandidatPathType = PATHTYPE_ABSOLUTE_UNC | PATHTYPE_IS_LONGPATH;
         }
         else if (path.matchIgnoreAsciiCase(WSTR_LONG_PATH_PREFIX))
         {
             /* This is long path */
-            oComponent = path.subView(SAL_N_ELEMENTS(WSTR_LONG_PATH_PREFIX) - 1);
+            oComponent = path.subView(WSTR_LONG_PATH_PREFIX.size());
 
             if (startsWithDriveColon(*oComponent))
             {
@@ -567,6 +567,71 @@ static OUString osl_encodeURL_(std::u16string_view sURL)
     return sEncodedURL.makeStringAndClear();
 }
 
+// A helper that makes sure that for existing part of the path, the case is correct.
+// Unlike GetLongPathNameW that it wraps, this function does not require the path to exist.
+static OUString GetCaseCorrectPathName(std::u16string_view sysPath)
+{
+    // Prepare a null-terminated string first.
+    // Neither OUString, nor u16string_view are guaranteed to be null-terminated
+    osl::LongPathBuffer<wchar_t> szPath(sysPath.size() + WSTR_LONG_PATH_PREFIX_UNC.size() + 1);
+    wchar_t* const pPath = szPath;
+    wchar_t* pEnd = pPath;
+    size_t sysPathOffset = 0;
+    if (sysPath.size() >= MAX_PATH && isAbsolute(sysPath)
+        && !o3tl::starts_with(sysPath, WSTR_LONG_PATH_PREFIX))
+    {
+        // Allow GetLongPathNameW consume long paths
+        std::u16string_view prefix = WSTR_LONG_PATH_PREFIX;
+        if (startsWithSlashSlash(sysPath))
+        {
+            sysPathOffset = 2; // skip leading "\\"
+            prefix = WSTR_LONG_PATH_PREFIX_UNC;
+        }
+        pEnd = std::copy(prefix.begin(), prefix.end(), pEnd);
+    }
+    wchar_t* const pStart = pEnd;
+    pEnd = std::copy(sysPath.begin() + sysPathOffset, sysPath.end(), pStart);
+    *pEnd = 0;
+    osl::LongPathBuffer<wchar_t> aBuf(MAX_LONG_PATH);
+    while (pEnd > pStart)
+    {
+        std::u16string_view curPath(o3tl::toU(pPath), pEnd - pPath);
+        if (curPath == u"\\\\" || curPath == WSTR_SYSTEM_ROOT_PATH
+            || curPath == WSTR_LONG_PATH_PREFIX
+            || o3tl::equalsIgnoreAsciiCase(curPath, WSTR_LONG_PATH_PREFIX_UNC))
+            break; // Do not check if the special path prefix exists itself
+
+        DWORD nNewLen = GetLongPathNameW(pPath, aBuf, aBuf.getBufSizeInSymbols());
+        if (nNewLen == 0)
+        {
+            // Error?
+            const DWORD err = GetLastError();
+            if (err == ERROR_FILE_NOT_FOUND || err == ERROR_PATH_NOT_FOUND)
+            {
+                // Check the base path; skip possible trailing separator
+                size_t sepPos = curPath.substr(0, curPath.size() - 1).rfind(u'\\');
+                if (sepPos != std::u16string_view::npos)
+                {
+                    pEnd = pPath + sepPos;
+                    *pEnd = 0;
+                    continue;
+                }
+            }
+            else
+            {
+                SAL_WARN("sal.osl", "GetLongPathNameW: Windows error code "
+                                        << err << " processing path " << OUString(curPath));
+            }
+            break; // All other errors, or no separators left
+        }
+        assert(nNewLen < aBuf.getBufSizeInSymbols());
+        // Combine the case-correct leading part with the non-existing trailing part
+        return OUString::Concat(std::u16string_view(o3tl::toU(aBuf), nNewLen))
+               + sysPath.substr(pEnd - pStart + sysPathOffset);
+    };
+    return OUString(sysPath); // We found no existing parts - just assume it's OK
+}
+
 oslFileError osl_getSystemPathFromFileURL_(const OUString& strURL, rtl_uString **pustrPath, bool bAllowRelative)
 {
     OUString sTempPath;
@@ -622,25 +687,21 @@ oslFileError osl_getSystemPathFromFileURL_(const OUString& strURL, rtl_uString *
                 }
                 else
                 {
-                    ::osl::LongPathBuffer< sal_Unicode > aBuf( MAX_LONG_PATH );
-                    sal_uInt32 nNewLen = GetLongPathNameW( o3tl::toW(sDecodedURL->getStr()) + nSkip,
-                                                                 o3tl::toW(aBuf),
-                                                                 aBuf.getBufSizeInSymbols() );
-
-                    if ( nNewLen <= MAX_PATH - 12
-                      || sDecodedURL->matchIgnoreAsciiCase(WSTR_SYSTEM_ROOT_PATH, nSkip)
-                      || sDecodedURL->matchIgnoreAsciiCase(WSTR_LONG_PATH_PREFIX, nSkip) )
+                    sDecodedURL = GetCaseCorrectPathName(sDecodedURL->subView(nSkip));
+                    if (sDecodedURL->getLength() <= MAX_PATH - 12
+                        || sDecodedURL->startsWith(WSTR_SYSTEM_ROOT_PATH)
+                        || sDecodedURL->startsWith(WSTR_LONG_PATH_PREFIX))
                     {
-                        sTempPath = std::u16string_view(aBuf, nNewLen);
+                        sTempPath = *sDecodedURL;
                     }
-                    else if ( sDecodedURL->match("\\\\", nSkip) )
+                    else if (sDecodedURL->startsWith("\\\\"))
                     {
                         /* it should be an UNC path, use the according prefix */
-                        sTempPath = OUString::Concat(WSTR_LONG_PATH_PREFIX_UNC) + std::u16string_view(aBuf + 2, nNewLen - 2);
+                        sTempPath = OUString::Concat(WSTR_LONG_PATH_PREFIX_UNC) + sDecodedURL->subView(2);
                     }
                     else
                     {
-                        sTempPath = OUString::Concat(WSTR_LONG_PATH_PREFIX) + std::u16string_view(aBuf, nNewLen);
+                        sTempPath = WSTR_LONG_PATH_PREFIX + *sDecodedURL;
                     }
                 }
             }
@@ -691,7 +752,7 @@ oslFileError osl_getFileURLFromSystemPath( rtl_uString* strPath, rtl_uString** p
             switch ( dwPathType & PATHTYPE_MASK_TYPE )
             {
                 case PATHTYPE_ABSOLUTE_UNC:
-                    static_assert(SAL_N_ELEMENTS(WSTR_LONG_PATH_PREFIX_UNC) - 1 == 8,
+                    static_assert(WSTR_LONG_PATH_PREFIX_UNC.size() == 8,
                                   "Unexpected long path UNC prefix!");
 
                     /* generate the normal UNC path */
@@ -699,7 +760,7 @@ oslFileError osl_getFileURLFromSystemPath( rtl_uString* strPath, rtl_uString** p
                     break;
 
                 case PATHTYPE_ABSOLUTE_LOCAL:
-                    static_assert(SAL_N_ELEMENTS(WSTR_LONG_PATH_PREFIX) - 1 == 4,
+                    static_assert(WSTR_LONG_PATH_PREFIX.size() == 4,
                                   "Unexpected long path prefix!");
 
                     /* generate the normal path */