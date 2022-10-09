LemmingAvalanche/libreoffice-repaired

commit c01c3053fc1b87522b9c23b739d97ea391044950

use more concrete types in chart2, Axis
Change-Id: If80b6487ad2b8ac75f98f798b839aff2b8a5c23e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129522
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/inc/ChartType.hxx
@@ -35,6 +35,7 @@
 
 namespace chart
 {
+class BaseCoordinateSystem;
 class DataSeries;
 class ModifyEventForwarder;
 
@@ -101,6 +102,9 @@ public:
     void setDataSeries(
         const std::vector< rtl::Reference< ::chart::DataSeries > >& aDataSeries );
     const std::vector< rtl::Reference< ::chart::DataSeries > > & getDataSeries2() const { return m_aDataSeries; }
+
+    static rtl::Reference< ::chart::BaseCoordinateSystem >
+        createCoordinateSystem2( ::sal_Int32 DimensionCount );
 
 protected:
 

chart2/source/model/main/BaseCoordinateSystem.cxx
@@ -22,7 +22,7 @@
 #include <UserDefinedProperties.hxx>
 #include <CloneHelper.hxx>
 #include <ModifyListenerHelper.hxx>
-#include "Axis.hxx"
+#include <Axis.hxx>
 #include <ChartType.hxx>
 #include <com/sun/star/chart2/AxisType.hpp>
 #include <com/sun/star/container/NoSuchElementException.hpp>
@@ -125,7 +125,7 @@ BaseCoordinateSystem::BaseCoordinateSystem(
     for( sal_Int32 nN=0; nN<m_nDimensionCount; nN++ )
     {
         m_aAllAxis[nN].resize( 1 );
-        Reference< chart2::XAxis > xAxis( new Axis );
+        rtl::Reference< Axis > xAxis( new Axis );
         m_aAllAxis[nN][0] = xAxis;
 
         ModifyListenerHelper::addListenerToAllElements( m_aAllAxis[nN], m_xModifyEventForwarder );
@@ -159,7 +159,7 @@ BaseCoordinateSystem::BaseCoordinateSystem(
     m_aAllAxis.resize(rSource.m_aAllAxis.size());
     tAxisVecVecType::size_type nN=0;
     for( nN=0; nN<m_aAllAxis.size(); nN++ )
-        CloneHelper::CloneRefVector<chart2::XAxis>( rSource.m_aAllAxis[nN], m_aAllAxis[nN] );
+        CloneHelper::CloneRefVector( rSource.m_aAllAxis[nN], m_aAllAxis[nN] );
     for (const auto & rxChartType : rSource.m_aChartTypes)
         m_aChartTypes.push_back(rxChartType->cloneChartType());
 
@@ -207,7 +207,33 @@ void SAL_CALL BaseCoordinateSystem::setAxisByDimension(
         m_aAllAxis[ nDimensionIndex ][nIndex] = nullptr;
     }
 
-    Reference< chart2::XAxis > xOldAxis( m_aAllAxis[ nDimensionIndex ][nIndex] );
+    rtl::Reference< Axis > xOldAxis( m_aAllAxis[ nDimensionIndex ][nIndex] );
+    if( xOldAxis.is())
+        ModifyListenerHelper::removeListener( xOldAxis, m_xModifyEventForwarder );
+    m_aAllAxis[ nDimensionIndex ][nIndex] = dynamic_cast<Axis*>(xAxis.get());
+    if( xAxis.is())
+        ModifyListenerHelper::addListener( xAxis, m_xModifyEventForwarder );
+    fireModifyEvent();
+}
+
+void BaseCoordinateSystem::setAxisByDimension(
+    sal_Int32 nDimensionIndex,
+    const rtl::Reference< Axis >& xAxis,
+    sal_Int32 nIndex )
+{
+    if( nDimensionIndex < 0 || nDimensionIndex >= getDimension() )
+        throw lang::IndexOutOfBoundsException();
+
+    if( nIndex < 0 )
+        throw lang::IndexOutOfBoundsException();
+
+    if( m_aAllAxis[ nDimensionIndex ].size() < o3tl::make_unsigned( nIndex+1 ))
+    {
+        m_aAllAxis[ nDimensionIndex ].resize( nIndex+1 );
+        m_aAllAxis[ nDimensionIndex ][nIndex] = nullptr;
+    }
+
+    rtl::Reference< Axis > xOldAxis( m_aAllAxis[ nDimensionIndex ][nIndex] );
     if( xOldAxis.is())
         ModifyListenerHelper::removeListener( xOldAxis, m_xModifyEventForwarder );
     m_aAllAxis[ nDimensionIndex ][nIndex] = xAxis;
@@ -225,6 +251,20 @@ Reference< chart2::XAxis > SAL_CALL BaseCoordinateSystem::getAxisByDimension(
     OSL_ASSERT( m_aAllAxis.size() == static_cast< size_t >( getDimension()));
 
     if( nAxisIndex < 0 || nAxisIndex > getMaximumAxisIndexByDimension(nDimensionIndex) )
+        throw lang::IndexOutOfBoundsException();
+
+    return m_aAllAxis[ nDimensionIndex ][nAxisIndex];
+}
+
+rtl::Reference< Axis > BaseCoordinateSystem::getAxisByDimension2(
+            sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex ) const
+{
+    if( nDimensionIndex < 0 || nDimensionIndex >= m_nDimensionCount )
+        throw lang::IndexOutOfBoundsException();
+
+    OSL_ASSERT( m_aAllAxis.size() == static_cast< size_t >( m_nDimensionCount));
+
+    if( nAxisIndex < 0 || nAxisIndex > static_cast<sal_Int32>(m_aAllAxis[ nDimensionIndex ].size()) )
         throw lang::IndexOutOfBoundsException();
 
     return m_aAllAxis[ nDimensionIndex ][nAxisIndex];

chart2/source/model/template/BubbleChartType.cxx
@@ -21,6 +21,7 @@
 #include <PropertyHelper.hxx>
 #include <servicenames_charttypes.hxx>
 #include <CartesianCoordinateSystem.hxx>
+#include <Axis.hxx>
 #include <AxisHelper.hxx>
 #include <AxisIndexDefines.hxx>
 #include <com/sun/star/chart2/AxisType.hpp>
@@ -123,7 +124,7 @@ Reference< chart2::XCoordinateSystem > SAL_CALL
 
     for( sal_Int32 i=0; i<DimensionCount; ++i )
     {
-        Reference< chart2::XAxis > xAxis( xResult->getAxisByDimension( i, MAIN_AXIS_INDEX ) );
+        rtl::Reference< Axis > xAxis = xResult->getAxisByDimension2( i, MAIN_AXIS_INDEX );
         if( !xAxis.is() )
         {
             OSL_FAIL("a created coordinate system should have an axis for each dimension");

chart2/source/model/template/ChartTypeTemplate.cxx
@@ -25,6 +25,7 @@
 #include <DataSeries.hxx>
 #include <DataSource.hxx>
 
+#include <Axis.hxx>
 #include <AxisHelper.hxx>
 #include <Diagram.hxx>
 #include <DiagramHelper.hxx>
@@ -420,18 +421,14 @@ void ChartTypeTemplate::resetStyles( const rtl::Reference< ::chart::Diagram >& x
     bool bPercent = (getStackMode(0) == StackMode::YStackedPercent);
     if( bPercent )
     {
-        const Sequence< Reference< chart2::XAxis > > aAxisSeq( AxisHelper::getAllAxesOfDiagram( xDiagram ) );
-        for( Reference< chart2::XAxis > const & axis : aAxisSeq )
+        const std::vector< rtl::Reference< Axis > > aAxisSeq( AxisHelper::getAllAxesOfDiagram( xDiagram ) );
+        for( rtl::Reference< Axis > const & axis : aAxisSeq )
         {
             if( AxisHelper::getDimensionIndexOfAxis( axis, xDiagram )== 1 )
             {
-                Reference< beans::XPropertySet > xAxisProp( axis, uno::UNO_QUERY );
-                if( xAxisProp.is())
-                {
-                    // set number format to source format
-                    xAxisProp->setPropertyValue(CHART_UNONAME_LINK_TO_SRC_NUMFMT, uno::Any(true));
-                    xAxisProp->setPropertyValue(CHART_UNONAME_NUMFMT, uno::Any());
-                }
+                // set number format to source format
+                axis->setPropertyValue(CHART_UNONAME_LINK_TO_SRC_NUMFMT, uno::Any(true));
+                axis->setPropertyValue(CHART_UNONAME_NUMFMT, uno::Any());
             }
         }
     }
@@ -499,7 +496,7 @@ void ChartTypeTemplate::createCoordinateSystems(
     rtl::Reference< ChartType > xChartType( getChartTypeForNewSeries(aFormerlyUsedChartTypes));
     if( ! xChartType.is())
         return;
-    Reference< XCoordinateSystem > xCooSys( xChartType->createCoordinateSystem( getDimension()));
+    rtl::Reference< BaseCoordinateSystem > xCooSys = ChartType::createCoordinateSystem2( getDimension());
     if( ! xCooSys.is())
     {
         // chart type wants no coordinate systems
@@ -514,13 +511,13 @@ void ChartTypeTemplate::createCoordinateSystems(
             AxisHelper::makeGridVisible( xAxis->getGridProperties() );
     }
 
-    Sequence< Reference< XCoordinateSystem > > aCoordinateSystems(
-        xDiagram->getCoordinateSystems());
+    std::vector< rtl::Reference< BaseCoordinateSystem > > aCoordinateSystems(
+        xDiagram->getBaseCoordinateSystems());
 
-    if( aCoordinateSystems.hasElements())
+    if( !aCoordinateSystems.empty() )
     {
         bool bOk = true;
-        for( sal_Int32 i=0; bOk && i<aCoordinateSystems.getLength(); ++i )
+        for( sal_Int32 i=0; bOk && i<static_cast<sal_Int32>(aCoordinateSystems.size()); ++i )
             bOk = bOk && ( xCooSys->getCoordinateSystemType() == aCoordinateSystems[i]->getCoordinateSystemType() &&
                            (xCooSys->getDimension() == aCoordinateSystems[i]->getDimension()) );
         // coordinate systems are ok
@@ -530,9 +527,9 @@ void ChartTypeTemplate::createCoordinateSystems(
     }
 
     //copy as much info from former coordinate system as possible:
-    if( aCoordinateSystems.hasElements() )
+    if( !aCoordinateSystems.empty() )
     {
-        Reference< XCoordinateSystem > xOldCooSys( aCoordinateSystems[0] );
+        rtl::Reference< BaseCoordinateSystem > xOldCooSys( aCoordinateSystems[0] );
         sal_Int32 nMaxDimensionCount = std::min( xCooSys->getDimension(), xOldCooSys->getDimension() );
 
         for(sal_Int32 nDimensionIndex=0; nDimensionIndex<nMaxDimensionCount; nDimensionIndex++)
@@ -540,7 +537,7 @@ void ChartTypeTemplate::createCoordinateSystems(
             const sal_Int32 nMaximumAxisIndex = xOldCooSys->getMaximumAxisIndexByDimension(nDimensionIndex);
             for(sal_Int32 nAxisIndex=0; nAxisIndex<=nMaximumAxisIndex; ++nAxisIndex)
             {
-                uno::Reference< XAxis > xAxis( xOldCooSys->getAxisByDimension( nDimensionIndex, nAxisIndex ) );
+                rtl::Reference< Axis > xAxis = xOldCooSys->getAxisByDimension2( nDimensionIndex, nAxisIndex );
                 if( xAxis.is())
                 {
                     xCooSys->setAxisByDimension( nDimensionIndex, xAxis, nAxisIndex );
@@ -573,7 +570,7 @@ void ChartTypeTemplate::adaptScales(
                 const sal_Int32 nMaxIndex = xCooSys->getMaximumAxisIndexByDimension(nDimensionX);
                 for(sal_Int32 nI=0; nI<=nMaxIndex; ++nI)
                 {
-                    Reference< XAxis > xAxis( xCooSys->getAxisByDimension(nDimensionX,nI) );
+                    rtl::Reference< Axis > xAxis = xCooSys->getAxisByDimension2(nDimensionX,nI);
                     if( xAxis.is())
                     {
                         ScaleData aData( xAxis->getScaleData() );
@@ -606,7 +603,7 @@ void ChartTypeTemplate::adaptScales(
                 const sal_Int32 nMaxIndex = xCooSys->getMaximumAxisIndexByDimension(1);
                 for(sal_Int32 nI=0; nI<=nMaxIndex; ++nI)
                 {
-                    Reference< chart2::XAxis > xAxis( xCooSys->getAxisByDimension( 1,nI ));
+                    rtl::Reference< Axis > xAxis = xCooSys->getAxisByDimension2( 1,nI );
                     if( xAxis.is())
                     {
                         bool bPercent = (getStackMode(0) == StackMode::YStackedPercent);

chart2/source/model/template/NetChartType.cxx
@@ -22,6 +22,7 @@
 #include <servicenames_charttypes.hxx>
 #include <AxisIndexDefines.hxx>
 #include <AxisHelper.hxx>
+#include <Axis.hxx>
 
 #include <cppuhelper/supportsservice.hxx>
 #include <com/sun/star/chart2/AxisType.hpp>
@@ -58,7 +59,7 @@ Reference< XCoordinateSystem > SAL_CALL
     rtl::Reference< PolarCoordinateSystem > xResult =
         new PolarCoordinateSystem( DimensionCount );
 
-    Reference< XAxis > xAxis( xResult->getAxisByDimension( 0, MAIN_AXIS_INDEX ) );
+    rtl::Reference< Axis > xAxis = xResult->getAxisByDimension2( 0, MAIN_AXIS_INDEX );
     if( xAxis.is() )
     {
         ScaleData aScaleData = xAxis->getScaleData();
@@ -68,7 +69,7 @@ Reference< XCoordinateSystem > SAL_CALL
         xAxis->setScaleData( aScaleData );
     }
 
-    xAxis = xResult->getAxisByDimension( 1, MAIN_AXIS_INDEX );
+    xAxis = xResult->getAxisByDimension2( 1, MAIN_AXIS_INDEX );
     if( xAxis.is() )
     {
         ScaleData aScaleData = xAxis->getScaleData();

chart2/source/model/template/PieChartType.cxx
@@ -20,6 +20,7 @@
 #include "PieChartType.hxx"
 #include <PropertyHelper.hxx>
 #include <PolarCoordinateSystem.hxx>
+#include <Axis.hxx>
 #include <AxisHelper.hxx>
 #include <servicenames_charttypes.hxx>
 #include <AxisIndexDefines.hxx>
@@ -158,7 +159,7 @@ Reference< chart2::XCoordinateSystem > SAL_CALL
 
     for( sal_Int32 i=0; i<DimensionCount; ++i )
     {
-        Reference< chart2::XAxis > xAxis( xResult->getAxisByDimension( i, MAIN_AXIS_INDEX ) );
+        rtl::Reference< Axis > xAxis = xResult->getAxisByDimension2( i, MAIN_AXIS_INDEX );
         if( !xAxis.is() )
         {
             OSL_FAIL("a created coordinate system should have an axis for each dimension");

chart2/source/model/template/ScatterChartType.cxx
@@ -21,6 +21,7 @@
 #include <PropertyHelper.hxx>
 #include <servicenames_charttypes.hxx>
 #include <CartesianCoordinateSystem.hxx>
+#include <Axis.hxx>
 #include <AxisHelper.hxx>
 #include <AxisIndexDefines.hxx>
 #include <unonames.hxx>
@@ -168,7 +169,7 @@ Reference< chart2::XCoordinateSystem > SAL_CALL
 
     for( sal_Int32 i=0; i<DimensionCount; ++i )
     {
-        Reference< chart2::XAxis > xAxis( xResult->getAxisByDimension( i, MAIN_AXIS_INDEX ) );
+        rtl::Reference< Axis > xAxis = xResult->getAxisByDimension2( i, MAIN_AXIS_INDEX );
         if( !xAxis.is() )
         {
             OSL_FAIL("a created coordinate system should have an axis for each dimension");

chart2/source/controller/chartapiwrapper/AxisWrapper.cxx
@@ -18,6 +18,7 @@
  */
 
 #include "AxisWrapper.hxx"
+#include <Axis.hxx>
 #include <AxisHelper.hxx>
 #include <TitleHelper.hxx>
 #include "Chart2ModelContact.hxx"
@@ -574,7 +575,7 @@ awt::Size AxisWrapper::getCurrentSizeForReference()
 
 Reference< chart2::XAxis > AxisWrapper::getAxis()
 {
-    Reference< chart2::XAxis > xAxis;
+    rtl::Reference< Axis > xAxis;
     try
     {
         sal_Int32 nDimensionIndex = 0;
@@ -586,9 +587,8 @@ Reference< chart2::XAxis > AxisWrapper::getAxis()
         if( !xAxis.is() )
         {
             xAxis = AxisHelper::createAxis( nDimensionIndex, bMainAxis, xDiagram, m_spChart2ModelContact->m_xContext );
-            Reference< beans::XPropertySet > xProp( xAxis, uno::UNO_QUERY );
-            if( xProp.is() )
-                xProp->setPropertyValue("Show", uno::Any( false ) );
+            if( xAxis.is() )
+                xAxis->setPropertyValue("Show", uno::Any( false ) );
         }
     }
     catch( const uno::Exception & )

commit a5aec62cb5abec49ad7fff17b94c753511e854a5

tdf#151424 Spider web chart drawing error
regression from
    commit c01c3053fc1b87522b9c23b739d97ea391044950
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Sat Feb 5 14:58:07 2022 +0200
    use more concrete types in chart2, Axis

Change-Id: Ib80bbec9f08b9fafa6ee1c8d0cace8b44fdadabc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141135
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/inc/ChartType.hxx
@@ -69,7 +69,7 @@ public:
     // still abstract ! implement !
     virtual OUString SAL_CALL getChartType() override = 0;
     virtual css::uno::Reference< css::chart2::XCoordinateSystem > SAL_CALL
-        createCoordinateSystem( ::sal_Int32 DimensionCount ) override;
+        createCoordinateSystem( ::sal_Int32 DimensionCount ) final override;
     virtual css::uno::Sequence< OUString > SAL_CALL
         getSupportedMandatoryRoles() override;
     virtual css::uno::Sequence< OUString > SAL_CALL
@@ -103,8 +103,8 @@ public:
         const std::vector< rtl::Reference< ::chart::DataSeries > >& aDataSeries );
     const std::vector< rtl::Reference< ::chart::DataSeries > > & getDataSeries2() const { return m_aDataSeries; }
 
-    static rtl::Reference< ::chart::BaseCoordinateSystem >
-        createCoordinateSystem2( ::sal_Int32 DimensionCount );
+    virtual rtl::Reference< ::chart::BaseCoordinateSystem >
+        createCoordinateSystem2( sal_Int32 DimensionCount );
 
 protected:
 

chart2/source/model/main/BaseCoordinateSystem.cxx
@@ -201,6 +201,8 @@ void SAL_CALL BaseCoordinateSystem::setAxisByDimension(
     if( nIndex < 0 )
         throw lang::IndexOutOfBoundsException();
 
+    assert(!xAxis || dynamic_cast<Axis*>(xAxis.get()));
+
     if( m_aAllAxis[ nDimensionIndex ].size() < o3tl::make_unsigned( nIndex+1 ))
     {
         m_aAllAxis[ nDimensionIndex ].resize( nIndex+1 );

chart2/source/model/template/BubbleChartType.cxx
@@ -116,8 +116,8 @@ rtl::Reference< ChartType > BubbleChartType::cloneChartType() const
 }
 
 // ____ XChartType ____
-Reference< chart2::XCoordinateSystem > SAL_CALL
-    BubbleChartType::createCoordinateSystem( ::sal_Int32 DimensionCount )
+rtl::Reference< ::chart::BaseCoordinateSystem >
+    BubbleChartType::createCoordinateSystem2( sal_Int32 DimensionCount )
 {
     rtl::Reference< CartesianCoordinateSystem > xResult =
         new CartesianCoordinateSystem( DimensionCount );

chart2/source/model/template/BubbleChartType.hxx
@@ -47,8 +47,6 @@ private:
         getSupportedMandatoryRoles() override;
     virtual css::uno::Sequence< OUString > SAL_CALL
         getSupportedPropertyRoles() override;
-    virtual css::uno::Reference< css::chart2::XCoordinateSystem > SAL_CALL
-        createCoordinateSystem( ::sal_Int32 DimensionCount ) override;
     virtual OUString SAL_CALL getRoleOfSequenceForSeriesLabel() override;
 
     // ____ OPropertySet ____
@@ -63,6 +61,9 @@ private:
 
     // ____ XCloneable ____
     virtual css::uno::Reference< css::util::XCloneable > SAL_CALL createClone() override;
+
+    virtual rtl::Reference< ::chart::BaseCoordinateSystem >
+        createCoordinateSystem2( sal_Int32 DimensionCount ) override;
 };
 
 } //  namespace chart

chart2/source/model/template/ChartTypeTemplate.cxx
@@ -482,7 +482,7 @@ void ChartTypeTemplate::createCoordinateSystems(
     rtl::Reference< ChartType > xChartType( getChartTypeForNewSeries(aFormerlyUsedChartTypes));
     if( ! xChartType.is())
         return;
-    rtl::Reference< BaseCoordinateSystem > xCooSys = ChartType::createCoordinateSystem2( getDimension());
+    rtl::Reference< BaseCoordinateSystem > xCooSys = xChartType->createCoordinateSystem2( getDimension());
     if( ! xCooSys.is())
     {
         // chart type wants no coordinate systems

chart2/source/model/template/NetChartType.cxx
@@ -48,8 +48,8 @@ NetChartType_Base::NetChartType_Base( const NetChartType_Base & rOther ) :
 NetChartType_Base::~NetChartType_Base()
 {}
 
-Reference< XCoordinateSystem > SAL_CALL
-    NetChartType_Base::createCoordinateSystem( ::sal_Int32 DimensionCount )
+rtl::Reference< ::chart::BaseCoordinateSystem >
+    NetChartType_Base::createCoordinateSystem2( sal_Int32 DimensionCount )
 {
     if( DimensionCount != 2 )
         throw lang::IllegalArgumentException(

chart2/source/model/template/NetChartType.hxx
@@ -32,10 +32,6 @@ public:
 protected:
     explicit NetChartType_Base( const NetChartType_Base & rOther );
 
-    // ____ XChartType ____
-    virtual css::uno::Reference< css::chart2::XCoordinateSystem > SAL_CALL
-        createCoordinateSystem( ::sal_Int32 DimensionCount ) override;
-
     // ____ OPropertySet ____
     virtual void GetDefaultValue( sal_Int32 nHandle, css::uno::Any& rAny ) const override;
 
@@ -44,6 +40,9 @@ protected:
     // ____ XPropertySet ____
     virtual css::uno::Reference< css::beans::XPropertySetInfo > SAL_CALL
         getPropertySetInfo() override;
+
+    virtual rtl::Reference< ::chart::BaseCoordinateSystem >
+        createCoordinateSystem2( sal_Int32 DimensionCount ) override;
 };
 
 class NetChartType final : public NetChartType_Base

chart2/source/model/template/PieChartType.cxx
@@ -122,8 +122,8 @@ OUString SAL_CALL PieChartType::getChartType()
     return CHART2_SERVICE_NAME_CHARTTYPE_PIE;
 }
 
-Reference< chart2::XCoordinateSystem > SAL_CALL
-    PieChartType::createCoordinateSystem( ::sal_Int32 DimensionCount )
+rtl::Reference< ::chart::BaseCoordinateSystem >
+    PieChartType::createCoordinateSystem2( sal_Int32 DimensionCount )
 {
     rtl::Reference< PolarCoordinateSystem > xResult =
         new PolarCoordinateSystem( DimensionCount );

chart2/source/model/template/PieChartType.hxx
@@ -43,8 +43,6 @@ private:
 
     // ____ XChartType ____
     virtual OUString SAL_CALL getChartType() override;
-    virtual css::uno::Reference< css::chart2::XCoordinateSystem > SAL_CALL
-        createCoordinateSystem( ::sal_Int32 DimensionCount ) override;
     virtual css::uno::Sequence< OUString > SAL_CALL
         getSupportedPropertyRoles() override;
 
@@ -60,6 +58,9 @@ private:
 
     // ____ XCloneable ____
     virtual css::uno::Reference< css::util::XCloneable > SAL_CALL createClone() override;
+
+    virtual rtl::Reference< ::chart::BaseCoordinateSystem >
+        createCoordinateSystem2( sal_Int32 DimensionCount ) override;
 };
 
 } //  namespace chart

chart2/source/model/template/ScatterChartType.cxx
@@ -128,8 +128,8 @@ rtl::Reference< ChartType > ScatterChartType::cloneChartType() const
 }
 
 // ____ XChartType ____
-Reference< chart2::XCoordinateSystem > SAL_CALL
-    ScatterChartType::createCoordinateSystem( ::sal_Int32 DimensionCount )
+rtl::Reference< ::chart::BaseCoordinateSystem >
+    ScatterChartType::createCoordinateSystem2( sal_Int32 DimensionCount )
 {
     rtl::Reference< CartesianCoordinateSystem > xResult =
         new CartesianCoordinateSystem( DimensionCount );

chart2/source/model/template/ScatterChartType.hxx
@@ -45,8 +45,6 @@ private:
     virtual OUString SAL_CALL getChartType() override;
     virtual css::uno::Sequence< OUString > SAL_CALL
         getSupportedMandatoryRoles() override;
-    virtual css::uno::Reference< css::chart2::XCoordinateSystem > SAL_CALL
-        createCoordinateSystem( ::sal_Int32 DimensionCount ) override;
 
     // ____ OPropertySet ____
     virtual void GetDefaultValue( sal_Int32 nHandle, css::uno::Any& rAny ) const override;
@@ -60,6 +58,9 @@ private:
 
     // ____ XCloneable ____
     virtual css::uno::Reference< css::util::XCloneable > SAL_CALL createClone() override;
+
+    virtual rtl::Reference< ::chart::BaseCoordinateSystem >
+        createCoordinateSystem2( sal_Int32 DimensionCount ) override;
 };
 
 } //  namespace chart