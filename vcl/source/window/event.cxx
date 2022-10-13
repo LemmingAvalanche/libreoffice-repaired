LemmingAvalanche/libreoffice-repaired

commit 7072bfd484620d9df66c79ff902bf31eef6ac1cb

IsDisposed->isDisposed in vcl/../window
Change-Id: Ic1c8bb6bb9a45d4e91c997b752e23aed35767f42
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116364
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/window/event.cxx
@@ -105,7 +105,7 @@ bool Window::EventNotify( NotifyEvent& rNEvt )
 {
     bool bRet = false;
 
-    if (IsDisposed())
+    if (isDisposed())
         return false;
 
     // check for docking window
@@ -223,7 +223,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
 
     Application::ImplCallEventListeners( aEvent );
 
-    if ( xWindow->IsDisposed() )
+    if ( xWindow->isDisposed() )
         return;
 
     // If maEventListeners is empty, the XVCLWindow has not yet been initialized.
@@ -241,7 +241,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
         comphelper::ScopeGuard aGuard(
             [&rWindowImpl, &xWindow]()
             {
-                if (!xWindow->IsDisposed())
+                if (!xWindow->isDisposed())
                 {
                     rWindowImpl.mnEventListenersIteratingCount--;
                     if (rWindowImpl.mnEventListenersIteratingCount == 0)
@@ -251,7 +251,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
         );
         for ( const Link<VclWindowEvent&,void>& rLink : aCopy )
         {
-            if (xWindow->IsDisposed()) break;
+            if (xWindow->isDisposed()) break;
             // check this hasn't been removed in some re-enterancy scenario fdo#47368
             if( rWindowImpl.maEventListenersDeleted.find(rLink) == rWindowImpl.maEventListenersDeleted.end() )
                 rLink.Call( aEvent );
@@ -261,7 +261,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
     while ( xWindow )
     {
 
-        if ( xWindow->IsDisposed() )
+        if ( xWindow->isDisposed() )
             return;
 
         auto& rWindowImpl = *xWindow->mpWindowImpl;
@@ -274,7 +274,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
             comphelper::ScopeGuard aGuard(
                 [&rWindowImpl, &xWindow]()
                 {
-                    if (!xWindow->IsDisposed())
+                    if (!xWindow->isDisposed())
                     {
                         rWindowImpl.mnChildEventListenersIteratingCount--;
                         if (rWindowImpl.mnChildEventListenersIteratingCount == 0)
@@ -284,7 +284,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
             );
             for ( const Link<VclWindowEvent&,void>& rLink : aCopy )
             {
-                if (xWindow->IsDisposed())
+                if (xWindow->isDisposed())
                     return;
                 // Check this hasn't been removed in some re-enterancy scenario fdo#47368.
                 if( rWindowImpl.maChildEventListenersDeleted.find(rLink) == rWindowImpl.maChildEventListenersDeleted.end() )
@@ -292,7 +292,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
             }
         }
 
-        if ( xWindow->IsDisposed() )
+        if ( xWindow->isDisposed() )
             return;
 
         xWindow = xWindow->GetParent();
@@ -468,7 +468,7 @@ void Window::ImplNotifyKeyMouseCommandEventListeners( NotifyEvent& rNEvt )
             CallEventListeners( VclEventId::WindowKeyUp, const_cast<KeyEvent *>(rNEvt.GetKeyEvent()) );
     }
 
-    if ( xWindow->IsDisposed() )
+    if ( xWindow->isDisposed() )
         return;
 
     // #106721# check if we're part of a compound control and notify

vcl/source/window/cursor.cxx
@@ -115,7 +115,7 @@ static tools::Rectangle ImplCursorInvert(vcl::RenderContext* pRenderContext, Imp
 
 static void ImplCursorInvert(vcl::Window* pWindow, ImplCursorData const * pData)
 {
-    if (!pWindow || pWindow->IsDisposed())
+    if (!pWindow || pWindow->isDisposed())
         return;
 
     vcl::PaintBufferGuardPtr pGuard;

vcl/source/window/dialog.cxx
@@ -843,7 +843,7 @@ bool Dialog::Close()
 {
     VclPtr<vcl::Window> xWindow = this;
     CallEventListeners( VclEventId::WindowClose );
-    if ( xWindow->IsDisposed() )
+    if ( xWindow->isDisposed() )
         return false;
 
     if ( mpWindowImpl->mxWindowPeer.is() && IsCreatedWithToolkit() && !IsInExecute() )
@@ -863,7 +863,7 @@ bool Dialog::Close()
     if (pCustomCancelButton)
     {
         pCustomCancelButton->Click();
-        if (xWindow->IsDisposed())
+        if (xWindow->isDisposed())
             return true;
         mbInClose = false;
         return false;
@@ -883,7 +883,7 @@ bool Dialog::Close()
             else
                 bRet = false;
         }
-        if ( xWindow->IsDisposed() )
+        if ( xWindow->isDisposed() )
             return true;
         return bRet;
     }
@@ -1048,14 +1048,14 @@ short Dialog::Execute()
 
     // Yield util EndDialog is called or dialog gets destroyed
     // (the latter should not happen, but better safe than sorry
-    while ( !xWindow->IsDisposed() && mbInExecute && !Application::IsQuit() )
+    while ( !xWindow->isDisposed() && mbInExecute && !Application::IsQuit() )
         Application::Yield();
 
     ImplEndExecuteModal();
 #ifdef DBG_UTIL
-    assert (!mpDialogParent || !mpDialogParent->IsDisposed());
+    assert (!mpDialogParent || !mpDialogParent->isDisposed());
 #endif
-    if ( !xWindow->IsDisposed() )
+    if ( !xWindow->isDisposed() )
         xWindow.clear();
     else
     {
@@ -1097,7 +1097,7 @@ void Dialog::RemoveFromDlgList()
 
 void Dialog::EndDialog( tools::Long nResult )
 {
-    if (!mbInExecute || IsDisposed())
+    if (!mbInExecute || isDisposed())
         return;
 
     const bool bModal = GetType() != WindowType::MODELESSDIALOG;
@@ -1641,7 +1641,7 @@ void TopLevelWindowLocker::decBusy()
     // unlock locked toplevel windows from being closed now busy is over
     for (auto& a : m_xImpl->m_aBusyStack.top())
     {
-        if (a->IsDisposed())
+        if (a->isDisposed())
             continue;
         a->DecModalCount();
         a->ImplGetFrame()->NotifyModalHierarchy(false);

vcl/source/window/dockwin.cxx
@@ -622,7 +622,7 @@ bool DockingWindow::Close()
 {
     VclPtr<vcl::Window> xWindow = this;
     CallEventListeners( VclEventId::WindowClose );
-    if ( xWindow->IsDisposed() )
+    if ( xWindow->isDisposed() )
         return false;
 
     if ( mpWindowImpl->mxWindowPeer.is() && IsCreatedWithToolkit() )

vcl/source/window/menu.cxx
@@ -2905,7 +2905,7 @@ sal_uInt16 PopupMenu::ImplExecute( const VclPtr<vcl::Window>& pW, const tools::R
     Activate();
     bInCallback = false;
 
-    if ( pW->IsDisposed() )
+    if ( pW->isDisposed() )
         return 0;   // Error
 
     if ( bCanceled || bKilled )
@@ -3065,7 +3065,7 @@ sal_uInt16 PopupMenu::ImplExecute( const VclPtr<vcl::Window>& pW, const tools::R
     if ( bRealExecute )
     {
         pWin->Execute();
-        if (pWin->IsDisposed())
+        if (pWin->isDisposed())
             return 0;
 
         xFocusId = pWin->GetFocusId();

vcl/source/window/menufloatingwindow.cxx
@@ -432,7 +432,7 @@ void MenuFloatingWindow::End()
     if (!bInExecute)
         return;
 
-    if (GetParent() && !GetParent()->IsDisposed())
+    if (GetParent() && !GetParent()->isDisposed())
         GetParent()->DecModalCount();
 
     // restore focus to previous window if we still have the focus
@@ -1194,7 +1194,7 @@ void MenuFloatingWindow::KeyInput( const KeyEvent& rKEvent )
     }
 
     // #105474# check if menu window was not destroyed
-    if ( !xWindow->IsDisposed() )
+    if ( !xWindow->isDisposed() )
     {
         bKeyInput = false;
     }

vcl/source/window/mouse.cxx
@@ -342,7 +342,7 @@ void Window::ImplGrabFocus( GetFocusFlags nFlags )
     }
 
     // call Get- and LoseFocus
-    if ( pOldFocusWindow && ! pOldFocusWindow->IsDisposed() )
+    if ( pOldFocusWindow && ! pOldFocusWindow->isDisposed() )
     {
         NotifyEvent aNEvt( MouseNotifyEvent::LOSEFOCUS, pOldFocusWindow );
         if ( !ImplCallPreNotify( aNEvt ) )
@@ -369,15 +369,15 @@ void Window::ImplGrabFocus( GetFocusFlags nFlags )
             // notify the new focus window so it can restore the inner focus
             // eg, toolboxes can select their recent active item
             if( pOldFocusWindow &&
-                ! pOldFocusWindow->IsDisposed() &&
+                ! pOldFocusWindow->isDisposed() &&
                 ( pOldFocusWindow->GetDialogControlFlags() & DialogControlFlags::FloatWinPopupModeEndCancel ) )
                 mpWindowImpl->mnGetFocusFlags |= GetFocusFlags::FloatWinPopupModeEndCancel;
             NotifyEvent aNEvt( MouseNotifyEvent::GETFOCUS, this );
-            if ( !ImplCallPreNotify( aNEvt ) && !xWindow->IsDisposed() )
+            if ( !ImplCallPreNotify( aNEvt ) && !xWindow->isDisposed() )
                 CompatGetFocus();
-            if( !xWindow->IsDisposed() )
-                ImplCallActivateListeners( (pOldFocusWindow && ! pOldFocusWindow->IsDisposed()) ? pOldFocusWindow : nullptr );
-            if( !xWindow->IsDisposed() )
+            if( !xWindow->isDisposed() )
+                ImplCallActivateListeners( (pOldFocusWindow && ! pOldFocusWindow->isDisposed()) ? pOldFocusWindow : nullptr );
+            if( !xWindow->isDisposed() )
             {
                 mpWindowImpl->mnGetFocusFlags = GetFocusFlags::NONE;
                 mpWindowImpl->mbInFocusHdl = false;

vcl/source/window/paint.cxx
@@ -1327,7 +1327,7 @@ void Window::PaintImmediately()
         if (comphelper::LibreOfficeKit::isActive() && pUpdateWindow->GetParentDialog())
             pUpdateWindow->LogicInvalidate(nullptr);
 
-        if (xWindow->IsDisposed())
+        if (xWindow->isDisposed())
            return;
 
         bFlush = true;

commit 69789c7d03fa48edc1968f61ce3b9c2215a62832

tdf#151352 crash closing form while TOTD dialog is displayed
regression from
    commit 7072bfd484620d9df66c79ff902bf31eef6ac1cb
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Sat May 29 08:34:28 2021 +0200
    IsDisposed->isDisposed in vcl/../window

Change-Id: I51889f2451f03797f5b89019e6df32a64acda707
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141292
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/window/event.cxx
@@ -224,7 +224,11 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
 
     Application::ImplCallEventListeners( aEvent );
 
-    if ( xWindow->isDisposed() )
+    // if we have ObjectDying, then the bIsDisposed flag has already been set,
+    // but we still need to let listeners know.
+    const bool bIgnoreDisposed = nEvent == VclEventId::ObjectDying;
+
+    if ( !bIgnoreDisposed && xWindow->isDisposed() )
         return;
 
     // If maEventListeners is empty, the XVCLWindow has not yet been initialized.
@@ -240,9 +244,9 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
         mpWindowImpl->mnEventListenersIteratingCount++;
         auto& rWindowImpl = *mpWindowImpl;
         comphelper::ScopeGuard aGuard(
-            [&rWindowImpl, &xWindow]()
+            [&rWindowImpl, &xWindow, &bIgnoreDisposed]()
             {
-                if (!xWindow->isDisposed())
+                if (bIgnoreDisposed || !xWindow->isDisposed())
                 {
                     rWindowImpl.mnEventListenersIteratingCount--;
                     if (rWindowImpl.mnEventListenersIteratingCount == 0)
@@ -252,7 +256,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
         );
         for ( const Link<VclWindowEvent&,void>& rLink : aCopy )
         {
-            if (xWindow->isDisposed()) break;
+            if (!bIgnoreDisposed && xWindow->isDisposed()) break;
             // check this hasn't been removed in some re-enterancy scenario fdo#47368
             if( rWindowImpl.maEventListenersDeleted.find(rLink) == rWindowImpl.maEventListenersDeleted.end() )
                 rLink.Call( aEvent );
@@ -262,7 +266,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
     while ( xWindow )
     {
 
-        if ( xWindow->isDisposed() )
+        if ( !bIgnoreDisposed && xWindow->isDisposed() )
             return;
 
         auto& rWindowImpl = *xWindow->mpWindowImpl;
@@ -273,9 +277,9 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
             // we use an iterating counter/flag and a set of deleted Link's to avoid O(n^2) behaviour
             rWindowImpl.mnChildEventListenersIteratingCount++;
             comphelper::ScopeGuard aGuard(
-                [&rWindowImpl, &xWindow]()
+                [&rWindowImpl, &xWindow, &bIgnoreDisposed]()
                 {
-                    if (!xWindow->isDisposed())
+                    if (bIgnoreDisposed || !xWindow->isDisposed())
                     {
                         rWindowImpl.mnChildEventListenersIteratingCount--;
                         if (rWindowImpl.mnChildEventListenersIteratingCount == 0)
@@ -285,7 +289,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
             );
             for ( const Link<VclWindowEvent&,void>& rLink : aCopy )
             {
-                if (xWindow->isDisposed())
+                if (!bIgnoreDisposed && xWindow->isDisposed())
                     return;
                 // Check this hasn't been removed in some re-enterancy scenario fdo#47368.
                 if( rWindowImpl.maChildEventListenersDeleted.find(rLink) == rWindowImpl.maChildEventListenersDeleted.end() )
@@ -293,7 +297,7 @@ void Window::CallEventListeners( VclEventId nEvent, void* pData )
             }
         }
 
-        if ( xWindow->isDisposed() )
+        if ( !bIgnoreDisposed && xWindow->isDisposed() )
             return;
 
         xWindow = xWindow->GetParent();