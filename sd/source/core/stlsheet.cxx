LemmingAvalanche/libreoffice-repaired

commit 2f02b37c75ab95985091498e3db5175c2effdcdd

tdf#94369 - Default set of Impress graphic styles
Several drawing styles added

Change-Id: I3c0efcdc5c0462844f84f5dbb355cfa800a70898
Reviewed-on: https://gerrit.libreoffice.org/60444
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <tietze.heiko@gmail.com>

sd/source/core/stlsheet.cxx
@@ -597,10 +597,19 @@ struct ApiNameMap
         { OUStringLiteral("notes"), HID_PSEUDOSHEET_NOTES },
         { OUStringLiteral("standard"), HID_STANDARD_STYLESHEET_NAME },
         { OUStringLiteral("objectwithoutfill"), HID_POOLSHEET_OBJWITHOUTFILL },
-        { OUStringLiteral("text"), HID_POOLSHEET_TEXT },
-        { OUStringLiteral("title"), HID_POOLSHEET_TITLE },
-        { OUStringLiteral("headline"), HID_POOLSHEET_HEADLINE },
-        { OUStringLiteral("measure"), HID_POOLSHEET_MEASURE },
+
+        { OUStringLiteral("Text"), HID_POOLSHEET_TEXT },
+        { OUStringLiteral("A4"), HID_POOLSHEET_A4 },
+        { OUStringLiteral("Title A4"), HID_POOLSHEET_A4_TITLE },
+        { OUStringLiteral("Heading A4"), HID_POOLSHEET_A4_HEADLINE },
+        { OUStringLiteral("Text A4"), HID_POOLSHEET_A4_TEXT },
+        { OUStringLiteral("A4"), HID_POOLSHEET_A0 },
+        { OUStringLiteral("Title A0"), HID_POOLSHEET_A0_TITLE },
+        { OUStringLiteral("Heading A0"), HID_POOLSHEET_A0_HEADLINE },
+        { OUStringLiteral("Text A0"), HID_POOLSHEET_A0_TEXT },
+
+        { OUStringLiteral("Graphic"), HID_POOLSHEET_GRAPHIC },
+        { OUStringLiteral("Shapes"), HID_POOLSHEET_SHAPES },
         { OUStringLiteral("Filled"), HID_POOLSHEET_FILLED },
         { OUStringLiteral("Filled Blue"), HID_POOLSHEET_FILLED_BLUE },
         { OUStringLiteral("Filled Green"), HID_POOLSHEET_FILLED_GREEN },
@@ -610,7 +619,11 @@ struct ApiNameMap
         { OUStringLiteral("Outlined Blue"), HID_POOLSHEET_OUTLINE_BLUE },
         { OUStringLiteral("Outlined Green"), HID_POOLSHEET_OUTLINE_GREEN },
         { OUStringLiteral("Outlined Red"), HID_POOLSHEET_OUTLINE_RED },
-        { OUStringLiteral("Outlined Yellow"), HID_POOLSHEET_OUTLINE_YELLOW } };
+        { OUStringLiteral("Outlined Yellow"), HID_POOLSHEET_OUTLINE_YELLOW },
+        { OUStringLiteral("Lines"), HID_POOLSHEET_LINES },
+        { OUStringLiteral("Arrow Line"), HID_POOLSHEET_MEASURE },
+        { OUStringLiteral("Arrow Dashed"), HID_POOLSHEET_LINES_DASHED }
+      };
 
 OUString GetApiNameForHelpId(sal_uLong nId)
 {

sd/inc/app.hrc
@@ -24,12 +24,19 @@
 
 // Helper IDs for templates
 #define HID_STANDARD_STYLESHEET_NAME        (HID_SD_START + 70)
-#define HID_POOLSHEET_TEXT                  (HID_SD_START + 71)
-#define HID_POOLSHEET_TITLE                 (HID_SD_START + 72)
 #define HID_POOLSHEET_OBJWITHOUTFILL        (HID_SD_START + 73)
 #define HID_POOLSHEET_OBJNOLINENOFILL       (HID_SD_START + 74)
-#define HID_POOLSHEET_HEADLINE              (HID_SD_START + 75)
-#define HID_POOLSHEET_MEASURE               (HID_SD_START + 76)
+#define HID_POOLSHEET_TEXT                  (HID_SD_START + 71)
+#define HID_POOLSHEET_A4                    (HID_SD_START + 104)
+#define HID_POOLSHEET_A4_TITLE              (HID_SD_START + 72)
+#define HID_POOLSHEET_A4_HEADLINE           (HID_SD_START + 75)
+#define HID_POOLSHEET_A4_TEXT               (HID_SD_START + 105)
+#define HID_POOLSHEET_A0                    (HID_SD_START + 109)
+#define HID_POOLSHEET_A0_TITLE              (HID_SD_START + 110)
+#define HID_POOLSHEET_A0_HEADLINE           (HID_SD_START + 111)
+#define HID_POOLSHEET_A0_TEXT               (HID_SD_START + 112)
+#define HID_POOLSHEET_GRAPHIC               (HID_SD_START + 106)
+#define HID_POOLSHEET_SHAPES                (HID_SD_START + 107)
 #define HID_POOLSHEET_FILLED                (HID_SD_START + 77)
 #define HID_POOLSHEET_FILLED_BLUE           (HID_SD_START + 78)
 #define HID_POOLSHEET_FILLED_GREEN          (HID_SD_START + 79)
@@ -40,6 +47,10 @@
 #define HID_POOLSHEET_OUTLINE_GREEN         (HID_SD_START + 84)
 #define HID_POOLSHEET_OUTLINE_RED           (HID_SD_START + 102)
 #define HID_POOLSHEET_OUTLINE_YELLOW        (HID_SD_START + 103)
+#define HID_POOLSHEET_LINES                 (HID_SD_START + 108)
+#define HID_POOLSHEET_MEASURE               (HID_SD_START + 76)
+#define HID_POOLSHEET_LINES_DASHED          (HID_SD_START + 113) //dummy
+#define HID_POOLSHEET_LAST                  (HID_SD_START + 114) //dummy
 
 #define HID_PSEUDOSHEET_TITLE               (HID_SD_START + 85)
 #define HID_PSEUDOSHEET_OUTLINE             (HID_SD_START + 86)

sd/inc/strings.hrc
@@ -313,12 +313,22 @@
 #define STR_STANDARD_STYLESHEET_NAME                    NC_("STR_STANDARD_STYLESHEET_NAME", "Default" )
 #define STR_UNDO_MOVEPAGES                              NC_("STR_UNDO_MOVEPAGES", "Move slides" )
 
-#define STR_POOLSHEET_MEASURE                           NC_("STR_POOLSHEET_MEASURE", "Dimension Line" )
-#define STR_POOLSHEET_TEXT                              NC_("STR_POOLSHEET_TEXT", "Text" )
-#define STR_POOLSHEET_TITLE                             NC_("STR_POOLSHEET_TITLE", "Title" )
-#define STR_POOLSHEET_HEADLINE                          NC_("STR_POOLSHEET_HEADLINE", "Heading" )
 #define STR_POOLSHEET_OBJWITHOUTFILL                    NC_("STR_POOLSHEET_OBJWITHOUTFILL", "Object without fill" )
 #define STR_POOLSHEET_OBJNOLINENOFILL                   NC_("STR_POOLSHEET_OBJNOLINENOFILL", "Object with no fill and no line" )
+#define STR_POOLSHEET_TEXT                              NC_("STR_POOLSHEET_TEXT", "Text" )
+#define STR_POOLSHEET_A4                                NC_("STR_POOLSHEET_A4", "A4" )
+#define STR_POOLSHEET_A4_TITLE                          NC_("STR_POOLSHEET_A4_TITLE", "Title A4" )
+#define STR_POOLSHEET_A4_HEADLINE                       NC_("STR_POOLSHEET_A4_HEADLINE", "Heading A4" )
+#define STR_POOLSHEET_A4_TEXT                           NC_("STR_POOLSHEET_A4_TEXT", "Text A4" )
+#define STR_POOLSHEET_A0                                NC_("STR_POOLSHEET_A0", "A0" )
+#define STR_POOLSHEET_A0_TITLE                          NC_("STR_POOLSHEET_A0_TITLE", "Title A0" )
+#define STR_POOLSHEET_A0_HEADLINE                       NC_("STR_POOLSHEET_A0_HEADLINE", "Heading A0" )
+#define STR_POOLSHEET_A0_TEXT                           NC_("STR_POOLSHEET_A0_TEXT", "Text A0" )
+#define STR_POOLSHEET_GRAPHIC                           NC_("STR_POOLSHEET_GRAPHIC", "Graphic" )
+#define STR_POOLSHEET_SHAPES                            NC_("STR_POOLSHEET_GRAPHIC", "Shapes" )
+#define STR_POOLSHEET_LINES                             NC_("STR_POOLSHEET_LINES", "Lines" )
+#define STR_POOLSHEET_MEASURE                           NC_("STR_POOLSHEET_MEASURE", "Arrow Line" )
+#define STR_POOLSHEET_LINES_DASHED                      NC_("STR_POOLSHEET_LINES_DASHED", "Dashed Line" )
 
 #define STR_POOLSHEET_FILLED                            NC_("STR_POOLSHEET_FILLED", "Filled" )
 #define STR_POOLSHEET_FILLED_BLUE                       NC_("STR_POOLSHEET_FILLED_BLUE", "Filled Blue" )

sd/source/core/stlpool.cxx
@@ -955,9 +955,16 @@ void SdStyleSheetPool::UpdateStdNames()
                 case HID_POOLSHEET_OBJWITHOUTFILL:  pNameId = STR_POOLSHEET_OBJWITHOUTFILL; break;
                 case HID_POOLSHEET_OBJNOLINENOFILL: pNameId = STR_POOLSHEET_OBJNOLINENOFILL;break;
                 case HID_POOLSHEET_TEXT:            pNameId = STR_POOLSHEET_TEXT;           break;
-                case HID_POOLSHEET_TITLE:           pNameId = STR_POOLSHEET_TITLE;          break;
-                case HID_POOLSHEET_HEADLINE:        pNameId = STR_POOLSHEET_HEADLINE;       break;
-                case HID_POOLSHEET_MEASURE:         pNameId = STR_POOLSHEET_MEASURE;        break;
+                case HID_POOLSHEET_A4:              pNameId = STR_POOLSHEET_A4;             break;
+                case HID_POOLSHEET_A4_TITLE:        pNameId = STR_POOLSHEET_A4_TITLE;       break;
+                case HID_POOLSHEET_A4_HEADLINE:     pNameId = STR_POOLSHEET_A4_HEADLINE;    break;
+                case HID_POOLSHEET_A4_TEXT:         pNameId = STR_POOLSHEET_A4_TEXT;        break;
+                case HID_POOLSHEET_A0:              pNameId = STR_POOLSHEET_A0;             break;
+                case HID_POOLSHEET_A0_TITLE:        pNameId = STR_POOLSHEET_A0_TITLE;       break;
+                case HID_POOLSHEET_A0_HEADLINE:     pNameId = STR_POOLSHEET_A0_HEADLINE;    break;
+                case HID_POOLSHEET_A0_TEXT:         pNameId = STR_POOLSHEET_A0_TEXT;        break;
+                case HID_POOLSHEET_GRAPHIC:         pNameId = STR_POOLSHEET_GRAPHIC;        break;
+                case HID_POOLSHEET_SHAPES:          pNameId = STR_POOLSHEET_SHAPES;         break;
                 case HID_POOLSHEET_FILLED:          pNameId = STR_POOLSHEET_FILLED;         break;
                 case HID_POOLSHEET_FILLED_BLUE:     pNameId = STR_POOLSHEET_FILLED_BLUE;    break;
                 case HID_POOLSHEET_FILLED_GREEN:    pNameId = STR_POOLSHEET_FILLED_GREEN;   break;
@@ -968,9 +975,10 @@ void SdStyleSheetPool::UpdateStdNames()
                 case HID_POOLSHEET_OUTLINE_GREEN:   pNameId = STR_POOLSHEET_OUTLINE_GREEN;  break;
                 case HID_POOLSHEET_OUTLINE_RED:     pNameId = STR_POOLSHEET_OUTLINE_RED;    break;
                 case HID_POOLSHEET_OUTLINE_YELLOW:  pNameId = STR_POOLSHEET_OUTLINE_YELLOW; break;
+                case HID_POOLSHEET_LINES:           pNameId = STR_POOLSHEET_LINES;          break;
+                case HID_POOLSHEET_MEASURE:         pNameId = STR_POOLSHEET_MEASURE;        break;
+                case HID_POOLSHEET_LINES_DASHED:    pNameId = STR_POOLSHEET_LINES_DASHED;   break;
 
-                case HID_PSEUDOSHEET_TITLE:         pNameId = STR_PSEUDOSHEET_TITLE;        break;
-                case HID_PSEUDOSHEET_SUBTITLE:      pNameId = STR_PSEUDOSHEET_SUBTITLE;     break;
                 case HID_PSEUDOSHEET_OUTLINE1:
                 case HID_PSEUDOSHEET_OUTLINE2:
                 case HID_PSEUDOSHEET_OUTLINE3:

commit 1540b3109077d55b0dcdbe9b2fb00afbee52889a

tdf#138111: Fix apparent copy-paste error in ApiNameMap for sd style "A0"
As introduced in commit 2f02b37c75ab95985091498e3db5175c2effdcdd.

Change-Id: Iece8595a401df7bd5a514244d4a4fc825a49f9de
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142819
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sd/source/core/stlsheet.cxx
@@ -598,7 +598,7 @@ struct ApiNameMap
         { std::u16string_view(u"Title A4"), HID_POOLSHEET_A4_TITLE },
         { std::u16string_view(u"Heading A4"), HID_POOLSHEET_A4_HEADLINE },
         { std::u16string_view(u"Text A4"), HID_POOLSHEET_A4_TEXT },
-        { std::u16string_view(u"A4"), HID_POOLSHEET_A0 },
+        { std::u16string_view(u"A0"), HID_POOLSHEET_A0 },
         { std::u16string_view(u"Title A0"), HID_POOLSHEET_A0_TITLE },
         { std::u16string_view(u"Heading A0"), HID_POOLSHEET_A0_HEADLINE },
         { std::u16string_view(u"Text A0"), HID_POOLSHEET_A0_TEXT },