LemmingAvalanche/libreoffice-repaired

commit 0fa36df521da3623b7f55ed8fce24d0901bc34e4

tdf#135923 DOCX shape import: set text color
to the character color of the shape if it is not comes
from direct formatting.

Change-Id: I3f0a3e952b5449a6d92c5bb273d353a561f3e3dc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130922
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/shape/WpsContext.cxx
@@ -15,6 +15,7 @@
 #include <drawingml/customshapeproperties.hxx>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/beans/XPropertyState.hpp>
+#include <com/sun/star/container/XEnumerationAccess.hpp>
 #include <com/sun/star/drawing/HomogenMatrix3.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/text/XText.hpp>
@@ -155,20 +156,59 @@ oox::core::ContextHandlerRef WpsContext::onCreateContext(sal_Int32 nElementToken
                 uno::Reference<text::XTextCursor> xTextCursor = xText->createTextCursor();
                 xTextCursor->gotoStart(false);
                 xTextCursor->gotoEnd(true);
-                const uno::Reference<beans::XPropertyState> xPropertyState(xTextCursor,
-                                                                           uno::UNO_QUERY);
-                const beans::PropertyState ePropertyState
-                    = xPropertyState->getPropertyState("CharColor");
-                if (ePropertyState == beans::PropertyState_DEFAULT_VALUE)
+                uno::Reference<beans::XPropertySet> xTextBoxPropertySet(xTextCursor,
+                                                                        uno::UNO_QUERY);
+                uno::Any xCharColor = xPropertySet->getPropertyValue("CharColor");
+                Color aColor = COL_AUTO;
+                if ((xCharColor >>= aColor) && aColor != COL_AUTO)
                 {
-                    uno::Reference<beans::XPropertySet> xTextBoxPropertySet(xTextCursor,
-                                                                            uno::UNO_QUERY);
-                    uno::Any xCharColor = xPropertySet->getPropertyValue("CharColor");
-                    Color aColor = COL_AUTO;
-                    if (xCharColor >>= aColor)
+                    const uno::Reference<beans::XPropertyState> xPropertyState(xTextCursor,
+                                                                               uno::UNO_QUERY);
+                    const beans::PropertyState ePropertyState
+                        = xPropertyState->getPropertyState("CharColor");
+                    if (ePropertyState == beans::PropertyState_DEFAULT_VALUE)
                     {
-                        if (aColor != COL_AUTO)
-                            xTextBoxPropertySet->setPropertyValue("CharColor", xCharColor);
+                        xTextBoxPropertySet->setPropertyValue("CharColor", xCharColor);
+                    }
+                    else
+                    {
+                        // tdf#135923 Apply character color of the shape to the textrun
+                        //            when the character color of the textrun is default.
+                        uno::Reference<container::XEnumerationAccess> paraEnumAccess(
+                            xText, uno::UNO_QUERY);
+                        if (paraEnumAccess.is())
+                        {
+                            uno::Reference<container::XEnumeration> paraEnum(
+                                paraEnumAccess->createEnumeration());
+
+                            while (paraEnum->hasMoreElements())
+                            {
+                                uno::Reference<text::XTextRange> xParagraph(paraEnum->nextElement(),
+                                                                            uno::UNO_QUERY);
+                                uno::Reference<container::XEnumerationAccess> runEnumAccess(
+                                    xParagraph, uno::UNO_QUERY);
+                                if (!runEnumAccess.is())
+                                    continue;
+
+                                uno::Reference<container::XEnumeration> runEnum
+                                    = runEnumAccess->createEnumeration();
+
+                                while (runEnum->hasMoreElements())
+                                {
+                                    uno::Reference<text::XTextRange> xRun(runEnum->nextElement(),
+                                                                          uno::UNO_QUERY);
+                                    const uno::Reference<beans::XPropertyState> xRunState(
+                                        xRun, uno::UNO_QUERY);
+                                    if (xRunState->getPropertyState("CharColor")
+                                        == beans::PropertyState_DEFAULT_VALUE)
+                                    {
+                                        uno::Reference<beans::XPropertySet> xRunPropSet(
+                                            xRun, uno::UNO_QUERY);
+                                        xRunPropSet->setPropertyValue("CharColor", xCharColor);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
 

sw/qa/extras/ooxmlexport/ooxmlexport17.cxx
@@ -419,6 +419,17 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf147978enhancedPathABVW)
     }
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf135923, "tdf135923-min.docx")
+{
+    uno::Reference<text::XText> xShape(getShape(1), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xParagraph = getParagraphOfText(1, xShape);
+
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(COL_WHITE),
+                         getProperty<sal_Int32>(getRun(xParagraph, 1), "CharColor"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(COL_BLACK),
+                         getProperty<sal_Int32>(getRun(xParagraph, 2), "CharColor"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit b74c95d15a8847b0322d3712306d2171c3f39420

tdf#152310 DOCX shape import: fix lost text color regression
Don't apply COL_AUTO as color of the run.

Regression from commit 0fa36df521da3623b7f55ed8fce24d0901bc34e4
"tdf#135923 DOCX shape import: set text color".

Change-Id: I9c2088b4c531f09c80715bffdae225ac0987be79
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143533
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/shape/WpsContext.cxx
@@ -220,7 +220,10 @@ oox::core::ContextHandlerRef WpsContext::onCreateContext(sal_Int32 nElementToken
                                     {
                                         uno::Reference<beans::XPropertySet> xRunPropSet(
                                             xRun, uno::UNO_QUERY);
-                                        xRunPropSet->setPropertyValue("CharColor", xCharColor);
+                                        Color aRunColor = COL_AUTO;
+                                        xRunPropSet->getPropertyValue("CharColor") >>= aRunColor;
+                                        if (aRunColor == COL_AUTO)
+                                            xRunPropSet->setPropertyValue("CharColor", xCharColor);
                                     }
                                 }
                             }

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -862,6 +862,16 @@ DECLARE_OOXMLEXPORT_TEST(testTdf104348_contextMargin, "tdf104348_contextMargin.d
     CPPUNIT_ASSERT_EQUAL(nMargin, getProperty<sal_Int32>(getParagraph(2), "ParaBottomMargin"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf152310, "ColorOverwritten.docx")
+{
+    uno::Reference<text::XText> xShape(getShape(1), uno::UNO_QUERY);
+
+    CPPUNIT_ASSERT_EQUAL(Color(0xFF0000),
+                         getProperty<Color>(getParagraphOfText(1, xShape), "CharColor"));
+    CPPUNIT_ASSERT_EQUAL(Color(0x00b050),
+                         getProperty<Color>(getParagraphOfText(2, xShape), "CharColor"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */