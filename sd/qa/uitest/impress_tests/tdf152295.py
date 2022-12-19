LemmingAvalanche/libreoffice-repaired

commit 86e35029fd3bb00077983fd3c3599fbe56fc72af

[GSoC] Move all fill style tabs inside area tab
Change-Id: I89e84b7b3c7075ad4107e7bc444ea5c07bc90795
Reviewed-on: https://gerrit.libreoffice.org/27866
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

cui/source/tabpages/tpbitmap.cxx
@@ -84,15 +84,12 @@ SvxBitmapTabPage::SvxBitmapTabPage( vcl::Window* pParent, const SfxItemSet& rInA
     m_aXBitmapItem( OUString(), Graphic() ),
     m_fObjectWidth(0.0),
     m_fObjectHeight(0.0),
-    m_nPageType(nullptr),
-    m_nDlgType(0),
-    m_nPos(nullptr),
-
-    m_pbAreaTP( nullptr ),
+    m_pPos(nullptr),
 
     m_aXFillAttr          ( rInAttrs.GetPool() ),
     m_rXFSet              ( m_aXFillAttr.GetItemSet() ),
     mpView(nullptr),
+    meDlgUnit(GetModuleFieldUnit( GetItemSet() )),
     nFilledWidthPercent(0),
     nFilledHeightPercent(0),
     nZoomedWidthPercent(0),
@@ -185,18 +182,12 @@ void SvxBitmapTabPage::Construct()
 
 void SvxBitmapTabPage::ActivatePage( const SfxItemSet& )
 {
-    if( m_nDlgType == 0 ) // area dialog
+    if( *m_pPos != LISTBOX_ENTRY_NOTFOUND )
     {
-        *m_pbAreaTP = false;
-
-        if( *m_nPageType == PageType::Bitmap && *m_nPos != LISTBOX_ENTRY_NOTFOUND )
-        {
-            sal_uInt16 nId = m_pBitmapLB->GetItemId( static_cast<size_t>( *m_nPos ) );
-            m_pBitmapLB->SelectItem(nId);
-        }
-        *m_nPageType = PageType::Bitmap;
-        *m_nPos = LISTBOX_ENTRY_NOTFOUND;
+        sal_uInt16 nId = m_pBitmapLB->GetItemId( static_cast<size_t>( *m_pPos ) );
+        m_pBitmapLB->SelectItem(nId);
     }
+    *m_pPos = LISTBOX_ENTRY_NOTFOUND;
 }
 
 
@@ -211,80 +202,73 @@ DeactivateRC SvxBitmapTabPage::DeactivatePage( SfxItemSet* _pSet )
 
 bool SvxBitmapTabPage::FillItemSet( SfxItemSet* rAttrs )
 {
-    if( m_nDlgType == 0 && !*m_pbAreaTP )
+    rAttrs->Put(XFillStyleItem(drawing::FillStyle_BITMAP));
+    size_t nPos = m_pBitmapLB->GetSelectItemPos();
+    if(VALUESET_ITEM_NOTFOUND != nPos)
     {
-        if(PageType::Bitmap == *m_nPageType)
+        const XBitmapEntry* pXBitmapEntry = m_pBitmapList->GetBitmap(nPos);
+        const OUString aString(m_pBitmapLB->GetItemText( m_pBitmapLB->GetSelectItemId() ));
+        rAttrs->Put(XFillBitmapItem(aString, pXBitmapEntry->GetGraphicObject()));
+    }
+
+    BitmapStyle eStylePos = (BitmapStyle)m_pBitmapStyleLB->GetSelectEntryPos();        bool bIsStretched( eStylePos == STRETCHED );
+    bool bIsTiled( eStylePos == TILED );
+
+    rAttrs->Put( XFillBmpTileItem(bIsTiled) );
+    rAttrs->Put( XFillBmpStretchItem(bIsStretched) );
+
+    if(!bIsStretched)
+    {
+        Size aSetBitmapSize;
+        switch(eStylePos)
         {
-            rAttrs->Put(XFillStyleItem(drawing::FillStyle_BITMAP));
-            size_t nPos = m_pBitmapLB->GetSelectItemPos();
-            if(VALUESET_ITEM_NOTFOUND != nPos)
+            case ORIGINAL:
+                aSetBitmapSize = rBitmapSize;
+                break;
+            case FILLED:
+                aSetBitmapSize = rFilledSize;
+                break;
+            case ZOOMED:
+                aSetBitmapSize = rZoomedSize;
+                break;
+            case CUSTOM:
+            case TILED:
             {
-                const XBitmapEntry* pXBitmapEntry = m_pBitmapList->GetBitmap(nPos);
-                const OUString aString(m_pBitmapLB->GetItemText( m_pBitmapLB->GetSelectItemId() ));
-                rAttrs->Put(XFillBitmapItem(aString, pXBitmapEntry->GetGraphicObject()));
-            }
-
-            BitmapStyle eStylePos = (BitmapStyle)m_pBitmapStyleLB->GetSelectEntryPos();
-            bool bIsStretched( eStylePos == STRETCHED );
-            bool bIsTiled( eStylePos == TILED );
-
-            rAttrs->Put( XFillBmpTileItem(bIsTiled) );
-            rAttrs->Put( XFillBmpStretchItem(bIsStretched) );
-
-            if(!bIsStretched)
-            {
-                Size aSetBitmapSize;
-                switch(eStylePos)
+                sal_Int64 nWidthPercent = m_pBitmapWidth->Denormalize(m_pBitmapWidth->GetValue());
+                sal_Int64 nHeightPercent = m_pBitmapHeight->Denormalize(m_pBitmapHeight->GetValue());
+                if(eStylePos == CUSTOM && m_pTsbScale->IsEnabled() && m_pTsbScale->GetState() == TRISTATE_TRUE)
                 {
-                    case ORIGINAL:
-                        aSetBitmapSize = rBitmapSize;
-                        break;
-                    case FILLED:
-                        aSetBitmapSize = rFilledSize;
-                        break;
-                    case ZOOMED:
-                        aSetBitmapSize = rZoomedSize;
-                        break;
-                    case CUSTOM:
-                    case TILED:
-                    {
-                        sal_Int64 nWidthPercent = m_pBitmapWidth->Denormalize(m_pBitmapWidth->GetValue());
-                        sal_Int64 nHeightPercent = m_pBitmapHeight->Denormalize(m_pBitmapHeight->GetValue());
-                        if(eStylePos == CUSTOM && m_pTsbScale->IsEnabled() && m_pTsbScale->GetState() == TRISTATE_TRUE)
-                        {
-                            aSetBitmapSize.Width() = -nWidthPercent;
-                            aSetBitmapSize.Height() = -nHeightPercent;
-                        }
-                        else
-                        {
-                            aSetBitmapSize.Width() = static_cast<long>(nWidthPercent*rBitmapSize.Width()/100);
-                            aSetBitmapSize.Height() = static_cast<long>(nHeightPercent*rBitmapSize.Height()/100);
-                        }
-                        break;
-                    }
-                    default:
-                        break;
+                    aSetBitmapSize.Width() = -nWidthPercent;
+                    aSetBitmapSize.Height() = -nHeightPercent;
                 }
-                rAttrs->Put( XFillBmpSizeXItem( aSetBitmapSize.Width() ) );
-                rAttrs->Put( XFillBmpSizeYItem( aSetBitmapSize.Height() ) );
+                else
+                {
+                    aSetBitmapSize.Width() = static_cast<long>(nWidthPercent*rBitmapSize.Width()/100);
+                    aSetBitmapSize.Height() = static_cast<long>(nHeightPercent*rBitmapSize.Height()/100);
+                }
+                break;
             }
-
-            if(m_pPositionLB->IsEnabled())
-                rAttrs->Put( XFillBmpPosItem( static_cast<RectPoint>( m_pPositionLB->GetSelectEntryPos() ) ) );
-            if(m_pPositionOffX->IsEnabled())
-                rAttrs->Put( XFillBmpPosOffsetXItem( m_pPositionOffX->GetValue() ) );
-            if(m_pPositionOffY->IsEnabled())
-                rAttrs->Put( XFillBmpPosOffsetYItem( m_pPositionOffY->GetValue() ) );
-            if(m_pTileOffBox->IsEnabled())
-            {
-                TileOffset eValue = static_cast<TileOffset>(m_pTileOffLB->GetSelectEntryPos());
-                sal_uInt16 nOffsetValue = static_cast<sal_uInt16>(m_pTileOffset->GetValue());
-                sal_uInt16 nRowOff = (eValue == ROW) ? nOffsetValue : 0;
-                sal_uInt16 nColOff = (eValue == COLUMN) ? nOffsetValue : 0;
-                rAttrs->Put( XFillBmpTileOffsetXItem(nRowOff) );
-                rAttrs->Put( XFillBmpTileOffsetYItem(nColOff) );
-            }
+            default:
+                break;
         }
+        rAttrs->Put( XFillBmpSizeXItem( aSetBitmapSize.Width() ) );
+        rAttrs->Put( XFillBmpSizeYItem( aSetBitmapSize.Height() ) );
+    }
+
+    if(m_pPositionLB->IsEnabled())
+        rAttrs->Put( XFillBmpPosItem( static_cast<RectPoint>( m_pPositionLB->GetSelectEntryPos() ) ) );
+    if(m_pPositionOffX->IsEnabled())
+        rAttrs->Put( XFillBmpPosOffsetXItem( m_pPositionOffX->GetValue() ) );
+    if(m_pPositionOffY->IsEnabled())
+        rAttrs->Put( XFillBmpPosOffsetYItem( m_pPositionOffY->GetValue() ) );
+    if(m_pTileOffBox->IsEnabled())
+    {
+        TileOffset eValue = static_cast<TileOffset>(m_pTileOffLB->GetSelectEntryPos());
+        sal_uInt16 nOffsetValue = static_cast<sal_uInt16>(m_pTileOffset->GetValue());
+        sal_uInt16 nRowOff = (eValue == ROW) ? nOffsetValue : 0;
+        sal_uInt16 nColOff = (eValue == COLUMN) ? nOffsetValue : 0;
+        rAttrs->Put( XFillBmpTileOffsetXItem(nRowOff) );
+        rAttrs->Put( XFillBmpTileOffsetYItem(nColOff) );
     }
     return true;
 }
@@ -505,7 +489,7 @@ IMPL_LINK_NOARG(SvxBitmapTabPage, ModifyBitmapHdl, ValueSet*, void)
 
     if( nPos != VALUESET_ITEM_NOTFOUND )
     {
-        pGraphicObject.reset(new GraphicObject(m_pBitmapList->GetBitmap(nPos)->GetGraphicObject()));
+        pGraphicObject.reset(new GraphicObject(m_pBitmapList->GetBitmap( static_cast<sal_uInt16>(nPos) )->GetGraphicObject()));
     }
     else
     {
@@ -604,7 +588,7 @@ IMPL_LINK_NOARG(SvxBitmapTabPage, ClickDeleteHdl, SvxPresetListBox*, void)
 
         if( aQueryBox->Execute() == RET_YES )
         {
-            m_pBitmapList->Remove(nPos);
+            m_pBitmapList->Remove( static_cast<sal_uInt16>(nPos) );
             m_pBitmapLB->RemoveItem( nId );
             nId = m_pBitmapLB->GetItemId(0);
             m_pBitmapLB->SelectItem( nId );

cui/source/dialogs/sdrcelldlg.cxx
@@ -51,10 +51,6 @@ void SvxFormatCellsDialog::PageCreated( sal_uInt16 nId, SfxTabPage &rPage )
         rAreaPage.SetGradientList( mpGradientList );
         rAreaPage.SetHatchingList( mpHatchingList );
         rAreaPage.SetBitmapList( mpBitmapList );
-        rAreaPage.SetPageType( PageType::Area );
-        rAreaPage.SetDlgType( 1 );
-        rAreaPage.SetPos( 0 );
-        rAreaPage.Construct();
         rAreaPage.ActivatePage( mrOutAttrs );
     }
     else if (nId == m_nBorderPageId)

commit 16f1665449ff110e5870b9a65b14a2fb6dd86c86

tdf#152295 sw sd UI: don't reset tiled image size by Area tab
The background tiling images become larger when
opening and closing Area tab: reopening the image
properties in the Area tab shows that the Width
and Height got reset to the original values of the
image.

Regression from commit 86e35029fd3bb00077983fd3c3599fbe56fc72af
"[GSoC] Move all fill style tabs inside area tab".

Change-Id: Ibc4dfd73dedd81b1b7fe17574198dd4999aab967
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143819
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

cui/source/tabpages/tpbitmap.cxx
@@ -316,15 +316,9 @@ void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
     if(rAttrs->GetItemState(XATTR_FILLBMP_SIZELOG) != SfxItemState::DONTCARE)
     {
         if (rAttrs->Get( XATTR_FILLBMP_SIZELOG ).GetValue())
-        {
             m_xTsbScale->set_state(TRISTATE_FALSE);
-            m_bLogicalSize = true;
-        }
         else
-        {
             m_xTsbScale->set_state(TRISTATE_TRUE);
-            m_bLogicalSize = false;
-        }
     }
     else
         m_xTsbScale->set_state(TRISTATE_INDET);
@@ -337,6 +331,7 @@ void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
             nWidth = rBitmapSize.Width();
         else if(nWidth < 0)
         {
+            m_bLogicalSize = true;
             eRelative = TRISTATE_TRUE;
             nWidth = std::abs(nWidth);
         }
@@ -349,6 +344,7 @@ void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
             nHeight = rBitmapSize.Height();
         else if(nHeight < 0)
         {
+            m_bLogicalSize = true;
             eRelative = TRISTATE_TRUE;
             nHeight = std::abs(nHeight);
         }

sd/qa/uitest/impress_tests/tdf152295.py
@@ -1,0 +1,55 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict, select_pos
+from uitest.uihelper.common import change_measurement_unit
+
+class tdf152295(UITestCase):
+    def test_tdf152295(self):
+        with self.ui_test.create_doc_in_start_center("impress") as document:
+            change_measurement_unit(self, "Centimeter")
+            TemplateDialog = self.xUITest.getTopFocusWindow()
+            close = TemplateDialog.getChild("close")
+            self.ui_test.close_dialog_through_button(close)
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageSetup") as DrawPageDialog:
+                xTabs = DrawPageDialog.getChild("tabcontrol")
+                select_pos(xTabs, "1")
+                btnbitmap = DrawPageDialog.getChild("btnbitmap")
+                btnbitmap.executeAction("CLICK",tuple())
+                width = DrawPageDialog.getChild("width")
+                for _ in range(50):
+                    width.executeAction("UP",tuple())
+                height = DrawPageDialog.getChild("height")
+                for _ in range(50):
+                    height.executeAction("UP",tuple())
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageSetup") as DrawPageDialog:
+                xTabs = DrawPageDialog.getChild("tabcontrol")
+                select_pos(xTabs, "1")
+                btnbitmap = DrawPageDialog.getChild("btnbitmap")
+                btnbitmap.executeAction("CLICK",tuple())
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageSetup") as DrawPageDialog:
+                xTabs = DrawPageDialog.getChild("tabcontrol")
+                select_pos(xTabs, "1")
+                btnbitmap = DrawPageDialog.getChild("btnbitmap")
+                btnbitmap.executeAction("CLICK",tuple())
+                width = DrawPageDialog.getChild("width")
+                height = DrawPageDialog.getChild("height")
+
+                # Without the fix in place, this test would have failed with
+                # AssertionError: '6.00 cm' != '13.55 cm'
+                # AssertionError: '6.00 cm' != '13.55 cm'
+                self.assertEqual("6.00 cm", get_state_as_dict(width)['Text'])
+                self.assertEqual("6.00 cm", get_state_as_dict(height)['Text'])
+
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:

sw/qa/uitest/writer_tests3/pageDialog.py
@@ -83,9 +83,9 @@ class WriterPageDialog(UITestCase):
             self.assertEqual(
                 document.StyleFamilies.PageStyles.Standard.FillBitmapLogicalSize, True)
             self.assertEqual(
-                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeX, 0)
+                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeX, 1000)
             self.assertEqual(
-                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeY, 0)
+                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeY, 1000)
             self.assertEqual(
                 document.StyleFamilies.PageStyles.Standard.FillBitmapName, 'Painted White')
         elif btn == 'btnpattern':
@@ -108,9 +108,9 @@ class WriterPageDialog(UITestCase):
             self.assertEqual(
                 document.StyleFamilies.PageStyles.Standard.FillBitmapLogicalSize, True)
             self.assertEqual(
-                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeX, 0)
+                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeX, 1000)
             self.assertEqual(
-                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeY, 0)
+                document.StyleFamilies.PageStyles.Standard.FillBitmapSizeY, 1000)
             self.assertEqual(
                 document.StyleFamilies.PageStyles.Standard.FillBitmapName, '5 Percent')
 

uitest/impress_tests/backgrounds.py
@@ -75,9 +75,9 @@ class ImpressBackgrounds(UITestCase):
             self.assertEqual(
               document.DrawPages.getByIndex(0).Background.FillBitmapLogicalSize, True)
             self.assertEqual(
-              document.DrawPages.getByIndex(0).Background.FillBitmapSizeX, 0)
+              document.DrawPages.getByIndex(0).Background.FillBitmapSizeX, 2540)
             self.assertEqual(
-              document.DrawPages.getByIndex(0).Background.FillBitmapSizeY, 0)
+              document.DrawPages.getByIndex(0).Background.FillBitmapSizeY, 2540)
             self.assertEqual(document.DrawPages.getByIndex(0).Background.FillBitmapName, 'Painted White')
         elif btn == 'btnpattern':
             self.assertEqual(