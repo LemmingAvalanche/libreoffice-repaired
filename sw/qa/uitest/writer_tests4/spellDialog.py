LemmingAvalanche/libreoffice-repaired

commit fc8f21c8e5d21d6c8e6c8834133b50517fdf778e

0xFFF9 is a better choice for CH_TXTATR_INWORD than 0x0002.
a) the default properties for the code point make it not split a word it
appears in into two different words in any break mode we have. Which is what we
want from a CH_TXTATR_INWORD

b) unicode TR#20 gives for the interlinear annotation anchor: "What to do if
detected: In a proxy context or browser context, remove U+FFF9", so when we
need to strip it from text to run that text through e.g. the spellchecker or
word counting then there's a solid precedent for stripping it

In addition I *do* want the footnote placeholder to break the word it appears
in, that gives the desired wordcount and cursor travelling behaviour

The BREAKWORD and other *random* selection of CH_TXTATR are still odd choices,
and there's way too many of them.

Change-Id: I930ff8ff806af448829bc1a1ae6cb92053e9a284

sw/source/core/edit/edlingu.cxx
@@ -1049,11 +1049,10 @@ uno::Reference< XSpellAlternatives >
                 xub_StrLen nLineEnd = GetCrsr()->GetPoint()->nContent.GetIndex();
                 Pop(sal_False);
 
-                // make sure the selection build later from the
-                // data below does not include footnotes and other
-                // "in word" character to the left and right in order
-                // to preserve those. Therefore count those "in words"
-                // in order to modify the selection accordingly.
+                // make sure the selection build later from the data below does
+                // not "in word" character to the left and right in order to
+                // preserve those. Therefore count those "in words" in order to
+                // modify the selection accordingly.
                 const sal_Unicode* pChar = aText.GetBuffer();
                 xub_StrLen nLeft = 0;
                 while (pChar && *pChar++ == CH_TXTATR_INWORD)
@@ -1179,11 +1178,10 @@ bool SwEditShell::GetGrammarCorrection(
                 xub_StrLen nLineEnd = GetCrsr()->GetPoint()->nContent.GetIndex();
                 Pop(sal_False);
 
-                // make sure the selection build later from the
-                // data below does not include footnotes and other
-                // "in word" character to the left and right in order
-                // to preserve those. Therefore count those "in words"
-                // in order to modify the selection accordingly.
+                // make sure the selection build later from the data below does
+                // not include "in word" character to the left and right in
+                // order to preserve those. Therefore count those "in words" in
+                // order to modify the selection accordingly.
                 const sal_Unicode* pChar = aText.GetBuffer();
                 xub_StrLen nLeft = 0;
                 while (pChar && *pChar++ == CH_TXTATR_INWORD)

i18npool/qa/cppunit/test_breakiterator.cxx
@@ -252,7 +252,7 @@ void TestBreakIterator::testWordBoundaries()
         }
     }
 
-    sal_Unicode aJoinTests[] = { 'X', 0x200C, 0x200D, 0x2060, 0xFEFF, 0xFFF9 };
+    sal_Unicode aJoinTests[] = { 'X', 0x200C, 0x200D, 0x2060, 0xFEFF, 0xFFF9, 0xFFFA, 0xFFFB };
     for (int mode = i18n::WordType::ANY_WORD; mode <= i18n::WordType::WORD_COUNT; ++mode)
     {
         //make sure that in all cases isBeginWord and isEndWord matches getWordBoundary

sw/inc/hintids.hxx
@@ -35,7 +35,7 @@
 // For SwTxtHints without end index the following char is added:
 
 #define CH_TXTATR_BREAKWORD     ((sal_Unicode)0x01)
-#define CH_TXTATR_INWORD        ((sal_Unicode)0x02)
+#define CH_TXTATR_INWORD        ((sal_Unicode)0xFFF9)
 #define CH_TXTATR_TAB           ((sal_Unicode)'\t')
 #define CH_TXTATR_NEWLINE       ((sal_Unicode)'\n')
 #define CH_TXT_ATR_FIELDSTART ((sal_Unicode)0x04)

sw/qa/core/swdoc-test.cxx
@@ -60,6 +60,7 @@
 #include "swtypes.hxx"
 #include "fmtftn.hxx"
 #include "fmtrfmrk.hxx"
+#include "fmtfld.hxx"
 
 SO2_DECL_REF(SwDocShell)
 SO2_IMPL_REF(SwDocShell)
@@ -366,6 +367,25 @@ void SwDocTest::testSwScanner()
         pTxtNode->CountWords(aDocStat, 0, pTxtNode->Len());
         CPPUNIT_ASSERT(aDocStat.nWord == 1);
         CPPUNIT_ASSERT_MESSAGE("refmark anchor should not be counted", aDocStat.nChar == 11);
+
+        m_pDoc->AppendTxtNode(*aPaM.GetPoint());
+        m_pDoc->InsertString(aPaM, rtl::OUString("Apple"));
+
+        DateTime aDate(DateTime::SYSTEM);
+        SwPostItField aPostIt(
+            (SwPostItFieldType*)m_pDoc->GetSysFldType(RES_POSTITFLD), rtl::OUString("An Author"),
+            rtl::OUString("Some Text"), rtl::OUString("WhatEver"), aDate );
+        m_pDoc->InsertPoolItem(aPaM, SwFmtFld(aPostIt), 0);
+
+        m_pDoc->InsertString(aPaM, rtl::OUString("Apple"));
+        pTxtNode = aPaM.GetNode()->GetTxtNode();
+        aDocStat.Reset();
+        pTxtNode->CountWords(aDocStat, 0, pTxtNode->Len());
+        CPPUNIT_ASSERT(aDocStat.nWord == 1);
+        CPPUNIT_ASSERT_MESSAGE("postit anchor should effectively not exist", aDocStat.nChar == 10);
+        CPPUNIT_ASSERT(pTxtNode->Len() == 11);
+
+        aDocStat.Reset();
     }
 
     //See https://bugs.freedesktop.org/show_bug.cgi?id=46757

sw/source/core/crsr/crsrsh.cxx
@@ -3369,9 +3369,9 @@ void SwCrsrShell::GetSmartTagTerm( const Point& rPt, SwRect& rSelectRect,
             Pop(sal_False);
 
             // make sure the selection build later from the data below does not
-            // include footnotes and other "in word" character to the left and
-            // right in order to preserve those. Therefore count those "in
-            // words" in order to modify the selection accordingly.
+            // include "in word" character to the left and right in order to
+            // preserve those. Therefore count those "in words" in order to
+            // modify the selection accordingly.
             const sal_Unicode* pChar = aText.GetBuffer();
             xub_StrLen nLeft = 0;
             while (pChar && *pChar++ == CH_TXTATR_INWORD)

sw/source/core/txtnode/ndtxt.cxx
@@ -1377,7 +1377,7 @@ void lcl_CopyHint( const sal_uInt16 nWhich, const SwTxtAttr * const pHt,
         case RES_TXTATR_META:
         case RES_TXTATR_METAFIELD:
             OSL_ENSURE(pNewHt, "copying Meta should not fail!");
-            OSL_ENSURE(pDest && (CH_TXTATR_INWORD ==
+            OSL_ENSURE(pDest && (CH_TXTATR_BREAKWORD ==
                                 pDest->GetTxt().GetChar(*pNewHt->GetStart())),
                    "missing CH_TXTATR?");
             break;

sw/source/core/txtnode/thints.cxx
@@ -3013,16 +3013,16 @@ sal_Unicode GetCharOfTxtAttr( const SwTxtAttr& rAttr )
     sal_Unicode cRet = CH_TXTATR_BREAKWORD;
     switch ( rAttr.Which() )
     {
-        case RES_TXTATR_FTN:
         case RES_TXTATR_REFMARK:
         case RES_TXTATR_TOXMARK:
-        case RES_TXTATR_META:
-        case RES_TXTATR_METAFIELD:
             cRet = CH_TXTATR_INWORD;
         break;
 
         case RES_TXTATR_FIELD:
         case RES_TXTATR_FLYCNT:
+        case RES_TXTATR_FTN:
+        case RES_TXTATR_META:
+        case RES_TXTATR_METAFIELD:
         {
             cRet = CH_TXTATR_BREAKWORD;
 

sw/source/core/txtnode/txtedt.cxx
@@ -942,11 +942,11 @@ sal_uInt16 SwTxtNode::Spell(SwSpellArgs* pArgs)
                     }
                     else
                     {
-                        // make sure the selection build later from the
-                        // data below does not include footnotes and other
-                        // "in word" character to the left and right in order
-                        // to preserve those. Therefore count those "in words"
-                        // in order to modify the selection accordingly.
+                        // make sure the selection build later from the data
+                        // below does not include "in word" character to the
+                        // left and right in order to preserve those. Therefore
+                        // count those "in words" in order to modify the
+                        // selection accordingly.
                         const sal_Unicode* pChar = rWord.GetBuffer();
                         xub_StrLen nLeft = 0;
                         while (pChar && *pChar++ == CH_TXTATR_INWORD)

commit 9cab023573e6df2dd98d91402b6e37fd531ecbb2

tdf#65535 sw: fix lost comments using spelling dialog
Choosing one of the spelling suggestions in the spelling
dialog, or rewriting the bad word removed also its
inword comment. Keep the comment at the end of the fixed word.

Regression from commit fc8f21c8e5d21d6c8e6c8834133b50517fdf778e
"0xFFF9 is a better choice for CH_TXTATR_INWORD than 0x0002".

Change-Id: Iaf083453fad4e0ca99e254f53b20ead86b3ba15e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144458
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/edit/edlingu.cxx
@@ -1164,6 +1164,22 @@ void SwEditShell::ApplyChangedSentence(const svx::SpellPortions& rNewPortions, b
                 // ... and apply language if necessary
                 if(aCurrentNewPortion->eLanguage != aCurrentOldPortion->eLanguage)
                     SetAttrItem( SvxLanguageItem(aCurrentNewPortion->eLanguage, nLangWhichId) );
+
+                // if there is a comment inside the original word, don't delete it:
+                // but keep it at the end of the replacement
+                // TODO: keep all the comments with a recursive function
+                sal_Int32 nCommentPos(pCursor->GetText().indexOf(OUStringChar(CH_TXTATR_INWORD)));
+                if ( nCommentPos > -1 )
+                {
+                    // delete the original word after the comment
+                    pCursor->GetPoint()->AdjustContent(nCommentPos + 1);
+
+                    mxDoc->getIDocumentContentOperations().ReplaceRange(*pCursor, OUString(), false);
+                    // and select only the remaining part before the comment
+                    pCursor->GetPoint()->AdjustContent(-(nCommentPos + 1));
+                    pCursor->GetMark()->AdjustContent(-1);
+                }
+
                 mxDoc->getIDocumentContentOperations().ReplaceRange(*pCursor, aCurrentNewPortion->sText, false);
             }
             else if(aCurrentNewPortion->eLanguage != aCurrentOldPortion->eLanguage)

sw/qa/uitest/data/tdf65535.fodt
@@ -1,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:meta><meta:creation-date>2022-12-17T23:29:19.781993530</meta:creation-date><dc:date>2022-12-17T23:30:52.855431377</dc:date><meta:editing-duration>PT1M34S</meta:editing-duration><meta:editing-cycles>1</meta:editing-cycles><meta:document-statistic meta:table-count="0" meta:image-count="0" meta:object-count="0" meta:page-count="1" meta:paragraph-count="1" meta:word-count="1" meta:character-count="4" meta:non-whitespace-character-count="4"/><meta:generator>LibreOfficeDev/7.5.0.0.alpha0$Linux_X86_64 LibreOffice_project/be538308dd5dee33436ae0e247c315df63a8b958</meta:generator></office:meta>
+ <office:body>
+  <office:text>
+   <text:p text:style-name="P1">Ba<office:annotation loext:resolved="false">
+     <dc:creator>x</dc:creator>
+     <dc:date>2022-12-17T23:29:53.289798307</dc:date>
+     <text:p text:style-name="P2"><text:span text:style-name="T1">with comment</text:span></text:p>
+    </office:annotation>ad</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/uitest/writer_tests4/spellDialog.py
@@ -205,4 +205,45 @@ frog, dogg, catt"""
             # This was "Baaed HTTP://www. baaad .org baaed" (skipped non-URL words of hypertext)
             self.assertEqual("Baaed http://www. baaed .org baaad", output_text)
 
+    def test_tdf65535(self):
+        supported_locale = self.is_supported_locale("en", "US")
+        if not supported_locale:
+            self.skipTest("no dictionary support for en_US available")
+
+        with self.ui_test.load_file(get_url_for_data_file("tdf65535.fodt")) as document:
+            cursor = document.getCurrentController().getViewCursor()
+            # Inserted text must be en_US, so make sure to set language in current location
+            cursor.CharLocale = Locale("en", "US", "")
+
+            xMainWindow = self.xUITest.getTopFocusWindow()
+            xEdit = xMainWindow.getChild("writer_edit")
+
+            # type a bad word after the word with comment
+            cursor.goRight(5, False)
+            type_text(xEdit, " baad")
+            cursor.goLeft(10, False)
+
+            # fix the first word using the spelling dialog
+            with self.ui_test.execute_modeless_dialog_through_command(".uno:SpellingAndGrammarDialog", close_button="close") as xDialog:
+                checkgrammar = xDialog.getChild('checkgrammar')
+                if get_state_as_dict(checkgrammar)['Selected'] == 'true':
+                    checkgrammar.executeAction('CLICK', ())
+                self.assertTrue(get_state_as_dict(checkgrammar)['Selected'] == 'false')
+
+                change = xDialog.getChild('change')
+                change.executeAction("CLICK", ())
+
+            output_text = document.Text.getString()
+            self.assertEqual("Bad baad", output_text)
+
+            # check the original comment
+            has_comment = False
+            textfields = document.getTextFields()
+            for textfield in textfields:
+                if textfield.supportsService("com.sun.star.text.TextField.Annotation"):
+                    has_comment = True
+
+            # This was False (lost comment)
+            self.assertEqual(True, has_comment)
+
 # vim: set shiftwidth=4 softtabstop=4 expandtab: