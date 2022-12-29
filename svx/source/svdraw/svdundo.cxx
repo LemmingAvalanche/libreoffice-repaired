LemmingAvalanche/libreoffice-repaired

commit 226c296fe41bf5b03be45cefdff007adbc68f4f1

tdf#54857 elide more dynamic_cast
add a helper, following the same style as the ones in sw/

Change-Id: I13c673bba5ff47df6beef9d90c200e5801030cf0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142119
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdundo.cxx
@@ -257,7 +257,7 @@ SdrUndoAttrObj::SdrUndoAttrObj(SdrObject& rNewObj, bool bStyleSheet1, bool bSave
 
     SdrObjList* pOL = rNewObj.GetSubList();
     bool bIsGroup(pOL!=nullptr && pOL->GetObjCount());
-    bool bIs3DScene(bIsGroup && dynamic_cast< E3dScene* >(mxObj.get()) !=  nullptr);
+    bool bIs3DScene(bIsGroup && DynCastE3dScene(mxObj.get()));
 
     if(bIsGroup)
     {
@@ -300,7 +300,7 @@ SdrUndoAttrObj::~SdrUndoAttrObj()
 void SdrUndoAttrObj::Undo()
 {
     E3DModifySceneSnapRectUpdater aUpdater(mxObj.get());
-    bool bIs3DScene(dynamic_cast< E3dScene* >(mxObj.get()) !=  nullptr);
+    bool bIs3DScene(DynCastE3dScene(mxObj.get()));
 
     // Trigger PageChangeCall
     ImpShowPageOfThisObject();
@@ -409,7 +409,7 @@ void SdrUndoAttrObj::Undo()
 void SdrUndoAttrObj::Redo()
 {
     E3DModifySceneSnapRectUpdater aUpdater(mxObj.get());
-    bool bIs3DScene(dynamic_cast< E3dScene* >(mxObj.get()) !=  nullptr);
+    bool bIs3DScene(DynCastE3dScene(mxObj.get()));
 
     if(!pUndoGroup || bIs3DScene)
     {
@@ -560,7 +560,7 @@ SdrUndoGeoObj::SdrUndoGeoObj(SdrObject& rNewObj)
      , mbSkipChangeLayout(false)
 {
     SdrObjList* pOL=rNewObj.GetSubList();
-    if (pOL!=nullptr && pOL->GetObjCount() && dynamic_cast<const E3dScene* >( &rNewObj) ==  nullptr)
+    if (pOL!=nullptr && pOL->GetObjCount() && DynCastE3dScene(&rNewObj))
     {
         // this is a group object!
         // If this were 3D scene, we'd only add an Undo for the scene itself

chart2/source/view/charttypes/BarChart.cxx
@@ -449,8 +449,7 @@ void BarChart::createShapes()
 
         for (rtl::Reference<SvxShape> const & rShape : aShapeSet)
         {
-            E3dScene* pScene = dynamic_cast<E3dScene*>(rShape->GetSdrObject());
-            if(nullptr != pScene)
+            if(E3dScene* pScene = DynCastE3dScene(rShape->GetSdrObject()))
             {
                 aSceneSet.insert(pScene->getRootE3dSceneFromE3dObject());
             }
@@ -690,10 +689,10 @@ void BarChart::doXSlot(
         aShapeSet.insert(xSeriesGroupShape_Shapes);
         aShapeSet.insert(xSeriesBackgroundShape_Shapes);
         // Suspend setting rects dirty for the duration of this call
-        E3dScene* pScene = dynamic_cast<E3dScene*>(xSeriesGroupShape_Shapes->GetSdrObject());
+        E3dScene* pScene = DynCastE3dScene(xSeriesGroupShape_Shapes->GetSdrObject());
         if (pScene)
             pScene->SuspendReportingDirtyRects();
-        pScene = dynamic_cast<E3dScene*>(xSeriesBackgroundShape_Shapes->GetSdrObject());
+        pScene = DynCastE3dScene(xSeriesBackgroundShape_Shapes->GetSdrObject());
         if (pScene)
             pScene->SuspendReportingDirtyRects();
 

chart2/source/view/diagram/VDiagram.cxx
@@ -183,7 +183,7 @@ void VDiagram::createShapes_2d()
 
 static E3dScene* lcl_getE3dScene( const rtl::Reference<SvxShapeGroupAnyD>& xShape )
 {
-    return dynamic_cast< E3dScene* >(xShape->GetSdrObject());
+    return DynCastE3dScene(xShape->GetSdrObject());
 }
 
 static void lcl_setLightSources(

include/svx/svdobj.hxx
@@ -41,6 +41,7 @@
 
 class SfxBroadcaster;
 class AutoTimer;
+class E3dScene;
 class OutlinerParaObject;
 class Outliner;
 class SdrOutliner;
@@ -995,6 +996,10 @@ private:
 
     SdrObject( const SdrObject& ) = delete;
 };
+
+SVXCORE_DLLPUBLIC E3dScene* DynCastE3dScene(SdrObject*);
+inline const E3dScene* DynCastE3dScene(const SdrObject* p) { return DynCastE3dScene(const_cast<SdrObject*>(p)); }
+
 
 struct SdrObjCreatorParams
 {

sd/source/ui/func/fudraw.cxx
@@ -556,7 +556,7 @@ void FuDraw::ForcePointer(const MouseEvent* pMEvt)
 
                 if (bDefPointer
                     && (dynamic_cast<const SdrObjGroup*>(pObj) != nullptr
-                        || dynamic_cast<const E3dScene*>(pObj) != nullptr))
+                        || DynCastE3dScene(pObj)))
                 {
                     // take a glance into the group
                     pObj = mpView->PickObj(aPnt, mpView->getHitTolLog(), pPV,
@@ -698,7 +698,7 @@ bool FuDraw::RequestHelp(const HelpEvent& rHEvt)
 
             bReturn = SetHelpText(pObj, aPosPixel, aVEvt);
 
-            if (!bReturn && (dynamic_cast< const SdrObjGroup *>( pObj ) != nullptr || dynamic_cast< const E3dScene* >(pObj) != nullptr))
+            if (!bReturn && (dynamic_cast< const SdrObjGroup *>( pObj ) != nullptr || DynCastE3dScene(pObj)))
             {
                 // take a glance into the group
                 SdrPageView* pPV = nullptr;

sd/source/ui/func/fusel.cxx
@@ -327,7 +327,7 @@ bool FuSelection::MouseButtonDown(const MouseEvent& rMEvt)
 
                     if (!bReturn
                         && (dynamic_cast<const SdrObjGroup*>(pObj) != nullptr
-                            || dynamic_cast<const E3dScene*>(pObj) != nullptr))
+                            || DynCastE3dScene(pObj)))
                     {
                         if (rMEvt.GetClicks() == 1)
                         {

sd/source/ui/view/drviewsj.cxx
@@ -136,7 +136,7 @@ void DrawViewShell::GetMenuStateSel( SfxItemSet &rSet )
             const auto* pSdrObjGroup = dynamic_cast<const SdrObjGroup*>(pObj);
 
             if( !( ( pSdrObjGroup != nullptr && nInv == SdrInventor::Default ) ||
-                   ( dynamic_cast< const E3dScene* >(pObj) != nullptr ) ) )
+                   DynCastE3dScene(pObj) ) )
             {
                 rSet.DisableItem( SID_ENTER_GROUP );
             }
@@ -382,7 +382,7 @@ void DrawViewShell::GetMenuStateSel( SfxItemSet &rSet )
                 }
                 else if (nInv == SdrInventor::E3d)
                 {
-                    if(dynamic_cast< const E3dScene *>( pObj ) !=  nullptr)
+                    if(DynCastE3dScene(pObj))
                         b3dObj = true;
                     else if(dynamic_cast< const E3dCompoundObject* >(pObj) !=  nullptr)
                         bE3dCompoundObject = true;

svx/source/engine3d/obj3d.cxx
@@ -264,10 +264,7 @@ void E3dObject::StructureChanged()
 
 E3dScene* E3dObject::getParentE3dSceneFromE3dObject() const
 {
-    SdrObject* pParent = getParentSdrObjectFromSdrObject();
-    if( pParent && pParent->GetObjIdentifier() == SdrObjKind::E3D_Scene )
-        return static_cast<E3dScene* >(pParent);
-    return nullptr;
+    return DynCastE3dScene(getParentSdrObjectFromSdrObject());
 }
 
 // Determine the top-level scene object

commit 6cf09c6e5e91734407f0d8be41d53700c58b466e

tdf#152683 LO Draw ignoring Undo:Resize Grouped Object
regression from
  commit 226c296fe41bf5b03be45cefdff007adbc68f4f1
  tdf#54857 elide more dynamic_cast

Change-Id: I4fbabe64649ee47a9360dd939b5f39d35bd29c51
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144858
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdundo.cxx
@@ -560,7 +560,7 @@ SdrUndoGeoObj::SdrUndoGeoObj(SdrObject& rNewObj)
      , mbSkipChangeLayout(false)
 {
     SdrObjList* pOL=rNewObj.GetSubList();
-    if (pOL!=nullptr && pOL->GetObjCount() && DynCastE3dScene(&rNewObj))
+    if (pOL!=nullptr && pOL->GetObjCount() && !DynCastE3dScene(&rNewObj))
     {
         // this is a group object!
         // If this were 3D scene, we'd only add an Undo for the scene itself