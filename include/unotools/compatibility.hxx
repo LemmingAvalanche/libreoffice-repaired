LemmingAvalanche/libreoffice-repaired

commit 61c8be64469a1c7da59414b7bddfc44366f81ab6

return SvtCompatibilityEntry from SvtCompatibilityOptions::GetList
instead of converting back and forth

Change-Id: Iec913494ccad65ebff76bf7b6cf5b2440fbade42
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119514
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/unotools/compatibility.hxx
@@ -25,6 +25,7 @@
 #include <unotools/unotoolsdllapi.h>
 #include <rtl/ustring.hxx>
 #include <memory>
+#include <vector>
 
 namespace com::sun::star::beans { struct PropertyValue; }
 namespace osl { class Mutex; }
@@ -211,7 +212,7 @@ class UNOTOOLS_DLLPUBLIC SvtCompatibilityOptions final : public utl::detail::Opt
 
             @onerror    We return an empty list.
         *//*-*****************************************************************************************************/
-        css::uno::Sequence< css::uno::Sequence< css::beans::PropertyValue > > GetList() const;
+        std::vector< SvtCompatibilityEntry > GetList() const;
 
     private:
         std::shared_ptr<SvtCompatibilityOptions_Impl> m_pImpl;

sw/source/ui/config/optcomp.cxx
@@ -206,27 +206,16 @@ void SwCompatibilityOptPage::InitControls( const SfxItemSet& rSet )
     m_xMain->set_label(rText.replaceAll("%DOCNAME", sDocTitle));
 
     // loading file formats
-    const Sequence< Sequence< PropertyValue > > aList = m_aConfigItem.GetList();
+    const std::vector< SvtCompatibilityEntry > aList = m_aConfigItem.GetList();
 
-    SvtCompatibilityEntry aEntry;
-
-    for ( const Sequence< PropertyValue >& rEntry : aList )
+    for ( const SvtCompatibilityEntry& rEntry : aList )
     {
-        for ( const PropertyValue& aValue : rEntry )
-        {
-            aEntry.setValue( SvtCompatibilityEntry::getIndex(aValue.Name), aValue.Value );
-        }
-
-        const OUString sEntryName = aEntry.getValue<OUString>( SvtCompatibilityEntry::Index::Name );
-
+        const OUString sEntryName = rEntry.getValue<OUString>( SvtCompatibilityEntry::Index::Name );
         const bool bIsUserEntry    = ( sEntryName == SvtCompatibilityEntry::USER_ENTRY_NAME );
-        const bool bIsDefaultEntry = ( sEntryName == SvtCompatibilityEntry::DEFAULT_ENTRY_NAME );
 
-        aEntry.setDefaultEntry( bIsDefaultEntry );
+        m_pImpl->m_aList.push_back( rEntry );
 
-        m_pImpl->m_aList.push_back( aEntry );
-
-        if ( aEntry.isDefaultEntry() )
+        if ( rEntry.isDefaultEntry() )
             continue;
 
         OUString sNewEntry;
@@ -245,22 +234,22 @@ void SwCompatibilityOptPage::InitControls( const SfxItemSet& rSet )
             sNewEntry = sEntryName;
 
         sal_uInt32 nOptions = convertBools2Ulong_Impl(
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::UsePrtMetrics ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::AddSpacing ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::AddSpacingAtPages ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseOurTabStops ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::NoExtLeading ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseLineSpacing ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::AddTableSpacing ),
-            aEntry.getValue<bool>(SvtCompatibilityEntry::Index::AddTableLineSpacing),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseObjectPositioning ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseOurTextWrapping ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::ConsiderWrappingStyle ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::ExpandWordSpace ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::ProtectForm ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::MsWordTrailingBlanks ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::SubtractFlysAnchoredAtFlys ),
-            aEntry.getValue<bool>( SvtCompatibilityEntry::Index::EmptyDbFieldHidesPara ) );
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::UsePrtMetrics ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::AddSpacing ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::AddSpacingAtPages ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseOurTabStops ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::NoExtLeading ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseLineSpacing ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::AddTableSpacing ),
+            rEntry.getValue<bool>(SvtCompatibilityEntry::Index::AddTableLineSpacing),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseObjectPositioning ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::UseOurTextWrapping ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::ConsiderWrappingStyle ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::ExpandWordSpace ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::ProtectForm ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::MsWordTrailingBlanks ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::SubtractFlysAnchoredAtFlys ),
+            rEntry.getValue<bool>( SvtCompatibilityEntry::Index::EmptyDbFieldHidesPara ) );
         m_xFormattingLB->append(OUString::number(nOptions), sNewEntry);
     }
 }

unotools/source/config/compatibility.cxx
@@ -125,7 +125,7 @@ class SvtCompatibilityOptions_Impl : public ConfigItem
         void SetDefault( SvtCompatibilityEntry::Index rIdx, bool rValue );
         bool GetDefault( SvtCompatibilityEntry::Index rIdx ) const;
 
-        Sequence< Sequence< PropertyValue > > GetList() const;
+        const std::vector< SvtCompatibilityEntry > & GetOptions() const { return m_aOptions; }
 
         /*-****************************************************************************************************
             @short      called for notify of configmanager
@@ -238,25 +238,6 @@ bool SvtCompatibilityOptions_Impl::GetDefault( SvtCompatibilityEntry::Index rIdx
     return m_aDefOptions.getValue<bool>( rIdx );
 }
 
-Sequence< Sequence< PropertyValue > > SvtCompatibilityOptions_Impl::GetList() const
-{
-    Sequence< PropertyValue >             lProperties( SvtCompatibilityEntry::getElementCount() );
-    Sequence< Sequence< PropertyValue > > lResult( m_aOptions.size() );
-
-    for ( int i = static_cast<int>(SvtCompatibilityEntry::Index::Name); i < static_cast<int>(SvtCompatibilityEntry::Index::INVALID); ++i )
-        lProperties[i].Name = SvtCompatibilityEntry::getName( SvtCompatibilityEntry::Index(i) );
-
-    sal_Int32 j = 0;
-    for ( const auto& rItem : m_aOptions )
-    {
-        for ( int i = static_cast<int>(SvtCompatibilityEntry::Index::Name); i < static_cast<int>(SvtCompatibilityEntry::Index::INVALID); ++i )
-            lProperties[i].Value = rItem.getValue( SvtCompatibilityEntry::Index(i) );
-        lResult[ j++ ] = lProperties;
-    }
-
-    return lResult;
-}
-
 void SvtCompatibilityOptions_Impl::Notify( const Sequence< OUString >& )
 {
     SAL_WARN( "unotools.config", "SvtCompatibilityOptions_Impl::Notify() Not implemented yet! I don't know how I can handle a dynamical list of unknown properties ..." );
@@ -359,10 +340,11 @@ bool SvtCompatibilityOptions::GetDefault( SvtCompatibilityEntry::Index rIdx ) co
     return m_pImpl->GetDefault( rIdx );
 }
 
-Sequence< Sequence< PropertyValue > > SvtCompatibilityOptions::GetList() const
+std::vector< SvtCompatibilityEntry > SvtCompatibilityOptions::GetList() const
 {
     MutexGuard aGuard( GetOwnStaticMutex() );
-    return m_pImpl->GetList();
+
+    return m_pImpl->GetOptions();
 }
 
 namespace

commit bb61c75de41cb89a9cd4f68d991ddebe8926064f

tdf#152676 Writer: Compatibility options no longer saved as default
regression from
    commit 61c8be64469a1c7da59414b7bddfc44366f81ab6
    Date:   Mon Jul 26 10:54:36 2021 +0200
    return SvtCompatibilityEntry from SvtCompatibilityOptions::GetList

where I mistakenly assumed that the bIsDefault flag in
SvtCompatibilityEntry was being set by the SvtCompatibilityOptions
code. Rather remove this field if it is not going to be maintained
properly.

Change-Id: I23963ae885563d5abc8a99cfd144d7fe00c6f192
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144882
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/unotools/compatibility.hxx
@@ -129,19 +129,8 @@ class SvtCompatibilityEntry
             setValue(rIdx, css::uno::Any(rValue));
         }
 
-        bool isDefaultEntry() const
-        {
-            return m_bDefaultEntry;
-        }
-
-        void setDefaultEntry( bool rValue )
-        {
-            m_bDefaultEntry = rValue;
-        }
-
     private:
         std::vector<css::uno::Any> m_aPropertyValue;
-        bool                       m_bDefaultEntry;
 };
 
 /*-************************************************************************************************************

sw/source/ui/config/optcomp.cxx
@@ -205,10 +205,11 @@ void SwCompatibilityOptPage::InitControls( const SfxItemSet& rSet )
     {
         const OUString sEntryName = rEntry.getValue<OUString>( SvtCompatibilityEntry::Index::Name );
         const bool bIsUserEntry    = ( sEntryName == SvtCompatibilityEntry::USER_ENTRY_NAME );
+        const bool bIsDefaultEntry = ( sEntryName == SvtCompatibilityEntry::DEFAULT_ENTRY_NAME );
 
         m_pImpl->m_aList.push_back( rEntry );
 
-        if ( rEntry.isDefaultEntry() )
+        if ( bIsDefaultEntry )
             continue;
 
         OUString sNewEntry;
@@ -260,7 +261,12 @@ IMPL_LINK_NOARG(SwCompatibilityOptPage, UseAsDefaultHdl, weld::Button&, void)
         return;
 
     auto pItem = std::find_if(m_pImpl->m_aList.begin(), m_pImpl->m_aList.end(),
-        [](const SvtCompatibilityEntry& rItem) { return rItem.isDefaultEntry(); });
+        [](const SvtCompatibilityEntry& rItem)
+        {
+            const OUString sEntryName = rItem.getValue<OUString>( SvtCompatibilityEntry::Index::Name );
+            const bool bIsDefaultEntry = ( sEntryName == SvtCompatibilityEntry::DEFAULT_ENTRY_NAME );
+            return bIsDefaultEntry;
+        });
     if (pItem != m_pImpl->m_aList.end())
     {
         const sal_Int32 nCount = m_xOptionsLB->n_children();

unotools/source/config/compatibility.cxx
@@ -65,8 +65,6 @@ SvtCompatibilityEntry::SvtCompatibilityEntry()
     setValue<bool>( Index::SubtractFlysAnchoredAtFlys, false );
     setValue<bool>( Index::EmptyDbFieldHidesPara, true );
     setValue<bool>( Index::AddTableLineSpacing, false );
-
-    setDefaultEntry( false );
 }
 
 OUString SvtCompatibilityEntry::getName( const Index rIdx )