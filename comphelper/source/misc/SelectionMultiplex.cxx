LemmingAvalanche/libreoffice-repaired

commit 984383903baed2d5f88ec9c3e3d808c7bc7b979b

tdf#150575: REPORTBUILDER: Crash when closing report when used report navigator
0  0x00007fe062bdda64 in comphelper::OSelectionChangeMultiplexer::disposing(com::sun::star::lang::EventObject const&) (this=0x55fe72b7b680, _rSource=...) at comphelper/source/misc/SelectionMultiplex.cxx:82
1  0x00007fdf90a97a6b in comphelper::OInterfaceContainerHelper3<com::sun::star::view::XSelectionChangeListener>::disposeAndClear(com::sun::star::lang::EventObject const&) (this=0x55fe6c94b6d0, rEvt=...)
    at include/comphelper/interfacecontainer3.hxx:362
2  0x00007fdf90a6ace4 in rptui::OReportController::disposing() (this=0x55fe6c94b200) at reportdesign/source/ui/report/ReportController.cxx:341
3  0x00007fe062539b5d in cppu::WeakComponentImplHelperBase::dispose() (this=0x55fe6c94b200) at cppuhelper/source/implbase.cxx:104
4  0x00007fe0393b76e5 in cppu::PartialWeakComponentImplHelper<com::sun::star::frame::XDispatch, com::sun::star::frame::XDispatchProviderInterceptor, com::sun::star::util::XModifyListener, com::sun::star::frame::XFrameActionListener, com::sun::star::lang::XInitialization, com::sun::star::lang::XServiceInfo, com::sun::star::frame::XDispatchInformationProvider, com::sun::star::frame::XController2, com::sun::star::frame::XTitle, com::sun::star::frame::XTitleChangeBroadcaster, com::sun::star::awt::XUserInputInterception>::dispose() (this=0x55fe6c94b200) at include/cppuhelper/compbase.hxx:90
5  0x00007fe0393af9d3 in dbaui::OGenericUnoController::dispose() (this=0x55fe6c94b200) at dbaccess/source/ui/browser/genericcontroller.cxx:1207

See full bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=181994

"addSelectionChangeListener" is called when "OSelectionChangeMultiplexer" is instantiated in "NavigatorTree" ctr
225     m_pSelectionListener = new OSelectionChangeMultiplexer(this,&m_rController);

but "removeSelectionChangeListener" was never called so add the mechanism to call it.

Change-Id: I157b147f7a17244dbbfa7d12743fc7d48e7d5f7d

comphelper/source/misc/SelectionMultiplex.cxx
@@ -95,7 +95,16 @@ void SAL_CALL OSelectionChangeMultiplexer::selectionChanged( const  EventObject&
         m_pListener->_selectionChanged(_rEvent);
 }
 
+void OSelectionChangeMultiplexer::dispose()
+{
+    osl_atomic_increment(&m_refCount);
+    {
+        Reference< XSelectionChangeListener> xPreventDelete(this);
+        m_xSet->removeSelectionChangeListener(xPreventDelete);
+    }
+    osl_atomic_decrement(&m_refCount);
 }
 
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/comphelper/SelectionMultiplex.hxx
@@ -80,6 +80,8 @@ namespace comphelper
         void        unlock();
         /// get the lock count
         sal_Int32   locked() const { return m_nLockCount; }
+
+        void dispose();
     };
 
 

reportdesign/source/ui/dlg/Navigator.cxx
@@ -239,6 +239,7 @@ NavigatorTree::~NavigatorTree()
         delete pData;
         return false;
     });
+    m_pSelectionListener->dispose();
     m_pReportListener->dispose();
 }
 

commit 795006adb43cd6e86bda26bf5176a742715d5685

comphelper: check for nullptr
See https://crashreport.libreoffice.org/stats/signature/comphelper::OSelectionChangeMultiplexer::dispose()

Regression from 984383903baed2d5f88ec9c3e3d808c7bc7b979b
"tdf#150575: REPORTBUILDER: Crash when closing report when used report
navigator"

Change-Id: I6e4ada368254879d983f80026e9b42ddb4186c51
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145461
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

comphelper/source/misc/SelectionMultiplex.cxx
@@ -100,7 +100,8 @@ void OSelectionChangeMultiplexer::dispose()
     osl_atomic_increment(&m_refCount);
     {
         Reference< XSelectionChangeListener> xPreventDelete(this);
-        m_xSet->removeSelectionChangeListener(xPreventDelete);
+        if(m_xSet.is())
+            m_xSet->removeSelectionChangeListener(xPreventDelete);
     }
     osl_atomic_decrement(&m_refCount);
 }