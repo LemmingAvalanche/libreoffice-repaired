LibreOffice/core

commit c5db3b93ee1058bd20ebcde2e757b52b9a67b74a

xmlfix3: unoxml: prevent invalid child-parent relationships:  new method CNode::IsChildTypeAllowed(NodeType).  use it in appendChild(), insertBefore(), replaceChild().

unoxml/source/dom/attr.cxx
@@ -70,6 +70,17 @@ namespace DOM
         return pNs;
     }
 
+    bool CAttr::IsChildTypeAllowed(NodeType const nodeType)
+    {
+        switch (nodeType) {
+            case NodeType_TEXT_NODE:
+            case NodeType_ENTITY_REFERENCE_NODE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
     OUString SAL_CALL CAttr::getNodeName()
         throw (RuntimeException)
     {

unoxml/source/dom/attr.hxx
@@ -68,6 +68,8 @@ namespace DOM
         /// return the libxml namespace corresponding to m_pNamespace on pNode
         xmlNsPtr GetNamespace(xmlNodePtr const pNode);
 
+        virtual bool IsChildTypeAllowed(NodeType const nodeType);
+
         /**
         Returns the name of this attribute.
         */

unoxml/source/dom/document.cxx
@@ -56,6 +56,20 @@
 
 namespace DOM
 {
+    static xmlNodePtr lcl_getDocumentType(xmlDocPtr const i_pDocument)
+    {
+        // find the doc type
+        xmlNodePtr cur = i_pDocument->children;
+        while (cur != NULL)
+        {
+            if ((cur->type == XML_DOCUMENT_TYPE_NODE) ||
+                (cur->type == XML_DTD_NODE)) {
+                    return cur;
+            }
+        }
+        return 0;
+    }
+
     /// get the pointer to the root element node of the document
     static xmlNodePtr lcl_getDocumentRootPtr(xmlDocPtr const i_pDocument)
     {
@@ -291,6 +305,24 @@ namespace DOM
         }
         rContext.mxDocHandler->endDocument();
     }
+
+    bool CDocument::IsChildTypeAllowed(NodeType const nodeType)
+    {
+        switch (nodeType) {
+            case NodeType_PROCESSING_INSTRUCTION_NODE:
+            case NodeType_COMMENT_NODE:
+                return true;
+            case NodeType_ELEMENT_NODE:
+                 // there may be only one!
+                return 0 == lcl_getDocumentRootPtr(m_aDocPtr);
+            case NodeType_DOCUMENT_TYPE_NODE:
+                 // there may be only one!
+                return 0 == lcl_getDocumentType(m_aDocPtr);
+            default:
+                return false;
+        }
+    }
+
 
     void SAL_CALL CDocument::addListener(const Reference< XStreamListener >& aListener )
         throw (RuntimeException)
@@ -601,15 +633,9 @@ namespace DOM
     {
         ::osl::MutexGuard const g(m_Mutex);
 
-        // find the doc type
-        xmlNodePtr cur = m_aDocPtr->children;
-        while (cur != NULL)
-        {
-            if (cur->type == XML_DOCUMENT_TYPE_NODE || cur->type == XML_DTD_NODE)
-                break;
-        }
+        xmlNodePtr const pDocType(lcl_getDocumentType(m_aDocPtr));
         Reference< XDocumentType > const xRet(
-            static_cast< XNode* >(GetCNode(cur).get()),
+            static_cast< XNode* >(GetCNode(pDocType).get()),
             UNO_QUERY);
         return xRet;
     }

unoxml/source/dom/document.hxx
@@ -131,6 +131,8 @@ namespace DOM
 
         virtual void fastSaxify( Context& rContext );
 
+        virtual bool IsChildTypeAllowed(NodeType const nodeType);
+
         /**
         Creates an Attr of the given name.
         */

unoxml/source/dom/documentfragment.cxx
@@ -37,6 +37,21 @@ namespace DOM
     {
     }
 
+    bool CDocumentFragment::IsChildTypeAllowed(NodeType const nodeType)
+    {
+        switch (nodeType) {
+            case NodeType_ELEMENT_NODE:
+            case NodeType_PROCESSING_INSTRUCTION_NODE:
+            case NodeType_COMMENT_NODE:
+            case NodeType_TEXT_NODE:
+            case NodeType_CDATA_SECTION_NODE:
+            case NodeType_ENTITY_REFERENCE_NODE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
     OUString SAL_CALL CDocumentFragment::getNodeName()throw (RuntimeException)
     {
         return OUString::createFromAscii("#document-fragment");

unoxml/source/dom/documentfragment.hxx
@@ -55,6 +55,8 @@ namespace DOM
                 xmlNodePtr const pNode);
 
     public:
+        virtual bool IsChildTypeAllowed(NodeType const nodeType);
+
         // ---- resolve uno inheritance problems...
         // overrides for XNode base
         virtual OUString SAL_CALL getNodeName()

unoxml/source/dom/element.cxx
@@ -206,6 +206,28 @@ namespace DOM
         popContext(i_rContext);
     }
 
+    bool CElement::IsChildTypeAllowed(NodeType const nodeType)
+    {
+        switch (nodeType) {
+            case NodeType_ELEMENT_NODE:
+            case NodeType_TEXT_NODE:
+            case NodeType_COMMENT_NODE:
+            case NodeType_PROCESSING_INSTRUCTION_NODE:
+            case NodeType_CDATA_SECTION_NODE:
+            case NodeType_ENTITY_REFERENCE_NODE:
+                return true;
+            case NodeType_ATTRIBUTE_NODE:
+                /* this is not relly allowed by the DOM spec, but this
+                   implementation has evidently supported it (by special case
+                   handling, so the attribute does not actually become a child)
+                   so allow it for backward compatiblity */
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
     /**
         Retrieves an attribute value by name.
         return empty string if attribute is not set

unoxml/source/dom/element.hxx
@@ -66,6 +66,8 @@ namespace DOM
 
         virtual void fastSaxify( Context& i_rContext );
 
+        virtual bool IsChildTypeAllowed(NodeType const nodeType);
+
         /**
         Retrieves an attribute value by name.
         */

LemmingAvalanche/libreoffice-repaired

commit 2a457f1b2641682481cb9a0b4bd7d99a46991151

tdf#153039 unoxml: replace root element of CDocument throws
XForms replace doesn't work because:

CDocument::IsChildTypeAllowed() tests that the document node does not
already have an element child, because only one is allowed - but when
called from CNode::replaceChild(), the existing child will be removed,
so that needs to be allowed to proceed (check that removed child is
also element).

(regression from commit c5db3b93ee1058bd20ebcde2e757b52b9a67b74a)

Change-Id: I167de3462f4d1934dbf8404ad395349897cfd981
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145757
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unoxml/inc/node.hxx
@@ -134,7 +134,8 @@ namespace DOM
         virtual void fastSaxify( Context& io_rContext );
 
         // constrains child relationship between nodes based on type
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType);
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType nodeType,
+                css::xml::dom::NodeType const* pReplacedNodeType);
 
         // ---- DOM interfaces
 

unoxml/source/dom/attr.cxx
@@ -70,7 +70,7 @@ namespace DOM
         return pNs;
     }
 
-    bool CAttr::IsChildTypeAllowed(NodeType const nodeType)
+    bool CAttr::IsChildTypeAllowed(NodeType const nodeType, NodeType const*const)
     {
         switch (nodeType) {
             case NodeType_TEXT_NODE:

unoxml/source/dom/attr.hxx
@@ -52,7 +52,8 @@ namespace DOM
         /// return the libxml namespace corresponding to m_pNamespace on pNode
         xmlNsPtr GetNamespace(xmlNodePtr const pNode);
 
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType) override;
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType,
+                        css::xml::dom::NodeType const*) override;
 
         /**
         Returns the name of this attribute.

unoxml/source/dom/document.cxx
@@ -291,7 +291,7 @@ namespace DOM
         rContext.mxDocHandler->endDocument();
     }
 
-    bool CDocument::IsChildTypeAllowed(NodeType const nodeType)
+    bool CDocument::IsChildTypeAllowed(NodeType const nodeType, NodeType const*const pReplacedNodeType)
     {
         switch (nodeType) {
             case NodeType_PROCESSING_INSTRUCTION_NODE:
@@ -299,10 +299,12 @@ namespace DOM
                 return true;
             case NodeType_ELEMENT_NODE:
                  // there may be only one!
-                return nullptr == lcl_getDocumentRootPtr(m_aDocPtr);
+                return (pReplacedNodeType && *pReplacedNodeType == nodeType)
+                    || nullptr == lcl_getDocumentRootPtr(m_aDocPtr);
             case NodeType_DOCUMENT_TYPE_NODE:
                  // there may be only one!
-                return nullptr == lcl_getDocumentType(m_aDocPtr);
+                return (pReplacedNodeType && *pReplacedNodeType == nodeType)
+                    || nullptr == lcl_getDocumentType(m_aDocPtr);
             default:
                 return false;
         }

unoxml/source/dom/document.hxx
@@ -114,7 +114,8 @@ namespace DOM
 
         virtual void fastSaxify( Context& rContext ) override;
 
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType) override;
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType,
+                css::xml::dom::NodeType const* pReplacedNodeType) override;
 
         /**
         Creates an Attr of the given name.

unoxml/source/dom/documentfragment.cxx
@@ -32,7 +32,7 @@ namespace DOM
     {
     }
 
-    bool CDocumentFragment::IsChildTypeAllowed(NodeType const nodeType)
+    bool CDocumentFragment::IsChildTypeAllowed(NodeType const nodeType, NodeType const*const)
     {
         switch (nodeType) {
             case NodeType_ELEMENT_NODE:

unoxml/source/dom/documentfragment.hxx
@@ -40,7 +40,8 @@ namespace DOM
                 xmlNodePtr const pNode);
 
     public:
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType) override;
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType,
+                        css::xml::dom::NodeType const*) override;
 
         // ---- resolve uno inheritance problems...
         // overrides for XNode base

unoxml/source/dom/element.cxx
@@ -202,7 +202,7 @@ namespace DOM
         popContext(i_rContext);
     }
 
-    bool CElement::IsChildTypeAllowed(NodeType const nodeType)
+    bool CElement::IsChildTypeAllowed(NodeType const nodeType, NodeType const*const)
     {
         switch (nodeType) {
             case NodeType_ELEMENT_NODE:

unoxml/source/dom/element.hxx
@@ -53,7 +53,8 @@ namespace DOM
 
         virtual void fastSaxify( Context& i_rContext ) override;
 
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType) override;
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType,
+                        css::xml::dom::NodeType const*) override;
 
         /**
         Retrieves an attribute value by name.

unoxml/source/dom/entity.cxx
@@ -37,7 +37,7 @@ namespace DOM
     {
     }
 
-    bool CEntity::IsChildTypeAllowed(NodeType const nodeType)
+    bool CEntity::IsChildTypeAllowed(NodeType const nodeType, NodeType const*const)
     {
         switch (nodeType) {
             case NodeType_ELEMENT_NODE:

unoxml/source/dom/entity.hxx
@@ -45,7 +45,8 @@ namespace DOM
                 xmlEntityPtr const pEntity);
 
     public:
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType) override;
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType,
+                        css::xml::dom::NodeType const*) override;
 
         /**
         For unparsed entities, the name of the notation for the entity.

unoxml/source/dom/entityreference.cxx
@@ -34,7 +34,7 @@ namespace DOM
     {
     }
 
-    bool CEntityReference::IsChildTypeAllowed(NodeType const nodeType)
+    bool CEntityReference::IsChildTypeAllowed(NodeType const nodeType, NodeType const*const)
     {
         switch (nodeType) {
             case NodeType_ELEMENT_NODE:

unoxml/source/dom/entityreference.hxx
@@ -43,7 +43,8 @@ namespace DOM
             xmlNodePtr const pNode);
 
     public:
-        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType) override;
+        virtual bool IsChildTypeAllowed(css::xml::dom::NodeType const nodeType,
+                        css::xml::dom::NodeType const*) override;
 
         // ---- resolve uno inheritance problems...
         // overrides for XNode base

unoxml/source/dom/node.cxx
@@ -256,7 +256,7 @@ namespace DOM
         // default: do nothing
     }
 
-    bool CNode::IsChildTypeAllowed(NodeType const /*nodeType*/)
+    bool CNode::IsChildTypeAllowed(NodeType const /*nodeType*/, NodeType const*const)
     {
         // default: no children allowed
         return false;
@@ -314,7 +314,7 @@ namespace DOM
         }
         checkNoParent(cur);
 
-        if (!IsChildTypeAllowed(pNewChild->m_aNodeType)) {
+        if (!IsChildTypeAllowed(pNewChild->m_aNodeType, nullptr)) {
             DOMException e;
             e.Code = DOMExceptionType_HIERARCHY_REQUEST_ERR;
             throw e;
@@ -648,7 +648,7 @@ namespace DOM
         // already has parent
         checkNoParent(pNewChild);
 
-        if (!IsChildTypeAllowed(pNewNode->m_aNodeType)) {
+        if (!IsChildTypeAllowed(pNewNode->m_aNodeType, nullptr)) {
             DOMException e;
             e.Code = DOMExceptionType_HIERARCHY_REQUEST_ERR;
             throw e;
@@ -817,7 +817,7 @@ namespace DOM
         // already has parent
         checkNoParent(pNew);
 
-        if (!IsChildTypeAllowed(pNewNode->m_aNodeType)) {
+        if (!IsChildTypeAllowed(pNewNode->m_aNodeType, &pOldNode->m_aNodeType)) {
             DOMException e;
             e.Code = DOMExceptionType_HIERARCHY_REQUEST_ERR;
             throw e;