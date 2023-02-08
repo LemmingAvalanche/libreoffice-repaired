LemmingAvalanche/libreoffice-repaired

commit 7f861f90f61cc62b45bd3f392e59fa7af747e60a

related tdf#150197: use SetListFormat instead of SetPrefix/Suffix
Commits prior to this handled all the instances that were caught
by a make check. I found a few more by temporarily setting an additional
boolean parameter on SetPrefix/SetSuffix which let me catch these.
This means we don't have unit tests related to these.

Change-Id: I1b3bb1e6eec7878e43ccf546f6df7cb38ac5299e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138072
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/core/edit/autofmt.cxx
@@ -1560,8 +1560,7 @@ void SwAutoFormat::BuildEnum( sal_uInt16 nLvl, sal_uInt16 nDigitLevel )
                         aFormat.SetBulletChar( cBullChar );
                         aFormat.SetNumberingType(SVX_NUM_CHAR_SPECIAL);
                         // #i93908# clear suffix for bullet lists
-                        aFormat.SetPrefix(OUString());
-                        aFormat.SetSuffix(OUString());
+                        aFormat.SetListFormat("", "", n);
                         aFormat.SetFirstLineOffset( lBulletFirstLineOffset );
                         aFormat.SetAbsLSpace( nAbsPos );
                         if( !aFormat.GetCharFormat() )
@@ -1618,9 +1617,8 @@ void SwAutoFormat::BuildEnum( sal_uInt16 nLvl, sal_uInt16 nDigitLevel )
                 if( !nDigitLevel )
                 {
                     SwNumFormat aFormat( aRule.Get( nLvl ) );
-                    aFormat.SetPrefix( aPrefix.getToken( 0, u'\x0001', nPrefixIdx ));
                     aFormat.SetStart( o3tl::narrowing<sal_uInt16>(o3tl::toInt32(o3tl::getToken(aPrefix, 0, u'\x0001', nPrefixIdx ))));
-                    aFormat.SetSuffix( aPostfix.getToken( 0, u'\x0001' ));
+                    aFormat.SetListFormat(aPrefix.getToken(0, u'\x0001', nPrefixIdx), aPostfix.getToken(0, u'\x0001'), nLvl);
                     aFormat.SetIncludeUpperLevels( 0 );
 
                     if( !aFormat.GetCharFormat() )
@@ -1642,10 +1640,9 @@ void SwAutoFormat::BuildEnum( sal_uInt16 nLvl, sal_uInt16 nDigitLevel )
                     {
                         SwNumFormat aFormat( aRule.Get( n ) );
 
-                        if( !n )
-                            aFormat.SetPrefix( aPrefix.getToken( 0, u'\x0001', nPrefixIdx )); // token 0, read only on first loop
                         aFormat.SetStart( o3tl::narrowing<sal_uInt16>(o3tl::toInt32(o3tl::getToken(aPrefix, 0, u'\x0001', nPrefixIdx )) ));
-                        aFormat.SetSuffix( aPostfix.getToken( 0, u'\x0001', nPostfixIdx ));
+                        const OUString sPrefix = n ? "" : aPrefix.getToken(0, u'\x0001', nPrefixIdx);
+                        aFormat.SetListFormat(sPrefix, aPostfix.getToken(0, u'\x0001', nPostfixIdx), n);
                         aFormat.SetIncludeUpperLevels( MAXLEVEL );
                         if( n < aNumTypes.getLength() )
                             aFormat.SetNumberingType(static_cast<SvxNumType>(aNumTypes[ n ] - '0'));

filter/source/msfilter/svdfppt.cxx
@@ -3494,83 +3494,79 @@ bool PPTNumberFormatCreator::ImplGetExtNumberFormat( SdrPowerPointImport const &
             case 0 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_CHARS_LOWER_LETTER );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 1 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_CHARS_UPPER_LETTER );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 2 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ARABIC );
-                rNumberFormat.SetSuffix( ")" );
+                rNumberFormat.SetListFormat("",  ")", nLevel);
             }
             break;
             case 3 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ARABIC );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 4 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ROMAN_LOWER );
-                rNumberFormat.SetSuffix( ")" );
-                rNumberFormat.SetPrefix( "(" );
+                rNumberFormat.SetListFormat("(", ")", nLevel);
             }
             break;
             case 5 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ROMAN_LOWER );
-                rNumberFormat.SetSuffix( ")" );
+                rNumberFormat.SetListFormat("", ")", nLevel);
             }
             break;
             case 6 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ROMAN_LOWER );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 7 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ROMAN_UPPER );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 8 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_CHARS_LOWER_LETTER );
-                rNumberFormat.SetSuffix( ")" );
-                rNumberFormat.SetPrefix( "(" );
+                rNumberFormat.SetListFormat("(", ")", nLevel);
             }
             break;
             case 9 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_CHARS_LOWER_LETTER );
-                rNumberFormat.SetSuffix( ")" );
+                rNumberFormat.SetListFormat("", ")", nLevel);
             }
             break;
             case 10 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_CHARS_UPPER_LETTER );
-                rNumberFormat.SetSuffix( ")" );
-                rNumberFormat.SetPrefix( "(" );
+                rNumberFormat.SetListFormat("(", ")", nLevel);
             }
             break;
             case 11 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_CHARS_UPPER_LETTER );
-                rNumberFormat.SetSuffix( ")" );
+                rNumberFormat.SetListFormat("", ")", nLevel);
             }
             break;
             case 12 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ARABIC );
-                rNumberFormat.SetSuffix( ")" );
-                rNumberFormat.SetPrefix( "(" );
+                rNumberFormat.SetListFormat("(", ")", nLevel);
             }
             break;
             case 13 :
@@ -3581,14 +3577,13 @@ bool PPTNumberFormatCreator::ImplGetExtNumberFormat( SdrPowerPointImport const &
             case 14 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ROMAN_UPPER );
-                rNumberFormat.SetSuffix( ")" );
-                rNumberFormat.SetPrefix( "(" );
+                rNumberFormat.SetListFormat("(", ")", nLevel);
             }
             break;
             case 15 :
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_ROMAN_UPPER );
-                rNumberFormat.SetSuffix( ")" );
+                rNumberFormat.SetListFormat("", ")", nLevel);
             }
             break;
             case 16: // Simplified Chinese.
@@ -3599,7 +3594,7 @@ bool PPTNumberFormatCreator::ImplGetExtNumberFormat( SdrPowerPointImport const &
             case 17: // Simplified Chinese with single-byte period.
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_NUMBER_UPPER_ZH );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 18: // Double byte circle numbers.
@@ -3617,13 +3612,13 @@ bool PPTNumberFormatCreator::ImplGetExtNumberFormat( SdrPowerPointImport const &
             case 22: // Traditional Chinese with single-byte period.
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_NUMBER_UPPER_ZH_TW );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 25: // Bidi Hebrew 2 with ANSI minus symbol.
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_NUMBER_HEBREW );
-                rNumberFormat.SetSuffix( "-" );
+                rNumberFormat.SetListFormat("", "-", nLevel);
             }
             break;
             case 26: // Japanese/Korean.
@@ -3634,7 +3629,7 @@ bool PPTNumberFormatCreator::ImplGetExtNumberFormat( SdrPowerPointImport const &
             case 27: // Japanese/Korean with single-byte period.
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_NUMBER_LOWER_ZH );
-                rNumberFormat.SetSuffix( "." );
+                rNumberFormat.SetListFormat("", ".", nLevel);
             }
             break;
             case 28: // Double-byte Arabic numbers.
@@ -3645,13 +3640,13 @@ bool PPTNumberFormatCreator::ImplGetExtNumberFormat( SdrPowerPointImport const &
             case 29: // Double-byte Arabic numbers with double-byte period.
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_FULL_WIDTH_ARABIC );
-                rNumberFormat.SetSuffix( OUString( u'\xff0e' ) );
+                rNumberFormat.SetListFormat("", OUString(u'\xff0e'), nLevel);
             }
             break;
             case 38: // Japanese with double-byte period.
             {
                 rNumberFormat.SetNumberingType( SVX_NUM_NUMBER_LOWER_ZH ); // No such type. Instead with Lower Chinese Number
-                rNumberFormat.SetSuffix( OUString( u'\xff0e' ) );
+                rNumberFormat.SetListFormat("", OUString(u'\xff0e'), nLevel);
             }
             break;
         }

sd/source/ui/func/bulmaper.cxx
@@ -94,8 +94,7 @@ void SdBulletMapper::MapFontsInNumRule( SvxNumRule& aNumRule, const SfxItemSet& 
         }
         else if( rSrcLevel.GetNumberingType() == css::style::NumberingType::CHAR_SPECIAL )
         {
-            aNewLevel.SetPrefix("");
-            aNewLevel.SetSuffix("");
+            aNewLevel.SetListFormat("", "", nLevel);
             aNumRule.SetLevel(nLevel, aNewLevel );
         }
     }

sw/source/filter/ww8/wrtw8num.cxx
@@ -489,6 +489,8 @@ void MSWordExportBase::NumberingLevel(
                 }
             }
         }
+        else if (rFormat.GetNumberingType() != SVX_NUM_NUMBER_NONE)
+            assert(false && "depricated format still exists and is unhandled. Inform Vasily or Justin");
     }
 
     if (SVX_NUM_CHAR_SPECIAL == rFormat.GetNumberingType() ||

sw/source/filter/ww8/ww8par2.cxx
@@ -590,7 +590,7 @@ static void SetBaseAnlv(SwNumFormat &rNum, WW8_ANLV const &rAV, sal_uInt8 nSwLev
     if( rAV.nfc == 5 || rAV.nfc == 7 )
     {
         OUString sP = "." + rNum.GetSuffix();
-        rNum.SetSuffix( sP );   // ordinal number
+        rNum.SetListFormat("", sP, nSwLevel); // ordinal number
     }
 }
 

writerfilter/source/dmapper/PropertyIds.cxx
@@ -240,8 +240,6 @@ OUString getPropertyName( PropertyIds eId )
         case PROP_BULLET_CHAR      :    sName = "BulletChar"; break;
         case PROP_BULLET_FONT_NAME :    sName = "BulletFontName"; break;
         case PROP_TABS_RELATIVE_TO_INDENT:    sName = "TabsRelativeToIndent"; break;
-        case PROP_PREFIX           :    sName = "Prefix"; break;
-        case PROP_SUFFIX           :    sName = "Suffix"; break;
         case PROP_CREATE_FROM_LEVEL_PARAGRAPH_STYLES:    sName = "CreateFromLevelParagraphStyles"; break;
         case PROP_DROP_CAP_FORMAT  :    sName = "DropCapFormat"; break;
         case PROP_REFERENCE_FIELD_PART :    sName = "ReferenceFieldPart"; break;

writerfilter/source/dmapper/PropertyIds.hxx
@@ -247,7 +247,6 @@ enum PropertyIds
         ,PROP_POSITION_AND_SPACE_MODE
         ,PROP_POSITION_PROTECTED
         ,PROP_IS_PROTECTED
-        ,PROP_PREFIX
         ,PROP_REDLINE_AUTHOR
         ,PROP_REDLINE_DATE_TIME
         ,PROP_REDLINE_TYPE
@@ -272,7 +271,6 @@ enum PropertyIds
         ,PROP_START_WITH
         ,PROP_STREAM_NAME
         ,PROP_SUB_TYPE
-        ,PROP_SUFFIX
         ,PROP_SURROUND
         ,PROP_SURROUND_CONTOUR
         ,PROP_TABLE_BORDER

commit 844fc816ff59022791a9d518fdf1916023b15b6c

tdf#153423 tdf#150197: use getToken in proper order
This fixes a LO 7.5 regression from my commit
7f861f90f61cc62b45bd3f392e59fa7af747e60a

getToken steps along the string and consumes it,
so these needed to remain in the same order as
they had been originally. Dumb thing to overlook.

Change-Id: I483980eafb7deb0c224063ad41616bb21ddb2191
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146672
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/core/edit/autofmt.cxx
@@ -1616,8 +1616,9 @@ void SwAutoFormat::BuildEnum( sal_uInt16 nLvl, sal_uInt16 nDigitLevel )
                 if( !nDigitLevel )
                 {
                     SwNumFormat aFormat( aRule.Get( nLvl ) );
+                    const OUString sPrefix = aPrefix.getToken(0, u'\x0001', nPrefixIdx);
                     aFormat.SetStart( o3tl::narrowing<sal_uInt16>(o3tl::toInt32(o3tl::getToken(aPrefix, 0, u'\x0001', nPrefixIdx ))));
-                    aFormat.SetListFormat(aPrefix.getToken(0, u'\x0001', nPrefixIdx), aPostfix.getToken(0, u'\x0001'), nLvl);
+                    aFormat.SetListFormat(sPrefix, aPostfix.getToken(0, u'\x0001'), nLvl);
                     aFormat.SetIncludeUpperLevels( 0 );
 
                     if( !aFormat.GetCharFormat() )
@@ -1639,8 +1640,8 @@ void SwAutoFormat::BuildEnum( sal_uInt16 nLvl, sal_uInt16 nDigitLevel )
                     {
                         SwNumFormat aFormat( aRule.Get( n ) );
 
-                        aFormat.SetStart( o3tl::narrowing<sal_uInt16>(o3tl::toInt32(o3tl::getToken(aPrefix, 0, u'\x0001', nPrefixIdx )) ));
                         const OUString sPrefix = n ? "" : aPrefix.getToken(0, u'\x0001', nPrefixIdx);
+                        aFormat.SetStart( o3tl::narrowing<sal_uInt16>(o3tl::toInt32(o3tl::getToken(aPrefix, 0, u'\x0001', nPrefixIdx )) ));
                         aFormat.SetListFormat(sPrefix, aPostfix.getToken(0, u'\x0001', nPostfixIdx), n);
                         aFormat.SetIncludeUpperLevels( MAXLEVEL );
                         if( n < aNumTypes.getLength() )