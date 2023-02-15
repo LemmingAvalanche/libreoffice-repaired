LemmingAvalanche/libreoffice-repaired

commit 85cd974784cd3ac727b93b891a235dfb62675a13

tdf#58134 Reset Button in Options Dialogs
fixed a couple of these.

Change-Id: Ibef29afee1d702af2812996093deb7204c74e32c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136195
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

cui/source/options/optchart.cxx
@@ -157,7 +157,16 @@ bool SvxDefaultColorOptPage::FillItemSet( SfxItemSet* rOutAttrs )
 
 void SvxDefaultColorOptPage::Reset( const SfxItemSet* )
 {
-    m_xLbChartColors->select( 0 );
+    if( m_SvxChartColorTableUniquePtr )
+    {
+        m_SvxChartColorTableUniquePtr->useDefault();
+
+        FillBoxChartColorLB();
+
+        m_xLbChartColors->grab_focus();
+        m_xLbChartColors->select( 0 );
+        m_xPBRemove->set_sensitive(true);
+    }
 }
 
 void SvxDefaultColorOptPage::FillPaletteLB()

cui/source/options/optinet2.cxx
@@ -204,6 +204,8 @@ void SvxProxyTabPage::ReadConfigData_Impl()
         nIntValue = *x;
         m_xHttpPortED->set_text( OUString::number( nIntValue ));
     }
+    else
+        m_xHttpPortED->set_text( "" );
 
     m_xHttpsProxyED->set_text( officecfg::Inet::Settings::ooInetHTTPSProxyName::get() );
     x = officecfg::Inet::Settings::ooInetHTTPSProxyPort::get();
@@ -212,6 +214,8 @@ void SvxProxyTabPage::ReadConfigData_Impl()
         nIntValue = *x;
         m_xHttpsPortED->set_text( OUString::number( nIntValue ));
     }
+    else
+        m_xHttpsPortED->set_text( "" );
 
     m_xFtpProxyED->set_text( officecfg::Inet::Settings::ooInetFTPProxyName::get() );
     x = officecfg::Inet::Settings::ooInetFTPProxyPort::get();
@@ -220,6 +224,8 @@ void SvxProxyTabPage::ReadConfigData_Impl()
         nIntValue = *x;
         m_xFtpPortED->set_text( OUString::number( nIntValue ));
     }
+    else
+        m_xFtpPortED->set_text( "" );
 
     m_xNoProxyForED->set_text( officecfg::Inet::Settings::ooInetNoProxy::get() );
 }

sc/source/ui/optdlg/tpcalc.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#undef SC_DLLIMPLEMENTATION
+    #undef SC_DLLIMPLEMENTATION
 
 #include <vcl/svapp.hxx>
 #include <vcl/weld.hxx>
@@ -143,6 +143,7 @@ void ScTpCalcOptions::Reset(const SfxItemSet* rCoreAttrs)
         m_xFtPrec->set_sensitive(false);
         m_xEdPrec->set_sensitive(false);
         m_xBtnGeneralPrec->set_active(false);
+        m_xEdPrec->set_value(0);
     }
     else
     {

commit f8d8377255d14a9c5467c696780c51d04b71c6c1

tdf#153172 Options > Charts > Default Colors no longer persistent
regression from
    commit 85cd974784cd3ac727b93b891a235dfb62675a13
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Mon Jun 20 19:17:00 2022 +0200
    tdf#58134 Reset Button in Options Dialogs

Change-Id: Idef9880e29c322ae2d11641354baf8a241fc7fb0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147049
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

cui/source/options/optchart.cxx
@@ -157,16 +157,7 @@ bool SvxDefaultColorOptPage::FillItemSet( SfxItemSet* rOutAttrs )
 
 void SvxDefaultColorOptPage::Reset( const SfxItemSet* )
 {
-    if( m_SvxChartColorTableUniquePtr )
-    {
-        m_SvxChartColorTableUniquePtr->useDefault();
-
-        FillBoxChartColorLB();
-
-        m_xLbChartColors->grab_focus();
-        m_xLbChartColors->select( 0 );
-        m_xPBRemove->set_sensitive(true);
-    }
+    m_xLbChartColors->select( 0 );
 }
 
 void SvxDefaultColorOptPage::FillPaletteLB()