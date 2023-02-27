LemmingAvalanche/libreoffice-repaired

commit 3fa67136bfa3de6abcb1c376f16cb463e1e983d3

tdf#94635 Add FORECAST.ETS functions to Calc
Change-Id: Ifbfff1c27fb3960a06f467630da0fa39665f0ce4
Reviewed-on: https://gerrit.libreoffice.org/20073
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sc/source/core/tool/interpr3.cxx
@@ -154,7 +154,7 @@ static double lcl_IterateInverse( const ScDistFunc& rFunction, double fAx, doubl
         {
             fAx = fRx; fAy = fRy;
         }
-        // if last iteration brought to small advance, then do bisection next
+        // if last interation brought to small advance, then do bisection next
         // time, for safety
         bHasToInterpolate = bHasToInterpolate && (fabs(fRy) * 2.0 <= fabs(fQy));
         ++nCount;
@@ -3349,12 +3349,6 @@ void ScInterpreter::ScMedian()
 double ScInterpreter::GetPercentile( vector<double> & rArray, double fPercentile )
 {
     size_t nSize = rArray.size();
-    if (rArray.empty() || nSize == 0 || nGlobalError)
-    {
-        SetError( errNoValue);
-        return 0.0;
-    }
-
     if (nSize == 1)
         return rArray[0];
     else
@@ -3420,6 +3414,11 @@ void ScInterpreter::ScPercentile( bool bInclusive )
     }
     vector<double> aArray;
     GetNumberSequenceArray( 1, aArray, false );
+    if ( aArray.empty() || aArray.size() == 0 || nGlobalError )
+    {
+        SetError( errNoValue );
+        return;
+    }
     if ( bInclusive )
         PushDouble( GetPercentile( aArray, alpha ));
     else
@@ -3438,6 +3437,11 @@ void ScInterpreter::ScQuartile( bool bInclusive )
     }
     vector<double> aArray;
     GetNumberSequenceArray( 1, aArray, false );
+    if ( aArray.empty() || aArray.size() == 0 || nGlobalError )
+    {
+        SetError( errNoValue );
+        return;
+    }
     if ( bInclusive )
         PushDouble( fFlag == 2.0 ? GetMedian( aArray ) : GetPercentile( aArray, 0.25 * fFlag ) );
     else

formula/source/core/resource/core_resource.src
@@ -368,6 +368,14 @@ Resource RID_STRLIST_FUNCTION_NAMES_ENGLISH_ODFF
     String SC_OPCODE_LINEST { Text = "LINEST" ; };
     String SC_OPCODE_LOGEST { Text = "LOGEST" ; };
     String SC_OPCODE_FORECAST { Text = "FORECAST" ; };
+    String SC_OPCODE_FORECAST_ETS_ADD { Text = "FORECAST.ETS" ; };
+    String SC_OPCODE_FORECAST_ETS_SEA { Text = "FORECAST.ETS.SEASONALITY" ; };
+    String SC_OPCODE_FORECAST_ETS_MUL { Text = "FORECAST.ETS.MULT" ; };
+    String SC_OPCODE_FORECAST_ETS_PIA { Text = "FORECAST.ETS.CONFINT" ; };
+    String SC_OPCODE_FORECAST_ETS_PIM { Text = "FORECAST.ETS.PI.MULT" ; };
+    String SC_OPCODE_FORECAST_ETS_STA { Text = "FORECAST.ETS.STAT" ; };
+    String SC_OPCODE_FORECAST_ETS_STM { Text = "FORECAST.ETS.STAT.MULT" ; };
+    String SC_OPCODE_FORECAST_LIN { Text = "FORECAST.LINEAR" ; };
     String SC_OPCODE_CHI_INV { Text = "LEGACY.CHIINV" ; };
     String SC_OPCODE_CHI_INV_MS { Text = "COM.MICROSOFT.CHISQ.INV.RT" ; };
     String SC_OPCODE_GAMMA_DIST { Text = "GAMMADIST" ; };
@@ -794,6 +802,14 @@ Resource RID_STRLIST_FUNCTION_NAMES_ENGLISH_OOXML
     String SC_OPCODE_LINEST { Text = "LINEST" ; };
     String SC_OPCODE_LOGEST { Text = "LOGEST" ; };
     String SC_OPCODE_FORECAST { Text = "FORECAST" ; };
+    String SC_OPCODE_FORECAST_ETS_ADD { Text = "_xlfn.FORECAST.ETS" ; };
+    String SC_OPCODE_FORECAST_ETS_SEA { Text = "_xlfn.FORECAST.ETS.SEASONALITY" ; };
+    String SC_OPCODE_FORECAST_ETS_MUL { Text = "_xlfn.ORG.LIBREOFFICE.FORECAST.ETS.MULT" ; };
+    String SC_OPCODE_FORECAST_ETS_PIA { Text = "_xlfn.FORECAST.ETS.CONFINT" ; };
+    String SC_OPCODE_FORECAST_ETS_PIM { Text = "_xlfn.ORG.LIBREOFFICE.FORECAST.ETS.PI.MULT" ; };
+    String SC_OPCODE_FORECAST_ETS_STA { Text = "_xlfn.FORECAST.ETS.STAT" ; };
+    String SC_OPCODE_FORECAST_ETS_STM { Text = "_xlfn.ORG.LIBREOFFICE.FORECAST.ETS.STAT.MULT" ; };
+    String SC_OPCODE_FORECAST_LIN { Text = "_xlfn.FORECAST.LINEAR" ; };
     String SC_OPCODE_CHI_INV { Text = "CHIINV" ; };
     String SC_OPCODE_CHI_INV_MS { Text = "_xlfn.CHISQ.INV.RT" ; };
     String SC_OPCODE_GAMMA_DIST { Text = "GAMMADIST" ; };
@@ -1220,6 +1236,14 @@ Resource RID_STRLIST_FUNCTION_NAMES_ENGLISH
     String SC_OPCODE_LINEST { Text = "LINEST" ; };
     String SC_OPCODE_LOGEST { Text = "LOGEST" ; };
     String SC_OPCODE_FORECAST { Text = "FORECAST" ; };
+    String SC_OPCODE_FORECAST_ETS_ADD { Text = "FORECAST.ETS.ADD" ; };
+    String SC_OPCODE_FORECAST_ETS_SEA { Text = "FORECAST.ETS.SEASONALITY" ; };
+    String SC_OPCODE_FORECAST_ETS_MUL { Text = "FORECAST.ETS.MULT" ; };
+    String SC_OPCODE_FORECAST_ETS_PIA { Text = "FORECAST.ETS.PI.ADD" ; };
+    String SC_OPCODE_FORECAST_ETS_PIM { Text = "FORECAST.ETS.PI.MULT" ; };
+    String SC_OPCODE_FORECAST_ETS_STA { Text = "FORECAST.ETS.STAT.ADD" ; };
+    String SC_OPCODE_FORECAST_ETS_STM { Text = "FORECAST.ETS.STAT.MULT" ; };
+    String SC_OPCODE_FORECAST_LIN { Text = "FORECAST.LINEAR" ; };
     String SC_OPCODE_CHI_INV { Text = "CHIINV" ; };
     String SC_OPCODE_CHI_INV_MS { Text = "CHISQ.INV.RT" ; };
     String SC_OPCODE_GAMMA_DIST { Text = "GAMMADIST" ; };
@@ -2620,6 +2644,38 @@ Resource RID_STRLIST_FUNCTION_NAMES
     String SC_OPCODE_FORECAST
     {
         Text [ en-US ] = "FORECAST" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_ADD
+    {
+        Text [ en-US ] = "FORECAST.ETS.ADD" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_SEA
+    {
+        Text [ en-US ] = "FORECAST.ETS.SEASONALITY" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_MUL
+    {
+        Text [ en-US ] = "FORECAST.ETS.MULT" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_PIA
+    {
+        Text [ en-US ] = "FORECAST.ETS.PI.ADD" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_PIM
+    {
+        Text [ en-US ] = "FORECAST.ETS.PI.MULT" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_STA
+    {
+        Text [ en-US ] = "FORECAST.ETS.STAT.ADD" ;
+    };
+    String SC_OPCODE_FORECAST_ETS_STM
+    {
+        Text [ en-US ] = "FORECAST.ETS.STAT.MULT" ;
+    };
+    String SC_OPCODE_FORECAST_LIN
+    {
+        Text [ en-US ] = "FORECAST.LINEAR" ;
     };
     String SC_OPCODE_CHI_INV
     {

include/formula/compiler.hrc
@@ -487,7 +487,15 @@
 #define SC_OPCODE_FLOOR_PRECISE     476
 #define SC_OPCODE_RAWSUBTRACT       477
 #define SC_OPCODE_WEEKNUM_OOO       478
-#define SC_OPCODE_STOP_2_PAR        479     /* last function with two or more parameters' OpCode + 1 */
+#define SC_OPCODE_FORECAST_ETS_ADD  479
+#define SC_OPCODE_FORECAST_ETS_SEA  480
+#define SC_OPCODE_FORECAST_ETS_MUL  481
+#define SC_OPCODE_FORECAST_ETS_PIA  482
+#define SC_OPCODE_FORECAST_ETS_PIM  483
+#define SC_OPCODE_FORECAST_ETS_STA  484
+#define SC_OPCODE_FORECAST_ETS_STM  485
+#define SC_OPCODE_FORECAST_LIN      486
+#define SC_OPCODE_STOP_2_PAR        487     /* last function with two or more parameters' OpCode + 1 */
 
 #define SC_OPCODE_STOP_FUNCTION     SC_OPCODE_STOP_2_PAR            /* last function's OpCode + 1 */
 #define SC_OPCODE_LAST_OPCODE_ID    (SC_OPCODE_STOP_FUNCTION - 1)   /* last OpCode */

include/formula/opcode.hxx
@@ -421,6 +421,14 @@ enum OpCode : sal_uInt16
         ocLinest            = SC_OPCODE_LINEST,
         ocLogest            = SC_OPCODE_LOGEST,
         ocForecast          = SC_OPCODE_FORECAST,
+        ocForecast_ETS_ADD  = SC_OPCODE_FORECAST_ETS_ADD,
+        ocForecast_ETS_SEA  = SC_OPCODE_FORECAST_ETS_SEA,
+        ocForecast_ETS_MUL  = SC_OPCODE_FORECAST_ETS_MUL,
+        ocForecast_ETS_PIA  = SC_OPCODE_FORECAST_ETS_PIA,
+        ocForecast_ETS_PIM  = SC_OPCODE_FORECAST_ETS_PIM,
+        ocForecast_ETS_STA  = SC_OPCODE_FORECAST_ETS_STA,
+        ocForecast_ETS_STM  = SC_OPCODE_FORECAST_ETS_STM,
+        ocForecast_LIN      = SC_OPCODE_FORECAST_LIN,
         ocChiInv            = SC_OPCODE_CHI_INV,
         ocChiInv_MS         = SC_OPCODE_CHI_INV_MS,
         ocGammaDist         = SC_OPCODE_GAMMA_DIST,

sc/Library_sc.mk
@@ -243,6 +243,7 @@ $(eval $(call gb_Library_add_exception_objects,sc,\
     sc/source/core/tool/interpr5 \
     sc/source/core/tool/interpr6 \
     sc/source/core/tool/interpr7 \
+    sc/source/core/tool/interpr8 \
     sc/source/core/tool/jumpmatrix \
     sc/source/core/tool/listenerquery \
     sc/source/core/tool/lookupcache \

sc/inc/helpids.h
@@ -628,5 +628,13 @@
 #define HID_FUNC_FLOOR_PRECISE                                  "SC_HID_FUNC_FLOOR_PRECISE"
 #define HID_FUNC_RAWSUBTRACT                                    "SC_HID_FUNC_RAWSUBTRACT"
 #define HID_FUNC_WEEKNUM_OOO                                    "SC_HID_FUNC_WEEKNUM_OOO"
+#define HID_FUNC_FORECAST_ETS_ADD                               "SC_HID_FUNC_FORECAST_ETS_ADD"
+#define HID_FUNC_FORECAST_ETS_MUL                               "SC_HID_FUNC_FORECAST_ETS_MUL"
+#define HID_FUNC_FORECAST_ETS_PIA                               "SC_HID_FUNC_FORECAST_ETS_PIA"
+#define HID_FUNC_FORECAST_ETS_PIM                               "SC_HID_FUNC_FORECAST_ETS_PIM"
+#define HID_FUNC_FORECAST_ETS_SEA                               "SC_HID_FUNC_FORECAST_ETS_SEA"
+#define HID_FUNC_FORECAST_ETS_STA                               "SC_HID_FUNC_FORECAST_ETS_STA"
+#define HID_FUNC_FORECAST_ETS_STM                               "SC_HID_FUNC_FORECAST_ETS_STM"
+#define HID_FUNC_FORECAST_LIN                                   "SC_HID_FUNC_FORECAST_LIN"
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/qa/unit/ucalc.cxx
@@ -2619,6 +2619,14 @@ void Test::testFunctionLists()
         "FISHER",
         "FISHERINV",
         "FORECAST",
+        "FORECAST.ETS.ADD",
+        "FORECAST.ETS.MULT",
+        "FORECAST.ETS.PI.ADD",
+        "FORECAST.ETS.PI.MULT",
+        "FORECAST.ETS.SEASONALITY",
+        "FORECAST.ETS.STAT.ADD",
+        "FORECAST.ETS.STAT.MULT",
+        "FORECAST.LINEAR",
         "FTEST",
         "GAMMA",
         "GAMMA.DIST",

sc/source/core/inc/interpre.hxx
@@ -102,6 +102,17 @@ enum ScIterFuncIfs
     ifCOUNTIFS    // Multi-Conditional count
 };
 
+enum ScETSType
+{
+    etsAdd,
+    etsMult,
+    etsSeason,
+    etsPIAdd,
+    etsPIMult,
+    etsStatAdd,
+    etsStatMult
+};
+
 struct FormulaTokenRef_less
 {
     bool operator () ( const formula::FormulaConstTokenRef& r1, const formula::FormulaConstTokenRef& r2 ) const
@@ -762,6 +773,7 @@ bool CheckMatrix(bool _bLOG,sal_uInt8& nCase,SCSIZE& nCX,SCSIZE& nCY,SCSIZE& nRX
 void ScLinest();
 void ScLogest();
 void ScForecast();
+void ScForecast_Ets( ScETSType eETSType );
 void ScNoName();
 void ScBadName();
 // Statistics:
@@ -772,6 +784,7 @@ public:
 static SC_DLLPUBLIC double phi(double x);
 static SC_DLLPUBLIC double integralPhi(double x);
 static SC_DLLPUBLIC double gaussinv(double x);
+static SC_DLLPUBLIC double GetPercentile( ::std::vector<double> & rArray, double fPercentile );
 
 private:
 double GetBetaDist(double x, double alpha, double beta);  //cumulative distribution function
@@ -841,7 +854,6 @@ void ScSkew();
 void ScSkewp();
 void ScMedian();
 double GetMedian( ::std::vector<double> & rArray );
-double GetPercentile( ::std::vector<double> & rArray, double fPercentile );
 double GetPercentileExclusive( ::std::vector<double> & rArray, double fPercentile );
 void GetNumberSequenceArray( sal_uInt8 nParamCount, ::std::vector<double>& rArray, bool bConvertTextInArray );
 void GetSortArray( sal_uInt8 nParamCount, ::std::vector<double>& rSortArray, ::std::vector<long>* pIndexOrder, bool bConvertTextInArray, bool bAllowEmptyArray );

commit 0268b4f9f363a4cea680078a0264be09bb0ea375

Always push a result, even if it's only an error
PERCENTILE() and QUARTILE() if an error was passed as argument (or
an error encountered during obtaining arguments) omitted to push
an error result, only setting the error.

Fallout from

    commit 3fa67136bfa3de6abcb1c376f16cb463e1e983d3
    CommitDate: Thu Mar 3 16:28:59 2016 +0000

        tdf#94635 Add FORECAST.ETS functions to Calc

Change-Id: I23e276fb0ce735cfd6383cc963446499dcf819f4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147922
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/source/core/tool/interpr3.cxx
@@ -3470,7 +3470,7 @@ void ScInterpreter::ScPercentile( bool bInclusive )
     GetNumberSequenceArray( 1, aArray, false );
     if ( aArray.empty() || nGlobalError != FormulaError::NONE )
     {
-        SetError( FormulaError::NoValue );
+        PushNoValue();
         return;
     }
     if ( bInclusive )
@@ -3493,7 +3493,7 @@ void ScInterpreter::ScQuartile( bool bInclusive )
     GetNumberSequenceArray( 1, aArray, false );
     if ( aArray.empty() || nGlobalError != FormulaError::NONE )
     {
-        SetError( FormulaError::NoValue );
+        PushNoValue();
         return;
     }
     if ( bInclusive )