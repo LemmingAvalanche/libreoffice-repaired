LemmingAvalanche/libreoffice-repaired

commit c27bfddbbe169e1d3077a8ad95dc0eb5a63daae0

i#32345 Support a matrix of rank argument for LARGE()/SMALL()
This is a followup of 2ae034c63a54635672d3bb9c04514db8d904c699.

Change-Id: I7052e113173204f7cfcd40622f52c5d4e14b3aa9
Reviewed-on: https://gerrit.libreoffice.org/64139
Tested-by: Jenkins
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/core/tool/interpr3.cxx
@@ -3639,14 +3639,15 @@ void ScInterpreter::CalculateSmallLarge(bool bSmall)
     if ( !MustHaveParamCount( GetByte(), 2 )  )
         return;
 
-    std::vector<double> aArray;
-    GetNumberSequenceArray(1, aArray, false);
+    SCSIZE nCol = 0, nRow = 0;
+    auto aArray = GetTopNumberArray(nCol, nRow);
     auto aArraySize = aArray.size();
     if (aArraySize == 0 || nGlobalError != FormulaError::NONE)
     {
         PushNoValue();
         return;
     }
+    assert(aArraySize == nCol * nRow);
     for (double fArg : aArray)
     {
         double f = ::rtl::math::approxFloor(fArg);
@@ -3701,8 +3702,7 @@ void ScInterpreter::CalculateSmallLarge(bool bSmall)
         aArray.clear();
         for (SCSIZE n : aRankArray)
             aArray.push_back(aSortArray[bSmall ? n-1 : nSize-n]);
-        ScMatrixRef pResult = GetNewMat(1, aArraySize, true);
-        pResult->PutDoubleVector(aArray, 0, 0);
+        ScMatrixRef pResult = GetNewMat(nCol, nRow, aArray);
         PushMatrix(pResult);
     }
 }
@@ -3839,6 +3839,91 @@ void ScInterpreter::ScTrimMean()
             fSum += aSortArray[i];
         PushDouble(fSum/static_cast<double>(nSize-2*nIndex));
     }
+}
+
+std::vector<double> ScInterpreter::GetTopNumberArray( SCSIZE& rCol, SCSIZE& rRow )
+{
+    std::vector<double> aArray;
+    switch (GetStackType())
+    {
+        case svDouble:
+            aArray.push_back(PopDouble());
+            rCol = rRow = 1;
+        break;
+        case svSingleRef:
+        {
+            ScAddress aAdr;
+            PopSingleRef(aAdr);
+            ScRefCellValue aCell(*pDok, aAdr);
+            if (aCell.hasNumeric())
+            {
+                aArray.push_back(GetCellValue(aAdr, aCell));
+                rCol = rRow = 1;
+            }
+        }
+        break;
+        case svDoubleRef:
+        {
+            ScRange aRange;
+            PopDoubleRef(aRange, true);
+            if (nGlobalError != FormulaError::NONE)
+                break;
+
+            // give up unless the start and end are in the same sheet
+            if (aRange.aStart.Tab() != aRange.aEnd.Tab())
+            {
+                SetError(FormulaError::IllegalParameter);
+                break;
+            }
+
+            // the range already is in order
+            assert(aRange.aStart.Col() <= aRange.aEnd.Col());
+            assert(aRange.aStart.Row() <= aRange.aEnd.Row());
+            rCol = aRange.aEnd.Col() - aRange.aStart.Col() + 1;
+            rRow = aRange.aEnd.Row() - aRange.aStart.Row() + 1;
+            aArray.reserve(rCol * rRow);
+
+            FormulaError nErr = FormulaError::NONE;
+            double fCellVal;
+            ScValueIterator aValIter(pDok, aRange, mnSubTotalFlags);
+            if (aValIter.GetFirst(fCellVal, nErr))
+            {
+                do
+                    aArray.push_back(fCellVal);
+                while (aValIter.GetNext(fCellVal, nErr) && nErr == FormulaError::NONE);
+            }
+            if (aArray.size() != rCol * rRow)
+            {
+                aArray.clear();
+                SetError(nErr);
+            }
+        }
+        break;
+        case svMatrix:
+        case svExternalSingleRef:
+        case svExternalDoubleRef:
+        {
+            ScMatrixRef pMat = GetMatrix();
+            if (!pMat)
+                break;
+
+            if (pMat->IsNumeric())
+            {
+                SCSIZE nCount = pMat->GetElementCount();
+                aArray.reserve(nCount);
+                for (SCSIZE i = 0; i < nCount; ++i)
+                    aArray.push_back(pMat->GetDouble(i));
+                pMat->GetDimensions(rCol, rRow);
+            }
+            else
+                SetError(FormulaError::IllegalParameter);
+        }
+        break;
+        default:
+            SetError(FormulaError::IllegalParameter);
+        break;
+    }
+    return aArray;
 }
 
 void ScInterpreter::GetNumberSequenceArray( sal_uInt8 nParamCount, vector<double>& rArray, bool bConvertTextInArray )

sc/qa/unit/data/functions/statistical/fods/large.fods
@@ -4794,10 +4794,18 @@
      <table:table-cell table:number-columns-repeated="2"/>
     </table:table-row>
     <table:table-row table:style-name="ro8">
-     <table:table-cell table:style-name="ce10"/>
-     <table:table-cell/>
-     <table:table-cell table:style-name="ce17"/>
-     <table:table-cell table:style-name="ce22"/>
+     <table:table-cell table:style-name="ce11" table:formula="of:=MDETERM(LARGE([.I30:.I39];{1;2|3;4}))" office:value-type="float" office:value="-2" calcext:value-type="float">
+      <text:p>-2</text:p>
+     </table:table-cell>
+     <table:table-cell office:value-type="float" office:value="-2" calcext:value-type="float">
+      <text:p>-2</text:p>
+     </table:table-cell>
+     <table:table-cell table:style-name="ce16" table:formula="of:=ROUND([.A30];12)=ROUND([.B30];12)" office:value-type="boolean" office:boolean-value="true" calcext:value-type="boolean">
+      <text:p>TRUE</text:p>
+     </table:table-cell>
+     <table:table-cell table:style-name="ce22" table:formula="of:=FORMULA([.A30])" office:value-type="string" office:string-value="=MDETERM(LARGE(I30:I39,{1,2;3,4}))" calcext:value-type="string">
+      <text:p>=MDETERM(LARGE(I30:I39,{1,2;3,4}))</text:p>
+     </table:table-cell>
      <table:table-cell table:number-columns-repeated="4"/>
      <table:table-cell table:style-name="ce11" office:value-type="float" office:value="2" calcext:value-type="float">
       <text:p>2</text:p>

sc/qa/unit/data/functions/statistical/fods/small.fods
@@ -4186,10 +4186,19 @@
      <table:table-cell table:style-name="ce27" table:number-columns-repeated="4"/>
      <table:table-cell table:number-columns-repeated="2"/>
     </table:table-row>
-    <table:table-row table:style-name="ro6">
-     <table:table-cell table:number-columns-repeated="2"/>
-     <table:table-cell table:style-name="ce28"/>
-     <table:table-cell table:style-name="ce32"/>
+    <table:table-row table:style-name="ro2">
+     <table:table-cell table:style-name="ce19" table:formula="of:=MDETERM(SMALL([.F1:.F11];{1;2|3;4}))" office:value-type="float" office:value="-13" calcext:value-type="float">
+      <text:p>-13</text:p>
+     </table:table-cell>
+     <table:table-cell office:value-type="float" office:value="-13" calcext:value-type="float">
+      <text:p>-13</text:p>
+     </table:table-cell>
+     <table:table-cell table:style-name="ce25" table:formula="of:=ROUND([.A13];12)=ROUND([.B13];12)" office:value-type="boolean" office:boolean-value="true" calcext:value-type="boolean">
+      <text:p>TRUE</text:p>
+     </table:table-cell>
+     <table:table-cell table:style-name="ce32" table:formula="of:=FORMULA([.A13])" office:value-type="string" office:string-value="=MDETERM(SMALL(F1:F11,{1,2;3,4}))" calcext:value-type="string">
+      <text:p>=MDETERM(SMALL(F1:F11,{1,2;3,4}))</text:p>
+     </table:table-cell>
      <table:table-cell table:number-columns-repeated="4"/>
      <table:table-cell office:value-type="float" office:value="900000012" calcext:value-type="float">
       <text:p>900000012</text:p>

sc/source/core/inc/interpre.hxx
@@ -175,6 +175,8 @@ public:
 
     ScMatrixRef GetNewMat(SCSIZE nC, SCSIZE nR, bool bEmpty = false);
 
+    ScMatrixRef GetNewMat(SCSIZE nC, SCSIZE nR, const std::vector<double>& rValues);
+
     enum VolatileType {
         VOLATILE,
         VOLATILE_MACRO,
@@ -224,6 +226,8 @@ private:
     bool        bMatrixFormula;         // formula cell is a matrix formula
 
     VolatileType meVolatileType;
+
+    void MakeMatNew(ScMatrixRef& rMat, SCSIZE nC, SCSIZE nR);
 
     /// Merge global and document specific settings.
     void MergeCalcConfig();
@@ -934,6 +938,7 @@ private:
     void ScMedian();
     double GetMedian( ::std::vector<double> & rArray );
     double GetPercentileExclusive( ::std::vector<double> & rArray, double fPercentile );
+    std::vector<double> GetTopNumberArray( SCSIZE& rCol, SCSIZE& rRow );
     void GetNumberSequenceArray( sal_uInt8 nParamCount, ::std::vector<double>& rArray, bool bConvertTextInArray );
     void GetSortArray( sal_uInt8 nParamCount, ::std::vector<double>& rSortArray, ::std::vector<long>* pIndexOrder, bool bConvertTextInArray, bool bAllowEmptyArray );
     static void QuickSort(::std::vector<double>& rSortArray, ::std::vector<long>* pIndexOrder);

sc/source/core/tool/interpr5.cxx
@@ -281,6 +281,21 @@ void ScInterpreter:: ScLCM()
     }
 }
 
+void ScInterpreter::MakeMatNew(ScMatrixRef& rMat, SCSIZE nC, SCSIZE nR)
+{
+    rMat->SetErrorInterpreter( this);
+    // A temporary matrix is mutable and ScMatrix::CloneIfConst() returns the
+    // very matrix.
+    rMat->SetMutable();
+    SCSIZE nCols, nRows;
+    rMat->GetDimensions( nCols, nRows);
+    if ( nCols != nC || nRows != nR )
+    {   // arbitrary limit of elements exceeded
+        SetError( FormulaError::MatrixSize);
+        rMat.reset();
+    }
+}
+
 ScMatrixRef ScInterpreter::GetNewMat(SCSIZE nC, SCSIZE nR, bool bEmpty)
 {
     ScMatrixRef pMat;
@@ -288,18 +303,14 @@ ScMatrixRef ScInterpreter::GetNewMat(SCSIZE nC, SCSIZE nR, bool bEmpty)
         pMat = new ScMatrix(nC, nR);
     else
         pMat = new ScMatrix(nC, nR, 0.0);
+    MakeMatNew(pMat, nC, nR);
+    return pMat;
+}
 
-    pMat->SetErrorInterpreter( this);
-    // A temporary matrix is mutable and ScMatrix::CloneIfConst() returns the
-    // very matrix.
-    pMat->SetMutable();
-    SCSIZE nCols, nRows;
-    pMat->GetDimensions( nCols, nRows);
-    if ( nCols != nC || nRows != nR )
-    {   // arbitrary limit of elements exceeded
-        SetError( FormulaError::MatrixSize);
-        pMat.reset();
-    }
+ScMatrixRef ScInterpreter::GetNewMat(SCSIZE nC, SCSIZE nR, const std::vector<double>& rValues)
+{
+    ScMatrixRef pMat(new ScMatrix(nC, nR, rValues));
+    MakeMatNew(pMat, nC, nR);
     return pMat;
 }
 

commit ae0757133dad63f43a55dcd1566590cff63832db

Resolves: tdf#153924 handle non-numeric and error values in rank array
... of LARGE()/SMALL() instead of yielding error value for all result
elements.

Fallout from

    commit c27bfddbbe169e1d3077a8ad95dc0eb5a63daae0
    CommitDate: Fri Nov 30 22:14:17 2018 +0100

        i#32345 Support a matrix of rank argument for LARGE()/SMALL()

that in ScInterpreter::GetTopNumberArray() required the entire
rank array would have to be numeric, which with an empty cell or
string it isn't.

Change-Id: Ieaa1a68bb8f98614119550b1442665b6fbb4817a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148178
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/source/core/tool/interpr3.cxx
@@ -3638,8 +3638,8 @@ void ScInterpreter::CalculateSmallLarge(bool bSmall)
         return;
 
     SCSIZE nCol = 0, nRow = 0;
-    auto aArray = GetTopNumberArray(nCol, nRow);
-    const auto nRankArraySize = aArray.size();
+    const auto aArray = GetTopNumberArray(nCol, nRow);
+    const size_t nRankArraySize = aArray.size();
     if (nRankArraySize == 0 || nGlobalError != FormulaError::NONE)
     {
         PushNoValue();
@@ -3667,7 +3667,12 @@ void ScInterpreter::CalculateSmallLarge(bool bSmall)
     {
         const SCSIZE k = aRankArray[0];
         if (k < 1 || nSize < k)
-            PushNoValue();
+        {
+            if (!std::isfinite(aArray[0]))
+                PushDouble(aArray[0]);  // propagates error
+            else
+                PushNoValue();
+        }
         else
         {
             vector<double>::iterator iPos = aSortArray.begin() + (bSmall ? k-1 : nSize-k);
@@ -3699,15 +3704,19 @@ void ScInterpreter::CalculateSmallLarge(bool bSmall)
         else
             std::sort(aSortArray.begin(), aSortArray.end());
 
-        aArray.clear();
-        for (SCSIZE n : aRankArray)
+        std::vector<double> aResultArray;
+        aResultArray.reserve(nRankArraySize);
+        for (size_t i = 0; i < nRankArraySize; ++i)
         {
+            const SCSIZE n = aRankArray[i];
             if (1 <= n && n <= nSize)
-                aArray.push_back( aSortArray[bSmall ? n-1 : nSize-n]);
+                aResultArray.push_back( aSortArray[bSmall ? n-1 : nSize-n]);
+            else if (!std::isfinite( aArray[i]))
+                aResultArray.push_back( aArray[i]);  // propagate error
             else
-                aArray.push_back( CreateDoubleError( FormulaError::NoValue));
+                aResultArray.push_back( CreateDoubleError( FormulaError::IllegalArgument));
         }
-        ScMatrixRef pResult = GetNewMat(nCol, nRow, aArray);
+        ScMatrixRef pResult = GetNewMat(nCol, nRow, aResultArray);
         PushMatrix(pResult);
     }
 }
@@ -3912,19 +3921,30 @@ std::vector<double> ScInterpreter::GetTopNumberArray( SCSIZE& rCol, SCSIZE& rRow
             if (!pMat)
                 break;
 
+            const SCSIZE nCount = pMat->GetElementCount();
+            aArray.reserve(nCount);
+            // Do not propagate errors from matrix elements as global error.
+            pMat->SetErrorInterpreter(nullptr);
             if (pMat->IsNumeric())
             {
-                SCSIZE nCount = pMat->GetElementCount();
-                aArray.reserve(nCount);
                 for (SCSIZE i = 0; i < nCount; ++i)
                     aArray.push_back(pMat->GetDouble(i));
-                pMat->GetDimensions(rCol, rRow);
             }
             else
-                SetError(FormulaError::IllegalParameter);
+            {
+                for (SCSIZE i = 0; i < nCount; ++i)
+                {
+                    if (pMat->IsValue(i))
+                        aArray.push_back( pMat->GetDouble(i));
+                    else
+                        aArray.push_back( CreateDoubleError( FormulaError::NoValue));
+                }
+            }
+            pMat->GetDimensions(rCol, rRow);
         }
         break;
         default:
+            PopError();
             SetError(FormulaError::IllegalParameter);
         break;
     }