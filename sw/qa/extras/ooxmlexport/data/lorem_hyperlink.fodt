LemmingAvalanche/libreoffice-repaired

commit d9eeaaaec884418349ca7f0a32010c38fc55039c

fdo#76316 : File gets corrupt after Roundtrip
Issue :
	DOCX containing nested Hyperlinks, have mismatch xml tags.
	End tag for outer Hyperlinks does not get closed.

Implementation :
	1] Added code at Export side to maintain Count of
	   Hyperlinks started and ended.
	   Added check endParagraph() to to close Hyperlink tag
	   if not yet closed but started.
	2] Written Export Unit Test case.
NOTE : Corruption was occurring on "Without debug-util" build and Crash was occurring on "WITH debug-util build".

Reviewed on:
	https://gerrit.libreoffice.org/8654

Change-Id: I9a0ab48578f3d5f4a13c615f4e42a69e790d3ced

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -359,6 +359,16 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
 
     --m_nTextFrameLevel;
 
+    /* If m_nHyperLinkCount > 0 that means hyperlink tag is not yet colsed.
+     * This is due to nested hyperlink tags. So close it before end of paragraph.
+     */
+    if(m_nHyperLinkCount > 0)
+    {
+        for(sal_Int32 nHyperLinkToClose = 0; nHyperLinkToClose < m_nHyperLinkCount; ++nHyperLinkToClose)
+            m_pSerializer->endElementNS( XML_w, XML_hyperlink );
+        m_nHyperLinkCount = 0;
+    }
+
     m_pSerializer->endElementNS( XML_w, XML_p );
 
     WriteParagraphSdt();
@@ -712,6 +722,7 @@ void DocxAttributeOutput::EndRun()
         {
             m_pSerializer->endElementNS( XML_w, XML_hyperlink );
             m_startedHyperlink = false;
+            m_nHyperLinkCount--;
         }
         m_closeHyperlinkInPreviousRun = false;
     }
@@ -743,6 +754,7 @@ void DocxAttributeOutput::EndRun()
         m_pSerializer->startElementNS( XML_w, XML_hyperlink, xAttrList );
         m_pHyperlinkAttrList = NULL;
         m_startedHyperlink = true;
+        m_nHyperLinkCount++;
     }
 
     // if there is some redlining in the document, output it
@@ -830,6 +842,7 @@ void DocxAttributeOutput::EndRun()
 
             m_pSerializer->endElementNS( XML_w, XML_hyperlink );
             m_startedHyperlink = false;
+            m_nHyperLinkCount--;
         }
         m_closeHyperlinkInThisRun = false;
     }
@@ -7060,6 +7073,7 @@ DocxAttributeOutput::DocxAttributeOutput( DocxExport &rExport, FSHelperPtr pSeri
       m_closeHyperlinkInThisRun( false ),
       m_closeHyperlinkInPreviousRun( false ),
       m_startedHyperlink( false ),
+      m_nHyperLinkCount(0),
       m_postponedGraphic( NULL ),
       m_postponedDiagram( NULL ),
       m_postponedVMLDrawing(NULL),

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -771,6 +771,8 @@ private:
     bool m_closeHyperlinkInThisRun;
     bool m_closeHyperlinkInPreviousRun;
     bool m_startedHyperlink;
+    // Count nested HyperLinks
+    sal_Int32 m_nHyperLinkCount;
 
     struct PostponedGraphic
     {

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -2865,6 +2865,19 @@ DECLARE_OOXMLEXPORT_TEST(test76108, "test76108.docx")
     assertXPath(pXmlDoc, "/w:document[1]/w:body[1]/w:p[1]/w:r[1]/w:fldChar[1]", "fldCharType", "begin");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testHyperLinkTagEnded, "fdo76316.docx")
+{
+    /* XML tag <w:hyperlink> was not getting closed when its inside another
+     * <w:hyperlink> tag.
+     */
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+
+    if (!pXmlDoc) return;
+
+    CPPUNIT_ASSERT(pXmlDoc);
+    assertXPath(pXmlDoc, "/w:document[1]/w:body[1]/w:tbl[1]/w:tr[2]/w:tc[1]/w:tbl[1]/w:tr[1]/w:tc[1]/w:tbl[1]/w:tr[7]/w:tc[1]/w:tbl[1]/w:tr[2]/w:tc[6]/w:tbl[1]/w:tr[1]/w:tc[1]/w:p[1]/w:hyperlink[1]/w:hyperlink[1]",1);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testSimpleSdts, "simple-sdts.docx")
 {
     xmlDocPtr pXmlDoc = parseExport("word/document.xml");

commit e909adecd399bd1e1de2496fb3b5f95289047a8b

tdf#149996 DOCX export: fix hyperlinks in nested paragraphs
Close all hyperlinks in the paragraph where they start.

Regression from commit d9eeaaaec884418349ca7f0a32010c38fc55039c
"fdo#76316 : File gets corrupt after Roundtrip".

Change-Id: Ied5be90601f5a494f927d43aa6ff7efd3545f993
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148353
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -516,6 +516,7 @@ sal_Int32 DocxAttributeOutput::StartParagraph(ww8::WW8TableNodeInfo::Pointer_t p
 
     m_bParagraphOpened = true;
     m_bIsFirstParagraph = false;
+    m_nHyperLinkCount.push(0);
 
     return nParaId;
 }
@@ -1083,12 +1084,12 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
     /* If m_nHyperLinkCount > 0 that means hyperlink tag is not yet closed.
      * This is due to nested hyperlink tags. So close it before end of paragraph.
      */
-    if(m_nHyperLinkCount > 0)
+    if(m_nHyperLinkCount.top() > 0)
     {
-        for(sal_Int32 nHyperLinkToClose = 0; nHyperLinkToClose < m_nHyperLinkCount; ++nHyperLinkToClose)
+        for(sal_Int32 nHyperLinkToClose = 0; nHyperLinkToClose < m_nHyperLinkCount.top(); ++nHyperLinkToClose)
             m_pSerializer->endElementNS( XML_w, XML_hyperlink );
-        m_nHyperLinkCount = 0;
     }
+    m_nHyperLinkCount.pop();
 
     if (m_aRunSdt.m_bStartedSdt)
     {
@@ -1688,7 +1689,7 @@ void DocxAttributeOutput::EndRun(const SwTextNode* pNode, sal_Int32 nPos, sal_In
             m_pSerializer->endElementNS( XML_w, XML_hyperlink );
             m_startedHyperlink = false;
             m_endPageRef = false;
-            m_nHyperLinkCount--;
+            m_nHyperLinkCount.top()--;
         }
         m_closeHyperlinkInPreviousRun = false;
     }
@@ -1768,7 +1769,7 @@ void DocxAttributeOutput::EndRun(const SwTextNode* pNode, sal_Int32 nPos, sal_In
 
         m_pSerializer->startElementNS( XML_w, XML_hyperlink, xAttrList );
         m_startedHyperlink = true;
-        m_nHyperLinkCount++;
+        m_nHyperLinkCount.top()++;
     }
 
     // if there is some redlining in the document, output it
@@ -1930,7 +1931,7 @@ void DocxAttributeOutput::EndRun(const SwTextNode* pNode, sal_Int32 nPos, sal_In
 
             m_pSerializer->endElementNS( XML_w, XML_hyperlink );
             m_startedHyperlink = false;
-            m_nHyperLinkCount--;
+            m_nHyperLinkCount.top()--;
         }
         m_closeHyperlinkInThisRun = false;
     }
@@ -6271,8 +6272,8 @@ void DocxAttributeOutput::pushToTableExportContext(DocxTableExportContext& rCont
     rContext.m_bStartedRunSdt = m_aRunSdt.m_bStartedSdt;
     m_aRunSdt.m_bStartedSdt = false;
 
-    rContext.m_nHyperLinkCount = m_nHyperLinkCount;
-    m_nHyperLinkCount = 0;
+    rContext.m_nHyperLinkCount = m_nHyperLinkCount.top();
+    m_nHyperLinkCount.top() = 0;
 }
 
 void DocxAttributeOutput::popFromTableExportContext(DocxTableExportContext const & rContext)
@@ -6282,7 +6283,7 @@ void DocxAttributeOutput::popFromTableExportContext(DocxTableExportContext const
     m_tableReference.m_nTableDepth = rContext.m_nTableDepth;
     m_aParagraphSdt.m_bStartedSdt = rContext.m_bStartedParaSdt;
     m_aRunSdt.m_bStartedSdt = rContext.m_bStartedRunSdt;
-    m_nHyperLinkCount = rContext.m_nHyperLinkCount;
+    m_nHyperLinkCount.top() = rContext.m_nHyperLinkCount;
 }
 
 void DocxAttributeOutput::WriteTextBox(uno::Reference<drawing::XShape> xShape)
@@ -9955,7 +9956,6 @@ DocxAttributeOutput::DocxAttributeOutput( DocxExport &rExport, const FSHelperPtr
       m_closeHyperlinkInThisRun( false ),
       m_closeHyperlinkInPreviousRun( false ),
       m_startedHyperlink( false ),
-      m_nHyperLinkCount(0),
       m_nFieldsInHyperlink( 0 ),
       m_bExportingOutline(false),
       m_nChartCount(0),
@@ -9971,6 +9971,7 @@ DocxAttributeOutput::DocxAttributeOutput( DocxExport &rExport, const FSHelperPtr
       m_nParaAfterSpacing(0)
     , m_nStateOfFlyFrame( FLY_NOT_PROCESSED )
 {
+    m_nHyperLinkCount.push(0);
 }
 
 DocxAttributeOutput::~DocxAttributeOutput()

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -928,7 +928,7 @@ private:
     bool m_closeHyperlinkInPreviousRun;
     bool m_startedHyperlink;
     // Count nested HyperLinks
-    sal_Int32 m_nHyperLinkCount;
+    std::stack<sal_Int32> m_nHyperLinkCount;
     sal_Int16 m_nFieldsInHyperlink;
 
     // If the exported numbering rule defines the outlines

sw/qa/extras/ooxmlexport/data/lorem_hyperlink.fodt
@@ -1,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:officeooo="http://openoffice.org/2009/office" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:styles>
+  <style:default-style style:family="graphic">
+   <style:graphic-properties svg:stroke-color="#3465a4" draw:fill-color="#729fcf" fo:wrap-option="no-wrap" draw:shadow-offset-x="0.3cm" draw:shadow-offset-y="0.3cm" draw:start-line-spacing-horizontal="0.283cm" draw:start-line-spacing-vertical="0.283cm" draw:end-line-spacing-horizontal="0.283cm" draw:end-line-spacing-vertical="0.283cm" style:writing-mode="lr-tb" style:flow-with-text="false"/>
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:line-break="strict" style:font-independent-line-spacing="false">
+    <style:tab-stops/>
+   </style:paragraph-properties>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="NSimSun" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Lucida Sans1" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN"/>
+  </style:default-style>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties fo:orphans="2" fo:widows="2" fo:hyphenation-ladder-count="no-limit" style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:tab-stop-distance="1.251cm" style:writing-mode="page"/>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="NSimSun" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Lucida Sans1" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN" fo:hyphenate="false" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false" loext:hyphenation-no-last-word="false" loext:hyphenation-word-char-count="5" loext:hyphenation-zone="no-limit"/>
+  </style:default-style>
+  <style:style style:name="Internet_20_link" style:display-name="Internet link" style:family="text">
+   <style:text-properties fo:color="#000080" loext:opacity="100%" fo:language="zxx" fo:country="none" style:text-underline-style="solid" style:text-underline-width="auto" style:text-underline-color="font-color" style:language-asian="zxx" style:country-asian="none" style:language-complex="zxx" style:country-complex="none"/>
+  </style:style>
+ </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:parent-style-name="Standard">
+   <style:text-properties officeooo:rsid="00102359" officeooo:paragraph-rsid="00102359"/>
+  </style:style>
+  <style:style style:name="P2" style:family="paragraph">
+   <style:paragraph-properties style:writing-mode="lr-tb"/>
+  </style:style>
+  <style:style style:name="gr1" style:family="graphic">
+   <style:graphic-properties draw:textarea-horizontal-align="justify" draw:textarea-vertical-align="middle" draw:auto-grow-height="false" fo:min-height="0.781cm" fo:min-width="0.808cm" style:writing-mode="lr-tb" style:run-through="foreground" style:wrap="run-through" style:number-wrapped-paragraphs="no-limit" style:vertical-pos="from-top" style:vertical-rel="paragraph" style:horizontal-pos="from-left" style:horizontal-rel="paragraph" draw:wrap-influence-on-position="once-concurrent" loext:allow-overlap="true" style:flow-with-text="false"/>
+   <style:paragraph-properties style:writing-mode="lr-tb"/>
+  </style:style>
+ </office:automatic-styles>
+ <office:body>
+  <office:text>
+   <text:p text:style-name="P1"><text:a xlink:type="simple" xlink:href="#foo" text:style-name="Internet_20_link" text:visited-style-name="Visited_20_Internet_20_Link">Lorem <draw:custom-shape text:anchor-type="char" draw:z-index="0" draw:name="Shape 1" draw:style-name="gr1" draw:text-style-name="P2" svg:width="1.615cm" svg:height="1.562cm" svg:x="1cm" svg:y="1cm">
+      <text:p>1</text:p>
+      <draw:enhanced-geometry svg:viewBox="0 0 21600 21600" draw:glue-points="10800 0 0 10800 10800 21600 21600 10800" draw:text-areas="5400 5400 16200 16200" draw:type="diamond" draw:enhanced-path="M 10800 0 L 21600 10800 10800 21600 0 10800 10800 0 Z N"/>
+     </draw:custom-shape>ipsum</text:a></text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -872,6 +872,12 @@ DECLARE_OOXMLEXPORT_TEST(testTdf152310, "ColorOverwritten.docx")
                          getProperty<Color>(getParagraphOfText(2, xShape), "CharColor"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf149996, "lorem_hyperlink.fodt")
+{
+    // Without the accompanying fix in place, this test would have crashed,
+    // because the exported file was corrupted.
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */