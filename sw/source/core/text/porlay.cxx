LemmingAvalanche/libreoffice-repaired

commit f3a1bf473f2a7b9259223ba74f578b6cf0b1947d

sw: PDF/UA export: produce Lbl tagged element
Commit 6573eb8535499e963e8debf307bdf7475d966d39 did this for editengine
text in shapes, but it turns out it's a little more complicated in sw.

The SwTaggedPDFHelper nicely lived stack allocated, but list labels are
SwLinePortions inside a paragraph, and they can't be painted separately,
so if there's a list label the creation of LBody has to be delayed until
that is processed.

The SwNumberPortion can't even generate the Lbl itself, because there
can be multiple portions, they are broken across lines and at script
boundaries, and checking their follow flags is also tricky (see previous
commit).

Change-Id: I0dd383089a7ca0edddf6f805e79615c611a446f9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148556
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/porlay.cxx
@@ -2683,6 +2683,21 @@ TextFrameIndex SwParaPortion::GetParLen() const
     return nLen;
 }
 
+bool SwParaPortion::HasNumberingPortion() const
+{
+    SwLinePortion const* pPortion(nullptr);
+    // the first line may contain only fly portion...
+    for (SwLineLayout const* pLine = this; pLine && !pPortion; pLine = pLine->GetNext())
+    {
+        pPortion = pLine->GetFirstPortion();
+        while (pPortion && (pPortion->InGlueGrp() || pPortion->IsFlyPortion()))
+        {   // skip margins and fly spacers - numbering should be first then
+            pPortion = pPortion->GetNextPortion();
+        }
+    }
+    return pPortion && pPortion->InNumberGrp() && !pPortion->IsFootnoteNumPortion();
+}
+
 const SwDropPortion *SwParaPortion::FindDropPortion() const
 {
     const SwLineLayout *pLay = this;

sw/inc/EnhancedPDFExportHelper.hxx
@@ -110,8 +110,10 @@ struct Por_Info
 {
     const SwLinePortion& mrPor;
     const SwTextPainter& mrTextPainter;
-    Por_Info( const SwLinePortion& rPor, const SwTextPainter& rTextPainer )
-            : mrPor( rPor ), mrTextPainter( rTextPainer ) {};
+    bool const m_isNumberingLabel;
+
+    Por_Info(const SwLinePortion& rPor, const SwTextPainter& rTextPainer, bool const isNumberingLabel)
+        : mrPor(rPor), mrTextPainter(rTextPainer), m_isNumberingLabel(isNumberingLabel) {};
 };
 
 struct lt_TableColumn

sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -154,6 +154,7 @@ constexpr OUStringLiteral aH9String = u"H9";
 constexpr OUStringLiteral aH10String = u"H10";
 constexpr OUStringLiteral aListString = u"L";
 constexpr OUStringLiteral aListItemString = u"LI";
+constexpr OUStringLiteral aListLabelString = u"Lbl";
 constexpr OUStringLiteral aListBodyString = u"LBody";
 constexpr OUStringLiteral aBlockQuoteString = u"BlockQuote";
 constexpr OUStringLiteral aCaptionString = u"Caption";
@@ -475,6 +476,15 @@ void SwTaggedPDFHelper::BeginTag( vcl::PDFWriter::StructElement eType, const OUS
     else if ( mpFrameInfo )
     {
         const SwFrame& rFrame = mpFrameInfo->mrFrame;
+
+        if (vcl::PDFWriter::LIBody == eType && rFrame.IsTextFrame())
+        {
+            SwTextFrame const& rTextFrame(static_cast<const SwTextFrame&>(rFrame));
+            SwTextNode const*const pTextNd(rTextFrame.GetTextNodeForParaProps());
+            SwNodeNum const*const pNodeNum(pTextNd->GetNum(rTextFrame.getRootFrame()));
+            NumListBodyIdMap& rNumListBodyIdMap = SwEnhancedPDFExportHelper::GetNumListBodyIdMap();
+            rNumListBodyIdMap[ pNodeNum ] = nId;
+        }
 
         if ( ( rFrame.IsPageFrame() && !static_cast<const SwPageFrame&>(rFrame).GetPrev() ) ||
              ( rFrame.IsFlowFrame() && !SwFlowFrame::CastFlowFrame(&rFrame)->IsFollow() && SwFlowFrame::CastFlowFrame(&rFrame)->HasFollow() ) ||
@@ -1026,7 +1036,12 @@ void SwTaggedPDFHelper::BeginNumberedListStructureElements()
     if ( bNewItemTag )
     {
         BeginTag( vcl::PDFWriter::ListItem, aListItemString );
-        BeginTag( vcl::PDFWriter::LIBody, aListBodyString );
+        assert(rTextFrame.GetPara());
+        // check whether to open LIBody now or delay until after Lbl
+        if (!rTextFrame.GetPara()->HasNumberingPortion())
+        {
+            BeginTag(vcl::PDFWriter::LIBody, aListBodyString);
+        }
     }
 }
 
@@ -1125,8 +1140,15 @@ void SwTaggedPDFHelper::BeginBlockStructureElements()
 
         case SwFrameType::Txt :
             {
-                const SwTextNode* pTextNd =
-                    static_cast<const SwTextFrame*>(pFrame)->GetTextNodeForParaProps();
+                SwTextFrame const& rTextFrame(*static_cast<const SwTextFrame*>(pFrame));
+                // lazy open LIBody after Lbl
+                if (rTextFrame.GetPara()->HasNumberingPortion())
+                {
+                    assert(!rTextFrame.IsFollow());
+                    BeginTag(vcl::PDFWriter::LIBody, aListBodyString);
+                }
+
+                const SwTextNode *const pTextNd(rTextFrame.GetTextNodeForParaProps());
 
                 const SwFormat* pTextFormat = pTextNd->GetFormatColl();
                 const SwFormat* pParentTextFormat = pTextFormat ? pTextFormat->DerivedFrom() : nullptr;
@@ -1516,6 +1538,16 @@ void SwTaggedPDFHelper::BeginInlineStructureElements()
                         aPDFType = aBibEntryString;
                     }
                 }
+            }
+            break;
+
+        case PortionType::Number:
+        case PortionType::Bullet:
+        case PortionType::GrfNum:
+            if (mpPorInfo->m_isNumberingLabel)
+            {   // only works for multiple lines via wrapper from PaintSwFrame
+                nPDFType = vcl::PDFWriter::LILabel;
+                aPDFType = aListLabelString;
             }
             break;
 

sw/source/core/text/frmpaint.cxx
@@ -647,12 +647,6 @@ void SwTextFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const&
     // #i16816# tagged pdf support
     SwViewShell *pSh = getRootFrame()->GetCurrShell();
 
-    Num_Info aNumInfo( *this );
-    SwTaggedPDFHelper aTaggedPDFHelperNumbering( &aNumInfo, nullptr, nullptr, rRenderContext );
-
-    Frame_Info aFrameInfo( *this );
-    SwTaggedPDFHelper aTaggedPDFHelperParagraph( nullptr, &aFrameInfo, nullptr, rRenderContext );
-
     if( IsEmpty() && PaintEmpty( rRect, true ) )
         return;
 
@@ -677,6 +671,22 @@ void SwTextFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const&
             OSL_ENSURE( false, "+SwTextFrame::PaintSwFrame: missing format information" );
             return;
         }
+    }
+
+    Num_Info aNumInfo( *this );
+    SwTaggedPDFHelper aTaggedPDFHelperNumbering( &aNumInfo, nullptr, nullptr, rRenderContext );
+
+    // Lbl unfortunately must be able to contain multiple numbering portions
+    // that may be on multiple lines of text (but apparently always in the
+    // master frame), so it gets complicated.
+    ::std::optional<SwTaggedPDFHelper> oTaggedLabel;
+    // Paragraph tag - if there is a list label, opening should be delayed.
+    ::std::optional<SwTaggedPDFHelper> oTaggedParagraph;
+
+    if (IsFollow() || !GetPara()->HasNumberingPortion())
+    {   // no Lbl needed => open paragraph tag now
+        Frame_Info aFrameInfo(*this);
+        oTaggedParagraph.emplace(nullptr, &aFrameInfo, nullptr, rRenderContext);
     }
 
     // We don't want to be interrupted while painting.
@@ -763,7 +773,7 @@ void SwTextFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const&
         {
             do
             {
-                aLine.DrawTextLine( rRect, aClip, IsUndersized() );
+                aLine.DrawTextLine(rRect, aClip, IsUndersized(), oTaggedLabel, oTaggedParagraph);
 
             } while( aLine.Next() && aLine.Y() <= nBottom );
         }
@@ -780,6 +790,8 @@ void SwTextFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const&
 
     OSL_ENSURE( ! IsSwapped(), "A frame is swapped after Paint" );
 
+    assert(!oTaggedLabel); // must have been closed if opened
+    assert(oTaggedParagraph || rRect.GetIntersection(getFrameArea()) != getFrameArea()); // must have been created during complete paint (PDF export is always complete paint)
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/text/itrpaint.cxx
@@ -32,6 +32,7 @@
 #include <txtfrm.hxx>
 #include <swfont.hxx>
 #include "txtpaint.hxx"
+#include "porfld.hxx"
 #include "portab.hxx"
 #include <txatbase.hxx>
 #include <charfmt.hxx>
@@ -118,7 +119,9 @@ SwLinePortion *SwTextPainter::CalcPaintOfst( const SwRect &rPaint )
 //    (objectively slow, subjectively fast)
 // Since the user usually judges subjectively the second method is set as default.
 void SwTextPainter::DrawTextLine( const SwRect &rPaint, SwSaveClip &rClip,
-                                 const bool bUnderSized )
+    const bool bUnderSized,
+    ::std::optional<SwTaggedPDFHelper> & roTaggedLabel,
+    ::std::optional<SwTaggedPDFHelper> & roTaggedParagraph)
 {
 #if OSL_DEBUG_LEVEL > 1
 //    sal_uInt16 nFntHeight = GetInfo().GetFont()->GetHeight( GetInfo().GetVsh(), GetInfo().GetOut() );
@@ -386,15 +389,38 @@ void SwTextPainter::DrawTextLine( const SwRect &rPaint, SwSaveClip &rClip,
             GetInfo().SetUnderFnt( nullptr );
         }
 
+        // multiple numbering portions are possible :(
+        if (pPor->InNumberGrp() && !pPor->IsFootnoteNumPortion()
+            && !static_cast<SwNumberPortion const*>(pPor)->IsFollow())
+        {
+            assert(!roTaggedLabel);
+            assert(!m_pFrame->IsFollow());
+            Por_Info aPorInfo(*pPor, *this, true); // open Lbl
+            roTaggedLabel.emplace(nullptr, nullptr, &aPorInfo, *pOut);
+        }
+
         {
             // #i16816# tagged pdf support
-            Por_Info aPorInfo( *pPor, *this );
+            Por_Info aPorInfo(*pPor, *this, false);
             SwTaggedPDFHelper aTaggedPDFHelper( nullptr, nullptr, &aPorInfo, *pOut );
 
             if( pPor->IsMultiPortion() )
                 PaintMultiPortion( rPaint, static_cast<SwMultiPortion&>(*pPor) );
             else
                 pPor->Paint( GetInfo() );
+        }
+
+        // lazy open LIBody and paragraph tag after num portions have been painted to Lbl
+        if (pPor->InNumberGrp() && !pPor->IsFootnoteNumPortion()
+            // note: numbering portion may be split if it has multiple scripts
+            && !static_cast<SwNumberPortion const*>(pPor)->HasFollow()) // so wait for the last one
+        {
+            assert(roTaggedLabel);
+            roTaggedLabel.reset(); // close Lbl
+            assert(!roTaggedParagraph);
+            assert(!m_pFrame->IsFollow());
+            Frame_Info aFrameInfo(*m_pFrame); // open LIBody
+            roTaggedParagraph.emplace(nullptr, &aFrameInfo, nullptr, *pOut);
         }
 
         // reset underline font

sw/source/core/text/itrpaint.hxx
@@ -21,8 +21,11 @@
 
 #include "itrtxt.hxx"
 
+#include <optional>
+
 class SwSaveClip;          // SwTextPainter
 class SwMultiPortion;
+class SwTaggedPDFHelper;
 
 class SwTextPainter : public SwTextCursor
 {
@@ -46,7 +49,9 @@ public:
         CtorInitTextPainter( pTextFrame, pTextPaintInf );
     }
     void DrawTextLine( const SwRect &rPaint, SwSaveClip &rClip,
-                       const bool bUnderSz );
+        const bool bUnderSz,
+        ::std::optional<SwTaggedPDFHelper> & roTaggedLabel,
+        ::std::optional<SwTaggedPDFHelper> & roTaggedParagraph);
     void PaintDropPortion();
     // if PaintMultiPortion is called recursively, we have to pass the
     // surrounding SwBidiPortion

sw/source/core/text/porlay.hxx
@@ -319,6 +319,7 @@ public:
     bool IsFootnoteNum() const { return m_bFootnoteNum; }
     void SetMargin( const bool bNew = true ) { m_bMargin = bNew; }
     bool IsMargin() const { return m_bMargin; }
+    bool HasNumberingPortion() const;
 
     // Set nErgo in the QuoVadisPortion
     void SetErgoSumNum( const OUString &rErgo );

commit 6214787a843e5455bac0d9693136e9db9fa0bcf4

sw: fix crashtesting assert on ooo90040-2.doc
itrpaint.cxx:421: void SwTextPainter::DrawTextLine: Assertion `!roTaggedParagraph' failed

because the first portion is a SwKernPortion due to some text grid.

(regression from commit f3a1bf473f2a7b9259223ba74f578b6cf0b1947d)

Change-Id: I95b87871b7db3c7ced223b477b9f7522c1622d69
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149469
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/porlay.cxx
@@ -2690,7 +2690,7 @@ bool SwParaPortion::HasNumberingPortion(FootnoteOrNot const eFootnote) const
     for (SwLineLayout const* pLine = this; pLine && !pPortion; pLine = pLine->GetNext())
     {
         pPortion = pLine->GetFirstPortion();
-        while (pPortion && (pPortion->InGlueGrp() || pPortion->IsFlyPortion()))
+        while (pPortion && (pPortion->InGlueGrp() || pPortion->IsKernPortion() || pPortion->IsFlyPortion()))
         {   // skip margins and fly spacers - numbering should be first then
             pPortion = pPortion->GetNextPortion();
         }
@@ -2705,7 +2705,7 @@ bool SwParaPortion::HasContentPortions() const
     for (SwLineLayout const* pLine = this; pLine && !pPortion; pLine = pLine->GetNext())
     {
         pPortion = pLine->GetFirstPortion();
-        while (pPortion && (pPortion->InGlueGrp() || pPortion->IsFlyPortion()))
+        while (pPortion && (pPortion->InGlueGrp() || pPortion->IsKernPortion() || pPortion->IsFlyPortion()))
         {   // skip margins and fly spacers
             pPortion = pPortion->GetNextPortion();
         }