LemmingAvalanche/libreoffice-repaired

commit f1d7199864b71031d9de0af04ab456df9c6e4184

tdf#89449 PPTX import: fix line connectors
Line connectors were imported as plain shapes, losing
their functionality during editing, i.e. keeping
connections of boxes and other shapes.

Change-Id: I0f1562be2dbcce0e45eb209c6ca4e035731039e1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131303
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/shapes.cxx
@@ -1496,7 +1496,8 @@ ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape
         pFS->startElementNS(mnXmlNamespace, XML_cNvCxnSpPr);
         WriteConnectorConnections(aConnectorEntry, GetShapeID(rXShapeA), GetShapeID(rXShapeB));
         pFS->endElementNS(mnXmlNamespace, XML_cNvCxnSpPr);
-        pFS->singleElementNS(mnXmlNamespace, XML_nvPr);
+        if (GetDocumentType() == DOCUMENT_PPTX)
+            pFS->singleElementNS(mnXmlNamespace, XML_nvPr);
         pFS->endElementNS(mnXmlNamespace, XML_nvCxnSpPr);
     }
 

oox/source/ppt/slidepersist.cxx
@@ -41,6 +41,9 @@
 #include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
 #include <com/sun/star/container/XNamed.hpp>
 #include <com/sun/star/animations/XAnimationNodeSupplier.hpp>
+#include <com/sun/star/drawing/XGluePointsSupplier.hpp>
+#include <com/sun/star/container/XIdentifierContainer.hpp>
+#include <com/sun/star/drawing/EnhancedCustomShapeGluePointType.hpp>
 
 using namespace ::com::sun::star;
 using namespace ::oox::core;
@@ -143,11 +146,18 @@ void SlidePersist::createXShapes( XmlFilterBase& rFilterBase )
             PPTShape* pPPTShape = dynamic_cast< PPTShape* >( child.get() );
             basegfx::B2DHomMatrix aTransformation;
             if ( pPPTShape )
+            {
                 pPPTShape->addShape( rFilterBase, *this, getTheme().get(), xShapes, aTransformation, &getShapeMap() );
+                if (pPPTShape->isConnectorShape())
+                    maConnectorShapeId.push_back(pPPTShape->getId());
+            }
             else
                 child->addShape( rFilterBase, getTheme().get(), xShapes, aTransformation, maShapesPtr->getFillProperties(), &getShapeMap() );
         }
     }
+
+    if (!maConnectorShapeId.empty())
+        createConnectorShapeConnection();
 
     Reference< XAnimationNodeSupplier > xNodeSupplier( getPage(), UNO_QUERY);
     if( !xNodeSupplier.is() )
@@ -328,6 +338,66 @@ Reference<XAnimationNode> SlidePersist::getAnimationNode(const OUString& sId) co
 
     Reference<XAnimationNode> aResult;
     return aResult;
+}
+
+// create connection between two shape with a connector shape.
+void SlidePersist::createConnectorShapeConnection()
+{
+    sal_Int32 nConnectorShapeCount = maConnectorShapeId.size();
+    for (sal_Int32 i = 0; i < nConnectorShapeCount; i++)
+    {
+        const auto& pIt = maShapeMap.find(maConnectorShapeId[i]);
+        oox::drawingml::ConnectorShapePropertiesList aConnectorShapeProperties
+            = pIt->second->getConnectorShapeProperties();
+        uno::Reference<drawing::XShape> xConnector(pIt->second->getXShape(), uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> xPropertySet(xConnector, uno::UNO_QUERY);
+
+        if (xConnector.is())
+        {
+            sal_Int32 nCount = aConnectorShapeProperties.size();
+            for (sal_Int32 j = 0; j < nCount; j++)
+            {
+                OUString aDestShapeId = aConnectorShapeProperties[j].maDestShapeId;
+                const auto& pShape = maShapeMap.find(aDestShapeId);
+                uno::Reference<drawing::XShape> xShape(pShape->second->getXShape(), uno::UNO_QUERY);
+                uno::Reference<beans::XPropertySet> xSet(xShape, uno::UNO_QUERY);
+                if (xShape.is())
+                {
+                    uno::Reference<drawing::XGluePointsSupplier> xSupplier(xShape, uno::UNO_QUERY);
+                    css::uno::Reference<css::container::XIdentifierContainer> xGluePoints(
+                        xSupplier->getGluePoints(), uno::UNO_QUERY);
+
+                    sal_Int32 nCountGluePoints = xGluePoints->getIdentifiers().getLength();
+                    sal_Int32 nGlueId = aConnectorShapeProperties[j].mnDestGlueId;
+
+                    // The first 4 glue points belong to the bounding box.
+                    if (nCountGluePoints > 4)
+                        nGlueId += 4;
+                    else
+                    {
+                        // change id of the left and right glue points of the bounding box (1 <-> 3)
+                        if (nGlueId == 1)
+                            nGlueId = 3; // Right
+                        else if (nGlueId == 3)
+                            nGlueId = 1; // Left
+                    }
+
+                    bool bStart = aConnectorShapeProperties[j].mbStartShape;
+                    if (bStart)
+                    {
+                        xPropertySet->setPropertyValue("StartShape", uno::Any(xShape));
+                        xPropertySet->setPropertyValue("StartGluePointIndex", uno::Any(nGlueId));
+                    }
+                    else
+                    {
+                        xPropertySet->setPropertyValue("EndShape", uno::Any(xShape));
+                        xPropertySet->setPropertyValue("EndGluePointIndex", uno::Any(nGlueId));
+                    }
+                }
+            }
+        }
+    }
+    maConnectorShapeId.clear();
 }
 
 }

sd/qa/unit/import-tests.cxx
@@ -52,6 +52,7 @@
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/presentation/XCustomPresentationSupplier.hpp>
 #include <com/sun/star/drawing/EnhancedCustomShapeParameterPair.hpp>
+#include <com/sun/star/drawing/ConnectorType.hpp>
 
 #include <stlpool.hxx>
 #include <comphelper/sequenceashashmap.hxx>
@@ -80,6 +81,7 @@ public:
     virtual void setUp() override;
 
     void testDocumentLayout();
+    void testTdf89449();
     void testTdf147459();
     void testTdf146223();
     void testTdf144918();
@@ -147,6 +149,7 @@ public:
     CPPUNIT_TEST_SUITE(SdImportTest);
 
     CPPUNIT_TEST(testDocumentLayout);
+    CPPUNIT_TEST(testTdf89449);
     CPPUNIT_TEST(testTdf147459);
     CPPUNIT_TEST(testTdf146223);
     CPPUNIT_TEST(testTdf144918);
@@ -290,6 +293,42 @@ void SdImportTest::testDocumentLayout()
                 OUStringConcatenation(m_directories.getPathFromSrc( u"/sd/qa/unit/data/" ) + aFilesToCompare[i].sDump),
                 i == nUpdateMe );
     }
+}
+
+void SdImportTest::testTdf89449()
+{
+    sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/tdf89449.pptx"), PPTX);
+
+    sal_Int32 nStartGlueId;
+    sal_Int32 nEndGlueId;
+    css::drawing::ConnectorType aConnectorType;
+
+    uno::Reference<beans::XPropertySet> xCurvedConnector(getShapeFromPage(3, 0, xDocShRef));
+    xCurvedConnector->getPropertyValue("EdgeKind") >>= aConnectorType;
+    CPPUNIT_ASSERT_EQUAL(drawing::ConnectorType::ConnectorType_CURVE, aConnectorType);
+    nStartGlueId = xCurvedConnector->getPropertyValue("StartGluePointIndex").get<sal_Int32>();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nStartGlueId);
+    nEndGlueId = xCurvedConnector->getPropertyValue("EndGluePointIndex").get<sal_Int32>();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), nEndGlueId);
+
+    uno::Reference<beans::XPropertySet> xStraightConnector(getShapeFromPage(4, 0, xDocShRef));
+    xStraightConnector->getPropertyValue("EdgeKind") >>= aConnectorType;
+    CPPUNIT_ASSERT_EQUAL(drawing::ConnectorType::ConnectorType_LINE, aConnectorType);
+    nStartGlueId = xStraightConnector->getPropertyValue("StartGluePointIndex").get<sal_Int32>();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nStartGlueId);
+    nEndGlueId = xStraightConnector->getPropertyValue("EndGluePointIndex").get<sal_Int32>();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3), nEndGlueId);
+
+    uno::Reference<beans::XPropertySet> xStandardConnector(getShapeFromPage(5, 0, xDocShRef));
+    xStandardConnector->getPropertyValue("EdgeKind") >>= aConnectorType;
+    CPPUNIT_ASSERT_EQUAL(drawing::ConnectorType::ConnectorType_STANDARD, aConnectorType);
+    nStartGlueId = xStandardConnector->getPropertyValue("StartGluePointIndex").get<sal_Int32>();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3), nStartGlueId);
+    nEndGlueId = xStandardConnector->getPropertyValue("EndGluePointIndex").get<sal_Int32>();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), nEndGlueId);
+
+    xDocShRef->DoClose();
 }
 
 void SdImportTest::testTdf147459()

include/oox/drawingml/connectorshapecontext.hxx
@@ -25,16 +25,29 @@
 #include <oox/drawingml/drawingmltypes.hxx>
 #include <oox/drawingml/shapecontext.hxx>
 #include <sal/types.h>
+#include <vector>
 
 namespace oox { class AttributeList; }
 namespace oox::core { class ContextHandler2Helper; }
 
 namespace oox::drawingml {
 
+    struct ConnectorShapeProperties
+    {
+        bool mbStartShape;
+        OUString maDestShapeId;
+        sal_Int32 mnDestGlueId;
+    };
+
 class OOX_DLLPUBLIC ConnectorShapeContext final : public ShapeContext
 {
+    std::vector<ConnectorShapeProperties>& mrConnectorShapePropertiesList;
+    ShapePtr mpConnectorShapePtr;
+
 public:
-    ConnectorShapeContext( ::oox::core::ContextHandler2Helper const & rParent, const ShapePtr& pMasterShapePtr, const ShapePtr& pGroupShapePtr );
+    ConnectorShapeContext(::oox::core::ContextHandler2Helper const& rParent,
+                          const ShapePtr& pMasterShapePtr, const ShapePtr& pGroupShapePtr,
+                          std::vector<ConnectorShapeProperties>& rConnectorShapePropertiesList);
     virtual ~ConnectorShapeContext() override;
     virtual ::oox::core::ContextHandlerRef onCreateContext( ::sal_Int32 Element, const ::oox::AttributeList& rAttribs ) override;
 };

include/oox/drawingml/shape.hxx
@@ -34,6 +34,7 @@
 #include <oox/core/xmlfilterbase.hxx>
 #include <oox/dllapi.h>
 #include <oox/drawingml/color.hxx>
+#include <oox/drawingml/connectorshapecontext.hxx>
 #include <oox/drawingml/drawingmltypes.hxx>
 #include <oox/helper/helper.hxx>
 #include <oox/helper/propertymap.hxx>
@@ -72,6 +73,8 @@ class CustomShapeProperties;
 typedef std::shared_ptr< CustomShapeProperties > CustomShapePropertiesPtr;
 
 typedef ::std::map< OUString, ShapePtr > ShapeIdMap;
+
+typedef std::vector<ConnectorShapeProperties> ConnectorShapePropertiesList;
 
 struct ShapeStyleRef
 {
@@ -127,6 +130,10 @@ public:
     const GraphicProperties& getGraphicProperties() const { return *mpGraphicPropertiesPtr; }
 
     CustomShapePropertiesPtr&       getCustomShapeProperties(){ return mpCustomShapePropertiesPtr; }
+
+    ConnectorShapePropertiesList&   getConnectorShapeProperties() { return maConnectorShapePropertiesList; }
+    void                            setConnectorShape(bool bConnector) { mbConnector = bConnector; }
+    bool                            isConnectorShape() const { return mbConnector; }
 
     Shape3DProperties&              get3DProperties() { return *mp3DPropertiesPtr; }
     const Shape3DProperties&        get3DProperties() const { return *mp3DPropertiesPtr; }
@@ -332,6 +339,7 @@ protected:
     PropertyMap                 maDefaultShapeProperties;
     TextListStylePtr            mpMasterTextListStyle;
     css::uno::Reference< css::drawing::XShape > mxShape;
+    ConnectorShapePropertiesList maConnectorShapePropertiesList;
 
     OUString                    msServiceName;
     OUString                    msName;
@@ -393,6 +401,9 @@ private:
 
     /// The shape fill should be set to that of the slide background surface.
     bool mbUseBgFill = false;
+
+    // Is this a connector shape?
+    bool mbConnector = false;
 
     // temporary space for DiagramHelper in preparation for collecting data
     // Note: I tried to use a unique_ptr here, but existing constructor func does not allow that

include/oox/ppt/slidepersist.hxx
@@ -125,6 +125,8 @@ public:
     CommentList& getCommentsList() { return maCommentsList; }
     CommentAuthorList& getCommentAuthors() { return maCommentAuthors; }
 
+    void createConnectorShapeConnection();
+
 private:
     OUString                                                                maPath;
     OUString                                                                maLayoutPath;
@@ -156,6 +158,8 @@ private:
     // slide comments
     CommentList                                                             maCommentsList;
     CommentAuthorList                                                       maCommentAuthors;
+
+    std::vector<OUString>                                                   maConnectorShapeId;
 };
 
 }

oox/inc/drawingml/customshapeproperties.hxx
@@ -98,7 +98,6 @@ public:
     CustomShapeProperties();
 
     void pushToPropSet( const css::uno::Reference < css::beans::XPropertySet > & xPropSet,
-                        const css::uno::Reference < css::drawing::XShape > & xShape,
                         const css::awt::Size &aSize );
 
     sal_Int32 getShapePresetType() const { return mnShapePresetType; }

oox/source/drawingml/chart/chartdrawingfragment.cxx
@@ -157,7 +157,8 @@ ContextHandlerRef ChartDrawingFragment::onCreateContext( sal_Int32 nElement, con
                     return new ShapeContext( *this, ShapePtr(), mxShape );
                 case CDR_TOKEN( cxnSp ):
                     mxShape = std::make_shared<Shape>( "com.sun.star.drawing.ConnectorShape" );
-                    return new ConnectorShapeContext( *this, ShapePtr(), mxShape );
+                    return new ConnectorShapeContext(*this, ShapePtr(), mxShape,
+                                                     mxShape->getConnectorShapeProperties());
                 case CDR_TOKEN( pic ):
                     mxShape = std::make_shared<Shape>( "com.sun.star.drawing.GraphicObjectShape" );
                     return new GraphicShapeContext( *this, ShapePtr(), mxShape );

commit 287bd76500ebdb2d8860c8b83356bd6c6fed9655

tdf#154363 sd: fix line connectors regression of mirrored shapes
caused by commit f1d7199864b71031d9de0af04ab456df9c6e4184
(tdf#89449 PPTX import: fix line connectors).

Note: partial revert of commit 9b00a016c1e60e3ca6ca321060702de59497c16a
"oox: fix crash in lcl_GetGluePointId by removing unused code".

Change-Id: Icc45c93c4fa3a22c0f34866ccb64ea6b9037d936
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149676
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

oox/source/export/shapes.cxx
@@ -1640,17 +1640,38 @@ static void lcl_GetConnectorAdjustValue(const Reference<XShape>& xShape, tools::
     }
 }
 
-static sal_Int32 lcl_GetGluePointId(sal_Int32 nGluePointId)
+static sal_Int32 lcl_GetGluePointId(const Reference<XShape>& xShape, sal_Int32 nGluePointId)
 {
     if (nGluePointId > 3)
         return nGluePointId - 4;
     else
     {
-        // change id of the bounding box (1 <-> 3)
-        if (nGluePointId == 1)
-            return 3; // Right
-        else if (nGluePointId == 3)
-            return 1; // Left
+        bool bFlipH = false;
+        bool bFlipV = false;
+        Reference<XPropertySet> xShapeProps(xShape, UNO_QUERY);
+        if (xShapeProps->getPropertySetInfo()->hasPropertyByName("CustomShapeGeometry"))
+        {
+            Sequence<PropertyValue> aGeometrySeq;
+            xShapeProps->getPropertyValue("CustomShapeGeometry") >>= aGeometrySeq;
+            for (int i = 0; i < aGeometrySeq.getLength(); i++)
+            {
+                const PropertyValue& rProp = aGeometrySeq[i];
+                if (rProp.Name == "MirroredX")
+                    rProp.Value >>= bFlipH;
+
+                if (rProp.Name == "MirroredY")
+                    rProp.Value >>= bFlipV;
+            }
+        }
+
+        if ((!bFlipH && !bFlipV) || (bFlipH && bFlipV))
+        {
+            // change id of the bounding box (1 <-> 3)
+            if (nGluePointId == 1)
+                nGluePointId = 3; // Right
+            else if (nGluePointId == 3)
+                nGluePointId = 1; // Left
+        }
     }
 
     return nGluePointId;
@@ -1708,12 +1729,12 @@ ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape
     if (GetProperty(rXPropSet, "StartGluePointIndex"))
         mAny >>= nStartGlueId;
     if (nStartGlueId != -1)
-        nStartGlueId = lcl_GetGluePointId(nStartGlueId);
+        nStartGlueId = lcl_GetGluePointId(rXShapeA, nStartGlueId);
 
     if (GetProperty(rXPropSet, "EndGluePointIndex"))
         mAny >>= nEndGlueId;
     if (nEndGlueId != -1)
-        nEndGlueId = lcl_GetGluePointId(nEndGlueId);
+        nEndGlueId = lcl_GetGluePointId(rXShapeB, nEndGlueId);
 
     // Position is relative to group in Word, but relative to anchor of group in API.
     if (GetDocumentType() == DOCUMENT_DOCX && !mbUserShapes && m_xParent.is())

oox/source/ppt/slidepersist.cxx
@@ -659,11 +659,16 @@ void SlidePersist::createConnectorShapeConnection()
                         nGlueId += 4;
                     else
                     {
-                        // change id of the left and right glue points of the bounding box (1 <-> 3)
-                        if (nGlueId == 1)
-                            nGlueId = 3; // Right
-                        else if (nGlueId == 3)
-                            nGlueId = 1; // Left
+                        bool bFlipH = pShape->second->getFlipH();
+                        bool bFlipV = pShape->second->getFlipV();
+                        if ((!bFlipH && !bFlipV) || (bFlipH && bFlipV))
+                        {
+                            // change id of the left and right glue points of the bounding box (1 <-> 3)
+                            if (nGlueId == 1)
+                                nGlueId = 3; // Right
+                            else if (nGlueId == 3)
+                                nGlueId = 1; // Left
+                        }
                     }
 
                     bool bStart = aConnectorShapeProperties[j].mbStartShape;

sd/qa/unit/import-tests.cxx
@@ -191,6 +191,30 @@ CPPUNIT_TEST_FIXTURE(SdImportTest, testDocumentLayout)
     }
 }
 
+CPPUNIT_TEST_FIXTURE(SdImportTest, testTdf154363)
+{
+    sal_Int32 nGlueId;
+    createSdImpressDoc("pptx/tdf154363.pptx");
+    {
+        uno::Reference<beans::XPropertySet> xConnector1(getShapeFromPage(1, 0), uno::UNO_SET_THROW);
+        uno::Reference<beans::XPropertySet> xConnector2(getShapeFromPage(3, 0), uno::UNO_SET_THROW);
+        nGlueId = xConnector1->getPropertyValue("StartGluePointIndex").get<sal_Int32>();
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nGlueId);
+        nGlueId = xConnector2->getPropertyValue("EndGluePointIndex").get<sal_Int32>();
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nGlueId);
+    }
+
+    saveAndReload("Impress MS PowerPoint 2007 XML");
+    {
+        uno::Reference<beans::XPropertySet> xConnector1(getShapeFromPage(1, 0), uno::UNO_SET_THROW);
+        uno::Reference<beans::XPropertySet> xConnector2(getShapeFromPage(3, 0), uno::UNO_SET_THROW);
+        nGlueId = xConnector1->getPropertyValue("StartGluePointIndex").get<sal_Int32>();
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nGlueId);
+        nGlueId = xConnector2->getPropertyValue("EndGluePointIndex").get<sal_Int32>();
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nGlueId);
+    }
+}
+
 CPPUNIT_TEST_FIXTURE(SdImportTest, testTdf153466)
 {
     createSdImpressDoc("pptx/tdf153466.pptx");