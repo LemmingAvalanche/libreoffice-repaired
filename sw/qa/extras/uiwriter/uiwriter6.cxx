LemmingAvalanche/libreoffice-repaired

commit 2690de4ed43b610453e2a533849bd5aabc136d62

tdf#146967 sw table: fix freezing in Hide Changes mode
During change tracking, drag & drop or Cut & Paste a
table row resulted freezing in Hide Changes mode.

Test: In Hide Changes mode, select a table row by
clicking on their left border, and drag & drop or
Cut & Paste it in a different table row.

Regression from commit 4c76c3871451f561a3979670b10d0cf2ce6ccd73.

Follow-up to commit 4c76c3871451f561a3979670b10d0cf2ce6ccd73
tdf#146965 sw track changes: fix tracked table row moving

Change-Id: Iab606ce036fedcc70e42638e3ccf804c9dbca064
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128875
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -1619,8 +1619,8 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
             bool bResult = SwTransferable::PasteData( rData, rSh, nAction, nActionFlags, nFormat,
                                         nDestination, false, false, nullptr, 0, false, nAnchorType, bIgnoreComments, &aPasteContext );
 
-            // set tracked insertion
-            if ( eOld & RedlineFlags::On )
+            // set tracked insertion, if it's not in a drag & drop action
+            if ( !rSh.ActionPend() && ( eOld & RedlineFlags::On) )
             {
                 SvxPrintItem aTracked(RES_PRINT, false);
                 rSh.GetDoc()->SetRowNotTracked( *rSh.GetCursor(), aTracked );
@@ -3925,6 +3925,11 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
         {
             bool bTableCol(SelectionType::TableCol & nSelection);
 
+            ::sw::mark::IMark* pMarkMoveFrom = rSh.SetBookmark(
+                                    vcl::KeyCode(),
+                                    OUString(),
+                                    IDocumentMarkAccess::MarkType::UNO_BOOKMARK );
+
             SwUndoId eUndoId = bMove ? SwUndoId::UI_DRAG_AND_MOVE : SwUndoId::UI_DRAG_AND_COPY;
 
             SwRewriter aRewriter;
@@ -3951,11 +3956,27 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                                     OUString(),
                                     IDocumentMarkAccess::MarkType::UNO_BOOKMARK );
 
+            // paste rows above/columns before
+            pDispatch->Execute(bTableCol ? FN_TABLE_PASTE_COL_BEFORE : FN_TABLE_PASTE_ROW_BEFORE, SfxCallMode::SYNCHRON);
+
+            // go to the previously inserted table row and set it to tracked insertion
+            rSh.Up(false);
+            SvxPrintItem aTracked(RES_PRINT, false);
+            rSh.GetDoc()->SetRowNotTracked( *rSh.GetCursor(), aTracked );
+
             rSrcSh.Pop(SwCursorShell::PopMode::DeleteCurrent); // restore selection...
 
             // delete source rows/columns
             if (bMove)
+            {
+                // restore cursor position
+                if (pMarkMoveFrom != nullptr)
+                {
+                    rSh.GotoMark( pMarkMoveFrom );
+                    rSh.getIDocumentMarkAccess()->deleteMark( pMarkMoveFrom );
+                }
                 pDispatch->Execute(bTableCol ? FN_TABLE_DELETE_COL : FN_TABLE_DELETE_ROW, SfxCallMode::SYNCHRON);
+            }
 
             // restore cursor position
             if (pMark != nullptr)
@@ -3963,12 +3984,6 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                 rSh.GotoMark( pMark );
                 rSh.getIDocumentMarkAccess()->deleteMark( pMark );
             }
-
-            // paste rows above/columns before
-            pDispatch->Execute(bTableCol ? FN_TABLE_PASTE_COL_BEFORE : FN_TABLE_PASTE_ROW_BEFORE, SfxCallMode::SYNCHRON);
-
-            if( rSrcSh.GetDoc() != rSh.GetDoc() )
-                rSrcSh.EndUndo();
 
             rSh.DestroyCursor();
             rSh.EndUndo();

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -5110,6 +5110,52 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testPasteTrackedTableRow)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTable->getRows()->getCount());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testPasteTrackedTableRowInHideChangesMode)
+{
+    // load a 1-row table
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf118311.fodt");
+
+    // turn on red-lining and show changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE("redlines should be invisible",
+                           !IDocumentRedlineAccess::IsShowChanges(
+                               pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    // check table count
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    // check table row count
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTable->getRows()->getCount());
+
+    // copy table row and paste it by Paste Special->Rows Above
+    dispatchCommand(mxComponent, ".uno:SelectTable", {});
+    dispatchCommand(mxComponent, ".uno:Copy", {});
+    dispatchCommand(mxComponent, ".uno:Escape", {});
+
+    // This resulted freezing
+    dispatchCommand(mxComponent, ".uno:PasteRowsBefore", {});
+
+    // 2-row table
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTable->getRows()->getCount());
+
+    // This was 2 (inserted as a nested table in the first cell of the new row)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {}); // FIXME Why 3 Undos?
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTable->getRows()->getCount());
+
+    dispatchCommand(mxComponent, ".uno:Redo", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTable->getRows()->getCount());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf145091)
 {
     // load a deleted table, reject them, and delete only its text and export to DOCX

commit 0b1d7d95d380e6b88256fc0a981ed92a2dd69a4d

tdf#154771 sw: fix drag & drop moving of table columns
Only first selected column was moved, the other ones
were copied during drag & drop moving of table columns.

Regression from commit 2690de4ed43b610453e2a533849bd5aabc136d62
"tdf#146967 sw table: fix freezing in Hide Changes mode".

Follow-up to commit 4f251af7e8882fd79d8cecaedfa9b374892d8b0c
"tdf#154599 sw: fix crash at drag & drop table columns".

Change-Id: I31ca5b3d6004ed53c11a951fa184b9db48f21041
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150280
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -3978,10 +3978,37 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
             // up to down, if the cursor is there in its last table row
             const SwSelBoxes& rBoxes = rSrcSh.GetTableCursor()->GetSelectedBoxes();
             const SwTableNode* pTableNd = rSh.IsCursorInTable();
-            sal_Int32 nSelRows = !rBoxes.back()
-                ? 0
-                : pTableNd->GetTable().GetTabLines().GetPos( rBoxes.back()->GetUpper() ) -
-                  pTableNd->GetTable().GetTabLines().GetPos( rBoxes.front()->GetUpper() ) + 1;
+            const SwTableLines& rLines = pTableNd->GetTable().GetTabLines();
+            const SwStartNode& rDelPos = rBoxes.back()
+                    ? *rBoxes.front()->GetSttNd()
+                    : *pTableNd->GetStartNode();
+
+            // count selected rows or columns
+            sal_Int32 nSelRowOrCols = 0;
+            if ( rBoxes.back() )
+            {
+                if ( bTableCol )
+                {
+                    // selected column count is the count of the cells
+                    // in the first row of the selection
+                    auto nLine = rLines.GetPos( rBoxes.front()->GetUpper() );
+                    for (auto pBox : rBoxes)
+                    {
+                        // cell is in the next row
+                        if ( nLine != rLines.GetPos( pBox->GetUpper() ) )
+                            break;
+                        ++nSelRowOrCols;
+                    }
+                }
+                else
+                {
+                   // selected row count is the difference of the row number of the
+                   // first and the last cell of the selection
+                   nSelRowOrCols = rLines.GetPos( rBoxes.back()->GetUpper() ) -
+                                   rLines.GetPos( rBoxes.front()->GetUpper() ) + 1;
+                }
+            }
+
             bool bSelUpToDown = rBoxes.back() && rBoxes.back()->GetUpper() ==
                            rSh.GetCursor()->GetPointNode().GetTableBox()->GetUpper();
 
@@ -4038,7 +4065,7 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                 const SwTableBox* pBoxStt = rSh.GetCursor()->GetPointNode().GetTableBox();
                 SwTableLine* pLine = pBoxStt ? const_cast<SwTableLine*>( pBoxStt->GetUpper()): nullptr;
 
-                for (sal_Int32 nDeleted = 0; bNeedTrack && nDeleted < nSelRows;)
+                for (sal_Int32 nDeleted = 0; bNeedTrack && nDeleted < nSelRowOrCols;)
                 {
                     // move up text cursor (note: "true" is important for the layout level)
                     if ( !rSh.Up(false) )
@@ -4072,12 +4099,14 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                         rSh.getIDocumentMarkAccess()->deleteMark( pMarkMoveFrom );
                     }
 
-                    // set all row as tracked deletion, otherwise go to the first moved row
-                    if ( bNeedTrack || ( bSelUpToDown && nSelRows > 1 ) )
+                    // tracked table row moving: set original rows as tracked deletion,
+                    // otherwise delete original rows/columns (tracking column deletion
+                    // and insertion is not supported yet)
+                    if ( !bTableCol && bNeedTrack )
                     {
                         pLine = nullptr;
 
-                        for (sal_Int32 nDeleted = 0; nDeleted < nSelRows - int(!bNeedTrack);)
+                        for (sal_Int32 nDeleted = 0; nDeleted < nSelRowOrCols;)
                         {
                             const SwTableBox* pBox = rSh.GetCursor()->GetPointNode().GetTableBox();
 
@@ -4087,10 +4116,7 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                             if ( pBox->GetUpper() != pLine )
                             {
                                 pLine = const_cast<SwTableLine*>(pBox->GetUpper());
-                                if (bNeedTrack)
-                                    pDispatch->Execute(bTableCol
-                                        ? FN_TABLE_DELETE_COL
-                                        : FN_TABLE_DELETE_ROW, SfxCallMode::SYNCHRON);
+                                pDispatch->Execute(FN_TABLE_DELETE_ROW, SfxCallMode::SYNCHRON);
                                 ++nDeleted;
                             }
 
@@ -4103,19 +4129,17 @@ bool SwTransferable::PrivateDrop( SwWrtShell& rSh, const Point& rDragPt,
                                 break;
                         }
                     }
-
-                    // delete original rows/columns, except in track changes mode
-                    // TODO remove all the columns, not only the first one
-                    if ( !bNeedTrack )
+                    else
                     {
-                        for (sal_Int32 nDeleted = 0; nDeleted < nSelRows; ++nDeleted)
+                        // set cursor in the first cell of the original selection
+                        rSh.GetCursor()->DeleteMark();
+                        rSh.GetCursor()->GetPoint()->Assign( rDelPos.GetIndex() + 1);
+
+                        for (sal_Int32 nDeleted = 0; nDeleted < nSelRowOrCols; ++nDeleted)
                         {
                             pDispatch->Execute(bTableCol
                                 ? FN_TABLE_DELETE_COL
                                 : FN_TABLE_DELETE_ROW, SfxCallMode::SYNCHRON);
-
-                            if ( bTableCol )
-                                break;
                         }
                     }
                 }

sw/qa/extras/uiwriter/uiwriter6.cxx
@@ -981,6 +981,62 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf154599_MovingColumn)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(3), xTable1->getColumns()->getCount());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf154771_MovingMultipleColumns)
+{
+    createSwDoc();
+    SwDoc* pDoc = getSwDoc();
+    CPPUNIT_ASSERT(pDoc);
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    // Create a table with less columns than rows
+    SwInsertTableOptions TableOpt(SwInsertTableFlags::DefaultBorder, 0);
+    (void)&pWrtShell->InsertTable(TableOpt, 5, 4);
+
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    uno::Reference<container::XNameAccess> xTableNames = xTablesSupplier->getTextTables();
+    CPPUNIT_ASSERT(xTableNames->hasByName("Table1"));
+    uno::Reference<text::XTextTable> xTable1(xTableNames->getByName("Table1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(5), xTable1->getRows()->getCount());
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xTable1->getColumns()->getCount());
+
+    // without redlining
+    CPPUNIT_ASSERT_MESSAGE("redlining should be off",
+                           !pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+
+    // Move first two columns of the table before column D by drag & drop
+
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    SwFrame* pPage = pLayout->Lower();
+    SwFrame* pBody = pPage->GetLower();
+    SwFrame* pTable = pBody->GetLower();
+    SwFrame* pRow1 = pTable->GetLower();
+    SwFrame* pCellA1 = pRow1->GetLower();
+    SwFrame* pCellB1 = pCellA1->GetNext();
+    SwFrame* pCellD1 = pCellB1->GetNext()->GetNext();
+    const SwRect& rCellA1Rect = pCellA1->getFrameArea();
+    const SwRect& rCellB1Rect = pCellB1->getFrameArea();
+    const SwRect& rCellD1Rect = pCellD1->getFrameArea();
+    Point ptTo(rCellD1Rect.Left() + rCellD1Rect.Width() / 2,
+               rCellD1Rect.Top() + rCellD1Rect.Height() / 2);
+    // select first two table columns by using
+    // the middle point of the top border of column A
+    // and middle point of the top border of column B
+    Point ptColumnA(rCellA1Rect.Left() + rCellA1Rect.Width() / 2, rCellA1Rect.Top() - 5);
+    const Point ptColumnB(rCellB1Rect.Left() + rCellB1Rect.Width() / 2, rCellB1Rect.Top() - 5);
+    pWrtShell->SelectTableRowCol(ptColumnA, &ptColumnB);
+
+    rtl::Reference<SwTransferable> xTransfer = new SwTransferable(*pWrtShell);
+    xTransfer->PrivateDrop(*pWrtShell, ptTo, /*bMove=*/true, /*bXSelection=*/true);
+
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(5), xTable1->getRows()->getCount());
+    // This was 5 before the fix (only the first selected column was moved, the
+    // other ones were copied instead of moving)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xTable1->getColumns()->getCount());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf115132)
 {
     createSwDoc();